291d302035b84b2accb071a54e47a3057e148f78
Raymond Kim <rkim@tenstorrent.com>
2023-02-22 16:22:07 +0000

    #11: Add initial script for checking commit msg, must use env

--- git_hooks/verify_commit_message.py
+++ git_hooks/verify_commit_message.py
@@ -1,4 +1,12 @@
 import sys
+import re
+
+"""
+Commit msg must be in the format:
+"""
+FORMAT_MSG = "#<GH ISSUE NUMBER>: <non-empty message>"
+MATCHING_REGEX = "^(#\d+\:\ .)"
+
 
 if __name__ == "__main__":
     argv = sys.argv
@@ -8,5 +16,15 @@ if __name__ == "__main__":
 
     commit_msg_filename = argv[1]
 
-    for line in open(commit_msg_filename):
-        pass
+    with open(commit_msg_filename) as commit_msg_file:
+        commit_msg_whole = commit_msg_file.read()
+
+    search_result = re.search(MATCHING_REGEX, commit_msg_whole)
+
+    if not search_result:
+        raise Exception(f"Commit message does match format {FORMAT_MSG}")
+
+    result_groups = search_result.groups(default=tuple())
+
+    if not result_groups:
+        raise Exception("Regex matching error during commit message sanitization")
