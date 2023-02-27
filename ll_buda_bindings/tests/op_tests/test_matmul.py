#53: Bmm/matmul - broadcast fixes for bert, extend test

ll_buda_bindings/tests/op_tests/test_matmul.py
@@ -1,7 +1,7 @@
 import ll_buda_bindings.ll_buda_bindings._C as _C
 import torch
 from python_api_testing.models.utility_functions import pad_activation, pad_weight, tilize, untilize, tilize_to_list, print_diff_argmax, pad_weight, is_close
-from python_api_testing.models.utility_functions import enable_compile_cache, enable_binary_cache
+#from python_api_testing.models.utility_functions import enable_compile_cache, enable_binary_cache
 
 def test_matmul():
 
@@ -33,6 +33,27 @@ def test_matmul():
 
     ref_bmm = torch.matmul(A.reshape(batch,M,K), B.reshape(1, K, N))
     ref_bmm = ref_bmm.reshape(batch, 1, M, N)
+    maxmag = ref_bmm.abs().max().item() # % of max magnitude since that determines cancellations
+    match = is_close(pyt_got_back_rm, ref_bmm, 0.07, 0.07, maxmag, 0.01)
+    print("Match=", match.item())
+
+    # test bmm variant without bcast
+    # batch*C are treated as batch
+    C = 2
+    A = torch.randn((batch,C,M,K))
+    B = torch.randn((batch,C,K,N)) - 0.95
+
+    t0 = _C.tensor.Tensor(tilize_to_list(A), [batch, C, M, K], _C.tensor.DataFormat.FLOAT32, _C.tensor.Layout.TILE, device)
+    t1 = _C.tensor.Tensor(tilize_to_list(B), [batch, C, K, N], _C.tensor.DataFormat.FLOAT32, _C.tensor.Layout.TILE, device)
+
+    t2 = _C.tensor.bmm(t0, t1)
+    assert(t2.shape() == [batch, C, M, N])
+    tt_host_rm = t2.to(host).data()
+    pyt_got_back = torch.Tensor(tt_host_rm).reshape((batch,C,M,N))
+    pyt_got_back_rm = untilize(pyt_got_back)
+
+    ref_bmm = torch.bmm(A.reshape(batch*C,M,K), B.reshape(batch*C, K, N))
+    ref_bmm = ref_bmm.reshape(batch, C, M, N)
     maxmag = ref_bmm.abs().max().item() # % of max magnitude since that determines cancellations
     match = is_close(pyt_got_back_rm, ref_bmm, 0.07, 0.07, maxmag, 0.01)
     print("Match=", match.item())

python_api_testing/models/bert/bert.py
@@ -62,7 +62,7 @@ def run_bert_question_and_answering_inference():
     hugging_face_reference_model.eval()
     tt_bert_model = TtBertForQuestionAnswering(2, 2, hugging_face_reference_model, device)
 
-    batch = 2
+    batch = 1
     seq_len = 128
     bert_input = torch.arange(seq_len*batch).reshape(batch, seq_len)
 