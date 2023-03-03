090c2374118ff8e36678d127e25198c99116efbf
Raymond Kim <rkim@tenstorrent.com>
2023-03-03 15:11:30 -0500

    #81: Add tt-smi installation and asset download script which fetches TT-SMI from VPN

---
+++ release/machine_setup/scripts/download_machine_setup_assets.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+BUDA_HOME=$(git rev-parse --show-toplevel)
+ASSETS_DIR=$BUDA_HOME/release/machine_setup/assets
+
+SERVER=yyz-lab-65
+GS_TT_SMI_SERVER_LOCATION=/home/software/syseng/bin/tt-smi/gs/tt-smi_2022-12-05-74801e089fb2e564
+WH_TT_SMI_SERVER_LOCATION=/home/software/syseng/bin/tt-smi/wh/tt-smi-wh_2023-02-07-eda6cb21c5788763
+GS_TT_SMI_LOCAL_FOLDER=$ASSETS_DIR/tt_smi/gs
+WH_TT_SMI_LOCAL_FOLDER=$ASSETS_DIR/tt_smi/wh
+GS_TT_SMI_LOCAL_LOCATION=$GS_TT_SMI_LOCAL_FOLDER/tt-smi
+WH_TT_SMI_LOCAL_LOCATION=$WH_TT_SMI_LOCAL_FOLDER/tt-smi
+
+rm -rf $ASSETS_DIR
+mkdir -p $GS_TT_SMI_LOCAL_FOLDER
+mkdir -p $WH_TT_SMI_LOCAL_FOLDER
+
+scp $SERVER:"$GS_TT_SMI_SERVER_LOCATION" $GS_TT_SMI_LOCAL_LOCATION
+scp $SERVER:"$WH_TT_SMI_SERVER_LOCATION" $WH_TT_SMI_LOCAL_LOCATION
