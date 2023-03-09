d56ae74a3874f673538c243f5e668138361459eb
Andrei Pokrovsky <apokrovsky@tenstorrent.com>
2023-03-09 19:41:15 +0000

    #154: fix dependency in tt_metal/module.mk

--- tt_metal/module.mk
+++ tt_metal/module.mk
@@ -1,6 +1,7 @@
 # Every variable in subdir must be prefixed with subdir (emulating a namespace)
 
 TT_METAL_LIB = $(LIBDIR)/libtt_metal.a
+TT_BUILD_LIB = $(LIBDIR)/libbuild_kernels_for_riscv.a
 TT_METAL_DEFINES = -DGIT_HASH=$(shell git rev-parse HEAD)
 TT_METAL_INCLUDES = $(COMMON_INCLUDES) -I$(TT_METAL_HOME)/tt_metal -I$(TT_METAL_HOME)/.
 TT_METAL_LDFLAGS = -L$(TT_METAL_HOME) -lcommon -lbuild_kernels_for_riscv -lllrt -ltt_metal_impl
@@ -36,7 +37,7 @@ TT_METAL_DEPS = $(addprefix $(OBJDIR)/, $(TT_METAL_SRCS:.cpp=.d))
 # Each module has a top level target as the entrypoint which must match the subdir name
 tt_metal: $(TT_METAL_LIB)
 
-$(TT_METAL_LIB): $(COMMON_LIB) $(TT_METAL_OBJS) $(TT_METAL_IMPL_LIB) $(LLRT_LIB)
+$(TT_METAL_LIB): $(COMMON_LIB) $(TT_METAL_OBJS) $(TT_METAL_IMPL_LIB) $(LLRT_LIB) $(TT_BUILD_LIB)
 	@mkdir -p $(LIBDIR)
 	$(CXX) $(TT_METAL_CFLAGS) $(CXXFLAGS) $(SHARED_LIB_FLAGS) -o $@ $^ $(LDFLAGS) $(TT_METAL_LDFLAGS)
 
