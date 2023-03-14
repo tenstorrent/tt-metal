1aa18926f95f05d9f34e2326827b73a0ce5eabfc
Raymond Kim <rkim@tenstorrent.com>
2023-03-14 19:42:28 +0000

    #191: Remove loguru and black from python_api_testing because it's unnecessary as it's in python_env/dev, but move torch and numpy to pymetal setup.py because we temporarily need it for utilities

--- pymetal/setup.py
+++ pymetal/setup.py
@@ -4,6 +4,10 @@ setup(
     name="ttmetal",
     version="0.1",
     description="",
+    install_requires=[
+        "torch==1.13.1",
+        "numpy==1.20.3",
+    ],
     python_requires='>=3.8',
     packages=["ttmetal"],
     package_dir={"ttmetal": "ttmetal"},
