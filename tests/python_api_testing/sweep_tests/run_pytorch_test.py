1f7e7198aa1cddc51f39394011c32cae34ba336f
Brian Liu <bliu@tenstorrent.com>
2023-03-18 17:55:42 +0000

    #0: Add prints for input shape and data seed when running sweep tests

--- tests/python_api_testing/sweep_tests/run_pytorch_test.py
+++ tests/python_api_testing/sweep_tests/run_pytorch_test.py
@@ -109,6 +109,7 @@ def run_pytorch_test(args):
                 data_seed = int(time.time())
                 torch.manual_seed(data_seed)
 
+                logger.info(f"Running with shape: {input_shapes} and seed: {data_seed}")
                 test_pass = run_test_and_save_results(
                     results_csv_writer,
                     test_name,
