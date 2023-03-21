2de80c7c0493db764a52dd04e48150b2563b3daa
coffeeandcheesecake <apokrovsky@tenstorrent.com>
2023-03-21 00:55:21 +0000

    #174: update relative path to nanoGPT checkpoint

--- python_api_testing/models/nanogpt/run.sh
+++ python_api_testing/models/nanogpt/run.sh
@@ -1,2 +1,2 @@
 python data/shakespeare_char/prepare.py
-python sample.py --out_dir=../../../third_party/lfs/nanogpt/out-shakespeare-char
+python sample.py --out_dir=../../../tt_metal/third_party/lfs/nanogpt/out-shakespeare-char
