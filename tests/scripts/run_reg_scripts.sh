4f1cfabeb05ae0e17bce766a13ef56ce60e34c58
Raymond Kim <rkim@tenstorrent.com>
2023-04-04 04:39:23 +0000

    #300: Add generic regression script to be deleted later

---
+++ tests/scripts/run_reg_scripts.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+set -eo pipefail
+
+if [[ -z "$TT_METAL_HOME" ]]; then
+  echo "Must provide TT_METAL_HOME in environment" 1>&2
+  exit 1
+fi
+
+cd $TT_METAL_HOME
+
+make clean
+make build
+make tests
+
+source build/python_env/bin/activate
+export PYTHONPATH=$TT_METAL_HOME
+python -m pip install -r tests/python_api_testing/requirements.txt
+
+env python tests/scripts/run_build_kernels_for_riscv.py -j 16
+env python tests/scripts/run_llrt.py --skip-driver-tests
+env ./build/test/llrt/test_silicon_driver
+env python tests/scripts/run_tt_metal.py
+
+env pytest $TT_METAL_HOME/tests/python_api_testing/unit_testing/ -s
+env python tests/python_api_testing/models/bert/bert.py
