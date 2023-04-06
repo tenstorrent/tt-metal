eb1f1cc1336a8f70368aa8f570bd4be472446e66
Brian Liu <bliu@tenstorrent.com>
2023-04-06 18:07:52 +0000

    #324: Add padding and unpadding tests

--- tests/python_api_testing/unit_testing/test_padding_test.py
+++ tests/python_api_testing/unit_testing/test_padding_test.py
@@ -13,9 +13,103 @@ from libs import tt_lib as ttl
 
 @pytest.mark.parametrize(
     "input_tensor_shape, output_tensor_shape, input_tensor_start, pad_value",
-    (((1, 1, 3, 4), (1, 1, 32, 32), (0, 0, 1, 1), 0),),
+    (
+        ((1, 1, 3, 3), (1, 1, 5, 5), (0, 0, 1, 1), 0),
+        ((1, 1, 3, 3), (2, 2, 5, 5), (0, 0, 0, 0), -1),
+        ((1, 3, 30, 30), (1, 3, 32, 32), (0, 0, 0, 0), 1),
+        ((1, 3, 30, 30), (3, 5, 32, 32), (1, 2, 0, 0), torch.inf),
+        ((1, 3, 30, 30), (3, 3, 64, 64), (0, 0, 31, 31), -torch.inf),
+    ),
 )
 def test_run_padding_test(
+    input_tensor_shape, output_tensor_shape, input_tensor_start, pad_value
+):
+    inp = torch.rand(*input_tensor_shape, dtype=torch.bfloat16)
+
+    # Create tensor on host
+    a = ttl.tensor.Tensor(
+        inp.reshape(-1).tolist(),
+        input_tensor_shape,
+        ttl.tensor.DataType.BFLOAT16,
+        ttl.tensor.Layout.ROW_MAJOR,
+    )
+
+    # Pad inputs on host
+    a_pad = a.pad(output_tensor_shape, input_tensor_start, pad_value)
+    a_pt = torch.Tensor(a_pad.data()).reshape(*output_tensor_shape)
+
+    # Pytorch reference
+    input_tensor_end = tuple(
+        input_tensor_start[i] + input_tensor_shape[i]
+        for i in range(len(input_tensor_shape))
+    )
+    a_ref = torch.ones(*output_tensor_shape, dtype=torch.bfloat16) * pad_value
+    a_ref[
+        input_tensor_start[0] : input_tensor_end[0],
+        input_tensor_start[1] : input_tensor_end[1],
+        input_tensor_start[2] : input_tensor_end[2],
+        input_tensor_start[3] : input_tensor_end[3],
+    ] = inp
+
+    print("\n", a_pt.shape)
+    print("\n", a_pt)
+    print("\n", a_ref)
+
+    assert a_pt.shape == output_tensor_shape
+    assert torch.equal(a_pt, a_ref)
+
+
+@pytest.mark.parametrize(
+    "input_tensor_shape, output_tensor_start, output_tensor_end",
+    (
+        ((1, 1, 5, 5), (0, 0, 1, 1), (0, 0, 3, 3)),
+        ((2, 2, 5, 5), (0, 0, 0, 0), (0, 0, 2, 2)),
+        ((1, 3, 32, 32), (0, 0, 0, 0), (0, 2, 29, 29)),
+        ((3, 5, 32, 32), (1, 2, 0, 0), (1, 4, 29, 29)),
+        ((3, 3, 64, 64), (0, 0, 32, 32), (0, 2, 61, 61)),
+    ),
+)
+def test_run_unpadding_test(input_tensor_shape, output_tensor_start, output_tensor_end):
+    inp = torch.rand(*input_tensor_shape, dtype=torch.bfloat16)
+
+    # Create tensor on host
+    a = ttl.tensor.Tensor(
+        inp.reshape(-1).tolist(),
+        input_tensor_shape,
+        ttl.tensor.DataType.BFLOAT16,
+        ttl.tensor.Layout.ROW_MAJOR,
+    )
+
+    # Unpad inputs on host
+    output_tensor_shape = tuple(
+        output_tensor_end[i] - output_tensor_start[i] + 1
+        for i in range(len(input_tensor_shape))
+    )
+    a_unpad = a.unpad(output_tensor_start, output_tensor_end)
+    a_pt = torch.Tensor(a_unpad.data()).reshape(*output_tensor_shape)
+
+    # Pytorch reference
+    a_ref = inp[
+        output_tensor_start[0] : output_tensor_end[0] + 1,
+        output_tensor_start[1] : output_tensor_end[1] + 1,
+        output_tensor_start[2] : output_tensor_end[2] + 1,
+        output_tensor_start[3] : output_tensor_end[3] + 1,
+    ]
+
+    print("\n", a_pt.shape)
+    print("\n", a_pt)
+    print("\n", a_ref)
+
+    assert a_pt.shape == output_tensor_shape
+    assert torch.equal(a_pt, a_ref)
+
+
+# Pad, run op, unpad
+@pytest.mark.parametrize(
+    "input_tensor_shape, output_tensor_shape, input_tensor_start, pad_value",
+    (((1, 1, 3, 4), (1, 1, 32, 32), (0, 0, 1, 1), 0),),
+)
+def test_run_padding_and_add_test(
     input_tensor_shape, output_tensor_shape, input_tensor_start, pad_value
 ):
     # Args for unpad
