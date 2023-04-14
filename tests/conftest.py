556141bd691369262dd35ec534b353d8f5702d86
Brian Liu <bliu@tenstorrent.com>
2023-04-14 15:41:24 +0000

    #382: Add function_level_defaults fixture in conftest that sets seeds for pytests

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+import torch
+import random
+import numpy as np
+
+
+@pytest.fixture(scope="function")
+def reset_seeds():
+    torch.manual_seed(213919)
+    np.random.seed(213919)
+    random.seed(213919)
+
+    yield
+
+
+@pytest.fixture(scope="function")
+def function_level_defaults(reset_seeds):
+    yield
