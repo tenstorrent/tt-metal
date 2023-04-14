4bea712e346c730366075361cddffe4618d5d3f2
Brian Liu <bliu@tenstorrent.com>
2023-04-14 21:24:10 +0000

    #382: Add script to run all tensor and tt_dnn pytests

---
+++ tests/scripts/run_tt_lib_regressions.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+set -eo pipefail
+
+if [[ -z "$TT_METAL_HOME" ]]; then
+  echo "Must provide TT_METAL_HOME in environment" 1>&2
+  exit 1
+fi
+
+if [ "$TT_METAL_ENV" != "dev" ]; then
+  echo "Must set TT_METAL_ENV as dev" 1>&2
+  exit 1
+fi
+
+cd $TT_METAL_HOME
+
+source build/python_env/bin/activate
+export PYTHONPATH=$TT_METAL_HOME
+
+pytest $TT_METAL_HOME/tests/python_api_testing/sweep_tests/pytests/ -vvv
