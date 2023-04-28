fa9039f22eaba7b26ea9118820a56c7a3737d84b
Raymond Kim <rkim@tenstorrent.com>
2023-04-27 22:42:57 -0400

    #225: Revert "#225: Add beginnings of new flash and driver"
    This reverts commit b95e67c962318e7b61b6a1e8b70b2947090b48c4.
    
    Save for later.

--- infra/machine_setup/scripts/download_public_machine_setup_assets.sh
+++ infra/machine_setup/scripts/download_public_machine_setup_assets.sh
@@ -10,15 +10,11 @@ fi
 TT_METAL_HOME=$(git rev-parse --show-toplevel)
 ASSETS_DIR=$TT_METAL_HOME/infra/machine_setup/assets
 
-FULL_REPO_NAME=tenstorrent-software/pybuda
+FULL_REPO_NAME=tenstorrent-metal/tt-metal-sys-eng-packages
 
 PYBUDA_GS_RELEASE=v1.0.23-tt-external.2f4901c8.gs
-PYBUDA_WH_RELEASE=v1.0.24-tt-external.e67d8051.wh_b0
+PYBUDA_WH_RELEASE=v0.0-alpha0
 GS_TT_SMI_FILENAME=tt-smi_2022-12-05-74801e089fb2e564
-WH_TT_SMI_FILENAME=tt-smi-wh-8.2.0.0_2023-04-19-621d61fca60a43eb
-GS_TT_FLASH_FILENAME=tt-flash_2022-09-06-fae5785cae3807a6
-WH_TT_FLASH_FILENAME=tt-flash_7.8.2.0_2023-03-29-ea858ffb9a5c19e3
-GS_TT_DRIVER_FILENAME=install_ttkmd_1.19.bash
 
 PYBUDA_GS_RELEASE_ID=$(curl -L -H "Authorization: Bearer $GITHUB_TOKEN" \
 	-H "X-GitHub-Api-Version: 2022-11-28" \
@@ -32,10 +28,6 @@ PYBUDA_GS_RELEASE_ASSETS=$(curl -L -H "Authorization: Bearer $GITHUB_TOKEN" \
 	https://api.github.com/repos/$FULL_REPO_NAME/releases/$PYBUDA_GS_RELEASE_ID/assets)
 
 GS_TT_SMI_SERVER_LOCATION=$(echo $PYBUDA_GS_RELEASE_ASSETS | jq ".[] | select(.name==\"$(echo $GS_TT_SMI_FILENAME)\")" | jq '.url' | tr \" \ )
-WH_TT_SMI_SERVER_LOCATION=$(echo $PYBUDA_GS_RELEASE_ASSETS | jq ".[] | select(.name==\"$(echo $WH_TT_SMI_FILENAME)\")" | jq '.url' | tr \" \ )
-GS_TT_FLASH_SERVER_LOCATION=$(echo $PYBUDA_GS_RELEASE_ASSETS | jq ".[] | select(.name==\"$(echo $GS_TT_FLASH_FILENAME)\")" | jq '.url' | tr \" \ )
-WH_TT_FLASH_SERVER_LOCATION=$(echo $PYBUDA_GS_RELEASE_ASSETS | jq ".[] | select(.name==\"$(echo $WH_TT_FLASH_FILENAME)\")" | jq '.url' | tr \" \ )
-GS_TT_DRIVER_SERVER_LOCATION=$(echo $PYBUDA_GS_RELEASE_ASSETS | jq ".[] | select(.name==\"$(echo $GS_TT_DRIVER_FILENAME)\")" | jq '.url' | tr \" \ )
 
 GS_TT_SMI_LOCAL_FOLDER=$ASSETS_DIR/tt_smi/gs
 WH_TT_SMI_LOCAL_FOLDER=$ASSETS_DIR/tt_smi/wh
@@ -56,14 +48,7 @@ WH_TT_FLASH_LOCAL_LOCATION=$WH_TT_FLASH_LOCAL_FOLDER/tt-flash
 TT_DRIVER_LOCAL_LOCATION=$TT_DRIVER_LOCAL_FOLDER/install_ttkmd.bash
 
 echo $GS_TT_FLASH_LOCAL_LOCATION
-echo $GS_TT_SMI_LOCAL_LOCATION
 curl -vL -H "Authorization: Bearer $GITHUB_TOKEN" \
 	-H "X-GitHub-Api-Version: 2022-11-28" \
 	-H "Accept: application/octet-stream" \
 	$GS_TT_SMI_SERVER_LOCATION -o $GS_TT_SMI_LOCAL_LOCATION
-
-echo $WH_TT_SMI_LOCAL_LOCATION
-curl -vL -H "Authorization: Bearer $GITHUB_TOKEN" \
-	-H "X-GitHub-Api-Version: 2022-11-28" \
-	-H "Accept: application/octet-stream" \
-	$WH_TT_SMI_SERVER_LOCATION -o $WH_TT_SMI_LOCAL_LOCATION
