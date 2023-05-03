ea6ea4d27e5bf28124c663b5be2ca5c0301c25f2
Nemanja Grujic <ngrujic@e12cs06.tenstorrent.net>
2023-05-03 12:07:26 +0000

    #443: Added bert_sample_qas test

---
+++ tests/python_api_testing/models/bert_large_perf/test_bert_sample_qas.py
@@ -0,0 +1,334 @@
+
+import pytest
+from loguru import logger
+import torch
+from transformers import BertForQuestionAnswering, BertTokenizer, pipeline
+import sys
+from pathlib import Path
+f = f"{Path(__file__).parent}"
+sys.path.append(f"{f}/../../..")
+sys.path.append(f"{f}/../../../..")
+
+import time
+import random
+import json
+from libs import tt_lib as ttl
+from python_api_testing.models.bert_large_perf.embeddings import PytorchEmbeddings
+from python_api_testing.models.bert_large_perf.bert_encoder import TtBertEncoder
+from python_api_testing.models.bert_large_perf.fused_ops.linear import Linear
+from python_api_testing.models.bert_large_perf.fused_ops.layernorm import create_var_scaler
+from libs.tt_lib.utils import pad_activation, pad_weight
+from utility_functions import enable_binary_cache, enable_compile_cache
+from utility_functions import profiler
+from utility_functions import disable_binary_cache, disable_compile_cache
+
+
+
+class DataSampler:
+    def __init__(self, file_name):
+        self.data = {}
+        try:
+            with open(file_name) as json_file:
+                self.data = json.load(json_file)
+        except FileNotFoundError:
+            print("File not found")
+
+        self.file_name = file_name
+
+    def read(self):
+        titles = []
+
+        for topic in self.data["data"]:
+            titles.append(topic['title'])
+
+        selection = random.choice(titles)
+
+        for topic in self.data["data"]:
+            if topic['title'] == selection:
+
+                # select paragraph
+                total_paragraphs = len(topic['paragraphs'])
+                selected_paragraph = random.randint(0, total_paragraphs-1)
+                paragraph = topic['paragraphs'][selected_paragraph]
+
+                # select question
+                total_questions = len(paragraph['qas'])
+                selected_question = random.randint(0, total_questions-1)
+                qas = paragraph['qas'][selected_question]
+                question = qas['question']
+
+                # get all related answers
+                answers = []
+
+                if len(qas['answers']) == 0:
+                    for answer in qas['plausible_answers']:
+                        answers.append(answer['text'])
+                else:
+                    for answer in qas['answers']:
+                        answers.append(answer['text'])
+
+                # get context
+                context = paragraph['context']
+
+        return {'context': context, 'question': question, 'answers': answers}
+
+    def readn(self, num_samples):
+        inputs = []
+
+        for i in range(num_samples):
+            sample = self.read()
+            inputs.append(sample)
+
+        return inputs
+
+
+def sample_bert_input(hugging_face_reference_model, tokenizer, seq_len, attention_mask, token_type_ids, qas_sample, num_samples):
+
+    samples = []
+
+    for i in range(num_samples):
+
+        input_qas = qas_sample.read()
+        context = [input_qas['context']]
+        question = [input_qas['question']]
+
+        bert_input = tokenizer.batch_encode_plus(zip(question, context), max_length=seq_len, padding="max_length", truncation=True, return_tensors="pt")
+        nlp = pipeline("question-answering", model=hugging_face_reference_model, tokenizer=tokenizer)
+        pl_answer = nlp(question = question[0], context=context[0])
+        preprocess_params, _, postprocess_params = nlp._sanitize_parameters()
+        preprocess_params["max_seq_len"] = seq_len
+        input_q = {"context": context[0], "question": question[0]}
+        examples = nlp._args_parser(input_q)
+        model_input = next(nlp.preprocess(examples[0], **preprocess_params))
+
+        single_input = {
+            "data": (
+                model_input["input_ids"],
+                model_input["attention_mask"] if attention_mask else None,
+                model_input["token_type_ids"] if token_type_ids else None
+            ),
+            "example": model_input["example"],
+            "inputs": model_input,
+        }
+
+        bert_input = {}
+        bert_input["input_ids"] = single_input["data"][0]
+        bert_input["attention_mask"] = single_input["data"][1]
+        bert_input["token_type_ids"] = single_input["data"][2]
+
+        sample = {}
+        sample["bert_input"] = bert_input
+        sample["single_input"] = single_input
+        sample["nlp"] = nlp
+        sample["postprocess_params"] = postprocess_params
+        sample["pl_answer"] = pl_answer
+        sample["context"] = context
+        sample["question"] = question
+        sample["answers"] = input_qas["answers"]
+
+        samples.append(sample)
+
+    return samples
+
+
+class TtBertForQuestionAnswering(torch.nn.Module):
+    def __init__(self, config, hugging_face_reference_model, seq_len, device):
+        super().__init__()
+
+        # NOTE: Once we make embeddings run on device, pass in state dict
+        # instead of model itself
+        state_dict = hugging_face_reference_model.state_dict()
+
+        # Constant prop -> create_var_scaler
+        var_scaler = create_var_scaler(seq_len, config.hidden_size, config.layer_norm_eps, device)
+
+        self.hidden_states_list = []
+        self.tt_attention_mask_list = []
+
+        # So far on CPU until we add embeddings support on device
+        self.embeddings = PytorchEmbeddings(hugging_face_reference_model)
+        self.get_extended_attention_mask = hugging_face_reference_model.get_extended_attention_mask
+        self.encoders = torch.nn.ModuleList([TtBertEncoder(config, encoder_idx, state_dict, var_scaler, device) for encoder_idx in range(config.num_hidden_layers)])
+
+        num_classes, hidden_size = state_dict["qa_outputs.weight"].shape
+
+        weight = pad_weight(state_dict["qa_outputs.weight"])
+        weight = ttl.tensor.Tensor(weight.reshape(-1).tolist(), weight.shape, ttl.tensor.DataType.BFLOAT16, ttl.tensor.Layout.ROW_MAJOR).to(ttl.tensor.Layout.TILE).to(device)
+        bias   = pad_weight(state_dict["qa_outputs.bias"])
+        bias = ttl.tensor.Tensor(bias.reshape(-1).tolist(), bias.shape, ttl.tensor.DataType.BFLOAT16, ttl.tensor.Layout.ROW_MAJOR).to(ttl.tensor.Layout.TILE).to(device)
+
+        # QA linear
+        self.qa_linear = Linear(hidden_size, 32, weight, bias, device)
+        self.device = device
+
+    def forward(self, samples):
+
+        for sample in samples:
+            profiler.start("_calc_embeddings")
+
+            input_ids = sample["bert_input"]["input_ids"]
+            attention_mask = sample["bert_input"]["attention_mask"]
+            token_type_ids = sample["bert_input"]["token_type_ids"]
+
+            embeddings = self.embeddings(input_ids, token_type_ids)
+
+            if attention_mask is not None:
+                extended_attention_mask = self.get_extended_attention_mask(attention_mask, input_ids.shape)
+                extended_attention_mask = torch.clamp(extended_attention_mask, -100000) # Limit neg value that goes into exp
+                extended_attention_mask = pad_activation(extended_attention_mask)
+                tt_attention_mask = ttl.tensor.Tensor(extended_attention_mask.reshape(-1).tolist(), extended_attention_mask.shape, ttl.tensor.DataType.BFLOAT16,  ttl.tensor.Layout.ROW_MAJOR).to(ttl.tensor.Layout.TILE)
+                tt_attention_mask = tt_attention_mask.to(self.device)
+            else:
+                tt_attention_mask = attention_mask
+
+            #Add to list mask
+            self.tt_attention_mask_list.append(tt_attention_mask)
+
+            # Convert to ll buda tensor
+            pad_embeddings = pad_activation(embeddings)
+            tt_embeddings = ttl.tensor.Tensor(pad_embeddings.reshape(-1).tolist(), (pad_embeddings.shape[0], 1, pad_embeddings.shape[-2], pad_embeddings.shape[-1]), ttl.tensor.DataType.BFLOAT16,  ttl.tensor.Layout.ROW_MAJOR).to(ttl.tensor.Layout.TILE)
+            tt_embeddings = tt_embeddings.to(self.device)
+            hidden_states = tt_embeddings # pad_embeddings #
+
+            self.hidden_states_list.append(hidden_states)
+            profiler.end("_calc_embeddings")
+
+        tt_out_list = []
+
+        for i in range(len(self.hidden_states_list)):
+            print(f"Running BERT model for sample {i}")
+            profiler.start("_run_encoders")
+
+            hidden_states = self.hidden_states_list[i]
+            attention_mask = self.tt_attention_mask_list[i]
+
+            for encoder in self.encoders:
+                profiler.start("__one_encoder")
+                hidden_states = encoder(hidden_states, attention_mask)
+                profiler.end("__one_encoder")
+
+            profiler.end("_run_encoders")
+
+            profiler.start("_qa_linear")
+            res = self.qa_linear(hidden_states)
+            profiler.end("_qa_linear")
+
+            tt_out_list.append(res)
+
+        return tt_out_list
+
+
+def model_location_generator_(rel_path):
+    internal_weka_path = Path("/mnt/MLPerf")
+    has_internal_weka = (internal_weka_path / "bit_error_tests").exists()
+
+    if has_internal_weka:
+        return Path("/mnt/MLPerf") / rel_path
+    else:
+        return Path("/opt/tt-metal-models") / rel_path
+
+
+def run_bert_question_and_answering_inference(model_version, batch, seq_len, on_weka, attention_mask, token_type_ids, pcc, model_location_generator, qas_sample, num_samples):
+
+    torch.manual_seed(1234)
+
+    device = ttl.device.CreateDevice(ttl.device.Arch.GRAYSKULL, 0)
+    ttl.device.InitializeDevice(device)
+    host = ttl.device.GetHost()
+
+    if on_weka:
+        model_name = str(model_location_generator("tt_dnn-models/Bert/BertForQuestionAnswering/models/") / model_version)
+        tokenizer_name = str(model_location_generator("tt_dnn-models/Bert/BertForQuestionAnswering/tokenizers/") / model_version)
+    else:
+        model_name = model_version
+        tokenizer_name = model_version
+
+    hugging_face_reference_model = BertForQuestionAnswering.from_pretrained(model_name, torchscript=False)
+    tt_bert_model = TtBertForQuestionAnswering(hugging_face_reference_model.config, hugging_face_reference_model, seq_len, device)
+
+    profiler.start("processing_of_input")
+    tokenizer = BertTokenizer.from_pretrained(tokenizer_name)
+
+    print(f"Sampling random context+question pairs")
+    samples = sample_bert_input(hugging_face_reference_model, tokenizer, seq_len, attention_mask, token_type_ids, qas_sample, num_samples)
+    profiler.end("processing_of_input")
+
+    #print(f"Running BERT model once to fill caches -> disable profiler")
+    #profiler.disable()
+    #tt_out = tt_bert_model(**bert_input).to(host)
+    #tt_untilized_output = torch.Tensor(tt_out.to(ttl.tensor.Layout.ROW_MAJOR).data()).reshape(batch, 1, seq_len, -1)
+    #print(f"Enable profiler and enable binary and compile cache")
+    #profiler.enable()
+
+    print(f"Enabling cache-s")
+    enable_binary_cache()
+    enable_compile_cache()
+
+    # NOTE: Passing in pytorch tensor here instead of ll buda tensor
+    # since we don't yet have embedding support on device
+    print(f"Running BERT model")
+
+    profiler.start("whole_model")
+    tt_out_list = tt_bert_model(samples)
+    profiler.end("whole_model", num_samples)
+
+    profiler.start("processing_output_to_string")
+
+    for i in range(len(tt_out_list)):
+
+        single_input = samples[i]["single_input"]
+        nlp = samples[i]["nlp"]
+        postprocess_params = samples[i]["postprocess_params"]
+        pl_answer = samples[i]["pl_answer"]
+        tt_out = tt_out_list[i]
+        context = samples[i]['context']
+        question = samples[i]['question']
+        answers = samples[i]['answers']
+
+        tt_out = tt_out.to(host)
+        tt_untilized_output = torch.Tensor(tt_out.to(ttl.tensor.Layout.ROW_MAJOR).data()).reshape(batch, 1, seq_len, -1)
+
+        tt_start_logits = tt_untilized_output[..., :, 0].squeeze(1)
+        tt_end_logits = tt_untilized_output[..., :, 1].squeeze(1)
+
+        tt_res = {
+            "start": tt_start_logits,
+            "end": tt_end_logits,
+            "example": single_input["example"],
+            **single_input["inputs"],
+        }
+
+        tt_answer = nlp.postprocess([tt_res], **postprocess_params)['answer']
+
+        print(f"Context: {context}")
+        print(f"Question: {question}")
+        print(f"Answer from GS: '{tt_answer}'")
+        print(f"All valid answers: {answers}\n")
+
+    profiler.end("processing_output_to_string")
+
+    ttl.device.CloseDevice(device)
+    profiler.print()
+
+    assert profiler.get("whole_model") < 61.0
+
+def test_bert_sample_qas():
+    model_version = "phiyodr/bert-large-finetuned-squad2"
+    batch = 1
+    seq_len = 384
+    on_weka = True
+    attention_mask = True
+    token_type_ids = True
+    pcc = 0.98
+    model_location_generator = model_location_generator_
+    qas_sample = DataSampler('./tests/python_api_testing/models/bert_large_perf/dev-v2.0.json')
+    num_samples = 10
+
+    disable_binary_cache()
+    disable_compile_cache()
+
+    run_bert_question_and_answering_inference(model_version, batch, seq_len, on_weka, attention_mask, token_type_ids, pcc, model_location_generator, qas_sample, num_samples)
+
+if __name__ == "__main__":
+
+    test_bert_sample_qas()
