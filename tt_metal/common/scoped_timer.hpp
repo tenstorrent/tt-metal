e3da1936f1a5f64848b9108e377929fdaaf09f12
Kei-Ming Kwong <kkwong@tenstorrent.com>
2023-05-04 04:49:17 +0000

    #0: Ported Scoped Timer from @hfarooq into tt-metal.
    Useful for general debugging and timing :)

---
+++ tt_metal/common/scoped_timer.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <chrono>
+
+#include "logger.hpp"
+
+namespace tt {
+template <typename TimeUnit>
+struct ScopedTimer {
+    using Clock = std::chrono::high_resolution_clock;
+    using TimeInstant = std::chrono::time_point<Clock, std::chrono::nanoseconds>;
+    using Duration = std::chrono::duration<std::uint64_t, typename TimeUnit::period>;
+
+    static std::string time_unit_to_string() {
+        if constexpr (std::is_same_v<TimeUnit, std::chrono::milliseconds>) {
+            return "ms";
+        } else if constexpr (std::is_same_v<TimeUnit, std::chrono::microseconds>) {
+            return "µs";
+        } else if constexpr (std::is_same_v<TimeUnit, std::chrono::seconds>) {
+            return "s";
+        } else {
+            return "time_unit";
+        }
+    }
+
+    ScopedTimer(std::string name_, bool print_duration_ = true) : name(name_), print_duration(print_duration_) {
+        this->start = std::chrono::time_point_cast<std::chrono::nanoseconds>(Clock::now());
+    }
+
+    ~ScopedTimer() {
+        auto end = std::chrono::time_point_cast<std::chrono::nanoseconds>(Clock::now());
+        this->elapsed = std::chrono::duration_cast<TimeUnit>(end - this->start);
+        if (print_duration) {
+            tt::log_info(tt::LogTimer, "{} -- elapsed: {}{}", this->name, this->elapsed.count(), time_unit_to_string());
+        }
+    }
+
+    std::string name;
+    bool print_duration;
+    TimeInstant start;
+    Duration elapsed;
+};
+
+}  // namespace tt
