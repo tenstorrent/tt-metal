8e72e7fb7e87849c03419f5d8e5d30355c9cd98b
Nitika Shanker <cloud_nshanker@e05cs04.tenstorrent.net>
2023-05-16 06:01:10 +0000

    #572: Caching dtx conv transform outputs to txt files to bypass long-running transform pass

--- libs/dtx/pass_conv_transform.cpp
+++ libs/dtx/pass_conv_transform.cpp
@@ -2,11 +2,54 @@
 #include "dtx_passes.hpp"
 #include "util.hpp"
 #include "util_vector_of_ints.hpp"
+#include <fstream>
+
+string get_dtx_output_file_name(vector<int> activation_shape, vector<int> conv_params, vector<int> block_shape_yx, uint32_t num_bytes_of_df) {
+    assert(activation_shape.size() == 3);
+    assert(conv_params.size() == 6);
+    assert(block_shape_yx.size() == 2);
+    int C = activation_shape[0];
+    int H = activation_shape[1];
+    int W = activation_shape[2];
+    int kernel_window_h = conv_params[0];
+    int kernel_window_w = conv_params[1];
+    int stride_h = conv_params[2];
+    int stride_w = conv_params[3];
+    int pad_h = conv_params[4];
+    int pad_w = conv_params[5];
+    vector<int> weight_shape = {-1, C, H, W};
+    string dtx_output_file_name = "actShape_1_" + to_string(C) + "_" + to_string(H) + "_" + to_string(W);
+    dtx_output_file_name += "_weightsShape_X_" + to_string(C) + "_" + to_string(kernel_window_h) + "_" + to_string(kernel_window_w);
+    dtx_output_file_name += "_kernelWindow_" + to_string(kernel_window_h) + "_" + to_string(kernel_window_w);
+    dtx_output_file_name += "_stride_" + to_string(stride_h) + "_" + to_string(stride_w);
+    dtx_output_file_name += "_padding_" + to_string(pad_h) + "_" + to_string(pad_w);
+    dtx_output_file_name += "_blockSize_" + to_string(block_shape_yx[0]) + "_" + to_string(block_shape_yx[1]);
+    dtx_output_file_name += "_numBytes_" + to_string(num_bytes_of_df);
+    dtx_output_file_name += ".txt";
+    return dtx_output_file_name;
+}
 
 std::vector<uint32_t> conv_transform(vector<int> shape, vector<int> conv_params, std::pair<vector<int>,vector<int>> block_info, uint32_t num_bytes_of_df) {
-    //assert(R == S && "Only square kernel window supported.");
-    //assert((R == 1 || R == 3) && "Only 1x1 and 3x3 kernel window supported.");
     assert(shape.size() == 3);
+    assert(conv_params.size() == 6);
+    auto dim_order = block_info.first;
+    auto block_shape = block_info.second;
+    assert(block_shape.size() == 2);
+    // Generate dtx output file name for the given parameters and check if the file already exists
+    string dtx_output_file_name = get_dtx_output_file_name(shape, conv_params, block_shape, num_bytes_of_df);
+    assert(std::getenv("TT_METAL_HOME"));
+    string dtx_file_path = ((string) std::getenv("TT_METAL_HOME")) + "/libs/dtx/dtx_conv_transform_outputs/" + dtx_output_file_name;
+    std::ifstream file(dtx_file_path);
+    if (file.is_open()) {
+        std::cout << "DTX transform output file already exists! No need to run transform pass. Read address map." << std::endl;
+        std::vector<uint32_t> address_map;
+        std::string line;
+        while (std::getline(file, line)) {
+            address_map.push_back(static_cast<uint32_t>(std::stoul(line)));
+        }
+        file.close();
+        return address_map;
+    }
     auto activation_C = shape[1];
     //assert(activation_C % 32 == 0 && "Channel depth of tensor needs to be divisible by 32");
     // Right side: AbstractTensor --> consumer conv/mm
@@ -23,8 +66,7 @@ std::vector<uint32_t> conv_transform(vector<int> shape, vector<int> conv_params,
     assert(matrix_shape[0] == 1);
     uint32_t num_rows = (uint32_t) matrix_shape[1];
     uint32_t num_cols = (uint32_t) matrix_shape[2];
-    auto dim_order = block_info.first;
-    auto block_shape = block_info.second;
+
     pass &= block_2d_matrix(dtx_right, dim_order, block_shape);
     auto blocked_matrix_shape = dtx_right->transformations.back()->groups[0]->shape;
     assert(blocked_matrix_shape.size() == 3);
@@ -99,5 +141,8 @@ std::vector<uint32_t> conv_transform(vector<int> shape, vector<int> conv_params,
     uint32_t in0_block_size_bytes = total_bytes / n_blocks;
     assert(in0_block_size_bytes == block_size_bytes);
     delete combined;
+    // Save and cache dtx transform output to file
+    std::ofstream outFile(dtx_file_path);
+    for (const auto &v : address_map) outFile << to_string(v) << "\n";
     return address_map;
 }
