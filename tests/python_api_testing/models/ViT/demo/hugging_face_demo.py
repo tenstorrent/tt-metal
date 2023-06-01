ccf6f1a8f2ab9b482e03cd424469f3586296cb25
Aaron Shayesteh <ashayestehmanesh@tenstorrent.com>
2023-06-01 19:55:50 +0000

    #922: ViT, added HF demo and torch demo, full CPU  so far

---
+++ tests/python_api_testing/models/ViT/demo/hugging_face_demo.py
@@ -0,0 +1,18 @@
+from transformers import AutoImageProcessor, ViTForImageClassification
+import torch
+from datasets import load_dataset
+
+dataset = load_dataset("huggingface/cats-image")
+image = dataset["test"]["image"][0]
+
+image_processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224")
+model = ViTForImageClassification.from_pretrained("google/vit-base-patch16-224")
+
+inputs = image_processor(image, return_tensors="pt")
+
+with torch.no_grad():
+    logits = model(**inputs).logits
+
+# model predicts one of the 1000 ImageNet classes
+predicted_label = logits.argmax(-1).item()
+print(model.config.id2label[predicted_label])
