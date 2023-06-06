1ff12bc0b603a83e4e3fac070ed11dccca91fdbc
Raymond Kim <rkim@tenstorrent.com>
2023-06-06 17:49:49 +0000

    #735: Revert "#735: Add ConvMnist Performance Demo" because scheduled failed
    This reverts commit 8d4ca5cb3a97ea5ce3ca39990d2a0f66bca24865.

--- tests/python_api_testing/models/ConvNet_MNIST/mnist.py
+++ tests/python_api_testing/models/ConvNet_MNIST/mnist.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 import sys
-
 f = f"{Path(__file__).parent}"
 sys.path.append(f"{f}/..")
+sys.path.append(f"{f}/../..")
 sys.path.append(f"{f}/../../..")
+sys.path.append(f"{f}/../../../..")
 
 import torch
 import torch.nn as nn
@@ -11,25 +12,25 @@ import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 
-import tt_lib
-from utils import tt_linear, get_shape
-from utility_functions_new import torch2tt_tensor
-from tt_lib.fallback_ops import fallback_ops
+from libs import tt_lib as ttl
+from python_api_testing.fused_ops.linear import Linear as TtLinear
+from python_api_testing.fused_ops.softmax import softmax as TtSoftmax
+from libs.tt_lib.utils import pad_weight
+
 
 batch_size = 64
 num_classes = 10
 learning_rate = 0.001
 num_epochs = 10
 
-
 class ConvNet(nn.Module):
     def __init__(self):
         super().__init__()
+
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
         # self.dropout2 = nn.Dropout(0.5) # commented out since makes no different in inference
-        self.fc1 = nn.Linear(2304, 128)
-        self.fc2 = nn.Linear(128, num_classes)
+        self.fc2 = nn.Linear(2304, num_classes)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -38,168 +39,140 @@ class ConvNet(nn.Module):
         x = self.conv2(x)
         x = F.relu(x)
         x = F.max_pool2d(x, 2)
-        x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
+        x = torch.flatten(x, start_dim=1)
+        # x = self.dropout2(x)
         x = self.fc2(x)
         return F.softmax(x, dim=1)
 
 
+
 class TtConvNet(nn.Module):
-    def __init__(self, device=None, host=None, state_dict=None) -> None:
+    def __init__(self, device, host, state_dict):
         super().__init__()
         self.device = device
         self.host = host
 
-        conv1_weight = state_dict["conv1.weight"]
-        self.tt_conv1_weight = tt_lib.tensor.Tensor(
-            conv1_weight.reshape(-1).tolist(),
-            get_shape(conv1_weight.shape),
-            tt_lib.tensor.DataType.BFLOAT16,
-            tt_lib.tensor.Layout.ROW_MAJOR,
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv1.weight = nn.Parameter(state_dict["conv1.weight"])
+        self.conv1.bias = nn.Parameter(state_dict["conv1.bias"])
+
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.conv2.weight = nn.Parameter(state_dict["conv2.weight"])
+        self.conv2.bias = nn.Parameter(state_dict["conv2.bias"])
+
+        # self.dropout2 = nn.Dropout(0.5)
+
+        weights = pad_weight(state_dict[f"fc2.weight"])
+        weights = ttl.tensor.Tensor(weights.reshape(-1).tolist(), weights.shape, ttl.tensor.DataType.BFLOAT16, ttl.tensor.Layout.ROW_MAJOR).to(ttl.tensor.Layout.TILE).data()
+        bias = pad_weight(state_dict[f"fc2.bias"])
+        bias = ttl.tensor.Tensor(bias.reshape(-1).tolist(), bias.shape, ttl.tensor.DataType.BFLOAT16, ttl.tensor.Layout.ROW_MAJOR).to(ttl.tensor.Layout.TILE).data()
+
+        self.fc2 = TtLinear(2304, 32, weights, bias, self.device) # 2304, num_classes
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out = self.conv1(x)
+        # PAD
+        tt_tensor = ttl.tensor.Tensor(
+        out.reshape(-1).tolist(),
+        out.shape,
+        ttl.tensor.DataType.BFLOAT16,
+        ttl.tensor.Layout.ROW_MAJOR,
         )
+        tt_tensor = tt_tensor.pad((batch_size, out.shape[1], 32, 32), (0, 0, 0, 0), 0)
+        out = tt_tensor.to(ttl.tensor.Layout.TILE).to(self.device)
 
-        conv1_bias = state_dict["conv1.bias"]
-        self.tt_conv1_bias = tt_lib.tensor.Tensor(
-            conv1_bias.reshape(-1).tolist(),
-            get_shape(conv1_bias.shape),
-            tt_lib.tensor.DataType.BFLOAT16,
-            tt_lib.tensor.Layout.ROW_MAJOR,
+        out = ttl.tensor.relu(out)
+        # UNPAD
+        out = out.to(self.host).to(ttl.tensor.Layout.ROW_MAJOR)
+        out = out.unpad((0, 0, 0, 0), (batch_size - 1, out.shape()[1] - 1, 29, 29))
+        out  = torch.Tensor(out.data()).reshape(out.shape())
+
+        out = F.max_pool2d(out, 2)
+        out = self.conv2(out)
+
+        # PAD
+        tt_tensor = ttl.tensor.Tensor(
+        out.reshape(-1).tolist(),
+        out.shape,
+        ttl.tensor.DataType.BFLOAT16,
+        ttl.tensor.Layout.ROW_MAJOR,
         )
+        tt_tensor = tt_tensor.pad((batch_size, out.shape[1], 32, 32), (0, 0, 0, 0), 0)
+        out = tt_tensor.to(ttl.tensor.Layout.TILE).to(self.device)
 
-        conv2_weight = state_dict["conv2.weight"]
-        self.tt_conv2_weight = tt_lib.tensor.Tensor(
-            conv2_weight.reshape(-1).tolist(),
-            get_shape(conv2_weight.shape),
-            tt_lib.tensor.DataType.BFLOAT16,
-            tt_lib.tensor.Layout.ROW_MAJOR,
-        )
+        out = ttl.tensor.relu(out)
+        #UNPAD
+        out = out.to(self.host).to(ttl.tensor.Layout.ROW_MAJOR)
+        out = out.unpad((0, 0, 0, 0), (batch_size - 1, out.shape()[1] - 1, 12, 12))
+        out  = torch.Tensor(out.data()).reshape(out.shape())
 
-        conv2_bias = state_dict["conv2.bias"]
-        self.tt_conv2_bias = tt_lib.tensor.Tensor(
-            conv2_bias.reshape(-1).tolist(),
-            get_shape(conv2_bias.shape),
-            tt_lib.tensor.DataType.BFLOAT16,
-            tt_lib.tensor.Layout.ROW_MAJOR,
-        )
-
-        linear1_weights = state_dict[f"fc1.weight"]
-        linear1_weights = tt_lib.tensor.Tensor(
-            linear1_weights.reshape(-1).tolist(),
-            get_shape(linear1_weights.shape),
-            tt_lib.tensor.DataType.BFLOAT16,
-            tt_lib.tensor.Layout.ROW_MAJOR,
-        )
-
-        linear1_bias = state_dict[f"fc1.bias"]
-        linear1_bias = tt_lib.tensor.Tensor(
-            linear1_bias.reshape(-1).tolist(),
-            get_shape(linear1_bias.shape),
-            tt_lib.tensor.DataType.BFLOAT16,
-            tt_lib.tensor.Layout.ROW_MAJOR,
-        )
-
-        linear2_weights = state_dict[f"fc2.weight"]
-        linear2_weights = tt_lib.tensor.Tensor(
-            linear2_weights.reshape(-1).tolist(),
-            get_shape(linear2_weights.shape),
-            tt_lib.tensor.DataType.BFLOAT16,
-            tt_lib.tensor.Layout.ROW_MAJOR,
-        )
-
-        linear2_bias = state_dict[f"fc2.bias"]
-        linear2_bias = tt_lib.tensor.Tensor(
-            linear2_bias.reshape(-1).tolist(),
-            get_shape(linear2_bias.shape),
-            tt_lib.tensor.DataType.BFLOAT16,
-            tt_lib.tensor.Layout.ROW_MAJOR,
-        )
-
-        self.fc1 = tt_linear(linear1_weights, linear1_bias, self.device)
-        self.fc2 = tt_linear(linear2_weights, linear2_bias, self.device)
-
-    def forward(self, tt_x: tt_lib.tensor.Tensor) -> tt_lib.tensor.Tensor:
-
-        out = fallback_ops.conv2d(tt_x, self.tt_conv1_weight, self.tt_conv1_bias)
-
-        out = tt_lib.tensor.relu(out)
-
-        # Convert TT tensor to Torch tensor
-        out = out.to(self.host)
-        out = torch.Tensor(out.data()).reshape(out.shape())
-
-        out = F.max_pool2d(out, 2)  # On CPU
-        out = fallback_ops.conv2d(out, self.tt_conv2_weight, self.tt_conv2_bias)
-
-        out = tt_lib.tensor.relu(out)
-
-        # Convert TT tensor to Torch tensor
-        out = out.to(self.host)
-        out = torch.Tensor(out.data()).reshape(out.shape())
-
-        out = F.max_pool2d(out, 2)  # On CPU
-
+        out = F.max_pool2d(out, 2)
         out = torch.flatten(out, start_dim=1).unsqueeze(1).unsqueeze(1)
 
-        tt_tensor = tt_lib.tensor.Tensor(
-            out.reshape(-1).tolist(),
-            out.shape,
-            tt_lib.tensor.DataType.BFLOAT16,
-            tt_lib.tensor.Layout.ROW_MAJOR,
-            self.device,
+        # PAD
+        tt_tensor = ttl.tensor.Tensor(
+        out.reshape(-1).tolist(),
+        out.shape,
+        ttl.tensor.DataType.BFLOAT16,
+        ttl.tensor.Layout.ROW_MAJOR,
         )
+        tt_tensor = tt_tensor.pad((batch_size, out.shape[1], 32, out.shape[-1]), (0, 0, 0, 0), 0)
+        out = tt_tensor.to(ttl.tensor.Layout.TILE).to(self.device)
 
-        out = self.fc1(tt_tensor)
-        out = tt_lib.tensor.relu(out)
-        out = self.fc2(out)  # On Device
+        out = self.fc2(out)
+        out = out.to(self.host).to(ttl.tensor.Layout.ROW_MAJOR)
+        out = out.unpad((0, 0, 0, 0), (batch_size - 1, out.shape()[1] - 1, 0, num_classes - 1))
+        out  = torch.Tensor(out.data()).reshape(out.shape())
 
-        out = fallback_ops.softmax(out)
+        tt_tensor = ttl.tensor.Tensor(
+        out.reshape(-1).tolist(),
+        out.shape,
+        ttl.tensor.DataType.BFLOAT16,
+        ttl.tensor.Layout.ROW_MAJOR,
+        )
+        tt_tensor = tt_tensor.pad((batch_size, out.shape[1], 32, 32), (0, 0, 0, 0), -100*1000)
+        out = tt_tensor.to(ttl.tensor.Layout.TILE).to(self.device)
+        out = TtSoftmax(out)
 
+        #UNPAD
+        out = out.to(self.host).to(ttl.tensor.Layout.ROW_MAJOR)
+        out = out.unpad((0, 0, 0, 0), (batch_size - 1, 0, 0, 9))
+        out  = torch.Tensor(out.data()).reshape(out.shape())
         return out
 
 
 def load_torch(weka_path):
-    model = ConvNet().to("cpu")
-    checkpoint = torch.load(weka_path, map_location=torch.device("cpu"))
+    model = ConvNet().to('cpu')
+    checkpoint = torch.load(weka_path, map_location=torch.device('cpu'))
+    model.load_state_dict(checkpoint['model_state_dict'])
     model.eval()
-    checkpoint = {"state_dict": model.state_dict()}
-    return model, checkpoint["state_dict"]
-
+    return model, checkpoint['model_state_dict']
 
 def prep_data():
-    # Loading the dataset and preprocessing
-    train_dataset = torchvision.datasets.MNIST(
-        root="./data",
-        train=True,
-        transform=transforms.Compose(
-            [
-                transforms.Resize((32, 32)),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=(0.05,), std=(0.05,)),
-            ]
-        ),
-        download=True,
-    )
+     #Loading the dataset and preprocessing
+    train_dataset = torchvision.datasets.MNIST(root = './data',
+                                            train = True,
+                                            transform = transforms.Compose([
+                                                    transforms.Resize((32,32)),
+                                                    transforms.ToTensor(),
+                                                    transforms.Normalize(mean = (0.1307,), std = (0.3081,))]),
+                                            download = True)
 
-    test_dataset = torchvision.datasets.MNIST(
-        root="./data",
-        train=False,
-        transform=transforms.Compose(
-            [
-                transforms.Resize((32, 32)),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=(0.05,), std=(0.05,)),
-            ]
-        ),
-        download=True,
-    )
+    test_dataset = torchvision.datasets.MNIST(root = './data',
+                                            train = False,
+                                            transform = transforms.Compose([
+                                                    transforms.Resize((32,32)),
+                                                    transforms.ToTensor(),
+                                                    transforms.Normalize(mean = (0.1325,), std = (0.3105,))]),
+                                            download=True)
 
-    train_loader = torch.utils.data.DataLoader(
-        dataset=train_dataset, batch_size=batch_size, shuffle=True
-    )
+    train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
+                                            batch_size = batch_size,
+                                            shuffle = True)
 
-    test_loader = torch.utils.data.DataLoader(
-        dataset=test_dataset, batch_size=batch_size, shuffle=True
-    )
+    test_loader = torch.utils.data.DataLoader(dataset = test_dataset,
+                                            batch_size = batch_size,
+                                            shuffle = True)
 
     return test_dataset, test_loader
-
--- tests/python_api_testing/models/ConvNet_MNIST/test_mnist.py
+++ tests/python_api_testing/models/ConvNet_MNIST/test_mnist.py
@@ -1,66 +1,48 @@
 from pathlib import Path
 import sys
-
 f = f"{Path(__file__).parent}"
 sys.path.append(f"{f}")
 sys.path.append(f"{f}/..")
+sys.path.append(f"{f}/../..")
+sys.path.append(f"{f}/../../..")
+sys.path.append(f"{f}/../../../..")
 
 import torch
 
 from loguru import logger
 
-import tt_lib
-from utility_functions_new import comp_pcc, tt2torch_tensor
+from libs import tt_lib as ttl
+from utility_functions import comp_pcc
 from mnist import *
-import pytest
 
+def test_mnist_convnet_inference(model_location_generator):
 
-def test_mnist_convnet_inference(pcc):  # model location
     with torch.no_grad():
         torch.manual_seed(1234)
         # Initialize the device
 
-        device = tt_lib.device.CreateDevice(tt_lib.device.Arch.GRAYSKULL, 0)
-        tt_lib.device.InitializeDevice(device)
-        tt_lib.device.SetDefaultDevice(device)
-        host = tt_lib.device.GetHost()
+        device = ttl.device.CreateDevice(ttl.device.Arch.GRAYSKULL, 0)
+        ttl.device.InitializeDevice(device)
+        host = ttl.device.GetHost()
 
-        torch_ConvNet, state_dict = load_torch("convnet_mnist.pt")
+        weka_path = model_location_generator('tt_dnn-models/MNIST/ConvNet/mnist_ConvNet.pt')
+        torch_ConvNet, state_dict = load_torch(weka_path)
         test_dataset, test_loader = prep_data()
-        first_input, label = next(iter(test_loader))
 
         tt_convnet = TtConvNet(device, host, state_dict)
-        with torch.no_grad():
-            img = first_input.to("cpu")
+        for image, labels in test_loader:
+            img = image.to('cpu')
             # unsqueeze to go from [batch, 10] to [batch, 1, 1, 10]
-
             torch_output = torch_ConvNet(img).unsqueeze(1).unsqueeze(1)
             _, torch_predicted = torch.max(torch_output.data, -1)
 
-            tt_image = tt_lib.tensor.Tensor(
-            img.reshape(-1).tolist(),
-            img.shape,
-            tt_lib.tensor.DataType.BFLOAT16,
-            tt_lib.tensor.Layout.ROW_MAJOR
-            )
-            tt_output = tt_convnet(tt_image)
-            tt_output = tt2torch_tensor(tt_output, host)
+            tt_output = tt_convnet(img)
 
-            pcc_passing, pcc_output = comp_pcc(torch_output, tt_output)
-            logger.info(f"Output {pcc_output}")
-            assert pcc_passing, f"Model output does not meet PCC requirement {pcc}."
+            _, tt_predicted = torch.max(tt_output.data, -1)
+            print(tt_output.shape, torch_output.shape)
 
-    tt_lib.device.CloseDevice(device)
-
-
-@pytest.mark.parametrize(
-    "pcc",
-    ((0.99),),
-)
-def test_mnist_inference(pcc):
-    test_mnist_convnet_inference(pcc)
-
-
-if __name__ == "__main__":
-    test_mnist_convnet_inference(0.99)
-
+            passing = comp_pcc(torch_output, tt_output)
+            assert passing[0], passing[1:]
+            break
+    logger.info(f"ConvNet MNIST PASSED {passing[1]}")
+    ttl.device.CloseDevice(device)
