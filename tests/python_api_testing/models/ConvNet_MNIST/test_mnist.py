3dbc97e4b4b8aab818e741150db23a9546919504
Vignesh K <vignesh.keerthivasan@multicorewareinc.com>
2023-06-06 10:28:50 +0000

    #735: Add ConvMnist Performance Demo

--- tests/python_api_testing/models/ConvNet_MNIST/test_mnist.py
+++ tests/python_api_testing/models/ConvNet_MNIST/test_mnist.py
@@ -1,48 +1,66 @@
 from pathlib import Path
 import sys
+
 f = f"{Path(__file__).parent}"
 sys.path.append(f"{f}")
 sys.path.append(f"{f}/..")
-sys.path.append(f"{f}/../..")
-sys.path.append(f"{f}/../../..")
-sys.path.append(f"{f}/../../../..")
 
 import torch
 
 from loguru import logger
 
-from libs import tt_lib as ttl
-from utility_functions import comp_pcc
+import tt_lib
+from utility_functions_new import comp_pcc, tt2torch_tensor
 from mnist import *
+import pytest
 
-def test_mnist_convnet_inference(model_location_generator):
 
+def test_mnist_convnet_inference(pcc):  # model location
     with torch.no_grad():
         torch.manual_seed(1234)
         # Initialize the device
 
-        device = ttl.device.CreateDevice(ttl.device.Arch.GRAYSKULL, 0)
-        ttl.device.InitializeDevice(device)
-        host = ttl.device.GetHost()
+        device = tt_lib.device.CreateDevice(tt_lib.device.Arch.GRAYSKULL, 0)
+        tt_lib.device.InitializeDevice(device)
+        tt_lib.device.SetDefaultDevice(device)
+        host = tt_lib.device.GetHost()
 
-        weka_path = model_location_generator('tt_dnn-models/MNIST/ConvNet/mnist_ConvNet.pt')
-        torch_ConvNet, state_dict = load_torch(weka_path)
+        torch_ConvNet, state_dict = load_torch("convnet_mnist.pt")
         test_dataset, test_loader = prep_data()
+        first_input, label = next(iter(test_loader))
 
         tt_convnet = TtConvNet(device, host, state_dict)
-        for image, labels in test_loader:
-            img = image.to('cpu')
+        with torch.no_grad():
+            img = first_input.to("cpu")
             # unsqueeze to go from [batch, 10] to [batch, 1, 1, 10]
+
             torch_output = torch_ConvNet(img).unsqueeze(1).unsqueeze(1)
             _, torch_predicted = torch.max(torch_output.data, -1)
 
-            tt_output = tt_convnet(img)
+            tt_image = tt_lib.tensor.Tensor(
+            img.reshape(-1).tolist(),
+            img.shape,
+            tt_lib.tensor.DataType.BFLOAT16,
+            tt_lib.tensor.Layout.ROW_MAJOR
+            )
+            tt_output = tt_convnet(tt_image)
+            tt_output = tt2torch_tensor(tt_output, host)
 
-            _, tt_predicted = torch.max(tt_output.data, -1)
-            print(tt_output.shape, torch_output.shape)
+            pcc_passing, pcc_output = comp_pcc(torch_output, tt_output)
+            logger.info(f"Output {pcc_output}")
+            assert pcc_passing, f"Model output does not meet PCC requirement {pcc}."
 
-            passing = comp_pcc(torch_output, tt_output)
-            assert passing[0], passing[1:]
-            break
-    logger.info(f"ConvNet MNIST PASSED {passing[1]}")
-    ttl.device.CloseDevice(device)
+    tt_lib.device.CloseDevice(device)
+
+
+@pytest.mark.parametrize(
+    "pcc",
+    ((0.99),),
+)
+def test_mnist_inference(pcc):
+    test_mnist_convnet_inference(pcc)
+
+
+if __name__ == "__main__":
+    test_mnist_convnet_inference(0.99)
+
