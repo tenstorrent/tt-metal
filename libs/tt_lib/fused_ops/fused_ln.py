96000dbff26c11276a01ca607c2dbddfbb57803f
Brian Liu <bliu@tenstorrent.com>
2023-06-14 15:15:15 +0000

    #0: Formatting for fused ln and fused softmax tests

--- libs/tt_lib/fused_ops/fused_ln.py
+++ libs/tt_lib/fused_ops/fused_ln.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import sys
 import time
 import os
+
 f = f"{Path(__file__).parent}"
 sys.path.append(f"{f}/../../../tests")
 
@@ -13,7 +14,8 @@ PROFILE = not DEBUG_PRINTS
 import torch
 
 from libs import tt_lib as ttl
-#from python_api_testing.models.utility_functions import pad_activation, pad_weight, tilize, untilize, tilize_to_list, print_diff_argmax, pad_weight, is_close
+
+# from python_api_testing.models.utility_functions import pad_activation, pad_weight, tilize, untilize, tilize_to_list, print_diff_argmax, pad_weight, is_close
 from libs.tt_lib.utils import (
     _nearest_32 as nearest_32,
     pad_activation,
@@ -30,58 +32,65 @@ from libs.tt_lib.utils import (
     divup,
     channels_last,
     convert_weights_2d_matrix,
-    is_close
+    is_close,
 )
 
 tensor = ttl.tensor
 device = ttl.device
 
+
 # This ref implementation is only here for debugging
-def ref_ln(x, gamma, beta = None, epsilon = 1e-5, b = None):
+def ref_ln(x, gamma, beta=None, epsilon=1e-5, b=None):
     # prints a tile slice with these tile coord range
-    torch.set_printoptions(precision=2, threshold=1000, sci_mode=False, edgeitems=8, linewidth=480)
+    torch.set_printoptions(
+        precision=2, threshold=1000, sci_mode=False, edgeitems=8, linewidth=480
+    )
     sth = 16
     stw = 16
     ph0, ph1, pw0, pw1 = 1, 2, 0, 1
-    #ph0, ph1, pw0, pw1 = 0, 1, 0, 1
+    # ph0, ph1, pw0, pw1 = 0, 1, 0, 1
 
     print("x.shape=", x.shape)
-    #print("eps=", epsilon)
+    # print("eps=", epsilon)
     print(f"slice={ph0}:{ph1}, {pw0}:{pw1}")
-    print("Ref x=\n", x[0, 0, ph0*32 : ph1*32 : sth, pw0*32 : pw1*32 : stw])
-    print("Ref a=\n", (x-b)[0, 0, ph0*32 : ph1*32 : sth, pw0*32 : pw1*32 : stw])
-    print("Ref b=\n", b[0, 0, ph0*32 : ph1*32 : sth, pw0*32 : pw1*32 : stw])
+    print("Ref x=\n", x[0, 0, ph0 * 32 : ph1 * 32 : sth, pw0 * 32 : pw1 * 32 : stw])
+    print(
+        "Ref a=\n", (x - b)[0, 0, ph0 * 32 : ph1 * 32 : sth, pw0 * 32 : pw1 * 32 : stw]
+    )
+    print("Ref b=\n", b[0, 0, ph0 * 32 : ph1 * 32 : sth, pw0 * 32 : pw1 * 32 : stw])
     mean = x.mean(dim=-1, keepdim=True)
-    #print("Ref Ex=\n", mean[0, 0, ph0*32 : ph1*32 : sth, 0*32 : 1*32 : stw])
+    # print("Ref Ex=\n", mean[0, 0, ph0*32 : ph1*32 : sth, 0*32 : 1*32 : stw])
     xmm = x - mean
-    #print("Ref xmm=\n", xmm[0, 0, ph0*32 : ph1*32 : st, pw0*32 : pw1*32 : st])
+    # print("Ref xmm=\n", xmm[0, 0, ph0*32 : ph1*32 : st, pw0*32 : pw1*32 : st])
     xmm2 = xmm**2
-    #print("Ref xmm2=\n", xmm2[0, 0, ph0*32 : ph1*32 : sth, pw0*32 : pw1*32 : stw])
+    # print("Ref xmm2=\n", xmm2[0, 0, ph0*32 : ph1*32 : sth, pw0*32 : pw1*32 : stw])
     exmm2 = xmm2.mean(dim=-1, keepdim=True)
-    #print("Ref exmm2=\n", exmm2[0, 0, ph0*32 : ph1*32 : sth, 0*32 : 1*32 : stw])
+    # print("Ref exmm2=\n", exmm2[0, 0, ph0*32 : ph1*32 : sth, 0*32 : 1*32 : stw])
 
     std = (exmm2 + epsilon).sqrt()
-    #print("Ref sqrt_exmm2=\n", std[0, 0, ph0*32 : ph1*32 : st, 0*32 : 1*32 : st])
+    # print("Ref sqrt_exmm2=\n", std[0, 0, ph0*32 : ph1*32 : st, 0*32 : 1*32 : st])
 
-    invstd = 1.0/std
-    #print("Ref 1/sqrt_exmm2=\n", invstd[0, 0, ph0*32 : ph1*32 : st, 0*32 : 1*32 : st])
+    invstd = 1.0 / std
+    # print("Ref 1/sqrt_exmm2=\n", invstd[0, 0, ph0*32 : ph1*32 : st, 0*32 : 1*32 : st])
     y1 = xmm * invstd
-    #print("Ref y*1+0=\n", y1[0, 0, ph0*32 : ph1*32 : st, pw0*32 : pw1*32 : st])
+    # print("Ref y*1+0=\n", y1[0, 0, ph0*32 : ph1*32 : st, pw0*32 : pw1*32 : st])
     y = y1.clone()
     if gamma is not None:
-        print("yshape=",y.shape)
-        print("gshape=",gamma.shape)
+        print("yshape=", y.shape)
+        print("gshape=", gamma.shape)
         y *= gamma
     if beta is not None:
         y += beta
-    #y = gamma.repeat(x.shape[0], x.shape[1], x.shape[2], x.shape[3]//gamma.shape[3]) # Debug gamma
+    # y = gamma.repeat(x.shape[0], x.shape[1], x.shape[2], x.shape[3]//gamma.shape[3]) # Debug gamma
     return y, mean, exmm2, std, invstd, y1
+
 
 def ref_layernorm(x, eps, gamma, beta, H, W):
     lnorm = torch.nn.LayerNorm((W,), eps)
     lnorm.weight = torch.nn.Parameter(torch.full((W,), gamma))
     lnorm.bias = torch.nn.Parameter(torch.full((W,), beta))
     return lnorm(x)
+
 
 if __name__ == "__main__":
     out_dram = False
@@ -91,60 +100,98 @@ if __name__ == "__main__":
         os.environ["TT_PROFILE"] = "1"
     os.environ["TT_BLOCK_SIZE"] = "2"
     os.environ["TT_FORCE_NUMCORES"] = "108"
-    #os.environ["TT_FORCE_NUMCORES"] = "1"
+    # os.environ["TT_FORCE_NUMCORES"] = "1"
     dev = device.CreateDevice(device.Arch.GRAYSKULL, 0)
     device.InitializeDevice(dev, ttl.device.MemoryAllocator.L1_BANKING)
-    memcfg = ttl.tensor.MemoryConfig(buffer_type=(ttl.tensor.BufferType.DRAM if in_dram else ttl.tensor.BufferType.L1))
+    memcfg = ttl.tensor.MemoryConfig(
+        buffer_type=(
+            ttl.tensor.BufferType.DRAM if in_dram else ttl.tensor.BufferType.L1
+        )
+    )
     if DEBUG_PRINTS:
-        device.StartDebugPrintServerOnCores(dev, [[1,1], [2,1], [3,1], [4,1], [5,1], [6,1]])
+        device.StartDebugPrintServerOnCores(
+            dev, [[1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1]]
+        )
     host = device.GetHost()
 
-
-    #N, C, H, W = 1, 1, 4*32, 9*32
+    # N, C, H, W = 1, 1, 4*32, 9*32
     epsf = 1e-2
     torch.manual_seed(123)
-    test_dims = ((1, 1, 4*32, 8*32), (1, 1, 3*32, 8*32), (1, 1, 32, 12*32), (1, 1, 8*32, 32*32), )
-    #test_dims = ((1,9,384,1024),)
-    test_dims = ((1,1,32*6,1024),)
-    test_dims = ((1,1,384,1024),)
-    test_dims = ((1,10,256,1024),)
-    #test_dims = ((1,6,32,1024),)
-    test_dims = ((1,1,32,1024),)
-    test_dims = ((1,9,384,1024),)
+    test_dims = (
+        (1, 1, 4 * 32, 8 * 32),
+        (1, 1, 3 * 32, 8 * 32),
+        (1, 1, 32, 12 * 32),
+        (1, 1, 8 * 32, 32 * 32),
+    )
+    # test_dims = ((1,9,384,1024),)
+    test_dims = ((1, 1, 32 * 6, 1024),)
+    test_dims = ((1, 1, 384, 1024),)
+    test_dims = ((1, 10, 256, 1024),)
+    # test_dims = ((1,6,32,1024),)
+    test_dims = ((1, 1, 32, 1024),)
+    test_dims = ((1, 9, 384, 1024),)
     for nchw in test_dims:
-        for i in range(0, 1): # 0: no gamma/beta, 1: gamma, 2: gamma+beta
-            i = 0 # force ln(x)*1+0 path
-            i = 1 # force ln(x)*g+0 path
-            i = 2 # force ln(a+b)*gamma+beta path
-            i = 3 # force ln(x)*gamma+beta path
+        for i in range(0, 1):  # 0: no gamma/beta, 1: gamma, 2: gamma+beta
+            i = 0  # force ln(x)*1+0 path
+            i = 1  # force ln(x)*g+0 path
+            i = 2  # force ln(a+b)*gamma+beta path
+            i = 3  # force ln(x)*gamma+beta path
             for nrepeat in range(0, 1):
                 (N, C, H, W) = nchw
                 print("NCHW=", nchw)
                 if i >= 0:
-                    gamma = torch.ones(1,1,1,W)
-                    beta = torch.zeros(1,1,1,W)
+                    gamma = torch.ones(1, 1, 1, W)
+                    beta = torch.zeros(1, 1, 1, W)
                 if i >= 1:
-                    gamma = torch.rand(1,1,1,W)*2-1
-                    #gamma = torch.arange(0.0, float(W)-0.01, 1.0).reshape(1,1,1,W) # debug gamma
-                    #gammah32 = tilize_to_list(pad_weight(gamma.repeat(1,1,32,1))) # debug gamma
+                    gamma = torch.rand(1, 1, 1, W) * 2 - 1
+                    # gamma = torch.arange(0.0, float(W)-0.01, 1.0).reshape(1,1,1,W) # debug gamma
+                    # gammah32 = tilize_to_list(pad_weight(gamma.repeat(1,1,32,1))) # debug gamma
                     print(gamma)
-                    #gamma[:,:,:,320:] = 0.0
+                    # gamma[:,:,:,320:] = 0.0
                     gammah32 = tilize_to_list(pad_weight(gamma))
-                    ttgamma = tensor.Tensor(gammah32, [1, 1, 32, W], tensor.DataType.BFLOAT16, tensor.Layout.TILE, dev, memcfg)
+                    ttgamma = tensor.Tensor(
+                        gammah32,
+                        [1, 1, 32, W],
+                        tensor.DataType.BFLOAT16,
+                        tensor.Layout.TILE,
+                        dev,
+                        memcfg,
+                    )
                 if i >= 2:
-                    beta = torch.rand(1,1,1,W)*2.0-1.1
+                    beta = torch.rand(1, 1, 1, W) * 2.0 - 1.1
                     betah32 = tilize_to_list(pad_weight(beta))
-                    ttbeta = tensor.Tensor(betah32, [1, 1, 32, W], tensor.DataType.BFLOAT16, tensor.Layout.TILE, dev, memcfg)
+                    ttbeta = tensor.Tensor(
+                        betah32,
+                        [1, 1, 32, W],
+                        tensor.DataType.BFLOAT16,
+                        tensor.Layout.TILE,
+                        dev,
+                        memcfg,
+                    )
 
-                x = torch.rand((N,C,H,W))*2-0.95
-                y = torch.rand((N,C,H,W))*2-0.8
+                x = torch.rand((N, C, H, W)) * 2 - 0.95
+                y = torch.rand((N, C, H, W)) * 2 - 0.8
                 if i < 3:
-                    y *= 0.0 # zero out the y to exclude x+y from reference calculation
-                #ref_lnorm = ref_layernorm(x, epsf, gammaf, betaf, H, W)
-                ref_lnorm, _, _, _, _, _ = ref_ln(x+y, gamma, beta, epsf, y)
+                    y *= 0.0  # zero out the y to exclude x+y from reference calculation
+                # ref_lnorm = ref_layernorm(x, epsf, gammaf, betaf, H, W)
+                ref_lnorm, _, _, _, _, _ = ref_ln(x + y, gamma, beta, epsf, y)
 
-                ttx = tensor.Tensor(tilize_to_list(x), [N, C, H, W], tensor.DataType.BFLOAT16, tensor.Layout.TILE, dev, memcfg)
-                tty = tensor.Tensor(tilize_to_list(y), [N, C, H, W], tensor.DataType.BFLOAT16, tensor.Layout.TILE, dev, memcfg)
+                ttx = tensor.Tensor(
+                    tilize_to_list(x),
+                    [N, C, H, W],
+                    tensor.DataType.BFLOAT16,
+                    tensor.Layout.TILE,
+                    dev,
+                    memcfg,
+                )
+                tty = tensor.Tensor(
+                    tilize_to_list(y),
+                    [N, C, H, W],
+                    tensor.DataType.BFLOAT16,
+                    tensor.Layout.TILE,
+                    dev,
+                    memcfg,
+                )
 
                 if i == 0:
                     print("=== Running LN_NOGB")
@@ -154,24 +201,28 @@ if __name__ == "__main__":
                     ttz = tensor.layernorm_gamma(ttx, epsf, ttgamma, out_dram)
                 elif i == 2:
                     print("=== Running LN_GB")
-                    ttz = tensor.layernorm_gamma_beta(ttx, epsf, ttgamma, ttbeta, out_dram)
+                    ttz = tensor.layernorm_gamma_beta(
+                        ttx, epsf, ttgamma, ttbeta, out_dram
+                    )
                 elif i == 3:
                     print("=== Running add_LN_GB")
-                    ttz = tensor.add_layernorm_gamma_beta(ttx, tty, epsf, ttgamma, ttbeta, out_dram)
+                    ttz = tensor.add_layernorm_gamma_beta(
+                        ttx, tty, epsf, ttgamma, ttbeta, out_dram
+                    )
                 else:
-                    assert(False)
+                    assert False
                 print("=== Done Running LN")
                 t2_data = ttz.to(host).data()
 
-                tt_got_back = torch.Tensor(t2_data).reshape((N,C,H,W))
+                tt_got_back = torch.Tensor(t2_data).reshape((N, C, H, W))
                 tt_got_back = untilize(tt_got_back)
-                #print("tt_result[ht=0,wt=0]=", tt_got_back[:,:,0:32:1,0:32:1])
+                # print("tt_result[ht=0,wt=0]=", tt_got_back[:,:,0:32:1,0:32:1])
 
-                time.sleep(0.3) # sleep to avoid print intermixing with kernel prints
+                time.sleep(0.3)  # sleep to avoid print intermixing with kernel prints
 
-                #if not is_close(tt_got_back[:,0,0:128,:], ref_lnorm[:,0,0:128,:]):
+                # if not is_close(tt_got_back[:,0,0:128,:], ref_lnorm[:,0,0:128,:]):
                 if not is_close(tt_got_back, ref_lnorm):
-                    assert(False)
+                    assert False
                     print("****  Mismatch!")
 
     device.CloseDevice(dev)
--- libs/tt_lib/fused_ops/fused_softmax.py
+++ libs/tt_lib/fused_ops/fused_softmax.py
@@ -5,6 +5,7 @@ import sys
 import time
 import random
 import numpy as np
+
 f = f"{Path(__file__).parent}"
 sys.path.append(f"{f}/../../../tests")
 
@@ -14,38 +15,57 @@ PROFILE = not DEBUG_PRINTS
 import torch
 
 from libs import tt_lib as ttl
-from python_api_testing.models.utility_functions import pad_activation, pad_weight, tilize, untilize, tilize_to_list, print_diff_argmax, pad_weight, is_close
+from python_api_testing.models.utility_functions import (
+    pad_activation,
+    pad_weight,
+    tilize,
+    untilize,
+    tilize_to_list,
+    print_diff_argmax,
+    pad_weight,
+    is_close,
+)
 from python_api_testing.models.utility_functions import tt2torch_tensor
-from libs.tt_lib.utils import ( tilize_to_list )
+from libs.tt_lib.utils import tilize_to_list
 
 tensor = ttl.tensor
 device = ttl.device
 
 
 def ref_stable_softmax(x):
-    torch.set_printoptions(precision=2, threshold=1000, sci_mode=False, edgeitems=8, linewidth=480)
-    z = x #- torch.max(x, dim=3, keepdim=True)[0]
+    torch.set_printoptions(
+        precision=2, threshold=1000, sci_mode=False, edgeitems=8, linewidth=480
+    )
+    z = x  # - torch.max(x, dim=3, keepdim=True)[0]
     numerator = torch.exp(z)
-    #print(x.shape)
+    # print(x.shape)
     H = x.shape[-2]
-    #print("H=", H)
-    pw0, pw1 = 0, 1 # prints a tile slice with these tile coord range
+    # print("H=", H)
+    pw0, pw1 = 0, 1  # prints a tile slice with these tile coord range
     ph0, ph1 = 0, 1
-    sh, sw   = 16, 16 # stride inside the tile
-    ow0, ow1 = 0, 0 # offset inside the tile
+    sh, sw = 16, 16  # stride inside the tile
+    ow0, ow1 = 0, 0  # offset inside the tile
     oh0, oh1 = 0, 0
-    print("Ref x=\n", x[0, 0, ph0*32+oh0:ph1*32+oh1:sh, pw0*32+ow0:pw1*32+ow1:sw])
-    #print("Ref exps=\n", numerator[0, 0, ph0*32 : ph1*32 : sh, pw0*32 : pw1*32 : sw])
+    print(
+        "Ref x=\n",
+        x[
+            0,
+            0,
+            ph0 * 32 + oh0 : ph1 * 32 + oh1 : sh,
+            pw0 * 32 + ow0 : pw1 * 32 + ow1 : sw,
+        ],
+    )
+    # print("Ref exps=\n", numerator[0, 0, ph0*32 : ph1*32 : sh, pw0*32 : pw1*32 : sw])
     denominator = torch.sum(numerator, 3, keepdim=True)
-    #print("denom shape=", denominator.shape)
-    #print("Ref sumexp=\n", torch.reshape(denominator, (-1,H))[:, ph0*32:ph1*32])
+    # print("denom shape=", denominator.shape)
+    # print("Ref sumexp=\n", torch.reshape(denominator, (-1,H))[:, ph0*32:ph1*32])
 
     denom1 = torch.reciprocal(denominator)
-    #print("ref 1/sumexp=\n", denom1[0, 0, 0:32:8, 0:64:8])
-    softmax = numerator*denom1
-    #print("softmaxManual=\n", softmax[0, 0, 0:32:8, 0:64:8])
+    # print("ref 1/sumexp=\n", denom1[0, 0, 0:32:8, 0:64:8])
+    softmax = numerator * denom1
+    # print("softmaxManual=\n", softmax[0, 0, 0:32:8, 0:64:8])
     softmax = torch.nn.Softmax(3)(x)
-    #print("softmaxTorch=\n", softmax[0, 0, 0:32:8, 0:64:8])
+    # print("softmaxTorch=\n", softmax[0, 0, 0:32:8, 0:64:8])
 
     return softmax
 
@@ -59,31 +79,32 @@ def ref_scale_mask_softmax(scale, mask, x):
 
 def generate_recip_tensor(dev, invsqrt):
     # Used to scale down the input to the softmax
-    valtorch = torch.Tensor([invsqrt]).reshape(1,1,1,1)
+    valtorch = torch.Tensor([invsqrt]).reshape(1, 1, 1, 1)
     return valtorch, invsqrt
 
 
 # generates an additive attention mask with some different values
 def generate_attn_mask(NC, W, dev, offs, memcfg):
-    assert(W % 32 == 0)
-    top_row = [offs*(i%2) for i in range(0, W)]
-    zero_rows = [0.0 for _ in range(31*W)]
+    assert W % 32 == 0
+    top_row = [offs * (i % 2) for i in range(0, W)]
+    zero_rows = [0.0 for _ in range(31 * W)]
     # For debugging
-    #top_row = [offs]*W
-    #zero_rows = [offs for _ in range(31*W)]
-    nc_tiles = top_row*NC + zero_rows*NC
-    nc_tiles_np = np.asarray(nc_tiles).reshape(N,C,32,W)
+    # top_row = [offs]*W
+    # zero_rows = [offs for _ in range(31*W)]
+    nc_tiles = top_row * NC + zero_rows * NC
+    nc_tiles_np = np.asarray(nc_tiles).reshape(N, C, 32, W)
     nc_tiles_tilized = tilize_to_list(nc_tiles_np)
     verifytorch = torch.Tensor(nc_tiles).reshape(NC, 32, W)
-    valtorch = torch.Tensor(top_row*NC).reshape(N, C, 1, W)
+    valtorch = torch.Tensor(top_row * NC).reshape(N, C, 1, W)
     val = ttl.tensor.Tensor(
         nc_tiles_tilized,
         [1, NC, 32, W],
         ttl.tensor.DataType.BFLOAT16,
         ttl.tensor.Layout.TILE,
-        dev, memcfg
+        dev,
+        memcfg,
     )
-    #print("Attn mask=", valtorch)
+    # print("Attn mask=", valtorch)
     return valtorch, val
 
 
@@ -93,49 +114,73 @@ if __name__ == "__main__":
         os.environ["TT_PROFILE"] = "1"
     dev = device.CreateDevice(device.Arch.GRAYSKULL, 0)
     device.InitializeDevice(dev, ttl.device.MemoryAllocator.L1_BANKING)
-    memcfg = ttl.tensor.MemoryConfig(buffer_type=(ttl.tensor.BufferType.DRAM if in_dram else ttl.tensor.BufferType.L1))
+    memcfg = ttl.tensor.MemoryConfig(
+        buffer_type=(
+            ttl.tensor.BufferType.DRAM if in_dram else ttl.tensor.BufferType.L1
+        )
+    )
     if DEBUG_PRINTS:
         device.StartDebugPrintServer(dev)
 
     host = device.GetHost()
-    #N, C, H, W = 1, 7, 5*32, 17*32
-    test_dims = ((1,1,32,4*8*32), (1, 1, 2048, 4*8*32), (1, 1, 128, 12*32), (1, 1, 32, 7*32))
-    #test_dims = ((1,1,4*32,7*32),)
-    #test_dims = ((1,9,6144,384),)
-    test_dims = ((1,9,6144,384),)
-    test_dims = ((1,1,6144,384),)
-    test_dims = ((1,9,6144,384),)
+    # N, C, H, W = 1, 7, 5*32, 17*32
+    test_dims = (
+        (1, 1, 32, 4 * 8 * 32),
+        (1, 1, 2048, 4 * 8 * 32),
+        (1, 1, 128, 12 * 32),
+        (1, 1, 32, 7 * 32),
+    )
+    # test_dims = ((1,1,4*32,7*32),)
+    # test_dims = ((1,9,6144,384),)
+    test_dims = ((1, 9, 6144, 384),)
+    test_dims = ((1, 1, 6144, 384),)
+    test_dims = ((1, 9, 6144, 384),)
     torch.manual_seed(123)
     random.seed(123)
-    #for test_fused_mask in [True, False]:
-    for test_fused_mask in [True,]:
+    # for test_fused_mask in [True, False]:
+    for test_fused_mask in [
+        True,
+    ]:
         for nchw in test_dims:
             for nrepeat in range(0, 1):
-                #nchw = (1,random.randint(1,5),random.randint(1,16)*32,random.randint(1,16)*32)
-                (N,C,H,W) = nchw
+                # nchw = (1,random.randint(1,5),random.randint(1,16)*32,random.randint(1,16)*32)
+                (N, C, H, W) = nchw
                 print("NCHW=", nchw)
-                print("Ht,Wt=", H//32, W//32)
-                x = torch.randn((N,C,H,W))*2.0 - 1.0
+                print("Ht,Wt=", H // 32, W // 32)
+                x = torch.randn((N, C, H, W)) * 2.0 - 1.0
                 if test_fused_mask:
-                    torch_scale, tt_scale = generate_recip_tensor(dev, 0.5+random.random())
-                    torch_attn_mask, tt_attn_mask = generate_attn_mask(N*C, W, dev, -4.2*1, memcfg)
+                    torch_scale, tt_scale = generate_recip_tensor(
+                        dev, 0.5 + random.random()
+                    )
+                    torch_attn_mask, tt_attn_mask = generate_attn_mask(
+                        N * C, W, dev, -4.2 * 1, memcfg
+                    )
                     ref_sm = ref_scale_mask_softmax(torch_scale, torch_attn_mask, x)
                 else:
                     ref_sm = ref_stable_softmax(x)
                 x_t = tilize_to_list(x)
-                t0 = tensor.Tensor(x_t, [N, C, H, W], tensor.DataType.BFLOAT16, tensor.Layout.TILE, dev, memcfg)
+                t0 = tensor.Tensor(
+                    x_t,
+                    [N, C, H, W],
+                    tensor.DataType.BFLOAT16,
+                    tensor.Layout.TILE,
+                    dev,
+                    memcfg,
+                )
 
                 if test_fused_mask:
                     print("=== Running scale_mask_softmax")
-                    t1_fused = tensor.scale_mask_softmax_in_place(tt_scale, tt_attn_mask, t0)
+                    t1_fused = tensor.scale_mask_softmax_in_place(
+                        tt_scale, tt_attn_mask, t0
+                    )
                 else:
                     print("=== Running softmax")
                     t1_fused = tensor.softmax_in_place(t0)
                 t2_data_fused = t1_fused.to(host).data()
-                tt_got_back_fused = torch.Tensor(t2_data_fused).reshape((N,C,H,W))
+                tt_got_back_fused = torch.Tensor(t2_data_fused).reshape((N, C, H, W))
                 tt_unt = untilize(tt_got_back_fused)
 
-                time.sleep(0.33) # so prints don't overlap with kernel prints
+                time.sleep(0.33)  # so prints don't overlap with kernel prints
                 if not is_close(tt_unt, ref_sm, rtol=5e-2, atol=5e-2):
                     print("****  Mismatch!")
                     print("Max abs diff=")
