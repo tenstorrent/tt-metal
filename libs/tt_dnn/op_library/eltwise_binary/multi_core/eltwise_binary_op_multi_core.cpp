daab7291aa7b6a8af961c3682d47885f3c76921e
Austin Ho <aho@tenstorrent.com>
2023-06-15 13:19:16 +0000

    #1127: Uplift eltwise binary to use CoreRange/CoreRangeSet

--- libs/tt_dnn/op_library/eltwise_binary/multi_core/eltwise_binary_op_multi_core.cpp
+++ libs/tt_dnn/op_library/eltwise_binary/multi_core/eltwise_binary_op_multi_core.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 
 #include "tt_dnn/op_library/eltwise_binary/eltwise_binary_op.hpp"
+#include "tt_dnn/op_library/work_split.hpp"
 
 #include "tt_metal/host_api.hpp"
 #include "tt_metal/common/constants.hpp"
@@ -39,38 +40,30 @@ Program eltwise_binary_multi_core(const Tensor &a, const Tensor &b, Tensor& outp
     auto compute_and_storage_grid_size = device->compute_and_storage_grid_size();
     uint32_t num_cores_x = compute_and_storage_grid_size.x;
     uint32_t num_cores_y = compute_and_storage_grid_size.y;
-    auto num_cores = std::min(num_tiles, num_cores_x * num_cores_y);
-    std::vector<uint32_t> num_tiles_per_core(num_cores, num_tiles / num_cores);
-    for(uint32_t i = 0; i < num_tiles % num_cores; i++){
-        num_tiles_per_core[i]++;
-    }
+    auto [num_cores, all_cores, core_group_1, core_group_2, num_tiles_per_core_group_1, num_tiles_per_core_group_2] = split_work_to_cores(compute_and_storage_grid_size, num_tiles);
 
     tt_metal::Buffer *dst_dram_buffer = output.buffer();
     TT_ASSERT(dst_dram_buffer != nullptr, "Output buffer should be allocated on device!");
     auto dram_dst_noc_xy = dst_dram_buffer->noc_coordinates();
 
-    std::vector<tt_metal::DataMovementKernel *> binary_reader_kernels;
-    std::vector<tt_metal::DataMovementKernel *> unary_writer_kernels;
-    for (uint32_t i = 0; i < num_cores; i++){
-        CoreCoord core = {i / num_cores_y, i % num_cores_y};
-        uint32_t src0_cb_index = 0;
+    uint32_t src0_cb_index = 0;
         uint32_t num_input_tiles = 2;
-        auto cb_src0 = tt_metal::CreateCircularBuffer(
+        auto cb_src0 = tt_metal::CreateCircularBuffers(
             program,
             device,
             src0_cb_index,
-            core,
+            all_cores,
             num_input_tiles,
             num_input_tiles * single_tile_size,
             DataFormat::Float16_b
         );
 
         uint32_t src1_cb_index = 1;
-        auto cb_src1 = tt_metal::CreateCircularBuffer(
+        auto cb_src1 = tt_metal::CreateCircularBuffers(
             program,
             device,
             src1_cb_index,
-            core,
+            all_cores,
             num_input_tiles,
             num_input_tiles * single_tile_size,
             DataFormat::Float16_b
@@ -78,11 +71,11 @@ Program eltwise_binary_multi_core(const Tensor &a, const Tensor &b, Tensor& outp
 
         uint32_t ouput_cb_index = 16; // output operands start at index 16
         uint32_t num_output_tiles = 2;
-        auto cb_output = tt_metal::CreateCircularBuffer(
+        auto cb_output = tt_metal::CreateCircularBuffers(
             program,
             device,
             ouput_cb_index,
-            core,
+            all_cores,
             num_output_tiles,
             num_output_tiles * single_tile_size,
             DataFormat::Float16_b
@@ -98,68 +91,91 @@ Program eltwise_binary_multi_core(const Tensor &a, const Tensor &b, Tensor& outp
         tt_metal::DataMovementKernel *binary_reader_kernel = tt_metal::CreateDataMovementKernel(
             program,
             "tt_metal/kernels/dataflow/reader_dual_8bank_start_id.cpp",
-            core,
+            all_cores,
             tt_metal::DataMovementProcessor::RISCV_1,
             tt_metal::NOC::RISCV_1_default);
-        binary_reader_kernels.push_back(binary_reader_kernel);
 
         tt_metal::DataMovementKernel *unary_writer_kernel = tt_metal::CreateDataMovementKernel(
             program,
             "tt_metal/kernels/dataflow/writer_unary_8bank_start_id.cpp",
-            core,
+            all_cores,
             reader_writer_compile_time_args,
             tt_metal::DataMovementProcessor::RISCV_0,
             tt_metal::NOC::RISCV_0_default);
-        unary_writer_kernels.push_back(unary_writer_kernel);
 
-        vector<uint32_t> compute_kernel_args = {
-            num_tiles_per_core[i], // per_core_block_cnt
-            1, // per_core_block_size
+    vector<uint32_t> compute_kernel_args_group_1 = {
+        num_tiles_per_core_group_1, // per_core_block_cnt
+        1 // per_core_block_size
+    };
+
+    bool fp32_dest_acc_en = false;
+    bool math_approx_mode = false;
+    auto eltwise_binary_kernel_group_1 = tt_metal::CreateComputeKernel(
+        program,
+        "tt_metal/kernels/compute/eltwise_binary.cpp",
+        core_group_1,
+        compute_kernel_args_group_1,
+        MathFidelity::HiFi4,
+        fp32_dest_acc_en,
+        math_approx_mode
+    );
+
+    eltwise_binary_op_utils::add_defines(eltwise_binary_kernel_group_1, op_type);
+    if(!core_group_2.ranges().empty()){
+        vector<uint32_t> compute_kernel_args_group_2 = {
+            num_tiles_per_core_group_2, // per_core_block_cnt
+            1 // per_core_block_size
         };
 
-        bool fp32_dest_acc_en = false;
-        bool math_approx_mode = false;
-        auto eltwise_binary_kernel = tt_metal::CreateComputeKernel(
+        auto eltwise_binary_kernel_group_2 = tt_metal::CreateComputeKernel(
             program,
             "tt_metal/kernels/compute/eltwise_binary.cpp",
-            core,
-            compute_kernel_args,
+            core_group_2,
+            compute_kernel_args_group_2,
             MathFidelity::HiFi4,
             fp32_dest_acc_en,
             math_approx_mode
         );
 
-        eltwise_binary_op_utils::add_defines(eltwise_binary_kernel, op_type);
+        eltwise_binary_op_utils::add_defines(eltwise_binary_kernel_group_2, op_type);
     }
 
     for (uint32_t i = 0, num_tiles_read = 0; i < num_cores; i++){
         CoreCoord core = {i / num_cores_y, i % num_cores_y};
+        uint32_t num_tiles_per_core;
+        if (core_group_1.core_coord_in_core_ranges(core)) {
+            num_tiles_per_core = num_tiles_per_core_group_1;
+        } else if (core_group_2.core_coord_in_core_ranges(core)) {
+            num_tiles_per_core = num_tiles_per_core_group_2;
+        } else {
+            TT_ASSERT(false, "Core not in specified core ranges");
+        }
         tt_metal::WriteRuntimeArgsToDevice(
             device,
-            binary_reader_kernels[i],
+            binary_reader_kernel,
             core,
             {src0_dram_buffer->address(),
             (std::uint32_t)dram_src0_noc_xy.x,
             (std::uint32_t)dram_src0_noc_xy.y,
-            (std::uint32_t)num_tiles_per_core[i],
+            (std::uint32_t)num_tiles_per_core,
             src1_dram_buffer->address(),
             (std::uint32_t)dram_src1_noc_xy.x,
             (std::uint32_t)dram_src1_noc_xy.y,
-            num_tiles_per_core[i],
+            num_tiles_per_core,
             num_tiles_read }
         );
 
         tt_metal::WriteRuntimeArgsToDevice(
             device,
-            unary_writer_kernels[i],
+            unary_writer_kernel,
             core,
             {dst_dram_buffer->address(),
             (std::uint32_t)dram_dst_noc_xy.x,
             (std::uint32_t)dram_dst_noc_xy.y,
-            num_tiles_per_core[i],
+            num_tiles_per_core,
             num_tiles_read }
         );
-        num_tiles_read+=num_tiles_per_core[i];
+        num_tiles_read+=num_tiles_per_core;
     }
 
     // output does not hold any data, contains pointer to buffer on device with the data
