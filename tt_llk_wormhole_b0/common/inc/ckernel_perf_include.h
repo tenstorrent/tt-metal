3418061237c0a29a4411e9e4456190896401b4a3
Keivan Dabiri <kdabiri@tenstorrent.com>
2023-06-16 19:53:55 +0000

    Add brisc perf dump. Add runtime brisc compile

--- tt_llk_wormhole_b0/common/inc/ckernel_perf_include.h
+++ tt_llk_wormhole_b0/common/inc/ckernel_perf_include.h
@@ -17,7 +17,7 @@ static constexpr uint32_t PERF_DUMP_END_SIGNAL = 0xbeeff00d;
 static constexpr uint32_t PERF_CNT_DUMP_ENTRY_SIZE = 16; // Entry size in bytes
 
 #if PERF_DUMP_LEVEL == 0
-static constexpr int32_t TRISC_PERF_BUF_SIZE = l1_mem::address_map::TRISC_PERF_BUF_SIZE_LEVEL_0;
+static constexpr int32_t TRISC_PERF_BUF_SIZE = l1_mem::address_map::UNPACK_PACK_PERF_BUF_SIZE_LEVEL_0;
 #else
-static constexpr int32_t TRISC_PERF_BUF_SIZE = l1_mem::address_map::TRISC_PERF_BUF_SIZE_LEVEL_1;
+static constexpr int32_t TRISC_PERF_BUF_SIZE = l1_mem::address_map::UNPACK_PACK_PERF_BUF_SIZE_LEVEL_1;
 #endif
