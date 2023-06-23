e0ae017d6461bc6e8543766a4c48e907f44c1354
Almeet Bhullar <abhullar@tenstorrent.com>
2023-06-23 18:58:09 +0000

    #0: Fix for bmm_op_multi_core_reuse_mcast_padding to create CB by specifying multiple buffer indices

--- docs/source/apis/host_apis/buffers/CreateCircularBuffer.rst
+++ docs/source/apis/host_apis/buffers/CreateCircularBuffer.rst
@@ -1,6 +1,8 @@
 CreateCircularBuffer
 =====================
 
+When multiple buffer indices use the same address space, API that accepts a set of buffer indices must be used to ensure that the same address space is not requested from the allocator.
+
 .. doxygenfunction:: CreateCircularBuffer(Program &program, Device *device, uint32_t buffer_index, const CoreCoord &core, uint32_t num_tiles, uint32_t size_in_bytes, uint32_t l1_address, DataFormat data_format)
 
 .. doxygenfunction:: CreateCircularBuffer(Program &program, Device *device, uint32_t buffer_index, const CoreCoord &core, uint32_t num_tiles, uint32_t size_in_bytes, DataFormat data_format)
--- libs/tt_dnn/op_library/bmm/multi_core_reuse_mcast_padding/bmm_op_multi_core_reuse_mcast_padding.cpp
+++ libs/tt_dnn/op_library/bmm/multi_core_reuse_mcast_padding/bmm_op_multi_core_reuse_mcast_padding.cpp
@@ -593,25 +593,14 @@ tt_metal::Program create_program_mcast_in0(
     );
 
     uint32_t ouput_cb_index = 16; // output operands start at index 16
+    uint32_t interm0_cb_index = 24;
     auto cb_output = tt_metal::CreateCircularBuffers(
         program,
         device,
-        ouput_cb_index,
-        all_cores,
+        {ouput_cb_index, interm0_cb_index},
+        CoreRangeSet({all_cores}),
         out_CB_tiles,
         out_CB_size,
-        cb_data_format
-    );
-
-    uint32_t interm0_cb_index = 24;
-    auto cb_interm0 = tt_metal::CreateCircularBuffers(
-        program,
-        device,
-        interm0_cb_index,
-        all_cores,
-        out_CB_tiles,
-        out_CB_size,
-        cb_output->address(),
         cb_data_format
     );
 
@@ -890,25 +879,14 @@ tt_metal::Program create_program_mcast_in1(
     );
 
     uint32_t ouput_cb_index = 16; // output operands start at index 16
+    uint32_t interm0_cb_index = 24;
     auto cb_output = tt_metal::CreateCircularBuffers(
         program,
         device,
-        ouput_cb_index,
-        all_cores,
+        {ouput_cb_index, interm0_cb_index},
+        CoreRangeSet({all_cores}),
         out_CB_tiles,
         out_CB_size,
-        cb_data_format
-    );
-
-    uint32_t interm0_cb_index = 24;
-    auto cb_interm0 = tt_metal::CreateCircularBuffers(
-        program,
-        device,
-        interm0_cb_index,
-        all_cores,
-        out_CB_tiles,
-        out_CB_size,
-        cb_output->address(),
         cb_data_format
     );
 
