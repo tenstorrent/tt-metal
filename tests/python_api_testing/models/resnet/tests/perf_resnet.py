d04238510cf897499b511dd4e8e6be4a2965ca34
farbabi <farbabi@tenstorrent.com>
2023-06-28 18:47:00 +0000

    #1392: Add perf file for resnet50

---
+++ tests/python_api_testing/models/resnet/tests/perf_resnet.py
@@ -0,0 +1,87 @@
+from pathlib import Path
+import sys
+
+f = f"{Path(__file__).parent}"
+sys.path.append(f"{f}")
+sys.path.append(f"{f}/..")
+sys.path.append(f"{f}/../tt")
+sys.path.append(f"{f}/../..")
+sys.path.append(f"{f}/../../..")
+sys.path.append(f"{f}/../../../..")
+
+import torch
+from datasets import load_dataset
+from loguru import logger
+from torchvision import models
+from transformers import AutoImageProcessor
+
+import tt_lib
+from utility_functions_new import torch_to_tt_tensor_rm, tt_to_torch_tensor, Profiler
+from utility_functions_new import disable_compile_cache, enable_compile_cache
+from utility_functions_new import prep_report
+
+from sweep_tests.comparison_funcs import comp_allclose, comp_pcc
+from loguru import logger
+from resnetBlock import ResNet, Bottleneck
+
+BATCH_SIZE = 1
+
+
+def test_perf():
+    profiler = Profiler()
+    disable_compile_cache()
+    first_key = "first_iter"
+    second_key = "second_iter"
+    cpu_key = "ref_key"
+    profiler_key = first_key
+
+    # Initialize the device
+    device = tt_lib.device.CreateDevice(tt_lib.device.Arch.GRAYSKULL, 0)
+    tt_lib.device.InitializeDevice(device)
+    tt_lib.device.SetDefaultDevice(device)
+    host = tt_lib.device.GetHost()
+
+    model_name = "resnet50"
+
+    dataset = load_dataset("huggingface/cats-image")
+    image = dataset["test"]["image"][0]
+    image_processor = AutoImageProcessor.from_pretrained("microsoft/resnet-50")
+    inputs = image_processor(image, return_tensors="pt")
+
+    inputs = inputs["pixel_values"]
+    comments = f"{list(inputs.shape)[-2]}x{list(inputs.shape)[-1]}"
+
+    torch_resnet50 = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
+    torch_resnet50.eval()
+
+    state_dict = torch_resnet50.state_dict()
+
+    tt_resnet50 = ResNet(Bottleneck, [3, 4, 6, 3],
+                    device=device,
+                    host=host,
+                    state_dict=state_dict,
+                    base_address="",
+                    fold_batchnorm=True)
+
+
+    with torch.no_grad():
+        profiler.start(cpu_key)
+        logits = torch_resnet50(inputs)
+        profiler.end(cpu_key)
+
+        profiler.start(first_key)
+        tt_output = tt_resnet50(inputs)
+        profiler.end(first_key)
+
+        enable_compile_cache()
+
+        profiler.start(second_key)
+        tt_output = tt_resnet50(inputs)
+        profiler.end(second_key)
+
+
+    first_iter_time = profiler.get(first_key)
+    second_iter_time = profiler.get(second_key)
+    cpu_time = profiler.get(cpu_key)
+
+    prep_report("resnet50", BATCH_SIZE, first_iter_time, second_iter_time, comments, cpu_time)
