e2dde3ddf65a048ed365be623a398f8fce8d07ce
Bill Teng <bteng@tenstorrent.com>
2023-06-30 23:32:54 +0000

    #1163: update intersect testcase

--- tests/tt_metal/tt_metal/gtest_unit_tests/core_coord/test_CoreRange_intersects.cpp
+++ tests/tt_metal/tt_metal/gtest_unit_tests/core_coord/test_CoreRange_intersects.cpp
@@ -9,13 +9,21 @@ namespace basic_tests::CoreRange{
 
 TEST_F(CoreCoordHarness, TestCoreRangeIntersects)
 {
-    EXPECT_EQ ( this->cr2.intersects(this->cr3), ::CoreRange({3,3}, {3,3}));
-    EXPECT_EQ ( this->cr1.intersects(this->cr3).value(), ::CoreRange({1,2}, {2,2}));
 
+    EXPECT_TRUE( this->cr1.intersects(this->cr5).has_value() );
+    EXPECT_EQ ( this->cr1.intersects(this->cr5).value(), ::CoreRange({1,0}, {1,1}));
+
+    EXPECT_TRUE( this->single_core.intersects(this->cr6).has_value());
     EXPECT_EQ ( this->single_core.intersects(this->cr6).value(), this->single_core);
+
+    EXPECT_TRUE( this->cr4.intersects(this->cr5).has_value());
     EXPECT_EQ ( this->cr4.intersects(this->cr5).value(), ::CoreRange({1,0}, {5,4} ));
-    EXPECT_EQ ( this->cr1.intersects(this->cr5).value(), ::CoreRange({1,0}, {2,2} ));
+
+    EXPECT_TRUE( this->cr1.intersects(this->cr6).has_value());
     EXPECT_EQ ( this->cr1.intersects(this->cr6).value(), this->cr1 );
+
+    EXPECT_TRUE( this->cr7.intersects(this->cr8).has_value());
+    EXPECT_EQ ( this->cr7.intersects(this->cr8).value(), this->cr7 );
 }
 
 TEST_F(CoreCoordHarness, TestCoreRangeNotIntersects){
