bb93e77691a51f702b6303082f210adec2f3133e
Bill Teng <bteng@tenstorrent.com>
2023-07-05 22:41:36 +0000

    #1163: separate into 2 tests

--- tests/tt_metal/tt_metal/gtest_unit_tests/core_coord/test_CoreRangeSet_construct.cpp
+++ tests/tt_metal/tt_metal/gtest_unit_tests/core_coord/test_CoreRangeSet_construct.cpp
@@ -4,18 +4,21 @@
 
 namespace basic_tests::CoreRangeSet{
 
-TEST_F(CoreCoordHarness, TestCoreRangeSetConstruct)
+TEST_F(CoreCoordHarness, TestCoreRangeSetValidConstruct)
 {
-
-    ::CoreRange overlapping_range = {.start={1, 2}, .end={3, 3}};
-
-    EXPECT_ANY_THROW( ::CoreRangeSet({this->cr1, this->cr2, overlapping_range}) );
-
     EXPECT_NO_THROW ( ::CoreRangeSet({this->single_core, this->cr2}));
-    EXPECT_ANY_THROW( ::CoreRangeSet({this->single_core, this->cr1}) );
+    EXPECT_NO_THROW ( ::CoreRangeSet({this->cr1, this->cr2}) );
+
     ::CoreRangeSet valid_ranges = ::CoreRangeSet({this->cr1, this->cr2});
     EXPECT_EQ(valid_ranges.ranges().size(), 2);
-
 }
+
+TEST_F(CoreCoordHarness, TestCoreRangeSetInvalidConstruct)
+{
+    ::CoreRange overlapping_range = {.start={1, 2}, .end={3, 3}};
+    EXPECT_ANY_THROW( ::CoreRangeSet({this->cr1, this->cr2, overlapping_range}) );
+    EXPECT_ANY_THROW( ::CoreRangeSet({this->single_core, this->cr1}) );
+}
+
 
 }
