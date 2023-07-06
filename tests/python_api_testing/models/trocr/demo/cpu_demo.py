d66208e4f72a983b3b455ce0feff4de6028b1831
Saichand A <saichand@multicorewareinc.com>
2023-07-06 08:20:46 +0000

    #1522: Uplift TrOCR CPU Demo to use fixture for input image from IAM dataset

--- tests/python_api_testing/models/trocr/demo/cpu_demo.py
+++ tests/python_api_testing/models/trocr/demo/cpu_demo.py
@@ -2,30 +2,27 @@ import torch
 import pytest
 from loguru import logger
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
-from PIL import Image
-import requests
+from torchvision.utils import save_image
 
 
 @pytest.mark.parametrize(
     "model_name",
     (("microsoft/trocr-base-handwritten"),),
 )
-def test_cpu_demo(model_name, reset_seeds):
-
-    url = "https://fki.tic.heia-fr.ch/static/img/a01-122-02-00.jpg"
-    image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
-
+def test_cpu_demo(model_name, iam_ocr_sample_input, reset_seeds):
     processor = TrOCRProcessor.from_pretrained(model_name)
     model = VisionEncoderDecoderModel.from_pretrained(model_name)
-    pixel_values = processor(images=image, return_tensors="pt").pixel_values
+    pixel_values = processor(
+        images=iam_ocr_sample_input, return_tensors="pt"
+    ).pixel_values
 
     with torch.no_grad():
         generated_ids = model.generate(pixel_values)
 
     generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
-    image.save("image.jpg")
-    logger.info("Image is saved under image.jpg for reference.")
+    save_image(iam_ocr_sample_input, "trocr_input_image.jpg")
+    logger.info("Image is saved under trocr_input_image.jpg for reference.")
 
     logger.info("HF Model answered")
     logger.info(generated_text)
