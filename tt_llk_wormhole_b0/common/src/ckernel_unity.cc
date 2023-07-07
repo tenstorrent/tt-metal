1a992ee75b0e6b0473db2c46ab259db8bebb6236
Milos Acimovic <macimovic@tenstorrent.com>
2023-07-07 23:13:00 +0000

    CKernels unity build

---
+++ tt_llk_wormhole_b0/common/src/ckernel_unity.cc
@@ -0,0 +1,7 @@
+// combining multiple C++ source files into a single file
+// to reduce the overhead of the compilation process and 
+// improve build times
+#include "ckernel.cc"
+#include "ckernel_template.cc"
+#include "ckernel_perf_unpack_pack.cc"
+#include "ckernel_main.cc"
