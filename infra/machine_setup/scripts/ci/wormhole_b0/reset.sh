f238c65a2fcf96010fc447f46b5e5f7a4cf39f63
Raymond Kim <rkim@tenstorrent.com>
2023-07-10 15:10:09 +0000

    #219: Add weka and hugepages checks to CI reset script

--- infra/machine_setup/scripts/ci/wormhole_b0/reset.sh
+++ infra/machine_setup/scripts/ci/wormhole_b0/reset.sh
@@ -1,1 +1,19 @@
+set -eo pipefail
+
 tt-smi -wr all
+
+WEKA_CHECK_DIR="/mnt/MLPerf/bit_error_tests"
+
+if [ -d $WEKA_CHECK_DIR ]; then
+  echo "Weka check passed"
+else
+  echo "$WEKA_CHECK_DIR does exist - Weka is likely disconnected from this runner"
+  exit 1
+fi
+
+if python3 /opt/tt_metal_infra/scripts/setup_hugepages.py check; then
+  echo "Hugepages check passed"
+else
+  echo "Hugepages check did not pass - error"
+  exit 1
+fi
