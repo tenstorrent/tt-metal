04f74b7326b130512d0963e85a63e23a425a9d30
tarafdarTT <ntarafdar@tenstorrent.com>
2023-07-14 02:24:23 +0000

    #1652: Removing headers in blank

--- tt_metal/kernels/compute/blank.cpp
+++ tt_metal/kernels/compute/blank.cpp
@@ -1,10 +1,4 @@
  #ifdef TRISC_MATH
- #include "llk_math_common.h"
- #include "llk_math_matmul.h"
- #include "llk_math_eltwise_unary_datacopy.h"
- #include "llk_math_eltwise_binary.h"
- #include "llk_math_eltwise_unary_sfpu.h"
- #include "llk_math_reduce.h"
  #define MATH(x) x
  #define MAIN math_main()
  #else
@@ -12,8 +6,6 @@
  #endif
 
  #ifdef TRISC_PACK
- #include "llk_pack_common.h"
- #include "llk_pack.h"
  #define PACK(x) x
  #define MAIN pack_main()
  #else
@@ -21,13 +13,6 @@
  #endif
 
  #ifdef TRISC_UNPACK
- #include "llk_unpack_common.h"
- #include "llk_unpack_AB_matmul.h"
- #include "llk_unpack_A.h"
- #include "llk_unpack_AB.h"
- #include "llk_unpack_reduce.h"
- #include "llk_unpack_tilize.h"
- #include "llk_unpack_untilize.h"
  #define UNPACK(x) x
  #define MAIN unpack_main()
  #else
