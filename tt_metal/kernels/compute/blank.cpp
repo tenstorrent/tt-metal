80388e9437c156e2d51212c075986b3105880a28
tarafdarTT <ntarafdar@tenstorrent.com>
2023-07-14 15:51:15 +0000

    #1657: Remove unnecessary defines

--- tt_metal/kernels/compute/blank.cpp
+++ tt_metal/kernels/compute/blank.cpp
@@ -1,24 +1,14 @@
  #ifdef TRISC_MATH
- #define MATH(x) x
  #define MAIN math_main()
- #else
- #define MATH(x)
  #endif
 
  #ifdef TRISC_PACK
- #define PACK(x) x
  #define MAIN pack_main()
- #else
- #define PACK(x)
  #endif
 
  #ifdef TRISC_UNPACK
- #define UNPACK(x) x
  #define MAIN unpack_main()
- #else
- #define UNPACK(x)
  #endif
-
 
 namespace NAMESPACE {
 void MAIN {
