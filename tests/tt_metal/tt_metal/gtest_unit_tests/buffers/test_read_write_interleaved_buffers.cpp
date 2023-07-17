d64d32ad3d2a527d20eee92d945d37f8c6e4b3bd
Almeet Bhullar <abhullar@tenstorrent.com>
2023-07-17 23:40:56 +0000

    #1662: Uplift 32B and non 32B aligned buffer unit tests to use enqueue APIs

--- tests/tt_metal/tt_metal/gtest_unit_tests/buffers/test_read_write_interleaved_buffers.cpp
+++ tests/tt_metal/tt_metal/gtest_unit_tests/buffers/test_read_write_interleaved_buffers.cpp
@@ -26,102 +26,95 @@ vector<u32> generate_arange_vector(u32 size_bytes) {
     return src;
 }
 
-bool test_buffer_loopback(Device* device, const BufferConfig& config) {
+bool test_buffer_loopback(Device* device, CommandQueue& cq, const BufferConfig& config) {
     Buffer buf(device, config.total_size, config.page_size, config.type);
 
     vector<u32> src = generate_arange_vector(buf.size());
 
     WriteToBuffer(buf, src);
+    EnqueueWriteBuffer(cq, buf, src, false);
 
     vector<u32> result;
-    ReadFromBuffer(buf, result);
+    EnqueueReadBuffer(cq, buf, result, true);
 
     TT_ASSERT(src.size() == result.size());
-
-    if (src != result) {
-        for (int i = 0; i < src.size(); i++) {
-            if (src.at(i) != result.at(i)) {
-                std::cout << "at " << i << " src is " << src.at(i) << " result is " << result.at(i) << std::endl;
-            }
-        }
-    }
 
     return src == result;
 }
 
-TEST_F(L1BankingDeviceHarness, Test32BAlignedDramBufferAndPageSize) {
+TEST_F(CommandQueueHarness, Test32BAlignedDramBufferAndPageSize) {
     constexpr u32 page_size = ADDRESS_ALIGNMENT * 64;
     constexpr BufferType buff_type = BufferType::DRAM;
     auto num_banks = this->device->num_banks(buff_type);
     BufferConfig buffer_config { .page_size = page_size, .total_size = page_size * (num_banks * 128) };
-    EXPECT_TRUE(test_buffer_loopback(this->device, buffer_config));
+    EXPECT_TRUE(test_buffer_loopback(this->device, *this->cq, buffer_config));
 }
 
-TEST_F(L1BankingDeviceHarness, Test32BAlignedL1BufferAndPageSize) {
+TEST_F(CommandQueueHarness, Test32BAlignedL1BufferAndPageSize) {
     constexpr u32 page_size = ADDRESS_ALIGNMENT * 64;
     constexpr BufferType buff_type = BufferType::L1;
     auto num_banks = this->device->num_banks(buff_type);
     BufferConfig buffer_config { .page_size = page_size, .total_size = page_size * (num_banks * 128) };
-    EXPECT_TRUE(test_buffer_loopback(this->device, buffer_config));
+    EXPECT_TRUE(test_buffer_loopback(this->device, *this->cq, buffer_config));
 }
 
-TEST_F(L1BankingDeviceHarness, Test32BAlignedDramBufferAndNon32BAlignedPageSize) {
+TEST_F(CommandQueueHarness, Test32BAlignedDramBufferAndNon32BAlignedPageSize) {
     u32 page_size = roundup((ADDRESS_ALIGNMENT - 1) * (64 - 1), sizeof(u32));
     constexpr BufferType buff_type = BufferType::DRAM;
     auto num_banks = this->device->num_banks(buff_type);
     BufferConfig buffer_config { .page_size = page_size, .total_size = page_size * (num_banks * 128) };
     EXPECT_NE(buffer_config.page_size % ADDRESS_ALIGNMENT, 0);
     EXPECT_EQ(buffer_config.total_size % ADDRESS_ALIGNMENT, 0);
-    EXPECT_TRUE(test_buffer_loopback(this->device, buffer_config));
+    EXPECT_TRUE(test_buffer_loopback(this->device, *this->cq, buffer_config));
 }
 
-TEST_F(L1BankingDeviceHarness, Test32BAlignedL1BufferAndNon32BAlignedPageSize) {
+TEST_F(CommandQueueHarness, Test32BAlignedL1BufferAndNon32BAlignedPageSize) {
     u32 page_size = roundup((ADDRESS_ALIGNMENT - 1) * (64 - 1), sizeof(u32));
     constexpr BufferType buff_type = BufferType::L1;
     auto num_banks = this->device->num_banks(buff_type);
     BufferConfig buffer_config { .page_size = page_size, .total_size = page_size * (num_banks * 2) };
     EXPECT_NE(buffer_config.page_size % ADDRESS_ALIGNMENT, 0);
     EXPECT_EQ(buffer_config.total_size % ADDRESS_ALIGNMENT, 0);
-    EXPECT_TRUE(test_buffer_loopback(this->device, buffer_config));
+    EXPECT_TRUE(test_buffer_loopback(this->device, *this->cq, buffer_config));
 }
 
-TEST_F(L1BankingDeviceHarness, TestNon32BAlignedDramBufferAndPageSize) {
+TEST_F(CommandQueueHarness, TestNon32BAlignedDramBufferAndPageSize) {
     u32 page_size = roundup((ADDRESS_ALIGNMENT - 1) * (64 - 1), sizeof(u32));
     constexpr BufferType buff_type = BufferType::DRAM;
     auto num_banks = this->device->num_banks(buff_type);
     BufferConfig buffer_config { .page_size = page_size, .total_size = page_size * ((num_banks + 1) * 127) };
     EXPECT_NE(buffer_config.page_size % ADDRESS_ALIGNMENT, 0);
     EXPECT_NE(buffer_config.total_size % ADDRESS_ALIGNMENT, 0);
-    EXPECT_TRUE(test_buffer_loopback(this->device, buffer_config));
+    EXPECT_TRUE(test_buffer_loopback(this->device, *this->cq, buffer_config));
 }
 
-TEST_F(L1BankingDeviceHarness, TestNon32BAlignedL1BufferAndPageSize) {
+TEST_F(CommandQueueHarness, TestNon32BAlignedL1BufferAndPageSize) {
     u32 page_size = roundup((ADDRESS_ALIGNMENT - 1) * (64 - 1), sizeof(u32));
     constexpr BufferType buff_type = BufferType::L1;
     auto num_banks = this->device->num_banks(buff_type);
     BufferConfig buffer_config { .page_size = page_size, .total_size = page_size * ((num_banks + 1) * 63) };
     EXPECT_NE(buffer_config.page_size % ADDRESS_ALIGNMENT, 0);
     EXPECT_NE(buffer_config.total_size % ADDRESS_ALIGNMENT, 0);
-    EXPECT_TRUE(test_buffer_loopback(this->device, buffer_config));
+    EXPECT_TRUE(test_buffer_loopback(this->device, *this->cq, buffer_config));
 }
 
-TEST_F(L1BankingDeviceHarness, TestBackToBackNon32BAlignedPageSize) {
+TEST_F(CommandQueueHarness, TestBackToBackNon32BAlignedPageSize) {
     constexpr BufferType buff_type = BufferType::DRAM;
     auto num_banks = this->device->num_banks(buff_type);
 
     Buffer bufa(device, 125000, 100, buff_type);
     auto src_a = generate_arange_vector(bufa.size());
-    WriteToBuffer(bufa, src_a);
+    EnqueueWriteBuffer(*this->cq, bufa, src_a, false);
 
     Buffer bufb(device, 152000, 152, buff_type);
     auto src_b = generate_arange_vector(bufb.size());
-    WriteToBuffer(bufb, src_b);
+    EnqueueWriteBuffer(*this->cq, bufb, src_b, false);
 
     vector<u32> result_a;
-    ReadFromBuffer(bufa, result_a);
+    EnqueueReadBuffer(*this->cq, bufa, result_a, true);
 
     vector<u32> result_b;
-    ReadFromBuffer(bufb, result_b);
+    EnqueueReadBuffer(*this->cq, bufb, result_b, true);
 
     EXPECT_EQ(src_a, result_a);
     EXPECT_EQ(src_b, result_b);
