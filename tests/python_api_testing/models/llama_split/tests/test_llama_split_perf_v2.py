48020db96e0da7805bad6e5ec15e2f7cc083ed21
Branko Arsic <barsic@e12cs05.tenstorrent.net>
2023-07-18 08:25:25 +0000

    #491: add initial perf test v2

---
+++ tests/python_api_testing/models/llama_split/tests/test_llama_split_perf_v2.py
@@ -0,0 +1,338 @@
+import math
+from pathlib import Path
+import sys
+
+f = f"{Path(__file__).parent}"
+sys.path.append(f"{f}/..")
+sys.path.append(f"{f}/../..")
+sys.path.append(f"{f}/../../..")
+sys.path.append(f"{f}/../../../..")
+
+import math
+import time
+import torch
+import pytest
+from torch import nn
+import tt_lib
+from loguru import logger
+from python_api_testing.models.llama.llama_utils import (
+    tt2torch_tensor,
+    gen_position_ids,
+)
+from transformers import AutoTokenizer, AutoModelForCausalLM, PreTrainedModel
+from typing import List, Optional, Tuple, Union
+from python_api_testing.models.llama.llama_layer_norm import TtLlamaRMSNorm
+from python_api_testing.models.llama.llama_decoder import TtLlamaDecoderLayer
+from python_api_testing.models.llama_split.llama_split_utils import (
+    pad_input_32_left,
+    prepare_llama_input,
+    get_next_llama_output_token,
+)
+from python_api_testing.models.llama_split.tt.llama import (
+    llama_first_half,
+    llama_second_half,
+)
+from llama_split_utils import get_logits_processor
+from utility_functions_new import (
+    profiler,
+    enable_compile_cache,
+    disable_compile_cache,
+    prep_report,
+)
+
+
+def run_llama_split_inference(
+    device,
+    state_dict,
+    base_url,
+    max_position_embeddings,
+    configuration,
+    num_decoders_start,
+    num_decoders,
+    x_inputs=None,
+    att_mask=None,
+    position_ids=None,
+    half=1,
+    is_causallm=False,
+):
+    if half == 1:
+        logger.debug("First pass through TT model")
+        tt_llama_model = llama_first_half(
+            device,
+            state_dict,
+            base_url,
+            max_position_embeddings,
+            configuration,
+            num_decoders_start,
+            num_decoders,
+        )
+        tt_out = tt_llama_model(
+            input_ids=x_inputs, attention_mask=att_mask, position_ids=position_ids
+        )
+    else:
+        logger.debug("Second pass through TT model")
+        tt_llama_model = llama_second_half(
+            device,
+            state_dict,
+            base_url,
+            max_position_embeddings,
+            configuration,
+            num_decoders_start,
+            num_decoders,
+            is_causallm,
+        )
+        tt_out = tt_llama_model(
+            input_ids=x_inputs, attention_mask=att_mask, position_ids=position_ids
+        )
+
+    # returned type from the model is tuple
+    tt_output = tt2torch_tensor(tt_out[0])
+    return tt_output
+
+
+def call_tt_llama_forward_func(
+    configuration,
+    state_dict,
+    base_url,
+    max_position_embeddings,
+    logits_processor,
+    tokenizer,
+    input_ids,
+    attention_mask,
+    first_decoder_start,
+    second_decoder_start,
+    num_consecutive_decoders,
+    is_causallm,
+):
+    # Disable compile cache
+    disable_compile_cache()
+
+    # Perf tests keys
+    first_key = "first_iter"
+    second_key = "second_iter"
+
+    input_ids_padded = input_ids
+    attention_mask_padded = attention_mask
+    position_ids_padded = gen_position_ids(input_ids_padded)
+
+    logger.debug(f"The first call of the first half started")
+    device = tt_lib.device.CreateDevice(tt_lib.device.Arch.GRAYSKULL, 0)
+    tt_lib.device.InitializeDevice(device)
+    tt_lib.device.SetDefaultDevice(device)
+    host = tt_lib.device.GetHost()
+
+    profiler.start(first_key)
+    first_out = run_llama_split_inference(
+        device,
+        state_dict,
+        base_url,
+        max_position_embeddings,
+        configuration,
+        num_decoders_start=first_decoder_start,
+        num_decoders=num_consecutive_decoders,
+        x_inputs=input_ids_padded,
+        att_mask=attention_mask_padded,
+        position_ids=position_ids_padded,
+        half=1,
+    )
+    profiler.start(first_key)
+    logger.debug(f"The first call of the first half ended")
+
+    # The second call of the first half
+    logger.debug(f"The second call of the first half started")
+    enable_compile_cache()
+    profiler.start(second_key)
+    first_out = run_llama_split_inference(
+        device,
+        state_dict,
+        base_url,
+        max_position_embeddings,
+        configuration,
+        num_decoders_start=first_decoder_start,
+        num_decoders=num_consecutive_decoders,
+        x_inputs=input_ids_padded,
+        att_mask=attention_mask_padded,
+        position_ids=position_ids_padded,
+        half=1,
+    )
+    profiler.start(second_key)
+    logger.debug(f"The second call of the first half ended")
+
+    print(f"The first compile time: {profiler.print()}")
+
+    tt_lib.device.CloseDevice(device)
+    logger.debug(f"The first call ended")
+
+    # The second call -------------------------------------------------------
+    logger.debug(f"The second call started")
+    device = tt_lib.device.CreateDevice(tt_lib.device.Arch.GRAYSKULL, 0)
+    tt_lib.device.InitializeDevice(device)
+    tt_lib.device.SetDefaultDevice(device)
+
+    # send input tensor from host to tt device
+    tt_input = first_out
+
+    tt_out = run_llama_split_inference(
+        device,
+        state_dict,
+        base_url,
+        max_position_embeddings,
+        configuration,
+        num_decoders_start=second_decoder_start,
+        num_decoders=num_consecutive_decoders,
+        x_inputs=tt_input,
+        att_mask=attention_mask_padded,
+        position_ids=position_ids_padded,
+        half=2,
+        is_causallm=is_causallm,
+    )
+    logger.debug(f"The second call ended")
+
+    # squeeze output
+    tt_out = tt_out.squeeze(1)
+
+    tt_lib.device.CloseDevice(device)
+    device = None
+
+    return tt_out
+
+
+# parameters --------------------------------------------------
+_tokenizer_name = "huggyllama/llama-7b"
+_llama_model_name = "huggyllama/llama-7b"
+# base url from the model state dictionary
+_base_url = "model.layers"
+_max_position_embeddings = 2048
+_is_causallm = False
+BATCH_SIZE = 1
+
+# how many decoders to use
+# number of decoders to be stacked started from the selected id in the original llama model
+# e.g. stack 16 consecutive decoders
+_num_consecutive_decoders = 1
+
+# decoder id from which decoder stacking starts (the first half of the model)
+# e.g. start from 0 add use 3 decoders (0, 1, and 2)
+_first_decoder_start = 0
+
+# decoder id from which decoder stacking starts (the second half of the model)
+# e.g. start from 16 add use 3 decoders (16, 17, and 18)
+_second_decoder_start = _num_consecutive_decoders
+# parameters --------------------------------------------------
+
+# prompt = """Author-contribution statements and acknowledgements in research papers should state clearly and specifically whether, and to what extent, the authors used AI technologies such as ChatGPT in the preparation of their manuscript and analysis.
+# They should also indicate which LLMs were used. This will alert editors and reviewers to scrutinize manuscripts more carefully for potential biases, inaccuracies and improper source crediting. Likewise, scientific journals should be transparent about their use of LLMs, for example when selecting submitted manuscripts.
+# Mention the large language model based product mentioned in the paragraph above:"""
+prompt = "I believe the meaning of life is to"
+
+
+@pytest.mark.parametrize(
+    "prompt",
+    ((prompt),),
+)
+def test_llama_pcc(prompt):
+    # disable_compile_cache()
+    # first_key = "first_iter"
+    # second_key = "second_iter"
+    cpu_key = "ref_key"
+    comments = "llama model with two loads (halfs)"
+
+    # set parameters =================================================================
+    tokenizer_name = _tokenizer_name
+    llama_model_name = _llama_model_name
+    is_causallm = _is_causallm
+    base_url = _base_url
+    max_position_embeddings = _max_position_embeddings
+
+    # how many decoders to use
+    first_decoder_start = _first_decoder_start
+    second_decoder_start = _second_decoder_start
+    num_consecutive_decoders = _num_consecutive_decoders
+
+    # load llama pytorch model ================================================
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+    hugging_face_reference_model = AutoModelForCausalLM.from_pretrained(
+        llama_model_name
+    )
+
+    hugging_face_reference_model.eval()
+    # get configurations
+    configuration = hugging_face_reference_model.config
+    state_dict = hugging_face_reference_model.state_dict()
+
+    # generate real input =====================================================
+    inputs = tokenizer(prompt, return_tensors="pt")
+    input_ids = inputs.input_ids
+    attention_mask = inputs.attention_mask
+
+    logits_processor = get_logits_processor(
+        input_ids, hugging_face_reference_model.config
+    )
+
+    is_input_padded = True
+    input_ids_padded, attention_mask_padded, position_ids_padded = prepare_llama_input(
+        prompt, tokenizer, configuration, is_input_padded
+    )
+
+    # PyTorch output ===========================================================
+    hugging_face_reference_model = hugging_face_reference_model.get_decoder()
+
+    # TT output: call forward() function several times ========================
+    with torch.no_grad():
+        # call huggingface model
+        profiler.start(cpu_key)
+        pytorch_out = hugging_face_reference_model(
+            input_ids=input_ids_padded,
+            attention_mask=attention_mask_padded,
+            position_ids=position_ids_padded,
+        )
+        pytorch_out = pytorch_out.last_hidden_state
+        profiler.end(cpu_key)
+
+        # The first TT model call
+        # profiler.start(first_key)
+        tt_output = call_tt_llama_forward_func(
+            configuration,
+            state_dict,
+            base_url,
+            max_position_embeddings,
+            logits_processor,
+            tokenizer,
+            input_ids_padded,
+            attention_mask_padded,
+            first_decoder_start,
+            second_decoder_start,
+            num_consecutive_decoders,
+            is_causallm,
+        )
+        # profiler.end(first_key)
+
+        # enable_compile_cache()
+
+        # The second TT model call
+        # profiler.start(second_key)
+        tt_output = call_tt_llama_forward_func(
+            configuration,
+            state_dict,
+            base_url,
+            max_position_embeddings,
+            logits_processor,
+            tokenizer,
+            input_ids_padded,
+            attention_mask_padded,
+            first_decoder_start,
+            second_decoder_start,
+            num_consecutive_decoders,
+            is_causallm,
+        )
+        # profiler.end(second_key)
+
+    # first_iter_time = profiler.get(first_key)
+    # second_iter_time = profiler.get(second_key)
+    first_iter_time = 0
+    second_iter_time = 0
+    cpu_time = profiler.get(cpu_key)
+
+    prep_report(
+        "lamma", BATCH_SIZE, first_iter_time, second_iter_time, comments, cpu_time
+    )
