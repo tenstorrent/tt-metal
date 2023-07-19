bb7b164ef06ab6d82d2b17531b10c61097f0c28b
tarafdarTT <ntarafdar@tenstorrent.com>
2023-07-19 16:11:30 +0000

    #1668: Remove unnecessary includes

--- tt_metal/src/ckernels/grayskull/llk_lib/llk_math_eltwise_unary_sfpu_recip.h
+++ tt_metal/src/ckernels/grayskull/llk_lib/llk_math_eltwise_unary_sfpu_recip.h
@@ -1,14 +1,4 @@
 #pragma once
-#include <type_traits>
-
-#include "ckernel_globals.h"
-#include "ckernel_include.h"
-#include "ckernel_sfpu.h"
-#include "ckernel_template.h"
-#include "cmath_common.h"
-#include "llk_format_conversions.h"
-#include "llk_math_common.h"
-#include "llk_param_structs.h"
 #include "llk_math_eltwise_unary_sfpu_common.h"
 using namespace ckernel;
 
