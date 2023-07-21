02f3fc37211dab41701c08011404bf89f0960038
Almeet Bhullar <abhullar@tenstorrent.com>
2023-07-21 20:59:10 +0000

    #1662: Add test case for writing/reading to two back to back buffers with non 32B aligned pages

--- tests/tt_metal/tt_metal/gtest_unit_tests/command_queue/test_EnqueueWriteBuffer_and_EnqueueReadBuffer.cpp
+++ tests/tt_metal/tt_metal/gtest_unit_tests/command_queue/test_EnqueueWriteBuffer_and_EnqueueReadBuffer.cpp
@@ -22,14 +22,22 @@ struct BufferStressTestConfig {
 };
 
 namespace local_test_functions {
-bool test_EnqueueWriteBuffer_and_EnqueueReadBuffer(Device* device, CommandQueue& cq, const BufferConfig& config) {
-    size_t buf_size = config.num_pages * config.page_size;
-    Buffer bufa(device, buf_size, config.page_size, config.buftype);
-    vector<u32> src(buf_size / sizeof(u32), 0);
+
+vector<u32> generate_arange_vector(u32 size_bytes) {
+    TT_ASSERT(size_bytes % sizeof(u32) == 0);
+    vector<u32> src(size_bytes / sizeof(u32), 0);
 
     for (u32 i = 0; i < src.size(); i++) {
         src.at(i) = i;
     }
+    return src;
+}
+
+bool test_EnqueueWriteBuffer_and_EnqueueReadBuffer(Device* device, CommandQueue& cq, const BufferConfig& config) {
+    size_t buf_size = config.num_pages * config.page_size;
+    Buffer bufa(device, buf_size, config.page_size, config.buftype);
+
+    vector<u32> src = generate_arange_vector(bufa.size());
 
     EnqueueWriteBuffer(cq, bufa, src, false);
 
@@ -200,6 +208,27 @@ TEST_F(CommandQueueHarness, TestNon32BAlignedPageSizeForL1) {
     BufferConfig config = {.num_pages = 1250, .page_size = 200, .buftype = BufferType::L1};
 
     EXPECT_TRUE(local_test_functions::test_EnqueueWriteBuffer_and_EnqueueReadBuffer(this->device, *this->cq, config));
+}
+
+TEST_F(CommandQueueHarness, TestBackToBackNon32BAlignedPageSize) {
+    constexpr BufferType buff_type = BufferType::L1;
+
+    Buffer bufa(device, 125000, 100, buff_type);
+    auto src_a = local_test_functions::generate_arange_vector(bufa.size());
+    EnqueueWriteBuffer(*this->cq, bufa, src_a, false);
+
+    Buffer bufb(device, 152000, 152, buff_type);
+    auto src_b = local_test_functions::generate_arange_vector(bufb.size());
+    EnqueueWriteBuffer(*this->cq, bufb, src_b, false);
+
+    vector<u32> result_a;
+    EnqueueReadBuffer(*this->cq, bufa, result_a, true);
+
+    vector<u32> result_b;
+    EnqueueReadBuffer(*this->cq, bufb, result_b, true);
+
+    EXPECT_EQ(src_a, result_a);
+    EXPECT_EQ(src_b, result_b);
 }
 
 }  // end namespace l1_tests
