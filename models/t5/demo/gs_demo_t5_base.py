280f71f663d2cd6ef52a9eaf400fa25f723c3e5b
Nemanja Grujic <ngrujic@e12cs06.tenstorrent.net>
2023-07-24 08:48:55 +0000

    #1787: Reduce code redudancy in gs_demo_t5 filed

--- models/t5/demo/gs_demo_t5_base.py
+++ models/t5/demo/gs_demo_t5_base.py
@@ -1,30 +1,8 @@
-import tt_lib
-from loguru import logger
-from transformers import AutoTokenizer
-
 from models.t5.tt.t5_for_conditional_generation import (
     t5_base_for_conditional_generation,
 )
-from models.generation_utils import run_generate
+from models.t5.demo.demo_utils import run_demo_t5
 
 
 def test_demo_t5_small():
-    input_sentance = "translate English to German: The house is wonderful."
-    tokenizer = AutoTokenizer.from_pretrained("t5-base", model_max_length=32)
-
-    device = tt_lib.device.CreateDevice(tt_lib.device.Arch.GRAYSKULL, 0)
-    tt_lib.device.InitializeDevice(device)
-
-    output_sentance = run_generate(
-        input_sentance,
-        tokenizer,
-        t5_base_for_conditional_generation,
-        device,
-        run_tt_model=True,
-        log=False,
-    )
-
-    logger.info(f"Input sentance: '{input_sentance}'")
-    logger.info(f"Tt output: '{output_sentance}'")
-
-    tt_lib.device.CloseDevice(device)
+    run_demo_t5(t5_base_for_conditional_generation)
