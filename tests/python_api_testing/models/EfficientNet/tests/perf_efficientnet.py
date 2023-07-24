df75183f8f64038ef24e2ee89629701a5094f5da
Nemanja Grujic <ngrujic@e12cs06.tenstorrent.net>
2023-07-24 13:51:29 +0000

    #1495: Efficient Net uplift perf test to new standards

--- tests/python_api_testing/models/EfficientNet/tests/perf_efficientnet.py
+++ tests/python_api_testing/models/EfficientNet/tests/perf_efficientnet.py
@@ -3,12 +3,13 @@ import torch
 from loguru import logger
 import torchvision
 from datasets import load_dataset
+import pytest
 
 from tests.python_api_testing.models.utility_functions_new import (
     torch2tt_tensor,
     tt2torch_tensor,
-    Profiler,
-    prep_report
+    profiler,
+    prep_report,
 )
 from models.EfficientNet.tt.efficientnet_model import efficientnet_b0
 from models.utility_functions import disable_compile_cache, enable_compile_cache
@@ -28,8 +29,21 @@ def make_input_tensor(imagenet_sample_input, resize=256, crop=224):
     return transform(imagenet_sample_input)
 
 
-def test_perf_efficientnet_b0(imagenet_sample_input):
-    profiler = Profiler()
+@pytest.mark.parametrize(
+    "expected_inference_time, expected_compile_time",
+    (
+        (
+            6.0,
+            16.5,
+        ),
+    ),
+)
+def test_perf_efficientnet_b0(
+    use_program_cache,
+    imagenet_sample_input,
+    expected_inference_time,
+    expected_compile_time,
+):
     disable_compile_cache()
     first_key = "first_iter"
     second_key = "second_iter"
@@ -43,29 +57,43 @@ def test_perf_efficientnet_b0(imagenet_sample_input):
 
     test_input = make_input_tensor(imagenet_sample_input)
 
-    hf_model = torchvision.models.efficientnet_b0(pretrained=True)
-    tt_input = torch2tt_tensor(test_input, tt_device=device, tt_layout=tt_lib.tensor.Layout.ROW_MAJOR)
+    hf_model = torchvision.models.efficientnet_b0()
+    tt_input = torch2tt_tensor(
+        test_input, tt_device=device, tt_layout=tt_lib.tensor.Layout.ROW_MAJOR
+    )
     tt_model = efficientnet_b0(device)
 
     with torch.no_grad():
         profiler.start(cpu_key)
         hf_model(test_input)
+        tt_lib.device.Synchronize()
         profiler.end(cpu_key)
 
         profiler.start(first_key)
-        tt_model(tt_input)
+        tt_output = tt_model(tt_input)
+        tt_lib.device.Synchronize()
         profiler.end(first_key)
+        del tt_output
 
         enable_compile_cache()
 
         profiler.start(second_key)
-        tt_model(tt_input)
+        tt_output = tt_model(tt_input)
+        tt_lib.device.Synchronize()
         profiler.end(second_key)
+        del tt_output
 
     first_iter_time = profiler.get(first_key)
     second_iter_time = profiler.get(second_key)
     cpu_time = profiler.get(cpu_key)
+    compile_time = first_iter_time - second_iter_time
 
-    prep_report(
-        "EfficientNet", 1, first_iter_time, second_iter_time, "b0", cpu_time
-    )
+    prep_report("EfficientNet", 1, first_iter_time, second_iter_time, "b0", cpu_time)
+
+    logger.info(f"efficientnet_b0 inference time: {second_iter_time}")
+    logger.info(f"efficientnet_b0 compile time: {compile_time}")
+
+    assert second_iter_time < expected_inference_time, "efficientnet_b0 is too slow"
+    assert (
+        compile_time < expected_compile_time
+    ), "efficientnet_b0 compile time is too slow"
