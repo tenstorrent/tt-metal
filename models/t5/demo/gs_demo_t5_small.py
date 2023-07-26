ec5bac026103fe06a9443597220c75612e74fa30
Raymond Kim <rkim@tenstorrent.com>
2023-07-26 15:52:41 +0000

    #1847: Revert "#1787: Reduce code redudancy in gs_demo_t5 filed"
    This reverts commit ec03e0b6c943ffc80431dec4f7b1230cf10a1ab9.

--- models/t5/demo/gs_demo_t5_small.py
+++ models/t5/demo/gs_demo_t5_small.py
@@ -1,8 +1,30 @@
+import tt_lib
+from loguru import logger
+from transformers import AutoTokenizer
+
 from models.t5.tt.t5_for_conditional_generation import (
     t5_small_for_conditional_generation,
 )
-from models.t5.demo.demo_utils import run_demo_t5
+from models.generation_utils import run_generate
 
 
 def test_demo_t5_small():
-    run_demo_t5(t5_small_for_conditional_generation)
+    input_sentance = "translate English to German: The house is wonderful."
+    tokenizer = AutoTokenizer.from_pretrained("t5-small", model_max_length=32)
+
+    device = tt_lib.device.CreateDevice(tt_lib.device.Arch.GRAYSKULL, 0)
+    tt_lib.device.InitializeDevice(device)
+
+    output_sentance = run_generate(
+        input_sentance,
+        tokenizer,
+        t5_small_for_conditional_generation,
+        device,
+        run_tt_model=True,
+        log=False,
+    )
+
+    logger.info(f"Input sentance: '{input_sentance}'")
+    logger.info(f"Tt output: '{output_sentance}'")
+
+    tt_lib.device.CloseDevice(device)
