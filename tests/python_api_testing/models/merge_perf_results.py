7810cfed9d224b8d29b9739a8fe2669acf20de50
Aaron Shayestehmanesh <ashayestehmanesh@e04cs06.tenstorrent.net>
2023-08-10 20:10:49 +0000

    #2115: sort perf table, include branch and hash in the report

--- tests/python_api_testing/models/merge_perf_results.py
+++ tests/python_api_testing/models/merge_perf_results.py
@@ -1,17 +1,27 @@
 from os import listdir, environ
 from os.path import isfile, join
 import time
+import git
 
 today = time.strftime("%Y_%m_%d")
 
+
 def merge_perf_files():
     mypath = "./"
-    csvfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f"{today}.csv" in f]
+    csvfiles = [
+        f for f in listdir(mypath) if isfile(join(mypath, f)) and f"{today}.csv" in f
+    ]
+
+    repo = git.Repo(search_parent_directories=True)
+
     merge_res = open(f"Models_Perf_{today}.csv", "w")
+    merge_res.write(f"branch: {repo.active_branch} \n")
+    merge_res.write(f"hash: {repo.head.object.hexsha} \n")
     merge_res.write(
         "Model, Setting, Batch, First Run (sec), Second Run (sec), Compiler Time (sec), Inference Time GS (sec), Throughput GS (Batch*inf/sec), Inference Time CPU (sec), Throughput CPU (Batch*inf/sec) \n"
-                    )
+    )
 
+    csvfiles.sort()
     for csvfile in csvfiles:
         row_name = csvfile.replace("perf_", "")
         row_name = row_name.replace(f"{today}", "")
