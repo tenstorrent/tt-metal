b8eeaba64fb4a9b2c9ae547d3386cb4a4336a7e4
Kei-Ming Kwong <kkwong@tenstorrent.com>
2023-08-14 23:10:00 +0000

    #2051: fix for header issue in multi-tile

--- tt_metal/src/ckernels/wormhole_b0/common/inc/ckernel_defs.h
+++ tt_metal/src/ckernels/wormhole_b0/common/inc/ckernel_defs.h
@@ -69,7 +69,7 @@ constexpr std::uint32_t FACE_WIDTH  = 16;
 constexpr std::uint32_t TILE_HEIGHT = 32;
 constexpr std::uint32_t TILE_WIDTH  = 32;
 constexpr std::uint32_t DATUMS_PER_ROW = 16;
-constexpr std::uint32_t TILE_HEADER_SIZE = 1;
+constexpr std::uint32_t TILE_HEADER_SIZE = 0;
 
 constexpr std::uint32_t FACE_R_DIM = FACE_HEIGHT;
 constexpr std::uint32_t FACE_C_DIM = FACE_WIDTH ;
