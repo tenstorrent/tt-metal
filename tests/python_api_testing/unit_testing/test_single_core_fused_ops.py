5c8445fafc307c04759a755e01a025f89b99d016
Austin Ho <aho@tenstorrent.com>
2023-08-18 18:06:01 +0000

    #0: Add single core unit tests for fused softmax, layernorm ops

---
+++ tests/python_api_testing/unit_testing/test_single_core_fused_ops.py
@@ -0,0 +1,68 @@
+import torch
+import pytest
+from loguru import logger
+
+import tt_lib as ttl
+
+from models.utility_functions import comp_pcc, pad_by_zero
+
+
+@pytest.mark.parametrize("shape", [[1, 1, 32, 128]])
+def test_softmax(shape):
+    # Initialize the device
+    device = ttl.device.CreateDevice(ttl.device.Arch.GRAYSKULL, 0)
+    ttl.device.InitializeDevice(device)
+
+    x = torch.randn(shape).bfloat16().float()
+
+    xt = (
+        ttl.tensor.Tensor(x, ttl.tensor.DataType.BFLOAT16)
+        .to(ttl.tensor.Layout.TILE)
+        .to(device)
+    )
+    xtt = ttl.tensor.softmax_in_place(xt)
+
+    tt_got_back = xtt.cpu().to(ttl.tensor.Layout.ROW_MAJOR).to_torch()
+
+    del xtt
+
+    ttl.device.CloseDevice(device)
+
+    pt_out = torch.nn.functional.softmax(x, dim=-1)
+
+    passing, output = comp_pcc(pt_out, tt_got_back)
+    logger.info(output)
+    assert passing
+
+
+@pytest.mark.parametrize("shape", [[1, 1, 32, 128]])
+def test_layernorm(shape):
+    # Initialize the device
+    device = ttl.device.CreateDevice(ttl.device.Arch.GRAYSKULL, 0)
+    ttl.device.InitializeDevice(device)
+
+    x = torch.randn(shape).bfloat16().float()
+    gamma = torch.randn([shape[-1]]).bfloat16().float()
+    beta = torch.randn([shape[-1]]).bfloat16().float()
+
+    xt = (
+        ttl.tensor.Tensor(x, ttl.tensor.DataType.BFLOAT16)
+        .to(ttl.tensor.Layout.TILE)
+        .to(device)
+    )
+    gammat = pad_by_zero(gamma, device)[0]
+    betat = pad_by_zero(beta, device)[0]
+
+    xtt = ttl.tensor.layernorm(xt, 1e-5, gammat, betat)
+
+    tt_got_back = xtt.cpu().to(ttl.tensor.Layout.ROW_MAJOR).to_torch()
+
+    del xtt
+
+    ttl.device.CloseDevice(device)
+
+    pt_out = torch.nn.functional.layer_norm(x, x.shape[-1:], gamma, beta, 1e-5)
+
+    passing, output = comp_pcc(pt_out, tt_got_back)
+    logger.info(output)
+    assert passing
