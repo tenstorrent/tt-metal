e1df0f67f2adde69318918fe55055c136db8e788
Mo Memarian <mmemarian@tenstorrent.com>
2023-08-23 18:06:22 +0000

    #2140: Remove default ops log locations in profiler regression script

--- tests/scripts/run_profiler_regressions.sh
+++ tests/scripts/run_profiler_regressions.sh
@@ -2,6 +2,11 @@
 
 set -eo pipefail
 
+remove_default_log_locations(){
+    rm -rf $TT_METAL_HOME/tt_metal/tools/profiler/logs/ops
+    rm -rf $TT_METAL_HOME/tt_metal/tools/profiler/logs/ops_device
+}
+
 run_profiling_test(){
     if [[ -z "$ARCH_NAME" ]]; then
       echo "Must provide ARCH_NAME in environment" 1>&2
@@ -11,6 +16,8 @@ run_profiling_test(){
     if [ "$ARCH_NAME" == "grayskull" ]; then
         make clean
         make build ENABLE_PROFILER=1
+
+        remove_default_log_locations
 
         source build/python_env/bin/activate
         export PYTHONPATH=$TT_METAL_HOME
@@ -22,6 +29,8 @@ run_profiling_test(){
 run_post_proc_test(){
     source build/python_env/bin/activate
     export PYTHONPATH=$TT_METAL_HOME
+
+    remove_default_log_locations
 
     pytest $TT_METAL_HOME/tests/tt_metal/tools/profiler/test_device_logs.py -vvv
 }
