00ca3fa1c005eb1fdc4c658f383f353b4c768d76
Raymond Kim <rkim@tenstorrent.com>
2023-08-28 22:21:55 +0000

    #2342: Add tt_eager dir prefix to wormhole_b0 post commit script directives

--- tests/scripts/run_python_api_unit_tests_wormhole_b0.sh
+++ tests/scripts/run_python_api_unit_tests_wormhole_b0.sh
@@ -9,19 +9,19 @@ fi
 
 
 # This must run in slow dispatch mode
-TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/python_api_testing/unit_testing/test_bmm_tilize_untilize.py
-TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/python_api_testing/sweep_tests/pytests/tt_dnn/test_broadcast.py
-TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/python_api_testing/sweep_tests/pytests/tt_dnn/test_composite.py
-TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/python_api_testing/sweep_tests/pytests/tt_dnn/test_eltwise_binary.py
-TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/python_api_testing/sweep_tests/pytests/tt_dnn/test_eltwise_ternary.py
-TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/python_api_testing/sweep_tests/pytests/tt_dnn/test_eltwise_unary.py
-TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/python_api_testing/sweep_tests/pytests/tt_dnn/test_outer.py
-TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/python_api_testing/sweep_tests/pytests/tt_dnn/test_pad.py
-TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/python_api_testing/sweep_tests/pytests/tt_dnn/test_permute.py
-TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/python_api_testing/sweep_tests/pytests/tt_dnn/test_reduce.py
-TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/python_api_testing/sweep_tests/pytests/tt_dnn/test_stats.py
-TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/python_api_testing/sweep_tests/pytests/tt_dnn/test_sum.py
-TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/python_api_testing/sweep_tests/pytests/tt_dnn/test_transpose.py
-TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/python_api_testing/sweep_tests/pytests/tt_dnn/test_tilize.py
-TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/python_api_testing/sweep_tests/pytests/tt_dnn/test_untilize.py
-TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/python_api_testing/sweep_tests/pytests/tt_dnn/test_tilize_with_zero_padding.py
+TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/tt_eager/python_api_testing/unit_testing/test_bmm_tilize_untilize.py
+TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_broadcast.py
+TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_composite.py
+TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_eltwise_binary.py
+TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_eltwise_ternary.py
+TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_eltwise_unary.py
+TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_outer.py
+TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_pad.py
+TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_permute.py
+TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_reduce.py
+TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_stats.py
+TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_sum.py
+TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_transpose.py
+TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_tilize.py
+TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_untilize.py
+TT_METAL_SLOW_DISPATCH_MODE=1 pytest ${TT_METAL_HOME}/tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_tilize_with_zero_padding.py
