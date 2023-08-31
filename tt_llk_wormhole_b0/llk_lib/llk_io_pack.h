48308dc7fc369ee68ccbf79c3fd9868e14b0e0da
Keivan Dabiri <kdabiri@tenstorrent.com>
2023-08-31 21:21:11 +0000

    Remove the wait-for-free-tile events from PACK_STREAM_SYNC

--- tt_llk_wormhole_b0/llk_lib/llk_io_pack.h
+++ tt_llk_wormhole_b0/llk_lib/llk_io_pack.h
@@ -249,31 +249,14 @@ inline void llk_wait_for_free_tiles(const std::int32_t operand, const std::int32
     } else {
 #if defined(PERF_DUMP)
         if (wait_for_tile_en) {
-    #if PERF_DUMP_LEVEL > 0
-        if (regfile[p_gpr_pack::PACK_STREAM_SYNC + output] > (0)) {
-            uint32_t event_id = perf::get_event_id(
-                operand, num_tiles, perf::EventType::WAIT_FOR_FREE_TILES, current_outer_loop_iter);
-            record_timestamp_64b(event_id, 9);  // Leave space for last-pack end-time, its possible upper 32b, and num_tiles
             while (regfile[p_gpr_pack::PACK_STREAM_SYNC + output] > (0))
                 ;  // Wait for prev push_tiles to complete write to register
-            record_timestamp_64b(event_id, 9);  // Leave space for last-pack end-time, its possible upper 32b, and num_tiles
-        }
-        if constexpr (!skip_sync) {
-            regfile[p_gpr_pack::PACK_STREAM_SYNC + output]++;
-            sync_regfile_write(p_gpr_pack::PACK_STREAM_SYNC + output);
-        }
+            if constexpr (!skip_sync) {
+                regfile[p_gpr_pack::PACK_STREAM_SYNC + output]++;
+                sync_regfile_write(p_gpr_pack::PACK_STREAM_SYNC + output);
+            }
 
-        stream_wait_for_free_tiles(operand, get_operand_stream_id(operand), num_tiles, num_words, 0, dram_output_no_push);
-    #else
-        while (regfile[p_gpr_pack::PACK_STREAM_SYNC + output] > (0))
-            ;  // Wait for prev push_tiles to complete write to register
-        if constexpr (!skip_sync) {
-            regfile[p_gpr_pack::PACK_STREAM_SYNC + output]++;
-            sync_regfile_write(p_gpr_pack::PACK_STREAM_SYNC + output);
-        }
-
-        stream_wait_for_free_tiles(operand, get_operand_stream_id(operand), num_tiles, num_words, 0, dram_output_no_push);
-    #endif
+            stream_wait_for_free_tiles(operand, get_operand_stream_id(operand), num_tiles, num_words, 0, dram_output_no_push);
         }
 #else
         while (regfile[p_gpr_pack::PACK_STREAM_SYNC + output] > (0))
