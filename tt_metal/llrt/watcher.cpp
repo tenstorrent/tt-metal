34b83cc8d8838bf25e443b76daf85fe6d8746828
Almeet Bhullar <abhullar@tenstorrent.com>
2023-09-12 19:54:21 +0000

    #2266: Update watcher to use metal soc desc

--- tt_metal/llrt/watcher.cpp
+++ tt_metal/llrt/watcher.cpp
@@ -384,7 +384,7 @@ static void print_stack_trace (void) {
 }
 
 static void watcher_sanitize_host_noc(const char* what,
-                                      tt_SocDescriptor soc_d,
+                                      metal_SocDescriptor soc_d,
                                       CoreCoord core,
                                       uint64_t addr,
                                       uint32_t lbytes) {
@@ -459,11 +459,11 @@ void watcher_detach(void *old) {
     watcher::devices.erase(old);
 }
 
-void watcher_sanitize_host_noc_read(tt_SocDescriptor soc_d, CoreCoord core, uint64_t addr, uint32_t lbytes) {
+void watcher_sanitize_host_noc_read(metal_SocDescriptor soc_d, CoreCoord core, uint64_t addr, uint32_t lbytes) {
     watcher_sanitize_host_noc("read", soc_d, core, addr, lbytes);
 }
 
-void watcher_sanitize_host_noc_write(tt_SocDescriptor soc_d, CoreCoord core, uint64_t addr, uint32_t lbytes) {
+void watcher_sanitize_host_noc_write(metal_SocDescriptor soc_d, CoreCoord core, uint64_t addr, uint32_t lbytes) {
     watcher_sanitize_host_noc("write", soc_d, core, addr, lbytes);
 }
 
--- tt_metal/llrt/watcher.hpp
+++ tt_metal/llrt/watcher.hpp
@@ -14,8 +14,8 @@ namespace llrt {
 void watcher_attach(void *dev, tt_cluster *cluster, int device_id, const std::function<CoreCoord ()>& get_grid_size, const std::function<CoreCoord (CoreCoord)>& worker_from_logical, const string& log_path);
 void watcher_detach(void *dev);
 
-void watcher_sanitize_host_noc_read(tt_SocDescriptor soc_d, CoreCoord core, uint64_t addr, uint32_t len);
-void watcher_sanitize_host_noc_write(tt_SocDescriptor soc_d, CoreCoord core, uint64_t addr, uint32_t len);
+void watcher_sanitize_host_noc_read(metal_SocDescriptor soc_d, CoreCoord core, uint64_t addr, uint32_t len);
+void watcher_sanitize_host_noc_write(metal_SocDescriptor soc_d, CoreCoord core, uint64_t addr, uint32_t len);
 
 } // namespace llrt
 } // namespace tt
