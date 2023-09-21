b9183b60d26d1316fc14acc4050c687c45123521
Abhinav Sarje <asarje@tenstorrent.com>
2023-09-21 16:40:40 +0000

    #2066: Update docs for reduce_tile

--- docs/source/tt_metal/apis/kernel_apis/compute/reduce_tile.rst
+++ docs/source/tt_metal/apis/kernel_apis/compute/reduce_tile.rst
@@ -1,4 +1,4 @@
 reduce_tile
 ===========
 
-.. doxygenfunction:: reduce_tile(PoolType reduce_op, ReduceDim dim, uint32_t icb, uint32_t itile, uint32_t idst, float scaler)
+.. doxygenfunction:: reduce_tile(PoolType reduce_op, ReduceDim dim, uint32_t icb0, uint32_t icb1, uint32_t itile0, uint32_t itile1, uint32_t idst)
--- tt_metal/include/compute_kernel_api/reduce.h
+++ tt_metal/include/compute_kernel_api/reduce.h
@@ -94,10 +94,11 @@ ALWI void reduce_revert_delta(uint32_t ocb = 16)
  * |----------------|-----------------------------------------------------------------|----------|------------------------------------------------|----------|
  * | reduce_func    | Enum value, specifying the type of reduce function to perform.  | uint32_t | One of ReduceFunc::Sum, ReduceFunc::Max        | True     |
  * | dim            | Dimension id, identifying the dimension to reduce in size to 1. | uint32_t | One of Reduce::R, Reduce::C, Reduce::RC        | True     |
- * | in_cb_id       | The identifier of the circular buffer (CB) containing A         | uint32_t | 0 to 31                                        | True     |
- * | in_tile_index  | The index of tile A within the first CB                         | uint32_t | Must be less than the size of the CB           | True     |
- * | dst_tile_index | The index of the tile in DST REG for the result B               | uint32_t | Must be less than the acquired size of DST REG | True     |
- * | coeff          | Scaling factor applied to each element of the resulting tile.   | float    | any float number                               | True     |
+ * | icb0           | The identifier of the circular buffer (CB) containing A         | uint32_t | 0 to 31                                        | True     |
+ * | icb1           | CB for Scaling factor applied to each element of the result.    | uint32_t | 0 to 31                                        | True     |
+ * | itile0         | The index of the tile within the first CB                       | uint32_t | Must be less than the size of the CB           | True     |
+ * | itile1         | The index of the tile within the scaling factor CB              | uint32_t | Must be less than the size of the CB           | True     |
+ * | idst           | The index of the tile in DST REG for the result                 | uint32_t | Must be less than the acquired size of DST REG | True     |
  */
 ALWI void reduce_tile(PoolType reduce_op, ReduceDim dim, uint32_t icb0, uint32_t icb1, uint32_t itile0, uint32_t itile1, uint32_t idst)
 {
