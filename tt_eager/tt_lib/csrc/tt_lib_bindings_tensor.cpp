1a9433cc267d1482f2c7e444bf06c76c67ed0a20
Bill Teng <bteng@tenstorrent.com>
2023-09-22 07:44:20 +0000

    #2148: split out bmm and custom ops

--- tt_eager/tt_lib/csrc/tt_lib_bindings_tensor.cpp
+++ tt_eager/tt_lib/csrc/tt_lib_bindings_tensor.cpp
@@ -1,6 +1,5 @@
 #include "tt_lib_bindings.hpp"
 #include "tt_dnn/op_library/eltwise_binary/eltwise_binary_op.hpp"
-#include "tt_dnn/op_library/bmm/bmm_op.hpp"
 #include "tt_dnn/op_library/conv/conv_op.hpp"
 #include "tt_dnn/op_library/conv/optimized_conv_op.hpp"
 #include "tt_dnn/op_library/softmax/softmax_op.hpp"
@@ -11,7 +10,6 @@
 #include "tt_dnn/op_library/layernorm/layernorm_op.hpp"
 #include "tt_dnn/op_library/eltwise_unary/eltwise_unary_op.hpp"
 #include "tt_dnn/op_library/auto_format.hpp"
-#include "tt_dnn/op_library/nlp_tms/nlp_tms.hpp"
 #include "tt_dnn/op_library/split/split_last_dim_two_chunks_tiled.hpp"
 #include "tt_dnn/op_library/rotate_half/rotate_half_op.hpp"
 #include "tt_dnn/op_library/rotary_embedding/rotary_embedding_op.hpp"
@@ -176,39 +174,6 @@ void TensorModule(py::module &m_tensor) {
 
     detail::bind_unary_op(m_tensor, "mean_hw", tt::tt_metal::mean_hw, R"doc(  Returns a new tensor with the variance of the input tensor ``{0}`` on H,W axes.)doc");
 
-    // *** matrix multiplication ***
-    m_tensor.def("matmul", &matmul,
-        py::arg("input").noconvert(), py::arg("other").noconvert(), py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, R"doc(
-        Perform a non-batched matrix multiplication ``arg0 x arg1`` with two tensors.
-
-        Both input tensors must have BFLOAT16 data type.
-
-        Output tensor will have BFLOAT16 data type.
-
-        .. csv-table::
-            :header: "Argument", "Description", "Data type", "Valid range", "Required"
-
-            "input", "First tensor to multiply", "Tensor", "Tensor of shape [1, 1, Y, S]", "Yes"
-            "other", "Second tensor to multiply", "Tensor", "Tensor of shape [1, 1, S, X]", "Yes"
-            "output_mem_config", "Layout of tensor in TT Accelerator device memory banks", "MemoryConfig", "Default is interleaved in DRAM", "No"
-    )doc");
-
-    m_tensor.def("bmm", &bmm,
-        py::arg("input").noconvert(), py::arg("other").noconvert(), py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, R"doc(
-        Perform a batched matmul ``arg0 x arg1`` with two tensors, where batch dims match.
-
-        Both input tensors must have BFLOAT16 data type.
-
-        Output tensor will have BFLOAT16 data type.
-
-        .. csv-table::
-            :header: "Argument", "Description", "Data type", "Valid range", "Required"
-
-            "input", "First tensor to multiply", "Tensor", "Tensor of shape [W, Z, Y, S]", "Yes"
-            "other", "Second tensor to multiply", "Tensor", "Tensor of shape [W, Z, S, X]", "Yes"
-            "output_mem_config", "Layout of tensor in TT Accelerator device memory banks", "MemoryConfig", "Default is interleaved in DRAM", "No"
-    )doc");
-
     detail::bind_unary_op_with_param(
         m_tensor, "sum", &sum,
         py::arg("dim"),
@@ -216,35 +181,6 @@ void TensorModule(py::module &m_tensor) {
         R"doc("dimension to sum along", "int", "0, 1, 2, or 3")doc"
     );
 
-    // matrix multiplication
-    m_tensor.def("bmm_tilize_untilize", &bmm_tilize_untilize, R"doc(
-        Perform a batched matmul ``A x B`` with two tensors, where batch and channel dims match.
-        This op also supports tiling tensor A and untiling the output.
-
-        +-------------------------------+-------------------------------------------------------+-----------+-------------+----------|
-        | Argument                      | Description                                           | Data type | Valid range | Required |
-        +===============================+=======================================================+===========+=============+==========+
-        | a                             | LHS matmul operand                                    | Tensor    |             | Yes      |
-        +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
-        | b                             | RHS matmul operand                                    | Tensor    |             | Yes      |
-        +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
-        | a_height_nblocks              | Number of blocks along A's height                     | uint32_t  |             | Yes      |
-        +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
-        | a_width_nblocks               | Number of blocks along A's width (= along B's height) | uint32_t  |             | Yes      |
-        +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
-        | b_width_nblocks               | Number of blocks along B's width                      | uint32_t  |             | Yes      |
-        +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
-        | a_block_height_ntiles         | Number of tiles along height of an A block            | uint32_t  |             | Yes      |
-        +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
-        | a_block_width_ntiles          | Number of tiles along width of an A block             | uint32_t  |             | Yes      |
-        +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
-        | b_block_width_ntiles          | Number of tiles along width of a B block              | uint32_t  |             | Yes      |
-        +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
-        | out_subblock_height_ntiles    | Height of subblocks on height for output              | uint32_t  |             | Yes      |
-        +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
-        | out_subblock_width_ntiles     | Number of subblocks on width for output               | uint32_t  |             | Yes      |
-        +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
-    )doc");
     m_tensor.def("conv", &conv, R"doc(
         Perform a conv ``A x B`` with two tensors
         This op tilizes tensor A and untilizes the output
@@ -333,67 +269,7 @@ void TensorModule(py::module &m_tensor) {
         +--------------+--------------------------------------------------------------------------------------------+-----------+-------------+----------+
     )doc");
 
-    // Custom BERT matmuls/bmms
-    m_tensor.def("bert_large_fused_qkv_matmul", &bert_large_fused_qkv_matmul,
-        py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
-        Perform a bert_large_fused_qkv non-batched matmul ``A x B`` with two tensors.
-    )doc");
-    m_tensor.def("bert_large_ff1_matmul", &bert_large_ff1_matmul,
-        py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("fused_activation") = std::nullopt, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
-        Perform a bert_large_ff1 non-batched matmul ``A x B`` with two tensors.
-    )doc");
-    m_tensor.def("bert_large_ff2_matmul", &bert_large_ff2_matmul,
-        py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
-        Perform a bert_large_ff2 non-batched matmul ``A x B`` with two tensors.
-    )doc");
-    m_tensor.def("bert_large_selfout_matmul", &bert_large_selfout_matmul,
-        py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
-        Perform a bert_large_selfout non-batched matmul ``A x B`` with two tensors.
-    )doc");
-    m_tensor.def("bert_large_pre_softmax_bmm", &bert_large_pre_softmax_bmm,
-        py::arg().noconvert(), py::arg().noconvert(), py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
-        Perform a bert_large_pre_softmax_bmm batched matmul ``[9, 16, 384, 64] x [9, 16, 64, 384]`` with two tensors and returns a reshaped output of [9, 1, 6144, 384].
-    )doc");
-    m_tensor.def("bert_large_post_softmax_bmm", &bert_large_post_softmax_bmm,
-        py::arg().noconvert(), py::arg().noconvert(), py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
-        Perform a bert_large_post_softmax_bmm batched matmul by reshaping tensor A to [9, 16, 384, 384] first, then returning ``[9, 16, 384, 384] x [9, 16, 384, 64]``.
-    )doc");
 
-    // Custom Falcon matmuls/bmms
-    m_tensor.def("falcon_fused_qkv_matmul", &falcon_fused_qkv_matmul,
-        py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
-        Perform a falcon_fused_qkv non-batched matmul ``A x B`` with two tensors.
-    )doc");
-    m_tensor.def("falcon_selfout_matmul", &falcon_selfout_matmul,
-        py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
-        Perform a falcon_selfout non-batched matmul ``A x B`` with two tensors.
-    )doc");
-    m_tensor.def("falcon_dense_4h_to_h_matmul", &falcon_dense_4h_to_h_matmul,
-        py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
-        Perform a falcon_dense_4h_to_h non-batched matmul ``A x B`` with two tensors.
-    )doc");
-    m_tensor.def("falcon_dense_h_to_4h_matmul", &falcon_dense_h_to_4h_matmul,
-        py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("fuse_gelu_activation") = false, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
-        Perform a falcon_dense_h_to_4h non-batched matmul ``A x B`` with two tensors. This invokes the MULTI_CORE matmul parallelization. This parallelization does not support bias option yet.
-    )doc");
-    m_tensor.def("falcon_lm_head_matmul", &falcon_lm_head_matmul,
-        py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
-        Perform a falcon_lm_head non-batched matmul ``A x B`` with two tensors. This invokes the MULTI_CORE matmul parallelization. This parallelization does not support bias option yet.
-    )doc");
-
-    // Custom Generic NLP TMs
-    // TODO: Uplift nlp_create_qkv_heads to support generic qkv num_heads and head_dim
-    // This op should support arbitrary B and S divisible by 32 on DRAM; on L1, might error out due to space
-    m_tensor.def("nlp_create_qkv_heads", &nlp_create_qkv_heads,
-        py::arg().noconvert(), py::arg("output_mem_config") = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, R"doc(
-        Shuffles [B, 1, S, 4672] fused qkv matrix into 3 heads with shapes [B, 71, S, 64], [B, 1, S, 64], and [B, 1, S, 64].
-    )doc");
-    // TODO: Uplift nlp_concat_heads to support generic num_heads and head_dim
-    // This op should support arbitrary B and S divisible by 32 on DRAM; on L1, might error out due to space
-    m_tensor.def("nlp_concat_heads", &nlp_concat_heads,
-        py::arg().noconvert(), py::arg("output_mem_config") = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, R"doc(
-        Shuffles [B, 71, S, 64] tensor into tensor with shape [B, 1, S, 4544].
-    )doc");
 
     // groupnorm
     m_tensor.def("groupnorm", &groupnorm,
@@ -591,6 +467,7 @@ void TensorModule(py::module &m_tensor) {
     detail::TensorModuleCompositeOPs( m_tensor);
     detail::TensorModulePyTensor ( m_tensor);
     detail::TensorModuleDMOPs ( m_tensor);
+    detail::TensorModuleCustomAndBMMOPs( m_tensor );
 
 }
 
---
+++ tt_eager/tt_lib/csrc/tt_lib_bindings_tensor_custom_bmm_ops.cpp
@@ -0,0 +1,134 @@
+#include "tt_lib_bindings.hpp"
+#include "tt_dnn/op_library/bmm/bmm_op.hpp"
+#include "tt_dnn/op_library/nlp_tms/nlp_tms.hpp"
+
+namespace tt::tt_metal::detail
+{
+    void TensorModuleCustomAndBMMOPs( py::module & m_tensor)
+    {
+        // matrix multiplication
+        m_tensor.def("bmm_tilize_untilize", &bmm_tilize_untilize, R"doc(
+            Perform a batched matmul ``A x B`` with two tensors, where batch and channel dims match.
+            This op also supports tiling tensor A and untiling the output.
+
+            +-------------------------------+-------------------------------------------------------+-----------+-------------+----------|
+            | Argument                      | Description                                           | Data type | Valid range | Required |
+            +===============================+=======================================================+===========+=============+==========+
+            | a                             | LHS matmul operand                                    | Tensor    |             | Yes      |
+            +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
+            | b                             | RHS matmul operand                                    | Tensor    |             | Yes      |
+            +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
+            | a_height_nblocks              | Number of blocks along A's height                     | uint32_t  |             | Yes      |
+            +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
+            | a_width_nblocks               | Number of blocks along A's width (= along B's height) | uint32_t  |             | Yes      |
+            +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
+            | b_width_nblocks               | Number of blocks along B's width                      | uint32_t  |             | Yes      |
+            +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
+            | a_block_height_ntiles         | Number of tiles along height of an A block            | uint32_t  |             | Yes      |
+            +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
+            | a_block_width_ntiles          | Number of tiles along width of an A block             | uint32_t  |             | Yes      |
+            +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
+            | b_block_width_ntiles          | Number of tiles along width of a B block              | uint32_t  |             | Yes      |
+            +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
+            | out_subblock_height_ntiles    | Height of subblocks on height for output              | uint32_t  |             | Yes      |
+            +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
+            | out_subblock_width_ntiles     | Number of subblocks on width for output               | uint32_t  |             | Yes      |
+            +-------------------------------+-------------------------------------------------------+-----------+-------------+----------+
+        )doc");
+        // *** matrix multiplication ***
+        m_tensor.def("matmul", &matmul,
+            py::arg("input").noconvert(), py::arg("other").noconvert(), py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, R"doc(
+            Perform a non-batched matrix multiplication ``arg0 x arg1`` with two tensors.
+
+            Both input tensors must have BFLOAT16 data type.
+
+            Output tensor will have BFLOAT16 data type.
+
+            .. csv-table::
+                :header: "Argument", "Description", "Data type", "Valid range", "Required"
+
+                "input", "First tensor to multiply", "Tensor", "Tensor of shape [1, 1, Y, S]", "Yes"
+                "other", "Second tensor to multiply", "Tensor", "Tensor of shape [1, 1, S, X]", "Yes"
+                "output_mem_config", "Layout of tensor in TT Accelerator device memory banks", "MemoryConfig", "Default is interleaved in DRAM", "No"
+        )doc");
+
+        m_tensor.def("bmm", &bmm,
+            py::arg("input").noconvert(), py::arg("other").noconvert(), py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, R"doc(
+            Perform a batched matmul ``arg0 x arg1`` with two tensors, where batch dims match.
+
+            Both input tensors must have BFLOAT16 data type.
+
+            Output tensor will have BFLOAT16 data type.
+
+            .. csv-table::
+                :header: "Argument", "Description", "Data type", "Valid range", "Required"
+
+                "input", "First tensor to multiply", "Tensor", "Tensor of shape [W, Z, Y, S]", "Yes"
+                "other", "Second tensor to multiply", "Tensor", "Tensor of shape [W, Z, S, X]", "Yes"
+                "output_mem_config", "Layout of tensor in TT Accelerator device memory banks", "MemoryConfig", "Default is interleaved in DRAM", "No"
+        )doc");
+
+        // Custom BERT matmuls/bmms
+        m_tensor.def("bert_large_fused_qkv_matmul", &bert_large_fused_qkv_matmul,
+            py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
+            Perform a bert_large_fused_qkv non-batched matmul ``A x B`` with two tensors.
+        )doc");
+        m_tensor.def("bert_large_ff1_matmul", &bert_large_ff1_matmul,
+            py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("fused_activation") = std::nullopt, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
+            Perform a bert_large_ff1 non-batched matmul ``A x B`` with two tensors.
+        )doc");
+        m_tensor.def("bert_large_ff2_matmul", &bert_large_ff2_matmul,
+            py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
+            Perform a bert_large_ff2 non-batched matmul ``A x B`` with two tensors.
+        )doc");
+        m_tensor.def("bert_large_selfout_matmul", &bert_large_selfout_matmul,
+            py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
+            Perform a bert_large_selfout non-batched matmul ``A x B`` with two tensors.
+        )doc");
+        m_tensor.def("bert_large_pre_softmax_bmm", &bert_large_pre_softmax_bmm,
+            py::arg().noconvert(), py::arg().noconvert(), py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
+            Perform a bert_large_pre_softmax_bmm batched matmul ``[9, 16, 384, 64] x [9, 16, 64, 384]`` with two tensors and returns a reshaped output of [9, 1, 6144, 384].
+        )doc");
+        m_tensor.def("bert_large_post_softmax_bmm", &bert_large_post_softmax_bmm,
+            py::arg().noconvert(), py::arg().noconvert(), py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
+            Perform a bert_large_post_softmax_bmm batched matmul by reshaping tensor A to [9, 16, 384, 384] first, then returning ``[9, 16, 384, 384] x [9, 16, 384, 64]``.
+        )doc");
+
+        // Custom Falcon matmuls/bmms
+        m_tensor.def("falcon_fused_qkv_matmul", &falcon_fused_qkv_matmul,
+            py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
+            Perform a falcon_fused_qkv non-batched matmul ``A x B`` with two tensors.
+        )doc");
+        m_tensor.def("falcon_selfout_matmul", &falcon_selfout_matmul,
+            py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
+            Perform a falcon_selfout non-batched matmul ``A x B`` with two tensors.
+        )doc");
+        m_tensor.def("falcon_dense_4h_to_h_matmul", &falcon_dense_4h_to_h_matmul,
+            py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
+            Perform a falcon_dense_4h_to_h non-batched matmul ``A x B`` with two tensors.
+        )doc");
+        m_tensor.def("falcon_dense_h_to_4h_matmul", &falcon_dense_h_to_4h_matmul,
+            py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("fuse_gelu_activation") = false, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
+            Perform a falcon_dense_h_to_4h non-batched matmul ``A x B`` with two tensors. This invokes the MULTI_CORE matmul parallelization. This parallelization does not support bias option yet.
+        )doc");
+        m_tensor.def("falcon_lm_head_matmul", &falcon_lm_head_matmul,
+            py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
+            Perform a falcon_lm_head non-batched matmul ``A x B`` with two tensors. This invokes the MULTI_CORE matmul parallelization. This parallelization does not support bias option yet.
+        )doc");
+
+        // Custom Generic NLP TMs
+        // TODO: Uplift nlp_create_qkv_heads to support generic qkv num_heads and head_dim
+        // This op should support arbitrary B and S divisible by 32 on DRAM; on L1, might error out due to space
+        m_tensor.def("nlp_create_qkv_heads", &nlp_create_qkv_heads,
+            py::arg().noconvert(), py::arg("output_mem_config") = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, R"doc(
+            Shuffles [B, 1, S, 4672] fused qkv matrix into 3 heads with shapes [B, 71, S, 64], [B, 1, S, 64], and [B, 1, S, 64].
+        )doc");
+        // TODO: Uplift nlp_concat_heads to support generic num_heads and head_dim
+        // This op should support arbitrary B and S divisible by 32 on DRAM; on L1, might error out due to space
+        m_tensor.def("nlp_concat_heads", &nlp_concat_heads,
+            py::arg().noconvert(), py::arg("output_mem_config") = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, R"doc(
+            Shuffles [B, 71, S, 64] tensor into tensor with shape [B, 1, S, 4544].
+        )doc");
+    }
+
+}
