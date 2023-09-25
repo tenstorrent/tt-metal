3dced86bae357f1d267da438257d391569f5e50d
Muthu Annamalai <mannamalai@tenstorrent.com>
2023-09-25 21:28:06 +0000

    #2579: fill rm works in WH B0 perhaps post DRAM bug fix by Almeet

--- tests/tt_eager/python_api_testing/unit_testing/test_fill_rm.py
+++ tests/tt_eager/python_api_testing/unit_testing/test_fill_rm.py
@@ -12,16 +12,25 @@ import torch
 
 import tt_lib
 from models.utility_functions import print_diff_argmax
-from tests.tt_eager.python_api_testing.sweep_tests.common import is_wormhole_b0, skip_for_wormhole_b0
+from tests.tt_eager.python_api_testing.sweep_tests.common import (
+    is_wormhole_b0,
+    skip_for_wormhole_b0,
+)
 
-@skip_for_wormhole_b0
+
 def test_fill_rm(device):
     N = 2
     C = 3
     H = 64
     W = 96
+
     fillH = 33
     fillW = 31
+
+    if is_wormhole_b0():
+        N, C, H, W = [1, 1, 32, 32]
+        fillH = 31
+        fillW = 31
 
     x = torch.zeros((N, C, H, W))
     xp = torch.clone(x)
