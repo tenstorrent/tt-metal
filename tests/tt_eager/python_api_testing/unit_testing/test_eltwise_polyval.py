c893819476ed04336ca0050509b80698424fe73e
banekg <barsic@tenstorrent.com>
2023-09-25 12:36:36 +0000

    #2793: Change polyval unit tests

--- tests/tt_eager/python_api_testing/unit_testing/test_eltwise_polyval.py
+++ tests/tt_eager/python_api_testing/unit_testing/test_eltwise_polyval.py
@@ -9,6 +9,7 @@ import time
 import os
 from loguru import logger
 import numpy as np
+import random
 
 f = f"{Path(__file__).parent}"
 sys.path.append(f"{f}/../../../../..")
@@ -29,35 +30,30 @@ from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_
 from tests.tt_eager.python_api_testing.sweep_tests.common import is_wormhole_b0, skip_for_wormhole_b0
 
 
+def tensor_to_device(x, device, buffer_type):
+    if buffer_type == None:
+        return x
+
+    return x.to(device, buffer_type)
+
+
 # This ref implementation is only here for debugging
 def ref_eltwise_polyval(x, coeffs):
     # polyval function
-    # y = np.polyval(np.poly1d(coeffs), x.numpy())
-    # y = torch.from_numpy(y)
+    y = np.polyval(np.poly1d(coeffs), x.numpy())
+    y = torch.from_numpy(y)
 
-    result = 0.0
-    for coeff in coeffs:
-        result = result * x + coeff
+    return y
 
-    return result
+    # result = 0.0
+    # for coeff in coeffs:
+    #     result = result * x + coeff
 
-
-def ref_eltwise_polyval_2(x, coeffs):
-    x = torch.FloatTensor(x)
-    coeffs = torch.FloatTensor(coeffs)
-
-    curVal=0
-    for curValIndex in range(len(coeffs)-1):
-        curVal=(curVal+coeffs[curValIndex])*x[0]
-    return(curVal+coeffs[len(coeffs)-1])
-
-
-# coeffs=torch.FloatTensor([1.,2.,1.,1.])
-# x=torch.FloatTensor([3.0])
-# getPolyVal(x,coeffs)
+    # return result
 
 
 def run_eltwise_polyval_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, coeffs, data_seed, device):
+    random.seed(0)
     torch.manual_seed(data_seed)
 
     # Initialize the device
@@ -71,34 +67,20 @@ def run_eltwise_polyval_tests(input_shape, dtype, dlayout, in_mem_config, out_me
     for N, C, H, W in test_dims:
         for nrepeat in range(0, 1):
             x = torch.Tensor(size=(N, C, H, W)).uniform_(-100, 100)
-            x_ref = x.detach().clone()
 
-            if dlayout == ttl.tensor.Layout.TILE:
-                x = tilize_to_list(x)
-            else:
-                x = x.reshape(-1).tolist()
+            # get referent value
+            ref_value = ref_eltwise_polyval(x, coeffs)
 
+            # calculate tt outpu
             if in_mem_config == "SYSTEM_MEMORY":
-                ttx = tensor.Tensor(
-                    x,
-                    [N, C, H, W],
-                    dtype,
-                    dlayout,
-                    dev,
-                ).cpu()
-            else:
-                ttx = tensor.Tensor(
-                    x,
-                    [N, C, H, W],
-                    dtype,
-                    dlayout,
-                    dev,
-                    in_mem_config,
-                )
+                in_mem_config = None
+
+            t0 = ttl.tensor.Tensor(x, dtype)
+            t0 = t0.to(dlayout)
+            ttx = tensor_to_device(t0, device, in_mem_config)
 
             logger.info("Running Eltwise Polyval test")
             ttz = tensor.polyval(ttx, coeffs, output_mem_config=out_mem_config)
-
             logger.info("Done")
 
             if in_mem_config != "SYSTEM_MEMORY":
@@ -110,15 +92,10 @@ def run_eltwise_polyval_tests(input_shape, dtype, dlayout, in_mem_config, out_me
             assert ttz.memory_config().buffer_type == out_mem_config.buffer_type
             logger.debug(f"ttz is on: {ttz.memory_config().buffer_type}")
 
-            t2_data = ttz.cpu().to_torch()
-
-            tt_got_back = torch.Tensor(t2_data).reshape((N, C, H, W))
+            tt_got_back = ttz.cpu().to(ttl.tensor.Layout.ROW_MAJOR).to_torch()
 
             if dlayout == ttl.tensor.Layout.TILE:
                 tt_got_back = untilize(tt_got_back)
-
-            # get referent value
-            ref_value = ref_eltwise_polyval(x_ref, coeffs)
 
             # compare tt and golden outputs
             success, pcc_value = comp_pcc(tt_got_back, ref_value)
@@ -128,9 +105,9 @@ def run_eltwise_polyval_tests(input_shape, dtype, dlayout, in_mem_config, out_me
 
 
 test_sweep_args=[
-    ((4, 12, 147, 108), ttl.tensor.DataType.BFLOAT16, ttl.tensor.Layout.ROW_MAJOR, ttl.tensor.MemoryConfig(True, ttl.tensor.BufferType.DRAM), ttl.tensor.MemoryConfig(True, ttl.tensor.BufferType.DRAM), [-48.0595703125],	1569665),
-    ((1, 6, 215, 252), ttl.tensor.DataType.BFLOAT16, ttl.tensor.Layout.ROW_MAJOR, ttl.tensor.MemoryConfig(True, ttl.tensor.BufferType.L1), ttl.tensor.MemoryConfig(True, ttl.tensor.BufferType.DRAM), [93.90789031982422],	1221114),
-    ((4, 7, 32, 96), ttl.tensor.DataType.BFLOAT16, ttl.tensor.Layout.TILE, ttl.tensor.MemoryConfig(True, ttl.tensor.BufferType.DRAM), ttl.tensor.MemoryConfig(True, ttl.tensor.BufferType.DRAM), [-84.57501220703125, -41.85026931762695],	17155532),
+    ((4, 12, 147, 108), ttl.tensor.DataType.BFLOAT16, ttl.tensor.Layout.ROW_MAJOR, ttl.tensor.MemoryConfig(True, ttl.tensor.BufferType.DRAM), ttl.tensor.MemoryConfig(True, ttl.tensor.BufferType.DRAM), [-48.0595703125], 1569665),
+    ((1, 6, 215, 252), ttl.tensor.DataType.BFLOAT16, ttl.tensor.Layout.ROW_MAJOR, ttl.tensor.MemoryConfig(True, ttl.tensor.BufferType.L1), ttl.tensor.MemoryConfig(True, ttl.tensor.BufferType.DRAM), [93.90789031982422], 1221114),
+    ((4, 7, 32, 96), ttl.tensor.DataType.BFLOAT16, ttl.tensor.Layout.TILE, ttl.tensor.MemoryConfig(True, ttl.tensor.BufferType.DRAM), ttl.tensor.MemoryConfig(True, ttl.tensor.BufferType.DRAM), [-26.537084579467773, -98.70405578613281, -3.4798622131347656, -5.008804798126221, 92.45339965820312, 51.021541595458984, 1.1626243591308594], 17155532),
 ]
 
 @skip_for_wormhole_b0
