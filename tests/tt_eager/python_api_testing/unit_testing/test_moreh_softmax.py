1c39afba34c8485767b15b26ee1bee22a0f03850
hschoi <hschoi4448@gmail.com>
2023-09-27 07:54:21 +0000

    #2858: apply mask to  softmax_backward

--- tests/tt_eager/python_api_testing/unit_testing/test_moreh_softmax.py
+++ tests/tt_eager/python_api_testing/unit_testing/test_moreh_softmax.py
@@ -193,3 +193,48 @@ def test_softmax_backward_for_dim_hw(shape_dim, device):
     tt_dev = tt_npu.cpu().to(ttl.tensor.Layout.ROW_MAJOR).to_torch().to(torch.bfloat16)
 
     assert torch.allclose(x.grad, tt_dev, rtol = 0.07, atol = 0.01)
+
+@pytest.mark.parametrize(
+    "shape_dim",
+    (
+        ((1, 1, 10, 15), 3), # single tile
+        ((1, 1, 10, 32 * 2 + 10), 3), # mutiple tile with dim
+        ((4, 3, 15, 10), 2), # single tile
+        ((15, 9, 32 * 2 + 10, 32), 2), # mutiple tile with dim
+    ),
+)
+def test_softmax_backward_not_multiple_of_32_for_dim_hw(shape_dim, device):
+    shape, dim = shape_dim
+    torch.manual_seed(0)
+
+    N = shape[0]
+    C = shape[1]
+    H = shape[2]
+    W = shape[3]
+
+    x = torch.randint(low=0, high=4, size=(N * C * H * W,)).reshape((N, C, H, W)).to(torch.bfloat16).requires_grad_(True)
+
+    y = torch.softmax(x, dim)
+    dev_y = ttl.tensor.Tensor(
+        y.reshape(-1).tolist(),
+        y.shape,
+        ttl.tensor.DataType.BFLOAT16,
+        ttl.tensor.Layout.ROW_MAJOR,
+    ).pad_to_tile(float('10')).to(ttl.tensor.Layout.TILE).to(device)
+
+    dy = torch.randint(low=0, high=4, size=(N * C * H * W,)).reshape((N, C, H, W)).to(torch.bfloat16)
+    dev_dy = ttl.tensor.Tensor(
+        dy.reshape(-1).tolist(),
+        dy.shape,
+        ttl.tensor.DataType.BFLOAT16,
+        ttl.tensor.Layout.ROW_MAJOR,
+    ).pad_to_tile(float('20')).to(ttl.tensor.Layout.TILE).to(device)
+
+    y.backward(dy)
+    tt_npu = ttl.operations.primary.moreh_softmax_backward(dev_y, dev_dy, dim)
+    tt_npu = tt_npu.cpu().to(ttl.tensor.Layout.ROW_MAJOR).unpad_from_tile((N, C, H, W))
+
+    assert tt_npu.shape() == list(x.grad.shape)
+    tt_dev = tt_npu.to_torch().to(torch.bfloat16)
+
+    assert torch.allclose(x.grad, tt_dev, rtol = 0.07, atol = 0.01)
