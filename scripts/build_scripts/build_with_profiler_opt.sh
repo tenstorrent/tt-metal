377d08e8baf6f3bb4d080d3f6d13df9c7ea01210
Muthu Annamalai <mannamalai@tenstorrent.com>
2023-09-29 22:38:07 +0000

    #3121: microkernel ops profile + postprocessing tool      : add option --ops to profile_unary.py      : usage info, move to tests/scripts/ folder      : fix postproc profiler for WH B0      : add memory option for DRAM or L1      : Add support for automatically dumping profiler files      : add options for shape, ops for profile_unary.py
    #0: update to use tt_lib.tensor.full instead of fallback
    #0: profiler for unary op
    #0: add 5 and 6 as kernel markers for 1T 1C experiment on GS
         - generate a post-process script
         - place holder

--- scripts/build_scripts/build_with_profiler_opt.sh
+++ scripts/build_scripts/build_with_profiler_opt.sh
@@ -5,6 +5,8 @@ set -eo pipefail
 remove_default_log_locations(){
     rm -rf $TT_METAL_HOME/tt_metal/tools/profiler/logs/ops
     rm -rf $TT_METAL_HOME/tt_metal/tools/profiler/logs/ops_device
+    rm -rf $TT_METAL_HOME/profiler_stats
+    rm -rf $TT_METAL_HOME/profiler_data.csv
 }
 
 if [[ -z "$TT_METAL_HOME" ]]; then
@@ -18,8 +20,18 @@ if [[ -z "$ARCH_NAME" ]]; then
     echo "Must provide ARCH_NAME in environment" 1>&2
     exit 1
 fi
+# Check if profile_unary.py exists
+if [[ ! -f "tests/scripts/profile_unary.py" ]]; then
+    echo "profile_unary.py does not exist in the TT_METAL_HOME directory." 1>&2
+    exit 1
+fi
+
+# Check if postproc_unary.py exists
+if [[ ! -f "tests/scripts/postproc_unary.py" ]]; then
+    echo "postproc_unary.py does not exist in the TT_METAL_HOME directory." 1>&2
+    exit 1
+fi
 
 remove_default_log_locations
 make clean
 make build ENABLE_PROFILER=1
-make programming_examples
