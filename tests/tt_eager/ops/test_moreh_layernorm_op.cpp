2d4506a347d9b97d983a53455431019222210a9c
seunghwan100 <seunghwan100@github.com>
2023-10-02 06:31:25 +0000

    #2794: Implement moreh_layernorm

---
+++ tests/tt_eager/ops/test_moreh_layernorm_op.cpp
@@ -0,0 +1,115 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <limits>
+#include <string>
+#include <tt_numpy/functions.hpp>
+#include <vector>
+
+#include "tt_eager/tensor/tensor.hpp"
+#include "tt_eager/tt_dnn/op_library/moreh_layernorm/moreh_layernorm_op.hpp"
+#include "tt_metal/host_api.hpp"
+
+using namespace tt;
+using namespace tt::tt_metal;
+
+int main(int argc, char **argv) {
+    bool pass = true;
+
+    const std::vector<std::string> run_types{"autoformat", "primary"};
+    const std::vector<std::vector<uint32_t>> normalized_dims_vec{
+        {3},
+        {2, 3},
+        {1, 2, 3},
+        {0, 1, 2, 3},
+    };
+    const std::vector<std::vector<uint32_t>> input_shapes{
+        {1, 1, TILE_HEIGHT, TILE_WIDTH},
+        {2, 2, 2 * TILE_HEIGHT - 15, 2 * TILE_WIDTH - 17},
+    };
+
+    for (const auto &input_shape : input_shapes) {
+        for (const auto &normalized_dims : normalized_dims_vec) {
+            for (const auto run_type : run_types) {
+                try {
+                    ////////////////////////////////////////////////////////////////////////////
+                    //                      Device Setup
+                    ////////////////////////////////////////////////////////////////////////////
+                    int device_id = 0;
+                    tt_metal::Device *device = tt_metal::CreateDevice(device_id);
+
+                    ////////////////////////////////////////////////////////////////////////////
+                    //                      Parameters Setup
+                    ////////////////////////////////////////////////////////////////////////////
+                    const bool is_autoformat = run_type == "autoformat";
+                    const auto nan = std::numeric_limits<float>::quiet_NaN();
+
+                    // input
+                    auto input_data = tt::numpy::random::random(input_shape);
+                    auto input = is_autoformat ? input_data.pad_to_tile(nan).to(device)
+                                               : input_data.pad_to_tile(nan).to(Layout::TILE).to(device);
+
+                    log_info(
+                        LogTest,
+                        "N: {}, C: {}, H: {}, W: {}.",
+                        input_shape[0],
+                        input_shape[1],
+                        input_shape[2],
+                        input_shape[3]);
+
+                    log_info(LogTest, "normalized_dims: [{}]", fmt::join(normalized_dims, ", "));
+
+                    // gamma_beta_shape
+                    Shape gamma_beta_shape = {1, 1, 1, 1};
+                    for (const auto dim : normalized_dims) {
+                        gamma_beta_shape[dim] = input_shape[dim];
+                    }
+
+                    // gamma
+                    auto gamma_data = tt::numpy::zeros(gamma_beta_shape);
+                    auto gamma = is_autoformat ? gamma_data.pad_to_tile(nan).to(device)
+                                               : gamma_data.pad_to_tile(nan).to(Layout::TILE).to(device);
+
+                    // beta
+                    auto beta_data = tt::numpy::ones(gamma_beta_shape);
+                    auto beta = is_autoformat ? beta_data.pad_to_tile(nan).to(device)
+                                              : beta_data.pad_to_tile(nan).to(Layout::TILE).to(device);
+
+                    // Validation
+                    auto expected = tt::numpy::ones(input_shape);
+
+                    // y * 0 + 1
+                    log_info(LogTest, "{} test start.", run_type);
+                    auto actual_npu =
+                        is_autoformat
+                            ? tt_metal::moreh_layernorm(input, 1e-5f, normalized_dims, gamma, beta)
+                            : operations::primary::moreh_layernorm(input, 1e-5f, normalized_dims, gamma, beta);
+                    auto actual = actual_npu.cpu().to(Layout::ROW_MAJOR).unpad_from_tile(input_shape);
+
+                    pass &= CloseDevice(device);
+                    pass &= tt::numpy::allclose<bfloat16>(actual, expected);
+                    if (pass) {
+                        log_info(LogTest, "{} test passed.", run_type);
+                    } else {
+                        log_fatal(LogTest, "{} test failed.", run_type);
+                    }
+                } catch (const std::exception &e) {
+                    pass = false;
+                    log_error(LogTest, "{}", e.what());
+                    log_error(LogTest, "System error message: {}", std::strerror(errno));
+                }
+            }  // run_type
+        }      // normalized_dims
+    }          // input_shape
+
+    if (pass) {
+        log_info(LogTest, "Test Passed");
+    } else {
+        log_fatal(LogTest, "Test Failed");
+    }
+
+    TT_ASSERT(pass);
+
+    return 0;
+}
---
+++ tt_eager/tt_dnn/op_library/moreh_layernorm/kernels/moreh_layernorm_large.cpp
@@ -0,0 +1,429 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <cstdint>
+
+#include "compute_kernel_api/bcast.h"
+#include "compute_kernel_api/eltwise_binary.h"
+#include "compute_kernel_api/layernorm.h"
+#include "compute_kernel_api/mask.h"
+#include "compute_kernel_api/reduce.h"
+#include "compute_kernel_api/tile_move_copy.h"
+#include "debug_print.h"
+
+ALWI void ACQ() { acquire_dst(tt::DstMode::Half); }
+ALWI void REL() { release_dst(tt::DstMode::Half); }
+
+inline bool need_to_do_mask_h(uint32_t w_idx, uint32_t origin_num_h_tiles, uint32_t origin_num_w_tiles) {
+    return ((w_idx / origin_num_w_tiles) + 1) % origin_num_h_tiles == 0;
+}
+
+namespace NAMESPACE {
+void MAIN {
+    constexpr uint32_t num_rows_per_core = get_compile_time_arg_val(0);
+    constexpr uint32_t origin_H = get_compile_time_arg_val(1);
+    constexpr uint32_t origin_W = get_compile_time_arg_val(2);
+    constexpr uint32_t Wt = get_compile_time_arg_val(3);
+    constexpr uint32_t block_size = get_compile_time_arg_val(4);
+    constexpr uint32_t do_gamma = get_compile_time_arg_val(5);
+    constexpr uint32_t do_beta = get_compile_time_arg_val(6);
+    constexpr uint32_t is_lastdim_layernorm = get_compile_time_arg_val(7);
+
+    binary_op_init_common(tt::CB::c_in0, tt::CB::c_in0);
+
+    constexpr auto cb_x = tt::CB::c_in0;       // input
+    constexpr auto cb_scaler = tt::CB::c_in1;  // scaler for reduction
+    constexpr auto cb_eps = tt::CB::c_in2;     // epsilon
+    constexpr auto cb_gamma = tt::CB::c_in3;   // gamma
+    constexpr auto cb_beta = tt::CB::c_in4;    // beta
+    constexpr auto cb_mask_h = tt::CB::c_in5;  // mask_h
+    constexpr auto cb_mask_w = tt::CB::c_in6;  // mask_w
+
+    constexpr auto cb_out = tt::CB::c_out0;  // output
+
+    constexpr auto cb_ex = tt::CB::c_intermed0;          // E[x]
+    constexpr auto cb_xmm = tt::CB::c_intermed1;         // x - E[x]
+    constexpr auto cb_xmm2 = tt::CB::c_intermed2;        // (x - E[x])^2
+    constexpr auto cb_xmm2sum = tt::CB::c_intermed3;     // Sum[(x - E[x])^2]
+    constexpr auto cb_var = tt::CB::c_intermed4;         // E[(x - E[x])^2] = Var[x]
+    constexpr auto cb_ex2pe = tt::CB::c_intermed5;       // 1.0/(sqrt(Var[x] + eps))
+    constexpr auto cb_gamma_beta = tt::CB::c_intermed6;  // p * gamm + beta
+    constexpr auto cb_xsum = tt::CB::c_intermed7;        // Sum[x]
+
+    constexpr uint32_t onetile = 1;
+
+    cb_wait_front(cb_scaler, onetile);  // comes from the reader
+    cb_wait_front(cb_eps, onetile);     // comes from the reader
+
+    constexpr bool do_mask_h = (origin_H % 32) != 0 && !is_lastdim_layernorm;
+    constexpr bool do_mask_w = (origin_W % 32) != 0;
+
+    if (do_mask_h) {
+        cb_wait_front(cb_mask_h, onetile);
+    }
+    if (do_mask_w) {
+        cb_wait_front(cb_mask_w, onetile);
+    }
+
+    constexpr uint32_t NCHt = num_rows_per_core;
+    constexpr uint32_t dst0 = 0;
+    constexpr uint32_t dst1 = 1;
+    constexpr uint32_t first_tile = 0;
+
+    constexpr uint32_t origin_Ht = (origin_H + 32 - 1) / 32;
+    constexpr uint32_t origin_Wt = (origin_W + 32 - 1) / 32;
+
+    for (uint32_t ncht = 0; ncht < NCHt; ncht++) {
+        /*
+         * Sum[x]
+         * cb_xsum
+         */
+        for (uint32_t wt = 0; wt < Wt; wt += block_size) {
+            cb_wait_front(cb_x, block_size);
+            for (uint32_t j = 0; j < block_size; j++) {
+                const uint32_t w_idx = wt + j;
+                if (w_idx == 0) {
+                    ACQ();
+                    cb_reserve_back(cb_xsum, onetile);
+
+                    copy_tile_init();
+                    copy_tile(cb_x, first_tile, dst0);  // input
+                    pack_tile(dst0, cb_xsum);
+
+                    if (do_mask_h && need_to_do_mask_h(w_idx, origin_Ht, origin_Wt)) {
+                        copy_tile(cb_mask_h, first_tile, dst1);  // mask_h
+                        mask_tile_init();
+                        mask_tile(dst0, dst1);
+                    }
+
+                    if (do_mask_w && ((w_idx + 1) % origin_Wt == 0)) {
+                        copy_tile(cb_mask_w, first_tile, dst1);  // mask_w
+                        mask_tile_init();
+                        mask_tile(dst0, dst1);
+                    }
+
+                    pack_tile(dst0, cb_xsum);
+                    cb_push_back(cb_xsum, onetile);
+                    REL();
+                } else {
+                    ACQ();
+                    // I use cb_ex temporarily.
+                    constexpr auto cb_tmp = cb_ex;
+                    cb_reserve_back(cb_tmp, onetile);
+
+                    copy_tile_init();
+                    copy_tile(cb_x, j, j);  // input
+
+                    const uint32_t mask_dst = j < 15 ? j + 1 : 0;
+
+                    if (do_mask_h && need_to_do_mask_h(w_idx, origin_Ht, origin_Wt)) {
+                        copy_tile(cb_mask_h, first_tile, mask_dst);  // mask_h
+                        mask_tile_init();
+                        mask_tile(j, mask_dst);
+                    }
+
+                    if (do_mask_w && ((w_idx + 1) % origin_Wt == 0)) {
+                        copy_tile(cb_mask_w, first_tile, mask_dst);  // mask_w
+                        mask_tile_init();
+                        mask_tile(j, mask_dst);
+                    }
+
+                    pack_tile(j, cb_tmp);
+                    cb_push_back(cb_tmp, onetile);
+                    REL();
+
+                    ACQ();
+                    cb_wait_front(cb_tmp, onetile);
+                    cb_wait_front(cb_xsum, onetile);
+                    cb_reserve_back(cb_xsum, onetile);
+
+                    add_tiles_init();
+                    add_tiles(cb_xsum, cb_tmp, first_tile, first_tile, dst0);
+                    pack_tile(dst0, cb_xsum);
+
+                    cb_pop_front(cb_tmp, onetile);
+                    cb_pop_front(cb_xsum, onetile);
+                    cb_push_back(cb_xsum, onetile);
+                    REL();
+                }
+            }  // block_size loop
+            cb_pop_front(cb_x, block_size);
+        }  // Wt loop
+
+        /*
+         * E[x]
+         * cb_ex
+         */
+        ACQ();
+        cb_wait_front(cb_xsum, onetile);
+        cb_reserve_back(cb_ex, onetile);
+        reduce_init_delta<false>(REDUCE_OP, REDUCE_DIM);
+
+        reduce_tile(REDUCE_OP, REDUCE_DIM, cb_xsum, cb_scaler, first_tile, first_tile, dst0);
+        pack_tile(dst0, cb_ex);
+
+        reduce_revert_delta();
+        cb_pop_front(cb_xsum, onetile);
+        cb_push_back(cb_ex, onetile);
+        REL();
+
+        /*
+         * x - E[x]
+         * xmm
+         */
+        cb_wait_front(cb_ex, onetile);
+        for (uint32_t wt = 0; wt < Wt; wt += block_size) {
+            cb_wait_front(cb_x, block_size);
+            cb_reserve_back(cb_xmm, block_size);
+            for (uint32_t j = 0; j < block_size; j++) {
+                ACQ();
+                if (is_lastdim_layernorm) {
+                    sub_bcast_cols_init_short();
+                    sub_tiles_bcast_cols(cb_x, cb_ex, j, first_tile, j);
+                } else {
+                    sub_tiles_bcast_scalar_init_short();
+                    sub_tiles_bcast_scalar(cb_x, cb_ex, j, first_tile, j);
+                }
+                pack_tile(j, cb_xmm);
+                REL();
+            }  // block_size loop
+            cb_pop_front(cb_x, block_size);
+            cb_push_back(cb_xmm, block_size);
+
+            /*
+             * mask xmm
+             */
+            if (do_mask_h || do_mask_w) {
+                cb_wait_front(cb_xmm, block_size);
+                cb_reserve_back(cb_xmm, block_size);
+                for (uint32_t j = 0; j < block_size; j++) {
+                    ACQ();
+
+                    copy_tile_init();
+                    copy_tile(cb_xmm, j, j);  // xmm
+
+                    const uint32_t mask_dst = j < 15 ? j + 1 : 0;
+                    const uint32_t w_idx = wt + j;
+
+                    if (do_mask_h && need_to_do_mask_h(w_idx, origin_Ht, origin_Wt)) {
+                        copy_tile(cb_mask_h, first_tile, mask_dst);  // mask_h
+                        mask_tile_init();
+                        mask_tile(j, mask_dst);
+                    }
+
+                    if (do_mask_w && (w_idx + 1) % origin_Wt == 0) {
+                        copy_tile(cb_mask_w, first_tile, mask_dst);  // mask_w
+                        mask_tile_init();
+                        mask_tile(j, mask_dst);
+                    }
+
+                    pack_tile(j, cb_xmm);
+                    REL();
+                }  // block_size loop
+                cb_pop_front(cb_xmm, block_size);
+                cb_push_back(cb_xmm, block_size);
+            }
+
+            /*
+             * (x - E[x])^2
+             * cb_xmm2
+             */
+            cb_wait_front(cb_xmm, block_size);
+            cb_reserve_back(cb_xmm2, block_size);
+            for (uint32_t j = 0; j < block_size; j++) {
+                ACQ();
+                mul_tiles_init();
+
+                mul_tiles(cb_xmm, cb_xmm, j, j, j);
+                pack_tile(j, cb_xmm2);
+
+                REL();
+            }  // block_size loop
+            cb_pop_front(cb_xmm, block_size);
+            cb_push_back(cb_xmm2, block_size);
+
+            /*
+             * Sum[(x-E[x])^2]
+             * cb_xmm2sum
+             */
+            cb_wait_front(cb_xmm2, block_size);
+            for (uint32_t j = 0; j < block_size; j++) {
+                if (wt == 0 && j == 0) {
+                    ACQ();
+                    cb_reserve_back(cb_xmm2sum, onetile);
+
+                    copy_tile_init();
+                    copy_tile(cb_xmm2, first_tile, dst0);
+                    pack_tile(dst0, cb_xmm2sum);
+
+                    cb_push_back(cb_xmm2sum, onetile);
+                    REL();
+                } else {
+                    ACQ();
+                    cb_wait_front(cb_xmm2sum, onetile);
+                    cb_reserve_back(cb_xmm2sum, onetile);
+
+                    add_tiles_init();
+                    add_tiles(cb_xmm2sum, cb_xmm2, first_tile, j, dst0);
+                    pack_tile(dst0, cb_xmm2sum);
+
+                    cb_pop_front(cb_xmm2sum, onetile);
+                    cb_push_back(cb_xmm2sum, onetile);
+                    REL();
+                }
+            }  // block_size loop
+            cb_pop_front(cb_xmm2, block_size);
+        }  // Wt loop
+        // Do not pop cb_ex here, we need it later.
+
+        /*
+         * E[(x-E[x])^2 = Var[x]
+         * cb_var
+         */
+        ACQ();
+        cb_reserve_back(cb_var, onetile);
+        cb_wait_front(cb_xmm2sum, onetile);
+        reduce_init_delta<false>(REDUCE_OP, REDUCE_DIM);
+
+        reduce_tile(REDUCE_OP, REDUCE_DIM, cb_xmm2sum, cb_scaler, first_tile, first_tile, dst0);
+        pack_tile(dst0, cb_var);
+
+        reduce_revert_delta();
+        cb_push_back(cb_var, onetile);
+        cb_pop_front(cb_xmm2sum, onetile);
+        REL();
+
+        /*
+         * 1.0/(sqrt(E[(x-E[x])^2] + eps))
+         * cb_ex2pe
+         */
+        cb_wait_front(cb_var, onetile);
+        cb_reserve_back(cb_ex2pe, onetile);
+
+        ACQ();
+        add_tiles_init();
+        add_tiles(cb_var, cb_eps, first_tile, first_tile, dst0);
+
+        sqrt_tile_init();
+        sqrt_tile(dst0);
+
+        recip_tile_init();
+        recip_tile(dst0);
+
+        pack_tile(dst0, cb_ex2pe);
+
+        cb_pop_front(cb_var, onetile);
+        cb_push_back(cb_ex2pe, onetile);
+        REL();
+
+        /*
+         * (x - E[x]) * (1.0/(sqrt(E[(x-E[x])^2] + eps)))
+         * (x - E[x]) * (1.0/(sqrt(E[(x-E[x])^2] + eps))) * gamma + beta
+         * cb_out
+         */
+        cb_wait_front(cb_ex2pe, onetile);
+        constexpr auto cb_reuse = cb_xmm;
+        for (uint32_t wt = 0; wt < Wt; wt += block_size) {
+            /*
+             * x - E[x]
+             * cb_reuse(==cb_xmm)
+             */
+            cb_wait_front(cb_x, block_size);
+            cb_reserve_back(cb_reuse, block_size);
+            for (uint32_t j = 0; j < block_size; j++) {
+                ACQ();
+                if (is_lastdim_layernorm) {
+                    sub_bcast_cols_init_short();
+                    sub_tiles_bcast_cols(cb_x, cb_ex, j, first_tile, j);
+                } else {
+                    sub_tiles_bcast_scalar_init_short();
+                    sub_tiles_bcast_scalar(cb_x, cb_ex, j, first_tile, j);
+                }
+                pack_tile(j, cb_reuse);
+                REL();
+            }  // block_size loop
+            cb_pop_front(cb_x, block_size);
+            cb_push_back(cb_reuse, block_size);
+
+            /*
+             * (x - E[x]) * 1.0/sqrt(Var[x] + eps)
+             * cb_gamma_beta_or_out
+             */
+            constexpr auto cb_gamma_beta_or_out = (do_gamma | do_beta) ? cb_gamma_beta : cb_out;
+            cb_wait_front(cb_reuse, block_size);
+            cb_reserve_back(cb_gamma_beta_or_out, block_size);
+            for (uint32_t j = 0; j < block_size; j++) {
+                ACQ();
+                if (is_lastdim_layernorm) {
+                    mul_bcast_cols_init_short();
+                    mul_tiles_bcast_cols(cb_reuse, cb_ex2pe, j, first_tile, j);
+                } else {
+                    mul_tiles_bcast_scalar_init_short();
+                    mul_tiles_bcast_scalar(cb_reuse, cb_ex2pe, j, first_tile, j);
+                }
+                pack_tile(j, cb_gamma_beta_or_out);
+                REL();
+            }  // block_size loop
+            cb_pop_front(cb_reuse, block_size);
+            cb_push_back(cb_gamma_beta_or_out, block_size);
+
+            // * gamma
+            if (do_gamma) {
+                uint32_t cb_outg = do_beta ? cb_gamma_beta : cb_out;
+                cb_wait_front(cb_gamma_beta_or_out, block_size);
+                cb_wait_front(cb_gamma, block_size);
+                cb_reserve_back(cb_outg, block_size);
+                for (uint32_t j = 0; j < block_size; j++) {
+                    ACQ();
+                    if (is_lastdim_layernorm) {
+                        mul_bcast_rows_init_short();
+                        mul_tiles_bcast_rows(cb_gamma_beta_or_out, cb_gamma, j, j, j);
+                    } else {
+                        mul_tiles_init();
+                        mul_tiles(cb_gamma_beta_or_out, cb_gamma, j, j, j);
+                    }
+                    pack_tile(j, cb_outg);
+                    REL();
+                }  // block_size loop
+                cb_pop_front(cb_gamma_beta_or_out, block_size);
+                cb_pop_front(cb_gamma, block_size);
+                cb_push_back(cb_outg, block_size);
+            }
+
+            // + beta
+            if (do_beta) {
+                cb_wait_front(cb_gamma_beta, block_size);
+                cb_wait_front(cb_beta, block_size);
+                cb_reserve_back(cb_out, block_size);
+                for (uint32_t j = 0; j < block_size; j++) {
+                    ACQ();
+                    if (is_lastdim_layernorm) {
+                        add_bcast_rows_init_short();
+                        add_tiles_bcast_rows(cb_gamma_beta, cb_beta, j, j, j);
+                    } else {
+                        add_tiles_init();
+                        add_tiles(cb_gamma_beta, cb_beta, j, j, j);
+                    }
+                    pack_tile(j, cb_out);
+                    REL();
+                }  // block_size loop
+                cb_pop_front(cb_gamma_beta, block_size);
+                cb_pop_front(cb_beta, block_size);
+                cb_push_back(cb_out, block_size);
+            }
+        }  // Wt loop
+        cb_pop_front(cb_ex2pe, onetile);
+        cb_pop_front(cb_ex, onetile);
+    }  // NCHt loop
+    cb_pop_front(cb_scaler, onetile);
+    cb_pop_front(cb_eps, onetile);
+
+    if (do_mask_h) {
+        cb_pop_front(cb_mask_h, onetile);
+    }
+    if (do_mask_w) {
+        cb_pop_front(cb_mask_w, onetile);
+    }
+}  // void MAIN
+}  // namespace NAMESPACE
