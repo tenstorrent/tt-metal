7a32a4338bb8db789b80679d00e6fd90a1b36cde
“Nenad <npetrovic@tenstorrent.com>
2023-10-03 10:10:25 +0000

    #2762: Fixed reglu test to pass

--- tests/tt_eager/python_api_testing/sweep_tests/tt_lib_ops.py
+++ tests/tt_eager/python_api_testing/sweep_tests/tt_lib_ops.py
@@ -1879,7 +1879,7 @@ def activation_geglu(x, *args, device, dtype, layout, input_mem_config, output_m
 def activation_reglu(
     x, *args, device, dtype, layout, input_mem_config, output_mem_config, **kwargs
 ):
-    dim = kwargs.get("dim", -2)
+    dim = kwargs.get("dim", -1)
     t0 = setup_tt_tensor(x, device, layout[0], input_mem_config[0], dtype[0])
     t1 = ttl.tensor.reglu(t0, dim, output_mem_config=output_mem_config)
 
