a8eb0b2e7b6ffbcd427d06457e1ee49737aa94df
Paul Keller <pkeller@tenstorrent.com>
2023-10-06 22:31:21 +0000

    #2220: Allow watcher intervals in ms

--- tt_metal/llrt/rtoptions.cpp
+++ tt_metal/llrt/rtoptions.cpp
@@ -6,6 +6,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <cstring>
+
 #include "rtoptions.hpp"
 
 namespace tt {
@@ -17,14 +19,17 @@ namespace llrt {
 // env state
 RunTimeOptions OptionsG;
 
-RunTimeOptions::RunTimeOptions() : watcher_interval(0), watcher_dump_all(false) {
+RunTimeOptions::RunTimeOptions() : watcher_interval_ms(0), watcher_dump_all(false) {
 
     const char *watcher_enable_str = getenv("TT_METAL_WATCHER");
     if (watcher_enable_str != nullptr) {
-        int sleep_secs = 0;
-        sscanf(watcher_enable_str, "%d", &sleep_secs);
-        constexpr int watcher_default_sleep_secs = 2 * 60;
-        watcher_interval = (sleep_secs == 0) ? watcher_default_sleep_secs : sleep_secs;
+        int sleep_val = 0;
+        sscanf(watcher_enable_str, "%d", &sleep_val);
+        if (strstr(watcher_enable_str, "ms") == nullptr) {
+            sleep_val *= 1000;
+        }
+        constexpr int watcher_default_sleep_msecs = 2 * 60 * 1000;
+        watcher_interval_ms = (sleep_val == 0) ? watcher_default_sleep_msecs : sleep_val;
     }
 
     const char *watcher_dump_all_str = getenv("TT_METAL_WATCHER_DUMP_ALL");
