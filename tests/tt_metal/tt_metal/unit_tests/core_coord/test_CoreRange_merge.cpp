90d737aba08747a3cc04edabc4b87235d0167e9c
Bill Teng <bteng@tenstorrent.com>
2023-10-07 00:02:47 +0000

    #0: add adjacent CoreRange merge functionality

--- tests/tt_metal/tt_metal/unit_tests/core_coord/test_CoreRange_merge.cpp
+++ tests/tt_metal/tt_metal/unit_tests/core_coord/test_CoreRange_merge.cpp
@@ -16,6 +16,13 @@ TEST_F(CoreCoordHarness, TestCoreRangeMerge)
     EXPECT_EQ ( this->cr4.merge(this->cr5).value(), this->cr6 );
     EXPECT_EQ ( this->cr4.merge(this->cr6).value(), this->cr6 );
     EXPECT_EQ ( this->cr4.merge(this->cr4).value(), this->cr4 );
+    EXPECT_EQ ( this->cr1.merge(this->cr9).value(), this->cr12 );
+    EXPECT_EQ ( this->cr1.merge(this->cr11).value(), this->cr12 );
+    EXPECT_EQ ( this->cr1.merge(this->cr10).value(), this->cr13 );
+    EXPECT_EQ ( this->cr1.merge(this->cr10).value(), this->cr13 );
+    EXPECT_EQ ( this->single_core.merge(this->single_core1).value(), this->cr14 );
+    EXPECT_EQ ( this->single_core1.merge(this->single_core2).value(), this->cr15 );
+
 }
 
 TEST_F(CoreCoordHarness, TestCoreRangeNotMergeable){
@@ -23,7 +30,8 @@ TEST_F(CoreCoordHarness, TestCoreRangeNotMergeable){
     EXPECT_FALSE ( this->cr2.merge(this->cr3));
     EXPECT_FALSE ( this->cr1.merge(this->cr6));
     EXPECT_FALSE ( this->sc1.merge(this->sc3));
-    EXPECT_FALSE ( this->sc1.merge(this->sc2) );
+    EXPECT_FALSE ( this->cr1.merge(this->cr5));
+    EXPECT_FALSE ( this->cr1.merge(this->cr7));
 }
 
 }
