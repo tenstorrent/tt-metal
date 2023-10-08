2cf19285ae0f6badec68dac2c820ad5791cf8a14
Bill Teng <bteng@tenstorrent.com>
2023-10-08 07:13:45 +0000

    #0: add "H" testcase for CoreRangeSet merge

--- tests/tt_metal/tt_metal/unit_tests/core_coord/test_CoreRangeSet_merge.cpp
+++ tests/tt_metal/tt_metal/unit_tests/core_coord/test_CoreRangeSet_merge.cpp
@@ -28,13 +28,19 @@ TEST_F(CoreCoordHarness, TestCoreRangeSetMergeCoreCoord)
     std::set<CoreRange> rect_pts;
     for (unsigned y = rect.start.y; y <= rect.end.y; y++){
         for (unsigned x = rect.start.x; x <= rect.end.x; x++){
-            rect_pts.insert ( CoreRange( CoreCoord(x, y), CoreCoord(x, y) ) );
+            rect_pts.insert ( CoreRange( {x, y}, {x, y} ) );
         }
     }
     EXPECT_EQ ( empty_crs.merge(rect_pts).ranges(), std::set<::CoreRange>( {rect} ));
+
+    // upside-down "T"
     rect_pts.insert ( { CoreRange ( { 2,0}, {3,5} )});
     EXPECT_EQ ( empty_crs.merge(rect_pts).ranges(), std::set<::CoreRange>( {rect, CoreRange( {2,3}, {3,5} ) } ));
 
+    // "H", sub-optimal currently, should be reduced down to 3 CRs instead of 5
+    EXPECT_EQ ( empty_crs.merge( { CoreRange { {0,0}, {1,5} }, CoreRange { {3,0}, {4,5}}, CoreRange { {0,2} , {4,3} }  } ).ranges(),
+                std::set<::CoreRange>( { CoreRange { {0,0}, {1,1} }, CoreRange { {0,2}, {4,3}}, CoreRange{ {0,4}, {1,5}},
+                                       CoreRange { {3,0}, {4,1} }, CoreRange{ {3,4}, {4,5} } } ));
 }
 
 TEST_F(CoreCoordHarness, TestCoreRangeSetMergeCoreRange)
