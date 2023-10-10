#1509: Fix softmax to use correct mask according to batch #1749: Remove restriction on softmax parallelization and update parallelization to not require even division of rows across used cores #1915: Add support for RM tensors for softmax with autoformatting

tests/models/bert_large_performant/unit_tests/fused_ops/test_bert_large_fused_softmax.py
@@ -2,17 +2,10 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-import math
-from pathlib import Path
-import os
-import sys
 import time
 import random
 import numpy as np
 from loguru import logger
-
-f = f"{Path(__file__).parent}"
-sys.path.append(f"{f}/../../../../..")
 
 import torch
 
@@ -21,39 +14,7 @@ from tt_lib.utils import untilize, tilize_to_list, print_diff_argmax, is_close
 
 
 def ref_stable_softmax(x):
-    torch.set_printoptions(
-        precision=2, threshold=1000, sci_mode=False, edgeitems=8, linewidth=480
-    )
-    z = x  # - torch.max(x, dim=3, keepdim=True)[0]
-    numerator = torch.exp(z)
-    # print(x.shape)
-    H = x.shape[-2]
-    # print("H=", H)
-    pw0, pw1 = 0, 1  # prints a tile slice with these tile coord range
-    ph0, ph1 = 0, 1
-    sh, sw = 16, 16  # stride inside the tile
-    ow0, ow1 = 0, 0  # offset inside the tile
-    oh0, oh1 = 0, 0
-    # print(
-    #     "Ref x=\n",
-    #     x[
-    #         0,
-    #         0,
-    #         ph0 * 32 + oh0 : ph1 * 32 + oh1 : sh,
-    #         pw0 * 32 + ow0 : pw1 * 32 + ow1 : sw,
-    #     ],
-    # )
-    # print("Ref exps=\n", numerator[0, 0, ph0*32 : ph1*32 : sh, pw0*32 : pw1*32 : sw])
-    denominator = torch.sum(numerator, 3, keepdim=True)
-    # print("denom shape=", denominator.shape)
-    # print("Ref sumexp=\n", torch.reshape(denominator, (-1,H))[:, ph0*32:ph1*32])
-
-    denom1 = torch.reciprocal(denominator)
-    # print("ref 1/sumexp=\n", denom1[0, 0, 0:32:8, 0:64:8])
-    softmax = numerator * denom1
-    # print("softmaxManual=\n", softmax[0, 0, 0:32:8, 0:64:8])
-    softmax = torch.nn.Softmax(3)(x)
-    # print("softmaxTorch=\n", softmax[0, 0, 0:32:8, 0:64:8])
+    softmax = torch.nn.functional.softmax(x, dim=-1)
 
     return softmax
 
@@ -76,22 +37,24 @@ def generate_attn_mask(N, C, W, dev, offs, dtype, mem_config):
     assert W % 32 == 0
     NC = N * C
     top_row = [offs * (i % 2) for i in range(0, W)]
+    neg_top_row = [-offs * (i % 2) for i in range(0, W)]
     zero_rows = [0.0 for _ in range(31 * W)]
     # For debugging
     # top_row = [offs]*W
     # zero_rows = [offs for _ in range(31*W)]
-    nc_tiles = top_row * NC + zero_rows * NC
-    nc_tiles_np = np.asarray(nc_tiles).reshape(N, C, 32, W)
-    nc_tiles_tilized = tilize_to_list(nc_tiles_np)
-    verifytorch = torch.Tensor(nc_tiles).reshape(NC, 32, W)
-    valtorch = torch.Tensor(top_row * NC).reshape(N, C, 1, W)
-    val = ttl.tensor.Tensor(
-        nc_tiles_tilized,
-        [1, NC, 32, W],
-        dtype,
-        ttl.tensor.Layout.TILE,
-        dev,
-        mem_config,
+    nc_tiles = [((top_row if i % 2 else neg_top_row) + zero_rows) for i in range(NC)]
+    nc_tiles_pt = torch.Tensor(nc_tiles).reshape(N, C, 32, W)
+    valtorch = torch.Tensor([(top_row if i % 2 else neg_top_row) for i in range(NC)]).reshape(N, C, 1, W)
+    val = (
+        ttl.tensor.Tensor(
+            nc_tiles_pt,
+            dtype,
+        )
+        .to(ttl.tensor.Layout.TILE)
+        .to(
+            dev,
+            mem_config,
+        )
     )
     # print("Attn mask=", valtorch)
     return valtorch, val
@@ -108,47 +71,38 @@ def run_softmax_tests(dev, test_id, batch, dtype, in0_mem_config):
 
     test_dims = ((batch, 1, 6144, 384),)
     for N, C, H, W in test_dims:
-        for nrepeat in range(0, 1):
-            x = torch.randn((N, C, H, W)) * 2.0 - 1.0
-            x_t = tilize_to_list(x)
+        x = torch.randn((N, C, H, W)) * 2.0 - 1.0
 
-            t0 = tensor.Tensor(
-                x_t,
-                [N, C, H, W],
+        t0 = (
+            tensor.Tensor(
+                x,
                 dtype,
-                tensor.Layout.TILE,
+            )
+            .to(tensor.Layout.TILE)
+            .to(
                 dev,
                 in0_mem_config,
             )
+        )
 
-            if test_id == 0:
-                logger.info("Running scale_mask_softmax")
-                torch_scale, tt_scale = generate_recip_tensor(
-                    dev, 0.5 + random.random()
-                )
-                torch_attn_mask, tt_attn_mask = generate_attn_mask(
-                    N, C, W, dev, -4.2 * 1, dtype, in0_mem_config
-                )
-                t1_fused = (
-                    ttl.operations.primary.transformers.scale_mask_softmax_in_place(
-                        t0, tt_scale, tt_attn_mask
-                    )
-                )
-                ref_sm = ref_scale_mask_softmax(torch_scale, torch_attn_mask, x)
-            elif test_id == 1:
-                logger.info("Running softmax")
-                t1_fused = ttl.operations.primary.softmax_in_place(t0)
-                ref_sm = ref_stable_softmax(x)
-            else:
-                assert False
+        if test_id == 0:
+            logger.info("Running scale_mask_softmax")
+            torch_scale, tt_scale = generate_recip_tensor(dev, 0.5 + random.random())
+            torch_attn_mask, tt_attn_mask = generate_attn_mask(N, C, W, dev, -4.2 * 1, dtype, in0_mem_config)
+            t1_fused = ttl.operations.primary.transformers.scale_mask_softmax_in_place(t0, tt_scale, tt_attn_mask)
+            ref_sm = ref_scale_mask_softmax(torch_scale, torch_attn_mask, x)
+        elif test_id == 1:
+            logger.info("Running softmax")
+            t1_fused = ttl.operations.primary.softmax_in_place(t0)
+            ref_sm = ref_stable_softmax(x)
+        else:
+            assert False
 
-            tt_got_back_fused = t1_fused.cpu().to_torch()
-            tt_unt = untilize(tt_got_back_fused)
+        tt_unt = t1_fused.cpu().to(ttl.tensor.Layout.ROW_MAJOR).to_torch()
 
-            time.sleep(0.33)  # so prints don't overlap with kernel prints
-
-            assert is_close(tt_unt, ref_sm, rtol=5e-2, atol=5e-2)
-            # print_diff_argmax(tt_unt, ref_sm)
+        passing = is_close(tt_unt, ref_sm, rtol=5e-2, atol=5e-2)
+        assert passing, "is_close check failed"
+        # print_diff_argmax(tt_unt, ref_sm)
 
 
 import pytest
@@ -172,17 +126,15 @@ import pytest
 )
 @pytest.mark.parametrize(
     "batch",
-    (9, 8),
-    ids=["batch_9", "batch_8"],
+    (9, 8, 7),
+    ids=["batch_9", "batch_8", "batch_7"],
 )
 @pytest.mark.parametrize(
     "test_id",
     (0, 1),
     ids=["scale_mask_softmax", "softmax"],
 )
-def test_bert_large_softmax_test(
-    device, test_id, batch, dtype, in0_mem_config, request
-):
+def test_bert_large_softmax_test(device, test_id, batch, dtype, in0_mem_config, request):
     ttl.profiler.set_profiler_location(
         f"tt_metal/tools/profiler/logs/BERT_large_fused_softmax_{request.node.callspec.id}"
     )

tests/tt_eager/python_api_testing/unit_testing/test_softmax.py
@@ -3,15 +3,18 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import torch
+import pytest
 
 import tt_lib as ttl
 from models.utility_functions import print_diff_argmax, comp_pcc
 from tests.tt_eager.python_api_testing.sweep_tests.common import skip_for_wormhole_b0
 
-@skip_for_wormhole_b0
-def test_softmax(device):
 
+@skip_for_wormhole_b0
+@pytest.mark.parametrize("inplace", [True, False])
+def test_softmax(device, inplace):
     torch.manual_seed(0)
+    sm_op = ttl.operations.primary.softmax_in_place if inplace else ttl.tensor.softmax
 
     input_shapes = [(3, 64, 128, 96), (1, 64, 32, 32)]
 
@@ -19,11 +22,9 @@ def test_softmax(device):
         input_tensor = torch.randn(input_shape).bfloat16()
 
         tt_input_tensor = (
-            ttl.tensor.Tensor(input_tensor, ttl.tensor.DataType.BFLOAT16)
-            .to(ttl.tensor.Layout.TILE)
-            .to(device)
+            ttl.tensor.Tensor(input_tensor, ttl.tensor.DataType.BFLOAT16).to(ttl.tensor.Layout.TILE).to(device)
         )
-        tt_output_tensor_on_device = ttl.operations.primary.softmax_in_place(tt_input_tensor)
+        tt_output_tensor_on_device = sm_op(tt_input_tensor)
         tt_output_tensor = tt_output_tensor_on_device.cpu().to(ttl.tensor.Layout.ROW_MAJOR).to_torch()
 
         golden_output_tensor = torch.softmax(input_tensor, dim=-1)
@@ -32,10 +33,12 @@ def test_softmax(device):
         allclose, output = comp_pcc(tt_output_tensor, golden_output_tensor)
         assert allclose, f"FAILED: {output}"
 
-@skip_for_wormhole_b0
-def test_softmax_with_program_cache(device, use_program_cache):
 
+@skip_for_wormhole_b0
+@pytest.mark.parametrize("inplace", [True, False])
+def test_softmax_with_program_cache(device, use_program_cache, inplace):
     torch.manual_seed(0)
+    sm_op = ttl.operations.primary.softmax_in_place if inplace else ttl.tensor.softmax
 
     input_shapes = [(3, 64, 128, 96), (1, 64, 32, 32)]
 
@@ -43,11 +46,9 @@ def test_softmax_with_program_cache(device, use_program_cache):
         input_tensor = torch.randn(input_shape).bfloat16()
 
         tt_input_tensor = (
-            ttl.tensor.Tensor(input_tensor, ttl.tensor.DataType.BFLOAT16)
-            .to(ttl.tensor.Layout.TILE)
-            .to(device)
+            ttl.tensor.Tensor(input_tensor, ttl.tensor.DataType.BFLOAT16).to(ttl.tensor.Layout.TILE).to(device)
         )
-        tt_output_tensor_on_device = ttl.operations.primary.softmax_in_place(tt_input_tensor)
+        tt_output_tensor_on_device = sm_op(tt_input_tensor)
         tt_output_tensor = tt_output_tensor_on_device.cpu().to(ttl.tensor.Layout.ROW_MAJOR).to_torch()
 
         golden_output_tensor = torch.softmax(input_tensor, dim=-1)