fe990d3f7a1785c6d58f99ce2e9b03a5c5b99cc3
Brian Liu <bliu@tenstorrent.com>
2023-10-11 16:37:14 +0000

    #2388: Add new conv op reader to read from sharded data with halo and padding - Add in_b, in_h and in_w as args to untilize_with_halo_concat - Add out_shard_size_max_per_core and store for compute output shapes - Add input_tensor_shape as optional arg to optimized_conv - Add unit tests for untilize_with_halo + conv TODO: Add suppport for stride=2

--- tt_eager/tt_dnn/op_library/conv/optimized_conv_op.hpp
+++ tt_eager/tt_dnn/op_library/conv/optimized_conv_op.hpp
@@ -44,19 +44,21 @@ struct OptimizedConvBlockConfig {
 struct OptimizedConv {
     OptimizedConvParallelizationConfig parallelization_config;
     OptimizedConvBlockConfig block_config;
+
     const std::vector<int> conv_params;
     const uint32_t output_channels;
     bool untilize_out, has_bias, fuse_relu;
     MathFidelity math_fidelity;
     uint32_t extra_padding_for_32B_alignment;
     const MemoryConfig output_mem_config;
+    Shape input_tensor_shape; // For sharded input, input tensor shape is nonsense
     OptimizedConv(const std::vector<int>&c_params,
         uint32_t output_channels, bool untile_out,
         bool has_bias, bool fuse_relu,
         MathFidelity mfidelity, const OptimizedConvParallelizationConfig& p_config,
         const OptimizedConvBlockConfig& b_config,
         uint32_t e_padding_for_32B_alignment,
-        MemoryConfig output_mem_config) :
+        MemoryConfig output_mem_config, Shape input_tensor_shape) :
             output_channels(output_channels),
             conv_params(c_params),
             untilize_out(untile_out),
@@ -66,7 +68,7 @@ struct OptimizedConv {
             parallelization_config(p_config),
             block_config(b_config),
             extra_padding_for_32B_alignment(e_padding_for_32B_alignment),
-            output_mem_config(output_mem_config) {}
+            output_mem_config(output_mem_config), input_tensor_shape(input_tensor_shape) {}
 
     void validate(const std::vector<Tensor>& input_tensors, const std::vector<std::optional<const Tensor>>& optional_input_tensors) const;
     std::vector<Shape> compute_output_shapes(const std::vector<Tensor>& input_tensors) const;
@@ -80,7 +82,9 @@ Tensor optimized_conv(const Tensor& a, const Tensor &b, std::optional<const Tens
     bool untilize_out, bool has_bias, bool fuse_relu, MathFidelity math_fidelity,
     const OptimizedConvParallelizationConfig& parallelization_config,
     const OptimizedConvBlockConfig& block_config, uint32_t extra_padding_for_32B_alignment=0,
-    std::optional<MemoryConfig> output_mem_config = std::nullopt);
+    std::optional<MemoryConfig> output_mem_config = std::nullopt,
+    std::optional<std::array<std::uint32_t, 4>> input_tensor_shape = std::nullopt
+);
 
 }  // namespace tt_metal
 
--- tt_eager/tt_lib/csrc/tt_lib_bindings_tensor.cpp
+++ tt_eager/tt_lib/csrc/tt_lib_bindings_tensor.cpp
@@ -282,7 +282,7 @@ void TensorModule(py::module &m_tensor) {
                  py::arg().noconvert(), py::arg().noconvert(), py::arg("bias").noconvert() = std::nullopt,
                  py::arg().noconvert(), py::arg().noconvert(), py::arg().noconvert(), py::arg().noconvert(),
                  py::arg().noconvert(), py::arg().noconvert(), py::arg().noconvert(), py::arg().noconvert(), py::arg().noconvert() = 0,
-                 py::arg("output_mem_config").noconvert() = std::nullopt, R"doc(
+                 py::arg("output_mem_config").noconvert() = std::nullopt, py::arg("input_tensor_shape").noconvert() = std::nullopt, R"doc(
         Perform a conv ``A x B`` with two tensors
         This op tilizes tensor A and untilizes the output
 
