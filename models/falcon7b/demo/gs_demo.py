8a4e7878f81722755e61d82ed29eeb35908117e9
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2023-10-11 11:30:45 +0000

    #2957: Falcon7b GS Demo

--- models/falcon7b/demo/gs_demo.py
+++ models/falcon7b/demo/gs_demo.py
@@ -11,12 +11,12 @@ from transformers import AutoTokenizer
 
 from models.falcon7b.tt.falcon_causallm import TtFalconCausalLM
 
-from models.falcon7b.reference.hf_modeling_falcon import FalconForCausalLM
+from models.falcon7b.reference.hf_modeling_falcon import FalconConfig, FalconForCausalLM
 from models.falcon7b.model_config import (
     get_model_config,
     get_tt_cache_path,
 )
-from models.utility_functions import torch2tt_tensor, tt2torch_tensor, nearest_32
+from models.utility_functions import torch2tt_tensor, tt2torch_tensor
 import time
 
 END_OF_TEXT = 11
@@ -28,6 +28,42 @@ def post_process(logits, index):
     ids = next_tokens[:, None]
     return ids
 
+def preprocess_and_validate_inputs(input_prompts, tokenizer, max_seq_len):
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenized_inputs = tokenizer(
+        input_prompts, padding="max_length", max_length=max_seq_len, add_special_tokens=False, return_tensors="pt"
+    )
+    prefill_ids = tokenized_inputs["input_ids"]
+
+    tokenized_inputs_nopad = tokenizer(
+        input_prompts, padding=False, max_length=max_seq_len, add_special_tokens=False, return_tensors="pt"
+    )
+
+    num_users = len(tokenized_inputs_nopad["input_ids"])
+    num_input_tokens = len(tokenized_inputs_nopad["input_ids"][0])
+    for input_prompt in tokenized_inputs_nopad["input_ids"]:
+        assert len(input_prompt) == num_input_tokens
+    logger.info(f"# of users: {num_users}")
+    logger.info(f"# of input tokens per user: {num_input_tokens}")
+
+    return prefill_ids, num_users, num_input_tokens
+
+def initialize_kv_cache(configuration, num_layers, batch_size, max_seq_len, device):
+    head_dim = configuration.hidden_size // configuration.num_attention_heads
+    kv_cache = ()
+    for _ in range(num_layers):
+        k_cache = torch.zeros(batch_size, 1, max_seq_len, head_dim)
+        v_cache = torch.zeros(batch_size, 1, max_seq_len, head_dim)
+        tt_k_cache = torch2tt_tensor(k_cache, device)
+        tt_v_cache = torch2tt_tensor(v_cache, device)
+        kv_cache += ((tt_k_cache, tt_v_cache),)
+    return kv_cache
+
+def print_output_prompts(generated_ids, tokenizer, num_users_to_display=None):
+    output_prompts = tokenizer.batch_decode(generated_ids.tolist())
+    for user_id, output_prompt in enumerate(output_prompts[:num_users_to_display]):
+        logger.info(f"Output for user {user_id}:\n{output_prompt}")
+
 
 def test_gs_demo_kv():
     torch.manual_seed(0)
@@ -36,85 +72,118 @@ def test_gs_demo_kv():
 
     device = tt_lib.device.CreateDevice(0)
     tt_lib.device.SetDefaultDevice(device)
-    start = time.time()
     model_version = "tiiuae/falcon-7b-instruct"
     model_config = get_model_config("BFLOAT16-DRAM")
     tt_cache_path = get_tt_cache_path(model_version)
 
     batch_size = 32
     num_layers = 32
-    num_max_tokens = 192
-    max_input_tokens = 32
-    max_seq_len = nearest_32(num_max_tokens)
+    max_seq_len = 256
 
-    input_prompts = [
-        "write a short poem about London in English",
-        "write a short poem about Madrid in Spanish",
-        "write a short poem about Paris in French",
-    ]
-
-    logger.info("Initializing tokenizer")
-    tokenizer = AutoTokenizer.from_pretrained(model_version)
-
-    logger.info("Tokenizing inputs")
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenized_inputs = tokenizer(
-        input_prompts, padding="max_length", max_length=max_input_tokens, add_special_tokens=False, return_tensors="pt"
+    configuration = FalconConfig(
+        **{
+            "_name_or_path": "tiiuae/falcon-7b-instruct",
+            "alibi": False,
+            "apply_residual_connection_post_layernorm": False,
+            "architectures": [
+                "FalconForCausalLM"
+            ],
+            "attention_dropout": 0.0,
+            "auto_map": {
+                "AutoConfig": "configuration_falcon.FalconConfig",
+                "AutoModel": "modeling_falcon.FalconModel",
+                "AutoModelForCausalLM": "modeling_falcon.FalconForCausalLM",
+                "AutoModelForQuestionAnswering": "modeling_falcon.FalconForQuestionAnswering",
+                "AutoModelForSequenceClassification": "modeling_falcon.FalconForSequenceClassification",
+                "AutoModelForTokenClassification": "modeling_falcon.FalconForTokenClassification"
+            },
+            "bias": False,
+            "bos_token_id": 11,
+            "eos_token_id": 11,
+            "hidden_dropout": 0.0,
+            "hidden_size": 4544,
+            "initializer_range": 0.02,
+            "layer_norm_epsilon": 1e-05,
+            "model_type": "falcon",
+            "multi_query": True,
+            "new_decoder_architecture": False,
+            "num_attention_heads": 71,
+            "num_hidden_layers": 32,
+            "num_kv_heads": 71,
+            "parallel_attn": True,
+            "torch_dtype": "bfloat16",
+            "transformers_version": "4.28.1",
+            "use_cache": True,
+            "vocab_size": 65024
+        }
     )
-    prefill_ids = tokenized_inputs["input_ids"]
 
-    tokenized_inputs_nopad = tokenizer(
-        input_prompts, padding=False, max_length=max_input_tokens, add_special_tokens=False, return_tensors="pt"
-    )
+    # State dict is needed for embeddings
+    state_dict = FalconForCausalLM.from_pretrained(model_version).state_dict()
 
-    num_users = len(tokenized_inputs_nopad["input_ids"])
-    num_input_tokens = len(tokenized_inputs_nopad["input_ids"][0])
-    for input_prompt in tokenized_inputs_nopad["input_ids"]:
-        assert len(input_prompt) == num_input_tokens
-    logger.info(f"# of users: {num_users}")
-    logger.info(f"# of input tokens: {num_input_tokens}")
-
-    hugging_face_reference_model = FalconForCausalLM.from_pretrained(model_version)
-    hugging_face_reference_model.eval()
-
-    configuration = hugging_face_reference_model.config
-    state_dict = hugging_face_reference_model.state_dict()
-
-    post_processor = partial(post_process)
-
-    # Prepare input ------------------------------------------------------------------------
+    logger.info("Loading TT model weights")
     base_url = ""
-    max_position_embeddings = max_seq_len
-    head_dim = configuration.hidden_size // configuration.num_attention_heads
-    use_cache = True
-
-    logger.info("Creating TT Model")
     tt_FalconCausalLM = TtFalconCausalLM(
         device,
         state_dict,
         base_url,
         num_layers,
         configuration,
-        max_position_embeddings,
+        max_seq_len,
         model_config,
         tt_cache_path,
     )
-    logger.info("Created TT Model")
+    logger.info("Loaded TT model weights")
 
-    logger.info("Setting up inputs and attention masks")
+    input_prompts = [
+        "write a short poem about London in English",
+        "write a short poem about Madrid in Spanish",
+        "write a short poem about Madrid in English",
+        "write a short poem about Paris in English",
+        "write a short poem about Paris in French",
+        "write a short poem about Istanbul in English",
+        "write a short poem about Shanghai in English",
+        "write a short poem about Lagos in English",
+        "what is the capital of USA? ",
+        "what is the capital of Canada? ",
+        "what is the capital of UK? ",
+        "what is the capital of Germany? ",
+        "what is the capital of France? ",
+        "what is the capital of Japan? ",
+        "what is the capital of India? ",
+        "what is the capital of China? ",
+        "what is the currency of Cuba? ",
+        "what is the currency of Lebanon? ",
+        "what is the currency of Brazil? ",
+        "what is the currency of Australia? ",
+        "what is the currency of Jamaica? ",
+        "what is the currency of Egypt? ",
+        "what is the currency of Uzbekistan? ",
+        "what is the currency of Argentina? ",
+        "describe the geographic location of London in UK",
+        "describe the geographic location of Toronto in Canada",
+        "describe the geographic location of Madrid in Spain",
+        "describe the geographic location of Paris in France",
+        "describe the geographic location of Rome in Italy",
+        "describe the geographic location of Istanbul in Turkey",
+        "describe the geographic location of Shanghai in China",
+        "describe the geographic location of Lagos in Nigeria",
+    ]
 
-    kv_cache = ()
-    k_cache = torch.zeros(batch_size, 1, max_position_embeddings, head_dim)
-    v_cache = torch.zeros(batch_size, 1, max_position_embeddings, head_dim)
-    for _ in range(num_layers):
-        tt_k_cache = torch2tt_tensor(k_cache, device)
-        tt_v_cache = torch2tt_tensor(v_cache, device)
-        kv_cache += ((tt_k_cache, tt_v_cache),)
+    logger.info("Tokenizing inputs")
+    tokenizer = AutoTokenizer.from_pretrained(model_version)
+    prefill_ids, num_users, num_input_tokens = preprocess_and_validate_inputs(input_prompts, tokenizer, max_seq_len)
 
-    # PREFILL
+    logger.info("Initializing KV cache")
+    kv_cache = initialize_kv_cache(configuration, num_layers, batch_size, max_seq_len, device)
+
+    logger.info("Running prefill stage")
+    post_processor = partial(post_process)
+    use_cache = True
     output_ids = torch.zeros(num_users, 1, dtype=torch.int64)
     for user_id in range(num_users):
-        logger.info(f"Falcon prefill for user {user_id} only")
+        prefill_start = time.time()
+        logger.info(f"Running prefill for user {user_id}")
 
         (
             tt_prefill_embeddings,
@@ -131,47 +200,36 @@ def test_gs_demo_kv():
             layer_past_len=0,
             use_cache=use_cache,
         )
+
         tt_prefill_embeddings.deallocate()
         if tt_prefill_attention_mask is not None:
             tt_prefill_attention_mask.deallocate()
 
         logits = tt2torch_tensor(tt_logits).squeeze(1)
         tt_logits.deallocate()
+
         user_output_ids = post_processor(logits=logits, index=num_input_tokens-1)
         output_ids[user_id] = user_output_ids
-    logger.info("finished prefill stage")
+
+        prefill_end = time.time()
+        logger.info(f"Finished prefill for user {user_id} in {round((prefill_end - prefill_start), 2)}s")
+
+    logger.info("Finished prefill stage")
 
     generated_ids = torch.concat((prefill_ids[..., :num_input_tokens], output_ids), dim=1)
-    output_prompts = tokenizer.batch_decode(generated_ids.tolist())
-    for output_prompt in output_prompts:
-        logger.info(f"output::: {output_prompt}")
+    print_output_prompts(generated_ids, tokenizer, 3)
 
-    zeroed_out_kv_cache = ()
-    for tt_k_cache, tt_v_cache in kv_cache:
-        k_cache = tt2torch_tensor(tt_k_cache)
-        v_cache = tt2torch_tensor(tt_v_cache)
-        k_cache[:, :, num_input_tokens:] = 0
-        v_cache[:, :, num_input_tokens:] = 0
-        tt_k_cache = torch2tt_tensor(k_cache, device)
-        tt_v_cache = torch2tt_tensor(v_cache, device)
-        zeroed_out_kv_cache += ((tt_k_cache, tt_v_cache),)
-    kv_cache = zeroed_out_kv_cache
-
-    kv_cache_len = num_input_tokens  # This will increment by one after each decode
-
-    end_prefill = time.time()
-    logger.info(f"Prefill Run Time: {round((end_prefill - start), 2)}")
-
-    # DECODE
+    logger.info("Running decode stage")
     decode_ids = torch.zeros(batch_size, 1, dtype=torch.int64)
     for user_id, output_id in enumerate(output_ids):
         decode_ids[user_id] = output_id
 
-    prompt_is_done = [False] * num_users
-    for output_token_index in range(num_max_tokens - num_input_tokens):
+    kv_cache_len = num_input_tokens  # This will increment by one after each decode
+    prompt_is_done = [False for _ in range(num_users)]
+    for output_token_index in range(max_seq_len - num_input_tokens):
+        logger.info(f"Running decode for token {output_token_index}")
         decode_start = time.time()
 
-        logger.info(f"Falcon decode token {output_token_index} for {batch_size} users")
         (
             tt_decode_embeddings,
             tt_decode_attention_mask,
@@ -209,23 +267,14 @@ def test_gs_demo_kv():
         generated_ids = torch.concat((generated_ids, decode_ids[:num_users]), dim=1)
         kv_cache_len += 1
 
-        output_prompts = tokenizer.batch_decode(generated_ids.tolist())
-        for output_prompt in output_prompts:
-            logger.info(f"output::: {output_prompt}")
+        print_output_prompts(generated_ids, tokenizer, 3)
 
         decode_end = time.time()
-        logger.info(f"Decode #{output_token_index} Run Time: {round((decode_end - decode_start), 2)}")
+        logger.info(f"Finished decode for token {output_token_index} in {round((decode_end - decode_start), 2)}")
 
-    end = time.time()
-    logger.info(f"Total Model Run Time: {round((end - start), 2)}")
+    logger.info("Finished decode stage")
 
-    output_prompts = tokenizer.batch_decode(generated_ids.tolist())
+    print_output_prompts(generated_ids, tokenizer)
 
     tt_lib.device.CloseDevice(device)
-    device = None
-
-    for input_prompt, output_prompt in zip(input_prompts, output_prompts):
-        logger.info(f"input: {input_prompt}")
-        logger.info(f"output: {output_prompt}")
-
     tt_lib.program_cache.disable_and_clear()
