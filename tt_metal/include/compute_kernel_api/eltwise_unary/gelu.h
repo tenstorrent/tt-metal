bb8063f5b0063aabf84332f716bafaf56fca5e26
tarafdarTT <ntarafdar@tenstorrent.com>
2023-10-12 20:36:59 +0000

    #3136: update gelu documentation

--- tt_metal/include/compute_kernel_api/eltwise_unary/gelu.h
+++ tt_metal/include/compute_kernel_api/eltwise_unary/gelu.h
@@ -25,7 +25,17 @@ ALWI void gelu_tile_init() {
 }
 
 /**
- *  Please refer to documentation for relu_tile.
+ * Performs element-wise computation of gelu  on each element of a tile
+ * in DST register at index tile_index. The DST register buffer must be in
+ * acquired state via *acquire_dst* call. This call is blocking and is only
+ * available on the compute engine.
+ *
+ * Return value: None
+ *
+ * | Argument         | Description                                                                | Type     | Valid Range                                           | Required |
+ * |------------------|----------------------------------------------------------------------------|----------|-------------------------------------------------------|----------|
+ * | tile_index       | The index of the tile in DST register buffer to perform the computation on | uint32_t | Must be less than the size of the DST register buffer | True     |
+ * | fast_and_approx  | Computation to be done faster and approximate                              | bool     |                                                       | False    |
  */
 ALWI void gelu_tile(uint32_t idst, bool fast_and_approx=true) {
     if (fast_and_approx) {
