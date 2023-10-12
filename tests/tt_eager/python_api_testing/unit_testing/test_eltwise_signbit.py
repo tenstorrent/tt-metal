9438edad798b97418de40c96739bcbdd09380303
banekg <barsic@tenstorrent.com>
2023-10-12 13:01:05 +0000

    #3129: Add unit test for signbit, layernorm and reduce_max_w ops failing on wormhole

---
+++ tests/tt_eager/python_api_testing/unit_testing/test_eltwise_signbit.py
@@ -0,0 +1,76 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import math
+from pathlib import Path
+import sys
+import time
+import os
+from loguru import logger
+
+f = f"{Path(__file__).parent}"
+sys.path.append(f"{f}/../../../../..")
+
+import pytest
+import torch
+
+import tt_lib as ttl
+from tests.tt_eager.python_api_testing.sweep_tests import pytorch_ops
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc, comp_equal
+from tests.tt_eager.python_api_testing.sweep_tests.tt_lib_ops import setup_tt_tensor
+from tests.tt_eager.python_api_testing.sweep_tests.common import is_wormhole_b0, skip_for_wormhole_b0
+from models.utility_functions import torch2tt_tensor, tt2torch_tensor
+
+
+def run_eltwise_signbit_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
+    torch.manual_seed(data_seed)
+
+    if in_mem_config == "SYSTEM_MEMORY":
+        in_mem_config = ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED)
+
+    x = torch.Tensor(size=input_shape).uniform_(-100, 100)
+    x_ref = x.detach().clone()
+
+    # compute ref value --------------------------
+    ref_value = pytorch_ops.signbit(x_ref)
+
+    tt_tensor = setup_tt_tensor(x, device, dlayout, in_mem_config, dtype)
+
+    tt_result = ttl.tensor.signbit(
+        input=tt_tensor,
+        output_mem_config=out_mem_config
+    )
+
+    tt_result = tt2torch_tensor(tt_result)
+
+    # compare tt and golden outputs -------------
+    success, pcc_value = comp_equal(ref_value, tt_result)
+    logger.debug(pcc_value)
+
+    assert success
+
+
+test_sweep_args=[
+    # ROW_MAJOR
+    ((6, 2, 216, 186), ttl.tensor.DataType.BFLOAT16, ttl.tensor.Layout.ROW_MAJOR, "SYSTEM_MEMORY", ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM), 13482735),
+    ((6, 2, 216, 186), ttl.tensor.DataType.BFLOAT16, ttl.tensor.Layout.ROW_MAJOR, ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1), ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM), 494232),
+    ((6, 2, 216, 186), ttl.tensor.DataType.BFLOAT16, ttl.tensor.Layout.ROW_MAJOR, ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM), ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM), 4379583),
+    # TILE
+    ((1, 10, 192, 160), ttl.tensor.DataType.BFLOAT16, ttl.tensor.Layout.TILE, "SYSTEM_MEMORY", ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM), 4175638),
+    ((1, 10, 192, 160), ttl.tensor.DataType.BFLOAT16, ttl.tensor.Layout.TILE, ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1), ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM), 895795),
+    ((1, 10, 192, 160), ttl.tensor.DataType.BFLOAT16, ttl.tensor.Layout.TILE, ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM), ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM), 2482923),
+   ]
+
+# @skip_for_wormhole_b0
+@pytest.mark.parametrize(
+    "input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed",
+    (
+        test_sweep_args
+    ),
+)
+
+def test_eltwise_signbit_test(
+    input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device
+):
+    run_eltwise_signbit_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device)
