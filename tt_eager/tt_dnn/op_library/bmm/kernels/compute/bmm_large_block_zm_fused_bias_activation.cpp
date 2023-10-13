fe7eed2f6a474dd2e6e022cf525d6d4677b7278f
Austin Ho <aho@tenstorrent.com>
2023-10-13 13:35:43 +0000

    #3144: #3146: Refactor MM compute kernel to move bias fusion into a separate loop after MM compute and remove interm buffer that is no longer needed. Reorganize initialization calls in the kernel to minimize reconfigures

--- tt_eager/tt_dnn/op_library/bmm/kernels/compute/bmm_large_block_zm_fused_bias_activation.cpp
+++ tt_eager/tt_dnn/op_library/bmm/kernels/compute/bmm_large_block_zm_fused_bias_activation.cpp
@@ -16,45 +16,60 @@
 namespace NAMESPACE {
 void MAIN {
 
-    uint32_t in0_block_w = get_compile_time_arg_val(0); // inner block size in tiles
-    uint32_t in0_num_subblocks = get_compile_time_arg_val(1); // outer row block size (in inner row blocks)
-    uint32_t in0_block_num_tiles = get_compile_time_arg_val(2); // out_subblock_h*in0_block_w*in0_num_subblocks;
-    uint32_t in0_subblock_num_tiles = get_compile_time_arg_val(3);  // out_subblock_h*in0_block_w
-    uint32_t in1_num_subblocks = get_compile_time_arg_val(4); // outer column block size (in inner column blocks)
-    uint32_t in1_block_num_tiles = get_compile_time_arg_val(5); //out_subblock_w*in0_block_w* in1_num_subblocks;
-    uint32_t in1_per_core_w = get_compile_time_arg_val(6); // out_subblock_w*in1_num_subblocks
-    uint32_t num_blocks = get_compile_time_arg_val(7);  // outer inner dim (in inner dim blocks)
-    uint32_t out_subblock_h = get_compile_time_arg_val(8); // inner row block size in tiles
-    uint32_t out_subblock_w = get_compile_time_arg_val(9); // inner column block size in tiles
-    uint32_t out_subblock_num_tiles = get_compile_time_arg_val(10); // out_subblock_h * out_subblock_w;
-    uint32_t batch = get_compile_time_arg_val(11); // batch dim
+    constexpr uint32_t in0_block_w = get_compile_time_arg_val(0); // inner block size in tiles
+    constexpr uint32_t in0_num_subblocks = get_compile_time_arg_val(1); // outer row block size (in inner row blocks)
+    constexpr uint32_t in0_block_num_tiles = get_compile_time_arg_val(2); // out_subblock_h*in0_block_w*in0_num_subblocks;
+    constexpr uint32_t in0_subblock_num_tiles = get_compile_time_arg_val(3);  // out_subblock_h*in0_block_w
+    constexpr uint32_t in1_num_subblocks = get_compile_time_arg_val(4); // outer column block size (in inner column blocks)
+    constexpr uint32_t in1_block_num_tiles = get_compile_time_arg_val(5); //out_subblock_w*in0_block_w* in1_num_subblocks;
+    constexpr uint32_t in1_per_core_w = get_compile_time_arg_val(6); // out_subblock_w*in1_num_subblocks
+    constexpr uint32_t num_blocks = get_compile_time_arg_val(7);  // outer inner dim (in inner dim blocks)
+    constexpr uint32_t out_subblock_h = get_compile_time_arg_val(8); // inner row block size in tiles
+    constexpr uint32_t out_subblock_w = get_compile_time_arg_val(9); // inner column block size in tiles
+    constexpr uint32_t out_subblock_num_tiles = get_compile_time_arg_val(10); // out_subblock_h * out_subblock_w;
+    constexpr uint32_t batch = get_compile_time_arg_val(11); // batch dim
 
-    uint32_t in0_cb_id = tt::CB::c_in0;
-    uint32_t in1_cb_id = tt::CB::c_in1;
-    uint32_t out_cb_id = tt::CB::c_out0;
-    uint32_t mm_partials_cb_id = tt::CB::c_intermed0;
-    uint32_t mm_bias_intermediate_cb_id = tt::CB::c_intermed1;
-    uint32_t bias_cb_id = tt::CB::c_in3;
+    constexpr uint32_t in0_cb_id = tt::CB::c_in0;
+    constexpr uint32_t in1_cb_id = tt::CB::c_in1;
+    constexpr uint32_t out_cb_id = tt::CB::c_out0;
+    constexpr uint32_t mm_partials_cb_id = tt::CB::c_intermed0;
+    constexpr uint32_t bias_cb_id = tt::CB::c_in3;
 
     #ifdef FUSE_BIAS
-        init_bcast<EltwiseBinaryType::ELWADD, BroadcastType::ROW>(mm_bias_intermediate_cb_id, bias_cb_id);
+    init_bcast<EltwiseBinaryType::ELWADD, BroadcastType::ROW>(mm_partials_cb_id, bias_cb_id);
+    constexpr uint32_t mm_out_cb_id = mm_partials_cb_id;
+    #else
+    constexpr uint32_t mm_out_cb_id = out_cb_id;
     #endif
 
-    mm_init(in0_cb_id, in1_cb_id, out_cb_id);
+    #ifdef SFPU_OP_INIT_ACTIVATION
+    SFPU_OP_INIT_ACTIVATION
+    #endif
 
+    constexpr bool spill = num_blocks > 1;
+
+    mm_init(in0_cb_id, in1_cb_id, out_cb_id);
     for (uint32_t b = 0; b < batch; b++){
-        bool spill = num_blocks > 1;
         bool enable_reload = false;
         uint32_t out_num_tiles_to_wait = out_subblock_num_tiles;
 
         #ifdef PACK_RELU
         // for each batch we start we relu disabled so that intermediate results are not relu'd
-        PACK(( llk_pack_relu_config(ReluType::NO_RELU) ));
+        if constexpr(spill and batch > 1) {
+            PACK(( llk_pack_relu_config(ReluType::NO_RELU) ));
+        }
         #endif
 
         for(uint32_t block = 0; block < num_blocks; block++)
         {
             bool last_out = block == (num_blocks-1);
+            // Configure packer once for pack out without Bias
+            #if not defined FUSE_BIAS and defined PACK_RELU
+            if (last_out) {
+                // if last block we pack the final result with relu enabled
+                PACK(( llk_pack_relu_config(ReluType::ZERO_RELU) ));
+            }
+            #endif
 
             cb_wait_front(in0_cb_id, in0_block_num_tiles);
             cb_wait_front(in1_cb_id, in1_block_num_tiles);
@@ -95,77 +110,25 @@ void MAIN {
                         }
                         in0_index_h_offset += in0_block_w;
                     }
-                    tile_regs_commit();
 
                     if (last_out) {
-
-                        #ifdef FUSE_BIAS
-                            // Move matmul result to interm buffer
-                            #ifdef PACK_RELU
-                            // need to have relu disabled when we pack out partial result before adding bias
-                            PACK(( llk_pack_relu_config(ReluType::NO_RELU) ));
-                            #endif
-                            cb_reserve_back(mm_bias_intermediate_cb_id, out_subblock_num_tiles);
-                            tile_regs_wait();
-                            for (uint32_t i = 0; i < out_subblock_num_tiles; i++) {
-                                pack_tile(i, mm_bias_intermediate_cb_id);
-                            }
-                            tile_regs_release();
-                            cb_push_back(mm_bias_intermediate_cb_id, out_subblock_num_tiles);
-
-                            // Redundant wait since we know data was just pushed
-                            cb_wait_front(mm_bias_intermediate_cb_id, out_subblock_num_tiles);
-                            cb_wait_front(bias_cb_id, in1_per_core_w);
-                            add_bcast_rows_init_short();
-                            // reconfigure unpacker df for src B
-                            unpack_reconfig_data_format(mm_bias_intermediate_cb_id, bias_cb_id);
-                            // reconfigure packer df for out
-                            pack_reconfig_data_format(out_cb_id);
-                            tile_regs_acquire();
-                            for (uint32_t i = 0, j = 0; j < out_subblock_h; j++) {
-                                uint32_t bcast_tile_idx = in1_index_subblock_offset;
-                                for (uint32_t k = 0; k < out_subblock_w; k++, i++) {
-                                    add_tiles_bcast_rows(mm_bias_intermediate_cb_id, bias_cb_id, i, bcast_tile_idx, i);
-                                    bcast_tile_idx++;
-                                }
-                            }
-                            // if there's no SFPU fusion, we commit the regs so packer can start packing
-                            #ifndef SFPU_OP_INIT_ACTIVATION
-                            tile_regs_commit();
-                            #endif
-
-                            cb_pop_front(mm_bias_intermediate_cb_id, out_subblock_num_tiles);
-                            // reconfigure init for matmul
-                            mm_init_short();
-                            // reconfigure unpacker df for src B
-                            unpack_reconfig_data_format(in1_cb_id, in0_cb_id);
+                        // If we fuse bias, we will pack out and run bias + optional sfpu in a separate loop
+                        #if not defined FUSE_BIAS and defined SFPU_OP_INIT_ACTIVATION
+                        for (uint32_t i = 0; i < out_subblock_num_tiles; i++) {
+                            SFPU_OP_FUNC_ACTIVATION
+                        }
                         #endif
-
-                        // sfpu activation
-                       #ifdef SFPU_OP_INIT_ACTIVATION
-                             SFPU_OP_INIT_ACTIVATION
-                            // acquire only if there is no bias fusion, because if there is, we already acquired
-                            #ifndef FUSE_BIAS
-                            tile_regs_acquire();
-                            #endif
-                            for (uint32_t i = 0; i < out_subblock_num_tiles; i++) {
-                              SFPU_OP_FUNC_ACTIVATION
-                            }
-                            tile_regs_commit();
-                        #endif
-                        #ifdef PACK_RELU
-                        // if last block we pack the final result with relu enabled
-                        PACK(( llk_pack_relu_config(ReluType::ZERO_RELU) ));
-                        #endif
+                        tile_regs_commit();
                         // Pack out to output buffer
-                        cb_reserve_back(out_cb_id, out_subblock_num_tiles);
+                        cb_reserve_back(mm_out_cb_id, out_subblock_num_tiles);
                         tile_regs_wait();
                         for (uint32_t i = 0; i < out_subblock_num_tiles; i++) {
-                            pack_tile(i, out_cb_id);
+                            pack_tile(i, mm_out_cb_id);
                         }
                         tile_regs_release();
-                        cb_push_back(out_cb_id, out_subblock_num_tiles);
+                        cb_push_back(mm_out_cb_id, out_subblock_num_tiles);
                     } else {
+                        tile_regs_commit();
                         // Wait for tiles in output buffer to be written out since interm and output share memory
                         if (block == 0) {
                             cb_reserve_back(out_cb_id, out_num_tiles_to_wait);
@@ -186,12 +149,73 @@ void MAIN {
                 in0_index_subblock_offset += in0_subblock_num_tiles;
             }
 
-            if (spill) enable_reload = true;
+            if constexpr (spill) enable_reload = true;
 
             cb_pop_front(in0_cb_id, in0_block_num_tiles);
             cb_pop_front(in1_cb_id, in1_block_num_tiles);
 
         }
+        #ifdef FUSE_BIAS
+        #ifdef PACK_RELU
+        // if last block we pack the final result with relu enabled
+        PACK(( llk_pack_relu_config(ReluType::ZERO_RELU) ));
+        #endif
+        add_bcast_rows_init_short();
+        // reconfigure unpacker df for src B
+        unpack_reconfig_data_format(mm_partials_cb_id, bias_cb_id);
+        // reconfigure packer df for out
+        pack_reconfig_data_format(out_cb_id);
+        int in0_index_subblock_offset = 0;
+        for (uint32_t in0_subblock = 0; in0_subblock < in0_num_subblocks; in0_subblock++) {
+            int in1_index_subblock_offset = 0;
+            for (uint32_t in1_subblock = 0; in1_subblock < in1_num_subblocks; in1_subblock++) {
+                // Redundant wait since we know data was just pushed
+                cb_wait_front(mm_partials_cb_id, out_subblock_num_tiles);
+                cb_wait_front(bias_cb_id, in1_per_core_w);
+                tile_regs_acquire();
+                for (uint32_t i = 0, j = 0; j < out_subblock_h; j++) {
+                    uint32_t bcast_tile_idx = in1_index_subblock_offset;
+                    for (uint32_t k = 0; k < out_subblock_w; k++, i++) {
+                        add_tiles_bcast_rows(mm_partials_cb_id, bias_cb_id, i, bcast_tile_idx, i);
+                        bcast_tile_idx++;
+                    }
+                }
+                // if there's no SFPU fusion, we commit the regs so packer can start packing
+                #ifndef SFPU_OP_INIT_ACTIVATION
+                tile_regs_commit();
+                #endif
+
+                cb_pop_front(mm_partials_cb_id, out_subblock_num_tiles);
+
+
+                // sfpu activation
+                #ifdef SFPU_OP_INIT_ACTIVATION
+                for (uint32_t i = 0; i < out_subblock_num_tiles; i++) {
+                    SFPU_OP_FUNC_ACTIVATION
+                }
+                tile_regs_commit();
+                #endif
+
+                // Pack out to output buffer
+                cb_reserve_back(out_cb_id, out_subblock_num_tiles);
+                tile_regs_wait();
+                for (uint32_t i = 0; i < out_subblock_num_tiles; i++) {
+                    pack_tile(i, out_cb_id);
+                }
+                tile_regs_release();
+                cb_push_back(out_cb_id, out_subblock_num_tiles);
+
+                in1_index_subblock_offset += out_subblock_w;
+            }
+            in0_index_subblock_offset += in0_subblock_num_tiles;
+        }
+        if constexpr(spill and batch > 1) {
+            // reconfigure init for matmul
+            mm_init_short();
+            // reconfigure unpacker df for src B
+            unpack_reconfig_data_format(in1_cb_id, in0_cb_id);
+        }
+        #endif
     }
 }
 }
