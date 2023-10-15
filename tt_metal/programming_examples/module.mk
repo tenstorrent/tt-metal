a763b35ee16df0b535ad90dd7d363edb0bc322bb
Mohamed Bahnas <mbahnas@tenstorrent.com>
2023-10-15 22:57:28 +0000

    #3475: matmul single core code and rst

--- tt_metal/programming_examples/module.mk
+++ tt_metal/programming_examples/module.mk
@@ -8,6 +8,7 @@ include $(TT_METAL_HOME)/tt_metal/programming_examples/loopback/module.mk
 include $(TT_METAL_HOME)/tt_metal/programming_examples/eltwise_binary/module.mk
 include $(TT_METAL_HOME)/tt_metal/programming_examples/eltwise_sfpu/module.mk
 include $(TT_METAL_HOME)/tt_metal/programming_examples/profiler/module.mk
+include $(TT_METAL_HOME)/tt_metal/programming_examples/matmul/module.mk
 
 
 PROFILER_TESTS += \
@@ -18,9 +19,11 @@ PROFILER_TESTS += \
 programming_examples: programming_examples/loopback \
                       programming_examples/eltwise_binary \
                       programming_examples/eltwise_sfpu \
-		      $(PROFILER_TESTS)
+                      programming_examples/matmul \
+                      $(PROFILER_TESTS)
 
 programming_examples/loopback: $(PROGRAMMING_EXAMPLES_TESTDIR)/loopback;
 programming_examples/eltwise_binary: $(PROGRAMMING_EXAMPLES_TESTDIR)/eltwise_binary;
 programming_examples/eltwise_sfpu: $(PROGRAMMING_EXAMPLES_TESTDIR)/eltwise_sfpu;
+programming_examples/matmul: $(PROGRAMMING_EXAMPLES_TESTDIR)/matmul;
 programming_examples/profiler/%: $(PROGRAMMING_EXAMPLES_TESTDIR)/profiler/%;
