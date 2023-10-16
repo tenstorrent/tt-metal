0c5ffed227bb1f9886a45c67018edd66388d77b0
Austin Ho <aho@tenstorrent.com>
2023-10-16 14:34:16 +0000

    #0: Enable program cache for metal resnet

--- tests/models/resnet/test_metal_resnet50.py
+++ tests/models/resnet/test_metal_resnet50.py
@@ -20,7 +20,7 @@ from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import (
 
 
 @pytest.mark.parametrize("batch_size", [1, 2, 8])
-def test_run_resnet50_inference(device, batch_size, imagenet_sample_input):
+def test_run_resnet50_inference(use_program_cache, device, batch_size, imagenet_sample_input):
     image1 = imagenet_sample_input
     image = image1
     for i in range(batch_size - 1):
