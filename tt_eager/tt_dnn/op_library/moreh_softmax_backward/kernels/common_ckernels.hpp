e5578cc8ef9ed8b62a27c5eebdc86cb77d024f6b
hschoi <hschoi4448@gmail.com>
2023-10-17 02:13:25 +0000

    #3011: fix common_ckernels pop code

--- tt_eager/tt_dnn/op_library/moreh_softmax_backward/kernels/common_ckernels.hpp
+++ tt_eager/tt_dnn/op_library/moreh_softmax_backward/kernels/common_ckernels.hpp
@@ -30,9 +30,9 @@ ALWI void mul_tiles_to_cb(
     pack_tile(dst0, ocb);
 
     if (pop0)
-        cb_pop_front(icb0, itile0 + 1);
+        cb_pop_front(icb0, pop0);
     if (pop1)
-        cb_pop_front(icb1, itile1 + 1);
+        cb_pop_front(icb1, pop1);
 
     cb_push_back(ocb, onetile);
 }
@@ -69,11 +69,11 @@ ALWI void mul_tiles_and_mask_tile_to_cb(
     pack_tile(dst0, ocb);
 
     if (pop0)
-        cb_pop_front(icb0, itile0 + 1);
+        cb_pop_front(icb0, pop0);
     if (pop1)
-        cb_pop_front(icb1, itile1 + 1);
+        cb_pop_front(icb1, pop1);
     if (popm)
-        cb_pop_front(maskcb, mtile + 1);
+        cb_pop_front(maskcb, popm);
 
     cb_push_back(ocb, onetile);
 }
@@ -100,9 +100,9 @@ ALWI void mul_tiles_bcast_rows_to_cb(
     pack_tile(dst0, ocb);
 
     if (pop0)
-        cb_pop_front(icb0, itile0 + 1);
+        cb_pop_front(icb0, pop0);
     if (pop1)
-        cb_pop_front(icb1, itile1 + 1);
+        cb_pop_front(icb1, pop1);
 
     cb_push_back(ocb, onetile);
 }
@@ -129,9 +129,9 @@ ALWI void mul_tiles_bcast_cols_to_cb(
     pack_tile(dst0, ocb);
 
     if (pop0)
-        cb_pop_front(icb0, itile0 + 1);
+        cb_pop_front(icb0, pop0);
     if (pop1)
-        cb_pop_front(icb1, itile1 + 1);
+        cb_pop_front(icb1, pop1);
 
     cb_push_back(ocb, onetile);
 }
@@ -304,9 +304,9 @@ ALWI void sub_tiles_bcast_rows_to_cb(
     pack_tile(dst0, ocb);
 
     if (pop0)
-        cb_pop_front(icb0, itile0 + 1);
+        cb_pop_front(icb0, pop0);
     if (pop1)
-        cb_pop_front(icb1, itile1 + 1);
+        cb_pop_front(icb1, pop1);
 
     cb_push_back(ocb, onetile);
 }
@@ -333,9 +333,9 @@ ALWI void sub_tiles_bcast_cols_to_cb(
     pack_tile(dst0, ocb);
 
     if (pop0)
-        cb_pop_front(icb0, itile0 + 1);
+        cb_pop_front(icb0, pop0);
     if (pop1)
-        cb_pop_front(icb1, itile1 + 1);
+        cb_pop_front(icb1, pop1);
 
     cb_push_back(ocb, onetile);
 }
