12fe05e1433c3f1beecbff9137b999a59070d489
vigneshkeerthivasanx <vignesh.keerthivasan@multicorewareinc.com>
2023-10-17 16:03:05 +0000

    #3113: Add TT implementation for mistral model

---
+++ models/experimental/mistral/demo/gs_demo.py
@@ -0,0 +1,65 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+import torch
+import pytest
+from loguru import logger
+import json
+from pathlib import Path
+from models.experimental.mistral.tt.mistral_transformer import TtTransformer
+from models.experimental.mistral.mistral_utils import generate
+from models.experimental.mistral.tt.mistral_configuration import TtModelArgs
+from models.experimental.mistral.demo.model import Transformer
+from models.experimental.mistral.demo.tokenizer import Tokenizer
+
+
+@pytest.mark.parametrize(
+    "batch_size",
+    ((1),),
+)
+def test_gs_demo_single_input_inference(batch_size, model_location_generator, device):
+    prompts = batch_size * [
+        "This is a sample text for single layer execution ",
+    ]
+
+    mistral_path = model_location_generator("mistral-7B-v0.1", model_subdir="Mistral")
+    tokenizer = Tokenizer(str(Path(mistral_path) / "tokenizer.model"))
+    state_dict = torch.load(mistral_path / "consolidated.00.pth")
+    base_address = f""
+    with open(mistral_path / "params.json", "r") as f:
+        model_args = TtModelArgs(**json.loads(f.read()))
+
+    state_dict = {
+        k[0:]: v
+        for k, v in state_dict.items()
+        if (
+            k.startswith("layers.0.")
+            or k.startswith("tok_embeddings")
+            or k.startswith("norm.weight")
+            or k.startswith("output.weight")
+        )
+    }
+
+    model_args.max_batch_size = batch_size
+    model_args.n_layers = 1
+    reference_model = Transformer(args=model_args)
+    reference_model.load_state_dict(state_dict)
+
+    tt_model = TtTransformer(
+        args=model_args,
+        state_dict=state_dict,
+        device=device,
+        base_address=base_address,
+    )
+
+    tt_output = generate(
+        prompts,
+        tt_model,
+        tokenizer,
+        max_tokens=35,
+    )
+
+    logger.info("Input Prompt")
+    logger.info(prompts)
+    logger.info("Mistral Model output")
+    logger.info(tt_output)
