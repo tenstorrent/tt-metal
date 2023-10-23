736efc8e535b3be419094d97bc5a83ea74ff4f46
Paul Keller <pkeller@tenstorrent.com>
2023-10-23 20:58:00 +0000

    #3289: Optimize/fix dram/l1 noc bank calculations
    Remove the _x and _y tables, replace with the pre=calculated _xy table
    Fix prior dram/l1 noc bank calculations that didn't factor in noc_index

--- tests/tt_metal/tt_metal/test_kernels/ping_legal_l1s.cpp
+++ tests/tt_metal/tt_metal/test_kernels/ping_legal_l1s.cpp
@@ -13,11 +13,10 @@ void kernel_main() {
 
     for (uint32_t id = 0; id < NUM_L1_BANKS; id += 1) {
         uint32_t bank_id = id & (NUM_L1_BANKS - 1);
-        uint32_t noc_x = l1_bank_to_noc_x[bank_id];
-        uint32_t noc_y = l1_bank_to_noc_y[bank_id];
+        uint32_t noc_xy = l1_bank_to_noc_xy[noc_index][bank_id];
         uint32_t l1_address = base_l1_address + bank_to_l1_offset[bank_id];
 
-        uint64_t noc_addr = get_noc_addr(noc_x, noc_y, l1_address);
+        uint64_t noc_addr = get_noc_addr_helper(noc_xy, l1_address);
 
         noc_async_read(noc_addr, intermediate_l1_addr, size_bytes);
         noc_async_read_barrier();
