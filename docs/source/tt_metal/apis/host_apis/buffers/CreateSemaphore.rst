545f0dddd5d099a45e8c6218a7ff7713c739d465
Bill Teng <bteng@tenstorrent.com>
2023-10-24 02:06:03 +0000

    #3330: merge 2 CreateSemaphore functions into 1

--- docs/source/tt_metal/apis/host_apis/buffers/CreateSemaphore.rst
+++ docs/source/tt_metal/apis/host_apis/buffers/CreateSemaphore.rst
@@ -1,6 +1,4 @@
 CreateSemaphore
 ================
 
-.. doxygenfunction:: CreateSemaphore(Program &program, const CoreRange &core_range, uint32_t initial_value)
-
-.. doxygenfunction:: CreateSemaphore(Program &program, const CoreRangeSet &core_range_set, uint32_t initial_value)
+.. doxygenfunction:: CreateSemaphore(Program &program, const std::variant<CoreRange,CoreRangeSet> &core_spec, uint32_t initial_value)
--- tt_metal/host_api.hpp
+++ tt_metal/host_api.hpp
@@ -129,26 +129,13 @@ CircularBufferConfig &GetCircularBufferConfig(Program &program, CircularBufferID
  *
  * Return value: Semaphore address (uint32_t)
  *
- * | Argument      | Description                                          | Type                                                  | Valid Range                                              | Required |
- * |---------------|------------------------------------------------------|-------------------------------------------------------|----------------------------------------------------------|----------|
- * | program       | The program to which semaphore will be added to      | Program &                                             |                                                          | Yes      |
- * | core_range    | Range of the Tensix co-ordinates using the semaphore | const CoreRange & (std::pair<CoreCoord, CoreCoord>)   | Pair of logical coords where first coord <= second coord | Yes      |
- * | initial_value | Initial value of the semaphore                       | uint32_t                                              |                                                          | Yes      |
+ * | Argument      | Description                                          | Type                                                      | Valid Range  | Required |
+ * |---------------|------------------------------------------------------|-----------------------------------------------------------|--------------|----------|
+ * | program       | The program to which semaphore will be added to      | Program &                                                 |              | Yes      |
+ * | core_spec     | Range of the Tensix co-ordinates using the semaphore | const std::variant<CoreRange,CoreRangeSet> &              |              | Yes      |
+ * | initial_value | Initial value of the semaphore                       | uint32_t                                                  |              | Yes      |
  */
-uint32_t CreateSemaphore(Program &program, const CoreRange &core_range, uint32_t initial_value);
-
-/**
- * Initializes semaphore on all cores within core range (inclusive). Each core can have up to four 32B semaphores.
- *
- * Return value: Semaphore address (uint32_t)
- *
- * | Argument       | Description                                                 | Type                   | Valid Range                                               | Required |
- * |----------------|-------------------------------------------------------------|------------------------|-----------------------------------------------------------|----------|
- * | program        | The program to which semaphore will be added to             | Program &              |                                                           | Yes      |
- * | core_range_set    | Set of Range of the Tensix co-ordinates using the semaphore | const CoreRangeSet &   | Pairs of logical coords where first coord <= second coord | Yes      |
- * | initial_value  | Initial value of the semaphore                              | uint32_t               |                                                           | Yes      |
- */
-uint32_t CreateSemaphore(Program &program, const CoreRangeSet &core_range_set, uint32_t initial_value);
+uint32_t CreateSemaphore(Program &program, const std::variant<CoreRange,CoreRangeSet> &core_spec, uint32_t initial_value);
 
 /**
 *  Allocates a DRAM or L1 buffer on device
