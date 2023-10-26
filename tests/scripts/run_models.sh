badef8249819cf1ec7cd21440341c070172be806
farbabi <farbabi@tenstorrent.com>
2023-10-26 17:03:43 +0000

    #3116: add tests for WH to pipeline | split model tests  for GS and WH

--- tests/scripts/run_models.sh
+++ tests/scripts/run_models.sh
@@ -1,19 +1,12 @@
 #/bin/bash
-
 set -eo pipefail
-
 if [[ -z "$TT_METAL_HOME" ]]; then
   echo "Must provide TT_METAL_HOME in environment" 1>&2
   exit 1
 fi
-
 cd $TT_METAL_HOME
-
 export PYTHONPATH=$TT_METAL_HOME
-
 env pytest models/experimental/whisper -k whisper_attention
-env pytest models/experimental/whisper -k WhipserEncoderLayer_inference
-env pytest models/experimental/whisper -k WhipserEncoder_inference
 env pytest models/experimental/whisper -k WhipserDecoderLayer_inference
 env pytest models/experimental/whisper -k WhipserDecoder_inference
 env pytest models/experimental/whisper -k whisper_model
@@ -39,13 +32,22 @@ env pytest models/demos/metal_BERT_large_15/tests/test_demo.py::test_demo_squadv
 
 env pytest models/experimental/t5/tests -k t5_dense_act_dense
 env pytest models/experimental/t5/tests -k t5_layer_norm
-env pytest models/experimental/t5/tests -k t5_attention
 env pytest models/experimental/t5/tests -k t5_layer_ff
-env pytest models/experimental/t5/tests -k t5_layer_self_attention
-env pytest models/experimental/t5/tests -k t5_layer_cross_attention
-env pytest models/experimental/t5/tests -k t5_block
-env pytest models/experimental/t5/tests -k t5_stack
-env pytest models/experimental/t5/tests -k t5_model
+
+env pytest models/experimental/roberta/tests -k roberta_self_attention
+env pytest models/experimental/roberta/tests -k roberta_self_output
+env pytest models/experimental/roberta/tests -k roberta_attention
+env pytest models/experimental/roberta/tests -k roberta_intermediate
+env pytest models/experimental/roberta/tests -k roberta_output
+env pytest models/experimental/roberta/tests -k roberta_pooler
+env pytest models/experimental/roberta/tests -k roberta_lm_head
+env pytest models/experimental/roberta/tests -k roberta_classification_head
+
+env pytest models/experimental/bloom/tests -k baddbmm
+env pytest models/experimental/bloom/tests -k bloom_attention
+env pytest models/experimental/bloom/tests -k bloom_gelu_forward
+env pytest models/experimental/bloom/tests -k bloom_merge_heads
+env pytest models/experimental/bloom/tests -k bloom_mlp
 
 env pytest models/experimental/synthetic_gradients/tests -k test_batchnorm1d
 env pytest models/experimental/synthetic_gradients/tests -k test_linear
@@ -54,44 +56,13 @@ env pytest models/experimental/synthetic_gradients/tests -k test_full_inference
 
 env pytest models/experimental/llama_old/tests -k llama_layer_norm
 env pytest models/experimental/llama_old/tests -k llama_mlp
-env pytest models/experimental/llama_old/tests -k llama_attention
 env pytest models/experimental/llama_old/tests -k llama_decoder
 
-env pytest models/demos/falcon7b/tests/test_falcon_end_to_end.py::test_FalconCausalLM_end_to_end_with_program_cache[BFLOAT16-L1-falcon_7b-layers_32-prefill_seq128]
-env pytest models/demos/falcon7b/tests/test_falcon_end_to_end.py::test_FalconCausalLM_end_to_end_with_program_cache[BFLOAT16-L1-falcon_7b-layers_32-decode_batch32]
-
 env pytest models/experimental/lenet/tests -k test_lenet_inference
-env pytest models/experimental/convnet_mnist/tests -k mnist_inference
-env pytest models/experimental/mnist/tests -k mnist_inference
 
-env pytest models/experimental/roberta/tests -k roberta_self_attention
-env pytest models/experimental/roberta/tests -k roberta_self_output
-env pytest models/experimental/roberta/tests -k roberta_attention
-env pytest models/experimental/roberta/tests -k roberta_intermediate
-env pytest models/experimental/roberta/tests -k roberta_output
-env pytest models/experimental/roberta/tests -k roberta_layer
-env pytest models/experimental/roberta/tests -k roberta_encoder
-env pytest models/experimental/roberta/tests -k roberta_pooler
-env pytest models/experimental/roberta/tests -k roberta_model
-env pytest models/experimental/roberta/tests -k roberta_lm_head
-env pytest models/experimental/roberta/tests -k roberta_for_masked_lm
-env pytest models/experimental/roberta/tests -k roberta_for_qa
-env pytest models/experimental/roberta/tests -k roberta_for_sequence_classification
-env pytest models/experimental/roberta/tests -k roberta_classification_head
-env pytest models/experimental/roberta/tests -k roberta_for_token_classification
-env pytest models/experimental/roberta/tests -k roberta_for_multiple_choice
+env pytest models/experimental/convnet_mnist/tests -k mnist_inference
 
 env pytest models/experimental/yolov5/tests -k Yolov5_detection_model
-
-env pytest models/experimental/bloom/tests -k baddbmm
-env pytest models/experimental/bloom/tests -k bloom_attention
-env pytest models/experimental/bloom/tests -k bloom_block
-env pytest models/experimental/bloom/tests -k bloom_gelu_forward
-env pytest models/experimental/bloom/tests -k bloom_merge_heads
-env pytest models/experimental/bloom/tests -k bloom_mlp
-env pytest models/experimental/bloom/tests -k bloom_model
-env pytest models/experimental/bloom/tests -k bloom_causal_lm
-
 env pytest models/experimental/yolov3 -k conv2d_module
 env pytest models/experimental/yolov3 -k conv_module
 env pytest models/experimental/yolov3 -k concat_module
@@ -103,6 +74,64 @@ env pytest models/experimental/yolov3 -k upsample_module
 env pytest models/experimental/efficientnet/tests -k efficientnet_b0_model_real
 env pytest models/experimental/efficientnet/tests -k efficientnet_v2_s_model_real
 env pytest models/experimental/efficientnet/tests -k efficientnet_lite0_model_real
+
+env pytest tests/models/falcon/tests/test_falcon_end_to_end.py::test_FalconCausalLM_end_to_end_with_program_cache[BFLOAT16-L1-falcon_7b-layers_32-prefill_seq128]
+
+env pytest models/experimental/stable_diffusion/tests/test_embedding.py
+
+
+# GRAYSKULL ONLY
+
+if [[ $ARCH_NAME == "grayskull" ]]; then
+
+env pytest models/experimental/whisper -k WhipserEncoderLayer_inference
+env pytest models/experimental/whisper -k WhipserEncoder_inference
+env pytest models/experimental/whisper -k WhipserDecoder_inference
+env pytest models/experimental/whisper -k whisper_model
+env pytest models/experimental/whisper -k whisper_for_audio_classification
+env pytest models/experimental/whisper -k whisper_for_conditional_generation
+
+env pytest models/experimental/t5/tests -k t5_attention
+env pytest models/experimental/t5/tests -k t5_layer_self_attention
+env pytest models/experimental/t5/tests -k t5_layer_cross_attention
+env pytest models/experimental/t5/tests -k t5_block
+env pytest models/experimental/t5/tests -k t5_stack
+env pytest models/experimental/t5/tests -k t5_model
+
+env pytest models/experimental/roberta/tests -k roberta_layer
+env pytest models/experimental/roberta/tests -k roberta_encoder
+env pytest models/experimental/roberta/tests -k roberta_model
+env pytest models/experimental/roberta/tests -k roberta_for_masked_lm
+env pytest models/experimental/roberta/tests -k roberta_for_qa
+env pytest models/experimental/roberta/tests -k roberta_for_sequence_classification
+env pytest models/experimental/roberta/tests -k roberta_for_token_classification
+env pytest models/experimental/roberta/tests -k roberta_for_multiple_choice
+
+env pytest models/experimental/bloom/tests -k bloom_block
+env pytest models/experimental/bloom/tests -k bloom_model
+env pytest models/experimental/bloom/tests -k bloom_causal_lm
+
+
+# Bad tests, don't enable: Hanging post commit 8/24/23 debug war room session, see PR#2297, PR#2301
+# env pytest tests/models/stable_diffusion/tests/test_cross_attn_down_block.py -k test_run_cross_attn_down_block_real_input_inference
+# env pytest tests/models/stable_diffusion/tests/test_cross_attn_up_block.py -k test_run_cross_attn_up_block_real_input_inference
+# env pytest tests/models/stable_diffusion/tests/test_downblock_2d.py -k test_run_downblock_real_input_inference
+# env pytest tests/models/stable_diffusion/tests/test_unet_mid_block.py -k test_run_unet_mid_block_real_input_inference
+# env pytest tests/models/stable_diffusion/tests/test_upblock_2d.py -k test_run_upblock_real_input_inference
+# env pytest tests/models/stable_diffusion/tests -k test_unbatched_stable_diffusion
+
+# Split bert tests as a workaround to issue #2892
+env pytest models/demos/metal_BERT_large_15/tests/test_bert_batch_dram.py::test_bert_batch_dram -k BFLOAT8_B
+env pytest models/demos/metal_BERT_large_15/tests/test_bert_batch_dram.py::test_bert_batch_dram -k BFLOAT16
+env pytest models/demos/metal_BERT_large_15/tests/test_bert_batch_dram.py::test_bert_batch_dram -k MIXED_PRECISION
+env pytest models/demos/metal_BERT_large_15/tests/test_bert_batch_dram.py::test_bert_batch_dram_with_program_cache
+env pytest models/demos/metal_BERT_large_15/tests/test_demo.py
+
+env pytest models/experimental/llama_old/tests -k llama_attention
+
+env pytest tests/models/falcon/tests/test_falcon_end_to_end.py::test_FalconCausalLM_end_to_end_with_program_cache[BFLOAT16-L1-falcon_7b-layers_32-decode_batch32] # failed on WH: RuntimeError: TT_ASSERT @ tt_eager/tt_dnn/op_library/transformer_tms/transformer_tms.cpp:171: (this->compute_with_storage_grid_size.x <= device_compute_with_storage_grid_size
+
+env pytest models/experimental/mnist/tests -k mnist_inference
 
 env pytest models/experimental/nanogpt/tests -k nanogpt_model_real
 
