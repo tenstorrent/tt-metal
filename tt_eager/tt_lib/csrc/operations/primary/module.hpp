b84e271774ca14523cd138d80e4a83320baa284e
Austin Ho <aho@tenstorrent.com>
2023-10-27 17:26:42 +0000

    #0: Add implicit pybinding for tuple of int -> CoreCoord

--- tt_eager/tt_lib/csrc/operations/primary/module.hpp
+++ tt_eager/tt_lib/csrc/operations/primary/module.hpp
@@ -34,29 +34,9 @@ void py_module(py::module& m_primary) {
 
     py::class_<MatmulMultiCoreReuseProgramConfig>(m_primary, "MatmulMultiCoreReuseProgramConfig")
         .def(
-            py::init<>(
-                [] (
-                    std::tuple<std::size_t, std::size_t> compute_with_storage_grid_size,
-                    std::size_t in0_block_w,
-                    std::size_t out_subblock_h,
-                    std::size_t out_subblock_w,
-                    std::size_t per_core_M,
-                    std::size_t per_core_N
-                ) {
-
-                    return MatmulMultiCoreReuseProgramConfig{
-                        .compute_with_storage_grid_size={std::get<0>(compute_with_storage_grid_size), std::get<1>(compute_with_storage_grid_size)},
-                        .in0_block_w=in0_block_w,
-                        .out_subblock_h=out_subblock_h,
-                        .out_subblock_w=out_subblock_w,
-                        .per_core_M=per_core_M,
-                        .per_core_N=per_core_N,
-                    };
-
-                }
-            ),
+            py::init<CoreCoord, std::size_t, std::size_t, std::size_t, std::size_t, std::size_t>(),
             py::kw_only(),
-            py::arg("compute_with_storage_grid_size").noconvert(),
+            py::arg("compute_with_storage_grid_size"),
             py::arg("in0_block_w").noconvert(),
             py::arg("out_subblock_h").noconvert(),
             py::arg("out_subblock_w").noconvert(),
@@ -65,33 +45,9 @@ void py_module(py::module& m_primary) {
         );
     py::class_<MatmulMultiCoreReuseMultiCastProgramConfig>(m_primary, "MatmulMultiCoreReuseMultiCastProgramConfig")
         .def(
-            py::init<>(
-                [] (
-                    std::tuple<std::size_t, std::size_t> compute_with_storage_grid_size,
-                    std::size_t in0_block_w,
-                    std::size_t out_subblock_h,
-                    std::size_t out_subblock_w,
-                    std::size_t per_core_M,
-                    std::size_t per_core_N,
-                    bool transpose_mcast,
-                    std::optional<UnaryWithParam> fused_activation
-                ) {
-
-                    return MatmulMultiCoreReuseMultiCastProgramConfig{
-                        .compute_with_storage_grid_size={std::get<0>(compute_with_storage_grid_size), std::get<1>(compute_with_storage_grid_size)},
-                        .in0_block_w=in0_block_w,
-                        .out_subblock_h=out_subblock_h,
-                        .out_subblock_w=out_subblock_w,
-                        .per_core_M=per_core_M,
-                        .per_core_N=per_core_N,
-                        .transpose_mcast=transpose_mcast,
-                        .fused_activation=fused_activation,
-                    };
-
-                }
-            ),
+            py::init<CoreCoord, std::size_t, std::size_t, std::size_t, std::size_t, std::size_t, bool, std::optional<UnaryWithParam>>(),
             py::kw_only(),
-            py::arg("compute_with_storage_grid_size").noconvert(),
+            py::arg("compute_with_storage_grid_size"),
             py::arg("in0_block_w").noconvert(),
             py::arg("out_subblock_h").noconvert(),
             py::arg("out_subblock_w").noconvert(),
@@ -104,35 +60,9 @@ void py_module(py::module& m_primary) {
 
     py::class_<MatmulMultiCoreReuseMultiCast1DProgramConfig>(m_primary, "MatmulMultiCoreReuseMultiCast1DProgramConfig")
         .def(
-            py::init<>(
-                [] (
-                    std::tuple<std::size_t, std::size_t> compute_with_storage_grid_size,
-                    std::size_t in0_block_w,
-                    std::size_t out_subblock_h,
-                    std::size_t out_subblock_w,
-                    std::size_t per_core_M,
-                    std::size_t per_core_N,
-                    bool fuse_batch,
-                    std::optional<UnaryWithParam> fused_activation,
-                    bool mcast_in0
-                ) {
-
-                    return MatmulMultiCoreReuseMultiCast1DProgramConfig{
-                        .compute_with_storage_grid_size={std::get<0>(compute_with_storage_grid_size), std::get<1>(compute_with_storage_grid_size)},
-                        .in0_block_w=in0_block_w,
-                        .out_subblock_h=out_subblock_h,
-                        .out_subblock_w=out_subblock_w,
-                        .per_core_M=per_core_M,
-                        .per_core_N=per_core_N,
-                        .fuse_batch=fuse_batch,
-                        .fused_activation=fused_activation,
-                        .mcast_in0=mcast_in0
-                    };
-
-                }
-            ),
+            py::init<CoreCoord, std::size_t, std::size_t, std::size_t, std::size_t, std::size_t, bool, std::optional<UnaryWithParam>, bool>(),
             py::kw_only(),
-            py::arg("compute_with_storage_grid_size").noconvert(),
+            py::arg("compute_with_storage_grid_size"),
             py::arg("in0_block_w").noconvert(),
             py::arg("out_subblock_h").noconvert(),
             py::arg("out_subblock_w").noconvert(),
