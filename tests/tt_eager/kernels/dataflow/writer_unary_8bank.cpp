11f9f0f9821acba9e2d93bcf8133ec9561164dca
Raymond Kim <rkim@tenstorrent.com>
2023-10-27 00:41:34 +0000

    #2749: Fix some remaining tt_metal and tt_eager integration tests

---
+++ tests/tt_eager/kernels/dataflow/writer_unary_8bank.cpp
@@ -0,0 +1,36 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "dataflow_api.h"
+
+
+void kernel_main() {
+    uint32_t dst_addr  = get_arg_val<uint32_t>(0);
+    uint32_t num_tiles = get_arg_val<uint32_t>(3); // Index 3 to match with regular writer_unary
+
+    constexpr uint32_t cb_id_out0 = 16;
+    constexpr uint32_t onetile = 1;
+    uint32_t tile_bytes = get_tile_size(cb_id_out0);
+
+    #ifdef KERNEL_COMPILE_TIME_ARG_0
+    constexpr bool write_to_dram = get_compile_time_arg_val(0);
+    #else
+    constexpr bool write_to_dram = true;
+    #endif
+
+    const InterleavedPow2AddrGen<write_to_dram> s = { dst_addr, 11 };
+
+    for (uint32_t i = 0; i<num_tiles; i ++) {
+        uint64_t dst_noc_addr = get_noc_addr(i, s);
+
+        cb_wait_front(cb_id_out0, onetile);
+        uint32_t l1_read_addr = get_read_ptr(cb_id_out0);
+
+        noc_async_write(l1_read_addr, dst_noc_addr, tile_bytes);
+
+        noc_async_write_barrier();
+
+        cb_pop_front(cb_id_out0, onetile);
+     }
+}
