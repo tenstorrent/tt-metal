7acff015ebb6602910e61c1ef8d3cd2095a235e4
Dongjin Na <dongjin.na@moreh.io>
2023-10-30 05:17:27 -0400

    #3364: Add inline functions in moreh_helper_functions

--- tt_eager/tt_dnn/op_library/moreh_helper_functions.hpp
+++ tt_eager/tt_dnn/op_library/moreh_helper_functions.hpp
@@ -22,6 +22,28 @@ inline bool is_dram(const Tensor &input_tensor);
 inline bool is_dram(const std::optional<const Tensor> input_tensor);
 inline bool is_dram(const Buffer *b);
 
+inline bool is_scalar(const Tensor &tensor) {
+    const auto &shape = tensor.shape().without_padding();
+    return (shape[0] == 1 && shape[1] == 1 && shape[2] == 1 && shape[3] == 1);
+}
+
+inline bool is_1d_tensor(const Tensor &tensor) {
+    const auto &shape = tensor.shape().without_padding();
+    return (shape[0] == 1 && shape[1] == 1 && shape[2] == 1);
+}
+
+inline bool is_same_shape(const Tensor &tensor_a, const Tensor &tensor_b) {
+    const auto &tensor_a_shape = tensor_a.shape().without_padding();
+    const auto &tensor_b_shape = tensor_b.shape().without_padding();
+    return (tensor_a_shape == tensor_b_shape);
+}
+
+inline bool is_same_batch_shape(const Tensor &tensor_a, const Tensor &tensor_b) {
+    const auto &tensor_a_shape = tensor_a.shape().without_padding();
+    const auto &tensor_b_shape = tensor_b.shape().without_padding();
+    return (tensor_a_shape[0] == tensor_b_shape[0] && tensor_a_shape[1] == tensor_b_shape[1]);
+}
+
 inline std::tuple<CoreRangeSet, CoreRangeSet, CoreRangeSet> add_core_offset(
     CoreRangeSet all_cores, CoreRangeSet core_group_1, CoreRangeSet core_group_2, uint32_t offset_x, uint32_t offset_y);
 
