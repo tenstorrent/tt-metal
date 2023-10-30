55d401d10c97ce9fd7a8de32f93d699631c4c593
“Nenad <npetrovic@tenstorrent.com>
2023-10-30 10:22:00 +0000

    #2793: Refactor add_layernorm_noweights test

--- tests/tt_eager/python_api_testing/unit_testing/test_add_layernorm_noweights.py
+++ tests/tt_eager/python_api_testing/unit_testing/test_add_layernorm_noweights.py
@@ -8,6 +8,7 @@ import sys
 import time
 import os
 from loguru import logger
+import random
 
 f = f"{Path(__file__).parent}"
 sys.path.append(f"{f}/../../../../..")
@@ -17,175 +18,59 @@ import torch
 
 import tt_lib as ttl
 
-from tt_lib.utils import (
-    pad_weight,
-    tilize_to_list,
-    untilize,
-    is_close,
-)
+from tests.tt_eager.python_api_testing.sweep_tests import pytorch_ops
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc
 from tests.tt_eager.python_api_testing.sweep_tests.common import is_wormhole_b0, skip_for_wormhole_b0
+from tests.tt_eager.python_api_testing.sweep_tests.tt_lib_ops import add_layernorm_noweights as tt_add_layernorm_noweights
 
-# This ref implementation is only here for debugging
-def ref_ln(x, gamma, beta=None, epsilon=1e-5, b=None):
-    # prints a tile slice with these tile coord range
-    torch.set_printoptions(
-        precision=2, threshold=1000, sci_mode=False, edgeitems=8, linewidth=480
+
+def run_add_layernorm_noweights_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
+    torch.manual_seed(data_seed)
+
+    if in_mem_config == "SYSTEM_MEMORY":
+        in_mem_config = None
+
+    x = torch.Tensor(size=input_shape).uniform_(-10.0, 10.0)
+    x_ref = x.detach().clone()
+
+
+    y = torch.Tensor(size=input_shape).uniform_(-10.0, 10.0)
+    y_ref = y.detach().clone()
+
+    # get ref result
+    ref_value = pytorch_ops.add_layernorm_noweights(x_ref, y_ref)
+
+    tt_result = tt_add_layernorm_noweights  (
+        x=x,
+        y=y,
+        device=device,
+        dtype=dtype,
+        layout=dlayout,
+        input_mem_config=in_mem_config,
+        output_mem_config=out_mem_config
     )
-    sth = 16
-    stw = 16
-    ph0, ph1, pw0, pw1 = 1, 2, 0, 1
-    # ph0, ph1, pw0, pw1 = 0, 1, 0, 1
 
-    # print("x.shape=", x.shape)
-    # print("eps=", epsilon)
-    # print(f"slice={ph0}:{ph1}, {pw0}:{pw1}")
-    # print("Ref x=\n", x[0, 0, ph0 * 32 : ph1 * 32 : sth, pw0 * 32 : pw1 * 32 : stw])
-    # print(
-    #     "Ref a=\n", (x - b)[0, 0, ph0 * 32 : ph1 * 32 : sth, pw0 * 32 : pw1 * 32 : stw]
-    # )
-    # print("Ref b=\n", b[0, 0, ph0 * 32 : ph1 * 32 : sth, pw0 * 32 : pw1 * 32 : stw])
-    mean = x.mean(dim=-1, keepdim=True)
-    # print("Ref Ex=\n", mean[0, 0, ph0*32 : ph1*32 : sth, 0*32 : 1*32 : stw])
-    xmm = x - mean
-    # print("Ref xmm=\n", xmm[0, 0, ph0*32 : ph1*32 : st, pw0*32 : pw1*32 : st])
-    xmm2 = xmm**2
-    # print("Ref xmm2=\n", xmm2[0, 0, ph0*32 : ph1*32 : sth, pw0*32 : pw1*32 : stw])
-    exmm2 = xmm2.mean(dim=-1, keepdim=True)
-    # print("Ref exmm2=\n", exmm2[0, 0, ph0*32 : ph1*32 : sth, 0*32 : 1*32 : stw])
+    # compare tt and golden outputs
+    success, pcc_value = comp_pcc(ref_value, tt_result,)
+    logger.debug(pcc_value)
 
-    std = (exmm2 + epsilon).sqrt()
-    # print("Ref sqrt_exmm2=\n", std[0, 0, ph0*32 : ph1*32 : st, 0*32 : 1*32 : st])
-
-    invstd = 1.0 / std
-    # print("Ref 1/sqrt_exmm2=\n", invstd[0, 0, ph0*32 : ph1*32 : st, 0*32 : 1*32 : st])
-    y1 = xmm * invstd
-    # print("Ref y*1+0=\n", y1[0, 0, ph0*32 : ph1*32 : st, pw0*32 : pw1*32 : st])
-    y = y1.clone()
-    if gamma is not None:
-        # print("yshape=", y.shape)
-        # print("gshape=", gamma.shape)
-        y *= gamma
-    if beta is not None:
-        y += beta
-    # y = gamma.repeat(x.shape[0], x.shape[1], x.shape[2], x.shape[3]//gamma.shape[3]) # Debug gamma
-    return y, mean, exmm2, std, invstd, y1
+    assert success
 
 
-def ref_layernorm(x, eps, gamma, beta, H, W):
-    lnorm = torch.nn.LayerNorm((W,), eps)
-    lnorm.weight = torch.nn.Parameter(torch.full((W,), gamma))
-    lnorm.bias = torch.nn.Parameter(torch.full((W,), beta))
-    return lnorm(x)
-
-
-def run_layernorm_tests(dtype, in0_mem_config, out_mem_config, device):
-    torch.manual_seed(1234)
-
-
-    tensor = ttl.tensor
-    dev = device
-
-    epsf = 1e-2
-
-    test_dims = ((1, 9, 384, 1024),)
-    for N, C, H, W in test_dims:
-
-        for nrepeat in range(0, 1):
-            gamma = torch.ones(1, 1, 1, W)
-            beta = torch.zeros(1, 1, 1, W)
-            gamma = torch.rand(1, 1, 1, W) * 2 - 1
-            gammah32 = tilize_to_list(pad_weight(gamma))
-            ttgamma = tensor.Tensor(
-                gammah32,
-                [1, 1, 32, W],
-                dtype,
-                tensor.Layout.TILE,
-                dev,
-                in0_mem_config,
-            )
-
-            beta = torch.rand(1, 1, 1, W) * 2.0 - 1.1
-            betah32 = tilize_to_list(pad_weight(beta))
-            ttbeta = tensor.Tensor(
-                betah32,
-                [1, 1, 32, W],
-                dtype,
-                tensor.Layout.TILE,
-                dev,
-                in0_mem_config,
-            )
-
-            x = torch.rand((N, C, H, W)) * 2 - 0.95
-            y = torch.rand((N, C, H, W)) * 2 - 0.8
-
-            ttx = tensor.Tensor(
-                tilize_to_list(x),
-                [N, C, H, W],
-                dtype,
-                tensor.Layout.TILE,
-                dev,
-                in0_mem_config,
-            )
-            tty = tensor.Tensor(
-                tilize_to_list(y),
-                [N, C, H, W],
-                dtype,
-                tensor.Layout.TILE,
-                dev,
-                in0_mem_config,
-            )
-
-            logger.info("Running add_LN_GB")
-            ttz = tensor.add_layernorm(
-                ttx, tty, epsf, None, None, out_mem_config
-            )
-
-            logger.info("Done")
-
-            assert ttx.memory_config().buffer_type == in0_mem_config.buffer_type
-            assert tty.memory_config().buffer_type == in0_mem_config.buffer_type
-            assert ttz.memory_config().buffer_type == out_mem_config.buffer_type
-
-            logger.debug(f"ttx is on: {ttx.memory_config().buffer_type}")
-            logger.debug(f"tty is on: {tty.memory_config().buffer_type}")
-            logger.debug(f"ttz is on: {ttz.memory_config().buffer_type}")
-
-            t2_data = ttz.cpu().to_torch()
-
-            tt_got_back = torch.Tensor(t2_data).reshape((N, C, H, W))
-            tt_got_back = untilize(tt_got_back)
-
-            # ref_lnorm = ref_layernorm(x, epsf, gammaf, betaf, H, W)
-            ref_lnorm, _, _, _, _, _ = ref_ln(x + y, gamma, beta, epsf, y)
-
-            time.sleep(0.3)  # sleep to avoid print intermixing with kernel prints
-
-            assert is_close(tt_got_back, ref_lnorm)
-
-
+test_sweep_args=[
+    ( (1, 1, 32, 32), (ttl.tensor.DataType.BFLOAT16, ttl.tensor.DataType.BFLOAT16), (ttl.tensor.Layout.TILE,ttl.tensor.Layout.TILE), (ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1), ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1)), ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM), 38346),
+    ( (1, 1, 32, 32), (ttl.tensor.DataType.BFLOAT16, ttl.tensor.DataType.BFLOAT16), (ttl.tensor.Layout.TILE,ttl.tensor.Layout.TILE), (ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1), ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM)), ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM), 38346),
+    ( (1, 1, 32, 96), (ttl.tensor.DataType.BFLOAT16, ttl.tensor.DataType.BFLOAT16), (ttl.tensor.Layout.TILE,ttl.tensor.Layout.TILE), (ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1), ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM)), ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM), 38346),
+]
 @skip_for_wormhole_b0
 @pytest.mark.parametrize(
-    "out_mem_config",
+    "input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed",
     (
-        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
-        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1),
+        test_sweep_args
     ),
-    ids=["out_DRAM", "out_L1"],
 )
-@pytest.mark.parametrize(
-    "in0_mem_config",
-    (
-        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
-        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1),
-    ),
-    ids=["in0_DRAM", "in0_L1"],
-)
-@pytest.mark.parametrize(
-    "dtype",
-    (ttl.tensor.DataType.BFLOAT16,),
-    ids=["BFLOAT16"],
-)
-def test_layernorm_test(
-    dtype, in0_mem_config, out_mem_config, request, device
+
+def test_add_layernorm_noweights_test(
+    input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device
 ):
-    run_layernorm_tests(dtype, in0_mem_config, out_mem_config, device)
+    run_add_layernorm_noweights_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device)
