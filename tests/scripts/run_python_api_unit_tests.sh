d66da793e018616326498e4c087c490ca6edb850
Austin Ho <aho@tenstorrent.com>
2023-10-31 20:13:26 +0000

    #3516: Skip test_untilize_with_halo_and_max_pool.py for slow dispatch

--- tests/scripts/run_python_api_unit_tests.sh
+++ tests/scripts/run_python_api_unit_tests.sh
@@ -8,7 +8,7 @@ if [[ -z "$TT_METAL_HOME" ]]; then
 fi
 
 if [[ ! -z "$TT_METAL_SLOW_DISPATCH_MODE" ]]; then
-  env pytest $TT_METAL_HOME/tests/tt_eager/python_api_testing/unit_testing/
+  env pytest $(find $TT_METAL_HOME/tests/tt_eager/python_api_testing/unit_testing/ -name 'test_*.py' -a ! -name 'test_untilize_with_halo_and_max_pool.py') -vvv
   env pytest $(find $TT_METAL_HOME/tests/tt_eager/python_api_testing/sweep_tests/pytests/ -name 'test_*.py' -a ! -name 'test_sweep_conv_with_address_map.py') -vvv
 else
   # Need to remove move for time being since failing
