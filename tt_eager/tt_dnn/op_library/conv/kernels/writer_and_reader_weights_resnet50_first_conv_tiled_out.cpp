6416a54d65cb1922639549e386a0abbfe5aa588e
Austin Ho <aho@tenstorrent.com>
2023-10-31 18:42:37 +0000

    #0: Fix bias include in some eager kernels after kernel migration

--- tt_eager/tt_dnn/op_library/conv/kernels/writer_and_reader_weights_resnet50_first_conv_tiled_out.cpp
+++ tt_eager/tt_dnn/op_library/conv/kernels/writer_and_reader_weights_resnet50_first_conv_tiled_out.cpp
@@ -7,7 +7,7 @@
 #include "debug_print.h"
 
 #ifdef FUSE_BIAS
-    #include "kernels/dataflow/reader_bmm_single_core_bias.hpp"
+    #include "tt_eager/tt_dnn/kernels/dataflow/reader_bmm_single_core_bias.hpp"
 #endif
 
 FORCE_INLINE void read_weight_blocks_inner_h_dim(uint32_t cb_id_weight,
