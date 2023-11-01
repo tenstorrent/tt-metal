a3914bbda56ebc8812ddc523f6c2669678ab230d
Raymond Kim <rkim@tenstorrent.com>
2023-11-01 17:30:24 +0000

    #3270: Add eltwise_sfpu example using exp operation

--- tt_metal/programming_examples/module.mk
+++ tt_metal/programming_examples/module.mk
@@ -2,10 +2,11 @@ PROGRAMMING_EXAMPLES_TESTDIR = $(OUT)/programming_examples
 PROGRAMMING_EXAMPLES_OBJDIR = $(OBJDIR)/programming_examples
 
 PROGRAMMING_EXAMPLES_INCLUDES = $(COMMON_INCLUDES)
-PROGRAMMING_EXAMPLES_LDFLAGS = -ltt_metal_detail -ltt_metal -lllrt -ldevice -lbuild_kernels_for_riscv -ldl -lcommon -lprofiler -lstdc++fs -pthread -lyaml-cpp -ltracy
+PROGRAMMING_EXAMPLES_LDFLAGS = -ltt_metal -lprofiler -ldl -lstdc++fs -pthread -lyaml-cpp -ltracy
 
 include $(TT_METAL_HOME)/tt_metal/programming_examples/loopback/module.mk
 include $(TT_METAL_HOME)/tt_metal/programming_examples/eltwise_binary/module.mk
+include $(TT_METAL_HOME)/tt_metal/programming_examples/eltwise_sfpu/module.mk
 include $(TT_METAL_HOME)/tt_metal/programming_examples/profiler/module.mk
 
 
@@ -16,8 +17,10 @@ PROFILER_TESTS += \
 
 programming_examples: programming_examples/loopback \
                       programming_examples/eltwise_binary \
+                      programming_examples/eltwise_sfpu \
 		      $(PROFILER_TESTS)
 
 programming_examples/loopback: $(PROGRAMMING_EXAMPLES_TESTDIR)/loopback;
 programming_examples/eltwise_binary: $(PROGRAMMING_EXAMPLES_TESTDIR)/eltwise_binary;
+programming_examples/eltwise_sfpu: $(PROGRAMMING_EXAMPLES_TESTDIR)/eltwise_sfpu;
 programming_examples/profiler/%: $(PROGRAMMING_EXAMPLES_TESTDIR)/profiler/%;
