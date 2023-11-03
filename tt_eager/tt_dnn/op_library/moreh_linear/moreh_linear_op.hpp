c1551f553a242423322ed24571d8e2cf7c3d4881
Dongjin Na <dongjin.na@moreh.io>
2023-11-03 09:25:29 -0400

    #3281: Add using tt_metal in moreh_linear header

--- tt_eager/tt_dnn/op_library/moreh_linear/moreh_linear_op.hpp
+++ tt_eager/tt_dnn/op_library/moreh_linear/moreh_linear_op.hpp
@@ -15,11 +15,13 @@ namespace tt {
 namespace operations {
 namespace primary {
 
-tt_metal::Tensor moreh_linear(
-    const tt_metal::Tensor& input,
-    const tt_metal::Tensor& weight,
-    std::optional<std::reference_wrapper<const tt_metal::Tensor>> bias = std::nullopt,
-    const tt_metal::MemoryConfig& output_mem_config = tt_metal::operation::DEFAULT_OUTPUT_MEMORY_CONFIG);
+using namespace tt_metal;
+
+Tensor moreh_linear(
+    const Tensor& input,
+    const Tensor& weight,
+    std::optional<std::reference_wrapper<const Tensor>> bias = std::nullopt,
+    const MemoryConfig& output_mem_config = operation::DEFAULT_OUTPUT_MEMORY_CONFIG);
 
 }  // namespace primary
 
