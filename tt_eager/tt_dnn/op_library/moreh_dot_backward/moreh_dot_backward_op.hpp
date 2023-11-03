48a8bf31ad63c867243207a1e60a6d1aaada6929
Dongjin Na <dongjin.na@moreh.io>
2023-11-03 09:15:27 -0400

    #3364: Add using tt_metal in moreh_mamtul_backward header

--- tt_eager/tt_dnn/op_library/moreh_dot_backward/moreh_dot_backward_op.hpp
+++ tt_eager/tt_dnn/op_library/moreh_dot_backward/moreh_dot_backward_op.hpp
@@ -14,36 +14,37 @@ namespace operations {
 
 namespace primary {
 
+using namespace tt_metal;
+
 /*
  * dot product backward
  */
-tt_metal::operation::ProgramWithCallbacks moreh_dot_backward_single_core(
-    const tt_metal::Tensor &output_grad,
-    const tt_metal::Tensor &input,
-    const tt_metal::Tensor &other,
-    const std::optional<const tt_metal::Tensor> &input_grad,
-    const std::optional<const tt_metal::Tensor> &other_grad);
+operation::ProgramWithCallbacks moreh_dot_backward_single_core(
+    const Tensor &output_grad,
+    const Tensor &input,
+    const Tensor &other,
+    const std::optional<const Tensor> &input_grad,
+    const std::optional<const Tensor> &other_grad);
 
 struct MorehDotBackward {
     void validate(
-        const std::vector<tt_metal::Tensor> &inputs,
-        const std::vector<std::optional<const tt_metal::Tensor>> &optional_inputs) const;
-    std::vector<tt_metal::Shape> compute_output_shapes(const std::vector<tt_metal::Tensor> &inputs) const;
-    std::vector<tt_metal::Tensor> create_output_tensors(const std::vector<tt_metal::Tensor> &inputs) const;
-    tt_metal::operation::ProgramWithCallbacks create_program(
-        const std::vector<tt_metal::Tensor> &inputs,
-        const std::vector<std::optional<const tt_metal::Tensor>> &optional_inputs,
-        std::vector<tt_metal::Tensor> &outputs) const;
+        const std::vector<Tensor> &inputs, const std::vector<std::optional<const Tensor>> &optional_inputs) const;
+    std::vector<Shape> compute_output_shapes(const std::vector<Tensor> &inputs) const;
+    std::vector<Tensor> create_output_tensors(const std::vector<Tensor> &inputs) const;
+    operation::ProgramWithCallbacks create_program(
+        const std::vector<Tensor> &inputs,
+        const std::vector<std::optional<const Tensor>> &optional_inputs,
+        std::vector<Tensor> &outputs) const;
     tt::stl::reflection::Attributes attributes() const;
 };
 
-[[maybe_unused]] std::vector<std::variant<tt_metal::Tensor, char *>> moreh_dot_backward(
-    const tt_metal::Tensor &output_grad,
-    const tt_metal::Tensor &input,
-    const tt_metal::Tensor &other,
-    std::optional<std::reference_wrapper<const tt_metal::Tensor>> input_grad = std::nullopt,
-    std::optional<std::reference_wrapper<const tt_metal::Tensor>> other_grad = std::nullopt,
-    const tt_metal::MemoryConfig &mem_config = tt_metal::operation::DEFAULT_OUTPUT_MEMORY_CONFIG);
+[[maybe_unused]] std::vector<std::variant<Tensor, char *>> moreh_dot_backward(
+    const Tensor &output_grad,
+    const Tensor &input,
+    const Tensor &other,
+    std::optional<std::reference_wrapper<const Tensor>> input_grad = std::nullopt,
+    std::optional<std::reference_wrapper<const Tensor>> other_grad = std::nullopt,
+    const MemoryConfig &mem_config = operation::DEFAULT_OUTPUT_MEMORY_CONFIG);
 
 }  // namespace primary
 
--- tt_eager/tt_dnn/op_library/moreh_matmul_backward/moreh_matmul_backward_op.hpp
+++ tt_eager/tt_dnn/op_library/moreh_matmul_backward/moreh_matmul_backward_op.hpp
@@ -14,28 +14,29 @@ namespace operations {
 
 namespace primary {
 
-tt_metal::operation::ProgramWithCallbacks moreh_sum_multi_core(
-    const tt_metal::Tensor &input, const tt_metal::Tensor &output);
+using namespace tt_metal;
+
+operation::ProgramWithCallbacks moreh_sum_multi_core(const Tensor &input, const Tensor &output);
 
 struct MorehSum {
-    void validate(const std::vector<tt_metal::Tensor> &inputs) const;
-    std::vector<tt_metal::Shape> compute_output_shapes(const std::vector<tt_metal::Tensor> &inputs) const;
-    std::vector<tt_metal::Tensor> create_output_tensors(const std::vector<tt_metal::Tensor> &inputs) const;
-    tt_metal::operation::ProgramWithCallbacks create_program(
-        const std::vector<tt_metal::Tensor> &inputs, std::vector<tt_metal::Tensor> &outputs) const;
+    void validate(const std::vector<Tensor> &inputs) const;
+    std::vector<Shape> compute_output_shapes(const std::vector<Tensor> &inputs) const;
+    std::vector<Tensor> create_output_tensors(const std::vector<Tensor> &inputs) const;
+    operation::ProgramWithCallbacks create_program(
+        const std::vector<Tensor> &inputs, std::vector<Tensor> &outputs) const;
     tt::stl::reflection::Attributes attributes() const;
 };
 
 /*
  * GENERAL matmul_backward
  */
-[[maybe_unused]] std::vector<std::variant<tt_metal::Tensor, char *>> moreh_matmul_backward(
-    const tt_metal::Tensor &output_grad,
-    const tt_metal::Tensor &input,
-    const tt_metal::Tensor &other,
-    std::optional<std::reference_wrapper<const tt_metal::Tensor>> input_grad = std::nullopt,
-    std::optional<std::reference_wrapper<const tt_metal::Tensor>> other_grad = std::nullopt,
-    const tt_metal::MemoryConfig &mem_config = tt_metal::operation::DEFAULT_OUTPUT_MEMORY_CONFIG);
+[[maybe_unused]] std::vector<std::variant<Tensor, char *>> moreh_matmul_backward(
+    const Tensor &output_grad,
+    const Tensor &input,
+    const Tensor &other,
+    std::optional<std::reference_wrapper<const Tensor>> input_grad = std::nullopt,
+    std::optional<std::reference_wrapper<const Tensor>> other_grad = std::nullopt,
+    const MemoryConfig &mem_config = operation::DEFAULT_OUTPUT_MEMORY_CONFIG);
 
 }  // namespace primary
 
