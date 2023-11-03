c2e980229e8c30ec8d9f56af2958195c629555d0
seunghwan100 <seunghwan100@github.com>
2023-11-03 12:47:24 +0000

    #2794: Make is_dram to inline function

--- tt_eager/tt_dnn/op_library/moreh_helper_functions.hpp
+++ tt_eager/tt_dnn/op_library/moreh_helper_functions.hpp
@@ -17,12 +17,38 @@ namespace primary {
 
 using namespace tt::tt_metal;
 
-bool is_dram(const Tensor &input_tensor);
-bool is_dram(const std::optional<const Tensor> input_tensor);
-bool is_dram(const std::optional<std::reference_wrapper<const Tensor>> input_tensor);
-bool is_dram(const Buffer *b);
+inline bool is_dram(const Tensor &tensor) { return tensor.memory_config().buffer_type == BufferType::DRAM; }
+inline bool is_dram(const std::optional<const Tensor> tensor) {
+    return tensor.has_value() ? is_dram(tensor.value()) : true;
+}
+inline bool is_dram(const std::optional<std::reference_wrapper<const Tensor>> tensor) {
+    return tensor.has_value() ? is_dram(tensor->get()) : true;
+}
+inline bool is_dram(const Buffer *buffer) { return buffer->buffer_type() == BufferType::DRAM; }
 
-inline std::tuple<CoreRangeSet, CoreRangeSet, CoreRangeSet> add_core_offset(
+inline bool is_scalar(const Tensor &tensor) {
+    const auto &shape = tensor.shape().without_padding();
+    return (shape[0] == 1 && shape[1] == 1 && shape[2] == 1 && shape[3] == 1);
+}
+
+inline bool is_1d_tensor(const Tensor &tensor) {
+    const auto &shape = tensor.shape().without_padding();
+    return (shape[0] == 1 && shape[1] == 1 && shape[2] == 1);
+}
+
+inline bool is_same_shape(const Tensor &tensor_a, const Tensor &tensor_b) {
+    const auto &tensor_a_shape = tensor_a.shape().without_padding();
+    const auto &tensor_b_shape = tensor_b.shape().without_padding();
+    return (tensor_a_shape == tensor_b_shape);
+}
+
+inline bool is_same_batch_shape(const Tensor &tensor_a, const Tensor &tensor_b) {
+    const auto &tensor_a_shape = tensor_a.shape().without_padding();
+    const auto &tensor_b_shape = tensor_b.shape().without_padding();
+    return (tensor_a_shape[0] == tensor_b_shape[0] && tensor_a_shape[1] == tensor_b_shape[1]);
+}
+
+std::tuple<CoreRangeSet, CoreRangeSet, CoreRangeSet> add_core_offset(
     CoreRangeSet all_cores, CoreRangeSet core_group_1, CoreRangeSet core_group_2, uint32_t offset_x, uint32_t offset_y);
 
 std::tuple<uint32_t, CoreRangeSet, CoreRangeSet, CoreRangeSet, uint32_t, uint32_t> split_work_to_cores(
