81dac1985645dd346c7f0aa98893d4087be0de95
Dongjin Na <dongjin.na@moreh.io>
2023-11-03 09:30:08 -0400

    #3188: Add using tt_metal in moreh_matmul header

--- tt_eager/tt_dnn/op_library/moreh_matmul/moreh_matmul_op.hpp
+++ tt_eager/tt_dnn/op_library/moreh_matmul/moreh_matmul_op.hpp
@@ -19,13 +19,15 @@ namespace operations {
 
 namespace primary {
 
+using namespace tt_metal;
+
 /*
  * GENERAL MATMUL
  */
-tt_metal::operation::ProgramWithCallbacks moreh_matmul_multi_core(
-    const tt_metal::Tensor &input_tensor,
-    const tt_metal::Tensor &other_tensor,
-    const tt_metal::Tensor &output_tensor,
+operation::ProgramWithCallbacks moreh_matmul_multi_core(
+    const Tensor &input_tensor,
+    const Tensor &other_tensor,
+    const Tensor &output_tensor,
     bool transpose_input,
     bool transpose_other,
     uint32_t input_start_tile_id,
@@ -38,21 +40,21 @@ struct MorehMatmul {
     uint32_t input_start_tile_id = 0;
     uint32_t other_start_tile_id = 0;
     uint32_t output_start_tile_id = 0;
-    void validate(const std::vector<tt_metal::Tensor> &input_tensors) const;
-    std::vector<tt_metal::Shape> compute_output_shapes(const std::vector<tt_metal::Tensor> &input_tensors) const;
-    std::vector<tt_metal::Tensor> create_output_tensors(const std::vector<tt_metal::Tensor> &input_tensors) const;
-    tt_metal::operation::ProgramWithCallbacks create_program(
-        const std::vector<tt_metal::Tensor> &input_tensors, std::vector<tt_metal::Tensor> &output_tensors) const;
+    void validate(const std::vector<Tensor> &input_tensors) const;
+    std::vector<Shape> compute_output_shapes(const std::vector<Tensor> &input_tensors) const;
+    std::vector<Tensor> create_output_tensors(const std::vector<Tensor> &input_tensors) const;
+    operation::ProgramWithCallbacks create_program(
+        const std::vector<Tensor> &input_tensors, std::vector<Tensor> &output_tensors) const;
     tt::stl::reflection::Attributes attributes() const;
 };
 
-tt_metal::Tensor moreh_matmul(
-    const tt_metal::Tensor &input_tensor,
-    const tt_metal::Tensor &other_tensor,
-    std::optional<std::reference_wrapper<const tt_metal::Tensor>> output_tensor = std::nullopt,
+Tensor moreh_matmul(
+    const Tensor &input_tensor,
+    const Tensor &other_tensor,
+    std::optional<std::reference_wrapper<const Tensor>> output_tensor = std::nullopt,
     bool transpose_input = false,
     bool transpose_other = false,
-    const tt_metal::MemoryConfig &mem_config = tt_metal::operation::DEFAULT_OUTPUT_MEMORY_CONFIG);
+    const MemoryConfig &mem_config = operation::DEFAULT_OUTPUT_MEMORY_CONFIG);
 
 }  // namespace primary
 
