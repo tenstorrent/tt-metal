33610ca8f58cb588157493de48b48c91c3a7976c
Dongjin Na <dongjin.na@moreh.io>
2023-11-04 10:54:43 -0400

    #3280: Refactoring moreh_bmm op

--- tests/tt_eager/python_api_testing/unit_testing/test_moreh_bmm.py
+++ tests/tt_eager/python_api_testing/unit_testing/test_moreh_bmm.py
@@ -9,64 +9,8 @@ from loguru import logger
 
 import tt_lib as ttl
 from tests.tt_eager.python_api_testing.sweep_tests.common import skip_for_wormhole_b0
-from models.utility_functions import comp_pcc
-
-
-def get_tensors(shape, require_input_grad, require_mat2_grad, device):
-    input_shape = [1, shape[0], shape[1], shape[2]]
-    mat2_shape = [1, shape[0], shape[2], shape[3]]
-    output_shape = [1, shape[0], shape[1], shape[3]]
-
-    torch.manual_seed(2023)
-    npu_dtype = ttl.tensor.DataType.BFLOAT16
-    cpu_dtype = torch.bfloat16
-    npu_layout = ttl.tensor.Layout.TILE
-    cpu_layout = ttl.tensor.Layout.ROW_MAJOR
-
-    # create tensors for forward
-    input = torch.randn(input_shape, dtype=cpu_dtype)
-    mat2 = torch.randn(mat2_shape, dtype=cpu_dtype)
-
-    tt_input = (ttl.tensor.Tensor(
-        input.reshape(-1).tolist(), input_shape, npu_dtype,
-        cpu_layout).pad_to_tile(1).to(npu_layout).to(device))
-
-    tt_mat2 = (ttl.tensor.Tensor(
-        mat2.reshape(-1).tolist(), mat2_shape, npu_dtype,
-        cpu_layout).pad_to_tile(float("nan")).to(npu_layout).to(device))
-
-    torch_input = input.reshape(-1, input_shape[2], input_shape[3])
-    torch_mat2 = mat2.reshape(-1, mat2_shape[2], mat2_shape[3])
-
-    # tensors for backward
-    output_grad = tt_output_grad = torch_output_grad = tt_input_grad = tt_mat2_grad = None
-    if require_input_grad or require_mat2_grad:
-        output_grad = torch.randn(output_shape, dtype=cpu_dtype)
-        tt_output_grad = (ttl.tensor.Tensor(
-            output_grad.reshape(-1).tolist(), output_shape, npu_dtype,
-            cpu_layout).pad_to_tile(float("nan")).to(npu_layout).to(device))
-        torch_output_grad = output_grad.reshape(-1, output_shape[2],
-                                                output_shape[3])
-
-        if require_input_grad:
-            input_grad = torch.full(input_shape, float('nan'), dtype=cpu_dtype)
-            tt_input_grad = ttl.tensor.Tensor(
-                input_grad.flatten().tolist(),
-                input_shape,
-                npu_dtype,
-                cpu_layout,
-            ).pad_to_tile(float('nan')).to(npu_layout).to(device)
-
-        if require_mat2_grad:
-            mat2_grad = torch.full(mat2_shape, float('nan'), dtype=cpu_dtype)
-            tt_mat2_grad = ttl.tensor.Tensor(
-                mat2_grad.flatten().tolist(),
-                mat2_shape,
-                npu_dtype,
-                cpu_layout,
-            ).pad_to_tile(float('nan')).to(npu_layout).to(device)
-
-    return tt_input, tt_mat2, tt_output_grad, tt_input_grad, tt_mat2_grad, torch_input, torch_mat2, torch_output_grad, input_shape, mat2_shape, output_shape
+from tests.tt_eager.python_api_testing.unit_testing.test_moreh_matmul import get_tensors
+from models.utility_functions import comp_allclose_and_pcc
 
 
 @skip_for_wormhole_b0
@@ -80,9 +24,13 @@ def get_tensors(shape, require_input_grad, require_mat2_grad, device):
     ),
 )
 def test_moreh_bmm(shape, device):
+    input_shape = [1, shape[0], shape[1], shape[2]]
+    mat2_shape = [1, shape[0], shape[2], shape[3]]
+    output_shape = [1, shape[0], shape[1], shape[3]]
+
     # get tensors
-    tt_input, tt_mat2, _, _, _, torch_input, torch_mat2, _, _, _, output_shape = get_tensors(
-        shape, False, False, device)
+    tt_input, tt_mat2, _, _, _, torch_input, torch_mat2, _ = get_tensors(
+        input_shape, mat2_shape, output_shape, False, False, False, device)
 
     # tt bmm
     cpu_layout = ttl.tensor.Layout.ROW_MAJOR
@@ -91,14 +39,17 @@ def test_moreh_bmm(shape, device):
         tt_mat2).cpu().to(cpu_layout).unpad_from_tile(output_shape).to_torch()
 
     # torch bmm
+    torch_input = torch_input.reshape(-1, input_shape[2], input_shape[3])
+    torch_mat2 = torch_mat2.reshape(-1, mat2_shape[2], mat2_shape[3])
     torch_out = torch.bmm(torch_input, torch_mat2)
+    torch_out = torch.unsqueeze(torch_out, dim=0)
 
     ## test for equivalance
-    passing_pcc, output_pcc = comp_pcc(torch_out, tt_out, pcc=0.999)
-    logger.info(f"Out passing={passing_pcc}")
+    passing, output_pcc = comp_allclose_and_pcc(torch_out, tt_out, pcc=0.999)
+    logger.info(f"Out passing={passing}")
     logger.info(f"Output pcc={output_pcc}")
 
-    assert passing_pcc
+    assert passing
 
 
 @pytest.mark.parametrize("shape", (
@@ -114,8 +65,14 @@ def test_moreh_bmm(shape, device):
 ))
 def test_moreh_bmm_backward(shape, requires_grad, device):
     require_input_grad, require_mat2_grad = requires_grad
-    tt_input, tt_mat2, tt_output_grad, tt_input_grad, tt_mat2_grad, torch_input, torch_mat2, torch_output_grad, input_shape, mat2_shape, _ = get_tensors(
-        shape, require_input_grad, require_mat2_grad, device)
+    input_shape = [1, shape[0], shape[1], shape[2]]
+    mat2_shape = [1, shape[0], shape[2], shape[3]]
+    output_shape = [1, shape[0], shape[1], shape[3]]
+
+    # get tensors
+    tt_input, tt_mat2, tt_output_grad, tt_input_grad, tt_mat2_grad, torch_input, torch_mat2, torch_output_grad = get_tensors(
+        input_shape, mat2_shape, output_shape, require_input_grad,
+        require_mat2_grad, False, device)
 
     # tt bmm fwd, bwd
     cpu_layout = ttl.tensor.Layout.ROW_MAJOR
@@ -124,29 +81,40 @@ def test_moreh_bmm_backward(shape, requires_grad, device):
                                               tt_mat2_grad)
 
     # torch bmm fwd, bwd
+    torch_input = torch_input.reshape(-1, input_shape[2], input_shape[3])
+    torch_mat2 = torch_mat2.reshape(-1, mat2_shape[2], mat2_shape[3])
+    torch_output_grad = torch_output_grad.reshape(-1, output_shape[2],
+                                                  output_shape[3])
     torch_out = torch.bmm(torch_input.requires_grad_(require_input_grad),
                           torch_mat2.requires_grad_(require_mat2_grad))
     torch_out.backward(torch_output_grad)
 
     # test for equivalance
+    rtol = atol = 0.1
     if require_input_grad:
         ttcpu_input_grad = tt_input_grad.cpu().to(cpu_layout).unpad_from_tile(
             input_shape).to_torch()
 
-        passing_pcc, output_pcc = comp_pcc(torch_input.grad,
-                                           ttcpu_input_grad,
-                                           pcc=0.999)
-        logger.info(f"input_grad passing={passing_pcc}")
+        torch_input_grad = torch.unsqueeze(torch_input.grad, dim=0)
+        passing, output_pcc = comp_allclose_and_pcc(torch_input_grad,
+                                                    ttcpu_input_grad,
+                                                    pcc=0.999,
+                                                    rtol=rtol,
+                                                    atol=atol)
+        logger.info(f"input_grad passing={passing}")
         logger.info(f"input_grad pcc={output_pcc}")
-        assert passing_pcc
+        assert passing
 
     if require_mat2_grad:
         ttcpu_mat2_grad = tt_mat2_grad.cpu().to(cpu_layout).unpad_from_tile(
             mat2_shape).to_torch()
 
-        passing_pcc, output_pcc = comp_pcc(torch_mat2.grad,
-                                           ttcpu_mat2_grad,
-                                           pcc=0.999)
-        logger.info(f"mat2_grad passing={passing_pcc}")
+        torch_mat2_grad = torch.unsqueeze(torch_mat2.grad, dim=0)
+        passing, output_pcc = comp_allclose_and_pcc(torch_mat2_grad,
+                                                    ttcpu_mat2_grad,
+                                                    pcc=0.999,
+                                                    rtol=rtol,
+                                                    atol=atol)
+        logger.info(f"mat2_grad passing={passing}")
         logger.info(f"mat2_grad pcc={output_pcc}")
-        assert passing_pcc
+        assert passing
