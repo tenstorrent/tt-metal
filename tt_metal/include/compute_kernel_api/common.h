4cdeb1443e101e94983a6182b377d398fa9c1b16
tarafdarTT <ntarafdar@tenstorrent.com>
2023-11-06 19:31:23 +0000

    #3581: common.h documentation

--- tt_metal/include/compute_kernel_api/common.h
+++ tt_metal/include/compute_kernel_api/common.h
@@ -12,11 +12,30 @@
 
 #define FORCE_INLINE inline __attribute__((always_inline))
 
+/**
+ * Returns the address in L1 for a given runtime argument index
+ *
+ * Return value: Associated L1 address of given runtime argument index
+ *
+ * | Argument       | Description                                                             | Type     | Valid Range                                    | Required |
+ * |----------------|-------------------------------------------------------------------------|----------|------------------------------------------------|----------|
+ * | arg_idx        | Runtime argument index                                                  | uint32_t | 0 to 31                                        | True     |
+ */
 constexpr static uint32_t get_arg_addr(int arg_idx) {
     // args are 4B in size
     return TRISC_L1_ARG_BASE + (arg_idx << 2);
 }
 
+
+/**
+ * Returns the value at a given runtime argument index
+ *
+ * Return value: The value associated with the runtime argument index
+ *
+ * | Argument       | Description                                                             | Type     | Valid Range                                    | Required |
+ * |----------------|-------------------------------------------------------------------------|----------|------------------------------------------------|----------|
+ * | arg_idx        | Runtime argument index                                                  | uint32_t | 0 to 31                                        | True     |
+ */
 template <typename T>
 FORCE_INLINE T get_arg_val(int arg_idx) {
     // only 4B args are supported (eg int32, uint32)
