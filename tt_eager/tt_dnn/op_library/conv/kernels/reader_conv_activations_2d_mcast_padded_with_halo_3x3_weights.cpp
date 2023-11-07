4b2f4fff80f065a317bfb431b0ce4f6bc39f1a73
Davor Capalija <dcapalija@tenstorrent.com>
2023-11-07 23:39:35 +0000

    #3472: 2d systolic conv, use stateful read one packet, window inner is const

--- tt_eager/tt_dnn/op_library/conv/kernels/reader_conv_activations_2d_mcast_padded_with_halo_3x3_weights.cpp
+++ tt_eager/tt_dnn/op_library/conv/kernels/reader_conv_activations_2d_mcast_padded_with_halo_3x3_weights.cpp
@@ -29,8 +29,10 @@ void kernel_main() {
     uint32_t last_partial_image_num_rows           = get_arg_val<uint32_t>(i); i+=1;
     uint32_t last_partial_left_aligned_row_width   = get_arg_val<uint32_t>(i); i+=1;
 
-    uint32_t window_outer                          = get_arg_val<uint32_t>(i); i+=1;
-    uint32_t window_inner                          = get_arg_val<uint32_t>(i); i+=1;
+    // moved these to compile-time args
+    // uint32_t window_outer                          = get_arg_val<uint32_t>(i); i+=1;
+    // uint32_t window_inner                          = get_arg_val<uint32_t>(i); i+=1;
+    i+=2; // skip 2 rt args
 
     uint32_t noop = get_arg_val<uint32_t>(i); i+=1;
     if(noop) {
@@ -59,6 +61,10 @@ void kernel_main() {
     constexpr uint32_t conv_output_w_last_index = get_compile_time_arg_val(4) - 1;
     constexpr uint32_t conv_act_c_read_bytes = get_compile_time_arg_val(5);
     constexpr uint32_t log_base_2_of_conv_act_size_c_bytes = get_compile_time_arg_val(6);
+    // TODO delete unused: get_compile_time_arg_val(7); (8), (9)
+    // need to have these as compile-time since we unroll loops based on them
+    constexpr uint32_t window_outer                        = get_compile_time_arg_val(10);
+    constexpr uint32_t window_inner                        = get_compile_time_arg_val(11);
 
     constexpr uint32_t cb_id_act = 0;
     constexpr uint32_t cb_id_sharded_act = 3;
@@ -170,7 +176,7 @@ void kernel_main() {
     );
 
     uint64_t act_mcast_receiver_semaphore_noc_addr = act_multicast_noc_addr | act_mcast_receiver_semaphore_addr;
-
+    uint32_t num_issued_reads_per_block = act_block_h_datums * window_inner;
 
     // TODO: need to make the read coalescing optimization cleaner
     // pass coalesce_window_inner_reads as a compile time arg and num_coalesced_reads so we can constexpr the if
@@ -217,6 +223,9 @@ void kernel_main() {
             noc_semaphore_wait(act_mcast_receiver_semaphore_addr_ptr, VALID);
         }
 
+
+        noc_async_read_one_packet_set_state(get_noc_addr(act_l1_read_addr), coalesced_read_bytes);
+
         for (uint32_t outer = 0; outer < window_outer; outer++) {
             // Reset reader_idx to finish act_block_h_datums
             reader_idx = 0;
@@ -225,15 +234,18 @@ void kernel_main() {
 
             for (uint32_t bh = 0; bh < act_block_h_datums; bh++) {
                 reader_offset_idx = 0;
+                #pragma unroll window_inner
                 for (uint32_t inner = 0; inner < window_inner; inner++) {
                     // local read from reader_index + reader_offset;
                     act_l1_offset = (reader_indices_ptr[reader_idx] + reader_offsets_ptr[reader_offset_idx]) << log_base_2_of_conv_act_size_c_bytes;
-                    noc_async_read_one_packet(get_noc_addr(act_l1_read_addr + act_l1_offset), l1_write_addr_act, coalesced_read_bytes);
+                     //noc_async_read_one_packet(get_noc_addr(act_l1_read_addr + act_l1_offset), l1_write_addr_act, coalesced_read_bytes);
+                    noc_async_read_one_packet_with_state<true>(act_l1_read_addr + act_l1_offset, l1_write_addr_act);
                     l1_write_addr_act += coalesced_read_bytes;
                     reader_offset_idx += num_coalesced_reads;
                 }
                 reader_idx++;
             }
+            // noc_async_read_inc_num_issued(num_issued_reads_per_block);
             noc_async_read_barrier();
             cb_push_back(cb_id_act, act_block_num_tiles);
         }
