5eb9dcd3ed047797caf3e8dfb318c4be56adc292
Davor Capalija <dcapalija@tenstorrent.com>
2023-11-11 00:35:24 +0000

    #3758: 1d systolic array conv, use stateful NOC APIs, use compile-time args for critical loop bounds

--- tt_eager/tt_dnn/op_library/conv/kernels/reader_conv_activations_padded_with_halo_3x3_weights.cpp
+++ tt_eager/tt_dnn/op_library/conv/kernels/reader_conv_activations_padded_with_halo_3x3_weights.cpp
@@ -14,7 +14,10 @@ void kernel_main() {
     uint32_t weight_size_h = get_arg_val<uint32_t>(i); i+=1;
     uint32_t weight_size_w = get_arg_val<uint32_t>(i); i+=1;
 
-    uint32_t act_block_h_datums = get_arg_val<uint32_t>(i); i+=1;
+    // inner loop bounds as compile-time args improve pef
+    // uint32_t act_block_h_datums = get_arg_val<uint32_t>(i); i+=1;
+    i+=1; // skip an arg
+
     uint32_t act_block_num_tiles = get_arg_val<uint32_t>(i); i+=1;
 
     uint32_t first_partial_right_aligned_row_width = get_arg_val<uint32_t>(i); i+=1;
@@ -26,8 +29,10 @@ void kernel_main() {
     uint32_t last_partial_image_num_rows           = get_arg_val<uint32_t>(i); i+=1;
     uint32_t last_partial_left_aligned_row_width   = get_arg_val<uint32_t>(i); i+=1;
 
-    uint32_t window_outer                          = get_arg_val<uint32_t>(i); i+=1;
-    uint32_t window_inner                          = get_arg_val<uint32_t>(i); i+=1;
+    // moved these to compile-time args
+    // uint32_t window_outer                          = get_arg_val<uint32_t>(i); i+=1;
+    // uint32_t window_inner                          = get_arg_val<uint32_t>(i); i+=1;
+    i+=2; // skip 2 rt args
 
     uint32_t noop = get_arg_val<uint32_t>(i); i+=1;
     if(noop) {
@@ -41,6 +46,11 @@ void kernel_main() {
     constexpr uint32_t conv_output_w_last_index = get_compile_time_arg_val(4) - 1;
     constexpr uint32_t conv_act_c_read_bytes = get_compile_time_arg_val(5);
     constexpr uint32_t log_base_2_of_conv_act_size_c_bytes = get_compile_time_arg_val(6);
+    // TODO delete unused: get_compile_time_arg_val(7); (8), (9)
+    // need to have these as compile-time, they are inner loop bouds / unroll loops / constexpr conditionals based on them
+    constexpr uint32_t window_outer                        = get_compile_time_arg_val(10);
+    constexpr uint32_t window_inner                        = get_compile_time_arg_val(11);
+    constexpr uint32_t act_block_h_datums                  = get_compile_time_arg_val(12);
 
     constexpr uint32_t cb_id_act = 0;
     constexpr uint32_t cb_id_sharded_act = 3;
@@ -55,18 +65,7 @@ void kernel_main() {
     // assert(act_block_w_ntiles == act_block_w_datums/32)
     // assert(act_block_num_tiles == (act_block_h_datums * act_block_w_datums)/1024)
 
-
-    //DPRINT << "partial right aligned width: " << first_partial_right_aligned_row_width << ENDL();
-    //DPRINT << "--- skip after: " << skip_after_partial_right_aligned_row << ENDL();
-    //DPRINT << "first partial image rows: " << first_partial_image_num_rows << ENDL();
-    //DPRINT << "--- skip after: " << skip_after_first_partial_image_row << ENDL();
-    //DPRINT << "full images: " << num_full_images << ENDL();
-    //DPRINT << "--- skip after: " << skip_after_full_image << ENDL();
-    //DPRINT << "last partial image rows: " << last_partial_image_num_rows << ENDL();
-    //DPRINT << "partial left aligned width: " << last_partial_left_aligned_row_width << ENDL();
-
-
-    // DUMMY LOOP TO FILL READER INDICES
+    // LOOP TO FILL READER INDICES
     constexpr uint32_t cb_reader_indices = tt::CB::c_in4;
     volatile tt_l1_ptr uint32_t* reader_indices_ptr = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(get_write_ptr(cb_reader_indices));
 
@@ -113,7 +112,7 @@ void kernel_main() {
     }
 
 
-    // DUMMY LOOP TO FILL READER OFFSETS
+    // LOOP TO FILL READER OFFSETS
     /* We can add another loop to read chunks of a stick as well.
      * - Duplicate reader_offset for same stick X times (window_inner must be 1)
      * - New loop between outer and inner that loops X times reading from same stick
@@ -139,23 +138,30 @@ void kernel_main() {
     // we check if window_inner == weight_size_w to make sure coalescing is legal along full window_inner so the loop can be removed
     constexpr bool coalesce_window_inner_reads = true;
     constexpr uint32_t num_coalesced_reads = 3;
-    const uint32_t coalesced_read_bytes = num_coalesced_reads * conv_act_c_read_bytes;
-    // we want to have the check hoisted out because in act_block_h_datums loop it would be to expensive (unless we make it ifdef)
-    if (coalesce_window_inner_reads and window_inner == num_coalesced_reads) {
+    constexpr uint32_t coalesced_read_bytes = num_coalesced_reads * conv_act_c_read_bytes;
+    // the conditional selecting between coalescing and no-colescing must be constexpr to that compiler can optimized the other path away
+    // this has shown to be a big perf win
+    if constexpr (coalesce_window_inner_reads and window_inner == num_coalesced_reads) {
         // coalesce reads along weight_size_w
         reader_offset_idx = 0;
         uint32_t act_l1_offset = 0;
         uint32_t act_l1_read_addr = get_read_ptr(cb_id_sharded_act);
+
+        static_assert(coalesced_read_bytes <= NOC_MAX_BURST_SIZE);
+        // set_state uses just x/y from the get_noc_addr, addr is ignored
+        noc_async_read_one_packet_set_state(get_noc_addr(act_l1_read_addr), coalesced_read_bytes);
+
         for (uint32_t outer = 0; outer < window_outer; outer++) {
             // Reset reader_idx to finish act_block_h_datums
             reader_idx = 0;
             cb_reserve_back(cb_id_act, act_block_num_tiles);
             uint32_t l1_write_addr_act = get_write_ptr(cb_id_act);
-            uint32_t reader_offset = reader_offsets_ptr[reader_offset_idx];
+            uint32_t reader_offset = act_l1_read_addr + (reader_offsets_ptr[reader_offset_idx] << log_base_2_of_conv_act_size_c_bytes);
+            // #pragma GCC unroll 4 // unroll didn't help, but act_block_h_datums (loop bound) being const does help
             for (uint32_t bh = 0; bh < act_block_h_datums; bh++) {
                 // local read from reader_index + reader_offset;
-                act_l1_offset = (reader_indices_ptr[reader_idx] + reader_offset) << log_base_2_of_conv_act_size_c_bytes;
-                noc_async_read_one_packet(get_noc_addr(act_l1_read_addr + act_l1_offset), l1_write_addr_act, coalesced_read_bytes);
+                act_l1_offset = reader_offset + (reader_indices_ptr[reader_idx] << log_base_2_of_conv_act_size_c_bytes);
+                noc_async_read_one_packet_with_state<true>(act_l1_offset, l1_write_addr_act);
                 l1_write_addr_act += coalesced_read_bytes;
                 reader_idx++;
             }
@@ -170,16 +176,23 @@ void kernel_main() {
         reader_offset_idx = 0;
         uint32_t act_l1_offset = 0;
         uint32_t act_l1_read_addr = get_read_ptr(cb_id_sharded_act);
+
+        static_assert(conv_act_c_read_bytes <= NOC_MAX_BURST_SIZE);
+        // set_state uses just x/y from the get_noc_addr, addr is ignored
+        noc_async_read_one_packet_set_state(get_noc_addr(act_l1_read_addr), conv_act_c_read_bytes);
+
         for (uint32_t outer = 0; outer < window_outer; outer++) {
             // Reset reader_idx to finish act_block_h_datums
             reader_idx = 0;
             cb_reserve_back(cb_id_act, act_block_num_tiles);
             uint32_t l1_write_addr_act = get_write_ptr(cb_id_act);
             for (uint32_t bh = 0; bh < act_block_h_datums; bh++) {
+                // when no read coalesing, main use case is window_inner == 1,
+                // and if window_inner is const this loop should be removed by the compiler
                 for (uint32_t inner = 0; inner < window_inner; inner++) {
                     // local read from reader_index + reader_offset;
-                    act_l1_offset = (reader_indices_ptr[reader_idx] + reader_offsets_ptr[reader_offset_idx + inner]) << log_base_2_of_conv_act_size_c_bytes;
-                    noc_async_read_one_packet(get_noc_addr(act_l1_read_addr + act_l1_offset), l1_write_addr_act, conv_act_c_read_bytes);
+                    act_l1_offset = act_l1_read_addr + ((reader_indices_ptr[reader_idx] + reader_offsets_ptr[reader_offset_idx + inner]) << log_base_2_of_conv_act_size_c_bytes);
+                    noc_async_read_one_packet_with_state<true>(act_l1_offset, l1_write_addr_act);
                     l1_write_addr_act += conv_act_c_read_bytes;
 
                 }
