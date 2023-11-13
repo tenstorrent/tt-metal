9c11eaf6aa596e90c51b4b0de5c33d6a967ea97f
Raymond Kim <rkim@tenstorrent.com>
2023-11-13 20:01:12 +0000

    #3745: Various fixes to python environment and packaging, check desc - Add wheel as a package-level build dep so that an installing environment has wheel available for things like pyyaml - Install setuptools and wheel in the Python dev environment at time of venv creation so later dependencies have it to install their wheels etc. - Pin platformdirs and multiprocess so we fix platformdirs and dill dependency issues. A deeper explanation is available in the requirements.txt comments

--- tt_metal/python_env/module.mk
+++ tt_metal/python_env/module.mk
@@ -13,11 +13,14 @@ python_env/clean:
 $(PYTHON_ENV)/.installed:
 	python3.8 -m venv $(PYTHON_ENV)
 	bash -c "source $(PYTHON_ENV)/bin/activate && python -m pip config set global.extra-index-url https://download.pytorch.org/whl/cpu"
+	echo "Installing python env build backend requirements..."
+	bash -c "source $(PYTHON_ENV)/bin/activate && python -m pip install setuptools wheel"
 	touch $@
 
 $(PYTHON_ENV)/%: $(PYTHON_ENV)/.installed
 	bash -c "source $(PYTHON_ENV)/bin/activate"
 
 $(PYTHON_ENV)/.installed-dev: tt_eager/tt_lib/dev_install python_env tt_metal/python_env/requirements-dev.txt
+	echo "Installing dev environment packages..."
 	bash -c "source $(PYTHON_ENV)/bin/activate && python -m pip install -r tt_metal/python_env/requirements-dev.txt"
 	touch $@
