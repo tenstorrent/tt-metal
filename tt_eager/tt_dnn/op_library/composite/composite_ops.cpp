982a48ff4f242f6c82b189f638b147a9203bca95
hschoi <hschoi4448@gmail.com>
2023-11-13 14:46:39 +0000

    #3689: fix comment typo

--- tt_eager/tt_dnn/op_library/composite/composite_ops.cpp
+++ tt_eager/tt_dnn/op_library/composite/composite_ops.cpp
@@ -1249,7 +1249,7 @@ Tensor power_fp(const Tensor& input_a, float exponent, const MemoryConfig& outpu
 
 //masked_fill - binary operator y = (mask) ? value : input_a; elementwise
 Tensor masked_fill(const Tensor& input_a, const Tensor& mask, float value, const MemoryConfig& output_mem_config) {
-    // TODO: remove t_value if `where` function recive float type `value`. because tensor type `value` ​​can waste memory
+    // TODO: remove t_value if `where` function receive float type `value`. because tensor type `value` ​​can waste memory
     Tensor t_value = mk_filled_tensor_like(input_a, value, output_mem_config);
     Tensor result = where(mask, t_value, input_a, output_mem_config);
     return result;
@@ -1264,7 +1264,7 @@ Tensor masked_fill_(const Tensor& input_a, const Tensor& mask, float value) {
 
 Tensor masked_fill_backward(const Tensor& output_grad, const Tensor& mask, const MemoryConfig& output_mem_config)
 {
-    // TODO: remove zero_like if `where` function recive float type `value`. because tensor type `value` ​​can waste memory
+    // TODO: remove zero_like if `where` function receive float type `value`. because tensor type `value` ​​can waste memory
     Tensor zero_like = mk_zero_tensor_like(output_grad, output_mem_config);
     Tensor result = where(mask, zero_like, output_grad, output_mem_config);
     return result;
