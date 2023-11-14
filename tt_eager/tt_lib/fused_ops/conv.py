84604c3fb0c85fb46bd5776e8b8866c43329e3d6
Bill Teng <bteng@tenstorrent.com>
2023-11-14 10:20:00 +0000

    Revert "#3642: Remove pytorch inheritance and FW path call constructor calls"
    This reverts commit c042726eb8021ddfb20d7784b693c86f69852263.

--- tt_eager/tt_lib/fused_ops/conv.py
+++ tt_eager/tt_lib/fused_ops/conv.py
@@ -228,21 +228,6 @@ def resnet50_optimized_conv(
     )
     bias_on_device = bias_.to(device)
 
-    opt_conv_parall_conf = tensor.OptimizedConvParallelizationConfig(
-        grid_size=grid_size,
-        per_core_out_matrix_height_ntiles=per_core_out_matrix_h_ntiles,
-        per_core_weight_matrix_width_ntiles=per_core_weight_matrix_w_ntiles,
-    )
-    opt_conv_block_conf = tensor.OptimizedConvBlockConfig(
-        act_block_h_ntiles=act_block_h,
-        act_block_w_ntiles=act_block_w,
-        act_c_num_blocks=act_c_num_blocks,
-        weight_block_w_ntiles=weight_block_w,
-        out_block_h_ntiles=out_block_h,
-        out_subblock_h_ntiles=out_subblock_h,
-        out_subblock_w_ntiles=out_subblock_w,
-    )
-
     def conv_(activation):
         # assert(activation.layout() == tensor.Layout.ROW_MAJOR)
         output = tensor.optimized_conv(
@@ -255,8 +240,20 @@ def resnet50_optimized_conv(
             True,
             fuse_relu,
             math_fidelity,
-            opt_conv_parall_conf,
-            opt_conv_block_conf,
+            tensor.OptimizedConvParallelizationConfig(
+                grid_size=grid_size,
+                per_core_out_matrix_height_ntiles=per_core_out_matrix_h_ntiles,
+                per_core_weight_matrix_width_ntiles=per_core_weight_matrix_w_ntiles,
+            ),
+            tensor.OptimizedConvBlockConfig(
+                act_block_h_ntiles=act_block_h,
+                act_block_w_ntiles=act_block_w,
+                act_c_num_blocks=act_c_num_blocks,
+                weight_block_w_ntiles=weight_block_w,
+                out_block_h_ntiles=out_block_h,
+                out_subblock_h_ntiles=out_subblock_h,
+                out_subblock_w_ntiles=out_subblock_w,
+            ),
             0,
             output_mem_config=activation.memory_config() if output_mem_config is None else output_mem_config,
             output_dtype=output_dtype,
@@ -347,20 +344,6 @@ def resnet50_first_conv(
     P_H = 0
     P_W = 0
 
-    opt_conv_parall_conf = tensor.OptimizedConvParallelizationConfig(
-        grid_size=grid_size,
-        per_core_out_matrix_height_ntiles=per_core_out_matrix_h_ntiles,
-        per_core_weight_matrix_width_ntiles=per_core_weight_matrix_w_ntiles,
-    )
-    opt_conv_block_conf = tensor.OptimizedConvBlockConfig(
-        act_block_h_ntiles=act_block_h,
-        act_block_w_ntiles=act_block_w,
-        weight_block_w_ntiles=weight_block_w,
-        out_block_h_ntiles=out_block_h,
-        out_subblock_h_ntiles=out_subblock_h,
-        out_subblock_w_ntiles=out_subblock_w,
-    )
-
     def conv_(activation):
         # assert(activation.layout() == tensor.Layout.ROW_MAJOR)
         output_plus_bias = tensor.optimized_conv(
@@ -373,8 +356,19 @@ def resnet50_first_conv(
             True,
             fuse_relu,
             math_fidelity,
-            opt_conv_parall_conf,
-            opt_conv_block_conf,
+            tensor.OptimizedConvParallelizationConfig(
+                grid_size=grid_size,
+                per_core_out_matrix_height_ntiles=per_core_out_matrix_h_ntiles,
+                per_core_weight_matrix_width_ntiles=per_core_weight_matrix_w_ntiles,
+            ),
+            tensor.OptimizedConvBlockConfig(
+                act_block_h_ntiles=act_block_h,
+                act_block_w_ntiles=act_block_w,
+                weight_block_w_ntiles=weight_block_w,
+                out_block_h_ntiles=out_block_h,
+                out_subblock_h_ntiles=out_subblock_h,
+                out_subblock_w_ntiles=out_subblock_w,
+            ),
             extra_padding_for_32B_alignment,
             out_mem_config,
             output_dtype,
