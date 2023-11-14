6c048f85881753a350ada1b45e7169666400434d
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2023-11-14 13:34:42 +0000

    #0: Fix outstanding comments for rsqrt

--- tt_metal/src/ckernels/wormhole_b0/common/inc/ckernel_sfpu.h
+++ tt_metal/src/ckernels/wormhole_b0/common/inc/ckernel_sfpu.h
@@ -189,20 +189,19 @@ inline void calculate_rsqrt()
     {
 
         vFloat in = dst_reg[0];
-        vFloat result = sfpu_reciprocal(in);
-        v_if(dst_reg[0] < 1.0f){
-            result = 1.0f;
-        }v_endif;
-
-        for (int r = 0; r < RECIPROCAL_ITERATIONS; r++)
-        {
-            // y = y * (1.5 - 0.5 * x * y * y) Newton's method iteration.
-            result = result * (1.5F - 0.5F  * dst_reg[0] * result * result);
-        }
-
         v_if(dst_reg[0] == 0.0f){
             dst_reg[0] = std::numeric_limits<float>::infinity();
         }v_else{
+            vFloat result = 1.0f;
+            v_if(dst_reg[0] > 1.0f){
+                result = sfpu_reciprocal(in);
+            }v_endif;
+
+            for (int r = 0; r < RECIPROCAL_ITERATIONS; r++)
+            {
+                // y = y * (1.5 - 0.5 * x * y * y) Newton's method iteration.
+                result = result * (1.5F - 0.5F  * dst_reg[0] * result * result);
+            }
             dst_reg[0] = result;
         }v_endif;
 
