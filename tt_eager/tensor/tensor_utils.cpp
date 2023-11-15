16ec3d70e43a1c99971903af071f7c7370f195da
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2023-11-15 06:28:25 +0000

    #3746: Fix outstanding comments from Host Reshape PR

--- tt_eager/tensor/tensor_utils.cpp
+++ tt_eager/tensor/tensor_utils.cpp
@@ -161,47 +161,6 @@ const Shape infer_dims_for_reshape(int N, int C, int H, int W, uint32_t old_volu
         case 2:
             TT_ASSERT(old_volume % N*C*W == 0);
             H = old_volume/(N*C*W);
-            TT_ASSERT(H%32 == 0);
-            break;
-        case 3:
-            TT_ASSERT(old_volume % N*C*H == 0);
-            W = old_volume/(N*C*H);
-            TT_ASSERT(W%32 == 0);
-            break;
-        case -1: // In case where there is no negative value in ns
-            TT_ASSERT(N*C*H*W == old_volume);
-            break;
-        default:
-            TT_ASSERT(false && "Unexpected neg_idx in reshape!");
-    }
-
-    return {(uint32_t)N, (uint32_t)C, (uint32_t)H, (uint32_t)W};
-}
-
-const Shape infer_dims_for_reshape_RM(int N, int C, int H, int W, uint32_t old_volume) {
-    vector<int> ns{N, C, H, W};
-    int neg_idx = -1;
-    for (int i = 0; i < ns.size(); i++) {
-        if (ns[i] == -1) {
-            TT_ASSERT(neg_idx == -1, "Only one -1 is allowed in reshape");
-            neg_idx = i;
-        } else {
-            TT_ASSERT(ns[i] > 0, "New shape entries can only have -1 or positive values");
-        }
-    }
-
-    switch (neg_idx) {
-        case 0:
-            TT_ASSERT(old_volume % C*H*W == 0);
-            N = old_volume/(C*H*W);
-            break;
-        case 1:
-            TT_ASSERT(old_volume % N*H*W == 0);
-            C = old_volume/(N*H*W);
-            break;
-        case 2:
-            TT_ASSERT(old_volume % N*C*W == 0);
-            H = old_volume/(N*C*W);
             break;
         case 3:
             TT_ASSERT(old_volume % N*C*H == 0);
