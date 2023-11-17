8961135d5bc2e6aa1f997132b8f0ce5ea3900f9b
agrebenisan <agrebenisan@tenstorrent.com>
2023-11-17 22:04:03 +0000

    #3829: Add documentation for new void* enqueue apis

--- docs/source/tt_metal/apis/host_apis/command_queue/EnqueueWriteBuffer.rst
+++ docs/source/tt_metal/apis/host_apis/command_queue/EnqueueWriteBuffer.rst
@@ -2,3 +2,4 @@ EnqueueWriteBuffer
 ==================
 
 .. doxygenfunction:: EnqueueWriteBuffer(CommandQueue &cq, Buffer &buffer, std::vector<uint32_t> &src, bool blocking)
+.. doxygenfunction:: EnqueueWriteBuffer(CommandQueue &cq, Buffer &buffer, const void* src, bool blocking)
--- tt_metal/host_api.hpp
+++ tt_metal/host_api.hpp
@@ -274,6 +274,20 @@ std::vector<uint32_t>& GetRuntimeArgs(const Program &program, KernelID kernel_id
 void EnqueueReadBuffer(CommandQueue& cq, Buffer& buffer, std::vector<uint32_t>& dst, bool blocking);
 
 /**
+ * Reads a buffer from the device
+ *
+ * Return value: void
+ *
+ * | Argument     | Description                                                            | Type                          | Valid Range                        | Required |
+ * |--------------|------------------------------------------------------------------------|-------------------------------|------------------------------------|----------|
+ * | cq           | The command queue object which dispatches the command to the hardware  | CommandQueue &                |                                    | Yes      |
+ * | buffer       | The device buffer we are reading from                                  | Buffer &                      |                                    | Yes      |
+ * | dst          | The memory where the result will be stored                             | void*                         |                                    | Yes      |
+ * | blocking     | Whether or not this is a blocking operation                            | bool                          |                                    | Yes      |
+ */
+void EnqueueReadBuffer(CommandQueue& cq, Buffer& buffer, void* dst, bool blocking);
+
+/**
  * Writes a buffer to the device
  *
  * Return value: void
@@ -282,10 +296,24 @@ void EnqueueReadBuffer(CommandQueue& cq, Buffer& buffer, std::vector<uint32_t>& 
  * |--------------|------------------------------------------------------------------------|-------------------------------|------------------------------------|----------|
  * | cq           | The command queue object which dispatches the command to the hardware  | CommandQueue &                |                                    | Yes      |
  * | buffer       | The device buffer we are writing to                                    | Buffer &                      |                                    | Yes      |
- * | src          | The vector we are writing to the device                                | vector<uint32_t> &                 |                                    | Yes      |
+ * | src          | The vector we are writing to the device                                | vector<uint32_t> &            |                                    | Yes      |
  * | blocking     | Whether or not this is a blocking operation                            | bool                          |                                    | Yes      |
  */
 void EnqueueWriteBuffer(CommandQueue& cq, Buffer& buffer, std::vector<uint32_t>& src, bool blocking);
+
+/**
+ * Writes a buffer to the device
+ *
+ * Return value: void
+ *
+ * | Argument     | Description                                                            | Type                          | Valid Range                        | Required |
+ * |--------------|------------------------------------------------------------------------|-------------------------------|------------------------------------|----------|
+ * | cq           | The command queue object which dispatches the command to the hardware  | CommandQueue &                |                                    | Yes      |
+ * | buffer       | The device buffer we are writing to                                    | Buffer &                      |                                    | Yes      |
+ * | src          | The memory we are writing to the device                                | const void*                   |                                    | Yes      |
+ * | blocking     | Whether or not this is a blocking operation                            | bool                          |                                    | Yes      |
+ */
+void EnqueueWriteBuffer(CommandQueue& cq, Buffer& buffer, const void* src, bool blocking);
 
 /**
  * Writes a program to the device and launches it
