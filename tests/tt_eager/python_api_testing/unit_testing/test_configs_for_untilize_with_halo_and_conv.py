127950fd15140558611169b75f7212f6fcd84174
Nitika Shanker <nshanker@tenstorrent.com>
2023-11-17 20:15:40 +0000

    #0: code to do shard size calculation

--- tests/tt_eager/python_api_testing/unit_testing/test_configs_for_untilize_with_halo_and_conv.py
+++ tests/tt_eager/python_api_testing/unit_testing/test_configs_for_untilize_with_halo_and_conv.py
@@ -9,22 +9,21 @@ from tests.tt_eager.python_api_testing.conv.untilize_with_halo_config_generation
     validate_tensor_metadata,
 )
 from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_equal, comp_allclose_and_pcc
+from tt_lib.utils import _nearest_y
 
 
 # conv params - output_channels, input_channels, filter_h, filter_w, stride_h, stride_w, pad_h, pad_w, dilation, groups
 @pytest.mark.parametrize(
-    "conv_params, input_nchw_shape, unpadded_input_shard_height, conv_output_shard_height, num_cores",
+    "conv_params, input_nchw_shape, num_cores",
     (
-        ((1, 1, 2, 2, 1, 1, 0, 0, 1, 1), (8, 1, 8, 8), 512, 392, 1),
-        ((1, 1, 2, 2, 1, 1, 0, 0, 1, 1), (8, 1, 8, 8), 256, 196, 2),
-        ((1, 1, 2, 2, 1, 1, 1, 1, 1, 1), (8, 1, 8, 8), 512, 648, 1),
-        ((1, 1, 2, 2, 1, 1, 1, 1, 1, 1), (8, 1, 8, 8), 256, 324, 2),
+        ((1, 1, 2, 2, 1, 1, 0, 0, 1, 1), (8, 1, 8, 8), 1),
+        ((1, 1, 2, 2, 1, 1, 0, 0, 1, 1), (8, 1, 8, 8), 2),
+        ((1, 1, 2, 2, 1, 1, 1, 1, 1, 1), (8, 1, 8, 8), 1),
+        ((1, 1, 2, 2, 1, 1, 1, 1, 1, 1), (8, 1, 8, 8), 2),
         # ((1, 1, 4, 4, 1, 1, 0, 0, 1, 1), (8, 1, 115, 115)),
     ),
 )
-def test_generate_all_configs_and_references(
-    conv_params, input_nchw_shape, unpadded_input_shard_height, conv_output_shard_height, num_cores
-):
+def test_generate_all_configs_and_references(conv_params, input_nchw_shape, num_cores):
     assert len(conv_params) == 10
     output_channels, input_channels, filter_h, filter_w, stride_h, stride_w, pad_h, pad_w, dilation, groups = [
         conv_params[i] for i in range(10)
@@ -37,6 +36,18 @@ def test_generate_all_configs_and_references(
     input_n, input_c, input_h, input_w = input_nchw_shape
     assert input_c == 1  # Ref done for channel size = 1
     input_volume = numpy.prod(input_nchw_shape)
+    assert output_channels == 1
+    conv_output_h = ((int)((input_h + (2 * pad_h) - filter_h) / stride_h)) + 1
+    conv_output_w = ((int)((input_w + (2 * pad_w) - filter_w) / stride_w)) + 1
+    conv_output_volume = input_n * conv_output_h * conv_output_w
+
+    input_size_to_shard_evenly = _nearest_y(input_volume, num_cores * 32)
+    untilize_with_halo_input_shard_height = (int)(input_size_to_shard_evenly / num_cores)
+    output_size_to_shard_evenly = _nearest_y(conv_output_volume, num_cores * 32)
+    conv_output_shard_height = (int)(output_size_to_shard_evenly / num_cores)
+
+    print("untilize with halo input shard height=", untilize_with_halo_input_shard_height)
+    print("conv_output_shard_height=", conv_output_shard_height)
 
     # Initialize tensor with data
     # Inserting sequential integer data
@@ -74,7 +85,7 @@ def test_generate_all_configs_and_references(
         pad_metadata,
         input_padded_width,
         conv_output_shard_height,
-        unpadded_input_shard_height,
+        untilize_with_halo_input_shard_height,
         num_cores,
         filter_h,
         filter_w,
@@ -94,7 +105,7 @@ def test_generate_all_configs_and_references(
     print("Validate tensor metadata")
     validate_tensor_metadata(
         input_tensor,
-        unpadded_input_shard_height,
+        untilize_with_halo_input_shard_height,
         tensor_metadata,
         req_conv_input_shard_start_end,
         golden_conv_input_shards,
