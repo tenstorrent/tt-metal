fdcf8bd3d2d3b05b4d0f0d0dbe62520b13ff9b5c
Bill Teng <bteng@tenstorrent.com>
2023-11-20 23:13:25 +0000

    #3466: update documentation to warn users that only blocking mode of EnqueueReadBuffer is supported

--- tt_metal/host_api.hpp
+++ tt_metal/host_api.hpp
@@ -246,12 +246,12 @@ std::vector<uint32_t>& GetRuntimeArgs(const Program &program, KernelID kernel_id
  *
  * Return value: void
  *
- * | Argument     | Description                                                            | Type                          | Valid Range                        | Required |
- * |--------------|------------------------------------------------------------------------|-------------------------------|------------------------------------|----------|
- * | cq           | The command queue object which dispatches the command to the hardware  | CommandQueue &                |                                    | Yes      |
- * | buffer       | The device buffer we are reading from                                  | Buffer &                      |                                    | Yes      |
- * | dst          | The vector where the results that are read will be stored              | vector<uint32_t> &                 |                                    | Yes      |
- * | blocking     | Whether or not this is a blocking operation                            | bool                          |                                    | Yes      |
+ * | Argument     | Description                                                            | Type                          | Valid Range                            | Required |
+ * |--------------|------------------------------------------------------------------------|-------------------------------|----------------------------------------|----------|
+ * | cq           | The command queue object which dispatches the command to the hardware  | CommandQueue &                |                                        | Yes      |
+ * | buffer       | The device buffer we are reading from                                  | Buffer &                      |                                        | Yes      |
+ * | dst          | The vector where the results that are read will be stored              | vector<uint32_t> &            |                                        | Yes      |
+ * | blocking     | Whether or not this is a blocking operation                            | bool                          | Only blocking mode supported currently | Yes      |
  */
 void EnqueueReadBuffer(CommandQueue& cq, Buffer& buffer, std::vector<uint32_t>& dst, bool blocking);
 
@@ -260,12 +260,12 @@ void EnqueueReadBuffer(CommandQueue& cq, Buffer& buffer, std::vector<uint32_t>& 
  *
  * Return value: void
  *
- * | Argument     | Description                                                            | Type                          | Valid Range                        | Required |
- * |--------------|------------------------------------------------------------------------|-------------------------------|------------------------------------|----------|
- * | cq           | The command queue object which dispatches the command to the hardware  | CommandQueue &                |                                    | Yes      |
- * | buffer       | The device buffer we are reading from                                  | Buffer &                      |                                    | Yes      |
- * | dst          | The memory where the result will be stored                             | void*                         |                                    | Yes      |
- * | blocking     | Whether or not this is a blocking operation                            | bool                          |                                    | Yes      |
+ * | Argument     | Description                                                            | Type                          | Valid Range                            | Required |
+ * |--------------|------------------------------------------------------------------------|-------------------------------|----------------------------------------|----------|
+ * | cq           | The command queue object which dispatches the command to the hardware  | CommandQueue &                |                                        | Yes      |
+ * | buffer       | The device buffer we are reading from                                  | Buffer &                      |                                        | Yes      |
+ * | dst          | The memory where the result will be stored                             | void*                         |                                        | Yes      |
+ * | blocking     | Whether or not this is a blocking operation                            | bool                          | Only blocking mode supported currently | Yes      |
  */
 void EnqueueReadBuffer(CommandQueue& cq, Buffer& buffer, void* dst, bool blocking);
 
