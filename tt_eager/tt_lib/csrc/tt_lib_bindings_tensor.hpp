983104103ccfadc476a8b4987d84a083433183b6
ruthreshk <ruthresh@multicorewareinc.com>
2023-11-22 13:10:43 +0000

    #3889: Add addalpha backward ops

--- tt_eager/tt_lib/csrc/tt_lib_bindings_tensor.hpp
+++ tt_eager/tt_lib/csrc/tt_lib_bindings_tensor.hpp
@@ -11,6 +11,7 @@ namespace tt::tt_metal{
 namespace detail{
     void TensorModuleXaryOPs ( py::module & m_tensor);
     void TensorModuleCompositeOPs( py::module & m_tensor);
+    void TensorModuleBackwardOPs( py::module & m_tensor);
     void TensorModuleDMOPs( py::module & m_tensor);
     void TensorModuleCustomAndBMMOPs( py::module & m_tensor);
     void TensorModulePyTensor( py::module & m_tensor);
