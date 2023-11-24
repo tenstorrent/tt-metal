b27530abcfa4de4214d9db2f59655bb8c96d835d
Raymond Kim <rkim@tenstorrent.com>
2023-11-24 16:20:37 +0000

    #3058: Give first pass at flattening build by getting rid of tt-metal intermediate libs

--- tt_metal/tools/profiler/module.mk
+++ tt_metal/tools/profiler/module.mk
@@ -1,8 +1,6 @@
 # Every variable in subdir must be prefixed with subdir (emulating a namespace)
-
 PROFILER_INCLUDES = $(BASE_INCLUDES)
 
-PROFILER_LIB = $(LIBDIR)/tools/libprofiler.a
 PROFILER_DEFINES =
 PROFILER_INCLUDES += -Itools/profiler
 PROFILER_CFLAGS = $(CFLAGS) -Werror
@@ -16,11 +14,7 @@ PROFILER_DEPS = $(addprefix $(OBJDIR)/, $(PROFILER_SRCS:.cpp=.d))
 -include $(PROFILER_DEPS)
 
 # Each module has a top level target as the entrypoint which must match the subdir name
-tools/profiler: $(PROFILER_LIB)
-
-$(PROFILER_LIB): $(PROFILER_OBJS)
-	@mkdir -p $(@D)
-	ar rcs -o $@ $(PROFILER_OBJS)
+tools/profiler: $(PROFILER_OBJS)
 
 $(OBJDIR)/tt_metal/tools/profiler/%.o: tt_metal/tools/profiler/%.cpp
 	@mkdir -p $(@D)
