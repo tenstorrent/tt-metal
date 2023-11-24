60cf434fe52533ced15980ea6042c4c6c6ff3b7e
umadevimcw <umadevi@multicorewareinc.com>
2023-11-24 07:35:19 +0000

    #3888: Add backward support for mutiplication

--- tt_eager/tt_dnn/op_library/backward/backward_ops.hpp
+++ tt_eager/tt_dnn/op_library/backward/backward_ops.hpp
@@ -20,6 +20,8 @@ std::vector<Tensor> addalpha_bw(const Tensor& grad, const Tensor& input, const T
 
 std::vector<Tensor> unary_mul_bw(const Tensor& grad, const Tensor& input, float scalar, const MemoryConfig& output_mem_config = operation::DEFAULT_OUTPUT_MEMORY_CONFIG);
 
+std::vector<Tensor> mul_bw(const Tensor& grad, const Tensor& input_a, const Tensor& input_b, const MemoryConfig& output_mem_config = operation::DEFAULT_OUTPUT_MEMORY_CONFIG);
+
 } //namespace tt_metal
 
 } //namespace tt
