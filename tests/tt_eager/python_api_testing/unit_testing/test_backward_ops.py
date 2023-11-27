b8c647c9f322c10b568c3175d916c50d94ee37f1
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2023-11-27 10:43:00 +0000

    #3892: Add backward support for unary division op

--- tests/tt_eager/python_api_testing/unit_testing/test_backward_ops.py
+++ tests/tt_eager/python_api_testing/unit_testing/test_backward_ops.py
@@ -238,6 +238,36 @@ class TestBackwardOps:
         logger.info(comp_out)
         assert comp_pass
 
+    @pytest.mark.parametrize("scalar", [0.05, 1.0, 0.5, 0.12])
+    def test_bw_unary_div(self, input_shapes, scalar, device):
+        torch.manual_seed(0)
+        in_data = torch.randn(input_shapes, requires_grad=True).bfloat16()
+        grad_data = torch.randn(input_shapes).bfloat16()
+
+        grad_tensor = (
+            tt_lib.tensor.Tensor(grad_data, tt_lib.tensor.DataType.BFLOAT16).to(tt_lib.tensor.Layout.TILE).to(device)
+        )
+
+        input_tensor = (
+            tt_lib.tensor.Tensor(in_data, tt_lib.tensor.DataType.BFLOAT16).to(tt_lib.tensor.Layout.TILE).to(device)
+        )
+
+        tt_output_tensor_on_device = tt_lib.tensor.unary_div_bw(grad_tensor, input_tensor, scalar=scalar)
+        tt_output_tensor = tt_output_tensor_on_device[0].cpu().to(tt_lib.tensor.Layout.ROW_MAJOR).to_torch()
+
+        in_data.retain_grad()
+
+        pyt_y = torch.div(in_data, torch.tensor(scalar))
+
+        pyt_y.backward(gradient=grad_data)
+
+        golden_output_tensor = in_data.grad
+
+        comp_pass, _ = comparison_funcs.comp_pcc(golden_output_tensor, tt_output_tensor, 0.99)
+        _, comp_out = comparison_funcs.comp_allclose_and_pcc(golden_output_tensor, tt_output_tensor)
+        logger.info(comp_out)
+        assert comp_pass
+
     @pytest.mark.parametrize("value", [0.05, 1.0, 0.5, 0.12])
     def test_bw_addcmul(self, input_shapes, value, device):
         torch.manual_seed(0)
