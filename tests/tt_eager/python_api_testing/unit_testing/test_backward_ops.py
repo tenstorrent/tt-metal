67f0a966b628ae0951c9624c886e772f8dce3cee
ruthreshk <ruthresh@multicorewareinc.com>
2023-11-27 08:39:50 +0000

    #3891: Add backward support for addcmul op

--- tests/tt_eager/python_api_testing/unit_testing/test_backward_ops.py
+++ tests/tt_eager/python_api_testing/unit_testing/test_backward_ops.py
@@ -165,3 +165,61 @@ class TestBackwardOps:
         _, comp_out = comparison_funcs.comp_allclose_and_pcc(golden_output_tensor, tt_output_tensor)
         logger.info(comp_out)
         assert comp_pass
+
+        
+    @pytest.mark.parametrize("value", [0.05, 1.0, 0.5, 0.12])
+    def test_bw_addcmul(self, input_shapes, value, device):
+        torch.manual_seed(0)
+        in_data = torch.randn(input_shapes, requires_grad=True).bfloat16()
+        tensor1_data = torch.randn(input_shapes, requires_grad=True).bfloat16()
+        tensor2_data = torch.randn(input_shapes, requires_grad=True).bfloat16()
+
+        grad_data = torch.randn(input_shapes).bfloat16()
+
+        grad_tensor = (
+            tt_lib.tensor.Tensor(grad_data, tt_lib.tensor.DataType.BFLOAT16).to(tt_lib.tensor.Layout.TILE).to(device)
+        )
+
+        input_tensor = (
+            tt_lib.tensor.Tensor(in_data, tt_lib.tensor.DataType.BFLOAT16).to(tt_lib.tensor.Layout.TILE).to(device)
+        )
+
+        tensor1_tensor = (
+            tt_lib.tensor.Tensor(tensor1_data, tt_lib.tensor.DataType.BFLOAT16).to(tt_lib.tensor.Layout.TILE).to(device)
+        )
+        tensor2_tensor = (
+            tt_lib.tensor.Tensor(tensor2_data, tt_lib.tensor.DataType.BFLOAT16).to(tt_lib.tensor.Layout.TILE).to(device)
+        )
+
+        tt_output_tensor_on_device = tt_lib.tensor.addcmul_bw(
+            grad_tensor, input_tensor, tensor1_tensor, tensor2_tensor, value
+        )
+        tt_output_tensor_a = tt_output_tensor_on_device[0].cpu().to(tt_lib.tensor.Layout.ROW_MAJOR).to_torch()
+        tt_output_tensor_b = tt_output_tensor_on_device[1].cpu().to(tt_lib.tensor.Layout.ROW_MAJOR).to_torch()
+        tt_output_tensor_c = tt_output_tensor_on_device[2].cpu().to(tt_lib.tensor.Layout.ROW_MAJOR).to_torch()
+
+        in_data.retain_grad()
+        tensor1_data.retain_grad()
+        tensor2_data.retain_grad()
+
+        pyt_y = torch.addcmul(in_data, tensor1_data, tensor2_data, value=value)
+
+        pyt_y.backward(gradient=grad_data)
+
+        golden_output_tensor_a = in_data.grad
+        golden_output_tensor_b = tensor1_data.grad
+        golden_output_tensor_c = tensor2_data.grad
+
+        comp_pass_a, _ = comparison_funcs.comp_pcc(golden_output_tensor_a, tt_output_tensor_a, 0.99)
+        _, comp_out_a = comparison_funcs.comp_allclose_and_pcc(golden_output_tensor_a, tt_output_tensor_a)
+
+        comp_pass_b, _ = comparison_funcs.comp_pcc(golden_output_tensor_b, tt_output_tensor_b, 0.99)
+        _, comp_out_b = comparison_funcs.comp_allclose_and_pcc(golden_output_tensor_b, tt_output_tensor_b)
+
+        comp_pass_c, _ = comparison_funcs.comp_pcc(golden_output_tensor_c, tt_output_tensor_c, 0.99)
+        _, comp_out_c = comparison_funcs.comp_allclose_and_pcc(golden_output_tensor_c, tt_output_tensor_c)
+
+        logger.info(comp_out_a)
+        logger.info(comp_out_b)
+        logger.info(comp_out_c)
+        assert comp_pass_a & comp_pass_b & comp_pass_c
