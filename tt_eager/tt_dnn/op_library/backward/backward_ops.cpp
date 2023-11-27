fb8e60f44e1693635d6b971884d770eb2f6af76e
ruthreshk <ruthresh@multicorewareinc.com>
2023-11-27 08:39:50 +0000

    #3891: Add backward support for addcmul op

--- tt_eager/tt_dnn/op_library/backward/backward_ops.cpp
+++ tt_eager/tt_dnn/op_library/backward/backward_ops.cpp
@@ -63,6 +63,21 @@ std::vector<Tensor> exp_bw(const Tensor& grad, const Tensor& exp_result, const M
     return operation::decorate_as_composite(__func__, _exp_bw)(grad, exp_result, output_mem_config);
 }
 
+std::vector<Tensor> _addcmul_bw(const Tensor& grad, const Tensor& input, const Tensor& tensor1, const Tensor& tensor2, float value, const MemoryConfig& output_mem_config) {
+    std::vector<Tensor> grad_tensor;
+    grad_tensor.push_back(grad);
+    Tensor grad_a = mul_unary(mul(grad, tensor2, std::nullopt, output_mem_config), value, output_mem_config);
+    grad_tensor.push_back(grad_a);
+    Tensor grad_b = mul_unary(mul(grad, tensor1, std::nullopt, output_mem_config), value, output_mem_config);
+    grad_tensor.push_back(grad_b);
+
+    return grad_tensor;
+}
+std::vector<Tensor> addcmul_bw(const Tensor& grad, const Tensor& input, const Tensor& tensor1, const Tensor& tensor2, float value, const MemoryConfig& output_mem_config)
+{
+    return operation::decorate_as_composite(__func__, _addcmul_bw)(grad, input, tensor1, tensor2, value, output_mem_config);
+}
+
 }//namespace tt_metal
 
 }//namespace tt
