a104e2b09d4b2edf2d42002bfe61a1a4140f3da5
banekg <barsic@tenstorrent.com>
2023-11-27 11:53:30 +0000

    #3994: Add unit test for emnedding op

---
+++ tests/tt_eager/python_api_testing/non_working_unit_tests/grayskull/test_embeddings.py
@@ -0,0 +1,76 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from pathlib import Path
+import sys
+from loguru import logger
+import random
+import pytest
+import torch
+import tt_lib as ttl
+from functools import partial
+
+from tests.tt_eager.python_api_testing.sweep_tests.common import set_dispatch_mode
+from tests.tt_eager.python_api_testing.sweep_tests import pytorch_ops
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc
+from tests.tt_eager.python_api_testing.sweep_tests.tt_lib_ops import embeddings as tt_embeddings
+
+
+def run_embeddings_test(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, dispatch_mode, device):
+    torch.manual_seed(data_seed)
+    set_dispatch_mode(dispatch_mode)
+
+    if in_mem_config[0] == "SYSTEM_MEMORY":
+        in_mem_config[0] = None
+
+    if in_mem_config[1] == "SYSTEM_MEMORY":
+        in_mem_config[1] = None
+
+    x = torch.Tensor(size=input_shape[0]).uniform_(-100, 100)
+    y = torch.Tensor(size=input_shape[1]).uniform_(-100, 100)
+
+    # compute ref value
+    ref_value = pytorch_ops.embeddings(x, y)
+
+    tt_result = tt_embeddings(
+        x=x,
+        y=y,
+        device=device,
+        dtype=dtype,
+        layout=dlayout,
+        input_mem_config=in_mem_config,
+        output_mem_config=out_mem_config,
+    )
+    # compare tt and golden outputs
+
+    success, pcc_value = comp_pcc(ref_value, tt_result)
+    logger.debug(pcc_value)
+    logger.debug(success)
+
+    assert success
+
+
+test_sweep_args = [
+    (
+        [(1, 1, 192, 1), (1, 1, 512, 2912)],
+        [ttl.tensor.DataType.UINT32, ttl.tensor.DataType.BFLOAT16],
+        [ttl.tensor.Layout.TILE, ttl.tensor.Layout.TILE],
+        [
+            ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+            ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        ],
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        79106,
+        False,
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, dispatch_mode",
+    (test_sweep_args),
+)
+def test_embeddings(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, dispatch_mode, device):
+    random.seed(0)
+    run_embeddings_test(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, dispatch_mode, device)
