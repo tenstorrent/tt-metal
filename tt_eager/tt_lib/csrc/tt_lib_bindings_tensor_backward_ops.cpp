cec11cb0386c67f33936cc9460321e2c6861dbc8
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2023-11-28 05:41:22 +0000

    #3889: Add backward unary add

--- tt_eager/tt_lib/csrc/tt_lib_bindings_tensor_backward_ops.cpp
+++ tt_eager/tt_lib/csrc/tt_lib_bindings_tensor_backward_ops.cpp
@@ -61,6 +61,23 @@ namespace tt::tt_metal::detail{
                     "output_mem_config", "Layout of tensor in TT Accelerator device memory banks", "MemoryConfig", "Default is interleaved in DRAM", "No"
             )doc");
 
+    m_tensor.def("unary_add_bw", &tt::tt_metal::unary_add_bw,
+            py::arg("grad").noconvert(), py::arg("input").noconvert(), py::arg("alpha") = 1.0f, py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, R"doc(
+                Performs backward operations for addition with given ``grad`` and ``alpha``.
+
+                Input tensors must have BFLOAT16 data type.
+
+                Output tensor will have BFLOAT16 data type.
+
+                .. csv-table::
+                    :header: "Argument", "Description", "Data type", "Valid range", "Required"
+
+                    "grad", "Gradient tensor", "Tensor", "Tensor of shape [W, Z, Y, X]", "Yes"
+                    "input", "Tensor", "Tensor", "Tensor of shape [W, Z, Y, X]", "Yes"
+                    "alpha", "Alpha value", "float", "default to 1.0f", "No"
+                    "output_mem_config", "Layout of tensor in TT Accelerator device memory banks", "MemoryConfig", "Default is interleaved in DRAM", "No"
+            )doc");
+
     m_tensor.def("mul_bw", &tt::tt_metal::mul_bw,
             py::arg("grad").noconvert(), py::arg("input_a").noconvert(), py::arg("input_b").noconvert(), py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, R"doc(
                 Performs backward operations for multiplication of two input tensors with given ``grad``
