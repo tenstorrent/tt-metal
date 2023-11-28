25542e5fce9f00f088d2fb93a7d419204250804a
Allan Liu <aliu@tenstorrent.com>
2023-11-28 18:34:37 +0000

    #3434: fix noc init and add some working noc tests for erisc

---
+++ tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/erisc/direct_writer_l1_to_dram.cpp
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <cstdint>
+
+/**
+ * NOC APIs are prefixed w/ "ncrisc" (legacy name) but there's nothing NCRISC specific, they can be used on NCRISC or
+ * other RISCs Any two RISC processors cannot use the same CMD_BUF non_blocking APIs shouldn't be mixed with slow noc.h
+ * APIs explicit flushes need to be used since the calls are non-blocking
+ * */
+void kernel_main() {
+    std::uint32_t dram_buffer_dst_addr_base = get_arg_val<uint32_t>(0);
+    std::uint32_t dram_dst_noc_x = get_arg_val<uint32_t>(1);
+    std::uint32_t dram_dst_noc_y = get_arg_val<uint32_t>(2);
+    std::uint32_t dram_buffer_size = get_arg_val<uint32_t>(3);
+    std::uint32_t local_eth_l1_addr_base = get_arg_val<uint32_t>(4);
+
+    std::uint32_t dram_buffer_dst_addr = dram_buffer_dst_addr_base;
+
+    // DRAM NOC dst address
+    std::uint64_t dram_buffer_dst_noc_addr = get_noc_addr(dram_dst_noc_x, dram_dst_noc_y, dram_buffer_dst_addr);
+
+    noc_async_write(local_eth_l1_addr_base, dram_buffer_dst_noc_addr, dram_buffer_size);
+    noc_async_write_barrier();
+}
--- tt_metal/hw/firmware/src/erisck.cc
+++ tt_metal/hw/firmware/src/erisck.cc
@@ -27,30 +27,32 @@ CQReadInterface cq_read_interface;
 
 void ApplicationHandler(void) __attribute__((__section__(".init")));
 
-uint32_t noc_reads_num_issued[NUM_NOCS];
-uint32_t noc_nonposted_writes_num_issued[NUM_NOCS];
-uint32_t noc_nonposted_writes_acked[NUM_NOCS];
-uint32_t noc_xy_local_addr[NUM_NOCS];
+uint8_t my_x[NUM_NOCS] __attribute__((used));
+uint8_t my_y[NUM_NOCS] __attribute__((used));
 
+uint32_t noc_reads_num_issued[NUM_NOCS] __attribute__((used));
+uint32_t noc_nonposted_writes_num_issued[NUM_NOCS] __attribute__((used));
+uint32_t noc_nonposted_writes_acked[NUM_NOCS] __attribute__((used));
 uint8_t noc_index = NOC_INDEX;
 
-void __attribute__((section("code_l1"))) risc_init();
+void __attribute__((section("code_l1"))) risc_init() {
+    for (uint32_t n = 0; n < NUM_NOCS; n++) {
+        uint32_t noc_id_reg = NOC_CMD_BUF_READ_REG(n, 0, NOC_NODE_ID);
+        my_x[n] = noc_id_reg & NOC_NODE_ID_MASK;
+        my_y[n] = (noc_id_reg >> NOC_ADDR_NODE_ID_BITS) & NOC_NODE_ID_MASK;
+    }
+}
 
 void __attribute__((__section__("erisc_l1_code"))) kernel_launch() {
-    RISC_POST_STATUS(0x12345678);
-    kernel_main();
-    RISC_POST_STATUS(0x12345679);
-    disable_erisc_app();
-}
-void __attribute__((section("erisc_l1_code"))) ApplicationHandler(void) {
     rtos_context_switch_ptr = (void (*)())RtosTable[0];
-
     noc_init();
-
+    for (uint32_t n = 0; n < NUM_NOCS; n++) {
+        noc_local_state_init(n);
+    }
+    ncrisc_noc_full_sync();
     risc_init();
 
-    setup_cb_read_write_interfaces(0, 1, true, true);
-    noc_local_state_init(noc_index);
-    // TODO:: if bytes done not updated, yield to task 1
-    kernel_launch();
+    setup_cb_read_write_interfaces(0, 0, true, true);
+    kernel_main();
+    disable_erisc_app();
 }
