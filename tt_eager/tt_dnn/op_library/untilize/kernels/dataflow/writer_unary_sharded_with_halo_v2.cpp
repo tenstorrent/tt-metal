9907014f4fa07167fff56417d14c273a216893e7
Abhinav <asarje@tenstorrent.com>
2023-11-28 04:33:18 +0000

    #0: Debugging of utwhv2 host code ongoing

--- tt_eager/tt_dnn/op_library/untilize/kernels/dataflow/writer_unary_sharded_with_halo_v2.cpp
+++ tt_eager/tt_dnn/op_library/untilize/kernels/dataflow/writer_unary_sharded_with_halo_v2.cpp
@@ -5,21 +5,21 @@
 #include <stdint.h>
 #include "dataflow_api.h"
 
-// #include "debug_print.h"
+#include "debug/dprint.h"
 
-// SliceRange srr = SliceRange{ .h0 = 0, .h1 = 1, .hs = 8, .w0 = 0, .w1 = 32, .ws = 1 };
-// SliceRange srt = SliceRange{ .h0 = 0, .h1 = 8, .hs = 1, .w0 = 0, .w1 = 4, .ws = 1 };
+SliceRange srr = SliceRange{ .h0 = 0, .h1 = 1, .hs = 8, .w0 = 0, .w1 = 32, .ws = 1 };
+SliceRange srt = SliceRange{ .h0 = 0, .h1 = 8, .hs = 1, .w0 = 0, .w1 = 4, .ws = 1 };
 
-// inline void print_sticks(uint32_t l1_addr, uint32_t stick_start, uint32_t nsticks, uint32_t stick_size = 64) {
-//     for (uint32_t i = stick_start; i < stick_start + nsticks; ++ i) {
-//         volatile tt_l1_ptr uint16_t* l1_ptr = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(l1_addr + i * stick_size * 2);
-//         DPRINT << i << ": ";
-//         for (uint32_t j = 0; j < stick_size; ++ j) {
-//             DPRINT << BF16(l1_ptr[j]) << " ";
-//         }
-//         DPRINT << ENDL();
-//     }
-// }
+inline void print_sticks(uint32_t l1_addr, uint32_t stick_start, uint32_t nsticks, uint32_t stick_size = 64) {
+    for (uint32_t i = stick_start; i < stick_start + nsticks; ++ i) {
+        volatile tt_l1_ptr uint16_t* l1_ptr = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(l1_addr + i * stick_size * 2);
+        DPRINT << i << ": ";
+        for (uint32_t j = 0; j < stick_size; ++ j) {
+            DPRINT << BF16(l1_ptr[j]) << " ";
+        }
+        DPRINT << ENDL();
+    }
+}
 
 // Fill an L1 buffer with the given val
 inline bool fill_with_val(uint32_t begin_addr, uint32_t n, uint16_t val) {
@@ -38,10 +38,22 @@ inline void push_to_neighbor_async(uint32_t noc_x,
                                    uint32_t in_l1_addr,
                                    uint32_t out_l1_addr,
                                    uint32_t stick_nbytes) {
-    volatile tt_l1_ptr uint16_t* data_ss = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(get_read_ptr(data_ss_cb_id));
-    for (uint32_t i = 0; i < 2 * data_nsegments; i += 2) {
-        uint32_t dst_addr = out_l1_addr + data_ss[i];
-        uint32_t seg_nbytes = (uint32_t) data_ss[i + 1] * stick_nbytes;
+    // volatile tt_l1_ptr uint16_t* data_ss = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(get_read_ptr(data_ss_cb_id));
+    // for (uint32_t i = 0; i < 2 * data_nsegments; i += 2) {
+    //     uint32_t dst_addr = out_l1_addr + data_ss[i];
+    //     uint32_t seg_nbytes = (uint32_t) data_ss[i + 1] * stick_nbytes;
+    //     noc_async_write(in_l1_addr, get_noc_addr(noc_x, noc_y, dst_addr), seg_nbytes);
+    //     in_l1_addr += seg_nbytes;
+    // }
+    volatile tt_l1_ptr uint32_t* data_ss = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(get_read_ptr(data_ss_cb_id));
+    for (uint32_t i = 0; i < data_nsegments; ++ i) {
+        uint32_t start_and_size = data_ss[i];
+        uint16_t dst_start = (uint16_t) ((start_and_size << 16) >> 16);
+        uint16_t dst_size = (uint16_t) (start_and_size >> 16);
+        // DPRINT << "DST: " << (uint) dst_start << ENDL();
+        // DPRINT << "SZ: " << (uint) dst_size << ENDL();
+        uint32_t dst_addr = out_l1_addr + (uint32_t) dst_start * stick_nbytes;
+        uint32_t seg_nbytes = (uint32_t) dst_size * stick_nbytes;
         noc_async_write(in_l1_addr, get_noc_addr(noc_x, noc_y, dst_addr), seg_nbytes);
         in_l1_addr += seg_nbytes;
     }
@@ -61,9 +73,10 @@ void kernel_main() {
     constexpr uint32_t pad_stick_len        = get_compile_time_arg_val(10); // pad stick size in nelems (post untilize)
     constexpr uint32_t stick_nbytes         = get_compile_time_arg_val(11); // stick size in bytes (post untilize)
 
-    constexpr nbytes = 2;   // TODO: pass this in
+    constexpr uint32_t nbytes = 2;   // TODO: pass this in
 
     static_assert(stick_nbytes <= NOC_MAX_BURST_SIZE); // stick_nbytes used in noc_async_read_one_packet
+    static_assert(stick_nbytes == pad_stick_len * nbytes);
 
     // construct the pad stick in its buffer
     cb_reserve_back(pad_cb_id, 1);
@@ -86,46 +99,76 @@ void kernel_main() {
     uint32_t rr_noc_x   = get_arg_val<uint32_t>(11);
     uint32_t rr_noc_y   = get_arg_val<uint32_t>(12);
 
-    uint32_t local_pad_nsegments            = get_arg_val<uint32_t>(13);
-    uint32_t local_data_src_start_offset    = get_arg_val<uint32_t>(14);
-    uint32_t local_data_nsegments           = get_arg_val<uint32_t>(15);
-    uint32_t ll_data_src_start_offset       = get_arg_val<uint32_t>(16);
-    uint32_t ll_data_nsegments              = get_arg_val<uint32_t>(17);
-    uint32_t l_data_src_start_offset        = get_arg_val<uint32_t>(18);
-    uint32_t l_data_nsegments               = get_arg_val<uint32_t>(19);
-    uint32_t r_data_src_start_offset        = get_arg_val<uint32_t>(20);
-    uint32_t r_data_nsegments               = get_arg_val<uint32_t>(21);
-    uint32_t rr_data_src_start_offset       = get_arg_val<uint32_t>(22);
-    uint32_t rr_data_nsegments              = get_arg_val<uint32_t>(23);
+    int32_t local_pad_nsegments            = get_arg_val<int32_t>(13);
+    int32_t local_data_src_start_offset    = get_arg_val<int32_t>(14);
+    int32_t local_data_nsegments           = get_arg_val<int32_t>(15);
+    int32_t ll_data_src_start_offset       = get_arg_val<int32_t>(16);
+    int32_t ll_data_nsegments              = get_arg_val<int32_t>(17);
+    int32_t l_data_src_start_offset        = get_arg_val<int32_t>(18);
+    int32_t l_data_nsegments               = get_arg_val<int32_t>(19);
+    int32_t r_data_src_start_offset        = get_arg_val<int32_t>(20);
+    int32_t r_data_nsegments               = get_arg_val<int32_t>(21);
+    int32_t rr_data_src_start_offset       = get_arg_val<int32_t>(22);
+    int32_t rr_data_nsegments              = get_arg_val<int32_t>(23);
 
     cb_wait_front(in_cb_id, in_nsticks);    // make sure untilized data is available
 
     const uint32_t in_base_l1_addr = get_read_ptr(in_cb_id);
     const uint32_t out_base_l1_addr = get_write_ptr(out_cb_id);
 
+    DPRINT << "IN:" << ENDL();
+    print_sticks(in_base_l1_addr, 0, 300, 32);
+
     uint32_t in_l1_addr = in_base_l1_addr;
 
     // insert all padding locally
     if (local_pad_nsegments > 0) {
-        cb_wait_front(local_pad_ss_cb_id, 1);
+        // cb_wait_front(local_pad_ss_cb_id, 1);
         uint32_t local_pad_ss_l1_addr = get_read_ptr(local_pad_ss_cb_id);
-        volatile tt_l1_ptr uint16_t* local_pad_ss = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(local_pad_ss_l1_addr);
-        for (uint32_t i = 0; i < 2 * local_pad_nsegments; i += 2) {
-            uint32_t dst_addr = out_base_l1_addr + local_pad_ss[i];
-            uint32_t size = (uint32_t) local_pad_ss[i + 1] * stick_nbytes;
-            noc_async_read(padding_noc_addr, dst_addr, size);
+        // volatile tt_l1_ptr uint16_t* local_pad_ss = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(local_pad_ss_l1_addr);
+        // for (int32_t i = 0; i < 2 * local_pad_nsegments; i += 2) {
+        //     uint32_t dst_addr = out_base_l1_addr + local_pad_ss[i];
+        //     uint32_t size = (uint32_t) local_pad_ss[i + 1] * stick_nbytes;
+        //     DPRINT << "DST: " << dst_addr << ENDL();
+        //     DPRINT << "SZ: " << size << ENDL();
+        //     noc_async_read(padding_noc_addr, dst_addr, size);
+        // }
+        volatile tt_l1_ptr uint32_t* local_pad_ss = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(local_pad_ss_l1_addr);
+        for (int32_t i = 0; i < local_pad_nsegments; ++ i) {
+            uint32_t start_and_size = local_pad_ss[i];
+            uint16_t dst_start = (uint16_t) ((start_and_size << 16) >> 16);
+            uint16_t dst_size = (uint16_t) (start_and_size >> 16);
+            // DPRINT << "SS: " << start_and_size << ENDL();
+            // DPRINT << "DST: " << (uint) dst_start << ENDL();
+            // DPRINT << "SZ: " << (uint) dst_size << ENDL();
+            uint32_t dst_addr = out_base_l1_addr + (uint32_t) dst_start * stick_nbytes;
+            for (int32_t j = 0; j < dst_size; ++ j) {
+                noc_async_read(padding_noc_addr, dst_addr, stick_nbytes);
+                dst_addr += stick_nbytes;
+            }
         }
+        // print_sticks(out_base_l1_addr, 0, 60, 32);
     }
 
     // then insert all local data
     if (local_data_nsegments > 0) {
-        cb_wait_front(local_data_ss_cb_id, 1);
+        // cb_wait_front(local_data_ss_cb_id, 1);
         in_l1_addr = in_base_l1_addr + local_data_src_start_offset;
         uint32_t local_data_ss_l1_addr = get_read_ptr(local_data_ss_cb_id);
-        volatile tt_l1_ptr uint16_t* local_data_ss = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(local_data_ss_l1_addr);
-        for (uint32_t i = 0; i < 2 * local_data_nsegments; i += 2) {
-            uint32_t dst_addr = out_base_l1_addr + local_data_ss[i];
-            uint32_t size = (uint32_t) local_data_ss[i + 1] * stick_nbytes;
+        // volatile tt_l1_ptr uint16_t* local_data_ss = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(local_data_ss_l1_addr);
+        // for (int32_t i = 0; i < 2 * local_data_nsegments; i += 2) {
+        //     uint32_t dst_addr = out_base_l1_addr + local_data_ss[i];
+        //     uint32_t size = (uint32_t) local_data_ss[i + 1] * stick_nbytes;
+        //     noc_async_read(get_noc_addr(in_l1_addr), dst_addr, size);
+        //     in_l1_addr += size;
+        // }
+        volatile tt_l1_ptr uint32_t* local_data_ss = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(local_data_ss_l1_addr);
+        for (int32_t i = 0; i < local_data_nsegments; ++ i) {
+            uint32_t start_and_size = local_data_ss[i];
+            uint16_t dst_start = (uint16_t) ((start_and_size << 16) >> 16);
+            uint16_t dst_size = (uint16_t) (start_and_size >> 16);
+            uint32_t dst_addr = out_base_l1_addr + (uint32_t) dst_start * stick_nbytes;
+            uint32_t size = (uint32_t) dst_size * stick_nbytes;
             noc_async_read(get_noc_addr(in_l1_addr), dst_addr, size);
             in_l1_addr += size;
         }
@@ -133,22 +176,25 @@ void kernel_main() {
 
     // push data to neighbors
     if (has_ll && ll_data_nsegments > 0) {
-        cb_wait_front(ll_data_ss_cb_id, 1);
-        push_to_neighbor_async(ll_noc_x, ll_noc_y, ll_data_ss_cb_id, ll_data_nsegments, in_base_l1_addr + ll_data_src_start_offset, out_base_l1_addr, stick_nbytes);
+        // cb_wait_front(ll_data_ss_cb_id, 1);
+        push_to_neighbor_async(ll_noc_x, ll_noc_y, ll_data_ss_cb_id, ll_data_nsegments, in_base_l1_addr + ll_data_src_start_offset * stick_nbytes, out_base_l1_addr, stick_nbytes);
     }
     if (has_l && l_data_nsegments > 0) {
-        cb_wait_front(l_data_ss_cb_id, 1);
-        push_to_neighbor_async(l_noc_x, l_noc_y, l_data_ss_cb_id, l_data_nsegments, in_base_l1_addr + l_data_src_start_offset, out_base_l1_addr, stick_nbytes);
+        // cb_wait_front(l_data_ss_cb_id, 1);
+        push_to_neighbor_async(l_noc_x, l_noc_y, l_data_ss_cb_id, l_data_nsegments, in_base_l1_addr + l_data_src_start_offset * stick_nbytes, out_base_l1_addr, stick_nbytes);
     }
     if (has_r && r_data_nsegments > 0) {
-        cb_wait_front(r_data_ss_cb_id, 1);
-        push_to_neighbor_async(r_noc_x, r_noc_y, r_data_ss_cb_id, r_data_nsegments, in_base_l1_addr + r_data_src_start_offset, out_base_l1_addr, stick_nbytes);
+        // cb_wait_front(r_data_ss_cb_id, 1);
+        push_to_neighbor_async(r_noc_x, r_noc_y, r_data_ss_cb_id, r_data_nsegments, in_base_l1_addr + r_data_src_start_offset * stick_nbytes, out_base_l1_addr, stick_nbytes);
     }
     if (has_rr && rr_data_nsegments > 0) {
-        cb_wait_front(rr_data_ss_cb_id, 1);
-        push_to_neighbor_async(rr_noc_x, rr_noc_y, rr_data_ss_cb_id, rr_data_nsegments, in_base_l1_addr + rr_data_src_start_offset, out_base_l1_addr, stick_nbytes);
+        // cb_wait_front(rr_data_ss_cb_id, 1);
+        push_to_neighbor_async(rr_noc_x, rr_noc_y, rr_data_ss_cb_id, rr_data_nsegments, in_base_l1_addr + rr_data_src_start_offset * stick_nbytes, out_base_l1_addr, stick_nbytes);
     }
 
     noc_async_read_barrier();
     noc_async_write_barrier();
+
+    DPRINT << "OUT:" << ENDL();
+    print_sticks(out_base_l1_addr, 0, 500, 32);
 }
