299f0817093f515384dda31b6f55341a7a1e9dbe
Muthu <muthu@tenstorrent.com>
2023-11-30 20:05:05 +0000

    #3211: [Bug Report] Sum result is invalid in WH B0 arch #3262: operator sum at dimension w fails with poor pcc

--- tt_eager/tt_dnn/op_library/reduce/reduce_op.cpp
+++ tt_eager/tt_dnn/op_library/reduce/reduce_op.cpp
@@ -215,7 +215,13 @@ Tensor sum(const Tensor &input_tensor, uint dim, const MemoryConfig& output_mem_
     constexpr float scaler1 = 1.0;
 
     if ( dim == 3 ) {
+      if (is_arch_whb0(input_tensor.device()->arch())) {
+        Tensor output = transpose(input_tensor, -1, -2, output_mem_config);
+        output = sum(output, 2, output_mem_config);
+        return transpose(output, -1, -2, output_mem_config);
+      } else {
         return reduce(input_tensor, ReduceOpMath::SUM, ReduceOpDim::W, scaler1, output_mem_config);
+      }
     } else if ( dim == 2 ) {
         return reduce(input_tensor, ReduceOpMath::SUM, ReduceOpDim::H, scaler1, output_mem_config);
     }
