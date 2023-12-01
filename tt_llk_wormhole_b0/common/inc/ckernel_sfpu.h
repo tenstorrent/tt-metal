d19f8c74415a044bb1594b53eba02e61fc727fee
Pavle Josipovic <pjosipovic@tenstorrent.com>
2023-12-01 12:50:42 +0000

    Add binary add for Int32 inputs
    * Add binary add for Int32 inputs
    
    Issue #2286
    
    Add support for binary add for Int32 inputs.
    This change only adds support for standalone version of the op,
    fused op support is not implemented yet.
    Sub and mul are not supported.
    
    Int8 and Int32 can be mixed on inputs.
    
    Op is implemented as binary sfpu op, and probably has signifiicantly
    different perf profile to other binary add versions.
    Perf estimates are not available yet.

--- tt_llk_wormhole_b0/common/inc/ckernel_sfpu.h
+++ tt_llk_wormhole_b0/common/inc/ckernel_sfpu.h
@@ -1320,6 +1320,25 @@ inline void _dequant_int32_(const int iterations, const uint dst_offset)
     }
 }
 
+template <bool APPROXIMATION_MODE, int ITERATIONS>
+inline void _add_int32_(const int iterations, const uint dst_offset) {
+    // Operand A is input1 (int32)
+    // Operand B is input2 (int32)
+    // Output is int32
+    #pragma GCC unroll 8
+    for (int d = 0; d < ITERATIONS; d++) {
+        // operand A - int32
+        TTI_SFPLOAD(0, 12, 3, 0);
+        // operand B - int32
+        TT_SFPLOAD(1, 12, 3, dst_offset * 64);
+        TTI_SFPIADD(0, 1, 0, 4);
+        // MAD has a 2-cycle pipeline latency so we need one cycle latency until next instr can consume the result
+        TTI_NOP;
+        // LREG_0 -> dest as int32
+        TTI_SFPSTORE(0, 12, 3, 0);
+        dst_reg++;
+    }
+}
 
 } // namespace sfpu
 } // namespace ckernel
