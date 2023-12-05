2dd81bc3aef802a7f1daa8e84ec0f07247cf3bfc
tt-aho <aho@tenstorrent.com>
2023-12-05 17:51:34 +0000

    #4143: Update/add unit tests for transferring interleaved buffers between L and R chips

--- tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/erisc/interleaved_buffer_to_buffer_receiver.cpp
+++ tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/erisc/interleaved_buffer_to_buffer_receiver.cpp
@@ -6,38 +6,42 @@
 
 void kernel_main() {
     uint32_t dst_addr = get_arg_val<uint32_t>(0);
-    uint32_t num_tiles = get_arg_val<uint32_t>(1);
-    uint32_t start_id = get_arg_val<uint32_t>(2);
-    uint32_t tile_bytes = get_arg_val<uint32_t>(3);
+    uint32_t page_size = get_arg_val<uint32_t>(1);
+    uint32_t max_buffer_size = get_arg_val<uint32_t>(2);
+    uint32_t num_loops = get_arg_val<uint32_t>(3);
+    uint32_t pages_per_loop = get_arg_val<uint32_t>(4);
+    uint32_t remaining_bytes = get_arg_val<uint32_t>(5);
+    uint32_t remaining_pages = get_arg_val<uint32_t>(6);
 
     constexpr bool dst_is_dram = get_compile_time_arg_val(0) == 1;
 
-#ifdef OUT_SHARDED
-    cb_wait_front(cb_id_out, num_tiles);
-#else
+    const InterleavedAddrGen<dst_is_dram> s = {
+        .bank_base_address = dst_addr, .page_size = page_size};
 
-    // single-tile ublocks
-    constexpr uint32_t onetile = 1;
-    DataFormat data_format = DataFormat::Float16_b;
-
-    const InterleavedAddrGenFast<dst_is_dram> s = {
-        .bank_base_address = dst_addr, .page_size = tile_bytes, .data_format = data_format};
-
-#ifdef BACKWARDS
-    uint32_t end_id = start_id - num_tiles;
-    for (uint32_t i = start_id; i != end_id; --i) {
-#else
-    uint32_t end_id = start_id + num_tiles;
-    for (uint32_t i = start_id; i < end_id; ++i) {
-#endif
-        // cb_wait_front(cb_id_out, onetile);
+    uint32_t page_idx = 0;
+    for (uint32_t i = 0; i < num_loops; ++i) {
         uint32_t l1_read_addr = eth_l1_mem::address_map::ERISC_L1_UNRESERVED_BASE;
-        eth_wait_for_bytes(tile_bytes);
+        eth_wait_for_bytes(max_buffer_size);
 
-        noc_async_write_tile(i, s, l1_read_addr);
-        noc_async_write_barrier();
-
+        for (uint32_t j = 0; j < pages_per_loop; ++j) {
+            uint64_t dst_noc_addr = get_noc_addr(page_idx, s);
+            noc_async_write(l1_read_addr, dst_noc_addr, page_size);
+            page_idx++;
+            l1_read_addr += page_size;
+            noc_async_write_barrier();
+        }
         eth_receiver_done();
     }
-#endif
+    if (remaining_bytes > 0) {
+        uint32_t l1_read_addr = eth_l1_mem::address_map::ERISC_L1_UNRESERVED_BASE;
+        eth_wait_for_bytes(remaining_bytes);
+        for (uint32_t j = 0; j < remaining_pages; ++j) {
+            uint64_t dst_noc_addr = get_noc_addr(page_idx, s);
+            noc_async_write(l1_read_addr, dst_noc_addr, page_size);
+            page_idx++;
+            l1_read_addr += page_size;
+            noc_async_write_barrier();
+        }
+        eth_receiver_done();
+    }
 }
--- tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/erisc/interleaved_buffer_to_buffer_sender.cpp
+++ tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/erisc/interleaved_buffer_to_buffer_sender.cpp
@@ -6,40 +6,49 @@
 
 #include "dataflow_api.h"
 
-//#include "debug/dprint.h"
-
 void kernel_main() {
     uint32_t src_addr = get_arg_val<uint32_t>(0);
-    uint32_t num_tiles = get_arg_val<uint32_t>(1);
-    uint32_t start_id = get_arg_val<uint32_t>(2);
-    uint32_t tile_bytes = get_arg_val<uint32_t>(3);
+    uint32_t page_size = get_arg_val<uint32_t>(1);
+    uint32_t max_buffer_size = get_arg_val<uint32_t>(2);
+    uint32_t num_loops = get_arg_val<uint32_t>(3);
+    uint32_t pages_per_loop = get_arg_val<uint32_t>(4);
+    uint32_t remaining_bytes = get_arg_val<uint32_t>(5);
+    uint32_t remaining_pages = get_arg_val<uint32_t>(6);
 
     constexpr bool src_is_dram = get_compile_time_arg_val(0) == 1;
 
-    constexpr uint32_t cb_id_in0 = 0;
+    const InterleavedAddrGen<src_is_dram> s = {
+        .bank_base_address = src_addr, .page_size = page_size};
 
-    // ublocks size defined in tiles
-    constexpr uint32_t onetile = 1;
-    DataFormat data_format = DataFormat::Float16_b;
-
-    const InterleavedAddrGenFast<src_is_dram> s = {
-        .bank_base_address = src_addr, .page_size = tile_bytes, .data_format = data_format};
-
-// read a ublock of tiles from src to CB, and then push the ublock to unpacker
-#ifdef BACKWARDS
-    uint32_t end_id = start_id - num_tiles;
-    for (uint32_t i = start_id; i != end_id; --i) {
-#else
-    uint32_t end_id = start_id + num_tiles;
-    for (uint32_t i = start_id; i < end_id; ++i) {
-#endif
+    uint32_t page_idx = 0;
+    for (uint32_t i = 0; i < num_loops; ++i) {
         uint32_t l1_write_addr = eth_l1_mem::address_map::ERISC_L1_UNRESERVED_BASE;
-        noc_async_read_tile(i, s, l1_write_addr);
-        noc_async_read_barrier();
+        for (uint32_t j = 0; j < pages_per_loop; ++j) {
+            uint64_t src_noc_addr = get_noc_addr(page_idx, s);
+            noc_async_read(src_noc_addr, l1_write_addr, page_size);
+            page_idx++;
+            l1_write_addr += page_size;
+            noc_async_read_barrier();
+        }
         eth_send_bytes(
             eth_l1_mem::address_map::ERISC_L1_UNRESERVED_BASE,
             eth_l1_mem::address_map::ERISC_L1_UNRESERVED_BASE,
-            tile_bytes);
+            max_buffer_size);
+        eth_wait_for_receiver_done();
+    }
+    if (remaining_bytes > 0) {
+        uint32_t l1_write_addr = eth_l1_mem::address_map::ERISC_L1_UNRESERVED_BASE;
+        for (uint32_t j = 0; j < remaining_pages; ++j) {
+            uint64_t src_noc_addr = get_noc_addr(page_idx, s);
+            noc_async_read(src_noc_addr, l1_write_addr, page_size);
+            page_idx++;
+            l1_write_addr += page_size;
+            noc_async_read_barrier();
+        }
+        eth_send_bytes(
+            eth_l1_mem::address_map::ERISC_L1_UNRESERVED_BASE,
+            eth_l1_mem::address_map::ERISC_L1_UNRESERVED_BASE,
+            remaining_bytes);
         eth_wait_for_receiver_done();
     }
 }
