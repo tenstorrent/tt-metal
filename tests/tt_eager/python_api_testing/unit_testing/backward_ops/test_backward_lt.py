2bf779c2fd2df4db134cc3e4ddd4a1fa9ca65a9d
Bharane AB <bharane.amutha@multicorewareinc.com>
2023-12-14 12:58:45 +0000

    #4341: Add backward support for LT, GT and NE op

---
+++ tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_lt.py
@@ -0,0 +1,38 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+import pytest
+import tt_lib
+from tests.tt_eager.python_api_testing.sweep_tests import (
+    comparison_funcs,
+)
+from loguru import logger
+
+
+@pytest.mark.parametrize(
+    "input_shapes",
+    (
+        (torch.Size([1, 1, 32, 32])),
+        (torch.Size([1, 1, 320, 384])),
+        (torch.Size([1, 3, 320, 384])),
+    ),
+)
+def test_bw_unary_lt(input_shapes, device):
+    torch.manual_seed(12386)
+    grad_data = torch.randn(input_shapes).bfloat16()
+
+    grad_tensor = (
+        tt_lib.tensor.Tensor(grad_data, tt_lib.tensor.DataType.BFLOAT16).to(tt_lib.tensor.Layout.TILE).to(device)
+    )
+    tt_output_tensor_on_device = tt_lib.tensor.lt_bw(grad_tensor)
+    pyt_y = torch.zeros_like(grad_data)
+
+    tt_output_tensor = tt_output_tensor_on_device[0].cpu().to(tt_lib.tensor.Layout.ROW_MAJOR).to_torch()
+
+    golden_output_tensor = pyt_y
+
+    comp_pass, comp_out = comparison_funcs.comp_pcc(golden_output_tensor, tt_output_tensor)
+    logger.info(comp_out)
+    assert comp_pass
