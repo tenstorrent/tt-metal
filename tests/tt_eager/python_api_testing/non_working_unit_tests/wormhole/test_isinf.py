f52559d0989c300e5114a929ceb00ae77546cf4c
JelenaTosicJtosic <jtosic@tenstorrent.com>
2023-12-14 09:15:00 +0000

    #4343: Add new op sweeps and units for GS and WH

---
+++ tests/tt_eager/python_api_testing/non_working_unit_tests/wormhole/test_isinf.py
@@ -0,0 +1,76 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from pathlib import Path
+import sys
+from loguru import logger
+import random
+import pytest
+import torch
+import tt_lib as ttl
+
+from tests.tt_eager.python_api_testing.sweep_tests.common import set_slow_dispatch_mode
+from tests.tt_eager.python_api_testing.sweep_tests import pytorch_ops
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_equal
+from tests.tt_eager.python_api_testing.sweep_tests import tt_lib_ops
+from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_rand_inf
+
+
+def run_eltwise_isinf_test(
+    input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, dispatch_mode, device
+):
+    torch.manual_seed(data_seed)
+    prev_dispatch_mode = set_slow_dispatch_mode(dispatch_mode)
+
+    x = gen_rand_inf(size=input_shape, low=-100, high=100)
+
+    # compute ref value
+    ref_value = pytorch_ops.isinf(x=x)
+
+    tt_result = tt_lib_ops.eltwise_isinf(
+        x=x,
+        device=device,
+        dtype=dtype,
+        layout=dlayout,
+        input_mem_config=in_mem_config,
+        output_mem_config=out_mem_config,
+    )
+
+    success, pcc_value = comp_equal(ref_value, tt_result)
+    logger.debug(pcc_value)
+    logger.debug(success)
+
+    set_slow_dispatch_mode(prev_dispatch_mode)
+    assert success
+
+
+test_sweep_args = [
+    (
+        (7, 14, 32, 160),
+        [ttl.tensor.DataType.BFLOAT16],
+        [ttl.tensor.Layout.TILE],
+        [None],
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        16305027,
+        "",
+    ),
+    (
+        (5, 10, 35, 162),
+        [ttl.tensor.DataType.BFLOAT16],
+        [ttl.tensor.Layout.ROW_MAJOR],
+        [ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1)],
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        3624344,
+        "1",
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, dispatch_mode",
+    (test_sweep_args),
+)
+def test_eltwise_isinf(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, dispatch_mode, device):
+    random.seed(0)
+    run_eltwise_isinf_test(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, dispatch_mode, device)
