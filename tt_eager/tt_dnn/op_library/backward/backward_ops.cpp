42c6503613d3a169883ce596f1a20adab553c2fb
umadevimcw <umadevi@multicorewareinc.com>
2023-12-15 08:04:07 +0000

    #0: Use emplace_back module for bw ops

--- tt_eager/tt_dnn/op_library/backward/backward_ops.cpp
+++ tt_eager/tt_dnn/op_library/backward/backward_ops.cpp
@@ -16,9 +16,9 @@ namespace tt_metal {
 
 std::vector<Tensor> _addalpha_bw(const Tensor& grad, const Tensor& input, const Tensor& other, float alpha, const MemoryConfig& output_mem_config) {
     std::vector<Tensor> grad_tensor;
-    grad_tensor.push_back(grad);
+    grad_tensor.emplace_back(grad);
     Tensor grad_b = mul_unary(grad, alpha, output_mem_config);
-    grad_tensor.push_back(grad_b);
+    grad_tensor.emplace_back(grad_b);
 
     return grad_tensor;
 }
@@ -35,7 +35,7 @@ std::vector<Tensor> add_bw(const Tensor& grad, const Tensor& input, const Tensor
 std::vector<Tensor> _unary_mul_bw(const Tensor& grad, const Tensor& input, float scalar, const MemoryConfig& output_mem_config) {
     std::vector<Tensor> grad_tensor;
     Tensor result = mul_unary(grad, scalar, output_mem_config);
-    grad_tensor.push_back(result);
+    grad_tensor.emplace_back(result);
     return grad_tensor;
 }
 std::vector<Tensor> unary_mul_bw(const Tensor& grad, const Tensor& input, float scalar, const MemoryConfig& output_mem_config)
@@ -49,7 +49,7 @@ std::vector<Tensor> _unary_pow_bw(const Tensor& grad, const Tensor& input, float
 
     Tensor result = mul_unary(power_input, exponent, output_mem_config);
     Tensor final_result = mul(result, grad, std::nullopt, output_mem_config);
-    grad_tensor.push_back(final_result);
+    grad_tensor.emplace_back(final_result);
     return grad_tensor;
 }
 std::vector<Tensor> unary_pow_bw(const Tensor& grad, const Tensor& input, float exponent, const MemoryConfig& output_mem_config)
@@ -59,7 +59,7 @@ std::vector<Tensor> unary_pow_bw(const Tensor& grad, const Tensor& input, float 
 
 std::vector<Tensor> _unary_add_bw(const Tensor& grad, const Tensor& input, float alpha, const MemoryConfig& output_mem_config) {
     std::vector<Tensor> grad_tensor;
-    grad_tensor.push_back(grad);
+    grad_tensor.emplace_back(grad);
     return grad_tensor;
 }
 std::vector<Tensor> unary_add_bw(const Tensor& grad, const Tensor& input, float alpha, const MemoryConfig& output_mem_config)
@@ -71,9 +71,9 @@ std::vector<Tensor> unary_add_bw(const Tensor& grad, const Tensor& input, float 
 std::vector<Tensor> _mul_bw(const Tensor& grad, const Tensor& input_a, const Tensor& input_b, const MemoryConfig& output_mem_config) {
     std::vector<Tensor> grad_tensor;
     Tensor grad_a = mul(grad, input_b, std::nullopt, output_mem_config);
-    grad_tensor.push_back(grad_a);
+    grad_tensor.emplace_back(grad_a);
     Tensor grad_b = mul(grad, input_a, std::nullopt, output_mem_config);
-    grad_tensor.push_back(grad_b);
+    grad_tensor.emplace_back(grad_b);
     return grad_tensor;
 }
 std::vector<Tensor> mul_bw(const Tensor& grad, const Tensor& input_a, const Tensor& input_b, const MemoryConfig& output_mem_config)
@@ -86,7 +86,7 @@ std::vector<Tensor> _exp_bw(const Tensor& grad, const Tensor& input, const Memor
     std::vector<Tensor> grad_tensor;
     Tensor exp_result = exp(input, output_mem_config);
     Tensor result = mul(grad, exp_result, std::nullopt, output_mem_config);
-    grad_tensor.push_back(result);
+    grad_tensor.emplace_back(result);
     return grad_tensor;
 }
 std::vector<Tensor> exp_bw(const Tensor& grad, const Tensor& input, const MemoryConfig& output_mem_config)
@@ -97,11 +97,11 @@ std::vector<Tensor> exp_bw(const Tensor& grad, const Tensor& input, const Memory
 
 std::vector<Tensor> _addcmul_bw(const Tensor& grad, const Tensor& input, const Tensor& tensor1, const Tensor& tensor2, float value, const MemoryConfig& output_mem_config) {
     std::vector<Tensor> grad_tensor;
-    grad_tensor.push_back(grad);
+    grad_tensor.emplace_back(grad);
     Tensor grad_a = mul_unary(mul(grad, tensor2, std::nullopt, output_mem_config), value, output_mem_config);
-    grad_tensor.push_back(grad_a);
+    grad_tensor.emplace_back(grad_a);
     Tensor grad_b = mul_unary(mul(grad, tensor1, std::nullopt, output_mem_config), value, output_mem_config);
-    grad_tensor.push_back(grad_b);
+    grad_tensor.emplace_back(grad_b);
 
     return grad_tensor;
 }
@@ -113,7 +113,7 @@ std::vector<Tensor> addcmul_bw(const Tensor& grad, const Tensor& input, const Te
 
 std::vector<Tensor> _unary_assign_bw(const Tensor& grad, const Tensor& input, const MemoryConfig& output_mem_config) {
     std::vector<Tensor> grad_tensor;
-    grad_tensor.push_back(grad);
+    grad_tensor.emplace_back(grad);
     return grad_tensor;
 }
 std::vector<Tensor> unary_assign_bw(const Tensor& grad, const Tensor& input, const MemoryConfig& output_mem_config)
@@ -128,7 +128,7 @@ std::vector<Tensor> binary_assign_bw(const Tensor& grad, const Tensor& input, co
 std::vector<Tensor> _sqrt_bw(const Tensor& grad, const Tensor& sqrt_result, const MemoryConfig& output_mem_config) {
     std::vector<Tensor> grad_tensor;
     Tensor result = mul(grad, recip(mul_unary(sqrt_result, 2.0, output_mem_config), output_mem_config), std::nullopt, output_mem_config);
-    grad_tensor.push_back(result);
+    grad_tensor.emplace_back(result);
     return grad_tensor;
 }
 std::vector<Tensor> sqrt_bw(const Tensor& grad, const Tensor& sqrt_result, const MemoryConfig& output_mem_config)
@@ -141,7 +141,7 @@ std::vector<Tensor> _unary_div_bw(const Tensor& grad, const Tensor& input, float
     std::vector<Tensor> grad_tensor;
     Tensor scalar_t = full_like(input, scalar, output_mem_config);
     Tensor result = mul(grad, recip(scalar_t, output_mem_config), std::nullopt, output_mem_config);
-    grad_tensor.push_back(result);
+    grad_tensor.emplace_back(result);
     return grad_tensor;
 }
 std::vector<Tensor> unary_div_bw(const Tensor& grad, const Tensor& input, float scalar, const MemoryConfig& output_mem_config)
@@ -153,9 +153,9 @@ std::vector<Tensor> unary_div_bw(const Tensor& grad, const Tensor& input, float 
 std::vector<Tensor> _div_bw(const Tensor& grad, const Tensor& input, const Tensor& other, const MemoryConfig& output_mem_config) {
     std::vector<Tensor> grad_tensor;
     Tensor grad_a = mul(grad, recip(other, output_mem_config), std::nullopt, output_mem_config);
-    grad_tensor.push_back(grad_a);
+    grad_tensor.emplace_back(grad_a);
     Tensor grad_b = mul(mul(neg(grad, output_mem_config), input, std::nullopt, output_mem_config), recip(square(other, output_mem_config), output_mem_config), std::nullopt, output_mem_config);
-    grad_tensor.push_back(grad_b);
+    grad_tensor.emplace_back(grad_b);
     return grad_tensor;
 }
 std::vector<Tensor> div_bw(const Tensor& grad, const Tensor& input, const Tensor& other, const MemoryConfig& output_mem_config)
@@ -170,7 +170,7 @@ std::vector<Tensor> _tanh_bw(const Tensor& grad, const Tensor& input, const Memo
     tanh_res = square(tanh_res, output_mem_config);
     tanh_res = rsub(tanh_res, 1.0, output_mem_config);
     Tensor result = mul(grad, tanh_res, std::nullopt, output_mem_config);
-    grad_tensor.push_back(result);
+    grad_tensor.emplace_back(result);
     return grad_tensor;
 }
 std::vector<Tensor> tanh_bw(const Tensor& grad, const Tensor& input, const MemoryConfig& output_mem_config)
@@ -185,7 +185,7 @@ std::vector<Tensor> _sigmoid_bw(const Tensor& grad, const Tensor& input, const M
     Tensor rsub_term = rsub(sig_result, 1.0f, output_mem_config);
     Tensor prod_term_1 = mul(sig_result, rsub_term,{},output_mem_config);
     Tensor prod_term_2 = mul(prod_term_1, grad,{},output_mem_config);
-    grad_tensor.push_back(prod_term_2);
+    grad_tensor.emplace_back(prod_term_2);
     return grad_tensor;
 }
 
@@ -199,7 +199,7 @@ std::vector<Tensor> sigmoid_bw(const Tensor& grad, const Tensor& input,
 std::vector<Tensor> _tan_bw(const Tensor& grad, const Tensor& tan_result, const MemoryConfig& output_mem_config) {
     std::vector<Tensor> grad_tensor;
     Tensor result = mul(grad, add1(square(tan_result, output_mem_config), output_mem_config), std::nullopt, output_mem_config);
-    grad_tensor.push_back(result);
+    grad_tensor.emplace_back(result);
     return grad_tensor;
 }
 std::vector<Tensor> tan_bw(const Tensor& grad, const Tensor& tan_result, const MemoryConfig& output_mem_config)
@@ -209,12 +209,12 @@ std::vector<Tensor> tan_bw(const Tensor& grad, const Tensor& tan_result, const M
 
 std::vector<Tensor> _addcdiv_bw(const Tensor& grad, const Tensor& input, const Tensor& tensor1, const Tensor& tensor2, float value, const MemoryConfig& output_mem_config) {
     std::vector<Tensor> grad_tensor;
-    grad_tensor.push_back(grad);
+    grad_tensor.emplace_back(grad);
     Tensor grad_a = mul(mul_unary(grad, value, output_mem_config), recip(tensor2, output_mem_config));
-    grad_tensor.push_back(grad_a);
+    grad_tensor.emplace_back(grad_a);
     Tensor tmp = mul(mul_unary(neg(grad, output_mem_config), value, output_mem_config), tensor1, std::nullopt, output_mem_config);
     Tensor grad_b = mul(tmp, recip(square(tensor2, output_mem_config), output_mem_config), std::nullopt, output_mem_config);
-    grad_tensor.push_back(grad_b);
+    grad_tensor.emplace_back(grad_b);
 
     return grad_tensor;
 }
@@ -227,9 +227,9 @@ std::vector<Tensor> _where_bw(const Tensor& grad, const Tensor& condition, const
     std::vector<Tensor> grad_tensor;
     Tensor t_zero = zeros_like(grad, output_mem_config);
     Tensor grad_a = where(condition, grad, t_zero, output_mem_config);
-    grad_tensor.push_back(grad_a);
+    grad_tensor.emplace_back(grad_a);
     Tensor grad_b = where(condition, t_zero, grad, output_mem_config);
-    grad_tensor.push_back(grad_b);
+    grad_tensor.emplace_back(grad_b);
 
     return grad_tensor;
 }
@@ -307,7 +307,7 @@ std::vector<Tensor> _embedding_bw(const Tensor& grad, const Tensor& input, const
     TT_FATAL(input.shape()[1] == 1 && input.shape()[2] == 1, "Only dim 0 && 3 for the input can be non 1");
     std::vector<Tensor> grad_tensor;
     Tensor grad_a = embeddings(input, grad, false);
-    grad_tensor.push_back(grad_a);
+    grad_tensor.emplace_back(grad_a);
 
     return grad_tensor;
 }
@@ -323,7 +323,7 @@ std::vector<Tensor> _sub_bw(const Tensor& grad, const Tensor& input, const Tenso
     std::vector<Tensor> grad_tensor;
     grad_tensor.emplace_back(grad);
     Tensor grad_b = neg(grad);
-    grad_tensor.push_back(grad_b);
+    grad_tensor.emplace_back(grad_b);
     return grad_tensor;
 }
 std::vector<Tensor> sub_bw(const Tensor& grad, const Tensor& input, const Tensor& other, const MemoryConfig& output_mem_config)
@@ -346,7 +346,7 @@ std::vector<Tensor> unary_sub_bw(const Tensor& grad, const Tensor& input, const 
 std::vector<Tensor> _neg_bw(const Tensor& grad, const Tensor& input, const MemoryConfig& output_mem_config) {
     std::vector<Tensor> grad_tensor;
     Tensor result = neg(grad, output_mem_config);
-    grad_tensor.push_back(result);
+    grad_tensor.emplace_back(result);
     return grad_tensor;
 }
 std::vector<Tensor> neg_bw(const Tensor& grad, const Tensor& input, const MemoryConfig& output_mem_config)
@@ -367,7 +367,7 @@ std::vector<Tensor> rsub_bw(const Tensor& grad, const Tensor& input, const Tenso
 std::vector<Tensor> _lt_bw(const Tensor& grad, const MemoryConfig& output_mem_config) {
     std::vector<Tensor> grad_tensor;
     Tensor t_zero = zeros_like(grad, output_mem_config);
-    grad_tensor.push_back(t_zero);
+    grad_tensor.emplace_back(t_zero);
     return grad_tensor;
 }
 std::vector<Tensor> lt_bw(const Tensor& grad, const MemoryConfig& output_mem_config)
@@ -378,7 +378,7 @@ std::vector<Tensor> lt_bw(const Tensor& grad, const MemoryConfig& output_mem_con
 std::vector<Tensor> _gt_bw(const Tensor& grad, const MemoryConfig& output_mem_config) {
     std::vector<Tensor> grad_tensor;
     Tensor t_zero = zeros_like(grad, output_mem_config);
-    grad_tensor.push_back(t_zero);
+    grad_tensor.emplace_back(t_zero);
     return grad_tensor;
 }
 std::vector<Tensor> gt_bw(const Tensor& grad, const MemoryConfig& output_mem_config)
@@ -389,7 +389,7 @@ std::vector<Tensor> gt_bw(const Tensor& grad, const MemoryConfig& output_mem_con
 std::vector<Tensor> _ne_bw(const Tensor& grad, const MemoryConfig& output_mem_config) {
     std::vector<Tensor> grad_tensor;
     Tensor t_zero = zeros_like(grad, output_mem_config);
-    grad_tensor.push_back(t_zero);
+    grad_tensor.emplace_back(t_zero);
     return grad_tensor;
 }
 std::vector<Tensor> ne_bw(const Tensor& grad, const MemoryConfig& output_mem_config)
