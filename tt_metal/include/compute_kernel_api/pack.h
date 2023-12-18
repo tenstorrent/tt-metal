5305975cd6716ef791c240782c87526bd3ccad56
Austin Ho <aho@tenstorrent.com>
2023-12-18 08:06:46 -0800

    #4376: Call llk reconfig functions in compute kernel apis for WH

--- tt_metal/include/compute_kernel_api/pack.h
+++ tt_metal/include/compute_kernel_api/pack.h
@@ -87,8 +87,9 @@ ALWI void matmul_pack_tile(uint32_t ifrom_dst, uint32_t icb, uint32_t ntiles)
 ALWI void pack_reconfig_data_format(const uint32_t new_operand) {
     #ifdef ARCH_GRAYSKULL
         PACK(( llk_pack_reconfig_data_format(new_operand) ));
+    #else
+        PACK(( llk_pack_reconfig_data_format(new_operand) ));
     #endif
-    // NOTE: For wormhole_b0, packer data format reconfig functions don;t yet exist. So skip.
 }
 
 /**
@@ -97,8 +98,9 @@ ALWI void pack_reconfig_data_format(const uint32_t new_operand) {
 ALWI void pack_reconfig_data_format(const uint32_t old_operand, const uint32_t new_operand) {
     #ifdef ARCH_GRAYSKULL
         PACK(( llk_pack_reconfig_data_format(old_operand, new_operand) ));
+    #else
+        PACK(( llk_pack_reconfig_data_format(old_operand, new_operand) ));
     #endif
-    // NOTE: For wormhole_b0, packer data format reconfig functions don;t yet exist. So skip.
 }
 
 }
