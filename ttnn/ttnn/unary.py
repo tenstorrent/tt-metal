bb07ed983e7c74f37a9e8fe3b6b65938b9d10130
Muthu Annamalai <muthua@tenstorrent.com>
2024-01-03 16:48:13 +0000

    #0: add 62 unary ops into ttnn   : add docs   : update personal dict   : permute already covered

--- ttnn/ttnn/unary.py
+++ ttnn/ttnn/unary.py
@@ -1,11 +1,11 @@
-# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+# SPDX-FileCopyrightText: © 2023-24 Tenstorrent Inc.
 
 # SPDX-License-Identifier: Apache-2.0
-
 import sys
-
 import tt_lib as ttl
-
+from ttnn.core import reshape, _reshape_to_4D
+from ttnn.decorators import decorate_operation
+from typing import Union
 from ttnn.tensor import (
     Tensor,
     has_storage_type_of,
@@ -15,31 +15,23 @@ from ttnn.tensor import (
 )
 from ttnn.core import reshape, _reshape_to_4D
 from ttnn.decorators import decorate_operation
-
+import torch
+import torch.nn.functional as F
 
 THIS_MODULE = sys.modules[__name__]
 
 __all__ = []
 
 
-def register_ttl_unary_function(name, ttl_unary_function):
+def register_ttl_unary_function(name, ttl_unary_function, torch_function):
     def _torch_unary(input_tensor: Tensor, **_):
         import torch
         import ttnn
 
-        name_to_torch_function = {
-            "exp": torch.exp,
-            "tanh": torch.tanh,
-            "gelu": torch.nn.functional.gelu,
-            "rsqrt": torch.rsqrt,
-            "relu": torch.relu,
-        }
-        torch_function = name_to_torch_function[name]
-
         input_tensor = ttnn.from_device(input_tensor)
         input_tensor = ttnn.to_layout(input_tensor, ttnn.ROW_MAJOR_LAYOUT)
         input_tensor = ttnn.to_torch(input_tensor)
-
+        assert torch_function, f"Torch function not implemented for {str(ttl_unary_function)}"
         return torch_function(input_tensor)
 
     @decorate_operation(torch_function=_torch_unary, name=name)
@@ -80,33 +72,18 @@ def register_ttl_unary_function(name, ttl_unary_function):
         """
     setattr(THIS_MODULE, name, unary_function)
     __all__.append(name)
+    return unary_function
 
 
-TTL_UNARY_FUNCTIONS = [
-    ("exp", ttl.tensor.exp),
-    ("tanh", ttl.tensor.tanh),
-    ("gelu", ttl.tensor.gelu),
-    ("relu", ttl.tensor.relu),
-    ("rsqrt", ttl.tensor.rsqrt),
-]
-
-
-for unary_function_name, ttl_unary_function in TTL_UNARY_FUNCTIONS:
-    register_ttl_unary_function(unary_function_name, ttl_unary_function)
-
-
-def register_ttl_unary_function_with_float_parameter(name, ttl_unary_function):
+def register_ttl_unary_function_with_float_parameter(name, ttl_unary_function, torch_function):
     def _torch_unary(input_tensor: Tensor, parameter, **_):
         import torch
         import ttnn
 
-        name_to_torch_function = {"pow": torch.pow}
-        torch_function = name_to_torch_function[name]
-
         input_tensor = ttnn.from_device(input_tensor)
         input_tensor = ttnn.to_layout(input_tensor, ttnn.ROW_MAJOR_LAYOUT)
         input_tensor = ttnn.to_torch(input_tensor)
-
+        assert torch_function, f"Torch function not implemented for {str(ttl_unary_function)}"
         return torch_function(input_tensor, parameter)
 
     @decorate_operation(torch_function=_torch_unary, name=name)
@@ -150,12 +127,119 @@ def register_ttl_unary_function_with_float_parameter(name, ttl_unary_function):
 
     setattr(THIS_MODULE, name, unary_function)
     __all__.append(name)
+    return unary_function
 
 
-TTL_UNARY_FUNCTIONS_WITH_FLOAT_PARAMETER = [
-    ("pow", ttl.tensor.power),
+# register functions
+def torch_cbrt(_t: torch.Tensor):
+    return torch.sgn(_t) * torch.pow(torch.abs(_t), 1.0 / 3)
+
+
+def torch_log_sigmoid(_t: torch.Tensor):
+    return torch.log(torch.sigmoid(_t))
+
+
+def ttl_pow(_t: ttl.tensor.Tensor, _exponent: Union[int, float], **kw_args):
+    from math import floor
+
+    if floor(_exponent) == _exponent:
+        return ttl.tensor.power(_t, int(_exponent), **kw_args)
+    return ttl.tensor.power_fp(_t, _exponent, **kw_args)
+
+
+TTL_UNARY_FUNCTIONS = [
+    ("exp", ttl.tensor.exp, torch.exp),
+    ("tanh", ttl.tensor.tanh, torch.tanh),
+    ("gelu", ttl.tensor.gelu, torch.nn.functional.gelu),
+    ("relu", ttl.tensor.relu, torch.relu),
+    ("rsqrt", ttl.tensor.rsqrt, torch.rsqrt),
+    ("abs", ttl.tensor.abs, torch.abs),
+    ("acos", ttl.tensor.acos, torch.acos),
+    ("acosh", ttl.tensor.acosh, torch.acosh),
+    ("asin", ttl.tensor.asin, torch.asin),
+    ("asinh", ttl.tensor.asinh, torch.asinh),
+    ("atan", ttl.tensor.acos, lambda _x: -_x.atan()),
+    ("atanh", ttl.tensor.atanh, torch.atanh),
+    ("cbrt", ttl.tensor.cbrt, torch_cbrt),
+    ("clone", ttl.tensor.clone, torch.clone),
+    ("cos", ttl.tensor.cos, torch.cos),
+    ("cosh", ttl.tensor.cosh, torch.cosh),
+    ("deg2rad", ttl.tensor.deg2rad, torch.deg2rad),
+    ("digamma", ttl.tensor.digamma, torch.digamma),
+    ("erf", ttl.tensor.erf, torch.erf),
+    ("erfc", ttl.tensor.erfc, torch.erfc),
+    ("erfinv", ttl.tensor.erfinv, torch.erfinv),
+    ("exp", ttl.tensor.exp, torch.exp),
+    ("exp2", ttl.tensor.exp2, torch.exp2),
+    ("expm1", ttl.tensor.expm1, torch.expm1),
+    # ("geglu", ttl.tensor.geglu, None),
+    ("gelu", ttl.tensor.gelu, None),
+    # ("glu", ttl.tensor.glu, F.glu),
+    ("hardsigmoid", ttl.tensor.hardsigmoid, F.hardsigmoid),
+    ("hardswish", ttl.tensor.hardswish, F.hardswish),
+    ("hardtanh", ttl.tensor.hardtanh, F.hardtanh),
+    ("i0", ttl.tensor.i0, torch.i0),
+    ("identity", ttl.tensor.identity, None),
+    ("isfinite", ttl.tensor.isfinite, torch.isfinite),
+    ("isinf", ttl.tensor.isinf, torch.isinf),
+    ("isnan", ttl.tensor.isnan, torch.isnan),
+    ("isneginf", ttl.tensor.isneginf, torch.isneginf),
+    ("isposinf", ttl.tensor.isposinf, torch.isposinf),
+    ("lerp", ttl.tensor.lerp, torch.lerp),
+    ("lgamma", ttl.tensor.lgamma, torch.lgamma),
+    ("log", ttl.tensor.log, torch.log),
+    ("log10", ttl.tensor.log10, torch.log10),
+    ("log1p", ttl.tensor.log1p, torch.log1p),
+    ("log2", ttl.tensor.log2, torch.log2),
+    ("log_sigmoid", ttl.tensor.log_sigmoid, torch_log_sigmoid),
+    # ("logaddexp", ttl.tensor.logaddexp, torch.logaddexp),
+    # ("logaddexp2", ttl.tensor.logaddexp2, torch.logaddexp2),
+    # ("max", ttl.tensor.max, torch.max),
+    # ("min", ttl.tensor.min, torch.min),
+    ("mish", ttl.tensor.mish, lambda _x: F.mish(_x.to(torch.float))),
+    ("move", ttl.tensor.move, None),
+    ("multigammaln", ttl.tensor.multigammaln, None),
+    ("neg", ttl.tensor.neg, torch.neg),
+    ("rad2deg", ttl.tensor.rad2deg, torch.rad2deg),
+    ("recip", ttl.tensor.recip, lambda _x: 1.0 / _x),
+    # ("reglu", ttl.tensor.reglu, None),
+    ("relu", ttl.tensor.relu, torch.relu),
+    ("relu6", ttl.tensor.relu6, F.relu6),
+    ("rsqrt", ttl.tensor.rsqrt, torch.rsqrt),
+    ("sigmoid", ttl.tensor.sigmoid, torch.sigmoid),
+    ("sign", ttl.tensor.sign, torch.sign),
+    # ("signbit", ttl.tensor.signbit, torch.signbit), #TODO: improve tt_dnn impl
+    ("silu", ttl.tensor.silu, F.silu),
+    ("sin", ttl.tensor.sin, torch.sin),
+    ("sinh", ttl.tensor.sinh, torch.sinh),
+    ("sqrt", ttl.tensor.sqrt, torch.sqrt),
+    ("square", ttl.tensor.square, torch.square),
+    # ("sum",ttl.tensor.sum,torch.sum),
+    # ("swiglu", ttl.tensor.swiglu, None),
+    ("swish", ttl.tensor.swish, F.hardswish),
+    ("tan", ttl.tensor.tan, torch.tan),
+    ("tanh", ttl.tensor.tanh, torch.tanh),
+    ("tanhshrink", ttl.tensor.tanhshrink, F.tanhshrink),
+    ("tril", ttl.tensor.tril, torch.tril),
+    ("triu", ttl.tensor.triu, torch.triu),
 ]
 
 
-for unary_function_name, ttl_unary_function in TTL_UNARY_FUNCTIONS_WITH_FLOAT_PARAMETER:
-    register_ttl_unary_function_with_float_parameter(unary_function_name, ttl_unary_function)
+for unary_function_name, ttl_unary_function, torch_function in TTL_UNARY_FUNCTIONS:
+    register_ttl_unary_function(unary_function_name, ttl_unary_function, torch_function)
+
+TTL_UNARY_FUNCTIONS_WITH_FLOAT_PARAMETER = [
+    ("pow", ttl_pow, torch.pow),
+    ("elu", ttl.tensor.elu, F.elu),
+    ("relu_max", ttl.tensor.relu_max, None),
+    ("relu_min", ttl.tensor.relu_min, None),
+    ("threshold", ttl.tensor.threshold, torch.threshold),
+    ("leaky_relu", ttl.tensor.leaky_relu, F.leaky_relu),
+    ("hardshrink", ttl.tensor.hardshrink, F.hardshrink),
+    ("prelu", ttl.tensor.prelu, torch.prelu),
+    ("polygamma", ttl.tensor.polygamma, torch.polygamma),
+    ("logit", ttl.tensor.logit, torch.logit),
+]
+
+for unary_function_name, ttl_unary_function, torch_function in TTL_UNARY_FUNCTIONS_WITH_FLOAT_PARAMETER:
+    register_ttl_unary_function_with_float_parameter(unary_function_name, ttl_unary_function, torch_function)
