69a8fd0109aa500629ac3e560bdf4f39e9d48af1
umadevimcw <umadevi@multicorewareinc.com>
2024-01-08 10:48:20 +0000

    #4607: Add ttnn support for subalpha

--- ttnn/binary.py
+++ ttnn/binary.py
@@ -47,7 +47,8 @@ def register_ttl_binary_function(name, ttl_binary_function, torch_function):
         Applies {name} to :attr:`input_tensor_a` and  :attr:`input_tensor_b` element-wise.
 
         .. math::
-            {name}(\\mathrm{{input\\_tensor}}_i)
+            {name}(\\mathrm{{input\\_tensor_a}}_i)
+            {name}(\\mathrm{{input\\_tensor_b}}_i)
 
         Args:
             * :attr:`input_tensor_a`
@@ -62,6 +63,8 @@ def register_ttl_binary_function(name, ttl_binary_function, torch_function):
             Tensor([ 1, 0], dtype=bfloat16 )
 
         """
+        if not (input_tensor_a.shape == input_tensor_b.shape):
+            raise RuntimeError("input_tensors must be of same size!")
 
         original_shape = input_tensor_a.shape
         input_tensor_a = _reshape_to_4D(input_tensor_a)
@@ -76,9 +79,85 @@ def register_ttl_binary_function(name, ttl_binary_function, torch_function):
             raise RuntimeError("input_tensors must be on device!")
 
         ttl_input_tensor_a = input_tensor_a.value
-        ttl_input_tensor_a = input_tensor_b.value
+        ttl_input_tensor_b = input_tensor_b.value
 
-        ttl_output_tensor = ttl_binary_function(ttl_input_tensor_a, ttl_input_tensor_a, output_mem_config=memory_config)
+        ttl_output_tensor = ttl_binary_function(ttl_input_tensor_a, ttl_input_tensor_b, output_mem_config=memory_config)
+
+        output_tensor = Tensor(ttl_output_tensor)
+        output_tensor = reshape(output_tensor, original_shape)
+        return output_tensor
+
+    setattr(THIS_MODULE, name, binary_function)
+    __all__.append(name)
+    return binary_function
+
+
+def register_ttl_binary_function_with_float_parameter(name, ttl_binary_function, torch_function):
+    def _torch_binary(input_tensor_a: Tensor, input_tensor_b: Tensor, parameter, **_):
+        import torch
+        import ttnn
+
+        input_tensor_a = ttnn.from_device(input_tensor_a)
+        input_tensor_a = ttnn.to_layout(input_tensor_a, ttnn.ROW_MAJOR_LAYOUT)
+        input_tensor_a = ttnn.to_torch(input_tensor_a)
+
+        input_tensor_b = ttnn.from_device(input_tensor_b)
+        input_tensor_b = ttnn.to_layout(input_tensor_b, ttnn.ROW_MAJOR_LAYOUT)
+        input_tensor_b = ttnn.to_torch(input_tensor_b)
+        assert torch_function, f"Torch function not implemented for {str(ttl_binary_function)}"
+        return torch_function(input_tensor_a, input_tensor_b, parameter)
+
+    @decorate_operation(torch_function=_torch_binary, name=name)
+    def binary_function(
+        input_tensor_a: Tensor,
+        input_tensor_b: Tensor,
+        parameter: float,
+        *,
+        memory_config: MemoryConfig = DRAM_MEMORY_CONFIG,
+    ) -> Tensor:
+        f"""{name}(input_tensor_a: Tensor, input_tensor_b: Tensor) -> Tensor
+
+        Applies {name} to :attr:`input_tensor_a`  and  :attr:`input_tensor_b` element-wise.
+
+        .. math::
+            {name}(\\mathrm{{input\\_tensor_a}}_i)
+            {name}(\\mathrm{{input\\_tensor_b}}_i)
+
+        Args:
+            * :attr:`input_tensor_a`
+            * :attr:`input_tensor_b`
+
+        Example::
+
+            >>> tensor_a = ttnn.to_device(ttnn.from_torch(torch.tensor((1, 2), dtype=torch.bfloat16)), device)
+            >>> tensor_b = ttnn.to_device(ttnn.from_torch(torch.tensor((2, 2), dtype=torch.bfloat16)), device)
+            >>> output = ttnn.{name}(tensor_a, tensor_b, 2.0)
+            >>> print(output)
+            Tensor([ -3, -2], dtype=bfloat16 )
+
+        """
+
+        if not (input_tensor_a.shape == input_tensor_b.shape):
+            raise RuntimeError("input_tensors must be of same size!")
+
+        original_shape = input_tensor_a.shape
+        input_tensor_a = _reshape_to_4D(input_tensor_a)
+        input_tensor_b = _reshape_to_4D(input_tensor_b)
+
+        if not isinstance(input_tensor_a, Tensor) or not isinstance(input_tensor_b, Tensor):
+            raise TypeError("Expected both arguments to be a ttnn.Tensor")
+
+        if not has_storage_type_of(input_tensor_a, DEVICE_STORAGE_TYPE) or not has_storage_type_of(
+            input_tensor_b, DEVICE_STORAGE_TYPE
+        ):
+            raise RuntimeError("input_tensors must be on device!")
+
+        ttl_input_tensor_a = input_tensor_a.value
+        ttl_input_tensor_b = input_tensor_b.value
+
+        ttl_output_tensor = ttl_binary_function(
+            ttl_input_tensor_a, ttl_input_tensor_b, parameter, output_mem_config=memory_config
+        )
 
         output_tensor = Tensor(ttl_output_tensor)
         output_tensor = reshape(output_tensor, original_shape)
@@ -104,3 +183,11 @@ TTL_BINARY_FUNCTIONS = [
 
 for binary_function_name, ttl_binary_function, torch_function in TTL_BINARY_FUNCTIONS:
     register_ttl_binary_function(binary_function_name, ttl_binary_function, torch_function)
+
+
+TTL_BINARY_FUNCTIONS_WITH_FLOAT_PARAMETER = [
+    ("subalpha", ttl.tensor.subalpha, torch.sub),
+]
+
+for binary_function_name, ttl_binary_function, torch_function in TTL_BINARY_FUNCTIONS_WITH_FLOAT_PARAMETER:
+    register_ttl_binary_function_with_float_parameter(binary_function_name, ttl_binary_function, torch_function)
--- ttnn/test/test_binary_tnn.py
+++ ttnn/test/test_binary_tnn.py
@@ -8,22 +8,22 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "input_shapes",
+    "input_shape_a, input_shape_b",
     (
-        (torch.Size([1, 1, 32, 32])),
-        (torch.Size([1, 1, 320, 384])),
-        (torch.Size([1, 3, 320, 384])),
+        (torch.Size([1, 1, 32, 32]), torch.Size([1, 1, 32, 32])),
+        (torch.Size([1, 1, 320, 384]), torch.Size([1, 1, 320, 384])),
+        (torch.Size([1, 3, 320, 384]), torch.Size([1, 3, 320, 384])),
     ),
 )
 class TestEltwiseBinary:
-    def test_ttnn_squared_difference(self, input_shapes):
+    def test_ttnn_squared_difference(self, input_shape_a, input_shape_b):
         device_id = 0
         device = ttnn.open(device_id)
 
         torch.manual_seed(0)
 
-        torch_a = torch.rand(input_shapes, dtype=torch.bfloat16)
-        torch_b = torch.rand(input_shapes, dtype=torch.bfloat16)
+        torch_a = torch.rand(input_shape_a, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shape_b, dtype=torch.bfloat16)
 
         a = ttnn.from_torch(torch_a)
         b = ttnn.from_torch(torch_b)
@@ -37,6 +37,30 @@ class TestEltwiseBinary:
         print(f"shape: {output.shape}")
         print(f"dtype: {output.dtype}")
         print(f"layout: {output.layout}")
-        # print(f"first row: {output[:1]}")
+
+        ttnn.close(device)
+
+    @pytest.mark.parametrize("alpha", [1.0, 2.5, -2.5])
+    def test_ttnn_subalpha(self, input_shape_a, input_shape_b, alpha):
+        device_id = 0
+        device = ttnn.open(device_id)
+
+        torch.manual_seed(0)
+
+        torch_a = torch.rand(input_shape_a, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shape_b, dtype=torch.bfloat16)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output = ttnn.subalpha(a, b, alpha)
+        output = ttnn.to_layout(output, ttnn.ROW_MAJOR_LAYOUT)
+
+        print(f"shape: {output.shape}")
+        print(f"dtype: {output.dtype}")
+        print(f"layout: {output.layout}")
 
         ttnn.close(device)
