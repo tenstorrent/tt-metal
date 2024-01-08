29af3fbae29f34818f43b50b1b35f0c7beac5785
Bharane AB <bharane.amutha@multicorewareinc.com>
2024-01-08 13:56:05 +0000

    #4605: Export relational operators to ttnn

---
+++ docs/source/ttnn/operations/relational.rst
@@ -0,0 +1,24 @@
+Relational Operations
+=====================
+
+.. autofunction:: ttnn.gez
+
+.. autofunction:: ttnn.gtz
+
+.. autofunction:: ttnn.eqz
+
+.. autofunction:: ttnn.ltz
+
+.. autofunction:: ttnn.nez
+
+.. autofunction:: ttnn.gt
+
+.. autofunction:: ttnn.gte
+
+.. autofunction:: ttnn.eq
+
+.. autofunction:: ttnn.lt
+
+.. autofunction:: ttnn.lte
+
+.. autofunction:: ttnn.ne
---
+++ tests/ttnn/test/test_relational_tnn.py
@@ -0,0 +1,266 @@
+# SPDX-FileCopyrightText: © 2023-24 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+import ttnn
+import pytest
+
+
+@pytest.mark.parametrize(
+    "input_shapes",
+    (
+        (torch.Size([1, 1, 32, 32])),
+        (torch.Size([1, 1, 320, 384])),
+        (torch.Size([1, 3, 320, 384])),
+    ),
+)
+class TestEltwiseRelational:
+    def test_ttnn_gt(self, input_shapes):
+        device_id = 0
+        device = ttnn.open(device_id)
+
+        torch.manual_seed(0)
+
+        torch_a = torch.rand(input_shapes, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shapes, dtype=torch.bfloat16)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output = ttnn.gt(a, b)
+        output = ttnn.to_layout(output, ttnn.ROW_MAJOR_LAYOUT)
+
+        print(f"dtype: {output.dtype}")
+        print(f"layout: {output.layout}")
+        # print(f"first row: {output[:1]}")
+
+        ttnn.close(device)
+
+    def test_ttnn_gte(self, input_shapes):
+        device_id = 0
+        device = ttnn.open(device_id)
+
+        torch.manual_seed(0)
+
+        torch_a = torch.rand(input_shapes, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shapes, dtype=torch.bfloat16)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output = ttnn.gte(a, b)
+        output = ttnn.to_layout(output, ttnn.ROW_MAJOR_LAYOUT)
+
+        print(f"dtype: {output.dtype}")
+        print(f"layout: {output.layout}")
+        # print(f"first row: {output[:1]}")
+
+        ttnn.close(device)
+
+    def test_ttnn_eq(self, input_shapes):
+        device_id = 0
+        device = ttnn.open(device_id)
+
+        torch.manual_seed(0)
+
+        torch_a = torch.rand(input_shapes, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shapes, dtype=torch.bfloat16)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output = ttnn.eq(a, b)
+        output = ttnn.to_layout(output, ttnn.ROW_MAJOR_LAYOUT)
+
+        print(f"dtype: {output.dtype}")
+        print(f"layout: {output.layout}")
+        # print(f"first row: {output[:1]}")
+
+        ttnn.close(device)
+
+    def test_ttnn_lt(self, input_shapes):
+        device_id = 0
+        device = ttnn.open(device_id)
+
+        torch.manual_seed(0)
+
+        torch_a = torch.rand(input_shapes, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shapes, dtype=torch.bfloat16)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output = ttnn.lt(a, b)
+        output = ttnn.to_layout(output, ttnn.ROW_MAJOR_LAYOUT)
+
+        print(f"dtype: {output.dtype}")
+        print(f"layout: {output.layout}")
+        # print(f"first row: {output[:1]}")
+
+        ttnn.close(device)
+
+    def test_ttnn_lte(self, input_shapes):
+        device_id = 0
+        device = ttnn.open(device_id)
+
+        torch.manual_seed(0)
+
+        torch_a = torch.rand(input_shapes, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shapes, dtype=torch.bfloat16)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output = ttnn.lte(a, b)
+        output = ttnn.to_layout(output, ttnn.ROW_MAJOR_LAYOUT)
+
+        print(f"dtype: {output.dtype}")
+        print(f"layout: {output.layout}")
+        # print(f"first row: {output[:1]}")
+
+        ttnn.close(device)
+
+    def test_ttnn_ne(self, input_shapes):
+        device_id = 0
+        device = ttnn.open(device_id)
+
+        torch.manual_seed(0)
+
+        torch_a = torch.rand(input_shapes, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shapes, dtype=torch.bfloat16)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output = ttnn.ne(a, b)
+        output = ttnn.to_layout(output, ttnn.ROW_MAJOR_LAYOUT)
+
+        print(f"dtype: {output.dtype}")
+        print(f"layout: {output.layout}")
+        # print(f"first row: {output[:1]}")
+
+        ttnn.close(device)
+
+    def test_ttnn_gtz(self, input_shapes):
+        device_id = 0
+        device = ttnn.open(device_id)
+
+        torch.manual_seed(0)
+
+        torch_a = torch.rand(input_shapes, dtype=torch.bfloat16)
+
+        a = ttnn.from_torch(torch_a)
+
+        a = ttnn.to_device(a, device)
+
+        output = ttnn.gtz(a)
+        output = ttnn.to_layout(output, ttnn.ROW_MAJOR_LAYOUT)
+
+        print(f"dtype: {output.dtype}")
+        print(f"layout: {output.layout}")
+        # print(f"first row: {output[:1]}")
+
+        ttnn.close(device)
+
+    def test_ttnn_gez(self, input_shapes):
+        device_id = 0
+        device = ttnn.open(device_id)
+
+        torch.manual_seed(0)
+
+        torch_a = torch.rand(input_shapes, dtype=torch.bfloat16)
+
+        a = ttnn.from_torch(torch_a)
+
+        a = ttnn.to_device(a, device)
+
+        output = ttnn.gez(a)
+        output = ttnn.to_layout(output, ttnn.ROW_MAJOR_LAYOUT)
+
+        print(f"dtype: {output.dtype}")
+        print(f"layout: {output.layout}")
+        # print(f"first row: {output[:1]}")
+
+        ttnn.close(device)
+
+    def test_ttnn_eqz(self, input_shapes):
+        device_id = 0
+        device = ttnn.open(device_id)
+
+        torch.manual_seed(0)
+
+        torch_a = torch.rand(input_shapes, dtype=torch.bfloat16)
+
+        a = ttnn.from_torch(torch_a)
+
+        a = ttnn.to_device(a, device)
+
+        output = ttnn.eqz(a)
+        output = ttnn.to_layout(output, ttnn.ROW_MAJOR_LAYOUT)
+
+        print(f"dtype: {output.dtype}")
+        print(f"layout: {output.layout}")
+        # print(f"first row: {output[:1]}")
+
+        ttnn.close(device)
+
+    def test_ttnn_ltz(self, input_shapes):
+        device_id = 0
+        device = ttnn.open(device_id)
+
+        torch.manual_seed(0)
+
+        torch_a = torch.rand(input_shapes, dtype=torch.bfloat16)
+
+        a = ttnn.from_torch(torch_a)
+
+        a = ttnn.to_device(a, device)
+
+        output = ttnn.ltz(a)
+        output = ttnn.to_layout(output, ttnn.ROW_MAJOR_LAYOUT)
+
+        print(f"dtype: {output.dtype}")
+        print(f"layout: {output.layout}")
+        # print(f"first row: {output[:1]}")
+
+        ttnn.close(device)
+
+    def test_ttnn_nez(self, input_shapes):
+        device_id = 0
+        device = ttnn.open(device_id)
+
+        torch.manual_seed(0)
+
+        torch_a = torch.rand(input_shapes, dtype=torch.bfloat16)
+
+        a = ttnn.from_torch(torch_a)
+
+        a = ttnn.to_device(a, device)
+
+        output = ttnn.nez(a)
+        output = ttnn.to_layout(output, ttnn.ROW_MAJOR_LAYOUT)
+
+        print(f"dtype: {output.dtype}")
+        print(f"layout: {output.layout}")
+        # print(f"first row: {output[:1]}")
+
+        ttnn.close(device)
