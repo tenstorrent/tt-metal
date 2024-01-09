3fba03417ba58a9de68922e53a863029b0a893cd
umadevimcw <umadevi@multicorewareinc.com>
2024-01-09 06:04:20 +0000

    #4607: Update ttnn binary test with comp

--- tests/ttnn/unit_tests/test_binary.py
+++ tests/ttnn/unit_tests/test_binary.py
@@ -5,6 +5,7 @@
 import torch
 import ttnn
 import pytest
+from tests.ttnn.utils_for_testing import assert_with_pcc
 
 
 @pytest.mark.parametrize(
@@ -16,14 +17,11 @@ import pytest
     ),
 )
 class TestEltwiseBinary:
-    def test_ttnn_squared_difference(self, input_shape_a, input_shape_b):
-        device_id = 0
-        device = ttnn.open(device_id)
-
-        torch.manual_seed(0)
-
+    def test_ttnn_squared_difference(self, input_shape_a, input_shape_b, device):
         torch_a = torch.rand(input_shape_a, dtype=torch.bfloat16)
         torch_b = torch.rand(input_shape_b, dtype=torch.bfloat16)
+
+        torch_output_tensor = torch.square(torch.sub(torch_a, torch_b))
 
         a = ttnn.from_torch(torch_a)
         b = ttnn.from_torch(torch_b)
@@ -31,24 +29,20 @@ class TestEltwiseBinary:
         a = ttnn.to_device(a, device)
         b = ttnn.to_device(b, device)
 
-        output = ttnn.squared_difference(a, b)
-        output = ttnn.to_layout(output, ttnn.ROW_MAJOR_LAYOUT)
+        output_tensor = ttnn.squared_difference(a, b)
+        output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+        output_tensor = ttnn.to_torch(output_tensor)
 
-        print(f"shape: {output.shape}")
-        print(f"dtype: {output.dtype}")
-        print(f"layout: {output.layout}")
-
-        ttnn.close(device)
+        assert_with_pcc(torch_output_tensor, output_tensor, 0.9998)
 
     @pytest.mark.parametrize("alpha", [1.0, 2.5, -2.5])
-    def test_ttnn_subalpha(self, input_shape_a, input_shape_b, alpha):
-        device_id = 0
-        device = ttnn.open(device_id)
-
+    def test_ttnn_subalpha(self, input_shape_a, input_shape_b, alpha, device):
         torch.manual_seed(0)
 
         torch_a = torch.rand(input_shape_a, dtype=torch.bfloat16)
         torch_b = torch.rand(input_shape_b, dtype=torch.bfloat16)
+
+        torch_output_tensor = torch.sub(torch_a, torch_b, alpha=alpha)
 
         a = ttnn.from_torch(torch_a)
         b = ttnn.from_torch(torch_b)
@@ -56,11 +50,8 @@ class TestEltwiseBinary:
         a = ttnn.to_device(a, device)
         b = ttnn.to_device(b, device)
 
-        output = ttnn.subalpha(a, b, alpha)
-        output = ttnn.to_layout(output, ttnn.ROW_MAJOR_LAYOUT)
+        output_tensor = ttnn.subalpha(a, b, alpha)
+        output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+        output_tensor = ttnn.to_torch(output_tensor)
 
-        print(f"shape: {output.shape}")
-        print(f"dtype: {output.dtype}")
-        print(f"layout: {output.layout}")
-
-        ttnn.close(device)
+        assert_with_pcc(torch_output_tensor, output_tensor, 0.9998)
