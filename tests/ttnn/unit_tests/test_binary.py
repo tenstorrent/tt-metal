4721dc4b55805a1cf67e78941e0d71f605404d14
umadevimcw <umadevi@multicorewareinc.com>
2024-01-09 08:17:49 +0000

    #4607: Update test of binary ops

--- tests/ttnn/unit_tests/test_binary.py
+++ tests/ttnn/unit_tests/test_binary.py
@@ -33,7 +33,7 @@ class TestEltwiseBinary:
         output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
         output_tensor = ttnn.to_torch(output_tensor)
 
-        assert_with_pcc(torch_output_tensor, output_tensor, 0.9998)
+        assert_with_pcc(torch_output_tensor, output_tensor, 0.99)
 
     @pytest.mark.parametrize("alpha", [1.0, 2.5, -2.5])
     def test_ttnn_subalpha(self, input_shape_a, input_shape_b, alpha, device):
@@ -54,4 +54,205 @@ class TestEltwiseBinary:
         output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
         output_tensor = ttnn.to_torch(output_tensor)
 
-        assert_with_pcc(torch_output_tensor, output_tensor, 0.9998)
+        assert_with_pcc(torch_output_tensor, output_tensor, 0.99)
+
+    @pytest.mark.parametrize("alpha", [1.0, 2.5, -2.5])
+    def test_ttnn_addalpha(self, input_shape_a, input_shape_b, alpha, device):
+        torch.manual_seed(0)
+
+        torch_a = torch.rand(input_shape_a, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shape_b, dtype=torch.bfloat16)
+
+        torch_output_tensor = torch.add(torch_a, torch_b, alpha=alpha)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output_tensor = ttnn.addalpha(a, b, alpha)
+        output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+        output_tensor = ttnn.to_torch(output_tensor)
+
+        assert_with_pcc(torch_output_tensor, output_tensor, 0.99)
+
+    def test_ttnn_atan2(self, input_shape_a, input_shape_b, device):
+        torch_a = torch.rand(input_shape_a, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shape_b, dtype=torch.bfloat16)
+
+        torch_output_tensor = torch.atan2(torch_b, torch_a)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output_tensor = ttnn.atan2(a, b)
+        output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+        output_tensor = ttnn.to_torch(output_tensor)
+
+        assert_with_pcc(torch_output_tensor, output_tensor, 0.99)
+
+    def test_ttnn_hypot(self, input_shape_a, input_shape_b, device):
+        torch_a = torch.rand(input_shape_a, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shape_b, dtype=torch.bfloat16)
+
+        torch_output_tensor = torch.hypot(torch_a, torch_b)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output_tensor = ttnn.hypot(a, b)
+        output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+        output_tensor = ttnn.to_torch(output_tensor)
+
+        assert_with_pcc(torch_output_tensor, output_tensor, 0.99)
+
+    def test_ttnn_ldexp(self, input_shape_a, input_shape_b, device):
+        torch_a = torch.rand(input_shape_a, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shape_b, dtype=torch.bfloat16)
+
+        torch_output_tensor = torch.ldexp(torch_a, torch_b)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output_tensor = ttnn.ldexp(a, b)
+        output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+        output_tensor = ttnn.to_torch(output_tensor)
+
+        assert_with_pcc(torch_output_tensor, output_tensor, 0.99)
+
+    def test_ttnn_logaddexp(self, input_shape_a, input_shape_b, device):
+        torch_a = torch.rand(input_shape_a, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shape_b, dtype=torch.bfloat16)
+
+        torch_output_tensor = torch.logaddexp(torch_a, torch_b)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output_tensor = ttnn.logaddexp(a, b)
+        output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+        output_tensor = ttnn.to_torch(output_tensor)
+
+        assert_with_pcc(torch_output_tensor, output_tensor, 0.99)
+
+    def test_ttnn_logaddexp2(self, input_shape_a, input_shape_b, device):
+        torch_a = torch.rand(input_shape_a, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shape_b, dtype=torch.bfloat16)
+
+        torch_output_tensor = torch.logaddexp2(torch_a, torch_b)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output_tensor = ttnn.logaddexp2(a, b)
+        output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+        output_tensor = ttnn.to_torch(output_tensor)
+
+        assert_with_pcc(torch_output_tensor, output_tensor, 0.98)
+
+    def test_ttnn_xlogy(self, input_shape_a, input_shape_b, device):
+        torch_a = torch.rand(input_shape_a, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shape_b, dtype=torch.bfloat16)
+
+        torch_output_tensor = torch.xlogy(torch_a, torch_b)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output_tensor = ttnn.xlogy(a, b)
+        output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+        output_tensor = ttnn.to_torch(output_tensor)
+
+        assert_with_pcc(torch_output_tensor, output_tensor, 0.99)
+
+    def test_ttnn_logical_or(self, input_shape_a, input_shape_b, device):
+        torch_a = torch.rand(input_shape_a, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shape_b, dtype=torch.bfloat16)
+
+        torch_output_tensor = torch.logical_or(torch_a, torch_b)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output_tensor = ttnn.logical_or(a, b)
+        output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+        output_tensor = ttnn.to_torch(output_tensor)
+
+        assert_with_pcc(torch_output_tensor, output_tensor, 0.99)
+
+    def test_ttnn_nextafter(self, input_shape_a, input_shape_b, device):
+        torch_a = torch.rand(input_shape_a, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shape_b, dtype=torch.bfloat16)
+
+        torch_output_tensor = torch.nextafter(torch_a, torch_b)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output_tensor = ttnn.nextafter(a, b)
+        output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+        output_tensor = ttnn.to_torch(output_tensor)
+
+        assert_with_pcc(torch_output_tensor, output_tensor, 0.99)
+
+    def test_ttnn_logical_xor(self, input_shape_a, input_shape_b, device):
+        torch_a = torch.rand(input_shape_a, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shape_b, dtype=torch.bfloat16)
+
+        torch_output_tensor = torch.logical_xor(torch_a, torch_b)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output_tensor = ttnn.logical_xor(a, b)
+        output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+        output_tensor = ttnn.to_torch(output_tensor)
+
+        assert_with_pcc(torch_output_tensor, output_tensor, 0.99)
+
+    def test_ttnn_logical_and(self, input_shape_a, input_shape_b, device):
+        torch_a = torch.rand(input_shape_a, dtype=torch.bfloat16)
+        torch_b = torch.rand(input_shape_b, dtype=torch.bfloat16)
+
+        torch_output_tensor = torch.logical_and(torch_a, torch_b)
+
+        a = ttnn.from_torch(torch_a)
+        b = ttnn.from_torch(torch_b)
+
+        a = ttnn.to_device(a, device)
+        b = ttnn.to_device(b, device)
+
+        output_tensor = ttnn.logical_and(a, b)
+        output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+        output_tensor = ttnn.to_torch(output_tensor)
+
+        assert_with_pcc(torch_output_tensor, output_tensor, 0.99)
