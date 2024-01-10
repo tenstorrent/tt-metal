2a1c90e4b75582ba695e7201657d8285a98cb468
Muthu <ubuntu@tenstorrent.com>
2024-01-10 21:24:11 +0000

    #0: common wrapper updated

--- ttnn/ttnn/common.py
+++ ttnn/ttnn/common.py
@@ -6,10 +6,10 @@
 def make_class_member_fn(TensorClass, symbols, function_names, nargs):
     functions = list(filter(lambda x: x, [v for k, v in symbols.items() if k in function_names]))
     for fn in functions:
-        handle = (
-            lambda self, *_: ((len(*_) + 1) >= nargs)
-            and fn(self, *_)
-            or ValueError(f"Too few arguments for {TensorClass.__name__}.{fn.__name__}")
-        )
+
+        def handle(self, *_):
+            assert (len(_) + 1) >= nargs, ValueError(f"Too few arguments for {TensorClass.__name__}.{fn.__name__}")
+            return fn(self, *_)
+
         setattr(TensorClass, fn.__name__.split(".")[-1], handle)
     return
