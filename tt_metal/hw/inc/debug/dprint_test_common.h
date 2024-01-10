9374b513aa655dbd9a3d5ff78d04b6b6c572b01c
David Ma <davidma@tenstorrent.com>
2024-01-10 00:43:58 +0000

    #4632: Add dprint server support for eth cores

--- tt_metal/hw/inc/debug/dprint_test_common.h
+++ tt_metal/hw/inc/debug/dprint_test_common.h
@@ -18,8 +18,11 @@ inline void print_test_data() {
     DPRINT << DEFAULTFLOAT();
     DPRINT << "SETW:\n" << SETW(10) << my_int << my_int << SETW(4) << "ab" << ENDL();
     DPRINT << "HEX/OCT/DEC:\n" << HEX() << my_int << OCT() << my_int << DEC() << my_int << ENDL();
+#ifndef COMPILE_FOR_ERISC
+    // Eth cores don't have CBs, so don't try TSLICE printing.
     DPRINT << "SLICE:\n";
     cb_wait_front(tt::CB::c_in0, 1);
     DPRINT << TSLICE(tt::CB::c_in0, 0, SliceRange::hw0_32_8());
     DPRINT << TSLICE(tt::CB::c_in0, 0, SliceRange::hw0_32_4());
+#endif
 }
