96c4a039b622b11fa84bbccd2b625f27c2c62a84
umadevimcw <umadevi@multicorewareinc.com>
2024-01-11 14:28:01 +0000

    #4607: Export concat op to ttnn

--- ttnn/ttnn/binary.py
+++ ttnn/ttnn/binary.py
@@ -452,6 +452,77 @@ def register_ttl_outer_function(name, ttl_binary_function, torch_function):
     __all__.append(name)
 
 
+def register_ttl_concat_function(name, ttl_binary_function, torch_function):
+    def _torch_binary(input_tensor_a: Tensor, input_tensor_b: Tensor, dim, **_):
+        import torch
+        import ttnn
+
+        input_tensor_a = ttnn.from_device(input_tensor_a)
+        input_tensor_a = ttnn.to_layout(input_tensor_a, ttnn.ROW_MAJOR_LAYOUT)
+        input_tensor_a = ttnn.to_torch(input_tensor_a)
+
+        input_tensor_b = ttnn.from_device(input_tensor_b)
+        input_tensor_b = ttnn.to_layout(input_tensor_b, ttnn.ROW_MAJOR_LAYOUT)
+        input_tensor_b = ttnn.to_torch(input_tensor_b)
+        assert torch_function, f"Torch function not implemented for {str(ttl_binary_function)}"
+        return torch_function([input_tensor_a, input_tensor_b], dim)
+
+    @decorate_operation(torch_function=_torch_binary, name=name)
+    def binary_function(
+        input_tensor_a: Tensor,
+        input_tensor_b: Tensor,
+        dim: int,
+        *,
+        memory_config: MemoryConfig = DRAM_MEMORY_CONFIG,
+    ) -> Tensor:
+        f"""{name}(input_tensor_a: Tensor, input_tensor_b: Tensor) -> Tensor
+
+        Applies {name} to :attr:`input_tensor_a` and  :attr:`input_tensor_b` element-wise.
+
+        .. math::
+            {name}(\\mathrm{{input\\_tensor_a}}_i)
+            {name}(\\mathrm{{input\\_tensor_b}}_i)
+
+        Args:
+            * :attr:`input_tensor_a`
+            * :attr:`input_tensor_b`
+
+        Example::
+
+            >>> tensor_a = ttnn.to_device(ttnn.from_torch(torch.tensor((1, 2), dtype=torch.bfloat16)), device)
+            >>> tensor_b = ttnn.to_device(ttnn.from_torch(torch.tensor((2, 2), dtype=torch.bfloat16)), device)
+            >>> output = ttnn.{name}(tensor_a, tensor_b)
+            >>> print(output)
+            Tensor([ 2, 2], dtype=bfloat16 )
+
+        """
+
+        original_shape = input_tensor_a.shape
+        input_tensor_a = _reshape_to_4D(input_tensor_a)
+        input_tensor_b = _reshape_to_4D(input_tensor_b)
+
+        if not isinstance(input_tensor_a, Tensor) or not isinstance(input_tensor_b, Tensor):
+            raise TypeError("Expected both arguments to be a ttnn.Tensor")
+
+        if not has_storage_type_of(input_tensor_a, DEVICE_STORAGE_TYPE) or not has_storage_type_of(
+            input_tensor_b, DEVICE_STORAGE_TYPE
+        ):
+            raise RuntimeError("input_tensors must be on device!")
+
+        ttl_input_tensor_a = input_tensor_a.value
+        ttl_input_tensor_b = input_tensor_b.value
+
+        ttl_output_tensor = ttl_binary_function(
+            [ttl_input_tensor_a, ttl_input_tensor_b], dim, output_mem_config=memory_config
+        )
+
+        output_tensor = Tensor(ttl_output_tensor)
+        return output_tensor
+
+    setattr(THIS_MODULE, name, binary_function)
+    __all__.append(name)
+
+
 # register functions
 
 
@@ -526,5 +597,14 @@ TTL_FUNCTION_OUTER = [
 
 for binary_function_name, ttl_binary_function, torch_function in TTL_FUNCTION_OUTER:
     register_ttl_outer_function(binary_function_name, ttl_binary_function, torch_function)
+
+
+TTL_FUNCTION_CONCAT = [
+    ("concat", ttl.tensor.concat, torch.cat),
+]
+
+for binary_function_name, ttl_binary_function, torch_function in TTL_FUNCTION_CONCAT:
+    register_ttl_concat_function(binary_function_name, ttl_binary_function, torch_function)
+
 
 make_class_member_fn(Tensor, locals(), __all__, 2)
