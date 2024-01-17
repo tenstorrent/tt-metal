3277b61ee328b78bc3d44fd38101c88e5af9bdca
banekg <barsic@tenstorrent.com>
2024-01-17 09:33:50 +0000

    #4748: Add some unit tests for ttlib ops

---
+++ tests/tt_eager/python_api_testing/non_working_unit_tests/wormhole/test_eltwise_threshold.py
@@ -0,0 +1,78 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from pathlib import Path
+import sys
+from loguru import logger
+import random
+import pytest
+import torch
+import tt_lib as ttl
+
+from tests.tt_eager.python_api_testing.sweep_tests import pytorch_ops
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_equal
+from tests.tt_eager.python_api_testing.sweep_tests.tt_lib_ops import eltwise_threshold as tt_eltwise_threshold
+from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_rand
+from tests.tt_eager.python_api_testing.sweep_tests.common import set_slow_dispatch_mode
+
+
+def run_eltwise_threshold_tests(
+    input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, threshold, value, dispatch_mode, device
+):
+    torch.manual_seed(data_seed)
+    prev_dispatch_mode = set_slow_dispatch_mode(dispatch_mode)
+
+    x = gen_rand(size=input_shape, low=-100, high=100).to(torch.bfloat16)
+    # compute ref value
+    x_ref = x.detach().clone()
+    ref_value = pytorch_ops.threshold(x_ref, threshold=threshold, value=value)
+
+    tt_result = tt_eltwise_threshold(
+        x=x,
+        threshold=threshold,
+        value=value,
+        device=device,
+        dtype=[dtype],
+        layout=[dlayout],
+        input_mem_config=[in_mem_config],
+        output_mem_config=out_mem_config,
+    )
+
+    # compare tt and golden outputs
+    success, pcc_value = comp_equal(ref_value, tt_result)
+    logger.debug(pcc_value)
+    logger.debug(success)
+
+    set_slow_dispatch_mode(prev_dispatch_mode)
+    assert success
+
+
+# 71.0	-16.375	4133507	(('TT_METAL_SLOW_DISPATCH_MODE', '1'),)	completed	Max ATOL Delta: 99.0, Max RTOL Delta: inf, PCC: 0.9968988009373057, Equal check failed	fail	NA	NA	NA	Details
+
+test_sweep_args = [
+    (
+        (4, 22, 346, 376),
+        ttl.tensor.DataType.BFLOAT16,
+        ttl.tensor.Layout.ROW_MAJOR,
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        4133507,
+        71.0,
+        -16.375,
+        "1",
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, threshold, value, dispatch_mode",
+    (test_sweep_args),
+)
+def test_eltwise_threshold_test(
+    input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, threshold, value, dispatch_mode, device
+):
+    random.seed(0)
+    run_eltwise_threshold_tests(
+        input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, threshold, value, dispatch_mode, device
+    )
---
+++ tests/tt_eager/python_api_testing/non_working_unit_tests/wormhole/test_transpose_hc.py
@@ -0,0 +1,73 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from pathlib import Path
+import sys
+from loguru import logger
+import random
+import pytest
+import torch
+import tt_lib as ttl
+
+from tests.tt_eager.python_api_testing.sweep_tests import pytorch_ops
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_equal
+from tests.tt_eager.python_api_testing.sweep_tests.tt_lib_ops import transpose_hc as tt_transpose_hc
+from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_rand
+from tests.tt_eager.python_api_testing.sweep_tests.common import set_slow_dispatch_mode
+
+
+def run_transpose_hc_tests(
+    input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, dispatch_mode, device
+):
+    torch.manual_seed(data_seed)
+    prev_dispatch_mode = set_slow_dispatch_mode(dispatch_mode)
+
+    x = gen_rand(size=input_shape, low=-100, high=100).to(torch.bfloat16)
+    # compute ref value
+    x_ref = x.detach().clone()
+    ref_value = pytorch_ops.transpose(x_ref, dim0=1, dim1=-2)
+
+    tt_result = tt_transpose_hc(
+        x=x,
+        device=device,
+        dtype=[dtype],
+        layout=[dlayout],
+        input_mem_config=[in_mem_config],
+        output_mem_config=out_mem_config,
+    )
+
+    # compare tt and golden outputs
+    success, pcc_value = comp_equal(ref_value, tt_result)
+    logger.debug(pcc_value)
+    logger.debug(success)
+
+    set_slow_dispatch_mode(prev_dispatch_mode)
+    assert success
+
+
+# 5239758	(('TT_METAL_SLOW_DISPATCH_MODE', '1'),)	completed	Max ATOL Delta: 198.0, Max RTOL Delta: inf, PCC: 0.9875252415486026, Equal check failed	fail	NA	NA	NA	Details
+
+
+test_sweep_args = [
+    (
+        (2, 4, 214, 398),
+        ttl.tensor.DataType.BFLOAT16,
+        ttl.tensor.Layout.ROW_MAJOR,
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        5239758,
+        "1",
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, dispatch_mode",
+    (test_sweep_args),
+)
+def test_transpose_hc_test(
+    input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, dispatch_mode, device
+):
+    random.seed(0)
+    run_transpose_hc_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, dispatch_mode, device)
