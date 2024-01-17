1895fb725fce69d2c59de7a5793b7ac0509f2068
seunghwan100 <seunghwan100@github.com>
2024-01-17 05:23:16 +0000

    #4736: Impl moreh_norm_w

--- tt_eager/tt_dnn/op_library/moreh_norm/moreh_norm_w/moreh_norm_w.cpp
+++ tt_eager/tt_dnn/op_library/moreh_norm/moreh_norm_w/moreh_norm_w.cpp
@@ -55,7 +55,11 @@ operation::ProgramWithCallbacks moreh_norm_w_impl(const Tensor &input, float p, 
     const auto Ht = H / TILE_HEIGHT;
     const auto Wt = W / TILE_WIDTH;
 
+    const auto origin_w = input_shape.without_padding()[3];
+
     auto [floored_p, decimal, p_is_negative] = get_floored_p_and_decimal_and_p_is_negative(p);
+    auto [floored_recip_p, recip_p_decimal, recip_p_is_negative] =
+        get_floored_p_and_decimal_and_p_is_negative(1.0f / p);
 
     ////////////////////////////////////////////////////////////////////////////
     //                         Core Setup
@@ -85,6 +89,8 @@ operation::ProgramWithCallbacks moreh_norm_w_impl(const Tensor &input, float p, 
     const uint32_t in0_t{1};  // input
     const uint32_t in1_t{1};  // one
     const uint32_t in2_t{1};  // decimal
+    const uint32_t in3_t{1};  // recip_p_decimal
+    const uint32_t in4_t{1};  // mask_w
 
     const uint32_t out0_t{1};  // output
 
@@ -108,6 +114,8 @@ operation::ProgramWithCallbacks moreh_norm_w_impl(const Tensor &input, float p, 
             {CB::c_in0, in0_t},    // input
             {CB::c_in1, in1_t},    // one
             {CB::c_in2, in2_t},    // decimal
+            {CB::c_in3, in3_t},    // recip_p_decimal
+            {CB::c_in4, in4_t},    // mask_w
             {CB::c_out0, out0_t},  // output
             {CB::c_intermed0, im0_t},
             {CB::c_intermed1, im1_t},
@@ -145,15 +153,11 @@ operation::ProgramWithCallbacks moreh_norm_w_impl(const Tensor &input, float p, 
         "tt_eager/tt_dnn/op_library/moreh_norm/moreh_norm_w/kernels/"
         "moreh_norm_w_kernel.cpp";
 
-    // const std::vector<uint32_t> compute_args_group_1{};
-
     const auto compute_kernels_id_1 = CreateComputeKernel(
         program, compute_kernel_file, {core_group_1, num_rows_per_core_group_1, {}}, compute_defines);
 
-    KernelHandle compute_kernels_id_2;
+    KernelHandle compute_kernels_id_2{0};
     if (!core_group_2.ranges().empty()) {
-        // const std::vector<uint32_t> compute_args_group_2{};
-
         compute_kernels_id_2 = CreateComputeKernel(
             program, compute_kernel_file, {core_group_2, num_rows_per_core_group_2, {}}, compute_defines);
     }
@@ -184,9 +188,11 @@ operation::ProgramWithCallbacks moreh_norm_w_impl(const Tensor &input, float p, 
             input_addr,
             static_cast<uint32_t>(is_dram(input)),
             *reinterpret_cast<uint32_t *>(&decimal),
+            *reinterpret_cast<uint32_t *>(&recip_p_decimal),
             num_rows_per_core,
             Wt,
-            tile_offset};
+            tile_offset,
+            origin_w};
         SetRuntimeArgs(program, reader_kernels_id, core, reader_runtime_args);
 
         // writer
@@ -196,7 +202,13 @@ operation::ProgramWithCallbacks moreh_norm_w_impl(const Tensor &input, float p, 
 
         // compute
         const std::vector<uint32_t> compute_runtime_args{
-            num_rows_per_core, Wt, static_cast<uint32_t>(floored_p), static_cast<uint32_t>(p_is_negative)};
+            num_rows_per_core,
+            Wt,
+            origin_w,
+            floored_p,
+            static_cast<uint32_t>(p_is_negative),
+            floored_recip_p,
+            static_cast<uint32_t>(recip_p_is_negative)};
         SetRuntimeArgs(program, compute_kernel_id, core, compute_runtime_args);
 
         tile_offset += num_rows_per_core * Wt;
