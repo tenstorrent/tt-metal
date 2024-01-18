8e03f7c2959c10a5349dc3afc7df0aef46e6c144
Austin Ho <aho@tenstorrent.com>
2024-01-18 15:59:29 +0000

    #4802: Update transpose wh to support sharding in specific cases. TODO: Generalize

--- tt_eager/tt_dnn/op_library/transpose/transpose_op.hpp
+++ tt_eager/tt_dnn/op_library/transpose/transpose_op.hpp
@@ -41,6 +41,7 @@ Tensor transpose(const Tensor &a, std::int64_t dim1, std::int64_t dim2, const Me
 
 operation::ProgramWithCallbacks transpose_single_core(const Tensor &a, Tensor &output, TransposeOpDim transpose_dim);
 operation::ProgramWithCallbacks transpose_wh_multi_core(const Tensor &a, Tensor &output);
+operation::ProgramWithCallbacks transpose_wh_multi_core_sharded(const Tensor &a, Tensor &output);
 operation::ProgramWithCallbacks transpose_hc_multi_core(const Tensor &a, Tensor &output);
 operation::ProgramWithCallbacks transpose_cn_multi_core(const Tensor &a, Tensor &output);
 
