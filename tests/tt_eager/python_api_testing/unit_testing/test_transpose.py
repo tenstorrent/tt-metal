c3f800e83989f928b0e5e070629205ba1dc1b6c5
Austin Ho <aho@tenstorrent.com>
2024-01-18 15:59:29 +0000

    #4802: Update transpose wh to support sharding in specific cases. TODO: Generalize

--- tests/tt_eager/python_api_testing/unit_testing/test_transpose.py
+++ tests/tt_eager/python_api_testing/unit_testing/test_transpose.py
@@ -5,40 +5,53 @@
 import torch
 
 import tt_lib as ttl
-from models.utility_functions import print_diff_argmax
-from models.utility_functions import is_wormhole_b0, skip_for_wormhole_b0
+from loguru import logger
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc, comp_equal
 
 
-def transpose(input_shape, device, dim0, dim1, expected_program_cache_size=None):
+def transpose(
+    input_shape,
+    device,
+    dim0,
+    dim1,
+    input_mem_config=ttl.tensor.MemoryConfig(),
+    output_mem_config=ttl.tensor.MemoryConfig(),
+    input_shard_spec=None,
+    input_dtype=ttl.tensor.DataType.BFLOAT16,
+    expected_program_cache_size=None,
+):
     output_shape = list(input_shape)
     output_shape[dim0], output_shape[dim1] = input_shape[dim1], input_shape[dim0]
 
     x = torch.randn(input_shape).bfloat16().float()
 
-    xt = (
-        ttl.tensor.Tensor(
-            x.reshape(-1).tolist(),
-            x.shape,
-            ttl.tensor.DataType.BFLOAT16,
-            ttl.tensor.Layout.ROW_MAJOR,
-        )
-        .to(ttl.tensor.Layout.TILE)
-        .to(device)
-    )
-    xtt = ttl.tensor.transpose(xt, dim0, dim1)
+    xt = ttl.tensor.Tensor(
+        x.reshape(-1).tolist(),
+        x.shape,
+        input_dtype,
+        ttl.tensor.Layout.ROW_MAJOR,
+    ).to(ttl.tensor.Layout.TILE)
+    if input_shard_spec is not None:
+        xt = xt.to(device, input_mem_config, input_shard_spec)
+    else:
+        xt = xt.to(device, input_mem_config)
+    xtt = ttl.tensor.transpose(xt, dim0, dim1, output_mem_config)
     assert list(xtt.shape()) == output_shape
     transposed_ref = x.transpose(dim0, dim1)
 
     tt_got_back = xtt.cpu().to(ttl.tensor.Layout.ROW_MAJOR).to_torch()
 
-    eq = torch.equal(tt_got_back, transposed_ref)
-    assert eq
+    if input_dtype == ttl.tensor.DataType.BFLOAT16:
+        passing, output = comp_equal(transposed_ref, tt_got_back)
+    else:
+        passing, output = comp_pcc(transposed_ref, tt_got_back)
+    logger.info(output)
+    assert passing
 
     if expected_program_cache_size != None:
         assert ttl.program_cache.num_entries() == expected_program_cache_size
 
 
-@skip_for_wormhole_b0()
 def test_transpose_hc(device):
     N = 3
     C = 32 * 2
@@ -115,3 +128,103 @@ def test_transpose_wh_program_cache(device, use_program_cache):
     input_shape = (N, C, H, W)
     # CACHE MISS since its single core
     transpose(input_shape, device, dim0=-2, dim1=-1, expected_program_cache_size=2)
+
+
+def test_transpose_wh_sharded_program_cache(device, use_program_cache):
+    compute_grid_size = device.compute_with_storage_grid_size()
+    mem_config = ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.HEIGHT_SHARDED, ttl.tensor.BufferType.L1)
+    input_dtype = ttl.tensor.DataType.BFLOAT8_B
+
+    N = 32
+    C = 2
+    H = 32 * 4
+    W = 64
+    input_shape = torch.Size([N, C, H, W])
+
+    num_cores = min(N, compute_grid_size.x * compute_grid_size.y)
+    shard_grid = ttl.tensor.CoreRangeSet(ttl.tensor.num_cores_to_corerange_set(num_cores, compute_grid_size, True))
+    input_shard_spec = ttl.tensor.ShardSpec(
+        shard_grid,
+        [
+            input_shape.numel() // input_shape[-1] // num_cores,
+            input_shape[-1],
+        ],
+        ttl.tensor.ShardOrientation.ROW_MAJOR,
+        False,
+    )
+
+    transpose(
+        input_shape,
+        device,
+        dim0=-2,
+        dim1=-1,
+        input_mem_config=mem_config,
+        output_mem_config=mem_config,
+        input_shard_spec=input_shard_spec,
+        input_dtype=input_dtype,
+        expected_program_cache_size=1,
+    )
+
+    # changing shape
+    N = 32
+    C = 8
+    H = 32 * 4
+    W = 64
+
+    input_shape = torch.Size([N, C, H, W])
+
+    num_cores = min(N, compute_grid_size.x * compute_grid_size.y)
+    shard_grid = ttl.tensor.CoreRangeSet(ttl.tensor.num_cores_to_corerange_set(num_cores, compute_grid_size, True))
+    input_shard_spec = ttl.tensor.ShardSpec(
+        shard_grid,
+        [
+            input_shape.numel() // input_shape[-1] // num_cores,
+            input_shape[-1],
+        ],
+        ttl.tensor.ShardOrientation.ROW_MAJOR,
+        False,
+    )
+
+    transpose(
+        input_shape,
+        device,
+        dim0=-2,
+        dim1=-1,
+        input_mem_config=mem_config,
+        output_mem_config=mem_config,
+        input_shard_spec=input_shard_spec,
+        input_dtype=input_dtype,
+        expected_program_cache_size=1,
+    )
+
+    # changing shape
+    N = 32
+    C = 2
+    H = 2048
+    W = 64
+
+    input_shape = torch.Size([N, C, H, W])
+
+    num_cores = min(N, compute_grid_size.x * compute_grid_size.y)
+    shard_grid = ttl.tensor.CoreRangeSet(ttl.tensor.num_cores_to_corerange_set(num_cores, compute_grid_size, True))
+    input_shard_spec = ttl.tensor.ShardSpec(
+        shard_grid,
+        [
+            input_shape.numel() // input_shape[-1] // num_cores,
+            input_shape[-1],
+        ],
+        ttl.tensor.ShardOrientation.ROW_MAJOR,
+        False,
+    )
+
+    transpose(
+        input_shape,
+        device,
+        dim0=-2,
+        dim1=-1,
+        input_mem_config=mem_config,
+        output_mem_config=mem_config,
+        input_shard_spec=input_shard_spec,
+        input_dtype=input_dtype,
+        expected_program_cache_size=1,
+    )
