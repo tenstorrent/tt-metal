32d18d8a651b1a205b23518df441f9b2604565cd
banekg <barsic@tenstorrent.com>
2024-01-19 14:21:42 +0000

    #4484: Add unit test for reduce_max_w op

---
+++ tests/tt_eager/python_api_testing/non_working_unit_tests/wormhole/test_reduce_max_w.py
@@ -0,0 +1,79 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from pathlib import Path
+import sys
+from loguru import logger
+import random
+import pytest
+import torch
+import tt_lib as ttl
+
+from tests.tt_eager.python_api_testing.sweep_tests import pytorch_ops
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_equal
+from tests.tt_eager.python_api_testing.sweep_tests.tt_lib_ops import reduce_max_w as tt_reduce_max_w
+from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_rand
+from tests.tt_eager.python_api_testing.sweep_tests.common import set_slow_dispatch_mode
+
+
+def run_reduce_max_w_tests(
+    input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, dispatch_mode, device
+):
+    torch.manual_seed(data_seed)
+    prev_dispatch_mode = set_slow_dispatch_mode(dispatch_mode)
+
+    x = gen_rand(size=input_shape, low=-100, high=100).to(torch.bfloat16)
+    # compute ref value
+    x_ref = x.detach().clone()
+    ref_value = pytorch_ops.reduce_max(x_ref, dims=(-1,))
+
+    tt_result = tt_reduce_max_w(
+        x=x,
+        device=device,
+        dtype=[dtype],
+        layout=[dlayout],
+        input_mem_config=[in_mem_config],
+        output_mem_config=out_mem_config,
+    )
+
+    # compare tt and golden outputs
+    success, pcc_value = comp_equal(ref_value, tt_result)
+    logger.debug(pcc_value)
+    logger.debug(success)
+
+    set_slow_dispatch_mode(prev_dispatch_mode)
+    assert success
+
+
+test_sweep_args = [
+    (
+        (5, 4, 160, 64),
+        ttl.tensor.DataType.BFLOAT16,
+        ttl.tensor.Layout.TILE,
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        8405597,
+        "",
+    ),
+    (
+        (3, 5, 178, 252),
+        ttl.tensor.DataType.BFLOAT16,
+        ttl.tensor.Layout.ROW_MAJOR,
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1),
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        3361506,
+        "",
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, dispatch_mode",
+    (test_sweep_args),
+)
+def test_reduce_max_w_test(
+    input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, dispatch_mode, device
+):
+    random.seed(0)
+    run_reduce_max_w_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, dispatch_mode, device)
