2a8cbb11ff143c4621709d57474629d2e000c9ac
jayasuryamaganuru <maganuru.jayasurya@multicorewareinc.com>
2024-01-22 08:36:38 +0000

    #4786: Add upsample_nearest2d functional stable diffusion

---
+++ models/experimental/functional_stable_diffusion/tt/ttnn_functional_upsample_nearest_2d.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+import ttnn
+
+
+def upsample_nearest2d(input, scale_factor=2):
+    assert scale_factor % 1 == 0 and scale_factor > 0, "We only support scaling by positive integer values"
+
+    up_output = ttnn.repeat_interleave(input, scale_factor, dim=3)
+    up_output = ttnn.repeat_interleave(up_output, scale_factor, dim=2)
+    return up_output
