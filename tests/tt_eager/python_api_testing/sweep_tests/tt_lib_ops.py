21c41c2dcff79433b70183ce91dbe0d5bfd2510d
banekg <barsic@tenstorrent.com>
2024-01-22 10:34:48 +0000

    #4825: Change num_rows position in tensor

--- tests/tt_eager/python_api_testing/sweep_tests/tt_lib_ops.py
+++ tests/tt_eager/python_api_testing/sweep_tests/tt_lib_ops.py
@@ -2150,14 +2150,12 @@ def embeddings(x, y, *args, device, dtype, layout, input_mem_config, output_mem_
     y_shape = y.shape
 
     batch_size = x_shape[0]
-    num_rows = x_shape[2]
+    num_rows = x_shape[3]
     embedding_dim = y_shape[3]
 
     x_ref = x.detach().clone()
 
-    # transpose last two dimensions
-    transposed_tensor = torch.transpose(x_ref, 3, 2)
-    t0 = torch.clamp(transposed_tensor, min=0, max=y.shape[-2] - 1)
+    t0 = torch.clamp(x_ref, min=0, max=y.shape[-2] - 1)
     t0 = ttl.tensor.Tensor(t0, dtype[0]).to(device, input_mem_config[0])
 
     t1 = ttl.tensor.Tensor(y, dtype[1]).to(device, input_mem_config[1])
