0a552131b2a457cf730dbcba9c0355ce914d11df
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2024-01-25 22:07:02 +0000

    #4003: split unary sweep into per op sweeps

--- tests/ttnn/sweep_tests/sweeps/rsqrt.py
+++ tests/ttnn/sweep_tests/sweeps/rsqrt.py
@@ -13,13 +13,6 @@ from models.utility_functions import torch_random
 
 
 parameters = {
-    "ttnn_function,torch_function": [
-        (ttnn.exp, torch.exp),
-        (ttnn.tanh, torch.tanh),
-        (ttnn.gelu, torch.nn.functional.gelu),
-        (ttnn.rsqrt, torch.rsqrt),
-        (ttnn.relu, torch.relu),
-    ],
     "batch_sizes": [(1,)],
     "height": [384, 1024],
     "width": [1024, 4096],
@@ -38,8 +31,6 @@ def is_expected_to_fail(**_) -> Tuple[bool, Optional[str]]:
 
 
 def run(
-    ttnn_function,
-    torch_function,
     batch_sizes,
     height,
     width,
@@ -51,19 +42,17 @@ def run(
 ) -> Tuple[bool, Optional[str]]:
     input_shape = (*batch_sizes, height, width)
 
-    low = -0.1
+    low = 0
     high = 0.1
-    if ttnn_function in {ttnn.rsqrt}:
-        low = 0.0
 
     torch_input_tensor = torch_random(input_shape, low, high, dtype=torch.float32)
-    torch_output_tensor = torch_function(torch_input_tensor)
+    torch_output_tensor = torch.rsqrt(torch_input_tensor)
 
     input_tensor = ttnn.from_torch(
         torch_input_tensor, dtype=input_dtype, device=device, memory_config=input_memory_config
     )
 
-    output_tensor = ttnn_function(input_tensor, memory_config=output_memory_config)
+    output_tensor = ttnn.rsqrt(input_tensor, memory_config=output_memory_config)
     output_tensor = ttnn.to_torch(output_tensor)
 
     return check_with_pcc(torch_output_tensor, output_tensor, 0.999)
