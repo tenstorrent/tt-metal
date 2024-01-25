b26e3ba683346b99e5b1a5a79268c0fb4ff9ad7a
David Ma <davidma@tenstorrent.com>
2024-01-25 20:03:36 +0000

    #4756: Fix watcher waypoint test to query clock frequency and allocate a L1 buffer for syncing
    Also update watcher test fixture to restore previous state

--- tests/tt_metal/tt_metal/test_kernels/misc/watcher_waypoints.cpp
+++ tests/tt_metal/tt_metal/test_kernels/misc/watcher_waypoints.cpp
@@ -6,17 +6,12 @@
 #include "ckernel.h"
 #include "debug/status.h"
 
-// Wait cycles to create a ~1sec delay
-#define WAIT_CYCLES 1200000000
-// Flag address for syncing between cores (BRISC controls this)
-#define SYNC_ADDR 409600
-
 // Helper function to sync execution by forcing other riscs to wait for brisc, which in turn waits
 // for a set number of cycles.
-void hacky_sync(uint32_t sync_num) {
-    volatile tt_l1_ptr uint32_t* sync_ptr = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(SYNC_ADDR);
+void hacky_sync(uint32_t sync_num, uint32_t wait_cycles, uint32_t sync_addr) {
+    volatile tt_l1_ptr uint32_t* sync_ptr = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(sync_addr);
 #if defined(COMPILE_FOR_BRISC)
-    ckernel::wait(WAIT_CYCLES);
+    ckernel::wait(wait_cycles);
     *(sync_ptr) = sync_num;
 #else
     while (*(sync_ptr) != sync_num) { ; }
@@ -33,15 +28,17 @@ void kernel_main() {
 namespace NAMESPACE {
 void MAIN {
 #endif
+    uint32_t sync_wait_cycles = get_arg_val<uint32_t>(0);
+    uint32_t sync_address     = get_arg_val<uint32_t>(1);
 
     // Post a new waypoint with a delay after (to let the watcher poll it)
-    hacky_sync(1);
+    hacky_sync(1, sync_wait_cycles, sync_address);
     DEBUG_STATUS('A', 'A', 'A', 'A');
-    hacky_sync(2);
+    hacky_sync(2, sync_wait_cycles, sync_address);
     DEBUG_STATUS('B', 'B', 'B', 'B');
-    hacky_sync(3);
+    hacky_sync(3, sync_wait_cycles, sync_address);
     DEBUG_STATUS('C', 'C', 'C', 'C');
-    hacky_sync(4);
+    hacky_sync(4, sync_wait_cycles, sync_address);
 #if defined(COMPILE_FOR_BRISC) | defined(COMPILE_FOR_NCRISC)
 }
 #else
