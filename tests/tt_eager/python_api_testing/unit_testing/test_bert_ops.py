05f0fe0fe71e707d322c0478d8f4e5af603ec198
yugaoTT <yugao@tenstorrent.com>
2024-01-26 15:46:29 +0000

    #4573: add fix to mm_block_init_short
    #0: fix doc

--- tests/tt_eager/python_api_testing/unit_testing/test_bert_ops.py
+++ tests/tt_eager/python_api_testing/unit_testing/test_bert_ops.py
@@ -319,8 +319,8 @@ def test_bert_linear_batch7(
             while out_block_w % out_subblock_w != 0:
                 out_subblock_w = out_block_w // 2
 
-    # out_subblock_h = 1
-    # out_subblock_w = 1
+    out_subblock_h = 1
+    out_subblock_w = 1
 
     logger.debug("in0 block w h " + str(in0_block_w * 32) + " " + str(in0_block_h * 32))
     logger.debug("in1 block w h " + str(out_block_w * 32) + " " + str(in0_block_w * 32))
