9d6b630844fa689777aff846d89e1e5b0f5e36d5
acejkov <acejkov@tenstorrent.com>
2024-01-29 21:07:40 +0000

    support for kernel broadcast in WH_B0 with matmul re-use when broadcast dim is 1

--- tt_llk_wormhole_b0/llk_lib/llk_unpack_AB_matmul.h
+++ tt_llk_wormhole_b0/llk_lib/llk_unpack_AB_matmul.h
@@ -14,9 +14,10 @@ using namespace ckernel;
 using namespace ckernel::unpacker;
 
 // transpose is unused, math is adjusted to take into account srca face layout when transpose=true
+template <std::uint32_t kernel_broadcast_a=0, std::uint32_t kernel_broadcast_b=0>
 inline void _llk_unpack_AB_matmul_mop_config_(const bool transpose, const std::uint32_t ct_dim, const std::uint32_t rt_dim, const std::uint32_t kt_dim, const bool unpA_partial_face, const bool unpB_partial_face) {
-    // in0 - loaded to SrcB
-    // in1 - loaded to SrcA
+    // in0/inA - loaded to SrcB
+    // in1/inB - loaded to SrcA
 
     const bool reuse_a = ct_dim >= rt_dim;
     const std::uint32_t replay_buf_prog_len = (reuse_a && unpA_partial_face) ? 16 : ((!reuse_a && unpB_partial_face) ? 14 : 10);
@@ -27,6 +28,7 @@ inline void _llk_unpack_AB_matmul_mop_config_(const bool transpose, const std::u
             TTI_REPLAY(0, 1, 0, 1);
             TTI_NOP;
         #else
+            static_assert(kernel_broadcast_b<=1, "kernel_broadcast>1 on matmul input 1 is not supported with reuse enabled");
             TT_REPLAY(0, replay_buf_prog_len, 0, 1);
             if (unpA_partial_face) {
                 TTI_UNPACR_NOP(p_unpacr_nop::UNP0, p_unpacr_nop::UNP_ZEROSRC);
@@ -36,10 +38,15 @@ inline void _llk_unpack_AB_matmul_mop_config_(const bool transpose, const std::u
             } else {
                 TTI_UNPACR(SrcA, 0, 0, 0, 0, 1 /*Set OvrdThreadId*/, 1 /*Set Dvalid*/, p_unpacr::RAREFYB_DISABLE, 0, 0 /* Set ContextIdInc */, 0, 0, 1);
             }    
-            TTI_RDCFG(p_gpr_unpack::TMP0, THCON_SEC0_REG3_Base_address_ADDR32);
-            TTI_ADDDMAREG(0, p_gpr_unpack::TMP0, p_gpr_unpack::TMP0, p_gpr_unpack::TILE_SIZE_A);
-            TTI_REG2FLOP(1,0,0,0,THCON_SEC0_REG3_Base_address_ADDR32-THCON_CFGREG_BASE_ADDR32, p_gpr_unpack::TMP0);
-            //TTI_REG2FLOP(1,0,0,0,THCON_SEC0_REG7_Offset_address_ADDR32-THCON_CFGREG_BASE_ADDR32, p_gpr_unpack::TMP_LO);
+            if constexpr (kernel_broadcast_b==1) {
+                TTI_NOP;
+                TTI_NOP;
+                TTI_NOP;
+            } else {
+                TTI_RDCFG(p_gpr_unpack::TMP0, THCON_SEC0_REG3_Base_address_ADDR32);
+                TTI_ADDDMAREG(0, p_gpr_unpack::TMP0, p_gpr_unpack::TMP0, p_gpr_unpack::TILE_SIZE_A);
+                TTI_REG2FLOP(1,0,0,0,THCON_SEC0_REG3_Base_address_ADDR32-THCON_CFGREG_BASE_ADDR32, p_gpr_unpack::TMP0);
+            }
             TTI_NOP;
             if (unpA_partial_face) {
                 TTI_UNPACR_NOP(p_unpacr_nop::UNP0, p_unpacr_nop::UNP_ZEROSRC);
@@ -49,10 +56,15 @@ inline void _llk_unpack_AB_matmul_mop_config_(const bool transpose, const std::u
             } else {
                 TTI_UNPACR(SrcA, 0, 0, 0, 0, 1 /*Set OvrdThreadId*/, 1 /*Set Dvalid*/, p_unpacr::RAREFYB_DISABLE, 0, 0 /* Set ContextIdInc */, 0, 0, 1);
             }    
-            TTI_RDCFG(p_gpr_unpack::TMP0, THCON_SEC0_REG3_Base_cntx1_address_ADDR32);
-            TTI_ADDDMAREG(0, p_gpr_unpack::TMP0, p_gpr_unpack::TMP0, p_gpr_unpack::TILE_SIZE_A);
-            TTI_REG2FLOP(1,0,0,0,THCON_SEC0_REG3_Base_cntx1_address_ADDR32-THCON_CFGREG_BASE_ADDR32, p_gpr_unpack::TMP0);
-            //TTI_REG2FLOP(1,0,0,0,THCON_SEC0_REG7_Offset_cntx1_address_ADDR32-THCON_CFGREG_BASE_ADDR32, p_gpr_unpack::TMP_LO);
+            if constexpr (kernel_broadcast_b==1) {
+                TTI_NOP;
+                TTI_NOP;
+                TTI_NOP;
+            } else {
+                TTI_RDCFG(p_gpr_unpack::TMP0, THCON_SEC0_REG3_Base_cntx1_address_ADDR32);
+                TTI_ADDDMAREG(0, p_gpr_unpack::TMP0, p_gpr_unpack::TMP0, p_gpr_unpack::TILE_SIZE_A);
+                TTI_REG2FLOP(1,0,0,0,THCON_SEC0_REG3_Base_cntx1_address_ADDR32-THCON_CFGREG_BASE_ADDR32, p_gpr_unpack::TMP0);
+            }    
             TTI_NOP;
         #endif
     } else {
@@ -60,6 +72,7 @@ inline void _llk_unpack_AB_matmul_mop_config_(const bool transpose, const std::u
             TTI_REPLAY(0, 1, 0, 1);
             TTI_NOP;
         #else
+            static_assert(kernel_broadcast_a<=1, "kernel_broadcast>1 on matmul input 0 is not supported with reuse enabled");
             TT_REPLAY(0, replay_buf_prog_len, 0, 1);
             if (unpB_partial_face) {
                 TTI_UNPACR(SrcB, 0b00010001, 0, 0, 0, 1 /*Set OvrdThreadId*/, 0 /*Set Dvalid*/, p_unpacr::RAREFYB_DISABLE, 0, 0 /* Set ContextIdInc */, 0, 0, 1);
@@ -68,10 +81,15 @@ inline void _llk_unpack_AB_matmul_mop_config_(const bool transpose, const std::u
             } else {
                 TTI_UNPACR(SrcB, 0, 0, 0, 0, 1 /*Set OvrdThreadId*/, 1 /*Set Dvalid*/, p_unpacr::RAREFYB_DISABLE, 0, 0 /* Set ContextIdInc */, 0, 0, 1);
             }
-            TTI_RDCFG(p_gpr_unpack::TMP0, THCON_SEC1_REG3_Base_address_ADDR32);
-            TTI_ADDDMAREG(0, p_gpr_unpack::TMP0, p_gpr_unpack::TMP0, p_gpr_unpack::TMP_LO);
-            TTI_REG2FLOP(1,0,0,0,THCON_SEC1_REG3_Base_address_ADDR32-THCON_CFGREG_BASE_ADDR32, p_gpr_unpack::TMP0);
-            //TTI_REG2FLOP(1,0,0,0,THCON_SEC1_REG7_Offset_address_ADDR32-THCON_CFGREG_BASE_ADDR32, p_gpr_unpack::TMP_LO);
+            if constexpr (kernel_broadcast_a==1) {
+                TTI_NOP;
+                TTI_NOP;
+                TTI_NOP;
+            } else {
+                TTI_RDCFG(p_gpr_unpack::TMP0, THCON_SEC1_REG3_Base_address_ADDR32);
+                TTI_ADDDMAREG(0, p_gpr_unpack::TMP0, p_gpr_unpack::TMP0, p_gpr_unpack::TMP_LO);
+                TTI_REG2FLOP(1,0,0,0,THCON_SEC1_REG3_Base_address_ADDR32-THCON_CFGREG_BASE_ADDR32, p_gpr_unpack::TMP0);
+            }    
             TTI_NOP;
             if (unpB_partial_face) {
                 TTI_UNPACR(SrcB, 0b00010001, 0, 0, 0, 1 /*Set OvrdThreadId*/, 0 /*Set Dvalid*/, p_unpacr::RAREFYB_DISABLE, 0, 0 /* Set ContextIdInc */, 0, 0, 1);
@@ -80,10 +98,15 @@ inline void _llk_unpack_AB_matmul_mop_config_(const bool transpose, const std::u
             } else {
                 TTI_UNPACR(SrcB, 0, 0, 0, 0, 1 /*Set OvrdThreadId*/, 1 /*Set Dvalid*/, p_unpacr::RAREFYB_DISABLE, 0, 0 /* Set ContextIdInc */, 0, 0, 1);
             }
-            TTI_RDCFG(p_gpr_unpack::TMP0, THCON_SEC1_REG3_Base_cntx1_address_ADDR32);
-            TTI_ADDDMAREG(0, p_gpr_unpack::TMP0, p_gpr_unpack::TMP0, p_gpr_unpack::TMP_LO);
-            TTI_REG2FLOP(1,0,0,0,THCON_SEC1_REG3_Base_cntx1_address_ADDR32-THCON_CFGREG_BASE_ADDR32, p_gpr_unpack::TMP0);
-            //TTI_REG2FLOP(1,0,0,0,THCON_SEC1_REG7_Offset_cntx1_address_ADDR32-THCON_CFGREG_BASE_ADDR32, p_gpr_unpack::TMP_LO);
+            if constexpr (kernel_broadcast_a==1) {
+                TTI_NOP;
+                TTI_NOP;
+                TTI_NOP;
+            } else {
+                TTI_RDCFG(p_gpr_unpack::TMP0, THCON_SEC1_REG3_Base_cntx1_address_ADDR32);
+                TTI_ADDDMAREG(0, p_gpr_unpack::TMP0, p_gpr_unpack::TMP0, p_gpr_unpack::TMP_LO);
+                TTI_REG2FLOP(1,0,0,0,THCON_SEC1_REG3_Base_cntx1_address_ADDR32-THCON_CFGREG_BASE_ADDR32, p_gpr_unpack::TMP0);
+            }    
             TTI_NOP;
         #endif
 
@@ -135,6 +158,7 @@ inline void _llk_unpack_AB_matmul_hw_configure_(const std::uint32_t unpA_src_for
     sync_regfile_write(p_gpr_unpack::TILE_SIZE_B);
 }
 
+template <std::uint32_t kernel_broadcast_a=0, std::uint32_t kernel_broadcast_b=0>
 __attribute__((always_inline)) inline void _llk_unpack_AB_matmul_init_(const std::uint32_t transpose=0, const std::uint32_t ct_dim=1, const std::uint32_t rt_dim=1, const std::uint32_t kt_dim=1, const std::uint32_t unpA_face_r_dim=FACE_R_DIM, const std::uint32_t unpB_face_r_dim=FACE_R_DIM, const std::uint32_t unpA_num_faces=4, const std::uint32_t unpB_num_faces=4, const bool unpA_partial_face=false, const bool unpB_partial_face=false) {
 
     const bool reuse_a = ct_dim >= rt_dim;
@@ -169,9 +193,11 @@ __attribute__((always_inline)) inline void _llk_unpack_AB_matmul_init_(const std
 
     TT_SETDMAREG(0, LOWER_HALFWORD(kt_dim), 0, LO_16(p_gpr_unpack::KT_DIM)); // store kt_dim to gpr for scaling tile size
 
-    _llk_unpack_AB_matmul_mop_config_(transpose != 0, ct_dim, rt_dim, kt_dim, unpA_partial_face, unpB_partial_face);
+    _llk_unpack_AB_matmul_mop_config_<kernel_broadcast_a, kernel_broadcast_b>(transpose != 0, ct_dim, rt_dim, kt_dim, unpA_partial_face, unpB_partial_face);
 }
 
+
+template <std::uint32_t kernel_broadcast_a=0, std::uint32_t kernel_broadcast_b=0>
 inline void _llk_unpack_AB_matmul_(
     const std::uint32_t base_address_a, const std::uint32_t base_address_b, const std::uint32_t tile_index_a, const std::uint32_t tile_index_b, const std::uint32_t tile_size_a, const std::uint32_t tile_size_b, const std::uint32_t unpA_face_r_dim=FACE_R_DIM, const std::uint32_t unpB_face_r_dim=FACE_R_DIM, const bool unpA_partial_face=false, const bool unpB_partial_face=false, std::uint32_t ct_dim=1, const std::uint32_t rt_dim=1, const std::uint32_t kt_dim=1) {
     // In0/InA -> srcB (supports partial face)
@@ -190,8 +216,16 @@ inline void _llk_unpack_AB_matmul_(
 
         std::uint32_t offset_address_a =tile_size_a*(tile_index_a + (reuse_a ? (t*kt_dim) : (0)));
         std::uint32_t next_offset_address_a = tile_size_a*(tile_index_a + (reuse_a ? ((t+1)*kt_dim) : (0)));
+        if constexpr (kernel_broadcast_a>0) {
+            offset_address_a =tile_size_a*(tile_index_a + (reuse_a ? ((t*kt_dim)%kernel_broadcast_a) : (0)));
+            next_offset_address_a = tile_size_a*(tile_index_a + (reuse_a ? (((t+1)*kt_dim)%kernel_broadcast_a) : (0)));
+        }
         std::uint32_t offset_address_b = tile_size_b*(tile_index_b + (reuse_a ? (0       ) : (t)));
         std::uint32_t next_offset_address_b = tile_size_b*(tile_index_b + (reuse_a ? (0       ) : (t+1)));
+        if constexpr (kernel_broadcast_b>0) {
+            offset_address_b = tile_size_b*(tile_index_b + (reuse_a ? (0       ) : (t%kernel_broadcast_b)));
+            next_offset_address_b = tile_size_b*(tile_index_b + (reuse_a ? (0       ) : ((t+1)%kernel_broadcast_b)));
+        }
         std::uint32_t address_a = base_address_a + offset_address_a;
         std::uint32_t next_address_a = base_address_a + next_offset_address_a;
         std::uint32_t address_b = base_address_b + offset_address_b;
