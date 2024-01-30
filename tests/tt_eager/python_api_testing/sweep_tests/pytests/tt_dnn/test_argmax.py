d8d8e6a3fbb143a7f76cbaba7d3e71888e1a9557
umadevimcw <umadevi@multicorewareinc.com>
2024-01-30 17:37:19 +0000

    #4215: Add channel support for argmax

--- tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_argmax.py
+++ tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_argmax.py
@@ -19,13 +19,13 @@ from tests.tt_eager.python_api_testing.sweep_tests import comparison_funcs
 )
 @pytest.mark.parametrize(
     "dim",
-    (3, 2, 0, -1, -2),
+    (3, 2, 1, 0, -1, -2, -3),
 )
 class TestArgmax:
     def test_argmax(self, input_shapes, dim, device):
         torch.manual_seed(0)
-        if input_shapes[0] * input_shapes[1] != 1:
-            pytest.skip(f"Dim 0, and 1 in {input_shapes} not supported for argmax.")
+        # if input_shapes[0] * input_shapes[1] != 1:
+        #     pytest.skip(f"Dim 0, and 1 in {input_shapes} not supported for argmax.")
         input_data = torch.randn(input_shapes).bfloat16()
         input_tensor = (
             tt_lib.tensor.Tensor(input_data, tt_lib.tensor.DataType.BFLOAT16).to(tt_lib.tensor.Layout.TILE).to(device)
@@ -39,9 +39,16 @@ class TestArgmax:
 
         else:
             golden_tensor = torch.argmax(input_data, dim=dim)
-            tt_out_tensor = tt_out_tensor[0, 0, 0]
-
+            if dim == 1 or dim == -3:
+                tt_out_tensor = tt_out_tensor[0]
+            else:
+                if input_shapes[1] != 1:
+                    tt_out_tensor = tt_out_tensor[0]
+                else:
+                    tt_out_tensor = tt_out_tensor[0, 0, 0]
         pt_out_tensor = golden_tensor
+        print(tt_out_tensor.shape)
+        print(pt_out_tensor.shape)
         comp_pass, comp_out = comparison_funcs.comp_pcc(pt_out_tensor, tt_out_tensor, pcc=0.99)
         comp_all, _ = comparison_funcs.comp_allclose(pt_out_tensor, tt_out_tensor, atol=4, rtol=1e-1)
         logger.info(comp_pass)
