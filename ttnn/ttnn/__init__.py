398b1863ea505b56fef85c592b8a3d3c9bb722e4
Aswinmcw <azayasankaran@tenstorrent.com>
2024-02-01 12:57:03 +0000

    #5057: Add relational ops to ttnn

--- ttnn/ttnn/__init__.py
+++ ttnn/ttnn/__init__.py
@@ -113,6 +113,15 @@ from ttnn.operations.binary import (
     multiply,
 )
 
+from ttnn.operations.relational import (
+    gtz,
+    ltz,
+    gez,
+    lez,
+    nez,
+    eqz,
+)
+
 from ttnn.operations.normalization import (
     layer_norm,
     rms_norm,
