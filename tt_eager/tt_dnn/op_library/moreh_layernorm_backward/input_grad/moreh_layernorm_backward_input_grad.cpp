923d847f0cd0c128007a33a9706ba20214149957
Olof Johansson <olofj@tenstorrent.com>
2024-02-02 14:15:56 -0800

    CoreCoord: Remove designated initializers
    Same as with CoreRange, these should ideally be moved to ctors but until
    then just use the ordered style initializer.
    
    Signed-off-by: Olof Johansson <olofj@tenstorrent.com>

--- tt_eager/tt_dnn/op_library/moreh_layernorm_backward/input_grad/moreh_layernorm_backward_input_grad.cpp
+++ tt_eager/tt_dnn/op_library/moreh_layernorm_backward/input_grad/moreh_layernorm_backward_input_grad.cpp
@@ -143,7 +143,7 @@ operation::ProgramWithCallbacks moreh_layernorm_backward_input_grad_impl(
     ////////////////////////////////////////////////////////////////////////////
     tt_metal::CoreGridDesc core_grid(device);
     const auto num_cores_y = core_grid.y_;
-    CoreCoord core_grid_coord = {.x = core_grid.x_, .y = num_cores_y};
+    CoreCoord core_grid_coord = {core_grid.x_, num_cores_y};
 
     const auto
         [num_cores_to_be_used,
