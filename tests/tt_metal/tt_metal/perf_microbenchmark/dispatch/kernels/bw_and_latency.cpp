cb9a61a01c0aab992d011a80c194a1bbdf59b9ca
Paul Keller <pkeller@tenstorrent.com>
2024-02-05 18:48:47 +0000

    #4620: Add FROM_ALL_DRAMs to bw test
    Plus ability to set the page size via a runtime arg or define

--- tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/kernels/bw_and_latency.cpp
+++ tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/kernels/bw_and_latency.cpp
@@ -3,24 +3,33 @@
 // SPDX-License-Identifier: Apache-2.0
 
 void kernel_main() {
+#ifdef PAGE_SIZE
+    uint32_t page_size = PAGE_SIZE;
+#else
+    uint32_t page_size = get_arg_val<uint32_t>(0);
+#endif
     cb_reserve_back(0, PAGE_COUNT);
     uint32_t cb_addr = get_write_ptr(0);
-    uint64_t noc_addr = NOC_XY_ADDR(NOC_X(NOC_ADDR_X), NOC_Y(NOC_ADDR_Y), NOC_MEM_ADDR);
     for (int i = 0; i < ITERATIONS; i++) {
         uint32_t read_ptr = cb_addr;
         for (int j = 0; j < PAGE_COUNT; j++) {
-#if READ_ONE_PACKET
-            noc_async_read_one_packet(noc_addr, read_ptr, PAGE_SIZE);
+#if DRAM_BANKED
+            uint64_t noc_addr = get_dram_noc_addr(j, page_size, 0);
 #else
-            noc_async_read(noc_addr, read_ptr, PAGE_SIZE);
+            uint64_t noc_addr = NOC_XY_ADDR(NOC_X(NOC_ADDR_X), NOC_Y(NOC_ADDR_Y), NOC_MEM_ADDR);
+#endif
+#if READ_ONE_PACKET
+            noc_async_read_one_packet(noc_addr, read_ptr, page_size);
+#else
+            noc_async_read(noc_addr, read_ptr, page_size);
 #endif
 #if LATENCY
             noc_async_read_barrier();
 #endif
-            read_ptr += PAGE_SIZE;
+            read_ptr += page_size;
         }
     }
-#if LATENCY
+#if !LATENCY
     noc_async_read_barrier();
 #endif
 }
