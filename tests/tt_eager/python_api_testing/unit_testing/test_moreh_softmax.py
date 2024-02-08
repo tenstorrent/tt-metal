8d4f18760a961f35708be17cda8cd1aa3b6c7cce
hschoi <hschoi4448@gmail.com>
2024-02-08 02:32:04 +0000

    #5198: add softmax callback test and fix softmax forward, backward callback

--- tests/tt_eager/python_api_testing/unit_testing/test_moreh_softmax.py
+++ tests/tt_eager/python_api_testing/unit_testing/test_moreh_softmax.py
@@ -403,3 +403,92 @@ def test_softmax_backward_for_dim_nc(shape_dim, device):
     passing, out = comp_allclose_and_pcc(x.grad, tt_dev, rtol=rtol, atol=atol)
     logger.info(out)
     assert passing
+
+
+@pytest.mark.parametrize(
+    "shape_dim_strategy",
+    (
+        ((1, 1, 32, 32), 3, ttl.operations.primary.MorehSoftmaxOpParallelizationStrategy.SMALL_W),
+        ((1, 1, 32, 32), 2, ttl.operations.primary.MorehSoftmaxOpParallelizationStrategy.SMALL_H),
+        ((1, 1, 32, 32), 3, ttl.operations.primary.MorehSoftmaxOpParallelizationStrategy.LARGE_W),
+        ((1, 1, 32, 32), 2, ttl.operations.primary.MorehSoftmaxOpParallelizationStrategy.LARGE_H),
+        ((1, 1, 32, 32), 1, ttl.operations.primary.MorehSoftmaxOpParallelizationStrategy.LARGE_C),
+        ((1, 1, 32, 32), 0, ttl.operations.primary.MorehSoftmaxOpParallelizationStrategy.LARGE_C),
+    ),
+)
+def test_softmax_callback(shape_dim_strategy, device):
+    ttl.program_cache.enable()
+
+    shape, dim, strategy = shape_dim_strategy
+    torch.manual_seed(0)
+
+    N = shape[0]
+    C = shape[1]
+    H = shape[2]
+    W = shape[3]
+
+    x = torch.randint(low=0, high=4, size=(N * C * H * W,)).reshape((N, C, H, W)).to(torch.bfloat16)
+
+    dev_x = ttl.tensor.Tensor(x, ttl.tensor.DataType.BFLOAT16).to(ttl.tensor.Layout.TILE).to(device)
+    dev_y = ttl.tensor.Tensor(x, ttl.tensor.DataType.BFLOAT16).to(ttl.tensor.Layout.TILE).to(device)
+
+    tt_cpu = torch.softmax(x, dim)
+    for i in range(2):
+        tt_npu = ttl.operations.primary.moreh_softmax(dev_x, dev_y, dim, strategy)
+
+    assert list(tt_npu.shape()) == list(tt_cpu.shape)
+    tt_dev = tt_npu.cpu().to(ttl.tensor.Layout.ROW_MAJOR).to_torch().to(torch.bfloat16)
+
+    rtol = atol = 0.05
+    passing, out = comp_allclose_and_pcc(tt_cpu, tt_dev, rtol=rtol, atol=atol)
+    logger.info(out)
+    assert passing
+
+
+@pytest.mark.parametrize(
+    "shape_dim_strategy",
+    (
+        ((1, 1, 32, 32), 3, ttl.operations.primary.MorehSoftmaxBackwardOpParallelizationStrategy.SMALL_W),
+        ((1, 1, 32, 32), 2, ttl.operations.primary.MorehSoftmaxBackwardOpParallelizationStrategy.SMALL_H),
+        ((1, 1, 32, 32), 3, ttl.operations.primary.MorehSoftmaxBackwardOpParallelizationStrategy.LARGE_W),
+        ((1, 1, 32, 32), 2, ttl.operations.primary.MorehSoftmaxBackwardOpParallelizationStrategy.LARGE_H),
+        ((1, 1, 32, 32), 1, ttl.operations.primary.MorehSoftmaxBackwardOpParallelizationStrategy.LARGE_C),
+        ((1, 1, 32, 32), 0, ttl.operations.primary.MorehSoftmaxBackwardOpParallelizationStrategy.LARGE_C),
+    ),
+)
+def test_softmax_backward_callback(shape_dim_strategy, device):
+    ttl.program_cache.enable()
+    shape, dim, strategy = shape_dim_strategy
+    torch.manual_seed(0)
+
+    N = shape[0]
+    C = shape[1]
+    H = shape[2]
+    W = shape[3]
+
+    x = (
+        torch.randint(low=0, high=4, size=(N * C * H * W,))
+        .reshape((N, C, H, W))
+        .to(torch.bfloat16)
+        .requires_grad_(True)
+    )
+
+    y = torch.softmax(x, dim)
+    dev_y = ttl.tensor.Tensor(y, ttl.tensor.DataType.BFLOAT16).to(ttl.tensor.Layout.TILE).to(device)
+
+    dy = torch.randint(low=0, high=4, size=(N * C * H * W,)).reshape((N, C, H, W)).to(torch.bfloat16)
+    dev_dy = ttl.tensor.Tensor(dy, ttl.tensor.DataType.BFLOAT16).to(ttl.tensor.Layout.TILE).to(device)
+
+    dev_dx = ttl.tensor.Tensor(dy, ttl.tensor.DataType.BFLOAT16).to(ttl.tensor.Layout.TILE).to(device)
+
+    y.backward(dy)
+    for i in range(2):
+        tt_npu = ttl.operations.primary.moreh_softmax_backward(dev_y, dev_dy, dev_dx, dim, strategy)
+
+    assert list(tt_npu.shape()) == list(x.grad.shape)
+    tt_dev = tt_npu.cpu().to(ttl.tensor.Layout.ROW_MAJOR).to_torch().to(torch.bfloat16)
+
+    rtol = atol = 0.05
+    passing, out = comp_allclose_and_pcc(x.grad, tt_dev, rtol=rtol, atol=atol)
+    logger.info(out)
+    assert passing
