99ef5127f8328535c49374744b03c34037434a5e
Nicholas Smith <nsmith@tenstorrent.com>
2024-02-09 14:49:20 +0000

    #0: Speedup incremental builds
    This change removed the use of phony targets and also refactored things
    in the python_env makefiles to avoid rebuilding the python env for
    incremental builds, which takes a significant amount of time.  Largely
    this involved ensuring that a file is produced for each target so that
    make can track the target's state.

--- tt_metal/python_env/module.mk
+++ tt_metal/python_env/module.mk
@@ -20,7 +20,11 @@ $(PYTHON_ENV)/.installed:
 $(PYTHON_ENV)/%: $(PYTHON_ENV)/.installed
 	bash -c "source $(PYTHON_ENV)/bin/activate"
 
-$(PYTHON_ENV)/.installed-dev: tt_eager/tt_lib/dev_install python_env tt_metal/python_env/requirements-dev.txt
+$(PYTHON_ENV)/.installed-dev: $(PYTHON_ENV)/.installed $(TT_LIB_LIB_LOCAL_SO) tt_metal/python_env/requirements-dev.txt
 	echo "Installing dev environment packages..."
 	bash -c "source $(PYTHON_ENV)/bin/activate && python -m pip install -r tt_metal/python_env/requirements-dev.txt"
+	echo "Installing editable dev version of tt_eager packages..."
+	bash -c "source $(PYTHON_ENV)/bin/activate && pip install -e ."
+	echo "Installing editable dev version of ttnn package..."
+	bash -c "source $(PYTHON_ENV)/bin/activate && pip install -e ttnn"
 	touch $@
--- ttnn/module.mk
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
-
-# SPDX-License-Identifier: Apache-2.0
-
-ttnn/dev_install: python_env/dev
-	echo "Installing editable dev version of ttnn package..."
-	bash -c "source $(PYTHON_ENV)/bin/activate && pip install -e ttnn"
