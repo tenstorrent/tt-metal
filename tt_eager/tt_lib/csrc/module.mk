784d613c29f373d815904b9087fe046fade4c48d
Nicholas Smith <nsmith@tenstorrent.com>
2024-02-09 14:49:20 +0000

    #0: Speedup incremental builds
    This change removed the use of phony targets and also refactored things
    in the python_env makefiles to avoid rebuilding the python env for
    incremental builds, which takes a significant amount of time.  Largely
    this involved ensuring that a file is produced for each target so that
    make can track the target's state.

--- tt_eager/tt_lib/csrc/module.mk
+++ tt_eager/tt_lib/csrc/module.mk
@@ -1,4 +1,5 @@
 TT_LIB_LIB = $(LIBDIR)/libtt_lib_csrc.so
+TT_LIB_LIB_LOCAL_SO = tt_eager/tt_lib/_C.so
 TT_LIB_DEFINES =
 TT_LIB_INCLUDES = $(TT_EAGER_INCLUDES) $(shell python3-config --includes) -Itt_metal/third_party/pybind11/include
 TT_LIB_LDFLAGS = -ltt_dnn -ldtx -ltensor -ltt_metal -lyaml-cpp $(LDFLAGS)
@@ -27,10 +28,8 @@ $(TT_LIB_LIB): $(TT_LIB_OBJS) $(TT_DNN_LIB) $(TENSOR_LIB) $(DTX_LIB) $(TT_METAL_
 	@mkdir -p $(LIBDIR)
 	$(CXX) $(TT_LIB_CFLAGS) $(CXXFLAGS) $(SHARED_LIB_FLAGS) -o $@ $(TT_LIB_OBJS) $(TT_LIB_LDFLAGS)
 
-.PHONY: tt_lib/csrc/setup_local_so
-tt_lib/csrc/setup_local_so: $(TT_LIB_LIB)
-	rm -f tt_eager/tt_lib/_C.so
-	cp $^ tt_eager/tt_lib/_C.so
+$(TT_LIB_LIB_LOCAL_SO): $(TT_LIB_LIB)
+	ln -sf $^ $@
 
 # Compile obj files
 $(OBJDIR)/tt_eager/tt_lib/csrc/%.o: tt_eager/tt_lib/csrc/%.cpp
