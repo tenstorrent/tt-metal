a0b4e722a25cbe5823666e6e7192a6126624ab03
kevinmi <kevinmi@tenstorrent.com>
2024-02-09 17:32:37 -0600

    #5151: Using Fused QKV matmul & Added Template Code for Splitting QKV heads on a single N300

--- models/demos/llama2_70b/tests/unit_tests/test_qkv_matmul_optimized_spilt_heads.py
+++ models/demos/llama2_70b/tests/unit_tests/test_qkv_matmul_optimized_spilt_heads.py
@@ -19,74 +19,6 @@ from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import (
 from models.utility_functions import torch2tt_tensor, tt2torch_tensor
 
 
-class TtLlamaQKV(torch.nn.Module):
-    def __init__(
-        self,
-        device,
-        state_dict,
-        base_url,
-        layer_num,
-        hidden_size: int,
-        model_config,
-        tt_cache_path,
-    ):
-        super().__init__()
-
-        self.state_dict = state_dict
-        self.device = device
-        self.hidden_size = hidden_size
-        self.model_config = model_config
-
-        layer_name = f"{base_url}.{layer_num}"
-
-        wq_str = f"{layer_name}.attention.wq.weight"
-        wk_str = f"{layer_name}.attention.wk.weight"
-        wv_str = f"{layer_name}.attention.wv.weight"
-
-        self.wq = torch2tt_tensor(
-            torch.transpose(
-                self.state_dict[wq_str],
-                -2,
-                -1,
-            ),
-            self.device,
-        )
-        self.wk = torch2tt_tensor(
-            torch.transpose(
-                self.state_dict[wk_str],
-                -2,
-                -1,
-            ),
-            self.device,
-        )
-        self.wv = torch2tt_tensor(
-            torch.transpose(
-                self.state_dict[wv_str],
-                -2,
-                -1,
-            ),
-            self.device,
-        )
-
-    def forward(self, x: tt_lib.tensor.Tensor) -> tt_lib.tensor.Tensor:
-        wq_out = tt_lib.tensor.matmul(
-            x,
-            self.wq,
-        )
-
-        wk_out = tt_lib.tensor.matmul(
-            x,
-            self.wk,
-        )
-
-        wv_out = tt_lib.tensor.matmul(
-            x,
-            self.wv,
-        )
-
-        return wq_out, wk_out, wv_out
-
-
 class TtLlamaQKV_optimized(torch.nn.Module):
     def __init__(
         self,
@@ -104,6 +36,9 @@ class TtLlamaQKV_optimized(torch.nn.Module):
         self.device = device
         self.hidden_size = hidden_size
         self.model_config = model_config
+
+        self.n_heads = 64
+        self.n_kv_heads = 8
 
         layer_name = f"{base_url}.{layer_num}"
 
@@ -167,53 +102,38 @@ class TtLlamaQKV_optimized(torch.nn.Module):
         )
 
         x.deallocate(True)
-        return fused_qkv_out
+        if self.model_config["FUSED_QKV_MM_OUTPUT_MEMCFG"] != self.model_config["CREATE_QKV_HEADS_INPUT_MEMCFG"]:
+            fused_qkv_out = tt_lib.tensor.sharded_to_interleaved(
+                fused_qkv_out, output_mem_config=self.model_config["DEFAULT_MEMCFG"]
+            )
+            fused_qkv_out = tt_lib.tensor.interleaved_to_sharded(
+                fused_qkv_out, sharded_mem_config=self.model_config["CREATE_QKV_HEADS_INPUT_MEMCFG"]
+            )
 
-    # def forward(self, x: tt_lib.tensor.Tensor) -> tt_lib.tensor.Tensor:
-    #     print(f"x shape:{x.shape()}")
+        print(self.device.compute_with_storage_grid_size())
+        (
+            q_heads,  # [seqlen, n_heads, bsz, head_dim]
+            k_heads,  # [seqlen, n_kv_heads, bsz, head_dim]
+            v_heads,  # [seqlen, n_kv_heads, bsz, head_dim]
+        ) = tt_lib.tensor.nlp_create_qkv_heads(
+            fused_qkv_out,
+            num_heads=64,
+            num_kv_heads=8,
+            transpose_k_heads=False,
+            output_mem_config=self.model_config["CREATE_QKV_HEADS_OUTPUT_MEMCFG"],
+        )
+        fused_qkv_out.deallocate(True)
 
-    #     x = tt_lib.tensor.interleaved_to_sharded(x, sharded_mem_config=self.model_config["QKV_MM_INPUT_MEMCFG"])
-    #     # wk = (8192, 1024)
-    #     # wv = (8192, 1024)
-    #     # wq = (8192, 8192)
-    #     # x = (1, 1, 32, 8192)
-    #     # wq_out = (1, 1, 32, 8192)
-    #     # wk_out = (1, 1, 32, 1024)
-    #     # wv_out = (1, 1, 32, 1024)
-
-    #     wq_out = tt_lib.operations.primary.matmul_1d(
-    #         x,
-    #         self.wq,
-    #         program_config=self.model_config["WQ_MM_PROGCFG"],
-    #         output_mem_config=self.model_config["WQ_MM_MEMCFG"],
-    #         output_dtype=self.model_config["FUSED_QKV_MM_OUTPUT_DTYPE"],
-    #     )
-
-    #     wk_out = tt_lib.operations.primary.matmul_1d(
-    #         x,
-    #         self.wk,
-    #         program_config=self.model_config["WK_MM_PROGCFG"],
-    #         output_mem_config=self.model_config["WK_MM_MEMCFG"],
-    #         output_dtype=self.model_config["FUSED_QKV_MM_OUTPUT_DTYPE"],
-    #     )
-
-    #     wv_out = tt_lib.operations.primary.matmul_1d(
-    #         x,
-    #         self.wv,
-    #         program_config=self.model_config["WK_MM_PROGCFG"],
-    #         output_mem_config=self.model_config["WK_MM_MEMCFG"],
-    #         output_dtype=self.model_config["FUSED_QKV_MM_OUTPUT_DTYPE"],
-    #     )
-
-    #     x.deallocate(True)
-    #     return wq_out, wk_out, wv_out
+        return q_heads, k_heads, v_heads
 
 
 class PytorchLlamaQKVModel(torch.nn.Module):
     def __init__(self, hf_reference_model, layer_num):
         super().__init__()
         self.attn = hf_reference_model.layers[layer_num].attention
-
+        self.n_heads = 64
+        self.kv_heads = 8
+        self.head_dim = 128
         # Disable dropout
         self.attn.eval()
 
@@ -221,6 +141,17 @@ class PytorchLlamaQKVModel(torch.nn.Module):
         xq = self.attn.wq(x)
         xk = self.attn.wk(x)
         xv = self.attn.wv(x)
+
+        batch_size = 32
+
+        xq = xq.view(1, batch_size, self.n_heads, self.head_dim)
+        xk = xk.view(1, batch_size, self.kv_heads, self.head_dim)
+        xv = xv.view(1, batch_size, self.kv_heads, self.head_dim)
+
+        xq = xq.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
+        xk = xk.transpose(1, 2)  # (bs, n_local_heads, cache_len + seqlen, head_dim)
+        xv = xv.transpose(1, 2)  # (bs, n_local_heads, cache_len + seqlen, head_dim)
+
         return xq, xk, xv
 
 
@@ -264,49 +195,29 @@ def run_test_LlamaQKV(
     pytorch_out = pytorch_LlamaQKV_model(inp)
 
     # TT hardware execution -------------------------------------------------------------
-    qkv_optimized = True
-    fused_qkv = True
 
-    if qkv_optimized:
-        tt_LlamaQKV_model = TtLlamaQKV_optimized(
-            device,
-            state_dict,
-            base_url,
-            layer_num,
-            configuration.dim,
-            model_config,
-            tt_cache_path=None,
-        )
-    else:
-        tt_LlamaQKV_model = TtLlamaQKV(
-            device,
-            state_dict,
-            base_url,
-            layer_num,
-            configuration.dim,
-            model_config,
-            tt_cache_path=None,
-        )
+    tt_LlamaQKV_model = TtLlamaQKV_optimized(
+        device,
+        state_dict,
+        base_url,
+        layer_num,
+        configuration.dim,
+        model_config,
+        tt_cache_path=None,
+    )
 
     tt_inp = torch2tt_tensor(inp, device)
     tt_out = tt_LlamaQKV_model(tt_inp)
-    if fused_qkv:
-        pytorch_out = torch.concat([pytorch_out[0], pytorch_out[1], pytorch_out[2]], dim=-1)
-        tt_out = tt2torch_tensor(tt_out)
-        logger.info(comp_allclose(pytorch_out, tt_out))
-        does_pass, output_pcc = comp_pcc(pytorch_out, tt_out, pcc)
-        logger.info(f"PCC value: {output_pcc}")
-    else:
-        tt_out = [tt2torch_tensor(tt_out_tensor) for tt_out_tensor in tt_out]
-        # check outputs ----------------------------------------------------------------------
-        for i in range(3):
-            logger.info(comp_allclose(pytorch_out[i], tt_out[i]))
+    tt_out = [tt2torch_tensor(tt_out_tensor) for tt_out_tensor in tt_out]
+    # check outputs ----------------------------------------------------------------------
+    for i in range(3):
+        logger.info(comp_allclose(pytorch_out[i], tt_out[i]))
 
-        does_pass = True
-        for i in range(3):
-            out_pass, output_pcc = comp_pcc(pytorch_out[i], tt_out[i], pcc)
-            logger.info(f"PCC value: {output_pcc}")
-            does_pass = does_pass and out_pass
+    does_pass = True
+    for i in range(3):
+        out_pass, output_pcc = comp_pcc(pytorch_out[i], tt_out[i], pcc)
+        logger.info(f"PCC value: {output_pcc}")
+        does_pass = does_pass and out_pass
 
     if does_pass:
         logger.info("Llama QKV output Passed!")
