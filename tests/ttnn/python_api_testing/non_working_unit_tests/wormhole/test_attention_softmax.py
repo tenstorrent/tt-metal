5a8275ffbd94ebd9bad20446ce1193eb39a60df7
Nenad Petrovic <npetrovic@tenstorrent.com>
2024-02-12 13:39:13 +0000

    #5298: Added unit test

---
+++ tests/ttnn/python_api_testing/non_working_unit_tests/wormhole/test_attention_softmax.py
@@ -0,0 +1,72 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from loguru import logger
+import random
+import pytest
+import torch
+import ttnn
+
+from tests.ttnn.python_api_testing.sweep_tests import ttnn_ops
+from tests.tt_eager.python_api_testing.sweep_tests import pytorch_ops
+
+
+def run_softmax_tests(
+    input_shape,
+    dtype,
+    dlayout,
+    in_mem_config,
+    output_mem_config,
+    data_seed,
+    scalar,
+    device,
+):
+    torch.manual_seed(data_seed)
+
+    x = torch.Tensor(size=input_shape).uniform_(-1, 1).to(torch.bfloat16)
+    y = torch.Tensor(size=input_shape).uniform_(-1, 1).to(torch.bfloat16)
+    y[y <= 0.50] = 0
+    y[y > 0.50] = 1
+    if scalar < 0:
+        scalar = -scalar
+
+    try:
+        # get ref result
+        ref_value = pytorch_ops.attention_softmax(x, y, scalar)
+        x = ttnn_ops.torch_to_ttnn(x, device, dlayout[0], in_mem_config, dtype[0])
+        y = ttnn_ops.torch_to_ttnn(y, device, dlayout[0], in_mem_config, dtype[0])
+
+        tt_result = ttnn_ops.attention_softmax(x, y, scalar)
+
+        tt_result = ttnn_ops.ttnn_tensor_to_torch(tt_result, output_mem_config)
+
+    except Exception as e:
+        logger.warning(f"Test execution crashed: {e}")
+        print(traceback.format_exc())
+        raise e
+
+    assert len(tt_result.shape) == len(ref_value.shape)
+    assert tt_result.shape == ref_value.shape
+    assert_with_pcc(ref_value, tt_result, 0.99)
+
+
+test_sweep_args = [
+    (
+        [(1, 1, 224, 32), (1, 1, 32, 32)],
+        [ttnn.bfloat16],
+        [ttnn.ROW_MAJOR_LAYOUT],
+        (ttnn.DRAM_MEMORY_CONFIG),
+        (ttnn.DRAM_MEMORY_CONFIG),
+        18139523,
+        -51.5,
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, scalar",
+    (test_sweep_args),
+)
+def test_softmax(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, scalar, device):
+    run_softmax_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, scalar, device)
