1d52b6484286f5a0e90486f1ca3d685db3a84bbb
Nicholas Smith <nsmith@tenstorrent.com>
2024-02-12 22:47:55 +0000

    #5323: Convolutions of small size fail during parallelization calculations
    Height Sharded fails because the code assumes the parallelization to be
    at least 12 cores and then special-cases the overflow if it's not a
    multiple of 12. But if the total number of par cores is < 12 to begin
    with the code still assumes at least 12 cores and we end up with
    rounding errors. The fix is to special case par for < 12 cores.

--- tt_eager/tt_dnn/op_library/sliding_window_op_infra/tt_py_untilize_with_halo.py
+++ tt_eager/tt_dnn/op_library/sliding_window_op_infra/tt_py_untilize_with_halo.py
@@ -161,23 +161,29 @@ class TTPyUntilizeWithHalo(TTPyOp):
             block_sharding = num_cores_nhw == num_cores_w
             if not block_sharding:
                 assert num_cores_w == 12
-                num_cores_height_excluding_remainder_last_row = num_cores_nhw // num_cores_w
-                assert num_cores_h >= num_cores_height_excluding_remainder_last_row
-                core_range_1 = ttl.tensor.CoreRange(
-                    ttl.tensor.CoreCoord(0, 0),
-                    ttl.tensor.CoreCoord(num_cores_w - 1, num_cores_height_excluding_remainder_last_row - 1),
-                )
-                num_cores_last = num_cores_nhw % num_cores_w
-                core_range_2 = None
-                if num_cores_last > 0:
-                    assert num_cores_h == num_cores_height_excluding_remainder_last_row + 1
-                    core_range_2 = ttl.tensor.CoreRange(
-                        ttl.tensor.CoreCoord(0, num_cores_height_excluding_remainder_last_row),
-                        ttl.tensor.CoreCoord(num_cores_last - 1, num_cores_height_excluding_remainder_last_row),
+                if num_cores_nhw >= num_cores_w:
+                    num_cores_height_excluding_remainder_last_row = num_cores_nhw // num_cores_w
+                    assert num_cores_h >= num_cores_height_excluding_remainder_last_row
+                    core_range_1 = ttl.tensor.CoreRange(
+                        ttl.tensor.CoreCoord(0, 0),
+                        ttl.tensor.CoreCoord(num_cores_w - 1, num_cores_height_excluding_remainder_last_row - 1),
                     )
-                    shard_grid = ttl.tensor.CoreRangeSet({core_range_1, core_range_2})
+                    num_cores_last = num_cores_nhw % num_cores_w
+                    if num_cores_last > 0:
+                        assert num_cores_h == num_cores_height_excluding_remainder_last_row + 1
+                        core_range_2 = ttl.tensor.CoreRange(
+                            ttl.tensor.CoreCoord(0, num_cores_height_excluding_remainder_last_row),
+                            ttl.tensor.CoreCoord(num_cores_last - 1, num_cores_height_excluding_remainder_last_row),
+                        )
+                        shard_grid = ttl.tensor.CoreRangeSet({core_range_1, core_range_2})
+                    else:
+                        assert num_cores_h == num_cores_height_excluding_remainder_last_row
+                        shard_grid = ttl.tensor.CoreRangeSet({core_range_1})
                 else:
-                    assert num_cores_h == num_cores_height_excluding_remainder_last_row
+                    core_range_1 = ttl.tensor.CoreRange(
+                        ttl.tensor.CoreCoord(0, 0),
+                        ttl.tensor.CoreCoord(num_cores_nhw - 1, 0),
+                    )
                     shard_grid = ttl.tensor.CoreRangeSet({core_range_1})
             else:
                 core_range = ttl.tensor.CoreRange(
