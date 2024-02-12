50cea86dac767d1d0c5089837a8f6baf268c827d
Nenad Petrovic <npetrovic@tenstorrent.com>
2024-02-12 08:24:46 +0000

    #5300: New WH clamp max,min unit tests

---
+++ tests/tt_eager/python_api_testing/non_working_unit_tests/wormhole/test_clamp_max_bw.py
@@ -0,0 +1,83 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import random
+from loguru import logger
+import pytest
+import torch
+import tt_lib as ttl
+
+
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc
+from tests.tt_eager.python_api_testing.sweep_tests import tt_lib_ops, pytorch_ops
+from models.utility_functions import tt2torch_tensor
+
+
+def run_clamp_bw(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, scalar, device):
+    random.seed(0)
+    torch.manual_seed(data_seed)
+
+    x = torch.Tensor(size=input_shape).uniform_(-100, 100)
+    y = torch.Tensor(size=input_shape).uniform_(-100, 100)
+
+    ref_value = pytorch_ops.clamp_max_bw(x, y, scalar)
+
+    tt_result = tt_lib_ops.clamp_max_bw(
+        x=x,
+        y=y,
+        scalar=scalar,
+        device=device,
+        dtype=dtype,
+        layout=dlayout,
+        input_mem_config=in_mem_config,
+        output_mem_config=out_mem_config,
+    )
+
+    # compare tt and golden outputs
+    success, pcc_value = comp_pcc(ref_value, tt_result)
+    logger.debug(pcc_value)
+
+    assert success
+
+
+test_sweep_args = [
+    (
+        (6, 2, 224, 192),
+        [
+            ttl.tensor.DataType.BFLOAT16,
+            ttl.tensor.DataType.BFLOAT16,
+        ],
+        [ttl.tensor.Layout.TILE, ttl.tensor.Layout.TILE],
+        [
+            ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+            None,
+        ],
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1),
+        17493725,
+        -88.5,
+    ),
+    (
+        (4, 7, 32, 96),
+        [
+            ttl.tensor.DataType.BFLOAT16,
+            ttl.tensor.DataType.BFLOAT16,
+        ],
+        [ttl.tensor.Layout.TILE, ttl.tensor.Layout.TILE],
+        [
+            None,
+            None,
+        ],
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        16934480,
+        -94.0,
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "input_shape, dtype, dlayout, in_mem_config, output_mem_config, data_seed, scalar",
+    (test_sweep_args),
+)
+def test_clamp_bw_test(input_shape, dtype, dlayout, in_mem_config, output_mem_config, data_seed, scalar, device):
+    run_clamp_bw(input_shape, dtype, dlayout, in_mem_config, output_mem_config, data_seed, scalar, device)
