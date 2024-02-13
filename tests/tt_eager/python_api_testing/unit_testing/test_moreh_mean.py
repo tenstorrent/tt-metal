5c82291a063ab0f0c8d57dcd56c61b1cc0e49802
Dongjin Na <dongjin.na@moreh.io>
2024-02-13 17:11:58 +0000

    #4742: Update unit tests to use int and float tensor for mean and mean backward op

--- tests/tt_eager/python_api_testing/unit_testing/test_moreh_mean.py
+++ tests/tt_eager/python_api_testing/unit_testing/test_moreh_mean.py
@@ -7,35 +7,45 @@ import torch
 from loguru import logger
 
 import tt_lib as ttl
-from models.utility_functions import comp_allclose_and_pcc, skip_for_wormhole_b0
+from models.utility_functions import comp_allclose_and_pcc, comp_pcc
 
 TILE_HEIGHT = 32
 TILE_WIDTH = 32
 
 
-def get_tensors(input_shape, output_shape, device):
+def get_tensors(input_shape, output_shape, use_randint, device):
     npu_dtype = ttl.tensor.DataType.BFLOAT16
-    cpu_dtype = torch.bfloat16
     npu_layout = ttl.tensor.Layout.TILE
+    cpu_dtype = torch.float
 
-    # create tensors for forward
-    torch_input = torch.randint(-2, 3, input_shape, dtype=cpu_dtype, requires_grad=True)
-    torch_output = torch.randint(-2, 3, output_shape, dtype=cpu_dtype)
+    if use_randint:
+        torch_input = torch.randint(-2, 3, input_shape, dtype=cpu_dtype)
+        torch_output = torch.randint(-2, 3, output_shape, dtype=cpu_dtype)
+    else:
+        torch_input = torch.rand(input_shape, dtype=cpu_dtype) * 200 - 100
+        torch_output = torch.rand(output_shape, dtype=cpu_dtype) * 200 - 100
 
+    torch_input = torch_input.bfloat16().requires_grad_()
+    torch_output = torch_output.bfloat16()
     tt_input = ttl.tensor.Tensor(torch_input, npu_dtype).pad_to_tile(float("nan")).to(npu_layout).to(device)
     tt_output = ttl.tensor.Tensor(torch_output, npu_dtype).pad_to_tile(float("nan")).to(npu_layout).to(device)
-
     return tt_input, tt_output, torch_input
 
 
-def get_backward_tensors(output_grad_shape, input_grad_shape, device):
+def get_backward_tensors(output_grad_shape, input_grad_shape, use_randint, device):
     npu_dtype = ttl.tensor.DataType.BFLOAT16
-    cpu_dtype = torch.bfloat16
     npu_layout = ttl.tensor.Layout.TILE
+    cpu_dtype = torch.float
 
-    torch_output_grad = torch.randint(-2, 3, output_grad_shape, dtype=cpu_dtype)
-    torch_input_grad = torch.randint(-2, 3, input_grad_shape, dtype=cpu_dtype)
+    if use_randint:
+        torch_input_grad = torch.randint(-2, 3, input_grad_shape, dtype=cpu_dtype)
+        torch_output_grad = torch.randint(-2, 3, output_grad_shape, dtype=cpu_dtype)
+    else:
+        torch_input_grad = torch.rand(input_grad_shape, dtype=cpu_dtype) * 200 - 100
+        torch_output_grad = torch.rand(output_grad_shape, dtype=cpu_dtype) * 200 - 100
 
+    torch_input_grad = torch_input_grad.bfloat16()
+    torch_output_grad = torch_output_grad.bfloat16()
     tt_output_grad = ttl.tensor.Tensor(torch_output_grad, npu_dtype).pad_to_tile(float("nan")).to(npu_layout).to(device)
     tt_input_grad = ttl.tensor.Tensor(torch_input_grad, npu_dtype).pad_to_tile(float("nan")).to(npu_layout).to(device)
 
@@ -76,14 +86,19 @@ def get_backward_tensors(output_grad_shape, input_grad_shape, device):
     ),
     ids=["0", "0,1", "0,1,2", "0,1,2,3", "0,1,3", "0,2,3", "1", "1,2", "1,2,3", "1,3", "2", "2,3", "3"],
 )
-def test_moreh_mean_dims(input_shape, dims, device):
+@pytest.mark.parametrize(
+    "use_randint",
+    (True, False),
+    ids=["True", "False"],
+)
+def test_moreh_mean_dims(input_shape, dims, use_randint, device):
     torch.manual_seed(2023)
     output_shape = input_shape.copy()
 
     for dim in dims:
         output_shape[dim] = 1
 
-    (tt_input, tt_output, torch_input) = get_tensors(input_shape, output_shape, device)
+    (tt_input, tt_output, torch_input) = get_tensors(input_shape, output_shape, use_randint, device)
 
     torch_output = torch.mean(torch_input, dims, True)
 
@@ -98,7 +113,10 @@ def test_moreh_mean_dims(input_shape, dims, device):
 
     # test for equivalance
     rtol = atol = 0.1
-    passing, output_pcc = comp_allclose_and_pcc(torch_output, tt_output_cpu, pcc=0.999, rtol=rtol, atol=atol)
+    if use_randint:
+        passing, output_pcc = comp_allclose_and_pcc(torch_output, tt_output_cpu, pcc=0.999, rtol=rtol, atol=atol)
+    else:
+        passing, output_pcc = comp_pcc(torch_output, tt_output_cpu, pcc=0.999)
 
     logger.info(f"Out passing={passing}")
     logger.info(f"Output pcc={output_pcc}")
@@ -140,15 +158,22 @@ def test_moreh_mean_dims(input_shape, dims, device):
     ),
     ids=["0", "0,1", "0,1,2", "0,1,2,3", "0,1,3", "0,2,3", "1", "1,2", "1,2,3", "1,3", "2", "2,3", "3"],
 )
-def test_moreh_mean_backward(input_shape, dims, device):
+@pytest.mark.parametrize(
+    "use_randint",
+    (True, False),
+    ids=["True", "False"],
+)
+def test_moreh_mean_backward(input_shape, dims, use_randint, device):
     torch.manual_seed(2023)
     output_shape = input_shape.copy()
 
     for dim in dims:
         output_shape[dim] = 1
 
-    (_, _, torch_input) = get_tensors(input_shape, output_shape, device)
-    (tt_output_grad, tt_input_grad, torch_output_grad) = get_backward_tensors(output_shape, input_shape, device)
+    (_, _, torch_input) = get_tensors(input_shape, output_shape, use_randint, device)
+    (tt_output_grad, tt_input_grad, torch_output_grad) = get_backward_tensors(
+        output_shape, input_shape, use_randint, device
+    )
 
     torch_output = torch.mean(torch_input, dims, True)
     torch_output.backward(torch_output_grad)
@@ -164,7 +189,12 @@ def test_moreh_mean_backward(input_shape, dims, device):
 
     # test for equivalance
     rtol = atol = 0.1
-    passing, output_pcc = comp_allclose_and_pcc(torch_input.grad, tt_input_grad_cpu, pcc=0.999, rtol=rtol, atol=atol)
+    if use_randint:
+        passing, output_pcc = comp_allclose_and_pcc(
+            torch_input.grad, tt_input_grad_cpu, pcc=0.999, rtol=rtol, atol=atol
+        )
+    else:
+        passing, output_pcc = comp_pcc(torch_input.grad, tt_input_grad_cpu, pcc=0.999)
 
     logger.info(f"Out passing={passing}")
     logger.info(f"Output pcc={output_pcc}")
