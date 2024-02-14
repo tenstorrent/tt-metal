8f73fa1607e223ca79df896231581e35cf07d685
acejkov <acejkov@tenstorrent.com>
2024-02-14 20:55:01 +0000

    #5258: Enable test_resnet50_first_conv unit test to run on wh_b0

--- tt_metal/hw/ckernels/grayskull/metal/llk_api/llk_math_unary_datacopy_api.h
+++ tt_metal/hw/ckernels/grayskull/metal/llk_api/llk_math_unary_datacopy_api.h
@@ -21,7 +21,7 @@ inline void llk_math_eltwise_unary_datacopy(uint dst_index) {
 }
 
 template <DataCopyType type, BroadcastType src_b_bcast_type = BroadcastType::NONE, DstSync Dst = DstSync::SyncFull, bool is_fp32_dest_acc_en = false>
-inline void llk_math_eltwise_unary_datacopy_block(uint start_dst_index, uint ntiles) {
+inline void llk_math_eltwise_unary_datacopy_block(uint start_dst_index, uint ntiles, uint operand = 0 /*not used*/) {
 
     for (uint32_t dst_index = start_dst_index; dst_index < start_dst_index + ntiles; dst_index++) {
         llk_math_eltwise_unary_datacopy<type, src_b_bcast_type, Dst>(dst_index);
