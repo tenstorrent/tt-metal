b38c72a4703c2d7320c3512098c8039473f83cf6
Abhinav <asarje@tenstorrent.com>
2024-02-16 02:44:00 +0000

    #0: testing parallel config from conv

--- tests/ttnn/integration_tests/unet/test_ttnn_functional_unet.py
+++ tests/ttnn/integration_tests/unet/test_ttnn_functional_unet.py
@@ -155,17 +155,17 @@ output_tensor = model(input_tensor)
 print("\n\n\n")
 print("output_tensor size is: ", output_tensor.size())
 print("\n\n\n")
-model_graph = draw_graph(
-    model,
-    # input_size=(1, 3, 1056, 160),
-    input_size=(2, 3, 1056, 160),
-    dtypes=[torch.float32],
-    expand_nested=True,
-    graph_name="unetSeqEdit2",
-    depth=2,
-    directory=".",
-)
-model_graph.visual_graph.render(format="pdf")
+# model_graph = draw_graph(
+#     model,
+#     # input_size=(1, 3, 1056, 160),
+#     input_size=(2, 3, 1056, 160),
+#     dtypes=[torch.float32],
+#     expand_nested=True,
+#     graph_name="unetSeqEdit2",
+#     depth=2,
+#     directory=".",
+# )
+# model_graph.visual_graph.render(format="pdf")
 
 device_id = 0
 device = ttnn.open(device_id)
