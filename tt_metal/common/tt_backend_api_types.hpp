aeba0fd0c42a35ac7f917b48f22572f8457af342
Milos <mtrajkovic@tenstorrent.com>
2024-02-17 05:40:47 +0000

    #4624: Fix Int32 reference

--- tt_metal/common/tt_backend_api_types.hpp
+++ tt_metal/common/tt_backend_api_types.hpp
@@ -47,7 +47,7 @@ enum class DataFormat : uint8_t {
  */
 inline bool is_integer_format(DataFormat format) {
     return (
-        (format == DataFormat::Int32) || (format == DataFormat::Int8) ||
+        (format == DataFormat::UInt32) || (format == DataFormat::Int8) ||
         (format == DataFormat::UInt16) || (format == DataFormat::UInt8) ||
         (format == DataFormat::RawUInt32) || (format == DataFormat::RawUInt16) || (format == DataFormat::RawUInt8));
 }
