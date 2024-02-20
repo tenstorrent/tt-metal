44f40aadc686e3ea82f7bd0ff745afc64f0ce522
Dalar Vartanians <dvartanians@tenstorrent.com>
2024-02-20 18:05:12 +0000

    #5016: remove commented code

--- models/experimental/functional_unet/tt/ttnn_functional_unet_maxpoolFallback.py
+++ models/experimental/functional_unet/tt/ttnn_functional_unet_maxpoolFallback.py
@@ -58,39 +58,6 @@ class UNet:
 
         output_tensor = torch.nn.functional.max_pool2d(output_tensor, kernel_size=2, stride=2)
 
-        """
-        # fall back maxpool to cpu
-        pt_nn = torch.nn.MaxPool2d(kernel_size=2, stride=2)
-        # pt_out = pt_nn(output_tensor)
-        t0 = ttl.tensor.Tensor(
-            output_tensor.reshape(-1).tolist(),
-            output_tensor.shape,
-            ttl.tensor.DataType.BFLOAT16,
-            ttl.tensor.Layout.ROW_MAJOR,
-        )
-        device_id = 0
-        device = ttnn.open(device_id)
-        kernel_size = 2
-        stride = 2
-        padding = 0
-        dilation = 1
-        return_indices = False
-        ceil_mode = False
-        t0 = t0.to(device)
-        tt_nn = ttl.fallback_ops.MaxPool2d(
-            kernel_size,
-            stride,
-            padding,
-            dilation,
-            return_indices,
-            ceil_mode,
-        )
-        t1 = tt_nn(t0)
-
-        output_tensor = t1.cpu().to(ttl.tensor.Layout.ROW_MAJOR).to_torch()
-
-        """
-
         output_tensor = torch.permute(output_tensor, (0, 2, 3, 1))
         output_tensor = ttnn.from_torch(output_tensor, dtype=ttnn.bfloat16)
         output_tensor = self.c2.copy_input_to_device(output_tensor)
@@ -102,39 +69,6 @@ class UNet:
         output_tensor = output_tensor.to(torch_input_tensor.dtype)
 
         output_tensor = torch.nn.functional.max_pool2d(output_tensor, kernel_size=2, stride=2)
-
-        """
-        # fall back maxpool to cpu
-        pt_nn = torch.nn.MaxPool2d(kernel_size=2, stride=2)
-        # pt_out = pt_nn(output_tensor)
-        t0 = ttl.tensor.Tensor(
-            output_tensor.reshape(-1).tolist(),
-            output_tensor.shape,
-            ttl.tensor.DataType.BFLOAT16,
-            ttl.tensor.Layout.ROW_MAJOR,
-        )
-        device_id = 0
-        device = ttnn.open(device_id)
-        kernel_size = 2
-        stride = 2
-        padding = 0
-        dilation = 1
-        return_indices = False
-        ceil_mode = False
-        t0 = t0.to(device)
-
-        tt_nn = ttl.fallback_ops.MaxPool2d(
-            kernel_size,
-            stride,
-            padding,
-            dilation,
-            return_indices,
-            ceil_mode,
-        )
-        t1 = tt_nn(t0)
-
-        output_tensor = t1.cpu().to(ttl.tensor.Layout.ROW_MAJOR).to_torch()
-        """
 
         output_tensor = torch.permute(output_tensor, (0, 2, 3, 1))
         output_tensor = ttnn.from_torch(output_tensor, dtype=ttnn.bfloat16)
@@ -148,38 +82,6 @@ class UNet:
 
         output_tensor = torch.nn.functional.max_pool2d(output_tensor, kernel_size=2, stride=2)
 
-        """
-        # fall back maxpool to cpu
-        pt_nn = torch.nn.MaxPool2d(kernel_size=2, stride=2)
-        # pt_out = pt_nn(output_tensor)
-        t0 = ttl.tensor.Tensor(
-            output_tensor.reshape(-1).tolist(),
-            output_tensor.shape,
-            ttl.tensor.DataType.BFLOAT16,
-            ttl.tensor.Layout.ROW_MAJOR,
-        )
-        device_id = 0
-        device = ttnn.open(device_id)
-        kernel_size = 2
-        stride = 2
-        padding = 0
-        dilation = 1
-        return_indices = False
-        ceil_mode = False
-        t0 = t0.to(device)
-        tt_nn = ttl.fallback_ops.MaxPool2d(
-            kernel_size,
-            stride,
-            padding,
-            dilation,
-            return_indices,
-            ceil_mode,
-        )
-        t1 = tt_nn(t0)
-
-        output_tensor = t1.cpu().to(ttl.tensor.Layout.ROW_MAJOR).to_torch()
-        """
-
         output_tensor = torch.permute(output_tensor, (0, 2, 3, 1))
         output_tensor = ttnn.from_torch(output_tensor, dtype=ttnn.bfloat16)
         output_tensor = self.c4.copy_input_to_device(output_tensor)
@@ -191,37 +93,6 @@ class UNet:
         output_tensor = output_tensor.to(torch_input_tensor.dtype)
 
         output_tensor = torch.nn.functional.max_pool2d(output_tensor, kernel_size=2, stride=2)
-        """
-        # fall back maxpool to cpu
-        pt_nn = torch.nn.MaxPool2d(kernel_size=2, stride=2)
-        # pt_out = pt_nn(output_tensor)
-        t0 = ttl.tensor.Tensor(
-            output_tensor.reshape(-1).tolist(),
-            output_tensor.shape,
-            ttl.tensor.DataType.BFLOAT16,
-            ttl.tensor.Layout.ROW_MAJOR,
-        )
-        device_id = 0
-        device = ttnn.open(device_id)
-        kernel_size = 2
-        stride = 2
-        padding = 0
-        dilation = 1
-        return_indices = False
-        ceil_mode = False
-        t0 = t0.to(device)
-        tt_nn = ttl.fallback_ops.MaxPool2d(
-            kernel_size,
-            stride,
-            padding,
-            dilation,
-            return_indices,
-            ceil_mode,
-        )
-        t1 = tt_nn(t0)
-
-        output_tensor = t1.cpu().to(ttl.tensor.Layout.ROW_MAJOR).to_torch()
-        """
         output_tensor = torch.permute(output_tensor, (0, 2, 3, 1))
         output_tensor = ttnn.from_torch(output_tensor, dtype=ttnn.bfloat16)
         output_tensor = self.bnc.copy_input_to_device(output_tensor)
@@ -232,7 +103,4 @@ class UNet:
         output_tensor = torch.permute(output_tensor, (0, 3, 1, 2))
         output_tensor = output_tensor.to(torch_input_tensor.dtype)
 
-        # output_tensor = torch.nn.functional.max_pool2d(output_tensor, kernel_size=2, stride=2)
-        #        import pdb
-        #        pdb.set_trace()
         return output_tensor
