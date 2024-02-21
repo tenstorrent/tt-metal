433ec51601903a848407b67714881e80dde19382
umadevimcw <umadevi@multicorewareinc.com>
2024-02-21 10:36:45 +0000

    #5510: #5511: Add test files

--- tt_eager/tt_lib/csrc/tt_lib_bindings_tensor_backward_ops.cpp
+++ tt_eager/tt_lib/csrc/tt_lib_bindings_tensor_backward_ops.cpp
@@ -742,6 +742,14 @@ namespace tt::tt_metal::detail{
     m_tensor.def("hypot_bw", &tt::tt_metal::hypot_bw,
             py::arg("grad").noconvert(), py::arg("input").noconvert(), py::arg("other").noconvert(), py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, R"doc(
             Performs backward operations for hypotenuse of ``input`` and ``other`` tensors with given ``grad``.
+
+            Input tensors must have BFLOAT16 data type.
+
+            Output tensors will have BFLOAT16 data type.
+
+            .. csv-table::
+                :header: "Argument", "Description", "Data type", "Valid range", "Required"
+
                 "input", "First input tensor ", "Tensor", "Tensor of shape [W, Z, Y, X]", "Yes"
                 "other", "Second input Tensor", "Tensor", "Tensor of shape [W, Z, Y, X]", "Yes"
                 "output_mem_config", "Layout of tensor in TT Accelerator device memory banks", "MemoryConfig", "Default is interleaved in DRAM", "No"
