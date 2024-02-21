e8284988cd3aded199bf58a919b0f1235e8b7a49
Nemanja Grujic <ngrujic@tenstorrent.com>
2024-02-21 11:50:20 +0000

    #5518: Make unit and sweep tests for complex abs logical coord problem

---
+++ tests/tt_eager/python_api_testing/non_working_unit_tests/grayskull/test_complex_abs_logical_coord.py
@@ -0,0 +1,79 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from loguru import logger
+import pytest
+import torch
+
+import tt_lib as ttl
+
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc
+
+from tests.tt_eager.python_api_testing.sweep_tests.pytorch_ops import complex_abs as pt_complex_abs
+from tests.tt_eager.python_api_testing.sweep_tests.tt_lib_ops import complex_abs as tt_complex_abs
+from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_rand_complex
+
+
+def run_complex_abs_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
+    torch.manual_seed(data_seed)
+
+    if in_mem_config == "SYSTEM_MEMORY":
+        in_mem_config = None
+
+    x = gen_rand_complex(size=input_shape, low=-100, high=100)
+    x_ref = x.detach().clone()
+
+    # get ref result
+    ref_value = pt_complex_abs(x_ref)
+
+    tt_result = tt_complex_abs(
+        x=x,
+        device=device,
+        dtype=[dtype],
+        layout=[dlayout],
+        input_mem_config=[in_mem_config],
+        output_mem_config=out_mem_config,
+    )
+
+    # compare tt and golden outputs
+    success, pcc_value = comp_pcc(ref_value, tt_result)
+    logger.debug(pcc_value)
+
+    assert success
+
+
+test_sweep_args = [
+    (
+        (4, 7, 32, 128),
+        ttl.tensor.DataType.BFLOAT16,
+        ttl.tensor.Layout.TILE,
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        38346,
+    ),
+    (
+        (4, 6, 160, 64),
+        ttl.tensor.DataType.BFLOAT16,
+        ttl.tensor.Layout.TILE,
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        38346,
+    ),
+    (
+        (4, 6, 160, 128),
+        ttl.tensor.DataType.BFLOAT16,
+        ttl.tensor.Layout.TILE,
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        38346,
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed",
+    (test_sweep_args),
+)
+def test_complex_abs_test(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
+    run_complex_abs_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device)
