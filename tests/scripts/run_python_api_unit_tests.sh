67b2ae9cbb4c4555bd9c0346dfb1464a51abec19
Michael Chiou <mchiou@tenstorrent.com>
2024-02-23 01:48:20 -0800

    #5588: split pytest scripts in run_python_api_unit_tests into scripts

--- tests/scripts/run_python_api_unit_tests.sh
+++ tests/scripts/run_python_api_unit_tests.sh
@@ -7,59 +7,8 @@ if [[ -z "$TT_METAL_HOME" ]]; then
   exit 1
 fi
 
-if [[ ! -z "$TT_METAL_SLOW_DISPATCH_MODE" ]]; then
-  env pytest $(find $TT_METAL_HOME/tests/tt_eager/python_api_testing/unit_testing/ -name 'test_*.py' -a ! -name 'test_untilize_with_halo_and_max_pool.py') -vvv
-  env pytest $(find $TT_METAL_HOME/tests/tt_eager/python_api_testing/sweep_tests/pytests/ -name 'test_*.py' -a ! -name 'test_sweep_conv_with_address_map.py') -vvv
-else
-  # Need to remove move for time being since failing
-  env pytest $TT_METAL_HOME/tests/tt_eager/python_api_testing/unit_testing/ -vvv
-  env pytest $(find $TT_METAL_HOME/tests/tt_eager/python_api_testing/sweep_tests/pytests/ -name 'test_*.py' -a ! -name 'test_sweep_conv_with_address_map.py' -a ! -name 'test_move.py') -vvv
-  env pytest $TT_METAL_HOME/tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_move.py -k input_L1 -vvv
-fi
+# Execute TT Eager unit and sweep tests here
+./tests/scripts/run_python_unit_tests.sh
 
-# This must run in slow dispatch mode
-# pytest -svv $TT_METAL_HOME/tests/python_api_testing/sweep_tests/pytests/test_sweep_conv_with_address_map.py
-
-# For now, adding tests with fast dispatch and non-32B divisible page sizes here. Python/models people,
-# you can move to where you'd like.
-
-if [ "$ARCH_NAME" != "wormhole_b0" ]; then
-
-  # TODO(arakhmati): Run ttnn tests only on graskull until the issue with ttnn.reshape on wormhole is resolved
-env pytest $TT_METAL_HOME/tests/ttnn/unit_tests
-
-# Tests for tensors in L1
-pytest $TT_METAL_HOME/models/experimental/bert_large_performant/unit_tests/test_bert_large*matmul* -k in0_L1-in1_L1-bias_L1-out_L1
-pytest $TT_METAL_HOME/models/experimental/bert_large_performant/unit_tests/test_bert_large*bmm* -k in0_L1-in1_L1-out_L1
-# Tests for mixed precision (sweeps combos of bfp8_b/bfloat16 dtypes for fused_qkv_bias and ff1_bias_gelu matmul and pre_softmax_bmm)
-pytest $TT_METAL_HOME/models/experimental/bert_large_performant/unit_tests/test_bert_large_matmuls_and_bmms_with_mixed_precision.py::test_bert_large_matmul -k "fused_qkv_bias and batch_9 and L1"
-pytest $TT_METAL_HOME/models/experimental/bert_large_performant/unit_tests/test_bert_large_matmuls_and_bmms_with_mixed_precision.py::test_bert_large_matmul -k "ff1_bias_gelu and batch_9 and DRAM"
-pytest $TT_METAL_HOME/models/experimental/bert_large_performant/unit_tests/test_bert_large_matmuls_and_bmms_with_mixed_precision.py::test_bert_large_bmm -k "pre_softmax_bmm and batch_9"
-
-# BERT TMs
-pytest $TT_METAL_HOME/models/experimental/bert_large_performant/unit_tests/test_bert_large_split_query_key_value_and_split_heads.py -k "in0_L1-out_L1 and batch_9"
-pytest $TT_METAL_HOME/models/experimental/bert_large_performant/unit_tests/test_bert_large_concatenate_heads.py -k "in0_L1-out_L1 and batch_9"
-
-# Test program cache
-pytest $TT_METAL_HOME/models/experimental/bert_large_performant/unit_tests/ -k program_cache
-
-# Fused ops unit tests
-pytest $TT_METAL_HOME/models/experimental/bert_large_performant/unit_tests/fused_ops/test_bert_large_fused_ln.py -k "in0_L1-out_L1 and batch_9"
-pytest $TT_METAL_HOME/models/experimental/bert_large_performant/unit_tests/fused_ops/test_bert_large_fused_softmax.py -k "in0_L1 and batch_9"
-
-# Resnet18 tests with conv on cpu and with conv on device
-pytest $TT_METAL_HOME/models/demos/resnet/tests/test_resnet18.py
-
-# Falcon tests
-pytest $TT_METAL_HOME/models/demos/falcon7b/tests/unit_tests/test_falcon_matmuls_and_bmms_with_mixed_precision.py -k "seq_len_128 and in0_BFLOAT16-in1_BFLOAT8_B-out_BFLOAT16-weights_DRAM"
-pytest $TT_METAL_HOME/models/demos/falcon7b/tests/unit_tests/test_falcon_matmuls_and_bmms_with_mixed_precision.py -k "seq_len_512 and in0_BFLOAT16-in1_BFLOAT8_B-out_BFLOAT16-weights_DRAM"
-pytest $TT_METAL_HOME/models/demos/falcon7b/tests/unit_tests/test_falcon_attn_matmul.py
-else
-# wormhole_b0
-
-# Falcon tests
-# attn_matmul_from_cache is currently not used in falcon7b
-pytest $TT_METAL_HOME/models/demos/falcon7b/tests/unit_tests/test_falcon_attn_matmul.py -k "not attn_matmul_from_cache"
-# higher sequence lengths and different formats trigger memory issues
-pytest $TT_METAL_HOME/models/demos/falcon7b/tests/unit_tests/test_falcon_matmuls_and_bmms_with_mixed_precision.py -k "seq_len_128 and in0_BFLOAT16-in1_BFLOAT8_B-out_BFLOAT16-weights_DRAM"
-fi
+# Execute python model tests here
+./tests/scripts/run_python_model_tests.sh
