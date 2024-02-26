6a6e3f9fce8adda586f1c27506ff9bff4143366e
Nicholas Smith <nsmith@tenstorrent.com>
2024-02-26 08:39:50 -0600

    #5186: Follow on UTWH neighbourhood distance >2 support
    Remove fixed neighborhood code-path.

--- tt_eager/tt_dnn/op_library/untilize/untilize_op.hpp
+++ tt_eager/tt_dnn/op_library/untilize/untilize_op.hpp
@@ -104,17 +104,6 @@ struct UntilizeWithHaloV2 {
     const uint32_t pad_val_;
     const uint32_t ncores_nhw_;
     const uint32_t max_out_nsticks_per_core_;
-    const std::vector<int32_t> local_pad_nsegments_per_core_;
-    const std::vector<int32_t> ll_data_nsegments_per_core_;
-    const std::vector<int32_t> l_data_nsegments_per_core_;
-    const std::vector<int32_t> local_data_nsegments_per_core_;
-    const std::vector<int32_t> r_data_nsegments_per_core_;
-    const std::vector<int32_t> rr_data_nsegments_per_core_;
-    const std::vector<int32_t> local_data_src_start_offsets_per_core_;
-    const std::vector<int32_t> ll_data_src_start_offsets_per_core_;
-    const std::vector<int32_t> l_data_src_start_offsets_per_core_;
-    const std::vector<int32_t> r_data_src_start_offsets_per_core_;
-    const std::vector<int32_t> rr_data_src_start_offsets_per_core_;
     const MemoryConfig out_mem_config_;
 
     void validate(const std::vector<Tensor> &input_tensors) const;
@@ -122,56 +111,24 @@ struct UntilizeWithHaloV2 {
     std::vector<Tensor> create_output_tensors(const std::vector<Tensor> &input_tensors) const;
     operation::ProgramWithCallbacks create_program(const std::vector<Tensor>& input_tensors, std::vector<Tensor> &output_tensors) const;
 
-    static constexpr auto attribute_names = std::make_tuple(
-        "pad_val_",
-        "ncores_nhw_",
-        "max_out_nsticks_per_core_",
-        "local_pad_nsegments_per_core_",
-        "ll_data_nsegments_per_core_",
-        "l_data_nsegments_per_core_",
-        "local_data_nsegments_per_core_",
-        "r_data_nsegments_per_core_",
-        "rr_data_nsegments_per_core_",
-        "out_mem_config_");
+    static constexpr auto attribute_names =
+        std::make_tuple("pad_val_", "ncores_nhw_", "max_out_nsticks_per_core_", "out_mem_config_");
     const auto attribute_values() const {
         return std::make_tuple(
             std::cref(pad_val_),
             std::cref(ncores_nhw_),
             std::cref(max_out_nsticks_per_core_),
-            std::cref(local_pad_nsegments_per_core_),
-            std::cref(ll_data_nsegments_per_core_),
-            std::cref(l_data_nsegments_per_core_),
-            std::cref(local_data_nsegments_per_core_),
-            std::cref(r_data_nsegments_per_core_),
-            std::cref(rr_data_nsegments_per_core_),
             std::cref(out_mem_config_));
     }
 };
 Tensor untilize_with_halo_v2(
     const Tensor &input_tensor,
-    const Tensor &local_pad_start_and_size,
-    const Tensor &ll_data_start_and_size,
-    const Tensor &l_data_start_and_size,
-    const Tensor &local_data_start_and_size,
-    const Tensor &r_data_start_and_size,
-    const Tensor &rr_data_start_and_size,
     const Tensor &padding_config,
     const Tensor &local_config,
     const Tensor &remote_config,
     const uint32_t pad_val,
     const uint32_t ncores_nhw,
     const uint32_t max_out_nsticks_per_core,
-    const std::vector<int32_t> &local_pad_nsegments_per_core,
-    const std::vector<int32_t> &ll_data_nsegments_per_core,
-    const std::vector<int32_t> &l_data_nsegments_per_core,
-    const std::vector<int32_t> &local_data_nsegments_per_core,
-    const std::vector<int32_t> &r_data_nsegments_per_core,
-    const std::vector<int32_t> &rr_data_nsegments_per_core,
-    const std::vector<int32_t> &local_data_src_start_offsets_per_core,
-    const std::vector<int32_t> &ll_data_src_start_offsets_per_core,
-    const std::vector<int32_t> &l_data_src_start_offsets_per_core,
-    const std::vector<int32_t> &r_data_src_start_offsets_per_core,
-    const std::vector<int32_t> &rr_data_src_start_offsets_per_core,
     const MemoryConfig &mem_config);
 
 namespace untilize_helpers {
