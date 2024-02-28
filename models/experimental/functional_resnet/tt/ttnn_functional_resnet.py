e918ca323bb3cc746ca405876837d4744490ffd3
Shwetank Singh <ssingh@tenstorrent.com>
2024-02-28 05:40:31 +0000

    #5984 Add resnet integration ttnn test running on grayskull

--- models/experimental/functional_resnet/tt/ttnn_functional_resnet.py
+++ models/experimental/functional_resnet/tt/ttnn_functional_resnet.py
@@ -26,3 +26,28 @@ def resnet_basic_block(x, *, parameters):
         ttnn.deallocate(identity)
 
     return out
+
+
+def resnet_bottleneck_block(x, parameters):
+    identity = x
+
+    conv1 = parameters.conv1(x)
+    conv2 = parameters.conv2(conv1)
+    ttnn.deallocate(conv1)
+    conv3 = parameters.conv3(conv2)
+    ttnn.deallocate(conv2)
+
+    if "downsample" in parameters and parameters.downsample is not None:
+        x = ttnn.to_memory_config(x, ttnn.DRAM_MEMORY_CONFIG)
+        identity = parameters.downsample(x)
+        ttnn.deallocate(x)
+
+    conv3 = ttnn.reshape(conv3, identity.shape)
+    out = ttnn.add_and_apply_activation(conv3, identity, activation="relu", memory_config=ttnn.DRAM_MEMORY_CONFIG)
+    ttnn.deallocate(conv3)
+
+    # ttnn.deallocate(conv3)
+    if x is not identity:
+        ttnn.deallocate(identity)
+
+    return out
