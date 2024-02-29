6cbb59d2a31ee56496aa0e73c2badaf32e514288
yugaoTT <yugao@tenstorrent.com>
2024-02-29 14:48:20 +0000

    #6023: add single core matmul u-benchmark

---
+++ tests/tt_metal/tt_metal/perf_microbenchmark/1_compute_mm/kernels/in0_reader_bmm_single_core.cpp
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdint.h>
+
+#include "dataflow_api.h"
+// #include "debug/dprint.h"
+
+void kernel_main() {
+    constexpr uint32_t in0_block_tiles = get_compile_time_arg_val(0);
+    constexpr uint32_t num_blocks = get_compile_time_arg_val(1);
+
+    constexpr uint32_t cb_id_in0 = 0;
+
+//    DPRINT  << TSLICE(cb_id_in0, 0, SliceRange::h0_w0_32()) << ENDL() ;
+
+
+    for (uint32_t block = 0; block < num_blocks; block++) {
+        cb_reserve_back(cb_id_in0, in0_block_tiles);
+        cb_push_back(cb_id_in0, in0_block_tiles);
+    }
+}
---
+++ tests/tt_metal/tt_metal/perf_microbenchmark/1_compute_mm/kernels/in1_reader_writer_bmm_single_core.cpp
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdint.h>
+
+#include "dataflow_api.h"
+
+void kernel_main() {
+    constexpr uint32_t in1_block_tiles = get_compile_time_arg_val(0);
+    constexpr uint32_t num_blocks = get_compile_time_arg_val(1);
+
+    constexpr uint32_t cb_id_in1 = 1;
+
+
+    for (uint32_t block = 0; block < num_blocks; block++) {
+        cb_reserve_back(cb_id_in1, in1_block_tiles);
+        cb_push_back(cb_id_in1, in1_block_tiles);
+    }
+}
