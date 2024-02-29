5cf04fab187439a635b193498a1376a0e2c236cf
umadevimcw <umadevi@multicorewareinc.com>
2024-02-29 14:11:32 +0000

    #5822: Add backward support for acosh

--- tt_eager/tt_dnn/op_library/backward/backward_ops.cpp
+++ tt_eager/tt_dnn/op_library/backward/backward_ops.cpp
@@ -947,6 +947,26 @@ std::vector<Tensor> cos_bw(const Tensor& grad, const Tensor& input, const Memory
     return operation::decorate_as_composite(__func__, _cos_bw)(grad, input, output_mem_config);
 }
 
+std::vector<Tensor> _acosh_bw(const Tensor& grad, const Tensor& input, const MemoryConfig& output_mem_config) {
+    std::vector<Tensor> grad_tensor;
+    Tensor in_rsqrt = square(input, output_mem_config);
+    in_rsqrt = rsqrt(sub_unary(in_rsqrt, 1.0, output_mem_config), true, output_mem_config);
+    Tensor grad_a = mul(grad, in_rsqrt, std::nullopt, output_mem_config);
+    float t_nan  = std::nanf("");
+    float t_inf = std::numeric_limits<float>::infinity();
+    Tensor cond_result = logical_or(lt(input, full_like(input, -1.0, output_mem_config), std::nullopt, output_mem_config),
+                        gt(input, full_like(input, 1.0, output_mem_config), std::nullopt, output_mem_config), std::nullopt, output_mem_config);
+    grad_a = where(eqz(cond_result, output_mem_config), t_nan, grad_a, output_mem_config);
+    cond_result = logical_or(eq(input, full_like(input, -1.0, output_mem_config), std::nullopt, output_mem_config),
+                        eq(input, full_like(input, 1.0, output_mem_config), std::nullopt, output_mem_config), std::nullopt, output_mem_config);
+    grad_a = where(eq(cond_result, ones_like(input, output_mem_config), std::nullopt, output_mem_config), t_inf, grad_a, output_mem_config);
+    grad_tensor.emplace_back(grad_a);
+    return grad_tensor;
+}
+std::vector<Tensor> acosh_bw(const Tensor& grad, const Tensor& input, const MemoryConfig& output_mem_config)
+{
+    return operation::decorate_as_composite(__func__, _acosh_bw)(grad, input, output_mem_config);
+}
 }//namespace tt_metal
 
 }//namespace tt
