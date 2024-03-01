f51f91239103daa460d8b420ff7872f8aabf99d6
Raymond Kim <rkim@tenstorrent.com>
2024-03-01 18:20:02 +0000

    #5873: Clean up LDFLAGS for tests by centralizing inheritance more into the top-level tests/tt_metal/module.mk and use it in the gtests underneath rather than referencing the global LDFLAGS directly and get rid of hardcoded rpath which hardcoded a dynamic lib link into our executables #5873: Get rid of unnecessary -L in ttnn ldflags and still use rpath in dev environment flow as we're still building things on silicon runners so without rpath or LD_LIBRARY_PATH it can't dynamically link against LIBDIR at runtime + don't want to pollute everywhere with LD_LIBRARY_PATH yet

--- module.mk
+++ module.mk
@@ -79,7 +79,7 @@ CXX ?= g++
 CFLAGS ?= -MMD $(WARNINGS) -I. $(CONFIG_CFLAGS) -mavx2 -DBUILD_DIR=\"$(OUT)\"
 CXXFLAGS ?= --std=c++17 -fvisibility-inlines-hidden -Werror
 
-LDFLAGS ?= $(CONFIG_LDFLAGS) -Wl,-rpath,$(PREFIX)/lib -L$(LIBDIR)/tools -L$(LIBDIR) \
+LDFLAGS ?= $(CONFIG_LDFLAGS) -L$(LIBDIR) \
 	-ldl \
 	-lz \
 	-lboost_thread \
@@ -89,6 +89,10 @@ LDFLAGS ?= $(CONFIG_LDFLAGS) -Wl,-rpath,$(PREFIX)/lib -L$(LIBDIR)/tools -L$(LIBD
 	-lpthread \
 	-latomic \
 	-lhwloc
+ifdef TT_METAL_ENV_IS_DEV
+LDFLAGS += \
+	-Wl,-rpath,$(PREFIX)/lib
+endif
 SHARED_LIB_FLAGS = -shared -fPIC
 STATIC_LIB_FLAGS = -fPIC
 ifeq ($(findstring clang,$(CC)),clang)
--- tests/tt_metal/tt_metal/gtest_smoke/module.mk
+++ tests/tt_metal/tt_metal/gtest_smoke/module.mk
@@ -12,7 +12,7 @@ TT_METAL_GTEST_SMOKE_OBJ_HOME = tt_metal/tests/gtest_smoke/
 TT_METAL_GTEST_SMOKE_SRCS = $(patsubst $(TT_METAL_GTEST_SMOKE_SRCS_HOME)%, $(TT_METAL_GTEST_SMOKE_OBJ_HOME)%, $(TT_METAL_GTEST_SMOKE))
 
 TT_METAL_GTEST_SMOKE_INCLUDES = $(TEST_INCLUDES) $(TT_METAL_INCLUDES)
-TT_METAL_GTEST_SMOKE_LDFLAGS = $(LDFFLAGS) -ltt_metal -ldl -lstdc++fs -pthread -lyaml-cpp -lgtest -lgtest_main -lm
+TT_METAL_GTEST_SMOKE_LDFLAGS = $(TT_METAL_UNIT_TESTS_LDFLAGS) -ltt_metal -ldl -lstdc++fs -pthread -lyaml-cpp -lgtest -lgtest_main -lm
 
 TT_METAL_GTEST_SMOKE_OBJS = $(addprefix $(OBJDIR)/, $(TT_METAL_GTEST_SMOKE_SRCS:.cpp=.o))
 TT_METAL_GTEST_SMOKE_DEPS = $(addprefix $(OBJDIR)/, $(TT_METAL_GTEST_SMOKE_SRCS:.cpp=.d))
@@ -25,7 +25,7 @@ tests/tt_metal/gtest_smoke: $(TESTDIR)/tt_metal/gtest_smoke
 .PRECIOUS: $(TESTDIR)/tt_metal/gtest_smoke
 $(TESTDIR)/tt_metal/gtest_smoke: $(TT_METAL_GTEST_SMOKE_OBJS) $(TT_METAL_LIB) $(TT_DNN_LIB)
 	@mkdir -p $(@D)
-	$(CXX) $(CFLAGS) $(CXXFLAGS) $(TT_METAL_GTEST_SMOKE_INCLUDES) -o $@ $^ $(LDFLAGS) $(TT_METAL_GTEST_SMOKE_LDFLAGS)
+	$(CXX) $(CFLAGS) $(CXXFLAGS) $(TT_METAL_GTEST_SMOKE_INCLUDES) -o $@ $^ $(TT_METAL_GTEST_SMOKE_LDFLAGS)
 
 .PRECIOUS: $(OBJDIR)/$(TT_METAL_GTEST_SMOKE_OBJ_HOME)/%.o
 $(OBJDIR)/$(TT_METAL_GTEST_SMOKE_OBJ_HOME)/%.o: $(TT_METAL_GTEST_SMOKE_SRCS_HOME)/%.cpp
