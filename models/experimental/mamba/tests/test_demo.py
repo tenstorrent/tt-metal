9306cd59162bb6fec794267b612f3e85f0fb4e72
Evan Smal <esmal@tenstorrent.com>
2024-03-01 23:10:43 +0000

    #5394: Add functional version of Mamba architecture

---
+++ models/experimental/mamba/tests/test_demo.py
@@ -0,0 +1,40 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+import pytest
+from loguru import logger
+
+from transformers import AutoTokenizer
+
+from models.experimental.mamba.demo.demo import run_demo
+from models.experimental.mamba.reference.decode_model import MambaPretrainedModelName
+
+
+@pytest.mark.parametrize(
+    "model_version, batch, genlen",
+    (
+        ("state-spaces/mamba-370m", 1, 4),
+        ("state-spaces/mamba-370m", 2, 4),
+        ("state-spaces/mamba-370m", 3, 4),
+    ),
+)
+def test_demo(
+    model_version: MambaPretrainedModelName,
+    batch: int,
+    genlen: int,
+):
+    prompt = ["Hello" for _ in range(batch)]
+
+    res1 = run_demo(
+        prompt, "cpu", generated_sequence_length=genlen, model_version=model_version, display=False, use_cache=False
+    )
+    assert len(res1) == genlen + 1
+
+    res2 = run_demo(
+        prompt, "wh", generated_sequence_length=genlen, model_version=model_version, display=False, use_cache=False
+    )
+    assert len(res2) == genlen + 1
+
+    assert res1 == res2, "Model outputs should match"
