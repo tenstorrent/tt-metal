c8f14bcedffbce78d7ea1656595f15b5b0f884b7
Aleks Knezevic <aknezevic@tenstorrent.com>
2024-03-01 01:28:14 +0000

    #0: unpad attention output if it was padded

--- models/experimental/functional_stable_diffusion/tt2/ttnn_functional_cross_attention.py
+++ models/experimental/functional_stable_diffusion/tt2/ttnn_functional_cross_attention.py
@@ -72,24 +72,47 @@ class cross_attention:
 
         self.scales = {40: self.scale_40, 80: self.scale_80, 160: self.scale_160}
 
-        attention_mask = torch.ones((1, 1, 1, 256)) * -1e9
-        attention_mask[:, :, :, :77] = 0
+        attention_mask_96 = torch.ones((1, 1, 1, 96)) * -1e9
+        attention_mask_96[:, :, :, :77] = 0
+        attention_mask_96 = ttnn.from_torch(
+            attention_mask_96, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=self.device
+        )
+        attention_mask_256 = torch.ones((1, 1, 1, 256)) * -1e9
+        attention_mask_256[:, :, :, :77] = 0
+        attention_mask_256 = ttnn.from_torch(
+            attention_mask_256, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=self.device
+        )
+        attention_mask_1024 = torch.ones((1, 1, 1, 1024)) * -1e9
+        attention_mask_1024[:, :, :, :77] = 0
+        attention_mask_1024 = ttnn.from_torch(
+            attention_mask_1024, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=self.device
+        )
+        attention_mask_4096 = torch.ones((1, 1, 1, 4096)) * -1e9
+        attention_mask_4096[:, :, :, :77] = 0
+        attention_mask_4096 = ttnn.from_torch(
+            attention_mask_4096, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=self.device
+        )
 
         # if key.shape[-1] == 512:
         #     attention_mask[:, :, :, 256 : 256 + 77] = 0
 
-        self.attention_mask = ttnn.from_torch(
-            attention_mask, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=self.device
-        )
+        self.attention_masks = {
+            96: attention_mask_96,
+            256: attention_mask_256,
+            1024: attention_mask_1024,
+            4096: attention_mask_4096,
+        }
 
     def get_attention_scores(self, query, t_key, attention_mask=None, scale=None, device=None):
         # t_key = ttnn.permute(key, (0, 1, 3, 2))
         attention_scores = ttnn.matmul(query, t_key)
-
+        del t_key, query
         attention_scores = ttnn.mul(attention_scores, scale)
+        del scale
 
         if attention_mask is not None:
             attention_scores = ttnn.add(attention_scores, attention_mask)
+            del attention_mask
 
         attention_scores = ttnn.softmax(attention_scores, dim=-1)
 
@@ -109,6 +132,7 @@ class cross_attention:
         cross_attention_kwargs={},
     ):
         assert dim_head in self.scales
+        original_seq_len = hidden_states.shape[-2]
         if len(hidden_states.shape) == 4:
             hidden_states = squeeze(hidden_states, 0)
         if encoder_hidden_states and len(encoder_hidden_states.shape) == 4:
@@ -132,11 +156,23 @@ class cross_attention:
             hidden_seq_len = hidden_states.shape.with_tile_padding()[-2]
             encoder_hidden_seq_len = encoder_hidden_states.shape.with_tile_padding()[-2]
 
-            q_proj = ttnn.linear(hidden_states, self.parameters.to_q.weight, memory_config=ttnn.L1_MEMORY_CONFIG)
+            q_proj = ttnn.linear(
+                hidden_states,
+                self.parameters.to_q.weight,
+                memory_config=ttnn.L1_MEMORY_CONFIG,
+                core_grid=ttnn.CoreGrid(y=8, x=8),
+            )
+            del hidden_states
             if encoder_hidden_seq_len > hidden_seq_len:
                 padding_needed = encoder_hidden_seq_len - hidden_seq_len
                 q_proj = ttnn.pad(q_proj, ((0, 0), (0, padding_needed), (0, 0)), 0)
-            kv_proj = ttnn.linear(encoder_hidden_states, self.parameters.kv.weight, memory_config=ttnn.L1_MEMORY_CONFIG)
+            kv_proj = ttnn.linear(
+                encoder_hidden_states,
+                self.parameters.kv.weight,
+                memory_config=ttnn.L1_MEMORY_CONFIG,
+                core_grid=ttnn.CoreGrid(y=8, x=8),
+            )
+            del encoder_hidden_states
             if hidden_seq_len > encoder_hidden_seq_len:
                 padding_needed = hidden_seq_len - encoder_hidden_seq_len
                 kv_proj = ttnn.pad(kv_proj, ((0, 0), (0, padding_needed), (0, 0)), 0)
@@ -144,19 +180,17 @@ class cross_attention:
             del kv_proj
             del q_proj
 
-            assert self.attention_mask.shape[-1] == key.shape[-1]
-            attention_mask = self.attention_mask
+            assert key.shape[-1] in self.attention_masks
+            attention_mask = self.attention_masks[key.shape[-1]]
 
         attention_probs = self.get_attention_scores(
             query, key, attention_mask, scale=self.scales[dim_head], device=self.device
         )
 
-        padding_needed = attention_probs.shape[-1] - value.shape[-2]
-        if padding_needed > 0:
-            value = ttnn.pad(value, ((0, padding_needed), (0, 0)), 0)
-
         hidden_states = ttnn.matmul(attention_probs, value)
 
+        if hidden_states.shape[-2] != original_seq_len:
+            hidden_states = hidden_states[:, :, :original_seq_len, :]
         hidden_states = ttnn.transformer.concatenate_heads(
             hidden_states, memory_config=ttnn.get_memory_config(hidden_states)
         )
