#5589: Fix softmax unit test

tests/ttnn/python_api_testing/non_working_unit_tests/grayskull/test_eltwise_softmax.py
@@ -9,8 +9,8 @@ import torch
 import ttnn
 import traceback
 
-from tests.ttnn.utils_for_testing import assert_with_pcc
 from tests.ttnn.python_api_testing.sweep_tests import ttnn_ops
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc
 
 
 def run_eltwise_softmax_tests(
@@ -27,11 +27,11 @@ def run_eltwise_softmax_tests(
 
     try:
         # get ref result
-        ref_value = torch.sin(x)
+        ref_value = torch.softmax(x, dim=-1)
 
         x = ttnn_ops.setup_ttnn_tensor(x, device, dlayout[0], in_mem_config[0], dtype[0])
 
-        tt_result = ttnn.sin(x)
+        tt_result = ttnn.softmax(x, dim=-1)
         tt_result = ttnn_ops.ttnn_tensor_to_torch(tt_result, output_mem_config)
 
     except Exception as e:
@@ -41,7 +41,13 @@ def run_eltwise_softmax_tests(
 
     assert len(tt_result.shape) == len(ref_value.shape)
     assert tt_result.shape == ref_value.shape
-    assert_with_pcc(ref_value, tt_result, 0.99)
+
+    # compare tt and golden outputs
+    success, pcc_value = comp_pcc(ref_value, tt_result)
+    logger.debug(pcc_value)
+    logger.debug(success)
+
+    assert success
 
 
 test_sweep_args = [
@@ -52,6 +58,22 @@ test_sweep_args = [
         [ttnn.DRAM_MEMORY_CONFIG],
         ttnn.L1_MEMORY_CONFIG,
         3181992,
+    ),
+    (
+        [(2, 4, 224, 64)],
+        [ttnn.bfloat16],
+        [ttnn.TILE_LAYOUT],
+        [ttnn.DRAM_MEMORY_CONFIG],
+        ttnn.L1_MEMORY_CONFIG,
+        10739250,
+    ),
+    (
+        [(6, 160, 64)],
+        [ttnn.bfloat16],
+        [ttnn.TILE_LAYOUT],
+        [ttnn.DRAM_MEMORY_CONFIG],
+        ttnn.DRAM_MEMORY_CONFIG,
+        16934480,
     ),
     (
         [(2, 4, 224, 64)],