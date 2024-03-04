c7138c3f7c9063e5c22ea056b95c70affae9fe46
Yan Zaretskiy <yzaretskiy@tenstorrent.com>
2024-03-04 16:47:20 +0000

    Revert "#4438: Make Resnet use the new fold op instead of plain Python implementation."
    This reverts commit f1953b646396ba7480e46416ddaf35e67dfdb2db.

--- tt_eager/tt_dnn/op_library/sharded/sharded_op.cpp
+++ tt_eager/tt_dnn/op_library/sharded/sharded_op.cpp
@@ -66,9 +66,9 @@ operation::ProgramWithCallbacks Sharded::create_program(
     auto& output_tensor = output_tensors.at(0);
 
     if (this->sharded_op_type == ShardedOpType::InterleavedToSharded) {
-        return interleaved_to_sharded_multi_core(input_tensor, output_tensor);
+        return interleaved_to_sharded_multi_core(input_tensor, output_tensor, this->grid_size);
     } else {
-        return sharded_to_interleaved_multi_core(input_tensor, output_tensor);
+        return sharded_to_interleaved_multi_core(input_tensor, output_tensor, this->grid_size);
     }
 }
 
