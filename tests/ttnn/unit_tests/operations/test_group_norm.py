5e0fac9a197da857fbec77bfcbb5145581b31ee0
Abhinav Sarje <asarje@tenstorrent.com>
2024-03-04 15:24:44 +0000

    #0: Hanging case repro'd in unit test

--- tests/ttnn/unit_tests/operations/test_group_norm.py
+++ tests/ttnn/unit_tests/operations/test_group_norm.py
@@ -122,3 +122,84 @@ def test_group_norm_with_height_sharded(device, N, C, H, W, num_groups):
     output_tensor = ttnn.to_torch(output_tensor)
 
     assert_with_pcc(torch_output_tensor, output_tensor, 0.9998)
+
+
+@pytest.mark.parametrize("N", [1])
+@pytest.mark.parametrize("C", [1280])
+@pytest.mark.parametrize("H", [16])
+@pytest.mark.parametrize("W", [16])
+@pytest.mark.parametrize("num_groups", [32])
+def test_group_norm_with_block_sharded(device, N, C, H, W, num_groups):
+    torch.manual_seed(0)
+
+    grid_size = ttnn.CoreGrid(y=8, x=4)
+
+    torch_input_tensor = torch.rand((N, C, H, W), dtype=torch.bfloat16)
+    torch_weight = torch.rand((C,), dtype=torch.bfloat16)
+    torch_bias = torch.rand((C,), dtype=torch.bfloat16)
+    torch_output_tensor = torch.nn.functional.group_norm(
+        torch_input_tensor, num_groups, weight=torch_weight, bias=torch_bias
+    )
+    torch_output_tensor = torch_output_tensor.permute(0, 2, 3, 1).view(N, 1, W * H, C)
+
+    input_tensor = torch_input_tensor.permute(0, 2, 3, 1).view(N, 1, W * H, C)
+    input_tensor = ttnn.from_torch(
+        input_tensor,
+        dtype=ttnn.DataType.BFLOAT16,
+        layout=ttnn.ROW_MAJOR_LAYOUT,
+        device=device,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    gamma = ttnn.create_group_norm_weight_bias_rm(torch_weight, C, num_groups)
+    beta = ttnn.create_group_norm_weight_bias_rm(torch_bias, C, num_groups)
+
+    gamma_t = ttnn.from_torch(
+        gamma,
+        dtype=ttnn.DataType.BFLOAT16,
+        layout=ttnn.ROW_MAJOR_LAYOUT,
+        device=device,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+    beta_t = ttnn.from_torch(
+        beta,
+        dtype=ttnn.DataType.BFLOAT16,
+        layout=ttnn.ROW_MAJOR_LAYOUT,
+        device=device,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    # sharded_mem_config = ttnn.create_sharded_memory_config(
+    #     input_tensor.shape,
+    #     grid_size,
+    #     ttnn.ShardStrategy.BLOCK,
+    #     ttnn.ShardOrientation.COLUMN_MAJOR,
+    # )
+    grid_coord = ttnn.experimental.tensor.CoreCoord(grid_size.x - 1, grid_size.y - 1)
+    shard_grid = ttnn.experimental.tensor.CoreRangeSet(
+        {ttnn.experimental.tensor.CoreRange(ttnn.experimental.tensor.CoreCoord(0, 0), grid_coord)}
+    )
+    shard_shape = N * H * W // grid_size.y, C // grid_size.x
+    shard_spec = ttnn.experimental.tensor.ShardSpec(
+        shard_grid, shard_shape, ttnn.experimental.tensor.ShardOrientation.COL_MAJOR, False
+    )
+    sharded_mem_config = ttnn.MemoryConfig(
+        ttnn.types.TensorMemoryLayout.BLOCK_SHARDED, ttnn.types.BufferType.L1, shard_spec
+    )
+
+    input_tensor = ttnn.to_memory_config(input_tensor, sharded_mem_config)
+
+    output_tensor = ttnn.group_norm(
+        input_tensor,
+        num_groups=num_groups,
+        weight=gamma_t,
+        bias=beta_t,
+        memory_config=sharded_mem_config,
+        core_grid=grid_size,
+    )
+
+    output_tensor = ttnn.to_memory_config(output_tensor, ttnn.DRAM_MEMORY_CONFIG)
+    output_tensor = ttnn.from_device(output_tensor)
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor, 0.9998)
