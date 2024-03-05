126afcee59a72b59b3f68704d83f3597fff6b9cb
Radomir Djogo <rdjogo@tenstorrent.com>
2024-03-05 19:42:49 +0000

    #5424: remove metal_ckernel_spfu ops for grayskull

---
+++ tt_metal/hw/ckernels/grayskull/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_expm1.h
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "llk_math_eltwise_unary_sfpu_init.h"
+#include "llk_math_eltwise_unary_sfpu_0_param.h"
+#include "ckernel_sfpu_expm1.h"
+
+namespace ckernel {
+
+// New LLK SFPU APIs
+
+template <bool APPROXIMATE>
+inline void llk_math_eltwise_unary_sfpu_expm1_init() {
+    llk_math_eltwise_unary_sfpu_init<APPROXIMATE>(sfpu::expm1_init<APPROXIMATE>);
+}
+
+template <bool APPROXIMATE, DstSync Dst = DstSync::SyncFull>
+inline void llk_math_eltwise_unary_sfpu_expm1(uint dst_index, int vector_mode = (int)VectorMode::RC) {
+    llk_math_eltwise_unary_sfpu_0_param<APPROXIMATE, Dst>
+                                (ckernel::sfpu::calculate_expm1<APPROXIMATE>,
+                                ckernel::sfpu::calculate_expm1<APPROXIMATE>,
+                                dst_index, vector_mode);
+}
+
+}
