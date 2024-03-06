d4ce8a1338ef288cff14e89c46bff8a5ca6ce1a5
Ivan Hamer <ihamer@tenstorrent.com>
2024-03-06 15:39:19 +0000

    #4809: Add tile selection

--- tt_metal/hw/inc/debug/dprint_tensix.h
+++ tt_metal/hw/inc/debug/dprint_tensix.h
@@ -7,13 +7,14 @@
 #include "dprint.h"
 #include "compute_kernel_api.h"
 
-void dprint_tensix_dest_reg() {
+// Print the contents of tile with index tile_id within the destination register
+void dprint_tensix_dest_reg(int tile_id = 0) {
     dbg_halt();
     MATH({
         DPRINT_MATH(DPRINT << FIXED() << SETPRECISION(2));
         uint32_t rd_data[8+1]; // data + array_type
         for (int row = 0; row < 64; row++) {
-            dbg_read_dest_acc_row(row, rd_data);
+            dbg_read_dest_acc_row(row + 64 * tile_id, rd_data);
             DPRINT_MATH(DPRINT << SETW(6) << TYPED_U32_ARRAY(TypedU32_ARRAY_Format_TensixRegister_FP16_B, rd_data, 8));
             if (row % 2 == 1) DPRINT_MATH(DPRINT << ENDL());
         }
