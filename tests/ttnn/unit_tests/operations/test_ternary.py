bfd30eb1464fc620f8b99e911805f38424ce8bd1
Nicholas Smith <nsmith@tenstorrent.com>
2024-03-07 12:21:31 -0600

    Revert "Revert "#5389: merged ttl and tensor classes into one""
    This reverts commit a5005c4be61f82e9f1342253bb410801eb372813.

--- tests/ttnn/unit_tests/operations/test_ternary.py
+++ tests/ttnn/unit_tests/operations/test_ternary.py
@@ -15,13 +15,15 @@ def torch_mac(input, tensor1, tensor2):
     return torch.add(torch.mul(input, tensor1), tensor2)
 
 
-def run_ternary_test_mac_TTT(device, h, w, ttnn_function, torch_function, pcc=0.9999):
+@pytest.mark.parametrize("h", [64])
+@pytest.mark.parametrize("w", [128])
+def test_mac_all_tensors(device, h, w):
     torch.manual_seed(0)
 
     torch_input_tensor = torch.rand((h, w), dtype=torch.bfloat16)
     torch_input_tensor1 = torch.rand((h, w), dtype=torch.bfloat16)
     torch_input_tensor2 = torch.rand((h, w), dtype=torch.bfloat16)
-    torch_output_tensor = torch_function(torch_input_tensor, torch_input_tensor1, torch_input_tensor2)
+    torch_output_tensor = torch_mac(torch_input_tensor, torch_input_tensor1, torch_input_tensor2)
 
     input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
     input_tensor = ttnn.to_device(input_tensor, device)
@@ -29,54 +31,46 @@ def run_ternary_test_mac_TTT(device, h, w, ttnn_function, torch_function, pcc=0.
     input_tensor1 = ttnn.to_device(input_tensor1, device)
     input_tensor2 = ttnn.from_torch(torch_input_tensor2, layout=ttnn.TILE_LAYOUT, device=device)
     input_tensor2 = ttnn.to_device(input_tensor2, device)
-    output_tensor = ttnn_function(input_tensor, input_tensor1, input_tensor2)
+    output_tensor = ttnn.mac(input_tensor, input_tensor1, input_tensor2)
     output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
     output_tensor = ttnn.from_device(output_tensor)
     output_tensor = ttnn.to_torch(output_tensor)
 
-    assert_with_pcc(torch_output_tensor, output_tensor, pcc)
-
-
-@pytest.mark.parametrize("h", [64])
-@pytest.mark.parametrize("w", [128])
-def test_mac_ttt(device, h, w):
-    run_ternary_test_mac_TTT(device, h, w, ttnn.mac, torch_mac)
-
-
-def run_ternary_test_mac_TSS(device, h, w, scalar1, scalar2, ttnn_function, torch_function, pcc=0.9999):
-    torch.manual_seed(0)
-
-    torch_input_tensor = torch.rand((h, w), dtype=torch.bfloat16)
-    torch_input_tensor1 = scalar1
-    torch_input_tensor2 = scalar2
-    torch_output_tensor = torch_function(torch_input_tensor, torch_input_tensor1, torch_input_tensor2)
-
-    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
-    input_tensor = ttnn.to_device(input_tensor, device)
-
-    output_tensor = ttnn_function(input_tensor, scalar1, scalar2)
-    output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
-    output_tensor = ttnn.from_device(output_tensor)
-    output_tensor = ttnn.to_torch(output_tensor)
-
-    assert_with_pcc(torch_output_tensor, output_tensor, pcc)
+    assert_with_pcc(torch_output_tensor, output_tensor)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 @pytest.mark.parametrize("scalar1", [5.5])
 @pytest.mark.parametrize("scalar2", [-13.2])
-def test_mac_tss(device, h, w, scalar1, scalar2):
-    run_ternary_test_mac_TSS(device, h, w, scalar1, scalar2, ttnn.mac, torch_mac)
+def test_mac_tensor_with_2_scalaras(device, h, w, scalar1, scalar2):
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.rand((h, w), dtype=torch.bfloat16)
+    torch_input_tensor1 = scalar1
+    torch_input_tensor2 = scalar2
+    torch_output_tensor = torch_mac(torch_input_tensor, torch_input_tensor1, torch_input_tensor2)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+    input_tensor = ttnn.to_device(input_tensor, device)
+
+    output_tensor = ttnn.mac(input_tensor, scalar1, scalar2)
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor)
 
 
-def run_ternary_test_where_TTT(device, h, w, ttnn_function, torch_function, pcc=0.9999):
+@pytest.mark.parametrize("h", [64])
+@pytest.mark.parametrize("w", [128])
+def test_where_all_tensors(device, h, w):
     torch.manual_seed(0)
 
     torch_input_tensor = torch.rand((h, w), dtype=torch.bfloat16)
     torch_input_tensor1 = torch.rand((h, w), dtype=torch.bfloat16)
     torch_input_tensor2 = torch.rand((h, w), dtype=torch.bfloat16)
-    torch_output_tensor = torch_function(torch_input_tensor.to(torch.bool), torch_input_tensor1, torch_input_tensor2)
+    torch_output_tensor = torch.where(torch_input_tensor.to(torch.bool), torch_input_tensor1, torch_input_tensor2)
 
     input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
     input_tensor = ttnn.to_device(input_tensor, device)
@@ -84,46 +78,36 @@ def run_ternary_test_where_TTT(device, h, w, ttnn_function, torch_function, pcc=
     input_tensor1 = ttnn.to_device(input_tensor1, device)
     input_tensor2 = ttnn.from_torch(torch_input_tensor2, layout=ttnn.TILE_LAYOUT, device=device)
     input_tensor2 = ttnn.to_device(input_tensor2, device)
-    output_tensor = ttnn_function(input_tensor, input_tensor1, input_tensor2)
+    output_tensor = ttnn.where(input_tensor, input_tensor1, input_tensor2)
     output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
     output_tensor = ttnn.from_device(output_tensor)
     output_tensor = ttnn.to_torch(output_tensor)
 
-    assert_with_pcc(torch_output_tensor, output_tensor, pcc)
-
-
-@pytest.mark.parametrize("h", [64])
-@pytest.mark.parametrize("w", [128])
-def test_where_ttt(device, h, w):
-    run_ternary_test_where_TTT(device, h, w, ttnn.where, torch.where)
-
-
-def run_ternary_test_where_TTS(device, h, w, scalar, ttnn_function, torch_function, pcc=0.9999):
-    torch.manual_seed(0)
-
-    torch_input_tensor = torch.rand((h, w), dtype=torch.bfloat16)
-    torch_input_tensor1 = torch.rand((h, w), dtype=torch.bfloat16)
-
-    torch_output_tensor = torch_function(torch_input_tensor.to(torch.bool), torch_input_tensor1, scalar)
-
-    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
-    input_tensor = ttnn.to_device(input_tensor, device)
-    input_tensor1 = ttnn.from_torch(torch_input_tensor1, layout=ttnn.TILE_LAYOUT, device=device)
-    input_tensor1 = ttnn.to_device(input_tensor1, device)
-
-    output_tensor = ttnn_function(input_tensor, input_tensor1, scalar)
-    output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
-    output_tensor = ttnn.from_device(output_tensor)
-    output_tensor = ttnn.to_torch(output_tensor)
-
-    assert_with_pcc(torch_output_tensor, output_tensor, pcc)
+    assert_with_pcc(torch_output_tensor, output_tensor)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 @pytest.mark.parametrize("scalar", [15.5])
 def test_where_tts(device, h, w, scalar):
-    run_ternary_test_where_TTS(device, h, w, scalar, ttnn.where, torch.where)
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.rand((h, w), dtype=torch.bfloat16)
+    torch_input_tensor1 = torch.rand((h, w), dtype=torch.bfloat16)
+
+    torch_output_tensor = torch.where(torch_input_tensor.to(torch.bool), torch_input_tensor1, scalar)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+    input_tensor = ttnn.to_device(input_tensor, device)
+    input_tensor1 = ttnn.from_torch(torch_input_tensor1, layout=ttnn.TILE_LAYOUT, device=device)
+    input_tensor1 = ttnn.to_device(input_tensor1, device)
+
+    output_tensor = ttnn.where(input_tensor, input_tensor1, scalar)
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor)
 
 
 def run_ternary_test_where_TST(device, h, w, scalar, ttnn_function, torch_function, pcc=0.9999):
