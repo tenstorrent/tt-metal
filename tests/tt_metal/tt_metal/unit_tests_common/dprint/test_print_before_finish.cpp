d9968e87a0190edccdbf96ebc6ea290385fc1e19
David Ma <davidma@tenstorrent.com>
2024-03-07 19:50:38 +0000

    #6095: Avoid closing L chip before R chip for PrintFinish test

--- tests/tt_metal/tt_metal/unit_tests_common/dprint/test_print_before_finish.cpp
+++ tests/tt_metal/tt_metal/unit_tests_common/dprint/test_print_before_finish.cpp
@@ -58,10 +58,11 @@ static void RunTest(DPrintFixture* fixture, Device* device) {
 }
 
 TEST_F(DPrintFixture, TestPrintFinish) {
-    for (Device* device : this->devices_) {
-        // TODO: #6095, fix this for fast dispatch remote device.
-        if (!this->slow_dispatch_ && device->id() > 0)
-            continue;
+    // Iterate devices in reverse for this test. Since the test closes the devices early, avoid
+    // closing L chip before R chip.
+    auto devices = this->devices_;
+    std::reverse(devices.begin(), devices.end());
+    for (Device* device : devices) {
         this->RunTestOnDevice(RunTest, device);
     }
 }
