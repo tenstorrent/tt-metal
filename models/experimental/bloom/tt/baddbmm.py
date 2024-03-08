b975b37ea851d2306d15d3d40c29efbbf8d26f46
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2024-03-08 17:49:20 +0000

    #4003: fixed bloom perf test

--- models/experimental/bloom/tt/baddbmm.py
+++ models/experimental/bloom/tt/baddbmm.py
@@ -5,21 +5,16 @@
 import tt_lib
 import models.experimental.bloom.bloom_utils as bloom_utils
 
-mem_config = tt_lib.tensor.MemoryConfig(
-    tt_lib.tensor.TensorMemoryLayout.INTERLEAVED, tt_lib.tensor.BufferType.L1
-)
+mem_config = tt_lib.tensor.MemoryConfig(tt_lib.tensor.TensorMemoryLayout.INTERLEAVED, tt_lib.tensor.BufferType.L1)
 
 
-def tt_baddbmm(
-    device, input, batch1, batch2, beta=1.0, alpha=1.0
-) -> tt_lib.tensor.Tensor:
+def tt_baddbmm(device, input, batch1, batch2, beta=1.0, alpha=1.0) -> tt_lib.tensor.Tensor:
     if beta != 1.0:
         input = tt_lib.tensor.mul(beta, input, output_mem_config=mem_config)
 
     tmp = bloom_utils.tt_bmm(batch1, batch2, device)
 
-    if alpha != 1.0:
-        tmp = tt_lib.tensor.mul(alpha, tmp, output_mem_config=mem_config)
+    tmp = tt_lib.tensor.mul(alpha, tmp, output_mem_config=mem_config)
 
     result = tt_lib.tensor.add(input, tmp, output_mem_config=mem_config)
 
