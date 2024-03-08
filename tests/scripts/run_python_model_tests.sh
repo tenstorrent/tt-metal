238ed92780145106eda0dcd7f492eee52a84817a
Michael Chiou <mchiou@tenstorrent.com>
2024-03-07 16:19:55 -0800

    #0: disable test_conv2d.py test_resnet_conv from ttnn unit_test
    #0: disable sd_test_conv
    
    #0: test black check

--- tests/scripts/run_python_model_tests.sh
+++ tests/scripts/run_python_model_tests.sh
@@ -20,6 +20,7 @@ fi
 
 if [ "$ARCH_NAME" != "wormhole_b0" ]; then
     env pytest $TT_METAL_HOME/tests/ttnn/unit_tests
+
     # Tests for tensors in L1
     pytest $TT_METAL_HOME/models/experimental/bert_large_performant/unit_tests/test_bert_large*matmul* -k in0_L1-in1_L1-bias_L1-out_L1
     pytest $TT_METAL_HOME/models/experimental/bert_large_performant/unit_tests/test_bert_large*bmm* -k in0_L1-in1_L1-out_L1
