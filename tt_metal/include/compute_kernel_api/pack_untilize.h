3a04d450c0d12a0cc09193dd3fb2f0899c7c27ab
rtawfik01 <rtawfik@tenstorrent.com>
2024-03-11 19:31:22 +0000

    #6193: Use pack untilize for update_cache

--- tt_metal/include/compute_kernel_api/pack_untilize.h
+++ tt_metal/include/compute_kernel_api/pack_untilize.h
@@ -40,16 +40,16 @@ ALWI void pack_untilize_init(uint32_t icb, uint32_t ocb)
 template <uint32_t block_ct_dim = 8>
 ALWI void pack_untilize_block(uint32_t icb, uint32_t block_rt_dim, uint32_t ocb) {
     for (uint32_t r = 0; r < block_rt_dim; ++r) {
-        MATH(( llk_math_wait_for_dest_available<SYNC>() ));
+        MATH(( llk_math_wait_for_dest_available<SyncHalf>() ));
         for (uint32_t c = 0; c < block_ct_dim; ++c) {
             UNPACK(( llk_unpack_A(icb, c) ));
             MATH(( llk_math_eltwise_unary_datacopy<A2D, BroadcastType::NONE, SyncHalf>(c) ));
         }
-        MATH(( llk_math_dest_section_done<SYNC>() ));
+        MATH(( llk_math_dest_section_done<SyncHalf>() ));
 
         PACK(( llk_packer_wait_for_math_done() ));
         PACK(( llk_pack_untilize<block_ct_dim>(1 /*num_blocks*/, ocb) ));
-        PACK(( llk_pack_dest_section_done<SYNC>() ));
+        PACK(( llk_pack_dest_section_done<SyncHalf>() ));
     }
 }
 
@@ -71,6 +71,14 @@ ALWI void pack_untilize_dst_init_short(uint32_t ocb, uint32_t face_r_dim = 16, u
 template <uint32_t block_ct_dim = 8, uint32_t full_ct_dim = block_ct_dim>
 ALWI void pack_untilize_dst(uint32_t ocb, uint32_t block_rt_dim = 1, uint32_t block_c_index = 0 /* used when full_ct_dim > block_ct_dim*/, uint32_t face_r_dim = 16, uint32_t num_faces = 4) {
     PACK(( llk_pack_untilize<block_ct_dim, full_ct_dim>(block_rt_dim, ocb, face_r_dim, num_faces, block_c_index) ));
+}
+
+template <uint32_t block_ct_dim = 8>
+ALWI void pack_untilize_init_short(uint32_t icb, uint32_t ocb)
+{
+    UNPACK(( llk_unpack_A_init(false, false, icb) )); // init must be after configure
+    MATH(( llk_math_eltwise_unary_datacopy_init<A2D, BroadcastType::NONE>(false /*transpose of faces*/, false /*transpose within 16x16 face*/, icb) ));
+    pack_untilize_dst_init_short<block_ct_dim>(ocb);
 }
 
 }
