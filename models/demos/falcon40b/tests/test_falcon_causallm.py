75eccd642f67d170f121298f5deb035f9a9f922d
Johanna Rock <jrock@tenstorrent.com>
2024-03-11 11:34:31 +0000

    #6241: Load only layer_nam many layers to avoid running out of RAM

--- models/demos/falcon40b/tests/test_falcon_causallm.py
+++ models/demos/falcon40b/tests/test_falcon_causallm.py
@@ -65,7 +65,9 @@ def run_test_FalconCausalLM_inference(
 ):
     model_name = model_location_generator(model_version, model_subdir="Falcon")
 
-    hugging_face_reference_model = FalconForCausalLM.from_pretrained(model_name, low_cpu_mem_usage=True)
+    hugging_face_reference_model = FalconForCausalLM.from_pretrained(
+        model_name, low_cpu_mem_usage=True, num_hidden_layers=num_layers
+    )
 
     hugging_face_reference_model.eval()
     configuration = hugging_face_reference_model.config
--- models/demos/falcon40b/tests/test_falcon_mlp.py
+++ models/demos/falcon40b/tests/test_falcon_mlp.py
@@ -46,7 +46,9 @@ def run_test_FalconMLP_inference(
 ):
     model_name = model_location_generator(model_version, model_subdir="Falcon")
 
-    hugging_face_reference_model = FalconForCausalLM.from_pretrained(model_name, low_cpu_mem_usage=True)
+    hugging_face_reference_model = FalconForCausalLM.from_pretrained(
+        model_name, low_cpu_mem_usage=True, num_hidden_layers=1
+    )
     hugging_face_reference_model.eval()
     configuration = hugging_face_reference_model.config
     state_dict = hugging_face_reference_model.state_dict()
