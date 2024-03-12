c1154ed61cf70a991c134bef3fa5c860ce8c9f80
Austin Ho <aho@tenstorrent.com>
2024-03-12 01:13:20 +0000

    #6197: Align pages for interleaved <-> sharded.

--- tt_eager/tt_dnn/op_library/sharded/kernels/dataflow/writer_unary_stick_layout_sharded_blocks_interleaved_start_id.cpp
+++ tt_eager/tt_dnn/op_library/sharded/kernels/dataflow/writer_unary_stick_layout_sharded_blocks_interleaved_start_id.cpp
@@ -11,8 +11,9 @@ void kernel_main() {
     const uint32_t stick_size               = get_arg_val<uint32_t>(1);
     const uint32_t block_height             = get_arg_val<uint32_t>(2);
     const uint32_t block_width_bytes        = get_arg_val<uint32_t>(3);
-    const uint32_t input_width_offset_bytes = get_arg_val<uint32_t>(4);
-    const uint32_t start_id                 = get_arg_val<uint32_t>(5);
+    const uint32_t padded_block_width_bytes = get_arg_val<uint32_t>(4);
+    const uint32_t input_width_offset_bytes = get_arg_val<uint32_t>(5);
+    const uint32_t start_id                 = get_arg_val<uint32_t>(6);
 
     constexpr uint32_t cb_id_out0 = get_compile_time_arg_val(0);
 
@@ -37,7 +38,7 @@ void kernel_main() {
         uint64_t dst_noc_addr = get_noc_addr(stick_id, s0, input_width_offset_bytes);
         noc_async_write(l1_read_addr, dst_noc_addr, block_width_bytes);
         stick_id++;
-        l1_read_addr += block_width_bytes;
+        l1_read_addr += padded_block_width_bytes;
         noc_async_write_barrier();
     }
     cb_pop_front(cb_id_out0, block_height);
