3653f9c09aa2041f358f505ca3597ff9861b630e
Ivan Hamer <ihamer@tenstorrent.com>
2024-03-13 10:24:35 +0000

    #6347: Print built-in defines once only

--- tt_metal/jit_build/kernel_args.hpp
+++ tt_metal/jit_build/kernel_args.hpp
@@ -11,7 +11,7 @@ using namespace tt;
 namespace tt::tt_metal {
 
 // Log current kernel compile-time arguments
-void log_kernel_defines_and_args (const string &out_kernel_root_path, const string &full_kernel_name, const string &built_in_defines, const string &defines_and_args_str);
+void log_kernel_defines_and_args (const string &out_dir, const string &full_kernel_name, const string &defines_and_args_str);
 // Dump all kernel compile-time arguments to a file
 void dump_kernel_defines_and_args(const string &out_kernel_root_path);
 
