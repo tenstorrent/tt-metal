1cf6076bfd2da8ce6ef0e2700eecbe1c930b93a8
hschoi <hschoi4448@gmail.com>
2024-03-15 04:04:40 +0000

    #6341: moreh_arange support row_major output

--- tt_eager/tt_dnn/op_library/moreh_arange/moreh_arange_op.hpp
+++ tt_eager/tt_dnn/op_library/moreh_arange/moreh_arange_op.hpp
@@ -20,29 +20,35 @@ struct MorehArange {
     float start;
     float end;
     float step;
+    bool untilize_out;
+    const DataType output_dtype;
     const CoreRange core_range;  // unused for now
     const MemoryConfig output_mem_config;
-    const bool inplace;
 
-    void validate(const std::vector<Tensor> &input_tensors) const;
+    void validate_with_output_tensors(const std::vector<Tensor> &input_tensors, const std::vector<std::optional<Tensor>>& output_tensors) const;
     std::vector<Shape> compute_output_shapes(const std::vector<Tensor> &input_tensors) const;
-    std::vector<Tensor> create_output_tensors(const std::vector<Tensor> &input_tensors) const;
-    operation::ProgramWithCallbacks create_program(
-        const std::vector<Tensor> &input_tensors, std::vector<Tensor> &output_tensors) const;
+    std::vector<Tensor> create_output_tensors(const std::vector<Tensor> &input_tensors, const std::vector<std::optional<Tensor>>& output_tensors) const;
+    operation::ProgramWithCallbacks create_program(const std::vector<Tensor>& input_tensors, std::vector<Tensor> &output_tensors) const;
     static constexpr auto attribute_names =
-        std::make_tuple("start", "end", "step", "output_mem_config");
+        std::make_tuple("start", "end", "step", "untilize_out", "output_dtype", "output_mem_config");
     const auto attribute_values() const {
         return std::make_tuple(
             std::cref(this->start),
             std::cref(this->end),
             std::cref(this->step),
+            std::cref(this->untilize_out),
+            std::cref(this->output_dtype),
             std::cref(this->output_mem_config));
     }
 };
 
-Tensor moreh_arange(float start, float end, float step, const Tensor& any, const MemoryConfig &output_mem_config = operation::DEFAULT_OUTPUT_MEMORY_CONFIG);
+Tensor moreh_arange(float start, float end, float step,
+    const Tensor& any,
+    std::optional<Tensor> output_tensor = std::nullopt,
+    bool untilize_out = false,
+    std::optional<DataType> output_dtype=std::nullopt,
+    const MemoryConfig &output_mem_config = operation::DEFAULT_OUTPUT_MEMORY_CONFIG);
 
-Tensor moreh_arange_inplace(Tensor &input_tensor, float start, float end, float step);
 
 }  // namespace primary
 }  // namespace operations
