b4f2823f7c7a800137051f417346612ded690fd4
mikevin920 <mikevin920@yahoo.com>
2024-03-18 12:24:54 -0500

    #6503: added 8-chip specific test suite

--- models/demos/llama2_70b/scripts/llama_test_suite_T3000.sh
+++ models/demos/llama2_70b/scripts/llama_test_suite_T3000.sh
@@ -16,9 +16,3 @@ pytest -svv models/demos/llama2_70b/tests/test_llama_decoder.py::test_LlamaDecod
 pytest -svv models/demos/llama2_70b/tests/test_llama_model.py::test_LlamaModel_inference[BFLOAT16-DRAM-decode-8chip-T3000-0.999-1]
 
 pytest -svv models/demos/llama2_70b/tests/test_llama_model.py::test_LlamaModel_inference[BFLOAT16-DRAM-decode-8chip-T3000-0.998-2]
-
-pytest -svv models/demos/llama2_70b/tests/test_llama_mlp.py::test_LlamaMLP_inference[BFLOAT16-DRAM-0.9999-decode-32chip-emulated]
-
-pytest -svv models/demos/llama2_70b/tests/test_llama_attention.py::test_LlamaAttention_inference[BFLOAT16-DRAM-0.9997-decode-32chip-emulated]
-
-pytest -svv models/demos/llama2_70b/tests/test_llama_decoder.py::test_LlamaDecoder_inference[BFLOAT16-DRAM-0.999-decode-32chip-emulated]
