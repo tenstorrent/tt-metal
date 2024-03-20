0dc6f80a67c97dfc0b40b8ea2ef848ba46181801
Nitika Shanker <nshanker@tenstorrent.com>
2024-03-20 21:30:39 +0000

    #0: more cleanup

--- models/experimental/functional_stable_diffusion/tt2/ttnn_functional_downblock_2d.py
+++ models/experimental/functional_stable_diffusion/tt2/ttnn_functional_downblock_2d.py
@@ -77,7 +77,6 @@ class downblock2d:
                 up=False,
                 down=False,
                 dtype=dtype,
-                dump_to_file=i == 0,
             )
 
             output_states += (hidden_states,)
