a06478afc92459626535736c20baddc600ff77b0
Radomir Djogo <rdjogo@tenstorrent.com>
2024-03-21 19:38:34 +0000

    change includes in order to remove additional include path from build

--- tt_llk_wormhole_b0/common/inc/ckernel_sfpu.h
+++ tt_llk_wormhole_b0/common/inc/ckernel_sfpu.h
@@ -2,7 +2,6 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-
 #pragma once
 
 #include "ckernel_defs.h"
@@ -13,32 +12,32 @@
 
 #include "sfpi.h"
 
-#include "ckernel_sfpu_abs.h"
-#include "ckernel_sfpu_add_int32.h"
-#include "ckernel_sfpu_cast_fp32_to_fp16a.h"
-#include "ckernel_sfpu_clamp.h"
-#include "ckernel_sfpu_comp.h"
-#include "ckernel_sfpu_dropout.h"
-#include "ckernel_sfpu_exp.h"
-#include "ckernel_sfpu_gelu.h"
-#include "ckernel_sfpu_hardtanh.h"
-#include "ckernel_sfpu_is_fp16_zero.h"
-#include "ckernel_sfpu_load_config.h"
-#include "ckernel_sfpu_log.h"
-#include "ckernel_sfpu_max_int32.h"
-#include "ckernel_sfpu_max.h"
-#include "ckernel_sfpu_power.h"
-#include "ckernel_sfpu_quant.h"
-#include "ckernel_sfpu_recip.h"
-#include "ckernel_sfpu_relu.h"
-#include "ckernel_sfpu_sigmoid.h"
-#include "ckernel_sfpu_sign.h"
-#include "ckernel_sfpu_sqrt.h"
-#include "ckernel_sfpu_square.h"
-#include "ckernel_sfpu_tanh_derivative.h"
-#include "ckernel_sfpu_tanh.h"
-#include "ckernel_sfpu_topk.h"
-#include "ckernel_sfpu_trigonometry.h"
+#include "sfpu/ckernel_sfpu_abs.h"
+#include "sfpu/ckernel_sfpu_add_int32.h"
+#include "sfpu/ckernel_sfpu_cast_fp32_to_fp16a.h"
+#include "sfpu/ckernel_sfpu_clamp.h"
+#include "sfpu/ckernel_sfpu_comp.h"
+#include "sfpu/ckernel_sfpu_dropout.h"
+#include "sfpu/ckernel_sfpu_exp.h"
+#include "sfpu/ckernel_sfpu_gelu.h"
+#include "sfpu/ckernel_sfpu_hardtanh.h"
+#include "sfpu/ckernel_sfpu_is_fp16_zero.h"
+#include "sfpu/ckernel_sfpu_load_config.h"
+#include "sfpu/ckernel_sfpu_log.h"
+#include "sfpu/ckernel_sfpu_max_int32.h"
+#include "sfpu/ckernel_sfpu_max.h"
+#include "sfpu/ckernel_sfpu_power.h"
+#include "sfpu/ckernel_sfpu_quant.h"
+#include "sfpu/ckernel_sfpu_recip.h"
+#include "sfpu/ckernel_sfpu_relu.h"
+#include "sfpu/ckernel_sfpu_sigmoid.h"
+#include "sfpu/ckernel_sfpu_sign.h"
+#include "sfpu/ckernel_sfpu_sqrt.h"
+#include "sfpu/ckernel_sfpu_square.h"
+#include "sfpu/ckernel_sfpu_tanh_derivative.h"
+#include "sfpu/ckernel_sfpu_tanh.h"
+#include "sfpu/ckernel_sfpu_topk.h"
+#include "sfpu/ckernel_sfpu_trigonometry.h"
 
 // using namespace sfpi;
 
