31384063dbec37731718e4878dc0f456c80807cc
Aleks Knezevic <aknezevic@tenstorrent.com>
2024-03-21 01:52:48 +0000

    #0: upsample requires NCHW

--- models/experimental/functional_stable_diffusion/tt2/ttnn_functional_upsample_nearest_2d.py
+++ models/experimental/functional_stable_diffusion/tt2/ttnn_functional_upsample_nearest_2d.py
@@ -9,9 +9,8 @@ import ttnn
 def upsample_nearest2d(input, scale_factor=2.0):
     assert scale_factor % 1 == 0 and scale_factor > 0, "We only support scaling by positive integer values"
 
-    # up_output = ttnn.repeat_interleave(input, scale_factor, dim=3)
-    # up_output = ttnn.repeat_interleave(up_output, scale_factor, dim=2)
-
-    ## permute to NHWC
+    # input is in N, 1, HW, C, upsample expects, [N, H, W, C]
+    # set h_scale to 1, w_scale to scale_factor, c_scale to 1
+    # scale_factor = (1, scale_factor*2, 1)
     up_output = ttnn.upsample(input, scale_factor)
     return up_output
