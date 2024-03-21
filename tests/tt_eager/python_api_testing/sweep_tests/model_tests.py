6aec96c78d9cc9c83b172fb58ae07bb2e7886316
banekg <barsic@tenstorrent.com>
2024-03-21 15:30:34 +0000

    #5589: Move conv tests for prepocessing_model to broken folder

--- tests/tt_eager/python_api_testing/sweep_tests/model_tests.py
+++ tests/tt_eager/python_api_testing/sweep_tests/model_tests.py
@@ -105,7 +105,8 @@ def run_conv_conv(model, torch_input_tensor):
     input_tensor = ttnn.from_torch(input_tensor, dtype=ttnn.bfloat16)
 
     input_tensor = model.conv1.copy_input_to_device(input_tensor)
-    output_tensor = model(input_tensor)
+    output_tensor = model.conv1(input_tensor)
+    output_tensor = model.conv2(output_tensor)
     output_tensor = model.conv2.copy_output_from_device(output_tensor)
 
     output_tensor = ttnn.to_torch(output_tensor)
@@ -122,10 +123,12 @@ def run_conv_relu_conv(model, torch_input_tensor, device):
     output_tensor = model.conv1.copy_input_to_device(input_tensor)
     output_tensor = model.conv1(output_tensor)
     output_tensor = model.conv1.copy_output_from_device(output_tensor)
+
     output_tensor = ttnn.to_device(output_tensor, device=device)
     output_tensor = ttnn.to_layout(output_tensor, ttnn.TILE_LAYOUT)
     output_tensor = ttnn.relu(output_tensor)
     output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+
     output_tensor = model.conv2.copy_input_to_device(output_tensor)
     output_tensor = model.conv2(output_tensor)
     output_tensor = model.conv2.copy_output_from_device(output_tensor)
@@ -135,47 +138,3 @@ def run_conv_relu_conv(model, torch_input_tensor, device):
     output_tensor = torch.reshape(output_tensor, torch_input_tensor.shape)
     output_tensor = output_tensor.to(torch_input_tensor.dtype)
     return output_tensor
-
-
-# from models.demos.bert.tt import ttnn_bert
-# from models.demos.bert.tt import ttnn_optimized_bert
-# from ttnn.model_preprocessing import preprocess_model_parameters
-
-# def run_ttnn_bert_model():
-#     model_name = "phiyodr/bert-large-finetuned-squad2"
-
-#     config = transformers.BertConfig.from_pretrained(model_name)
-#     config.num_hidden_layers = 1
-
-#     batch_size = 8
-#     sequence_size = 384
-
-#     parameters = preprocess_model_parameters(
-#         initialize_model=lambda: transformers.BertForQuestionAnswering.from_pretrained(
-#             model_name, config=config
-#         ).eval(),
-#         custom_preprocessor=bert.custom_preprocessor,
-#         device=device,
-#     )
-
-#     with trace():
-#         input_ids = torch.randint(0, config.vocab_size, (batch_size, sequence_size)).to(torch.int32)
-#         torch_token_type_ids = torch.zeros((batch_size, sequence_size), dtype=torch.int32)
-#         torch_attention_mask = torch.zeros(1, sequence_size) if bert == ttnn_optimized_bert else None
-
-
-#         ttnn_bert_inputs = bert.preprocess_inputs(
-#             input_ids,
-#             torch_token_type_ids,
-#             torch_attention_mask,
-#             device=device,
-#         )
-
-#         output = bert.bert_for_question_answering(
-#             config,
-#             *ttnn_bert_inputs,
-#             parameters=parameters,
-#         )
-#         output = ttnn.from_device(output)
-
-#     return output
