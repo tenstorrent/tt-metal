a3432dd8c09c882a84fd8821c78b73e25391f60f
Shwetank Singh <ssingh@tenstorrent.com>
2024-03-22 05:54:39 +0000

    #6662: Width Sharding support for eltwise OP

--- tests/ttnn/unit_tests/operations/test_silu.py
+++ tests/ttnn/unit_tests/operations/test_silu.py
@@ -15,57 +15,13 @@ import tt_lib as ttl
 import ttnn
 
 from tests.ttnn.utils_for_testing import check_with_pcc_without_tensor_printout
+from tests.ttnn.ttnn_utility_fuction import get_shard_grid_from_num_cores
 from models.utility_functions import skip_for_wormhole_b0, skip_for_grayskull
 from tt_lib.utils import (
     _nearest_y,
 )
 
 TILE_WIDTH = 32
-
-
-def get_shard_grid_from_num_cores(ncores: Union[int, Tuple[int, int]], device) -> ttnn.experimental.tensor.CoreRangeSet:
-    max_grid_size = (8, 8) if device.arch() == ttl.device.Arch.WORMHOLE_B0 else (9, 12)  ## (y, x)
-    if isinstance(ncores, int):
-        if ncores % max_grid_size[1] == 0:
-            core_grid = ttnn.CoreGrid(y=ncores // max_grid_size[1], x=max_grid_size[1])
-            grid_coord = ttnn.experimental.tensor.CoreCoord(core_grid.x - 1, core_grid.y - 1)
-            return ttnn.experimental.tensor.CoreRangeSet(
-                {ttnn.experimental.tensor.CoreRange(ttnn.experimental.tensor.CoreCoord(0, 0), grid_coord)}
-            )
-        else:
-            if ncores < max_grid_size[1]:
-                core_grid = ttnn.CoreGrid(y=1, x=ncores)
-                grid_coord = ttnn.experimental.tensor.CoreCoord(core_grid.x - 1, 0)
-                return ttnn.experimental.tensor.CoreRangeSet(
-                    {ttnn.experimental.tensor.CoreRange(ttnn.experimental.tensor.CoreCoord(0, 0), grid_coord)}
-                )
-            else:
-                core_grid_1 = ttnn.CoreGrid(y=ncores // max_grid_size[1], x=max_grid_size[1])
-                core_grid_2 = ttnn.CoreGrid(y=ncores // max_grid_size[1] + 1, x=ncores % max_grid_size[1])
-                grid_coord_1 = ttnn.experimental.tensor.CoreCoord(core_grid_1.x - 1, core_grid_1.y - 1)
-                grid_coord_2 = ttnn.experimental.tensor.CoreCoord(core_grid_2.x - 1, core_grid_2.y - 1)
-                return ttnn.experimental.tensor.CoreRangeSet(
-                    {
-                        ttnn.experimental.tensor.CoreRange(ttnn.experimental.tensor.CoreCoord(0, 0), grid_coord_1),
-                        ttnn.experimental.tensor.CoreRange(
-                            ttnn.experimental.tensor.CoreCoord(0, grid_coord_2.y), grid_coord_2
-                        ),
-                    }
-                )
-    elif isinstance(ncores, tuple):
-        ncores_h, ncores_w = ncores
-        assert ncores_h <= max_grid_size[0]
-        assert ncores_w <= max_grid_size[1]
-        return ttnn.experimental.tensor.CoreRangeSet(
-            {
-                ttnn.experimental.tensor.CoreRange(
-                    ttnn.experimental.tensor.CoreCoord(0, 0),
-                    ttnn.experimental.tensor.CoreCoord(ncores_w - 1, ncores_h - 1),
-                )
-            }
-        )
-    else:
-        raise ValueError("Invalid ncores")
 
 
 def run_elt_silu_relu(
@@ -111,14 +67,18 @@ def run_elt_silu_relu(
         shard_width = input_2d_width
         shard_orientation = ttnn.experimental.tensor.ShardOrientation.ROW_MAJOR
         tensor_memory_layout = ttnn.types.TensorMemoryLayout.HEIGHT_SHARDED
+    elif shard_strategy == ttnn.ShardStrategy.WIDTH:
+        shard_height = input_2d_height
+        input_2d_width_padded = _nearest_y(input_2d_width, ncores * 32)
+        shard_width = math.ceil(input_2d_width_padded / ncores)
+        shard_orientation = ttnn.experimental.tensor.ShardOrientation.ROW_MAJOR
+        tensor_memory_layout = ttnn.types.TensorMemoryLayout.WIDTH_SHARDED
 
     assert shard_height % TILE_WIDTH == 0
     assert shard_width % TILE_WIDTH == 0
 
-    core_grid = ttnn.CoreGrid(y=shard_grid[1], x=shard_grid[0])
     shard_grid = get_shard_grid_from_num_cores(ncores, device)
     logger.debug(f"shard_grid={shard_grid}")
-    logger.debug(f"grid_size={core_grid}")
     logger.debug(f"input_shard_height={shard_height}, input_shard_width={shard_width}")
 
     shard_spec = ttnn.experimental.tensor.ShardSpec(shard_grid, (shard_height, shard_width), shard_orientation, False)
@@ -158,6 +118,9 @@ def run_elt_silu_relu(
         (8, 512, 28, 28, 32, (4, 8), ttnn.ShardStrategy.BLOCK, ttnn.ShardOrientation.COLUMN_MAJOR),
         (8, 1024, 14, 14, 56, (7, 8), ttnn.ShardStrategy.BLOCK, ttnn.ShardOrientation.COLUMN_MAJOR),
         (16, 256, 56, 56, 98, (12, 9), ttnn.ShardStrategy.HEIGHT, ttnn.ShardOrientation.ROW_MAJOR),
+        (1, 5120, 32, 1, 32, (1, 32), ttnn.ShardStrategy.WIDTH, ttnn.ShardOrientation.ROW_MAJOR),
+        (1, 1024, 64, 1, 32, (1, 32), ttnn.ShardStrategy.WIDTH, ttnn.ShardOrientation.ROW_MAJOR),
+        (2, 10240, 64, 1, 64, (1, 64), ttnn.ShardStrategy.WIDTH, ttnn.ShardOrientation.ROW_MAJOR),
     ),
 )
 @pytest.mark.parametrize("op", ["silu", "relu"])
@@ -196,10 +159,44 @@ def test_gs_silu_relu(
         (8, 512, 28, 28, 32, (4, 8), ttnn.ShardStrategy.BLOCK, ttnn.ShardOrientation.COLUMN_MAJOR),
         (8, 1024, 14, 14, 56, (7, 8), ttnn.ShardStrategy.BLOCK, ttnn.ShardOrientation.COLUMN_MAJOR),
         (8, 256, 56, 56, 56, (7, 8), ttnn.ShardStrategy.HEIGHT, ttnn.ShardOrientation.ROW_MAJOR),
+        (1, 5120, 32, 1, 32, (1, 32), ttnn.ShardStrategy.WIDTH, ttnn.ShardOrientation.ROW_MAJOR),
+        (1, 1024, 64, 1, 32, (1, 32), ttnn.ShardStrategy.WIDTH, ttnn.ShardOrientation.ROW_MAJOR),
+        (2, 10240, 64, 1, 32, (1, 32), ttnn.ShardStrategy.WIDTH, ttnn.ShardOrientation.ROW_MAJOR),
     ),
 )
 @pytest.mark.parametrize("op", ["silu", "relu"])
 def test_wh_silu_relu(
+    device,
+    batch_size,
+    input_channels,
+    input_height,
+    input_width,
+    shard_grid,
+    ncores,
+    shard_strategy,
+    shard_orientation,
+    op,
+):
+    run_elt_silu_relu(
+        device,
+        batch_size,
+        input_channels,
+        input_height,
+        input_width,
+        shard_grid,
+        ncores,
+        shard_strategy,
+        shard_orientation,
+        op,
+    )
+
+
+@pytest.mark.parametrize(
+    "batch_size, input_channels, input_height, input_width, ncores, shard_grid, shard_strategy, shard_orientation",
+    ((1, 5120, 32, 1, 32, (1, 32), ttnn.ShardStrategy.WIDTH, ttnn.ShardOrientation.ROW_MAJOR),),
+)
+@pytest.mark.parametrize("op", ["silu", "relu"])
+def test_silu_llm(
     device,
     batch_size,
     input_channels,
