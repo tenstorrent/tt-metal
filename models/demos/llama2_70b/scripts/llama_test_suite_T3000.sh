e7530c31bcd7daa2d2d130dce53bfc1e2f559420
mikevin920 <mikevin920@yahoo.com>
2024-03-27 17:06:02 +0000

    #6549: Add prefill tests to the test suite

--- models/demos/llama2_70b/scripts/llama_test_suite_T3000.sh
+++ models/demos/llama2_70b/scripts/llama_test_suite_T3000.sh
@@ -11,3 +11,13 @@ pytest models/demos/llama2_70b/tests/test_llama_mlp.py::test_LlamaMLP_inference[
 pytest models/demos/llama2_70b/tests/test_llama_attention.py::test_LlamaAttention_inference[decode-8chip-T3000]
 pytest models/demos/llama2_70b/tests/test_llama_decoder.py::test_LlamaDecoder_inference[decode-8chip-T3000]
 pytest models/demos/llama2_70b/tests/test_llama_model.py::test_LlamaModel_inference[decode-8chip-T3000-1L]
+
+pytest models/demos/llama2_70b/tests/test_llama_mlp.py::test_LlamaMLP_inference[prefill_128-8chip-T3000]
+pytest models/demos/llama2_70b/tests/test_llama_attention.py::test_LlamaAttention_inference[prefill_128-8chip-T3000]
+pytest models/demos/llama2_70b/tests/test_llama_decoder.py::test_LlamaDecoder_inference[prefill_128-8chip-T3000]
+pytest models/demos/llama2_70b/tests/test_llama_model.py::test_LlamaModel_inference[prefill_128-8chip-T3000-1L]
+
+pytest models/demos/llama2_70b/tests/test_llama_mlp.py::test_LlamaMLP_inference[prefill_2k-8chip-T3000]
+pytest models/demos/llama2_70b/tests/test_llama_attention.py::test_LlamaAttention_inference[prefill_2k-8chip-T3000]
+pytest models/demos/llama2_70b/tests/test_llama_decoder.py::test_LlamaDecoder_inference[prefill_2k-8chip-T3000]
+pytest models/demos/llama2_70b/tests/test_llama_model.py::test_LlamaModel_inference[prefill_2k-8chip-T3000-1L]
