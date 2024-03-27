292b6169f6424ed2688c9f811ba96df27b72b7f5
mikevin920 <mikevin920@yahoo.com>
2024-03-27 17:06:02 +0000

    #6549: Add prefill tests to the test suite

--- models/demos/llama2_70b/scripts/llama_test_suite_emulated.sh
+++ models/demos/llama2_70b/scripts/llama_test_suite_emulated.sh
@@ -15,3 +15,8 @@ pytest models/demos/llama2_70b/tests/test_llama_model.py::test_LlamaModel_infere
 pytest models/demos/llama2_70b/tests/test_llama_mlp.py::test_LlamaMLP_inference[decode-32chip-emulated]
 pytest models/demos/llama2_70b/tests/test_llama_attention.py::test_LlamaAttention_inference[decode-32chip-emulated]
 pytest models/demos/llama2_70b/tests/test_llama_decoder.py::test_LlamaDecoder_inference[decode-32chip-emulated]
+
+pytest models/demos/llama2_70b/tests/test_llama_mlp.py::test_LlamaMLP_inference[prefill_128-8chip-emulated]
+pytest models/demos/llama2_70b/tests/test_llama_attention.py::test_LlamaAttention_inference[prefill_128-8chip-emulated]
+pytest models/demos/llama2_70b/tests/test_llama_decoder.py::test_LlamaDecoder_inference[prefill_128-8chip-emulated]
+pytest models/demos/llama2_70b/tests/test_llama_model.py::test_LlamaModel_inference[prefill_128-8chip-emulated-1L]
