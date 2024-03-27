0d71e130e1523cb7313f795469b5ed2b0520ded0
ruthreshk <ruthresh@multicorewareinc.com>
2024-03-27 07:46:49 +0000

    #6443: update hard related test ops

--- tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_hardtanh.py
+++ tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_hardtanh.py
@@ -5,7 +5,7 @@
 import torch
 import pytest
 import tt_lib
-from tests.tt_eager.python_api_testing.unit_testing.backward_ops.utility_funcs import data_gen_pt_tt, compare_results
+from tests.tt_eager.python_api_testing.unit_testing.backward_ops.utility_funcs import data_gen_with_range, compare_pcc
 
 
 @pytest.mark.parametrize(
@@ -16,9 +16,9 @@ from tests.tt_eager.python_api_testing.unit_testing.backward_ops.utility_funcs i
         (torch.Size([1, 3, 320, 384])),
     ),
 )
-def test_bw_asin(input_shapes, device):
-    in_data, input_tensor = data_gen_pt_tt(input_shapes, device, True)
-    grad_data, grad_tensor = data_gen_pt_tt(input_shapes, device)
+def test_bw_hardtanh(input_shapes, device):
+    in_data, input_tensor = data_gen_with_range(input_shapes, -100, 100, device, True)
+    grad_data, grad_tensor = data_gen_with_range(input_shapes, -100, 100, device)
     min_val = -1
     max_val = 1
     pyt_y = torch.nn.functional.hardtanh(in_data, min_val, max_val)
@@ -30,5 +30,5 @@ def test_bw_asin(input_shapes, device):
     pyt_y.backward(gradient=grad_data)
 
     golden_tensor = [in_data.grad]
-    comp_pass = compare_results(tt_output_tensor_on_device, golden_tensor)
+    comp_pass = compare_pcc(tt_output_tensor_on_device, golden_tensor)
     assert comp_pass
