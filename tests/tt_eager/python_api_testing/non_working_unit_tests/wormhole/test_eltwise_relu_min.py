a9c56f3ddec1885572ac54b134599852b870f94c
banekg <barsic@tenstorrent.com>
2024-03-27 09:10:01 +0000

    #6586: Add bfloat8_b and L1 to exp, log, relu, relu_min, relu_max ops

---
+++ tests/tt_eager/python_api_testing/non_working_unit_tests/wormhole/test_eltwise_relu_min.py
@@ -0,0 +1,78 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from loguru import logger
+import pytest
+import torch
+import tt_lib as ttl
+
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc
+from tests.tt_eager.python_api_testing.sweep_tests.pytorch_ops import relu_min as pt_relu_min
+from tests.tt_eager.python_api_testing.sweep_tests.tt_lib_ops import eltwise_relu_min as tt_relu_min
+
+
+def run_eltwise_relu_min_tests(
+    input_shape, dtype, dlayout, in_mem_config, out_mem_config, lower_limit, data_seed, device
+):
+    torch.manual_seed(data_seed)
+
+    x = torch.Tensor(size=input_shape).uniform_(-100, 100).to(torch.bfloat16)
+    x_ref = x.detach().clone()
+
+    # get referent value
+    ref_value = pt_relu_min(x_ref, lower_limit=lower_limit)
+
+    # calculate tt output
+    if in_mem_config == "SYSTEM_MEMORY":
+        in_mem_config = None
+
+    tt_result = tt_relu_min(
+        x=x,
+        lower_limit=lower_limit,
+        device=device,
+        dtype=[dtype],
+        layout=[dlayout],
+        input_mem_config=[in_mem_config],
+        output_mem_config=out_mem_config,
+    )
+
+    # compare tt and golden outputs
+    success, pcc_value = comp_pcc(ref_value, tt_result)
+    logger.debug(pcc_value)
+
+    assert success
+
+
+test_sweep_args = [
+    (
+        (4, 2, 96, 160),
+        ttl.tensor.DataType.BFLOAT8_B,
+        ttl.tensor.Layout.TILE,
+        "SYSTEM_MEMORY",
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1),
+        99.5,
+        12915139,
+    ),
+    (
+        (3, 10, 64, 96),
+        ttl.tensor.DataType.BFLOAT8_B,
+        ttl.tensor.Layout.TILE,
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1),
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        97.5,
+        8726038,
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "input_shape, dtype, dlayout, in_mem_config, out_mem_config, lower_limit, data_seed",
+    (test_sweep_args),
+)
+def test_eltwise_relu_min_test(
+    input_shape, dtype, dlayout, in_mem_config, out_mem_config, lower_limit, data_seed, device
+):
+    run_eltwise_relu_min_tests(
+        input_shape, dtype, dlayout, in_mem_config, out_mem_config, lower_limit, data_seed, device
+    )
