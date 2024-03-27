b3eefdf5d69e2d26a328030047e3e4ccac0cd0af
umadevimcw <umadevi@multicorewareinc.com>
2024-03-27 10:15:49 +0000

    #6633: Updates test files for ldexp

--- tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_ldexp.py
+++ tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_ldexp.py
@@ -5,7 +5,7 @@
 import torch
 import pytest
 import tt_lib
-from tests.tt_eager.python_api_testing.unit_testing.backward_ops.utility_funcs import data_gen_pt_tt, compare_results
+from tests.tt_eager.python_api_testing.unit_testing.backward_ops.utility_funcs import data_gen_with_range, compare_pcc
 
 
 @pytest.mark.parametrize(
@@ -17,10 +17,10 @@ from tests.tt_eager.python_api_testing.unit_testing.backward_ops.utility_funcs i
     ),
 )
 def test_bw_ldexp(input_shapes, device):
-    in_data, input_tensor = data_gen_pt_tt(input_shapes, device, True)
-    other_data, other_tensor = data_gen_pt_tt(input_shapes, device, True)
+    in_data, input_tensor = data_gen_with_range(input_shapes, -10, 10, device, True)
+    other_data, other_tensor = data_gen_with_range(input_shapes, -20, 20, device, True)
 
-    grad_data, grad_tensor = data_gen_pt_tt(input_shapes, device)
+    grad_data, grad_tensor = data_gen_with_range(input_shapes, -5, 5, device)
 
     tt_output_tensor_on_device = tt_lib.tensor.ldexp_bw(grad_tensor, input_tensor, other_tensor)
 
@@ -32,5 +32,5 @@ def test_bw_ldexp(input_shapes, device):
     pyt_y.backward(gradient=grad_data)
 
     golden_tensor = [in_data.grad, other_data.grad]
-    comp_pass = compare_results(tt_output_tensor_on_device, golden_tensor)
+    comp_pass = compare_pcc(tt_output_tensor_on_device, golden_tensor)
     assert comp_pass
