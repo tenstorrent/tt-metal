f1dc40356e4411e04913eef99cb7c02608edae88
Tony Zhou <tzhou@tenstorrent.com>
2024-03-28 18:40:12 +0000

    #5830: Implement host API trace capture on FD2

--- tt_metal/impl/trace/trace.cpp
+++ tt_metal/impl/trace/trace.cpp
@@ -2,20 +2,40 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include <algorithm>  // for copy() and assign()
-#include <iterator>   // for back_inserter
 #include <memory>
 #include <string>
 
+#include "logger.hpp"
 #include "tt_metal/host_api.hpp"
 #include "tt_metal/impl/dispatch/command_queue.hpp"
 
 namespace tt::tt_metal {
 
-Trace::Trace() : trace_complete(false), num_data_bytes(0) { this->cq = std::make_unique<CommandQueue>(this); }
+// List of supported commands for tracing
+const unordered_set<EnqueueCommandType> trace_supported_commands = {
+    EnqueueCommandType::ENQUEUE_PROGRAM,
+};
+
+unordered_map<uint32_t, shared_ptr<Buffer>> Trace::buffer_pool;
+std::mutex Trace::pool_mutex;
+
+Trace::Trace() : capture_complete(false), num_data_bytes(0), state(TraceState::EMPTY) {
+    this->tq = std::make_unique<CommandQueue>(*this);
+}
+
+void Trace::begin_capture() {
+    TT_FATAL(this->state == TraceState::EMPTY, "Cannot begin capture in a non-empty state");
+    TT_FATAL(this->queue().empty(), "Cannot begin trace on one that already captured commands");
+    this->state = TraceState::CAPTURING;
+}
+
+void Trace::end_capture() {
+    TT_FATAL(this->state == TraceState::CAPTURING, "Cannot end capture that has not begun");
+    this->validate();
+    this->state = TraceState::CAPTURED;
+}
 
 void Trace::record(const TraceNode& trace_node) {
-    TT_FATAL(not this->trace_complete, "Cannot record any more for a completed trace");
     this->num_data_bytes += trace_node.num_data_bytes;
     this->history.push_back(trace_node);
 }
@@ -25,17 +45,21 @@ void Trace::validate() {
         if (cmd.blocking.has_value()) {
             TT_FATAL(cmd.blocking.value() == false, "Blocking commands are not supported in traces");
         }
+        if (trace_supported_commands.find(cmd.type) == trace_supported_commands.end()) {
+            TT_THROW("Unsupported command type for tracing");
+        }
     }
 }
 
-uint32_t Trace::next_trace_id() {
+uint32_t Trace::next_id() {
     static uint32_t global_trace_id = 0;
     return global_trace_id++;
 }
 
 uint32_t Trace::instantiate(CommandQueue& cq) {
-    uint32_t trace_id = next_trace_id();
-    cq.trace_ptr = this;
+    this->state = TraceState::INSTANTIATING;
+    uint32_t tid = next_id();
+    TT_FATAL(this->has_instance(tid) == false, "Trace ID " + std::to_string(tid) + " already exists");
 
     // Stage the trace commands into device DRAM that the command queue will read from
     // - flatten commands into tightly packed data structure
@@ -43,12 +67,68 @@ uint32_t Trace::instantiate(CommandQueue& cq) {
     // - commit the DRAM buffer via an enqueue WB command
     // - map the trace id to the DRAM buffer for later enqueue Trace
 
-    if (trace_instances.count(trace_id)) {
-        TT_THROW("Trace ID " + std::to_string(trace_id) + " already exists");
+    for (auto cmd : this->queue().worker_queue) {
+        TT_FATAL(
+            trace_supported_commands.find(cmd.type) != trace_supported_commands.end(),
+            "Unsupported command type found in trace");
+        cmd.trace = *this;
+        // #6024: Trace command flattening to a buffer should avoid using CQ
+        // however the use of it offloads work to a worker thread for speedup
+        // while can be queued up behind other commands and requires sync before
+        // yielding back to the main thread (eg. this->history usage below requires wait_until_empty)
+        log_debug(LogMetalTrace, "Trace::instantiate found command {}", cmd.type);
+        cq.run_command(cmd);
     }
+    cq.wait_until_empty();
 
-    trace_instances.insert(trace_id);
-    return trace_id;
+    vector<uint32_t> trace_data;
+
+    SystemMemoryManager& manager = cq.hw_command_queue().manager;
+    uint32_t data_size = 0;
+    for (const auto& node : this->history) {
+        trace_data.insert(trace_data.end(), node.data.begin(), node.data.end());
+        data_size += node.num_data_bytes;
+    }
+    log_debug(LogMetalTrace, "Trace data size = {}, trace num_bytes = {}", data_size, this->num_data_bytes);
+    TT_FATAL(data_size == this->num_data_bytes, "Data size mismatch in trace");
+
+    auto trace_buffer = std::make_shared<Buffer>(
+        cq.device(),
+        this->num_data_bytes,
+        DeviceCommand::PROGRAM_PAGE_SIZE,
+        BufferType::DRAM,
+        TensorMemoryLayout::INTERLEAVED);
+
+    // Pin the trace buffer in memory through trace memory mgmt
+    this->add_instance(tid, trace_buffer);
+
+    // Commit the trace buffer to device DRAM in a blocking fashion
+    // Optional optimization: use a non-blocking enqueue WB command
+    EnqueueWriteBuffer(cq, trace_buffer, trace_data, true);
+
+    this->history.clear();
+    this->state = TraceState::READY;
+    return tid;
 }
 
+bool Trace::has_instance(const uint32_t tid) {
+    return _safe_pool([&] {
+        return Trace::buffer_pool.find(tid) != Trace::buffer_pool.end();
+    });
 }
+
+void Trace::add_instance(const uint32_t tid, shared_ptr<Buffer> buffer) {
+    _safe_pool([&] {
+        TT_FATAL(Trace::buffer_pool.find(tid) == Trace::buffer_pool.end());
+        Trace::buffer_pool.insert({tid, buffer});
+    });
+}
+
+void Trace::remove_instance(const uint32_t tid) {
+    _safe_pool([&] {
+        TT_FATAL(Trace::buffer_pool.find(tid) != Trace::buffer_pool.end());
+        Trace::buffer_pool.erase(tid);
+    });
+}
+
+}  // namespace tt::tt_metal
