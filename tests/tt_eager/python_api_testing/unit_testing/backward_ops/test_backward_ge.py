ca56686564a269105e38c289d128f8436de1ae3a
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2024-03-28 12:42:14 +0000

    #6443: Update Unary Div backward

--- tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_ge.py
+++ tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_ge.py
@@ -5,7 +5,7 @@
 import torch
 import pytest
 import tt_lib
-from tests.tt_eager.python_api_testing.unit_testing.backward_ops.utility_funcs import data_gen_pt_tt, compare_results
+from tests.tt_eager.python_api_testing.unit_testing.backward_ops.utility_funcs import data_gen_with_range, compare_pcc
 
 
 @pytest.mark.parametrize(
@@ -17,12 +17,12 @@ from tests.tt_eager.python_api_testing.unit_testing.backward_ops.utility_funcs i
     ),
 )
 def test_bw_unary_ge(input_shapes, device):
-    grad_data, grad_tensor = data_gen_pt_tt(input_shapes, device)
+    grad_data, grad_tensor = data_gen_with_range(input_shapes, -100, 100, device)
     tt_output_tensor_on_device = tt_lib.tensor.ge_bw(grad_tensor)
 
     pyt_y = torch.zeros_like(grad_data)
 
     golden_tensor = [pyt_y]
 
-    comp_pass = compare_results(tt_output_tensor_on_device, golden_tensor)
+    comp_pass = compare_pcc(tt_output_tensor_on_device, golden_tensor)
     assert comp_pass
