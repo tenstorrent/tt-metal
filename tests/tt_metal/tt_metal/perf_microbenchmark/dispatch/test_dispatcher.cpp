b315e406b239fa45ce6b498fe4de0d16707e34a7
Paul Keller <pkeller@tenstorrent.com>
2024-03-29 21:19:32 +0000

    #5480: Refactor prefetcher split
    Backing out the split done before, making prefetcher work like dispatcher, ie
    with all args present at all times

--- tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/test_dispatcher.cpp
+++ tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/test_dispatcher.cpp
@@ -482,7 +482,8 @@ int main(int argc, char **argv) {
         args.push_back(prefetcher_iterations_g);
         tt_metal::SetRuntimeArgs(program, sp1, spoof_prefetch_core, args);
 
-        configure_dispatch_kernel_variant<true, true>(program,
+        configure_kernel_variant<true, true>(program,
+            "tt_metal/impl/dispatch/kernels/cq_dispatch.cpp",
             dispatch_compile_args,
             dispatch_core,
             phys_dispatch_core,
