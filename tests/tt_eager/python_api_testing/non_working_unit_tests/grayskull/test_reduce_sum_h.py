dcdf6ad0aee21298c25c1e1b8e99f04cade1a790
banekg <barsic@tenstorrent.com>
2024-04-01 11:16:15 +0000

    #6873: Add bfloat8_b and L1 for GS reduce ops

--- tests/tt_eager/python_api_testing/non_working_unit_tests/grayskull/test_reduce_sum_h.py
+++ tests/tt_eager/python_api_testing/non_working_unit_tests/grayskull/test_reduce_sum_h.py
@@ -11,12 +11,12 @@ import torch
 import tt_lib as ttl
 
 from tests.tt_eager.python_api_testing.sweep_tests import pytorch_ops
-from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_equal
-from tests.tt_eager.python_api_testing.sweep_tests.tt_lib_ops import reduce_max_h as tt_reduce_max_h
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc
+from tests.tt_eager.python_api_testing.sweep_tests.tt_lib_ops import reduce_sum_h as tt_reduce_sum_h
 from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_rand
 
 
-def run_reduce_max_h_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
+def run_reduce_sum_h_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
     torch.manual_seed(data_seed)
 
     if in_mem_config == "SYSTEM_MEMORY":
@@ -25,9 +25,9 @@ def run_reduce_max_h_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_c
     x = gen_rand(size=input_shape, low=-100, high=100).to(torch.bfloat16)
     # compute ref value
     x_ref = x.detach().clone()
-    ref_value = pytorch_ops.reduce_max(x_ref, dims=(-2,))
+    ref_value = pytorch_ops.reduce_sum(x_ref, dims=(-2,))
 
-    tt_result = tt_reduce_max_h(
+    tt_result = tt_reduce_sum_h(
         x=x,
         device=device,
         dtype=[dtype],
@@ -37,7 +37,7 @@ def run_reduce_max_h_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_c
     )
 
     # compare tt and golden outputs
-    success, pcc_value = comp_equal(ref_value, tt_result)
+    success, pcc_value = comp_pcc(ref_value, tt_result)
     logger.debug(pcc_value)
     logger.debug(success)
 
@@ -46,20 +46,28 @@ def run_reduce_max_h_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_c
 
 test_sweep_args = [
     (
-        (3, 12, 12, 140),
-        ttl.tensor.DataType.BFLOAT16,
-        ttl.tensor.Layout.ROW_MAJOR,
-        "SYSTEM_MEMORY",
+        (5, 11, 64, 160),
+        ttl.tensor.DataType.BFLOAT8_B,
+        ttl.tensor.Layout.TILE,
         ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
-        8057439,
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        8740671,
     ),
     (
-        (3, 3, 168, 134),
+        (4, 1, 154, 26),
+        ttl.tensor.DataType.BFLOAT16,
+        ttl.tensor.Layout.ROW_MAJOR,
+        "SYSTEM_MEMORY",
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1),
+        19118259,
+    ),
+    (
+        (1, 4, 20, 166),
         ttl.tensor.DataType.BFLOAT16,
         ttl.tensor.Layout.ROW_MAJOR,
         "SYSTEM_MEMORY",
         ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
-        8057439,
+        14632035,
     ),
 ]
 
@@ -68,5 +76,5 @@ test_sweep_args = [
     "input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed",
     (test_sweep_args),
 )
-def test_reduce_max_h_test(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
-    run_reduce_max_h_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device)
+def test_reduce_sum_h_test(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
+    run_reduce_sum_h_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device)
