d379a4b791c94894d5e94c23726fbb297a0a1fc6
banekg <barsic@tenstorrent.com>
2024-04-03 10:36:01 +0000

    #6873: Add bfloat8_b and L1 for stats, and some activation functions

--- tests/tt_eager/python_api_testing/non_working_unit_tests/grayskull/test_stats_normalize_hw.py
+++ tests/tt_eager/python_api_testing/non_working_unit_tests/grayskull/test_stats_normalize_hw.py
@@ -2,33 +2,29 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-from pathlib import Path
-import sys
 from loguru import logger
-import random
 import pytest
 import torch
 import tt_lib as ttl
 
 from tests.tt_eager.python_api_testing.sweep_tests import pytorch_ops
-from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_equal, comp_pcc
-from tests.tt_eager.python_api_testing.sweep_tests.tt_lib_ops import reduce_min_h as tt_reduce_min_h
-from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_rand
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc
+from tests.tt_eager.python_api_testing.sweep_tests.tt_lib_ops import normalize_hw as tt_normalize_hw
 
 
-def run_reduce_min_h_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
+def run_normalize_hw_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
     torch.manual_seed(data_seed)
 
     if in_mem_config == "SYSTEM_MEMORY":
         in_mem_config = None
 
-    x = gen_rand(size=input_shape, low=-100, high=100).to(torch.bfloat16)
+    x = torch.Tensor(size=input_shape).uniform_(-10, 10)
+    x_ref = x.detach().clone()
 
     # compute ref value
-    x_ref = x.detach().clone()
-    ref_value = pytorch_ops.reduce_min(x_ref, dims=(-2,))
+    ref_value = pytorch_ops.normalize_hw(x_ref)
 
-    tt_result = tt_reduce_min_h(
+    tt_result = tt_normalize_hw(
         x=x,
         device=device,
         dtype=[dtype],
@@ -40,27 +36,26 @@ def run_reduce_min_h_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_c
     # compare tt and golden outputs
     success, pcc_value = comp_pcc(ref_value, tt_result)
     logger.debug(pcc_value)
-    logger.debug(success)
 
     assert success
 
 
 test_sweep_args = [
     (
-        (4, 10, 96, 128),
+        (4, 7, 32, 96),
         ttl.tensor.DataType.BFLOAT8_B,
         ttl.tensor.Layout.TILE,
         ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
-        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
-        16530771,
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1),
+        4673250,
     ),
     (
-        (3, 11, 96, 128),
+        (3, 10, 64, 96),
         ttl.tensor.DataType.BFLOAT8_B,
         ttl.tensor.Layout.TILE,
+        "SYSTEM_MEMORY",
         ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
-        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
-        8419903,
+        15989726,
     ),
 ]
 
@@ -69,5 +64,5 @@ test_sweep_args = [
     "input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed",
     (test_sweep_args),
 )
-def test_reduce_min_h_test(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
-    run_reduce_min_h_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device)
+def test_stats_normalize_hw_test(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
+    run_normalize_hw_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device)
