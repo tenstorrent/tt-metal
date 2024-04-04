9e89ac8107ada25d8d1b666ff67cd1526c5ccfc0
Johanna Rock <jrock@tenstorrent.com>
2024-04-04 09:23:18 +0000

    #0: Add perf_summary script

---
+++ models/experimental/falcon40b/perf_summary.py
@@ -0,0 +1,325 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import pandas as pd
+import argparse
+
+DRAM_INTERLEAVED = "{'buffer_type': 'DRAM'; 'memory_layout': 'INTERLEAVED'}"
+
+
+def assign_op_id_cross_device(df, num_chips):
+    df.loc[:, "OP ID CROSS DEVICE"] = df["GLOBAL CALL COUNT"].apply(lambda x: x // num_chips)
+
+    # Fix for partially scliced groups
+    partial_group_counter = 0
+    partial_group_size = 0
+    partial_group_idx = 0
+    partial_group_assignments = {}
+    partial_group_sizes = {}
+    partial_group_op_ids = {}
+    found_partial_group_size = False
+
+    def count_partial_group_size(row):
+        nonlocal partial_group_counter
+        nonlocal partial_group_size
+        nonlocal partial_group_idx
+        nonlocal partial_group_assignments
+        nonlocal partial_group_sizes
+        nonlocal found_partial_group_size
+        nonlocal partial_group_op_ids
+        if row["OP CODE"] == "InterleavedToShardedPartial":  # Start of a partial group
+            partial_group_counter += 1
+
+        elif row["OP CODE"] == "ShardedToInterleavedPartial":
+            if not found_partial_group_size:
+                found_partial_group_size = True
+                partial_group_sizes[
+                    str(partial_group_idx)
+                ] = partial_group_size  # Size of partial group including ShardedToInterleavedPartial op, excluding InterleavedToShardedPartial op
+
+            partial_group_counter -= 1
+            if partial_group_counter == 0:  # Done with the partial group, resetting for next group
+                partial_group_idx += 1
+                found_partial_group_size = False
+                partial_group_size = 0
+
+        if partial_group_counter > 0 and row["OP CODE"] != "InterleavedToShardedPartial":
+            # This means we are within a partial group but it's not the start (InterleavedToShardedPartial)
+            if not str(partial_group_idx) in partial_group_assignments:
+                partial_group_assignments[str(partial_group_idx)] = [row["GLOBAL CALL COUNT"]]
+                partial_group_op_ids[str(partial_group_idx)] = row["OP ID CROSS DEVICE"]
+            else:
+                partial_group_assignments[str(partial_group_idx)].append(row["GLOBAL CALL COUNT"])
+            if not found_partial_group_size:
+                partial_group_size += 1
+        return row
+
+    df = df.apply(count_partial_group_size, axis=1)
+
+    def update_op_id_cross_device(row):
+        nonlocal partial_group_assignments
+        nonlocal partial_group_sizes
+        for groups in partial_group_assignments:
+            if row["GLOBAL CALL COUNT"] in partial_group_assignments[groups]:
+                row["OP ID CROSS DEVICE"] = partial_group_op_ids[groups] + (
+                    (row["GLOBAL CALL COUNT"] - partial_group_assignments[groups][0])
+                    % (partial_group_sizes[groups] + 1)
+                )
+                return row
+        return row
+
+    df = df.apply(update_op_id_cross_device, axis=1)
+
+    return df
+
+
+def group_by_op_all_devices(df):
+    # Split the DataFrame into AllGather and the rest
+    df1 = df[df["OP CODE"] != "tt::tt_metal::AllGather"]
+    df2 = df[df["OP CODE"] == "tt::tt_metal::AllGather"]
+
+    # Perform the groupby operation separately on each DataFrame
+    # We want to aggregate AllGather by the min and the rest by the max duration
+    grouped_df1 = df1.groupby("OP ID CROSS DEVICE").max()
+    grouped_df2 = df2.groupby("OP ID CROSS DEVICE").min()
+
+    # Concatenate the results
+    grouped_df = pd.concat([grouped_df1, grouped_df2])
+
+    # Sort by global call id again
+    grouped_df = grouped_df.sort_values(by="GLOBAL CALL COUNT", ascending=True)
+
+    grouped_df = grouped_df.reset_index()
+
+    return grouped_df
+
+
+def analyze_perf_csv(csv_file, layers, show_all, out_file=None, num_chips=1, seq=32, group=False):
+    df = pd.read_csv(csv_file)
+    df = df[df["DEVICE FW DURATION [ns]"] != "-"]
+    df["DEVICE FW DURATION [ns]"] = df["DEVICE FW DURATION [ns]"].astype(int)
+
+    df = assign_op_id_cross_device(df, num_chips)
+
+    def str_shape_to_dims(shape):
+        try:
+            W = shape.split(";")[0].split("{'W': ")[1]
+            X = shape.split(";")[1].split("'X': ")[1]
+            Y = shape.split(";")[2].split("'Y': ")[1]
+            Z = shape.split(";")[3].split("'Z': ")[1].split("}")[0]
+        except:
+            W = 0
+            X = 0
+            Y = 0
+            Z = 0
+        return pd.Series([int(W), int(Z), int(Y), int(X)])
+
+    df[
+        [
+            "INPUT_0_W",
+            "INPUT_0_Z",
+            "INPUT_0_Y",
+            "INPUT_0_X",
+        ]
+    ] = df[
+        "INPUT_0_SHAPE"
+    ].apply(lambda x: str_shape_to_dims(x))
+    df[
+        [
+            "INPUT_1_W",
+            "INPUT_1_Z",
+            "INPUT_1_Y",
+            "INPUT_1_X",
+        ]
+    ] = df[
+        "INPUT_1_SHAPE"
+    ].apply(lambda x: str_shape_to_dims(x))
+    df[
+        [
+            "OUTPUT_0_W",
+            "OUTPUT_0_Z",
+            "OUTPUT_0_Y",
+            "OUTPUT_0_X",
+        ]
+    ] = df[
+        "OUTPUT_0_SHAPE"
+    ].apply(lambda x: str_shape_to_dims(x))
+    # sorted_df = df.sort_values(by="DEVICE FW DURATION [ns]", ascending=False)
+    sorted_df = df
+    sum_duration = df["DEVICE FW DURATION [ns]"].sum()
+
+    matmul_rows = sorted_df[sorted_df["OP CODE"].str.contains("Matmul")]
+
+    matmul_rows.loc[:, "bytes"] = (
+        matmul_rows["INPUT_1_W"] * matmul_rows["INPUT_1_Z"] * matmul_rows["INPUT_1_Y"] * matmul_rows["INPUT_1_X"]
+    )
+    matmul_rows.loc[:, "flops"] = 2 * matmul_rows["INPUT_0_Y"] * matmul_rows["INPUT_0_X"] * matmul_rows["OUTPUT_0_X"]
+    matmul_rows["GB/s"] = matmul_rows["bytes"] / matmul_rows["DEVICE FW DURATION [ns]"]
+    matmul_rows["TFLOP/s"] = matmul_rows["flops"] / matmul_rows["DEVICE FW DURATION [ns]"] / 1000
+    matmul_rows["% DRAM (240)"] = 100 * matmul_rows["GB/s"] / 240  # Peak expected WH bandwidth
+    matmul_rows["% FPU (82)"] = 100 * matmul_rows["TFLOP/s"] / 82  # Peak theoretical FP16 FPU performance
+    matmul_rows["% TIME"] = 100 * matmul_rows["DEVICE FW DURATION [ns]"] / sum_duration
+    # matmul_rows["% TIME SUM"] = matmul_rows["% TIME"].cumsum()
+    matmul_sum_duration = matmul_rows["DEVICE FW DURATION [ns]"].sum()
+
+    # shorten some column names
+    matmul_rows.rename(columns={"DEVICE FW DURATION [ns]": "DURATION [ns]"}, inplace=True)
+    sorted_df.rename(columns={"DEVICE FW DURATION [ns]": "DURATION [ns]"}, inplace=True)
+
+    data_type = {"BFLOAT16": 2, "BFLOAT8_B": 1}
+    sorted_df[["INPUT_0_DTYPE", "INPUT_1_DTYPE", "OUTPUT_0_DTYPE"]] = (
+        sorted_df[["INPUT_0_DTYPE", "INPUT_1_DTYPE", "OUTPUT_0_DTYPE"]].applymap(data_type.get).fillna(0).astype(int)
+    )
+
+    sorted_df.loc[:, "TOTAL_BYTES"] = (
+        (sorted_df["INPUT_0_STORAGE_TYPE"].str.contains(DRAM_INTERLEAVED, na=False)).astype(int)
+        * (
+            sorted_df["INPUT_0_W"]
+            * sorted_df["INPUT_0_Z"]
+            * sorted_df["INPUT_0_Y"]
+            * sorted_df["INPUT_0_X"]
+            * sorted_df["INPUT_0_DTYPE"]
+        )
+        + (sorted_df["INPUT_1_STORAGE_TYPE"].str.contains(DRAM_INTERLEAVED, na=False)).astype(int)
+        * (
+            sorted_df["INPUT_1_W"]
+            * sorted_df["INPUT_1_Z"]
+            * sorted_df["INPUT_1_Y"]
+            * sorted_df["INPUT_1_X"]
+            * sorted_df["INPUT_1_DTYPE"]
+        )
+        + (sorted_df["OUTPUT_0_STORAGE_TYPE"].str.contains(DRAM_INTERLEAVED, na=False)).astype(int)
+        * (
+            sorted_df["OUTPUT_0_W"]
+            * sorted_df["OUTPUT_0_Z"]
+            * sorted_df["OUTPUT_0_Y"]
+            * sorted_df["OUTPUT_0_X"]
+            * sorted_df["OUTPUT_0_DTYPE"]
+        )
+    )
+
+    sorted_df.loc[:, "flops"] = 2 * sorted_df["INPUT_0_Y"] * sorted_df["INPUT_0_X"] * sorted_df["OUTPUT_0_X"]
+    sorted_df["DRAM BW GB/s"] = sorted_df["TOTAL_BYTES"] * (1000**3) / (sorted_df["DURATION [ns]"] * (1024**3))
+    sorted_df["TFLOP/s"] = sorted_df["flops"] / sorted_df["DURATION [ns]"] / 1000
+    sorted_df["% DRAM (240)"] = 100 * sorted_df["DRAM BW GB/s"] / 240  # Peak expected WH bandwidth
+    sorted_df["% FPU (82)"] = 100 * sorted_df["TFLOP/s"] / 82  # Peak theoretical FP16 FPU performance
+    sorted_df["TOTAL MBs"] = sorted_df["TOTAL_BYTES"] / 1024 / 1024
+    selected_columns = [
+        "GLOBAL CALL COUNT",
+        "OP ID CROSS DEVICE",
+        "OP CODE",
+        "% TIME",
+        # "% TIME SUM",
+        "% DRAM (240)",
+        "% FPU (82)",
+        "DURATION [ns]",
+        "GB/s",
+        "TFLOP/s",
+        "CORE COUNT",
+        "INPUT_0_Y",
+        "INPUT_0_X",
+        "INPUT_1_Y",
+        "INPUT_1_X",
+        "OUTPUT_0_Y",
+        "OUTPUT_0_X",
+    ]
+    print(matmul_rows[selected_columns])
+
+    if show_all:
+        selected_columns = [
+            "GLOBAL CALL COUNT",
+            "OP ID CROSS DEVICE",
+            "OP CODE",
+            "% TIME",
+            # "% TIME SUM",
+            "DURATION [ns]",
+            "% DEV CB WAIT",
+            # "DURATION SUM [ns]",
+            "DRAM BW GB/s",
+            "% DRAM (240)",
+            "CORE COUNT",
+            "INPUT_0_Y",
+            "INPUT_0_X",
+            "INPUT_1_Y",
+            "INPUT_1_X",
+            "OUTPUT_0_Y",
+            "OUTPUT_0_X",
+            "TOTAL MBs",
+            "DEVICE COMPUTE CB WAIT FRONT [ns]",
+        ]
+        sorted_df["% TIME"] = 100 * sorted_df["DURATION [ns]"] / sum_duration
+        sorted_df["% DEV CB WAIT"] = (
+            100 * sorted_df["DEVICE COMPUTE CB WAIT FRONT [ns]"].astype(int) / sorted_df["DURATION [ns]"]
+        )
+
+        # trim all floats to 6 decimal places
+        sorted_df = sorted_df.round(6)
+        print(sorted_df[selected_columns])
+        selected_df = sorted_df[selected_columns]
+
+        # save to file for further analysis
+        if out_file:
+            selected_df.to_csv(out_file, index=False, sep="\t")
+
+        if group:
+            grouped_df = group_by_op_all_devices(selected_df)
+
+            sum_duration = grouped_df["DURATION [ns]"].sum()
+            grouped_df["% TIME"] = 100 * grouped_df["DURATION [ns]"] / sum_duration
+            grouped_df["% DEV CB WAIT"] = (
+                100 * grouped_df["DEVICE COMPUTE CB WAIT FRONT [ns]"].astype(int) / grouped_df["DURATION [ns]"]
+            )
+
+            grouped_matmul_rows = group_by_op_all_devices(matmul_rows)
+            matmul_sum_duration = grouped_matmul_rows["DURATION [ns]"].sum()
+
+            print(grouped_df)
+
+            if out_file:
+                grouped_df.to_csv(out_file.split(".")[0] + ".grouped.csv", index=False, sep="\t")
+
+    if layers:
+        tokens_per_sec_user = 1000000000 / sum_duration / layers
+        tokens_per_sec = seq * tokens_per_sec_user
+        print(f"Layer ms: {sum_duration / 1000000:.8f} ({matmul_sum_duration / sum_duration:.2%} matmul)")
+        print(f"Inferences/sec: {tokens_per_sec_user:.8f}")
+        print(f"Tokens/sec: {tokens_per_sec:.8f}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Analyze perf CSV file")
+    parser.add_argument(
+        "-a", "--all", action="store_true", help="List ops in the CSV file - by default only matmul ops are shown."
+    )
+    parser.add_argument("-l", "--layers", type=int, help="Number of layers to extrapolate perf results up to.")
+    parser.add_argument(
+        "csv_file", type=str, help="Path to the perf CSV file from tt-metal for a single decoder layer."
+    )
+    parser.add_argument(
+        "-o", "--out_file", required=False, type=str, help="Path to the output file for further analysis."
+    )
+
+    parser.add_argument(
+        "--num-chips", type=int, default=1, help="Number of chips running model in parallel mode. Default is 1."
+    )
+
+    parser.add_argument("--seq", type=int, default=32, help="Sequence length or number of users.")
+
+    parser.add_argument("-g", "--group", action="store_true", help="Group by ops running in parallel on all devices.")
+
+    args = parser.parse_args()
+
+    analyze_perf_csv(
+        args.csv_file,
+        layers=args.layers,
+        show_all=args.all,
+        out_file=args.out_file,
+        num_chips=args.num_chips,
+        seq=args.seq,
+        group=args.group,
+    )
+
+
+if __name__ == "__main__":
+    main()
