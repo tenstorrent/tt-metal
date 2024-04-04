ed37beb9ee5cd802e3106115cb17a4e36867960b
ruthreshk <ruthresh@multicorewareinc.com>
2024-04-04 14:04:30 +0000

    #7108: Accuracy enhancement for sigmoid using ttnn

--- tests/ttnn/python_api_testing/sweep_tests/ttnn_ops.py
+++ tests/ttnn/python_api_testing/sweep_tests/ttnn_ops.py
@@ -1265,6 +1265,22 @@ def eltwise_silu(
     return ttnn_tensor_to_torch(t1)
 
 
+def eltwise_sigmoid_accurate(
+    x,
+    *args,
+    device,
+    dtype,
+    layout,
+    input_mem_config,
+    output_mem_config,
+    **kwargs,
+):
+    t0 = setup_ttnn_tensor(x, device, layout[0], input_mem_config[0], dtype[0])
+    t1 = ttnn.sigmoid_accurate(t0, memory_config=memory_config_to_ttnn(output_mem_config))
+
+    return ttnn_tensor_to_torch(t1)
+
+
 def eltwise_sin(
     x,
     *args,
