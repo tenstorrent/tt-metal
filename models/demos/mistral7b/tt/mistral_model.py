77e334739b609a4f5bbd3075ecc744a409fa7836
Mark O'Connor <moconnor@tenstorrent.com>
2024-04-05 16:05:15 +0000

    #6874: Manually-specify program configs for mistral for now

--- models/demos/mistral7b/tt/mistral_model.py
+++ models/demos/mistral7b/tt/mistral_model.py
@@ -64,6 +64,12 @@ class TtTransformer(nn.Module):
             memory_config=ttnn.DRAM_MEMORY_CONFIG,
             cache_file_name=weight_cache_path / "output.weight",
         )
+        self.output_program_config = ttnn.operations.matmul.create_matmul_1d_systolic_array_program_config(
+            input_shape_a=ttnn.Shape([1, 1, args.max_batch_size, args.dim]),
+            input_shape_b=self.output_weight.shape,
+            core_grid=args.max_grid_size,
+            fp32_dst=args.get_compute_kernel_config().fp32_dest_acc_en,
+        )
 
     def forward(
         self,
@@ -75,5 +81,10 @@ class TtTransformer(nn.Module):
             x = layer(x, current_pos, attn_masks)
 
         x = self.norm(x)
-        output = ttnn.linear(x, self.output_weight, core_grid=self.args.max_grid_size)
+        output = ttnn.linear(
+            x,
+            self.output_weight,
+            program_config=self.output_program_config,
+            compute_kernel_config=self.args.get_compute_kernel_config(),
+        )
         return output
