6de5311a7e82817e53af0a03839b1321faa6fca7
Kyle Mabee <kmabee@tenstorrent.com>
2024-04-05 03:38:10 +0000

    #4476: Initial SetCommonRuntimeArgs() API/Tests
     - Slow dispatch path covered for now, not FD2.0 yet
     - Update tests for data-movement, compute to cover SetCommonRuntimeArgs too
     - Cleanup test infra, a lot of it was able to be commonized (checking)
     - Add a test with unique rt arg vals per core, and a test that varies
       number of unique rt args per core, to show common offset used.
     - Add docs, and update words in dataflow_api.h / compute_kernel_api/common.h
       to be the same for existing APIs
     - Add note about combined max of 255 args between unique/combined in
       all flavors host APIs for runtime args.
     - Update validation to catch unique+common args exceeding 255 limit
     - Add default define COMMON_RT_ARGS_OFFSET in dataflow_api.h and
       common.h to prevent compile errors in unused kernels. When configured,
       the JIT Build flow will automatically populate this define.

--- tests/tt_metal/tt_metal/test_kernels/compute/increment_runtime_arg.cpp
+++ tests/tt_metal/tt_metal/test_kernels/compute/increment_runtime_arg.cpp
@@ -9,15 +9,25 @@ void MAIN {
     // Tests get_arg_val API
     uint32_t arg_a  = get_arg_val<uint32_t>(0);
     uint32_t arg_b = get_arg_val<uint32_t>(1);
-
+    uint32_t common_arg_a = get_common_arg_val<uint32_t>(0);
+    uint32_t common_arg_b = get_common_arg_val<uint32_t>(1);
+    uint32_t common_arg_c = get_common_arg_val<uint32_t>(2);
+    uint32_t common_arg_d = get_common_arg_val<uint32_t>(3);
 
     // Need pointer as well to modify arg address to test in host
     volatile tt_l1_ptr std::uint32_t* arg_a_ptr = (volatile tt_l1_ptr uint32_t*)(TRISC_L1_ARG_BASE);
     volatile tt_l1_ptr std::uint32_t* arg_b_ptr = (volatile tt_l1_ptr uint32_t*)(TRISC_L1_ARG_BASE + 4);
-
+    volatile tt_l1_ptr std::uint32_t* common_arg_a_ptr = (volatile tt_l1_ptr uint32_t*)(TRISC_L1_ARG_BASE + COMMON_RT_ARGS_OFFSET);
+    volatile tt_l1_ptr std::uint32_t* common_arg_b_ptr = (volatile tt_l1_ptr uint32_t*)(TRISC_L1_ARG_BASE + COMMON_RT_ARGS_OFFSET + 4);
+    volatile tt_l1_ptr std::uint32_t* common_arg_c_ptr = (volatile tt_l1_ptr uint32_t*)(TRISC_L1_ARG_BASE + COMMON_RT_ARGS_OFFSET + 8);
+    volatile tt_l1_ptr std::uint32_t* common_arg_d_ptr = (volatile tt_l1_ptr uint32_t*)(TRISC_L1_ARG_BASE + COMMON_RT_ARGS_OFFSET + 12);
 
     UNPACK(arg_a_ptr[0] = arg_a + 87);
     UNPACK(arg_b_ptr[0] = arg_b + 216);
+    UNPACK(common_arg_a_ptr[0] = common_arg_a + 123);
+    UNPACK(common_arg_b_ptr[0] = common_arg_b + 234);
+    UNPACK(common_arg_c_ptr[0] = common_arg_c + 345);
+    UNPACK(common_arg_d_ptr[0] = common_arg_d + 456);
 
 }
 }
