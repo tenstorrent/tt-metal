9993f40d457e71601c1e76d4e68e6c01faaaa626
acejkov <acejkov@tenstorrent.com>
2024-04-05 16:38:22 +0000

    rebase with main

--- tt_eager/tt_dnn/op_library/conv/kernels/reader_conv_activations_padded_with_halo_3x3_weights_v2.cpp
+++ tt_eager/tt_dnn/op_library/conv/kernels/reader_conv_activations_padded_with_halo_3x3_weights_v2.cpp
@@ -9,7 +9,7 @@
 
 
 void kernel_main() {
-    constexpr uint32_t LOCAL_PACKED_READER_INDICES_MAX_SIZE = 256;
+    constexpr uint32_t LOCAL_PACKED_READER_INDICES_MAX_SIZE = 128;
     uint32_t local_packed_reader_indices[LOCAL_PACKED_READER_INDICES_MAX_SIZE];
 
     constexpr bool act_in_dram = get_compile_time_arg_val(0) == 1;
