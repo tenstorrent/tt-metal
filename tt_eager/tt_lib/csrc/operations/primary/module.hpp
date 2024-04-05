7f8581b7ab7f326c1b311f15bc2b7d1d3cc1ef0a
Salar <skhorasgani@tenstorrent.com>
2024-04-05 17:37:42 +0000

    #6795: Add grid size option to get_mcast_1d_config, set falcon7b matmul grid size on wh to 8x7 until hang issue is resolved
    Signed-off-by: Salar <skhorasgani@tenstorrent.com>

--- tt_eager/tt_lib/csrc/operations/primary/module.hpp
+++ tt_eager/tt_lib/csrc/operations/primary/module.hpp
@@ -129,7 +129,8 @@ void py_module(py::module& m_primary) {
         py::arg("fuse_batch").noconvert() = false,
         py::arg("fused_activation") = std::nullopt,
         py::arg("mcast_in0").noconvert() = true,
-        py::arg("out_sharded").noconvert() = false);
+        py::arg("out_sharded").noconvert() = false,
+        py::arg("compute_with_storage_grid_size") = std::nullopt);
 
     // TODO(arakhmati):
     // delete
