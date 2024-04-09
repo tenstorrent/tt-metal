621ffacfe5601da61905402add9a882bc49647c1
Almeet Bhullar <abhullar@tenstorrent.com>
2024-04-09 14:37:41 +0000

    #0: Remove test_pgm_dispatch from CI

--- tests/scripts/run_cpp_unit_tests.sh
+++ tests/scripts/run_cpp_unit_tests.sh
@@ -14,7 +14,6 @@ if [[ ! -z "$TT_METAL_SLOW_DISPATCH_MODE" ]]; then
 else
     test_filter="CommandQueueSingleCardFixture.*:CommandQueueFixture.*:FastDispatchHostSuite.*"
     ./build/test/tt_metal/unit_tests_fast_dispatch --gtest_filter="${test_filter}"
-    ./build/test/tt_metal/perf_microbenchmark/dispatch/test_pgm_dispatch
     # env python tests/scripts/run_tt_eager.py --dispatch-mode fast
     # env python tests/scripts/run_tt_metal.py --dispatch-mode fast
 fi
