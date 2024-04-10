8a88f50f62c8c372896217382b8608b7bce8a7f5
punithsekar <punith.sekar@multicorewareinc.com>
2024-04-10 10:28:34 +0000

    #6343: Demo for functional bloom QA

---
+++ models/demos/grayskull/functional_bloom/README.md
@@ -0,0 +1,25 @@
+# ttnn_functional_bloom for QA Demo
+
+## How to Run
+
+Use `pytest --disable-warnings --input-path="models/demos/grayskull/functional_bloom/demo/input_data.json" models/demos/grayskull/functional_bloom/demo/demo_qa.py::test_demo[models.experimental.functional_bloom.tt.ttnn_functional_bloom]` to run the ttnn functional bloom for question answering demo.
+
+Use `pytest --disable-warnings --input-path="models/demos/grayskull/functional_bloom/demo/input_data.json" models/demos/grayskull/functional_bloom/demo/demo_qa.py::test_demo[models.experimental.functional_bloom.tt.ttnn_optimized_functional_bloom]` to run the ttnn optimized functional bloom for question answering demo.
+
+If you wish to run the demo with a different input use `pytest --disable-warnings --input-path=<address_to_your_json_file.json> models/demos/grayskull/functional_bloom/demo/demo_qa.py::test_demo[path_to_functional_bloom]`.
+
+Our second demo is designed to run with SQuADV2 dataset.
+
+Use `pytest --disable-warnings models/demos/grayskull/functional_bloom/demo/demo_qa.py::test_demo_squadv2[5-models.experimental.functional_bloom.tt.ttnn_functional_bloom]` to run the second demo with ttnn functional bloom for question answering demo.
+
+Use `pytest --disable-warnings models/demos/grayskull/functional_bloom/demo/demo_qa.py::test_demo_squadv2[5-models.experimental.functional_bloom.tt.ttnn_optimized_functional_bloom]` to run the second demo with ttnn optimized functional bloom for question answering demo.
+
+## Inputs
+
+Inputs are provided from `input_data.json` by default. If you wish you to change the inputs or provide a different path to `test_demo`.
+
+We do not recommend modifying `input_data.json` file.
+
+## Details
+
+The entry point to functional bloom model is `bloom` in `ttnn_functional_bloom.py`. We used `bigscience/bloom-560m` version from huggingface as reference.
---
+++ models/demos/grayskull/functional_bloom/dataset_utils.py
@@ -0,0 +1,117 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import json
+from typing import Any
+from loguru import logger
+from datasets import load_dataset
+from torch.utils.data import Dataset
+from models.datasets.dataset_squadv2 import squad_divide_chunks
+
+
+class InputExample(object):
+    def __init__(self, input_sentence, endings=None, label=None):
+        self.input_sentence = input_sentence
+        self.endings = endings
+        self.label = label
+
+
+def get_input(item):
+    if "ctx_a" not in item:
+        return item["ctx"]
+    if "ctx" not in item:
+        return item["ctx_a"]
+    if len(item["ctx"]) == len(item["ctx_a"]):
+        return item["ctx"]
+    return item["ctx_a"]
+
+
+def get_endings(item):
+    if ("ctx_b" not in item) or len(item["ctx_b"]) == 0:
+        return item["endings"]
+    return ["{} {}".format(item["ctx_b"], x) for x in item["endings"]]
+
+
+def get_data(input_loc):
+    examples = []
+    with open(input_loc, "r") as file:
+        for data in file:
+            item = json.loads(data)
+            examples.append(
+                InputExample(
+                    input_sentence=get_input(item),
+                    endings=get_endings(item),
+                    label=item["label"],
+                )
+            )
+
+    return examples
+
+
+class SQUADV2Dataset(Dataset):
+    def __init__(
+        self,
+        dataset_question: Any,
+        dataset_context: Any,
+        dataset_reference: Any,
+        tokenizer: Any,
+        seq_len: int,
+        attention_mask: bool,
+        token_type_ids: bool,
+    ):
+        self.data = []
+        for i in range(len(dataset_question)):
+            self.data.append(
+                (
+                    tokenizer.batch_encode_plus(
+                        list(zip(dataset_question[i], dataset_context[i])),
+                        max_length=seq_len,
+                        padding="max_length",
+                        truncation=True,
+                        return_attention_mask=attention_mask,
+                        return_token_type_ids=token_type_ids,
+                        return_tensors="pt",
+                    ),
+                    dataset_reference[i],
+                    dataset_question[i],
+                    dataset_context[i],
+                )
+            )
+
+    def __getitem__(self, index: int):
+        X = self.data[index]
+        return X
+
+
+def squadv2_1K_samples_input(tokenizer, seq_len, attention_mask, token_type_ids, microbatch=8):
+    squadv2_dataset = load_dataset("squad_v2", use_auth_token=False, streaming=True)["validation"]
+    dataset_iter = iter(squadv2_dataset)
+    dataset_question = []
+    dataset_context = []
+    dataset_reference = []
+
+    for _ in range(2048):
+        dataset_sgl = next(dataset_iter)
+        if len(dataset_sgl["answers"]["text"]) > 0:
+            dataset_question.append(dataset_sgl["question"])
+            dataset_context.append(dataset_sgl["context"])
+            dataset_reference.append({"answers": dataset_sgl["answers"], "id": dataset_sgl["id"]})
+        if len(dataset_question) == 1024:
+            logger.info("SQuADv2 1024 samples load ..done")
+            break
+
+    dataset_question, dataset_context, dataset_reference = squad_divide_chunks(
+        dataset_question, dataset_context, dataset_reference, microbatch
+    )
+    dataset_processed = SQUADV2Dataset(
+        dataset_question,
+        dataset_context,
+        dataset_reference,
+        tokenizer=tokenizer,
+        seq_len=seq_len,
+        attention_mask=attention_mask,
+        token_type_ids=token_type_ids,
+    )
+
+    return dataset_processed
