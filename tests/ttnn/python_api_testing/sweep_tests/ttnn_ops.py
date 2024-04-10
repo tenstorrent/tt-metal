fa1414e2f0ff3e06d87bbab7675a60a8de9bdd87
banekg <barsic@tenstorrent.com>
2024-04-10 12:11:59 +0000

    #7292: Add parameters to GS rsqrt, sign, and soft operations

--- tests/ttnn/python_api_testing/sweep_tests/ttnn_ops.py
+++ tests/ttnn/python_api_testing/sweep_tests/ttnn_ops.py
@@ -942,6 +942,8 @@ def eltwise_softmax(
 def eltwise_softplus(
     x,
     *args,
+    beta,
+    threshold,
     device,
     dtype,
     layout,
@@ -950,7 +952,7 @@ def eltwise_softplus(
     **kwargs,
 ):
     t0 = setup_ttnn_tensor(x, device, layout[0], input_mem_config[0], dtype[0])
-    t1 = ttnn.softplus(t0, memory_config=memory_config_to_ttnn(output_mem_config))
+    t1 = ttnn.softplus(t0, beta, threshold, memory_config=memory_config_to_ttnn(output_mem_config))
     return ttnn_tensor_to_torch(t1)
 
 
