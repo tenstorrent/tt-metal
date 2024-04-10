7011fff9ddf9f7b31a940c0412b2175c902b14c9
Milos Trajkovic <mtrajkovic@tenstorrent.com>
2024-04-10 16:10:13 +0000

    #5472: add tests for int32 format

--- tests/ttnn/unit_tests/test_to_and_from_torch.py
+++ tests/ttnn/unit_tests/test_to_and_from_torch.py
@@ -21,13 +21,17 @@ def test_to_and_from_4D(height, width):
 
 @pytest.mark.parametrize("height", [7])
 @pytest.mark.parametrize("width", [3])
-@pytest.mark.parametrize("dtype", [ttnn.bfloat16, ttnn.bfloat8_b, ttnn.bfloat4_b])
+@pytest.mark.parametrize("dtype", [ttnn.bfloat16, ttnn.bfloat8_b, ttnn.bfloat4_b, ttnn.int32])
 @pytest.mark.parametrize("layout", [ttnn.ROW_MAJOR_LAYOUT, ttnn.TILE_LAYOUT])
 def test_to_and_from_2D(height, width, dtype, layout):
     if (dtype == ttnn.bfloat8_b or dtype == ttnn.bfloat4_b) and layout == ttnn.ROW_MAJOR_LAYOUT:
         pytest.skip("ROW_MAJOR_LAYOUT not supported for bfloat8_b and bfloat4_b")
 
-    torch_input_tensor = torch.rand((height, width), dtype=torch.bfloat16)
+    if dtype == ttnn.int32:
+        torch_input_tensor = torch.randint(-100, 100, (height, width)).to(torch.int32)
+    else:
+        torch_input_tensor = torch.rand((height, width), dtype=torch.bfloat16)
+
     tensor = ttnn.from_torch(torch_input_tensor, dtype=dtype, layout=layout)
     torch_output_tensor = ttnn.to_torch(tensor).to(torch_input_tensor.dtype)
 
