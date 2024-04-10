9d11e99b62b79a301013f66e37d6bab1953d1bb5
mikevin920 <mikevin920@yahoo.com>
2024-04-10 03:36:29 +0000

    #7162: Tempory fix to lower peak memory usage by disabling atol and rotl calc

--- models/demos/llama2_70b/tt/llama_common.py
+++ models/demos/llama2_70b/tt/llama_common.py
@@ -6,6 +6,7 @@ import os
 from loguru import logger
 import re
 from typing import Tuple
+import numpy as np
 import torch
 from torch import nn
 import tt_lib
@@ -208,3 +209,107 @@ def gather_cos_sin(position_ids, cos, sin):
     cos = torch.stack([cos, cos], dim=-1).flatten(-2).unsqueeze(0).unsqueeze(0)
     sin = torch.stack([sin, sin], dim=-1).flatten(-2).unsqueeze(0).unsqueeze(0)
     return cos, sin
+
+
+def comp_pcc(golden, calculated, pcc=0.99):
+    if golden.dtype != calculated.dtype:
+        calculated = calculated.type(golden.dtype)
+    _, _, cal_pcc, output_str = get_atol_rtol_pcc(golden, calculated)
+    passing = cal_pcc >= pcc
+    if not passing:
+        output_str += ", PCC check failed"
+    return passing, output_str
+
+
+def get_atol_rtol_pcc(golden, calculated):
+    if golden.is_complex() and calculated.is_complex():
+        golden = torch.view_as_real(golden.clone())
+        calculated = torch.view_as_real(calculated.clone())
+
+    if not (golden.is_floating_point() or calculated.is_floating_point()):
+        golden = golden.to(torch.float)
+        calculated = calculated.to(torch.float)
+
+    # Calculate atol and rtol
+    # cal_atol = torch.max(torch.abs(golden - calculated)).item()
+    # cal_rtol = torch.max(torch.abs(golden - calculated) / torch.abs(calculated)).item()
+    cal_atol = 0
+    cal_rtol = 0
+
+    # Calculate PCC
+    def get_pcc(golden, calculated):
+        # Both tensors are nan
+        if torch.all(torch.isnan(golden)) and torch.all(torch.isnan(calculated)):
+            logger.warning("Both tensors are 'nan'")
+            return 1.0
+
+        # One tensor is all nan, the other is not
+        if torch.all(torch.isnan(golden)) or torch.all(torch.isnan(calculated)):
+            logger.error("One tensor is all nan, the other is not.")
+            return 0.0
+
+        # One tensor is all zero, the other is not
+        if torch.any(golden.bool()) != torch.any(calculated.bool()):
+            logger.warning("One tensor is all zero")
+            return 0.0
+
+        # if torch.any(torch.isinf(golden)) or torch.any(torch.isinf(calculated)):
+        #    raise RuntimeError(f"Tensor overflow to infinity: \n{golden}\n{calculated}")
+
+        # if torch.any(torch.isneginf(golden)) or torch.any(torch.isneginf(calculated)):
+        #    raise RuntimeError(f"Tensor overflow to negative infinity: \n{golden}\n{calculated}")
+
+        else:
+            # For now, mask all infs and nans so that we check the rest... TODO
+            golden = golden.clone()
+            golden[
+                torch.logical_or(
+                    torch.isnan(golden),
+                    torch.logical_or(torch.isinf(golden), torch.isneginf(golden)),
+                )
+            ] = 0
+            calculated = calculated.clone()
+            calculated[
+                torch.logical_or(
+                    torch.isnan(calculated),
+                    torch.logical_or(torch.isinf(calculated), torch.isneginf(calculated)),
+                )
+            ] = 0
+
+            if torch.equal(golden, calculated):
+                return 1.0
+
+            if golden.dtype == torch.bfloat16:
+                golden = golden.type(torch.float32)
+                calculated = calculated.type(torch.float32)
+
+            # Single element case
+            if golden.numel() == 1:
+                return float(torch.equal(golden, calculated))
+
+            # one tensor is constant
+            if torch.max(golden) == torch.min(golden) or torch.max(calculated) == torch.min(calculated):
+                return float(torch.equal(golden, calculated))
+
+            cal_pcc = np.ma.corrcoef(
+                np.ma.masked_invalid(torch.squeeze(golden).detach().numpy()).flatten(),
+                np.ma.masked_invalid(torch.squeeze(calculated).detach().numpy()).flatten(),
+            )
+            # Remove correlation coefficient with self (typically always 1.0)
+            mask = np.ones(cal_pcc.shape, dtype=bool)
+            np.fill_diagonal(mask, 0)
+            cal_pcc = np.min(cal_pcc[mask])
+
+            if isinstance(cal_pcc, np.ma.core.MaskedConstant):
+                return 1.0
+
+            return cal_pcc
+
+    cal_pcc = get_pcc(golden, calculated)
+
+    return (
+        cal_atol,
+        cal_rtol,
+        cal_pcc,
+        f"Max ATOL Delta: {cal_atol}, Max RTOL Delta: {cal_rtol}, PCC: {cal_pcc}",
+    )
