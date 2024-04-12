4fd0e2ea2be03add989795f220e8354ddcd60029
banekg <barsic@tenstorrent.com>
2024-04-12 11:41:23 +0000

    #7292: Add parameters to WH tan, tanh, sqrt, swiglu and other operations

--- tests/ttnn/python_api_testing/non_working_unit_tests/grayskull/test_eltwise_rad2deg.py
+++ tests/ttnn/python_api_testing/non_working_unit_tests/grayskull/test_eltwise_rad2deg.py
@@ -13,13 +13,12 @@ from tests.ttnn.utils_for_testing import assert_with_pcc
 from tests.ttnn.python_api_testing.sweep_tests import ttnn_ops
 
 
-def run_eltwise_polygamma_tests(
+def run_eltwise_rad2deg_tests(
     input_shape,
     dtype,
     dlayout,
     in_mem_config,
     output_mem_config,
-    k,
     data_seed,
     device,
 ):
@@ -28,11 +27,11 @@ def run_eltwise_polygamma_tests(
 
     try:
         # get ref result
-        ref_value = torch.polygamma(k, x)
+        ref_value = torch.rad2deg(x)
 
         x = ttnn_ops.setup_ttnn_tensor(x, device, dlayout[0], in_mem_config[0], dtype[0])
 
-        tt_result = ttnn.polygamma(x, k)
+        tt_result = ttnn.rad2deg(x)
         tt_result = ttnn_ops.ttnn_tensor_to_torch(tt_result, output_mem_config)
 
     except Exception as e:
@@ -48,37 +47,18 @@ def run_eltwise_polygamma_tests(
 test_sweep_args = [
     (
         [(192, 224)],
-        [ttnn.bfloat16],
-        [ttnn.TILE_LAYOUT],
-        [ttnn.DRAM_MEMORY_CONFIG],
-        ttnn.L1_MEMORY_CONFIG,
-        3,
-        3313947,
-    ),
-    (
-        [(96, 32)],
-        [ttnn.bfloat16],
-        [ttnn.TILE_LAYOUT],
-        [ttnn.DRAM_MEMORY_CONFIG],
-        ttnn.DRAM_MEMORY_CONFIG,
-        1,
-        18411293,
-    ),
-    (
-        [(96, 32)],
         [ttnn.bfloat8_b],
         [ttnn.TILE_LAYOUT],
         [ttnn.DRAM_MEMORY_CONFIG],
-        ttnn.DRAM_MEMORY_CONFIG,
-        1,
-        18411293,
+        ttnn.L1_MEMORY_CONFIG,
+        3313947,
     ),
 ]
 
 
 @pytest.mark.parametrize(
-    "input_shape, dtype, dlayout, in_mem_config, out_mem_config, k, data_seed",
+    "input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed",
     (test_sweep_args),
 )
-def test_eltwise_polygamma(input_shape, dtype, dlayout, in_mem_config, out_mem_config, k, data_seed, device):
-    run_eltwise_polygamma_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, k, data_seed, device)
+def test_eltwise_rad2deg(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
+    run_eltwise_rad2deg_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device)
