7a293f6c6f96e4195518acdacb0c409d9b586c4f
Aswinmcw <azayasankaran@tenstorrent.com>
2024-04-16 18:54:54 +0000

    #3019: Fix mean_hw

--- tests/tt_eager/python_api_testing/sweep_tests/tt_lib_ops.py
+++ tests/tt_eager/python_api_testing/sweep_tests/tt_lib_ops.py
@@ -486,7 +486,7 @@ def mean_hw(x, *args, device, dtype, layout, input_mem_config, output_mem_config
     t1 = ttl.tensor.mean_hw(t0, output_mem_config=output_mem_config)
 
     output = tt2torch_tensor(t1)
-    output = output.max(2, True)[0].max(3, True)[0]
+    output = output[:, :, 0, 0]
 
     return output
 
