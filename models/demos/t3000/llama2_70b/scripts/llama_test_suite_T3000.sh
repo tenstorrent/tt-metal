1c2be81cdb9ff3ac072d781f9472be057f64707b
kevinmiTT11 <kevinmi@tenstorrent.com>
2024-04-16 16:51:56 +0000

    #7459: Move demo to models/demos/t3000, Add "path to local weights" to readme, Update CI test script, Update links in Docs

--- models/demos/t3000/llama2_70b/scripts/llama_test_suite_T3000.sh
+++ models/demos/t3000/llama2_70b/scripts/llama_test_suite_T3000.sh
@@ -1,31 +1,12 @@
-#/bin/bash
+#!/bin/bash
 
-set -eo pipefail
+# Exit script if any command fails
+set -e
 
-if [[ -z "$TT_METAL_HOME" ]]; then
-  echo "Must provide TT_METAL_HOME in environment" 1>&2
-  exit 1
-fi
-
-if [[ -z "$ARCH_NAME" ]]; then
-  echo "Must provide ARCH_NAME in environment" 1>&2
-  exit 1
-fi
-
+# change directory to TT_METAL_HOME
 cd $TT_METAL_HOME
-export PYTHONPATH=$TT_METAL_HOME
 
-pytest tests/ttnn/unit_tests/test_multi_device.py
-pytest models/demos/ttnn_falcon7b/tests/multi_chip -k test_falcon_mlp
-pytest models/demos/ttnn_falcon7b/tests/multi_chip -k test_falcon_attention
-pytest models/demos/ttnn_falcon7b/tests/multi_chip -k test_falcon_decoder
-pytest models/demos/ttnn_falcon7b/tests/multi_chip -k test_falcon_causallm
-
-# Llama2_70b related cached files and tests (the test should parse env variables similar to these)
-export LLAMA_CKPT_DIR=/mnt/MLPerf/tt_dnn-models/llama-2/llama-2-70b-repacked/
-export LLAMA_TOKENIZER_PATH=/mnt/MLPerf/tt_dnn-models/llama-2/tokenizer.model
-export LLAMA_CACHE_PATH=/mnt/MLPerf/tt_dnn-models/llama-2/llama-data-cache/weights-cache-2
-
+# Run 8-chip tests on T3000s
 pytest models/demos/t3000/llama2_70b/tests/test_llama_mlp.py::test_LlamaMLP_inference[decode-8chip-T3000]
 pytest models/demos/t3000/llama2_70b/tests/test_llama_attention.py::test_LlamaAttention_inference[decode-8chip-T3000]
 pytest models/demos/t3000/llama2_70b/tests/test_llama_decoder.py::test_LlamaDecoder_inference[decode-8chip-T3000]
