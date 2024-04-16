#7386: Add better validation for sharded buffers - Add more stringent checks for row/col for BLOCK sharding - Add fix to softmax and groupnorm for HEIGHT sharding - Update silu to respect new BLOCK sharding checks - Fix incorrect shard grids for ROW_MAJOR in test_sharded_tensor - Fix incorrect strategy and shard spec in reshard tests - Add proper program cache test for reshard op - Remove manual override hash function from reshard op - Fix nlp_concat_heads grid and fix COL_MAJOR for sharded_to_from_l1 test

tests/tt_eager/python_api_testing/unit_testing/misc/test_groupnorm_sharded.py
@@ -103,7 +103,7 @@ def test_groupnorm_sharded_narrow_channel_per_group(
 ):
     torch.manual_seed(1234)
 
-    out_mem_config = ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.BLOCK_SHARDED, ttl.tensor.BufferType.L1)
+    out_mem_config = ttl.tensor.MemoryConfig(shard_layout, ttl.tensor.BufferType.L1)
     gamma_beta_mem_config = ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1)
     in0_mem_config = ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM)
 