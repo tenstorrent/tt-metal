0b3a5bfae50737a3e1b64e863e5e3c92b98203f4
banekg <barsic@tenstorrent.com>
2024-04-17 11:52:04 +0000

    #7292: Add parameters to GS preprocess_model, concat, upsample and global_avg_pool2d operations

---
+++ tests/ttnn/python_api_testing/non_working_unit_tests/grayskull/test_preprocess_model_parameter.py
@@ -0,0 +1,96 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from loguru import logger
+import random
+import pytest
+import torch
+import ttnn
+import traceback
+
+from tests.ttnn.python_api_testing.sweep_tests import ttnn_ops
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc
+
+from ttnn.model_preprocessing import preprocess_model, preprocess_model_parameters
+import transformers
+from models.demos.bert.tt import ttnn_bert
+
+from tests.tt_eager.python_api_testing.sweep_tests.model_tests import (
+    BertFeedForward,
+)
+
+
+def run_preprocess_model_params_tests(
+    input_shape,
+    dtype,
+    dlayout,
+    in_mem_config,
+    output_mem_config,
+    data_seed,
+    device,
+):
+    torch.manual_seed(data_seed)
+    x = torch.Tensor(size=input_shape[0]).uniform_(-0.1, 0.1).to(torch.bfloat16)
+
+    model_name = "phiyodr/bert-large-finetuned-squad2"
+
+    config = transformers.BertConfig.from_pretrained(model_name)
+    model = BertFeedForward(config).eval()
+    model = model.to(torch.bfloat16)
+
+    torch_hidden_states = x
+
+    try:
+        # get torch output
+        ref_value = model(torch_hidden_states)
+
+        parameters = preprocess_model_parameters(
+            initialize_model=lambda: model,
+            device=device,
+        )
+
+        hidden_states = ttnn.from_torch(torch_hidden_states, dtype[0], layout=dlayout[0], device=device)
+        tt_result = ttnn_bert.bert_feedforward(
+            config,
+            hidden_states,
+            parameters=parameters,
+        )
+        tt_result = ttnn.to_torch(tt_result)
+
+    except Exception as e:
+        logger.warning(f"Test execution crashed: {e}")
+        print(traceback.format_exc())
+        raise e
+
+    assert len(tt_result.shape) == len(ref_value.shape)
+    assert tt_result.shape == ref_value.shape
+
+    # compare tt and golden outputs
+    success, pcc_value = comp_pcc(ref_value, tt_result)
+    logger.debug(pcc_value)
+    logger.debug(success)
+
+    assert success
+
+
+# ,16305027,(),error,"TT_FATAL @ ./tt_eager/tt_dnn/op_library/bmm/bmm_op.hpp:411: input_tensor_a.get_dtype() == input_tensor_b.get_dtype()
+
+test_sweep_args = [
+    (
+        [(4, 1216, 1024)],
+        [ttnn.bfloat8_b],
+        [ttnn.TILE_LAYOUT],
+        [ttnn.DRAM_MEMORY_CONFIG],
+        ttnn.DRAM_MEMORY_CONFIG,
+        16305027,
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed",
+    (test_sweep_args),
+)
+def test_preprocess_model_params(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
+    run_preprocess_model_params_tests(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device)
