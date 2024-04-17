b44982b49cfc03b6795e4b109505e1e917bb3697
Vincent Tang <vtang@tenstorrent.com>
2024-04-17 19:07:23 +0000

    #7502: skipping mamba + unet failures

--- tests/scripts/nightly/run_models_1.sh
+++ tests/scripts/nightly/run_models_1.sh
@@ -25,7 +25,7 @@ env pytest models/experimental/roberta/tests/test_roberta_for_multiple_choice.py
 
 env pytest models/experimental/bloom/tests/test_bloom_block.py
 env pytest models/experimental/bloom/tests/test_bloom_model.py
-env pytest models/experimental/bloom/tests/test_bloom_block.py
+env pytest models/experimental/bloom/tests/test_bloom_causal_lm.py
 
 env pytest models/experimental/stable_diffusion/tests/test_cross_attn_down_block.py # -> test failing , #7536
 env pytest models/experimental/stable_diffusion/tests/test_cross_attn_up_block.py    # -> test failing, #7536
@@ -44,6 +44,6 @@ env pytest models/experimental/nanogpt/tests/test_nanogpt_block.py         # -> 
 env pytest models/experimental/nanogpt/tests/test_nanogpt_mlp.py
 env pytest models/experimental/nanogpt/tests/test_nanogpt_model.py        # failing, #7534
 
-# why is this not in test_perf_device_resnet.py, also these are literally skipped inside the test bruh
+# why is this not in test_perf_device_resnet.py, also these parameters are specifically skipped inside the test
 # env pytest models/demos/resnet/tests/test_metal_resnet50.py::test_run_resnet50_inference[HiFi4-activations_BFLOAT16-weights_BFLOAT16-batch_1]
 # env pytest models/demos/resnet/tests/test_metal_resnet50.py::test_run_resnet50_inference[HiFi4-activations_BFLOAT16-weights_BFLOAT16-batch_2]
--- tests/scripts/nightly/run_models_3.sh
+++ tests/scripts/nightly/run_models_3.sh
@@ -1,14 +1,14 @@
 # wormhole_b0 only
-# env pytest tests/ttnn/integration_tests/unet                # -> failing
+env pytest tests/ttnn/integration_tests/unet                # -> failing: issue #7556
 # env pytest tests/ttnn/integration_tests/stable_diffusion    # -> failing: Expected number of shards to be less than or equal to total number of L1 banks in compute cores
 
-env pytest models/experimental/mamba/tests/test_full_model.py       # -> failing with autoformat error
+env pytest models/experimental/mamba/tests/test_full_model.py       # -> failing with autoformat error: issue #7551
 env pytest models/experimental/mamba/tests_opt/test_full_model.py
 env pytest models/experimental/mamba/tests/test_benchmarks.py
-env pytest models/experimental/mamba/tests/test_demo.py             # -> failing with autoformat error
+env pytest models/experimental/mamba/tests/test_demo.py             # -> failing with autoformat error: issue #7551
 
 env pytest models/demos/mistral7b/tests/test_mistral_embedding.py
 env pytest models/demos/mistral7b/tests/test_mistral_rms_norm.py
 env pytest models/demos/mistral7b/tests/test_mistral_mlp.py
 env pytest models/demos/mistral7b/tests/test_mistral_attention.py
-env pytest models/demos/mistral7b/tests/test_mistral_decoder.py     # -> failing: Grid is invalid for mcast matmul!
+env pytest models/demos/mistral7b/tests/test_mistral_decoder.py     # -> failing: issue #7524
