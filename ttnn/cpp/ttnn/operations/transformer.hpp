cf6552a854efaf60ebaf7375fd9ffceb7a205963
Eyon <eyonland@gmail.com>
2024-04-18 14:24:54 +0000

    #7518: Move concatenate_heads to C++

---
+++ ttnn/cpp/ttnn/operations/transformer.hpp
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+// This is a place holder for when the cpp/ttnn folder structure and ttnn namespace is moved over to tt_eager.
+#include "tt_dnn/op_library/nlp_tms/nlp_tms.hpp"
