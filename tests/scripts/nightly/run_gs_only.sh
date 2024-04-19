e40bc6808810442db455a152fcbde9b1d5a3c7c2
Bill Teng <bteng@tenstorrent.com>
2024-04-19 04:49:18 +0000

    #0: only return 0 (success) from script if all tests succeed

--- tests/scripts/nightly/run_gs_only.sh
+++ tests/scripts/nightly/run_gs_only.sh
@@ -1,3 +1,12 @@
+#/bin/bash
+
+set -eo pipefail
+
+if [[ -z "$TT_METAL_HOME" ]]; then
+  echo "Must provide TT_METAL_HOME in environment" 1>&2
+  exit 1
+fi
+
 echo "Running nightly tests for GS only"
 
 env pytest tests/ttnn/integration_tests
