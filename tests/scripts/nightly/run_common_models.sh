665b8f3c427a28d56320f43baf1af3cbdc1aaa22
Bill Teng <bteng@tenstorrent.com>
2024-04-19 04:49:18 +0000

    #0: only return 0 (success) from script if all tests succeed

--- tests/scripts/nightly/run_common_models.sh
+++ tests/scripts/nightly/run_common_models.sh
@@ -1,3 +1,12 @@
+#/bin/bash
+
+set -eo pipefail
+
+if [[ -z "$TT_METAL_HOME" ]]; then
+  echo "Must provide TT_METAL_HOME in environment" 1>&2
+  exit 1
+fi
+
 echo "Running common models for archs"
 
 env pytest models/experimental/whisper/tests/test_whisper_attention.py
