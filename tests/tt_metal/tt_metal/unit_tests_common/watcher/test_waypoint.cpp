2226784e4023b4bacee50cc8781306e17ef21cd0
David Ma <davidma@tenstorrent.com>
2024-04-22 23:41:12 +0000

    #5973: Add support for dprint/watcher on idle eth cores (slow dispatch only for now)

--- tests/tt_metal/tt_metal/unit_tests_common/watcher/test_waypoint.cpp
+++ tests/tt_metal/tt_metal/unit_tests_common/watcher/test_waypoint.cpp
@@ -77,9 +77,12 @@ static void RunTest(WatcherFixture* fixture, Device* device) {
     }
     // Also run on ethernet cores if they're present
     bool has_eth_cores = !device->get_active_ethernet_cores(true).empty();
+    bool has_idle_eth_cores = !device->get_inactive_ethernet_cores().empty();
     // TODO: revert this when #6860 is fixed.
-    if (fixture->NumDevices() > 2) // T3000
+    if (fixture->NumDevices() > 2) {// T3000
         has_eth_cores = false;
+        has_idle_eth_cores = false;
+    }
     if (has_eth_cores) {
         KernelHandle erisc_kid;
         std::set<CoreRange> eth_core_ranges;
@@ -96,23 +99,47 @@ static void RunTest(WatcherFixture* fixture, Device* device) {
             SetRuntimeArgs(program, erisc_kid, core, args);
         }
     }
+    if (has_idle_eth_cores) {
+        KernelHandle ierisc_kid;
+        std::set<CoreRange> eth_core_ranges;
+        for (const auto& core : device->get_inactive_ethernet_cores()) {
+            eth_core_ranges.insert(CoreRange(core, core));
+        }
+        ierisc_kid = CreateKernel(
+            program,
+            "tests/tt_metal/tt_metal/test_kernels/misc/watcher_waypoints.cpp",
+            eth_core_ranges,
+            tt_metal::EthernetConfig{
+                .eth_mode = Eth::IDLE,
+                .noc = tt_metal::NOC::NOC_0
+            }
+        );
+
+        for (const auto& core : device->get_inactive_ethernet_cores()) {
+            SetRuntimeArgs(program, ierisc_kid, core, args);
+        }
+    }
 
 
     // Run the program in a new thread, we'll have to update gate values in this thread.
     fixture->RunProgram(device, program);
 
     // Check that the expected waypoints are in the watcher log, a set for each core.
-    auto check_core = [&](const CoreCoord &logical_core, const CoreCoord &phys_core, bool is_eth_core) {
+    auto check_core = [&](const CoreCoord &logical_core, const CoreCoord &phys_core, bool is_eth_core, bool is_active) {
         vector<string> expected_waypoints;
         string expected;
         // Need to update the expected strings based on each core.
         for (string waypoint : {"AAAA", "BBBB", "CCCC"}) {
             if (is_eth_core) {
-                //     blank | prefetch, dispatch kernels | 4 kernels/prev device | brisc,ncrisc,trisc kernels on current device
-                int k_id = 1 + 2 * fixture->NumDevices() + 4 * device->id() + 3;
+                //     blank | prefetch, dispatch kernels | 5 kernels/prev device | brisc,ncrisc,trisc kernels on current device
+                int k_id = 1 + 2 * fixture->NumDevices() + 5 * device->id() + 3;
+                if (!is_active)
+                    k_id += 1; // Inactive erisc 1 higher than active erisc
                 // Different k_ids for slow dispatch
                 if (fixture->IsSlowDispatch()) {
-                    k_id = 4 * (device->id() + 1);
+                    k_id = 5 * device->id() + 4;
+                    if (!is_active)
+                        k_id += 1; // Inactive erisc 1 higher than active erisc
                 }
                 string k_id_s = fmt::format("{}", k_id);
                 if (fixture->NumDevices() > 2) {
@@ -120,7 +147,7 @@ static void RunTest(WatcherFixture* fixture, Device* device) {
                     k_id_s = "";
                 }
                 expected = fmt::format(
-                    "Device {} ethnet core(x={:2},y={:2}) phys(x={:2},y={:2}):   {},   X,   X,   X,   X  k_id:{}",
+                    "Device {} ethnet core(x={:2},y={:2}) phys(x={:2},y={:2}): {},   X,   X,   X,   X  k_id:{}",
                     device->id(), logical_core.x, logical_core.y, phys_core.x, phys_core.y,
                     waypoint,
                     k_id_s
@@ -161,13 +188,19 @@ static void RunTest(WatcherFixture* fixture, Device* device) {
         for (uint32_t y = xy_start.y; y <= xy_end.y; y++) {
             CoreCoord logical_core = {x, y};
             CoreCoord phys_core = device->worker_core_from_logical_core(logical_core);
-            check_core(logical_core, phys_core, false);
+            check_core(logical_core, phys_core, false, false);
         }
     }
     if (has_eth_cores) {
         for (const auto& core : device->get_active_ethernet_cores(true)) {
             CoreCoord phys_core = device->ethernet_core_from_logical_core(core);
-            check_core(core, phys_core, true);
+            check_core(core, phys_core, true, true);
+        }
+    }
+    if (has_idle_eth_cores) {
+        for (const auto& core : device->get_inactive_ethernet_cores()) {
+            CoreCoord phys_core = device->ethernet_core_from_logical_core(core);
+            check_core(core, phys_core, true, false);
         }
     }
 }
