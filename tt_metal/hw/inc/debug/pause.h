fc9b99e6d2865737b7f493020b786727c3f0dbf6
David Ma <davidma@tenstorrent.com>
2024-04-22 23:41:12 +0000

    #5973: Add support for dprint/watcher on idle eth cores (slow dispatch only for now)

--- tt_metal/hw/inc/debug/pause.h
+++ tt_metal/hw/inc/debug/pause.h
@@ -5,6 +5,8 @@
 #pragma once
 
 #include "watcher_common.h"
+#include "ring_buffer.h"
+#include "status.h"
 
 #if defined(WATCHER_ENABLED) && !defined(WATCHER_DISABLE_PAUSE)
 
@@ -12,6 +14,9 @@ void watcher_pause() {
     // Write the pause flag for this core into the memory mailbox for host to read.
     debug_pause_msg_t tt_l1_ptr *pause_msg = GET_MAILBOX_ADDRESS_DEV(pause_status);
     pause_msg->flags[debug_get_which_riscv()] = 1;
+    WATCHER_RING_BUFFER_PUSH((uint32_t) pause_msg);
+    WATCHER_RING_BUFFER_PUSH(debug_get_which_riscv());
+    DEBUG_STATUS('P', 'A', 'U', 'S');
 
     // Wait for the pause flag to be cleared.
     while (pause_msg->flags[debug_get_which_riscv()]) {
