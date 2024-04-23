7e6b260d74d190b4d0b90b8e7991cca9ee036ea7
ppopovic <ppopovic@tenstorrent.com>
2024-04-23 12:07:05 +0000

    #7704: Fix interleaved_to_sharded_partial and sharded_to_interleaved_partial with program caching.

--- models/demos/falcon7b/tests/unit_tests/test_falcon_matmuls_and_bmms_with_mixed_precision.py
+++ models/demos/falcon7b/tests/unit_tests/test_falcon_matmuls_and_bmms_with_mixed_precision.py
@@ -479,6 +479,7 @@ def test_falcon7b_attention_softmax_sequence(
     device,
     seq_len,
     num_cores,
+    use_program_cache,
     function_level_defaults,
 ):
     compute_grid_size = device.compute_with_storage_grid_size()
