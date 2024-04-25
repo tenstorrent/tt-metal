a5903ac97371055027edfeedfc717f7c16457943
umadevimcw <umadevi@multicorewareinc.com>
2024-04-25 09:16:05 +0000

    #7644: Updates test input range for polygamma and isclose

--- tests/ttnn/python_api_testing/non_working_unit_tests/grayskull/test_eltwise_polygamma.py
+++ tests/ttnn/python_api_testing/non_working_unit_tests/grayskull/test_eltwise_polygamma.py
@@ -24,7 +24,7 @@ def run_eltwise_polygamma_tests(
     device,
 ):
     torch.manual_seed(data_seed)
-    x = torch.Tensor(size=input_shape[0]).uniform_(-100, 100).to(torch.bfloat16)
+    x = torch.Tensor(size=input_shape[0]).uniform_(1, 10).to(torch.bfloat16)
 
     try:
         # get ref result
