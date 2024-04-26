dd6d548fa1e37d05d31f997d22d6cf26cfd33cbb
Raymond Kim <rkim@tenstorrent.com>
2024-04-26 12:11:12 -0400

    #0: Put falcon7b ttnn tests back into unit tests and get rid of unstable pipeline for now

--- tests/scripts/multi_chip/run_unstable_multi_device.sh
+++ tests/scripts/multi_chip/run_unstable_multi_device.sh
@@ -11,8 +11,3 @@ if [[ -z "$ARCH_NAME" ]]; then
   echo "Must provide ARCH_NAME in environment" 1>&2
   exit 1
 fi
-
-pytest models/demos/ttnn_falcon7b/tests/multi_chip/test_falcon_mlp.py
-pytest models/demos/ttnn_falcon7b/tests/multi_chip/test_falcon_attention.py
-pytest models/demos/ttnn_falcon7b/tests/multi_chip/test_falcon_decoder.py
-pytest models/demos/ttnn_falcon7b/tests/multi_chip/test_falcon_causallm.py
