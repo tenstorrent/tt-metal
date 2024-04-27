93e0dd47a90457a4e5aedbbb5d8b293a08ab8be6
Almeet Bhullar <abhullar@tenstorrent.com>
2024-04-27 22:18:50 +0000

    #0: reduece fetchQ to 128 entries

--- tt_metal/impl/dispatch/command_queue_interface.hpp
+++ tt_metal/impl/dispatch/command_queue_interface.hpp
@@ -75,7 +75,7 @@ struct dispatch_constants {
         // make this 2^N as required by the packetized stages
         uint32_t dispatch_buffer_block_size;
         if (core_type == CoreType::WORKER) {
-            prefetch_q_entries_ = 4 * 1024;
+            prefetch_q_entries_ = 128;
             max_prefetch_command_size_ = 128 * 1024;
             cmddat_q_size_ = 256 * 1024;
             scratch_db_size_ = 128 * 1024;
