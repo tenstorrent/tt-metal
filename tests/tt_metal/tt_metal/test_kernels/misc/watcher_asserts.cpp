71c2429d1b987f2ea23f3dfdbec111d0830a4737
Umair <ucheema@tenstorrent.com>
2024-04-30 03:25:12 +0000

    #0: Enable Watcher Tests

--- tests/tt_metal/tt_metal/test_kernels/misc/watcher_asserts.cpp
+++ tests/tt_metal/tt_metal/test_kernels/misc/watcher_asserts.cpp
@@ -5,6 +5,7 @@
 #include <cstdint>
 #include "debug/assert.h"
 #include "debug/ring_buffer.h"
+#include "tt_metal/impl/dispatch/dispatch_address_map.hpp"
 
 /*
  * A test for the assert feature.
@@ -27,6 +28,25 @@ void MAIN {
     (defined(COMPILE_FOR_BRISC) || defined(COMPILE_FOR_NCRISC) || defined(COMPILE_FOR_ERISC))
     WATCHER_RING_BUFFER_PUSH(a);
     WATCHER_RING_BUFFER_PUSH(b);
+#if defined(COMPILE_FOR_BRISC) or defined(COMPILE_FOR_NCRISC) or defined(COMPILE_FOR_ERISC)
+    //For Erisc do a dummy increment since there is no worker kernel that would increment dispatch message addr to signal compute kernel completion.
+    if (a == b) {
+        //We will assert later. This kernel will hang.
+        //Need to signal completion to dispatcher before hanging so that
+        //Dispatcher Kernel is able to finish.
+        //Device Close () requires fast dispatch kernels to finish.
+        uint64_t dispatch_addr = NOC_XY_ADDR(NOC_X(DISPATCH_CORE_X), NOC_Y(DISPATCH_CORE_Y), DISPATCH_MESSAGE_ADDR);
+        noc_fast_atomic_increment(noc_index, NCRISC_AT_CMD_BUF, dispatch_addr, NOC_UNICAST_WRITE_VC, 1, 31 /*wrap*/, false /*linked*/);
+    }
+#else
+#if defined(TRISC0) or defined(TRISC1) or defined(TRISC2)
+#define GET_TRISC_RUN_EVAL(x, t) x##t
+#define GET_TRISC_RUN(x, t) GET_TRISC_RUN_EVAL(x, t)
+    volatile tt_l1_ptr uint8_t * const trisc_run = &GET_TRISC_RUN(((tt_l1_ptr mailboxes_t *)(MEM_MAILBOX_BASE))->slave_sync.trisc, COMPILE_FOR_TRISC);
+    *trisc_run = RUN_SYNC_MSG_DONE;
+#endif
+#endif
+
     ASSERT(a != b);
 #endif
 
