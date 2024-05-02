7fe7fe37e51e8c4dfe2798dcef4e39b42228dab7
Jack Cai <xuncai@tenstorrent.com>
2024-05-02 14:37:49 -0500

    #8071: Added test for nlp create head decode and following rotary matmuls. Added a new op "nlp_create_qkv_heads_decode" with placeholder code that is compatible with existing usage.

--- tt_eager/tt_dnn/op_library/nlp_tms/nlp_tms.cpp
+++ tt_eager/tt_dnn/op_library/nlp_tms/nlp_tms.cpp
@@ -62,6 +62,97 @@ tt::stl::reflection::Attributes NlpCreateHeadsFalcon7B::attributes() const {
 }
 
 
+// Generic NLP CreateHeads op for decode
+void NlpCreateHeadsDecode::validate(const std::vector<Tensor>& input_tensors) const {
+    const auto& input_tensor = input_tensors.at(0);
+    const auto input_shape = input_tensor.get_legacy_shape();
+    // TODO: Rewrite validation for this decode case
+    // NOTE: Checks for head_dim and shape[3] is done in nlp_create_qkv_heads because it's needed to infer head_dim
+    TT_FATAL(input_tensor.storage_type() == StorageType::DEVICE, "Operands to TM need to be on device!");
+    TT_FATAL(input_tensor.buffer() != nullptr, "Operands to TM need to be allocated in buffers on device!");
+    TT_FATAL(input_tensor.get_dtype() == tt::tt_metal::DataType::FLOAT32 || input_tensor.get_dtype() == tt::tt_metal::DataType::BFLOAT16 || input_tensor.get_dtype() == tt::tt_metal::DataType::BFLOAT8_B, "Unsupported data format");
+    TT_FATAL(input_tensor.get_layout() == Layout::TILE);
+
+    TT_FATAL(input_shape[2] % TILE_HEIGHT == 0, "Unsupported input shape");
+    TT_FATAL(input_shape[1] == 1, "Unsupported input shape");
+    TT_FATAL(input_tensor.is_sharded(), "Input must be sharded");
+    TT_FATAL(input_tensor.shard_spec().value().shape[0] == input_tensor.volume() / input_tensor.get_legacy_shape()[-1]);
+    TT_FATAL(this->output_mem_config.is_sharded() && this->output_mem_config.memory_layout != TensorMemoryLayout::WIDTH_SHARDED);
+    TT_FATAL(input_tensor.shard_spec().value().orientation == ShardOrientation::ROW_MAJOR);
+    auto core_grid = input_tensor.device()->compute_with_storage_grid_size();
+    uint32_t num_cores = core_grid.x * core_grid.y;
+    // 1 Head Per Core Max for now
+    TT_FATAL(this->num_q_heads <= num_cores);
+    TT_FATAL(this->num_kv_heads <= num_cores);
+    TT_FATAL(this->num_q_heads >= this->num_kv_heads);
+    TT_FATAL(this->num_q_heads % input_tensor.shard_spec().value().num_cores() == 0);
+    TT_FATAL(this->num_kv_heads % input_tensor.shard_spec().value().num_cores() == 0);
+    TT_FATAL(input_tensor.shard_spec().value().shape[1] == (this->num_q_heads / this->num_kv_heads + 2) * this->head_dim);
+}
+
+std::vector<Shape> NlpCreateHeadsDecode::compute_output_shapes(const std::vector<Tensor>& input_tensors) const {
+    std::vector<Shape> output_shape_vec;
+    const auto& input_tensor = input_tensors.at(0);
+    const auto input_shape = input_tensor.get_legacy_shape();
+
+    auto sequence_length = input_shape[2];
+    auto head_dim = this->head_dim;
+    if (sequence_length % TILE_HEIGHT != 0) {
+        sequence_length = (sequence_length / TILE_HEIGHT + 1) * TILE_HEIGHT;
+    }
+    if (head_dim % TILE_WIDTH != 0) {
+        head_dim = (head_dim / TILE_WIDTH + 1) * TILE_WIDTH;
+    }
+
+    const Shape q_output_shape = {input_shape[0], this->num_q_heads, sequence_length, head_dim};
+    const Shape v_output_shape = {input_shape[0], this->num_kv_heads, sequence_length, head_dim};
+    const Shape k_output_shape = v_output_shape;
+    output_shape_vec = {q_output_shape, k_output_shape, v_output_shape};
+
+    return output_shape_vec;
+}
+
+std::vector<Tensor> NlpCreateHeadsDecode::create_output_tensors(const std::vector<Tensor>& input_tensors) const {
+    const auto& input_tensor = input_tensors.at(0);
+    if (this->output_mem_config.is_sharded()) {
+        auto core_grid = input_tensor.device()->compute_with_storage_grid_size();
+        auto q_shard_grid = num_cores_to_corerange_set(this->num_q_heads, core_grid, true);
+        ShardSpec q_shard_spec{q_shard_grid, {TILE_HEIGHT, this->head_dim}};
+        auto q_mem_config = this->output_mem_config;
+        q_mem_config.shard_spec = q_shard_spec;
+        auto kv_shard_grid = num_cores_to_corerange_set(this->num_kv_heads, core_grid, true);
+        ShardSpec kv_shard_spec{kv_shard_grid, {TILE_HEIGHT, this->head_dim}};
+        auto kv_mem_config = this->output_mem_config;
+        kv_mem_config.shard_spec = kv_shard_spec;
+        auto output_shapes = this->compute_output_shapes(input_tensors);
+        return {
+            create_sharded_device_tensor(output_shapes[0], input_tensor.get_dtype(), input_tensor.get_layout(), input_tensor.device(), q_mem_config),
+            create_sharded_device_tensor(output_shapes[1], input_tensor.get_dtype(), input_tensor.get_layout(), input_tensor.device(), kv_mem_config),
+            create_sharded_device_tensor(output_shapes[2], input_tensor.get_dtype(), input_tensor.get_layout(), input_tensor.device(), kv_mem_config)
+        };
+
+    } else {
+        return operation::generic_create_output_tensors(*this, input_tensors, input_tensor.get_dtype(), Layout::TILE, this->output_mem_config);
+    }
+}
+
+operation::ProgramWithCallbacks NlpCreateHeadsDecode::create_program(const std::vector<Tensor>& input_tensors, std::vector<Tensor> &output_tensors) const {
+    const auto& input_tensor = input_tensors.at(0);
+    auto& output_tensor = output_tensors.at(0);
+
+    CoreCoord compute_with_storage_grid_size = input_tensor.device()->compute_with_storage_grid_size();
+    return  multi_core_nlp_create_qkv_heads_decode(input_tensor, this->num_q_heads, this->num_kv_heads, this->head_dim, output_tensors, compute_with_storage_grid_size);
+}
+
+tt::stl::reflection::Attributes NlpCreateHeadsDecode::attributes() const {
+    return {
+        {"num_q_heads", this->num_q_heads},
+        {"num_kv_heads", this->num_kv_heads},
+        {"output_mem_config", this->output_mem_config},
+    };
+}
+
+
 // Generic NLP CreateHeads op
 void NlpCreateHeads::validate(const std::vector<Tensor>& input_tensors, const std::vector<std::optional<const Tensor>>& optional_input_tensors) const {
     const auto& input_tensor = input_tensors.at(0);
