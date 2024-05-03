f53cd80eb3503ffc207a27bdc5fd6ff36ead6305
Austin Ho <aho@tenstorrent.com>
2024-05-03 06:08:09 +0000

    #0: Update dram bank offset api calls

--- tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/test_prefetcher_multichip.cpp
+++ tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/test_prefetcher_multichip.cpp
@@ -935,7 +935,7 @@ void gen_prefetcher_exec_buf_cmd_and_write_to_dram(Device *device,
     uint32_t index = 0;
     for (uint32_t page_id = 0; page_id < pages; page_id++) {
         uint32_t bank_id = page_id % num_dram_banks_g;
-        auto offset = device->dram_bank_offset_from_bank_id(bank_id);
+        auto offset = device->bank_offset(BufferType::DRAM, bank_id);
         auto dram_channel = device->dram_channel_from_bank_id(bank_id);
         auto bank_core = device->core_from_dram_channel(dram_channel);
 
@@ -1252,7 +1252,7 @@ void populate_interleaved_dram(Device *device, unordered_map<uint32_t, vector<ui
     num_dram_banks_g = device->num_banks(BufferType::DRAM);
 
     for (int bank_id = 0; bank_id < num_dram_banks_g; bank_id++) {
-        auto offset = device->dram_bank_offset_from_bank_id(bank_id);
+        auto offset = device->bank_offset(BufferType::DRAM, bank_id);
         auto dram_channel = device->dram_channel_from_bank_id(bank_id);
         auto bank_core = device->core_from_dram_channel(dram_channel);
 
@@ -1281,7 +1281,7 @@ void initialize_dram_banks(Device *device)
     auto fill = std::vector<uint32_t>(bank_size / sizeof(uint32_t), 0xBADDF00D);
 
     for (int bank_id = 0; bank_id < num_banks; bank_id++) {
-        auto offset = device->dram_bank_offset_from_bank_id(bank_id);
+        auto offset = device->bank_offset(BufferType::DRAM, bank_id);
         auto dram_channel = device->dram_channel_from_bank_id(bank_id);
         auto bank_core = device->core_from_dram_channel(dram_channel);
 
