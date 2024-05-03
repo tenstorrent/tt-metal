0964e9568f93507509ba07d3b553bec031b3c9b6
asaigal <asaigal@tenstorrent.com>
2024-05-03 23:48:50 +0000

    #0: Add async support for composite ops
      - Allow tensors created inside worker threads to be explcitly deallocated
        inside workers (composite ops such as argmax do this)
      - Uplift argmax/argmin and mish to support async mode
      - Add async mode tests for argmax and mish (multichip)

--- tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_argmax.py
+++ tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_argmax.py
@@ -24,34 +24,42 @@ from tests.tt_eager.python_api_testing.sweep_tests import comparison_funcs
     (3, 2, 1, 0, -1, -2, -3, -4),
 )
 @pytest.mark.parametrize("all", (True, False))
+@pytest.mark.parametrize(
+    "enable_async, num_loops",
+    ((True, 5), (False, 1)),
+)
 class TestArgmax:
-    def test_argmax(self, input_shapes, dim, all, device):
+    def test_argmax(self, input_shapes, dim, all, device, enable_async, num_loops):
         torch.manual_seed(0)
+        device.enable_async(enable_async)
+        for _ in range(num_loops):
+            input_data = torch.randn(input_shapes).bfloat16()
+            input_tensor = (
+                tt_lib.tensor.Tensor(input_data, tt_lib.tensor.DataType.BFLOAT16)
+                .to(tt_lib.tensor.Layout.TILE)
+                .to(device)
+            )
+            tt_output_tensor_on_device = tt_lib.tensor.argmax(input_tensor, dim=dim, all=all)
 
-        input_data = torch.randn(input_shapes).bfloat16()
-        input_tensor = (
-            tt_lib.tensor.Tensor(input_data, tt_lib.tensor.DataType.BFLOAT16).to(tt_lib.tensor.Layout.TILE).to(device)
-        )
-        tt_output_tensor_on_device = tt_lib.tensor.argmax(input_tensor, dim=dim, all=all)
-
-        tt_out_tensor = tt_output_tensor_on_device.cpu().to(tt_lib.tensor.Layout.ROW_MAJOR).to_torch()
-        if all:
-            golden_tensor = torch.argmax(input_data)
-            tt_out_tensor = tt_out_tensor[0, 0, 0, 0]
-        else:
-            golden_tensor = torch.argmax(input_data, dim=dim)
-            if dim == 1 or dim == -3 or dim == 0 or dim == -4:
-                tt_out_tensor = tt_out_tensor[0]
+            tt_out_tensor = tt_output_tensor_on_device.cpu().to(tt_lib.tensor.Layout.ROW_MAJOR).to_torch()
+            if all:
+                golden_tensor = torch.argmax(input_data)
+                tt_out_tensor = tt_out_tensor[0, 0, 0, 0]
             else:
-                if input_shapes[1] != 1 or input_shapes[0] != 1:
+                golden_tensor = torch.argmax(input_data, dim=dim)
+                if dim == 1 or dim == -3 or dim == 0 or dim == -4:
                     tt_out_tensor = tt_out_tensor[0]
                 else:
-                    tt_out_tensor = tt_out_tensor[0, 0, 0]
-        pt_out_tensor = golden_tensor
-        comp_pass, comp_out = comparison_funcs.comp_pcc(pt_out_tensor, tt_out_tensor, pcc=0.99)
-        comp_all, _ = comparison_funcs.comp_allclose(pt_out_tensor, tt_out_tensor, atol=4, rtol=1e-1)
-        logger.info(comp_pass)
-        logger.info(comp_all)
-        logger.info(comp_out)
-        status = comp_pass | comp_all
-        assert status
+                    if input_shapes[1] != 1 or input_shapes[0] != 1:
+                        tt_out_tensor = tt_out_tensor[0]
+                    else:
+                        tt_out_tensor = tt_out_tensor[0, 0, 0]
+            pt_out_tensor = golden_tensor
+            comp_pass, comp_out = comparison_funcs.comp_pcc(pt_out_tensor, tt_out_tensor, pcc=0.99)
+            comp_all, _ = comparison_funcs.comp_allclose(pt_out_tensor, tt_out_tensor, atol=4, rtol=1e-1)
+            logger.info(comp_pass)
+            logger.info(comp_all)
+            logger.info(comp_out)
+            status = comp_pass | comp_all
+            assert status
+        device.enable_async(False)
