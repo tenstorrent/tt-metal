5016a7b488ab505ab570c1856b7e9fa326fdb27e
Jack Cai <caixunshiren@gmail.com>
2024-05-07 19:53:00 +0000

    #8071: removed debug prints which are pretty funny

--- tt_eager/tt_dnn/op_library/nlp_tms/kernels/dataflow/reader_tm_tile_layout_nlp_concat_heads_decode.cpp
+++ tt_eager/tt_dnn/op_library/nlp_tms/kernels/dataflow/reader_tm_tile_layout_nlp_concat_heads_decode.cpp
@@ -31,44 +31,16 @@ void kernel_main() {
     uint32_t total_input_cores = num_x * num_y;
     uint32_t num_tiles_per_core = (head_size_num_tiles * batch) / total_input_cores;
 
-    // // debug for loop
-    // DPRINT << "[mikevin DPRINT] NOC coordinates:" << ENDL();
-    // DPRINT << "     total_input_cores: " << total_input_cores << ENDL();
-    // DPRINT << "     num_tiles_per_core: " << num_tiles_per_core << ENDL();
-    // for (uint32_t i = 0; i < num_x; i++){
-    //     for (uint32_t j = 0; j < num_y; j++){
-    //         DPRINT << "         " << in0_mcast_noc_x[i] << ", " << in0_mcast_noc_y[j] << ENDL();
-    //     }
-    // }
-
-
     uint64_t qkv_read_addr = get_noc_addr(in0_mcast_noc_x[qkv_x], in0_mcast_noc_y[qkv_y], q_start_addr) + in_tile_offset_by_head;
     uint32_t num_tiles_read_cur_core = 0;
     uint32_t q_write_addr = 0;
     uint32_t tile_size = head_size/head_size_num_tiles;
     const uint32_t cb_write_ptr_base = get_write_ptr(cb_id_q_out);
 
-    // DPRINT << "[xuncai DPRINT] head_size = " << head_size << ENDL();
-    // DPRINT << "[xuncai DPRINT] head_size_num_tiles = " << head_size_num_tiles << ENDL();
-    // DPRINT << "[xuncai DPRINT] ELEMENT_SIZE = " << ELEMENT_SIZE << ENDL();
-    // DPRINT << "[xuncai DPRINT] SUBTILE_LINE_BYTES = " << SUBTILE_LINE_BYTES << ENDL();
-    // DPRINT << "[xuncai DPRINT] tile_size = " << tile_size << ENDL();
-    // DPRINT << "[xuncai DPRINT] batch = " << batch << ENDL();
-    // DPRINT << "[xuncai DPRINT] num_kv_heads = " << num_kv_heads << ENDL();
-    // DPRINT << "[xuncai DPRINT] in_tile_offset_by_head = " << in_tile_offset_by_head << ENDL();
-
-    // Read 2 phases per tile, where there are batch * q_num_tiles tiles
-    // DPRINT << "[xuncai DPRINT] Q read:" << ENDL();
-    // DPRINT << "         qkv_read_addr = " << qkv_read_addr << ENDL();
-    // DPRINT << "         q_write_addr = " << q_write_addr << ENDL();
     for (uint32_t q = 0; q < batch; ++q) {
-        // DPRINT << "[xuncai DPRINT] q = " << q << ENDL();
         uint32_t wptr_offset = q < 16 ? q * SUBTILE_LINE_BYTES : (q - 16) * SUBTILE_LINE_BYTES + 512*ELEMENT_SIZE;
         uint32_t q_write_addr = cb_write_ptr_base + wptr_offset;
         for (uint32_t i = 0; i < head_size_num_tiles; ++i) {
-            // DPRINT << "[xuncai DPRINT] i = " << i << ENDL();
-            // DPRINT << "         qkv_read_addr = " << qkv_read_addr << ENDL();
-            // DPRINT << "         q_write_addr = " << q_write_addr << ENDL();
             // Read first phase
             if constexpr (PHASES_TO_READ == 0 || PHASES_TO_READ == 1) {
                 noc_async_read(qkv_read_addr, q_write_addr, SUBTILE_LINE_BYTES);
