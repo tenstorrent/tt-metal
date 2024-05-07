c0d43942538b4280f300cbe5dee8bd292ff5780e
banekg <barsic@tenstorrent.com>
2024-05-07 09:37:47 +0000

    #6586: Fix some ttlib yaml files

--- tests/tt_eager/python_api_testing/non_working_unit_tests/grayskull/test_eltwise_logical_andi.py
+++ tests/tt_eager/python_api_testing/non_working_unit_tests/grayskull/test_eltwise_logical_andi.py
@@ -8,8 +8,9 @@ import pytest
 import torch
 import tt_lib as ttl
 
+
+from tests.tt_eager.python_api_testing.sweep_tests import pytorch_ops
 from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc
-from tests.tt_eager.python_api_testing.sweep_tests.pytorch_ops import ne as pt_ne
 from tests.tt_eager.python_api_testing.sweep_tests.tt_lib_ops import eltwise_logical_andi as tt_eltwise_logical_andi
 
 
@@ -26,17 +27,14 @@ def run_eltwise_logical_andi_tests(
     random.seed(0)
     torch.manual_seed(data_seed)
 
-    if in_mem_config[0] == "SYSTEM_MEMORY":
-        input0_mem_config[0] = None
+    if in_mem_config == "SYSTEM_MEMORY":
+        in_mem_config = None
 
-    if in_mem_config[1] == "SYSTEM_MEMORY":
-        input0_mem_config[1] = None
-
-    x = torch.Tensor(size=input_shape[0]).uniform_(-100, 100).to(torch.bfloat16)
+    x = torch.Tensor(size=input_shape).uniform_(-100, 100).to(torch.bfloat16)
     x_ref = x.detach().clone()
 
     # get referent value
-    ref_value = torch.logical_and(x_ref, immediate)
+    ref_value = pytorch_ops.logical_andi(x_ref, immediate=immediate)
 
     # calculate tt output
     logger.info("Running eltwise_andi test")
@@ -44,9 +42,9 @@ def run_eltwise_logical_andi_tests(
         x=x,
         immediate=immediate,
         device=device,
-        dtype=dtype,
-        layout=dlayout,
-        input_mem_config=in_mem_config,
+        dtype=[dtype],
+        layout=[dlayout],
+        input_mem_config=[in_mem_config],
         output_mem_config=out_mem_config,
     )
 
@@ -57,18 +55,17 @@ def run_eltwise_logical_andi_tests(
     assert success
 
 
+# eltwise-logical_andi,"[[6, 9, 192, 128]]","{'dtype': [<DataType.BFLOAT8_B: 3>], 'layout': [<Layout.TILE: 1>], 'input_mem_config': [None], 'output_mem_config': tt::tt_metal::MemoryConfig(memory_layout=TensorMemoryLayout::INTERLEAVED,buffer_type=BufferType::L1,shard_spec=std::nullopt), 'immediate': 0}",19790443,(),error,"TT_FATAL @ /home/ubuntu/tt-metal/tt_eager/tt_dnn/op_library/bcast/bcast_op.cpp:94: input_tensor_a.get_dtype() == input_tensor_b.get_dtype()
+
 test_sweep_args = [
     (
-        [(3, 9, 96, 192), (3, 9, 96, 192)],
-        [ttl.tensor.DataType.BFLOAT8_B, ttl.tensor.DataType.BFLOAT8_B],
-        [ttl.tensor.Layout.TILE, ttl.tensor.Layout.TILE],
-        [
-            ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
-            "SYSTEM_MEMORY",
-        ],
+        (6, 9, 192, 128),
+        ttl.tensor.DataType.BFLOAT8_B,
+        ttl.tensor.Layout.TILE,
+        "SYSTEM_MEMORY",
         ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1),
         0,
-        10638326,
+        19790443,
     ),
 ]
 
