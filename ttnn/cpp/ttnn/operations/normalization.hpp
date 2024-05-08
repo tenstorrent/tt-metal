9d4b98fdf96d0ceca83e1f5b361ee8a6082a2b19
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2024-05-08 20:15:27 +0000

    #5389: updated ttnn::layer_norm and ttnn::rms_norm to use register_operation

--- ttnn/cpp/ttnn/operations/normalization.hpp
+++ ttnn/cpp/ttnn/operations/normalization.hpp
@@ -10,36 +10,120 @@ namespace ttnn {
 namespace operations {
 namespace normalization {
 
-
-inline ttnn::Tensor layer_norm(
-    const ttnn::Tensor& input_tensor,
-    float epsilon,
-    std::optional<const ttnn::Tensor> & weight,
-    std::optional<const ttnn::Tensor>& bias,
-    std::optional<const ttnn::Tensor>& residual_input_tensor,
-    const MemoryConfig& memory_config,
-    std::optional<const LayerNormProgramConfig>& program_config
-) {
-
-    const LayerNormProgramConfig&  actual_program_config = program_config.value_or(LayerNormDefaultProgramConfig{});
-
-    if (residual_input_tensor.has_value()) {
-        return tt::operations::primary::add_layernorm(input_tensor, residual_input_tensor.value(), epsilon, weight, bias, memory_config, actual_program_config);
+struct LayerNorm : tt::operations::primary::LayerNorm {
+    static inline const std::array<ttnn::TensorSchema, 4> input_tensor_schemas() {
+        return {
+            ttnn::TensorSchema{
+                2,
+                4,
+                {ttnn::bfloat16, ttnn::bfloat8_b, ttnn::bfloat4_b},
+                {ttnn::TILE_LAYOUT},
+                true,
+                false,
+                false,
+                false},
+            ttnn::TensorSchema{
+                1,
+                4,
+                {ttnn::bfloat16, ttnn::bfloat8_b, ttnn::bfloat4_b},
+                {ttnn::TILE_LAYOUT, ttnn::ROW_MAJOR_LAYOUT},
+                true,
+                false,
+                false,
+                true},
+            ttnn::TensorSchema{
+                1,
+                4,
+                {ttnn::bfloat16, ttnn::bfloat8_b, ttnn::bfloat4_b},
+                {ttnn::TILE_LAYOUT, ttnn::ROW_MAJOR_LAYOUT},
+                true,
+                false,
+                false,
+                true},
+            ttnn::TensorSchema{
+                1,
+                4,
+                {ttnn::bfloat16, ttnn::bfloat8_b, ttnn::bfloat4_b},
+                {ttnn::TILE_LAYOUT, ttnn::ROW_MAJOR_LAYOUT},
+                true,
+                false,
+                false,
+                true}};
     }
-    else {
-        return tt::operations::primary::layernorm(input_tensor, epsilon, weight, bias, memory_config, actual_program_config);
-    }
-}
 
-inline ttnn::Tensor rms_norm(
-    const ttnn::Tensor& input_tensor,
-    const ttnn::Tensor& weight,
-    float epsilon = 1e-6
-) {
-    const MemoryConfig & dram_memory_config = tt::tt_metal::MemoryConfig{.memory_layout=tt::tt_metal::TensorMemoryLayout::INTERLEAVED,.buffer_type=tt::tt_metal::BufferType::DRAM};
-    return tt::operations::primary::rmsnorm(input_tensor, epsilon, std::optional<const ttnn::Tensor>(weight), std::nullopt, dram_memory_config);
-}
+    template <typename... Args>
+    static auto input_tensors_to_validate(
+        const Tensor& input_tensor,
+        float epsilon = 1e-12,
+        const std::optional<const ttnn::Tensor>& weight = std::nullopt,
+        const std::optional<const ttnn::Tensor>& bias = std::nullopt,
+        const std::optional<const ttnn::Tensor>& residual_input_tensor = std::nullopt,
+        Args&&... args) {
+        return std::make_tuple(input_tensor, weight, bias, residual_input_tensor);
+    };
+
+    static inline ttnn::Tensor execute(
+        const ttnn::Tensor& input_tensor,
+        float epsilon = 1e-12,
+        const std::optional<const ttnn::Tensor>& weight = std::nullopt,
+        const std::optional<const ttnn::Tensor>& bias = std::nullopt,
+        const std::optional<const ttnn::Tensor>& residual_input_tensor = std::nullopt,
+        const std::optional<MemoryConfig>& memory_config_arg = std::nullopt,
+        const std::optional<const LayerNormProgramConfig>& program_config_arg = std::nullopt) {
+        const LayerNormProgramConfig& program_config = program_config_arg.value_or(LayerNormDefaultProgramConfig{});
+
+        auto memory_config = memory_config_arg.value_or(input_tensor.memory_config());
+        if (residual_input_tensor.has_value()) {
+            return tt::operations::primary::add_layernorm(
+                input_tensor, residual_input_tensor.value(), epsilon, weight, bias, memory_config, program_config);
+        } else {
+            return tt::operations::primary::layernorm(
+                input_tensor, epsilon, weight, bias, memory_config, program_config);
+        }
+    }
+};
+
+struct RMSNorm : tt::operations::primary::LayerNorm {
+    static inline const std::array<ttnn::TensorSchema, 2> input_tensor_schemas() {
+        return {
+            ttnn::TensorSchema{
+                2,
+                4,
+                {ttnn::bfloat16, ttnn::bfloat8_b, ttnn::bfloat4_b},
+                {ttnn::TILE_LAYOUT},
+                true,
+                false,
+                false,
+                false},
+            ttnn::TensorSchema{
+                1,
+                4,
+                {ttnn::bfloat16, ttnn::bfloat8_b, ttnn::bfloat4_b},
+                {ttnn::TILE_LAYOUT, ttnn::ROW_MAJOR_LAYOUT},
+                true,
+                false,
+                false,
+                false}};
+    }
+
+    template <typename... Args>
+    static auto input_tensors_to_validate(const Tensor& input_tensor, const Tensor& weight, Args&&... args) {
+        return std::make_tuple(input_tensor, weight);
+    };
+
+    static inline ttnn::Tensor execute(
+        const ttnn::Tensor& input_tensor,
+        const ttnn::Tensor& weight,
+        float epsilon = 1e-12,
+        const std::optional<MemoryConfig>& memory_config_arg = std::nullopt) {
+        auto memory_config = memory_config_arg.value_or(input_tensor.memory_config());
+        return tt::operations::primary::rmsnorm(input_tensor, epsilon, weight, std::nullopt, memory_config);
+    }
+};
 
 } // namespace normalization
 } // namespace operations
+
+constexpr auto layer_norm = ttnn::register_operation<ttnn::operations::normalization::LayerNorm>("ttnn::layer_norm");
+constexpr auto rms_norm = ttnn::register_operation<ttnn::operations::normalization::RMSNorm>("ttnn::rms_norm");
 } // namespace ttnn
