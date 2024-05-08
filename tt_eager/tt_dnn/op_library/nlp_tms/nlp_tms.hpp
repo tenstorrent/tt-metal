74e0722cd852d57626b11e78e22f5134853acd1f
Colman Glagovich <cglagovich@tenstorrent.com>
2024-05-08 23:25:22 +0000

    #8189: support async create_heads
    (cherry picked from commit 09213560bc7a4443fdebe9c0fd274f17b8c83218)

--- tt_eager/tt_dnn/op_library/nlp_tms/nlp_tms.hpp
+++ tt_eager/tt_dnn/op_library/nlp_tms/nlp_tms.hpp
@@ -147,14 +147,23 @@ inline std::vector<Tensor> nlp_create_qkv_heads_decode(
     const uint32_t num_heads, std::optional<const uint32_t> num_kv_heads,
     const MemoryConfig& mem_config
 ) {
-    const uint32_t num_kv_heads_val = num_kv_heads.value_or(num_heads);
+    std::vector<Tensor> output_tensors = {Tensor(operation::get_workers_for_op_output({input_tensor})),
+                                        Tensor(operation::get_workers_for_op_output({input_tensor})),
+                                        Tensor(operation::get_workers_for_op_output({input_tensor}))};
+    operation::launch_op(
+        [num_heads, num_kv_heads, mem_config] (std::vector<Tensor> input_tensors, const std::vector<std::optional<const Tensor>>& optional_input_tensors) mutable -> std::vector<Tensor> {
+            const uint32_t num_kv_heads_val = num_kv_heads.value_or(num_heads);
 
-    // Infer head_dim
-    uint32_t head_dim;
-    TT_FATAL(input_tensor.get_legacy_shape()[3] % (num_heads + 2 * num_kv_heads_val) == 0, "Unsupported input shape");
-    head_dim = input_tensor.get_legacy_shape()[3] / (num_heads + 2 * num_kv_heads_val);
+            // Infer head_dim
+            uint32_t head_dim;
+            TT_FATAL(input_tensors.at(0).get_legacy_shape()[3] % (num_heads + 2 * num_kv_heads_val) == 0, "Unsupported input shape");
+            head_dim = input_tensors.at(0).get_legacy_shape()[3] / (num_heads + 2 * num_kv_heads_val);
 
-    return operation::run(NlpCreateHeadsDecode{num_heads, num_kv_heads_val, head_dim, mem_config}, {input_tensor});
+            return operation::run(NlpCreateHeadsDecode{num_heads, num_kv_heads_val, head_dim, mem_config}, input_tensors);
+
+            return operation::run(NlpConcatHeadsDecode{num_heads}, input_tensors);
+        }, {input_tensor}, output_tensors);
+    return output_tensors;
 }
 inline std::vector<Tensor> nlp_create_qkv_heads(
     const Tensor &input_tensor, std::optional<const Tensor> input_tensor_kv,
