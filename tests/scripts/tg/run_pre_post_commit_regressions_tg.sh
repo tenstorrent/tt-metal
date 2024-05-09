9dd3c5895dde422ef4e9a2e28bb807213907a491
Tapasvi Patel <tpatel@tenstorrent.com>
2024-05-09 17:09:14 +0000

    #8311: Add TG workflow and pipeline tests

---
+++ tests/scripts/tg/run_pre_post_commit_regressions_tg.sh
@@ -0,0 +1,17 @@
+
+#/bin/bash
+
+set -eo pipefail
+
+if [[ -z "$TT_METAL_HOME" ]]; then
+  echo "Must provide TT_METAL_HOME in environment" 1>&2
+  exit 1
+fi
+
+if [[ -z "$ARCH_NAME" ]]; then
+  echo "Must provide ARCH_NAME in environment" 1>&2
+  exit 1
+fi
+
+# Write tests here!
+echo "Fill me!"
