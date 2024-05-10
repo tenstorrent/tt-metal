e37f18d849b52b599f8050914f0b94eedbe7d49c
Nitika Shanker <nshanker@tenstorrent.com>
2024-05-10 21:38:46 +0000

    #0: wip

--- tt_eager/tt_dnn/op_library/conv/optimized_conv_op.hpp
+++ tt_eager/tt_dnn/op_library/conv/optimized_conv_op.hpp
@@ -67,6 +67,17 @@ struct OptimizedConvBlockConfig {
 operation::ProgramWithCallbacks multi_core_optimized_conv_(const Tensor& a, const Tensor &b, const Shape& ashape, std::optional<const Tensor> bias, vector<int> conv_params, uint32_t output_channels, bool untilize_out, bool has_bias, bool fuse_relu, const MathFidelity math_fidelity, const OptimizedConvParallelizationConfig& parallelization_config, const OptimizedConvBlockConfig& block_config, uint32_t extra_padding_for_32B_alignment, Tensor &output);
 operation::ProgramWithCallbacks multi_core_optimized_conv_sharded_(const Tensor& a, const Tensor &b, const Shape& ashape, std::optional<const Tensor> bias, vector<int> conv_params, uint32_t output_channels, bool untilize_out, bool has_bias, bool fuse_relu, const MathFidelity math_fidelity, const OptimizedConvParallelizationConfig& parallelization_config, const OptimizedConvBlockConfig& block_config, uint32_t extra_padding_for_32B_alignment, Tensor &output);
 operation::ProgramWithCallbacks multi_core_optimized_conv_sharded_v2_(const Tensor& a, const Tensor &b, const Shape& ashape, std::optional<const Tensor> bias, const std::optional<const Tensor> conv_reader_indices, vector<int> conv_params, uint32_t output_channels, bool untilize_out, bool has_bias, bool fuse_relu, const OptimizedConvParallelizationConfig& parallelization_config, const OptimizedConvBlockConfig& block_config, uint32_t extra_padding_for_32B_alignment, bool use_shallow_conv_variant, bool transpose_mcast, Tensor &output, DeviceComputeKernelConfig compute_kernel_config);
+operation::ProgramWithCallbacks multi_core_optimized_conv_sharded_v2_new(const Tensor& a, const Tensor &b, std::optional<const Tensor> bias,
+    const SlidingWindowConfig& sliding_window_config,
+    uint32_t output_channels,
+    bool untilize_out, bool fuse_relu, MathFidelity math_fidelity,
+    const OptimizedConvParallelizationConfig& parallelization_config,
+    const OptimizedConvBlockConfig& block_config, uint32_t extra_padding_for_32B_alignment=0,
+    DataType output_dtype,
+    std::array<std::uint32_t, 4> input_tensor_shape,
+    bool use_shallow_conv_variant,
+    DeviceComputeKernelConfig compute_kernel_config,
+    Tensor& output);
 
 struct OptimizedConv {
     OptimizedConvParallelizationConfig parallelization_config;
@@ -157,18 +168,90 @@ Tensor optimized_conv(const Tensor& a, const Tensor &b, std::optional<const Tens
 );
 
 // new micro op
+struct OptimizedConvNew {
+    OptimizedConvParallelizationConfig parallelization_config;
+    OptimizedConvBlockConfig block_config;
+    const SlidingWindowConfig sliding_window_config;
+    const uint32_t output_channels;
+    bool untilize_out, has_bias, fuse_relu;
+    MathFidelity math_fidelity;
+    uint32_t extra_padding_for_32B_alignment;
+    MemoryConfig output_mem_config;
+    const DataType output_dtype;
+    std::array<std::uint32_t, 4> input_tensor_shape; // For sharded input, input tensor shape is nonsense
+    bool use_shallow_conv_variant;
+    const DeviceComputeKernelConfig compute_kernel_config;
+    OptimizedConvNew(const SlidingWindowConfig&s_config,
+        uint32_t output_channels, bool untile_out,
+        bool has_bias, bool fuse_relu,
+        MathFidelity mfidelity, const OptimizedConvParallelizationConfig& p_config,
+        const OptimizedConvBlockConfig& b_config,
+        uint32_t e_padding_for_32B_alignment, MemoryConfig out_mem_config,
+        DataType output_dtype,
+        std::array<std::uint32_t, 4> input_tensor_shape, bool use_shallow_conv_variant,
+        const DeviceComputeKernelConfig compute_kernel_config) :
+            output_channels(output_channels),
+            sliding_window_config(s_config),
+            untilize_out(untile_out),
+            has_bias(has_bias),
+            fuse_relu(fuse_relu),
+            math_fidelity(mfidelity),
+            parallelization_config(p_config),
+            block_config(b_config),
+            extra_padding_for_32B_alignment(e_padding_for_32B_alignment),
+            output_mem_config(out_mem_config),
+            output_dtype(output_dtype), input_tensor_shape(input_tensor_shape),
+            use_shallow_conv_variant(use_shallow_conv_variant),
+            compute_kernel_config(compute_kernel_config) {}
+
+    void validate(const std::vector<Tensor>& input_tensors, const std::vector<std::optional<const Tensor>>& optional_input_tensors) const;
+    std::vector<Shape> compute_output_shapes(const std::vector<Tensor>& input_tensors) const;
+    std::vector<Tensor> create_output_tensors(const std::vector<Tensor>& input_tensors) const;
+    operation::ProgramWithCallbacks create_program(const std::vector<Tensor>& input_tensors, const std::vector<std::optional<const Tensor>>& optional_input_tensors, std::vector<Tensor> &output_tensors) const;
+
+    operation::OpPerformanceModel create_op_performance_model(const std::vector<Tensor>& input_tensors, const std::vector<std::optional<const Tensor>>& optional_input_tensors, const std::vector<Tensor> &output_tensors) const;
+
+    static constexpr auto attribute_names = std::make_tuple(
+        "parallelization_config",
+        "block_config",
+        "sliding_window_config",
+        "output_channels",
+        "untilize_out",
+        "has_bias",
+        "fuse_relu",
+        "math_fidelity",
+        "extra_padding_for_32B_alignment",
+        "output_dtype",
+        "input_tensor_shape",
+        "use_shallow_conv_variant");
+    const auto attribute_values() const {
+        return std::make_tuple(
+            std::cref(this->parallelization_config),
+            std::cref(this->block_config),
+            std::cref(this->sliding_window_config),
+            std::cref(this->output_channels),
+            std::cref(this->untilize_out),
+            std::cref(this->has_bias),
+            std::cref(this->fuse_relu),
+            std::cref(this->math_fidelity),
+            std::cref(this->extra_padding_for_32B_alignment),
+            std::cref(this->output_dtype),
+            std::cref(this->input_tensor_shape),
+            std::cref(this->use_shallow_conv_variant));
+    }
+};
+
 Tensor optimized_conv_new(const Tensor& a, const Tensor &b, std::optional<const Tensor> bias,
     const SlidingWindowConfig& sliding_window_config,
-    const ParallelConfig& parallel_config,
     uint32_t output_channels,
     bool untilize_out, bool fuse_relu, MathFidelity math_fidelity,
     const OptimizedConvParallelizationConfig& parallelization_config,
-    const OptimizedConvBlockConfig& block_config, uint32_t extra_padding_for_32B_alignment=0,
-    std::optional<MemoryConfig> output_mem_config = std::nullopt,
-    std::optional<DataType> output_dtype=std::nullopt,
-    std::optional<std::array<std::uint32_t, 4>> input_tensor_shape = std::nullopt,
-    bool use_shallow_conv_variant = false,
-    std::optional<const DeviceComputeKernelConfig> compute_kernel_config = std::nullopt
+    const OptimizedConvBlockConfig& block_config, uint32_t extra_padding_for_32B_alignment,
+    MemoryConfig output_mem_config,
+    DataType output_dtype,
+    std::array<std::uint32_t, 4> input_tensor_shape,
+    bool use_shallow_conv_variant,
+    const DeviceComputeKernelConfig& compute_kernel_config
 );
 
 }  // namespace tt_metal
