98646cf642b37542d5a5d4ba3c0d226bbce74c42
Artem Yerofieiev <artem@tenstorrent.com>
2024-05-10 18:06:34 +0000

    #8114: move softplus to ttnn c++

--- ttnn/cpp/pybind11/operations/unary.hpp
+++ ttnn/cpp/pybind11/operations/unary.hpp
@@ -94,11 +94,49 @@ void bind_unary_with_bool_parameter_set_to_false_by_default(py::module& module, 
             py::arg("memory_config") = std::nullopt});
 }
 
+void bind_softplus(py::module& module) {
+    auto doc = fmt::format(
+        R"doc({0}(input_tensor: ttnn.Tensor, *, beta: float = 1.0, threshold: float = 20.0, memory_config: Optional[ttnn.MemoryConfig] = None) -> ttnn.Tensor
+
+        Applies {0} to :attr:`input_tensor` element-wise.
+
+        .. math::
+            {0}(\\mathrm{{input\\_tensor}}_i)
+
+        Args:
+            * :attr:`input_tensor`
+
+        Keyword Args:
+            * :attr:`beta` (float): Scales the input before applying the Softplus function. By modifying beta, you can adjust the steepness of the function. A higher beta value makes the function steeper, approaching a hard threshold like the ReLU function for large values of beta
+            * :attr:`threshold` (float): Used to switch to a linear function for large values to improve numerical stability. This avoids issues with floating-point representation for very large values
+            * :attr:`memory_config` (Optional[ttnn.MemoryConfig]): Memory configuration for the operation.
+
+        Example::
+
+            >>> tensor = ttnn.from_torch(torch.tensor((1, 2), dtype=torch.bfloat16), device=device)
+            >>> output = {1}(tensor, parameter=true)
+    )doc",
+        ttnn::softplus.name(),
+        ttnn::softplus.python_fully_qualified_name());
+
+    bind_registered_operation(
+        module,
+        ttnn::softplus,
+        doc,
+        ttnn::pybind_arguments_t{
+            py::arg("input_tensor"),
+            py::kw_only(),
+            py::arg("beta") = 1.0f,
+            py::arg("threshold") = 20.0f,
+            py::arg("memory_config") = std::nullopt});
+}
+
 }  // namespace detail
 
 void py_module(py::module& module) {
     detail::bind_unary_with_bool_parameter_set_to_false_by_default(module, ttnn::exp);
     detail::bind_unary(module, ttnn::silu);
+    detail::bind_softplus(module);
 }
 
 }  // namespace unary
