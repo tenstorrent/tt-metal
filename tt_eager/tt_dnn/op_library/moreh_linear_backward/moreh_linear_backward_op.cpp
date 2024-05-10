2f6170bc1e4f3498b8b4b8bf05ba9154c34008cc
Dongjin Na <dongjin.na@moreh.io>
2024-05-10 15:06:53 +0000

    #7838: Add non-4D support for moreh_matmul

--- tt_eager/tt_dnn/op_library/moreh_linear_backward/moreh_linear_backward_op.cpp
+++ tt_eager/tt_dnn/op_library/moreh_linear_backward/moreh_linear_backward_op.cpp
@@ -114,13 +114,12 @@ std::vector<std::optional<Tensor>> moreh_linear_backward(
 
     if (input_required_grad) {
         TT_ASSERT(input_grad.has_value(), "input_grad tensor should not be std::nullopt");
-        result[0] = moreh_matmul(output_grad, weight, input_grad, false, false, input_grad_mem_config);
+        result[0] = moreh_matmul(output_grad, weight, false, false, input_grad, std::nullopt, input_grad_mem_config);
     }
 
     if (weight_required_grad) {
-        // TODO: Add output transpose and remove transpose wh
         const auto& temp_weight_grad =
-            moreh_matmul(output_grad, input, std::nullopt, true, false, weight_grad_mem_config);
+            moreh_matmul(output_grad, input, true, false, std::nullopt, std::nullopt, weight_grad_mem_config);
         std::vector<int64_t> dims{0, 1};
         TT_ASSERT(weight_grad.has_value(), "weight_grad tensor should not be std::nullopt");
         result[1] = moreh_sum(temp_weight_grad, dims, weight_grad.value());
