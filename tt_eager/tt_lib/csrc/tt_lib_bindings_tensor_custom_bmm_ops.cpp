1b57b4016921387131a9d5964f37e7fbbe57be89
Jack Cai <caixunshiren@gmail.com>
2024-05-10 20:23:54 +0000

    #7379: Added new nlp tm, unpad to sharded for kv cache

--- tt_eager/tt_lib/csrc/tt_lib_bindings_tensor_custom_bmm_ops.cpp
+++ tt_eager/tt_lib/csrc/tt_lib_bindings_tensor_custom_bmm_ops.cpp
@@ -126,6 +126,15 @@ namespace tt::tt_metal::detail
             py::arg("input").noconvert(), py::arg("num_heads").noconvert(), py::arg("num_kv_heads").noconvert() = std::nullopt, py::arg("output_mem_config") = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, R"doc(
             Shuffles [1, S=1, B=32, head_dim * (num_heads + 2*num_kv_heads)] fused qkv matrix into Q, K, and V heads with shape [S, B, num_heads, head_dim] for Q and [S, B, num_kv_heads, head_dim] for K and V, where num_heads and num_kv_heads will be padded to nearest 32. Input must be sharded, B=32 and S=1.
         )doc");
+        m_tensor.def("nlp_concat_heads_decode", &nlp_concat_heads_decode,
+            py::arg().noconvert(), py::arg("num_heads").noconvert() = std::nullopt, R"doc(
+            Shuffles [S=1, B=32, 32(num_heads), head_dim] tensor into tensor with shape [S=1, 1, B=32, num_heads * head_dim]. num_heads should be specified and be less than 32; the op will assume the input padded num_heads to 32 and will unpad it. The output is default width sharded by num heads.
+        )doc");
+        m_tensor.def("nlp_kv_cache_unpad_to_sharded", &nlp_kv_cache_unpad_to_sharded,
+            py::arg("input").noconvert(), py::arg("seq_len_start"), py::arg("seq_len_end"), R"doc(
+            Unpad TT INTERLEAVED, TILE layout Tensor into a height sharded tensor. Typically used to unpad the KV cache from [B,n_heads,max_seq_length,head_dim] into [B,n_heads,S,head_dim], where S = seq_len_end-seq_len_start. seq_len_start and seq_len_end are the start and end of the sequence length to unpad, and must be multiples of 32.
+            Returns an output tensor that is height sharded on B x n_heads corees (note the B and n_heads dims are interchangeable), where each shard is [S, head_dim].
+        )doc");
         // More general implementation, but perf might be worse since the cbs are very small and writer calls noc_async_write_barrier() a lot
         m_tensor.def("nlp_create_qkv_heads", &nlp_create_qkv_heads,
             py::arg("input").noconvert(), py::arg("input_kv").noconvert() = std::nullopt, py::arg("num_heads").noconvert(), py::arg("num_kv_heads").noconvert() = std::nullopt, py::arg("transpose_k_heads").noconvert() = true, py::arg("output_mem_config") = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, R"doc(
@@ -134,10 +143,6 @@ namespace tt::tt_metal::detail
         m_tensor.def("nlp_concat_heads", &nlp_concat_heads,
             py::arg().noconvert(), py::arg("output_mem_config") = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, R"doc(
             Shuffles [B, num_heads, S, head_dim] tensor into tensor with shape [B, 1, S, num_heads * head_dim].
-        )doc");
-        m_tensor.def("nlp_concat_heads_decode", &nlp_concat_heads_decode,
-            py::arg().noconvert(), py::arg("num_heads").noconvert() = std::nullopt, R"doc(
-            Shuffles [S=1, B=32, 32(num_heads), head_dim] tensor into tensor with shape [S=1, 1, B=32, num_heads * head_dim]. num_heads should be specified and be less than 32; the op will assume the input padded num_heads to 32 and will unpad it. The output is default width sharded by num heads.
         )doc");
 
         // Custom Resnet matmuls
