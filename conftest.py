98eb536a9fd5e6929fb7c4c141bbab66af495287
Almeet Bhullar <abhullar@tenstorrent.com>
2024-05-14 18:06:46 +0000

    #8453: Update UMD version and t3k device indices
    #0: add submodule update to build artifact
    
    #0: Update umd device cmake to include newly added files

--- conftest.py
+++ conftest.py
@@ -375,7 +375,7 @@ def t3k_device_mesh(request, silicon_arch_name, silicon_arch_wormhole_b0):
 
     if ttnn.get_num_devices() < 8:
         pytest.skip()
-    device_ids = [0, 7, 6, 1, 2, 5, 4, 3]
+    device_ids = [0, 4, 5, 1, 2, 6, 7, 3]
     try:
         num_devices_requested = min(request.param, len(device_ids))
     except (ValueError, AttributeError):
