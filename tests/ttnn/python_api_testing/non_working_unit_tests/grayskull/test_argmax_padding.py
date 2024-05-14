fd6379bfc49c4634be5199840d10da93e4512378
umadevimcw <umadevi@multicorewareinc.com>
2024-05-14 10:18:19 +0000

    #7571: Update index module for dim 1, 0 and test files

--- tests/ttnn/python_api_testing/non_working_unit_tests/grayskull/test_argmax_padding.py
+++ tests/ttnn/python_api_testing/non_working_unit_tests/grayskull/test_argmax_padding.py
@@ -19,13 +19,11 @@ from tests.tt_eager.python_api_testing.sweep_tests import comparison_funcs
         (torch.Size([5, 4, 3, 20])),
     ),
 )
-@pytest.mark.parametrize("dim", (3, 2, -1, -2))
+@pytest.mark.parametrize("dim", (3, 2, 1, 0, -1, -2, -3, -4))
 @pytest.mark.parametrize("all", (True, False))
 class TestArgmax:
     def test_argmax(self, input_shapes, dim, all, device):
         torch.manual_seed(10)
-
-        # input_data = torch.tensor([5,3,2,1,10,9,6,7,4,8]).reshape(input_shapes).bfloat16()
         input_data = torch.randn(input_shapes).bfloat16()
         input_tensor = (
             tt_lib.tensor.Tensor(input_data, tt_lib.tensor.DataType.BFLOAT16)
@@ -34,16 +32,14 @@ class TestArgmax:
             .to(device)
         )
         tt_output_tensor_on_device = tt_lib.tensor.argmax(input_tensor, dim=dim, all=all)
-
         tt_out_tensor = tt_output_tensor_on_device.cpu().to(tt_lib.tensor.Layout.ROW_MAJOR).to_torch()
-        # tt_out_tensor = tt_output_tensor_on_device.cpu().to(tt_lib.tensor.Layout.ROW_MAJOR)
         if all:
             golden_tensor = torch.argmax(input_data)
             tt_out_tensor = tt_out_tensor[0, 0, 0, 0]
         else:
             golden_tensor = torch.argmax(input_data, dim=dim)
             if dim == 1 or dim == -3 or dim == 0 or dim == -4:
-                tt_out_tensor = tt_out_tensor[0]
+                tt_out_tensor = tt_out_tensor[0, :, 0 : input_shapes[2], 0 : input_shapes[3]]
             else:
                 if input_shapes[1] != 1 or input_shapes[0] != 1:
                     if dim == 2 or dim == -2:
