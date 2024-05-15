e7e264e0805bd1d7775829a87a548d2ffe1ec6b1
mcw-anasuya <mcw-anair@ext.tenstorrent.com>
2024-05-15 12:58:59 +0000

    #8681: Add unary_floor_div

--- tests/tt_eager/python_api_testing/sweep_tests/op_map.py
+++ tests/tt_eager/python_api_testing/sweep_tests/op_map.py
@@ -265,8 +265,12 @@ op_map = {
         "pytorch_op": pytorch_ops.div,
     },
     "eltwise-floor_div": {
-        "tt_lib_op": tt_lib_ops.eltwise_floor_div,
+        "tt_op": tt_lib_ops.eltwise_floor_div,
         "pytorch_op": pytorch_ops.floor_div,
+    },
+    "eltwise-unary_floor_div": {
+        "tt_op": tt_lib_ops.eltwise_unary_floor_div,
+        "pytorch_op": pytorch_ops.unary_floor_div,
     },
     "eltwise-div_no_nan": {
         "tt_op": tt_lib_ops.eltwise_div_no_nan,
