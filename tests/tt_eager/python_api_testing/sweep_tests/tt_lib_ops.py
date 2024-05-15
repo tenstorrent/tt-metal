53afafa32abf468fc673c2bc0942eb0e9fa971f5
MOULIRAJ-E <mcw-melamurugan@ext.tenstorrent.com>
2024-05-15 10:12:40 +0000

    #8681: Add binary floor div

--- tests/tt_eager/python_api_testing/sweep_tests/tt_lib_ops.py
+++ tests/tt_eager/python_api_testing/sweep_tests/tt_lib_ops.py
@@ -1047,6 +1047,25 @@ def eltwise_div(
 
 
 @setup_host_and_device
+def eltwise_floor_div(
+    x,
+    y,
+    *args,
+    device,
+    dtype,
+    layout,
+    input_mem_config,
+    output_mem_config,
+    **kwargs,
+):
+    t0 = setup_tt_tensor(x, device, layout[0], input_mem_config[0], dtype[0])
+    t1 = setup_tt_tensor(y, device, layout[1], input_mem_config[1], dtype[1])
+    t2 = ttl.tensor.floor_div(t0, t1, output_mem_config=output_mem_config)
+
+    return tt2torch_tensor(t2)
+
+
+@setup_host_and_device
 def eltwise_div_no_nan(
     x,
     y,
