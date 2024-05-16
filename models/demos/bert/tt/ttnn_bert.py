ff114ef87087972d5f1b5415f6b3f997b8a56c4e
Artem Yerofieiev <artem@tenstorrent.com>
2024-05-16 22:43:00 +0000

    #0: update models/example to use fallback.reshape

--- models/demos/bert/tt/ttnn_bert.py
+++ models/demos/bert/tt/ttnn_bert.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import ttnn
+import ttnn.fallback
 
 from models.experimental.functional_common.attention_mask_functions import get_extended_attention_mask
 
@@ -21,21 +22,21 @@ def bert_attention(
     query = hidden_states @ parameters.self.query.weight
     query = query + parameters.self.query.bias
     query = ttnn.to_layout(query, layout=ttnn.ROW_MAJOR_LAYOUT)
-    query = ttnn.reshape(query, (batch_size, sequence_size, num_heads, head_size))
+    query = ttnn.fallback.reshape(query, (batch_size, sequence_size, num_heads, head_size))
     query = ttnn.to_layout(query, layout=ttnn.TILE_LAYOUT)
     query = ttnn.permute(query, (0, 2, 1, 3))
 
     key = hidden_states @ parameters.self.key.weight
     key = key + parameters.self.key.bias
     key = ttnn.to_layout(key, layout=ttnn.ROW_MAJOR_LAYOUT)
-    key = ttnn.reshape(key, (batch_size, sequence_size, num_heads, head_size))
+    key = ttnn.fallback.reshape(key, (batch_size, sequence_size, num_heads, head_size))
     key = ttnn.to_layout(key, layout=ttnn.TILE_LAYOUT)
     key = ttnn.permute(key, (0, 2, 3, 1))
 
     value = hidden_states @ parameters.self.value.weight
     value = value + parameters.self.value.bias
     value = ttnn.to_layout(value, layout=ttnn.ROW_MAJOR_LAYOUT)
-    value = ttnn.reshape(value, (batch_size, sequence_size, num_heads, head_size))
+    value = ttnn.fallback.reshape(value, (batch_size, sequence_size, num_heads, head_size))
     value = ttnn.to_layout(value, layout=ttnn.TILE_LAYOUT)
     value = ttnn.permute(value, (0, 2, 1, 3))
 
@@ -49,7 +50,7 @@ def bert_attention(
     context_layer = attention_probs @ value
     context_layer = ttnn.permute(context_layer, (0, 2, 1, 3))
     context_layer = ttnn.to_layout(context_layer, ttnn.ROW_MAJOR_LAYOUT)
-    context_layer = ttnn.reshape(context_layer, (batch_size, sequence_size, hidden_size))
+    context_layer = ttnn.fallback.reshape(context_layer, (batch_size, sequence_size, hidden_size))
     context_layer = ttnn.to_layout(context_layer, ttnn.TILE_LAYOUT)
 
     self_output = context_layer
