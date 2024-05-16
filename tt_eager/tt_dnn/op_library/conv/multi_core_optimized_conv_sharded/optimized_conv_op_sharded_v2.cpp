e367cb6341164fbb7dea0474b5f831d0607f0cde
Abhinav Sarje <asarje@tenstorrent.com>
2024-05-16 20:52:59 +0000

    #0: more fixes

--- tt_eager/tt_dnn/op_library/conv/multi_core_optimized_conv_sharded/optimized_conv_op_sharded_v2.cpp
+++ tt_eager/tt_dnn/op_library/conv/multi_core_optimized_conv_sharded/optimized_conv_op_sharded_v2.cpp
@@ -1365,7 +1365,10 @@ operation::ProgramWithCallbacks multi_core_optimized_conv_sharded_v2_new(const T
     //         }
     //     }
     // }
+<<<<<<< HEAD
     bool is_block_sharded = a.memory_config().memory_layout == TensorMemoryLayout::BLOCK_SHARDED;
+=======
+>>>>>>> #0: more fixes
     auto conv_reader_indices_tensor = sliding_window::construct_on_host_config_tensor(conv_sharded_input_top_left_indices, sliding_window_config, parallel_config);
     conv_reader_indices_tensor = sliding_window::move_config_tensor_to_device(conv_reader_indices_tensor, parallel_config, is_block_sharded, a.device());
 
