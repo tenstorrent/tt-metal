a60d17cd0a351b72fb8fdbf40953000842800fd8
Artem Yerofieiev <artem@tenstorrent.com>
2024-05-17 01:43:20 +0000

    #8117: Move global_avg_pool2d to C++

--- ttnn/ttnn/__init__.py
+++ ttnn/ttnn/__init__.py
@@ -465,7 +465,7 @@ from ttnn.operations.ccl import all_gather
 from ttnn.operations import transformer
 from ttnn.operations import kv_cache
 from ttnn.operations.conv2d import Conv2d
-from ttnn.operations.maxpool2d import (
+from ttnn.operations.pool import (
     MaxPool2d,
     global_avg_pool2d,
 )
