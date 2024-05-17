63e87eac7dacca650b520ba981ec117617b8e145
asaigal <asaigal@tenstorrent.com>
2024-05-17 02:39:51 +0000

    #0: Move ttnn trace APIs to separate trace.py include

--- ttnn/ttnn/operations/core.py
+++ ttnn/ttnn/operations/core.py
@@ -668,20 +668,4 @@ def as_tensor(
         return tensor
 
 
-begin_trace_capture = ttnn.register_operation(
-    name="ttnn.begin_trace_capture",
-)(ttnn._ttnn.operations.core.begin_trace_capture)
-
-end_trace_capture = ttnn.register_operation(
-    name="ttnn.end_trace_capture",
-)(ttnn._ttnn.operations.core.end_trace_capture)
-
-execute_trace = ttnn.register_operation(
-    name="ttnn.execute_trace",
-)(ttnn._ttnn.operations.core.execute_trace)
-
-release_trace = ttnn.register_operation(
-    name="ttnn.release_trace",
-)(ttnn._ttnn.operations.core.release_trace)
-
 __all__ = []
