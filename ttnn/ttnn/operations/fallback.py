7b64aa5bf7f5e83faef65d15655c48452b12c817
Artem Yerofieiev <artem@tenstorrent.com>
2024-05-17 16:43:15 +0000

    #0: correct fallback to not throw when tensor is not on device

--- ttnn/ttnn/operations/fallback.py
+++ ttnn/ttnn/operations/fallback.py
@@ -5,12 +5,14 @@
 import sys
 import ttnn
 import torch
+import tt_lib
 
 THIS_MODULE = sys.modules[__name__]
 
 
 def reshape(tensor: ttnn.Tensor, shape) -> ttnn.Tensor:
+    device = tensor.device() if tensor.storage_type() == tt_lib.tensor.StorageType.DEVICE else None
     torch_tensor = ttnn.to_torch(tensor)
     torch_tensor = torch.reshape(torch_tensor, tuple(shape))
-    tensor = ttnn.from_torch(torch_tensor, layout=tensor.layout, dtype=tensor.dtype, device=tensor.device())
+    tensor = ttnn.from_torch(torch_tensor, layout=tensor.layout, dtype=tensor.dtype, device=device)
     return tensor
