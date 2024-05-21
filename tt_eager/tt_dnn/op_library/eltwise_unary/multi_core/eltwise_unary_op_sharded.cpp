b7bd4a957310f19c7b6faa3fb15c1ad06b665db0
Abhinav Sarje <asarje@tenstorrent.com>
2024-05-21 02:12:48 +0000

    #0: eltwise, handle bfp8 and others

--- tt_eager/tt_dnn/op_library/eltwise_unary/multi_core/eltwise_unary_op_sharded.cpp
+++ tt_eager/tt_dnn/op_library/eltwise_unary/multi_core/eltwise_unary_op_sharded.cpp
@@ -38,19 +38,17 @@ operation::ProgramWithCallbacks eltwise_unary_sharded(const Tensor &input, Tenso
 
     uint32_t num_tile_per_core = 0;
 
-    if (input.get_dtype() == DataType::BFLOAT16) {
+    if (input.get_dtype() == DataType::BFLOAT8_B) {
+        uint32_t ntiles_along_width = ceil(shard_spec.shape[1] / (float) constants::TILE_WIDTH);
+        uint32_t ntiles_along_height = ceil(shard_spec.shape[0] / (float) constants::TILE_HEIGHT);
+        num_tile_per_core = ntiles_along_width * ntiles_along_height;
+    } else {
         TT_FATAL((shard_spec.shape[1] * datum_size(act_df)) % L1_ALIGNMENT == 0, "Shard width should be multiple of L1_ADRESS_ALIGNMENT");
         size_t shard_height = shard_spec.shape[0];
         size_t shard_width = round_up_to_mul16(shard_spec.shape[1]); // rounding up is done to aligned with  --> tt-metal/tt_metal/detail/util.hpp:31
         size_t shard_size_in_bytes = shard_height * shard_width * datum_size(act_df);
         TT_FATAL(shard_size_in_bytes % input_tile_size == 0, "Shard Size must be multiple of input_tile_size");
         num_tile_per_core = (shard_size_in_bytes + input_tile_size - 1) / input_tile_size; //ceil value
-    } else if (input.get_dtype() == DataType::BFLOAT8_B) {
-        uint32_t ntiles_along_width = ceil(shard_spec.shape[1] / (float) constants::TILE_WIDTH);
-        uint32_t ntiles_along_height = ceil(shard_spec.shape[0] / (float) constants::TILE_HEIGHT);
-        num_tile_per_core = ntiles_along_width * ntiles_along_height;
-    } else {
-        TT_FATAL(false, "Unsupported data type");
     }
 
     uint32_t in_cb_id = CB::c_in0;
