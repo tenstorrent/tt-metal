0dbed45181af152a788dd8f1992fb12234a2a5e6
KalaivaniMCW <kbaskar@tenstorrent.com>
2024-05-21 13:14:20 +0000

    #5044: add optional output and q_id to bw binary EQ

--- tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_binary_eq.py
+++ tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_binary_eq.py
@@ -18,10 +18,97 @@ from tests.tt_eager.python_api_testing.unit_testing.backward_ops.utility_funcs i
 )
 def test_bw_binary_eq(input_shapes, device):
     in_data, input_tensor = data_gen_with_range(input_shapes, -100, 100, device, True)
-    grad_data, grad_tensor = data_gen_with_range(input_shapes, -100, 100, device)
+    other_data, other_tensor = data_gen_with_range(input_shapes, -90, 100, device, True)
+    _, grad_tensor = data_gen_with_range(input_shapes, -20, 40, device)
 
-    tt_output_tensor_on_device = tt_lib.tensor.binary_eq_bw(grad_tensor, input_tensor)
-    pt_y = torch.zeros_like(grad_data)
-    golden_tensor = [pt_y, pt_y]
+    tt_output_tensor_on_device = tt_lib.tensor.binary_eq_bw(grad_tensor, input_tensor, other_tensor)
+    in_grad = torch.zeros_like(in_data)
+    other_grad = torch.zeros_like(other_data)
+
+    golden_tensor = [in_grad, other_grad]
     comp_pass = compare_pcc(tt_output_tensor_on_device, golden_tensor)
     assert comp_pass
+
+
+@pytest.mark.parametrize(
+    "input_shapes",
+    (
+        (torch.Size([1, 1, 32, 32])),
+        (torch.Size([1, 1, 320, 384])),
+        (torch.Size([1, 3, 320, 384])),
+    ),
+)
+@pytest.mark.parametrize("are_required_outputs", [[True, True], [True, False], [False, True]])
+def test_bw_binary_eq_opt_output(input_shapes, device, are_required_outputs):
+    in_data, input_tensor = data_gen_with_range(input_shapes, -100, 100, device, True)
+    other_data, other_tensor = data_gen_with_range(input_shapes, -90, 100, device, True)
+    _, grad_tensor = data_gen_with_range(input_shapes, -20, 40, device)
+    input_grad = None
+    other_grad = None
+    if are_required_outputs[0]:
+        _, input_grad = data_gen_with_range(input_shapes, -1, 1, device)
+    if are_required_outputs[1]:
+        _, other_grad = data_gen_with_range(input_shapes, -1, 1, device)
+
+    tt_output_tensor_on_device = tt_lib.tensor.binary_eq_bw(
+        grad_tensor,
+        input_tensor,
+        other_tensor,
+        are_required_outputs=are_required_outputs,
+        input_grad=input_grad,
+        other_grad=other_grad,
+    )
+
+    in_grad = torch.zeros_like(in_data)
+    other_grad = torch.zeros_like(other_data)
+
+    golden_tensor = [in_grad, other_grad]
+
+    status = True
+    for i in range(len(are_required_outputs)):
+        if are_required_outputs[i]:
+            status = status & compare_pcc([tt_output_tensor_on_device[i]], [golden_tensor[i]])
+    assert status
+
+
+@pytest.mark.parametrize(
+    "input_shapes",
+    (
+        (torch.Size([1, 1, 32, 32])),
+        (torch.Size([1, 1, 320, 384])),
+    ),
+)
+@pytest.mark.parametrize("are_required_outputs", [[True, True], [True, False], [False, True]])
+def test_bw_binary_eq_opt_output_qid(input_shapes, device, are_required_outputs):
+    in_data, input_tensor = data_gen_with_range(input_shapes, -100, 100, device, True)
+    other_data, other_tensor = data_gen_with_range(input_shapes, -90, 100, device, True)
+    _, grad_tensor = data_gen_with_range(input_shapes, -20, 40, device)
+    input_grad = None
+    other_grad = None
+    if are_required_outputs[0]:
+        _, input_grad = data_gen_with_range(input_shapes, -1, 1, device)
+    if are_required_outputs[1]:
+        _, other_grad = data_gen_with_range(input_shapes, -1, 1, device)
+
+    queue_id = 0
+
+    tt_output_tensor_on_device = tt_lib.tensor.binary_eq_bw(
+        queue_id,
+        grad_tensor,
+        input_tensor,
+        other_tensor,
+        are_required_outputs=are_required_outputs,
+        input_grad=input_grad,
+        other_grad=other_grad,
+    )
+
+    in_grad = torch.zeros_like(in_data)
+    other_grad = torch.zeros_like(other_data)
+
+    golden_tensor = [in_grad, other_grad]
+
+    status = True
+    for i in range(len(are_required_outputs)):
+        if are_required_outputs[i]:
+            status = status & compare_pcc([tt_output_tensor_on_device[i]], [golden_tensor[i]])
+    assert status
