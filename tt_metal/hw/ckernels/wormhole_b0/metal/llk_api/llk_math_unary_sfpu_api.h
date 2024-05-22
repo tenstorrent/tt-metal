cec5aaf7b94316bc236a3de584a5c47c0b51efa1
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2024-05-22 07:56:16 +0000

    #8683: Add Unary right shift support for WH_B0

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_math_unary_sfpu_api.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_math_unary_sfpu_api.h
@@ -25,3 +25,4 @@
 #include "llk_math_eltwise_unary_sfpu_unary_comp.h"
 #include "llk_math_eltwise_unary_sfpu_trigonometry.h"
 #include "llk_math_eltwise_unary_sfpu_unary_comp.h"
+#include "llk_math_eltwise_unary_sfpu_right_shift.h"
