edeb5db6f23f94e1d776402f22e2eabea3830905
Dongjin Na <dongjin.na@moreh.io>
2024-05-23 05:30:15 +0000

    #8632: Update fp32 dest acc support in moreh_sum_nc

--- tt_eager/tt_dnn/op_library/moreh_sum/moreh_sum_nc_impl/kernels/moreh_sum_nc.cpp
+++ tt_eager/tt_dnn/op_library/moreh_sum/moreh_sum_nc_impl/kernels/moreh_sum_nc.cpp
@@ -34,6 +34,9 @@ void MAIN {
             }
 
             tile_regs_acquire();
+            #if defined FP32_DEST_ACC_EN
+                unpack_reconfig_data_format(cb_in0, cb_add);
+            #endif
             add_tiles_init(cb_in0, cb_add);
             add_tiles(cb_in0, cb_add, first_tile, first_tile, dst0);
             tile_regs_commit();
@@ -46,6 +49,9 @@ void MAIN {
             uint32_t cb_out = (last_out) ? (cb_out0) : (cb_intermed0);
             cb_reserve_back(cb_out, onetile);
             tile_regs_wait();
+            #if defined FP32_DEST_ACC_EN
+                pack_reconfig_data_format(cb_out);
+            #endif
             pack_tile(dst0, cb_out);
             tile_regs_release();
             cb_push_back(cb_out, onetile);
