dafc6de0b1a71027ae67ca8c25e4d30a1acfc14b
Artem Yerofieiev <artem@tenstorrent.com>
2024-05-23 04:57:48 +0000

    #8364: Remote allow_to_fallback_to_golden_function_on_failure flag from ttnn.Operation

--- ttnn/ttnn/operations/core.py
+++ ttnn/ttnn/operations/core.py
@@ -39,7 +39,6 @@ def _golden_function(input_tensor: ttnn.Tensor, slices):
     validate_input_tensors=_getitem_validate_input_tensors,
     is_method=True,
     golden_function=_golden_function,
-    allow_to_fallback_to_golden_function_on_failure=False,
 )
 def __getitem__(input_tensor: ttnn.Tensor, slices) -> ttnn.Tensor:
     input_rank = len(input_tensor.shape)
@@ -187,7 +186,6 @@ reshape = ttnn.register_operation(
     golden_function=_golden_function,
     preprocess_golden_function_inputs=_preprocess_golden_function_inputs,
     postprocess_golden_function_outputs=_postprocess_golden_function_outputs,
-    allow_to_fallback_to_golden_function_on_failure=False,
     doc=doc,
 )(ttnn._ttnn.operations.core.reshape)
 
