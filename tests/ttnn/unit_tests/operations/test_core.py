fba6803a8f2a272aa85356e1d037d4ac1be706c3
tarafdarTT <ntarafdar@tenstorrent.com>
2024-05-24 19:23:53 +0000

    #0: remove test too large for wh

--- tests/ttnn/unit_tests/operations/test_core.py
+++ tests/ttnn/unit_tests/operations/test_core.py
@@ -199,93 +199,6 @@ from enum import Enum
             (32, 96),
         ),
         (
-            15680,
-            512,
-            ttnn.TILE_LAYOUT,
-            dict(
-                core_grid=ttnn.experimental.tensor.CoreRangeSet(
-                    {
-                        ttnn.experimental.tensor.CoreRange(
-                            ttnn.experimental.tensor.CoreCoord(0, 0), ttnn.experimental.tensor.CoreCoord(11, 7)
-                        ),
-                        ttnn.experimental.tensor.CoreRange(
-                            ttnn.experimental.tensor.CoreCoord(0, 8), ttnn.experimental.tensor.CoreCoord(1, 8)
-                        ),
-                    }
-                ),
-                strategy=ttnn.ShardStrategy.HEIGHT,
-            ),
-            dict(
-                core_grid=ttnn.experimental.tensor.CoreRangeSet(
-                    {
-                        ttnn.experimental.tensor.CoreRange(
-                            ttnn.experimental.tensor.CoreCoord(0, 0), ttnn.experimental.tensor.CoreCoord(11, 7)
-                        ),
-                    }
-                ),
-                strategy=ttnn.ShardStrategy.BLOCK,
-                orientation=ttnn.ShardOrientation.COL_MAJOR,
-            ),
-            (160, 512),
-            (64, 1312),
-        ),
-        (
-            2560,
-            512,
-            ttnn.TILE_LAYOUT,
-            dict(
-                core_grid=ttnn.experimental.tensor.CoreRangeSet(
-                    {
-                        ttnn.experimental.tensor.CoreRange(
-                            ttnn.experimental.tensor.CoreCoord(0, 0), ttnn.experimental.tensor.CoreCoord(3, 3)
-                        ),
-                    }
-                ),
-                strategy=ttnn.ShardStrategy.HEIGHT,
-            ),
-            dict(
-                core_grid=ttnn.experimental.tensor.CoreRangeSet(
-                    {
-                        ttnn.experimental.tensor.CoreRange(
-                            ttnn.experimental.tensor.CoreCoord(0, 0), ttnn.experimental.tensor.CoreCoord(1, 7)
-                        ),
-                    }
-                ),
-                strategy=ttnn.ShardStrategy.BLOCK,
-                orientation=ttnn.ShardOrientation.COL_MAJOR,
-            ),
-            (160, 512),
-            (64, 1312),
-        ),
-        (
-            2560,
-            128,
-            ttnn.TILE_LAYOUT,
-            dict(
-                core_grid=ttnn.experimental.tensor.CoreRangeSet(
-                    {
-                        ttnn.experimental.tensor.CoreRange(
-                            ttnn.experimental.tensor.CoreCoord(0, 0), ttnn.experimental.tensor.CoreCoord(3, 3)
-                        ),
-                    }
-                ),
-                strategy=ttnn.ShardStrategy.HEIGHT,
-            ),
-            dict(
-                core_grid=ttnn.experimental.tensor.CoreRangeSet(
-                    {
-                        ttnn.experimental.tensor.CoreRange(
-                            ttnn.experimental.tensor.CoreCoord(0, 0), ttnn.experimental.tensor.CoreCoord(1, 1)
-                        ),
-                    }
-                ),
-                strategy=ttnn.ShardStrategy.BLOCK,
-                orientation=ttnn.ShardOrientation.COL_MAJOR,
-            ),
-            (160, 128),
-            (64, 1312),
-        ),
-        (
             192,
             128,
             ttnn.TILE_LAYOUT,
@@ -362,7 +275,6 @@ from enum import Enum
                     }
                 ),
                 strategy=ttnn.ShardStrategy.BLOCK,
-                # orientation=ttnn.ShardOrientation.COL_MAJOR,
             ),
             (32, 128),
             (64, 64),
