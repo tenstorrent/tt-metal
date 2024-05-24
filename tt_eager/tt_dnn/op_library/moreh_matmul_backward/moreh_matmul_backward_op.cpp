e94507b9b9f3e7ef55634e7e93a42ef2ee9cb0d5
Dongjin Na <dongjin.na@moreh.io>
2024-05-24 06:39:48 +0000

    #8788: Add compute_kernel_config param to moreh_matmul_backward

--- tt_eager/tt_dnn/op_library/moreh_matmul_backward/moreh_matmul_backward_op.cpp
+++ tt_eager/tt_dnn/op_library/moreh_matmul_backward/moreh_matmul_backward_op.cpp
@@ -41,7 +41,8 @@ std::vector<std::optional<Tensor>> moreh_matmul_backward_(
     const std::vector<bool>& are_required_outputs,
     const std::optional<const Tensor> &input_grad,
     const std::optional<const Tensor> &other_grad,
-    const MemoryConfig& output_mem_config) {
+    const MemoryConfig& output_mem_config,
+    std::optional<const DeviceComputeKernelConfig> compute_kernel_config) {
     std::vector<std::optional<Tensor>> outputs(2);
     outputs.reserve(2);
 
@@ -54,13 +55,13 @@ std::vector<std::optional<Tensor>> moreh_matmul_backward_(
         if (is_same_batch_dim(output_grad, input_grad_tensor)) {
             const auto& input_grad_shape = input_grad_tensor.get_legacy_shape().without_padding();
             const auto& output_grad_shape = output_grad.get_legacy_shape().without_padding();
-            moreh_matmul(output_grad, other, false, true, input_grad_tensor, std::nullopt, output_mem_config);
+            moreh_matmul(output_grad, other, false, true, input_grad_tensor, std::nullopt, output_mem_config, compute_kernel_config);
         } else {
             const auto& input_shape = input.get_legacy_shape().without_padding();
             const auto& temp_input_grad =
-                moreh_matmul(output_grad, other, false, true, std::nullopt, std::nullopt, output_mem_config);
+                moreh_matmul(output_grad, other, false, true, std::nullopt, std::nullopt, output_mem_config, compute_kernel_config);
             auto reduce_dims = find_reduce_dim(temp_input_grad.get_legacy_shape(), input_grad_tensor.get_legacy_shape());
-            moreh_sum(temp_input_grad, reduce_dims, true, input_grad_tensor);
+            moreh_sum(temp_input_grad, reduce_dims, true, input_grad_tensor, output_mem_config, compute_kernel_config);
         }
         outputs[0] = input_grad_tensor;
     }
@@ -69,12 +70,12 @@ std::vector<std::optional<Tensor>> moreh_matmul_backward_(
         TT_ASSERT(other_grad.has_value());
         const auto& other_grad_tensor = other_grad.value();
         if (is_same_batch_dim(output_grad, other_grad_tensor)) {
-            moreh_matmul(input, output_grad, true, false, other_grad_tensor, std::nullopt, output_mem_config);
+            moreh_matmul(input, output_grad, true, false, other_grad_tensor, std::nullopt, output_mem_config, compute_kernel_config);
         } else {
             const auto& temp_other_grad =
-                moreh_matmul(input, output_grad, true, false, std::nullopt, std::nullopt, output_mem_config);
+                moreh_matmul(input, output_grad, true, false, std::nullopt, std::nullopt, output_mem_config, compute_kernel_config);
             auto reduce_dims = find_reduce_dim(temp_other_grad.get_legacy_shape(), other_grad_tensor.get_legacy_shape());
-            moreh_sum(temp_other_grad, reduce_dims, true, other_grad_tensor);
+            moreh_sum(temp_other_grad, reduce_dims, true, other_grad_tensor, output_mem_config, compute_kernel_config);
         }
         outputs[1] = other_grad_tensor;
     }
@@ -89,13 +90,14 @@ std::vector<std::optional<Tensor>> moreh_matmul_backward(
     const std::vector<bool>& are_required_outputs,
     std::optional<const Tensor> input_grad,
     std::optional<const Tensor> other_grad,
-    const MemoryConfig& output_mem_config) {
+    const MemoryConfig& output_mem_config,
+    std::optional<const DeviceComputeKernelConfig> compute_kernel_config) {
     if (is_dot_backward(output_grad, input, other)) {
         // TODO(seunghwan100): Add the argument "are_required_outputs" to moreh_dot_backward.
         return moreh_dot_backward(output_grad, input, other, input_grad, other_grad, output_mem_config);
     } else {
         return moreh_matmul_backward_(
-            output_grad, input, other, are_required_outputs, input_grad, other_grad, output_mem_config);
+            output_grad, input, other, are_required_outputs, input_grad, other_grad, output_mem_config, compute_kernel_config);
     }
 }
 
