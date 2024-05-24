f91d9ec47f34dd552603a1968b198e01e852bc08
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2024-05-24 01:56:21 +0000

    #5389: removed ttnn::register_operation that was taking a function as template argument

--- tests/ttnn/unit_tests/gtests/test_add.cpp
+++ tests/ttnn/unit_tests/gtests/test_add.cpp
@@ -31,10 +31,10 @@ TEST_P(Add1DTensorAndScalarFixture, AddsScalarCorrectly) {
     ttnn::Shape shape(dimensions);
 
     {
-        const auto input_tensor = ttnn::zeros(shape, ttnn::bfloat16, ttnn::TILE_LAYOUT, device, std::nullopt);
+        const auto input_tensor = ttnn::zeros(shape, ttnn::bfloat16, ttnn::TILE_LAYOUT, device);
         const auto output_tensor = input_tensor + param.scalar;
-        const auto expected_tensor = ttnn::operations::creation::full(
-            shape, param.scalar, ttnn::bfloat16, ttnn::TILE_LAYOUT, device, std::nullopt);
+        const auto expected_tensor =
+            ttnn::operations::creation::full(shape, param.scalar, ttnn::bfloat16, ttnn::TILE_LAYOUT, device);
         TT_FATAL(tt::numpy::allclose<::bfloat16>(ttnn::from_device(expected_tensor), ttnn::from_device(output_tensor)));
     }
     ttnn::close_device(device);
