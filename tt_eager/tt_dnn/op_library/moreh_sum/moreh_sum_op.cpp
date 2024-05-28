9b380231d4eda81401de80c699a455cf065f2ab8
namhyeongKim <namhyeong.kim@moreh.io>
2024-05-28 04:29:00 +0000

    #8883: use operation::launch_op in moreh_sum and moreh_sum_backward

--- tt_eager/tt_dnn/op_library/moreh_sum/moreh_sum_op.cpp
+++ tt_eager/tt_dnn/op_library/moreh_sum/moreh_sum_op.cpp
@@ -4,12 +4,12 @@
 
 #include "tt_dnn/op_library/moreh_sum/moreh_sum_op.hpp"
 
+#include <numeric>
+
 #include "tt_dnn/op_library/reduce/reduce_op.hpp"
 #include "tt_eager/tt_dnn/op_library/moreh_helper_functions.hpp"
 #include "tt_metal/common/constants.hpp"
 #include "tt_metal/host_api.hpp"
-
-#include <numeric>
 
 namespace tt {
 using namespace constants;
@@ -20,43 +20,60 @@ namespace primary {
 //                         MorehSum
 ////////////////////////////////////////////////////////////////////////////
 namespace {
-    // TODO: move these check functions to a common header.
-    inline void check_tensor(
-        const Tensor& tensor,
-        const std::string& op_name,
-        DataType data_type = DataType::BFLOAT16,
-        Layout layout = Layout::TILE) {
-        TT_FATAL(tensor.get_layout() == layout, fmt::format("{} only supports tiled layout.", op_name));
-        TT_FATAL(tensor.get_dtype() == data_type, fmt::format("{} only supports data type {}.", op_name, data_type));
-        TT_FATAL(
-            tensor.storage_type() == StorageType::DEVICE, fmt::format("Operands to {} need to be on device!", op_name));
-        TT_FATAL(
-            tensor.buffer() != nullptr, fmt::format("Operands to {} need to be allocated in buffers on device!", op_name));
-    }
+// TODO: move these check functions to a common header.
+inline void check_tensor(
+    const Tensor& tensor,
+    const std::string& op_name,
+    DataType data_type = DataType::BFLOAT16,
+    Layout layout = Layout::TILE) {
+    TT_FATAL(tensor.get_layout() == layout, fmt::format("{} only supports tiled layout.", op_name));
+    TT_FATAL(tensor.get_dtype() == data_type, fmt::format("{} only supports data type {}.", op_name, data_type));
+    TT_FATAL(
+        tensor.storage_type() == StorageType::DEVICE, fmt::format("Operands to {} need to be on device!", op_name));
+    TT_FATAL(
+        tensor.buffer() != nullptr, fmt::format("Operands to {} need to be allocated in buffers on device!", op_name));
+}
 
-    inline void check_tensor(
-        std::optional<Tensor> tensor,
-        const std::string& op_name,
-        tt_metal::DataType data_type = DataType::BFLOAT16,
-        Layout layout = Layout::TILE) {
-        if (!tensor.has_value()) {
-            return;
-        }
-        check_tensor(tensor.value(), op_name, data_type, layout);
+inline void check_tensor(
+    std::optional<Tensor> tensor,
+    const std::string& op_name,
+    tt_metal::DataType data_type = DataType::BFLOAT16,
+    Layout layout = Layout::TILE) {
+    if (!tensor.has_value()) {
+        return;
     }
+    check_tensor(tensor.value(), op_name, data_type, layout);
+}
 
-    Tensor _moreh_sum(
-        const Tensor& input,
-        const int64_t& dim,
-        const std::optional<const Tensor>& output,
-        const MemoryConfig& output_mem_config) {
-        return operation::run(MorehSum{ .dim = dim, .output_mem_config = output_mem_config }, { input }, {}, { output }).at(0);
-    }
+Tensor _moreh_sum(
+    const Tensor& input,
+    const int64_t& dim,
+    const std::optional<const Tensor>& output,
+    const MemoryConfig& output_mem_config) {
+    std::vector<Tensor> output_tensors = {Tensor(operation::get_workers_for_op_output({input}))};
+
+    operation::launch_op(
+        [dim, output_mem_config](
+            const std::vector<Tensor>& input_tensors,
+            const std::vector<std::optional<const Tensor>>& optional_input_tensors,
+            const std::vector<std::optional<Tensor>>& optional_output_tensors) mutable -> std::vector<Tensor> {
+            return operation::run(
+                MorehSum{.dim = dim, .output_mem_config = output_mem_config},
+                input_tensors,
+                optional_input_tensors,
+                optional_output_tensors);
+        },
+        {input},
+        output_tensors,
+        {},
+        {output});
+
+    return output_tensors.at(0);
+}
 }  // namespace
 
 void MorehSum::validate_with_output_tensors(
     const std::vector<Tensor>& input_tensors, const std::vector<std::optional<Tensor>>& output_tensors) const {
-
     const auto& input = input_tensors.at(0);
     auto& output = output_tensors.at(0);
 
@@ -69,13 +86,16 @@ void MorehSum::validate_with_output_tensors(
     auto input_shape_wo_padding = input.get_legacy_shape().without_padding();
     const auto input_rank = input_shape.rank();
     log_debug(LogOp, "{}:{} input_rank {}", __func__, __LINE__, input_rank);
-    TT_FATAL((this->dim >= 0 && this->dim <= tt::tt_metal::MAX_NUM_DIMENSIONS), "dim must be between 0 and {}.", tt::tt_metal::MAX_NUM_DIMENSIONS);
+    TT_FATAL(
+        (this->dim >= 0 && this->dim <= tt::tt_metal::MAX_NUM_DIMENSIONS),
+        "dim must be between 0 and {}.",
+        tt::tt_metal::MAX_NUM_DIMENSIONS);
     TT_FATAL((this->dim < input_rank), "dim must be smaller than input tensor rank {}.", input_rank);
 
-    // validate shape
-    // keepdim=true
-    // TODO: fix when input rank and output rank are different.
-    #if 0
+// validate shape
+// keepdim=true
+// TODO: fix when input rank and output rank are different.
+#if 0
     if (output.has_value()) {
         const auto& output_shape = output.value().get_legacy_shape();
         const auto& output_shape_wo_padding = output.value().get_legacy_shape().without_padding();
@@ -98,7 +118,7 @@ void MorehSum::validate_with_output_tensors(
             TT_FATAL(input_shape_wo_padding[i] == output_shape_wo_padding[i]);
         }
     }
-    #endif
+#endif
 }
 
 std::vector<Shape> MorehSum::compute_output_shapes(const std::vector<Tensor>& input_tensors) const {
@@ -113,7 +133,7 @@ std::vector<Shape> MorehSum::compute_output_shapes(const std::vector<Tensor>& in
     // last 2-dim
     if (this->dim == input_rank - 1 || this->dim == input_rank - 2) {
         output_shape[this->dim] = TILE_HEIGHT;
-        padding[this->dim] = Padding::PadDimension{ 0, 31 };
+        padding[this->dim] = Padding::PadDimension{0, 31};
     } else {
         output_shape[this->dim] = 1;
     }
@@ -144,7 +164,7 @@ operation::ProgramWithCallbacks MorehSum::create_program(
     const auto input_rank = input.get_legacy_shape().rank();
     if (this->dim == input_rank - 1) {
         return moreh_sum_w_impl(input, output);
-    } else if(this->dim == input_rank - 2) {
+    } else if (this->dim == input_rank - 2) {
         return moreh_sum_h_impl(input, output);
     } else {
         return moreh_sum_nc_impl(input, output, dim);
