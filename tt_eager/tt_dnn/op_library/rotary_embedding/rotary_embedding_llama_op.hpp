40a30eef8282d598bb3fe3329dbbfd015e6c7848
avoraTT <avora@tenstorrent.com>
2024-05-28 13:07:18 -0500

    #8577: replacing operation::run_with_autoformat with operation::run.

--- tt_eager/tt_dnn/op_library/rotary_embedding/rotary_embedding_llama_op.hpp
+++ tt_eager/tt_dnn/op_library/rotary_embedding/rotary_embedding_llama_op.hpp
@@ -41,33 +41,16 @@ inline Tensor rotary_embedding_llama(
     const MemoryConfig &output_mem_config = operation::DEFAULT_OUTPUT_MEMORY_CONFIG,
     std::optional<const DeviceComputeKernelConfig> compute_kernel_config = std::nullopt) {
     std::vector<Tensor> output_tensors = {Tensor(operation::get_workers_for_op_output({input_tensor, cos, sin, trans_mat}))};
-    operation::launch_with_autoformat(
+    operation::launch_op(
         [output_mem_config, compute_kernel_config] (const std::vector<Tensor>& input_tensors, const std::vector<std::optional<const Tensor>>& optional_input_tensors, const std::vector<std::optional<Tensor>>& optional_output_tensors) mutable -> std::vector<Tensor> {
             auto& input_tensor = input_tensors.at(0);
-            auto& cos = input_tensors.at(1);
-            auto& sin = input_tensors.at(2);
-            auto& trans_mat = input_tensors.at(3);
             uint32_t seq_len = input_tensor.get_legacy_shape()[-2];
-            uint32_t head_dim = input_tensor.get_legacy_shape()[-1];
-
-            TT_FATAL(head_dim <= 128 || std::get<WormholeComputeKernelConfig>(compute_kernel_config.value()).fp32_dest_acc_en == false, "If head_dim is > 128, fp32_dest_acc_en must be False");
 
             auto arch = input_tensor.storage_type() == StorageType::DEVICE ? input_tensor.device()->arch() : AutoFormat::GetDefaultDevice()->arch();
             auto kernel_config_val = init_device_compute_kernel_config(arch, compute_kernel_config, MathFidelity::HiFi4, true, false, false);
 
-            Shape input_pad_shape = AutoFormat::pad_to_tile_shape(input_tensor.get_legacy_shape());
-            FormatParams input_format_params = {.pad_shape = input_pad_shape, .pad_value = 0.0, .target_layout = Layout::TILE};
-            Shape cos_pad_shape = AutoFormat::pad_to_tile_shape(cos.get_legacy_shape());
-            FormatParams cos_format_params = {.pad_shape = cos_pad_shape, .pad_value = 0.0, .target_layout = Layout::TILE};
-            Shape sin_pad_shape = AutoFormat::pad_to_tile_shape(sin.get_legacy_shape());
-            FormatParams sin_format_params = {.pad_shape = sin_pad_shape, .pad_value = 0.0, .target_layout = Layout::TILE};
-            Shape trans_mat_pad_shape = AutoFormat::pad_to_tile_shape(trans_mat.get_legacy_shape());
-            FormatParams trans_mat_format_params = {.pad_shape = trans_mat_pad_shape, .pad_value = 0.0, .target_layout = Layout::TILE};
-            return operation::run_with_autoformat(
-                    RotaryEmbeddingLlama{seq_len, output_mem_config, kernel_config_val},
-                    {input_tensor, cos, sin, trans_mat},
-                    {input_format_params, cos_format_params, sin_format_params, trans_mat_format_params},
-                    {Layout::TILE});
+            return operation::run(
+                    RotaryEmbeddingLlama{seq_len, output_mem_config, kernel_config_val}, input_tensors);
         }, {input_tensor, cos, sin, trans_mat}, output_tensors);
     return output_tensors.at(0);
 }
