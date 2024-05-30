ac31ba3f893ad8d8de5ea490678cd439565ac81c
Dalar Vartanians <dvartanians@tenstorrent.com>
2024-05-30 21:55:05 +0000

    #0: add dump commands

--- models/experimental/functional_yolov4/tt/ttnn_yolov4.py
+++ models/experimental/functional_yolov4/tt/ttnn_yolov4.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
 
 # SPDX-License-Identifier: Apache-2.0
-
+import tt_lib as ttl
 from models.experimental.functional_yolov4.tt.ttnn_downsample1 import TtDownSample1
 from models.experimental.functional_yolov4.tt.ttnn_downsample2 import TtDownSample2
 from models.experimental.functional_yolov4.tt.ttnn_downsample3 import TtDownSample3
@@ -24,9 +24,12 @@ class TtYolov4:
     def __call__(self, device, input_tensor):
         d1 = self.downsample1(device, input_tensor)
         d2 = self.downsample2(device, d1)
+        ttl.device.DumpDeviceProfiler(device)
         d3 = self.downsample3(device, d2)
         d4 = self.downsample4(device, d3)
+        ttl.device.DumpDeviceProfiler(device)
         d5 = self.downsample5(device, d4)
         x20, x13, x6 = self.neck(device, [d5, d4, d3])
+        ttl.device.DumpDeviceProfiler(device)
         x4, x5, x6 = self.head(device, [x20, x13, x6])
         return x4, x5, x6
