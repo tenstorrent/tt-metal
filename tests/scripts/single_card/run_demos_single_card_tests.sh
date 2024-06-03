26fec891666a32813e208767359f7c5893958ac3
Raymond Kim <109366641+tt-rkim@users.noreply.github.com>
2024-06-03 12:04:11 -0400

    #8764: Add single card WH demo tests (#9058)
    #8764: Add single-card demo tests and move nightly scripts into new single_card folder

---
+++ tests/scripts/single_card/run_demos_single_card_tests.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+set -eo pipefail
+
+if [[ -z "$TT_METAL_HOME" ]]; then
+  echo "Must provide TT_METAL_HOME in environment" 1>&2
+  exit 1
+fi
+
+export WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml
+
+# working on both
+pytest --disable-warnings -q -s --input-method=cli --cli-input="YOUR PROMPT GOES HERE!"  models/demos/wormhole/falcon7b/demo_wormhole.py::test_demo[user_input0-default_mode_stochastic]
+
+# working on both
+pytest --disable-warnings -q -s --input-method=cli --cli-input="YOUR PROMPT GOES HERE!"  models/demos/wormhole/mistral7b/demo/demo.py
+
+# working on both
+pytest --disable-warnings -q -s --input-method=cli --cli-input="YOUR PROMPT GOES HERE!"  models/demos/mamba/demo/demo.py
+
+# working on both
+pytest --disable-warnings --input-path="models/demos/wormhole/stable_diffusion/demo/input_data.json" models/demos/wormhole/stable_diffusion/demo/demo.py::test_demo
+
+# Not working on N150, working on N300
+unset WH_ARCH_YAML
+rm -rf built
+pytest --disable-warnings models/demos/metal_BERT_large_11/demo/demo.py::test_demo[models/demos/metal_BERT_large_11/demo/input_data.json-1-batch_7]
