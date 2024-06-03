fd9d90c911d22d3570a5f17a09593917c2e358ad
Austin Ho <aho@tenstorrent.com>
2024-06-03 17:14:03 +0000

    #0: Split 2cq tests into separate files to follow convention

--- tests/scripts/single_card/nightly/run_gs_only.sh
+++ tests/scripts/single_card/nightly/run_gs_only.sh
@@ -11,8 +11,6 @@ echo "Running model nightly tests for GS only"
 
 env pytest models/demos/metal_BERT_large_11/tests/test_demo.py
 
-env pytest models/demos/resnet/tests/test_metal_resnet50.py::test_run_resnet50_inference[LoFi-activations_BFLOAT8_B-weights_BFLOAT8_B-batch_20-device_params0]
+env pytest models/demos/resnet/tests/test_metal_resnet50_performant.py
 
-env pytest models/demos/resnet/tests/test_metal_resnet50.py::test_run_resnet50_2cqs_inference[LoFi-activations_BFLOAT8_B-weights_BFLOAT8_B-batch_20-device_params0]
-
-env pytest models/demos/resnet/tests/test_metal_resnet50.py::test_run_resnet50_trace_inference -k "LoFi-activations_BFLOAT8_B-weights_BFLOAT8_B-batch_20-device_params0"
+env pytest models/demos/resnet/tests/test_metal_resnet50_2cqs_performant.py
