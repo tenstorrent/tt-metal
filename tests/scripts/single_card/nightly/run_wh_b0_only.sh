e970b8b7984b88d35e2a7263d54b37d37da974f3
Raymond Kim <rkim@tenstorrent.com>
2024-06-04 14:43:01 +0000

    #7586: Move current wh b0 only single-card nightly tests to the ln model we were talking about so we can invoke all files in the same command

--- tests/scripts/single_card/nightly/run_wh_b0_only.sh
+++ tests/scripts/single_card/nightly/run_wh_b0_only.sh
@@ -8,22 +8,5 @@ if [[ -z "$TT_METAL_HOME" ]]; then
 fi
 
 echo "Running nightly tests for WH B0 only"
-
-env pytest tests/ttnn/integration_tests/unet                # -> failing: issue #7556
-
-env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest models/demos/falcon7b/tests/ci/test_falcon_end_to_end_prefill.py
-
-env pytest models/demos/mamba/tests/test_benchmarks.py
-env pytest models/demos/mamba/tests/test_reference_model.py
-env pytest models/demos/mamba/tests/test_mamba_ssm.py
-env pytest models/demos/mamba/tests/test_mamba_block.py
-env pytest models/demos/mamba/tests/test_residual_block.py
-env pytest models/demos/mamba/tests/test_full_model.py
-env pytest models/demos/mamba/tests/test_mamba_demo.py
-
-env pytest models/demos/wormhole/mistral7b/tests/test_mistral_embedding.py
-env pytest models/demos/wormhole/mistral7b/tests/test_mistral_rms_norm.py
-env pytest models/demos/wormhole/mistral7b/tests/test_mistral_mlp.py
-env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest models/demos/wormhole/mistral7b/tests/test_mistral_attention.py
-env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest models/demos/wormhole/mistral7b/tests/test_mistral_decoder.py
-#env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest models/experimental/functional_unet/tests
+env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest tests/nightly/wh_b0_only_eth
+env pytest tests/nightly/wh_b0_only
