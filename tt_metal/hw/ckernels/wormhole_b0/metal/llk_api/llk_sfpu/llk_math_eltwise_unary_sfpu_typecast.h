eedfc6be81973e3777c9e13bba6a6efac9286f27
Radomir Djogo <rdjogo@tenstorrent.com>
2024-06-05 00:54:15 +0000

    #4858: use static_cast and update llk_math_eltwise_unary_sfpu_params

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_typecast.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_typecast.h
@@ -5,7 +5,7 @@
 #pragma once
 
 #include "llk_math_eltwise_unary_sfpu_init.h"
-#include "llk_math_eltwise_unary_sfpu_0_param.h"
+#include "llk_math_eltwise_unary_sfpu_params.h"
 #include "ckernel_sfpu_typecast.h"
 
 namespace ckernel {
@@ -15,16 +15,16 @@ namespace ckernel {
 template <bool APPROXIMATE, uint32_t OUT_DTYPE>
 inline void llk_math_eltwise_unary_sfpu_typecast(uint dst_index, int vector_mode = (int)VectorMode::RC) {
     if constexpr (OUT_DTYPE == (uint32_t)DataFormat::UInt16) {
-        llk_math_eltwise_unary_sfpu_0_param<APPROXIMATE>
-            (ckernel::sfpu::calculate_typecast_fp16b_to_uint16<APPROXIMATE,8>,
+        llk_math_eltwise_unary_sfpu_params<APPROXIMATE>(
             ckernel::sfpu::calculate_typecast_fp16b_to_uint16<APPROXIMATE,8>,
-                                        dst_index, vector_mode);
+            dst_index,
+            vector_mode);
     }
     else if constexpr (OUT_DTYPE == (uint32_t)DataFormat::UInt32) {
-        llk_math_eltwise_unary_sfpu_0_param<APPROXIMATE>
-            (ckernel::sfpu::calculate_typecast_fp16b_to_uint32<APPROXIMATE,8>,
+        llk_math_eltwise_unary_sfpu_params<APPROXIMATE>(
             ckernel::sfpu::calculate_typecast_fp16b_to_uint32<APPROXIMATE,8>,
-                                        dst_index, vector_mode);
+            dst_index,
+            vector_mode);
     }
 }
 
