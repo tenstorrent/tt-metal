141ef88e07d26d9eef96e89efbcaaa7b5498ae48
Evan Smal <esmal@tenstorrent.com>
2024-06-05 20:06:24 +0000

    #0: Support math fidelity configuration in `ssm_eltwise_mul`

--- tt_eager/tt_lib/csrc/operations/primary/transformers/module.hpp
+++ tt_eager/tt_lib/csrc/operations/primary/transformers/module.hpp
@@ -49,7 +49,7 @@ void py_module(py::module& m_transformers) {
             torch.matmul(A.transpose(0, 2), B).transpose(0, 2). Similar concept for post-softmax matmul.
     )doc");
     m_transformers.def("ssm_eltwise_mul", &ssm_eltwise_mul,
-        py::arg().noconvert(), py::arg().noconvert(), py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, R"doc(
+        py::arg().noconvert(), py::arg().noconvert(), py::arg("output_mem_config").noconvert() = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, py::arg("output_dtype").noconvert() = std::nullopt, py::arg("math_fidelity").noconvert() = MathFidelity::HiFi4, R"doc(
         Performs a special eltwise multiply for SSM models. Given tensor A with shape [1, 1, 32, 32] and tensor B with shape [1, 1, 32, W] where W is some multiple of 32, perform the following PyTorch equivalent:
             A.repeat(1, 1, 1, W) * B.repeat_interleave(32, dim=-1)
     )doc");
