8257693da717f63308c654399dbdc81432fd0853
Dongjin Na <dongjin.na@moreh.io>
2024-06-05 13:03:01 +0000

    #7543: Rename keepdim to keep_batch_dim

--- tt_eager/tt_dnn/op_library/moreh_sum_backward/moreh_sum_backward_op.hpp
+++ tt_eager/tt_dnn/op_library/moreh_sum_backward/moreh_sum_backward_op.hpp
@@ -21,7 +21,7 @@ using namespace tt_metal;
 
 struct MorehSumBackward {
     std::vector<int64_t> dims;
-    bool keepdim;
+    bool keep_batch_dim;
     MemoryConfig input_grad_mem_config;
     const DeviceComputeKernelConfig compute_kernel_config;
     void validate_with_output_tensors(
@@ -31,19 +31,19 @@ struct MorehSumBackward {
         const std::vector<Tensor> &input_tensors, const std::vector<std::optional<Tensor>> &output_tensors) const;
     operation::ProgramWithCallbacks create_program(
         const std::vector<Tensor> &input_tensors, std::vector<Tensor> &output_tensors) const;
-    static constexpr auto attribute_names = std::make_tuple("dims", "keepdim", "input_grad_mem_config", "compute_kernel_config");
+    static constexpr auto attribute_names = std::make_tuple("dims", "keep_batch_dim", "input_grad_mem_config", "compute_kernel_config");
     const auto attribute_values() const {
-        return std::make_tuple(std::cref(this->dims), std::cref(this->keepdim), std::cref(this->input_grad_mem_config), std::cref(this->compute_kernel_config));
+        return std::make_tuple(std::cref(this->dims), std::cref(this->keep_batch_dim), std::cref(this->input_grad_mem_config), std::cref(this->compute_kernel_config));
     }
 };
 
-operation::ProgramWithCallbacks moreh_sum_backward_impl(const Tensor &output_grad, const Tensor &input_grad, const std::vector<int64_t> &dims, const bool &keepdim, const DeviceComputeKernelConfig &compute_kernel_config);
+operation::ProgramWithCallbacks moreh_sum_backward_impl(const Tensor &output_grad, const Tensor &input_grad, const std::vector<int64_t> &dims, const bool &keep_batch_dim, const DeviceComputeKernelConfig &compute_kernel_config);
 
 Tensor moreh_sum_backward(
     const Tensor &output_grad,
     const Tensor &input,
     std::optional<std::variant<int64_t, std::vector<int64_t>>> dim = std::nullopt,
-    const bool keepdim = false,
+    const bool keep_batch_dim = false,
     const std::optional<const Tensor> input_grad = std::nullopt,
     const MemoryConfig &input_grad_mem_config = operation::DEFAULT_OUTPUT_MEMORY_CONFIG,
     std::optional<const DeviceComputeKernelConfig> compute_kernel_config = std::nullopt);
