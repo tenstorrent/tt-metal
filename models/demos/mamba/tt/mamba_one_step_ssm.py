8759c2468e26c0790bbf41d2ae4aaa6f41c27543
Evan Smal <esmal@tenstorrent.com>
2024-06-05 20:06:24 +0000

    #0: Support math fidelity configuration in `ssm_eltwise_mul`

--- models/demos/mamba/tt/mamba_one_step_ssm.py
+++ models/demos/mamba/tt/mamba_one_step_ssm.py
@@ -98,6 +98,7 @@ class TtMambaSSM(torch.nn.Module):
             math_approx_mode=False,
             fp32_dest_acc_en=True,
         )
+        self.eltwise_math_fidelity = ttl.tensor.MathFidelity.HiFi2
         self.core_grid_row = 5
         self.core_grid_col = 8
 
@@ -144,6 +145,7 @@ class TtMambaSSM(torch.nn.Module):
                 ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1
             ),
             output_dtype=self.configs["dtype"]["activations"],
+            math_fidelity=self.eltwise_math_fidelity,
         )
         ttnn.deallocate(abar0)
 
@@ -183,6 +185,7 @@ class TtMambaSSM(torch.nn.Module):
                 ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1
             ),
             output_dtype=self.configs["dtype"]["activations"],
+            math_fidelity=self.eltwise_math_fidelity,
         )
         ttnn.deallocate(delta_t2)
         ttnn.deallocate(B0)
@@ -195,6 +198,7 @@ class TtMambaSSM(torch.nn.Module):
                 ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1
             ),
             output_dtype=self.configs["dtype"]["activations"],
+            math_fidelity=self.eltwise_math_fidelity,
         )
 
         # deallocate bbar
@@ -228,6 +232,7 @@ class TtMambaSSM(torch.nn.Module):
                 ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1
             ),
             output_dtype=self.configs["dtype"]["activations"],
+            math_fidelity=self.eltwise_math_fidelity,
         )
         ttnn.deallocate(hidden_state1)
         ttnn.deallocate(C0)
