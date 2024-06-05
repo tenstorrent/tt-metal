80d431c1127efdb719c6819191435c79f3163ce3
Artem Yerofieiev <artem@tenstorrent.com>
2024-06-05 05:31:00 +0000

    #0: Update test to include tt_lib ttnn and preallocated ttnn

--- tests/ttnn/unit_tests/operations/test_typecast.py
+++ tests/ttnn/unit_tests/operations/test_typecast.py
@@ -7,6 +7,7 @@ import pytest
 import torch
 
 import ttnn
+import tt_lib
 from models.utility_functions import is_grayskull
 
 from tests.ttnn.utils_for_testing import assert_with_pcc
@@ -22,22 +23,26 @@ def test_typecast_output_tensor(device):
 
     torch.manual_seed(0)
 
-    h = w = 64
-    gold_tensor_ones = ttnn.ones([h, w], ttnn.DataType.UINT32, ttnn.TILE_LAYOUT, device, ttnn.L1_MEMORY_CONFIG)
-    tensor_ones_bfloat16 = ttnn.ones([h, w], ttnn.DataType.BFLOAT16, ttnn.TILE_LAYOUT, device, ttnn.L1_MEMORY_CONFIG)
-    logger.warning(tensor_ones_bfloat16)
-    output_tensor_preallocated_uint32 = ttnn.zeros(
-        [h, w], ttnn.DataType.UINT32, ttnn.TILE_LAYOUT, device, ttnn.L1_MEMORY_CONFIG
-    )
+    h = w = 32
+    gold_tensor = ttnn.ones([h, w], ttnn.DataType.UINT32, ttnn.TILE_LAYOUT, device, ttnn.L1_MEMORY_CONFIG)
+    bfloat16_tensor = ttnn.ones([h, w], ttnn.DataType.BFLOAT16, ttnn.TILE_LAYOUT, device, ttnn.L1_MEMORY_CONFIG)
+    uint32_preallocated = ttnn.empty([h, w], ttnn.DataType.UINT32, ttnn.TILE_LAYOUT, device, ttnn.L1_MEMORY_CONFIG)
+
+    output_ttlib = tt_lib.tensor.typecast(bfloat16_tensor, ttnn.DataType.UINT32, ttnn.L1_MEMORY_CONFIG)
+    logger.warning(output_ttlib)
+
+    output_ttnn = ttnn.typecast(bfloat16_tensor, ttnn.DataType.UINT32, memory_config=ttnn.L1_MEMORY_CONFIG)
+    logger.warning(output_ttnn)
 
     pages_before = ttnn._ttnn.reports.get_buffer_pages()
-    output = ttnn.typecast(
-        tensor_ones_bfloat16, ttnn.DataType.UINT16, memory_config=ttnn.L1_MEMORY_CONFIG
-    )  # , output_tensor=output_tensor_preallocated_uint32)
-    # assert len(pages_before) == len(ttnn._ttnn.reports.get_buffer_pages())
-    logger.warning(output)
-    logger.warning(output_tensor_preallocated_uint32)
-    torch_gold_ones = ttnn.to_torch(gold_tensor_ones)
-    torch_after_typecast = ttnn.to_torch(output_tensor_preallocated_uint32)
-    logger.warning(torch_after_typecast)
-    assert_with_pcc(torch_gold_ones, torch_after_typecast, 0.999)
+    ttnn.typecast(
+        bfloat16_tensor, ttnn.DataType.UINT32, memory_config=ttnn.L1_MEMORY_CONFIG, output_tensor=uint32_preallocated
+    )
+    assert len(pages_before) == len(ttnn._ttnn.reports.get_buffer_pages())
+    logger.warning(uint32_preallocated)
+
+    torch_gold = ttnn.to_torch(gold_tensor)
+    torch_output_ttlib = ttnn.to_torch(output_ttlib)
+    torch_output_ttnn = ttnn.to_torch(output_ttnn)
+    assert_with_pcc(torch_gold, torch_output_ttlib, 0.999)
+    assert_with_pcc(torch_gold, torch_output_ttnn, 0.999)
