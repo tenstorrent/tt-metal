8e18acb27233743332ea466bce301b15e85138f1
Joseph Chu <jchu@tenstorrent.com>
2024-06-05 16:40:15 +0000

    #9088: fix ttnn_falcon_7b single-device regression in decoder module

--- models/demos/ttnn_falcon7b/tt/falcon_decoder.py
+++ models/demos/ttnn_falcon7b/tt/falcon_decoder.py
@@ -31,7 +31,7 @@ class TtFalconDecoderLayer:
             max_position_embeddings=config.max_position_embeddings,
             model_config=model_config,
             parameters=parameters.self_attention,
-            core_grid=device.get_devices()[0].core_grid,
+            core_grid=device.core_grid if isinstance(device, ttnn.Device) else device.get_devices()[0].core_grid,
         )
 
         self.mlp = TtFalconMLP(model_config, parameters=parameters.mlp)
