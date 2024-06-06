543dca44e02a283e320d04d974d57125ec7ec18b
avoraTT <avora@tenstorrent.com>
2024-06-06 18:07:20 +0000

    #9114: modifying llama2 experimental tests to work with multi device.

--- models/experimental/llama2_70b/tests/test_llama_decoder.py
+++ models/experimental/llama2_70b/tests/test_llama_decoder.py
@@ -141,7 +141,7 @@ def run_test_LlamaDecoder_inference(
         hugging_face_reference_model, UNIT_TEST_LAYER_NUM, configuration.rope_theta
     )
     # TT model -------------------------------------------------------------------------
-    transformation_mat_torch = get_rot_transformation_mat(head_dim)
+    transformation_mat_torch = get_rot_transformation_mat(32)  # 32 for tile size
     transformation_mats = ttnn.as_tensor(
         transformation_mat_torch,
         dtype=ttnn.bfloat16,
