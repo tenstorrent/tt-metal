96f7de2d810414a477e87b40a32273a89efb6137
Reem Tawfik <rtawfik@tenstorrent.com>
2024-06-06 16:24:21 -0400

    #9142: Remove unused addrmod func, update llk sfpu params

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_params.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_params.h
@@ -13,44 +13,35 @@ inline void llk_math_eltwise_unary_sfpu_params(
     int vector_mode = (int)VectorMode::RC,
     ARGS&& ... args) {
 
-    math::set_dst_write_addr<DstTileLayout::Default, DstTileShape::Tile32x32>(dst_index);
-    math::set_addr_mod_base();
+    _llk_math_eltwise_unary_sfpu_start_<DstSync::SyncHalf>(dst_index);
 
-    TTI_STALLWAIT(p_stall::STALL_SFPU, p_stall::MATH);
     if (vector_mode == (int)VectorMode::R) {
         // Do a row vector, Face0 + Face1 -- first iteration (first row)
-        const int ITERATIONS = 1;
 #pragma GCC unroll 0
         for (int face = 0; face < 2; face++) {
-            sfpu_func(static_cast<ARGS&&>(args)...);
-            TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
-            TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
+           sfpu_func(static_cast<ARGS&&>(args)...);
+            // Move to the next face
+            _llk_math_eltwise_unary_sfpu_inc_dst_face_addr_();
         }
-        // Skip the next 2 faces
-        TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
-        TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
-        TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
-        TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
+        // Skip next two faces
+        _llk_math_eltwise_unary_sfpu_inc_dst_face_addr_();
+        _llk_math_eltwise_unary_sfpu_inc_dst_face_addr_();
     } else if (vector_mode == (int)VectorMode::C) {
         // Do a column vector, Face0 + Face2 -- All iterations for full face
 #pragma GCC unroll 0
         for (int face = 0; face < 2; face++) {
             sfpu_func(static_cast<ARGS&&>(args)...);
-            TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
-            TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
-            TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
-            TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
+            _llk_math_eltwise_unary_sfpu_inc_dst_face_addr_();
+            _llk_math_eltwise_unary_sfpu_inc_dst_face_addr_();
         }
-    } else if (vector_mode == (int)VectorMode::RC) {
+    } else {
         // Do all four faces, and iterate through all 4 blocks of 4 rows each
 #pragma GCC unroll 0
         for (int face = 0; face < 4; face++) {
             sfpu_func(static_cast<ARGS&&>(args)...);
-            TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
-            TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
+            // Move to the next face
+            _llk_math_eltwise_unary_sfpu_inc_dst_face_addr_();
         }
-    } else {
-        sfpu_func(static_cast<ARGS&&>(args)...);
     }
-    math::clear_dst_reg_addr();
+    _llk_math_eltwise_unary_sfpu_done_();
 }
