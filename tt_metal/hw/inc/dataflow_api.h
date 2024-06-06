27cfa3feb3daffa1fe22b2208975dd2abb263f35
Austin Ho <aho@tenstorrent.com>
2024-06-06 16:53:30 +0000

    #0: Update noc_async_read/write docs to not specify only dram coords

--- tt_metal/hw/inc/dataflow_api.h
+++ tt_metal/hw/inc/dataflow_api.h
@@ -503,7 +503,7 @@ std::uint64_t get_noc_addr(std::uint32_t addr) {
  *
  * | Argument          | Description                                        | Data type | Valid range | required |
  * |-------------------|----------------------------------------------------|-----------|------------------------------------------|----------|
- * | src_noc_addr      | Encoding of the source DRAM location (x,y)+address | uint64_t  | DOX-TODO(ref to explain valid coords)    | Yes      |
+ * | src_noc_addr      | Encoding of the source NOC location (x,y)+address  | uint64_t  | DOX-TODO(ref to explain valid coords)    | Yes      |
  * | dst_local_l1_addr | Address in local L1 memory                         | uint32_t  | 0..1MB                                   | Yes      |
  * | size              | Size of data transfer in bytes                     | uint32_t  | 0..1MB                                   | Yes      |
  */
@@ -1215,11 +1215,11 @@ FORCE_INLINE void noc_async_read_tile(
  *
  * Return value: None
  *
- * | Argument          | Description                                             | Type     | Valid Range | Required |
- * |-------------------|---------------------------------------------------------|----------|-----------------------------------------------------------|----------|
- * | src_local_l1_addr | Source address in local L1 memory                       | uint32_t | 0..1MB | True     |
- * | dst_noc_addr      | Encoding of the destination DRAM location (x,y)+address | uint64_t | DOX-TODO(insert a reference  to what constitutes valid coords) | True     |
- * | size              | Size of data transfer in bytes | uint32_t | 0..1MB                                                    | True     |
+ * | Argument          | Description                                             | Type     | Valid Range                                                    | Required |
+ * |-------------------|---------------------------------------------------------|----------|----------------------------------------------------------------|----------|
+ * | src_local_l1_addr | Source address in local L1 memory                       | uint32_t | 0..1MB                                                         | True     |
+ * | dst_noc_addr      | Encoding of the destination NOC location (x,y)+address  | uint64_t | DOX-TODO(insert a reference  to what constitutes valid coords) | True     |
+ * | size              | Size of data transfer in bytes                          | uint32_t | 0..1MB                                                         | True     |
  */
 template<uint32_t max_page_size=NOC_MAX_BURST_SIZE + 1>
 inline
