5f65a1997cb9d0486637047355e3742122a6ffb7
Denis Kartashevskiy <denis.kartashevskiy@gmail.com>
2024-06-07 11:04:24 -0400

    add FastAPI and sample requests

--- ttnn-visualizer/src/App.tsx
+++ ttnn-visualizer/src/App.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { FocusStyleManager } from "@blueprintjs/core";
 import ApplicationList from "./components/ApplicationList.tsx";
+import axios from "axios";
 
 function App() {
     FocusStyleManager.onlyShowFocusOnTabs();
+
+    const [message, setMessage] = useState('');
+    const [operations, setOperations] = useState('');
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api')
+            .then(response => {
+                setMessage(response.data.message);
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+
+    }, []);
+    useEffect(() => {
+        const fetchOperations = async () => {
+            console.log('fetching operations')
+            try {
+                const response = await axios.get('/api/get-operations');
+                console.log(response)
+                setOperations(response.data);
+            } catch (error) {
+                console.error('Error fetching operations:', error);
+            }
+        };
+
+        fetchOperations();
+    }, []);
+
     return (
         <>
+            {message && <h1>{message}</h1>}
             <ApplicationList/>
         </>
     )
--- ttnn-visualizer/vite.config.ts
+++ ttnn-visualizer/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': 'http://localhost:8000',
+        },
+    },
 })
