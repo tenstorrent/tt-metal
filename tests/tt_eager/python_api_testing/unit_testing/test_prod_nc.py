26979010d44421f6cb8a06298bb6a0fe87a25c5e
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2024-06-07 12:55:28 +0000

    #9150: Add optional output tensor support for prod along N,C dimension

--- tests/tt_eager/python_api_testing/unit_testing/test_prod_nc.py
+++ tests/tt_eager/python_api_testing/unit_testing/test_prod_nc.py
@@ -5,9 +5,12 @@
 import pytest
 import torch
 from loguru import logger
-
+from tests.tt_eager.python_api_testing.unit_testing.backward_ops.utility_funcs import data_gen_pt_tt
 import tt_lib as ttl
 from models.utility_functions import comp_allclose_and_pcc
+from tests.tt_eager.python_api_testing.sweep_tests import (
+    comparison_funcs,
+)
 
 TILE_HEIGHT = 32
 TILE_WIDTH = 32
@@ -88,3 +91,40 @@ def test_prod_dims(input_shape, dims, device):
     logger.info(f"Output pcc={output_pcc}")
 
     assert passing
+
+
+mem_configs = [
+    ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+    ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1),
+]
+
+
+@pytest.mark.parametrize(
+    "dst_mem_config",
+    mem_configs,
+)
+@pytest.mark.parametrize(
+    "input_shapes",
+    (
+        ([1, 1, 32, 32]),
+        ([2, 2, 32, 32]),
+        ([4, 3, 32, 32]),
+    ),
+)
+@pytest.mark.parametrize(
+    "dim",
+    [0, 1],
+)
+@pytest.mark.parametrize("all_dimensions", [False])
+def test_prod_with_output_nc(input_shapes, all_dimensions, dim, dst_mem_config, device):
+    in_data, input_tensor = data_gen_pt_tt(input_shapes, device)
+    golden_tensor = torch.prod(in_data, dim, keepdim=True)
+
+    output_shape = input_shapes
+    output_shape[dim] = 1
+    out_data, output_tensor = data_gen_pt_tt(output_shape, device)
+
+    tt_output_tensor_on_device = ttl.tensor.prod(input_tensor, all_dimensions, dim, dst_mem_config, output_tensor)
+    tt_out_tensor = tt_output_tensor_on_device.cpu().to(ttl.tensor.Layout.ROW_MAJOR).to_torch()
+    comp_pass, comp_out = comparison_funcs.comp_pcc(golden_tensor, tt_out_tensor)
+    assert comp_pass
