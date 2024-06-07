916df3e0ecf468462f008f4c1209c2b1e5663dcb
Brian Liu <bliu@tenstorrent.com>
2024-06-07 19:56:02 +0000

    #9189: Add optional output tensor + queue id - Add check for supported argmax dim

--- ttnn/cpp/pybind11/operations/reduction.hpp
+++ ttnn/cpp/pybind11/operations/reduction.hpp
@@ -17,6 +17,7 @@ namespace operations {
 namespace reduction {
 
 namespace detail {
+
 template <typename reduction_operation_t>
 void bind_reduction_operation(py::module& module, const reduction_operation_t& operation) {
     auto doc = fmt::format(
@@ -33,21 +34,11 @@ void bind_reduction_operation(py::module& module, const reduction_operation_t& o
             py::arg("keepdim") = true,
             py::arg("memory_config") = std::nullopt});
 }
-}  // namespace detail
 
-void py_module(py::module& module) {
-    // Generic reductions
-    detail::bind_reduction_operation(module, ttnn::sum);
-    detail::bind_reduction_operation(module, ttnn::mean);
-    detail::bind_reduction_operation(module, ttnn::max);
-    detail::bind_reduction_operation(module, ttnn::min);
-    detail::bind_reduction_operation(module, ttnn::std);
-    detail::bind_reduction_operation(module, ttnn::var);
-
-    // Special reductions
-    ttnn::bind_registered_operation(
-        module, ttnn::argmax,
-        R"doc(argmax(input_tensor: ttnn.Tensor, *, dim: Optional[int] = None, memory_config: MemoryConfig = std::nullopt) -> ttnn.Tensor
+using argmax_operation_t = decltype(ttnn::argmax);
+void bind_reduction_argmax_operation(py::module& module, const argmax_operation_t& operation) {
+    auto doc =
+        R"doc(argmax(input_tensor: ttnn.Tensor, *, dim: Optional[int] = None, memory_config: MemoryConfig = std::nullopt, output_tensor : Optional[ttnn.Tensor] = std::nullopt, queue_id : [int] = 0) -> ttnn.Tensor
 
             Returns the indices of the maximum value of elements in the ``input`` tensor
             If no ``dim`` is provided, it will return the indices of maximum value of all elements in given ``input``
@@ -68,12 +59,44 @@ void py_module(py::module& module) {
             Keyword Args:
                 * :attr:`dim`: the dimension to reduce. If None, the argmax of the flattened input is returned
                 * :attr:`memory_config`: Memory Config of the output tensor
-        )doc",
-        ttnn::pybind_arguments_t{
-            py::arg("input_tensor").noconvert(),
-            py::kw_only(),
-            py::arg("dim") = std::nullopt,
-            py::arg("memory_config") = std::nullopt});
+                * :attr:`output_tensor` (Optional[ttnn.Tensor]): preallocated output tensor
+                * :attr:`queue_id` (Optional[uint8]): command queue id
+        )doc";
+
+    bind_registered_operation(
+        module,
+        operation,
+        doc,
+        ttnn::pybind_overload_t{
+            [] (const argmax_operation_t& self,
+                const ttnn::Tensor& input_tensor,
+                const std::optional<int> dim,
+                const std::optional<ttnn::MemoryConfig>& memory_config,
+                std::optional<ttnn::Tensor> optional_output_tensor,
+                uint8_t queue_id) {
+                    return self(queue_id, input_tensor, dim, memory_config, optional_output_tensor);
+                },
+                py::arg("input_tensor").noconvert(),
+                py::kw_only(),
+                py::arg("dim") = std::nullopt,
+                py::arg("memory_config") = std::nullopt,
+                py::arg("output_tensor") = std::nullopt,
+                py::arg("queue_id") = 0});
+}
+
+}  // namespace detail
+
+void py_module(py::module& module) {
+    // Generic reductions
+    detail::bind_reduction_operation(module, ttnn::sum);
+    detail::bind_reduction_operation(module, ttnn::mean);
+    detail::bind_reduction_operation(module, ttnn::max);
+    detail::bind_reduction_operation(module, ttnn::min);
+    detail::bind_reduction_operation(module, ttnn::std);
+    detail::bind_reduction_operation(module, ttnn::var);
+
+    // Special reductions
+    detail::bind_reduction_argmax_operation(module, ttnn::argmax);
 }
 
 }  // namespace reduction
