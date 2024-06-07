c40aed599f3275d5cf6e5cd7b9683f1a291bca75
Raymond Kim <109366641+tt-rkim@users.noreply.github.com>
2024-06-07 16:23:21 -0400

    #8764: add ttnn usage examples in ci (#9304)
    * #8764: Move ttnn usage examples into ttnn/examples folder and symlink them into the docs
    
    * #8764: Add some (ttnn) examples to CI for all single-cards
    
    * #8764: Add SPDX headers
    
    * #8764: Rename job to old name because previous was too long

---
+++ ttnn/examples/usage/string_repr.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+
+import ttnn
+
+# Profile can be set to "empty", "short" or "full"
+
+ttnn.set_printoptions(profile="full")
