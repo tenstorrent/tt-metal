2d5308021e2c0432ff834064b2ee537f2cedb067
Raymond Kim <109366641+tt-rkim@users.noreply.github.com>
2024-06-11 12:16:39 -0400

    #9258: Remove ARCH_NAME and TT_METAL_ENV from wheel testing (#9354)

--- tests/scripts/set_up_end_to_end_tests_env.sh
+++ tests/scripts/set_up_end_to_end_tests_env.sh
@@ -3,10 +3,8 @@
 set -eo pipefail
 
 validate_env_vars() {
-  # if [[ -n "${TT_METAL_HOME}" || -n "${PYTHONPATH}" || -n "${ARCH_NAME}" ]]; then
-  if [[ -n "${TT_METAL_HOME}" || -n "${PYTHONPATH}" ]]; then
-    # echo "Developer-level environment variables seem set. This is not allowed in production environments"
-    echo "TT_METAL_HOME / PYTHONPATH is set. This is not allowed in production environments"
+  if [[ -n "${TT_METAL_HOME}" || -n "${PYTHONPATH}" || -n "${ARCH_NAME}" ]]; then
+    echo "TT_METAL_HOME / PYTHONPATH / ARCH_NAME is set. This is not allowed in production environments"
     exit 1
   fi
 }
