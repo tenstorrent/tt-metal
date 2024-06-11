53ccc166c68961893af405aef47b44be5b6cd01c
Bill Teng <135061747+TT-billteng@users.noreply.github.com>
2024-06-10 18:58:05 -0700

    Remove unused dependencies and fetch gtest via CPM (#9332)
    * #9332: remove boost, clang, and sndfile dependencies
    
    * #9332: remove glog
    
    * #9332: remove boost, glog, and libsndfile1 from install instructions
    
    * #9332: fetch gtest via CPM

--- scripts/docker/install_test_deps.sh
+++ scripts/docker/install_test_deps.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
 # Check if two arguments are provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <gtest_version> <doxygen_version>"
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <doxygen_version>"
     exit 1
 fi
 
-GTEST_VERSION=$1
-DOXYGEN_VERSION=$2
-
-# Installs Google test
-mkdir -p /opt/tt_metal_infra/googletest
-chmod ugo+w /opt/tt_metal_infra/googletest
-wget -O /opt/tt_metal_infra/googletest/googletest-release-${GTEST_VERSION}.tar.gz https://github.com/google/googletest/archive/refs/tags/v${GTEST_VERSION}.tar.gz
-tar -xzf /opt/tt_metal_infra/googletest/googletest-release-${GTEST_VERSION}.tar.gz -C /opt/tt_metal_infra/googletest/
-cd /opt/tt_metal_infra/googletest/googletest-${GTEST_VERSION}
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=ON .
-make
-make install
+DOXYGEN_VERSION=$1
 
 # Install doxygen
 mkdir -p /opt/tt_metal_infra/doxygen
