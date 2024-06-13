1e983a82b8de8bf83dab6a4ffc39dcfd31366ab3
Vincent Tang <vtang@tenstorrent.com>
2024-06-13 18:59:19 +0000

    #0: force tracy_tools to use g++ since it will encounter linker errros with clang17 ld

--- cmake/tracy.cmake
+++ cmake/tracy.cmake
@@ -29,7 +29,7 @@ ExternalProject_Add(
     INSTALL_COMMAND
         cp ${TRACY_HOME}/csvexport/build/unix/csvexport-release .
     BUILD_COMMAND
-        cd ${TRACY_HOME}/csvexport/build/unix && TRACY_NO_LTO=1 make -f ${TRACY_HOME}/csvexport/build/unix/Makefile
+        cd ${TRACY_HOME}/csvexport/build/unix && CXX=g++ TRACY_NO_LTO=1 make -f ${TRACY_HOME}/csvexport/build/unix/Makefile
 )
 ExternalProject_Add(
     tracy_capture_tools
@@ -44,6 +44,6 @@ ExternalProject_Add(
     INSTALL_COMMAND
         cp ${TRACY_HOME}/capture/build/unix/capture-release .
     BUILD_COMMAND
-        cd ${TRACY_HOME}/capture/build/unix && TRACY_NO_LTO=1 make -f ${TRACY_HOME}/capture/build/unix/Makefile
+        cd ${TRACY_HOME}/capture/build/unix && CXX=g++ TRACY_NO_LTO=1 make -f ${TRACY_HOME}/capture/build/unix/Makefile
 )
 add_custom_target(tracy_tools ALL DEPENDS tracy_csv_tools tracy_capture_tools)
