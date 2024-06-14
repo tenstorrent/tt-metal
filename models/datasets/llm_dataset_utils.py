13ca02f1ac254d6b6f822ac1881576902011b2b6
Salar Hosseini <skhorasgani@tenstorrent.com>
2024-06-14 16:02:13 +0000

    #5383: [Falcon7b] Add top-1/top-5 metrics to perplexity test and add 128/2K seq lengths
    Signed-off-by: Salar Hosseini <skhorasgani@tenstorrent.com>

--- models/datasets/llm_dataset_utils.py
+++ models/datasets/llm_dataset_utils.py
@@ -5,6 +5,9 @@
 import torch
 import re
 import datasets
+from sklearn.metrics import top_k_accuracy_score
+import numpy as np
+from loguru import logger
 
 
 def wikitext_detokenizer(string):
@@ -80,3 +83,38 @@ def prepare_textgen_dataloader(
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False, drop_last=True)
 
     return dataloader
+
+
+def calculate_acc_metrics(
+    logits: torch.tensor, labels: torch.tensor  # [batch_size * seq_len, vocab_size]  # [batch_size * seq_len]
+):
+    # Calculate negative log-likelihood
+    nll = torch.nn.functional.cross_entropy(logits, labels).item()
+
+    # Calculate top-1/5 accuracy
+    logits = logits.float().detach().numpy()
+    labels = labels.float().detach().numpy()
+    top1 = top_k_accuracy_score(labels, logits, k=1, labels=np.arange(logits.shape[-1]))
+    top5 = top_k_accuracy_score(labels, logits, k=5, labels=np.arange(logits.shape[-1]))
+
+    return nll, top1, top5
+
+
+def verify_acc_metrics(calculated_metrics: dict, expected_metrics: dict):
+    if (
+        calculated_metrics["ppl"] > expected_metrics["ppl"]
+        or calculated_metrics["top1_acc"] < expected_metrics["top1_acc"]
+        or calculated_metrics["top5_acc"] < expected_metrics["top5_acc"]
+    ):
+        assert (
+            False
+        ), f"At least one of Perplexity {calculated_metrics['ppl']}, Top1-Acc {calculated_metrics['top1_acc']}, or Top5-Acc {calculated_metrics['top5_acc']} is worse (higher for perplexity or lower for acc) than {expected_metrics}"
+    elif (
+        calculated_metrics["ppl"] < expected_metrics["ppl"] * 0.96
+        or calculated_metrics["top1_acc"] > expected_metrics["top1_acc"] * 1.04
+        or calculated_metrics["top5_acc"] > expected_metrics["top5_acc"] * 1.04
+    ):
+        assert (
+            False
+        ), f"At least one of Perplexity {calculated_metrics['ppl']}, Top1-Acc {calculated_metrics['top1_acc']}, or Top5-Acc {calculated_metrics['top5_acc']} is better (lower for perplexity or higher for acc) than {expected_metrics}. Please update the expected targets."
+    logger.info("Perplexity/Accuracy Check Passed!")
