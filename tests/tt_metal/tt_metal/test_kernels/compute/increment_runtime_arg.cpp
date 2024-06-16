e4873408cf29abf8736e3f096a68bf61dc131f06
Paul Keller <pkeller@tenstorrent.com>
2024-06-16 18:08:04 +0000

    #8602: Remove test dependence on *L1_ARG_BASE*

--- tests/tt_metal/tt_metal/test_kernels/compute/increment_runtime_arg.cpp
+++ tests/tt_metal/tt_metal/test_kernels/compute/increment_runtime_arg.cpp
@@ -7,9 +7,9 @@ namespace NAMESPACE {
 void MAIN {
 
     // Get configurable number of unique and common runtime args, and increment them all in place by a fixed value.
-    uint32_t rt_args_base = get_arg_addr(0);
     constexpr uint32_t num_unique_rt_args = get_compile_time_arg_val(0);
     constexpr uint32_t num_common_rt_args = get_compile_time_arg_val(1);
+    constexpr uint32_t rt_args_base = get_compile_time_arg_val(2);
     constexpr uint32_t unique_arg_incr_val = 10;
     constexpr uint32_t common_arg_incr_val = 100;
 
