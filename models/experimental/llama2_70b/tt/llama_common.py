2efab54eb39a551b3fc73d104c015af7d3f75f27
avoraTT <avora@tenstorrent.com>
2024-06-17 21:56:04 +0000

    #0: Fix cache path and env variables on CI. Add max batch size and max context len as model config params.

--- models/experimental/llama2_70b/tt/llama_common.py
+++ models/experimental/llama2_70b/tt/llama_common.py
@@ -49,19 +49,18 @@ def should_skip_model_load():
     return skip_model_load
 
 
-def setup_llama_env(llama_version="llama3", batch=32, seq_len=1, n_devices=8, max_batch_size=16, max_context_len=8192):
-    os.environ["WH_ARCH_YAML"] = "wormhole_b0_80_arch_eth_dispatch.yaml"
+def setup_llama_env(llama_version="llama3", batch=32, seq_len=1, n_devices=8, max_batch_size=32, max_context_len=2048):
     if os.getenv("CI") == "true":
         os.environ["TT_METAL_ASYNC_DEVICE_QUEUE"] = "1"
 
         if llama_version == "llama3":
             ckpt_dir = "/mnt/MLPerf/tt_dnn-models/llama-3/llama-3-70b-repacked/"
             tokenizer_path = "/mnt/MLPerf/tt_dnn-models/llama-3/tokenizer.model"
-            cache_path = "/mnt/MLPerf/tt_dnn-models/llama-3/llama-data-cache/weights-cache-3"
+            cache_path = Path("/mnt/MLPerf/tt_dnn-models/llama-3/llama-data-cache/weights-cache-3")
         else:
             ckpt_dir = "/mnt/MLPerf/tt_dnn-models/llama-2/llama-2-70b-repacked/"
             tokenizer_path = "/mnt/MLPerf/tt_dnn-models/llama-2/tokenizer.model"
-            cache_path = "/mnt/MLPerf/tt_dnn-models/llama-2/llama-data-cache/weights-cache-2"
+            cache_path = Path("/mnt/MLPerf/tt_dnn-models/llama-2/llama-data-cache/weights-cache-2")
     else:
         if llama_version == "llama3":
             ckpt_dir = os.getenv("LLAMA_CKPT_DIR", "/home/llama3-data-repacked/llama-3-70b/")
