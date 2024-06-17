144779e3d379c51f920f3e258313e1ec54d1a8ee
Paul Keller <pkeller@tenstorrent.com>
2024-06-17 16:09:11 +0000

    #8602: Tweak dev_mem_map/dev_msgs to make easier to maintain

--- tt_metal/hw/inc/grayskull/eth_l1_address_map.h
+++ tt_metal/hw/inc/grayskull/eth_l1_address_map.h
@@ -15,7 +15,7 @@ struct address_map {
   static constexpr std::int32_t FIRMWARE_SIZE = 0;
   // Base addresses
   static constexpr std::int32_t FIRMWARE_BASE = 0;
-  static constexpr std::int32_t ERISC_MEM_MAILBOX_BASE = 20;
+  static constexpr std::int32_t ERISC_MEM_MAILBOX_BASE = 16;
   static constexpr std::int32_t ERISC_MEM_MAILBOX_STACK_SAVE = 0;
 
   static constexpr std::int32_t COMMAND_Q_BASE = 0;
