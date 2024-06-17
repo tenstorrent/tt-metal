1b364ccf7516e2c1f5546670490f4a2bc43ed37b
Aditya Saigal <129097327+tt-asaigal@users.noreply.github.com>
2024-06-17 08:37:57 -0400

    #0: Add trace_region_size parameter to test_perf_trace_2cqs_bare_metal (#9468)

--- models/demos/resnet/tests/test_perf_resnet_2cqs.py
+++ models/demos/resnet/tests/test_perf_resnet_2cqs.py
@@ -29,7 +29,9 @@ def test_perf_2cqs_bare_metal(
 
 
 @skip_for_wormhole_b0(reason_str="Not tested on single WH")
-@pytest.mark.parametrize("device_params", [{"l1_small_size": 32768, "num_hw_cqs": 2}], indirect=True)
+@pytest.mark.parametrize(
+    "device_params", [{"l1_small_size": 32768, "num_hw_cqs": 2, "trace_region_size": 1327104}], indirect=True
+)
 @pytest.mark.models_performance_bare_metal
 @pytest.mark.parametrize(
     "batch_size, expected_inference_time, expected_compile_time",
