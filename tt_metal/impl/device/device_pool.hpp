c01b773dbb6171279142500680822660262b9062
Allan Liu <aliu@tenstorrent.com>
2024-06-18 15:15:43 +0000

    #0: fix eager_package_silicon ND segfaults

--- tt_metal/impl/device/device_pool.hpp
+++ tt_metal/impl/device/device_pool.hpp
@@ -28,18 +28,23 @@ class DevicePool {
         const uint8_t num_hw_cqs,
         size_t l1_small_size,
         size_t trace_region_size = DEFAULT_TRACE_REGION_SIZE,
-        const std::vector<uint32_t> &l1_bank_remap = {},
-        bool skip_remote_devices = false) noexcept {
+        const std::vector<uint32_t> &l1_bank_remap = {}) noexcept {
         log_debug(tt::LogMetal, "DevicePool initialize");
         if (_inst == nullptr) {
-            static DevicePool device_pool(device_ids, num_hw_cqs, l1_small_size, trace_region_size, l1_bank_remap, skip_remote_devices);
+            static DevicePool device_pool(device_ids, num_hw_cqs, l1_small_size, trace_region_size, l1_bank_remap);
             _inst = &device_pool;
         }
          _inst->l1_small_size = l1_small_size;
          _inst->trace_region_size = trace_region_size;
          _inst->num_hw_cqs = num_hw_cqs;
          _inst->l1_bank_remap = l1_bank_remap;
-         _inst->skip_remote_devices = skip_remote_devices;
+
+         bool skip = true;
+         for (const auto& device_id : device_ids) {
+            const auto& mmio_device_id = tt::Cluster::instance().get_associated_mmio_device(device_id);
+            skip &= (device_id == mmio_device_id);
+         }
+         _inst->skip_remote_devices = skip;
 
          _inst->add_devices_to_pool(device_ids);
          _inst->init_firmware_on_active_devices();
@@ -58,8 +63,7 @@ class DevicePool {
         const uint8_t num_hw_cqs,
         size_t l1_small_size,
         size_t trace_region_size,
-        const std::vector<uint32_t> &l1_bank_remap,
-        bool skip_remote_devices);
+        const std::vector<uint32_t> &l1_bank_remap);
     uint8_t num_hw_cqs;
     size_t l1_small_size;
     size_t trace_region_size;
