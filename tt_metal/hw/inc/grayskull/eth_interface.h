0276af64a92c6ad186d21dfd06b687c082b68dfd
Almeet Bhullar <abhullar@tenstorrent.com>
2024-06-18 04:23:18 +0000

    #9500: Update eth_interface include in tt_cluster to not be hardcoded for WH

---
+++ tt_metal/hw/inc/grayskull/eth_interface.h
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+const uint32_t ETH_RACK_COORD_WIDTH = 0;
+const uint32_t CMD_BUF_SIZE_MASK = 0;
+const uint32_t MAX_BLOCK_SIZE = 0;
+const uint32_t REQUEST_CMD_QUEUE_BASE = 0;
+const uint32_t RESPONSE_CMD_QUEUE_BASE = 0;
+const uint32_t CMD_COUNTERS_SIZE_BYTES = 0;
+const uint32_t REMOTE_UPDATE_PTR_SIZE_BYTES = 0;
+const uint32_t CMD_DATA_BLOCK = 0;
+const uint32_t CMD_WR_REQ = 0;
+const uint32_t CMD_WR_ACK = 0;
+const uint32_t CMD_RD_REQ = 0;
+const uint32_t CMD_RD_DATA = 0;
+const uint32_t CMD_BUF_SIZE = 0;
+const uint32_t CMD_DATA_BLOCK_DRAM = 0;
+const uint32_t ETH_ROUTING_DATA_BUFFER_ADDR = 0;
+const uint32_t REQUEST_ROUTING_CMD_QUEUE_BASE = 0;
+const uint32_t RESPONSE_ROUTING_CMD_QUEUE_BASE = 0;
+const uint32_t CMD_BUF_PTR_MASK = 0;
