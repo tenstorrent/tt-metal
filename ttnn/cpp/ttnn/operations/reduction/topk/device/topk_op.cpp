18945dd1ffcf6f949785e4112d92a8a5ef53df64
Saad Jameel <sjameel@tenstorrent.com>
2024-06-20 14:58:37 +0000

    #9657: add topk multicore to support larger dimension sizes - parallelize by splitting width into local chunks of size 2^x, running topk and then pushing to a a gather core - the gather core then runs topk one last time and pushes it to the interleaved output - only supports input widths of size 2^X - arbitrary height supported - TODO: use remaining cores to parallelize height - TODO: gather into one of the local cores instead of a separate core to minimize noc transactions and cores used, combine kernels to do so - TODO: support k != 32 - TODO: optimize out an unnecessary width sized CB in the gather core which limits the width - TODO: parallelize width along columns rather than rows as we have more cores - TODO: allow parallelization to spillover to next row/col of cores as it will allow for greater widths

--- ttnn/cpp/ttnn/operations/reduction/topk/device/topk_op.cpp
+++ ttnn/cpp/ttnn/operations/reduction/topk/device/topk_op.cpp
@@ -5,6 +5,24 @@
 #include "topk_op.hpp"
 #include "topk_program_factory.hpp"
 
+namespace topk_utils {
+
+static inline bool verify_available_cores(uint16_t width, uint16_t min_dim, uint16_t max_dim, CoreCoord grid, uint16_t k, const uint32_t value_tile_size, const uint32_t index_tile_size) {
+    const auto max_cores = grid.y - 1; // reserve one core for the gather - switch to grid.x as it allows for more cores and allow spillover to next row
+    for (uint16_t split_size = max_dim; split_size >= min_dim; split_size/=2) {
+        uint16_t rem = width % split_size;
+        uint16_t num_cores = width / split_size + (rem > 0);
+        uint32_t memory_cost_gather = 2*num_cores * (value_tile_size + index_tile_size); // gathering one index and one value tile from each local core, allocating two CBs for each
+        uint32_t memory_cost_local = (split_size / TILE_WIDTH) * (value_tile_size + index_tile_size); // we divide the width into split_size chunks and each chunk, as well as a matching set of indices, is processed by a core
+        if (num_cores <= max_cores && (memory_cost_gather + memory_cost_local) < L1_SIZE && num_cores > 1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+constexpr uint32_t multi_core_min_width = 8192;
+}
 namespace ttnn::operations::reduction {
 
 void TopK::validate_with_output_tensors(
@@ -20,6 +38,17 @@ void TopK::validate_with_output_tensors(
 
     TT_FATAL(this->output_mem_config.is_sharded() == false, "Sharded implementation not supported yet");
     TT_FATAL(input_tensors.at(0).get_layout() == Layout::TILE, "The input must be in tiled format");
+    if (input_shape[dim] >=  topk_utils::multi_core_min_width) { // multicore implementation
+        auto device = input_tensors.at(0).device();
+
+        tt::DataFormat value_cb_data_format = tt::tt_metal::datatype_to_dataformat_converter(input_tensors.at(0).get_dtype());
+        tt::DataFormat index_cb_data_format = tt::DataFormat::UInt16;
+
+        uint32_t value_tile_size = tile_size(value_cb_data_format);
+        uint32_t index_tile_size = tile_size(index_cb_data_format);
+        TT_FATAL(topk_utils::verify_available_cores(input_shape[this->dim], 64, input_shape[this->dim]/2, device->compute_with_storage_grid_size(),
+                            this->k, value_tile_size, index_tile_size), "Not enough cores available to run topk operation");
+    }
 }
 
 std::vector<tt::tt_metal::Shape> TopK::compute_output_shapes(const std::vector<Tensor>& input_tensors) const {
@@ -44,7 +73,11 @@ std::vector<Tensor> TopK::create_output_tensors(
 
 operation::ProgramWithCallbacks TopK::create_program(const std::vector<Tensor>& input_tensors, std::vector<Tensor> &output_tensors) const {
     const auto& input_tensor = input_tensors.at(0);
-    return detail::topk_single_core_interleaved(input_tensor, this->k, this->dim, output_tensors.at(0), output_tensors.at(1));
+    if (input_tensor.get_legacy_shape()[dim] < topk_utils::multi_core_min_width) {
+        return detail::topk_single_core_interleaved(input_tensor, this->k, this->dim, output_tensors.at(0), output_tensors.at(1));
+    } else {
+        return detail::topk_multicore_interleaved(input_tensor, this->k, this->dim, output_tensors.at(0), output_tensors.at(1));
+    }
 }
 
 }  // namespace ttnn::operations::reduction
