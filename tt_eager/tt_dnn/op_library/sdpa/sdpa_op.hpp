c283adf881e8e8a684ba535e9fc51236c526b99c
Jack Cai <caixunshiren@gmail.com>
2024-06-20 16:52:45 +0000

    #9510: added new op, flash decode

--- tt_eager/tt_dnn/op_library/sdpa/sdpa_op.hpp
+++ tt_eager/tt_dnn/op_library/sdpa/sdpa_op.hpp
@@ -79,9 +79,42 @@ operation::ProgramWithCallbacks sdpa_multi_core(
     std::optional<const uint32_t> valid_seq_len
 );
 
+struct ScaledDotProductAttentionDecode {
+    const std::optional<float> scale;
+    const MemoryConfig output_mem_config;
+    const tt::operations::primary::transformers::SDPAProgramConfig program_config;
+    const DeviceComputeKernelConfig compute_kernel_config;
+    std::optional<const uint32_t> valid_seq_len;
+
+    void validate(const std::vector<Tensor> &input_tensors, const std::vector<std::optional<const Tensor>>& optional_input_tensors) const;
+    std::vector<Shape> compute_output_shapes(const std::vector<Tensor> &input_tensors) const;
+    std::vector<Tensor> create_output_tensors(const std::vector<Tensor> &input_tensors) const;
+    operation::ProgramWithCallbacks create_program(
+        const std::vector<Tensor>& input_tensors,
+        const std::vector<std::optional<const Tensor>>& optional_input_tensors,
+        std::vector<Tensor> &output_tensors
+    ) const;
+    tt::stl::reflection::Attributes attributes() const;
+};
+
+operation::ProgramWithCallbacks sdpa_decode_multi_core(
+    const Tensor &input_tensor_q,
+    const Tensor &input_tensor_k,
+    const Tensor &input_tensor_v,
+    const Tensor &output_tensor,
+    const std::optional<const Tensor> mask,
+    std::optional<float> scale,
+    std::size_t k_chunk_size,
+    DeviceComputeKernelConfig compute_kernel_config,
+    tt::operations::primary::transformers::SDPAProgramConfig program_config,
+    std::optional<const uint32_t> valid_seq_len
+);
+
 namespace transformers {
 
 Tensor scaled_dot_product_attention(Tensor& input_tensor_q, Tensor& input_tensor_k, Tensor& input_tensor_v, std::optional<const Tensor> causal_mask = std::nullopt, const bool is_causal = true, std::optional<float> scale = std::nullopt, const MemoryConfig& output_mem_config = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, const SDPAProgramConfig& program_config = SDPADefaultProgramConfig{}, std::optional<const DeviceComputeKernelConfig> compute_kernel_config = std::nullopt, std::optional<const uint32_t> valid_seq_len = std::nullopt);
+
+Tensor scaled_dot_product_attention_decode(Tensor& input_tensor_q, Tensor& input_tensor_k, Tensor& input_tensor_v, std::optional<const Tensor> mask = std::nullopt, std::optional<float> scale = std::nullopt, const MemoryConfig& output_mem_config = operation::DEFAULT_OUTPUT_MEMORY_CONFIG, const SDPAProgramConfig& program_config = SDPADefaultProgramConfig{}, std::optional<const DeviceComputeKernelConfig> compute_kernel_config = std::nullopt, std::optional<const uint32_t> valid_seq_len = std::nullopt);
 
 }   // namespace transformers
 
