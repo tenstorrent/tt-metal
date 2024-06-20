cbd0980a3a132feab2d18364fefbc4ae70766d95
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2024-06-20 23:11:56 +0000

    #8835: moved data structures back into Binary struct

--- ttnn/cpp/ttnn/device_operation.hpp
+++ ttnn/cpp/ttnn/device_operation.hpp
@@ -52,24 +52,24 @@ concept ProgramFactoryConcept = requires {
     };
 };
 
-template <typename operation_t>
+template <typename device_operation_t>
 concept DeviceOperationConcept = requires {
-    [](const typename operation_t::operation_attributes_t& operation_attributes,
-       const typename operation_t::tensor_args_t& tensor_args) {
-        operation_t::validate_on_program_cache_hit(operation_attributes, tensor_args);
-        operation_t::validate_on_program_cache_miss(operation_attributes, tensor_args);
+    [](const typename device_operation_t::operation_attributes_t& operation_attributes,
+       const typename device_operation_t::tensor_args_t& tensor_args) {
+        device_operation_t::validate_on_program_cache_hit(operation_attributes, tensor_args);
+        device_operation_t::validate_on_program_cache_miss(operation_attributes, tensor_args);
 
-        using shape_return_value_t = typename operation_t::shape_return_value_t;
+        using shape_return_value_t = typename device_operation_t::shape_return_value_t;
         static_assert(std::same_as<
-                      decltype(operation_t::compute_output_shapes(operation_attributes, tensor_args)),
+                      decltype(device_operation_t::compute_output_shapes(operation_attributes, tensor_args)),
                       shape_return_value_t>);
 
-        using tensor_return_value_t = typename operation_t::tensor_return_value_t;
+        using tensor_return_value_t = typename device_operation_t::tensor_return_value_t;
         static_assert(std::same_as<
-                      decltype(operation_t::create_output_tensors(operation_attributes, tensor_args)),
+                      decltype(device_operation_t::create_output_tensors(operation_attributes, tensor_args)),
                       tensor_return_value_t>);
 
-        const auto program_factory = operation_t::select_program_factory(operation_attributes, tensor_args);
+        const auto program_factory = device_operation_t::select_program_factory(operation_attributes, tensor_args);
         std::visit(
             [](auto&& program_factory) {
                 using program_factory_t = std::decay_t<decltype(program_factory)>;
@@ -79,12 +79,12 @@ concept DeviceOperationConcept = requires {
     };
 };
 
-template <typename operation_t>
-concept DeviceOperationWithCustomProgramCacheConcept = DeviceOperationConcept<operation_t> and requires {
+template <typename device_operation_t>
+concept DeviceOperationWithCustomProgramCacheConcept = DeviceOperationConcept<device_operation_t> and requires {
     [](auto&& program_factory,
-       const typename operation_t::operation_attributes_t& operation_attributes,
-       const typename operation_t::tensor_args_t& tensor_args) {
-        operation_t::compute_program_hash(operation_attributes, tensor_args);
+       const typename device_operation_t::operation_attributes_t& operation_attributes,
+       const typename device_operation_t::tensor_args_t& tensor_args) {
+        device_operation_t::compute_program_hash(operation_attributes, tensor_args);
     };
 };
 
@@ -138,31 +138,31 @@ constexpr auto get_first_tensor(T&& object) {
 
 inline const auto USE_FAST_DISPATCH = std::getenv("TT_METAL_SLOW_DISPATCH_MODE") == nullptr;
 
-template <typename operation_t>
+template <typename device_operation_t>
 inline auto compute_program_hash(
-    const typename operation_t::operation_attributes_t& operation_attributes,
-    const typename operation_t::tensor_args_t& tensor_args) {
-    if constexpr (DeviceOperationWithCustomProgramCacheConcept<operation_t>) {
+    const typename device_operation_t::operation_attributes_t& operation_attributes,
+    const typename device_operation_t::tensor_args_t& tensor_args) {
+    if constexpr (DeviceOperationWithCustomProgramCacheConcept<device_operation_t>) {
         ZoneScopedN("Compute custom program hash");
-        return operation_t::compute_program_hash(operation_attributes, tensor_args);
+        return device_operation_t::compute_program_hash(operation_attributes, tensor_args);
     } else {
         ZoneScopedN("Compute default program hash");
         return tt::stl::hash::hash_objects_with_default_seed(
-            tt::stl::hash::type_hash<operation_t>, operation_attributes, tensor_args);
+            tt::stl::hash::type_hash<device_operation_t>, operation_attributes, tensor_args);
     }
 }
 
-template <typename operation_t>
+template <typename device_operation_t>
 inline auto& create_or_get_program_from_cache(
     auto& program_cache,
     auto cache_hit,
     auto program_hash,
-    const typename operation_t::operation_attributes_t& operation_attributes,
-    const typename operation_t::tensor_args_t& tensor_args,
-    typename operation_t::tensor_return_value_t& tensor_return_value) {
+    const typename device_operation_t::operation_attributes_t& operation_attributes,
+    const typename device_operation_t::tensor_args_t& tensor_args,
+    typename device_operation_t::tensor_return_value_t& tensor_return_value) {
     if (not cache_hit) {
         ZoneScopedN("Program Cache Miss");
-        auto program_factory = operation_t::select_program_factory(operation_attributes, tensor_args);
+        auto program_factory = device_operation_t::select_program_factory(operation_attributes, tensor_args);
 
         auto& program = std::visit(
             [&program_cache,
@@ -191,7 +191,7 @@ inline auto& create_or_get_program_from_cache(
         auto program_factory_index = cached_program_factory.program_factory_index;
 
         using program_factory_variant_t =
-            decltype(operation_t::select_program_factory(operation_attributes, tensor_args));
+            decltype(device_operation_t::select_program_factory(operation_attributes, tensor_args));
         auto program_factory = map_index_to_variant(program_factory_index, program_factory_variant_t{});
 
         auto& program = std::visit(
@@ -217,40 +217,40 @@ constexpr auto check_tensor_types = [](auto&& tensor) {
     static_assert(std::same_as<std::decay_t<decltype(tensor)>, Tensor>);
 };
 
-template <DeviceOperationConcept operation_t>
-typename operation_t::tensor_return_value_t run(
+template <DeviceOperationConcept device_operation_t>
+typename device_operation_t::tensor_return_value_t run(
     uint8_t cq_id,
-    const typename operation_t::operation_attributes_t& operation_attributes,
-    const typename operation_t::tensor_args_t& tensor_args) {
+    const typename device_operation_t::operation_attributes_t& operation_attributes,
+    const typename device_operation_t::tensor_args_t& tensor_args) {
     ZoneScopedN("TT_DNN_DEVICE_OP");
     auto operation_id = assign_operation_id();
 
     tt::stl::reflection::visit_object_of_type<Tensor>(check_tensor_types, tensor_args);
 
-    using tensor_return_value_t = typename operation_t::tensor_return_value_t;
+    using tensor_return_value_t = typename device_operation_t::tensor_return_value_t;
     static_assert(not std::same_as<tensor_return_value_t, void>, "Operation cannot return type cannot be void");
 
     auto device = get_first_tensor(tensor_args).get().device();
     auto& program_cache = device->program_cache;
 
-    auto program_hash = compute_program_hash<operation_t>(operation_attributes, tensor_args);
+    auto program_hash = compute_program_hash<device_operation_t>(operation_attributes, tensor_args);
     auto cache_hit = program_cache.contains(program_hash);
 
     if (cache_hit) {
         ZoneScopedN("Validate on Program Cache Hit");
-        operation_t::validate_on_program_cache_hit(operation_attributes, tensor_args);
+        device_operation_t::validate_on_program_cache_hit(operation_attributes, tensor_args);
     } else {
         ZoneScopedN("Validate on Program Cache Miss");
-        operation_t::validate_on_program_cache_miss(operation_attributes, tensor_args);
+        device_operation_t::validate_on_program_cache_miss(operation_attributes, tensor_args);
     }
     auto tensor_return_value = [&operation_attributes, &tensor_args]() {
         ZoneScopedN("Create Output Tensors");
-        return operation_t::create_output_tensors(operation_attributes, tensor_args);
+        return device_operation_t::create_output_tensors(operation_attributes, tensor_args);
     }();
 
     tt::stl::reflection::visit_object_of_type<Tensor>(check_tensor_types, tensor_return_value);
 
-    auto& program = create_or_get_program_from_cache<operation_t>(
+    auto& program = create_or_get_program_from_cache<device_operation_t>(
         program_cache, cache_hit, program_hash, operation_attributes, tensor_args, tensor_return_value);
 
     if (USE_FAST_DISPATCH) {
@@ -271,7 +271,7 @@ typename operation_t::tensor_return_value_t run(
 
     // TODO: update this to work properly take program cache info, as well as tensors
     TracyOpTNNNDeviceV2(
-        operation_t{},
+        device_operation_t{},
         operation_id,
         device->id(),
         program,
