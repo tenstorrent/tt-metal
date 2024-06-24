93911538d2bbbc0104aebc7735364f16c096f89a
“Nenad <npetrovic@tenstorrent.com>
2024-06-24 14:40:37 +0000

    #9547: Add unit tests for hanging ops

---
+++ tests/tt_eager/python_api_testing/non_working_unit_tests/grayskull/test_complex_abs.py
@@ -0,0 +1,73 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from pathlib import Path
+import sys
+from loguru import logger
+import random
+import pytest
+import torch
+import tt_lib as ttl
+
+from tests.tt_eager.python_api_testing.sweep_tests import pytorch_ops
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc
+from tests.tt_eager.python_api_testing.sweep_tests import tt_lib_ops
+from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_rand_complex
+
+
+def run_complex_abs_test(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
+    torch.manual_seed(data_seed)
+
+    real = torch.Tensor(size=input_shape).uniform_(-100, 100).to(torch.bfloat16).to(torch.float)
+    imag = torch.Tensor(size=input_shape).uniform_(-100, 100).to(torch.bfloat16).to(torch.float)
+
+    torch_x = torch.complex(real, imag)
+
+    # compute ref value
+    ref_value = pytorch_ops.complex_abs(torch_x)
+
+    tt_result = tt_lib_ops.complex_abs(
+        x=torch_x,
+        device=device,
+        dtype=dtype,
+        layout=dlayout,
+        input_mem_config=in_mem_config,
+        output_mem_config=out_mem_config,
+    )
+    # compare tt and golden outputs
+
+    success, pcc_value = comp_pcc(ref_value, tt_result)
+    logger.debug(pcc_value)
+    logger.debug(success)
+
+    assert success
+
+
+test_sweep_args = [
+    (
+        (6, 11, 192, 64),
+        [ttl.tensor.DataType.BFLOAT16],
+        [ttl.tensor.Layout.TILE],
+        [ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM)],
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.DRAM),
+        14854324,
+    ),
+    (
+        (5, 2, 98, 94),
+        [ttl.tensor.DataType.BFLOAT16],
+        [ttl.tensor.Layout.ROW_MAJOR],
+        [ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1)],
+        ttl.tensor.MemoryConfig(ttl.tensor.TensorMemoryLayout.INTERLEAVED, ttl.tensor.BufferType.L1),
+        6114327,
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed",
+    (test_sweep_args),
+)
+def test_complex_abs(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
+    random.seed(0)
+    run_complex_abs_test(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device)
