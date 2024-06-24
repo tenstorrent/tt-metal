765d6c57442d3eca8140e2d74a70382504ca5ef7
umadevimcw <umadevi@multicorewareinc.com>
2024-06-24 10:41:43 +0000

    #9628: Migrate xlogy backward op to ttnn

--- ttnn/ttnn/operations/binary_backward.py
+++ ttnn/ttnn/operations/binary_backward.py
@@ -30,6 +30,21 @@ def _golden_function(grad_tensor, input_tensor_a, input_tensor_b, *args, **kwarg
 atan2_bw = ttnn.register_operation(golden_function=_golden_function)(ttnn._ttnn.operations.binary_backward.atan2_bw)
 
 
+def _golden_function(grad_tensor, input_tensor_a, input_tensor_b, *args, **kwargs):
+    import torch
+
+    pyt_y = torch.xlogy(input_tensor_a, input_tensor_b)
+    input_tensor_a.retain_grad()
+    input_tensor_b.retain_grad()
+
+    pyt_y.backward(gradient=grad_tensor)
+    golden_tensor = [input_tensor_a.grad, input_tensor_b.grad]
+    return golden_tensor
+
+
+xlogy_bw = ttnn.register_operation(golden_function=_golden_function)(ttnn._ttnn.operations.binary_backward.xlogy_bw)
+
+
 def _golden_function(grad_tensor, input_tensor, weight_tensor, *args, **kwargs):
     import torch
 
