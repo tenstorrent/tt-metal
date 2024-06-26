913d1096a21b0ec1282deed9ecfd87cabb24d083
Salar Hosseini <159165450+skhorasganiTT@users.noreply.github.com>
2024-06-26 16:55:07 -0400

    #0: Fix bug with var name in single-chip falcon7b demo tests (#9740)
    Signed-off-by: Salar Hosseini <skhorasgani@tenstorrent.com>
    
    Forcing in to fix single chip pipelines

--- models/demos/wormhole/falcon7b/demo_wormhole.py
+++ models/demos/wormhole/falcon7b/demo_wormhole.py
@@ -7,9 +7,9 @@ from models.demos.falcon7b.demo.demo import run_falcon_demo_kv
 
 
 @pytest.mark.parametrize(
-    "perf_mode, expected_perf_prefill_decode, greedy_sampling, expected_greedy_output_path",
+    "perf_mode, expected_perf_metrics, greedy_sampling, expected_greedy_output_path",
     (
-        (True, [1100, 335], False, None),
+        (True, {"prefill_t/s": 1100, "decode_t/s": 335, "decode_t/s/u": 10.4}, False, None),
         (True, None, False, None),
         (False, None, True, "models/demos/wormhole/falcon7b/expected_greedy_output.json"),
         (False, None, True, None),
@@ -25,7 +25,7 @@ from models.demos.falcon7b.demo.demo import run_falcon_demo_kv
 )
 def test_demo(
     perf_mode,  # Option to measure perf using max seq length (with invalid outputs) and expected perf (t/s)
-    expected_perf_prefill_decode,  # Expected perf (t/s) for prefill and decode in perf mode
+    expected_perf_metrics,  # Expected perf (t/s) for prefill and decode in perf mode
     greedy_sampling,  # Option to use greedy decoding instead of top-k/p
     expected_greedy_output_path,  # Path for expected outputs for greedy decoding
     user_input,
@@ -44,6 +44,6 @@ def test_demo(
         devices=[device],
         perf_mode=perf_mode,
         greedy_sampling=greedy_sampling,
-        expected_perf_prefill_decode=expected_perf_prefill_decode,
+        expected_perf_metrics=expected_perf_metrics,
         expected_greedy_output_path=expected_greedy_output_path,
     )
