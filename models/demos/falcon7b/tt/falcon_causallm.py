3f9461da52704cdec2233f1e90c61fc4db52fd89
Borys Bradel <bbradel@tenstorrent.com>
2024-06-28 12:42:22 +0000

    #9492: add compute kernel configs for some falcon model matmul calls

--- models/demos/falcon7b/tt/falcon_causallm.py
+++ models/demos/falcon7b/tt/falcon_causallm.py
@@ -9,7 +9,50 @@ import ttnn
 from models.demos.falcon7b.tt.falcon_lm_head import falcon_lm_head_matmul_2d
 from models.demos.falcon7b.tt.falcon_model import TtFalconModelShared
 from models.demos.falcon7b.tt.model_utils import get_falcon_default_core_grid, get_weights_cached
-from models.utility_functions import torch_tensors_to_tt_tensors
+from models.utility_functions import (
+    is_grayskull,
+    is_wormhole_b0,
+    torch_tensors_to_tt_tensors,
+)
+
+
+def falcon_lm_head_matmul(
+    input_tensor_a,
+    input_tensor_b,
+    core_grid,
+    output_mem_config=ttnn.DRAM_MEMORY_CONFIG,
+    output_dtype=None,
+):
+    seq_len = input_tensor_a.get_legacy_shape()[2]
+    if seq_len > 512:
+        # TODO: Review if this path is used? If not, we can delete
+        return ttnn.matmul(input_tensor_a, input_tensor_b, memory_config=output_mem_config, dtype=output_dtype)
+    else:
+        if is_grayskull():
+            compute_kernel_config = ttnn.GrayskullComputeKernelConfig(
+                math_fidelity=ttnn.MathFidelity.LoFi,
+                math_approx_mode=True,
+                fp32_dest_acc_en=False,
+                packer_l1_acc=True,
+            )
+        elif is_wormhole_b0():
+            compute_kernel_config = ttnn.WormholeComputeKernelConfig(
+                math_fidelity=ttnn.MathFidelity.LoFi,
+                math_approx_mode=True,
+                fp32_dest_acc_en=False,
+                packer_l1_acc=True,
+            )
+        else:
+            raise RuntimeError(f"Unsupported arch: {device_arch}")
+
+        return ttnn.matmul(
+            input_tensor_a,
+            input_tensor_b,
+            memory_config=output_mem_config,
+            dtype=output_dtype,
+            core_grid=core_grid,
+            compute_kernel_config=compute_kernel_config,
+        )
 
 
 class TtFalconCausalLM(TtFalconModelShared):
@@ -135,11 +178,11 @@ class TtFalconCausalLM(TtFalconModelShared):
                 ]
         else:
             lm_logits = [
-                ttnn.matmul(
+                falcon_lm_head_matmul(
                     hidden_states[device_id],
                     self.lm_head_weights[device_id],
-                    memory_config=self.model_config["LM_HEAD_MM_OUTPUT_MEMCFG"],
-                    dtype=self.model_config["LM_HEAD_MM_OUTPUT_DTYPE"],
+                    output_mem_config=self.model_config["LM_HEAD_MM_OUTPUT_MEMCFG"],
+                    output_dtype=self.model_config["LM_HEAD_MM_OUTPUT_DTYPE"],
                     compute_kernel_config=self.model_config["LM_HEAD_KERNEL_CONFIG"],
                     core_grid=get_falcon_default_core_grid(hidden_states[device_id].device()),
                 )
