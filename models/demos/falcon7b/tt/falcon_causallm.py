a0d94e4f418b46e5499b2ef7f276bfd289c9846e
Borys Bradel <bbradel@tenstorrent.com>
2024-06-28 16:52:00 +0000

    #9492: remove unnecessary extra parameter for falcon model matmul call

--- models/demos/falcon7b/tt/falcon_causallm.py
+++ models/demos/falcon7b/tt/falcon_causallm.py
@@ -181,7 +181,6 @@ class TtFalconCausalLM(TtFalconModelShared):
                     self.lm_head_weights[device_id],
                     output_mem_config=self.model_config["LM_HEAD_MM_OUTPUT_MEMCFG"],
                     output_dtype=self.model_config["LM_HEAD_MM_OUTPUT_DTYPE"],
-                    compute_kernel_config=self.model_config["LM_HEAD_KERNEL_CONFIG"],
                     core_grid=get_falcon_default_core_grid(hidden_states[device_id].device()),
                 )
                 for device_id in range(self.num_devices)
