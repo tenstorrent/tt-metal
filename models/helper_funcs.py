eb7f721d7f1a31a5b3aa9bb5f7d1b0994e5a1e82
Borys Bradel <bbradel@tenstorrent.com>
2024-07-03 02:49:09 +0000

    #9492: make matmul param keyword based in helper funcs

--- models/helper_funcs.py
+++ models/helper_funcs.py
@@ -35,7 +35,7 @@ def Linear(
 
     def linear_(activation):
         assert activation.get_legacy_shape()[-1] == in_features, "activation tensor do not have the expected shape"
-        output = ttnn.matmul(activation, weight_T, output_mem_config)
+        output = ttnn.matmul(activation, weight_T, memory_config=output_mem_config)
 
         if bias is not None:
             output_plus_bias = tensor.bcast(
