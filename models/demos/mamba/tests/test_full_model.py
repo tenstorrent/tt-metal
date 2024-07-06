94f10cc017190807501cbac9754318e398c78249
kpaigwar <kpaigwar@tenstorrent.com>
2024-07-06 21:34:16 +0000

    #0: added prefill mode for mamba modules

--- models/demos/mamba/tests/test_full_model.py
+++ models/demos/mamba/tests/test_full_model.py
@@ -9,6 +9,8 @@ from transformers import AutoTokenizer
 from typing import Optional
 import ttnn
 from models.demos.mamba.reference.decode_model import MambaDecode, MambaPretrainedModelName
+from models.demos.mamba.reference.prefill_decode_model import Mamba, MambaPretrainedModelName
+from models.demos.mamba.reference.args import ModelMode
 from models.demos.mamba.tt.full_model import MambaTT
 from models.demos.mamba.tt import model_config
 from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import (
@@ -44,27 +46,44 @@ def run_inference(
     device: ttnn.Device,
     use_program_cache,
     model_version: MambaPretrainedModelName,
+    mode: ModelMode,
     batch: int,
+    seq_len: int,
     pcc: float,
     num_layers: int,
     iterations: int,
     cache_dir: Optional[str],
 ):
-    torch.manual_seed(10)
+    torch.manual_seed(0)
 
-    reference_model = MambaDecode.from_pretrained(model_version, batch_size=batch)
+    reference_model = Mamba.from_pretrained(model_version, batch_size=batch)
+    reference_model.args.mode = mode
+
+    input_sentence = "Climate change refers to long-term shifts in temperatures and weather patterns. \
+Such shifts can be natural, due to changes in the sun's activity or large volcanic eruptions. \
+But since the 1800s, human activities have been the main driver of climate change, primarily \
+due to the burning of fossil fuels like coal, oil and gas. Burning fossil fuels generates \
+greenhouse gas emissions that act like a blanket wrapped around the Earth, trapping \
+the sun's heat and raising temperatures. The main greenhouse gases that are causing \
+climate change include carbon dioxide and methane. These come from using gasoline \
+for driving a car or coal for heating a building, for example. Clearing land and \
+cutting down forests can also release carbon dioxide. Agriculture, oil and gas \
+operations are major sources of methane emissions. Energy, industry, transport, \
+buildings, agriculture and land use are among the main sectors causing greenhouse gases."
 
     tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
-    input_ids = tokenizer("Hello", return_tensors="pt")["input_ids"]
+    input_ids = tokenizer(input_sentence, return_tensors="pt")["input_ids"]
+    input_ids = input_ids[:, :seq_len]
     input_ids = input_ids.repeat(batch, 1)
 
     mamba_model_pytorch = MambaPytorch(reference_model, num_layers)
     mamba_model_pytorch.eval()
-    for _ in range(iterations):
+    for idx in range(iterations):
         with torch.no_grad():
             reference_output = mamba_model_pytorch(input_ids)
 
-    config = model_config.create_model_config(batch, reference_model.args.d_model)
+    config = model_config.create_model_config(batch, reference_model.args.d_model, mode=mode, seq_len=seq_len)
+
     mamba_model_tt = MambaTT(reference_model, device, config, tt_cache_path=cache_dir, num_layers=num_layers)
 
     for _ in range(iterations):
@@ -80,16 +99,26 @@ def run_inference(
         assert does_pass, f"PCC value is lower than {pcc}"
 
 
-@skip_for_grayskull("Not supported on Grayskull")
 @pytest.mark.parametrize(
-    "model_version, batch, pcc, num_layers, iterations",
+    "model_version, mode, batch, seq_len, num_layers, iterations, pcc",
     (
         (
             "state-spaces/mamba-2.8b",
-            32,
-            0.94,
+            ModelMode.PREFILL,
+            1,
+            64,
             64,
             1,
+            0.96,
+        ),
+        (
+            "state-spaces/mamba-2.8b",
+            ModelMode.DECODE,
+            32,
+            1,
+            64,
+            1,
+            0.96,
         ),
     ),
 )
@@ -98,16 +127,20 @@ def test_inference(
     use_program_cache,
     get_tt_cache_path,
     model_version: MambaPretrainedModelName,
+    mode: ModelMode,
     batch: int,
-    pcc: float,
+    seq_len: int,
     num_layers: int,
     iterations: int,
+    pcc: float,
 ):
     run_inference(
         device,
         use_program_cache,
         model_version,
+        mode,
         batch,
+        seq_len,
         pcc,
         num_layers,
         iterations,
@@ -134,7 +167,9 @@ def test_device_perf(
         device,
         use_program_cache,
         model_version,
+        ModelMode.DECODE,
         batch,
+        1,
         pcc,
         num_layers,
         iterations,
