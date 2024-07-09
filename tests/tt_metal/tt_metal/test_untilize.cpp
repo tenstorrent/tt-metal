41081aae166b320bcca955410f833fb5462c50f5
Reem Tawfik <rtawfik@tenstorrent.com>
2024-07-09 19:22:36 +0000

    #10052: Add metal pack untilize test

--- tests/tt_metal/tt_metal/test_untilize.cpp
+++ tests/tt_metal/tt_metal/test_untilize.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -74,157 +74,159 @@ int main(int argc, char **argv) {
     auto slow_dispatch_mode = getenv("TT_METAL_SLOW_DISPATCH_MODE");
     TT_FATAL(slow_dispatch_mode, "This test only supports TT_METAL_SLOW_DISPATCH_MODE");
 
-    try {
+    std::vector<string> untilize_types = {"unpack", "pack"};
+    Program programs[] = {tt_metal::CreateProgram(), tt_metal::CreateProgram()};
 
-        ////////////////////////////////////////////////////////////////////////////
-        //                      Device Setup
-        ////////////////////////////////////////////////////////////////////////////
-        int device_id = 0;
-        tt_metal::Device *device =
-            tt_metal::CreateDevice(device_id);
+    ////////////////////////////////////////////////////////////////////////////
+    //                      Device Setup
+    ////////////////////////////////////////////////////////////////////////////
+    int device_id = 0;
+    tt_metal::Device *device = tt_metal::CreateDevice(device_id);
 
-
-
-        // ////////////////////////////////////////////////////////////////////////////
-        //                      Application Setup
-        ////////////////////////////////////////////////////////////////////////////
-        tt_metal::Program program = tt_metal::CreateProgram();
-
-        CoreCoord core = {0, 0};
-
-        uint32_t single_tile_size = 2 * 1024;
-
-        uint32_t num_tiles_r = 1;
-        uint32_t num_tiles_c = 4;
-        uint32_t num_tiles = num_tiles_r * num_tiles_c;
-
-        uint32_t dram_buffer_size = single_tile_size * num_tiles; // num_tiles of FP16_B, hard-coded in the reader/writer kernels
-
-        int dram_src_channel_id = 0;
-        int dram_dst_channel_id = 0;
-
-        tt_metal::InterleavedBufferConfig dram_config{
-                    .device=device,
-                    .size = dram_buffer_size,
-                    .page_size = dram_buffer_size,
-                    .buffer_type = tt_metal::BufferType::DRAM
-        };
+    CommandQueue& cq = device->command_queue();
 
 
-        auto src_dram_buffer = CreateBuffer(dram_config);
-        uint32_t dram_buffer_src_addr = src_dram_buffer->address();
+    for (uint untilize_idx = 0;  untilize_idx < untilize_types.size(); untilize_idx++) {
+        try {
+            log_info(LogTest, "====================================================================");
+            tt::log_info(tt::LogTest, "Running untilize test for type={}", untilize_types[untilize_idx]);
+            // ////////////////////////////////////////////////////////////////////////////
+            //                      Application Setup
+            ////////////////////////////////////////////////////////////////////////////
+            tt_metal::Program& program = programs[untilize_idx];
 
-        auto dst_dram_buffer = CreateBuffer(dram_config);
-        uint32_t dram_buffer_dst_addr = dst_dram_buffer->address();
+            CoreCoord core = {0, 0};
 
-        auto dram_src_noc_xy = src_dram_buffer->noc_coordinates();
-        auto dram_dst_noc_xy = dst_dram_buffer->noc_coordinates();
+            uint32_t single_tile_size = 2 * 1024;
 
-        // input CB is larger than the output CB, to test the backpressure from the output CB all the way into the input CB
-        // CB_out size = 1 forces the serialization of packer and writer kernel, generating backpressure to math kernel, input CB and reader
-        uint32_t src0_cb_index = 0;
-        uint32_t num_input_tiles = 8;
-        tt_metal::CircularBufferConfig cb_src0_config = tt_metal::CircularBufferConfig(num_input_tiles * single_tile_size, {{src0_cb_index, tt::DataFormat::Float16_b}})
-            .set_page_size(src0_cb_index, single_tile_size);
-        auto cb_src0 = tt_metal::CreateCircularBuffer(program, core, cb_src0_config);
+            uint32_t num_tiles_r = 1;
+            uint32_t num_tiles_c = 4;
+            uint32_t num_tiles = num_tiles_r * num_tiles_c;
 
-        uint32_t ouput_cb_index = 16; // output operands start at index 16
-        uint32_t num_output_tiles = 8;
-        tt_metal::CircularBufferConfig cb_output_config = tt_metal::CircularBufferConfig(num_output_tiles * single_tile_size, {{ouput_cb_index, tt::DataFormat::Float16_b}})
-            .set_page_size(ouput_cb_index, single_tile_size);
-        auto cb_output = tt_metal::CreateCircularBuffer(program, core, cb_output_config);
+            uint32_t dram_buffer_size = single_tile_size * num_tiles; // num_tiles of FP16_B, hard-coded in the reader/writer kernels
 
-        auto unary_reader_kernel = tt_metal::CreateKernel(
-            program,
-            "tt_metal/kernels/dataflow/reader_unary.cpp",
-            core,
-            tt_metal::DataMovementConfig{.processor = tt_metal::DataMovementProcessor::RISCV_1, .noc = tt_metal::NOC::RISCV_1_default});
+            int dram_src_channel_id = 0;
+            int dram_dst_channel_id = 0;
 
-        auto unary_writer_kernel = tt_metal::CreateKernel(
-            program,
-            "tt_metal/kernels/dataflow/writer_unary.cpp",
-            core,
-            tt_metal::DataMovementConfig{.processor = tt_metal::DataMovementProcessor::RISCV_0, .noc = tt_metal::NOC::RISCV_0_default});
-
-        vector<uint32_t> compute_kernel_args = {
-            1, // per_core_block_cnt
-            uint(num_tiles_c) // per_core_block_tile_cnt
-        };
-
-        auto eltwise_unary_kernel = tt_metal::CreateKernel(
-            program,
-            "tests/tt_metal/tt_metal/test_kernels/compute/untilize.cpp",
-            core,
-            tt_metal::ComputeConfig{.compile_args = compute_kernel_args}
-        );
-
-        ////////////////////////////////////////////////////////////////////////////
-        //                      Compile Application
-        ////////////////////////////////////////////////////////////////////////////
-        std::vector<uint32_t> src_vec = create_arange_vector_of_bfloat16(
-            dram_buffer_size, false);
-
-        vector<uint32_t> golden = gold_standard_untilize(src_vec, {num_tiles_r * 32, num_tiles_c * 32});
-
-        ////////////////////////////////////////////////////////////////////////////
-        //                      Execute Application
-        ////////////////////////////////////////////////////////////////////////////
-        pass &= tt_metal::detail::WriteToDeviceDRAMChannel(device, dram_src_channel_id, src_dram_buffer->address(), src_vec);
+            tt_metal::InterleavedBufferConfig dram_config{
+                        .device=device,
+                        .size = dram_buffer_size,
+                        .page_size = dram_buffer_size,
+                        .buffer_type = tt_metal::BufferType::DRAM
+            };
 
 
+            auto src_dram_buffer = CreateBuffer(dram_config);
+            uint32_t dram_buffer_src_addr = src_dram_buffer->address();
 
-        tt_metal::SetRuntimeArgs(
-            program,
-            unary_reader_kernel,
-            core,
-            {dram_buffer_src_addr,
-            (std::uint32_t)dram_src_noc_xy.x,
-            (std::uint32_t)dram_src_noc_xy.y,
-            num_tiles});
+            auto dst_dram_buffer = CreateBuffer(dram_config);
+            uint32_t dram_buffer_dst_addr = dst_dram_buffer->address();
 
-        tt_metal::SetRuntimeArgs(
-            program,
-            unary_writer_kernel,
-            core,
-            {dram_buffer_dst_addr,
-            (std::uint32_t)dram_dst_noc_xy.x,
-            (std::uint32_t)dram_dst_noc_xy.y,
-            num_tiles});
+            auto dram_src_noc_xy = src_dram_buffer->noc_coordinates();
+            auto dram_dst_noc_xy = dst_dram_buffer->noc_coordinates();
+
+            // input CB is larger than the output CB, to test the backpressure from the output CB all the way into the input CB
+            // CB_out size = 1 forces the serialization of packer and writer kernel, generating backpressure to math kernel, input CB and reader
+            uint32_t src0_cb_index = 0;
+            uint32_t num_input_tiles = 8;
+            tt_metal::CircularBufferConfig cb_src0_config = tt_metal::CircularBufferConfig(num_input_tiles * single_tile_size, {{src0_cb_index, tt::DataFormat::Float16_b}})
+                .set_page_size(src0_cb_index, single_tile_size);
+            auto cb_src0 = tt_metal::CreateCircularBuffer(program, core, cb_src0_config);
+
+            uint32_t ouput_cb_index = 16; // output operands start at index 16
+            uint32_t num_output_tiles = 8;
+            tt_metal::CircularBufferConfig cb_output_config = tt_metal::CircularBufferConfig(num_output_tiles * single_tile_size, {{ouput_cb_index, tt::DataFormat::Float16_b}})
+                .set_page_size(ouput_cb_index, single_tile_size);
+            auto cb_output = tt_metal::CreateCircularBuffer(program, core, cb_output_config);
+
+            auto unary_reader_kernel = tt_metal::CreateKernel(
+                program,
+                "tt_metal/kernels/dataflow/reader_unary.cpp",
+                core,
+                tt_metal::DataMovementConfig{.processor = tt_metal::DataMovementProcessor::RISCV_1, .noc = tt_metal::NOC::RISCV_1_default});
+
+            auto unary_writer_kernel = tt_metal::CreateKernel(
+                program,
+                "tt_metal/kernels/dataflow/writer_unary.cpp",
+                core,
+                tt_metal::DataMovementConfig{.processor = tt_metal::DataMovementProcessor::RISCV_0, .noc = tt_metal::NOC::RISCV_0_default});
+
+            vector<uint32_t> compute_kernel_args = {
+                1, // per_core_block_cnt
+                uint(num_tiles_c) // per_core_block_tile_cnt
+            };
+
+            string untilize_kernel = "tests/tt_metal/tt_metal/test_kernels/compute/" + untilize_types[untilize_idx] + "_untilize.cpp";
+
+            auto eltwise_unary_kernel = tt_metal::CreateKernel(
+                program,
+                untilize_kernel,
+                core,
+                tt_metal::ComputeConfig{.compile_args = compute_kernel_args}
+            );
+
+            tt_metal::SetRuntimeArgs(
+                program,
+                unary_reader_kernel,
+                core,
+                {dram_buffer_src_addr,
+                (std::uint32_t)dram_src_noc_xy.x,
+                (std::uint32_t)dram_src_noc_xy.y,
+                num_tiles});
+
+            tt_metal::SetRuntimeArgs(
+                program,
+                unary_writer_kernel,
+                core,
+                {dram_buffer_dst_addr,
+                (std::uint32_t)dram_dst_noc_xy.x,
+                (std::uint32_t)dram_dst_noc_xy.y,
+                num_tiles});
+
+            ////////////////////////////////////////////////////////////////////////////
+            //                      Compile & Execute Application
+            ////////////////////////////////////////////////////////////////////////////
+            std::vector<uint32_t> src_vec = create_arange_vector_of_bfloat16(
+                dram_buffer_size, false);
+
+            pass &= tt_metal::detail::WriteToDeviceDRAMChannel(device, dram_src_channel_id, src_dram_buffer->address(), src_vec);
 
 
+            tt_metal::detail::LaunchProgram(device, program);
 
-        tt_metal::detail::LaunchProgram(device, program);
+            std::vector<uint32_t> result_vec;
+            tt_metal::detail::ReadFromDeviceDRAMChannel(
+                device, dram_dst_channel_id, dst_dram_buffer->address(), dst_dram_buffer->size(), result_vec);
+            ////////////////////////////////////////////////////////////////////////////
+            //                      Validation & Teardown
+            ////////////////////////////////////////////////////////////////////////////
+            vector<uint32_t> golden = gold_standard_untilize(src_vec, {num_tiles_r * 32, num_tiles_c * 32});
 
-        std::vector<uint32_t> result_vec;
-        tt_metal::detail::ReadFromDeviceDRAMChannel(
-            device, dram_dst_channel_id, dst_dram_buffer->address(), dst_dram_buffer->size(), result_vec);
-        ////////////////////////////////////////////////////////////////////////////
-        //                      Validation & Teardown
-        ////////////////////////////////////////////////////////////////////////////
+            TT_FATAL(golden.size() == result_vec.size());
+            pass &= (golden == result_vec);
+            tt::log_info(tt::LogTest, "Test {} for type={}", pass ? "Passed" : "Failed", untilize_types[untilize_idx]);
 
-        TT_FATAL(golden.size() == result_vec.size());
-        pass &= (golden == result_vec);
+            if (not pass) {
+                std::cout << "GOLDEN" << std::endl;
+                print_vec_of_uint32_as_packed_bfloat16(golden, num_tiles);
 
-        if (not pass) {
-            std::cout << "GOLDEN" << std::endl;
-            print_vec_of_uint32_as_packed_bfloat16(golden, num_tiles);
+                std::cout << "RESULT" << std::endl;
+                print_vec_of_uint32_as_packed_bfloat16(result_vec, num_tiles);
+            }
 
-            std::cout << "RESULT" << std::endl;
-            print_vec_of_uint32_as_packed_bfloat16(result_vec, num_tiles);
+        } catch (const std::exception &e) {
+            pass = false;
+            // Capture the exception error message
+            log_error(LogTest, "{}", e.what());
+            // Capture system call errors that may have returned from driver/kernel
+            log_error(LogTest, "System error message: {}", std::strerror(errno));
         }
-
-        pass &= tt_metal::CloseDevice(device);
-
-    } catch (const std::exception &e) {
-        pass = false;
-        // Capture the exception error message
-        log_error(LogTest, "{}", e.what());
-        // Capture system call errors that may have returned from driver/kernel
-        log_error(LogTest, "System error message: {}", std::strerror(errno));
     }
 
+    pass &= tt_metal::CloseDevice(device);
+
     if (pass) {
-        log_info(LogTest, "Test Passed");
+        log_info(LogTest, "All Tests Passed");
     } else {
         TT_THROW("Test Failed");
     }
