7597eace9c98f9b5bd03d069817f0ccbd50d9a7f
Jacob DeSousa <jdesousa@tenstorrent.com>
2024-07-09 19:02:01 +0000

    #8957: Build out query tool for elastic search

--- tests/sweep_framework/query.py
+++ tests/sweep_framework/query.py
@@ -3,76 +3,37 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import click
-from pymongo import MongoClient
-from test_status import TestStatus
-from beautifultable import BeautifulTable
+import os
+import pathlib
+from elasticsearch import Elasticsearch
+from tests.sweep_framework.statuses import TestStatus
+from beautifultable import BeautifulTable, STYLE_COMPACT
 from termcolor import colored
+
+ELASTIC_PASSWORD = os.getenv("ELASTIC_PASSWORD")
 
 
 @click.group()
 @click.option("--module-name", default=None, help="Name of the module to be queried.")
 @click.option("--batch-name", default=None, help="Batch name to filter by.")
 @click.option("--vector-id", default=None, help="Individual Vector ID to filter by.")
-@click.option("--mongo", default="mongodb://localhost:27017", help="Mongo Connection String")
+@click.option("--elastic", default="http://localhost:9200", help="Elastic Connection String")
 @click.option("--all", default=False, help="Displays total run statistics instead of the most recent run.")
 @click.pass_context
-def cli(ctx, module_name, batch_name, vector_id, mongo, all):
+def cli(ctx, module_name, batch_name, vector_id, elastic, all):
     ctx.ensure_object(dict)
 
     ctx.obj["module_name"] = module_name
     ctx.obj["batch_name"] = batch_name
     ctx.obj["vector_id"] = vector_id
-    ctx.obj["mongo"] = mongo
+    ctx.obj["elastic"] = elastic
     ctx.obj["all"] = all
-
-
-def build_match_filters(batch_name, vector_id):
-    if not batch_name:
-        return [{"$match": {"status": str(x)}} for x in TestStatus]
-    elif not vector_id:
-        return [{"$match": {"$and": [{"status": str(x)}, {"batch_name": batch_name}]}} for x in TestStatus]
-    else:
-        return [
-            {"$match": {"$and": [{"status": str(x)}, {"batch_name": batch_name}, {"vector_id": vector_id}]}}
-            for x in TestStatus
-        ]
-
-
-def get_runs(collection, match_filters, all_filter):
-    pass_vectors = list(collection.aggregate([match_filters[0]] + all_filter))
-    fail_assert_exception_vectors = list(collection.aggregate([match_filters[1]] + all_filter))
-    fail_crash_hang_vectors = list(collection.aggregate([match_filters[2]] + all_filter))
-    not_run_vectors = list(collection.aggregate([match_filters[3]] + all_filter))
-
-    return pass_vectors, fail_assert_exception_vectors, fail_crash_hang_vectors, not_run_vectors
-
-
-def gather_results(ctx):
-    client = MongoClient(ctx.obj["mongo"])
-    db = client.test_results
-
-    all_filter = [{"$group": {"_id": "$vector_id", "first": {"$max": "$timestamp"}}}] if not ctx.obj["all"] else []
-
-    if not ctx.obj["module_name"]:
-        # Table 1, no module specified
-        collection_names = db.list_collection_names()
-        match_filters = build_match_filters(ctx.obj["batch_name"], ctx.obj["vector_id"])
-        for collection_name in collection_names:
-            collection = db[collection_name]
-            pv, faev, fchv, nrv = get_runs(collection, match_filters, all_filter)
-            yield pv, faev, fchv, nrv
-    else:
-        collection = db[ctx.obj["module_name"] + "_test_results"]
-        match_filters = build_match_filters(ctx.obj["batch_name"], ctx.obj["vector_id"])
-
-        pc, fase, fchc, nrc = get_runs(collection_name, match_filters, all_filter)
-        yield pv, faev, fchv, nrv
 
 
 @cli.command()
 @click.pass_context
 def summary(ctx):
-    table = BeautifulTable()
+    table = BeautifulTable(maxwidth=200)
     table.columns.header = [
         colored("PASS", "light_green"),
         colored("FAIL (ASSERT/EXCEPTION)", "light_red"),
@@ -80,14 +41,236 @@ def summary(ctx):
         colored("NOT RUN", "light_grey"),
     ]
 
-    pv, faev, fchv, nrv = gather_results(ctx)
+    client = Elasticsearch(ctx.obj["elastic"], basic_auth=("elastic", ELASTIC_PASSWORD))
+    sweeps_path = pathlib.Path(__file__).parent / "sweeps"
+
+    if ctx.obj["module_name"] is None:
+        for file in sorted(sweeps_path.glob("*.py")):
+            module_name = str(pathlib.Path(file).relative_to(sweeps_path))[:-3]
+            results_index = module_name + "_test_results"
+            if not ctx.obj["all"]:
+                response = client.search(
+                    index=results_index,
+                    size=0,
+                    aggs={
+                        "latest_runs": {
+                            "terms": {"field": "vector_id.keyword"},
+                            "aggs": {
+                                "latest_timestamp": {
+                                    "top_hits": {"sort": [{"timestamp.keyword": {"order": "desc"}}], "size": 1}
+                                }
+                            },
+                        }
+                    },
+                )["aggregations"]["latest_runs"]["buckets"]
+                passes = sum(
+                    bucket["latest_timestamp"]["hits"]["hits"][0]["_source"]["status"] == str(TestStatus.PASS)
+                    for bucket in response
+                )
+                fail_ae = sum(
+                    bucket["latest_timestamp"]["hits"]["hits"][0]["_source"]["status"]
+                    == str(TestStatus.FAIL_ASSERT_EXCEPTION)
+                    for bucket in response
+                )
+                fail_ch = sum(
+                    bucket["latest_timestamp"]["hits"]["hits"][0]["_source"]["status"]
+                    == str(TestStatus.FAIL_CRASH_HANG)
+                    for bucket in response
+                )
+                not_run = sum(
+                    bucket["latest_timestamp"]["hits"]["hits"][0]["_source"]["status"] == str(TestStatus.NOT_RUN)
+                    for bucket in response
+                )
+            else:
+                passes = client.count(index=results_index, query={"match": {"status": str(TestStatus.PASS)}})["count"]
+                fail_ae = client.count(
+                    index=results_index, query={"match": {"status": str(TestStatus.FAIL_ASSERT_EXCEPTION)}}
+                )["count"]
+                fail_ch = client.count(
+                    index=results_index, query={"match": {"status": str(TestStatus.FAIL_CRASH_HANG)}}
+                )["count"]
+                not_run = client.count(index=results_index, query={"match": {"status": str(TestStatus.NOT_RUN)}})[
+                    "count"
+                ]
+
+            table.rows.append([passes, fail_ae, fail_ch, not_run], module_name)
+    elif ctx.obj["batch_name"] is None:
+        results_index = ctx.obj["module_name"] + "_test_results"
+        if not ctx.obj["all"]:
+            response = client.search(
+                index=results_index,
+                size=0,
+                aggs={
+                    "group_by_batch_name": {
+                        "terms": {"field": "batch_name.keyword", "size": 50},
+                        "aggs": {
+                            "group_by_vector_id": {
+                                "terms": {"field": "vector_id.keyword", "size": 10000},
+                                "aggs": {
+                                    "latest_timestamp": {
+                                        "top_hits": {"sort": [{"timestamp.keyword": {"order": "desc"}}], "size": 1}
+                                    }
+                                },
+                            }
+                        },
+                    }
+                },
+            )["aggregations"]["group_by_batch_name"]["buckets"]
+            for bucket in response:
+                passes = sum(
+                    bucket["latest_timestamp"]["hits"]["hits"][0]["_source"]["status"] == str(TestStatus.PASS)
+                    for bucket in bucket["group_by_vector_id"]["buckets"]
+                )
+                fail_ae = sum(
+                    bucket["latest_timestamp"]["hits"]["hits"][0]["_source"]["status"]
+                    == str(TestStatus.FAIL_ASSERT_EXCEPTION)
+                    for bucket in bucket["group_by_vector_id"]["buckets"]
+                )
+                fail_ch = sum(
+                    bucket["latest_timestamp"]["hits"]["hits"][0]["_source"]["status"]
+                    == str(TestStatus.FAIL_CRASH_HANG)
+                    for bucket in bucket["group_by_vector_id"]["buckets"]
+                )
+                not_run = sum(
+                    bucket["latest_timestamp"]["hits"]["hits"][0]["_source"]["status"] == str(TestStatus.NOT_RUN)
+                    for bucket in bucket["group_by_vector_id"]["buckets"]
+                )
+                table.rows.append([passes, fail_ae, fail_ch, not_run], bucket["key"])
+        else:
+            response = client.search(
+                index=results_index,
+                size=0,
+                aggs={"group_by_batch_name": {"terms": {"field": "batch_name.keyword", "size": 50}}},
+            )
+            batches = [bucket["key"] for bucket in response["aggregations"]["group_by_batch_name"]["buckets"]]
+            for batch in batches:
+                passes = client.count(
+                    index=results_index,
+                    query={
+                        "bool": {
+                            "must": [{"match": {"status": str(TestStatus.PASS)}}, {"match": {"batch_name": batch}}]
+                        }
+                    },
+                )["count"]
+                fail_ae = client.count(
+                    index=results_index,
+                    query={
+                        "bool": {
+                            "must": [
+                                {"match": {"status": str(TestStatus.FAIL_ASSERT_EXCEPTION)}},
+                                {"match": {"batch_name": batch}},
+                            ]
+                        }
+                    },
+                )["count"]
+                fail_ch = client.count(
+                    index=results_index,
+                    query={
+                        "bool": {
+                            "must": [
+                                {"match": {"status": str(TestStatus.FAIL_CRASH_HANG)}},
+                                {"match": {"batch_name": batch}},
+                            ]
+                        }
+                    },
+                )["count"]
+                not_run = client.count(
+                    index=results_index,
+                    query={
+                        "bool": {
+                            "must": [{"match": {"status": str(TestStatus.NOT_RUN)}}, {"match": {"batch_name": batch}}]
+                        }
+                    },
+                )["count"]
+                table.rows.append([passes, fail_ae, fail_ch, not_run], batch)
+
+    elif ctx.obj["vector_id"] is None:
+        results_index = ctx.obj["module_name"] + "_test_results"
+        response = client.search(
+            index=results_index,
+            size=0,
+            query={"match": {"batch_name": ctx.obj["batch_name"]}},
+            aggs={
+                "group_by_vector_id": {
+                    "terms": {"field": "vector_id.keyword", "size": 10000},
+                    "aggs": {
+                        "latest_timestamp": {
+                            "top_hits": {"sort": [{"timestamp.keyword": {"order": "desc"}}], "size": 1}
+                        }
+                    },
+                }
+            },
+        )["aggregations"]["group_by_vector_id"]["buckets"]
+        for bucket in response:
+            passes = bucket["latest_timestamp"]["hits"]["hits"][0]["_source"]["status"] == str(TestStatus.PASS)
+            fail_ae = bucket["latest_timestamp"]["hits"]["hits"][0]["_source"]["status"] == str(
+                TestStatus.FAIL_ASSERT_EXCEPTION
+            )
+            fail_ch = bucket["latest_timestamp"]["hits"]["hits"][0]["_source"]["status"] == str(
+                TestStatus.FAIL_CRASH_HANG
+            )
+            not_run = bucket["latest_timestamp"]["hits"]["hits"][0]["_source"]["status"] == str(TestStatus.NOT_RUN)
+            table.rows.append([passes, fail_ae, fail_ch, not_run], bucket["key"])
+
+    print(table)
+    client.close()
 
 
 @cli.command()
 @click.pass_context
 def detail(ctx):
-    results = gather_results(ctx)
-    pass
+    table = BeautifulTable(maxwidth=200)
+    table.set_style(STYLE_COMPACT)
+    table.columns.header = [
+        colored("Sweep", "light_red"),
+        colored("Batch", "light_red"),
+        colored("Vector ID", "light_red"),
+        colored("Timestamp", "light_red"),
+        colored("Status", "light_red"),
+        colored("Details", "light_red"),
+        colored("Git Hash", "light_red"),
+    ]
+
+    client = Elasticsearch(ctx.obj["elastic"], basic_auth=("elastic", ELASTIC_PASSWORD))
+    sweeps_path = pathlib.Path(__file__).parent / "sweeps"
+
+    matches = []
+    if ctx.obj["batch_name"]:
+        matches.append({"match": {"batch_name": ctx.obj["batch_name"]}})
+    if ctx.obj["vector_id"]:
+        matches.append({"match": {"vector_id": ctx.obj["vector_id"]}})
+
+    def add_results_for_module(module_name):
+        results_index = module_name + "_test_results"
+        results = client.search(index=results_index, size=10000, query={"bool": {"must": matches}})["hits"]["hits"]
+        for result in results:
+            source = result["_source"]
+            try:
+                detail = source["message"]
+            except:
+                detail = str(source["exception"]).replace("\n", " ")[:50]
+            table.rows.append(
+                [
+                    module_name,
+                    source["batch_name"],
+                    source["vector_id"],
+                    source["timestamp"],
+                    source["status"][11:],
+                    detail,
+                    source["git_hash"],
+                ],
+                result["_id"],
+            )
+
+    if not ctx.obj["module_name"]:
+        for file in sorted(sweeps_path.glob("*.py")):
+            module_name = str(pathlib.Path(file).relative_to(sweeps_path))[:-3]
+            add_results_for_module(module_name)
+    else:
+        add_results_for_module(ctx.obj["module_name"])
+
+    print(table)
+    client.close()
 
 
 if __name__ == "__main__":
