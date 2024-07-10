c26d6967e138bbb55f0875eb334d9c67b941c48d
Aswinmcw <azayasankaran@tenstorrent.com>
2024-07-10 06:54:31 +0000

    #10073: Merge sigmoid_bw to TTNN

--- tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_sigmoid.py
+++ tests/tt_eager/python_api_testing/unit_testing/backward_ops/test_backward_sigmoid.py
@@ -4,7 +4,7 @@
 
 import torch
 import pytest
-import tt_lib
+import ttnn
 from tests.tt_eager.python_api_testing.unit_testing.backward_ops.utility_funcs import data_gen_with_range, compare_pcc
 
 
@@ -22,7 +22,7 @@ def test_bw_sigmoid(input_shapes, device):
 
     pyt_y = torch.sigmoid(in_data)
 
-    tt_output_tensor_on_device = tt_lib.tensor.sigmoid_bw(grad_tensor, input_tensor)
+    tt_output_tensor_on_device = ttnn.sigmoid_bw(grad_tensor, input_tensor)
 
     in_data.retain_grad()
 
