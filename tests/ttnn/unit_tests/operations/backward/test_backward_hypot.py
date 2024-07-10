445b33c8515e8c0225c3dcd848cd387690d2f121
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2024-07-10 14:10:12 +0000

    #9874: Update test files with ttnn comparison function

--- tests/ttnn/unit_tests/operations/backward/test_backward_hypot.py
+++ tests/ttnn/unit_tests/operations/backward/test_backward_hypot.py
@@ -5,7 +5,7 @@
 import torch
 import pytest
 import ttnn
-from tests.tt_eager.python_api_testing.unit_testing.backward_ops.utility_funcs import data_gen_with_range, compare_pcc
+from tests.ttnn.unit_tests.operations.backward.utility_funcs import data_gen_with_range, compare_pcc
 
 
 @pytest.mark.parametrize(
