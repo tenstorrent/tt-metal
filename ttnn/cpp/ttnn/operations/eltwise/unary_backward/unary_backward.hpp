93591fb956c78112caee7e7630e01c0ba05481f9
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2024-07-10 07:47:20 +0000

    #9874: Merge frac_bw to TTNN

--- ttnn/cpp/ttnn/operations/eltwise/unary_backward/unary_backward.hpp
+++ ttnn/cpp/ttnn/operations/eltwise/unary_backward/unary_backward.hpp
@@ -74,5 +74,6 @@ constexpr auto add_bw = ttnn::register_operation<operations::unary_backward::Exe
 constexpr auto eq_bw = ttnn::register_operation<operations::unary_backward::ExecuteUnaryBackward<operations::unary_backward::UnaryBackwardOpType::EQ_BW>>("ttnn::eq_bw");
 constexpr auto lgamma_bw = ttnn::register_operation<operations::unary_backward::ExecuteUnaryBackward<operations::unary_backward::UnaryBackwardOpType::LGAMMA_BW>>("ttnn::lgamma_bw");
 constexpr auto sub_bw = ttnn::register_operation<operations::unary_backward::ExecuteUnaryBackward<operations::unary_backward::UnaryBackwardOpType::SUB_BW>>("ttnn::sub_bw");
+constexpr auto frac_bw = ttnn::register_operation<operations::unary_backward::ExecuteUnaryBackward<operations::unary_backward::UnaryBackwardOpType::FRAC_BW>>("ttnn::frac_bw");
 
 }  // namespace ttnn
