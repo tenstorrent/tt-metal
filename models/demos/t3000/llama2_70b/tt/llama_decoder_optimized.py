33920bbcaee03d7e55fead14cf4119e63c71e32c
avoraTT <avora@tenstorrent.com>
2024-07-11 06:47:17 -0700

    #0: Enabling read_cache again because tests ran on CI.

--- models/demos/t3000/llama2_70b/tt/llama_decoder_optimized.py
+++ models/demos/t3000/llama2_70b/tt/llama_decoder_optimized.py
@@ -32,7 +32,6 @@ class TtLlamaDecoder_optimized:
         self.device_mesh = device_mesh
         self.num_devices = device_mesh.get_num_devices()
         self.model_config = model_config
-        read_cache = False
         self.read_cache = read_cache
 
         self.hidden_size = configuration.dim
