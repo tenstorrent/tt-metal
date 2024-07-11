c127e70f30587ff379bc835fbf64c9d5946dc75c
KalaivaniMCW <kbaskar@tenstorrent.com>
2024-07-11 15:38:23 +0000

    #10175: Remove std::function and restructure ternary_bw

--- ttnn/cpp/ttnn/operations/eltwise/ternary_backward/ternary_backward.hpp
+++ ttnn/cpp/ttnn/operations/eltwise/ternary_backward/ternary_backward.hpp
@@ -8,7 +8,6 @@
 #include "device/ternary_backward_op.hpp"
 #include "ttnn/device_operation.hpp"
 #include "ttnn/operations/data_movement.hpp"
-#include "ttnn/operations/eltwise/ternary_backward/device/ternary_backward_op.hpp"
 
 namespace ttnn {
 
@@ -17,7 +16,7 @@ namespace operations::ternary_backward {
 template <TernaryBackwardOpType ternary_backward_op_type>
 struct ExecuteTernaryBackward {
 
-    static inline std::vector<ttnn::Tensor> create_async_output_tensors(
+    static inline std::vector<Tensor> create_async_output_tensors(
         const std::vector<Tensor> &input_tensors, const std::vector<std::optional<const Tensor>>& optional_inputs) {
         const auto& input_tensor = input_tensors.at(0);
         return {Tensor(operation::get_workers_for_op_output({input_tensor})),
@@ -27,32 +26,57 @@ struct ExecuteTernaryBackward {
 
      //Type 0: 3 inputs, 1 grad tensor, 1 float
 
-    static std::vector<ttnn::Tensor> execute_on_worker_thread(
+    static std::vector<Tensor> execute_on_worker_thread(
         const Tensor &grad_tensor_arg,
         const Tensor &input_tensor_a_arg,
         const Tensor &input_tensor_b_arg,
         const Tensor &input_tensor_c_arg,
         const MemoryConfig &memory_config) {
-        auto op_type = TernaryBackwardFunction::get_function_type0(ternary_backward_op_type);
+        auto op_type = get_ternary_fn<ternary_backward_op_type>();
         return op_type(grad_tensor_arg, input_tensor_a_arg, input_tensor_b_arg, input_tensor_c_arg, memory_config);
         }
+};
+
+template <TernaryBackwardOpType ternary_backward_op_type>
+struct ExecuteTernaryBackwardFloat {
+
+    static inline std::vector<Tensor> create_async_output_tensors(
+        const std::vector<Tensor> &input_tensors, const std::vector<std::optional<const Tensor>>& optional_inputs) {
+        const auto& input_tensor = input_tensors.at(0);
+        return {Tensor(operation::get_workers_for_op_output({input_tensor})),
+                                            Tensor(operation::get_workers_for_op_output({input_tensor})),
+                                            Tensor(operation::get_workers_for_op_output({input_tensor}))};
+    }
 
     //Type 1: 3 inputs, 1 grad tensor, 1 float
 
-    static std::vector<ttnn::Tensor> execute_on_worker_thread(
+    static std::vector<Tensor> execute_on_worker_thread(
         const Tensor &grad_tensor_arg,
         const Tensor &input_tensor_a_arg,
         const Tensor &input_tensor_b_arg,
         const Tensor &input_tensor_c_arg,
         float alpha,
         const MemoryConfig &memory_config) {
-        auto op_type = TernaryBackwardFunction::get_function_type(ternary_backward_op_type);
+        auto op_type = get_ternary_fn_float<ternary_backward_op_type>();
         return op_type(grad_tensor_arg, input_tensor_a_arg, input_tensor_b_arg, input_tensor_c_arg, alpha, memory_config);
         }
 
+};
+
+template <TernaryBackwardOpType ternary_backward_op_type>
+struct ExecuteTernaryBackwardOptional {
+
+    static inline std::vector<Tensor> create_async_output_tensors(
+        const std::vector<Tensor> &input_tensors, const std::vector<std::optional<const Tensor>>& optional_inputs) {
+        const auto& input_tensor = input_tensors.at(0);
+        return {Tensor(operation::get_workers_for_op_output({input_tensor})),
+                                            Tensor(operation::get_workers_for_op_output({input_tensor})),
+                                            Tensor(operation::get_workers_for_op_output({input_tensor}))};
+    }
+
     //Q_ID, type1 args, optional output tensor for inputs based on are_required_outputs value
 
-    static std::vector<std::optional<ttnn::Tensor>> execute_on_main_thread(
+    static std::vector<OptionalTensor> execute_on_main_thread(
         uint8_t queue_id,
         const Tensor &grad_tensor_arg,
         const Tensor &input_tensor_a_arg,
@@ -60,29 +84,29 @@ struct ExecuteTernaryBackward {
         const Tensor &input_tensor_c_arg,
         const std::optional<MemoryConfig> &memory_config = std::nullopt,
         const std::vector<bool>& are_required_outputs = std::vector<bool>{true, true},
-        std::optional<Tensor> input_a_grad = std::nullopt,
-        std::optional<Tensor> input_b_grad = std::nullopt) {
+        OptionalTensor input_a_grad = std::nullopt,
+        OptionalTensor input_b_grad = std::nullopt) {
 
         auto output_memory_config = memory_config.value_or(input_tensor_a_arg.memory_config());
-        auto op_type = TernaryBackwardFunction::get_function_type_opt(ternary_backward_op_type);
+        auto op_type = get_ternary_fn_opt_output<ternary_backward_op_type>();
         return op_type(queue_id, grad_tensor_arg, input_tensor_a_arg, input_tensor_b_arg, input_tensor_c_arg, output_memory_config, are_required_outputs, input_a_grad, input_b_grad);
     }
 
     //type1 args, optional output tensor for inputs based on are_required_outputs value
 
-    static std::vector<std::optional<ttnn::Tensor>> execute_on_main_thread(
+    static std::vector<OptionalTensor> execute_on_main_thread(
         const Tensor &grad_tensor_arg,
         const Tensor &input_tensor_a_arg,
         const Tensor &input_tensor_b_arg,
         const Tensor &input_tensor_c_arg,
         const std::optional<MemoryConfig> &memory_config = std::nullopt,
         const std::vector<bool>& are_required_outputs = std::vector<bool>{true, true},
-        std::optional<Tensor> input_a_grad = std::nullopt,
-        std::optional<Tensor> input_b_grad = std::nullopt) {
+        OptionalTensor input_a_grad = std::nullopt,
+        OptionalTensor input_b_grad = std::nullopt) {
 
         auto output_memory_config = memory_config.value_or(input_tensor_a_arg.memory_config());
-        auto op_type = TernaryBackwardFunction::get_function_type_opt_wo_qid(ternary_backward_op_type);
-        return op_type(grad_tensor_arg, input_tensor_a_arg, input_tensor_b_arg, input_tensor_c_arg, output_memory_config, are_required_outputs, input_a_grad, input_b_grad);
+        auto op_type = get_ternary_fn_opt_output<ternary_backward_op_type>();
+        return op_type(DefaultQueueId, grad_tensor_arg, input_tensor_a_arg, input_tensor_b_arg, input_tensor_c_arg, output_memory_config, are_required_outputs, input_a_grad, input_b_grad);
     }
 
 };
@@ -90,8 +114,8 @@ struct ExecuteTernaryBackward {
 }  // operations::ternary_backward
 
 //type 1
-constexpr auto addcmul_bw = ttnn::register_operation<operations::ternary_backward::ExecuteTernaryBackward<operations::ternary_backward::TernaryBackwardOpType::ADDCMUL_BW>>("ttnn::addcmul_bw");
-constexpr auto addcdiv_bw = ttnn::register_operation<operations::ternary_backward::ExecuteTernaryBackward<operations::ternary_backward::TernaryBackwardOpType::ADDCDIV_BW>>("ttnn::addcdiv_bw");
-constexpr auto where_bw = ttnn::register_operation<operations::ternary_backward::ExecuteTernaryBackward<operations::ternary_backward::TernaryBackwardOpType::WHERE_BW>>("ttnn::where_bw");
+constexpr auto addcmul_bw = ttnn::register_operation<operations::ternary_backward::ExecuteTernaryBackwardFloat<operations::ternary_backward::TernaryBackwardOpType::ADDCMUL_BW>>("ttnn::addcmul_bw");
+constexpr auto addcdiv_bw = ttnn::register_operation<operations::ternary_backward::ExecuteTernaryBackwardFloat<operations::ternary_backward::TernaryBackwardOpType::ADDCDIV_BW>>("ttnn::addcdiv_bw");
+constexpr auto where_bw = ttnn::register_operation<operations::ternary_backward::ExecuteTernaryBackwardOptional<operations::ternary_backward::TernaryBackwardOpType::WHERE_BW>>("ttnn::where_bw");
 
 }  // namespace ttnn
