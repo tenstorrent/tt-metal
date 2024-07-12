916a582a670f9675f73798934edaff7d9b477dac
Raymond Kim <rkim@tenstorrent.com>
2024-07-12 22:30:07 +0000

    #256: Move bert above SD so that we can check if it fails on N300

--- tests/scripts/single_card/run_demos_single_card_n300_tests.sh
+++ tests/scripts/single_card/run_demos_single_card_n300_tests.sh
@@ -7,11 +7,11 @@ if [[ -z "$TT_METAL_HOME" ]]; then
   exit 1
 fi
 
+# Not working on N150, working on N300
+unset WH_ARCH_YAML
+pytest -n auto --disable-warnings models/demos/metal_BERT_large_11/demo/demo.py -k batch_7
+rm -rf built
+
 export WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml
 
 source tests/scripts/single_card/run_demos_single_card_n150_tests.sh
-
-# Not working on N150, working on N300
-unset WH_ARCH_YAML
-rm -rf built
-pytest -n auto --disable-warnings models/demos/metal_BERT_large_11/demo/demo.py -k batch_7
