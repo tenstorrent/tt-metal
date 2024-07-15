f6b6a3b5a39829c5a24667d6cd7684248f2e7321
Aswinmcw <azayasankaran@tenstorrent.com>
2024-07-15 09:12:55 +0000

    #9806: Remove std function
    #9806: Remove std function for ops with float param

--- ttnn/cpp/ttnn/operations/eltwise/complex_binary_backward/complex_binary_backward.hpp
+++ ttnn/cpp/ttnn/operations/eltwise/complex_binary_backward/complex_binary_backward.hpp
@@ -14,7 +14,7 @@ namespace ttnn {
 namespace operations::complex_binary_backward {
 
 template <ComplexBinaryBackwardOpType complex_binary_backward_op_type>
-struct ExecuteComplexBinaryBackward {
+struct ExecuteComplexBinaryBackwardWFloat {
 
     //Type 1: 2 inputs, 1 grad tensor 1 float
 
@@ -25,9 +25,14 @@ struct ExecuteComplexBinaryBackward {
         float alpha,
         const MemoryConfig &memory_config) {
 
-        auto op_type = ComplexBinaryBackwardFunction::get_function_type1(complex_binary_backward_op_type);
+        auto op_type = get_function_w_float<complex_binary_backward_op_type>();
         return op_type(grad_tensor_arg, input_tensor_a_arg, input_tensor_b_arg, alpha, memory_config);
         }
+
+};
+
+template <ComplexBinaryBackwardOpType complex_binary_backward_op_type>
+struct ExecuteComplexBinaryBackwardWoFloat {
 
     //Type 1: 2 inputs, 1 grad tensor
 
@@ -35,18 +40,18 @@ struct ExecuteComplexBinaryBackward {
         const ComplexTensor &grad_tensor_arg,
         const ComplexTensor &input_tensor_a_arg,
         const ComplexTensor &input_tensor_b_arg,
-        const MemoryConfig &memory_config) {
+        const MemoryConfig& memory_config) {
 
-        auto op_type = ComplexBinaryBackwardFunction::get_function_type2(complex_binary_backward_op_type);
+        auto op_type = get_function_wo_float<complex_binary_backward_op_type>();
         return op_type(grad_tensor_arg, input_tensor_a_arg, input_tensor_b_arg, memory_config);
         }
 
 };
 
 }
-constexpr auto complex_add_bw = ttnn::register_operation<operations::complex_binary_backward::ExecuteComplexBinaryBackward<operations::complex_binary_backward::ComplexBinaryBackwardOpType::COMPLEX_ADD_BW>>("ttnn::complex_add_bw");
-constexpr auto complex_sub_bw = ttnn::register_operation<operations::complex_binary_backward::ExecuteComplexBinaryBackward<operations::complex_binary_backward::ComplexBinaryBackwardOpType::COMPLEX_SUB_BW>>("ttnn::complex_sub_bw");
-constexpr auto complex_mul_bw = ttnn::register_operation<operations::complex_binary_backward::ExecuteComplexBinaryBackward<operations::complex_binary_backward::ComplexBinaryBackwardOpType::COMPLEX_MUL_BW>>("ttnn::complex_mul_bw");
-constexpr auto complex_div_bw = ttnn::register_operation<operations::complex_binary_backward::ExecuteComplexBinaryBackward<operations::complex_binary_backward::ComplexBinaryBackwardOpType::COMPLEX_DIV_BW>>("ttnn::complex_div_bw");
+constexpr auto complex_add_bw = ttnn::register_operation<operations::complex_binary_backward::ExecuteComplexBinaryBackwardWFloat<operations::complex_binary_backward::ComplexBinaryBackwardOpType::COMPLEX_ADD_BW>>("ttnn::complex_add_bw");
+constexpr auto complex_sub_bw = ttnn::register_operation<operations::complex_binary_backward::ExecuteComplexBinaryBackwardWFloat<operations::complex_binary_backward::ComplexBinaryBackwardOpType::COMPLEX_SUB_BW>>("ttnn::complex_sub_bw");
+constexpr auto complex_mul_bw = ttnn::register_operation<operations::complex_binary_backward::ExecuteComplexBinaryBackwardWoFloat<operations::complex_binary_backward::ComplexBinaryBackwardOpType::COMPLEX_MUL_BW>>("ttnn::complex_mul_bw");
+constexpr auto complex_div_bw = ttnn::register_operation<operations::complex_binary_backward::ExecuteComplexBinaryBackwardWoFloat<operations::complex_binary_backward::ComplexBinaryBackwardOpType::COMPLEX_DIV_BW>>("ttnn::complex_div_bw");
 
 }
