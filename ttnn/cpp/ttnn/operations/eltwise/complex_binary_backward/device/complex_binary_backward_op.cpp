41d73f3129b9def92f4695441ff0f9eadc1031f8
Aswinmcw <azayasankaran@tenstorrent.com>
2024-07-16 06:00:09 +0000

    #9806: Replace ttdnn ops with TTNN

--- ttnn/cpp/ttnn/operations/eltwise/complex_binary_backward/device/complex_binary_backward_op.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/complex_binary_backward/device/complex_binary_backward_op.cpp
@@ -24,7 +24,7 @@ std::vector<ComplexTensor> _complex_add_bw(const ComplexTensor& grad, const Comp
     grad_tensor.emplace_back(grad_a);
     const Tensor& grad_r = grad.real();
     const Tensor& grad_i = grad.imag();
-    ComplexTensor grad_b = ComplexTensor({mul_unary(grad_r, alpha, output_mem_config), mul_unary(grad_i, alpha, output_mem_config)});
+    ComplexTensor grad_b = ComplexTensor({ttnn::multiply(grad_r, alpha, std::nullopt, output_mem_config), ttnn::multiply(grad_i, alpha, std::nullopt, output_mem_config)});
     grad_tensor.emplace_back(grad_b);
     return grad_tensor;
 }
@@ -37,9 +37,12 @@ std::vector<ComplexTensor> _complex_sub_bw(const ComplexTensor& grad, const Comp
     grad_tensor.emplace_back(grad);
     const Tensor& grad_r = grad.real();
     const Tensor& grad_i = grad.imag();
-    UnaryWithParam op1 {UnaryOpType::NEG};
-    UnaryWithParam op2 {UnaryOpType::MUL_UNARY_SFPU, alpha};
-    ComplexTensor grad_b = ComplexTensor({tt::tt_metal::unary_chain( grad_r, {op1, op2}, output_mem_config), tt::tt_metal::unary_chain( grad_i, {op1, op2}, output_mem_config)});
+    using ttnn::operations::unary::UnaryWithParam;
+    using ttnn::operations::unary::UnaryOpType;
+    std::vector<UnaryWithParam> ops_chain = {
+    UnaryWithParam{UnaryOpType::NEG},
+    UnaryWithParam{UnaryOpType::MUL_UNARY_SFPU, alpha} };
+    ComplexTensor grad_b = ComplexTensor({ttnn::unary_chain( grad_r, ops_chain, output_mem_config), ttnn::unary_chain( grad_i, ops_chain, output_mem_config)});
     grad_tensor.emplace_back(grad_b);
     return grad_tensor;
 }
@@ -49,9 +52,9 @@ std::vector<ComplexTensor> _complex_sub_bw(const ComplexTensor& grad, const Comp
 // grad_other = grad * input.conj()
 std::vector<ComplexTensor> _complex_mul_bw(const ComplexTensor& grad, const ComplexTensor& input, const ComplexTensor& other, const MemoryConfig& output_mem_config) {
     std::vector<ComplexTensor> grad_tensor;
-    ComplexTensor grad_a = tt::tt_metal::complex_mul(grad, conj(other,output_mem_config), output_mem_config);
+    ComplexTensor grad_a = complex_mul(grad, conj(other,output_mem_config), output_mem_config);
     grad_tensor.emplace_back(grad_a);
-    ComplexTensor grad_b = tt::tt_metal::complex_mul(grad, conj(input,output_mem_config), output_mem_config);
+    ComplexTensor grad_b = complex_mul(grad, conj(input,output_mem_config), output_mem_config);
     grad_tensor.emplace_back(grad_b);
     return grad_tensor;
 }
@@ -61,19 +64,19 @@ std::vector<ComplexTensor> _complex_mul_bw(const ComplexTensor& grad, const Comp
 //  other: -grad * ((self / other) / other).conj();
 std::vector<ComplexTensor> _complex_div_bw(const ComplexTensor& grad, const ComplexTensor& input, const ComplexTensor& other, const MemoryConfig& output_mem_config) {
     std::vector<ComplexTensor> grad_tensor;
-    Tensor condition_nan = ttnn::logical_and(eqz(other.real(),output_mem_config), eqz(other.imag(),output_mem_config), std::nullopt, output_mem_config);
-    ComplexTensor grad_a = tt::tt_metal::complex_div(grad, conj(other,output_mem_config), output_mem_config);
-    Tensor grad_a_r = where(condition_nan, tt::tt_metal::full_like(grad.real(), std::nanf(""), output_mem_config), real(grad_a,output_mem_config),  output_mem_config);
-    Tensor grad_a_i = where(condition_nan, tt::tt_metal::full_like(grad.imag(), std::nanf(""), output_mem_config), imag(grad_a,output_mem_config),  output_mem_config);
+    Tensor condition_nan = ttnn::logical_and(ttnn::eqz(other.real(),output_mem_config), ttnn::eqz(other.imag(),output_mem_config), std::nullopt, output_mem_config);
+    ComplexTensor grad_a = complex_div(grad, conj(other,output_mem_config), output_mem_config);
+    Tensor grad_a_r = where(condition_nan, ttnn::operations::creation::full_like(grad.real(), std::nanf(""), std::nullopt, std::nullopt, std::nullopt, output_mem_config), real(grad_a,output_mem_config),  output_mem_config);
+    Tensor grad_a_i = where(condition_nan, ttnn::operations::creation::full_like(grad.imag(), std::nanf(""), std::nullopt, std::nullopt, std::nullopt, output_mem_config), imag(grad_a,output_mem_config),  output_mem_config);
     grad_a = ComplexTensor({grad_a_r, grad_a_i});
     grad_a_r.deallocate();
     grad_a_i.deallocate();
     grad_tensor.emplace_back(grad_a);
-    ComplexTensor neg_grad = ComplexTensor({neg(grad.real(),output_mem_config), neg(grad.imag(),output_mem_config)});
-    ComplexTensor grad_b = complex_mul(neg_grad, conj(tt::tt_metal::complex_div(tt::tt_metal::complex_div(input, other, output_mem_config), other, output_mem_config ),output_mem_config), output_mem_config);
+    ComplexTensor neg_grad = ComplexTensor({ttnn::neg(grad.real(),output_mem_config), ttnn::neg(grad.imag(),output_mem_config)});
+    ComplexTensor grad_b = complex_mul(neg_grad, conj(complex_div(complex_div(input, other, output_mem_config), other, output_mem_config ),output_mem_config), output_mem_config);
     neg_grad.deallocate();
-    Tensor grad_b_r = where(condition_nan, tt::tt_metal::full_like(grad.real(), std::nanf(""), output_mem_config), real(grad_b,output_mem_config),  output_mem_config);
-    Tensor grad_b_i = where(condition_nan, tt::tt_metal::full_like(grad.imag(), std::nanf(""), output_mem_config), imag(grad_b,output_mem_config),  output_mem_config);
+    Tensor grad_b_r = where(condition_nan, ttnn::operations::creation::full_like(grad.real(), std::nanf(""), std::nullopt, std::nullopt, std::nullopt, output_mem_config), real(grad_b,output_mem_config),  output_mem_config);
+    Tensor grad_b_i = where(condition_nan, ttnn::operations::creation::full_like(grad.imag(), std::nanf(""), std::nullopt, std::nullopt, std::nullopt, output_mem_config), imag(grad_b,output_mem_config),  output_mem_config);
     grad_b = ComplexTensor({grad_b_r, grad_b_i});
     grad_b_r.deallocate();
     grad_b_i.deallocate();
