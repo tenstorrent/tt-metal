5c788d63bc3e70e08c0ad5bee911bd6d3fda3832
--global <--global>
2024-07-16 20:34:51 +0000

    #7528: add new ethernet microbenchmark, cleanup and re-enable others
    Adds new ping latency microbenchmark which measures the time taken (in
    cycles) for an erisc to send a message (of user specifyable size) to the
    core on the other end of the ethernet link and receive a response.
    
    Other attributes that can be specified are the number of samples to
    measure as well as the number of concurrent channels to send pings over
    (where each channel is a separate, concurrent transaction/packet on the
    same link).
    
    Initial results indicate an almost exactly 1us roundtrip ping time for a
    flit (16B payload + 16B message available tail - not a true flit but
    very close)
    
    Bidirectional bandwidth and ring latency microbenchmarks are also
    re-enabled and improved.
    
    Ring latency improves to about 655ns per hop for an 8 hop ring.
    
    Banwidth is improved to around 23.5GBps for packet size range from 4k to
    16k per packet.
    
    Some samples. Buffer size in bytes is on the left. Number of
    channels/buffers instantiated per direction are on the right.
    So a num channels of 2 indicates 4 channels total (2 in sender direction
    and 2 in receiver).
    
    | Buf   |    |
    | Size  | #  |
    | (B)   | Ch |
    |-------|----|
    | 36864 | 2  |
    | 32768 | 2  |
    | 24576 | 3  |
    | 16384 | 4  |
    | 8192  | 9  |
    | 4096  | 18 |
    | 2048  | 30 |
    | 1024  | 30 |

--- tests/tt_metal/microbenchmarks/ethernet/test_ethernet_link_ping_latency.py
+++ tests/tt_metal/microbenchmarks/ethernet/test_ethernet_link_ping_latency.py
@@ -8,34 +8,42 @@ import sys
 from loguru import logger
 import pytest
 from tt_metal.tools.profiler.process_device_log import import_log_run_stats
-import tt_metal.tools.profiler.device_post_proc_config as device_post_proc_config
+import tt_metal.tools.profiler.device_post_proc_config as device_possetupt_proc_config
 
 
-@pytest.mark.parametrize("sample_counts", [(1024,)])
-@pytest.mark.parametrize("page_sizes", [(16,)])
-@pytest.mark.parametrize("channel_counts", [(1,)])
-@pytest.mark.parametrize("hop_counts", [(8,)])
-def test_multichip_hop_latency(sample_counts, page_sizes, channel_counts, hop_counts):
+@pytest.mark.parametrize("sample_counts", [(1024,)])  # , 8, 16, 64, 256],
+@pytest.mark.parametrize(
+    "sample_sizes",
+    [
+        (
+            16,
+            16384,
+        )
+    ],
+)  # , 1024, 2048, 4096],
+@pytest.mark.parametrize(
+    "channel_counts",
+    [(1,)],
+)
+def test_bidirectional_erisc_bandwidth(sample_counts, sample_sizes, channel_counts):
     test_string_name = f"test_ethernet_send_data_microbenchmark - \
             sample_counts: {sample_counts}, \
-                page_sizes: {page_sizes}, \
-                    channel_counts: {channel_counts}, \
-                        hop_counts: {hop_counts}"
+                sample_sizes: {sample_sizes}, \
+                    channel_counts: {channel_counts}"
     print(f"{test_string_name}")
     os.system(f"rm -rf {os.environ['TT_METAL_HOME']}/generated/profiler/.logs/profile_log_device.csv")
 
     sample_counts_str = " ".join([str(s) for s in sample_counts])
-    page_sizes_str = " ".join([str(s) for s in page_sizes])
+    sample_sizes_str = " ".join([str(s) for s in sample_sizes])
     channel_counts_str = " ".join([str(s) for s in channel_counts])
-    hop_counts_str = " ".join([str(s) for s in hop_counts])
 
     rc = os.system(
         f"TT_METAL_DEVICE_PROFILER=1 \
-            ./build/test/tt_metal/perf_microbenchmark/ethernet/test_ethernet_hop_latencies_no_edm \
+            {os.environ['TT_METAL_HOME']}/build/test/tt_metal/perf_microbenchmark/ethernet/test_ethernet_link_ping_latency_no_edm \
                 {len(sample_counts)} {sample_counts_str} \
-                    {len(page_sizes)} {page_sizes_str} \
+                    {len(sample_sizes)} {sample_sizes_str} \
                         {len(channel_counts)} {channel_counts_str} \
-                            {len(hop_counts)} {hop_counts_str}"
+            "
     )
     if rc != 0:
         print("Error in running the test")
