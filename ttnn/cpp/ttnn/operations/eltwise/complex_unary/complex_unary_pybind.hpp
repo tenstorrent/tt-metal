dfa8cf480294a90e1231670ed2d536d74b30e620
Aswinmcw <azayasankaran@tenstorrent.com>
2024-07-17 13:06:55 +0000

    #10380: Overload complex abs

--- ttnn/cpp/ttnn/operations/eltwise/complex_unary/complex_unary_pybind.hpp
+++ ttnn/cpp/ttnn/operations/eltwise/complex_unary/complex_unary_pybind.hpp
@@ -86,11 +86,6 @@ void py_module(py::module& module) {
         ttnn::is_real,
         R"doc(Returns boolean tensor if value of :attr:`input_tensor` is real.)doc");
 
-    detail::bind_complex_unary_type1(
-        module,
-        ttnn::complex_abs,
-        R"doc(Returns absolute value of complex :attr:`input_tensor`.)doc");
-
 }
 
 }  // namespace complex_unary
