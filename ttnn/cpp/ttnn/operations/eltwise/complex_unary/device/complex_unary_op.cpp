f0ff75df58081ff2fbc8cc354712589cd89cb995
Aswinmcw <azayasankaran@tenstorrent.com>
2024-07-17 09:01:25 +0000

    #10380: Migrate real to TTNN with generalized structure

---
+++ ttnn/cpp/ttnn/operations/eltwise/complex_unary/device/complex_unary_op.cpp
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+
+#include "third_party/magic_enum/magic_enum.hpp"
+
+#include "tt_eager/tt_dnn/op_library/bcast/bcast_op.hpp"
+#include "tt_eager/tt_dnn/op_library/eltwise_unary/eltwise_unary_op.hpp"
+#include "tt_eager/tt_dnn/op_library/composite/composite_ops.hpp"
+#include "tt_metal/common/constants.hpp"
+#include "tt_metal/host_api.hpp"
+#include "tt_metal/tools/profiler/op_profiler.hpp"
+#include "ttnn/cpp/ttnn/operations/eltwise/binary/binary.hpp"
+#include "tt_eager/tt_dnn/op_library/complex/complex_ops.hpp"
+
+namespace ttnn::operations::complex_unary {
+
+Tensor _real(const ComplexTensor& input, const MemoryConfig& output_mem_config) {
+    return input[0];
+}
+
+}  // namespace ttnn::operations::complex_unary
---
+++ ttnn/cpp/ttnn/operations/eltwise/complex_unary/device/complex_unary_op.hpp
@@ -0,0 +1,39 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <functional>
+#include <optional>
+#include "tensor/tensor.hpp"
+#include "third_party/magic_enum/magic_enum.hpp"
+#include "tt_eager/tt_dnn/op_library/complex/complex_ops.hpp"
+
+namespace ttnn::operations::complex_unary {
+
+constexpr uint8_t DefaultQueueId = 0;
+enum class ComplexUnaryOpType {
+    REAL,
+};
+
+Tensor _real(const ComplexTensor& input, const MemoryConfig& output_mem_config);
+
+template <ComplexUnaryOpType OpType>
+struct OpHandler_complex_type1;
+
+template <>
+struct OpHandler_complex_type1<ComplexUnaryOpType::REAL> {
+    static Tensor handle( const ComplexTensor& input, const MemoryConfig& output_mem_config ) {
+        return _real(input, output_mem_config);
+    }
+};
+
+
+template <ComplexUnaryOpType OpType>
+auto get_function_type1() {
+    return &OpHandler_complex_type1<OpType>::handle;
+}
+
+
+}  // namespace ttnn::operations::complex_unary
