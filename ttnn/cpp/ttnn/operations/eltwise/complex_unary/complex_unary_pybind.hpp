70bc317a4ade122512d600424ca2c91f20ee1d6b
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2024-07-17 11:39:46 +0000

    #10380: Update docs

--- ttnn/cpp/ttnn/operations/eltwise/complex_unary/complex_unary_pybind.hpp
+++ ttnn/cpp/ttnn/operations/eltwise/complex_unary/complex_unary_pybind.hpp
@@ -23,25 +23,20 @@ namespace detail {
 template <typename complex_unary_operation_t>
 void bind_complex_unary_type1(py::module& module, const complex_unary_operation_t& operation, const std::string& description) {
     auto doc = fmt::format(
-R"doc({0}(grad_tensor: ttnn.Tensor, input_tensor_a: ttnn.Tensor, input_tensor_b: ttnn.Tensor, alpha: float, *, memory_config: ttnn.MemoryConfig) -> Tensor
+R"doc({0}(input_tensor: ttnn.Tensor, *, memory_config: ttnn.MemoryConfig) -> Tensor
 
 {2}
 
 Args:
-    * :attr:`grad_tensor`
-    * :attr:`input_tensor_a`
-    * :attr:`input_tensor_b`
-    * :attr:`alpha`
+    * :attr:`input_tensor`
 
 Keyword args:
     * :attr:`memory_config` (Optional[ttnn.MemoryConfig]): memory config for the output tensor
 
 Example:
 
-    >>> grad_tensor = ttnn.to_device(ttnn.from_torch(torch.tensor((1, 2), dtype=torch.bfloat16)), device)
-    >>> tensor1 = ttnn.to_device(ttnn.from_torch(torch.tensor((1, 2), dtype=torch.bfloat16)), device)
-    >>> tensor2 = ttnn.to_device(ttnn.from_torch(torch.tensor((0, 1), dtype=torch.bfloat16)), device)
-    >>> output = {1}(grad_tensor, tensor1, tensor2, alpha)
+    >>> tensor = ttnn.to_device(ttnn.from_torch(torch.tensor((0, 1), dtype=torch.bfloat16)), device)
+    >>> output = {1}(tensor)
 )doc",
         operation.base_name(),
         operation.python_fully_qualified_name(),
@@ -69,27 +64,27 @@ void py_module(py::module& module) {
     detail::bind_complex_unary_type1(
         module,
         ttnn::real,
-        R"doc(Performs complex operations for real of :attr:`input_tensor_a`.)doc");
+        R"doc(Performs complex operations for real of :attr:`input_tensor`.)doc");
 
     detail::bind_complex_unary_type1(
         module,
         ttnn::imag,
-        R"doc(Performs complex operations for imag of :attr:`input_tensor_a`.)doc");
+        R"doc(Performs complex operations for imag of :attr:`input_tensor`.)doc");
 
     detail::bind_complex_unary_type1(
         module,
         ttnn::angle,
-        R"doc(Performs complex operations for angle of :attr:`input_tensor_a`.)doc");
+        R"doc(Performs complex operations for angle of :attr:`input_tensor`.)doc");
 
     detail::bind_complex_unary_type1(
         module,
         ttnn::is_imag,
-        R"doc(Returns boolean tensor if value of :attr:`input_tensor_a` is imag)doc");
+        R"doc(Returns boolean tensor if value of :attr:`input_tensor` is imag)doc");
 
     detail::bind_complex_unary_type1(
         module,
         ttnn::is_real,
-        R"doc(Returns boolean tensor if value of :attr:`input_tensor_a` is real)doc");
+        R"doc(Returns boolean tensor if value of :attr:`input_tensor` is real)doc");
 
 }
 
