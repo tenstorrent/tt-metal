f5fa66b1fcbcb6303331898299151c192b1bfa77
Allan Liu <aliu@tenstorrent.com>
2024-07-18 22:44:57 +0000

    #7426: change semaphore api to return id

--- tt_metal/impl/buffers/semaphore.cpp
+++ tt_metal/impl/buffers/semaphore.cpp
@@ -8,24 +8,32 @@ namespace tt {
 
 namespace tt_metal {
 
-Semaphore::Semaphore(const Semaphore &other) : core_range_set_(other.core_range_set_), address_(other.address_), initial_value_(other.initial_value_), core_type_(other.core_type_) {}
+Semaphore::Semaphore(const Semaphore &other) :
+    core_range_set_(other.core_range_set_),
+    id_(other.id_),
+    initial_value_(other.initial_value_),
+    core_type_(other.core_type_) {}
 
 Semaphore &Semaphore::operator=(const Semaphore &other) {
     if (this != &other) {
         this->core_range_set_ = other.core_range_set_;
-        this->address_ = other.address_;
+        this->id_ = other.id_;
         this->initial_value_ = other.initial_value_;
         this->core_type_ = other.core_type_;
     }
     return *this;
 }
 
-Semaphore::Semaphore(Semaphore &&other) : core_range_set_(other.core_range_set_), address_(other.address_), initial_value_(other.initial_value_), core_type_(other.core_type_) {}
+Semaphore::Semaphore(Semaphore &&other) :
+    core_range_set_(other.core_range_set_),
+    id_(other.id_),
+    initial_value_(other.initial_value_),
+    core_type_(other.core_type_) {}
 
 Semaphore &Semaphore::operator=(Semaphore &&other) {
     if (this != &other) {
         this->core_range_set_ = other.core_range_set_;
-        this->address_ = other.address_;
+        this->id_ = other.id_;
         this->initial_value_ = other.initial_value_;
         this->core_type_ = other.core_type_;
     }
@@ -34,6 +42,13 @@ Semaphore &Semaphore::operator=(Semaphore &&other) {
 
 bool Semaphore::initialized_on_logical_core(const CoreCoord &logical_core) const {
     return this->core_range_set_.core_coord_in_core_ranges(logical_core);
+}
+
+uint32_t Semaphore::address() const {
+    // TODO: need to modify this for ring buffer changes
+    // TODO: no support for active eth cores, where the base addr is eth_l1_mem::address_map::SEMAPHORE_BASE
+    uint32_t address = SEMAPHORE_BASE + (L1_ALIGNMENT * id_);
+    return address;
 }
 
 }  // namespace tt_metal
