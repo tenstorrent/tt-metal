8a7e45088623216982153fb8468caa5586564116
amalbasaTT <amalbasa@tenstorrent.com>
2024-07-19 09:30:23 +0000

    #10147: Migrted addcdiv_bw to ttnn and fixed addcmul_bw op_map name

--- tests/ttnn/python_api_testing/sweep_tests/op_map.py
+++ tests/ttnn/python_api_testing/sweep_tests/op_map.py
@@ -693,6 +693,7 @@ op_map = {
         "tt_op": ttnn_ops.eltwise_relu_max,
         "pytorch_op": pytorch_ops.relu_max,
     },
+<<<<<<< HEAD
     "unpad": {
         "tt_op": ttnn_ops.unpad,
         "pytorch_op": pytorch_ops.unpad,
@@ -756,5 +757,13 @@ op_map = {
     "sqrt-bw": {
         "tt_op": ttnn_ops.sqrt_bw,
         "pytorch_op": pytorch_ops.sqrt_bw,
+    }
+    "addcmul-bw": {
+        "tt_op": ttnn_ops.addcmul_bw,
+        "pytorch_op": pytorch_ops.addcmul_bw,
+    },
+    "addcdiv-bw": {
+        "tt_op": ttnn_ops.addcdiv_bw,
+        "pytorch_op": pytorch_ops.addcdiv_bw,
     },
 }
