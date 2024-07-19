f25edc5570baa282e99f363d59fb59036e2566cf
amalbasaTT <amalbasa@tenstorrent.com>
2024-07-19 14:45:07 +0000

    #10147: Minor fixes

--- tests/ttnn/python_api_testing/sweep_tests/ttnn_ops.py
+++ tests/ttnn/python_api_testing/sweep_tests/ttnn_ops.py
@@ -3368,9 +3368,8 @@ def unary_add_bw(
 ):
     t0 = setup_ttnn_tensor(x, device, layout[0], input_mem_config[0], dtype[0])
     t1 = setup_ttnn_tensor(y, device, layout[1], input_mem_config[1], dtype[1])
-
     t3 = ttnn.add_bw(t0, t1, alpha=scalar, memory_config=output_mem_config)
-    
+
     return ttnn_tensor_to_torch(t3[0])
 
 
