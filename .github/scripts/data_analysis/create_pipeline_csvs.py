d067274b601b70a9789f951a42372347a43c80be
Raymond Kim <109366641+tt-rkim@users.noreply.github.com>
2024-07-22 12:49:11 -0400

    #9319: Use runner environment variables instead of github context  (#10528)
    * #9319: Use runner environment variables instead of github context as it can sometimes fail because of commit messages with bad characters
    
    * #0: Put ansible back down to 6.7.0
    
    * #9319: Use proper variable

--- .github/scripts/data_analysis/create_pipeline_csvs.py
+++ .github/scripts/data_analysis/create_pipeline_csvs.py
@@ -1,12 +1,12 @@
-from infra.data_collection.github.utils import create_csvs_for_data_analysis
+from infra.data_collection.github.utils import create_csvs_for_data_analysis, get_github_runner_environment
 
 if __name__ == "__main__":
-    github_context_json_filename = "github_context.json"
+    github_runner_environment = get_github_runner_environment()
     github_pipeline_json_filename = "workflow.json"
     github_jobs_json_filename = "workflow_jobs.json"
 
     create_csvs_for_data_analysis(
-        github_context_json_filename,
+        github_runner_environment,
         github_pipeline_json_filename,
         github_jobs_json_filename,
     )
