387e57b51f93e73e414a1676b51d92add4a4c68a
Raymond Kim <rkim@tenstorrent.com>
2024-07-22 16:31:28 +0000

    #9319: Use runner environment variables instead of github context as it can sometimes fail because of commit messages with bad characters

--- .github/scripts/data_analysis/create_pipeline_csvs.py
+++ .github/scripts/data_analysis/create_pipeline_csvs.py
@@ -1,7 +1,7 @@
-from infra.data_collection.github.utils import create_csvs_for_data_analysis
+from infra.data_collection.github.utils import create_csvs_for_data_analysis, get_github_runner_environment
 
 if __name__ == "__main__":
-    github_context_json_filename = "github_context.json"
+    github_runner_environment = get_github_runner_environment()
     github_pipeline_json_filename = "workflow.json"
     github_jobs_json_filename = "workflow_jobs.json"
 
