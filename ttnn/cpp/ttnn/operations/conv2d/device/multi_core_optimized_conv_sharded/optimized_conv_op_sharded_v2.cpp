dd989f33d9b10fa1a43d5ffbb2ad3c8c1e8cee24
kpaigwar <kpaigwar@tenstorrent.com>
2024-07-23 10:46:37 -0400

    #0: moved conv1d kernels

--- ttnn/cpp/ttnn/operations/conv2d/device/multi_core_optimized_conv_sharded/optimized_conv_op_sharded_v2.cpp
+++ ttnn/cpp/ttnn/operations/conv2d/device/multi_core_optimized_conv_sharded/optimized_conv_op_sharded_v2.cpp
@@ -1086,13 +1086,13 @@ operation::ProgramWithCallbacks multi_core_optimized_conv_sharded_v2_impl(
             TT_ASSERT(act_block_w_datums == round_up(conv_act_size_c * weight_size_w, TILE_WIDTH));
             TT_ASSERT(split_reader == false, "Split reader not supported for this conv yet!");
 
-            compute_kernel = "ttnn/cpp/ttnn/deprecated/tt_dnn/op_library/conv/kernels/compute_depthwise_conv1d.cpp";
+            compute_kernel = "ttnn/cpp/ttnn/operations/conv2d/device/kernels/compute_depthwise_conv1d.cpp";
             reader_kernel =
-                "ttnn/cpp/ttnn/deprecated/tt_dnn/op_library/conv/kernels/reader_depthwise_conv1d.cpp";
+                "ttnn/cpp/ttnn/operations/conv2d/device/kernels/reader_depthwise_conv1d.cpp";
             writer_mcast_sender_kernel =
-                "ttnn/cpp/ttnn/deprecated/tt_dnn/op_library/conv/kernels/writer_mcast_sender_depthwise_conv1d.cpp";
+                "ttnn/cpp/ttnn/operations/conv2d/device/kernels/writer_mcast_sender_depthwise_conv1d.cpp";
             writer_mcast_receiver_kernel =
-                "ttnn/cpp/ttnn/deprecated/tt_dnn/op_library/conv/kernels/writer_mcast_receiver_depthwise_conv1d.cpp";
+                "ttnn/cpp/ttnn/operations/conv2d/device/kernels/writer_mcast_receiver_depthwise_conv1d.cpp";
 
         } else {
             // 1D conv
