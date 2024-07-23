8a6b7e9753dd3cc8c0c9dafccc2c36408b610fe3
mtairum <mtairum@tenstorrent.com>
2024-07-23 14:48:25 +0000

    #5337: Moved Mistral tests between resnet50 and Mamba

--- tests/scripts/single_card/nightly/run_wh_b0_only.sh
+++ tests/scripts/single_card/nightly/run_wh_b0_only.sh
@@ -9,7 +9,10 @@ fi
 fail=0
 
 echo "Running nightly tests for WH B0 only"
-env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto tests/nightly/wh_b0_only_eth ; fail+=$?
+env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto tests/nightly/wh_b0_only_eth/models/demos/ttnn_resnet/ ; fail+=$?
+env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto tests/nightly/wh_b0_only_eth/models/demos/wormhole/mistral7b/ ; fail+=$?
+env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto tests/nightly/wh_b0_only_eth/models/demos/wormhole/mamba/ ; fail+=$?
+# env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto tests/nightly/wh_b0_only_eth ; fail+=$?
 env pytest -n auto tests/nightly/wh_b0_only ; fail+=$?
 
 if [[ $fail -ne 0 ]]; then
