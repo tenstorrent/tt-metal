bbd57a1f8843bd651e341c4a8ad01009cdb35bcb
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2024-07-24 13:30:24 -0400

    #0: removed RankedShape

--- ttnn/cpp/ttnn/operations/data_movement.hpp
+++ ttnn/cpp/ttnn/operations/data_movement.hpp
@@ -80,7 +80,7 @@ struct Repeat {
         const ttnn::Shape& shape,
         std::optional<MemoryConfig> output_mem_config = std::nullopt) {
         MemoryConfig mem_config = output_mem_config.value_or(input_tensor.memory_config());
-        auto output_tensor = tt::tt_metal::repeat(input_tensor, shape.value(), mem_config);
+        auto output_tensor = tt::tt_metal::repeat(input_tensor, shape.value, mem_config);
         return output_tensor;
     }
 };
