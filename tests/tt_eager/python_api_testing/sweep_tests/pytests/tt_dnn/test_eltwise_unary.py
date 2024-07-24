fe2d1f0c22bf87ffd910334b6475692d15bbffcc
Andrija Malbasa <amalbasa@tenstorrent.com>
2024-07-24 17:22:53 +0200

    Migration of rsub yaml files and api calls to ttnn (#10656)
    #10147: Migrate rsub yaml files and api calls to ttnn

--- tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_eltwise_unary.py
+++ tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_eltwise_unary.py
@@ -1047,7 +1047,7 @@ class TestEltwiseUnary:
             device,
         )
 
-    @pytest.mark.parametrize("fn_kind", ["rdiv"])
+    @pytest.mark.parametrize("fn_kind", ["rsub", "rdiv"])
     def test_run_eltwise_reverse_ops(
         self,
         input_shapes,
@@ -1057,7 +1057,8 @@ class TestEltwiseUnary:
         input_mem_config,
         output_mem_config,
     ):
-        values = {"rdiv": (1.0, 100.0)}
+        values = {"rdiv": (1.0, 100.0), "rsub": (1.0, 50.0)}
+
         low_v, high_v = values[fn_kind]
         datagen_func = [
             generation_funcs.gen_func_with_cast(
@@ -1084,7 +1085,7 @@ class TestEltwiseUnary:
             ttnn_op=True,
         )
 
-    @pytest.mark.parametrize("fn_kind", ["rdiv"])
+    @pytest.mark.parametrize("fn_kind", ["rsub", "rdiv"])
     def test_run_eltwise_reverse_neg_ops(
         self,
         input_shapes,
@@ -1094,7 +1095,8 @@ class TestEltwiseUnary:
         input_mem_config,
         output_mem_config,
     ):
-        values = {"rdiv": (1.0, 100.0)}
+        values = {"rdiv": (1.0, 100.0), "rsub": (1.0, 50.0)}
+
         low_v, high_v = values[fn_kind]
         datagen_func = [
             generation_funcs.gen_func_with_cast(
@@ -1102,6 +1104,7 @@ class TestEltwiseUnary:
                 torch.bfloat16,
             )
         ]
+
         comparison_func = comparison_funcs.comp_pcc
         test_args = generation_funcs.gen_default_dtype_layout_device(input_shapes)[0]
         test_args.update(
