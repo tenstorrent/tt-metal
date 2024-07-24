7c94ef398cc6b01dbc76a757dfc3a92374edde3c
athul-bos-semi <athulshibu@bos-semi.com>
2024-07-24 16:47:45 +0900

    Created Pybind for test_ops

---
+++ ttnn/ttnn/operations/test_ops/test_ops_pybind.hpp
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: © 2024 BOS
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <pybind11/pybind11.h>
+
+#include "ttnn/operations/test_ops/Concat/concat_pybind.hpp"
+
+namespace ttnn::operations::test_ops {
+
+void py_module(py::module& module) { 
+    detail::bind_test_concat(module, ttnn::test_concat); 
+}
+
+}
