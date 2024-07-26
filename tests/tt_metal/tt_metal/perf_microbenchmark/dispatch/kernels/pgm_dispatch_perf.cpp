878a85817e75d86faa973791bd39de0a772d9d40
Paul Keller <pkeller@tenstorrent.com>
2024-07-26 15:37:13 +0000

    #4984: Add multiple spans test to test_pgm_dispatch

--- tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/kernels/pgm_dispatch_perf.cpp
+++ tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/kernels/pgm_dispatch_perf.cpp
@@ -7,6 +7,10 @@
 uint8_t data1[KERNEL_BYTES-30] __attribute__ ((section ("l1_data"))) __attribute__((used));
 #endif
 
+#ifdef KERNEL_GLOBAL
+volatile uint32_t global = 4;
+#endif
+
 #ifdef COMPILE_FOR_TRISC
 #include "compute_kernel_api/common.h"
 namespace NAMESPACE {
