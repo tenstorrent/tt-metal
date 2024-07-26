0bd51a245212d7a5ef5d15f17796c298d56700f9
Jacob DeSousa <jdesousa@tenstorrent.com>
2024-07-26 16:45:41 +0000

    #8957: Change sweep serialization to use tt::stl::json bindings

--- tests/sweep_framework/serialize.py
+++ tests/sweep_framework/serialize.py
@@ -8,29 +8,8 @@ from statuses import VectorValidity
 
 
 def serialize(object):
-    if "attribute_names" in dir(object) and "attribute_values" in dir(object):
-        serialized_object = dict()
-        serialized_object["type"] = str(type(object)).split("'")[1]
-        attr_names = object.attribute_names
-        attr_values = object.attribute_values()
-        for i in range(len(attr_names)):
-            if attr_names[i] == "shard_spec":
-                if attr_values[i] == None:
-                    continue
-            serialized_object[attr_names[i]] = serialize(attr_values[i])
-        return serialized_object
-    elif type(object) == ttnn.CoreRange:
-        serialized_object = dict()
-        serialized_object["type"] = "tt_lib.tensor.CoreRange"
-        serialized_object["start"] = f"ttnn.CoreCoord({object.start.x}, {object.start.y})"
-        serialized_object["end"] = f"ttnn.CoreCoord({object.end.x}, {object.end.y})"
-        return serialized_object
-    elif type(object) == ttnn.CoreRangeSet:
-        serialized_object = dict()
-        serialized_object["type"] = "tt_lib.tensor.CoreRangeSet"
-        core_ranges = [serialize(core_set) for core_set in object.core_ranges()]
-        serialized_object["core_ranges"] = core_ranges
-        return serialized_object
+    if "to_json" in dir(object):
+        return {"type": str(type(object)).split("'")[1], "data": object.to_json()}
     else:
         return str(object)
 
@@ -38,16 +17,7 @@ def serialize(object):
 def deserialize(object):
     if isinstance(object, dict):
         type = eval(object["type"])
-        object.pop("type")
-        if type == ttnn.CoreRangeSet:
-            core_ranges = set()
-            for core_range in object["core_ranges"]:
-                core_ranges.add(deserialize(core_range))
-            return ttnn.CoreRangeSet(core_ranges)
-        deserialized_parameters = dict()
-        for elem in object:
-            deserialized_parameters[elem] = deserialize(object[elem])
-        return type(**deserialized_parameters)
+        return type.from_json(object["data"])
     else:
         try:
             return eval(object)
