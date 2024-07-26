1e229f9c773d6eb1887e21126d9c97d0210e63c2
Sankar Manoj <167172568+sankarmanoj-tt@users.noreply.github.com>
2024-07-26 16:05:39 +0530

    #0: Fixed module loader for python3.12 (#10432)
    * #0: Fixed module loader for python3.12

--- ttnn/ttnn/operations/__init__.py
+++ ttnn/ttnn/operations/__init__.py
@@ -8,5 +8,5 @@ __all__ = []
 
 for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
     __all__.append(module_name)
-    _module = loader.find_module(module_name).load_module(module_name)
+    _module = loader.find_spec(module_name).loader.load_module(module_name)
     globals()[module_name] = _module
