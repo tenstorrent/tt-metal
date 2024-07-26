79c292ba48292bd3c07d89225b6aa95c4dea8ec8
keerthana-r-mcw <keerthana.rajendran@multicorewareinc.com>
2024-07-26 08:06:37 +0000

    #11187: Yolov4 on WH

---
+++ tests/ttnn/integration_tests/yolov4/test_ttnn_downsample2.py
@@ -0,0 +1,56 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+import ttnn
+from tests.ttnn.utils_for_testing import assert_with_pcc
+import pytest
+import time
+from models.utility_functions import skip_for_grayskull
+from models.experimental.yolov4.reference.downsample2 import DownSample2
+from models.experimental.yolov4.ttnn.downsample2 import Down2
+from loguru import logger
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 16384}], indirect=True)
+def test_down2(device, reset_seeds, model_location_generator):
+    model_path = model_location_generator("models", model_subdir="Yolo")
+
+    if model_path == "models":
+        pytest.skip(
+            "Requires weights file to be downloaded from https://drive.google.com/file/d/1wv_LiFeCRYwtpkqREPeI13-gPELBDwuJ/view"
+        )
+    else:
+        weights_pth = str(model_path / "yolov4.pth")
+
+    ttnn_model = Down2(weights_pth)
+
+    torch_input = torch.randn((1, 160, 160, 64), dtype=torch.bfloat16)
+    ttnn_input = ttnn.from_torch(torch_input, dtype=ttnn.bfloat16)
+    torch_input = torch_input.permute(0, 3, 1, 2).float()
+    torch_model = DownSample2()
+
+    new_state_dict = {}
+    ds_state_dict = {k: v for k, v in ttnn_model.torch_model.items() if (k.startswith("down2."))}
+
+    keys = [name for name, parameter in torch_model.state_dict().items()]
+    values = [parameter for name, parameter in ds_state_dict.items()]
+    for i in range(len(keys)):
+        new_state_dict[keys[i]] = values[i]
+
+    torch_model.load_state_dict(new_state_dict)
+    torch_model.eval()
+
+    result_ttnn = ttnn_model(device, ttnn_input)
+
+    start_time = time.time()
+    for x in range(2):
+        result_ttnn = ttnn_model(device, ttnn_input)
+    logger.info(f"Time taken: {time.time() - start_time}")
+    result = ttnn.to_torch(result_ttnn)
+    ref = torch_model(torch_input)
+    ref = ref.permute(0, 2, 3, 1)
+    result = result.reshape(ref.shape)
+    assert_with_pcc(result, ref, 0.99)
---
+++ tests/ttnn/integration_tests/yolov4/test_ttnn_neck.py
@@ -0,0 +1,79 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+import ttnn
+from models.experimental.yolov4.ttnn.neck import TtNeck
+from models.experimental.yolov4.reference.neck import Neck
+from tests.ttnn.utils_for_testing import assert_with_pcc
+import pytest
+import time
+from loguru import logger
+
+
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 16384}], indirect=True)
+def test_neck(device, reset_seeds, model_location_generator):
+    model_path = model_location_generator("models", model_subdir="Yolo")
+
+    if model_path == "models":
+        pytest.skip(
+            "Requires weights file to be downloaded from https://drive.google.com/file/d/1wv_LiFeCRYwtpkqREPeI13-gPELBDwuJ/view"
+        )
+    else:
+        weights_pth = str(model_path / "yolov4.pth")
+
+    ttnn_model = TtNeck(weights_pth)
+
+    torch_input_tensor1 = torch.randn(1, 10, 10, 1024, dtype=torch.bfloat16)
+    torch_input_tensor2 = torch.randn(1, 20, 20, 512, dtype=torch.bfloat16)
+    torch_input_tensor3 = torch.randn(1, 40, 40, 256, dtype=torch.bfloat16)
+    ttnn_input_tensor1 = ttnn.from_torch(torch_input_tensor1, dtype=ttnn.bfloat16)
+    ttnn_input_tensor1 = ttnn.reshape(ttnn_input_tensor1, (1, 1, 100, 1024))
+    ttnn_input_tensor1 = ttnn.to_layout(ttnn_input_tensor1, layout=ttnn.TILE_LAYOUT)
+    ttnn_input_tensor1 = ttnn.to_device(ttnn_input_tensor1, device=device)
+    ttnn_input_tensor2 = ttnn.from_torch(torch_input_tensor2, dtype=ttnn.bfloat16)
+    ttnn_input_tensor2 = ttnn.reshape(ttnn_input_tensor2, (1, 1, 400, 512))
+    ttnn_input_tensor2 = ttnn.to_layout(ttnn_input_tensor2, layout=ttnn.TILE_LAYOUT)
+    ttnn_input_tensor2 = ttnn.to_device(ttnn_input_tensor2, device=device)
+    ttnn_input_tensor3 = ttnn.from_torch(torch_input_tensor3, dtype=ttnn.bfloat16)
+    ttnn_input_tensor3 = ttnn.reshape(ttnn_input_tensor3, (1, 1, 1600, 256))
+    ttnn_input_tensor3 = ttnn.to_layout(ttnn_input_tensor3, layout=ttnn.TILE_LAYOUT)
+    ttnn_input_tensor3 = ttnn.to_device(ttnn_input_tensor3, device=device)
+    ttnn_input_tensor = [ttnn_input_tensor1, ttnn_input_tensor2, ttnn_input_tensor3]
+    torch_input_tensor1 = torch_input_tensor1.permute(0, 3, 1, 2).float()
+    torch_input_tensor2 = torch_input_tensor2.permute(0, 3, 1, 2).float()
+    torch_input_tensor3 = torch_input_tensor3.permute(0, 3, 1, 2).float()
+    torch_input_tensor = [torch_input_tensor1, torch_input_tensor2, torch_input_tensor3]
+    torch_model = Neck()
+
+    new_state_dict = {}
+    ds_state_dict = {k: v for k, v in ttnn_model.torch_model.items() if (k.startswith("neek."))}
+
+    keys = [name for name, parameter in torch_model.state_dict().items()]
+    values = [parameter for name, parameter in ds_state_dict.items()]
+    for i in range(len(keys)):
+        new_state_dict[keys[i]] = values[i]
+
+    torch_model.load_state_dict(new_state_dict)
+    torch_model.eval()
+
+    result_ttnn = ttnn_model(device, ttnn_input_tensor)
+    start_time = time.time()
+    for x in range(2):
+        result_ttnn = ttnn_model(device, ttnn_input_tensor)
+    logger.info(f"Time taken: {time.time() - start_time}")
+
+    result_1 = ttnn.to_torch(result_ttnn[0])
+    result_2 = ttnn.to_torch(result_ttnn[1])
+    result_3 = ttnn.to_torch(result_ttnn[2])
+    ref1, ref2, ref3 = torch_model(torch_input_tensor[0], torch_input_tensor[1], torch_input_tensor[2])
+    ref1 = ref1.permute(0, 2, 3, 1)
+    ref2 = ref2.permute(0, 2, 3, 1)
+    ref3 = ref3.permute(0, 2, 3, 1)
+    result1 = result_1.reshape(ref1.shape)
+    result2 = result_2.reshape(ref2.shape)
+    result3 = result_3.reshape(ref3.shape)
+    assert_with_pcc(result1, ref1, 0.94)  # PCC = 0.94
+    assert_with_pcc(result2, ref2, 0.99)  # PCC = 0.99
+    assert_with_pcc(result3, ref3, 0.96)  # PCC = 0.96
