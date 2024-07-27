39f0858ecc4cda33e8198355dc5bee58af4ad750
Artem Yerofieiev <169092593+ayerofieiev-tt@users.noreply.github.com>
2024-07-27 09:35:35 -0700

    #0: Update pad.hpp

--- ttnn/cpp/ttnn/operations/data_movement/pad/pad.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/pad/pad.hpp
@@ -33,7 +33,7 @@ struct ExecutePad {
     // Any rank tensor supported
     static ttnn::Tensor operator()(uint8_t queue_id,
                                    const ttnn::Tensor& input_tensor,
-                                   std::vector<std::pair<uint32_t, uint32_t>> padding,
+                                   const std::vector<std::pair<uint32_t, uint32_t>>& padding,
                                    const float value,
                                    const bool use_multicore,
                                    const std::optional<MemoryConfig>& memory_config_arg);
