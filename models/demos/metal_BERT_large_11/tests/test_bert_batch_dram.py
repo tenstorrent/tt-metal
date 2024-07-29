7cca5324b5521237b94470917e23969d2ea18d69
Raymond Kim <rkim@tenstorrent.com>
2024-07-29 16:43:00 +0000

    #10395: Add skips for WH X1 metal bert batch dram

--- models/demos/metal_BERT_large_11/tests/test_bert_batch_dram.py
+++ models/demos/metal_BERT_large_11/tests/test_bert_batch_dram.py
@@ -22,6 +22,7 @@ from models.utility_functions import (
     profiler,
     is_e75,
     skip_for_wormhole_b0,
+    is_x2_harvested,
 )
 
 
@@ -288,6 +289,8 @@ def test_bert_batch_dram(
             pytest.skip("Only batch_8-BFLOAT8_B-SHARDED supported for WH B0")
         elif batch == 8 and device.core_grid.y == 7:
             pytest.skip("This test is only supported for 8x8 grids")
+        elif not is_x2_harvested(device):
+            pytest.skip("This test is only supported on WH X1")
 
     model_config = get_model_config(batch, device.compute_with_storage_grid_size(), model_config_str)
     tt_cache_path = get_tt_cache_path(model_version)
