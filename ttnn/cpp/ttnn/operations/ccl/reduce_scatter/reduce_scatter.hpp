38166ac44c7d17f3366ca36c5495849f1462ff8f
Artem Yerofieiev <169092593+ayerofieiev-tt@users.noreply.github.com>
2024-07-29 14:04:42 -0700

    #9322: Split ttnn ccl ops to .hpp and .cpp (#10862)

--- ttnn/cpp/ttnn/operations/ccl/reduce_scatter/reduce_scatter.hpp
+++ ttnn/cpp/ttnn/operations/ccl/reduce_scatter/reduce_scatter.hpp
@@ -4,8 +4,9 @@
 
 #pragma once
 
-#include "ttnn/operations/ccl/reduce_scatter/device/reduce_scatter_op.hpp"
-#include "ttnn/multi_device.hpp"
+#include "ttnn/decorators.hpp"
+
+#include "ttnn/cpp/ttnn/deprecated/tt_dnn/op_library/reduce/common.hpp"
 
 namespace ttnn {
 namespace operations {
@@ -15,12 +16,9 @@ struct ExecuteReduceScatter {
     static ttnn::Tensor operator()(
         const ttnn::Tensor& input_tensor,
         const uint32_t scatter_dim,
-        ReduceOpMath math_op,
+        tt::tt_metal::ReduceOpMath math_op,
         const uint32_t num_links = 1,
-        const std::optional<ttnn::MemoryConfig>& memory_config = std::nullopt) {
-        MemoryConfig out_memory_config = memory_config.value_or(input_tensor.memory_config());
-        return ttnn::operations::ccl::reduce_scatter(input_tensor, scatter_dim, math_op, num_links, out_memory_config);
-    }
+        const std::optional<ttnn::MemoryConfig>& memory_config = std::nullopt);
 };
 
 }  // namespace ccl
