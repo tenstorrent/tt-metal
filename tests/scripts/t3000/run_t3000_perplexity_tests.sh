c8857691881193726d4adc512fe8b49ef6682a8d
Miguel Tairum <150826086+mtairum@users.noreply.github.com>
2024-07-30 10:09:37 +0100

    Add Mixtral Perplexity and Top-1/5 tests to CI (#10850)

--- tests/scripts/t3000/run_t3000_perplexity_tests.sh
+++ tests/scripts/t3000/run_t3000_perplexity_tests.sh
@@ -17,6 +17,10 @@ run_t3000_perplexity_tests() {
   # Llama-70B perplexity tests
   WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/demos/t3000/llama2_70b/demo/eval_t3000.py --timeout=7200 ; fail+=$?
 
+  # Mixtral8x7B perplexity tests
+  WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/demos/t3000/mixtral8x7b/tests/test_mixtral_perplexity.py --timeout=3600 ; fail+=$?
+  WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/demos/t3000/mixtral8x7b/tests/test_mixtral_topk.py --timeout=3600 ; fail+=$?
+
   # Record the end time
   end_time=$(date +%s)
   duration=$((end_time - start_time))
