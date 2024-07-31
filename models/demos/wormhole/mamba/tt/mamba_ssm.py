9ca746b05e47fa94729bfda2a0bc5a2c3abf2a13
Evan Smal <esmal@tenstorrent.com>
2024-07-31 14:50:54 +0000

    #10682: Generate run/measurement CSVs for Mamba single-card demo test
    This change also removes old prefill-by-decode demo code and does some
    minor refactoring to improve overall code quality.

--- models/demos/wormhole/mamba/tt/mamba_ssm.py
+++ models/demos/wormhole/mamba/tt/mamba_ssm.py
@@ -174,15 +174,13 @@ class TtMambaSSM(torch.nn.Module):
         ttnn.deallocate(delta_t1)
 
         # calculate abar
-        abar0 = ttnn.to_memory_config(self.A, memory_config=ttnn.L1_MEMORY_CONFIG)
         abar1 = ttnn.experimental.repeat_and_interleave_eltwise_mul(
-            abar0,
+            self.A,
             delta_t2,
             memory_config=ttnn.L1_MEMORY_CONFIG,
             dtype=self.configs["dtype"]["activations"],
             math_fidelity=self.eltwise_math_fidelity,
         )
-        ttnn.deallocate(abar0)
 
         abar2 = ttnn.exp(
             abar1,
@@ -264,19 +262,17 @@ class TtMambaSSM(torch.nn.Module):
                 prev_hidden_state,
                 memory_config=self.configs["sharded_scan"],
                 dtype=ttnn.bfloat8_b,
+                math_fidelity=ttnn.MathFidelity.HiFi3,
             )
             ttnn.deallocate(abar2_sharded)
             ttnn.deallocate(bmulx0_sharded)
-
-            # We have to move hidden states results to DRAM or else we don't have enough room for the prev_hidden_state copy
-            hidden_state0 = ttnn.to_memory_config(hidden_states_sharded, memory_config=ttnn.DRAM_MEMORY_CONFIG)
-            ttnn.deallocate(hidden_states_sharded)
 
             prev_hidden_state = ttnn.to_memory_config(prev_hidden_state, memory_config=ttnn.DRAM_MEMORY_CONFIG)
             self.hidden_state_cache.set(self.configs["current_user"], 0, prev_hidden_state)
             ttnn.deallocate(prev_hidden_state)
 
-            hidden_state0 = ttnn.to_memory_config(hidden_state0, memory_config=ttnn.L1_MEMORY_CONFIG)
+            hidden_state0 = ttnn.to_memory_config(hidden_states_sharded, memory_config=ttnn.L1_MEMORY_CONFIG)
+            ttnn.deallocate(hidden_states_sharded)
 
         # compute C
         C0 = ttnn.linear(
