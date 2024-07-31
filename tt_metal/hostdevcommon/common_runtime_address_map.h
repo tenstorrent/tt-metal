1b0854bb5fcbe892f06c3d8296443544bec3be23
Paul Keller <pkeller@tenstorrent.com>
2024-07-31 22:08:21 +0000

    #4984: Move watcher ring buffer to dev_mem_map/msgs

--- tt_metal/hostdevcommon/common_runtime_address_map.h
+++ tt_metal/hostdevcommon/common_runtime_address_map.h
@@ -90,11 +90,7 @@ constexpr static std::uint32_t PRINT_BUFFER_T2 = PRINT_BUFFER_T1 + PRINT_BUFFER_
 constexpr static std::uint32_t PRINT_BUFFER_BR = PRINT_BUFFER_T2 + PRINT_BUFFER_SIZE; // BRISC
 constexpr static std::uint32_t PRINT_BUFFER_IDLE_ER = PRINT_BUFFER_START; // Idle ERISC
 
-// Debug ring buffer, shared between all cores
-constexpr static std::uint32_t RING_BUFFER_ADDR = PRINT_BUFFER_START + PRINT_BUFFER_MAX_SIZE;
-constexpr static std::uint32_t RING_BUFFER_SIZE = 128;
-
-constexpr static std::uint32_t L1_UNRESERVED_BASE = (((RING_BUFFER_ADDR + RING_BUFFER_SIZE) - 1) | (DRAM_ALIGNMENT - 1)) + 1;
+constexpr static std::uint32_t L1_UNRESERVED_BASE = (((PRINT_BUFFER_START + PRINT_BUFFER_MAX_SIZE) - 1) | (DRAM_ALIGNMENT - 1)) + 1;
 constexpr static std::uint32_t ERISC_L1_UNRESERVED_BASE = L1_UNRESERVED_BASE; // Start of unreserved space
 
 // Helper functions to convert NoC coordinates to NoC-0 coordinates, used in metal as "physical" coordinates.
