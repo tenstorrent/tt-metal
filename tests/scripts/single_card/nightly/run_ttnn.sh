a635f511d3cd002ca384cd55339824c5d0ad5532
Raymond Kim <rkim@tenstorrent.com>
2024-07-31 10:00:26 -0400

    #10395: Add WH_ARCH_YAML for N300 ttnn nightly

--- tests/scripts/single_card/nightly/run_ttnn.sh
+++ tests/scripts/single_card/nightly/run_ttnn.sh
@@ -10,7 +10,7 @@ fail=0
 
 echo "Running ttnn nightly tests"
 
-env pytest -n auto tests/ttnn/integration_tests -m "not models_performance_bare_metal and not models_device_performance_bare_metal" ; fail+=$?
+env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto tests/ttnn/integration_tests -m "not models_performance_bare_metal and not models_device_performance_bare_metal" ; fail+=$?
 
 if [[ $fail -ne 0 ]]; then
   exit 1
