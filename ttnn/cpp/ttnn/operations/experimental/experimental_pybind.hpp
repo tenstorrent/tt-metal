cbc1c0e18006b15988b163881485b297e3e47277
Saad Jameel <sjameel@tenstorrent.com>
2024-08-01 14:57:36 +0000

    #10123: move nlp_create_qkv_heads to ttnn/operations/experimental from deprecated

--- ttnn/cpp/ttnn/operations/experimental/experimental_pybind.hpp
+++ ttnn/cpp/ttnn/operations/experimental/experimental_pybind.hpp
@@ -11,12 +11,14 @@
 #include "ttnn/operations/experimental/ssm/prefix_scan/prefix_scan_pybind.hpp"
 #include "ttnn/operations/experimental/ssm/repeat_and_interleave_eltwise_mul/repeat_and_interleave_eltwise_mul_pybind.hpp"
 #include "ttnn/operations/experimental/transformer/transformer_pybind.hpp"
+#include "ttnn/operations/experimental/transformer/nlp_create_qkv_heads/nlp_create_qkv_heads_pybind.hpp"
 
 namespace ttnn::operations::experimental {
 
 void py_module(py::module& module) {
     // Transformer ops
     transformer::detail::bind_experimental_transformer_operations(module);
+    transformer::detail::bind_nlp_create_qkv_heads(module);
     reduction::detail::bind_argmax_operation(module);
     reduction::detail::bind_argmin_operation(module);
     ssm::detail::bind_prefix_scan(module);
