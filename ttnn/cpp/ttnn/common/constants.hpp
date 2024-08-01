b46081608a1a1019c9b91a75b1f8c8d76175d2d9
Artem Yerofieiev <artem@tenstorrent.com>
2024-08-01 18:41:57 +0000

    #0: chatgpt review

--- ttnn/cpp/ttnn/common/constants.hpp
+++ ttnn/cpp/ttnn/common/constants.hpp
@@ -9,11 +9,12 @@
 namespace ttnn {
 
 /*
-    We have 2 software command queues available to make it possible to overlap some work and reduce latency.
-    For example, Op2 can be prepared in a different queue while the first queue is blocked waiting for data readout by Op1
-    TT-NN operations allows to specify which queue should be used.
-    The default Queue is 0 and possible values are 0 and 1.
+    We have two software command queues available to overlap some work and reduce latency.
+    For example, Op2 can be prepared in a different queue while the first queue is blocked, waiting for data readout by Op1.
+    TT-NN operations allow specifying which queue should be used.
+    The default queue is 0, and the possible values are 0 and 1.
 */
+
 constexpr uint8_t DefaultQueueId = 0;
 
 }
