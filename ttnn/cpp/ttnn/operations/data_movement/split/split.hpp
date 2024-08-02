71365f1d0847d019f81266a7a3b76b57e2b1b54b
Naif Tarafdar <135640067+tarafdarTT@users.noreply.github.com>
2024-08-02 13:19:50 -0400

    #9754: moved split to ttnn (#11018)

---
+++ ttnn/cpp/ttnn/operations/data_movement/split/split.hpp
@@ -0,0 +1,36 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "ttnn/run_operation.hpp"
+#include "ttnn/decorators.hpp"
+
+
+namespace ttnn {
+namespace operations::data_movement {
+
+struct SplitOperation {
+    static std::vector<ttnn::Tensor> operator()(
+        uint8_t queue_id,
+        const ttnn::Tensor& input_tensor,
+        int64_t& num_splits,
+        int64_t& dim,
+        const std::optional<MemoryConfig>& memory_config_arg);
+
+    static std::vector<ttnn::Tensor> operator()(
+        const ttnn::Tensor& input_tensor,
+        int64_t& num_splits,
+        int64_t& dim,
+        const std::optional<MemoryConfig>& memory_config);
+
+    static std::vector<ttnn::Tensor> operator()(const ttnn::Tensor& input_tensor, int64_t& num_splits, int64_t& dim);
+};
+
+
+}  // namespace operations::data_movement
+
+constexpr auto split = ttnn::register_operation<"ttnn::split", ttnn::operations::data_movement::SplitOperation>();
+
+}  // namespace ttnn
