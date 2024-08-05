40a5a7f93cfc00fd0e8997a0a54ef96a4ef0b3ea
Stuti Raizada <sraizada@tenstorrent.com>
2024-08-05 07:58:14 -0700

    #10341: bcast init changes

--- ttnn/cpp/ttnn/operations/reduction/moe/device/kernels/compute/moe.cpp
+++ ttnn/cpp/ttnn/operations/reduction/moe/device/kernels/compute/moe.cpp
@@ -88,16 +88,23 @@ void sub_exp_block_bcast_cols_inplace() {
     }
 }
 
-void add_block_bcast_rows_inplace(uint32_t in0_cb, uint32_t in1_cb, uint32_t rows, uint32_t cols) {
+void add_block_bcast_rows_inplace(uint32_t in0_cb, uint32_t in1_cb, uint32_t rows, uint32_t cols, bool first_call) {
     // Precondition: in0_cb and in1_cb have num_tiles produced
     // Postcondition: in0_cb has num_tiles produced
     // Postcondition: in1_cb has num_tiles consumed
 
     uint32_t num_tiles = rows * cols;
-    unpack_reconfig_data_format(in0_cb, in1_cb);
-    add_bcast_rows_init_short(in0_cb, in1_cb);
+    if (first_call) {
+        init_bcast<EltwiseBinaryType::ELWADD, BroadcastType::ROW>(in0_cb, in1_cb);
+    }
+    else{
+        unpack_reconfig_data_format(in0_cb, in1_cb);
+        add_bcast_rows_init_short(in0_cb, in1_cb);
+    }
     cb_wait_front(in0_cb, num_tiles);
     cb_wait_front(in1_cb, cols);
+    //print_full_tile(in0_cb);
+    //print_full_tile(in1_cb);
     for (uint32_t i = 0; i < rows; ++i) {
         for (uint32_t j = 0; j < cols; ++j) {
             acquire_dst(tt::DstMode::Half);
@@ -110,7 +117,9 @@ void add_block_bcast_rows_inplace(uint32_t in0_cb, uint32_t in1_cb, uint32_t row
             release_dst(tt::DstMode::Half);
         }
     }
-    //cb_pop_front(in1_cb, cols);
+    //print_full_tile(in0_cb, 0);
+
+    cb_pop_front(in1_cb, cols);
 }
 void mul_block_inplace(uint32_t in0_cb, uint32_t in1_cb, uint32_t num_tiles) {
     // Precondition: in0_cb and in1_cb have num_tiles produced
@@ -236,13 +245,12 @@ void top_k() {
     constexpr uint32_t index_dest_start = 2;
     constexpr uint32_t input_dest_end = 1;
     constexpr uint32_t index_dest_end = 3;
-    cb_wait_front(input_cb_index, Ht*Wt);
+    //cb_wait_front(input_cb_index, Ht*Wt);
     ckernel::topk_tile_init();
     //print_full_tile(input_cb_index);
-    transpose_wh_init(input_cb_index, input_transposed_cb_index);
-    //transpose_wh_init_short(input_cb_index);
-    //cb_wait_front(input_cb_index, 1);
-    print_full_tile(input_cb_index);
+    //transpose_wh_init(input_cb_index, input_transposed_cb_index);
+
+    //print_full_tile(input_cb_index);
 
     for(uint32_t ht = 0; ht < Ht; ++ht) {
         bool ascending = false;
@@ -398,27 +406,27 @@ void MAIN {
     constexpr uint32_t Kt =  K % 32 == 0 ? K/32 : K/32 + 1;
 
     // mask out invalid experts
-    add_block_bcast_rows_inplace(input_cb_index, expert_mask_cb_index, Ht,Wt);
+    add_block_bcast_rows_inplace(input_cb_index, expert_mask_cb_index, Ht,Wt, true);
+    //cb_wait_front(input_cb_index, Ht*Wt);
+    //print_full_tile(input_cb_index);
     // top-k
     top_k<Ht, Wt, K, logWt, logk, input_cb_index, index_cb_index, input_transposed_cb_index, index_transposed_cb_index, values_cb_index, output_ind_cb_index>();
     //print_full_tile(input_cb_index);
-    //top_k<Ht, Wt, K, logWt, logk, values_cb_index, index_cb_index, input_transposed_cb_index, index_transposed_cb_index, values_cb_index, output_ind_cb_index>();
-
     //sfpu::_init_sfpu_config_reg();
 
     // mask out all experts except the top-k
-    //add_block_bcast_rows_inplace(values_cb_index, topk_mask_cb_index, Ht,Kt);
-    //eqz_block_inplace(output_ind_cb_index, Ht*Kt);
+    add_block_bcast_rows_inplace(values_cb_index, topk_mask_cb_index, Ht,Kt, false);
+    eqz_block_inplace(output_ind_cb_index, Ht*Kt);
 
     //softmax
-    // reduce_c<PoolType::MAX, ReduceDim::REDUCE_ROW, values_cb_index, scale_cb_index, cb_cur_max, Ht, Kt>();
-    // sub_exp_block_bcast_cols_inplace<values_cb_index, cb_cur_max, Ht, Kt>();
-    // reduce_c<PoolType::SUM, ReduceDim::REDUCE_ROW, values_cb_index, scale_cb_index, cb_cur_sum, Ht, Kt>();
-    // recip_block_inplace(cb_cur_sum, Ht);
-    // mul_block_bcast_cols_inplace(values_cb_index, cb_cur_sum, Ht, Kt);
+    reduce_c<PoolType::MAX, ReduceDim::REDUCE_ROW, values_cb_index, scale_cb_index, cb_cur_max, Ht, Kt>();
+    sub_exp_block_bcast_cols_inplace<values_cb_index, cb_cur_max, Ht, Kt>();
+    reduce_c<PoolType::SUM, ReduceDim::REDUCE_ROW, values_cb_index, scale_cb_index, cb_cur_sum, Ht, Kt>();
+    recip_block_inplace(cb_cur_sum, Ht);
+    mul_block_bcast_cols_inplace(values_cb_index, cb_cur_sum, Ht, Kt);
 
     // select 0th expert
-    //mul_block_inplace(values_cb_index, output_ind_cb_index, Ht*Kt);
+    mul_block_inplace(values_cb_index, output_ind_cb_index, Ht*Kt);
 
     //final sum
     reduce_c<PoolType::SUM, ReduceDim::REDUCE_ROW, values_cb_index, scale_cb_index, out_cb_index, Ht, Kt>();
