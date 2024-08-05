1a19fabd036f7e5dfbbaff399ed49af2830922a2
Bharane AB <bharane.amutha@multicorewareinc.com>
2024-08-05 12:27:56 +0000

    #10881: update golden function

--- tests/ttnn/unit_tests/operations/test_unary.py
+++ tests/ttnn/unit_tests/operations/test_unary.py
@@ -9,14 +9,16 @@ import torch
 import ttnn
 
 from tests.ttnn.utils_for_testing import assert_with_pcc, assert_equal
+from tests.ttnn.unit_tests.operations.backward.utility_funcs import data_gen_with_range, compare_pcc
 from models.utility_functions import torch_random, skip_for_grayskull, skip_for_wormhole_b0
 
 
-def run_unary_test(device, h, w, ttnn_function, torch_function, pcc=0.9999):
+def run_unary_test(device, h, w, ttnn_function, pcc=0.9999):
     torch.manual_seed(0)
 
     torch_input_tensor = torch.rand((h, w), dtype=torch.bfloat16)
-    torch_output_tensor = torch_function(torch_input_tensor)
+    golden_function = ttnn.get_golden_function(ttnn_function)
+    torch_output_tensor = golden_function(torch_input_tensor)
 
     input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
     output_tensor = ttnn_function(input_tensor)
@@ -29,7 +31,7 @@ def run_unary_test(device, h, w, ttnn_function, torch_function, pcc=0.9999):
 
 def run_identity_test(device, h, w, data_type, pcc=0.9999):
     torch.manual_seed(0)
-
+    ttnn_function = ttnn.identity
     if data_type == ttnn.uint8:
         # init value
         torch_input_tensor = torch.randint(0, 245, (1, 1, h, w), dtype=torch.uint8)
@@ -37,7 +39,8 @@ def run_identity_test(device, h, w, data_type, pcc=0.9999):
 
         # run torch
         torch_input_tensor = torch_input_tensor + bias
-        torch_output_tensor = torch_input_tensor
+        golden_function = ttnn.get_golden_function(ttnn_function)
+        torch_output_tensor = golden_function(torch_input_tensor)
 
         # run tt
         input_tensor = ttnn.from_torch(torch_input_tensor, data_type, layout=ttnn.TILE_LAYOUT, device=device)
@@ -53,7 +56,8 @@ def run_identity_test(device, h, w, data_type, pcc=0.9999):
 
         # run torch
         torch_input_tensor = torch_input_tensor + bias
-        torch_output_tensor = torch_input_tensor
+        golden_function = ttnn.get_golden_function(ttnn_function)
+        torch_output_tensor = golden_function(torch_input_tensor)
 
         # run tt
         input_tensor = ttnn.from_torch(torch_input_tensor, data_type, layout=ttnn.TILE_LAYOUT, device=device)
@@ -70,7 +74,8 @@ def run_identity_test(device, h, w, data_type, pcc=0.9999):
 
         # run torch
         torch_input_tensor = torch_input_tensor + bias
-        torch_output_tensor = torch_input_tensor
+        golden_function = ttnn.get_golden_function(ttnn_function)
+        torch_output_tensor = golden_function(torch_input_tensor)
 
         # run tt
         input_tensor = ttnn.from_torch(torch_input_tensor, data_type, layout=ttnn.TILE_LAYOUT, device=device)
@@ -87,7 +92,8 @@ def run_identity_test(device, h, w, data_type, pcc=0.9999):
 
         # run torch
         torch_input_tensor = torch_input_tensor + bias
-        torch_output_tensor = torch_input_tensor
+        golden_function = ttnn.get_golden_function(ttnn_function)
+        torch_output_tensor = golden_function(torch_input_tensor)
 
         # run tt
         input_tensor = ttnn.from_torch(torch_input_tensor, data_type, layout=ttnn.TILE_LAYOUT, device=device)
@@ -103,7 +109,8 @@ def run_identity_test(device, h, w, data_type, pcc=0.9999):
 
         # run torch
         torch_input_tensor = torch_input_tensor
-        torch_output_tensor = torch_input_tensor
+        golden_function = ttnn.get_golden_function(ttnn_function)
+        torch_output_tensor = golden_function(torch_input_tensor)
 
         # run tt
         input_tensor = ttnn.from_torch(torch_input_tensor, data_type, layout=ttnn.TILE_LAYOUT, device=device)
@@ -125,125 +132,127 @@ def test_fp32_uint32(device, h, w, dtype):
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_exp(device, h, w):
-    run_unary_test(device, h, w, ttnn.exp, torch.exp, pcc=0.9998)
+    run_unary_test(device, h, w, ttnn.exp, pcc=0.9998)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_tanh(device, h, w):
-    run_unary_test(device, h, w, ttnn.tanh, torch.tanh, pcc=0.993)
+    run_unary_test(device, h, w, ttnn.tanh, pcc=0.993)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_gelu(device, h, w):
-    run_unary_test(device, h, w, ttnn.gelu, torch.nn.functional.gelu, pcc=0.9996)
+    run_unary_test(device, h, w, ttnn.gelu, pcc=0.9996)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_relu(device, h, w):
-    run_unary_test(device, h, w, ttnn.relu, torch.relu)
+    run_unary_test(device, h, w, ttnn.relu)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_rsqrt(device, h, w):
-    run_unary_test(device, h, w, ttnn.rsqrt, torch.rsqrt)
+    run_unary_test(device, h, w, ttnn.rsqrt)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_silu(device, h, w):
-    run_unary_test(device, h, w, ttnn.silu, torch.nn.functional.silu)
+    run_unary_test(device, h, w, ttnn.silu)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_log(device, h, w):
-    run_unary_test(device, h, w, ttnn.log, torch.log)
+    run_unary_test(device, h, w, ttnn.log)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_sin(device, h, w):
-    run_unary_test(device, h, w, ttnn.sin, torch.sin)
+    run_unary_test(device, h, w, ttnn.sin)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_asin(device, h, w):
-    run_unary_test(device, h, w, ttnn.asin, torch.asin, pcc=0.999)
+    run_unary_test(device, h, w, ttnn.asin, pcc=0.999)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_cos(device, h, w):
-    run_unary_test(device, h, w, ttnn.cos, torch.cos, pcc=0.999)
+    run_unary_test(device, h, w, ttnn.cos, pcc=0.999)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_acos(device, h, w):
-    run_unary_test(device, h, w, ttnn.acos, torch.acos, pcc=0.999)
+    run_unary_test(device, h, w, ttnn.acos, pcc=0.999)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_tan(device, h, w):
-    run_unary_test(device, h, w, ttnn.tan, torch.tan)
+    run_unary_test(device, h, w, ttnn.tan)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_atan(device, h, w):
-    run_unary_test(device, h, w, ttnn.atan, torch.atan)
+    run_unary_test(device, h, w, ttnn.atan)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_sinh(device, h, w):
-    run_unary_test(device, h, w, ttnn.sinh, torch.sinh)
+    run_unary_test(device, h, w, ttnn.sinh)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_asinh(device, h, w):
-    run_unary_test(device, h, w, ttnn.asinh, torch.asinh, pcc=0.9997)
+    run_unary_test(device, h, w, ttnn.asinh, pcc=0.9997)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_cosh(device, h, w):
-    run_unary_test(device, h, w, ttnn.cosh, torch.cosh, pcc=0.999)
+    run_unary_test(device, h, w, ttnn.cosh, pcc=0.999)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 @skip_for_wormhole_b0("Issue #6991: Failing on wormhole_b0 PCC issue")
 def test_acosh(device, h, w):
-    run_unary_test(device, h, w, ttnn.acosh, torch.acosh)
+    run_unary_test(device, h, w, ttnn.acosh)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_atanh(device, h, w):
-    run_unary_test(device, h, w, ttnn.atanh, torch.atanh)
+    run_unary_test(device, h, w, ttnn.atanh)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_logical_not(device, h, w):
-    run_unary_test(device, h, w, ttnn.logical_not, torch.logical_not)
+    run_unary_test(device, h, w, ttnn.logical_not)
 
 
-def run_unary_test_range(device, h, w, ttnn_function, torch_function, pcc=0.9999):
+def run_unary_test_range(device, h, w, ttnn_function, pcc=0.9999):
     torch.manual_seed(0)
     low = -100
     high = 100
 
     torch_input_tensor = torch_random((h, w), low, high, dtype=torch.bfloat16)
-    torch_output_tensor = torch_function(torch_input_tensor)
+
+    golden_function = ttnn.get_golden_function(ttnn_function)
+    torch_output_tensor = golden_function(torch_input_tensor)
 
     input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
     output_tensor = ttnn_function(input_tensor)
@@ -257,36 +266,29 @@ def run_unary_test_range(device, h, w, ttnn_function, torch_function, pcc=0.9999
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_signbit(device, h, w):
-    run_unary_test_range(device, h, w, ttnn.signbit, torch.signbit, pcc=0.99)
+    run_unary_test_range(device, h, w, ttnn.signbit, pcc=0.99)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 @skip_for_grayskull("Op not supported for Grayskull, supported for wormhole_b0")
 def test_floor(device, h, w):
-    run_unary_test_range(device, h, w, ttnn.floor, torch.floor, pcc=0.99)
+    run_unary_test_range(device, h, w, ttnn.floor, pcc=0.99)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 @skip_for_grayskull("Op not supported for Grayskull, supported for wormhole_b0")
 def test_ceil(device, h, w):
-    run_unary_test_range(device, h, w, ttnn.ceil, torch.ceil, pcc=0.99)
+    run_unary_test_range(device, h, w, ttnn.ceil, pcc=0.99)
 
 
-def torch_relu_max(x, upper_limit, *args, **kwargs):
-    return torch.relu(torch.min(x, torch.tensor(upper_limit)))
-
-
-def torch_relu_min(x, lower_limit, *args, **kwargs):
-    return torch.max(x, torch.tensor(lower_limit))
-
-
-def run_unary_test_with_float(device, h, w, scalar, ttnn_function, torch_function, pcc=0.9999):
+def run_unary_test_with_float(device, h, w, scalar, ttnn_function, pcc=0.9999):
     torch.manual_seed(0)
 
     torch_input_tensor = torch.rand((h, w), dtype=torch.bfloat16)
-    torch_output_tensor = torch_function(torch_input_tensor, scalar)
+    golden_function = ttnn.get_golden_function(ttnn_function)
+    torch_output_tensor = golden_function(torch_input_tensor, scalar)
 
     input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
     output_tensor = ttnn_function(input_tensor, scalar)
@@ -320,21 +322,45 @@ def test_logit(device, h, w, scalar):
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_pow(device, h, w, scalar):
-    run_unary_test_with_float(device, h, w, scalar, ttnn.pow, torch.pow, pcc=0.9)
+    run_unary_test_with_float(device, h, w, scalar, ttnn.pow, pcc=0.9)
 
 
-@pytest.mark.parametrize("scalar", [-0.5, -2.0])
+@pytest.mark.parametrize("lower_limit", [0, 1.0, 2])
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
-def test_relu_min(device, h, w, scalar):
-    run_unary_test_with_float(device, h, w, scalar, ttnn.relu_min, torch_relu_min)
+def test_relu_min(device, h, w, lower_limit):
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.rand((h, w), dtype=torch.bfloat16)
+    golden_function = ttnn.get_golden_function(ttnn.relu_min)
+    torch_output_tensor = golden_function(torch_input_tensor, lower_limit=lower_limit)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+    output_tensor = ttnn.relu_min(input_tensor, lower_limit)
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor)
 
 
-@pytest.mark.parametrize("scalar", [7.5, 2.0])
+@pytest.mark.parametrize("upper_limit", [0, 1.0, 2])
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
-def test_relu_max(device, h, w, scalar):
-    run_unary_test_with_float(device, h, w, scalar, ttnn.relu_max, torch_relu_max)
+def test_relu_max(device, h, w, upper_limit):
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.rand((h, w), dtype=torch.bfloat16)
+    golden_function = ttnn.get_golden_function(ttnn.relu_max)
+    torch_output_tensor = golden_function(torch_input_tensor, upper_limit=upper_limit)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+    output_tensor = ttnn.relu_max(input_tensor, upper_limit)
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor)
 
 
 @pytest.mark.parametrize("scalar", [1.5, 2.0])
@@ -342,7 +368,7 @@ def test_relu_max(device, h, w, scalar):
 @pytest.mark.parametrize("w", [128])
 @skip_for_grayskull("Op not supported for Grayskull, supported for wormhole_b0")
 def test_remainder(device, h, w, scalar):
-    run_unary_test_with_float(device, h, w, scalar, ttnn.remainder, torch.remainder)
+    run_unary_test_with_float(device, h, w, scalar, ttnn.remainder)
 
 
 @pytest.mark.parametrize("scalar", [1.5, 2.0])
@@ -350,4 +376,4 @@ def test_remainder(device, h, w, scalar):
 @pytest.mark.parametrize("w", [128])
 @skip_for_grayskull("Op not supported for Grayskull, supported for wormhole_b0")
 def test_fmod(device, h, w, scalar):
-    run_unary_test_with_float(device, h, w, scalar, ttnn.fmod, torch.fmod)
+    run_unary_test_with_float(device, h, w, scalar, ttnn.fmod)
