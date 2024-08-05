79211c8d4f3d65e902679eedc24c5900a48a8619
Andrija Malbasa <amalbasa@tenstorrent.com>
2024-08-05 11:37:13 +0200

    Migration of normalize ops sweeps from tt_eager to ttnn (#10839)
    * #10147: Migrated normalize_global from tt_eager to ttnn
    
    * #10147: Migrated normalize_gw from tt_eager to ttnn
    
    * #10147: Modified test_stats
    
    * #10147: Modify test_stats.py and reformating
    
    * #10147: Reformating

--- tests/ttnn/python_api_testing/sweep_tests/op_map.py
+++ tests/ttnn/python_api_testing/sweep_tests/op_map.py
@@ -865,4 +865,12 @@ op_map = {
         "tt_op": ttnn_ops.cos_bw,
         "pytorch_op": pytorch_ops.cos_bw,
     },
+    "stats-normalize_global": {
+        "tt_op": ttnn_ops.normalize_global,
+        "pytorch_op": pytorch_ops.normalize_global,
+    },
+    "stats-normalize_hw": {
+        "tt_op": ttnn_ops.normalize_hw,
+        "pytorch_op": pytorch_ops.normalize_hw,
+    },
 }
