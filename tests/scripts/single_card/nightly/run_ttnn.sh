17d7419fc30edca823f11d703b5f2ebea109098e
Raymond Kim <rkim@tenstorrent.com>
2024-08-06 15:56:32 -0400

    #10395: Gate WH_ARCH_YAML on wormhole_b0 ARCH_NAME and tighten bounds for weka mounting timeouts

--- tests/scripts/single_card/nightly/run_ttnn.sh
+++ tests/scripts/single_card/nightly/run_ttnn.sh
@@ -10,7 +10,11 @@ fail=0
 
 echo "Running ttnn nightly tests"
 
-env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto tests/ttnn/integration_tests -m "not models_performance_bare_metal and not models_device_performance_bare_metal" ; fail+=$?
+if [[ "$ARCH_NAME" == "wormhole_b0" ]]; then
+  export WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml
+fi
+
+env pytest -n auto tests/ttnn/integration_tests -m "not models_performance_bare_metal and not models_device_performance_bare_metal" ; fail+=$?
 
 if [[ $fail -ne 0 ]]; then
   exit 1
