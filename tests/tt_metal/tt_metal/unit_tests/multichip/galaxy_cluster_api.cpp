84efee8a249596ca4b34d673679a002b2cf36f8e
Samarth Agarwal <sagarwal@tenstorrent.com>
2024-08-06 17:53:06 +0000

    #10612: Added assert messages that will be displayed when an assert fails.

--- tests/tt_metal/tt_metal/unit_tests/multichip/galaxy_cluster_api.cpp
+++ tests/tt_metal/tt_metal/unit_tests/multichip/galaxy_cluster_api.cpp
@@ -39,7 +39,8 @@ TEST_F(GalaxyFixture, ValidateNumLinksBetweenAdjacentGalaxyChips) {
                     continue;
                 }
                 const std::vector<CoreCoord>& ethernet_sockets = tt::Cluster::instance().get_ethernet_sockets(device_id, connected_device_id);
-                ASSERT_TRUE(ethernet_sockets.size() == 4);
+                const uint32_t num_links = ethernet_sockets.size();
+                ASSERT_TRUE(num_links == 4) << "Detected " << num_links << " links between chip " << device_id << " and chip " << connected_device_id << std::endl;
             }
         }
     }
@@ -54,28 +55,29 @@ TEST_F(GalaxyFixture, ValidateLinksBetweenMMIOAndGalaxyChips) {
         const std::unordered_set<chip_id_t>& connected_device_ids = tt::Cluster::instance().get_ethernet_connected_device_ids(device_id);
         if (is_galaxy_device(device_id))
         {
-            uint32_t num_links_to_mmio_chips = 0;
+            uint32_t num_mmio_chips_that_curr_chip_is_linked_to = 0;
             for (const chip_id_t connected_device_id : connected_device_ids)
             {
                 if (is_galaxy_device(connected_device_id))
                 {
                     continue;
                 }
-                num_links_to_mmio_chips++;
+                num_mmio_chips_that_curr_chip_is_linked_to++;
             }
-            ASSERT_TRUE(num_links_to_mmio_chips <= 1);
+            ASSERT_TRUE(num_mmio_chips_that_curr_chip_is_linked_to <= 1) << "Detected " << num_mmio_chips_that_curr_chip_is_linked_to << " MMIO chips that chip " << device_id << " is linked to" << std::endl;
         }
         else
         {
-            const bool do_both_links_go_to_separate_devices = connected_device_ids.size() == 2;
-            ASSERT_TRUE(do_both_links_go_to_separate_devices);
+            const uint32_t num_chips_that_curr_chip_is_linked_to = connected_device_ids.size();
+            const bool do_both_links_go_to_separate_devices = num_chips_that_curr_chip_is_linked_to == 2;
+            ASSERT_TRUE(do_both_links_go_to_separate_devices) << "Detected " << num_chips_that_curr_chip_is_linked_to << " chips that chip " << device_id << " is linked to" << std::endl;
 
             bool do_both_links_go_to_galaxy_devices = true;
             for (const chip_id_t connected_device_id : connected_device_ids)
             {
                 do_both_links_go_to_galaxy_devices = do_both_links_go_to_galaxy_devices && is_galaxy_device(connected_device_id);
             }
-            ASSERT_TRUE(do_both_links_go_to_galaxy_devices);
+            ASSERT_TRUE(do_both_links_go_to_galaxy_devices) << "Detected links from chip " << device_id << " to chip " << *(connected_device_ids.begin()) << " and chip " << *(++connected_device_ids.begin()) << std::endl;
         }
     }
 }
@@ -88,7 +90,7 @@ TEST_F(GalaxyFixture, ValidateAllGalaxyChipsAreUnharvested) {
         if (is_galaxy_device(device_id))
         {
             const uint32_t harvest_mask = tt::Cluster::instance().get_harvested_rows(device_id);
-            ASSERT_TRUE(harvest_mask == 0);
+            ASSERT_TRUE(harvest_mask == 0) << "Harvest mask for chip " << device_id << ": " << harvest_mask << std::endl;
         }
     }
 }
@@ -110,19 +112,19 @@ TEST_F(GalaxyFixture, ValidateAllMMIOChipsHaveSingleRowHarvested) {
                 }
                 harvest_mask = harvest_mask >> 1;
             }
-            ASSERT_TRUE(num_rows_harvested == 1);
+            ASSERT_TRUE(num_rows_harvested == 1) << "Detected " << num_rows_harvested << " harvested rows for chip " << device_id << std::endl;
         }
     }
 }
 
 TEST_F(TGFixture, ValidateNumMMIOChips) {
     const size_t num_mmio_chips = tt::Cluster::instance().number_of_pci_devices();
-    ASSERT_TRUE(num_mmio_chips == 4);
+    ASSERT_TRUE(num_mmio_chips == 4) << "Detected " << num_mmio_chips << " MMIO chips" << std::endl;
 }
 
 TEST_F(TGFixture, ValidateNumGalaxyChips) {
     const size_t num_galaxy_chips = tt::Cluster::instance().number_of_user_devices();
-    ASSERT_TRUE(num_galaxy_chips == 32);
+    ASSERT_TRUE(num_galaxy_chips == 32) << "Detected " << num_galaxy_chips << " Galaxy chips" << std::endl;
 }
 
 // Validate that there are 4 N150 chips and 32 Galaxy chips
@@ -141,18 +143,18 @@ TEST_F(TGFixture, ValidateChipBoardTypes) {
             num_n150_chips++;
         }
     }
-    ASSERT_TRUE(num_galaxy_chips == 32);
-    ASSERT_TRUE(num_n150_chips == 4);
+    ASSERT_TRUE(num_galaxy_chips == 32) << "Detected " << num_galaxy_chips << " Galaxy chips" << std::endl;
+    ASSERT_TRUE(num_n150_chips == 4) << "Detected " << num_n150_chips << " N150 chips" << std::endl;
 }
 
 TEST_F(TGGFixture, ValidateNumMMIOChips) {
     const size_t num_mmio_chips = tt::Cluster::instance().number_of_pci_devices();
-    ASSERT_TRUE(num_mmio_chips == 8);
+    ASSERT_TRUE(num_mmio_chips == 8) << "Detected " << num_mmio_chips << " MMIO chips" << std::endl;
 }
 
 TEST_F(TGGFixture, ValidateNumGalaxyChips) {
     const size_t num_galaxy_chips = tt::Cluster::instance().number_of_user_devices();
-    ASSERT_TRUE(num_galaxy_chips == 64);
+    ASSERT_TRUE(num_galaxy_chips == 64) << "Detected " << num_galaxy_chips << " Galaxy chips" << std::endl;
 }
 
 // Validate that there are 8 N150 chips and 64 Galaxy chips
@@ -171,6 +173,6 @@ TEST_F(TGGFixture, ValidateChipBoardTypes) {
             num_n150_chips++;
         }
     }
-    ASSERT_TRUE(num_galaxy_chips == 64);
-    ASSERT_TRUE(num_n150_chips == 8);
+    ASSERT_TRUE(num_galaxy_chips == 64) << "Detected " << num_galaxy_chips << " Galaxy chips" << std::endl;
+    ASSERT_TRUE(num_n150_chips == 8) << "Detected " << num_n150_chips << " N150 chips" << std::endl;
 }
