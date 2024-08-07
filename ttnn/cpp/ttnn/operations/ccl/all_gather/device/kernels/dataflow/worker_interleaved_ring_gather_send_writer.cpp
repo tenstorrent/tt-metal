172f8d464e35e2e38ba106a5c60528330a229932
Sean Nijjar <sean.nijjar@gmail.com>
2024-08-07 15:04:41 -0400

    Generalizing sharding support in CCLs (#11074)
    #10786: add generalized (page granularity) sharded tensor indexing to CCLs
    
    Added a page index to noc address component, analogous to InterleavedAddrGen. In addition to this, templatized builders have been added to allow a kernel to invoke a generic AddrGen that will be selected based on a passed in `TensorMemoryLayout` argument. This way the op kernel code isn't required to change but can automatically support performance improvements, new sharding configurations, and new memory layouts.
    
    As a results we now support:
    - Width Sharded All Gather (Line and Ring)
    - Height Sharded All Gather (Line and Ring)
    - Block Sharded All Gather (Line and Ring)
    - Width Sharded Reduce Scatter
    - Block Sharded Reduce Scatter
    - FullTensor bidirectional mode for sharded all-gather
      - Results in a 25% cycle count reduction for small all-gathers
    
    Additionally, over 1000 lines of code have been able to be deleted.

--- ttnn/cpp/ttnn/operations/ccl/all_gather/device/kernels/dataflow/worker_interleaved_ring_gather_send_writer.cpp
+++ ttnn/cpp/ttnn/operations/ccl/all_gather/device/kernels/dataflow/worker_interleaved_ring_gather_send_writer.cpp
@@ -5,12 +5,22 @@
 #include <cstdint>
 #include "dataflow_api.h"
 #include "ttnn/cpp/ttnn/operations/ccl/all_gather/device/kernels/dataflow/worker_ring_gather_utils.hpp"
+#include "ttnn/cpp/ttnn/operations/ccl/shared_with_host/sharded_tensor_addr_gen.hpp"
 
 void kernel_main() {
+    uint32_t arg_idx = 0;
+    const uint32_t dst_addr = get_arg_val<uint32_t>(arg_idx++);
+    const uint32_t eth_sender_l1_base_addr = get_arg_val<uint32_t>(arg_idx++);
+    const uint32_t eth_sender_l1_sem_addr = get_arg_val<uint32_t>(arg_idx++);
 
-    const uint32_t dst_addr = get_arg_val<uint32_t>(0);
-    const uint32_t eth_sender_l1_base_addr = get_arg_val<uint32_t>(1);
-    const uint32_t eth_sender_l1_sem_addr = get_arg_val<uint32_t>(2);
+    #ifdef SHARDED_MEM_LAYOUT
+    uint32_t output_shard_grid_nrows = get_arg_val<uint32_t>(arg_idx++);
+    const uint32_t * const output_shard_grid_row_map = reinterpret_cast<const uint32_t * const>(get_arg_addr(arg_idx));
+    arg_idx += output_shard_grid_nrows;
+    uint32_t output_shard_grid_ncols = get_arg_val<uint32_t>(arg_idx++);
+    const uint32_t * const output_shard_grid_col_map = reinterpret_cast<const uint32_t * const>(get_arg_addr(arg_idx));
+    arg_idx += output_shard_grid_ncols;
+    #endif
 
     constexpr bool dst_is_dram = get_compile_time_arg_val(0) == 1;
     constexpr uint32_t num_transfers = get_compile_time_arg_val(1);
@@ -35,18 +45,64 @@ void kernel_main() {
     constexpr uint32_t half_cb_n_pages = get_compile_time_arg_val(20);
     static_assert(half_cb_n_pages > rem_num_pages, "half_cb_n_pages must be greater than or equal to rem_num_pages");
 
-    constexpr uint32_t cb_id_in0 = tt::CB::c_in0;
-    #ifdef RM_INTERLEAVED
-    InterleavedAddrGen<dst_is_dram> d = {
-        .bank_base_address = dst_addr + output_start_addr_offset, .page_size = output_page_size};
-    #elif defined TILE_INTERLEAVED
-    const DataFormat in0_df = get_dataformat(cb_id_in0);
+    #ifdef SHARDED_MEM_LAYOUT
+    constexpr tt::tt_metal::TensorMemoryLayout output_tensor_memory_layout = static_cast<tt::tt_metal::TensorMemoryLayout>(get_compile_time_arg_val(21));
+    constexpr uint32_t output_tensor_shard_grid_height = get_compile_time_arg_val(22);
+    constexpr uint32_t output_tensor_shard_grid_width = get_compile_time_arg_val(23);
+    constexpr uint32_t output_tensor_shard_grid_start_y_logical = get_compile_time_arg_val(24);
+    constexpr uint32_t output_tensor_shard_grid_start_x_logical = get_compile_time_arg_val(25);
+    constexpr uint32_t output_tensor_shard_pages_per_shard_y = get_compile_time_arg_val(26);
+    constexpr uint32_t output_tensor_shard_pages_per_shard_x = get_compile_time_arg_val(27);
+    constexpr bool output_tensor_shard_grid_transposed = get_compile_time_arg_val(28) != 0;
+    #endif
 
-    const InterleavedAddrGenFast<dst_is_dram> d = {
-        .bank_base_address = dst_addr,
-        .page_size = output_page_size,
-        .data_format = in0_df
-    };
+    constexpr uint32_t cb_id_in0 = tt::CB::c_in0;
+    #ifdef ROW_MAJOR_LAYOUT
+        #ifdef INTERLEAVED_MEM_LAYOUT
+        InterleavedAddrGen<dst_is_dram> d = {
+            .bank_base_address = dst_addr + output_start_addr_offset, .page_size = output_page_size};
+        #elif defined SHARDED_MEM_LAYOUT
+            auto d = tt::tt_metal::address_generators::build_sharded_addr_gen<output_tensor_memory_layout>(
+                tt::tt_metal::address_generators::HarvestedWormholeWorkerToNocLookup(output_shard_grid_nrows, output_shard_grid_row_map, output_shard_grid_ncols, output_shard_grid_col_map),
+                tt::tt_metal::address_generators::DeviceShardSpecTypeGetter<output_tensor_memory_layout>::type(
+                    output_tensor_shard_pages_per_shard_y,
+                    output_tensor_shard_pages_per_shard_x,
+                    output_tensor_shard_grid_height,
+                    output_tensor_shard_grid_width,
+                    output_tensor_shard_grid_start_y_logical,
+                    output_tensor_shard_grid_start_x_logical,
+                    output_tensor_shard_grid_transposed
+                ),
+                output_page_size,
+                dst_addr
+            );
+            ASSSERT(false); // unimplemented and untested
+        #endif
+    #elif defined TILED_LAYOUT
+        #ifdef INTERLEAVED_MEM_LAYOUT
+        const DataFormat in0_df = get_dataformat(cb_id_in0);
+
+        const InterleavedAddrGenFast<dst_is_dram> d = {
+            .bank_base_address = dst_addr,
+            .page_size = output_page_size,
+            .data_format = in0_df
+        };
+        #elif defined SHARDED_MEM_LAYOUT
+            auto d = tt::tt_metal::address_generators::build_sharded_addr_gen<output_tensor_memory_layout>(
+                tt::tt_metal::address_generators::HarvestedWormholeWorkerToNocLookup(output_shard_grid_nrows, output_shard_grid_row_map, output_shard_grid_ncols, output_shard_grid_col_map),
+                tt::tt_metal::address_generators::DeviceShardSpecTypeGetter<output_tensor_memory_layout>::type(
+                    output_tensor_shard_pages_per_shard_y,
+                    output_tensor_shard_pages_per_shard_x,
+                    output_tensor_shard_grid_height,
+                    output_tensor_shard_grid_width,
+                    output_tensor_shard_grid_start_y_logical,
+                    output_tensor_shard_grid_start_x_logical,
+                    output_tensor_shard_grid_transposed
+                ),
+                output_page_size,
+                dst_addr
+            );
+        #endif
     #endif
 
     // Used to wait until eth sender has space available
@@ -66,7 +122,6 @@ void kernel_main() {
         for (uint32_t c = 0; c < num_full_chunks; ++c) {
             noc_semaphore_wait(writer_send_semaphore_addr_ptr, 1);
             noc_semaphore_set(writer_send_semaphore_addr_ptr, 0);
-            // TODO: Might be better to split this?
             write_and_send_chunk(output_page_idx, col_idx, row_idx, cb_id_in0, d, num_cols, num_rows, col_offset, row_offset, num_pages, page_size, eth_l1_sender_base_noc_addr, eth_l1_sender_semaphore_addr);
         }
     }
