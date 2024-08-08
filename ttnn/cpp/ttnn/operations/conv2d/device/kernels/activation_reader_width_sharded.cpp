c8277b745175efb6e99bf3b6688de911ea4f7aac
Sankar Manoj <smanoj@tenstorrent.com>
2024-08-08 05:25:46 -0400

    #0: Updated comments

--- ttnn/cpp/ttnn/operations/conv2d/device/kernels/activation_reader_width_sharded.cpp
+++ ttnn/cpp/ttnn/operations/conv2d/device/kernels/activation_reader_width_sharded.cpp
@@ -22,22 +22,25 @@ inline void print_pages(uint32_t l1_addr, uint32_t pagelen, uint32_t npages, uin
 }
 #endif
 
-template<int window_inner>
+//Only a part of the total channel depth (width) is used in one block.
+template<int window_height, int window_width>
 FORCE_INLINE void read_channels(uint32_t& l1_write_addr_act, const uint32_t act_l1_read_addr, const uint32_t reader_channel_idx,
         const uint32_t conv_act_c_bytes, const uint32_t conv_act_c_read_bytes, const uint32_t stride_h_bytes) {
 
     uint32_t act_l1_read_addr_plus_offset = act_l1_read_addr + (reader_channel_idx * conv_act_c_bytes);
-    // DPRINT<<"read_channels "<<reader_channel_idx<<"\n";
     #pragma GCC unroll 3
-    for(uint32_t outer = 0; outer < 3; outer++)
+    for(uint32_t outer = 0; outer < window_height; outer++)
     {
         uint32_t act_l1_read_addr_row_offset = act_l1_read_addr_plus_offset;
-        for (uint32_t inner = 0; inner < window_inner; inner++)
+        for (uint32_t inner = 0; inner < window_width; inner++)
         {
+            //Read the partial depth.
             noc_async_read_one_packet_with_state<true>(act_l1_read_addr_row_offset, l1_write_addr_act);
+            //Increment by full depth to go to the next pixel
             l1_write_addr_act += conv_act_c_read_bytes;
             act_l1_read_addr_row_offset += conv_act_c_bytes;
         }
+        //Go to the next row
         act_l1_read_addr_plus_offset += stride_h_bytes;
     }
 }
@@ -77,21 +80,22 @@ void kernel_main() {
 
     uint32_t this_core_x = get_arg_val<uint32_t>(i); i+=1;
     uint32_t this_core_y = get_arg_val<uint32_t>(i); i+=1;
+
+    //Num of cols of compute cores. (Total Cores, not active cores.)
     uint32_t num_cores_x = get_arg_val<uint32_t>(i); i+=1;
+    //X and Y lookup are independant.
+    //X Lookup table for translating logical to physical cores.
     tt_l1_ptr uint32_t *act_mcast_x_lookup  = (tt_l1_ptr uint32_t*)(get_arg_addr(i));
     i+=num_cores_x;
+    //Y lookup.
     tt_l1_ptr uint32_t *act_mcast_y_lookup  = (tt_l1_ptr uint32_t*)(get_arg_addr(i));
     // DPRINT<<"Act Params L1 :  "<<conv_act_size_w<<"  "<<conv_act_c_read_bytes<<"  "<<weight_size_h<<"  "<<weight_size_w<<"  "<<act_block_h_datums<<"  "<<act_block_num_tiles<<ENDL()<<
     // "L2  "<<act_w_num_outer<<"  "<<act_num_blocks_w<<"  "<<act_mcast_sender_semaphore_addr<<"  "<<act_mcast_receiver_semaphore_addr<<"  "<<act_mcast_dest_noc_start_x<<
     // "L3  "<<act_mcast_dest_noc_start_y<<"  "<<act_mcast_dest_noc_end_x<<"  "<<act_mcast_dest_noc_end_y<<"  "<<act_mcast_sender_size_bytes<<"  "<<act_mcast_num_cores<<ENDL();
 
-    // uint32_t act_mcast_dest_noc_end_x   = get_arg_val<uint32_t>(i); i+=1;
-    // uint32_t act_mcast_dest_noc_end_y   = get_arg_val<uint32_t>(i); i+=1;
-    // uint32_t act_mcast_sender_id        = get_arg_val<uint32_t>(i); i+=1;
-    // uint32_t act_mcast_sender_noc_x     = get_arg_val<uint32_t>(i); i+=1;
 
+    //Equivalent to Core Index.
     uint32_t act_mcast_sender_id = this_core_x+(num_cores_x*this_core_y) ;
-    // tt_l1_ptr uint32_t *act_mcast_sender_noc_y  = (tt_l1_ptr uint32_t*)(get_arg_addr(i));
 
     constexpr uint32_t cb_id_act = tt::CB::c_in0;
     constexpr uint32_t cb_id_weight = tt::CB::c_in1;
@@ -129,13 +133,15 @@ void kernel_main() {
 
     uint64_t act_mcast_receiver_semaphore_noc_addr = act_multicast_noc_addr | act_mcast_receiver_semaphore_addr;
 
-    // TODO: need to make the read coalescing optimization cleaner
-    // currently works for the case of num_coalesced_reads == weight_size_w since these reads are contiguous on both src/dst side
+    //Compute is divided along the width to reduce the size of CBs.
+    //Only a part of the width on each core is used in one block.
+    //Bytes read is conv_act_c_read_bytes.
+    //Striding to next pixel happens using conv_act_c_bytes.
     constexpr uint32_t conv_act_c_bytes = conv_act_c_read_bytes * act_num_blocks_w;
+
+    //Striding to next row happens using stride_h_bytes
     constexpr uint32_t stride_h_bytes = (conv_act_size_w ) * conv_act_c_bytes;
 
-    // Fully create act matrix and tilize it before mcast
-    // set_state uses just x/y from the get_noc_addr, addr is ignored
     uint32_t act_l1_read_addr = get_read_ptr(cb_id_sharded_act);
     noc_async_read_one_packet_set_state(get_noc_addr(act_l1_read_addr), conv_act_c_read_bytes);
 
@@ -150,23 +156,22 @@ void kernel_main() {
         // DPRINT<<"L1 Write Addr "<<l1_write_addr_act<<"\n";
 
 
-        // #pragma GCC unroll 4 // didn't seem to help (neutral), manual unroll 2x perf drop
         for (uint32_t bh = 0; bh < act_block_h_datums / 2; bh++) {
             uint32_t two_reader_indices = packed_reader_indices_ptr[reader_idx];
-            read_channels<weight_size_h>(l1_write_addr_act, act_l1_read_addr, two_reader_indices & 0xffff, conv_act_c_bytes, conv_act_c_read_bytes, stride_h_bytes);
-            read_channels<weight_size_h>(l1_write_addr_act, act_l1_read_addr, two_reader_indices >> 16   , conv_act_c_bytes, conv_act_c_read_bytes, stride_h_bytes);
+            read_channels<weight_size_h,weight_size_w>(l1_write_addr_act, act_l1_read_addr, two_reader_indices & 0xffff, conv_act_c_bytes, conv_act_c_read_bytes, stride_h_bytes);
+            read_channels<weight_size_h,weight_size_w>(l1_write_addr_act, act_l1_read_addr, two_reader_indices >> 16   , conv_act_c_bytes, conv_act_c_read_bytes, stride_h_bytes);
 
             reader_idx++;
         }
 
+        //After reading one block, increment the starting read pointer by the width of the block.
+        //Next read uses the next set of channels.
         act_l1_read_addr +=conv_act_c_read_bytes;
-        // // incrementing num issued in one shot is actually slower
-        // // noc_async_read_inc_num_issued(num_issued_reads_per_block); // "false" on read
+
         noc_async_read_barrier();
         cb_push_back(cb_id_act_row_major_bfloat16, act_block_num_tiles);
 
 
-        // DPRINT<<"MCast "<<act_w_num_outer<<"  "<<act_mcast_sender_id<<"num cores "<<act_mcast_num_cores<<"\n";
         // Round robin self-mcast and receive tilized act matrix in cb_id_act
         // Compute should function like regular mm
 
@@ -177,7 +182,6 @@ void kernel_main() {
                 // MCAST SENDER: send entire tilized input to other cores in column
                 // wait until all act mcast destinations have atomically incremented the act semaphore_addr (i.e. its value should be act_mcast_num_dests), then reset
                 // the semaphore_addr value back to zero for the next block
-                // DPRINT<<"Sender Core "<<this_core_x<<","<<this_core_y<<":"<<*(uint32_t *)act_mcast_sender_semaphore_addr_ptr<<"\n\n";
 
                 noc_semaphore_wait_min(act_mcast_sender_semaphore_addr_ptr, act_mcast_num_cores-1);
                 noc_semaphore_set(act_mcast_sender_semaphore_addr_ptr, 0);
@@ -197,7 +201,6 @@ void kernel_main() {
 
                 // Note: no need for write barrier, since these two multicasts are done on the same noc id, same vc, same cmd_buf
                 // Also, this only works because we are setting VCs statically (using NOC_CMD_STATIC_VC).
-                // DPRINT<<"Core "<<this_core_x<<","<<this_core_y<<": act_mcast_sender_semaphore_valid_addr"<<*(uint32_t *)act_mcast_sender_semaphore_valid_addr<<"\n\n";
 
                 // // We should also multicast VALID flag to destinations for receiver semaphore
                 noc_semaphore_set_multicast_loopback_src(act_mcast_sender_semaphore_valid_addr, act_mcast_receiver_semaphore_noc_addr, 16 , false, false);
