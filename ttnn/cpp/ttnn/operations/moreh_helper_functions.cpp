b4256baf79cf3677a3391312e9e23b32d8e4067b
hschoi <hschoi4448@gmail.com>
2024-08-08 06:03:34 +0000

    #10826: add missing spdx copyright text

--- ttnn/cpp/ttnn/operations/moreh_helper_functions.cpp
+++ ttnn/cpp/ttnn/operations/moreh_helper_functions.cpp
@@ -1,4 +1,10 @@
 
+/*
+ * SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include "ttnn/cpp/ttnn/operations/moreh_helper_functions.hpp"
 
 #include "common/constants.hpp"
