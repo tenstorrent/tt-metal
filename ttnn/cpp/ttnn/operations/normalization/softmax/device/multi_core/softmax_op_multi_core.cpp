369fa626f225c79b3ddf5b9992752b832b519f8d
pjanevski <pjanevski@tenstorrent.com>
2024-08-08 12:06:01 +0000

    #111: Softmax works

--- ttnn/cpp/ttnn/operations/normalization/softmax/device/multi_core/softmax_op_multi_core.cpp
+++ ttnn/cpp/ttnn/operations/normalization/softmax/device/multi_core/softmax_op_multi_core.cpp
@@ -173,8 +173,6 @@ operation::ProgramWithCallbacks scale_mask_softmax_multi_core(
         softmax_defines["CAUSAL_MASK"] = "1";
     }
 
-    // TODO(pjanevski): finished here
-
     auto reader_kernels_id = CreateKernel(
         program,
         "ttnn/cpp/ttnn/operations/normalization/softmax/device/kernels/dataflow/reader_unary_interleaved_sm.cpp",
