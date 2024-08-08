06cf634d9b94314b91d90c72451515fcd37b259d
hschoi <hschoi4448@gmail.com>
2024-08-08 09:16:25 +0000

    #10826: update moreh_embedding doc

--- ttnn/cpp/ttnn/operations/moreh_embedding/moreh_embedding_pybind.hpp
+++ ttnn/cpp/ttnn/operations/moreh_embedding/moreh_embedding_pybind.hpp
@@ -15,35 +15,23 @@ namespace py = pybind11;
 
 void py_module(py::module& module) {
     const auto doc =
-        R"doc(embedding(inxput_tensor: ttnn.Tensor, weight: ttnn.Tensor, *, padding_idx: Optional[int] = None, layout: ttnn.Layout = ttnn.ROW_MAJOR_LAYOUT, memory_config: Optional[ttnn.MemoryConfig] = None) -> ttnn.Tensor
+        R"doc(moreh_embedding(input: ttnn.Tensor, weight: ttnn.Tensor, *, max_norm: Optional[float] = None, norm_type = float = 2.0 , output: Optional[ttnn.Tensor] = None, dtype: Optional[ttnn.DataType] = None, memory_config: Optional[ttnn.MemoryConfig] = None, compute_kernel_config: Optional[ttnn.MemoryConfig] = None) -> ttnn.Tensor
 
-            Retrieves word embeddings using input_tensor. The input_tensor is a list of indices, and the embedding matrix, and the output is the corresponding word embeddings.
+            Retrieves word embeddings using input. The input is a list of indices, and the embedding matrix, and the output is the corresponding word embeddings.
 
             Args:
-                * :attr:`input_tensor`: the indices ttnn.Tensor
+                * :attr:`input`: the indices ttnn.Tensor
                 * :attr:`weight`: the embeddings ttnn.Tensor that correspond to the indices ttnn.Tensor
 
             Keyword Args:
+                * :attr:`max_norm`: If given, each embedding vector with norm larger than max_norm is renormalized to have norm max_norm. Note: this will modify weight in-place.
+                * :attr:`norm_type`: The p of the p-norm to compute for the max_norm option. Default 2.
+                * :attr:`output`: the optional output tensor. Default is None.
                 * :attr:`dtype`: the data type for the output tensor. Default is None.
-                * :attr:`output_tensor`: the optional output tensor. Default is None.
                 * :attr:`memory_config`: the memory configuration of the output tensor. Default is input tensor memory config.
+                * :attr:`compute_kernel_config`: the compute kernel configuration for the op. If not provided, the default configuration of the op is used.
                 * :attr:`queue_id`: the command queue id. Default is 0.
-
-            Example:
-                >>> device_id = 0
-                >>> device = ttnn.open_device(device_id=device_id)
-                >>> input_tensor = ttnn.to_device(ttnn.from_torch(torch.tensor([[1, 2, 4, 5], [4, 3, 2, 9]]), dtype=ttnn.uint32), device)
-                >>> # an embedding matrix containing 10 tensors of size 4
-                >>> weight = ttnn.to_device(ttnn.from_torch(torch.rand(10, 4), dtype=ttnn.bfloat16), device)
-                >>> ttnn.embedding(input_tensor, weight)
-                ttnn.Tensor([ [[1, 0.106445, 0.988281, 0.59375],
-                    [0.212891, 0.964844, 0.199219, 0.996094],
-                    [3.78362e-38, 0, 7.89785e-39, 0],
-                    [8.04479e-38, 0, 1.25815e-38, 0]],
-                [[2.71833e-38, 0, 3.59995e-38, 0],
-                    [7.60398e-38, 0, 1.83671e-38, 0],
-                    [2.22242e-38, 0, 1.88263e-38, 0],
-                    [1.35917e-38, 0, 4.49994e-39, 0]]], dtype=bfloat16 ))doc";
+            )doc";
     using OperationType = decltype(ttnn::moreh_embedding);
     bind_registered_operation(
         module,
