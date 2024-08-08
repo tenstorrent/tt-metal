d92e7f169b78bec30de33599e1453b93924239c9
Sankar Manoj <smanoj@tenstorrent.com>
2024-08-08 02:38:33 -0400

    #9757: Add license header

--- ttnn/cpp/ttnn/operations/sliding_window/halo/halo.cpp
+++ ttnn/cpp/ttnn/operations/sliding_window/halo/halo.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 #include "halo.hpp"
 #include "device/halo_device_operation.hpp"
 namespace ttnn::operations::sliding_window::halo
