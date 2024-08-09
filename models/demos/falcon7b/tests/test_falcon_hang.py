585076ff734e87ba466e4117c5052d95a9a0bb99
Sofija Jovic <sjovic@tenstorrent.com>
2024-08-09 16:18:47 +0000

    #0: Add determinism checks to didt tests

--- models/demos/falcon7b/tests/test_falcon_hang.py
+++ models/demos/falcon7b/tests/test_falcon_hang.py
@@ -12,7 +12,7 @@ import torch
 
 
 @pytest.mark.parametrize("num_devices", [1, 2, 8], ids=["1chips", "2chips", "8chips"])
-def test_reproduce_lm_head_nd_32(all_devices, num_devices, use_program_cache):
+def test_reproduce_lm_head_nd_32(all_devices, num_devices, use_program_cache, determinism_check_enabled=False):
     devices = []
     if num_devices == 8:
         devices = get_devices_for_t3000(all_devices, num_devices)
@@ -76,7 +76,10 @@ def test_reproduce_lm_head_nd_32(all_devices, num_devices, use_program_cache):
         packer_l1_acc=True,
     )
 
+    num_nd_outputs = [0] * num_devices
     out = []
+    reference_out = []
+
     for device_idx in range(num_devices):
         out.append(
             ttnn.matmul(
@@ -89,7 +92,12 @@ def test_reproduce_lm_head_nd_32(all_devices, num_devices, use_program_cache):
             )
         )
 
+    if determinism_check_enabled:
+        for device_idx in range(num_devices):
+            reference_out.append(tt2torch_tensor(out[device_idx]))
+
     for i in range(100000):
+        # run matmul on all devices
         for device_idx in range(num_devices):
             out[device_idx].deallocate(True)
             out[device_idx] = ttnn.matmul(
@@ -101,6 +109,7 @@ def test_reproduce_lm_head_nd_32(all_devices, num_devices, use_program_cache):
                 compute_kernel_config=wh_compute_kernel_config,
             )
 
+        # synchronize
         for device_idx in range(num_devices):
             if num_devices != 1:
                 if num_devices == 2:
@@ -128,7 +137,25 @@ def test_reproduce_lm_head_nd_32(all_devices, num_devices, use_program_cache):
             else:
                 print("End single device sync")
 
-        logger.debug(f"Iteration = {i}")
+        # check if the output matches the first run output
+        if determinism_check_enabled:
+            for device_idx in range(num_devices):
+                pt_out = tt2torch_tensor(out[device_idx])
+                if torch.equal(reference_out[device_idx], pt_out):
+                    logger.info(f"Device {device_idx} PCC: 1.0")
+                else:
+                    # for determinism check, we avoid calling comp_pcc func as it is heavy and with too many operations,
+                    # part of the code that replaces nans/infs with zeros starts leaking memory, even if deallocation is forced,
+                    # so we call it only in case we see tensors are not equal
+                    _, pcc = comp_pcc(reference_out[device_idx], pt_out)
+                    logger.info(f"Device {device_idx} PCC: {pcc}")
+                    num_nd_outputs[device_idx] += 1
+
+        logger.info(f"Iteration = {i}")
+
+    if determinism_check_enabled:
+        for device_idx in range(num_devices):
+            logger.info(f"Number of non-deterministic outputs on device {device_idx} is {num_nd_outputs[device_idx]}")
 
 
 @pytest.mark.parametrize(
@@ -170,3 +197,58 @@ def test_specific_chip_lm_head_nd_32_t3000(all_devices, logical_chip_index, use_
     target_device = devices[logical_chip_index]
     devices = [target_device]
     test_reproduce_lm_head_nd_32(devices, 1, use_program_cache)
+
+
+@pytest.mark.parametrize("num_devices", [1, 2, 8], ids=["1chips", "2chips", "8chips"])
+def test_determinism(
+    all_devices,
+    num_devices,
+    use_program_cache,
+):
+    test_reproduce_lm_head_nd_32(all_devices, num_devices, use_program_cache, determinism_check_enabled=True)
+
+
+@pytest.mark.parametrize(
+    "logical_chip_index",
+    [0, 1, 2, 3, 4, 5, 6, 7],
+    ids=[
+        "logical_chip0",
+        "logical_chip1",
+        "logical_chip2",
+        "logical_chip3",
+        "logical_chip4",
+        "logical_chip5",
+        "logical_chip6",
+        "logical_chip7",
+    ],
+)
+def test_determinism_specific_chip(
+    all_devices,
+    logical_chip_index,
+    use_program_cache,
+):
+    num_devices_t3000 = 8
+    if len(all_devices) != num_devices_t3000:
+        pytest.skip("Test is only valid for t3000 machines")
+    devices = get_devices_for_t3000(all_devices, num_devices_t3000)
+
+    logical_chip_id_to_coordinates = [None] * num_devices_t3000
+    logical_chip_id_to_coordinates[0] = (1, 0)
+    logical_chip_id_to_coordinates[1] = (0, 0)
+    logical_chip_id_to_coordinates[2] = (0, 1)
+    logical_chip_id_to_coordinates[3] = (1, 1)
+    logical_chip_id_to_coordinates[4] = (2, 1)
+    logical_chip_id_to_coordinates[5] = (3, 1)
+    logical_chip_id_to_coordinates[6] = (3, 0)
+    logical_chip_id_to_coordinates[7] = (2, 0)
+
+    print(
+        "Selecting logical device id: ",
+        logical_chip_index,
+        " eth coordinates: ",
+        logical_chip_id_to_coordinates[logical_chip_index],
+    )
+    target_device = devices[logical_chip_index]
+    devices = [target_device]
+
+    test_reproduce_lm_head_nd_32(devices, 1, use_program_cache, determinism_check_enabled=True)
--- models/experimental/falcon_7b/tests/test_reproduce_hang_matmul.py
+++ models/experimental/falcon_7b/tests/test_reproduce_hang_matmul.py
@@ -6,20 +6,22 @@ import ttnn
 
 import pytest
 
-import time
-
 import tt_lib as ttl
 from models.utility_functions import (
+    comp_pcc,
     torch2tt_tensor,
     get_devices_for_t3000,
+    tt2torch_tensor,
 )
 import torch
+
+FF1_HANG_PARAMETRIZATION = (1024, 4608, 18432, 4, 72, 3, 1, 8, 100000)
 
 
 # Used to reproduce issue #8665 with matmul 2D (Falcon 7b matmuls)
 @pytest.mark.parametrize(
     "seq_len, inner_dim, weights_n, per_core_M, per_core_N, in_block_w, out_subblock_h, out_subblock_w, loop_count",
-    ((1024, 4608, 18432, 4, 72, 3, 1, 8, 100000), (1024, 4608, 18432, 4, 72, 3, 1, 1, 100000)),
+    (FF1_HANG_PARAMETRIZATION, (1024, 4608, 18432, 4, 72, 3, 1, 1, 100000)),
     ids=["ff1-hang", "ff1-pass"],
 )
 @pytest.mark.parametrize("num_devices", [1, 2, 8], ids=["1chips", "2chips", "8chips"])
@@ -36,6 +38,7 @@ def test_reproduce_matmul_2d_hang(
     out_subblock_w,
     loop_count,
     use_program_cache,
+    determinism_check_enabled=False,
 ):
     torch.manual_seed(1234)
 
@@ -119,7 +122,10 @@ def test_reproduce_matmul_2d_hang(
         packer_l1_acc=True,
     )
 
+    num_nd_outputs = [0] * num_devices
     out = []
+    reference_out = []
+
     # First run for a reference output
     for device_idx in range(num_devices):
         out.append(
@@ -133,10 +139,13 @@ def test_reproduce_matmul_2d_hang(
             )
         )
 
-    start_time = time.time()
+    if determinism_check_enabled:
+        for device_idx in range(num_devices):
+            reference_out.append(tt2torch_tensor(out[device_idx]))
 
     # loop_count iterations to test determinism/hang
     for i in range(loop_count):
+        # run matmul on all devices
         for device_idx in range(num_devices):
             out[device_idx].deallocate(True)
             out[device_idx] = ttnn.matmul(
@@ -148,6 +157,7 @@ def test_reproduce_matmul_2d_hang(
                 compute_kernel_config=compute_config,
             )
 
+        # synchronize
         for device_idx in range(num_devices):
             if num_devices != 1:
                 if num_devices == 2:
@@ -175,7 +185,25 @@ def test_reproduce_matmul_2d_hang(
             else:
                 print("End single device sync")
 
-        logger.debug(f"Iteration = {i}, done")
+        # check if the output matches the first run output
+        if determinism_check_enabled:
+            for device_idx in range(num_devices):
+                pt_out = tt2torch_tensor(out[device_idx])
+                if torch.equal(reference_out[device_idx], pt_out):
+                    logger.info(f"Device {device_idx} PCC: 1.0")
+                else:
+                    # for determinism check, we avoid calling comp_pcc func as it is heavy and with too many operations,
+                    # part of the code that replaces nans/infs with zeros starts leaking memory, even if deallocation is forced,
+                    # so we call it only in case we see tensors are not equal
+                    _, pcc = comp_pcc(reference_out[device_idx], pt_out)
+                    logger.info(f"Device {device_idx} PCC: {pcc}")
+                    num_nd_outputs[device_idx] += 1
+
+        logger.info(f"Iteration = {i}, done")
+
+    if determinism_check_enabled:
+        for device_idx in range(num_devices):
+            logger.info(f"Number of non-deterministic outputs on device {device_idx} is {num_nd_outputs[device_idx]}")
 
     for device_idx in range(num_devices):
         out[device_idx].deallocate(True)
@@ -224,3 +252,118 @@ def test_specific_chip_reproduce_matmul_2d_hang_t3000(all_devices, logical_chip_
     devices = [target_device]
 
     test_reproduce_matmul_2d_hang(1, devices, 1024, 4608, 18432, 4, 72, 3, 1, 8, 100000, use_program_cache)
+
+
+@pytest.mark.parametrize(
+    "seq_len, inner_dim, weights_n, per_core_M, per_core_N, in_block_w, out_subblock_h, out_subblock_w, loop_count",
+    (FF1_HANG_PARAMETRIZATION,),
+    ids=[
+        "ff1-hang",
+    ],
+)
+@pytest.mark.parametrize("num_devices", [1, 2, 8], ids=["1chips", "2chips", "8chips"])
+def test_determinism(
+    num_devices,
+    all_devices,
+    seq_len,
+    inner_dim,
+    weights_n,
+    per_core_M,
+    per_core_N,
+    in_block_w,
+    out_subblock_h,
+    out_subblock_w,
+    loop_count,
+    use_program_cache,
+):
+    test_reproduce_matmul_2d_hang(
+        num_devices,
+        all_devices,
+        seq_len,
+        inner_dim,
+        weights_n,
+        per_core_M,
+        per_core_N,
+        in_block_w,
+        out_subblock_h,
+        out_subblock_w,
+        loop_count,
+        use_program_cache,
+        determinism_check_enabled=True,
+    )
+
+
+@pytest.mark.parametrize(
+    "logical_chip_index",
+    [0, 1, 2, 3, 4, 5, 6, 7],
+    ids=[
+        "logical_chip0",
+        "logical_chip1",
+        "logical_chip2",
+        "logical_chip3",
+        "logical_chip4",
+        "logical_chip5",
+        "logical_chip6",
+        "logical_chip7",
+    ],
+)
+@pytest.mark.parametrize(
+    "seq_len, inner_dim, weights_n, per_core_M, per_core_N, in_block_w, out_subblock_h, out_subblock_w, loop_count",
+    (FF1_HANG_PARAMETRIZATION,),
+    ids=[
+        "ff1-hang",
+    ],
+)
+def test_determinism_specific_chip(
+    all_devices,
+    logical_chip_index,
+    seq_len,
+    inner_dim,
+    weights_n,
+    per_core_M,
+    per_core_N,
+    in_block_w,
+    out_subblock_h,
+    out_subblock_w,
+    loop_count,
+    use_program_cache,
+):
+    num_devices_t3000 = 8
+    if len(all_devices) != num_devices_t3000:
+        pytest.skip("Test is only valid for t3000 machines")
+    devices = get_devices_for_t3000(all_devices, num_devices_t3000)
+
+    logical_chip_id_to_coordinates = [None] * num_devices_t3000
+    logical_chip_id_to_coordinates[0] = (1, 0)
+    logical_chip_id_to_coordinates[1] = (0, 0)
+    logical_chip_id_to_coordinates[2] = (0, 1)
+    logical_chip_id_to_coordinates[3] = (1, 1)
+    logical_chip_id_to_coordinates[4] = (2, 1)
+    logical_chip_id_to_coordinates[5] = (3, 1)
+    logical_chip_id_to_coordinates[6] = (3, 0)
+    logical_chip_id_to_coordinates[7] = (2, 0)
+
+    print(
+        "Selecting logical device id: ",
+        logical_chip_index,
+        " coordinates: ",
+        logical_chip_id_to_coordinates[logical_chip_index],
+    )
+    target_device = devices[logical_chip_index]
+    devices = [target_device]
+
+    test_reproduce_matmul_2d_hang(
+        1,
+        devices,
+        seq_len,
+        inner_dim,
+        weights_n,
+        per_core_M,
+        per_core_N,
+        in_block_w,
+        out_subblock_h,
+        out_subblock_w,
+        loop_count,
+        use_program_cache,
+        determinism_check_enabled=True,
+    )
