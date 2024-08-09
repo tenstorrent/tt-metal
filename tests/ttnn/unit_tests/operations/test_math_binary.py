15d07b26e6e793b2b152300c4765b0409761530f
Anasuya G Nair <mcw-anair@ext.tenstorrent.com>
2024-08-09 11:55:43 +0530

    #10882: Update Golden Function for test_math, test_math_binary, test_relational (#11145)
    * #10882: Update test_math.py
    
    * #10882: Update math_binary and relational

--- tests/ttnn/unit_tests/operations/test_math_binary.py
+++ tests/ttnn/unit_tests/operations/test_math_binary.py
@@ -13,13 +13,14 @@ from tests.ttnn.utils_for_testing import assert_with_pcc
 from models.utility_functions import torch_random
 
 
-def run_math_binary_test(device, h, w, ttnn_function, torch_function, pcc=0.9999):
+def run_math_binary_test(device, h, w, ttnn_function, pcc=0.9999):
     torch.manual_seed(0)
     torch_input_tensor_a = torch.rand((h, w), dtype=torch.bfloat16)
     torch.manual_seed(42)
     torch_input_tensor_b = torch.rand((h, w), dtype=torch.bfloat16)
 
-    torch_output_tensor = torch_function(torch_input_tensor_a, torch_input_tensor_b)
+    golden_function = ttnn.get_golden_function(ttnn_function)
+    torch_output_tensor = golden_function(torch_input_tensor_a, torch_input_tensor_b)
 
     input_tensor_a = ttnn.from_torch(torch_input_tensor_a, layout=ttnn.TILE_LAYOUT, device=device)
     input_tensor_b = ttnn.from_torch(torch_input_tensor_b, layout=ttnn.TILE_LAYOUT, device=device)
@@ -35,14 +36,15 @@ def run_math_binary_test(device, h, w, ttnn_function, torch_function, pcc=0.9999
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_hypot_a(device, h, w):
-    run_math_binary_test(device, h, w, ttnn.hypot, torch.hypot)
+    run_math_binary_test(device, h, w, ttnn.hypot)
 
 
-def run_math_binary_test_atan2(device, h, w, a1, a2, b1, b2, ttnn_function, torch_function, pcc=0.9999):
+def run_math_binary_test_atan2(device, h, w, a1, a2, b1, b2, ttnn_function, pcc=0.9999):
     torch_input_tensor_a = torch.linspace(a1, a2, steps=h * w, dtype=torch.bfloat16).reshape((h, w))
     torch_input_tensor_b = torch.linspace(b1, b2, steps=h * w, dtype=torch.bfloat16).reshape((h, w))
 
-    torch_output_tensor = torch_function(torch_input_tensor_a, torch_input_tensor_b)
+    golden_function = ttnn.get_golden_function(ttnn_function)
+    torch_output_tensor = golden_function(torch_input_tensor_a, torch_input_tensor_b)
 
     input_tensor_a = ttnn.from_torch(torch_input_tensor_a, layout=ttnn.TILE_LAYOUT, device=device)
     input_tensor_b = ttnn.from_torch(torch_input_tensor_b, layout=ttnn.TILE_LAYOUT, device=device)
@@ -58,22 +60,23 @@ def run_math_binary_test_atan2(device, h, w, a1, a2, b1, b2, ttnn_function, torc
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_atan2_a(device, h, w):
-    run_math_binary_test_atan2(device, h, w, 20, 40, 40, 80, ttnn.atan2, torch.atan2)
+    run_math_binary_test_atan2(device, h, w, 20, 40, 40, 80, ttnn.atan2)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_atan2_b(device, h, w):
-    run_math_binary_test_atan2(device, h, w, -1, 1, -1, 1, ttnn.atan2, torch.atan2)
+    run_math_binary_test_atan2(device, h, w, -1, 1, -1, 1, ttnn.atan2)
 
 
-def run_math_binary_test_range(device, h, w, ttnn_function, torch_function, low, high, pcc=0.9999):
+def run_math_binary_test_range(device, h, w, ttnn_function, low, high, pcc=0.9999):
     torch.manual_seed(0)
     torch_input_tensor_a = torch_random((h, w), low, high, dtype=torch.bfloat16)
     torch.manual_seed(42)
     torch_input_tensor_b = torch_random((h, w), low, high, dtype=torch.bfloat16)
 
-    torch_output_tensor = torch_function(torch_input_tensor_a, torch_input_tensor_b)
+    golden_function = ttnn.get_golden_function(ttnn_function)
+    torch_output_tensor = golden_function(torch_input_tensor_a, torch_input_tensor_b)
 
     input_tensor_a = ttnn.from_torch(torch_input_tensor_a, layout=ttnn.TILE_LAYOUT, device=device)
     input_tensor_b = ttnn.from_torch(torch_input_tensor_b, layout=ttnn.TILE_LAYOUT, device=device)
@@ -86,17 +89,13 @@ def run_math_binary_test_range(device, h, w, ttnn_function, torch_function, low,
     assert_with_pcc(torch_output_tensor, output_tensor, pcc)
 
 
-def torch_squared_difference(x, y, *args, **kwargs):
-    return torch.square(torch.sub(x, y))
-
-
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_squared_difference(device, h, w):
-    run_math_binary_test_range(device, h, w, ttnn.squared_difference, torch_squared_difference, -100, 100)
+    run_math_binary_test_range(device, h, w, ttnn.squared_difference, -100, 100)
 
 
 @pytest.mark.parametrize("h", [64])
 @pytest.mark.parametrize("w", [128])
 def test_hypot(device, h, w):
-    run_math_binary_test_range(device, h, w, ttnn.hypot, torch.hypot, 0, 100)
+    run_math_binary_test_range(device, h, w, ttnn.hypot, 0, 100)
