362653a737014d78665d7de9e5491b25775eccae
Andrija Malbasa <amalbasa@tenstorrent.com>
2024-08-09 12:56:11 +0200

    Concat_bw fix (#11116)
    * #11110: Concat_bw fix and sweeps added
    
    * #11110: Added new comparison func
    
    * #11110: Changed args-gen function
    
    * #10147: Reformating after rebaseing on main
    
    * #11110: Addded golden function to ttnn_pytorch_ops.py
    
    * #11110: Reformating

--- tests/ttnn/python_api_testing/sweep_tests/op_map.py
+++ tests/ttnn/python_api_testing/sweep_tests/op_map.py
@@ -897,4 +897,8 @@ op_map = {
         "tt_op": ttnn_ops.eltwise_bitwise_or,
         "pytorch_op": pytorch_ops.bitwise_or,
     },
+    "concat-bw": {
+        "tt_op": ttnn_ops.concat_bw,
+        "pytorch_op": ttnn_pytorch_ops.concat_bw,
+    },
 }
