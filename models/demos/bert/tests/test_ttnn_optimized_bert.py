56e503c7bf1abdd7d39c18f7edc6cb5c30b8402d
KalaivaniMCW <kbaskar@tenstorrent.com>
2024-08-09 13:46:35 +0000

    #11241: Replace tt_lib in models/demos/bert and falcon7b_common

--- models/demos/bert/tests/test_ttnn_optimized_bert.py
+++ models/demos/bert/tests/test_ttnn_optimized_bert.py
@@ -10,7 +10,6 @@ import transformers
 from models.demos.bert.reference import torch_bert
 from models.demos.bert.tt import ttnn_optimized_sharded_bert
 
-import tt_lib
 import ttnn
 from ttnn.model_preprocessing import preprocess_model_parameters
 
@@ -25,7 +24,7 @@ from models.utility_functions import skip_for_wormhole_b0
 def test_bert_for_question_answering(device, use_program_cache, model_name, batch_size, sequence_size):
     torch.manual_seed(1234)
 
-    tt_lib.device.EnableMemoryReports()
+    ttnn.experimental.device.EnableMemoryReports()
 
     config = transformers.BertConfig.from_pretrained(model_name)
 
