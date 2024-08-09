84af68b01c4601d6e921073c68db585894bad78f
mtairum <mtairum@tenstorrent.com>
2024-08-09 18:32:29 +0000

    Revert "#11061: Moved Mistral perf test before mamba"
    This reverts commit 9f55bf002cac5da8b47758258b1025a6dcbab5b4.

--- tests/scripts/run_performance.sh
+++ tests/scripts/run_performance.sh
@@ -33,11 +33,11 @@ run_perf_models_llm_javelin() {
 
     env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/demos/falcon7b_common/tests -m $test_marker
 
-    env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/demos/wormhole/mistral7b/tests -m $test_marker
-
     if [ "$tt_arch" == "wormhole_b0" ]; then
         env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/demos/wormhole/mamba/tests -m $test_marker --timeout=360
     fi
+
+    env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/demos/wormhole/mistral7b/tests -m $test_marker
 
     ## Merge all the generated reports
     env python models/perf/merge_perf_results.py
