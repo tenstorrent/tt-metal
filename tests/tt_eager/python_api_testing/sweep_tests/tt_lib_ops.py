702bbec850644432111258b96855f7003157b6a0
“Nenad <npetrovic@tenstorrent.com>
2024-08-12 21:06:20 +0000

    #10147: Migrated div_no_nan to TTNN

--- tests/tt_eager/python_api_testing/sweep_tests/tt_lib_ops.py
+++ tests/tt_eager/python_api_testing/sweep_tests/tt_lib_ops.py
@@ -862,43 +862,6 @@ def eltwise_round(
 
 
 @setup_host_and_device
-def eltwise_div_no_nan(
-    x,
-    y,
-    *args,
-    device,
-    dtype,
-    layout,
-    input_mem_config,
-    output_mem_config,
-    **kwargs,
-):
-    t0 = setup_tt_tensor(x, device, layout[0], input_mem_config[0], dtype[0])
-    t1 = setup_tt_tensor(y, device, layout[1], input_mem_config[1], dtype[1])
-    t2 = ttnn.div_no_nan(t0, t1, memory_config=output_mem_config)
-
-    return tt2torch_tensor(t2)
-
-
-@setup_host_and_device
-def eltwise_unary_div_no_nan(
-    x,
-    *args,
-    value,
-    device,
-    dtype,
-    layout,
-    input_mem_config,
-    output_mem_config,
-    **kwargs,
-):
-    t0 = setup_tt_tensor(x, device, layout[0], input_mem_config[0], dtype[0])
-    t1 = ttnn.div_no_nan(t0, value, memory_config=output_mem_config)
-
-    return tt2torch_tensor(t1)
-
-
-@setup_host_and_device
 def eltwise_unary_rdiv_trunc(
     x,
     *args,
