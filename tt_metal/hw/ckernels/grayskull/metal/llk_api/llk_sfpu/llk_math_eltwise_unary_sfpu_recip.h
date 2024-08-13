85961e498fd2a4ee9dd98591a660f562b0bbbfc7
Radomir Djogo <159184120+rdjogoTT@users.noreply.github.com>
2024-08-13 19:02:06 -0400

    Fix rounding in recip causing pcc issues in models (#11319)
    * Accuracy of RECIP for fp16b (#10464)
    
    #10163: round recip output when in 16bit mode
    
    * #11287: Only round recip when APPROX==false

--- tt_metal/hw/ckernels/grayskull/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_recip.h
+++ tt_metal/hw/ckernels/grayskull/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_recip.h
@@ -12,7 +12,7 @@ namespace ckernel {
 
 // New LLK SFPU APIs
 
-template <bool APPROXIMATE>
+template <bool APPROXIMATE, bool is_fp32_dest_acc_en = false /*unused*/>
 inline void llk_math_eltwise_unary_sfpu_reciprocal(uint dst_index, int vector_mode = (int)VectorMode::RC) {
     llk_math_eltwise_unary_sfpu_params<APPROXIMATE>(
         ckernel::sfpu::calculate_reciprocal<APPROXIMATE>,
