400d69858797fae08f5fd83eaf26f3f41664bd77
Artem Yerofieiev <artem@tenstorrent.com>
2024-08-18 06:36:47 +0000

    #0: fix

--- ttnn/cpp/ttnn/operations/normalization/layernorm/layernorm.hpp
+++ ttnn/cpp/ttnn/operations/normalization/layernorm/layernorm.hpp
@@ -15,13 +15,13 @@ namespace operations::normalization {
 struct ExecuteLayerNorm {
     static ttnn::Tensor invoke(
         const ttnn::Tensor& input_tensor,
-        float epsilon,
-        const std::optional<const ttnn::Tensor>& weight,
-        const std::optional<const ttnn::Tensor>& bias,
-        const std::optional<const ttnn::Tensor>& residual_input_tensor,
-        const std::optional<MemoryConfig>& memory_config,
-        const std::optional<const LayerNormProgramConfig>& program_config,
-        const std::optional<const DeviceComputeKernelConfig> compute_kernel_config);
+        float epsilon = 1e-12,
+        const std::optional<const ttnn::Tensor>& weight = std::nullopt,
+        const std::optional<const ttnn::Tensor>& bias = std::nullopt,
+        const std::optional<const ttnn::Tensor>& residual_input_tensor = std::nullopt,
+        const std::optional<MemoryConfig>& memory_config = std::nullopt,
+        const std::optional<const LayerNormProgramConfig>& program_config = std::nullopt,
+        const std::optional<const DeviceComputeKernelConfig> compute_kernel_config = std::nullopt);
 };
 
 }  // namespace operations::normalization
