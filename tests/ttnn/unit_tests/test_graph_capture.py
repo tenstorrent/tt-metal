6bb3db0a72ca0ba329679a1def0b0a5cc0337d11
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2024-08-19 22:31:24 +0000

    #0: Extended tests

--- tests/ttnn/unit_tests/test_graph_capture.py
+++ tests/ttnn/unit_tests/test_graph_capture.py
@@ -26,4 +26,5 @@ def test_graph_capture(device, scalar, size, mode):
     assert captured_graph[0]["name"] == "capture_start"
     assert captured_graph[1]["name"] == "begin_function"
     assert captured_graph[1]["params"]["name"] == "tt::tt_metal::detail::convert_python_tensor_to_tt_tensor"
-    assert captured_graph[-1]["name"] == "buffer_deallocate"
+    assert captured_graph[-2]["name"] == "buffer_deallocate"
+    assert captured_graph[-1]["name"] == "capture_end"
