1c3115152e26e26f890b7d73f132053558f6d831
Jacob DeSousa <jdesousa@tenstorrent.com>
2024-08-20 15:55:02 +0000

    #11610: Enable tt-smi resets on the CI machine/tt-cloud executables

---
+++ tests/sweep_framework/tt_smi_util.py
@@ -0,0 +1,33 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import shutil
+import subprocess
+
+GRAYSKULL_ARGS = ["-tr", "all"]
+WORMHOLE_ARGS = ["-wr", "all"]
+
+
+def run_tt_smi(arch: str):
+    if arch not in ["grayskull", "wormhole_b0"]:
+        raise Exception(f"SWEEPS: Unsupported Architecture for TT-SMI Reset: {arch}")
+
+    smi_options = [
+        "tt-smi",
+        "tt-smi-metal",
+        "/home/software/syseng/gs/tt-smi" if arch == "grayskull" else "/home/software/syseng/wh/tt-smi",
+    ]
+    args = GRAYSKULL_ARGS if arch == "grayskull" else WORMHOLE_ARGS
+
+    while len(smi_options) != 0:
+        executable = shutil.which(smi_options.pop())
+        if executable is not None:
+            smi_process = subprocess.run([executable, *args])
+            if smi_process.returncode == 0:
+                print("SWEEPS: TT-SMI Reset Complete Successfully")
+                return
+            else:
+                raise Exception(f"SWEEPS: TT-SMI Reset Failed with Exit Code: {smi_process.returncode}")
+
+    raise Exception("SWEEPS: Unable to Locate TT-SMI Executable")
