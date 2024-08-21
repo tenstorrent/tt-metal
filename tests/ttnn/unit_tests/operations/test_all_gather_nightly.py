cbcf02a5ec2214c4b51dc680c4b79d40c86e1369
Aswinmcw <azayasankaran@tenstorrent.com>
2024-08-21 04:08:22 +0000

    #10874: Use deprecated version

--- tests/ttnn/unit_tests/operations/test_all_gather_nightly.py
+++ tests/ttnn/unit_tests/operations/test_all_gather_nightly.py
@@ -8,61 +8,12 @@ from loguru import logger
 import ttnn
 from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_equal, comp_pcc
 from models.utility_functions import skip_for_grayskull, get_devices_for_t3000
-from tests.ttnn.unit_tests.operations.test_all_gather import is_unsupported_case
+from tests.ttnn.unit_tests.operations.test_all_gather import (
+    is_unsupported_case,
+    run_line_all_gather,
+    run_line_all_gather_deprecated,
+)
 from ttnn import ShardTensorToMesh
-
-
-def run_line_all_gather(
-    t3k_device_mesh,
-    num_devices,
-    input_shape,
-    dim,
-    num_links,
-    input_dtype,
-    layout,
-    mem_config,
-    use_program_cache,
-    function_level_defaults,
-    enable_async,
-    num_iters=1,
-):
-    if t3k_device_mesh.get_num_devices() != 8:
-        pytest.skip("Not T3000!")
-
-    for device in t3k_device_mesh.get_devices():
-        device.enable_async(enable_async)
-
-    logger.info(f"Input shape: {input_shape}")
-    logger.info(f"dim: {dim}")
-
-    (is_known_failure, message) = is_unsupported_case(
-        input_shape, dim, mem_config, num_devices, num_links, input_dtype, layout
-    )
-    if is_known_failure:
-        pytest.skip(f"Skipping unsupported case {message}.")
-
-    logger.info(f"Input shape: {input_shape}")
-    logger.info(f"dim: {dim}")
-
-    input_tensor = torch.rand(input_shape).bfloat16()
-
-    ttnn_tensor = ttnn.from_torch(input_tensor, mesh_mapper=ShardTensorToMesh(t3k_device_mesh, dim=dim))
-    input_tensor_mesh = ttnn.to_device(ttnn_tensor, t3k_device_mesh)
-
-    for i in range(num_iters):
-        tt_out_tensor = ttnn.line_all_gather(input_tensor_mesh, dim, num_links=num_links, memory_config=mem_config)
-
-        logger.info(f"Done iteration {i}")
-
-    for i, t in enumerate(ttnn.get_device_tensors(tt_out_tensor)):
-        tt_output_tensor = t.cpu().to(ttnn.ROW_MAJOR_LAYOUT).to_torch()
-        if input_dtype == ttnn.bfloat16:
-            eq, output = comp_equal(tt_output_tensor, input_tensor)
-        else:
-            eq, output = comp_pcc(tt_output_tensor, input_tensor)
-        if not eq:
-            logger.error(f"output mismatch for tensor {i}")
-        assert eq, f"{i} FAILED: {output}"
 
 
 # Enumerate the post-commit cases explicitly
@@ -114,6 +65,64 @@ def test_line_all_gather_on_t3000_nightly(
 ):
     run_line_all_gather(
         t3k_device_mesh,
+        num_devices,
+        input_shape,
+        dim,
+        num_links,
+        input_dtype,
+        layout,
+        mem_config,
+        use_program_cache,
+        function_level_defaults,
+        enable_async,
+        num_iters,
+    )
+
+
+@skip_for_grayskull("Requires eth connected devices to run")
+@pytest.mark.parametrize(
+    "num_devices, num_links, input_shape, dim, layout",
+    [
+        (
+            4,
+            2,
+            [8, 8, 256, 384],
+            1,
+            ttnn.TILE_LAYOUT,
+        ),  # test cases with num_links = 2 is currently not supported by new mesh fixture
+    ],
+)
+@pytest.mark.parametrize(
+    "input_dtype",
+    [
+        ttnn.bfloat16,
+        ttnn.bfloat8_b,
+    ],
+)
+@pytest.mark.parametrize(
+    "mem_config",
+    [
+        ttnn.MemoryConfig(buffer_type=ttnn.BufferType.DRAM),
+        ttnn.MemoryConfig(buffer_type=ttnn.BufferType.L1),
+    ],
+)
+@pytest.mark.parametrize("enable_async", [True, False])
+def test_line_all_gather_on_t3000_nightly_two_link(
+    all_devices,
+    num_devices,
+    input_shape,
+    dim,
+    num_links,
+    input_dtype,
+    layout,
+    mem_config,
+    use_program_cache,
+    function_level_defaults,
+    enable_async,
+    num_iters=1,
+):
+    run_line_all_gather_deprecated(
+        all_devices,
         num_devices,
         input_shape,
         dim,
