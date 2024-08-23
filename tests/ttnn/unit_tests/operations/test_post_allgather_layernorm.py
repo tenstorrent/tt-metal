c7da9c4c579c99fdf127eb5a8256428e4c08a838
kpaigwar <kpaigwar@tenstorrent.com>
2024-08-23 13:19:27 +0000

    #0: added robust tests

--- tests/ttnn/unit_tests/operations/test_post_allgather_layernorm.py
+++ tests/ttnn/unit_tests/operations/test_post_allgather_layernorm.py
@@ -1,5 +1,6 @@
 import ttnn
 import torch
+import pytest
 from models.utility_functions import (
     skip_for_wormhole_b0,
     comp_allclose_and_pcc,
@@ -15,17 +16,27 @@ def rms_norm(x, gamma, eps):
     return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + eps) * gamma
 
 
-def test_post_allgather_layernorm(device, use_program_cache, RMSNORM=True):
-    torch.manual_seed(0)
+@pytest.mark.parametrize("RMSNORM", [True, False])
+@pytest.mark.parametrize("seed", [0, 100, 2000, 50000])  # Test across 5 different seeds
+@pytest.mark.parametrize("eps", [1e-5, 1e-6])
+@pytest.mark.parametrize("min_pcc", [0.9998])
+@pytest.mark.parametrize("max_atol", [0.38])
+@pytest.mark.parametrize("input_width", [1024, 2048, 4096])
+def test_post_allgather_layernorm(device, use_program_cache, input_width, RMSNORM, seed, eps, min_pcc, max_atol):
+    if RMSNORM:
+        print("Testing RMSNorm")
+    else:
+        print("Testing LayerNorm")
+    torch.manual_seed(seed)
 
-    torch_input_tensor = torch.randn((1, 1, 32, 1024), dtype=torch.bfloat16)
-    torch_weight = torch.randn((1, 1, 1, 1024), dtype=torch.bfloat16)
-    eps = 1e-6
+    torch_input_tensor = torch.randn((1, 1, 32, input_width), dtype=torch.bfloat16)
+    torch_weight = torch.randn((1, 1, 1, input_width), dtype=torch.bfloat16)
+
     if RMSNORM:
         torch_output_tensor = rms_norm(torch_input_tensor, torch_weight, eps=eps)
     else:
         torch_output_tensor = torch.nn.functional.layer_norm(
-            torch_input_tensor, (1024,), weight=torch_weight.squeeze(0).squeeze(0).squeeze(0), eps=eps
+            torch_input_tensor, (input_width,), weight=torch_weight.squeeze(0).squeeze(0).squeeze(0), eps=eps
         )
 
     tt_input_tensor = ttnn.from_torch(
@@ -37,7 +48,7 @@ def test_post_allgather_layernorm(device, use_program_cache, RMSNORM=True):
     )
     # shard to 32 cores
     tt_sharded_config = ttnn.create_sharded_memory_config(
-        shape=(1, 1, 32, 1024),
+        shape=(1, 1, 32, input_width),
         core_grid=ttnn.CoreGrid(y=2, x=8),
         strategy=ttnn.ShardStrategy.WIDTH,
     )
@@ -47,9 +58,9 @@ def test_post_allgather_layernorm(device, use_program_cache, RMSNORM=True):
 
     SHARDED_NORM_PRGM_CFG = ttnn.LayerNormShardedMultiCoreProgramConfig(
         compute_with_storage_grid_size=[8, 2],
-        subblock_w=(1024 // 16) // 32,
+        subblock_w=(input_width // 16) // 32,
         block_h=1,
-        block_w=(1024 // 16) // 32,
+        block_w=(input_width // 16) // 32,
         inplace=False,
     )
 
@@ -59,7 +70,7 @@ def test_post_allgather_layernorm(device, use_program_cache, RMSNORM=True):
         layout=ttnn.TILE_LAYOUT,
         memory_config=ttnn.DRAM_MEMORY_CONFIG,
         dtype=ttnn.bfloat16,
-        cache_file_name="rms_weights_cache_1024",
+        # cache_file_name="rms_weights_cache_1024",
     )
 
     if RMSNORM:
@@ -78,12 +89,12 @@ def test_post_allgather_layernorm(device, use_program_cache, RMSNORM=True):
             program_config=SHARDED_NORM_PRGM_CFG,
             memory_config=tt_sharded_config,
         )
-    tt_output_torch = ttnn.to_torch(tt_output_tensor)
+    tt_output_torch = ttnn.to_torch(tt_output_tensor).to(torch.bfloat16)
 
-    rtol = atol = 0.1
-    pcc = 0.99
-    passing, out = comp_allclose_and_pcc(torch_output_tensor, tt_output_torch, pcc=pcc, rtol=rtol, atol=atol)
-    print(torch_output_tensor)
-    print(tt_output_torch)
-    print(out)
-    assert passing
+    pcc_passing, pcc_out = comp_pcc(torch_output_tensor, tt_output_torch, pcc=min_pcc)
+    all_close_passing = torch.allclose(torch_output_tensor, tt_output_torch, atol=max_atol, equal_nan=False)
+    atol_delta = torch.max(torch.abs(torch_output_tensor - tt_output_torch)).item()
+    print(f"PCC: {pcc_out}")
+    print(f"all_close : {all_close_passing}, Max ATOL: {atol_delta}")
+    assert pcc_passing, "PCC test failed"
+    assert all_close_passing, "Allclose test failed"
