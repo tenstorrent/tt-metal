ebf902040f8661d0a5ac5c171963488da4466247
Lazar Premović <lpremovic@tenstorrent.com>
2024-08-23 17:20:58 +0200

    #0: Expand llk ctest coverage (#11653)
    * #0: Extend Tilize/Untilize tests to cover diferent shapes
    
    * #0: Add eltwise broadcast llk ctest
    
    * #0: Extend transpose test to test short init
    
    * #0: Extend Untilize tests to cover short init and untilize dst
    
    * #11463: Fix stimulus generation and tolerances in broadcast tests
    
    * #0: Rename TilizeConfig to TestConfig after refactoring in main
    
    * #0: Add short init option to tilize test
    
    * #0: Skip UntilizeDst tests on Blackhole
    
    * #0: Extend reduce tests to cover short init
    
    * #0: Fix reduce tests hanging on grayskull
    
    * #0: Update copyright year on new files

--- tests/tt_metal/tt_metal/test_kernels/compute/dst_untilize.cpp
+++ tests/tt_metal/tt_metal/test_kernels/compute/dst_untilize.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -6,6 +6,7 @@
 
 #include "compute_kernel_api/untilize.h"
 #include "compute_kernel_api/pack_untilize.h"
+#include "compute_kernel_api/tile_move_copy.h"
 #include "compute_kernel_api/eltwise_unary/eltwise_unary.h"
 
 namespace NAMESPACE {
@@ -13,18 +14,24 @@ void MAIN {
 
     constexpr uint32_t per_core_block_cnt = get_compile_time_arg_val(0);
     constexpr uint32_t per_core_block_tile_cnt = get_compile_time_arg_val(1);
-#ifndef SHORT_INIT
-    pack_untilize_init<per_core_block_tile_cnt>(tt::CB::c_in0, tt::CB::c_out0);
-#else
+
     unary_op_init_common(tt::CB::c_in0, tt::CB::c_out0);
-    pack_untilize_init_short<per_core_block_tile_cnt>(tt::CB::c_in0, tt::CB::c_out0);
-#endif
 
     for(uint32_t b = 0; b < per_core_block_cnt; ++ b) {
         cb_wait_front(tt::CB::c_in0, per_core_block_tile_cnt);
         cb_reserve_back(tt::CB::c_out0, per_core_block_tile_cnt);
 
-        pack_untilize_block<per_core_block_tile_cnt>(tt::CB::c_in0, 1, tt::CB::c_out0);
+        tile_regs_acquire();
+        copy_tile_init();
+        for (uint32_t i = 0; i < per_core_block_tile_cnt; ++i) {
+            copy_tile(tt::CB::c_in0, i, i);
+        }
+        tile_regs_commit();
+
+        tile_regs_wait();
+        pack_untilize_dst_init_short<per_core_block_tile_cnt>(tt::CB::c_out0);
+        pack_untilize_dst<per_core_block_tile_cnt>(tt::CB::c_out0);
+        tile_regs_release();
 
         cb_push_back(tt::CB::c_out0, per_core_block_tile_cnt);
         cb_pop_front(tt::CB::c_in0, per_core_block_tile_cnt);
