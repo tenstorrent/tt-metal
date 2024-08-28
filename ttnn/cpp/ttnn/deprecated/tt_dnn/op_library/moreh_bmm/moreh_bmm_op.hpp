7c05e86238262f52691b71934484b01a99a833db
Dongjin Na <dongjin.na@moreh.io>
2024-08-28 13:02:27 +0000

    #11938: Add optional output, kernel config in moreh_bmm

--- ttnn/cpp/ttnn/deprecated/tt_dnn/op_library/moreh_bmm/moreh_bmm_op.hpp
+++ ttnn/cpp/ttnn/deprecated/tt_dnn/op_library/moreh_bmm/moreh_bmm_op.hpp
@@ -6,7 +6,10 @@
 
 #pragma once
 
+#include <optional>
+
 #include "ttnn/tensor/tensor.hpp"
+#include "ttnn/deprecated/tt_dnn/op_library/compute_kernel_config.hpp"
 #include "ttnn/operation.hpp"
 
 namespace tt {
@@ -18,7 +21,9 @@ using namespace tt_metal;
 Tensor moreh_bmm(
     const Tensor& input,
     const Tensor& mat2,
-    const MemoryConfig& output_mem_config = operation::DEFAULT_OUTPUT_MEMORY_CONFIG);
+    std::optional<const Tensor> output = std::nullopt,
+    const MemoryConfig& output_mem_config = operation::DEFAULT_OUTPUT_MEMORY_CONFIG,
+    std::optional<const DeviceComputeKernelConfig> compute_kernel_config = std::nullopt);
 
 }  // namespace primary
 
