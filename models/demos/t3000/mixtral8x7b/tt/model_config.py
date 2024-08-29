3d70c82181d5daaec57c66d36ab9e95fc7c8d9e1
mtairum <mtairum@tenstorrent.com>
2024-08-29 17:23:32 +0000

    #10934: [Mixtral] Update default max_seq_len to 32k

--- models/demos/t3000/mixtral8x7b/tt/model_config.py
+++ models/demos/t3000/mixtral8x7b/tt/model_config.py
@@ -22,7 +22,7 @@ class TtModelArgs:
     vocab_size = 32000
 
     max_batch_size = 32  # default
-    max_seq_len = 16384  # default
+    max_seq_len = 32768  # default
     moe = True
     num_experts = 8
     num_experts_per_tok = 2
@@ -66,7 +66,7 @@ class TtModelArgs:
         "OUTPUT_MM",
     )
 
-    def __init__(self, device=None, instruct=False, dummy_weights=False, max_seq_len=16384, max_batch_size=32):
+    def __init__(self, device=None, instruct=False, dummy_weights=False, max_seq_len=32768, max_batch_size=32):
         self.max_seq_len = max_seq_len
         self.max_batch_size = max_batch_size
 
