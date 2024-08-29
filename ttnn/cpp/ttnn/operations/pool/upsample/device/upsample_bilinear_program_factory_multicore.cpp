b13781c2ab7f6a7c7be58626ff6f33453d3d6e13
Shwetank Singh <ssingh@tenstorrent.com>
2024-08-29 06:11:49 +0000

    #5725: Adding bilinear support in upsample

--- ttnn/cpp/ttnn/operations/pool/upsample/device/upsample_bilinear_program_factory_multicore.cpp
+++ ttnn/cpp/ttnn/operations/pool/upsample/device/upsample_bilinear_program_factory_multicore.cpp
@@ -11,24 +11,74 @@
 #include "tt_metal/common/constants.hpp"
 #include "tt_metal/detail/util.hpp"
 #include "tt_metal/common/math.hpp"
+//#include "ttnn/tensor/tensor_utils.hpp"
+#include "ttnn/operations/reduction/generic/device/reduce_op.hpp"  // for reduce_op_utils
 
 #include "tt_metal/tt_stl/reflection.hpp"
+#include "ttnn/deprecated/tt_numpy/functions.hpp"
+#include "ttnn/operations/sliding_window/sliding_window.hpp"
+#include "ttnn/operations/sliding_window/halo/halo.hpp"
+
+#include "ttnn/operations/core/core.hpp"
 
 using namespace tt::constants;
 
-namespace ttnn::operations::upsample {
+namespace  ttnn::operations::upsample  {
 using namespace tt;
+using sliding_window::SlidingWindowConfig;
 
-operation::ProgramWithCallbacks upsample_multi_core(const Tensor &input, Tensor& output, const uint32_t scale_factor_h, const uint32_t scale_factor_w) {
+Tensor HaloTensorCreation(const Tensor &input){
+    int batch_size = input.get_legacy_shape()[0];
+    int input_height = input.get_legacy_shape()[1];
+    int input_width = input.get_legacy_shape()[2];
+    int num_cores_nhw = input.shard_spec().value().num_cores();
+
+    ttnn::Tensor input_tensor = input;  // tensor to return
+    SlidingWindowConfig sliding_window_config = SlidingWindowConfig(
+            batch_size,
+            {input_height, input_width},
+            {2, 2}, //kernel size
+            {1, 1}, // stride
+            {1, 0}, //padding
+            {1, 1}, //dilation
+            num_cores_nhw,
+            input_tensor.memory_config().shard_spec.value().grid,
+            false, true);
+
+    input_tensor = ttnn::operations::core::reshape(
+        input_tensor,
+        Shape(std::array<uint32_t, 4>{
+            1,
+            1,
+            input.get_shape()[0] * input.get_shape()[1] * input.get_shape()[2],
+            input.get_shape()[3]}));
+
+    auto halo_output = ttnn::halo(
+        DefaultQueueId,
+        input_tensor,
+        sliding_window_config,
+        0,
+        false,
+        false,
+        0,
+        input_tensor.memory_config(),
+        false);
+
+    return halo_output;
+}
+
+operation::ProgramWithCallbacks bilinear_multi_core(const Tensor &input, Tensor& output, const uint32_t scale_factor_h, const uint32_t scale_factor_w, const DeviceComputeKernelConfig compute_kernel_config) {
     Program program = CreateProgram();
     Device *device = input.device();
+
+    auto input_shape = input.get_legacy_shape();
+    auto output_shape = output.get_legacy_shape();
 
     tt::DataFormat input_cb_data_format = tt::tt_metal::datatype_to_dataformat_converter(input.get_dtype());
     tt::DataFormat output_cb_data_format = tt::tt_metal::datatype_to_dataformat_converter(output.get_dtype());
 
     // NOTE: input is assumed to have channels last format: {N, H, W, C}, {N, 1, H * W, C}, {1, 1, N * H * W, C}
     // NOTE: Bfp8_b/TILE is not yet supported
-
     uint32_t input_stick_nbytes = input.get_legacy_shape()[-1] * input.element_size();
     uint32_t output_stick_nbytes = output.get_legacy_shape()[-1] * output.element_size();
     TT_FATAL(input_stick_nbytes == output_stick_nbytes, "Input and output sticks should have same size");
@@ -37,7 +87,10 @@ operation::ProgramWithCallbacks upsample_multi_core(const Tensor &input, Tensor&
     uint32_t input_nsticks = input.volume() / input.get_legacy_shape()[-1];
 
     uint32_t in_w = input.get_legacy_shape()[2];
-    uint32_t out_w = output.get_legacy_shape()[2];
+    uint32_t out_w =output.get_legacy_shape()[2];
+
+    auto [math_fidelity, math_approx_mode, fp32_dest_acc_en, packer_l1_acc] =
+        get_compute_kernel_config_args(input.device()->arch(), compute_kernel_config);
 
     auto shard_spec = input.shard_spec().value();
     auto all_cores = shard_spec.grid;
@@ -54,38 +107,54 @@ operation::ProgramWithCallbacks upsample_multi_core(const Tensor &input, Tensor&
     // extra limitation to avoid post upsample step of resharding
     if (input.memory_config().memory_layout == TensorMemoryLayout::HEIGHT_SHARDED) {
         TT_FATAL(in_nsticks_per_core % in_w == 0, "Restriction: Input sticks per core {} should be divisible by input width {}. TODO to remove this restriction", in_nsticks_per_core, in_w);
-    } else if (input.memory_config().memory_layout == TensorMemoryLayout::BLOCK_SHARDED) {
-        ncores_x = all_cores.ranges().begin()->end_coord.x + 1;
-        ncores_nhw = all_cores.ranges().begin()->end_coord.y + 1;
-        input_stick_nbytes = input_stick_nbytes / ncores_x;
-        output_stick_nbytes = output_stick_nbytes / ncores_x;
     } else {
-        TT_THROW("Unsupported sharding layout");
+        TT_FATAL(false, "Unsupported sharding layout");
     }
 
     uint32_t input_nsticks_per_core = div_up(input_nsticks, ncores_nhw);
     uint32_t output_nsticks_per_core = div_up(output_nsticks, ncores_nhw);
 
-    // TODO: Support non-multiple case
     TT_FATAL(in_nsticks_per_core == input_nsticks_per_core, "Input sticks per shard {} should be same as input sticks per core {}", in_nsticks_per_core, input_nsticks_per_core);
     TT_FATAL(out_nsticks_per_core == output_nsticks_per_core, "Output sticks per shard {} should be same as output sticks per core {}", out_nsticks_per_core, output_nsticks_per_core);
     TT_FATAL(input_nsticks_per_core % in_w == 0, "Error");
 
-    // CBs
+    //creating halo input tensor
+    auto halo_in = HaloTensorCreation(input);
+    auto halo_shard_shape = halo_in.shard_spec().value().shape;
 
+    // CBs
     uint32_t buffering_factor = 1;  // data is already fully buffered in the CBs since its sharded
 
     // input data is in a sharded CB
     uint32_t in_cb_id = CB::c_in0;
     uint32_t aligned_input_stick_nbytes = round_up_to_mul32(input_stick_nbytes);
     uint32_t in_cb_pagesize = aligned_input_stick_nbytes;
-    uint32_t in_cb_npages = input_nsticks_per_core * buffering_factor;
+    uint32_t in_cb_npages = halo_shard_shape[0] * buffering_factor;
     CircularBufferConfig cb_src0_config = CircularBufferConfig(
                                             in_cb_pagesize * in_cb_npages,
                                             {{in_cb_id, input_cb_data_format}})
                                           .set_page_size(in_cb_id, in_cb_pagesize)
-                                          .set_globally_allocated_address(*input.buffer());
+                                          .set_globally_allocated_address(*halo_in.buffer());
     auto cb_src0 = tt_metal::CreateCircularBuffer(program, all_cores, cb_src0_config);
+
+    //intermediate tensor CB
+    uint32_t in1_cb_id = CB::c_in1;
+    CircularBufferConfig cb_src1_config = CircularBufferConfig(
+                                            4 * in_cb_pagesize,  //since 4 pixels per page are needed for intermediate tensor.
+                                            {{in1_cb_id, input_cb_data_format}})
+                                          .set_page_size(in1_cb_id, in_cb_pagesize);
+    auto cb_src1 = tt_metal::CreateCircularBuffer(program, all_cores, cb_src1_config);
+
+    //scaler CB
+    uint32_t in_scalar_cb_id = CB::c_in4;
+    uint32_t in_scalar_cb_pagesize = tile_size(input_cb_data_format);
+    uint32_t in_scalar_cb_npages = 1;
+    CircularBufferConfig in_scalar_cb_config =
+        CircularBufferConfig(in_scalar_cb_npages * in_scalar_cb_pagesize, {{in_scalar_cb_id, input_cb_data_format}})
+                    .set_page_size(in_scalar_cb_id, in_scalar_cb_pagesize);
+
+
+    auto in_scalar_cb = tt_metal::CreateCircularBuffer(program, all_cores, in_scalar_cb_config);
 
     // output sharded CB with upsampled data
     uint32_t out_cb_id = CB::c_out0;
@@ -106,71 +175,81 @@ operation::ProgramWithCallbacks upsample_multi_core(const Tensor &input, Tensor&
     log_debug(LogOp, "input_nsticks_per_core: {}, output_nsticks_per_core: {}", input_nsticks_per_core, output_nsticks_per_core);
 
     // Kernels
-
-    std::vector<uint32_t> writer_compile_time_args = {
+    std::vector<uint32_t> reader_compile_time_args = {
         in_cb_id,
         out_cb_id,
         false,
     };
-    auto writer_kernel_fname = std::string("ttnn/cpp/ttnn/operations/pool/upsample/device/kernels/dataflow/writer_upsample_multi_core_sharded.cpp");
-    auto writer_kernel =
-        CreateKernel(program, writer_kernel_fname, all_cores, WriterDataMovementConfig(writer_compile_time_args));
 
-    std::vector<uint32_t> reader_compile_time_args = {
-        in_cb_id,
-        out_cb_id,
-        true,
+    string writer_kernel_fname, reader_kernel_fname, compute_kernel_fname;
+
+    reader_kernel_fname = std::string("ttnn/cpp/ttnn/operations/pool/upsample/device/kernels/dataflow/reader_bilinear_multi_core_sharded.cpp");
+    compute_kernel_fname = std::string("ttnn/cpp/ttnn/operations/pool/upsample/device/kernels/compute/bilinear.cpp");
+
+    uint32_t in_ntiles_c = (uint32_t)std::ceil((float)input_shape[3] / constants::TILE_WIDTH);
+    std::vector<uint32_t> compute_compile_time_args = {
+        1,
+        in_ntiles_c,
+        1 * in_ntiles_c,
+        4,
+        output_shape[1],
+        output_shape[2],
+        (uint32_t)std::ceil((float)output_shape[2] / constants::TILE_HEIGHT),
+        (uint32_t)std::ceil((float)output_shape[3] / constants::TILE_WIDTH),
+        output_nsticks_per_core,  // loop count with blocks
+        input_shape[3],
     };
-    auto reader_kernel_fname = std::string("ttnn/cpp/ttnn/operations/pool/upsample/device/kernels/dataflow/writer_upsample_multi_core_sharded.cpp");
+
     auto reader_kernel =
         CreateKernel(program, reader_kernel_fname, all_cores, ReaderDataMovementConfig(reader_compile_time_args));
+    TT_FATAL(fp32_dest_acc_en == false, "fp32_dest_acc_en as true not supported. #12787 issue raised");
+    auto reduce_op = ReduceOpMath::SUM;
+    auto reduce_dim = ReduceOpDim::H;
+    auto compute_config = ComputeConfig{
+        .math_fidelity = math_fidelity,
+        .fp32_dest_acc_en = fp32_dest_acc_en,
+        .math_approx_mode = math_approx_mode,
+        .compile_args = compute_compile_time_args,
+        .defines = reduce_op_utils::get_defines(reduce_op, reduce_dim)};
 
-    // no compute kernel
+    auto compute_kernel =
+        CreateKernel(program, compute_kernel_fname, all_cores, compute_config);
 
     // runtime args
-
-    uint32_t writer_nargs = 7;
-    vector<uint32_t> writer_rt_args(writer_nargs);
-    writer_rt_args[0] = input_stick_nbytes;
-    writer_rt_args[1] = input_nsticks_per_core / in_w;
-    writer_rt_args[2] = scale_factor_h;
-    writer_rt_args[3] = scale_factor_w;
-    writer_rt_args[4] = in_w;
-    writer_rt_args[5] = out_w;
-    writer_rt_args[6] = 0;  // set for each core below
+    uint32_t reader_nargs = 10;
+    vector<uint32_t> reader_rt_args(reader_nargs);
+    reader_rt_args[0] = input_stick_nbytes;
+    reader_rt_args[1] = input_nsticks_per_core / in_w;
+    reader_rt_args[2] = scale_factor_h;
+    reader_rt_args[3] = scale_factor_w;
+    reader_rt_args[4] = in_w;
+    reader_rt_args[5] = out_w;
+    reader_rt_args[6] = 0;  // set for each core below
 
     uint32_t start_input_stick_id = 0;
-    if (input.memory_config().memory_layout == TensorMemoryLayout::BLOCK_SHARDED) {
-        for (int32_t core = 0; core < ncores_nhw; ++core) {
-            for (int32_t core_x = 0; core_x < ncores_x; ++core_x) {
-                CoreCoord core_coord(core_x, core); // logical
-                writer_rt_args[6] = start_input_stick_id;
-                SetRuntimeArgs(program, writer_kernel, core_coord, writer_rt_args);
-                SetRuntimeArgs(program, reader_kernel, core_coord, writer_rt_args);
-            }
-            start_input_stick_id += input_nsticks_per_core;
-        }
-    } else if (input.memory_config().memory_layout == TensorMemoryLayout::HEIGHT_SHARDED) {
+
+    if (input.memory_config().memory_layout == TensorMemoryLayout::HEIGHT_SHARDED) {
         for (int32_t core = 0; core < ncores_nhw; ++core) {
             CoreCoord core_coord(core % ncores_x, core / ncores_x); // logical
-            writer_rt_args[6] = start_input_stick_id;
-            SetRuntimeArgs(program, writer_kernel, core_coord, writer_rt_args);
-            SetRuntimeArgs(program, reader_kernel, core_coord, writer_rt_args);
+            reader_rt_args[6] = start_input_stick_id;
+            reader_rt_args[8] = (core == 0) ? 1 : 0;
+            reader_rt_args[9] = (core == ncores_nhw-1) ? 1 : 0;
+            SetRuntimeArgs(program, reader_kernel, core_coord, reader_rt_args);
             start_input_stick_id += input_nsticks_per_core;
         }
     } else {
-        TT_THROW("Unsupported memory layout");
+        TT_FATAL(false, "Unsupported memory layout");
     }
 
-    auto override_runtime_args_callback = [writer_kernel, cb_src0, out_cb](
+    auto override_runtime_args_callback = [reader_kernel, cb_src0, out_cb](
         const void* operation,
         Program &program,
         const std::vector<Tensor>& input_tensors,
         const std::vector<std::optional<const Tensor>>&,
         const std::vector<Tensor>& output_tensors
     ) {
-
-        auto src_buffer = input_tensors.at(0).buffer();
+        auto halo_in = HaloTensorCreation(input_tensors.at(0));
+        auto src_buffer = halo_in.buffer();
         auto dst_buffer = output_tensors.at(0).buffer();
 
         UpdateDynamicCircularBufferAddress(program, cb_src0, *src_buffer);
