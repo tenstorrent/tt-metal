4b7420e72327fc4d0264685294aa0c314c970973
Johanna Rock <jrock@tenstorrent.com>
2024-09-02 16:42:15 +0000

    #0: Fix sharded distributed layernorm end to end test

--- tests/ttnn/unit_tests/operations/test_layernorm_distributed_sharded.py
+++ tests/ttnn/unit_tests/operations/test_layernorm_distributed_sharded.py
@@ -122,7 +122,7 @@ def test_sharded_layernorm(
     # assert atol_delta <= max_atol, f"Max Atol exceeded: {atol_delta} (allowed: {max_atol})"
 
 
-@pytest.mark.parametrize("is_rmsnorm", [True])
+@pytest.mark.parametrize("is_rmsnorm", [True])  # Layernorm not supported for now
 @pytest.mark.parametrize("seed", [0])  # Test across 5 different seeds
 @pytest.mark.parametrize("eps", [1e-6])
 @pytest.mark.parametrize("min_pcc", [0.9997])
@@ -384,145 +384,207 @@ def test_pre_allgather_layernorm(
         prev_tt_output_torch = tt_output_torch
 
 
-# def reference_layernorm(x, gamma, beta, epsilon, is_rmsnorm):
-#     if is_rmsnorm:
-#         return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + epsilon) * gamma
-#     else:
-#         return torch.nn.functional.layer_norm(x, x.shape[-1:], gamma, beta, epsilon)
+def reference_layernorm(x, gamma, beta, epsilon, is_rmsnorm):
+    if is_rmsnorm:
+        return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + epsilon) * gamma
+    else:
+        return torch.nn.functional.layer_norm(x, x.shape[-1:], gamma, beta, epsilon)
 
 
-# def tt_distributed_layernorm(inp, gamma, beta, epsilon, is_rmsnorm, compute_kernel_config, sharded_program_config, sharded_memory_config):
-#     n_devices = len(inp)
+def tt_distributed_layernorm(
+    inp, gamma, beta, epsilon, is_rmsnorm, compute_kernel_config, sharded_program_config, sharded_memory_config
+):
+    n_devices = len(inp)
 
-#     # Run layernorm part 1
-#     tt_stats = []
-#     for d in range(n_devices):
-#         if is_rmsnorm:
-#             tt_stats.append(
-#                 ttnn.rmsnorm_pre_all_gather(inp[d], program_config=sharded_program_config)
-#             )
-#         else:
-#             tt_stats.append(
-#                 ttnn.layernorm_pre_all_gather(inp[d], program_config=sharded_program_config)
-#             )
+    # Run layernorm part 1
+    tt_stats = []
+    for d in range(n_devices):
+        if is_rmsnorm:
+            tt_stats.append(ttnn.rmsnorm_pre_all_gather(inp[d], program_config=sharded_program_config))
+        else:
+            tt_stats.append(ttnn.layernorm_pre_all_gather(inp[d], program_config=sharded_program_config))
 
-#     tt_stats = ttnn.aggregate_as_tensor(tt_stats)
-#     # AllGather stats
-#     tt_stats = ttnn.all_gather(tt_stats, dim=3, num_links=1, memory_config=ttnn.DRAM_MEMORY_CONFIG)
+    if n_devices > 1:
+        # AllGather stats
+        tt_stats = ttnn.aggregate_as_tensor(tt_stats)
+        tt_stats = ttnn.all_gather(tt_stats, dim=3, num_links=1, memory_config=ttnn.DRAM_MEMORY_CONFIG)
+        tt_stats = ttnn.get_device_tensors(tt_stats)
 
-#     # Run layernorm part 2
-#     tt_stats = ttnn.get_device_tensors(tt_stats)
-#     tt_out = []
-#     for d in range(n_devices):
-#         if is_rmsnorm:
-#             tt_out = ttnn.rmsnorm_post_all_gather(
-#                 inp[d],
-#                 epsilon=epsilon,
-#                 weight=gamma,
-#                 bias=beta,
-#                 program_config=sharded_program_config,
-#                 memory_config=sharded_memory_config,
-#                 sum_x2=tt_stats[d],
-#             )
-#         else:
-#             tt_out = ttnn.layernorm_post_all_gather(
-#                 inp[d],
-#                 epsilon=epsilon,
-#                 weight=gamma,
-#                 bias=beta,
-#                 program_config=sharded_program_config,
-#                 memory_config=sharded_memory_config,
-#                 stats=tt_stats[d],
-#             )
-#         tt_stats[d].deallocate(True)
-#     return tt_out
+    # Run layernorm part 2
+    tt_out = []
+    for d in range(n_devices):
+        if is_rmsnorm:
+            tt_out.append(
+                ttnn.rmsnorm_post_all_gather(
+                    inp[d],
+                    epsilon=epsilon,
+                    weight=gamma[d],
+                    program_config=sharded_program_config,
+                    memory_config=sharded_memory_config,
+                    stats=tt_stats[d],
+                )
+            )
+        else:
+            tt_out.append(
+                ttnn.layernorm_post_all_gather(
+                    inp[d],
+                    epsilon=epsilon,
+                    weight=gamma[d],
+                    program_config=sharded_program_config,
+                    memory_config=sharded_memory_config,
+                    stats=tt_stats[d],
+                )
+            )
+        tt_stats[d].deallocate(True)
+    return tt_out
 
 
-# def run_distributed_layernorm(
-#     inp_shape, n_devices, is_rmsnorm, dtype, stats_dtype, devices, fp32_enabled=False, iterations=1
-# ):
-#     compute_kernel_config = ttnn.experimental.tensor.WormholeComputeKernelConfig(
-#         math_fidelity=ttnn.experimental.tensor.MathFidelity.HiFi4,  # Highest fidelity
-#         math_approx_mode=False,
-#         fp32_dest_acc_en=fp32_enabled,
-#         packer_l1_acc=False,
-#     )
+def run_distributed_layernorm(inp_shape, n_devices, is_rmsnorm, input_df, devices, fp32_enabled=False, iterations=1):
+    compute_kernel_config = ttnn.experimental.tensor.WormholeComputeKernelConfig(
+        math_fidelity=ttnn.experimental.tensor.MathFidelity.HiFi4,  # Highest fidelity
+        math_approx_mode=False,
+        fp32_dest_acc_en=fp32_enabled,
+        packer_l1_acc=False,
+    )
 
-#     torch.manual_seed(1234)
+    torch.manual_seed(1234)
 
-#     canon_inp = torch.randn(inp_shape) * 4 - 1
-#     gamma = torch.rand(inp_shape[-1]) * 2 - 1
-#     beta = torch.rand(inp_shape[-1]) * 2 - 1
+    canon_inp = torch.randn(inp_shape, dtype=torch.bfloat16) * 4 - 1
+    gamma = torch.rand(inp_shape[-1]) * 2 - 1
+    beta = torch.rand(inp_shape[-1]) * 2 - 1
 
-#     gamma_chunked = gamma.chunk(n_devices, dim=-1)
-#     beta_chunked = beta.chunk(n_devices, dim=-1)
-#     inp_chunked = canon_inp.chunk(n_devices, dim=-1)
+    gamma_chunked = gamma.chunk(n_devices, dim=-1)
+    beta_chunked = beta.chunk(n_devices, dim=-1)
+    inp_chunked = canon_inp.chunk(n_devices, dim=-1)
 
-#     epsilon = 1e-5
+    epsilon = 1e-5
 
-#     # reference impl
-#     out_torch = reference_layernorm(canon_inp, gamma, beta, epsilon, is_rmsnorm)
+    # reference impl
+    out_torch = reference_layernorm(canon_inp, gamma, beta, epsilon, is_rmsnorm)
 
-#     # shard to 32 cores
-#     sharded_memory_config = ttnn.create_sharded_memory_config(
-#         shape=inp_shape,
-#         core_grid=ttnn.CoreGrid(y=2, x=8),
-#         strategy=ttnn.ShardStrategy.WIDTH,
-#     )
+    # shard to 32 cores
+    sharded_memory_config = ttnn.create_sharded_memory_config(
+        shape=inp_shape,
+        core_grid=ttnn.CoreGrid(y=2, x=8),
+        strategy=ttnn.ShardStrategy.WIDTH,
+    )
 
-#     sharded_program_config = ttnn.LayerNormShardedMultiCoreProgramConfig(
-#         compute_with_storage_grid_size=[8, 2],
-#         subblock_w=(inp_shape[3] // 16) // 32,
-#         block_h=1,
-#         block_w=(inp_shape[3] // 16) // 32,
-#         inplace=False,
-#     )
+    sharded_program_config = ttnn.LayerNormShardedMultiCoreProgramConfig(
+        compute_with_storage_grid_size=[8, 2],
+        subblock_w=(inp_shape[3] // 16) // 32,
+        block_h=1,
+        block_w=(inp_shape[3] // 16) // 32,
+        inplace=False,
+    )
 
-#     tt_inp = []
-#     for d in range(n_devices):
-#         tt_inp.append(
-#             ttnn.as_tensor(
-#                 inp_chunked[d],
-#                 dtype=dtype,
-#                 device=devices[d],
-#                 layout=ttnn.TILE_LAYOUT,
-#                 memory_config=sharded_memory_config,
-#             )
-#         )
+    tt_inp = []
+    for d in range(n_devices):
+        tt_inp.append(
+            ttnn.as_tensor(
+                inp_chunked[d],
+                dtype=input_df,
+                device=devices[d],
+                layout=ttnn.TILE_LAYOUT,
+                memory_config=sharded_memory_config,
+            )
+        )
 
-#     tt_gamma = []
-#     for d in range(n_devices):
-#         tt_gamma.append(
-#             ttnn.as_tensor(
-#                 gamma_chunked[d].reshape(1, 1, -1, 32),
-#                 dtype=ttnn.bfloat16,
-#                 device=devices[d],
-#                 layout=ttnn.ROW_MAJOR_LAYOUT,
-#                 memory_config=ttnn.DRAM_MEMORY_CONFIG,
-#             )
-#         )
+    tt_gamma = []
+    for d in range(n_devices):
+        tt_gamma.append(
+            ttnn.as_tensor(
+                gamma_chunked[d].reshape(1, 1, -1, 32),
+                dtype=ttnn.bfloat16,
+                device=devices[d],
+                layout=ttnn.ROW_MAJOR_LAYOUT,
+                memory_config=ttnn.DRAM_MEMORY_CONFIG,
+            )
+        )
 
-#     tt_beta = []
-#     for d in range(n_devices):
-#         tt_beta.append(
-#             ttnn.as_tensor(
-#                 beta_chunked[d].reshape(1, 1, -1, 32),
-#                 dtype=ttnn.bfloat16,
-#                 device=devices[d],
-#                 layout=ttnn.ROW_MAJOR_LAYOUT,
-#                 memory_config=ttnn.DRAM_MEMORY_CONFIG,
-#             )
-#         )
-#     for _ in range(iterations):
-#         tt_out = tt_distributed_layernorm(
-#             tt_inp, tt_gamma, tt_beta, epsilon, is_rmsnorm, compute_kernel_config, sharded_program_config, sharded_memory_config, stats_dtype
-#         )
-#         tt_output_host = torch.concat([tt2torch_tensor(tt_o) for tt_o in tt_out], -1)
+    tt_beta = []
+    for d in range(n_devices):
+        tt_beta.append(
+            ttnn.as_tensor(
+                beta_chunked[d].reshape(1, 1, -1, 32),
+                dtype=ttnn.bfloat16,
+                device=devices[d],
+                layout=ttnn.ROW_MAJOR_LAYOUT,
+                memory_config=ttnn.DRAM_MEMORY_CONFIG,
+            )
+        )
+    for _ in range(iterations):
+        tt_out = tt_distributed_layernorm(
+            inp=tt_inp,
+            gamma=tt_gamma,
+            beta=tt_beta,
+            epsilon=epsilon,
+            is_rmsnorm=is_rmsnorm,
+            compute_kernel_config=compute_kernel_config,
+            sharded_program_config=sharded_program_config,
+            sharded_memory_config=sharded_memory_config,
+        )
+        tt_output_host = torch.concat([tt2torch_tensor(tt_o) for tt_o in tt_out], -1)
 
-#     passing, output_str = comp_allclose(tt_output_host, out_torch, rtol=1e-1, atol=1e-01)
-#     logger.debug(f"torch vs tt distributed layernorm = {output_str}")
+    max_atol = (
+        0.3  # TODO: pass in as parameter and choose something reasonable (check reference sharded implementation!)
+    )
+    max_pcc = 0.99
 
-#     assert passing
+    print(f"torch_output_tensor: {out_torch}")
+    print(f"tt_output_torch: {tt_output_host}")
+
+    tt_output_host = tt_output_host.to(torch.bfloat16)
+    out_torch = out_torch.to(torch.bfloat16)
+
+    _, pcc_out = comp_pcc(tt_output_host, out_torch, pcc=max_pcc)
+    all_close_passing = torch.allclose(tt_output_host, out_torch, atol=max_atol, equal_nan=False)
+    atol_delta = torch.max(torch.abs(tt_output_host - out_torch)).item()
+    print(f"PCC: {pcc_out}")
+    print(f"Atol: {atol_delta}")
+
+    assert pcc_out >= max_pcc, f"PCC test failed: {pcc_out} (threshold: {max_pcc})"
+    assert all_close_passing, f"All close test failed. Atol: {atol_delta} (threshold: {max_atol})"
+
+
+@skip_for_grayskull("Requires eth connected devices to run")
+@pytest.mark.parametrize(
+    "iterations",
+    [2],
+    ids=["loops2"],
+)
+@pytest.mark.parametrize("input_df", [ttnn.bfloat16])
+@pytest.mark.parametrize(
+    "inp_shape",
+    [
+        (1, 1, 32, 1024),
+    ],
+    ids=["w1024"],
+)
+@pytest.mark.parametrize(
+    "n_devices",
+    [1],
+    # [4, 8],
+)
+@pytest.mark.parametrize(
+    "is_rmsnorm",
+    [
+        True,
+        # False
+    ],
+    ids=[
+        "rmsnorm",
+        # "layernorm" # Layernorm not supported for now
+    ],
+)
+def test_distributed_layernorm(inp_shape, n_devices, is_rmsnorm, input_df, iterations, all_devices):
+    # if len(all_devices) != 8:
+    #     pytest.skip("Not T3000!")
+
+    # devices = get_devices_for_t3000(all_devices, n_devices)
+
+    devices = all_devices[:n_devices]
+
+    run_distributed_layernorm(inp_shape, n_devices, is_rmsnorm, input_df, devices, iterations=iterations)
 
 
 # @skip_for_grayskull("Requires eth connected devices to run")
@@ -531,61 +593,7 @@ def test_pre_allgather_layernorm(
 #     [2],
 #     ids=["loops2"],
 # )
-# @pytest.mark.parametrize(
-#     "dtype",
-#     (ttnn.bfloat16, ttnn.bfloat8_b),
-#     ids=["BFLOAT16_in", "BFLOAT8_B_in"],
-# )
-# @pytest.mark.parametrize(
-#     "stats_dtype",
-#     (ttnn.bfloat16, ttnn.bfloat8_b),
-#     ids=["BFLOAT16_stats", "BFLOAT8_B_stats"],
-# )
-# @pytest.mark.parametrize(
-#     "inp_shape",
-#     [
-#         (1, 1, 2048, 8192),
-#         (1, 1, 128, 8192),
-#         (2, 1, 128, 8192),
-#     ],
-#     ids=["inp_shape0", "inp_shape1", "inp_shape2"],
-# )
-# @pytest.mark.parametrize(
-#     "n_devices",
-#     [4, 8],
-# )
-# @pytest.mark.parametrize(
-#     "is_rmsnorm",
-#     [True, False],
-#     ids=["rmsnorm", "layernorm"],
-# )
-# def test_distributed_layernorm(
-#     inp_shape, n_devices, is_rmsnorm, dtype, stats_dtype, iterations, all_devices
-# ):
-#     if len(all_devices) != 8:
-#         pytest.skip("Not T3000!")
-
-#     devices = get_devices_for_t3000(all_devices, n_devices)
-
-#     run_distributed_layernorm(inp_shape, n_devices, is_rmsnorm, dtype, stats_dtype, devices, iterations=iterations)
-
-
-# @skip_for_grayskull("Requires eth connected devices to run")
-# @pytest.mark.parametrize(
-#     "iterations",
-#     [2],
-#     ids=["loops2"],
-# )
-# @pytest.mark.parametrize(
-#     "dtype",
-#     (ttnn.bfloat16),
-#     ids=["BFLOAT16_in"],
-# )
-# @pytest.mark.parametrize(
-#     "stats_dtype",
-#     (ttnn.bfloat16),
-#     ids=["BFLOAT16_stats"],
-# )
+# @pytest.mark.parametrize("input_df", [ttnn.bfloat16])
 # @pytest.mark.parametrize(
 #     "inp_shape",
 #     [
@@ -603,14 +611,14 @@ def test_pre_allgather_layernorm(
 #     ids=["rmsnorm", "layernorm"],
 # )
 # def test_distributed_layernorm_with_program_cache(
-#     inp_shape, n_devices, is_rmsnorm, dtype, stats_dtype, iterations, all_devices, use_program_cache
+#     inp_shape, n_devices, is_rmsnorm, input_df, iterations, all_devices, use_program_cache
 # ):
 #     if len(all_devices) != 8:
 #         pytest.skip("Not T3000!")
 
 #     devices = get_devices_for_t3000(all_devices, n_devices)
 
-#     run_distributed_layernorm(inp_shape, n_devices, is_rmsnorm, dtype, stats_dtype, devices, iterations=iterations)
+#     run_distributed_layernorm(inp_shape, n_devices, is_rmsnorm, input_df, devices, iterations=iterations)
 
 #     for d in range(len(devices)):
 #         assert devices[d].num_program_cache_entries() == 3, "Program cache should have only 3 entries, but has " + str(
