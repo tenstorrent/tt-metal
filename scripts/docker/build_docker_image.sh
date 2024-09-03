3d6de59b9af394a73e47580731c5d931e4bb5cbf
Michael Chiou <156848643+ttmchiou@users.noreply.github.com>
2024-09-03 00:37:57 -0700

    Revert "#11132 Run Post-Commit Python Tests against wheels  (#11870)"
    This reverts commit 089cd2f2984559a158dbcdf1095fdd41df19664f.

--- scripts/docker/build_docker_image.sh
+++ scripts/docker/build_docker_image.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-TT_METAL_DOCKERFILE="${1:-ubuntu-20.04-amd64}"
-TT_METAL_DOCKER_IMAGE_TAG="${2:-$TT_METAL_DOCKERFILE}"
+TT_METAL_DOCKER_IMAGE_TAG=${1:-ubuntu-20.04-amd64}
 
 TT_METAL_HOME=$(git rev-parse --show-toplevel)
 (
   cd ${TT_METAL_HOME} || exit
-  docker build -f dockerfile/${TT_METAL_DOCKERFILE}.Dockerfile -t ${TT_METAL_DOCKER_IMAGE_TAG} .
+  docker build -f dockerfile/${TT_METAL_DOCKER_IMAGE_TAG}.Dockerfile -t ${TT_METAL_DOCKER_IMAGE_TAG}:latest .
 )
--- tests/scripts/run_ttnn_examples.sh
+++ tests/scripts/run_ttnn_examples.sh
@@ -2,6 +2,11 @@
 
 set -eo pipefail
 
+if [[ -z "$TT_METAL_HOME" ]]; then
+  echo "Must provide TT_METAL_HOME in environment" 1>&2
+  exit 1
+fi
+
 for example in ttnn/examples/usage/*.py; do
   echo "[Info] Running ttnn example: $example"
   python3 $example
