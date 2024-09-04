03ccbd08b89d4ac1ffa9291b1dac9010a9b36b13
Stefan Djordjevic <sdjordjevic@tenstorrent.com>
2024-09-04 16:33:41 +0000

    #12101: Fixing reduce to account padding while calculating output dimension

--- tests/ttnn/unit_tests/operations/test_max.py
+++ tests/ttnn/unit_tests/operations/test_max.py
@@ -11,9 +11,9 @@ from tests.ttnn.utils_for_testing import assert_with_pcc
 from models.utility_functions import torch_random
 
 
-@pytest.mark.parametrize("batch_size", [1, 16])
-@pytest.mark.parametrize("h", [32, 64])
-@pytest.mark.parametrize("w", [32, 64])
+@pytest.mark.parametrize("batch_size", [1, 16, 1, 16])
+@pytest.mark.parametrize("h", [32, 64, 41, 37])
+@pytest.mark.parametrize("w", [32, 64, 31, 63])
 @pytest.mark.parametrize("dim", [-1, -2])
 def test_max(device, batch_size, h, w, dim):
     torch.manual_seed(0)
@@ -32,9 +32,9 @@ def test_max(device, batch_size, h, w, dim):
     assert_with_pcc(torch_output_tensor, output_tensor)
 
 
-@pytest.mark.parametrize("batch_size", [1, 16])
-@pytest.mark.parametrize("h", [32, 64])
-@pytest.mark.parametrize("w", [32, 64])
+@pytest.mark.parametrize("batch_size", [1, 16, 1, 16])
+@pytest.mark.parametrize("h", [32, 64, 41, 37])
+@pytest.mark.parametrize("w", [32, 64, 31, 63])
 def test_max_global(device, batch_size, h, w):
     torch.manual_seed(0)
 
