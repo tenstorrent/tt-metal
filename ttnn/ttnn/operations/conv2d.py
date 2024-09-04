1f2d9101dfe60811df75be943b762b9839ac4eb3
Borys Bradel <bbradel@tenstorrent.com>
2024-09-04 18:39:12 +0000

    #12009: Remove depracated conv setup classes and remove model preprocessing conv tests

--- ttnn/ttnn/operations/conv2d.py
+++ ttnn/ttnn/operations/conv2d.py
@@ -15,7 +15,6 @@ from ttnn.operations.conv.sliding_window_op_utils import (
     get_output_dim as get_conv_output_dim,
 )
 from ttnn.operations.conv.tt_py_composite_conv import (
-    TTPyCompositeConvSetupDepracated,
     SlidingWindowOpParams,
     find_closest_common_largest_divisor,
     find_closest_largest_divisor,
@@ -36,162 +35,6 @@ Conv2dConfig = ttnn._ttnn.operations.conv2d.Conv2dConfig
 get_conv_padded_input_shape_and_mem_config = ttnn._ttnn.operations.conv2d.get_conv_padded_input_shape_and_mem_config
 OptimizedConvParallelizationConfig = ttnn._ttnn.operations.conv2d.OptimizedConvParallelizationConfig
 OptimizedConvBlockConfig = ttnn._ttnn.operations.conv2d.OptimizedConvBlockConfig
-
-
-class Conv2dSetupDepracated:
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        kernel_size: Union[int, Tuple[int, int]],
-        stride: Union[int, Tuple[int, int]] = 1,
-        padding: Union[int, Tuple[int, int]] = 0,
-        dilation: Union[int, Tuple[int, int]] = 1,
-        groups: int = 1,
-        padding_mode: str = "zeros",
-        dtype: ttnn.DataType = None,
-        *,
-        device: ttnn.Device,
-        use_1d_systolic_array: bool,
-        batch_size: int,
-        input_height: int,
-        input_width: int,
-        reader_patterns_cache: Optional[Dict],
-        weight: ttnn.Tensor,
-        bias: ttnn.Tensor = None,
-        math_fidelity: ttnn.MathFidelity = None,
-        weights_dtype: ttnn.DataType = None,
-        activation: str = None,
-        conv_blocking_and_parallelization_config_override: Dict = None,
-        reallocate_halo_output: bool = False,
-        using_parameters_cache: bool = False,
-        move_weights_to_device: bool = True,
-        use_shallow_conv_variant: bool = False,
-        transpose_mcast: bool = True,
-        enable_auto_formatting: bool = False,
-        deallocate_activation: bool = False,
-        padded_input_channels: Optional[int] = None,
-        compute_kernel_config: Union[ttnn.GrayskullComputeKernelConfig, ttnn.WormholeComputeKernelConfig] = None,
-        use_dram_for_matmul: bool = False,
-        output_layout: ttnn.Layout = ttnn.TILE_LAYOUT,
-    ):
-        assert (
-            padding_mode == "zeros"
-        ), f"Only convs with padding_mode=zeroes supported. Found padding_mode set to {padding_mode}."
-        if isinstance(kernel_size, int):
-            window_h = kernel_size
-            window_w = kernel_size
-        else:
-            window_h, window_w = kernel_size
-
-        if isinstance(stride, int):
-            stride_h = stride
-            stride_w = stride
-        else:
-            stride_h, stride_w = stride
-
-        if isinstance(padding, int):
-            pad_h = padding
-            pad_w = padding
-        else:
-            pad_h, pad_w = padding
-
-        if isinstance(dilation, int):
-            dilation_h = dilation
-            dilation_w = dilation
-        else:
-            dilation_h, dilation_w = dilation
-
-        assert dilation_h == 1, f"Only convs with dilation == 1 supported. Found dilation_h={dilation_h}"
-        assert dilation_w == 1, f"Only convs with dilation == 1 supported. Found dilation_w={dilation_w}"
-        assert groups == 1, "Only convs with groups == 1 supported"
-        sliding_window_op_params = SlidingWindowOpParams(
-            stride_h=stride_h,
-            stride_w=stride_w,
-            pad_h=pad_h,
-            pad_w=pad_w,
-            window_h=window_h,
-            window_w=window_w,
-            batch_size=batch_size,
-            input_h=input_height,
-            input_w=input_width,
-        )
-        fuse_relu = False
-        if activation is not None:
-            activation = activation.lower()
-            assert activation == "relu", f"Only support relu fusion with conv. Got activation={activation}."
-            fuse_relu = True
-        self.conv_setup = TTPyCompositeConvSetupDepracated(
-            sliding_window_op_params,
-            weight,
-            out_channels,
-            in_channels,
-            device,
-            use_1d_systolic_array,
-            reader_patterns_cache,
-            bias=bias,
-            conv_blocking_and_parallelization_config_override=conv_blocking_and_parallelization_config_override,
-            fuse_relu=fuse_relu,
-            output_dtype=dtype,
-            weights_dtype=weights_dtype,
-            math_fidelity=math_fidelity,
-            move_utwh_output=reallocate_halo_output,
-            using_parameters_cache=using_parameters_cache,
-            move_weights_to_device=move_weights_to_device,
-            use_shallow_conv_variant=use_shallow_conv_variant,
-            transpose_mcast=transpose_mcast,
-            enable_auto_formatting=enable_auto_formatting,
-            deallocate_activation=deallocate_activation,
-            padded_input_channels=padded_input_channels,
-            compute_kernel_config=compute_kernel_config,
-            use_dram_for_matmul=use_dram_for_matmul,
-            output_layout=output_layout,
-        )
-        self.batch_size = batch_size
-        self.input_height = input_height
-        self.input_width = input_width
-        self.output_height = (input_height + (2 * pad_h) - dilation_h * (window_h - 1) - 1) // stride_h + 1
-        self.output_width = (input_width + (2 * pad_w) - dilation_w * (window_w - 1) - 1) // stride_w + 1
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.original_weight = ttnn.to_torch(weight)
-        self.original_bias = None if bias is None else ttnn.to_torch(bias)
-        self.stride_h = stride_h
-        self.stride_w = stride_w
-        self.pad_h = pad_h
-        self.pad_w = pad_w
-        self.dilation_h = dilation_h
-        self.dilation_w = dilation_w
-        self.groups = groups
-
-    def _golden_function_conv2d(self, activations):
-        import torch
-
-        # inputs in [1, 1, NWH, C] format, reshape to N, H, W, C
-        if self.in_channels < 32:
-            activations = activations[:, :, :, : self.in_channels]
-        activations = activations.reshape(self.batch_size, self.input_height, self.input_width, self.in_channels)
-        # permute to N, C, H, W
-        activations = activations.permute(0, 3, 1, 2)
-
-        torch_out = torch.nn.functional.conv2d(
-            activations.float(),
-            self.original_weight.float(),
-            self.original_bias.squeeze().float() if self.original_bias is not None else None,
-            stride=(self.stride_h, self.stride_w),
-            padding=(self.pad_h, self.pad_w),
-            dilation=(self.dilation_h, self.dilation_w),
-            groups=self.groups,
-        )
-        torch_out = torch_out.permute(0, 2, 3, 1)
-        torch_out = torch_out.reshape(1, 1, -1, self.out_channels)
-        return torch_out
-
-    def _golden_function_copy_input(self, input):
-        return input
-
-    def _golden_function_copy_output(self, output):
-        return output
 
 
 # internal. not user facing
@@ -416,21 +259,22 @@ def conv2d(
     debug=False,  # ignored
 ) -> Tuple[ttnn.Tensor, int, int, ttnn.Tensor, ttnn.Tensor]:
     return ttnn._ttnn.operations.conv2d.conv2d(
-            input_tensor=input_tensor,
-            weight_tensor=weight_tensor,
-            device=device,
-            in_channels=in_channels,
-            out_channels=out_channels,
-            batch_size=batch_size,
-            input_height=input_height,
-            input_width=input_width,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            dilation=dilation,
-            groups=groups,
-            bias_tensor=bias_tensor,
-            conv_config=conv_config,
-        )
+        input_tensor=input_tensor,
+        weight_tensor=weight_tensor,
+        device=device,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        batch_size=batch_size,
+        input_height=input_height,
+        input_width=input_width,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        dilation=dilation,
+        groups=groups,
+        bias_tensor=bias_tensor,
+        conv_config=conv_config,
+    )
+
 
 __all__ = []
