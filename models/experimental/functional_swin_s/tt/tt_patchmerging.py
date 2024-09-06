e043b2207391d9c703afbefde2c3ba000673fff2
punith.sekar <punith.sekar@multicorewareinc.com>
2024-09-06 09:22:55 +0000

    #0: shard MM

--- models/experimental/functional_swin_s/tt/tt_patchmerging.py
+++ models/experimental/functional_swin_s/tt/tt_patchmerging.py
@@ -18,6 +18,31 @@ def ttnn_to_torch(input):
     return input
 
 
+program_configs = {
+    "linear_config_1": ttnn.MatmulMultiCoreReuseMultiCast1DProgramConfig(
+        compute_with_storage_grid_size=(8, 8),
+        in0_block_w=1,
+        out_subblock_h=1,
+        out_subblock_w=3,
+        per_core_M=2,
+        per_core_N=6,
+        fuse_batch=True,
+        fused_activation=None,
+        mcast_in0=False,
+    ),
+    "linear_config_2": ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+        compute_with_storage_grid_size=(8, 8),
+        in0_block_w=1,
+        out_subblock_h=1,
+        out_subblock_w=3,
+        per_core_M=4,
+        per_core_N=12,
+        transpose_mcast=False,
+        fused_activation=None,
+    ),
+}
+
+
 class TtPatchMerging:
     def __init__(self, device, parameters, dim):
         self.dim = dim
@@ -41,5 +66,59 @@ class TtPatchMerging:
         x3 = torch_to_ttnn(x3, self.device)
         x = ttnn.concat([x0, x1, x2, x3], -1)
         x = ttnn.layer_norm(x, weight=self.parameters.norm["weight"], bias=self.parameters.norm["bias"])
-        x = ttnn.linear(x, self.parameters.reduction["weight"], core_grid=ttnn.CoreGrid(y=8, x=8))
+        if x.shape[-1] == 384:
+            x = ttnn.to_memory_config(
+                x,
+                memory_config=ttnn.create_sharded_memory_config(
+                    x.shape,
+                    core_grid=ttnn.CoreGrid(y=8, x=8),
+                    strategy=ttnn.ShardStrategy.HEIGHT,
+                    orientation=ttnn.ShardOrientation.ROW_MAJOR,
+                ),
+                dtype=ttnn.bfloat8_b,
+            )
+
+            x = ttnn.linear(
+                x,
+                self.parameters.reduction["weight"],
+                memory_config=ttnn.L1_HEIGHT_SHARDED_MEMORY_CONFIG,
+                dtype=ttnn.bfloat8_b,
+                compute_kernel_config=ttnn.WormholeComputeKernelConfig(
+                    math_fidelity=ttnn.MathFidelity.LoFi,
+                ),
+                program_config=program_configs["linear_config_1"],
+            )
+        elif x.shape[-1] == 768:
+            x = ttnn.to_memory_config(
+                x,
+                memory_config=ttnn.create_sharded_memory_config(
+                    x.shape,
+                    core_grid=ttnn.CoreGrid(y=8, x=8),
+                    strategy=ttnn.ShardStrategy.BLOCK,
+                    orientation=ttnn.ShardOrientation.ROW_MAJOR,
+                ),
+                dtype=ttnn.bfloat8_b,
+            )
+            x = ttnn.linear(
+                x,
+                self.parameters.reduction["weight"],
+                memory_config=ttnn.L1_BLOCK_SHARDED_MEMORY_CONFIG,
+                dtype=ttnn.bfloat8_b,
+                compute_kernel_config=ttnn.WormholeComputeKernelConfig(
+                    math_fidelity=ttnn.MathFidelity.LoFi,
+                ),
+                program_config=program_configs["linear_config_2"],
+            )
+        else:
+            x = ttnn.linear(
+                x,
+                self.parameters.reduction["weight"],
+                dtype=ttnn.bfloat8_b,
+                compute_kernel_config=ttnn.WormholeComputeKernelConfig(
+                    math_fidelity=ttnn.MathFidelity.LoFi,
+                ),
+                core_grid=ttnn.CoreGrid(y=8, x=8),
+            )
+
+        x = ttnn.to_memory_config(x, ttnn.L1_MEMORY_CONFIG, dtype=ttnn.bfloat16)
         return x
