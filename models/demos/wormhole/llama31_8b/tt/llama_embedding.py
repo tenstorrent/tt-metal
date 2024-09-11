540d11b5e3ae479939fffbf2f94d650449a88e26
Mark O'Connor <moconnor@tenstorrent.com>
2024-09-11 13:56:11 +0000

    #12328: Move embeddings on device

--- models/demos/wormhole/llama31_8b/tt/llama_embedding.py
+++ models/demos/wormhole/llama31_8b/tt/llama_embedding.py
@@ -33,4 +33,6 @@ class TtLlamaEmbedding(torch.nn.Module):
         )
 
     def forward(self, x: ttnn.Tensor) -> ttnn.Tensor:
-        return ttnn.embedding(x, self.weights)
+        x = ttnn.embedding(x, self.weights, layout=ttnn.TILE_LAYOUT)
+        x = ttnn.reshape(x, [x.shape[0], 1, x.shape[1], x.shape[2]])
+        return x
