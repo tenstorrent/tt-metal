9bf1a1a70a92147d60085efb242e80a8db474829
punith.sekar <punith.sekar@multicorewareinc.com>
2024-09-12 10:14:12 +0000

    #0: Precompute attn_mask tensor

--- tests/ttnn/integration_tests/swin_s/test_ttnn_swin_transformer_block.py
+++ tests/ttnn/integration_tests/swin_s/test_ttnn_swin_transformer_block.py
@@ -19,6 +19,48 @@ from tests.ttnn.integration_tests.swin_s.test_ttnn_mlp import (
 import pytest
 
 
+def preprocess_attn_mask(input_shape, patch_size, window_size, shift_size, device):
+    h, w = input_shape[2], input_shape[3]
+    attention_h = ((h - (patch_size[0] - 1) - 1) // patch_size[0]) + 1
+    attention_w = ((w - (patch_size[0] - 1) - 1) // patch_size[0]) + 1
+    attn_mask_tuple = ()
+    for i in range(4):
+        pad_r = (window_size[1] - attention_w % window_size[1]) % window_size[1]
+        pad_b = (window_size[0] - attention_h % window_size[0]) % window_size[0]
+        pad_H = attention_h + pad_b
+        pad_W = attention_w + pad_r
+        num_windows = (pad_H // window_size[0]) * (pad_W // window_size[1])
+
+        attn_mask = torch.zeros((pad_H, pad_W))
+        h_slices = (
+            (0, -window_size[0]),
+            (-window_size[0], -shift_size[0]),
+            (-shift_size[0], None),
+        )
+        w_slices = (
+            (0, -window_size[1]),
+            (-window_size[1], -shift_size[1]),
+            (-shift_size[1], None),
+        )
+        count = 0
+        for h in h_slices:
+            for w in w_slices:
+                attn_mask[h[0] : h[1], w[0] : w[1]] = count
+                count += 1
+        attn_mask = attn_mask.view(pad_H // window_size[0], window_size[0], pad_W // window_size[1], window_size[1])
+        attn_mask = attn_mask.permute(0, 2, 1, 3).reshape(num_windows, window_size[0] * window_size[1])
+        attn_mask = attn_mask.unsqueeze(1) - attn_mask.unsqueeze(2)
+        attn_mask = attn_mask.masked_fill(attn_mask != 0, float(-100.0)).masked_fill(attn_mask == 0, float(0.0))
+        attn_mask = attn_mask.unsqueeze(1).unsqueeze(0)
+        attn_mask = ttnn.from_torch(attn_mask, device=device, layout=ttnn.TILE_LAYOUT)
+        attention_h = attention_h // 2
+        attention_w = attention_w // 2
+
+        attn_mask_tuple += (attn_mask,)
+
+    return attn_mask_tuple
+
+
 def create_custom_preprocessor(device):
     def custom_preprocessor(torch_model, name, ttnn_module_args):
         parameters = {}
@@ -62,9 +104,9 @@ def create_custom_preprocessor(device):
         (192, [7, 7], [0, 0], 6, 64, 3, 0),
         (192, [7, 7], [3, 3], 6, 64, 3, 1),
         (384, [7, 7], [0, 0], 12, 32, 5, 0),
-        (384, [7, 7], [3, 3], 12, 32, 5, 1),
+        (384, [7, 7], [3, 3], 12, 32, 5, 1),  # Low pcc after precomputing attn_mask outside,  check it
         (768, [7, 7], [0, 0], 24, 16, 7, 0),
-        (768, [7, 7], [3, 3], 24, 16, 7, 1),
+        (768, [7, 7], [3, 3], 24, 16, 7, 1),  # Low pcc after precomputing attn_mask outside, check it
     ],
 )
 def test_swin_transformer_block(
@@ -73,6 +115,17 @@ def test_swin_transformer_block(
     model = models.swin_s(weights="IMAGENET1K_V1")
     state_dict = model.state_dict()
     ds_state_dict = {k: v for k, v in state_dict.items() if (k.startswith(f"features.{i}.{j}."))}
+
+    attn_mask_tuple = preprocess_attn_mask([1, 3, 512, 512], [4, 4], [7, 7], [3, 3], device)
+
+    if seq_len == 128:
+        attn_mask = attn_mask_tuple[0]
+    elif seq_len == 64:
+        attn_mask = attn_mask_tuple[1]
+    elif seq_len == 32:
+        attn_mask = attn_mask_tuple[2]
+    elif seq_len == 16:
+        attn_mask = attn_mask_tuple[3]
 
     torch_model = SwinTransformerBlock(dim, num_heads, window_size, shift_size)
 
@@ -92,7 +145,9 @@ def test_swin_transformer_block(
         initialize_model=lambda: torch_model, custom_preprocessor=create_custom_preprocessor(device), device=device
     )
 
-    ttnn_model = TtSwinTransformerBlock(device, parameters, dim, num_heads, window_size, shift_size)
+    ttnn_model = TtSwinTransformerBlock(
+        device, parameters, dim, num_heads, window_size, shift_size, attn_mask=attn_mask
+    )
 
     input_tensor = ttnn.from_torch(torch_input_tensor, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device)
 
