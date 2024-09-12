076431d41554e46aea50ed38845f54b7847a105f
Hai Dang Tran <44207868+thd1007@users.noreply.github.com>
2024-09-12 12:39:26 +0700

    #12371: Migrate `moreh_getitem` operation from `tt_eager` to `ttnn`  (#12372)
    #12371: un-deprecate moreh_getitem

---
+++ ttnn/cpp/ttnn/operations/moreh/moreh_getitem/moreh_getitem_pybind.cpp
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "moreh_getitem_pybind.hpp"
+
+#include "pybind11/decorators.hpp"
+#include "ttnn/operations/moreh/moreh_getitem/moreh_getitem.hpp"
+
+namespace ttnn::operations::moreh::moreh_getitem {
+void bind_moreh_getitem_operation(py::module& module) {
+    bind_registered_operation(
+        module,
+        ttnn::moreh_getitem,
+        "Moreh moreh_getitem operation",
+        ttnn::pybind_arguments_t{
+            py::arg("input"),
+            py::arg("index_tensors"),
+            py::arg("index_dims"),
+            py::kw_only(),
+            py::arg("output") = std::nullopt,
+            py::arg("output_memory_config") = std::nullopt});
+}
+}  // namespace ttnn::operations::moreh::moreh_getitem
--- ttnn/cpp/ttnn/operations/moreh/moreh_pybind.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_pybind.cpp
@@ -6,10 +6,12 @@
 
 #include "ttnn/operations/moreh/moreh_adam/moreh_adam_pybind.hpp"
 #include "ttnn/operations/moreh/moreh_arange/moreh_arange_pybind.hpp"
+#include "ttnn/operations/moreh/moreh_getitem/moreh_getitem_pybind.hpp"
 
 namespace ttnn::operations::moreh {
 void bind_moreh_operations(py::module &module) {
     moreh_arange::bind_moreh_arange_operation(module);
     moreh_adam::bind_moreh_adam_operation(module);
+    moreh_getitem::bind_moreh_getitem_operation(module);
 }
 }  // namespace ttnn::operations::moreh
