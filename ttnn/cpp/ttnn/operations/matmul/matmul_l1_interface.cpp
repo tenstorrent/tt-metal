030f1c64bfd98ed7db5ddd3a2da959fba3c13540
Dusan Erdeljan <derdeljan@tenstorrent.com>
2024-09-13 14:51:57 +0000

    #0: Fix CB sharded operand space reuse

--- ttnn/cpp/ttnn/operations/matmul/matmul_l1_interface.cpp
+++ ttnn/cpp/ttnn/operations/matmul/matmul_l1_interface.cpp
@@ -6,6 +6,8 @@
 #include <tuple>
 #include <variant>
 
+#include "impl/buffers/buffer.hpp"
+#include "impl/buffers/buffer_constants.hpp"
 #include "ttnn/operations/common/l1_interface_common.hpp"
 #include "ttnn/operations/matmul/device/matmul_types.hpp"
 #include "ttnn/tensor/tensor_utils.hpp"
@@ -41,19 +43,27 @@ MatmulMultiCoreReuseMultiCastOpL1Usage::get_circular_buffer_l1_allocations_per_c
     uint32_t num_blocks = K / program_config.in0_block_w;
     const uint32_t input_cb_size_multiplier = B * num_blocks > 1 ? 2 : 1;
 
-    uint32_t in0_CB_tiles = input_cb_size_multiplier * program_config.per_core_M * program_config.in0_block_w;
-    sizes.push_back(std::make_tuple(in0_CB_tiles * get_tile_size(input_a), get_num_of_cores()));
+    uint32_t in0_CB_size = has_layout(input_a, tt::tt_metal::TensorMemoryLayout::HEIGHT_SHARDED)
+                               ? c_cb_shares_space_with_sharded_operand
+                               : input_cb_size_multiplier * program_config.per_core_M * program_config.in0_block_w *
+                                     get_tile_size(input_a);
+    sizes.push_back(std::make_tuple(in0_CB_size, get_num_of_cores()));
 
-    uint32_t in1_CB_tiles = input_cb_size_multiplier * program_config.per_core_N * program_config.in0_block_w;
-    sizes.push_back(std::make_tuple(in1_CB_tiles * get_tile_size(input_b), get_num_of_cores()));
+    uint32_t in1_CB_size = is_sharded(input_b) && !std::get<tt::tt_metal::MemoryConfig>(input_b).is_dram()
+                               ? c_cb_shares_space_with_sharded_operand
+                               : input_cb_size_multiplier * program_config.per_core_N * program_config.in0_block_w *
+                                     get_tile_size(input_b);
+    sizes.push_back(std::make_tuple(in1_CB_size, get_num_of_cores()));
 
     if (has_layout(input_a, TensorMemoryLayout::BLOCK_SHARDED)) {
         sizes.push_back(std::make_tuple(c_cb_shares_space_with_sharded_operand, get_num_of_cores()));
         sizes.push_back(std::make_tuple(2 * 32, get_num_of_cores()));
     }
 
-    uint32_t out_block_tiles = program_config.per_core_M * program_config.per_core_N;
-    sizes.push_back(std::make_tuple(out_block_tiles * get_tile_size(input_a), get_num_of_cores()));
+    uint32_t out_block_size = is_sharded(output)
+                                  ? c_cb_shares_space_with_sharded_operand
+                                  : program_config.per_core_M * program_config.per_core_N * get_tile_size(output);
+    sizes.push_back(std::make_tuple(out_block_size, get_num_of_cores()));
 
     return sizes;
 }
