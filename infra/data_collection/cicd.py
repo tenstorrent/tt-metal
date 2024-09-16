e7f1a1ef7b2596c7ac7d3ced0942c61f77068f8c
Raymond Kim <rkim@tenstorrent.com>
2024-09-16 11:20:48 -0400

    #6857: intentionally fail

--- infra/data_collection/cicd.py
+++ infra/data_collection/cicd.py
@@ -16,7 +16,7 @@ from infra.data_collection import pydantic_models
 
 
 def create_cicd_json_for_data_analysis(
-    github_runner_environment,
+        github_runner_environment,
     github_pipeline_json_filename,
     github_jobs_json_filename,
     cicd_json_filename=None,
