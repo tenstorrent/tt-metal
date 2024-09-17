9bd9f54eb64871c21d93e58c3ca939575308e092
Stuti Raizada <159130512+sraizada-tt@users.noreply.github.com>
2024-09-17 17:03:53 +0100

    #0: Update plusone_pybind.cpp

--- ttnn/cpp/ttnn/operations/experimental/plusone/plusone_pybind.cpp
+++ ttnn/cpp/ttnn/operations/experimental/plusone/plusone_pybind.cpp
@@ -15,7 +15,7 @@ void bind_experimental_plusone_operation(py::module& module) {
 
             Returns input tensor elements increased by 1.
             Input tensor must have UINT32 data type, ROW_MAJOR layout, and 1-D shape.
-            This op only gives decent performace for small tensors (upto 100 elements).
+            This op only gives decent performance for small tensors (upto 100 elements).
 
             Equivalent pytorch code:
 
