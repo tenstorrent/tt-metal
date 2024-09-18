728a05bdf01db0891ff55ad01337a3d8f274ae9e
Brian Liu <bliu@tenstorrent.com>
2024-09-18 20:50:42 +0000

    #0: Update device perf for unet

--- models/experimental/functional_unet/tests/test_unet_perf.py
+++ models/experimental/functional_unet/tests/test_unet_perf.py
@@ -39,7 +39,7 @@ def synchronize_devices(device):
 @pytest.mark.models_device_performance_bare_metal
 @pytest.mark.parametrize(
     "batch, groups, expected_device_perf_fps",
-    ((2, 1, 639.0),),
+    ((2, 1, 656.0),),
 )
 def test_unet_perf_device(batch: int, groups: int, expected_device_perf_fps: float, reset_seeds):
     command = f"pytest models/experimental/functional_unet/tests/test_unet_model.py::test_unet_model[device_params0-{groups}-{batch}]"
