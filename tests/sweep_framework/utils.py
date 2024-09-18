3255f662e55a96813da6e52aec761528ae829d01
Andrija Malbasa <amalbasa@tenstorrent.com>
2024-09-18 17:20:43 +0200

    Add subtract, subalpha and rsub sweeps, interleaved (#12822)
    * #11512: Add sub sweep
    
    * #11512: Add subalpha sweep and utils.py
    
    * #11512: Add rsub sweep
    
    * #11512: Add subtract, subalpha and rsub to ttnn-run-sweeps.yaml
    
    * #11512: Add sa copyright note and a license header to sweep_framework/sweeps/utils.py
    
    * #11512: Minor fixes
    
    * #11512: Change parameter names for subtract, subalpha and rsub sweeps to nightly.
    
    * Remove ttnn_ops.py change

---
+++ tests/sweep_framework/utils.py
@@ -0,0 +1,39 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+
+import random
+from itertools import product
+
+
+def gen_shapes(start_shape, end_shape, interval, num_samples="all"):
+    def align_to_interval(x, start_val, interval):
+        dx = x - start_val
+        dx = (dx // interval) * interval
+        return start_val + dx
+
+    shapes = []
+
+    num_dims = len(start_shape)
+
+    if num_samples == "all":
+        dim_ranges = [range(start_shape[i], end_shape[i] + interval[i], interval[i]) for i in range(num_dims)]
+
+        for shape in product(*dim_ranges):
+            shapes.append(shape)
+
+    else:
+        samples_id = 0
+
+        while samples_id < num_samples:
+            shape = []
+
+            for i in range(-num_dims, 0):
+                x = random.randint(start_shape[i], end_shape[i])
+                shape.append(align_to_interval(x, start_shape[i], interval[i]))
+
+            samples_id += 1
+            shapes.append(shape)
+
+    return shapes
