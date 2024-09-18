523ba80644cb3f0355395f60da0621c1cd9eb2ba
Colman Glagovich <114512306+cglagovichTT@users.noreply.github.com>
2024-09-18 14:42:45 -0400

    Update paged_fill_cache_program_factory.cpp

--- ttnn/cpp/ttnn/operations/experimental/paged_cache/device/paged_fill_cache_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/experimental/paged_cache/device/paged_fill_cache_program_factory.cpp
@@ -47,8 +47,6 @@ operation::ProgramWithCallbacks paged_fill_cache_multi_core(const Tensor& cache_
     uint32_t log2_page_table_stick_size_B = std::log2(page_table_stick_size_B);
     tt::DataFormat page_table_data_format = tt_metal::datatype_to_dataformat_converter(page_table_tensor.get_dtype());
 
-    // TT_FATAL(1 << log2_page_table_stick_size_B == page_table_stick_size_B, "page_table_stick_size_B must be a power of 2");
-
     tt_metal::Device *device = input_tensor.device();
 
     auto compute_with_storage_grid_size = device->compute_with_storage_grid_size();
