225fa3a0104e3a66c0ec96511b4600343807c8b9
amalbasaTT <amalbasa@tenstorrent.com>
2024-09-18 12:29:39 +0000

    #11512: Add subalpha sweep and utils.py

--- tests/sweep_framework/sweeps/eltwise/subalpha/subalpha_interleaved.py
+++ tests/sweep_framework/sweeps/eltwise/subalpha/subalpha_interleaved.py
@@ -5,8 +5,10 @@
 from typing import Optional, Tuple
 
 import torch
-
+from tests.sweep_framework.permutations import *
+import random
 import ttnn
+from tests.sweep_framework.sweeps.utils import gen_shapes
 
 from tests.ttnn.utils_for_testing import check_with_pcc, start_measuring_time, stop_measuring_time
 from models.utility_functions import torch_random
@@ -14,23 +16,18 @@ from models.utility_functions import torch_random
 # Override the default timeout in seconds for hang detection.
 TIMEOUT = 30
 
+random.seed(0)
+
+
 # Parameters provided to the test vector generator are defined here.
 # They are defined as dict-type suites that contain the arguments to the run function as keys, and lists of possible inputs as values.
 # Each suite has a key name (in this case "suite_1" and "suite_2") which will associate the test vectors to this specific suite of inputs.
 # Developers can create their own generator functions and pass them to the parameters as inputs.
 parameters = {
     "suite_1": {
-        "input_shape": [
-            [8, 1, 33, 256],
-            [8, 1, 256, 32],
-            [8, 8, 256, 384],
-            [8, 5, 13, 512],
-            [8, 5, 32, 512],
-            [1, 1, 32, 16384],
-        ],
-        "broadcast": [None, "h", "w", "hw", "unary"],
-        "input_a_dtype": [ttnn.bfloat16],
-        "input_b_dtype": [ttnn.bfloat16],
+        "input_shape": gen_shapes([1, 1, 32, 32], [6, 12, 256, 256], [1, 1, 32, 32], 32),
+        "input_a_dtype": [ttnn.bfloat16, ttnn.bfloat8_b],
+        "input_b_dtype": [ttnn.bfloat16, ttnn.bfloat8_b],
         "input_a_layout": [ttnn.TILE_LAYOUT],
         "input_b_layout": [ttnn.TILE_LAYOUT],
         "input_b_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
@@ -40,22 +37,12 @@ parameters = {
 }
 
 
-# Invalidate vector is called during the generation phase where each vector will be passed in.
-# If invalidated, the vector will still be stored but will be skipped.
-# Returns False, None if the vector is valid, and True, str with a reason for invalidation if it is invalid.
-def invalidate_vector(test_vector) -> Tuple[bool, Optional[str]]:
-    if test_vector["broadcast"] in {"w", "hw"} and test_vector["input_b_layout"] == ttnn.ROW_MAJOR_LAYOUT:
-        return True, "Broadcasting along width is not supported for row major layout"
-    return False, None
-
-
 # This is the run instructions for the test, defined by the developer.
 # The run function must take the above-defined parameters as inputs.
 # The runner will call this run function with each test vector, and the returned results from this function will be stored.
 # If you defined a mesh_device_fixture above, the object you yielded will be passed into this function as 'device'. Otherwise, it will be the default ttnn device opened by the infra.
 def run(
     input_shape,
-    broadcast,
     input_a_dtype,
     input_b_dtype,
     input_a_layout,
@@ -66,17 +53,43 @@ def run(
     *,
     device,
 ) -> list:
-    input_shape_b = input_shape
+    if (
+        input_a_dtype == ttnn.bfloat16
+        and input_b_dtype == ttnn.bfloat16
+        and input_a_memory_config == ttnn.DRAM_MEMORY_CONFIG
+        and input_b_memory_config == ttnn.DRAM_MEMORY_CONFIG
+        and output_memory_config == ttnn.DRAM_MEMORY_CONFIG
+    ):
+        print(input_shape)
+    data_seed = random.randint(0, 20000000)
+    torch.manual_seed(data_seed)
 
-    if broadcast == "hw":
-        input_shape_b[-1] = 1
-        input_shape_b[-2] = 1
-    elif broadcast == "h":
-        input_shape_b[-2] = 1
-    elif broadcast == "w":
-        input_shape_b[-1] = 1
+    torch_input_tensor_a = torch_random(input_shape, -100, 100, dtype=torch.float32)
+    torch_input_tensor_b = torch_random(input_shape, -100, 100, dtype=torch.float32)
 
-    torch_input_tensor_a = torch_random(input_shape, -100, 100, dtype=torch.bfloat16)
+    if input_a_dtype == ttnn.bfloat16:
+        torch_input_tensor_a = torch_input_tensor_a.to(torch.bfloat16)
+
+    elif input_a_dtype == ttnn.bfloat8_b:
+        tt_tensor = ttnn.from_torch(
+            torch_input_tensor_a, dtype=ttnn.bfloat8_b, layout=ttnn.TILE_LAYOUT, device=None, memory_config=None
+        )
+
+        torch_input_tensor_a = ttnn.to_torch(tt_tensor)
+
+    if input_b_dtype == ttnn.bfloat16:
+        torch_input_tensor_b = torch_input_tensor_b.to(torch.bfloat16)
+
+    elif input_b_dtype == ttnn.bfloat8_b:
+        tt_tensor = ttnn.from_torch(
+            torch_input_tensor_b, dtype=ttnn.bfloat8_b, layout=ttnn.TILE_LAYOUT, device=None, memory_config=None
+        )
+
+        torch_input_tensor_b = ttnn.to_torch(tt_tensor)
+
+    alpha = torch.tensor(1, dtype=torch.bfloat16).uniform_(-100, 100).item()
+
+    torch_output_tensor = torch.sub(torch_input_tensor_a, torch_input_tensor_b, alpha=alpha)
 
     input_tensor_a = ttnn.from_torch(
         torch_input_tensor_a,
@@ -86,23 +99,16 @@ def run(
         memory_config=input_b_memory_config,
     )
 
-    if broadcast == "unary":
-        torch_input_tensor_b = torch_random(1, -100, 100, dtype=torch.bfloat16).item()
-        input_tensor_b = torch_input_tensor_b
-    else:
-        torch_input_tensor_b = torch_random(input_shape_b, -100, 100, dtype=torch.bfloat16)
-        input_tensor_b = ttnn.from_torch(
-            torch_input_tensor_b,
-            dtype=input_b_dtype,
-            layout=input_b_layout,
-            device=device,
-            memory_config=input_a_memory_config,
-        )
-
-    torch_output_tensor = torch.sub(torch_input_tensor_a, torch_input_tensor_b)
+    input_tensor_b = ttnn.from_torch(
+        torch_input_tensor_b,
+        dtype=input_b_dtype,
+        layout=input_b_layout,
+        device=device,
+        memory_config=input_a_memory_config,
+    )
 
     start_time = start_measuring_time()
-    output_tensor = ttnn.subtract(input_tensor_a, input_tensor_b, memory_config=output_memory_config)
+    output_tensor = ttnn.subalpha(input_tensor_a, input_tensor_b, alpha=alpha, memory_config=output_memory_config)
     output_tensor = ttnn.to_torch(output_tensor)
     e2e_perf = stop_measuring_time(start_time)
 
