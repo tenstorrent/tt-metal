b3a7aa79d5d775c2cc95e582a47fff86ed96fa05
Paul Keller <pkeller@tenstorrent.com>
2024-09-20 16:16:28 +0000

    #11789: Add test for initializing local memory

---
+++ tests/tt_metal/tt_metal/test_kernels/misc/local_mem.cpp
@@ -0,0 +1,54 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "debug/assert.h"
+
+volatile uint32_t nonzero[4] = {
+    0xAABB0000,
+    0xAABB0001,
+    0xAABB0002,
+    0xAABB0003,
+};
+volatile uint32_t zero[4] = { 0, 0, 0, 0 };
+
+#if defined(COMPILE_FOR_BRISC) || defined(COMPILE_FOR_NCRISC) || defined(COMPILE_FOR_ERISC) || defined(COMPILE_FOR_IDLE_ERISC)
+void kernel_main() {
+#else
+#include "compute_kernel_api/common.h"
+namespace NAMESPACE {
+void MAIN {
+#endif
+
+    if (nonzero[0] != 0xAABB0000 ||
+        nonzero[1] != 0xAABB0001 ||
+        nonzero[2] != 0xAABB0002 ||
+        nonzero[3] != 0xAABB0003) {
+        ASSERT(0);
+        while(1);
+    }
+
+    if (zero[0] != 0 ||
+        zero[1] != 0 ||
+        zero[2] != 0 ||
+        zero[3] != 0) {
+        ASSERT(0);
+        while(1);
+    }
+
+    // Ensure back to back runs get fresh data
+    nonzero[0] = 0xdeadbeef;
+    nonzero[1] = 0xdeadbeef;
+    nonzero[2] = 0xdeadbeef;
+    nonzero[3] = 0xdeadbeef;
+
+    zero[0] = 0xdeadbeef;
+    zero[1] = 0xdeadbeef;
+    zero[2] = 0xdeadbeef;
+    zero[3] = 0xdeadbeef;
+#if defined(COMPILE_FOR_BRISC) || defined(COMPILE_FOR_NCRISC) || defined(COMPILE_FOR_ERISC) || defined(COMPILE_FOR_IDLE_ERISC)
+}
+#else
+}
+}
+#endif
