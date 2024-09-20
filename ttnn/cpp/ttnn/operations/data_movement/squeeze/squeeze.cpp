649bdeba6da63cd12955efda1a21232aabbd9360
Naif Tarafdar <135640067+tarafdarTT@users.noreply.github.com>
2024-09-20 17:12:52 -0400

    Fix squeeze Dim == 0 with padding  (#12940)
    #12938: squeeze fix for dim == 0 with padding

--- ttnn/cpp/ttnn/operations/data_movement/squeeze/squeeze.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/squeeze/squeeze.cpp
@@ -13,30 +13,33 @@ ttnn::Tensor SqueezeOperation::invoke(
     const int dim
     ) {
 
-    const auto tensor_shape = input_tensor.get_shape();
-    const auto rank = tensor_shape.rank();
-    std::vector<uint32_t> output_shape_vector;
+    const auto original_logical_shape = input_tensor.get_shape();
+    const auto padded_shape = input_tensor.get_shape().with_tile_padding();
+    const auto input_tensor_rank = original_logical_shape.rank();
 
     int normal_dim =  dim;
     if (dim < 0) {
         // Handle negative dimension by converting it to positive
-        normal_dim += rank;
+        normal_dim += input_tensor_rank;
     }
 
-    // Remove the dimension if it is of size 1
-    for (size_t i = 0; i < tensor_shape.size(); ++i) {
-        if (static_cast<int>(i) != normal_dim || tensor_shape[i] != 1) {
-            output_shape_vector.push_back(tensor_shape[i]);
+    std::vector<uint32_t> original_logical_shape_vector(input_tensor_rank - 1);
+    std::vector<uint32_t> padded_shape_vector(input_tensor_rank - 1);
+    uint32_t vector_id = 0;
+    for(int i=0; i< input_tensor_rank; i++) {
+        if(i != normal_dim or original_logical_shape[i] != 1) {
+            original_logical_shape_vector[vector_id] = original_logical_shape[i];
+            padded_shape_vector[vector_id] = padded_shape[i];
+            vector_id++;
         }
     }
 
     // If dim is out of range or original dimension was not of size 1, include all dimensions
-    if (dim >= static_cast<int>(tensor_shape.size()) || tensor_shape[dim] != 1) {
+    if (normal_dim >= static_cast<int>(original_logical_shape.size()) || original_logical_shape[normal_dim] != 1) {
         return input_tensor;
     }
 
-    ttnn::Shape output_shape(output_shape_vector);
-    return ttnn::reshape(input_tensor, output_shape);
+    return ttnn::reshape(input_tensor, ttnn::Shape(original_logical_shape_vector, padded_shape_vector));
 
 }
 
