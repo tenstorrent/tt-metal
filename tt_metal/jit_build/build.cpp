115fa1155cf95ed4d20efdd8b9f91d1bbe99dc10
Nathan Sidwell <nsidwell@tenstorrent.com>
2024-09-21 07:14:06 -0400

    #0: Simplify erisc entry/exit (#12702)

--- tt_metal/jit_build/build.cpp
+++ tt_metal/jit_build/build.cpp
@@ -350,7 +350,6 @@ JitBuildEthernet::JitBuildEthernet(const JitBuildEnv& env, int which, bool is_fw
 
             if (this->is_fw_) {
                 this->srcs_.push_back("tt_metal/hw/firmware/src/erisc.cc");
-                this->srcs_.push_back("tt_metal/hw/toolchain/erisc-early-exit.S");
             } else {
                 this->srcs_.push_back("tt_metal/hw/firmware/src/erisck.cc");
             }
