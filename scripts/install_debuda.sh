0b2de7be302312feea027aa24d6a54a35a31bb75
Vuk Jovanovic <vjovanovic@tenstorrent.com>
2024-09-23 14:05:25 +0000

    Small script that will install debuda with pip

---
+++ scripts/install_debuda.sh
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt install software-properties-common build-essential libyaml-cpp-dev libboost-all-dev libhwloc-dev libzmq3-dev libgtest-dev libgmock-dev xxd -y
+pip install git+https://github.com/tenstorrent/tt-debuda.git
