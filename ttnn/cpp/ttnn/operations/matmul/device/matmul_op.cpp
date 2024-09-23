61cb429f45df82132775275b5ff16b69b3bc35b3
Sankar Manoj <smanoj@tenstorrent.com>
2024-09-23 14:07:19 +0530

    #12141: Fixed matmul shape validation issue

--- ttnn/cpp/ttnn/operations/matmul/device/matmul_op.cpp
+++ ttnn/cpp/ttnn/operations/matmul/device/matmul_op.cpp
@@ -982,10 +982,10 @@ void Matmul::validate(
         (input_tensor_a.get_layout() == Layout::TILE && input_tensor_b.get_layout() == Layout::TILE),
         "Inputs to matmul must be tilized");
     TT_FATAL(
-        a_shape[-1] == b_shape[-2],
+        a_shape.with_tile_padding()[-1] == b_shape.with_tile_padding()[-2],
         "The width of the first tensor must be equal to the height of the second tensor. Mismatch: width={} height={}",
-        a_shape[-1],
-        b_shape[-2]);
+        a_shape.with_tile_padding()[-1],
+        b_shape.with_tile_padding()[-2]);
 
     TT_FATAL(this->bcast_batch.has_value(), "Error");
     if (this->bcast_batch.value()) {
