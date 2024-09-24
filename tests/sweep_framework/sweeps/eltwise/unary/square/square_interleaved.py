d7ab16342c702607ba2f1ff92924adbb90ddfb7d
dveizovicTT <dveizovic@tenstorrent.com>
2024-09-24 09:11:36 +0000

    #11512: fix low

--- tests/sweep_framework/sweeps/eltwise/unary/square/square_interleaved.py
+++ tests/sweep_framework/sweeps/eltwise/unary/square/square_interleaved.py
@@ -51,7 +51,7 @@ def run(
     torch.manual_seed(data_seed)
 
     torch_input_tensor_a = gen_func_with_cast_tt(
-        partial(torch_random, low=0, high=100, dtype=torch.float32), input_a_dtype
+        partial(torch_random, low=-100, high=100, dtype=torch.float32), input_a_dtype
     )(input_shape)
     torch_output_tensor = torch.square(torch_input_tensor_a)
 
