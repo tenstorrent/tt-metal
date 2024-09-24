1230818eae3fabc6d39ecd2ff37ee260460e6b0b
amalbasaTT <amalbasa@tenstorrent.com>
2024-09-24 13:20:51 +0000

    #11512: Override invalidate_vector in sweeps which don't support grayskull

--- tests/sweep_framework/sweeps/eltwise/binary/fmod/fmod_interleaved.py
+++ tests/sweep_framework/sweeps/eltwise/binary/fmod/fmod_interleaved.py
@@ -12,7 +12,7 @@ from tests.sweep_framework.utils import gen_shapes
 from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_func_with_cast_tt
 
 from tests.ttnn.utils_for_testing import check_with_pcc, start_measuring_time, stop_measuring_time
-from models.utility_functions import torch_random
+from models.utility_functions import torch_random, is_wormhole_b0
 
 # Override the default timeout in seconds for hang detection.
 TIMEOUT = 30
@@ -54,6 +54,8 @@ parameters = {
 
 # TO-DO: Create an issue on this, since these constrictions are not mentioned in the documentation
 def invalidate_vector(test_vector) -> Tuple[bool, Optional[str]]:
+    if not is_wormhole_b0():
+        return True, "Supported only for WH_B0"
     if test_vector["input_a_dtype"] == ttnn.bfloat8_b:
         return True, "Input_tensor_a doesn't support bfloat8_b"
     return False, None
--- tests/sweep_framework/sweeps/eltwise/unary/frac/frac_interleaved.py
+++ tests/sweep_framework/sweeps/eltwise/unary/frac/frac_interleaved.py
@@ -12,7 +12,7 @@ from tests.sweep_framework.utils import gen_shapes
 from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_func_with_cast_tt
 
 from tests.ttnn.utils_for_testing import check_with_pcc, start_measuring_time, stop_measuring_time
-from models.utility_functions import torch_random
+from models.utility_functions import torch_random, is_wormhole_b0
 
 # Override the default timeout in seconds for hang detection.
 TIMEOUT = 30
@@ -32,6 +32,13 @@ parameters = {
         "output_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
     },
 }
+
+
+# TO-DO: Create an issue on this, since these constrictions are not mentioned in the documentation
+def invalidate_vector(test_vector) -> Tuple[bool, Optional[str]]:
+    if not is_wormhole_b0():
+        return True, "Supported only for WH_B0"
+    return False, None
 
 
 # This is the run instructions for the test, defined by the developer.
