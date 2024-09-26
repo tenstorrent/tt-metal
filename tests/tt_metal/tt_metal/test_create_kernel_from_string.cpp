8b8c19cd92b0f0ba8466feb074eda7a973eee9f9
Samarth Agarwal <sagarwal@tenstorrent.com>
2024-09-26 15:03:24 +0000

    #11005: Modified unit tests to run the program after compiling

--- tests/tt_metal/tt_metal/test_create_kernel_from_string.cpp
+++ tests/tt_metal/tt_metal/test_create_kernel_from_string.cpp
@@ -4,8 +4,6 @@
 
 #include <gtest/gtest.h>
 
-#include <unordered_set>
-
 #include "core_coord.h"
 #include "detail/tt_metal.hpp"
 #include "host_api.hpp"
@@ -13,28 +11,13 @@
 #include "impl/kernels/data_types.hpp"
 #include "impl/kernels/kernel_types.hpp"
 #include "impl/program/program.hpp"
-#include "tt_cluster_descriptor_types.h"
+
+#include "tests/tt_metal/tt_metal/unit_tests_common/common/common_fixture.hpp"
 
 using namespace tt;
 using namespace tt::tt_metal;
 
-class CompileProgramWithKernelCreatedFromString : public ::testing::Test {
-   protected:
-    void SetUp() override {
-        vector<chip_id_t> device_ids;
-        const uint32_t num_devices = GetNumAvailableDevices();
-        for (chip_id_t i = 0; i < num_devices; i++) {
-            device_ids.push_back(i);
-        }
-        this->devices_ = detail::CreateDevices(device_ids);
-    }
-
-    void TearDown() override { detail::CloseDevices(this->devices_); }
-
-    std::map<chip_id_t, Device *> devices_;
-};
-
-TEST_F(CompileProgramWithKernelCreatedFromString, DataMovementKernel) {
+TEST_F(CommonFixture, ProgramWithDataMovementKernelCreatedFromString) {
     const CoreRange cores({0, 0}, {1, 1});
     const string &kernel_src_code = R"(
     #include <stdint.h>
@@ -74,12 +57,13 @@ TEST_F(CompileProgramWithKernelCreatedFromString, DataMovementKernel) {
         cores,
         tt_metal::DataMovementConfig{.processor = DataMovementProcessor::RISCV_1, .noc = NOC::RISCV_1_default});
 
-    for (auto [_, device] : this->devices_) {
+    for (Device *device : this->devices_) {
         detail::CompileProgram(device, program);
+        this->RunProgram(device, program);
     };
 }
 
-TEST_F(CompileProgramWithKernelCreatedFromString, ComputeKernel) {
+TEST_F(CommonFixture, ProgramWithComputeKernelCreatedFromString) {
     const CoreRange cores({0, 0}, {1, 1});
     const string &kernel_src_code = R"(
 #include "debug/dprint.h"
@@ -107,12 +91,13 @@ namespace NAMESPACE {
             .math_approx_mode = false,
             .compile_args = {}});
 
-    for (auto [_, device] : this->devices_) {
+    for (Device *device : this->devices_) {
         detail::CompileProgram(device, program);
+        this->RunProgram(device, program);
     };
 }
 
-TEST_F(CompileProgramWithKernelCreatedFromString, EthernetKernel) {
+TEST_F(CommonFixture, ProgramWithEthernetKernelCreatedFromString) {
     const string &kernel_src_code = R"(
 #include <cstdint>
 #include "debug/ring_buffer.h"
@@ -142,9 +127,10 @@ void MAIN {
 #endif
 )";
 
-    for (auto [device_id, device] : this->devices_) {
+    for (Device *device : this->devices_) {
         const std::unordered_set<CoreCoord> &active_ethernet_cores = device->get_active_ethernet_cores(true);
         if (active_ethernet_cores.empty()) {
+            const chip_id_t device_id = device->id();
             log_info(LogTest, "Skipping this test on device {} because it has no active ethernet cores.", device_id);
             continue;
         }
@@ -155,5 +141,6 @@ void MAIN {
             *active_ethernet_cores.begin(),
             tt_metal::EthernetConfig{.noc = tt_metal::NOC::NOC_0});
         detail::CompileProgram(device, program);
+        this->RunProgram(device, program);
     };
 }
