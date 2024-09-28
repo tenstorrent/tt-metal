0398d00d832a6c63201e6c09fa3424e94c03b52a
Nenad Petrovic <109360062+npetrovic-tenstorrent@users.noreply.github.com>
2024-09-28 12:47:52 +0200

    Sweeps: log, log1p, log2, log10 (#13045)
    * #11512: Add log, log1p, log2, log10 sweeps
    
    * #11512: Fix logical sweeps
    
    * #11512: Fix gen_shapes

--- tests/sweep_framework/utils.py
+++ tests/sweep_framework/utils.py
@@ -60,7 +60,7 @@ def gen_shapes(start_shape, end_shape, interval, num_samples="all"):
 
     else:
         samples_id = 0
-
+        TIMEOUT = 5
         while samples_id < num_samples:
             shape = []
 
@@ -68,11 +68,8 @@ def gen_shapes(start_shape, end_shape, interval, num_samples="all"):
                 x = random.randint(start_shape[i], end_shape[i])
                 shape.append(align_to_interval(x, start_shape[i], interval[i]))
 
-            if shape in shapes:
-                pass
-            else:
-                samples_id += 1
-                shapes.append(shape)
+            samples_id += 1
+            shapes.append(shape)
 
     return shapes
 
