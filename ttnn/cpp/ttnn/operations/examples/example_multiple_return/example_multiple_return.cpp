517ff557a2474fd24673a778c7853d0b5586764c
Choi HyungSuk(최형석) <hschoi4448@gmail.com>
2024-09-30 15:11:05 +0900

    #13084: fix return vector optional tensor with launch_op (#13085)
    #13084: fix return vector optional tensor with launch_op

--- ttnn/cpp/ttnn/operations/examples/example_multiple_return/example_multiple_return.cpp
+++ ttnn/cpp/ttnn/operations/examples/example_multiple_return/example_multiple_return.cpp
@@ -7,8 +7,8 @@
 
 namespace ttnn::operations::examples {
 
-std::vector<std::optional<Tensor>> CompositeExampleMutipleReturnOperation::invoke(const Tensor& input_tensor) {
-    return prim::example_multiple_return(input_tensor);
+std::vector<std::optional<Tensor>> CompositeExampleMutipleReturnOperation::invoke(const Tensor& input_tensor, bool return_output1, bool return_output2) {
+    return prim::example_multiple_return(input_tensor, return_output1, return_output2);
 }
 
 std::vector<Tensor> CompositeExampleMutipleReturnOperation::create_async_output_tensors(
@@ -17,6 +17,11 @@ std::vector<Tensor> CompositeExampleMutipleReturnOperation::create_async_output_
     return {
         Tensor(operation::get_workers_for_op_output({input_tensor})),
         Tensor(operation::get_workers_for_op_output({input_tensor}))};
+}
+
+std::vector<bool> CompositeExampleMutipleReturnOperation::create_async_return_flag(const Tensor& input_tensor, bool return_output1, bool return_output2) {
+
+    return {return_output1, return_output2};
 }
 
 }  // namespace ttnn::operations::examples
