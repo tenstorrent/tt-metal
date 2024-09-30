8d64e353ea2bee32d4e2ddf6eebb27aa1cb48da0
Nemanja Grujic <109360083+nemanjagrujic@users.noreply.github.com>
2024-09-30 11:40:39 +0200

    #11512: Added sweep for ttnn.bcast (#13200)
    * #11512: Added sweep for ttnn.bcast
    * #11512: Fixed sweep shape generation

--- tests/sweep_framework/sweeps/eltwise/binary/bcast/bcast.py
+++ tests/sweep_framework/sweeps/eltwise/binary/bcast/bcast.py
@@ -15,7 +15,7 @@ from tests.ttnn.utils_for_testing import check_with_pcc, start_measuring_time, s
 from models.utility_functions import torch_random
 
 # Override the default timeout in seconds for hang detection.
-TIMEOUT = 30
+TIMEOUT = 130
 
 random.seed(0)
 
@@ -24,15 +24,17 @@ random.seed(0)
 # Each suite has a key name (in this case "suite_1" and "suite_2") which will associate the test vectors to this specific suite of inputs.
 # Developers can create their own generator functions and pass them to the parameters as inputs.
 parameters = {
-    "xfail": {
-        "input_shape": gen_shapes([1, 1, 1024, 1024], [1, 4, 1024, 1024], [1, 1, 32, 512], 16),
+    "nightly": {
+        "input_shape": gen_shapes([1, 1, 64, 64], [6, 12, 256, 256], [1, 1, 32, 32], 16),
         "op": [ttnn.BcastOpMath.ADD, ttnn.BcastOpMath.SUB, ttnn.BcastOpMath.MUL],
-        "input_a_dtype": [ttnn.bfloat16],
-        "input_b_dtype": [ttnn.bfloat16],
+        "axis": [ttnn.BcastOpDim.H, ttnn.BcastOpDim.W, ttnn.BcastOpDim.HW],
+        "input_a_dtype": [ttnn.bfloat16, ttnn.bfloat8_b],
+        "input_b_dtype": [ttnn.bfloat16, ttnn.bfloat8_b],
         "input_a_layout": [ttnn.TILE_LAYOUT],
         "input_b_layout": [ttnn.TILE_LAYOUT],
         "input_a_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
         "input_b_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
+        "output_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
     },
 }
 
@@ -44,21 +46,29 @@ parameters = {
 def run(
     input_shape,
     op,
+    axis,
     input_a_dtype,
     input_b_dtype,
     input_a_layout,
     input_b_layout,
     input_a_memory_config,
     input_b_memory_config,
+    output_memory_config,
     *,
     device,
 ) -> list:
     try:
+        input_shape_2 = [input_shape[-4], input_shape[-3], input_shape[-2], input_shape[-1]]
+
+        if axis == ttnn.BcastOpDim.H or axis == ttnn.BcastOpDim.HW:
+            input_shape_2[-2] = 1
+
+        if axis == ttnn.BcastOpDim.W or axis == ttnn.BcastOpDim.HW:
+            input_shape_2[-1] = 1
+
         torch_input_tensor_a = gen_func_with_cast_tt(
             partial(torch_random, low=-100, high=100, dtype=torch.float32), input_a_dtype
         )(input_shape)
-
-        input_shape_2 = [input_shape[-4], input_shape[-3], 1, input_shape[-1]]
         torch_input_tensor_b = gen_func_with_cast_tt(
             partial(torch_random, low=-100, high=100, dtype=torch.float32), input_b_dtype
         )(input_shape_2)
@@ -69,10 +79,13 @@ def run(
             torch_output_tensor = torch.sub(torch_input_tensor_a, torch_input_tensor_b)
         else:
             torch_output_tensor = torch.mul(torch_input_tensor_a, torch_input_tensor_b)
-        torch_input_tensor_b = torch_input_tensor_b.repeat(1, 1, 32, 1)
 
-        # print(f"torch_input_tensor_a.shape {torch_input_tensor_a.shape} **********************************")
-        # print(f"torch_input_tensor_b.shape {torch_input_tensor_b.shape} **********************************")
+        if axis == ttnn.BcastOpDim.H:
+            torch_input_tensor_b = torch_input_tensor_b.repeat(1, 1, 32, 1)
+        elif axis == ttnn.BcastOpDim.W:
+            torch_input_tensor_b = torch_input_tensor_b.repeat(1, 1, 1, 32)
+        else:
+            torch_input_tensor_b = torch_input_tensor_b.repeat(1, 1, 32, 32)
 
         input_tensor_a = ttnn.from_torch(
             torch_input_tensor_a,
@@ -90,21 +103,8 @@ def run(
             memory_config=input_b_memory_config,
         )
 
-        compute_with_storage_grid_size = device.compute_with_storage_grid_size()
-        device_grid_size = ttnn.CoreGrid(y=compute_with_storage_grid_size.y, x=compute_with_storage_grid_size.x)
-
-        block_sharded_mem_config = ttnn.create_sharded_memory_config(
-            shape=input_tensor_a.shape,
-            core_grid=device_grid_size,
-            strategy=ttnn.ShardStrategy.BLOCK,
-            orientation=ttnn.ShardOrientation.ROW_MAJOR,
-            use_height_and_width_as_shard_shape=False,
-        )
-
-        input_tensor_a = ttnn.to_memory_config(input_tensor_a, block_sharded_mem_config)
-
         start_time = start_measuring_time()
-        output_tensor = ttnn.bcast(input_tensor_a, input_tensor_b, op, ttnn.BcastOpDim.H)
+        output_tensor = ttnn.bcast(input_tensor_a, input_tensor_b, op, axis)
         output_tensor = ttnn.to_torch(output_tensor)
 
         e2e_perf = stop_measuring_time(start_time)
