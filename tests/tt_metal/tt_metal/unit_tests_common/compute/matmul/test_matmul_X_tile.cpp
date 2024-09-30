4ea835bef7e5be554c1b3d32d6346ce406ab9e70
Nikola Cvetkovic <ncvetkovic@tenstorrent.com>
2024-09-30 17:58:03 +0000

    #0: Skip FP32 dest acc for GS since it's not possible

--- tests/tt_metal/tt_metal/unit_tests_common/compute/matmul/test_matmul_X_tile.cpp
+++ tests/tt_metal/tt_metal/unit_tests_common/compute/matmul/test_matmul_X_tile.cpp
@@ -48,7 +48,7 @@ void create_test_stimuli(MatmulTileStimuli &stimuli, uint32_t M, uint32_t K, uin
         std::chrono::system_clock::now().time_since_epoch().count()
     );
     stimuli.t = tensor.get_values();
-    
+
     auto activations_tilized = test_utils::tilize(tensor.get_values(), M * 32, K * 32);
     auto activations_tile_layout = convert_to_tile_layout(activations_tilized);
     auto activations = pack_bfloat16_vec_into_uint32_vec(activations_tile_layout);
