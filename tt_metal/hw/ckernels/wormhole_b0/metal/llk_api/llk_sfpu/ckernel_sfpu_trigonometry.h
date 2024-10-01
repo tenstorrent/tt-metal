966be6de924ff203be2405518a0a3054d6be18b0
Chen-Pang He <chen.pang.he@jdh8.org>
2024-10-01 14:19:47 +0000

    #13337: Optimize sine and cosine
    The coefficients in this commit were generated by:
    https://gitlab.inria.fr/sfilip/rminimax
    
    That tool is used in the CORE-MATH project, a library of correctly rounded mathematical functions.
    https://core-math.gitlabpages.inria.fr/

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_trigonometry.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_trigonometry.h
@@ -16,8 +16,9 @@ namespace ckernel {
 
 namespace sfpu {
 
-#define PI   (3.14159265358979323846)
-#define PI_2 (1.570796326794)
+static const float PI = 3.1415927f;
+static const float PI_2 = 1.5707964f;
+static const float FRAC_1_PI = 0.31830987f;
 
 template <bool APPROXIMATION_MODE>
 sfpi_inline vFloat sfpu_tangent_maclaurin_series(vFloat val)
@@ -80,78 +81,28 @@ inline void calculate_tangent()
 }
 
 template <bool APPROXIMATION_MODE>
-sfpi_inline vFloat sfpu_sine_maclaurin_series(vFloat val)
+static vFloat sfpu_sinpi(vFloat x);
+
+template <>
+sfpi_inline vFloat sfpu_sinpi<true>(vFloat x)
 {
-    // Good for [-pi:pi]
-    // Mclauren series = x - x^3/3! + x^5/5! - x^7/7! + x^9/9! - x^11/11!
-    vFloat tmp = val;
-    // x
-    vFloat output = tmp;
-    // x^3/3!
-    tmp = tmp*val*val;
-    output += -0.166666666*tmp;
-    // x^5/5!
-    tmp = tmp*val*val;
-    output +=  0.0083333333*tmp;
-    // x^7/7!
-    tmp = tmp*val*val;
-    output += -0.0001984126*tmp;
+    vFloat xx = x * x;
 
-    // x^9/9!
-    tmp = tmp*val*val;
-    output +=  0.0000027557*tmp;
-
-    if constexpr (not APPROXIMATION_MODE) {
-	// x^11/11!
-        tmp = tmp*val*val;
-        output += -0.00000002505*tmp;
-
-	// x^13/13!
-	tmp = tmp*val*val;
-	output += 1.6059043836821613e-10*(tmp);
-    }
-
-    // Write out output
-    return output;
+    return x * ((0x1.29cf02p+1f
+        * xx - 0x1.4954d4p+2f)
+        * xx + 0x1.92149p+1f);
 }
 
-template <bool APPROXIMATION_MODE>
-sfpi_inline vFloat sfpu_cosine_maclaurin_series(vFloat val)
+template <>
+sfpi_inline vFloat sfpu_sinpi<false>(vFloat x)
 {
-    // Good for [-pi:pi]
-    // Mclauren series = 1 - x^2/2! + x^4/4! - x^6/6! + x^8/8! - x^10/10! + x^12/12!
-    // 1
-    vFloat output = 1.0f;
-    // x^2/2!
-    vFloat tmp = val*val;
-    output += -0.5*tmp;
-    // x^4/4!
-    tmp = tmp*val*val;
-    output +=  0.0416666666*tmp;
-    // x^6/6!
-    tmp = tmp*val*val;
-    output += -0.0013888888*tmp;
+    vFloat xx = x * x;
 
-    // x^8/8!
-    tmp = tmp*val*val;
-    output +=  0.0000248015*tmp;
-
-    // x^10/10!
-    tmp = tmp*val*val;
-    output += -0.0000002755*tmp;
-
-    if constexpr (not APPROXIMATION_MODE) {
-	// x^12/12!
-	tmp = tmp*val*val;
-	output += 2.08767569878681e-9*tmp;
-
-	// x^14/14!
-	tmp = tmp*val*val;
-	output += -1.1470745597729725e-11*tmp;
-    }
-
-    // Write out output
-    return output;
+    return x * ((((0x1.406628p-4f
+        * xx - 0x9.93f86p-4f)
+        * xx + 0x2.8cd64p+0f)
+        * xx - 0x5.2aef6p+0f)
+        * xx + 0x3.243f6cp+0f);
 }
 
 template <bool APPROXIMATION_MODE, int ITERATIONS>
@@ -160,17 +111,13 @@ inline void calculate_sine()
     // SFPU microcode
     for (int d = 0; d < ITERATIONS; d++)
     {
-        vFloat v = dst_reg[0];
-        v = 0.318309886183791f*v; // *1/pi to get number of pi rads.
-        vInt whole_v = float_to_int16(v, 0);
-        vFloat whole_v_float = int32_to_float(whole_v, 0);
-        v = v - whole_v_float;
-        v *= 3.141592653589793f; // fractional * pi to get it in [-pi:pi]
-        v = sfpu_sine_maclaurin_series<APPROXIMATION_MODE>(v);
-        whole_v = whole_v & 0x1;
-        v_if(whole_v != 0) {
-            // odd so flip the sign
-            v *= -1;
+        vFloat v = dst_reg[0] * FRAC_1_PI;
+        vInt whole_v = float_to_int16(v);
+        v -= int32_to_float(whole_v, 0);
+        v = sfpu_sinpi<APPROXIMATION_MODE>(v);
+
+        v_if (whole_v & 1) {
+            v = -v;
         }
         v_endif;
         dst_reg[0] = v;
@@ -184,24 +131,19 @@ inline void calculate_cosine()
     // SFPU microcode
     for (int d = 0; d < ITERATIONS; d++)
     {
-        vFloat v = dst_reg[0];
-        v = 0.318309886183791f*v; // *1/pi to get number of pi rads.
-        vInt whole_v = float_to_int16(v, 0);
-        vFloat whole_v_float = int32_to_float(whole_v, 0);
-        v = v - whole_v_float;
-        v *= 3.141592653589793f; // fractional * pi to get it in [-pi:pi]
-        v = sfpu_cosine_maclaurin_series<APPROXIMATION_MODE>(v);
-        whole_v = whole_v & 0x1;
-        v_if(whole_v != 0) {
-            // odd so flip the sign
-            v *= -1;
+        vFloat v = dst_reg[0] * FRAC_1_PI + 0.5f;
+        vInt whole_v = float_to_int16(v);
+        v -= int32_to_float(whole_v, 0);
+        v = sfpu_sinpi<APPROXIMATION_MODE>(v);
+
+        v_if (whole_v & 1) {
+            v = -v;
         }
         v_endif;
         dst_reg[0] = v;
         dst_reg++;
     }
 }
-
 
 template <SfpuType operation, bool APPROXIMATION_MODE, int ITERATIONS=8>
 inline void calculate_sfpu_trig() {
