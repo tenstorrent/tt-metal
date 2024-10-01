b120ac85f0ce8c57a56772f6bcaeaa1a10e93b4f
keerthana-r-mcw <keerthana.rajendran@multicorewareinc.com>
2024-10-01 16:34:01 +0000

    #13050: Efficientnet_b0 Bringup ttnn

--- models/experimental/functional_efficientnetb0/reference/efficientnetb0.py
+++ models/experimental/functional_efficientnetb0/reference/efficientnetb0.py
@@ -10,10 +10,10 @@ import torch.nn.functional as F
 from torchview import draw_graph
 
 
-class efficientnetb0(nn.Module):
+class Efficientnetb0(nn.Module):
     def __init__(self):
         super().__init__()
-        self._conv_stem = Conv2dDynamicSamePadding(3, 32, kernel_size=3, stride=2, bias=False)
+        self._conv_stem = Conv2dDynamicSamePadding(3, 32, kernel_size=(3, 3), stride=(2, 2), bias=False)
         self._bn0 = nn.BatchNorm2d(32, eps=0.001, momentum=0.010000000000000009, affine=True, track_running_stats=True)
         self._blocks0 = MBConvBlock([32, 32, 8, 32], [32, 8, 32, 16], [3, 1], is_depthwise_first=True)
         self._blocks1 = MBConvBlock([16, 96, 96, 4, 96], [96, 96, 4, 96, 24], [3, 2])
@@ -21,43 +21,52 @@ class efficientnetb0(nn.Module):
         self._blocks3 = MBConvBlock([24, 144, 144, 6, 144], [144, 144, 6, 144, 40], [5, 2])
         self._blocks4 = MBConvBlock([40, 240, 240, 10, 240], [240, 240, 10, 240, 40], [5, 1])
         self._blocks5 = MBConvBlock([40, 240, 240, 10, 240], [240, 240, 10, 240, 80], [3, 2])
-        self._blocks6 = MBConvBlock([80, 480, 480, 20, 480], [480, 480, 20, 480, 80], [1, 1])
-        self._blocks7 = MBConvBlock([80, 480, 480, 20, 480], [480, 480, 20, 480, 80], [1, 1])
+        self._blocks6 = MBConvBlock([80, 480, 480, 20, 480], [480, 480, 20, 480, 80], [3, 1])
+        self._blocks7 = MBConvBlock([80, 480, 480, 20, 480], [480, 480, 20, 480, 80], [3, 1])
         self._blocks8 = MBConvBlock([80, 480, 480, 20, 480], [480, 480, 20, 480, 112], [5, 1])
         self._blocks9 = MBConvBlock([112, 672, 672, 28, 672], [672, 672, 28, 672, 112], [5, 1])
         self._blocks10 = MBConvBlock([112, 672, 672, 28, 672], [672, 672, 28, 672, 112], [5, 1])
-        self._blocks11 = MBConvBlock([112, 672, 672, 28, 672], [672, 672, 28, 672, 192], [1, 1])
-        self._blocks12 = MBConvBlock([192, 1152, 1152, 48, 1151], [1152, 1152, 48, 1151, 192], [1, 1])
-        self._blocks13 = MBConvBlock([192, 1152, 1152, 48, 1151], [1152, 1152, 48, 1151, 192], [1, 1])
-        self._blocks14 = MBConvBlock([192, 1152, 1152, 48, 1151], [1152, 1152, 48, 1151, 192], [1, 1])
-        self._blocks15 = MBConvBlock([192, 1152, 1152, 48, 1152], [1152, 1152, 48, 1152, 320], [1, 1])
+        self._blocks11 = MBConvBlock([112, 672, 672, 28, 672], [672, 672, 28, 672, 192], [5, 2])
+        self._blocks12 = MBConvBlock([192, 1152, 1152, 48, 1152], [1152, 1152, 48, 1152, 192], [5, 1])
+        self._blocks13 = MBConvBlock([192, 1152, 1152, 48, 1152], [1152, 1152, 48, 1152, 192], [5, 1])
+        self._blocks14 = MBConvBlock([192, 1152, 1152, 48, 1152], [1152, 1152, 48, 1152, 192], [5, 1])
+        self._blocks15 = MBConvBlock([192, 1152, 1152, 48, 1152], [1152, 1152, 48, 1152, 320], [3, 1])
 
         self._conv_head = Conv2dDynamicSamePadding(320, 1280, kernel_size=(1, 1), stride=(1, 1), bias=False)
         self._avg_pooling = nn.AdaptiveAvgPool2d(output_size=1)
-        self._fc = nn.Linear(in_features=1280, out_features=1000, bias=True)
         self._bn1 = nn.BatchNorm2d(
             1280, eps=0.001, momentum=0.010000000000000009, affine=True, track_running_stats=True
         )
+        self._fc = nn.Linear(in_features=1280, out_features=1000, bias=True)
 
     def forward(self, x):
         x = self._conv_stem(x)
         x = self._bn0(x)
         x = x * torch.sigmoid(x)
         x = self._blocks0(x)
-        x = self._blocks1(x)
-        x = self._blocks2(x)
-        x = self._blocks3(x)
-        x = self._blocks4(x)
-        x = self._blocks5(x)
-        x = self._blocks6(x)
-        x = self._blocks7(x)
-        x = self._blocks8(x)
-        x = self._blocks9(x)
-        x = self._blocks10(x)
-        x = self._blocks11(x)
-        x = self._blocks12(x)
-        x = self._blocks13(x)
+        x_1 = self._blocks1(x)
+        x = self._blocks2(x_1)
+        x = x + x_1
+        x_3 = self._blocks3(x)
+        x = self._blocks4(x_3)
+        x = x + x_3
+        x_5 = self._blocks5(x)
+        x = self._blocks6(x_5)
+        x_7_in = x + x_5
+        x = self._blocks7(x_7_in)
+        x = x_7_in + x
+        x_8 = self._blocks8(x)
+        x = self._blocks9(x_8)
+        x_10_in = x + x_8
+        x = self._blocks10(x_10_in)
+        x = x + x_10_in
+        x_11 = self._blocks11(x)
+        x = self._blocks12(x_11)
+        x_13_in = x + x_11
+        x = self._blocks13(x_13_in)
+        x_14_in = x + x_13_in
         x = self._blocks14(x)
+        x = x_14_in + x
         x = self._blocks15(x)
         x = self._conv_head(x)
         x = self._bn1(x)
@@ -74,22 +83,30 @@ class Conv2dDynamicSamePadding(nn.Conv2d):
     The padding is operated in forward function by calculating dynamically.
     """
 
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, dilation=1, groups=1, bias=True):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=(1, 1), dilation=(1, 1), groups=1, bias=True):
         super().__init__(in_channels, out_channels, kernel_size, stride, 0, dilation, groups, bias)
-        self.conv = nn.Conv2d(
-            in_channels, out_channels, kernel_size, stride=stride, dilation=dilation, groups=groups, bias=bias
-        )
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.dilation = dilation
+        self.groups = groups
+        # self.conv = nn.Conv2d(
+        #     in_channels, out_channels, kernel_size, stride=stride, dilation=dilation, groups=groups, bias=bias
+        # )
 
     def forward(self, x):
         ih, iw = x.size()[-2:]
-        kh, kw = self.conv.kernel_size
-        sh, sw = self.conv.stride
+        kh, kw = self.kernel_size
+        sh, sw = self.stride
         oh, ow = math.ceil(ih / sh), math.ceil(iw / sw)  # change the output size according to stride ! ! !
-        pad_h = max((oh - 1) * self.conv.stride[0] + (kh - 1) * self.conv.dilation[0] + 1 - ih, 0)
-        pad_w = max((ow - 1) * self.conv.stride[1] + (kw - 1) * self.conv.dilation[1] + 1 - iw, 0)
+        pad_h = max((oh - 1) * self.stride[0] + (kh - 1) * self.dilation[0] + 1 - ih, 0)
+        pad_w = max((ow - 1) * self.stride[1] + (kw - 1) * self.dilation[1] + 1 - iw, 0)
         if pad_h > 0 or pad_w > 0:
             x = F.pad(x, [pad_w // 2, pad_w - pad_w // 2, pad_h // 2, pad_h - pad_h // 2])
-        return self.conv(x)
+
+        return F.conv2d(x, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
 
 
 class MBConvBlock(nn.Module):
@@ -150,10 +167,10 @@ class MBConvBlock(nn.Module):
             x = self._expand_conv(x)
             x = self._bn0(x)
             x = x * torch.sigmoid(x)
-        else:
-            x = x
+        print(x.shape)
         x = self._depthwise_conv(x)
         x = self._bn1(x)
+        torch.save(x, "torch_out.pt")
         x = x * torch.sigmoid(x)
         mul1 = x
         x = self._avg_pooling(x)
@@ -166,22 +183,3 @@ class MBConvBlock(nn.Module):
         x = self._bn2(x)
 
         return x
-
-
-dummy_input = torch.randn(1, 3, 224, 224)
-model = efficientnetb0()
-
-model.eval()
-out = model(dummy_input)
-
-
-model_graph = draw_graph(
-    model,
-    input_size=(1, 3, 224, 224),
-    dtypes=[torch.float32],
-    expand_nested=True,
-    graph_name="efficientnet_debug1",
-    depth=100,
-    directory=".",
-)
-model_graph.visual_graph.render(format="pdf")
