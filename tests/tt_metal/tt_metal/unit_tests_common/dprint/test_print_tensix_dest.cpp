1f18cf48297ab26b0a851a722be7d11aa44fb374
Milos Acimovic <macimovic@tenstorrent.com>
2024-10-02 12:51:31 +0000

    #13052: Disabling grayskull and blackhole print dest register unit tests

--- tests/tt_metal/tt_metal/unit_tests_common/dprint/test_print_tensix_dest.cpp
+++ tests/tt_metal/tt_metal/unit_tests_common/dprint/test_print_tensix_dest.cpp
@@ -233,6 +233,11 @@ TEST_F(DPrintFixture, TestDestPrintFloat16b) {
         .writer_kernel = "tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/dram/direct_writer_unary.cpp",
         .compute_kernel = "tests/tt_metal/tt_metal/test_kernels/compute/eltwise_copy_print_dest.cpp"};
 
+    if (this->arch_ == ARCH::BLACKHOLE) {
+        // Issue #12234: [Blackhole] Fix dest print for dest remap
+        GTEST_SKIP();
+    }
+
     // Run the test on the device
     this->RunTestOnDevice(
         [&](DPrintFixture* fixture, Device* device) { reader_datacopy_writer(fixture, device, test_config); },
@@ -248,6 +253,15 @@ TEST_F(DPrintFixture, TestDestPrintFloat32) {
         .reader_kernel = "tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/dram/direct_reader_unary.cpp",
         .writer_kernel = "tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/dram/direct_writer_unary.cpp",
         .compute_kernel = "tests/tt_metal/tt_metal/test_kernels/compute/eltwise_copy_print_dest.cpp"};
+
+    if (this->arch_ == ARCH::GRAYSKULL) {
+        GTEST_SKIP() << "Float32 dest is not supported on grayskull.";
+    }
+
+    if (this->arch_ == ARCH::BLACKHOLE) {
+        // Issue #12234: [Blackhole] Fix dest print for dest remap
+        GTEST_SKIP();
+    }
 
     // Run the test on the device
     this->RunTestOnDevice(
