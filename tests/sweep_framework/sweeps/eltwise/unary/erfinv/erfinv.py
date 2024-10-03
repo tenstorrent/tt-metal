3d8b162edc931984ffdf9f2501406fa0683486be
“Nenad <npetrovic@tenstorrent.com>
2024-10-03 11:15:39 +0000

    #11512: Fix suites

--- tests/sweep_framework/sweeps/eltwise/unary/erfinv/erfinv.py
+++ tests/sweep_framework/sweeps/eltwise/unary/erfinv/erfinv.py
@@ -24,7 +24,7 @@ random.seed(0)
 # Each suite has a key name (in this case "suite_1") which will associate the test vectors to this specific suite of inputs.
 # Developers can create their own generator functions and pass them to the parameters as inputs.
 parameters = {
-    "nightly": {
+    "xfail": {
         "input_shape": gen_shapes([1, 1, 32, 32], [6, 12, 256, 256], [1, 1, 32, 32], 16)
         + gen_shapes([1, 32, 32], [12, 256, 256], [1, 32, 32], 16)
         + gen_shapes([32, 32], [256, 256], [32, 32], 32),
