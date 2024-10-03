d75c5b2d2ad7438a4bda442995a3341f59db473d
Jay Kruer <jkruer@tenstorrent.com>
2024-10-03 19:14:17 +0000

    #11168: Fix ttnn.repeat for padded tiled tensors.

--- tests/ttnn/unit_tests/operations/test_repeat.py
+++ tests/ttnn/unit_tests/operations/test_repeat.py
@@ -23,3 +23,21 @@ def test_repeat(device):
     output = ttnn.to_torch(output)
 
     assert_with_pcc(torch_result, output, 0.9999)
+
+
+def test_repeat_with_tile_padding(device):
+    torch_input_tensor = torch.randn((1, 1, 1, 1), dtype=torch.bfloat16)
+    repeat_shape = (2, 2, 2, 2)
+
+    torch_result = torch_input_tensor.repeat(repeat_shape)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+
+    output = ttnn.repeat(input_tensor, ttnn.Shape(repeat_shape))
+    output = ttnn.to_torch(output)
+
+    assert (
+        output.shape == torch_result.shape
+    ), f"Output shape {output.shape} does not match torch shape {torch_result.shape}"
+
+    assert_with_pcc(torch_result, output, 0.9999), f"Output {output} does not match torch output {torch_result}"
--- ttnn/cpp/ttnn/operations/data_movement/repeat/repeat.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/repeat/repeat.cpp
@@ -8,6 +8,9 @@
 #include "ttnn/decorators.hpp"
 #include "ttnn/operations/data_movement/repeat/repeat.hpp"
 #include "device/repeat_op.hpp"
+#include "ttnn/operations/data_movement/untilize/untilize.hpp"
+#include "ttnn/operations/data_movement/tilize/tilize.hpp"
+#include "ttnn/operations/data_movement/slice/slice.hpp"
 
 namespace ttnn::operations::data_movement {
 
@@ -18,10 +21,18 @@ ttnn::Tensor RepeatOperation::invoke(
     const Shape & repeat_dims,
     const std::optional<MemoryConfig>& memory_config_arg) {
 
-    std::vector<Tensor> output_tensors = {Tensor(operation::get_workers_for_op_output({input_tensor}))};
+    // special path for handling padded tiled tensors that are naively repeated
+    // *with padding* in the current implementation
+    std::optional<Tensor> formatted_input_tensor = std::nullopt;
+    if (input_tensor.get_layout() != Layout::ROW_MAJOR
+        && input_tensor.get_legacy_shape().without_padding() != input_tensor.get_legacy_shape()) {
+        formatted_input_tensor = ttnn::untilize(input_tensor);
+    }
+
+    std::vector<Tensor> output_tensors = {Tensor(operation::get_workers_for_op_output({formatted_input_tensor.has_value() ? *formatted_input_tensor : input_tensor}))};
     operation::launch_op(
         [repeat_dims, memory_config_arg] (const std::vector<Tensor>& input_tensors, const std::vector<std::optional<const Tensor>>& optional_input_tensors, const std::vector<std::optional<Tensor>>& optional_output_tensors) -> std::vector<Tensor> {
-            auto& input_tensor = input_tensors.at(0);
+            auto& input_tensor = input_tensors[0];
             auto memory_config = memory_config_arg.value_or(input_tensor.memory_config());
             uint32_t input_rank = input_tensor.get_legacy_shape().rank();
             TT_FATAL(repeat_dims.rank() == input_rank, "Number of repeat dims must be equal to number of tensor dims");
@@ -36,11 +47,26 @@ ttnn::Tensor RepeatOperation::invoke(
                         (input_tensor.get_legacy_shape()[dim] * input_tensor.element_size()) % input_tensor.buffer()->alignment() == 0,
                         "Current repeat implementation requires aligned last dim when repeating on last dim");
                 }
-                output = operation::run_without_autoformat(RepeatDeviceOperation{dim, repeat_dims[dim], memory_config}, {output}).at(0);
+                auto outputs = operation::run_without_autoformat(RepeatDeviceOperation{dim, repeat_dims[dim], memory_config}, {output});
+                TT_FATAL(outputs.size() == 1, "ttnn.repeat: expected 1 output tensor from run_without_autoformat, but got {}", outputs.size());
+                output = outputs[0];
             }
             return {output};
-        }, {input_tensor}, output_tensors);
-    return output_tensors.at(0);
+        }, {formatted_input_tensor.has_value() ? *formatted_input_tensor : input_tensor}, output_tensors);
+    TT_FATAL(output_tensors.size() == 1, "ttnn.repeat: expected 1 output tensor, but got {}", output_tensors.size());
+    if (formatted_input_tensor.has_value()) {
+        auto zero_indices = std::vector<uint32_t>(input_tensor.get_legacy_shape().rank(), 0);
+        std::vector<uint32_t> end_indices(repeat_dims.size());
+        for (uint32_t i = 0; i < repeat_dims.size(); ++i) {
+            end_indices[i] = repeat_dims[i] * input_tensor.get_legacy_shape().without_padding()[i];
+        }
+        auto step = std::vector<uint32_t>(input_tensor.get_legacy_shape().rank(), 1);
+        auto sliced_output = ttnn::slice(output_tensors[0], zero_indices, end_indices, step, input_tensor.memory_config(), std::nullopt);
+        auto padded_to_tile = sliced_output.cpu().pad_to_tile(0.0).to(input_tensor.device(), input_tensor.memory_config());
+        auto tiled_output = ttnn::tilize(padded_to_tile);
+        return tiled_output;
+    }
+    return output_tensors[0];
 
 }
 
