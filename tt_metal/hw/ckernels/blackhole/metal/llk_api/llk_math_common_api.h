4dcffa98451973079eb12638c5939b0a0d532039
Milos Trajkovic <mtrajkovic@tenstorrent.com>
2024-10-04 17:24:06 -0400

    #11239: Add APIs to read and clear FPU/SFPU special number registers

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_math_common_api.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_math_common_api.h
@@ -112,3 +112,28 @@ inline void llk_math_reconfig_data_format_srcb(
         llk_math_reconfig_data_format_srcb(srcb_new_operand);
     }
 }
+
+inline std::uint32_t llk_math_get_compute_special_value_flags() {
+    return _llk_math_get_compute_special_value_flags_();
+}
+
+inline std::uint32_t llk_math_get_compute_special_value_flags_fpu(std::uint32_t special_value_flags_reg) {
+    constexpr std::uint32_t special_value_flags_fpu_mask = 0xf;
+    constexpr std::uint32_t special_value_flags_fpu_shift = 4;
+    return (special_value_flags_reg & special_value_flags_fpu_mask) >> special_value_flags_fpu_shift;
+}
+
+inline std::uint32_t llk_math_get_compute_special_value_flags_sfpu(std::uint32_t special_value_flags_reg) {
+    constexpr std::uint32_t special_value_flags_sfpu_mask = 0xf;
+    constexpr std::uint32_t special_value_flags_sfpu_shift = 0;
+    return (special_value_flags_reg & special_value_flags_sfpu_mask) >> special_value_flags_sfpu_shift;
+}
+
+inline void llk_math_clear_compute_special_value_flags() {
+    _llk_math_clear_compute_special_value_flags_();
+}
+
+inline void llk_math_store_compute_special_value_flags_to_l1(std::uint32_t l1_addr) {
+    volatile tt_l1_ptr std::uint32_t* l1_addr_ptr = reinterpret_cast<volatile tt_l1_ptr std::uint32_t*>(l1_addr);
+    l1_addr_ptr[0] = _llk_math_get_compute_special_value_flags_();
+}
