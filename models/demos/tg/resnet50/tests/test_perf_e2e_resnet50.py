ef9f50876dfaa65958a8382dbd06c007b8388435
asaigal <asaigal@tenstorrent.com>
2024-10-04 18:19:12 +0000

    #0: Add TG ResNet-50 Demos with Traced and Eager Execution on 2 CQs
      - Allow global dispatch_constants object to be toggled based on the
        number of CQs during device init -> this allows toggling CQs in a
        workload on TG
      - Map prefetch_q_entries_ to system type: This needs to be a function
        of num_hw_cqs for TG, since issue queue size is limited (4 chips per
        host memory channel). Can be hardcoded to 1534 entries for all other
        systems, since each device has full access to dedicated 1G host
        memory channel

--- models/demos/tg/resnet50/tests/test_perf_e2e_resnet50.py
+++ models/demos/tg/resnet50/tests/test_perf_e2e_resnet50.py
@@ -76,3 +76,65 @@ def test_perf_trace(
         f"resnet50_trace_{mode}",
         model_location_generator,
     )
+
+
+@run_for_wormhole_b0()
+@pytest.mark.model_perf_tg
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 32768, "num_command_queues": 2}], indirect=True)
+@pytest.mark.parametrize(
+    "device_batch_size, enable_async_mode, expected_inference_time, expected_compile_time",
+    ((16, True, 0.0391, 60),),
+    indirect=["enable_async_mode"],
+)
+def test_perf_2cqs(
+    mesh_device,
+    use_program_cache,
+    device_batch_size,
+    expected_inference_time,
+    expected_compile_time,
+    hf_cat_image_sample_input,
+    enable_async_mode,
+    model_location_generator,
+):
+    mode = "async" if enable_async_mode else "sync"
+    run_perf_resnet(
+        device_batch_size,
+        expected_inference_time,
+        expected_compile_time,
+        hf_cat_image_sample_input,
+        mesh_device,
+        f"resnet50_2cqs_{mode}",
+        model_location_generator,
+    )
+
+
+@run_for_wormhole_b0()
+@pytest.mark.model_perf_tg
+@pytest.mark.parametrize(
+    "device_params", [{"l1_small_size": 32768, "num_command_queues": 2, "trace_region_size": 1332224}], indirect=True
+)
+@pytest.mark.parametrize(
+    "device_batch_size, enable_async_mode, expected_inference_time, expected_compile_time",
+    ((16, True, 0.0069, 60),),
+    indirect=["enable_async_mode"],
+)
+def test_perf_trace_2cqs(
+    mesh_device,
+    use_program_cache,
+    device_batch_size,
+    expected_inference_time,
+    expected_compile_time,
+    hf_cat_image_sample_input,
+    enable_async_mode,
+    model_location_generator,
+):
+    mode = "async" if enable_async_mode else "sync"
+    run_perf_resnet(
+        device_batch_size,
+        expected_inference_time,
+        expected_compile_time,
+        hf_cat_image_sample_input,
+        mesh_device,
+        f"resnet50_trace_2cqs_{mode}",
+        model_location_generator,
+    )
