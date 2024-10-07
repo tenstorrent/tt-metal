2ca247f806408b84443c8bc765390f5a918e837c
amalbasaTT <amalbasa@tenstorrent.com>
2024-10-07 11:49:52 +0000

    #11512: Add sweeps for backward_ops: clamp_bw, hardtanh_bw, mul_bw, softplus_bw, threshold_bw

--- tests/sweep_framework/sweeps/eltwise/unary_backward/clamp_bw/clamp_bw.py
+++ tests/sweep_framework/sweeps/eltwise/unary_backward/clamp_bw/clamp_bw.py
@@ -26,10 +26,15 @@ random.seed(0)
 # Developers can create their own generator functions and pass them to the parameters as inputs.
 parameters = {
     "nightly": {
-        "input_shape": gen_shapes([1, 1, 32, 32], [6, 12, 256, 256], [1, 1, 32, 32], 32),
+        "input_shape": gen_shapes([1, 1, 32, 32], [6, 12, 256, 256], [1, 1, 32, 32], 8)
+        + gen_shapes([1, 32, 32], [12, 256, 256], [1, 32, 32], 8)
+        + gen_shapes([32, 32], [256, 256], [32, 32], 8),
         "mode": ["both", "min", "max"],
+        "grad_dtype": [ttnn.bfloat16],
         "input_a_dtype": [ttnn.bfloat16],
+        "grad_layout": [ttnn.TILE_LAYOUT],
         "input_a_layout": [ttnn.TILE_LAYOUT],
+        "grad_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
         "input_a_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
         "output_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
     },
@@ -43,8 +48,11 @@ parameters = {
 def run(
     input_shape,
     mode,
+    grad_dtype,
     input_a_dtype,
+    grad_layout,
     input_a_layout,
+    grad_memory_config,
     input_a_memory_config,
     output_memory_config,
     *,
@@ -53,9 +61,14 @@ def run(
     data_seed = random.randint(0, 20000000)
     torch.manual_seed(data_seed)
 
+    torch_grad_tensor = gen_func_with_cast_tt(
+        partial(torch_random, low=-100, high=100, dtype=torch.float32), grad_dtype
+    )(input_shape)
     torch_input_tensor_a = gen_func_with_cast_tt(
         partial(torch_random, low=-100, high=100, dtype=torch.float32), input_a_dtype
     )(input_shape)
+    torch_input_tensor_a.requires_grad = True
+    torch_input_tensor_a.retain_grad()
 
     low, high = gen_low_high_scalars()
 
@@ -64,10 +77,20 @@ def run(
     elif mode == "max":
         low = None
 
-    torch_output_tensor = torch.clamp(torch_input_tensor_a, low, high)
+    intermediate_result = torch.clamp(torch_input_tensor_a, low, high)
+    intermediate_result.backward(gradient=torch_grad_tensor)
+    torch_output_tensor = torch_input_tensor_a.grad
+
+    grad_tensor = ttnn.from_torch(
+        torch_grad_tensor,
+        dtype=grad_dtype,
+        layout=grad_layout,
+        device=device,
+        memory_config=grad_memory_config,
+    )
 
     input_tensor_a = ttnn.from_torch(
-        torch_input_tensor_a,
+        torch_input_tensor_a.detach().clone(),
         dtype=input_a_dtype,
         layout=input_a_layout,
         device=device,
@@ -75,7 +98,7 @@ def run(
     )
 
     start_time = start_measuring_time()
-    result = ttnn.clamp(input_tensor_a, low, high, memory_config=output_memory_config)
+    result = ttnn.clamp_bw(grad_tensor, input_tensor_a, min=low, max=high, memory_config=output_memory_config)[0]
     output_tensor = ttnn.to_torch(result)
     e2e_perf = stop_measuring_time(start_time)
 
