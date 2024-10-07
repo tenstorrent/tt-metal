6a4696b3a9f212e0dc9273986eb50b5a16e7148e
Bill Teng <135061747+TT-billteng@users.noreply.github.com>
2024-10-07 11:39:39 -0700

    Remove un-used device code (#13492)
    * #13553: remove unused file
    
    * #13553: remove unused structs
    
    * #13553: remove unused constants

--- tt_metal/hostdevcommon/common_values.hpp
+++ tt_metal/hostdevcommon/common_values.hpp
@@ -11,12 +11,5 @@
 
 constexpr static std::uint32_t INVALID = 0;
 constexpr static std::uint32_t VALID = 1;
-constexpr static std::uint32_t NOTIFY_HOST_KERNEL_COMPLETE_VALUE = 512;
 constexpr static std::size_t DEFAULT_L1_SMALL_SIZE = 0;  //(1 << 15);  // 32KB
 constexpr static std::size_t DEFAULT_TRACE_REGION_SIZE = 0;
-
-// Number of entries for each core/shard in dispatch command
-constexpr static std::uint32_t NUM_ENTRIES_PER_SHARD = 3;    //per shard/core 3 entries (uint32_t each)
-                                                             //first number of pages in core/shard
-                                                             //second x coordinate
-                                                             //third y coordinate
