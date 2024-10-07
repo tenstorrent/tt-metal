786181c6d9c694b9c29c1acae63d52572db31093
Shaw Nguyen <nguyenvanthanhtung2001@gmail.com>
2024-10-07 09:15:30 +0000

    #13463: Revise Reviews 1

--- ttnn/cpp/ttnn/operations/data_movement/clone/device/kernels/compute_kernel.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/clone/device/kernels/compute_kernel.cpp
@@ -13,19 +13,16 @@ void MAIN {
     uint32_t per_core_tile_count = get_compile_time_arg_val(0);
     unary_op_init_common(tt::CB::c_in0);
     for (uint32_t tile_index = 0; tile_index < per_core_tile_count; ++tile_index) {
-        acquire_dst(tt::DstMode::Half);
-
         cb_wait_front(tt::CB::c_in0, 1);
         cb_reserve_back(tt::CB::c_out0, 1);
-
+        tile_regs_acquire();
         copy_tile(tt::CB::c_in0, 0, 0);
-
+        tile_regs_commit();
+        tile_regs_wait();
         pack_tile(0, tt::CB::c_out0);
-
+        tile_regs_release();
         cb_pop_front(tt::CB::c_in0, 1);
         cb_push_back(tt::CB::c_out0, 1);
-
-        release_dst(tt::DstMode::Half);
     }
 }
 }  // namespace NAMESPACE
