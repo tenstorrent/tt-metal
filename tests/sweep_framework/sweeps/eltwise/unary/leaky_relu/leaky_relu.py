325c04b9a75829ff927e80a5c144bb3189ad23b5
mcw-anasuya <mcw-anair@ext.tenstorrent.com>
2024-10-08 12:05:09 +0000

    #8157: Add golden function

--- tests/sweep_framework/sweeps/eltwise/unary/leaky_relu/leaky_relu.py
+++ tests/sweep_framework/sweeps/eltwise/unary/leaky_relu/leaky_relu.py
@@ -3,17 +3,19 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from typing import Optional, Tuple
+from functools import partial
 
 import torch
-import random
 import ttnn
 
 from tests.ttnn.utils_for_testing import check_with_pcc, start_measuring_time, stop_measuring_time
+from tests.sweep_framework.utils import gen_shapes
+from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_func_with_cast_tt
+from models.utility_functions import torch_random
+
 
 # Override the default timeout in seconds for hang detection.
 TIMEOUT = 30
-
-random.seed(0)
 
 
 # Parameters provided to the test vector generator are defined here.
@@ -22,13 +24,11 @@ random.seed(0)
 # Developers can create their own generator functions and pass them to the parameters as inputs.
 parameters = {
     "nightly": {
-        "batch_sizes": [(1,)],
-        "height": [384, 1024],
-        "width": [1024, 4096],
-        "dtype": [ttnn.bfloat16, ttnn.bfloat8_b],
-        "input_memory_config": [ttnn.DRAM_MEMORY_CONFIG],
+        "input_shape": gen_shapes([1, 1, 32, 32], [6, 12, 256, 256], [1, 1, 32, 32], 16),
+        "input_a_dtype": [ttnn.bfloat16, ttnn.bfloat8_b],
+        "input_a_layout": [ttnn.TILE_LAYOUT, ttnn.ROW_MAJOR_LAYOUT],
+        "input_a_memory_config": [ttnn.DRAM_MEMORY_CONFIG],
         "output_memory_config": [ttnn.DRAM_MEMORY_CONFIG],
-        "layout": [ttnn.TILE_LAYOUT, ttnn.ROW_MAJOR_LAYOUT],
         "negative_slope": [-0.5, 0, 0.01, 0.5],
     },
 }
@@ -38,7 +38,7 @@ parameters = {
 # If invalidated, the vector will still be stored but will be skipped.
 # Returns False, None if the vector is valid, and True, str with a reason for invalidation if it is invalid.
 def invalidate_vector(test_vector) -> Tuple[bool, Optional[str]]:
-    if test_vector["layout"] == ttnn.ROW_MAJOR_LAYOUT:
+    if test_vector["input_a_layout"] == ttnn.ROW_MAJOR_LAYOUT:
         return True, "Row Major layout is not supported"
     return False, None
 
@@ -48,28 +48,34 @@ def invalidate_vector(test_vector) -> Tuple[bool, Optional[str]]:
 # The runner will call this run function with each test vector, and the returned results from this function will be stored.
 # If you defined a device_mesh_fixture above, the object you yielded will be passed into this function as 'device'. Otherwise, it will be the default ttnn device opened by the infra.
 def run(
-    batch_sizes,
-    height,
-    width,
-    dtype,
-    input_memory_config,
+    input_shape,
+    input_a_dtype,
+    input_a_layout,
+    input_a_memory_config,
     output_memory_config,
-    layout,
     negative_slope,
     *,
     device,
 ) -> list:
-    input_shape = (*batch_sizes, height, width)
+    torch.manual_seed(0)
 
-    torch_input_tensor = torch.randn(input_shape, dtype=torch.float32)
-    torch_output_tensor = torch.nn.functional.leaky_relu(torch_input_tensor, negative_slope)
+    torch_input_tensor_a = gen_func_with_cast_tt(
+        partial(torch_random, low=-100, high=100, dtype=torch.float32), input_a_dtype
+    )(input_shape)
 
-    input_tensor = ttnn.from_torch(
-        torch_input_tensor, dtype=dtype, device=device, memory_config=input_memory_config, layout=layout
+    golden_function = ttnn.get_golden_function(ttnn.leaky_relu)
+    torch_output_tensor = golden_function(torch_input_tensor_a, negative_slope=negative_slope)
+
+    input_tensor_a = ttnn.from_torch(
+        torch_input_tensor_a,
+        dtype=input_a_dtype,
+        layout=input_a_layout,
+        device=device,
+        memory_config=input_a_memory_config,
     )
 
     start_time = start_measuring_time()
-    result = ttnn.leaky_relu(input_tensor, negative_slope, memory_config=output_memory_config)
+    result = ttnn.leaky_relu(input_tensor_a, negative_slope, memory_config=output_memory_config)
     output_tensor = ttnn.to_torch(result)
     e2e_perf = stop_measuring_time(start_time)
 
