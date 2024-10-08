b6ff73c8d84e03562b402437057728806dbe1ddf
Jay Kruer <jkruer@tenstorrent.com>
2024-10-08 03:06:05 +0000

    #0: Add more tests for ttnn.repeat

--- tests/ttnn/unit_tests/operations/test_repeat.py
+++ tests/ttnn/unit_tests/operations/test_repeat.py
@@ -8,26 +8,24 @@ import torch
 
 import ttnn
 
+from models.utility_functions import comp_pcc
 from tests.ttnn.utils_for_testing import assert_with_pcc
+from functools import reduce
+
+dtypes = [torch.bfloat16]
+shapes = [(1, 2, 4, 4), (1, 1, 1, 1)] + [(1, 2, 2 * j, 4 * j) for j in range(2, 12)]
+repeat_specs = [(2, 2, 2, 2), (1, 2, 1, 1)]
 
 
-def test_repeat(device):
-    torch_input_tensor = torch.randn((1, 2, 4, 4), dtype=torch.bfloat16)
-    repeat_shape = (1, 2, 1, 1)
+@pytest.mark.parametrize("dtype", dtypes)
+@pytest.mark.parametrize("shape", shapes)
+@pytest.mark.parametrize("repeat_shape", repeat_specs)
+def test_repeat(device, dtype, shape, repeat_shape):
+    if dtype == torch.bfloat16 and shape[-1] < 2 and repeat_shape[-1] < 2:
+        pytest.skip("bfloat16 needs 4 byte inner dim on the output.")
 
-    torch_result = torch_input_tensor.repeat(repeat_shape)
-
-    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
-
-    output = ttnn.repeat(input_tensor, ttnn.Shape(repeat_shape))
-    output = ttnn.to_torch(output)
-
-    assert_with_pcc(torch_result, output, 0.9999)
-
-
-def test_repeat_with_tile_padding(device):
-    torch_input_tensor = torch.randn((1, 1, 1, 1), dtype=torch.bfloat16)
-    repeat_shape = (2, 2, 2, 2)
+    mul = lambda x, y: x * y
+    torch_input_tensor = torch.arange(0, reduce(mul, shape, 1), dtype=dtype).reshape(shape)
 
     torch_result = torch_input_tensor.repeat(repeat_shape)
 
@@ -40,4 +38,4 @@ def test_repeat_with_tile_padding(device):
         output.shape == torch_result.shape
     ), f"Output shape {output.shape} does not match torch shape {torch_result.shape}"
 
-    assert_with_pcc(torch_result, output, 0.9999), f"Output {output} does not match torch output {torch_result}"
+    assert_with_pcc(torch_result, output, 0.9999)
