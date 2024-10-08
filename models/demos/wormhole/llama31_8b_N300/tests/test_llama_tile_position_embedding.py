65111187d9d845fdbf23ff5677eb82116aea4f5e
avoraTT <avora@tenstorrent.com>
2024-10-08 14:16:58 -0700

    #13668: Add TT module for Tile Position Embeddings. Note, load_hook and dynamic_resize are not implemented. Tests pass for llama23 shapes.

---
+++ models/demos/wormhole/llama31_8b_N300/tests/test_llama_tile_position_embedding.py
@@ -0,0 +1,203 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+##### Python imports #####
+import math
+import pytest
+from loguru import logger
+import os
+
+##### PyTorch imports #####
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+##### TTNN imports #####
+import ttnn
+from ttnn import experimental as ttl
+from ttnn import ConcatMeshToTensor, ReplicateTensorToMesh
+from models.utility_functions import skip_for_grayskull
+from models.utility_functions import (
+    comp_pcc,
+    comp_allclose,
+)
+from models.utility_functions import (
+    nearest_32,
+)
+from models.demos.wormhole.llama31_8b_N300.tt.llama_tile_position_embedding import (
+    TtLlamaTilePositionEmbedding,
+)
+
+
+##### Torch op #####
+class TilePositionEmbedding(nn.Module):
+    def __init__(
+        self,
+        num_tiles: int,
+        width: int,
+        gated: bool = False,
+    ):
+        super().__init__()
+        self.num_tiles = num_tiles
+        self.width = width
+        self.embedding = nn.Parameter(torch.randn(num_tiles, num_tiles, 1, width) / math.sqrt(width))
+        self.gated = gated
+        if gated:
+            self.gate = nn.Parameter(torch.randn(1))
+
+    """NOTE: The _dynamic_resize function is only ever used in the load hooks function call that loads the weights.
+    Currently, this function is NOT tested by the test suite
+    """
+
+    @staticmethod
+    def _dynamic_resize(embed: torch.Tensor, num_tiles: int):
+        nt_old, nt_old, _, w = embed.shape
+        embed = embed.permute(2, 3, 0, 1)
+
+        embed_new = F.interpolate(
+            embed,
+            size=(num_tiles, num_tiles),
+            mode="bilinear",
+            align_corners=True,
+        )
+        # reshape the weights to the correct shape
+        embed_new = embed_new.permute(2, 3, 0, 1)
+        return embed_new
+
+    def forward(self, x: torch.Tensor, ar: torch.Tensor, num_tiles: int = None):
+        embed = self.embedding
+        if num_tiles is None:
+            num_tiles = self.num_tiles
+        elif num_tiles > self.num_tiles:
+            embed = TilePositionEmbedding._dynamic_resize(self.embedding, num_tiles)
+        out_pos_embed = torch.zeros(x.shape[0], num_tiles, 1, self.width, device=x.device, dtype=x.dtype)
+        for idx, arx in enumerate(ar):
+            h, w = arx
+            out_pos_embed[idx, : w * h] = embed[:h, :w].reshape(w * h, 1, self.width)
+        if self.gated:
+            out_pos_embed = out_pos_embed * self.gate.tanh()
+
+        x = x + out_pos_embed
+        return x
+
+
+@skip_for_grayskull("Requires wormhole_b0 to run")
+@pytest.mark.parametrize(
+    "mesh_device",
+    [
+        {"N150": (1, 1), "N300": (1, 2), "T3K": (2, 4), "TG": (8, 4)}.get(
+            os.environ.get("FAKE_DEVICE"), len(ttnn.get_device_ids())
+        )
+    ],
+    indirect=True,
+)
+@pytest.mark.parametrize(
+    "input_shape, dim, gated, max_num_tiles",
+    [
+        ((1, 32, 4, 1032), 1280, True, 4),
+        ((1, 4, 4, 1032), 1280, True, 4),
+        ((1, 1, 4, 1032), 1280, True, 4),
+        ((1, 1, 4, 1032), 1280, False, 4),
+        ((1, 32, 4, 1032), 1280, False, 4),
+    ],
+)
+@pytest.mark.parametrize(
+    "layout",
+    [
+        ttnn.TILE_LAYOUT,
+    ],
+)
+@pytest.mark.parametrize(
+    "input_dtype",
+    [
+        ttnn.bfloat16,
+        # ttnn.bfloat8_b,
+    ],
+)
+def test_llama_conv2d_inference(
+    mesh_device,
+    use_program_cache,
+    reset_seeds,
+    # Input params
+    input_shape,
+    input_dtype,
+    layout,
+    # Tile Position Embedding params
+    dim,
+    gated,
+    max_num_tiles,
+):
+    pcc = 0.9999
+
+    devices = mesh_device.get_devices()
+    num_devices = len(devices)
+
+    bsz, num_concurrent_media, num_chunks, ntok = input_shape
+
+    ##### Check parms #####
+    assert num_chunks == max_num_tiles, "num_chunks must be the same value as max_num_tiles!"
+
+    ##### Prepare inputs #####
+    input_tensor = torch.randn(bsz * num_concurrent_media, num_chunks, ntok, dim)
+    logger.info(f"Input tensor shape: {input_tensor.shape}")
+
+    tt_input_tensor = ttnn.as_tensor(
+        input_tensor,
+        dtype=input_dtype,
+        layout=layout,
+        device=mesh_device,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        mesh_mapper=ReplicateTensorToMesh(mesh_device),
+    )
+    logger.info(f"TT Input tensor shape: {tt_input_tensor.shape}")
+
+    aspect_ratios = torch.ones(bsz * num_concurrent_media, 2, dtype=torch.int64)  # Collapse
+    logger.info(f"Aspects ratios shape: {aspect_ratios.shape}")
+
+    tt_aspect_ratios = aspect_ratios.tolist()
+
+    ##### Perform the torch ops #####
+    reference_model = TilePositionEmbedding(
+        num_tiles=max_num_tiles,
+        width=dim,
+        gated=gated,
+    )
+    reference_output = reference_model(input_tensor, aspect_ratios)
+
+    ##### Perform the TT ops #####
+    _embedding_config = {
+        "dtype": input_dtype,
+        "layout": layout,
+        "memory_config": ttnn.DRAM_MEMORY_CONFIG,
+        "mesh_mapper": ttnn.ReplicateTensorToMesh,
+    }
+
+    tt_model = TtLlamaTilePositionEmbedding(
+        mesh_device,
+        num_tiles=max_num_tiles,
+        width=dim,
+        gate=reference_model.gate if gated else None,
+        embedding=reference_model.embedding,
+        embedding_config=_embedding_config,
+    )
+    tt_output = tt_model(tt_input_tensor, tt_aspect_ratios)
+
+    ##### Check the outputs #####
+    print("Checking outputs")
+    out = ttnn.from_device(tt_output)
+    tt_output_torch = ttnn.to_torch(out, mesh_composer=ConcatMeshToTensor(mesh_device, dim=3))
+
+    # Only select output from one device
+    tt_output_torch = tt_output_torch[..., :dim]
+
+    passing, pcc_message = comp_pcc(reference_output, tt_output_torch, pcc)
+
+    logger.info(comp_allclose(reference_output, tt_output_torch))
+    logger.info(pcc_message)
+
+    if passing:
+        logger.info(f"Llama_TilePositionEmbedding Passed!")
+    else:
+        logger.warning(f"Llama_TilePositionEmbedding Failed!")
+        assert passing, f"PCC value is lower than {pcc} for some of the outputs. Check Warnings!"
---
+++ models/demos/wormhole/llama31_8b_N300/tt/llama_tile_position_embedding.py
@@ -0,0 +1,131 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import List, Optional
+import torch
+import torch.nn as nn
+import itertools
+
+import ttnn
+from models.utility_functions import (
+    nearest_32,
+)
+from models.common.lightweightmodule import LightweightModule
+
+from ttnn import ShardTensorToMesh, ConcatMeshToTensor, ReplicateTensorToMesh
+
+TILE_SIZE = 32
+
+
+class TtLlamaTilePositionEmbedding(LightweightModule):
+    """Tile Position Embedding layer.
+    Arguments:
+        num_tiles: Input channels.
+        width: Width/Dim for the model.
+        gate: Use gating mechanism.
+        embedding: Position embedding tensor.
+        embedding_config: TTNN configuration for embedding tensor.
+    Input: (bsz, num_chunks, ntok, dim)
+    Output: (bsz, num_chunks, ntok, dim)
+    """
+
+    def __init__(
+        self,
+        mesh_device,
+        num_tiles: int,
+        width: int,
+        gate,
+        embedding,  # [num_tiles, num_tiles, 1, width]
+        embedding_config,
+    ):
+        super().__init__()
+
+        self.mesh_device = mesh_device
+        self.num_devices = len(self.mesh_device.get_devices())
+
+        self.num_tiles = num_tiles
+        self.width = width
+        self.gated = gate is not None
+        self._embedding_config = embedding_config
+        self._gate_config = embedding_config
+
+        padded_embeddings, self.ar_mapping = self.generate_padded_embeddings(embedding, num_tiles, width)
+        self.padded_embeddings = ttnn.as_tensor(
+            padded_embeddings,
+            dtype=self._embedding_config["dtype"],
+            layout=self._embedding_config["layout"],
+            device=self.mesh_device,
+            memory_config=self._embedding_config["memory_config"],
+            mesh_mapper=self._embedding_config["mesh_mapper"](self.mesh_device),
+        )
+
+        if self.gated:
+            self.gate = ttnn.as_tensor(
+                gate,
+                dtype=self._gate_config["dtype"],
+                layout=self._gate_config["layout"],
+                device=self.mesh_device,
+                memory_config=self._gate_config["memory_config"],
+                mesh_mapper=self._gate_config["mesh_mapper"](self.mesh_device),
+            )
+
+    def generate_padded_embeddings(self, embedding: torch.Tensor, num_tiles, width):
+        embedding = torch.cat(
+            [
+                embedding,
+            ]
+            * TILE_SIZE,
+            dim=2,
+        )  # Pad to tile size
+
+        # Generate all possible aspect ratios (H * W must be less than or equal to num_tiles)
+        ratios = list(itertools.product(range(1, num_tiles + 1), repeat=2))
+        ratios = [x for x in ratios if x[0] * x[1] <= num_tiles]
+
+        # Used to select the correct embedding for a given aspect ratio
+        ar_mapping = {value: index for index, value in enumerate(ratios)}
+
+        padded_embeds = []
+        for h, w in ratios:
+            out_pos_embed = torch.zeros(
+                1, num_tiles, TILE_SIZE, width
+            )  # 0th dim holds all possible embeddings for different aspect ratios
+            out_pos_embed[0, : w * h] = embedding[:h, :w].reshape(1, w * h, TILE_SIZE, width)
+            padded_embeds.append(out_pos_embed)
+
+        return torch.cat(padded_embeds, dim=0), ar_mapping
+
+    def forward(self, x: ttnn.Tensor, ar: torch.Tensor, num_tiles: int = None):
+        if num_tiles is None:
+            num_tiles = self.num_tiles
+        elif num_tiles > self.num_tiles:
+            # TODO: Need to implement
+            assert False, "_dynamic_resize is currently not supported for TtLllamaTilePositionEmbedding"
+
+        # Get the correct embeddings for the given aspect ratios
+        out_pos_embed = []
+        for [h, w] in ar:
+            idx = self.ar_mapping[(h, w)]
+            out_pos_embed.append(
+                self.padded_embeddings[idx : idx + 1],  # Select the correct embedding
+            )
+        out_pos_embed = ttnn.concat(out_pos_embed, dim=0)  # Concat batch
+
+        # Apply gating mechanism
+        if self.gated:
+            out_pos_embed = out_pos_embed * ttnn.tanh(self.gate)
+
+        # Broadcast along ntok dimension
+        out_pos_embed = ttnn.concat(
+            [
+                out_pos_embed,
+            ]
+            * (nearest_32(x.shape[2]) // 32),
+            dim=2,
+        )
+
+        # Concatenate with input tensor
+        x = x + out_pos_embed[:, :, : x.shape[2], :]
+
+        return x
