2856e44e9ca4583e408e4d450ba539713dff04ce
Samarth Agarwal <sagarwal@tenstorrent.com>
2024-10-08 22:12:31 +0000

    #0: Initial changes

---
+++ tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/kernels/dispatcher_kernel_size_and_runtime.cpp
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "c_tensix_core.h"
+
+#if KERNEL_SIZE_BYTES > 16
+constexpr uint32_t empty_kernel_bytes = 16;
+uint8_t data1[KERNEL_SIZE_BYTES - empty_kernel_bytes] __attribute__ ((section ("l1_data_test_only"))) __attribute__((used));
+#endif
+
+void kernel_main() {
+    const uint64_t end_time = c_tensix_core::read_wall_clock() + KERNEL_RUNTIME_SECONDS;
+    while (c_tensix_core::read_wall_clock() < end_time);
+}
