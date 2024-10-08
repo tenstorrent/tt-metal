e797e42b52a1c1265e76ad344d413bba4ae0bb0e
Vuk Jovanovic <157483960+tt-vjovanovic@users.noreply.github.com>
2024-10-08 09:09:09 +0200

    Adding script that installs tt-debugger into current python environment (#13534)
    Adding simple script that installs tt-debugger into current python environment. At the moment it is heavy, until tt-debugger becomes public repository. Idea is to have script to enable people to test it with tt-metal, and later we will make it install faster from public repository releases instead of building it on install.

---
+++ scripts/install_debugger.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt install software-properties-common build-essential libyaml-cpp-dev libboost-all-dev libhwloc-dev libzmq3-dev libgtest-dev libgmock-dev xxd -y
+pip uninstall -y debuda
+pip install git+https://github.com/tenstorrent/tt-debuda.git
