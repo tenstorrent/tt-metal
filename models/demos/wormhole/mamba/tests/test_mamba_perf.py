e0d5b20b96cb11014a3dd08e8a5983da8cca89ed
Raymond Kim <109366641+tt-rkim@users.noreply.github.com>
2024-10-09 10:08:01 -0400

    #0: Increase mamba device perf threshold because looks like there was a slight performance bump [skip ci] (#13635)
    Force-merging to fix device perf single-card.

--- models/demos/wormhole/mamba/tests/test_mamba_perf.py
+++ models/demos/wormhole/mamba/tests/test_mamba_perf.py
@@ -143,7 +143,7 @@ def test_mamba_perf_e2e(
 @pytest.mark.models_device_performance_bare_metal
 @pytest.mark.parametrize(
     "batch, expected_layer_duration_ms",
-    ((32, 1.71),),
+    ((32, 1.689),),
 )
 def test_mamba_perf_device(batch, expected_layer_duration_ms):
     subdir = "ttnn_mamba"
