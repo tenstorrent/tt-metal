241a8903458a0d479b3255250766205fc3680cd4
Kalaivani Baskar <156762498+KalaivaniMCW@users.noreply.github.com>
2024-10-09 17:16:10 +0530

    #0: erisc sections

--- tt_metal/hw/firmware/src/erisc.cc
+++ tt_metal/hw/firmware/src/erisc.cc
@@ -37,7 +37,7 @@ uint32_t tt_l1_ptr *rta_l1_base __attribute__((used));
 uint32_t tt_l1_ptr *crta_l1_base __attribute__((used));
 uint32_t tt_l1_ptr *sem_l1_base[ProgrammableCoreType::COUNT] __attribute__((used));
 
-void __attribute__((section("erisc_l1_code.1"), noinline)) Application(void) {
+void __attribute__((noinline)) Application(void) {
     WAYPOINT("I");
     rtos_context_switch_ptr = (void (*)())RtosTable[0];
 
@@ -117,7 +117,7 @@ void __attribute__((section("erisc_l1_code.1"), noinline)) Application(void) {
 // The function relies on optimization to avoid unexpected register
 // usage.
 
-__attribute__((section("erisc_l1_code.0"), naked, optimize("Os")))
+__attribute__((section(".start"), naked, optimize("Os")))
 void ApplicationHandler(void) {
 
   { // ApplicationHander pseudo-scope
