f6dde33c32a5928965e0aec88a2f93b9be434bde
amalbasaTT <amalbasa@tenstorrent.com>
2024-10-09 13:06:38 +0000

    #11512: Add mesh_device_fixture to embedding_bw sweep

--- tests/sweep_framework/sweeps/eltwise/binary_backward/embedding_bw/embedding_bw.py
+++ tests/sweep_framework/sweeps/eltwise/binary_backward/embedding_bw/embedding_bw.py
@@ -4,7 +4,6 @@
 
 from typing import Optional, Tuple
 from functools import partial
-import os
 
 import torch
 import random
@@ -59,9 +58,8 @@ parameters = {
 
 def mesh_device_fixture():
     device = ttnn.open_device(device_id=0)
-    assert not ttnn.device.is_wormhole_b0(device), "This op is not available for Wormhole_B0"
-    device_name = os.environ.get("ARCH_NAME", os.environ.get("TT_ARCH_NAME", "default")).lower()
-    yield (device, device_name)
+    assert ttnn.device.is_wormhole_b0(device), "This op is only available for Wormhole_B0"
+    yield (device, "Wormhole_B0")
     ttnn.close_device(device)
     del device
 
