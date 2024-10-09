969171fb4545b3f30356b75d120a53957087b771
Nemanja Grujic <109360083+nemanjagrujic@users.noreply.github.com>
2024-10-09 09:38:45 +0200

    Ngrujic/sweep tests 1 (#13281)
    * #11512: Add sweep for ttnn.isfinite
    * #11512: Add sweep for ttnn.isinf, isnan, isposinf and isneginf
    * #11512: Add sweeps for logit, lgamma, mish and multigammaln
    * #11512: Add sweeps for lerp

--- tests/sweep_framework/sweeps/eltwise/unary/isinf.py
+++ tests/sweep_framework/sweeps/eltwise/unary/isinf.py
@@ -5,30 +5,30 @@
 from typing import Optional, Tuple
 from functools import partial
 
+import os
 import torch
 import random
 import ttnn
 from tests.sweep_framework.utils import gen_shapes
-from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_func_with_cast_tt
+from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_func_with_cast_tt, gen_rand_inf
 
 from tests.ttnn.utils_for_testing import check_with_pcc, start_measuring_time, stop_measuring_time
-from models.utility_functions import torch_random
+from models.utility_functions import torch_random, is_wormhole_b0
 
 # Override the default timeout in seconds for hang detection.
 TIMEOUT = 30
 
 random.seed(0)
 
+
 # Parameters provided to the test vector generator are defined here.
 # They are defined as dict-type suites that contain the arguments to the run function as keys, and lists of possible inputs as values.
-# Each suite has a key name (in this case "suite_1") which will associate the test vectors to this specific suite of inputs.
+# Each suite has a key name (in this case "suite_1" and "suite_2") which will associate the test vectors to this specific suite of inputs.
 # Developers can create their own generator functions and pass them to the parameters as inputs.
 parameters = {
     "nightly": {
-        "input_shape": gen_shapes([1, 1, 32, 32], [6, 12, 256, 256], [1, 1, 32, 32], 16)
-        + gen_shapes([1, 32, 32], [12, 256, 256], [1, 32, 32], 16)
-        + gen_shapes([32, 32], [256, 256], [32, 32], 32),
-        "input_a_dtype": [ttnn.bfloat16],
+        "input_shape": gen_shapes([1, 1, 1, 1], [6, 12, 256, 256], [1, 1, 1, 1], 64),
+        "input_a_dtype": [ttnn.bfloat16, ttnn.bfloat8_b],
         "input_a_layout": [ttnn.TILE_LAYOUT],
         "input_a_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
         "output_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
@@ -39,7 +39,7 @@ parameters = {
 # This is the run instructions for the test, defined by the developer.
 # The run function must take the above-defined parameters as inputs.
 # The runner will call this run function with each test vector, and the returned results from this function will be stored.
-# If you defined a device_mesh_fixture above, the object you yielded will be passed into this function as 'device'. Otherwise, it will be the default ttnn device opened by the infra.
+# If you defined a mesh_device_fixture above, the object you yielded will be passed into this function as 'device'. Otherwise, it will be the default ttnn device opened by the infra.
 def run(
     input_shape,
     input_a_dtype,
@@ -52,10 +52,8 @@ def run(
     data_seed = random.randint(0, 20000000)
     torch.manual_seed(data_seed)
 
-    torch_input_tensor_a = gen_func_with_cast_tt(
-        partial(torch_random, low=0.1, high=1000, dtype=torch.float32), input_a_dtype
-    )(input_shape)
-    torch_output_tensor = torch.lgamma(torch_input_tensor_a)
+    torch_input_tensor_a = gen_rand_inf(input_shape, low=-100, high=100)
+    torch_output_tensor = torch.isinf(torch_input_tensor_a)
 
     input_tensor_a = ttnn.from_torch(
         torch_input_tensor_a,
@@ -66,8 +64,10 @@ def run(
     )
 
     start_time = start_measuring_time()
-    result = ttnn.lgamma(input_tensor_a, memory_config=output_memory_config)
-    output_tensor = ttnn.to_torch(result)
+    output_tensor = ttnn.isinf(input_tensor_a, memory_config=output_memory_config)
+    output_tensor = ttnn.to_torch(output_tensor)
     e2e_perf = stop_measuring_time(start_time)
 
-    return [check_with_pcc(torch_output_tensor, output_tensor, 0.999), e2e_perf]
+    pcc = check_with_pcc(torch_output_tensor, output_tensor, 0.999)
+    # print(pcc)
+    return [pcc, e2e_perf]
