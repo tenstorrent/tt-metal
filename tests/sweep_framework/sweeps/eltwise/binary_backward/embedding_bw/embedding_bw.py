222dfc4f9927563dd903dbd0f1afcaf2a31938e9
amalbasaTT <amalbasa@tenstorrent.com>
2024-10-09 13:05:15 +0000

    #11512: Add mesh_device_fixture to embedding_bw sweep

--- tests/sweep_framework/sweeps/eltwise/binary_backward/embedding_bw/embedding_bw.py
+++ tests/sweep_framework/sweeps/eltwise/binary_backward/embedding_bw/embedding_bw.py
@@ -4,6 +4,7 @@
 
 from typing import Optional, Tuple
 from functools import partial
+import os
 
 import torch
 import random
@@ -54,6 +55,15 @@ parameters = {
         "output_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
     },
 }
+
+
+def mesh_device_fixture():
+    device = ttnn.open_device(device_id=0)
+    assert not ttnn.device.is_wormhole_b0(device), "This op is not available for Wormhole_B0"
+    device_name = os.environ.get("ARCH_NAME", os.environ.get("TT_ARCH_NAME", "default")).lower()
+    yield (device, device_name)
+    ttnn.close_device(device)
+    del device
 
 
 # TO-DO: Create an issue on this, since these constrictions are not mentioned in the documentation
