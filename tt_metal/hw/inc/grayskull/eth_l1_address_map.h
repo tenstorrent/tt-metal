f8ef4f78451eafd663381d11fe0bfb6c3e4c8279
Nathan Sidwell <nsidwell@tenstorrent.com>
2024-10-10 16:12:15 +0000

    #13554: Separate out erisc application startup/teardown

--- tt_metal/hw/inc/grayskull/eth_l1_address_map.h
+++ tt_metal/hw/inc/grayskull/eth_l1_address_map.h
@@ -16,7 +16,6 @@ struct address_map {
   // Base addresses
   static constexpr std::int32_t FIRMWARE_BASE = 0;
   static constexpr std::int32_t ERISC_MEM_MAILBOX_BASE = 16;
-  static constexpr std::int32_t ERISC_MEM_MAILBOX_STACK_SAVE = 0;
 
   static constexpr std::int32_t COMMAND_Q_BASE = 0;
   static constexpr std::int32_t TILE_HEADER_BUFFER_BASE = 0;
