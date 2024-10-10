d2d41b555bf98f127435751f79aad81ecd3ef1c0
Colman Glagovich <114512306+cglagovichTT@users.noreply.github.com>
2024-10-10 10:51:02 -0400

    #13300: Refactor Llama ROPE op and enable non-power of 2 (#13348)

--- ttnn/cpp/ttnn/operations/experimental/transformer/rotary_embedding_llama/device/kernels/dataflow/writer_rotary_embedding_llama_interleaved_start_id.cpp
+++ ttnn/cpp/ttnn/operations/experimental/transformer/rotary_embedding_llama/device/kernels/dataflow/writer_rotary_embedding_llama_interleaved_start_id.cpp
@@ -6,15 +6,18 @@
 
 void kernel_main() {
 
-    uint32_t dst_addr  = get_arg_val<uint32_t>(0);
-    uint32_t start_row_idx = get_arg_val<uint32_t>(1); // Index correctly in the for loop
+    uint32_t argrt = 0;
+    uint32_t dst_addr  = get_arg_val<uint32_t>(argrt++);
+    uint32_t batch_start = get_arg_val<uint32_t>(argrt++);
+    uint32_t batch_end = get_arg_val<uint32_t>(argrt++);
+    uint32_t seq_t_start = get_arg_val<uint32_t>(argrt++);
+    uint32_t seq_t_end = get_arg_val<uint32_t>(argrt++);
 
     constexpr uint32_t cb_id_out = get_compile_time_arg_val(0);
     constexpr bool dst_is_dram = get_compile_time_arg_val(1) == 1;
-    constexpr uint32_t num_rows_per_core =  get_compile_time_arg_val(2);
-    constexpr uint32_t num_sin_cos_rows_per_core = get_compile_time_arg_val(3);
-    constexpr uint32_t Wt =  get_compile_time_arg_val(4);
-    constexpr uint32_t Ht =  get_compile_time_arg_val(5);
+    constexpr uint32_t n_heads = get_compile_time_arg_val(2);
+    constexpr uint32_t Wt =  get_compile_time_arg_val(3);
+    constexpr uint32_t Ht =  get_compile_time_arg_val(4);
 
 
     // single-tile ublocks
@@ -29,25 +32,21 @@ void kernel_main() {
         .data_format = data_format
     };
 
-    uint32_t output_row_cnt = 0;
-
-    uint32_t tile_idx = start_row_idx * Wt; // start index in tiles, instead of rows
-    for (uint32_t i = 0; i < num_rows_per_core; i++) {
-        cb_wait_front(cb_id_out, Wt);
-
-        // Write a row
-        uint32_t l1_read_addr = get_read_ptr(cb_id_out);
-        for (uint32_t j = 0; j < Wt; j++) {
-            noc_async_write_tile(tile_idx, s, l1_read_addr);
-            l1_read_addr += tile_bytes;
-            tile_idx++;
-        }
-        noc_async_write_barrier();
-        cb_pop_front(cb_id_out, Wt);
-        output_row_cnt++;
-
-        if (output_row_cnt % num_sin_cos_rows_per_core == 0) {
-            tile_idx += (Ht - num_sin_cos_rows_per_core) * Wt; // Increment by stride
+    for (uint32_t batch_id = batch_start; batch_id < batch_end; ++batch_id) {
+        for (uint32_t head_num = 0; head_num < n_heads; ++head_num) {
+            for (uint32_t seq_tile = seq_t_start; seq_tile < seq_t_end; ++seq_tile) {
+                uint32_t output_curr_idx = batch_id * n_heads * Ht * Wt + head_num * Ht * Wt + seq_tile * Wt;
+                cb_wait_front(cb_id_out, Wt);
+                // Write a row
+                uint32_t l1_read_addr = get_read_ptr(cb_id_out);
+                for (uint32_t j = 0; j < Wt; j++) {
+                    noc_async_write_tile(output_curr_idx, s, l1_read_addr);
+                    l1_read_addr += tile_bytes;
+                    output_curr_idx++;
+                }
+                noc_async_write_barrier();
+                cb_pop_front(cb_id_out, Wt);
+            }
         }
     }
 }
