b4eb60cc211ec1345e1ab638cb15bbc39bb1914e
namhyeong-kim <141107133+namhyeong-kim@users.noreply.github.com>
2024-10-10 14:46:54 +0900

    #13576: Apply unpack_to_dest and use matmul_tiles instread of reduce_tile in w-dim (#13617)
    * #13576: Apply unpack_to_dest to moreh_mean_h
    
    * #13576: Apply unpack_to_dest to moreh_linear_bwd's bias_bwd
    
    * #13576: Add fp32 moreh_matmul test
    
    * #13576: Use matmul_tiles instead of reduce_tile in moreh_mean_w
    
    * #13576: Do not check rank of tiled idx tensor
    
    * #13576: Use matmul_tiles instead of reduce_w in moreh_sum_w
    
    * #13576: Apply unpack_to_dest in moreh_sum_h
    
    * #13576: Fix check-black error
    
    ---------
    
    Co-authored-by: Dongjin Na <dongjin.na@moreh.io>

--- tests/ttnn/unit_tests/operations/test_moreh_linear.py
+++ tests/ttnn/unit_tests/operations/test_moreh_linear.py
@@ -5,32 +5,14 @@
 import pytest
 import torch
 import ttnn
-from models.utility_functions import comp_allclose_and_pcc
-from tests.ttnn.unit_tests.operations.test_moreh_matmul import get_tensors
+from models.utility_functions import comp_allclose_and_pcc, skip_for_grayskull
+from tests.ttnn.unit_tests.operations.test_moreh_matmul import get_tensors, get_bias_tensors
 from loguru import logger
 from tests.ttnn.unit_tests.operations.test_utils import (
     get_compute_kernel_options,
     compute_kernel_options,
     compute_kernel_ids,
 )
-
-
-# TODO: add this feature in get_tensors method
-def get_bias_tensors(bias_shape, require_bias_grad, device):
-    npu_dtype = ttnn.bfloat16
-    cpu_dtype = torch.bfloat16
-    npu_layout = ttnn.TILE_LAYOUT
-    cpu_layout = ttnn.ROW_MAJOR_LAYOUT
-
-    bias = torch.randint(-10, 10, bias_shape, dtype=cpu_dtype)
-    tt_bias = ttnn.Tensor(bias, npu_dtype).pad_to_tile(float("nan")).to(npu_layout).to(device)
-
-    tt_bias_grad = None
-    if require_bias_grad:
-        bias_grad = torch.full(bias_shape, float("nan"), dtype=cpu_dtype)
-        tt_bias_grad = ttnn.Tensor(bias_grad, npu_dtype).pad_to_tile(float("nan")).to(npu_layout).to(device)
-
-    return tt_bias, bias, tt_bias_grad
 
 
 def moreh_linear(shapes, has_bias, has_output, compute_kernel_config, device):
@@ -267,3 +249,93 @@ def test_moreh_linear_backward_enable_cache(shapes, device, use_program_cache):
         num_program_cache_entries_list.append(device.num_program_cache_entries())
         assert passing
     assert len(set(num_program_cache_entries_list)) == 1
+
+
+@skip_for_grayskull("GS does not support fp32")
+@pytest.mark.parametrize(
+    "shapes",
+    (
+        # input, weight, bias(1d or scalar), output
+        # GPT2-Small cases
+        ([8, 512, 768], [2304, 768], [1, 2304], [8, 512, 2304]),
+        ([8, 512, 768], [768, 768], [1, 768], [8, 512, 768]),
+        ([8, 512, 768], [3072, 768], [1, 3072], [8, 512, 3072]),
+    ),
+)
+def test_moreh_bias_backward_fp32(shapes, device):
+    torch.manual_seed(3072)
+    compute_kernel_fp32_config = get_compute_kernel_options(True)
+    compute_kernel_config = get_compute_kernel_options(False)
+    requires_input_grad, requires_weight_grad, requires_bias_grad = (True, False, True)
+    input_shape, weight_shape, bias_shape, output_shape = shapes
+    (
+        tt_input,
+        tt_weight,
+        _,
+        tt_output_grad,
+        tt_input_grad,
+        _,
+        torch_input,
+        torch_weight,
+        torch_output_grad,
+    ) = get_tensors(
+        input_shape, weight_shape, output_shape, requires_input_grad, requires_weight_grad, False, device, False
+    )
+    tt_bias, torch_bias, tt_bias_grad = get_bias_tensors(bias_shape, requires_bias_grad, device, False)
+    (_, _, _, _, tt_input_grad_fp32, _, _, _, _) = get_tensors(
+        input_shape, weight_shape, output_shape, requires_input_grad, requires_weight_grad, False, device, False
+    )
+    (_, _, tt_bias_grad_fp32) = get_bias_tensors(bias_shape, requires_bias_grad, device, False)
+    ## tt linear backward (fp32 mode)
+    tt_input_grad_fp32, _, tt_bias_grad_fp32 = ttnn.operations.moreh.linear_backward(
+        tt_output_grad,
+        tt_input,
+        tt_weight,
+        are_required_outputs=(requires_input_grad, requires_weight_grad, requires_bias_grad),
+        bias=tt_bias,
+        input_grad=tt_input_grad_fp32,
+        weight_grad=None,
+        bias_grad=tt_bias_grad_fp32,
+        compute_kernel_config=compute_kernel_fp32_config,
+    )
+    ## tt linear backward (bf16 mode)
+    tt_input_grad, _, tt_bias_grad = ttnn.operations.moreh.linear_backward(
+        tt_output_grad,
+        tt_input,
+        tt_weight,
+        are_required_outputs=(requires_input_grad, requires_weight_grad, requires_bias_grad),
+        bias=tt_bias,
+        input_grad=tt_input_grad,
+        weight_grad=None,
+        bias_grad=tt_bias_grad,
+        compute_kernel_config=compute_kernel_config,
+    )
+    torch_input_fp32 = torch_input.float()
+    torch_weight_fp32 = torch_weight.float()
+    torch_bias_fp32 = torch_bias.float()
+    ## reference
+    torch_output = torch.nn.functional.linear(
+        torch_input_fp32.requires_grad_(requires_input_grad),
+        torch_weight_fp32.requires_grad_(requires_weight_grad),
+        torch_bias_fp32.requires_grad_(requires_bias_grad),
+    )
+    torch_output.backward(torch_output_grad.float())
+    ## test for equivalance
+    rtol = atol = 0.1
+    tt_bias_grad_fp32_cpu = tt_bias_grad_fp32.cpu().to(ttnn.ROW_MAJOR_LAYOUT).unpad_from_tile(bias_shape).to_torch()
+    tt_bias_grad_cpu = tt_bias_grad.cpu().to(ttnn.ROW_MAJOR_LAYOUT).unpad_from_tile(bias_shape).to_torch()
+    passing, output_pcc = comp_allclose_and_pcc(
+        torch_bias_fp32.grad, tt_bias_grad_fp32_cpu, pcc=0.98, rtol=rtol, atol=atol
+    )
+    logger.debug(f"Out passing={passing}")
+    logger.debug(f"Output pcc={output_pcc}")
+    assert passing
+    diff_fp32 = torch.abs(torch_bias_fp32.grad - tt_bias_grad_fp32_cpu)
+    logger.debug(f"std={torch.std(diff_fp32)}")
+    logger.debug(f"mean={diff_fp32.mean()}")
+    logger.debug(f"topk(5) {torch.topk(diff_fp32.reshape(-1), 5)}")
+    diff = torch.abs(torch_bias_fp32.grad - tt_bias_grad_cpu)
+    logger.debug(f"std={torch.std(diff)}")
+    logger.debug(f"mean={diff.mean()}")
+    logger.debug(f"topk(5) {torch.topk(diff.reshape(-1), 5)}")
+    assert diff_fp32.mean() < diff.mean()
