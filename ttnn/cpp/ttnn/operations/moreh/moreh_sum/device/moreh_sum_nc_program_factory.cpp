d17b6220be9d7c064ecec7bb7cd2ebefd4928086
Shaw Nguyen <49144872+mrshaw01@users.noreply.github.com>
2024-10-11 08:32:52 +0700

    #13663: Clean Up Deprecated Operations (#13664)
    * #13663: Clean Up Deprecated moreh_bmm (forward, backward)
    
    * #13663: Clean Up Deprecated moreh_matmul (forward, backward)
    
    * #13663: Clean Up Deprecated moreh_dot (forward, backward)
    
    * #13663: Rename folder moreh_dot_op_backward to moreh_dot_backward
    
    * #13663: Clean Up Deprecated moreh_layer_norm (forward, backward)
    
    * #13663: Clean Up Deprecated moreh_sum (forward, backward)
    
    * #13663: Relocate moreh_helper_functions
    
    * #13663: Relocate math.hpp
    
    * #13663: Relocate cb_utils.hpp
    
    * #13663: Relocate sharding_utilities
    
    * #13663: Revise minor output_mem_config to memory_config
    
    * #13663: Revise minor mem_config to memory_config
    
    * #13663: Revise docstring
    
    * #13663: Revise keepdim
    
    * #13663: Revise dtype
    
    * #13663: Relocate cb_utils, sharding_utilities, math
    
    ---------
    
    Co-authored-by: Artem Yerofieiev <169092593+ayerofieiev-tt@users.noreply.github.com>

--- ttnn/cpp/ttnn/operations/moreh/moreh_sum/device/moreh_sum_nc_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_sum/device/moreh_sum_nc_program_factory.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 #include "moreh_sum_device_operation.hpp"
-#include "ttnn/deprecated/tt_dnn/op_library/moreh_helper_functions.hpp"
+#include "ttnn/operations/moreh/moreh_helper_functions.hpp"
 #include "tt_metal/common/work_split.hpp"
 #include "ttnn/operations/core/compute_kernel/compute_kernel_config.hpp"
 
@@ -18,7 +18,7 @@ MorehSumOperation::MorehSumNCFactory::cached_program_t MorehSumOperation::MorehS
     auto output = output_tensor;
     auto dim = operation_attributes.dim;
 
-    auto output_mem_config = operation_attributes.output_mem_config;
+    auto memory_config = operation_attributes.memory_config;
     const DeviceComputeKernelConfig &compute_kernel_config = operation_attributes.compute_kernel_config;
 
     auto* device = input.device();
