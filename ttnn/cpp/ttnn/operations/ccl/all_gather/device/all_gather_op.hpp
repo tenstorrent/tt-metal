06402a3cb4c8bda6d9cd747eb8ba23509c2050b0
Stanislav Minakov <sminakov@tenstorrent.com>
2024-10-10 23:26:31 -0700

    #13707: Revert changes to all gather op related to shape refactor (#13719)

--- ttnn/cpp/ttnn/operations/ccl/all_gather/device/all_gather_op.hpp
+++ ttnn/cpp/ttnn/operations/ccl/all_gather/device/all_gather_op.hpp
@@ -132,7 +132,7 @@ struct AllGather {
     const ccl::Topology topology;
 
     void validate(const std::vector<Tensor> &input_tensors) const;
-    std::vector<ttnn::SimpleShape> compute_output_shapes(const std::vector<Tensor> &input_tensors) const;
+    std::vector<tt::tt_metal::LegacyShape> compute_output_shapes(const std::vector<Tensor> &input_tensors) const;
     std::vector<Tensor> create_output_tensors(const std::vector<Tensor> &input_tensors) const;
     operation::ProgramWithCallbacks create_program(const std::vector<Tensor>& input_tensors, std::vector<Tensor> &output_tensors) const;
 };
