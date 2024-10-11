e0f7216c8d17e76777b6ef1f7f012886bc871570
Samarth Agarwal <sagarwal@tenstorrent.com>
2024-10-11 16:59:40 +0000

    #0: Added support for runtime args

--- tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/kernels/dispatcher_kernel_size_and_runtime.cpp
+++ tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/kernels/dispatcher_kernel_size_and_runtime.cpp
@@ -2,8 +2,14 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-// #include "debug/dprint.h"
+#include <cstdint>
+#include "dataflow_api.h"
 #include "c_tensix_core.h"
+
+constexpr uint32_t num_unique_rt_args = get_compile_time_arg_val(0);
+constexpr uint32_t num_common_rt_args = get_compile_time_arg_val(1);
+constexpr uint32_t unique_rt_args_vals_offset = get_compile_time_arg_val(2);
+constexpr uint32_t common_rt_args_vals_offset = get_compile_time_arg_val(3);
 
 #if KERNEL_SIZE_BYTES > 16
 constexpr uint32_t empty_kernel_bytes = 16;
@@ -12,9 +18,21 @@ uint8_t data1[KERNEL_SIZE_BYTES - empty_kernel_bytes] __attribute__ ((section ("
 
 void kernel_main() {
     const uint64_t end_time = c_tensix_core::read_wall_clock() + KERNEL_RUNTIME_SECONDS;
-    // DPRINT << KERNEL_RUNTIME_SECONDS << ENDL();
-    // DPRINT << end_time << ENDL();
-    while (c_tensix_core::read_wall_clock() < end_time) {
-        // DPRINT << c_tensix_core::read_wall_clock() << ENDL();
+    while (c_tensix_core::read_wall_clock() < end_time);
+
+    for (uint32_t i = 0; i < num_unique_rt_args; i++) {
+        const uint32_t rt_arg = get_arg_val<uint32_t>(i);
+        const uint32_t expected = i + unique_rt_args_vals_offset;
+        if (rt_arg != expected) {
+            while(true); // Hang kernel if values aren't correct
+        }
+    }
+
+    for (uint32_t i = 0; i < num_common_rt_args; i++) {
+        const uint32_t common_rt_arg = get_common_arg_val<uint32_t>(i);
+        uint32_t expected = i + common_rt_args_vals_offset;
+        if (common_rt_arg != expected){
+            while(true); // Hang kernel if values aren't correct
+        }
     }
 }
--- tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/test_dispatcher_kernel_size_and_runtime.cpp
+++ tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/test_dispatcher_kernel_size_and_runtime.cpp
@@ -4,6 +4,7 @@
 
 #include <cstdint>
 #include <cstdlib>
+#include <vector>
 #include "assert.hpp"
 #include "common/core_coord.h"
 #include "impl/device/device.hpp"
@@ -11,6 +12,7 @@
 #include "logger.hpp"
 #include "device/tt_cluster_descriptor_types.h"
 #include "test_common.hpp"
+#include "tests/tt_metal/tt_metal/unit_tests_common/common/test_utils.hpp"
 #include "tt_metal/impl/kernels/kernel_types.hpp"
 #include "tt_metal/impl/program/program.hpp"
 #include "tt_metal/host_api.hpp"
@@ -27,6 +29,8 @@ constexpr uint32_t DEFAULT_MIN_KERNEL_SIZE_BYTES = 20;
 constexpr uint32_t DEFAULT_MAX_KERNEL_SIZE_BYTES = 4096;
 constexpr uint32_t DEFAULT_MIN_KERNEL_RUNTIME_CYCLES = 0;
 constexpr uint32_t DEFAULT_MAX_KERNEL_RUNTIME_CYCLES = 100000;
+constexpr uint32_t DEFAULT_MIN_NUM_RUNTIME_ARGS = 0;
+constexpr uint32_t DEFAULT_MAX_NUM_RUNTIME_ARGS = max_runtime_args;
 constexpr bool DEFAULT_ONLY_DISPATCH_TO_TENSIX_CORES = false;
 constexpr bool DEFAULT_ONLY_DISPATCH_TO_ACTIVE_ETH_CORES = false;
 
@@ -36,6 +40,8 @@ uint32_t min_kernel_size_bytes_g;
 uint32_t max_kernel_size_bytes_g;
 uint32_t min_kernel_runtime_cycles_g;
 uint32_t max_kernel_runtime_cycles_g;
+uint32_t min_num_rt_args_g;
+uint32_t max_num_rt_args_g;
 bool only_dispatch_to_tensix_cores_g;
 bool only_dispatch_to_active_eth_cores_g;
 
@@ -53,6 +59,8 @@ void init(int argc, char **argv) {
             log_info(LogTest, "  -smax: Maximum kernel size in bytes. Value must be divisible by 4. (default: {})", DEFAULT_MAX_KERNEL_SIZE_BYTES);
             log_info(LogTest, "  -rmin: Minimum kernel runtime in cycles. (default: {})", DEFAULT_MIN_KERNEL_RUNTIME_CYCLES);
             log_info(LogTest, "  -rmax: Maximum kernel runtime in cycles. (default: {})", DEFAULT_MAX_KERNEL_RUNTIME_CYCLES);
+            log_info(LogTest, "  -rtamin: Minimum number of runtime args. (default: {})", DEFAULT_MIN_NUM_RUNTIME_ARGS);
+            log_info(LogTest, "  -rtamax: Maximum number of runtime args. (default: {})", DEFAULT_MAX_NUM_RUNTIME_ARGS);
             log_info(LogTest, "  -t: Only dispatch to Tensix cores. (default: {})", DEFAULT_ONLY_DISPATCH_TO_TENSIX_CORES);
             log_info(LogTest, "  -e: Only dispatch to active ethernet cores. (default: {})", DEFAULT_ONLY_DISPATCH_TO_ACTIVE_ETH_CORES);
             exit(0);
@@ -64,6 +72,8 @@ void init(int argc, char **argv) {
     max_kernel_size_bytes_g = test_args::get_command_option_uint32(input_args, "-smax", DEFAULT_MAX_KERNEL_SIZE_BYTES);
     min_kernel_runtime_cycles_g = test_args::get_command_option_uint32(input_args, "-rmin", DEFAULT_MIN_KERNEL_RUNTIME_CYCLES);
     max_kernel_runtime_cycles_g = test_args::get_command_option_uint32(input_args, "-rmax", DEFAULT_MAX_KERNEL_RUNTIME_CYCLES);
+    min_num_rt_args_g = test_args::get_command_option_uint32(input_args, "-rtamin", DEFAULT_MIN_NUM_RUNTIME_ARGS);
+    max_num_rt_args_g = test_args::get_command_option_uint32(input_args, "-rtamax", DEFAULT_MAX_NUM_RUNTIME_ARGS);
     only_dispatch_to_tensix_cores_g = test_args::has_command_option(input_args, "-t");
     only_dispatch_to_active_eth_cores_g = test_args::has_command_option(input_args, "-e");
 
@@ -99,6 +109,18 @@ void init(int argc, char **argv) {
         log_fatal("Maximum kernel size must be <= {} cycles", DEFAULT_MAX_KERNEL_RUNTIME_CYCLES);
         exit(0);
     }
+    if (min_num_rt_args_g < DEFAULT_MIN_NUM_RUNTIME_ARGS) {
+        log_fatal("Minimum number of runtime args must be >= {}", DEFAULT_MIN_NUM_RUNTIME_ARGS);
+        exit(0);
+    }
+    if (max_num_rt_args_g > DEFAULT_MAX_NUM_RUNTIME_ARGS) {
+        log_fatal("Maximum number of runtime args must be <= {}", DEFAULT_MAX_NUM_RUNTIME_ARGS);
+        exit(0);
+    }
+    if (min_num_rt_args_g > max_num_rt_args_g) {
+        log_fatal("Minimum number of runtime args must be <= maximum number of runtime args");
+        exit(0);
+    }
     if (only_dispatch_to_tensix_cores_g && only_dispatch_to_active_eth_cores_g) {
         log_fatal("Flags {-t, -e} are mutually exclusive");
         exit(0);
@@ -109,12 +131,8 @@ bool does_device_have_active_eth_cores(const Device *device) {
     return !(device->get_active_ethernet_cores(true).empty());
 }
 
-uint32_t generate_random_size(const uint32_t min, const uint32_t max) {
-    return min + (rand() % ((max - min) / 4 + 1)) * 4;
-}
-
-uint32_t generate_random_runtime(const uint32_t min, const uint32_t max) {
-    return min + rand() % (max - min + 1);
+uint32_t generate_random_num(const uint32_t min, const uint32_t max, const uint32_t divisible_by = 1) {
+    return min + (rand() % ((max - min) / divisible_by + 1)) * divisible_by;
 }
 
 CoreRangeSet get_kernel_core_range_set(const Device* device, const CoreType& core_type) {
@@ -136,8 +154,8 @@ CoreRangeSet get_kernel_core_range_set(const Device* device, const CoreType& cor
 }
 
 KernelHandle initialize_kernel(Program& program, const Device* device) {
-    const uint32_t kernel_size_bytes = generate_random_size(min_kernel_size_bytes_g, max_kernel_size_bytes_g);
-    const uint32_t kernel_runtime_cycles = generate_random_runtime(min_kernel_runtime_cycles_g, max_kernel_runtime_cycles_g);
+    const uint32_t kernel_size_bytes = generate_random_num(min_kernel_size_bytes_g, max_kernel_size_bytes_g, 4);
+    const uint32_t kernel_runtime_cycles = generate_random_num(min_kernel_runtime_cycles_g, max_kernel_runtime_cycles_g);
     const std::map<string, string> defines = {
         {"KERNEL_SIZE_BYTES", std::to_string(kernel_size_bytes)},
         {"KERNEL_RUNTIME_SECONDS", std::to_string(kernel_runtime_cycles)}
@@ -146,24 +164,41 @@ KernelHandle initialize_kernel(Program& program, const Device* device) {
     log_info(LogTest, "Size: {}", kernel_size_bytes);
     log_info(LogTest, "Runtime: {}", kernel_runtime_cycles);
 
+    const uint32_t num_unique_rt_args = generate_random_num(min_num_rt_args_g, max_num_rt_args_g);
+    const uint32_t num_common_rt_args = generate_random_num(0, max_num_rt_args_g - num_unique_rt_args);
+    const uint32_t unique_rt_args_vals_offset = 50;
+    const uint32_t common_rt_args_vals_offset = 100;
+    const auto [unique_rt_args, common_rt_args] = create_runtime_args(
+        num_unique_rt_args, num_common_rt_args, unique_rt_args_vals_offset, common_rt_args_vals_offset);
+
+    log_info(LogTest, "Num unique rt args: {}", num_unique_rt_args);
+    log_info(LogTest, "Num common rt args: {}", num_common_rt_args);
+
+    const std::vector<uint32_t> compile_args = {
+        num_unique_rt_args, num_common_rt_args, unique_rt_args_vals_offset, common_rt_args_vals_offset};
+
+    CoreRangeSet cores({});
     KernelHandle kernel_id;
     if (only_dispatch_to_tensix_cores_g || !does_device_have_active_eth_cores(device)) {
-        CoreRangeSet cores = get_kernel_core_range_set(device, CoreType::WORKER);
+        cores = get_kernel_core_range_set(device, CoreType::WORKER);
         log_info(LogTest, "Cores: {}", cores.str());
-        kernel_id = CreateKernel(program, kernel_file_path, cores, DataMovementConfig{.defines = defines});
+        kernel_id = CreateKernel(program, kernel_file_path, cores, DataMovementConfig{.compile_args=compile_args, .defines = defines});
     } else if (only_dispatch_to_active_eth_cores_g) {
-        CoreRangeSet cores = get_kernel_core_range_set(device, CoreType::ETH);
+        cores = get_kernel_core_range_set(device, CoreType::ETH);
         log_info(LogTest, "Cores: {}", cores.str());
-        kernel_id = CreateKernel(program, kernel_file_path, cores, EthernetConfig{.defines = defines});
+        kernel_id = CreateKernel(program, kernel_file_path, cores, EthernetConfig{.compile_args=compile_args, .defines = defines});
     } else {
         if (rand() % 2 == 0) {
-            CoreRangeSet cores = get_kernel_core_range_set(device, CoreType::WORKER);
-            kernel_id = CreateKernel(program, kernel_file_path, cores, DataMovementConfig{.defines = defines});
+            cores = get_kernel_core_range_set(device, CoreType::WORKER);
+            kernel_id = CreateKernel(program, kernel_file_path, cores, DataMovementConfig{.compile_args=compile_args, .defines = defines});
         } else {
-            CoreRangeSet cores = get_kernel_core_range_set(device, CoreType::ETH);
-            kernel_id = CreateKernel(program, kernel_file_path, cores, EthernetConfig{.defines = defines});
+            cores = get_kernel_core_range_set(device, CoreType::ETH);
+            kernel_id = CreateKernel(program, kernel_file_path, cores, EthernetConfig{.compile_args=compile_args, .defines = defines});
         }
     }
+
+    SetRuntimeArgs(program, kernel_id, cores, unique_rt_args);
+    SetCommonRuntimeArgs(program, kernel_id, common_rt_args);
 
     return kernel_id;
 }
