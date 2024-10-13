49e5f614fd9c4db2bd5b172ec63396c8f19a6bfe
Shaw Nguyen <nguyenvanthanhtung2001@gmail.com>
2024-10-13 18:48:24 +0000

    #13766: revise names

--- ttnn/cpp/ttnn/operations/data_movement/clone/device/kernels/read_kernel_rm.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/clone/device/kernels/read_kernel_rm.cpp
@@ -5,21 +5,25 @@
 #include "dataflow_api.h"
 
 void kernel_main() {
-    uint32_t src_addr = get_arg_val<uint32_t>(0);
     uint32_t stick_size = get_arg_val<uint32_t>(1);
     uint32_t num_sticks = get_arg_val<uint32_t>(2);
     uint32_t start_id = get_arg_val<uint32_t>(3);
 
     constexpr uint32_t src_cb_id = get_compile_time_arg_val(0);
-    constexpr bool src_is_dram = get_compile_time_arg_val(1) == 1;
+    constexpr bool input_is_dram = get_compile_time_arg_val(1) == 1;
 
 #define src_stick_size_is_power_of_two get_compile_time_arg_val(2) == 1
 #if (src_stick_size_is_power_of_two)
     constexpr uint32_t src_log_base_2_of_page_size = get_compile_time_arg_val(3);
-    const InterleavedPow2AddrGen<src_is_dram> s = {
-        .bank_base_address = src_addr, .log_base_2_of_page_size = src_log_base_2_of_page_size};
+    const InterleavedPow2AddrGen<input_is_dram> s = {
+        .bank_base_address = get_arg_val<uint32_t>(0),
+        .log_base_2_of_page_size = src_log_base_2_of_page_size,
+    };
 #else
-    const InterleavedAddrGen<src_is_dram> s = {.bank_base_address = src_addr, .page_size = stick_size};
+    const InterleavedAddrGen<input_is_dram> s = {
+        .bank_base_address = get_arg_val<uint32_t>(0),
+        .page_size = stick_size,
+    };
 #endif
 
     uint32_t end_id = start_id + num_sticks;
--- ttnn/cpp/ttnn/operations/data_movement/clone/device/kernels/write_kernel_rm.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/clone/device/kernels/write_kernel_rm.cpp
@@ -5,21 +5,25 @@
 #include "dataflow_api.h"
 
 void kernel_main() {
-    uint32_t dst_addr = get_arg_val<uint32_t>(0);
     uint32_t stick_size = get_arg_val<uint32_t>(1);
     uint32_t num_sticks = get_arg_val<uint32_t>(2);
     uint32_t start_id = get_arg_val<uint32_t>(3);
 
     constexpr uint32_t dst_cb_id = get_compile_time_arg_val(0);
-    constexpr bool dst_is_dram = get_compile_time_arg_val(1) == 1;
+    constexpr bool output_is_dram = get_compile_time_arg_val(1) == 1;
 
 #define dst_stick_size_is_power_of_two get_compile_time_arg_val(2) == 1
 #if (dst_stick_size_is_power_of_two)
     constexpr uint32_t dst_log_base_2_of_page_size = get_compile_time_arg_val(3);
-    const InterleavedPow2AddrGen<dst_is_dram> s = {
-        .bank_base_address = dst_addr, .log_base_2_of_page_size = dst_log_base_2_of_page_size};
+    const InterleavedPow2AddrGen<output_is_dram> s = {
+        .bank_base_address = get_arg_val<uint32_t>(0),
+        .log_base_2_of_page_size = dst_log_base_2_of_page_size,
+    };
 #else
-    const InterleavedAddrGen<dst_is_dram> s = {.bank_base_address = dst_addr, .page_size = stick_size};
+    const InterleavedAddrGen<output_is_dram> s = {
+        .bank_base_address = get_arg_val<uint32_t>(0),
+        .page_size = stick_size,
+    };
 #endif
 
     uint32_t end_id = start_id + num_sticks;
