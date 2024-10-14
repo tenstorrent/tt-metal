871abfad75f77281011b134006a2f29cbd4045c0
Shaw Nguyen <nguyenvanthanhtung2001@gmail.com>
2024-10-14 04:08:47 +0000

    #13766: revise explicit arguments

--- ttnn/cpp/ttnn/operations/data_movement/clone/device/kernels/read_kernel.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/clone/device/kernels/read_kernel.cpp
@@ -5,6 +5,7 @@
 #include "dataflow_api.h"
 
 void kernel_main() {
+    uint32_t input_buffer_address = get_arg_val<uint32_t>(0);
     uint32_t num_tiles = get_arg_val<uint32_t>(1);
     uint32_t start_id = get_arg_val<uint32_t>(2);
 
@@ -12,7 +13,7 @@ void kernel_main() {
     constexpr bool input_is_dram = get_compile_time_arg_val(1) == 1;
 
     const InterleavedAddrGenFast<input_is_dram> s = {
-        .bank_base_address = get_arg_val<uint32_t>(0),
+        .bank_base_address = input_buffer_address,
         .page_size = get_tile_size(src_cb_id),
         .data_format = get_dataformat(src_cb_id),
     };
@@ -20,7 +21,8 @@ void kernel_main() {
     uint32_t end_id = start_id + num_tiles;
     for (uint32_t i = start_id; i < end_id; ++i) {
         cb_reserve_back(src_cb_id, 1);
-        noc_async_read_tile(i, s, get_write_ptr(src_cb_id));
+        uint32_t src_cb_write_addr = get_write_ptr(src_cb_id);
+        noc_async_read_tile(i, s, src_cb_write_addr);
         noc_async_read_barrier();
         cb_push_back(src_cb_id, 1);
     }
--- ttnn/cpp/ttnn/operations/data_movement/clone/device/kernels/read_kernel_rm.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/clone/device/kernels/read_kernel_rm.cpp
@@ -5,6 +5,7 @@
 #include "dataflow_api.h"
 
 void kernel_main() {
+    uint32_t input_buffer_address = get_arg_val<uint32_t>(0);
     uint32_t stick_size = get_arg_val<uint32_t>(1);
     uint32_t num_sticks = get_arg_val<uint32_t>(2);
     uint32_t start_id = get_arg_val<uint32_t>(3);
@@ -16,12 +17,12 @@ void kernel_main() {
 #if (src_stick_size_is_power_of_two)
     constexpr uint32_t src_log_base_2_of_page_size = get_compile_time_arg_val(3);
     const InterleavedPow2AddrGen<input_is_dram> s = {
-        .bank_base_address = get_arg_val<uint32_t>(0),
+        .bank_base_address = input_buffer_address,
         .log_base_2_of_page_size = src_log_base_2_of_page_size,
     };
 #else
     const InterleavedAddrGen<input_is_dram> s = {
-        .bank_base_address = get_arg_val<uint32_t>(0),
+        .bank_base_address = input_buffer_address,
         .page_size = stick_size,
     };
 #endif
@@ -29,7 +30,9 @@ void kernel_main() {
     uint32_t end_id = start_id + num_sticks;
     for (uint32_t i = start_id; i < end_id; ++i) {
         cb_reserve_back(src_cb_id, 1);
-        noc_async_read(get_noc_addr(i, s), get_write_ptr(src_cb_id), stick_size);
+        uint64_t input_noc_addr = get_noc_addr(i, s);
+        uint32_t src_cb_write_addr = get_write_ptr(src_cb_id);
+        noc_async_read(input_noc_addr, src_cb_write_addr, stick_size);
         noc_async_read_barrier();
         cb_push_back(src_cb_id, 1);
     }
