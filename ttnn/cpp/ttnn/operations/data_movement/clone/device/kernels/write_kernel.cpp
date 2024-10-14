dd4ae2b41032ed18db6d23f754c28567659a4fdb
Shaw Nguyen <nguyenvanthanhtung2001@gmail.com>
2024-10-14 04:08:47 +0000

    #13766: revise explicit arguments

--- ttnn/cpp/ttnn/operations/data_movement/clone/device/kernels/write_kernel.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/clone/device/kernels/write_kernel.cpp
@@ -5,6 +5,7 @@
 #include "dataflow_api.h"
 
 void kernel_main() {
+    uint32_t output_buffer_address = get_arg_val<uint32_t>(0);
     uint32_t num_tiles = get_arg_val<uint32_t>(1);
     uint32_t start_id = get_arg_val<uint32_t>(2);
 
@@ -12,7 +13,7 @@ void kernel_main() {
     constexpr bool output_is_dram = get_compile_time_arg_val(1) == 1;
 
     const InterleavedAddrGenFast<output_is_dram> s = {
-        .bank_base_address = get_arg_val<uint32_t>(0),
+        .bank_base_address = output_buffer_address,
         .page_size = get_tile_size(dst_cb_id),
         .data_format = get_dataformat(dst_cb_id),
     };
@@ -20,7 +21,8 @@ void kernel_main() {
     uint32_t end_id = start_id + num_tiles;
     for (uint32_t i = start_id; i < end_id; ++i) {
         cb_wait_front(dst_cb_id, 1);
-        noc_async_write_tile(i, s, get_read_ptr(dst_cb_id));
+        uint32_t dst_cb_read_addr = get_read_ptr(dst_cb_id);
+        noc_async_write_tile(i, s, dst_cb_read_addr);
         noc_async_write_barrier();
         cb_pop_front(dst_cb_id, 1);
     }
