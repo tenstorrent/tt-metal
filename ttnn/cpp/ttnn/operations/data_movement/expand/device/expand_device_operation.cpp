3729b97e2496d246ca23cb9a2291506ffaf3947d
o2buzzle <76864037+o2buzzle@users.noreply.github.com>
2024-10-15 02:31:18 +0000

    #13523: tile "support" for expand

--- ttnn/cpp/ttnn/operations/data_movement/expand/device/expand_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/expand/device/expand_device_operation.cpp
@@ -8,11 +8,18 @@
 
 #include "ttnn/operations/core/compute_kernel/compute_kernel_config.hpp"
 #include "ttnn/tensor/tensor.hpp"
+#include "ttnn/tensor/types.hpp"
 
 namespace ttnn::operations::expand {
 ExpandOperation::program_factory_t ExpandOperation::select_program_factory(
     const operation_attributes_t& operation_attributes, const tensor_args_t& tensor_args) {
-    return Expand2DFactory{};
+    const auto& input = tensor_args.input;
+
+    switch (input.get_layout()) {
+        case Layout::ROW_MAJOR: return ExpandRowMajorFactory{};
+        // case Layout::TILE: return ExpandTileFactory{};
+        default: TT_FATAL(false, "Unsupported input layout");
+    }
 }
 
 void validate(
@@ -25,7 +32,7 @@ void validate(
 
     TT_FATAL(input.get_layout() == Layout::ROW_MAJOR, "Input tensor layout must be ROW_MAJOR");
     if (output.has_value()) {
-        TT_FATAL(output.value().get_layout() == Layout::ROW_MAJOR, "Output tensor layout must be ROW_MAJOR");
+        // TT_FATAL(output.value().get_layout() == Layout::ROW_MAJOR, "Output tensor layout must be ROW_MAJOR");
         TT_FATAL(
             output->get_shape().logical_shape().as_vector() == operation_attributes.output_shape,
             "Output shape must match operation attributes");
@@ -49,37 +56,33 @@ ExpandOperation::shape_return_value_t ExpandOperation::compute_output_shapes(
     const operation_attributes_t& operation_attributes, const tensor_args_t& tensor_args) {
     auto to_return = Shape{operation_attributes.output_shape};
 
-    if (tensor_args.input.get_layout() == Layout::TILE) {
-        return to_return.padded_shape();
-    }
-
     return to_return.logical_shape();
 };
 
 ExpandOperation::tensor_return_value_t ExpandOperation::create_output_tensors(
     const operation_attributes_t& operation_attributes, const tensor_args_t& tensor_args) {
     // Let's just require it to be allocated ahead of time for now
-    if (tensor_args.output.has_value())
+    if (tensor_args.output.has_value()) {
         return {tensor_args.output.value()};
+    }
 
     return create_device_tensor(
         compute_output_shapes(operation_attributes, tensor_args),
         tensor_args.input.get_dtype(),
         tensor_args.input.get_layout(),
         tensor_args.input.device(),
-        operation_attributes.output_mem_config);
+        operation_attributes.memory_config);
 }
 
 std::tuple<ExpandOperation::operation_attributes_t, ExpandOperation::tensor_args_t> ExpandOperation::invoke(
     const Tensor& input,
     const std::vector<uint32_t>& output_shape,
-
     const std::optional<Tensor>& output,
-    const std::optional<MemoryConfig>& output_mem_config,
+    const std::optional<MemoryConfig>& memory_config,
     const std::optional<DeviceComputeKernelConfig>& compute_kernel_config) {
     return {
         {output_shape,
-         output_mem_config.value_or(input.memory_config()),
+         memory_config.value_or(input.memory_config()),
          init_device_compute_kernel_config(input.device()->arch(), compute_kernel_config)},
         {input, output}};
 }
