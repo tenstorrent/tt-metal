9d8bbd0f4768cd340dfe9705c80a6b5de6cc6520
Lewis Panos <lpanos@tenstorrent.com>
2024-10-15 15:49:50 +0000

    Added new weight and bias preparation ops, and new conv op that can only take pre-prepared weights. Working conv test with pre-prepared weights

--- ttnn/cpp/ttnn/operations/conv/conv2d/conv2d_pybind.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/conv2d_pybind.cpp
@@ -20,6 +20,104 @@ void py_bind_conv2d(py::module& module) {
 
     bind_registered_operation(
         module,
+        ttnn::conv2d_host_weights,
+        R"doc(
+        Conv 2D
+        +-------------------+-------------------------------+---------------+-------------+----------+
+        | Argument          | Description                   | Data type     | Valid range | Required |
+        +===================+===============================+===============+=============+==========+
+        | input             | Input activations tensor      | Tensor        |             | Yes      |
+        | in_n              | Input nbatch                  | Tensor        |             | Yes      |
+        | in_h              | Input height                  | Tensor        |             | Yes      |
+        | in_w              | Input width                   | Tensor        |             | Yes      |
+        | kernel_h          | kernel window height          | uint32_t      |             | Yes      |
+        | kernel_w          | kernel window width           | uint32_t      |             | Yes      |
+        | stride_h          | stride in height dim          | uint32_t      |             | No       |
+        | stride_w          | stride in width dim           | uint32_t      |             | No       |
+        | pad_h             | padding in height dim         | uint32_t      |             | No       |
+        | pad_w             | padding in width dim          | uint32_t      |             | No       |
+        | dilation_h        | kernel dilation in height dim | uint32_t      |             | No       |
+        | dilation_w        | kernel dilation in width dim  | uint32_t      |             | No       |
+        | memory_config     | Output memory config          | MemoryConfig  |             | No       |
+        +-------------------+-------------------------------+---------------+-------------+----------+
+        )doc",
+        ttnn::pybind_overload_t{
+            [](const decltype(ttnn::conv2d_host_weights)& self, const ttnn::Tensor& input_tensor,
+                const ttnn::Tensor& weight_tensor,
+                ttnn::Device* device,
+                uint32_t in_channels,
+                uint32_t out_channels,
+                uint32_t batch_size,
+                uint32_t input_height,
+                uint32_t input_width,
+                std::array<uint32_t, 2> kernel_size,
+                std::array<uint32_t, 2> stride,
+                std::array<uint32_t, 2> padding,
+                std::array<uint32_t, 2> dilation,
+                uint32_t groups,
+                std::optional<const ttnn::Tensor> bias_tensor,
+                std::optional<const Conv2dConfig> conv_config,
+                const uint8_t& queue_id) -> std::tuple<ttnn::Tensor, uint32_t, uint32_t, ttnn::Tensor, std::optional<ttnn::Tensor>> {
+                return self(queue_id, input_tensor, weight_tensor, device, in_channels, out_channels, batch_size, input_height, input_width, kernel_size, stride, padding, dilation, groups, bias_tensor, conv_config);
+            },
+            py::kw_only(),
+            py::arg("input_tensor"),
+            py::arg("weight_tensor"),
+            py::arg("device"),
+            py::arg("in_channels"),
+            py::arg("out_channels"),
+            py::arg("batch_size"),
+            py::arg("input_height"),
+            py::arg("input_width"),
+            py::arg("kernel_size"),
+            py::arg("stride"),
+            py::arg("padding"),
+            py::arg("dilation"),
+            py::arg("groups"),
+            py::arg("bias_tensor") = std::nullopt,
+            py::arg("conv_config") = std::nullopt,
+            py::arg("queue_id") = 0},
+
+        ttnn::pybind_overload_t{
+            [](const decltype(ttnn::conv2d_host_weights)& self, const ttnn::Tensor& input_tensor,
+                const ttnn::Tensor& weight_tensor,
+                ttnn::MeshDevice* device,
+                uint32_t in_channels,
+                uint32_t out_channels,
+                uint32_t batch_size,
+                uint32_t input_height,
+                uint32_t input_width,
+                std::array<uint32_t, 2> kernel_size,
+                std::array<uint32_t, 2> stride,
+                std::array<uint32_t, 2> padding,
+                std::array<uint32_t, 2> dilation,
+                uint32_t groups,
+                std::optional<const ttnn::Tensor> bias_tensor,
+                std::optional<const Conv2dConfig> conv_config,
+                const uint8_t& queue_id) -> std::tuple<ttnn::Tensor, uint32_t, uint32_t, ttnn::Tensor, std::optional<ttnn::Tensor>> {
+                return self(queue_id, input_tensor, weight_tensor, device, in_channels, out_channels, batch_size, input_height, input_width, kernel_size, stride, padding, dilation, groups, bias_tensor, conv_config);
+            },
+            py::kw_only(),
+            py::arg("input_tensor"),
+            py::arg("weight_tensor"),
+            py::arg("device"),
+            py::arg("in_channels"),
+            py::arg("out_channels"),
+            py::arg("batch_size"),
+            py::arg("input_height"),
+            py::arg("input_width"),
+            py::arg("kernel_size"),
+            py::arg("stride"),
+            py::arg("padding"),
+            py::arg("dilation"),
+            py::arg("groups"),
+            py::arg("bias_tensor") = std::nullopt,
+            py::arg("conv_config") = std::nullopt,
+            py::arg("queue_id") = 0}
+    );
+
+    bind_registered_operation(
+        module,
         ttnn::conv2d,
         R"doc(
         Conv 2D
@@ -57,9 +155,8 @@ void py_bind_conv2d(py::module& module) {
                 uint32_t groups,
                 std::optional<const ttnn::Tensor> bias_tensor,
                 std::optional<const Conv2dConfig> conv_config,
-                const std::optional<const MemoryConfig> memory_config,
-                const uint8_t& queue_id) -> std::tuple<ttnn::Tensor, uint32_t, uint32_t, ttnn::Tensor, std::optional<ttnn::Tensor>> {
-                return self(queue_id, input_tensor, weight_tensor, device, in_channels, out_channels, batch_size, input_height, input_width, kernel_size, stride, padding, dilation, groups, bias_tensor, conv_config, memory_config);
+                const uint8_t& queue_id) -> ttnn::Tensor {
+                return self(queue_id, input_tensor, weight_tensor, device, in_channels, out_channels, batch_size, input_height, input_width, kernel_size, stride, padding, dilation, groups, bias_tensor, conv_config);
             },
             py::kw_only(),
             py::arg("input_tensor"),
@@ -96,9 +193,8 @@ void py_bind_conv2d(py::module& module) {
                 uint32_t groups,
                 std::optional<const ttnn::Tensor> bias_tensor,
                 std::optional<const Conv2dConfig> conv_config,
-                const std::optional<const MemoryConfig> memory_config,
-                const uint8_t& queue_id) -> std::tuple<ttnn::Tensor, uint32_t, uint32_t, ttnn::Tensor, std::optional<ttnn::Tensor>> {
-                return self(queue_id, input_tensor, weight_tensor, device, in_channels, out_channels, batch_size, input_height, input_width, kernel_size, stride, padding, dilation, groups, bias_tensor, conv_config, memory_config);
+                const uint8_t& queue_id) -> ttnn::Tensor {
+                return self(queue_id, input_tensor, weight_tensor, device, in_channels, out_channels, batch_size, input_height, input_width, kernel_size, stride, padding, dilation, groups, bias_tensor, conv_config);
             },
             py::kw_only(),
             py::arg("input_tensor"),
@@ -119,6 +215,74 @@ void py_bind_conv2d(py::module& module) {
             py::arg("memory_config") = std::nullopt,
             py::arg("queue_id") = 0}
     );
+
+    module.def(
+        "prepare_conv_weights_for_ttnn",
+        [](const ttnn::Tensor& weight_tensor,
+            std::string weights_format,
+            uint32_t in_channels,
+            uint32_t out_channels,
+            uint32_t batch_size,
+            uint32_t input_height,
+            uint32_t input_width,
+            std::array<uint32_t, 2> kernel_size,
+            std::array<uint32_t, 2> stride,
+            std::array<uint32_t, 2> padding,
+            std::array<uint32_t, 2> dilation,
+            uint32_t groups,
+            ttnn::Device *device,
+            std::optional<const Conv2dConfig> conv_config_) -> ttnn::Tensor {
+                return ttnn::operations::conv::conv2d::prepare_conv_weights_for_ttnn(
+                    weight_tensor, weights_format, in_channels, out_channels, batch_size, input_height, input_width, kernel_size, stride, padding, dilation, groups, device, conv_config_);
+        },
+        py::kw_only(),
+        py::arg("weight_tensor"),
+        py::arg("weights_format"),
+        py::arg("in_channels"),
+        py::arg("out_channels"),
+        py::arg("batch_size"),
+        py::arg("input_height"),
+        py::arg("input_width"),
+        py::arg("kernel_size"),
+        py::arg("stride"),
+        py::arg("padding"),
+        py::arg("dilation"),
+        py::arg("groups"),
+        py::arg("device"),
+        py::arg("conv_config") = std::nullopt);
+
+    module.def(
+        "prepare_conv_bias_for_ttnn",
+        [](const ttnn::Tensor& bias_tensor,
+            uint32_t in_channels,
+            uint32_t out_channels,
+            uint32_t batch_size,
+            uint32_t input_height,
+            uint32_t input_width,
+            std::array<uint32_t, 2> kernel_size,
+            std::array<uint32_t, 2> stride,
+            std::array<uint32_t, 2> padding,
+            std::array<uint32_t, 2> dilation,
+            uint32_t groups,
+            ttnn::Device *device,
+            std::optional<const Conv2dConfig> conv_config_) -> ttnn::Tensor {
+                return ttnn::operations::conv::conv2d::prepare_conv_bias_for_ttnn(
+                    bias_tensor, in_channels, out_channels, batch_size, input_height, input_width, kernel_size, stride, padding, dilation, groups, device, conv_config_);
+        },
+        py::kw_only(),
+        py::arg("bias_tensor"),
+        py::arg("in_channels"),
+        py::arg("out_channels"),
+        py::arg("batch_size"),
+        py::arg("input_height"),
+        py::arg("input_width"),
+        py::arg("kernel_size"),
+        py::arg("stride"),
+        py::arg("padding"),
+        py::arg("dilation"),
+        py::arg("groups"),
+        py::arg("device"),
+        py::arg("conv_config") = std::nullopt);
 
     module.def(
         "get_conv_padded_input_shape_and_mem_config",
--- ttnn/ttnn/operations/conv2d.py
+++ ttnn/ttnn/operations/conv2d.py
@@ -33,6 +33,74 @@ def get_conv_output_dim(input, window, stride=1, pad=0, dilation=1):
     return (input + (2 * pad) - dilation * (window - 1) - 1) // stride + 1
 
 
+def prepare_conv_weights_for_ttnn(
+    *,
+    weight_tensor,
+    weights_format,
+    in_channels,
+    out_channels,
+    batch_size,
+    input_height,
+    input_width,
+    kernel_size,
+    stride,
+    padding,
+    dilation,
+    groups,
+    device,
+    conv_config=None,
+):
+    return ttnn._ttnn.operations.conv2d.prepare_conv_weights_for_ttnn(
+        weight_tensor=weight_tensor,
+        weights_format=weights_format,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        batch_size=batch_size,
+        input_height=input_height,
+        input_width=input_width,
+        kernel_size=list(kernel_size),
+        stride=list(stride),
+        padding=list(padding),
+        dilation=list(dilation),
+        groups=groups,
+        device=device,
+        conv_config=conv_config,
+    )
+
+
+def prepare_conv_bias_for_ttnn(
+    *,
+    bias_tensor,
+    in_channels,
+    out_channels,
+    batch_size,
+    input_height,
+    input_width,
+    kernel_size,
+    stride,
+    padding,
+    dilation,
+    groups,
+    device,
+    conv_config=None,
+):
+    return ttnn._ttnn.operations.conv2d.prepare_conv_bias_for_ttnn(
+        bias_tensor=bias_tensor,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        batch_size=batch_size,
+        input_height=input_height,
+        input_width=input_width,
+        kernel_size=list(kernel_size),
+        stride=list(stride),
+        padding=list(padding),
+        dilation=list(dilation),
+        groups=groups,
+        device=device,
+        conv_config=conv_config,
+    )
+
+
 def convert_conv_weight_tensor_to_tiled_layout(conv_weight_tensor, in1_block_h, in1_block_w, output_dtype=None):
     """
     Converts convolution weights to 2d matrix tiled layout on host
@@ -85,6 +153,88 @@ def convert_conv_weight_tensor_to_grouped_layout(conv_weight_tensor, num_groups,
 
 @ttnn.register_python_operation(name="ttnn.conv2d")
 def conv2d(
+    *,
+    input_tensor: ttnn.Tensor,  # may or may not be sharded
+    weight_tensor: ttnn.Tensor,
+    device: ttnn.Device,
+    in_channels: int,
+    out_channels: int,
+    batch_size: int,
+    input_height: int,
+    input_width: int,
+    kernel_size: Union[int, Tuple[int, int]],
+    stride: Union[int, Tuple[int, int]],
+    padding: Union[int, Tuple[int, int]],
+    dilation: Union[int, Tuple[int, int]] = (1, 1),
+    groups: int = 1,
+    bias_tensor: ttnn.Tensor = None,
+    conv_config: Conv2dConfig = None,  # config overrides by user
+    conv_op_cache={},  # basic conv object caching in python needed for intermediate refactoring. Not needed after full op refactoring in C++.
+    debug=False,  # ignored
+) -> Tuple[ttnn.Tensor, int, int, ttnn.Tensor, ttnn.Tensor]:
+    return ttnn._ttnn.operations.conv2d.conv2d_host_weights(
+        input_tensor=input_tensor,
+        weight_tensor=weight_tensor,
+        device=device,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        batch_size=batch_size,
+        input_height=input_height,
+        input_width=input_width,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        dilation=dilation,
+        groups=groups,
+        bias_tensor=bias_tensor,
+        conv_config=conv_config,
+    )
+
+
+@ttnn.register_python_operation(name="ttnn.conv2d_device_weights")
+def conv2d_device_weights(
+    *,
+    input_tensor: ttnn.Tensor,  # may or may not be sharded
+    weight_tensor: ttnn.Tensor,
+    device: ttnn.Device,
+    in_channels: int,
+    out_channels: int,
+    batch_size: int,
+    input_height: int,
+    input_width: int,
+    kernel_size: Union[int, Tuple[int, int]],
+    stride: Union[int, Tuple[int, int]],
+    padding: Union[int, Tuple[int, int]],
+    dilation: Union[int, Tuple[int, int]] = (1, 1),
+    groups: int = 1,
+    bias_tensor: ttnn.Tensor = None,
+    conv_config: Conv2dConfig = None,  # config overrides by user
+    memory_config: ttnn.MemoryConfig = None,  # memory config overrides by user
+    conv_op_cache={},  # basic conv object caching in python needed for intermediate refactoring. Not needed after full op refactoring in C++.
+    debug=False,  # ignored
+) -> Tuple[ttnn.Tensor, int, int, ttnn.Tensor, ttnn.Tensor]:
+    return ttnn._ttnn.operations.conv2d.conv2d_host_weights(
+        input_tensor=input_tensor,
+        weight_tensor=weight_tensor,
+        device=device,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        batch_size=batch_size,
+        input_height=input_height,
+        input_width=input_width,
+        kernel_size=kernel_size,
+        stride=stride,
+        padding=padding,
+        dilation=dilation,
+        groups=groups,
+        bias_tensor=bias_tensor,
+        conv_config=conv_config,
+        memory_config=memory_config,
+    )
+
+
+@ttnn.register_python_operation(name="ttnn.conv2d_device_weights")
+def conv2d_device_weights(
     *,
     input_tensor: ttnn.Tensor,  # may or may not be sharded
     weight_tensor: ttnn.Tensor,
