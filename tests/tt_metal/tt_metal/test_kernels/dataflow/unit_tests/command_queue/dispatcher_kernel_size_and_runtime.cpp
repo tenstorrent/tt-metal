1573e061d337224647b6a89571b03fa65a22f660
Samarth Agarwal <sagarwal@tenstorrent.com>
2024-10-15 20:12:40 +0000

    #0: More work on tests

--- tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/command_queue/dispatcher_kernel_size_and_runtime.cpp
+++ tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/command_queue/dispatcher_kernel_size_and_runtime.cpp
@@ -5,11 +5,14 @@
 #include <cstdint>
 #include "dataflow_api.h"
 #include "c_tensix_core.h"
+#include "debug/dprint.h"
 
 constexpr uint32_t num_unique_rt_args = get_compile_time_arg_val(0);
 constexpr uint32_t num_common_rt_args = get_compile_time_arg_val(1);
 constexpr uint32_t unique_rt_args_vals_offset = get_compile_time_arg_val(2);
 constexpr uint32_t common_rt_args_vals_offset = get_compile_time_arg_val(3);
+constexpr uint32_t num_sems = get_compile_time_arg_val(4);
+constexpr uint32_t expected_sem_val = get_compile_time_arg_val(5);
 
 #if KERNEL_SIZE_BYTES > 16
 constexpr uint32_t empty_kernel_bytes = 16;
@@ -17,13 +20,21 @@ uint8_t data1[KERNEL_SIZE_BYTES - empty_kernel_bytes] __attribute__ ((section ("
 #endif
 
 void kernel_main() {
-    const uint64_t end_time = c_tensix_core::read_wall_clock() + KERNEL_RUNTIME_SECONDS;
+    const uint64_t end_time = c_tensix_core::read_wall_clock() + KERNEL_RUNTIME_CYCLES;
     while (c_tensix_core::read_wall_clock() < end_time);
 
     for (uint32_t i = 0; i < num_unique_rt_args; i++) {
         const uint32_t rt_arg = get_arg_val<uint32_t>(i);
         const uint32_t expected = i + unique_rt_args_vals_offset;
         if (rt_arg != expected) {
+            while(true); // Hang kernel if values aren't correct
+        }
+    }
+
+    for (uint32_t i = num_unique_rt_args; i < num_unique_rt_args + num_sems; i++) {
+        const uint32_t sem_id = get_arg_val<uint32_t>(i);
+        const uint32_t actual_sem_val = *(reinterpret_cast<volatile tt_l1_ptr uint32_t*>(get_semaphore(sem_id)));
+        if (expected_sem_val != actual_sem_val) {
             while(true); // Hang kernel if values aren't correct
         }
     }
