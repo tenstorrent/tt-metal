fc4fbc3ec57735cf8dca00a03036edeb1dee2b93
Bharane Bhaskar <138196316+bharane-ab@users.noreply.github.com>
2024-10-16 16:43:07 +0530

    #8143: update sweep test and doc for neg op (#13103)
    * #8143: update sweep test and doc for neg op
    
    * #8143: Move sweep test
    
    * #8143: Add golden function
    
    ---------
    
    Co-authored-by: mcw-anasuya <mcw-anair@ext.tenstorrent.com>

--- ttnn/cpp/ttnn/operations/eltwise/unary/unary_pybind.hpp
+++ ttnn/cpp/ttnn/operations/eltwise/unary/unary_pybind.hpp
@@ -1421,6 +1421,7 @@ void py_module(py::module& module) {
            |    BFLOAT16, BFLOAT8_B     |          TILE                   |      2, 3, 4      |
            +----------------------------+---------------------------------+-------------------+
         )doc");
+
     detail::bind_unary_operation(module, ttnn::eqz, R"doc(\mathrm{{output\_tensor}}_i = (\mathrm{{input\_tensor_i\ == 0}}))doc");
     detail::bind_unary_operation(module, ttnn::floor, R"doc(\mathrm{{output\_tensor}}_i = floor(\mathrm{{input\_tensor}}_i))doc", "Available for Wormhole_B0 only");
     detail::bind_unary_operation(module, ttnn::ceil, R"doc(\mathrm{{output\_tensor}}_i = ceil(\mathrm{{input\_tensor}}_i))doc", "Available for Wormhole_B0 only");
@@ -1474,7 +1475,16 @@ void py_module(py::module& module) {
            +----------------------------+---------------------------------+-------------------+
         )doc");
 
-    detail::bind_unary_operation(module, ttnn::neg, R"doc(\mathrm{{output\_tensor}}_i = neg(\mathrm{{input\_tensor}}_i))doc");
+    detail::bind_unary_operation(module, ttnn::neg, R"doc(\mathrm{{output\_tensor}}_i = neg(\mathrm{{input\_tensor}}_i))doc",
+        R"doc(Supported dtypes, layouts, and ranks:
+
+           +----------------------------+---------------------------------+-------------------+
+           |     Dtypes                 |         Layouts                 |     Ranks         |
+           +----------------------------+---------------------------------+-------------------+
+           |    BFLOAT16, BFLOAT8_B     |          TILE                   |      2, 3, 4      |
+           +----------------------------+---------------------------------+-------------------+
+        )doc");
+
     detail::bind_unary_operation(module, ttnn::nez, R"doc(\mathrm{{output\_tensor}}_i = (\mathrm{{input\_tensor_i\ != 0}}))doc",
         R"doc(Supported dtypes, layouts, and ranks:
 
