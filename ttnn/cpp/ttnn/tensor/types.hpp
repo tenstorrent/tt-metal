62cae91b0c5e47418fe631b0107923bbf717ee9f
Dongjin Na <128335412+dongjin-na@users.noreply.github.com>
2024-10-17 03:03:43 +0900

    #13867: Fix build failure due to incomplete type in fmt formatter (#13871)
    Co-authored-by: Artem Yerofieiev <169092593+ayerofieiev-tt@users.noreply.github.com>

--- ttnn/cpp/ttnn/tensor/types.hpp
+++ ttnn/cpp/ttnn/tensor/types.hpp
@@ -303,14 +303,7 @@ class LegacyShape {
     }
     friend std::ostream &operator<<(std::ostream &os, const LegacyShape &shape);
 
-    Array4D to_array_4D() const {
-        TT_FATAL(rank() == 4, "to_array_4D is only valid for 4D shapes! Called for {}.", *this);
-        Array4D ret_array;
-        for (int i = 0; i < rank(); i++) {
-            ret_array[i] = this->operator[](i);
-        }
-        return ret_array;
-    }
+    Array4D to_array_4D() const;
 };
 
 inline std::ostream &operator<<(std::ostream &os, const tt::tt_metal::LegacyShape &shape) {
