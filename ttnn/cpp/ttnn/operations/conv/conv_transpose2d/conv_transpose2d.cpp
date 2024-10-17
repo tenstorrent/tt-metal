6f61e58009422bbc307a0187f8bffbb28a2f3813
Sankar Manoj <smanoj@tenstorrent.com>
2024-10-17 09:28:49 +0000

    #0: Moved conv_transpose2d weights transform to ttnn C++

--- ttnn/cpp/ttnn/operations/conv/conv_transpose2d/conv_transpose2d.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv_transpose2d/conv_transpose2d.cpp
@@ -5,7 +5,7 @@
 #include "conv_transpose2d.hpp"
 #include <sys/types.h>
 #include <cstdint>
-
+#include "common/bfloat16.hpp"
 
 using namespace tt;
 namespace ttnn {
@@ -15,6 +15,72 @@ using sliding_window::ParallelConfig;
 
 namespace conv_transpose2d {
 
+template <typename T>
+Tensor _transform_weights_for_conv_transpose2d(
+    const Tensor& conv_weight_tensor) {
+    auto in_w_shape = conv_weight_tensor.get_legacy_shape();
+    auto dtype = conv_weight_tensor.dtype();
+    // in_w_shape = {in_channels, out_channels, kernel_height, kernel_width}
+    // out_w_shape = {out_channels, in_channels, kernel_height, kernel_width}
+    //Flip kernel_height and kernel_width
+    auto compute = [&in_w_shape, &dtype](const auto& input_buffer) {
+        auto in_channels   = in_w_shape[0];
+        auto out_channels  = in_w_shape[1];
+        auto kernel_height = in_w_shape[2];
+        auto kernel_width  = in_w_shape[3];
+        ttnn::SimpleShape output_shape{out_channels, in_channels, kernel_height, kernel_width};
+        auto output_buffer = owned_buffer::create<T>(output_shape.volume());
+
+
+        for(auto in_kernel_height_index = 0; in_kernel_height_index < kernel_height; in_kernel_height_index++)
+        {
+            auto out_buffer_kh_index = kernel_height - in_kernel_height_index - 1;
+            for(auto in_kernel_width_index = 0; in_kernel_width_index < kernel_width; in_kernel_width_index++)
+            {
+                auto out_buffer_kw_index = kernel_width - in_kernel_width_index - 1;
+                for(auto in_channels_index = 0; in_channels_index < in_channels; in_channels_index++)
+                {
+                    for(auto out_channels_index = 0; out_channels_index < out_channels; out_channels_index++)
+                    {
+                        auto in_idx = in_channels_index * kernel_height * kernel_width * out_channels + out_channels_index * kernel_height * kernel_width + in_kernel_height_index * kernel_width + in_kernel_width_index;
+                        auto out_idx = out_channels_index * in_channels * kernel_height * kernel_width + in_channels_index * kernel_height * kernel_width + out_buffer_kh_index * kernel_width + out_buffer_kw_index;
+                        output_buffer[out_idx] = input_buffer[in_idx];
+                    }
+                }
+            }
+        }
+        return Tensor(std::move(OwnedStorage{std::move(output_buffer)}), output_shape, dtype, Layout::ROW_MAJOR);
+    };
+    auto convert_tensor = [&compute](const auto& conv_weight_tensor) {
+        return std::visit(
+            [&compute](auto&& storage) -> Tensor {
+                using StorageType = std::decay_t<decltype(storage)>;
+                if constexpr (std::is_same_v<StorageType, OwnedStorage>) {
+                    return compute(owned_buffer::get_as<T>(storage.buffer));
+                } else if constexpr (std::is_same_v<StorageType, BorrowedStorage>) {
+                    return compute(borrowed_buffer::get_as<T>(storage.buffer));
+                } else {
+                    TT_THROW("Unsupported storage type");
+                }
+            },
+            conv_weight_tensor.get_storage());
+    };
+
+    return is_multi_device_tensor(conv_weight_tensor) ? transform(conv_weight_tensor, convert_tensor) : convert_tensor(conv_weight_tensor);
+}
+
+
+Tensor transform_weights_for_conv_transpose2d(const Tensor& conv_weight_tensor) {
+            switch (conv_weight_tensor.get_dtype()) {
+                case DataType::BFLOAT16:
+                    return _transform_weights_for_conv_transpose2d<::bfloat16>(conv_weight_tensor);
+                case DataType::FLOAT32:
+                    return _transform_weights_for_conv_transpose2d<float>(conv_weight_tensor);
+                case DataType::UINT32:
+                    return _transform_weights_for_conv_transpose2d<uint32_t>(conv_weight_tensor);
+                default: TT_THROW("Unsupported data type for transform_weights_for_conv_transpose2d",conv_weight_tensor.get_dtype());
+            }
+};
 
 template<typename T>
 std::tuple<ttnn::Tensor, uint32_t, uint32_t, ttnn::Tensor, std::optional<ttnn::Tensor>> conv_transpose2d(
@@ -157,8 +223,10 @@ std::tuple<ttnn::Tensor, uint32_t, uint32_t, ttnn::Tensor, std::optional<ttnn::T
         std::optional<ttnn::Tensor> bias_tensor_on_device = bias_tensor;
         if (!weight_is_on_device) {
             // prepare weights in desired layout and move to device
+
+
             tie(weight_tensor_on_device, bias_tensor_on_device) = conv2d::prepare_conv_weights_biases_and_move_to_device(
-                weight_tensor,
+                transform_weights_for_conv_transpose2d(weight_tensor),
                 bias_tensor,
                 conv_config.input_channels_alignment,
                 conv_config.weights_dtype,
