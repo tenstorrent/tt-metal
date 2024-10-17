98dfb61ce728aab1c9b7dd88f897d0d95a07ebeb
Arik  Yaacob <ayaacob@tenstorrent.com>
2024-10-17 12:10:22 -0500

    #13612: Add write barrier wait option into NOTIFY_DISPATCH_S_GO_SIGNAL (#13814)
    * #13612: Add write barrier wait option into NOTIFY_DISPATCH_S_GO_SIGNAL
    
    The dispatcher_d->dispatcher_s go command almost always follows a write barrier (using the wait command). Because the prefetcher is a bottleneck, coalescing the wait into the notify command saves prefetch overhead caused by relaying separate commands.
    This change added a flag to the notify_slave_go command in order to indicate whether a barrier is needed.
    
    * #13612: merge command sequence size calculations per PR review

--- tests/tt_metal/tools/profiler/test_device_profiler.py
+++ tests/tt_metal/tools/profiler/test_device_profiler.py
@@ -166,12 +166,12 @@ def test_dispatch_cores():
     ZONE_COUNT = 37
     REF_COUNT_DICT = {
         "grayskull": {
-            "Tensix CQ Dispatch": 17,
-            "Tensix CQ Prefetch": 25,
+            "Tensix CQ Dispatch": 16,
+            "Tensix CQ Prefetch": 24,
         },
         "wormhole_b0": {
-            "Tensix CQ Dispatch": 17,
-            "Tensix CQ Prefetch": 25,
+            "Tensix CQ Dispatch": 16,
+            "Tensix CQ Prefetch": 24,
         },
     }
 
