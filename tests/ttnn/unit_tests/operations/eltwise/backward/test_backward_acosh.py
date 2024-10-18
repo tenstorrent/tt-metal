aecfccb9554626094f7d0e4ab249d0b23f51a321
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2024-10-18 09:32:09 +0000

    #6583: Fix acosh_bw to handle nan, inf cases

--- tests/ttnn/unit_tests/operations/eltwise/backward/test_backward_acosh.py
+++ tests/ttnn/unit_tests/operations/eltwise/backward/test_backward_acosh.py
@@ -5,7 +5,11 @@
 import torch
 import pytest
 import ttnn
-from tests.ttnn.unit_tests.operations.eltwise.backward.utility_funcs import data_gen_with_range, compare_pcc
+from tests.ttnn.unit_tests.operations.eltwise.backward.utility_funcs import (
+    data_gen_with_range,
+    compare_pcc,
+    compare_results,
+)
 
 
 @pytest.mark.parametrize(
@@ -23,6 +27,35 @@ def test_bw_acosh(input_shapes, device):
     tt_output_tensor_on_device = ttnn.acosh_bw(grad_tensor, input_tensor)
 
     golden_function = ttnn.get_golden_function(ttnn.acosh_bw)
-    golden_tensor = golden_function(grad_data, in_data)
+    golden_tensor = golden_function(grad_data, in_data, device=device)
     comp_pass = compare_pcc(tt_output_tensor_on_device, golden_tensor)
+    assert comp_pass
+
+
+def data_gen_pt_tt(input_shapes, device, required_grad=False, val=1):
+    pt_tensor = (torch.ones(input_shapes, requires_grad=required_grad) * val).bfloat16()
+    tt_tensor = ttnn.Tensor(pt_tensor, ttnn.bfloat16).to(ttnn.TILE_LAYOUT).to(device)
+    return pt_tensor, tt_tensor
+
+
+@pytest.mark.parametrize(
+    "input_shapes",
+    ((torch.Size([1, 1, 32, 32])),),
+)
+def test_bw_acosh_nan_inf(input_shapes, device):
+    in_data, input_tensor = data_gen_pt_tt(input_shapes, device, True, val=0.5)
+    grad_data, grad_tensor = data_gen_pt_tt(input_shapes, device, False, val=1)
+
+    print("input_tensor", input_tensor)
+    print("grad_tensor", grad_tensor)
+
+    tt_output_tensor_on_device = ttnn.acosh_bw(grad_tensor, input_tensor)
+
+    golden_function = ttnn.get_golden_function(ttnn.acosh_bw)
+    golden_tensor = golden_function(grad_data, in_data, device=device)
+
+    comp_pass = compare_results(tt_output_tensor_on_device, golden_tensor)
+
+    print("tt_output_tensor_on_device", tt_output_tensor_on_device)
+    print("golden_tensor", golden_tensor)
     assert comp_pass
