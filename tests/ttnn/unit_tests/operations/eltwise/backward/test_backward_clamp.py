ee8ad8e854c15b4db35bd4f7119c1e5ef3fdab86
Virdhatchani Narayanamoorthy <138196495+VirdhatchaniKN@users.noreply.github.com>
2024-10-18 09:57:21 +0530

    #5147: Update ttnn.clamp_bw (#13850)

--- tests/ttnn/unit_tests/operations/eltwise/backward/test_backward_clamp.py
+++ tests/ttnn/unit_tests/operations/eltwise/backward/test_backward_clamp.py
@@ -16,59 +16,32 @@ from tests.ttnn.unit_tests.operations.eltwise.backward.utility_funcs import data
         (torch.Size([1, 3, 320, 384])),
     ),
 )
-def test_bw_clamp(input_shapes, device):
-    min = -10.0
-    max = 10.0
-    in_data, input_tensor = data_gen_with_range(input_shapes, -100, -1, device, True)
-    grad_data, grad_tensor = data_gen_with_range(input_shapes, -10, -1, device, True)
-
-    tt_output_tensor_on_device = ttnn.clamp_bw(grad_tensor, input_tensor, min=min, max=max)
-
-    golden_function = ttnn.get_golden_function(ttnn.clamp_bw)
-    golden_tensor = golden_function(grad_data, in_data, min, max)
-    comp_pass = compare_pcc(tt_output_tensor_on_device, golden_tensor)
-    assert comp_pass
-
-
 @pytest.mark.parametrize(
-    "input_shapes",
-    (
-        (torch.Size([1, 1, 32, 32])),
-        (torch.Size([1, 1, 320, 384])),
-        (torch.Size([1, 3, 320, 384])),
-    ),
+    "min, max",
+    [
+        (-10.0, 10.0),
+        (10.0, -10.0),
+        (1, -1),
+        (0, 0),
+        (-1.0, None),
+        (None, 1.0),
+        (None, None),
+        (-0.5, None),
+        (None, -0.5),
+        (1.0, 0.0),
+        (0.0, 1.0),
+    ],
 )
-@pytest.mark.parametrize("min", (-1.0, 1.0, 0.0, -0.5, -10.0, 10.0))
-def test_bw_clamp_min(input_shapes, min, device):
+def test_unary_bw_clamp(input_shapes, min, max, device):
     in_data, input_tensor = data_gen_with_range(input_shapes, -100, -1, device, True)
     grad_data, grad_tensor = data_gen_with_range(input_shapes, -10, -1, device, True)
-
-    tt_output_tensor_on_device = ttnn.clamp_bw(grad_tensor, input_tensor, min=min)
-
-    golden_function = ttnn.get_golden_function(ttnn.clamp_bw)
-    golden_tensor = golden_function(grad_data, in_data, min, None)
-
-    comp_pass = compare_pcc(tt_output_tensor_on_device, golden_tensor)
-    assert comp_pass
-
-
-@pytest.mark.parametrize(
-    "input_shapes",
-    (
-        (torch.Size([1, 1, 32, 32])),
-        (torch.Size([1, 1, 320, 384])),
-        (torch.Size([1, 3, 320, 384])),
-    ),
-)
-@pytest.mark.parametrize("max", (1.0, 0.5, 0.0, -1.0, 10.0))
-def test_bw_clamp_max(input_shapes, max, device):
-    in_data, input_tensor = data_gen_with_range(input_shapes, -100, -1, device, True)
-    grad_data, grad_tensor = data_gen_with_range(input_shapes, -10, -1, device, True)
-
-    tt_output_tensor_on_device = ttnn.clamp_bw(grad_tensor, input_tensor, max=max)
-
-    golden_function = ttnn.get_golden_function(ttnn.clamp_bw)
-    golden_tensor = golden_function(grad_data, in_data, None, max)
-
-    comp_pass = compare_pcc(tt_output_tensor_on_device, golden_tensor)
-    assert comp_pass
+    if min is None and max is None:
+        with pytest.raises(RuntimeError, match="Only one of 'min' or 'max' can be None. Please provide one value"):
+            ttnn.clamp_bw(grad_tensor, input_tensor, min=min, max=max)
+        assert True
+    else:
+        tt_output_tensor_on_device = ttnn.clamp_bw(grad_tensor, input_tensor, min=min, max=max)
+        golden_function = ttnn.get_golden_function(ttnn.clamp_bw)
+        golden_tensor = golden_function(grad_data, in_data, min, max)
+        comp_pass = compare_pcc(tt_output_tensor_on_device, golden_tensor)
+        assert comp_pass
