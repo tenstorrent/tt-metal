77ebee17441c4f4fcf0b0d73ca3aa8f58582b6b8
Shwetank Singh <ssingh@tenstorrent.com>
2024-10-19 05:55:55 +0000

    #0: testing

--- ttnn/cpp/ttnn/operations/conv/conv2d/device/kernels/conv_bmm_tilize_col_major_out_blocks.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/device/kernels/conv_bmm_tilize_col_major_out_blocks.cpp
@@ -58,48 +58,45 @@ inline void reblock_and_untilize(
     uint32_t num_tiles_in_row_of_subblocks = mulsi3(out_subblock_num_tiles, num_out_subblocks_in_col);
     cb_wait_front(interm_cb_id, num_tiles_in_row_of_subblocks);
     uint32_t within_block_index = 0;
-    for (uint32_t h = 0; h < out_subblock_h; h++) {
-        uint32_t block_offset = 0;
-        if constexpr(is_non_tile_height_) {
-            for(uint32_t h = 0; h < out_subblock_h; h++){
-                uint32_t block_offset = 0;
-                uint32_t out_sub_block_rows_h = output_rows_h <= 32 ? output_rows_h : 32;
-                cb_reserve_back(out_cb_id, out_sub_block_rows_h);
-                for (uint32_t n = 0; n < num_out_subblocks_in_col; n++) {
-                    tile_regs_acquire();
-                    for (uint32_t w = 0; w < out_subblock_w; w++) {
-                        uint32_t tile_index = block_offset + within_block_index + w;
-                        copy_tile(interm_cb_id, tile_index, w);
-                    }
-                    tile_regs_commit();
-                    tile_regs_wait();
-                    pack_untilize_dst<out_subblock_w, out_block_w>(out_cb_id, 1, n, out_sub_block_rows_h);
-                    tile_regs_release();
-                    block_offset += out_subblock_num_tiles;
+    if constexpr(is_non_tile_height_) {
+        for(uint32_t h = 0; h < out_subblock_h; h++){
+            uint32_t block_offset = 0;
+            uint32_t out_sub_block_rows_h = output_rows_h <= 32 ? output_rows_h : 32;
+            cb_reserve_back(out_cb_id, out_sub_block_rows_h);
+            for (uint32_t n = 0; n < num_out_subblocks_in_col; n++) {
+                tile_regs_acquire();
+                for (uint32_t w = 0; w < out_subblock_w; w++) {
+                    uint32_t tile_index = block_offset + within_block_index + w;
+                    copy_tile(interm_cb_id, tile_index, w);
                 }
-                cb_push_back(out_cb_id, out_sub_block_rows_h);
-                output_rows_h -= out_sub_block_rows_h;
-                within_block_index += out_subblock_w;
+                tile_regs_commit();
+                tile_regs_wait();
+                pack_untilize_dst<out_subblock_w, out_block_w>(out_cb_id, 1, n, out_sub_block_rows_h);
+                tile_regs_release();
+                block_offset += out_subblock_num_tiles;
             }
-        } else {
-            for(uint32_t h = 0; h < out_subblock_h; h++){
-                uint32_t block_offset = 0;
-                cb_reserve_back(out_cb_id, out_block_w);
-                for (uint32_t n = 0; n < num_out_subblocks_in_col; n++) {
-                    tile_regs_acquire();
-                    for (uint32_t w = 0; w < out_subblock_w; w++) {
-                        uint32_t tile_index = block_offset + within_block_index + w;
-                        copy_tile(interm_cb_id, tile_index, w);
-                    }
-                    tile_regs_commit();
-                    tile_regs_wait();
-                    pack_untilize_dst<out_subblock_w, out_block_w>(out_cb_id, 1, n);
-                    tile_regs_release();
-                    block_offset += out_subblock_num_tiles;
+            cb_push_back(out_cb_id, out_sub_block_rows_h);
+            output_rows_h -= out_sub_block_rows_h;
+            within_block_index += out_subblock_w;
+        }
+    } else {
+        for(uint32_t h = 0; h < out_subblock_h; h++){
+            uint32_t block_offset = 0;
+            cb_reserve_back(out_cb_id, out_block_w);
+            for (uint32_t n = 0; n < num_out_subblocks_in_col; n++) {
+                tile_regs_acquire();
+                for (uint32_t w = 0; w < out_subblock_w; w++) {
+                    uint32_t tile_index = block_offset + within_block_index + w;
+                    copy_tile(interm_cb_id, tile_index, w);
                 }
-                cb_push_back(out_cb_id, out_block_w);
-                within_block_index += out_subblock_w;
+                tile_regs_commit();
+                tile_regs_wait();
+                pack_untilize_dst<out_subblock_w, out_block_w>(out_cb_id, 1, n);
+                tile_regs_release();
+                block_offset += out_subblock_num_tiles;
             }
+            cb_push_back(out_cb_id, out_block_w);
+            within_block_index += out_subblock_w;
         }
     }
     cb_pop_front(interm_cb_id, num_tiles_in_row_of_subblocks);
