9a8c94c196fc5f34b3726a239f5bdc3da93532c7
Stanislav Minakov <sminakov@tenstorrent.com>
2024-10-19 11:17:01 +0000

    #0: Buffer fixup

--- tt_metal/impl/buffers/buffer.cpp
+++ tt_metal/impl/buffers/buffer.cpp
@@ -161,16 +161,16 @@ Buffer::Buffer(
     validate_buffer_size_and_page_size(size, page_size, buffer_type, buffer_layout, shard_parameters);
 }
 
-BufferPageMapping generate_buffer_page_mapping(const Buffer &buffer) {
+BufferPageMapping Buffer::generate_buffer_page_mapping_locked() const {
     BufferPageMapping buffer_page_mapping;
 
-    if (buffer.size() == 0) {
+    if (size() == 0) {
         return buffer_page_mapping;
     }
-    auto shard_spec = buffer.shard_spec();
+    auto shard_spec = this->shard_spec_locked();
 
     bool row_major = shard_spec.orientation() == ShardOrientation::ROW_MAJOR;
-    uint32_t num_cores = buffer.num_cores();
+    uint32_t num_cores = this->num_cores_locked();
 
     buffer_page_mapping.all_cores_ = corerange_to_cores(shard_spec.grid(), num_cores, row_major);
     TT_ASSERT(num_cores == buffer_page_mapping.all_cores_.size());
@@ -180,12 +180,12 @@ BufferPageMapping generate_buffer_page_mapping(const Buffer &buffer) {
         core_id++;
     }
 
-    uint32_t num_dev_pages = buffer.num_dev_pages();
+    uint32_t num_dev_pages = this->num_dev_pages_locked();
     auto [core_host_page_indices, shard_shape] = core_to_host_pages(
         num_dev_pages,
         shard_spec.size(),
         num_cores,
-        buffer.buffer_layout(),
+        this->buffer_layout(),
         shard_spec.page_shape,
         shard_spec.shape(),
         shard_spec.tensor2d_shape);
@@ -196,8 +196,8 @@ BufferPageMapping generate_buffer_page_mapping(const Buffer &buffer) {
         std::vector<std::optional<uint32_t>>(num_dev_pages, std::nullopt);
     buffer_page_mapping.dev_page_to_core_mapping_ = std::vector<uint32_t>(num_dev_pages);
 
-    buffer_page_mapping.host_page_to_local_shard_page_mapping_ = std::vector<uint32_t>(buffer.num_pages());
-    buffer_page_mapping.host_page_to_dev_page_mapping_ = std::vector<uint32_t>(buffer.num_pages());
+    buffer_page_mapping.host_page_to_local_shard_page_mapping_ = std::vector<uint32_t>(this->num_pages_locked());
+    buffer_page_mapping.host_page_to_dev_page_mapping_ = std::vector<uint32_t>(this->num_pages_locked());
     buffer_page_mapping.core_shard_shape_ = std::move(shard_shape);
     uint32_t dev_page_index = 0;
 
@@ -225,6 +225,13 @@ BufferPageMapping generate_buffer_page_mapping(const Buffer &buffer) {
     }
 
     return buffer_page_mapping;
+}
+BufferPageMapping Buffer::generate_buffer_page_mapping() const {
+    std::unique_lock lock(config_mutex_);
+    return generate_buffer_page_mapping_locked();
+}
+BufferPageMapping generate_buffer_page_mapping(const Buffer& buffer) {
+    return buffer.generate_buffer_page_mapping();
 }
 
 void Buffer::allocate() {
@@ -288,6 +295,10 @@ void Buffer::set_address(uint64_t addr) {
 
 DeviceAddr Buffer::page_size() const {
     std::unique_lock lock(config_mutex_);
+    return page_size_locked();
+}
+
+DeviceAddr Buffer::page_size_locked() const {
     return page_size_;
 }
 
@@ -298,16 +309,24 @@ void Buffer::set_page_size(DeviceAddr page_size) {
     this->buffer_page_mapping_ = nullptr;
 }
 
+uint32_t Buffer::num_pages_locked() const {
+    return this->size() / this->page_size_locked();
+}
 uint32_t Buffer::num_pages() const {
-    return this->size() / this->page_size();
+    std::unique_lock lock(config_mutex_);
+    return num_pages_locked();
 }
 
-uint32_t Buffer::num_dev_pages() const {
+uint32_t Buffer::num_dev_pages_locked() const {
     if (!is_sharded(this->buffer_layout_)) {
-        return this->num_pages();
+        return this->num_pages_locked();
     }
 
-    return this->shard_spec().size() * this->num_cores();
+    return this->shard_spec_locked().size() * this->num_cores_locked();
+}
+uint32_t Buffer::num_dev_pages() const {
+    std::unique_lock lock(config_mutex_);
+    return num_dev_pages_locked();
 }
 
 CoreType Buffer::core_type() const {
@@ -363,14 +382,16 @@ CoreCoord Buffer::noc_coordinates(uint32_t bank_id) const {
 
 CoreCoord Buffer::noc_coordinates() const { return this->noc_coordinates(0); }
 
-DeviceAddr Buffer::page_address(uint32_t bank_id, uint32_t page_index) const {
-    std::unique_lock lock(config_mutex_);
-
+DeviceAddr Buffer::page_address_locked(uint32_t bank_id, uint32_t page_index) const {
     auto num_banks = this->device_->num_banks(this->buffer_type_);
     TT_ASSERT(bank_id < num_banks, "Invalid Bank ID: {} exceeds total numbers of banks ({})!", bank_id, num_banks);
     int pages_offset_within_bank = (int)page_index / num_banks;
     auto offset = (round_up(this->page_size_, this->alignment()) * pages_offset_within_bank);
     return translate_page_address(offset, bank_id);
+}
+DeviceAddr Buffer::page_address(uint32_t bank_id, uint32_t page_index) const {
+    std::unique_lock lock(config_mutex_);
+    return page_address_locked(bank_id, page_index);
 }
 
 uint32_t Buffer::alignment() const {
@@ -378,25 +399,41 @@ uint32_t Buffer::alignment() const {
 }
 DeviceAddr Buffer::aligned_page_size() const {
     std::unique_lock lock(config_mutex_);
+    return aligned_page_size_locked();
+}
+DeviceAddr Buffer::aligned_page_size_locked() const {
     return align(page_size_, this->alignment());
 }
+DeviceAddr Buffer::aligned_size_locked() const {
+    return this->num_dev_pages_locked() * this->aligned_page_size_locked();
+}
 DeviceAddr Buffer::aligned_size() const {
-    return this->num_dev_pages() * this->aligned_page_size();
+    std::unique_lock lock(config_mutex_);
+    return aligned_size_locked();
 }
 
 DeviceAddr Buffer::sharded_page_address(uint32_t bank_id, uint32_t page_index) const {
     std::unique_lock lock(config_mutex_);
+    return sharded_page_address_locked(bank_id, page_index);
+}
+
+DeviceAddr Buffer::sharded_page_address_locked(uint32_t bank_id, uint32_t page_index) const {
     TT_ASSERT(is_sharded(this->buffer_layout()));
-    int pages_offset_within_bank = page_index % shard_spec().size();
+    TT_ASSERT(shard_parameters_.has_value());
+    int pages_offset_within_bank = page_index % shard_parameters_.value().size();
     auto offset = (round_up(this->page_size_, this->alignment()) * pages_offset_within_bank);
     return translate_page_address(offset, bank_id);
 }
 
-ShardSpecBuffer Buffer::shard_spec() const {
-    std::unique_lock lock(config_mutex_);
+ShardSpecBuffer Buffer::shard_spec_locked() const {
     TT_ASSERT(is_sharded(this->buffer_layout_), "Buffer not sharded");
     TT_ASSERT(shard_parameters_.has_value());
     return this->shard_parameters_.value();
+}
+
+ShardSpecBuffer Buffer::shard_spec() const {
+    std::unique_lock lock(config_mutex_);
+    return shard_spec_locked();
 }
 
 void Buffer::set_shard_spec(const ShardSpecBuffer& shard_spec) {
@@ -405,11 +442,15 @@ void Buffer::set_shard_spec(const ShardSpecBuffer& shard_spec) {
     this->buffer_page_mapping_ = nullptr;
 }
 
-uint32_t Buffer::num_cores() const {
+uint32_t Buffer::num_cores_locked() const {
     if (!is_sharded(this->buffer_layout_))
         return 1;
 
-    return this->shard_spec().tensor_shard_spec.grid.num_cores();
+    return this->shard_spec_locked().tensor_shard_spec.grid.num_cores();
+}
+uint32_t Buffer::num_cores() const {
+    std::unique_lock lock(config_mutex_);
+    return num_cores_locked();
 }
 
 DeviceAddr Buffer::translate_page_address(uint64_t offset, uint32_t bank_id) const {
@@ -417,13 +458,17 @@ DeviceAddr Buffer::translate_page_address(uint64_t offset, uint32_t bank_id) con
     return base_page_address + offset;
 }
 
-const std::shared_ptr<const BufferPageMapping>& Buffer::get_buffer_page_mapping() {
-    std::unique_lock lock(config_mutex_);
+const std::shared_ptr<const BufferPageMapping>& Buffer::get_buffer_page_mapping_locked() {
     TT_ASSERT(is_sharded(this->buffer_layout_), "Buffer not sharded");
     if (!this->buffer_page_mapping_) {
-        this->buffer_page_mapping_ = std::make_shared<const BufferPageMapping>(generate_buffer_page_mapping(*this));
+        this->buffer_page_mapping_ = std::make_shared<const BufferPageMapping>(generate_buffer_page_mapping_locked());
     }
     return this->buffer_page_mapping_;
+}
+
+const std::shared_ptr<const BufferPageMapping>& Buffer::get_buffer_page_mapping() {
+    std::unique_lock lock(config_mutex_);
+    return get_buffer_page_mapping_locked();
 }
 
 bool ShardSpec::operator==(const ShardSpec&) const = default;
