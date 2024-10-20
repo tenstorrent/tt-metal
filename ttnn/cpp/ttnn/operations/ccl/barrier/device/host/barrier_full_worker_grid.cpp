35458a1324b0d6669b6f53fcdac7c939f8e05fef
Juan Camilo Vega <jvega@tenstorrent.com>
2024-10-19 21:44:20 -0400

    #0: license information added to barrier_full_worker_grid.cpp

--- ttnn/cpp/ttnn/operations/ccl/barrier/device/host/barrier_full_worker_grid.cpp
+++ ttnn/cpp/ttnn/operations/ccl/barrier/device/host/barrier_full_worker_grid.cpp
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+///
+
 #include "common/core_coord.h"
 #include "impl/buffers/buffer.hpp"
 #include "ttnn/operation.hpp"
