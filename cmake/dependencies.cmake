36f1453a8738bf636849c07800451e20df5efbfe
Patrick Roberts <proberts@tenstorrent.com>
2024-10-21 16:41:10 +0000

    #14052: Remove C++20 span dependency

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -7,12 +7,11 @@ set(ENV{CPM_SOURCE_CACHE} "${PROJECT_SOURCE_DIR}/.cpmcache")
 
 include(${PROJECT_SOURCE_DIR}/cmake/fetch_boost.cmake)
 
-add_library(span INTERFACE)
-if(CMAKE_CXX_STANDARD LESS 20)
-  fetch_boost_library(core)
-  target_link_libraries(span INTERFACE Boost::core)
-endif()
+fetch_boost_library(core)
 fetch_boost_library(smart_ptr)
+
+add_library(span INTERFACE)
+target_link_libraries(span INTERFACE Boost::core)
 
 ############################################################################################################################
 # yaml-cpp
