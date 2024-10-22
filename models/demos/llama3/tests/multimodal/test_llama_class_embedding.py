#13368: Fix LM head splits calculation

models/demos/llama3/tests/multimodal/test_llama_class_embedding.py
@@ -65,9 +65,7 @@ class ClassEmbedding(nn.Module):
 @pytest.mark.parametrize(
     "input_shape",
     [
-        ((1, 4, 4, 1024, 1280)),
-        ((1, 4, 4, 1024 + 1, 1280)),
-        ((1, 4, 4, 1032, 1280)),
+        ((1, 4, 4, 1024, 1280)),  # Patch 448
     ],
 )
 @pytest.mark.parametrize(

models/demos/llama3/tt/multimodal/llama_cross_attention_transformer_text.py
@@ -96,7 +96,7 @@ class TtLlamaCrossAttentionTransformerText(LightweightModule):
 
         lm_head_torch = self.state_dict[f"{state_dict_prefix}output.weight"].transpose(-1, -2)
         total_splits = 8  # Arbitrary value which allows whole-tile splits in LM Head
-        num_splits = total_splits // self.mesh_device.num_devices
+        num_splits = total_splits // self.configuration.num_devices
         lm_head_torch = torch.chunk(lm_head_torch, num_splits, dim=-1)
 
         cache_name = lambda name, suffix, split: weight_cache_path / (state_dict_prefix + f"{name}{suffix}{split}")