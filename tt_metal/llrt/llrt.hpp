518b06c59198c61615518a78f40269916f1e5f9d
blozano-tt <blozano@tenstorrent.com>
2024-10-22 23:28:36 +0000

    Remove includes from llrt.hpp

--- tt_metal/llrt/llrt.hpp
+++ tt_metal/llrt/llrt.hpp
@@ -3,21 +3,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #pragma once
-#include <algorithm>
-#include <filesystem>
-#include <functional>
-#include <iostream>
-#include <random>
-#include <tuple>
+
+#include <cstdint>
+#include <vector>
 
 // clang-format off
 #include "llrt/tt_cluster.hpp"
-#include "tensix.h"
 #include "tt_metal/third_party/umd/device/device_api_metal.h"
 #include "tt_metal/third_party/umd/device/tt_xy_pair.h"
 #include "llrt_common/tiles.hpp"
 #include "llrt/tt_memory.h"
-#include "hostdevcommon/common_runtime_address_map.h"
 #include "jit_build/build.hpp"
 #include "dev_msgs.h"
 // clang-format on
