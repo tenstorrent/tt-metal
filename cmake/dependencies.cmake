0be480e4450b902174f8c1f03559a8fc6eebb827
Andrew Fuller <afuller@tenstorrent.com>
2024-10-23 10:58:58 -0400

    Format CMake files using gersemi (#14117)
    * #14001: Format CMake files
    
    * #14001: Add stub for CPMAddPackage for better formatting

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,4 +1,3 @@
-
 set(ENV{CPM_SOURCE_CACHE} "${PROJECT_SOURCE_DIR}/.cpmcache")
 
 ############################################################################################################################
@@ -18,18 +17,23 @@ target_link_libraries(span INTERFACE Boost::core)
 ############################################################################################################################
 
 CPMAddPackage(
-  NAME yaml-cpp
-  GITHUB_REPOSITORY jbeder/yaml-cpp
-  GIT_TAG 0.8.0
-  OPTIONS
-    "YAML_CPP_BUILD_TESTS OFF"
-    "YAML_CPP_BUILD_TOOLS OFF"
-    "YAML_BUILD_SHARED_LIBS OFF"
+    NAME yaml-cpp
+    GITHUB_REPOSITORY jbeder/yaml-cpp
+    GIT_TAG 0.8.0
+    OPTIONS
+        "YAML_CPP_BUILD_TESTS OFF"
+        "YAML_CPP_BUILD_TOOLS OFF"
+        "YAML_BUILD_SHARED_LIBS OFF"
 )
 
-if (yaml-cpp_ADDED)
+if(yaml-cpp_ADDED)
     target_link_libraries(yaml-cpp PRIVATE stdlib)
-    set_target_properties(yaml-cpp PROPERTIES DEBUG_POSTFIX "")
+    set_target_properties(
+        yaml-cpp
+        PROPERTIES
+            DEBUG_POSTFIX
+                ""
+    )
 endif()
 
 ############################################################################################################################
@@ -37,14 +41,15 @@ endif()
 ############################################################################################################################
 
 CPMAddPackage(
-  NAME googletest
-  GITHUB_REPOSITORY google/googletest
-  GIT_TAG v1.13.0
-  VERSION 1.13.0
-  OPTIONS "INSTALL_GTEST OFF"
+    NAME googletest
+    GITHUB_REPOSITORY google/googletest
+    GIT_TAG v1.13.0
+    VERSION 1.13.0
+    OPTIONS
+        "INSTALL_GTEST OFF"
 )
 
-if (googletest_ADDED)
+if(googletest_ADDED)
     target_compile_options(gtest PRIVATE -Wno-implicit-int-float-conversion)
     target_link_libraries(gtest PRIVATE stdlib)
     target_link_libraries(gtest_main PRIVATE stdlib)
@@ -54,28 +59,16 @@ endif()
 # boost-ext reflect : https://github.com/boost-ext/reflect
 ############################################################################################################################
 
-CPMAddPackage(
-  NAME reflect
-  GITHUB_REPOSITORY boost-ext/reflect
-  GIT_TAG v1.1.1
-)
+CPMAddPackage(NAME reflect GITHUB_REPOSITORY boost-ext/reflect GIT_TAG v1.1.1)
 
 ############################################################################################################################
 # magic_enum : https://github.com/Neargye/magic_enum
 ############################################################################################################################
 
-CPMAddPackage(
-  NAME magic_enum
-  GITHUB_REPOSITORY Neargye/magic_enum
-  GIT_TAG v0.9.6
-)
+CPMAddPackage(NAME magic_enum GITHUB_REPOSITORY Neargye/magic_enum GIT_TAG v0.9.6)
 
 ############################################################################################################################
 # fmt : https://github.com/fmtlib/fmt
 ############################################################################################################################
 
-CPMAddPackage(
-  NAME fmt
-  GITHUB_REPOSITORY fmtlib/fmt
-  GIT_TAG 11.0.1
-)
+CPMAddPackage(NAME fmt GITHUB_REPOSITORY fmtlib/fmt GIT_TAG 11.0.1)
