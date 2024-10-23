629d0a2bf85956477bb502b3c6ff0c9c569fd2ab
yugi957 <akrishnadas@tenstorrent.com>
2024-10-23 21:11:18 +0000

    #13837: added sweeps and one_tests for fill and full. full 100%, fill 71%

--- tests/sweep_framework/sweeps/data_movement/full/full_one_test.py
+++ tests/sweep_framework/sweeps/data_movement/full/full_one_test.py
@@ -27,17 +27,11 @@ def run(
     # Create a tensor filled with `fill_value` using torch.full
     torch_tensor = torch.full(shape, fill_value, dtype=torch.float32)
 
-    # Create a random tensor in PyTorch of shape `shape` and dtype float32
-    rand_tensor = torch_random(shape, -256, 256, dtype=torch.float32)
-
-    # Convert the torch tensor to the `ttnn` format
-    ttnn_tensor = ttnn.from_torch(rand_tensor, device=device, layout=layout, dtype=dtype)
-
     # Measure performance of the full operation in `ttnn`
     start_time = start_measuring_time()
 
     # Apply the `ttnn.full` operation
-    ttnn_filled_tensor = ttnn.fill(fill_value, any=ttnn_tensor, memory_config=None)
+    ttnn_filled_tensor = ttnn.full(shape, fill_value, device, memory_config=None)
 
     e2e_perf = stop_measuring_time(start_time)
 
@@ -53,7 +47,7 @@ def run(
 if __name__ == "__main__":
     # Run the test
 
-    fill_specs = {"shape": [128, 128, 300, 300], "fill_value": 64}
+    fill_specs = {"shape": [23 * 7, 34 * 17], "fill_value": 64}
 
     dtype = ttnn.bfloat16
     # layout = ttnn.TILE_LAYOUT
