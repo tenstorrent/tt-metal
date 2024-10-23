9225168c7973c039eaea389be80459a82a40406b
Almeet Bhullar <abhullar@tenstorrent.com>
2024-10-23 02:54:54 +0000

    #10973: Load and launch FW on both BH eth core riscs

--- tt_metal/impl/device/device.cpp
+++ tt_metal/impl/device/device.cpp
@@ -279,8 +279,7 @@ void Device::initialize_build() {
     uint32_t dispatch_message_addr =
         dispatch_constants::get(dispatch_core_type, this->num_hw_cqs_).get_device_command_queue_addr(CommandQueueDeviceAddrType::DISPATCH_MESSAGE);
 
-    // TODO now: total number of processor types should be pulled from the HAL
-    uint32_t num_build_states = this->arch() == tt::ARCH::GRAYSKULL ? 5 : 7;
+    uint32_t num_build_states = hal.get_num_risc_processors();
 
     auto init_helper = [this, dispatch_message_addr, num_build_states] (bool is_fw) -> JitBuildStateSet {
         std::vector<std::shared_ptr<JitBuildState>> build_states;
@@ -320,12 +319,12 @@ void Device::initialize_build() {
                         }
                         case HalProgrammableCoreType::ACTIVE_ETH: {
                             build_states[index] = std::make_shared<JitBuildActiveEthernet>(
-                                this->build_env_, JitBuiltStateConfig{.processor_id = processor_type, .is_fw=is_fw, .dispatch_message_addr=dispatch_message_addr});
+                                this->build_env_, JitBuiltStateConfig{.processor_id = processor_class, .is_fw=is_fw, .dispatch_message_addr=dispatch_message_addr});
                             break;
                         }
                         case HalProgrammableCoreType::IDLE_ETH: {
                             build_states[index] = std::make_shared<JitBuildIdleEthernet>(
-                                this->build_env_, JitBuiltStateConfig{.processor_id = processor_type, .is_fw=is_fw, .dispatch_message_addr=dispatch_message_addr});
+                                this->build_env_, JitBuiltStateConfig{.processor_id = processor_class, .is_fw=is_fw, .dispatch_message_addr=dispatch_message_addr});
                             break;
                         }
                         default:
