8ff3a70e956b0eaf8373ada0d1393a50e470b7ec
Shaw Nguyen <nguyenvanthanhtung2001@gmail.com>
2024-10-23 04:04:47 +0000

    #14087: Use float value, modulize unit test moreh_bmm (forward, backward)

--- tests/ttnn/unit_tests/operations/test_moreh_bmm.py
+++ tests/ttnn/unit_tests/operations/test_moreh_bmm.py
@@ -8,7 +8,6 @@ import torch.nn.functional as F
 from loguru import logger
 
 import ttnn
-from tests.ttnn.unit_tests.operations.test_moreh_matmul import get_tensors
 from models.utility_functions import comp_allclose_and_pcc
 
 from tests.ttnn.unit_tests.operations.test_utils import (
@@ -16,6 +15,53 @@ from tests.ttnn.unit_tests.operations.test_utils import (
     compute_kernel_options,
     compute_kernel_ids,
 )
+
+
+def create_ttnn_tilized_tensor(torch_tensor, device, dtype):
+    return ttnn.from_torch(torch_tensor, device=device, dtype=dtype, layout=ttnn.TILE_LAYOUT)
+
+
+def get_tensors(
+    input_shape,
+    mat2_shape,
+    output_shape,
+    require_input_grad,
+    require_mat2_grad,
+    device,
+):
+    torch_dtype = torch.float32
+    ttnn_dtype = ttnn.bfloat16
+
+    torch_input = torch.rand(input_shape, dtype=torch_dtype)
+    torch_mat2 = torch.rand(mat2_shape, dtype=torch_dtype)
+    torch_output = torch.rand(output_shape, dtype=torch_dtype)
+
+    ttnn_input = create_ttnn_tilized_tensor(torch_input, device, ttnn_dtype)
+    ttnn_mat2 = create_ttnn_tilized_tensor(torch_mat2, device, ttnn_dtype)
+    ttnn_output = create_ttnn_tilized_tensor(torch_output, device, ttnn_dtype)
+
+    ttnn_output_grad = torch_output_grad = ttnn_input_grad = ttnn_mat2_grad = None
+    if require_input_grad or require_mat2_grad:
+        torch_output_grad = torch.rand(output_shape, dtype=torch_dtype)
+        ttnn_output_grad = create_ttnn_tilized_tensor(torch_output_grad, device, ttnn_dtype)
+        if require_input_grad:
+            torch_input_grad = torch.full(input_shape, float("nan"), dtype=torch_dtype)
+            ttnn_input_grad = create_ttnn_tilized_tensor(torch_input_grad, device, ttnn_dtype)
+        if require_mat2_grad:
+            torch_mat2_grad = torch.full(mat2_shape, float("nan"), dtype=torch_dtype)
+            ttnn_mat2_grad = create_ttnn_tilized_tensor(torch_mat2_grad, device, ttnn_dtype)
+
+    return (
+        ttnn_input,
+        ttnn_mat2,
+        ttnn_output,
+        ttnn_output_grad,
+        ttnn_input_grad,
+        ttnn_mat2_grad,
+        torch_input,
+        torch_mat2,
+        torch_output_grad,
+    )
 
 
 def run_moreh_bmm(shape, optional_output, compute_kernel_options, device):
@@ -41,7 +87,7 @@ def run_moreh_bmm(shape, optional_output, compute_kernel_options, device):
         input,
         mat2,
         _,
-    ) = get_tensors(input_shape, mat2_shape, output_shape, False, False, False, device)
+    ) = get_tensors(input_shape, mat2_shape, output_shape, False, False, device)
     compute_kernel_config = get_compute_kernel_options(compute_kernel_options)
 
     # Perform PyTorch BMM
@@ -57,7 +103,9 @@ def run_moreh_bmm(shape, optional_output, compute_kernel_options, device):
     actual_output = ttnn.to_torch(ttnn_output)
 
     # Compare results for equivalence
-    passing, output_pcc = comp_allclose_and_pcc(output, actual_output, pcc=0.999)
+    rtol = atol = 0.1
+    pcc = 0.998 if compute_kernel_options else 0.97
+    passing, output_pcc = comp_allclose_and_pcc(output, actual_output, pcc=pcc, rtol=rtol, atol=atol)
     logger.debug(f"Out passing={passing}")
     logger.debug(f"Output pcc={output_pcc}")
     assert passing
@@ -87,7 +135,7 @@ def run_moreh_bmm_backward(shape, requires_grad, compute_kernel_options, device)
         input,
         mat2,
         output_grad,
-    ) = get_tensors(input_shape, mat2_shape, output_shape, require_input_grad, require_mat2_grad, False, device)
+    ) = get_tensors(input_shape, mat2_shape, output_shape, require_input_grad, require_mat2_grad, device)
     compute_kernel_config = get_compute_kernel_options(compute_kernel_options)
 
     # Perform PyTorch BMM (backward)
@@ -107,16 +155,17 @@ def run_moreh_bmm_backward(shape, requires_grad, compute_kernel_options, device)
 
     # Compare results for equivalence
     rtol = atol = 0.1
+    pcc = 0.998 if compute_kernel_options else 0.97
     if require_input_grad:
         actual_input_grad = ttnn.to_torch(ttnn_input_grad)
-        passing, output_pcc = comp_allclose_and_pcc(input.grad, actual_input_grad, pcc=0.999, rtol=rtol, atol=atol)
+        passing, output_pcc = comp_allclose_and_pcc(input.grad, actual_input_grad, pcc=pcc, rtol=rtol, atol=atol)
         logger.debug(f"input_grad passing={passing}")
         logger.debug(f"input_grad pcc={output_pcc}")
         assert passing
 
     if require_mat2_grad:
         actual_mat2_grad = ttnn.to_torch(ttnn_mat2_grad)
-        passing, output_pcc = comp_allclose_and_pcc(mat2.grad, actual_mat2_grad, pcc=0.999, rtol=rtol, atol=atol)
+        passing, output_pcc = comp_allclose_and_pcc(mat2.grad, actual_mat2_grad, pcc=pcc, rtol=rtol, atol=atol)
         logger.debug(f"mat2_grad passing={passing}")
         logger.debug(f"mat2_grad pcc={output_pcc}")
         assert passing
@@ -127,26 +176,41 @@ def run_moreh_bmm_backward(shape, requires_grad, compute_kernel_options, device)
     [
         [1, 31, 639, 31],
         [5, 95, 415, 65],
+        [10, 77, 320, 320],
         [10, 191, 447, 159],
     ],
 )
-@pytest.mark.parametrize("optional_output", [False, True])
-@pytest.mark.parametrize("compute_kernel_options", compute_kernel_options, ids=compute_kernel_ids)
-def test_moreh_bmm(shape, optional_output, compute_kernel_options, device):
+def test_moreh_bmm_shape(shape, device):
     """
     PyTest wrapper for running BMM tests with multiple configurations.
     """
     torch.manual_seed(2024)
-    run_moreh_bmm(shape, optional_output, compute_kernel_options, device)
+    run_moreh_bmm(shape, True, True, device)
+
+
+@pytest.mark.parametrize("optional_output", [False, True])
+def test_moreh_bmm_optional_output(optional_output, device):
+    """
+    PyTest wrapper for running BMM tests with multiple configurations.
+    """
+    torch.manual_seed(2024)
+    run_moreh_bmm([10, 191, 447, 159], optional_output, True, device)
+
+
+@pytest.mark.parametrize("compute_kernel_options", compute_kernel_options, ids=compute_kernel_ids)
+def test_moreh_bmm_compute_kernel_options(compute_kernel_options, device):
+    """
+    PyTest wrapper for running BMM tests with multiple configurations.
+    """
+    torch.manual_seed(2024)
+    run_moreh_bmm([10, 191, 447, 159], True, compute_kernel_options, device)
 
 
 @pytest.mark.parametrize(
     "shape",
     [[10, 191, 447, 159]],
 )
-@pytest.mark.parametrize("optional_output", [False, True])
-@pytest.mark.parametrize("compute_kernel_options", compute_kernel_options, ids=compute_kernel_ids)
-def test_moreh_bmm_callback(shape, optional_output, compute_kernel_options, device, use_program_cache):
+def test_moreh_bmm_callback(shape, device, use_program_cache):
     """
     PyTest wrapper for running BMM tests with multiple configurations.
     AssertionError: If the number of program cache entries differs between runs with the same settings.
@@ -154,9 +218,9 @@ def test_moreh_bmm_callback(shape, optional_output, compute_kernel_options, devi
     torch.manual_seed(2024)
     num_program_cache_entries_list = []
     for i in range(2):
-        run_moreh_bmm(shape, optional_output, compute_kernel_options, device)
+        run_moreh_bmm(shape, True, True, device)
         torch_dummy = torch.randn([32, 32])
-        tt_dummy = ttnn.from_torch(torch_dummy, device=device)
+        ttnn_dummy = ttnn.from_torch(torch_dummy, device=device)
         num_program_cache_entries_list.append(device.num_program_cache_entries())
     logger.info(f"num_program_cache_entries_list={num_program_cache_entries_list}")
     assert num_program_cache_entries_list[0] > 0
@@ -168,30 +232,18 @@ def test_moreh_bmm_callback(shape, optional_output, compute_kernel_options, devi
     [
         [1, 32, 32, 32],
         [3, 31, 31, 31],
+        [10, 77, 320, 320],
         [7, 511, 313, 765],
     ],
 )
-@pytest.mark.parametrize(
-    "requires_grad",
-    [
-        [True, False],
-        [False, True],
-        [True, True],
-    ],
-)
-@pytest.mark.parametrize("compute_kernel_options", compute_kernel_options, ids=compute_kernel_ids)
-def test_moreh_bmm_backward(shape, requires_grad, compute_kernel_options, device):
+def test_moreh_bmm_backward_shape(shape, device):
     """
     PyTest wrapper for running BMM backward tests with multiple configurations.
     """
     torch.manual_seed(2024)
-    run_moreh_bmm_backward(shape, requires_grad, compute_kernel_options, device)
+    run_moreh_bmm_backward(shape, [True, True], True, device)
 
 
-@pytest.mark.parametrize(
-    "shape",
-    [[7, 511, 313, 765]],
-)
 @pytest.mark.parametrize(
     "requires_grad",
     [
@@ -200,8 +252,32 @@ def test_moreh_bmm_backward(shape, requires_grad, compute_kernel_options, device
         [True, True],
     ],
 )
+def test_moreh_bmm_backward_requires_grad(requires_grad, device):
+    """
+    PyTest wrapper for running BMM backward tests with multiple configurations.
+    """
+    torch.manual_seed(2024)
+    run_moreh_bmm_backward([7, 511, 313, 765], requires_grad, True, device)
+
+
 @pytest.mark.parametrize("compute_kernel_options", compute_kernel_options, ids=compute_kernel_ids)
-def test_moreh_bmm_backward_callback(shape, requires_grad, compute_kernel_options, device, use_program_cache):
+def test_moreh_bmm_backward_compute_kernel_options(compute_kernel_options, device):
+    """
+    PyTest wrapper for running BMM backward tests with multiple configurations.
+    """
+    torch.manual_seed(2024)
+    run_moreh_bmm_backward([7, 511, 313, 765], [True, True], compute_kernel_options, device)
+
+
+@pytest.mark.parametrize(
+    "requires_grad",
+    [
+        [True, False],
+        [False, True],
+        [True, True],
+    ],
+)
+def test_moreh_bmm_backward_callback(requires_grad, device, use_program_cache):
     """
     PyTest wrapper for running BMM backward tests with multiple configurations.
     AssertionError: If the number of program cache entries differs between runs with the same settings.
@@ -209,9 +285,9 @@ def test_moreh_bmm_backward_callback(shape, requires_grad, compute_kernel_option
     torch.manual_seed(2024)
     num_program_cache_entries_list = []
     for i in range(2):
-        run_moreh_bmm_backward(shape, requires_grad, compute_kernel_options, device)
+        run_moreh_bmm_backward([7, 511, 313, 765], requires_grad, True, device)
         torch_dummy = torch.randn([32, 32])
-        tt_dummy = ttnn.from_torch(torch_dummy, device=device)
+        ttnn_dummy = ttnn.from_torch(torch_dummy, device=device)
         num_program_cache_entries_list.append(device.num_program_cache_entries())
     logger.info(f"num_program_cache_entries_list={num_program_cache_entries_list}")
     assert num_program_cache_entries_list[0] > 0
