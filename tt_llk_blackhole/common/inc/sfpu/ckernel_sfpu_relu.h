f28aec4c011851ff6ce2622167408d195e180045
Paytin Gardner <pgardner@tenstorrent.com>
2024-10-24 13:58:11 -0400

    Code cleanup leaky relu (#46)
    * Code cleanup leaky relu
    
    * add include for converter

--- tt_llk_blackhole/common/inc/sfpu/ckernel_sfpu_relu.h
+++ tt_llk_blackhole/common/inc/sfpu/ckernel_sfpu_relu.h
@@ -6,6 +6,7 @@
 
 #include "ckernel_defs.h"
 #include "ckernel.h"
+#include "ckernel_sfpu_converter.h"
 
 #include "sfpi.h"
 
@@ -16,11 +17,10 @@ namespace ckernel
 namespace sfpu
 {
 
-template <bool APPROXIMATION_MODE, int ITERATIONS>
+template <bool APPROXIMATION_MODE>
 inline void _calculate_lrelu_(const int iterations, uint slope)
 {
-    // SFPU microcode
-    vFloat s = s2vFloat16b(slope);
+    vFloat s = Converter::to_float(slope);
 
     #pragma GCC unroll 0
     for (int d = 0; d < iterations; d++) {
