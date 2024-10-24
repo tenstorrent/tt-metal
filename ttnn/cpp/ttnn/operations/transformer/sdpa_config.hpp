fc906d6e45ef15cd000158777aff79958e886e9d
Jack Cai <caixunshiren@gmail.com>
2024-10-24 08:31:59 -0700

    #14103: improved multi-core parallelization in flash decode

--- ttnn/cpp/ttnn/operations/transformer/sdpa_config.hpp
+++ ttnn/cpp/ttnn/operations/transformer/sdpa_config.hpp
@@ -14,6 +14,7 @@ struct SDPAProgramConfig {
     std::size_t q_chunk_size;
     std::size_t k_chunk_size;
     std::optional<bool> exp_approx_mode;
+    uint32_t max_cores_per_head_batch = 16;
 };
 
 }  // namespace ttnn::operations::transformer
