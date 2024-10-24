50717788ba8ac31ab7d636cad8b503887182bb51
Eyon Land <41128502+eyonland@users.noreply.github.com>
2024-10-24 16:50:36 -0500

    #11206: Define specification for metal api 1.0 (#13759)
    * #11206: Define specification for metal api 1.0
    
    * #11206: Moving host api directory to tt_metal/include/tt_metal

---
+++ tt_metal/include/tt_metal/metal.hpp
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+#include "types.hpp"
+#include "buffer.hpp"
+#include "command_queue.hpp"
+#include "device.hpp"
+#include "event.hpp"
+#include "kernel.hpp"
+#include "program.hpp"
+#include "trace.hpp"
