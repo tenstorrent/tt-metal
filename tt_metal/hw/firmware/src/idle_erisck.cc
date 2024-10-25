e966f7768c9db0ed45cd8733831622b303f3e2c3
Austin Ho <109362939+tt-aho@users.noreply.github.com>
2024-10-25 01:34:08 -0400

    Revert Kernel Ring Buffer Changes (#14244)
    * Revert "#11790: Use ring buffer for kernel dispatch"
    
    This reverts commit e8e25d17d6b3d22df89994879c2c42aef5a27e6b.
    
    * Revert "#11790: Fix alignment of launch_msg members"
    
    This reverts commit 0ba602f3ab7751d2aab66d580e48c50a8e4ec6d2.
    
    * Revert "#11791: Elf xipification pieces"
    
    This reverts commit a97ee9a6e9024f791d4e9004f4155540a932b791.

--- tt_metal/hw/firmware/src/idle_erisck.cc
+++ tt_metal/hw/firmware/src/idle_erisck.cc
@@ -22,12 +22,10 @@
 #include <kernel_includes.hpp>
 
 extern uint32_t __kernel_init_local_l1_base[];
-extern uint32_t __fw_export_end_text[];
 
-void kernel_launch(uint32_t kernel_base_addr) {
+void kernel_launch() {
     DeviceZoneScopedMainChildN("ERISC-KERNEL");
-
-    firmware_kernel_common_init((void tt_l1_ptr *)(kernel_base_addr + (uint32_t) __kernel_init_local_l1_base - (uint32_t)__fw_export_end_text));
+    firmware_kernel_common_init((void tt_l1_ptr *)__kernel_init_local_l1_base);
 
     noc_local_state_init(NOC_INDEX);
 
