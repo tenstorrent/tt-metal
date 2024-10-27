f4ba61a75bb6c7bb0f736d145704d3fd478e646e
Bryan Wilder Field Lozano <blozano@tenstorrent.com>
2024-10-26 19:39:13 -0700

    Update buffer.cpp
    Just sort things a bit better

--- tt_metal/impl/buffers/buffer.cpp
+++ tt_metal/impl/buffers/buffer.cpp
@@ -10,13 +10,13 @@
 #include "tt_metal/impl/allocator/allocator.hpp"
 #include "tt_metal/impl/device/device.hpp"
 
-#include <mutex>                                  // for unique_lock
 #include <algorithm>                              // for min
+#include <mutex>                                  // for unique_lock
 #include <string>                                 // for basic_string
 #include <utility>                                // for move
-#include "tt_metal/impl/buffers/buffer_constants.hpp"
 #include "tt_metal/common/base.hpp"               // for align
-#include "device/tt_soc_descriptor.h"             // for CoreType
+#include "tt_metal/impl/buffers/buffer_constants.hpp"
+#include "third_party/umd/device/tt_soc_descriptor.h" // for CoreType
 #include "fmt/base.h"                             // for format_string
 #include "tt_stl/reflection.hpp"                  // for from_json
 
