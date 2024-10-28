ad61399224ea969909886b4fd8964bb8ecafd15e
Nikola Cvetkovic <ncvetkovic@tenstorrent.com>
2024-10-28 19:32:36 +0100

    #0: SFPU LLK Unit Test Parameter Sweep (#14218)
    #0: SFPU UT param sweep:
    - in_range [-7.0f, 7.0f]
    - fp_32_dest_accum_en
    - shape - from 1x1x1x1 up to 1x1x16x16
    - num_tiles - from 1 uo to 256
    - data_formats - Bfp4_b, Bfp8_b, Float16_b, Fp32

--- tests/tt_metal/tt_metal/unit_tests/compute/test_golden_impls.hpp
+++ tests/tt_metal/tt_metal/unit_tests/compute/test_golden_impls.hpp
@@ -9,6 +9,15 @@
 #include <limits>
 #include <random>
 #include <vector>
+#include <algorithm>
+
+#include "common/test_tiles.hpp"
+#include "tt_metal/common/bfloat16.hpp"
+#include "tt_metal/common/bfloat8.hpp"
+#include "tt_metal/common/bfloat4.hpp"
+#include "tt_metal/host_api.hpp"
+#include "tt_metal/detail/tt_metal.hpp"
+#include "tests/tt_metal/test_utils/packing.hpp"
 
 //TODO: RT these functions should be templated for different data formats
 namespace unit_tests::compute {
@@ -53,5 +62,32 @@ std::vector<uint16_t> gold_reduce_hw(const std::vector<uint16_t> &src_vec, const
 // returns tilized result of eltwise addition
 // Assumes all elements in bfloat16
 std::vector<uint32_t> gold_standard_tilize_w_elwadd(const std::vector<uint32_t> &src0_vec, const std::vector<uint32_t> &src1_vec, const GoldenConfig &config);
+
+// Random packed uint32_t vector generator which is data-format agnostic.
+// Takes the following parameters:
+//
+// lower - a lower limit of the input range
+// upper - an upper limit of the input range
+// num_bytes - number of bytes that the vector will occupy
+// data_format - data format of each element, packed to uint32_t, currently supporting Bfloat16, Float32, Bfp8_b and Bfp4_b
+// seed - randomization seed
+// exclude_zeroes - if true, excludes values around zero, with the limits given by next two parameters
+// golden_neg_epsilon - small negative value above which no elements of the vector will take value from
+// golden_pos_epsilon - small positive value below which no elements of the vector will take value from
+//
+// Returns:
+//
+// a uint32_t vector of packed values depending on the data format and given limits
+std::vector<uint32_t> generate_random_vector_generalized(const float lower, const float upper, const size_t num_bytes, const tt::DataFormat data_format, const int seed, bool exclude_zeroes = false, float golden_neg_epsilon = -0.0001f, float golden_pos_epsilon = 0.0001f);
+
+// Unpacking function which is data-format agnostic
+// Takes the following parameters:
+//
+// data_format -  data format in which the vector was packed, currently supporting Bfloat16, Float32, Bfp8_b and Bfp4_b
+// packed_input - a uint32_t packed vector
+//
+// Returns:
+// a float vector of unpacked values depending on the data format
+std::vector<float> unpack_generalized(const tt::DataFormat data_format, const std::vector<uint32_t>& packed_input);
 
 }   // unit_tests::compute
