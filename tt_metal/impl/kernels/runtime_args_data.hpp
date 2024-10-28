c9eba351c9c31f75d8f7787f33b7889f6ff692ca
blozano-tt <blozano@tenstorrent.com>
2024-10-28 19:48:30 +0000

    Use cassert for debug help

--- tt_metal/impl/kernels/runtime_args_data.hpp
+++ tt_metal/impl/kernels/runtime_args_data.hpp
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
 #include <iostream>
@@ -20,45 +21,60 @@ struct RuntimeArgsData {
     std::uint32_t * rt_args_data;
     std::size_t rt_args_count;
 
-    inline bool in_bounds(std::size_t index) const {
+    inline bool in_bounds(std::size_t index) const noexcept {
         if(index >= rt_args_count) [[unlikely]] {
             std::cerr << "TT_FATAL: Index " << index << " is larger than runtime args size "
                       << rt_args_count << " at " << __FILE__ << ":" << __LINE__ << std::endl;
+            return false;
+        }
+        return true;
+    }
+
+    inline bool in_bounds_or_throw(std::size_t index) const {
+        if(!in_bounds(index) [[unlikely]] {
             throw std::out_of_range(
                 "Index " + std::to_string(index) + " is larger than runtime args size " + std::to_string(rt_args_count)
             );
-            __builtin_unreachable();
         }
         return true;
     }
 
     inline std::uint32_t & operator[](std::size_t index) noexcept {
+        assert(in_bounds(index));
         return this->rt_args_data[index];
     }
+
     inline const std::uint32_t& operator[](std::size_t index) const noexcept {
+        assert(in_bounds(index));
         return this->rt_args_data[index];
     }
+
     inline std::uint32_t & at(std::size_t index) {
-        if (in_bounds(index)) [[likely]] {
+        if (in_bounds_or_throw(index)) [[likely]] {
             return this->rt_args_data[index];
         }
         __builtin_unreachable();
     }
+
     inline const std::uint32_t& at(std::size_t index) const {
-        if (in_bounds(index)) [[likely]] {
+        if (in_bounds_or_throw(index)) [[likely]] {
             return this->rt_args_data[index];
         }
         __builtin_unreachable();
     }
+
     inline std::uint32_t * data() noexcept {
         return rt_args_data;
     }
+
     inline const std::uint32_t * data() const noexcept {
         return rt_args_data;
     }
+
     inline std::size_t size() const noexcept{
         return rt_args_count;
     }
+
 };
 
 }
