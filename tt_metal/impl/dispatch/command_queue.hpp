618821f1ceba31021a141ba1abde730b199dc07e
Bryan Wilder Field Lozano <blozano@tenstorrent.com>
2024-10-28 08:52:05 -0700

    #0: Cleanup public vs private header files for buffer.hpp (#14327)
    * #0: Cleanup public vs private header files for buffer.hpp

--- tt_metal/impl/dispatch/command_queue.hpp
+++ tt_metal/impl/dispatch/command_queue.hpp
@@ -474,11 +474,6 @@ using CompletionReaderVariant = std::variant<std::monostate, ReadBufferDescripto
 using CompletionReaderQueue = LockFreeQueue<CompletionReaderVariant>;
 }  // namespace detail
 
-struct AllocBufferMetadata {
-    Buffer* buffer;
-    std::reference_wrapper<Allocator> allocator;
-};
-
 struct RuntimeArgsMetadata {
     CoreCoord core_coord;
     std::shared_ptr<RuntimeArgs> runtime_args_ptr;
@@ -586,7 +581,6 @@ struct CommandInterface {
     std::optional<bool> blocking;
     std::optional<std::variant<std::reference_wrapper<Buffer>, std::shared_ptr<Buffer>>> buffer;
     Program* program;
-    std::optional<AllocBufferMetadata> alloc_md;
     std::optional<RuntimeArgsMetadata> runtime_args_md;
     std::optional<const Buffer*> shadow_buffer;
     std::optional<HostDataType> src;
