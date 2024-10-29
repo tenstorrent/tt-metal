9ee4c90181f605cb6ce902c2a26a6eacfa38af5d
Bryan Wilder Field Lozano <blozano@tenstorrent.com>
2024-10-29 13:35:42 -0700

    #14367: Inline operator[] Again for RuntimeArgsData (#14384)

--- tt_metal/impl/kernels/runtime_args_data.hpp
+++ tt_metal/impl/kernels/runtime_args_data.hpp
@@ -4,8 +4,13 @@
 
 #pragma once
 
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <utility>
 
 namespace tt::tt_metal {
 // RuntimeArgsData provides an indirection to the runtime args
@@ -16,19 +21,55 @@ struct RuntimeArgsData {
     std::uint32_t * rt_args_data;
     std::size_t rt_args_count;
 
-    std::uint32_t & operator[](std::size_t index);
+    inline bool in_bounds(std::size_t index) const noexcept {
+        if(index >= rt_args_count) {
+            std::cerr << "TT_FATAL: Index " << index << " is larger than runtime args size "
+                      << rt_args_count << " at " << __FILE__ << ":" << __LINE__ << std::endl;
+            return false;
+        }
+        return true;
+    }
 
-    const std::uint32_t& operator[](std::size_t index) const;
+    inline std::uint32_t & operator[](std::size_t index) noexcept {
+        assert(in_bounds(index));
+        return this->rt_args_data[index];
+    }
 
-    std::uint32_t & at(std::size_t index);
+    inline const std::uint32_t& operator[](std::size_t index) const noexcept {
+        assert(in_bounds(index));
+        return this->rt_args_data[index];
+    }
 
-    const std::uint32_t& at(std::size_t index) const;
+    inline std::uint32_t & at(std::size_t index) {
+        if (!in_bounds(index)) {
+            throw std::out_of_range(
+                "Index " + std::to_string(index) + " is larger than runtime args size " + std::to_string(rt_args_count)
+            );
+        }
+        return this->rt_args_data[index];
+    }
 
-    std::uint32_t * data() noexcept;
+    inline const std::uint32_t& at(std::size_t index) const {
+        if (!in_bounds(index)) {
+            throw std::out_of_range(
+                "Index " + std::to_string(index) + " is larger than runtime args size " + std::to_string(rt_args_count)
+            );
+        }
+        return this->rt_args_data[index];
+    }
 
-    const std::uint32_t * data() const noexcept;
+    inline std::uint32_t * data() noexcept {
+        return rt_args_data;
+    }
 
-    std::size_t size() const noexcept;
+    inline const std::uint32_t * data() const noexcept {
+        return rt_args_data;
+    }
+
+    inline std::size_t size() const noexcept{
+        return rt_args_count;
+    }
+
 };
 
 }
