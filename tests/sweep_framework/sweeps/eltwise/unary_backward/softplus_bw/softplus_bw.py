3d426731846266d7ac69990a3c32bf77e6b0bd16
amalbasaTT <amalbasa@tenstorrent.com>
2024-10-29 13:44:17 +0000

    #11512: Move softplus_bw to xfail

--- tests/sweep_framework/sweeps/eltwise/unary_backward/softplus_bw/softplus_bw.py
+++ tests/sweep_framework/sweeps/eltwise/unary_backward/softplus_bw/softplus_bw.py
@@ -26,7 +26,7 @@ random.seed(0)
 # Each suite has a key name (in this case "suite_1" and "suite_2") which will associate the test vectors to this specific suite of inputs.
 # Developers can create their own generator functions and pass them to the parameters as inputs.
 parameters = {
-    "nightly": {
+    "xfail": {
         "input_shape": gen_shapes([1, 1, 1, 1], [6, 12, 256, 256], [1, 1, 1, 1], 8)
         + gen_shapes([1, 1, 1], [12, 256, 256], [1, 1, 1], 8)
         + gen_shapes([1, 1], [256, 256], [1, 1], 8),
