fd9124b0c204ccaeed790d053f2f10131ea954c6
Samarth Agarwal <sagarwal@tenstorrent.com>
2024-10-29 21:35:44 +0000

    #0: Initial commit

--- tests/tt_metal/tt_metal/unit_tests_fast_dispatch/command_queue/test_EnqueueTrace.cpp
+++ tests/tt_metal/tt_metal/unit_tests_fast_dispatch/command_queue/test_EnqueueTrace.cpp
@@ -2,8 +2,11 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+#include <array>
+#include <cmath>
 #include <cstdint>
 #include <memory>
+#include <vector>
 
 #include "command_queue_fixture.hpp"
 #include "detail/tt_metal.hpp"
@@ -367,3 +370,212 @@ TEST_F(SingleDeviceTraceFixture, EnqueueMultiProgramTraceBenchmark) {
 }
 
 } // end namespace basic_tests
+
+TEST_F(RandomProgramTraceFixture, TensixTestSimpleProgramsTrace) {
+    // warmup iterations
+    for (uint32_t i = 0; i < 5; i++) {
+        if (i % 10 == 0) {
+            log_info(tt::LogTest, "Creating Program {}", i);
+        }
+        Program program = CreateProgram();
+        this->create_kernel(program, CoreType::WORKER, true);
+        EnqueueProgram(device_->command_queue(), program, false);
+    }
+
+    Program programs[NUM_PROGRAMS];
+    for (uint32_t i = 0; i < NUM_PROGRAMS; i++) {
+        if (i % 10 == 0) {
+            log_info(tt::LogTest, "Creating Program {}", i);
+        }
+        programs[i] = CreateProgram();
+        Program& program = programs[i];
+        this->create_kernel(program, CoreType::WORKER, true);
+    }
+
+    uint32_t trace_id = BeginTraceCapture(this->device_, this->device_->command_queue().id());
+    // for (Program& program : programs) {
+    //     EnqueueProgram(device_->command_queue(), program, false);
+    // }
+    EnqueueProgram(device_->command_queue(), programs[0], false);
+    EndTraceCapture(this->device_, this->device_->command_queue().id(), trace_id);
+
+    EnqueueTrace(this->device_->command_queue(), trace_id, false);
+
+    Finish(this->device_->command_queue());
+    ReleaseTrace(this->device_, trace_id);
+}
+
+// TEST_F(RandomProgramTraceFixture, TestSimpleProgramsOnEth) {
+//     if (!local_test_functions::does_device_have_active_eth_cores(device_)) {
+//         GTEST_SKIP() << "Skipping test because device " << device_->id() << "does not have any active ethernet cores";
+//     }
+
+//     for (uint32_t i = 0; i < NUM_PROGRAMS; i++) {
+//         if (i % 10 == 0) {
+//             log_info(tt::LogTest, "Creating Program {}", i);
+//         }
+//         Program program = CreateProgram();
+//         this->create_kernel(program, CoreType::ETH, true);
+//         EnqueueProgram(device_->command_queue(), program, false);
+//     }
+
+//     Finish(device_->command_queue());
+// }
+
+// TEST_F(RandomProgramTraceFixture, TestSimpleProgramsOnTensixAndEth) {
+//     if (!local_test_functions::does_device_have_active_eth_cores(device_)) {
+//         GTEST_SKIP() << "Skipping test because device " << device_->id() << "does not have any active ethernet cores";
+//     }
+
+//     for (uint32_t i = 0; i < NUM_PROGRAMS; i++) {
+//         if (i % 10 == 0) {
+//             log_info(tt::LogTest, "Creating Program {}", i);
+//         }
+//         Program program = CreateProgram();
+
+//         bool eth_kernel_added_to_program = false;
+//         if (rand() % 2 == 0) {
+//             this->create_kernel(program, CoreType::ETH, true);
+//             eth_kernel_added_to_program = true;
+//         }
+//         if (rand() % 2 == 0 || !eth_kernel_added_to_program) {
+//             this->create_kernel(program, CoreType::WORKER, true);
+//         }
+
+//         EnqueueProgram(device_->command_queue(), program, false);
+//     }
+
+//     Finish(device_->command_queue());
+// }
+
+// TEST_F(RandomProgramTraceFixture, TestProgramsOnTensix) {
+//     for (uint32_t i = 0; i < NUM_PROGRAMS; i++) {
+//         if (i % 10 == 0) {
+//             log_info(tt::LogTest, "Creating Program {}", i);
+//         }
+//         Program program = CreateProgram();
+//         this->create_kernel(program, CoreType::WORKER);
+//         EnqueueProgram(device_->command_queue(), program, false);
+//     }
+
+//     Finish(device_->command_queue());
+// }
+
+// TEST_F(RandomProgramTraceFixture, TestProgramsOnEth) {
+//     if (!local_test_functions::does_device_have_active_eth_cores(device_)) {
+//         GTEST_SKIP() << "Skipping test because device " << device_->id() << "does not have any active ethernet cores";
+//     }
+
+//     for (uint32_t i = 0; i < NUM_PROGRAMS; i++) {
+//         if (i % 10 == 0) {
+//             log_info(tt::LogTest, "Creating Program {}", i);
+//         }
+//         Program program = CreateProgram();
+//         // Large eth kernels currently don't fit in the ring buffer, so we're reducing the max number of RTAs
+//         // and the max kernel size to ensure that the kernel can fit in the ring buffer
+//         KernelProperties kernel_properties;
+//         kernel_properties.max_kernel_size_bytes = MAX_KERNEL_SIZE_BYTES / 2;
+//         kernel_properties.max_num_rt_args = MAX_NUM_RUNTIME_ARGS / 4;
+//         this->create_kernel(program, CoreType::ETH, false, kernel_properties);
+//         EnqueueProgram(device_->command_queue(), program, false);
+//     }
+
+//     Finish(device_->command_queue());
+// }
+
+// TEST_F(RandomProgramTraceFixture, TestProgramsOnTensixAndEth) {
+//     if (!local_test_functions::does_device_have_active_eth_cores(device_)) {
+//         GTEST_SKIP() << "Skipping test because device " << device_->id() << "does not have any active ethernet cores";
+//     }
+
+//     for (uint32_t i = 0; i < NUM_PROGRAMS; i++) {
+//         if (i % 10 == 0) {
+//             log_info(tt::LogTest, "Creating Program {}", i);
+//         }
+//         Program program = CreateProgram();
+
+//         bool eth_kernel_added_to_program = false;
+//         if (rand() % 2 == 0) {
+//             // Large eth kernels currently don't fit in the ring buffer, so we're reducing the max number of RTAs
+//             // and the max kernel size to ensure that the kernel can fit in the ring buffer
+//             KernelProperties kernel_properties;
+//             kernel_properties.max_kernel_size_bytes = MAX_KERNEL_SIZE_BYTES / 2;
+//             kernel_properties.max_num_rt_args = MAX_NUM_RUNTIME_ARGS / 4;
+//             kernel_properties.max_num_sems = MAX_NUM_SEMS / 2;
+//             this->create_kernel(program, CoreType::ETH, false, kernel_properties);
+//             eth_kernel_added_to_program = true;
+//         }
+//         if (rand() % 2 == 0 || !eth_kernel_added_to_program) {
+//             KernelProperties kernel_properties;
+//             kernel_properties.max_num_sems = MAX_NUM_SEMS / 2;
+//             this->create_kernel(program, CoreType::WORKER, false, kernel_properties);
+//         }
+
+//         EnqueueProgram(device_->command_queue(), program, false);
+//     }
+
+//     Finish(device_->command_queue());
+// }
+
+// TEST_F(RandomProgramTraceFixture, TestAlternatingLargeAndSmallProgramsOnTensix) {
+//     for (uint32_t i = 0; i < NUM_PROGRAMS; i++) {
+//         if (i % 10 == 0) {
+//             log_info(tt::LogTest, "Creating Program {}", i);
+//         }
+//         Program program = CreateProgram();
+
+//         KernelProperties kernel_properties;
+//         if (i % 2 == 0) {
+//             kernel_properties = this->get_large_kernel_properties();
+//         } else {
+//             kernel_properties = this->get_small_kernel_properties();
+//         }
+
+//         this->create_kernel(program, CoreType::WORKER, false, kernel_properties);
+//         EnqueueProgram(device_->command_queue(), program, false);
+//     }
+
+//     Finish(device_->command_queue());
+// }
+
+// TEST_F(RandomProgramTraceFixture, TestLargeProgramFollowedBySmallProgramsOnTensix) {
+//     for (uint32_t i = 0; i < NUM_PROGRAMS; i++) {
+//         if (i % 10 == 0) {
+//             log_info(tt::LogTest, "Creating Program {}", i);
+//         }
+//         Program program = CreateProgram();
+
+//         KernelProperties kernel_properties;
+//         if (i == 0) {
+//             kernel_properties = this->get_large_kernel_properties();
+//         } else {
+//             kernel_properties = this->get_small_kernel_properties();
+//         }
+
+//         this->create_kernel(program, CoreType::WORKER, false, kernel_properties);
+//         EnqueueProgram(device_->command_queue(), program, false);
+//     }
+
+//     Finish(device_->command_queue());
+// }
+
+// TEST_F(RandomProgramTraceFixture, TestLargeProgramInBetweenFiveSmallProgramsOnTensix) {
+//     for (uint32_t i = 0; i < NUM_PROGRAMS; i++) {
+//         if (i % 10 == 0) {
+//             log_info(tt::LogTest, "Creating Program {}", i);
+//         }
+//         Program program = CreateProgram();
+
+//         KernelProperties kernel_properties;
+//         if (i % 6 == 0) {
+//             kernel_properties = this->get_large_kernel_properties();
+//         } else {
+//             kernel_properties = this->get_small_kernel_properties();
+//         }
+
+//         this->create_kernel(program, CoreType::WORKER, false, kernel_properties);
+//         EnqueueProgram(device_->command_queue(), program, false);
+//     }
+
+//     Finish(device_->command_queue());
+// }
