9e42f8c01a47317c90ae882c0eaf605461a3b074
Artem Yerofieiev <artem@tenstorrent.com>
2024-10-29 22:46:04 +0000

    #0: replace TL comment

--- ttnn/cpp/ttnn/tensor/layout/tensor_layout.hpp
+++ ttnn/cpp/ttnn/tensor/layout/tensor_layout.hpp
@@ -17,38 +17,9 @@ namespace tt::tt_metal {
 
 using Strides = std::vector<size_t>;
 
-// Alignment is a physical row alignment for each dimension of the tensor (except innermost dimension - there its a column alignment).
-// Example:
-//  Given Tensor with shape (2, 3, 10, 16) and Alignment (A:768, B:300, C:32, 32) we will physically allocate
-//     W = round_up(16, 32) = 32
-//     H_R1 = round_up(10, C:32) = 32
-//     H_R2 = round_up(R1:32 * 3, B:300) = round_up(96, 300) = 300
-//     H_R3 = = round_up(R2:300 * 2, A:768) = round_up(600, 768) = 768
-//     PHY = [H:768, W:32]
-//
-// LegacyPaddedShape is a padding shape for each dimension of the tensor.
-// All tensor calculations (like physical_size or strides) are done using Alignment, so internally we always convert LegacyPaddedShape to Alignment.
-// Example:
-//   Given tensor with shape (2, 3, 10, 16) and LegacyPaddedShape (2, 4, 32, 32).
-//   PHY_LEGACY = [2*4*32, 32] = [256, 32]
-//   Lets convert LegacyPaddedShape to Alignment:
-//   Alignment = (2*4*32, 4*32, 32, 32) = (A:256, B:128, C:32, 32)
-//   W = round_up(16, 32) = 32
-//   H_R1 = round_up(10, C:32) = 32
-//   H_R2 = round_up(R1:32 * 3, B:128) = round_up(96, 128) = 128
-//   H_R3 = = round_up(R2:128 * 2, A:256) = round_up(256, 256) = 256
-//   PHY_ALIGNMENT = [H:256, W:32]
-//   As you can see, PHY_LEGACY and PHY_ALIGNMENT are the same.
-//
-// Warning:
-//   Alignment can encode strides of a higher granularity than LegacyPaddedShape.
-//   It means Alignment can represnet whatever value of LegacyPaddedShape can, but not the other way around.
-//   Most of the codebase today uses LegacyPaddedShape and get_padded_shape()
-//   As we migrate to Alignment we can't use get_padded_shape anymore, because it can't represent aligned shapes.
-//   So get_padded_shape() must be replaced with get_physical_size() and get_strides()
-//
-// Note:
-//   This class is a work in progress. Many of its public methods have to be moved to private or even pImpl.
+// TensorLayout describes how a tensor is laid out in memory
+// It takes datatype, layout (eg. TILE vs. RM), memory (eg. DRAM vs. L1), sharding (ie. how you want to cut your logical shape)
+// And provides information required to physically lay out the tensor in memory
 class TensorLayout {
 public:
     TensorLayout(DataType dtype, const PageConfig& page_config, const MemoryConfig& memory_config);
