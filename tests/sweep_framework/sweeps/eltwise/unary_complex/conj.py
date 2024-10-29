77115be7fea7fb7f50959bc41260ca33a9410cd3
Nemanja Grujic <109360083+nemanjagrujic@users.noreply.github.com>
2024-10-29 12:01:54 +0100

    #11512: sub_bw, fill_zero_bw, log_sigmoid_bw (#13805)
    * #11512: Add sweeps for sub_bw, fill_zero_bw, log_sigmoid_bw
    * #11512: Add sweeps for ldexp_bw, logaddexp_bw, hardshrink_bw, logit_bw, neg_bw, softhhrink_bw
    * #11512: Add sweep for addcmul_bw
    * #11512: Add sweeps for complex conj, complex reciprocal and complex reciprocal_bw

---
+++ tests/sweep_framework/sweeps/eltwise/unary_complex/conj.py
@@ -0,0 +1,95 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import Optional, Tuple
+from functools import partial
+
+import torch
+import random
+import ttnn
+from tests.sweep_framework.sweep_utils.utils import gen_shapes
+from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_func_with_cast_tt
+
+from tests.ttnn.utils_for_testing import check_with_pcc, start_measuring_time, stop_measuring_time
+from models.utility_functions import torch_random
+
+# Override the default timeout in seconds for hang detection.
+TIMEOUT = 30
+
+random.seed(0)
+
+# Parameters provided to the test vector generator are defined here.
+# They are defined as dict-type suites that contain the arguments to the run function as keys, and lists of possible inputs as values.
+# Each suite has a key name (in this case "suite_1") which will associate the test vectors to this specific suite of inputs.
+# Developers can create their own generator functions and pass them to the parameters as inputs.
+parameters = {
+    "nightly": {
+        "input_shape": gen_shapes([1, 1, 1, 1], [6, 12, 256, 256], [1, 1, 1, 1], 16)
+        + gen_shapes([1, 1, 1], [12, 256, 256], [1, 1, 1], 16)
+        + gen_shapes([1, 1], [256, 256], [1, 1], 16),
+        "input_a_dtype": [ttnn.bfloat16, ttnn.bfloat8_b],
+        "input_a_layout": [ttnn.TILE_LAYOUT],
+        "input_a_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
+        "output_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
+    },
+}
+
+
+# This is the run instructions for the test, defined by the developer.
+# The run function must take the above-defined parameters as inputs.
+# The runner will call this run function with each test vector, and the returned results from this function will be stored.
+# If you defined a device_mesh_fixture above, the object you yielded will be passed into this function as 'device'. Otherwise, it will be the default ttnn device opened by the infra.
+def run(
+    input_shape,
+    input_a_dtype,
+    input_a_layout,
+    input_a_memory_config,
+    output_memory_config,
+    *,
+    device,
+) -> list:
+    data_seed = random.randint(0, 20000000)
+    torch.manual_seed(data_seed)
+
+    torch_input_tensor_ar = gen_func_with_cast_tt(
+        partial(torch_random, low=0.01, high=100, dtype=torch.float32), input_a_dtype
+    )(input_shape)
+
+    torch_input_tensor_ac = gen_func_with_cast_tt(
+        partial(torch_random, low=0.01, high=100, dtype=torch.float32), input_a_dtype
+    )(input_shape)
+
+    torch_input_tensor_a = torch.complex(
+        torch_input_tensor_ar.to(torch.float32), torch_input_tensor_ac.to(torch.float32)
+    )
+    torch_output_tensor = torch.conj(torch_input_tensor_a).resolve_conj()
+
+    input_tensor_ar = ttnn.from_torch(
+        torch_input_tensor_ar,
+        dtype=input_a_dtype,
+        layout=input_a_layout,
+        device=device,
+        memory_config=input_a_memory_config,
+    )
+
+    input_tensor_ac = ttnn.from_torch(
+        torch_input_tensor_ac,
+        dtype=input_a_dtype,
+        layout=input_a_layout,
+        device=device,
+        memory_config=input_a_memory_config,
+    )
+    input_tensor_a = ttnn.complex_tensor(input_tensor_ar, input_tensor_ac)
+
+    start_time = start_measuring_time()
+    result = ttnn.conj(input_tensor_a, memory_config=output_memory_config)
+    output_tensor = torch.complex(
+        ttnn.to_torch(result.real).to(torch.float32), ttnn.to_torch(result.imag).to(torch.float32)
+    )
+    output_tensor = output_tensor.resolve_conj()
+    e2e_perf = stop_measuring_time(start_time)
+
+    pcc = check_with_pcc(torch.view_as_real(torch_output_tensor), torch.view_as_real(output_tensor), 0.99)
+    # print(f"pcc {pcc} input_a_dtype {input_a_dtype}")
+    return [pcc, e2e_perf]
