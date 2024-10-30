35f3f586a88c2e8a7d576ed1f9e0f52fcbaea879
Juan Camilo Vega <jvega@tenstorrent.com>
2024-10-30 17:43:15 -0400

    #12516: Implementing a Barrier CCL OP (#14036)
    * #12516 Created non-device connection for barrier implementation
    
    * #12516: Made ping latency uBenchmark into OP
    
    * #0: Bug fix on hop latencies uBenchmark
    
    Runtime arguments were flipped between semaphore and Buffer
    
    * #0: Update Programming Mesh of Devices with TT-NN.md
    
    * #12516: Added barrier op, has ethernet semaphore issue
    
    Adding the barrier operation but it fails due to the fact ethernet semaphore does not initialize correctly
    
    * #12516: First working barrier
    Need to gauge performance and effectiveness
    
    * #12516 Created non-device connection for barrier implementation
    
    * #12516: Made ping latency uBenchmark into OP
    
    * #0: Bug fix on hop latencies uBenchmark
    
    Runtime arguments were flipped between semaphore and Buffer
    
    * #12516: Added barrier op, has ethernet semaphore issue
    
    Adding the barrier operation but it fails due to the fact ethernet semaphore does not initialize correctly
    
    * #0: Update Programming Mesh of Devices with TT-NN.md
    
    * #12516: First working barrier
    Need to gauge performance and effectiveness
    
    * #12516: Added nightly and post commit testing to the barrier op
    
    * #12516: Moving barrier testing from nightly to t3000 frequent
    
    * #0: Correction to Bug fix on hop latencies uBenchmark commit
    
    Also rotating the semaphore in the sender, not just in the receiver
    
    * #0: Adding license information to barrier_sem_creator.cpp
    
    * #0: license information added to barrier_full_worker_grid.cpp
    
    * #0: Fixing licensing bug
    
    * #0: Making buffer region a multiple of 32
    
    * #12516: Modifying dataflow_api.h for performance
    
    Creating the send_eth_receiver_channnel_done for when the eth_channel_sync_t object is already known
    Will spin loop a bit before calling run_routing() for semaphore waits to prevent perf loss from over-calling run_routing()
    Formatting changes for comments
    
    * #12516: Applying PR recommendations
    
    * #12516: Removing now depricated test_barrier.py
    
    * #12516: Adding test validation for all gather in sharded test
    
    * #12516: Moving from launch_op to invoke
    
    * #12516: Other suggested changes
    
    * #12516: Reusing pre-made handshake API
    
    * #12516: Moving as much as possible pre-processing from launch_op to invoke
    
    * #12516: Removed redundant send_eth_receiver_channel_done which is now part of the api
    
    * #12516: Added pyskip for n300
    
    * #0: Removing blank space in C make lists
    
    * #12516: Improvements to barrier op and dataflow_api.h
    
    Created an eth_wait_for_bytes_on_channel_sync_addr function for when the channel buffer is not one of the defaults in erisc_info->channels
    Added wait_for_eth_txq_cmd_space to properly wait for the txq function to finish rather than using an empty while loop
    Moved the barrier frequent test to the ccl folder
    Improvements on the barrier op using new aforementioned functions
    
    * #12516: Updated run_t3000_frequent_tests.sh with new location of ccl tests
    
    * #12516: Move rt_args from std::vector to std::array
    
    * #12516: changing get_t3k_physical_devices_ids_ring to open_mesh_device as recommended
    
    * #12516: Applying api recommendations
    
    Using ttnn.synchronize_devices instead of synchronize_device
    
    Using from_torch to create the tensor mesh in one step
    
    * #12516: Minor documentation error on barrier OP
    
    * #12516: removing unused libraries in barrier.hpp
    
    * #12516: Removing barrier_op.hpp inclusion in barrier.hpp
    
    * #12516: Changing function name from barrier to barrier_function
    
    * #12516: Correcting clang-diagnostics-error
    
    * #12516: Other clang errors fix

---
+++ ttnn/cpp/ttnn/operations/ccl/barrier/device/barrier_op.hpp
@@ -0,0 +1,53 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "ttnn/run_operation.hpp"
+#include "ttnn/operations/ccl/ccl_common.hpp"
+#include "ttnn/operations/ccl/ccl_host_datastructures.hpp"
+#include "ttnn/operations/eltwise/binary/binary.hpp"
+namespace ttnn {
+
+struct Barrier {
+    const MemoryConfig output_mem_config;
+    const ttnn::ccl::Topology topology;
+    std::vector<Device*> devices;
+    bool is_starting_core=false;
+    uint32_t ring_size=devices.size();
+    uint32_t ring_index=0;
+    std::optional<chip_id_t> receiver_device_id=0;
+    std::optional<chip_id_t> sender_device_id=0;
+
+    //Required functions to all tensor op functions
+    void update_structure (const Tensor& input_tensor);
+    void validate(const std::vector<Tensor> &input_tensors) const;
+    std::vector<SimpleShape> compute_output_shapes(const std::vector<Tensor> &input_tensors) const;
+    std::vector<Tensor> create_output_tensors(const std::vector<Tensor> &input_tensors) const;
+    operation::ProgramWithCallbacks create_program(
+        const std::vector<Tensor> &input_tensors, std::vector<Tensor> &output_tensors) const;
+};
+
+namespace ccl::barrier::detail {
+//Template for the barrier_with_workers function
+//Found in device/host/barrier_full_worker_grid.cpp
+operation::ProgramWithCallbacks barrier_with_workers(
+    const Tensor& input_tensors,
+    const Tensor& output_tensors,
+    const bool is_starting_core,
+    const uint32_t ring_size,
+    const uint32_t ring_index,
+    const std::optional<chip_id_t> receiver_device_id,
+    const std::optional<chip_id_t> sender_device_id,
+    ttnn::ccl::Topology topology);
+}; // namespace ccl::barrier::detail
+
+namespace operations::ccl{
+    //Template for the barrier tensor found in device/barrier_op.cpp
+Tensor barrier_function(
+    const Tensor& input_tensor,
+    const ttnn::Barrier& barrier_struct);
+} // namespace operations::ccl
+
+}// namespace ttnn
