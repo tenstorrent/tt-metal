c3d23cf23e59c50ab14b86741a39521e18d69f41
Anasuya G Nair <mcw-anair@ext.tenstorrent.com>
2024-10-30 20:38:51 +0530

    #13626: Update doc for lgamma, normalize_global, normalize_hw, mish (#14214)
    * #13626: Update doc and sweep test for lgamma
    
    * #13626: Update normalize_hw, normalize_global
    
    * #13626: Update doc for mish
    
    * #13626: Update range for rpow

--- tests/sweep_framework/sweeps/eltwise/unary/normalize_hw/normalize_hw.py
+++ tests/sweep_framework/sweeps/eltwise/unary/normalize_hw/normalize_hw.py
@@ -6,18 +6,12 @@ from typing import Optional, Tuple
 from functools import partial
 
 import torch
-import random
 import ttnn
 from tests.sweep_framework.sweep_utils.utils import gen_shapes
 from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_func_with_cast_tt
 
 from tests.ttnn.utils_for_testing import check_with_pcc, start_measuring_time, stop_measuring_time
 from models.utility_functions import torch_random
-
-# Override the default timeout in seconds for hang detection.
-TIMEOUT = 30
-
-random.seed(0)
 
 
 # Parameters provided to the test vector generator are defined here.
@@ -27,12 +21,21 @@ random.seed(0)
 parameters = {
     "nightly": {
         "input_shape": gen_shapes([1, 1, 32, 32], [6, 12, 256, 256], [1, 1, 32, 32], 64),
-        "input_a_dtype": [ttnn.bfloat16],
-        "input_a_layout": [ttnn.TILE_LAYOUT],
+        "input_a_dtype": [ttnn.bfloat16, ttnn.bfloat8_b],
+        "input_a_layout": [ttnn.TILE_LAYOUT, ttnn.ROW_MAJOR_LAYOUT],
         "input_a_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
         "output_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
     },
 }
+
+
+# Invalidate vector is called during the generation phase where each vector will be passed in.
+# If invalidated, the vector will still be stored but will be skipped.
+# Returns False, None if the vector is valid, and True, str with a reason for invalidation if it is invalid.
+def invalidate_vector(test_vector) -> Tuple[bool, Optional[str]]:
+    if test_vector["input_a_dtype"] == ttnn.bfloat8_b:
+        return True, "bfloat8_b is not supported"
+    return False, None
 
 
 # This is the run instructions for the test, defined by the developer.
@@ -48,22 +51,16 @@ def run(
     *,
     device,
 ) -> list:
-    data_seed = random.randint(0, 20000000)
-    torch.manual_seed(data_seed)
+    torch.manual_seed(0)
 
     torch_input_tensor_a = gen_func_with_cast_tt(
         partial(torch_random, low=-10, high=10, dtype=torch.float32), input_a_dtype
     )(input_shape)
 
     torch_output_tensor = torch_input_tensor_a.clone()
-    mean_hw = torch.mean(torch_output_tensor, [-2, -1], keepdim=True)
-    std_hw = torch.std(torch_output_tensor, [-2, -1], keepdim=True)
 
-    for i in range(torch_output_tensor.shape[0]):
-        for j in range(torch_output_tensor.shape[1]):
-            torch_output_tensor[i, j, :, :] = (torch_output_tensor[i, j, :, :] - mean_hw[i, j, :, :]) / std_hw[
-                i, j, :, :
-            ]
+    golden_function = ttnn.get_golden_function(ttnn.normalize_global)
+    torch_output_tensor = golden_function(torch_output_tensor)
 
     input_tensor_a = ttnn.from_torch(
         torch_input_tensor_a,
