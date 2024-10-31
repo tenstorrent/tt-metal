1cde7eba35f973e60d7872c2fbb89aae295ff49a
Samarth Agarwal <sagarwal@tenstorrent.com>
2024-10-31 12:05:16 -0400

    #11795: Implemented randomized FD tests with trace to test the ring buffer (#14485)
    * #0: Initial commit
    
    * #0: Added trace tests
    
    * #0: Run trace for several iterations

--- tests/tt_metal/tt_metal/unit_tests_fast_dispatch/common/command_queue_fixture.hpp
+++ tests/tt_metal/tt_metal/unit_tests_fast_dispatch/common/command_queue_fixture.hpp
@@ -17,6 +17,7 @@
 #include "impl/device/device.hpp"
 #include "impl/kernels/data_types.hpp"
 #include "impl/kernels/kernel_types.hpp"
+#include "impl/dispatch/command_queue.hpp"
 #include "llrt/hal.hpp"
 #include "tt_cluster_descriptor_types.h"
 #include "tt_metal/host_api.hpp"
@@ -91,34 +92,51 @@ class CommandQueueMultiDeviceFixture : public ::testing::Test {
 class CommandQueueSingleCardFixture : public ::testing::Test {
    protected:
     void SetUp() override {
+        this->validate_dispatch_mode();
+        this->arch_ = tt::get_arch_from_string(tt::test_utils::get_umd_arch_name());
+        this->create_devices();
+    }
+
+    void TearDown() override { tt::tt_metal::detail::CloseDevices(reserved_devices_); }
+
+    void validate_dispatch_mode() {
         auto slow_dispatch = getenv("TT_METAL_SLOW_DISPATCH_MODE");
         if (slow_dispatch) {
             TT_THROW("This suite can only be run with fast dispatch or TT_METAL_SLOW_DISPATCH_MODE unset");
             GTEST_SKIP();
         }
-        auto enable_remote_chip = getenv("TT_METAL_ENABLE_REMOTE_CHIP");
-        arch_ = tt::get_arch_from_string(tt::test_utils::get_umd_arch_name());
-
-        const auto &dispatch_core_type = tt::llrt::OptionsG.get_dispatch_core_type();
-        const chip_id_t mmio_device_id = 0;
-        reserved_devices_ = tt::tt_metal::detail::CreateDevices({mmio_device_id}, 1, DEFAULT_L1_SMALL_SIZE, DEFAULT_TRACE_REGION_SIZE, dispatch_core_type);
-        if (enable_remote_chip) {
-            for (const auto &[id, device] : reserved_devices_) {
-                devices_.push_back(device);
-            }
-        } else {
-            devices_.push_back(reserved_devices_.at(mmio_device_id));
-        }
-
-        num_devices_ = reserved_devices_.size();
     }
 
-    void TearDown() override { tt::tt_metal::detail::CloseDevices(reserved_devices_); }
+    void create_devices(const std::size_t trace_region_size = DEFAULT_TRACE_REGION_SIZE) {
+        const auto &dispatch_core_type = tt::llrt::OptionsG.get_dispatch_core_type();
+        const chip_id_t mmio_device_id = 0;
+        this->reserved_devices_ = tt::tt_metal::detail::CreateDevices(
+            {mmio_device_id}, 1, DEFAULT_L1_SMALL_SIZE, trace_region_size, dispatch_core_type);
+        auto enable_remote_chip = getenv("TT_METAL_ENABLE_REMOTE_CHIP");
+        if (enable_remote_chip) {
+            for (const auto &[id, device] : this->reserved_devices_) {
+                this->devices_.push_back(device);
+            }
+        } else {
+            this->devices_.push_back(this->reserved_devices_.at(mmio_device_id));
+        }
 
-    std::vector<tt::tt_metal::Device*> devices_;
-    std::map<chip_id_t, tt::tt_metal::Device*> reserved_devices_;
+        this->num_devices_ = this->reserved_devices_.size();
+    }
+
+    std::vector<tt::tt_metal::Device *> devices_;
+    std::map<chip_id_t, tt::tt_metal::Device *> reserved_devices_;
     tt::ARCH arch_;
     size_t num_devices_;
+};
+
+class CommandQueueSingleCardTraceFixture : virtual public CommandQueueSingleCardFixture {
+    protected:
+     void SetUp() override {
+        this->validate_dispatch_mode();
+        this->arch_ = tt::get_arch_from_string(tt::test_utils::get_umd_arch_name());
+        this->create_devices(90000000);
+     }
 };
 
 class SingleDeviceTraceFixture: public ::testing::Test {
@@ -155,7 +173,7 @@ protected:
 
 };
 
-class RandomProgramFixture : public CommandQueueSingleCardFixture {
+class RandomProgramFixture : virtual public CommandQueueSingleCardFixture {
    protected:
     static const uint32_t MIN_KERNEL_SIZE_BYTES = 20;
     static const uint32_t MAX_KERNEL_SIZE_BYTES = 4096;
@@ -209,9 +227,11 @@ class RandomProgramFixture : public CommandQueueSingleCardFixture {
 
     void SetUp() override {
         CommandQueueSingleCardFixture::SetUp();
-
         this->device_ = this->devices_[0];
+        this->initialize_seed();
+    }
 
+    void initialize_seed() {
         const uint32_t seed = tt::parse_env("TT_METAL_SEED", static_cast<uint32_t>(time(nullptr)));
         log_info(tt::LogTest, "Using seed: {}", seed);
         srand(seed);
@@ -488,5 +508,39 @@ class RandomProgramFixture : public CommandQueueSingleCardFixture {
         CoreRangeSet empty_crs;
         CoreRangeSet resulting_cores = empty_crs.merge(cores_subset);
         return resulting_cores;
+    }
+};
+
+class RandomProgramTraceFixture : public RandomProgramFixture, public CommandQueueSingleCardTraceFixture {
+   protected:
+    static const uint32_t NUM_TRACE_ITERATIONS = 50;
+    Program programs[NUM_PROGRAMS];
+
+    void SetUp() override {
+        CommandQueueSingleCardTraceFixture::SetUp();
+        this->device_ = this->devices_[0];
+        this->initialize_seed();
+    }
+
+    uint32_t trace_programs() {
+        const uint32_t trace_id = this->capture_trace();
+        this->run_trace(trace_id);
+        return trace_id;
+    }
+
+   private:
+    uint32_t capture_trace() {
+        const uint32_t trace_id = BeginTraceCapture(this->device_, this->device_->command_queue().id());
+        for (Program &program : this->programs) {
+            EnqueueProgram(this->device_->command_queue(), program, false);
+        }
+        EndTraceCapture(this->device_, this->device_->command_queue().id(), trace_id);
+        return trace_id;
+    }
+
+    void run_trace(const uint32_t trace_id) {
+        for (uint32_t i = 0; i < NUM_TRACE_ITERATIONS; i++) {
+            EnqueueTrace(this->device_->command_queue(), trace_id, false);
+        }
     }
 };
