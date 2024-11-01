fd6ac2189edcce8aa069930cee9a5b3540b60bc7
Artem Yerofieiev <artem@tenstorrent.com>
2024-11-01 02:46:47 +0000

    #0: add tests. many fail

--- tests/ttnn/unit_tests/gtests/tensor/test_create_tensor.cpp
+++ tests/ttnn/unit_tests/gtests/tensor/test_create_tensor.cpp
@@ -11,6 +11,8 @@
 #include "ttnn/operations/numpy/functions.hpp"
 #include "tt_metal/common/logger.hpp"
 
+#include "common_tensor_test_utils.hpp"
+
 #include "ttnn_test_fixtures.hpp"
 
 namespace {
@@ -57,10 +59,6 @@ struct CreateTensorParams {
     ttnn::SimpleShape shape;
 };
 
-struct EmptyTensorParams {
-    ttnn::Shape shape;
-};
-
 }
 
 class CreateTensorTest : public ttnn::TTNNFixtureWithDevice, public ::testing::WithParamInterface<CreateTensorParams> {};
@@ -82,33 +80,220 @@ INSTANTIATE_TEST_SUITE_P(
     )
 );
 
-class EmptyTensorTest : public ttnn::TTNNFixtureWithDevice, public ::testing::WithParamInterface<EmptyTensorParams> {};
-
-TEST_P(EmptyTensorTest, Tile) {
-    EmptyTensorParams params = GetParam();
-    auto tensor = tt::tt_metal::create_device_tensor(params.shape, DataType::BFLOAT16, ttnn::TILE_LAYOUT, device_, ttnn::DRAM_MEMORY_CONFIG);
-    EXPECT_EQ(tensor.get_logical_shape(), params.shape.logical_shape());
+namespace tt::tt_metal {
+inline std::ostream& operator<<(std::ostream& os, Layout layout) {
+    switch (layout) {
+        case Layout::ROW_MAJOR: os << "ROW_MAJOR"; break;
+        case Layout::TILE: os << "TILE"; break;
+        case Layout::INVALID: os << "INVALID"; break;
+        default: os << "UNKNOWN";
+    }
+    return os;
 }
 
-TEST_P(EmptyTensorTest, RowMajor) {
-    EmptyTensorParams params = GetParam();
-    auto tensor = tt::tt_metal::create_device_tensor(params.shape, DataType::BFLOAT16, ttnn::ROW_MAJOR_LAYOUT, device_, ttnn::DRAM_MEMORY_CONFIG);
-    EXPECT_EQ(tensor.get_logical_shape(), params.shape.logical_shape());
+inline std::ostream& operator<<(std::ostream& os, DataType data_type) {
+    switch (data_type) {
+        case DataType::BFLOAT16: os << "BFLOAT16"; break;
+        case DataType::FLOAT32: os << "FLOAT32"; break;
+        case DataType::UINT32: os << "UINT32"; break;
+        case DataType::BFLOAT8_B: os << "BFLOAT8_B"; break;
+        case DataType::BFLOAT4_B: os << "BFLOAT4_B"; break;
+        case DataType::UINT8: os << "UINT8"; break;
+        case DataType::UINT16: os << "UINT16"; break;
+        case DataType::INT32: os << "INT32"; break;
+        case DataType::INVALID: os << "INVALID"; break;
+        default: os << "UNKNOWN";
+    }
+    return os;
+}
+
+inline std::ostream& operator<<(std::ostream& os, TensorMemoryLayout layout) {
+    switch (layout) {
+        case TensorMemoryLayout::INTERLEAVED: os << "INTERLEAVED"; break;
+        case TensorMemoryLayout::SINGLE_BANK: os << "SINGLE_BANK"; break;
+        case TensorMemoryLayout::HEIGHT_SHARDED: os << "HEIGHT_SHARDED"; break;
+        case TensorMemoryLayout::WIDTH_SHARDED: os << "WIDTH_SHARDED"; break;
+        case TensorMemoryLayout::BLOCK_SHARDED: os << "BLOCK_SHARDED"; break;
+        default: os << "UNKNOWN";
+    }
+    return os;
+}
+
+inline std::ostream& operator<<(std::ostream& os, ShardOrientation orientation) {
+    switch (orientation) {
+        case ShardOrientation::ROW_MAJOR: os << "ROW_MAJOR"; break;
+        case ShardOrientation::COL_MAJOR: os << "COL_MAJOR"; break;
+        default: os << "UNKNOWN";
+    }
+    return os;
+}
+
+inline std::ostream& operator<<(std::ostream& os, BufferType buffer) {
+    switch (buffer) {
+        case BufferType::DRAM: os << "DRAM"; break;
+        case BufferType::L1: os << "L1"; break;
+        case BufferType::SYSTEM_MEMORY: os << "SYSTEM_MEMORY"; break;
+        case BufferType::L1_SMALL: os << "L1_SMALL"; break;
+        case BufferType::TRACE: os << "TRACE"; break;
+        default: os << "UNKNOWN";
+    }
+    return os;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const ShardSpec& spec) {
+    os << "ShardSpec(grid=?"
+       << ", shape={" << spec.shape[0] << ", " << spec.shape[1] << "})";
+    return os;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const MemoryConfig& config) {
+    os << "MemoryConfig(memory_layout=" << config.memory_layout
+       << ", buffer_type=" << config.buffer_type
+       << ", shard_spec=";
+    if (config.shard_spec) {
+        os << *config.shard_spec;
+    } else {
+        os << "nullopt";
+    }
+    os << ")";
+    return os;
+}
+}
+
+using CombinationInputParams = std::tuple<ttnn::Shape, tt::tt_metal::DataType, tt::tt_metal::Layout, tt::tt_metal::MemoryConfig>;
+class EmptyTensorTest : public ttnn::TTNNFixtureWithDevice, public ::testing::WithParamInterface<CombinationInputParams> {};
+
+TEST_P(EmptyTensorTest, Combinations) {
+    auto params = GetParam();
+    auto shape = std::get<0>(params);
+    auto dtype = std::get<1>(params);
+    auto layout = std::get<2>(params);
+    auto memory_config = std::get<3>(params);
+    tt::log_info("Running test with shape={}, dtype={}, layout={}, memory_config={}", shape, dtype, layout, memory_config);
+
+    if(layout == tt::tt_metal::Layout::ROW_MAJOR && dtype == tt::tt_metal::DataType::BFLOAT8_B) {
+        return;
+    }
+
+    auto tensor = tt::tt_metal::create_device_tensor(shape, dtype, layout, device_, memory_config);
+    EXPECT_EQ(tensor.get_logical_shape(), shape.logical_shape());
+
+    auto tensor_layout = tt::tt_metal::TensorLayout::fromLegacyPaddedShape(dtype, PageConfig(layout), memory_config, shape);
+    test_utils::test_tensor_on_device(shape.logical_shape(), tensor_layout, device_);
 }
 
 INSTANTIATE_TEST_SUITE_P(
     EmptyTensorTestWithShape,
     EmptyTensorTest,
-    ::testing::Values(
-        EmptyTensorParams{.shape=ttnn::Shape({1, 1, 32, 32})},
-        EmptyTensorParams{.shape=ttnn::Shape({2, 1, 32, 32})},
-        EmptyTensorParams{.shape=ttnn::Shape({64, 1, 256, 1})},
-        EmptyTensorParams{.shape=ttnn::Shape({1, 1, 21120, 16})},
-        EmptyTensorParams{.shape=ttnn::Shape({0, 0, 0, 0})},
-        EmptyTensorParams{.shape=ttnn::Shape({0, 1, 32, 32})},
-        EmptyTensorParams{.shape=ttnn::Shape({0})},
-        EmptyTensorParams{.shape=ttnn::Shape({1, 1, 1, 1})},
-        EmptyTensorParams{.shape=ttnn::Shape({1})},
-        EmptyTensorParams{.shape=ttnn::Shape({})}
+    ::testing::Combine(
+        ::testing::Values(
+            //ttnn::Shape({}),
+            //ttnn::Shape({0}),
+            //ttnn::Shape({1}),
+            ttnn::Shape({1, 2}),
+            ttnn::Shape({1, 2, 3}),
+            ttnn::Shape({1, 2, 3, 4}),
+            //ttnn::Shape({0, 0, 0, 0}), fails with width sharded case
+            ttnn::Shape({1, 1, 1, 1}),
+            //ttnn::Shape({0, 1, 32, 32}), fails with width sharded case
+            ttnn::Shape({1, 1, 32, 32}),
+            ttnn::Shape({2, 1, 32, 32}),
+            ttnn::Shape({64, 1, 256, 1}),
+            ttnn::Shape({1, 1, 21120, 16}),
+            ttnn::Shape({1, 2, 3, 4, 5})
+        ),
+
+        ::testing::Values(
+            tt::tt_metal::DataType::BFLOAT16,
+            tt::tt_metal::DataType::UINT32,
+            tt::tt_metal::DataType::FLOAT32,
+            tt::tt_metal::DataType::BFLOAT8_B),
+
+        ::testing::Values(
+            tt::tt_metal::Layout::TILE,
+            tt::tt_metal::Layout::ROW_MAJOR),
+
+        ::testing::Values(
+            tt::tt_metal::MemoryConfig{
+                .memory_layout = tt::tt_metal::TensorMemoryLayout::SINGLE_BANK,
+                .buffer_type = ttnn::BufferType::L1
+            },
+
+            tt::tt_metal::MemoryConfig{
+                .memory_layout = tt::tt_metal::TensorMemoryLayout::SINGLE_BANK,
+                .buffer_type = ttnn::BufferType::DRAM
+            },
+
+
+            tt::tt_metal::MemoryConfig{
+                .memory_layout = tt::tt_metal::TensorMemoryLayout::INTERLEAVED,
+                .buffer_type = tt::tt_metal::BufferType::L1
+            },
+
+            tt::tt_metal::MemoryConfig{
+                .memory_layout = tt::tt_metal::TensorMemoryLayout::INTERLEAVED,
+                .buffer_type = tt::tt_metal::BufferType::DRAM
+            },
+
+
+            // tt::tt_metal::MemoryConfig{
+            //     .memory_layout = tt::tt_metal::TensorMemoryLayout::HEIGHT_SHARDED,
+            //     .buffer_type = tt::tt_metal::BufferType::L1,
+            //     .shard_spec = ShardSpec{
+            //         CoreRangeSet{std::set<CoreRange>{CoreRange{CoreCoord{0, 0}, CoreCoord{4, 1}}}},
+            //         {32, 64},
+            //         tt::tt_metal::ShardOrientation::ROW_MAJOR,
+            //         false}
+            // },
+            // tt::tt_metal::MemoryConfig{
+            //     .memory_layout = tt::tt_metal::TensorMemoryLayout::HEIGHT_SHARDED,
+            //     .buffer_type = tt::tt_metal::BufferType::DRAM,
+            //     .shard_spec = ShardSpec{
+            //         CoreRangeSet{std::set<CoreRange>{CoreRange{CoreCoord{0, 0}, CoreCoord{4, 1}}}},
+            //         {32, 64},
+            //         tt::tt_metal::ShardOrientation::ROW_MAJOR,
+            //         false}
+            // },
+
+
+            ttnn::MemoryConfig{
+                .memory_layout = tt::tt_metal::TensorMemoryLayout::WIDTH_SHARDED,
+                .buffer_type = tt::tt_metal::BufferType::L1,
+                .shard_spec = ShardSpec{
+                    CoreRangeSet{std::set<CoreRange>{CoreRange{CoreCoord{0, 0}, CoreCoord{1, 4}}}},
+                    {32, 64},
+                    tt::tt_metal::ShardOrientation::ROW_MAJOR,
+                    false}
+            },
+            // tt::tt_metal::MemoryConfig{
+            //     .memory_layout = tt::tt_metal::TensorMemoryLayout::WIDTH_SHARDED,
+            //     .buffer_type = tt::tt_metal::BufferType::DRAM,
+            //     .shard_spec = ShardSpec{
+            //         CoreRangeSet{std::set<CoreRange>{CoreRange{CoreCoord{0, 0}, CoreCoord{1, 4}}}},
+            //         {32, 64},
+            //         tt::tt_metal::ShardOrientation::ROW_MAJOR,
+            //         false}
+            // },
+
+
+            tt::tt_metal::MemoryConfig{
+                .memory_layout = tt::tt_metal::TensorMemoryLayout::BLOCK_SHARDED,
+                .buffer_type = tt::tt_metal::BufferType::L1,
+                .shard_spec = ShardSpec{
+                    CoreRangeSet{std::set<CoreRange>{CoreRange{CoreCoord{0, 0}, CoreCoord{4, 4}}}},
+                    {64, 64},
+                    tt::tt_metal::ShardOrientation::ROW_MAJOR,
+                    false}
+            }
+            // tt::tt_metal::MemoryConfig{
+            //     .memory_layout = tt::tt_metal::TensorMemoryLayout::BLOCK_SHARDED,
+            //     .buffer_type = tt::tt_metal::BufferType::DRAM,
+            //     .shard_spec = ShardSpec{
+            //         CoreRangeSet{std::set<CoreRange>{CoreRange{CoreCoord{0, 0}, CoreCoord{4, 4}}}},
+            //         {64, 64},
+            //         tt::tt_metal::ShardOrientation::ROW_MAJOR,
+            //         false}
+            // }
+        )
     )
 );
