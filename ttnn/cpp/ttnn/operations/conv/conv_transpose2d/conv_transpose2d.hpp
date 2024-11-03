c736d688ccf9d9a4cb778f6d9de8e2739754960f
Sankar Manoj <smanoj@tenstorrent.com>
2024-11-03 09:46:04 +0000

    #0: Cleanup

--- ttnn/cpp/ttnn/operations/conv/conv_transpose2d/conv_transpose2d.hpp
+++ ttnn/cpp/ttnn/operations/conv/conv_transpose2d/conv_transpose2d.hpp
@@ -11,26 +11,6 @@ namespace operations::conv {
 namespace conv_transpose2d {
 
 
-template <typename T>
-std::tuple<ttnn::Tensor, uint32_t, uint32_t, ttnn::Tensor, std::optional<ttnn::Tensor>> conv_transpose2d(
-    const ttnn::Tensor& input_tensor,
-    const ttnn::Tensor& weight_tensor,
-    T * device,
-    uint32_t in_channels,
-    uint32_t out_channels,
-    uint32_t batch_size,
-    uint32_t input_height,
-    uint32_t input_width,
-    std::array<uint32_t, 2> kernel_size,
-    std::array<uint32_t, 2> stride,
-    std::array<uint32_t, 2> padding,
-    std::array<uint32_t, 2> output_padding,
-    std::array<uint32_t, 2> dilation,
-    uint32_t groups,
-    std::optional<const ttnn::Tensor> bias_tensor = std::nullopt,
-    std::optional<const conv2d::Conv2dConfig> conv_config_ = std::nullopt);
-
-
 struct ConvTranpose2dOperation{
     static std::tuple<ttnn::Tensor, uint32_t, uint32_t, ttnn::Tensor, std::optional<ttnn::Tensor>> invoke(
         uint8_t queue_id,
