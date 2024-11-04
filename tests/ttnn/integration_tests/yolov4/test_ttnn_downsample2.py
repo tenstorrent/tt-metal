25d80e837eeebb74765b5428d12139da324a1bb2
Shwetank Singh <ssingh@tenstorrent.com>
2024-11-04 18:02:37 +0000

    #14401: yolo v4 on nighty CI.

--- tests/ttnn/integration_tests/yolov4/test_ttnn_downsample2.py
+++ tests/ttnn/integration_tests/yolov4/test_ttnn_downsample2.py
@@ -8,8 +8,8 @@ from tests.ttnn.utils_for_testing import assert_with_pcc
 import pytest
 import time
 from models.utility_functions import skip_for_grayskull
-from models.experimental.yolov4.reference.downsample2 import DownSample2
-from models.experimental.yolov4.ttnn.downsample2 import Down2
+from models.demos.yolov4.reference.downsample2 import DownSample2
+from models.demos.yolov4.ttnn.downsample2 import Down2
 from loguru import logger
 import os
 
@@ -17,6 +17,7 @@ import os
 @skip_for_grayskull()
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 16384}], indirect=True)
 def test_down2(device, reset_seeds, model_location_generator):
+    torch.manual_seed(0)
     model_path = model_location_generator("models", model_subdir="Yolo")
 
     if model_path == "models":
