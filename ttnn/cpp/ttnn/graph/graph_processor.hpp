2fd11e453d88c66a9bf8eb54f7417b2809e59d74
Dusan Erdeljan <derdeljan@tenstorrent.com>
2024-11-04 19:05:33 +0100

    #12624: Fix tracking of globally allocated CBs in graph capture (#14521)
    * #12624: Fix tracking of globally allocated CBs in graph capture
    
    * #12624: Update graph tracing docs

--- ttnn/cpp/ttnn/graph/graph_processor.hpp
+++ ttnn/cpp/ttnn/graph/graph_processor.hpp
@@ -44,7 +44,7 @@ namespace ttnn::graph {
 
         void track_deallocate(tt::tt_metal::Buffer* buffer) override;
 
-        void track_allocate_cb(const CoreRangeSet &core_range, uint64_t addr, uint64_t size) override;
+        void track_allocate_cb(const CoreRangeSet &core_range, uint64_t addr, uint64_t size, bool is_globally_allocated) override;
 
         void track_deallocate_cb() override;
 
