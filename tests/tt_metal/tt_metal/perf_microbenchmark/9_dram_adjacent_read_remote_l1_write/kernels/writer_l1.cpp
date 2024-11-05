c5478daee9e75e9df5cd743238b734e49b007698
Johanna Rock <129077594+johanna-rock-tt@users.noreply.github.com>
2024-11-05 10:02:11 +0100

    #14512: Add DRAM pre-fetcher benchmark (#14528)
    * #14512: Add DRAM pre-fetcher benchmark

--- tests/tt_metal/tt_metal/perf_microbenchmark/9_dram_adjacent_read_remote_l1_write/kernels/writer_l1.cpp
+++ tests/tt_metal/tt_metal/perf_microbenchmark/9_dram_adjacent_read_remote_l1_write/kernels/writer_l1.cpp
@@ -11,40 +11,55 @@
 
 void kernel_main() {
     constexpr uint32_t num_blocks = get_compile_time_arg_val(0);
-    constexpr uint32_t num_pages = get_compile_time_arg_val(1);
-    constexpr uint32_t block_num_tiles = get_compile_time_arg_val(2);
-    constexpr uint32_t page_size = get_compile_time_arg_val(3);
-    constexpr uint32_t noc = get_compile_time_arg_val(4);
+    constexpr uint32_t num_pages_w_per_receiver = get_compile_time_arg_val(1);
+    constexpr uint32_t num_tiles_h = get_compile_time_arg_val(2);
+    constexpr uint32_t block_num_tiles = get_compile_time_arg_val(3);
+    constexpr uint32_t page_size = get_compile_time_arg_val(4);
+    constexpr uint32_t noc = get_compile_time_arg_val(5);
 
     const uint32_t vc = get_arg_val<uint32_t>(0);
-    const uint32_t noc_x = get_arg_val<uint32_t>(1);
-    const uint32_t noc_y = get_arg_val<uint32_t>(2);
+    // First L1 writer core coordinates
+    const uint32_t noc_x1 = get_arg_val<uint32_t>(1);
+    const uint32_t noc_y1 = get_arg_val<uint32_t>(2);
+    // Second L1 writer core coordinates
+    const uint32_t noc_x2 = get_arg_val<uint32_t>(3);
+    const uint32_t noc_y2 = get_arg_val<uint32_t>(4);
 
     constexpr uint32_t cb_id = 0;
 
     uint32_t l1_write_addr = get_write_ptr(cb_id);
-    const uint64_t l1_noc_write_addr = get_noc_addr(noc_x, noc_y, l1_write_addr, noc);
+    const uint64_t l1_noc_write_addr1 = get_noc_addr(noc_x1, noc_y1, l1_write_addr, noc);
+    const uint64_t l1_noc_write_addr2 = get_noc_addr(noc_x2, noc_y2, l1_write_addr, noc);
 
-    noc_async_write_one_packet_set_state(l1_noc_write_addr, page_size, noc, vc);
-
-    for (uint32_t block = 0; block < num_blocks; ++block) {
-
-        auto remote_l1_write_addr = l1_noc_write_addr;
+    for (uint32_t block = 0; block < num_blocks; ++block) { // Iterate over blocks
 
         cb_wait_front(cb_id, block_num_tiles);
-        auto l1_read_addr = get_read_ptr(cb_id);
 
-        for (uint32_t h = 0; h < num_pages; ++h) {
-            noc_async_write_one_packet_with_state(l1_read_addr, remote_l1_write_addr, noc);
-            l1_read_addr += page_size;
-            remote_l1_write_addr += page_size;
+        for (uint32_t core_id = 0; core_id < 2; ++core_id) { // Iterate over two neighboring cores
+            uint64_t l1_noc_write_addr_for_receiver_core = 0;
+            uint32_t l1_read_addr = get_read_ptr(cb_id);
+            if (core_id == 0) {
+                l1_noc_write_addr_for_receiver_core = l1_noc_write_addr1; // Set write pointer to start of cb for first core
+            } else {
+                l1_noc_write_addr_for_receiver_core = l1_noc_write_addr2; // Set write pointer to start of cb for second core
+                l1_read_addr += page_size * num_pages_w_per_receiver; // Stride read pointer to start of second core
+            }
+
+            noc_async_write_one_packet_set_state(l1_noc_write_addr_for_receiver_core, page_size, noc, vc); // Set state to write a page to noc/vc
+
+            for (uint32_t h = 0; h < num_tiles_h; ++h) { // Iterate over page rows per receiver core
+                for (uint32_t w = 0; w < num_pages_w_per_receiver; ++w) { // Iterate over page columns per receiver core
+                    noc_async_write_one_packet_with_state(l1_read_addr, l1_noc_write_addr_for_receiver_core, noc);
+                    l1_read_addr += page_size;
+                    l1_noc_write_addr_for_receiver_core += page_size;
+                }
+                l1_read_addr += page_size * num_pages_w_per_receiver; // Stride read pointer over other core's data
+            }
         }
 
         noc_async_write_barrier(noc);
 
         cb_pop_front(cb_id, block_num_tiles);
-
     }
-
 
 }
