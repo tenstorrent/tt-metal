180c305b56dd786623c08c20fc666672fd5b4475
blozano-tt <blozano@tenstorrent.com>
2024-11-06 09:28:30 +0000

    #0: Fix grayskull bug

--- tt_metal/llrt/hal.hpp
+++ tt_metal/llrt/hal.hpp
@@ -133,6 +133,8 @@ class Hal {
 
     void initialize(tt::ARCH arch);
 
+    tt::ARCH get_arch() {return arch_;}
+
     uint32_t get_programmable_core_type_count() const;
     HalProgrammableCoreType get_programmable_core_type(uint32_t core_type_index) const;
     uint32_t get_programmable_core_type_index(HalProgrammableCoreType programmable_core_type_index) const;
