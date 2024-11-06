c53dc01b1d620d64e68ed158dddef5ac013e8b6d
Nigel Huang <nhuang@tenstorrent.com>
2024-11-06 10:46:51 -0800

    Add missing header to tests/tt_metal/tt_metal/perf_microbenchmark/routing/test_tx_rx.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/tt_metal/tt_metal/perf_microbenchmark/routing/test_tx_rx.cpp
+++ tests/tt_metal/tt_metal/perf_microbenchmark/routing/test_tx_rx.cpp
@@ -8,6 +8,7 @@
 #include "tt_metal/llrt/rtoptions.hpp"
 #include "tt_metal/impl/dispatch/kernels/packet_queue_ctrl.hpp"
 #include "kernels/traffic_gen_test.hpp"
+#include "utils.hpp"
 
 using std::vector;
 using namespace tt;
