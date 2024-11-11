311b3550208bd2ea9feede6ff60354ab4f0333ef
Mohamed Bahnas <mbahnas@tenstorrent.com>
2024-11-11 21:54:06 +0000

    #0: yolov4 web demo

--- models/demos/yolov4/web_demo/server/run_uvicorn.sh
+++ models/demos/yolov4/web_demo/server/run_uvicorn.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-TT_BACKEND_TIMEOUT=0 /home/dvartanians/Metal/tt-metal/python_env/bin/uvicorn --host 0.0.0.0 --port 7000 fast_api_yolov5:app
+uvicorn --host 0.0.0.0 --port 7000 models.demos.yolov4.web_demo.server.fast_api_yolov4:app
