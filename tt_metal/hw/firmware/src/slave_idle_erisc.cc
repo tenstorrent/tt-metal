701b5943bf5bded07fdc8ba8338e274cace0126e
arikTT <ayaacob@tenstorrent.com>
2024-11-13 16:55:04 +0000

    use do_crt1 like other cores

--- tt_metal/hw/firmware/src/slave_idle_erisc.cc
+++ tt_metal/hw/firmware/src/slave_idle_erisc.cc
@@ -54,9 +54,7 @@ int main(int argc, char *argv[]) {
     conditionally_disable_l1_cache();
     DIRTY_STACK_MEMORY();
     WAYPOINT("I");
-
-    int32_t num_words = ((uint)__ldm_data_end - (uint)__ldm_data_start) >> 2;
-    l1_to_local_mem_copy((uint *)__ldm_data_start, (uint tt_l1_ptr *)MEM_SLAVE_IERISC_INIT_LOCAL_L1_BASE_SCRATCH, num_words);
+    do_crt1((uint32_t *)MEM_IERISC_INIT_LOCAL_L1_BASE_SCRATCH);
 
     risc_init();
 
