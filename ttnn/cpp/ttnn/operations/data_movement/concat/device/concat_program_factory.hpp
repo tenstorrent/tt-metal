9a5133446e51aa79ab1898a16c9e1e5bc1336fb8
Andrew Fuller <afuller@tenstorrent.com>
2024-11-13 19:50:50 +0000

    #0: right down to the line...

--- ttnn/cpp/ttnn/operations/data_movement/concat/device/concat_program_factory.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/concat/device/concat_program_factory.hpp
@@ -23,7 +23,7 @@ struct CorePageRange {
     PageRange range;
 };
 
-tt::tt_metal::operation::ProgramWithCallbacks s2s_rm_concat_two_tensors_multi_core(
+tt::tt_metal::operation::ProgramWithCallbacks s2s_rm_concat_two_tensors_multi_core( // Look ma! a bug!
     const std::vector<Tensor> &input_tensors, uint32_t dim, Tensor &output, unsigned int groups = 1);
 
 tt::tt_metal::operation::ProgramWithCallbacks s2i_rm_concat_multi_core(
