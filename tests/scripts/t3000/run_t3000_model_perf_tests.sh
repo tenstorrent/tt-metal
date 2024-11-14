970ecb18aa01386075d2fc9ef9faf2dbeba80105
Aswin Zayasankaran <156493059+Aswinmcw@users.noreply.github.com>
2024-11-14 15:36:47 +0530

    Add reduce_scatter t3k perf to pipeline (#14950)
    ### Ticket
    #14406 
    
    Adds reduce_scatter t3k perf to pipeline
    
    
    https://github.com/tenstorrent/tt-metal/actions/runs/11795073011/job/32854208345
    
    ### Checklist
    - [ ] Post commit CI passes
    - [ ] Blackhole Post commit (if applicable)
    - [ ] Model regression CI testing passes (if applicable)
    - [ ] Device performance regression CI testing passes (if applicable)
    - [ ] New/Existing tests provide coverage for changes

--- tests/scripts/t3000/run_t3000_model_perf_tests.sh
+++ tests/scripts/t3000/run_t3000_model_perf_tests.sh
@@ -161,6 +161,25 @@ run_t3000_ccl_all_gather_perf_tests() {
   fi
 }
 
+run_t3000_ccl_reduce_scatter_perf_tests() {
+  # Record the start time
+  fail=0
+  start_time=$(date +%s)
+
+  echo "LOG_METAL: Running run_t3000_ccl_reduce_scatter_perf_tests"
+
+  tests/ttnn/unit_tests/operations/ccl/perf/run_reduce_scatter_profile.sh -t t3000
+  fail+=$?
+
+  # Record the end time
+  end_time=$(date +%s)
+  duration=$((end_time - start_time))
+  echo "LOG_METAL: run_t3000_ccl_reduce_scatter_perf_tests $duration seconds to complete"
+  if [[ $fail -ne 0 ]]; then
+    exit 1
+  fi
+}
+
 run_t3000_llm_tests() {
   # Run falcon7b tests
   run_t3000_falcon7b_tests
@@ -195,6 +214,7 @@ run_t3000_cnn_tests() {
 run_t3000_ccl_tests() {
   # Run ccl performance tests
   run_t3000_ccl_all_gather_perf_tests
+  run_t3000_ccl_reduce_scatter_perf_tests
 
 }
 
