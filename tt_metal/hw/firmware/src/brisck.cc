52742ebb68e084ec2884c48bc205ac05ab85915c
Michael Chiou <156848643+ttmchiou@users.noreply.github.com>
2024-11-14 09:37:37 -0800

    Revert "#14826: Remove misoptimizations from init code (#14861)"
    This reverts commit 650c5c33628f31c947c26af26439f33b3f263d86.

--- tt_metal/hw/firmware/src/brisck.cc
+++ tt_metal/hw/firmware/src/brisck.cc
@@ -18,6 +18,9 @@
 #include "tools/profiler/kernel_profiler.hpp"
 #include <kernel_includes.hpp>
 
+extern uint32_t __kernel_init_local_l1_base[];
+extern uint32_t __fw_export_end_text[];
+
 void kernel_launch(uint32_t kernel_base_addr) {
 
 #if defined(DEBUG_NULL_KERNELS) && !defined(DISPATCH_KERNEL)
@@ -26,10 +29,7 @@ void kernel_launch(uint32_t kernel_base_addr) {
     while (c_tensix_core::read_wall_clock() < end_time);
 #endif
 #else
-    extern uint32_t __kernel_init_local_l1_base[];
-    extern uint32_t __fw_export_end_text[];
-    do_crt1((uint32_t tt_l1_ptr
-                 *)(kernel_base_addr + (uint32_t)__kernel_init_local_l1_base - (uint32_t)__fw_export_end_text));
+    firmware_kernel_common_init((void tt_l1_ptr *)(kernel_base_addr + (uint32_t) __kernel_init_local_l1_base - (uint32_t)__fw_export_end_text));
 
     if constexpr (NOC_MODE == DM_DEDICATED_NOC) {
         noc_local_state_init(NOC_INDEX);
