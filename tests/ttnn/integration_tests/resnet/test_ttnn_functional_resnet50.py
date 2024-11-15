39f5ec1a7daa86ffdede3fb93fc226aeccdebf68
Abhinav Sarje <asarje@tenstorrent.com>
2024-11-15 00:52:31 +0000

    Some cleaning and minor updates

--- tests/ttnn/integration_tests/resnet/test_ttnn_functional_resnet50.py
+++ tests/ttnn/integration_tests/resnet/test_ttnn_functional_resnet50.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+# SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
 
 # SPDX-License-Identifier: Apache-2.0
 
@@ -9,25 +9,15 @@ from loguru import logger
 
 from models.demos.ttnn_resnet.tests.resnet50_test_infra import create_test_infra
 from models.utility_functions import (
-    is_wormhole_b0,
-    enable_memory_reports,
-    skip_for_wormhole_b0,
-    skip_for_grayskull,
+    run_for_blackhole,
 )
 
 
-@skip_for_grayskull("This is Blackhole specific test")
-@skip_for_wormhole_b0("This is Blackhole specific test")
+@run_for_blackhole("This is Blackhole specific test")
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 24576}], indirect=True)
 @pytest.mark.parametrize(
     "batch_size, act_dtype, weight_dtype, math_fidelity",
     (
-        (
-            8,
-            ttnn.bfloat8_b,
-            ttnn.bfloat8_b,
-            ttnn.MathFidelity.LoFi,
-        ),  ## memory config issue due to l4m1 downsample reshard
         (16, ttnn.bfloat8_b, ttnn.bfloat8_b, ttnn.MathFidelity.HiFi2),
         (16, ttnn.bfloat8_b, ttnn.bfloat8_b, ttnn.MathFidelity.LoFi),
         (20, ttnn.bfloat8_b, ttnn.bfloat8_b, ttnn.MathFidelity.HiFi2),
@@ -52,13 +42,6 @@ def test_resnet_50(
     use_pretrained_weight,
     model_location_generator,
 ):
-    if batch_size == 8:
-        pytest.skip("Skipping batch size 8 due to memory config issue")
-    if is_wormhole_b0() and batch_size == 20:
-        pytest.skip("Skipping batch size 20 for Wormhole B0 due to fitting issue")
-    if (device.compute_with_storage_grid_size().x, device.compute_with_storage_grid_size().y) == (8, 7):
-        pytest.skip("Test is not supported on n300 (8,7) grid")
-
     test_infra = create_test_infra(
         device,
         batch_size,
@@ -68,7 +51,6 @@ def test_resnet_50(
         use_pretrained_weight,
         model_location_generator=model_location_generator,
     )
-    enable_memory_reports()
     tt_inputs_host, input_mem_config = test_infra.setup_l1_sharded_input(device)
     test_infra.input_tensor = tt_inputs_host.to(device, input_mem_config)
     # First run configures convs JIT
