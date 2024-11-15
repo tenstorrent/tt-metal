451512f097832404e2e2f5f8eca5020fbeafd286
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2024-11-15 09:22:26 +0000

    #14985: Update the examples for binary backward doc

--- tests/ttnn/unit_tests/operations/eltwise/backward/test_backward_mul.py
+++ tests/ttnn/unit_tests/operations/eltwise/backward/test_backward_mul.py
@@ -34,6 +34,23 @@ def test_bw_mul(input_shapes, device):
     assert status
 
 
+def test_bw_mul_example(device):
+    x1_torch = torch.tensor([[1, 2], [3, 4]], dtype=torch.bfloat16, requires_grad=True)
+    x2_torch = torch.tensor([[1, 2], [3, 4]], dtype=torch.bfloat16, requires_grad=True)
+    grad_tensor = torch.tensor([[1, 2], [3, 4]], dtype=torch.bfloat16)
+    golden_function = ttnn.get_golden_function(ttnn.mul_bw)
+    golden_tensor = golden_function(grad_tensor, x1_torch, x2_torch)
+    grad_tt = ttnn.from_torch(grad_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+    x1_tt = ttnn.from_torch(x1_torch, layout=ttnn.TILE_LAYOUT, device=device)
+    x2_tt = ttnn.from_torch(x2_torch, layout=ttnn.TILE_LAYOUT, device=device)
+    y_tt = ttnn.mul_bw(grad_tt, x1_tt, x2_tt)
+    tt_out_1 = ttnn.to_torch(y_tt[1])
+    tt_out_0 = ttnn.to_torch(y_tt[0])
+    comp_pass_1 = torch.allclose(tt_out_1, golden_tensor[1])
+    comp_pass_0 = torch.allclose(tt_out_0, golden_tensor[0])
+    assert comp_pass_1 and comp_pass_0
+
+
 @pytest.mark.parametrize(
     "input_shapes",
     (
