96f7d2fda30d9e19a590509cef490ea186a561d0
Samarth Agarwal <sagarwal@tenstorrent.com>
2024-11-19 00:17:18 +0000

    Updated scripts

--- tests/scripts/run_cpp_unit_tests.sh
+++ tests/scripts/run_cpp_unit_tests.sh
@@ -9,19 +9,18 @@ fi
 
 kernel_path="/tmp/kernels"
 mkdir -p $kernel_path
-TT_METAL_KERNEL_PATH=$kernel_path ./build/test/tt_metal/ --gtest_filter=CompileProgramWithKernelPathEnvVarFixture.*
+TT_METAL_KERNEL_PATH=$kernel_path ./build/test/tt_metal/unit_tests_api --gtest_filter=CompileProgramWithKernelPathEnvVarFixture.*
 rm -rf $kernel_path
 
+./build/test/tt_metal/unit_tests_dispatch
 if [[ ! -z "$TT_METAL_SLOW_DISPATCH_MODE" ]]; then
-    ./build/test/tt_metal/
     env python tests/scripts/run_tt_metal.py --dispatch-mode slow
     env python tests/scripts/run_tt_eager.py --dispatch-mode slow
 else
-    ./build/test/tt_metal/
-    TT_METAL_GTEST_NUM_HW_CQS=2 ./build/test/tt_metal/ --gtest_filter=MultiCommandQueueSingleDeviceFixture.*
+    TT_METAL_GTEST_NUM_HW_CQS=2 ./build/test/tt_metal/unit_tests_dispatch --gtest_filter=MultiCommandQueueSingleDevice.*Fixture.*
     # Enable this on BH after #14613
     if [[ "$ARCH_NAME" == "wormhole_b0" ]]; then
-        TT_METAL_GTEST_ETH_DISPATCH=1 ./build/test/tt_metal/
+        TT_METAL_GTEST_ETH_DISPATCH=1 ./build/test/tt_metal/unit_tests_dispatch
     fi
     env python tests/scripts/run_tt_eager.py --dispatch-mode fast
     env python tests/scripts/run_tt_metal.py --dispatch-mode fast
