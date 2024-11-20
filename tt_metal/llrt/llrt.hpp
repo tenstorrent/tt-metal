13f5801a6687ec6319f9e9a5e11fd75e98317575
Almeet Bhullar <abhullar@tenstorrent.com>
2024-11-20 16:21:17 +0000

    Add address and value that will launch fw on a programmable core into the HalJitBuildConfig

--- tt_metal/llrt/llrt.hpp
+++ tt_metal/llrt/llrt.hpp
@@ -91,8 +91,6 @@ CoreCoord logical_core_from_ethernet_core(chip_id_t chip_id, CoreCoord &physical
 
 void write_launch_msg_to_core(chip_id_t chip, CoreCoord core, launch_msg_t *msg, go_msg_t * go_msg, uint64_t addr, bool send_go = true);
 
-void launch_erisc_app_fw_on_core(chip_id_t chip, CoreCoord core);
-
 void print_worker_cores(chip_id_t chip_id = 0);
 
 inline bool is_worker_core(const CoreCoord &core, chip_id_t chip_id) {
@@ -106,9 +104,6 @@ inline bool is_ethernet_core(const CoreCoord &core, chip_id_t chip_id) {
     return std::find(soc_desc.physical_ethernet_cores.begin(), soc_desc.physical_ethernet_cores.end(), core) !=
            soc_desc.physical_ethernet_cores.end();
 }
-
-uint32_t generate_risc_startup_addr(bool is_eth_core);
-void program_risc_startup_addr(chip_id_t chip_id, const CoreCoord &core);
 
 bool test_load_write_read_risc_binary(
     ll_api::memory const& mem,
