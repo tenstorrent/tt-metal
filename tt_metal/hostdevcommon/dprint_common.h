7840821b9ae538c39b299ef410bc9b21230d7576
Nigel Huang <nhuang@tenstorrent.com>
2024-11-21 02:17:00 +0000

    15178: fix dprint race condition
    - Fix intermittent DPrint issues on N300 due
      to a race condition and not properly checking
      init value was cleared
    
    - Improved the error message if it gets thrown
    
    - Increased init magics. They must never collide with
      real read/write pointer values

--- tt_metal/hostdevcommon/dprint_common.h
+++ tt_metal/hostdevcommon/dprint_common.h
@@ -89,21 +89,22 @@ enum DPrintTypeID : uint8_t {
 static_assert(DPrintTypeID_Count < 64, "Exceeded number of dprint types");
 
 
-// We need to set thw wpos, rpos pointers to 0 in the beginning of the kernel startup
+// We need to set the wpos, rpos pointers to 0 in the beginning of the kernel startup
 // Because there's no mechanism (known to me) to initialize values at fixed mem locations in kernel code,
 // in order to initialize the pointers in the buffers we use a trick with print server writing
 // a magic value to a fixed location, we look for it on device, and only if it's present we initialize
 // the read and write ptrs to 0 in DebugPrinter() constructor. This check is actually done every time
 // a DebugPrinter() object is created (which can be many times), but results in resetting the pointers
 // only once.
-constexpr int DEBUG_PRINT_SERVER_STARTING_MAGIC = 0x12341234;
-constexpr int DEBUG_PRINT_SERVER_DISABLED_MAGIC = 0x23455432;
+// These magic values must not be equal to any real wpos/rpos values.
+constexpr uint32_t DEBUG_PRINT_SERVER_STARTING_MAGIC = 0x98989898;
+constexpr uint32_t DEBUG_PRINT_SERVER_DISABLED_MAGIC = 0xf8f8f8f8;
 
 // In case a single argument to operator << (such as a string) is larger than the buffer size
 // (making it impossible to print) we will instead print this message.
 constexpr const char* debug_print_overflow_error_message = "*** INTERNAL DEBUG PRINT BUFFER OVERFLOW ***\n\n";
 
-#define ATTR_PACK   __attribute__((packed))
+#define ATTR_PACK __attribute__((packed))
 
 struct DebugPrintMemLayout {
     struct Aux {
