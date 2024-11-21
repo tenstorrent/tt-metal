597ac0f651d2c956c2959147524883fb57d764d5
Nathan Sidwell <nsidwell@tenstorrent.com>
2024-11-21 16:11:36 +0000

    #0: unroll wzerorange

--- tt_metal/hw/toolchain/substitutes.cpp
+++ tt_metal/hw/toolchain/substitutes.cpp
@@ -14,11 +14,23 @@ extern "C" void exit(int ec) {
 }
 
 extern "C" void wzerorange(uint32_t *start, uint32_t *end) {
+    // manually unrolled 4 times.
+    start += 4;
 #pragma GCC unroll 0
-    while (start != end) {
-        *start = 0;
+    while (start <= end) {
+        start[-4] = start[-3] = start[-2] = start[-1] = 0;
         // Prevent optimizer considering this loop equivalent to
-        // memset (start, 0, end - start) -- that's code bloat.
-        asm inline("addi %0,%0,%1" : "+r"(start) : "i"(sizeof(*start)));
+        // memset (start, 0, (end - start) * sizeof (*start)) -- that's code bloat.
+        asm inline("addi %0,%0,4 * %1" : "+r"(start) : "i"(sizeof(*start)));
     }
+    // There are 0, 1, 2 or 3 words of residue.
+    // We get better code layout expecting the conditions to be true.
+    start -= 2;
+    if (__builtin_expect(start <= end, true)) {
+        start[-2] = start[-1] = 0;
+        start += 2;
+    }
+    start -= 1;
+    if (__builtin_expect(start <= end, true))
+        start[-1] = 0;
 }
