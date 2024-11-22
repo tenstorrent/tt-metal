c634e013b2827f75cad866490d1760f0a6d55b6d
Allan Liu <aliu@tenstorrent.com>
2024-11-22 16:28:54 +0000

    #13844: add initial bringup of routing table, control plane, mesh graph

---
+++ tests/tt_metal/tt_fabric/fabric_router/test_routing_tables.cpp
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <memory>
+#include "fabric_fixture.hpp"
+#include "tt_fabric/control_plane.hpp"
+#include "tt_fabric/mesh_graph.hpp"
+#include "tt_fabric/routing_table_generator.hpp"
+
+namespace tt::tt_fabric {
+namespace fabric_router_tests {
+
+TEST_F(ControlPlaneFixture, TestTGControlPlaneInit) {
+  const std::filesystem::path tg_mesh_graph_desc_path = std::filesystem::path(tt::llrt::OptionsG.get_root_dir()) / "tt_fabric/mesh_graph_descriptors/tg_mesh_graph_descriptor.yaml";
+  auto control_plane = std::make_unique<ControlPlane>(tg_mesh_graph_desc_path.string());
+}
+
+TEST_F(ControlPlaneFixture, TestTGMeshGraphInit) {
+  const std::filesystem::path tg_mesh_graph_desc_path = std::filesystem::path(tt::llrt::OptionsG.get_root_dir()) / "tt_fabric/mesh_graph_descriptors/tg_mesh_graph_descriptor.yaml";
+  std::unique_ptr<MeshGraph> mesh_graph_desc = std::make_unique<MeshGraph>(tg_mesh_graph_desc_path.string());
+}
+}  // namespace fabric_router_tests
+}  // namespace tt::tt_fabric
