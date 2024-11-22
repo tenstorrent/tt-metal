1627c8334b5264ecafc06b085cc260940fceb527
Sankar Manoj <smanoj@tenstorrent.com>
2024-11-22 16:57:27 +0000

    #0: Changed defaults for ttnn.GetComputeKernelConfig

--- ttnn/ttnn/device.py
+++ ttnn/ttnn/device.py
@@ -150,7 +150,7 @@ pad_to_tile_shape = ttnn._ttnn.device.pad_to_tile_shape
 
 
 def GetComputeKernelConfig(
-    math_fidelity=ttnn.MathFidelity.LoFi,
+    math_fidelity=ttnn.MathFidelity.HiFi4,
     math_approx_mode=True,
     fp32_dest_acc_en=False,
     packer_l1_acc=False,
