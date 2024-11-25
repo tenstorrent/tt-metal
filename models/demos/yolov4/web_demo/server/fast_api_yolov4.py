8007cf93233efd25ba19ea7726b7dbd3737b1632
Dalar Vartanians <dvartanians@tenstorrent.com>
2024-11-25 23:46:07 +0000

    #0: move back some post processing from client to sever for speed-up

--- models/demos/yolov4/web_demo/server/fast_api_yolov4.py
+++ models/demos/yolov4/web_demo/server/fast_api_yolov4.py
@@ -40,10 +40,114 @@ async def shutdown():
     model.release_yolov4_trace_2cqs_inference()
 
 
-def process_request(output):
-    # Cast tensors to float32 before converting to NumPy
-    output_serializable = [tensor.to(torch.float32).numpy().tolist() for tensor in output]
-    return output_serializable
+def process_output(output):
+    output = output[0]
+    output = [element.item() for element in output]
+    return [output]
+
+
+def post_processing(img, conf_thresh, nms_thresh, output):
+    box_array = output[0]
+    confs = output[1]
+
+    t1 = time.time()
+
+    box_array = np.array(box_array.to(torch.float32))
+    confs = np.array(confs.to(torch.float32))
+
+    num_classes = confs.shape[2]
+
+    # [batch, num, 4]
+    box_array = box_array[:, :, 0]
+
+    # [batch, num, num_classes] --> [batch, num]
+    max_conf = np.max(confs, axis=2)
+    max_id = np.argmax(confs, axis=2)
+
+    t2 = time.time()
+
+    bboxes_batch = []
+    for i in range(box_array.shape[0]):
+        argwhere = max_conf[i] > conf_thresh
+        l_box_array = box_array[i, argwhere, :]
+        l_max_conf = max_conf[i, argwhere]
+        l_max_id = max_id[i, argwhere]
+
+        bboxes = []
+        # nms for each class
+        for j in range(num_classes):
+            cls_argwhere = l_max_id == j
+            ll_box_array = l_box_array[cls_argwhere, :]
+            ll_max_conf = l_max_conf[cls_argwhere]
+            ll_max_id = l_max_id[cls_argwhere]
+
+            keep = nms_cpu(ll_box_array, ll_max_conf, nms_thresh)
+
+            if keep.size > 0:
+                ll_box_array = ll_box_array[keep, :]
+                ll_max_conf = ll_max_conf[keep]
+                ll_max_id = ll_max_id[keep]
+
+                for k in range(ll_box_array.shape[0]):
+                    bboxes.append(
+                        [
+                            ll_box_array[k, 0],
+                            ll_box_array[k, 1],
+                            ll_box_array[k, 2],
+                            ll_box_array[k, 3],
+                            ll_max_conf[k],
+                            ll_max_conf[k],
+                            ll_max_id[k],
+                        ]
+                    )
+
+        bboxes_batch.append(bboxes)
+
+    t3 = time.time()
+
+    print("-----------------------------------")
+    print("       max and argmax : %f" % (t2 - t1))
+    print("                  nms : %f" % (t3 - t2))
+    print("Post processing total : %f" % (t3 - t1))
+    print("-----------------------------------")
+
+    return bboxes_batch
+
+
+def nms_cpu(boxes, confs, nms_thresh=0.5, min_mode=False):
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+
+    areas = (x2 - x1) * (y2 - y1)
+    order = confs.argsort()[::-1]
+
+    keep = []
+    while order.size > 0:
+        idx_self = order[0]
+        idx_other = order[1:]
+
+        keep.append(idx_self)
+
+        xx1 = np.maximum(x1[idx_self], x1[idx_other])
+        yy1 = np.maximum(y1[idx_self], y1[idx_other])
+        xx2 = np.minimum(x2[idx_self], x2[idx_other])
+        yy2 = np.minimum(y2[idx_self], y2[idx_other])
+
+        w = np.maximum(0.0, xx2 - xx1)
+        h = np.maximum(0.0, yy2 - yy1)
+        inter = w * h
+
+        if min_mode:
+            over = inter / np.minimum(areas[order[0]], areas[order[1:]])
+        else:
+            over = inter / (areas[order[0]] + areas[order[1:]] - inter)
+
+        inds = np.where(over <= nms_thresh)[0]
+        order = order[inds + 1]
+
+    return np.array(keep)
 
 
 @app.post("/objdetection_v2")
@@ -64,8 +168,13 @@ async def objdetection_v2(file: UploadFile = File(...)):
     response = model.run_traced_inference(image)
     t2 = time.time()
     print("the inference on the sever side took: ", t2 - t1)
-    # Convert response tensors to JSON-serializable format
-    output = process_request(response)
+    conf_thresh = 0.6
+    nms_thresh = 0.5
+
+    boxes = post_processing(image, conf_thresh, nms_thresh, response)
+    output = boxes[0]
+
+    output = process_output(output)
     t3 = time.time()
-    print("the processing of the response to convert to lists takes: (this is a requirement per streamlit) ", t3 - t2)
+    print("the post processing to get the boexes took: ", t3 - t2)
     return output
