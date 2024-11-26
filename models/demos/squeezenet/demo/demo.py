bb7636a12d65854d10c425e1b71893d174ae2747
vguduruTT <venkatesh.guduru@multicorewareinc.com>
2024-11-26 06:13:51 +0000

    #14786: pcc drops when ttnn maxpool is introduced and drop persists with torch maxpool

--- models/demos/squeezenet/demo/demo.py
+++ models/demos/squeezenet/demo/demo.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+# SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
 
 # SPDX-License-Identifier: Apache-2.0
 
@@ -6,8 +6,14 @@
 import pytest
 import torch
 import ttnn
-from PIL import Image
-from torchvision import models, transforms
+from torchvision import models
+from models.utility_functions import (
+    disable_compilation_reports,
+    disable_persistent_kernel_cache,
+    enable_persistent_kernel_cache,
+    profiler,
+)
+from models.demos.squeezenet.demo_utils import get_data_loader, get_batch, preprocess
 from loguru import logger
 import torch.nn as nn
 from ttnn.model_preprocessing import preprocess_model_parameters
@@ -23,58 +29,74 @@ def custom_preprocessor(model, name):
     return parameters
 
 
-@pytest.mark.parametrize("device_params", [{"l1_small_size": 24576}], indirect=True)
-def run_demo_dataset(batch_size, device, iterations):
+def run_squeezenet_imagenet_inference(batch_size, iterations, imagenet_label_dict, model_location_generator, device):
+    disable_persistent_kernel_cache()
+    disable_compilation_reports()
+    profiler.clear()
+
     torch_squeezenet = models.squeezenet1_0(weights=models.SqueezeNet1_0_Weights.IMAGENET1K_V1)
+    torch_squeezenet.to(torch.bfloat16)
     torch_squeezenet.eval()
     parameters = preprocess_model_parameters(
         initialize_model=lambda: torch_squeezenet, custom_preprocessor=custom_preprocessor, device=None
     )
-    filename = "models/demos/squeezenet/demo/dog_image.jpeg"
-    input_image = Image.open(filename)
-    preprocess = transforms.Compose(
-        [
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ]
-    )
-    input_tensor = preprocess(input_image)
-    input_batch = input_tensor.unsqueeze(0)
-    input_batch = input_batch.repeat(batch_size, 1, 1, 1)
-    torch_out = torch_squeezenet(input_batch)
+    logger.info("ImageNet-1k validation Dataset")
+    input_loc = str(model_location_generator("ImageNet_data"))
+    data_loader = get_data_loader(input_loc, batch_size, iterations)
     correct = 0
-    for iters in range(iterations):
-        tt_input = ttnn.from_torch(torch.permute(input_batch, (0, 2, 3, 1)))
-        tt_out = tt_squeezenet(device=device, parameters=parameters, tt_input=tt_input)
-        tt_out_torch = ttnn.to_torch(tt_out)
-        _, torch_predicted = torch.max(torch_out.data, -1)
-        _, ttnn_predicted = torch.max(tt_out_torch.data, -1)
+    torch_ttnn_correct = 0
+    torch_correct = 0
+    for iter in range(iterations):
+        predictions = []
+        torch_predictions = []
+        inputs, labels = get_batch(data_loader)
+        torch_outputs = torch_squeezenet(inputs)
+        tt_batched_input_tensor = ttnn.from_torch(
+            inputs.permute(0, 2, 3, 1),
+            ttnn.bfloat16,
+        )
+        tt_output = tt_squeezenet(device=device, parameters=parameters, tt_input=tt_batched_input_tensor)
+        tt_output = ttnn.to_torch(tt_output)
+        prediction = tt_output.argmax(dim=-1)
+        torch_prediction = torch_outputs.argmax(dim=-1)
         for i in range(batch_size):
-            logger.info(f"Iter: {iters} Sample {i}:")
-            logger.info(f"torch Label: {torch_predicted[i]}")
-            logger.info(f"Predicted Label: {ttnn_predicted[i]}")
+            if prediction.dim() == 0:
+                predictions.append(imagenet_label_dict[prediction.item()])
+            else:
+                predictions.append(imagenet_label_dict[prediction[i].item()])
 
-            if torch_predicted[i] == ttnn_predicted[i]:
+            if torch_prediction.dim() == 0:
+                torch_predictions.append(imagenet_label_dict[torch_prediction.item()])
+            else:
+                torch_predictions.append(imagenet_label_dict[torch_prediction[i].item()])
+
+            logger.info(
+                f"Iter: {iter} Sample: {i} - Expected Label: {imagenet_label_dict[labels[i]]} -- \n Torch Predicted label:{predictions[-1]} \tPredicted Label: {predictions[-1]}"
+            )
+            if imagenet_label_dict[labels[i]] == predictions[-1]:
                 correct += 1
+            if imagenet_label_dict[labels[i]] == torch_predictions[-1]:
+                torch_correct += 1
+            if predictions[-1] == torch_predictions[-1]:
+                torch_ttnn_correct += 1
 
+        del tt_output, tt_batched_input_tensor, inputs, labels, predictions
     accuracy = correct / (batch_size * iterations)
-    logger.info(f"Dataset Inference Accuracy for {batch_size}x{iterations} Samples : {accuracy}")
+    torch_accuracy = torch_correct / (batch_size * iterations)
+    torch_ttnn_accuracy = torch_ttnn_correct / (batch_size * iterations)
+
+    logger.info(f"Model SqueezeNet for Image Classification")
+    logger.info(f"TTNN Accuracy for {batch_size}x{iterations} inputs: {accuracy}")
+    logger.info(f"Torch Accuracy for {batch_size}x{iterations} inputs: {torch_accuracy}")
+    logger.info(f"Torch vs TTNN Accuracy for {batch_size}x{iterations} inputs: {torch_ttnn_accuracy}")
 
 
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
-@pytest.mark.parametrize("batch_size", [1])
-@pytest.mark.parametrize("iterations", [1])
-def test_demo_dataset(
-    device,
-    batch_size,
-    iterations,
-    model_location_generator,
-    reset_seeds,
-):
-    return run_demo_dataset(
-        device=device,
-        batch_size=batch_size,
-        iterations=iterations,
+@pytest.mark.parametrize(
+    "batch_size, iterations",
+    ((1, 100),),
+)
+def test_demo_dataset(batch_size, iterations, imagenet_label_dict, model_location_generator, device):
+    return run_squeezenet_imagenet_inference(
+        batch_size, iterations, imagenet_label_dict, model_location_generator, device
     )
--- models/demos/squeezenet/tt/tt_squeezenet.py
+++ models/demos/squeezenet/tt/tt_squeezenet.py
@@ -1,8 +1,5 @@
 # SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
-
 # SPDX-License-Identifier: Apache-2.0
-
-
 import ttnn
 import numpy as np
 import torch
@@ -71,7 +68,6 @@ def tt_Fire(
         in_h=input_tensor.shape[1],
         in_w=input_tensor.shape[2],
     )
-
     expand1x1_weights = parameters.expand1x1.weight
     expand1x1_bias = parameters.expand1x1.bias
     output_tt_tensor_2, out_height_2, out_width_2 = conv(
@@ -88,7 +84,6 @@ def tt_Fire(
         in_h=out_height_1,
         in_w=out_width_1,
     )
-
     expand3x3_weights = parameters.expand3x3.weight
     expand3x3_bias = parameters.expand3x3.bias
     output_tt_tensor_3, out_height_3, out_width_3 = conv(
@@ -105,13 +100,11 @@ def tt_Fire(
         in_h=out_height_1,
         in_w=out_width_1,
     )
-
     output_tt_tensor_2 = ttnn.to_memory_config(output_tt_tensor_2, memory_config=ttnn.L1_MEMORY_CONFIG)
     output_tt_tensor_3 = ttnn.to_memory_config(output_tt_tensor_3, memory_config=ttnn.L1_MEMORY_CONFIG)
     output_tt_tensor_2 = ttnn.to_layout(output_tt_tensor_2, layout=ttnn.ROW_MAJOR_LAYOUT)
     output_tt_tensor_3 = ttnn.to_layout(output_tt_tensor_3, layout=ttnn.ROW_MAJOR_LAYOUT)
     final_output = ttnn.concat([output_tt_tensor_2, output_tt_tensor_3], dim=3)
-
     final_output_reshaped = ttnn.reshape(
         final_output, (output_tt_tensor_3.shape[0], out_height_3, out_width_3, expand3x3_planes + expand1x1_planes)
     )
@@ -119,10 +112,11 @@ def tt_Fire(
     return final_output_reshaped
 
 
-def tt_squeezenet(device, parameters, tt_input, dtype=ttnn.bfloat16, activation="relu"):
+def tt_squeezenet(device, parameters, tt_input, dtype=ttnn.bfloat16, activation="relu", num_classes=1000):
+    max_pool_in_tt = False  # ceilmode issue
     conv_1_weights = parameters.features[0].weight
     conv_1_bias = parameters.features[0].bias
-
+    batch_size = tt_input.shape[0]
     output_tt, out_height_1, out_width_1 = conv(
         activation="relu",
         input_tensor=tt_input,
@@ -137,56 +131,105 @@ def tt_squeezenet(device, parameters, tt_input, dtype=ttnn.bfloat16, activation=
         in_h=tt_input.shape[1],
         in_w=tt_input.shape[2],
     )
-    output_tt = ttnn.to_layout(output_tt, layout=ttnn.ROW_MAJOR_LAYOUT)
-    output_tt = ttnn.reshape(output_tt, (1, 109, 109, 96))
-    torch_tensor_1 = ttnn.to_torch(output_tt)
-    torch_tensor_1 = torch.permute(torch_tensor_1, (0, 3, 1, 2))
-    torch_pool = torch.nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True)  # torch op
-    torch_out = torch_pool(torch_tensor_1)
-    tt_input_2 = ttnn.from_torch(torch_out, dtype=ttnn.bfloat16, device=device)
-    tt_input_2 = ttnn.permute(tt_input_2, (0, 2, 3, 1))
-    tt_input_2 = ttnn.from_device(tt_input_2)
+    output_tt_reshaped = ttnn.reshape(
+        output_tt, (1, 1, (output_tt.shape[0] * output_tt.shape[1] * output_tt.shape[2]), output_tt.shape[3])
+    )
+    max_pool_in_tt_1 = False  # PCC Drops once tt_maxpool is enabled and this drop persists after it is disabled
+    if max_pool_in_tt_1:
+        out_pool = ttnn.max_pool2d(
+            input_tensor=output_tt,
+            batch_size=batch_size,
+            input_h=out_height_1,
+            input_w=out_width_1,
+            channels=output_tt.shape[3],
+            kernel_size=[3, 3],
+            stride=[2, 2],
+            padding=[0, 0],
+            dilation=[1, 1],
+            memory_config=None,
+            applied_shard_scheme=None,
+        )
+        out_pool_reshaped = ttnn.reshape(out_pool, (batch_size, 54, 54, out_pool.shape[3]))
+        tt_input_2 = ttnn.from_device(out_pool_reshaped)
+    else:
+        torch_tensor_1 = ttnn.to_torch(output_tt)
+        torch_tensor_1 = torch.reshape(torch_tensor_1, (batch_size, out_height_1, out_width_1, output_tt.shape[3]))
+        torch_tensor_1 = torch.permute(torch_tensor_1, (0, 3, 1, 2))
+        torch_pool = torch.nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True)  # torch op
+        torch_out = torch_pool(torch_tensor_1)
+        tt_input_2 = ttnn.from_torch(torch_out, dtype=ttnn.bfloat16, device=device)
+        tt_input_2 = ttnn.permute(tt_input_2, (0, 2, 3, 1))
+        tt_input_2 = ttnn.from_device(tt_input_2)
     tt_int_tensor_3 = tt_Fire(
         96, 16, 64, 64, input_tensor=tt_input_2, device=device, parameters=parameters["features"][3]
     )
-
     tt_int_tensor_4 = tt_Fire(
         128, 16, 64, 64, input_tensor=tt_int_tensor_3, device=device, parameters=parameters["features"][4]
     )
-
     tt_int_tensor_5 = tt_Fire(
         128, 32, 128, 128, input_tensor=tt_int_tensor_4, device=device, parameters=parameters["features"][5]
     )
-    tt_int_tensor_5 = ttnn.permute(tt_int_tensor_5, (0, 3, 1, 2))
-    torch_in = ttnn.to_torch(tt_int_tensor_5)
-    torch_out_2 = torch_pool(torch_in)
-    torch_out_permute_2 = torch.permute(torch_out_2, (0, 2, 3, 1))
-    tt_input_3 = ttnn.from_torch(torch_out_permute_2, dtype=ttnn.bfloat16)
+
+    if max_pool_in_tt:  # enable when ceil_mode issue is fixed(#15039)
+        tt_int_tensor_5_reshaped = ttnn.reshape(
+            tt_int_tensor_5,
+            (
+                1,
+                1,
+                (tt_int_tensor_5.shape[0] * tt_int_tensor_5.shape[1] * tt_int_tensor_5.shape[2]),
+                tt_int_tensor_5.shape[3],
+            ),
+        )
+        out_pool_2 = ttnn.max_pool2d(
+            input_tensor=tt_int_tensor_5_reshaped,
+            batch_size=batch_size,
+            input_h=tt_int_tensor_5.shape[1],
+            input_w=tt_int_tensor_5.shape[2],
+            channels=tt_int_tensor_5_reshaped.shape[3],
+            kernel_size=[3, 3],
+            stride=[2, 2],
+            padding=[0, 0],
+            dilation=[1, 1],
+            memory_config=None,
+            applied_shard_scheme=None,
+        )
+        out_pool_2_reshaped = ttnn.reshape(out_pool_2, (batch_size, 27, 27, out_pool_2.shape[3]))
+        tt_input_3 = ttnn.from_device(out_pool_2_reshaped)
+    else:
+        tt_int_tensor_5 = ttnn.permute(tt_int_tensor_5, (0, 3, 1, 2))
+        torch_in = ttnn.to_torch(tt_int_tensor_5)
+        torch_pool = torch.nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True)  # torch op
+        torch_out_2 = torch_pool(torch_in)
+        torch_out_permute_2 = torch.permute(torch_out_2, (0, 2, 3, 1))
+        tt_input_3 = ttnn.from_torch(torch_out_permute_2, dtype=ttnn.bfloat16)
     tt_int_tensor_6 = tt_Fire(
         256, 32, 128, 128, input_tensor=tt_input_3, device=device, parameters=parameters["features"][7]
     )
-
     tt_int_tensor_7 = tt_Fire(
         256, 48, 192, 192, input_tensor=tt_int_tensor_6, device=device, parameters=parameters["features"][8]
     )
-
     tt_int_tensor_8 = tt_Fire(
         384, 48, 192, 192, input_tensor=tt_int_tensor_7, device=device, parameters=parameters["features"][9]
     )
-
     tt_int_tensor_9 = tt_Fire(
         384, 64, 256, 256, input_tensor=tt_int_tensor_8, device=device, parameters=parameters["features"][10]
     )
-
-    # pool 3
     tt_int_tensor_9 = ttnn.to_layout(tt_int_tensor_9, layout=ttnn.ROW_MAJOR_LAYOUT)
-    tt_int_tensor_9 = ttnn.reshape(tt_int_tensor_9, (1, 1, (27 * 27), 512))
-    out_pool = ttnn.max_pool2d(
-        input_tensor=tt_int_tensor_9,
-        batch_size=1,
-        input_h=27,
-        input_w=27,
-        channels=512,
+    tt_int_tensor_9_reshaped = ttnn.reshape(
+        tt_int_tensor_9,
+        (
+            1,
+            1,
+            (tt_int_tensor_9.shape[0] * tt_int_tensor_9.shape[1] * tt_int_tensor_9.shape[2]),
+            tt_int_tensor_9.shape[3],
+        ),
+    )
+    out_pool_3 = ttnn.max_pool2d(
+        input_tensor=tt_int_tensor_9_reshaped,
+        batch_size=batch_size,
+        input_h=tt_int_tensor_9.shape[1],
+        input_w=tt_int_tensor_9.shape[2],
+        channels=tt_int_tensor_9.shape[3],
         kernel_size=[3, 3],
         stride=[2, 2],
         padding=[0, 0],
@@ -194,16 +237,13 @@ def tt_squeezenet(device, parameters, tt_input, dtype=ttnn.bfloat16, activation=
         memory_config=None,
         applied_shard_scheme=None,
     )
-
-    out_pool = ttnn.from_device(out_pool)  # bought outside device due to seg fault
-    out_pool_reshaped = ttnn.reshape(out_pool, (1, 13, 13, 512))
-    out_pool_reshaped = ttnn.to_device(out_pool_reshaped, device=device)
+    out_pool_reshaped = ttnn.reshape(out_pool_3, (1, 13, 13, 512))
+    out_pool_reshaped = ttnn.from_device(out_pool_reshaped)
     tt_int_tensor_10 = tt_Fire(
         512, 64, 256, 256, input_tensor=out_pool_reshaped, device=device, parameters=parameters["features"][12]
     )
     classifier_w = parameters.classifier[1].weight
     classifier_b = parameters.classifier[1].bias
-
     output_tt_tensor_11, out_height_11, out_width_11 = conv(
         activation="relu",
         input_tensor=tt_int_tensor_10,
@@ -218,11 +258,10 @@ def tt_squeezenet(device, parameters, tt_input, dtype=ttnn.bfloat16, activation=
         in_h=tt_int_tensor_10.shape[1],
         in_w=tt_int_tensor_10.shape[2],
     )
-
     output_tt_tensor_11 = ttnn.to_memory_config(output_tt_tensor_11, memory_config=ttnn.L1_MEMORY_CONFIG)
-    output_tt_tensor_11 = ttnn.reshape(output_tt_tensor_11, (1, 13, 13, 1000))
+    output_tt_tensor_11 = ttnn.reshape(output_tt_tensor_11, (batch_size, out_height_11, out_width_11, num_classes))
     output_tensor = ttnn.global_avg_pool2d(output_tt_tensor_11)
-
-    out_torch = ttnn.to_torch(output_tensor).squeeze(0).squeeze(0)
-    out_tt = ttnn.from_torch(out_torch, dtype=ttnn.bfloat16)
-    return out_tt
+    output_tensor = ttnn.to_layout(output_tensor, layout=ttnn.ROW_MAJOR_LAYOUT)
+    output_tensor = ttnn.squeeze(output_tensor, dim=1)
+    output_tensor = ttnn.squeeze(output_tensor, dim=1)
+    return output_tensor
