02959242b74e2a3d7b19f228eb41324b106fbc5a
Sankar Manoj <smanoj@tenstorrent.com>
2024-11-27 11:34:32 +0000

    #0: CB Size Calc for Width Sharded Conv2d

--- ttnn/cpp/ttnn/operations/conv/conv2d/device/conv2d_op.hpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/device/conv2d_op.hpp
@@ -107,7 +107,8 @@ struct OptimizedConvNew {
             enable_subblock_padding(enable_subblock_padding),
             use_non_tile_height(use_non_tile_height) {}
 
-    uint32_t estimate_L1_usage();
+    std::pair<uint32_t,uint32_t> estimate_L1_usage(DataType input_dtype, DataType weights_dtype, DataType output_dtype, DataType accum_dtype, bool enable_bias) const;
+
     void validate(const std::vector<Tensor>& input_tensors, const std::vector<std::optional<const Tensor>>& optional_input_tensors) const;
     std::vector<TensorSpec> compute_output_specs(const std::vector<Tensor>& input_tensors) const;
     operation::ProgramWithCallbacks create_program(const std::vector<Tensor>& input_tensors, const std::vector<std::optional<const Tensor>>& optional_input_tensors, std::vector<Tensor> &output_tensors) const;
