46a47b690031beb958bd4597d165331ff29be207
Jay Kruer <jkruer@tenstorrent.com>
2024-11-27 23:04:11 +0000

    #0: Add 5d tilize tests

--- tests/tt_eager/python_api_testing/unit_testing/misc/test_tilize_test.py
+++ tests/tt_eager/python_api_testing/unit_testing/misc/test_tilize_test.py
@@ -10,14 +10,14 @@ from models.utility_functions import tilize
 
 
 @pytest.mark.parametrize(
-    "nb, nc, nh, nw",
+    "shape",
     (
-        (5, 2, 4, 8),
-        (5, 2, 4, 7),
+        [5, 2, 4, 8],
+        [5, 2, 4, 7],
         ## resnet shapes
-        (1, 1, 784, 2),
-        (8, 1, 2, 64),
-        (1, 1, 1, 64),
+        [1, 1, 784, 2],
+        [8, 1, 2, 64],
+        [1, 1, 1, 64],
     ),
 )
 @pytest.mark.parametrize(
@@ -27,9 +27,9 @@ from models.utility_functions import tilize
         True,
     ),
 )
-def test_run_tilize_test(nb, nc, nh, nw, multicore, device):
-    nt = nb * nc * nh * nw
-    shape = [nb, nc, 32 * nh, 32 * nw]
+def test_run_tilize_test(shape, multicore, device):
+    shape[-1] *= 32
+    shape[-2] *= 32
 
     inp = torch.rand(*shape).bfloat16()
 
@@ -43,3 +43,37 @@ def test_run_tilize_test(nb, nc, nh, nw, multicore, device):
     tilized_inp = tilize(inp)
     passing = torch.equal(tilized_inp, c)
     assert passing
+
+
+@pytest.mark.parametrize(
+    "shape",
+    (
+        [1, 1, 1, 5, 1],
+        [1, 1, 1, 4, 2],
+        [1, 1, 1, 3, 3],
+        [1, 1, 1, 2, 4],
+        [1, 1, 1, 1, 5],
+        [1, 2, 3, 2, 1],
+    ),
+)
+@pytest.mark.parametrize(
+    "multicore",
+    (
+        False,
+        True,
+    ),
+)
+def test_tilize_5d(shape, multicore, device):
+    # tests that host -> device -> tilize -> untilize -> host is a no-op
+    shape[-1] *= 32
+    shape[-2] *= 32
+
+    inp = torch.rand(*shape).bfloat16()
+    a = ttnn.Tensor(
+        inp,
+        ttnn.bfloat16,
+    ).to(device)
+    b = ttnn.tilize(a, use_multicore=multicore)
+    c = ttnn.untilize(b)
+    d = c.cpu().to_torch()
+    assert torch.equal(inp, d)
