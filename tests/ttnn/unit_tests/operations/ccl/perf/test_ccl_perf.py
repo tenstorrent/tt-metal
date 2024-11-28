455c63649c00326c677cc809ba940972e536032f
Aswin Zayasankaran <156493059+Aswinmcw@users.noreply.github.com>
2024-11-28 12:36:29 +0530

    Add reduce scatter perf to tg (#15160)
    ### Ticket
    #14406 
    
    Adds reduce scatter perf to tg
    
    <img width="1371" alt="Screenshot 2024-11-18 at 4 34 41 PM"
    src="https://github.com/user-attachments/assets/2bea60f3-95cb-41ce-9710-452b5b0feb08">
    
    <img width="1299" alt="Screenshot 2024-11-18 at 4 35 03 PM"
    src="https://github.com/user-attachments/assets/f831a6ad-f73b-4851-82d4-b0360b3f8bdc">
    
    https://github.com/tenstorrent/tt-metal/actions/runs/12052228747
    
    ### Checklist
    - [ ] Post commit CI passes
    - [ ] Blackhole Post commit (if applicable)
    - [ ] Model regression CI testing passes (if applicable)
    - [ ] Device performance regression CI testing passes (if applicable)
    - [ ] New/Existing tests provide coverage for changes

--- tests/ttnn/unit_tests/operations/ccl/perf/test_ccl_perf.py
+++ tests/ttnn/unit_tests/operations/ccl/perf/test_ccl_perf.py
@@ -15,6 +15,9 @@ from tests.ttnn.unit_tests.operations.ccl.test_reduce_scatter_post_commit import
 from tests.ttnn.unit_tests.operations.ccl.test_all_gather_TG_post_commit import (
     run_line_all_gather_on_TG_with_mesh_tensor_along_rows,
 )
+from tests.ttnn.unit_tests.operations.ccl.test_reduce_scatter_TG_nightly import (
+    run_line_reduce_scatter_on_TG_with_mesh_tensor_along_rows,
+)
 
 
 @skip_for_grayskull("Requires eth connected devices to run")
@@ -329,6 +332,71 @@ def test_all_gather_on_tg(
         enable_async=enable_async,
         num_iters=num_iters,
         num_all_gather_instances=replication_factor,
+        cluster_axis=1,
+        trace_mode=True,
+    )
+
+
+@skip_for_grayskull("Requires eth connected devices to run")
+@pytest.mark.parametrize(
+    "num_devices, num_links, per_chip_output_shape, dim, layout",
+    [
+        (4, 2, [1, 4, 32, 2304], 1, ttnn.TILE_LAYOUT),
+        (4, 2, [1, 4, 64, 2304], 1, ttnn.TILE_LAYOUT),
+        (4, 2, [1, 4, 64, 6656], 1, ttnn.TILE_LAYOUT),
+    ],
+)
+@pytest.mark.parametrize(
+    "input_dtype",
+    [
+        ttnn.bfloat16,
+    ],
+)
+@pytest.mark.parametrize(
+    "buffer_type",
+    [
+        ttnn.BufferType.DRAM,
+        ttnn.BufferType.L1,
+    ],
+)
+@pytest.mark.parametrize("replication_factor", [8])
+@pytest.mark.parametrize("enable_async", [True])
+@pytest.mark.parametrize("num_iters", [20])
+@pytest.mark.parametrize("mesh_device", [pytest.param((8, 4), id="8x4_grid")], indirect=True)
+@pytest.mark.parametrize("math_op", [ttnn.ReduceType.Sum])
+@pytest.mark.parametrize("device_params", [{"trace_region_size": 10281600}], indirect=True)
+def test_reduce_scatter_on_tg(
+    mesh_device,
+    num_devices,
+    per_chip_output_shape,
+    dim,
+    num_links,
+    math_op,
+    input_dtype,
+    layout,
+    buffer_type,
+    use_program_cache,
+    function_level_defaults,
+    enable_async,
+    replication_factor,
+    num_iters,
+):
+    run_line_reduce_scatter_on_TG_with_mesh_tensor_along_rows(
+        mesh_device,
+        num_devices,
+        per_chip_output_shape,
+        ttnn.TensorMemoryLayout.INTERLEAVED,
+        dim,
+        num_links,
+        math_op,
+        input_dtype,
+        layout,
+        buffer_type,
+        use_program_cache,
+        function_level_defaults,
+        enable_async=enable_async,
+        num_iters=num_iters,
+        num_reduce_scatter_instances=replication_factor,
         cluster_axis=1,
         trace_mode=True,
     )
