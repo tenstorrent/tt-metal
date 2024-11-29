568d1c8f135b2b820dde243a3669fc19c11ec03e
Sankar Manoj <smanoj@tenstorrent.com>
2024-11-29 12:08:11 +0000

    #0: Fixed Stable Diffusion

--- models/demos/wormhole/stable_diffusion/tt/ttnn_functional_resnetblock2d_new_conv.py
+++ models/demos/wormhole/stable_diffusion/tt/ttnn_functional_resnetblock2d_new_conv.py
@@ -537,7 +537,7 @@ class resnetBlock2D:
                     transpose_shards=False,
                     reshard_if_not_optimal=False,
                 )
-                compute_config = self.ttnn.init_device_compute_kernel_config(
+                compute_config = ttnn.init_device_compute_kernel_config(
                     self.device.arch(),
                     math_fidelity=ttnn.MathFidelity.LoFi,
                     math_approx_mode=True,
