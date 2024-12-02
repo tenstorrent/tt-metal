4c6f5282c82f2a004bf5e4fd705266fad2245ae3
Aswinmcw <azayasankaran@tenstorrent.com>
2024-12-02 12:30:32 +0000

    Add reduce scatter to N300 Perf

--- tests/scripts/run_tests.sh
+++ tests/scripts/run_tests.sh
@@ -179,6 +179,23 @@ run_ccl_microbenchmarks_pipeline_tests() {
     if [[ $fail -ne 0 ]]; then
       exit 1
     fi
+
+    # Record the start time
+    fail=0
+    start_time=$(date +%s)
+
+    echo "LOG_METAL: Running run_n300_ccl_reduce_scatter_perf_tests"
+
+    tests/ttnn/unit_tests/operations/ccl/perf/run_reduce_scatter_profile.sh -t n300
+    fail+=$?
+
+    # Record the end time
+    end_time=$(date +%s)
+    duration=$((end_time - start_time))
+    echo "LOG_METAL: run_n300_ccl_reduce_scatter_perf_tests $duration seconds to complete"
+    if [[ $fail -ne 0 ]]; then
+      exit 1
+    fi
 }
 
 run_ttnn_sweeps_pipeline_tests() {
