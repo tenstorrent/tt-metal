9da1b5860ee27ab02f25041df9fd33950a82841b
Oleg Milyutin <omilyutin@tenstorrent.com>
2024-12-02 20:26:49 +0000

    Update overloaded doc example

--- tt_metal/tt_stl/overloaded.hpp
+++ tt_metal/tt_stl/overloaded.hpp
@@ -11,22 +11,18 @@ namespace tt::stl {
 // `overloaded` allows to combine multiple lambdas into a single object with the overloaded `operator()`.
 // This is useful for creating visitor objects for using in `std::visit`, for example:
 //
-// void f(const std::variant<int, std::string>& v) {
-//   std::visit(overloaded {
-//     [](int i) { std::cout << "Got an int: " << i << std::endl; },
-//     [](const std::string& str) { std::cout << "Got a string: " << str << std::endl; },
-//   }, v);
-// }
 //
+// class A, B, C;
+// std::variant<A, B, C> my_variant;
+// ...
 //
-// Generic lambdas can also be used to simplify handling of variant types:
-//
-// void f(const std::variant<std::vector<int>, int, std::string>& v) {
-//   std::visit(overloaded {
-//     [](const std::vector<int>& vec) { std::cout << "Got a vector of size: " << vec.size() << std::endl; },
-//     [](const auto& other) { std::cout << "Got something else: " << other << std::endl; },
-//   }, v);
-// }
+// std::visit(
+//     tt::stl::overloaded{
+//         [](const A&) { /*Process A...*/ },
+//         [](const B&) { /*Process B...*/ },
+//         [](const C&) { /*Process C...*/ },
+//     },
+//     my_variant);
 //
 //
 
