72f2d5c53af5e82cda229bc2b196af6dd0f577ee
Juan Camilo Vega <jvega@tenstorrent.com>
2024-12-03 18:43:02 +0000

    #15269: added packet form of noc_async_read and write

--- ttnn/cpp/ttnn/operations/data_movement/common/kernels/common.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/common/kernels/common.hpp
@@ -15,7 +15,33 @@
 
 namespace tt::data_movement::common {
 
-template <bool guaranteed_16B_alligned, bool copy_async, bool use_read_datamover>
+#define max_packet_size 8192
+
+template <uint32_t max_transfer_size, bool only_reads>
+FORCE_INLINE void enhanced_noc_async_read(
+    const uint64_t src_noc_addr, const uint32_t dst_l1_addr, const uint32_t bytes) {
+    // If you do not know the max_transfer_size at compile time write 0 to it.
+    // only reads is true if we ONLY use noc_async_read and all calls to tt_memmove have use_read_datamover as True
+    if constexpr (((max_transfer_size < max_packet_size) && (max_transfer_size != 0)) || only_reads) {
+        noc_async_read_one_packet(src_noc_addr, dst_l1_addr, bytes);
+    } else {
+        noc_async_read(src_noc_addr, dst_l1_addr, bytes);
+    }
+}
+
+template <uint32_t max_transfer_size, bool only_writes>
+FORCE_INLINE void enhanced_noc_async_write(
+    const uint32_t src_l1_addr, const uint64_t dst_noc_addr, const uint32_t bytes) {
+    // If you do not know the max_transfer_size at compile time write 0 to it.
+    // only writes is true if we ONLY use noc_async_read and all calls to tt_memmove have use_read_datamover as False
+    if constexpr (((max_transfer_size < max_packet_size) && (max_transfer_size != 0)) || only_writes) {
+        noc_async_write_one_packet(src_l1_addr, dst_noc_addr, bytes);
+    } else {
+        noc_async_write(src_l1_addr, dst_noc_addr, bytes);
+    }
+}
+
+template <bool guaranteed_16B_alligned, bool copy_async, bool use_read_datamover, uint32_t max_transfer_size>
 FORCE_INLINE void tt_memmove(const uint32_t dst_l1_addr, const uint32_t src_l1_addr, const uint32_t bytes) {
     //Function performs a memory copy between two l1 addresses in the local core
     //Uses noc_async_read when possible to copy the data over
@@ -23,13 +49,13 @@ FORCE_INLINE void tt_memmove(const uint32_t dst_l1_addr, const uint32_t src_l1_a
     //Set copy_async to true if you wish to perform the operation asynchronously, in this case you can add a noc_async_read_barrier to synchronize later
     if constexpr (use_read_datamover) {
         if constexpr (guaranteed_16B_alligned) {
-            noc_async_read(get_noc_addr(src_l1_addr), dst_l1_addr, bytes);
+            enhanced_noc_async_read<max_transfer_size, false>(get_noc_addr(src_l1_addr), dst_l1_addr, bytes);
             if constexpr (!copy_async) {
                 noc_async_read_barrier();
             }
         } else {
             if ((dst_l1_addr & OFFSET_16) == (src_l1_addr & OFFSET_16)) {
-                noc_async_read(get_noc_addr(src_l1_addr), dst_l1_addr, bytes);
+                enhanced_noc_async_read<max_transfer_size, false>(get_noc_addr(src_l1_addr), dst_l1_addr, bytes);
                 if constexpr (!copy_async) {
                     noc_async_read_barrier();
                 }
@@ -39,13 +65,13 @@ FORCE_INLINE void tt_memmove(const uint32_t dst_l1_addr, const uint32_t src_l1_a
         }
     } else {
         if constexpr (guaranteed_16B_alligned) {
-            noc_async_write(src_l1_addr, get_noc_addr(dst_l1_addr), bytes);
+            enhanced_noc_async_write<max_transfer_size, false>(src_l1_addr, get_noc_addr(dst_l1_addr), bytes);
             if constexpr (!copy_async) {
                 noc_async_write_barrier();
             }
         } else {
             if ((dst_l1_addr & OFFSET_16) == (src_l1_addr & OFFSET_16)) {
-                noc_async_write(src_l1_addr, get_noc_addr(dst_l1_addr), bytes);
+                enhanced_noc_async_write<max_transfer_size, false>(src_l1_addr, get_noc_addr(dst_l1_addr), bytes);
                 if constexpr (!copy_async) {
                     noc_async_write_barrier();
                 }
--- ttnn/cpp/ttnn/operations/data_movement/reshape_view/device/device/rm_reshape_interleaved.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/reshape_view/device/device/rm_reshape_interleaved.cpp
@@ -56,6 +56,7 @@ void kernel_main() {
     constexpr uint32_t source_page_pow_2 = get_compile_time_arg_val(8);
 #define dest_page_is_pow_2 get_compile_time_arg_val(9) == 1
     constexpr uint32_t dest_page_pow_2 = get_compile_time_arg_val(10);
+
     //Since we need to operate on a grid of cores but sometimes pages don't split properly, if nop then don't use this core
     if (nop == 1)
     {
@@ -92,8 +93,8 @@ void kernel_main() {
     cb_reserve_back(cb_id_in1, 1);
     const uint32_t source_buffer = get_write_ptr(cb_id_in0);
     const uint32_t dest_buffer = get_write_ptr(cb_id_in1);
-    cb_push_back(cb_id_in0, 1);
     cb_push_back(cb_id_in1, 1);
+    cb_push_back(cb_id_in0, 1);
 
     uint64_t dst_noc_addr = get_noc_addr(write_page, d);
     uint64_t write_offset = (dst_noc_addr & OFFSET_16) + write_start_offset;
@@ -106,15 +107,18 @@ void kernel_main() {
 
 #if (src_aligned_to_64 || ((!tensor_is_dram) && src_aligned_to_16))
         //Aligned to 64 bytes or 16 bytes but L1
-        noc_async_read(src_noc_addr, source_buffer, source_page_size_bytes);
+        tt::data_movement::common::enhanced_noc_async_read<source_page_size_bytes, false>(
+            src_noc_addr, source_buffer, source_page_size_bytes);
         read_offset = 0;
 #elif (tensor_is_dram)
         //DDR but not alligned to 64 (potentially also not alligned to 16)
-        noc_async_read(src_noc_addr&MASK_64, source_buffer, source_read_size_bytes);
+        tt::data_movement::common::enhanced_noc_async_read<(source_page_size_bytes + 128), false>(
+            src_noc_addr & MASK_64, source_buffer, source_read_size_bytes);
         read_offset = src_noc_addr&OFFSET_64;
 #else
         //L1 but not alligned to 16
-        noc_async_read(src_noc_addr&MASK_16, source_buffer, source_read_size_bytes);
+        tt::data_movement::common::enhanced_noc_async_read<(source_page_size_bytes + 128), false>(
+            src_noc_addr & MASK_16, source_buffer, source_read_size_bytes);
         read_offset = src_noc_addr&OFFSET_16;
 #endif
         readable = source_page_size_bytes;
@@ -127,13 +131,15 @@ void kernel_main() {
             if (readable < writable)
             {
                 if constexpr (can_be_clean) {
-                    noc_async_write(source_buffer + read_offset, dst_noc_addr + dst_noc_addr_offset, readable);
+                    tt::data_movement::common::enhanced_noc_async_write<dest_page_size_bytes, false>(
+                        source_buffer + read_offset, dst_noc_addr + dst_noc_addr_offset, readable);
                     dst_noc_addr_offset = dst_noc_addr_offset + readable;
                 } else {
-                    tt::data_movement::common::tt_memmove<false, true, false>(
+                    tt::data_movement::common::tt_memmove<false, true, false, dest_page_size_bytes>(
                         dest_buffer + write_offset, source_buffer + read_offset, readable);
                     if (i == read_end_page - 1) {
-                        noc_async_write(dest_buffer + begin_write_offset, dst_noc_addr, end_to_write);
+                        tt::data_movement::common::enhanced_noc_async_write<dest_page_size_bytes, false>(
+                            dest_buffer + begin_write_offset, dst_noc_addr, end_to_write);
                         return;
                     }
                 }
@@ -146,11 +152,13 @@ void kernel_main() {
             else if (readable == writable)
             {
                 if constexpr (can_be_clean) {
-                    noc_async_write(source_buffer + read_offset, dst_noc_addr + dst_noc_addr_offset, readable);
+                    tt::data_movement::common::enhanced_noc_async_write<dest_page_size_bytes, false>(
+                        source_buffer + read_offset, dst_noc_addr + dst_noc_addr_offset, readable);
                 } else {
-                    tt::data_movement::common::tt_memmove<false, false, false>(
+                    tt::data_movement::common::tt_memmove<false, false, false, dest_page_size_bytes>(
                         dest_buffer + write_offset, source_buffer + read_offset, readable);
-                    noc_async_write(dest_buffer + begin_write_offset, dst_noc_addr, dest_page_size_bytes);
+                    tt::data_movement::common::enhanced_noc_async_write<dest_page_size_bytes, false>(
+                        dest_buffer + begin_write_offset, dst_noc_addr, dest_page_size_bytes);
                 }
                 dst_noc_addr_offset = 0;
 
@@ -168,11 +176,13 @@ void kernel_main() {
             else
             {
                 if constexpr (can_be_clean) {
-                    noc_async_write(source_buffer + read_offset, dst_noc_addr + dst_noc_addr_offset, writable);
+                    tt::data_movement::common::enhanced_noc_async_write<dest_page_size_bytes, false>(
+                        source_buffer + read_offset, dst_noc_addr + dst_noc_addr_offset, writable);
                 } else {
-                    tt::data_movement::common::tt_memmove<false, false, false>(
+                    tt::data_movement::common::tt_memmove<false, false, false, dest_page_size_bytes>(
                         dest_buffer + write_offset, source_buffer + read_offset, writable);
-                    noc_async_write(dest_buffer + begin_write_offset, dst_noc_addr, dest_page_size_bytes);
+                    tt::data_movement::common::enhanced_noc_async_write<dest_page_size_bytes, false>(
+                        dest_buffer + begin_write_offset, dst_noc_addr, dest_page_size_bytes);
                 }
                 // writable < readable
                 end_to_write = 0;
