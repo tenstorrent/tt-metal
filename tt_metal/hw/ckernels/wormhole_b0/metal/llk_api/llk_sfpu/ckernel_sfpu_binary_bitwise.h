5811236aa34149d308e7320acaa27cdb63b39932
Radomir Djogo <rdjogo@tenstorrent.com>
2024-12-04 10:54:17 -0500

    #13857: Add SPFU bitwise binary and add int32

---
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_binary_bitwise.h
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "ckernel.h"
+#include "ckernel_defs.h"
+#include "noc_nonblocking_api.h"
+#include "sfpi.h"
+
+using namespace sfpi;
+
+namespace ckernel {
+namespace sfpu {
+
+template <bool APPROXIMATION_MODE, int BITWISE_MODE, int ITERATIONS = 8>
+inline void calculate_sfpu_binary_bitwise(const uint dst_offset) {
+    _calculate_sfpu_binary_bitwise_<APPROXIMATION_MODE, BITWISE_MODE, ITERATIONS>(dst_offset);
+}
+
+}  // namespace sfpu
+}  // namespace ckernel
