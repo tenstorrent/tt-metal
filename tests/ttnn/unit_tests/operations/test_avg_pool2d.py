73efa3d6e867a2c8bc35b2aeea651f34315c0cc5
Jerry Wu <jerry@skymizer.com>
2024-12-04 09:29:25 +0000

    Add specific avg_pool2d sharding tests

--- tests/ttnn/unit_tests/operations/test_avg_pool2d.py
+++ tests/ttnn/unit_tests/operations/test_avg_pool2d.py
@@ -9,21 +9,34 @@ import pytest
 from tests.ttnn.utils_for_testing import assert_with_pcc
 
 
-def run_once(device, input_shape, kernel_size, stride, padding, dilation):
+def run_once(device, input_shape, kernel_size, stride, padding, dilation, shard_scheme=None):
     batch_size, in_c, in_h, in_w = input_shape
     torch_input = torch.rand(input_shape, dtype=torch.bfloat16)
 
     input_tensor = torch.permute(torch_input, (0, 2, 3, 1))
     input_tensor = torch.reshape(input_tensor, (1, 1, -1, in_c))
     input_tensor = ttnn.from_torch(input_tensor, layout=ttnn.ROW_MAJOR_LAYOUT, device=device)
-    output_tensor = ttnn.avg_pool2d(input_tensor, batch_size, in_h, in_w, in_c, kernel_size, stride, padding, dilation)
+    output_tensor = ttnn.avg_pool2d(
+        input_tensor,
+        batch_size,
+        in_h,
+        in_w,
+        in_c,
+        kernel_size,
+        stride,
+        padding,
+        dilation,
+        applied_shard_scheme=shard_scheme,
+    )
 
     expected_output = torch.nn.functional.avg_pool2d(
         torch_input, kernel_size, stride, padding, ceil_mode=False, count_include_pad=True, divisor_override=None
     )
+
     output_tensor = ttnn.to_torch(output_tensor)
+    _, out_c, out_h, out_w = expected_output.shape
+    output_tensor = torch.reshape(output_tensor, (batch_size, out_h, out_w, out_c))
     output_tensor = torch.permute(output_tensor, (0, 3, 1, 2))
-    output_tensor = torch.reshape(output_tensor, expected_output.shape)
     assert_with_pcc(expected_output, output_tensor, 0.99)
 
 
@@ -136,3 +149,19 @@ def run_once(device, input_shape, kernel_size, stride, padding, dilation):
 )
 def test_avg_pool2d(device, input_shape, kernel_size, stride, padding, dilation):
     run_once(device, input_shape, kernel_size, stride, padding, dilation)
+
+
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 8192}], indirect=True)
+@pytest.mark.parametrize(
+    "input_shape, kernel_size, stride, padding, dilation, shard_scheme",
+    [
+        ((4, 256, 28, 28), (2, 2), (2, 2), (0, 0), (1, 1), ttnn.TensorMemoryLayout.HEIGHT_SHARDED),
+        ((4, 256, 56, 56), (13, 13), (1, 1), (0, 0), (1, 1), ttnn.TensorMemoryLayout.HEIGHT_SHARDED),
+        ((1, 2048, 28, 28), (2, 2), (2, 2), (0, 0), (1, 1), ttnn.TensorMemoryLayout.WIDTH_SHARDED),
+        ((1, 2048, 56, 56), (13, 13), (1, 1), (0, 0), (1, 1), ttnn.TensorMemoryLayout.WIDTH_SHARDED),
+        ((2, 512, 28, 28), (2, 2), (2, 2), (0, 0), (1, 1), ttnn.TensorMemoryLayout.BLOCK_SHARDED),
+        ((2, 512, 56, 56), (13, 13), (1, 1), (0, 0), (1, 1), ttnn.TensorMemoryLayout.BLOCK_SHARDED),
+    ],
+)
+def test_avg_pool2d_with_specific_sharding(device, input_shape, kernel_size, stride, padding, dilation, shard_scheme):
+    run_once(device, input_shape, kernel_size, stride, padding, dilation, shard_scheme)
