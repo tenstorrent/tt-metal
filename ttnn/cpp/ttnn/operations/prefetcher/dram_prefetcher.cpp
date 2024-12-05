b9a41fc6109caaeb0f1a97c14c8e1d03ed0a2dbe
Johanna Rock <jrock@tenstorrent.com>
2024-12-05 15:31:36 +0000

    #0: change global cb to be const ref

--- ttnn/cpp/ttnn/operations/prefetcher/dram_prefetcher.cpp
+++ ttnn/cpp/ttnn/operations/prefetcher/dram_prefetcher.cpp
@@ -12,8 +12,7 @@
 namespace ttnn::operations::dram_prefetcher {
 
 ttnn::Tensor ExecuteDramPrefetcher::invoke(
-    std::vector<ttnn::Tensor>& tensors,
-    std::shared_ptr<tt::tt_metal::v1::experimental::GlobalCircularBuffer> global_cb) {
+    std::vector<ttnn::Tensor>& tensors, const tt::tt_metal::v1::experimental::GlobalCircularBuffer& global_cb) {
     operation::run(
         // DramPrefetcher{},
         DramPrefetcher{.global_cb = global_cb},
