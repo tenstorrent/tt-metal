051ad111728919aa361a59fff4673e79164582c9
Nikola Velickovic <nvelickovic@tenstorrent.com>
2024-12-05 14:27:37 +0000

    #15450: Add more explicit ocb arguments to calls that relied on default value

--- ttnn/cpp/ttnn/operations/reduction/generic/device/kernels/compute/reduce_w.cpp
+++ ttnn/cpp/ttnn/operations/reduction/generic/device/kernels/compute/reduce_w.cpp
@@ -18,7 +18,7 @@ void MAIN {
     uint32_t NC = get_compile_time_arg_val(2);
 
 #ifndef REDUCE_ROW_SUM_VIA_MM
-    reduce_init<true>(tt::CBIndex::c_0, tt::CBIndex::c_2);
+    reduce_init<true>(tt::CBIndex::c_0, tt::CBIndex::c_2, tt::CBIndex::c_16);
 #else
     mm_init(tt::CBIndex::c_0, tt::CBIndex::c_2);
 #endif
