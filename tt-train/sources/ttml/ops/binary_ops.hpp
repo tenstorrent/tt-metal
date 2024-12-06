f9859bc23b2eb3071729bed7618be41c69fc894c
Roman Furko <rfurko@tenstorrent.com>
2024-12-06 11:46:08 -0800

    [tt-train] Add multiple options for positional embedding (#15763)
    ### Problem description
    We want to try out original positional non-trainable embedding. 
    
    ### What's changed
    Add positional embedding modules. 
    Add missing dropout after embedding composition. 
    Add batch broadcast to plus operation. 
    Add tests 
    
    Main vs trainable + Dropout (previously missing)
    
    ![image](https://github.com/user-attachments/assets/f9b1d180-1bb7-4daf-9bd8-86334936c247)
    
    Trainable vs non-trainable positional embedding (both include previously
    missing dropout)
    
    ![image](https://github.com/user-attachments/assets/9b06e63c-a723-49e8-921f-6aea39155c49)
    
    
    ### Checklist
    - [x] Post commit CI passes
    - [x] New/Existing tests provide coverage for changes

--- tt-train/sources/ttml/ops/binary_ops.hpp
+++ tt-train/sources/ttml/ops/binary_ops.hpp
@@ -4,15 +4,18 @@
 
 #pragma once
 
+#include "autograd/autocast_tensor.hpp"
 #include "autograd/tensor.hpp"
 namespace ttml::ops {
 
+autograd::TensorPtr operator+(const autograd::TensorPtr& a, const autograd::AutocastTensor& b);
 autograd::TensorPtr operator+(const autograd::TensorPtr& a, const autograd::TensorPtr& b);
 autograd::TensorPtr operator*(const autograd::TensorPtr& a, const autograd::TensorPtr& b);
 autograd::TensorPtr operator*(const autograd::TensorPtr& a, float b);
 autograd::TensorPtr operator-(const autograd::TensorPtr& a, const autograd::TensorPtr& b);
 autograd::TensorPtr operator/(const autograd::TensorPtr& a, const autograd::TensorPtr& b);
 
+autograd::TensorPtr add(const autograd::TensorPtr& a, const autograd::AutocastTensor& b);
 autograd::TensorPtr add(const autograd::TensorPtr& a, const autograd::TensorPtr& b);
 autograd::TensorPtr sub(const autograd::TensorPtr& a, const autograd::TensorPtr& b);
 autograd::TensorPtr mul(const autograd::TensorPtr& a, const autograd::TensorPtr& b);
