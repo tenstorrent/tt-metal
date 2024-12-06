452cd4398de4b93bcd4aa97e60c7cda46a822ffe
Roman Furko <rfurko@tenstorrent.com>
2024-12-06 19:20:25 +0000

    Add missing license

--- tt-train/sources/ttml/modules/positional_embeddings.cpp
+++ tt-train/sources/ttml/modules/positional_embeddings.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
 #include "modules/positional_embeddings.hpp"
 
 #include "autograd/autocast_tensor.hpp"
