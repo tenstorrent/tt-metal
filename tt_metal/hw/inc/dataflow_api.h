5f07a394cf16d8b2c82babf37d915906da60c9d8
Austin Ho <aho@tenstorrent.com>
2024-12-06 19:00:35 +0000

    #0: Remove checks for if the coords passed in to NOC_XY_PCIE_ENCODING are an actual pcie coord NOC_XY_PCIE_ENCODING is specifically for getting pcie coordinate. User Error to do otherwise

--- tt_metal/hw/inc/dataflow_api.h
+++ tt_metal/hw/inc/dataflow_api.h
@@ -694,7 +694,7 @@ uint64_t get_system_memory_noc_addr(
     const uint32_t offset = 0,
     uint8_t noc = noc_index) {
     uint64_t pcie_core_noc_encoding =
-        uint64_t(NOC_XY_PCIE_ENCODING(DYNAMIC_NOC_X(noc, PCIE_NOC_X), DYNAMIC_NOC_Y(noc, PCIE_NOC_Y), noc));
+        uint64_t(NOC_XY_PCIE_ENCODING(DYNAMIC_NOC_X(noc, PCIE_NOC_X), DYNAMIC_NOC_Y(noc, PCIE_NOC_Y)));
     uint32_t addr = base_addr + page_size * id + offset;
     uint64_t noc_addr = pcie_core_noc_encoding | addr;
     return noc_addr;
