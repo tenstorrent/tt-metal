863474b2d3379d39dc365ab3f2b4d845740e350a
Radomir Djogo <159184120+rdjogoTT@users.noreply.github.com>
2024-12-06 11:43:02 -0500

    Add binary SFPU OPs - eltwise & bitwise (#56)
    * Add binary sfpu LLKs
    
    * Add SFPU binary bitwise, int32 add, div

--- tt_llk_blackhole/llk_lib/llk_math_eltwise_binary_sfpu.h
+++ tt_llk_blackhole/llk_lib/llk_math_eltwise_binary_sfpu.h
@@ -37,6 +37,7 @@ inline void _llk_math_eltwise_binary_sfpu_start_(const uint dst_index) {
     } else {
         math::set_dst_write_addr<DstTileLayout::Default, DstTileShape::Tile32x32>(dst_index);
     }
+    TTI_STALLWAIT(p_stall::STALL_SFPU, p_stall::MATH);
 }
 
 inline void _llk_math_eltwise_binary_sfpu_done_() {
@@ -48,6 +49,7 @@ inline void _llk_math_eltwise_binary_sfpu_inc_dst_face_addr_() {
     math::inc_dst_addr<8>();
 }
 
+template <SfpuType sfpu_op>
 inline void _llk_math_eltwise_binary_sfpu_init_() {
     sfpu::_init_sfpu_config_reg();
     eltwise_binary_sfpu_configure_addrmod();
