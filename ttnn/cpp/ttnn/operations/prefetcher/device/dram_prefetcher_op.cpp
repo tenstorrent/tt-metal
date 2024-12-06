9a635db90af9512b37cc611020e77b08b8cd29e9
Johanna Rock <jrock@tenstorrent.com>
2024-12-06 12:59:21 +0000

    #0: setup output tensor to map to global cb

--- ttnn/cpp/ttnn/operations/prefetcher/device/dram_prefetcher_op.cpp
+++ ttnn/cpp/ttnn/operations/prefetcher/device/dram_prefetcher_op.cpp
@@ -5,6 +5,7 @@
 #include "dram_prefetcher_op.hpp"
 #include "ttnn/run_operation.hpp"
 #include "ttnn/operations/math.hpp"
+// #include "ttnn/tensor/types.hpp"
 
 #include "tt_metal/common/constants.hpp"
 
@@ -27,27 +28,56 @@ void DramPrefetcher::validate(
     for (const auto& tensor : input_tensors) {
         TT_FATAL(tensor.get_legacy_shape()[0] % 24 == 0, "All tensors' k must be divisible by 24");
     }
-    // TT_FATAL(global_cb != nullptr, "Global circular buffer must be provided");
-    // // Check that global_cb sender_receiver_core_mapping has same number of receivers for each sender core
-    // auto sender_receiver_core_mapping = global_cb.sender_receiver_core_mapping();
-    // for (const auto& [sender_core, receiver_core_range] : sender_receiver_core_mapping) {
-    //     TT_FATAL(receiver_core_range.size() == sender_receiver_core_mapping.begin()->second.size(), "Global circular
-    //     buffer must have same number of receivers for each sender core");
-    // }
+    TT_FATAL(global_cb.has_value(), "Global circular buffer must be provided");
+    // Check that global_cb sender_receiver_core_mapping has same number of receivers for each sender core
+    auto sender_receiver_core_mapping = global_cb->sender_receiver_core_mapping();
+    for (const auto& [sender_core, receiver_core_range] : sender_receiver_core_mapping) {
+        TT_FATAL(
+            receiver_core_range.size() == sender_receiver_core_mapping.begin()->second.size(),
+            "Global circular buffer must have same number of receivers for each sender core");
+    }
 }
 std::vector<ttnn::SimpleShape> DramPrefetcher::compute_output_shapes(const std::vector<Tensor>& input_tensors) const {
-    // Do nothing because it's an in-place operation
-    return {};
+    return {input_tensors.at(0).get_logical_shape()};
 }
 std::vector<Tensor> DramPrefetcher::create_output_tensors(const std::vector<Tensor>& input_tensors) const {
-    // Do nothing because it's an in-place operation
-    return {};
+    // Configure L1 interleaved memory layout
+    MemoryConfig output_mem_config = L1_MEMORY_CONFIG;
+
+    auto input_tensor = input_tensors.at(0);
+
+    auto shard_shape = input_tensor.get_logical_shape();
+    shard_shape[-1] = shard_shape[-1] / 24;  // width is sharded over 24 cores
+
+    // Create core ranges
+    CoreRange first_core_range(CoreCoord(1, 0), CoreCoord(2, 0));
+    CoreRange second_core_range(CoreCoord(1, 5), CoreCoord(1, 6));
+
+    // Create core range set using std::set
+    std::set<CoreRange> ranges = {first_core_range, second_core_range};
+    CoreRangeSet core_range_set(ranges);
+
+    // Create shard spec with core range set and shard shape
+    ShardSpec shard_spec(core_range_set, {shard_shape[0], shard_shape[1]}, ShardOrientation::ROW_MAJOR);
+
+    // print shard spec
+    std::cout << "Shard spec: " << shard_spec << std::endl;
+
+    // Update memory config with shard spec
+    output_mem_config.shard_spec = shard_spec;
+
+    auto output_tensor_spec = TensorSpec(
+        input_tensor.get_logical_shape(),
+        TensorLayout(input_tensor.get_dtype(), PageConfig(Layout::TILE), output_mem_config));
+
+    return {create_device_tensor(output_tensor_spec, input_tensor.device())};
 }
 operation::ProgramWithCallbacks DramPrefetcher::create_program(
     const std::vector<Tensor>& input_tensors,
     const std::vector<std::optional<const Tensor>>& optional_input_tensors,
     std::vector<Tensor>& output_tensors) const {
-    return dram_prefetcher_multi_core(input_tensors, global_cb);
+    auto& output_tensor = output_tensors.at(0);
+    return dram_prefetcher_multi_core(input_tensors, global_cb, output_tensor);
 }
 
 }  // namespace ttnn::operations::dram_prefetcher
