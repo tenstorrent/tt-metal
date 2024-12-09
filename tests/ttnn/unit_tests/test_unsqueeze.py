bc20fe0d13bbaee2e824f8b96c1f4e44e7a46ac9
Juan Camilo Vega <jvega@tenstorrent.com>
2024-12-09 19:08:21 +0000

    #0: testing that invalid cases cause an assertion

--- tests/ttnn/unit_tests/test_unsqueeze.py
+++ tests/ttnn/unit_tests/test_unsqueeze.py
@@ -33,3 +33,17 @@ def test_unsqueeze(device, input_shape, dim, layout):
     ttnn_output = ttnn.unsqueeze(input_tensor, dim)
     torch_output_tensor = ttnn.to_torch(ttnn_output)
     assert torch.allclose(torch_output_tensor, torch_unsqueeze_tensor)
+
+
+@pytest.mark.parametrize(
+    "input_shape, dim, layout",
+    [
+        ((1, 1, 253), 4, ttnn.ROW_MAJOR_LAYOUT),
+        ((1, 1, 253), -5, ttnn.ROW_MAJOR_LAYOUT),
+    ],
+)
+def test_invalid_cases(device, input_shape, dim, layout):
+    torch_input_tensor = torch.rand(input_shape, dtype=torch.bfloat16)
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=layout, device=device)
+    with pytest.raises(RuntimeError):
+        ttnn.unsqueeze(input_tensor, dim)
