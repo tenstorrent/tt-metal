fd2cd27f93eddeb7ec16cba1890ba1dbf0235314
Juan Camilo Vega <jvega@tenstorrent.com>
2024-12-09 16:37:43 -0500

    #15835: Adding tile support to unsqueeze OP (#15837)
    ### Ticket
    Link to Github Issue
    https://github.com/tenstorrent/tt-metal/issues/15835
    ### Problem description
    We do the following to unsqueeze:
    
    Add Tile Support
    Add parameter validation
    Fix bugs in logic for handling dim=rank case
    Added testing to validate the changes
    
    Sweeps pass rate from 56% to 88.62%
    
    Remaining errors should be resolved with this PR
    https://github.com/tenstorrent/tt-metal/pull/15289 as they pertain to
    the 0 sized dimensions cases
    
    ### Checklist
    - [x] Post commit CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/12240795525
    - [ ] Blackhole Post commit (if applicable)
    - [ ] Model regression CI testing passes (if applicable)
    - [ ] Device performance regression CI testing passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new
    models](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    tests passes
    - [x] New/Existing tests provide coverage for changes

--- tests/ttnn/unit_tests/test_unsqueeze.py
+++ tests/ttnn/unit_tests/test_unsqueeze.py
@@ -10,20 +10,40 @@ import ttnn
 
 
 @pytest.mark.parametrize(
-    "input_shape, dim",
+    "input_shape, dim, layout",
     [
-        ((1, 1, 256), 2),
-        ((1, 1, 256), -2),
-        ((1, 256), 1),
-        ((1, 1, 30), 2),
-        ((1, 1, 30), -2),
-        ((1, 30), 1),
+        ((1, 1, 253), 2, ttnn.ROW_MAJOR_LAYOUT),
+        ((1, 1, 253), -2, ttnn.ROW_MAJOR_LAYOUT),
+        ((1, 253), 1, ttnn.ROW_MAJOR_LAYOUT),
+        ((1, 1, 253), -2, ttnn.TILE_LAYOUT),
+        ((1, 253), 1, ttnn.TILE_LAYOUT),
+        ((57, 83), 1, ttnn.TILE_LAYOUT),
+        ((123, 259), -2, ttnn.TILE_LAYOUT),
+        ((57, 83), 1, ttnn.ROW_MAJOR_LAYOUT),
+        ((123, 259), -2, ttnn.ROW_MAJOR_LAYOUT),
+        ((8732,), 1, ttnn.ROW_MAJOR_LAYOUT),
+        ((8732,), -1, ttnn.ROW_MAJOR_LAYOUT),
+        ((8732,), 0, ttnn.ROW_MAJOR_LAYOUT),
     ],
 )
-def test_unsqueeze(device, input_shape, dim):
+def test_unsqueeze(device, input_shape, dim, layout):
     torch_input_tensor = torch.rand(input_shape, dtype=torch.bfloat16)
     torch_unsqueeze_tensor = torch.unsqueeze(torch_input_tensor, dim)
-    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.ROW_MAJOR_LAYOUT, device=device)
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=layout, device=device)
     ttnn_output = ttnn.unsqueeze(input_tensor, dim)
     torch_output_tensor = ttnn.to_torch(ttnn_output)
     assert torch.allclose(torch_output_tensor, torch_unsqueeze_tensor)
+
+
+@pytest.mark.parametrize(
+    "input_shape, dim, layout",
+    [
+        ((1, 1, 253), 4, ttnn.ROW_MAJOR_LAYOUT),
+        ((1, 1, 253), -5, ttnn.ROW_MAJOR_LAYOUT),
+    ],
+)
+def test_invalid_cases(device, input_shape, dim, layout):
+    torch_input_tensor = torch.rand(input_shape, dtype=torch.bfloat16)
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=layout, device=device)
+    with pytest.raises(RuntimeError):
+        ttnn.unsqueeze(input_tensor, dim)
