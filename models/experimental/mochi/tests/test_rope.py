8abe4d5a451c911eff6723e7eaa06a9e85b080ad
Colman Glagovich <cglagovich@tenstorrent.com>
2024-12-09 06:27:16 -0800

    #0: temporal rope works for NH=1 but op asserts for NH>1

--- models/experimental/mochi/tests/test_rope.py
+++ models/experimental/mochi/tests/test_rope.py
@@ -4,13 +4,67 @@ import ttnn
 from models.experimental.mochi.common import compute_metrics
 
 from genmo.mochi_preview.dit.joint_model.temporal_rope import apply_rotary_emb_qk_real
+from models.demos.llama3.tt.llama_rope import TtLlamaRotarySetup
+from models.demos.llama3.tt.llama_common import get_rot_transformation_mat
 
 
-def test_apply_rotary_emb_qk_real():
+def stack_cos_sin(cos, sin):
+    cos = torch.stack([cos, cos], dim=-1).flatten(-2)
+    sin = torch.stack([sin, sin], dim=-1).flatten(-2)
+    return cos, sin
+
+
+def rotate_half(x):
+    x1 = x[..., : x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2 :]
+    return torch.cat((-x2, x1), dim=-1)
+
+
+def apply_rotary_pos_emb(x, cos_cached, sin_cached, token_idx=None):
+    seq_len = x.shape[-2]
+    if token_idx is None:
+        cos = cos_cached[:, :, :seq_len, ...]
+        sin = sin_cached[:, :, :seq_len, ...]
+    else:
+        cos = cos_cached[:, :, token_idx : token_idx + 1, ...]
+        sin = sin_cached[:, :, token_idx : token_idx + 1, ...]
+
+    x_embed = (x * cos) + (rotate_half(x) * sin)
+    return x_embed
+
+
+# def test_apply_rotary_emb_qk_real_torch():
+#     # Test parameters
+#     batch_size = 1
+#     seq_len = 32
+#     # num_heads = 8
+#     num_heads = 1
+#     head_dim = 64
+
+#     # Create input tensors
+#     xqk = torch.randn(batch_size, seq_len, num_heads, head_dim)
+#     freqs_cos = torch.randn(seq_len, num_heads, head_dim // 2)
+#     freqs_sin = torch.randn(seq_len, num_heads, head_dim // 2)
+
+#     # Run reference implementation
+#     gt = apply_rotary_emb_qk_real(xqk, freqs_cos, freqs_sin)
+
+
+#     # Run rotate_half impl
+#     # Get inputs to have NH in -3 dimension
+#     xqk_reshape = xqk.permute(0, 2, 1, 3)
+#     freqs_cos_reshape = freqs_cos.permute(1, 0, 2).unsqueeze(0)
+#     freqs_sin_reshape = freqs_sin.permute(1, 0, 2).unsqueeze(0)
+
+#     cos_stacked, sin_stacked = stack_cos_sin(freqs_cos_reshape, freqs_sin_reshape)
+
+
+def test_apply_rotary_emb_qk_real(device):
     # Test parameters
-    batch_size = 2
-    seq_len = 4
-    num_heads = 8
+    batch_size = 1
+    seq_len = 32
+    # num_heads = 8
+    num_heads = 2
     head_dim = 64
 
     # Create input tensors
@@ -28,14 +82,35 @@ def test_apply_rotary_emb_qk_real():
     # expected_out = torch.stack([cos_part, sin_part], dim=-1).flatten(-2)
 
     # Run ttnn implementation
-    xqk_tt = ttnn.from_torch(xqk)
-    freqs_cos_tt = ttnn.from_torch(freqs_cos)
-    freqs_sin_tt = ttnn.from_torch(freqs_sin)
-    out_tt = ttnn.experimental.rotary_embedding_llama(xqk_tt, freqs_cos_tt, freqs_sin_tt)
+
+    # Using rope_setup to just get the transformation matrices
+    trans_mat = get_rot_transformation_mat(None)
+    print(trans_mat)
+
+    # Get inputs to have NH in -3 dimension
+    xqk_reshape = xqk.permute(0, 2, 1, 3)
+    freqs_cos_reshape = freqs_cos.permute(1, 0, 2).unsqueeze(0)
+    freqs_sin_reshape = freqs_sin.permute(1, 0, 2).unsqueeze(0)
+
+    cos_stacked, sin_stacked = stack_cos_sin(freqs_cos_reshape, freqs_sin_reshape)
+    print(f"{cos_stacked.shape=}")
+    print(f"{sin_stacked.shape=}")
+    print(f"{xqk_reshape.shape=}")
+    print(f"{trans_mat.shape=}")
+
+    xqk_tt = ttnn.from_torch(xqk_reshape, device=device, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT)
+    freqs_cos_tt = ttnn.from_torch(cos_stacked, device=device, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT)
+    freqs_sin_tt = ttnn.from_torch(sin_stacked, device=device, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT)
+    trans_mat_tt = ttnn.from_torch(trans_mat, device=device, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT)
+
+    out_tt = ttnn.experimental.rotary_embedding_llama(xqk_tt, freqs_cos_tt, freqs_sin_tt, trans_mat_tt)
     out = ttnn.to_torch(out_tt)
 
-    # Shape assertions
-    assert out.shape == xqk.shape
+    print(f"{out.shape=}")
+    # Reshape back to match shape of gt
+    out = out.permute(0, 2, 1, 3)
+    print(f"{out.shape=}")
+    print(f"{gt.shape=}")
 
     # Compute accuracy metrics
     pcc, mse, mae = compute_metrics(gt, out)
