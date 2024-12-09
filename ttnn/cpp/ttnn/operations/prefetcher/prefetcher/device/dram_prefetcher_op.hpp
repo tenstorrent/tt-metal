205e0c5218fc9babf42bb1822b13c28330944fe8
avoraTT <avora@tenstorrent.com>
2024-12-09 06:08:32 -0800

    Reading from DRAM works (using tensor_addrs Tensor). Things to note: output tensor only has the first input tensor, debug mode must be use to workaround known segfault.

--- ttnn/cpp/ttnn/operations/prefetcher/prefetcher/device/dram_prefetcher_op.hpp
+++ ttnn/cpp/ttnn/operations/prefetcher/prefetcher/device/dram_prefetcher_op.hpp
@@ -18,12 +18,12 @@ namespace ttnn::operations::dram_prefetcher {
 operation::ProgramWithCallbacks dram_prefetcher_multi_core(
     const std::vector<Tensor>& tensors,
     const Tensor& tensor_addrs,
-    const std::optional<tt::tt_metal::v1::experimental::GlobalCircularBuffer>& global_cb,
+    const std::optional<const tt::tt_metal::v1::experimental::GlobalCircularBuffer>& global_cb,
     Tensor& output_tensor);
 
 struct DramPrefetcher {
     const Tensor tensor_addrs = Tensor();
-    const std::optional<tt::tt_metal::v1::experimental::GlobalCircularBuffer> global_cb;
+    const std::optional<const tt::tt_metal::v1::experimental::GlobalCircularBuffer> global_cb;
 
     void validate(const std::vector<Tensor>& input_tensors) const;
     std::vector<ttnn::SimpleShape> compute_output_shapes(const std::vector<Tensor>& input_tensors) const;
