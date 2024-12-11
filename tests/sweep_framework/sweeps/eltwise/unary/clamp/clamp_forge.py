597678ca591188bafd8b7ca7b63fb24eb36588aa
umadevimcw <umadevi@multicorewareinc.com>
2024-12-11 14:11:05 +0000

    #15857: Update sweep tests
    #15857: Update clamp forge sweep tests
    
    #15857: Update floor sweep tests
    
    #15857: Update log forge sweep tests

--- tests/sweep_framework/sweeps/eltwise/unary/clamp/clamp_forge.py
+++ tests/sweep_framework/sweeps/eltwise/unary/clamp/clamp_forge.py
@@ -28,52 +28,87 @@ random.seed(0)
 
 parameters = {
     "nightly": {
-        "input_shape": [
-            [1, 4800, 512],
-            [1, 300, 2048],
-            [320, 1],
-            [27, 1],
-            [60, 1],
-            [1, 256, 5120],
-            [1, 197, 3072],
-            [1, 10, 3072],
-            [1, 3072, 8],
-            [160],
-            [128, 1],
-            [1, 19, 4096],
-            [240, 1],
-            [1, 1445, 768],
-            [1, 64, 5120],
-            [1, 10, 768],
-            [1, 201, 3072],
-            [1, 25, 3072],
-            [480, 1],
-            [1, 16, 3072],
-            [1, 256, 256],
-            [80],
-            [1, 16384, 128],
-            [1, 2048, 768],
-            [1, 1024, 512],
-            [1, 197, 4096],
-            [1, 1024, 640],
-            [40],
-            [1, 4096, 256],
-            [1, 1536],
-            [1, 256, 1024],
-            [128],
-            [1, 7, 18176],
-            [1, 256, 4096],
-            [1, 4096, 1280],
-            [30, 1],
-            [320],
-            [1, 256, 1280],
-            [27],
-            [640],
-            [1, 1200, 1280],
-            [120, 1],
-            [1, 19200, 256],
+        "input_specs": [
+            {"input_shape": [1, 4800, 512], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 4800, 512], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 300, 2048], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 300, 2048], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [320, 1], "min": 0.000000e00, "max": 3.190000e02},
+            {"input_shape": [27, 1], "min": 0.000000e00, "max": 2.600000e01},
+            {"input_shape": [60, 1], "min": 0.000000e00, "max": 2.900000e01},
+            {"input_shape": [1, 256, 5120], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 256, 5120], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 197, 3072], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 197, 3072], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 10, 3072], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 10, 3072], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 3072, 8], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 3072, 8], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [160], "min": 0.000000e00, "max": 7.900000e01},
+            {"input_shape": [128, 1], "min": 0.000000e00, "max": 1.270000e02},
+            {"input_shape": [128, 1], "min": 0.000000e00, "max": 1.500000e01},
+            {"input_shape": [128, 1], "min": 0.000000e00, "max": 3.100000e01},
+            {"input_shape": [128, 1], "min": 0.000000e00, "max": 6.300000e01},
+            {"input_shape": [1, 19, 4096], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 19, 4096], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [240, 1], "min": 0.000000e00, "max": 1.190000e02},
+            {"input_shape": [1, 1445, 768], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 1445, 768], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 64, 5120], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 64, 5120], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 10, 768], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 10, 768], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 201, 3072], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 201, 3072], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 25, 3072], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 25, 3072], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [480, 1], "min": 0.000000e00, "max": 2.390000e02},
+            {"input_shape": [1, 16, 3072], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 16, 3072], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 256, 256], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 256, 256], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [80], "min": 0.000000e00, "max": 3.900000e01},
+            {"input_shape": [1, 16384, 128], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 16384, 128], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 2048, 768], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 2048, 768], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 1024, 512], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 1024, 512], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 197, 4096], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 197, 4096], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 1024, 640], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 1024, 640], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [40], "min": 0.000000e00, "max": 1.900000e01},
+            {"input_shape": [1, 4096, 256], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 4096, 256], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 1536], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 1536], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 256, 1024], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 256, 1024], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [128], "min": 0.000000e00, "max": 1.270000e02},
+            {"input_shape": [128], "min": 0.000000e00, "max": 1.500000e01},
+            {"input_shape": [128], "min": 0.000000e00, "max": 3.100000e01},
+            {"input_shape": [128], "min": 0.000000e00, "max": 6.300000e01},
+            {"input_shape": [1, 7, 18176], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 7, 18176], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 256, 4096], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 256, 4096], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [1, 4096, 1280], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 4096, 1280], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [30, 1], "min": 0.000000e00, "max": 1.400000e01},
+            {"input_shape": [320], "min": 0.000000e00, "max": 1.590000e02},
+            {"input_shape": [320], "min": 0.000000e00, "max": 3.190000e02},
+            {"input_shape": [1, 256, 1280], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 256, 1280], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [27], "min": 0.000000e00, "max": 2.600000e01},
+            {"input_shape": [640], "min": 0.000000e00, "max": 3.190000e02},
+            {"input_shape": [1, 1200, 1280], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 1200, 1280], "min": -1.000000e00, "max": 1.000000e00},
+            {"input_shape": [120, 1], "min": 0.000000e00, "max": 5.900000e01},
+            {"input_shape": [1, 19200, 256], "min": -4.000000e00, "max": 4.000000e00},
+            {"input_shape": [1, 19200, 256], "min": -1.000000e00, "max": 1.000000e00},
         ],
-        "input_a_dtype": [["ttnn.int32", "ttnn.float32"]],
+        "input_a_dtype": [ttnn.float32],  # [ttnn.int32, ttnn.float32],
         "input_a_layout": [ttnn.TILE_LAYOUT],
         "input_a_memory_config": [ttnn.DRAM_MEMORY_CONFIG],
         "output_memory_config": [ttnn.DRAM_MEMORY_CONFIG],
@@ -95,7 +130,7 @@ def invalidate_vector(test_vector) -> Tuple[bool, Optional[str]]:
 # The runner will call this run function with each test vector, and the returned results from this function will be stored.
 # If you defined a mesh_device_fixture above, the object you yielded will be passed into this function as 'device'. Otherwise, it will be the default ttnn device opened by the infra.
 def run(
-    input_shape,
+    input_specs,
     input_a_dtype,
     input_a_layout,
     input_a_memory_config,
@@ -107,10 +142,13 @@ def run(
 
     torch_input_tensor_a = gen_func_with_cast_tt(
         partial(torch_random, low=-100, high=100, dtype=torch.float32), input_a_dtype
-    )(input_shape)
+    )(input_specs["input_shape"])
+
+    min_val = input_specs.get("min", None)
+    max_val = input_specs.get("max", None)
 
     golden_function = ttnn.get_golden_function(ttnn.clamp)
-    torch_output_tensor = golden_function(torch_input_tensor_a)
+    torch_output_tensor = golden_function(torch_input_tensor_a, min_val, max_val)
 
     input_tensor_a = ttnn.from_torch(
         torch_input_tensor_a,
@@ -121,8 +159,9 @@ def run(
     )
 
     start_time = start_measuring_time()
-    result = ttnn.clamp(input_tensor_a, memory_config=output_memory_config)
-    output_tensor = ttnn.to_torch(result)
+    result = ttnn.clamp(input_tensor_a, min=min_val, max=max_val, memory_config=output_memory_config)
+    # ToDo: Update it once the tensor layout support with rank < 2 is supported in mid of Jan
+    output_tensor = ttnn.to_torch(result, torch_rank=len(input_specs["input_shape"]))
     e2e_perf = stop_measuring_time(start_time)
 
     return [check_with_pcc(torch_output_tensor, output_tensor, 0.999), e2e_perf]
