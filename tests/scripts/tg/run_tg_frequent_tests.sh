0069a14e77d9ade95749e67dbb93a636ed6a8cea
mtairum <mtairum@tenstorrent.com>
2024-12-11 17:17:07 +0000

    #0: Refactored TG frequent pipeline. Fixes to demo pipeline

--- tests/scripts/tg/run_tg_frequent_tests.sh
+++ tests/scripts/tg/run_tg_frequent_tests.sh
@@ -7,20 +7,21 @@ run_tg_llama3_tests() {
 
   echo "LOG_METAL: Running run_tg_llama3_tests"
 
-  # Llama3.1-70B
+  Llama3.2-1B
+  llama1b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.2-1B-Instruct/
+  Llama3.2-3B
+  llama3b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.2-3B-Instruct/
+  Llama3.1-8B
+  llama8b=/mnt/MLPerf/tt_dnn-models/llama/Meta-Llama-3.1-8B-Instruct/
+  Llama3.2-11B
+  llama11b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.2-11B-Vision-Instruct/
+  Llama3.1-70B
   llama70b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.1-70B-Instruct/
-  # Llama3.1-8B
-  # llama8b=/mnt/MLPerf/tt_dnn-models/llama/Meta-Llama-3.1-8B-Instruct/
-  # Llama3.2-1B
-  # llama1b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.2-1B-Instruct/
-  # Llama3.2-3B
-  # llama3b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.2-3B-Instruct/
-  # Llama3.2-11B
-  # llama11b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.2-11B-Vision-Instruct/
 
   # Run all Llama3 tests for 8B, 1B, and 3B weights
+  # for llama_dir in "$llama1b" "$llama3b" "$llama8b" "$llama11b" "$llama70b"; do
   for llama_dir in "$llama70b"; do
-    LLAMA_DIR=$llama_dir FAKE_DEVICE=TG pytest -n auto models/demos/llama3/tests/test_llama_model.py -k full ; fail+=$?
+    LLAMA_DIR=$llama_dir FAKE_DEVICE=TG pytest -n auto models/demos/llama3/tests/test_llama_model.py -k full --timeout=1800 ; fail+=$?
     echo "LOG_METAL: Llama3 tests for $llama_dir completed"
   done
 
@@ -34,26 +35,39 @@ run_tg_llama3_tests() {
 }
 
 run_tg_tests() {
-  # Add tests here
-  echo "LOG_METAL: running run_tg_frequent_tests"
 
-  pytest -n auto tests/ttnn/distributed/test_data_parallel_example_TG.py --timeout=900 ; fail+=$?
-  pytest -n auto tests/ttnn/distributed/test_multidevice_TG.py --timeout=900 ; fail+=$?
-  pytest -n auto tests/ttnn/unit_tests/test_multi_device_trace_TG.py --timeout=900 ; fail+=$?
-  pytest -n auto models/demos/tg/llama3_70b/tests/test_llama_mlp_galaxy.py --timeout=300 ; fail+=$?
-  pytest -n auto models/demos/tg/llama3_70b/tests/test_llama_attention_galaxy.py --timeout=480 ; fail+=$?
-  pytest -n auto models/demos/tg/llama3_70b/tests/test_llama_decoder_galaxy.py --timeout=600 ; fail+=$?
-  pytest -n auto models/demos/tg/llama3_70b/tests/test_llama_model_galaxy_ci.py --timeout=800 ; fail+=$?
-  pytest -n auto models/demos/tg/resnet50/tests/test_resnet50_performant.py ; fail+=$?
-  pytest -n auto tests/ttnn/unit_tests/operations/ccl/test_all_gather_TG_post_commit.py --timeout=300 ; fail+=$?
+  if [[ "$1" == "llama3-70b-old" ]]; then
+    echo "LOG_METAL: running llama3_70b (old) run_tg_frequent_tests"
+    pytest -n auto models/demos/tg/llama3_70b/tests/test_llama_mlp_galaxy.py --timeout=300 ; fail+=$?
+    pytest -n auto models/demos/tg/llama3_70b/tests/test_llama_attention_galaxy.py --timeout=480 ; fail+=$?
+    pytest -n auto models/demos/tg/llama3_70b/tests/test_llama_decoder_galaxy.py --timeout=600 ; fail+=$?
+    pytest -n auto models/demos/tg/llama3_70b/tests/test_llama_model_galaxy_ci.py --timeout=800 ; fail+=$?
+
+  elif [[ "$1" == "llama3" ]]; then
+    echo "LOG_METAL: running Llama3 run_tg_frequent_tests"
+    run_tg_llama3_tests
+
+  elif [[ "$1" == "resnet50" ]]; then
+    echo "LOG_METAL: running resnet50 run_tg_frequent_tests"
+    pytest -n auto models/demos/tg/resnet50/tests/test_resnet50_performant.py ; fail+=$?
+
+  elif [[ "$1" == "unit" ]]; then
+    echo "LOG_METAL: running unit/distributed run_tg_frequent_tests"
+    pytest -n auto tests/ttnn/distributed/test_data_parallel_example_TG.py --timeout=900 ; fail+=$?
+    pytest -n auto tests/ttnn/distributed/test_multidevice_TG.py --timeout=900 ; fail+=$?
+    pytest -n auto tests/ttnn/unit_tests/test_multi_device_trace_TG.py --timeout=900 ; fail+=$?
+    pytest -n auto tests/ttnn/unit_tests/operations/ccl/test_all_gather_TG_post_commit.py --timeout=300 ; fail+=$?
+
+  else
+    echo "LOG_METAL: Unknown model type: $1"
+    return 1
+  fi
 
   if [[ $fail -ne 0 ]]; then
     echo "LOG_METAL: run_tg_frequent_tests failed"
     exit 1
   fi
 
-  # Run llama3 tests
-  run_tg_llama3_tests
 }
 
 main() {
@@ -67,11 +81,26 @@ main() {
     exit 1
   fi
 
+  # Parse the arguments
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      --model)
+        model=$2
+        shift
+        ;;
+      *)
+        echo "Unknown option: $1"
+        exit 1
+        ;;
+    esac
+    shift
+  done
+
   # Run all tests
   cd $TT_METAL_HOME
   export PYTHONPATH=$TT_METAL_HOME
 
-  run_tg_tests
+  run_tg_tests "$model"
 }
 
 main "$@"
