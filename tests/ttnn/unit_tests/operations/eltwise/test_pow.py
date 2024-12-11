b959916b5ae1f45603d79c6b05776c632ea8aa6f
Kalaivani Baskar <156762498+KalaivaniMCW@users.noreply.github.com>
2024-12-11 13:39:12 +0530

    #15483: Binary sfpu ops for Float32 precision (#15805)
    ### Ticket
    Link to Github Issue 
    #15483  
    #14825   
    #13857   
    #13582 
    #13754 
    
    ### Problem description
    To enable Float32 precision support for elementwise binary ops
    
    ### What's changed
    This is PR implements the new LLK functionalities from
    https://github.com/tenstorrent/tt-metal/pull/15782 for TTNN apis
    
    For element-wise binary ops, FP32 precision is not possible during the
    current FPU operations, hence we introduce binary_sfpu_ops which gives
    Float32 support for binary operation by running the operation in SFPU.
    
    1. enabled Float32 precision support for elementwise binary ops
    2. added support for binary pow op
    3. added support for 3 binary bitwise ops 
    4. added support for binary rsub op
    5. Pow - pytorch2 sweep test : 
    <img width="1129" alt="image"
    src="https://github.com/user-attachments/assets/600e4f7b-56fa-4f2b-a7a4-6c2bf78652bc">
    
    
    ### Checklist
    - [x] Post commit CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/12238579964
    https://github.com/tenstorrent/tt-metal/actions/runs/12270807620
    - [x] Blackhole Post commit (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/12238601878
    - [ ] Nightly FD -
    https://github.com/tenstorrent/tt-metal/actions/runs/12235128037/attempts/1
    https://github.com/tenstorrent/tt-metal/actions/runs/12238605626
    - [x] SC Model Perf testing passes (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/12235130984
    - [x] SC Device performance regression CI testing passes (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/12235124425
    - [x] SC Demo tests -
    https://github.com/tenstorrent/tt-metal/actions/runs/12256052513
    - [ ] Test for new models -
    https://github.com/tenstorrent/tt-metal/actions/runs/12256106557
    - [x] New/Existing tests provide coverage for changes

--- tests/ttnn/unit_tests/operations/eltwise/test_pow.py
+++ tests/ttnn/unit_tests/operations/eltwise/test_pow.py
@@ -6,6 +6,8 @@ import torch
 import pytest
 import ttnn
 from tests.ttnn.unit_tests.operations.eltwise.backward.utility_funcs import data_gen_with_range, compare_pcc
+from tests.ttnn.utils_for_testing import assert_with_pcc
+from models.utility_functions import skip_for_grayskull
 
 
 @pytest.mark.parametrize(
@@ -28,3 +30,130 @@ def test_unary_pow_ttnn(input_shapes, exponent, device):
 
     comp_pass = compare_pcc([output_tensor], [golden_tensor], pcc=0.9)
     assert comp_pass
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize(
+    "input_shapes",
+    ([20, 20], [2, 32, 320], [1, 1, 32, 32], [1, 3, 320, 384], [1, 2, 32, 64, 64]),
+)
+@pytest.mark.parametrize("input", [10.0, 5.5, -5.0, -2.5, -10, -3, 9.5, -7.25, -6.15])
+@pytest.mark.parametrize("exponent", [2.75, 2.5, 1.5, 4, 5.75, 0, -1.5, -2.25, -3, -4.25, -5.5])
+# Both input and exponent are -ve and exponent is a non-integer, TT and Torch output = nan
+# input = non-zero and exponent = 0, TT and Torch output = 1
+# Both input and exponent are 0, TT = 1 and Torch output = 0
+def test_binary_pow_scalar_input(input_shapes, input, exponent, device):
+    torch_input_tensor_b = torch.full(input_shapes, exponent, dtype=torch.float32)
+    torch_output_tensor = torch.pow(input, torch_input_tensor_b)
+
+    golden_fn = ttnn.get_golden_function(ttnn.pow)
+    golden_tensor = golden_fn(input, torch_input_tensor_b)
+
+    cq_id = 0
+    input_tensor_b = ttnn.from_torch(torch_input_tensor_b, dtype=ttnn.float32, layout=ttnn.TILE_LAYOUT, device=device)
+    output = ttnn.pow(input, input_tensor_b, queue_id=cq_id)
+    output = ttnn.to_torch(output)
+
+    assert_with_pcc(torch_output_tensor, output, 0.999)
+
+
+def generate_torch_tensor(shape, low, high, step=0.0025, dtype=torch.float32):
+    num_elements = torch.prod(torch.tensor(shape))
+    values = torch.arange(low, high + step, step, dtype=dtype)
+
+    if values.numel() < num_elements:
+        values = values.repeat((num_elements // values.numel()) + 1)
+    values = values[:num_elements]
+    return values.reshape(shape)
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize(
+    "input_shapes",
+    [[64, 640], [2, 32, 320], [2, 1, 32, 1024], [1, 1, 32, 32], [1, 3, 320, 384], [1, 2, 32, 64, 128]],
+)
+def test_binary_sfpu_pow(
+    device,
+    input_shapes,
+):
+    torch_input_tensor_a = generate_torch_tensor(input_shapes, -30, 30, step=0.0022)
+    torch_input_tensor_b = generate_torch_tensor(input_shapes, -20, 20)
+    torch_output_tensor = torch.pow(torch_input_tensor_a, torch_input_tensor_b)
+
+    input_tensor_a = ttnn.from_torch(torch_input_tensor_a, dtype=ttnn.float32, layout=ttnn.TILE_LAYOUT, device=device)
+    input_tensor_b = ttnn.from_torch(torch_input_tensor_b, dtype=ttnn.float32, layout=ttnn.TILE_LAYOUT, device=device)
+
+    output = ttnn.pow(input_tensor_a, input_tensor_b)
+    output = ttnn.to_torch(output)
+
+    pcc = ttnn.pearson_correlation_coefficient(torch_output_tensor, output)
+    assert pcc >= 0.99
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize(
+    "input_shapes",
+    [[64, 640], [2, 32, 320], [2, 1, 1024, 1024], [1, 1, 32, 32], [1, 3, 320, 384], [1, 2, 32, 64, 64]],
+)
+def test_binary_sfpu_pow_bf16(
+    device,
+    input_shapes,
+):
+    torch_input_tensor_a = generate_torch_tensor(input_shapes, -30, 30, step=0.0021, dtype=torch.bfloat16)
+    torch_input_tensor_b = generate_torch_tensor(input_shapes, -20, 20, dtype=torch.bfloat16)
+    torch_output_tensor = torch.pow(torch_input_tensor_a, torch_input_tensor_b)
+
+    input_tensor_a = ttnn.from_torch(torch_input_tensor_a, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device)
+    input_tensor_b = ttnn.from_torch(torch_input_tensor_b, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device)
+
+    output = ttnn.pow(input_tensor_a, input_tensor_b)
+    output = ttnn.to_torch(output)
+
+    pcc = ttnn.pearson_correlation_coefficient(torch_output_tensor, output)
+    assert pcc >= 0.99
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize(
+    "input_shapes",
+    [[2, 1, 32, 1024], [1, 3, 320, 384], [1, 2, 32, 64, 128]],
+)
+def test_binary_sfpu_pow_pos(
+    device,
+    input_shapes,
+):
+    torch_input_tensor_a = generate_torch_tensor(input_shapes, 0, 30, step=0.0111)
+    torch_input_tensor_b = generate_torch_tensor(input_shapes, -20, 20)
+    torch_output_tensor = torch.pow(torch_input_tensor_a, torch_input_tensor_b)
+
+    input_tensor_a = ttnn.from_torch(torch_input_tensor_a, dtype=ttnn.float32, layout=ttnn.TILE_LAYOUT, device=device)
+    input_tensor_b = ttnn.from_torch(torch_input_tensor_b, dtype=ttnn.float32, layout=ttnn.TILE_LAYOUT, device=device)
+
+    output = ttnn.pow(input_tensor_a, input_tensor_b)
+    output = ttnn.to_torch(output)
+
+    pcc = ttnn.pearson_correlation_coefficient(torch_output_tensor, output)
+    assert pcc >= 0.99
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize(
+    "input_shapes",
+    [[2, 1, 32, 1024], [1, 3, 320, 384], [1, 2, 32, 64, 128]],
+)
+def test_binary_sfpu_pow_neg(
+    device,
+    input_shapes,
+):
+    torch_input_tensor_a = generate_torch_tensor(input_shapes, -30, 0, step=0.0111)
+    torch_input_tensor_b = generate_torch_tensor(input_shapes, 0, 10)
+    torch_output_tensor = torch.pow(torch_input_tensor_a, torch_input_tensor_b)
+
+    input_tensor_a = ttnn.from_torch(torch_input_tensor_a, dtype=ttnn.float32, layout=ttnn.TILE_LAYOUT, device=device)
+    input_tensor_b = ttnn.from_torch(torch_input_tensor_b, dtype=ttnn.float32, layout=ttnn.TILE_LAYOUT, device=device)
+
+    output = ttnn.pow(input_tensor_a, input_tensor_b)
+    output = ttnn.to_torch(output)
+
+    pcc = ttnn.pearson_correlation_coefficient(torch_output_tensor, output)
+    assert pcc >= 0.99
