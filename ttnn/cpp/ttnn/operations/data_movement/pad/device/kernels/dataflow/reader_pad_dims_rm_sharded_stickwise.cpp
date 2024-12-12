6989b6dde4b3ad38b5028045d5fb76a34d1a87cc
Jay Kruer <jkruer@tenstorrent.com>
2024-12-12 23:01:27 +0000

    #0: remove dprints from reader; change l1 ptr type to stop last iteration from getting optimized out.

--- ttnn/cpp/ttnn/operations/data_movement/pad/device/kernels/dataflow/reader_pad_dims_rm_sharded_stickwise.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/pad/device/kernels/dataflow/reader_pad_dims_rm_sharded_stickwise.cpp
@@ -5,12 +5,20 @@
 #include <stdint.h>
 #include <cstring>
 #include "dataflow_api.h"
+#include "ttnn/cpp/ttnn/operations/data_movement/common/kernels/debug.hpp"
 
 #define u8_l1_ptr volatile tt_l1_ptr uint8_t*
 #define u8_vol_ptr volatile uint8_t*
 #define u8_ptr uint8_t*
 
+#define DEBUG 1
+
+#ifdef DEBUG
+#include "debug/dprint.h"
+#endif
+
 void kernel_main() {
+    DPRINT << "entered reader" << ENDL();
     constexpr uint32_t unpadded_stick_bytes         = get_compile_time_arg_val(0);
     constexpr uint32_t padded_stick_bytes         = get_compile_time_arg_val(1);
     constexpr uint32_t unpadded_shard_height        = get_compile_time_arg_val(2);
@@ -30,8 +38,8 @@ void kernel_main() {
     // we start writing into the output shard
     uint32_t output_shard_addr = output_shard_base_addr + output_offset_bytes;
 
-    auto input_stick_ptr = reinterpret_cast<u8_vol_ptr>(input_shard_base_addr);
-    auto output_stick_ptr = reinterpret_cast<u8_vol_ptr>(output_shard_addr);
+    auto input_stick_ptr = reinterpret_cast<u8_l1_ptr>(input_shard_base_addr);
+    auto output_stick_ptr = reinterpret_cast<u8_l1_ptr>(output_shard_addr);
 
     // fill the sticks that aren't entirely padding with data from the input tensor
     for (uint32_t h = 0; h < unpadded_shard_height; h++) {
@@ -41,11 +49,9 @@ void kernel_main() {
         // front padding
 
         // FIXME: this isn't aligned. we need to do a memcpy for now. we can try
-        // to do a noc_async_read later on with a trick.
+        // to do a noc_async_read for better performance later on with a trick.
 
-        // noc_async_read(output_stick_addr + W_front_pad_bytes, input_stick_addr, unpadded_stick_bytes);
-
-        // NOTE: memcpy is safe here because the input/output tensors have disjoint buffers
+        // NOTE: memcpy is safe here because the input/output tensors have disjoint buffers.
         for (uint32_t i = 0; i < unpadded_stick_bytes; i++) {
             output_stick_ptr[W_front_pad_bytes + i] = input_stick_ptr[i];
         }
