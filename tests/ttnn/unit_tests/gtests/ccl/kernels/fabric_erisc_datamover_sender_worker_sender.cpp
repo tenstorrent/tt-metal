ee05a9c38bace789936171f01923737c4a3b1baa
Sean Nijjar <sean.nijjar@gmail.com>
2024-12-12 18:33:27 +0000

    fix all-gather - use noc0 for packet based fabric writes
    WIP: reduce scatter V2 and multi-input ccl command reader kernel
    
    CHECKPOINT: reduce scatter reader kernel imlp and tests created
    
    time for debug
    
    fix all-gather - use noc0 for packet based fabric writes
    
    checkpoint: get multi-input command processing kernel working with basic tests
    
    checkpoint: Added fabric write support to 2input command processor kernel
    
    no regresisons but new fabric enabled tests not added/tested yet (WIP)
    
    Checkpoint: multi-input command processor (basic) tested with fabric connections
    
    move multi-input reader kernel builder to common location outside of test
    
    Add extra ccl commands:
    
    semaphore inc (remote and local)
    semaphore wait
    
    TODO: add the reader ccl  command kernel support for varying addressing types (rather than a hardcoded number):
    - (Derive from) Semaphore ID
    - raw address
    
    TODO: Add tests for these new commands
    
    When sermaphore based, we can resolve it during command update
    
    chkpt: unit tests not passing:
    
    massive overhaul. Current issue is with command arg alignment.
    
    small fix
    
    more fixes. Not hanging now! :D But PCC issue >:[ but all zeros! So probably just sent it to the wrong chip :)
    
    fix test bug
    
    correctness issue resolved! But hangs during UMD teardown
    
    Add side buffer for packet headers not associated with payloads from CBs
    
    keeping these stashing the rest
    
    sq
    
    fix arg generation bug
    
    revisit: shrink structs and args to avoid stack overflow
    
    checkpoint
    
    bug fix
    
    more bug fixes + compile issue fixes
    
    another test fix
    
    test fixes
    
    fixed some test issues
    
    everything passes with DPRINT
    
    added more tests
    
    added more sophisticated test
    
    a pipeline of workers pushing and pulling from tensors in different orders
    
    uhh... atleast majority of reduce scatter v2 prog factory implemented
    
    compiling but still a small list of functional TODOs:
    
    - [ ] grab neighbour "from remote" input tensors so we can address them from the current chip in program factory
    - [ ] Update CallBack to support looping
    - [ ] Populate tensor sync info on tensor collection at start of factory
    - [ ] Update synchronization to be on a per worker, rather than per slice basis
    - [ ] Ensure proper fabric teardown codepaths all enabled/complete
    
    checkpoint

--- tests/ttnn/unit_tests/gtests/ccl/kernels/fabric_erisc_datamover_sender_worker_sender.cpp
+++ tests/ttnn/unit_tests/gtests/ccl/kernels/fabric_erisc_datamover_sender_worker_sender.cpp
@@ -7,6 +7,7 @@
 #include "dataflow_api.h"
 #include "ttnn/cpp/ttnn/operations/ccl/kernels/edm_fabric/fabric_edm_packet_header.hpp"
 #include "ttnn/cpp/ttnn/operations/ccl/kernels/edm_fabric/edm_fabric_worker_adapters.hpp"
+#include "tests/ttnn/unit_tests/gtests/ccl/kernels/test_kernels.common.hpp"
 
 struct unicast_mode {
     uint8_t distance;
@@ -77,7 +78,8 @@ void kernel_main() {
         config.unicast.distance = static_cast<uint8_t>(get_arg_val<uint32_t>(arg_idx++));
     }
 
-    const InterleavedAddrGen<dest_is_dram> dest_addr_gen = {.bank_base_address = dest_addr, .page_size = page_size};
+    const InterleavedAddrGen<dest_is_dram> dest_addr_gen = {
+        .bank_base_address = dest_addr, .page_size = page_size};
 
     ASSERT(num_buffers_per_channel > 0);
     auto sender = tt::fabric::WorkerToFabricEdmSender(
@@ -107,6 +109,7 @@ void kernel_main() {
     uint32_t buffer_index = 0;
     cb_wait_front(cb_id_in0, 1);
     auto a_packet_header_addr = get_read_ptr(cb_id_in0);
+    DPRINT << "total_pages_to_send: " << (uint32_t)total_pages_to_send << "\n";
     for (uint32_t p = 0; p < total_pages_to_send; p += num_pages_per_send) {
         uint32_t pages_to_send = std::min<uint32_t>(num_pages_per_send, total_pages_to_send - p);
         sender.wait_for_empty_write_slot();
@@ -167,40 +170,9 @@ void kernel_main() {
         noc_semaphore_wait(last_message_semaphore_address, 1);
     }
 
-    bool closed = false;
-    size_t num_endpoints_to_terminate = get_arg_val<uint32_t>(arg_idx++);
-    for (size_t i = 0; i < num_endpoints_to_terminate; i++) {
-        size_t edm_noc_x = get_arg_val<uint32_t>(arg_idx++);
-        size_t edm_noc_y = get_arg_val<uint32_t>(arg_idx++);
-        size_t distance = get_arg_val<uint32_t>(arg_idx++);
-        size_t termination_addr = get_arg_val<uint32_t>(arg_idx++);
+    bool closed_fabric_connection = terminate_fabric_endpoints_farthest_to_nearest(sender, a_packet_header_addr, arg_idx);
 
-        if (!closed && distance == 0) {
-            closed = true;
-            sender.close();
-        }
-        if (distance == 0) {
-            noc_inline_dw_write(
-                get_noc_addr(edm_noc_x, edm_noc_y, termination_addr),
-                tt::fabric::TerminationSignal::GRACEFULLY_TERMINATE);
-        } else {
-            auto& packet_header = *reinterpret_cast<tt::fabric::PacketHeader*>(a_packet_header_addr);
-            reinterpret_cast<volatile uint32_t*>(a_packet_header_addr)[sizeof(tt::fabric::PacketHeader) >> 2] =
-                tt::fabric::TerminationSignal::GRACEFULLY_TERMINATE;
-            sender.wait_for_empty_write_slot();
-            packet_header.to_write()
-                .to_chip_unicast(tt::fabric::UnicastRoutingCommandHeader{static_cast<uint8_t>(distance/* - 1*/)})
-                .to_noc_unicast(tt::fabric::NocUnicastCommandHeader{
-                    termination_addr,
-                    sizeof(tt::fabric::PacketHeader) + sizeof(uint32_t),
-                    static_cast<uint8_t>(edm_noc_x),
-                    static_cast<uint8_t>(edm_noc_y)});
-            sender.send_payload_blocking_from_address(
-                a_packet_header_addr, packet_header.get_payload_size_including_header());
-            noc_async_writes_flushed();
-        }
-    }
-    if (!closed) {
+    if (!closed_fabric_connection) {
         sender.close();
     }
 }
