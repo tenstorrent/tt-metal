30f195e6c0bfacfd8008971dca5ab627ade0361a
Andrew Fuller <afuller@tenstorrent.com>
2024-12-12 17:56:52 -0500

    Enable throw-by-val/catch-by-ref check (#15981)
    ### Ticket
    #15123 
    
    ### Problem description
    C++ should always throw exceptions by value and catch them by reference.
    
    ### What's changed
    Enabled the check in clang-tidy.
    Fixed the violations.

--- tests/tt_metal/tt_metal/perf_microbenchmark/ethernet/test_ethernet_bidirectional_bandwidth_no_edm.cpp
+++ tests/tt_metal/tt_metal/perf_microbenchmark/ethernet/test_ethernet_bidirectional_bandwidth_no_edm.cpp
@@ -270,7 +270,7 @@ int main(int argc, char** argv) {
                 }
             }
         }
-    } catch (std::exception e) {
+    } catch (std::exception& e) {
         test_fixture.TearDown();
         return -1;
     }
