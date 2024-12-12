b72292d052637495f5dd4e25d550025154a247a7
Andrew Fuller <afuller@tenstorrent.com>
2024-12-12 23:41:58 +0000

    Enable noexcept-move-ctor check

--- tt_metal/impl/allocator/allocator.cpp
+++ tt_metal/impl/allocator/allocator.cpp
@@ -169,7 +169,7 @@ BankManager::~BankManager() {
     this->allocator_.reset(nullptr);
 }
 
-BankManager&& BankManager::operator=(BankManager&& that) {
+BankManager&& BankManager::operator=(BankManager&& that) noexcept {
     buffer_type_ = that.buffer_type_;
     allocated_buffers_ = that.allocated_buffers_;
     bank_id_to_bank_offset_ = that.bank_id_to_bank_offset_;
