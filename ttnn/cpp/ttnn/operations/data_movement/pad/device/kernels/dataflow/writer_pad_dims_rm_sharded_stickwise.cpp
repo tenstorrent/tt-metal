c37841c970d216ad4b0c105569964c5be424ca7d
Jay Kruer <jkruer@tenstorrent.com>
2024-12-13 00:11:46 +0000

    #0: a few optimizations for the writer

--- ttnn/cpp/ttnn/operations/data_movement/pad/device/kernels/dataflow/writer_pad_dims_rm_sharded_stickwise.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/pad/device/kernels/dataflow/writer_pad_dims_rm_sharded_stickwise.cpp
@@ -8,24 +8,23 @@
 
 #define u16_l1_ptr volatile tt_l1_ptr uint16_t*
 #define u32_l1_ptr volatile tt_l1_ptr uint32_t*
-#define u8_l1_ptr volatile tt_l1_ptr uint8_t*
 
-#define u8_ptr uint8_t*
-
-template <uint32_t padding_value_num_bytes>
+template <uint32_t padding_value_num_bytes, uint32_t num_bytes>
 inline __attribute__((always_inline)) void fill_cb_with_padding_value(
-    const uint32_t cb, const uint32_t num_bytes, const uint32_t padding_value_as_u32) {
+    const uint32_t cb, const uint32_t padding_value_as_u32) {
+    constexpr uint32_t num_elts =
+        num_bytes / padding_value_num_bytes;  // constexpr so that this division happens once on host
     uint32_t cb_write_addr = get_write_ptr(cb);
 
     if constexpr (padding_value_num_bytes == 4) {
         u32_l1_ptr cb_write_addr_as_u32 = reinterpret_cast<u32_l1_ptr>(cb_write_addr);
-        for (uint32_t i = 0; i < num_bytes / sizeof(uint32_t); i++) {
+        for (uint32_t i = 0; i < num_elts; i++) {
             cb_write_addr_as_u32[i] = padding_value_as_u32;
         }
     } else if constexpr (padding_value_num_bytes == 2) {
         uint16_t padding_value_as_u16 = static_cast<uint16_t>(padding_value_as_u32);
         u16_l1_ptr cb_write_addr_as_u16 = reinterpret_cast<u16_l1_ptr>(cb_write_addr);
-        for (uint32_t i = 0; i < num_bytes / sizeof(uint16_t); i++) {
+        for (uint32_t i = 0; i < num_elts; i++) {
             cb_write_addr_as_u16[i] = padding_value_as_u16;
         }
     } else {
@@ -46,20 +45,16 @@ void kernel_main() {
     cb_reserve_back(output_shard_cb, padded_shard_height);
     uint32_t output_shard_base_addr = get_write_ptr(output_shard_cb);
 
-    fill_cb_with_padding_value<padding_value_num_bytes>(padding_value_cb, padded_stick_bytes, padding_value_as_u32);
+    fill_cb_with_padding_value<padding_value_num_bytes, padded_stick_bytes>(padding_value_cb, padding_value_as_u32);
     uint32_t padding_value_base_addr = get_read_ptr(padding_value_cb);
 
-    uint32_t output_stick_addr = output_shard_base_addr;
+    uint64_t output_stick_noc_addr = get_noc_addr(output_shard_base_addr);
     for (uint32_t h = 0; h < padded_shard_height; h++) {
-        auto output_stick_ptr = reinterpret_cast<u8_ptr>(output_stick_addr);
-        auto padding_value_ptr = reinterpret_cast<u8_ptr>(padding_value_base_addr);
-
-        for (uint32_t i = 0; i < padded_stick_bytes; i++) {
-            output_stick_ptr[i] = padding_value_ptr[i];
-        }
+        noc_async_write(padding_value_base_addr, output_stick_noc_addr, padded_stick_bytes);
+        noc_async_write_barrier();
 
         cb_push_back(output_shard_cb, 1);
 
-        output_stick_addr += padded_stick_bytes;
+        output_stick_noc_addr += padded_stick_bytes;
     }
 }
