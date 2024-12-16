44e19f8de6c5485db71d993bd2a50d681dc8b82c
kpaigwar <kpaigwar@tenstorrent.com>
2024-12-16 16:44:26 +0000

    #0: supported subcoregrid in concat heads

--- ttnn/cpp/ttnn/operations/experimental/transformer/nlp_concat_heads_decode/device/nlp_concat_heads_decode_device_operation.hpp
+++ ttnn/cpp/ttnn/operations/experimental/transformer/nlp_concat_heads_decode/device/nlp_concat_heads_decode_device_operation.hpp
@@ -15,8 +15,12 @@ namespace ttnn::operations::experimental::transformer {
 operation::ProgramWithCallbacks multi_core_nlp_concat_heads_decode(
     const Tensor& input_tensor, Tensor& output, CoreCoord compute_with_storage_grid_size);
 
+operation::ProgramWithCallbacks multi_core_nlp_concat_heads_decode_subcoregrids(
+    const Tensor& input_tensor, Tensor& output, CoreCoord compute_with_storage_grid_size);
+
 struct NLPConcatHeadsDecodeDeviceOperation {
     const uint32_t num_heads;
+    const bool on_subcoregrids = false;
 
     void validate(const std::vector<Tensor>& input_tensors) const;
     std::vector<tt::tt_metal::LegacyShape> compute_output_shapes(const std::vector<Tensor>& input_tensors) const;
