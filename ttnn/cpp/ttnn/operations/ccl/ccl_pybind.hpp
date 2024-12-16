300a064d24b4a2dbd73e3ebc99951d885c75130f
Sean Nijjar <sean.nijjar@gmail.com>
2024-12-16 18:45:43 +0000

    sq with other branches

--- ttnn/cpp/ttnn/operations/ccl/ccl_pybind.hpp
+++ ttnn/cpp/ttnn/operations/ccl/ccl_pybind.hpp
@@ -8,8 +8,6 @@
 #include "ttnn/operations/ccl/reduce_scatter/reduce_scatter_pybind.hpp"
 #include "ttnn/operations/ccl/barrier/barrier_pybind.hpp"
 
-// #include "ttnn/cpp/pybind11/decorators.hpp"
-
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
@@ -19,11 +17,6 @@ namespace ttnn::operations::ccl {
 
 void py_bind_common(pybind11::module& module);
 
-void py_module(py::module& module) {
-    ccl::py_bind_common(module);
-    ccl::py_bind_all_gather(module);
-    ccl::py_bind_reduce_scatter(module);
-    ccl::py_bind_barrier(module);
-}
+void py_module(py::module& module);
 
 }  // namespace ttnn::operations::ccl
