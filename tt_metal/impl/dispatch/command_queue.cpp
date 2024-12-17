714651dc2f944000f9a6fa6e9e28143fa060d989
John Bauman <jbauman@tenstorrent.com>
2024-12-17 20:05:21 +0000

    #15018: Increase linking of program binary multicasts
    The BRISC, NCRISC, and set of all TRISCs each have their own entry in
    kq_transfer_info.dst_base_addrs, which prevents the current code from linking
    their transactions. Modify the code to avoid unlinking in the case where writes
    to the same set of cores happen to be contiguous (which happens to be always).
    
    This reduces the cost of linking and will help a lot more in the future, once
    we put barriers between unlinked mcasts.

--- tt_metal/impl/dispatch/command_queue.cpp
+++ tt_metal/impl/dispatch/command_queue.cpp
@@ -1029,6 +1029,12 @@ void EnqueueProgramCommand::assemble_device_commands(
                         read_length = max_paged_length_per_sub_cmd;
                         write_length = read_length;
                     }
+                    if (!kernel_bins_dispatch_subcmds.back().empty()) {
+                        auto& back = kernel_bins_dispatch_subcmds.back().back();
+                        if (back.noc_xy_addr != noc_encoding) {
+                            back.flags = CQ_DISPATCH_CMD_PACKED_WRITE_LARGE_FLAG_UNLINK;
+                        }
+                    }
                     kernel_bins_dispatch_subcmds.back().emplace_back(CQDispatchWritePackedLargeSubCmd{
                         .noc_xy_addr = noc_encoding,
                         .addr = kernel_config_buffer_offset,
@@ -1050,9 +1056,13 @@ void EnqueueProgramCommand::assemble_device_commands(
                 }
             }
         }
-        // Unlink the last subcmd of the current core range
-        if (!write_linear) {
-            kernel_bins_dispatch_subcmds.back().back().flags |= CQ_DISPATCH_CMD_PACKED_WRITE_LARGE_FLAG_UNLINK;
+    }
+    // Unlink the last subcmd of every dispatch, to ensure we don't hold the
+    // path reservation for an incredible long time. This also prevents a hang
+    // if the next mcast is to a different destination.
+    for (auto& subcmd_list : kernel_bins_dispatch_subcmds) {
+        if (!subcmd_list.empty()) {
+            subcmd_list.back().flags |= CQ_DISPATCH_CMD_PACKED_WRITE_LARGE_FLAG_UNLINK;
         }
     }
     uint32_t pcie_alignment = hal.get_alignment(HalMemType::HOST);
