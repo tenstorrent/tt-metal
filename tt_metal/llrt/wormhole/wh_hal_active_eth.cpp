5381e4370ab2b67e8dd8a1fa3f938b222930a258
Bojan Roško <156314064+broskoTT@users.noreply.github.com>
2024-12-17 13:08:13 +0100

    [UMD] Removed set_*_params calls and constants (#15908)
    ### Ticket
    Related to https://github.com/tenstorrent/tt-metal/issues/13948
    
    ### Problem description
    Related to UMD change: https://github.com/tenstorrent/tt-umd/pull/395
    These parameters are defined by hardware.
    The only parameters to be defined by the client are membars. This change
    adds set_barrier_address_params accordingly.
    
    ### What's changed
    Removed set_device_dram_address_params and set_device_l1_address_params,
    and related constants.
    Replace with call to set_barrier_address_params.
    
    ### Checklist
    - [ ] All post-commit tests :
    https://github.com/tenstorrent/tt-metal/actions/runs/12361649190 - main
    generally unhealthy, I haven't seen new fails related to this change.
    - [x] Blackhole post-commit tests :
    https://github.com/tenstorrent/tt-metal/actions/runs/12361650912
    - [ ] (Single-card) Model perf tests :
    https://github.com/tenstorrent/tt-metal/actions/runs/12361652594 - N300
    job failing on main
    - [x] (Single-card) Device perf regressions :
    https://github.com/tenstorrent/tt-metal/actions/runs/12361654455
    - [x] (T3K) T3000 unit tests :
    https://github.com/tenstorrent/tt-metal/actions/runs/12361656223
    - [ ] (T3K) T3000 demo tests :
    https://github.com/tenstorrent/tt-metal/actions/runs/12361657485 -
    falcon job failing on main
    - [ ] (TG) TG unit tests :
    https://github.com/tenstorrent/tt-metal/actions/runs/12361658889 - unit
    test failing on main
    - [ ] (TG) TG demo tests :
    https://github.com/tenstorrent/tt-metal/actions/runs/12361660547 -
    failing on main
    - [ ] (TGG) TGG unit tests :
    https://github.com/tenstorrent/tt-metal/actions/runs/12361662343 -
    failing on main for a long time due to hugepages
    - [x] (TGG) TGG demo tests :
    https://github.com/tenstorrent/tt-metal/actions/runs/12361664160

--- tt_metal/llrt/wormhole/wh_hal_active_eth.cpp
+++ tt_metal/llrt/wormhole/wh_hal_active_eth.cpp
@@ -41,8 +41,6 @@ HalCoreInfoType create_active_eth_mem_map() {
     mem_map_bases[static_cast<std::size_t>(HalL1MemAddrType::GO_MSG)] = GET_ETH_MAILBOX_ADDRESS_HOST(go_message);
     mem_map_bases[static_cast<std::size_t>(HalL1MemAddrType::LAUNCH_MSG_BUFFER_RD_PTR)] =
         GET_ETH_MAILBOX_ADDRESS_HOST(launch_msg_rd_ptr);
-    mem_map_bases[static_cast<std::size_t>(HalL1MemAddrType::FW_VERSION_ADDR)] =
-        eth_l1_mem::address_map::FW_VERSION_ADDR;
     mem_map_bases[static_cast<std::size_t>(HalL1MemAddrType::BANK_TO_NOC_SCRATCH)] =
         eth_l1_mem::address_map::ERISC_MEM_BANK_TO_NOC_SCRATCH;
 
@@ -63,7 +61,6 @@ HalCoreInfoType create_active_eth_mem_map() {
         eth_l1_mem::address_map::MAX_SIZE - eth_l1_mem::address_map::ERISC_L1_UNRESERVED_BASE;
     mem_map_sizes[static_cast<std::size_t>(HalL1MemAddrType::GO_MSG)] = sizeof(go_msg_t);
     mem_map_sizes[static_cast<std::size_t>(HalL1MemAddrType::LAUNCH_MSG_BUFFER_RD_PTR)] = sizeof(uint32_t);
-    mem_map_sizes[static_cast<std::size_t>(HalL1MemAddrType::FW_VERSION_ADDR)] = sizeof(std::uint32_t);
     mem_map_sizes[static_cast<std::size_t>(HalL1MemAddrType::BANK_TO_NOC_SCRATCH)] = eth_l1_mem::address_map::ERISC_MEM_BANK_TO_NOC_SIZE;
 
     std::vector<std::vector<HalJitBuildConfig>> processor_classes(NumEthDispatchClasses);
