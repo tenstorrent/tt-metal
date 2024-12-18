c252e0eef0c0b0ed5bd9a972b22221e15bfbfdd8
Abhinav Sarje <asarje@tenstorrent.com>
2024-12-18 21:35:49 +0000

    Utility functions to dprint data from l1 addresses

---
+++ tt_metal/hw/inc/debug/dprint_l1_data.h
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#if defined(DEBUG_PRINT_ENABLED) && !defined(FORCE_DPRINT_OFF)
+#if defined(COMPILE_FOR_NCRISC) | defined(COMPILE_FOR_BRISC)
+
+#include "dprint.h"
+
+// To print data sequentially from given L1 address
+
+inline void print_pages_bf16(uint32_t l1_addr, uint32_t pagelen, uint32_t npages, uint32_t start = 0) {
+    volatile tt_l1_ptr uint16_t* ptr = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(l1_addr) + start * pagelen;
+    for (uint32_t page = 0; page < npages; ++page) {
+        DPRINT << start + page << ": ";
+        for (uint32_t j = 0; j < pagelen; ++j, ++ptr) {
+            DPRINT << BF16(*ptr) << " ";
+        }
+        DPRINT << ENDL();
+    }
+}
+
+inline void print_data_u16(uint32_t l1_addr, uint32_t pagelen, uint32_t npages, uint32_t start = 0) {
+    volatile tt_l1_ptr uint16_t* ptr = reinterpret_cast<volatile tt_l1_ptr uint16_t*>(l1_addr) + start * pagelen;
+    for (uint32_t page = 0; page < npages; ++page) {
+        DPRINT << start + page << ": ";
+        for (uint32_t j = 0; j < pagelen; ++j, ++ptr) {
+            DPRINT << uint16_t(*ptr) << " ";
+        }
+        DPRINT << ENDL();
+    }
+}
+
+#endif
+#endif
