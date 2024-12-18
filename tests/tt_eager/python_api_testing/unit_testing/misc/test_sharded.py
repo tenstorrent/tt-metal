c9cbdff8598031231d2f0d6bb06e3ea837fe4cdc
Ligang Long <llong@tenstorrent.com>
2024-12-18 00:00:32 +0000

    #13609: fix the failed test_sharded tests, using keep_l1_aligned flag

--- tests/tt_eager/python_api_testing/unit_testing/misc/test_sharded.py
+++ tests/tt_eager/python_api_testing/unit_testing/misc/test_sharded.py
@@ -168,7 +168,7 @@ def test_sharded_rm(
         ),
     )
 
-    yt = ttnn.interleaved_to_sharded(xt, grid_size, shard_size, shard_scheme, shard_orientation)
+    yt = ttnn.interleaved_to_sharded(xt, grid_size, shard_size, shard_scheme, shard_orientation, keep_l1_aligned=True)
 
     zt = ttnn.sharded_to_interleaved(
         yt,
@@ -176,6 +176,7 @@ def test_sharded_rm(
             memory_layout=ttnn.TensorMemoryLayout.INTERLEAVED,
             buffer_type=ttnn.BufferType.L1,
         ),
+        is_l1_aligned=True,
     )
 
     tt_og = xt.cpu().to_torch()
