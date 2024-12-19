f5be04ef283a5b6cc89698631c4830e10471ad23
Stuti Raizada <sraizada@tenstorrent.com>
2024-12-19 05:37:05 -0600

    #0: Add init function

--- tests/tt_eager/python_api_testing/unit_testing/misc/test_untilize_test.py
+++ tests/tt_eager/python_api_testing/unit_testing/misc/test_untilize_test.py
@@ -13,35 +13,38 @@ from models.utility_functions import is_grayskull, skip_for_blackhole
 
 @pytest.mark.parametrize(
     "dtype",
-    (ttnn.bfloat16, ttnn.float32),
-    ids=["bfloat16", "float"],
+    (ttnn.bfloat16,),
+    ids=[
+        "bfloat16",
+    ],
 )
 @pytest.mark.parametrize(
     "nb, nc, nh, nw",
     (
-        (1, 1, 1, 2),
-        (5, 2, 4, 8),
-        (5, 2, 4, 7),
-        ## resnet shapes
-        (1, 1, 1, 1),
-        (1, 1, 7, 8),
-        (1, 1, 49, 1),
-        (1, 1, 49, 16),
-        (1, 1, 49, 32),
-        (1, 1, 196, 4),
-        (1, 1, 196, 8),
-        (1, 1, 196, 16),
-        (1, 1, 784, 2),
-        (1, 1, 784, 4),
-        (1, 1, 784, 8),
-        (1, 1, 3136, 2),
+        # (1, 1, 1, 2),
+        # (5, 2, 4, 8),
+        # (5, 2, 4, 7),
+        # ## resnet shapes
+        # (1, 1, 1, 1),
+        # (1, 1, 7, 8),
+        # (1, 1, 49, 1),
+        # (1, 1, 49, 16),
+        # (1, 1, 49, 32),
+        # (1, 1, 196, 4),
+        # (1, 1, 196, 8),
+        # (1, 1, 196, 16),
+        # (1, 1, 784, 2),
+        # (1, 1, 784, 4),
+        # (1, 1, 784, 8),
+        # (1, 1, 3136, 2),
+        (1, 1, 32, 16 * 1024),
     ),
 )
 def test_run_untilize_test(dtype, nb, nc, nh, nw, device):
     if is_grayskull() and dtype == ttnn.float32:
         pytest.skip("Skipping float32 tests on Grayskull")
-
-    shape = [nb, nc, 32 * nh, 32 * nw]
+    device.enable_async(True)
+    shape = [nb, nc, nh, nw]
 
     torch.set_printoptions(precision=3, sci_mode=False, linewidth=3000, threshold=10000, edgeitems=128)
 
@@ -62,7 +65,7 @@ def test_run_untilize_test(dtype, nb, nc, nh, nw, device):
 
     out_mem_config = ttnn.MemoryConfig(ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.L1)
 
-    b1 = ttnn.untilize(a, memory_config=out_mem_config, use_multicore=True, use_pack_untilize=True)
+    b1 = ttnn.untilize(a, memory_config=out_mem_config, use_multicore=True)
     c1 = b1.cpu().to_torch()
 
     untilized_inp = untilize(inp)
