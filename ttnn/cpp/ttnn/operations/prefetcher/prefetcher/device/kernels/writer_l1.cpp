4390b4ae0c0107897cef7058bca2b9ae299e05d3
Austin Ho <aho@tenstorrent.com>
2024-12-19 18:12:32 +0000

    #0: Add missing call to update persistent state in prefetcher

--- ttnn/cpp/ttnn/operations/prefetcher/prefetcher/device/kernels/writer_l1.cpp
+++ ttnn/cpp/ttnn/operations/prefetcher/prefetcher/device/kernels/writer_l1.cpp
@@ -24,34 +24,6 @@ FORCE_INLINE uint32_t get_fifo_start_size(uint32_t cb_id) {
     return fifo_size;
 }
 
-FORCE_INLINE void resize_local_cb_interface(
-    uint32_t cb_id, uint32_t page_size, uint32_t fifo_start_addr, uint32_t fifo_start_size) {
-    LocalCBInterface& local_cb = get_local_cb_interface(cb_id);
-
-    uint32_t fifo_limit = local_cb.fifo_limit;
-
-    uint32_t fifo_wr_ptr = local_cb.fifo_wr_ptr;
-    uint32_t fifo_rd_ptr = local_cb.fifo_rd_ptr;
-    uint32_t fifo_size_page_aligned = fifo_start_size - fifo_start_size % page_size;
-    uint32_t fifo_limit_page_aligned = fifo_start_addr + fifo_size_page_aligned;
-    uint32_t fifo_num_pages = fifo_size_page_aligned / page_size;
-
-    uint32_t next_fifo_wr_ptr = fifo_start_addr + align(fifo_wr_ptr - fifo_start_addr, page_size);
-    if (next_fifo_wr_ptr >= fifo_limit_page_aligned) {
-        next_fifo_wr_ptr = fifo_start_addr;
-    }
-    uint32_t next_fifo_rd_ptr = fifo_start_addr + align(fifo_rd_ptr - fifo_start_addr, page_size);
-    if (next_fifo_rd_ptr >= fifo_limit_page_aligned) {
-        next_fifo_rd_ptr = fifo_start_addr;
-    }
-    local_cb.fifo_limit = fifo_limit_page_aligned;
-    local_cb.fifo_size = fifo_size_page_aligned;
-    local_cb.fifo_page_size = page_size;
-    local_cb.fifo_num_pages = fifo_num_pages;
-    local_cb.fifo_wr_ptr = next_fifo_wr_ptr;
-    local_cb.fifo_rd_ptr = next_fifo_rd_ptr;
-}
-
 FORCE_INLINE uint32_t get_local_cb_rd_ptr(uint32_t cb_id, uint32_t fifo_start_addr) {
     LocalCBInterface& local_cb = get_local_cb_interface(cb_id);
     return local_cb.fifo_rd_ptr - fifo_start_addr;
@@ -76,57 +48,7 @@ FORCE_INLINE void print_remote_fifo(uint32_t cb_id) {
     uint32_t fifo_aligned_num_pages = cb.fifo_limit_page_aligned / REMOTE_CIRCULAR_BUFFER_ALIGNED_PAGE_SIZE;
     uint32_t free_pages = fifo_aligned_num_pages - (*pages_sent_ptr - *pages_acked_ptr);
 }
-template <bool update_remote_over_noc = false>
-FORCE_INLINE void resize_remote_sender_cb_interface_(uint32_t cb_id, uint32_t page_size, uint8_t noc) {
-    ASSERT(page_size % REMOTE_CIRCULAR_BUFFER_ALIGNED_PAGE_SIZE == 0);
-    RemoteSenderCBInterface& sender_cb_interface = get_remote_sender_cb_interface(cb_id);
-    uint32_t fifo_size = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(sender_cb_interface.config_ptr)[3];
-    uint32_t fifo_start_addr = sender_cb_interface.fifo_start_addr;
-    uint32_t fifo_wr_ptr = sender_cb_interface.fifo_wr_ptr;
-    uint32_t cb_size_page_aligned = fifo_size - fifo_size % page_size;
-    uint32_t fifo_limit_page_aligned = fifo_start_addr + cb_size_page_aligned;
-    uint32_t prev_fifo_limit_page_aligned = sender_cb_interface.fifo_limit_page_aligned;
 
-    uint32_t next_fifo_wr_ptr = fifo_start_addr + align(fifo_wr_ptr - fifo_start_addr, page_size);
-    if (next_fifo_wr_ptr >= fifo_limit_page_aligned) {
-        next_fifo_wr_ptr = fifo_start_addr;
-    } else {
-#ifndef COMPILE_FOR_TRISC
-        if constexpr (update_remote_over_noc) {
-            uint32_t aligned_pages_sent_addr = sender_cb_interface.aligned_pages_sent_ptr;
-            uint32_t remote_noc_xy_addr = sender_cb_interface.receiver_noc_xy_ptr;
-            uint32_t num_receivers = sender_cb_interface.num_receivers;
-            uint32_t aligned_page_adjustment =
-                (next_fifo_wr_ptr - fifo_wr_ptr) / REMOTE_CIRCULAR_BUFFER_ALIGNED_PAGE_SIZE;
-            if (prev_fifo_limit_page_aligned < fifo_limit_page_aligned) {
-                aligned_page_adjustment +=
-                    (fifo_limit_page_aligned - prev_fifo_limit_page_aligned) / REMOTE_CIRCULAR_BUFFER_ALIGNED_PAGE_SIZE;
-            }
-            // increment the aligned pages sent because we skipped to next aligned page location
-            volatile tt_l1_ptr uint32_t* pages_sent_ptr =
-                reinterpret_cast<volatile tt_l1_ptr uint32_t*>(aligned_pages_sent_addr);
-            volatile tt_l1_ptr uint32_t* remote_noc_xy_ptr =
-                reinterpret_cast<volatile tt_l1_ptr uint32_t*>(remote_noc_xy_addr);
-            for (uint32_t i = 0; i < num_receivers; ++i) {
-                uint32_t remote_noc_xy = uint32_t(NOC_XY_ENCODING(
-                    DYNAMIC_NOC_X(noc, remote_noc_xy_ptr[0]), DYNAMIC_NOC_Y(noc, remote_noc_xy_ptr[1])));
-                *pages_sent_ptr += aligned_page_adjustment;
-                uint64_t remote_ack_ptr_addr = get_noc_addr_helper(remote_noc_xy, (uint32_t)pages_sent_ptr);
-                if (aligned_page_adjustment != 0) {
-                    noc_semaphore_inc(remote_ack_ptr_addr, aligned_page_adjustment, noc);
-                }
-                pages_sent_ptr += 2 * L1_ALIGNMENT / sizeof(uint32_t);
-                remote_noc_xy_ptr += 2;
-
-                // DPRINT << "aligned_page_adjustment " << aligned_page_adjustment <<ENDL();
-            }
-        }
-#endif
-    }
-    sender_cb_interface.fifo_wr_ptr = next_fifo_wr_ptr;
-    sender_cb_interface.fifo_limit_page_aligned = fifo_limit_page_aligned;
-    sender_cb_interface.fifo_page_size = page_size;
-}
 /*
 TODO:
     - How do the coalesced sizes differ?
@@ -178,7 +100,7 @@ void kernel_main() {
             uint32_t curr_block_size = curr_block_num_tiles * curr_single_tile_sizes;
             uint32_t curr_block_size_per_receiver = curr_block_size / num_receivers;
 
-            resize_remote_sender_cb_interface_<true>(remote_cb_id, curr_block_size_per_receiver, noc);
+            experimental::resize_remote_sender_cb_interface<true>(remote_cb_id, curr_block_size_per_receiver, noc);
             {
                 DeviceZoneScopedN("reserve_back");
                 experimental::remote_cb_reserve_back(remote_cb_id, num_blocks);
@@ -215,4 +137,5 @@ void kernel_main() {
             }
         }
     }
+    experimental::update_remote_cb_config_in_l1(remote_cb_id);
 }
