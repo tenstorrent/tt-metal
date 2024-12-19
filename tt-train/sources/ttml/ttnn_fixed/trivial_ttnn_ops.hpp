9874d0e231e69c9b64a921f7a8deba060361267f
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2024-12-18 19:10:44 -0800

    Update tt-train/sources/ttml/ttnn_fixed/trivial_ttnn_ops.hpp
    Co-authored-by: Roman Furko <rfurko@tenstorrent.com>

--- tt-train/sources/ttml/ttnn_fixed/trivial_ttnn_ops.hpp
+++ tt-train/sources/ttml/ttnn_fixed/trivial_ttnn_ops.hpp
@@ -15,7 +15,7 @@ tt::tt_metal::Tensor log_softmax(const tt::tt_metal::Tensor& t, int dim);
 tt::tt_metal::Tensor softmax(const tt::tt_metal::Tensor& t, int dim);
 tt::tt_metal::Tensor divide(const tt::tt_metal::Tensor& a, const tt::tt_metal::Tensor& b);
 
-tt::tt_metal ::Tensor mean_moreh(const tt::tt_metal::Tensor& t, int dim, bool keep_dim);
+tt::tt_metal::Tensor mean_moreh(const tt::tt_metal::Tensor& t, int dim, bool keep_dim);
 tt::tt_metal::Tensor mean_ttnn(const tt::tt_metal::Tensor& t, int dim, bool keep_dim);
 
 tt::tt_metal ::Tensor sum_moreh(const tt::tt_metal::Tensor& t, int dim, bool keep_dim);
