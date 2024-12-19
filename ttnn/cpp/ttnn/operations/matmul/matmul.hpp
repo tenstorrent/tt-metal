d1c6a98d9edc88737f6e596f7d977b91209bf313
avoraTT <avora@tenstorrent.com>
2024-12-19 15:42:01 -0600

    Build broken, trying to fix rebase issues with GlobalCircularBuffer.

--- ttnn/cpp/ttnn/operations/matmul/matmul.hpp
+++ ttnn/cpp/ttnn/operations/matmul/matmul.hpp
@@ -50,7 +50,7 @@ struct MatmulOperation {
         const std::optional<const CoreGrid> core_grid = std::nullopt,
         const std::optional<const tt::tt_metal::Tile>& output_tile = std::nullopt,
         std::optional<Tensor> optional_output_tensor = std::nullopt,
-        const std::optional<const tt::tt_metal::v1::experimental::GlobalCircularBuffer>& global_cb = std::nullopt);
+        const std::shared_ptr<const tt::tt_metal::v1::experimental::GlobalCircularBuffer>& global_cb = nullptr);
 };
 
 struct LinearOperation {
@@ -68,7 +68,7 @@ struct LinearOperation {
         const std::optional<const CoreGrid> core_grid = std::nullopt,
         const std::optional<const tt::tt_metal::Tile>& output_tile = std::nullopt,
         std::optional<Tensor> optional_output_tensor = std::nullopt,
-        const std::optional<const tt::tt_metal::v1::experimental::GlobalCircularBuffer>& global_cb = std::nullopt);
+        const std::shared_ptr<const tt::tt_metal::v1::experimental::GlobalCircularBuffer>& global_cb = nullptr);
 };
 
 }  // namespace matmul
--- ttnn/cpp/ttnn/operations/prefetcher/prefetcher/device/dram_prefetcher_op.hpp
+++ ttnn/cpp/ttnn/operations/prefetcher/prefetcher/device/dram_prefetcher_op.hpp
@@ -19,12 +19,12 @@ operation::ProgramWithCallbacks dram_prefetcher_multi_core(
     const std::vector<Tensor>& tensors,
     const Tensor& tensor_addrs,
     const uint32_t num_layers,
-    const std::optional<const tt::tt_metal::v1::experimental::GlobalCircularBuffer>& global_cb,
+    const std::shared_ptr<const tt::tt_metal::v1::experimental::GlobalCircularBuffer>& global_cb,
     std::vector<Tensor>& output_tensor);
 
 struct DramPrefetcher {
     const Tensor tensor_addrs = Tensor();
-    const std::optional<const tt::tt_metal::v1::experimental::GlobalCircularBuffer> global_cb;
+    const std::shared_ptr<const tt::tt_metal::v1::experimental::GlobalCircularBuffer> global_cb;
     const uint32_t num_layers;
 
     const MemoryConfig reader_output_mem_config;
@@ -35,6 +35,17 @@ struct DramPrefetcher {
 
     operation::ProgramWithCallbacks create_program(
         const std::vector<Tensor>& input_tensors, std::vector<Tensor>& output_tensors) const;
+
+    static constexpr auto attribute_names = std::forward_as_tuple(
+        "tensor_addrs", "global_cb", "num_layers", "reader_output_mem_config", "writer_output_mem_config");
+    const auto attribute_values() const {
+        return std::forward_as_tuple(
+            this->tensor_addrs,
+            *this->global_cb,
+            this->num_layers,
+            this->reader_output_mem_config,
+            this->writer_output_mem_config);
+    }
 };
 
 }  // namespace ttnn::operations::dram_prefetcher
