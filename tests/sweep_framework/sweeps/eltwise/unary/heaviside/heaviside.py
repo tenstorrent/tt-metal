6db6fed6d5b320a80395706cfd3f68cefda965f0
Andrija Malbasa <amalbasa@tenstorrent.com>
2024-12-23 12:22:06 +0100

    Add unary sharded sweeps (#15300)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/11512)
    
    ### Problem description
    1) Inside file sharded_utils.py, function `gen_sharded_spec_unary` needs
    support for generating input spec when parameter `tile_layout` of a
    function `ttnn.create_sharded_memory_config_` is set to `True`
    2) All existing sharded sweeps should include vector invalidation for
    when `input_layout == "ROW_MAJOR_LAYOUT"` and `shard_orientation ==
    "COL_MAJOR"` and `input_shape[-1] % input_shape[-2] != 0`.
    3) Missing sweeps for sharded versions of ops:
    - triu
    - tril
    - heaviside
    - hardtanh
    - hardsigmoid
    - hardswish
    - hardshrink
    
    
    ### What's changed
    1) Added mention vector invalidation to all existing sharded sweeps
    (including the new ones).
    2) Added sharded versions of sweeps for ops mentioned in the Problem
    description section above.
    3) Minor refactoring of non-sharded versions of added sharded sweeps to
    include shapes with last two dimensions not divisible by 32.
    
    ### Pass rates for all sharded sweeps:
    `sweeps/eltwise/unary/frac/frac_sharded.py`: 114 fail, 462 pass
    (80.208333%).
    `sweeps/eltwise/unary/hardshrink/hardshrink_sharded.py`: 295 fail, 473
    pass (61.588542%).
    `sweeps/eltwise/unary/hardsigmoid/hardsigmoid_sharded.py`: 174 fail, 594
    pass 768 (77.34375%).
    `sweeps/eltwise/unary/hardswish/hardswish_sharded.py`: 174 fail, 594
    pass (77.34375%).
    `sweeps/eltwise/unary/hardtanh/hardtanh_sharded.py`: 174 fail, 594 pass
    (77.34375%).
    `sweeps/eltwise/unary/heaviside/heaviside_sharded.py`: 144 fail, 624
    pass (81.25%)
    `sweeps/eltwise/unary/isfinite/isfinite_sharded.py `: 147 fail, 832 pass
    (84.984678%).
    `sweeps/eltwise/unary/isinf/isinf_sharded.py`: 979 fail, 0 pass  (0%).  
    `sweeps/eltwise/unary/isnan/isnan_sharded.py`: 979 fail, 0 pass  (0%).  
    `sweeps/eltwise/unary//isneginf/isneginf_sharded.py`: 979 fail, 0 pass
    (0%).
    `sweeps/eltwise/unary/isposinf/isposinf_sharded.py`: 147 fail, 832 pass
    (84.984678%).
    `sweeps/eltwise/unary/lgamma/lgamma_sharded.py`: 384 fail, 0 pass (0%).
    `sweeps/eltwise/unary/logit/logit_sharded.py`: 68 fail, 316 pass
    (82.291667%).
    `sweeps/eltwise/unary/mish/mish_sharded.py`: 174 fail, 594 pass
    (77.34375%).
    `sweeps/eltwise/unary/multigammaln/multigammaln_sharded.py` 384 fail, 0
    pass (0%).
    `sweeps/eltwise/unary/softshrink/softshrink_sharded.py `: 208 fail, 944
    pass (81.944444%).
    `sweeps/eltwise/unary/tril/tril_sharded.py`: 66 fail, 318 pass
    (82.812%).
    `sweeps/eltwise/unary/triu/triu_sharded.py`:68 fail, 316 pass
    (82.291667%).
    `sweeps/eltwise/unary/trunc/trunc_sharded.py`: 174 fail, 594 pass
    (77.34375%).
    
    
    ### Checklist
    - [X] [Post commit CI
    passes](https://github.com/tenstorrent/tt-metal/actions/runs/12159246607)
    - [X] Sweep tests pass
    
    ---------
    
    Co-authored-by: ngrujic <ngrujic@tenstorrent.com>

--- tests/sweep_framework/sweeps/eltwise/unary/heaviside/heaviside.py
+++ tests/sweep_framework/sweeps/eltwise/unary/heaviside/heaviside.py
@@ -8,7 +8,7 @@ from functools import partial
 import torch
 import random
 import ttnn
-from tests.sweep_framework.sweep_utils.utils import gen_shapes
+from tests.sweep_framework.sweep_utils.utils import gen_shapes, sanitize_shape_rm
 from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_func_with_cast_tt
 
 from tests.ttnn.utils_for_testing import check_with_pcc, start_measuring_time, stop_measuring_time
@@ -26,11 +26,11 @@ random.seed(0)
 # Developers can create their own generator functions and pass them to the parameters as inputs.
 parameters = {
     "nightly": {
-        "input_shape": gen_shapes([1, 1, 32, 32], [6, 12, 256, 256], [1, 1, 32, 32], 16)
-        + gen_shapes([1, 32, 32], [12, 256, 256], [1, 32, 32], 16)
-        + gen_shapes([32, 32], [256, 256], [32, 32], 16),
+        "input_shape": gen_shapes([1, 1, 1, 1], [6, 12, 256, 256], [1, 1, 1, 1], 16)
+        + gen_shapes([1, 1, 1], [12, 256, 256], [1, 1, 1], 16)
+        + gen_shapes([1, 1], [256, 256], [1, 1], 16),
         "input_a_dtype": [ttnn.bfloat16, ttnn.bfloat8_b],
-        "input_a_layout": [ttnn.TILE_LAYOUT, ttnn.ROW_MAJOR_LAYOUT],
+        "input_layout": [ttnn.TILE_LAYOUT, ttnn.ROW_MAJOR_LAYOUT],
         "input_a_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
         "output_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
     },
@@ -41,7 +41,7 @@ parameters = {
 # If invalidated, the vector will still be stored but will be skipped.
 # Returns False, None if the vector is valid, and True, str with a reason for invalidation if it is invalid.
 def invalidate_vector(test_vector) -> Tuple[bool, Optional[str]]:
-    if test_vector["input_a_layout"] == ttnn.ROW_MAJOR_LAYOUT:
+    if test_vector["input_layout"] == ttnn.ROW_MAJOR_LAYOUT:
         return True, "Row Major layout is not supported"
     return False, None
 
@@ -53,7 +53,7 @@ def invalidate_vector(test_vector) -> Tuple[bool, Optional[str]]:
 def run(
     input_shape,
     input_a_dtype,
-    input_a_layout,
+    input_layout,
     input_a_memory_config,
     output_memory_config,
     *,
@@ -61,6 +61,9 @@ def run(
 ) -> list:
     data_seed = random.randint(0, 20000000)
     torch.manual_seed(data_seed)
+
+    if input_layout == ttnn.ROW_MAJOR_LAYOUT:
+        input_shape = sanitize_shape_rm(input_shape)
 
     torch_input_tensor_a = gen_func_with_cast_tt(
         partial(torch_random, low=-100, high=100, dtype=torch.float32), input_a_dtype
@@ -74,7 +77,7 @@ def run(
     input_tensor_a = ttnn.from_torch(
         torch_input_tensor_a,
         dtype=input_a_dtype,
-        layout=input_a_layout,
+        layout=input_layout,
         device=device,
         memory_config=input_a_memory_config,
     )
