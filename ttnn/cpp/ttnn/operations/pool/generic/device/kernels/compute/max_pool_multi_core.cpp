f4913ae71ddf08110c1cd8bd9a71e4514eeee07a
wransom-TT <wransom@tenstorrent.com>
2024-12-23 21:17:24 +0000

    #0: more cleanup

--- ttnn/cpp/ttnn/operations/pool/generic/device/kernels/compute/max_pool_multi_core.cpp
+++ ttnn/cpp/ttnn/operations/pool/generic/device/kernels/compute/max_pool_multi_core.cpp
@@ -104,7 +104,7 @@ void MAIN {
     constexpr uint32_t in_c = get_compile_time_arg_val(14);
     constexpr uint32_t in_nblocks_c = get_compile_time_arg_val(15);
 
-    constexpr uint32_t in_cb_id = tt::CBIndex::c_0;  // and tt::CB::c_in1 for split reader
+    constexpr uint32_t in_cb_id = tt::CBIndex::c_0;  // and CBIndex::c_1 for split reader
     constexpr uint32_t in_scalar_cb_id = tt::CBIndex::c_4;
     constexpr uint32_t in_tiled_cb_id = tt::CBIndex::c_24;
     constexpr uint32_t out_cb_id = tt::CBIndex::c_16;
