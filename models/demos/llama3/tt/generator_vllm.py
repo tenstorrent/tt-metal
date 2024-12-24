9f24a7131625735e68b105193636cb135675dfb2
Salar Hosseini <159165450+skhorasganiTT@users.noreply.github.com>
2024-12-24 17:54:15 -0500

    [Llama3-text vLLM integration] Modify Llama3 text model (new and old codebase) forward apis for vLLM compatibility (#16292)

--- models/demos/llama3/tt/generator_vllm.py
+++ models/demos/llama3/tt/generator_vllm.py
@@ -6,8 +6,12 @@ from typing import List, Union
 import torch
 import PIL
 from llama_models.llama3.api.chat_format import create_vision_mask
+from llama_models.llama3.api.tokenizer import Tokenizer
+import ttnn
 
 from models.demos.llama3.tt.generator import LlamaGenerator
+from models.demos.llama3.tt.llama_model import TtTransformer
+from models.demos.llama3.tt.model_config import LlamaOptimizations, TtModelArgs
 from models.demos.llama3.demo.simple_vision_demo import create_multimodal_model
 from models.utility_functions import nearest_32
 
@@ -106,3 +110,47 @@ class TtMllamaForConditionalGeneration(LlamaGenerator, SupportsMultiModal):
             kv_cache=kv_cache,
             cross_page_table=cross_page_table,
         )
+
+
+class TtLlamaForCausalLM(LlamaGenerator):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def initialize_vllm_model(cls, hf_config, mesh_device, max_batch_size, n_layers=None):
+        instruct_mode = "Instruct" in hf_config._name_or_path
+        max_seq_len = 131072  # TODO: modify this for different models/devices
+        optimizations = LlamaOptimizations.performance  # TODO: maybe change to accuracy
+        dtype = ttnn.bfloat8_b
+
+        # Load model args, weights
+        model_args = TtModelArgs(
+            mesh_device,
+            instruct=instruct_mode,
+            max_batch_size=max_batch_size,
+            optimizations=optimizations,
+            max_seq_len=max_seq_len,
+        )
+        if n_layers is not None:
+            model_args.n_layers = n_layers
+        state_dict = model_args.load_state_dict()
+
+        tt_model = TtTransformer(
+            args=model_args,
+            mesh_device=mesh_device,
+            dtype=dtype,
+            state_dict=state_dict,
+            weight_cache_path=model_args.weight_cache_path(dtype),
+            use_paged_kv_cache=True,
+        )
+        return cls(tt_model, model_args, mesh_device)
+
+    @property
+    def cache_path(self):
+        return self.model_args.model_cache_path
+
+    def prefill_forward(self, *args, **kwargs):
+        return super().prefill_forward_text(*args, **kwargs)
+
+    def decode_forward(self, *args, **kwargs):
+        return super().decode_forward_text(*args, **kwargs)
