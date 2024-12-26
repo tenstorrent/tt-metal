bc32dda0867ce38fedbf047ad21a23f1848decc1
Ligang Long <llong@tenstorrent.com>
2024-12-26 20:00:10 +0000

    #13609: Temporarily skip the failed tests to see if more tests fail

--- tests/ttnn/unit_tests/operations/test_group_norm.py
+++ tests/ttnn/unit_tests/operations/test_group_norm.py
@@ -323,6 +323,7 @@ def test_group_norm_with_block_sharded_v2_8x8_grid(device, N, C, H, W, num_group
     ],
 )
 def test_group_norm_with_block_sharded_v2_8x8_grid_tile_layout(device, N, C, H, W, num_groups):
+    pytest.skip("skiping for now to reveal more failures")
     torch.manual_seed(0)
     if device.core_grid.y == 7:
         pytest.skip()
