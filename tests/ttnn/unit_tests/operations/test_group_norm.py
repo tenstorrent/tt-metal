de04fc0b050341abdd8707ac7741fe6cf1ad17ef
Ligang Long <llong@tenstorrent.com>
2024-12-30 18:02:15 +0000

    #13609: skip more tests

--- tests/ttnn/unit_tests/operations/test_group_norm.py
+++ tests/ttnn/unit_tests/operations/test_group_norm.py
@@ -230,6 +230,7 @@ def test_group_norm_with_block_sharded_v2_8x4_grid(device, N, C, H, W, num_group
     ],
 )
 def test_group_norm_with_block_sharded_v2_8x8_grid(device, N, C, H, W, num_groups):
+    pytest.skip("skiping for now to reveal more failures")
     torch.manual_seed(0)
     if device.core_grid.y == 7:
         pytest.skip()
