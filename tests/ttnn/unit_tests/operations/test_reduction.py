ecdea70f1b03b647921b8f130e0e8c3b90f7cef2
Amruth Sandhupatla <asandhupatla@tenstorrent.com>
2025-01-02 13:45:05 -0500

    suppport all dim lengths for reduction (#16247)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/16233
    
    ### Problem description
    reduction doesn't support dims of all possible lengths
    
    ### What's changed
    Added code to support all permutations of dims
    
    ### Checklist
    - [x] Post commit CI passes :
    https://github.com/tenstorrent/tt-metal/actions/runs/12585299553
    - [x] Blackhole Post commit (if applicable) :
    https://github.com/tenstorrent/tt-metal/actions/runs/12585300944
    - [x] Model regression CI testing passes (if applicable) :
    https://github.com/tenstorrent/tt-metal/actions/runs/12585301911
    - [x] Device performance regression CI testing passes (if applicable) :
    https://github.com/tenstorrent/tt-metal/actions/runs/12585302835
    - [x] **(For models and ops writers)** Full [new
    models](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    tests passes
    - [x] New/Existing tests provide coverage for changes
    
    ---------
    
    Signed-off-by: Amruth Sandhupatla <asandhupatla@tenstorrent.com>

--- tests/ttnn/unit_tests/operations/test_reduction.py
+++ tests/ttnn/unit_tests/operations/test_reduction.py
@@ -50,28 +50,6 @@ def test_var(device, batch_size, h, w, dim):
     assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)
 
 
-@pytest.mark.parametrize("batch_size", [1, 16])
-@pytest.mark.parametrize("c", [1, 4, 8, 16])
-@pytest.mark.parametrize("h", [32, 64, 41, 37])
-@pytest.mark.parametrize("w", [32, 64, 31, 63])
-@pytest.mark.parametrize("dim", [None, [0, 1, 2, 3]])
-@pytest.mark.parametrize("keepdim", [True])
-def test_sum_4d_tensors(device, batch_size, c, h, w, dim, keepdim):
-    torch.manual_seed(0)
-
-    torch_input_tensor = torch.randn((batch_size, c, h, w), dtype=torch.bfloat16)
-    torch_output_tensor = torch.sum(torch_input_tensor, dim=dim, keepdim=keepdim)
-
-    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
-
-    output_tensor = ttnn.sum(input_tensor, dim=dim, keepdim=keepdim)
-    output_tensor = ttnn.to_layout(output_tensor, ttnn.TILE_LAYOUT)
-    output_tensor = ttnn.from_device(output_tensor)
-
-    output_tensor = ttnn.to_torch(output_tensor)
-    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)
-
-
 @pytest.mark.parametrize("batch_size", [1])
 @pytest.mark.parametrize("c", [11])
 @pytest.mark.parametrize("h", [67])
@@ -95,3 +73,129 @@ def test_prod(device, batch_size, c, h, w, dim, keepdim):
     assert len(output_tensor.shape) == len(torch_output_tensor.shape)
     assert output_tensor.shape == torch_output_tensor.shape
     # assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)
+
+
+@pytest.mark.parametrize("batch_size", [3])
+@pytest.mark.parametrize("c", [5])
+@pytest.mark.parametrize("h", [37])
+@pytest.mark.parametrize("w", [63])
+@pytest.mark.parametrize("dim", [None, [], 0, 2, [0, 1], [1, 3], [0, 1, 2], [1, 2, 3], [0, 1, 2, 3]])
+@pytest.mark.parametrize("keepdim", [True])
+def test_sum_4d_tensor_dims(device, batch_size, c, h, w, dim, keepdim):
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.randn((batch_size, c, h, w), dtype=torch.bfloat16)
+    torch_output_tensor = torch.sum(torch_input_tensor, dim=dim, keepdim=keepdim)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+
+    output_tensor = ttnn.sum(input_tensor, dim=dim, keepdim=keepdim)
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.TILE_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+
+    output_tensor = ttnn.to_torch(output_tensor)
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)
+
+
+@pytest.mark.parametrize("c", [3])
+@pytest.mark.parametrize("h", [31])
+@pytest.mark.parametrize("w", [32])
+@pytest.mark.parametrize("dim", [[0, 2], [0, 1, 2]])
+@pytest.mark.parametrize("keepdim", [True])
+def test_sum_3d_tensor_dims(device, c, h, w, dim, keepdim):
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.randn((c, h, w), dtype=torch.bfloat16)
+    torch_output_tensor = torch.sum(torch_input_tensor, dim=dim, keepdim=keepdim)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+
+    output_tensor = ttnn.sum(input_tensor, dim=dim, keepdim=keepdim)
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.TILE_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+
+    output_tensor = ttnn.to_torch(output_tensor)
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)
+
+
+@pytest.mark.parametrize("h", [41])
+@pytest.mark.parametrize("w", [31])
+@pytest.mark.parametrize("dim", [0, 1, [0, 1]])
+@pytest.mark.parametrize("keepdim", [True])
+def test_sum_2d_tensor_dims(device, h, w, dim, keepdim):
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.randn((h, w), dtype=torch.bfloat16)
+    torch_output_tensor = torch.sum(torch_input_tensor, dim=dim, keepdim=keepdim)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+
+    output_tensor = ttnn.sum(input_tensor, dim=dim, keepdim=keepdim)
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.TILE_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+
+    output_tensor = ttnn.to_torch(output_tensor)
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)
+
+
+@pytest.mark.parametrize("batch_size", [3])
+@pytest.mark.parametrize("c", [5])
+@pytest.mark.parametrize("h", [37])
+@pytest.mark.parametrize("w", [63])
+@pytest.mark.parametrize("dim", [None, [], 0, 2, [0, 1], [1, 3], [0, 1, 2], [1, 2, 3], [0, 1, 2, 3]])
+@pytest.mark.parametrize("keepdim", [True])
+def test_mean_4d_tensor_dims(device, batch_size, c, h, w, dim, keepdim):
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.randn((batch_size, c, h, w), dtype=torch.bfloat16)
+    torch_output_tensor = torch.mean(torch_input_tensor, dim=dim, keepdim=keepdim)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+
+    output_tensor = ttnn.mean(input_tensor, dim=dim, keepdim=keepdim)
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.TILE_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+
+    output_tensor = ttnn.to_torch(output_tensor)
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)
+
+
+@pytest.mark.parametrize("c", [3])
+@pytest.mark.parametrize("h", [31])
+@pytest.mark.parametrize("w", [32])
+@pytest.mark.parametrize("dim", [[0, 2], [0, 1, 2]])
+@pytest.mark.parametrize("keepdim", [True])
+def test_mean_3d_tensor_dims(device, c, h, w, dim, keepdim):
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.randn((c, h, w), dtype=torch.bfloat16)
+    torch_output_tensor = torch.mean(torch_input_tensor, dim=dim, keepdim=keepdim)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+
+    output_tensor = ttnn.mean(input_tensor, dim=dim, keepdim=keepdim)
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.TILE_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+
+    output_tensor = ttnn.to_torch(output_tensor)
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)
+
+
+@pytest.mark.parametrize("h", [41])
+@pytest.mark.parametrize("w", [31])
+@pytest.mark.parametrize("dim", [0, 1, [0, 1]])
+@pytest.mark.parametrize("keepdim", [True])
+def test_mean_2d_tensor_dims(device, h, w, dim, keepdim):
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.randn((h, w), dtype=torch.bfloat16)
+    torch_output_tensor = torch.mean(torch_input_tensor, dim=dim, keepdim=keepdim)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+
+    output_tensor = ttnn.mean(input_tensor, dim=dim, keepdim=keepdim)
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.TILE_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+
+    output_tensor = ttnn.to_torch(output_tensor)
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)
