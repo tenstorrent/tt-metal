d472a7f82ebd64c88f5e3000c76d0cafdc0253a1
Sean Nijjar <sean.nijjar@gmail.com>
2025-01-07 18:17:22 +0000

    fix multi-iter in reduce scatter and adopt runtime arg overrider infra

--- ttnn/cpp/ttnn/operations/ccl/common/host/command_backend_runtime_args_overrider.cpp
+++ ttnn/cpp/ttnn/operations/ccl/common/host/command_backend_runtime_args_overrider.cpp
@@ -33,16 +33,19 @@ void tensor_address_runtime_args_overrider::add_runtime_arg_index(size_t tensor_
 
 void tensor_address_runtime_args_overrider::override_runtime_args(
     size_t tensor_idx, uint32_t new_value, tt::tt_metal::RuntimeArgsData& runtime_args_to_modify) const {
+    if (tensor_idx >= tensor_address_runtime_arg_indices.size()) {
+        log_info(tt::LogOp, "Tensor index {} is out of bounds. Skipping override", tensor_idx);
+    }
     TT_FATAL(
         tensor_idx < tensor_address_runtime_arg_indices.size(), "Invalid tensor index when overriding runtime args");
 
     const auto& indices = tensor_address_runtime_arg_indices[tensor_idx];
     TT_FATAL(!indices.empty(), "No runtime arg indices associated with tensor");
 
-    log_trace(tt::LogOp, "Overriding {} runtime args for tensor {} to value {}", indices.size(), tensor_idx, new_value);
+    log_info(tt::LogOp, "Overriding {} runtime args for tensor {} to value {}", indices.size(), tensor_idx, new_value);
     for (size_t idx : indices) {
         TT_FATAL(idx < runtime_args_to_modify.size(), "Runtime arg index out of bounds when overriding args");
-        log_trace(tt::LogOp, "\t- {}", idx);
+        log_info(tt::LogOp, "\t- {}", idx);
         runtime_args_to_modify[idx] = new_value;
     }
 }
