cec68ecc1fcff002506b878f4f2f5077b5a11cc4
yugaoTT <yugao@tenstorrent.com>
2025-01-07 16:26:39 +0000

    #0: fix gs trisc overflow

--- tt_metal/hw/inc/remote_circular_buffer_api.h
+++ tt_metal/hw/inc/remote_circular_buffer_api.h
@@ -58,6 +58,7 @@ FORCE_INLINE void update_pages_acked(
 #endif
 }  // namespace detail
 
+#ifndef COMPILE_FOR_TRISC
 template <bool update_remote_over_noc = false>
 FORCE_INLINE void resize_remote_sender_cb_interface(uint32_t cb_id, uint32_t page_size, uint8_t noc) {
     ASSERT(page_size % REMOTE_CIRCULAR_BUFFER_ALIGNED_PAGE_SIZE == 0);
@@ -119,6 +120,12 @@ FORCE_INLINE void resize_remote_receiver_cb_interface(uint32_t cb_id, uint32_t p
     receiver_cb_interface.fifo_limit_page_aligned = fifo_limit_page_aligned;
     receiver_cb_interface.fifo_page_size = page_size;
 }
+#else
+template <bool update_remote_over_noc = false>
+FORCE_INLINE void resize_remote_sender_cb_interface(uint32_t cb_id, uint32_t page_size, uint8_t noc) {}
+template <bool update_remote_over_noc = false>
+FORCE_INLINE void resize_remote_receiver_cb_interface(uint32_t cb_id, uint32_t page_size, uint8_t noc) {}
+#endif
 
 #ifndef COMPILE_FOR_TRISC
 
