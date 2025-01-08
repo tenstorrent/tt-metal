8e43393405c568e5a1ecbd55300cd13e052fd833
Nour Ardo <nardo@tenstorrent.com>
2025-01-08 21:27:46 +0000

    run sc for cb overflow

--- ttnn/cpp/ttnn/operations/data_movement/tilize_with_val_padding/device/tilize_with_val_padding_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/tilize_with_val_padding/device/tilize_with_val_padding_program_factory.cpp
@@ -18,6 +18,36 @@ using namespace tt::tt_metal;
 
 namespace ttnn::operations::data_movement::detail {
 
+inline uint32_t get_estimated_size_of_cbs(const Tensor& input_tensor_a, const Tensor& output) {
+    // Circular Buffer sizes:
+    uint32_t element_size = input_tensor_a.element_size();
+    uint32_t Wt = output.get_legacy_shape()[-1] / tt::constants::TILE_WIDTH;
+    uint32_t Ht = output.get_legacy_shape()[-2] / tt::constants::TILE_HEIGHT;
+    uint32_t HtWt = Ht * Wt;
+    auto data_format = tt::tt_metal::datatype_to_dataformat_converter(input_tensor_a.get_dtype());
+    uint32_t tile_size = tt::tt_metal::detail::TileSize(data_format);
+
+    uint32_t cb_src0_size = 2 * Wt * tile_size;
+    uint32_t cb_output_size = 2 * Ht * tile_size;
+    uint32_t cb_im_size = Ht * Wt * tile_size;
+    uint32_t cb_im2_size = Ht * tile_size;
+    return cb_src0_size + cb_output_size + cb_im_size + cb_im2_size;
+}
+
+inline uint32_t get_max_l1_space(const Tensor& input_tensor_a) {
+    auto device = input_tensor_a.device();
+    auto lowest_address = device->lowest_occupied_compute_l1_address();
+    uint32_t max_l1_space = lowest_address.has_value() ? lowest_address.value() : device->l1_size_per_core();
+    max_l1_space = max_l1_space - device->get_base_allocator_addr(HalMemType::L1);
+    return max_l1_space;
+}
+
+inline bool rm_enough_available_space(const Tensor& input_tensor_a, const Tensor& output) {
+    uint32_t max_l1_space = get_max_l1_space(input_tensor_a);
+    uint32_t estimated_size_of_cbs = get_estimated_size_of_cbs(input_tensor_a, output);
+    return max_l1_space > estimated_size_of_cbs;
+}
+
 uint32_t get_packed_value(const Tensor tensor, const ttnn::PadValue pad_value) {
     return std::visit(
         [&tensor](auto&& pad_value) {
@@ -241,6 +271,11 @@ operation::ProgramWithCallbacks tilize_with_val_padding_multi_core_interleaved(
 
     IDevice* device = a.device();
     CoreCoord grid_size = device->compute_with_storage_grid_size();
+
+    bool enough_space = rm_enough_available_space(a, output);
+    if (enough_space == 0) {
+        return tilize_with_val_padding_single_core(a, output, pad_value);
+    }
 
     uint32_t num_blocks = output.volume() / output.get_legacy_shape()[-1] / TILE_HEIGHT;
     uint32_t num_tiles_per_row = output.get_legacy_shape()[-1] / TILE_WIDTH;
