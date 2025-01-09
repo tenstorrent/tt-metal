a5041f8bfa1cbb4ae58e3f769c10e8e33b71811a
Bill Teng <bteng@tenstorrent.com>
2025-01-09 22:16:58 +0000

    switch to invoke calls since operator() is only legal in C++23 and beyond

--- ttnn/cpp/ttnn/operations/generic/generic_op/generic_op.hpp
+++ ttnn/cpp/ttnn/operations/generic/generic_op/generic_op.hpp
@@ -10,36 +10,42 @@
 namespace ttnn::operations::generic {
 
 struct GenericOp {
-    static Tensor operator()(
+    static Tensor invoke(
         uint8_t queue_id,
-        const Tensor &input_tensor,
+        const Tensor& input_tensor,
         Tensor& output_tensor,
         const GenericOpDeviceOperation::operation_attributes_t& operation_attributes) {
-            return ttnn::device_operation::run<GenericOpDeviceOperation>(
-                queue_id,
-                GenericOpDeviceOperation::operation_attributes_t{operation_attributes},
-                GenericOpDeviceOperation::tensor_args_t{{input_tensor, output_tensor}});
-        }
-
-    static Tensor operator()(const Tensor &input_tensor, Tensor& output_tensor, const GenericOpDeviceOperation::operation_attributes_t& operation_attributes) {
-        return operator()(0, input_tensor, output_tensor, operation_attributes);
+        return ttnn::device_operation::invoke<GenericOpDeviceOperation>(
+            queue_id,
+            GenericOpDeviceOperation::operation_attributes_t{operation_attributes},
+            GenericOpDeviceOperation::tensor_args_t{{input_tensor, output_tensor}});
     }
 
-    static Tensor operator()(
-        uint8_t queue_id,
-        const std::vector<Tensor> &input_tensor,
+    static Tensor invoke(
+        const Tensor& input_tensor,
         Tensor& output_tensor,
         const GenericOpDeviceOperation::operation_attributes_t& operation_attributes) {
-            std::vector<Tensor> io_tensors = input_tensor;
-            io_tensors.push_back(output_tensor);
-            return ttnn::device_operation::run<GenericOpDeviceOperation>(
-                queue_id,
-                GenericOpDeviceOperation::operation_attributes_t{operation_attributes},
-                GenericOpDeviceOperation::tensor_args_t{io_tensors});
-        }
+        return invoke(0, input_tensor, output_tensor, operation_attributes);
+    }
 
-    static Tensor operator()(const std::vector<Tensor> &input_tensors, Tensor& output_tensor, const GenericOpDeviceOperation::operation_attributes_t& operation_attributes) {
-        return operator()(0, input_tensors, output_tensor, operation_attributes);
+    static Tensor invoke(
+        uint8_t queue_id,
+        const std::vector<Tensor>& input_tensor,
+        Tensor& output_tensor,
+        const GenericOpDeviceOperation::operation_attributes_t& operation_attributes) {
+        std::vector<Tensor> io_tensors = input_tensor;
+        io_tensors.push_back(output_tensor);
+        return ttnn::device_operation::run<GenericOpDeviceOperation>(
+            queue_id,
+            GenericOpDeviceOperation::operation_attributes_t{operation_attributes},
+            GenericOpDeviceOperation::tensor_args_t{io_tensors});
+    }
+
+    static Tensor invoke(
+        const std::vector<Tensor>& input_tensors,
+        Tensor& output_tensor,
+        const GenericOpDeviceOperation::operation_attributes_t& operation_attributes) {
+        return invoke(0, input_tensors, output_tensor, operation_attributes);
     }
 };  // struct UnaryGenericOperation
 
