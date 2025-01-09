0a4605d0ab1b94f24c65000c9a46f60739f711d5
Bill Teng <bteng@tenstorrent.com>
2025-01-09 02:52:23 +0000

    #20073: rebase, fix compile errors, small changes -revert changes to device_operation.hpp -fix header path -fix compile errors and header path -switch to invoke calls since operator() is only legal in C++23 and beyond -fix typo in error message -remove headers that don't exist anymore -register ttnn::prim::generic -pass underlying types needed by op as function arguments into invoke -fix compile errors -Update tests/tt_eager/ops/test_generic_op.cpp -Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com> -switch back to passing operation_attributes_t as function argument into invoke -fix compile errors -fix error when resolving merge conflict -update header paths -move impl to cpp -fix include paths -get_legacy_shape -> get_logical_shape -move operation_attributes_t out of GenericOpDeviceOperation -to_layout, to_device -revert file to main -move generic op types into separate header -program_attributes_t -multi-input

--- ttnn/cpp/ttnn/operations/generic/generic_op/device/generic_op_device_operation.hpp
+++ ttnn/cpp/ttnn/operations/generic/generic_op/device/generic_op_device_operation.hpp
@@ -8,72 +8,26 @@
 #include <optional>
 #include <unordered_map>
 #include <variant>
+#include "ttnn/decorators.hpp"
 
-// #include "core_coord.h"
-#include "common/core_coord.h"
-#include "impl/kernels/kernel_types.hpp"
 #include "ttnn/tensor/tensor.hpp"
 #include "ttnn/core.hpp"
 #include "ttnn/device_operation.hpp"
 #include "ttnn/types.hpp"
+#include "ttnn/operations/generic/generic_op/generic_op_types.hpp"
 
 namespace ttnn::operations::generic {
 
-struct circular_buffer_attributes_t {
-    CoreRangeSet core_spec = {{}};
-    uint32_t total_size;
-    uint32_t page_size;
-    // uint8_t buffer_index;
-    tt::DataFormat data_format;
-
-    // this needs better solution as we now have input tensors (std::vector) and output tensor so index is not great
-    std::optional<int> set_globally_allocated_address = std::nullopt; // an index to io_tensors that will set globally allocated address on CB
-};
-
-struct data_movement_attributes_t {
-    CoreRangeSet core_spec= {{}};
-    std::string kernel_path;
-    tt::tt_metal::DataMovementConfig config;
-    std::unordered_map<CoreCoord, std::vector<uint32_t>> runtime_args_per_core = {};
-
-    // std::variant<CoreCoord, CoreRange, CoreRangeSet> core_spec;
-    // std::shared_ptr<RuntimeArgs> runtime_args;
-    // std::vector<std::shared_ptr<RuntimeArgs>> runtime_args;
-
-};
-
-struct compute_attributes_t {
-    CoreRangeSet core_spec = {{}};
-    std::string  kernel_path;
-    tt::tt_metal::ComputeConfig config;
-    // std::vector<uint32_t> runtime_args = {};
-    std::unordered_map<CoreCoord, std::vector<uint32_t>> runtime_args_per_core = {};
-
-};
-
 struct GenericOpDeviceOperation {
-
-    struct operation_attributes_t {
-
-        std::unordered_map<uint8_t, circular_buffer_attributes_t> circular_buffer_attributes;
-
-        std::vector<data_movement_attributes_t> data_movement_attributes;
-
-        // Ethernet not supported at the moment.
-        // std::optional<tt::metal::EthernetConfig> ethernetConfig;
-
-        std::vector<compute_attributes_t> compute_attributes;
-    };
-
-
-    // Define the return types for the shape(s) of the operation
-    using shape_return_value_t = ttnn::Shape;
+    using operation_attributes_t = program_attributes_t;
 
     // Define the return types for the tensor(s) of the operation
     // Can be a single Tensor, std::optional<Tensor, ...>, std::vector<Tensor>, std::tuple<Tensor, ...> etc.
     using tensor_return_value_t = Tensor;
 
+    using spec_return_value_t = TensorSpec;
     struct tensor_args_t {
+        const Tensor& input_tensor;
         // std::vector<std::reference_wrapper<Tensor>> io_tensors;
         std::vector<Tensor> io_tensors;
         // std::vector<Tensor> input_tensors; // this might not be the best thing?
@@ -110,8 +64,14 @@ struct GenericOpDeviceOperation {
 
     // Mandatory methods
 
+    std::vector<ttnn::TensorSpec> compute_output_specs(const operation_attributes_t&, const tensor_args_t&) const;
+
+    std::vector<Tensor> create_output_tensors(const operation_attributes_t&, const tensor_args_t&) const;
+
     // Select the program factory based on the operation attributes and tensor args
     static program_factory_t select_program_factory(const operation_attributes_t&, const tensor_args_t&);
+
+    static void validate_inputs(const operation_attributes_t& attributes, const tensor_args_t& tensor_args);
 
     // Validate the operation when it creates a program. Usually will have more checks
     static void validate_on_program_cache_miss(const operation_attributes_t&, const tensor_args_t&);
@@ -119,12 +79,10 @@ struct GenericOpDeviceOperation {
     // Validate the operation when it reuses a program. Usually will have less checks
     static void validate_on_program_cache_hit(const operation_attributes_t&, const tensor_args_t&);
 
-    // Compute the output shapes based on the operation attributes and tensor args
-    static shape_return_value_t compute_output_shapes(const operation_attributes_t&, const tensor_args_t&);
+    static std::tuple<operation_attributes_t, tensor_args_t> invoke(
+        const Tensor&, const operation_attributes_t&, const std::vector<Tensor>& = {});
 
-    // Create the output tensors based on the operation attributes and tensor args
-    static tensor_return_value_t create_output_tensors(const operation_attributes_t&, const tensor_args_t&);
-
+    static tt::stl::hash::hash_t compute_program_hash(const operation_attributes_t&, const tensor_args_t&);
 };  // struct GenericOpDeviceOperation
 
-}   // namespace ttnn::operations::generic
+}  // namespace ttnn::operations::generic
