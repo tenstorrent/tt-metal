32f589d79b5387b95fb1ac0248f0b13b248f122b
Andrija Malbasa <amalbasa@tenstorrent.com>
2025-01-10 16:50:39 +0100

    #11512: Refactor bitwise sweeps, add bitwise sharded sweeps, modify t… (#15704)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/11512)
    
    
    ### Problem description
    1) Missing sharded versions of ops sweeps for bitwise ops:
    
    - bitwise_and
    - bitwise_or
    - bitwise_xor
    - bitwise_and
    - bitwise_left_shift
    - bitwise_right_shift
    2) Non-sharded versions if ops sweeps for bitwise ops need some minor
    factoring
    
    
    ### What's changed
    1. Refactored bitwise interleaved sweeps so that they include vector
    invalidation for row major layout tensors and modified their shape
    generation function to also use shapes with last two dims not divisible
    by 32. Removed `"xfail" `suite for `bitwise_left_shift.py` as well as
    `use_safe_nums` parameter inside the `"nightly"` suite since the issue
    is fixed
    (https://github.com/tenstorrent/tt-metal/issues/13415#event-15483411894).
    3. Added sweeps for sharded version of bitwise ops.
    4. Modified ttnn-run-sweeps.yaml.
    
    
    ### Pass rates for all bitwise unary sharded sweeps:
    
    `sweeps/eltwise/unary/bitwise/bitwise_and/bitwise_and_unary_sharded.py`:
    87 fail, 297 pass (77.34375%).
    
    `sweeps/eltwise/unary/bitwise/bitwise_left_shift/bitwise_left_shift_unary_sharded.py`:
    450 fail, 2430 pass (84.375%).
    `sweeps/eltwise/unary/bitwise/bitwise_not/bitwise_not_sharded.py`: 87
    fail, 297 pass (77.34375%).
    `sweeps/eltwise/unary/bitwise/bitwise_or/bitwise_or_unary_sharded.py`:
    65 fail, 319 pass (83.073%).
    
    `sweeps/eltwise/unary/bitwise/bitwise_right_shift/bitwise_right_shift_unary_sharded.py`:
    450 fail, 2430 pass (84.375%).
    
    `sweeps/eltwise/unary/bitwise/bitwise_xor/bitwise_xor_unary_sharded.py.py`:
    65 fail, 319 pass (83.073%).
    
    
    ### Pass rates for all bitwise binary sharded sweeps:
    `sweeps/eltwise/unary/bitwise/bitwise_and/bitwise_and_sharded.py`: 249
    fail, 135 pass (35.15625%).
    
    `sweeps/eltwise/unary/bitwise/bitwise_left_shift/bitwise_left_shift_sharded.py`:
    232 fail, 152 pass (39.5833%).
    `sweeps/eltwise/unary/bitwise/bitwise_or/bitwise_or_sharded.py`: 249
    fail, 135 pass (35.15625%).
    
    `sweeps/eltwise/unary/bitwise/bitwise_right_shift/bitwise_right_shift_sharded.py`:
    232 fail, 152 pass (39.5833%).
    `sweeps/eltwise/unary/bitwise/bitwise_xor/bitwise_xor_sharded.py.py`:
    249 fail, 135 pass (35.15625%).
    
    
    ### Pass rates for all bitwise binary non-sharded sweeps:
    `sweeps/eltwise/unary/bitwise/bitwise_and/bitwise_and_sharded.py`: 0
    fail, 384 pass (100%).
    
    `sweeps/eltwise/unary/bitwise/bitwise_left_shift/bitwise_left_shift_sharded.py`:
    0 fail, 384 pass (100%).
    `sweeps/eltwise/unary/bitwise/bitwise_or/bitwise_or_sharded.py`: 0 fail,
    384 pass (100%).
    
    `sweeps/eltwise/unary/bitwise/bitwise_right_shift/bitwise_right_shift_sharded.py`:
    0 fail, 384 pass (100%).
    `sweeps/eltwise/unary/bitwise/bitwise_xor/bitwise_xor_sharded.py.py`: 0
    fail, 384 pass (100%).
    
    
    ### Checklist
    - [X] [Post commit CI passes]
    (https://github.com/tenstorrent/tt-metal/actions/runs/12671675872)
    - [X] Sweep tests pass

--- tests/sweep_framework/sweeps/eltwise/unary/bitwise/bitwise_right_shift/bitwise_right_shift_unary.py
+++ tests/sweep_framework/sweeps/eltwise/unary/bitwise/bitwise_right_shift/bitwise_right_shift_unary.py
@@ -8,7 +8,7 @@ from functools import partial
 import torch
 import random
 import ttnn
-from tests.sweep_framework.sweep_utils.utils import gen_shapes
+from tests.sweep_framework.sweep_utils.utils import gen_shapes, sanitize_shape_rm
 from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_func_with_cast_tt
 
 from tests.ttnn.utils_for_testing import check_with_pcc, start_measuring_time, stop_measuring_time
@@ -25,11 +25,12 @@ random.seed(0)
 # Developers can create their own generator functions and pass them to the parameters as inputs.
 parameters = {
     "nightly": {
-        "input_shape": gen_shapes([1, 1, 32, 32], [6, 12, 512, 512], [1, 1, 32, 32], 16)
-        + gen_shapes([1, 32, 32], [12, 1024, 1024], [1, 32, 32], 16)
-        + gen_shapes([32, 32], [1024, 1024], [32, 32], 16),
+        "input_shape": gen_shapes([1, 1, 1, 1], [6, 12, 128, 128], [1, 1, 1, 1], 4)
+        + gen_shapes([1, 1, 1], [12, 256, 256], [1, 1, 1], 4)
+        + gen_shapes([1, 1], [256, 256], [1, 1], 4),
+        "shift_bits": list(range(1, 31)),
         "input_a_dtype": [ttnn.int32],
-        "input_a_layout": [ttnn.TILE_LAYOUT],
+        "input_layout": [ttnn.TILE_LAYOUT, ttnn.ROW_MAJOR_LAYOUT],
         "input_a_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
         "output_memory_config": [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG],
     },
@@ -44,14 +45,24 @@ def mesh_device_fixture():
     del device
 
 
+# Invalidate vector is called during the generation phase where each vector will be passed in.
+# If invalidated, the vector will still be stored but will be skipped.
+# Returns False, None if the vector is valid, and True, str with a reason for invalidation if it is invalid.
+def invalidate_vector(test_vector) -> Tuple[bool, Optional[str]]:
+    if test_vector["input_layout"] == ttnn.ROW_MAJOR_LAYOUT:
+        return True, "Unary operation requires tensor to be in Tile layout when working with non-sharded input tensor"
+    return False, None
+
+
 # This is the run instructions for the test, defined by the developer.
 # The run function must take the above-defined parameters as inputs.
 # The runner will call this run function with each test vector, and the returned results from this function will be stored.
 # If you defined a device_mesh_fixture above, the object you yielded will be passed into this function as 'device'. Otherwise, it will be the default ttnn device opened by the infra.
 def run(
     input_shape,
+    shift_bits,
     input_a_dtype,
-    input_a_layout,
+    input_layout,
     input_a_memory_config,
     output_memory_config,
     *,
@@ -60,25 +71,27 @@ def run(
     data_seed = random.randint(0, 20000000)
     torch.manual_seed(data_seed)
 
+    if input_layout == ttnn.ROW_MAJOR_LAYOUT:
+        input_shape = sanitize_shape_rm(input_shape)
+
     torch_input_tensor_a = gen_func_with_cast_tt(
-        partial(torch_random, low=-2147483647, high=2147483648, dtype=torch.int64), input_a_dtype
+        partial(torch_random, low=-2147483647, high=2147483648, dtype=torch.int32), input_a_dtype
     )(input_shape)
-
-    torch_input_tensor_a = torch.full(size=input_shape, fill_value=-2147483647).to(torch.int32)
-
-    torch_output_tensor = torch.bitwise_not(torch_input_tensor_a)
+    golden_function = ttnn.get_golden_function(ttnn.bitwise_right_shift)
+    torch_output_tensor = torch.bitwise_right_shift(torch_input_tensor_a, shift_bits).to(torch.int32)
 
     input_tensor_a = ttnn.from_torch(
         torch_input_tensor_a,
         dtype=input_a_dtype,
-        layout=input_a_layout,
+        layout=input_layout,
         device=device,
         memory_config=input_a_memory_config,
     )
 
     start_time = start_measuring_time()
-    result = ttnn.bitwise_not(input_tensor_a, memory_config=output_memory_config)
-    output_tensor = ttnn.to_torch(result)
+    result = ttnn.bitwise_right_shift(input_tensor_a, shift_bits, memory_config=output_memory_config)
     e2e_perf = stop_measuring_time(start_time)
+
+    output_tensor = ttnn.to_torch(result).to(torch.int32)
 
     return [check_with_pcc(torch_output_tensor, output_tensor, 0.999), e2e_perf]
