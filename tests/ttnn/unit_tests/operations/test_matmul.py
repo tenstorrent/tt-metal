652490d827a15486387a12f69dc7206447a10697
Vasisht Suresh <vsuresh@tenstorrent.com>
2025-01-10 12:39:31 -0800

    #16366: Changed default kernal_config_val for 32bit matmul (#16567)
    ### Ticket
    Link to Github Issue
    https://github.com/tenstorrent/tt-metal/issues/16366
    
    ### Problem description
    FP32 PCC not accurate enough for a matmul with default kernel config
    
    ### What's changed
    Enabled FP32 and disabled L1ACC for FP32
    
    ### Checklist
    - [x] Post commit CI passes
    - [x] Blackhole Post commit (if applicable)
    - [x] Model regression CI testing passes (if applicable)
    - [x] Device performance regression CI testing passes (if applicable)
    - [x] **(For models and ops writers)** Full [new
    models](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    tests passes
    - [ ] New/Existing tests provide coverage for changes

--- tests/ttnn/unit_tests/operations/test_matmul.py
+++ tests/ttnn/unit_tests/operations/test_matmul.py
@@ -2111,3 +2111,18 @@ def test_optional_output_argument(device, n_size, c, m, k, n):
     assert_with_pcc(torch_output_tensor, output, 0.999)
     assert_with_pcc(torch_output_tensor, optional_output_tensor, 0.999)
     assert_with_pcc(output, optional_output_tensor, 0.999)
+
+
+def test_small_matmul_pcc(device):
+    torch.manual_seed(0)
+    pcc = 0.99
+    torch_input_tensor_a = torch.rand([1, 2048])
+    torch_input_tensor_b = torch.rand([2048, 1000])
+    torch_output_tensor = torch.matmul(torch_input_tensor_a, torch_input_tensor_b)
+
+    input_tensor_a = ttnn.from_torch(torch_input_tensor_a, layout=ttnn.TILE_LAYOUT, device=device)
+    input_tensor_b = ttnn.from_torch(torch_input_tensor_b, layout=ttnn.TILE_LAYOUT, device=device)
+    output1 = ttnn.matmul(input_tensor_a, input_tensor_b)
+    output_tensor = ttnn.to_torch(output1)
+
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=pcc)
