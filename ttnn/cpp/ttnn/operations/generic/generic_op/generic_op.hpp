b5a8864e1af1a76fb0534235ebc52cbe7991d3f3
Bill Teng <bteng@tenstorrent.com>
2025-01-10 23:48:04 +0000

    register ttnn::prim::generic

--- ttnn/cpp/ttnn/operations/generic/generic_op/generic_op.hpp
+++ ttnn/cpp/ttnn/operations/generic/generic_op/generic_op.hpp
@@ -13,44 +13,31 @@ struct GenericOp {
     static Tensor invoke(
         uint8_t queue_id,
         const Tensor& input_tensor,
-        Tensor& output_tensor,
         const GenericOpDeviceOperation::operation_attributes_t& operation_attributes) {
-        return ttnn::device_operation::invoke<GenericOpDeviceOperation>(
-            queue_id,
-            GenericOpDeviceOperation::operation_attributes_t{operation_attributes},
-            GenericOpDeviceOperation::tensor_args_t{{input_tensor, output_tensor}});
+        return ttnn::prim::generic(queue_id, input_tensor, operation_attributes);
     }
 
     static Tensor invoke(
-        const Tensor& input_tensor,
-        Tensor& output_tensor,
-        const GenericOpDeviceOperation::operation_attributes_t& operation_attributes) {
-        return invoke(0, input_tensor, output_tensor, operation_attributes);
+        const Tensor& input_tensor, const GenericOpDeviceOperation::operation_attributes_t& operation_attributes) {
+        return invoke(0, input_tensor, operation_attributes);
     }
 
-    static Tensor invoke(
-        uint8_t queue_id,
-        const std::vector<Tensor>& input_tensor,
-        Tensor& output_tensor,
-        const GenericOpDeviceOperation::operation_attributes_t& operation_attributes) {
-        std::vector<Tensor> io_tensors = input_tensor;
-        io_tensors.push_back(output_tensor);
-        return ttnn::device_operation::run<GenericOpDeviceOperation>(
-            queue_id,
-            GenericOpDeviceOperation::operation_attributes_t{operation_attributes},
-            GenericOpDeviceOperation::tensor_args_t{io_tensors});
-    }
+    // static Tensor invoke(
+    //     uint8_t queue_id,
+    //     const std::vector<Tensor>& input_tensor,
+    //     const GenericOpDeviceOperation::operation_attributes_t& operation_attributes) {
+    //     return ttnn::prim::generic(0, input_tensor, operation_attributes);
+    // }
 
-    static Tensor invoke(
-        const std::vector<Tensor>& input_tensors,
-        Tensor& output_tensor,
-        const GenericOpDeviceOperation::operation_attributes_t& operation_attributes) {
-        return invoke(0, input_tensors, output_tensor, operation_attributes);
-    }
+    // static Tensor invoke(
+    //     const std::vector<Tensor>& input_tensors,
+    //     const GenericOpDeviceOperation::operation_attributes_t& operation_attributes) {
+    //     return invoke(0, input_tensors, operation_attributes);
+    // }
 };  // struct UnaryGenericOperation
 
 }   // namespace ttnn::operations::generic
 
 namespace ttnn {
-constexpr auto generic_op = ttnn::register_operation<"ttnn::generic_op", operations::generic::GenericOp>();
+constexpr auto generic_op = ttnn::register_operation<"ttnn::generic_op", ttnn::operations::generic::GenericOp>();
 }  // namespace ttnn
