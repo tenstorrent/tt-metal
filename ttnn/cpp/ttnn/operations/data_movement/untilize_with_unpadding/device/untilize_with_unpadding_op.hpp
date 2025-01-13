2e596e0515a1fa7298fbe4e5514738b595726baf
Nour Ardo <nardo@tenstorrent.com>
2025-01-13 20:49:12 +0000

    replace legacy shape for tilize/untilize with padding

--- ttnn/cpp/ttnn/operations/data_movement/untilize_with_unpadding/device/untilize_with_unpadding_op.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/untilize_with_unpadding/device/untilize_with_unpadding_op.hpp
@@ -12,7 +12,8 @@
 namespace ttnn::operations::data_movement {
 
 struct UntilizeWithUnpadding {
-    const tt::tt_metal::LegacyShape output_tensor_end;
+    // const tt::tt_metal::LegacyShape output_tensor_end;
+    const ttnn::SimpleShape output_tensor_end;
     const MemoryConfig output_mem_config;
     const bool use_multicore;
     const bool use_pack_untilize;
