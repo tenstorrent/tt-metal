c5200ec6d34b0606cee038be3e25be3f3c390eb4
amalbasaTT <amalbasa@tenstorrent.com>
2025-01-14 13:07:25 +0000

    #11512: Reduce test cases for bitwise_xor_unary_output

--- tests/sweep_framework/sweeps/eltwise/unary/bitwise/bitwise_xor/bitwise_xor_unary_output.py
+++ tests/sweep_framework/sweeps/eltwise/unary/bitwise/bitwise_xor/bitwise_xor_unary_output.py
@@ -25,9 +25,9 @@ random.seed(0)
 # Developers can create their own generator functions and pass them to the parameters as inputs.
 parameters = {
     "nightly": {
-        "input_shape": gen_shapes([1, 1, 1, 1], [6, 12, 128, 128], [1, 1, 1, 1], 32)
-        + gen_shapes([1, 1, 1], [12, 256, 256], [1, 132, 1], 32)
-        + gen_shapes([1, 1], [256, 256], [1, 1], 32),
+        "input_shape": gen_shapes([1, 1, 1, 1], [6, 12, 128, 128], [1, 1, 1, 1], 16)
+        + gen_shapes([1, 1, 1], [12, 256, 256], [1, 132, 1], 16)
+        + gen_shapes([1, 1], [256, 256], [1, 1], 16),
         "input_a_dtype": [ttnn.int32],
         "output_dtype": [ttnn.int32],
         "input_layout": [ttnn.TILE_LAYOUT, ttnn.ROW_MAJOR_LAYOUT],
