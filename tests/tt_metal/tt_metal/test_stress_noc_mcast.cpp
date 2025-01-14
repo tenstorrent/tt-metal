0ce388dd606c499985bd87e2cba07183d201070e
Almeet Bhullar <abhullar@tenstorrent.com>
2025-01-14 13:57:20 -0500

    Make stress noc mcast test respect physical coordinates + allow option to skip mcaster

--- tests/tt_metal/tt_metal/test_stress_noc_mcast.cpp
+++ tests/tt_metal/tt_metal/test_stress_noc_mcast.cpp
@@ -46,6 +46,7 @@ uint32_t mcast_size_g = 16;
 uint32_t ucast_size_g = 8192;
 uint32_t mcast_from_n_eth_g;
 bool mcast_from_eth_g;
+bool ucast_only = false;
 bool rnd_delay_g = false;
 bool rnd_coord_g = true;
 tt_metal::NOC noc_g = tt_metal::NOC::NOC_0;
@@ -66,6 +67,7 @@ void init(int argc, char** argv) {
         log_info(LogTest, "     -e: mcast from nth idle eth core (ignores -mx,-my)");
         log_info(LogTest, "     -m: mcast packet size");
         log_info(LogTest, "     -u: ucast packet size");
+        log_info(LogTest, "     -ucast-only: skip multicasting");
         log_info(LogTest, "-rdelay: insert random delay between noc transactions");
         log_info(LogTest, "     -n: noc to use (default 0)");
         log_info(LogTest, "     -s: seed random number generator");
@@ -84,14 +86,19 @@ void init(int argc, char** argv) {
     mcast_size_g = test_args::get_command_option_uint32(input_args, "-m", 16);
     ucast_size_g = test_args::get_command_option_uint32(input_args, "-u", 8192);
     mcast_from_eth_g = (mcast_from_n_eth_g != 0xffff);
+    ucast_only = test_args::has_command_option(input_args, "-ucast-only");
     rnd_delay_g = test_args::has_command_option(input_args, "-rdelay");
     uint32_t noc = test_args::get_command_option_uint32(input_args, "-n", 0);
     noc_g = (noc == 0) ? tt_metal::NOC::NOC_0 : tt_metal::NOC::NOC_1;
     uint32_t seed = test_args::get_command_option_uint32(input_args, "-s", 0);
     srand(seed);
 
-    if (!mcast_from_eth_g && mcast_x_g >= tlx_g && mcast_x_g <= tlx_g + width_g - 1 && mcast_y_g >= tly_g &&
-        mcast_y_g <= tly_g + height_g - 1) {
+    if (mcast_from_eth_g && ucast_only) {
+        log_fatal("Cannot request both mcast from eth and ucast only");
+    }
+
+    if (!ucast_only && !mcast_from_eth_g && mcast_x_g >= tlx_g && mcast_x_g <= tlx_g + width_g - 1 &&
+        mcast_y_g >= tly_g && mcast_y_g <= tly_g + height_g - 1) {
         log_fatal("Mcast core can't be within mcast grid");
         exit(-1);
     }
@@ -126,18 +133,29 @@ int main(int argc, char** argv) {
         mcast_logical = eth_logical;
     }
 
-    CoreCoord virtual_offset = device->worker_core_from_logical_core({0, 0});
-    TT_ASSERT(virtual_offset.x == virtual_offset.y);
+    bool virtualization_enabled = tt::tt_metal::hal.is_coordinate_virtualization_enabled();
+    CoreCoord virtual_offset;
+    CoreCoord mcast_end = workers_logical.end_coord;
+    if (virtualization_enabled) {
+        virtual_offset = device->worker_core_from_logical_core({0, 0});
+    } else {
+        virtual_offset = CoreCoord(0, 0);  // In this case pass physical coordinates as runtime args
+        mcast_end = device->worker_core_from_logical_core(mcast_end);
+    }
+    uint32_t num_dests = workers_logical.size();
+
     std::vector<uint32_t> compile_args = {
         false,
         tl_core.x,
         tl_core.y,
-        width_g,
-        height_g,
+        mcast_end.x,
+        mcast_end.y,
+        num_dests,
         time_secs_g,
         ucast_size_g,
         mcast_size_g,
         virtual_offset.x,
+        virtual_offset.y,
         N_RANDS,
         rnd_delay_g,
         tt::tt_metal::hal.get_dev_addr(HalProgrammableCoreType::TENSIX, HalL1MemAddrType::UNRESERVED),
@@ -166,6 +184,11 @@ int main(int argc, char** argv) {
             for (int j = 0; j < sizeof(uint32_t); j++) {
                 uint32_t x = rand() % grid_size.x;
                 uint32_t y = rand() % grid_size.y;
+                if (!virtualization_enabled) {
+                    CoreCoord physical_coord = device->worker_core_from_logical_core(CoreCoord(x, y));
+                    x = physical_coord.x;
+                    y = physical_coord.y;
+                }
                 rnd = (rnd << 8) | (y << 4) | x;
             }
             runtime_args.push_back(rnd);
@@ -173,55 +196,60 @@ int main(int argc, char** argv) {
         tt::tt_metal::SetRuntimeArgs(program, ucast_kernel, coord, runtime_args);
     }
 
-    compile_args[0] = true;
-    KernelHandle mcast_kernel;
-    if (mcast_from_eth_g) {
-        mcast_kernel = tt_metal::CreateKernel(
-            program,
-            "tests/tt_metal/tt_metal/test_kernels/stress_noc_mcast.cpp",
-            mcast_logical,
-            tt_metal::EthernetConfig{
-                .eth_mode = Eth::IDLE,
-                .noc = noc_g,
-                .compile_args = compile_args,
-            });
-    } else {
-        mcast_kernel = tt_metal::CreateKernel(
-            program,
-            "tests/tt_metal/tt_metal/test_kernels/stress_noc_mcast.cpp",
-            mcast_logical,
-            tt_metal::DataMovementConfig{
-                .processor = tt_metal::DataMovementProcessor::RISCV_0,
-                .noc = noc_g,
-                .compile_args = compile_args,
-            });
-    }
-    std::vector<uint32_t> runtime_args;
-    for (int i = 0; i < 128; i++) {
-        runtime_args.push_back(rand());
-    }
-    tt::tt_metal::SetRuntimeArgs(program, mcast_kernel, mcast_logical, runtime_args);
+    if (!ucast_only) {
+        compile_args[0] = true;
+        KernelHandle mcast_kernel;
+        if (mcast_from_eth_g) {
+            mcast_kernel = tt_metal::CreateKernel(
+                program,
+                "tests/tt_metal/tt_metal/test_kernels/stress_noc_mcast.cpp",
+                mcast_logical,
+                tt_metal::EthernetConfig{
+                    .eth_mode = Eth::IDLE,
+                    .noc = noc_g,
+                    .compile_args = compile_args,
+                });
+        } else {
+            mcast_kernel = tt_metal::CreateKernel(
+                program,
+                "tests/tt_metal/tt_metal/test_kernels/stress_noc_mcast.cpp",
+                mcast_logical,
+                tt_metal::DataMovementConfig{
+                    .processor = tt_metal::DataMovementProcessor::RISCV_0,
+                    .noc = noc_g,
+                    .compile_args = compile_args,
+                });
+        }
 
-    CoreCoord mcast_virtual;
-    CoreCoord mcast_physical;
-    if (mcast_from_eth_g) {
-        mcast_virtual = device->ethernet_core_from_logical_core(mcast_logical);
-        mcast_physical =
-            tt::Cluster::instance().get_soc_desc(device_num_g).get_physical_ethernet_core_from_logical(mcast_logical);
-    } else {
-        mcast_virtual = device->worker_core_from_logical_core(mcast_logical);
-        mcast_physical =
-            tt::Cluster::instance().get_soc_desc(device_num_g).get_physical_tensix_core_from_logical(mcast_logical);
+        std::vector<uint32_t> runtime_args;
+        for (int i = 0; i < 128; i++) {
+            runtime_args.push_back(rand());
+        }
+        tt::tt_metal::SetRuntimeArgs(program, mcast_kernel, mcast_logical, runtime_args);
+
+        CoreCoord mcast_virtual;
+        CoreCoord mcast_physical;
+        if (mcast_from_eth_g) {
+            mcast_virtual = device->ethernet_core_from_logical_core(mcast_logical);
+            mcast_physical = tt::Cluster::instance()
+                                 .get_soc_desc(device_num_g)
+                                 .get_physical_ethernet_core_from_logical(mcast_logical);
+        } else {
+            mcast_virtual = device->worker_core_from_logical_core(mcast_logical);
+            mcast_physical =
+                tt::Cluster::instance().get_soc_desc(device_num_g).get_physical_tensix_core_from_logical(mcast_logical);
+        }
+
+        log_info(
+            LogTest,
+            "MCast {} core: {}, virtual {}, physical {}, writing {} bytes per xfer",
+            mcast_from_eth_g ? "ETH" : "TENSIX",
+            mcast_logical,
+            mcast_virtual,
+            mcast_physical,
+            mcast_size_g);
     }
 
-    log_info(
-        LogTest,
-        "MCast {} core: {}, virtual {}, physical {}, writing {} bytes per xfer",
-        mcast_from_eth_g ? "ETH" : "TENSIX",
-        mcast_logical,
-        mcast_virtual,
-        mcast_physical,
-        mcast_size_g);
     log_info(LogTest, "Unicast grid: {}, writing {} bytes per xfer", workers_logical.str(), ucast_size_g);
 
     if (rnd_coord_g) {
