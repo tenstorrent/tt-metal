e8db7f82f1d9e9608aa6d94c9f083dbbea76ab13
yugi957 <atul.krishnadas@outlook.com>
2025-01-16 19:38:32 +0000

    #16393: pytest unnecessary lines removal

--- tests/ttnn/unit_tests/operations/test_fill_pad.py
+++ tests/ttnn/unit_tests/operations/test_fill_pad.py
@@ -9,43 +9,6 @@ from tests.ttnn.utils_for_testing import assert_with_pcc
 from models.utility_functions import torch_random, run_for_wormhole_b0
 
 
-def create_padded_tiled_tensor(height, width, tile_size, fill_value, dtype):
-    """
-    Creates a 2D tensor of height x width with random values, creates another tensor of
-    (height rounded up to the nearest multiple of tile_size) x (width rounded up to the nearest multiple of tile_size) with the same random values,
-    and pads the original tensor with a fill_value to match the size of the second tensor.
-
-    Parameters:
-        height (int): The height of the tensor (number of rows).
-        width (int): The width of the tensor (number of columns).
-        tile_size (int): The size of each square tile (tile_size x tile_size).
-
-    Returns:
-        torch.Tensor: A 2D tensor with tile indices.
-    """
-    # Create a tensor with random values
-    # tensor = torch_random((height, width), 0, 10, dtype=dtype)
-    # tensor = create_tile_tensor(height, width, tile_size)
-    tensor = torch_random((height, width), -15.0, 15.0, dtype=dtype)
-    # tensor = torch.zeros((height, width), dtype=torch.int32)
-    # tensor = create_rm_tensor(height, width)
-    # tensor = torch.randint(0, 10, (height, width), dtype=dtype)
-    # tensor to dtype
-    # tensor = tensor.to(dtype=dtype)
-
-    # Calculate the size of the padded tensor
-    padded_height = (height + tile_size - 1) // tile_size * tile_size
-    padded_width = (width + tile_size - 1) // tile_size * tile_size
-
-    # Create a padded tensor padded with fill_value
-    padded_tensor = torch.full((padded_height, padded_width), fill_value, dtype=dtype)
-
-    # Copy the original tensor to the padded tensor
-    padded_tensor[:height, :width] = tensor
-
-    return tensor, padded_tensor
-
-
 def create_nd_padded_tiled_tensor(shape, tile_size, fill_value, dtype):
     """
     Creates a tensor with shape `shape` with random values, and another tensor with the same values,
