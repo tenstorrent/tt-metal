e965b54b16faa4446b8df899462639a2c5ebb1a4
Roman Furko <rfurko@tenstorrent.com>
2025-01-16 12:51:38 -0800

    [tt-train] Change weights initialization for GPT-2 (#16815)
    ### Problem description
    GPT-2 has specific initialization, which is different from default
    initialization of layers.
    
    ### What's changed
    * Fix tt-train build in main
    * Add weights initialization similar to GPT-2 original paper 
    
    <img width="864" alt="image"
    src="https://github.com/user-attachments/assets/f61af075-cc99-4881-b118-80caf0e6675d"
    />
    
    
    ### Checklist
    - [x] Post commit CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/12815660577
    - [x] New/Existing tests provide coverage for changes

--- tt-train/sources/ttml/models/gpt2.cpp
+++ tt-train/sources/ttml/models/gpt2.cpp
@@ -7,6 +7,7 @@
 #include "autograd/graph_utils.hpp"
 #include "autograd/tensor.hpp"
 #include "core/scoped.hpp"
+#include "init/tensor_initializers.hpp"
 #include "modules/positional_embeddings.hpp"
 #include "ops/binary_ops.hpp"
 #include "ops/unary_ops.hpp"
@@ -57,6 +58,18 @@ autograd::TensorPtr memory_efficient_runner(
     auto links = autograd::get_links(input);
     out->set_node(autograd::ctx().add_backward_node(std::move(grad), links));
     return out;
+}
+
+void weights_initialization(Transformer& model) {
+    auto params = model.parameters();
+    for (auto& [name, tensor_ptr] : params) {
+        const auto& tensor = tensor_ptr->get_value();
+        if (name.find("weight") != std::string::npos) {
+            init::normal_init(tensor_ptr, tensor.shape(), {0.F, 0.02F});
+        } else if (name.find("bias") != std::string::npos) {
+            init::constant_init(tensor_ptr, tensor.shape(), 0.F);
+        }
+    }
 }
 
 }  // namespace
@@ -135,6 +148,8 @@ Transformer::Transformer(const TransformerConfig& config) {
         // tie weights between embedding and fc
         tok_emb->set_weight(fc->get_weight());
     }
+
+    weights_initialization(*this);
 }
 
 ttml::autograd::TensorPtr Transformer::operator()(
