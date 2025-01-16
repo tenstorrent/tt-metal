ed5958584ab12035729cd7f1cf053051c2f223b7
Sudharsan-V <sudharsan.vijayaraghavan@multicorewareinc.com>
2025-01-16 05:32:51 +0000

    #13398: Optimize MNIST model

--- models/demos/wormhole/mnist/tt/tt_mnist.py
+++ models/demos/wormhole/mnist/tt/tt_mnist.py
@@ -12,17 +12,52 @@ def mnist(mesh_device, batch_size, x, parameters):
     x = ttnn.to_device(x, device=mesh_device, memory_config=ttnn.L1_MEMORY_CONFIG)
     x = ttnn.to_layout(x, layout=ttnn.TILE_LAYOUT)
 
-    x = ttnn.linear(
-        x, parameters.fc1.weight, bias=parameters.fc1.bias, memory_config=ttnn.L1_MEMORY_CONFIG, activation="relu"
+    memory_config = ttnn.create_sharded_memory_config(
+        x.shape,
+        core_grid=ttnn.CoreGrid(y=mesh_device.core_grid.y, x=mesh_device.core_grid.x),
+        strategy=ttnn.ShardStrategy.BLOCK,
+        # orientation=ttnn.ShardOrientation.TILE,
     )
 
     x = ttnn.linear(
-        x, parameters.fc2.weight, bias=parameters.fc2.bias, memory_config=ttnn.L1_MEMORY_CONFIG, activation="relu"
+        x,
+        parameters.fc1.weight,
+        bias=parameters.fc1.bias,
+        memory_config=memory_config,
+        activation="relu",
+        core_grid=mesh_device.core_grid,
+        compute_kernel_config=ttnn.WormholeComputeKernelConfig(
+            math_fidelity=ttnn.MathFidelity.LoFi,
+        ),
     )
 
     x = ttnn.linear(
-        x, parameters.fc3.weight, bias=parameters.fc3.bias, memory_config=ttnn.L1_MEMORY_CONFIG, activation="relu"
+        x,
+        parameters.fc2.weight,
+        bias=parameters.fc2.bias,
+        memory_config=memory_config,
+        activation="relu",
+        core_grid=mesh_device.core_grid,
+        compute_kernel_config=ttnn.WormholeComputeKernelConfig(
+            math_fidelity=ttnn.MathFidelity.LoFi,
+        ),
     )
+
+    x = ttnn.linear(
+        x,
+        parameters.fc3.weight,
+        bias=parameters.fc3.bias,
+        memory_config=memory_config,
+        activation="relu",
+        core_grid=mesh_device.core_grid,
+        compute_kernel_config=ttnn.WormholeComputeKernelConfig(
+            math_fidelity=ttnn.MathFidelity.LoFi,
+        ),
+    )
+
+    if x.is_sharded():
+        x = ttnn.sharded_to_interleaved(x, ttnn.L1_MEMORY_CONFIG)
 
     x = ttnn.softmax(x)
+
     return x
