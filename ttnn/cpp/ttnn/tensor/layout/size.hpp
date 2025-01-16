684e4c27269963265dbe442324d1d3a844b730e8
Oleg Milyutin <omilyutin@tenstorrent.com>
2025-01-16 18:13:29 +0000

    Lower Size to metalium as Shape2D

--- ttnn/cpp/ttnn/tensor/layout/size.hpp
+++ ttnn/cpp/ttnn/tensor/layout/size.hpp
@@ -4,41 +4,11 @@
 
 #pragma once
 
-#include <array>
-#include <cstddef>
-#include <cstdint>
-#include <utility>
-#include <tuple>
-#include <ostream>
+#include <tt-metalium/shape2d.hpp>
 
 namespace tt::tt_metal {
 
-class Size final {
-public:
-    Size(std::size_t height, std::size_t width);
-    Size(const std::pair<std::size_t, std::size_t>& size);
-    Size(const std::array<std::size_t, 2>& size);
-    Size(const std::array<std::uint32_t, 2>& size);
-
-    operator std::pair<std::size_t, std::size_t>() const;
-    operator std::array<std::size_t, 2>() const;
-    operator std::array<std::uint32_t, 2>() const;
-
-    Size operator*(std::size_t scalar) const;
-
-    bool operator==(const Size& rhs) const;
-
-    [[nodiscard]] std::size_t height() const;
-    [[nodiscard]] std::size_t width() const;
-
-    static constexpr auto attribute_names = std::forward_as_tuple("height", "width");
-    auto attribute_values() const { return std::forward_as_tuple(height_, width_); }
-
-private:
-    std::size_t height_ = 0;
-    std::size_t width_ = 0;
-};
-
-std::ostream& operator<<(std::ostream& os, const tt::tt_metal::Size& size);
+// TODO: Refactor s/Size/Shape2D/ in TTNN.
+using Size = Shape2D;
 
 }  // namespace tt::tt_metal
