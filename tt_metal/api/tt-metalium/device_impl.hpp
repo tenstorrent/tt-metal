15395af6bd5483e1d611ab8e9d15f5a07cd3623e
Austin Ho <aho@tenstorrent.com>
2025-01-17 20:02:34 +0000

    #15944: Remove get_fabric_sub_device_id and related apis since this did not end up being used

--- tt_metal/api/tt-metalium/device_impl.hpp
+++ tt_metal/api/tt-metalium/device_impl.hpp
@@ -242,8 +242,8 @@ public:
     uint32_t num_sub_devices() const override;
 
     // TODO #15944: Temporary api until migration to actual fabric is complete
-    std::tuple<SubDeviceManagerId, SubDeviceId> create_sub_device_manager_with_fabric(tt::stl::Span<const SubDevice> sub_devices, DeviceAddr local_l1_size) override;
-    std::optional<SubDeviceId> get_fabric_sub_device_id() const override;
+    std::tuple<SubDeviceManagerId, SubDeviceId> create_sub_device_manager_with_fabric(
+        tt::stl::Span<const SubDevice> sub_devices, DeviceAddr local_l1_size) override;
 
     uint32_t get_completion_queue_reader_core() const override { return completion_queue_reader_core_; }
 
