05c72016dcc04cc09494b33e5663e4479b684564
Nour Ardo <nardo@tenstorrent.com>
2025-01-17 17:13:23 -0500

    Fix scaling issue with RT arguments in tilize/untilize with padding (#16690)
    ### Ticket
    Link to Github Issue
    https://github.com/tenstorrent/tt-metal/issues/16689
    
    ### Problem description
    The runtime arguments for the reader and the writer kernels of the
    tilize/untilize with padding operations scale with the size of the
    tensor. For large inputs, the number of RT args exceeds the limit and
    the test fails
    
    ### What's changed
    Add a runtime argument that keeps track of the number of repeated RT
    args and pass them just once
    
    ### Checklist
    - [x] Post commit CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/12717283206
    - [ ] Blackhole Post commit (if applicable)
    - [ ] Model regression CI testing passes (if applicable)
    - [ ] Device performance regression CI testing passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new
    models](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    tests passes
    - [ ] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/data_movement/tilize_with_val_padding/device/kernels/dataflow/reader_unary_pad_dims_split_rows_multicore.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/tilize_with_val_padding/device/kernels/dataflow/reader_unary_pad_dims_split_rows_multicore.cpp
@@ -74,12 +74,28 @@ void kernel_main() {
 
     uint32_t stick_id = start_stick_id;
     uint32_t rt_arg_idx = 6;
-    for (uint32_t block_rep_idx = 0; block_rep_idx < n_block_reps; ++block_rep_idx) {
-        const uint32_t n_data = get_arg_val<uint32_t>(rt_arg_idx++);   // number of full tile-rows
-        const uint32_t n_mixed = get_arg_val<uint32_t>(rt_arg_idx++);  // number of rows in a partially filled tile-row
-        const uint32_t n_pads = get_arg_val<uint32_t>(rt_arg_idx++);   // number of padding tile-rows
-        const uint32_t times = get_arg_val<uint32_t>(rt_arg_idx++);  // number of times the pattern of tile-rows repeats
+    uint32_t count = 1;
+    constexpr int32_t n_mixed_idx = 1;
+    constexpr int32_t n_pad_idx = 2;
+    constexpr int32_t times_idx = 3;
+    constexpr uint32_t repeat_ct_idx = 4;
+    constexpr int32_t num_rt_idx = 5;
 
+    for (uint32_t block_rep_idx = 0; block_rep_idx < n_block_reps; ++block_rep_idx) {
+        const uint32_t repeat_count =
+            get_arg_val<uint32_t>(rt_arg_idx + repeat_ct_idx);  // number of times the same block representation is used
+        const uint32_t n_data = get_arg_val<uint32_t>(rt_arg_idx);  // number of full tile-rows
+        const uint32_t n_mixed =
+            get_arg_val<uint32_t>(rt_arg_idx + n_mixed_idx);  // number of rows in a partially filled tile-row
+        const uint32_t n_pads = get_arg_val<uint32_t>(rt_arg_idx + n_pad_idx);  // number of padding tile-rows
+        const uint32_t times =
+            get_arg_val<uint32_t>(rt_arg_idx + times_idx);  // number of times the pattern of tile-rows repeats
+        if (count == repeat_count) {
+            rt_arg_idx = rt_arg_idx + num_rt_idx;
+            count = 1;
+        } else {
+            count++;
+        }
         for (uint32_t t = 0; t < times; ++t) {
             for (uint32_t y_t = 0; y_t < n_data; y_t++) {
                 read_block(stick_id, tile_height);
