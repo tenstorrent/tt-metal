ebd1540d47476ee48b0950ee9250cf2a64485ea4
Ligang Long <llong@tenstorrent.com>
2025-01-17 18:48:30 +0000

    #13609: fix the mis-used scratch pad issue, also update the device for allocator l1 reserved space padding

--- ttnn/cpp/ttnn/operations/data_movement/sharded/device/kernels/dataflow/reshard_same_width_reader.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/sharded/device/kernels/dataflow/reshard_same_width_reader.cpp
@@ -26,14 +26,16 @@ void kernel_main() {
         uint32_t addr = src_addr + args[args_idx++];
         uint32_t units_to_transfer = args[args_idx++];
         uint32_t unit_size = args[args_idx++];
+        uint32_t read_stride_bytes = args[args_idx++];
+        uint32_t write_stride_bytes = args[args_idx++];
         uint64_t read_addr = get_noc_addr_from_bank_id<read_from_dram>(bank_id, addr);
         for (uint32_t unit_idx = 0; unit_idx < units_to_transfer; ++unit_idx) {
             noc_async_read(read_addr, scratch_l1_write_addr, unit_size);
             noc_async_read_barrier();
             noc_async_read(scratch_l1_noc_read_addr, l1_write_addr, unit_size);
-            read_addr += unit_size;
-            l1_write_addr += unit_size;
+            read_addr += read_stride_bytes;
+            l1_write_addr += write_stride_bytes;
+            noc_async_read_barrier();
         }
     }
-    noc_async_read_barrier();
 }
