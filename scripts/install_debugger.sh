1f273b35b75f9152085d2e6341419f6b59e63e34
Almeet Bhullar <abhullar@tenstorrent.com>
2025-01-17 22:46:48 +0000

    Update tt-lens install script to use install lfs

--- scripts/install_debugger.sh
+++ scripts/install_debugger.sh
@@ -1,4 +1,6 @@
 sudo apt update
-sudo apt install libzmq3-dev
+sudo apt install libzmq3-dev git-lfs
+git submodule foreach 'git lfs fetch --all && git lfs pull'
+git lfs install
 pip uninstall -y tt-lens
 pip install git+ssh://git@github.com/tenstorrent/tt-lens.git
