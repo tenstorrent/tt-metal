0f57a0e1eede18638ab074e9a40b5d06a42b891d
blozano-tt <blozano@tenstorrent.com>
2025-01-18 05:27:29 +0000

    desperate

--- cmake/x86-linux-clang-17-libcpp-toolchain.cmake
+++ cmake/x86-linux-clang-17-libcpp-toolchain.cmake
@@ -4,10 +4,12 @@ set(CMAKE_C_COMPILER clang-17 CACHE INTERNAL "C compiler")
 
 set(CMAKE_CXX_COMPILER clang++-17 CACHE INTERNAL "C++ compiler")
 
-set(CMAKE_CXX_FLAGS_INIT "-stdlib=libc++")
-
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-lc++ -lc++abi")
-set(CMAKE_SHARED_LINKER_FLAGS_INIT "-lc++ -lc++abi")
+#set(CMAKE_CXX_FLAGS_INIT "-stdlib=libc++")
+#set(CMAKE_EXE_LINKER_FLAGS_INIT "-lc++ -lc++abi")
+#set(CMAKE_SHARED_LINKER_FLAGS_INIT "-lc++ -lc++abi")
+set(CMAKE_CXX_FLAGS "-stdlib=libc++")
+set(CMAKE_EXE_LINKER_FLAGS "-lc++ -lc++abi")
+set(CMAKE_SHARED_LINKER_FLAGS "-lc++ -lc++abi")
 
 # Use for configure time
 set(ENABLE_LIBCXX TRUE CACHE INTERNAL "Using clang's libc++")
