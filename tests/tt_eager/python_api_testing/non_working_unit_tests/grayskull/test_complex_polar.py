8aa1bdd54291ecb9b030c63975c1b855319c60d6
Nemanja Grujic <ngrujic@tenstorrent.com>
2025-01-20 12:11:44 +0000

    #5164: Fixed complex_polar unit test

--- tests/tt_eager/python_api_testing/non_working_unit_tests/grayskull/test_complex_polar.py
+++ tests/tt_eager/python_api_testing/non_working_unit_tests/grayskull/test_complex_polar.py
@@ -2,8 +2,6 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-from pathlib import Path
-import sys
 from loguru import logger
 import random
 import pytest
@@ -12,22 +10,23 @@ import ttnn
 
 from tests.tt_eager.python_api_testing.sweep_tests import pytorch_ops
 from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc
-from tests.tt_eager.python_api_testing.sweep_tests import tt_lib_ops
+from tests.ttnn.python_api_testing.sweep_tests import ttnn_ops
 from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_rand_complex
 
 
 def run_complex_polar_test(input_shape, dtype, dlayout, in_mem_config, out_mem_config, data_seed, device):
     torch.manual_seed(data_seed)
 
-    x = torch.Tensor(size=input_shape).uniform_(-100, 100)
-    y = torch.Tensor(size=input_shape).uniform_(-100, 100)
+    real = torch.Tensor(size=input_shape).uniform_(-100, 100)
+    imag = torch.Tensor(size=input_shape).uniform_(-100, 100)
+
+    x = torch.complex(real, imag)
 
     # compute ref value
-    ref_value = pytorch_ops.complex_polar(x, y)
+    ref_value = pytorch_ops.complex_polar(x)
 
-    tt_result = tt_lib_ops.complex_polar(
+    tt_result = ttnn_ops.complex_polar(
         x=x,
-        y=y,
         device=device,
         dtype=dtype,
         layout=dlayout,
