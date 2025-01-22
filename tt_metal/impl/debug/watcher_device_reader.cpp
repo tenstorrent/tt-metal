3c2e4a3bc8f6f024c5616b696c5c577c0969639f
David Ma <davidma@tenstorrent.com>
2025-01-22 19:39:31 +0000

    #16716: Correctly sanitize local L1 for ethernet cores

--- tt_metal/impl/debug/watcher_device_reader.cpp
+++ tt_metal/impl/debug/watcher_device_reader.cpp
@@ -484,6 +484,10 @@ void WatcherDeviceReader::DumpNocSanitizeStatus(
             error_msg = get_noc_target_str(device, core, noc, san);
             error_msg += " (mixing virtual and physical coordinates in Mcast).";
             break;
+        case DebugSanitizeNocAddrMailbox:
+            error_msg = get_noc_target_str(device, core, noc, san);
+            error_msg += string(san->is_target ? " (NOC target" : " (Local L1") + " overwrites mailboxes).";
+            break;
         default:
             error_msg = fmt::format(
                 "Watcher unexpected data corruption, noc debug state on core {}, unknown failure code: {}",
