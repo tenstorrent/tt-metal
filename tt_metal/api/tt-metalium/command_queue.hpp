69be557ad592699cde16543462c7ecf3f59858a3
Oleg Milyutin <omilyutin@tenstorrent.com>
2025-01-24 23:18:20 +0000

    Remove set/get queue mode methods

--- tt_metal/api/tt-metalium/command_queue.hpp
+++ tt_metal/api/tt-metalium/command_queue.hpp
@@ -22,15 +22,11 @@ class Program;
 class Kernel;
 }  // namespace v0
 
-class CommandQueue {
-public:
-    virtual ~CommandQueue() = default;
+class HWCommandQueue;
 
-    virtual const CoreCoord& virtual_enqueue_program_dispatch_core() const = 0;
-    virtual const CoreCoord& completion_queue_writer_core() const = 0;
-
-    virtual volatile bool is_dprint_server_hung() = 0;
-    virtual volatile bool is_noc_hung() = 0;
+// Only contains the types of commands which are enqueued onto the device
+enum class EnqueueCommandType {
+    ENQUEUE_READ_BUFFER, ENQUEUE_WRITE_BUFFER, virtual volatile bool is_noc_hung() = 0;
 
     virtual void record_begin(const uint32_t tid, const std::shared_ptr<TraceDescriptor>& ctx) = 0;
     virtual void record_end() = 0;
@@ -45,29 +41,29 @@ public:
     virtual uint32_t id() const = 0;
     virtual std::optional<uint32_t> tid() const = 0;
 
-    virtual SystemMemoryManager& sysmem_manager() = 0;
+    virtual SystemMemoryManager & sysmem_manager() = 0;
 
     virtual void terminate() = 0;
 
-    virtual IDevice* device() = 0;
+    virtual IDevice * device() = 0;
 
     // These functions are temporarily needed since MeshCommandQueue relies on the CommandQueue object
     virtual uint32_t get_expected_num_workers_completed_for_sub_device(uint32_t sub_device_index) const = 0;
     virtual void set_expected_num_workers_completed_for_sub_device(uint32_t sub_device_index, uint32_t num_workers) = 0;
-    virtual WorkerConfigBufferMgr& get_config_buffer_mgr(uint32_t index) = 0;
+    virtual WorkerConfigBufferMgr & get_config_buffer_mgr(uint32_t index) = 0;
 
     virtual void enqueue_trace(const uint32_t trace_id, bool blocking) = 0;
 
-    virtual void enqueue_program(Program& program, bool blocking) = 0;
+    virtual void enqueue_program(Program & program, bool blocking) = 0;
 
     virtual void enqueue_read_buffer(
-        std::shared_ptr<Buffer>& buffer,
+        std::shared_ptr<Buffer> & buffer,
         void* dst,
         const BufferRegion& region,
         bool blocking,
         tt::stl::Span<const SubDeviceId> sub_device_ids = {}) = 0;
     virtual void enqueue_read_buffer(
-        Buffer& buffer,
+        Buffer & buffer,
         void* dst,
         const BufferRegion& region,
         bool blocking,
@@ -86,7 +82,7 @@ public:
         bool blocking,
         tt::stl::Span<const SubDeviceId> sub_device_ids = {}) = 0;
     virtual void enqueue_write_buffer(
-        Buffer& buffer,
+        Buffer & buffer,
         const void* src,
         const BufferRegion& region,
         bool blocking,
