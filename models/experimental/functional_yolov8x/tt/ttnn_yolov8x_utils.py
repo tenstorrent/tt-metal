38f2590817e6c026dc05355cbb00f58d63d5e72d
Saichand A <saichand@multicorewareinc.com>
2025-01-24 09:55:50 +0000

    #0: Removed extra ttnn ops sharded_to_interleaved, reshape from detect method.

--- models/experimental/functional_yolov8x/tt/ttnn_yolov8x_utils.py
+++ models/experimental/functional_yolov8x/tt/ttnn_yolov8x_utils.py
@@ -32,7 +32,7 @@ def autopad(k, p=None, d=1):
 
 
 def ttnn_decode_bboxes(device, distance, anchor_points, xywh=True, dim=1):
-    lt, rb = ttnn.split(distance, 2, 1)
+    lt, rb = ttnn.split(distance, 2, 1)  # if done in tile : tt-metal issue #17017
     lt = ttnn.to_layout(lt, ttnn.TILE_LAYOUT)
     rb = ttnn.to_layout(rb, ttnn.TILE_LAYOUT)
 
@@ -48,6 +48,7 @@ def ttnn_decode_bboxes(device, distance, anchor_points, xywh=True, dim=1):
 def ttnn_make_anchors(device, feats, strides, grid_cell_offset=0.5):
     anchor_points, stride_tensor = [], []
     assert feats is not None
+
     for i, stride in enumerate(strides):
         h, w = feats[i].shape[1], feats[i].shape[2]
 
@@ -66,30 +67,18 @@ def ttnn_make_anchors(device, feats, strides, grid_cell_offset=0.5):
         sy = ttnn.reshape(sy, (w, 1, 1, 1))
         sy = ttnn.repeat(sy, ttnn.Shape((1, h, 1, 1)))
 
-        sx = ttnn.reshape(sx, (w, h))
-        sy = ttnn.reshape(sy, (h, w))
-
-        sx = ttnn.sharded_to_interleaved(sx, ttnn.L1_MEMORY_CONFIG)
-        sx = ttnn.to_layout(sx, ttnn.ROW_MAJOR_LAYOUT)
-        sx = ttnn.reshape(sx, (sx.shape[0], sx.shape[1], -1))
-
-        sy = ttnn.sharded_to_interleaved(sy, ttnn.L1_MEMORY_CONFIG)
-        sy = ttnn.to_layout(sy, ttnn.ROW_MAJOR_LAYOUT)
-        sy = ttnn.reshape(sy, (sx.shape[0], sx.shape[1], -1))
+        sx = ttnn.reshape(sx, (w, h, -1))
+        sy = ttnn.reshape(sy, (h, w, -1))
 
         temp = ttnn.concat([sx, sy], dim=-1)
         temp = ttnn.reshape(temp, (-1, 2))
 
-        temp = ttnn.sharded_to_interleaved(temp, ttnn.L1_MEMORY_CONFIG)
-        temp = ttnn.to_layout(temp, ttnn.TILE_LAYOUT)
-        temp = ttnn.to_device(temp, device)
         anchor_points.append(temp)
 
-        temp = ttnn.full(shape=[h * w, 1], fill_value=stride, dtype=ttnn.bfloat16)
+        temp = ttnn.full(
+            shape=[h * w, 1], fill_value=stride, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device
+        )
 
-        temp = ttnn.sharded_to_interleaved(temp, ttnn.L1_MEMORY_CONFIG)
-        temp = ttnn.to_layout(temp, ttnn.TILE_LAYOUT)
-        temp = ttnn.to_device(temp, device)
         stride_tensor.append(temp)
 
     a = ttnn.concat(anchor_points, dim=0)
@@ -98,120 +87,128 @@ def ttnn_make_anchors(device, feats, strides, grid_cell_offset=0.5):
     return (a, b)
 
 
-def preprocess_parameters(state_dict, path, bias=True):
+def preprocess_parameters(state_dict, path, bias=True, bfloat8=True):
     if bias:
         conv_weight = state_dict[f"{path}.2.weight"]
         conv_bias = state_dict[f"{path}.2.bias"]
 
-        conv_weight = ttnn.from_torch(conv_weight, dtype=ttnn.bfloat16)
-        conv_bias = ttnn.reshape(ttnn.from_torch(conv_bias, dtype=ttnn.bfloat16), (1, 1, 1, -1))
+        if bfloat8:
+            conv_weight = ttnn.from_torch(conv_weight, dtype=ttnn.float32)
+            conv_bias = ttnn.reshape(ttnn.from_torch(conv_bias, dtype=ttnn.float32), (1, 1, 1, -1))
+        else:
+            conv_weight = ttnn.from_torch(conv_weight, dtype=ttnn.bfloat16)
+            conv_bias = ttnn.reshape(ttnn.from_torch(conv_bias, dtype=ttnn.bfloat16), (1, 1, 1, -1))
 
         return (conv_weight, conv_bias)
 
     else:
         conv_weight = state_dict[f"{path}.conv.weight"]
-        conv_weight = ttnn.from_torch(conv_weight, dtype=ttnn.bfloat16)
+
+        if bfloat8:
+            conv_weight = ttnn.from_torch(conv_weight, dtype=ttnn.float32)
+        else:
+            conv_weight = ttnn.from_torch(conv_weight, dtype=ttnn.bfloat16)
 
         return conv_weight
 
 
 def custom_preprocessor(device, state_dict):
     pairs = [
-        ("model.0", False),
-        ("model.1", False),
-        ("model.2.cv1", False),
-        ("model.2.m.0.cv1", False),
-        ("model.2.m.0.cv2", False),
-        ("model.2.m.1.cv1", False),
-        ("model.2.m.1.cv2", False),
-        ("model.2.m.2.cv1", False),
-        ("model.2.m.2.cv2", False),
-        ("model.2.cv2", False),
-        ("model.3", False),
+        ("model.0", True),
+        ("model.1", True),
+        ("model.2.cv1", True),
+        ("model.2.m.0.cv1", True),
+        ("model.2.m.0.cv2", True),
+        ("model.2.m.1.cv1", True),
+        ("model.2.m.1.cv2", True),
+        ("model.2.m.2.cv1", True),
+        ("model.2.m.2.cv2", True),
+        ("model.2.cv2", True),
+        ("model.3", True),
         ("model.4.cv1", True),
-        ("model.4.m.0.cv1", False),
-        ("model.4.m.0.cv2", False),
-        ("model.4.m.1.cv1", False),
-        ("model.4.m.1.cv2", False),
-        ("model.4.m.2.cv1", False),
-        ("model.4.m.2.cv2", False),
-        ("model.4.m.3.cv1", False),
-        ("model.4.m.3.cv2", False),
-        ("model.4.m.4.cv1", False),
-        ("model.4.m.4.cv2", False),
-        ("model.4.m.5.cv1", False),
-        ("model.4.m.5.cv2", False),
+        ("model.4.m.0.cv1", True),
+        ("model.4.m.0.cv2", True),
+        ("model.4.m.1.cv1", True),
+        ("model.4.m.1.cv2", True),
+        ("model.4.m.2.cv1", True),
+        ("model.4.m.2.cv2", True),
+        ("model.4.m.3.cv1", True),
+        ("model.4.m.3.cv2", True),
+        ("model.4.m.4.cv1", True),
+        ("model.4.m.4.cv2", True),
+        ("model.4.m.5.cv1", True),
+        ("model.4.m.5.cv2", True),
         ("model.4.cv2", True),
-        ("model.5", False),
-        ("model.6.cv1", False),
-        ("model.6.m.0.cv1", False),
-        ("model.6.m.0.cv2", False),
-        ("model.6.m.1.cv1", False),
-        ("model.6.m.1.cv2", False),
-        ("model.6.m.2.cv1", False),
-        ("model.6.m.2.cv2", False),
-        ("model.6.m.3.cv1", False),
-        ("model.6.m.3.cv2", False),
-        ("model.6.m.4.cv1", False),
-        ("model.6.m.4.cv2", False),
-        ("model.6.m.5.cv1", False),
-        ("model.6.m.5.cv2", False),
-        ("model.6.cv2", False),
-        ("model.7", False),
+        ("model.5", True),
+        ("model.6.cv1", True),
+        ("model.6.m.0.cv1", True),
+        ("model.6.m.0.cv2", True),
+        ("model.6.m.1.cv1", True),
+        ("model.6.m.1.cv2", True),
+        ("model.6.m.2.cv1", True),
+        ("model.6.m.2.cv2", True),
+        ("model.6.m.3.cv1", True),
+        ("model.6.m.3.cv2", True),
+        ("model.6.m.4.cv1", True),
+        ("model.6.m.4.cv2", True),
+        ("model.6.m.5.cv1", True),
+        ("model.6.m.5.cv2", True),
+        ("model.6.cv2", True),
+        ("model.7", True),
         ("model.8.cv1", True),
-        ("model.8.m.0.cv1", False),
-        ("model.8.m.0.cv2", False),
-        ("model.8.m.1.cv1", False),
-        ("model.8.m.1.cv2", False),
-        ("model.8.m.2.cv1", False),
-        ("model.8.m.2.cv2", False),
+        ("model.8.m.0.cv1", True),
+        ("model.8.m.0.cv2", True),
+        ("model.8.m.1.cv1", True),
+        ("model.8.m.1.cv2", True),
+        ("model.8.m.2.cv1", True),
+        ("model.8.m.2.cv2", True),
         ("model.8.cv2", True),
-        ("model.9.cv1", False),
-        ("model.9.cv2", False),
+        ("model.9.cv1", True),
+        ("model.9.cv2", True),
         ("model.12.cv1", True),
-        ("model.12.m.0.cv1", False),
-        ("model.12.m.0.cv2", False),
-        ("model.12.m.1.cv1", False),
-        ("model.12.m.1.cv2", False),
-        ("model.12.m.2.cv1", False),
-        ("model.12.m.2.cv2", False),
+        ("model.12.m.0.cv1", True),
+        ("model.12.m.0.cv2", True),
+        ("model.12.m.1.cv1", True),
+        ("model.12.m.1.cv2", True),
+        ("model.12.m.2.cv1", True),
+        ("model.12.m.2.cv2", True),
         ("model.12.cv2", True),
-        ("model.15.cv1", False),
-        ("model.15.m.0.cv1", False),
-        ("model.15.m.0.cv2", False),
-        ("model.15.m.1.cv1", False),
-        ("model.15.m.1.cv2", False),
-        ("model.15.m.2.cv1", False),
-        ("model.15.m.2.cv2", False),
-        ("model.15.cv2", False),
-        ("model.16", False),
+        ("model.15.cv1", True),
+        ("model.15.m.0.cv1", True),
+        ("model.15.m.0.cv2", True),
+        ("model.15.m.1.cv1", True),
+        ("model.15.m.1.cv2", True),
+        ("model.15.m.2.cv1", True),
+        ("model.15.m.2.cv2", True),
+        ("model.15.cv2", True),
+        ("model.16", True),
         ("model.18.cv1", True),
-        ("model.18.m.0.cv1", False),
-        ("model.18.m.0.cv2", False),
-        ("model.18.m.1.cv1", False),
-        ("model.18.m.1.cv2", False),
-        ("model.18.m.2.cv1", False),
-        ("model.18.m.2.cv2", False),
+        ("model.18.m.0.cv1", True),
+        ("model.18.m.0.cv2", True),
+        ("model.18.m.1.cv1", True),
+        ("model.18.m.1.cv2", True),
+        ("model.18.m.2.cv1", True),
+        ("model.18.m.2.cv2", True),
         ("model.18.cv2", True),
-        ("model.19", False),
+        ("model.19", True),
         ("model.21.cv1", True),
-        ("model.21.m.0.cv1", False),
-        ("model.21.m.0.cv2", False),
-        ("model.21.m.1.cv1", False),
-        ("model.21.m.1.cv2", False),
-        ("model.21.m.2.cv1", False),
-        ("model.21.m.2.cv2", False),
+        ("model.21.m.0.cv1", True),
+        ("model.21.m.0.cv2", True),
+        ("model.21.m.1.cv1", True),
+        ("model.21.m.1.cv2", True),
+        ("model.21.m.2.cv1", True),
+        ("model.21.m.2.cv2", True),
         ("model.21.cv2", True),
-        ("model.22.cv2.0.0", False),
-        ("model.22.cv2.0.1", False),
+        ("model.22.cv2.0.0", True),
+        ("model.22.cv2.0.1", True),
         ("model.22.cv3.0.0", True),
         ("model.22.cv3.0.1", True),
-        ("model.22.cv2.1.0", False),
-        ("model.22.cv2.1.1", False),
+        ("model.22.cv2.1.0", True),
+        ("model.22.cv2.1.1", True),
         ("model.22.cv3.1.0", True),
         ("model.22.cv3.1.1", True),
-        ("model.22.cv2.2.0", False),
-        ("model.22.cv2.2.1", False),
+        ("model.22.cv2.2.0", True),
+        ("model.22.cv2.2.1", True),
         ("model.22.cv3.2.0", True),
         ("model.22.cv3.2.1", True),
     ]
@@ -219,21 +216,21 @@ def custom_preprocessor(device, state_dict):
     parameters = {}
 
     for path, bfloat8 in pairs:
-        parameters[path] = fold_batch_norm2d_into_conv2d(device, state_dict, path=path, bfloat8=bfloat8)
+        parameters[path] = fold_batch_norm2d_into_conv2d(device, state_dict, path=path, bfloat8=True)
 
     # Detect
 
-    parameters["model.22.cv2.0"] = preprocess_parameters(state_dict, "model.22.cv2.0")
-    parameters["model.22.cv3.0"] = preprocess_parameters(state_dict, "model.22.cv3.0")
+    parameters["model.22.cv2.0"] = preprocess_parameters(state_dict, "model.22.cv2.0", bfloat8=True)
+    parameters["model.22.cv3.0"] = preprocess_parameters(state_dict, "model.22.cv3.0", bfloat8=True)
 
-    parameters["model.22.cv2.1"] = preprocess_parameters(state_dict, "model.22.cv2.1")
-    parameters["model.22.cv3.1"] = preprocess_parameters(state_dict, "model.22.cv3.1")
+    parameters["model.22.cv2.1"] = preprocess_parameters(state_dict, "model.22.cv2.1", bfloat8=True)
+    parameters["model.22.cv3.1"] = preprocess_parameters(state_dict, "model.22.cv3.1", bfloat8=True)
 
-    parameters["model.22.cv2.2"] = preprocess_parameters(state_dict, "model.22.cv2.2")
-    parameters["model.22.cv3.2"] = preprocess_parameters(state_dict, "model.22.cv3.2")
+    parameters["model.22.cv2.2"] = preprocess_parameters(state_dict, "model.22.cv2.2", bfloat8=True)
+    parameters["model.22.cv3.2"] = preprocess_parameters(state_dict, "model.22.cv3.2", bfloat8=True)
 
     # DFL conv
 
-    parameters["model.22.dfl"] = preprocess_parameters(state_dict, "model.22.dfl", bias=False)
+    parameters["model.22.dfl"] = preprocess_parameters(state_dict, "model.22.dfl", bias=False, bfloat8=True)
 
     return parameters
