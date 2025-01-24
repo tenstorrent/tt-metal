42d2c97db5044bca89f0f65a4a3684d66f469907
Kalaivani Baskar <156762498+KalaivaniMCW@users.noreply.github.com>
2025-01-24 18:33:53 +0530

    Migrate Binary Sfpu ops to binary_ng with activations (#16523)
    ### Ticket
    Link to Github Issue #16144 
    
    ### Problem description
    - Binary_ng ops support only bfloat16 datatype
    - binary bitwise ops, rsub, pow, add(int32) are not present in binary_ng
    
    ### What's changed
    - Added float32 support for binary_ng ops
    - Added bitwise ops
    - Added add(int32), rsub and pow to binary_ng
    - Fixed bias_gelu logic
    
    ### Checklist
    - [x] Post commit CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/12796247845
    https://github.com/tenstorrent/tt-metal/actions/runs/12834197476
    https://github.com/tenstorrent/tt-metal/actions/runs/12922250157
    https://github.com/tenstorrent/tt-metal/actions/runs/12948620892
    - [x] Blackhole Post commit (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/12805291674
    https://github.com/tenstorrent/tt-metal/actions/runs/12834199165
    https://github.com/tenstorrent/tt-metal/actions/runs/12914904913
    https://github.com/tenstorrent/tt-metal/actions/runs/12948614665
    - [ ] Model regression CI testing passes (if applicable)
    - [ ] Device performance regression CI testing passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new
    models](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    tests https://github.com/tenstorrent/tt-metal/actions/runs/12842128253
    https://github.com/tenstorrent/tt-metal/actions/runs/12916189194
    - [x] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: Patrick Roberts <proberts@tenstorrent.com>

--- ttnn/cpp/ttnn/operations/eltwise/binary_ng/binary_ng.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/binary_ng/binary_ng.cpp
@@ -6,6 +6,14 @@
 #include "binary_ng.hpp"
 #include "device/binary_ng_device_operation.hpp"
 
+inline Tensor typecast_to(DataType dtype, const Tensor& input) {
+    return input.get_dtype() == dtype ? input : ttnn::typecast(input, dtype);
+}
+
+inline bool needs_typecast_to_bfloat16(const Tensor& input) {
+    return (input.get_dtype() == DataType::BFLOAT8_B || input.get_dtype() == DataType::BFLOAT4_B);
+}
+
 namespace ttnn::operations::binary_ng {
 
 template <BinaryOpType binary_op_type>
@@ -19,8 +27,10 @@ Tensor BinaryNg<binary_op_type>::invoke(
     tt::stl::Span<const ttnn::operations::unary::UnaryOpType> lhs_activations,
     tt::stl::Span<const ttnn::operations::unary::UnaryOpType> rhs_activations,
     tt::stl::Span<const ttnn::operations::unary::UnaryOpType> post_activations) {
-    auto input_a = typecast_to(DataType::BFLOAT16, input_tensor_a);
-    auto input_b = typecast_to(DataType::BFLOAT16, input_tensor_b);
+    bool typecast_a = needs_typecast_to_bfloat16(input_tensor_a);
+    bool typecast_b = needs_typecast_to_bfloat16(input_tensor_b);
+    Tensor input_a = typecast_a ? typecast_to(DataType::BFLOAT16, input_tensor_a) : input_tensor_a;
+    Tensor input_b = typecast_b ? typecast_to(DataType::BFLOAT16, input_tensor_b) : input_tensor_b;
 
     return ttnn::prim::binary_ng(
         queue_id,
@@ -68,7 +78,8 @@ Tensor BinaryNg<binary_op_type>::invoke(
     tt::stl::Span<const ttnn::operations::unary::UnaryOpType> lhs_activations,
     tt::stl::Span<const ttnn::operations::unary::UnaryOpType> rhs_activations,
     tt::stl::Span<const ttnn::operations::unary::UnaryOpType> post_activations) {
-    auto input_a = typecast_to(DataType::BFLOAT16, input_tensor_a);
+    bool typecast_a = needs_typecast_to_bfloat16(input_tensor_a);
+    Tensor input_a = typecast_a ? typecast_to(DataType::BFLOAT16, input_tensor_a) : input_tensor_a;
 
     return ttnn::prim::binary_ng(
         queue_id,
@@ -105,10 +116,87 @@ Tensor BinaryNg<binary_op_type>::invoke(
         post_activations);
 }
 
+template <BinaryOpType binary_op_type>
+Tensor BinaryNgBitwise<binary_op_type>::invoke(
+    uint8_t queue_id,
+    const Tensor& input_tensor_a,
+    const Tensor& input_tensor_b,
+    const std::optional<MemoryConfig>& memory_config,
+    std::optional<Tensor> optional_output_tensor) {
+    TT_FATAL(
+        input_tensor_a.get_dtype() == DataType::INT32 && input_tensor_b.get_dtype() == DataType::INT32,
+        "Bitwise ops require input tensors to be of INT32 datatype ");
+
+    tt::stl::Span<const unary::UnaryOpType> lhs_activations = {};
+    tt::stl::Span<const unary::UnaryOpType> rhs_activations = {};
+    tt::stl::Span<const unary::UnaryOpType> post_activations = {};
+
+    return ttnn::prim::binary_ng(
+        queue_id,
+        input_tensor_a,
+        input_tensor_b,
+        binary_op_type,
+        std::nullopt,
+        memory_config,
+        optional_output_tensor,
+        lhs_activations,
+        rhs_activations,
+        post_activations);
+}
+
+template <BinaryOpType binary_op_type>
+Tensor BinaryNgBitwise<binary_op_type>::invoke(
+    const Tensor& input_tensor_a,
+    const Tensor& input_tensor_b,
+    const std::optional<MemoryConfig>& memory_config,
+    std::optional<Tensor> optional_output_tensor) {
+    return BinaryNgBitwise<binary_op_type>::invoke(
+        DefaultQueueId, input_tensor_a, input_tensor_b, memory_config, optional_output_tensor);
+}
+
+template <BinaryOpType binary_op_type>
+Tensor BinaryNgBitwise<binary_op_type>::invoke(
+    uint8_t queue_id,
+    const Tensor& input_tensor_a,
+    float scalar,
+    const std::optional<MemoryConfig>& memory_config,
+    std::optional<Tensor> optional_output_tensor) {
+    TT_FATAL(
+        input_tensor_a.get_dtype() == DataType::INT32, "Bitwise ops require input tensor to be of INT32 datatype ");
+
+    tt::stl::Span<const unary::UnaryOpType> lhs_activations = {};
+    tt::stl::Span<const unary::UnaryOpType> rhs_activations = {};
+    tt::stl::Span<const unary::UnaryOpType> post_activations = {};
+
+    return ttnn::prim::binary_ng(
+        queue_id,
+        input_tensor_a,
+        scalar,
+        binary_op_type,
+        std::nullopt,
+        memory_config,
+        optional_output_tensor,
+        lhs_activations,
+        rhs_activations,
+        post_activations);
+}
+
+template <BinaryOpType binary_op_type>
+Tensor BinaryNgBitwise<binary_op_type>::invoke(
+    const Tensor& input_tensor_a,
+    float scalar,
+    const std::optional<MemoryConfig>& memory_config,
+    std::optional<Tensor> optional_output_tensor) {
+    return BinaryNgBitwise<binary_op_type>::invoke(
+        DefaultQueueId, input_tensor_a, scalar, memory_config, optional_output_tensor);
+}
+
 template struct BinaryNg<BinaryOpType::ADD>;
 template struct BinaryNg<BinaryOpType::SUB>;
 template struct BinaryNg<BinaryOpType::MUL>;
 template struct BinaryNg<BinaryOpType::DIV>;
+template struct BinaryNg<BinaryOpType::RSUB>;
+template struct BinaryNg<BinaryOpType::POWER>;
 template struct BinaryNg<BinaryOpType::GT>;
 template struct BinaryNg<BinaryOpType::LT>;
 template struct BinaryNg<BinaryOpType::LTE>;
@@ -123,5 +211,11 @@ template struct BinaryNg<BinaryOpType::LOGICAL_XOR>;
 template struct BinaryNg<BinaryOpType::LDEXP>;
 template struct BinaryNg<BinaryOpType::LOGADDEXP>;
 template struct BinaryNg<BinaryOpType::LOGADDEXP2>;
+
+template struct BinaryNgBitwise<BinaryOpType::BITWISE_AND>;
+template struct BinaryNgBitwise<BinaryOpType::BITWISE_OR>;
+template struct BinaryNgBitwise<BinaryOpType::BITWISE_XOR>;
+template struct BinaryNgBitwise<BinaryOpType::LEFT_SHIFT>;
+template struct BinaryNgBitwise<BinaryOpType::RIGHT_SHIFT>;
 
 }  // namespace ttnn::operations::binary_ng
