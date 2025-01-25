ad0b8069103da34c82f61ee8c787e415d71513bc
Anasuya G Nair <mcw-anair@ext.tenstorrent.com>
2025-01-26 00:23:09 +0530

    #16143: Inplace support for binary_ng ops with fused activations (#16449)
    ### Ticket
    #16143
    #16153 
    
    ### Problem description
    Provide inplace support for binary_ng ops with fused activations with
    validation checks for input.
    
    ### What's changed
    Added in-place support for 18 ops 
    Added fused activations on input tensors.
    Added validation checks for input tensor a.
    
    ### Checklist
    - [x] Post commit CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/12966520490 -
    PASSED
    - [x] Blackhole post-commit tests
    https://github.com/tenstorrent/tt-metal/actions/runs/12966965482 -
    PASSED
    - [x] (Single-card) Tests for new models
    https://github.com/tenstorrent/tt-metal/actions/runs/12966971183 - same
    as main
    - [x] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: Patrick Roberts <proberts@tenstorrent.com>

--- ttnn/cpp/ttnn/operations/eltwise/binary_ng/binary_ng.hpp
+++ ttnn/cpp/ttnn/operations/eltwise/binary_ng/binary_ng.hpp
@@ -86,6 +86,39 @@ struct BinaryNgBitwise {
         std::optional<Tensor> optional_output_tensor = std::nullopt);
 };
 
+template <BinaryOpType binary_op_type>
+struct InplaceBinaryNg {
+    static Tensor invoke(
+        uint8_t queue_id,
+        const Tensor& input_tensor_a,
+        const Tensor& input_tensor_b,
+        tt::stl::Span<const unary::UnaryWithParam> lhs_activations = {},
+        tt::stl::Span<const unary::UnaryWithParam> rhs_activations = {},
+        tt::stl::Span<const unary::UnaryWithParam> post_activations = {});
+
+    static Tensor invoke(
+        const Tensor& input_tensor_a,
+        const Tensor& input_tensor_b,
+        tt::stl::Span<const unary::UnaryWithParam> lhs_activations = {},
+        tt::stl::Span<const unary::UnaryWithParam> rhs_activations = {},
+        tt::stl::Span<const unary::UnaryWithParam> post_activations = {});
+
+    static Tensor invoke(
+        uint8_t queue_id,
+        const Tensor& input_tensor,
+        float scalar,
+        tt::stl::Span<const unary::UnaryWithParam> lhs_activations = {},
+        tt::stl::Span<const unary::UnaryWithParam> rhs_activations = {},
+        tt::stl::Span<const unary::UnaryWithParam> post_activations = {});
+
+    static Tensor invoke(
+        const Tensor& input_tensor,
+        float scalar,
+        tt::stl::Span<const unary::UnaryWithParam> lhs_activations = {},
+        tt::stl::Span<const unary::UnaryWithParam> rhs_activations = {},
+        tt::stl::Span<const unary::UnaryWithParam> post_activations = {});
+};
+
 }  // namespace ttnn::operations::binary_ng
 
 namespace ttnn::experimental {
@@ -188,5 +221,85 @@ constexpr auto bitwise_left_shift = ttnn::register_operation_with_auto_launch_op
 constexpr auto bitwise_right_shift = ttnn::register_operation_with_auto_launch_op<
     "ttnn::experimental::bitwise_right_shift",
     ttnn::operations::binary_ng::BinaryNgBitwise<operations::binary_ng::BinaryOpType::RIGHT_SHIFT>>();
+
+constexpr auto add_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::add_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::ADD>>();
+
+constexpr auto sub_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::sub_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::SUB>>();
+
+constexpr auto mul_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::mul_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::MUL>>();
+
+constexpr auto div_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::div_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::DIV>>();
+
+constexpr auto rsub_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::rsub_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::RSUB>>();
+
+constexpr auto pow_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::pow_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::POWER>>();
+
+constexpr auto gt_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::gt_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::GT>>();
+
+constexpr auto lt_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::lt_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::LT>>();
+
+constexpr auto lte_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::lte_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::LTE>>();
+
+constexpr auto gte_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::gte_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::GTE>>();
+
+constexpr auto eq_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::eq_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::EQ>>();
+
+constexpr auto ne_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::ne_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::NE>>();
+
+constexpr auto squared_difference_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::squared_difference_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::SQUARED_DIFFERENCE>>();
+
+constexpr auto bias_gelu_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::bias_gelu_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::BIAS_GELU>>();
+
+constexpr auto logical_and_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::logical_and_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::LOGICAL_AND>>();
+
+constexpr auto logical_or_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::logical_or_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::LOGICAL_OR>>();
+
+constexpr auto logical_xor_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::logical_xor_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::LOGICAL_XOR>>();
+
+constexpr auto ldexp_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::ldexp_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::LDEXP>>();
+
+constexpr auto logaddexp_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::logaddexp_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::LOGADDEXP>>();
+
+constexpr auto logaddexp2_ = ttnn::register_operation_with_auto_launch_op<
+    "ttnn::experimental::logaddexp2_",
+    ttnn::operations::binary_ng::InplaceBinaryNg<operations::binary_ng::BinaryOpType::LOGADDEXP2>>();
 
 }  // namespace ttnn::experimental
--- ttnn/cpp/ttnn/operations/eltwise/binary_ng/binary_ng_pybind.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/binary_ng/binary_ng_pybind.cpp
@@ -126,6 +126,53 @@ void bind_binary_ng_bitwise_ops(py::module& module, T op, const std::string& doc
             py::arg("output_tensor") = std::nullopt,
             py::arg("queue_id") = 0});
 }
+
+template <typename T>
+void bind_inplace_binary_ng_operation(py::module& module, T op, const std::string& docstring) {
+    bind_registered_operation(
+        module,
+        op,
+        docstring,
+
+        // tensor and scalar
+        ttnn::pybind_overload_t{
+            [](const T& self,
+               const ttnn::Tensor& input_tensor_a,
+               const float scalar,
+               const ttnn::SmallVector<unary::UnaryWithParam>& lhs_activations,
+               const ttnn::SmallVector<unary::UnaryWithParam>& rhs_activations,
+               const ttnn::SmallVector<unary::UnaryWithParam>& post_activations,
+               const uint8_t& queue_id) -> ttnn::Tensor {
+                return self(queue_id, input_tensor_a, scalar, lhs_activations, rhs_activations, post_activations);
+            },
+            py::arg("input_tensor_a"),
+            py::arg("scalar"),
+            py::kw_only(),
+            py::arg("lhs_activations") = ttnn::SmallVector<unary::UnaryWithParam>(),
+            py::arg("rhs_activations") = ttnn::SmallVector<unary::UnaryWithParam>(),
+            py::arg("post_activations") = ttnn::SmallVector<unary::UnaryWithParam>(),
+            py::arg("queue_id") = 0},
+
+        // tensor and tensor
+        ttnn::pybind_overload_t{
+            [](const T& self,
+               const ttnn::Tensor& input_tensor_a,
+               const ttnn::Tensor& input_tensor_b,
+               const ttnn::SmallVector<unary::UnaryWithParam>& lhs_activations,
+               const ttnn::SmallVector<unary::UnaryWithParam>& rhs_activations,
+               const ttnn::SmallVector<unary::UnaryWithParam>& post_activations,
+               uint8_t queue_id) -> ttnn::Tensor {
+                return self(
+                    queue_id, input_tensor_a, input_tensor_b, lhs_activations, rhs_activations, post_activations);
+            },
+            py::arg("input_tensor_a"),
+            py::arg("input_tensor_b"),
+            py::kw_only(),
+            py::arg("lhs_activations") = ttnn::SmallVector<unary::UnaryWithParam>(),
+            py::arg("rhs_activations") = ttnn::SmallVector<unary::UnaryWithParam>(),
+            py::arg("post_activations") = ttnn::SmallVector<unary::UnaryWithParam>(),
+            py::arg("queue_id") = 0});
+}
 }  // namespace detail
 
 void py_module(py::module& module) {
@@ -158,5 +205,35 @@ void py_module(py::module& module) {
         module, ttnn::experimental::bitwise_left_shift, "Binary bitwise_left_shift Operation");
     detail::bind_binary_ng_bitwise_ops(
         module, ttnn::experimental::bitwise_right_shift, "Binary bitwise_right_shift Operation");
+
+    detail::bind_inplace_binary_ng_operation(module, ttnn::experimental::add_, "Binary Add In-place Operation");
+    detail::bind_inplace_binary_ng_operation(module, ttnn::experimental::sub_, "Binary Subtract In-place Operation");
+    detail::bind_inplace_binary_ng_operation(module, ttnn::experimental::mul_, "Binary Multiply In-place Operation");
+    detail::bind_inplace_binary_ng_operation(module, ttnn::experimental::div_, "Binary Divide In-place Operation");
+    detail::bind_inplace_binary_ng_operation(module, ttnn::experimental::rsub_, "Binary Rsub In-place Operation");
+    detail::bind_inplace_binary_ng_operation(module, ttnn::experimental::pow_, "Binary Power In-place Operation");
+    detail::bind_inplace_binary_ng_operation(module, ttnn::experimental::gt_, "Binary Greater Than In-place Operation");
+    detail::bind_inplace_binary_ng_operation(module, ttnn::experimental::lt_, "Binary Less Than In-place Operation");
+    detail::bind_inplace_binary_ng_operation(
+        module, ttnn::experimental::lte_, "Binary Less Than or Equal To In-place Operation");
+    detail::bind_inplace_binary_ng_operation(
+        module, ttnn::experimental::gte_, "Binary Greater Than or Equal To In-place Operation");
+    detail::bind_inplace_binary_ng_operation(module, ttnn::experimental::eq_, "Binary Equal In-place Operation");
+    detail::bind_inplace_binary_ng_operation(module, ttnn::experimental::ne_, "Binary Not Equal In-place Operation");
+    detail::bind_inplace_binary_ng_operation(
+        module, ttnn::experimental::squared_difference_, "Binary Squared Difference In-place Operation");
+    detail::bind_inplace_binary_ng_operation(
+        module, ttnn::experimental::bias_gelu_, "Binary Bias GELU In-place Operation");
+    detail::bind_inplace_binary_ng_operation(
+        module, ttnn::experimental::logical_and_, "Binary Logical And In-place Operation");
+    detail::bind_inplace_binary_ng_operation(
+        module, ttnn::experimental::logical_or_, "Binary Logical Or In-place Operation");
+    detail::bind_inplace_binary_ng_operation(
+        module, ttnn::experimental::logical_xor_, "Binary Logical Xor In-place Operation");
+    detail::bind_inplace_binary_ng_operation(module, ttnn::experimental::ldexp_, "Binary Ldexp In-place Operation");
+    detail::bind_inplace_binary_ng_operation(
+        module, ttnn::experimental::logaddexp_, "Binary Logaddexp In-place Operation");
+    detail::bind_inplace_binary_ng_operation(
+        module, ttnn::experimental::logaddexp2_, "Binary Logaddexp2 In-place Operation");
 }
 }  // namespace ttnn::operations::binary_ng
