07ab115ab9e447665102553241f8ca020eb44a30
Bryan Wilder Field Lozano <blozano@tenstorrent.com>
2025-01-26 15:49:11 -0800

    Update core.hpp

--- ttnn/cpp/ttnn/core.hpp
+++ ttnn/cpp/ttnn/core.hpp
@@ -41,7 +41,6 @@ void dump_stack_trace_on_segfault();
 
 using core::get_memory_config;
 using core::has_storage_type_of;
-using core::pad_to_multiple_of_tile_size;
 using core::set_printoptions;
 
 class CoreIDs {
