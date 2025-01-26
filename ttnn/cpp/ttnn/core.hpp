6f73eb566df7a30beada4a201e38532b1827d329
blozano-tt <blozano@tenstorrent.com>
2025-01-26 18:00:26 +0000

    Move definitions to implementation

--- ttnn/cpp/ttnn/core.hpp
+++ ttnn/cpp/ttnn/core.hpp
@@ -4,7 +4,9 @@
 
 #pragma once
 #include <csignal>
+#include <cstdint>
 #include <optional>
+#include <string>
 
 #include <magic_enum/magic_enum.hpp>
 #include "ttnn/tensor/tensor.hpp"
@@ -28,39 +30,17 @@ namespace ttnn {
 
 namespace core {
 
-inline std::uint32_t pad_to_multiple_of_tile_size(std::uint32_t value, std::uint32_t tile_size) {
-    return (value + (tile_size - 1)) / tile_size * tile_size;
-}
+std::uint32_t pad_to_multiple_of_tile_size(std::uint32_t value, std::uint32_t tile_size);
 
-inline bool has_storage_type_of(const ttnn::Tensor& tensor, const ttnn::StorageType& storage_type) {
-    return tensor.storage_type() == storage_type;
-}
+bool has_storage_type_of(const ttnn::Tensor& tensor, const ttnn::StorageType& storage_type);
 
-inline std::optional<ttnn::MemoryConfig> get_memory_config(const ttnn::Tensor& tensor) {
-    if (not tensor.is_allocated() or not is_tensor_on_device_or_multidevice(tensor)) {
-        return std::nullopt;
-    }
-    return tensor.memory_config();
-}
+std::optional<ttnn::MemoryConfig> get_memory_config(const ttnn::Tensor& tensor);
 
-inline void set_printoptions(const std::string& profile) {
-    tt::tt_metal::tensor_impl::TTNN_TENSOR_PRINT_PROFILE =
-        magic_enum::enum_cast<tt::tt_metal::tensor_impl::TensorPrintProfile>(profile, [](char lhs, char rhs) {
-            return std::tolower(lhs) == std::tolower(rhs);
-        }).value();
-}
+void set_printoptions(const std::string& profile);
 
-inline void segfault_handler(int sig) {
-    std::cerr << tt::assert::backtrace_to_string() << std::endl;
-    exit(EXIT_FAILURE);
-}
+void segfault_handler(int sig);
 
-inline void dump_stack_trace_on_segfault() {
-    if (std::signal(SIGSEGV, segfault_handler) == SIG_ERR) {
-        std::cerr << "Error: cannot handle SIGSEGV" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-}
+void dump_stack_trace_on_segfault();
 
 }  // namespace core
 
@@ -71,10 +51,7 @@ using core::set_printoptions;
 
 class CoreIDs {
 public:
-    static CoreIDs& instance() {
-        static CoreIDs instance;
-        return instance;
-    }
+    static CoreIDs& instance();
 
     std::int64_t get_python_operation_id();
     void set_python_operation_id(std::int64_t operation_id);
