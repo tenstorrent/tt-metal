d5ed7d1639706dc713ba8d0b8637778b6780644b
mtairum <mtairum@tenstorrent.com>
2025-01-27 16:48:56 +0000

    #0: Updated ref in llama accuracy

--- models/demos/llama3/tests/test_llama_accuracy.py
+++ models/demos/llama3/tests/test_llama_accuracy.py
@@ -15,7 +15,7 @@ from models.demos.llama3.tt.llama_common import (
 from models.demos.llama3.tt.llama_model import TtTransformer
 from models.demos.llama3.tt.model_config import TtModelArgs, LlamaOptimizations
 from models.demos.t3000.llama2_70b.reference.llama.llama31_8b.tokenizer import Tokenizer
-from models.demos.llama3.demo.demo import preprocess_inputs_prefill
+from models.demos.llama3.demo.simple_text_demo import preprocess_inputs_prefill
 from pathlib import Path
 
 
