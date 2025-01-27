7535430d2e3f1dc2c8bfb696f953c2ef1b6f7462
Saad Jameel <sjameel@tenstorrent.com>
2025-01-27 15:54:19 +0000

    slice changes start

--- ttnn/cpp/ttnn/operations/data_movement/slice/slice.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/slice/slice.cpp
@@ -28,6 +28,18 @@ ttnn::Tensor SliceOperation::invoke(
 
     const auto& input_shape = input_tensor.get_logical_shape();
     uint32_t input_rank = input_shape.rank();
+    auto input_layout = input_tensor.get_layout();
+    if (input_rank < 2) {
+        TT_FATAL(input_layout == Layout::ROW_MAJOR, "Slice is not supported for non-row-major tensors with rank < 2");
+    }
+    TT_FATAL(
+        input_rank == begins.size(), "Input rank {} and begins {} must have the same size", input_rank, begins.size());
+    TT_FATAL(begins.size() == ends.size(), "Start {} and end {} must have the same size", begins.size(), ends.size());
+    TT_FATAL(
+        step.size() == begins.size(),
+        "Step {} must have the same size as start {} and end",
+        step.size(),
+        begins.size());
 
     bool no_step = std::ranges::all_of(step, [](uint32_t s) { return s == 1; });
     bool starts_zero = std::ranges::all_of(begins, [](uint32_t s) { return s == 0; });
@@ -39,59 +51,58 @@ ttnn::Tensor SliceOperation::invoke(
         }
     }
 
-    if (no_step && starts_zero && ends_max) {
+    const auto& tile_shape = input_tensor.get_tensor_spec().tile().get_tile_shape();
+
+    auto memory_config = optional_output_tensor.has_value() ? optional_output_tensor.value().memory_config()
+                                                            : memory_config_arg.value_or(input_tensor.memory_config());
+
+    auto ret_adjustment([&](const ttnn::Tensor& input_tensor) {
         if (input_tensor.storage_type() == StorageType::DEVICE) {
-            auto memory_config = optional_output_tensor.has_value()
-                                     ? optional_output_tensor.value().memory_config()
-                                     : memory_config_arg.value_or(input_tensor.memory_config());
-            return ttnn::to_memory_config(input_tensor, memory_config, std::nullopt);
+            auto tensor = ttnn::to_memory_config(input_tensor, memory_config, std::nullopt);
+            tensor = ttnn::to_layout(tensor, input_layout, std::nullopt, std::nullopt, (IDevice*)nullptr);
+            return tensor;
         }
         return input_tensor;
+    });
+
+    // No-op check
+    if (no_step && starts_zero && ends_max) {
+        return ret_adjustment(input_tensor);
     }
-
-    TT_FATAL(
-        input_rank == begins.size(), "Input rank {} and begins {} must have the same size", input_rank, begins.size());
-    TT_FATAL(begins.size() == ends.size(), "Start {} and end {} must have the same size", begins.size(), ends.size());
-    TT_FATAL(
-        step.size() == begins.size(),
-        "Step {} must have the same size as start {} and end",
-        step.size(),
-        begins.size());
-
-    bool rm_only = !no_step && input_tensor.get_layout() == Layout::TILE;
-    Tensor input = input_tensor;
-    if (rm_only) {
-        TT_FATAL(input.get_dtype() == DataType::BFLOAT16, "Strided slice is not supported for BFLOAT8 tensors");
-        input = ttnn::to_layout(input, Layout::ROW_MAJOR, std::nullopt, std::nullopt, (IDevice*)nullptr);
-    }
-
-    // Unsqueeze tensor to 4D if necessary
-    if (input_rank < 4) {
-        input = ttnn::unsqueeze_to_4D(input);
-    }
-
-    auto padded_shape = input.get_padded_shape();
-    size_t adjusted_rank = padded_shape.rank();  // Now adjusted to 4 after unsqueeze
 
     // Create modified vectors with wrapped indices and adjust them to match the tensor's rank
-    ttnn::SmallVector<uint32_t> modified_begins(adjusted_rank, 0);
-    ttnn::SmallVector<uint32_t> modified_ends(padded_shape.cbegin(), padded_shape.cend());
-    ttnn::SmallVector<uint32_t> modified_step(adjusted_rank, 1);
-
-    size_t rank_diff = adjusted_rank - input_rank;
+    ttnn::SmallVector<uint32_t> modified_begins(input_rank, 0);
+    ttnn::SmallVector<uint32_t> modified_ends(input_rank, 0);
+    ttnn::SmallVector<uint32_t> modified_step(input_rank, 1);
 
     // Wrap indices and adjust begins, ends, and step
     for (size_t i = 0; i < begins.size(); ++i) {
-        size_t idx = i + rank_diff;
-
         if constexpr (std::is_signed_v<T>) {
-            modified_begins[idx] = wrap_index(begins[i], input_shape[i]);
-            modified_ends[idx] = wrap_index(ends[i], input_shape[i]);
-            modified_step[idx] = static_cast<uint32_t>(step[i]);
+            modified_begins[i] = wrap_index(begins[i], input_shape[i]);
+            modified_ends[i] = wrap_index(ends[i], input_shape[i]);
+            modified_step[i] = static_cast<uint32_t>(step[i]);
         } else {
-            modified_begins[idx] = begins[i];
-            modified_ends[idx] = ends[i];
-            modified_step[idx] = step[i];
+            modified_begins[i] = begins[i];
+            modified_ends[i] = ends[i];
+            modified_step[i] = step[i];
+        }
+    }
+
+    bool unaligned_begins = false;
+    bool unaligned_ends = false;
+    bool rm_only = false;
+
+    Tensor input = input_tensor;
+    if (input_tensor.get_layout() == Layout::TILE) {
+        unaligned_begins |= (modified_begins[input_rank - 2] % tile_shape[0] != 0) ||
+                            (modified_begins[input_rank - 1] % tile_shape[1] != 0);
+        // TODO: if only the ends are unaligned, we can use fill_pad to pad the tensor
+        unaligned_ends |= (modified_ends[input_rank - 2] % tile_shape[0] != 0) ||
+                          (modified_ends[input_rank - 1] % tile_shape[1] != 0);
+        rm_only = !no_step || unaligned_begins || unaligned_ends;
+        if (rm_only) {
+            TT_FATAL(input.get_dtype() == DataType::BFLOAT16, "Strided slice is not supported for BFLOAT8 tensors");
+            input = ttnn::to_layout(input, Layout::ROW_MAJOR, std::nullopt, memory_config, (IDevice*)nullptr);
         }
     }
 
@@ -114,13 +125,14 @@ ttnn::Tensor SliceOperation::invoke(
 
     // Compute actual and padded shapes for the original input rank
     for (size_t i = 0; i < input_rank; ++i) {
-        size_t idx = i + rank_diff;
         TT_FATAL(
-            modified_ends[idx] >= modified_begins[idx],
-            "End {} must be greater than or equal to start {}",
-            modified_ends[idx],
-            modified_begins[idx]);
-        auto val = output_dim_i(idx, modified_ends);
+            modified_begins[i] <= modified_ends[i],
+            "Invalid slice operation: begin[{}] must be less than or equal to end[{}], but got {} > {}",
+            i,
+            i,
+            modified_begins[i],
+            modified_ends[i]);
+        auto val = output_dim_i(i, modified_ends);
         if (val == 0) {
             empty = true;
         }
@@ -132,8 +144,7 @@ ttnn::Tensor SliceOperation::invoke(
 
     if (empty) {
         TT_FATAL(
-            input_tensor.storage_type() == StorageType::DEVICE,
-            "Host tensor slice cannot return a scalar or empty tensor");
+            input.storage_type() == StorageType::DEVICE, "Host tensor slice cannot return a scalar or empty tensor");
         return ttnn::empty(
             actual_shape,
             input_tensor.dtype(),
@@ -210,7 +221,7 @@ ttnn::Tensor SliceOperation::invoke(
     tt::stl::Span<const T> step,
     const std::optional<MemoryConfig>& memory_config_arg,
     const std::optional<Tensor>& optional_output_tensor) {
-    return SliceOperation::invoke<T>(ttnn::DefaultQueueId, input_tensor, begins, ends, step, memory_config_arg);
+    return SliceOperation::invoke<T>(ttnn::DefaultQueueId, input_tensor, begins, ends, step, memory_config_arg, optional_output_tensor);
 }
 
 // Specialization for uint32_t and N=4
@@ -344,7 +355,8 @@ ttnn::Tensor SliceOperation::invoke(
     tt::stl::Span<const T> start(output_tensor_start.begin(), output_tensor_start.end());
     tt::stl::Span<const T> end(output_tensor_end.begin(), output_tensor_end.end());
     tt::stl::Span<const T> step_vec(step.begin(), step.end());
-    return SliceOperation::invoke<T>(queue_id, input_tensor, start, end, step_vec, memory_config_arg);
+    return SliceOperation::invoke<T>(
+        queue_id, input_tensor, start, end, step_vec, memory_config_arg, optional_output_tensor);
 }
 
 template <typename T, std::size_t N>
@@ -356,7 +368,13 @@ ttnn::Tensor SliceOperation::invoke(
     const std::optional<MemoryConfig>& memory_config_arg,
     const std::optional<Tensor>& optional_output_tensor) {
     return SliceOperation::invoke<T, N>(
-        ttnn::DefaultQueueId, input_tensor, output_tensor_start, output_tensor_end, step, memory_config_arg);
+        ttnn::DefaultQueueId,
+        input_tensor,
+        output_tensor_start,
+        output_tensor_end,
+        step,
+        memory_config_arg,
+        optional_output_tensor);
 }
 
 template ttnn::Tensor SliceOperation::invoke<int>(
@@ -394,10 +412,36 @@ template ttnn::Tensor SliceOperation::invoke<uint32_t>(
     const std::optional<Tensor>& optional_output_tensor);
 
 template ttnn::Tensor SliceOperation::invoke<uint32_t, 4>(
+    uint8_t queue_id,
     const ttnn::Tensor& input_tensor,
     const std::array<uint32_t, 4>& output_tensor_start,
     const std::array<uint32_t, 4>& output_tensor_end,
     const std::array<uint32_t, 4>& step,
+    const std::optional<MemoryConfig>& memory_config_arg,
+    const std::optional<Tensor>& optional_output_tensor);
+
+template ttnn::Tensor SliceOperation::invoke<uint32_t, 4>(
+    const ttnn::Tensor& input_tensor,
+    const std::array<uint32_t, 4>& output_tensor_start,
+    const std::array<uint32_t, 4>& output_tensor_end,
+    const std::array<uint32_t, 4>& step,
+    const std::optional<MemoryConfig>& memory_config_arg,
+    const std::optional<Tensor>& optional_output_tensor);
+
+template ttnn::Tensor SliceOperation::invoke<uint32_t, 3>(
+    uint8_t queue_id,
+    const ttnn::Tensor& input_tensor,
+    const std::array<uint32_t, 3>& output_tensor_start,
+    const std::array<uint32_t, 3>& output_tensor_end,
+    const std::array<uint32_t, 3>& step,
+    const std::optional<MemoryConfig>& memory_config_arg,
+    const std::optional<Tensor>& optional_output_tensor);
+
+template ttnn::Tensor SliceOperation::invoke<uint32_t, 3>(
+    const ttnn::Tensor& input_tensor,
+    const std::array<uint32_t, 3>& output_tensor_start,
+    const std::array<uint32_t, 3>& output_tensor_end,
+    const std::array<uint32_t, 3>& step,
     const std::optional<MemoryConfig>& memory_config_arg,
     const std::optional<Tensor>& optional_output_tensor);
 
