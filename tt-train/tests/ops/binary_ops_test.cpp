0a1983ee02a5cc8b28a99b7c1a4a9a3810f6944e
Jay Kruer <jkruer@tenstorrent.com>
2025-01-27 21:43:07 +0000

    #0: Add tests for eltwise tensor mul and div by float

--- tt-train/tests/ops/binary_ops_test.cpp
+++ tt-train/tests/ops/binary_ops_test.cpp
@@ -26,8 +26,8 @@ protected:
 
 TEST_F(BinaryOpsTest, TensorAdd_Broadcasted) {
     xt::xarray<float> a = {{1.F, 2.F, 3.F, 4.F, 1.F, 2.F, 3.F, 4.F}};
-    xt::xarray<float> b = xt::xarray<float>::from_shape({1, 1});
-    b(0, 0) = 1.F;
+    xt::xarray<float> b = xt::xarray<float>::from_shape({1, 1, 1, 1});
+    b(0, 0, 0, 0) = 1.F;
 
     auto a_tensor = ttml::autograd::create_tensor(ttml::core::from_xtensor(a, &ttml::autograd::ctx().get_device()));
     auto b_tensor = ttml::autograd::create_tensor(ttml::core::from_xtensor(b, &ttml::autograd::ctx().get_device()));
@@ -36,6 +36,34 @@ TEST_F(BinaryOpsTest, TensorAdd_Broadcasted) {
     auto result_xarray = ttml::core::to_xtensor(result->get_value());
 
     auto expected_result = xt::xarray<float>{2.F, 3.F, 4.F, 5.F, 2.F, 3.F, 4.F, 5.F};
+
+    EXPECT_TRUE(xt::allclose(result_xarray, expected_result));
+}
+
+TEST_F(BinaryOpsTest, TensorMul_Eltwise) {
+    xt::xarray<float> a = {{1.F, 2.F, 3.F, 4.F, 1.F, 2.F, 3.F, 4.F}};
+    xt::xarray<float> b = {{1.F, 2.F, 3.F, 4.F, 1.F, 2.F, 3.F, 4.F}};
+
+    auto a_tensor = ttml::autograd::create_tensor(ttml::core::from_xtensor(a, &ttml::autograd::ctx().get_device()));
+    auto b_tensor = ttml::autograd::create_tensor(ttml::core::from_xtensor(b, &ttml::autograd::ctx().get_device()));
+
+    auto result = ttml::ops::mul(a_tensor, b_tensor);
+    auto result_xarray = ttml::core::to_xtensor(result->get_value());
+
+    auto expected_result = xt::xarray<float>{{1.F, 4.F, 9.F, 16.F, 1.F, 4.F, 9.F, 16.F}};
+
+    EXPECT_TRUE(xt::allclose(result_xarray, expected_result));
+}
+
+TEST_F(BinaryOpsTest, TensorDivByFloat) {
+    xt::xarray<float> a = {{1.F, 2.F, 3.F, 4.F, 1.F, 2.F, 3.F, 4.F}};
+
+    auto a_tensor = ttml::autograd::create_tensor(ttml::core::from_xtensor(a, &ttml::autograd::ctx().get_device()));
+    float b = 2.F;
+    auto result = ttml::ops::div(a_tensor, b);
+    auto result_xarray = ttml::core::to_xtensor(result->get_value());
+
+    auto expected_result = xt::xarray<float>{{0.5F, 1.F, 1.5F, 2.F, 0.5F, 1.F, 1.5F, 2.F}};
 
     EXPECT_TRUE(xt::allclose(result_xarray, expected_result));
 }
