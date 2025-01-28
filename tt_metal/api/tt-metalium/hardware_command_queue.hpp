7b935645198bae8708cb1a9703df71467389fa75
Artem Yerofieiev <artem@tenstorrent.com>
2025-01-28 06:42:17 +0000

    remove friends from HWCQ

--- tt_metal/api/tt-metalium/hardware_command_queue.hpp
+++ tt_metal/api/tt-metalium/hardware_command_queue.hpp
@@ -104,6 +104,42 @@ public:
     void set_expected_num_workers_completed_for_sub_device(uint32_t sub_device_index, uint32_t num_workers);
     WorkerConfigBufferMgr& get_config_buffer_mgr(uint32_t index);
 
+    void enqueue_trace(const uint32_t trace_id, bool blocking);
+    void enqueue_program(Program& program, bool blocking);
+    void enqueue_read_buffer(
+        std::shared_ptr<Buffer>& buffer,
+        void* dst,
+        const BufferRegion& region,
+        bool blocking,
+        tt::stl::Span<const SubDeviceId> sub_device_ids = {});
+    void enqueue_read_buffer(
+        Buffer& buffer,
+        void* dst,
+        const BufferRegion& region,
+        bool blocking,
+        tt::stl::Span<const SubDeviceId> sub_device_ids = {});
+
+    void enqueue_record_event(
+        const std::shared_ptr<Event>& event,
+        bool clear_count = false,
+        tt::stl::Span<const SubDeviceId> sub_device_ids = {});
+    void enqueue_wait_for_event(const std::shared_ptr<Event>& sync_event, bool clear_count = false);
+
+    void enqueue_write_buffer(
+        const std::variant<std::reference_wrapper<Buffer>, std::shared_ptr<Buffer>>& buffer,
+        HostDataType src,
+        const BufferRegion& region,
+        bool blocking,
+        tt::stl::Span<const SubDeviceId> sub_device_ids = {});
+    void enqueue_write_buffer(
+        Buffer& buffer,
+        const void* src,
+        const BufferRegion& region,
+        bool blocking,
+        tt::stl::Span<const SubDeviceId> sub_device_ids = {});
+
+    void finish(tt::stl::Span<const SubDeviceId> sub_device_ids);
+
 private:
     uint32_t id;
     uint32_t size_B;
@@ -153,62 +189,8 @@ private:
     template <typename T>
     void enqueue_command(T& command, bool blocking, tt::stl::Span<const SubDeviceId> sub_device_ids);
 
-    void enqueue_read_buffer(
-        std::shared_ptr<Buffer>& buffer,
-        void* dst,
-        const BufferRegion& region,
-        bool blocking,
-        tt::stl::Span<const SubDeviceId> sub_device_ids = {});
-    void enqueue_read_buffer(
-        Buffer& buffer,
-        void* dst,
-        const BufferRegion& region,
-        bool blocking,
-        tt::stl::Span<const SubDeviceId> sub_device_ids = {});
-    void enqueue_write_buffer(
-        const std::variant<std::reference_wrapper<Buffer>, std::shared_ptr<Buffer>>& buffer,
-        HostDataType src,
-        const BufferRegion& region,
-        bool blocking,
-        tt::stl::Span<const SubDeviceId> sub_device_ids = {});
-    void enqueue_write_buffer(
-        Buffer& buffer,
-        const void* src,
-        const BufferRegion& region,
-        bool blocking,
-        tt::stl::Span<const SubDeviceId> sub_device_ids = {});
-    void enqueue_program(Program& program, bool blocking);
-    void enqueue_record_event(
-        const std::shared_ptr<Event>& event,
-        bool clear_count = false,
-        tt::stl::Span<const SubDeviceId> sub_device_ids = {});
-    void enqueue_wait_for_event(const std::shared_ptr<Event>& sync_event, bool clear_count = false);
-    void enqueue_trace(const uint32_t trace_id, bool blocking);
-    void finish(tt::stl::Span<const SubDeviceId> sub_device_ids);
     void increment_num_entries_in_completion_q();
     void set_exit_condition();
-
-    friend void EnqueueTraceImpl(CommandQueue& cq, uint32_t trace_id, bool blocking);
-    friend void EnqueueProgramImpl(CommandQueue& cq, Program& program, bool blocking);
-    friend void EnqueueReadBufferImpl(
-        CommandQueue& cq,
-        std::variant<std::reference_wrapper<Buffer>, std::shared_ptr<Buffer>> buffer,
-        void* dst,
-        const BufferRegion& region,
-        bool blocking);
-    friend void EnqueueWriteBufferImpl(
-        CommandQueue& cq,
-        const std::variant<std::reference_wrapper<Buffer>, std::shared_ptr<Buffer>>& buffer,
-        HostDataType src,
-        const BufferRegion& region,
-        bool blocking);
-    friend void EnqueueGetBufferAddrImpl(void* dst_buf_addr, const Buffer* buffer);
-    friend void EnqueueRecordEventImpl(
-        CommandQueue& cq, const std::shared_ptr<Event>& event, tt::stl::Span<const SubDeviceId> sub_device_ids);
-    friend void EnqueueWaitForEventImpl(CommandQueue& cq, const std::shared_ptr<Event>& event);
-    friend void FinishImpl(CommandQueue& cq, tt::stl::Span<const SubDeviceId> sub_device_ids);
-    friend CommandQueue;
-    friend detail::Program_;
 };
 
 }  // namespace tt::tt_metal
