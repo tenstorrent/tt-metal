20663019139b18c93f39327faf72fe6ea5845af0
Guangyu Feng <gfeng@tenstorrent.com>
2025-01-27 22:05:03 -0500

    Improve tt-smi util in sweep framework
    tt-smi reset now work better with both old and new (>=3.0) version.

--- tests/sweep_framework/framework/tt_smi_util.py
+++ tests/sweep_framework/framework/tt_smi_util.py
@@ -8,7 +8,8 @@ import subprocess
 from tests.sweep_framework.framework.sweeps_logger import sweeps_logger as logger
 
 GRAYSKULL_ARGS = ["-tr", "all"]
-WORMHOLE_ARGS = ["-wr", "all"]
+LEGACY_WORMHOLE_ARGS = ["-wr", "all"]
+WORMHOLE_ARGS = ["-r"]
 
 RESET_OVERRIDE = os.getenv("TT_SMI_RESET_COMMAND")
 
@@ -48,9 +49,27 @@ def run_tt_smi(arch: str):
             # Corner case for newer version of tt-smi, -tr and -wr are removed on this version (tt-smi-metal). Default device 0, if needed use TT_SMI_RESET_COMMAND override.
             if smi_option == "tt-smi-metal":
                 args = ["-r", "0"]
+            elif arch == "grayskull":
+                args = GRAYSKULL_ARGS
+            elif arch == "wormhole_b0":
+                smi_process = subprocess.run([executable, "-v"], capture_output=True, text=True)
+                smi_version = smi_process.stdout.strip()
+                if not smi_version.startswith("3.0"):
+                    args = LEGACY_WORMHOLE_ARGS
+                else:
+                    args = WORMHOLE_ARGS
+                    smi_process = subprocess.run([executable, "-g", ".reset.json"])
+                    import json
+                    with open(".reset.json", "r") as f:
+                        reset_json = json.load(f)
+                        card_id = reset_json["wh_link_reset"]["pci_index"]
+                        if len(card_id) < 1:
+                            raise Exception(f"SWEEPS: TT-SMI Reset Failed to Find Card ID.")
+                        args.append(str(card_id[0]))
+                    subprocess.run(["rm", "-f", ".reset.json"])
             smi_process = subprocess.run([executable, *args])
             if smi_process.returncode == 0:
-                logger.info("TT-SMI Reset Complete Successfully")
+                logger.info("SWEEPS: TT-SMI Reset Complete Successfully")
                 return
             else:
                 raise Exception(f"SWEEPS: TT-SMI Reset Failed with Exit Code: {smi_process.returncode}")
