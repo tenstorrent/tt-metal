92bc2bfd3c8b1a7958fb7b2d1244a67d55d5491d
Ligang Long <llong@tenstorrent.com>
2025-01-30 22:18:30 +0000

    fix a compile issue

--- ttnn/cpp/ttnn/operations/data_movement/concat/device/concat_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/concat/device/concat_program_factory.cpp
@@ -527,8 +527,8 @@ tt_metal::operation::ProgramWithCallbacks concat_multi_core(
     uint32_t single_page_size;
     uint32_t common_align_len = std::max(input_tensors[0].buffer()->alignment(), output.buffer()->alignment());
     if (rm_layout) {
-        num_output_pages = output.volume() / output.get_legacy_shape()[-1];
-        single_page_size = tt::align(output.element_size() * output.get_legacy_shape()[-1], common_align_len);
+        num_output_pages = output.volume() / output.get_padded_shape()[-1];
+        single_page_size = tt::align(output.element_size() * output.get_padded_shape()[-1], common_align_len);
     } else {
         num_output_pages = output.volume() / TILE_HW;
         single_page_size = tt_metal::detail::TileSize(cb_data_format);
