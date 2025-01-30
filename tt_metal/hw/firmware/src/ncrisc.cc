c4b4b431e0b4a27d502aba7ad2fe76763ba03b86
John Bauman <jbauman@tenstorrent.com>
2025-01-30 21:04:44 +0000

    #17307: Move ncrisc copy into ncrisc firmware
    By moving the NCRISC kernel copy into the NCRISC firmware (on wormhole) we can do it in parallel with setting up CBs.

--- tt_metal/hw/firmware/src/ncrisc.cc
+++ tt_metal/hw/firmware/src/ncrisc.cc
@@ -14,6 +14,7 @@
 #include "risc_attribs.h"
 #include "circular_buffer.h"
 #include "circular_buffer_init.h"
+#include "tdma_xmov.h"
 
 #include "debug/waypoint.h"
 #include "debug/dprint.h"
@@ -72,7 +73,11 @@ inline __attribute__((always_inline)) void notify_brisc_and_wait() {
 #if NCRISC_FIRMWARE_IN_IRAM
     notify_brisc_and_halt(RUN_SYNC_MSG_DONE);
 #else
-    while (*ncrisc_run != RUN_SYNC_MSG_GO) {
+    while (true) {
+        uint8_t run_value = *ncrisc_run;
+        if (run_value == RUN_SYNC_MSG_GO || run_value == RUN_SYNC_MSG_LOAD) {
+            break;
+        }
         invalidate_l1_cache();
     }
 #endif
@@ -83,6 +88,20 @@ inline __attribute__((always_inline)) void signal_ncrisc_completion() {
     *ncrisc_run = RUN_SYNC_MSG_DONE;
 #endif
 }
+
+#if defined(ARCH_WORMHOLE)
+#define MEM_MOVER_VIEW_IRAM_BASE_ADDR (0x4 << 12)
+void l1_to_ncrisc_iram_copy(uint32_t src_addr, uint16_t size, uint32_t address_offset = 0) {
+    // Always copy ncrisc even if its size is 0 (save branch)...
+    // Copy NCRISC firmware from L1 to local IRAM using tensix DMA
+    tdma_xmov(TDMA_MOVER0, src_addr, MEM_MOVER_VIEW_IRAM_BASE_ADDR + address_offset, size, XMOV_L1_TO_L0);
+}
+
+void l1_to_ncrisc_iram_copy_wait() {
+    // Wait for DMA to finish
+    wait_tdma_movers_done(RISCV_TDMA_STATUS_FLAG_MOVER0_BUSY_MASK);
+}
+#endif
 
 int main(int argc, char *argv[]) {
     configure_l1_data_cache();
@@ -109,25 +128,37 @@ int main(int argc, char *argv[]) {
         launch_msg_t* launch_msg = &(mailboxes->launch[launch_msg_rd_ptr]);
 
         uint32_t kernel_config_base = firmware_config_init(mailboxes, ProgrammableCoreType::TENSIX, DISPATCH_CLASS_TENSIX_DM1);
+        int index = static_cast<std::underlying_type<TensixProcessorTypes>::type>(TensixProcessorTypes::DM1);
+
+#if defined(ARCH_WORMHOLE)
+        uint32_t ncrisc_kernel_src_address = kernel_config_base + launch_msg->kernel_config.kernel_text_offset[index];
+        l1_to_ncrisc_iram_copy(ncrisc_kernel_src_address >> 4, launch_msg->kernel_config.ncrisc_kernel_size16, 0);
+#endif
         uint32_t tt_l1_ptr* cb_l1_base =
             (uint32_t tt_l1_ptr*)(kernel_config_base + launch_msg->kernel_config.local_cb_offset);
         uint32_t end_cb_index = launch_msg->kernel_config.max_local_cb_end_index;
         setup_local_cb_read_write_interfaces(cb_l1_base, 0, end_cb_index, true, true, false);
+
+#if defined(ARCH_WORMHOLE)
+        l1_to_ncrisc_iram_copy_wait();
+#endif
 
         cb_l1_base = (uint32_t tt_l1_ptr*)(kernel_config_base + launch_msg->kernel_config.remote_cb_offset);
         end_cb_index = launch_msg->kernel_config.min_remote_cb_start_index;
         experimental::setup_remote_cb_interfaces(cb_l1_base, end_cb_index);
         WAYPOINT("R");
 
-        int index = static_cast<std::underlying_type<TensixProcessorTypes>::type>(TensixProcessorTypes::DM1);
         void (*kernel_address)(uint32_t) = (void (*)(uint32_t))
             (kernel_config_base + launch_msg->kernel_config.kernel_text_offset[index]);
 #ifdef ARCH_BLACKHOLE
+        while (*ncrisc_run != RUN_SYNC_MSG_GO) {
+            invalidate_l1_cache();
+        }
         (*kernel_address)((uint32_t)kernel_address);
 #elif defined(ARCH_WORMHOLE)
         // Jumping to IRAM causes bizarre behavior, so signal the brisc to reset the ncrisc to the IRAM address.
         mailboxes->ncrisc_halt.resume_addr = (uint32_t)kernel_init;
-        notify_brisc_and_halt_to_iram(RUN_SYNC_MSG_DONE, (uint32_t)kernel_address);
+        notify_brisc_and_halt_to_iram(RUN_SYNC_MSG_WAITING_FOR_RESET, (uint32_t)kernel_address);
 #else
         kernel_init((uint32_t)kernel_address);
 #endif
