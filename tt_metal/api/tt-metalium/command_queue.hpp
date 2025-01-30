tenstorrent/tt-metal

commit 779a2969b310018fc57677ca34e883b4c365c69a

Remove CommandQueue redirecting usages straight to HWCQ (#17219)
### Ticket
https://github.com/tenstorrent/tt-metal/issues/17208

### Problem description
Over time `CommandQueue` stopped playing any particular function.
Today it is simply proxying calls to HWCQ via creating temporary command
structures and immediately routing their execution with a big switch
statement, calling into hwcq methods.
This adds unreasonable complexity. 

After the sync with @tt-asaigal @dmakoviichuk-tt @cfjchu @omilyutin-tt ,
we agreed to remove this class.

### What's changed
Remove CommandQueue class.
Redirect all usage to HWCQ.
HWCommandQueue is renamed to CommandQueue

Note:
* command_queue.hpp was not removed. It contains definitions of commands
used by hardware_command_queue, I would prefer us to clean this up in
the next PR.

### Checklist
- [x] [Post commit
CI](https://github.com/tenstorrent/tt-metal/actions/runs/13023052350)
- [x] [Blackhole Post
commit](https://github.com/tenstorrent/tt-metal/actions/runs/13023279261)
- [x] [T3k
Frequent](https://github.com/tenstorrent/tt-metal/actions/runs/13021799389)
-

tt_metal/api/tt-metalium/command_queue.hpp
@@ -22,19 +22,17 @@
 #include "worker_config_buffer.hpp"
 #include "program_impl.hpp"
 #include "trace_buffer.hpp"
+#include "hardware_command_queue.hpp"
 
 namespace tt::tt_metal {
 inline namespace v0 {
 
-class CommandQueue;
 class BufferRegion;
 class Event;
 class Trace;
 using RuntimeArgs = std::vector<std::variant<Buffer*, uint32_t>>;
 
 }  // namespace v0
-
-class HWCommandQueue;
 
 // Only contains the types of commands which are enqueued onto the device
 enum class EnqueueCommandType {
@@ -54,10 +52,6 @@ enum class EnqueueCommandType {
 };
 
 string EnqueueCommandTypeToString(EnqueueCommandType ctype);
-
-class CommandInterface;
-
-using WorkerQueue = LockFreeQueue<CommandInterface>;
 
 class Command {
    public:
@@ -205,137 +199,19 @@ class EnqueueTerminateCommand : public Command {
     constexpr bool has_side_effects() { return false; }
 };
 
-struct RuntimeArgsMetadata {
-    CoreCoord core_coord;
-    std::shared_ptr<RuntimeArgs> runtime_args_ptr;
-    std::shared_ptr<Kernel> kernel;
-    std::vector<uint32_t> update_idx;
-};
-
-// Common interface for all command queue types
-struct CommandInterface {
-    EnqueueCommandType type;
-    std::optional<bool> blocking;
-    std::optional<std::variant<std::reference_wrapper<Buffer>, std::shared_ptr<Buffer>>> buffer;
-    Program* program;
-    std::optional<RuntimeArgsMetadata> runtime_args_md;
-    std::optional<const Buffer*> shadow_buffer;
-    std::optional<HostDataType> src;
-    std::optional<void*> dst;
-    std::optional<std::shared_ptr<Event>> event;
-    std::optional<uint32_t> trace_id;
-    std::optional<BufferRegion> region;
-    tt::stl::Span<const SubDeviceId> sub_device_ids;
-};
-
-inline namespace v0 {
-
-class CommandQueue {
-    friend class Trace;
-
-public:
-    enum class CommandQueueMode {
-        PASSTHROUGH = 0,
-        ASYNC = 1,
-        TRACE = 2,
-    };
-    enum class CommandQueueState {
-        IDLE = 0,
-        RUNNING = 1,
-        TERMINATE = 2,
-    };
-
-    CommandQueue() = delete;
-
-    CommandQueue(IDevice* device, uint32_t id, CommandQueueMode mode = CommandQueue::default_mode());
-    ~CommandQueue();
-
-    // Trace queue constructor
-    CommandQueue(Trace& trace);
-
-    // Getters for private members
-    IDevice* device() const { return this->device_ptr; }
-    uint32_t id() const { return this->cq_id; }
-
-    // Blocking method to wait for all commands to drain from the queue
-    // Optional if used in passthrough mode (async_mode = false)
-    void wait_until_empty();
-
-    // Schedule a command to be run on the device
-    // Blocking if in passthrough mode. Non-blocking if in async mode
-    void run_command(CommandInterface&& command);
-
-    // API for setting/getting the mode of the command queue
-    // TODO: disallow changing the mode of the queue. This is error prone, because changing mode requires
-    // accordingly updating the higher-level abstractions.
-    void set_mode(const CommandQueueMode& mode);
-    CommandQueueMode get_mode() const { return this->mode; }
-
-    // Reference to the underlying hardware command queue, non-const because side-effects are allowed
-    HWCommandQueue& hw_command_queue();
-
-    // The empty state of the worker queue
-    bool empty() const { return this->worker_queue.empty(); }
-
-    // Dump methods for name and pending commands in the queue
-    void dump();
-    std::string name();
-
-    static CommandQueueMode default_mode() {
-        // Envvar is used for bringup and debug only. Will be removed in the future and should not be relied on in
-        // production.
-        static int value =
-            parse_env<int>("TT_METAL_CQ_ASYNC_MODE", /*default_value=*/static_cast<int>(CommandQueueMode::PASSTHROUGH));
-        return static_cast<CommandQueue::CommandQueueMode>(value);
-    }
-    // Determine if any CQ is using Async mode
-    static bool async_mode_set() { return num_async_cqs > 0; }
-
-   private:
-    // Initialize Command Queue Mode based on the env-var. This will be default, unless the user excplictly sets the
-    // mode using set_mode.
-    CommandQueueMode mode;
-    CommandQueueState worker_state;
-    std::unique_ptr<std::thread> worker_thread;
-    WorkerQueue worker_queue;
-    uint32_t cq_id = 0;
-    IDevice* device_ptr = nullptr;
-    Trace* trace_ptr = nullptr;
-
-    void start_worker();
-    void stop_worker();
-    void run_worker();
-    void run_command_impl(const CommandInterface& command);
-
-    bool async_mode() { return this->mode == CommandQueueMode::ASYNC; }
-    bool trace_mode() { return this->mode == CommandQueueMode::TRACE; }
-    bool passthrough_mode() { return this->mode == CommandQueueMode::PASSTHROUGH; }
-
-    std::atomic<std::size_t> worker_thread_id = -1;
-    std::atomic<std::size_t> parent_thread_id = -1;
-    // Track the number of CQs using async vs pt mode
-    inline static uint32_t num_async_cqs = 0;
-    inline static uint32_t num_passthrough_cqs = 0;
-};
-
-}  // namespace v0
-
 // Primitives used to place host only operations on the SW Command Queue.
 // These are used in functions exposed through tt_metal.hpp or host_api.hpp
-void EnqueueGetBufferAddr(CommandQueue& cq, uint32_t* dst_buf_addr, const Buffer* buffer, bool blocking);
+void EnqueueGetBufferAddr(uint32_t* dst_buf_addr, const Buffer* buffer, bool blocking);
 void EnqueueSetRuntimeArgs(
-    CommandQueue& cq,
     const std::shared_ptr<Kernel>& kernel,
     const CoreCoord& core_coord,
     std::shared_ptr<RuntimeArgs> runtime_args_ptr,
     bool blocking);
 void EnqueueAddBufferToProgram(
-    CommandQueue& cq,
     const std::variant<std::reference_wrapper<Buffer>, std::shared_ptr<Buffer>>& buffer,
     Program& program,
     bool blocking);
 
 }  // namespace tt::tt_metal
 
-std::ostream& operator<<(std::ostream& os, tt::tt_metal::EnqueueCommandType const& type);
-std::ostream& operator<<(std::ostream& os, tt::tt_metal::CommandQueue::CommandQueueMode const& type);
+std::ostream& operator<<(std::ostream& os, const tt::tt_metal::EnqueueCommandType& type);

tests/tt_metal/tt_metal/api/CMakeLists.txt
@@ -17,7 +17,6 @@ set(UNIT_TESTS_API_SRC
     ${CMAKE_CURRENT_SOURCE_DIR}/test_banked.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/test_bit_utils.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/test_buffer_region.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/test_CommandQueue.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/test_direct.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/test_dram_to_l1_multicast.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/test_dram.cpp

tests/tt_metal/tt_metal/api/test_CommandQueue.cpp
@@ -1,152 +1,0 @@
-// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
-//
-// SPDX-License-Identifier: Apache-2.0
-
-#include "gtest/gtest.h"
-
-#include "command_queue_fixture.hpp"
-#include <tt-metalium/host_api.hpp>
-#include "tt_metal/common/scoped_timer.hpp"
-#include <tt-metalium/device.hpp>
-#include <tt-metalium/circular_buffer.hpp>
-#include "tt_metal/test_utils/stimulus.hpp"
-
-using namespace tt::tt_metal;
-
-namespace host_tests {
-
-TEST_F(CommandQueueMultiDeviceFixture, DISABLED_TestAccessCommandQueue) {
-    for (unsigned int device_id = 0; device_id < num_devices_; device_id++) {
-        EXPECT_NO_THROW(devices_[device_id]->command_queue());
-    }
-}
-
-TEST_F(CommandQueueFixture, TestCannotAccessCommandQueueForClosedDevice) {
-    EXPECT_NO_THROW(device_->command_queue());
-    CloseDevice(device_);
-    EXPECT_ANY_THROW(device_->command_queue());
-}
-
-TEST_F(CommandQueueFixture, DISABLED_TensixTestAsyncAssertForDeprecatedAPI) {
-    auto& command_queue = this->device_->command_queue();
-    auto current_mode = CommandQueue::default_mode();
-    command_queue.set_mode(CommandQueue::CommandQueueMode::ASYNC);
-    Program program;
-    CoreCoord core = {0, 0};
-    uint32_t buf_size = 4096;
-    uint32_t page_size = 4096;
-    auto dummy_kernel = CreateKernel(
-        program,
-        "tt_metal/kernels/dataflow/reader_binary_diff_lengths.cpp",
-        core,
-        DataMovementConfig{.processor = DataMovementProcessor::RISCV_0, .noc = NOC::RISCV_0_default});
-    auto src0 = Buffer::create(this->device_, buf_size, page_size, BufferType::DRAM);
-    std::vector<uint32_t> runtime_args = {src0->address()};
-    try {
-        SetRuntimeArgs(program, dummy_kernel, core, runtime_args);
-    } catch (std::runtime_error& e) {
-        std::string expected =
-            "This variant of SetRuntimeArgs can only be called when Asynchronous SW Command Queues are disabled for "
-            "Fast Dispatch.";
-        const string error = string(e.what());
-        EXPECT_TRUE(error.find(expected) != std::string::npos);
-    }
-    command_queue.set_mode(current_mode);
-}
-
-TEST_F(CommandQueueProgramFixture, TensixTestAsyncCommandQueueSanityAndProfile) {
-    auto& command_queue = this->device_->command_queue();
-    auto current_mode = CommandQueue::default_mode();
-    command_queue.set_mode(CommandQueue::CommandQueueMode::ASYNC);
-    Program program;
-
-    CoreRange cr({0, 0}, {0, 0});
-    CoreRangeSet cr_set({cr});
-    // Add an NCRISC blank manually, but in compile program, the BRISC blank will be
-    // added separately
-    auto dummy_reader_kernel = CreateKernel(
-        program,
-        "tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/command_queue/arbiter_hang.cpp",
-        cr_set,
-        DataMovementConfig{.processor = DataMovementProcessor::RISCV_1, .noc = NOC::RISCV_1_default});
-    // Use scoper timer to benchmark time for pushing 2 commands
-    {
-        tt::ScopedTimer timer("AsyncCommandQueue");
-        EnqueueProgram(command_queue, program, false);
-        Finish(command_queue);
-    }
-    command_queue.set_mode(current_mode);
-}
-
-TEST_F(CommandQueueBufferFixture, DISABLED_TensixTestAsyncCBAllocation) {
-    // Test asynchronous allocation of buffers and their assignment to CBs
-    auto& command_queue = this->device_->command_queue();
-    auto current_mode = CommandQueue::default_mode();
-    command_queue.set_mode(CommandQueue::CommandQueueMode::ASYNC);
-    Program program;
-
-    const uint32_t num_pages = 1;
-    const uint32_t page_size = detail::TileSize(tt::DataFormat::Float16_b);
-    const tt::DataFormat data_format = tt::DataFormat::Float16_b;
-
-    auto buffer_size = page_size;
-    tt::tt_metal::InterleavedBufferConfig buff_config{
-        .device = this->device_,
-        .size = buffer_size,
-        .page_size = buffer_size,
-        .buffer_type = tt::tt_metal::BufferType::L1};
-    // Asynchronously allocate an L1 Buffer
-    auto l1_buffer = CreateBuffer(buff_config);
-    CoreRange cr({0, 0}, {0, 2});
-    CoreRangeSet cr_set({cr});
-    std::vector<uint8_t> buffer_indices = {16, 24};
-
-    CircularBufferConfig config1 =
-        CircularBufferConfig(
-            page_size, {{buffer_indices[0], data_format}, {buffer_indices[1], data_format}}, *l1_buffer)
-            .set_page_size(buffer_indices[0], page_size)
-            .set_page_size(buffer_indices[1], page_size);
-    // Asynchronously assign the L1 Buffer to the CB
-    auto multi_core_cb = CreateCircularBuffer(program, cr_set, config1);
-    auto cb_ptr = detail::GetCircularBuffer(program, multi_core_cb);
-    Finish(this->device_->command_queue());
-    // Addresses should match
-    EXPECT_EQ(cb_ptr->address(), l1_buffer->address());
-    // Asynchronously allocate a new L1 buffer
-    auto l1_buffer_2 = CreateBuffer(buff_config);
-    // Asynchronously update CB address to match new L1 buffer
-    UpdateDynamicCircularBufferAddress(program, multi_core_cb, *l1_buffer_2);
-    Finish(this->device_->command_queue());
-    // Addresses should match
-    EXPECT_EQ(cb_ptr->address(), l1_buffer_2->address());
-    command_queue.set_mode(current_mode);
-}
-
-TEST_F(CommandQueueMultiDeviceFixture, DISABLED_TestDirectedLoopbackToUniqueHugepage) {
-    std::unordered_map<chip_id_t, std::vector<uint32_t>> golden_data;
-
-    const uint32_t byte_size = 2048 * 16;
-    const uint64_t address = 0;
-
-    for (chip_id_t device_id = 0; device_id < num_devices_; device_id++) {
-        std::vector<uint32_t> data =
-            tt::test_utils::generate_uniform_random_vector<uint32_t>(0, UINT32_MAX, byte_size / sizeof(uint32_t));
-
-        chip_id_t mmio_device_id = tt::Cluster::instance().get_associated_mmio_device(device_id);
-        uint16_t channel = tt::Cluster::instance().get_assigned_channel_for_device(device_id);
-        tt::Cluster::instance().write_sysmem(
-            data.data(), data.size() * sizeof(uint32_t), address, mmio_device_id, channel);
-
-        golden_data[device_id] = data;
-    }
-
-    std::vector<uint32_t> readback_data;
-    readback_data.resize(byte_size / sizeof(uint32_t));
-    for (chip_id_t device_id = 0; device_id < num_devices_; device_id++) {
-        chip_id_t mmio_device_id = tt::Cluster::instance().get_associated_mmio_device(device_id);
-        uint16_t channel = tt::Cluster::instance().get_assigned_channel_for_device(device_id);
-        tt::Cluster::instance().read_sysmem(readback_data.data(), byte_size, address, mmio_device_id, channel);
-        EXPECT_EQ(readback_data, golden_data.at(device_id));
-    }
-}
-}  // namespace host_tests

tests/tt_metal/tt_metal/dispatch/dispatch_buffer/test_EnqueueWriteBuffer_and_EnqueueReadBuffer.cpp
@@ -462,52 +462,6 @@ bool test_EnqueueWriteBuffer_and_EnqueueReadBuffer_multi_queue(
 namespace basic_tests {
 namespace dram_tests {
 
-TEST_F(CommandQueueBufferFixture, DISABLED_TestAsyncBufferRW) {
-    // Test Async Enqueue Read and Write + Get Addr + Buffer Allocation and Deallocation
-    auto& command_queue = this->device_->command_queue();
-    auto current_mode = CommandQueue::default_mode();
-    command_queue.set_mode(CommandQueue::CommandQueueMode::ASYNC);
-    Program program;
-    for (int j = 0; j < 10; j++) {
-        // Asynchronously initialize a buffer on device
-        uint32_t first_buf_value = j + 1;
-        uint32_t second_buf_value = j + 2;
-        uint32_t first_buf_size = 4096;
-        uint32_t second_buf_size = 2048;
-        // Asynchronously allocate buffer on device
-        std::shared_ptr<Buffer> buffer =
-            Buffer::create(this->device_, first_buf_size, first_buf_size, BufferType::DRAM);
-        std::shared_ptr<uint32_t> allocated_buffer_address = std::make_shared<uint32_t>();
-        EnqueueGetBufferAddr(this->device_->command_queue(), allocated_buffer_address.get(), buffer.get(), true);
-        // Ensure returned addr is correct
-        EXPECT_EQ((*allocated_buffer_address), buffer->address());
-
-        std::shared_ptr<std::vector<uint32_t>> vec =
-            std::make_shared<std::vector<uint32_t>>(first_buf_size / 4, first_buf_value);
-        std::vector<uint32_t> readback_vec = {};
-        // Write first vector to existing on device buffer.
-        EnqueueWriteBuffer(this->device_->command_queue(), buffer, vec, false);
-        // Reallocate the vector in the main thread after asynchronously pushing it (ensure that worker still has access
-        // to this data)
-        vec = std::make_shared<std::vector<uint32_t>>(second_buf_size / 4, second_buf_value);
-        // Simulate what tt-eager does: Share buffer ownership with program
-        AssignGlobalBufferToProgram(buffer, program);
-        // Reallocate buffer (this is safe, since the program also owns the existing buffer, which will not be
-        // deallocated)
-        buffer = Buffer::create(this->device_, second_buf_size, second_buf_size, BufferType::DRAM);
-        // Write second vector to second buffer
-        EnqueueWriteBuffer(this->device_->command_queue(), buffer, vec, false);
-        // Have main thread give up ownership immediately after writing
-        vec.reset();
-        // Read both buffer and ensure data is correct
-        EnqueueReadBuffer(this->device_->command_queue(), buffer, readback_vec, true);
-        for (int i = 0; i < readback_vec.size(); i++) {
-            EXPECT_EQ(readback_vec[i], second_buf_value);
-        }
-    }
-    command_queue.set_mode(current_mode);
-}
-
 TEST_F(CommandQueueSingleCardBufferFixture, WriteOneTileToDramBank0) {
     TestBufferConfig config = {.num_pages = 1, .page_size = 2048, .buftype = BufferType::DRAM};
     for (IDevice* device : devices_) {

tests/tt_metal/tt_metal/integration/test_flatten.cpp
@@ -299,17 +299,3 @@ TEST_F(DispatchFixture, TensixFlatten) {
         ASSERT_TRUE(test_flatten::flatten(this, this->devices_.at(id), num_tiles_r, num_tiles_c));
     }
 }
-
-TEST_F(CommandQueueProgramFixture, DISABLED_TensixTestAsyncFlattenStress) {
-    auto& command_queue = this->device_->command_queue();
-    auto current_mode = CommandQueue::default_mode();
-    command_queue.set_mode(CommandQueue::CommandQueueMode::ASYNC);
-    uint32_t num_tiles_r = 2;
-    uint32_t num_tiles_c = 2;
-    if (!this->IsSlowDispatch()) {
-        num_tiles_r = 1;
-        num_tiles_c = 1;
-    }
-    ASSERT_TRUE(test_flatten::flatten_stress(this->device_, num_tiles_r, num_tiles_c));
-    command_queue.set_mode(current_mode);
-}

tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/test_dispatcher.cpp
@@ -420,9 +420,6 @@ int main(int argc, char** argv) {
     try {
         int device_id = 0;
         tt_metal::IDevice* device = tt_metal::CreateDevice(device_id);
-
-        CommandQueue& cq = device->command_queue();
-
         tt_metal::Program program = tt_metal::CreateProgram();
 
         CoreCoord spoof_prefetch_core = {0, 0};

tt_metal/api/tt-metalium/device.hpp
@@ -39,13 +39,12 @@ inline namespace v0 {
 
 class Buffer;
 class Program;
-class CommandQueue;
 class SubDevice;
 
 }  // namespace v0
 
 class JitBuildEnv;
-class HWCommandQueue;
+class CommandQueue;
 class TraceBuffer;
 
 inline namespace v0 {
@@ -175,7 +174,6 @@ public:
     virtual const JitBuildStateSubset build_kernel_states(uint32_t programmable_core, uint32_t processor_class) const = 0;
 
     virtual SystemMemoryManager& sysmem_manager() = 0;
-    virtual HWCommandQueue& hw_command_queue(size_t cq_id = 0) = 0;
     virtual CommandQueue& command_queue(size_t cq_id = 0) = 0;
 
     // Metal trace device capture mode
@@ -199,7 +197,6 @@ public:
     virtual void initialize_and_launch_firmware() = 0;
     virtual void init_command_queue_host() = 0;
     virtual void init_command_queue_device() = 0;
-    virtual void initialize_synchronous_sw_cmd_queue() = 0;
     virtual void update_dispatch_cores_for_multi_cq_eth_dispatch() = 0;
 
     // Puts device into reset

commit d816104fd67075f384941ead3840b411d51810e8

Make runtime_args_ptr const ref to solve clang-tidy error due to 779a2969b (#17364)
### Ticket
FYI @tt-rkim 
Related to @ayerofieiev-tt ticket
https://github.com/tenstorrent/tt-metal/issues/17208

Fixes clang-tidy

### Problem description
clang-tidy on CI
([link](https://github.com/tenstorrent/tt-metal/actions/runs/13039390588/job/36414797756))
failing wtih this error since commit
https://github.com/tenstorrent/tt-metal/commit/779a2969b310018fc57677ca34e883b4c365c69a

```
/home/ubuntu/actions-runner-2/_work/tt-metal/tt-metal/tt_metal/impl/dispatch/command_queue.cpp:469:34: error: the parameter 'runtime_args_ptr' is copied for each invocation but only used as a const reference; consider making it a const reference [performance-unnecessary-value-param,-warnings-as-errors]
  469 |     std::shared_ptr<RuntimeArgs> runtime_args_ptr,
      |                                  ^
      |     const                       &
200249 warnings generated.
Suppressed 200310 warnings (200248 in non-user code, 62 NOLINT).
Use -header-filter=.* to display errors from all non-system headers. Use -system-headers to display errors from system headers as well.
1 warning treated as error
```

### What's changed

- Make this argument const ref as suggested and kicked off code-analysis
job on this branch here:
https://github.com/tenstorrent/tt-metal/actions/runs/13053370584

### Checklist
- [ ] Code Analyis job previously failing
- [ ] Post commit CI passes
- [ ] Blackhole Post commit (if applicable)
- [ ] Model regression CI testing passes (if applicable)
- [ ] Device performance regression CI testing passes (if applicable)
- [ ] **(For models and ops writers)** Full [new
models](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
tests passes
- [ ] New/Existing tests provide coverage for changes

tt_metal/api/tt-metalium/command_queue.hpp
@@ -205,7 +205,7 @@ void EnqueueGetBufferAddr(uint32_t* dst_buf_addr, const Buffer* buffer, bool blo
 void EnqueueSetRuntimeArgs(
     const std::shared_ptr<Kernel>& kernel,
     const CoreCoord& core_coord,
-    std::shared_ptr<RuntimeArgs> runtime_args_ptr,
+    const std::shared_ptr<RuntimeArgs>& runtime_args_ptr,
     bool blocking);
 void EnqueueAddBufferToProgram(
     const std::variant<std::reference_wrapper<Buffer>, std::shared_ptr<Buffer>>& buffer,

tt_metal/impl/dispatch/command_queue.cpp
@@ -466,7 +466,7 @@ void EnqueueAddBufferToProgram(
 void EnqueueSetRuntimeArgs(
     const std::shared_ptr<Kernel>& kernel,
     const CoreCoord& core_coord,
-    std::shared_ptr<RuntimeArgs> runtime_args_ptr,
+    const std::shared_ptr<RuntimeArgs>& runtime_args_ptr,
     bool blocking) {
     std::vector<uint32_t> resolved_runtime_args = {};
     resolved_runtime_args.reserve(runtime_args_ptr->size());