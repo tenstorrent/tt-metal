51ea69ff1ded027875d923cafbf08866568db546
Stuti Raizada <159130512+sraizada-tt@users.noreply.github.com>
2025-01-30 18:35:24 +0000

    Implement ttnn.sampling op for top-k, top-p sampling (#17136)
    ### Ticket
    (https://github.com/tenstorrent/tt-metal/issues/16854)
    
    ### Problem description
    Need to do per-user top-p/top-k sampling for LLMs on device 
    
    ### What's changed
    Added ttnn.sampling op 
    
    ### Checklist
    - [x] Post commit CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/12989372474
    - [x] New/Existing tests provide coverage for changes

---
+++ ttnn/cpp/ttnn/operations/reduction/sampling/sampling_pybind.hpp
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "pybind11/pybind_fwd.hpp"
+
+namespace ttnn::operations::reduction::detail {
+void bind_reduction_sampling_operation(pybind11::module& module);
+}  // namespace ttnn::operations::reduction::detail
