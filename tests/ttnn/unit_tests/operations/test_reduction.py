a42c151b69484d0b06bc1fbb143364f8fe1ae111
Borys Bradel <164946524+bbradelTT@users.noreply.github.com>
2025-01-31 13:10:55 -0500

    #16380: add nD support for generic reductions (#17399)
    ### Ticket
    Link to Github Issue #16380
    
    ### Problem description
    - generic reductions do not support tensors with rank greater than 4
    
    ### What's changed
    - only change tensors to be rank 4 when calling the ops that run on the
    device, and outside of that leave the rank as is, allowing nD tensor
    support
    - also tiling correction needs to be extended to 8d
    
    ### Checklist
    - [x] Post commit CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/13074810420/job/36484687679
    has only one error: `The self-hosted runner: tt-metal-ci-vm-96 lost
    communication with the server` in a job that passed in.a previous run
    https://github.com/tenstorrent/tt-metal/actions/runs/13073025674
    - [x] Blackhole Post commit (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/13062110967
    - [x] Model regression CI testing passes (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/13062115226
    - [x] Device performance regression CI testing passes (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/13072996613
    - [ ] **(For models and ops writers)** Full [new
    models](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    tests passes
    - [x] New/Existing tests provide coverage for changes

--- tests/ttnn/unit_tests/operations/test_reduction.py
+++ tests/ttnn/unit_tests/operations/test_reduction.py
@@ -8,6 +8,7 @@ import torch
 
 import ttnn
 from tests.ttnn.utils_for_testing import assert_with_pcc
+from models.utility_functions import skip_for_grayskull
 
 
 @pytest.mark.parametrize("batch_size", [1, 16])
@@ -78,6 +79,107 @@ def test_prod(device, batch_size, c, h, w, dim, keepdim):
     # assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)
 
 
+@skip_for_grayskull("Not a tile size multiple, may fail on GS if run all tests. #17084")
+@pytest.mark.parametrize("dim_1", [1])
+@pytest.mark.parametrize("dim_2", [2])
+@pytest.mark.parametrize("dim_3", [3])
+@pytest.mark.parametrize("dim_4", [4])
+@pytest.mark.parametrize("dim_5", [4])
+@pytest.mark.parametrize("dim_6", [6])
+@pytest.mark.parametrize("dim_7", [7])
+@pytest.mark.parametrize("dim_8", [8])
+@pytest.mark.parametrize("dim", [[3, 7]])
+@pytest.mark.parametrize("keepdim", [True, False])
+def test_sum_8d_tensor_dims(device, dim_1, dim_2, dim_3, dim_4, dim_5, dim_6, dim_7, dim_8, dim, keepdim):
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.randn((dim_1, dim_2, dim_3, dim_4, dim_5, dim_6, dim_7, dim_8), dtype=torch.bfloat16)
+    torch_output_tensor = torch.sum(torch_input_tensor, dim=dim, keepdim=keepdim)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+    output_tensor = ttnn.sum(input_tensor, dim=dim, keepdim=keepdim)
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.TILE_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+
+    output_tensor = ttnn.to_torch(output_tensor)
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)
+
+
+@skip_for_grayskull("Not a tile size multiple, may fail on GS if run all tests. #17084")
+@pytest.mark.parametrize("dim_1", [1])
+@pytest.mark.parametrize("dim_2", [2])
+@pytest.mark.parametrize("dim_3", [3])
+@pytest.mark.parametrize("dim_4", [4])
+@pytest.mark.parametrize("dim_5", [5])
+@pytest.mark.parametrize("dim_6", [6])
+@pytest.mark.parametrize("dim_7", [7])
+@pytest.mark.parametrize("dim", [[2, 5]])
+@pytest.mark.parametrize("keepdim", [True, False])
+def test_sum_7d_tensor_dims(device, dim_1, dim_2, dim_3, dim_4, dim_5, dim_6, dim_7, dim, keepdim):
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.randn((dim_1, dim_2, dim_3, dim_4, dim_5, dim_6, dim_7), dtype=torch.bfloat16)
+    torch_output_tensor = torch.sum(torch_input_tensor, dim=dim, keepdim=keepdim)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+
+    output_tensor = ttnn.sum(input_tensor, dim=dim, keepdim=keepdim)
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.TILE_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+
+    output_tensor = ttnn.to_torch(output_tensor)
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)
+
+
+@skip_for_grayskull("Not a tile size multiple, may fail on GS if run all tests. #17084")
+@pytest.mark.parametrize("dim_1", [1])
+@pytest.mark.parametrize("dim_2", [2])
+@pytest.mark.parametrize("dim_3", [3])
+@pytest.mark.parametrize("dim_4", [4])
+@pytest.mark.parametrize("dim_5", [5])
+@pytest.mark.parametrize("dim_6", [6])
+@pytest.mark.parametrize("dim", [[1, 4], -1, None])
+@pytest.mark.parametrize("keepdim", [True])
+def test_sum_6d_tensor_dims(device, dim_1, dim_2, dim_3, dim_4, dim_5, dim_6, dim, keepdim):
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.randn((dim_1, dim_2, dim_3, dim_4, dim_5, dim_6), dtype=torch.bfloat16)
+    torch_output_tensor = torch.sum(torch_input_tensor, dim=dim, keepdim=keepdim)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+
+    output_tensor = ttnn.sum(input_tensor, dim=dim, keepdim=keepdim)
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.TILE_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+
+    output_tensor = ttnn.to_torch(output_tensor)
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)
+
+
+@skip_for_grayskull("Not a tile size multiple, may fail on GS if run all tests. #17084")
+@pytest.mark.parametrize("dim_1", [33])
+@pytest.mark.parametrize("dim_2", [5])
+@pytest.mark.parametrize("dim_3", [7])
+@pytest.mark.parametrize("dim_4", [2])
+@pytest.mark.parametrize("dim_5", [59])
+@pytest.mark.parametrize("dim", [[1, 4], -1, None])
+@pytest.mark.parametrize("keepdim", [True])
+def test_sum_5d_tensor_dims(device, dim_1, dim_2, dim_3, dim_4, dim_5, dim, keepdim):
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.randn((dim_1, dim_2, dim_3, dim_4, dim_5), dtype=torch.bfloat16)
+    torch_output_tensor = torch.sum(torch_input_tensor, dim=dim, keepdim=keepdim)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+
+    output_tensor = ttnn.sum(input_tensor, dim=dim, keepdim=keepdim)
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.TILE_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+
+    output_tensor = ttnn.to_torch(output_tensor)
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)
+
+
 @pytest.mark.parametrize("batch_size", [32])
 @pytest.mark.parametrize("c", [32])
 @pytest.mark.parametrize("h", [37])
@@ -103,7 +205,7 @@ def test_sum_4d_tensor_dims(device, batch_size, c, h, w, dim, keepdim):
 @pytest.mark.parametrize("c", [3])
 @pytest.mark.parametrize("h", [31])
 @pytest.mark.parametrize("w", [32])
-@pytest.mark.parametrize("dim", [[0, 2], [0, 1, 2]])
+@pytest.mark.parametrize("dim", [[0, 2], [0, 1, 2], None])
 @pytest.mark.parametrize("keepdim", [True])
 def test_sum_3d_tensor_dims(device, c, h, w, dim, keepdim):
     torch.manual_seed(0)
@@ -123,7 +225,7 @@ def test_sum_3d_tensor_dims(device, c, h, w, dim, keepdim):
 
 @pytest.mark.parametrize("h", [41])
 @pytest.mark.parametrize("w", [31])
-@pytest.mark.parametrize("dim", [0, 1, [0, 1]])
+@pytest.mark.parametrize("dim", [0, 1, [0, 1], None])
 @pytest.mark.parametrize("keepdim", [True])
 def test_sum_2d_tensor_dims(device, h, w, dim, keepdim):
     torch.manual_seed(0)
