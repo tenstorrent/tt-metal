f11c2d72adf92acbedb0679367c593cadf6e5cb5
Jeffrey Jiang <jjiang@tenstorrent.com>
2025-01-31 21:42:46 +0000

    Bash remaining imports and usages

--- models/demos/t3000/falcon40b/tests/test_falcon_model.py
+++ models/demos/t3000/falcon40b/tests/test_falcon_model.py
@@ -6,7 +6,7 @@ import torch
 import pytest
 from loguru import logger
 import ttnn
-from ttnn import ShardTensorToMesh, ConcatMeshToTensor, ListMeshToTensor
+from ttnn import ShardTensorToMesh, ConcatMeshToTensor
 from models.demos.t3000.falcon40b.reference.hf_modeling_falcon import (
     FalconForCausalLM,
 )
@@ -213,7 +213,7 @@ def run_test_FalconModel_inference(
             use_cache=use_cache,
         )
         # Output of model is replicated
-        tensors = ttnn.to_torch(tt_out, device=mesh_device, mesh_composer=ListMeshToTensor(mesh_device))
+        tensors = ttnn._ttnn.multi_device.shardedtensor_to_tensorlist(tt_out)
         tt_out = tensors[0].squeeze(1).transpose(0, 1)
 
     # check outputs ----------------------------------------------------------------------
