618c098befcec6fd3df3acd69eea68e0e1d5ce7e
Bojan Rosko <brosko@tenstorrent.com>
2025-01-31 20:49:51 +0000

    move to .get_cores

--- tt_metal/api/tt-metalium/metal_soc_descriptor.h
+++ tt_metal/api/tt-metalium/metal_soc_descriptor.h
@@ -24,12 +24,6 @@ public:
     uint64_t dram_core_size;
     uint64_t dram_view_size;
 
-    // in tt_SocDescriptor worker_log_to_routing_x and worker_log_to_routing_y map logical coordinates to NOC virtual
-    // coordinates UMD accepts NOC virtual coordinates but Metal needs NOC physical coordinates to ensure a harvested
-    // core is not targetted
-    std::unordered_map<tt_xy_pair, CoreDescriptor> physical_cores;
-    std::vector<tt_xy_pair> physical_workers;
-    std::vector<tt_xy_pair> physical_harvested_workers;
     std::vector<tt_xy_pair> physical_ethernet_cores;
 
     std::map<CoreCoord, int> logical_eth_core_to_chan_map;
@@ -45,7 +39,6 @@ public:
     size_t get_channel_for_dram_view(int dram_view) const;
     size_t get_num_dram_views() const;
 
-    bool is_harvested_core(const CoreCoord& core) const;
     const std::vector<CoreCoord>& get_pcie_cores() const;
     const std::vector<CoreCoord> get_dram_cores() const;
     const std::vector<CoreCoord>& get_logical_ethernet_cores() const;
@@ -63,12 +56,15 @@ public:
 
     tt_cxy_pair convert_to_umd_coordinates(const tt_cxy_pair& physical_cxy) const;
 
+    // During the transition of the UMD's api to CoreCoords, this function is needed to make the transition smoother.
+    // At the moment, different coordinate systems are expected for grayskull and other architectures.
+    CoordSystem get_umd_coord_system() const;
+
     // Number of cores per DRAM bank ceiled to nearest integer
     int profiler_ceiled_core_count_perf_dram_bank = 0;
     std::map<CoreCoord, int32_t> physical_routing_to_profiler_flat_id;
 
 private:
-    void generate_physical_descriptors_from_virtual(uint32_t harvesting_mask);
     void load_dram_metadata_from_device_descriptor();
     void generate_logical_eth_coords_mapping();
     void generate_physical_routing_to_profiler_flat_id();
