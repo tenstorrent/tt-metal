9fbb3ef44eb6aad9f2dea3032adc35a141968eae
Adrian Morrison <amorrison@tenstorrent.com>
2025-01-31 17:16:20 +0000

    PR comments 4

--- ttnn/cpp/ttnn/operations/data_movement/repeat/device/host/repeat_program_factory.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/repeat/device/host/repeat_program_factory.hpp
@@ -8,7 +8,7 @@
 
 namespace ttnn::operations::data_movement::repeat {
 
-tt::tt_metal::operation::ProgramWithCallbacks rm_repeat_program_factory(
+tt::tt_metal::operations::ProgramWithCallbacks rm_repeat_program_factory(
     const Tensor& input, uint32_t num_repeats, const Tensor& output, bool is_last_dim);
 
 }  // namespace ttnn::operations::data_movement::repeat
