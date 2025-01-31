14d6ca351216173626893db7d4ae87cd9d53cc80
Jeffrey Jiang <jjiang@tenstorrent.com>
2025-01-31 21:42:46 +0000

    Bash remaining imports and usages

--- tests/ttnn/unit_tests/test_multi_device_trace.py
+++ tests/ttnn/unit_tests/test_multi_device_trace.py
@@ -10,7 +10,7 @@ import tempfile
 from loguru import logger
 import os
 from tests.ttnn.utils_for_testing import assert_with_pcc
-from ttnn import ShardTensorToMesh, ReplicateTensorToMesh, ConcatMeshToTensor, ListMeshToTensor
+from ttnn import ShardTensorToMesh, ReplicateTensorToMesh, ConcatMeshToTensor
 
 NUM_TRACE_LOOPS = int(os.getenv("NUM_TRACE_LOOPS", 15))
 
