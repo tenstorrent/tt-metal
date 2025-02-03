5e5bb17a572648b6815d845862eca653a85e16a8
Gustavo Sarabando <ctr-gsarabando@ext.tenstorrent.com>
2025-02-03 12:09:17 -0800

    update deprecated API use (#17509)
    ### Ticket
    [Update use of deprecated API in eltwise_sfpu
    #17444](https://github.com/tenstorrent/tt-metal/issues/17444)
    
    ### Problem description
    Deprecated API was still used in tt_metal/kernels/eltwise_sfpu.cpp
    acquire_dst() /  release_dst()
    
    ### What's changed
    updated it to use:
    tile_regs_acquire();
    tile_regs_commit();
    tile_regs_wait();
    tile_regs_release();

--- tt_metal/kernels/compute/eltwise_sfpu.cpp
+++ tt_metal/kernels/compute/eltwise_sfpu.cpp
@@ -17,22 +17,21 @@ void MAIN {
     for (uint32_t block_index = 0; block_index < per_core_block_cnt; block_index++) {
         cb_reserve_back(tt::CBIndex::c_16, per_core_block_dim);
         for (uint32_t tile_index = 0; tile_index < per_core_block_dim; ++tile_index) {
-            acquire_dst();
+            tile_regs_acquire();
 
             // Pop tile after tile, copy to DST and pack
             cb_wait_front(tt::CBIndex::c_0, 1);
-
             copy_tile(tt::CBIndex::c_0, 0, 0);
 
 #ifdef SFPU_OP_CHAIN_0
             SFPU_OP_CHAIN_0
 #endif
-
+            tile_regs_commit();
+            tile_regs_wait();
             pack_tile(0, tt::CBIndex::c_16);
 
             cb_pop_front(tt::CBIndex::c_0, 1);
-
-            release_dst();
+            tile_regs_release();
         }
         cb_push_back(tt::CBIndex::c_16, per_core_block_dim);
     }
