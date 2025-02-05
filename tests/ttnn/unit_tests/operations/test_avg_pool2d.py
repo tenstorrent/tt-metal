000a38a6feefc721abfcf774791a7195a3078161
Jong Bin Lim <jongbinlim@tenstorrent.com>
2025-02-05 23:53:01 +0000

    Minor lint. Refactoring some test code.

--- tests/ttnn/unit_tests/operations/test_avg_pool2d.py
+++ tests/ttnn/unit_tests/operations/test_avg_pool2d.py
@@ -9,13 +9,64 @@ import ttnn
 from tests.ttnn.utils_for_testing import assert_with_pcc
 
 
+# @pytest.fixture
+# def setup():
+#     """Fixture to prepare input data for ttnn.avg_pool2d() and torch.nn.functional.avg_pool2d()."""
+#     def _prepare_test_input_data(device, input_shape):
+#         # Generate random test input data - suitable for torch.nn.functional.avg_pool2d().
+#         input_data = torch.rand(input_shape, dtype=torch.bfloat16)
+
+#         # Preprocess the input data - suitable for ttnn.avg_pool2d().
+#         preprocessed_input_data = torch.permute(input_data, (0, 2, 3, 1))
+#         preprocessed_input_data = torch.reshape(preprocessed_input_data, (1, 1, -1, in_c))
+#         preprocessed_input_data = ttnn.from_torch(preprocessed_input_data, layout=ttnn.ROW_MAJOR_LAYOUT, device=device)
+
+#         batch_size, in_c, in_h, in_w = input_shape
+#         return input_data, preprocessed_input_data, batch_size, in_h, in_w, in_c
+#     return _prepare_test_input_data
+
+# def post_process_output_data(output_data, batch_size, out_h, out_w, out_c):
+#     output_data = ttnn.to_torch(output_data)
+#     output_data = torch.reshape(output_data, (batch_size, out_h, out_w, out_c))
+#     output_data = torch.permute(output_data, (0, 3, 1, 2))
+#     return output_data
+
+
+# @pytest.mark.parametrize("device_params", [{"l1_small_size": 4096}], indirect=True)
+# @pytest.mark.parametrize(
+#     "input_shape, kernel_size, stride, padding, dilation",
+#     [
+#         ((1, 16, 56, 56), (2, 2), (2, 2), (0, 0), (1, 1)),
+#         ((1, 128, 56, 56), (2, 2), (2, 2), (0, 0), (1, 1)),
+#     ]
+# )
+# def test_avg_pool2d(setup, device, input_shape, kernel_size, stride, padding, dilation):
+#     """Test ttnn.avg_pool2d() by comparing with torch.nn.functional.avg_pool2d()."""
+
+#     # Using fixture, prepare the test input data for both ttnn.avg_pool2d() and torch.nn.functional.avg_pool2d().
+#     input_data, preprocessed_input_data, batch_size, in_h, in_w, in_c = setup(device, input_shape, kernel_size, stride, padding, dilation)
+
+#     # Get actual output data from ttnn.avg_pool2d().
+#     actual_output_data = ttnn.avg_pool2d(
+#         preprocessed_input_data, batch_size, in_h, in_w, in_c, kernel_size, stride, padding, dilation, applied_shard_scheme=None)
+
+#     # Get expected output data from torch.nn.functional.avg_pool2d().
+#     expected_output_data = torch.nn.functional.avg_pool2d(
+#         input_data, kernel_size, stride, padding, ceil_mode=False, count_include_pad=True, divisor_override=None)
+
+#     # Compare the expected and actual outputs.
+#     _, out_c, out_h, out_w = expected_output_data.shape
+#     post_processed_actual_output_data = post_process_output_data(actual_output_data, batch_size, out_h, out_w, out_c)
+#     assert_with_pcc(expected_output_data, post_processed_actual_output_data, 0.99)
+
+
 def run_once(device, input_shape, kernel_size, stride, padding, dilation, shard_scheme=None):
+    batch_size, in_c, in_h, in_w = input_shape
     torch_input = torch.rand(input_shape, dtype=torch.bfloat16)
 
     input_tensor = torch.permute(torch_input, (0, 2, 3, 1))
     input_tensor = torch.reshape(input_tensor, (1, 1, -1, in_c))
     input_tensor = ttnn.from_torch(input_tensor, layout=ttnn.ROW_MAJOR_LAYOUT, device=device)
-    batch_size, in_c, in_h, in_w = input_shape
 
     # Function under test
     output_tensor = ttnn.avg_pool2d(
