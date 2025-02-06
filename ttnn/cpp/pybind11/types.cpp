949d5d8829c6392e89c1c0f09bfa2cd0e2a44afe
Artem Yerofieiev <artem@tenstorrent.com>
2025-02-06 07:44:19 +0000

    fix

--- ttnn/cpp/pybind11/types.cpp
+++ ttnn/cpp/pybind11/types.cpp
@@ -1,6 +1,6 @@
-#SPDX - FileCopyrightText : © 2025 Tenstorrent Inc.
-
-#SPDX - License - Identifier : Apache - 2.0
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
 
 #include "types.hpp"
 
