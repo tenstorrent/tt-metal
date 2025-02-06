9e14393968fce47fd8c0b0627ae69237f8f5456b
vguduruTT <venkatesh.guduru@multicorewarein.com>
2025-02-06 15:19:12 +0000

    refactoring code

--- models/experimental/functional_yolov11/test/test_ttnn_yolov11.py
+++ models/experimental/functional_yolov11/test/test_ttnn_yolov11.py
@@ -62,7 +62,7 @@ def attempt_load(weights, map_location=None):
 
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 79104}], indirect=True)
 def test_yolov11(device, use_program_cache, reset_seeds):
-    torch_input, ttnn_input = create_yolov11_input_tensors(device)
+    torch_input, ttnn_input = create_yolov11_input_tensors(device, input_channels=3, input_height=320, input_width=320)
 
     torch_model = attempt_load("yolov11n.pt", map_location="cpu")
     state_dict = torch_model.state_dict()
@@ -72,16 +72,13 @@ def test_yolov11(device, use_program_cache, reset_seeds):
     for (name1, parameter1), (name2, parameter2) in zip(torch_model.state_dict().items(), ds_state_dict.items()):
         if isinstance(parameter2, torch.FloatTensor):
             new_state_dict[name1] = parameter2
-    torch_model.load_state_dict(new_state_dict)
+    torch_model.load_state_dict(state_dict)
     torch_model.eval()
 
     torch_output = torch_model(torch_input)
     parameters = create_yolov11_model_parameters(torch_model, torch_input, device=device)
     ttnn_model = ttnn_yolov11.YoloV11(device, parameters)
     ttnn_output = ttnn_model(ttnn_input)
-    # l1 = torch.load("/home/ubuntu/tt-metal/models/experimental/functional_yolov11/dumps/torch_out.pth")
-    # l2 = torch.load("/home/ubuntu/tt-metal/models/experimental/functional_yolov11/dumps/ttnn_out.pth")
-    # assert_with_pcc(l1,l2,0.99999)
     ttnn_output = ttnn.to_torch(ttnn_output)
     # ttnn_output = ttnn_output.permute(0, 2, 1)
     print(ttnn_output.shape, torch_output.shape)
