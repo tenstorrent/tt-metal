31404df9f75616fa7c8116cd29ec291e1083b92d
Jay Kruer <jkruer@tenstorrent.com>
2025-02-06 02:55:29 +0000

    #0: Add manual RMSNorm grad (untested)

--- tt-train/sources/ttml/ops/rmsnorm_op.cpp
+++ tt-train/sources/ttml/ops/rmsnorm_op.cpp
@@ -28,9 +28,36 @@ autograd::TensorPtr rmsnorm(const autograd::TensorPtr& tensor, const autograd::T
     auto mean_of_squares = ttml::ops::mean(squares);
     auto mean_of_squares_plus_epsilon = mean_of_squares + eps_tensor;
     auto rms_eps = ttml::ops::sqrt(mean_of_squares_plus_epsilon);
-    auto gamma_times_activations = gamma * tensor;
+    auto gamma_times_activations = (gamma * tensor)->get_value();
     float rms_eps_value = core::to_xtensor(rms_eps->get_value())[0];
-    auto out = gamma_times_activations / rms_eps_value;
+    auto out_tensor = ttnn::divide(gamma_times_activations, rms_eps_value);
+    
+    auto out = autograd::create_tensor(out_tensor);
+
+    autograd::GradFunction grad = [tensor, out, gamma, epsilon]() {
+        auto dout = out->get_grad();
+        // let tensor = {a_i | i = 0, 1, ..., n}
+        // and gamma = {g_i | i = 0, 1, ..., n}
+
+        // backward grads in terms of dout:
+        // dL/da_i = dL/dout * eps * gamma_i / (eps + a_i^2)^(3/2)
+        // dL/dg_i = dL/dout * a_i / sqrt(eps + a_i^2)
+
+        auto dtensor_divisor = ttnn::pow(ttnn::add(epsilon, ttnn::square(tensor->get_value())), 3.0F / 2.0F);
+        auto dtensor_dividend = ttnn::multiply(ttnn::multiply(dout, gamma), epsilon);
+        auto dtensor = ttnn::divide(dtensor_dividend, dtensor_divisor);
+        
+        auto dgamma_dividend = ttnn::multiply(dout, tensor);
+        auto dgamma_divisor = ttnn::sqrt(ttnn::add(epsilon, ttnn::square(tensor->get_value())));
+        auto dgamma = ttnn::divide(dgamma_dividend, dgamma_divisor);
+
+        tensor->add_grad(dtensor);
+        gamma->add_grad(dgamma);
+    };
+
+    auto links = autograd::get_links(tensor);
+    out->set_node(autograd::ctx().add_backward_node(std::move(grad), links));
+
     return out;
 }
 
