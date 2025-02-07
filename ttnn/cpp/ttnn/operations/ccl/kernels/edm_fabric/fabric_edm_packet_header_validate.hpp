293e87bd22841d9442b6e068ff4a17edf913b70a
Sean Nijjar <sean.nijjar@gmail.com>
2025-02-07 10:33:17 -0500

    optimize edm fabric packet header structure (#17579)
    flatten the command and noc command type field to a single field to
    simplify header processing; lowers burden on workers and eriscs.
    
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/17429
    
    ### Problem description
    The packet header added an unnecessary additional level of nesting by
    having a field for write vs atomic and a separate one for noc_unicast vs
    noc_multicast. This leads to inefficiencies in kernels processing or
    inspecting headers because it means they often require nested checks.
    Additionally, it forces an additional call on the worker when setting up
    the packet header.
    
    ### What's changed
    Flattened/merged these two packet fields to remove nesting code in EDM
    fabric and workers.
    
    
    ### Additional Changes
    Updated reduce scatter async tests. The reduce scatter test is passing in the same global semaphores to reduce scatter for every back to back iteration which is unsafe and can lead to hangs due to race between op iterations using the same global semaphore.
    
    Additionally, reduce scatter currently does not correctly override RT args for global semaphores when the op is rerun with different global semaphores across iterations (to fix first mentioned issue).
    
    For that reason we are patching the test for CI pipeline stability until the reduce scatter RT arg update is resolved. We patch the test by synching after each iteration which removes the race for global semaphore usage between op iterations.

--- ttnn/cpp/ttnn/operations/ccl/kernels/edm_fabric/fabric_edm_packet_header_validate.hpp
+++ ttnn/cpp/ttnn/operations/ccl/kernels/edm_fabric/fabric_edm_packet_header_validate.hpp
@@ -9,13 +9,9 @@
 
 namespace tt::fabric {
 
-FORCE_INLINE void validate(PacketHeader const& packet_header) {
-    ASSERT(packet_header.command_type == CommandType::WRITE || packet_header.command_type == CommandType::ATOMIC_INC);
-    ASSERT(packet_header.chip_send_type < 2);
-    ASSERT(packet_header.noc_send_type < 2);
-}
+FORCE_INLINE void validate(const PacketHeader& packet_header) { ASSERT(packet_header.chip_send_type < 2); }
 FORCE_INLINE bool is_valid(PacketHeader const& packet_header) {
-    return (packet_header.command_type < 2) && (packet_header.chip_send_type < 2) && (packet_header.noc_send_type < 2);
+    return (packet_header.chip_send_type < 2) && (packet_header.noc_send_type < 2);
 }
 
 }  // namespace tt::fabric
