1b7bfb854d8ecb67c215588d6df3bd49401f9ff2
Kyle Mabee <kmabee@tenstorrent.com>
2025-02-07 21:26:19 +0000

    LightMetal - pybind LightMetalBegin/EndCapture() APIs as ttnn.light_metal_begin/end_capture() (Issue #18145)

--- ttnn/ttnn/core.py
+++ ttnn/ttnn/core.py
@@ -42,6 +42,8 @@ def is_sharded(tensor) -> bool:
 
 
 get_memory_config = ttnn._ttnn.core.get_memory_config
+light_metal_begin_capture = ttnn._ttnn.core.light_metal_begin_capture
+light_metal_end_capture = ttnn._ttnn.core.light_metal_end_capture
 
 
 def num_cores_to_corerangeset(
