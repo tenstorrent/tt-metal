8df03823fca7e8dd9e52bf1f8167675d2393d0d1
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2025-02-09 04:51:19 +0000

    #0: Update Buffer index in writer file

--- ttnn/cpp/ttnn/operations/normalization/batch_norm/device/batch_norm_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/normalization/batch_norm/device/batch_norm_program_factory.cpp
@@ -253,6 +253,11 @@ BatchNormOperation::BatchNormFactory::cached_program_t BatchNormOperation::Batch
                 f_is_dram,
                 static_cast<uint32_t>(weight_has_value),
                 static_cast<uint32_t>(bias_has_value),
+                batch_mean_tensor_cb,
+                output_tensor_cb,
+                batch_var_tensor_cb,
+                weight_tensor_cb,
+                bias_tensor_cb,
             },
             std::move(writer_defines)));
 
