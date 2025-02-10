3bb01f57002d19b9b6e7808bc9c7030a8a830424
Jason Davies <jason@jasondavies.com>
2025-02-10 19:30:31 +0000

    Fast pure SFPU implementation of binary GCD algorithm for int32.
    This is around 50x faster compared to the old/limited floating point implementation.
    
    Fixes #17771.

---
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_gcd.h
@@ -0,0 +1,137 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "ckernel.h"
+#include "ckernel_defs.h"
+#include "sfpi.h"
+
+using namespace sfpi;
+
+namespace ckernel {
+namespace sfpu {
+
+template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
+inline void calculate_sfpu_gcd(const uint dst_offset)
+{
+    // Binary GCD algorithm
+    for (int d = 0; d < ITERATIONS; d++) {
+        constexpr uint dst_tile_size = 64;
+        TTI_SFPLOAD(p_sfpu::LREG0, 4, 3, 0); // a
+        TT_SFPLOAD(p_sfpu::LREG1, 4, 3, dst_offset * dst_tile_size); // b
+        TTI_SFPMOV(0, p_sfpu::LCONST_0, p_sfpu::LREG2, 0); // sa
+        TTI_SFPMOV(0, p_sfpu::LCONST_0, p_sfpu::LREG3, 0); // sb
+        TT_SFPABS(0, p_sfpu::LREG0, p_sfpu::LREG0, 0);
+        TT_SFPABS(0, p_sfpu::LREG1, p_sfpu::LREG1, 0);
+
+        // Emulate ctz instruction by counting trailing zeroes using a series of bit masks.
+        // There is a maximum of 31 trailing zero bits.  The value 31 can be represented with 5 bits.
+        // We progressively set these 5 bits by checking the masks 0xffff, 0xff, 0xf, 0x7, 0x3, 0x1,
+        // each time shifting right by the width of the mask if the masked region is all zeroes.
+
+        // For the binary GCD algorithm, we do this for both a and b.
+
+        uint32_t mask = 0xffff;
+        uint32_t mask_width = 16;
+        #pragma GCC unroll 5
+        for (int i = 0; i < 5; ++i) {
+            // l4 = mask
+            TT_SFPLOADI(p_sfpu::LREG4, 2, mask);
+            // l4 &= a
+            TT_SFPAND(0, p_sfpu::LREG0, p_sfpu::LREG4, 0);
+            // if (a & mask) == 0
+            TT_SFPSETCC(0, p_sfpu::LREG4, 0, 6);
+            // then, a >>= mask_width
+            TT_SFPSHFT((-mask_width) & ((1<<12)-1), p_sfpu::LREG0, p_sfpu::LREG0, 1);
+            // sa += mask_width
+            TT_SFPLOADI(p_sfpu::LREG4, 2, mask_width);
+            TT_SFPOR(0, p_sfpu::LREG4, p_sfpu::LREG2, 0);
+            TT_SFPENCC(0, 0, 0, 0);
+
+            // l4 = mask
+            TT_SFPLOADI(p_sfpu::LREG4, 2, mask);
+            // l4 &= b
+            TT_SFPAND(0, p_sfpu::LREG1, p_sfpu::LREG4, 0);
+            // if (b & mask) == 0
+            TT_SFPSETCC(0, p_sfpu::LREG4, 0, 6);
+            // then, b >>= mask_width
+            TT_SFPSHFT((-mask_width) & ((1<<12)-1), p_sfpu::LREG1, p_sfpu::LREG1, 1);
+            // sb += mask_width
+            TT_SFPLOADI(p_sfpu::LREG4, 2, mask_width);
+            TT_SFPOR(0, p_sfpu::LREG4, p_sfpu::LREG3, 0);
+            TT_SFPENCC(0, 0, 0, 0);
+
+            mask_width >>= 1;
+            mask >>= mask_width;
+        }
+
+        // This is possibly a convoluted way to say: sa = min(sa, sb)
+
+        // set sb = sa - sb
+        TT_SFPIADD(0, p_sfpu::LREG2, p_sfpu::LREG3, 8 | 2);
+        TTI_SFPNOP;
+        // if (sa - sb) >= 0 then sa >= sb and we need to swap
+        // now we do sb = sa - (sa - sb), which gives us the original sb
+        TT_SFPIADD(0, p_sfpu::LREG2, p_sfpu::LREG3, 4 | 2);
+        TTI_SFPNOP;
+        // now just copy back to sa
+        TT_SFPMOV(0, p_sfpu::LREG3, p_sfpu::LREG2, 0);
+        TT_SFPENCC(0, 0, 0, 0);
+
+        // The worst case for binary GCD with signed 32-bit integers requires 32 iterations.
+
+        #pragma GCC unroll 32
+        for (int i = 0; i < 32; ++i) {
+            // if (b > a) swap(a, b)
+            // a -= b
+
+            // l4 = b
+            TT_SFPMOV(0, p_sfpu::LREG1, p_sfpu::LREG4, 0);
+            // l4 = a - b
+            TT_SFPIADD(0, p_sfpu::LREG0, p_sfpu::LREG4, 8 | 2);
+            TTI_SFPNOP;
+            // if (a - b) >= 0 then that's the new value of a
+            TT_SFPMOV(0, p_sfpu::LREG4, p_sfpu::LREG0, 0);
+            // else we set b = a and set a = -l4
+            TT_SFPCOMPC(0, 0, 0, 0);
+            // set b = a
+            TT_SFPMOV(0, p_sfpu::LREG0, p_sfpu::LREG1, 0);
+            // set a = abs(l4)
+            TT_SFPABS(0, p_sfpu::LREG4, p_sfpu::LREG0, 0);
+            TT_SFPENCC(0, 0, 0, 0);
+
+            // Again, we emulate ctz, but this time for a only, and we don't need to store the shift amount this time.
+
+            mask = 0xffff;
+            mask_width = 16;
+
+            #pragma GCC unroll 5
+            for (int j = 0; j < 5; ++j) {
+
+                // l4 = mask
+                TT_SFPLOADI(p_sfpu::LREG4, 2, mask);
+                // l4 &= a
+                TT_SFPAND(0, p_sfpu::LREG0, p_sfpu::LREG4, 0);
+                // if (a & mask) == 0
+                TT_SFPSETCC(0, p_sfpu::LREG4, 0, 6);
+                // then, a >>= mask_width
+                TT_SFPSHFT((-mask_width) & ((1<<12)-1), p_sfpu::LREG0, p_sfpu::LREG0, 1);
+                TT_SFPENCC(0, 0, 0, 0);
+
+                mask_width >>= 1;
+                mask >>= mask_width;
+            }
+        }
+
+        //dst_reg[0] = a << sa;
+        TT_SFPSHFT(0, p_sfpu::LREG2, p_sfpu::LREG0, 0);
+        TT_SFPSTORE(p_sfpu::LREG0, 4, 3, 0);
+        //dst_reg[0] = a;
+        dst_reg++;
+    }
+}
+
+}  // namespace sfpu
+}  // namespace ckernel
---
+++ tt_metal/include/compute_kernel_api/gcd.h
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "compute_kernel_api/common_globals.h"
+#ifdef TRISC_MATH
+#include "llk_math_eltwise_binary_sfpu_gcd.h"
+#define MAIN math_main()
+#define MATH(x) x
+#else
+#define MATH(x)
+#endif
+
+namespace ckernel {
+
+ALWI void gcd_tile(uint32_t idst0, uint32_t idst1) {
+    MATH((llk_math_eltwise_binary_sfpu_gcd<APPROX>(idst0, idst1)));
+}
+
+/**
+ * Please refer to documentation for any_init.
+ */
+ALWI void gcd_tile_init() { MATH((llk_math_eltwise_binary_sfpu_gcd_init<APPROX>())); }
+
+}  // namespace ckernel
