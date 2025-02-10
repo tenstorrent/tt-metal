2e850056130ee66f083cc260d5aa7322697b18fb
Jeffrey Jiang <jjiang@tenstorrent.com>
2025-02-10 17:54:11 +0000

    move class definitions from from distributed_tensor.cpp to.hpp so they can be exposed to the pybind.cpp; add dummy void methods in .cpp to satisfy linker; add new constructors and factory methods to fix type errors

--- ttnn/cpp/ttnn/distributed/distributed_tensor.cpp
+++ ttnn/cpp/ttnn/distributed/distributed_tensor.cpp
@@ -2,14 +2,8 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include <algorithm>
-
-#include "ttnn/distributed/api.hpp"
 #include "ttnn/distributed/distributed_tensor.hpp"
-#include <tt-metalium/assert.hpp>
-#include "ttnn/distributed/distributed_tensor_config.hpp"
-#include "ttnn/distributed/types.hpp"
-#include "ttnn/tensor/xtensor/partition.hpp"
+#include "tt-metalium/assert.hpp"
 
 namespace ttnn::distributed {
 namespace {
