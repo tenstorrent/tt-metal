13fb7a80633734810a39ecfaeb044790fdc805ac
Nigel Huang <nhuang@tenstorrent.com>
2025-02-11 07:00:34 +0000

    #0: increase test vc/mux demux thresholds

--- tests/tt_metal/tt_metal/perf_microbenchmark/routing/test_vc_mux_demux.cpp
+++ tests/tt_metal/tt_metal/perf_microbenchmark/routing/test_vc_mux_demux.cpp
@@ -593,10 +593,10 @@ int main(int argc, char **argv) {
                 && (demux_queue_size_bytes >= 0x20000)) {
                     double target_bandwidth = 0;
                     if (max_packet_size_words >= 1024) {
-                        target_bandwidth = 11;
+                        target_bandwidth = 17;
                         log_info(LogTest, "Perf check for pkt size >= 1024 words");
                     } else if (max_packet_size_words >= 256) {
-                        target_bandwidth = 3;
+                        target_bandwidth = 7;
                         log_info(LogTest, "Perf check for pkt size >= 256 words");
                     }
                     if (mux_bw < target_bandwidth) {
