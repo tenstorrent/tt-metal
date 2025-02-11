c2434be0009673361b4b740d4b8039a69ecff043
Jason Davies <jason@jasondavies.com>
2025-02-11 08:50:49 +0000

    Add documentation for gcd_tiles.

--- tt_metal/include/compute_kernel_api/gcd.h
+++ tt_metal/include/compute_kernel_api/gcd.h
@@ -15,6 +15,23 @@
 
 namespace ckernel {
 
+// clang-format off
+/**
+ * Performs an elementwise GCD operation on two inputs: y = gcd(x0, x1).
+ * Both inputs must be int32.
+ * Output overwrites first operand in DST.
+ *
+ * The DST register buffer must be in acquired state via *acquire_dst* call. This call is blocking and is only available
+ * on the compute engine.
+ *
+ * Return value: None
+ *
+ * | Argument       | Description                                                           | Type     | Valid Range                                           | Required |
+ * |----------------|-----------------------------------------------------------------------|----------|-------------------------------------------------------|----------|
+ * | idst0          | The index of the tile in DST register buffer to use as first operand  | uint32_t | Must be less than the size of the DST register buffer | True     |
+ * | idst1          | The index of the tile in DST register buffer to use as second operand | uint32_t | Must be less than the size of the DST register buffer | True     |
+ */
+ // clang-format on
 ALWI void gcd_tile(uint32_t idst0, uint32_t idst1) {
     MATH((llk_math_eltwise_binary_sfpu_gcd<APPROX>(idst0, idst1)));
 }
