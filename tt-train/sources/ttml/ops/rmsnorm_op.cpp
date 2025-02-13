ec14446d7acef55d1390f6551adb3da900eef170
Jay Kruer <jkruer@tenstorrent.com>
2025-02-13 06:18:11 +0000

    drop use of TT_ASSERT, add some shape annotations

--- tt-train/sources/ttml/ops/rmsnorm_op.cpp
+++ tt-train/sources/ttml/ops/rmsnorm_op.cpp
@@ -9,6 +9,7 @@
 #include <cstdint>
 #include <initializer_list>
 #include <optional>
+#include <stdexcept>
 #include <ttnn/operations/eltwise/unary/unary.hpp>
 
 #include "autograd/auto_context.hpp"
@@ -23,36 +24,42 @@
 namespace ttml::ops {
 
 autograd::TensorPtr rmsnorm(const autograd::TensorPtr &tensor, const autograd::TensorPtr &gamma, float epsilon) {
+    // let [B,N,H,W] = tensor.logical_shape()
     auto device = &autograd::ctx().get_device();
-    ttnn::Tensor squares = ttnn::square(tensor->get_value());
+    ttnn::Tensor squares = ttnn::square(tensor->get_value());  // [B,N,H,W]
     std::array<uint32_t, 4> eps_shape = {1, 1, 1, 1};
     ttnn::Tensor eps_tensor = core::from_vector({epsilon}, core::create_shape(eps_shape), device);
-    ttnn::Tensor seq_means_of_squares = ttnn::mean(squares, /*dim_arg=*/-1, /*keep_dim=*/true); /* [N,C,H,1] */
-    ttnn::Tensor seq_means_of_squares_plus_epsilon = ttnn::experimental::add(seq_means_of_squares, eps_tensor);
-    ttnn::Tensor rms_a = ttnn::sqrt(seq_means_of_squares_plus_epsilon);
-    ttnn::Tensor gamma_times_activations = ttnn::experimental::mul(gamma->get_value(), tensor->get_value());
-    ttnn::Tensor out_tensor = ttnn::experimental::div(gamma_times_activations, rms_a);
+    ttnn::Tensor seq_means_of_squares = ttnn::mean(squares, /*dim_arg=*/-1, /*keep_dim=*/true);  // [B,N,H,1]
+    std::cout << "mean of square shape: " << seq_means_of_squares.logical_shape() << std::endl;
+    ttnn::Tensor seq_means_of_squares_plus_epsilon =
+        ttnn::experimental::add(seq_means_of_squares, eps_tensor);       // [B,N,H,1]
+    ttnn::Tensor rms_a = ttnn::sqrt(seq_means_of_squares_plus_epsilon);  // [B,N,H,1]
+    ttnn::Tensor gamma_times_activations =
+        ttnn::experimental::mul(gamma->get_value(), tensor->get_value());               // [B,N,H,W]
+    ttnn::Tensor out_tensor = ttnn::experimental::div(gamma_times_activations, rms_a);  // [B,N,H,W]
 
     auto out = autograd::create_tensor(out_tensor);
 
     autograd::GradFunction grad = [tensor, gamma, out, rms_a, eps_tensor, device]() {
-        auto a = tensor->get_value();
-        auto g = gamma->get_value();
+        auto a = tensor->get_value();  // [B,N,H,W]
+        auto g = gamma->get_value();   // [B,N,H,W]
         auto n = static_cast<float>(a.logical_shape()[-1]);
-        auto dL_dout = out->get_grad();
+        auto dL_dout = out->get_grad();  // Grad w.r.t normalized arctivations, hence [B,N,H,W]
 
-        TT_ASSERT(
-            std::ranges::all_of(rms_a.logical_shape().view(), [](const auto &d) { return d == 1; }),
-            "Expected a scalar in RMS(a, epsilon).");
+        auto rms_a_all_dims_one =
+            std::ranges::all_of(rms_a.logical_shape().view(), [](const auto &d) { return d == 1; });
+        if (!rms_a_all_dims_one) {
+            throw std::runtime_error{"Expected a scalar in RMS(a, epsilon)."};
+        }
 
         auto outer =
             ttnn::matmul(a, a, /*transpose_a=*/true, /*transpose_b=*/false);  // row vectors so the order is flipped
-        TT_ASSERT(
-            [=]() {
-                auto outer_shape = outer.logical_shape();
-                return (outer_shape[-1] == static_cast<int>(n) && outer_shape[-2] == static_cast<int>(n));
-            }(),
-            "Expected outer product to be an n-square tensor.");
+
+        auto outer_shape = outer.logical_shape();
+        auto outer_is_square = outer_shape[-1] == static_cast<int>(n) && outer_shape[-2] == static_cast<int>(n);
+        if (!outer_is_square) {
+            throw std::runtime_error{"Expected outer product to be a square tensor of order n."};
+        }
 
         auto ms_a = ttnn::square(rms_a);
         auto n_by_ms_a = ttnn::experimental::mul(ms_a, n);
