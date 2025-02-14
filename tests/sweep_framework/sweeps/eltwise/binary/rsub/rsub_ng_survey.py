88d407d74d56b6a9dbdfdc4d3fdbe8fa570b4374
mcw-anasuya <mcw-anair@ext.tenstorrent.com>
2025-02-14 09:42:39 +0000

    #17829: Test mixed dtypes - rsub

--- tests/sweep_framework/sweeps/eltwise/binary/rsub/rsub_ng_survey.py
+++ tests/sweep_framework/sweeps/eltwise/binary/rsub/rsub_ng_survey.py
@@ -23,19 +23,27 @@ from models.utility_functions import torch_random
 # Each suite has a key name (in this case "suite_1") which will associate the test vectors to this specific suite of inputs.
 # Developers can create their own generator functions and pass them to the parameters as inputs.
 parameters = {
-    "rsub_bf16_1": {
-        "input_shape": [{"self": [1, 1, 1024, 1024], "other": [1, 1, 1024, 1024]}],
-        # "input_shape": [{"self": [1, 1, 512, 512], "other": [1, 1, 512, 512]}],  # for float32 and int32 dtypes
-        "input_a_dtype": [ttnn.bfloat16],
-        "input_b_dtype": [ttnn.bfloat16],
-        # "input_a_dtype": [ttnn.float32],
-        # "input_b_dtype": [ttnn.float32],
-        # "input_a_dtype": [ttnn.int32],
-        # "input_b_dtype": [ttnn.int32],
-        # "input_a_dtype": [ttnn.bfloat8_b],
-        # "input_b_dtype": [ttnn.bfloat8_b],
-        # "input_a_dtype": [ttnn.bfloat4_b],
-        # "input_b_dtype": [ttnn.bfloat4_b],
+    "rsub_mixed_1": {
+        # "input_shape": [{"self": [1, 1, 1024, 1024], "other": [1, 1, 1024, 1024]}],
+        "input_shape": [{"self": [1, 1, 512, 512], "other": [1, 1, 512, 512]}],  # for float32 and int32 dtypes
+        "input_dtype": [
+            # {"input_a_dtype": "ttnn.bfloat16", "input_b_dtype": "ttnn.bfloat16"},
+            # {"input_a_dtype": "ttnn.float32", "input_b_dtype": "ttnn.float32"},
+            # {"input_a_dtype": "ttnn.bfloat8_b", "input_b_dtype": "ttnn.bfloat8_b"},
+            # {"input_a_dtype": "ttnn.bfloat4_b", "input_b_dtype": "ttnn.bfloat4_b"},
+            {"input_a_dtype": "ttnn.bfloat16", "input_b_dtype": "ttnn.float32"},
+            {"input_a_dtype": "ttnn.bfloat16", "input_b_dtype": "ttnn.bfloat8_b"},
+            {"input_a_dtype": "ttnn.bfloat16", "input_b_dtype": "ttnn.bfloat4_b"},
+            {"input_a_dtype": "ttnn.float32", "input_b_dtype": "ttnn.bfloat16"},
+            {"input_a_dtype": "ttnn.float32", "input_b_dtype": "ttnn.bfloat8_b"},
+            {"input_a_dtype": "ttnn.float32", "input_b_dtype": "ttnn.bfloat4_b"},
+            {"input_a_dtype": "ttnn.bfloat8_b", "input_b_dtype": "ttnn.float32"},
+            {"input_a_dtype": "ttnn.bfloat8_b", "input_b_dtype": "ttnn.bfloat16"},
+            {"input_a_dtype": "ttnn.bfloat8_b", "input_b_dtype": "ttnn.bfloat4_b"},
+            {"input_a_dtype": "ttnn.bfloat4_b", "input_b_dtype": "ttnn.float32"},
+            {"input_a_dtype": "ttnn.bfloat4_b", "input_b_dtype": "ttnn.bfloat16"},
+            {"input_a_dtype": "ttnn.bfloat4_b", "input_b_dtype": "ttnn.bfloat8_b"},
+        ],
         "input_a_layout": [ttnn.TILE_LAYOUT],
         "input_b_layout": [ttnn.TILE_LAYOUT],
         "input_mem_config": [
@@ -66,6 +74,17 @@ parameters = {
 }
 
 
+def return_dtype(dtype):
+    if dtype == "ttnn.bfloat16":
+        return ttnn.bfloat16
+    elif dtype == "ttnn.float32":
+        return ttnn.float32
+    elif dtype == "ttnn.bfloat8_b":
+        return ttnn.bfloat8_b
+    elif dtype == "ttnn.bfloat4_b":
+        return ttnn.bfloat4_b
+
+
 def return_mem_config(mem_config_string):
     if mem_config_string == "l1_interleaved":
         return ttnn.L1_MEMORY_CONFIG
@@ -73,8 +92,8 @@ def return_mem_config(mem_config_string):
         return ttnn.DRAM_MEMORY_CONFIG
     elif mem_config_string == "l1_height_sharded_rm":
         return ttnn.create_sharded_memory_config(
-            # shape=(512 // 8, 512),
-            shape=(1024 // 8, 1024),
+            shape=(512 // 8, 512),
+            # shape=(1024 // 8, 1024),
             core_grid=ttnn.CoreGrid(y=2, x=4),
             strategy=ttnn.ShardStrategy.HEIGHT,
             orientation=ttnn.ShardOrientation.ROW_MAJOR,
@@ -82,8 +101,8 @@ def return_mem_config(mem_config_string):
         )
     elif mem_config_string == "l1_height_sharded_cm":
         return ttnn.create_sharded_memory_config(
-            # shape=(512, 512 // 8),
-            shape=(1024, 1024 // 8),
+            shape=(512, 512 // 8),
+            # shape=(1024, 1024 // 8),
             core_grid=ttnn.CoreGrid(y=2, x=4),
             strategy=ttnn.ShardStrategy.HEIGHT,
             orientation=ttnn.ShardOrientation.COL_MAJOR,
@@ -91,8 +110,8 @@ def return_mem_config(mem_config_string):
         )
     elif mem_config_string == "l1_width_sharded_rm":
         return ttnn.create_sharded_memory_config(
-            # shape=(512, 512 // 8),
-            shape=(1024, 1024 // 8),
+            shape=(512, 512 // 8),
+            # shape=(1024, 1024 // 8),
             core_grid=ttnn.CoreGrid(y=2, x=4),
             strategy=ttnn.ShardStrategy.WIDTH,
             orientation=ttnn.ShardOrientation.ROW_MAJOR,
@@ -100,8 +119,8 @@ def return_mem_config(mem_config_string):
         )
     elif mem_config_string == "l1_width_sharded_cm":
         return ttnn.create_sharded_memory_config(
-            # shape=(512 // 8, 512),
-            shape=(1024 // 8, 1024),
+            shape=(512 // 8, 512),
+            # shape=(1024 // 8, 1024),
             core_grid=ttnn.CoreGrid(y=2, x=4),
             strategy=ttnn.ShardStrategy.WIDTH,
             orientation=ttnn.ShardOrientation.COL_MAJOR,
@@ -109,8 +128,8 @@ def return_mem_config(mem_config_string):
         )
     elif mem_config_string == "l1_block_sharded_rm":
         return ttnn.create_sharded_memory_config(
-            # shape=(512 // 2, 512 // 4),
-            shape=(1024 // 2, 1024 // 4),
+            shape=(512 // 2, 512 // 4),
+            # shape=(1024 // 2, 1024 // 4),
             core_grid=ttnn.CoreGrid(y=2, x=4),
             strategy=ttnn.ShardStrategy.BLOCK,
             orientation=ttnn.ShardOrientation.ROW_MAJOR,
@@ -118,8 +137,8 @@ def return_mem_config(mem_config_string):
         )
     elif mem_config_string == "l1_block_sharded_cm":
         return ttnn.create_sharded_memory_config(
-            # shape=(512 // 2, 512 // 4),
-            shape=(1024 // 2, 1024 // 4),
+            shape=(512 // 2, 512 // 4),
+            # shape=(1024 // 2, 1024 // 4),
             core_grid=ttnn.CoreGrid(y=2, x=4),
             strategy=ttnn.ShardStrategy.BLOCK,
             orientation=ttnn.ShardOrientation.COL_MAJOR,
@@ -134,8 +153,7 @@ def return_mem_config(mem_config_string):
 # If you defined a device_mesh_fixture above, the object you yielded will be passed into this function as 'device'. Otherwise, it will be the default ttnn device opened by the infra.
 def run(
     input_shape,
-    input_a_dtype,
-    input_b_dtype,
+    input_dtype,
     input_a_layout,
     input_b_layout,
     input_mem_config,
@@ -144,16 +162,18 @@ def run(
 ) -> list:
     torch.manual_seed(0)
 
+    input_a_dtype = return_dtype(input_dtype["input_a_dtype"])
+    input_b_dtype = return_dtype(input_dtype["input_b_dtype"])
     torch_input_tensor_a = gen_func_with_cast_tt(
-        partial(torch_random, low=-100, high=100, dtype=torch.bfloat16), input_a_dtype
+        partial(torch_random, low=-100, high=100, dtype=torch.float32), input_a_dtype
     )(input_shape["self"])
 
     if isinstance(input_shape["other"], list):
         torch_input_tensor_b = gen_func_with_cast_tt(
-            partial(torch_random, low=-100, high=100, dtype=torch.bfloat16), input_b_dtype
+            partial(torch_random, low=-100, high=100, dtype=torch.float32), input_b_dtype
         )(input_shape["other"])
     else:
-        torch_input_tensor_b = torch.tensor(input_shape["other"], dtype=torch.bfloat16)
+        torch_input_tensor_b = torch.tensor(input_shape["other"], dtype=torch.float32)
 
     input_a_memory_config = input_mem_config["a_mem"]
     input_b_memory_config = input_mem_config["b_mem"]
@@ -174,17 +194,8 @@ def run(
         memory_config=return_mem_config(input_b_memory_config),
     )
 
-    if input_a_dtype == ttnn.bfloat8_b:
-        torch_input_tensor_a = ttnn.to_torch(input_tensor_a)
-
-    if input_b_dtype == ttnn.bfloat8_b:
-        torch_input_tensor_b = ttnn.to_torch(input_tensor_b)
-
-    if input_a_dtype == ttnn.bfloat4_b:
-        torch_input_tensor_a = ttnn.to_torch(input_tensor_a)
-
-    if input_b_dtype == ttnn.bfloat4_b:
-        torch_input_tensor_b = ttnn.to_torch(input_tensor_b)
+    torch_input_tensor_a = ttnn.to_torch(input_tensor_a)
+    torch_input_tensor_b = ttnn.to_torch(input_tensor_b)
 
     golden_function = ttnn.get_golden_function(ttnn.experimental.rsub)
     torch_output_tensor = golden_function(torch_input_tensor_a, torch_input_tensor_b)
@@ -193,5 +204,10 @@ def run(
     result = ttnn.experimental.rsub(input_tensor_a, input_tensor_b)
     output_tensor = ttnn.to_torch(result)
     e2e_perf = stop_measuring_time(start_time)
+    print(torch_output_tensor)
+    print(output_tensor)
+    diff = torch.abs(torch_output_tensor - output_tensor)
+    max_atol = torch.max(diff)
+    print(f"Max absolute tolerance (atol): {max_atol.item()}")
 
     return [check_with_pcc(torch_output_tensor, output_tensor, pcc=0.99), e2e_perf]
