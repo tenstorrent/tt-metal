18b6c6652b18a989f6d1f348120f0bd69bea69d0
Pavle Josipovic <pjosipovic@tenstorrent.com>
2025-02-16 22:12:18 +0000

    Remove non tile multiple width/height from conv2d
    These two features are non critical for conv2d
    meaning they don't contribute to enabling any model
    perf on any model or improve pass rate on any sweep.
    
    Problem with these features is that they kick in
    in very unpredictable conditions for both users and
    developers as they have many limits/conditions.
    
    They are adding to conv2d test matrix, but they are
    hard to test for as deriving tests that will trigger
    them on multiple hw platforms is not easy.
    
    Moreover they are source of bugs like #17647, and it's
    often non obvious that bugs originate from these features
    and when faced with a bug in conv2d first thing is to go to the
    code and manually disable them to check for that.
    
    For the reasons above these will get removed, and by
    removing them #17647 will be fixed.

--- ttnn/cpp/ttnn/operations/conv/conv2d/prepare_conv2d_weights.hpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/prepare_conv2d_weights.hpp
@@ -117,8 +117,7 @@ std::pair<ttnn::Tensor, std::optional<ttnn::Tensor>> prepare_conv_weights_biases
     uint32_t groups,
     uint32_t act_block_h_ntiles,
     uint32_t input_width,
-    const bool parameters_on_device = true,
-    bool is_non_tile_mul_width = false);
+    const bool parameters_on_device = true);
 
 }  // namespace conv2d
 }  // namespace operations::conv
