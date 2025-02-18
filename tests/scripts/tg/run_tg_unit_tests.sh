76596070df47545d07a03a7ae07d5c66c7c2fc63
Andrew Fuller <afuller@tenstorrent.com>
2025-02-18 16:27:42 -0500

    Afuller/rm rf archname (#17894)
    ### Ticket
    A step towards #17851
    
    ### Problem description
    Some lines in these tests weren't using HAL and thus had to be built
    1x/arch and produced multiple binaries.
    
    ### What's changed
    Switch to HAL and drop the arch-specific binaries.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI
    [passes](https://github.com/tenstorrent/tt-metal/actions/runs/13381523927)

--- tests/scripts/tg/run_tg_unit_tests.sh
+++ tests/scripts/tg/run_tg_unit_tests.sh
@@ -105,11 +105,11 @@ run_tg_prefetcher_tests() {
 run_tg_tests() {
   if [[ "$1" == "unit" ]]; then
     echo "LOG_METAL: running run_tg_unit_tests"
-    TT_METAL_ENABLE_REMOTE_CHIP=1 ./build/test/tt_metal/unit_tests_dispatch_${ARCH_NAME} --gtest_filter="CommandQueueSingleCard*Fixture.*"
+    TT_METAL_ENABLE_REMOTE_CHIP=1 ./build/test/tt_metal/unit_tests_dispatch --gtest_filter="CommandQueueSingleCard*Fixture.*"
     ./build/test/ttnn/galaxy_unit_tests_ttnn
     TT_METAL_SLOW_DISPATCH_MODE=1 ./build/test/tt_metal/unit_tests_device --gtest_filter="GalaxyFixture.*:TGFixture.*"
     ./build/test/tt_metal/unit_tests_device --gtest_filter="GalaxyFixture.*:TGFixture.*"
-    TT_METAL_GTEST_NUM_HW_CQS=2 ./build/test/tt_metal/unit_tests_dispatch_${ARCH_NAME} --gtest_filter="MultiCommandQueueMultiDevice*Fixture.*"
+    TT_METAL_GTEST_NUM_HW_CQS=2 ./build/test/tt_metal/unit_tests_dispatch --gtest_filter="MultiCommandQueueMultiDevice*Fixture.*"
 
   elif [[ "$1" == "fabric" ]]; then
     echo "LOG_FABRIC: running run_tg_fabric_tests"
