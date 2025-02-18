9fbc458157ef311572cd0dc59a9b04aa3e345f6b
Sankar Manoj <smanoj@tenstorrent.com>
2025-02-18 16:37:33 +0000

    #0: Update license

--- tests/ttnn/unit_tests/operations/convolution/conv_test_utils.py
+++ tests/ttnn/unit_tests/operations/convolution/conv_test_utils.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
 import ttnn
 import pytest
 import torch
--- tests/ttnn/unit_tests/operations/convolution/models/test_resnet50.py
+++ tests/ttnn/unit_tests/operations/convolution/models/test_resnet50.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
 from ..conv_test_utils import *
 
 from loguru import logger
