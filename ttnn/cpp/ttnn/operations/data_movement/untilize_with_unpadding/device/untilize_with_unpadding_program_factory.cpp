df65ff425c49862e024796f2049eed9b582181f2
Nour Ardo <nardo@tenstorrent.com>
2025-02-19 19:19:37 +0000

    test another threshold

--- ttnn/cpp/ttnn/operations/data_movement/untilize_with_unpadding/device/untilize_with_unpadding_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/untilize_with_unpadding/device/untilize_with_unpadding_program_factory.cpp
@@ -670,15 +670,37 @@ operation::ProgramWithCallbacks untilize_with_unpadding_multi_core_interleaved(
 
     uint32_t num_tiles_per_col = a.get_padded_shape()[-2] / TILE_HEIGHT;
 
+    auto [ncores, all_cores, core_range, core_range_cliff, nblocks_per_core, nblocks_per_core_cliff] =
+        ttnn::split_blocks_for_tilize(grid_size, num_blocks);
+
     constexpr uint32_t threshold_row_block = 32;
     if (num_tiles_per_row > threshold_row_block) {
         if (num_tiles_per_col > threshold_row_block || num_tiles_per_row > num_tiles_per_col) {
-            return untilize_with_unpadding_multi_core_block_interleaved(a, output, use_pack_untilize, fp32_dest_acc_en);
+            uint32_t num_blocks_block =
+                (a.get_padded_shape()[-1] * a.get_padded_shape()[-2]) / (TILE_HEIGHT * TILE_WIDTH);
+
+            auto
+                [ncores_block,
+                 all_cores_block,
+                 core_range_block,
+                 cliff_row_core_range,
+                 cliff_col_core_range,
+                 cliff_col_row_core_range,
+                 nblocks_per_core_block,
+                 single_block_size,
+                 single_block_size_cliff_row,
+                 single_block_size_cliff_col,
+                 has_cliff_row,
+                 has_cliff_col,
+                 full_cores_per_row,
+                 full_cores_per_col] =
+                    ttnn::split_blocks_for_tilize_wh(grid_size, num_blocks_block, num_tiles_per_row, num_tiles_per_col);
+            if (ncores < ncores_block) {
+                return untilize_with_unpadding_multi_core_block_interleaved(
+                    a, output, use_pack_untilize, fp32_dest_acc_en);
+            }
         }
     }
-
-    auto [ncores, all_cores, core_range, core_range_cliff, nblocks_per_core, nblocks_per_core_cliff] =
-        ttnn::split_blocks_for_tilize(grid_size, num_blocks);
 
     bool has_cliff = core_range_cliff.size() > 0;
 
