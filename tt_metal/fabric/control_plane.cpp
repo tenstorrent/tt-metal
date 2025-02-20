d670b2391fdf96ee058f7cc4eb897346c0d13691
Allan Liu <aliu@tenstorrent.com>
2025-02-20 18:23:26 +0000

    Decouple control plane init and configuring routing tables

--- tt_metal/fabric/control_plane.cpp
+++ tt_metal/fabric/control_plane.cpp
@@ -52,7 +52,6 @@ ControlPlane::ControlPlane(const std::string& mesh_graph_desc_file) {
     this->routing_table_generator_->print_routing_tables();
 
     this->initialize_from_mesh_graph_desc_file(mesh_graph_desc_file);
-    this->configure_routing_tables();
 
     // Printing, only enabled with log_debug
     this->print_ethernet_channels();
