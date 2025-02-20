0f94ecdef8d0d8acc97cc893aee1d478f1e84237
Stanislav Minakov <sminakov@tenstorrent.com>
2025-02-20 19:53:06 +0000

    Update reshape_view C++ API (#18080)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/17720
    
    ### Problem description
    Currently reshape view C++ API is inconsistent with other operations,
    not allowing to call it specifying memory_config and not specifying
    queue id, which creates some issues for tt-mlir
    
    ### What's changed
    Changed reshape_view invoke calls, making QueueId the first argument. In
    this case decorators automatically allow the calls both with and without
    QueueId specified.
    
    ### Checklist
    - [x] [All post commit CI
    passes](https://github.com/tenstorrent/tt-metal/actions/runs/13429855903)
    - [x] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/data_movement/reshape_view/reshape.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/reshape_view/reshape.hpp
@@ -69,28 +69,24 @@ ttnn::Tensor PerformView(
 
 struct ReshapeViewOperation {
     static ttnn::Tensor invoke(
+        const QueueId queue_id,
         const ttnn::Tensor& input_tensor,
         const ttnn::Shape& logical_shape,
-        const std::optional<MemoryConfig>& memory_config,
-        const QueueId queue_id,
-        const std::optional<PadValue>& pad_value);
+        const std::optional<MemoryConfig>& memory_config = std::nullopt,
+        const std::optional<PadValue>& pad_value = std::nullopt);
     static ttnn::Tensor invoke(
+        const QueueId queue_id,
         const ttnn::Tensor& input_tensor,
         const ttnn::Shape& logical_shape,
         const ttnn::Shape& padded_shape,
-        const std::optional<MemoryConfig>& memory_config,
-        const QueueId queue_id,
-        const std::optional<PadValue>& pad_value);
+        const std::optional<MemoryConfig>& memory_config = std::nullopt,
+        const std::optional<PadValue>& pad_value = std::nullopt);
     static ttnn::Tensor invoke(
+        const QueueId queue_id,
         const ttnn::Tensor& input_tensor,
         tt::stl::Span<const int32_t> shape_vector,
-        const std::optional<MemoryConfig>& memory_config,
-        const QueueId queue_id,
-        const std::optional<PadValue>& pad_value);
-    static ttnn::Tensor invoke(const ttnn::Tensor& input_tensor, const ttnn::Shape& logical_shape);
-    static ttnn::Tensor invoke(
-        const ttnn::Tensor& input_tensor, const ttnn::Shape& logical_shape, const ttnn::Shape& padded_shape);
-    static ttnn::Tensor invoke(const ttnn::Tensor& input_tensor, tt::stl::Span<const int32_t> shape_vector);
+        const std::optional<MemoryConfig>& memory_config = std::nullopt,
+        const std::optional<PadValue>& pad_value = std::nullopt);
 };
 
 }  // namespace operations::data_movement
