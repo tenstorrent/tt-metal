3a8b3fea15b3795b61086f75a6b301773c8a0216
Jeffrey Jiang <jjiang@tenstorrent.com>
2025-02-20 17:16:07 +0000

    add back distributed.py for now, clean up class overloads

--- ttnn/cpp/ttnn/distributed/distributed_tensor.cpp
+++ ttnn/cpp/ttnn/distributed/distributed_tensor.cpp
@@ -44,6 +44,7 @@ std::unique_ptr<TensorToMesh> shard_tensor_to_2d_mesh_mapper(
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     return std::make_unique<ShardTensorTo2dMesh>(mesh_shape[0], mesh_shape[1], config);
 =======
     return std::make_unique<ShardTensor2dMesh>(mesh_shape, config);
@@ -54,6 +55,9 @@ std::unique_ptr<TensorToMesh> shard_tensor_to_2d_mesh_mapper(
 =======
     return std::make_unique<ShardTensor2dMesh>(mesh_shape, config);
 >>>>>>> fix naming errors, add tests, add imports - TODO, fix weird aliasing error with meshdevice vs ttnn.multidevice.meshdevice
+=======
+    return std::make_unique<ShardTensorTo2dMesh>(mesh_shape, config);
+>>>>>>> add back distributed.py for now, clean up class overloads
 }
 
 std::unique_ptr<MeshToTensor> concat_mesh_to_tensor_composer(int dim) {
@@ -69,6 +73,7 @@ std::unique_ptr<MeshToTensor> concat_2d_mesh_to_tensor_composer(MeshDevice& mesh
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     TT_FATAL(mesh_device.shape().dims() == 2, "Mesh device is not configured as a 2D mesh: {}", mesh_device.shape());
     return std::make_unique<Concat2dMeshToTensor>(mesh_device.shape()[0], mesh_device.shape()[1], config);
 =======
@@ -80,6 +85,9 @@ std::unique_ptr<MeshToTensor> concat_2d_mesh_to_tensor_composer(MeshDevice& mesh
 =======
     return std::make_unique<ConcatMesh2dToTensor>(mesh_device, config);
 >>>>>>> fix naming errors, add tests, add imports - TODO, fix weird aliasing error with meshdevice vs ttnn.multidevice.meshdevice
+=======
+    return std::make_unique<Concat2dMeshToTensor>(mesh_device, config);
+>>>>>>> add back distributed.py for now, clean up class overloads
 }
 
 Tensor distribute_tensor(
