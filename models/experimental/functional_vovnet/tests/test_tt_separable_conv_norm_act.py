928c0c22465904c5d8f30ae1d99abfa810998226
Sudharsan-V <sudharsan.vijayaraghavan@multicorewareinc.com>
2025-02-21 13:15:44 +0000

    #0: Add ttnn support for vovnet model- WIP

---
+++ models/experimental/functional_vovnet/tests/test_tt_separable_conv_norm_act.py
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+import pytest
+import timm
+
+import ttnn
+from tests.ttnn.utils_for_testing import assert_with_pcc
+
+
+from models.experimental.functional_vovnet.tt.separable_conv_norm_act import TtSeparableConvNormAct
+
+
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 16384}], indirect=True)
+def test_separable_conv_norm_act_inference(device, reset_seeds):
+    base_address = f"stem.1"
+
+    model = timm.create_model("hf_hub:timm/ese_vovnet19b_dw.ra_in1k", pretrained=True)
+
+    torch_model = model.stem[1]
+
+    tt_model = TtSeparableConvNormAct(
+        stride=1,
+        padding=1,
+        torch_model=model.state_dict(),
+        base_address=base_address,
+        device=device,
+    )
+
+    input = torch.randn(1, 64, 112, 112)
+
+    # run torch model
+    model_output = torch_model(input)
+    # run tt model
+
+    tt_input = ttnn.from_torch(input, device=device, dtype=ttnn.bfloat16)
+    tt_output = tt_model.forward(tt_input)
+    tt_output_torch = ttnn.to_torch(tt_output[0])
+    assert_with_pcc(model_output, tt_output_torch, 0.99)
---
+++ models/experimental/functional_vovnet/tt/vovnet.py
@@ -0,0 +1,82 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+
+import ttnn
+
+from models.experimental.functional_vovnet.tt.classifier_head import TtClassifierHead
+from models.experimental.functional_vovnet.tt.conv_norm_act import TtConvNormAct
+from models.experimental.functional_vovnet.tt.osa_stage import TtOsaStage
+from models.experimental.functional_vovnet.tt.separable_conv_norm_act import TtSeparableConvNormAct
+
+
+class TtVoVNet:
+    def __init__(
+        self,
+        num_classes=1000,
+        device=None,
+        # torch_model=None,
+        parameters=None,
+        base_address=None,
+    ):
+        self.num_classes = num_classes
+
+        self.device = device
+        # self.torch_model = torch_model
+        self.base_address = base_address
+
+        self.stem = [
+            TtConvNormAct(
+                stride=2,
+                base_address=f"stem.0",
+                device=self.device,
+                parameters=parameters,
+            ),
+            TtSeparableConvNormAct(
+                base_address=f"stem.1",
+                device=device,
+                parameters=parameters,
+                stride=1,
+                padding=1,
+            ),
+            TtSeparableConvNormAct(
+                base_address=f"stem.2",
+                device=device,
+                parameters=parameters,
+                stride=2,
+                padding=1,
+            ),
+        ]
+
+        self.stages = []
+
+        downsample = False
+        for i in range(4):
+            self.stages += [
+                TtOsaStage(
+                    block_per_stage=1,
+                    downsample=downsample,
+                    base_address=f"stages.{i}",
+                    # torch_model=self.torch_model,
+                    parameters=parameters,
+                    device=self.device,
+                )
+            ]
+            downsample = True
+
+        self.head = TtClassifierHead(
+            base_address=f"head",
+            device=self.device,
+            # torch_model=self.torch_model,
+            parameters=parameters,
+        )
+
+    def forward(self, x: ttnn.Tensor) -> ttnn.Tensor:
+        for i, module in enumerate(self.stem):
+            x = module.forward(x)[0]
+        for i, module in enumerate(self.stages):
+            x = module.forward(x)
+
+        x = self.head.forward(x)
+        return x
