c674c26e79c251e19723309744ceb2b65b1a36f2
David Ma <davidma@tenstorrent.com>
2025-02-21 19:09:05 +0000

    #0: Suppress device init warnings after the first to avoid spam

--- tt_metal/impl/device/device.cpp
+++ tt_metal/impl/device/device.cpp
@@ -961,7 +961,10 @@ bool Device::initialize(const uint8_t num_hw_cqs, size_t l1_small_size, size_t t
 
 void Device::push_work(std::function<void()> work, bool blocking) {
     if (not this->initialized_) {
-        log_warning("Attempting to push work to Device {} which is not initialized. Ignoring...", this->id_);
+        if (!uninitialized_error_fired_) {
+            log_fatal("Attempting to push work to Device {} which is not initialized. Ignoring...", this->id_);
+            uninitialized_error_fired_ = true;
+        }
         return;
     }
     this->work_executor_.push_work(std::move(work), blocking);
