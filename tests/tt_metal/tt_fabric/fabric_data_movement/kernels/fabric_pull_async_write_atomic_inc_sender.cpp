aedbb978517152dc5f077045f54c2cb6676dd2c0
Austin Ho <aho@tenstorrent.com>
2025-02-24 17:03:49 +0000

    #0: Update fabric api unit tests

--- tests/tt_metal/tt_fabric/fabric_data_movement/kernels/fabric_pull_async_write_atomic_inc_sender.cpp
+++ tests/tt_metal/tt_fabric/fabric_data_movement/kernels/fabric_pull_async_write_atomic_inc_sender.cpp
@@ -1,30 +1,17 @@
-// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 //
 // SPDX-License-Identifier: Apache-2.0
 
-// clang-format off
 #include "dataflow_api.h"
-#include "debug/dprint.h"
-#include "tt_fabric/hw/inc/tt_fabric.h"
-#include "tt_fabric/hw/inc/tt_fabric_interface.h"
-#include "tt_fabric/hw/inc/tt_fabric_api.h"
+#include "tt_metal/fabric/hw/inc/tt_fabric_api.h"
+#include "tt_metal/fabric/hw/inc/tt_fabric_interface.h"
 #include "tests/tt_metal/tt_metal/perf_microbenchmark/common/kernel_utils.hpp"
-
-// clang-format on
 
 using namespace tt::tt_fabric;
 
-volatile fabric_client_interface_t* client_interface;
-
-uint64_t xy_local_addr;
-
 void kernel_main() {
+    constexpr uint32_t client_interface_cb = get_compile_time_arg_val(0);
     uint32_t rt_args_idx = 0;
-    // Fabric configuration specific arguments
-    uint32_t client_interface_addr = get_arg_val<uint32_t>(increment_arg_idx(rt_args_idx));
-    uint32_t gk_interface_addr_l = get_arg_val<uint32_t>(increment_arg_idx(rt_args_idx));
-    uint32_t gk_interface_addr_h = get_arg_val<uint32_t>(increment_arg_idx(rt_args_idx));
-
     uint32_t src_addr = get_arg_val<uint32_t>(increment_arg_idx(rt_args_idx));
     uint32_t dst_noc_offset = get_arg_val<uint32_t>(increment_arg_idx(rt_args_idx));
     uint32_t dst_write_addr = get_arg_val<uint32_t>(increment_arg_idx(rt_args_idx));
@@ -38,7 +25,15 @@ void kernel_main() {
     uint64_t dst_write_noc_addr = get_noc_addr_helper(dst_noc_offset, dst_write_addr);
     uint64_t dst_atomic_noc_addr = get_noc_addr_helper(dst_noc_offset, dst_atomic_addr);
     uint32_t packet_size_bytes = num_bytes + PACKET_HEADER_SIZE_BYTES;
-    fabric_async_write_atomic_inc_add_header(
+
+    uint32_t client_interface_addr = get_write_ptr(client_interface_cb);
+    volatile tt_l1_ptr fabric_client_interface_t* client_interface =
+        reinterpret_cast<volatile tt_l1_ptr fabric_client_interface_t*>(client_interface_addr);
+    fabric_endpoint_init(client_interface, 0 /* unused */);
+
+    fabric_async_write_atomic_inc(
+        client_interface,
+        router_noc_xy,
         src_addr,  // source address in sender’s memory
         dst_mesh_id,
         dst_device_id,
@@ -48,14 +43,5 @@ void kernel_main() {
         atomic_inc            // atomic increment value
     );
 
-    // make sure fabric node gatekeeper is available.
-    fabric_endpoint_init<false>(client_interface_addr, gk_interface_addr_l, gk_interface_addr_h);
-
-    fabric_setup_pull_request(
-        src_addr,          // source address in sender’s memory
-        packet_size_bytes  // number of bytes to write to remote destination
-    );
-
-    fabric_send_pull_request<false>(router_noc_xy, dst_mesh_id, dst_device_id);
-    fabric_wait_for_pull_request_flushed();
+    fabric_wait_for_pull_request_flushed(client_interface);
 }
