768de9ed6d035b5947bdf84cb298e2bc0e1f0c18
Mohamed Bahnas <116673264+mbahnasTT@users.noreply.github.com>
2025-02-25 11:41:54 -0800

    fix yolov4 faster webdemo (#18178)
    ### Ticket
    Link to Github Issue
    
    ### Problem description
    Provide context for the problem.
    
    ### What's changed
    Describe the approach used to solve the problem.
    Summarize the changes made and its impact.
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/13476817943
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [ ] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: Dalar Vartanians <132954887+dvartaniansTT@users.noreply.github.com>
    Co-authored-by: Dalar Vartanians <dvartanians@tenstorrent.com>

--- models/demos/wormhole/yolov4/test_yolov4_performant_webdemo.py
+++ models/demos/wormhole/yolov4/test_yolov4_performant_webdemo.py
@@ -8,52 +8,12 @@ import time
 import torch
 
 from models.utility_functions import run_for_wormhole_b0
-from models.demos.yolov4.tests.yolov4_perfomant_webdemo import (
-    run_yolov4_inference,
-    run_yolov4_trace_inference,
-    run_yolov4_trace_2cqs_inference,
-    Yolov4Trace2CQ,
-)
-
-
-@run_for_wormhole_b0()
-@pytest.mark.parametrize("device_params", [{"l1_small_size": 24576}], indirect=True)
-@pytest.mark.parametrize(
-    "batch_size, act_dtype, weight_dtype",
-    ((1, ttnn.bfloat16, ttnn.bfloat16),),
-)
-def test_run_yolov4_inference(device, use_program_cache, batch_size, act_dtype, weight_dtype, model_location_generator):
-    run_yolov4_inference(device, batch_size, act_dtype, weight_dtype, model_location_generator)
-
-
-@run_for_wormhole_b0()
-@pytest.mark.parametrize("device_params", [{"l1_small_size": 24576, "trace_region_size": 1617920}], indirect=True)
-@pytest.mark.parametrize(
-    "batch_size, act_dtype, weight_dtype",
-    ((1, ttnn.bfloat16, ttnn.bfloat16),),
-)
-@pytest.mark.parametrize("enable_async_mode", (False, True), indirect=True)
-def test_run_yolov4_trace_inference(
-    device,
-    use_program_cache,
-    batch_size,
-    act_dtype,
-    weight_dtype,
-    enable_async_mode,
-    model_location_generator,
-):
-    run_yolov4_trace_inference(
-        device,
-        batch_size,
-        act_dtype,
-        weight_dtype,
-        model_location_generator,
-    )
+from models.demos.yolov4.tests.yolov4_perfomant_webdemo import Yolov4Trace2CQ
 
 
 @run_for_wormhole_b0()
 @pytest.mark.parametrize(
-    "device_params", [{"l1_small_size": 24576, "trace_region_size": 1617920, "num_command_queues": 2}], indirect=True
+    "device_params", [{"l1_small_size": 24576, "trace_region_size": 3211264, "num_command_queues": 2}], indirect=True
 )
 @pytest.mark.parametrize(
     "batch_size, act_dtype, weight_dtype",
