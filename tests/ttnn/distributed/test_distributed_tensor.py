8af15d15f4fc69401a1330a118646df902e830f2
Jeffrey Jiang <jjiang@tenstorrent.com>
2025-02-25 07:55:00 +0000

    start trying to fix rebase errors

--- tests/ttnn/distributed/test_distributed_tensor.py
+++ tests/ttnn/distributed/test_distributed_tensor.py
@@ -1,30 +1,51 @@
-# # SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
 
-# # SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: Apache-2.0
 
-# import torch
-# import pytest
-# import ttnn
-# from loguru import logger
-# from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc
-# from models.utility_functions import nearest_32
+import torch
+import pytest
+import ttnn
+from loguru import logger
+from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc
+from models.utility_functions import nearest_32
 
 
-# @pytest.mark.parametrize(
-#     "mesh_device",
-#     [
-#         32,
-#     ],
-#     indirect=True,
-# )
+@pytest.mark.parametrize(
+    "mesh_device",
+    [
+        32,
+    ],
+    indirect=True,
+)
+@pytest.mark.parametrize("dtype", [ttnn.uint16, ttnn.bfloat16, ttnn.bfloat4_b, ttnn.bfloat8_b, ttnn.float32])
+def test_direct_replicate_to_tensor_mesh(mesh_device, dtype):
+    torch.manual_seed(1234)
+
+    mapper = ttnn.ReplicateTensorToMesh(mesh_device)
+
+    torch_tensor = torch.randn(1, 1, 32, 256)
+    replicated_tensors = ttnn.from_torch(
+        torch_tensor,
+        dtype=dtype,
+        layout=ttnn.TILE_LAYOUT,
+        mesh_mapper = mapper,
+        device=mesh_device,
+    )
+
+    out_tensors = ttnn.get_device_tensors(replicated_tensors)
+
+    out_pass, out_pcc = comp_pcc(ttnn.to_torch(out_tensors[0]), torch_tensor, pcc=0.99)
+    logger.info(f"PCC value: {out_pcc}")
+    assert out_pass
+
 # @pytest.mark.parametrize("dtype", [ttnn.uint16, ttnn.bfloat16, ttnn.bfloat4_b, ttnn.bfloat8_b, ttnn.float32])
-# def test_direct_replicate_to_tensor_mesh(mesh_device, dtype):
+# def test_direct_shard_to_tensor_mesh(mesh_device, dtype):
 #     torch.manual_seed(1234)
 
-#     mapper = ttnn.ReplicateTensorToMesh(mesh_device)
+# mapper = ttnn.ShardTensorToMesh(mesh_device, dim=3)
 
 #     torch_tensor = torch.randn(1, 1, 32, 256)
-#     replicated_tensors = ttnn.from_torch(
+#     sharded_tensor = ttnn.from_torch(
 #         torch_tensor,
 #         dtype=dtype,
 #         layout=ttnn.TILE_LAYOUT,
@@ -32,11 +53,53 @@
 #         device=mesh_device,
 #     )
 
-#     out_tensors = ttnn.get_device_tensors(replicated_tensors)
-
-#     out_pass, out_pcc = comp_pcc(ttnn.to_torch(out_tensors[0]), torch_tensor, pcc=0.99)
+#     out_pass, out_pcc = comp_pcc(ttnn.to_torch(sharded_tensor), torch_tensor, pcc=0.99)
 #     logger.info(f"PCC value: {out_pcc}")
 #     assert out_pass
+
+# @pytest.mark.parametrize(
+#     "mesh_shape, mesh_device", [pytest.param((8, 4), (8, 4), id="8x4_grid")], indirect=["mesh_device"]
+# )
+# @pytest.mark.parametrize(
+#     "M, K, N",
+#     [pytest.param(32, 64, 128), pytest.param(32, 128, 64)],
+# )
+# @pytest.mark.parametrize("dtype", [ttnn.uint16, ttnn.bfloat16, ttnn.bfloat4_b, ttnn.bfloat8_b, ttnn.float32])
+# def test_direct_shard2d_to_tensor_mesh(M, K, N, dtype, mesh_shape, mesh_device):
+#     torch.manual_seed(1234)
+
+#     torch_tensor = torch.randn(1, 1, M, K)
+#     core_grid = ttnn.CoreGrid(y=1, x=8)
+
+#     # If K < N it's FF1-like test case, else FF2-like test case
+#     shard_dim = (0, 3) if K < N else (3, 0)
+
+#     K = K // mesh_shape[1] if K < N else K // mesh_shape[0]
+#     N = N // mesh_shape[0] if K < N else N // mesh_shape[1]
+
+#     sharded_mem_config = ttnn.create_sharded_memory_config(
+#         shape=(M // core_grid.y, K // core_grid.x),
+#         core_grid=core_grid,
+#         strategy=ttnn.ShardStrategy.WIDTH,
+#         orientation=ttnn.ShardOrientation.ROW_MAJOR,
+#         use_height_and_width_as_shard_shape=True,
+#     )
+
+#     mapper = ttnn.ShardTensor2dMesh(mesh_device, mesh_shape=mesh_shape, dims=shard_dim)
+
+#     sharded_tensor = ttnn.from_torch(
+#         torch_tensor,
+#         dtype=dtype,
+#         layout=ttnn.TILE_LAYOUT,
+#         memory_config=sharded_mem_config if M == 32 else ttnn.DRAM_MEMORY_CONFIG,
+#         mesh_mapper = mapper,
+#         device=mesh_device,
+#     )
+
+#     out_pass, out_pcc = comp_pcc(ttnn.to_torch(sharded_tensor), torch_tensor, pcc=0.99)
+#     logger.info(f"PCC value: {out_pcc}")
+#     assert out_pass
+
 
 # @pytest.mark.parametrize(
 #     "mesh_device",
