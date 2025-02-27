a282ab00ea4a4c4121861cd2d037bb892fe3424e
Saad Jameel <sjameel@tenstorrent.com>
2025-02-27 22:25:56 +0000

    add conftest fabric init

---
+++ ttnn/cpp/pybind11/fabric.hpp
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "pybind11/pybind_fwd.hpp"
+
+namespace py = pybind11;
+
+namespace ttnn::fabric {
+
+void py_bind_fabric_api(py::module& module);
+
+}  // namespace ttnn::fabric
