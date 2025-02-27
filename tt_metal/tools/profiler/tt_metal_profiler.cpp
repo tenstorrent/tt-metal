44e509766cf644ba4ec2c7330544b1b40f959ba9
Almeet Bhullar <abhullar@tenstorrent.com>
2025-02-27 22:49:25 +0000

    Make profiler skip over active eth cores without links on BH

--- tt_metal/tools/profiler/tt_metal_profiler.cpp
+++ tt_metal/tools/profiler/tt_metal_profiler.cpp
@@ -351,6 +351,10 @@ void syncDeviceDevice(chip_id_t device_id_sender, chip_id_t device_id_receiver) 
         chip_id_t device_id_receiver_curr = std::numeric_limits<chip_id_t>::max();
         while ((device_id_receiver != device_id_receiver_curr) and (eth_sender_core_iter != active_eth_cores.end())) {
             eth_sender_core = *eth_sender_core_iter;
+            if (not tt::Cluster::instance().is_ethernet_link_up(device_sender->id(), eth_sender_core)) {
+                eth_sender_core_iter++;
+                continue;
+            }
             std::tie(device_id_receiver_curr, eth_receiver_core) =
                 device_sender->get_connected_ethernet_core(eth_sender_core);
             eth_sender_core_iter++;
@@ -467,6 +471,9 @@ void ProfilerSync(ProfilerSyncState state) {
                 tt_xy_pair receiver_eth_core;
                 bool doSync = true;
                 for (auto& sender_eth_core : active_eth_cores) {
+                    if (not tt::Cluster::instance().is_ethernet_link_up(sender_device_id, sender_eth_core)) {
+                        continue;
+                    }
                     doSync = false;
                     std::tie(receiver_device_id, receiver_eth_core) =
                         sender_device->get_connected_ethernet_core(sender_eth_core);
