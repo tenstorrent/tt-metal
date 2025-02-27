5f675a5fc392d747a305c05cb24e6024b06188e2
Samarth Agarwal <sagarwal@tenstorrent.com>
2025-02-27 03:53:04 +0000

    Fixing kernels

--- ttnn/cpp/ttnn/deprecated/tt_dnn/kernels/dataflow/writer_unary_stick_layout_interleaved_start_id.cpp
+++ ttnn/cpp/ttnn/deprecated/tt_dnn/kernels/dataflow/writer_unary_stick_layout_interleaved_start_id.cpp
@@ -15,15 +15,15 @@ void kernel_main() {
 
     constexpr bool dst0_is_dram = get_compile_time_arg_val(1) == 1;
     constexpr bool dst_stick_size_is_pow2 = get_compile_time_arg_val(2) == 1;
-#if (dst_stick_size_is_pow2)
-    constexpr uint32_t dst_log_base_2_of_page_size = get_compile_time_arg_val(3);
-    const InterleavedPow2AddrGen<dst0_is_dram> s0 = {
-        .bank_base_address = dst_addr,
-        .log_base_2_of_page_size = dst_log_base_2_of_page_size  // TODO(AP): refactor
-    };
-#else
-    const InterleavedAddrGen<dst0_is_dram> s0 = {.bank_base_address = dst_addr, .page_size = stick_size};
-#endif
+    if constexpr (dst_stick_size_is_pow2) {
+        constexpr uint32_t dst_log_base_2_of_page_size = get_compile_time_arg_val(3);
+        const InterleavedPow2AddrGen<dst0_is_dram> s0 = {
+            .bank_base_address = dst_addr,
+            .log_base_2_of_page_size = dst_log_base_2_of_page_size  // TODO(AP): refactor
+        };
+    } else {
+        const InterleavedAddrGen<dst0_is_dram> s0 = {.bank_base_address = dst_addr, .page_size = stick_size};
+    }
 
 #ifdef BACKWARDS
     uint32_t end_id = start_id - num_sticks;
