7092f25c5606f8ae0e43a311c9fe71f821c41ca2
Nigel Huang <nhuang@tenstorrent.com>
2025-02-27 19:54:58 +0000

    #0: Remove ThreadManager from api

--- tt_metal/api/tt-metalium/utils.hpp
+++ tt_metal/api/tt-metalium/utils.hpp
@@ -31,52 +31,6 @@ struct DefinesHash {
     size_t operator()(const std::map<std::string, std::string>& c_defines) const;
 };
 
-inline std::vector<std::string> strsplit(std::string input, char delimiter) {
-    std::vector<std::string> result = {};
-    std::stringstream ss(input);
-
-    while (ss.good()) {
-        std::string substr;
-        getline(ss, substr, delimiter);
-        result.push_back(substr);
-    }
-    return result;
-}
-
-// A simple thread manager that joins all threads and rethrows the first caught exception
-// instead of letting the program terminate.
-class ThreadManager {
-public:
-    template <typename Func, typename... Args>
-    void start(Func&& func, Args&&... args) {
-        threads.emplace_back(std::thread([=]() {
-            try {
-                func(args...);
-            } catch (...) {
-                std::lock_guard<std::mutex> lock(exceptionMutex);
-                exceptions.push_back(std::current_exception());
-            }
-        }));
-    }
-
-    void join_and_rethrow() {
-        for (auto& thread : threads) {
-            if (thread.joinable()) {
-                thread.join();
-            }
-        }
-
-        if (!exceptions.empty()) {
-            std::rethrow_exception(exceptions.front());
-        }
-    }
-
-private:
-    std::vector<std::thread> threads;
-    std::vector<std::exception_ptr> exceptions;
-    std::mutex exceptionMutex;
-};
-
 template <typename E, std::enable_if_t<std::is_enum<E>::value, bool> = true>
 auto underlying_type(const E& e) {
     return static_cast<typename std::underlying_type<E>::type>(e);
