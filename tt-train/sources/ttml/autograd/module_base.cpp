f05457a1eb5b45fddbd60b35835439994e06ae9d
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2025-02-28 14:43:53 -0800

    [TT-Train]Training infra update (#18167)
    ### Problem description
    It is not easy to customize different neural networks.
    
    ### What's changed
    Now all created NNs are immutable by desing.
    Added operator () to the base class. All should derive.
    Id it is possible you should always store std::shared_ptr<ModuleBase>
    instead of your class.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [x] New/Existing tests provide coverage for changes

--- tt-train/sources/ttml/autograd/module_base.cpp
+++ tt-train/sources/ttml/autograd/module_base.cpp
@@ -106,5 +106,13 @@ void ModuleBase::train() {
 void ModuleBase::eval() {
     set_run_mode(RunMode::EVAL);
 }
+autograd::TensorPtr ModuleBase::operator()(const autograd::TensorPtr& tensor) {
+    throw std::logic_error("ModuleBase::operator()(const autograd::TensorPtr& tensor) is Not implemented");
+}
+autograd::TensorPtr ModuleBase::operator()(const autograd::TensorPtr& tensor, const autograd::TensorPtr& other) {
+    throw std::logic_error(
+        "ModuleBase::operator()(const autograd::TensorPtr& tensor, const autograd::TensorPtr& other) is Not "
+        "implemented");
+}
 
 }  // namespace ttml::autograd
