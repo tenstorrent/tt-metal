4966d4d216c0685a27a547be20b3233f77b99d44
Jonathan Su <jonathansu@tenstorrent.com>
2025-03-03 06:01:31 +0000

    enable multi device in attention

--- models/experimental/stable_diffusion3/tests/test_attention.py
+++ models/experimental/stable_diffusion3/tests/test_attention.py
@@ -4,33 +4,48 @@
 
 from typing import TYPE_CHECKING
 
+import os
 import pytest
 import torch
 import ttnn
 
 from ..reference import SD3Transformer2DModel
 from ..tt.attention import TtAttention, TtAttentionParameters
-from ..tt.utils import allocate_tensor_on_device_like, assert_quality
+from ..tt.utils import from_torch, assert_quality
 
 if TYPE_CHECKING:
     from ..reference.attention import Attention
 
 
 @pytest.mark.parametrize(
+    "mesh_device",
+    [
+        {"N150": (1, 1), "N300": (1, 2), "T3K": (1, 8), "TG": (8, 4)}.get(
+            os.environ.get("FAKE_DEVICE"), len(ttnn.get_device_ids())
+        )
+    ],
+    indirect=True,
+)
+@pytest.mark.parametrize(
     ("model_name", "block_index", "batch_size", "spatial_sequence_length", "prompt_sequence_length"),
     [
         #        ("medium", 0, 1, 4096, 333),
         #        ("medium", 23, 1, 4096, 333),
         ("large", 0, 1, 4096, 333),
-        ("large", 23, 1, 4096, 333),
+        #        ("large", 23, 1, 4096, 333),
     ],
 )
-@pytest.mark.parametrize("joint_attention", [False, True])
+@pytest.mark.parametrize(
+    "joint_attention",
+    [
+        #        False,
+        True,
+    ],
+)
 @pytest.mark.parametrize("device_params", [{"trace_region_size": 517120}], indirect=True)
-@pytest.mark.usefixtures("use_program_cache")
 def test_attention(
     *,
-    device: ttnn.Device,
+    mesh_device: ttnn.MeshDevice,
     model_name,
     block_index: int,
     batch_size: int,
@@ -38,6 +53,7 @@ def test_attention(
     prompt_sequence_length: int,
     joint_attention: bool,
 ) -> None:
+    mesh_device.enable_async(True)
     torch_dtype = torch.float32
     ttnn_dtype = ttnn.bfloat16
 
@@ -52,7 +68,7 @@ def test_attention(
     torch_model: Attention = parent_torch_model.transformer_blocks[block_index].attn
     torch_model.eval()
 
-    parameters = TtAttentionParameters.from_torch(torch_model.state_dict(), device=device, dtype=ttnn_dtype)
+    parameters = TtAttentionParameters.from_torch(torch_model.state_dict(), device=mesh_device, dtype=ttnn_dtype)
     tt_model = TtAttention(parameters, num_heads=torch_model.num_heads)
 
     torch.manual_seed(0)
@@ -61,21 +77,21 @@ def test_attention(
         torch.randn((batch_size, prompt_sequence_length, embedding_dim), dtype=torch_dtype) if joint_attention else None
     )
 
-    tt_spatial_host = ttnn.from_torch(spatial, layout=ttnn.TILE_LAYOUT, dtype=ttnn_dtype)
-    tt_prompt_host = ttnn.from_torch(prompt, layout=ttnn.TILE_LAYOUT, dtype=ttnn_dtype) if joint_attention else None
+    tt_spatial = from_torch(spatial, dtype=ttnn_dtype, mesh_device=mesh_device)
+    tt_prompt = from_torch(prompt, dtype=ttnn_dtype, mesh_device=mesh_device) if joint_attention else None
 
     with torch.no_grad():
         spatial_output, prompt_output = torch_model(spatial=spatial, prompt=prompt)
 
-    tt_spatial = allocate_tensor_on_device_like(tt_spatial_host, device=device)
-    tt_prompt = allocate_tensor_on_device_like(tt_prompt_host, device=device) if joint_attention else None
+    # tt_spatial = allocate_tensor_on_device_like(tt_spatial_host, device=device)
+    # tt_prompt = allocate_tensor_on_device_like(tt_prompt_host, device=device) if joint_attention else None
 
-    ttnn.copy_host_to_device_tensor(tt_spatial_host, tt_spatial)
-    if joint_attention:
-        ttnn.copy_host_to_device_tensor(tt_prompt_host, tt_prompt)
+    # ttnn.copy_host_to_device_tensor(tt_spatial_host, tt_spatial)
+    # if joint_attention:
+    #     ttnn.copy_host_to_device_tensor(tt_prompt_host, tt_prompt)
     tt_spatial_output, tt_prompt_output = tt_model(spatial=tt_spatial, prompt=tt_prompt)
 
-    assert_quality(spatial_output, tt_spatial_output, pcc=0.990)
+    assert_quality(spatial_output, tt_spatial_output, pcc=0.990, shard_dim=0)
 
     if joint_attention:
-        assert_quality(prompt_output, tt_prompt_output, pcc=0.990)
+        assert_quality(prompt_output, tt_prompt_output, pcc=0.990, shard_dim=0)
