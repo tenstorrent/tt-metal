07e1761e01c6ba492bb16d43eb8044be75e1800d
Stuti Raizada <sraizada@tenstorrent.com>
2025-03-04 08:45:18 -0600

    #0: fix untilize

--- ttnn/cpp/ttnn/operations/data_movement/untilize/device/untilize_op.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/untilize/device/untilize_op.cpp
@@ -120,6 +120,14 @@ operation::ProgramWithCallbacks Untilize::create_program(
         return detail::untilize_single_core(
             input_tensor_a, output_tensor, this->use_pack_untilize, this->fp32_dest_acc_en);
     }
+    if (this->sub_core_grids.has_value()) {
+        return detail::untilize_multi_core_parallelize_column_subgrid(
+            input_tensor_a,
+            output_tensor,
+            this->use_pack_untilize,
+            this->fp32_dest_acc_en,
+            this->sub_core_grids.value());
+    }
     if (!this->enough_space_height) {
         return detail::untilize_multi_core_block(
             input_tensor_a, output_tensor, this->use_pack_untilize, this->fp32_dest_acc_en);
