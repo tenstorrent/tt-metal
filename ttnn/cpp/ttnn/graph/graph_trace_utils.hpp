afc672a4a3832c7c3c0cb2e509a8292b4253cb40
Diego Gomez <dgomez@tenstorrent.com>
2025-03-04 18:37:25 +0000

    improved the trace utils method for arguments so is clearer for customers

--- ttnn/cpp/ttnn/graph/graph_trace_utils.hpp
+++ ttnn/cpp/ttnn/graph/graph_trace_utils.hpp
@@ -11,6 +11,11 @@
 
 namespace ttnn::graph {
 
+struct OperationInfo {
+    std::string operation_name;
+    std::vector<std::string> arguments;
+};
+
 enum class ExecutionStatus { Success, Error };
 
 uint32_t extract_peak_L1_memory_usage(const nlohmann::json& trace);
@@ -24,7 +29,7 @@ std::pair<uint32_t, uint32_t> count_intermediate_and_output_tensors(const nlohma
 
 std::vector<std::string> extract_calltrace(const nlohmann::json& trace);
 
-std::vector<std::vector<std::string>> extract_arguments(const nlohmann::json& trace);
+std::vector<OperationInfo> extract_arguments(const nlohmann::json& trace);
 
 std::unordered_set<uint32_t> extract_output_tensors(const nlohmann::json& trace);
 
