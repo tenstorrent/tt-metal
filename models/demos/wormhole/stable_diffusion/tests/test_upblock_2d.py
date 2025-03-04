ea2517ac2ded7742a783fcfd5859874ad5fdbd7f
Sofija Jovic <sjovic@tenstorrent.com>
2025-03-04 07:29:58 +0000

    #0: remove WH didt workaround

--- models/demos/wormhole/stable_diffusion/tests/test_upblock_2d.py
+++ models/demos/wormhole/stable_diffusion/tests/test_upblock_2d.py
@@ -38,7 +38,7 @@ from models.demos.wormhole.stable_diffusion.tests.parameterizations import DOWN_
 def test_upblock_512x512(
     reset_seeds, device, res_hidden_states_tuple, hidden_states, shard_layout, shard_end_core, shard_shape, temb
 ):
-    os.environ["SLOW_MATMULS"] = "1"
+    # os.environ["SLOW_MATMULS"] = "1"
 
     pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", torch_dtype=torch.float32)
     unet = pipe.unet
