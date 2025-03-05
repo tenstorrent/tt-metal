6160eab88a047eafd59324a3713959e81d167508
keerthana-r-mcw <keerthana.rajendran@multicorewareinc.com>
2025-03-05 12:57:41 +0000

    #18401: Generalization of neck, DS1-DS5 modules for 640 res

--- models/demos/yolov4/ttnn/downsample2.py
+++ models/demos/yolov4/ttnn/downsample2.py
@@ -9,6 +9,7 @@ from models.demos.yolov4.ttnn.common import Conv
 
 class Down2:
     def __init__(self, device, parameters, conv_args) -> None:
+        self.parameters = parameters
         self.conv1 = Conv(
             device,
             conv_args.c1,
@@ -64,6 +65,7 @@ class Down2:
         output_tensor_left = ttnn.mish(output_tensor_left)
 
         res1_split = self.conv3(output_tensor_split)[0]
+        ttnn.deallocate(output_tensor_split)
         res1_split = ttnn.mish(res1_split)
 
         output_tensor = self.res1_conv1(res1_split)[0]
@@ -86,16 +88,33 @@ class Down2:
 
         output_tensor = ttnn.to_layout(output_tensor, layout=ttnn.ROW_MAJOR_LAYOUT)
         output_tensor_left = ttnn.to_layout(output_tensor_left, layout=ttnn.ROW_MAJOR_LAYOUT)
-        output_sharded_memory_config = ttnn.create_sharded_memory_config(
-            [128, 128],
-            core_grid=output_tensor_left.memory_config().shard_spec.grid,
-            strategy=ttnn.ShardStrategy.HEIGHT,
-            use_height_and_width_as_shard_shape=True,
-        )
+
+        if self.parameters.is_320_res:
+            output_sharded_memory_config = ttnn.create_sharded_memory_config(
+                [128, 128],
+                core_grid=output_tensor_left.memory_config().shard_spec.grid,
+                strategy=ttnn.ShardStrategy.HEIGHT,
+                use_height_and_width_as_shard_shape=True,
+            )
+        else:
+            output_sharded_memory_config = ttnn.create_sharded_memory_config(
+                [
+                    output_tensor.memory_config().shard_spec.shape[0],
+                    2 * output_tensor.memory_config().shard_spec.shape[1],
+                ],
+                core_grid=output_tensor_left.memory_config().shard_spec.grid,
+                strategy=ttnn.ShardStrategy.HEIGHT,
+                use_height_and_width_as_shard_shape=True,
+            )
+
         output_tensor = ttnn.concat(
             [output_tensor, output_tensor_left], dim=3, memory_config=output_sharded_memory_config
         )
         ttnn.deallocate(output_tensor_left)
+
+        if not self.parameters.is_320_res:
+            if output_tensor.is_sharded():
+                output_tensor = ttnn.sharded_to_interleaved(output_tensor, ttnn.L1_MEMORY_CONFIG)
 
         output_tensor = self.conv5(output_tensor)[0]
         output_tensor = ttnn.mish(output_tensor)
