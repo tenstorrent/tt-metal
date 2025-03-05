6530594840ce78dbadafa206fa5253e2abdc4853
Oleg Milyutin <omilyutin@tenstorrent.com>
2025-03-05 00:45:56 +0000

    Fix dereference

--- tt_metal/impl/sub_device/sub_device_manager_tracker.cpp
+++ tt_metal/impl/sub_device/sub_device_manager_tracker.cpp
@@ -137,7 +137,7 @@ std::optional<DeviceAddr> SubDeviceManagerTracker::lowest_occupied_compute_l1_ad
     } else {
         DeviceAddr lowest_addr = std::numeric_limits<DeviceAddr>::max();
         for (const auto& sub_device_id : sub_device_ids) {
-            const auto& allocator = this->get_active_sub_device_manager()->sub_device_allocator(sub_device_id);
+            const auto& allocator = this->get_active_sub_device_manager().sub_device_allocator(sub_device_id);
             if (allocator) {
                 auto found_addr = allocator->get_lowest_occupied_l1_address(global_bank_id);
                 if (found_addr.has_value()) {
