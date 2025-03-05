9270e94989462bb634fd979121c97f164b194dc2
Jeffrey Jiang <jjiang@tenstorrent.com>
2025-03-05 15:45:06 +0000

    make the switch, satisfy linker without dummy virtual function definitions

--- ttnn/ttnn/distributed/distributed.py
+++ ttnn/ttnn/distributed/distributed.py
@@ -210,22 +210,6 @@ def synchronize_devices(
 
 
 # TODO: All of the TensorTo and MeshTo classes will be slowly cut out over the next few days
-class TensorToMesh:
-    """
-    Defines the mapping of a torch.Tensor to a device mesh: e.g. Shard/Replicate.
-    You can also "Bring your own TensorToMesh" based on your custom mapping.
-    """
-
-    def __init__(self, mesh_device):
-        self.mesh_device = mesh_device
-
-    def map(self, tensor: "torch.Tensor"):
-        raise NotImplementedError("Subclasses must implement this method")
-
-    def config(self):
-        raise NotImplementedError("Subclasses must implement this method")
-
-
 class MeshToTensor:
     """
     Defines the inverse operation of TensorToMesh. Given a set of per-device
@@ -236,107 +220,6 @@ class MeshToTensor:
 
     def compose(self, tensor: ttnn.Tensor):
         raise NotImplementedError("Subclasses must implement this method")
-
-
-class ShardTensorToMesh(TensorToMesh):
-    def __init__(self, mesh_device, dim):
-        super().__init__(mesh_device)
-        self.shard_dim = dim
-
-    def map(self, tensor: "torch.Tensor") -> Dict[int, ttnn.Tensor]:
-        import torch
-
-        sliced_tensors = torch.chunk(tensor, self.mesh_device.get_num_devices(), dim=self.shard_dim)
-        return list(sliced_tensors)
-
-    def config(self):
-        return {
-            "strategy": "shard",
-            "shard_dim": f"{self.shard_dim}",
-        }
-
-
-class ShardTensor2dMesh(TensorToMesh):
-    """
-    Shard a tensor across a 2D mesh of devices.
-    This class implements a strategy for distributing a tensor across a 2D grid of devices,
-    allowing for efficient parallel processing in distributed computing environments.
-    """
-
-    def __init__(self, mesh_device: MeshDevice, mesh_shape: Tuple[int, int], dims: Tuple[Optional[int], Optional[int]]):
-        """
-        Initialize the ShardTensor2dMesh.
-        Args:
-            mesh_device: The target device mesh for distributing the tensor.
-            mesh_shape: The shape of the 2D mesh as (rows, cols).
-            dims: The dimensions to shard along, specified as (row_dim, col_dim).
-        The `dims` tuple determines how the tensor is sharded across the 2D mesh:
-        - row_dim: The dimension to shard across mesh rows (or None for replication).
-        - col_dim: The dimension to shard across mesh columns (or None for replication).
-        Examples:
-        1. dims=(2, 3) for a tensor of shape (A, B, C, D):
-           - Shard along dimension 2 (C) across mesh rows
-           - Shard along dimension 3 (D) across mesh columns
-        2. dims=(None, 3):
-           - Replicate across mesh rows
-           - Shard along dimension 3 (D) across mesh columns
-        3. dims=(None, None):
-           - Fully replicate the tensor across all devices
-        """
-        super().__init__(mesh_device)
-        self.mesh_shape: Tuple[int, int] = mesh_shape
-        self.dims: Tuple[Optional[int], Optional[int]] = dims
-
-        mesh_device_rows, mesh_device_cols = self.mesh_device.shape
-        if mesh_shape[0] > mesh_device_rows or mesh_shape[1] > mesh_device_cols:
-            raise ValueError("ShardTensor2dMesh: Device mesh shape does not match the provided mesh shape.")
-
-    def map(self, tensor: "torch.Tensor") -> List["torch.Tensor"]:
-        """
-        Map the input tensor to a list of sharded tensors.
-        Args:
-            tensor: The input tensor to be sharded.
-        Returns:
-            A list of sharded tensors, one for each device in the mesh.
-        Raises:
-            ValueError: If the number of sharding dimensions is not 2.
-        """
-        import torch
-
-        if len(self.dims) != 2:
-            raise ValueError("ShardTensor2dMesh only supports 2D shard dimensions")
-
-        rows, cols = self.mesh_shape
-        row_dim, col_dim = self.dims
-
-        # Shard along rows
-        row_tensors = (
-            [tensor.clone() for _ in range(rows)] if row_dim is None else torch.chunk(tensor, rows, dim=row_dim)
-        )
-
-        # Shard along columns
-        if col_dim is None:
-            return [t.clone() for t in row_tensors for _ in range(cols)]
-        tensor_shards = [tt for t in row_tensors for tt in torch.chunk(t, cols, dim=col_dim)]
-
-        if len(tensor_shards) != rows * cols:
-            raise ValueError(
-                f"ShardTensor2dMesh: Sharding failed. Number of shards should match the product of the mesh dimensions. Got {len(tensor_shards)} shards but expected {rows * cols} ({rows} rows * {cols} cols)."
-            )
-
-        return tensor_shards
-
-    def config(self) -> Dict[str, str]:
-        """
-        Provide the configuration of the sharding strategy.
-        Returns:
-            A dictionary containing the sharding strategy and dimensions.
-        """
-        return {
-            "strategy": "shard_2d",
-            "mesh_shape_y": str(self.mesh_shape[0]),
-            "mesh_shape_x": str(self.mesh_shape[1]),
-        }
 
 
 class ConcatMesh2dToTensor(MeshToTensor):
@@ -397,20 +280,6 @@ class ConcatMesh2dToTensor(MeshToTensor):
         return torch.cat(row_concatenated, dim=row_dim)
 
 
-class ReplicateTensorToMesh(TensorToMesh):
-    def __init__(self, mesh_device: MeshDevice):
-        super().__init__(mesh_device)
-
-    def map(self, tensor: "torch.Tensor"):
-        return [tensor for i in range(self.mesh_device.get_num_devices())]
-
-    def config(self):
-        return {
-            "strategy": "replicate",
-            "replication_factor": str(self.mesh_device.get_num_devices()),
-        }
-
-
 class ConcatMeshToTensor(MeshToTensor):
     def __init__(self, mesh_device: MeshDevice, dim: int):
         self.concat_dim = dim
@@ -423,44 +292,6 @@ class ConcatMeshToTensor(MeshToTensor):
             ttnn.to_torch(tt_input_tensor, mesh_composer=None) for tt_input_tensor in ttnn.get_device_tensors(tensor)
         ]
         return torch.cat(device_shards_converted_to_torch, dim=self.concat_dim)
-
-
-@contextlib.contextmanager
-def distribute(default: Union[TensorToMesh, MeshToTensor]):
-    """
-    Context manager to temporarily modify the behavior of ttnn.from_torch and ttnn.to_torch to use the specified
-    mesh_mapper or mesh_composer for tensor distribution and composition to/from MeshDevice.
-    Invocations of ttnn.from_torch(..) will use the mesh_mapper as defined by the default in ttnn.distribute.
-    Invocations of ttnn.to_torch(..) will use the mesh_composer as defined by the default in ttnn.distribute.
-
-    Args:
-        mesh_mapper_or_composer (Union[TensorToMesh, MeshToTensor]): An instance of either TensorToMesh or MeshToTensor
-            used to map tensors to a mesh or compose tensors from a mesh.
-
-    Example:
-        with distribute(ShardTensorToMesh(mesh_device, dim=3)):
-            # Code here will use the default mapper
-            result = ttnn.from_torch(torch_tensor)
-
-        is equivalent to:
-        result = ttnn.from_torch(torch_tensor, mesh_mapper=ShardTensorToMesh(mesh_device, dim=3))
-    """
-    _original_to_torch = ttnn.to_torch
-    _original_from_torch = ttnn.from_torch
-
-    try:
-        if isinstance(default, TensorToMesh):
-            ttnn.from_torch = functools.partial(_original_from_torch, mesh_mapper=default)
-        elif isinstance(default, MeshToTensor):
-            ttnn.to_torch = functools.partial(_original_to_torch, mesh_composer=default)
-        else:
-            raise ValueError("Argument must be an instance of either TensorToMesh or MeshToTensor.")
-        yield
-
-    finally:
-        # Restore the original functions
-        ttnn.from_torch = _original_from_torch
-        ttnn.to_torch = _original_to_torch
 
 
 __all__ = []
