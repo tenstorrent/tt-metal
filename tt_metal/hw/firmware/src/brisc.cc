f49d6ed18b0e06fc6b20d8f0dfcbce0597e1af33
mtairum <mtairum@tenstorrent.com>
2025-03-05 15:19:14 +0000

    Revert "#16643: Disabling dispatch posting atomic increments on blackhole"
    This reverts commit 57ba436ec4366d9129df6a53b2d9e1e828ef0356.

--- tt_metal/hw/firmware/src/brisc.cc
+++ tt_metal/hw/firmware/src/brisc.cc
@@ -406,15 +406,6 @@ int main() {
     uint8_t prev_noc_mode = DM_DEDICATED_NOC;
     trigger_sync_register_init();
 
-
-#if defined(ARCH_BLACKHOLE)
-    // When dispatch_s is on an ethernet core on blockhole, we've been seeing
-    // issues where posted atomic incremenets seem to fail to complete.
-    const bool post_atomic_increments = false;
-#else
-    const bool post_atomic_increments = true;
-#endif
-
     while (1) {
         reset_ncrisc_with_iram();
 
@@ -511,7 +502,7 @@ int main() {
                     (uint32_t tt_l1_ptr*)(kernel_config_base + launch_msg_address->kernel_config.remote_cb_offset);
                 end_cb_index = launch_msg_address->kernel_config.min_remote_cb_start_index;
                 experimental::setup_remote_cb_interfaces<true>(
-                    cb_l1_base, end_cb_index, noc_index, noc_mode, post_atomic_increments, cmd_buf);
+                    cb_l1_base, end_cb_index, noc_index, noc_mode, true, cmd_buf);
                 start_ncrisc_kernel_run(enables);
                 int index = static_cast<std::underlying_type<TensixProcessorTypes>::type>(TensixProcessorTypes::DM0);
                 void (*kernel_address)(uint32_t) = (void (*)(uint32_t))
@@ -533,7 +524,7 @@ int main() {
                         (uint32_t tt_l1_ptr*)(kernel_config_base + launch_msg_address->kernel_config.remote_cb_offset);
                     uint32_t end_cb_index = launch_msg_address->kernel_config.min_remote_cb_start_index;
                     experimental::setup_remote_cb_interfaces<true>(
-                        cb_l1_base, end_cb_index, noc_index, noc_mode, post_atomic_increments, cmd_buf);
+                        cb_l1_base, end_cb_index, noc_index, noc_mode, true, cmd_buf);
                 }
                 start_ncrisc_kernel_run(enables);
                 wait_for_go_message();
