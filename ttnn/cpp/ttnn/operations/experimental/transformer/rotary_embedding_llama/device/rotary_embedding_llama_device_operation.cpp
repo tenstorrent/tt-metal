4f5d81100a31995086bc79e1ede38c22669a1597
Jay Kruer <jkruer@tenstorrent.com>
2025-03-05 19:14:27 +0000

    fix assertion one last time :D

--- ttnn/cpp/ttnn/operations/experimental/transformer/rotary_embedding_llama/device/rotary_embedding_llama_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/experimental/transformer/rotary_embedding_llama/device/rotary_embedding_llama_device_operation.cpp
@@ -27,9 +27,9 @@ void RotaryEmbeddingLlama::validate(const std::vector<Tensor>& input_tensors) co
         TT_FATAL(input.device() == ref_device, "Operands to rotary embedding need to be on same device!");
         TT_FATAL((input.get_layout() == Layout::TILE), "Inputs to rotary embedding must be tilized");
         // we require L1 interleaved inputs when using prefill mode
-        TT_FATAL(
-            !this->is_decode_mode || input.memory_config() == ttnn::types::L1_MEMORY_CONFIG,
-            "Inputs to rotary embedding must be interleaved over L1 when using prefill mode.");
+        if (!this->is_decode_mode && input.memory_config() != ttnn::types::L1_MEMORY_CONFIG) {
+            TT_THROW("Inputs to rotary embedding must be interleaved over L1 when using prefill mode.");
+        }
     }
 
     uint32_t head_dim = input_tensor.get_logical_shape()[-1];
