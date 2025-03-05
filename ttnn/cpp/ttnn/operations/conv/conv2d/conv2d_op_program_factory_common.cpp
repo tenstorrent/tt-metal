2b7e8184eef078274b24e6f65a0ca5a4bd17ce50
Dragica Stoiljkovic <dstoiljkovic@tenstorrent.com>
2025-03-05 11:09:49 +0000

    Post commit error fix, adding copy right

--- ttnn/cpp/ttnn/operations/conv/conv2d/conv2d_op_program_factory_common.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/conv2d_op_program_factory_common.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 #include "conv2d_op_program_factory_common.hpp"
 
 namespace ttnn::operations::conv {
