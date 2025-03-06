fdcae2e472412651819b3806531a6827fb3a54a3
ssinghalTT <ssinghal@tenstorrent.com>
2025-03-06 01:05:51 +0000

    updated the event code to latest, non blocking on execute_trace

--- models/demos/ttnn_resnet/tests/resnet50_performant_imagenet.py
+++ models/demos/ttnn_resnet/tests/resnet50_performant_imagenet.py
@@ -57,19 +57,21 @@ class ResNet50Trace2CQ:
             device
         )
         self.tt_image_res = self.tt_inputs_host.to(device, sharded_mem_config_DRAM)
-        self.op_event = ttnn.create_event(device)
-        self.write_event = ttnn.create_event(device)
+        # self.op_event = ttnn.create_event(device)
+        # self.write_event = ttnn.create_event(device)
+        self.op_event = ttnn.record_event(device, 0)
+        self.write_event = ttnn.record_event(device, 1)
         # Initialize the op event so we can write
-        ttnn.record_event(0, self.op_event)
+        # ttnn.record_event(0, self.op_event)
 
         # First run configures convs JIT
         ttnn.wait_for_event(1, self.op_event)
         ttnn.copy_host_to_device_tensor(self.tt_inputs_host, self.tt_image_res, 1)
-        ttnn.record_event(1, self.write_event)
+        self.write_event = ttnn.record_event(device, 1)
         ttnn.wait_for_event(0, self.write_event)
         self.test_infra.input_tensor = ttnn.to_memory_config(self.tt_image_res, self.input_mem_config)
         spec = self.test_infra.input_tensor.spec
-        ttnn.record_event(0, self.op_event)
+        self.op_event = ttnn.record_event(device, 0)
         self.test_infra.run()
         self.test_infra.validate()
         self.test_infra.output_tensor.deallocate(force=True)
@@ -77,20 +79,20 @@ class ResNet50Trace2CQ:
         # Optimized run
         ttnn.wait_for_event(1, self.op_event)
         ttnn.copy_host_to_device_tensor(self.tt_inputs_host, self.tt_image_res, 1)
-        ttnn.record_event(1, self.write_event)
+        self.write_event = ttnn.record_event(device, 1)
         ttnn.wait_for_event(0, self.write_event)
         self.test_infra.input_tensor = ttnn.to_memory_config(self.tt_image_res, self.input_mem_config)
-        ttnn.record_event(0, self.op_event)
+        self.op_event = ttnn.record_event(device, 0)
         self.test_infra.run()
         self.test_infra.validate()
 
         # Capture
         ttnn.wait_for_event(1, self.op_event)
         ttnn.copy_host_to_device_tensor(self.tt_inputs_host, self.tt_image_res, 1)
-        ttnn.record_event(1, self.write_event)
+        self.write_event = ttnn.record_event(device, 1)
         ttnn.wait_for_event(0, self.write_event)
         self.test_infra.input_tensor = ttnn.to_memory_config(self.tt_image_res, self.input_mem_config)
-        ttnn.record_event(0, self.op_event)
+        self.op_event = ttnn.record_event(device, 0)
         self.test_infra.output_tensor.deallocate(force=True)
         trace_input_addr = ttnn.buffer_address(self.test_infra.input_tensor)
         self.tid = ttnn.begin_trace_capture(device, cq_id=0)
@@ -102,12 +104,12 @@ class ResNet50Trace2CQ:
     def execute_resnet50_trace_2cqs_inference(self, tt_inputs_host=None):
         ttnn.wait_for_event(1, self.op_event)
         ttnn.copy_host_to_device_tensor(tt_inputs_host, self.tt_image_res, 1)
-        ttnn.record_event(1, self.write_event)
+        self.write_event = ttnn.record_event(self.device, 1)
         ttnn.wait_for_event(0, self.write_event)
         # TODO: Add in place support to ttnn to_memory_config
         self.input_tensor = ttnn.reshard(self.tt_image_res, self.input_mem_config, self.input_tensor)
-        ttnn.record_event(0, self.op_event)
-        ttnn.execute_trace(self.device, self.tid, cq_id=0, blocking=True)
+        self.op_event = ttnn.record_event(self.device, 0)
+        ttnn.execute_trace(self.device, self.tid, cq_id=0, blocking=False)
         outputs = ttnn.from_device(self.test_infra.output_tensor, blocking=True)
 
         return outputs
