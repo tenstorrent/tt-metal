67270827df5e3fbca6dfaf620e45a8f962951c6c
Nigel Huang <nhuang@tenstorrent.com>
2025-03-05 16:45:13 -0800

    Fabric initialization infrastructure for fast dispatch (#18578)
    ### Ticket
    N/A
    
    ### Problem description
    Dispatch initialization needs to support tt-fabric
    
    ### What's changed
    - Updated `DispatchMemMap` to reserve L1 space for the Fabric client
    interface struct
    - Provide the necessary upstream and downstream chip+mesh ID for
    Prefetch and Dispatch to use tt-fabric APIs
    - Split host and device device initialization in DevicePool to support
    the use case below.
    - Split static args and dependent args generation in `FDKernel`.
    Previously, static + dependent args are initialized on 1 device at a
    time. But with this change, it's possible for all static args to be
    initialized at once. This lets you put upstream and downstream nodes on
    a different device
    - This is needed because on Fabric, Prefetch H node (device 0) is
    "directly" connected to a Prefetch D (device 1) downstream.
    - There is a type conflict when including the fabric host interface on
    the host code. This issue is fixed with
    https://github.com/tenstorrent/tt-metal/commit/3b28664ab14fb0131e39ad76837280cd4d9267fc
    but I put everything into a namespace anyways to prevent this issue in
    the future.
    - New intermediary node called `FabricRouterVC` in topology to replace
    the tunneling nodes. This node is used indicate that two `FDKernel`
    nodes will communicate using fabric. It provides a fabric router
    obtained from the `control plane` for the upstream and downstream nodes.
    - Remove the dispatch fixture from dispatch settings. not needed and changed settings
      may interfere with other tests
    ~~~
    
    Additional compile args to be passed into `cq_prefetch` and
    `cq_dispatch`
    
    1. `downstream_mesh_id`
    2. `downstream_chip_id`
    3. `upstream_mesh_id`
    4. `upstream_chip_id`
    5. `fabric_router_noc_xy`
    
    ~~~
    
    ### Checklist
    APC
    
    https://github.com/tenstorrent/tt-metal/actions/runs/13643384535/job/38138384125
    
    ---------
    
    Co-authored-by: Austin Ho <aho@tenstorrent.com>
    Co-authored-by: Andrew Fuller <afuller@tenstorrent.com>

--- tt_metal/impl/dispatch/kernel_config/mux.hpp
+++ tt_metal/impl/dispatch/kernel_config/mux.hpp
@@ -10,7 +10,7 @@ typedef struct mux_static_config {
     std::optional<uint32_t> rx_queue_size_words;
     std::optional<uint32_t> mux_fan_in;
 
-    std::array<std::optional<uint32_t>, MAX_SWITCH_FAN_IN> remote_rx_network_type;  // [4:7]
+    std::array<std::optional<uint32_t>, tt::packet_queue::MAX_SWITCH_FAN_IN> remote_rx_network_type;  // [4:7]
     std::optional<uint32_t> tx_network_type;
 
     std::optional<uint32_t> test_results_buf_addr_arg;
@@ -18,23 +18,24 @@ typedef struct mux_static_config {
     std::optional<uint32_t> timeout_cycles;
     std::optional<uint32_t> output_depacketize;
     std::optional<uint32_t> output_depacketize_info;  // Packed, pack with above same is input?
-    std::array<std::optional<uint32_t>, MAX_SWITCH_FAN_IN> input_packetize_local_sem;
+    std::array<std::optional<uint32_t>, tt::packet_queue::MAX_SWITCH_FAN_IN> input_packetize_local_sem;
     std::optional<uint32_t> input_packetize_src_endpoint;   // Packed w/ max 4 assumption
     std::optional<uint32_t> input_packetize_dest_endpoint;  // Same as src
 } mux_static_config_t;
 
 typedef struct mux_dependent_config {
-    std::array<std::optional<uint32_t>, MAX_SWITCH_FAN_IN> remote_rx_x;                    // [4:7], dependent
-    std::array<std::optional<uint32_t>, MAX_SWITCH_FAN_IN> remote_rx_y;                    // [4:7], dependent
-    std::array<std::optional<uint32_t>, MAX_SWITCH_FAN_IN> remote_rx_queue_id;             // [4:7], dependent
+    std::array<std::optional<uint32_t>, tt::packet_queue::MAX_SWITCH_FAN_IN> remote_rx_x;         // [4:7], dependent
+    std::array<std::optional<uint32_t>, tt::packet_queue::MAX_SWITCH_FAN_IN> remote_rx_y;         // [4:7], dependent
+    std::array<std::optional<uint32_t>, tt::packet_queue::MAX_SWITCH_FAN_IN> remote_rx_queue_id;  // [4:7], dependent
     std::optional<uint32_t> remote_tx_queue_start_addr_words;                              // Dependent
     std::optional<uint32_t> remote_tx_queue_size_words;                                    // Dependent
     std::optional<uint32_t> remote_tx_x;                                                   // Dependent
     std::optional<uint32_t> remote_tx_y;                                                   // Dependent
     std::optional<uint32_t> remote_tx_queue_id;                                            // Dependent
-    std::array<std::optional<uint32_t>, MAX_SWITCH_FAN_IN> input_packetize;                // Dependent
-    std::array<std::optional<uint32_t>, MAX_SWITCH_FAN_IN> input_packetize_log_page_size;  // Dependent
-    std::array<std::optional<uint32_t>, MAX_SWITCH_FAN_IN> input_packetize_upstream_sem;   // Dependent
+    std::array<std::optional<uint32_t>, tt::packet_queue::MAX_SWITCH_FAN_IN> input_packetize;  // Dependent
+    std::array<std::optional<uint32_t>, tt::packet_queue::MAX_SWITCH_FAN_IN>
+        input_packetize_log_page_size;                                                                      // Dependent
+    std::array<std::optional<uint32_t>, tt::packet_queue::MAX_SWITCH_FAN_IN> input_packetize_upstream_sem;  // Dependent
 } mux_dependent_config_t;
 
 class MuxKernel : public FDKernel {
