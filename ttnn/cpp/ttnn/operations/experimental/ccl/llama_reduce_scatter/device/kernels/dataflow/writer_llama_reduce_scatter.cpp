712347fa8f88bd773d4436b771e3dbac107be869
Saad Jameel <sjameel@tenstorrent.com>
2025-03-07 00:50:24 +0000

    allocate intermediate buffers for the reduction

--- ttnn/cpp/ttnn/operations/experimental/ccl/llama_reduce_scatter/device/kernels/dataflow/writer_llama_reduce_scatter.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/llama_reduce_scatter/device/kernels/dataflow/writer_llama_reduce_scatter.cpp
@@ -32,12 +32,15 @@ inline void print_full_tile(uint32_t cb_id, uint32_t tile_id = 0, bool untilize 
 
 void kernel_main() {
     constexpr uint32_t page_size = get_compile_time_arg_val(0);
-    constexpr uint32_t cb_id_out = get_compile_time_arg_val(1);
+    constexpr uint32_t cb_id_in = get_compile_time_arg_val(1);
     constexpr uint32_t fabric_sender_cb_index = get_compile_time_arg_val(2);
     constexpr uint32_t fabric_receiver_cb_index = get_compile_time_arg_val(3);
     constexpr uint32_t client_interface_cb_index = get_compile_time_arg_val(4);
     constexpr uint32_t start_tile = get_compile_time_arg_val(5);
     constexpr uint32_t end_tile = get_compile_time_arg_val(6);
+    constexpr uint32_t output_cb_index = get_compile_time_arg_val(7);
+    constexpr uint32_t dst_cb_index = get_compile_time_arg_val(8);
+    constexpr uint32_t chip_id = get_compile_time_arg_val(9);
     // constexpr uint32_t num_devices = 2;
 
     const uint32_t output_addr = get_arg_val<uint32_t>(0);
@@ -52,45 +55,33 @@ void kernel_main() {
     }
     // ublocks size defined in tiles
     constexpr uint32_t onetile = 1;
-    constexpr uint32_t tile_bytes = get_tile_size(cb_id_out);
-    const DataFormat data_format = get_dataformat(cb_id_out);
+    constexpr uint32_t tile_bytes = get_tile_size(cb_id_in);
+    const DataFormat data_format = get_dataformat(cb_id_in);
     // my dst noc offset is actually the same as the noc xy my fabric receiver_cb_index but on the other device
     // uint64_t dst_noc_addr = get_noc_addr_helper(my_noc_xy, get_write_ptr(fabric_receiver_cb_index));
     uint32_t packet_size_bytes = (end_tile - start_tile) * tile_bytes + PACKET_HEADER_SIZE_BYTES;
 
-    uint64_t output_noc_addr = get_noc_addr(get_write_ptr(cb_id_out));
-    for (uint32_t tile = start_tile; tile < end_tile; ++tile) {
-        if (write_output) {
-            DPRINT << "This core's writer is just writing to the output buffer" << ENDL();
-            print_full_tile(cb_id_out, tile, true);
-        } else {
+    uint64_t output_noc_addr = get_noc_addr(get_write_ptr(cb_id_in));
+    if (!write_output) {
+        for (uint32_t tile = start_tile; tile < end_tile; ++tile) {
             DPRINT << "This core's writer is writing to the fabric sender buffer" << " start_tile: " << start_tile
                    << " end_tile: " << end_tile << " my_noc_xy: " << my_noc_xy << " router_noc_xy: " << router_noc_xy
                    << " offset_to_receiver: " << offset_to_receiver << " packet_size_bytes: " << packet_size_bytes
                    << ENDL();
             // write to fabric sender buffer
-            uint32_t cb_out_read_ptr = get_read_ptr(cb_id_out);
+            uint32_t cb_out_read_ptr = get_read_ptr(cb_id_in);
             uint32_t fabric_sender_cb_read_ptr = get_write_ptr(fabric_sender_cb_index);
             tt::data_movement::common::tt_memmove<true, false, true, tile_bytes>(
                 fabric_sender_cb_read_ptr + tile * tile_bytes + PACKET_HEADER_SIZE_BYTES,
                 cb_out_read_ptr + tile * tile_bytes,
                 tile_bytes);
         }
-    }
-    noc_async_read_barrier();
-    if (!write_output) {
-        // get_local_cb_interface(fabric_sender_cb_index).fifo_rd_ptr += PACKET_HEADER_SIZE_BYTES;
-        // get_local_cb_interface(fabric_sender_cb_index).fifo_wr_ptr += PACKET_HEADER_SIZE_BYTES;
-        // for (uint32_t tile = start_tile; tile < end_tile; ++tile) {
-        //     print_full_tile(fabric_sender_cb_index, tile, true);
-        // }
-        // get_local_cb_interface(fabric_sender_cb_index).fifo_rd_ptr -= PACKET_HEADER_SIZE_BYTES;
-        // get_local_cb_interface(fabric_sender_cb_index).fifo_wr_ptr -= PACKET_HEADER_SIZE_BYTES;
+        noc_async_read_barrier();
 
-        uint32_t dst_addr = output_addr;
-        uint64_t dst_noc_addr =
-            get_noc_addr(get_read_ptr(cb_id_out));  // for now just overwrite the input buffer on the remote device to
-                                                    // check if the data is correct
+        uint64_t dst_noc_addr = get_noc_addr(
+            get_read_ptr(fabric_receiver_cb_index) +
+            offset_to_receiver);  // for now just overwrite the input buffer on the remote device to
+                                  // check if the data is correct
 
         uint32_t client_interface_addr = get_write_ptr(client_interface_cb_index);
         volatile tt_l1_ptr fabric_pull_client_interface_t* client_interface =
@@ -108,5 +99,7 @@ void kernel_main() {
         );
 
         fabric_wait_for_pull_request_flushed(client_interface);
+    } else {
+        DPRINT << "This core's writer is writing to the output buffer" << ENDL();
     }
 }
