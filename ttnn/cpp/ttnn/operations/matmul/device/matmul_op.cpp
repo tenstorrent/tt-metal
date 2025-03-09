2833697403171540d99efe55fbdf3a86d54d60e9
asaigal <asaigal@tenstorrent.com>
2025-03-08 17:15:41 -0800

    Remove MultiDeviceGlobalCircularBuffer
      - TT-Metal already supports Global CBs backed by MeshBuffer
      - Expose this concept to TTNN, and remove the concept of the
        MultiDeviceGlobalCircularBuffer
      - create_global_circular_buffer(mesh_device...) is now hooked
        up to a GlobalCircularBuffer
      - Add T3K prefetcher test using Global CBs to track regressions

--- ttnn/cpp/ttnn/operations/matmul/device/matmul_op.cpp
+++ ttnn/cpp/ttnn/operations/matmul/device/matmul_op.cpp
@@ -2271,10 +2271,6 @@ operation::ProgramWithCallbacks Matmul::create_program(
                     program_config.fused_activation,
                     this->untilize_out);
             } else if constexpr (std::is_same_v<ProgramConfigType, MatmulMultiCoreReuseMultiCast1DProgramConfig>) {
-                std::optional<tt::tt_metal::v1::experimental::GlobalCircularBuffer> global_cb = std::nullopt;
-                if (this->global_cb.has_value()) {
-                    global_cb = get_global_circular_buffer(*this->global_cb, input_tensor_a.device()->id());
-                }
                 return matmul_multi_core_reuse_mcast_1d_optimized(
                     input_tensor_a,
                     input_tensor_b,
@@ -2296,7 +2292,7 @@ operation::ProgramWithCallbacks Matmul::create_program(
                     program_config.gather_in0,
                     program_config.hop_cores,
                     this->untilize_out,
-                    global_cb,
+                    this->global_cb,
                     program_config.num_global_cb_receivers);
             } else if constexpr (std::is_same_v<
                                      ProgramConfigType,
