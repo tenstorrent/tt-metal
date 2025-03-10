94b202c08ca35ee4934d43c49beeb9e26aead7db
daminakaTT <daminaka@tenstorrent.com>
2025-03-10 04:47:14 +0000

    support 1,K,M,N tensor with 3dim allgather
    cleanup
    
    address comments
    
    support [1,8,M,N] tensor
    
    fix 'if'
    
    increase timeout (experimental)
    
    use pytest timeout
    
    unify all dim case. address comments
    
    split test cases for each data type and mem type. coment out huge tensor
    
    cleanup
    
    implement two phase barrier to sync all state
    
    address comments (partial)
    
    rollback generic allgather
    
    use template for barrier release phase
    
    two_phase_release flag as compile time arg
    
    manual rollback
    
    more
    
    fix bug

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/device/all_gather_async_op.hpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/device/all_gather_async_op.hpp
@@ -40,7 +40,7 @@ struct AllGatherAsync {
     const uint32_t ring_index;
     const MemoryConfig output_mem_config;
     const ccl::Topology topology;
-    const GlobalSemaphore semaphore;
+    const std::pair<GlobalSemaphore, GlobalSemaphore> semaphore;
     std::optional<tt::tt_metal::SubDeviceId> sub_device_id;
     bool enable_persistent_fabric_mode;
 
@@ -53,7 +53,7 @@ struct AllGatherAsync {
         uint32_t ring_index,
         MemoryConfig output_mem_config,
         ccl::Topology topology,
-        GlobalSemaphore semaphore,
+        std::pair<GlobalSemaphore, GlobalSemaphore> semaphore,
         std::optional<tt::tt_metal::SubDeviceId>& sub_device_id,
         bool enable_persistent_fabric_mode) :
         forward_device(forward_device),
@@ -79,7 +79,8 @@ struct AllGatherAsync {
         attrs.emplace_back("ring_index", ring_index);
         attrs.emplace_back("output_mem_config", output_mem_config);
         attrs.emplace_back("topology", topology);
-        attrs.emplace_back("semaphore", semaphore);
+        attrs.emplace_back("semaphore_first", semaphore.first);
+        attrs.emplace_back("semaphore_second", semaphore.second);
 
         return attrs;
     }
@@ -102,7 +103,7 @@ AllGatherAsync create_all_gather_async_struct(
     const std::optional<MemoryConfig>& memory_config,
     const std::vector<IDevice*>& devices,
     const ccl::Topology topology,
-    const std::vector<GlobalSemaphore>& semaphores,
+    const std::pair<std::vector<GlobalSemaphore>, std::vector<GlobalSemaphore>>& semaphores,
     std::optional<tt::tt_metal::SubDeviceId> sub_device_id,
     bool enable_persistent_fabric_mode);
 }  // namespace all_gather_async_detail
@@ -138,7 +139,7 @@ tt::tt_metal::operation::ProgramWithCallbacks all_gather_async_minimal_interleav
     const uint32_t ring_size,
     const uint32_t ring_index,
     ccl::Topology topology,
-    const GlobalSemaphore& semaphore,
+    const std::pair<GlobalSemaphore, GlobalSemaphore>& semaphores,
     const std::optional<tt::tt_metal::SubDeviceId>& sub_device_id,
     bool enable_persistent_fabric_mode);
 tt::tt_metal::operation::ProgramWithCallbacks all_gather_async_llama_sharded(
@@ -151,7 +152,7 @@ tt::tt_metal::operation::ProgramWithCallbacks all_gather_async_llama_sharded(
     const uint32_t ring_size,
     const uint32_t ring_index,
     ccl::Topology topology,
-    const GlobalSemaphore& semaphore,
+    const std::pair<GlobalSemaphore, GlobalSemaphore>& semaphores,
     const std::optional<tt::tt_metal::SubDeviceId>& sub_device_id,
     bool enable_persistent_fabric_mode);
 
@@ -181,6 +182,14 @@ Tensor all_gather_async(
     std::optional<tt::tt_metal::SubDeviceId> sub_device_id = std::nullopt,
     bool enable_persistent_fabric_mode = false);
 
+Tensor all_gather_async(
+    const Tensor& input_tensor,
+    const uint32_t dim,
+    MeshDevice& mesh_device,
+    const CoreRangeSet& cores,
+    const uint32_t num_links = 1,
+    const std::optional<MemoryConfig>& memory_config = std::nullopt,
+    const ttnn::ccl::Topology topology = ttnn::ccl::Topology::Ring);  // TODO make reference
 }  // namespace ccl
 }  // namespace experimental
 }  // namespace operations
