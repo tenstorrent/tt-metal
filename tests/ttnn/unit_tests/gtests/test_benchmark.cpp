451c7d96829589155bada9f904794788b1644c9e
Edwin Lee <edwinlee@tenstorrent.com>
2025-03-10 22:03:07 +0000

    Enables full sized matmuls, resolving L1 space issues by deallocating the old outputs. Also adds support for block float by using from_vector instead of ones and random calls.

--- tests/ttnn/unit_tests/gtests/test_benchmark.cpp
+++ tests/ttnn/unit_tests/gtests/test_benchmark.cpp
@@ -14,6 +14,9 @@
 
 #include "tools/profiler/op_profiler.hpp"
 #include "ttnn/cpp/ttnn/operations/creation.hpp"
+#include "ttnn/tensor/tensor_spec.hpp"
+#include "ttnn/tensor/layout/tensor_layout.hpp"
+
 #include "ttnn/operations/core/core.hpp"
 #include "ttnn/cpp/ttnn/operations/functions.hpp"
 #include "ttnn/cpp/ttnn/operations/core/compute_kernel/compute_kernel_config.hpp"
@@ -81,14 +84,13 @@ std::vector<std::tuple<int, int, int, bool, bool, int, int, int>> matmul_shapes_
     {1024, 2048, 2048, true, true, 1, 1, 1},
     {2048, 2048, 2048, true, true, 1, 1, 1},
     {2048, 2048, 3072, true, true, 1, 1, 1},
-    // TODO: Enable high dim tests
-    //  {2048, 3072, 3072, true, true, 1, 1, 1},
-    //  {3072, 3072, 3072, true, true, 2, 1, 1},
-    //  {3072, 3072, 4096, true, true, 2, 1, 1},
-    //  {3072, 4096, 4096, true, true, 1, 2, 2},
-    //  {4096, 4096, 4096, false, false, 1, 2, 2},
-    //  {8192, 8192, 8192, false, false, 2, 4, 4},
-    //  {16384, 16384, 16384, false, false, 4, 8, 8},
+    {2048, 3072, 3072, true, true, 1, 1, 1},
+    {3072, 3072, 3072, true, true, 2, 1, 1},
+    {3072, 3072, 4096, true, true, 2, 1, 1},
+    {3072, 4096, 4096, true, true, 1, 2, 2},
+    {4096, 4096, 4096, false, false, 1, 2, 2},
+    {8192, 8192, 8192, false, false, 2, 4, 4},
+    {16384, 16384, 16384, false, false, 4, 8, 8},
 };
 
 std::vector<std::tuple<int, int, int, bool, bool, int, int, int>> matmul_shapes_bfloat4_b = {
@@ -100,13 +102,13 @@ std::vector<std::tuple<int, int, int, bool, bool, int, int, int>> matmul_shapes_
     {1024, 2048, 2048, true, true, 1, 1, 1},
     {2048, 2048, 2048, true, true, 1, 1, 1},
     {2048, 2048, 3072, true, true, 1, 1, 1},
-    // {2048, 3072, 3072, true, true, 1, 1, 1},
-    // {3072, 3072, 3072, true, true, 1, 1, 1},
-    // {3072, 3072, 4096, true, true, 1, 1, 1},
-    // {3072, 4096, 4096, true, true, 2, 1, 1},
-    // {4096, 4096, 4096, true, true, 2, 1, 1},
-    // {8192, 8192, 8192, false, false, 2, 2, 2},
-    // {16384, 16384, 16384, false, false, 4, 4, 4},
+    {2048, 3072, 3072, true, true, 1, 1, 1},
+    {3072, 3072, 3072, true, true, 1, 1, 1},
+    {3072, 3072, 4096, true, true, 1, 1, 1},
+    {3072, 4096, 4096, true, true, 2, 1, 1},
+    {4096, 4096, 4096, true, true, 2, 1, 1},
+    {8192, 8192, 8192, false, false, 2, 2, 2},
+    {16384, 16384, 16384, false, false, 4, 4, 4},
 };
 
 std::vector<std::tuple<DataType, MathFidelity, bool>> matmul_configs = {
@@ -163,7 +165,7 @@ TEST_P(Matmul2DHostPerfTestFixture, Matmul2DHostPerfTest) {
     for (const auto& config : matmul_configs) {
         DataType dtype = std::get<0>(config);
         MathFidelity math_fidelity = std::get<1>(config);
-        bool use_trace = std::get<2>(config);
+        const bool use_trace = std::get<2>(config);
         tt::log_info("Running test with dtype: {}, math_fidelity: {}, use_trace: {}", dtype, math_fidelity, use_trace);
         std::vector<std::tuple<int, int, int, bool, bool, int, int, int>> matmul_shapes;
         if (dtype == DataType::BFLOAT16) {
@@ -175,23 +177,23 @@ TEST_P(Matmul2DHostPerfTestFixture, Matmul2DHostPerfTest) {
         }
 
         for (const auto& shape : matmul_shapes) {
-            int m = std::get<0>(shape);
-            int k = std::get<1>(shape);
-            int n = std::get<2>(shape);
-            bool in0_sharded = std::get<3>(shape);
-            bool out_sharded = std::get<4>(shape);
-            int in0_block_w_div = std::get<5>(shape);
-            int num_out_blocks_h = std::get<6>(shape);
-            int num_out_blocks_w = std::get<7>(shape);
-            std::vector<int64_t> in0_shape = {1, 1, m, k};
-            std::vector<int64_t> in1_shape = {1, 1, k, n};
+            const int m = std::get<0>(shape);
+            const int k = std::get<1>(shape);
+            const int n = std::get<2>(shape);
+            const bool in0_sharded = std::get<3>(shape);
+            const bool out_sharded = std::get<4>(shape);
+            const int in0_block_w_div = std::get<5>(shape);
+            const int num_out_blocks_h = std::get<6>(shape);
+            const int num_out_blocks_w = std::get<7>(shape);
+            const std::vector<int64_t> in0_shape = {1, 1, m, k};
+            const std::vector<int64_t> in1_shape = {1, 1, k, n};
 
-            int in0_block_w = k / std::get<0>(grid_size) / 32 / in0_block_w_div;
-            int per_core_M = m / std::get<1>(grid_size) / tile_h;
-            int per_core_N = n / std::get<0>(grid_size) / tile_w;
-            int out_block_h = per_core_M / num_out_blocks_h;
-            int out_block_w = per_core_N / num_out_blocks_w;
-            auto [out_subblock_h, out_subblock_w] = get_subblock_sizes(out_block_h, out_block_w, out_sharded);
+            const int in0_block_w = k / std::get<0>(grid_size) / 32 / in0_block_w_div;
+            const int per_core_M = m / std::get<1>(grid_size) / tile_h;
+            const int per_core_N = n / std::get<0>(grid_size) / tile_w;
+            const int out_block_h = per_core_M / num_out_blocks_h;
+            const int out_block_w = per_core_N / num_out_blocks_w;
+            const auto [out_subblock_h, out_subblock_w] = get_subblock_sizes(out_block_h, out_block_w, out_sharded);
 
             tt::log_info(
                 "M*K*N = {}*{}*{} out_subblock_h: {}, out_subblock_w: {}", m, k, n, out_subblock_h, out_subblock_w);
@@ -200,7 +202,7 @@ TEST_P(Matmul2DHostPerfTestFixture, Matmul2DHostPerfTest) {
             std::string in1_storage_type = "DRAM";
             std::string out_storage_type = out_sharded ? "L1" : "DRAM";
 
-            ttnn::MemoryConfig in0_memory_config =
+            const ttnn::MemoryConfig in0_memory_config =
                 in0_sharded ? ttnn::operations::data_movement::create_sharded_memory_config(
                                   ttnn::Shape{1, 1, m, k},
                                   ttnn::CoreRangeSet(ttnn::CoreRange(
@@ -210,13 +212,28 @@ TEST_P(Matmul2DHostPerfTestFixture, Matmul2DHostPerfTest) {
                                   tt::tt_metal::ShardOrientation::ROW_MAJOR)
                             : ttnn::DRAM_MEMORY_CONFIG;
 
-            ttnn::Tensor in0_t =
-                ttnn::ones(ttnn::Shape({m, k}), tt::tt_metal::DataType::BFLOAT16, tt::tt_metal::Layout::TILE);
-            in0_t = ttnn::operations::core::to_device(in0_t, device, in0_memory_config, ttnn::DefaultQueueId);
-            ttnn::Tensor in1_t =
-                ttnn::random::random(ttnn::Shape({k, n}), tt::tt_metal::DataType::BFLOAT16, tt::tt_metal::Layout::TILE);
+            // In0 is all ones
+            const std::vector<float> in0_data(m * k, 1.0f);
+            ttnn::Tensor in0_t = Tensor::from_vector(
+                in0_data,
+                ttnn::TensorSpec(
+                    ttnn::Shape({m, k}),
+                    tt::tt_metal::TensorLayout(dtype, tt::tt_metal::Layout::TILE, in0_memory_config)),
+                device);
+            // In1 is random data
+            std::vector<float> in1_data(k * n);
+            std::generate(in1_data.begin(), in1_data.end(), []() {
+                float value = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+                return std::round(value * 15.0f) / 15.0f;
+            });
 
-            in1_t = ttnn::operations::core::to_device(in1_t, device, ttnn::DRAM_MEMORY_CONFIG, ttnn::DefaultQueueId);
+            ttnn::Tensor in1_t = Tensor::from_vector(
+                in1_data,
+                ttnn::TensorSpec(
+                    ttnn::Shape({k, n}),
+                    tt::tt_metal::TensorLayout(dtype, tt::tt_metal::Layout::TILE, ttnn::DRAM_MEMORY_CONFIG)),
+                device);
+
             ttnn::operations::matmul::MatmulMultiCoreReuseMultiCastProgramConfig program_config{
                 /* compute_with_storage_grid_size */ {std::get<0>(grid_size), std::get<1>(grid_size)},
                 /* in0_block_w */ in0_block_w,
@@ -229,59 +246,55 @@ TEST_P(Matmul2DHostPerfTestFixture, Matmul2DHostPerfTest) {
                 /* transpose_mcast */ false,
                 /* fused_activation */ std::nullopt};
 
-            ttnn::WormholeComputeKernelConfig compute_kernel_config =
+            const ttnn::WormholeComputeKernelConfig compute_kernel_config =
                 ttnn::WormholeComputeKernelConfig{math_fidelity, true, false, true};
 
-            ttnn::MemoryConfig out_mem_config =
+            const ttnn::MemoryConfig out_mem_config =
                 out_sharded ? ttnn::MemoryConfig{ttnn::TensorMemoryLayout::BLOCK_SHARDED, ttnn::BufferType::L1}
                             : ttnn::DRAM_MEMORY_CONFIG;
 
-            Tile output_tile =
+            const Tile output_tile =
                 out_sharded ? (tile_h <= 16 ? tt::tt_metal::Tile({tile_h, 32}) : tt::tt_metal::Tile({tile_h, tile_w}))
                             : tt::tt_metal::Tile({tile_h, tile_w});
-            ttnn::Tensor output_tensor = ttnn::matmul(
-                in0_t,
-                in1_t,
+
+            const ttnn::operations::matmul::Matmul matmul_params(
+                program_config,
+                /*bcast_batch*/ std::nullopt,
+                /* output_mem_config */ out_mem_config,
+                /* output_dtype */ dtype,
+                /* compute_kernel_config */ compute_kernel_config,
+                /* untilize_out */ false,
+                /* user_core_coord */ std::nullopt,
+                /* user_fused_activation */ std::nullopt,
+                /* user_run_batched */ false,
                 /* transpose_a */ false,
                 /* transpose_b */ false,
-                /* memory_config */ out_mem_config,
-                /* dtype */ dtype,
-                /* program_config */ program_config,
-                /* activation */ std::nullopt,
-                /* compute_kernel_config */ compute_kernel_config,
-                /* core_grid */ std::nullopt,
                 /* output_tile */ output_tile);
+
+            ttnn::Tensor output_tensor;
+
+            // Warmup iterations
             for (int iter = 0; iter < num_warmup_iterations; ++iter) {
-                ttnn::Tensor output_tensor = ttnn::matmul(
+                output_tensor = ttnn::operations::matmul::matmul(
                     in0_t,
                     in1_t,
-                    /* transpose_a */ false,
-                    /* transpose_b */ false,
-                    /* memory_config */ out_mem_config,
-                    /* dtype */ dtype,
-                    /* program_config */ program_config,
-                    /* activation */ std::nullopt,
-                    /* compute_kernel_config */ compute_kernel_config,
-                    /* core_grid */ std::nullopt,
-                    /* output_tile */ output_tile);
+                    /* bias */ std::nullopt,
+                    /* parameters */ matmul_params);
+                output_tensor.deallocate();
             }
+
             std::chrono::duration<double> total_time = std::chrono::duration<double>::zero();
 
+            // Performance measurement iterations
             if (use_trace) {
                 auto tid = ttnn::operations::trace::begin_trace_capture(device, ttnn::DefaultQueueId);
                 for (int iter = 0; iter < num_measurement_iterations; ++iter) {
-                    ttnn::Tensor output_tensor = ttnn::matmul(
+                    output_tensor = ttnn::operations::matmul::matmul(
                         in0_t,
                         in1_t,
-                        /* transpose_a */ false,
-                        /* transpose_b */ false,
-                        /* memory_config */ out_mem_config,
-                        /* dtype */ dtype,
-                        /* program_config */ program_config,
-                        /* activation */ std::nullopt,
-                        /* compute_kernel_config */ compute_kernel_config,
-                        /* core_grid */ std::nullopt,
-                        /* output_tile */ output_tile);
+                        /* bias */ std::nullopt,
+                        /* parameters */ matmul_params);
+                    output_tensor.deallocate();
                 }
                 ttnn::operations::trace::end_trace_capture(device, tid, ttnn::DefaultQueueId);
 
@@ -293,18 +306,12 @@ TEST_P(Matmul2DHostPerfTestFixture, Matmul2DHostPerfTest) {
             } else {
                 auto start_time = std::chrono::high_resolution_clock::now();
                 for (int iter = 0; iter < num_measurement_iterations; ++iter) {
-                    ttnn::Tensor output_tensor = ttnn::matmul(
+                    output_tensor = ttnn::operations::matmul::matmul(
                         in0_t,
                         in1_t,
-                        /* transpose_a */ false,
-                        /* transpose_b */ false,
-                        /* memory_config */ out_mem_config,
-                        /* dtype */ dtype,
-                        /* program_config */ program_config,
-                        /* activation */ std::nullopt,
-                        /* compute_kernel_config */ compute_kernel_config,
-                        /* core_grid */ std::nullopt,
-                        /* output_tile */ output_tile);
+                        /* bias */ std::nullopt,
+                        /* parameters */ matmul_params);
+                    output_tensor.deallocate();
                 }
                 auto end_time = std::chrono::high_resolution_clock::now();
                 total_time = end_time - start_time;
@@ -342,10 +349,9 @@ TEST_P(Matmul2DHostPerfTestFixture, Matmul2DHostPerfTest) {
                  << utilization_full_grid_percentage << "\n";
             */
 
-            // Deallocate tensors
+            // Deallocate input tensors
             in0_t.deallocate();
             in1_t.deallocate();
-            output_tensor.deallocate();
         }
     }
 }
@@ -353,4 +359,10 @@ TEST_P(Matmul2DHostPerfTestFixture, Matmul2DHostPerfTest) {
 INSTANTIATE_TEST_SUITE_P(
     /*Prefix for the instantiated tests*/ MatmulTests,
     /*Test suite*/ Matmul2DHostPerfTestFixture,
-    ::testing::Values(std::make_tuple(std::make_tuple(8, 8), 32, 32, 1, 1, false)));
+    ::testing::Values(std::make_tuple(
+        /* grid_size */ std::make_tuple(8, 8),
+        /* tile_h */ 32,
+        /* tile_w */ 32,
+        /* num_warmup_iterations */ 1,
+        /* num_measurement_iterations */ 1,
+        /* use_program_cache */ false)));
