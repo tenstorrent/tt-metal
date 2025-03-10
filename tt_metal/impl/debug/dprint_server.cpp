154a5f88a2721cc27611dde7d60e3765730d893f
Artem Yerofieiev <169092593+ayerofieiev-tt@users.noreply.github.com>
2025-03-10 10:45:34 -0700

    Remove details of blockfloat implementation to impl (#18852)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/18434
    
    ### Problem description
    bfloat4/8/16 and blockfloat_common headers contain all the
    implementation.
    
    ### What's changed
    Moved all impl to `tt_metal/impl/blockfloat/*`
    Used explicit instantiation for templates in blockfloat_common.
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/13758997857)
    
    ---------
    
    Co-authored-by: Andrew Fuller <afuller@tenstorrent.com>

--- tt_metal/impl/debug/dprint_server.cpp
+++ tt_metal/impl/debug/dprint_server.cpp
@@ -22,10 +22,12 @@
 #include "dprint_server.hpp"
 #include "debug_helpers.hpp"
 #include <rtoptions.hpp>
-#include <bfloat8.hpp>
+
+#include <tt-metalium/bfloat8.hpp>
+#include <tt-metalium/blockfloat_common.hpp>
+#include <tt-metalium/device.hpp>
 
 #include "hostdevcommon/dprint_common.h"
-#include <device.hpp>
 
 using std::cout;
 using std::endl;
