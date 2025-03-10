ee639f7fdd2e676cb3d653990b4d7611ebf842a2
Nigel Huang <nhuang@tenstorrent.com>
2025-03-10 17:30:41 +0000

    Rename dispatch_downstream_noc to k_dispatch_downstream_noc

--- tt_metal/impl/dispatch/topology.hpp
+++ tt_metal/impl/dispatch/topology.hpp
@@ -13,7 +13,7 @@ constexpr uint32_t k_dispatch_max_downstream_kernels = 4;
 
 // NOC ID used by dispatch kernels to communicate with downstream cores. This parameter
 // is required when setting up Command Queue objects on host.
-static constexpr NOC dispatch_downstream_noc = NOC::NOC_0;
+constexpr NOC k_dispatch_downstream_noc = NOC::NOC_0;
 
 struct DispatchKernelNode {
     int id;
