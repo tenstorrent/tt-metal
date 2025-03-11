98f7395f88124ce0d8c6b63092034615463f5957
Artem Yerofieiev <artem@tenstorrent.com>
2025-03-11 00:13:21 +0000

    removed outdated comment

--- tt_metal/impl/allocator/l1_banking_allocator.hpp
+++ tt_metal/impl/allocator/l1_banking_allocator.hpp
@@ -14,12 +14,6 @@ namespace tt_metal {
 
 struct AllocatorConfig;
 
-// For Grayskull:
-// There are 108 (9x12) compute and storage cores where each core has one 1 MB bank with top 512 KB (non-exclusively)
-// dedicated to L1 buffer storage. Circular buffers can grow into L1 buffer storage space but L1 buffers cannot grow
-// past 512 KB. There are an additional 10 storage cores where each core has two banks of 512 KB dedicated solely to L1
-// buffer storage. This gives a total of (108 + 1 bank) + (10 * 2 banks) = 128 banks of 512 KB for L1 buffers DRAM
-// allocation is the same as BasicAllocator
 class L1BankingAllocator : public Allocator {
 public:
     L1BankingAllocator(const AllocatorConfig& alloc_config);
