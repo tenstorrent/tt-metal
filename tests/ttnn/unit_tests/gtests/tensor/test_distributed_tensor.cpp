b684d7ff9814a7d45e0173946a2f14c8b8866432
Jeffrey Jiang <jjiang@tenstorrent.com>
2025-03-11 18:31:33 -0500

    #18842: Add borrowed storage support for the aggregate_as_tensor function (#18555)
    ### Ticket
    Link to Github Issue
    
    ### Problem description
    The aggregate_as_tensor function currently goes straight to assuming
    anything that isn't an owned tensor is a device tensor and then failing.
    Borrowed storage is currently the "preferred default" so newly created
    non-bfp_b types would require a conversion before they could be
    aggregated otherwise.
    
    ### What's changed
    Added support for borrowed storage tensors by turning them into owned
    tensors and then aggregating them into a multidevicehost storage backed
    tensor.
    Added TT_FATAL checking to prevent any other types from crashing on the
    storage variant fetch and give some more visibility into failures.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/13790103623
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    - [x] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [x] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [x] New/Existing tests provide coverage for changes:
    tests/ttnn/unit_tests/gtests/tensor/test_distributed_tensor.cpp
    
    ---------
    
    Co-authored-by: Oleg Milyutin <omilyutin@tenstorrent.com>

--- tests/ttnn/unit_tests/gtests/tensor/test_distributed_tensor.cpp
+++ tests/ttnn/unit_tests/gtests/tensor/test_distributed_tensor.cpp
@@ -16,11 +16,52 @@ namespace ttnn::distributed::test {
 
 using ::testing::ElementsAre;
 
-using TensorDistributionTest = T3kMultiDeviceFixture;
-
 TensorSpec get_tensor_spec(const ttnn::Shape& shape, DataType dtype) {
     return TensorSpec(shape, TensorLayout(dtype, Layout::ROW_MAJOR, MemoryConfig{}));
 }
+
+class AggregateTensorTest : public T3kMultiDeviceFixture,
+                            public ::testing::WithParamInterface</*use_borrowed_storage*/ bool> {};
+
+TEST_P(AggregateTensorTest, Roundtrip) {
+    const bool use_borrowed_storage = GetParam();
+    const int num_devices = mesh_device_->num_devices();
+    std::vector<std::vector<float>> test_data(num_devices);
+    for (int i = 0; i < num_devices; i++) {
+        test_data[i] = std::vector<float>{i * 1.F, i * 2.F, i * 3.F};
+    }
+
+    std::vector<Tensor> tensors;
+    tensors.reserve(num_devices);
+
+    for (int i = 0; i < num_devices; i++) {
+        if (use_borrowed_storage) {
+            tensors.push_back(Tensor::from_borrowed_data(
+                tt::stl::Span(test_data[i]),
+                ttnn::Shape{1, 1, 3, 1},
+                /*on_creation_callback=*/[]() {},
+                /*on_destruction_callback=*/[]() {}));
+        } else {
+            tensors.push_back(
+                Tensor::from_vector(test_data[i], get_tensor_spec(ttnn::Shape{1, 1, 3, 1}, DataType::FLOAT32)));
+        }
+    }
+
+    Tensor aggregated_tensor = aggregate_as_tensor(tensors, AllGatherTensor{});
+    EXPECT_TRUE(aggregated_tensor.storage_type() == StorageType::MULTI_DEVICE_HOST);
+
+    const auto tensor_shards = get_device_tensors(aggregated_tensor);
+    ASSERT_EQ(tensor_shards.size(), test_data.size());
+
+    size_t i = 0;
+    for (const auto& tensor_shard : tensor_shards) {
+        EXPECT_EQ(tensor_shard.to_vector<float>(), test_data[i++]);
+    }
+}
+
+INSTANTIATE_TEST_SUITE_P(AggregateTensorTest, AggregateTensorTest, ::testing::Values(true, false));
+
+using TensorDistributionTest = T3kMultiDeviceFixture;
 
 TEST_F(TensorDistributionTest, DistributeToDevice) {
     Tensor input_tensor = Tensor::from_vector(
