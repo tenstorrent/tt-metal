daf893aa167d416dca3556e1a384f08e52d11c08
Samarth Agarwal <sagarwal@tenstorrent.com>
2025-03-12 17:06:51 +0000

    Updated test

--- tests/tt_metal/tt_metal/api/test_compile_time_args.cpp
+++ tests/tt_metal/tt_metal/api/test_compile_time_args.cpp
@@ -6,6 +6,8 @@
 
 #include "device.hpp"
 #include "device_fixture.hpp"
+#include "hal.hpp"
+#include <string>
 #include <tt-metalium/kernel.hpp>
 #include <tt-metalium/tt_metal.hpp>
 #include <tt-metalium/host_api.hpp>
@@ -13,19 +15,20 @@
 using namespace tt;
 using namespace tt::tt_metal;
 
-TEST_F(DeviceFixture, TensixTestFourHundredCompileTimeArgs) {
-    // This test will hang/assert if there is a failure
+TEST_F(DeviceFixture, TensixTestSixtyThousandCompileTimeArgs) {
     for (IDevice* device : this->devices_) {
         CoreCoord core = {0, 0};
         Program program;
 
-        const uint32_t num_compile_time_args = 400;
+        const uint32_t write_addr = hal.get_dev_addr(HalProgrammableCoreType::TENSIX, HalL1MemAddrType::UNRESERVED);
+
+        const std::map<string, string>& defines = {{"WRITE_ADDRESS", std::to_string(write_addr)}};
+
+        const uint32_t num_compile_time_args = 60000;
         std::vector<uint32_t> compile_time_args;
         for (uint32_t i = 0; i < num_compile_time_args; i++) {
-            compile_time_args.push_back(i);
+            compile_time_args.push_back(1);
         }
-
-        const std::map<string, string>& defines = {{"NUM_COMPILE_TIME_ARGS", std::to_string(num_compile_time_args)}};
 
         CreateKernel(
             program,
@@ -37,23 +40,12 @@ TEST_F(DeviceFixture, TensixTestFourHundredCompileTimeArgs) {
                 .compile_args = compile_time_args,
                 .defines = defines});
         this->RunProgram(device, program);
-    }
-}
 
-TEST_F(DeviceFixture, TensixTestZeroCompileTimeArgs) {
-    // This test will hang/assert if there is a failure
-    for (IDevice* device : this->devices_) {
-        CoreCoord core = {0, 0};
-        Program program;
+        const std::vector<uint32_t> compile_time_args_expected{num_compile_time_args};
 
-        const std::map<string, string>& defines = {{"NUM_COMPILE_TIME_ARGS", "0"}};
+        std::vector<uint32_t> compile_time_args_actual;
+        detail::ReadFromDeviceL1(device, core, write_addr, sizeof(uint32_t), compile_time_args_actual);
 
-        CreateKernel(
-            program,
-            "tests/tt_metal/tt_metal/test_kernels/misc/compile_time_args_kernel.cpp",
-            core,
-            DataMovementConfig{
-                .processor = DataMovementProcessor::RISCV_0, .noc = NOC::RISCV_0_default, .defines = defines});
-        this->RunProgram(device, program);
+        ASSERT_EQ(compile_time_args_actual, compile_time_args_expected);
     }
 }
