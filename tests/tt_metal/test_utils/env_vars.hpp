403573ce2659e33fe8f4a7c9bbc4624e1b94a512
Pavle Janevski <165378935+pjanevskiTT@users.noreply.github.com>
2025-03-12 09:58:09 +0100

    Bump UMD with UBB changes (#18859)
    ### Ticket
    
    Bump UMD with the changes to support UBB workloads
    
    ### Problem description
    
    Cluster descriptor API which always uses create-ethernet-map is moved to
    private in order to not call create-ethernet-map for architectures which
    are not supported. Make metal changes accordingly
    
    ### What's changed
    
    - Replace API calls for proper creation of cluster descriptor
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes -
    https://github.com/tenstorrent/tt-metal/actions/runs/13765772388
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [ ] New/Existing tests provide coverage for changes

--- tests/tt_metal/test_utils/env_vars.hpp
+++ tests/tt_metal/test_utils/env_vars.hpp
@@ -46,7 +46,7 @@ inline std::string get_umd_arch_name() {
         return tt::arch_to_str(init.get_arch_name());
     }
 
-    auto cluster_desc = tt_ClusterDescriptor::create();
+    auto cluster_desc = tt::umd::Cluster::create_cluster_descriptor();
     const std::unordered_set<chip_id_t> &device_ids = cluster_desc->get_all_chips();
     tt::ARCH arch = cluster_desc->get_arch(*device_ids.begin());
     for (auto device_id : device_ids) {
