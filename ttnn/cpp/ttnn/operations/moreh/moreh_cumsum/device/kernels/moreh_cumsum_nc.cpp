51cd2d84122929307d7b23343173f3d8d573539d
bfilipovicTT <ctr-bfilipovic@ext.tenstorrent.com>
2025-03-12 13:50:59 +0000

    #17594: split moreh_cumsum compute into sfpu and non-sfpu kernel

--- ttnn/cpp/ttnn/operations/moreh/moreh_cumsum/device/kernels/moreh_cumsum_nc.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_cumsum/device/kernels/moreh_cumsum_nc.cpp
@@ -3,15 +3,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include <cstdint>
-
-#include "compute_kernel_api/eltwise_binary_sfpu.h"
 #include "compute_kernel_api/eltwise_binary.h"
-#include "compute_kernel_api/eltwise_unary/eltwise_unary.h"
 #include "compute_kernel_api/tile_move_copy.h"
-
-#define APPROX false
-#include "compute_kernel_api/add_int32_sfpu.h"
-#include "compute_kernel_api/common.h"
 
 ALWI void ACQ() { acquire_dst(); }
 ALWI void REL() { release_dst(); }
@@ -24,126 +17,48 @@ void MAIN {
     constexpr auto cb_in0 = tt::CBIndex::c_0;
     constexpr auto cb_in1 = tt::CBIndex::c_1;
     constexpr auto cb_out0 = tt::CBIndex::c_16;
-    constexpr auto cb_intermed = tt::CBIndex::c_24;
+    constexpr auto cb_intermed0 = tt::CBIndex::c_24;
+    constexpr uint32_t onetile = 1;
     constexpr uint32_t dst0 = 0;
-    constexpr uint32_t dst1 = 1;
     constexpr uint32_t first_tile = 0;
 
-    // binary_op_init_common(cb_in0, cb_in1, cb_out0);
-    unary_op_init_common(cb_in0, cb_out0);
-    // unary_op_init_common(cb_out0, cb_intermed);
-
-    cb_wait_front(cb_in1, 1);
+    binary_op_init_common(cb_in0, cb_in1, cb_out0);
+    cb_wait_front(cb_in1, onetile);
 
     for (uint32_t i = 0; i < num_output_tiles_per_core; i++) {
-        // Initialize cb_intermed with cb_in1 (0)
-        ACQ();
-        copy_tile_to_dst_init_short(cb_in1);
-        copy_tile(cb_in1, first_tile, dst0);
-
-        cb_reserve_back(cb_intermed, 1);
-        pack_tile(dst0, cb_intermed);
-        cb_push_back(cb_intermed, 1);
-        REL();
-
+        bool enable_reload = false;
         for (uint32_t j = 0; j < num_tiles_to_cumsum; ++j) {
             ACQ();
+            uint32_t cb_add = (enable_reload) ? (cb_intermed0) : (cb_in1);
+            cb_wait_front(cb_in0, onetile);
 
-            // Copy intermediate sum to dst0
-            cb_wait_front(cb_intermed, 1);  // Wait for one tile
-            copy_tile_to_dst_init_short(cb_intermed);
-            copy_tile(cb_intermed, first_tile, dst0);
-            cb_pop_front(cb_intermed, 1);  // Pop one tile
+            add_tiles_init(cb_in0, cb_add);
+            add_tiles(cb_in0, cb_add, first_tile, first_tile, dst0);
 
-            // Copy input tile to dst1
-            cb_wait_front(cb_in0, 1);
-            copy_tile_to_dst_init_short(cb_in0);
-            copy_tile(cb_in0, first_tile, dst1);
-            cb_pop_front(cb_in0, 1);
+            cb_pop_front(cb_in0, onetile);
+            if (enable_reload) {
+                cb_pop_front(cb_intermed0, onetile);
+            }
 
-            // add_binary_tile_init();
-            // add_binary_tile(dst0, dst1);
-
-            // Add tiles in dst0 and dst1. Store result to dst0
-            add_int32_tile_init();
-            add_int32_tile(dst0, dst1);
-
-            // Copy sum to intermediate CB
-            cb_reserve_back(cb_intermed, 1);
-            pack_tile(dst0, cb_intermed);
-            cb_push_back(cb_intermed, 1);
-
-            // Copy sum to output
-            cb_reserve_back(cb_out0, 1);
-            pack_tile(dst0, cb_out0);
-            cb_push_back(cb_out0, 1);
-
+            // pack to intermed0
+            cb_reserve_back(cb_intermed0, onetile);
+            pack_tile(dst0, cb_intermed0);
+            cb_push_back(cb_intermed0, onetile);
             REL();
+
+            // copy tile from intermed0 to out0
+            ACQ();
+            cb_wait_front(cb_intermed0, onetile);
+            copy_tile_to_dst_init_short(tt::CBIndex::c_24);
+            copy_tile(tt::CBIndex::c_24, first_tile, dst0);
+            cb_reserve_back(cb_out0, onetile);
+            pack_tile(dst0, cb_out0);
+            cb_push_back(cb_out0, onetile);
+            REL();
+            enable_reload = true;
         }
+        cb_pop_front(cb_intermed0, onetile);
     }
-    cb_pop_front(cb_in1, 1);
+    cb_pop_front(cb_in1, onetile);
 }
 }  // namespace NAMESPACE
-
-//
-// #include <cstdint>
-
-// #include "compute_kernel_api/eltwise_binary.h"
-// #include "compute_kernel_api/tile_move_copy.h"
-
-// ALWI void ACQ() { acquire_dst(); }
-// ALWI void REL() { release_dst(); }
-
-// namespace NAMESPACE {
-// void MAIN {
-//     const auto num_tiles_to_cumsum = get_arg_val<uint32_t>(0);
-//     const auto num_output_tiles_per_core = get_arg_val<uint32_t>(1);
-
-//     constexpr auto cb_in0 = tt::CBIndex::c_0;
-//     constexpr auto cb_in1 = tt::CBIndex::c_1;
-//     constexpr auto cb_out0 = tt::CBIndex::c_16;
-//     constexpr auto cb_intermed0 = tt::CBIndex::c_24;
-//     constexpr uint32_t onetile = 1;
-//     constexpr uint32_t dst0 = 0;
-//     constexpr uint32_t first_tile = 0;
-
-//     binary_op_init_common(cb_in0, cb_in1, cb_out0);
-//     cb_wait_front(cb_in1, onetile);
-
-//     for (uint32_t i = 0; i < num_output_tiles_per_core; i++) {
-//         bool enable_reload = false;
-//         for (uint32_t j = 0; j < num_tiles_to_cumsum; ++j) {
-//             ACQ();
-//             uint32_t cb_add = (enable_reload) ? (cb_intermed0) : (cb_in1);
-//             cb_wait_front(cb_in0, onetile);
-
-//             add_tiles_init(cb_in0, cb_add);
-//             add_tiles(cb_in0, cb_add, first_tile, first_tile, dst0);
-
-//             cb_pop_front(cb_in0, onetile);
-//             if (enable_reload) {
-//                 cb_pop_front(cb_intermed0, onetile);
-//             }
-
-//             // pack to intermed0
-//             cb_reserve_back(cb_intermed0, onetile);
-//             pack_tile(dst0, cb_intermed0);
-//             cb_push_back(cb_intermed0, onetile);
-//             REL();
-
-//             // copy tile from intermed0 to out0
-//             ACQ();
-//             cb_wait_front(cb_intermed0, onetile);
-//             copy_tile_to_dst_init_short(tt::CBIndex::c_24);
-//             copy_tile(tt::CBIndex::c_24, first_tile, dst0);
-//             cb_reserve_back(cb_out0, onetile);
-//             pack_tile(dst0, cb_out0);
-//             cb_push_back(cb_out0, onetile);
-//             REL();
-//             enable_reload = true;
-//         }
-//         cb_pop_front(cb_intermed0, onetile);
-//     }
-//     cb_pop_front(cb_in1, onetile);
-// }
-// }  // namespace NAMESPACE
