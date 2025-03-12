a0eb6cd00e4611ebceeeee873f13c33fdf6a208e
Samarth Agarwal <sagarwal@tenstorrent.com>
2025-03-12 17:06:51 +0000

    Updated test

--- tests/tt_metal/tt_metal/test_kernels/misc/compile_time_args_kernel.cpp
+++ tests/tt_metal/tt_metal/test_kernels/misc/compile_time_args_kernel.cpp
@@ -2,15 +2,19 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+#include "dataflow_api.h"
+
+template <size_t N>
+constexpr uint32_t sum(const std::array<uint32_t, N>& array) {
+    uint32_t sum = 0;
+    for (uint32_t i = 0; i < N; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
+
 void kernel_main() {
-    if (kernel_compile_time_args.size() != NUM_COMPILE_TIME_ARGS) {
-        ASSERT(0);
-        while (1);
-    }
-    for (uint32_t i = 0; i < NUM_COMPILE_TIME_ARGS; i++) {
-        if (kernel_compile_time_args[i] != i) {
-            ASSERT(0);
-            while (1);
-        }
-    }
+    uint32_t compile_time_args_sum = sum(kernel_compile_time_args);
+    volatile uint32_t tt_l1_ptr* l1_ptr = (volatile uint32_t tt_l1_ptr*)WRITE_ADDRESS;
+    *l1_ptr = compile_time_args_sum;
 }
