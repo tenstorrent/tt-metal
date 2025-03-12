3559de3c81a5841e70117c304101631fadac7d4f
blozano-tt <blozano@tenstorrent.com>
2025-03-12 05:48:07 +0000

    Fix thing

--- tt_metal/llrt/hal.cpp
+++ tt_metal/llrt/hal.cpp
@@ -16,6 +16,8 @@ namespace tt_metal {
 // Once it knows the architecture it can self initialize architecture specific memory maps
 Hal::Hal() : arch_(get_platform_architecture()) {
     switch (this->arch_) {
+        case tt::ARCH::GRAYSKULL: /*TT_THROW("Unsupported arch for HAL")*/; break;
+
         case tt::ARCH::WORMHOLE_B0: initialize_wh(); break;
 
         case tt::ARCH::BLACKHOLE: initialize_bh(); break;
