69e206e0ff11b721061b6efe4c5f7259123987ae
Stuti Raizada <159130512+sraizada-tt@users.noreply.github.com>
2025-03-12 09:03:21 +0000

    Add Llama70B on TG with prefetcher and fabric(30 t/s/u) (#18778)
    ### Problem description
    Bringup optimized llama 70B TP=32 on TG 
    
    ### What's changed
    Added llama_subdevices folder that runs llama modules with prefetched
    and fabric enabled ccls
    
    ### Checklist
    - [x] [All post
    commit]https://github.com/tenstorrent/tt-metal/actions/runs/13789656753
    CI passes
    - [x] TG demo :
    https://github.com/tenstorrent/tt-metal/actions/runs/13787572929
    - [x] unit/frequent:
    https://github.com/tenstorrent/tt-metal/actions/runs/13787675538/job/38572327369
    
    ---------
    
    Co-authored-by: kpaigwar <kpaigwar@tenstorrent.com>
    Co-authored-by: avoraTT <avora@tenstorrent.com>
    Co-authored-by: Johanna Rock <129077594+johanna-rock-tt@users.noreply.github.com>

--- tests/scripts/tg/run_tg_frequent_tests.sh
+++ tests/scripts/tg/run_tg_frequent_tests.sh
@@ -7,21 +7,13 @@ run_tg_llama3_tests() {
 
   echo "LOG_METAL: Running run_tg_llama3_tests"
 
-  # Llama3.2-1B
-  llama1b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.2-1B-Instruct/
-  # Llama3.2-3B
-  llama3b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.2-3B-Instruct/
-  # Llama3.1-8B
-  llama8b=/mnt/MLPerf/tt_dnn-models/llama/Meta-Llama-3.1-8B-Instruct/
-  # Llama3.2-11B
-  llama11b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.2-11B-Vision-Instruct/
   # Llama3.1-70B
   llama70b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.1-70B-Instruct/
 
   # Run all Llama3 tests for 8B, 1B, and 3B weights
   # for llama_dir in "$llama1b" "$llama3b" "$llama8b" "$llama11b" "$llama70b"; do
   for llama_dir in "$llama70b"; do
-    LLAMA_DIR=$llama_dir MESH_DEVICE=TG pytest --timeout 1800 -n auto models/tt_transformers/tests/test_model.py -k full --timeout=1800 ; fail+=$?
+    LLAMA_DIR=$llama_dir FAKE_DEVICE=TG pytest --timeout 1800 -n auto models/demos/llama3_subdevices/tests/test_llama_model.py -k full --timeout=1800 ; fail+=$?
     echo "LOG_METAL: Llama3 tests for $llama_dir completed"
   done
 
