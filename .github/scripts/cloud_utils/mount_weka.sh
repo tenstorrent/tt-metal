1fc79c92a35e21b2cb21c3977a7a94fdacd5a51c
Raymond Kim <rkim@tenstorrent.com>
2025-03-13 16:47:35 -0400

    Wtf why is complaining about systemd?

--- .github/scripts/cloud_utils/mount_weka.sh
+++ .github/scripts/cloud_utils/mount_weka.sh
@@ -2,6 +2,9 @@
 
 set -eo pipefail
 
+echo "Checking for weka..."
+sleep 3
+
 if [ ! -d "/mnt/MLPerf/ccache" ]; then
   echo "::error title=mlperf-not-mounted::Weka is not mounted. Please let the infra team know."
   exit 1
