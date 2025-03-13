6607cf773dac344ac21d9eeae353bc616745dc45
Brett Grady <bgrady@tenstorrent.com>
2025-03-13 11:44:33 -0400

    #18890 - Device Profiler NoC Tracing Feature (#18890)
    ### Problem Description
    This PR adds support for recording detailed traces of all NoC activity
    initiated by Tensix worker cores. This data is then written to a JSON
    format file. These JSON traces can be either analyzed directly (as a
    sort of log file) or consumed by either flows (e.g. used to verify
    software noc performance estimator).
    
    ### What's Changed
    NoC tracing builds on top of the timestamped data packets
    (`PacketType::TS_DATA`) feature in the kernel profiler.
    
    NoC tracing can be enabled on top of normal kernel profiling by setting
    the ENV variable `TT_METAL_DEVICE_PROFILER_NOC_EVENTS=1`.
    
    The directory NoC traces are written to is configurable using the ENV
    variable `TT_METAL_DEVICE_PROFILER_NOC_EVENTS_RPT_PATH`.
    
    A timestamped packet is recorded on each worker core for each call to
    `dataflow_api.h:noc_async_*` functions. All arguments to the
    dataflow_api.h function are bit-packed into the TS_DATA payload. This is
    done by adding new stripped-by-default macros to each noc_async* call of
    interest in dataflow_api.h.
    
    ```cpp
      // example of RECORD macro within dataflow_api.h
      inline void noc_async_read(...) {
          // if TT_METAL_DEVICE_PROFILER_NOC_EVENTS != 1, the following macro is stripped out
          RECORD_NOC_EVENT_WITH_ADDR(...);
          // noc_async_read continues unmodified here ... 
      }
      ```
    
    #### Checklist
     - [X] Post commit CI passes
     - [X] Blackhole post commit CI passes
     - [X] Device Perf, uBenchmark, and multi-device (T3K) tests pass

---
+++ tt_metal/programming_examples/profiler/test_noc_event_profiler/test_noc_event_profiler.cpp
@@ -0,0 +1,101 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <tt-metalium/host_api.hpp>
+#include <tt-metalium/device.hpp>
+#include <tt-metalium/bfloat16.hpp>
+
+using namespace tt::tt_metal;
+
+/*
+ * This test serves as a simple, stable tt_metal executable that issues both
+ * reads and writes from Tensix to the NoC. It is used to do sanity checking of
+ * the Device Profiler's NoC event capture feature during CI in
+ * test_device_profiler.py.
+ */
+
+int main(int argc, char** argv) {
+    if (getenv("TT_METAL_SLOW_DISPATCH_MODE") != nullptr) {
+        TT_THROW("Test not supported w/ slow dispatch, exiting");
+    }
+
+    bool pass = true;
+
+    try {
+        constexpr int device_id = 0;
+        IDevice* device = CreateDevice(device_id);
+        CommandQueue& cq = device->command_queue();
+        Program program = CreateProgram();
+
+        constexpr CoreCoord core = {0, 0};
+
+        // See kernel cpp code for details on which noc calls are captured
+        KernelHandle dram_copy_kernel_id = CreateKernel(
+            program,
+            "tt_metal/programming_examples/profiler/test_noc_event_profiler/kernels/loopback_dram_copy.cpp",
+            core,
+            DataMovementConfig{.processor = DataMovementProcessor::RISCV_0, .noc = NOC::RISCV_0_default});
+
+        // boilerplate setup for reading and writing multiple tiles from DRAM
+        constexpr uint32_t single_tile_size = 2 * (32 * 32);
+        constexpr uint32_t num_tiles = 5;
+        constexpr uint32_t dram_buffer_size = single_tile_size * num_tiles;
+
+        tt::tt_metal::InterleavedBufferConfig dram_config{
+            .device = device,
+            .size = dram_buffer_size,
+            .page_size = dram_buffer_size,
+            .buffer_type = tt::tt_metal::BufferType::DRAM};
+        tt::tt_metal::InterleavedBufferConfig l1_config{
+            .device = device,
+            .size = dram_buffer_size,
+            .page_size = dram_buffer_size,
+            .buffer_type = tt::tt_metal::BufferType::L1};
+
+        auto l1_buffer = CreateBuffer(l1_config);
+
+        auto input_dram_buffer = CreateBuffer(dram_config);
+        const uint32_t input_dram_buffer_addr = input_dram_buffer->address();
+
+        auto output_dram_buffer = CreateBuffer(dram_config);
+        const uint32_t output_dram_buffer_addr = output_dram_buffer->address();
+
+        // Since all interleaved buffers have size == page_size, they are entirely contained in the first DRAM bank
+        const uint32_t input_bank_id = 0;
+        const uint32_t output_bank_id = 0;
+
+        const std::vector<uint32_t> runtime_args = {
+            l1_buffer->address(),
+            input_dram_buffer->address(),
+            input_bank_id,
+            output_dram_buffer->address(),
+            output_bank_id,
+            l1_buffer->size()};
+        SetRuntimeArgs(program, dram_copy_kernel_id, core, runtime_args);
+
+        EnqueueProgram(cq, program, false);
+        Finish(cq);
+
+        // It is necessary to explictly dump profile results at the end of the
+        // program to get noc traces for standalone tt_metal programs.  For
+        // ttnn, this is called _automatically_
+        DumpDeviceProfileResults(device, program);
+
+        pass &= CloseDevice(device);
+
+    } catch (const std::exception& e) {
+        tt::log_error(tt::LogTest, "Test failed with exception!");
+        tt::log_error(tt::LogTest, "{}", e.what());
+
+        throw;
+    }
+
+    if (pass) {
+        tt::log_info(tt::LogTest, "Test Passed");
+    } else {
+        TT_THROW("Test Failed");
+    }
+
+    return 0;
+}
