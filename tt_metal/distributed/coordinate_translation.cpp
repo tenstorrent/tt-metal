4122eb39ae5817dcd1da98ca387cba148999d5dc
Joseph Chu <jchu@tenstorrent.com>
2025-03-13 04:33:33 +0000

    #19037: TT-NN MeshDevice Support for 6U
    (cherry picked from commit e451d40ec3e2aed6223b98987681dd868a15d33d)

--- tt_metal/distributed/coordinate_translation.cpp
+++ tt_metal/distributed/coordinate_translation.cpp
@@ -3,90 +3,40 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "tt_metal/distributed/coordinate_translation.hpp"
+#include "tt_metal/api/tt-metalium/tt_metal.hpp"
 
-#include <tt_stl/indestructible.hpp>
-#include "tt_cluster.hpp"
-
-#include <nlohmann/json.hpp>
-
+#include "indestructible.hpp"
 namespace tt::tt_metal::distributed {
 
-namespace {
+const MeshContainer<PhysicalMeshCoordinate>& get_system_mesh_coordinate_translation_map() {
+    static tt::stl::Indestructible<MeshContainer<PhysicalMeshCoordinate>> kTranslationMap([]() {
+        const auto* control_plane = tt::Cluster::instance().get_control_plane();
+        TT_FATAL(control_plane != nullptr, "Control plane must be initialized before MeshDevice can be created.");
 
-std::string get_config_path(const std::string& filename) {
-    std::string root_path = getenv("TT_METAL_HOME") ? getenv("TT_METAL_HOME") : "./";
-    return root_path + "/tt_metal/distributed/config/" + filename;
-}
+        const auto mesh_ids = control_plane->get_user_physical_mesh_ids();
+        TT_FATAL(!mesh_ids.empty(), "There are no user physical meshes in the system found by control plane.");
 
-CoordinateTranslationMap load_translation_map(const std::string& filename, const std::string& key) {
-    std::ifstream file(filename);
-    TT_FATAL(file.is_open(), "Unable to open file: {}", filename);
-
-    nlohmann::json j;
-    try {
-        file >> j;
-    } catch (const nlohmann::json::parse_error& e) {
-        TT_THROW("JSON parsing error in file {}: {}", filename, e.what());
-    }
-
-    TT_FATAL(j.contains(key), "Key '{}' not found in JSON file: {}", key, filename);
-
-    CoordinateTranslationMap result;
-    for (const auto& mapping : j[key]) {
-        if (mapping.size() != 2 || mapping[0].size() != 2 || mapping[1].size() != 5) {
-            TT_THROW("Invalid coordinate format in JSON file: {}", filename);
+        if (mesh_ids.size() > 1) {
+            tt::log_warning(LogMetal, "Only one user physical mesh is supported, using the first one");
         }
-        result.emplace(
-            MeshCoordinate(mapping[0][0], mapping[0][1]),
-            PhysicalCoordinate{
-                mapping[1][0],  // cluster_id
-                mapping[1][2],  // x
-                mapping[1][1],  // y
-                mapping[1][3],  // rack
-                mapping[1][4]   // shelf
-            });
-    }
 
-    return result;
-}
+        const auto mesh_id = mesh_ids.front();
+        auto mesh_shape = control_plane->get_physical_mesh_shape(mesh_id);
+        MeshContainer<PhysicalMeshCoordinate> physical_coordinates(
+            mesh_shape, PhysicalMeshCoordinate(/*mesh_id=*/mesh_id, /*chip_id=*/0));
 
-}  // namespace
+        for (int logical_chip_id = 0; logical_chip_id < mesh_shape.mesh_size(); ++logical_chip_id) {
+            // Query the control plane to get the physical chip id from logical chip id
+            auto logical_row_idx = logical_chip_id / mesh_shape[1];
+            auto logical_col_idx = logical_chip_id % mesh_shape[1];
+            auto logical_coordinate = MeshCoordinate(logical_row_idx, logical_col_idx);
+            auto physical_chip_id = control_plane->get_physical_chip_id_from_mesh_chip_id({mesh_id, logical_chip_id});
 
-const std::pair<CoordinateTranslationMap, MeshShape>& get_system_mesh_coordinate_translation_map() {
-    static tt::stl::Indestructible<std::pair<CoordinateTranslationMap, MeshShape>> kTranslationMap([]() {
-        const auto system_num_devices = tt::Cluster::instance().number_of_user_devices();
-
-        const bool is_qg = tt::Cluster::instance().number_of_pci_devices() == system_num_devices;
-
-        // TODO: #17477 - This assumes shapes and coordinates are in 2D. This will be extended for 3D.
-        // Consider if 1D can be used for single device and N300.
-        // TODO: These assume devices have chip coordinates which is not true for BH or WH QCs
-        const std::unordered_map<size_t, std::pair<std::string, MeshShape>> system_mesh_translation_map = {
-            {1, std::make_pair("device.json", MeshShape(1, 1))},
-            {2, std::make_pair("N300.json", MeshShape(1, 2))},
-            {4, std::make_pair("4xP150.json", MeshShape(1, 4))},
-            {8, std::make_pair("T3000.json", MeshShape(2, 4))},
-            {32, std::make_pair(is_qg ? "QG.json" : "TG.json", MeshShape(8, 4))},
-            {64, std::make_pair("TGG.json", MeshShape(8, 8))},
-        };
-        TT_FATAL(
-            system_mesh_translation_map.contains(system_num_devices),
-            "Unsupported number of devices: {}",
-            system_num_devices);
-
-        const auto [translation_config_file, shape] = system_mesh_translation_map.at(system_num_devices);
-        TT_FATAL(
-            system_num_devices == shape.mesh_size(),
-            "Mismatch between number of devices and the mesh shape: {} != {}",
-            system_num_devices,
-            shape.mesh_size());
-        log_debug(LogMetal, "Logical SystemMesh Shape: {}", shape);
-
-        return std::pair<CoordinateTranslationMap, MeshShape>{
-            load_translation_map(get_config_path(translation_config_file), /*key=*/"logical_to_physical_coordinates"),
-            shape};
+            auto physical_coordinate = PhysicalMeshCoordinate(/*mesh_id=*/mesh_id, /*chip_id=*/physical_chip_id);
+            physical_coordinates.at(logical_coordinate) = physical_coordinate;
+        }
+        return physical_coordinates;
     }());
-
     return kTranslationMap.get();
 }
 
