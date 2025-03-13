08076162e52fb7cc4ad96256fce0b6ba3c0be621
blozano-tt <blozano@tenstorrent.com>
2025-03-13 08:10:45 +0000

    Revert "Fix clang-tidy issue"
    This reverts commit c6279392cf76a0fff951deaaba7b0fadfff7f74c.

--- ttnn/cpp/ttnn/operations/data_movement/move/device/move_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/move/device/move_program_factory.cpp
@@ -10,7 +10,6 @@
 #include <tt-metalium/host_api.hpp>
 #include <tt-metalium/constants.hpp>
 #include <tt-metalium/util.hpp>
-#include <tt-metalium/allocator.hpp>
 #include <algorithm>
 
 using namespace tt::constants;
