c0980763a8692c0dfaeff1f1fa3e43418c0a1a74
Michael Chiou <156848643+ttmchiou@users.noreply.github.com>
2025-03-13 11:05:49 -0700

    Revert "Add initial support for Llama3-8B & 70B on BH" (#19079)
    Reverts tenstorrent/tt-metal#18697
    deterministic errors in post-commit stemming from here
    https://github.com/tenstorrent/tt-metal/pull/18697
    Log attached
    
    https://github.com/tenstorrent/tt-metal/actions/runs/13839854758/job/38724790632
    
    https://github.com/tenstorrent/tt-metal/actions/runs/13839854758/job/38724790632
    
    https://github.com/tenstorrent/tt-metal/actions/runs/13840286527/job/38726270066
    Profiler-regression issue (edited) 
    
    [#18697 Add initial support for Llama3-8B & 70B on
    BH](https://github.com/tenstorrent/tt-metal/pull/18697)

--- conftest.py
+++ conftest.py
@@ -182,7 +182,7 @@ def set_fabric(fabric_config):
 
 
 @pytest.fixture(scope="function")
-def mesh_device(request, silicon_arch_name, device_params):
+def mesh_device(request, silicon_arch_name, silicon_arch_wormhole_b0, device_params):
     """
     Pytest fixture to set up a device mesh for tests.
 
@@ -193,6 +193,7 @@ def mesh_device(request, silicon_arch_name, device_params):
     Args:
         request: Pytest request object.
         silicon_arch_name: Name of the silicon architecture.
+        silicon_arch_wormhole_b0: Silicon architecture parameter.
         device_params: Additional device configuration parameters.
 
     Yields:
@@ -406,7 +407,6 @@ ALL_ARCHS = set(
     [
         "grayskull",
         "wormhole_b0",
-        "blackhole",
     ]
 )
 
@@ -418,7 +418,7 @@ def pytest_addoption(parser):
         "--tt-arch",
         choices=[*ALL_ARCHS],
         default=ttnn.get_arch_name(),
-        help="Target arch, ex. grayskull, wormhole_b0, blackhole",
+        help="Target arch, ex. grayskull, wormhole_b0",
     )
     parser.addoption(
         "--pipeline-type",
@@ -521,11 +521,6 @@ def pytest_generate_tests(metafunc):
         "silicon_arch_wormhole_b0": set(
             [
                 "wormhole_b0",
-            ]
-        ),
-        "silicon_arch_blackhole": set(
-            [
-                "blackhole",
             ]
         ),
     }
