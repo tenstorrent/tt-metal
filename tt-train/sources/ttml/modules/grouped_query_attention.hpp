7c8620bcb87f8d9121e6e46fa71e66e7f601066f
Jay Kruer <jkruer@tenstorrent.com>
2025-03-13 19:47:20 +0000

    resolve most of denys's comments

--- tt-train/sources/ttml/modules/grouped_query_attention.hpp
+++ tt-train/sources/ttml/modules/grouped_query_attention.hpp
@@ -4,33 +4,32 @@
 
 #pragma once
 
-#include <cstdint>
-
+#include "autograd/module_base.hpp"
 #include "autograd/tensor.hpp"
-#include "dropout_module.hpp"
-#include "linear_module.hpp"
-#include "modules/rotary_embedding.hpp"
 
 namespace ttml::modules {
+
+struct GQAConfig {
+    uint32_t embedding_dim{};
+    uint32_t num_heads{};
+    uint32_t num_groups{};
+    float dropout_prob{};
+    std::shared_ptr<autograd::ModuleBase> embedding{};
+};
 
 class GroupedQueryAttention : public ttml::autograd::ModuleBase {
 private:
     uint32_t m_embedding_dim{};
     uint32_t m_num_heads{};
     uint32_t m_num_groups{};
-    std::shared_ptr<LinearLayer> m_q_linear;
-    std::shared_ptr<LinearLayer> m_kv_linear;
-    std::shared_ptr<LinearLayer> m_out_linear;
-    std::shared_ptr<DropoutLayer> m_dropout;
-    std::optional<std::shared_ptr<RotaryEmbedding>> m_rope;
+    std::shared_ptr<ModuleBase> m_q_linear;
+    std::shared_ptr<ModuleBase> m_kv_linear;
+    std::shared_ptr<ModuleBase> m_out_linear;
+    std::shared_ptr<ModuleBase> m_dropout;
+    std::shared_ptr<ModuleBase> m_embedding;
 
 public:
-    explicit GroupedQueryAttention(
-        uint32_t embedding_dim,
-        uint32_t num_heads,
-        uint32_t num_groups,
-        float dropout_prob,
-        const ops::RotaryEmbeddingParams* rope_params = nullptr);
+    explicit GroupedQueryAttention(const GQAConfig& config);
 
     [[nodiscard]] autograd::TensorPtr operator()(
         const autograd::TensorPtr& x, const autograd::TensorPtr& mask) override;
