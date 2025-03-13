13fae990baaccb40f3b950d965234a84dc60c5ae
Sofija Jovic <148721049+s-jovic@users.noreply.github.com>
2025-03-13 17:47:14 +0100

    #17725: Fix golden functions (#19048)
    ### Ticket
    Needed for Stable Diffusion PCC debug on BH (#17725)
    
    ### What's changed
    - `softmax` was updated to allow None for dim parameter
    - `create_qkv_heads_from_separate_tensors` was updated to match argument
    name in ttnn
    - `sharded_to_interleaved_partial` golden function was implemented since
    we didn't have it before
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/13831330657)

--- ttnn/ttnn/operations/normalization.py
+++ ttnn/ttnn/operations/normalization.py
@@ -11,10 +11,10 @@ from tt_lib.utils import find_closest_largest_divisor
 import math
 
 
-def _golden_function(input_tensor: ttnn.Tensor, dim: int, **_):
+def _golden_function(input_tensor: ttnn.Tensor, dim: Optional[int] = None, **_):
     import torch
 
-    return torch.softmax(input_tensor, dim)
+    return torch.nn.Softmax(dim)(input_tensor)
 
 
 ttnn.attach_golden_function(
