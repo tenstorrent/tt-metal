ca5b499f501adcf334a03c6a6a098ec40de12003
ngrujic <ngrujic@tenstorrent.com>
2025-03-14 09:06:24 +0000

    #17594: Add more cumsum unit tests

--- tests/ttnn/python_api_testing/non_working_unit_tests/wormhole/test_cumsum.py
+++ tests/ttnn/python_api_testing/non_working_unit_tests/wormhole/test_cumsum.py
@@ -22,8 +22,6 @@ def run_moreh_cumsum_tests(
 ):
     x = torch.tensor(in_data[0]).to(torch.int32)
 
-    print(in_data[0])
-
     try:
         x = x.reshape(len(in_data[0]), 1, 1, 1)
         ref_value = aten.cumsum.default(
@@ -39,8 +37,8 @@ def run_moreh_cumsum_tests(
             0,
         )
         tt_result = ttnn.to_torch(tt_result)
-        print(f"tt_result {tt_result.shape} {tt_result}")
-        print(f"ref_value {ref_value.shape} {ref_value}")
+        # print(f"tt_result {tt_result.shape} {tt_result}")
+        # print(f"ref_value {ref_value.shape} {ref_value}")
 
         passed, message = check_with_pcc(ref_value, tt_result)
         assert passed, f"{message, tt_x}"
@@ -67,3 +65,71 @@ test_sweep_args = [
 def test_moreh_cumsum(x, device):
     disable_persistent_kernel_cache()
     run_moreh_cumsum_tests(x, device)
+
+
+def run_moreh_cumsum_tests_2(
+    in_shape,
+    torch_type,
+    dtype,
+    dim,
+    device,
+):
+    x = (torch.rand(in_shape) * 10).to(torch_type)
+
+    try:
+        ref_value = aten.cumsum.default(
+            x,
+            dim,
+        )
+
+        tt_x = ttnn.from_torch(x, dtype=dtype, layout=ttnn.TILE_LAYOUT, device=device)  # int32 uint32 bfloat16
+
+        # tt_x = ttnn.typecast(tt_x, ttnn.bfloat16)
+        tt_result = ttnn.moreh_cumsum(
+            tt_x,
+            dim,
+        )
+        tt_result = ttnn.to_torch(tt_result)
+        # print(f"tt_result {tt_result.shape} {tt_result}")
+        # print(f"ref_value {ref_value.shape} {ref_value}")
+
+        passed, message = check_with_pcc(ref_value, tt_result)
+        assert passed, f"{message, tt_x}"
+
+    except Exception as e:
+        logger.warning(f"Test execution crashed: {e}")
+        print(traceback.format_exc())
+        raise e
+
+    # assert len(tt_result.shape) == len(ref_value.shape)
+    assert tt_result.shape == ref_value.shape
+    assert_with_pcc(ref_value, tt_result, 0.999)
+
+
+test_sweep_args_2 = [
+    (
+        (3, 27, 17, 14),
+        torch.int32,
+        ttnn.uint32,
+        1,
+    ),
+    ((3, 27, 17, 14), torch.int32, ttnn.uint32, 0),
+    ((21, 27, 17, 14), torch.int32, ttnn.uint32, 1),
+    ((3, 27, 33, 34), torch.int32, ttnn.uint32, 1),
+    ((129, 27, 17, 14), torch.float32, ttnn.bfloat16, 1),
+    # (
+    #     (129, 27, 17, 14),
+    #     torch.int32,
+    #     ttnn.uint32,
+    #     1
+    # ),
+]
+
+
+@pytest.mark.parametrize(
+    "shape, torch_type, dtype, dim",
+    (test_sweep_args_2),
+)
+def test_moreh_cumsum_2(shape, torch_type, dtype, dim, device):
+    disable_persistent_kernel_cache()
+    run_moreh_cumsum_tests_2(shape, torch_type, dtype, dim, device)
