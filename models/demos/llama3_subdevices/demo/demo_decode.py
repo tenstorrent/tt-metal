0d721303a957a0f59c7c80d5b9b7554baf78bb49
Djordje Ivanovic <133343976+djordje-tt@users.noreply.github.com>
2025-03-14 14:24:43 +0100

    Update demo_decode.py
    Return max_generated_tokens to original value

--- models/demos/llama3_subdevices/demo/demo_decode.py
+++ models/demos/llama3_subdevices/demo/demo_decode.py
@@ -522,7 +522,7 @@ def run_llama3_demo(
             1,  # repeat_batches
             1024,  # max_seq_len
             32,  # batch_size
-            5,  # max_generated_tokens
+            200,  # max_generated_tokens
             False,  # paged_attention
             {"page_block_size": 32, "page_max_num_blocks": 1024},  # page_params  # TODO This will be serviced by vLLM
             {"top_k": 32, "top_p": 0.08, "seed": 42},  # sampling_params (argmax)
