78134a12649d278c659c289b62e494f6af8aad73
Jay Kruer <jkruer@tenstorrent.com>
2025-03-14 04:57:17 +0000

    fix linker error for common

--- tt-train/sources/ttml/models/common/transformer_common.cpp
+++ tt-train/sources/ttml/models/common/transformer_common.cpp
@@ -8,50 +8,6 @@
 
 namespace ttml::models::common::transformer {
 
-autograd::TensorPtr memory_efficient_runner(
-    auto&& forward_impl, const autograd::TensorPtr& input, const autograd::TensorPtr& mask) {
-    if (autograd::ctx().get_gradient_mode() == autograd::GradMode::DISABLED) {
-        return forward_impl(input, mask);
-    }
-
-    // make a copy of a generator before running forward pass
-    auto generator = autograd::ctx().get_generator();
-
-    // running forward pass
-    autograd::TensorPtr out;
-    {
-        auto scoped = ttml::core::Scoped(
-            []() { autograd::ctx().set_gradient_mode(autograd::GradMode::DISABLED); },
-            []() { autograd::ctx().set_gradient_mode(autograd::GradMode::ENABLED); });
-        out = forward_impl(input, mask);
-    }
-
-    // define grad function and copy generator (in the state before forward pass)
-    autograd::GradFunction grad = [input, mask, out, &forward_impl, generator]() {
-        // detach input from existing graph
-        auto input_detached = autograd::create_tensor(input->get_value());
-        // run forward pass again
-        autograd::TensorPtr output;
-        {
-            // set generator to the state before forward pass during construction
-            // restore generator state after grad function is executed
-            auto scoped = ttml::core::Scoped(
-                [&generator]() { autograd::ctx().set_generator(generator); },
-                [generator = autograd::ctx().get_generator()]() { autograd::ctx().set_generator(generator); });
-            output = forward_impl(input_detached, mask);
-        }
-        // use gradients from new output
-        output->set_grad(out->get_grad());
-        output->backward();
-        // reuse gradients from detached input
-        input->add_grad(input_detached->get_grad());
-    };
-
-    auto links = autograd::get_links(input);
-    out->set_node(autograd::ctx().add_backward_node(std::move(grad), links));
-    return out;
-}
-
 void weights_initialization(autograd::ModuleBase& model) {
     auto params = model.parameters();
     for (auto& [name, tensor_ptr] : params) {
