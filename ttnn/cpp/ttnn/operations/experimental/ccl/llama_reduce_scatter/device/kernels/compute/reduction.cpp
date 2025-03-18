fff4c30645535999bb16a1dbc09b60b946f82cfc
Saad Jameel <sjameel@tenstorrent.com>
2025-03-18 19:34:56 +0000

    add constant folding

--- ttnn/cpp/ttnn/operations/experimental/ccl/llama_reduce_scatter/device/kernels/compute/reduction.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/llama_reduce_scatter/device/kernels/compute/reduction.cpp
@@ -27,20 +27,28 @@
 // }
 namespace NAMESPACE {
 void MAIN {
+    // Define all compile-time arguments at the beginning
     constexpr uint32_t accumulator_cb_id = get_compile_time_arg_val(0);
     constexpr uint32_t output_tensor_cb_id = get_compile_time_arg_val(1);
     constexpr uint32_t num_devices = get_compile_time_arg_val(2);
     constexpr uint32_t tiles_per_core_width_output = get_compile_time_arg_val(3);
+
+    // Initialize binary operations - use the same constants consistently
     binary_op_init_common(accumulator_cb_id, accumulator_cb_id, output_tensor_cb_id);
     add_tiles_init(accumulator_cb_id, accumulator_cb_id, true);
 
     // UNPACK(DPRINT << "waiting on cb" << accumulator_cb_id << " num tiles:" << num_devices << ENDL());
     cb_wait_front(accumulator_cb_id, num_devices);
-    for (uint32_t i = 0; i < num_devices; i++) {
-        // UNPACK(print_full_tile(accumulator_cb_id, i, true));
-    }
+
+    // This loop doesn't do anything now that print statements are commented out
+    // for (uint32_t i = 0; i < num_devices; i++) {
+    //     // UNPACK(print_full_tile(accumulator_cb_id, i, true));
+    // }
+
+    // Reserve output space once before processing
     cb_reserve_back(output_tensor_cb_id, tiles_per_core_width_output);
 
+    // Process tiles in pairs for efficient addition
     tile_regs_acquire();
     for (uint32_t i = 0; i < num_devices; i += 2) {
         add_tiles(accumulator_cb_id, accumulator_cb_id, i, i + 1, 0);
@@ -52,6 +60,7 @@ void MAIN {
     tile_regs_wait();
     pack_tile(0, output_tensor_cb_id);
     tile_regs_release();
+
     // UNPACK(DPRINT << "popping front from cb " << accumulator_cb_id << " tiles: " << num_devices << ENDL());
     cb_pop_front(accumulator_cb_id, num_devices);
     cb_push_back(output_tensor_cb_id, tiles_per_core_width_output);
--- ttnn/cpp/ttnn/operations/experimental/ccl/llama_reduce_scatter/device/kernels/dataflow/reader_llama_reduce_scatter.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/llama_reduce_scatter/device/kernels/dataflow/reader_llama_reduce_scatter.cpp
@@ -38,27 +38,24 @@ void kernel_main() {
     // DPRINT << "Starting kernel_main for reader" << ENDL();
     size_t ct_arg_idx = 0, rt_arg_idx = 0;
 
+    // Define all compile-time arguments at the beginning
     constexpr uint32_t input_tensor_cb_id = get_compile_time_arg_val(0);
     constexpr uint32_t fabric_sender_cb_id = get_compile_time_arg_val(1);
     constexpr uint32_t packet_header_cb_id = get_compile_time_arg_val(2);
     constexpr uint32_t fabric_receiver_cb_id = get_compile_time_arg_val(3);
     constexpr uint32_t accumulator_cb_id = get_compile_time_arg_val(4);
     constexpr uint32_t output_tensor_cb_id = get_compile_time_arg_val(5);
-
     constexpr uint32_t chip_id = get_compile_time_arg_val(6);
-
     constexpr uint32_t tiles_per_core_width = get_compile_time_arg_val(7);
     constexpr uint32_t tiles_per_core_width_output = get_compile_time_arg_val(8);
     constexpr uint32_t num_pages_per_packet = get_compile_time_arg_val(9);
-
     constexpr uint32_t input_shard_cores_per_device = get_compile_time_arg_val(10);
     constexpr uint32_t num_devices = get_compile_time_arg_val(11);
     constexpr uint32_t page_size_bytes = get_compile_time_arg_val(12);
     constexpr uint32_t ncores_output = get_compile_time_arg_val(13);
 
+    // Derived compile-time constants
     constexpr uint32_t input_tensor_cores = input_shard_cores_per_device * num_devices;
-
-    // Calculate the total number of packets needed to send all the tiles
     constexpr uint32_t num_packets_total_per_device =
         (input_shard_cores_per_device * tiles_per_core_width + num_pages_per_packet - 1) / num_pages_per_packet;
 
@@ -68,18 +65,20 @@ void kernel_main() {
     constexpr uint8_t input_core_xy[input_tensor_cores][2] = INPUT_CORE_XY;
     constexpr uint8_t output_core_xy[ncores_output][2] = OUTPUT_CORE_XY;
 
+    // Runtime arguments
     uint32_t receiver_semaphore_address = get_arg_val<uint32_t>(rt_arg_idx++);
     uint32_t local_semaphore_address = get_semaphore(get_arg_val<uint32_t>(rt_arg_idx++));
     bool sender_core = (bool)get_arg_val<uint32_t>(rt_arg_idx++);
     bool worker_core = (bool)get_arg_val<uint32_t>(rt_arg_idx++);
-    // Our output only requires 1 local page
     uint32_t linear_input_page_idx = get_arg_val<uint32_t>(rt_arg_idx++);
-    bool receiver_core = true;
-    uint32_t receiver_for_device_id = 0;
 
+    // Constants for indexing
+    constexpr uint32_t x_index = 0;
+    constexpr uint32_t y_index = 1;
+
+    // Constants for memory operations
     uint32_t bank_base_address = get_write_ptr(input_tensor_cb_id);
-    uint32_t x_index = 0;
-    uint32_t y_index = 1;
+
     if (sender_core) {
         // DPRINT << "input_shard_cores_per_device " << input_shard_cores_per_device << ENDL();
         for (auto target_device_id : device_order) {
@@ -88,9 +87,12 @@ void kernel_main() {
             }
 
             uint32_t base_core = target_device_id * input_shard_cores_per_device;
-            uint32_t curr_tile = 0;  // this is 0 to tiles_per_core_width - 1
             // DPRINT << "base_core " << base_core << " input_shard_cores_per_device " << input_shard_cores_per_device
             //        << ENDL();
+
+            // Precompute the number of bytes to transfer per tile group
+            constexpr uint32_t bytes_per_tile_group = tiles_per_core_width * page_size_bytes;
+
             for (uint32_t curr_core = base_core; curr_core < base_core + input_shard_cores_per_device; ++curr_core) {
                 uint32_t x = input_core_xy[curr_core][x_index];
                 uint32_t y = input_core_xy[curr_core][y_index];
@@ -98,7 +100,7 @@ void kernel_main() {
                 // DPRINT << "reserving " << tiles_per_core_width << " pages" << ENDL();
                 cb_reserve_back(fabric_sender_cb_id, tiles_per_core_width);
                 uint32_t sender_read_addr = get_write_ptr(fabric_sender_cb_id);
-                noc_async_read(shard_noc_addr, sender_read_addr, tiles_per_core_width * page_size_bytes);
+                noc_async_read(shard_noc_addr, sender_read_addr, bytes_per_tile_group);
                 noc_async_read_barrier();
                 // print_tiles(fabric_sender_cb_id, 0, tiles_per_core_width, true);
                 cb_push_back(fabric_sender_cb_id, tiles_per_core_width);
@@ -106,6 +108,7 @@ void kernel_main() {
         }
     } else if (worker_core) {
         // DPRINT << "linear_input_page_idx " << linear_input_page_idx << ENDL();
+        // Calculate core index and tile offset once
         uint32_t linear_input_core_idx = linear_input_page_idx / tiles_per_core_width;
         // DPRINT << "linear_input_core_idx " << linear_input_core_idx << ENDL();
         uint32_t linear_input_tile_offset = linear_input_page_idx % tiles_per_core_width;
@@ -116,12 +119,15 @@ void kernel_main() {
 
         uint32_t tile_offset = linear_input_tile_offset * page_size_bytes;
         uint64_t tile_addr = get_noc_addr(core_x, core_y, get_read_ptr(input_tensor_cb_id) + tile_offset);
-        noc_async_read(tile_addr, get_write_ptr(accumulator_cb_id) + chip_id * page_size_bytes, page_size_bytes);
+
+        // Precompute the destination address
+        uint32_t dest_addr = get_write_ptr(accumulator_cb_id) + chip_id * page_size_bytes;
+
+        noc_async_read(tile_addr, dest_addr, page_size_bytes);
         noc_async_read_barrier();
         noc_semaphore_wait((uint32_t*)local_semaphore_address, num_devices - 1);
-        for (uint32_t target_device_id = 0; target_device_id < num_devices; ++target_device_id) {
-            // print_full_tile(accumulator_cb_id, target_device_id * tiles_per_core_width_output, true);
-        }
+
+        // Push all accumulated data at once
         cb_push_back(accumulator_cb_id, tiles_per_core_width_output * num_devices);
         *(uint32_t*)local_semaphore_address = 0;
     } else {
