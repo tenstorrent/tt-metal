692f7ad755b73e2a6d1b650758055fde7de06ea2
Oleg Milyutin <omilyutin@tenstorrent.com>
2025-03-18 17:20:17 +0000

    Revert unintended change

--- ttnn/cpp/ttnn/tensor/storage.cpp
+++ ttnn/cpp/ttnn/tensor/storage.cpp
@@ -65,9 +65,6 @@ void DeviceStorage::update_specs(const TensorSpec& new_spec) {
 }
 
 bool DeviceStorage::is_uniform_storage() const {
-    if (mesh_buffer.get() != nullptr) {
-        return true;
-    }
     return specs.size() == mesh_buffer->device()->num_devices() &&
            std::all_of(specs.begin(), specs.end(), [this](const auto& spec) { return spec.second == specs[0].second; });
 }
