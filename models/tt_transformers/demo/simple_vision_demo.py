a46da98f7c30539d1a8f4a0ba0feda1c4b908dd2
blozano-tt <blozano@tenstorrent.com>
2025-03-18 21:37:41 +0000

    Decrease performance threshold

--- models/tt_transformers/demo/simple_vision_demo.py
+++ models/tt_transformers/demo/simple_vision_demo.py
@@ -360,7 +360,7 @@ def test_multimodal_demo_text(
 
     tt_device_name = model_args[0].device_name
     target_prefill_tok_s = {
-        "N300_Llama3.2-11B": 9,
+        "N300_Llama3.2-11B": 8,
         "T3K_Llama3.2-11B": 4,
     }[f"{tt_device_name}_{model_args[0].base_model_name}"]
 
