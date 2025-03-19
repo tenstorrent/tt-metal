6bcfabba6ebf2ecb2c72864785f42d6016b21eea
Slavko Krstic <skrstic@tenstorrent.com>
2025-03-19 18:31:03 +0100

    #19077: Add resnet50 batch=16 to blackhole post-commit (#19196)
    ### Ticket
    #19077
    
    Added Resnet50 with a batch size of 16 to the blackhole post-commit to
    track its regression.
    
    Added new file
    (`tests/ttnn/integration_tests/resnet/test_ttnn_functional_resnet50_batch_16.py`)
    for running test to be aligned with #7586
    
    ### Checklist
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/13920852155)
    - the added test is passing, but there are some failures in pipeline
    that seem to be caused by the bad state of the blackhole post-commit

---
+++ tests/ttnn/integration_tests/resnet/test_ttnn_functional_resnet50_batch_16.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import ttnn
+from tests.ttnn.integration_tests.resnet.test_ttnn_functional_resnet50 import run_resnet_50
+
+
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 24576}], indirect=True)
+def test_resnet_batch_16(
+    device,
+    use_program_cache,
+    model_location_generator,
+):
+    run_resnet_50(
+        device,
+        use_program_cache,
+        16,
+        ttnn.bfloat8_b,
+        ttnn.bfloat8_b,
+        ttnn.MathFidelity.LoFi,
+        False,
+        model_location_generator,
+    )
