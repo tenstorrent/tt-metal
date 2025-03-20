19b86d1d6c55e0b02ef9953091df740bc6734315
Radoica Draskic <rdraskic@tenstorrent.com>
2025-03-20 15:43:56 +0100

    Remove unused imports from `setup.py` and `conftest.py` (#19355)
    ### Ticket
    Link to Github Issue
    
    ### Problem description
    Provide context for the problem.
    
    ### What's changed
    Describe the approach used to solve the problem.
    Summarize the changes made and its impact.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [ ] New/Existing tests provide coverage for changes

--- conftest.py
+++ conftest.py
@@ -8,11 +8,9 @@ import random
 import os
 import numpy as np
 from functools import partial
-from itertools import chain
 from operator import contains, eq, getitem
 from pathlib import Path
 import json
-import copy
 import multiprocess
 import signal
 import time
