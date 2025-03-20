fcb59b48772d7bf805a6baf28807c274823bae94
Ilija Anastasijevic <ianastasijevic@tenstorrent.com>
2025-03-20 10:46:05 +0000

    Refactor padding so that both t2cq and bare tests function at the same time

--- models/demos/segformer/tests/perf/e2e_perf/test_perf_segformer_trace_2cq.py
+++ models/demos/segformer/tests/perf/e2e_perf/test_perf_segformer_trace_2cq.py
@@ -20,6 +20,9 @@ def buffer_address(tensor):
     return addr
 
 
+# ttnn.CONFIG.enable_logging = True
+
+
 @run_for_wormhole_b0()
 @pytest.mark.models_performance_bare_metal
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 24576, "num_command_queues": 2}], indirect=True)
@@ -32,6 +35,8 @@ def buffer_address(tensor):
 def test_perf_segformer_trace_2cq(
     device, batch_size, act_dtype, weight_dtype, expected_compile_time, expected_inference_time
 ):
+    # device.enable_program_cache()
+
     test_infra = create_test_infra(
         device,
         batch_size,
@@ -40,31 +45,33 @@ def test_perf_segformer_trace_2cq(
         model_location_generator=None,
     )
 
-    tt_inputs_host, padded_shape, l1_config_input = test_infra.setup_l1_sharded_input(device)
+    tt_inputs_host, l1_config_input, padded_shape = test_infra.setup_l1_sharded_input(device)
     dram_config_input = ttnn.DRAM_MEMORY_CONFIG
     dram_config_output = ttnn.DRAM_MEMORY_CONFIG
     tt_inputs_dram = ttnn.allocate_tensor_on_device(
         tt_inputs_host.shape, tt_inputs_host.dtype, tt_inputs_host.layout, device, dram_config_input
     )
+    tt_inputs_dram_padded = ttnn.allocate_tensor_on_device(
+        padded_shape, tt_inputs_host.dtype, tt_inputs_host.layout, device, dram_config_input
+    )
 
     ## MODEL COMPILATION
 
     # initialize events on both CQs
-    # first_op_event = ttnn.record_event(device, 0)
-    # read_event = ttnn.record_event(device, 1)
+    first_op_event = ttnn.record_event(device, 0)
+    read_event = ttnn.record_event(device, 1)
 
     # write host input to DRAM
     # we do padding on the device side to avoid a variety of issues
     jit_start = time.time()
-    first_op_event = ttnn.record_event(device, 0)
     ttnn.wait_for_event(1, first_op_event)
     ttnn.copy_host_to_device_tensor(tt_inputs_host, tt_inputs_dram, cq_id=1)
-    tt_inputs_dram = ttnn.pad(tt_inputs_dram, padded_shape, [0, 0, 0, 0], 0)
+    tt_inputs_dram_padded = ttnn.pad(tt_inputs_dram, padded_shape, [0, 0, 0, 0], 0)
     write_event = ttnn.record_event(device, 1)
 
     # reshard input to L1
     ttnn.wait_for_event(0, write_event)
-    test_infra.input_tensor = ttnn.to_memory_config(tt_inputs_dram, l1_config_input)
+    test_infra.input_tensor = ttnn.to_memory_config(tt_inputs_dram_padded, l1_config_input)
     first_op_event = ttnn.record_event(device, 0)
 
     # run model
@@ -75,21 +82,21 @@ def test_perf_segformer_trace_2cq(
     last_op_event = ttnn.record_event(device, 0)
     jit_end = time.time()
 
-    # deallocate and reallocate tt_inputs dram due to padding issue
-    # this defeats the purpose of persistent DRAM input but its the only way for now
-    tt_inputs_dram.deallocate(force=True)
-    tt_inputs_dram = ttnn.allocate_tensor_on_device(
-        tt_inputs_host.shape, tt_inputs_host.dtype, tt_inputs_host.layout, device, dram_config_input
-    )
+    # # deallocate and reallocate tt_inputs dram due to padding issue
+    # # this defeats the purpose of persistent DRAM input but its the only way for now
+    # tt_inputs_dram.deallocate(force=True)
+    # tt_inputs_dram = ttnn.allocate_tensor_on_device(
+    #     tt_inputs_host.shape, tt_inputs_host.dtype, tt_inputs_host.layout, device, dram_config_input
+    # )
 
     ## TRACE CAPTURE
 
     ttnn.wait_for_event(1, last_op_event)
     ttnn.copy_host_to_device_tensor(tt_inputs_host, tt_inputs_dram, cq_id=1)
-    tt_inputs_dram = ttnn.pad(tt_inputs_dram, padded_shape, [0, 0, 0, 0], 0)
+    tt_inputs_dram_padded = ttnn.pad(tt_inputs_dram, padded_shape, [0, 0, 0, 0], 0)
     write_event = ttnn.record_event(device, 1)
     ttnn.wait_for_event(0, write_event)
-    test_infra.input_tensor = ttnn.to_memory_config(tt_inputs_dram, l1_config_input)
+    test_infra.input_tensor = ttnn.to_memory_config(tt_inputs_dram_padded, l1_config_input)
     first_op_event = ttnn.record_event(device, 0)
 
     input_trace_addr = buffer_address(test_infra.input_tensor)
@@ -99,10 +106,11 @@ def test_perf_segformer_trace_2cq(
     test_infra.output_tensor.logits.deallocate(force=True)
 
     tid = ttnn.begin_trace_capture(device, cq_id=0)
+    pdb.set_trace()
     test_infra.run()
 
     test_infra.input_tensor = ttnn.allocate_tensor_on_device(input_spec, device)
-    assert input_trace_addr == test_infra.input_tensor.buffer_address()
+    assert input_trace_addr[0] == test_infra.input_tensor.buffer_address()
 
     ttnn.end_trace_capture(device, cq_id=0)
 
@@ -121,12 +129,12 @@ def test_perf_segformer_trace_2cq(
     # luckily we can reuse it here for all loop iterations
     ttnn.wait_for_event(1, first_op_event)
     ttnn.copy_host_to_device_tensor(tt_inputs_host, tt_inputs_dram, cq_id=1)
-    tt_inputs_dram = ttnn.pad(tt_inputs_dram, padded_shape, [0, 0, 0, 0], 0)
+    tt_inputs_dram_padded = ttnn.pad(tt_inputs_dram, padded_shape, [0, 0, 0, 0], 0)
     write_event = ttnn.record_event(device, 1)
 
     for iter in range(0, 2):
         ttnn.wait_for_event(0, write_event)
-        test_infra.input_tensor = ttnn.to_memory_config(tt_inputs_dram, l1_config_input)
+        test_infra.input_tensor = ttnn.to_memory_config(tt_inputs_dram_padded, l1_config_input)
         first_op_event = ttnn.record_event(device, 0)
 
         ttnn.execute_trace(device, tid, cq_id=0, blocking=False)
@@ -135,9 +143,9 @@ def test_perf_segformer_trace_2cq(
         tt_outputs_dram = ttnn.to_memory_config(test_infra.output_tensor.logits, dram_config_output)
         last_op_event = ttnn.record_event(device, 0)
 
-        ttnn.wait_for_event(1, first_op_event)
-        ttnn.copy_host_to_device_tensor(tt_inputs_host, tt_inputs_dram, cq_id=1)
-        write_event = ttnn.record_event(device, 1)
+        # ttnn.wait_for_event(1, first_op_event)
+        # ttnn.copy_host_to_device_tensor(tt_inputs_host, tt_inputs_dram, cq_id=1)
+        # write_event = ttnn.record_event(device, 1)
 
         ttnn.wait_for_event(0, last_op_event)
         tt_outputs_host.append(tt_outputs_dram.cpu(blocking=False, cq_id=1))
@@ -146,7 +154,7 @@ def test_perf_segformer_trace_2cq(
     ## BENCHMARK TRACE
     start = time.time()
     ttnn.wait_for_event(0, write_event)
-    test_infra.input_tensor = ttnn.to_memory_config(tt_inputs_dram, l1_config_input)
+    test_infra.input_tensor = ttnn.to_memory_config(tt_inputs_dram_padded, l1_config_input)
     first_op_event = ttnn.record_event(device, 0)
 
     ttnn.execute_trace(device, tid, cq_id=0, blocking=False)
@@ -155,9 +163,9 @@ def test_perf_segformer_trace_2cq(
     tt_outputs_dram = ttnn.to_memory_config(test_infra.output_tensor.logits, dram_config_output)
     last_op_event = ttnn.record_event(device, 0)
 
-    ttnn.wait_for_event(1, first_op_event)
-    ttnn.copy_host_to_device_tensor(tt_inputs_host, tt_inputs_dram, cq_id=1)
-    write_event = ttnn.record_event(device, 1)
+    # ttnn.wait_for_event(1, first_op_event)
+    # ttnn.copy_host_to_device_tensor(tt_inputs_host, tt_inputs_dram, cq_id=1)
+    # write_event = ttnn.record_event(device, 1)
 
     ttnn.wait_for_event(0, last_op_event)
     tt_outputs_host.append(tt_outputs_dram.cpu(blocking=False, cq_id=1))
@@ -169,7 +177,7 @@ def test_perf_segformer_trace_2cq(
     prep_perf_report(
         model_name="segformer_e2e",
         batch_size=batch_size,
-        inference_and_compile_time=end_jit - start_jit,
+        inference_and_compile_time=jit_end - jit_start,
         inference_time=end - start,
         expected_compile_time=expected_compile_time,
         expected_inference_time=expected_inference_time,
