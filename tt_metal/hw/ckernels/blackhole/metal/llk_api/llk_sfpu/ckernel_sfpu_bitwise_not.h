5340389d97b85d240d9a2b2d7cea8c96ab111aa2
Aswinmcw <azayasankaran@tenstorrent.com>
2025-03-20 03:56:07 +0000

    Bitwise ops fix in BH

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_bitwise_not.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_bitwise_not.h
@@ -19,19 +19,8 @@ inline void calculate_bitwise_not() {
 #pragma GCC unroll 0
     for (int d = 0; d < ITERATIONS; d++) {
         vInt input = dst_reg[0];
-        vInt res;
 
-        v_if(input < 0) {
-            vInt unsigned_mag = input & 0x7FFFFFFF;
-            res = unsigned_mag - 1;
-        }
-        v_else {
-            res = setsgn(input, -1);
-            res = res + 1;
-        }
-        v_endif;
-
-        dst_reg[0] = res;
+        dst_reg[0] = ~input;
         dst_reg++;
     }
 }
