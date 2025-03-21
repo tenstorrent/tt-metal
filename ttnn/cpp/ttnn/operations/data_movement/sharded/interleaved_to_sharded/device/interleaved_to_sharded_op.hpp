da83d770d0ed92ea4929082b21841e1927999e06
Ilija Anastasijevic <ianastasijevic@tenstorrent.com>
2025-03-21 17:50:55 +0000

    Fix hash collision for InterleavedToSharded when output dtype is different

--- ttnn/cpp/ttnn/operations/data_movement/sharded/interleaved_to_sharded/device/interleaved_to_sharded_op.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/sharded/interleaved_to_sharded/device/interleaved_to_sharded_op.hpp
@@ -20,7 +20,9 @@ struct InterleavedToShardedDeviceOperation {
     tt::tt_metal::operation::ProgramWithCallbacks create_program(
         const std::vector<Tensor>& input_tensors, std::vector<Tensor>& output_tensors) const;
 
-    static constexpr auto attribute_names = std::make_tuple("output_mem_config");
-    const auto attribute_values() const { return std::make_tuple(std::cref(this->output_mem_config)); }
+    static constexpr auto attribute_names = std::make_tuple("output_mem_config", "output_dtype");
+    const auto attribute_values() const {
+        return std::make_tuple(std::cref(this->output_mem_config), std::cref(this->output_dtype));
+    }
 };
 }  // namespace ttnn::operations::data_movement
