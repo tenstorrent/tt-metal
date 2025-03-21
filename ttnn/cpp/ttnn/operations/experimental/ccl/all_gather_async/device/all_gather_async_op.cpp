3099c91b9e37d4556907fc7ee4fe3d1ba6674c47
daminakaTT <daminaka@tenstorrent.com>
2025-03-21 20:27:33 +0000

    fix if condition

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/device/all_gather_async_op.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/device/all_gather_async_op.cpp
@@ -140,13 +140,15 @@ AllGatherAsyncVersion AllGatherAsync::select_version(const Tensor& input_tensor)
     log_trace(tt::LogOp, "[select_version] output_shard_num_cores: {}", output_shard_num_cores);
 
     // Check for minimal interleaved case
-    if (num_links == 1 &&  // 1 for T3K only
-        ((((input_tensor_shape[0] * input_tensor_shape[1] * input_tensor_shape[2]) / 32) % ring_size == 0 && dim < 3) ||
-         ((input_tensor_shape[0] == 1 && input_tensor_shape[1] == 1 && (input_tensor_shape[3] / 32) % ring_size == 0 &&
-           dim == 3))) &&
+    if (num_links == 1 &&
+        ((dim == 3 && (input_tensor.dtype() == DataType::BFLOAT16 ||
+                       (input_tensor.dtype() == DataType::BFLOAT8_B &&
+                        ((input_tensor_shape[0] == 1 && input_tensor_shape[1] == 1 && input_tensor_shape[2] == 32 &&
+                          input_tensor_shape[3] == 64128))))  // llama 8B N300
+          ) ||
+         (dim < 3 && ((input_tensor_shape[0] * input_tensor_shape[1] * input_tensor_shape[2]) % 32 == 0))) &&
         input_tensor_buffer_layout == tt::tt_metal::TensorMemoryLayout::INTERLEAVED &&
         input_tensor_page_layout == tt::tt_metal::Layout::TILE && this->enable_persistent_fabric_mode) {
-        fprintf(stderr, "DEBUG: AllGatherAsyncVersion::MINIMAL_INTERLEAVED_32\n");
         return AllGatherAsyncVersion::MINIMAL_INTERLEAVED_32;
     }
 
@@ -202,7 +204,6 @@ AllGatherAsyncVersion AllGatherAsync::select_version(const Tensor& input_tensor)
         }
     }
     log_trace(tt::LogOp, "Using generic implementation");
-    fprintf(stderr, "DEBUG: AllGatherAsyncVersion::GENERIC\n");
     return AllGatherAsyncVersion::GENERIC;
 }
 
