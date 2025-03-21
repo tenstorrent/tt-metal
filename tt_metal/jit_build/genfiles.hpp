fd36dd44d9a6263a81df161747e2247c55031a5d
Samarth Agarwal <sagarwal@tenstorrent.com>
2025-03-21 20:11:03 +0000

    Cleanup

--- tt_metal/jit_build/genfiles.hpp
+++ tt_metal/jit_build/genfiles.hpp
@@ -23,6 +23,4 @@ void jit_build_genfiles_triscs_src(
 
 void jit_build_genfiles_descriptors(const JitBuildEnv& env, JitBuildOptions& options);
 
-// void jit_build_genfiles_binaries_built_success_marker(const JitBuildEnv& env, const JitBuildSettings& settings);
-
 }  // namespace tt::tt_metal
