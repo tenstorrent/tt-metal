eaeae29fcbfeb6c52c6d3bb7d0094c9ec6a90d5a
mradosavljevicTT <mradosavljevic@tenstorrent.com>
2025-03-21 14:09:03 +0000

    Add data parallelism for resnet50 demo (n300, t3k, tg)

--- models/demos/tg/resnet50/demo/demo.py
+++ models/demos/tg/resnet50/demo/demo.py
@@ -2,27 +2,29 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-
 import pytest
-from models.demos.ttnn_resnet.demo.demo import run_resnet_imagenet_inference, run_resnet_inference
-from models.utility_functions import run_for_wormhole_b0
 import ttnn
+from models.utility_functions import (
+    run_for_wormhole_b0,
+)
 from models.demos.wormhole.resnet50.tests.test_resnet50_performant_imagenet import (
     test_run_resnet50_trace_2cqs_inference,
 )
 
+from models.demos.ttnn_resnet.demo.demo import run_resnet_imagenet_inference, run_resnet_inference
 
+
+@run_for_wormhole_b0()
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 24576}], indirect=True)
 @pytest.mark.parametrize(
     "batch_size, iterations",
     ((16, 100),),
 )
-def test_demo_imagenet(
-    batch_size, use_program_cache, iterations, imagenet_label_dict, model_location_generator, mesh_device
-):
+def test_demo_imagenet(batch_size, iterations, imagenet_label_dict, model_location_generator, mesh_device):
     run_resnet_imagenet_inference(batch_size, iterations, imagenet_label_dict, model_location_generator, mesh_device)
 
 
+@run_for_wormhole_b0()
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 24576}], indirect=True)
 @pytest.mark.parametrize(
     "batch_size, input_loc",
