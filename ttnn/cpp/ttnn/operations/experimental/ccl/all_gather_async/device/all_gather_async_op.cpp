ff14c932610d2c44a1b110578933eec84e2fcbe0
kpaigwar <kpaigwar@tenstorrent.com>
2025-03-24 18:34:01 +0000

    remove output padding for post mul+silu all gather

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/device/all_gather_async_op.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/device/all_gather_async_op.cpp
@@ -165,8 +165,16 @@ static void validate_output_tensor_allocation(const std::vector<Tensor>& output_
 
 std::vector<ttnn::TensorSpec> AllGatherAsync::compute_output_specs(const std::vector<Tensor>& input_tensors) const {
     const auto& input_tensor = input_tensors[0];
+    AllGatherAsyncVersion version = select_version(input_tensor);
     auto shape = input_tensor.get_padded_shape();  // TODO: Replace with get_logical_shape()
-    shape[this->dim] *= this->ring_size;
+
+    // Adjust shape for llama post binary mult+silu case to remove padding in the output tensor
+    if (version == AllGatherAsyncVersion::LLAMA_MINIMAL_SHARDED && shape[this->dim] == 960) {
+        shape[this->dim] = 896 * this->ring_size;
+    } else {
+        shape[this->dim] *= this->ring_size;
+    }
+
     return {TensorSpec(
         shape,
         TensorLayout(input_tensor.get_dtype(), input_tensor.get_tensor_spec().page_config(), output_mem_config))};
@@ -264,7 +272,6 @@ AllGatherAsyncVersion AllGatherAsync::select_version(const Tensor& input_tensor)
 tt::tt_metal::operation::ProgramWithCallbacks AllGatherAsync::create_program(
     const std::vector<Tensor>& input_tensors, std::vector<Tensor>& output_tensors) const {
     tt::log_debug(tt::LogOp, "DEBUG: create_program is called");
-
     AllGatherAsyncVersion version = select_version(input_tensors[0]);
 
     log_trace(tt::LogOp, "version: {}", static_cast<uint32_t>(version));
