3198ec614166385f59815a3f1c59b1565eb83046
Armin Ale <arminale@tenstorrent.com>
2025-03-24 10:29:34 -0400

    #19198: Return the TensorSpec for the output tensor from query_op_constraints to enable the compiler optimizer (#19454)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/19198
    
    ### Problem description
    We need to reduce the search space for the forge optimizer. One way to
    do that is by taking advantage of the fact that for many ops, there is a
    1-1 mapping between input layout and output layout. By inspecting the
    output tensor for a given input layout, we can avoid searching for legal
    output layouts. For more details, see:
    https://github.com/tenstorrent/tt-mlir/issues/2450
    
    ### What's changed
    - `query_op_constraints` now includes the `TensorSpec` of the output
    tensor in its result struct
    - In tt-mlir, we will have conversion functions to create an MLIR type
    from this
    - Most ops return just a `Tensor`, but some (like conv2d) return more
    complex objects. Created overloaded wrappers around the op execution to
    get the output tensor based on the return types of currently supported
    ops in tt-mlir.
    - Attempting to get the constraints for an op with an unsupported return
    type will cause an overload-resolution compile error
    - Added a unit test to exercise the `conv2d` wrapper
    - Modified existing unit tests to verify the `TensorSpec`
    - Closes #19198 
    - **This is the fixed version of #19352. That PR had to be reverted due
    to build issues**
    - In C++20, when using if-constexpr in a templated function, the
    discarded branch cannot not be ill-formed for all template
    specializations. i.e. the following structure used in the old PR is
    **ill-formed**
    ```
    template<typename T>
    void f()
    {
        if constexpr (std::is_arithmetic_v<T>)
            // ...
        else {
            static_assert(false, "Must be arithmetic"); // ill-formed before CWG2518 (C++23)
        }
    }
    ```
    
    For more information, see
    https://en.cppreference.com/w/cpp/language/if#Constexpr_If
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [X] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/graph/graph_query_op_constraints.hpp
+++ ttnn/cpp/ttnn/graph/graph_query_op_constraints.hpp
@@ -13,6 +13,23 @@
 
 namespace ttnn::graph {
 
+namespace detail {
+
+// These overloaded extract_output_tensor functions abstract the return type of an arbitrary op from the rest of the
+// constraints query function. An overload resolution failure means the return type for the op in that query is not yet
+// supported and a new overload should be added
+
+// most ops just return a tensor
+inline Tensor extract_output_tensor(const Tensor& result) { return result; }
+
+// conv2d output
+template <typename... Args>
+Tensor extract_output_tensor(const std::tuple<Tensor, Args...>& result) {
+    return std::get<0>(result);
+}
+
+}  // namespace detail
+
 struct ResourceUsage {
     size_t cb_peak_size_per_core = 0;
     size_t l1_buffers_peak_per_core = 0;
@@ -22,6 +39,7 @@ struct ResourceUsage {
 struct ConstraintQueryResponse {
     ExecutionStatus status = ExecutionStatus::Error;
     ResourceUsage resource_usage;
+    std::optional<TensorSpec> output_tensor_spec;
     std::optional<std::string> error_message;
 };
 
@@ -45,6 +63,7 @@ auto query_op_constraints(Op op, IDevice* device, Args&&... args) {
     uint32_t num_of_active_graph_captures = 0;
     try {
         nlohmann::json op_trace;
+        std::optional<TensorSpec> output_spec = std::nullopt;
         // outer graph capture is to avoid dispatching/allocating dummy input tensors
         {
             auto capture_outer = ScopedGraphCapture(GraphProcessor::RunMode::NO_DISPATCH);
@@ -62,7 +81,8 @@ auto query_op_constraints(Op op, IDevice* device, Args&&... args) {
             // inner graph capture is to capture the actual op graph trace
             {
                 auto capture_inner = ScopedGraphCapture(GraphProcessor::RunMode::NO_DISPATCH);
-                std::apply(op, transformed_args);
+                Tensor output = detail::extract_output_tensor(std::apply(op, transformed_args));
+                output_spec = output.get_tensor_spec();
                 op_trace = capture_inner.end_graph_capture();
             }  // end of inner graph capture
 
@@ -77,11 +97,14 @@ auto query_op_constraints(Op op, IDevice* device, Args&&... args) {
             extract_l1_output_buffer_allocation_size_per_core(op_trace, interleaved_storage_cores);
 
         return ConstraintQueryResponse{
-            ExecutionStatus::Success, {cb_peak_size_per_core, l1_buffers_peak_per_core, l1_output_buffer_per_core}};
+            ExecutionStatus::Success,
+            {cb_peak_size_per_core, l1_buffers_peak_per_core, l1_output_buffer_per_core},
+            output_spec};
 
     } catch (const std::exception& e) {
         tt::log_debug(tt::LogOp, "op_constraints - error: {}", e.what());
-        return ConstraintQueryResponse{ExecutionStatus::Error, {0, 0, 0}, e.what()};
+        return ConstraintQueryResponse{
+            ExecutionStatus::Error, {0, 0, 0}, /* output_tensor_spec= */ std::nullopt, e.what()};
     }
 }
 
