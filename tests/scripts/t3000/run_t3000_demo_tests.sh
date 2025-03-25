6b6b18c092a03a0e481d9d7772d5010ac28ee6b7
Gongyu Wang <gwang@tenstorrent.com>
2025-03-25 18:31:00 +0000

    Restore `ro` on docker mounts and set lower timeouts now that cache is generated and stored in the CI machines.

--- tests/scripts/t3000/run_t3000_demo_tests.sh
+++ tests/scripts/t3000/run_t3000_demo_tests.sh
@@ -111,14 +111,14 @@ run_t3000_llama3_90b_vision_tests() {
   fail=0
   start_time=$(date +%s)
 
-  echo "LOG_METAL: Running run_t3000_llama3_vision_tests"
+  echo "LOG_METAL: Running run_t3000_llama3_90b_vision_tests"
 
   wh_arch_yaml=wormhole_b0_80_arch_eth_dispatch.yaml
   # Llama3.2-90B
   llama11b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.2-90B-Vision-Instruct
   mesh_device=T3K
 
-  MESH_DEVICE=$mesh_device LLAMA_DIR=$llama11b WH_ARCH_YAML=$wh_arch_yaml pytest -n auto models/tt_transformers/demo/simple_vision_demo.py -k "batch1-notrace" --timeout 6000; fail+=$? # todo: change timeout to a tighter value
+  MESH_DEVICE=$mesh_device LLAMA_DIR=$llama11b WH_ARCH_YAML=$wh_arch_yaml pytest -n auto models/tt_transformers/demo/simple_vision_demo.py -k "batch1-notrace" --timeout 900; fail+=$?
   echo "LOG_METAL: Llama3.2-90B vision tests for $mesh_device completed"
 
   # Record the end time
