4f0459d0b01ec17044ca4e686255d5d9b3be2af4
Keerthana <152590865+kkeerthana0573@users.noreply.github.com>
2025-03-26 00:19:22 +0530

    #17700: Yolov9c model ttnn bringup (#18937)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/17700)
    
    ### Problem description
    - Add optimized ttnn support for the Yolov9c model.
    
    ### What's changed
    - Added Reference pipeline for Yolov9c.
    - Added ttnn support for the model.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [ ] New/Existing tests provide coverage for changes
    
    Co-authored-by: Mohamed Bahnas <116673264+mbahnasTT@users.noreply.github.com>

--- models/experimental/yolo_common/yolo_utils.py
+++ models/experimental/yolo_common/yolo_utils.py
@@ -40,3 +40,42 @@ def get_core_grid_from_num_cores(num_cores: int, grid_rows: int = 8, grid_cols: 
             )
         )
     return ttnn.CoreRangeSet({*ranges})
+
+def sharded_concat(input_tensors, num_cores=56, dim=3):
+    shard_grid = get_core_grid_from_num_cores(num_cores=num_cores)
+    in_shard_width = input_tensors[0].shape[-1]
+    shard_height = (input_tensors[0].shape[2] + num_cores - 1) // num_cores
+    input_sharded_memory_config = ttnn.create_sharded_memory_config_(
+        (shard_height, in_shard_width),
+        core_grid=shard_grid,
+        strategy=ttnn.ShardStrategy.HEIGHT,
+        orientation=ttnn.ShardOrientation.ROW_MAJOR,
+        use_height_and_width_as_shard_shape=True,
+    )
+    out_shard_width = 0
+    for i in range(len(input_tensors)):
+        out_shard_width += input_tensors[i].shape[-1]
+        input_tensors[i] = ttnn.to_memory_config(input_tensors[i], input_sharded_memory_config)
+
+    output_sharded_memory_config = ttnn.create_sharded_memory_config_(
+        (shard_height, out_shard_width),
+        core_grid=shard_grid,
+        strategy=ttnn.ShardStrategy.HEIGHT,
+        orientation=ttnn.ShardOrientation.ROW_MAJOR,
+        use_height_and_width_as_shard_shape=True,
+    )
+    output = ttnn.concat(input_tensors, dim, memory_config=output_sharded_memory_config)
+    return output
+
+
+def concat(dim=-1, use_sharded_concat=True, *tensors):
+    if use_sharded_concat:
+        processed_tensors = [
+            ttnn.to_dtype(ttnn.to_layout(tensor, ttnn.ROW_MAJOR_LAYOUT), ttnn.bfloat16) for tensor in tensors
+        ]
+        return sharded_concat(processed_tensors)
+    else:
+        processed_tensors = [
+            ttnn.sharded_to_interleaved(tensor) if tensor.is_sharded() else tensor for tensor in tensors
+        ]
+        return ttnn.concat(processed_tensors, dim=dim, memory_config=ttnn.L1_MEMORY_CONFIG)
