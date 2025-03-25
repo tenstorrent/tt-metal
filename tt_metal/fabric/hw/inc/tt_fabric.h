de8de593658da57952738da36e0a77191f41ace8
Nigel Huang <nhuang@tenstorrent.com>
2025-03-25 22:46:15 +0000

    Add an overload to specify a custom L1 for out of band headers

--- tt_metal/fabric/hw/inc/tt_fabric.h
+++ tt_metal/fabric/hw/inc/tt_fabric.h
@@ -49,6 +49,32 @@ inline uint64_t get_timestamp() {
     return (((uint64_t)timestamp_high) << 32) | timestamp_low;
 }
 
+void tt_fabric_add_header_checksum(volatile tt_l1_ptr packet_header_t* p_header) {
+    uint16_t* ptr = (uint16_t*)p_header;
+    uint32_t sum = 0;
+    for (uint32_t i = 2; i < sizeof(packet_header_t) / 2; i++) {
+        sum += ptr[i];
+    }
+    sum = ~sum;
+    sum += sum;
+    p_header->packet_parameters.misc_parameters.words[0] = sum;
+}
+
+bool tt_fabric_is_header_valid(packet_header_t* p_header) {
+#ifdef TT_FABRIC_DEBUG
+    uint16_t* ptr = (uint16_t*)p_header;
+    uint32_t sum = 0;
+    for (uint32_t i = 2; i < sizeof(packet_header_t) / 2; i++) {
+        sum += ptr[i];
+    }
+    sum = ~sum;
+    sum += sum;
+    return (p_header->packet_parameters.misc_parameters.words[0] == sum);
+#else
+    return true;
+#endif
+}
+
 typedef struct fvc_outbound_push_state {
     uint32_t slots_to_forward;
     uint32_t local_rdptr;
