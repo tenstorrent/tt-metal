fabfea70e28d920bb7e67bbead780aa97fa204d3
blozano-tt <blozano@tenstorrent.com>
2025-03-25 19:52:26 +0000

    Add host_api.hpp to signal deprecation

---
+++ tt_metal/api/tt-metalium/host_api.hpp
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#warning "host_api.hpp is deprecated. Please include tt_metal.hpp directly instead."
+
+#include "tt_metal.hpp"
