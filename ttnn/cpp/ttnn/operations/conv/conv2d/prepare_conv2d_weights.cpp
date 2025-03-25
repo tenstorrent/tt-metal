912eeda511e33d914ebe53f8b4e8a1fb7d53ea7c
Pavle Josipović <pavlejosipovic@users.noreply.github.com>
2025-03-25 09:28:34 +0100

    Conv2d fix output image dim calculation (#19558)
    Output image dim calculation is wrong for conv2d and this bad code is
    c/p
    in multiple places. 
    Calculation is fixed and factored out into a function to avoid c/p.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14045271485)
    - [x] [Nightly tt-metal L2
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/14037739314)
    
    Co-authored-by: Pavle Josipovic <pjosipovic@tenstorrent.com>

--- ttnn/cpp/ttnn/operations/conv/conv2d/prepare_conv2d_weights.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/prepare_conv2d_weights.cpp
@@ -1055,10 +1055,9 @@ ttnn::Tensor prepare_conv_weights(
     DeviceComputeKernelConfig compute_config = compute_config_.value_or(get_conv_default_compute_kernel_config(device));
     const bool mm_conv = use_matmul_for_1x1_conv(kernel_size, stride, padding, dilation, groups, conv_config);
 
-    const uint32_t output_height =
-        ((input_height - kernel_size[0] - ((kernel_size[0] - 1) * (dilation[0] - 1)) + 2 * padding[0]) / stride[0]) + 1;
-    const uint32_t output_width =
-        ((input_width - kernel_size[1] - ((kernel_size[0] - 1) * (dilation[0] - 1)) + 2 * padding[1]) / stride[1]) + 1;
+    auto [output_height, output_width] =
+        calculate_output_image_size({input_height, input_width}, kernel_size, stride, padding, dilation);
+
     auto opt_conv_op_block_config = get_opt_block_config(
         mm_conv,
         in_channels,
@@ -1154,10 +1153,8 @@ ttnn::Tensor prepare_conv_bias(
     Conv2dConfig conv_config = conv_config_.value_or(Conv2dConfig());
 
     const bool mm_conv = use_matmul_for_1x1_conv(kernel_size, stride, padding, dilation, groups, conv_config);
-    const uint32_t output_height =
-        ((input_height - kernel_size[0] - ((kernel_size[0] - 1) * (dilation[0] - 1)) + 2 * padding[0]) / stride[0]) + 1;
-    const uint32_t output_width =
-        ((input_width - kernel_size[1] - ((kernel_size[0] - 1) * (dilation[0] - 1)) + 2 * padding[1]) / stride[1]) + 1;
+    auto [output_height, output_width] =
+        calculate_output_image_size({input_height, input_width}, kernel_size, stride, padding, dilation);
 
     DeviceComputeKernelConfig compute_config = compute_config_.value_or(get_conv_default_compute_kernel_config(device));
 
