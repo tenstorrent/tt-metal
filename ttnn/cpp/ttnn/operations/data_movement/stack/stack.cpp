73bfd8bae2d5492a63b02f781a7b9d71b09883f2
nikileshx <nikilesh.kumar@multicorewareinc.com>
2025-03-26 07:47:21 +0000

    Modified SDPX headers for all the files

--- ttnn/cpp/ttnn/operations/data_movement/stack/stack.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/stack/stack.cpp
@@ -1,6 +1,7 @@
-// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
+
 #include "stack.hpp"
 #include "ttnn/operations/core/core.hpp"
 #include "cpp/ttnn/operations/data_movement/unsqueeze/unsqueeze.hpp"
