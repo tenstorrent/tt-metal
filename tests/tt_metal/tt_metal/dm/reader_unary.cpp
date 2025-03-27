e7615c5d233c52fb81f745fee942f2140bcb1fde
Ata Tuzuner <atuzuner@tenstorrent.com>
2025-03-27 17:51:44 +0000

    Reconfigured kernels to use pages instead of tiles.

--- tests/tt_metal/tt_metal/dm/reader_unary.cpp
+++ tests/tt_metal/tt_metal/dm/reader_unary.cpp
@@ -11,25 +11,30 @@
 void kernel_main() {
     uint32_t src_addr = get_compile_time_arg_val(0);
     constexpr uint32_t bank_id = get_compile_time_arg_val(1);
-    constexpr uint32_t total_num_tiles = get_compile_time_arg_val(2);
-    constexpr uint32_t ublock_size_tiles = get_compile_time_arg_val(3);
-    constexpr uint32_t cb_id_in0 = get_compile_time_arg_val(4);
+    constexpr uint32_t num_of_transactions = get_compile_time_arg_val(2);
+    constexpr uint32_t transaction_size_pages = get_compile_time_arg_val(3);
+    constexpr uint32_t page_size_bytes = get_compile_time_arg_val(4);
+    constexpr uint32_t cb_id_in0 = get_compile_time_arg_val(5);
+    // constexpr uint32_t total_num_tiles = get_compile_time_arg_val(2);
+    // constexpr uint32_t ublock_size_tiles = get_compile_time_arg_val(3);
 
     // ublocks size defined in tiles
-    uint32_t ublock_size_bytes = get_tile_size(cb_id_in0) * ublock_size_tiles;
+    // constexpr uint32_t ublock_size_bytes = get_tile_size(cb_id_in0) * ublock_size_tiles;
+    constexpr uint32_t transaction_size_bytes = transaction_size_pages * page_size_bytes;
 
     // read a ublock of tiles from src to CB, and then push the ublock to unpacker
-    for (uint32_t i = 0; i < total_num_tiles; i += ublock_size_tiles) {
+    // for (uint32_t i = 0; i < total_num_tiles; i += ublock_size_tiles) {
+    for (uint32_t i = 0; i < num_of_transactions; i++) {
         // TODO: Change src address to change DRAM/core locations (single/multiple core)
         uint64_t src_noc_addr = get_noc_addr_from_bank_id<true>(bank_id, src_addr);
 
-        cb_reserve_back(cb_id_in0, ublock_size_tiles);
+        cb_reserve_back(cb_id_in0, transaction_size_pages);
         uint32_t l1_write_addr = get_write_ptr(cb_id_in0);
-        noc_async_read(src_noc_addr, l1_write_addr, ublock_size_bytes);
+        noc_async_read(src_noc_addr, l1_write_addr, transaction_size_bytes);
 
         noc_async_read_barrier();
 
-        cb_push_back(cb_id_in0, ublock_size_tiles);
-        src_addr += ublock_size_bytes;
+        cb_push_back(cb_id_in0, transaction_size_pages);
+        src_addr += transaction_size_bytes;
     }
 }
--- tests/tt_metal/tt_metal/dm/test_unary_basic.cpp
+++ tests/tt_metal/tt_metal/dm/test_unary_basic.cpp
@@ -19,9 +19,9 @@ uint32_t runtime_host_id = 0;
 
 // Test config
 struct DmConfig {
-    size_t total_num_tiles = 0;
-    size_t num_tiles_per_ublock = 0;
-    size_t tile_byte_size = 0;
+    size_t num_of_transactions = 0;
+    size_t transaction_size_pages = 0;
+    size_t page_size_bytes = 0;
     DataFormat l1_data_format = DataFormat::Invalid;
     CoreRangeSet cores = CoreRangeSet();
 };
@@ -35,10 +35,11 @@ bool run_dm(IDevice* device, const DmConfig& test_config) {
     Program program = CreateProgram();
 
     // DRAM Buffers
-    const size_t byte_size = test_config.total_num_tiles * test_config.tile_byte_size;
+    const size_t total_size_bytes =
+        test_config.num_of_transactions * test_config.transaction_size_pages * test_config.page_size_bytes;
     // TODO: Test for sharded dram buffer as well
     InterleavedBufferConfig dram_config{
-        .device = device, .size = byte_size, .page_size = byte_size, .buffer_type = BufferType::DRAM};
+        .device = device, .size = total_size_bytes, .page_size = total_size_bytes, .buffer_type = BufferType::DRAM};
 
     auto input_dram_buffer = CreateBuffer(dram_config);
     uint32_t input_dram_byte_address = input_dram_buffer->address();
@@ -47,7 +48,10 @@ bool run_dm(IDevice* device, const DmConfig& test_config) {
 
     // Input
     vector<uint32_t> packed_input = generate_packed_uniform_random_vector<uint32_t, bfloat16>(
-        -100.0f, 100.0f, byte_size / bfloat16::SIZEOF, std::chrono::system_clock::now().time_since_epoch().count());
+        -100.0f,
+        100.0f,
+        total_size_bytes / bfloat16::SIZEOF,
+        std::chrono::system_clock::now().time_since_epoch().count());
 
     // Golden output
     vector<uint32_t> packed_golden = packed_input;
@@ -58,22 +62,25 @@ bool run_dm(IDevice* device, const DmConfig& test_config) {
     vector<uint32_t> reader_compile_args = {
         (uint32_t)input_dram_byte_address,
         (uint32_t)0,
-        (uint32_t)test_config.total_num_tiles,
-        (uint32_t)test_config.num_tiles_per_ublock,
+        (uint32_t)test_config.num_of_transactions,
+        (uint32_t)test_config.transaction_size_pages,
+        (uint32_t)test_config.page_size_bytes,
         (uint8_t)l1_cb_index,
     };
 
     vector<uint32_t> writer_compile_args = {
         (uint32_t)output_dram_byte_address,
         (uint32_t)0,
-        (uint32_t)test_config.total_num_tiles,
-        (uint32_t)test_config.num_tiles_per_ublock,
+        (uint32_t)test_config.num_of_transactions,
+        (uint32_t)test_config.transaction_size_pages,
+        (uint32_t)test_config.page_size_bytes,
         (uint8_t)l1_cb_index,
     };
 
     // Create circular buffers
-    CircularBufferConfig l1_cb_config = CircularBufferConfig(byte_size, {{l1_cb_index, test_config.l1_data_format}})
-                                            .set_page_size(l1_cb_index, test_config.tile_byte_size);
+    CircularBufferConfig l1_cb_config =
+        CircularBufferConfig(total_size_bytes, {{l1_cb_index, test_config.l1_data_format}})
+            .set_page_size(l1_cb_index, test_config.page_size_bytes);
     auto l1_cb = CreateCircularBuffer(program, test_config.cores, l1_cb_config);
 
     // Kernels
@@ -117,14 +124,14 @@ bool run_dm(IDevice* device, const DmConfig& test_config) {
 }  // namespace unit_tests::dm
 
 /* ========== Test case for varying transaction numbers and sizes ========== */
-TEST_F(DeviceFixture, TensixDataMovementDRAMInterleaved) {
-    // TODO: Change total_num_tiles to test with different total data sizes
-    // TODO: Change num_tiles_per_ublock to test with different packet sizes
-    // TODO: Set tile byte size to minimum packet size (one flit) depending on ARCH
+TEST_F(DeviceFixture, TensixDataMovementDRAMInterleavedPacketSizes) {
+    // TODO: Change num_of_transactions to test with different total data sizes
+    // TODO: Change transaction_size_pages to test with different packet sizes
+    // TODO: Set page_size_bytes to minimum packet size (one flit) depending on ARCH
     // Parameters
     size_t num_of_transactions = 1;     // Number of transactions
-    size_t transaction_size_tiles = 1;  // Transaction size
-    size_t tile_byte_size = 16 * 16;    // Tile byte size
+    size_t transaction_size_pages = 1;  // Transaction size in pages
+    size_t page_size_bytes = 16 * 16;   // Page size in bytes
 
     // Cores
     CoreRange core_range({0, 0}, {0, 0});
@@ -132,9 +139,9 @@ TEST_F(DeviceFixture, TensixDataMovementDRAMInterleaved) {
 
     // Test config
     unit_tests::dm::DmConfig test_config = {
-        .total_num_tiles = num_of_transactions,
-        .num_tiles_per_ublock = transaction_size_tiles,
-        .tile_byte_size = tile_byte_size,
+        .num_of_transactions = num_of_transactions,
+        .transaction_size_pages = transaction_size_pages,
+        .page_size_bytes = page_size_bytes,
         .l1_data_format = DataFormat::Float16_b,
         .cores = core_range_set};
 
@@ -146,12 +153,13 @@ TEST_F(DeviceFixture, TensixDataMovementDRAMInterleaved) {
     }
 }
 
-// TODO: New test for different core locations
-// TODO: Configure master cores here
-// TODO: Use another core range set to configure slave cores, and add it to config
+// TODO: New test for varying core locations
+/* ========== Test case for varying core locations ========== */
+// TEST_F(DeviceFixture, TensixDataMovementDRAMInterleavedCoreLocations) {
 
 // TODO: New test for sharded DRAM buffer with
 //      1. different transaction numbers and sizes
 //      2. different core locations
+// TODO: New test for core-to-core transactions. May use master and slave core range sets, added in the test config
 
 }  // namespace tt::tt_metal
