a76262dbe407dccbf58d10b470312ca60d1b7e94
Pavle Josipovic <pjosipovic@tenstorrent.com>
2025-03-27 15:03:35 +0000

    Revert "In Place Halo (#19056)"
    This reverts commit e8a36c2cee99bf33d51ea333e5b44675d4c8c9ab.

--- ttnn/cpp/ttnn/operations/sliding_window/halo/device/untilize_with_halo_v2_program_factory.hpp
+++ ttnn/cpp/ttnn/operations/sliding_window/halo/device/untilize_with_halo_v2_program_factory.hpp
@@ -13,9 +13,7 @@ tt::tt_metal::operation::ProgramWithCallbacks untilize_with_halo_multi_core_v2(
     const Tensor& input_tensor,
     const uint32_t pad_val,
     const uint32_t ncores_nhw,
-    const uint32_t ncores_c,
     const uint32_t max_out_nsticks_per_core,
-    const uint32_t max_ref_size,
     const Tensor& padding_config1,
     const Tensor& padding_config2,
     const Tensor& local_config1,
@@ -25,7 +23,6 @@ tt::tt_metal::operation::ProgramWithCallbacks untilize_with_halo_multi_core_v2(
     const bool remote_read,
     const bool transpose_mcast,
     Tensor& output_tensor,
-    const bool capture_buffers,
-    bool in_place = false);  // Used by halo op to cache internally created config buffers with the program
-                             // Untilize with Halo V2 op takes them as inputs from the user, so doesn't capture
+    const bool capture_buffers);  // Used by halo op to cache internally created config buffers with the program
+                                  // Untilize with Halo V2 op takes them as inputs from the user, so doesn't capture
 }  // namespace ttnn::operations::data_movement::detail
