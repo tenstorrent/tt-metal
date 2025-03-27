67815d950eb9e64b37a83eb124ae90098f5375fd
Pavle Josipović <pavlejosipovic@users.noreply.github.com>
2025-03-27 17:38:19 +0100

    Revert "In Place Halo (#19056)" (#19787)
    This reverts commit e8a36c2cee99bf33d51ea333e5b44675d4c8c9ab.
    This commit is causing hangs on BH for conv/pool ops that run inplace
    halo.
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14110174744)
    
    Co-authored-by: Pavle Josipovic <pjosipovic@tenstorrent.com>

--- models/experimental/functional_unet/tests/test_unet_perf.py
+++ models/experimental/functional_unet/tests/test_unet_perf.py
@@ -49,7 +49,7 @@ def test_unet_perf_device(batch: int, groups: int, expected_device_perf_fps: flo
 )
 @pytest.mark.parametrize(
     "batch, groups, iterations, expected_compile_time, expected_throughput",
-    ((1, 2, 128, 25.0, 825.0),),
+    ((1, 2, 128, 25.0, 830.0),),
 )
 def test_unet_trace_perf(
     batch: int,
@@ -93,7 +93,7 @@ def test_unet_trace_perf(
     "batch, groups, iterations, expected_compile_time, expected_throughput, use_async_mode",
     (
         (1, 2, 128, 25.0, 1450.0, True),
-        (1, 2, 128, 25.0, 1635.0, False),
+        (1, 2, 128, 25.0, 1650.0, False),
     ),
 )
 def test_unet_trace_perf_multi_device(
