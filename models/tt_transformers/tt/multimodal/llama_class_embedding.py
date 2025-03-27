5f439ade5dd3b13767538de25187f4e2807b5cd3
Gongyu Wang <gwang@tenstorrent.com>
2025-03-27 20:16:15 +0000

    Fix a bug in llama_class_embedding.py where TILE_LAYOUT are used on input tensors to ttnn.concat operator with effectively dim=-2 (not supported by ttnn).

--- models/tt_transformers/tt/multimodal/llama_class_embedding.py
+++ models/tt_transformers/tt/multimodal/llama_class_embedding.py
@@ -29,7 +29,7 @@ class TtLlamaClassEmbedding(LightweightModule):
         self.class_embedding = ttnn.as_tensor(
             class_embedding,
             dtype=ttnn.bfloat16,
-            layout=ttnn.TILE_LAYOUT,
+            layout=ttnn.ROW_MAJOR_LAYOUT,  # [INFO] TILE_LAYOUT shouldn't be used here because of the `dim=2` concat that this tensor is used in
             device=self.mesh_device,
             memory_config=ttnn.DRAM_MEMORY_CONFIG,
             mesh_mapper=ttnn.ReplicateTensorToMesh(self.mesh_device),
@@ -39,6 +39,7 @@ class TtLlamaClassEmbedding(LightweightModule):
         bsz = x.shape[1]
         # Broadcast class embedding to match input batch size
         class_embedding = ttnn.concat([self.class_embedding] * bsz, dim=1)  # Broadcast batch size
-        x = ttnn.concat([class_embedding, x], dim=2)
+        x = ttnn.concat([class_embedding, x], dim=2)  # Output ROW_MAJOR_LAYOUT
+        x = ttnn.tilize(x)  # Convert back to TILE_LAYOUT
 
         return x
