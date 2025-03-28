c399ee1ba3273c52e3b1bf2339bcb73581ea0059
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-03-28 16:30:50 +0000

    #0: sfpi staging

--- tt_metal/hw/cmake/sfpi-version.cmake
+++ tt_metal/hw/cmake/sfpi-version.cmake
@@ -1,6 +1,6 @@
 # Define the SFPI version to use
 
 set(SFPI_x86_64_Linux_RELEASE
-    "v6.6.0/sfpi-release.tgz"
-    "8eed4d1128809e2fb2df00e04b5a51ea"
+    "v6.6.0-staging/sfpi-release.tgz"
+    "1d38cb69cac6df73cb90c7f1da06bdc7"
 )
