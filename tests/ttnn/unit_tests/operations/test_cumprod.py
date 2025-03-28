afdbb1ff824c9a70e87815b10854de8665c51a96
Jaroslaw Bartosz Bieniek <jbbieniek@tenstorrent.com>
2025-03-28 08:31:32 +0000

    19549: update test_cumprod.py
    Update tests in `test_cumprod.py`

--- tests/ttnn/unit_tests/operations/test_cumprod.py
+++ tests/ttnn/unit_tests/operations/test_cumprod.py
@@ -13,29 +13,54 @@ import ttnn
 @pytest.mark.parametrize(
     "shape",
     [
+        [],
         [0],
         [1],
         [3, 3],
         [16, 16],
         [32, 32, 32],
         [32, 32, 64],
-        [64, 64, 64, 64],
+        [1, 0, 32, 32],
         [1000, 32, 32],
     ],
 )
-@pytest.mark.parametrize(
-    "dtype",
-    [
-        (torch.bfloat16, ttnn.bfloat16),
-    ],
-)
-def test_cumprod(dim, shape, dtype, device):
+def test_cumprod_with_bfloat16(dim, shape, device):
     torch.manual_seed(22041997)
 
-    input_tensor = torch.randn(*shape, dtype=dtype[0])
+    input_tensor = torch.randn(shape, dtype=torch.bfloat16)
     result_tensor_torch = torch.cumprod(input_tensor, 0)
-    ttnn_tensor = ttnn.from_torch(input_tensor, dtype[1], layout=ttnn.Layout.TILE, device=device)
-    result_tensor = ttnn.experimental.cumprod(ttnn_tensor, 0)
+    ttnn_tensor = ttnn.from_torch(input_tensor, ttnn.bfloat16, layout=ttnn.Layout.TILE, device=device)
+    result_tensor = ttnn.experimental.cumprod(ttnn_tensor, dim)
+
+    assert ttnn_tensor.shape == result_tensor.shape
+    assert ttnn_tensor.dtype == result_tensor.dtype
+    assert input_tensor.shape == ttnn_tensor.shape
+    assert result_tensor_torch.shape == ttnn_tensor.shape
+    assert result_tensor_torch.shape == result_tensor.shape
+
+
+@pytest.mark.parametrize("dim,", [-10, -1, 0, 3, 10])
+@pytest.mark.parametrize(
+    "shape",
+    [
+        [],
+        [0],
+        [1],
+        [3, 3],
+        [16, 16],
+        [32, 32, 32],
+        [32, 32, 64],
+        [1, 0, 32, 32],
+        [1000, 32, 32],
+    ],
+)
+def test_cumprod_with_uint8(dim, shape, device):
+    torch.manual_seed(22041997)
+
+    input_tensor = torch.randint(0, 100, shape, dtype=torch.uint8)
+    result_tensor_torch = torch.cumprod(input_tensor, 0)
+    ttnn_tensor = ttnn.from_torch(input_tensor, ttnn.uint8, layout=ttnn.Layout.TILE, device=device)
+    result_tensor = ttnn.experimental.cumprod(ttnn_tensor, dim)
 
     assert ttnn_tensor.shape == result_tensor.shape
     assert ttnn_tensor.dtype == result_tensor.dtype
