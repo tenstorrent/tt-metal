1cb38e3aec2abb29d3d8a8220e3aa9ebb0c93866
Pavlo Hilei <45758974+philei-tt@users.noreply.github.com>
2025-03-31 18:53:41 +0200

    Make ttnn.squeeze great again: unify behavior with torch (#19747)
    ### Ticket
    Github Issue https://github.com/tenstorrent/tt-metal/issues/19746 
    
    ### Problem description
    `ttnn.squeeze` doesn't support `dim=None` and `dim=[...]` cases, like
    [torch.squeeze](https://pytorch.org/docs/stable/generated/torch.squeeze.html)
    does.
    
    ### What's changed
    - If `ttnn.squeeze` is not provided with dim (or dim=None explicitly),
    is squeezes all 1-sized dimensions
    - if `ttnn.squeeze` is provided with list of dims, then it tries to
    squeeze those dimensions
    - Added more checks to have similar errors to pytorch
    - Added more tests (dim=None, dim=[...], errors) and extend shapes for
    available tests (0 rank handling)
    - As a small bonus I fixed printing 0-ranked tensors
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    (https://github.com/tenstorrent/tt-metal/actions/runs/14145537850)
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    (https://github.com/tenstorrent/tt-metal/actions/runs/14145537850)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [ ] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: Pavlo Hilei <pavlo.hilei@gmail.com>

--- ttnn/cpp/ttnn/operations/data_movement/squeeze/squeeze.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/squeeze/squeeze.cpp
@@ -1,37 +1,82 @@
-// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
 
 #include "squeeze.hpp"
+#include "tt-metalium/small_vector.hpp"
 #include "ttnn/operations/core/core.hpp"
 
 namespace ttnn::operations::data_movement {
 
-ttnn::Tensor SqueezeOperation::invoke(const ttnn::Tensor& input_tensor, const int dim) {
+ttnn::Tensor SqueezeOperation::invoke(const ttnn::Tensor& input_tensor, const ttnn::SmallVector<int>& dim) {
     const auto original_logical_shape = input_tensor.get_logical_shape();
     const auto padded_shape = input_tensor.get_padded_shape();
-    const auto input_tensor_rank = original_logical_shape.rank();
+    auto input_tensor_rank = original_logical_shape.rank();
 
-    int normal_dim = dim;
-    if (dim < 0) {
-        // Handle negative dimension by converting it to positive
-        normal_dim += input_tensor_rank;
+    SmallVector<uint32_t> new_logical_shape(original_logical_shape.cbegin(), original_logical_shape.cend());
+    SmallVector<uint32_t> new_padded_shape(padded_shape.cbegin(), padded_shape.cend());
+
+    // Explicitly copy dim to avoid modifying the input
+    auto dims = dim;
+
+    // handle negative dimensions
+    for (size_t i = 0; i < dims.size(); ++i) {
+        if (dims[i] < 0) {
+            dims[i] += input_tensor_rank;
+        }
+    }
+    // Sort the dimensions in descending order to avoid issues with modifying new_shape in loop
+    std::sort(dims.rbegin(), dims.rend());
+
+    // Special ugly case for 0-ranked input
+    if (input_tensor_rank == 0) [[unlikely]] {
+        if (dims.empty() || (dims.size() == 1 && (dims[0] == 0 || dims[0] == -1))) {
+            return input_tensor;
+        }
+        TT_THROW("Dimension out of range (expected to be of [-1, 0], but got {})", dims[0]);
     }
 
-    // If dim is out of range or original dimension was not of size 1, include all dimensions
-    if (normal_dim < 0 || normal_dim >= original_logical_shape.rank() || original_logical_shape[normal_dim] != 1) {
+    for (size_t i = 0; i < dims.size(); ++i) {
+        const auto dim = dims[i];
+        // Check duplicate dimensions
+        if (i > 0) {
+            TT_FATAL(dim != dims[i - 1], "dim {} appears multiple times in the list of dims", dim);
+        }
+        TT_FATAL(
+            (dim >= 0) && (dim < input_tensor_rank),
+            "Dimension out of range (expected to be in range of [{},{}], but got {})",
+            -static_cast<std::ptrdiff_t>(input_tensor_rank),
+            input_tensor_rank - 1,
+            dim);
+
+        // If original dimension was not of size 1, include all dimensions
+        if (original_logical_shape[dim] != 1) {
+            continue;
+        }
+
+        new_logical_shape.erase(new_logical_shape.begin() + dim);
+        new_padded_shape.erase(new_padded_shape.begin() + dim);
+    }
+
+    // Note: don't have to check padded too
+    if (new_logical_shape == original_logical_shape) {
         return input_tensor;
     }
 
-    SmallVector<uint32_t> original_logical_shape_vector(original_logical_shape.cbegin(), original_logical_shape.cend());
-    SmallVector<uint32_t> padded_shape_vector(padded_shape.cbegin(), padded_shape.cend());
-    original_logical_shape_vector.erase(original_logical_shape_vector.begin() + normal_dim);
-    padded_shape_vector.erase(padded_shape_vector.begin() + normal_dim);
-
     return ttnn::reshape(
-        input_tensor,
-        ttnn::Shape(std::move(original_logical_shape_vector)),
-        ttnn::Shape(std::move(padded_shape_vector)));
+        input_tensor, ttnn::Shape(std::move(new_logical_shape)), ttnn::Shape(std::move(new_padded_shape)));
+}
+
+ttnn::Tensor SqueezeOperation::invoke(const ttnn::Tensor& input_tensor, int dim) {
+    ttnn::SmallVector<int> dims{dim};
+    return invoke(input_tensor, dims);
+}
+
+ttnn::Tensor SqueezeOperation::invoke(const ttnn::Tensor& input_tensor) {
+    auto input_tensor_rank = input_tensor.get_logical_shape().rank();
+    ttnn::SmallVector<int> dims(input_tensor_rank);
+    std::iota(dims.begin(), dims.end(), 0);
+    return invoke(input_tensor, dims);
 }
 
 }  // namespace ttnn::operations::data_movement
--- ttnn/cpp/ttnn/operations/data_movement/squeeze/squeeze_pybind.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/squeeze/squeeze_pybind.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -9,7 +9,6 @@
 
 #include "cpp/pybind11/decorators.hpp"
 #include "ttnn/operations/data_movement/squeeze/squeeze.hpp"
-#include "ttnn/types.hpp"
 
 namespace ttnn::operations::data_movement {
 
@@ -22,11 +21,22 @@ void bind_squeeze(pybind11::module& module, const data_movement_operation_t& ope
         operation,
         doc,
         ttnn::pybind_overload_t{
-            [](const data_movement_operation_t& self, const ttnn::Tensor& input_tensor, const int dim) -> ttnn::Tensor {
-                return self(input_tensor, dim);
+            [](const data_movement_operation_t& self, const ttnn::Tensor& input_tensor, const pybind11::object& dim)
+                -> ttnn::Tensor {
+                if (dim.is_none()) {  // None
+                    return self(input_tensor);
+                } else if (pybind11::isinstance<pybind11::int_>(dim)) {  // int
+                    return self(input_tensor, dim.cast<int>());
+                } else if (pybind11::isinstance<pybind11::list>(dim)) {  // List[int]
+                    auto dims = dim.cast<ttnn::SmallVector<int>>();
+                    return self(input_tensor, dims);
+                } else {
+                    throw std::invalid_argument("dim must be an int, a list of ints, or None");
+                }
             },
             py::arg("input_tensor"),
-            py::arg("dim")});
+            py::arg("dim") = pybind11::none()  // Default value is None
+        });
 }
 
 }  // namespace detail
@@ -37,7 +47,9 @@ void py_bind_squeeze(pybind11::module& module) {
         ttnn::squeeze,
         R"doc(squeeze(input_tensor: ttnn.Tensor,  dim: int) -> ttnn.Tensor
 
-        Returns a tensor squeezed at the specified dimension. Pytorch supports a tuple as well as a single scalar value for dim, currently our version only supports scalar values. We will address this in the future. If input_tensor.shape()[dim] is not 1, squeeze will be ignored for that shape.
+        Returns a tensor with the specified dimensions squeezed. If `dim` is not provided, all dimensions of size 1 will be squeezed. If `dim` is an integer, only the specified dimension will be squeezed. If `dim` is a list of integers, all specified dimensions will be squeezed.
+
+        If a specified dimension in `dim` does not have size 1, it will be ignored.
 
         Equivalent pytorch code:
 
