d37aa013b93277307ddb66caf73f176741e85a82
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-03-31 08:43:43 -0400

    #0: sfpi update v6.7.0 (#19909)
    Update https://github.com/tenstorrent/sfpi/releases/tag/v6.7.0

--- tt_metal/hw/cmake/sfpi-version.cmake
+++ tt_metal/hw/cmake/sfpi-version.cmake
@@ -1,6 +1,6 @@
 # Define the SFPI version to use
 
 set(SFPI_x86_64_Linux_RELEASE
-    "v6.6.0/sfpi-release.tgz"
-    "8eed4d1128809e2fb2df00e04b5a51ea"
+    "v6.7.0/sfpi-release.tgz"
+    "7d955cfce78e0bcf3325151b3bd478c8"
 )
