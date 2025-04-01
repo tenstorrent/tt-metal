366ccc8f4cef10c62d3f3966d433a92ad9ee8324
Kannika Kabilar <kkabilar@tenstorrent.com>
2025-03-31 21:28:49 -0400

    Update demo.py

--- models/demos/wormhole/mamba/demo/demo.py
+++ models/demos/wormhole/mamba/demo/demo.py
@@ -372,7 +372,7 @@ def run_mamba_demo(
     )
     logger.info(f"Time to first token: {(1e3 * time_to_first_token):.2f} ms")
 
-    chunk_size_to_prefill_targets_tok_per_s = {32: 135.0, 128: 430.0}  # perf is different for different chunk sizes
+    chunk_size_to_prefill_targets_tok_per_s = {32: 135.0, 128: 400.0}  # perf is different for different chunk sizes
     targets = {
         "prefill_t/s": chunk_size_to_prefill_targets_tok_per_s[prefill_chunk_size],
         "decode_t/s": 340.6,
