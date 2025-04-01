2e06f5eb87fba835309a14a4bf57413fdcb89b5a
Pratikkumar Prajapati <pprajapati@tenstorrent.com>
2025-04-01 19:40:27 +0000

    Add mesh_device=T3K to mistral t3000 test scripts
    Signed-off-by: Pratikkumar Prajapati <pprajapati@tenstorrent.com>

--- tests/scripts/t3000/run_t3000_frequent_tests.sh
+++ tests/scripts/t3000/run_t3000_frequent_tests.sh
@@ -192,11 +192,12 @@ run_t3000_mistral_tests() {
 
   echo "LOG_METAL: Running run_t3000_mistral_frequent_tests"
 
+  mesh_device=T3K
   wh_arch_yaml=wormhole_b0_80_arch_eth_dispatch.yaml
   tt_cache_path="/mnt/MLPerf/tt_dnn-models/Mistral/TT_CACHE/Mistral-7B-Instruct-v0.3"
   hf_model="/mnt/MLPerf/tt_dnn-models/Mistral/hub/models--mistralai--Mistral-7B-Instruct-v0.3/snapshots/e0bc86c23ce5aae1db576c8cca6f06f1f73af2db"
-  WH_ARCH_YAML=$wh_arch_yaml TT_CACHE_PATH=$tt_cache_path HF_MODEL=$hf_model pytest -n auto models/tt_transformers/tests/test_model.py -k full ; fail+=$?
-  WH_ARCH_YAML=$wh_arch_yaml TT_CACHE_PATH=$tt_cache_path HF_MODEL=$hf_model pytest -n auto models/tt_transformers/tests/test_model_prefill.py ; fail+=$?
+  MESH_DEVICE=$mesh_device WH_ARCH_YAML=$wh_arch_yaml TT_CACHE_PATH=$tt_cache_path HF_MODEL=$hf_model pytest -n auto models/tt_transformers/tests/test_model.py -k full ; fail+=$?
+  MESH_DEVICE=$mesh_device WH_ARCH_YAML=$wh_arch_yaml TT_CACHE_PATH=$tt_cache_path HF_MODEL=$hf_model pytest -n auto models/tt_transformers/tests/test_model_prefill.py ; fail+=$?
 
   # Record the end time
   end_time=$(date +%s)
