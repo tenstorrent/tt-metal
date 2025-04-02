eb4127d0a8f5f9a7e3112c77ee7a16cc239dbaa8
Stanislav Minakov <sminakov@tenstorrent.com>
2025-04-02 06:45:04 +0000

    T3K tests

--- tests/ttnn/unit_tests/gtests/ccl/test_multi_tensor_ccl.cpp
+++ tests/ttnn/unit_tests/gtests/ccl/test_multi_tensor_ccl.cpp
@@ -4,45 +4,76 @@
 
 #include "gtest/gtest.h"
 
-#include "ttnn_test_fixtures.hpp"
-
 #include "ttnn/operations/ccl/all_gather/all_gather.hpp"
 #include "ttnn/operations/ccl/reduce_scatter/reduce_scatter.hpp"
 #include "ttnn/operations/experimental/ccl/all_reduce/all_reduce.hpp"
 #include "ttnn/operations/experimental/ccl/all_gather_async/all_gather_async.hpp"
+#include "ttnn/operations/experimental/ccl/reduce_scatter_async/reduce_scatter.hpp"
+#include "ttnn/operations/experimental/ccl/all_reduce_async/all_reduce_async.hpp"
+
+#include "tt_metal/tt_metal/common/multi_device_fixture.hpp"
+#include "test_fabric_edm_common.hpp"
 
 #include <vector>
 
-namespace ttnn {
+namespace tt::tt_metal {
 
 namespace {
 namespace CMAKE_UNIQUE_NAMESPACE {
-global_semaphore::MultiDeviceGlobalSemaphore create_global_semaphore(
-    const std::map<chip_id_t, tt::tt_metal::IDevice*>& devs) {
-    std::vector<IDevice*> devices;
-    for (const auto& dev : devs) {
-        devices.push_back(dev.second);
-    }
-    return global_semaphore::create_global_semaphore_with_same_address(
+
+ttnn::global_semaphore::MultiDeviceGlobalSemaphore create_global_semaphore(const std::vector<IDevice*>& devices) {
+    return ttnn::global_semaphore::create_global_semaphore_with_same_address(
         devices,
         devices.at(0)->worker_cores(HalProgrammableCoreType::TENSIX, SubDeviceId{0}),
         0,
         tt::tt_metal::BufferType::L1,
         10);
 }
+
+std::vector<IDevice*> get_line_devices(distributed::MeshDevice* mesh_device) {
+    auto view = mesh_device->get_view();
+    return {
+        view.get_device(distributed::MeshCoordinate(0, 0)),
+        view.get_device(distributed::MeshCoordinate(0, 1)),
+        view.get_device(distributed::MeshCoordinate(0, 2)),
+        view.get_device(distributed::MeshCoordinate(0, 3))};
+}
+
+struct FabricGuard {
+    FabricGuard(const std::vector<IDevice*>& devices) : devices(devices) {
+        setup_test_with_persistent_fabric(
+            devices, programs, subdevice_managers, fabric_programs, fabric_program_ptrs, line_fabric, true);
+    }
+    ~FabricGuard() {
+        persistent_fabric_teardown_sequence(
+            devices, subdevice_managers, line_fabric.value(), tt::tt_fabric::TerminationSignal::IMMEDIATELY_TERMINATE);
+    }
+
+    std::vector<IDevice*> devices;
+    std::vector<Program> programs;
+    std::optional<SubdeviceInfo> subdevice_managers;
+    std::optional<std::vector<Program>> fabric_programs;
+    std::vector<Program*> fabric_program_ptrs;
+    std::optional<ttnn::ccl::EdmLineFabricOpInterface> line_fabric;
+};
+
 }  // namespace CMAKE_UNIQUE_NAMESPACE
 }  // namespace
 
-TEST_F(MultiDeviceN300Fixture, AllGather) {
+TEST_F(T3000MultiCQMeshDeviceFixture, AllGather) {
+    mesh_device_->enable_program_cache();
+    auto devices = CMAKE_UNIQUE_NAMESPACE::get_line_devices(mesh_device_.get());
+
     std::vector<ttnn::Tensor> tensors;
     TensorSpec tensor_spec(
         ttnn::Shape({1, 8, 1024, 768}), TensorLayout(DataType::BFLOAT16, PageConfig(Layout::TILE), MemoryConfig{}));
-    for (int dev_idx = 0; dev_idx < devs.size(); dev_idx++) {
+    for (int dev_idx = 0; dev_idx < devices.size(); dev_idx++) {
         std::vector<bfloat16> data(tensor_spec.logical_shape().volume(), bfloat16(static_cast<float>(dev_idx)));
-        tensors.push_back(Tensor::from_vector(std::move(data), tensor_spec, devs[dev_idx]));
+        tensors.push_back(Tensor::from_vector(std::move(data), tensor_spec, devices[dev_idx]));
     }
-    auto all_gathered = ttnn::all_gather(tensors, 0);
-    for (int dev_idx = 0; dev_idx < devs.size(); dev_idx++) {
+    auto all_gathered =
+        ttnn::all_gather(tensors, 0, 1, std::nullopt, std::nullopt, std::nullopt, ttnn::ccl::Topology::Linear);
+    for (int dev_idx = 0; dev_idx < devices.size(); dev_idx++) {
         auto data = all_gathered[dev_idx].to_vector<bfloat16>();
         for (int i = 0; i < data.size(); i++) {
             float expected = static_cast<float>(i / tensor_spec.logical_shape().volume());
@@ -51,17 +82,22 @@ TEST_F(MultiDeviceN300Fixture, AllGather) {
     }
 }
 
-TEST_F(MultiDeviceN300Fixture, AllGatherAsync) {
+TEST_F(T3000MultiCQMeshDeviceFixture, AllGatherAsync) {
+    mesh_device_->enable_program_cache();
+    auto devices = CMAKE_UNIQUE_NAMESPACE::get_line_devices(mesh_device_.get());
+    CMAKE_UNIQUE_NAMESPACE::FabricGuard fabric_guard(devices);
+
     std::vector<ttnn::Tensor> tensors;
     TensorSpec tensor_spec(
         ttnn::Shape({1, 8, 1024, 768}), TensorLayout(DataType::BFLOAT16, PageConfig(Layout::TILE), MemoryConfig{}));
-    for (int dev_idx = 0; dev_idx < devs.size(); dev_idx++) {
+    for (int dev_idx = 0; dev_idx < devices.size(); dev_idx++) {
         std::vector<bfloat16> data(tensor_spec.logical_shape().volume(), bfloat16(static_cast<float>(dev_idx)));
-        tensors.push_back(Tensor::from_vector(std::move(data), tensor_spec, devs[dev_idx]));
+        tensors.push_back(Tensor::from_vector(std::move(data), tensor_spec, devices[dev_idx]));
     }
-    auto semaphore = CMAKE_UNIQUE_NAMESPACE::create_global_semaphore(devs);
-    auto all_gathered = ttnn::experimental::all_gather_async(tensors, 0, semaphore);
-    for (int dev_idx = 0; dev_idx < devs.size(); dev_idx++) {
+    auto semaphore = CMAKE_UNIQUE_NAMESPACE::create_global_semaphore(devices);
+    auto all_gathered = ttnn::experimental::all_gather_async(
+        tensors, 0, semaphore, 1, std::nullopt, ttnn::ccl::Topology::Linear, SubDeviceId(0), true);
+    for (int dev_idx = 0; dev_idx < devices.size(); dev_idx++) {
         auto data = all_gathered[dev_idx].to_vector<bfloat16>();
         for (int i = 0; i < data.size(); i++) {
             float expected = static_cast<float>(i / tensor_spec.logical_shape().volume());
@@ -70,40 +106,48 @@ TEST_F(MultiDeviceN300Fixture, AllGatherAsync) {
     }
 }
 
-TEST_F(MultiDeviceN300Fixture, ReduceScatter) {
+TEST_F(T3000MultiCQMeshDeviceFixture, ReduceScatter) {
+    mesh_device_->enable_program_cache();
+    auto devices = CMAKE_UNIQUE_NAMESPACE::get_line_devices(mesh_device_.get());
+
     std::vector<ttnn::Tensor> tensors;
     TensorSpec tensor_spec(
         ttnn::Shape({1, 8, 1024, 768}), TensorLayout(DataType::BFLOAT16, PageConfig(Layout::TILE), MemoryConfig{}));
-    for (int dev_idx = 0; dev_idx < devs.size(); dev_idx++) {
+    for (int dev_idx = 0; dev_idx < devices.size(); dev_idx++) {
         std::vector<bfloat16> data(tensor_spec.logical_shape().volume(), bfloat16(static_cast<float>(1)));
-        tensors.push_back(Tensor::from_vector(std::move(data), tensor_spec, devs[dev_idx]));
+        tensors.push_back(Tensor::from_vector(std::move(data), tensor_spec, devices[dev_idx]));
     }
-    auto reduced = ttnn::reduce_scatter(tensors, 3, operations::reduction::ReduceType::Sum);
-    for (int dev_idx = 0; dev_idx < devs.size(); dev_idx++) {
+    auto reduced = ttnn::reduce_scatter(
+        tensors, 3, ttnn::operations::reduction::ReduceType::Sum, 1, std::nullopt, ttnn::ccl::Topology::Linear);
+    for (int dev_idx = 0; dev_idx < devices.size(); dev_idx++) {
         auto data = reduced[dev_idx].to_vector<bfloat16>();
         for (int i = 0; i < data.size(); i++) {
-            float expected = static_cast<float>(devs.size());
+            float expected = static_cast<float>(devices.size());
             EXPECT_EQ(data[i].to_float(), expected);
         }
     }
 }
 
-TEST_F(MultiDeviceN300Fixture, AllReduce) {
+TEST_F(T3000MultiCQMeshDeviceFixture, AllReduce) {
+    mesh_device_->enable_program_cache();
+    auto devices = CMAKE_UNIQUE_NAMESPACE::get_line_devices(mesh_device_.get());
+
     std::vector<ttnn::Tensor> tensors;
     TensorSpec tensor_spec(
         ttnn::Shape({1, 8, 1024, 768}), TensorLayout(DataType::BFLOAT16, PageConfig(Layout::TILE), MemoryConfig{}));
-    for (int dev_idx = 0; dev_idx < devs.size(); dev_idx++) {
+    for (int dev_idx = 0; dev_idx < devices.size(); dev_idx++) {
         std::vector<bfloat16> data(tensor_spec.logical_shape().volume(), bfloat16(static_cast<float>(1)));
-        tensors.push_back(Tensor::from_vector(std::move(data), tensor_spec, devs[dev_idx]));
+        tensors.push_back(Tensor::from_vector(std::move(data), tensor_spec, devices[dev_idx]));
     }
-    auto reduced = ttnn::experimental::all_reduce(tensors, operations::reduction::ReduceType::Sum);
-    for (int dev_idx = 0; dev_idx < devs.size(); dev_idx++) {
+    auto reduced = ttnn::experimental::all_reduce(
+        tensors, ttnn::operations::reduction::ReduceType::Sum, 1, std::nullopt, ttnn::ccl::Topology::Linear);
+    for (int dev_idx = 0; dev_idx < devices.size(); dev_idx++) {
         auto data = reduced[dev_idx].to_vector<bfloat16>();
         for (int i = 0; i < data.size(); i++) {
-            float expected = static_cast<float>(devs.size());
+            float expected = static_cast<float>(devices.size());
             EXPECT_EQ(data[i].to_float(), expected);
         }
     }
 }
 
-}  // namespace ttnn
+}  // namespace tt::tt_metal
--- tests/ttnn/unit_tests/gtests/ttnn_test_fixtures.hpp
+++ tests/ttnn/unit_tests/gtests/ttnn_test_fixtures.hpp
@@ -118,26 +118,4 @@ protected:
     size_t num_devices_;
 };
 
-class MultiDeviceN300Fixture : public ::testing::Test {
-protected:
-    void SetUp() override {
-        auto slow_dispatch = getenv("TT_METAL_SLOW_DISPATCH_MODE");
-        auto arch = tt::get_arch_from_string(tt::test_utils::get_umd_arch_name());
-        auto num_devices = tt::tt_metal::GetNumAvailableDevices();
-        if (slow_dispatch) {
-            GTEST_SKIP() << "Skipping Multi CQ test suite, since it can only be run in Fast Dispatch Mode.";
-        }
-        if (num_devices != 2 or arch != tt::ARCH::WORMHOLE_B0) {
-            GTEST_SKIP() << "Skipping T3K Multi CQ test suite on non T3K machine.";
-        }
-
-        devs = tt::tt_metal::detail::CreateDevices(
-            {0, 1}, 2, DEFAULT_L1_SMALL_SIZE, DEFAULT_TRACE_REGION_SIZE, DispatchCoreConfig{DispatchCoreType::ETH});
-    }
-
-    void TearDown() override { tt::tt_metal::detail::CloseDevices(devs); }
-
-    std::map<chip_id_t, tt::tt_metal::IDevice*> devs;
-};
-
 }  // namespace ttnn
