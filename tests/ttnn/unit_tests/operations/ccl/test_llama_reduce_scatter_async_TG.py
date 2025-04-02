5f426babc50ae6a5e1867a4a299e7ce5d052d63e
Oleg Milyutin <omilyutin@tenstorrent.com>
2025-04-02 21:05:12 +0000

    Fix create_global_semaphore

--- tests/ttnn/unit_tests/operations/ccl/test_llama_reduce_scatter_async_TG.py
+++ tests/ttnn/unit_tests/operations/ccl/test_llama_reduce_scatter_async_TG.py
@@ -13,7 +13,6 @@ from models.utility_functions import skip_for_grayskull
 from tests.ttnn.unit_tests.operations.ccl.test_ccl_common import (
     create_and_load_sub_device_manager_with_fabric_interface,
     teardown_fabric_interface,
-    create_global_semaphore,
 )
 
 from tests.ttnn.unit_tests.operations.ccl.test_new_all_reduce import (
@@ -180,7 +179,7 @@ def run_reduce_scatter_test(
     mesh_device.set_sub_device_stall_group(sub_device_stall_group)
 
     # create global semaphore handles
-    ccl_semaphore_handles = [create_global_semaphore(mesh_device, ccl_sub_device_crs, 0) for _ in range(num_iters)]
+    ccl_semaphore_handles = [ttnn.create_global_semaphore(mesh_device, ccl_sub_device_crs, 0) for _ in range(num_iters)]
 
     tt_out_tensor_list = []
     if trace_mode:
