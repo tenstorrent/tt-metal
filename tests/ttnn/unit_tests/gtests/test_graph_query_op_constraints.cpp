a0c60a843bdc58ee3b12795aec1b49b9d95e93e1
Raymond Kim <109366641+tt-rkim@users.noreply.github.com>
2025-04-02 08:33:43 -0400

    #0: Revert "#16147: Replace binary with binary_ng (#17160)" (#20044)
    This reverts commit bf05a247138cbb46465b20789d17d52637732113.
    
    ### Ticket
    
    This is to fix some performance pipelines, specifically a mamba
    regression.
    
    ### Problem description
    
    Mamba regressed heavily!
    
    ### What's changed
    
    Revert for now while @patrickroberts investigates
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [ ] New/Existing tests provide coverage for changes
    
    Co-authored-by: Raymond Kim <raymond.kim243@outlook.com>

--- tests/ttnn/unit_tests/gtests/test_graph_query_op_constraints.cpp
+++ tests/ttnn/unit_tests/gtests/test_graph_query_op_constraints.cpp
@@ -417,13 +417,13 @@ INSTANTIATE_TEST_SUITE_P(
             ResourceUsageMap{
                 {BoardType::N300,
                  ttnn::graph::ResourceUsage{
-                     .cb_peak_size_per_core = 3 * (2 * 2 * 32 * 32),
-                     .l1_buffers_peak_per_core = 10240,
+                     .cb_peak_size_per_core = 57344,
+                     .l1_buffers_peak_per_core = 26688,
                      .l1_output_buffer_per_core = 10240}},
                 {BoardType::E150,
                  ttnn::graph::ResourceUsage{
-                     .cb_peak_size_per_core = 3 * (2 * 2 * 32 * 32),
-                     .l1_buffers_peak_per_core = 6144,
+                     .cb_peak_size_per_core = 57344,
+                     .l1_buffers_peak_per_core = 14720,
                      .l1_output_buffer_per_core = 6144}}}),
         std::make_tuple(  // broadcast
             g_interleave_4_2_160_244_tiled,
@@ -431,13 +431,13 @@ INSTANTIATE_TEST_SUITE_P(
             ResourceUsageMap{
                 {BoardType::N300,
                  ttnn::graph::ResourceUsage{
-                     .cb_peak_size_per_core = 3 * (2 * 2 * 32 * 32),
-                     .l1_buffers_peak_per_core = 10240,
+                     .cb_peak_size_per_core = 57344,
+                     .l1_buffers_peak_per_core = 26688,
                      .l1_output_buffer_per_core = 10240}},
                 {BoardType::E150,
                  ttnn::graph::ResourceUsage{
-                     .cb_peak_size_per_core = 3 * (2 * 2 * 32 * 32),
-                     .l1_buffers_peak_per_core = 6144,
+                     .cb_peak_size_per_core = 57344,
+                     .l1_buffers_peak_per_core = 14720,
                      .l1_output_buffer_per_core = 6144}}})),
     [](const testing::TestParamInfo<std::tuple<ttnn::TensorSpec, ttnn::TensorSpec, ResourceUsageMap>>& info) {
         std::stringstream ss;
