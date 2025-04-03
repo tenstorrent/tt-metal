be6fa09d8d261c7d4327579a6a6de93aeebc6e6e
Patrick Roberts <proberts@tenstorrent.com>
2025-04-03 15:29:43 +0000

    Revert "#0: Revert "#16147: Replace binary with binary_ng (#17160)" (#20044)"
    This reverts commit a0c60a843bdc58ee3b12795aec1b49b9d95e93e1.

--- models/demos/llama3_subdevices/tt/llama_mlp.py
+++ models/demos/llama3_subdevices/tt/llama_mlp.py
@@ -150,7 +150,7 @@ class TtLlamaMLP(LightweightModule):
         ff1ff3 = ttnn.mul(
             w1_out_reduced,
             w3_out_reduced,
-            input_tensor_a_activation=ttnn.UnaryOpType.SILU,
+            input_tensor_a_activations=[ttnn.UnaryOpType.SILU],
             dtype=ttnn.bfloat8_b,
             memory_config=self.model_config["REDUCE_SCATTER_OUT_MEMCFG"],
         )
@@ -247,7 +247,7 @@ class TtLlamaMLP(LightweightModule):
         w2_in = ttnn.mul(
             w1_out_reduced,
             w3_out_reduced,
-            input_tensor_a_activation=ttnn.UnaryOpType.SILU,
+            input_tensor_a_activations=[ttnn.UnaryOpType.SILU],
             dtype=ttnn.bfloat8_b,
             memory_config=w1_out.memory_config(),
         )
