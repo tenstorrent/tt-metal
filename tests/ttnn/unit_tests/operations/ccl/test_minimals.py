de3b01bd66c491902fff219491e6dc9c85e976b6
aagarwalTT <aagarwal@tenstorrent.com>
2025-04-04 20:04:07 +0000

    Port rms all gather

--- tests/ttnn/unit_tests/operations/ccl/test_minimals.py
+++ tests/ttnn/unit_tests/operations/ccl/test_minimals.py
@@ -379,7 +379,12 @@ def test_all_gather_only(
 @pytest.mark.parametrize("trace_mode", [True])
 @pytest.mark.parametrize(
     "device_params",
-    [{"trace_region_size": 23887872}],
+    [
+        {
+            "trace_region_size": 23887872,
+            "fabric_config": ttnn.FabricConfig.FABRIC_1D,
+        }
+    ],
     indirect=True,
 )
 @pytest.mark.parametrize("mesh_device", [pytest.param((8, 4), id="8x4_grid")], indirect=True)
