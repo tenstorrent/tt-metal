0146bfc23524dae8b7ee917f9336e094b6744eda
Borys Bradel <164946524+bbradelTT@users.noreply.github.com>
2025-04-04 18:19:34 -0400

    #18485: update mm test shard for BH and skip non-working and unskip fixed tests for BH (#20206)
    ### Ticket
    Link to Github Issue #18485
    
    ### Problem description
    One test was fixed, one test had WH specific sharding, and several tests
    have not been worked on yet
    
    ### What's changed
    - unskip one fixed test
    - update sharding to be different for BH for one test
    - skip the other tests to not waste time analyzing their failures
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/14272122443 all
    ttnn tests pass. two failures in other jobs are unrelated to these tests
    and are due to infra issues
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable) N/A
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable) N/A
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable) N/A
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable) N/A
    - [x] New/Existing tests provide coverage for changes

--- tests/ttnn/unit_tests/operations/test_experimental.py
+++ tests/ttnn/unit_tests/operations/test_experimental.py
@@ -174,7 +174,7 @@ def test_ttnn_matmul_dram_sharded(device, m_size, k_size, n_size):
     # in1 shard config
     in1_shard_grid = ttnn.CoreCoord(device.dram_grid_size().x - 1, device.dram_grid_size().y - 1)
     in1_shard_grid = ttnn.CoreRangeSet({ttnn.CoreRange(ttnn.CoreCoord(0, 0), in1_shard_grid)})
-    in1_shard_shape = (8192, 96)
+    in1_shard_shape = (8192, 128) if is_blackhole() else (8192, 96)
     in1_shard_spec = ttnn.ShardSpec(in1_shard_grid, in1_shard_shape, ttnn.ShardOrientation.ROW_MAJOR)
     in1_mem_config = ttnn.MemoryConfig(ttnn.TensorMemoryLayout.WIDTH_SHARDED, ttnn.BufferType.DRAM, in1_shard_spec)
     input_tensor_in1 = ttnn.from_torch(
