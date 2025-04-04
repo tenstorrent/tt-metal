8011c02f5095be09951409c4deda7cb7fa2e2326
Sofija Jovic <148721049+s-jovic@users.noreply.github.com>
2025-04-04 11:10:11 +0200

    #0: remove GS SD implementation (#20110)
    ### Problem description
    Remove legacy SD 1.4 implementation on Grayskull since GS is no longer
    being maintained.
    
    ### What's changed
    - Removed model implementation in models/experimental.
    - Removed model CODEOWNERS.
    - Removed GS workaround in `dataflow_api.h` added for this model.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14242814115)
    - build docs failing on main as well

--- tt_metal/hw/inc/dataflow_api.h
+++ tt_metal/hw/inc/dataflow_api.h
@@ -345,12 +345,6 @@ bool cb_pages_reservable_at_back(int32_t operand, int32_t num_pages) {
     // uint16_t's here because Tensix updates the val at tiles_acked_ptr as uint16 in llk_pop_tiles
     // TODO: I think we could have TRISC update tiles_acked_ptr, and we wouldn't need uint16 here
     uint16_t pages_acked = (uint16_t)reg_read(pages_acked_ptr);
-#ifdef ARCH_GRAYSKULL
-    // The following test slows down by 5% when removing the barrier
-    // TODO(pgk) investigate GS arbiter WAR in compiler, is this fixing an issue there?
-    // models/experimental/stable_diffusion/tests/test_perf_unbatched_stable_diffusion.py::test_perf_bare_metal
-    volatile uint32_t local_mem_barrier = pages_acked;
-#endif
     uint16_t free_space_pages_wrap = get_local_cb_interface(operand).fifo_num_pages - (pages_received - pages_acked);
     return num_pages <= static_cast<int32_t>(free_space_pages_wrap);
 }
@@ -384,12 +378,6 @@ void cb_reserve_back(int32_t operand, int32_t num_pages) {
         // uint16_t's here because Tensix updates the val at tiles_acked_ptr as uint16 in llk_pop_tiles
         // TODO: I think we could have TRISC update tiles_acked_ptr, and we wouldn't need uint16 here
         uint16_t pages_acked = (uint16_t)reg_read(pages_acked_ptr);
-#ifdef ARCH_GRAYSKULL
-        // The following test slows down by 5% when removing the barrier
-        // TODO(pgk) investigate GS arbiter WAR in compiler, is this fixing an issue there?
-        // models/experimental/stable_diffusion/tests/test_perf_unbatched_stable_diffusion.py::test_perf_bare_metal
-        volatile uint32_t local_mem_barrier = pages_acked;
-#endif
         uint16_t free_space_pages_wrap =
             get_local_cb_interface(operand).fifo_num_pages - (pages_received - pages_acked);
         free_space_pages = (int32_t)free_space_pages_wrap;
