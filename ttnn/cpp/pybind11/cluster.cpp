b2634c40ecf14af37f70b7bfc863702d00c1f3dc
Scott Mountenay <ctr-smountenay@ext.tenstorrent.com>
2025-04-04 21:52:53 +0000

    Remove unused imports from cluster.cpp

--- ttnn/cpp/pybind11/cluster.cpp
+++ ttnn/cpp/pybind11/cluster.cpp
@@ -8,18 +8,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include "small_vector_caster.hpp"  // NOLINT - for pybind11 SmallVector binding support.
-#include <tt-metalium/persistent_kernel_cache.hpp>
-#include <tt-metalium/memory_reporter.hpp>
-#include <tt-metalium/device_impl.hpp>
 #include <tt-metalium/tt_metal.hpp>
-#include <tt-metalium/distributed.hpp>
-#include <tt-metalium/host_api.hpp>
-#include <tt-metalium/hal.hpp>
-#include <tt-metalium/trace.hpp>
-#include "ttnn/operations/experimental/auto_format/auto_format.hpp"
-#include <tt-metalium/hal.hpp>
-#include "tools/profiler/op_profiler.hpp"
 
 using namespace tt::tt_metal;
 
