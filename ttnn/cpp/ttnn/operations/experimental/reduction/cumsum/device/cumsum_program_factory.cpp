8c378afafa7bf88bdb491aea4b35886a2c47d8c1
Nathan Maurice <nmaurice@tenstorrent.com>
2025-04-07 10:27:20 +0000

    cumsum impl: Set up simnple RISCV operation

--- ttnn/cpp/ttnn/operations/experimental/reduction/cumsum/device/cumsum_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/experimental/reduction/cumsum/device/cumsum_program_factory.cpp
@@ -3,11 +3,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include <memory>
+#include <numeric>
+#include <random>
 #include "cumsum_device_operation.hpp"
+#include "hostdevcommon/kernel_structs.h"
 #include "tt-metalium/bfloat16.hpp"
 #include "tt-metalium/buffer.hpp"
 #include "tt-metalium/circular_buffer_types.hpp"
+#include "tt-metalium/command_queue.hpp"
 #include "tt-metalium/data_types.hpp"
+#include "tt-metalium/device.hpp"
 #include "tt-metalium/host_api.hpp"
 #include "tt-metalium/kernel_types.hpp"
 #include "tt-metalium/tt_backend_api_types.hpp"
@@ -20,55 +25,69 @@ namespace ttnn::operations::experimental::reduction {
 CumSumDeviceOperation::SingleCore::cached_program_t CumSumDeviceOperation::SingleCore::create(
     const operation_attributes_t& operation_attributes,
     const tensor_args_t& tensor_args,
-    tensor_return_value_t& tensor_return_value) {
+    tensor_return_value_t& output_tensor) {
     // Scaffold: No 'real' program for now
 
-    const auto& input_tensor = tensor_args.input_tensor;
-
     using namespace tt;
-    using namespace tt::tt_metal;
+
+    Program program;
+    const auto& input_tensor = tensor_args.input_tensor;
 
     IDevice* device = input_tensor.device();
     CommandQueue& cq = device->command_queue();
 
-    Program program;
-
     constexpr CoreCoord core{0, 0};
+    constexpr uint32_t TILE_SIZE = 1024;
 
-    const uint32_t single_tile_size = input_tensor.element_size() * 1024;
+    TT_FATAL(input_tensor.get_dtype() == output_tensor.get_dtype(), "Type conversion not supported yet");
+
+    TT_FATAL(output_tensor.get_dtype() == DataType::FLOAT32, "Only float32 data type supported for now");
+
+    TT_FATAL(output_tensor.get_layout() == Layout::TILE, "Only supported layout is TILE");
+
+    const uint32_t single_tile_size = sizeof(float) * TILE_SIZE;
+
     InterleavedBufferConfig dram_config{
         .device = device, .size = single_tile_size, .page_size = single_tile_size, .buffer_type = BufferType::DRAM};
+    InterleavedBufferConfig sram_config{
+        .device = device, .size = single_tile_size, .page_size = single_tile_size, .buffer_type = BufferType::L1};
 
-    std::shared_ptr<Buffer> src_dram_buffer = CreateBuffer(dram_config);
-    std::shared_ptr<Buffer> dst_dram_buffer = CreateBuffer(dram_config);
+    std::shared_ptr<Buffer> tmp_sram_buffer = CreateBuffer(sram_config);
 
     uint32_t src_bank_id = 0;
     uint32_t dst_bank_id = 0;
 
-    // Write input tensor data into DRAM (?)
-    // Is this needed in this function ?
-    EnqueueWriteBuffer(cq, src_dram_buffer, input_tensor.buffer(), false);
+    constexpr uint32_t cb_in_index = CBIndex::c_0;
+    constexpr uint32_t cb_out_index = CBIndex::c_1;
 
-    constexpr uint32_t src_cb_index = CBIndex::c_0;
+    DataFormat in_df = DataFormat::Float32;
+    DataFormat out_df = DataFormat::Float32;
 
-    DataFormat in_df = datatype_to_dataformat_converter(input_tensor.dtype());
-    DataFormat out_df = datatype_to_dataformat_converter(operation_attributes.dtype);
+    CircularBufferConfig cb_in_config =
+        CircularBufferConfig(single_tile_size, {{cb_in_index, in_df}}).set_page_size(cb_in_index, single_tile_size);
 
-    CircularBufferConfig cb_config =
-        CircularBufferConfig(single_tile_size, {{src_cb_index, in_df}}).set_page_size(src_cb_index, single_tile_size);
-    CBHandle cb_src = CreateCircularBuffer(program, core, cb_config);
+    CBHandle cb_in = CreateCircularBuffer(program, core, cb_in_config);
 
     KernelHandle cumsum_reader_handle_id = CreateKernel(
         program,
-        "ttnn/cpp/operations/experimental/reduction/cumsum/kernels/dataflow/cumsum_reader.cpp",
+        "ttnn/cpp/ttnn/operations/experimental/reduction/cumsum/device/kernels/dataflow/cumsum_reader.cpp",
         core,
         DataMovementConfig{.processor = DataMovementProcessor::RISCV_0, .noc = NOC::RISCV_0_default});
+
+    uint32_t num_tiles = output_tensor.volume() / TILE_SIZE;  // TODO: Not sure
 
     SetRuntimeArgs(
         program,
         cumsum_reader_handle_id,
         core,
-        {src_dram_buffer->address(), dst_dram_buffer->address(), src_bank_id, dst_bank_id});
+        {
+            input_tensor.buffer()->address(),
+            output_tensor.buffer()->address(),
+            tmp_sram_buffer->address(),
+            src_bank_id,
+            dst_bank_id,
+            num_tiles,
+        });
 
     return {std::move(program), {}};
 }
--- ttnn/cpp/ttnn/operations/experimental/reduction/cumsum/device/kernels/dataflow/cumsum_reader.cpp
+++ ttnn/cpp/ttnn/operations/experimental/reduction/cumsum/device/kernels/dataflow/cumsum_reader.cpp
@@ -13,17 +13,62 @@ void kernel_main() {
     // 2) float32 elements
     // 3) Along x-axis
 
-    constexpr uint32_t tile_size = 32;  // 'reader' does everythingß
+    DPRINT << "[Cumsum Reader] start" << ENDL();
 
-    uint32_t src_dram = get_arg_val<uint32_t>(0);
-    uint32_t dst_dram = get_arg_val<uint32_t>(1);
+    uint32_t src_dram_addr = get_arg_val<uint32_t>(0);
+    uint32_t dst_dram_addr = get_arg_val<uint32_t>(1);
+    uint32_t tmp_sram_addr = get_arg_val<uint32_t>(2);
+    uint32_t src_bank_id = get_arg_val<uint32_t>(3);
+    uint32_t dst_bank_id = get_arg_val<uint32_t>(4);
+    uint32_t num_tiles = get_arg_val<uint32_t>(5);
+    uint32_t dim = get_arg_val<uint32_t>(6);
+    uint32_t tensor_dim = get_arg_val<uint32_t>(7);
 
-    float* src = (float*)l1_addr_in;
-    float* dst = (float*)l1_addr_out;
+    // Load DRAM data into SRAM
+    uint32_t src_dram_noc_addr = get_noc_addr_from_bank_id<true>(src_bank_id, src_dram_addr);
+    uint32_t dst_dram_noc_addr = get_noc_addr_from_bank_id<true>(dst_bank_id, dst_dram_addr);
+
+    constexpr uint32_t cb_out = tt::CBIndex::c_0;
+
+    // single tile ublock
+    uint32_t ublock_size_bytes = get_tile_size(cb_out);
+
+    uint32_t l1_addr_in = get_write_ptr(cb_out);
+    uint32_t tile_card = ublock_size_bytes / sizeof(uint32_t);
 
     float sum = 0.f;
-    for (uint32_t i = 0; i < tile_size; i++) {
-        sum += src[i];
-        dst[i] = sum;
+
+    // TODO: add `dim`/`how` argument + tensor dimension
+    // 2 parameters: tile_offset, and <TODO>
+
+    // If tile-based increment => keep previous tile in memory
+    // data[i] += previous[i]
+    // previous[i] = data[i]
+
+    // Initialize accumulator buffer
+    float* tmp_accumulator = (float*)tmp_sram_addr;
+    for (uint32_t i = 0; i < tile_card; i++) {
+        tmp_accumulator[i] = 0.f;
+    }
+
+    if (dim == tensor_dim - 1) {  // x-axis
+        for (uint32_t tile = 0; tile < num_tiles; tile++) {
+            // Read DRAM => SRAM
+            noc_async_read(src_dram_noc_addr + tile * ublock_size_bytes, l1_addr_in, ublock_size_bytes);
+            noc_async_read_barrier();
+
+            float* data = (float*)l1_addr_in;
+            DPRINT_DATA0(DPRINT << "[Cumsum reader] data[0] = " << data[0] << ENDL());
+
+            for (uint32_t i = 0; i < tile_card; i++) {
+                sum += data[i];
+                data[i] = sum;
+            }
+
+            noc_async_write(l1_addr_in, dst_dram_noc_addr + tile * ublock_size_bytes, ublock_size_bytes);
+            noc_async_write_barrier();
+
+            DPRINT_DATA0(DPRINT << "[Cumsum reader] completed = " << sum << ENDL());
+        }
     }
 }
