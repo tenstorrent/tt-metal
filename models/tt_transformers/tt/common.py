f8fdd6e791e47e27e02e7dfbc5299f6cada9209a
Pratikkumar Prajapati <pprajapati@tenstorrent.com>
2025-04-07 21:38:17 +0000

    Remove nearest power of 2 padding to have padding with max len

--- models/tt_transformers/tt/common.py
+++ models/tt_transformers/tt/common.py
@@ -118,20 +118,17 @@ def preprocess_inputs_prefill(
     decoding_pos = []
     prefill_lens = []
 
-    # Always prefill the nearest power of 2 for each user. This means that the majority of cases we will prefill more tokens than needed.
+    # Pad each prompt to the maximum length among all prompts.
     # To avoid issues, we keep track of the decoding position to decode correctly the user's prompt
     for i, encoded in enumerate(encoded_prompts):
-        # Prefill size is nearest power of 2
-        prefill_seq_len = max(2 ** math.ceil(math.log(len(encoded), 2)), 128)
-
         # Initial prefill tensors full of pad tokens
-        input_tokens_prefill_i = torch.full((1, prefill_seq_len), 0, dtype=torch.int32)
+        input_tokens_prefill_i = torch.full((1, max_prompt_len), 0, dtype=torch.int32)
         input_tokens_prefill_i[0, : len(encoded[:])] = torch.tensor(encoded[:]).to(input_tokens_prefill_i)
         input_tokens_prefill.append(input_tokens_prefill_i)
 
         # Keep the correct decoding position of each user
         decoding_pos.append(len(encoded))
-        prefill_lens.append(prefill_seq_len)
+        prefill_lens.append(max_prompt_len)
 
     return (
         input_tokens_prefill,
