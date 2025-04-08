0ebd27306ada08fd4aebe417076fc833099e746d
Pratikkumar Prajapati <pprajapati@tenstorrent.com>
2025-04-08 01:10:58 +0000

    Fix rebase issues

--- tests/scripts/t3000/run_t3000_model_perf_tests.sh
+++ tests/scripts/t3000/run_t3000_model_perf_tests.sh
@@ -19,25 +19,6 @@ run_t3000_falcon7b_tests() {
   fi
 }
 
-# TODO [Deprecation notice] - Llama2-70B will be deprecated soon for the new Llama3-70B. The CI tests will be deprecated with it.
-run_t3000_llama2_70b_tests() {
-  # Record the start time
-  fail=0
-  start_time=$(date +%s)
-
-  echo "LOG_METAL: Running run_t3000_llama2_70b_tests"
-
-  env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/demos/t3000/llama2_70b/tests/test_llama_perf_decode.py -m "model_perf_t3000" --timeout=600 ; fail+=$?
-
-  # Record the end time
-  end_time=$(date +%s)
-  duration=$((end_time - start_time))
-  echo "LOG_METAL: run_t3000_llama2_70b_tests $duration seconds to complete"
-  if [[ $fail -ne 0 ]]; then
-    exit 1
-  fi
-}
-
 run_t3000_mistral7b_perf_tests() {
   # Record the start time
   fail=0
@@ -131,28 +112,6 @@ run_t3000_ccl_reduce_scatter_perf_tests() {
   if [[ $fail -ne 0 ]]; then
     exit 1
   fi
-}
-
-run_t3000_llm_tests() {
-  # Run falcon7b tests
-  run_t3000_falcon7b_tests
-
-  # Run llama2-70b tests
-  run_t3000_llama2_70b_tests
-
-  # Run falcon40b tests
-  run_t3000_falcon40b_tests
-
-  # Merge all the generated reports
-  env python3 models/perf/merge_perf_results.py
-}
-
-run_t3000_cnn_tests() {
-  # Run resnet50 tests
-  run_t3000_resnet50_tests
-
-  # Merge all the generated reports
-  env python3 models/perf/merge_perf_results.py
 }
 
 run_t3000_ccl_tests() {
