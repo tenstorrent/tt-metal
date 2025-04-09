7e4a593d3718c9658c5ab783be5cfe4def21e188
Colman Glagovich <cglagovich@tenstorrent.com>
2025-04-09 15:32:30 +0000

    HiFi4 all modulating norms

--- models/experimental/mochi/tt/dit/norms.py
+++ models/experimental/mochi/tt/dit/norms.py
@@ -2,13 +2,25 @@ import ttnn
 
 
 def residual_tanh_gated_rmsnorm(x, x_res, gate, eps=1e-6):
+    compute_kernel_config = ttnn.WormholeComputeKernelConfig(
+        math_fidelity=ttnn.MathFidelity.HiFi4,
+        math_approx_mode=False,
+        fp32_dest_acc_en=True,
+        packer_l1_acc=True,
+    )
     tanh_gate = ttnn.tanh(gate)
-    x_normed = ttnn.rms_norm(x_res, weight=tanh_gate, epsilon=eps)
+    x_normed = ttnn.rms_norm(x_res, weight=tanh_gate, epsilon=eps, compute_kernel_config=compute_kernel_config)
     output = x + x_normed
     return output
 
 
 def modulated_rmsnorm(x, scale, eps=1e-6):
+    compute_kernel_config = ttnn.WormholeComputeKernelConfig(
+        math_fidelity=ttnn.MathFidelity.HiFi4,
+        math_approx_mode=False,
+        fp32_dest_acc_en=True,
+        packer_l1_acc=True,
+    )
     weight = 1.0 + scale
-    x = ttnn.rms_norm(x, weight=weight, epsilon=eps)
+    x = ttnn.rms_norm(x, weight=weight, epsilon=eps, compute_kernel_config=compute_kernel_config)
     return x
