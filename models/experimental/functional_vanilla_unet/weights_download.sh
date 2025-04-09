9a70b7faee6cd0ad4d4379261d08ee6e20ec5920
Keerthana Rajendran <keerthana.rajendran@multicorewareinc.com>
2025-04-09 06:19:28 +0530

    #13272: Vanilla UNet bring up (#19405)
    ### Ticket
    Link to Github Issue
    https://github.com/tenstorrent/tt-metal/issues/13272
    ### Problem description
    - Add ttnn support for the Vanilla Unet model.
    
    ### What's changed
    - Added Reference pipeline for Vanilla Unet.
    - Added ttnn support for the model.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14105906158)
    CI passes
    - [x] [(Single-card) Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/14100589034/job/39496118019)
    CI passes (if applicable)
    
    ---------
    
    Co-authored-by: Sudhanshu Singhal <ssinghal@tenstorrent.com>
    Co-authored-by: Dalar Vartanians <132954887+dvartaniansTT@users.noreply.github.com>
    Co-authored-by: Dalar Vartanians <dvartanians@tenstorrent.com>

---
+++ models/experimental/functional_vanilla_unet/weights_download.sh
@@ -0,0 +1,17 @@
+# This script automatically download the model weights from Google Drive
+
+#!/bin/bash
+
+# Output filename
+OUTPUT="models/experimental/functional_vanilla_unet/unet.pt"
+
+# Create output directory if it doesn't exist
+mkdir -p "$(dirname "$OUTPUT")"
+
+# Download the file using wget
+if wget "https://github.com/mateuszbuda/brain-segmentation-pytorch/raw/master/weights/unet.pt" -O "${OUTPUT}"; then
+    echo "File downloaded successfully: ${OUTPUT}"
+else
+    echo "Error downloading the file."
+    exit 1
+fi
