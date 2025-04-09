1b75aa56bc130407229125844aa43edff812a27b
Colman Glagovich <cglagovich@tenstorrent.com>
2025-04-09 16:42:24 +0000

    use HiFi4 in tanh rmsnorm

--- models/experimental/mochi/tt/dit/norms.py
+++ models/experimental/mochi/tt/dit/norms.py
@@ -2,15 +2,15 @@ import ttnn
 
 
 def residual_tanh_gated_rmsnorm(x, x_res, gate, eps=1e-6):
-    # compute_kernel_config = ttnn.WormholeComputeKernelConfig(
-    #     math_fidelity=ttnn.MathFidelity.HiFi4,
-    #     math_approx_mode=False,
-    #     fp32_dest_acc_en=True,
-    #     packer_l1_acc=True,
-    # )
+    compute_kernel_config = ttnn.WormholeComputeKernelConfig(
+        math_fidelity=ttnn.MathFidelity.HiFi4,
+        math_approx_mode=False,
+        fp32_dest_acc_en=True,
+        packer_l1_acc=True,
+    )
     # tanh_gate = ttnn.tanh(gate)
     tanh_gate = ttnn.div(ttnn.sinh(gate), ttnn.cosh(gate))
-    x_normed = ttnn.rms_norm(x_res, weight=tanh_gate, epsilon=eps, compute_kernel_config=None)
+    x_normed = ttnn.rms_norm(x_res, weight=tanh_gate, epsilon=eps, compute_kernel_config=compute_kernel_config)
     output = x + x_normed
     return output
 
