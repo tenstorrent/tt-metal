bcf6dcc58c8e6f61a01e3587cd320b901365d418
MohammedTaherMcW <mohammed.rasheed@multicorewareinc.com>
2025-04-09 15:22:03 +0530

    Added unit test case for Gridsample

---
+++ tests/ttnn/unit_tests/operations/pool/test_gridsample.py
@@ -0,0 +1,52 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import math
+from loguru import logger
+from typing import Union, Tuple
+
+import torch
+import torch.nn as nn
+import ttnn
+from models.utility_functions import skip_for_grayskull, skip_for_blackhole, is_grayskull
+from tests.ttnn.utils_for_testing import assert_with_pcc, check_with_pcc_without_tensor_printout
+
+
+@pytest.mark.parametrize(
+    "input_shape",
+    [
+        [1, 1, 1, 3],
+        [1, 1, 32, 32],
+    ],
+)
+@pytest.mark.parametrize("align_corners", [True])
+@pytest.mark.parametrize(
+    "grid",
+    [
+        torch.tensor(
+            [
+                [0.25, 0.75],
+                [0.75, 0.25],
+                [0.5, 1.5],
+                [1.5, 0.5],
+                [0.0, 0.0],
+                [1.0, 1.0],
+            ],
+            dtype=torch.float32,
+        ).view(1, 6, 1, 2)
+    ],
+)
+def test_gridsample(device, input_shape, align_corners, grid):
+    torch.manual_seed(0)
+
+    input_tensor = torch.rand((input_shape), dtype=torch.float32)
+    torch_output = torch.nn.functional.grid_sample(input_tensor, grid, mode="bilinear", align_corners=align_corners)
+
+    input_tensor = ttnn.from_torch(input_tensor, device=device)
+    grid_tensor = ttnn.from_torch(grid, device=device)
+
+    output_tensor = ttnn.gridsample(input_tensor, grid_tensor, mode="bilinear", align_corners=align_corners)
+    output_tensor = ttnn.to_torch(output_tensor)
+    assert_with_pcc(torch_output, output_tensor, 0.99999)
