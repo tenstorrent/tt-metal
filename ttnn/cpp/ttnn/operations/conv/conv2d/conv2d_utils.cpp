408bde4f329cd0279055c2b0d0568bc27cf4073e
Sankar Manoj <smanoj@tenstorrent.com>
2025-04-10 10:08:13 +0000

    #0: to_device doesn't respect memory config

--- ttnn/cpp/ttnn/operations/conv/conv2d/conv2d_utils.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/conv2d_utils.cpp
@@ -658,6 +658,15 @@ std::tuple<ttnn::Tensor, ParallelConfig, ParallelConfig> shard_or_reshard_tensor
         } else {
             input_tensor = ttnn::to_device(
                 input_tensor, device, (auto_shard_mm ? ttnn::DRAM_MEMORY_CONFIG : input_tensor_sharded_memory_config));
+            log_debug(
+                tt::LogOp,
+                "Input Tensor Memory Config is {}, expected {}",
+                input_tensor.memory_config(),
+                (auto_shard_mm ? ttnn::DRAM_MEMORY_CONFIG : input_tensor_sharded_memory_config));
+            input_tensor = ttnn::to_memory_config(
+                input_tensor,
+                (auto_shard_mm ? ttnn::DRAM_MEMORY_CONFIG : input_tensor_sharded_memory_config),
+                std::nullopt);  // TODO: remove this line
         }
     }
     return {input_tensor, parallel_config, output_parallel_config};
