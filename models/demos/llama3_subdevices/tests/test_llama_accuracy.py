a0799d853e8c0f2a0a4b5b8d10160a7621a27069
Nikhil Soraba <nsoraba@tenstorrent.com>
2025-04-10 21:57:04 +0000

    Swap reduction axes for test_llama_model to work with ttnn.argmax

--- models/demos/llama3_subdevices/tests/test_llama_accuracy.py
+++ models/demos/llama3_subdevices/tests/test_llama_accuracy.py
@@ -277,7 +277,7 @@ def test_tt_model_acc(
                     dims=(3, 1) if model_args.is_galaxy else (1, 3),
                     mesh_shape=model_args.cluster_shape,
                 ),
-            )[0, 0, 0, :32].view(32, 1)
+            )[0, 0, :32, 0].view(32, 1)
             print("output", tokenizer.decode([ref_token]), tokenizer.decode([tt_out_tok.squeeze(1).tolist()[0]]))
             ttnn.plus_one(
                 current_pos_tensor,
