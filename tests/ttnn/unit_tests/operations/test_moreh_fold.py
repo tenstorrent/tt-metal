e6574fc0e755cef8c364201d3f8d3000e1655aaa
Borys Bradel <164946524+bbradelTT@users.noreply.github.com>
2025-04-11 17:43:38 -0400

    #18485: skip more ttnn unit tests that fail on BH (#20302)
    ### Ticket
    Link to Github Issue #18485
    
    ### Problem description
    After the past weekend's run, more tests were identified as failing, and
    one of the failures results in a hang.
    
    ### What's changed
    Skip these tests while the underlying issues are being worked on to
    avoid a hang, move the pipeline towards a green status, and reduce
    triage time.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/14318451087 and new
    one
    https://github.com/tenstorrent/tt-metal/actions/runs/14409140231/job/40413163156
    has ttnn tests passing
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable) N/A
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable) N/A
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable) N/A
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable) N/A
    - [ ] New/Existing tests provide coverage for changes N/A

--- tests/ttnn/unit_tests/operations/test_moreh_fold.py
+++ tests/ttnn/unit_tests/operations/test_moreh_fold.py
@@ -7,7 +7,7 @@ import torch
 import ttnn
 
 from loguru import logger
-from models.utility_functions import comp_allclose_and_pcc
+from models.utility_functions import comp_allclose_and_pcc, skip_for_blackhole
 
 
 def run_fold_test(device, input_shape, output_size, kernel_size, dilation, padding, stride, dtype):
@@ -33,6 +33,7 @@ def run_fold_test(device, input_shape, output_size, kernel_size, dilation, paddi
     assert passing
 
 
+@skip_for_blackhole("Fails on BH. Issue #20577")
 @pytest.mark.parametrize(
     "input_shape,output_size,kernel_size,dilation,padding,stride",
     [
