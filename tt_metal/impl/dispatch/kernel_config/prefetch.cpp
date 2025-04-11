tenstorrent/tt-metal

commit 7d3d24a3d984cbca6a31091782563d82216f9fbd

Add outbound_eth_chan compile arg to dispatch kernel (#20312)
### Ticket
#18726

### Problem description
Needed to Initialize 2D push fabric

### What's changed
This value is needed to initialize 2D Push fabric properly according to
the test cases
Renamed chip_id args to dev_id
Added a comment in RTOptions about the `TT_METAL_FD_FABRIC` flag

### Checklist
APC
https://github.com/tenstorrent/tt-metal/actions/runs/14343617731
https://github.com/tenstorrent/tt-metal/actions/runs/14387060269
TG
https://github.com/tenstorrent/tt-metal/actions/runs/14343620211
https://github.com/tenstorrent/tt-metal/actions/runs/14343621412
T3K
https://github.com/tenstorrent/tt-metal/actions/runs/14343624404
https://github.com/tenstorrent/tt-metal/actions/runs/14387060302
Single Card
https://github.com/tenstorrent/tt-metal/actions/runs/14387060659
FD Unit Tests
https://github.com/tenstorrent/tt-metal/actions/runs/14395758505

tt_metal/impl/dispatch/kernel_config/prefetch.cpp
@@ -385,15 +385,16 @@ void PrefetchKernel::CreateKernel() {
         static_config_.dispatch_s_buffer_size.value(),
         static_config_.dispatch_s_cb_log_page_size.value(),
         dependent_config_.downstream_mesh_id.value_or(0),
-        dependent_config_.downstream_chip_id.value_or(0),
+        dependent_config_.downstream_dev_id.value_or(0),
         dependent_config_.upstream_mesh_id.value_or(0),
-        dependent_config_.upstream_chip_id.value_or(0),
+        dependent_config_.upstream_dev_id.value_or(0),
         dependent_config_.fabric_router_noc_xy.value_or(0),
+        dependent_config_.outbound_eth_chan.value_or(0),
         static_config_.client_interface_addr.value_or(0),
         static_config_.is_d_variant.value(),
         static_config_.is_h_variant.value(),
     };
-    TT_ASSERT(compile_args.size() == 34);
+    TT_ASSERT(compile_args.size() == 36);
     auto my_virtual_core = device_->virtual_core_from_logical_core(logical_core_, GetCoreType());
     auto upstream_virtual_core =
         device_->virtual_core_from_logical_core(dependent_config_.upstream_logical_core.value(), GetCoreType());
@@ -473,16 +474,18 @@ void PrefetchKernel::ConfigureCore() {
 
 void PrefetchKernel::UpdateArgsForFabric(
     const CoreCoord& fabric_router_virtual,
+    uint32_t outbound_eth_chan,
     tt::tt_fabric::mesh_id_t upstream_mesh_id,
-    chip_id_t upstream_chip_id,
+    chip_id_t upstream_dev_id,
     tt::tt_fabric::mesh_id_t downstream_mesh_id,
-    chip_id_t downstream_chip_id) {
+    chip_id_t downstream_dev_id) {
     dependent_config_.fabric_router_noc_xy =
         tt::tt_metal::hal_ref.noc_xy_encoding(fabric_router_virtual.x, fabric_router_virtual.y);
     dependent_config_.upstream_mesh_id = upstream_mesh_id;
-    dependent_config_.upstream_chip_id = upstream_chip_id;
+    dependent_config_.upstream_dev_id = upstream_dev_id;
     dependent_config_.downstream_mesh_id = downstream_mesh_id;
-    dependent_config_.downstream_chip_id = downstream_chip_id;
+    dependent_config_.downstream_dev_id = downstream_dev_id;
+    dependent_config_.outbound_eth_chan = outbound_eth_chan;
 
     auto& my_dispatch_constants = DispatchMemMap::get(GetCoreType());
     static_config_.client_interface_addr =

tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/test_dispatcher.cpp
@@ -600,6 +600,8 @@ int main(int argc, char** argv) {
             0,
             0,
             0,
+            0,
+            0,
             true,  // is_dram_variant
             true,  // is_host_variant
         };

tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/test_prefetcher.cpp
@@ -1903,6 +1903,7 @@ void configure_for_single_chip(
         0,
         0,
         0,
+        0,
     };
 
     constexpr NOC my_noc_index = NOC::NOC_0;
@@ -2185,6 +2186,7 @@ void configure_for_single_chip(
         host_completion_queue_wr_ptr,
         dev_completion_queue_wr_ptr,
         dev_completion_queue_rd_ptr,
+        0,
         0,
         0,
         0,

tt_metal/impl/dispatch/kernel_config/dispatch.cpp
@@ -27,6 +27,7 @@
 #include "mux.hpp"
 #include "prefetch.hpp"
 #include "impl/context/metal_context.hpp"
+#include "rtoptions.hpp"
 #include <umd/device/types/xy_pair.h>
 #include "utils.hpp"
 
@@ -160,7 +161,11 @@ void DispatchKernel::GenerateStaticConfigs() {
             my_dispatch_constants.mux_buffer_pages(device_->num_hw_cqs()),
             GetCoreType());  // Apparently unused
 
-        static_config_.split_dispatch_page_preamble_size = sizeof(tt::packet_queue::dispatch_packet_header_t);
+        if (tt::llrt::RunTimeOptions::get_instance().get_fd_fabric()) {
+            static_config_.split_dispatch_page_preamble_size = 0;
+        } else {
+            static_config_.split_dispatch_page_preamble_size = sizeof(tt::packet_queue::dispatch_packet_header_t);
+        }
         static_config_.prefetch_h_max_credits = my_dispatch_constants.mux_buffer_pages(device_->num_hw_cqs());
 
         static_config_.packed_write_max_unicast_sub_cmds =
@@ -378,10 +383,11 @@ void DispatchKernel::CreateKernel() {
         static_config_.dev_completion_q_rd_ptr.value(),
 
         dependent_config_.downstream_mesh_id.value_or(0),
-        dependent_config_.downstream_chip_id.value_or(0),
+        dependent_config_.downstream_dev_id.value_or(0),
         dependent_config_.upstream_mesh_id.value_or(0),
-        dependent_config_.upstream_chip_id.value_or(0),
+        dependent_config_.upstream_dev_id.value_or(0),
         dependent_config_.fabric_router_noc_xy.value_or(0),
+        dependent_config_.outbound_eth_chan.value_or(0),
         static_config_.client_interface_addr.value_or(0),
 
         static_config_.first_stream_used.value(),
@@ -389,7 +395,7 @@ void DispatchKernel::CreateKernel() {
         static_config_.is_d_variant.value(),
         static_config_.is_h_variant.value(),
     };
-    TT_ASSERT(compile_args.size() == 38);
+    TT_ASSERT(compile_args.size() == 39);
     auto my_virtual_core = device_->virtual_core_from_logical_core(logical_core_, GetCoreType());
     auto upstream_virtual_core =
         device_->virtual_core_from_logical_core(dependent_config_.upstream_logical_core.value(), GetCoreType());
@@ -444,16 +450,18 @@ void DispatchKernel::ConfigureCore() {
 
 void DispatchKernel::UpdateArgsForFabric(
     const CoreCoord& fabric_router_virtual,
+    uint32_t outbound_eth_chan,
     tt::tt_fabric::mesh_id_t upstream_mesh_id,
-    chip_id_t upstream_chip_id,
+    chip_id_t upstream_dev_id,
     tt::tt_fabric::mesh_id_t downstream_mesh_id,
-    chip_id_t downstream_chip_id) {
+    chip_id_t downstream_dev_id) {
     dependent_config_.fabric_router_noc_xy =
         tt::tt_metal::hal_ref.noc_xy_encoding(fabric_router_virtual.x, fabric_router_virtual.y);
     dependent_config_.upstream_mesh_id = upstream_mesh_id;
-    dependent_config_.upstream_chip_id = upstream_chip_id;
+    dependent_config_.upstream_dev_id = upstream_dev_id;
     dependent_config_.downstream_mesh_id = downstream_mesh_id;
-    dependent_config_.downstream_chip_id = downstream_chip_id;
+    dependent_config_.downstream_dev_id = downstream_dev_id;
+    dependent_config_.outbound_eth_chan = outbound_eth_chan;
     auto& my_dispatch_constants = DispatchMemMap::get(GetCoreType());
     static_config_.client_interface_addr =
         my_dispatch_constants.get_device_command_queue_addr(CommandQueueDeviceAddrType::FABRIC_INTERFACE);

tt_metal/impl/dispatch/kernel_config/dispatch.hpp
@@ -73,9 +73,10 @@ typedef struct dispatch_dependent_config {
     // Populated if fabric is being used to talk to downstream
     std::optional<uint32_t> fabric_router_noc_xy;
     std::optional<uint32_t> upstream_mesh_id;
-    std::optional<uint32_t> upstream_chip_id;
+    std::optional<uint32_t> upstream_dev_id;
     std::optional<uint32_t> downstream_mesh_id;
-    std::optional<uint32_t> downstream_chip_id;
+    std::optional<uint32_t> downstream_dev_id;
+    std::optional<uint32_t> outbound_eth_chan;
 } dispatch_dependent_config_t;
 
 class DispatchKernel : public FDKernel {
@@ -121,6 +122,7 @@ public:
 
     void UpdateArgsForFabric(
         const CoreCoord& fabric_router,
+        uint32_t outbound_eth_chan,
         tt::tt_fabric::mesh_id_t src_mesh_id,
         chip_id_t src_chip_id,
         tt::tt_fabric::mesh_id_t dst_mesh_id,

tt_metal/impl/dispatch/kernel_config/fabric_router_vc.cpp
@@ -73,9 +73,17 @@ void FabricRouterVC::GenerateDependentConfigs() {
 
         TT_FATAL(valid_path, "FabricRouterVC is not implemented for this path");
 
+        // Get outbound ethernet channels
+        auto us_outbound_eth_channels = cluster.get_fabric_ethernet_channels(src_chip_id);
+        auto ds_outbound_eth_channels = cluster.get_fabric_ethernet_channels(dst_chip_id);
+        TT_FATAL(!us_outbound_eth_channels.empty(), "No outbound ethernet channels for upstream kernel");
+        TT_FATAL(!ds_outbound_eth_channels.empty(), "No outbound ethernet channels for downstream kernel");
+
         // Downstream path. src -> dst
-        us_kernel->UpdateArgsForFabric(fabric_router, src_mesh_id, src_chip_id, dst_mesh_id, dst_chip_id);
-        ds_kernel->UpdateArgsForFabric(fabric_router_rev, src_mesh_id, src_chip_id, dst_mesh_id, dst_chip_id);
+        us_kernel->UpdateArgsForFabric(
+            fabric_router, *us_outbound_eth_channels.begin(), src_mesh_id, src_chip_id, dst_mesh_id, dst_chip_id);
+        ds_kernel->UpdateArgsForFabric(
+            fabric_router_rev, *ds_outbound_eth_channels.begin(), src_mesh_id, src_chip_id, dst_mesh_id, dst_chip_id);
     }
 }
 

tt_metal/impl/dispatch/kernel_config/fd_kernel.cpp
@@ -18,12 +18,15 @@
 #include "dprint_server.hpp"
 #include "eth_router.hpp"
 #include "eth_tunneler.hpp"
+#include "fabric_types.hpp"
 #include "hal.hpp"
 #include "hal_types.hpp"
 #include "kernel_types.hpp"
 #include "mux.hpp"
 #include "prefetch.hpp"
 #include "rtoptions.hpp"
+#include "impl/context/metal_context.hpp"
+#include "tt_cluster.hpp"
 #include <umd/device/tt_core_coordinates.h>
 
 using namespace tt::tt_metal;
@@ -135,8 +138,12 @@ void FDKernel::configure_kernel_variant(
     if (force_watcher_no_inline) {
         defines.insert({"WATCHER_NOINLINE", std::to_string(force_watcher_no_inline)});
     }
-    if (tt::llrt::RunTimeOptions::get_instance().watcher_dispatch_disabled()) {
+    auto& rt_options = tt::llrt::RunTimeOptions::get_instance();
+    if (rt_options.watcher_dispatch_disabled()) {
         defines["FORCE_WATCHER_OFF"] = "1";
+    }
+    if (tt::tt_metal::MetalContext::instance().get_cluster().get_fabric_config() != FabricConfig::FABRIC_2D_PUSH) {
+        defines["FVC_MODE_PULL"] = "1";
     }
     if (!DPrintServerReadsDispatchCores(device_->id())) {
         defines["FORCE_DPRINT_OFF"] = "1";

tt_metal/impl/dispatch/kernel_config/fd_kernel.hpp
@@ -93,6 +93,7 @@ public:
     // an intermediary FDKernel for indicating a fabric router path needs to be found.
     virtual void UpdateArgsForFabric(
         const CoreCoord& fabric_router_virtual,
+        uint32_t outbound_eth_chan,
         tt::tt_fabric::mesh_id_t upstream_mesh_id,
         chip_id_t upstream_chip_id,
         tt::tt_fabric::mesh_id_t downstream_mesh_id,

commit 3a7dcbf715d70197342f7fab5ff4285ac1240e5e

Fix compile args count assert (#20565)
### Ticket
Link to Github Issue

### Problem description
Runs fail due to an incorrect assert introduced by
https://github.com/tenstorrent/tt-metal/pull/20312

### What's changed
Describe the approach used to solve the problem.
Summarize the changes made and its impact.

### Checklist
- [x] [All post
commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
CI passes
- [ ] [Blackhole Post
commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
CI passes (if applicable)
- [ ] [Model
regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
CI passes (if applicable)
- [ ] [Device performance
regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
CI passes (if applicable)
- [ ] **(For models and ops writers)** Full [new models
tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
CI passes (if applicable)
- [ ] New/Existing tests provide coverage for changes

tt_metal/impl/dispatch/kernel_config/prefetch.cpp
@@ -394,7 +394,7 @@ void PrefetchKernel::CreateKernel() {
         static_config_.is_d_variant.value(),
         static_config_.is_h_variant.value(),
     };
-    TT_ASSERT(compile_args.size() == 36);
+    TT_ASSERT(compile_args.size() == 35);
     auto my_virtual_core = device_->virtual_core_from_logical_core(logical_core_, GetCoreType());
     auto upstream_virtual_core =
         device_->virtual_core_from_logical_core(dependent_config_.upstream_logical_core.value(), GetCoreType());