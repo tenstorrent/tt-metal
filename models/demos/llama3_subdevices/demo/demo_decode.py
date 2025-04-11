3387bfa10d716db24df488ca44881eabb25b2528
Ammar Vora <avora@tenstorrent.com>
2025-04-11 07:18:09 -0400

    Adjusting Llama TG targets to match main/CI (#20534)
    ### Problem description
    Optimizations have been added to main, so targets need to be updated.
    Regarding perf unit tests, targets were previously set on a faster
    machine, so they need to be updated to what's being seen in CI.
    
    ### What's changed
    Update targets on:
    - demo
    - ring matmul
    - all gathers
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes

--- models/demos/llama3_subdevices/demo/demo_decode.py
+++ models/demos/llama3_subdevices/demo/demo_decode.py
@@ -32,7 +32,7 @@ from models.demos.llama3_subdevices.tt.model_config import LlamaOptimizations
 TSU_PERF_DROP_LIMIT_COUNT = 5
 
 # Constants for TSU thresholds based on the number of layers
-TSU_THRESHOLDS = {1: {"min": 340, "max": 355}, 10: {"min": 180, "max": 200}, 80: {"min": 39, "max": 41}}
+TSU_THRESHOLDS = {1: {"min": 355, "max": 375}, 10: {"min": 180, "max": 200}, 80: {"min": 40, "max": 44}}
 
 
 def load_and_cache_context(context_url, cache_dir, max_length=None):
--- models/demos/llama3_subdevices/tests/tg_perf_unit_tests/test_ring_matmul_perf_TG_llama.py
+++ models/demos/llama3_subdevices/tests/tg_perf_unit_tests/test_ring_matmul_perf_TG_llama.py
@@ -14,11 +14,11 @@ THRESHOLD = 1.0
 @pytest.mark.parametrize(
     "mm_type, perf_target_us",
     [
-        ("qkv", 8.9),
+        ("qkv", 9.2),
         ("do", 7.3),
-        ("ff13", 8.9),
-        ("ff2", 13.8),
-        ("lm_head", 340),
+        ("ff13", 9.9),
+        ("ff2", 14.8),
+        ("lm_head", 380),
     ],
 )
 @pytest.mark.models_device_performance_bare_metal
