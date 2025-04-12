5cba8885d96bdab6abc55645a3633a85154006d6
Bryan Wilder Field Lozano <blozano@tenstorrent.com>
2025-04-11 20:55:40 -0700

    Add support for using sfpi from a system path (#20545)
    ### Problem description
    The current scheme of using TT_METAL_HOME to find the sfpi compiler
    causes some difficulty in shipping a package.
    Today, we have to bundle the sfpi compiler along with ttnn wheel.
    The wheel then becomes too large to distribute on pypi, as it becomes
    ~180MB.
    The wheel has some "tricks" to automatically set TT_METAL_HOME and
    symlink to site-packages.
    All this gets easier if we have an installer for sfpi, to install it to
    a known system path.
    
    ### What's changed
    If the sfpi compiler does not exist in `$TT_METAL_HOME/runtime/`
    fallback to `/opt/tenstorrent/`.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14393337744)
    CI passes
    - [x] [All post commit after
    refactor](https://github.com/tenstorrent/tt-metal/actions/runs/14413097598)
    CI passes
    - [x] New/Existing tests provide coverage for changes

--- tt_metal/jit_build/build.cpp
+++ tt_metal/jit_build/build.cpp
@@ -6,6 +6,7 @@
 
 #include <taskflow/core/async.hpp>
 #include <algorithm>
+#include <array>
 #include <atomic>
 #include <cstdio>
 #include <cstdlib>
@@ -126,6 +127,7 @@ void JitBuildEnv::init(
     this->out_firmware_root_ = this->out_root_ + to_string(build_key) + "/firmware/";
     this->out_kernel_root_ = this->out_root_ + to_string(build_key) + "/kernels/";
 
+    // Tools
     const static bool use_ccache = std::getenv("TT_METAL_CCACHE_KERNEL_SUPPORT") != nullptr;
     if (use_ccache) {
         this->gpp_ = "ccache ";
@@ -133,8 +135,28 @@ void JitBuildEnv::init(
         this->gpp_ = "";
     }
 
-    // Tools
-    this->gpp_ += this->root_ + "runtime/sfpi/compiler/bin/riscv32-unknown-elf-g++ ";
+    // Use local sfpi for development
+    // Use system sfpi for production to avoid packaging it
+    // Ordered by precedence
+    const std::array<std::string, 2> sfpi_roots = {
+        this->root_ + "runtime/sfpi",
+        "/opt/tenstorrent/sfpi"
+    };
+
+    bool sfpi_found = false;
+    for (unsigned i = 0; i < 2; ++i) {
+        auto gxx = sfpi_roots[i] + "/compiler/bin/riscv32-unknown-elf-g++";
+        if (std::filesystem::exists(gxx)) {
+            this->gpp_ += gxx + " ";
+            this->gpp_include_dir_ = sfpi_roots[i] + "/include";
+            log_debug(tt::LogBuildKernels, "Using {} sfpi at {}", i ? "system" : "local", sfpi_roots[i]);
+            sfpi_found = true;
+            break;
+        }
+    }
+    if (!sfpi_found) {
+        TT_THROW("sfpi not found at {} or {}", sfpi_roots[0], sfpi_roots[1]);
+    }
 
     // Flags
     string common_flags;
@@ -425,7 +447,7 @@ JitBuildCompute::JitBuildCompute(const JitBuildEnv& env, const JitBuiltStateConf
         "-I" + env_.root_ + "tt_metal/hw/ckernels/" + env.arch_name_ + "/metal/llk_io " +
         "-I" + env_.root_ + "tt_metal/hw/ckernels/" + env.arch_name_ + "/metal/llk_api " +
         "-I" + env_.root_ + "tt_metal/hw/ckernels/" + env.arch_name_ + "/metal/llk_api/llk_sfpu " +
-        "-I" + env_.root_ + "runtime/sfpi/include " +
+        "-I" + env_.gpp_include_dir_ + " " +
         "-I" + env_.root_ + "tt_metal/hw/firmware/src " +
         "-I" + env_.root_ + "tt_metal/third_party/tt_llk/tt_llk_" + env.arch_name_ + "/llk_lib ";
     // clang-format on
