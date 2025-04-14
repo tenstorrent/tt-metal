91b2fac3912f29a4331f55205a6d4b69684e627f
Stefan Krsmanovic <skrsmanovic@tenstorrent.com>
2025-04-14 09:21:18 +0000

    #0: Update untilize/tilize perf testing infrastructure
    - Update test name
    - Run perf test for all targets(op, op without dm, unpack and pack)

--- tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_tilize_perf.py
+++ tests/tt_eager/python_api_testing/sweep_tests/pytests/tt_dnn/test_tilize_perf.py
@@ -112,7 +112,7 @@ def test_tilize_test(input_shapes, tilize_args, device, function_level_defaults)
     if perf_scope == "block":
         os.environ["TT_LLK_PERF_BLOCK"] = "1"
 
-    for perf in ["op", "op_no_dm", "unpack", "math", "pack"]:
+    for perf in ["op", "op_no_dm", "unpack", "pack"]:
         # Set log csv file name, file will be used to store perf data
         ENVS = dict(os.environ)
         TT_METAL_HOME = Path(ENVS["TT_METAL_HOME"])
