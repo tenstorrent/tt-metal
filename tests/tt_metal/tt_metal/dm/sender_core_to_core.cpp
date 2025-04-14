000d1d435b50432c178b264eec418adc215c2576
Ata Tuzuner <atuzuner@tenstorrent.com>
2025-04-14 16:34:01 +0000

    Initial working one to one data movement test

--- tests/tt_metal/tt_metal/dm/sender_core_to_core.cpp
+++ tests/tt_metal/tt_metal/dm/sender_core_to_core.cpp
@@ -9,20 +9,18 @@
 void kernel_main() {
     constexpr uint32_t src_addr = get_compile_time_arg_val(0);
     constexpr uint32_t dst_addr = get_compile_time_arg_val(1);
-    // constexpr uint32_t num_of_transactions = get_compile_time_arg_val(1);
-    constexpr uint32_t transaction_num_pages = get_compile_time_arg_val(2);
-    constexpr uint32_t page_size_bytes = get_compile_time_arg_val(3);
-    // constexpr uint32_t cb_id_out0 = get_compile_time_arg_val(4);
-    // constexpr uint32_t test_id = get_compile_time_arg_val(5);
+    constexpr uint32_t num_of_transactions = get_compile_time_arg_val(2);
+    constexpr uint32_t transaction_num_pages = get_compile_time_arg_val(3);
+    constexpr uint32_t page_size_bytes = get_compile_time_arg_val(4);
+    constexpr uint32_t test_id = get_compile_time_arg_val(5);
 
-    auto sem = get_semaphore(get_arg_val<uint32_t>(0));
-    DPRINT << "Get sem result: " << sem << ENDL();
+    uint32_t semaphore = get_semaphore(get_arg_val<uint32_t>(0));
+    uint32_t receiver_x_coord = get_arg_val<uint32_t>(1);
+    uint32_t receiver_y_coord = get_arg_val<uint32_t>(2);
 
-    // auto sem_ptr = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(get_semaphore(get_arg_val<uint32_t>(0)));
-    uint64_t sem_addr = get_noc_addr(2, 3, sem);
+    uint64_t sem_addr = get_noc_addr(receiver_x_coord, receiver_y_coord, semaphore);
 
     constexpr uint32_t transaction_size_bytes = transaction_num_pages * page_size_bytes;
-    // constexpr uint32_t total_num_pages = num_of_transactions * transaction_num_pages;
 
     // DeviceTimestampedData("Number of transactions", num_of_transactions);
     // DeviceTimestampedData("Transaction size in bytes", transaction_size_bytes);
@@ -30,14 +28,10 @@ void kernel_main() {
 
     {
         // DeviceZoneScopedN("SENDER");
-        uint64_t dst_noc_addr = get_noc_addr(1, 1, dst_addr);  // TODO: Sending to right location?
-        // uint32_t l1_read_addr = get_read_ptr(cb_id_out0);
+        uint64_t dst_noc_addr = get_noc_addr(receiver_x_coord, receiver_y_coord, dst_addr);
 
         noc_async_write(src_addr, dst_noc_addr, transaction_size_bytes);
         noc_async_write_barrier();
-
-        DPRINT << "Sender before sem call" << ENDL();
         noc_semaphore_inc(sem_addr, 1);
-        DPRINT << "Sender after sem call" << ENDL();
     }
 }
