693e1587b57d32620270a7e2ea40cb9233b05a88
Jason Davies <jason@jasondavies.com>
2025-04-14 13:27:10 +0100

    Move llk_unpack_tilizeA_B_* from tt-metal to tt-llk. (#103)
    ### Ticket
    
    tenstorrent/tt-metal#20409
    
    ### Problem description
    
    Allows these APIs to be tested with LLK test infra.
    
    ### What's changed
    
    Moved functions from tt-metal
    (tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_unpack_tilize_api.h)
    to tt-llk.
    
    Will need to coordinate removal on tt-metal side.
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)

--- tt_llk_wormhole_b0/llk_lib/llk_unpack_tilize.h
+++ tt_llk_wormhole_b0/llk_lib/llk_unpack_tilize.h
@@ -92,7 +92,7 @@ inline void _llk_unpack_tilize_(
     const std::uint32_t num_faces   = 4,
     const bool narrow_tile          = false)
 {
-    volatile uint tt_reg_ptr *cfg = get_cfg_pointer(); // get pointer to registers for current state ID
+    volatile uint tt_reg_ptr* cfg = get_cfg_pointer(); // get pointer to registers for current state ID
 
     std::uint32_t top_face_offset_address = SCALE_DATUM_SIZE(unpack_src_format, tile_index) << (narrow_tile ? 0 : 1);
     // Each iteration unpacks 2 face_r_dimx16 faces (1st 0,1 2nd 2,3 unless tile is <=16x32)
@@ -145,4 +145,199 @@ inline void _llk_unpack_tilize_(
 #ifdef PERF_DUMP
     first_unpack_recorded = true;
 #endif
+}
+
+/*************************************************************************
+ * LLK UNPACK TILIZE SRC A, UNPACK SRC B
+ *************************************************************************/
+
+template <bool neginf_srcA = false, std::uint32_t reload_srcB = false, bool zero_srcA = false, bool zero_srcA_reduce = false>
+inline void _llk_unpack_tilizeA_B_mop_config_(const bool narrow_tile = false, const std::uint32_t num_faces = 4)
+{
+    static constexpr uint unpack_srca =
+        TT_OP_UNPACR(SrcA, (zero_srcA ? 0b010001 : 0b1), 0, 0, 0, 1, (zero_srcA ? 0 : 1), p_unpacr::RAREFYB_DISABLE, 0, 0, 0, 0, 1);
+    static constexpr uint unpack_srcb = TT_OP_UNPACR(
+        SrcB,
+        (zero_srcA ? 0b010001 : (reload_srcB ? 0b0 : 0b1)),
+        0,
+        0,
+        0,
+        1,
+        (zero_srcA ? 0 : 1),
+        p_unpacr::RAREFYB_DISABLE,
+        0,
+        0,
+        0,
+        0,
+        1);                                                                                         // Skip face ptr inc if same face is reloaded into srcB
+    static constexpr uint unpack_neginf_srca = TT_OP_UNPACR_NOP(SrcA, p_unpacr_nop::UNP_NEGINFSRC); // Needed for max pool
+    static constexpr uint unpack_zero_srca   = TT_OP_UNPACR_NOP(SrcA, p_unpacr_nop::UNP_ZEROSRC);   // Needed for dot product
+    static constexpr uint unpack_srcb_2_face = TT_OP_UNPACR(SrcB, 0b100010, 0, 0, 0, 1, 0, p_unpacr::RAREFYB_DISABLE, 0, 0, 0, 0, 1); // Needed for dot product
+    static constexpr uint unpack_srca_dat_valid = TT_OP_UNPACR(SrcA, 0b1, 0, 0, 0, 1, 1, p_unpacr::RAREFYB_DISABLE, 0, 0, 0, 0, 1);   // Needed for dot product
+    static constexpr uint unpack_srcb_dat_valid =
+        TT_OP_UNPACR(SrcB, (reload_srcB ? 0b0 : 0b1), 0, 0, 0, 1, 1, p_unpacr::RAREFYB_DISABLE, 0, 0, 0, 0, 1); // Needed for dot
+                                                                                                                // product
+
+    const uint32_t innerloop = zero_srcA ? (num_faces > 2 ? 2 : (num_faces - 1)) : 1;
+    const uint32_t outerloop = zero_srcA ? 1 : (num_faces > 2) ? num_faces / 2 : num_faces;
+    ckernel_template tmp(outerloop, innerloop, unpack_srca, ((zero_srcA && num_faces == 2) ? unpack_srcb_2_face : unpack_srcb));
+    if constexpr (neginf_srcA)
+    {
+        tmp.set_start_op(unpack_neginf_srca);
+    }
+    else if constexpr (zero_srcA_reduce)
+    {
+        tmp.set_start_op(unpack_zero_srca);
+    }
+    else if constexpr (zero_srcA)
+    {
+        if (num_faces < 4)
+        {
+            tmp.set_start_op(unpack_zero_srca);
+            tmp.set_end_ops(unpack_srca_dat_valid, unpack_srcb_dat_valid);
+        }
+    }
+    tmp.program(instrn_buffer);
+}
+
+template <bool neginf_srcA = false, std::uint32_t reload_srcB = false, bool zero_srcA = false, bool zero_srcA_reduce = false>
+inline void _llk_unpack_tilizeA_B_init_(
+    const std::uint32_t unpack_src_format,
+    const std::uint32_t unpack_dst_format,
+    const bool narrow_tile,
+    const std::uint32_t ct_dim,
+    const std::uint32_t num_faces       = 4,
+    const std::uint32_t unpA_face_r_dim = FACE_R_DIM,
+    const std::uint32_t unpB_face_r_dim = FACE_R_DIM)
+{
+    cfg_reg_rmw_tensix<THCON_SEC0_REG2_Haloize_mode_RMW>(0);
+
+    const std::uint32_t block_c_dim = ct_dim * ((narrow_tile || (num_faces == 1)) ? FACE_C_DIM : TILE_C_DIM);
+
+    // Set face dim
+    TT_SETADCXX(p_setadc::UNP_A, unpA_face_r_dim * FACE_C_DIM - 1, 0x0);
+    TT_SETADCXX(p_setadc::UNP_B, unpB_face_r_dim * FACE_C_DIM - 1, 0x0);
+
+    // Override default settings to enable tilize mode
+    unpack_config_u config   = {0};
+    config.f.out_data_format = unpack_dst_format;
+    config.f.throttle_mode   = 2;
+    config.f.tileize_mode    = 1;
+    config.f.shift_amount    = (SCALE_DATUM_SIZE(unpack_src_format, block_c_dim)) >> 4;
+
+    TT_SETDMAREG(0, LOWER_HALFWORD(config.val[0]), 0, LO_16(p_gpr_unpack::TMP0));
+    TT_SETDMAREG(0, UPPER_HALFWORD(config.val[0]), 0, HI_16(p_gpr_unpack::TMP0));
+    TTI_REG2FLOP(1, 0, 0, 0, THCON_SEC0_REG2_Out_data_format_ADDR32 + 0 - THCON_CFGREG_BASE_ADDR32,
+                 p_gpr_unpack::TMP0); // Load unpack config[0]
+    TTI_REG2FLOP(
+        1,
+        0,
+        0,
+        0,
+        THCON_SEC0_REG5_Tile_x_dim_cntx0_ADDR32 - THCON_CFGREG_BASE_ADDR32,
+        p_gpr_unpack::FACE_DIM_1x16); // GPR preloaded with  16 | (16 << 16)
+
+    _llk_unpack_tilizeA_B_mop_config_<neginf_srcA, reload_srcB, zero_srcA, zero_srcA_reduce>(narrow_tile, num_faces);
+}
+
+template <bool zero_srcA = false>
+inline void _llk_unpack_tilizeA_B_(
+    std::uint32_t unpA_src_format,
+    std::uint32_t face_r_dim,
+    std::uint32_t narrow_tile,
+    std::uint32_t base_address_a,
+    std::uint32_t address_b,
+    std::uint32_t tile_index_a,
+    std::uint32_t tile_index_b,
+    std::uint32_t block_ct_dim,
+    std::uint32_t num_faces = 4)
+{
+    std::uint32_t top_face_offset_address = SCALE_DATUM_SIZE(unpA_src_format, tile_index_a) << (narrow_tile ? 0 : 1);
+
+    // Each iteration unpacks 2 face_r_dimx16 faces (1st 0,1 2nd 2,3 unless tile is <=16x32)
+    // For narrow tile we unpack 1 face in each iteration
+    // Offset address is in 16B words
+    // Datum count = tile_index*face_r_dim (/16 to get word count)
+
+    const std::uint32_t block_c_dim_16B   = block_ct_dim * ((narrow_tile || (num_faces == 1)) ? FACE_C_DIM / 16 : TILE_C_DIM / 16);
+    std::uint32_t bot_face_offset_address = SCALE_DATUM_SIZE(unpA_src_format, face_r_dim * block_c_dim_16B); //*N rows / 16 to get 16B word aligned address
+
+    // Program srcA and srcB base addresses
+    std::uint32_t num_loops = narrow_tile ? 2 : ((num_faces > 1) ? num_faces / 2 : 1);
+
+    // Clear z/w start counters for SrcB
+    TTI_SETADCZW(UNP1, 0, 0, 0, 0, 0b1111);
+
+    // Program srcA and srcB base addresses
+    volatile uint tt_reg_ptr* cfg = get_cfg_pointer(); // get pointer to registers for current state ID
+
+    for (std::uint32_t n = 0; n < num_loops; n++)
+    {
+        std::uint32_t address_a = base_address_a + top_face_offset_address + ((n == 1) ? bot_face_offset_address : 0);
+
+        // Clear z/w start counters
+        if constexpr (zero_srcA)
+        {
+            if (num_faces == 4 && n == 1)
+            {
+                TTI_SETADCZW(UNP0, 0, 0, 0, 0, 0b1011);
+            }
+            else
+            {
+                TTI_SETADCZW(UNP0, 0, 0, 0, 0, 0b1111);
+            }
+        }
+        else
+        {
+            TTI_SETADCZW(UNP0, 0, 0, 0, 0, 0b1111);
+        }
+
+        // Wait for free context
+        wait_for_next_context(2);
+
+        // Get tile address
+        if (0 == unp_cfg_context)
+        {
+            cfg[THCON_SEC0_REG3_Base_address_ADDR32] = address_a;
+            cfg[THCON_SEC1_REG3_Base_address_ADDR32] = address_b;
+        }
+        else
+        {
+            cfg[THCON_SEC0_REG3_Base_cntx1_address_ADDR32] = address_a;
+            cfg[THCON_SEC1_REG3_Base_cntx1_address_ADDR32] = address_b;
+        }
+
+        // Trisc::SEMPOST for context acquire
+        semaphore_post(semaphore::UNPACK_SYNC);
+
+        // Stall unpacker until pending CFG writes from Trisc have completed
+        TTI_STALLWAIT(p_stall::STALL_UNPACK, p_stall::TRISC_CFG);
+
+        // Run MOP
+        if constexpr (zero_srcA)
+        {
+            if (num_faces == 4 && n == 0)
+            {
+                TTI_UNPACR_NOP(SrcA, p_unpacr_nop::UNP_ZEROSRC);
+            }
+
+            ckernel::ckernel_template::run(instrn_buffer);
+
+            if (num_faces == 4 && n != 0)
+            {
+                TTI_UNPACR_NOP(SrcA, p_unpacr_nop::UNP_SET_DVALID);
+                TTI_UNPACR_NOP(SrcB, p_unpacr_nop::UNP_SET_DVALID);
+            }
+        }
+        else
+        {
+            ckernel::ckernel_template::run(instrn_buffer);
+        }
+
+        // T6::SEMGET for context release
+        t6_semaphore_get(semaphore::UNPACK_SYNC);
+
+        // Switch unpacker config context
+        switch_config_context(unp_cfg_context);
+    }
 }
