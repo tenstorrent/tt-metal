655a458188679d3580db582ea0c44bca78240690
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-04-15 23:20:13 +0000

    #0: sfpi v6.10.0

--- tt_metal/hw/cmake/sfpi-version.cmake
+++ tt_metal/hw/cmake/sfpi-version.cmake
@@ -1,6 +1,10 @@
 # Define the SFPI version to use
 
 set(SFPI_x86_64_Linux_RELEASE
-    "v6.9.0/sfpi-x86_64-Linux.tgz"
-    "9bb1e0ef4287b9bbf71c205984842873"
+    "v6.10.0/sfpi-x86_64-Linux.tgz"
+    "e6ad407354e66c27a7a7b81d4f7e1118"
+)
+set(SFPI_aarch64_Linux_RELEASE
+    "v6.10.0/sfpi-aarch64-Linux.tgz"
+    "e1fda029ca82bd449f715a5e6da2828a"
 )
