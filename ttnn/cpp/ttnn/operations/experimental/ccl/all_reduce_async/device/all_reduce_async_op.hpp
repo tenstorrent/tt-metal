e1d5a5b7ce2b9b032034fc5725877bdfa834db62
asaigal <asaigal@tenstorrent.com>
2025-04-16 15:14:30 -0500

    Reland CCL Port to Device Init Based 1D Fabric

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_reduce_async/device/all_reduce_async_op.hpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_reduce_async/device/all_reduce_async_op.hpp
@@ -36,7 +36,6 @@ struct AllReduceAsync {
     const ccl::Topology topology;
     const GlobalSemaphore semaphore;
     std::optional<tt::tt_metal::SubDeviceId> sub_device_id;
-    bool enable_persistent_fabric_mode;
 
     AllReduceAsync(
         std::optional<IDevice*> forward_device,
@@ -48,8 +47,7 @@ struct AllReduceAsync {
         MemoryConfig output_mem_config,
         ccl::Topology topology,
         GlobalSemaphore semaphore,
-        std::optional<tt::tt_metal::SubDeviceId>& sub_device_id,
-        bool enable_persistent_fabric_mode) :
+        std::optional<tt::tt_metal::SubDeviceId>& sub_device_id) :
         forward_device(forward_device),
         backward_device(backward_device),
         num_links(num_links),
@@ -59,8 +57,7 @@ struct AllReduceAsync {
         output_mem_config(output_mem_config),
         topology(topology),
         semaphore(semaphore),
-        sub_device_id(sub_device_id),
-        enable_persistent_fabric_mode(enable_persistent_fabric_mode) {}
+        sub_device_id(sub_device_id) {}
 
     // Add attributes method for reflection
     auto attributes() const {
@@ -95,8 +92,7 @@ AllReduceAsync create_all_reduce_async_struct(
     const std::vector<IDevice*>& devices,
     const ccl::Topology topology,
     const std::vector<GlobalSemaphore>& semaphores,
-    std::optional<tt::tt_metal::SubDeviceId> sub_device_id,
-    bool enable_persistent_fabric_mode);
+    std::optional<tt::tt_metal::SubDeviceId> sub_device_id);
 
 }  // namespace all_reduce_async_detail
 }  // namespace ccl
@@ -116,8 +112,7 @@ tt::tt_metal::operation::ProgramWithCallbacks all_reduce_async_minimal_multi_cor
     const uint32_t ring_index,
     ccl::Topology topology,
     const GlobalSemaphore& semaphore,
-    const std::optional<tt::tt_metal::SubDeviceId>& sub_device_id,
-    bool enable_persistent_fabric_mode);
+    const std::optional<tt::tt_metal::SubDeviceId>& sub_device_id);
 
 namespace operations {
 namespace experimental {
@@ -133,8 +128,7 @@ Tensor all_reduce_async(
     const std::optional<const DataType> dtype = std::nullopt,
     const std::optional<MemoryConfig>& memory_config = std::nullopt,
     const std::optional<size_t> num_preferred_links = std::nullopt,
-    std::optional<tt::tt_metal::SubDeviceId> sub_device_id = std::nullopt,
-    bool enable_persistent_fabric_mode = false);
+    std::optional<tt::tt_metal::SubDeviceId> sub_device_id = std::nullopt);
 
 }  // namespace ccl
 }  // namespace experimental
