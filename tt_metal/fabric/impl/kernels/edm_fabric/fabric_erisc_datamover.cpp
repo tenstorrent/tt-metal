d8d0f1d6e2c16fa80e5171e80a8681ee1ecbe33f
Austin Ho <109362939+tt-aho@users.noreply.github.com>
2025-04-16 12:33:06 -0400

    1D Fabric Ring Optimizations (#20709)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/20151
    
    ### Problem description
    Optimize ring perf.
    
    ### What's changed
    Add a `ubench_quick_tests` marker to select fabric ubench tests to
    enable easily running a smaller subset of important tests. This enables
    faster iteration when testing changes for 1D fabric since the full test
    suite now takes over 20 minutes.
    
    Remove preemptive eth busy check in run_sender_channel_step, and instead
    check/spin in send_next_data. This has a slightly negative perf affect
    on its own, but coupled with the free slots change enables perf
    improvement almost entirely across the board.
    
    Refactor OutboundReceiverChannelPointers to track remote buffer index
    and number of free slots.
    
    Below is a summary of the quick test results of baseline, preemptive
    check, and free slots + preemptive check changes.
    
    <img width="1710" alt="image"
    src="https://github.com/user-attachments/assets/a7a5ac90-d631-485c-9a61-d40b80493fab"
    />
    
    
    When running the full sweep, all tests had better perf apart from the
    linear non-forwarding tests. This saw an ~0.07B/c drop. However, overall
    we gain up to ~0.25B/c in the best case.
    
    Summary stats of the full sweep:
    Min: -0.0713127862373
    Max: 0.25790263773487
    Avg: 0.0484651893262067
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/14480652132
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [x] New/Existing tests provide coverage for changes
    
    ubench:
    https://github.com/tenstorrent/tt-metal/actions/runs/14495829494/job/40663774156

--- tt_metal/fabric/impl/kernels/edm_fabric/fabric_erisc_datamover.cpp
+++ tt_metal/fabric/impl/kernels/edm_fabric/fabric_erisc_datamover.cpp
@@ -290,23 +290,10 @@ FORCE_INLINE void init_ptr_val(int32_t val) {
  */
 template <uint8_t RECEIVER_NUM_BUFFERS>
 struct OutboundReceiverChannelPointers {
-    static constexpr bool is_pow2 = is_power_of_2(RECEIVER_NUM_BUFFERS);
+    BufferIndex remote_receiver_buffer_index{0};
+    uint8_t num_free_slots = RECEIVER_NUM_BUFFERS;
 
-    tt::tt_fabric::ChannelBufferPointer<RECEIVER_NUM_BUFFERS> wrptr;
-    tt::tt_fabric::ChannelBufferPointer<RECEIVER_NUM_BUFFERS> ack_ptr;
-    tt::tt_fabric::ChannelBufferPointer<RECEIVER_NUM_BUFFERS> completion_ptr;
-
-    FORCE_INLINE bool has_space_for_packet() const {
-        return completion_ptr.distance_behind(wrptr) < RECEIVER_NUM_BUFFERS;
-    }
-
-    FORCE_INLINE bool has_unacknowledged_eth_packets() const { return ack_ptr.get_ptr() != wrptr.get_ptr(); }
-
-    FORCE_INLINE bool has_incomplete_eth_packets() const { return completion_ptr.get_ptr() != wrptr.get_ptr(); }
-
-    FORCE_INLINE bool has_unacknowledged_or_incomplete_eth_packets() const {
-        return has_incomplete_eth_packets() || has_unacknowledged_eth_packets();
-    }
+    FORCE_INLINE bool has_space_for_packet() const { return num_free_slots; }
 };
 
 /*
@@ -366,11 +353,10 @@ FORCE_INLINE void send_next_data(
     OutboundReceiverChannelPointers<RECEIVER_NUM_BUFFERS>& outbound_to_receiver_channel_pointers,
     tt::tt_fabric::EthChannelBuffer<RECEIVER_NUM_BUFFERS>& receiver_buffer_channel,
     uint8_t sender_channel_index) {
-    auto& remote_receiver_wrptr = outbound_to_receiver_channel_pointers.wrptr;
+    auto& remote_receiver_buffer_index = outbound_to_receiver_channel_pointers.remote_receiver_buffer_index;
+    auto& remote_receiver_num_free_slots = outbound_to_receiver_channel_pointers.num_free_slots;
     auto& local_sender_wrptr = sender_worker_interface.local_wrptr;
     auto local_sender_wrptr_buffer_index = local_sender_wrptr.get_buffer_index();
-
-    ASSERT(!internal_::eth_txq_is_busy(DEFAULT_ETH_TXQ));
 
     // TODO: TUNING - experiment with only conditionally breaking the transfer up into multiple packets if we are
     //       a certain threshold less than full packet
@@ -384,19 +370,26 @@ FORCE_INLINE void send_next_data(
     size_t payload_size_bytes = pkt_header->get_payload_size_including_header();
     pkt_header->src_ch_id = sender_channel_index;
 
-    auto src_addr = sender_buffer_channel.get_buffer_address(local_sender_wrptr_buffer_index);
-    auto dest_addr = receiver_buffer_channel.get_buffer_address(remote_receiver_wrptr.get_buffer_index());
+    auto src_addr = (uint32_t)pkt_header;
+    auto dest_addr = receiver_buffer_channel.get_buffer_address(remote_receiver_buffer_index);
+    while (internal_::eth_txq_is_busy(DEFAULT_ETH_TXQ)) {
+    };
     internal_::eth_send_packet_bytes_unsafe(DEFAULT_ETH_TXQ, src_addr, dest_addr, payload_size_bytes);
 
     // Note: We can only advance to the next buffer index if we have fully completed the send (both the payload and sync
     // messages)
     local_sender_wrptr.increment();
+
+    // TODO: Put in fn
+    remote_receiver_buffer_index =
+        BufferIndex{wrap_increment<RECEIVER_NUM_BUFFERS>(remote_receiver_buffer_index.get())};
+    remote_receiver_num_free_slots--;
+
     // update the remote reg
     static constexpr uint32_t words_to_forward = 1;
     while (internal_::eth_txq_is_busy(DEFAULT_ETH_TXQ)) {
     };
     remote_update_ptr_val<to_receiver_pkts_sent_id>(words_to_forward);
-    remote_receiver_wrptr.increment();
 }
 
 /////////////////////////////////////////////
@@ -573,8 +566,7 @@ void run_sender_channel_step(
     // TODO: update to be stream reg based. Initialize to space available and simply check for non-zero
     bool receiver_has_space_for_packet = outbound_to_receiver_channel_pointers.has_space_for_packet();
     bool has_unsent_packet = local_sender_channel_worker_interface.has_unsent_payload();
-    bool eth_txq_not_busy = !internal_::eth_txq_is_busy(DEFAULT_ETH_TXQ);
-    bool can_send = receiver_has_space_for_packet && has_unsent_packet && eth_txq_not_busy;
+    bool can_send = receiver_has_space_for_packet && has_unsent_packet;
     if constexpr (enable_first_level_ack) {
         bool sender_backpressured_from_sender_side =
             !(local_sender_channel_worker_interface.local_rdptr.distance_behind(
@@ -601,7 +593,7 @@ void run_sender_channel_step(
     int32_t completions_since_last_check = get_ptr_val(to_sender_packets_completed_streams[sender_channel_index]);
     if (completions_since_last_check) {
         auto& sender_rdptr = local_sender_channel_worker_interface.local_rdptr;
-        outbound_to_receiver_channel_pointers.completion_ptr.increment_n(completions_since_last_check);
+        outbound_to_receiver_channel_pointers.num_free_slots += completions_since_last_check;
         sender_rdptr.increment_n(completions_since_last_check);
         increment_local_update_ptr_val(
             to_sender_packets_completed_streams[sender_channel_index], -completions_since_last_check);
