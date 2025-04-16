871b7ce33642a6447e3a27da86f03594454bcdd5
Marko Radosavljevic <mradosavljevic@tenstorrent.com>
2025-04-16 08:29:08 +0000

    Add stability script

---
+++ models/demos/wormhole/resnet50/tests/test_resnet50_stability.py
@@ -0,0 +1,62 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import ttnn
+from models.utility_functions import (
+    run_for_wormhole_b0,
+)
+import time
+from loguru import logger
+from models.demos.wormhole.resnet50.demo.demo import test_demo_trace_with_imagenet
+
+test_demo_trace_with_imagenet.__test__ = False
+
+TEST_DURATION_SECONDS = 60 * 24 * 24
+
+
+@run_for_wormhole_b0()
+@pytest.mark.parametrize(
+    "device_params", [{"l1_small_size": 24576, "trace_region_size": 1605632, "num_command_queues": 2}], indirect=True
+)
+@pytest.mark.parametrize(
+    "batch_size, iterations, act_dtype, weight_dtype",
+    ((16, 100, ttnn.bfloat8_b, ttnn.bfloat8_b),),
+)
+@pytest.mark.parametrize("mesh_device", [1], indirect=True)
+def test_resnet_stability(
+    mesh_device,
+    use_program_cache,
+    batch_size,
+    iterations,
+    imagenet_label_dict,
+    act_dtype,
+    weight_dtype,
+    model_location_generator,
+    enable_async_mode=True,
+):
+    logger.info(f"Running ResNet stability test for {TEST_DURATION_SECONDS} seconds")
+
+    start = time.time()
+    iter = 0
+
+    while True:
+        iter += 1
+
+        test_demo_trace_with_imagenet(
+            mesh_device,
+            use_program_cache,
+            batch_size,
+            iterations,
+            imagenet_label_dict,
+            act_dtype,
+            weight_dtype,
+            model_location_generator,
+            enable_async_mode=True,
+        )
+
+        if time.time() - start > TEST_DURATION_SECONDS:
+            break
+
+    logger.info(f"ResNet stability test completed after {iter} iterations and {time.time() - start} seconds")
