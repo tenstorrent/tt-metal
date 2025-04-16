6d57fb539d7836e279a89f9a6f03ffe0da19e269
mgajewskiTT <mgajewski@tenstorrent.com>
2025-04-16 07:37:52 +0000

    19891 - PR comments fix.

--- ttnn/cpp/ttnn/operations/experimental/reduction/sort/device/kernels/compute/sort.cpp
+++ ttnn/cpp/ttnn/operations/experimental/reduction/sort/device/kernels/compute/sort.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -17,7 +17,37 @@
 // this can only be removed once that's fixed
 int32_t topk_replay_init = 0;
 namespace NAMESPACE {
+/*
+The sorting algorithm is based on Bitonic Merge Sort.
 
+ The program operates on the arrangement of input data in the form of tiles. After the data passes through
+ preprocessing, the dimension by which the data is to be sorted is the last dimension in the tensor, hence from the
+ point of view of the arrangement of tiles, the sorting is practically based on sorting successive rows of the matrix.
+
+ First, the program reads one full row of tiles (size Wt) from DRAM to L1 and generates a corresponding set of tiles
+ containing the data indices in the initial arrangement. The basis for the described sorting implementation is
+ ckernel::topk_local_sort LLK, which takes as input arguments to the DST register two tiles on which
+ the sorting is to be performed and two tiles containing the indices of this data. LLK sorts the two input tiles in the
+ inplace method and sets the indices of the data that have been changed in place. However, it performs sorting on
+ columns, hence the additional step of performing transposition. Since LLK always takes a set of two tiles, their number
+ in the Wt dimension must always be a multiple of 64 (2 * Tile_Width (32)).
+
+ sort_Wt_tiles_row_to_bitonic_sequence takes pairs of tiles and sorts them among themselves changing the sorting order
+ (for the first pair according to the desired order, for the next one vice versa, etc.), as a result we get a set of
+ sorted pairs of tiles with a variable sorting order.
+
+ The next step is to sort the tiles among themselves, so that the entire row of input data is sorted. As in the
+ bitonic merge sort algorithm, this is done in stages – the next indexes of tiles in CB are calculated, which are to be
+ sorted among themselves. Finally, all tiles have their values ​​sorted with respect to one dimension and are
+ ready to be transposed to the desired dimension and written to DRAM memory.
+
+ Example:
+ The input tensor is a 32x128 matrix, which translates to 1x4 tiles: T0 T1 T2 T3, we sort the values ​​ascending.
+ Pairwise sorting: Tiles T0 and T1 are sorted as a pair ascending, T2 and T3 are sorted as a pair descending.
+ Sorting among themselves: Stage 1 - we sort T0 and T2 ascending and T1 and T3 ascending (we do not change the order),
+                           Stage 2 – We sort T0 and T1 ascending and T2 and T3 ascending.
+ Data saving: Values ​​have been sorted by a common dimension and are ready to be saved.
+ */
 void MAIN {
     // Compile time args
     constexpr uint32_t input_tensor_cb_index = get_compile_time_arg_val(0);
@@ -52,9 +82,9 @@ void MAIN {
             input_tensor_transposed_cb_index,
             index_tensor_transposed_cb_index,
             Wt,
-            true,
+            /*switch_dir=*/true,
             ascending,
-            5);
+            /*end_phase(log2(K))=*/5);
 
         // Wait for bitonic sequence of Wt tiles
         cb_wait_front(input_tensor_transposed_cb_index, Wt);
@@ -62,7 +92,7 @@ void MAIN {
 
         // Sort and merge step of bitonic merge sort
         uint32_t stages = 0;
-        for (uint32_t temp = Wt; temp > 1; temp >>= 1) {
+        for (uint32_t i = Wt; i > 1; i >>= 1) {
             stages++;
         }
 
