0cd7d9d4cafafb47b1bf4fbd9c3cfd3cf464790e
Sofija Jovic <148721049+s-jovic@users.noreply.github.com>
2025-04-16 18:30:44 +0200

    #17138: SD: Reuse conv weights and bias (#20752)
    ### Ticket
    #17138
    
    ### Problem description
    We always pass in host weights to conv2d in VAE upsample, so conv2d
    always needs to preprocess them.
    
    ### What's changed
    - Add ConvBlock class that handles weights and bias caching.
    - Update VAE upsample to use ConvBlock.
    - Fix a bug in split conv - check `conv_out_channel_split_factor`
    instead of `conv_in_channel_split_factor`
    - Deallocate all midresults in split conv
    - Renamed test and symlinks pointing to it
    
    ### Checklist
    [WH] Frequent model and ttnn:
    https://github.com/tenstorrent/tt-metal/actions/runs/14494284549
    [BH] Post commit:
    https://github.com/tenstorrent/tt-metal/actions/runs/14494287604

--- models/demos/wormhole/stable_diffusion/tt/vae/ttnn_vae_upsample.py
+++ models/demos/wormhole/stable_diffusion/tt/vae/ttnn_vae_upsample.py
@@ -3,12 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import ttnn
-from models.demos.wormhole.stable_diffusion.tt.vae.ttnn_vae_utils import (
-    get_default_compute_config,
-    get_default_conv_config,
-    prepare_split_conv_weights_bias,
-    split_conv_and_run,
-)
+from models.demos.wormhole.stable_diffusion.tt.vae.ttnn_conv_block import ConvBlock
 
 
 class UpsampleBlock:
@@ -17,6 +12,8 @@ class UpsampleBlock:
         torch_upsample,
         device,
         in_channels,
+        input_height,
+        input_width,
         out_channels,
         output_height,
         output_width,
@@ -26,24 +23,19 @@ class UpsampleBlock:
     ):
         self.device = device
         self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.output_height = output_height
-        self.output_width = output_width
-        self.conv_in_channel_split_factor = conv_in_channel_split_factor
-        self.conv_out_channel_split_factor = conv_out_channel_split_factor
+        self.input_height = input_height
+        self.input_width = input_width
         self.scale_factor = scale_factor
-        self.return_weights_and_bias = True
 
-        self.compute_config = get_default_compute_config(device)
-        self.conv_config = get_default_conv_config()
-
-        self.conv_weights, self.conv_bias = prepare_split_conv_weights_bias(
+        self.conv = ConvBlock(
+            torch_upsample.conv,
+            device,
             in_channels,
+            output_height,
+            output_width,
             out_channels,
             conv_in_channel_split_factor,
             conv_out_channel_split_factor,
-            torch_upsample.conv.weight,
-            torch_upsample.conv.bias.unsqueeze(0).unsqueeze(0).unsqueeze(0),
         )
 
     def __call__(self, hidden_states):
@@ -53,30 +45,6 @@ class UpsampleBlock:
 
         hidden_states = ttnn.upsample(hidden_states, self.scale_factor)
 
-        conv_result = split_conv_and_run(
-            hidden_states,
-            self.conv_weights,
-            self.conv_bias,
-            self.device,
-            self.in_channels,
-            self.output_height,
-            self.output_width,
-            self.out_channels,
-            self.conv_in_channel_split_factor,
-            self.conv_out_channel_split_factor,
-            self.compute_config,
-            self.conv_config,
-            return_weights_and_bias=self.return_weights_and_bias,
-        )
+        hidden_states = self.conv(hidden_states)
 
-        if self.return_weights_and_bias:
-            # In the first pass, we pass in weights on host,
-            # so we want to keep the weights on device and reuse them
-            hidden_states, self.conv_weights, self.conv_bias = conv_result
-            self.return_weights_and_bias = False
-        else:
-            hidden_states = conv_result
-
-        hidden_states = ttnn.to_memory_config(hidden_states, ttnn.DRAM_MEMORY_CONFIG)
-        hidden_states = ttnn.reshape(hidden_states, [1, self.output_height, self.output_width, self.out_channels])
         return hidden_states
