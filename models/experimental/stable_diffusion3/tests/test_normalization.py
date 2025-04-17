3fb3bb420c5ddd20016932fb7be8b8d4d822226c
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2025-04-17 18:31:45 +0200

    work around distibuted layer norm requiring beta and gamma tensors

--- models/experimental/stable_diffusion3/tests/test_normalization.py
+++ models/experimental/stable_diffusion3/tests/test_normalization.py
@@ -16,32 +16,51 @@ from ..tt.utils import assert_quality
 @pytest.mark.parametrize(
     "input_shape",
     [
-        [2, 24, 4096, 64],
+        [4096, 3072],
     ],
 )
+@pytest.mark.parametrize("mesh_device", [(1, 1), (1, 2), (2, 2)], indirect=True)
+@pytest.mark.parametrize("affine", [True, False], ids=["affine", "noaffine"])
 @pytest.mark.usefixtures("use_program_cache")
 def test_layer_norm(
     *,
-    device: ttnn.Device,
+    mesh_device: ttnn.MeshDevice,
     input_shape: list[int],
+    affine: bool,
 ) -> None:
-    dtype = torch.bfloat16
+    batch_size, _ = mesh_device.shape
+    input_shape = [batch_size, *input_shape]
 
-    torch_model = torch.nn.LayerNorm(input_shape[-1:], eps=1.0).to(dtype=dtype)
+    torch.manual_seed(0)
 
-    parameters = TtLayerNormParameters.from_torch(torch_model.state_dict(), device=device, dtype=ttnn.bfloat8_b)
+    torch_model = torch.nn.LayerNorm(input_shape[-1:], eps=1.0, elementwise_affine=affine)
+
+    parameters = TtLayerNormParameters.from_torch(
+        torch_model.state_dict(),
+        device=mesh_device,
+        dtype=ttnn.bfloat8_b,
+        weight_shape=input_shape[-1:],
+    )
     tt_model = TtLayerNorm(parameters, eps=torch_model.eps)
 
-    torch_input_tensor = torch.randn(input_shape, dtype=dtype)
+    torch_input_tensor = torch.randn(input_shape)
 
-    tt_input_tensor = ttnn.from_torch(torch_input_tensor, device=device, layout=ttnn.TILE_LAYOUT)
+    tt_input_tensor = ttnn.from_torch(
+        torch_input_tensor,
+        device=mesh_device,
+        layout=ttnn.TILE_LAYOUT,
+        dtype=ttnn.bfloat8_b,
+        mesh_mapper=ttnn.ShardTensor2dMesh(mesh_device, tuple(mesh_device.shape), (0, -1)),
+    )
 
     with torch.no_grad():
         torch_output = torch_model(torch_input_tensor)
 
     tt_output = tt_model(tt_input_tensor)
 
-    assert_quality(torch_output, tt_output, pcc=0.999_950)
+    composer = ttnn.ConcatMesh2dToTensor(mesh_device, tuple(mesh_device.shape), (0, -1))
+    tt_output_torch = ttnn.to_torch(tt_output, mesh_composer=composer)
+    assert_quality(torch_output, tt_output_torch, pcc=0.99990)
 
 
 @pytest.mark.parametrize(
@@ -50,26 +69,34 @@ def test_layer_norm(
         [2, 24, 4096, 64],
     ],
 )
+@pytest.mark.parametrize("mesh_device", [(1, 1), (1, 2)], indirect=True)
 @pytest.mark.usefixtures("use_program_cache")
 def test_rms_norm(
     *,
-    device: ttnn.Device,
+    mesh_device: ttnn.MeshDevice,
     input_shape: list[int],
 ) -> None:
-    dtype = torch.bfloat16
+    torch.manual_seed(0)
 
-    torch_model = RmsNorm(dim=input_shape[-1], eps=1.0).to(dtype=dtype)
+    torch_model = RmsNorm(dim=input_shape[-1], eps=1.0)
     torch.nn.init.normal_(torch_model.weight)
 
-    parameters = TtRmsNormParameters.from_torch(torch_model.state_dict(), device=device)
+    parameters = TtRmsNormParameters.from_torch(torch_model.state_dict(), device=mesh_device, dtype=ttnn.bfloat8_b)
+
     tt_model = TtRmsNorm(parameters, eps=torch_model.eps)
 
-    torch_input_tensor = torch.randn(input_shape, dtype=dtype)
+    torch_input_tensor = torch.randn(input_shape)
 
-    tt_input_tensor = ttnn.from_torch(torch_input_tensor, device=device, layout=ttnn.TILE_LAYOUT)
+    tt_input_tensor = ttnn.from_torch(
+        torch_input_tensor, device=mesh_device, layout=ttnn.TILE_LAYOUT, dtype=ttnn.bfloat8_b
+    )
 
     torch_output = torch_model(torch_input_tensor)
 
     tt_output = tt_model(tt_input_tensor)
+    tt_output_torch = ttnn.to_torch(
+        tt_output,
+        mesh_composer=ttnn.ConcatMeshToTensor(mesh_device, dim=0),
+    )[: input_shape[0]]
 
-    assert_quality(torch_output, tt_output, pcc=0.999_950)
+    assert_quality(torch_output, tt_output_torch, pcc=0.99985)
