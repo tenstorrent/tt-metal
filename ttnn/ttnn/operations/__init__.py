9b9e5285e170ea43ce232fbfbe225b91155f9efd
blozano-tt <blozano@tenstorrent.com>
2025-04-17 20:27:44 +0000

    Fix thing?

--- ttnn/ttnn/operations/__init__.py
+++ ttnn/ttnn/operations/__init__.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import pkgutil
+import sys
 from importlib.util import module_from_spec
 
 __all__ = []
@@ -11,5 +12,7 @@ for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
     __all__.append(module_name)
     spec = loader.find_spec(module_name)
     _module = module_from_spec(spec)
+    # Register the module in sys.modules before executing it
+    sys.modules[f"{__package__}.{module_name}"] = _module
     spec.loader.exec_module(_module)
     globals()[module_name] = _module
