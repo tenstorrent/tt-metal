cfc1fcf9a42294a2a5cab01eac1ef6392f91181d
mcw-anasuya <mcw-anair@ext.tenstorrent.com>
2025-04-18 15:29:41 +0000

    #0: Binary uint16 support

--- tests/ttnn/unit_tests/operations/eltwise/test_binary_bcast.py
+++ tests/ttnn/unit_tests/operations/eltwise/test_binary_bcast.py
@@ -8,11 +8,13 @@ import ttnn
 
 from tests.ttnn.unit_tests.operations.eltwise.backward.utility_funcs import (
     compare_pcc,
+    compare_equal,
 )
 from models.utility_functions import skip_for_grayskull, torch_random
 from itertools import product as parameters
 from functools import partial
 from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_func_with_cast_tt
+from tests.ttnn.utils_for_testing import assert_with_pcc
 
 
 binary_fns = {
@@ -1895,3 +1897,97 @@ def test_binary_sharded_row_major_layout(
     out_tt_sharded = ttnn.add(a_tt, b_tt, memory_config=a_sharded_config, use_legacy=False)
     out_tt_sharded = ttnn.to_torch(out_tt_sharded)
     torch.testing.assert_close(out_tt_sharded, out_pt)
+
+
+@pytest.mark.parametrize(
+    "input_shape_a, input_shape_b",
+    [
+        (torch.Size([1, 2, 32]), torch.Size([1, 2, 32])),
+        (torch.Size([1]), torch.Size([1, 5, 12])),
+        (torch.Size([1, 2, 32, 64, 125]), torch.Size([1, 2, 32, 1, 1])),
+        (torch.Size([]), torch.Size([])),
+        (torch.Size([5]), torch.Size([1])),
+    ],
+)
+@pytest.mark.parametrize(
+    "low_a, high_a, low_b, high_b",
+    [
+        (0, 100, 0, 300),
+        (1000, 10000, 500, 1000),
+    ],
+)
+def test_binary_add_uint16_bcast(input_shape_a, input_shape_b, low_a, high_a, low_b, high_b, device):
+    num_elements = max(int(torch.prod(torch.tensor(input_shape_a)).item()), 1)
+    torch_input_a = torch.linspace(high_a, low_a, num_elements, dtype=torch.int16)
+    torch_input_a = torch_input_a[:num_elements].reshape(input_shape_a).nan_to_num(0.0)
+
+    num_elements = max(int(torch.prod(torch.tensor(input_shape_b)).item()), 1)
+    torch_input_b = torch.linspace(high_b, low_b, num_elements, dtype=torch.int16)
+    torch_input_b = torch_input_b[:num_elements].reshape(input_shape_b).nan_to_num(0.0)
+
+    golden_function = ttnn.get_golden_function(ttnn.add)
+    golden = golden_function(torch_input_a, torch_input_b, device=device)
+
+    tt_in_a = ttnn.from_torch(
+        torch_input_a,
+        dtype=ttnn.uint16,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    tt_in_b = ttnn.from_torch(
+        torch_input_b,
+        dtype=ttnn.uint16,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+    tt_result = ttnn.minimum(tt_in_a, tt_in_b, use_legacy=False)
+    result = ttnn.to_torch(tt_result)
+    assert_with_pcc(golden, result, 0.999)
+
+
+@pytest.mark.parametrize(
+    "input_shapes",
+    ((torch.Size([1, 1, 32, 32])),),
+)
+@pytest.mark.parametrize(
+    "input_a_val, input_b_val",
+    [
+        (756, 100),
+        (32768, 65535),
+        (11, 1),
+    ],
+)
+def test_binary_add_fill_val_uint16(input_shapes, input_a_val, input_b_val, device):
+    torch_input_a = torch.ones(input_shapes, dtype=torch.int16) * input_a_val
+    torch_input_b = torch.ones(input_shapes, dtype=torch.int16) * input_b_val
+
+    golden_function = ttnn.get_golden_function(ttnn.add)
+    golden = golden_function(torch_input_a, torch_input_b, device=device)
+
+    tt_in_a = ttnn.from_torch(
+        torch_input_a,
+        dtype=ttnn.uint16,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    tt_in_b = ttnn.from_torch(
+        torch_input_b,
+        dtype=ttnn.uint16,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    tt_result = ttnn.minimum(tt_in_a, tt_in_b, use_legacy=False)
+    result = ttnn.to_torch(tt_result)
+    torch.set_printoptions(threshold=10000)
+    print(golden)
+    print(result)
+
+    comp_pass = compare_equal([tt_result], [golden])
+    assert comp_pass
--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_binary_max_min.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_binary_max_min.h
@@ -18,18 +18,22 @@ inline void calculate_binary_max_min(const uint dst_offset) {
     for (int d = 0; d < ITERATIONS; d++) {
         constexpr uint dst_tile_size = 64;
 
-        TTI_SFPLOAD(p_sfpu::LREG0, 0, ADDR_MOD_3, 0);                          // a
-        TT_SFPLOAD(p_sfpu::LREG1, 0, ADDR_MOD_3, dst_offset * dst_tile_size);  // b
+        TTI_SFPLOAD(p_sfpu::LREG0, 8, ADDR_MOD_3, 0);                          // a
+        TT_SFPLOAD(p_sfpu::LREG1, 8, ADDR_MOD_3, dst_offset * dst_tile_size);  // b
+        TTI_SFPIADD(0, p_sfpu::LREG1, p_sfpu::LREG0, 8);
+        TTI_SFPSTORE(p_sfpu::LREG0, 8, ADDR_MOD_3, 0);
 
         // Swap and store maximum in lreg1, minimum in lreg0
-        TTI_SFPSWAP(0, p_sfpu::LREG1, p_sfpu::LREG0, 1);
-        TTI_SFPNOP;
+        // TTI_SFPSWAP(0, p_sfpu::LREG1, p_sfpu::LREG0, 8);
+        // TTI_SFPNOP;
+        // TTI_SFPSTORE(p_sfpu::LREG1, 8, ADDR_MOD_3, 0);
 
-        if constexpr (IS_MAX_OP) {
-            TTI_SFPSTORE(p_sfpu::LREG1, 0, ADDR_MOD_3, 0);
-        } else {
-            TTI_SFPSTORE(p_sfpu::LREG0, 0, ADDR_MOD_3, 0);
-        }
+        // if constexpr (IS_MAX_OP) {
+        //     TTI_SFPSTORE(p_sfpu::LREG1, 8, ADDR_MOD_3, 0);
+        // } else {
+        //     TTI_SFPSTORE(p_sfpu::LREG0, 8, ADDR_MOD_3, 0);
+        // }
+
         dst_reg++;
     }
 }
