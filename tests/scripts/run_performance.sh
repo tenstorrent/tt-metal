75f3475a581a883406cc18b6327c499acd9ae8c4
Sabira Shaik <shaik.sabira@multicorewareinc.com>
2025-04-22 03:50:14 +0530

    perf and trace tests for mobilenetv2 (#20396)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/13798)
    
    ### Problem description
    Add Performance tests for mobilenetv2 model.
    
    ### What's changed
    Added e2e and Device performance tests .
    Added trace flow
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14530725035)
    - Pass.
    - [x] [(Single-card) Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/14530739928)
    - Pass
    - [x] [(Single-card) Model perf
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/14534879566)
    - Pass
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/runs/14571460644)-
    Pass
    
    ---------
    
    Co-authored-by: Dalar Vartanians <132954887+dvartaniansTT@users.noreply.github.com>

--- tests/scripts/run_performance.sh
+++ tests/scripts/run_performance.sh
@@ -39,6 +39,7 @@ run_perf_models_other() {
         env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/experimental/functional_vanilla_unet/test/test_perf_vanilla_unet.py -m $test_marker
 
         env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/experimental/yolov8s_world/tests -m $test_marker
+        env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/experimental/mobilenetv2/tests -m $test_marker
 
         env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/experimental/ufld_v2/tests -m $test_marker
 
