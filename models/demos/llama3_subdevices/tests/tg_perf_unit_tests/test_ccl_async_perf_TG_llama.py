daf813257bf6ac54d88a9dee119a439362edaf03
Ligang Long <llong@tenstorrent.com>
2025-04-21 11:01:24 -0700

    upload perf test for ar+create heads. (#20929)
    ### Ticket
    N/A
    
    ### Problem description
    perf UT test for all reduce + create heads fusion.
    
    
    ### What's changed
    Created new perf UT for all reduce + create heads fusion. Located in
    models/demos/llama3_subdevices/tests/tg_perf_unit_tests/test_ccl_async_perf_TG_llama.py
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [ ] New/Existing tests provide coverage for changes

--- models/demos/llama3_subdevices/tests/tg_perf_unit_tests/test_ccl_async_perf_TG_llama.py
+++ models/demos/llama3_subdevices/tests/tg_perf_unit_tests/test_ccl_async_perf_TG_llama.py
@@ -205,10 +205,60 @@ def test_fused_all_gather_concat_perf(
     profiler.end("run")
 
     # Get the measured performance
-    measured_min = results[cols[0]]["MIN"] / 1000
-    measured_max = results[cols[0]]["MAX"] / 1000
-    measured_avg = results[cols[0]]["AVG"] / 1000
-    measured_std = results[cols[0]]["STD"] / 1000
+    measured_min = results[cols[0]]["MIN"]
+    measured_max = results[cols[0]]["MAX"]
+    measured_avg = results[cols[0]]["AVG"]
+    measured_std = results[cols[0]]["STD"]
+    measured_avg_us = measured_avg / 1000
+
+    logger.info(f"Measured performance: {measured_avg_us:.3f} us vs. target: {perf_target_us} us")
+
+    # Save the measurement
+    benchmark_data.add_measurement(profiler, 0, step_name, f"{op_name}-min", measured_min)
+    benchmark_data.add_measurement(profiler, 0, step_name, f"{op_name}-max", measured_max)
+    benchmark_data.add_measurement(profiler, 0, step_name, f"{op_name}-avg", measured_avg)
+    benchmark_data.add_measurement(profiler, 0, step_name, f"{op_name}-std", measured_std)
+    benchmark_data.save_partial_run_json(
+        profiler,
+        run_type=f"tg_llama_ops",
+        ml_model_name="llama70b-tg",
+    )
+
+    assert measured_avg_us < perf_target_us, f"Performance target not met: {measured_avg_us} us > {perf_target_us} us"
+
+
+@pytest.mark.parametrize(
+    "warmup_iters, perf_target_us",
+    [
+        (5, 14.6),
+    ],
+)
+@pytest.mark.models_device_performance_bare_metal
+def test_fused_all_reduce_create_heads_perf(
+    warmup_iters,
+    perf_target_us,
+):
+    profiler = BenchmarkProfiler()
+    benchmark_data = BenchmarkData()
+    step_name = f"all_reduce_create_heads"
+
+    subdir = "llama_ccl_perf"
+    command = f"pytest tests/ttnn/unit_tests/operations/ccl/test_qkv_all_reduce_minimal.py::test_all_reduce_qkv_heads_fuse_perf"
+    cols = ["DEVICE KERNEL"]
+    op_name = "AllReduceCreateQkvHeads"
+    warmup_iters = warmup_iters * 32  # 5 iterations per device
+
+    profiler.start("run")
+    profiler.start(step_name)
+    results = run_device_perf_detailed(command, subdir, cols, op_name, has_signposts=True, warmup_iters=0)
+    profiler.end(step_name)
+    profiler.end("run")
+
+    # Get the measured performance
+    measured_min = results[cols[0]]["MIN"]
+    measured_max = results[cols[0]]["MAX"]
+    measured_avg = results[cols[0]]["AVG"]
+    measured_std = results[cols[0]]["STD"]
     measured_avg_us = measured_avg / 1000
 
     logger.info(f"Measured performance: {measured_avg_us:.3f} us vs. target: {perf_target_us} us")
