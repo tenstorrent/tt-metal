d0a681107351e5d68d559d8864a50d407cc8883d
Kannika Kabilar <kkabilar@tenstorrent.com>
2025-04-21 16:58:53 -0500

    Update performance in test_perf_yolov9c.py

--- models/experimental/functional_yolov9c/tests/test_perf_yolov9c.py
+++ models/experimental/functional_yolov9c/tests/test_perf_yolov9c.py
@@ -89,7 +89,7 @@ def test_perf(device, use_weights_from_ultralytics):
 @pytest.mark.parametrize(
     "batch_size, expected_perf",
     [
-        [1, 74.50],
+        [1, 76.27],
     ],
 )
 @pytest.mark.models_device_performance_bare_metal
