2c92ca87381e8364ae54c62d4c001ff3022415cf
Keerthana <152590865+kkeerthana0573@users.noreply.github.com>
2025-04-23 03:02:42 +0530

    #17700: Yolov9c model trace perf bringup (#20747)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/17700)
    
    ### Problem description
    - Add Trace+CQs perf test for Yolov9c model.
    
    ### What's changed
    - Added e2e trace+2cqs perf test for ttnn yolov9c model.
    - Updated Readme to include commands for yolov9c model e2e perf with
    trace+cqs.
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] ~~[Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)~~
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] ~~[Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)~~
    - [ ] ~~**(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)~~
    - [ ] New/Existing tests provide coverage for changes
    
    Co-authored-by: Dalar Vartanians <132954887+dvartaniansTT@users.noreply.github.com>

--- tests/scripts/run_performance.sh
+++ tests/scripts/run_performance.sh
@@ -34,7 +34,7 @@ run_perf_models_other() {
 
         env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/experimental/functional_vgg_unet/tests -m $test_marker
 
-        env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/experimental/functional_yolov9c/tests -m $test_marker
+        env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/experimental/functional_yolov9c/tests/perf -m $test_marker
 
         env WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/experimental/functional_vanilla_unet/test/test_perf_vanilla_unet.py -m $test_marker
 
