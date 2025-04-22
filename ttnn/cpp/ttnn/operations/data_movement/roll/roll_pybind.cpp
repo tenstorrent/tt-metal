a2afb9862fe9bd6e4cc48b9f56cc94fdef029e39
Bhuvanesh194Sankar <bhuvanesh.sankr@multicorewareinc.com>
2025-04-22 04:45:48 +0000

    Updated the pybind docs and removed the reference throughout the specified datatypes and worked on duplication of the code

--- ttnn/cpp/ttnn/operations/data_movement/roll/roll_pybind.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/roll/roll_pybind.cpp
@@ -31,16 +31,14 @@ void bind_roll(pybind11::module& module, const data_movement_operation_t& operat
             py::arg("dim")},
 
         ttnn::pybind_overload_t{
-            [](const data_movement_operation_t& self,
-               const ttnn::Tensor& input_tensor,
-               const int& shifts,
-               const int& dim) -> ttnn::Tensor { return self(input_tensor, shifts, dim); },
+            [](const data_movement_operation_t& self, const ttnn::Tensor& input_tensor, const int shifts, const int dim)
+                -> ttnn::Tensor { return self(input_tensor, shifts, dim); },
             py::arg("input_tensor"),
             py::arg("shifts"),
             py::arg("dim")},
 
         ttnn::pybind_overload_t{
-            [](const data_movement_operation_t& self, const ttnn::Tensor& input_tensor, const int& shifts)
+            [](const data_movement_operation_t& self, const ttnn::Tensor& input_tensor, const int shifts)
                 -> ttnn::Tensor { return self(input_tensor, shifts); },
             py::arg("input_tensor"),
             py::arg("shifts")});
@@ -60,7 +58,7 @@ void py_bind_roll(pybind11::module& module) {
         Args:
             input_tensor: A tensor whose elements will be rolled.
             shifts: The number of places by which elements are shifted. Can be an integer or a list of integers (one per dimension).
-            dim: The dimension(s) along which to roll. Must have the same length as `shifts` if a list is provided.
+            dim: The dimension(s) along which to roll. If shifts is a list, then dim must be a list of the same length as shifts.
         )doc");
 }
 
