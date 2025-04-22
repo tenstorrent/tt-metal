414fdd98e836e5b459cc959994c0e62bebe685f0
Virdhatchani Narayanamoorthy <138196495+VirdhatchaniKN@users.noreply.github.com>
2025-04-22 16:24:50 +0530

    #20506: Add int32 support for Tensor-Tensor version of maximum (#20886)
    ### Ticket
    #20506, #20977
    
    ### Problem description
    Implement Tensor-Tensor version of maximum int32
    
    ### What's changed
    Implemented Tensor-Tensor version of maximum int32
    
    - Used TTI_ instructions : SFPU requires 2's complement format. Hence in
    BH,
      - Loaded both inputs as signed magnitude using `instr_mod0 = 12`
      - Casted to int32 2’s complement
      - Set sign from input’s bit
    - Supports bcast using `use_legacy=False`
    - Tested in WH, BH
    - Removed existing GS support
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14538278404)
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14538279676)
    - [x] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/runs/14538280980)
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/runs/14538282112)
    - [x] [C++
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/14538283655)
    - [x] [Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/14538285054)
    - [x] [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/14538285961)

--- ttnn/cpp/ttnn/operations/eltwise/binary_ng/device/binary_ng_utils.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/binary_ng/device/binary_ng_utils.cpp
@@ -307,7 +307,12 @@ std::pair<std::string, std::string> get_sfpu_init_fn(OpConfig::SfpuBinaryOp sfpu
         case BITWISE_AND: return {"binary_bitwise_tile_init();", "and_binary_tile"};
         case BITWISE_OR: return {"binary_bitwise_tile_init();", "or_binary_tile"};
         case BITWISE_XOR: return {"binary_bitwise_tile_init();", "xor_binary_tile"};
-        case MAXIMUM: return {"binary_max_tile_init();", "binary_max_tile"};
+        case MAXIMUM:
+            if (dtype == DataType::INT32) {
+                return {"binary_max_tile_init();", "binary_max_int32_tile"};
+            } else {
+                return {"binary_max_tile_init();", "binary_max_tile"};
+            }
         case MINIMUM: return {"binary_min_tile_init();", "binary_min_tile"};
         case QUANT: return {"quant_tile_init(get_arg_val<uint32_t>(QUANT_ZERO_POINT_RT_ARGS_IDX));", "quant_tile"};
         case REQUANT:
