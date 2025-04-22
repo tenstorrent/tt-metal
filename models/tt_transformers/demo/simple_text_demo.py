ccd2bd53a7baee4672c5866df99bdf8bcd5319bc
Rashid Kaleem <rkaleem@tenstorrent.com>
2025-04-22 15:13:01 +0000

    Fix prefill warmup
    Signed-off-by: Rashid Kaleem <rkaleem@tenstorrent.com>

--- models/tt_transformers/demo/simple_text_demo.py
+++ models/tt_transformers/demo/simple_text_demo.py
@@ -623,7 +623,7 @@ def test_demo_text(
         logger.info("Starting prefill warmup...")
         profiler.start(f"compile_prefill", iteration=batch_idx)
         logits = generator.prefill_forward_text(
-            input_tokens_prefill_pt,  # Prefill warmup for all users, in case some users have different seqlens than others
+            input_tokens_prefill_pt[:num_devices, :],  # Warmup prefill for each device
             page_table=page_table,
             kv_cache=tt_kv_cache,
             prompt_lens=decoding_pos,
