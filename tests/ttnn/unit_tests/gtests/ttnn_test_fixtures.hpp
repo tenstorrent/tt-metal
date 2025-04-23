8df0f3110788dddf36f188b2d00d7a078880df10
Jeffrey Jiang <jjiang@tenstorrent.com>
2025-04-23 10:11:27 -0500

    #18973: Deduplicate ttnn_test_fixtures.hpp code (#20863)
    ### Ticket
    Link to Github Issue:
    https://github.com/tenstorrent/tt-metal/issues/18973
    
    ### Problem description
    A lot of similar code is duplicated between the fixtures in the
    jjiang/tt-metal/tests/ttnn/unit_tests/gtests/ttnn_test_fixtures.hpp
    file. Moving things into TTNNFixtureWithDevice and making
    MultiCommandQueueSingleDeviceFixture and MultiCommandQueueT3KFixture
    inherit from it makes things a bit cleaner.
    
    ### What's changed
    Add InitStateEnv(); and checkSlowDispatch(); methods to
    TTNNFixtureWithDevice, move shared variables into TTNNFixtureWithDevice.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/14599336507
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    - [x] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [x] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [x] New/Existing tests provide coverage for changes:
    tests/ttnn/unit_tests

--- tests/ttnn/unit_tests/gtests/ttnn_test_fixtures.hpp
+++ tests/ttnn/unit_tests/gtests/ttnn_test_fixtures.hpp
@@ -25,45 +25,57 @@
 using namespace tt::tt_metal;  // For test
 
 namespace ttnn {
-
-class TTNNFixtureWithDevice : public ::testing::Test {
-private:
+class TTNNFixtureBase : public ::testing::Test {
+protected:
     int trace_region_size_ = DEFAULT_TRACE_REGION_SIZE;
     int l1_small_size_ = DEFAULT_L1_SMALL_SIZE;
-
-protected:
-    tt::tt_metal::distributed::MeshDevice* device_ = nullptr;
-    std::shared_ptr<tt::tt_metal::distributed::MeshDevice> device_holder_;
     tt::ARCH arch_ = tt::ARCH::Invalid;
     size_t num_devices_ = 0;
 
-    void SetUp() override {
+public:
+    void check_slow_dispatch() {
+        auto slow_dispatch = getenv("TT_METAL_SLOW_DISPATCH_MODE");
+        if (slow_dispatch) {
+            GTEST_SKIP() << "Skipping test, since it can only be run in Fast Dispatch Mode.";
+        }
+    }
+
+public:
+    TTNNFixtureBase() : TTNNFixtureBase(DEFAULT_TRACE_REGION_SIZE, DEFAULT_L1_SMALL_SIZE) { }
+
+    TTNNFixtureBase(int trace_region_size, int l1_small_size) :
+        trace_region_size_(trace_region_size), l1_small_size_(l1_small_size) {
         std::srand(0);
         arch_ = tt::get_arch_from_string(tt::test_utils::get_umd_arch_name());
-        num_devices_ = tt::tt_metal::GetNumAvailableDevices();
+        num_devices_ = GetNumAvailableDevices();
+    }
+};
+
+class TTNNFixtureWithDevice : public TTNNFixtureBase {
+protected:
+    tt::tt_metal::distributed::MeshDevice* device_ = nullptr;
+    std::shared_ptr<tt::tt_metal::distributed::MeshDevice> device_holder_;
+
+    void SetUp() override {
         device_holder_ = ttnn::open_mesh_device(/*device_id=*/0, l1_small_size_, trace_region_size_);
         device_ = device_holder_.get();
     }
 
     void TearDown() override { device_->close(); }
 
-public:
-    TTNNFixtureWithDevice() : trace_region_size_(DEFAULT_TRACE_REGION_SIZE), l1_small_size_(DEFAULT_L1_SMALL_SIZE) {}
+    TTNNFixtureWithDevice() : TTNNFixtureBase() {}
 
     TTNNFixtureWithDevice(int trace_region_size, int l1_small_size) :
-        trace_region_size_(trace_region_size), l1_small_size_(l1_small_size) {}
+        TTNNFixtureBase(trace_region_size, l1_small_size) {}
 };
 
-// TODO: deduplicate the code with `TTNNFixtureWithDevice`.
-class MultiCommandQueueSingleDeviceFixture : public ::testing::Test {
+class MultiCommandQueueSingleDeviceFixture : public TTNNFixtureBase {
 protected:
+    tt::tt_metal::distributed::MeshDevice* device_ = nullptr;
+    std::shared_ptr<tt::tt_metal::distributed::MeshDevice> device_holder_;
+
     void SetUp() override {
-        auto slow_dispatch = getenv("TT_METAL_SLOW_DISPATCH_MODE");
-        arch_ = tt::get_arch_from_string(tt::test_utils::get_umd_arch_name());
-        num_devices_ = tt::tt_metal::GetNumAvailableDevices();
-        if (slow_dispatch) {
-            GTEST_SKIP() << "Skipping Multi CQ test suite, since it can only be run in Fast Dispatch Mode.";
-        }
+        check_slow_dispatch();
 
         DispatchCoreType dispatch_core_type = DispatchCoreType::WORKER;
         if (arch_ == tt::ARCH::WORMHOLE_B0 and num_devices_ != 1) {
@@ -77,28 +89,20 @@ protected:
     }
 
     void TearDown() override { device_->close(); }
-
-    tt::tt_metal::distributed::MeshDevice* device_ = nullptr;
-    std::shared_ptr<tt::tt_metal::distributed::MeshDevice> device_holder_;
-    tt::ARCH arch_;
-    size_t num_devices_;
 };
 
-// TODO: deduplicate the code with `TTNNFixtureWithDevice`.
-class MultiCommandQueueT3KFixture : public ::testing::Test {
+class MultiCommandQueueT3KFixture : public TTNNFixtureBase {
 protected:
+    std::map<chip_id_t, std::shared_ptr<tt::tt_metal::distributed::MeshDevice>> devs;
+
     void SetUp() override {
-        auto slow_dispatch = getenv("TT_METAL_SLOW_DISPATCH_MODE");
-        arch_ = tt::get_arch_from_string(tt::test_utils::get_umd_arch_name());
-        num_devices_ = tt::tt_metal::GetNumAvailableDevices();
-        if (slow_dispatch) {
-            GTEST_SKIP() << "Skipping Multi CQ test suite, since it can only be run in Fast Dispatch Mode.";
-        }
+        check_slow_dispatch();
+
         if (num_devices_ < 8 or arch_ != tt::ARCH::WORMHOLE_B0) {
             GTEST_SKIP() << "Skipping T3K Multi CQ test suite on non T3K machine.";
         }
-        // Enable Ethernet Dispatch for Multi-CQ tests.
 
+        // Enable Ethernet Dispatch for Multi-CQ tests.
         devs = tt::tt_metal::distributed::MeshDevice::create_unit_meshes(
             {0, 1, 2, 3, 4, 5, 6, 7},
             DEFAULT_L1_SMALL_SIZE,
@@ -112,10 +116,6 @@ protected:
             dev->close();
         }
     }
-
-    std::map<chip_id_t, std::shared_ptr<tt::tt_metal::distributed::MeshDevice>> devs;
-    tt::ARCH arch_;
-    size_t num_devices_;
 };
 
 }  // namespace ttnn
