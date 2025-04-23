d6c06ec39eb3bc97131fb83458d5318be1631d4a
Jonathan Su <jonathansu@tenstorrent.com>
2025-04-23 00:56:12 +0000

    remove hardcoded values from both hidden dim and head padding

--- models/experimental/stable_diffusion3/tt/linear.py
+++ models/experimental/stable_diffusion3/tt/linear.py
@@ -5,7 +5,6 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import TYPE_CHECKING
 
 import ttnn
 
@@ -99,6 +98,8 @@ class TtLinearParameters:
         state: dict[str, torch.Tensor],
         *,
         n_local_heads: int,
+        unpadded_num_heads: int,
+        hidden_dim_padding: int,
         dtype: ttnn.DataType | None = None,
         device: ttnn.Device,
     ) -> TtLinearParameters:
@@ -108,42 +109,23 @@ class TtLinearParameters:
             torch_bias = None
 
         weight = state["weight"]
+        torch_weight = weight.transpose(0, 1)
+
         if os.environ["FAKE_DEVICE"] == "T3K":
-            hidden_dim = 2432
-            hidden_dim_pad = 128
-            hidden_dim_new = 2560
-            weight_h, weight_w = weight.shape
-            weight_h_mult = weight_h // hidden_dim
-            weight_w_mult = weight_w // hidden_dim
-            if weight_h % hidden_dim == 0:
-                if weight_h_mult == 1:
-                    weight = torch.nn.functional.pad(weight, pad=(0, 0, 0, hidden_dim_pad), mode="constant", value=0)
-                elif weight_h_mult > 1:
-                    weight = weight.reshape(weight_h_mult, hidden_dim, weight_w)
-                    weight = torch.nn.functional.pad(weight, pad=(0, 0, 0, hidden_dim_pad), mode="constant", value=0)
-                    weight = weight.reshape(weight_h_mult * hidden_dim_new, weight_w)
-            weight_h, weight_w = weight.shape
-            if weight_w % hidden_dim == 0:
-                if weight_w_mult == 1:
-                    weight = torch.nn.functional.pad(weight, pad=(0, hidden_dim_pad), mode="constant", value=0)
-                elif weight_w_mult > 1:
-                    weight = weight.reshape(weight_h, weight_w_mult, hidden_dim)
-                    weight = torch.nn.functional.pad(weight, pad=(0, hidden_dim_pad), mode="constant", value=0)
-                    weight = weight.reshape(weight_h, weight_w_mult * hidden_dim_new)
+            head_size = torch_weight.shape[1] // 3 // unpadded_num_heads
+            head_padding = device.get_num_devices() - (unpadded_num_heads % device.get_num_devices())
+            weight_h, weight_w = torch_weight.shape
+
+            torch_weight = torch_weight.reshape(weight_h, 3, unpadded_num_heads, head_size)
+            torch_weight = torch.nn.functional.pad(
+                torch_weight, pad=(0, 0, 0, head_padding, 0, 0, 0, hidden_dim_padding), mode="constant", value=0
+            )
+            torch_weight = torch_weight.reshape(weight_h + hidden_dim_padding, -1)
             if not torch_bias == None:
                 bias_h, bias_w = torch_bias.shape
-                bias_w_mult = bias_w // hidden_dim
-                if bias_w % hidden_dim == 0:
-                    if bias_w_mult == 1:
-                        torch_bias = torch.nn.functional.pad(
-                            torch_bias, pad=(0, hidden_dim_pad), mode="constant", value=0
-                        )
-                    elif bias_w_mult > 1:
-                        torch_bias = torch_bias.reshape(bias_h, bias_w_mult, hidden_dim)
-                        torch_bias = torch.nn.functional.pad(
-                            torch_bias, pad=(0, hidden_dim_pad), mode="constant", value=0
-                        )
-                        torch_bias = torch_bias.reshape(bias_h, bias_w_mult * hidden_dim_new)
+                torch_bias = torch_bias.reshape(bias_h, 3, unpadded_num_heads, head_size)
+                torch_bias = torch.nn.functional.pad(torch_bias, pad=(0, 0, 0, head_padding), mode="constant", value=0)
+                torch_bias = torch_bias.reshape(bias_h, -1)
 
         def shuffle_heads(tensor):
             # Given torch tensor with output features in the last dimension,
@@ -154,7 +136,6 @@ class TtLinearParameters:
             tensor = tensor.reshape(in_dim, -1)  # [ID, ND*3*NLH*DH]
             return tensor
 
-        torch_weight = weight.transpose(0, 1)
         return cls(
             weight=from_torch(
                 shuffle_heads(torch_weight),
@@ -180,6 +161,7 @@ class TtLinearParameters:
         state: dict[str, torch.Tensor],
         *,
         num_chunks: int,
+        hidden_dim_padding: int,
         dtype: ttnn.DataType | None = None,
         device: ttnn.Device,
         unsqueeze_bias: bool = False,
@@ -189,45 +171,21 @@ class TtLinearParameters:
         else:
             torch_bias = None
         weight = state["weight"]
+        torch_weight = weight.transpose(0, 1)
+
         if os.environ["FAKE_DEVICE"] == "T3K":
-            hidden_dim = 2432
-            hidden_dim_pad = 128
-            hidden_dim_new = 2560
-            weight_h, weight_w = weight.shape
-            weight_h_mult = weight_h // hidden_dim
-            weight_w_mult = weight_w // hidden_dim
-            if weight_h % hidden_dim == 0:
-                if weight_h_mult == 1:
-                    weight = torch.nn.functional.pad(weight, pad=(0, 0, 0, hidden_dim_pad), mode="constant", value=0)
-                elif weight_h_mult > 1:
-                    weight = weight.reshape(weight_h_mult, hidden_dim, weight_w)
-                    weight = torch.nn.functional.pad(weight, pad=(0, 0, 0, hidden_dim_pad), mode="constant", value=0)
-                    weight = weight.reshape(weight_h_mult * hidden_dim_new, weight_w)
-            weight_h, weight_w = weight.shape
-            if weight_w % hidden_dim == 0:
-                if weight_w_mult == 1:
-                    weight = torch.nn.functional.pad(weight, pad=(0, hidden_dim_pad), mode="constant", value=0)
-                elif weight_w_mult > 1:
-                    weight = weight.reshape(weight_h, weight_w_mult, hidden_dim)
-                    weight = torch.nn.functional.pad(weight, pad=(0, hidden_dim_pad), mode="constant", value=0)
-                    weight = weight.reshape(weight_h, weight_w_mult * hidden_dim_new)
+            weight_h, weight_w = torch_weight.shape
+            torch_weight = torch_weight.reshape(weight_h, num_chunks, -1)
+            torch_weight = torch.nn.functional.pad(
+                torch_weight, pad=(0, hidden_dim_padding, 0, 0, 0, hidden_dim_padding), mode="constant", value=0
+            )
+            torch_weight = torch_weight.reshape(weight_h + hidden_dim_padding, -1)
 
             if not torch_bias == None:
                 bias_h, bias_w = torch_bias.shape
-                bias_w_mult = bias_w // hidden_dim
-                if bias_w % hidden_dim == 0:
-                    if bias_w_mult == 1:
-                        torch_bias = torch.nn.functional.pad(
-                            torch_bias, pad=(0, hidden_dim_pad), mode="constant", value=0
-                        )
-                    elif bias_w_mult > 1:
-                        torch_bias = torch_bias.reshape(bias_h, bias_w_mult, hidden_dim)
-                        torch_bias = torch.nn.functional.pad(
-                            torch_bias, pad=(0, hidden_dim_pad), mode="constant", value=0
-                        )
-                        torch_bias = torch_bias.reshape(bias_h, bias_w_mult * hidden_dim_new)
-
-        torch_weight = weight.transpose(0, 1)
+                torch_bias = torch_bias.reshape(bias_h, num_chunks, -1)
+                torch_bias = torch.nn.functional.pad(torch_bias, pad=(0, hidden_dim_padding), mode="constant", value=0)
+                torch_bias = torch_bias.reshape(bias_h, -1)
 
         def shuffle_chunks(tensor):
             # Given torch tensor with output features in the last dimension,
