188cd512beda5917e158367affc9b75264d68c65
Raymond Kim <rkim@tenstorrent.com>
2025-04-23 19:02:21 -0400

    Try running a different pytest that should hopefully not hang...

--- tests/scripts/wh_6u/run_wh_6u_upstream_tests.sh
+++ tests/scripts/wh_6u/run_wh_6u_upstream_tests.sh
@@ -2,7 +2,9 @@
 set -euo pipefail
 
 echo "[upstream-tests] Running minimal model unit tests"
-pytest tests/ttnn/unit_tests/operations/ccl/test_minimals.py
+# hang?
+pytest tests/ttnn/unit_tests/operations/ccl/test_ccl_async_TG_llama.py
+# pytest tests/ttnn/unit_tests/operations/ccl/test_minimals.py
 
 echo "[upstream-tests] running metalium section. Note that skips should be treated as failures"
 TT_METAL_SKIP_ETH_CORES_WITH_RETRAIN=1 TT_METAL_SLOW_DISPATCH_MODE=1 ./build/test/tt_metal/tt_fabric/fabric_unit_tests --gtest_filter="ControlPlaneFixture.TestQuantaGalaxyControlPlaneInit"
