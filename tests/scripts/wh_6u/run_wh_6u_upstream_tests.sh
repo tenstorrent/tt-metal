7579b966077b165c7b93ef06aa8bdcd4654fee7c
Raymond Kim <rkim@tenstorrent.com>
2025-04-23 18:42:28 -0400

    Try installing wheel and running one pytest

--- tests/scripts/wh_6u/run_wh_6u_upstream_tests.sh
+++ tests/scripts/wh_6u/run_wh_6u_upstream_tests.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+echo "[upstream-tests] Running minimal model unit tests"
+pytest tests/ttnn/unit_tests/operations/ccl/test_minimals.py
+
 echo "[upstream-tests] running metalium section. Note that skips should be treated as failures"
 TT_METAL_SKIP_ETH_CORES_WITH_RETRAIN=1 TT_METAL_SLOW_DISPATCH_MODE=1 ./build/test/tt_metal/tt_fabric/fabric_unit_tests --gtest_filter="ControlPlaneFixture.TestQuantaGalaxyControlPlaneInit"
 TT_METAL_SKIP_ETH_CORES_WITH_RETRAIN=1 ./build/test/tt_metal/unit_tests_dispatch --gtest_filter="CommandQueueSingleCardFixture.*"
