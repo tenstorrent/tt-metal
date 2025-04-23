tenstorrent/tt-metal

commit 4527ef97c1d4439546bb88a6270782e4ab6be775

tt_transformers: Enable support for non-uniform decoder configurations - after bug fixes (#20643)
### Ticket
The PR https://github.com/tenstorrent/tt-metal/pull/19431 introduced
some bugs and was reverted in
https://github.com/tenstorrent/tt-metal/pull/20573. This PR includes a
fix for those issues.

### Problem description
See description in https://github.com/tenstorrent/tt-metal/pull/19431

### What's changed
This PR includes https://github.com/tenstorrent/tt-metal/pull/19431 and
https://github.com/tenstorrent/tt-metal/pull/20525.

### Checklist
All post commit tests:
https://github.com/tenstorrent/tt-metal/actions/runs/14449485943
Single card demo tests:
https://github.com/tenstorrent/tt-metal/actions/runs/14449511995
T3k tests:
https://github.com/tenstorrent/tt-metal/actions/runs/14449504818
(falcon7b error is unrelated)

---------

Signed-off-by: Tomer Levin <tlevin@tenstorrent.com>

models/tt_transformers/tt/attention.py
@@ -8,6 +8,7 @@ import torch
 import ttnn
 from models.common.lightweightmodule import LightweightModule
 from models.tt_transformers.tt.ccl import tt_all_reduce, tt_all_gather
+from models.tt_transformers.tt.model_config import TensorGroup, OpGroup
 
 
 class Attention(LightweightModule):
@@ -92,6 +93,36 @@ class Attention(LightweightModule):
         self.model_config = configuration.get_model_config()
         self.ccl_topology = configuration.ccl_topology()
         self.is_multichip = configuration.is_multichip
+        self.activation_dtype = self.model_config["DECODERS_OPTIMIZATIONS"].get_tensor_dtype(
+            decoder_id=layer_num, tensor=TensorGroup.ACTIVATION
+        )
+        self.wqkv_dtype = self.model_config["DECODERS_OPTIMIZATIONS"].get_tensor_dtype(
+            decoder_id=layer_num, tensor=TensorGroup.WQKV
+        )
+        self.wo_dtype = self.model_config["DECODERS_OPTIMIZATIONS"].get_tensor_dtype(
+            decoder_id=layer_num, tensor=TensorGroup.WO
+        )
+        self.kv_cache_dtype = self.model_config["DECODERS_OPTIMIZATIONS"].get_tensor_dtype(
+            decoder_id=layer_num, tensor=TensorGroup.KV_CACHE
+        )
+        self.li_qkv_decode_compute_kernel_cfg = self.model_config["DECODERS_OPTIMIZATIONS"].get_math_fidelity(
+            decoder_id=layer_num, op=OpGroup.LI_QKV_DECODE, configuration=configuration
+        )
+        self.sdpa_decode_compute_kernel_cfg = self.model_config["DECODERS_OPTIMIZATIONS"].get_math_fidelity(
+            decoder_id=layer_num, op=OpGroup.SDPA_DECODE, configuration=configuration
+        )
+        self.li_o_decode_compute_kernel_cfg = self.model_config["DECODERS_OPTIMIZATIONS"].get_math_fidelity(
+            decoder_id=layer_num, op=OpGroup.LI_O_DECODE, configuration=configuration
+        )
+        self.sdpa_prefill_compute_kernel_cfg = self.model_config["DECODERS_OPTIMIZATIONS"].get_math_fidelity(
+            decoder_id=layer_num, op=OpGroup.SDPA_PREFILL, configuration=configuration
+        )
+        self.li_qkv_prefill_compute_kernel_cfg = self.model_config["DECODERS_OPTIMIZATIONS"].get_math_fidelity(
+            decoder_id=layer_num, op=OpGroup.LI_QKV_PREFILL, configuration=configuration
+        )
+        self.li_o_prefill_compute_kernel_cfg = self.model_config["DECODERS_OPTIMIZATIONS"].get_math_fidelity(
+            decoder_id=layer_num, op=OpGroup.LI_O_PREFILL, configuration=configuration
+        )
 
         layer_name = configuration.get_state_dict_prefix(self.__class__.__name__, layer_num)
         if configuration.dummy_weights or (weight_cache_path is None):
@@ -191,7 +222,7 @@ class Attention(LightweightModule):
 
         self.wqkv = ttnn.as_tensor(
             qkv_cat,
-            dtype=self.model_config["WQKV_DTYPE"],
+            dtype=self.wqkv_dtype,
             layout=ttnn.TILE_LAYOUT,
             device=self.mesh_device,
             memory_config=ttnn.DRAM_MEMORY_CONFIG if self.TG else wqkv_mem_config,
@@ -211,7 +242,7 @@ class Attention(LightweightModule):
 
         self.wo = ttnn.as_tensor(
             pt_wo,
-            dtype=self.model_config["WO_DTYPE"],
+            dtype=self.wo_dtype,
             layout=ttnn.TILE_LAYOUT,
             device=self.mesh_device,
             memory_config=ttnn.DRAM_MEMORY_CONFIG if (self.use_fused_all_gather_matmul or self.TG) else wo_mem_config,
@@ -273,7 +304,7 @@ class Attention(LightweightModule):
         self.layer_past = [
             ttnn.as_tensor(
                 k_or_v,
-                dtype=self.model_config["KV_CACHE_DTYPE"],
+                dtype=self.kv_cache_dtype,
                 layout=self.model_config["ATTN_W_LAYOUT_TILE"],
                 device=self.mesh_device,
                 memory_config=ttnn.DRAM_MEMORY_CONFIG,
@@ -311,8 +342,8 @@ class Attention(LightweightModule):
             # bias=self.wqkv_bias,
             memory_config=ttnn.L1_WIDTH_SHARDED_MEMORY_CONFIG,
             program_config=self.model_config["XQKV_DECODE_PROGCFG"],
-            compute_kernel_config=self.model_config["LI_QKV_DECODE_COMPUTE_KERNEL_CFG"],
-            dtype=self.ccl_dtype if self.TG else self.model_config["ACTIVATION_DTYPE"] or ttnn.bfloat16,
+            compute_kernel_config=self.li_qkv_decode_compute_kernel_cfg,
+            dtype=xself.ccl_dtype if self.TG else self.activation_dtype or ttnn.bfloat16,
         )
         # FIXME: File bug against dram-sharded matmuls with bias
         if self.wqkv_bias_decode:
@@ -416,7 +447,7 @@ class Attention(LightweightModule):
                 page_table_tensor=page_table,
                 scale=self.scale,
                 program_config=self.model_config["SDPA_DECODE_PROGCFG"],
-                compute_kernel_config=self.model_config["SDPA_DECODE_COMPUTE_KERNEL_CFG"],
+                compute_kernel_config=self.sdpa_decode_compute_kernel_cfg,
                 memory_config=ttnn.DRAM_MEMORY_CONFIG,
             )
         else:
@@ -427,7 +458,7 @@ class Attention(LightweightModule):
                 cur_pos_tensor=current_pos,
                 scale=self.scale,
                 program_config=self.model_config["SDPA_DECODE_PROGCFG"],
-                compute_kernel_config=self.model_config["SDPA_DECODE_COMPUTE_KERNEL_CFG"],
+                compute_kernel_config=self.sdpa_decode_compute_kernel_cfg,
                 memory_config=ttnn.DRAM_MEMORY_CONFIG,  # FIXME: why not L1 height sharded e.g. SCORES_BATCHED_MM_OUTPUT_MEMCFG?
             )
 
@@ -455,7 +486,7 @@ class Attention(LightweightModule):
                 all_gather_core_grid_offset=(0, 4),
                 num_links=1,
                 program_config=self.model_config["ATTN_ALL_GATHER_MATMUL_PROGCFG"],
-                compute_kernel_config=self.model_config["LI_O_DECODE_COMPUTE_KERNEL_CFG"],
+                compute_kernel_config=self.li_o_decode_compute_kernel_cfg,
                 memory_config_ag=self.model_config["ATTN_ALL_GATHER_MATMUL_OUTPUT_MEMCFG"],
                 memory_config_mm=self.model_config["DECODE_RESIDUAL_MEMCFG"],
             )
@@ -494,7 +525,7 @@ class Attention(LightweightModule):
                 program_config=self.model_config["ATTN_OUTPUT_PROGCFG"] if not self.TG else None,
                 memory_config=ttnn.L1_WIDTH_SHARDED_MEMORY_CONFIG if self.TG else attn_output_cat.memory_config(),
                 dtype=ttnn.bfloat8_b if self.TG else None,
-                compute_kernel_config=self.model_config["LI_O_DECODE_COMPUTE_KERNEL_CFG"],
+                compute_kernel_config=self.li_o_decode_compute_kernel_cfg,
             )
 
             ttnn.deallocate(attn_output_cat)
@@ -554,9 +585,9 @@ class Attention(LightweightModule):
         xqkv_fused = ttnn.linear(
             x_11SH,
             self.wqkv,
-            dtype=self.ccl_dtype if self.TG else self.model_config["ACTIVATION_DTYPE"] or ttnn.bfloat16,
+            dtype=self.ccl_dtype if self.TG else self.activation_dtype or ttnn.bfloat16,
             memory_config=ttnn.DRAM_MEMORY_CONFIG,
-            compute_kernel_config=self.model_config["LI_QKV_PREFILL_COMPUTE_KERNEL_CFG"],
+            compute_kernel_config=self.li_qkv_prefill_compute_kernel_cfg,
             program_config=self.model_config["XQKV_PREFILL_PROGCFG"](seq_len),
         )
 
@@ -628,7 +659,7 @@ class Attention(LightweightModule):
         else:
             keys_BKSD, values_BKSD = self.layer_past[0], self.layer_past[1]
 
-        k_heads_1KSD_8b = ttnn.typecast(k_heads_1KSD, dtype=self.model_config["KV_CACHE_DTYPE"])
+        k_heads_1KSD_8b = ttnn.typecast(k_heads_1KSD, dtype=self.kv_cache_dtype)
         ttnn.deallocate(k_heads_1KSD)
 
         # sharding k_fill to deal with update_cache memory limitation
@@ -637,7 +668,7 @@ class Attention(LightweightModule):
         else:
             k_fill = k_heads_1KSD_8b
 
-        v_heads_1VSD_8b = ttnn.typecast(v_heads_1VSD, dtype=self.model_config["KV_CACHE_DTYPE"])
+        v_heads_1VSD_8b = ttnn.typecast(v_heads_1VSD, dtype=self.kv_cache_dtype)
 
         ttnn.deallocate(v_heads_1VSD)
 
@@ -679,7 +710,7 @@ class Attention(LightweightModule):
             ttnn.deallocate(v_fill)
 
         # SDPA
-        q_heads_1QSD_8b = ttnn.typecast(q_heads_1QSD, dtype=self.model_config["ACTIVATION_DTYPE"] or ttnn.bfloat8_b)
+        q_heads_1QSD_8b = ttnn.typecast(q_heads_1QSD, dtype=self.activation_dtype or ttnn.bfloat8_b)
         ttnn.deallocate(q_heads_1QSD)
 
         if chunk_start_idx is not None:
@@ -689,7 +720,7 @@ class Attention(LightweightModule):
                 values_BKSD,
                 page_table,
                 chunk_start_idx,
-                compute_kernel_config=self.model_config["SDPA_PREFILL_COMPUTE_KERNEL_CFG"],
+                compute_kernel_config=self.sdpa_prefill_compute_kernel_cfg,
                 program_config=self.model_config["SDPA_PROGCFG"](seq_len),
             )
         else:
@@ -699,7 +730,7 @@ class Attention(LightweightModule):
                 v_heads_1VSD_8b,
                 is_causal=True,
                 scale=self.scale,
-                compute_kernel_config=self.model_config["SDPA_PREFILL_COMPUTE_KERNEL_CFG"],
+                compute_kernel_config=self.sdpa_prefill_compute_kernel_cfg,
                 program_config=self.model_config["SDPA_PROGCFG"](seq_len),
             )
 
@@ -735,8 +766,8 @@ class Attention(LightweightModule):
         output_11SH = ttnn.linear(
             attn_output_11SH,
             self.wo,
-            compute_kernel_config=self.model_config["LI_O_PREFILL_COMPUTE_KERNEL_CFG"],
-            dtype=self.model_config["ACTIVATION_DTYPE"] or ttnn.bfloat8_b,
+            compute_kernel_config=self.li_o_prefill_compute_kernel_cfg,
+            dtype=self.activation_dtype or ttnn.bfloat8_b,
             memory_config=ttnn.DRAM_MEMORY_CONFIG,
             program_config=self.model_config["WO_PREFILL_PROGCFG"](seq_len),
         )

models/tt_transformers/demo/conftest.py
@@ -30,3 +30,10 @@ def pytest_addoption(parser):
         type=parse_optimizations,
         help="Precision and fidelity configuration diffs over default (i.e., accuracy)",
     )
+    parser.addoption(
+        "--decoder_config_file",
+        action="store",
+        default=None,
+        type=str,
+        help="Provide a JSON file defining per-decoder precision and fidelity settings",
+    )

models/tt_transformers/demo/simple_text_demo.py
@@ -15,7 +15,8 @@ import ttnn
 
 
 from models.tt_transformers.tt.generator import Generator, SamplingParams
-from models.tt_transformers.tt.model_config import ModelOptimizations
+from models.tt_transformers.tt.model_config import DecodersPrecision, parse_decoder_json
+
 from models.tt_transformers.tt.common import (
     preprocess_inputs_prefill,
     PagedAttentionConfig,
@@ -409,9 +410,10 @@ def prepare_generator_args(
 @pytest.mark.parametrize(
     "optimizations",
     [
-        ModelOptimizations.performance,
-        ModelOptimizations.accuracy,
+        lambda model_args: DecodersPrecision.performance(model_args.n_layers, model_args.model_name),
+        lambda model_args: DecodersPrecision.accuracy(model_args.n_layers, model_args.model_name),
     ],
+    ids=["performance", "accuracy"],
 )
 @pytest.mark.parametrize("device_params", [{"trace_region_size": 23887872, "num_command_queues": 2}], indirect=True)
 @pytest.mark.parametrize(
@@ -446,8 +448,8 @@ def test_demo_text(
     """
     Simple demo with limited dependence on reference code.
     """
-
-    if is_ci_env and (optimizations == ModelOptimizations.accuracy or not ci_only):
+    test_id = request.node.callspec.id
+    if is_ci_env and (("accuracy" in test_id) or not ci_only):
         pytest.skip("CI only runs the CI-only tests")
 
     # TODO: Remove this once all batch sizes are supported on TG
@@ -472,7 +474,13 @@ def test_demo_text(
     paged_attention = request.config.getoption("--paged_attention") or paged_attention
     page_params = request.config.getoption("--page_params") or page_params
     sampling_params = request.config.getoption("--sampling_params") or sampling_params
-    optimizations = request.config.getoption("--optimizations") or optimizations
+    json_config_file = request.config.getoption("--decoder_config_file")
+
+    if json_config_file:
+        optimizations = parse_decoder_json(json_config_file)
+    else:
+        optimizations = request.config.getoption("--optimizations") or optimizations
+
     if request.config.getoption("--stop_at_eos") in [
         0,
         1,

models/tt_transformers/lt
@@ -16,6 +16,7 @@ import psutil
 import json
 import matplotlib.pyplot as plt
 import numpy as np
+from pathlib import Path
 
 from models.tt_transformers.tt.model_config import parse_optimizations
 
@@ -47,6 +48,18 @@ pareto_commands = {
         for i, setting in enumerate(dtype_mf_settings)
     ]
     for k, v in cmd.items()
+}
+
+DECODER_CONFIG_FOLDER = Path("models/tt_transformers/tests/configurations/")
+decoder_config_files = [file for file in DECODER_CONFIG_FOLDER.glob("*.json")]
+
+pareto_commands_from_json = {
+    k: v
+    for i, file in enumerate(decoder_config_files)
+    for k, v in {
+        f"accuracy-dmf-json-{i}": f"pytest models/tt_transformers/tests/test_accuracy.py -k 'attention-accuracy and file' --decoder_config_file {file}",
+        f"demo-dmf-json-{i}": f"pytest models/tt_transformers/demo/simple_text_demo.py -k 'params0 and batch-1' --decoder_config_file {file}",
+    }.items()
 }
 
 
@@ -518,6 +531,9 @@ def main(stdscr):
                         if command_input == "pareto":
                             command_input = ",".join(pareto_commands.keys())
 
+                        if command_input == "pareto_from_json":
+                            command_input = ",".join(pareto_commands_from_json.keys())
+
                         # Parse models, devices, and commands
                         models = parse_list(model_input)
                         devices = parse_list(device_input)
@@ -917,6 +933,7 @@ def run_entry_command(entry, screen_lock, output_entries, screen_needs_update):
         "model-prefill": "pytest models/tt_transformers/tests/test_model_prefill.py -k performance-4096",
         # Precision and Math Fidelity tests for Pareto analysis
         **pareto_commands,
+        **pareto_commands_from_json,
         # Vision tests (require 11B weights)
         "vision-mlp": "pytest models/tt_transformers/tests/multimodal/test_image_mlp.py",
         "vision-attn": "pytest models/tt_transformers/tests/multimodal/test_image_attention.py",
@@ -1341,7 +1358,10 @@ def export_results_to_markdown(output_entries, stdscr):
                 speed = entry.speed
                 ttft = entry.ttft
 
-                setting_idx = int(entry.command_name[len("demo-dmf-") :])
+                match = re.search(r"demo-dmf(?:-json)?-(\d+)", entry.command_name)
+                if not match:
+                    continue
+                setting_idx = int(match.group(1))
                 if setting_idx not in dmf_entries:
                     dmf_entries[setting_idx] = {key: ["N/A", "N/A", speed or "N/A", ttft]}
                 else:
@@ -1356,7 +1376,10 @@ def export_results_to_markdown(output_entries, stdscr):
                     if match:
                         top1, top5 = match.group(1), match.group(2)
 
-                setting_idx = int(entry.command_name[len("accuracy-dmf-") :])
+                match = re.search(r"accuracy-dmf(?:-json)?-(\d+)", entry.command_name)
+                if not match:
+                    continue
+                setting_idx = int(match.group(1))
                 if setting_idx not in dmf_entries:
                     dmf_entries[setting_idx] = {key: [top1, top5, "N/A", "N/A"]}
                 else:
@@ -1417,7 +1440,9 @@ def export_results_to_markdown(output_entries, stdscr):
     if dmf_entries:
         data_to_plot = {}
         for setting_idx, entries in dmf_entries.items():
-            desc = gen_description(setting_idx)
+            # Check if any entry used this setting_idx and came from JSON
+            used_json_for_setting = any(f"json-{setting_idx}" in e.command_name for e in output_entries)
+            desc = "" if used_json_for_setting else gen_description(setting_idx)
             markdown_lines.extend(
                 [
                     "",
@@ -1445,26 +1470,33 @@ def export_results_to_markdown(output_entries, stdscr):
             speed, top1, desc = data
             plot_data_with_desc(speed, top1, "Speed (t/s/u)", "Top-1 (%)", desc, model_device_name)
 
-    # Write to PERF.md
-    with open("PERF.md", "w") as f:
-        f.write("\n".join(markdown_lines) + "\n")
+    # Check if any entry came from a JSON config
+    used_json = any("json" in entry.command_name for entry in output_entries)
 
-    # Clear screen and show message
-    stdscr.clear()
+    # Only update PERF.md and display it if not using JSON
+    if not used_json:
+        with open("PERF.md", "w") as f:
+            f.write("\n".join(markdown_lines) + "\n")
 
-    # display 'PERF.md' using less
-    # Save current terminal state
-    curses.def_prog_mode()
-    # Exit curses temporarily
-    curses.endwin()
-    # Run less command
-    os.system(f"less -R PERF.md")
-    # Resume curses
-    curses.reset_prog_mode()
-    stdscr.refresh()
+        # Clear screen and show message
+        stdscr.clear()
 
-    # display helpful message at the bottom of the screen
-    stdscr.addstr(0, 0, f"Table written to {os.path.abspath('PERF.md')}")
+        # display 'PERF.md' using less
+        # Save current terminal state
+        curses.def_prog_mode()
+        # Exit curses temporaril
+        curses.endwin()
+        # Run less command
+        os.system(f"less -R PERF.md")
+        # Resume curses
+        curses.reset_prog_mode()
+        stdscr.refresh()
+
+        # display helpful message at the bottom of the screen
+        stdscr.addstr(0, 0, f"Table written to {os.path.abspath('PERF.md')}")
+    else:
+        stdscr.clear()
+
     stdscr.addstr(1, 0, f"Plots written to .svg files in {os.path.abspath('data_plots/')}")
     stdscr.addstr(2, 0, "Press any key to return...")
     stdscr.refresh()
@@ -1578,35 +1610,36 @@ def plot_data_with_desc(objective1, objective2, objective1_label, objective2_lab
     x = 0.1
     y = height_incr / 4
 
-    # add footnote to the plot
+    # add footnote to the plot only when json is not used
     for i in pareto_indices:
         desc_splits = point_desc[i].split("}")
-        fidelity_desc = desc_splits[1][2:] + "}"  # 2: slice to remove the leading ", "
-        # find substring "li_qkv_prefill" in fidelity_desc and insert a new line before it so the text fits in the plot
-        idx = fidelity_desc.find("li_qkv_prefill")
-        assert idx != -1, f"li_qkv_prefill not found in {fidelity_desc}"
-        fidelity_desc_part2 = " " * len("fidelity_cfg = {") + fidelity_desc[idx:]
-        plt.figtext(
-            x, y, f"{fidelity_desc_part2}", ha="left", transform=fig.dpi_scale_trans, family="monospace", fontsize=6
-        )
-        y += height_incr / 8
-        fidelity_desc_part1 = fidelity_desc[:idx]
-        plt.figtext(
-            x, y, f"{fidelity_desc_part1}", ha="left", transform=fig.dpi_scale_trans, family="monospace", fontsize=6
-        )
-        y += height_incr / 8
-        dtype_desc = desc_splits[0] + "}"
-        plt.figtext(x, y, f"{dtype_desc}", ha="left", transform=fig.dpi_scale_trans, family="monospace", fontsize=6)
-        y += height_incr / 8
-        plt.figtext(
-            x,
-            y,
-            f"{i}: {objective1_label}={objective1[i]}, {objective2_label}={objective2[i]}",
-            ha="left",
-            transform=fig.dpi_scale_trans,
-            fontsize=10,
-        )
-        y += height_incr / 3
+        if len(desc_splits) >= 2:
+            fidelity_desc = desc_splits[1][2:] + "}"  # 2: slice to remove the leading ", "
+            # find substring "li_qkv_prefill" in fidelity_desc and insert a new line before it so the text fits in the plot
+            idx = fidelity_desc.find("li_qkv_prefill")
+            assert idx != -1, f"li_qkv_prefill not found in {fidelity_desc}"
+            fidelity_desc_part2 = " " * len("fidelity_cfg = {") + fidelity_desc[idx:]
+            plt.figtext(
+                x, y, f"{fidelity_desc_part2}", ha="left", transform=fig.dpi_scale_trans, family="monospace", fontsize=6
+            )
+            y += height_incr / 8
+            fidelity_desc_part1 = fidelity_desc[:idx]
+            plt.figtext(
+                x, y, f"{fidelity_desc_part1}", ha="left", transform=fig.dpi_scale_trans, family="monospace", fontsize=6
+            )
+            y += height_incr / 8
+            dtype_desc = desc_splits[0] + "}"
+            plt.figtext(x, y, f"{dtype_desc}", ha="left", transform=fig.dpi_scale_trans, family="monospace", fontsize=6)
+            y += height_incr / 8
+            plt.figtext(
+                x,
+                y,
+                f"{i}: {objective1_label}={objective1[i]}, {objective2_label}={objective2[i]}",
+                ha="left",
+                transform=fig.dpi_scale_trans,
+                fontsize=10,
+            )
+            y += height_incr / 3
 
     x = fig_width / 2
     plt.figtext(

models/tt_transformers/tests/conftest.py
@@ -20,3 +20,11 @@ def pytest_addoption(parser):
         type=parse_optimizations,
         help="Precision and fidelity configuration diffs over default (i.e., accuracy)",
     )
+
+    parser.addoption(
+        "--decoder_config_file",
+        action="store",
+        default=None,
+        type=str,
+        help="Provide a JSON file defining per-decoder precision and fidelity settings",
+    )

models/tt_transformers/tests/test_accuracy.py
@@ -13,11 +13,11 @@ from models.tt_transformers.tt.common import (
     preprocess_inputs_prefill,
 )
 from models.tt_transformers.tt.model import Transformer
-from models.tt_transformers.tt.model_config import ModelArgs, ModelOptimizations
+from models.tt_transformers.tt.model_config import ModelArgs, DecodersPrecision, parse_decoder_json
 from pathlib import Path
 
 
-def get_accuracy_thresholds(base_model_name: str, device_name: str, optimizations: ModelOptimizations):
+def get_accuracy_thresholds(model_args, optimizations):
     """Parse accuracy thresholds from PERF.md for the given model, optimization mode, and device."""
     # Read PERF.md
     perf_file = Path(__file__).parent.parent / "PERF.md"
@@ -26,10 +26,15 @@ def get_accuracy_thresholds(base_model_name: str, device_name: str, optimization
 
     # Split into sections based on optimization mode
     sections = content.split("## ")
-    target_section = next(s for s in sections if s.lower().startswith(f"{optimizations.__name__}\n"))
+    if callable(optimizations):
+        optimizations = optimizations(model_args)
+    first_decoder_conf = optimizations.decoder_optimizations[0]
+    target_section = next(s for s in sections if s.lower().startswith(f"{first_decoder_conf.__name__}\n"))
 
     # Parse the table and find the row for our model and device
     # Potential lines have the form "| Llama3.1-8b    | T3K    | 91        | 99        | 49.8          |"
+    base_model_name = model_args.base_model_name
+    device_name = model_args.device_name
     correct_line = (
         lambda line: "|" in line
         and base_model_name.lower() in line.split("|")[1].strip().lower()
@@ -76,9 +81,10 @@ def get_accuracy_thresholds(base_model_name: str, device_name: str, optimization
 @pytest.mark.parametrize(
     "optimizations",
     [
-        ModelOptimizations.accuracy,
-        ModelOptimizations.performance,
+        lambda model_args: DecodersPrecision.performance(model_args.n_layers, model_args.model_name),
+        lambda model_args: DecodersPrecision.accuracy(model_args.n_layers, model_args.model_name),
     ],
+    ids=["performance", "accuracy"],
 )
 @pytest.mark.parametrize(
     "paged_attention",
@@ -129,7 +135,11 @@ def test_tt_model_acc(
 
     mesh_device.enable_async(True)
 
-    optimizations = request.config.getoption("--optimizations") or optimizations
+    json_config_file = request.config.getoption("--decoder_config_file")
+    if json_config_file:
+        optimizations = parse_decoder_json(json_config_file)
+    else:
+        optimizations = request.config.getoption("--optimizations") or optimizations
 
     # Load model args and tokenizer
     model_args = ModelArgs(mesh_device, optimizations=optimizations, max_batch_size=batch_size, max_seq_len=max_seq_len)
@@ -446,17 +456,17 @@ def test_tt_model_acc(
                 logger.info(f"{error['position']}: {context}[{incorrect}] != [{expected}], true: [{true_word}]")
 
     if use_reference_file:
-        # Get accuracy thresholds from PERF.md
-        min_top1_acc, min_top5_acc = get_accuracy_thresholds(
-            model_args.base_model_name,
-            model_args.device_name,
-            optimizations,
-        )
+        if not json_config_file:
+            # Get accuracy thresholds from PERF.md, unless the configuration is from a json
+            min_top1_acc, min_top5_acc = get_accuracy_thresholds(
+                model_args,
+                optimizations,
+            )
+            assert (
+                total_top1_acc >= min_top1_acc
+            ), f"Top-1 accuracy {total_top1_acc:.1f}% is too low (expected >={min_top1_acc}%)"
+            assert (
+                total_top5_acc >= min_top5_acc
+            ), f"Top-5 accuracy {total_top5_acc:.1f}% is too low (expected >={min_top5_acc}%)"
 
         logger.info(f"Top-1: {total_top1_acc:.0f}% | Top-5: {total_top5_acc:.0f}%")
-        assert (
-            total_top1_acc >= min_top1_acc
-        ), f"Top-1 accuracy {total_top1_acc:.1f}% is too low (expected >={min_top1_acc}%)"
-        assert (
-            total_top5_acc >= min_top5_acc
-        ), f"Top-5 accuracy {total_top5_acc:.1f}% is too low (expected >={min_top5_acc}%)"

models/tt_transformers/tests/test_chunked_generation.py
@@ -12,7 +12,7 @@ from models.tt_transformers.tt.common import (
     num_blocks_in_seq,
 )
 from models.tt_transformers.tt.model import Transformer
-from models.tt_transformers.tt.model_config import ModelArgs, ModelOptimizations
+from models.tt_transformers.tt.model_config import ModelArgs, DecodersPrecision
 from models.tt_transformers.tt.generator import Generator
 from models.demos.t3000.llama2_70b.reference.llama.llama31_8b.model import Transformer as ReferenceTransformer
 from models.utility_functions import (
@@ -50,7 +50,9 @@ from models.utility_functions import skip_for_grayskull
 @pytest.mark.parametrize(
     "optimizations",
     [
-        pytest.param(ModelOptimizations.accuracy, id="accuracy"),
+        pytest.param(
+            lambda model_args: DecodersPrecision.accuracy(model_args.n_layers, model_args.model_name), id="accuracy"
+        ),
     ],
 )
 def test_chunked_prefill_single_user(
@@ -64,6 +66,7 @@ def test_chunked_prefill_single_user(
     reset_seeds,
     ensure_gc,
     is_ci_env,
+    request,
 ):
     mesh_device.enable_async(True)
 
@@ -71,10 +74,11 @@ def test_chunked_prefill_single_user(
     batch_size = 1  # For prefill we only support batch_size = 1
 
     # This sets the minimum PCC for each iteration based on optimization mode
-    if optimizations == ModelOptimizations.accuracy:
+    test_id = request.node.callspec.id
+    if "accuracy" in test_id:
         pcc = 0.91  # TODO Look on improving PCC
     else:  # performance mode
-        assert optimizations == ModelOptimizations.performance
+        assert "performance" in test_id
         pcc = 0.869  # TODO Look on improving PCC
 
     model_args = ModelArgs(mesh_device, max_batch_size=batch_size, optimizations=optimizations, max_seq_len=seq_len)

commit 50ac07bb8cb71179048c7c84335496ac329712b0

tt_transformets/tt/attention: Fix typo (#21081)
### Problem description
Small typo in `tt_transformets/tt/attention.py` introduced in
https://github.com/tenstorrent/tt-metal/pull/20643
It seems that this code-path is not tested in the post-commit tests

### What's changed
This PR fixes this typo

Signed-off-by: Tomer Levin <tlevin@tenstorrent.com>

models/tt_transformers/tt/attention.py
@@ -343,7 +343,7 @@ class Attention(LightweightModule):
             memory_config=ttnn.L1_WIDTH_SHARDED_MEMORY_CONFIG,
             program_config=self.model_config["XQKV_DECODE_PROGCFG"],
             compute_kernel_config=self.li_qkv_decode_compute_kernel_cfg,
-            dtype=xself.ccl_dtype if self.TG else self.activation_dtype or ttnn.bfloat16,
+            dtype=self.ccl_dtype if self.TG else self.activation_dtype or ttnn.bfloat16,
         )
         # FIXME: File bug against dram-sharded matmuls with bias
         if self.wqkv_bias_decode: