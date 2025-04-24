c9636af2ada429f1904a9b2e3f1fd1e2e4e2feb1
punith.sekar <punith.sekar@multicorewareinc.com>
2025-04-24 09:59:13 +0000

    #0: move the predict outside model

--- models/experimental/functional_pointpillars/reference/mvx_faster_rcnn.py
+++ models/experimental/functional_pointpillars/reference/mvx_faster_rcnn.py
@@ -449,9 +449,9 @@ class MVXFasterRCNN(nn.Module):
         batch_input_metas = batch_data_samples  # modified and passed
         img_feats, pts_feats = self.extract_feat(batch_inputs_dict, batch_input_metas)
         if pts_feats and self.with_pts_bbox:
-            results_list_3d = self.pts_bbox_head.predict(pts_feats, batch_data_samples, **kwargs)
+            outs = self.pts_bbox_head(pts_feats)
         else:
-            results_list_3d = None
+            outs = None
 
         if img_feats and self.with_img_bbox:
             # TODO check this for camera modality
@@ -461,4 +461,4 @@ class MVXFasterRCNN(nn.Module):
 
         # print("results_list_3d", results_list_3d)
         # detsamples = self.add_pred_to_datasample(batch_data_samples, results_list_3d, results_list_2d)
-        return results_list_3d
+        return outs
