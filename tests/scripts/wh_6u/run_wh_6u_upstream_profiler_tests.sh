6646f35f6117db54121d60005a3d7593573f1ab9
Raymond Kim <rkim@tenstorrent.com>
2025-04-24 17:26:22 -0400

    Add Ethernet API ubenchmark tests

--- tests/scripts/wh_6u/run_wh_6u_upstream_profiler_tests.sh
+++ tests/scripts/wh_6u/run_wh_6u_upstream_profiler_tests.sh
@@ -3,3 +3,8 @@ set -euo pipefail
 
 echo "[upstream-profiler-tests] Running sanity fabric tests"
 pytest tests/tt_metal/microbenchmarks/ethernet/test_fabric_edm_bandwidth.py -m sanity_6u
+
+echo "[upstream-profiler-tests] Running Ethernet API tests"
+eth_api_iterations=5
+pytest tests/tt_metal/microbenchmarks/ethernet/test_all_ethernet_links_latency.py::test_erisc_latency_uni_dir --num-iterations $eth_api_iterations
+pytest tests/tt_metal/microbenchmarks/ethernet/test_all_ethernet_links_bandwidth.py::test_erisc_bw_uni_dir --num-iterations $eth_api_iterations
