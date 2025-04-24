1f441f762701a01473f062051b3fb70ebd586bdc
Raymond Kim <rkim@tenstorrent.com>
2025-04-24 18:48:37 -0400

    Add some models unit tests that don't need the weights, although one seems to have some program cache error. Can't find it on CI

--- tests/scripts/wh_6u/run_wh_6u_upstream_tests.sh
+++ tests/scripts/wh_6u/run_wh_6u_upstream_tests.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 echo "[upstream-tests] Running minimal model unit tests"
 pytest tests/ttnn/unit_tests/operations/ccl/test_ccl_async_TG_llama.py
+pytest tests/ttnn/unit_tests/operations/test_prefetcher_TG.py
+# pytest tests/tt_eager/python_api_testing/unit_testing/misc/test_matmul_1d_gather_in0.py::test_matmul_1d_ring_llama_perf Failing with a no program cache found error
+pytest tests/ttnn/unit_tests/operations/ccl/test_ccl_async_TG_llama.py
 # pytest tests/ttnn/unit_tests/operations/ccl/test_minimals.py hang???
 
 echo "[upstream-tests] running metalium section. Note that skips should be treated as failures"
