428e47e5355d5c6035414830fbeff34c5cb6d696
myply <917561109@qq.com>
2025-04-24 23:35:34 -0700

    optimize circular buffer allocation in flash decode

--- ttnn/cpp/ttnn/operations/transformer/sdpa_decode/device/sdpa_decode_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/transformer/sdpa_decode/device/sdpa_decode_program_factory.cpp
@@ -351,7 +351,7 @@ operation::ProgramWithCallbacks sdpa_decode_multi_core(
     uint32_t im_tile_size = im_tile.get_tile_size(im_df);
     uint32_t stats_tile_size = stats_tile.get_tile_size(stats_df);
 
-    uint32_t intermed_output_tiles = (out0_t + 2 * PNHt) * (num_cores_per_batch - 1);
+    uint32_t intermed_output_tiles = (out0_t + 2 * PNHt) * (num_cores_per_head - 1);
 
     uint32_t pos_tensor_tile_size = 0;
     uint32_t log2_page_size = 0;
