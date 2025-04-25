8ddcc19bc90edd6c922fa3326ad7936d65d23926
Salar Hosseini <159165450+skhorasganiTT@users.noreply.github.com>
2025-04-25 16:31:12 -0400

    [skip ci] Slightly update perf target for Falcon7b-t3k-2048 (#21241)
    ### Ticket
    N/A
    
    ### Problem description
    - Missed a target update in
    https://github.com/tenstorrent/tt-metal/pull/21232
    
    ### What's changed
    - Updated Falcon7b-t3k-2048 prefill target
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [x] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [x] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [x] New/Existing tests provide coverage for changes
    
    Signed-off-by: Salar <skhorasgani@tenstorrent.com>

--- models/demos/t3000/falcon7b/demo_t3000.py
+++ models/demos/t3000/falcon7b/demo_t3000.py
@@ -12,7 +12,7 @@ from models.utility_functions import is_wormhole_b0
     (
         (True, 128, {"prefill_t/s": 11070, "decode_t/s": 3710, "decode_t/s/u": 14.5}, False, None),
         (True, 1024, {"prefill_t/s": 12200, "decode_t/s": 3434, "decode_t/s/u": 13.4}, False, None),
-        (True, 2048, {"prefill_t/s": 10770, "decode_t/s": 3203, "decode_t/s/u": 12.5}, False, None),
+        (True, 2048, {"prefill_t/s": 10700, "decode_t/s": 3203, "decode_t/s/u": 12.5}, False, None),
         (True, 128, None, False, None),
         (True, 1024, None, False, None),
         (True, 2048, None, False, None),
