a185be9159298d50b12c312e185dddd1deaff993
Raymond Kim <rkim@tenstorrent.com>
2025-04-25 13:10:14 -0400

    WAITTTTT no need? https://github.com/tenstorrent/tt-metal/actions/runs/14653126314/job/41157818666
    Revert "Skip ethernet tests for now as they have some key id error"
    
    This reverts commit 2a6eca8c27c68cc3c4a13aaba6b11f21d39fe65c.

--- tests/scripts/wh_6u/run_wh_6u_upstream_profiler_tests.sh
+++ tests/scripts/wh_6u/run_wh_6u_upstream_profiler_tests.sh
@@ -6,5 +6,5 @@ pytest tests/tt_metal/microbenchmarks/ethernet/test_fabric_edm_bandwidth.py -m s
 
 echo "[upstream-profiler-tests] Running Ethernet API tests"
 eth_api_iterations=5
-# ARCH_NAME=wormhole_b0 pytest tests/tt_metal/microbenchmarks/ethernet/test_all_ethernet_links_latency.py::test_erisc_latency_uni_dir --num-iterations $eth_api_iterations key_id error
-# ARCH_NAME=wormhole_b0 pytest tests/tt_metal/microbenchmarks/ethernet/test_all_ethernet_links_bandwidth.py::test_erisc_bw_uni_dir --num-iterations $eth_api_iterations key_id error
+ARCH_NAME=wormhole_b0 pytest tests/tt_metal/microbenchmarks/ethernet/test_all_ethernet_links_latency.py::test_erisc_latency_uni_dir --num-iterations $eth_api_iterations
+ARCH_NAME=wormhole_b0 pytest tests/tt_metal/microbenchmarks/ethernet/test_all_ethernet_links_bandwidth.py::test_erisc_bw_uni_dir --num-iterations $eth_api_iterations
