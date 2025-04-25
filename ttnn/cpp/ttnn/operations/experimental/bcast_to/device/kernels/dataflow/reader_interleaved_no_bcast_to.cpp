f63edd844321c2c4a6c685c3942eca496f3b702c
Debin Chen <dchen@tenstorrent.com>
2025-04-24 21:15:02 -0700

    #16015: Support new op ttnn.experimental.broadcast_to (#19759)
    ### Ticket
    Link to Github Issue
    (https://github.com/tenstorrent/tt-metal/issues/16015)
    
    ### Problem description
    Add broadcast_to op that is the counter part of torch.broadcast_to.
    However, currently metal framework does not support tensor strided view,
    so tensor is allocated.
    
    ### What's changed
    The broadcast logic is adopted from eltwise binary op broadcast logic,
    using subtile broadcast. All new files for this PR since it is a new op.
    
    Currently it is an experimental op with basic support as following
    
          Data Type
    	      Bfloat16
    	      Float32
    
          Tensor Shape
    	      1D to 4D
          
          Memory Layout
    	      Tile
          
          Memory Config
    	      Interleaved (DRAM / L1)
    
    Performance Profiling
    
    Before this broadcast_to op, ttnn.repeat can be used as workaround.
    However, from profiling result, ttnn.repeat can be much slower than
    broadcast_to, at least for large tensor. Following is the profiling
    result
    
    | Input | Output | Subtile broadcast type | Dimension to broadcast |
    | ---------------- | ------------------ | ---------------------- |
    ---------------------- |
    | (1, 1, 256, 256) | (4, 128, 256, 256) | no change | N C |
    | (4, 128, 1, 1) | (4, 128, 256, 256) | scalar | H W |
    | (4, 1, 256, 1) | (4, 128, 256, 256) | col | C W |
    | (4, 1, 1, 256) | (4, 128, 256, 256) | row | C H |
    
    | Subtile broadcast type | No change | Scalar | Col | Row |
    | ---------------------------------------------------------- | ---------
    | ------ | ----- | ----- |
    | Repeat kernel / broadcast_to kernel duration (in slowness) | 3.27x |
    3.73x | 2.48x | 1.99x |
    
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [ ] New/Existing tests provide coverage for changes

---
+++ ttnn/cpp/ttnn/operations/experimental/bcast_to/device/kernels/dataflow/reader_interleaved_no_bcast_to.cpp
@@ -0,0 +1,57 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdint.h>
+
+#include "dataflow_api.h"
+
+void kernel_main() {
+    uint32_t arg_index = 0;
+    uint32_t src_addr = get_arg_val<uint32_t>(arg_index++);
+    uint32_t start_n = get_arg_val<uint32_t>(arg_index++);
+    uint32_t start_c = get_arg_val<uint32_t>(arg_index++);
+    uint32_t start_t = get_arg_val<uint32_t>(arg_index++);
+    uint32_t start_th = get_arg_val<uint32_t>(arg_index++);
+    uint32_t start_tw = get_arg_val<uint32_t>(arg_index++);
+    uint32_t num_tiles = get_arg_val<uint32_t>(arg_index++);
+    uint32_t n_stride = get_arg_val<uint32_t>(arg_index++);
+    uint32_t c_stride = get_arg_val<uint32_t>(arg_index++);
+    uint32_t N = get_arg_val<uint32_t>(arg_index++);
+    uint32_t C = get_arg_val<uint32_t>(arg_index++);
+    uint32_t Ht = get_arg_val<uint32_t>(arg_index++);
+    uint32_t Wt = get_arg_val<uint32_t>(arg_index++);
+
+    constexpr bool src_is_dram = get_compile_time_arg_val(0) == 1;
+    constexpr auto cb_id_src = get_compile_time_arg_val(1);
+    constexpr uint32_t onetile = 1;
+
+    const uint32_t src_tile_bytes = get_tile_size(cb_id_src);
+    const DataFormat src_data_format = get_dataformat(cb_id_src);
+    const InterleavedAddrGenFast<src_is_dram> src = {
+        .bank_base_address = src_addr, .page_size = src_tile_bytes, .data_format = src_data_format};
+
+    uint32_t HtWt = Ht * Wt;
+
+    // this is the INPUT tile offset
+    uint32_t tile_offset = start_n * n_stride + start_c * c_stride;
+    uint32_t next_batch_shift = n_stride - c_stride * C;
+    uint32_t next_channel_shift = c_stride - HtWt;
+
+    uint32_t num_tiles_read = 0;
+    tile_offset += start_t;
+    for (uint32_t n = start_n; n < N && num_tiles_read < num_tiles; ++n, start_c = 0) {
+        for (uint32_t c = start_c; c < C && num_tiles_read < num_tiles; ++c, start_t = 0) {
+            for (uint32_t t = start_t; t < HtWt && num_tiles_read < num_tiles; ++t) {
+                cb_reserve_back(cb_id_src, onetile);
+                uint32_t l1_write_addr_src = get_write_ptr(cb_id_src);
+                noc_async_read_tile(tile_offset++, src, l1_write_addr_src);
+                noc_async_read_barrier();
+                cb_push_back(cb_id_src, onetile);
+                ++num_tiles_read;
+            }
+            tile_offset += next_channel_shift;
+        }
+        tile_offset += next_batch_shift;
+    }
+}
