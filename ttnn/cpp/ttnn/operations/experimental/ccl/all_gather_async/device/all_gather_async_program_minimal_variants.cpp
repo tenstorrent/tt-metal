b9857e845c88755e2c560d6bfda8c73b8f3b27ee
daminakaTT <daminaka@tenstorrent.com>
2025-04-25 06:33:38 +0000

    try CI

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/device/all_gather_async_program_minimal_variants.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/device/all_gather_async_program_minimal_variants.cpp
@@ -68,7 +68,10 @@ tt::tt_metal::operation::ProgramWithCallbacks all_gather_async_minimal_interleav
     const GlobalSemaphore& semaphore,
     const std::optional<tt::tt_metal::SubDeviceId>& sub_device_id) {
     tt::tt_metal::Program program{};
-    auto mesh_device = input_tensor.mesh_device();
+    IDevice* mesh_device = input_tensor.mesh_device();
+    if (!mesh_device) {
+        mesh_device = input_tensor.device();
+    }
     const bool enable_async_output_tensor = false;
     const bool enable_persistent_fabric_mode = true;
     bool is_first_chip = ring_index == 0;
