1375455352b1b612b8b7ccd5ad9c9f348aa44672
Raymond Kim <rkim@tenstorrent.com>
2025-04-25 14:51:21 -0400

    Don't run ethernet tests still... something is wrong

--- tests/scripts/wh_6u/run_wh_6u_upstream_profiler_tests.sh
+++ tests/scripts/wh_6u/run_wh_6u_upstream_profiler_tests.sh
@@ -6,5 +6,6 @@ pytest tests/tt_metal/microbenchmarks/ethernet/test_fabric_edm_bandwidth.py -m s
 
 echo "[upstream-profiler-tests] Running Ethernet API tests"
 eth_api_iterations=5
-ARCH_NAME=wormhole_b0 pytest tests/tt_metal/microbenchmarks/ethernet/test_all_ethernet_links_latency.py::test_erisc_latency_uni_dir --num-iterations $eth_api_iterations
-ARCH_NAME=wormhole_b0 pytest tests/tt_metal/microbenchmarks/ethernet/test_all_ethernet_links_bandwidth.py::test_erisc_bw_uni_dir --num-iterations $eth_api_iterations
+# shit still broke on main
+# ARCH_NAME=wormhole_b0 pytest tests/tt_metal/microbenchmarks/ethernet/test_all_ethernet_links_latency.py::test_erisc_latency_uni_dir --num-iterations $eth_api_iterations
+# ARCH_NAME=wormhole_b0 pytest tests/tt_metal/microbenchmarks/ethernet/test_all_ethernet_links_bandwidth.py::test_erisc_bw_uni_dir --num-iterations $eth_api_iterations
