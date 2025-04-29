5373a674429ae2981759c83d6bf4940f44dabbd3
Almeet Bhullar <abhullar@tenstorrent.com>
2025-04-29 22:23:03 +0000

    Disable tests failing on harvested BH

--- tests/tt_eager/python_api_testing/unit_testing/misc/test_transpose.py
+++ tests/tt_eager/python_api_testing/unit_testing/misc/test_transpose.py
@@ -11,7 +11,7 @@ import ttnn
 from loguru import logger
 from models.utility_functions import is_grayskull, is_blackhole, torch_random
 from tests.tt_eager.python_api_testing.sweep_tests.comparison_funcs import comp_pcc, comp_equal
-from models.utility_functions import skip_for_grayskull, skip_for_blackhole, run_for_blackhole
+from models.utility_functions import skip_for_grayskull, skip_for_blackhole, run_for_blackhole, skip_for_wormhole_b0
 from tests.ttnn.utils_for_testing import assert_with_pcc
 
 
@@ -56,7 +56,7 @@ def transpose(
         assert device.num_program_cache_entries() == expected_program_cache_size
 
 
-@run_for_blackhole()
+@pytest.mark.skip("Failing on harvested BH, see #21147 (never ran on WH)")
 def test_fold_transpose(device, use_program_cache):
     N = 32
     C = 4
