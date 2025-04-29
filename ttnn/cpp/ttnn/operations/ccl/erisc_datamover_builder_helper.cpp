9958b7c18d424d082c13da5e4acba7c79b22f3cd
yugaoTT <yugao@tenstorrent.com>
2025-04-29 16:10:01 +0000

    #0: fixes

--- ttnn/cpp/ttnn/operations/ccl/erisc_datamover_builder_helper.cpp
+++ ttnn/cpp/ttnn/operations/ccl/erisc_datamover_builder_helper.cpp
@@ -191,27 +191,50 @@ EdmLineFabricOpInterface::EdmLineFabricOpInterface(
             start_bidirectional_device_index = 0;
             end_bidirectional_device_index = device_sequence.size();
         }
-
         for (size_t i = 0; i < device_sequence.size(); i++) {
+            tt::log_info("device_sequence id {}", device_sequence[i]->id());
+        }
+
+        uint32_t fwd_edm_start_index = 0;
+        uint32_t fwd_edm_end_index = (topology == Topology::Ring) ? device_sequence.size() : device_sequence.size() - 1;
+        uint32_t bwd_edm_start_index = (topology == Topology::Ring) ? 0 : 1;
+        uint32_t bwd_edm_end_index = device_sequence.size();
+
+        for (size_t i = fwd_edm_start_index; i < fwd_edm_end_index; i++) {
             const size_t num_links = edm_builders_forward_direction.at(device_sequence[i]->id()).size();
             auto& forward_direction_edm = edm_builders_forward_direction.at(device_sequence[i]->id());
-            auto& backward_direction_edm = edm_builders_backward_direction.at(device_sequence[i]->id());
 
             for (size_t l = 0; l < num_links; l++) {
                 auto& edm_fwd = forward_direction_edm[l];
-                auto& edm_bwd = backward_direction_edm[l];
 
                 auto edm_noc_vc = edm_fwd.config.DEFAULT_NOC_VC + (l & 0x1);
                 edm_fwd.config.edm_noc_vc = edm_noc_vc;
-                edm_bwd.config.edm_noc_vc = edm_noc_vc;
 
                 log_info(
                     tt::LogTest,
-                    "device {} edm_fwd {} {} has vc {} edm_bwd {} {} has vc {} on link {}",
+                    "device {} edm_fwd {} {} has vc {} on link {}",
                     edm_fwd.my_chip_id,
                     edm_fwd.my_noc_x,
                     edm_fwd.my_noc_y,
                     edm_noc_vc,
+                    l);
+            }
+        }
+
+        for (size_t i = bwd_edm_start_index; i < bwd_edm_end_index; i++) {
+            const size_t num_links = edm_builders_backward_direction.at(device_sequence[i]->id()).size();
+            auto& backward_direction_edm = edm_builders_backward_direction.at(device_sequence[i]->id());
+
+            for (size_t l = 0; l < num_links; l++) {
+                auto& edm_bwd = backward_direction_edm[l];
+
+                auto edm_noc_vc = edm_bwd.config.DEFAULT_NOC_VC + (l & 0x1);
+                edm_bwd.config.edm_noc_vc = edm_noc_vc;
+
+                log_info(
+                    tt::LogTest,
+                    "device {} edm_bwd {} {} has vc {} on link {}",
+                    edm_bwd.my_chip_id,
                     edm_bwd.my_noc_x,
                     edm_bwd.my_noc_y,
                     edm_noc_vc,
