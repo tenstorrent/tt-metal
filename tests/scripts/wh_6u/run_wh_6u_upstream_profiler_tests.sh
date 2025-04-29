c4dff90abac74394142bf4dbc8bba2297d08bd04
Raymond Kim <raymond.kim243@outlook.com>
2025-04-29 08:11:37 -0400

    Remove EDM b/w tests for now because they fail consistently on g05glx6u02. Can add back easily later

--- tests/scripts/wh_6u/run_wh_6u_upstream_profiler_tests.sh
+++ tests/scripts/wh_6u/run_wh_6u_upstream_profiler_tests.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 echo "[upstream-profiler-tests] Running sanity fabric tests"
-pytest tests/tt_metal/microbenchmarks/ethernet/test_fabric_edm_bandwidth.py -m sanity_6u
+# pytest tests/tt_metal/microbenchmarks/ethernet/test_fabric_edm_bandwidth.py -m sanity_6u Issue https://github.com/tenstorrent/tt-metal/issues/21360
 
 echo "[upstream-profiler-tests] Running Ethernet API tests"
 eth_api_iterations=5
