d8c518fb84aae386663551981a901e784ce050b0
Viacheslav Melnykov <vmelnykov@tenstorrent.com>
2025-04-29 19:32:35 +0000

    init commit

---
+++ tt-train/sources/ttml/metal/ops/cross_entropy_bw/cross_entropy_bw.cpp
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "cross_entropy_bw.hpp"
+
+#include "device/cross_entropy_bw_device_operation.hpp"
+
+namespace ttml::metal::ops::cross_entropy_bw {
+
+ttnn::Tensor CrossEntropyBackwardOperation::invoke(
+    const ttnn::Tensor& input_tensor, const ttnn::Tensor& target_tensor) {
+    auto result = ttnn::prim::ttml_cross_entropy_bw(input_tensor, target_tensor);
+    return result;
+}
+}  // namespace ttml::metal::ops::cross_entropy_bw
