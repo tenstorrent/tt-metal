5346160e8576fbd4a4826805319bf9a247714b66
Anasuya G Nair <mcw-anair@ext.tenstorrent.com>
2025-04-29 20:38:37 +0530

    #21115: Add uint32 support for ttnn.add (#21116)
    ### Ticket
    #21115 
    
    ### Problem description
    Provide uint32 support for ttnn.add with LLK implementation
    
    ### What's changed
    - Added WH_B0 and BH LLK implementations for ttnn.add to support uint32
    datatype.
    - Added support in binary and binary_ng infra.
    - Added support for uint32 bcast.
    
    ### BH Test Results
    <img width="1290" alt="Screenshot 2025-04-29 at 19 29 24"
    src="https://github.com/user-attachments/assets/a63e74b7-143a-43da-8eaa-e2eb87495b61"
    />
    
    
    ### Checklist
    - [x] Tests pass in BH local
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14723749310)
    - PASSED
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14643499771)
    - PASSED
    - [x] New/Existing tests provide coverage for changes

---
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_add_uint32.h
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "ckernel_addrmod.h"
+#include "sfpi.h"
+
+namespace ckernel {
+namespace sfpu {
+
+template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
+inline void add_uint32(const uint dst_offset) {
+#pragma GCC unroll 8
+    for (int d = 0; d < ITERATIONS; d++) {
+        constexpr uint dst_tile_size = 64;
+        // operand A - uint32
+        TTI_SFPLOAD(p_sfpu::LREG0, INT32, ADDR_MOD_7, 0);
+        // operand B - uint32
+        TT_SFPLOAD(p_sfpu::LREG1, INT32, ADDR_MOD_7, dst_offset * dst_tile_size);
+
+        TTI_SFPIADD(0, p_sfpu::LREG1, p_sfpu::LREG0, INT32);
+        TTI_SFPSTORE(p_sfpu::LREG0, INT32, ADDR_MOD_7, 0);
+
+        sfpi::dst_reg++;
+    }
+}
+
+}  // namespace sfpu
+}  // namespace ckernel
--- ttnn/cpp/ttnn/operations/eltwise/binary_ng/device/binary_ng_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/binary_ng/device/binary_ng_device_operation.cpp
@@ -13,7 +13,10 @@ bool is_binary_sfpu_op(BinaryOpType val, DataType a, DataType b) {
     using enum BinaryOpType;
     using enum DataType;
     switch (val) {
-        case ADD: return ((a == FLOAT32 && b == FLOAT32) || (a == INT32 && b == INT32)) || (a == UINT16 && b == UINT16);
+        case ADD:
+            return (
+                (a == FLOAT32 && b == FLOAT32) || (a == INT32 && b == INT32) || (a == UINT32 && b == UINT32) ||
+                (a == UINT16 && b == UINT16));
         case SUB: return ((a == FLOAT32 && b == FLOAT32) || (a == INT32 && b == INT32));
         case MUL:
         case DIV:
