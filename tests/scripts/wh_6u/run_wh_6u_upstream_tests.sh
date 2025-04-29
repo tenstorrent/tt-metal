19a3f191e7379288339f6e43fa18cbd36e5c4573
Raymond Kim <109366641+tt-rkim@users.noreply.github.com>
2025-04-29 09:59:21 -0400

    #21067: [skip ci] Add initial scaffolding for deliverable docker containers which contain Metalium/TT-NN tests that upstream teams can run out-of-the-box to verify HW/FW releases (#21173)
    ### Ticket
    
    #21067
    
    ### Problem description
    
    We need to provide a litany of tests to upstream teams.
    
    These kinds of tests include:
    
    - metal unit tests
    - metal unit tests w/ profiler
    - python unit tests
    - python unit tests w/ profiler
    - long running stress tests
    - tests that need cached weights from MLPerf, of which are manually
    downloaded and [behind a
    TOS-wall](https://huggingface.co/meta-llama/Llama-3.3-70B-Instruct)...
    
    ### What's changed
    
    - Set precedent of creating docker containers that will contain the
    tests that people can easily run.
    - Two containers: one with profiler build, one without
    - Two scripts for people to plug tests into. Unfortunately I hate that
    there isn't a SSOT so that CI can run these too... or well could they?
    - Future plan:
    - Upstream teams should just need their devices, FW, driver, and Docker
    installed on an `amd64` system
    - The plan is to create a script checked into repo that upstream teams
    can call via something like `https://github.com/raw/path/to/script.sh |
    sh - ...` or something like that. May have some options in there
    
    ### Checklist
    - [ ]
    https://github.com/tenstorrent/tt-metal/actions/workflows/upstream-tests.yaml:
    https://github.com/tenstorrent/tt-metal/actions/runs/14653126314
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [ ] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: Raymond Kim <raymond.kim243@outlook.com>

---
+++ tests/scripts/wh_6u/run_wh_6u_upstream_tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "[upstream-tests] Running minimal model unit tests"
+pytest tests/ttnn/unit_tests/operations/ccl/test_ccl_async_TG_llama.py
+pytest tests/ttnn/unit_tests/operations/test_prefetcher_TG.py
+pytest tests/tt_eager/python_api_testing/unit_testing/misc/test_matmul_1d_gather_in0.py::test_matmul_1d_ring_llama_perf
+pytest tests/ttnn/unit_tests/operations/ccl/test_ccl_async_TG_llama.py
+# pytest tests/ttnn/unit_tests/operations/ccl/test_minimals.py hang???
+
+echo "[upstream-tests] running metalium section. Note that skips should be treated as failures"
+TT_METAL_SKIP_ETH_CORES_WITH_RETRAIN=1 TT_METAL_SLOW_DISPATCH_MODE=1 ./build/test/tt_metal/tt_fabric/fabric_unit_tests --gtest_filter="ControlPlaneFixture.TestQuantaGalaxyControlPlaneInit"
+TT_METAL_SKIP_ETH_CORES_WITH_RETRAIN=1 ./build/test/tt_metal/unit_tests_dispatch --gtest_filter="CommandQueueSingleCardFixture.*"
+TT_METAL_SKIP_ETH_CORES_WITH_RETRAIN=1 ./build/test/tt_metal/unit_tests_dispatch --gtest_filter="CommandQueueSingleCardProgramFixture.*"
+TT_METAL_SKIP_ETH_CORES_WITH_RETRAIN=1 ./build/test/tt_metal/unit_tests_dispatch --gtest_filter="CommandQueueSingleCardBufferFixture.ShardedBufferLarge*ReadWrites"
+TT_METAL_SLOW_DISPATCH_MODE=1 ./build/test/tt_metal/tt_fabric/fabric_unit_tests --gtest_filter="Fabric2D*Fixture.*"
