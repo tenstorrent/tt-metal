c3d6cb5eda326f69ea3839b69a4e8afb35c62fa9
mcw-anasuya <mcw-anair@ext.tenstorrent.com>
2025-04-29 15:21:33 +0000

    #20979: Update and cleanup ckernels

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_add_uint16.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_add_uint16.h
@@ -4,11 +4,8 @@
 
 #pragma once
 
-#include "ckernel.h"
-#include "ckernel_defs.h"
+#include "ckernel_addrmod.h"
 #include "sfpi.h"
-
-using namespace sfpi;
 
 namespace ckernel {
 namespace sfpu {
@@ -19,14 +16,14 @@ inline void add_uint16(const uint dst_offset) {
     for (int d = 0; d < ITERATIONS; d++) {
         constexpr uint dst_tile_size = 64;
         // operand A - uint16
-        TTI_SFPLOAD(p_sfpu::LREG0, 6, ADDR_MOD_3, 0);
+        TTI_SFPLOAD(p_sfpu::LREG0, LO16, ADDR_MOD_3, 0);
         // operand B - uint16
-        TT_SFPLOAD(p_sfpu::LREG1, 6, ADDR_MOD_3, dst_offset * dst_tile_size);
+        TT_SFPLOAD(p_sfpu::LREG1, LO16, ADDR_MOD_3, dst_offset * dst_tile_size);
 
         TTI_SFPIADD(0, p_sfpu::LREG1, p_sfpu::LREG0, 4);
-        TTI_SFPSTORE(p_sfpu::LREG0, 6, ADDR_MOD_3, 0);
+        TTI_SFPSTORE(p_sfpu::LREG0, LO16, ADDR_MOD_3, 0);
 
-        dst_reg++;
+        sfpi::dst_reg++;
     }
 }
 
