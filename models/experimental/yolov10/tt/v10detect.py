06b7a2c68b20a3633e2f1760b6775265346b007b
vigneshkumarkeerthivasan <133634754+vigneshkeerthivasanx@users.noreply.github.com>
2025-04-29 19:54:36 +0530

    #17935: Yolov10x model performance tests (#20410)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/17935)
    
    ### Problem description
    Add Performance tests for Yolov10x model.
    
    ### What's changed
    
    - Added e2e and Device performance tests for Yolov10x model.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    -
    [Passing](https://github.com/tenstorrent/tt-metal/actions/runs/14658145817)
    - [x]
    [Nightly](https://github.com/tenstorrent/tt-metal/actions/workflows/fast-dispatch-full-regressions-and-models.yaml)
    -
    [Passing](https://github.com/tenstorrent/tt-metal/actions/runs/14658146894)
    - [x] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    -
    [Passing](https://github.com/tenstorrent/tt-metal/actions/runs/14658151389)
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    -
    [Passing](https://github.com/tenstorrent/tt-metal/actions/runs/14658155075)
    
    ---------
    
    Co-authored-by: keerthana-r-mcw <keerthana.rajendran@multicorewareinc.com>

--- models/experimental/yolov10/tt/v10detect.py
+++ models/experimental/yolov10/tt/v10detect.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import ttnn
-from models.experimental.yolov10.tt.common import Conv, TtYolov10_Conv2D, deallocate_tensors
+from models.experimental.yolov10.tt.common import Conv, TtYolov10Conv2D, deallocate_tensors
 from models.experimental.yolo_common.yolo_utils import concat
 
 
@@ -15,38 +15,53 @@ class TtnnV10Detect:
         self.conv_pt = conv_pt
         self.device = device
         self.cv2_0_0 = Conv(
-            device, parameters.one2one_cv2[0][0], self.conv_pt.one2one_cv2[0][0], is_detect=True, auto_shard=True
+            device,
+            parameters.one2one_cv2[0][0],
+            self.conv_pt.one2one_cv2[0][0],
+            is_detect=True,
         )
         self.cv2_0_1 = Conv(
-            device, parameters.one2one_cv2[0][1], self.conv_pt.one2one_cv2[0][1], is_detect=True, auto_shard=True
+            device,
+            parameters.one2one_cv2[0][1],
+            self.conv_pt.one2one_cv2[0][1],
+            is_detect=True,
         )
-        self.cv2_0_2 = TtYolov10_Conv2D(
+        self.cv2_0_2 = TtYolov10Conv2D(
             parameters.one2one_cv2[0][2], self.conv_pt.one2one_cv2[0][2], device=device, is_detect=True
         )
 
         self.cv2_1_0 = Conv(device, parameters.one2one_cv2[1][0], self.conv_pt.one2one_cv2[1][0], is_detect=True)
         self.cv2_1_1 = Conv(device, parameters.one2one_cv2[1][1], self.conv_pt.one2one_cv2[1][1], is_detect=True)
-        self.cv2_1_2 = TtYolov10_Conv2D(
+        self.cv2_1_2 = TtYolov10Conv2D(
             parameters.one2one_cv2[1][2], self.conv_pt.one2one_cv2[1][2], device=device, is_detect=True
         )
 
         self.cv2_2_0 = Conv(device, parameters.one2one_cv2[2][0], self.conv_pt.one2one_cv2[2][0], is_detect=True)
         self.cv2_2_1 = Conv(device, parameters.one2one_cv2[2][1], self.conv_pt.one2one_cv2[2][1], is_detect=True)
-        self.cv2_2_2 = TtYolov10_Conv2D(
+        self.cv2_2_2 = TtYolov10Conv2D(
             parameters.one2one_cv2[2][2], self.conv_pt.one2one_cv2[2][2], device=device, is_detect=True
         )
 
         self.cv3_0_0_0 = Conv(
-            device, parameters.one2one_cv3[0][0][0], conv_pt.one2one_cv3[0][0][0], is_detect=True, auto_shard=True
+            device,
+            parameters.one2one_cv3[0][0][0],
+            conv_pt.one2one_cv3[0][0][0],
+            is_detect=True,
         )
         self.cv3_0_0_1 = Conv(device, parameters.one2one_cv3[0][0][1], conv_pt.one2one_cv3[0][0][1], is_detect=True)
         self.cv3_0_1_0 = Conv(
-            device, parameters.one2one_cv3[0][1][0], conv_pt.one2one_cv3[0][1][0], is_detect=True, auto_shard=True
+            device,
+            parameters.one2one_cv3[0][1][0],
+            conv_pt.one2one_cv3[0][1][0],
+            is_detect=True,
         )
         self.cv3_0_1_1 = Conv(
-            device, parameters.one2one_cv3[0][1][1], conv_pt.one2one_cv3[0][1][1], is_detect=True, auto_shard=True
+            device,
+            parameters.one2one_cv3[0][1][1],
+            conv_pt.one2one_cv3[0][1][1],
+            is_detect=True,
         )
-        self.cv3_0_2_0 = TtYolov10_Conv2D(
+        self.cv3_0_2_0 = TtYolov10Conv2D(
             parameters.one2one_cv3[0][2], conv_pt.one2one_cv3[0][2], device=device, is_detect=True
         )
 
@@ -58,13 +73,19 @@ class TtnnV10Detect:
             use_1d_systolic_array=False,
         )
         self.cv3_1_0_1 = Conv(
-            device, parameters.one2one_cv3[1][0][1], conv_pt.one2one_cv3[1][0][1], is_detect=True, auto_shard=True
+            device,
+            parameters.one2one_cv3[1][0][1],
+            conv_pt.one2one_cv3[1][0][1],
+            is_detect=True,
         )
         self.cv3_1_1_0 = Conv(device, parameters.one2one_cv3[1][1][0], conv_pt.one2one_cv3[1][1][0], is_detect=True)
         self.cv3_1_1_1 = Conv(
-            device, parameters.one2one_cv3[1][1][1], conv_pt.one2one_cv3[1][1][1], is_detect=True, auto_shard=True
+            device,
+            parameters.one2one_cv3[1][1][1],
+            conv_pt.one2one_cv3[1][1][1],
+            is_detect=True,
         )
-        self.cv3_1_2_0 = TtYolov10_Conv2D(
+        self.cv3_1_2_0 = TtYolov10Conv2D(
             parameters.one2one_cv3[1][2], conv_pt.one2one_cv3[1][2], device=device, is_detect=True
         )
 
@@ -73,25 +94,28 @@ class TtnnV10Detect:
             parameters.one2one_cv3[1][0][0],
             conv_pt.one2one_cv3[1][0][0],
             is_detect=True,
-            use_1d_systolic_array=True,
-            auto_shard=True,
+            use_1d_systolic_array=False,
         )
 
         self.cv3_1_0_1 = Conv(device, parameters.one2one_cv3[1][0][1], conv_pt.one2one_cv3[1][0][1], is_detect=True)
         self.cv3_1_1_0 = Conv(device, parameters.one2one_cv3[1][1][0], conv_pt.one2one_cv3[1][1][0], is_detect=True)
         self.cv3_1_1_1 = Conv(device, parameters.one2one_cv3[1][1][1], conv_pt.one2one_cv3[1][1][1], is_detect=True)
-        self.cv3_1_2_0 = TtYolov10_Conv2D(
+        self.cv3_1_2_0 = TtYolov10Conv2D(
             parameters.one2one_cv3[1][2], conv_pt.one2one_cv3[1][2], device=device, is_detect=True
         )
 
         self.cv3_2_0_0 = Conv(
-            device, parameters.one2one_cv3[2][0][0], conv_pt.one2one_cv3[2][0][0], is_detect=True, auto_shard=True
+            device,
+            parameters.one2one_cv3[2][0][0],
+            conv_pt.one2one_cv3[2][0][0],
+            is_detect=True,
+            use_1d_systolic_array=False,
         )
 
         self.cv3_2_0_1 = Conv(device, parameters.one2one_cv3[2][0][1], conv_pt.one2one_cv3[2][0][1], is_detect=True)
         self.cv3_2_1_0 = Conv(device, parameters.one2one_cv3[2][1][0], conv_pt.one2one_cv3[2][1][0], is_detect=True)
         self.cv3_2_1_1 = Conv(device, parameters.one2one_cv3[2][1][1], conv_pt.one2one_cv3[2][1][1], is_detect=True)
-        self.cv3_2_2_0 = TtYolov10_Conv2D(
+        self.cv3_2_2_0 = TtYolov10Conv2D(
             parameters.one2one_cv3[2][2], conv_pt.one2one_cv3[2][2], device=device, is_detect=True
         )
         self.dfl = Conv(device, parameters.dfl, self.conv_pt.dfl, is_dfl=True)
@@ -117,6 +141,8 @@ class TtnnV10Detect:
         x4 = self.cv3_0_1_1(x4)
         x4 = self.cv3_0_2_0(x4)
 
+        if y2.is_sharded():
+            y2 = ttnn.sharded_to_interleaved(y2, ttnn.L1_MEMORY_CONFIG)
         x5 = self.cv3_1_0_0(y2)
 
         x5 = self.cv3_1_0_1(x5)
@@ -124,18 +150,26 @@ class TtnnV10Detect:
         x5 = self.cv3_1_1_1(x5)
         x5 = self.cv3_1_2_0(x5)
 
+        if y3.is_sharded():
+            y3 = ttnn.sharded_to_interleaved(y3, ttnn.L1_MEMORY_CONFIG)
         x6 = self.cv3_2_0_0(y3)
         x6 = self.cv3_2_0_1(x6)
         x6 = self.cv3_2_1_0(x6)
         x6 = self.cv3_2_1_1(x6)
         x6 = self.cv3_2_2_0(x6)
 
-        x1 = ttnn.sharded_to_interleaved(x1, memory_config=ttnn.L1_MEMORY_CONFIG)
-        x2 = ttnn.sharded_to_interleaved(x2, memory_config=ttnn.L1_MEMORY_CONFIG)
-        x3 = ttnn.sharded_to_interleaved(x3, memory_config=ttnn.L1_MEMORY_CONFIG)
-        x4 = ttnn.sharded_to_interleaved(x4, memory_config=ttnn.L1_MEMORY_CONFIG)
-        x5 = ttnn.sharded_to_interleaved(x5, memory_config=ttnn.L1_MEMORY_CONFIG)
-        x6 = ttnn.sharded_to_interleaved(x6, memory_config=ttnn.L1_MEMORY_CONFIG)
+        if x1.is_sharded():
+            x1 = ttnn.sharded_to_interleaved(x1, memory_config=ttnn.L1_MEMORY_CONFIG)
+        if x2.is_sharded():
+            x2 = ttnn.sharded_to_interleaved(x2, memory_config=ttnn.L1_MEMORY_CONFIG)
+        if x3.is_sharded():
+            x3 = ttnn.sharded_to_interleaved(x3, memory_config=ttnn.L1_MEMORY_CONFIG)
+        if x4.is_sharded():
+            x4 = ttnn.sharded_to_interleaved(x4, memory_config=ttnn.L1_MEMORY_CONFIG)
+        if x5.is_sharded():
+            x5 = ttnn.sharded_to_interleaved(x5, memory_config=ttnn.L1_MEMORY_CONFIG)
+        if x6.is_sharded():
+            x6 = ttnn.sharded_to_interleaved(x6, memory_config=ttnn.L1_MEMORY_CONFIG)
 
         y1 = concat(-1, False, x1, x4)
         y2 = concat(-1, False, x2, x5)
@@ -146,23 +180,27 @@ class TtnnV10Detect:
         y = ttnn.squeeze(y, dim=0)
 
         ya, yb = y[:, :, :64], y[:, :, 64:144]
-        ya = ttnn.reshape(ya, (ya.shape[0], y.shape[1], 4, 16))
-        ya = ttnn.permute(ya, (0, 2, 1, 3))
+        ya = ttnn.permute(ya, (0, 2, 1))
+        ya = ttnn.reshape(ya, (ya.shape[0], 4, 16, y.shape[1]))
+        ya = ttnn.permute(ya, (0, 1, 3, 2))
+        ya = ttnn.to_layout(ya, ttnn.TILE_LAYOUT)
         ya = ttnn.softmax(ya, dim=-1)
         c = self.dfl(ya)
 
         deallocate_tensors(y1, y2, y3, x1, x2, x3, x4, x5, x6, y, ya)
-        c = ttnn.sharded_to_interleaved(c, memory_config=ttnn.L1_MEMORY_CONFIG)
-        c = ttnn.to_layout(c, layout=ttnn.ROW_MAJOR_LAYOUT)
+        if c.is_sharded():
+            c = ttnn.sharded_to_interleaved(c, memory_config=ttnn.L1_MEMORY_CONFIG)
+        if c.get_layout() == ttnn.TILE_LAYOUT:
+            c = ttnn.to_layout(c, layout=ttnn.ROW_MAJOR_LAYOUT)
         c = ttnn.permute(c, (0, 3, 1, 2))
         c = ttnn.reshape(c, (c.shape[0], 1, 4, int(c.shape[3] / 4)))
         c = ttnn.reshape(c, (c.shape[0], c.shape[1] * c.shape[2], c.shape[3]))
         c1, c2 = c[:, :2, :], c[:, 2:4, :]
         anchor, strides = self.anchors, self.strides
-        anchor = ttnn.to_memory_config(anchor, memory_config=ttnn.L1_MEMORY_CONFIG)
-        strides = ttnn.to_memory_config(strides, memory_config=ttnn.L1_MEMORY_CONFIG)
-        c1 = ttnn.to_layout(c1, layout=ttnn.TILE_LAYOUT)
-        c2 = ttnn.to_layout(c2, layout=ttnn.TILE_LAYOUT)
+        if c1.get_layout() == ttnn.ROW_MAJOR_LAYOUT:
+            c1 = ttnn.to_layout(c1, layout=ttnn.TILE_LAYOUT)
+        if c2.get_layout() == ttnn.ROW_MAJOR_LAYOUT:
+            c2 = ttnn.to_layout(c2, layout=ttnn.TILE_LAYOUT)
         c1 = anchor - c1
         c2 = anchor + c2
 
@@ -171,10 +209,9 @@ class TtnnV10Detect:
         z = ttnn.multiply(z, strides)
         yb = ttnn.permute(yb, (0, 2, 1))
         yb = ttnn.sigmoid_accurate(yb)
-        z = ttnn.to_layout(z, layout=ttnn.ROW_MAJOR_LAYOUT)
-        yb = ttnn.to_layout(yb, layout=ttnn.ROW_MAJOR_LAYOUT)
+
         output = concat(1, False, z, yb)
 
-        deallocate_tensors(c, c1, c2, anchor, strides, yb, z)
+        deallocate_tensors(c, c1, c2, yb, z)
 
         return output
