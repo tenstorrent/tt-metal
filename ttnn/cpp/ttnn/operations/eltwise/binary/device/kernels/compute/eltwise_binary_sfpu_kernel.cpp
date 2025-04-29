f5eee5253b60dc9abd376d1da0a66bb63a196947
mcw-anasuya <mcw-anair@ext.tenstorrent.com>
2025-04-29 10:44:14 +0000

    #21354: Add uint16 support for ttnn.sub

--- ttnn/cpp/ttnn/operations/eltwise/binary/device/kernels/compute/eltwise_binary_sfpu_kernel.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/binary/device/kernels/compute/eltwise_binary_sfpu_kernel.cpp
@@ -16,6 +16,7 @@
 #include "compute_kernel_api/add_uint32_sfpu.h"
 #include "compute_kernel_api/add_uint16_sfpu.h"
 #include "compute_kernel_api/sub_int32_sfpu.h"
+#include "compute_kernel_api/sub_uint16_sfpu.h"
 #include "compute_kernel_api/binary_max_min.h"
 
 #define PRE_SCALE defined SFPU_OP_INIT_PRE_IN0_0 || defined SFPU_OP_INIT_PRE_IN1_0
@@ -125,6 +126,9 @@ void MAIN {
 #endif
 #ifdef SUB_INT32_INIT
             SUB_INT32_INIT
+#endif
+#ifdef SUB_UINT16_INIT
+            SUB_UINT16_INIT
 #endif
 #ifdef BITWISE_INIT
             BITWISE_INIT
