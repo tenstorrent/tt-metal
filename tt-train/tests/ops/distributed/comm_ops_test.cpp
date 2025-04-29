099dc2339b58d837df1f82c5629d6b7b572d80e7
Bojan Rosko <brosko@tenstorrent.com>
2025-04-29 06:10:17 +0000

    fix build error

--- tt-train/tests/ops/distributed/comm_ops_test.cpp
+++ tt-train/tests/ops/distributed/comm_ops_test.cpp
@@ -5,7 +5,7 @@
 #include "ops/distributed/comm_ops.hpp"
 
 #include <gtest/gtest.h>
-#include <umd/device/tt_cluster_descriptor.h>
+#include <umd/device/cluster.h>
 
 #include <core/ttnn_all_includes.hpp>
 #include <core/xtensor_utils.hpp>
