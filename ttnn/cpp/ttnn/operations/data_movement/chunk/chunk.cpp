ad1ff1c735eaf3dbc086f02e8c233a655bdd1fd2
Bhuvanesh194Sankar <bhuvanesh.sankar@multicoreware.com>
2025-04-29 10:25:41 +0000

    Resolved the build error

--- ttnn/cpp/ttnn/operations/data_movement/chunk/chunk.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/chunk/chunk.cpp
@@ -18,8 +18,7 @@ std::vector<ttnn::Tensor> ChunkOperation::invoke(const ttnn::Tensor& input_tenso
         dim += num_dims;
     }
     TT_FATAL(
-        num_dims > dim, "... Invalid dimension for chunk operation, {} needs to be greater
-than {}", num_dims, dim);
+        num_dims > dim, "... Invalid dimension for chunk operation, {} needs to be greater than {}", num_dims, dim);
 
     int size_along_dim = size[dim];
     int chunk_size = tt::div_up(size_along_dim, num_chunks);
