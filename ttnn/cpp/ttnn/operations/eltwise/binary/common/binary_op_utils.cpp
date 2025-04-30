308b2b83c256eadfd34561d2a8b0d6af66deaced
Anasuya G Nair <mcw-anair@ext.tenstorrent.com>
2025-04-30 14:51:14 +0530

    #21354: Add uint16 support for ttnn.sub (#21356)
    ### Ticket
    #21354 
    
    ### Problem description
    Provide uint16 support for ttnn.sub with LLK implementation
    
    ### What's changed
    - Added WH_B0 and BH LLK implementations for ttnn.sub to support uint16
    datatype.
    - Added support in binary and binary_ng infra.
    
    ### Checklist
    - [x] BH Testing
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14747246502)
    - PASSED
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14748822321)
    - same as main
    - [x] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/eltwise/binary/common/binary_op_utils.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/binary/common/binary_op_utils.cpp
@@ -203,6 +203,9 @@ std::map<std::string, std::string> get_defines_fp32(
             if (input_a_dtype == DataType::INT32 && input_b_dtype == DataType::INT32) {
                 new_defines.insert({"SUB_INT32_INIT", "sub_int32_tile_init();"});
                 op_name = "sub_int32_tile";
+            } else if (input_a_dtype == DataType::UINT16 && input_b_dtype == DataType::UINT16) {
+                new_defines.insert({"SUB_UINT16_INIT", fmt::format("sub_uint16_tile_init();")});
+                op_name = "sub_uint16_tile";
             } else {
                 new_defines.insert({"BINOP_INIT", "sub_binary_tile_init();"});
                 op_name = "sub_binary_tile";
