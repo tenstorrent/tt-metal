b0200b69c90955d2effad1f2b08897d614b6278c
Philipp Moritz <pcmoritz@gmail.com>
2025-05-01 08:34:18 +0000

    update

--- ttnn/cpp/ttnn/operations/eltwise/binary/device/binary_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/binary/device/binary_device_operation.cpp
@@ -117,8 +117,6 @@ void BinaryDeviceOperation::validate_on_program_cache_miss(
 
     BinaryDeviceOperation::validate_on_program_cache_hit(attributes, tensor_args);
 
-    TT_FATAL(input_tensor_a.get_layout() == Layout::TILE, "Input to eltwise binary must be tilized");
-
     bool tensor_b_sharded = false;
 
     if (input_tensor_b.has_value()) {
@@ -128,7 +126,6 @@ void BinaryDeviceOperation::validate_on_program_cache_miss(
                 input_tensor_a.device() == input_tensor_b->device(),
                 "Operands to eltwise binary need to be on the same device!");
         }
-        TT_FATAL(input_tensor_b->get_layout() == Layout::TILE, "Inputs to eltwise binary must be tilized");
     }
 
     if (input_tensor_a.memory_config().is_sharded()) {
