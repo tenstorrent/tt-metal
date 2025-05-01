bbe58347a9bb358538f494986b96d18d95565c22
Evan Smal <esmal@tenstorrent.com>
2025-05-01 22:03:23 +0000

    get something janky working

--- ttnn/cpp/ttnn/operations/experimental/cnn/convert_to_hwc/device/convert_to_hwc_op.cpp
+++ ttnn/cpp/ttnn/operations/experimental/cnn/convert_to_hwc/device/convert_to_hwc_op.cpp
@@ -20,15 +20,14 @@ void ConvertToHWC::validate(const std::vector<Tensor>& input_tensors) const {
 
     TT_FATAL(shape.size() == 4, "Input shape must be rank 4 (was rank {})", shape.size());
     TT_FATAL(shape[0] == 1 && shape[1] == 1, "Expected input tensor to be shape [1, 1, C, HW]");
-    TT_FATAL(HW <= TILE_HEIGHT, "HW must be less than or equal to 32 (was {})", HW);
-    TT_FATAL(C % TILE_HEIGHT == 0, "C must be divisible by tile size");
+    TT_FATAL(C <= TILE_HEIGHT, "C must be less than or equal to 32 (was {})", C);
 
     TT_FATAL(input.is_sharded(), "Input tensor must be sharded");
 
     const auto& input_shard_spec = input.memory_config().shard_spec.value();
-    TT_FATAL(
-        input_shard_spec.shape[0] % TILE_HEIGHT == 0,
-        "Shard height must be divisible by tile size");  // input shards can be padded so C may not match shard height
+    // TT_FATAL(
+    // input_shard_spec.memory_layout == tt::tt_metal::TensorMemoryLayout::WIDTH_SHARDED,
+    //"Input tensor must be width sharded");
     TT_FATAL(
         this->memory_config.is_sharded() &&
             this->memory_config.memory_layout == tt::tt_metal::TensorMemoryLayout::HEIGHT_SHARDED,
@@ -49,7 +48,6 @@ tt::tt_metal::operation::ProgramWithCallbacks ConvertToHWC::create_program(
     const std::vector<Tensor>& input_tensors, std::vector<Tensor>& output_tensors) const {
     const auto& a = input_tensors.at(0);
     auto& output = output_tensors.at(0);
-    auto device_compute_with_storage_grid_size = a.device()->compute_with_storage_grid_size();
-    return detail::multi_core_convert_to_hwc(a, output, device_compute_with_storage_grid_size);
+    return detail::multi_core_convert_to_hwc(a, output);
 }
 }  // namespace ttnn::operations::experimental::cnn
