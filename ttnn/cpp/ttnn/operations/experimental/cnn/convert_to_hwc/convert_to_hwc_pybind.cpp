9cf5fba25bb05634bd37424453d0861ac14a9499
Evan Smal <esmal@tenstorrent.com>
2025-05-01 20:07:43 +0000

    Add `ttnn.experimental.convert_to_hwc` for converting to channels last

--- ttnn/cpp/ttnn/operations/experimental/cnn/convert_to_hwc/convert_to_hwc_pybind.cpp
+++ ttnn/cpp/ttnn/operations/experimental/cnn/convert_to_hwc/convert_to_hwc_pybind.cpp
@@ -1,31 +1,31 @@
-// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include "convert_to_chw_pybind.hpp"
+#include "convert_to_hwc_pybind.hpp"
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include "convert_to_chw.hpp"
-#include "ttnn-pybind/decorators.hpp"
+#include "convert_to_hwc.hpp"
+#include "cpp/ttnn-pybind/decorators.hpp"
 
 namespace ttnn::operations::experimental::cnn::detail {
 
 namespace py = pybind11;
 
-void bind_convert_to_chw(py::module& module) {
-    using OperationType = decltype(ttnn::experimental::convert_to_chw);
+void bind_convert_to_hwc(py::module& module) {
+    using OperationType = decltype(ttnn::experimental::convert_to_hwc);
 
     const auto doc = R"doc(
-    Convert a tensor from HWC channel ordering to CHW channel ordering.
+    Convert a tensor from CHW channel ordering to HWC channel ordering.
 
-    The input tensor is expected to be tiled and height-sharded in L1. The output is a row-major width-sharded tensor.
+    The input tensor is expected to be in row-major layout and width-sharded in L1 or DRAM. The output is a row-major height-sharded tensor.
     )doc";
 
     ttnn::bind_registered_operation(
         module,
-        ttnn::experimental::convert_to_chw,
+        ttnn::experimental::convert_to_hwc,
         doc,
         ttnn::pybind_overload_t{
             [](const OperationType& self,
