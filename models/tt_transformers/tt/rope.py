abaf894ad82bc6d7d62bdd3ba4cfe53e05fae9c8
Miguel Tairum <150826086+mtairum@users.noreply.github.com>
2025-05-01 16:19:52 +0100

    Fixed TTT sharded specs for rot mats on BH (#21307)
    And also added a new parametrized unit test to SDPA decode with batch-32
    config.
    
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/19798
    
    ### Problem description
    On blackhole, long batch numbers were giving bad outputs due to a
    misconfiguration of the shard specs on the decode side.
    
    ### Checklist
    - [ ]
    [All-post-commit](https://github.com/tenstorrent/tt-metal/actions/runs/14714265696)
    - [ ]
    [T3K](https://github.com/tenstorrent/tt-metal/actions/runs/14714258543)
    - [ ]
    [Single-device](https://github.com/tenstorrent/tt-metal/actions/runs/14714291337)
    - [x] [BH
    nightly](https://github.com/tenstorrent/tt-metal/actions/runs/14714282126)
    - [x] [BH
    Demo](https://github.com/tenstorrent/tt-metal/actions/runs/14714284480)

--- models/tt_transformers/tt/rope.py
+++ models/tt_transformers/tt/rope.py
@@ -65,7 +65,11 @@ class RotarySetup(LightweightModule):
             mesh_mapper=ReplicateTensorToMesh(device) if self.is_mesh_device else None,
         )
 
-        batch_grid = ttnn.num_cores_to_corerangeset(batch_size, self.core_grid, row_wise=True)
+        self.batch_grid = (
+            ttnn.CoreGrid(y=4, x=8)
+            if ttnn.get_arch_name() == "blackhole"
+            else ttnn.num_cores_to_corerangeset(batch_size, self.core_grid, row_wise=True)
+        )
         # Generate the transformation matrix
         trans_mat = get_rot_transformation_mat(dhead=ttnn.TILE_SIZE).repeat(
             1,
@@ -76,7 +80,7 @@ class RotarySetup(LightweightModule):
         )  # Repeat across all cores on device
         trans_mat_mem_config = ttnn.create_sharded_memory_config(
             shape=(ttnn.TILE_SIZE, ttnn.TILE_SIZE),
-            core_grid=batch_grid,
+            core_grid=self.batch_grid,
             strategy=ttnn.ShardStrategy.HEIGHT,
             orientation=ttnn.ShardOrientation.ROW_MAJOR,
             use_height_and_width_as_shard_shape=True,
@@ -174,10 +178,9 @@ class RotarySetup(LightweightModule):
             cos = cos[:, : self.batch_size_per_device_group, :, :]
             sin = sin[:, : self.batch_size_per_device_group, :, :]
 
-        grid = ttnn.num_cores_to_corerangeset(self.batch_size, self.core_grid, row_wise=True)
         mem_config = ttnn.create_sharded_memory_config(
             shape=(ttnn.TILE_SIZE, self.head_dim),
-            core_grid=grid,
+            core_grid=self.batch_grid,
             strategy=ttnn.ShardStrategy.HEIGHT,
             orientation=ttnn.ShardOrientation.ROW_MAJOR,
             use_height_and_width_as_shard_shape=True,
