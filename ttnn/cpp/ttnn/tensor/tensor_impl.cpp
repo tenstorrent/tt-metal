a7eda89e12c4be8b0feac0c0132bf88a5f6c25b7
Oleg Milyutin <omilyutin@tenstorrent.com>
2025-05-03 16:32:28 -0400

    #0: Remove functionality related to tracking worker threads (#21573)
    ### Ticket
    N/A
    
    ### Problem description
    Async runtime was removed, so the methods tracking worker threads are
    now obsolete.
    
    ### What's changed
    Delete dead code.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14800161447)
    - [x] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/tensor/tensor_impl.cpp
+++ ttnn/cpp/ttnn/tensor/tensor_impl.cpp
@@ -547,7 +547,6 @@ Tensor to_host<bfloat8_b>(const Tensor& tensor, bool blocking, ttnn::QueueId cq_
 
 template <typename T>
 Tensor to_host_mesh_tensor(const Tensor& tensor, bool blocking, ttnn::QueueId cq_id) {
-    TT_FATAL(tt::tt_metal::detail::InMainThread(), "to_host_mesh_tensor must be called from the main thread");
     TT_ASSERT(tensor.is_allocated(), "Buffer must be allocated on device!");
     const auto& storage = std::get<DeviceStorage>(tensor.get_storage());
     const auto& mesh_buffer = storage.mesh_buffer;
@@ -818,7 +817,6 @@ Tensor to_device_mesh_tensor(
         return tensor;  // Tensor already on device
     }
 
-    TT_FATAL(tt::tt_metal::detail::InMainThread(), "to_device_mesh_tensor must be called from the main thread");
     TT_FATAL(mesh_device != nullptr, "Need target device in order to move tensor to device!");
     TT_FATAL(tensor.is_allocated(), "Need data to exist in order to move it to device");
 
@@ -846,7 +844,6 @@ template <typename T>
 void copy_to_mesh_tensor(const Tensor& host_tensor, Tensor& mesh_tensor, ttnn::QueueId cq_id) {
     TT_FATAL(host_tensor.storage_type() != StorageType::DEVICE, "Host tensor is on device.");
     TT_FATAL(mesh_tensor.storage_type() == StorageType::DEVICE, "Mesh tensor is not on device.");
-    TT_FATAL(tt::tt_metal::detail::InMainThread(), "copy_to_mesh_tensor must be called from the main thread");
     TT_FATAL(mesh_tensor.is_allocated(), "Need data to exist in order to move it to device");
 
     TT_FATAL(host_tensor.get_logical_shape() == mesh_tensor.get_logical_shape(), "Host tensor has different shape");
