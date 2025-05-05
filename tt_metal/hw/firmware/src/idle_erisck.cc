c65ac1fa380162f48accd248b72f6c0485e9b329
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-05-05 13:38:36 +0000

    Deregionify linker scripts, compute LMA

--- tt_metal/hw/firmware/src/idle_erisck.cc
+++ tt_metal/hw/firmware/src/idle_erisck.cc
@@ -23,9 +23,9 @@
 
 void kernel_launch(uint32_t kernel_base_addr) {
     extern uint32_t __kernel_init_local_l1_base[];
-    extern uint32_t __fw_export_end_text[];
+    extern uint32_t __fw_export_text_end[];
     do_crt1((uint32_t tt_l1_ptr
-                 *)(kernel_base_addr + (uint32_t)__kernel_init_local_l1_base - (uint32_t)__fw_export_end_text));
+                 *)(kernel_base_addr + (uint32_t)__kernel_init_local_l1_base - (uint32_t)__fw_export_text_end));
 
     noc_local_state_init(NOC_INDEX);
 
