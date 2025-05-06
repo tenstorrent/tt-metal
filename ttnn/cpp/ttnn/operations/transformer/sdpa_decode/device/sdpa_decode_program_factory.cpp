d313a980999a15684452a83a9455255dbededdc4
Djordje <divanovic@tenstorrent.com>
2025-05-06 15:41:08 +0000

    Revert "#0: Fix for SDPA decode op for blackhole (#21320)"
    This reverts commit 532ae64b66207456f2526997efa37e2e484a2d2e.

--- ttnn/cpp/ttnn/operations/transformer/sdpa_decode/device/sdpa_decode_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/transformer/sdpa_decode/device/sdpa_decode_program_factory.cpp
@@ -344,7 +344,7 @@ operation::ProgramWithCallbacks sdpa_decode_multi_core(
     // - In non-causal mode, mask can be an input tensor which needs proper handling to read as 16x32 tiles
     // - Only support Float16_b since block float w/ shared exp needs special handling to read as 16x32 tiles
     // In compute, need to find a proper way to get num_faces for sfpu functions
-    const bool use_half_tile = (is_causal and num_q_heads <= 16 and q_df == tt::DataFormat::Float16_b and device->arch() == tt::ARCH::WORMHOLE_B0);
+    const bool use_half_tile = (is_causal and num_q_heads <= 16 and q_df == tt::DataFormat::Float16_b);
     if (use_half_tile) {
         q_tile = half_tile;
         mask_tile = half_tile;
