f3485d6dbc812f5e47cef9fc82ee7ab1008b354d
Djordje Zivanovic <dzivanovic@tenstorrent.com>
2025-05-06 09:19:19 +0200

    Add cluster type for simulator (#21357)
    ### Problem description
    A runtime exception `Unknown cluster type` occurs when running tt-metal
    tests on the Simulator. This happens because the cluster type is not
    initialized for the Simulator target.
    Reference to test:
    https://yyz-gitlab.local.tenstorrent.com/tenstorrent/tt-umd-simulators/-/jobs/10197480
    
    ### What's changed
    A new cluster type for the Simulator target has been added, and the
    cluster_type is now properly initialized for this target. This ensures
    that tt-metal tests on the Simulator no longer raise the Unknown cluster
    type exception.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14840588363)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14840595075)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [ ] New/Existing tests provide coverage for changes

--- tt_metal/llrt/tt_cluster.hpp
+++ tt_metal/llrt/tt_cluster.hpp
@@ -63,15 +63,17 @@ enum class TargetDevice : std::uint8_t {
 
 enum class ClusterType : std::uint8_t {
     INVALID = 0,
-    N150 = 1,     // Production N150
-    N300 = 2,     // Production N300
-    T3K = 3,      // Production T3K, built with 4 N300s
-    GALAXY = 4,   // Production Galaxy, all chips with mmio
-    TG = 5,       // Will be deprecated
-    P100 = 6,     // Blackhole single card, ethernet disabled
-    P150 = 7,     // Blackhole single card, ethernet enabled
-    P150_X2 = 8,  // 2 Blackhole single card, ethernet connected
-    P150_X4 = 9,  // 4 Blackhole single card, ethernet connected
+    N150 = 1,                    // Production N150
+    N300 = 2,                    // Production N300
+    T3K = 3,                     // Production T3K, built with 4 N300s
+    GALAXY = 4,                  // Production Galaxy, all chips with mmio
+    TG = 5,                      // Will be deprecated
+    P100 = 6,                    // Blackhole single card, ethernet disabled
+    P150 = 7,                    // Blackhole single card, ethernet enabled
+    P150_X2 = 8,                 // 2 Blackhole single card, ethernet connected
+    P150_X4 = 9,                 // 4 Blackhole single card, ethernet connected
+    SIMULATOR_WORMHOLE_B0 = 10,  // Simulator Wormhole B0
+    SIMULATOR_BLACKHOLE = 11,    // Simulator Blackhole
 };
 
 enum class EthRouterMode : uint32_t {
