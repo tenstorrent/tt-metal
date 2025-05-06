c1d1ee08bb804126ec8ca4b3c84f034210125da4
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2025-05-06 16:37:13 +0200

    #20050: SDXL VAE upblocks (#21684)
    ### Ticket
    [Issue](https://github.com/tenstorrent/tt-metal/issues/20050)
    
    ### What's changed
    Functional implementation with unit tests for `TtUpsample2D` and
    `TtUpDecoderBlock2D`.
    Additionally, to reduce memory consumption we load only UNet/VAE weights
    not the whole pipeline and call the garbage collector explicitly.
    
    ### Checklist
    CI in progress
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14858308277)
    CI passes
    - [x] [(Single-card) Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/14855585066)
    CI passes

--- models/experimental/stable_diffusion_xl_base/tests/pcc/test_module_tt_upsample2d.py
+++ models/experimental/stable_diffusion_xl_base/tests/pcc/test_module_tt_upsample2d.py
@@ -1,11 +1,12 @@
 # SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
 
 # SPDX-License-Identifier: Apache-2.0
+import gc
 import torch
 import pytest
 import ttnn
 from models.experimental.stable_diffusion_xl_base.tt.tt_upsample2d import TtUpsample2D
-from diffusers import DiffusionPipeline
+from diffusers import UNet2DConditionModel
 from tests.ttnn.utils_for_testing import assert_with_pcc
 from models.utility_functions import torch_random
 from models.experimental.stable_diffusion_xl_base.tt.sdxl_utility import (
@@ -20,10 +21,10 @@ from models.experimental.stable_diffusion_xl_base.tt.sdxl_utility import (
 @pytest.mark.parametrize("dilation", [(1, 1)])
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 16384}], indirect=True)
 def test_upsample2d(device, input_shape, up_block_id, stride, padding, dilation, use_program_cache, reset_seeds):
-    pipe = DiffusionPipeline.from_pretrained(
-        "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float32, use_safetensors=True, variant="fp16"
+    unet = UNet2DConditionModel.from_pretrained(
+        "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float32, use_safetensors=True, subfolder="unet"
     )
-    unet = pipe.unet
+    # unet = pipe.unet
     unet.eval()
     state_dict = unet.state_dict()
 
@@ -43,5 +44,8 @@ def test_upsample2d(device, input_shape, up_block_id, stride, padding, dilation,
     output_tensor = from_channel_last_ttnn(
         ttnn_output_tensor, [input_shape[0], output_shape[1], output_shape[2], output_shape[0]]
     )
+
+    del unet
+    gc.collect()
 
     assert_with_pcc(torch_output_tensor, output_tensor, 0.996)
