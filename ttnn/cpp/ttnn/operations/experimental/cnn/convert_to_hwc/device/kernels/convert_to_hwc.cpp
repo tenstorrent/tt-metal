513dc5e44d8652cbf83a7305b9791cde4702dc26
Evan Smal <esmal@tenstorrent.com>
2025-05-07 16:55:39 +0000

    Get working with multiple tiles

--- ttnn/cpp/ttnn/operations/experimental/cnn/convert_to_hwc/device/kernels/convert_to_hwc.cpp
+++ ttnn/cpp/ttnn/operations/experimental/cnn/convert_to_hwc/device/kernels/convert_to_hwc.cpp
@@ -10,12 +10,11 @@
 #include "compute_kernel_api/transpose_wh.h"
 #include "compute_kernel_api/tilize.h"
 
-constexpr uint32_t ONE_TILE = 1;
-
+template <size_t BATCH_SIZE = 1>
 FORCE_INLINE void transpose(uint32_t cb_in, uint32_t cb_out) {
-    pack_untilize_dst_init_short<1>(cb_in);
+    pack_untilize_dst_init_short<BATCH_SIZE>(cb_in);
 
-    cb_wait_front(cb_in, ONE_TILE);
+    cb_wait_front(cb_in, BATCH_SIZE);
 
     tile_regs_acquire();
     tile_regs_wait();
@@ -23,27 +22,27 @@ FORCE_INLINE void transpose(uint32_t cb_in, uint32_t cb_out) {
     transpose_wh_init_short(cb_in);
     transpose_wh_tile(cb_in, 0, 0);
 
-    cb_reserve_back(cb_out, ONE_TILE);
-    pack_untilize_dst<1>(cb_out, ONE_TILE);
+    cb_reserve_back(cb_out, BATCH_SIZE);
+    pack_untilize_dst<1>(cb_out, BATCH_SIZE);
 
     tile_regs_commit();
     tile_regs_release();
 
-    cb_push_back(cb_out, ONE_TILE);
-    cb_pop_front(cb_in, ONE_TILE);
+    cb_push_back(cb_out, BATCH_SIZE);
+    cb_pop_front(cb_in, BATCH_SIZE);
 }
 
-template <int BATCH_SIZE>
-FORCE_INLINE void tilize(uint32_t cb_in, uint32_t num_tiles_in_row, uint32_t cb_out) {
-    tilize_init_short(cb_in, num_tiles_in_row, cb_out);
+FORCE_INLINE void tilize(
+    uint32_t cb_in, uint32_t total_tiles_per_block, uint32_t total_sticks_per_block, uint32_t cb_out) {
+    tilize_init_short(cb_in, total_tiles_per_block, cb_out);
 
-    cb_wait_front(cb_in, 8);
-    cb_reserve_back(cb_out, num_tiles_in_row);
+    cb_wait_front(cb_in, total_sticks_per_block);
+    cb_reserve_back(cb_out, total_tiles_per_block);
 
-    tilize_block(cb_in, num_tiles_in_row, cb_out);
+    tilize_block(cb_in, total_tiles_per_block, cb_out);
 
-    cb_pop_front(cb_in, 8);
-    cb_push_back(cb_out, num_tiles_in_row);
+    cb_pop_front(cb_in, total_sticks_per_block);
+    cb_push_back(cb_out, total_tiles_per_block);
 
     tilize_uninit(cb_in, cb_out);
 }
@@ -51,17 +50,19 @@ FORCE_INLINE void tilize(uint32_t cb_in, uint32_t num_tiles_in_row, uint32_t cb_
 namespace NAMESPACE {
 void MAIN {
     const uint32_t total_tiles = get_arg_val<uint32_t>(0);
+    const uint32_t total_sticks_per_block = get_arg_val<uint32_t>(1);
+
     constexpr uint32_t cb_in = get_compile_time_arg_val(0);
     constexpr uint32_t cb_tiled_in = get_compile_time_arg_val(1);
     constexpr uint32_t cb_transpose_in = get_compile_time_arg_val(2);
 
-    cb_push_back(cb_in, 8 * total_tiles);
+    cb_push_back(cb_in, total_sticks_per_block);
 
     pack_untilize_init(cb_tiled_in, cb_transpose_in);
     transpose_wh_init(cb_tiled_in, cb_transpose_in);
     tilize_init(cb_in, total_tiles, cb_tiled_in);
 
-    tilize<1>(cb_in, total_tiles, cb_tiled_in);
+    tilize(cb_in, total_tiles, total_sticks_per_block, cb_tiled_in);
     for (uint32_t idx = 0; idx < total_tiles; idx++) {
         transpose(cb_tiled_in, cb_transpose_in);
     }
--- ttnn/cpp/ttnn/operations/experimental/cnn/convert_to_hwc/device/kernels/reader_convert_to_hwc.cpp
+++ ttnn/cpp/ttnn/operations/experimental/cnn/convert_to_hwc/device/kernels/reader_convert_to_hwc.cpp
@@ -7,5 +7,5 @@
 void kernel_main() {
     const uint32_t total_tiles = get_arg_val<uint32_t>(0);
     constexpr uint32_t cb_in = get_compile_time_arg_val(0);
-    cb_push_back(cb_in, total_tiles);
+    // cb_push_back(cb_in, total_tiles);
 }
