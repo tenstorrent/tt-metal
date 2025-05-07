ba39e280ae3631f861c4b94bd7425587f62fcf79
blozano-tt <blozano@tenstorrent.com>
2025-05-07 08:29:43 +0000

    Fix usage in tests

--- tests/tt_metal/tt_metal/api/circular_buffer/test_CircularBuffer_creation.cpp
+++ tests/tt_metal/tt_metal/api/circular_buffer/test_CircularBuffer_creation.cpp
@@ -112,7 +112,7 @@ TEST_F(DeviceFixture, TensixTestCreateCircularBufferAtValidIndices) {
 
     for (unsigned int id = 0; id < num_devices_; id++) {
         detail::CompileProgram(devices_.at(id), program);
-        program_dispatch::finalize_program_offsets(program, devices_.at(id));
+        program.finalize_offsets(devices_.at(id));
         EXPECT_TRUE(test_cb_config_written_to_core(program, this->devices_.at(id), cr_set, golden_cb_config));
     }
 }
