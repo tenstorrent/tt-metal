8bb3e933fd07ad6c7f8a820757b27ad7570adbf8
Jarosław Bartosz Bieniek <jbbieniek@tenstorrent.com>
2025-05-07 19:11:42 +0200

    Add ttnncumprod multi-core implementation (#20890)
    ### Tickets
    #19892 
    #21609
    
    ### Problem description
    Add single-core version of `ttnn.cumprod`.
    
    ### What's changed
    The existing device operation (from the scaffold, #19549 ) is to be
    fully implemented.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [x] New/Existing tests provide coverage for changes
    
    ### Performance
    #### Legend:
    - red line signifies total composite operation max kernel time (in
    nanoseconds) (preprocessing + cumprod + post processing)
    - green line signifies cumprod max kernel time (in nanoseconds)
    - blue line signifies permute max kernel time (in nanoseconds)
    
    #### Graphs:
    - Growing acc without permutation:
    
    ![out1](https://github.com/user-attachments/assets/07417c8b-c1d0-43ed-a16c-19b7eb932749)
    
    - Growing acc with permutation:
    
    ![out2](https://github.com/user-attachments/assets/b8b3df78-ce1a-4cfa-9081-fd3bad99541d)
    
    - Growing work units without permutation:
    
    ![out3](https://github.com/user-attachments/assets/b204b837-dad7-4477-9c38-7093ba2ad7df)
    
    - Growing work units with permutation:
    
    ![out4](https://github.com/user-attachments/assets/1c5fbb09-8e4a-4459-94a0-12d9e03d55af)

--- ttnn/cpp/ttnn/operations/experimental/reduction/cumprod/device/cumprod_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/experimental/reduction/cumprod/device/cumprod_device_operation.cpp
@@ -3,21 +3,77 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "cumprod_device_operation.hpp"
-#include "ttnn/common/queue_id.hpp"
+#include <magic_enum/magic_enum.hpp>
+#include "ttnn/tensor/tensor.hpp"
 
 namespace ttnn::operations::experimental::reduction {
 
-// the result depends on tensor_args!
 CumprodDeviceOperation::program_factory_t CumprodDeviceOperation::select_program_factory(
     const operation_attributes_t& operation_attributes, const tensor_args_t& tensor_args) {
-    return CumprodProgramFactory{};
+    return MultiCoreCumprodProgramFactory{};
 }
 
 void CumprodDeviceOperation::validate_on_program_cache_miss(
-    const operation_attributes_t& attributes, const tensor_args_t& tensor_args) {}
+    const operation_attributes_t& attributes, const tensor_args_t& tensor_args) {
+    const auto& input_tensor{tensor_args.input_tensor};
+    auto& optional_out{tensor_args.optional_out};
+    auto out_memory_config{optional_out.has_value() ? optional_out->memory_config() : attributes.output_memory_config};
+    auto out_dtype = DataType::INVALID;
+    const auto& input_dtype{attributes.dtype};
+    const auto& dim = attributes.dim;
+
+    if (optional_out.has_value()) {
+        const auto computed_output_shape{compute_output_specs(attributes, tensor_args).logical_shape()};
+        const auto preallocated_output_shape = optional_out.value().get_logical_shape();
+        TT_FATAL(
+            computed_output_shape == preallocated_output_shape,
+            "The shapes of the input and the preallocated tensors are not equal.\n"
+            "Input tensor's shape: {}\n"
+            "Preallocated tensor's shape: {}",
+            computed_output_shape,
+            preallocated_output_shape);
+
+        out_dtype = optional_out->get_dtype();
+    } else {
+        out_dtype = input_tensor.get_dtype();
+    }
+
+    TT_FATAL(
+        ((dim >= -static_cast<decltype(dim)>(input_tensor.get_padded_shape().rank())) &&
+         (dim < static_cast<decltype(dim)>(input_tensor.get_padded_shape().rank()))),
+        "The requested cumulation axis is {}, while the input thensor has rank {}.",
+        dim,
+        input_tensor.get_padded_shape().rank());
+
+    TT_FATAL(
+        input_tensor.storage_type() == StorageType::DEVICE,
+        "The ttnn.cumprod operation requires input to be on a Tenstorrent device. "
+        "The input tensor is stored on {}.",
+        magic_enum::enum_name(input_tensor.storage_type()));
+
+    TT_FATAL(
+        input_tensor.buffer() != nullptr,
+        "The ttnn.cumprod operation requires input to be allocated in buffers on the device. "
+        "The buffer is null.");
+
+    TT_FATAL(!input_tensor.is_sharded(), "The ttnn.cumprod operation does not support sharded input tensors.");
+
+    TT_FATAL(
+        input_tensor.get_layout() == Layout::TILE,
+        "The provided input tensor has a non-tile layout: {}.",
+        magic_enum::enum_name(input_tensor.get_layout()));
+
+    TT_FATAL(
+        input_tensor.memory_config().memory_layout == TensorMemoryLayout::INTERLEAVED,
+        "The ttnn.cumprod operation requires the memory layout of the input tensor to be "
+        "interleaved. Instead, it is {}.",
+        magic_enum::enum_name(input_tensor.memory_config().memory_layout));
+}
 
 void CumprodDeviceOperation::validate_on_program_cache_hit(
-    const operation_attributes_t& attributes, const tensor_args_t& tensor_args) {}
+    const operation_attributes_t& attributes, const tensor_args_t& tensor_args) {
+    validate_on_program_cache_miss(attributes, tensor_args);
+}
 
 CumprodDeviceOperation::spec_return_value_t CumprodDeviceOperation::compute_output_specs(
     const operation_attributes_t& attributes, const tensor_args_t& tensor_args) {
@@ -25,21 +81,22 @@ CumprodDeviceOperation::spec_return_value_t CumprodDeviceOperation::compute_outp
         return tensor_args.optional_out->get_tensor_spec();
     }
 
-    auto output_layout = Layout::TILE;
+    auto output_layout{Layout::TILE};
     if (attributes.output_memory_config.is_sharded()) {
         output_layout = tensor_args.input_tensor.get_layout();
     }
 
-    const auto output_shape = tensor_args.input_tensor.logical_shape();
-    return TensorSpec(
+    const auto output_shape{tensor_args.input_tensor.logical_shape()};
+    return TensorSpec{
         output_shape,
-        TensorLayout(tensor_args.input_tensor.get_dtype(), output_layout, attributes.output_memory_config));
+        TensorLayout{tensor_args.input_tensor.get_dtype(), output_layout, attributes.output_memory_config}};
 }
 
 CumprodDeviceOperation::tensor_return_value_t CumprodDeviceOperation::create_output_tensors(
     const operation_attributes_t& operation_attributes, const tensor_args_t& tensor_args) {
     if (tensor_args.optional_out.has_value()) {
-        return *tensor_args.optional_out;  // a new Python reference to the same tensor is returned here
+        // a copy of a Python object (referencing to the same tensor though) is returned here
+        return *tensor_args.optional_out;
     }
     return create_device_tensor(
         compute_output_specs(operation_attributes, tensor_args), tensor_args.input_tensor.device());
@@ -47,11 +104,14 @@ CumprodDeviceOperation::tensor_return_value_t CumprodDeviceOperation::create_out
 
 CumprodDeviceOperation::invocation_result_t CumprodDeviceOperation::invoke(
     const Tensor& input_tensor,
-    const int32_t dim,
+    const int32_t& dim,
+    std::optional<DataType>& dtype,
     std::optional<Tensor> optional_out,
     const MemoryConfig& memory_config,
     const QueueId& queue_id) {
-    return {operation_attributes_t{dim, memory_config}, tensor_args_t{input_tensor, std::move(optional_out)}};
+    return {
+        operation_attributes_t{dim, dtype.has_value() ? dtype.value() : DataType::INVALID, memory_config},
+        tensor_args_t{input_tensor, std::move(optional_out)}};
 }
 
 }  // namespace ttnn::operations::experimental::reduction
