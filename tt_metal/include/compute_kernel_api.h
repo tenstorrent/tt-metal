ec9a2e5ca51c5914245a90072b8cb310390dca3b
Aswinmcw <azayasankaran@tenstorrent.com>
2025-05-08 11:30:40 +0000

    Add int32 support for unary_lt

--- tt_metal/include/compute_kernel_api.h
+++ tt_metal/include/compute_kernel_api.h
@@ -1067,6 +1067,26 @@ ALWI void unary_lt_tile(uint32_t idst, uint32_t param0) {
     MATH((llk_math_eltwise_unary_sfpu_unary_lt<APPROX>(idst, param0)));
 }
 
+// unary lt : if x < value --> 1, else 0
+// clang-format off
+/**
+ * Performs element-wise computation of:  result = 1 if x<value , where x is each element of a tile
+ * in DST register at index tile_index. The value is provided as const param0 The DST register buffer must be in
+ * acquired state via *acquire_dst* call. This call is blocking and is only
+ * available on the compute engine.
+ *
+ * Return value: None
+ *
+ * | Argument        | Description                                                                | Type     | Valid Range                                           | Required |
+ * |-----------------|----------------------------------------------------------------------------|----------|-------------------------------------------------------|----------|
+ * | idst            | The index of the tile in DST register buffer to perform the computation on | uint32_t | Must be less than the size of the DST register buffer | True     |
+ * | param0          | The value to be compared with the input tensor                             | uint32_t |                                                       | True     |
+ */
+// clang-format on
+ALWI void unary_lt_tile_int32(uint32_t idst, uint32_t param0) {
+    MATH((llk_math_eltwise_unary_sfpu_unary_lt_int32<APPROX>(idst, param0)));
+}
+
 /**
  * Please refer to documentation for any_init.
  */
