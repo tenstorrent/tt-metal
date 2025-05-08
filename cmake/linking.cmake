d36ca0f45d3de85434948dbbb3b251ccaa2fabf3
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-05-08 19:53:10 -0400

    Add LTO/IPO support. #21844.
    * Added project option TT_ENABLE_LTO (default: OFF)
    * Added cmake/linking.cmake
    * linking.cmake attempts to enable LTO if TT_ENABLE_LTO is ON.
    * If the checks in linking.cmake succeed, set variable TT_LTO_ENABLED
      TRUE.
    * LTO being enabled also adds -Wodr for warning of One Definition Rule
      violations.
    * References to -flto updated to -flto=auto to enable multithreaded LTO.

---
+++ cmake/linking.cmake
@@ -0,0 +1,22 @@
+if(TT_ENABLE_LTO)
+    cmake_policy(SET CMP0069 NEW)
+    set(CMAKE_POLICY_DEFAULT_CMP0069 NEW)
+    include(CheckIPOSupported)
+    check_ipo_supported(RESULT result OUTPUT output)
+
+    # LTO can be requested but not supported, so handle that case
+    if(result)
+        set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
+        message(STATUS "LTO/IPO is supported.")
+        set(TT_LTO_ENABLED ON)
+
+        # Enable one-definition-rule (ODR) warnings.
+        # Only works when LTO is enabled.
+        add_compile_options(-Wodr)
+    else()
+        message(SEND_ERROR "LTO/IPO is not supported: ${output}")
+        set(TT_LTO_ENABLED OFF)
+    endif()
+else()
+    set(TT_LTO_ENABLED OFF)
+endif()
