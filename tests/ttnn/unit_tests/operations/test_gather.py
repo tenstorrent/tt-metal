0a6cc3f23a402b2b4eb8344510a448f2aa068dda
mgajewskiTT <mgajewski@tenstorrent.com>
2025-05-08 08:41:25 +0000

    Changes reverted after wrong commit

--- tests/ttnn/unit_tests/operations/test_gather.py
+++ tests/ttnn/unit_tests/operations/test_gather.py
@@ -5,11 +5,9 @@
 import pytest
 import torch
 import ttnn
-from models.utility_functions import skip_for_grayskull
 from tests.ttnn.utils_for_testing import assert_with_pcc
 
 
-@skip_for_grayskull()
 @pytest.mark.parametrize(
     "input_shape, index_shape, dim",
     [
@@ -18,6 +16,15 @@ from tests.ttnn.utils_for_testing import assert_with_pcc
         ([64, 128, 256], [64, 128, 128], -1),
         ([1, 2048, 1, 64], [1, 2048, 1, 32], -1),
         ([1, 1, 1, 1], [1, 1, 1, 1], -1),
+        ([4, 4], [4, 4], 1),  # xfail case
+        ([128, 64], [128, 32], 1),  # xfail case
+        ([16, 16, 16], [16, 16, 16], 0),  # xfail case
+        ([1, 1, 1, 1], [1, 1, 1, 1], 1),  # xfail case
+        ([64, 128, 256], [64, 128, 128], 1),  # xfail case
+        ([256, 2, 32], [160, 2, 32], 1),  # xfail case
+        ([2, 256, 2, 32], [2, 128, 2, 32], 1),  # xfail case
+        ([2, 32, 96], [2, 32, 32], 1),  # xfail case
+        ([128, 128], [128, 64], 1),  # xfail case
     ],
 )
 def test_gather_general(input_shape, index_shape, dim, device):
@@ -28,6 +35,11 @@ def test_gather_general(input_shape, index_shape, dim, device):
     index = torch.randint(
         0, input_shape[dim], index_shape, dtype=torch.int64
     )  # torch.int64 is required for torch.gather
+
+    if dim != -1:
+        pytest.xfail(
+            reason="uint32 types support does not exist in ttnn.transpose yet which is used in ttnn.gather. See issue: https://github.com/tenstorrent/tt-metal/issues/18057"
+        )
 
     torch_gather = torch.gather(input, dim, index)
 
@@ -40,56 +52,6 @@ def test_gather_general(input_shape, index_shape, dim, device):
     assert_with_pcc(torch_gather, ttnn.to_torch(ttnn_gather))
 
 
-@skip_for_grayskull()
-@pytest.mark.parametrize(
-    "input_shape, index_shape, dim",
-    [
-        ([4, 4], [4, 4], 1),
-        ([128, 64], [128, 32], 1),
-        ([16, 16, 16], [16, 16, 16], 0),
-        ([1, 1, 1, 1], [1, 1, 1, 1], 1),
-        ([64, 128, 256], [64, 128, 128], 1),
-        ([256, 2, 32], [160, 2, 32], 1),
-        ([2, 256, 2, 32], [2, 128, 2, 32], 1),
-        ([2, 32, 96], [2, 32, 32], 1),
-        ([128, 128], [128, 64], 1),
-    ],
-)
-def test_gather_not_last_dim(input_shape, index_shape, dim, device):
-    """
-    The ttnn.transpose does not support uint/int types yet.
-    Since this functionality is used inside the gather operation, on index tensor for test purposes for now we do it manually.
-    """
-    torch.manual_seed(0)
-
-    torch_dtype = torch.bfloat16
-    input = torch.randn(input_shape, dtype=torch_dtype)
-    index = torch.randint(0, input_shape[dim], index_shape, dtype=torch.int64)
-
-    torch_gather = torch.gather(input, dim, index)
-
-    if dim != -1:
-        input = input.transpose(dim, -1)
-        index = index.transpose(dim, -1)
-
-    ttnn_input = ttnn.from_torch(input, ttnn.bfloat16, layout=ttnn.Layout.TILE, device=device)
-    ttnn_index = ttnn.from_torch(index, ttnn.uint16, layout=ttnn.Layout.TILE, device=device)
-
-    ttnn_gather = ttnn.experimental.gather(ttnn_input, -1, index=ttnn_index)
-
-    if dim != -1:
-        ttnn_gather = ttnn.from_device(ttnn_gather).to_torch().transpose(dim, -1)
-        input = input.transpose(dim, -1)
-        index = index.transpose(dim, -1)
-
-    assert ttnn_gather.shape == index.shape
-    if dim != -1:
-        assert_with_pcc(torch_gather, ttnn_gather)
-    else:
-        assert_with_pcc(torch_gather, ttnn.to_torch(ttnn_gather))
-
-
-@skip_for_grayskull()
 @pytest.mark.parametrize(
     "input_shape, index_shape, dim",
     [
@@ -121,7 +83,6 @@ def test_gather_preallocated_output(input_shape, index_shape, dim, device):
     assert_with_pcc(torch_gather, ttnn.to_torch(ttnn_output))
 
 
-@skip_for_grayskull()
 @pytest.mark.parametrize(
     "input_shape, index_shape, dim",
     [
@@ -150,7 +111,6 @@ def test_gather_multicore_cases(input_shape, index_shape, dim, device):
     assert_with_pcc(torch_gather, ttnn.to_torch(ttnn_gather))
 
 
-@skip_for_grayskull()
 @pytest.mark.parametrize(
     "input_shape, index_shape, dim, torch_input_datatype, ttnn_input_datatype, ttnn_index_datatype",
     [
