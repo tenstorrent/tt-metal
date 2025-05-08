6476104e1e28dccdfabfcb9a03e1085ffb628698
Aswinmcw <azayasankaran@tenstorrent.com>
2025-05-08 11:30:40 +0000

    Add int32 support for unary_lt

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_unary_comp.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_unary_comp.h
@@ -71,6 +71,13 @@ inline void llk_math_eltwise_unary_sfpu_unary_gt(uint dst_index, uint param0, in
 
 // Unary lesser than
 template <bool APPROXIMATE>
+inline void llk_math_eltwise_unary_sfpu_unary_lt_int32(
+    uint dst_index, uint param0, int vector_mode = (int)VectorMode::RC) {
+    llk_math_eltwise_unary_sfpu_params<APPROXIMATE>(
+        ckernel::sfpu::calculate_comp_unary_int<APPROXIMATE, SfpuType::unary_lt>, dst_index, vector_mode, param0);
+}
+
+template <bool APPROXIMATE>
 inline void llk_math_eltwise_unary_sfpu_unary_lt_init() {
     llk_math_eltwise_unary_sfpu_init<SfpuType::unary_lt, APPROXIMATE>();
 }
