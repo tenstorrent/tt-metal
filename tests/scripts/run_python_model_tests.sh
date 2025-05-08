d7587678f2cb5213aa7afc0782a1b18cc667b7a7
Mohamed Bahnas <116673264+mbahnasTT@users.noreply.github.com>
2025-05-07 18:38:22 -0700

    Update run_python_model_tests.sh

--- tests/scripts/run_python_model_tests.sh
+++ tests/scripts/run_python_model_tests.sh
@@ -53,7 +53,7 @@ run_python_model_tests_wormhole_b0() {
 
     #Yolov10
     WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -svv tests/ttnn/integration_tests/yolov10/test_ttnn_yolov10.py::test_yolov10x
-    
+
     # ViT-base
     WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest tests/ttnn/integration_tests/vit/test_ttnn_optimized_sharded_vit_wh.py
 
