7281ed522111c4d1cb32f39b52dd8ef483d75aed
Borys Bradel <bbradel@tenstorrent.com>
2025-05-08 19:55:50 +0000

    #18485: add skips for tests not working on BH

--- tests/ttnn/unit_tests/operations/fused/test_group_norm_DRAM.py
+++ tests/ttnn/unit_tests/operations/fused/test_group_norm_DRAM.py
@@ -11,9 +11,10 @@ from loguru import logger
 import ttnn
 
 from tests.ttnn.utils_for_testing import assert_with_pcc, check_with_pcc
-from models.utility_functions import skip_for_wormhole_b0
+from models.utility_functions import skip_for_wormhole_b0, skip_for_blackhole
 
 
+@skip_for_blackhole("Fails on Blackhole. Issue #20913")
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 0}], indirect=True)
 @pytest.mark.parametrize(
     "N, C, H, W, num_groups, num_out_blocks, cores_y, cores_x",
