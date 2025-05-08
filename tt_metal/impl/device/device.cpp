f3fb62fe09bf8fe8ff93a6406163108eee06f6a2
Stanislav Minakov <sminakov@tenstorrent.com>
2025-05-08 05:18:02 +0000

    Ensure matching hooks on Buffer allocation/deallocation (#21806)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/21315
    
    ### Problem description
    Currently there is an issue with inconsistent hooks on Buffer
    allocation/deallocation, which happens specifically when a Buffer was
    cached by an OP (stored in override runtime args callbacks) and
    preserved by ProgramCache.
    This PR ensures that program cache is cleared at the appropriate time of
    device closing to ensure correct handling and adds diagnostic to easily
    find such issues in the future.
    
    We shouldn't be storing the Buffers inside of override runtime arguments
    to begin with, because it's basically a memory leak with program cache
    enabled, but this will be addressed separately for each of the OPs
    individually.
    
    ### What's changed
    Moved clearing program cache up in the Device::close()
    Stored a set of hooked buffers in GraphTracker with validation that we
    hook deallocation for exactly the same buffers where we hooked the
    allocation
    Stored in buffer whether its allocation was hooked and avoiding
    deallocation for them even if the hook was removed
    
    ### Checklist
    - [x] [All post commit CI
    passes](https://github.com/tenstorrent/tt-metal/actions/runs/14897246210)
    - [x] New/Existing tests provide coverage for changes

--- tt_metal/impl/device/device.cpp
+++ tt_metal/impl/device/device.cpp
@@ -1267,6 +1267,9 @@ bool Device::close() {
 
     tt_metal::detail::DumpDeviceProfileResults(this, ProfilerDumpState::LAST_CLOSE_DEVICE);
 
+    this->disable_and_clear_program_cache();
+    this->set_program_cache_misses_allowed(true);
+
     sub_device_manager_tracker_.reset(nullptr);
 
     std::unordered_map<chip_id_t, std::unordered_set<CoreCoord>> not_done_dispatch_cores;
@@ -1329,8 +1332,6 @@ bool Device::close() {
     this->compute_cores_.clear();
     this->storage_only_cores_.clear();
     this->ethernet_cores_.clear();
-    this->disable_and_clear_program_cache();
-    this->set_program_cache_misses_allowed(true);
     this->command_queue_programs_.clear();
     this->command_queues_.clear();
     this->sysmem_manager_.reset();
