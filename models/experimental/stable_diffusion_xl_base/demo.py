2c3b048e820557ab5098ad1dac7d23eca3f1b802
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2025-05-09 17:18:08 +0200

    #20050: SDXL VAE decoder (#21836)
    ### Ticket
    [Issue](https://github.com/tenstorrent/tt-metal/issues/20050)
    
    ### What's changed
    Functional implementation with unit test for `TtDecoder` and
    `TtAutoencoderKL`.
    Additionally pcc of VAE modules improved with `fp32_dest_acc_en`,
    `HiFi2` and group norm fallback to host in resnet block: [Issue
    #21833](https://github.com/tenstorrent/tt-metal/issues/21833)
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14911739220)
    CI passes - profiler regression fails same as base commit; seems
    unrelated
    - [x] [(Single-card) Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/14930383229)
    CI passes

--- models/experimental/stable_diffusion_xl_base/demo.py
+++ models/experimental/stable_diffusion_xl_base/demo.py
@@ -11,6 +11,7 @@ from diffusers import DiffusionPipeline
 from loguru import logger
 import ttnn
 from models.experimental.stable_diffusion_xl_base.tt.tt_unet import TtUNet2DConditionModel
+from models.experimental.stable_diffusion_xl_base.vae.tt.tt_autoencoder_kl import TtAutoencoderKL
 
 
 # Copied from sdxl pipeline
@@ -108,6 +109,7 @@ def run_demo_inference(
     prompt,
     num_inference_steps,
     classifier_free_guidance,
+    vae_on_device,
 ):
     torch.manual_seed(0)
 
@@ -141,6 +143,7 @@ def run_demo_inference(
         conv_weights_dtype=ttnn.bfloat16,
         transformer_weights_dtype=ttnn.bfloat16,
     )
+    tt_vae = TtAutoencoderKL(ttnn_device, pipeline.vae.state_dict()) if vae_on_device else None
 
     cpu_device = "cpu"
 
@@ -355,14 +358,29 @@ def run_demo_inference(
     # VAE upcasting to float32 is happening in the reference SDXL demo if VAE dtype is float16. If it's bfloat16, it will not be upcasted.
     latents = latents / pipeline.vae.config.scaling_factor
 
-    image = pipeline.vae.decode(latents, return_dict=False)[0]
+    if vae_on_device:
+        B, C, H, W = list(latents.shape)
+        latents = torch.permute(latents, (0, 2, 3, 1))
+        latents = latents.reshape(1, 1, B * H * W, C)
+        ttnn_latents = ttnn.from_torch(
+            latents,
+            dtype=ttnn.bfloat16,
+            device=ttnn_device,
+            layout=ttnn.ROW_MAJOR_LAYOUT,
+            memory_config=ttnn.L1_MEMORY_CONFIG,
+        )
+
+        logger.info("Running TT VAE")
+        image = tt_vae.forward(ttnn_latents, [B, C, H, W])
+    else:
+        image = pipeline.vae.decode(latents, return_dict=False)[0]
     image = pipeline.image_processor.postprocess(image, output_type="pil")[0]
 
     image.save("output.png")
     logger.info(f"Image saved to output.png")
 
 
-@pytest.mark.parametrize("device_params", [{"l1_small_size": 4 * 16384}], indirect=True)
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 6 * 16384}], indirect=True)
 @pytest.mark.parametrize(
     "prompt",
     (("An astronaut riding a green horse"),),
@@ -391,4 +409,37 @@ def test_demo(
         prompt,
         num_inference_steps,
         classifier_free_guidance,
+        vae_on_device=False,
+    )
+
+
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 6 * 16384}], indirect=True)
+@pytest.mark.parametrize(
+    "prompt",
+    (("An astronaut riding a green horse"),),
+)
+@pytest.mark.parametrize(
+    "num_inference_steps",
+    ((50),),
+)
+@pytest.mark.parametrize(
+    "classifier_free_guidance",
+    [
+        (True),
+        (False),
+    ],
+    ids=("with_classifier_free_guidance", "no_classifier_free_guidance"),
+)
+def test_demo_with_vae(
+    device,
+    prompt,
+    num_inference_steps,
+    classifier_free_guidance,
+):
+    return run_demo_inference(
+        device,
+        prompt,
+        num_inference_steps,
+        classifier_free_guidance,
+        vae_on_device=True,
     )
