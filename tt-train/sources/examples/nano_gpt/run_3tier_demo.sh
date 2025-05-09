da06c4cfd7684846e1e45713a4c53a196768791d
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2025-05-09 20:28:21 +0000

    modify script

--- tt-train/sources/examples/nano_gpt/run_3tier_demo.sh
+++ tt-train/sources/examples/nano_gpt/run_3tier_demo.sh
@@ -3,16 +3,16 @@ echo "Running nanogpt 3tier demo"
 # copy to LBOX_MPI_1
 scp  /home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/nano_gpt ttuser@11.228.0.11:/home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/
 scp  /home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/nano_gpt_optimizer ttuser@11.228.0.11:/home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/
-# scp  /home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/nano_gpt_aggregator ttuser@11.228.0.11:/home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/
+scp  /home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/nano_gpt_aggregator ttuser@11.228.0.11:/home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/
 
 # copy to LBOX_MPI_2
 scp  /home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/nano_gpt ttuser@11.228.0.14:/home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/
 scp  /home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/nano_gpt_optimizer ttuser@11.228.0.14:/home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/
-# scp  /home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/nano_gpt_aggregator ttuser@11.228.0.14:/home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/
+scp  /home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/nano_gpt_aggregator ttuser@11.228.0.14:/home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/
 
 # copy to LBOX_MPI_3
-scp  /home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/nano_gpt ttuser@11.228.0.15:/home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/
-scp  /home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/nano_gpt_optimizer ttuser@11.228.0.15:/home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/
+# scp  /home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/nano_gpt ttuser@11.228.0.15:/home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/
+# scp  /home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/nano_gpt_optimizer ttuser@11.228.0.15:/home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/
 # scp  /home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/nano_gpt_aggregator ttuser@11.228.0.15:/home/ttuser/git/tt-metal/tt-train/build/sources/examples/nano_gpt/
 
 # copy config file to LBOX_MPI_xxx
@@ -24,6 +24,6 @@ echo "executables and config file succesfully copied"
 
 echo "Running nano_gpt 3tier demo..."
 mpirun --hostfile ~/mpi_hosts \
-  -np 1 /bin/bash -c 'cd /home/ttuser/git/tt-metal && TT_METAL_HOME=/home/ttuser/git/tt-metal TT_METAL_LOGGER_LEVEL=FATAL ./tt-train/build/sources/examples/nano_gpt/nano_gpt' \
-  : -np 1 /bin/bash -c 'cd /home/ttuser/git/tt-metal && TT_METAL_HOME=/home/ttuser/git/tt-metal TT_METAL_LOGGER_LEVEL=FATAL ./tt-train/build/sources/examples/nano_gpt/nano_gpt_aggregator' \
-  : -np 1 /bin/bash -c 'cd /home/ttuser/git/tt-metal && TT_METAL_HOME=/home/ttuser/git/tt-metal TT_METAL_LOGGER_LEVEL=FATAL ./tt-train/build/sources/examples/nano_gpt/nano_gpt_optimizer'
+  -np 1 /bin/bash -c 'tt-smi -r && cd /home/ttuser/git/tt-metal && TT_METAL_HOME=/home/ttuser/git/tt-metal TT_METAL_LOGGER_LEVEL=FATAL ./tt-train/build/sources/examples/nano_gpt/nano_gpt' \
+  : -np 1 /bin/bash -c 'tt-smi -r && cd /home/ttuser/git/tt-metal && TT_METAL_HOME=/home/ttuser/git/tt-metal TT_METAL_LOGGER_LEVEL=FATAL ./tt-train/build/sources/examples/nano_gpt/nano_gpt_aggregator' \
+  : -np 1 /bin/bash -c 'tt-smi -r && cd /home/ttuser/git/tt-metal && TT_METAL_HOME=/home/ttuser/git/tt-metal TT_METAL_LOGGER_LEVEL=FATAL ./tt-train/build/sources/examples/nano_gpt/nano_gpt_optimizer'
