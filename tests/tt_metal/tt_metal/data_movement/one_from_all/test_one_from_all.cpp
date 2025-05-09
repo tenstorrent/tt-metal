9f2ae85912e62890631e38491e9671fc37a4b9fa
Ata Tuzuner <atuzuner@tenstorrent.com>
2025-05-09 13:43:00 +0000

    Documentation and renaming friendly

--- tests/tt_metal/tt_metal/data_movement/one_from_all/test_one_from_all.cpp
+++ tests/tt_metal/tt_metal/data_movement/one_from_all/test_one_from_all.cpp
@@ -19,7 +19,7 @@ namespace unit_tests::dm::core_to_core {
 struct OneFromAllConfig {
     uint32_t test_id = 0;
     CoreCoord master_core_coord = CoreCoord();
-    CoreRangeSet slave_core_set;
+    CoreRangeSet subordinate_core_set;
     uint32_t num_of_transactions = 0;
     uint32_t transaction_size_pages = 0;
     uint32_t page_size_bytes = 0;
@@ -40,12 +40,12 @@ bool run_dm(IDevice* device, const OneFromAllConfig& test_config) {
 
     // Sharded L1 buffers
     CoreRangeSet master_core_set({CoreRange(test_config.master_core_coord)});
-    size_t total_slave_cores = test_config.slave_core_set.num_cores();
+    size_t total_subordinate_cores = test_config.subordinate_core_set.num_cores();
 
     const size_t total_size_bytes = test_config.num_of_transactions * test_config.transaction_size_pages *
-                                    test_config.page_size_bytes * total_slave_cores;
+                                    test_config.page_size_bytes * total_subordinate_cores;
     const size_t total_size_pages =
-        test_config.num_of_transactions * test_config.transaction_size_pages * total_slave_cores;
+        test_config.num_of_transactions * test_config.transaction_size_pages * total_subordinate_cores;
 
     auto master_shard_parameters = ShardSpecBuffer(
         master_core_set,
@@ -63,9 +63,9 @@ bool run_dm(IDevice* device, const OneFromAllConfig& test_config) {
     });
     uint32_t master_l1_byte_address = master_l1_buffer->address();
 
-    const size_t slave_size_bytes =
+    const size_t subordinate_size_bytes =
         test_config.num_of_transactions * test_config.transaction_size_pages * test_config.page_size_bytes;
-    const size_t slave_size_pages = test_config.num_of_transactions * test_config.transaction_size_pages;
+    const size_t subordinate_size_pages = test_config.num_of_transactions * test_config.transaction_size_pages;
 
     // Compile-time arguments for kernels
     vector<uint32_t> gatherer_compile_args = {
@@ -74,7 +74,7 @@ bool run_dm(IDevice* device, const OneFromAllConfig& test_config) {
         (uint32_t)test_config.transaction_size_pages,
         (uint32_t)test_config.page_size_bytes,
         (uint32_t)test_config.test_id,
-        (uint32_t)total_slave_cores,
+        (uint32_t)total_subordinate_cores,
     };
 
     // Kernels
@@ -90,26 +90,26 @@ bool run_dm(IDevice* device, const OneFromAllConfig& test_config) {
     // Runtime Arguments
     vector<uint32_t> master_runtime_args;
 
-    // Create buffers for each slave core and add to runtime args
-    vector<shared_ptr<Buffer>> slave_l1_buffers;
-    for (auto& core : corerange_to_cores(test_config.slave_core_set)) {
-        auto slave_shard_parameters = ShardSpecBuffer(
+    // Create buffers for each subordinate core and add to runtime args
+    vector<shared_ptr<Buffer>> subordinate_l1_buffers;
+    for (auto& core : corerange_to_cores(test_config.subordinate_core_set)) {
+        auto subordinate_shard_parameters = ShardSpecBuffer(
             CoreRangeSet({CoreRange(core)}),
-            {1, slave_size_bytes / 2},
+            {1, subordinate_size_bytes / 2},
             ShardOrientation::ROW_MAJOR,
             {1, test_config.page_size_bytes / 2},
-            {1, slave_size_pages});
-        ShardedBufferConfig slave_buffer_config{
+            {1, subordinate_size_pages});
+        ShardedBufferConfig subordinate_buffer_config{
             .device = device,
-            .size = slave_size_bytes,
+            .size = subordinate_size_bytes,
             .page_size = test_config.page_size_bytes,
             .buffer_type = BufferType::L1,
             .buffer_layout = TensorMemoryLayout::WIDTH_SHARDED,
-            .shard_parameters = std::move(slave_shard_parameters),
+            .shard_parameters = std::move(subordinate_shard_parameters),
         };
-        auto slave_l1_buffer = CreateBuffer(slave_buffer_config);
-        slave_l1_buffers.push_back(slave_l1_buffer);
-        master_runtime_args.push_back((uint32_t)slave_l1_buffer->address());
+        auto subordinate_l1_buffer = CreateBuffer(subordinate_buffer_config);
+        subordinate_l1_buffers.push_back(subordinate_l1_buffer);
+        master_runtime_args.push_back((uint32_t)subordinate_l1_buffer->address());
 
         CoreCoord physical_core = device->worker_core_from_logical_core(core);
         master_runtime_args.push_back(physical_core.x);
@@ -130,10 +130,10 @@ bool run_dm(IDevice* device, const OneFromAllConfig& test_config) {
     vector<uint32_t> packed_output;
 
     // Launch program and record outputs
-    for (size_t i = 0; i < total_slave_cores; i++) {
-        auto begin = packed_input.data() + i * slave_size_bytes / sizeof(uint32_t);
-        auto end = packed_input.data() + (i + 1) * slave_size_bytes / sizeof(uint32_t);
-        detail::WriteToBuffer(slave_l1_buffers[i], vector<uint32_t>(begin, end));
+    for (size_t i = 0; i < total_subordinate_cores; i++) {
+        auto begin = packed_input.data() + i * subordinate_size_bytes / sizeof(uint32_t);
+        auto end = packed_input.data() + (i + 1) * subordinate_size_bytes / sizeof(uint32_t);
+        detail::WriteToBuffer(subordinate_l1_buffers[i], vector<uint32_t>(begin, end));
     }
     MetalContext::instance().get_cluster().l1_barrier(device->id());
 
@@ -168,15 +168,16 @@ TEST_F(DeviceFixture, TensixDataMovementOneFromAllPacketSizes) {
 
     // Cores
     CoreCoord master_core_coord = {0, 0};
-    CoreRangeSet slave_core_set = {CoreRange(CoreCoord(1, 1), CoreCoord(4, 4))};
-    size_t total_slave_cores = slave_core_set.num_cores();
+    CoreRangeSet subordinate_core_set = {CoreRange(CoreCoord(1, 1), CoreCoord(4, 4))};
+    size_t total_subordinate_cores = subordinate_core_set.num_cores();
 
     uint32_t l1_size = 1024 * 1024;  // 1MB
 
     for (uint32_t num_of_transactions = 1; num_of_transactions <= max_transactions; num_of_transactions *= 2) {
         for (uint32_t transaction_size_pages = 1; transaction_size_pages <= max_transaction_size_pages;
              transaction_size_pages *= 2) {
-            if (num_of_transactions * transaction_size_pages * page_size_bytes * total_slave_cores >= 1024 * 1024) {
+            if (num_of_transactions * transaction_size_pages * page_size_bytes * total_subordinate_cores >=
+                1024 * 1024) {
                 continue;
             }
 
@@ -184,7 +185,7 @@ TEST_F(DeviceFixture, TensixDataMovementOneFromAllPacketSizes) {
             unit_tests::dm::core_to_core::OneFromAllConfig test_config = {
                 .test_id = 6,
                 .master_core_coord = master_core_coord,
-                .slave_core_set = slave_core_set,
+                .subordinate_core_set = subordinate_core_set,
                 .num_of_transactions = num_of_transactions,
                 .transaction_size_pages = transaction_size_pages,
                 .page_size_bytes = page_size_bytes,
