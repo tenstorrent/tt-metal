f5b0963cf27a2f6d0a636f3ea68ecd6ee71209a8
Aswinmcw <azayasankaran@tenstorrent.com>
2025-05-09 06:19:20 +0000

    Use template to pass ins_mod

--- tt_metal/include/compute_kernel_api/binary_bitwise_sfpu.h
+++ tt_metal/include/compute_kernel_api/binary_bitwise_sfpu.h
@@ -34,11 +34,11 @@ namespace ckernel {
  */
 // clang-format on
 ALWI void bitwise_and_binary_tile(uint32_t idst0, uint32_t idst1) {
-    MATH((llk_math_eltwise_binary_sfpu_bitwise<APPROX, ckernel::sfpu::BinaryBitwiseOp::AND>(idst0, idst1)));
+    MATH((llk_math_eltwise_binary_sfpu_bitwise<APPROX, ckernel::sfpu::BinaryBitwiseOp::AND, INT32>(idst0, idst1)));
 }
 
 ALWI void bitwise_and_uint16_binary_tile(uint32_t idst0, uint32_t idst1) {
-    MATH((llk_math_eltwise_binary_sfpu_bitwise_uint16<APPROX, ckernel::sfpu::BinaryBitwiseOp::AND>(idst0, idst1)));
+    MATH((llk_math_eltwise_binary_sfpu_bitwise<APPROX, ckernel::sfpu::BinaryBitwiseOp::AND, LO16>(idst0, idst1)));
 }
 
 ALWI void or_binary_tile(uint32_t idst0, uint32_t idst1) {
