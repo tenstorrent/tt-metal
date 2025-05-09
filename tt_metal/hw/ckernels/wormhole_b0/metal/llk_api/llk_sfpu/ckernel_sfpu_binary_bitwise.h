5bb4e075ab5ebcbd1302588a08c536a65b7f2342
Aswinmcw <azayasankaran@tenstorrent.com>
2025-05-09 06:21:11 +0000

    Update submodule pointer

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_binary_bitwise.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_binary_bitwise.h
@@ -18,10 +18,5 @@ inline void calculate_sfpu_binary_bitwise(const uint dst_offset) {
     _calculate_sfpu_binary_bitwise_<APPROXIMATION_MODE, BITWISE_OP, DTYPE, ITERATIONS>(dst_offset);
 }
 
-template <bool APPROXIMATION_MODE, BinaryBitwiseOp BITWISE_OP, uint DTYPE = INT32, int ITERATIONS = 8>
-inline void calculate_sfpu_binary_bitwise_uint16(const uint dst_offset) {
-    _calculate_sfpu_binary_bitwise_<APPROXIMATION_MODE, BITWISE_OP, DTYPE, ITERATIONS>(dst_offset);
-}
-
 }  // namespace sfpu
 }  // namespace ckernel
