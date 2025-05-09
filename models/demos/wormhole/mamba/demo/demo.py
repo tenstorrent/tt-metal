0040ca8bab92035d4809cf9acc738d76c392c42a
Evan Smal <esmal@tenstorrent.com>
2025-05-09 11:58:50 +0000

    Update Mamba demo performance targets

--- models/demos/wormhole/mamba/demo/demo.py
+++ models/demos/wormhole/mamba/demo/demo.py
@@ -372,11 +372,11 @@ def run_mamba_demo(
     )
     logger.info(f"Time to first token: {(1e3 * time_to_first_token):.2f} ms")
 
-    chunk_size_to_prefill_targets_tok_per_s = {32: 135.0, 128: 448.0}  # perf is different for different chunk sizes
+    chunk_size_to_prefill_targets_tok_per_s = {32: 135.0, 128: 400.0}  # perf is different for different chunk sizes
     targets = {
         "prefill_t/s": chunk_size_to_prefill_targets_tok_per_s[prefill_chunk_size],
-        "decode_t/s": 442.0,
-        "decode_t/s/u": 13.8,
+        "decode_t/s": 400.0,
+        "decode_t/s/u": 12.5,
     }
     warmup_iterations = {"inference_prefill": 0, "inference_decode": 0}
 
@@ -396,7 +396,7 @@ def run_mamba_demo(
     )
 
     if assert_on_performance_measurements:
-        verify_perf(measurements, targets, high_tol_percentage=1.20)
+        verify_perf(measurements, targets, high_tol_percentage=1.25)
     else:
         logger.warning(f"Skipping performance checks (this is expected for functional tests)")
 
