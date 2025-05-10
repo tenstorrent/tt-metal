b0f6e9bb4b9edd4f86f98ff29136cb6cd4452603
Evan Smal <esmal@tenstorrent.com>
2025-05-10 14:27:18 +0000

    improve perf

--- ttnn/cpp/ttnn/operations/experimental/cnn/convert_to_hwc/device/kernels/writer_convert_to_hwc.cpp
+++ ttnn/cpp/ttnn/operations/experimental/cnn/convert_to_hwc/device/kernels/writer_convert_to_hwc.cpp
@@ -7,44 +7,40 @@
 
 constexpr uint32_t TILE_SIZE = 32;
 constexpr uint32_t ELEMENT_SIZE_BYTES = 2;
-constexpr uint32_t STICK_SIZE = TILE_SIZE * ELEMENT_SIZE_BYTES;
+constexpr uint32_t TILE_STICK_SIZE_BYTES = TILE_SIZE * ELEMENT_SIZE_BYTES;
 
-FORCE_INLINE void copy_padded_sticks(
-    uint64_t l1_read_addr,
-    uint32_t& l1_write_addr,
-    uint32_t num_sticks,
-    uint32_t stick_size,
-    uint32_t padded_stick_size) {
-    for (uint32_t row = 0; row < num_sticks; row++) {
-        noc_async_read(l1_read_addr, l1_write_addr, stick_size);
-        l1_read_addr += padded_stick_size;
-        l1_write_addr += stick_size;
+template <uint32_t StickSize, uint32_t PaddedStickSize, uint32_t NumSticks>
+FORCE_INLINE void copy_padded_sticks(uint64_t l1_read_addr, uint32_t& l1_write_addr) {
+    noc_async_read_one_packet_set_state(l1_read_addr, StickSize);
+    for (uint32_t row = 0; row < NumSticks; row++) {
+        noc_async_read_one_packet_with_state<true>(l1_read_addr, l1_write_addr);
+        l1_read_addr += PaddedStickSize;
+        l1_write_addr += StickSize;
     }
 }
 
 void kernel_main() {
     constexpr uint32_t cb_in_transpose = get_compile_time_arg_val(0);
     constexpr uint32_t cb_out = get_compile_time_arg_val(1);
-    constexpr uint32_t channels = get_compile_time_arg_val(2);
-    constexpr uint32_t hw = get_compile_time_arg_val(3);
+    constexpr uint32_t channels = get_compile_time_arg_val(2);  // stick size
+    constexpr uint32_t hw = get_compile_time_arg_val(3);        // total number of sticks to copy into output
 
     constexpr uint32_t channel_size = channels * ELEMENT_SIZE_BYTES;
-
-    constexpr uint32_t num_full_tiles = hw / TILE_SIZE;     // TODO: make compile time
-    constexpr uint32_t hw_per_final_tile = hw % TILE_SIZE;  // TODO: make compile time
+    constexpr uint32_t num_full_tiles = hw / TILE_SIZE;
+    constexpr uint32_t hw_per_final_tile = hw % TILE_SIZE;
 
     const uint32_t base_l1_write_addr = get_write_ptr(cb_out);
     uint32_t l1_write_addr = base_l1_write_addr;
     for (uint32_t i = 0; i < num_full_tiles; i++) {
         cb_wait_front(cb_in_transpose, 1);
         const uint64_t l1_read_addr = get_noc_addr(get_read_ptr(cb_in_transpose));
-        copy_padded_sticks(l1_read_addr, l1_write_addr, TILE_SIZE, channel_size, STICK_SIZE);
+        copy_padded_sticks<channel_size, TILE_STICK_SIZE_BYTES, TILE_SIZE>(l1_read_addr, l1_write_addr);
         cb_pop_front(cb_in_transpose, 1);
     }
-    if (hw_per_final_tile > 0) {
+    if constexpr (hw_per_final_tile > 0) {
         cb_wait_front(cb_in_transpose, 1);
         const uint64_t l1_read_addr = get_noc_addr(get_read_ptr(cb_in_transpose));
-        copy_padded_sticks(l1_read_addr, l1_write_addr, hw_per_final_tile, channel_size, STICK_SIZE);
+        copy_padded_sticks<channel_size, TILE_STICK_SIZE_BYTES, hw_per_final_tile>(l1_read_addr, l1_write_addr);
         cb_pop_front(cb_in_transpose, 1);
     }
     noc_async_read_barrier();
