98dc84687de4dd3c2631fd35656708b9768c666c
Evan Smal <esmal@tenstorrent.com>
2025-05-10 13:48:09 +0000

    move rt to ct args

--- ttnn/cpp/ttnn/operations/experimental/cnn/convert_to_hwc/device/convert_to_hwc_op.cpp
+++ ttnn/cpp/ttnn/operations/experimental/cnn/convert_to_hwc/device/convert_to_hwc_op.cpp
@@ -24,13 +24,13 @@ void ConvertToHWC::validate(const std::vector<Tensor>& input_tensors) const {
 
     TT_FATAL(input.is_sharded(), "Input tensor must be sharded");
 
-    const auto& input_shard_spec = input.memory_config().shard_spec.value();
+    const auto& input_shard_spec = input.memory_config().shard_spec().value();
     // TT_FATAL(
     // input_shard_spec.memory_layout == tt::tt_metal::TensorMemoryLayout::WIDTH_SHARDED,
     //"Input tensor must be width sharded");
     TT_FATAL(
         this->memory_config.is_sharded() &&
-            this->memory_config.memory_layout == tt::tt_metal::TensorMemoryLayout::HEIGHT_SHARDED,
+            this->memory_config.memory_layout() == tt::tt_metal::TensorMemoryLayout::HEIGHT_SHARDED,
         "Output tensor must be height sharded");
 }
 
