6790aadadc091cb0f844e82442894d494486a9b4
Almeet Bhullar <109364210+abhullar-tt@users.noreply.github.com>
2025-05-12 18:24:59 -0400

    Update Watcher checks and DPrint addresses to fix BH related host <-> dev address mismatches  (#21944)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/21839
    
    ### Problem description
    Some of the BH gtests are running on active eth cores are failing
    because `DPRINT_BUFFERS_COUNT` has different value on host vs. device
    since the device side macros are ignored on host.
    
    ### What's changed
    Fix test in ticket above and tests included in syseng upstream testing
    ```
    CommandQueueSingleCardProgramFixture.TensixActiveEthTestSubDeviceMyLogicalCoordinates
    CommandQueueSingleCardProgramFixture.TestLogicalCoordinatesEth
    ```
    by setting `DPRINT_BUFFERS_COUNT` to max.
    
    This PR also includes:
    - update Watcher to throw error when DRAM endpoint is not the same as
    the DRAM endpoint exposed through Runtime API. This avoids a known BH
    DRAM HW bug (SYS-592)
    - add back to static asserts (one static assert is failing for WH eth
    but this exists on main). Enabled on this pr rather than
    https://github.com/tenstorrent/tt-metal/pull/21476 because I updated
    dev_msgs and wanted to make sure nothing is being violated.
    
    FYI @williamlyTT 
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14952846151)
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14952847731)

--- tt_metal/tt_metal.cpp
+++ tt_metal/tt_metal.cpp
@@ -321,7 +321,7 @@ bool WriteToDeviceDRAMChannel(IDevice* device, int dram_channel, uint32_t addres
         "Cannot write to reserved DRAM region, addresses [0, {}) are reserved!",
         device->allocator()->get_base_allocator_addr(HalMemType::DRAM));
     tt::tt_metal::MetalContext::instance().get_cluster().write_dram_vec(
-        host_buffer, tt_target_dram{device->id(), dram_channel, 0}, address);
+        host_buffer, device->id(), dram_channel, address);
     return pass;
 }
 
@@ -330,7 +330,7 @@ bool ReadFromDeviceDRAMChannel(
     bool pass = true;
     tt::tt_metal::MetalContext::instance().get_cluster().dram_barrier(device->id());
     tt::tt_metal::MetalContext::instance().get_cluster().read_dram_vec(
-        host_buffer, size, tt_target_dram{device->id(), dram_channel, 0}, address);
+        host_buffer, size, device->id(), dram_channel, address);
     return pass;
 }
 
