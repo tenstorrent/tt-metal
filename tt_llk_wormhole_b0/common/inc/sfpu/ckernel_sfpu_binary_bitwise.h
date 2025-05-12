63615625fc2c1fdf116c882b858bb76bed907343
Aswin Zayasankaran <156493059+Aswinmcw@users.noreply.github.com>
2025-05-12 21:34:47 +0530

    Add DataType instruction modifier template for bitwise ops (#188)
    ### Ticket
    NA
    
    ### Problem description
    Only int32 dtype supported
    
    ### What's changed
    Expose the dtype param as a template parameter
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)

--- tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_binary_bitwise.h
+++ tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_binary_bitwise.h
@@ -4,7 +4,11 @@
 
 #pragma once
 
+#include <type_traits>
+
+#include "ckernel_addrmod.h"
 #include "ckernel_ops.h"
+#include "llk_defs.h"
 #include "sfpi.h"
 
 namespace ckernel
@@ -19,16 +23,17 @@ enum class BinaryBitwiseOp : uint8_t
     XOR = 2,
 };
 
-template <bool APPROXIMATION_MODE, BinaryBitwiseOp BITWISE_OP, int ITERATIONS = 8>
+template <bool APPROXIMATION_MODE, BinaryBitwiseOp BITWISE_OP, InstrModLoadStore INSTRUCTION_MODE = INT32, int ITERATIONS = 8>
 inline void _calculate_sfpu_binary_bitwise_(const uint dst_offset)
 {
+    constexpr auto instruction_mode = static_cast<std::underlying_type_t<InstrModLoadStore>>(INSTRUCTION_MODE);
     // SFPU microcode
     for (int d = 0; d < ITERATIONS; d++)
     {
         constexpr uint dst_tile_size = 64;
 
-        TTI_SFPLOAD(0, 4, 3, 0);
-        TT_SFPLOAD(1, 4, 3, dst_offset * dst_tile_size);
+        TTI_SFPLOAD(0, instruction_mode, 3, 0);
+        TT_SFPLOAD(1, instruction_mode, 3, dst_offset * dst_tile_size);
 
         if constexpr (BITWISE_OP == BinaryBitwiseOp::AND)
         {
@@ -43,7 +48,7 @@ inline void _calculate_sfpu_binary_bitwise_(const uint dst_offset)
             TTI_SFPXOR(0, 1, 0, 0);
         }
 
-        TTI_SFPSTORE(0, 4, 3, 0);
+        TTI_SFPSTORE(0, instruction_mode, 3, 0);
         sfpi::dst_reg++;
     }
 }
