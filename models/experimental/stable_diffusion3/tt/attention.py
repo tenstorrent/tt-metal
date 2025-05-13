8d37e4c51f65a6d163831a0e0ce6fb4914e77634
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2025-05-13 17:12:41 +0200

    increase Q chunk size in attention for better fidelity

--- models/experimental/stable_diffusion3/tt/attention.py
+++ models/experimental/stable_diffusion3/tt/attention.py
@@ -233,8 +233,8 @@ class TtAttention:
 
         program_config = ttnn.SDPAProgramConfig(
             compute_with_storage_grid_size=device.compute_with_storage_grid_size(),
-            q_chunk_size=128,
-            k_chunk_size=1024,
+            q_chunk_size=768,
+            k_chunk_size=256,
             exp_approx_mode=False,  # NOTE: False is more correct
         )
 
