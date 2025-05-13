e3c4e233645a1246b62121d1fe53e1ac4d36a4e2
Sean Nijjar <sean.nijjar@gmail.com>
2025-05-13 14:54:00 +0000

    fix after rebase

--- tests/tt_metal/tt_fabric/fabric_data_movement/test_basic_1d_fabric.cpp
+++ tests/tt_metal/tt_fabric/fabric_data_movement/test_basic_1d_fabric.cpp
@@ -673,7 +673,7 @@ TEST_F(Fabric1DFixture, TestEDMConnectionStressTestQuick) {
     uint32_t fabric_write_dest_bank_addr = 0x50000;
 
     // Set up the EDM connection config
-    const auto edm_config = get_1d_fabric_config();
+    const auto edm_config = get_tt_fabric_config();
 
     // For each epoch, run with increasing number of workers
     log_debug(tt::LogTest, "Starting EDM connection stress test");
