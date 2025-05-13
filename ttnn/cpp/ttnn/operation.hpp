ab279ff29a386ced03becf7c251a7ffea70ae9f4
Andrew Fuller <afuller@tenstorrent.com>
2025-05-13 13:23:43 -0700

    [skip ci] Format core TT-NN files (#22050)
    ### Ticket
    N/A
    
    ### Problem description
    Upcoming work will shuffle some files around; pre-commit will attempt to
    format touched files. Changed content has a higher risk of merge
    conflicts.
    
    ### What's changed
    Do a dedicated pass for just formatting -- no logic nor location change.

--- ttnn/cpp/ttnn/operation.hpp
+++ ttnn/cpp/ttnn/operation.hpp
@@ -449,9 +449,8 @@ constexpr bool implements_get_parallelization_strategy() {
 
 template <typename ConcreteOperation>
 auto default_create_output_tensors(
-    const ConcreteOperation& operation,
-    const Tensors& input_tensors,
-    const OptionalTensors& optional_output_tensors) -> ProgramOutputTensors<ConcreteOperation> {
+    const ConcreteOperation& operation, const Tensors& input_tensors, const OptionalTensors& optional_output_tensors)
+    -> ProgramOutputTensors<ConcreteOperation> {
     using OutputTensors = ProgramOutputTensors<ConcreteOperation>;
     OutputTensors output_tensors;
 
