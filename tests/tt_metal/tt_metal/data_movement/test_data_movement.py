7d812874732284f99d9bac7f2367d3c255561cf4
Ata Tuzuner <atuzuner@tenstorrent.com>
2025-05-13 20:41:33 +0000

    Adjusting wormhole test bounds

--- tests/tt_metal/tt_metal/data_movement/test_data_movement.py
+++ tests/tt_metal/tt_metal/data_movement/test_data_movement.py
@@ -91,7 +91,7 @@ test_bounds = {
             "riscv_0": {"latency": {"lower": 200, "upper": 100000}, "bandwidth": 0.04},
         },
         15: {
-            "riscv_1": {"latency": {"lower": 700, "upper": 30000}, "bandwidth": 0.7},
+            "riscv_1": {"latency": {"lower": 700, "upper": 120000}, "bandwidth": 0.7},
         },
     },
     "blackhole": {
