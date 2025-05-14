173c969d4b03b75f1b8c925c85baa2e527d0ffb0
Filip Vranic <fvranic@tenstorrent.com>
2025-05-14 15:43:28 +0200

    build: update version of exalens (#195)
    ### Ticket
    None
    
    ### Problem description
    After latest FW update for BH, according to the Debug team, we would
    face issues running our test infra or IRD machines if we don't update
    exalens version. This shouldn't affect CI runner, but we'll update all
    the Docker images anyway.
    
    ### What's changed
    New exalens version containing fixes for UMD.
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update

--- .github/scripts/install-exalens.sh
+++ .github/scripts/install-exalens.sh
@@ -5,7 +5,7 @@
 
 set -e
 
-wget -O ttexalens-0.1.250326+dev.0c4381a-cp310-cp310-linux_x86_64.whl \
-    https://github.com/tenstorrent/tt-exalens/releases/download/0.1/ttexalens-0.1.250326+dev.0c4381a-cp310-cp310-linux_x86_64.whl
-pip install --no-cache-dir ttexalens-0.1.250326+dev.0c4381a-cp310-cp310-linux_x86_64.whl
-rm ttexalens-0.1.250326+dev.0c4381a-cp310-cp310-linux_x86_64.whl
+wget -O ttexalens-0.1.250514+dev.d45dfb0-cp310-cp310-linux_x86_64.whl \
+    https://github.com/tenstorrent/tt-exalens/releases/download/0.1.250514/ttexalens-0.1.250514+dev.d45dfb0-cp310-cp310-linux_x86_64.whl
+pip install --no-cache-dir ttexalens-0.1.250514+dev.d45dfb0-cp310-cp310-linux_x86_64.whl
+rm ttexalens-0.1.250514+dev.d45dfb0-cp310-cp310-linux_x86_64.whl
