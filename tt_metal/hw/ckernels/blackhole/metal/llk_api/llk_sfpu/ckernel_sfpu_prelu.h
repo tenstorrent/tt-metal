d918ae88b02652f233e93c5084175216f4f738bc
Filip Vranic <fvranic@tenstorrent.com>
2025-05-14 18:38:54 +0200

    chore: intake llk changes - May 12th, 2025 (#22026)
    ### Ticket
    None
    
    ### Problem description
    This is an uplift of LLK changes from May 12th, 2025.
    
    This intake contains the following change:
    
    https://github.com/tenstorrent/tt-llk/pull/189 (fix: remove
    hardware-specific header files shared with tt-metal)
    
    ### What's changed
    As part of the effort to reduce duplicate files between tt-llk and
    tt-metal, we've pulled in `ckernel_sfpu_converter.h` to tt-llk, and are
    removing it from tt-metal.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/15015376674)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/15015790690)
    CI with demo tests passes (if applicable)

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_prelu.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_prelu.h
@@ -7,7 +7,7 @@
 #include "ckernel.h"
 #include "ckernel_defs.h"
 #include "noc_nonblocking_api.h"
-#include "ckernel_sfpu_converter.h"
+#include "sfpu/ckernel_sfpu_converter.h"
 
 using namespace sfpi;
 
@@ -17,9 +17,7 @@ namespace sfpu {
 template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
 inline void calculate_prelu(const uint value) {
     // SFPU microcode
-    Converter c_value;
-    c_value.u = value;
-    vFloat init = c_value.f;
+    vFloat init = Converter::as_float(value);
 
 #pragma GCC unroll 0
     for (int d = 0; d < ITERATIONS; d++) {
