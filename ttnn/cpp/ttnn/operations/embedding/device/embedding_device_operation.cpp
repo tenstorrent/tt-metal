dc6680ade1b9b7bea16c766e03d8b94e54a1db4d
kpaigwar <kpaigwar@tenstorrent.com>
2025-05-14 00:23:48 +0000

    minor

--- ttnn/cpp/ttnn/operations/embedding/device/embedding_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/embedding/device/embedding_device_operation.cpp
@@ -40,13 +40,13 @@ void Embeddings::validate(const std::vector<Tensor> &input_tensors) const {
             TT_FATAL(weights.get_padded_shape()[-1] % shard_spec->shape[1] == 0, "Number of columns in table {} must be factor of shard width {}", weights.get_padded_shape()[-1], shard_spec->shape[1]);
         }
     } else {
+        if (is_sharded(this->output_mem_config.memory_layout())) {
+            TT_FATAL(this->output_mem_config.memory_layout() == TensorMemoryLayout::HEIGHT_SHARDED, "Embedding only supports height sharded Row Major outputs");
+        }
         TT_FATAL(!is_block_float(this->output_dtype), "Output cannot be a block float dtype when not tilized");
     }
     if(a.get_layout() == Layout::ROW_MAJOR) {
         TT_FATAL(a.get_padded_shape()[1] == 1 && a.get_padded_shape()[2] == 1, "Only dim 0 && 3 for the input can be non 1");
-        if (is_sharded(this->output_mem_config.memory_layout())) {
-            TT_FATAL(this->output_mem_config.memory_layout() == TensorMemoryLayout::HEIGHT_SHARDED, "Embedding only supports height sharded Row Major outputs");
-        }
     }
     switch (this->embeddings_type) {
         case EmbeddingsType::PADDED: TT_FATAL(this->pad_token.has_value(), "Pad token must be specified when PADDED Embeddings Type is specified"); break;
