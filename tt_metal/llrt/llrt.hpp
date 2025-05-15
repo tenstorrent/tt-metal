a3e0c70b96e0f5e2adffbfbe325df24bdf3190da
Mo <mmemarian@tenstorrent.com>
2025-05-15 22:17:31 +0000

    TEMP fix on slow to fast dispatch switch

--- tt_metal/llrt/llrt.hpp
+++ tt_metal/llrt/llrt.hpp
@@ -91,6 +91,8 @@ std::vector<std::uint32_t> read_hex_vec_from_core(chip_id_t chip, const CoreCoor
 
 CoreCoord logical_core_from_ethernet_core(chip_id_t chip_id, CoreCoord& ethernet_core);
 
+void send_reset_go_signal(chip_id_t chip, const CoreCoord core);
+
 void write_launch_msg_to_core(
     chip_id_t chip, CoreCoord core, launch_msg_t* msg, go_msg_t* go_msg, uint64_t addr, bool send_go = true);
 
