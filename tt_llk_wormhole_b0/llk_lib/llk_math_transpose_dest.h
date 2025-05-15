0c8b12f965278f404c7f223a67336f503ecb92a1
Jason Davies <jason@jasondavies.com>
2025-05-15 14:54:30 +0100

    Minimise data movement in transpose_dest. (#176)
    ### Ticket
    
    #175
    
    ### Problem description
    
    The original code only copied data between B and Dst. When swapping the
    middle two tiles, this meant using rows 0-15 of B as a temporary buffer
    for one of the middle tiles before later copying back to Dst and then
    back to B to rows 16-31 for transpose, and finally to the free tile
    position in Dst.
    
    Note that MOVA2D supports 8-row copies, so that only two instructions
    are required to copy a 16x16 tile, rather than 4 instructions as
    required for the other MOV* instructions that only support up to 4 rows
    at a time. Therefore it makes more sense to use A as the temporary
    buffer.
    
    This also opens up the possibility of reducing the total number of tile
    copies, since we also have MOVB2A, and we can exploit this to do the
    following for the 2nd and 3rd tiles:
    
    1. Copy 2nd tile to B
    2. Transpose B
    3. Copy to A, which frees up the original 2nd tile in Dst.
    4. Copy the 3rd tile to B
    5. Transpose B
    6. Overwrite 2nd tile in Dst
    7. Finally, copy the transposed tile from A to Dst, overwriting the 3rd
    tile.
    
    This reduces the total number of _tile movements_ from 10 to 9.
    
    ### What's changed
    
    - Replace the use of rows 0-15 of SrcB with SrcA
    - 0x3ff mask added to negative destination offsets to avoid strange
    macro expansion effects when using negative numbers there
    - MOP template 0 used to store 7 instructions that reference a 15-slot
    replay buffer
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    
    ---------
    
    Co-authored-by: Nikola Velickovic <nvelickovic@tenstorrent.com>
    Co-authored-by: Filip Vranic <fvranic@tenstorrent.com>

--- tt_llk_wormhole_b0/llk_lib/llk_math_transpose_dest.h
+++ tt_llk_wormhole_b0/llk_lib/llk_math_transpose_dest.h
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+// SPDX-FileCopyrightText: © 2025 Jason Davies <jason@jasondavies.com>
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -24,14 +25,10 @@ inline void _llk_math_transpose_dest_(const std::uint32_t dst_index)
 
     TTI_STALLWAIT(p_stall::STALL_MATH, p_stall::WAIT_SFPU);
 
-    ckernel_template::run(instrn_buffer);
+    ckernel_unpack_template::run(instrn_buffer, 2, 2);
 
-    TTI_REPLAY(20, 5, 0, 0);
-    TTI_REPLAY(26, 4, 0, 0);
-
-    TTI_SETRWC(p_setrwc::CLR_AB, 0, 0, 0, 0, p_setrwc::SET_AB);
-
-    math::clear_dst_reg_addr();
+    // clear SrcA, SrcB valids; reset SrcA, SrcB, Dst counters to zero
+    TTI_SETRWC(p_setrwc::CLR_AB, 0, 0, 0, 0, p_setrwc::SET_ABD);
 }
 
 inline void transpose_dest_configure_addrmod()
@@ -56,50 +53,58 @@ inline void transpose_dest_configure_addrmod()
         .dest = {.incr = -16},
     }
         .set(ADDR_MOD_2);
+
+    addr_mod_t {
+        .srca = {.incr = 0},
+        .srcb = {.incr = 0},
+        .dest = {.incr = 32},
+    }
+        .set(ADDR_MOD_3);
 }
 
 inline void transpose_dest_configure_mop()
 {
-    TTI_REPLAY(16, 16, 0, 1);
+    TTI_REPLAY(16, 15, 0, 1);
 
-    // A
-    TTI_MOVD2B(0, p_movd2b::SRC_ZERO_OFFSET + 0, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 0 - 16);
-    TTI_MOVD2B(0, p_movd2b::SRC_ZERO_OFFSET + 4, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 4 - 16);
-    TTI_MOVD2B(0, p_movd2b::SRC_ZERO_OFFSET + 8, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 8 - 16);
-    TTI_MOVD2B(0, p_movd2b::SRC_ZERO_OFFSET + 12, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 12 - 16);
+    // ABCD
+    TTI_MOVB2A(0, ADDR_MOD_1, p_movb2a::MOV_4_ROWS, 16);
+    TTI_MOVB2A(4, ADDR_MOD_1, p_movb2a::MOV_4_ROWS, 20);
+    TTI_MOVB2A(8, ADDR_MOD_1, p_movb2a::MOV_4_ROWS, 24);
+    TTI_MOVB2A(12, ADDR_MOD_0, p_movb2a::MOV_4_ROWS, 28); // dst += 16
 
-    // B
-    TTI_MOVD2B(0, p_movd2b::SRC_ROW16_OFFSET + 0, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 0);
-    TTI_MOVD2B(0, p_movd2b::SRC_ROW16_OFFSET + 4, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 4);
-    TTI_MOVD2B(0, p_movd2b::SRC_ROW16_OFFSET + 8, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 8);
-    TTI_MOVD2B(0, p_movd2b::SRC_ROW16_OFFSET + 12, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 12);
-
-    // C
+    // EFGHIJKLM
+    TTI_MOVD2B(0, 16, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 0);
+    TTI_MOVD2B(0, 20, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 4);
+    TTI_MOVD2B(0, 24, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 8);
+    TTI_MOVD2B(0, 28, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 12);
     TTI_TRNSPSRCB;
+    TTI_MOVB2D(0, 16, ADDR_MOD_1, p_movb2d::MOV_4_ROWS, 0);
+    TTI_MOVB2D(0, 20, ADDR_MOD_1, p_movb2d::MOV_4_ROWS, 4);
+    TTI_MOVB2D(0, 24, ADDR_MOD_1, p_movb2d::MOV_4_ROWS, 8);
+    TTI_MOVB2D(0, 28, ADDR_MOD_0, p_movb2d::MOV_4_ROWS, 12); // dst += 16
 
-    // D
-    TTI_MOVD2B(0, p_movd2b::SRC_ZERO_OFFSET + 32, ADDR_MOD_2, p_movd2b::MOV_1_ROW, 0); // throwaway to decrement dst
+    // NO
+    // Note: the 0x3ff mask ensures the negative offsets are 10 bits.
+    TTI_MOVA2D(0, 0, ADDR_MOD_2, p_mova2d::MOV_8_ROWS, 0x3ff & (0 - 32)); // dst -= 16
+    TTI_MOVA2D(0, 8, ADDR_MOD_2, p_mova2d::MOV_8_ROWS, 0x3ff & (8 - 16)); // dst -= 16
 
-    // E
-    TTI_MOVB2D(0, p_movd2b::SRC_ROW16_OFFSET + 0, ADDR_MOD_1, p_movb2d::MOV_4_ROWS, 0);
-    TTI_MOVB2D(0, p_movd2b::SRC_ROW16_OFFSET + 4, ADDR_MOD_1, p_movb2d::MOV_4_ROWS, 4);
-    TTI_MOVB2D(0, p_movd2b::SRC_ROW16_OFFSET + 8, ADDR_MOD_1, p_movb2d::MOV_4_ROWS, 8);
-    TTI_MOVB2D(0, p_movd2b::SRC_ROW16_OFFSET + 12, ADDR_MOD_0, p_movb2d::MOV_4_ROWS, 12);
+    // The next 7 instructions expand as follows:
+    // Face 0: 4x MOVD2B, TRNSPSRCB, 4x MOVB2D, dst += 16 (EFGHIJKLM)
+    // Face 1: 4x MOVD2B, TRNSPSRCB, 4x MOVB2A, dst += 16 (EFGHI, ABCD..)
+    // Face 2: 4x MOVD2B (dst -= 16), TRNSPSRCB, 4x MOVB2D, dst += 32 (..EFG, P, IJKL, Q)
+    // Face 3: 4x MOVD2B, TRNSPSRCB, 4x MOVB2D, dst += 16 (EFGHIJKLM..)
+    // Face 1: 2x MOVA2D (2x dst -= 16) (..NO)
 
-    // F
-    TTI_MOVB2D(0, p_movd2b::SRC_ZERO_OFFSET + 0, ADDR_MOD_1, p_movb2d::MOV_4_ROWS, 0);
-    TTI_MOVB2D(0, p_movd2b::SRC_ZERO_OFFSET + 4, ADDR_MOD_1, p_movb2d::MOV_4_ROWS, 4);
+    uint EFGHIJKLM   = TT_OP_REPLAY(20, 9, 0, 0);
+    uint EFGHI       = TT_OP_REPLAY(20, 5, 0, 0);
+    uint ABCDEFG     = TT_OP_REPLAY(16, 7, 0, 0);
+    uint P           = TT_OP_MOVD2B(0, 28, ADDR_MOD_2, p_movd2b::MOV_4_ROWS, 12); // dst -= 16
+    uint IJKL        = TT_OP_REPLAY(24, 4, 0, 0);
+    uint Q           = TT_OP_MOVB2D(0, 28, ADDR_MOD_3, p_movb2d::MOV_4_ROWS, 12); // dst += 32
+    uint EFGHIJKLMNO = TT_OP_REPLAY(20, 11, 0, 0);
 
-    uint AF = TT_OP_REPLAY(16, 16, 0, 0);
-    uint BC = TT_OP_REPLAY(20, 5, 0, 0);
-    uint E  = TT_OP_REPLAY(26, 4, 0, 0);
-    uint X  = TT_OP_MOVB2D(0, p_movd2b::SRC_ZERO_OFFSET + 8, ADDR_MOD_1, p_movb2d::MOV_4_ROWS, 8);
-    uint Y  = TT_OP_MOVB2D(0, p_movd2b::SRC_ZERO_OFFSET + 12, ADDR_MOD_0, p_movb2d::MOV_4_ROWS, 12);
-
-    ckernel_template tmp(1, 2, E, BC);
-    tmp.set_start_op(BC);
-    tmp.set_last_outer_loop_instr(AF);
-    tmp.set_end_ops(X, Y);
+    // The following MOP config simply runs the above 7 instructions in order (when executed with zmask 0b10):
+    ckernel_unpack_template tmp(true, true, EFGHIJKLM, EFGHI, ABCDEFG, P, /* skip A */ Q, /* B */ IJKL, /* skip B */ EFGHIJKLMNO);
     tmp.program(instrn_buffer);
 }
 
