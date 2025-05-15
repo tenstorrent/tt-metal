b49e655e060a114693fc5f6c087933a184546da7
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2025-05-15 22:02:39 +0000

    remvoed sudo

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -260,7 +260,7 @@ install_mpi_uflm(){
     # 2. Install
     echo "→ Installing $DEB_FILE …"
 
-    sudo apt install -y "$TMP_DIR/$DEB_FILE"
+    apt install -y "$TMP_DIR/$DEB_FILE"
 }
 
 # We don't really want to have hugepages dependency
