230470239cf4v7r09237m/tt-metal-newci

#17785: Swin_S ttnn implementation

models/experimental/functional_swin_s/reference/swin_transformer.py
@@ -1,0 +1,113 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from functools import partial
+from typing import Any, Callable, List, Optional
+import torch
+import torch.nn.functional as F
+from torch import nn, Tensor
+
+from models.experimental.functional_swin_s.reference.swin_transformer_block import SwinTransformerBlock
+from models.experimental.functional_swin_s.reference.patchmerging import PatchMerging
+
+
+class Permute(torch.nn.Module):
+    def __init__(self, dims: List[int]):
+        super().__init__()
+        self.dims = dims
+
+    def forward(self, x: Tensor) -> Tensor:
+        return torch.permute(x, self.dims)
+
+
+class SwinTransformer(nn.Module):
+    def __init__(
+        self,
+        patch_size: List[int],
+        embed_dim: int,
+        depths: List[int],
+        num_heads: List[int],
+        window_size: List[int],
+        mlp_ratio: float = 4.0,
+        dropout: float = 0.0,
+        attention_dropout: float = 0.0,
+        stochastic_depth_prob: float = 0.1,
+        num_classes: int = 1000,
+        norm_layer: Optional[Callable[..., nn.Module]] = None,
+        block: Optional[Callable[..., nn.Module]] = None,
+        downsample_layer: Callable[..., nn.Module] = PatchMerging,
+    ):
+        super().__init__()
+
+        self.num_classes = num_classes
+
+        if block is None:
+            block = SwinTransformerBlock
+        if norm_layer is None:
+            norm_layer = partial(nn.LayerNorm, eps=1e-5)
+
+        layers: List[nn.Module] = []
+        # split image into non-overlapping patches
+        layers.append(
+            nn.Sequential(
+                nn.Conv2d(
+                    3, embed_dim, kernel_size=(patch_size[0], patch_size[1]), stride=(patch_size[0], patch_size[1])
+                ),
+                Permute([0, 2, 3, 1]),
+                norm_layer(embed_dim),
+            )
+        )
+
+        total_stage_blocks = sum(depths)
+        stage_block_id = 0
+        # build SwinTransformer blocks
+        for i_stage in range(len(depths)):
+            stage: List[nn.Module] = []
+            dim = embed_dim * 2**i_stage
+            for i_layer in range(depths[i_stage]):
+                # adjust stochastic depth probability based on the depth of the stage block
+                sd_prob = stochastic_depth_prob * float(stage_block_id) / (total_stage_blocks - 1)
+                stage.append(
+                    block(
+                        dim,
+                        num_heads[i_stage],
+                        window_size=window_size,
+                        shift_size=[0 if i_layer % 2 == 0 else w // 2 for w in window_size],
+                        mlp_ratio=mlp_ratio,
+                        dropout=dropout,
+                        attention_dropout=attention_dropout,
+                        stochastic_depth_prob=sd_prob,
+                        norm_layer=norm_layer,
+                    )
+                )
+                stage_block_id += 1
+            layers.append(nn.Sequential(*stage))
+            # add patch merging layer
+            if i_stage < (len(depths) - 1):
+                layers.append(downsample_layer(dim, norm_layer))
+        self.features = nn.Sequential(*layers)
+
+        num_features = embed_dim * 2 ** (len(depths) - 1)
+        self.norm = norm_layer(num_features)
+        self.permute = Permute([0, 3, 1, 2])  # B H W C -> B C H W
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.flatten = nn.Flatten(1)
+        self.head = nn.Linear(num_features, num_classes)
+
+        # for m in self.modules():
+        #     if isinstance(m, nn.Linear):
+        #         nn.init.trunc_normal_(m.weight, std=0.02)
+        #         if m.bias is not None:
+        #             nn.init.zeros_(m.bias)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.norm(x)
+
+        x = self.permute(x)
+        x = self.avgpool(x)
+
+        x = self.flatten(x)
+        x = self.head(x)
+        return x

tests/ttnn/integration_tests/swin_s/test_ttnn_mlp.py
@@ -1,0 +1,99 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+from torch import nn
+import pytest
+from ttnn.model_preprocessing import (
+    preprocess_model_parameters,
+    preprocess_linear_weight,
+    preprocess_layernorm_parameter,
+    preprocess_linear_bias,
+)
+from models.utility_functions import skip_for_grayskull
+from tests.ttnn.utils_for_testing import assert_with_pcc
+from models.experimental.functional_swin_s.reference.mlp import MLP
+from models.experimental.functional_swin_s.tt.tt_mlp import TtMLP
+from torchvision import models
+import ttnn
+
+
+def create_custom_preprocessor(device):
+    def custom_preprocessor(torch_model, name, ttnn_module_args):
+        parameters = {}
+        if isinstance(torch_model, MLP):
+            parameters[0] = {}
+            parameters[3] = {}
+            parameters[0]["weight"] = preprocess_linear_weight(torch_model[0].weight, dtype=ttnn.bfloat16)
+            parameters[0]["bias"] = preprocess_linear_bias(torch_model[0].bias, dtype=ttnn.bfloat16)
+            parameters[3]["weight"] = preprocess_linear_weight(torch_model[3].weight, dtype=ttnn.bfloat16)
+            parameters[3]["bias"] = preprocess_linear_bias(torch_model[3].bias, dtype=ttnn.bfloat16)
+            if torch.layer_norm in torch_model:
+                parameters[1] = {}
+                parameters[1]["weight"] = preprocess_layernorm_parameter(torch_model[1].weight, dtype=ttnn.bfloat16)
+                parameters[1]["bias"] = preprocess_layernorm_parameter(torch_model[1].bias, dtype=ttnn.bfloat16)
+        return parameters
+
+    return custom_preprocessor
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
+@pytest.mark.parametrize(
+    "batch_size",
+    [
+        1,
+        # 8, #MM shard is given for bs=1 alone in the pipeline
+    ],
+)
+@pytest.mark.parametrize(
+    "in_channels,hidden_channels,seq_len,i,j",
+    [
+        (96, [384, 96], 128, 1, 0),
+        (192, [768, 192], 64, 3, 0),
+        (384, [1536, 384], 32, 5, 0),
+        (768, [3072, 768], 16, 7, 0),
+    ],
+)
+def test_mlp(device, in_channels, hidden_channels, batch_size, seq_len, i, j, reset_seeds):
+    model = models.swin_s(weights="IMAGENET1K_V1")
+    state_dict = state_dict = model.state_dict()
+    mlp_state_dict = {k: v for k, v in state_dict.items() if (k.startswith(f"features.{i}.{j}.mlp."))}
+
+    if not mlp_state_dict:
+        raise ValueError("No parameters found in mlp_state_dict")
+
+    torch_model = MLP(in_channels, hidden_channels, activation_layer=nn.GELU)
+
+    new_state_dict = {}
+    keys = [name for name, parameter in torch_model.state_dict().items()]
+    values = [parameter for name, parameter in mlp_state_dict.items()]
+
+    for i in range(len(keys)):
+        new_state_dict[keys[i]] = values[i]
+
+    torch_model.load_state_dict(new_state_dict)
+    torch_model.eval()
+
+    # Input tensor for testing
+    torch_input_tensor = torch.randn(batch_size, seq_len, seq_len, in_channels)  # Sample input tensor
+    torch_output_tensor = torch_model(torch_input_tensor)
+
+    parameters = preprocess_model_parameters(
+        initialize_model=lambda: torch_model, custom_preprocessor=create_custom_preprocessor(device), device=device
+    )
+
+    # Convert the model to TTNN
+    ttnn_model = TtMLP(hidden_channels, device, parameters, activation_layer=ttnn.gelu)
+
+    # Convert input tensor to TTNN format
+    input_tensor = ttnn.from_torch(torch_input_tensor, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+
+    # Apply TTNN model
+    output_tensor = ttnn_model(input_tensor)
+
+    # Convert output tensor back to Torch format
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)

tests/ttnn/integration_tests/swin_s/test_ttnn_patchmerging.py
@@ -1,0 +1,96 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+from torchvision import models
+import pytest
+import ttnn
+from ttnn.model_preprocessing import (
+    preprocess_model_parameters,
+    preprocess_linear_weight,
+    preprocess_layernorm_parameter,
+)
+from models.utility_functions import skip_for_grayskull
+from tests.ttnn.utils_for_testing import assert_with_pcc
+from models.experimental.functional_swin_s.reference.patchmerging import PatchMerging
+from models.experimental.functional_swin_s.tt.tt_patchmerging import TtPatchMerging
+
+
+def create_custom_preprocessor(device):
+    def custom_preprocessor(torch_model, name, ttnn_module_args):
+        parameters = {}
+        if isinstance(torch_model, PatchMerging):
+            parameters["reduction"] = {}
+            parameters["reduction"]["weight"] = preprocess_linear_weight(
+                torch_model.reduction.weight, dtype=ttnn.bfloat16
+            )
+            parameters["norm"] = {}
+            parameters["norm"]["weight"] = preprocess_layernorm_parameter(torch_model.norm.weight, dtype=ttnn.bfloat16)
+            parameters["norm"]["bias"] = preprocess_layernorm_parameter(torch_model.norm.bias, dtype=ttnn.bfloat16)
+        return parameters
+
+    return custom_preprocessor
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
+@pytest.mark.parametrize(
+    "batch_size",
+    [
+        1,
+        # 8, #MM shard is given for bs=1 alone in the pipeline
+    ],
+)
+@pytest.mark.parametrize(
+    "dim,seq_len,i",
+    [
+        (96, 128, 2),
+        (192, 64, 4),
+        (384, 32, 6),
+    ],
+)
+def test_patchmerging(device, batch_size, dim, seq_len, i, reset_seeds):
+    model = models.swin_s(weights="IMAGENET1K_V1")
+    state_dict = state_dict = model.state_dict()
+    patchmerging_state_dict = {k: v for k, v in state_dict.items() if (k.startswith(f"features.{i}."))}
+
+    if not patchmerging_state_dict:
+        raise ValueError("No parameters found in resblock_state_dict")
+
+    torch_model = PatchMerging(dim)
+    for layer in torch_model.children():
+        print(layer)
+
+    new_state_dict = {}
+    keys = [name for name, parameter in torch_model.state_dict().items()]
+    values = [parameter for name, parameter in patchmerging_state_dict.items()]
+
+    for i in range(len(keys)):
+        new_state_dict[keys[i]] = values[i]
+
+    torch_model.load_state_dict(new_state_dict)
+    torch_model.eval()
+
+    # Input tensor for testing
+    torch_input_tensor = torch.randn(batch_size, seq_len, seq_len, dim)  # Sample input tensor
+    torch_output_tensor = torch_model(torch_input_tensor)
+
+    # Preprocess the model parameters for TTNN
+    parameters = preprocess_model_parameters(
+        initialize_model=lambda: torch_model, custom_preprocessor=create_custom_preprocessor(device), device=device
+    )
+
+    # Convert the model to TTNN
+    ttnn_model = TtPatchMerging(device, parameters, dim)
+
+    # Convert input tensor to TTNN format
+    input_tensor = ttnn.from_torch(torch_input_tensor, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+
+    # Apply TTNN model
+    output_tensor = ttnn_model(input_tensor)
+
+    # Convert output tensor back to Torch format
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)

tenstorrent/tt-metal

models/experimental/swin_s/reference/swin_transformer.py
@@ -1,0 +1,106 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from functools import partial
+from typing import Callable, List, Optional
+import torch
+from torch import nn, Tensor
+
+from models.experimental.swin_s.reference.swin_transformer_block import SwinTransformerBlock
+from models.experimental.swin_s.reference.patchmerging import PatchMerging
+
+
+class Permute(torch.nn.Module):
+    def __init__(self, dims: List[int]):
+        super().__init__()
+        self.dims = dims
+
+    def forward(self, x: Tensor) -> Tensor:
+        return torch.permute(x, self.dims)
+
+
+class SwinTransformer(nn.Module):
+    def __init__(
+        self,
+        patch_size: List[int],
+        embed_dim: int,
+        depths: List[int],
+        num_heads: List[int],
+        window_size: List[int],
+        mlp_ratio: float = 4.0,
+        dropout: float = 0.0,
+        attention_dropout: float = 0.0,
+        stochastic_depth_prob: float = 0.1,
+        num_classes: int = 1000,
+        norm_layer: Optional[Callable[..., nn.Module]] = None,
+        block: Optional[Callable[..., nn.Module]] = None,
+        downsample_layer: Callable[..., nn.Module] = PatchMerging,
+    ):
+        super().__init__()
+
+        self.num_classes = num_classes
+
+        if block is None:
+            block = SwinTransformerBlock
+        if norm_layer is None:
+            norm_layer = partial(nn.LayerNorm, eps=1e-5)
+
+        layers: List[nn.Module] = []
+        # split image into non-overlapping patches
+        layers.append(
+            nn.Sequential(
+                nn.Conv2d(
+                    3, embed_dim, kernel_size=(patch_size[0], patch_size[1]), stride=(patch_size[0], patch_size[1])
+                ),
+                Permute([0, 2, 3, 1]),
+                norm_layer(embed_dim),
+            )
+        )
+
+        total_stage_blocks = sum(depths)
+        stage_block_id = 0
+        # build SwinTransformer blocks
+        for i_stage in range(len(depths)):
+            stage: List[nn.Module] = []
+            dim = embed_dim * 2**i_stage
+            for i_layer in range(depths[i_stage]):
+                # adjust stochastic depth probability based on the depth of the stage block
+                sd_prob = stochastic_depth_prob * float(stage_block_id) / (total_stage_blocks - 1)
+                stage.append(
+                    block(
+                        dim,
+                        num_heads[i_stage],
+                        window_size=window_size,
+                        shift_size=[0 if i_layer % 2 == 0 else w // 2 for w in window_size],
+                        mlp_ratio=mlp_ratio,
+                        dropout=dropout,
+                        attention_dropout=attention_dropout,
+                        stochastic_depth_prob=sd_prob,
+                        norm_layer=norm_layer,
+                    )
+                )
+                stage_block_id += 1
+            layers.append(nn.Sequential(*stage))
+            # add patch merging layer
+            if i_stage < (len(depths) - 1):
+                layers.append(downsample_layer(dim, norm_layer))
+        self.features = nn.Sequential(*layers)
+
+        num_features = embed_dim * 2 ** (len(depths) - 1)
+        self.norm = norm_layer(num_features)
+        self.permute = Permute([0, 3, 1, 2])  # B H W C -> B C H W
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.flatten = nn.Flatten(1)
+        self.head = nn.Linear(num_features, num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.norm(x)
+
+        x = self.permute(x)
+        x = self.avgpool(x)
+
+        x = self.flatten(x)
+        x = self.head(x)
+        return x

tests/ttnn/integration_tests/swin_s/test_ttnn_mlp.py
@@ -1,0 +1,98 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+from torch import nn
+import pytest
+from ttnn.model_preprocessing import (
+    preprocess_model_parameters,
+    preprocess_linear_weight,
+    preprocess_layernorm_parameter,
+    preprocess_linear_bias,
+)
+from models.utility_functions import skip_for_grayskull
+from tests.ttnn.utils_for_testing import assert_with_pcc
+from models.experimental.swin_s.reference.mlp import MLP
+from models.experimental.swin_s.tt.tt_mlp import TtMLP
+from torchvision import models
+import ttnn
+
+
+def create_custom_preprocessor(device):
+    def custom_preprocessor(torch_model, name, ttnn_module_args):
+        parameters = {}
+        if isinstance(torch_model, MLP):
+            parameters[0] = {}
+            parameters[3] = {}
+            parameters[0]["weight"] = preprocess_linear_weight(torch_model[0].weight, dtype=ttnn.bfloat16)
+            parameters[0]["bias"] = preprocess_linear_bias(torch_model[0].bias, dtype=ttnn.bfloat16)
+            parameters[3]["weight"] = preprocess_linear_weight(torch_model[3].weight, dtype=ttnn.bfloat16)
+            parameters[3]["bias"] = preprocess_linear_bias(torch_model[3].bias, dtype=ttnn.bfloat16)
+            if torch.layer_norm in torch_model:
+                parameters[1] = {}
+                parameters[1]["weight"] = preprocess_layernorm_parameter(torch_model[1].weight, dtype=ttnn.bfloat16)
+                parameters[1]["bias"] = preprocess_layernorm_parameter(torch_model[1].bias, dtype=ttnn.bfloat16)
+        return parameters
+
+    return custom_preprocessor
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
+@pytest.mark.parametrize(
+    "batch_size",
+    [
+        1,
+    ],
+)
+@pytest.mark.parametrize(
+    "in_channels,hidden_channels,seq_len,i,j",
+    [
+        (96, [384, 96], 128, 1, 0),
+        (192, [768, 192], 64, 3, 0),
+        (384, [1536, 384], 32, 5, 0),
+        (768, [3072, 768], 16, 7, 0),
+    ],
+)
+def test_mlp(device, in_channels, hidden_channels, batch_size, seq_len, i, j, reset_seeds):
+    model = models.swin_s(weights="IMAGENET1K_V1")
+    state_dict = state_dict = model.state_dict()
+    mlp_state_dict = {k: v for k, v in state_dict.items() if (k.startswith(f"features.{i}.{j}.mlp."))}
+
+    if not mlp_state_dict:
+        raise ValueError("No parameters found in mlp_state_dict")
+
+    torch_model = MLP(in_channels, hidden_channels, activation_layer=nn.GELU)
+
+    new_state_dict = {}
+    keys = [name for name, parameter in torch_model.state_dict().items()]
+    values = [parameter for name, parameter in mlp_state_dict.items()]
+
+    for i in range(len(keys)):
+        new_state_dict[keys[i]] = values[i]
+
+    torch_model.load_state_dict(new_state_dict)
+    torch_model.eval()
+
+    # Input tensor for testing
+    torch_input_tensor = torch.randn(batch_size, seq_len, seq_len, in_channels)  # Sample input tensor
+    torch_output_tensor = torch_model(torch_input_tensor)
+
+    parameters = preprocess_model_parameters(
+        initialize_model=lambda: torch_model, custom_preprocessor=create_custom_preprocessor(device), device=device
+    )
+
+    # Convert the model to TTNN
+    ttnn_model = TtMLP(hidden_channels, device, parameters, activation_layer=ttnn.gelu)
+
+    # Convert input tensor to TTNN format
+    input_tensor = ttnn.from_torch(torch_input_tensor, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+
+    # Apply TTNN model
+    output_tensor = ttnn_model(input_tensor)
+
+    # Convert output tensor back to Torch format
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)

tests/ttnn/integration_tests/swin_s/test_ttnn_patchmerging.py
@@ -1,0 +1,93 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+from torchvision import models
+import pytest
+import ttnn
+from ttnn.model_preprocessing import (
+    preprocess_model_parameters,
+    preprocess_linear_weight,
+    preprocess_layernorm_parameter,
+)
+from models.utility_functions import skip_for_grayskull
+from tests.ttnn.utils_for_testing import assert_with_pcc
+from models.experimental.swin_s.reference.patchmerging import PatchMerging
+from models.experimental.swin_s.tt.tt_patchmerging import TtPatchMerging
+
+
+def create_custom_preprocessor(device):
+    def custom_preprocessor(torch_model, name, ttnn_module_args):
+        parameters = {}
+        if isinstance(torch_model, PatchMerging):
+            parameters["reduction"] = {}
+            parameters["reduction"]["weight"] = preprocess_linear_weight(
+                torch_model.reduction.weight, dtype=ttnn.bfloat16
+            )
+            parameters["norm"] = {}
+            parameters["norm"]["weight"] = preprocess_layernorm_parameter(torch_model.norm.weight, dtype=ttnn.bfloat16)
+            parameters["norm"]["bias"] = preprocess_layernorm_parameter(torch_model.norm.bias, dtype=ttnn.bfloat16)
+        return parameters
+
+    return custom_preprocessor
+
+
+@skip_for_grayskull()
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
+@pytest.mark.parametrize(
+    "batch_size",
+    [
+        1,
+    ],
+)
+@pytest.mark.parametrize(
+    "dim,seq_len,i",
+    [
+        (96, 128, 2),
+        (192, 64, 4),
+        (384, 32, 6),
+    ],
+)
+def test_patchmerging(device, batch_size, dim, seq_len, i, reset_seeds):
+    model = models.swin_s(weights="IMAGENET1K_V1")
+    state_dict = state_dict = model.state_dict()
+    patchmerging_state_dict = {k: v for k, v in state_dict.items() if (k.startswith(f"features.{i}."))}
+
+    if not patchmerging_state_dict:
+        raise ValueError("No parameters found in resblock_state_dict")
+
+    torch_model = PatchMerging(dim)
+
+    new_state_dict = {}
+    keys = [name for name, parameter in torch_model.state_dict().items()]
+    values = [parameter for name, parameter in patchmerging_state_dict.items()]
+
+    for i in range(len(keys)):
+        new_state_dict[keys[i]] = values[i]
+
+    torch_model.load_state_dict(new_state_dict)
+    torch_model.eval()
+
+    # Input tensor for testing
+    torch_input_tensor = torch.randn(batch_size, seq_len, seq_len, dim)  # Sample input tensor
+    torch_output_tensor = torch_model(torch_input_tensor)
+
+    # Preprocess the model parameters for TTNN
+    parameters = preprocess_model_parameters(
+        initialize_model=lambda: torch_model, custom_preprocessor=create_custom_preprocessor(device), device=device
+    )
+
+    # Convert the model to TTNN
+    ttnn_model = TtPatchMerging(device, parameters, dim)
+
+    # Convert input tensor to TTNN format
+    input_tensor = ttnn.from_torch(torch_input_tensor, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+
+    # Apply TTNN model
+    output_tensor = ttnn_model(input_tensor)
+
+    # Convert output tensor back to Torch format
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.99)