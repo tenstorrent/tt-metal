e764c0fcadfc1d307abdc85aec83c9c9c4ebc73b
Gustavo Sarabando <ctr-gsarabando@ext.tenstorrent.com>
2025-05-16 13:42:14 -0700

    Merge global_circular_buffer.hpp and global_circular_buffer_impl.hpp (#22185)
    ### Ticket
    API cleanup
    Assisting task: [Minimize tt_metal API surface
    area](https://github.com/tenstorrent/tt-metal/issues/17824)
    
    ### Problem description
    tt_metal/api/tt-metalium/global_circular_buffer_impl.hpp and
    tt_metal/api/tt-metalium/global_circular_buffer.hpp were slipt
    unnecessarily. Impl contained class definition while the other defined a
    couple functions.
    
    Both headers were included in almost all uses and merging them will not
    pull in new includes to class definition.
    
    ### What's changed
    Merged global_circular_buffer_impl.hpp into global_circular_buffer.hpp
    to clean up metal API
    Removed file global_circular_buffer_impl.hpp 
    Updated all mentions to it
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/15054004438)
    CI passes
    - [x] New/Existing tests provide coverage for changes
    
    Co-authored-by: gsarabandoTT <ctr.gsarabando@ext.tenstorrent.com>

--- ttnn/cpp/ttnn-pybind/global_circular_buffer.cpp
+++ ttnn/cpp/ttnn-pybind/global_circular_buffer.cpp
@@ -4,7 +4,7 @@
 
 #include "global_circular_buffer.hpp"
 
-#include <tt-metalium/global_circular_buffer_impl.hpp>
+#include <tt-metalium/global_circular_buffer.hpp>
 #include "ttnn/global_circular_buffer.hpp"
 #include "pybind11/pybind11.h"
 
