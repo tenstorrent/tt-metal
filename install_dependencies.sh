9fb0abc8b60ff82619dcf485e402a1c586240372
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2025-05-16 02:35:21 +0000

    updated install deps

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -85,6 +85,7 @@ ub_runtime_packages()
      libc++-17-dev \
      libc++abi-17-dev \
      libstdc++6 \
+     libevent-dev \
     )
 }
 
@@ -103,6 +104,7 @@ ub_buildtime_packages()
      libc++abi-17-dev \
      build-essential \
      xz-utils \
+     libevent-dev \
     )
 }
 
