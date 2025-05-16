67064c4c1d7903a93cbd5412e222cfb8e7ddb252
Keerthana Rajendran <keerthana.rajendran@multicorewareinc.com>
2025-05-16 08:35:23 +0530

    VGG_Unet demo (#20250)
    ### Ticket
    Link to Github Issue
    https://github.com/tenstorrent/tt-metal/issues/18990
    
    ### Problem description
    - Add demo for the VGG_Unet model.
    
    ### Performance:
    Perf sheet:
    [ops_perf_results_VGG_Unet_perf_2025_04_25_08_05_05.csv](https://github.com/user-attachments/files/19904996/ops_perf_results_VGG_Unet_perf_2025_04_25_08_05_05.csv)
    E2E FPS: 75
    Device FPS: 92.3
    
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/14749016709)
    - [x] [(Single-card) Demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/14640377909)
    
    Co-authored-by: Mohamed Bahnas <116673264+mbahnasTT@users.noreply.github.com>

--- models/demos/vgg_unet/tests/test_e2e_performant.py
+++ models/demos/vgg_unet/tests/test_e2e_performant.py
@@ -2,14 +2,15 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-import pytest
-import ttnn
 import time
+
+import pytest
 import torch
 from loguru import logger
 
+import ttnn
+from models.demos.vgg_unet.tests.vgg_unet_e2e_performant import VggUnetTrace2CQ
 from models.utility_functions import run_for_wormhole_b0
-from models.experimental.functional_vgg_unet.tests.vgg_unet_e2e_performant import VggUnetTrace2CQ
 
 
 @run_for_wormhole_b0()
