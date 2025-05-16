adbcc40d7c52d55296dfcb81c1d028440762cf82
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-05-16 18:41:38 +0000

    fix test

--- tests/tt_metal/tt_metal/debug_tools/watcher/test_stack_usage.cpp
+++ tests/tt_metal/tt_metal/debug_tools/watcher/test_stack_usage.cpp
@@ -15,10 +15,11 @@
 #include <gtest/gtest.h>
 
 namespace {
-void RunTest(WatcherFixture* fixture, IDevice* device) {
+    void RunOneTest(WatcherFixture* fixture, IDevice* device, unsigned usage, bool warning) {
     // Set up program
     Program program = Program();
     CoreCoord coord = {0, 0};
+    std::vector<uint32_t> compile_args{usage};
 
     // Run a kernel that posts waypoints and waits on certain gating values to be written before
     // posting the next waypoint.
@@ -26,17 +27,19 @@ void RunTest(WatcherFixture* fixture, IDevice* device) {
         program,
         "tests/tt_metal/tt_metal/test_kernels/misc/watcher_stack.cpp",
         coord,
-        DataMovementConfig{.processor = DataMovementProcessor::RISCV_0, .noc = NOC::RISCV_0_default});
+        DataMovementConfig{.processor = DataMovementProcessor::RISCV_0, .noc = NOC::RISCV_0_default,
+            .compile_args = compile_args});
     auto ncrisc_kid = CreateKernel(
         program,
         "tests/tt_metal/tt_metal/test_kernels/misc/watcher_stack.cpp",
         coord,
-        DataMovementConfig{.processor = DataMovementProcessor::RISCV_1, .noc = NOC::RISCV_1_default});
+        DataMovementConfig{.processor = DataMovementProcessor::RISCV_1, .noc = NOC::RISCV_1_default,
+            .compile_args = compile_args});
     auto trisc_kid = CreateKernel(
         program,
         "tests/tt_metal/tt_metal/test_kernels/misc/watcher_stack.cpp",
         coord,
-        ComputeConfig{});
+        ComputeConfig{.compile_args = compile_args});
 
     // Also run on ethernet cores if they're present
     bool has_eth_cores = false && !device->get_active_ethernet_cores(true).empty();
@@ -71,34 +74,23 @@ void RunTest(WatcherFixture* fixture, IDevice* device) {
     expected.push_back("bla");
     expected.push_back("bla");
 
-    static const uint32_t usages[] = {8192, 64, 16, 0};
-    for (auto usage : usages) {
-        bool warning = usage < 64;
-
-        const std::vector<uint32_t> args = { usage };
-        SetRuntimeArgs(program, brisc_kid, coord, args);
-        SetRuntimeArgs(program, ncrisc_kid, coord, args);
-        SetRuntimeArgs(program, trisc_kid, coord, args);
-
-        fixture->RunProgram(device, program, true);
+    fixture->RunProgram(device, program, true);
 #if 0
-
-        CoreCoord virtual_core = device->worker_core_from_logical_core(coord);
-        check_core(coord, virtual_core, false, false, usage, warning);
-        if (has_eth_cores) {
-            for (const auto& core : device->get_active_ethernet_cores(true)) {
-                CoreCoord virtual_core = device->ethernet_core_from_logical_core(core);
-                check_core(core, virtual_core, true, true, usage, warning);
-            }
+    CoreCoord virtual_core = device->worker_core_from_logical_core(coord);
+    check_core(coord, virtual_core, false, false, usage, warning);
+    if (has_eth_cores) {
+        for (const auto& core : device->get_active_ethernet_cores(true)) {
+            CoreCoord virtual_core = device->ethernet_core_from_logical_core(core);
+            check_core(core, virtual_core, true, true, usage, warning);
         }
-        if (has_idle_eth_cores) {
-            for (const auto& core : device->get_inactive_ethernet_cores()) {
-                CoreCoord virtual_core = device->ethernet_core_from_logical_core(core);
-                check_core(core, virtual_core, true, false, usage, warning);
-            }
-        }
-#endif
     }
+    if (has_idle_eth_cores) {
+        for (const auto& core : device->get_inactive_ethernet_cores()) {
+            CoreCoord virtual_core = device->ethernet_core_from_logical_core(core);
+            check_core(core, virtual_core, true, false, usage, warning);
+        }
+    }
+#endif
     EXPECT_TRUE(
         FileContainsAllStrings(
             fixture->log_file_name,
@@ -106,6 +98,14 @@ void RunTest(WatcherFixture* fixture, IDevice* device) {
             )
         );
 }
+
+void RunTest(WatcherFixture* fixture, IDevice* device) {
+    RunOneTest(fixture, device, 0, true);
+    RunOneTest(fixture, device, 16, true);
+    RunOneTest(fixture, device, 128, false);
+    RunOneTest(fixture, device, 8192, false);
+}
+
 } // namespace
 
 TEST_F(WatcherFixture, TestWatcherStackUsage) {
