9ca5791f573674d2e23e68b3527569a3558de5cc
Lewis Panos <lpanos@tenstorrent.com>
2025-05-16 02:55:26 +0000

    Stress tests for w1/3 and w2 passing

--- llama_mlp_tests/glx_full/llama_mlp_best_perf_4U.py
+++ llama_mlp_tests/glx_full/llama_mlp_best_perf_4U.py
@@ -7,6 +7,8 @@ import math
 import pytest
 import tracy
 
+from loguru import logger
+
 TILE_SIZE = 32
 DRAM_WEIGHT_GRID = ttnn.CoreRangeSet({ttnn.CoreRange(ttnn.CoreCoord(0, 0), ttnn.CoreCoord(11, 0))})
 
@@ -29,23 +31,209 @@ SEQ_LENS = [
     65536,
     86016,
     131072,
+    "all",
 ]
 
 
-def generate_w1_w3_program_config(seq_len):
-    return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
-        compute_with_storage_grid_size=(7, 10),
-        in0_block_w=8,
-        out_subblock_h=1,  # Must be divisible by per_core_M
-        out_subblock_w=4,  # Must be divisible by per_core_N, out_subblock_w * out_subblock_h <= 4
-        per_core_M=max(
-            1, 8 if seq_len >= 2048 else seq_len // TILE_SIZE // 8  # 8 rows
-        ),  # M / TILE_HEIGHT / Grid_Size (dynamic based on seqlen)
-        per_core_N=math.ceil(28672 / 8 / 32 / 7),  # N / TILE_WIDTH / grid width
-        transpose_mcast=False,
-        fused_activation=None,
-        fuse_batch=seq_len <= 2048,
-    )
+def gen_w1_pcfg_new(height):
+    if height < 4096:
+        return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+            compute_with_storage_grid_size=(7, 10),
+            in0_block_w=8,
+            out_subblock_h=1,  # Must be divisible by per_core_M
+            out_subblock_w=4,  # Must be divisible by per_core_N, out_subblock_w * out_subblock_h <= 4
+            per_core_M=max(
+                1, 8 if height >= 2048 else height // TILE_SIZE // 8  # 8 rows
+            ),  # M / TILE_HEIGHT / Grid_Size (dynamic based on seqlen)
+            per_core_N=math.ceil(28672 / 8 / 32 / 7),  # N / TILE_WIDTH / grid width
+            transpose_mcast=False,
+            fused_activation=None,
+            fuse_batch=height <= 2048,
+        )
+
+    if height == 4096:
+        return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+            compute_with_storage_grid_size=(7, 7),
+            in0_block_w=4,
+            out_subblock_h=1,
+            out_subblock_w=8,
+            out_block_h=19,
+            out_block_w=16,
+            per_core_M=19,
+            per_core_N=16,
+            transpose_mcast=False,
+            fused_activation=None,
+            fuse_batch=False,
+        )
+    if height == 6144:
+        return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+            compute_with_storage_grid_size=(7, 7),
+            in0_block_w=4,
+            out_subblock_h=1,
+            out_subblock_w=8,
+            out_block_h=14,
+            out_block_w=16,
+            per_core_M=28,
+            per_core_N=16,
+            transpose_mcast=False,
+            fused_activation=None,
+            fuse_batch=False,
+        )
+    if height == 8192:
+        return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+            compute_with_storage_grid_size=(7, 7),
+            in0_block_w=4,
+            out_subblock_h=1,
+            out_subblock_w=8,
+            out_block_h=19,
+            out_block_w=16,
+            per_core_M=38,
+            per_core_N=16,
+            transpose_mcast=False,
+            fused_activation=None,
+            fuse_batch=False,
+        )
+
+    if height == 10240:
+        return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+            compute_with_storage_grid_size=(7, 7),
+            in0_block_w=4,
+            out_subblock_h=1,
+            out_subblock_w=8,
+            out_block_h=12,
+            out_block_w=16,
+            per_core_M=48,
+            per_core_N=16,
+            transpose_mcast=False,
+            fused_activation=None,
+            fuse_batch=False,
+        )
+    if height == 12288:
+        return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+            compute_with_storage_grid_size=(7, 7),
+            in0_block_w=4,
+            out_subblock_h=1,
+            out_subblock_w=8,
+            out_block_h=14,
+            out_block_w=16,
+            per_core_M=56,
+            per_core_N=16,
+            transpose_mcast=False,
+            fused_activation=None,
+            fuse_batch=False,
+        )
+    if height == 14336:
+        return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+            compute_with_storage_grid_size=(7, 7),
+            in0_block_w=4,
+            out_subblock_h=1,
+            out_subblock_w=8,
+            out_block_h=16,
+            out_block_w=16,
+            per_core_M=64,
+            per_core_N=16,
+            transpose_mcast=False,
+            fused_activation=None,
+            fuse_batch=False,
+        )
+    if height == 16384:
+        return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+            compute_with_storage_grid_size=(7, 7),
+            in0_block_w=4,
+            out_subblock_h=1,
+            out_subblock_w=8,
+            out_block_h=15,
+            out_block_w=16,
+            per_core_M=75,
+            per_core_N=16,
+            transpose_mcast=False,
+            fused_activation=None,
+            fuse_batch=False,
+        )
+    if height == 24576:
+        return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+            compute_with_storage_grid_size=(7, 7),
+            in0_block_w=4,
+            out_subblock_h=1,
+            out_subblock_w=8,
+            out_block_h=16,
+            out_block_w=16,
+            per_core_M=112,
+            per_core_N=16,
+            transpose_mcast=False,
+            fused_activation=None,
+            fuse_batch=False,
+        )
+    if height == 32768:
+        return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+            compute_with_storage_grid_size=(7, 7),
+            in0_block_w=4,
+            out_subblock_h=1,
+            out_subblock_w=8,
+            out_block_h=15,
+            out_block_w=16,
+            per_core_M=150,
+            per_core_N=16,
+            transpose_mcast=False,
+            fused_activation=None,
+            fuse_batch=False,
+        )
+    if height == 51200:
+        return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+            compute_with_storage_grid_size=(7, 7),
+            in0_block_w=4,
+            out_subblock_h=1,
+            out_subblock_w=8,
+            out_block_h=10,
+            out_block_w=16,
+            per_core_M=230,
+            per_core_N=16,
+            transpose_mcast=False,
+            fused_activation=None,
+            fuse_batch=False,
+        )
+    if height == 65536:
+        return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+            compute_with_storage_grid_size=(7, 7),
+            in0_block_w=4,
+            out_subblock_h=1,
+            out_subblock_w=8,
+            out_block_h=15,
+            out_block_w=16,
+            per_core_M=300,
+            per_core_N=16,
+            transpose_mcast=False,
+            fused_activation=None,
+            fuse_batch=False,
+        )
+    if height == 86016:
+        return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+            compute_with_storage_grid_size=(7, 7),
+            in0_block_w=4,
+            out_subblock_h=1,
+            out_subblock_w=8,
+            out_block_h=12,
+            out_block_w=16,
+            per_core_M=384,
+            per_core_N=16,
+            transpose_mcast=False,
+            fused_activation=None,
+            fuse_batch=False,
+        )
+    if height == 131072:
+        return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+            compute_with_storage_grid_size=(7, 7),
+            in0_block_w=4,
+            out_subblock_h=1,
+            out_subblock_w=8,
+            out_block_h=16,
+            out_block_w=16,
+            per_core_M=592,
+            per_core_N=16,
+            transpose_mcast=False,
+            fused_activation=None,
+            fuse_batch=False,
+        )
 
 
 def create_dram_sharded_mem_config(k, n):
@@ -56,13 +244,16 @@ def create_dram_sharded_mem_config(k, n):
     return ttnn.MemoryConfig(ttnn.TensorMemoryLayout.WIDTH_SHARDED, ttnn.BufferType.DRAM, shard_spec)
 
 
+w1 = torch.randn((1, 1, 8192, 28672))
+
+
 @pytest.mark.parametrize(
     "device_params",
     [{"dispatch_core_axis": ttnn.DispatchCoreAxis.COL}],
     indirect=True,
 )
 @pytest.mark.parametrize(
-    "device",
+    "mesh_device",
     [
         (8, 4),
     ],
@@ -72,68 +263,93 @@ def create_dram_sharded_mem_config(k, n):
     "seq_len",
     SEQ_LENS,
 )
+@pytest.mark.parametrize(
+    "stress_test",
+    [True, False],
+)
 ###################################################################################
 # ON 4U THE BEST PERF WE CAN GET (WITHOUT HANGING) IS ALREADY IMPLEMENTED IN MAIN #
 ###################################################################################
-def test_w1(device, seq_len):
-    activations = (
-        torch.randn((1, seq_len // 1024, 1024, 2048)) if (seq_len >= 1024) else torch.randn((1, 1, seq_len, 2048))
-    )
-    w1 = torch.randn((1, 1, 2048, 3584))
-
-    golden = activations @ w1.squeeze()
-
-    activations_tt = ttnn.from_torch(
-        activations,
-        device=device,
-        dtype=ttnn.bfloat16,
-        memory_config=ttnn.DRAM_MEMORY_CONFIG,
-        layout=ttnn.TILE_LAYOUT,
-    )
-    w1_tt = ttnn.from_torch(
+def test_w1(mesh_device, seq_len, stress_test):
+    w1_tt_sharded = ttnn.from_torch(
         w1,
-        device=device,
+        device=mesh_device,
         dtype=ttnn.bfloat4_b,
         memory_config=create_dram_sharded_mem_config(2048, 3584),
+        mesh_mapper=ttnn.ShardTensor2dMesh(mesh_device, dims=(-1, -2), mesh_shape=(8, 4)),
+        layout=ttnn.TILE_LAYOUT,
+    )
+    w1_tt_interleaved = ttnn.from_torch(
+        w1,
+        device=mesh_device,
+        dtype=ttnn.bfloat4_b,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        mesh_mapper=ttnn.ShardTensor2dMesh(mesh_device, dims=(-1, -2), mesh_shape=(8, 4)),
         layout=ttnn.TILE_LAYOUT,
     )
 
-    out_tt = ttnn.linear(
-        activations_tt,
-        w1_tt,
-        compute_kernel_config=ttnn.WormholeComputeKernelConfig(
-            math_fidelity=ttnn.MathFidelity.LoFi,
-            math_approx_mode=False,
-            fp32_dest_acc_en=False,
-            packer_l1_acc=True,
-            dst_full_sync_en=True,
-        ),
-        program_config=generate_w1_w3_program_config(seq_len),
-        dtype=ttnn.bfloat8_b,
-        memory_config=ttnn.DRAM_MEMORY_CONFIG,
-    )
+    def run_test(seq_len):
+        activations = torch.randn((1, 1, seq_len, 2048))
+        w1_tt = w1_tt_sharded if seq_len < 4096 else w1_tt_interleaved
+        activations_tt = ttnn.from_torch(
+            activations,
+            device=mesh_device,
+            dtype=ttnn.bfloat16,
+            memory_config=ttnn.DRAM_MEMORY_CONFIG,
+            mesh_mapper=ttnn.ReplicateTensorToMesh(mesh_device),
+            layout=ttnn.TILE_LAYOUT,
+        )
+        iterations = 1000 if stress_test else 1
+        logger.info("BEGINNING TEST")
+        for i in range(iterations):
+            logger.info(f"SEQ_LEN: {seq_len} | Running iteration {i}")
+            out_tt = ttnn.linear(
+                activations_tt,
+                w1_tt,
+                compute_kernel_config=ttnn.WormholeComputeKernelConfig(
+                    math_fidelity=ttnn.MathFidelity.LoFi,
+                    math_approx_mode=False,
+                    fp32_dest_acc_en=False,
+                    packer_l1_acc=True,
+                    dst_full_sync_en=True,
+                ),
+                program_config=gen_w1_pcfg_new(seq_len),
+                dtype=ttnn.bfloat8_b,
+                memory_config=ttnn.DRAM_MEMORY_CONFIG,
+            )
 
-    out_torch = ttnn.to_torch(out_tt)
-    passed, msg = comp_pcc(golden, out_torch, 0.99)
-    assert passed, msg
+    if seq_len == "all":
+        for seq_len in SEQ_LENS[:-1]:
+            logger.info(f"Testing W1: seq_len = {seq_len}")
+            run_test(seq_len)
+    else:
+        logger.info(f"Testing W1: seq_len = {seq_len}")
+        run_test(seq_len)
 
 
-def gen_w2_pcfg_4U(activation_height):
-    if activation_height < 4096:
+def gen_w2_pcfg_new(height):
+    if height < 4096:
         return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
             compute_with_storage_grid_size=(7, 10),
             in0_block_w=8,  # FIXME: optimize this config for prefill, careful use DI_DT_WORKAROUND if necessary
             out_subblock_h=1,  # Must be divisible by per_core_M
             out_subblock_w=2,  # Must be divisible by per_core_N, out_subblock_w * out_subblock_h <= 4
-            per_core_M=max(1, 8 if activation_height >= 2048 else activation_height // TILE_SIZE // 8),  # 8~10 rows
+            per_core_M=max(1, 8 if height >= 2048 else height // TILE_SIZE // 8),  # 8~10 rows
             per_core_N=math.ceil(2048 / 32 / 7),  # N / TILE_WIDTH / grid width
             transpose_mcast=False,
             fused_activation=None,
-            fuse_batch=activation_height <= 2048,
+            fuse_batch=height <= 2048,
         )
 
-    # Using 224 to decide per_core_M as 224 = TILE_SIZE * core_grid_y. core_grid_y = 7
-    per_core_M = math.ceil(activation_height / 224)
+    # For very large activation heights (arbitrarily chosen to be > 320) we want the per_core_M to have many divisors
+    # so that there are many options for out_block_h and out_block_w. Padding to the next multiple of 8 ensures that
+    # per_core_M can at least be divisible by 2, 4, and 8 in addition to 1 and itself.
+    #
+    # If the number is less than or equal to 320 we still wouldn't want it to be prime so we'll add one if thats the case.
+    next_multiple_of_8 = lambda x: int(x + (8 - x % 8) % 8)
+    add_one_if_prime = lambda n: n + 1 if n > 1 and all(n % i != 0 for i in range(2, int(n**0.5) + 1)) else n
+    total_per_core_out_M = add_one_if_prime(math.ceil(height / (7 * TILE_SIZE)))
+    per_core_M = next_multiple_of_8(total_per_core_out_M) if total_per_core_out_M > 320 else total_per_core_out_M
     per_core_N = 10
 
     # Want out_block_h and out_block_w such that:
@@ -174,7 +390,7 @@ def gen_w2_pcfg_4U(activation_height):
     elif out_block_h_if_w_10 * 10 > out_block_h_if_w_5 * 5:
         out_block_h = out_block_h_if_w_10
         out_block_w = 10
-    # If both have the same volume, pick the configuration that is more "square"
+    # If neither exist pick the one which is more "square"
     else:
         # Want to use the out_block_h/w combination which is the most "square"
         # This calculates the height/width ratio of the blocks and then gets their
@@ -204,15 +420,6 @@ def gen_w2_pcfg_4U(activation_height):
     )
 
 
-def get_mlp_activation_height(seq_len):
-    """Find largest divisor of of seq_len that is <= 16384."""
-    if 16384 >= seq_len:
-        return seq_len
-    for i in range(min(16384, seq_len), 0, -1):
-        if seq_len % i == 0:
-            return i
-
-
 w2 = torch.randn(28672, 8192)
 
 
@@ -234,20 +441,9 @@ w2 = torch.randn(28672, 8192)
 )
 @pytest.mark.parametrize(
     "stress_test",
-    [True, False],
+    [True],
 )
 def test_w2(mesh_device, seq_len, stress_test):
-    act_height = get_mlp_activation_height(seq_len)
-    activations = torch.randn(1, 1, seq_len // act_height, act_height, 3584)
-
-    activations_tt = ttnn.from_torch(
-        activations,
-        device=mesh_device,
-        dtype=ttnn.bfloat8_b,
-        memory_config=ttnn.DRAM_MEMORY_CONFIG,
-        mesh_mapper=ttnn.ReplicateTensorToMesh(mesh_device),
-        layout=ttnn.TILE_LAYOUT,
-    )
     w2_tt = ttnn.from_torch(
         w2,
         device=mesh_device,
@@ -256,24 +452,66 @@ def test_w2(mesh_device, seq_len, stress_test):
         mesh_mapper=ttnn.ShardTensor2dMesh(mesh_device, dims=(-2, -1), mesh_shape=(8, 4)),
         layout=ttnn.TILE_LAYOUT,
     )
-    iterations = 1000 if stress_test else 1
-    program_config = gen_w2_pcfg_4U(activations_tt.shape[-2])
-    for i in range(iterations):
-        out_tt = ttnn.linear(
-            activations_tt,
-            w2_tt,
-            compute_kernel_config=ttnn.WormholeComputeKernelConfig(
-                math_fidelity=ttnn.MathFidelity.HiFi2,
-                math_approx_mode=False,
-                fp32_dest_acc_en=False,
-                packer_l1_acc=True,
-                dst_full_sync_en=True,
-            ),
-            program_config=program_config,
+
+    def run_test(seq_len):
+        activations = torch.randn(1, 1, seq_len, 3584)
+
+        activations_tt = ttnn.from_torch(
+            activations,
+            device=mesh_device,
             dtype=ttnn.bfloat8_b,
             memory_config=ttnn.DRAM_MEMORY_CONFIG,
+            mesh_mapper=ttnn.ReplicateTensorToMesh(mesh_device),
+            layout=ttnn.TILE_LAYOUT,
         )
 
+        logger.info(f"BEGINNING TEST")
+        iterations = 1000 if stress_test else 1
+        for i in range(iterations):
+            logger.info(f"SEQ_LEN: {seq_len} | Running iteration {i+1}")
+            out_tt = ttnn.linear(
+                activations_tt,
+                w2_tt,
+                compute_kernel_config=ttnn.WormholeComputeKernelConfig(
+                    math_fidelity=ttnn.MathFidelity.HiFi2,
+                    math_approx_mode=False,
+                    fp32_dest_acc_en=False,
+                    packer_l1_acc=True,
+                    dst_full_sync_en=True,
+                ),
+                program_config=gen_w2_pcfg_new(seq_len),
+                dtype=ttnn.bfloat8_b,
+                memory_config=ttnn.DRAM_MEMORY_CONFIG,
+            )
+
+    if seq_len == "all":
+        for seq_len in SEQ_LENS[:-1]:
+            logger.info(f"Testing W2: seq_len = {seq_len}")
+            run_test(seq_len)
+    else:
+        logger.info(f"Testing W2: seq_len = {seq_len}")
+        run_test(seq_len)
+
+
+# W1 STRESS TEST RESULTS:
+# 128 - PASS
+# 256 - PASS
+# 512 - PASS
+# 1024 - PASS
+# 2048 - PASS
+# 4096 - PASS
+# 6144 - PASS
+# 8192 - PASS
+# 10240 - PASS
+# 12288 - PASS
+# 14336 - PASS
+# 16384 - PASS
+# 24576 - PASS
+# 32768 - PASS
+# 51200 - PASS
+# 65536 - PASS
+# 86016 - PASS
+# 131072 - PASS
 
 # W2 STRESS TEST RESULTS:
 # 128 - PASS
