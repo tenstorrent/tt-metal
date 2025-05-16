51b7d832410b487e7332a70eca5f0b9cdd2f4b76
Aswinmcw <azayasankaran@tenstorrent.com>
2025-05-16 07:54:31 +0000

    avoid sfpi namespace include

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_rounding_ops.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_rounding_ops.h
@@ -7,43 +7,42 @@
 #include "ckernel.h"
 #include "ckernel_defs.h"
 #include "sfpi.h"
-#include "noc_nonblocking_api.h"
 #include "limits.h"
-
-using namespace sfpi;
 
 namespace ckernel {
 namespace sfpu {
 
-inline vInt float_to_int32(vFloat in) {
-    vInt result;
-    vInt exp = exexp(in);  // extract exponent
+inline sfpi::vInt float_to_int32(sfpi::vFloat in) {
+    sfpi::vInt result;
+    sfpi::vInt exp = exexp(in);  // extract exponent
     v_if(exp < 0) { result = 0; }
     v_elseif(exp > 30) {
         // set to int32 max value in case of overflow
         result = std::numeric_limits<int32_t>::max();
         // check sign
-        v_if(in < 0) { result = reinterpret<vInt>(setsgn(reinterpret<vFloat>(result), 1)); }
+        v_if(in < 0) {
+            result = sfpi::reinterpret<sfpi::vInt>(sfpi::setsgn(sfpi::reinterpret<sfpi::vFloat>(result), 1));
+        }
         v_endif
     }
     v_else {
         // extract mantissa
-        vInt man = exman8(in);
+        sfpi::vInt man = exman8(in);
         // shift the mantissa by (23-exponent) to the right
-        vInt shift = exp - 23;
-        man = shft(reinterpret<vUInt>(man), shift);
+        sfpi::vInt shift = exp - 23;
+        man = shft(sfpi::reinterpret<sfpi::vUInt>(man), shift);
         // check sign
-        v_if(in < 0) { man = reinterpret<vInt>(setsgn(reinterpret<vFloat>(man), 1)); }
+        v_if(in < 0) { man = sfpi::reinterpret<sfpi::vInt>(sfpi::setsgn(sfpi::reinterpret<sfpi::vFloat>(man), 1)); }
         v_endif result = man;
     }
     v_endif return result;
 }
 
-inline vInt float_to_int31(vFloat v) {
-    vInt q = float_to_int16(v * 0x1p-15f, 0);
-    vInt r = float_to_int16(v - int32_to_float(q, 0) * 0x1p15f, 0);
+inline sfpi::vInt float_to_int31(sfpi::vFloat v) {
+    sfpi::vInt q = float_to_int16(v * 0x1p-15f, 0);
+    sfpi::vInt r = float_to_int16(v - int32_to_float(q, 0) * 0x1p15f, 0);
     v_if(r < 0) {
-        r = setsgn(r, 0);
+        r = sfpi::setsgn(r, 0);
         q = (q << 15) - r;
     }
     v_else { q = (q << 15) + r; }
@@ -62,71 +61,71 @@ inline constexpr float TABLE[] = {
 template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
 inline void calculate_floor() {
     for (int d = 0; d < ITERATIONS; d++) {
-        vFloat result = dst_reg[0];
-        vFloat v = result;
-        vInt tmp = float_to_int16(result, 0);
+        sfpi::vFloat result = sfpi::dst_reg[0];
+        sfpi::vFloat v = result;
+        sfpi::vInt tmp = float_to_int16(result, 0);
         result = int32_to_float(tmp, 0);
         v_if(result > v) { result = result - 1; }
         v_endif;
         v_if(v <= SHRT_MIN || v >= SHRT_MAX) { result = v; }
         v_endif;
-        dst_reg[0] = result;
-        dst_reg++;
+        sfpi::dst_reg[0] = result;
+        sfpi::dst_reg++;
     }
 }
 
 template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
 inline void calculate_floor_float32() {
     for (int d = 0; d < ITERATIONS; d++) {
-        vFloat result = dst_reg[0];
-        vFloat v = result;
-        vInt tmp = float_to_int32(result);
+        sfpi::vFloat result = sfpi::dst_reg[0];
+        sfpi::vFloat v = result;
+        sfpi::vInt tmp = float_to_int32(result);
         result = int32_to_float(tmp, 0);
         v_if(result > v) { result = result - 1; }
         v_endif;
-        dst_reg[0] = result;
-        dst_reg++;
+        sfpi::dst_reg[0] = result;
+        sfpi::dst_reg++;
     }
 }
 
 template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
 inline void calculate_ceil() {
     for (int d = 0; d < ITERATIONS; d++) {
-        vFloat result = dst_reg[0];
-        vFloat v = result;
-        vInt tmp = float_to_int16(result, 0);
+        sfpi::vFloat result = sfpi::dst_reg[0];
+        sfpi::vFloat v = result;
+        sfpi::vInt tmp = float_to_int16(result, 0);
         result = int32_to_float(tmp, 0);
         v_if(result < v) { result = result + 1; }
         v_endif;
         v_if(v <= SHRT_MIN || v >= SHRT_MAX) { result = v; }
         v_endif;
-        dst_reg[0] = result;
-        dst_reg++;
+        sfpi::dst_reg[0] = result;
+        sfpi::dst_reg++;
     }
 }
 
 template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
 inline void calculate_ceil_float32() {
     for (int d = 0; d < ITERATIONS; d++) {
-        vFloat result = dst_reg[0];
-        vFloat v = result;
-        vInt tmp = float_to_int32(result);
+        sfpi::vFloat result = sfpi::dst_reg[0];
+        sfpi::vFloat v = result;
+        sfpi::vInt tmp = float_to_int32(result);
         result = int32_to_float(tmp, 0);
         v_if(result < v) { result = result + 1; }
         v_endif;
-        dst_reg[0] = result;
-        dst_reg++;
+        sfpi::dst_reg[0] = result;
+        sfpi::dst_reg++;
     }
 }
 
-inline vFloat round_even(vFloat v) {
-    vFloat result;
+inline sfpi::vFloat round_even(sfpi::vFloat v) {
+    sfpi::vFloat result;
     v_if(sfpi::abs(v) < 0x1p30f) {
         result = int32_to_float(float_to_int31(v), 0);
         v_if(sfpi::abs(v - result) == 0.5F) {
-            vInt res = float_to_int16(result, 0);
+            sfpi::vInt res = float_to_int16(result, 0);
             res = res & 0x7FFE;
-            result = int32_to_float(res, 0);
+            result = sfpi::int32_to_float(res, 0);
         }
         v_endif;
     }
@@ -148,15 +147,15 @@ void calculate_round(const int decimals) {
         return TABLE[n + 45];
     };
 
-    const vFloat coeff = exp10i(decimals);
-    const vFloat inverse = exp10i(-decimals);
+    const sfpi::vFloat coeff = exp10i(decimals);
+    const sfpi::vFloat inverse = exp10i(-decimals);
 
     for (int _ = 0; _ < ITERATIONS; ++_) {
-        vFloat v = dst_reg[0];
-        vFloat result = inverse * round_even(sfpi::abs(v) * coeff);
-        result = setsgn(result, v);
-        dst_reg[0] = result;
-        ++dst_reg;
+        sfpi::vFloat v = sfpi::dst_reg[0];
+        sfpi::vFloat result = inverse * round_even(sfpi::abs(v) * coeff);
+        result = sfpi::setsgn(result, v);
+        sfpi::dst_reg[0] = result;
+        sfpi::dst_reg++;
     }
 }
 
