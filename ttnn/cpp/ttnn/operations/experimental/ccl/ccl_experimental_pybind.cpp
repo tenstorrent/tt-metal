4062ad4e4bb7a1630e0d437d8ef31e863858a9a7
Ligang Long <llong@tenstorrent.com>
2025-05-16 11:09:43 -0700

    RS fuse create qkv heads (#20904)
    ### Ticket
    
    [N/A](https://docs.google.com/spreadsheets/d/1d87Lk7DNA2rfVwo6JDdyzHXt4aUtIwvYPdfPU80zTFY/edit?gid=1638063046#gid=1638063046)
    
    ### Problem description
    Fusion of new reduce scatter + create qkv heads.
    
    ### What's changed
    Fusion of new reduce scatter + create qkv heads.
    Currently the llama3 model use all reduce + create qkv heads with kernel
    time 14.3us.
    The new fusion runs in at kernel time 8.4 us.
    This is only the op PR, model integration waits for the 1D ring MM and
    ROPE update to support Row Major layout.
    
    ### Checklist
    - [x] [All post
    commit]https://github.com/tenstorrent/tt-metal/actions/runs/14963804756
    CI passes
    - [x] [TG unit
    tests]https://github.com/tenstorrent/tt-metal/actions/runs/14963900819
    - [x] [TG model perf
    test]https://github.com/tenstorrent/tt-metal/actions/runs/14982930938
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [ ] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/experimental/ccl/ccl_experimental_pybind.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/ccl_experimental_pybind.cpp
@@ -11,6 +11,7 @@
 #include "ttnn/operations/experimental/ccl/reduce_scatter_async/reduce_scatter_pybind.hpp"
 #include "ttnn/operations/experimental/ccl/all_reduce_async/all_reduce_async_pybind.hpp"
 #include "ttnn/operations/experimental/ccl/llama_reduce_scatter/llama_reduce_scatter_pybind.hpp"
+#include "ttnn/operations/experimental/ccl/llama_reduce_scatter_create_heads/llama_reduce_scatter_create_heads_pybind.hpp"
 
 namespace ttnn::operations::experimental::ccl {
 
@@ -24,6 +25,7 @@ void py_module(pybind11::module& module) {
     ccl::py_bind_reduce_scatter_async(module);
     ccl::py_bind_all_reduce_async(module);
     ccl::py_bind_llama_reduce_scatter(module);
+    ccl::py_bind_llama_rs_create_heads(module);
 }
 
 }  // namespace ttnn::operations::experimental::ccl
