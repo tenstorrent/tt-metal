334a419c35b7249d0e57f66abb9a1d49af21b4d5
Mo <mmemarian@tenstorrent.com>
2025-05-16 18:34:24 +0000

    MOCK_L1

--- tt_metal/api/tt-metalium/profiler_types.hpp
+++ tt_metal/api/tt-metalium/profiler_types.hpp
@@ -8,5 +8,6 @@ namespace tt::tt_metal {
 
 enum class ProfilerDumpState { NORMAL, CLOSE_DEVICE_SYNC, LAST_CLOSE_DEVICE, FORCE_UMD_READ };
 enum class ProfilerSyncState { INIT, CLOSE_DEVICE };
+enum class ProfilerDataBufferSource { L1, DRAM };
 
 }  // namespace tt::tt_metal
