8317841e267f4d368cc242e8bcca00a617f4c09c
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2025-05-16 17:35:43 +0200

    reintroduce general purpose conv2d

---
+++ models/experimental/stable_diffusion3/tests/test_conv2d.py
@@ -0,0 +1,78 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from __future__ import annotations
+
+import pytest
+import torch
+import ttnn
+
+from ..tt.conv2d import TtConv2d, TtConv2dParameters
+from ..tt.utils import assert_quality, from_torch_fast, to_torch
+
+
+@pytest.mark.parametrize(
+    ("batch_size", "in_channels", "out_channels", "kernel_size", "stride", "height", "width", "slice_count"),
+    [
+        (10, 32, 32, (2, 3), (2, 2), 64, 64, 1),
+        (10, 20, 32, (3, 3), (2, 3), 128, 256, 1),
+        # these are needed in the VAE for an image resolution of 1024x1024:
+        (1, 128, 128, (3, 3), (1, 1), 1024, 1024, 16),
+        # the next case with lower slice_count https://github.com/tenstorrent/tt-metal/issues/17489#issuecomment-2886552080
+        (1, 128, 3, (3, 3), (1, 1), 1024, 1024, 64),
+        (1, 16, 512, (3, 3), (1, 1), 128, 128, 1),
+        (1, 256, 128, (3, 3), (1, 1), 1024, 1024, 32),
+        (1, 256, 256, (3, 3), (1, 1), 1024, 1024, 32),
+        (1, 256, 256, (3, 3), (1, 1), 512, 512, 8),
+        (1, 512, 512, (3, 3), (1, 1), 128, 128, 1),
+        # (1, 512, 512, (3, 3), (1, 1), 256, 256, 2),  # low PCC of about 95%
+        (1, 512, 256, (3, 3), (1, 1), 512, 512, 8),
+        # (1, 512, 512, (3, 3), (1, 1), 512, 512, 8),  # low PCC of about 95%
+    ],
+)
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 8192 * 2}], indirect=True)
+# @pytest.mark.usefixtures("use_program_cache")
+def test_conv2d(
+    *,
+    device: ttnn.Device,
+    batch_size: int,
+    in_channels: int,
+    out_channels: int,
+    kernel_size: tuple[int, int],
+    stride: tuple[int, int],
+    height: int,
+    width: int,
+    slice_count: int,
+) -> None:
+    dtype = ttnn.bfloat16
+
+    torch.manual_seed(0)
+
+    torch_model = torch.nn.Conv2d(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        stride=stride,
+    )
+    torch_model.eval()
+
+    parameters = TtConv2dParameters.from_torch(torch_model.state_dict(), dtype=dtype, device=device)
+    tt_model = TtConv2d(parameters, stride=stride, slice_count=slice_count)
+
+    torch_input_tensor = torch.randn((batch_size, in_channels, height, width))
+
+    tt_input_tensor = from_torch_fast(
+        torch_input_tensor.permute([0, 2, 3, 1]),  # BCYX -> BYXC
+        device=device,
+        layout=ttnn.ROW_MAJOR_LAYOUT if slice_count > 1 else ttnn.TILE_LAYOUT,
+        dtype=dtype,
+    )
+
+    with torch.no_grad():
+        torch_output = torch_model(torch_input_tensor)
+
+    tt_output = tt_model(tt_input_tensor, memory_config=ttnn.DRAM_MEMORY_CONFIG)
+    tt_output_torch = to_torch(tt_output).permute([0, 3, 1, 2])
+
+    assert_quality(torch_output, tt_output_torch, pcc=0.994, ccc=0.992)
