dc39d8b513d52787dca03ca83d5671c677e568cb
Philipp Moritz <pcmoritz@gmail.com>
2025-05-17 15:29:00 -0700

    update

--- tt_metal/programming_examples/add_2_integers_in_compute/kernels/compute/add_2_tiles.cpp
+++ tt_metal/programming_examples/add_2_integers_in_compute/kernels/compute/add_2_tiles.cpp
@@ -6,7 +6,26 @@
 #include "compute_kernel_api/eltwise_binary.h"
 #include "compute_kernel_api/tile_move_copy.h"
 
-#include "debug/dprint_pages.h"
+inline void print_full_tile(uint32_t cb_id, uint32_t tile_id = 0, bool untilize = false) {
+    DPRINT << "======" << ENDL();
+    for (uint16_t r = 0; r < 32; ++r) {
+        DPRINT << (uint)r << " : "
+               << TileSlice(
+                      cb_id,
+                      tile_id,
+                      SliceRange{
+                          .h0 = (uint8_t)r,
+                          .h1 = (uint8_t)(r + 1),
+                          .hs = (uint8_t)1,
+                          .w0 = (uint8_t)0,
+                          .w1 = (uint8_t)32,
+                          .ws = (uint8_t)1},
+                      true,
+                      untilize)
+               << ENDL();
+    }
+    DPRINT << "++++++" << ENDL();
+}
 
 namespace NAMESPACE {
 void MAIN {
@@ -23,7 +42,7 @@ void MAIN {
 
     tile_regs_acquire();  // acquire 8 tile registers
 
-    tt::compute::common::print_full_tile(cb_in0);
+    print_full_tile(cb_in0);
     add_tiles(cb_in0, cb_in1, 0, 0, 0);
 
     tile_regs_commit();  // signal the packer
