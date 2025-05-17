ff18a79a1bb041db955d7165227b7d5c2d6436ed
Philipp Moritz <pcmoritz@gmail.com>
2025-05-17 15:10:54 -0700

    update

--- tt_metal/api/tt-metalium/uint8.hpp
+++ tt_metal/api/tt-metalium/uint8.hpp
@@ -4,5 +4,8 @@
 
 #pragma once
 
+#include <cstdint>
+#include <vector>
+
 // Number of elements needs to be divisible by 4 for the time being
 std::vector<std::uint32_t> create_constant_vector_of_uint8(uint32_t num_bytes, uint8_t value);
