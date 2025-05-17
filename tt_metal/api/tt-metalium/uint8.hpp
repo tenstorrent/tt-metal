1d944be1758efd4be6188e02de490a396db363c1
Philipp Moritz <pcmoritz@gmail.com>
2025-05-17 15:00:22 -0700

    Add initial code

---
+++ tt_metal/api/tt-metalium/uint8.hpp
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+// Number of elements needs to be divisible by 4 for the time being
+std::vector<std::uint32_t> create_constant_vector_of_uint8(uint32_t num_bytes, uint8_t value);
