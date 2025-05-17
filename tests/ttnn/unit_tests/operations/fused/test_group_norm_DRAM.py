1f3ca46e0b9a71285574ed10f9aef914bc4de50d
Vasisht Suresh <vsuresh@tenstorrent.com>
2025-05-16 23:43:26 -0700

    Vsuresh tt/#21833/vaegn (#22157)
    ### Ticket
    Link to Github Issue
    https://github.com/tenstorrent/tt-metal/issues/21833
    ### Problem description
    Provide context for the problem.
    Issue where illegal core grids were not caught by the program factory
    ### What's changed
    Describe the approach used to solve the problem.
    Summarize the changes made and its impact.
    
    Added TT_FATALs to better cover these types of errors, and
    added/compacted tests with model specific shapes that are important.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [x] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [ ] New/Existing tests provide coverage for changes

--- tests/ttnn/unit_tests/operations/fused/test_group_norm_DRAM.py
+++ tests/ttnn/unit_tests/operations/fused/test_group_norm_DRAM.py
@@ -38,8 +38,20 @@ from models.utility_functions import skip_for_wormhole_b0, skip_for_blackhole
             32,
             2,
             4,
-            8,
+            4,
         ),  # test all groups on core fit in less than one tile, so need to reduce col core count
+        # # # SDXL 1024x1024 resoultion
+        (2, 1920, 64, 64, 32, 4, 4, 4),
+        (2, 320, 128, 128, 32, 12, 2, 2),
+        (2, 640, 128, 128, 32, 6, 4, 4),
+        (2, 960, 128, 128, 32, 12, 2, 2),
+        # VAE
+        # tensor is too large, but good example
+        (1, 256, 1024, 1024, 32, 128, 8, 4),
+        (1, 256, 515, 512, 32, 32, 4, 4),
+        (1, 512, 128, 128, 32, 4, 1, 4),
+        (1, 512, 256, 256, 32, 16, 4, 4),
+        (1, 512, 512, 512, 32, 32, 4, 4),
     ],
 )
 def test_group_norm_DRAM(device, N, C, H, W, num_groups, num_out_blocks, cores_y, cores_x):
@@ -48,108 +60,6 @@ def test_group_norm_DRAM(device, N, C, H, W, num_groups, num_out_blocks, cores_y
         pytest.skip()
 
     grid_size = ttnn.CoreGrid(y=cores_y, x=cores_x)
-
-    # torch input tensor
-    torch_input_tensor = torch.rand((N, C, H, W), dtype=torch.bfloat16)
-    torch_weight = torch.rand((C,), dtype=torch.bfloat16)
-    torch_bias = torch.rand((C,), dtype=torch.bfloat16)
-    torch_output_tensor = torch.nn.functional.group_norm(
-        torch_input_tensor, num_groups, weight=torch_weight, bias=torch_bias
-    )
-    torch_output_tensor = torch_output_tensor.permute(0, 2, 3, 1).view(N, 1, W * H, C)
-
-    # input tensor
-    input_tensor = torch_input_tensor.permute(0, 2, 3, 1).view(N, 1, W * H, C)
-    input_tensor_row_major = ttnn.from_torch(
-        input_tensor,
-        dtype=ttnn.DataType.BFLOAT16,
-        layout=ttnn.ROW_MAJOR_LAYOUT,
-        device=device,
-        memory_config=ttnn.DRAM_MEMORY_CONFIG,
-    )
-    input_tensor_tilized = ttnn.tilize_with_zero_padding(input_tensor_row_major, use_multicore=True)
-
-    # input mask
-    input_mask_tensor = ttnn.create_group_norm_input_mask(C, num_groups, grid_size.y)
-    input_mask_tensor = ttnn.from_torch(
-        input_mask_tensor,
-        dtype=ttnn.DataType.BFLOAT8_B,
-        layout=ttnn.TILE_LAYOUT,
-        device=device,
-        memory_config=ttnn.DRAM_MEMORY_CONFIG,
-    )
-
-    # gamma/beta
-    gamma = ttnn.create_group_norm_weight_bias_rm(torch_weight, C, grid_size.y)
-    beta = ttnn.create_group_norm_weight_bias_rm(torch_bias, C, grid_size.y)
-
-    gamma_t = ttnn.from_torch(
-        gamma,
-        dtype=ttnn.DataType.BFLOAT16,
-        layout=ttnn.ROW_MAJOR_LAYOUT,
-        device=device,
-        memory_config=ttnn.DRAM_MEMORY_CONFIG,
-    )
-    beta_t = ttnn.from_torch(
-        beta,
-        dtype=ttnn.DataType.BFLOAT16,
-        layout=ttnn.ROW_MAJOR_LAYOUT,
-        device=device,
-        memory_config=ttnn.DRAM_MEMORY_CONFIG,
-    )
-
-    # groupnorm
-    output_tensor = ttnn.group_norm(
-        input_tensor_tilized,
-        num_groups=num_groups,
-        input_mask=input_mask_tensor,
-        weight=gamma_t,
-        bias=beta_t,
-        memory_config=ttnn.DRAM_MEMORY_CONFIG,
-        output_layout=ttnn.TILE_LAYOUT,
-        core_grid=grid_size,
-        inplace=False,
-        num_out_blocks=num_out_blocks,
-    )
-
-    # output tensor
-    output_tensor = ttnn.from_device(output_tensor)
-    output_tensor = ttnn.to_torch(output_tensor)
-
-    assert_with_pcc(torch_output_tensor, output_tensor, 0.9996)
-
-
-def generate_sdxl_dram_test_inputs():
-    inputs = []
-
-    # 1024x1024 resoultion
-    inputs.append(((2, 1920, 64, 64), 4, 4))
-    inputs.append(((2, 320, 128, 128), 2, 4))
-    inputs.append(((2, 640, 128, 128), 4, 4))
-    inputs.append(((2, 960, 128, 128), 2, 4))
-
-    # VAE tests
-    inputs.append(((1, 256, 1024, 1024), 4, 64))
-    inputs.append(((1, 256, 1024, 1024), 8, 64))
-    inputs.append(((1, 256, 515, 512), 8, 16))
-    inputs.append(((1, 512, 128, 128), 4, 4))
-    inputs.append(((1, 512, 256, 256), 8, 4))
-    inputs.append(((1, 512, 512, 512), 8, 16))
-
-    return inputs
-
-
-@skip_for_blackhole("Fails on Blackhole. Issue #20913")
-@pytest.mark.parametrize("device_params", [{"l1_small_size": 0}], indirect=True)
-@pytest.mark.parametrize("input_shape, core_grid_y, num_out_blocks", generate_sdxl_dram_test_inputs())
-def test_sdxl_base_group_norm(device, input_shape, core_grid_y, num_out_blocks, use_program_cache):
-    torch.manual_seed(0)
-    if device.core_grid.y == 7:
-        pytest.skip()
-
-    num_groups = 32
-    N, C, H, W = input_shape
-    grid_size = ttnn.CoreGrid(y=core_grid_y, x=8)
 
     # torch input tensor
     torch_input_tensor = torch.rand((N, C, H, W), dtype=torch.bfloat16)
