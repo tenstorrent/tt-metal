003458cf3d5720019224240f3c0cdf74a5e0e6f4
Dragica Stoiljkovic <dstoiljkovic@tenstorrent.com>
2025-05-19 09:22:46 +0000

    Not passing correct scalars to the cores that are calulcating only one elements and close to the border of the output tesnor

--- tests/ttnn/unit_tests/operations/pool/test_avgpool2d.py
+++ tests/ttnn/unit_tests/operations/pool/test_avgpool2d.py
@@ -47,8 +47,8 @@ def tensor_map():
 @pytest.mark.parametrize(
     "ceil_mode",
     [
-        False,
-        # True,
+        # False,
+        True,
     ],
 )
 @pytest.mark.parametrize(
@@ -66,8 +66,8 @@ def tensor_map():
     "shard_scheme",
     [
         ttnn.TensorMemoryLayout.HEIGHT_SHARDED,
-        ttnn.TensorMemoryLayout.WIDTH_SHARDED,
-        ttnn.TensorMemoryLayout.BLOCK_SHARDED,
+        # ttnn.TensorMemoryLayout.WIDTH_SHARDED,
+        # ttnn.TensorMemoryLayout.BLOCK_SHARDED,
     ],
 )
 @pytest.mark.parametrize(
