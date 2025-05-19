16c5c241d874cb5c4acafca97ed6adddf06cd647
mouliraj-mcw <mcw-melamurugan@ext.tenstorrent.com>
2025-05-19 10:15:18 +0000

    #21826: Migrate tanhshrink as device op

--- tests/ttnn/unit_tests/operations/eltwise/test_composite.py
+++ tests/ttnn/unit_tests/operations/eltwise/test_composite.py
@@ -554,25 +554,6 @@ def test_unary_composite_swish_ttnn(input_shapes, device):
         (torch.Size([1, 3, 320, 384])),
     ),
 )
-def test_unary_composite_tanhshrink_ttnn(input_shapes, device):
-    in_data1, input_tensor1 = data_gen_with_range(input_shapes, -100, 100, device)
-
-    output_tensor = ttnn.tanhshrink(input_tensor1)
-    golden_function = ttnn.get_golden_function(ttnn.tanhshrink)
-    golden_tensor = golden_function(in_data1)
-
-    comp_pass = compare_pcc([output_tensor], [golden_tensor])
-    assert comp_pass
-
-
-@pytest.mark.parametrize(
-    "input_shapes",
-    (
-        (torch.Size([1, 1, 32, 32])),
-        (torch.Size([1, 1, 320, 384])),
-        (torch.Size([1, 3, 320, 384])),
-    ),
-)
 def test_unary_composite_threshold_ttnn(input_shapes, device):
     in_data1, input_tensor1 = data_gen_with_range(input_shapes, -100, 100, device)
     threshold = 1.0
