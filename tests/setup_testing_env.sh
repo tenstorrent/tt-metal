d5e030df7675e3bf7470848b15e91ce6645e9c6b
Filip Vranic <fvranic@tenstorrent.com>
2025-05-19 17:39:35 +0200

    build: switch sfpi to version 6.11.1 (#212)
    ### Ticket
    None
    
    ### Problem description
    Using latest release of SFPI (6.11.1)
    New release started using xz compression instead of gunzip when
    distributing release, hence a small change in the way we deal with the
    package.
    
    ### What's changed
    Package Name | Old Version | New Version
    -- | -- | --
    sfpi |
    [6.10.2](https://github.com/tenstorrent/sfpi/releases/tag/v6.10.2) |
    [6.11.1](https://github.com/tenstorrent/sfpi/releases/tag/v6.11.1)
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)

--- tests/setup_testing_env.sh
+++ tests/setup_testing_env.sh
@@ -8,21 +8,21 @@ set -o pipefail  # Fail if any command in a pipeline fails
 
 SFPI_VERSION=$(cat sfpi_version.txt)
 
-SFPI_RELEASE_URL="https://github.com/tenstorrent/sfpi/releases/download/${SFPI_VERSION}/sfpi-x86_64_Linux.tgz"
+SFPI_RELEASE_URL="https://github.com/tenstorrent/sfpi/releases/download/${SFPI_VERSION}/sfpi-x86_64_Linux.txz"
 
 if [ ! -d "sfpi" ]; then
     echo "sfpi directory not found. Downloading and extracting SFPI ${SFPI_VERSION} release..."
-    if ! wget --waitretry=5 --retry-connrefused "$SFPI_RELEASE_URL" -O sfpi-release.tgz; then
+    if ! wget --waitretry=5 --retry-connrefused "$SFPI_RELEASE_URL" -O sfpi-release.txz; then
         echo "Error: Failed to download SFPI release from $SFPI_RELEASE_URL" >&2
         exit 1
     fi
 
-    if ! tar -xzf sfpi-release.tgz; then
+    if ! tar -xJf sfpi-release.txz; then
         echo "Error: Failed to extract SFPI release" >&2
         exit 1
     fi
 
-    rm sfpi-release.tgz
+    rm sfpi-release.txz
 else
     echo "sfpi directory already exists. Skipping download and extraction."
 fi
