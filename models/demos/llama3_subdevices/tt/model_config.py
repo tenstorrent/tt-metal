da3c631db1b3bcd4bd5660449d30a83cfe7a0e1f
Pavle Petrovic <ppetrovic@tenstorrent.com>
2025-05-21 13:47:30 +0200

    Move changes from vLLM test PR

--- models/demos/llama3_subdevices/tt/model_config.py
+++ models/demos/llama3_subdevices/tt/model_config.py
@@ -447,6 +447,8 @@ class TtModelArgs:
             self.CACHE_PATH = os.getenv("TT_CACHE_PATH")
             if not self.CACHE_PATH:
                 self.CACHE_PATH = os.path.join("model_cache", HF_MODEL, self.device_name)
+            else:  # For HF models, always append the device name (e.g. N150/N300/T3K/TG) to the cache path
+                self.CACHE_PATH = os.path.join(self.CACHE_PATH, self.device_name)
             self.model_name = HF_MODEL  # May be overridden by config
             self.from_hf_url = True
         else:
