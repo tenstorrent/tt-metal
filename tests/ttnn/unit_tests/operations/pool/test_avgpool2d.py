014c1e0383c1ac40a0e0ae2bfa3b6664535b8006
Dragica Stoiljkovic <dstoiljkovic@tenstorrent.com>
2025-05-21 09:33:14 +0000

    Removal of unused branches and variables

--- tests/ttnn/unit_tests/operations/pool/test_avgpool2d.py
+++ tests/ttnn/unit_tests/operations/pool/test_avgpool2d.py
@@ -62,8 +62,8 @@ def tensor_map():
     "shard_scheme",
     [
         ttnn.TensorMemoryLayout.HEIGHT_SHARDED,
-        # ttnn.TensorMemoryLayout.WIDTH_SHARDED,
-        # ttnn.TensorMemoryLayout.BLOCK_SHARDED,
+        ttnn.TensorMemoryLayout.WIDTH_SHARDED,
+        ttnn.TensorMemoryLayout.BLOCK_SHARDED,
     ],
 )
 @pytest.mark.parametrize(
