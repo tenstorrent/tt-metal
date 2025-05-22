bd9cf210571a89509bd54c6aa3de381136048831
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2025-05-22 05:29:14 +0000

    #0: Add int32 support for unary max

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_unary_max_min.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_unary_max_min.h
@@ -40,5 +40,33 @@ inline void calculate_unary_max_min(uint value) {
     }
 }
 
+template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
+inline void calculate_unary_max_int32(uint value) {
+   // Load value param to lreg2 --> sign + magnitude
+   _sfpu_load_imm32_(p_sfpu::LREG2, value);
+   TTI_SFPCAST(p_sfpu::LREG2, p_sfpu::LREG3, 2); // InstrModCast::INT32_2S_COMP_TO_INT_SIGN_MAGN;
+   TTI_SFPSETSGN(0, p_sfpu::LREG3, p_sfpu::LREG2, 0);
+
+#pragma GCC unroll 0
+   for (int d = 0; d < ITERATIONS; d++) {
+       // Load input to lreg0
+       TTI_SFPLOAD(p_sfpu::LREG0, 12, ADDR_MOD_3, 0);
+       TTI_SFPCAST(p_sfpu::LREG0, p_sfpu::LREG3, 2); // InstrModCast::INT32_2S_COMP_TO_INT_SIGN_MAGN;
+       TTI_SFPSETSGN(0, p_sfpu::LREG3, p_sfpu::LREG0, 0);
+
+       // Copy value param to lreg1
+       TTI_SFPMOV(0, p_sfpu::LREG2, p_sfpu::LREG1, 0);
+
+       // Swap and store maximum in lreg1 (sign + magnitude format)
+       TTI_SFPSWAP(0, p_sfpu::LREG1, p_sfpu::LREG0, 1);
+
+       TTI_SFPCAST(p_sfpu::LREG1, p_sfpu::LREG3, 3); // InstrModCast::INT_SIGN_MAGN_TO_INT32_2S_COMP;
+       TTI_SFPSETSGN(0, p_sfpu::LREG3, p_sfpu::LREG1, 0);
+       TTI_SFPSTORE(p_sfpu::LREG3, 12, ADDR_MOD_3, 0);
+       
+       dst_reg++;
+   }
+}
+
 }  // namespace sfpu
 }  // namespace ckernel
