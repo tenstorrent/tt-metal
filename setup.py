2bc41e4a6b6d3c204ad90d7a62c0afba4096a0bc
Andrew Fuller <afuller@tenstorrent.com>
2025-05-21 17:23:39 -0700

    Move ttnn/tensor to the new core and api dirs (#22352)
    ### Ticket
    Re-doing #22130 (this time with a fix)
    
    ### Problem description
    #22130 was (rightfully) reverted because some #include's within kernels
    weren't updated to the new path.
    
    ### What's changed
    * Move Tensor to the new core / api home.
    * Teach JIT Build about the API for TT-NN
    * Update kernels that reach into TT-NN for includes to use the new path
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI
    [passes](https://github.com/tenstorrent/tt-metal/actions/runs/15173762751)
    - [x] T3K unit test
    [passes](https://github.com/tenstorrent/tt-metal/actions/runs/15152112361)
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests
    [passes](https://github.com/tenstorrent/tt-metal/actions/runs/15174318045)

--- setup.py
+++ setup.py
@@ -205,8 +205,11 @@ class CMakeBuild(build_ext):
                 "riscv32-unknown-elf-gcc-nm",
                 "riscv32-unknown-elf-gdb-add-index",
             ]
+        ttnn_patterns = [
+            # These weren't supposed to be in the JIT API, but one file currently is
+            "api/ttnn/tensor/enum_types.hpp",
+        ]
         ttnn_cpp_patterns = [
-            "ttnn/tensor/**/*",
             "ttnn/deprecated/**/kernels/**/*",
             "ttnn/operations/**/kernels/**/*",
             "ttnn/operations/ccl/**/*",
@@ -239,6 +242,7 @@ class CMakeBuild(build_ext):
         copy_tree_with_patterns(
             source_dir / "runtime", self.build_lib + "/ttnn/runtime", runtime_patterns, runtime_exclude_files
         )
+        copy_tree_with_patterns(source_dir / "ttnn", self.build_lib + "/ttnn", ttnn_patterns)
         copy_tree_with_patterns(source_dir / "ttnn/cpp", self.build_lib + "/ttnn/cpp", ttnn_cpp_patterns)
         copy_tree_with_patterns(source_dir / "tt_metal", self.build_lib + "/ttnn/tt_metal", tt_metal_patterns)
 
