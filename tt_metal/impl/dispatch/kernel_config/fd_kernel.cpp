de483046035d32b6be1338f2306ec7ef3055445d
Umair Bilal Cheema <129103768+ubcheema@users.noreply.github.com>
2025-05-22 11:16:53 -0700

    Remove pull router from runtime TT-Fabric launch (#22432)
    Now that we have a single tt-fabric router kernel that is launched with
    1D or 2D routing topology, pull mode tt-fabric router is being
    deprecated.
    This PR cleans up the runtime fabric launch code paths to remove support
    for launching legacy 2D fabric routers.
    Remove pull mode tests as well when fabric is launched on device init.
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: Raymond Kim <rkim@tenstorrent.com>

--- tt_metal/impl/dispatch/kernel_config/fd_kernel.cpp
+++ tt_metal/impl/dispatch/kernel_config/fd_kernel.cpp
@@ -142,7 +142,7 @@ void FDKernel::configure_kernel_variant(
     if (rt_options.watcher_dispatch_disabled()) {
         defines["FORCE_WATCHER_OFF"] = "1";
     }
-    if (tt::tt_metal::MetalContext::instance().get_cluster().get_fabric_config() != FabricConfig::FABRIC_2D_PUSH) {
+    if (tt::tt_metal::MetalContext::instance().get_cluster().get_fabric_config() != FabricConfig::FABRIC_2D) {
         defines["FVC_MODE_PULL"] = "1";
     }
     if (!DPrintServerReadsDispatchCores(device_->id())) {
