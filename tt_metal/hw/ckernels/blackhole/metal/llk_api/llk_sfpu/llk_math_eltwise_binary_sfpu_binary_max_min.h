0f5c530e6dab0491948101f37d6721961d6fcf88
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2025-05-22 14:44:05 +0000

    #0: Update

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_binary_sfpu_binary_max_min.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_binary_sfpu_binary_max_min.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -6,7 +6,6 @@
 
 #include "llk_math_eltwise_binary_sfpu_init.h"
 #include "llk_math_eltwise_binary_sfpu_params.h"
-#include "ckernel_sfpu_binary_max_min.h"
 
 namespace ckernel {
 
@@ -22,14 +21,14 @@ template <bool APPROXIMATE>
 inline void llk_math_eltwise_binary_sfpu_binary_max(
     uint dst_index0, uint32_t dst_index1, int vector_mode = VectorMode::RC) {
     llk_math_eltwise_binary_sfpu_params<APPROXIMATE>(
-        ckernel::sfpu::calculate_binary_max_min<true>, dst_index0, dst_index1, vector_mode);
+        ckernel::sfpu::_calculate_binary_max_min_<true>, dst_index0, dst_index1, vector_mode);
 }
 
 template <bool APPROXIMATE>
 inline void llk_math_eltwise_binary_sfpu_binary_max_int32(
     uint dst_index0, uint32_t dst_index1, int vector_mode = VectorMode::RC) {
     llk_math_eltwise_binary_sfpu_params<APPROXIMATE>(
-        ckernel::sfpu::calculate_binary_max_min_int32<true>, dst_index0, dst_index1, vector_mode);
+        ckernel::sfpu::_calculate_binary_max_min_int32_<true>, dst_index0, dst_index1, vector_mode);
 }
 
 // Binary minimum
@@ -42,14 +41,14 @@ template <bool APPROXIMATE>
 inline void llk_math_eltwise_binary_sfpu_binary_min(
     uint dst_index0, uint32_t dst_index1, int vector_mode = VectorMode::RC) {
     llk_math_eltwise_binary_sfpu_params<APPROXIMATE>(
-        ckernel::sfpu::calculate_binary_max_min<false>, dst_index0, dst_index1, vector_mode);
+        ckernel::sfpu::_calculate_binary_max_min_<false>, dst_index0, dst_index1, vector_mode);
 }
 
 template <bool APPROXIMATE>
 inline void llk_math_eltwise_binary_sfpu_binary_min_int32(
     uint dst_index0, uint32_t dst_index1, int vector_mode = VectorMode::RC) {
     llk_math_eltwise_binary_sfpu_params<APPROXIMATE>(
-        ckernel::sfpu::calculate_binary_max_min_int32<false>, dst_index0, dst_index1, vector_mode);
+        ckernel::sfpu::_calculate_binary_max_min_int32_<false>, dst_index0, dst_index1, vector_mode);
 }
 
 }  // namespace ckernel
