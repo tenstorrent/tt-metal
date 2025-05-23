0f674aaf902d4db375be5559e9a4ac97a54a320a
Brian Liu <109366241+TT-BrianLiu@users.noreply.github.com>
2025-05-23 18:02:05 -0400

    #22206: Add generic ShardedAccessor for interacting with ND sharded buffers (#22229)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/22206)
    
    ### Problem description
    ND sharded buffers are now supported in TTNN and tt-metal. There are two
    remaining pieces of work for complete end-to-end functionality:
    - Support in tensors
    - Support in kernels
    
    This PR addresses the latter by introducing a generic `ShardedAccessor`
    for interacting with ND sharded buffers on device. As a proof of
    functionality,
    `tests/ttnn/unit_tests/gtests/accessor/test_sharded_accessor_on_device.cpp`
    implements a single-core reshard program/OP to demonstrate how the
    accessor can be used.
    
    ### What's changed
    - Core implementation:
      - Add compile-time DistributionSpec for kernel usage
    - Add ShardedAccessor for page lookup and reading/writing (similar to
    exisitng address generators)
    - Add get_sharded_accessor_args util for generating sequential kernel
    args
    - Add basic single-core reshard program and kernels to demonstrate usage
    - Add shape getters to BufferDistributionSpec and DistributionSpec
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/15218606019
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable):
    https://github.com/tenstorrent/tt-metal/actions/runs/15197686118
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [x] New/Existing tests provide coverage for changes

---
+++ tests/ttnn/unit_tests/gtests/accessor/kernels/writer_reshard.cpp
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <cstdint>
+#include "accessor/sharded_accessor.h"
+
+void kernel_main() {
+    const uint32_t bank_base_address = get_arg_val<uint32_t>(0);
+
+    // The compile-time args are set up like this to highlight how you can use compile_time_args_skip
+    // Recommended usage is to place the sequential compile-time args for distribution spec at the end
+    constexpr uint32_t rank = get_compile_time_arg_val(0);
+    constexpr uint32_t num_banks = get_compile_time_arg_val(1);
+    constexpr uint32_t base_idx = 2;
+
+    using output_dspec = distribution_spec_t<base_idx, rank, num_banks>;
+    constexpr uint32_t new_base_idx = base_idx + compile_time_args_skip<output_dspec>;
+
+    constexpr uint32_t cb_id = get_compile_time_arg_val(new_base_idx);
+    // TODO: Expose generic interface to get page size for cb operand
+    // - get_tile_size(cb_id) only works for tile layout
+    constexpr uint32_t page_size = get_compile_time_arg_val(new_base_idx + 1);
+
+    auto sharded_accessor = ShardedAccessor<output_dspec, page_size>{.bank_base_address = bank_base_address};
+
+    constexpr uint32_t one_tile = 1;
+    for (size_t i = 0; i < output_dspec::tensor_volume; ++i) {
+        cb_wait_front(cb_id, one_tile);
+        uint32_t l1_read_addr = get_read_ptr(cb_id);
+        sharded_accessor.noc_async_write_page(i, l1_read_addr);
+        noc_async_write_barrier();
+        cb_pop_front(cb_id, one_tile);
+    }
+}
