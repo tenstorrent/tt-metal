27d098ddf2c7bb1986ee9240ab9dc6d6ac08c0f5
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-05-23 14:17:05 +0000

    active & idle erisck

--- tt_metal/hw/firmware/src/active_erisck.cc
+++ tt_metal/hw/firmware/src/active_erisck.cc
@@ -23,10 +23,8 @@
 #include <stdint.h>
 
 void kernel_launch(uint32_t kernel_base_addr) {
-    extern uint32_t __kernel_init_local_l1_base[];
-    extern uint32_t __fw_export_text_end[];
-    do_crt1((uint32_t tt_l1_ptr*)(kernel_base_addr + (uint32_t)__kernel_init_local_l1_base -
-                                  (uint32_t)__fw_export_text_end));
+    extern uint32_t __kernel_data_lma[];
+    do_crt1((uint32_t tt_l1_ptr*)__kernel_data_lma);
 
     noc_local_state_init(NOC_INDEX);
 
