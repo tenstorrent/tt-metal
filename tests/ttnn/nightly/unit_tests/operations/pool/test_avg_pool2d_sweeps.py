a486c94d28bb48f213436a88ad0488a3ee7ecd01
Dragica Stoiljkovic <dstoiljkovic@tenstorrent.com>
2025-05-23 10:17:11 +0000

    Removing None argument from avg pool sweeps

--- tests/ttnn/nightly/unit_tests/operations/pool/test_avg_pool2d_sweeps.py
+++ tests/ttnn/nightly/unit_tests/operations/pool/test_avg_pool2d_sweeps.py
@@ -1,5 +1,4 @@
-# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
-
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 # SPDX-License-Identifier: Apache-2.0
 
 from tests.ttnn.unit_tests.operations.pool.test_avgpool2d import run_avg_pool2d
@@ -19,30 +18,30 @@ def tensor_map():
 parameters = {
     "input_specs": [
         # Contains following parameters
-        # [batch_size, input_channels, input_height, input_width, kernel_height, kernel_width, stride_h, stride_w, pad_h, pad_w, ceil_mode, count_include_pad, divisor_override]
-        [1, 1056, 14, 14, 2, 2, 2, 2, 0, 0, False, True, None],
-        [1, 128, 56, 56, 2, 2, 2, 2, 0, 0, False, True, None],
-        [1, 160, 7, 7, 2, 2, 2, 2, 0, 0, False, True, None],
-        [1, 192, 56, 56, 2, 2, 2, 2, 0, 0, False, True, None],
-        [1, 256, 28, 28, 2, 2, 2, 2, 0, 0, False, True, None],
-        [1, 384, 28, 28, 2, 2, 2, 2, 0, 0, False, True, None],
-        [1, 512, 14, 14, 2, 2, 2, 2, 0, 0, False, True, None],
-        [1, 640, 14, 14, 2, 2, 2, 2, 0, 0, False, True, None],
-        [1, 896, 14, 14, 2, 2, 2, 2, 0, 0, False, True, None],
-        [1, 1024, 17, 17, 3, 3, 1, 1, 1, 1, False, False, None],
-        [1, 112, 14, 14, 2, 2, 2, 2, 0, 0, False, True, None],
-        [1, 1536, 8, 8, 3, 3, 1, 1, 1, 1, False, False, None],
-        [1, 24, 56, 56, 2, 2, 2, 2, 0, 0, False, True, None],
-        [1, 384, 35, 35, 3, 3, 1, 1, 1, 1, False, False, None],
-        [1, 40, 28, 28, 2, 2, 2, 2, 0, 0, False, True, None],
-        [1, 80, 14, 14, 2, 2, 2, 2, 0, 0, False, True, None],
+        # [batch_size, input_channels, input_height, input_width, kernel_height, kernel_width, stride_h, stride_w, pad_h, pad_w, ceil_mode, count_include_pad]
+        [1, 1056, 14, 14, 2, 2, 2, 2, 0, 0, False, True],
+        [1, 128, 56, 56, 2, 2, 2, 2, 0, 0, False, True],
+        [1, 160, 7, 7, 2, 2, 2, 2, 0, 0, False, True],
+        [1, 192, 56, 56, 2, 2, 2, 2, 0, 0, False, True],
+        [1, 256, 28, 28, 2, 2, 2, 2, 0, 0, False, True],
+        [1, 384, 28, 28, 2, 2, 2, 2, 0, 0, False, True],
+        [1, 512, 14, 14, 2, 2, 2, 2, 0, 0, False, True],
+        [1, 640, 14, 14, 2, 2, 2, 2, 0, 0, False, True],
+        [1, 896, 14, 14, 2, 2, 2, 2, 0, 0, False, True],
+        [1, 1024, 17, 17, 3, 3, 1, 1, 1, 1, False, False],
+        [1, 112, 14, 14, 2, 2, 2, 2, 0, 0, False, True],
+        [1, 1536, 8, 8, 3, 3, 1, 1, 1, 1, False, False],
+        [1, 24, 56, 56, 2, 2, 2, 2, 0, 0, False, True],
+        [1, 384, 35, 35, 3, 3, 1, 1, 1, 1, False, False],
+        [1, 40, 28, 28, 2, 2, 2, 2, 0, 0, False, True],
+        [1, 80, 14, 14, 2, 2, 2, 2, 0, 0, False, True],
     ],
     "failing_parameters": [
         # [batch_size, input_channels, input_height, input_width, kernel_height, kernel_width, stride_h, stride_w, pad_h, pad_w, ceil_mode, count_include_pad]
-        [1, 112, 14, 14, 2, 2, 2, 2, 0, 0, False, True, None],  # 11
-        [1, 24, 56, 56, 2, 2, 2, 2, 0, 0, False, True, None],  # 13
-        [1, 40, 28, 28, 2, 2, 2, 2, 0, 0, False, True, None],  # 15
-        [1, 80, 14, 14, 2, 2, 2, 2, 0, 0, False, True, None],  # 16
+        [1, 112, 14, 14, 2, 2, 2, 2, 0, 0, False, True],  # 11
+        [1, 24, 56, 56, 2, 2, 2, 2, 0, 0, False, True],  # 13
+        [1, 40, 28, 28, 2, 2, 2, 2, 0, 0, False, True],  # 15
+        [1, 80, 14, 14, 2, 2, 2, 2, 0, 0, False, True],  # 16
     ],
 }
 
@@ -64,7 +63,6 @@ def test_ttnn_pytorch_sweep(device, tensor_map, input_spec):
         pad_w,
         ceil_mode,
         count_include_pad,
-        divisor_override,
     ) = input_spec
 
     # Check if input_spec is in failing_parameters
@@ -80,6 +78,6 @@ def test_ttnn_pytorch_sweep(device, tensor_map, input_spec):
         padding=(pad_h, pad_w),
         ceil_mode=ceil_mode,
         count_include_pad=count_include_pad,
-        divisor_override=divisor_override,
+        divisor_override=None,
         shard_scheme=ttnn.TensorMemoryLayout.HEIGHT_SHARDED,
     )
