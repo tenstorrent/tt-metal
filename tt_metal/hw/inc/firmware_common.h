025e7a4a7021b5cc9462c36a84c08a7cc76435a0
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-05-23 13:48:51 +0000

    fix trisc

--- tt_metal/hw/inc/firmware_common.h
+++ tt_metal/hw/inc/firmware_common.h
@@ -26,7 +26,6 @@ extern uint16_t l1_bank_to_noc_xy[NUM_NOCS][NUM_L1_BANKS];
 extern int32_t bank_to_l1_offset[NUM_L1_BANKS];
 
 extern void kernel_init(uint32_t kernel_init);
-extern void kernel_launch(uint32_t kernel_base_addr);
 void l1_to_local_mem_copy(uint32_t* dst, uint32_t tt_l1_ptr* src, int32_t len);
 
 inline void do_crt1(uint32_t tt_l1_ptr* data_image) {
