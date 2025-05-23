dbaf6ff32ab891784099f1663289d4a21b4dce3a
grechsteinerTT <grechsteiner@tenstorrent.com>
2025-05-23 13:41:06 +0000

    Remove unneeded comments

--- ttnn/cpp/ttnn/operations/data_movement/untilize/device/kernels/dataflow/reader_unary_interleaved_start_id.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/untilize/device/kernels/dataflow/reader_unary_interleaved_start_id.cpp
@@ -49,8 +49,6 @@ void kernel_main() {
         cb_reserve_back(cb_id_in0, onetile);
         uint32_t l1_write_addr = get_write_ptr(cb_id_in0);
 
-        // noc_async_read_tile not yet supported for ShardedAddrGen
-        // noc_async_read_page not yet supported for InterleavedAddrGenFast
 #ifdef SHARDED
         noc_async_read_page(i, s, l1_write_addr);
 #else
