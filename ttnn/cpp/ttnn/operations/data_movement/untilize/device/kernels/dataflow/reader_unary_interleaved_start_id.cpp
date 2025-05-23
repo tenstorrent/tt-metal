6bdb9cf962005ef3a0189b755518939c7dbf3851
grechsteinerTT <grechsteiner@tenstorrent.com>
2025-05-23 00:16:14 +0000

    Remove uneeded includes

--- ttnn/cpp/ttnn/operations/data_movement/untilize/device/kernels/dataflow/reader_unary_interleaved_start_id.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/untilize/device/kernels/dataflow/reader_unary_interleaved_start_id.cpp
@@ -4,7 +4,6 @@
 
 #include <stdint.h>
 #include "dataflow_api.h"
-#include "cpp/ttnn/operations/ccl/shared_with_host/sharded_tensor_addr_gen.hpp"
 #include "ttnn/cpp/ttnn/operations/ccl/kernel_common/sharding_addrgen.hpp"
 
 void kernel_main() {
