e394fc19e3b01cef3e5b45e4e007efa9f88d26d0
Pavle Josipovic <pjosipovic@tenstorrent.com>
2025-05-24 21:16:12 +0000

    Revert "#0: remove path reserve arg from all mcast api (#22523)"
    This reverts commit a1a43b38a0d20b58eeae5c3b809ee0bb79fe4d5d.

--- ttnn/cpp/ttnn/operations/conv/conv2d/device/kernels/activation_reader_width_sharded.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/device/kernels/activation_reader_width_sharded.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: © 2024 Tenstorrent AI ULC
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -212,6 +212,7 @@ void kernel_main() {
                         act_multicast_data_addr,
                         act_mcast_sender_size_bytes,
                         num_reader_cores,
+                        false,
                         false);
 
                     // Note: no need for write barrier, since these two multicasts are done on the same noc id and same
@@ -228,6 +229,7 @@ void kernel_main() {
                         act_mcast_sender_semaphore_valid_addr,
                         act_mcast_receiver_semaphore_noc_addr,
                         num_reader_cores,
+                        false,
                         false);
 
                     noc_semaphore_wait(act_mcast_receiver_semaphore_addr_ptr, VALID);
