8c7359ecd2d622f47564eef415af4fea4d6b8f4e
Ben Parks <bnprks+git@gmail.com>
2025-05-25 23:19:00 -0700

    #9261: Tidy ckernel includes in llk rounding_ops and rand

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_rand.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_rand.h
@@ -4,7 +4,6 @@
 
 #pragma once
 
-#include "ckernel_instr_params.h"
 #include "ckernel_sfpu_rand.h"
 #include "llk_math_eltwise_unary_sfpu_init.h"
 #include "llk_math_eltwise_unary_sfpu_params.h"
--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_rounding_ops.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_rounding_ops.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include "ckernel_sfpu_rounding_ops.h"
 #include "llk_math_eltwise_unary_sfpu_init.h"
 #include "llk_math_eltwise_unary_sfpu_params.h"
 
