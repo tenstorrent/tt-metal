e2e6334fab24527eaf0aa81eeb1f7bf77306058c
marty1885 <marty188586@gmail.com>
2025-05-27 18:24:05 +0800

    clean up mm example

--- tt_metal/programming_examples/matmul_common/kernels/dataflow/reader_bmm_8bank.cpp
+++ tt_metal/programming_examples/matmul_common/kernels/dataflow/reader_bmm_8bank.cpp
@@ -14,71 +14,45 @@ void kernel_main() {
     uint32_t Mt = get_arg_val<uint32_t>(2);
     uint32_t Kt = get_arg_val<uint32_t>(3);
     uint32_t Nt = get_arg_val<uint32_t>(4);
-    uint32_t MtKt = get_arg_val<uint32_t>(5);  // if 0
-    uint32_t KtNt = get_arg_val<uint32_t>(6);
-    uint32_t batch = get_arg_val<uint32_t>(7);
-    uint32_t bcast_B = get_arg_val<uint32_t>(8);  // if 1 we broadcast B to batch
-
-    constexpr bool src0_is_dram = get_compile_time_arg_val(0) == 1;
-    constexpr bool src1_is_dram = get_compile_time_arg_val(1) == 1;
-
-    // DPRINT << "Mt=" << Mt << " Kt=" << Kt << " Nt=" << Nt << " MtKt=" << MtKt << "KtNt=" << KtNt << ENDL();
-    // DPRINT << "src0=" << src0_addr << " src1=" << src1_addr << ENDL();
-    // DPRINT << "batch=" << batch << ENDL();
 
     constexpr uint32_t cb_id_in0 = 0;
     constexpr uint32_t cb_id_in1 = 1;
 
-    constexpr uint32_t onetile = 1;
-    const uint32_t src0_tile_bytes = get_tile_size(cb_id_in0);
-    const DataFormat src0_data_format = get_dataformat(cb_id_in0);
-    const uint32_t src1_tile_bytes = get_tile_size(cb_id_in1);
-    const DataFormat src1_data_format = get_dataformat(cb_id_in1);
+    // Declare address in which we stored the source matrices. We have set the exact same format between CBs and DRAM
+    // buffers in the host code, so we can use the same address for both DRAM and CBs.
+    const InterleavedAddrGenFast<true> s0 = {
+        .bank_base_address = src0_addr,
+        .page_size = get_tile_size(cb_id_in0),
+        .data_format = get_dataformat(cb_id_in0)};
+    const InterleavedAddrGenFast<true> s1 = {
+        .bank_base_address = src1_addr,
+        .page_size = get_tile_size(cb_id_in1),
+        .data_format = get_dataformat(cb_id_in1)};
 
-    uint32_t itileA_batch = 0;
-    uint32_t itileB_batch = 0;
+    // Loop through the dimensions of the matrices. Read them and push to the circular buffers.
+    // Dimension names are called M, N and K. `t` in `mt` means tile.
+    for (uint32_t mt = 0; mt < Mt; mt++) {
+        uint32_t itileB = 0;
+        for (uint32_t nt = 0; nt < Nt; nt++) {
+            for (uint32_t kt = 0; kt < Kt; kt++) {
+                {                                          // Read A's tile at (mt, kt)
+                    uint32_t a_tile_index = mt * Kt + kt;  // A is MK, so we stride by Kt
+                    cb_reserve_back(cb_id_in0, 1);
+                    uint32_t l1_write_addr_in0 = get_write_ptr(cb_id_in0);
+                    noc_async_read_tile(a_tile_index, s0, l1_write_addr_in0);
+                    noc_async_read_barrier();
+                    cb_push_back(cb_id_in0, 1);
+                }
 
-    const InterleavedAddrGenFast<src0_is_dram> s0 = {
-        .bank_base_address = src0_addr, .page_size = src0_tile_bytes, .data_format = src0_data_format};
-
-    const InterleavedAddrGenFast<src1_is_dram> s1 = {
-        .bank_base_address = src1_addr, .page_size = src1_tile_bytes, .data_format = src1_data_format};
-
-    for (uint32_t nb = 0; nb < batch; nb++) {
-        uint32_t itileA = itileA_batch;
-        for (uint32_t mt = 0; mt < Mt; mt++) {
-            uint32_t itileB = itileB_batch;
-            for (uint32_t nt = 0; nt < Nt; nt++) {
-                for (uint32_t kt = 0; kt < Kt; kt++) {
-                    {  // Read A's tile at (mt, kt)
-                        cb_reserve_back(cb_id_in0, onetile);
-                        uint32_t l1_write_addr_in0 = get_write_ptr(cb_id_in0);
-                        noc_async_read_tile(itileA, s0, l1_write_addr_in0);
-                        noc_async_read_barrier();
-                        cb_push_back(cb_id_in0, onetile);
-                    }
-
-                    {  // Read B's tile at (kt, nt)
-                        cb_reserve_back(cb_id_in1, onetile);
-                        uint32_t l1_write_addr_in1 = get_write_ptr(cb_id_in1);
-                        noc_async_read_tile(itileB, s1, l1_write_addr_in1);
-                        noc_async_read_barrier();
-                        cb_push_back(cb_id_in1, onetile);
-                    }
-                    // DPRINT << "Pushed itileA=" << itileA << " itileB=" << itileB << ENDL();
-
-                    itileA += 1;   // A is MK
-                    itileB += Nt;  // B is KN, so to get k++ we stride by Nt
-                }  // Kt loop
-                itileB -= KtNt;  // revert B to previous state before the K loop (to avoid multiplies)
-                itileB += 1;     // B is KN, so here in the end of Nt loop we increment N by 1
-                itileA -= Kt;    // resets tileA to kt=0, keep the same mt
-            }  // Nt loop
-            itileA += Kt;  // A is MK, advance to next M
-        }  // Mt loop
-        itileA_batch += MtKt;  // update batch strides
-        if (bcast_B == 0) {    // don't increment batch if we broadcast matrix B
-            itileB_batch += KtNt;
-        }
-    }  // batch loop
+                {                                          // Read B's tile at (kt, nt)
+                    uint32_t b_tile_index = kt * Nt + nt;  // B is KN, so we stride by Nt
+                    cb_reserve_back(cb_id_in1, 1);
+                    uint32_t l1_write_addr_in1 = get_write_ptr(cb_id_in1);
+                    noc_async_read_tile(b_tile_index, s1, l1_write_addr_in1);
+                    noc_async_read_barrier();
+                    cb_push_back(cb_id_in1, 1);
+                }
+            }  // Kt loop
+        }  // Nt loop
+    }  // Mt loop
 }
--- tt_metal/programming_examples/matmul_common/kernels/dataflow/writer_bmm_8bank.cpp
+++ tt_metal/programming_examples/matmul_common/kernels/dataflow/writer_bmm_8bank.cpp
@@ -4,45 +4,37 @@
 
 #include "dataflow_api.h"
 
-// #include "debug/dprint.h"
-
 void kernel_main() {
-    // same arg indices as in reader_bmm_8bank for reuse
+    // Runtime arguments to write data back into the output buffer.
     uint32_t dst_addr = get_arg_val<uint32_t>(0);
-    uint32_t Mt = get_arg_val<uint32_t>(2);
-    uint32_t Nt = get_arg_val<uint32_t>(4);
-    uint32_t batch = get_arg_val<uint32_t>(7);
+    uint32_t Mt = get_arg_val<uint32_t>(1);
+    uint32_t Nt = get_arg_val<uint32_t>(2);
 
-    constexpr bool dst_is_dram = get_compile_time_arg_val(0) == 1;
-
-    constexpr int onetile = 1;
     constexpr uint32_t cb_id_out0 = 16;
-    const uint32_t tile_bytes = get_tile_size(cb_id_out0);
-    uint32_t itileC = 0;
-    const DataFormat data_format = get_dataformat(cb_id_out0);
 
-    const InterleavedAddrGenFast<dst_is_dram> s = {
-        .bank_base_address = dst_addr, .page_size = tile_bytes, .data_format = data_format};
+    // Create the address generator for the output buffer. Due to us sharing buffer and circular buffer
+    // configuration parameters (e.g. same data type and same page size) in the host code, we can grab
+    // the same parameters from the circular buffer as we would from the DRAM buffer.
+    const InterleavedAddrGenFast<true> s = {
+        .bank_base_address = dst_addr,
+        .page_size = get_tile_size(cb_id_out0),
+        .data_format = get_dataformat(cb_id_out0)};
 
-    // C is MN so we iterate in tile RM order
-    for (uint32_t nb = 0; nb < batch; nb++) {
-        for (uint32_t mt_C = 0; mt_C < Mt; ++mt_C) {      // output tile of C
-            for (uint32_t nt_C = 0; nt_C < Nt; ++nt_C) {  // output tile index of C
-                // bmm will generate C's tiles C=A*B, MN=MK*KN, in row major order, we just read them from CB and write
-                // out to DRAM
-                cb_wait_front(cb_id_out0, onetile);
-                uint32_t l1_read_addr = get_read_ptr(cb_id_out0);
-                noc_async_write_tile(itileC, s, l1_read_addr);
-                noc_async_write_barrier();  // This will wait until the write is done. As an alternative,
-                                            // noc_async_write_flushed() can be faster because it waits
-                                            // until the write request is sent. In that case, you have to
-                                            // use noc_async_write_barrier() at least once at the end of
-                                            // data movement kernel to make sure all writes are done.
-                cb_pop_front(cb_id_out0, onetile);
-                // DPRINT << 'W' << 'C' << itileC << ' ' << 'a' << dst_addr << ENDL();
-                // DPRINT << itileC << ' ' << uint32_t(dst_noc_addr) << ENDL();
-                itileC++;
-            }
+    // Loop throught the matrix dimensions Mt and Nt. bmm will generate C's tiles C=A*B, MN=MK*KN,
+    // in row major order, we just read them from CB and write out to DRAM
+    for (uint32_t m = 0; m < Mt; ++m) {
+        for (uint32_t n = 0; n < Nt; ++n) {
+            // Wait for the matrix multiplication kernel to produce an output
+            cb_wait_front(cb_id_out0, 1);
+            // Write the output tile to DRAM.
+            uint32_t l1_read_addr = get_read_ptr(cb_id_out0);
+            noc_async_write_tile(m * Nt + n, s, l1_read_addr);
+            noc_async_write_barrier();  // This will wait until the write is done. As an alternative,
+                                        // noc_async_write_flushed() can be faster because it waits
+                                        // until the write request is sent. In that case, you have to
+                                        // use noc_async_write_barrier() at least once at the end of
+                                        // data movement kernel to make sure all writes are done.
+            cb_pop_front(cb_id_out0, 1);
         }
     }
 }
