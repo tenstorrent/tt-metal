1e957bf849f6fc0d69a745c5b1fe5f042de15c6b
Sofija Jovic <148721049+s-jovic@users.noreply.github.com>
2025-05-27 08:55:44 +0200

    #21737: Add resnet test to BH upstream pipelines (#22041)
    ### Ticket
    #21737
    
    ### What's changed
    Added BH tests to upstream pipelines.
    
    ### Checklist
    Upstream pipeline:
    https://github.com/tenstorrent/tt-metal/actions/runs/15187413179 - bh
    jobs passing
    Frequent ttnn and model:
    https://github.com/tenstorrent/tt-metal/actions/runs/15213549260

---
+++ models/demos/blackhole/resnet50/tests/upstream_pipeline/test_perf_e2e_resnet50_batch32.py
@@ -0,0 +1,39 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+
+from models.demos.blackhole.resnet50.tests.resnet_test_utils import skip_resnet_if_blackhole_p100
+from models.demos.ttnn_resnet.tests.perf_e2e_resnet50 import run_perf_resnet
+from models.utility_functions import run_for_blackhole
+
+
+@run_for_blackhole()
+@pytest.mark.models_performance_bare_metal
+@pytest.mark.parametrize(
+    "device_params", [{"l1_small_size": 32768, "num_command_queues": 2, "trace_region_size": 2777088}], indirect=True
+)
+@pytest.mark.parametrize(
+    "batch_size, expected_inference_time, expected_compile_time",
+    ((32, 0.003, 30),),
+)
+def test_perf_trace_2cqs(
+    device,
+    use_program_cache,
+    batch_size,
+    expected_inference_time,
+    expected_compile_time,
+    hf_cat_image_sample_input,
+    model_location_generator,
+):
+    skip_resnet_if_blackhole_p100(device)
+    run_perf_resnet(
+        batch_size,
+        expected_inference_time,
+        expected_compile_time,
+        hf_cat_image_sample_input,
+        device,
+        "resnet50_trace_2cqs",
+        model_location_generator,
+    )
