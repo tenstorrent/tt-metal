eee44b8326f758af4b4d6f7f73cd42d3d4503ba7
Stanislav Minakov <sminakov@tenstorrent.com>
2025-05-27 13:20:44 -0700

    Added a comment

--- ttnn/api/ttnn/tensor/tensor.hpp
+++ ttnn/api/ttnn/tensor/tensor.hpp
@@ -209,6 +209,8 @@ public:
     uint64_t padded_volume() const;
     const DistributedTensorConfig& distributed_tensor_config() const;
     const MemoryConfig& memory_config() const;
+
+    // For sharded tensors, at least one of ShardSpec or NdShardSpec will be provided.
     const std::optional<ShardSpec>& shard_spec() const;
     const std::optional<NdShardSpec>& nd_shard_spec() const;
 
