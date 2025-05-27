bd11d6e81e4b4fbbc30bb799cdfc21b712d130de
Evan Smal <159198142+esmalTT@users.noreply.github.com>
2025-05-27 10:08:37 -0400

    Add `ttnn.convert_to_hwc` fused op for converting input tensor to channels last (#22152)
    ### Summary
    
    This change adds a new operation which can reorder a CNN input tensor
    from channels-first to channels-last ordering.
    
    It is intended to work seamlessly with the sliding window ops, plus
    common CNN 2CQ+trace configurations which typically involve resharding
    inputs from DRAM to L1.
    
    This operation works with both DRAM width-sharded and L1 width-sharded
    inputs and produces L1 height-sharded outputs.
    
    On Shallow UNet-like workloads, this op provides a 2X speedup over the
    existing solution. This PR does not currently integrate this op with
    UNet because it is blocked by
    [#20726](https://github.com/tenstorrent/tt-metal/issues/20726).
    
    Some notes about the changes:
    - Because this op implements a fused reshard, I moved the logic which
    computes all necessary copies from DRAM from in the reshard op to a
    common location
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/15048077065/job/42296462655
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable):
    https://github.com/tenstorrent/tt-metal/actions/runs/15048078889
    - [x] Blackhole nightly:
    https://github.com/tenstorrent/tt-metal/actions/runs/15139936145/job/42561811888
    
    ---------
    
    Co-authored-by: Raymond Kim <109366641+tt-rkim@users.noreply.github.com>
    Co-authored-by: Artem Yerofieiev <169092593+ayerofieiev-tt@users.noreply.github.com>

--- ttnn/cpp/ttnn/operations/data_movement/sharded/sharded_common.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/sharded/sharded_common.cpp
@@ -3,7 +3,11 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include <tt-metalium/constants.hpp>
+#include <tt-metalium/allocator.hpp>
+
 #include "ttnn/tensor/tensor.hpp"
+
+#include "sharded_common.hpp"
 
 namespace ttnn::operations::data_movement::detail {
 
@@ -20,6 +24,88 @@ uint32_t calculate_starting_idx_h(const Tensor& tensor, uint32_t num_slices, uin
     uint32_t num_tiles_per_slice = total_num_tiles / num_slices;
     uint32_t starting_tile_in_slice = num_tiles_per_slice * slice_index;
     return starting_tile_in_slice;
+}
+
+std::tuple<std::vector<std::vector<WidthShardingReshardSegment>>, uint32_t, uint32_t, uint32_t>
+compute_width_sharding_reshard_segments(
+    const std::array<uint32_t, 2>& local_shard_shape,
+    const std::array<uint32_t, 2>& remote_shard_shape,
+    const std::vector<CoreCoord>& local_cores,
+    const std::vector<CoreCoord>& remote_cores,
+    const tt::tt_metal::BufferType& remote_buffer_type,
+    const CoreType& remote_core_type,
+    tt::tt_metal::IDevice* device,
+    uint32_t element_size) {
+    const uint32_t num_local_shards = local_cores.size();
+    const uint32_t num_remote_shards = remote_cores.size();
+
+    const uint32_t local_shard_height = local_shard_shape[0];
+    const uint32_t local_shard_width = local_shard_shape[1];
+    const uint32_t remote_shard_height = remote_shard_shape[0];
+    const uint32_t remote_shard_width = remote_shard_shape[1];
+
+    using WidthShardingReshardSegmentForSingleCore = std::vector<WidthShardingReshardSegment>;
+
+    TT_FATAL(
+        local_shard_height == remote_shard_height,
+        "Unexpected mismatch in shard heights ({} != {}",
+        local_shard_height,
+        remote_shard_height);
+
+    const uint32_t total_num_sticks = local_shard_height;
+    const uint32_t local_stride_bytes = element_size * local_shard_width;
+    const uint32_t remote_stride_bytes = element_size * remote_shard_width;
+
+    std::vector<WidthShardingReshardSegmentForSingleCore> runtime_args_for_each_core;
+
+    bool is_final_transfer = false;
+    uint32_t local_shard_offset = 0;
+    uint32_t remote_shard_offset = 0;
+    uint32_t current_remote_core_idx = 0;
+    for (uint32_t current_local_core_idx = 0; current_local_core_idx < local_cores.size(); current_local_core_idx++) {
+        const auto& core = local_cores[current_local_core_idx];
+        WidthShardingReshardSegmentForSingleCore core_args;
+        while (local_shard_offset < local_shard_width) {
+            const uint32_t remaining_input = local_shard_width - local_shard_offset;
+            const uint32_t remaining_output = remote_shard_width - remote_shard_offset;
+
+            // The last core might have some garbage in it because of uneven shards
+            is_final_transfer = (current_local_core_idx >= local_cores.size() - 1) &&
+                                (current_remote_core_idx >= remote_cores.size() - 1);
+            const uint32_t transfer_size =
+                is_final_transfer ? remaining_output : std::min(remaining_input, remaining_output);
+
+            const auto bank_id = device->allocator()->get_bank_ids_from_logical_core(
+                remote_buffer_type, remote_cores[current_remote_core_idx])[0];
+            core_args.emplace_back(
+                element_size * transfer_size,
+                element_size * local_shard_offset,
+                bank_id,
+                element_size * remote_shard_offset);
+
+            local_shard_offset += transfer_size;
+            remote_shard_offset += transfer_size;
+
+            // If the current output shard is full, move to the next one
+            if (remote_shard_offset == remote_shard_width) {
+                ++current_remote_core_idx;
+                remote_shard_offset = 0;
+            }
+            if (is_final_transfer) {
+                break;
+            }
+        }
+        local_shard_offset = 0;
+        runtime_args_for_each_core.push_back(core_args);
+    }
+
+    TT_FATAL(
+        runtime_args_for_each_core.size() == num_local_shards,
+        "Expect to have one set of runtime args per local core (expected {} but was {})",
+        num_local_shards,
+        runtime_args_for_each_core.size());  // sanity check
+
+    return {runtime_args_for_each_core, total_num_sticks, local_stride_bytes, remote_stride_bytes};
 }
 
 }  // namespace ttnn::operations::data_movement::detail
