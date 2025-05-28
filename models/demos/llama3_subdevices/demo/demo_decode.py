8db4c19bb4a186a64e7dd6c4eade8b6f20a5c5f4
Djordje Ivanovic <133343976+djordje-tt@users.noreply.github.com>
2025-05-28 14:55:54 +0200

    Llama70b TG - Update 4U tsu range (#22670)
    ### Problem description
    After LMHead improvement tsu for 1L got improved and started to go above
    expected range and assert.
    ### What's changed
    TSU range is changed (510, 540) -> (520, 545)
    
    ### Checklist
    - [ ] [Referenced Quick
    run](https://github.com/tenstorrent/tt-metal/actions/runs/15297325901/job/43030211422#step:5:733)

--- models/demos/llama3_subdevices/demo/demo_decode.py
+++ models/demos/llama3_subdevices/demo/demo_decode.py
@@ -33,7 +33,7 @@ TSU_PERF_DROP_LIMIT_COUNT = 20
 
 # Constants for TSU thresholds based on the number of layers
 TSU_THRESHOLDS = {
-    "4U": {1: {"min": 510, "max": 540}, 10: {"min": 230, "max": 250}, 80: {"min": 48.5, "max": 53}},
+    "4U": {1: {"min": 520, "max": 545}, 10: {"min": 230, "max": 250}, 80: {"min": 48.5, "max": 53}},
     # TODO: Update thresholds for 6U 10L and 80L based on actual perf when 6U are available and added into CI
     "6U": {1: {"min": 545, "max": 570}, 10: {"min": 230, "max": 250}, 80: {"min": 49, "max": 53}},
 }
