b1ce872d9627f60bb16d7ab6a6a97a0e2f1304ed
Juan Camilo Vega <jvega@tenstorrent.com>
2025-05-28 16:03:42 +0000

    Segregated program processing in matmul

---
+++ ttnn/cpp/ttnn/operations/experimental/ccl/reduce_scatter_matmul/device/rs_matmul_program_factory.cpp
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "ttnn/operations/experimental/ccl/reduce_scatter_matmul/device/rs_matmul_op.hpp"
