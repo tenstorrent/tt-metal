53b232d2a6407942436f3a8a0dbc3f5a789e9e99
Ammar Vora <avora@tenstorrent.com>
2025-05-29 14:12:09 -0400

    Revert "Embedding perf test (#22681)" (#22748)
    This reverts commit 3e62a92f1153cecb98ea1dbbb8b72b0befc10496.
    
    ### Problem description
    - Adds a module test instead of a minimal single device test, testing
    the shapes used in Llama
    - As a result, there is dependency on weight caches

--- models/demos/llama3_subdevices/tests/tg_perf_unit_tests/test_llama_ops_perf_TG_llama.py
+++ models/demos/llama3_subdevices/tests/tg_perf_unit_tests/test_llama_ops_perf_TG_llama.py
@@ -18,7 +18,6 @@ from models.perf.benchmarking_utils import BenchmarkData, BenchmarkProfiler
         ("NLPConcatHeadsDecodeDeviceOperation", 6.07, 0.05),
         ("PagedUpdateCacheDeviceOperation", 4.5, 0.1),
         ("RotaryEmbeddingLlamaFusedQK", 4.15, 0.05),
-        ("Embeddings", 255.0, 0.05),
     ],
 )
 def test_llama_tg_ops_perf_device(op_name, expected_kernel_duration_us, perf_margin):
