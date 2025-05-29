cc77ac9758644d2c6a24463fe706a18f5bf25256
Ambrose Ling <aling@tenstorrent.com>
2025-05-29 20:30:59 +0000

    Added BinaryMultSilu Op Perf Test for Llama TG

--- tests/tt_eager/python_api_testing/unit_testing/misc/test_eltwise_binary.py
+++ tests/tt_eager/python_api_testing/unit_testing/misc/test_eltwise_binary.py
@@ -125,6 +125,45 @@ def test_run_elt_binary_add_with_sub_devices(device):
     assert passing
 
 
+def run_elt_binary_mul_with_sub_devices(
+    batch, num_heads, seq_len, dim, dtype, in_mem_config, out_mem_config, device, mesh_mapper, mesh_composer, pcc
+):
+    input_shape = [batch, num_heads, seq_len, dim]
+    in0 = torch.randn(input_shape)
+    in1 = torch.randn(input_shape)
+    in0_ref = in0.clone()
+    in1_ref = in1.clone()
+    in0_t = ttnn.from_torch(
+        in0,
+        device=device,
+        mesh_mapper=mesh_mapper,
+        dtype=dtype,
+        memory_config=in_mem_config,
+        layout=ttnn.TILE_LAYOUT,
+    )
+    in1_t = ttnn.from_torch(
+        in1,
+        device=device,
+        mesh_mapper=mesh_mapper,
+        dtype=dtype,
+        memory_config=in_mem_config,
+        layout=ttnn.TILE_LAYOUT,
+    )
+    out_t = ttnn.mul(
+        in0_t,
+        in1_t,
+        input_tensor_a_activations=[ttnn.UnaryOpType.SILU],
+        dtype=ttnn.bfloat8_b,
+        memory_config=out_mem_config,
+    )
+    tt_out = ttnn.to_torch(out_t, mesh_composer=mesh_composer)
+    tt_out = tt_out[:, :1, :, :dim]
+    torch_silu = torch.nn.SiLU()
+    passing, output = comp_pcc(tt_out, torch.mul(torch_silu(in0_ref), in1_ref), pcc)
+    logger.info(output)
+    assert passing
+
+
 @run_for_wormhole_b0()
 @pytest.mark.parametrize("device_params", [{"dispatch_core_axis": ttnn.DispatchCoreAxis.COL}], indirect=True)
 def test_run_elt_binary_mul_with_sub_devices(device):
@@ -133,9 +172,12 @@ def test_run_elt_binary_mul_with_sub_devices(device):
     if unharvested_grid_size[0] > compute_grid_size.x or unharvested_grid_size[1] > compute_grid_size.y:
         pytest.skip(f"Need {unharvested_grid_size} grid size to run this test but core grid is {compute_grid_size}")
 
+    pcc = 0.999
     shape = (1, 1, 32, 896)
+    dtype = ttnn.bfloat8_b
+    mesh_mapper = None
+    mesh_composer = None
     torch.manual_seed(10)
-
     start_core = ttnn.CoreCoord(1, 0)
     core_grid = ttnn.CoreRangeSet(
         [
@@ -145,40 +187,12 @@ def test_run_elt_binary_mul_with_sub_devices(device):
     )
     shard_grid = ttnn.num_cores_to_corerangeset_in_subcoregrids(start_core, 28, core_grid, row_wise=True)
     shard_spec = ttnn.ShardSpec(shard_grid, (32, 32), ttnn.ShardOrientation.ROW_MAJOR)
-    output_mem_config = ttnn.MemoryConfig(
+    in_mem_config = ttnn.MemoryConfig(
         ttnn.TensorMemoryLayout.WIDTH_SHARDED,
         ttnn.BufferType.L1,
         shard_spec,
     )
-    in0 = torch.randn(shape).bfloat16().float()
-    in1 = torch.randn(shape).bfloat16().float()
-    in0_t = ttnn.from_torch(
-        in0,
-        device=device,
-        dtype=ttnn.bfloat8_b,
-        layout=ttnn.TILE_LAYOUT,
-        memory_config=output_mem_config,
+    out_mem_config = in_mem_config
+    run_elt_binary_mul_with_sub_devices(
+        *shape, dtype, in_mem_config, out_mem_config, device, mesh_mapper, mesh_composer, pcc
     )
-    in0 = ttnn.to_torch(in0_t)
-    in1_t = ttnn.from_torch(
-        in0,
-        device=device,
-        dtype=ttnn.bfloat8_b,
-        layout=ttnn.TILE_LAYOUT,
-        memory_config=output_mem_config,
-    )
-    in1 = ttnn.to_torch(in1_t)
-
-    out_t = ttnn.mul(
-        in0_t,
-        in1_t,
-        input_tensor_a_activations=[ttnn.UnaryOpType.SILU],
-        dtype=ttnn.bfloat8_b,
-        memory_config=output_mem_config,
-    )
-
-    out = ttnn.to_torch(out_t)
-    torch_silu = torch.nn.SiLU()
-    passing, output = comp_pcc(out, torch.mul(torch_silu(in0), in1), 0.999)
-    logger.info(output)
-    assert passing
