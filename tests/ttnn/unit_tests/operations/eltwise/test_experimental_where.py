92e374a0edbff31e8d8d84062577ee88262c6eb5
Aliaksei Sala <aliaksei_sala@epam.com>
2025-05-29 00:06:27 +0000

    Add unit test for where experimental op

---
+++ tests/ttnn/unit_tests/operations/eltwise/test_experimental_where.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+
+import torch
+import ttnn
+
+from tests.ttnn.utils_for_testing import assert_with_pcc
+
+
+@pytest.mark.parametrize("h", [32, 64, 128, 256, 512, 1024])
+@pytest.mark.parametrize("w", [32, 64, 128, 256, 512, 1024])
+def test_where_all_tensors(device, h, w):
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.rand((h, w), dtype=torch.bfloat16)
+    torch_input_tensor1 = torch.rand((h, w), dtype=torch.bfloat16)
+    torch_input_tensor2 = torch.rand((h, w), dtype=torch.bfloat16)
+    golden_fn = ttnn.get_golden_function(ttnn.where)
+    torch_output_tensor = golden_fn(torch_input_tensor.to(torch.bool), torch_input_tensor1, torch_input_tensor2)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
+    input_tensor = ttnn.to_device(input_tensor, device)
+    input_tensor1 = ttnn.from_torch(torch_input_tensor1, layout=ttnn.TILE_LAYOUT, device=device)
+    input_tensor1 = ttnn.to_device(input_tensor1, device)
+    input_tensor2 = ttnn.from_torch(torch_input_tensor2, layout=ttnn.TILE_LAYOUT, device=device)
+    input_tensor2 = ttnn.to_device(input_tensor2, device)
+
+    output_tensor = ttnn.experimental.where(input_tensor, input_tensor1, input_tensor2)
+
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor)
