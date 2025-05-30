3f3212f04ff512824ca42686d222e3f9f6e3c2be
Pavlo Hilei <philei@tenstorrent.com>
2025-05-30 18:26:12 +0000

    First dynamic tensor/shard shape prototype + temp tests

--- tt_metal/hw/inc/accessor/detail.h
+++ tt_metal/hw/inc/accessor/detail.h
@@ -4,23 +4,27 @@
 
 #pragma once
 
+#include <variant>
+#include <tuple>
+
 namespace detail {
 
 template <size_t... Dims>
 struct ShapeWrapper {
-    // Check that rank is > 0
     static constexpr size_t rank = sizeof...(Dims);
+    static constexpr bool is_static = true;
+    using ShapeBase = std::array<uint32_t, rank>;
+    static constexpr ShapeBase shape = {Dims...};
+
+    // Check that rank is > 0
     static_assert(rank > 0, "Shape rank must be greater than 0!");
 
     // Check that all Dims are > 0
     static_assert(((Dims > 0) && ...), "Shape dims must be greater than 0!");
 
-    static constexpr std::array<uint32_t, rank> shape = {Dims...};
-
     // Compute shape properities at compile time
-    static constexpr std::pair<size_t, std::array<uint32_t, rank>> compute_volume_and_strides(
-        const std::array<uint32_t, rank>& shape) {
-        std::array<uint32_t, rank> strides = {};
+    static constexpr std::pair<size_t, ShapeBase> compute_volume_and_strides(const ShapeBase& shape) {
+        ShapeBase strides = {};
         uint32_t stride = 1;
         for (int i = rank - 1; i >= 0; --i) {
             strides[i] = stride;
@@ -32,6 +36,39 @@ struct ShapeWrapper {
     // Compiler should optimize out the second call
     static constexpr auto volume = compute_volume_and_strides(shape).first;
     static constexpr auto strides = compute_volume_and_strides(shape).second;
+};
+
+template <size_t Rank>
+struct ShapeWrapperDynamic {
+    static constexpr size_t rank = Rank;
+    static constexpr bool is_static = false;
+    using ShapeBase = std::array<uint32_t, rank>;
+    ShapeBase shape;    // runtime shape
+    ShapeBase strides;  // runtime strides
+    size_t volume;      // runtime volume
+
+    // Check that rank is > 0
+    static_assert(rank > 0, "Shape rank must be greater than 0!");
+
+    template <class... Ts, std::enable_if_t<sizeof...(Ts) == Rank, int> = 0>
+    constexpr explicit ShapeWrapperDynamic(Ts... exts) : shape{static_cast<uint32_t>(exts)...} {
+        compute_volume_and_strides(shape);
+    }
+
+    constexpr explicit ShapeWrapperDynamic(const ShapeBase& shape) : shape{shape} { compute_volume_and_strides(shape); }
+
+    constexpr explicit ShapeWrapperDynamic(ShapeBase&& shape) : shape{std::move(shape)} {
+        compute_volume_and_strides(shape);
+    }
+
+    inline void compute_volume_and_strides(const ShapeBase& shape) {
+        uint32_t stride = 1;
+        for (int i = rank - 1; i >= 0; --i) {
+            strides[i] = stride;
+            stride *= shape[i];
+        }
+        volume = strides[0] * shape[0];
+    }
 };
 
 template <size_t... PackedCoords>
@@ -54,43 +91,188 @@ using struct_sequence_wrapper_t =
 template <typename TensorShape, typename ShardShape, typename BankCoords>
 struct DistributionSpec {
     static_assert(TensorShape::rank == ShardShape::rank, "Tensor and shard shapes must have the same rank!");
+    static_assert(
+        std::is_same_v<typename TensorShape::ShapeBase, typename ShardShape::ShapeBase>,
+        "Tensor and shard shapes bases must be the same");
     static constexpr auto rank = TensorShape::rank;
+    using ShapeBase = typename TensorShape::ShapeBase;
     static_assert(rank > 0, "Tensor and shard shape ranks must be greater than 0!");
 
-    static constexpr auto tensor_shape = TensorShape::shape;
-    static constexpr auto tensor_strides = TensorShape::strides;
-    static constexpr auto tensor_volume = TensorShape::volume;
+    static constexpr bool is_static = TensorShape::is_static && ShardShape::is_static;
 
-    static constexpr auto shard_shape = ShardShape::shape;
-    static constexpr auto shard_strides = ShardShape::strides;
-    static constexpr auto shard_volume = ShardShape::volume;
-
-    // Compute shard grid and shard grid strides at compile time
-    static constexpr std::pair<std::array<uint32_t, rank>, std::array<uint32_t, rank>> compute_shard_grid_and_strides(
-        const std::array<uint32_t, rank>& tensor_shape, const std::array<uint32_t, rank>& shard_shape) {
-        std::array<uint32_t, rank> shard_grid = {};
-        std::array<uint32_t, rank> shard_grid_strides = {};
-        uint32_t stride = 1;
-        for (int i = rank - 1; i >= 0; --i) {
-            shard_grid[i] = (tensor_shape[i] - 1) / shard_shape[i] + 1;  // div_up
-            shard_grid_strides[i] = stride;
-            stride *= shard_grid[i];
+    template <
+        class... Ts1,
+        class... Ts2,
+        std::enable_if_t<
+            (sizeof...(Ts1) == (TensorShape::is_static ? 0 : rank) &&
+             sizeof...(Ts2) == (ShardShape::is_static ? 0 : rank)),
+            int> = 0>
+    constexpr DistributionSpec(
+        std::tuple<Ts1...> tensor_shape = {},  // empty tuple == “nothing to pass”
+        std::tuple<Ts2...> shard_shape = {}) :
+        tensor_shape_{std::make_from_tuple<TensorShape>(tensor_shape)},
+        shard_shape_{std::make_from_tuple<ShardShape>(shard_shape)} {
+        // Check that the number of shards is greater than or equal to the number of banks
+        // Here, shard_grid_strides[0] * shard_grid[0] is the total number of shards
+        if constexpr (!is_static) {
+            compute_shard_grid_and_strides_rt(tensor_shape_.shape, shard_shape_.shape);
+            ASSERT(
+                shard_grid_rt[0] * shard_grid_strides_rt[0] >= num_banks,
+                "Number of shards must be greater than or equal to number of banks!");
+        } else {
+            static_assert(
+                shard_grid_ct(TensorShape::shape, ShardShape::shape)[0] *
+                        shard_grid_strides_ct(TensorShape::shape, ShardShape::shape)[0] >=
+                    num_banks,
+                "Number of shards must be greater than or equal to number of banks!");
         }
-        return {shard_grid, shard_grid_strides};
     }
 
-    // Compiler should optimize out the second call
-    // NOTE: shard_grid is not really needed, but probably more idiomatic to keep it with shard_grid_strides
-    static constexpr auto shard_grid = compute_shard_grid_and_strides(tensor_shape, shard_shape).first;
-    static constexpr auto shard_grid_strides = compute_shard_grid_and_strides(tensor_shape, shard_shape).second;
+    // Single constructor that handles all array cases
+    constexpr DistributionSpec(const ShapeBase& tensor_shape_arr, const ShapeBase& shard_shape_arr = {}) :
+        DistributionSpec(tensor_shape_arr, shard_shape_arr, select_constructor_tag()) {}
+
+private:
+    struct dynamic_tensor_dynamic_shard_tag {};
+    struct dynamic_tensor_static_shard_tag {};
+    struct static_tensor_dynamic_shard_tag {};
+
+    // Function to select the appropriate tag based on static/dynamic properties
+    static constexpr auto select_constructor_tag() {
+        if constexpr (!TensorShape::is_static && !ShardShape::is_static) {
+            return dynamic_tensor_dynamic_shard_tag{};
+        } else if constexpr (!TensorShape::is_static && ShardShape::is_static) {
+            return dynamic_tensor_static_shard_tag{};
+        } else if constexpr (TensorShape::is_static && !ShardShape::is_static) {
+            return static_tensor_dynamic_shard_tag{};
+        } else {
+            // This case is handled by the tuple constructor, so it shouldn't reach here
+            static_assert(!is_static, "Static tensor and static shard should use the tuple constructor");
+        }
+    }
+
+    // Specializations for each case
+    constexpr DistributionSpec(
+        const ShapeBase& tensor_shape_arr, const ShapeBase& shard_shape_arr, dynamic_tensor_dynamic_shard_tag) :
+        tensor_shape_(tensor_shape_arr), shard_shape_(shard_shape_arr) {
+        compute_shard_grid_and_strides_rt(tensor_shape_.shape, shard_shape_.shape);
+    }
+
+    constexpr DistributionSpec(const ShapeBase& tensor_shape_arr, const ShapeBase&, dynamic_tensor_static_shard_tag) :
+        tensor_shape_(tensor_shape_arr) {
+        compute_shard_grid_and_strides_rt(tensor_shape_.shape, ShardShape::shape);
+    }
+
+    constexpr DistributionSpec(const ShapeBase&, const ShapeBase& shard_shape_arr, static_tensor_dynamic_shard_tag) :
+        shard_shape_(shard_shape_arr) {
+        compute_shard_grid_and_strides_rt(TensorShape::shape, shard_shape_.shape);
+    }
+
+public:
+    constexpr ShapeBase get_shard_grid() const {
+        if constexpr (is_static) {
+            return shard_grid_ct(TensorShape::shape, ShardShape::shape);
+        } else {
+            return shard_grid_rt;
+        }
+    }
+
+    constexpr ShapeBase get_shard_grid_strides() const {
+        if constexpr (is_static) {
+            return shard_grid_strides_ct(TensorShape::shape, ShardShape::shape);
+        } else {
+            return shard_grid_strides_rt;
+        }
+    }
+
+    constexpr ShapeBase get_tensor_shape() const {
+        if constexpr (is_static) {
+            return TensorShape::shape;
+        } else {
+            return tensor_shape_.shape;
+        }
+    }
+
+    constexpr ShapeBase get_tensor_strides() const {
+        if constexpr (is_static) {
+            return TensorShape::strides;
+        } else {
+            return tensor_shape_.strides;
+        }
+    }
+
+    constexpr size_t get_tensor_volume() const {
+        if constexpr (is_static) {
+            return TensorShape::volume;
+        } else {
+            return tensor_shape_.volume;
+        }
+    }
+
+    constexpr ShapeBase get_shard_shape() const {
+        if constexpr (is_static) {
+            return ShardShape::shape;
+        } else {
+            return shard_shape_.shape;
+        }
+    }
+
+    constexpr ShapeBase get_shard_strides() const {
+        if constexpr (is_static) {
+            return ShardShape::strides;
+        } else {
+            return shard_shape_.strides;
+        }
+    }
+
+    constexpr size_t get_shard_volume() const {
+        if constexpr (is_static) {
+            return ShardShape::volume;
+        } else {
+            return shard_shape_.volume;
+        }
+    }
+
+    // Compute shard grid and shard grid strides at compile time
+    static constexpr ShapeBase shard_grid_ct(const ShapeBase& tensor_shape, const ShapeBase& shard_shape) {
+        ShapeBase shard_grid = {};
+        for (int i = rank - 1; i >= 0; --i) {
+            shard_grid[i] = (tensor_shape[i] - 1) / shard_shape[i] + 1;  // div_up
+        }
+        return shard_grid;
+    }
+
+    static constexpr ShapeBase shard_grid_strides_ct(const ShapeBase& tensor_shape, const ShapeBase& shard_shape) {
+        ShapeBase shard_grid_strides = {};
+        uint32_t stride = 1;
+        for (int i = rank - 1; i >= 0; --i) {
+            shard_grid_strides[i] = stride;
+            stride *= (tensor_shape[i] - 1) / shard_shape[i] + 1;  // div_up
+        }
+        return shard_grid_strides;
+    }
+
+    void compute_shard_grid_and_strides_rt(const ShapeBase& tensor_shape, const ShapeBase& shard_shape) {
+        uint32_t stride = 1;
+        for (int i = rank - 1; i >= 0; --i) {
+            shard_grid_rt[i] = (tensor_shape[i] - 1) / shard_shape[i] + 1;  // div_up
+            shard_grid_strides_rt[i] = stride;
+            stride *= shard_grid_rt[i];
+        }
+        // Check that the number of shards is greater than or equal to the number of banks
+        ASSERT(
+            shard_grid_rt[0] * shard_grid_strides_rt[0] >= num_banks,
+            "Number of shards must be greater than or equal to number of banks!");
+    }
+
+    std::conditional_t<is_static, std::monostate, ShapeBase> shard_grid_rt{};
+    std::conditional_t<is_static, std::monostate, ShapeBase> shard_grid_strides_rt{};
+
+    TensorShape tensor_shape_;  // Tensor shape
+    ShardShape shard_shape_;    // Shard shape
 
     static constexpr auto num_banks = BankCoords::num_banks;
     static constexpr auto packed_xy_coords = BankCoords::packed_xy_coords;
-    // Check that the number of shards is greater than or equal to the number of banks
-    // Here, shard_grid_strides[0] * shard_grid[0] is the total number of shards
-    static_assert(
-        shard_grid_strides[0] * shard_grid[0] >= num_banks,
-        "Number of shards must be greater than or equal to number of banks!");
 };
 
 template <size_t BASE, size_t RANK, size_t NUM_BANKS>
--- tt_metal/hw/inc/accessor/sharded_accessor.h
+++ tt_metal/hw/inc/accessor/sharded_accessor.h
@@ -18,11 +18,45 @@ constexpr auto compile_time_args_skip = DSpec::rank * 2 + DSpec::num_banks;
 
 template <typename DSpec, size_t PageSize>
 struct ShardedAccessor {
-    static constexpr DSpec dspec{};
+    static constexpr auto rank = DSpec::rank;
     static constexpr auto page_size = PageSize;
+    static constexpr DSpec static_dspec{};  // Used only if DSpec is static
 
-    // Runtime args
+    std::conditional_t<DSpec::is_static, std::monostate, DSpec> dspec_instance;  // Used only if DSpec is not static
     const size_t bank_base_address;
+
+    ShardedAccessor(const size_t bank_base_address_in = 0) : bank_base_address(bank_base_address_in) {}
+    ShardedAccessor(const DSpec& dspec, const size_t bank_base_address_in = 0) :
+        dspec_instance(dspec), bank_base_address(bank_base_address_in) {}
+
+    // Helper to get the appropriate DSpec instance
+    constexpr auto& get_dspec() const {
+        if constexpr (DSpec::is_static) {
+            return static_dspec;
+        } else {
+            return dspec_instance;
+        }
+    }
+
+    // NOC APIs
+    FORCE_INLINE
+    std::uint64_t get_noc_addr(const uint32_t id, uint8_t noc = noc_index) const {
+        const auto [bank_id, bank_offset] = this->get_bank_and_offset(id);
+        return NOC_XY_ADDR(
+            DYNAMIC_NOC_X(noc, (get_dspec().packed_xy_coords[bank_id] >> 16) & 0xFFFF),
+            DYNAMIC_NOC_Y(noc, get_dspec().packed_xy_coords[bank_id] & 0xFFFF),
+            bank_base_address + bank_offset * page_size);
+    }
+
+    FORCE_INLINE
+    void noc_async_read_page(const uint32_t id, const uint32_t dest_addr, uint8_t noc = noc_index) const {
+        noc_async_read(get_noc_addr(id, noc), dest_addr, page_size, noc);
+    }
+
+    FORCE_INLINE
+    void noc_async_write_page(const uint32_t id, const uint32_t src_addr, uint8_t noc = noc_index) const {
+        noc_async_write(src_addr, get_noc_addr(id, noc), page_size, noc);
+    }
 
     // Helpers
     struct PageMapping {
@@ -32,18 +66,18 @@ struct ShardedAccessor {
 
     PageMapping get_bank_and_offset(uint32_t page_id) const {
         // Check that page_id is within bounds
-        ASSERT(page_id < dspec.tensor_volume);
+        ASSERT(page_id < get_dspec().get_tensor_volume());
         // TODO: Should be possible to directly implement get_bank_and_offset logic with page_id and skip computing the
         // page_coord
-        std::array<uint32_t, dspec.rank> page_coord;
-        for (int i = dspec.rank - 1; i >= 0; --i) {
-            page_coord[i] = page_id % dspec.tensor_shape[i];
-            page_id /= dspec.tensor_shape[i];
+        std::array<uint32_t, rank> page_coord;
+        for (int i = rank - 1; i >= 0; --i) {
+            page_coord[i] = page_id % get_dspec().get_tensor_shape()[i];
+            page_id /= get_dspec().get_tensor_shape()[i];
         }
         return get_bank_and_offset(page_coord);
     }
 
-    PageMapping get_bank_and_offset(const std::array<uint32_t, dspec.rank> page_coord) const {
+    PageMapping get_bank_and_offset(const std::array<uint32_t, rank> page_coord) const {
         // Flattened shard id is used to compute the bank id and shard id within a bank
         // - First, get the shard coordinate with page_coord[i] / dspec.shard_shape[i]
         // - Then, multiply by the shard grid strides and accumulate
@@ -56,39 +90,21 @@ struct ShardedAccessor {
 
         size_t flattened_shard_id = 0;
         size_t page_offset_within_shard = 0;
-        for (size_t i = 0; i < dspec.rank; ++i) {
+        for (size_t i = 0; i < rank; ++i) {
             // Check that page_coord is within bounds
-            ASSERT(page_coord[i] < dspec.tensor_shape[i]);
-            flattened_shard_id += (page_coord[i] / dspec.shard_shape[i]) * dspec.shard_grid_strides[i];
-            page_offset_within_shard += (page_coord[i] % dspec.shard_shape[i]) * dspec.shard_strides[i];
+            ASSERT(page_coord[i] < get_dspec().get_tensor_shape()[i]);
+            flattened_shard_id +=
+                (page_coord[i] / get_dspec().get_shard_shape()[i]) * get_dspec().get_shard_grid_strides()[i];
+            page_offset_within_shard +=
+                (page_coord[i] % get_dspec().get_shard_shape()[i]) * get_dspec().get_shard_strides()[i];
         }
 
         // NOTE: This assumes shards are round-robin assigned across banks
-        size_t bank_id = flattened_shard_id % dspec.num_banks;
-        size_t bank_shard_id = flattened_shard_id / dspec.num_banks;
+        size_t bank_id = flattened_shard_id % get_dspec().num_banks;
+        size_t bank_shard_id = flattened_shard_id / get_dspec().num_banks;
 
-        size_t bank_page_offset = bank_shard_id * dspec.shard_volume + page_offset_within_shard;
+        size_t bank_page_offset = bank_shard_id * get_dspec().get_shard_volume() + page_offset_within_shard;
 
         return {bank_id, bank_page_offset};
-    }
-
-    // NOC APIs
-    FORCE_INLINE
-    std::uint64_t get_noc_addr(const uint32_t id, uint8_t noc = noc_index) const {
-        const auto [bank_id, bank_offset] = this->get_bank_and_offset(id);
-        return NOC_XY_ADDR(
-            DYNAMIC_NOC_X(noc, (dspec.packed_xy_coords[bank_id] >> 16) & 0xFFFF),
-            DYNAMIC_NOC_Y(noc, dspec.packed_xy_coords[bank_id] & 0xFFFF),
-            bank_base_address + bank_offset * page_size);
-    }
-
-    FORCE_INLINE
-    void noc_async_read_page(const uint32_t id, const uint32_t dest_addr, uint8_t noc = noc_index) const {
-        noc_async_read(get_noc_addr(id, noc), dest_addr, page_size, noc);
-    }
-
-    FORCE_INLINE
-    void noc_async_write_page(const uint32_t id, const uint32_t src_addr, uint8_t noc = noc_index) const {
-        noc_async_write(src_addr, get_noc_addr(id, noc), page_size, noc);
     }
 };
