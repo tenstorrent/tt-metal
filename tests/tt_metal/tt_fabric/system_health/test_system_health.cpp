f9de452fb3bf23b6f542509e7f312d4ca56e69c2
Austin Ho <109362939+tt-aho@users.noreply.github.com>
2025-05-30 11:00:43 -0400

    #20712: Report tray and N-id for UBBs and add option to specify minimum number of links to check to system health check (#22744)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/20712
    
    ### Problem description
    Need to provide user additional details/options when validating a system
    configuration.
    
    ### What's changed
    Print the tray and id of a chip if it is a UBB.
    Add option to specify topology and number of links to expect, which
    allows us to either relax tolerances (ex reduce number of links) or
    increase validation (ex check all 4 sides for torus 2d).
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [x] New/Existing tests provide coverage for changes

--- tests/tt_metal/tt_fabric/system_health/test_system_health.cpp
+++ tests/tt_metal/tt_fabric/system_health/test_system_health.cpp
@@ -11,10 +11,29 @@
 #include <utility>
 
 #include <tt-metalium/logger.hpp>
+#include <tt-metalium/mesh_graph.hpp>
 #include "impl/context/metal_context.hpp"
+#include "tests/tt_metal/test_utils/test_common.hpp"
 
 namespace tt::tt_fabric {
 namespace system_health_tests {
+
+const std::unordered_map<tt::ARCH, std::vector<std::uint16_t>> ubb_bus_ids = {
+    {tt::ARCH::WORMHOLE_B0, {0x00, 0x40, 0xC0, 0x80}},
+    {tt::ARCH::BLACKHOLE, {0xC0, 0x80, 0x00, 0x40}},
+};
+
+std::pair<std::uint32_t, std::uint32_t> get_ubb_ids(chip_id_t chip_id) {
+    const auto& cluster = tt::tt_metal::MetalContext::instance().get_cluster();
+    const auto& tray_bus_ids = ubb_bus_ids.at(cluster.arch());
+    auto tray_bus_id_it = std::find(tray_bus_ids.begin(), tray_bus_ids.end(), cluster.get_bus_id(chip_id) & 0xF0);
+    if (tray_bus_id_it != tray_bus_ids.end()) {
+        auto unique_chip_id = cluster.get_unique_chip_ids().at(chip_id);
+        auto ubb_asic_id = ((unique_chip_id >> 56) & 0xFF);
+        return std::make_pair(tray_bus_id_it - tray_bus_ids.begin() + 1, ubb_asic_id);
+    }
+    return std::make_pair(0, 0);
+}
 
 TEST(Cluster, ReportSystemHealth) {
     // Despite potential error messages, this test will not fail
@@ -36,11 +55,17 @@ TEST(Cluster, ReportSystemHealth) {
         }
     }
 
+    auto cluster_type = cluster.get_cluster_type();
+
     std::vector<std::string> unexpected_system_states;
     for (const auto& [chip_id, unique_chip_id] : unique_chip_ids) {
         const auto& soc_desc = cluster.get_soc_desc(chip_id);
         std::stringstream chip_id_ss;
         chip_id_ss << std::dec << "Chip: " << chip_id << " Unique ID: " << std::hex << unique_chip_id;
+        if (cluster_type == tt::ClusterType::GALAXY) {
+            auto [tray_id, ubb_asic_id] = get_ubb_ids(chip_id);
+            chip_id_ss << " Tray: " << tray_id << " N" << ubb_asic_id;
+        }
         ss << chip_id_ss.str() << std::endl;
         for (const auto& [eth_core, chan] : soc_desc.logical_eth_core_to_chan_map) {
             tt_cxy_pair virtual_eth_core(
@@ -100,14 +125,86 @@ TEST(Cluster, TestMeshFullConnectivity) {
     }
     EXPECT_EQ(eth_connections.size(), num_expected_chips)
         << " Expected " << num_expected_chips << " in " << magic_enum::enum_name(cluster_type) << " cluster";
+
+    auto input_args = ::testing::internal::GetArgvs();
+
+    if (test_args::has_command_option(input_args, "-h") || test_args::has_command_option(input_args, "--help")) {
+        log_info(LogTest, "Usage:");
+        log_info(
+            LogTest,
+            "  --min-connections: target minimum number of connections between chips (default depends on system "
+            "type) ");
+        log_info(
+            LogTest,
+            "  --system-topology: system topology to check (defaults to no topology check) Valid values: {}",
+            magic_enum::enum_names<FabricType>());
+        return;
+    }
+
+    // Parse command line arguments
+    std::uint32_t num_target_connections = 0;
+    std::tie(num_target_connections, input_args) =
+        test_args::get_command_option_uint32_and_remaining_args(input_args, "--min-connections", 0);
+    if (num_target_connections > num_connections_per_side) {
+        log_warning(
+            tt::LogTest,
+            "Min connections specified is greater than expected num connections per side for {}, overriding to {}.",
+            magic_enum::enum_name(cluster_type),
+            num_connections_per_side);
+        num_target_connections = num_connections_per_side;
+    }
+
+    std::optional<FabricType> target_system_topology = std::nullopt;
+    std::string target_system_topology_str = "";
+    std::tie(target_system_topology_str, input_args) =
+        test_args::get_command_option_and_remaining_args(input_args, "--system-topology", "");
+    if (not target_system_topology_str.empty()) {
+        target_system_topology =
+            magic_enum::enum_cast<FabricType>(target_system_topology_str, magic_enum::case_insensitive);
+        if (*target_system_topology != FabricType::TORUS_2D) {
+            log_warning(
+                tt::LogTest,
+                "System topology {} not supported for mesh check, skipping topology verification",
+                target_system_topology_str);
+            target_system_topology = std::nullopt;
+        }
+    }
     for (const auto& [chip, connections] : eth_connections) {
+        std::stringstream chip_ss;
+        chip_ss << "Chip " << chip;
+        if (cluster_type == tt::ClusterType::GALAXY) {
+            auto [tray_id, ubb_asic_id] = get_ubb_ids(chip);
+            chip_ss << " Tray: " << tray_id << " N" << ubb_asic_id;
+        }
         std::map<chip_id_t, int> num_connections_to_chip;
         for (const auto& [channel, remote_chip_and_channel] : connections) {
             num_connections_to_chip[std::get<0>(remote_chip_and_channel)]++;
         }
+        if (target_system_topology.has_value()) {
+            if (*target_system_topology == FabricType::TORUS_2D) {
+                static constexpr std::uint32_t num_expected_chip_connections = 4;
+                EXPECT_EQ(num_connections_to_chip.size(), num_expected_chip_connections)
+                    << chip_ss.str() << " has " << num_connections_to_chip.size()
+                    << " connections to other chips, expected " << num_expected_chip_connections << " for "
+                    << magic_enum::enum_name(*target_system_topology) << " topology";
+            }
+        }
         for (const auto& [other_chip, count] : num_connections_to_chip) {
-            EXPECT_EQ(count, num_connections_per_side) << "Chip " << chip << " has " << count << " connections to Chip "
-                                                       << other_chip << ", expected " << num_connections_per_side;
+            std::stringstream other_chip_ss;
+            other_chip_ss << "Chip " << other_chip;
+            if (cluster_type == tt::ClusterType::GALAXY) {
+                auto [tray_id, ubb_asic_id] = get_ubb_ids(other_chip);
+                other_chip_ss << " Tray: " << tray_id << " N" << ubb_asic_id;
+            }
+            if (num_target_connections > 0) {
+                EXPECT_GE(count, num_target_connections)
+                    << chip_ss.str() << " has " << count << " connections to " << other_chip_ss.str()
+                    << ", expected at least " << num_target_connections;
+            } else {
+                EXPECT_EQ(count, num_connections_per_side)
+                    << chip_ss.str() << " has " << count << " connections to " << other_chip_ss.str() << ", expected "
+                    << num_connections_per_side;
+            }
         }
     }
 }
