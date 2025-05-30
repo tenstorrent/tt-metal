ec9c2054ed4737fb6c111aeaa0768245a1150903
punith.sekar <punith.sekar@multicorewareinc.com>
2025-05-30 09:57:16 +0000

    #0:opt

--- tests/ttnn/integration_tests/pointpillars/test_ttnn_second.py
+++ tests/ttnn/integration_tests/pointpillars/test_ttnn_second.py
@@ -13,6 +13,23 @@ from ttnn.model_preprocessing import preprocess_model_parameters
 from models.experimental.functional_pointpillars.reference.mvx_faster_rcnn import MVXFasterRCNN
 from models.experimental.functional_pointpillars.reference.second import SECOND
 from models.experimental.functional_pointpillars.tt.ttnn_second import TtSECOND
+import math
+from models.experimental.yolo_common.yolo_utils import determine_num_cores, get_core_grid_from_num_cores
+
+
+def interleaved_to_sharded(x):
+    if x.get_layout() == ttnn.TILE_LAYOUT:
+        x = ttnn.to_layout(x, layout=ttnn.ROW_MAJOR_LAYOUT)
+    if x.shape[1] == 1:
+        x = ttnn.reshape(x, (x.shape[0], int(math.sqrt(x.shape[2])), int(math.sqrt(x.shape[2])), x.shape[3]))
+    nhw = x.shape[0] * x.shape[1] * x.shape[2]
+    num_cores = determine_num_cores(nhw, x.shape[2])
+    core_grid = get_core_grid_from_num_cores(num_cores)
+    shardspec = ttnn.create_sharded_memory_config_(
+        x.shape, core_grid, ttnn.ShardStrategy.HEIGHT, orientation=ttnn.ShardOrientation.ROW_MAJOR
+    )
+
+    return ttnn.reshard(x, shardspec) if x.is_sharded() else ttnn.interleaved_to_sharded(x, shardspec)
 
 
 def fold_batch_norm2d_into_conv2d(conv, bn):
@@ -86,6 +103,8 @@ def test_ttnn_second(device, use_pretrained_weight, reset_seeds):
     x_permute = x.permute(0, 2, 3, 1)
     ttnn_x = ttnn.from_torch(x_permute, device=device, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT)
 
+    ttnn_x = interleaved_to_sharded(ttnn_x)
+
     reference_output = reference_model(x=x)
 
     ttnn_model = TtSECOND(
@@ -101,5 +120,12 @@ def test_ttnn_second(device, use_pretrained_weight, reset_seeds):
     ttnn_output = ttnn_model(x=ttnn_x)
 
     for i in range(len(ttnn_output)):
-        passing, pcc = assert_with_pcc(reference_output[i], ttnn.to_torch(ttnn_output[i]).permute(0, 3, 1, 2), 0.98)
+        ttnn_output_temp = ttnn.to_torch(ttnn_output[i]).permute(0, 3, 1, 2)
+        ttnn_output_temp = ttnn_output_temp.reshape(
+            ttnn_output_temp.shape[0],
+            ttnn_output_temp.shape[1],
+            int(math.sqrt(ttnn_output_temp.shape[3])),
+            int(math.sqrt(ttnn_output_temp.shape[3])),
+        )
+        passing, pcc = assert_with_pcc(reference_output[i], ttnn_output_temp, 0.98)
         logger.info(f"Passing: {passing}, PCC: {pcc}")
