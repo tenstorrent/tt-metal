3ca07ebce1d6013bbf1a2d90c79e5c5082b9d3ac
Mouliraj Elamurugan <ctr-mcw-melamurugan@ext.tenstorrent.com>
2025-06-01 12:40:47 +0530

    #21824: Add uint16 support for eqz and nez (#22207)
    ### Ticket
    Link to Github Issue #21824 
    
    ### Problem description
     Zero-comparison operations are not supported for uint16.
     
    ### What's changed
    Added uint16 for eqz and nez.
    
    ### Checklist
    - [x] [All post commit CI
    ](https://github.com/tenstorrent/tt-metal/actions/runs/15294079000)

--- tt_metal/include/compute_kernel_api.h
+++ tt_metal/include/compute_kernel_api.h
@@ -357,6 +357,22 @@ ALWI void eqz_tile(uint32_t idst) { MATH((llk_math_eltwise_unary_sfpu_eqz<APPROX
 // clang-format on
 ALWI void eqz_tile_int32(uint32_t idst) { MATH((llk_math_eltwise_unary_sfpu_eqz_int32<APPROX>(idst))); }
 
+// clang-format off
+/**
+ * Will store in the output of the compute core True if each element of a equal to zero.
+ * The DST register buffer must be in acquired state via *acquire_dst* call.
+ * This call is blocking and is only
+ * available on the compute engine.
+ *
+ * Return value: None
+ *
+ * | Argument       | Description                                                                | Type     | Valid Range                                           | Required |
+ * |----------------|----------------------------------------------------------------------------|----------|-------------------------------------------------------|----------|
+ * | idst           | The index of the tile in DST register buffer to perform the computation on | uint32_t | Must be less than the size of the DST register buffer | True     |
+ */
+// clang-format on
+ALWI void eqz_tile_uint16(uint32_t idst) { MATH((llk_math_eltwise_unary_sfpu_eqz_uint16<APPROX>(idst))); }
+
 /**
  * Please refer to documentation for any_init.
  */
@@ -488,6 +504,22 @@ ALWI void nez_tile(uint32_t idst) { MATH((llk_math_eltwise_unary_sfpu_nez<APPROX
  */
 // clang-format on
 ALWI void nez_tile_int32(uint32_t idst) { MATH((llk_math_eltwise_unary_sfpu_nez_int32<APPROX>(idst))); }
+
+// clang-format off
+/**
+ * Will store in the output of the compute core True if each element is not equal to zero.
+ * The DST register buffer must be in acquired state via *acquire_dst* call.
+ * This call is blocking and is only
+ * available on the compute engine.
+ *
+ * Return value: None
+ *
+ * | Argument       | Description                                                                | Type     | Valid Range                                           | Required |
+ * |----------------|----------------------------------------------------------------------------|----------|-------------------------------------------------------|----------|
+ * | idst           | The index of the tile in DST register buffer to perform the computation on | uint32_t | Must be less than the size of the DST register buffer | True     |
+ */
+// clang-format on
+ALWI void nez_tile_uint16(uint32_t idst) { MATH((llk_math_eltwise_unary_sfpu_nez_uint16<APPROX>(idst))); }
 
 /**
  * Please refer to documentation for any_init.
--- ttnn/cpp/ttnn/operations/eltwise/unary/unary_pybind.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/unary/unary_pybind.cpp
@@ -1773,7 +1773,7 @@ void py_module(py::module& module) {
         ttnn::eqz,
         R"doc(\mathrm{{output\_tensor}}_i = (\mathrm{{input\_tensor_i\ == 0}}))doc",
         "",
-        R"doc(BFLOAT16, BFLOAT8_B, INT32)doc");
+        R"doc(BFLOAT16, BFLOAT8_B, INT32, UINT16)doc");
     bind_unary_operation(
         module,
         ttnn::ceil,
@@ -1896,7 +1896,7 @@ void py_module(py::module& module) {
         ttnn::nez,
         R"doc(\mathrm{{output\_tensor}}_i = (\mathrm{{input\_tensor_i\ != 0}}))doc",
         "",
-        R"doc(BFLOAT16, BFLOAT8_B, INT32)doc");
+        R"doc(BFLOAT16, BFLOAT8_B, INT32, UINT16)doc");
 
     bind_unary_operation_overload_complex_return_complex(
         module,
