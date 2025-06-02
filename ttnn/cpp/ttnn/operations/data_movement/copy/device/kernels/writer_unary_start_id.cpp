b897fd850bc4732d49827dc954748b5033ed1d12
Nour Ardo <nardo@tenstorrent.com>
2025-06-02 16:24:03 -0400

    Add sharding support for RM tensors in ttnn.copy (#22792)
    ### Ticket
    Link to Github Issue
    https://github.com/tenstorrent/tt-metal/issues/22323#issuecomment-2902145760
    
    ### Problem description
    In https://github.com/tenstorrent/tt-metal/pull/22591, we added support
    for ttnn.copy for sharded tensors in tile layout only
    
    ### What's changed
    This PR adds the support for sharded RM tensors for the copy operation 
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/15329510054
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/data_movement/copy/device/kernels/writer_unary_start_id.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/copy/device/kernels/writer_unary_start_id.cpp
@@ -7,7 +7,6 @@
 #include "ttnn/cpp/ttnn/operations/ccl/kernel_common/sharding_addrgen.hpp"
 
 void kernel_main() {
-    DPRINT << "writer_unary_start_id" << ENDL();
     uint32_t dst_addr = get_arg_val<uint32_t>(0);
     uint32_t num_tiles = get_arg_val<uint32_t>(1);
     uint32_t start_id = get_arg_val<uint32_t>(2);
