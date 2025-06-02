99f5a7478858482fcb4174114e1d5d41cde30fbb
Predrag Ilkic <pilkic@tenstorrent.com>
2025-06-02 16:48:22 +0200

    tracy: find binaries pt. 2

--- tools/tracy/common.py
+++ tools/tracy/common.py
@@ -27,11 +27,43 @@ PROFILER_BIN_DIR = TT_METAL_HOME / "build/tools/profiler/bin"
 
 TRACY_OPS_TIMES_FILE_NAME = "tracy_ops_times.csv"
 TRACY_OPS_DATA_FILE_NAME = "tracy_ops_data.csv"
-TRACY_MODULE_PATH = TT_METAL_HOME / "tt_metal/third_party/tracy"
 TRACY_FILE_NAME = "tracy_profile_log_host.tracy"
 
 TRACY_CAPTURE_TOOL = "capture-release"
 TRACY_CSVEXPROT_TOOL = "csvexport-release"
+
+
+def find_all(start_dir: str, filename: str):
+    for root, _dirs, files in os.walk(start_dir, followlinks=True):
+        if filename in files:
+            yield Path(root) / filename
+
+
+def find_profiler_binaries():
+    logger.info(f"Searching for profiler binaries in {TT_METAL_HOME}...")
+    global PROFILER_BIN_DIR
+    for binary in find_all(TT_METAL_HOME, TRACY_CAPTURE_TOOL):
+        if binary.is_file():
+            capture_tool = binary
+            logger.info(f"Found profiler capture tool: {capture_tool}")
+
+            PROFILER_BIN_DIR = binary.parent
+
+            csvexport_tool = PROFILER_BIN_DIR / TRACY_CSVEXPROT_TOOL
+            if not csvexport_tool.is_file():
+                logger.error(f"CSV export tool {csvexport_tool} not found in the same directory as capture tool.")
+                sys.exit(1)
+
+            logger.info(f"Found profiler CSV export tool: {csvexport_tool}")
+            return
+
+    logger.error(
+        "Profiler binaries not found. Please ensure that the TT_METAL_HOME environment variable is set correctly."
+    )
+    sys.exit(1)
+
+
+find_profiler_binaries()
 
 
 def generate_logs_folder(outFolder):
