ad2f1becd7b41ba141c567714a39928465f07260
asaigal <asaigal@tenstorrent.com>
2025-06-02 16:38:53 +0000

    Initial Changes for Multi Host Sockets
      - Add MeshSocket serialization tests and functionality. Current serialization
        schema relies on FlatBuffers
      - Add SocketPeerDescriptor struct encapsulating runtime dependent endpoint state
        - This includes the socket config and data buffer addresses + chip IDs (needed
          for computing fabric encodings
        - This data structure will be used for Multi-Host Socket Handshaking
      - Add sender_rank and receiver_rank fields to SocketConfig (0 by default)
      - Move mesh_coordinate.fbs out of ttnn/ and into distributed/ since its used by
        the MeshSocket FB schema. Update namespaces and build flow to account for this

--- cmake/flatbuffers.cmake
+++ cmake/flatbuffers.cmake
@@ -1,13 +1,27 @@
 # Function to generate FlatBuffers C++ headers from schema files
 function(GENERATE_FBS_HEADER FBS_FILE)
+    # Remaining args are include dirs (can be passed in as a list by the caller)
+    set(INCLUDE_DIRS ${ARGN})
+
     get_filename_component(FBS_FILE_NAME ${FBS_FILE} NAME_WE)
     set(FBS_GENERATED_HEADER_DIR "${CMAKE_CURRENT_BINARY_DIR}/flatbuffers")
     set(FBS_GENERATED_HEADER_FILE "${FBS_GENERATED_HEADER_DIR}/${FBS_FILE_NAME}_generated.h")
+
+    foreach(DIR IN LISTS INCLUDE_DIRS)
+        message(STATUS "FB Adding include dir: ${DIR}")
+        list(
+            APPEND
+            FLATC_INCLUDE_ARGS
+            -I
+            ${DIR}
+        )
+    endforeach()
+
     add_custom_command(
         OUTPUT
             ${FBS_GENERATED_HEADER_FILE}
         COMMAND
-            flatc --cpp --scoped-enums -I ${CMAKE_CURRENT_SOURCE_DIR} -o "${FBS_GENERATED_HEADER_DIR}" ${FBS_FILE}
+            flatc --cpp --scoped-enums ${FLATC_INCLUDE_ARGS} -o "${FBS_GENERATED_HEADER_DIR}" ${FBS_FILE}
         DEPENDS
             flatc
             ${FBS_FILE}
