52fde125739128f2b044719df413c4abbb51cce1
Ambrose Ling <aling@tenstorrent.com>
2025-06-02 16:15:25 +0000

    Modified op perf test cases to run binary silu on CI

--- models/demos/llama3_subdevices/tests/tg_perf_unit_tests/test_llama_ops_perf_TG_llama.py
+++ models/demos/llama3_subdevices/tests/tg_perf_unit_tests/test_llama_ops_perf_TG_llama.py
@@ -12,12 +12,12 @@ from models.perf.benchmarking_utils import BenchmarkData, BenchmarkProfiler
 @pytest.mark.parametrize(
     ("op_name", "expected_kernel_duration_us", "perf_margin"),
     [
-        ("LayerNorm", 12.5, 0.05),
-        ("ScaledDotProductAttentionDecode", 13.2, 0.05),
-        ("NLPCreateHeadsDecodeDeviceOperation", 8.32, 0.05),
-        ("NLPConcatHeadsDecodeDeviceOperation", 6.07, 0.05),
-        ("PagedUpdateCacheDeviceOperation", 4.5, 0.1),
-        ("RotaryEmbeddingLlamaFusedQK", 4.15, 0.05),
+        # ("LayerNorm", 12.5, 0.05),
+        # ("ScaledDotProductAttentionDecode", 13.2, 0.05),
+        # ("NLPCreateHeadsDecodeDeviceOperation", 8.32, 0.05),
+        # ("NLPConcatHeadsDecodeDeviceOperation", 6.07, 0.05),
+        # ("PagedUpdateCacheDeviceOperation", 4.5, 0.1),
+        # ("RotaryEmbeddingLlamaFusedQK", 4.15, 0.05),
         ("BinaryDeviceOperation", 2.71, 0.05),
     ],
 )
@@ -25,7 +25,7 @@ def test_llama_tg_ops_perf_device(op_name, expected_kernel_duration_us, perf_mar
     batch = 32
     test = "llama-distributed-ln"
     subdir = "llama-unit-tests"
-    num_iterations = 3
+    num_iterations = 1
 
     profiler = BenchmarkProfiler()
     benchmark_data = BenchmarkData()
