1776cc994391ca6a3da69a7d3fec5cd8f37fe8b9
Sankar Manoj <smanoj@tenstorrent.com>
2025-06-03 10:44:44 +0000

    #0: WIP: Tiled padded slice

--- ttnn/cpp/ttnn/operations/experimental/padded_slice/device/kernels/dataflow/padded_slice_reader_tiled_interleaved_start_id.cpp
+++ ttnn/cpp/ttnn/operations/experimental/padded_slice/device/kernels/dataflow/padded_slice_reader_tiled_interleaved_start_id.cpp
@@ -9,44 +9,40 @@
 
 void kernel_main() {
     const uint32_t src_addr = get_arg_val<uint32_t>(0);
-    const uint32_t padded_stick_size = get_arg_val<uint32_t>(1);
-    const uint32_t unpadded_stick_size = get_arg_val<uint32_t>(2);
-    const uint32_t stick_size_offset = get_arg_val<uint32_t>(3);
-    const uint32_t num_dims = get_arg_val<uint32_t>(4);
-    const uint32_t start_id = get_arg_val<uint32_t>(5);
-    const uint32_t num_tiles_per_core = get_arg_val<uint32_t>(7);
-    const uint32_t num_tiles_per_barrier = get_arg_val<uint32_t>(8);
-    const uint32_t num_unpadded_sticks_addr = get_arg_addr(9);
+    const uint32_t num_dims = get_arg_val<uint32_t>(1);
+    const uint32_t start_id = get_arg_val<uint32_t>(2);
+    const uint32_t num_tiles_per_core = get_arg_val<uint32_t>(3);
+    const uint32_t num_tiles_per_barrier = get_arg_val<uint32_t>(4);
+    const uint32_t num_unpadded_sticks_addr = get_arg_addr(5);
 
     tt_l1_ptr uint32_t* num_unpadded_sticks = (tt_l1_ptr uint32_t*)(num_unpadded_sticks_addr);
     volatile tt_l1_ptr uint32_t* num_padded_sticks = num_unpadded_sticks + num_dims;
     volatile tt_l1_ptr uint32_t* id_per_dim = num_padded_sticks + num_dims;
 
     constexpr bool src0_is_dram = get_compile_time_arg_val(0) == 1;
-    constexpr uint32_t misalignment = get_compile_time_arg_val(1);
-    uint32_t read_size = unpadded_stick_size + misalignment;
-
-    const InterleavedAddrGen<src0_is_dram> s0 = {.bank_base_address = src_addr, .page_size = padded_stick_size};
 
     constexpr uint32_t cb_id_in0 = 0;
 
     uint32_t src_stick_id = start_id;
     uint32_t tiles_read = 0;
-#ifdef DEBUG
-    DPRINT << "src_addr: " << src_addr << ", padded_stick_size: " << padded_stick_size
-           << ", unpadded_stick_size: " << unpadded_stick_size << ", stick_size_offset: " << stick_size_offset
-           << ", num_dims: " << num_dims << ", start_id: " << start_id << ", num_tiles_per_core: " << num_tiles_per_core
-           << ", num_tiles_per_barrier: " << num_tiles_per_barrier << ENDL();
+    const uint32_t tile_size = get_tile_size(cb_id_in0);
+    const InterleavedAddrGen<src0_is_dram> s0 = {.bank_base_address = src_addr, .page_size = tile_size};
 
-    DPRINT << "misalignment: " << misalignment << ", read_size: " << read_size << ", src_stick_id: " << src_stick_id
-           << ", tiles_read: " << tiles_read << ENDL();
+#define DEBUG
+#ifdef DEBUG
+    DPRINT << "src_addr: " << src_addr << ", num_dims: " << num_dims << ", start_id: " << start_id
+           << ", num_tiles_per_core: " << num_tiles_per_core << ", num_tiles_per_barrier: " << num_tiles_per_barrier
+           << ENDL();
+
+    DPRINT << "tile_size: " << tile_size << ", src_stick_id: " << src_stick_id << ", tiles_read: " << tiles_read
+           << ENDL();
 
     DPRINT << "num_unpadded_sticks: " << num_unpadded_sticks[0] << " " << num_unpadded_sticks[1] << " "
            << num_unpadded_sticks[2] << " " << num_unpadded_sticks[3] << " " << ENDL();
     DPRINT << "num_padded_sticks: " << num_padded_sticks[0] << " " << num_padded_sticks[1] << " "
            << num_padded_sticks[2] << " " << num_padded_sticks[3] << " " << ENDL();
-    DPRINT << "Out CB Page size: " << get_local_cb_interface(cb_id_in0).fifo_page_size << ENDL();
 #endif
+
     const uint32_t base_src_buffer_l1_addr = get_write_ptr(cb_id_in0);
     const uint64_t base_noc_addr = get_noc_addr(0, s0);
 
@@ -57,8 +53,11 @@ void kernel_main() {
         for (uint32_t i = 0; i < num_tiles_per_barrier and tiles_read < num_tiles_per_core; ++i) {
             tiles_read++;
             uint64_t src_noc_addr = get_noc_addr(src_stick_id, s0);
-            noc_async_read(src_noc_addr, src_buffer_l1_addr, unpadded_stick_size);
-            src_buffer_l1_addr += stick_size_offset;
+            noc_async_read(src_noc_addr, src_buffer_l1_addr, tile_size);
+#ifdef DEBUG
+            DPRINT << "src_stick_id: " << src_stick_id << ENDL();
+#endif
+            src_buffer_l1_addr += tile_size;
             src_stick_id++;
             for (uint32_t j = 0; j < num_dims; j++) {
                 id_per_dim[j]++;
