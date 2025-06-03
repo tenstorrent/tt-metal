9477fac6935639ee3ed739171e41a1e34f48cd82
Raymond Kim <109366641+tt-rkim@users.noreply.github.com>
2025-06-03 19:04:40 -0400

    #21732: [skip ci] Add final llama demo and long stress tests and TORUS_2D test (#22991)
    ### Ticket
    
    #21732
    
    ### Problem description
    
    We need these tests
    
    ### What's changed
    
    Add llama stress test
    Add 2d torus system health check (will prob fail all the time but oh
    well)
    
    ### Checklist
    - https://github.com/tenstorrent/tt-metal/actions/runs/15428580360
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes

--- dockerfile/upstream_test_images/run_upstream_tests_vanilla.sh
+++ dockerfile/upstream_test_images/run_upstream_tests_vanilla.sh
@@ -35,6 +35,11 @@ test_suite_wh_6u_metal_unit_tests() {
     TT_METAL_SLOW_DISPATCH_MODE=1 ./build/test/tt_metal/tt_fabric/fabric_unit_tests --gtest_filter="Fabric2D*Fixture.*"
 }
 
+test_suite_wh_6u_metal_2d_torus_health_check_tests() {
+    echo "[upstream-tests] Checking for 2D Torus topology on WH 6U"
+    ./build/test/tt_metal/tt_fabric/test_system_health --system-topology TORUS_2D
+}
+
 test_suite_wh_6u_model_unit_tests() {
     echo "[upstream-tests] running WH 6U upstream model unit tests"
     pytest tests/ttnn/unit_tests/operations/ccl/test_ccl_async_TG_llama.py
@@ -64,6 +69,32 @@ test_suite_wh_6u_llama_demo_tests() {
     pytest models/demos/llama3_subdevices/tests/test_llama_model.py -k "quick"
     pytest models/demos/llama3_subdevices/tests/unit_tests/test_llama_model_prefill.py
     pytest models/demos/llama3_subdevices/demo/text_demo.py -k "repeat"
+    # Some AssertionError: Throughput is out of targets 49 - 53 t/s/u in 200 iterations
+    # assert 200 <= 20
+    # pytest models/demos/llama3_subdevices/demo/demo_decode.py -k "full"
+    pytest models/demos/llama3_subdevices/demo/demo_decode.py -k "mini-stress-test"
+}
+
+test_suite_wh_6u_llama_long_stress_tests() {
+    echo "[upstream-tests] running WH 6U upstream Llama long stress tests. Note that on 6U systems built as of End of May 2025, this may take up to 4 hours to run."
+    echo "[upstream-tests] Ensure that you have a TG directory populated with .bin files in LLAMA_DIR on the host."
+
+    if [ -z "${LLAMA_DIR}" ]; then
+        echo "[upstream-tests] Error: LLAMA_DIR environment variable not detected. Please set this environment variable to tell the tests where to find the downloaded Llama weights." >&2
+        exit 1
+    fi
+
+    if [ -d "$LLAMA_DIR" ] && [ "$(ls -A $LLAMA_DIR)" ]; then
+        echo "[upstream-tests] Llama weights exist, continuing"
+    else
+        echo "[upstream-tests] Error: Llama weights do not seem to exist in $LLAMA_DIR, exiting" >&2
+        exit 1
+    fi
+
+    # TODO: to remove...
+    pip install -r models/tt_transformers/requirements.txt
+    # This will take almost 3 hours. Ensure that the tensors are cached in the LLAMA_DIR.
+    pytest models/demos/llama3_subdevices/demo/demo_decode.py -k "stress-test and not mini-stress-test"
 }
 
 # Define test suite mappings for different hardware topologies
@@ -71,7 +102,7 @@ declare -A hw_topology_test_suites
 
 hw_topology_test_suites["blackhole"]="test_suite_bh_single_pcie_python_unit_tests test_suite_bh_single_pcie_metal_unit_tests test_suite_bh_single_pcie_small_ml_model_tests"
 hw_topology_test_suites["blackhole_no_models"]="test_suite_bh_single_pcie_python_unit_tests test_suite_bh_single_pcie_metal_unit_tests"
-hw_topology_test_suites["wh_6u"]="test_suite_wh_6u_model_unit_tests test_suite_wh_6u_llama_demo_tests test_suite_wh_6u_metal_unit_tests"
+hw_topology_test_suites["wh_6u"]="test_suite_wh_6u_model_unit_tests test_suite_wh_6u_llama_demo_tests test_suite_wh_6u_metal_unit_tests test_suite_wh_6u_metal_2d_torus_health_check_tests"
 
 # Function to display help
 show_help() {
