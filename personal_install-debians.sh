4526ce8ab793323f18640172b1170ee01a5f0f96
Evan Banerjee <ebanerjee@tenstorrent.com>
2025-06-04 13:59:34 +0000

    adding personal files. DO NOT MERGE

---
+++ personal_install-debians.sh
@@ -0,0 +1,10 @@
+apt install ./tt-metalium_0.59.0~rc29+13.7acd4295c4~ubuntu22.04_amd64.deb ./tt-metalium-dev_0.59.0~rc29+13.7acd4295c4~ubuntu22.04_amd64.deb ./tt-metalium-examples_0.59.0~rc29+13.7acd4295c4~ubuntu22.04_amd64.deb 
+
+export CC=gcc-12
+export CXX=g++-12
+
+/usr/share/tt-metalium/examples/hello_world_compute_kernel
+
+/hello_world_compute_kernel/build
+
+/usr/share/tt-metalium/examples/hello_world_compute_kernel/build/metal_example_hello_world_compute_kernel
---
+++ personal_reload_container.sh
@@ -0,0 +1,18 @@
+cd .build/default
+rm -rf *.deb
+rm -rf *.ddeb
+
+cd ..
+cd ..
+
+cmake --preset default
+cmake --build default --preset dev --clean-first
+cd .build/default
+ninja package
+cd ..
+cd ..
+docker stop evan-package-testing
+docker rm evan-package-testing
+docker rmi evan-testing
+docker build -f dockerfile/Dockerfile.basic-dev -t evan-testing .
+docker run -it --name evan-package-testing --privileged --device /dev/tenstorrent:/dev/tenstorrent -v /dev/hugepages-1G/:/dev/hugepages-1G/ -v /opt/tenstorrent/sfpi:/opt/tenstorrent/sfpi evan-testing bash -c "export CC=gcc-12 && export CXX=g++-12 && export TT_METAL_HOME=/usr/libexec/tt-metalium && add-apt-repository -y ppa:mhier/libboost-latest && cd /debians && apt install -y ./*.deb && cd .. && ./personal_cmake_stuff.sh && cd /usr/share/tt-metalium/examples && exec /bin/bash"
