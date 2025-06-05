2477224fdacf5173bf0e0faae4ab24deea3c86c2
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-06-05 13:10:33 -0400

    Dynamic stack size (#23031)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/19983
    
    ### Problem description
    The current stack and data sizes are statically fixed.
    
    ### What's changed
    * Remove `MEM_${FOO}_STACK_TOP` define.
    * Rename `MEM_${FOO}_STACK_SIZE` to `MEM_${FOO}_STACK_MIN_SIZE`. Values
    remain unchanged.
    * Alter linker scripts to remove stack segment -- it is not needed. Add
    __stack_base symbol at the end of .bss.
    * Remove HAL's `get_stack_size` API. It's unused (AFAICT), and now a
    property of the kernel being run.
    
    A later patch will shrink the minimum stack sizes.
    ### Checklist
    - [YES] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [YES] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes

--- tt_metal/llrt/wormhole/wh_hal.cpp
+++ tt_metal/llrt/wormhole/wh_hal.cpp
@@ -135,20 +135,6 @@ void Hal::initialize_wh(bool is_base_routing_fw_enabled) {
     this->noc_ucast_addr_y_func_ = [](uint64_t addr) -> uint64_t { return NOC_UNICAST_ADDR_Y(addr); };
     this->noc_local_addr_func_ = [](uint64_t addr) -> uint64_t { return NOC_LOCAL_ADDR(addr); };
 
-    this->stack_size_func_ = [](uint32_t type) -> uint32_t {
-        switch (type) {
-            case DebugBrisc: return MEM_BRISC_STACK_SIZE;
-            case DebugNCrisc: return MEM_NCRISC_STACK_SIZE;
-            case DebugErisc: return 0;  // Not managed/checked by us.
-            case DebugIErisc: return MEM_IERISC_STACK_SIZE;
-            case DebugSubordinateIErisc: return MEM_BRISC_STACK_SIZE;
-            case DebugTrisc0: return MEM_TRISC0_STACK_SIZE;
-            case DebugTrisc1: return MEM_TRISC1_STACK_SIZE;
-            case DebugTrisc2: return MEM_TRISC2_STACK_SIZE;
-        }
-        return 0xdeadbeef;
-    };
-
     this->eth_fw_arg_addr_func_ = [&](uint32_t arg_index) -> uint32_t { return 0; };
 
     this->num_nocs_ = NUM_NOCS;
