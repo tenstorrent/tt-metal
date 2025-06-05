6151d1e4afdbfff2e8e4cc5e7bdbdde339e26bce
Nathan Maurice <nmaurice@tenstorrent.com>
2025-06-05 11:01:53 +0000

    cumsum: remove ttnn.experimental.cumsum_backwared (will be add in another PR)

--- ttnn/cpp/ttnn/operations/experimental/reduction/cumsum/cumsum.hpp
+++ ttnn/cpp/ttnn/operations/experimental/reduction/cumsum/cumsum.hpp
@@ -22,24 +22,10 @@ struct CumSumOperation {
         const std::optional<MemoryConfig>& memory_config = std::nullopt);
 };
 
-struct CumSumBackwardOperation {
-    static Tensor invoke(
-        QueueId queue_id,
-        const Tensor& input,
-        int64_t dim,
-        std::optional<ttnn::DataType> dtype = std::nullopt,
-        std::optional<Tensor> preallocated_output = std::nullopt,
-        const std::optional<MemoryConfig>& memory_config = std::nullopt);
-};
-
 }  // namespace ttnn::operations::experimental::reduction
 
 namespace ttnn::experimental {
 constexpr auto cumsum = decorators::
     register_operation<"ttnn::experimental::cumsum", ttnn::operations::experimental::reduction::CumSumOperation>();
-
-constexpr auto cumsum_backward = decorators::register_operation<
-    "ttnn::experimental::cumsum_backward",
-    ttnn::operations::experimental::reduction::CumSumBackwardOperation>();
 
 }  // namespace ttnn::experimental
