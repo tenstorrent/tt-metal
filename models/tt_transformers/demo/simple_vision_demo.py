6b78f62bb5f5c1a2544e24c49b7ed071169a9212
Colman Glagovich <114512306+cglagovichTT@users.noreply.github.com>
2025-06-05 11:46:18 -0400

    Update simple_vision_demo.py

--- models/tt_transformers/demo/simple_vision_demo.py
+++ models/tt_transformers/demo/simple_vision_demo.py
@@ -374,7 +374,7 @@ def test_multimodal_demo_text(
         base_model_name = model_args[0].base_model_name
         target_prefill_tok_s = {
             "N300_Llama3.2-11B": 10.8,
-            "T3K_Llama3.2-11B": 7.7,
+            "T3K_Llama3.2-11B": 6.5,
             "T3K_Llama3.2-90B": 3,
         }[f"{tt_device_name}_{base_model_name}"]
 
