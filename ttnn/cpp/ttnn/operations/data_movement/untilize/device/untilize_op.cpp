8b069d0cf99e18cd396f065d21624da40d0a33b6
grechsteinerTT <grechsteiner@tenstorrent.com>
2025-06-05 17:47:15 +0000

    Update validate() to properly represent uneven sharding support

--- ttnn/cpp/ttnn/operations/data_movement/untilize/device/untilize_op.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/untilize/device/untilize_op.cpp
@@ -15,62 +15,70 @@ namespace ttnn::operations::data_movement {
 void Untilize::validate(const std::vector<Tensor>& input_tensors) const {
     using namespace tt::constants;
     const auto& input_tensor_a = input_tensors.at(0);
+
+    uint32_t tensor_width = input_tensor_a.get_padded_shape()[-1];
+    uint32_t tensor_height = input_tensor_a.volume() / tensor_width;
+
+    bool input_is_sharded = input_tensor_a.is_sharded();
+    bool output_is_sharded = this->output_mem_config.is_sharded();
+
+    TensorMemoryLayout input_memory_layout = input_tensor_a.memory_config().memory_layout();
+    TensorMemoryLayout output_memory_layout = this->output_mem_config.memory_layout();
+
     TT_FATAL(input_tensor_a.storage_type() == StorageType::DEVICE, "Operands to untilize need to be on device!");
     TT_FATAL(input_tensor_a.buffer() != nullptr, "Operands to untilize need to be allocated in buffers on device!");
     TT_FATAL(input_tensor_a.layout() == Layout::TILE, "Can only untilize tile major data");
 
     // Input must be in valid tile layout
-    TT_FATAL(input_tensor_a.get_padded_shape()[-1] % TILE_WIDTH == 0, "Width must be evenly divisible into tiles");
-    TT_FATAL(
-        (input_tensor_a.physical_volume() / input_tensor_a.padded_shape()[-1]) % TILE_HEIGHT == 0,
-        "Height must be evenly divisible into tiles");
+    TT_FATAL(tensor_width % TILE_WIDTH == 0, "Width must be evenly divisible into tiles");
+    TT_FATAL(tensor_height % TILE_HEIGHT == 0, "Height must be evenly divisible into tiles");
 
-    // Only support interleaved or sharded memory layout
+    // Only support interleaved or sharded memory layouts
     TT_FATAL(
-        input_tensor_a.memory_config().memory_layout() != TensorMemoryLayout::SINGLE_BANK,
-        "Input memory layout must be interleaved or sharded");
+        input_memory_layout != TensorMemoryLayout::SINGLE_BANK, "Input memory layout must be interleaved or sharded");
     TT_FATAL(
-        this->output_mem_config.memory_layout() != TensorMemoryLayout::SINGLE_BANK,
-        "Output memory layout must be interleaved or sharded");
+        output_memory_layout != TensorMemoryLayout::SINGLE_BANK, "Output memory layout must be interleaved or sharded");
 
     // Special conditions for sub_core_grids special case
     if (this->sub_core_grids.has_value()) {
         TT_FATAL(
-            input_tensor_a.memory_config().memory_layout() == TensorMemoryLayout::INTERLEAVED,
+            input_memory_layout == TensorMemoryLayout::INTERLEAVED,
             "Input memory layout must be interleaved when sub_core_grid argument provided");
         TT_FATAL(
-            this->output_mem_config.memory_layout() == TensorMemoryLayout::INTERLEAVED,
+            output_memory_layout == TensorMemoryLayout::INTERLEAVED,
             "Output memory layout must be interleaved when sub_core_grid argument provided");
         TT_FATAL(
             this->use_multicore == true,
             "sub_core_grid implementation only supported when use_multicore flag argument is set to true");
     }
 
-    if (input_tensor_a.is_sharded()) {
+    // If input is sharded, then the shard shape must be in multiples of tiles
+    if (input_is_sharded) {
         std::array<uint32_t, 2> input_shard_shape = input_tensor_a.shard_spec().value().shape;
-        TT_FATAL(input_shard_shape[0] % TILE_HEIGHT == 0, "Input shard height must be a multiple of tile height");
-        TT_FATAL(input_shard_shape[1] % TILE_WIDTH == 0, "Input shard width must be a multiple of tile width");
+        uint32_t input_shard_width = input_shard_shape[1];
+        uint32_t input_shard_height = input_shard_shape[0];
+        TT_FATAL(input_shard_width % TILE_WIDTH == 0, "Input shard width must be a multiple of tile width");
+        TT_FATAL(input_shard_height % TILE_HEIGHT == 0, "Input shard height must be a multiple of tile height");
     }
 
-    // TODO: (GR) Uneven shard support
-    // Shard shape restrictions
-    if (input_tensor_a.is_sharded()) {
-        std::array<uint32_t, 2> input_shard_shape = input_tensor_a.shard_spec().value().shape;
-        TT_FATAL(
-            input_tensor_a.get_padded_shape()[-1] % input_shard_shape[1] == 0,
-            "Uneven input shard shape not supported");
-        TT_FATAL(
-            (input_tensor_a.volume() / input_tensor_a.get_padded_shape()[-1]) % input_shard_shape[0] == 0,
-            "Uneven input shard shape not supported");
-    }
-    if (this->output_mem_config.is_sharded()) {
+    // We always support uneven input sharding. Uneven output sharding is only supported if the input and output memory
+    // layouts are identical (i.e. height->height, width->width, block->block) and the input and output shard specs
+    // are identical. Otherwise uneven output sharding is not supported.
+    if (output_is_sharded) {
         std::array<uint32_t, 2> output_shard_shape = this->output_mem_config.shard_spec().value().shape;
-        TT_FATAL(
-            input_tensor_a.get_padded_shape()[-1] % output_shard_shape[1] == 0,
-            "Uneven output shard shape not supported");
-        TT_FATAL(
-            (input_tensor_a.volume() / input_tensor_a.get_padded_shape()[-1]) % output_shard_shape[0] == 0,
-            "Uneven output shard shape not supported");
+        uint32_t output_shard_width = output_shard_shape[1];
+        uint32_t output_shard_height = output_shard_shape[0];
+
+        bool output_is_uneven_sharded_width_wise = tensor_width % output_shard_width != 0;
+        bool output_is_uneven_sharded_height_wise = tensor_height % output_shard_height != 0;
+        if (output_is_uneven_sharded_width_wise || output_is_uneven_sharded_height_wise) {
+            TT_FATAL(
+                input_memory_layout == output_memory_layout,
+                "Input and output memory layouts must be identical if output is uneven sharded");
+            TT_FATAL(
+                input_tensor_a.shard_spec().value() == this->output_mem_config.shard_spec().value(),
+                "Input and output shard specs must be identical if output is uneven sharded");
+        }
     }
 }
 
@@ -97,6 +105,9 @@ operation::ProgramWithCallbacks Untilize::create_program(
     bool input_is_sharded = input_tensor_a.is_sharded();
     bool output_is_sharded = output_tensor.is_sharded();
 
+    TensorMemoryLayout input_memory_layout = input_tensor_a.memory_config().memory_layout();
+    TensorMemoryLayout output_memory_layout = output_tensor.memory_config().memory_layout();
+
     if (!this->use_multicore) {
         // Single core implementation
         return detail::untilize_single_core(
@@ -118,9 +129,10 @@ operation::ProgramWithCallbacks Untilize::create_program(
         return detail::untilize_multi_core_block(
             input_tensor_a, output_tensor, this->use_pack_untilize, this->fp32_dest_acc_en);
     }
-    if (input_is_sharded && output_is_sharded && input_tensor_a.shard_spec() == output_tensor.shard_spec()) {
-        // Optimized special case implementation for when both input and output are sharded, have identical shard specs,
-        // and have identical memory layouts
+    if (input_is_sharded && output_is_sharded && input_memory_layout == output_memory_layout &&
+        input_tensor_a.shard_spec() == output_tensor.shard_spec()) {
+        // Optimized special case implementation for when both input and output are sharded, have identical
+        // memory layouts (i.e. height->height, width->width, block->block), and have identical shard specs
         return detail::untilize_multi_core_input_and_output_shard_spec_identical(
             input_tensor_a, output_tensor, this->use_pack_untilize, this->fp32_dest_acc_en);
     }
