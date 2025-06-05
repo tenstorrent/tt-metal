404fcbe6685236472465259a5826d7bdd7e030ae
grechsteinerTT <grechsteiner@tenstorrent.com>
2025-06-05 14:45:41 +0000

    Input uneven sharding width wise now supported, still need to update validate() and tests

--- ttnn/cpp/ttnn/operations/data_movement/untilize/device/kernels/dataflow/writer_unary_stick_layout_split_rows_multi_core.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/untilize/device/kernels/dataflow/writer_unary_stick_layout_split_rows_multi_core.cpp
@@ -9,25 +9,23 @@
 void kernel_main() {
     // run-time args
     const uint32_t dst_addr = get_arg_val<uint32_t>(0);
-    const uint32_t num_blocks = get_arg_val<uint32_t>(1);
+    const uint32_t num_input_blocks_to_process = get_arg_val<uint32_t>(1);
     const uint32_t height_wise_input_block_start_index = get_arg_val<uint32_t>(2);
     const uint32_t width_wise_input_block_index = get_arg_val<uint32_t>(3);
+    const uint32_t num_unpadded_cols_per_input_block = get_arg_val<uint32_t>(4);
 
     // compile-time args
     constexpr bool dst_is_dram = get_compile_time_arg_val(0) == 1;
     constexpr uint32_t cb_id_out0 = get_compile_time_arg_val(1);
-    constexpr uint32_t stick_size = get_compile_time_arg_val(2);
-    constexpr bool stick_size_is_power_of_two = get_compile_time_arg_val(3) == 1;
-    constexpr uint32_t log_base_2_of_page_size = get_compile_time_arg_val(4);
+    constexpr uint32_t output_stick_size = get_compile_time_arg_val(2);
+    constexpr bool output_stick_size_is_power_of_two = get_compile_time_arg_val(3) == 1;
+    constexpr uint32_t output_log_base_2_of_page_size = get_compile_time_arg_val(4);
     constexpr uint32_t tile_height = get_compile_time_arg_val(5);
-    constexpr uint32_t num_tiles_per_input_block =
-        get_compile_time_arg_val(6);  // TODO: (GR) move to run-time to handle uneven shard
+    constexpr uint32_t num_tiles_per_input_block = get_compile_time_arg_val(6);
     constexpr uint32_t num_output_blocks_across_width = get_compile_time_arg_val(7);
     constexpr uint32_t output_element_size = get_compile_time_arg_val(8);
-    constexpr uint32_t num_cols_per_input_block =
-        get_compile_time_arg_val(9);  // TODO: (GR) move to run-time to handle uneven shard
-    constexpr uint32_t num_cols_per_output_block =
-        get_compile_time_arg_val(10);  // TODO: (GR) move to run-time to handle uneven shard
+    constexpr uint32_t num_cols_per_input_block = get_compile_time_arg_val(9);
+    constexpr uint32_t num_cols_per_output_block = get_compile_time_arg_val(10);
 
 #ifdef SHARDED
     using tensor_shard_info = ShardedInfo<
@@ -40,27 +38,34 @@ void kernel_main() {
         get_compile_time_arg_val(17)>;  // pages_per_shard_y
 
     const auto [mapping_table, rt_increment] =
-        experimental::shard_addr_gen_utils::get_shard_map<tensor_shard_info>(get_arg_addr(4));
+        experimental::shard_addr_gen_utils::get_shard_map<tensor_shard_info>(get_arg_addr(5));
     experimental::ShardedAddrGen<tensor_shard_info> s = {.bank_base_address = dst_addr, .shard_array = mapping_table};
 #else
-    const auto s = get_interleaved_addr_gen<dst_is_dram, stick_size_is_power_of_two>(
-        dst_addr, stick_size, log_base_2_of_page_size);
+    const auto s = get_interleaved_addr_gen<dst_is_dram, output_stick_size_is_power_of_two>(
+        dst_addr, output_stick_size, output_log_base_2_of_page_size);
 #endif
     // TODO: (GR) num_cols_per_input_block = num_tiles_per_input_block * tile_width
 
     auto write_tiles_in_current_block = [&](uint32_t block_height_index) {
         cb_wait_front(cb_id_out0, num_tiles_per_input_block);
-        uint32_t l1_read_addr = get_read_ptr(cb_id_out0);
 
-        // Process each row-major row in the input block
+        // Base address of the row of elements we are going to be writing.
+        // If the input is unevenly sharded width wise and we are processing a block in the
+        // last shard width wise, we will not be writing the entire row of elements as the
+        // last x elements will be garbage. Tracking the base address of the row allows us to
+        // easily increment the current read addr to the next row of elements.
+        uint32_t base_l1_read_addr = get_read_ptr(cb_id_out0);
+
+        // Process each row of elements in the input block
         for (uint32_t j = 0; j < tile_height; ++j) {
+            uint32_t current_l1_read_addr = base_l1_read_addr += j * num_cols_per_input_block * output_element_size;
             uint32_t num_rows_already_processed = block_height_index * tile_height + j;
 
             // For width or block sharding, the input tensor may have more/less shards width wise compared
             // to the output tensor. In the less case different sections of the row-major row in the input block
             // may map to different output blocks. In both cases, we may be writing to the middle of an output page.
             uint32_t num_input_cols_processed = 0;
-            while (num_input_cols_processed < num_cols_per_input_block) {
+            while (num_input_cols_processed < num_unpadded_cols_per_input_block) {
                 // Index of the column that we are going to start writing elements to (relative to the entire tensor)
                 uint32_t write_col_index =
                     width_wise_input_block_index * num_cols_per_input_block + num_input_cols_processed;
@@ -79,17 +84,17 @@ void kernel_main() {
                 // Min of the number of remaining unprocessed columns in the input block
                 // and the number of remaining unprocessed columns in the output block.
                 uint32_t num_cols_to_write = std::min(
-                    num_cols_per_input_block - num_input_cols_processed,
+                    num_unpadded_cols_per_input_block - num_input_cols_processed,
                     num_cols_per_output_block - num_cols_already_processed_in_output_block);
                 uint32_t num_bytes_to_write = num_cols_to_write * output_element_size;
 
                 // Perform the write
                 uint64_t dst_noc_addr = get_noc_addr(output_page_id, s, output_offset_within_page_in_bytes);
-                noc_async_write(l1_read_addr, dst_noc_addr, num_bytes_to_write);
+                noc_async_write(current_l1_read_addr, dst_noc_addr, num_bytes_to_write);
 
                 // Update number of columns processed and increment the read ptr
                 num_input_cols_processed += num_cols_to_write;
-                l1_read_addr += num_bytes_to_write;
+                current_l1_read_addr += num_bytes_to_write;
             }
         }
 
@@ -98,7 +103,7 @@ void kernel_main() {
     };
 
     // Each input block processed separately
-    for (uint32_t i = 0; i < num_blocks; ++i) {
+    for (uint32_t i = 0; i < num_input_blocks_to_process; ++i) {
         uint32_t height_wise_input_block_index = height_wise_input_block_start_index + i;
 
         // Process the current block
