7e30c8fca8182f1558828ddb1ce54189402fc1eb
Austin Ho <109362939+tt-aho@users.noreply.github.com>
2025-06-05 15:49:20 -0400

    #23093: Classify chan 15 on WH as an active eth core on mmio chips even when it is unconnected (#23094)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/23093
    
    ### Problem description
    Channel 15 on WH is skipped from ever being added to the inactive eth
    core list because it is reserved for syseng on mmio chips. This is okay
    when channel 15 is an eth core with an active link, but if it is not
    then we have an eth core that is not classified as active or idle, which
    can cause issues with core type queries.
    
    ### What's changed
    Always mark channel 15 as an active eth core for WH mmio chips that
    service remote chips. Change the loop to set routing to be over active
    eth cores, instead of `device_eth_routing_info_`, which is only
    populated with eth cores with active connections.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/15470087491
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/15458693804
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [x] New/Existing tests provide coverage for changes
    APC Debug:
    https://github.com/tenstorrent/tt-metal/actions/runs/15470093489
    T3K Unit:
    https://github.com/tenstorrent/tt-metal/actions/runs/15470098372
    T3K Profiler:
    https://github.com/tenstorrent/tt-metal/actions/runs/15458590750

--- tests/tt_metal/tt_metal/device/test_galaxy_cluster_api.cpp
+++ tests/tt_metal/tt_metal/device/test_galaxy_cluster_api.cpp
@@ -45,6 +45,9 @@ std::unordered_set<chip_id_t> get_ethernet_connected_device_ids(const chip_id_t 
     const std::unordered_set<CoreCoord>& active_ethernet_cores =
         tt::tt_metal::MetalContext::instance().get_cluster().get_active_ethernet_cores(device_id);
     for (const CoreCoord& ethernet_core : active_ethernet_cores) {
+        if (not tt::tt_metal::MetalContext::instance().get_cluster().is_ethernet_link_up(device_id, ethernet_core)) {
+            continue;
+        }
         const auto& [connected_device_id, _] =
             tt::tt_metal::MetalContext::instance().get_cluster().get_connected_ethernet_core(
                 {device_id, ethernet_core});
@@ -67,6 +70,10 @@ TEST_F(TGFixture, ActiveEthValidateNumLinksBetweenAdjacentGalaxyChips) {
             const std::unordered_set<CoreCoord>& active_ethernet_cores =
                 tt::tt_metal::MetalContext::instance().get_cluster().get_active_ethernet_cores(device_id);
             for (const CoreCoord& ethernet_core : active_ethernet_cores) {
+                if (not tt::tt_metal::MetalContext::instance().get_cluster().is_ethernet_link_up(
+                        device_id, ethernet_core)) {
+                    continue;
+                }
                 const auto& [connected_device_id, _] =
                     tt::tt_metal::MetalContext::instance().get_cluster().get_connected_ethernet_core(
                         {device_id, ethernet_core});
