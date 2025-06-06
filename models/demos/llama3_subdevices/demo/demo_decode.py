1edc7df1477f80532d1865bf0a025551355c6127
Johanna Rock <jrock@tenstorrent.com>
2025-06-06 12:33:01 +0000

    Update perf targets

--- models/demos/llama3_subdevices/demo/demo_decode.py
+++ models/demos/llama3_subdevices/demo/demo_decode.py
@@ -36,7 +36,7 @@ TSU_PERF_DROP_LIMIT_PERCENT = 10
 # Constants for TSU thresholds based on the number of layers
 TSU_THRESHOLDS = {
     "4U": {1: {"min": 617, "max": 640}, 80: {"min": 50, "max": 54}},
-    "6U": {1: {"min": 738, "max": 755}, 80: {"min": 55, "max": 59}},
+    "6U": {1: {"min": 738, "max": 762}, 80: {"min": 55, "max": 59}},
 }
 
 
