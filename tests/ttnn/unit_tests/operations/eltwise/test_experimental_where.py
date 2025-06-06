859c0718ec5c30eaedd601a7d1da815057dd85a9
Aliaksei Sala <aliaksei_sala@epam.com>
2025-06-06 16:19:18 -0700

    Added 4D shape test and a 2D test with zero elements.

--- tests/ttnn/unit_tests/operations/eltwise/test_experimental_where.py
+++ tests/ttnn/unit_tests/operations/eltwise/test_experimental_where.py
@@ -66,15 +66,18 @@ def test_ttt_where_1d(device, h):
     _ttt_where_test_impl(device, (h))
 
 
-@pytest.mark.parametrize("h", [16, 32, 64, 65, 1024])
-@pytest.mark.parametrize("w", [16, 32, 64, 65, 1024])
+@pytest.mark.parametrize("h", [0, 16, 32, 64, 65, 1024])
+@pytest.mark.parametrize("w", [0, 16, 32, 64, 65, 1024])
 def test_ttt_where_2d(device, h, w):
     _ttt_where_test_impl(device, (h, w))
 
 
-@pytest.mark.parametrize("shape", [tuple([32] * i) for i in range(6)])
-def test_ttt_where_shapes(device, shape):
-    _ttt_where_test_impl(device, shape)
+@pytest.mark.parametrize("d4", [16])
+@pytest.mark.parametrize("d3", [16])
+@pytest.mark.parametrize("h", [16])
+@pytest.mark.parametrize("w", [16])
+def test_ttt_where_4d(device, d4, d3, h, w):
+    _ttt_where_test_impl(device, (d4, d3, h, w))
 
 
 @pytest.mark.parametrize("d5", [16])
@@ -82,8 +85,13 @@ def test_ttt_where_shapes(device, shape):
 @pytest.mark.parametrize("d3", [16])
 @pytest.mark.parametrize("h", [16])
 @pytest.mark.parametrize("w", [16])
-def test_ttt_where_multidim(device, d5, d4, d3, h, w):
+def test_ttt_where_5d(device, d5, d4, d3, h, w):
     _ttt_where_test_impl(device, (d5, d4, d3, h, w))
+
+
+@pytest.mark.parametrize("shape", [tuple([32] * i) for i in range(6)])
+def test_ttt_where_shapes(device, shape):
+    _ttt_where_test_impl(device, shape)
 
 
 @pytest.mark.xfail(reason="Integer data types are not yet supported.")
