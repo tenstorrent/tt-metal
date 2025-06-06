bf9c477552d8c06ad6c10fdabb5b70e31bdb838f
Nour Ardo <nardo@tenstorrent.com>
2025-06-06 12:52:05 -0400

    All broadcast operation (#23125)
    ### Ticket
    Link to Github Issue
    https://github.com/tenstorrent/tt-metal/issues/23070
    
    ### Problem description
    For All Gather generality, we are aiming to implement the operation as
    all_broadcast operation + local concat
    In all_broadcast, each device broadcasts its tensor to the remaining
    devices. The op takes a single tensor as input and generates a vector of
    tensors where output_vector[i] is the tensor of the device at index i
    The output vector should become the input of the local concat operation
    in all_gather implementation
    
    ### What's changed
    This PR implements the first version of all_broadcast. Each device has
    only a sender side that mcasts its data to the other devices. The op has
    been tested for:
    
    - 2D, 3D and 4D input tensors
    - different memory layouts: 
                      - L1/DRAM interleaved 
    - width/height and block sharded with neighbouring and non-neighbouring
    core grids
    - different tensor layouts: Row Major and Tile layouts
    - different numbers of devices
    - padded input tensors in RM layout
    - bfloat8_b and bfloat16 datatypes
    - only tested for 1 link because the op was implemented and debugged on
    a T3K machine. (the code covers multiple links but it needs to be
    tested)
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/15454555697
    - [x] [T3K unit tests]
    https://github.com/tenstorrent/tt-metal/actions/runs/15454548859
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes

---
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_broadcast_async/device/all_broadcast_async_op.hpp
@@ -0,0 +1,120 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <cstdint>
+#include <tt-metalium/core_coord.hpp>
+#include <tt-metalium/buffer.hpp>
+#include "ttnn/tensor/tensor.hpp"
+#include "ttnn/operations/ccl/shared_with_host/hetergeneous_data_structs.hpp"
+#include <tt-metalium/constants.hpp>
+#include "ttnn/operations/ccl/ccl_host_datastructures.hpp"
+#include "ttnn/operations/ccl/ccl_common.hpp"
+#include "ttnn/operations/ccl/ccl_op_fusion.hpp"
+#include <tt-metalium/global_semaphore.hpp>
+#include "ttnn/global_semaphore.hpp"
+
+#include "ttnn/run_operation.hpp"
+
+#include <optional>
+#include <vector>
+
+namespace ttnn {
+
+using ccl::EriscDatamoverBuilder;
+
+struct AllBroadcastAsync {
+    std::vector<IDevice*> devices;
+    const uint32_t num_links;
+    const uint32_t ring_size;
+    const MemoryConfig output_mem_config;
+    const ccl::Topology topology;
+    const GlobalSemaphore semaphore;
+    std::optional<tt::tt_metal::SubDeviceId> sub_device_id;
+    std::optional<uint32_t> cluster_axis;
+
+    AllBroadcastAsync(
+        std::vector<IDevice*> devices,
+        uint32_t num_links,
+        uint32_t ring_size,
+        MemoryConfig output_mem_config,
+        ccl::Topology topology,
+        GlobalSemaphore semaphore,
+        std::optional<tt::tt_metal::SubDeviceId>& sub_device_id,
+        std::optional<uint32_t> cluster_axis) :
+        devices(std::move(devices)),
+        num_links(num_links),
+        ring_size(ring_size),
+        output_mem_config(output_mem_config),
+        topology(topology),
+        semaphore(semaphore),
+        sub_device_id(sub_device_id),
+        cluster_axis(cluster_axis) {}
+
+    // Add attributes method for reflection
+    auto attributes() const {
+        using tt::stl::reflection::Attribute;
+        std::vector<std::tuple<std::string, Attribute>> attrs;
+
+        attrs.emplace_back("num_links", num_links);
+        attrs.emplace_back("ring_size", ring_size);
+        attrs.emplace_back("output_mem_config", output_mem_config);
+        attrs.emplace_back("topology", topology);
+        attrs.emplace_back("semaphore", semaphore);
+        attrs.emplace_back("cluster_axis", cluster_axis);
+        return attrs;
+    }
+
+    void validate_with_output_tensors(
+        const std::vector<Tensor>& input_tensors, const std::vector<std::optional<Tensor>>& output_tensors) const;
+    std::vector<ttnn::TensorSpec> compute_output_specs(const std::vector<Tensor>& input_tensors) const;
+    tt::tt_metal::operation::MeshWorkloadWithCallbacks create_mesh_workload(
+        const ttnn::MeshCoordinateRangeSet& tensor_coords,
+        const std::vector<Tensor>& input_tensors,
+        std::vector<Tensor>& output_tensors) const;
+    tt::tt_metal::operation::ProgramWithCallbacks create_program_at(
+        const ttnn::MeshCoordinate& coord,
+        const std::vector<Tensor>& input_tensors,
+        std::vector<Tensor>& output_tensors) const;
+    tt::tt_metal::operation::Hash compute_program_hash(const std::vector<Tensor>& input_tensors) const;
+};
+
+tt::tt_metal::operation::ProgramWithCallbacks all_broadcast_async_multicore(
+    const Tensor& input_tensor,
+    IDevice* target_device,
+    std::optional<IDevice*> forward_device,
+    std::optional<IDevice*> backward_device,
+    std::vector<Tensor>& output_tensors,
+    const uint32_t num_links,
+    const uint32_t ring_size,
+    const uint32_t ring_index,
+    ccl::Topology topology,
+    const GlobalSemaphore& semaphore,
+    const std::optional<tt::tt_metal::SubDeviceId>& sub_device_id);
+
+namespace operations::experimental::ccl {
+
+std::vector<Tensor> all_broadcast_async(
+    const Tensor& input_tensor,
+    const GlobalSemaphore& multi_device_global_semaphore,
+    const uint32_t num_links = 1,
+    const std::optional<MemoryConfig>& memory_config = std::nullopt,
+    const ttnn::ccl::Topology topology = ttnn::ccl::Topology::Linear,
+    std::optional<tt::tt_metal::SubDeviceId> sub_device_id = std::nullopt);
+
+std::vector<Tensor> all_broadcast_async(
+    const Tensor& input_tensor,
+    const uint32_t cluster_axis,
+    const MeshDevice& mesh_device,
+    const ttnn::ccl::Topology topology,
+    const GlobalSemaphore& multi_device_global_semaphore,
+    const std::optional<ttnn::Tensor>& persistent_output_tensor = std::nullopt,
+    const std::optional<MemoryConfig>& memory_config = std::nullopt,
+    const std::optional<size_t> num_preferred_links = std::nullopt,
+    std::optional<tt::tt_metal::SubDeviceId> sub_device_id = std::nullopt);
+
+}  // namespace operations::experimental::ccl
+
+}  // namespace ttnn
