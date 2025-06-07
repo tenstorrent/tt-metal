3ac5610a8bd611407c0807c4a122328f46ed4df5
Michael Chiou <156848643+ttmchiou@users.noreply.github.com>
2025-06-07 13:58:01 -0700

    Revert "#22205: Unify ND and 2D sharding, ND sharding performance improvements" (#23239)
    Reverts tenstorrent/tt-metal#23152
    Post Commit now breaking - hung on multiple ttnn tests
    
    https://github.com/tenstorrent/tt-metal/actions/runs/15503046690/job/43660393722

--- tt_metal/api/tt-metalium/buffer_distribution_spec.hpp
+++ tt_metal/api/tt-metalium/buffer_distribution_spec.hpp
@@ -6,43 +6,36 @@
 
 #include <tt-metalium/buffer_types.hpp>
 #include <tt-metalium/core_coord.hpp>
-#include <tt-metalium/shape.hpp>
+#include <tt-metalium/distribution_spec.hpp>
 #include <tt-metalium/shape2d.hpp>
-#include <tt-metalium/buffer_page_mapping.hpp>
 
 namespace tt::tt_metal {
 
 class BufferDistributionSpec {
 public:
     static BufferDistributionSpec from_shard_spec(
-        tt::tt_metal::Shape tensor_shape,
-        tt::tt_metal::Shape shard_shape,
-        tt::tt_metal::Shape2D page_shape,
-        CoreRangeSet core_range_set,
-        ShardOrientation shard_orientation);
+        const tt::tt_metal::Shape& tensor_shape,
+        const tt::tt_metal::Shape& physical_shard_shape,
+        const Shape2D& page_shape,
+        const CoreRangeSet& corerangeset,
+        const ShardOrientation shard_orientation);
 
-    BufferDistributionSpec(
-        tt::tt_metal::Shape tensor_shape_in_pages,
-        tt::tt_metal::Shape shard_shape_in_pages,
-        CoreRangeSet core_range_set,
-        ShardOrientation shard_orientation);
+    tt::tt_metal::Shape get_tensor_shape_in_pages() const { return page_distribution_spec_.get_tensor_shape(); }
+    tt::tt_metal::Shape get_shard_shape_in_pages() const { return page_distribution_spec_.get_shard_shape(); }
 
-    tt::tt_metal::Shape get_tensor_shape_in_pages() const { return tensor_shape_in_pages_; }
-    tt::tt_metal::Shape get_shard_shape_in_pages() const { return shard_shape_in_pages_; }
-
-    size_t num_shards() const;
-    size_t num_shards_per_core() const;
-    size_t num_dev_pages_per_core() const;
+    size_t num_dev_pages_per_core() const {
+        return page_distribution_spec_.get_shard_shape().volume() *
+               page_distribution_spec_.get_max_num_shards_per_target();
+    }
     size_t num_cores() const { return cores_.size(); }
     const std::vector<CoreCoord>& get_cores() const { return cores_; }
 
-    BufferPageMapping compute_page_mapping() const;
+    const std::vector<DistributionSpec::TargetData>& get_page_mapping(DistributionSpec::MappingMode mapping_mode);
 
 private:
-    tt::tt_metal::Shape tensor_shape_in_pages_;
-    tt::tt_metal::Shape shard_shape_in_pages_;
-    ShardOrientation shard_orientation_ = ShardOrientation::ROW_MAJOR;
+    BufferDistributionSpec(const DistributionSpec& page_distribution_spec, const std::vector<CoreCoord>& cores);
 
+    DistributionSpec page_distribution_spec_;
     std::vector<CoreCoord> cores_;
 };
 
