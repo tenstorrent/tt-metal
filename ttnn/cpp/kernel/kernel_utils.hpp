063ad3ac02d10fedab82fd33ed789ccb34aa2504
Aliaksei Sala <aliaksei_sala@epam.com>
2025-06-09 12:22:29 -0700

    Use more descriptive name for template parameter: KernelArgsStruct

--- ttnn/cpp/kernel/kernel_utils.hpp
+++ ttnn/cpp/kernel/kernel_utils.hpp
@@ -12,32 +12,26 @@
 #include "compile_time_args.h"
 
 namespace ttnn::kernel_utils {
-template <typename T, uint32_t... I>
-T make_runtime_struct_from_args(std::integer_sequence<uint32_t, I...>) {
-    static_assert(std::is_aggregate_v<T>, "T must be aggregate-initializable");
-
-    return T{get_arg_val<uint32_t>(I)...};
+template <typename KernelArgsStruct, uint32_t... I>
+KernelArgsStruct make_runtime_struct_from_args(std::integer_sequence<uint32_t, I...>) {
+    return KernelArgsStruct{get_arg_val<uint32_t>(I)...};
 }
 
-template <typename T>
-T make_runtime_struct_from_args() {
-    constexpr uint32_t num_fields = sizeof(T) / sizeof(uint32_t);
-    static_assert(sizeof(T) % sizeof(uint32_t) == 0, "Struct must be a multiple of uint32_t");
-    return make_runtime_struct_from_args<T>(std::make_integer_sequence<uint32_t, num_fields>{});
+template <typename KernelArgsStruct>
+KernelArgsStruct make_runtime_struct_from_args() {
+    constexpr uint32_t num_fields = sizeof(KernelArgsStruct) / sizeof(uint32_t);
+    return make_runtime_struct_from_args<KernelArgsStruct>(std::make_integer_sequence<uint32_t, num_fields>{});
 }
 
-template <typename T, uint32_t... I>
-constexpr T make_compile_time_struct_from_args(std::integer_sequence<uint32_t, I...>) {
-    static_assert(std::is_aggregate_v<T>, "T must be aggregate-initializable");
-
-    return T{get_compile_time_arg_val(I)...};
+template <typename KernelArgsStruct, uint32_t... I>
+constexpr KernelArgsStruct make_compile_time_struct_from_args(std::integer_sequence<uint32_t, I...>) {
+    return KernelArgsStruct{get_compile_time_arg_val(I)...};
 }
 
-template <typename T>
-constexpr T make_compile_time_struct_from_args() {
-    constexpr uint32_t num_fields = sizeof(T) / sizeof(uint32_t);
-    static_assert(sizeof(T) % sizeof(uint32_t) == 0, "Struct must be a multiple of uint32_t");
-    return make_compile_time_struct_from_args<T>(std::make_integer_sequence<uint32_t, num_fields>{});
+template <typename KernelArgsStruct>
+constexpr KernelArgsStruct make_compile_time_struct_from_args() {
+    constexpr uint32_t num_fields = sizeof(KernelArgsStruct) / sizeof(uint32_t);
+    return make_compile_time_struct_from_args<KernelArgsStruct>(std::make_integer_sequence<uint32_t, num_fields>{});
 }
 }  // namespace ttnn::kernel_utils
 #endif
