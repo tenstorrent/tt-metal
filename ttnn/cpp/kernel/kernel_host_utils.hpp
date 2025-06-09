c0fe9ee00e3f3b28731a789f042361c9174ca717
Aliaksei Sala <aliaksei_sala@epam.com>
2025-06-09 12:12:14 -0700

    Rename KernelStructConcept and improve validation error message.

--- ttnn/cpp/kernel/kernel_host_utils.hpp
+++ ttnn/cpp/kernel/kernel_host_utils.hpp
@@ -9,7 +9,7 @@
 namespace ttnn::kernel_utils {
 
 template <typename KernelStruct, typename FieldType = uint32_t>
-concept KernelStructConcept =
+concept KernelArgsStructConcept =
     alignof(KernelStruct) == alignof(FieldType) && sizeof(KernelStruct) % sizeof(FieldType) == 0 &&
     std::is_aggregate_v<KernelStruct> && [] {
         bool result = true;
@@ -20,7 +20,7 @@ concept KernelStructConcept =
         return result;
     }();
 
-template <KernelStructConcept KernStruct>
+template <KernelArgsStructConcept KernStruct>
 std::vector<uint32_t> to_vector(const KernStruct& kernel_data) {
     std::vector<uint32_t> res(sizeof(KernStruct) / sizeof(uint32_t), 0);
     uint32_t idx = 0;
@@ -28,15 +28,17 @@ std::vector<uint32_t> to_vector(const KernStruct& kernel_data) {
     return res;
 }
 
-template <KernelStructConcept KernStruct>
+template <KernelArgsStructConcept KernStruct>
 consteval uint32_t amount_of_fields() {
     return sizeof(KernStruct) / sizeof(reflect::get<0>(KernStruct{}));
 }
 
 }  // namespace ttnn::kernel_utils
 
-#define VALIDATE_KERNEL_ARGS_STRUCT(KernStruct) \
-    static_assert(ttnn::kernel_utils::KernelStructConcept<KernStruct>, "All fields must have uint32_t type");
+#define VALIDATE_KERNEL_ARGS_STRUCT(KernStruct)                  \
+    static_assert(                                               \
+        ttnn::kernel_utils::KernelArgsStructConcept<KernStruct>, \
+        "Struct does not satisfy the requirements of KernelArgsStructConcept.");
 
 #else
 #define VALIDATE_KERNEL_ARGS_STRUCT(KernStruct)
