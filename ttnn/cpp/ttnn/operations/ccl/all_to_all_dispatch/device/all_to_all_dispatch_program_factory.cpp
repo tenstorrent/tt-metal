cd86431eb0146c6651a19ed7ae33af1ff7eb3ee9
Saad Jameel <sjameel@tenstorrent.com>
2025-06-09 16:15:18 +0000

    generalize page shape calculations

--- ttnn/cpp/ttnn/operations/ccl/all_to_all_dispatch/device/all_to_all_dispatch_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/ccl/all_to_all_dispatch/device/all_to_all_dispatch_program_factory.cpp
@@ -21,16 +21,52 @@
 namespace ttnn::operations::ccl {
 
 namespace detail {
-size_t get_num_pages(size_t buffer_size_bytes, size_t page_size_bytes) {
-    TT_FATAL(buffer_size_bytes > 0, "Buffer size must be greater than 0");
-    TT_FATAL(page_size_bytes > 0, "Page size must be greater than 0");
-    TT_FATAL(
-        buffer_size_bytes % page_size_bytes == 0,
-        "Buffer size {} must be multiple of page size {}",
-        buffer_size_bytes,
-        page_size_bytes);
-    return buffer_size_bytes / page_size_bytes;
+
+tt::tt_metal::Shape2D get_physical_size(const ttnn::Tensor& tensor) {
+    auto memory_config = tensor.memory_config();
+    auto tensor_spec = tensor.tensor_spec();
+    auto page_config = tensor_spec.page_config();
+    if (tensor.layout() == tt::tt_metal::Layout::TILE) {
+        return page_config.get_tile().get_tile_shape();
+    } else if (tensor.layout() == tt::tt_metal::Layout::ROW_MAJOR) {
+        if (memory_config.shard_spec().has_value()) {
+            return {1, memory_config.shard_spec().value().shape[1]};
+        } else {
+            return {1, tensor.padded_shape()[-1]};
+        }
+    } else {
+        TT_FATAL(false, "Invalid layout: neither tile nor row major");
+    }
 }
+
+uint32_t get_num_pages(const ttnn::Tensor& tensor) {
+    const auto& shape = tensor.padded_shape();
+    auto physical_size = get_physical_size(tensor);
+    auto num_pages = shape.volume() / (physical_size.height() * physical_size.width());
+    return num_pages;
+}
+
+uint32_t get_page_size(const ttnn::Tensor& tensor) {
+    auto memory_config = tensor.memory_config();
+    auto tensor_spec = tensor.tensor_spec();
+    auto page_config = tensor_spec.page_config();
+    auto physical_size = get_physical_size(tensor);
+    bool sharded = memory_config.shard_spec().has_value();
+
+    std::optional<tt::tt_metal::Shape2D> physical_shard_size =
+        sharded ? std::optional<tt::tt_metal::Shape2D>(memory_config.shard_spec().value().shape) : std::nullopt;
+    auto page_shape = page_config.get_page_shape(physical_size, tensor.dtype(), memory_config, physical_shard_size);
+    auto page_size = page_config.get_page_size_bytes(page_shape, tensor.dtype());
+    return (uint32_t)page_size;
+}
+
+uint32_t get_aligned_page_size(const ttnn::Tensor& tensor) {
+    auto BUFFER_ALIGNMENT = tensor.buffer()->buffer_type() == tt::tt_metal::BufferType::DRAM
+                                ? tt::tt_metal::hal::get_dram_alignment()
+                                : tt::tt_metal::hal::get_l1_alignment();
+    return tt::round_up(get_page_size(tensor), BUFFER_ALIGNMENT);
+}
+
 }  // namespace detail
 
 AllToAllDispatchDeviceOperation::AllToAllDispatchSparse::cached_mesh_workload_t
@@ -72,6 +108,10 @@ AllToAllDispatchDeviceOperation::AllToAllDispatchSparse::create_at(
 
     auto mesh_device = input_tensor.mesh_device();
     const auto& mesh_view = mesh_device->get_view();
+    auto src_device = mesh_device->get_device(mesh_coordinate);
+    auto src_physical_device_id = src_device->id();
+    // auto* control_plane = tt::tt_metal::MetalContext::instance().get_cluster().get_control_plane();
+    // auto src_fabric_node_id = control_plane->get_fabric_node_id_from_physical_chip_id(src_physical_device_id);
 
     auto input_shape = input_tensor.get_tensor_spec().logical_shape();
     auto indices_shape = indices_tensor.get_tensor_spec().logical_shape();
@@ -80,26 +120,31 @@ AllToAllDispatchDeviceOperation::AllToAllDispatchSparse::create_at(
     uint32_t num_devices = mesh_view.num_devices();
     uint32_t hidden_size = input_shape[-1];
     uint32_t batch_size = input_shape[0] * num_devices;
+    uint32_t batches_per_device = input_shape[0];
     uint32_t selected_experts_k = indices_shape[-1];
     uint32_t experts = mapping_shape[-2];
 
-    auto input_spec = input_tensor.get_tensor_spec();
-    auto indices_spec = indices_tensor.get_tensor_spec();
-    auto mapping_spec = mapping_tensor.get_tensor_spec();
-    auto output_spec = output_tensor.get_tensor_spec();
-    auto metadata_spec = metadata_tensor.get_tensor_spec();
+    auto input_page_size = detail::get_aligned_page_size(input_tensor);
+    auto indices_page_size = detail::get_aligned_page_size(indices_tensor);
+    auto mapping_page_size = detail::get_aligned_page_size(mapping_tensor);
+    auto output_page_size = detail::get_page_size(output_tensor);
+    auto metadata_page_size = detail::get_page_size(metadata_tensor);
 
-    auto input_page_size = input_spec.compute_page_size_bytes();
-    auto indices_page_size = indices_spec.compute_page_size_bytes();
-    auto mapping_page_size = mapping_spec.compute_page_size_bytes();
-    auto output_page_size = output_spec.compute_page_size_bytes();
-    auto metadata_page_size = metadata_spec.compute_page_size_bytes();
-
-    auto input_pages = detail::get_num_pages(input_spec.compute_packed_buffer_size_bytes(), input_page_size);
-    auto indices_pages = detail::get_num_pages(indices_spec.compute_packed_buffer_size_bytes(), indices_page_size);
-    auto mapping_pages = detail::get_num_pages(mapping_spec.compute_packed_buffer_size_bytes(), mapping_page_size);
-    auto output_pages = detail::get_num_pages(output_spec.compute_packed_buffer_size_bytes(), output_page_size);
-    auto metadata_pages = detail::get_num_pages(metadata_spec.compute_packed_buffer_size_bytes(), metadata_page_size);
+    auto input_pages = detail::get_num_pages(input_tensor);
+    // tt::log_info(tt::LogAlways, "input shape: {}, input_pages: {}, input_page_size: {}",
+    // input_tensor.logical_shape(), input_pages, input_page_size);
+    auto indices_pages = detail::get_num_pages(indices_tensor);
+    // tt::log_info(tt::LogAlways, "indices shape: {}, indices_pages: {}, indices_page_size: {}",
+    // indices_tensor.logical_shape(), indices_pages, indices_page_size);
+    auto mapping_pages = detail::get_num_pages(mapping_tensor);
+    // tt::log_info(tt::LogAlways, "mapping shape: {}, mapping_pages: {}, mapping_page_size: {}",
+    // mapping_tensor.logical_shape(), mapping_pages, mapping_page_size);
+    auto output_pages = detail::get_num_pages(output_tensor);
+    // tt::log_info(tt::LogAlways, "output shape: {}, output_pages: {}, output_page_size: {}",
+    // output_tensor.logical_shape(), output_pages, output_page_size);
+    auto metadata_pages = detail::get_num_pages(metadata_tensor);
+    // tt::log_info(tt::LogAlways, "metadata shape: {}, metadata_pages: {}, metadata_page_size: {}",
+    // metadata_tensor.logical_shape(), metadata_pages, metadata_page_size);
 
     auto input_data_format = tt::tt_metal::datatype_to_dataformat_converter(input_tensor.get_dtype());
     auto indices_data_format = tt::tt_metal::datatype_to_dataformat_converter(indices_tensor.get_dtype());
@@ -116,6 +161,7 @@ AllToAllDispatchDeviceOperation::AllToAllDispatchSparse::create_at(
     // client interface
     uint32_t client_interface_cb_id = tt::CBIndex::c_3;
     // metadata buffer
+    uint32_t send_preparation_buffer_id = tt::CBIndex::c_4;
 
     tt::tt_metal::CircularBufferConfig cb_input_tensor_config =
         tt::tt_metal::CircularBufferConfig(
@@ -124,18 +170,25 @@ AllToAllDispatchDeviceOperation::AllToAllDispatchSparse::create_at(
 
     tt::tt_metal::CircularBufferConfig cb_indices_tensor_config =
         tt::tt_metal::CircularBufferConfig(
-            indices_spec.compute_packed_buffer_size_bytes(), {{indices_tensor_cb_id, indices_data_format}})
+            indices_page_size * indices_pages, {{indices_tensor_cb_id, indices_data_format}})
             .set_page_size(indices_tensor_cb_id, indices_page_size);
 
     tt::tt_metal::CircularBufferConfig cb_mapping_tensor_config =
         tt::tt_metal::CircularBufferConfig(
-            mapping_spec.compute_packed_buffer_size_bytes(), {{mapping_tensor_cb_id, mapping_data_format}})
+            mapping_page_size * mapping_pages, {{mapping_tensor_cb_id, mapping_data_format}})
             .set_page_size(mapping_tensor_cb_id, mapping_page_size);
 
     tt::tt_metal::CircularBufferConfig client_interface_cb_config =
         tt::tt_metal::CircularBufferConfig(
             tt::tt_fabric::CLIENT_INTERFACE_SIZE, {{client_interface_cb_id, tt::DataFormat::UInt32}})
             .set_page_size(client_interface_cb_id, tt::tt_fabric::CLIENT_INTERFACE_SIZE);
+
+    uint32_t send_preparation_buffer_size = num_devices * batches_per_device * sizeof(uint16_t);
+
+    tt::tt_metal::CircularBufferConfig send_preparation_buffer_config =
+        tt::tt_metal::CircularBufferConfig(
+            send_preparation_buffer_size, {{send_preparation_buffer_id, tt::DataFormat::UInt16}})
+            .set_page_size(send_preparation_buffer_id, send_preparation_buffer_size);
 
     auto subdevice_core_range_set =
         mesh_device->worker_cores(tt::tt_metal::HalProgrammableCoreType::TENSIX, operation_attributes.subdevice_id);
@@ -148,7 +201,6 @@ AllToAllDispatchDeviceOperation::AllToAllDispatchSparse::create_at(
         num_links);
 
     std::vector<CoreCoord> sender_cores;
-
     // select
     for (uint32_t i = 0; i < num_links; i++) {
         sender_cores.push_back(subdevice_cores.at(i));
@@ -162,12 +214,15 @@ AllToAllDispatchDeviceOperation::AllToAllDispatchSparse::create_at(
     auto indices_tensor_cb = tt::tt_metal::CreateCircularBuffer(program, sender_core, cb_indices_tensor_config);
     auto mapping_tensor_cb = tt::tt_metal::CreateCircularBuffer(program, sender_core, cb_mapping_tensor_config);
     auto client_interface_cb = tt::tt_metal::CreateCircularBuffer(program, sender_core, client_interface_cb_config);
+    auto send_preparation_buffer =
+        tt::tt_metal::CreateCircularBuffer(program, sender_core, send_preparation_buffer_config);
 
     std::vector<uint32_t> reader_compile_time_args = {
         input_tensor_cb_id,
         indices_tensor_cb_id,
         mapping_tensor_cb_id,
         client_interface_cb_id,
+        send_preparation_buffer_id,
         input_pages,
         indices_pages,
         mapping_pages,
