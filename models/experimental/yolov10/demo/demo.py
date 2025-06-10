5b29eb0fdc678631b316c36ff5fa62db555fee8f
keerthana-r-mcw <keerthana.rajendran@multicorewareinc.com>
2025-06-10 12:54:16 +0000

    #17935: Yolov10x demo with trace 2cq

--- models/experimental/yolov10/demo/demo.py
+++ models/experimental/yolov10/demo/demo.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 
 # SPDX-License-Identifier: Apache-2.0
 
@@ -18,20 +18,18 @@ from models.experimental.yolov10.demo.demo_utils import (
     save_yolo_predictions_by_model,
 )
 from models.experimental.yolov10.reference.yolov10 import YOLOv10
-from models.experimental.yolov10.tt.model_preprocessing import (
-    create_yolov10x_input_tensors,
-    create_yolov10x_model_parameters,
-)
-from models.experimental.yolov10.tt.yolov10 import TtnnYolov10
 from models.utility_functions import disable_persistent_kernel_cache, run_for_wormhole_b0
+from models.experimental.yolov10.tt.performant_runner import YOLOv10PerformantRunner
 
 
 @run_for_wormhole_b0()
-@pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
+@pytest.mark.parametrize(
+    "device_params", [{"l1_small_size": 79104, "trace_region_size": 23887872, "num_command_queues": 2}], indirect=True
+)
 @pytest.mark.parametrize(
     "source",
     [
-        "models/sample_data/huggingface_cat_image.jpg",
+        "models/experimental/yolov10/demo/images/dog.jpg",
     ],
 )
 @pytest.mark.parametrize(
@@ -48,7 +46,7 @@ from models.utility_functions import disable_persistent_kernel_cache, run_for_wo
         "True",
     ],
 )
-def test_demo_ttnn(device, source, model_type, use_pretrained_weight):
+def test_demo_ttnn(device, use_program_cache, source, model_type, use_pretrained_weight):
     disable_persistent_kernel_cache()
 
     if model_type == "torch_model":
@@ -70,49 +68,39 @@ def test_demo_ttnn(device, source, model_type, use_pretrained_weight):
             torch_model.eval()
             model = torch_model
             logger.info("Inferencing [Torch] Model")
-    else:
-        torch_input, ttnn_input = create_yolov10x_input_tensors(device)
-        state_dict = None
-        if use_pretrained_weight:
-            torch_model = YOLO("yolov10x.pt")
-            state_dict = torch_model.state_dict()
+        save_dir = "models/experimental/yolov10/demo/runs"
+        dataset = LoadImages(path=source)
+        model_save_dir = os.path.join(save_dir, model_type)
+        os.makedirs(model_save_dir, exist_ok=True)
+        names = load_coco_class_names()
 
-        torch_model = YOLOv10()
-        state_dict = torch_model.state_dict() if state_dict is None else state_dict
-        ds_state_dict = {k: v for k, v in state_dict.items()}
-        new_state_dict = {}
-        for (name1, parameter1), (name2, parameter2) in zip(torch_model.state_dict().items(), ds_state_dict.items()):
-            if isinstance(parameter2, torch.FloatTensor):
-                new_state_dict[name1] = parameter2
-        torch_model.load_state_dict(new_state_dict)
-        torch_model.eval()
-        parameters = create_yolov10x_model_parameters(torch_model, torch_input, device=device)
-        model = TtnnYolov10(device, parameters, conv_pt=parameters)
-
-    save_dir = "models/demos/yolov10/demo/runs"
-    dataset = LoadImages(path=source)
-    model_save_dir = os.path.join(save_dir, model_type)
-    os.makedirs(model_save_dir, exist_ok=True)
-    names = load_coco_class_names()
-
-    for batch in dataset:
-        paths, im0s, s = batch
-        im = preprocess(im0s)
-        if model_type == "torch_model":
+        for batch in dataset:
+            paths, im0s, s = batch
+            im = preprocess(im0s)
             preds = model(im)
-        else:
+            results = postprocess(preds, im, im0s, batch, names)[0]
+            save_yolo_predictions_by_model(results, save_dir, source, model_type)
+    else:
+        save_dir = "models/experimental/yolov10/demo/runs"
+        dataset = LoadImages(path=source)
+        model_save_dir = os.path.join(save_dir, model_type)
+        os.makedirs(model_save_dir, exist_ok=True)
+        names = load_coco_class_names()
+
+        for batch in dataset:
+            paths, im0s, s = batch
+            im = preprocess(im0s)
             img = torch.permute(im, (0, 2, 3, 1))
-            img = img.reshape(
-                1,
-                1,
-                img.shape[0] * img.shape[1] * img.shape[2],
-                img.shape[3],
+            model = YOLOv10PerformantRunner(
+                device,
+                act_dtype=ttnn.bfloat8_b,
+                weight_dtype=ttnn.bfloat8_b,
+                torch_input_tensor=im,
             )
-            ttnn_im = ttnn.from_torch(img, layout=ttnn.TILE_LAYOUT, dtype=ttnn.bfloat8_b, device=device)
-            preds = model(ttnn_im)
+            preds = model.run(img)
             preds = ttnn.to_torch(preds, dtype=torch.float32)
 
-        results = postprocess(preds, im, im0s, batch, names)[0]
-        save_yolo_predictions_by_model(results, save_dir, source, model_type)
+            results = postprocess(preds, im, im0s, batch, names)[0]
+            save_yolo_predictions_by_model(results, save_dir, source, model_type)
 
     logger.info("Inference done")
