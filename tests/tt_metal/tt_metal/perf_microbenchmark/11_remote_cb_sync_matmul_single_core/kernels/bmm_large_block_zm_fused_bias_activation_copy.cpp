ae79947390be662bd2243574245bdf7c7707db6d
Nikola Cvetkovic <ncvetkovic@tenstorrent.com>
2025-06-10 11:08:39 +0000

    #22941: Clean up pack_untilize.h

--- tests/tt_metal/tt_metal/perf_microbenchmark/11_remote_cb_sync_matmul_single_core/kernels/bmm_large_block_zm_fused_bias_activation_copy.cpp
+++ tests/tt_metal/tt_metal/perf_microbenchmark/11_remote_cb_sync_matmul_single_core/kernels/bmm_large_block_zm_fused_bias_activation_copy.cpp
@@ -72,7 +72,7 @@ void MAIN {
         cb_reserve_back(out_cb_id, out_block_num_tiles);
         tile_regs_wait();
         uint32_t start_dst_index = 0;
-        matmul_pack_tile(start_dst_index, out_cb_id, out_block_num_tiles);
+        pack_tile_block(start_dst_index, out_cb_id, out_block_num_tiles);
         tile_regs_release();
         cb_push_back(out_cb_id, out_block_num_tiles);
     }
