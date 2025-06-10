7a78c33fee661bbbfc9b4fd89b4b49edb8fd6a8e
lpremovicTT <lpremovic@tenstorrent.com>
2025-06-10 14:24:48 +0000

    #21624: Fix DST_ACCUM_MODE in fast tilize

--- tt_metal/include/compute_kernel_api/tilize.h
+++ tt_metal/include/compute_kernel_api/tilize.h
@@ -229,8 +229,8 @@ ALWI void fast_tilize_init_short(uint32_t icb, uint32_t full_dim, uint32_t ocb) 
 }
 
 ALWI void fast_tilize_init_short_with_dt(uint32_t icb, uint32_t full_dim, uint32_t ocb) {
-    UNPACK((llk_unpack_reconfig_data_format(icb, icb)));
-    MATH((llk_math_reconfig_data_format(icb, icb)));
+    UNPACK((llk_unpack_reconfig_data_format<DST_ACCUM_MODE>(icb, icb)));
+    MATH((llk_math_reconfig_data_format<DST_ACCUM_MODE>(icb, icb)));
 
     fast_tilize_init_short(icb, full_dim, ocb);
 }
