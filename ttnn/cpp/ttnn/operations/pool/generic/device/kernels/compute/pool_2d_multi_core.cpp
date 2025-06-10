79045f65efbc1d4e41f4b98872d033a7a9327b02
Dragica Stoiljkovic <dstoiljkovic@tenstorrent.com>
2025-06-10 12:26:31 +0000

    Additional comment

--- ttnn/cpp/ttnn/operations/pool/generic/device/kernels/compute/pool_2d_multi_core.cpp
+++ ttnn/cpp/ttnn/operations/pool/generic/device/kernels/compute/pool_2d_multi_core.cpp
@@ -118,7 +118,6 @@ void MAIN {
                 in_cb_id_0, curr_scalar_cb_id, max_tiles_per_iter, num_faces_in_tile, face_r_dim, 1)));
         }
 
-        //  perform the reduction over the first N - 1 whole chunks
         for (uint32_t b_i = 0; b_i < in_nblocks_c - 1; ++b_i) {
             reduce_h_fused<
                 max_tiles_per_iter,
