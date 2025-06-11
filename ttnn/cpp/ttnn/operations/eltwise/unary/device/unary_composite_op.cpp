1f27a20d9f94ac675499280bcc838bb454709a2b
mouliraj-mcw <ctr-mcw-melamurugan@ext.tenstorrent.com>
2025-06-11 05:21:20 +0000

    #23170: Update angle conversion ops

--- ttnn/cpp/ttnn/operations/eltwise/unary/device/unary_composite_op.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/unary/device/unary_composite_op.cpp
@@ -26,16 +26,6 @@
 #include "ttnn/operations/data_movement/fill_pad/fill_pad.hpp"
 namespace ttnn::operations::unary {
 
-Tensor _deg2rad(const Tensor& input_tensor, const std::optional<MemoryConfig>& output_mem_config) {
-    return ttnn::multiply(
-        input_tensor, (float)(M_PI / 180.0), std::nullopt, output_mem_config.value_or(input_tensor.memory_config()));
-}
-
-Tensor _rad2deg(const Tensor& input_tensor, const std::optional<MemoryConfig>& output_mem_config) {
-    return ttnn::multiply(
-        input_tensor, (float)(180.0 / M_PI), std::nullopt, output_mem_config.value_or(input_tensor.memory_config()));
-}
-
 // acosh(x) = log(x + sqrt(x^2 - 1))
 Tensor _acosh(const Tensor& input_a, const std::optional<MemoryConfig>& output_mem_config) {
     TT_FATAL(input_a.storage_type() == StorageType::DEVICE, "Unary operation requires input to be on Device.");
