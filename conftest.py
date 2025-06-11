2eb0f74bcc7a497697aee582d2d1630ab996a302
Adrian F. Morrison <amorrison@tenstorrent.com>
2025-06-11 14:19:29 +0000

    remove the subdevice scheme. A couple of other fixes

--- conftest.py
+++ conftest.py
@@ -371,7 +371,6 @@ def set_fabric(fabric_config):
         ttnn.set_fabric_config(fabric_config)
 
 
-
 @pytest.fixture(scope="function")
 def mesh_device(request, silicon_arch_name, device_params):
     """
@@ -425,9 +424,8 @@ def mesh_device(request, silicon_arch_name, device_params):
         ttnn.close_mesh_device(submesh)
 
     ttnn.close_mesh_device(mesh_device)
-    # !TODO figure out why this line triggers hangs for subsequent tests
-    del mesh_device
     reset_fabric(fabric_config)
+    del mesh_device
 
 
 @pytest.fixture(scope="function")
--- ttnn/cpp/ttnn/operations/point_to_point/device/kernels/dataflow/writer_unary_interleaved_start_id_gen.cpp
+++ ttnn/cpp/ttnn/operations/point_to_point/device/kernels/dataflow/writer_unary_interleaved_start_id_gen.cpp
@@ -12,30 +12,21 @@ void kernel_main() {
     constexpr uint32_t cb_id_out = get_compile_time_arg_val(0);
     constexpr bool dst_is_dram = get_compile_time_arg_val(1) == 1;
 
-#ifdef OUT_SHARDED
-    cb_wait_front(cb_id_out, num_tiles);
-#else
-
     // single-tile ublocks
     constexpr uint32_t onetile = 1;
-    const uint32_t tile_bytes = get_tile_size(cb_id_out);
-    const DataFormat data_format = get_dataformat(cb_id_out);
 
-    const InterleavedAddrGenFast<dst_is_dram> s = {
-        .bank_base_address = dst_addr, .page_size = tile_bytes, .data_format = data_format};
+    const uint32_t page_bytes = get_arg_val<uint32_t>(3);
+    const InterleavedAddrGen<dst_is_dram> s = {.bank_base_address = dst_addr, .page_size = page_bytes};
 
-#ifdef BACKWARDS
-    uint32_t end_id = start_id - num_tiles;
-    for (uint32_t i = start_id; i != end_id; --i) {
-#else
     uint32_t end_id = start_id + num_tiles;
     for (uint32_t i = start_id; i < end_id; ++i) {
-#endif
         cb_wait_front(cb_id_out, onetile);
         uint32_t l1_read_addr = get_read_ptr(cb_id_out);
-        noc_async_write_tile(i, s, l1_read_addr);
+
+        const uint64_t dst_noc_addr = get_noc_addr(i, s);
+        noc_async_write(l1_read_addr, dst_noc_addr, s.page_size);
+
         noc_async_write_barrier();
         cb_pop_front(cb_id_out, onetile);
     }
-#endif
 }
