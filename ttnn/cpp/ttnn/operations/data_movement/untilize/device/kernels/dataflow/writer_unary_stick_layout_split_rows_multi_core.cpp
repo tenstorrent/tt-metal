f4106f9a574b07e67064e44a21d15fb659aaab62
grechsteinerTT <grechsteiner@tenstorrent.com>
2025-06-11 18:42:51 +0000

    Initial writer kernel simplification

--- ttnn/cpp/ttnn/operations/data_movement/untilize/device/kernels/dataflow/writer_unary_stick_layout_split_rows_multi_core.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/untilize/device/kernels/dataflow/writer_unary_stick_layout_split_rows_multi_core.cpp
@@ -45,6 +45,13 @@ void kernel_main() {
         dst_addr, output_stick_size, output_log_base_2_of_page_size);
 #endif
 
+    // Index of the column that we are going to start writing elements to (relative to the entire tensor)
+    uint32_t write_col_index = width_wise_input_block_index * num_cols_per_input_block;
+
+    /*
+    uint32_t output_page_id_within_row = (write_col_index) / num_cols_per_output_block;
+    */
+
     auto write_tiles_in_current_block = [&](uint32_t block_height_index) {
         cb_wait_front(cb_id_out0, num_tiles_per_input_block);
 
@@ -55,46 +62,77 @@ void kernel_main() {
         // easily increment the current_read_addr to the next row of elements.
         uint32_t base_l1_read_addr = get_read_ptr(cb_id_out0);
 
+        // Output page_id that we are going to be writing to
+        /*
+        uint32_t num_pages_already_processed_in_previous_rows = block_height_index * tile_height *
+        num_output_blocks_across_width; uint32_t output_page_id = num_pages_already_processed_in_previous_rows +
+        output_page_id_within_row;
+        */
+
         // Process each row of elements in the input block
         for (uint32_t j = 0; j < tile_height; ++j) {
             uint32_t current_l1_read_addr = base_l1_read_addr + j * num_cols_per_input_block * output_element_size;
-            uint32_t num_rows_already_processed = block_height_index * tile_height + j;
 
-            // For width or block sharding, the input tensor may have more/less shards width wise compared
-            // to the output tensor. In the less case, different sequences of the row in the input block
-            // may map to different output shards. In both cases, we may be writing to the middle of an output page.
+            // For width or block sharding (either input, output, or both), there may be more/less blocks width wise in
+            // the output tensor compared to the input tensor. As a result, for the first output block we write to,
+            // we may be writing to the middle of a page (i.e. some byte offset within the page). For all following
+            // writes we'll be writing to the beginning of a page and not require an offset.
+
+            // Output page_id that we are going to be writing to
+            uint32_t num_rows_already_processed = block_height_index * tile_height + j;
+            uint32_t num_pages_already_processed_in_previous_rows =
+                num_rows_already_processed * num_output_blocks_across_width;
+            uint32_t output_page_id_within_row = (write_col_index) / num_cols_per_output_block;
+            uint32_t output_page_id = num_pages_already_processed_in_previous_rows + output_page_id_within_row;
+
+            // For the first output page that we write to in the current row, it's first x columns may have been
+            // written to from a different input block. So we need to calculate the offset within this first output page
+            // to write to (which may be 0). We also need to determine how many columns in this output page have not yet
+            // been written to in order to determine how many columns from the input block to writer (min of all
+            // remaining columns in the input block, and the remaining unprocessed columns in the current output block).
+            uint32_t num_cols_already_processed_in_output_block = write_col_index % num_cols_per_output_block;
+            uint32_t num_cols_remaining_in_current_output_block =
+                num_cols_per_output_block - num_cols_already_processed_in_output_block;
+            uint32_t output_offset_within_page_in_bytes =
+                num_cols_already_processed_in_output_block * output_element_size;
+
+            // Iterate through all columns in the input block that this core is processing
             uint32_t num_input_cols_processed = 0;
             while (num_input_cols_processed < num_unpadded_cols_per_input_block) {
-                // Index of the column that we are going to start writing elements to (relative to the entire tensor)
-                uint32_t write_col_index =
-                    width_wise_input_block_index * num_cols_per_input_block + num_input_cols_processed;
-
-                // page_id to write to
-                uint32_t output_page_id_within_row = write_col_index / num_cols_per_output_block;
-                uint32_t output_page_id =
-                    num_rows_already_processed * num_output_blocks_across_width + output_page_id_within_row;
-
-                // Memory offset within page we're writing to
-                uint32_t num_cols_already_processed_in_output_block = write_col_index % num_cols_per_output_block;
-                uint32_t output_offset_within_page_in_bytes =
-                    num_cols_already_processed_in_output_block * output_element_size;
-
                 // How many elements to write from the input block to the output block.
                 // Min of the number of remaining unprocessed columns in the input block
                 // and the number of remaining unprocessed columns in the current output block.
                 uint32_t num_cols_to_write = std::min(
                     num_unpadded_cols_per_input_block - num_input_cols_processed,
-                    num_cols_per_output_block - num_cols_already_processed_in_output_block);
+                    num_cols_remaining_in_current_output_block);
                 uint32_t num_bytes_to_write = num_cols_to_write * output_element_size;
 
                 // Perform the write
                 uint64_t dst_noc_addr = get_noc_addr(output_page_id, s, output_offset_within_page_in_bytes);
                 noc_async_write(current_l1_read_addr, dst_noc_addr, num_bytes_to_write);
 
-                // Update number of columns processed and increment the read ptr
+                // Increment the number of cols we've processed in the input block
                 num_input_cols_processed += num_cols_to_write;
+
+                // Increment l1_read_addr past the bytes we just read
                 current_l1_read_addr += num_bytes_to_write;
+
+                // Increment the output_page_id we're writing to. If we wrote the entire input block
+                // then this has no effect as the while loop terminates. If we wrote to a subset of the
+                // input block, then that subset corresponds to an entire output block, so we increment
+                // the output_page_id to the following output block.
+                output_page_id++;
+
+                // Only the first output block we write to can have some of it's columns already processed/written-to
+                num_cols_remaining_in_current_output_block = num_cols_per_output_block;
+                output_offset_within_page_in_bytes = 0;
             }
+
+            // Output page_id that we are going to be writing to
+            /*
+            uint32_t num_pages_already_processed_in_previous_rows += num_output_blocks_across_width;
+            uint32_t output_page_id = num_pages_already_processed_in_previous_rows + output_page_id_within_row;
+            */
         }
 
         noc_async_write_barrier();
