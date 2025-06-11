1dc5c2dfafc6880cd9978dd3d25f648e51be5a2d
Jason Davies <jason@jasondavies.com>
2025-06-11 12:11:25 +0000

    Second attempt: add support for int32 to transpose_wh_tile.

--- tests/tt_metal/tt_metal/test_kernels/compute/transpose_wh_dest.cpp
+++ tests/tt_metal/tt_metal/test_kernels/compute/transpose_wh_dest.cpp
@@ -4,29 +4,30 @@
 
 #include <cstdint>
 
-#include "compute_kernel_api/transpose_wh.h"
+#include "compute_kernel_api/tile_move_copy.h"
+#include "compute_kernel_api/transpose_wh_dest.h"
 #include "compute_kernel_api/eltwise_unary/eltwise_unary.h"
 
 namespace NAMESPACE {
 void MAIN {
     uint32_t NHtWt = get_compile_time_arg_val(0);
-#ifndef SHORT_INIT
-    transpose_wh_init(tt::CBIndex::c_0, tt::CBIndex::c_16);
-#else
+
     unary_op_init_common(tt::CBIndex::c_0, tt::CBIndex::c_16);
-    transpose_wh_init_short(tt::CBIndex::c_0);
-#endif
 
     // transpose a row-major block:
     // - assumes the tiles come in in column major order from reader
     // - uses reader_unary_transpose_wh
-    // - transpose_wh each tile
+    // - transpose_wh_dest each tile
     for (uint32_t n = 0; n < NHtWt; n++) {
         cb_wait_front(tt::CBIndex::c_0, 1);
         cb_reserve_back(tt::CBIndex::c_16, 1);
 
         tile_regs_acquire();
-        transpose_wh_tile(tt::CBIndex::c_0, 0, 0);
+        copy_tile_init(tt::CBIndex::c_0);
+        copy_tile(tt::CBIndex::c_0, 0, 0);
+
+        transpose_wh_dest_init_short();
+        transpose_wh_dest(0);
         tile_regs_commit();
 
         tile_regs_wait();
