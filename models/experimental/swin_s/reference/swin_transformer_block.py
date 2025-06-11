0e101ad2dbd96a1f58002d51d3879dae47d708c3
Harini Mohan <164483522+HariniMohan0102@users.noreply.github.com>
2025-06-11 07:14:07 +0530

    #17785: Swin_S model ttnn bringup  (#18963)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/17785)
    
    ### Problem description
    Add ttnn support for the Swin_S model.
    
    ### What's changed
    
    - Added reference for Swin_S.
    - Added ttnn support for the model.
    - Added ttnn roll op support
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml):
    [CI
    Passed](https://github.com/tenstorrent/tt-metal/actions/runs/15553494402)
    - [x] [Integration Tests
    ](https://github.com/tenstorrent/tt-metal/actions/workflows/fast-dispatch-full-regressions-and-models.yaml):
    [CI
    Passed](https://github.com/tenstorrent/tt-metal/actions/runs/15553501752)
    
    Co-authored-by: Mohamed Bahnas <116673264+mbahnasTT@users.noreply.github.com>

---
+++ models/experimental/swin_s/reference/swin_transformer_block.py
@@ -0,0 +1,48 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+from torch import nn, Tensor
+from typing import Callable, List
+from models.experimental.swin_s.reference.shifted_window_attention import ShiftedWindowAttention
+from models.experimental.swin_s.reference.mlp import MLP
+
+
+class SwinTransformerBlock(nn.Module):
+    def __init__(
+        self,
+        dim: int,
+        num_heads: int,
+        window_size: List[int],
+        shift_size: List[int],
+        mlp_ratio: float = 4.0,
+        dropout: float = 0.0,
+        attention_dropout: float = 0.0,
+        stochastic_depth_prob: float = 0.0,
+        norm_layer: Callable[..., nn.Module] = nn.LayerNorm,
+        attn_layer: Callable[..., nn.Module] = ShiftedWindowAttention,
+    ):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = attn_layer(
+            dim,
+            window_size,
+            shift_size,
+            num_heads,
+            attention_dropout=attention_dropout,
+            dropout=dropout,
+        )
+
+        self.norm2 = norm_layer(dim)
+        self.mlp = MLP(dim, [int(dim * mlp_ratio), dim], activation_layer=nn.GELU, inplace=None, dropout=dropout)
+
+        for m in self.mlp.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    nn.init.normal_(m.bias, std=1e-6)
+
+    def forward(self, x: Tensor):
+        x = x + self.attn(self.norm1(x))
+        x = x + self.mlp(self.norm2(x))
+        return x
