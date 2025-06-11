bce12ef5ed233f5e5fe9ae778e43da59cf9755f7
Edwin Lee <edwinlee@tenstorrent.com>
2025-06-11 12:36:47 -0400

    Isolating out to smaller testcase for reproducing the issue

--- tests/ttnn/unit_tests/operations/fused/test_group_norm_DRAM.py
+++ tests/ttnn/unit_tests/operations/fused/test_group_norm_DRAM.py
@@ -14,42 +14,43 @@ from tests.ttnn.utils_for_testing import assert_with_pcc, check_with_pcc
 from models.utility_functions import skip_for_wormhole_b0, skip_for_blackhole
 
 
-#@skip_for_blackhole("Fails on Blackhole. Issue #20913")
+# @skip_for_blackhole("Fails on Blackhole. Issue #20913")
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 0}], indirect=True)
 @pytest.mark.parametrize(
     "N, C, H, W, num_groups, num_out_blocks, cores_y, cores_x",
     [
-        (8, 768, 1, 512, 32, 2, 8, 8),  # base case
-        (9, 768, 1, 512, 32, 2, 8, 8),  # test batch size 9 (uneven batch sizes)
-        (1, 768, 1, 512, 32, 2, 8, 8),  # test group channel count is less than tile size
-        (1, 480, 1, 64, 8, 1, 1, 1),  # test last group ends less than max tile span
-        (1, 2560, 1, 512, 32, 2, 8, 8),  # test mcast num_out_blocks 2
-        (1, 2560, 1, 1024, 32, 4, 8, 8),  # test mcast num_out_blocks 4
-        (1, 768, 1, 512, 32, 2, 8, 8),  # test group channel count is less than tile size
-        (2, 768, 1, 512, 32, 2, 8, 8),  # test batch size 2 (still multicast)
-        (8, 768, 1, 512, 32, 2, 8, 8),  # test batch size 8 (no multicast)
-        (8, 768, 1, 512, 32, 3, 8, 8),  # test batch size 8 (no multicast), but uneven num_out_blocks divisor
-        (9, 768, 1, 512, 32, 2, 8, 8),  # test batch size 9 (uneven batch sizes)
-        (
-            1,
-            128,
-            1,
-            512,
-            32,
-            2,
-            4,
-            4,
-        ),  # test all groups on core fit in less than one tile, so need to reduce col core count
-        # # # SDXL 1024x1024 resoultion
-        (1, 640, 128, 128, 32, 3, 4, 4),
-        (1, 960, 128, 128, 32, 6, 2, 2),
-        # VAE
-        # tensor is too large, but good example
-        (1, 256, 1024, 1024, 32, 128, 8, 4),
-        (1, 256, 515, 512, 32, 32, 4, 4),
-        (1, 512, 128, 128, 32, 4, 1, 4),
-        (1, 512, 256, 256, 32, 16, 4, 4),
-        (1, 512, 512, 512, 32, 32, 4, 4),
+        (1, 32, 1, 32, 32, 1, 1, 1),  # base case
+        # (8, 768, 1, 512, 32, 2, 8, 8),  # base case
+        # (9, 768, 1, 512, 32, 2, 8, 8),  # test batch size 9 (uneven batch sizes)
+        # (1, 768, 1, 512, 32, 2, 8, 8),  # test group channel count is less than tile size
+        # (1, 480, 1, 64, 8, 1, 1, 1),  # test last group ends less than max tile span
+        # (1, 2560, 1, 512, 32, 2, 8, 8),  # test mcast num_out_blocks 2
+        # (1, 2560, 1, 1024, 32, 4, 8, 8),  # test mcast num_out_blocks 4
+        # (1, 768, 1, 512, 32, 2, 8, 8),  # test group channel count is less than tile size
+        # (2, 768, 1, 512, 32, 2, 8, 8),  # test batch size 2 (still multicast)
+        # (8, 768, 1, 512, 32, 2, 8, 8),  # test batch size 8 (no multicast)
+        # (8, 768, 1, 512, 32, 3, 8, 8),  # test batch size 8 (no multicast), but uneven num_out_blocks divisor
+        # (9, 768, 1, 512, 32, 2, 8, 8),  # test batch size 9 (uneven batch sizes)
+        # (
+        #     1,
+        #     128,
+        #     1,
+        #     512,
+        #     32,
+        #     2,
+        #     4,
+        #     4,
+        # ),  # test all groups on core fit in less than one tile, so need to reduce col core count
+        # # # # SDXL 1024x1024 resoultion
+        # (1, 640, 128, 128, 32, 3, 4, 4),
+        # (1, 960, 128, 128, 32, 6, 2, 2),
+        # # VAE
+        # # tensor is too large, but good example
+        # (1, 256, 1024, 1024, 32, 128, 8, 4),
+        # (1, 256, 515, 512, 32, 32, 4, 4),
+        # (1, 512, 128, 128, 32, 4, 1, 4),
+        # (1, 512, 256, 256, 32, 16, 4, 4),
+        # (1, 512, 512, 512, 32, 32, 4, 4),
     ],
 )
 def test_group_norm_DRAM(device, N, C, H, W, num_groups, num_out_blocks, cores_y, cores_x):
@@ -123,6 +124,8 @@ def test_group_norm_DRAM(device, N, C, H, W, num_groups, num_out_blocks, cores_y
     )
 
     # output tensor
+    ttnn.synchronize_device(device)
+    print("Fetching the output tensor")
     output_tensor = ttnn.from_device(output_tensor)
     output_tensor = ttnn.to_torch(output_tensor)
 
