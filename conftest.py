5654670a0cea63b0c81001efa0b867cd382201b2
Diego Gomez <dgomez@tenstorrent.com>
2025-06-11 12:19:18 -0400

    Clean cache on test before enabiling it by default

--- conftest.py
+++ conftest.py
@@ -601,6 +601,8 @@ def use_program_cache(request):
     if not devices:
         logger.warning("No device fixture found to apply program cache to: PROGRAM CACHE DISABLED")
     for dev in devices:
+        # Clean up before reenabling program cache
+        dev.disable_and_clear_program_cache()
         dev.enable_program_cache()
     yield
     for dev in devices:
