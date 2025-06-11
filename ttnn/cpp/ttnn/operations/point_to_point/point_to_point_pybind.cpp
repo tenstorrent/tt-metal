862e5830ae20a69061afae4e743577bf3f3ca730
Adrian F. Morrison <amorrison@tenstorrent.com>
2025-06-11 16:05:41 +0000

    asserts->fatals, add issue number for todo

--- ttnn/cpp/ttnn/operations/point_to_point/point_to_point_pybind.cpp
+++ ttnn/cpp/ttnn/operations/point_to_point/point_to_point_pybind.cpp
@@ -14,9 +14,31 @@ namespace py = pybind11;
 
 void py_bind_point_to_point(py::module& module) {
     auto doc =
-        R"doc(
-        ! TODO
-        )doc";
+        R"doc(point_to_point(input_tensor: ttnn.Tensor, send_coord: ttnn.MeshCoordinate, receive_coord: ttnn.MeshCoordinate, topology: ttnn.Topology, receiver_semaphore: ttnn.GlobalSemaphore) -> ttnn.Tensor
+
+            Point-to-point send receive Op. Send a tensor from one device to another.
+
+            Args:
+                input_tensor (ttnn.Tensor): the input tensor.
+                send_coord (ttnn.MeshCoordinate): Coordinate of device containing input_tensor (shard).
+                receive_coord (ttnn.MeshCoordinate): Coordinate of device receiving input_tensor (shard).
+                topology (ttnn.Topology): Fabric topology.
+                receiver_semaphore (ttnn.GlobalSemaphore): Semaphore allocated on receiving device
+
+            Keyword Args:
+                queue_id (int, optional): command queue id. Defaults to `0`.
+
+           Returns:
+               ttnn.Tensor: the output tensor, with transferred shard on receiving device.
+
+            Example:
+
+                >>> sent_tensor = ttnn.point_to_point(
+                        input_tensor,
+                        coord0,
+                        coord1,
+                        ttnn.Topology.Linear,
+                        receiver_semaphore))doc";
 
     using OperationType = decltype(ttnn::point_to_point);
     ttnn::bind_registered_operation(
