e91c809f3d42f35eb98d2863c89ea678e587ed7b
Ryan Zhu <ryanzhu@tenstorrent.com>
2025-06-12 15:17:21 -0400

    [DM] Directly Write to/Read from L1 for Test Preparation and Checking (Test: One From All) (#23365)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/22803)
    
    ### Problem description
    The use of sharded buffers for allocating space on the master and
    subordinate cores limited the ability of the tests to make use of the
    entire available L1 spaces on each core. In the case of the One from All
    tests, this restricted the movement of data amounting to more than half
    of the available L1 space since a master buffer and a subordinate buffer
    were each being allocated for all cores, including the master and
    subordinate cores involved in the transactions.
    
    ### What's changed
    This has been fixed for the One from All tests. Instead of using sharded
    buffers, the test now uses the `WriteToDeviceL1()` and
    `ReadFromDeviceL1()` APIs to set up the subordinate's L1 data and check
    the master's L1 data respectively.
    
    In addition, at the kernel level, the directed ideal test case now
    performs a single transaction containing the maximum specified data
    size, as this is more ideal than performing multiple smaller
    transactions.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes

--- tests/tt_metal/tt_metal/data_movement/one_from_all/kernels/gatherer.cpp
+++ tests/tt_metal/tt_metal/data_movement/one_from_all/kernels/gatherer.cpp
@@ -8,24 +8,22 @@
 
 // L1 to L1 request
 void kernel_main() {
-    uint32_t dst_addr = get_compile_time_arg_val(0);
+    uint32_t l1_local_addr = get_compile_time_arg_val(0);
     constexpr uint32_t num_of_transactions = get_compile_time_arg_val(1);
-    constexpr uint32_t transaction_num_pages = get_compile_time_arg_val(2);
-    constexpr uint32_t page_size_bytes = get_compile_time_arg_val(3);
-    constexpr uint32_t test_id = get_compile_time_arg_val(4);
-    constexpr uint32_t total_subordinate_cores = get_compile_time_arg_val(5);
+    constexpr uint32_t transaction_size_bytes = get_compile_time_arg_val(2);
+    constexpr uint32_t test_id = get_compile_time_arg_val(3);
+    constexpr uint32_t total_subordinate_cores = get_compile_time_arg_val(4);
 
     std::array<uint32_t, total_subordinate_cores> subordinate_l1_byte_addresses;
     std::array<std::array<uint32_t, 2>, total_subordinate_cores> responder_coords;
     uint32_t rt_args_idx = 0;
     for (uint32_t i = 0; i < total_subordinate_cores; i++) {
-        subordinate_l1_byte_addresses[i] = get_arg_val<uint32_t>(rt_args_idx++);
+        subordinate_l1_byte_addresses[i] = l1_local_addr;
         responder_coords[i][0] = get_arg_val<uint32_t>(rt_args_idx++);
         responder_coords[i][1] = get_arg_val<uint32_t>(rt_args_idx++);
     }
 
-    constexpr uint32_t transaction_size_bytes = transaction_num_pages * page_size_bytes;
-    constexpr uint32_t subordinate_size_bytes = num_of_transactions * transaction_num_pages * page_size_bytes;
+    constexpr uint32_t subordinate_size_bytes = num_of_transactions * transaction_size_bytes;
 
     DeviceTimestampedData("Number of transactions", num_of_transactions);
     DeviceTimestampedData("Transaction size in bytes", transaction_size_bytes * total_subordinate_cores);
@@ -42,10 +40,10 @@ void kernel_main() {
                  * Then, we OR it with the local address in each iteration to get the full NOC address. */
                 uint64_t src_noc_addr = src_base_noc_addr | subordinate_l1_byte_addresses[sub_core];
 
-                noc_async_read(src_noc_addr, dst_addr, transaction_size_bytes);
+                noc_async_read(src_noc_addr, l1_local_addr, transaction_size_bytes);
 
                 subordinate_l1_byte_addresses[sub_core] += transaction_size_bytes;
-                dst_addr += transaction_size_bytes;
+                l1_local_addr += transaction_size_bytes;
             }
         }
         noc_async_read_barrier();
