8037ae3fdc252bb962bb18449baf9eb2cf2d3885
Aliaksei Sala <aliaksei_sala@epam.com>
2025-06-12 16:57:24 -0700

    Remove reflect from runtime context

--- ttnn/cpp/kernel/kernel_host_utils.hpp
+++ ttnn/cpp/kernel/kernel_host_utils.hpp
@@ -7,40 +7,39 @@
 #include <type_traits>
 #include <reflect>
 #include <vector>
+#include <bit>
 
 namespace ttnn::kernel_utils {
 
-template <typename KernelStruct, typename FieldType = uint32_t>
-concept KernelArgsStructConcept =
-    alignof(KernelStruct) == alignof(FieldType) && sizeof(KernelStruct) % sizeof(FieldType) == 0 &&
+template <typename KernelStruct>
+concept KernelArgsStructU32Concept =
+    alignof(KernelStruct) == alignof(uint32_t) && sizeof(KernelStruct) % sizeof(uint32_t) == 0 &&
     std::is_aggregate_v<KernelStruct> && [] {
         bool result = true;
         KernelStruct val{};
         reflect::for_each(
-            [&](auto I) { result &= reflect::type_name(FieldType{}) == reflect::type_name(reflect::get<I>(val)); },
-            val);
+            [&](auto I) { result &= reflect::type_name(uint32_t{}) == reflect::type_name(reflect::get<I>(val)); }, val);
         return result;
     }();
 
-template <KernelArgsStructConcept KernStruct>
-std::vector<uint32_t> to_vector(const KernStruct& kernel_data) {
-    std::vector<uint32_t> res(sizeof(KernStruct) / sizeof(uint32_t), 0);
-    uint32_t idx = 0;
-    reflect::for_each([&](auto I) { res[idx++] = reflect::get<I>(kernel_data); }, kernel_data);
-    return res;
+template <KernelArgsStructU32Concept KernStruct>
+consteval uint32_t amount_of_fields() {
+    return sizeof(KernStruct) / sizeof(uint32_t);
 }
 
-template <KernelArgsStructConcept KernStruct>
-consteval uint32_t amount_of_fields() {
-    return sizeof(KernStruct) / sizeof(reflect::get<0>(KernStruct{}));
+template <KernelArgsStructU32Concept KernStruct>
+std::vector<uint32_t> to_vector(const KernStruct& kernel_data) {
+    constexpr auto N = amount_of_fields<KernStruct>();
+    auto res = std::bit_cast<std::array<std::uint32_t, N>>(kernel_data);
+    return std::vector<uint32_t>(res.begin(), res.end());
 }
 
 }  // namespace ttnn::kernel_utils
 
-#define VALIDATE_KERNEL_ARGS_STRUCT(KernStruct)                  \
-    static_assert(                                               \
-        ttnn::kernel_utils::KernelArgsStructConcept<KernStruct>, \
-        "Struct does not satisfy the requirements of KernelArgsStructConcept.");
+#define VALIDATE_KERNEL_ARGS_STRUCT(KernStruct)                     \
+    static_assert(                                                  \
+        ttnn::kernel_utils::KernelArgsStructU32Concept<KernStruct>, \
+        "Struct does not satisfy the requirements of KernelArgsStructU32Concept.");
 
 #else
 #define VALIDATE_KERNEL_ARGS_STRUCT(KernStruct)
