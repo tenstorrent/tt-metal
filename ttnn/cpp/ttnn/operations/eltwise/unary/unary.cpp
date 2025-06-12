1eb987d8db6e71eaf699b00415bb9193eb38df48
Aleksandar Stancov <astancov@tenstorrent.com>
2025-06-12 15:15:31 +0000

    removed ttnn.downsample op

--- ttnn/cpp/ttnn/operations/eltwise/unary/unary.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/unary/unary.cpp
@@ -7,7 +7,6 @@
 #include "ttnn/common/queue_id.hpp"
 #include "device/unary_device_operation.hpp"
 #include "ttnn/run_operation.hpp"
-#include "ttnn/operations/pool/downsample/device/downsample_op.hpp"
 #include "ttnn/operations/core/core.hpp"
 #include "ttnn/operations/eltwise/complex/complex.hpp"
 #include "ttnn/operations/eltwise/binary/binary_composite.hpp"
