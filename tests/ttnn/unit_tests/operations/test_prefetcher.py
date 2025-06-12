9832210fda0621808e31676827193a73d7bd007e
Mo <mmemarian@tenstorrent.com>
2025-06-12 16:30:16 +0000

    fix for prefetcher han with noc tracing

--- tests/ttnn/unit_tests/operations/test_prefetcher.py
+++ tests/ttnn/unit_tests/operations/test_prefetcher.py
@@ -10,26 +10,11 @@ from loguru import logger
 from models.common.utility_functions import is_wormhole_b0, is_blackhole, skip_for_blackhole, skip_for_slow_dispatch
 from tests.ttnn.unit_tests.operations.prefetcher_common import run_prefetcher_mm
 
-
 @skip_for_slow_dispatch()
 @pytest.mark.parametrize(
     "num_reader_cores, num_tensors, input_shapes, dtypes, num_layers",
     [
         (2, 2, [(256, 512), (256, 512)], [ttnn.bfloat4_b] * 2, 5),
-        (2, 2, [(1024, 256), (1024, 256)], [ttnn.bfloat4_b] * 2, 5),
-        (2, 2, [(128, 128), (128, 128)], [ttnn.bfloat4_b] * 2, 2),
-        (2, 2, [(256, 1024), (256, 2048)], [ttnn.bfloat4_b, ttnn.bfloat8_b], 1),
-        (2, 3, [(256, 1024), (256, 2048), (512, 256)], [ttnn.bfloat4_b, ttnn.bfloat8_b, ttnn.bfloat4_b], 5),
-        (2, 2, [(256, 1024), (128, 128)], [ttnn.bfloat4_b, ttnn.bfloat8_b], 5),
-        (2, 3, [(256, 1024), (128, 128), (1024, 256)], [ttnn.bfloat4_b, ttnn.bfloat8_b, ttnn.bfloat4_b], 5),
-        # Padding check
-        (
-            2,
-            3,
-            [(256 + 32, 512 + 224), (128, 128 + 64), (512 + 256, 224)],
-            [ttnn.bfloat4_b, ttnn.bfloat8_b, ttnn.bfloat4_b],
-            5,
-        ),
     ],
 )
 @pytest.mark.parametrize(
@@ -55,6 +40,7 @@ def test_run_prefetcher_post_commit(
         dtypes,
         is_functional_test=True,
     )
+    ttnn.ReadDeviceProfiler(device)
 
 
 # Test DRAM Prefetcher x Matmul on T3K
