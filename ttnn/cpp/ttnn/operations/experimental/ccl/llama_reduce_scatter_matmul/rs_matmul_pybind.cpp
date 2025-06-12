2bb3ccbabaccc399314cf67af5e73a54785363b9
Juan Camilo Vega <jvega@tenstorrent.com>
2025-06-12 10:36:49 -0400

    Merging FF1 Reduce Scatter and FF3 Linear operations in LLama (#22579)
    ### Ticket
    Part of the LLama 70tkps effort
    
    ### Problem description
    This PR provides a 2tkps improvement for 4U in llama by merging FF
    Matmul 3 and FF Reduce Scatter 1 in the decode loop. These two functions
    operate independently in parallel in the model carrying no mutual
    dependencies allowing them to run at the same time, rather than
    sequentially running them, this PR shapes the tensors in such a way that
    we remove any overlap in the cores required for the two operations,
    resulting in the following compute grid, where the CCL and packet worker
    cores are for reduce scatter and the others for matmul. Note due to a
    bug we are unable to use the cores in the same column as a prefetched
    core and that constraint is maintained
    ![image
    (5)](https://github.com/user-attachments/assets/8bce9647-6416-423d-a83a-3f2a0818731d)
    This allows us to then overlap the two functions to run simultaneously
    
    Note for maintainability we try to maximize code reuse so any future
    changes or bug fixes in matmul or RS will immediately update this new
    merged operation. This required a refactor of the two operations to
    create compatibility and new entry points. For instance we need to be
    able to separate the program cache handling and the program handling in
    matmul and allow entry points that perform all the requisite
    preprocessing but only runs one or the other. We also need to support
    the mechanism where each device in a mesh device can have a different
    programming which RS supports, we needed to add that support to matmul
    to be compatible.
    
    ### What's changed
    Describe the approach used to solve the problem.
    Summarize the changes made and its impact.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/15535208350 re-ran
    after pr changes
    https://github.com/tenstorrent/tt-metal/actions/runs/15568789558
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable) All except perf:
    https://github.com/tenstorrent/tt-metal/actions/runs/15569469016, perf:
    https://github.com/tenstorrent/tt-metal/actions/runs/15570100910
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes

---
+++ ttnn/cpp/ttnn/operations/experimental/ccl/llama_reduce_scatter_matmul/rs_matmul_pybind.cpp
@@ -0,0 +1,134 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "rs_matmul_pybind.hpp"
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "ttnn-pybind/decorators.hpp"
+#include "ttnn/operations/experimental/ccl/llama_reduce_scatter_matmul/rs_matmul.hpp"
+#include "ttnn/types.hpp"
+
+namespace ttnn::operations::experimental::ccl {
+
+// TODO: Update with all_gather_matmul docs
+void py_bind_rs_matmul(pybind11::module& module) {
+    namespace py = pybind11;
+
+    ttnn::bind_registered_operation(
+        module,
+        ttnn::experimental::llama_rs_matmul,
+        R"doc(all_gather_matmul(input_tensor: ttnn.Tensor, weight_tensor: ttnn.Tensor, dim: int, *, num_links: int = 1, memory_config: Optional[ttnn.MemoryConfig] = None) -> (ttnn.Tensor, ttnn.Tensor)
+
+        Performs an all-gather operation on multi-device :attr:`input_tensor` across all devices.
+
+        Args:
+            * :attr:`input_tensor` (ttnn.Tensor): multi-device tensor
+            * :attr:`weight_tensor` (ttnn.Tensor): multi-device tensor
+            * :attr:`dim` (int)
+            * :attr:`all_gather_core_grid_offset` (ttnn.CoreCoord): Core grid offset for the all-gather operation.
+
+        Keyword Args:
+            * :attr:`num_links` (int): Number of links to use for the all-gather operation.
+            * :attr:`memory_config_ag` (Optional[ttnn.MemoryConfig]): Memory configuration for the All Gather operation.
+            * :attr:`memory_config_mm` (Optional[ttnn.MemoryConfig]): Memory configuration for the Matmul operation.
+            * :attr:`transpose_a` (bool)
+            * :attr:`transpose_b` (bool)
+            * :attr:`dtype` (Optional[DataType])
+            * :attr:`program_config` (Optional[ttnn.MatmulProgramConfig])
+            * :attr:`activation` (Optional[str])
+            * :attr:`compute_kernel_config` (Optional[DeviceComputeKernelConfig])
+            * :attr:`core_grid` (Optional[ttnn.CoreGrid])
+
+        Example:
+
+            >>> tensor = ttnn.from_torch(torch.tensor((1, 2), dtype=torch.bfloat16), device=device)
+            >>> weight_tensor = ttnn.from_torch(torch.tensor((2, 1), dtype=torch.bfloat16), device=device)
+            >>> all_gathered_mm_in, mm_out = ttnn.all_gather_matmul(tensor, weight_tensor, dim=0, (0, 0))
+
+        )doc",
+        ttnn::pybind_overload_t{
+            [](const decltype(ttnn::experimental::llama_rs_matmul)& self,
+               const ttnn::Tensor& input_tensor,               // mm0 used
+               const ttnn::Tensor& weight_tensor,              // mm1 used
+               const ttnn::Tensor& rs_tensor,                  // rs1
+               ttnn::Tensor& intermediate_packet_buffer,       // rs2
+               int32_t dim,                                    // rs3
+               const GlobalSemaphore& cross_device_semaphore,  // rs4
+               const uint32_t cluster_axis,                    // rs 5
+               const MeshDevice& mesh_device,                  // rs 6
+               const uint32_t num_links,                       // rs 7 default 1
+               const tt::tt_metal::SubDeviceId& subdevice_id,
+               tt::tt_fabric::Topology topology,
+               const std::optional<ttnn::MemoryConfig>& memory_config_rs,  // rs 8 default std::nullopt
+               const std::optional<ttnn::MemoryConfig>& memory_config_mm,  // mm4 used but default std::nullopt
+               const std::optional<const ttnn::DeviceComputeKernelConfig>&
+                   compute_kernel_config,                                   // mm8 used but default std::nullopt
+               const std::optional<const GlobalCircularBuffer>& global_cb,  // mm12 used but default std::nullopt
+               std::optional<const ttnn::CoreGrid>& core_grid,              // mm9 may use but default std::nullopt
+               const bool transpose_a,                                      // mm2 set false
+               const bool transpose_b,                                      // mm3 set false
+               const std::optional<const DataType>& dtype,                  // mm5 set false
+               const std::optional<const operations::matmul::MatmulProgramConfig>& program_config,  // mm6 std::nullopt
+               const std::optional<const std::string>& activation,                                  // mm7 set false
+               const std::optional<const tt::tt_metal::Tile>& output_tile,                          // mm10 std::nullopt
+               std::optional<Tensor>& optional_output_tensor,                                       // mm11 std::nullopt
+               QueueId queue_id  // rs 9 default DefaultQueueId
+
+               ) -> std::vector<ttnn::Tensor> {
+                return self(
+                    queue_id,
+                    input_tensor,
+                    weight_tensor,
+                    rs_tensor,
+                    intermediate_packet_buffer,
+                    dim,
+                    cross_device_semaphore,
+                    cluster_axis,
+                    mesh_device,
+                    num_links,
+                    subdevice_id,
+                    topology,
+                    memory_config_rs,
+                    memory_config_mm,
+                    compute_kernel_config,
+                    global_cb,
+                    core_grid,
+                    transpose_a,
+                    transpose_b,
+                    dtype,
+                    program_config,
+                    activation,
+                    output_tile,
+                    optional_output_tensor);
+            },
+            py::arg("input_tensor"),
+            py::arg("weight_tensor"),
+            py::arg("rs_tensor"),
+            py::arg("intermediate_packet_buffer"),
+            py::arg("dim"),
+            py::arg("cross_device_semaphore"),
+            py::arg("cluster_axis"),
+            py::arg("mesh_device"),
+            py::arg("num_links"),
+            py::arg("subdevice_id"),
+            py::kw_only(),
+            py::arg("topology") = tt::tt_fabric::Topology::Linear,
+            py::arg("memory_config_rs") = std::nullopt,
+            py::arg("memory_config_mm") = std::nullopt,
+            py::arg("compute_kernel_config") = std::nullopt,
+            py::arg("global_cb") = std::nullopt,
+            py::arg("core_grid") = std::nullopt,
+            py::arg("transpose_a") = false,
+            py::arg("transpose_b") = false,
+            py::arg("dtype") = std::nullopt,
+            py::arg("program_config") = std::nullopt,
+            py::arg("activation") = std::nullopt,
+            py::arg("output_tile") = std::nullopt,
+            py::arg("optional_output_tensor") = std::nullopt,
+            py::arg("queue_id") = DefaultQueueId});
+}
+
+}  // namespace ttnn::operations::experimental::ccl
