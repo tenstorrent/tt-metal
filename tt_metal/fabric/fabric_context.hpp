b8f57e112b1ebcbaca36bf9767fe8c97652bb536
Daiki Aminaka <daminaka@tenstorrent.com>
2025-06-12 12:44:11 -0700

    [Fabric] Parallel compile (#23322)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/23142
    
    ### Problem description
    Fabric kernel compile is really slow, It is doing sequentially for each
    chip.
    Especially the one by device init fabric takes 25 - 33 sec on 6U for all
    32 chips.
    Entire test takes really long as well as single test run (when no build
    cache available) takes long.
    
    ### What's changed
    - Parallelize build (& enqueue) phase
    - Change `FabricContext`'s member variable to thread safe one. From
    unordered_map to vector
    
    
    
    - `pytest
    /localdev/daminaka/tt-metal/tests/tt_metal/microbenchmarks/ethernet/test_fabric_edm_bandwidth.py`
    duration
    - T3K: 163.37sec -> +device init fabric parallel 137.33sec (16%) -> +non
    device init fabric parallel 112sec (31%)
    - 6U: 362.97sec -> +device init fabric parallel 322.42sec (11%) -> +non
    device init fabric parallel 265sec (27%)
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes

--- tt_metal/fabric/fabric_context.hpp
+++ tt_metal/fabric/fabric_context.hpp
@@ -10,6 +10,7 @@
 #include <umd/device/types/cluster_descriptor_types.h>  // chip_id_t
 #include <tt-metalium/erisc_datamover_builder.hpp>
 #include <vector>
+#include <limits>
 #include "tt_metal/fabric/fabric_host_utils.hpp"
 
 namespace tt::tt_fabric {
@@ -62,8 +63,14 @@ private:
     size_t channel_buffer_size_bytes_ = 0;
     std::unique_ptr<tt::tt_fabric::FabricEriscDatamoverConfig> router_config_ = nullptr;
     std::unique_ptr<tt::tt_fabric::FabricEriscDatamoverConfig> dateline_router_config_ = nullptr;
-    std::unordered_map<chip_id_t, chan_id_t> master_router_chans_{};
-    std::unordered_map<chip_id_t, uint32_t> num_initialized_routers_{};
+
+    // Using vectors. Use Device IDs as indices
+    size_t num_devices = 0;
+    static constexpr chan_id_t UNINITIALIZED_MASTER_ROUTER_CHAN = std::numeric_limits<chan_id_t>::max();
+    static constexpr uint32_t UNINITIALIZED_ROUTERS = std::numeric_limits<uint32_t>::max();
+    // Use vector instead of unordered_map to be thread safe
+    std::vector<chan_id_t> master_router_chans_;
+    std::vector<uint32_t> num_initialized_routers_;
 };
 
 }  // namespace tt::tt_fabric
