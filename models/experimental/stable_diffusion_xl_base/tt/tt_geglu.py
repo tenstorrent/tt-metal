3bbe84ee2a8388e5943870370763c6e0b2489f6b
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2025-06-12 08:55:03 +0200

    #23273: SDXL UNet matmul (#23419)
    #23273 
    ### What's changed
    Defined program configs for matmuls in `TtTransformer2DModel` and
    `TtGEGLU`. Removed some fragmentation present in the UNet iteration.
    
    New device perf: 387,473us
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/15586352781)
    CI passes
    - [x] [(Single-card) Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/15586358667)
    CI passes
    
    Note: Device perf test run done locally as
    [pipeline](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    is in a bad state.

--- models/experimental/stable_diffusion_xl_base/tt/tt_geglu.py
+++ models/experimental/stable_diffusion_xl_base/tt/tt_geglu.py
@@ -9,7 +9,7 @@ from models.experimental.stable_diffusion_xl_base.tt.sdxl_utility import prepare
 
 
 class TtGEGLU(nn.Module):
-    def __init__(self, device, state_dict, module_path, weights_dtype=ttnn.bfloat16):
+    def __init__(self, device, state_dict, module_path, model_config, weights_dtype=ttnn.bfloat16):
         super().__init__()
 
         self.device = device
@@ -17,17 +17,33 @@ class TtGEGLU(nn.Module):
         bias = state_dict[f"{module_path}.proj.bias"]
 
         self.tt_weights, self.tt_bias = prepare_linear_params(device, weights, bias, weights_dtype)
+        self.program_config = model_config.get_matmul_config(matmul_path=f"{module_path}.proj")
+        self.compute_config = model_config.get_mm_compute_config(f"{module_path}.proj")
 
-    def forward(self, hidden_states):
+    def forward(self, input_tensor):
+        output_memory_config = ttnn.create_sharded_memory_config(
+            (self.program_config.per_core_M * 32, self.program_config.per_core_N * 32),
+            core_grid=ttnn.CoreGrid(y=8, x=8),
+            strategy=ttnn.ShardStrategy.WIDTH if input_tensor.shape[-1] == 1280 else ttnn.ShardStrategy.BLOCK,
+            orientation=ttnn.ShardOrientation.ROW_MAJOR,
+            use_height_and_width_as_shard_shape=True,
+        )
         hidden_states = ttnn.linear(
-            hidden_states,
+            input_tensor,
             self.tt_weights,
             bias=self.tt_bias,
+            memory_config=output_memory_config,
+            program_config=self.program_config,
+            compute_kernel_config=self.compute_config,
         )
+        ttnn.deallocate(input_tensor)
+
+        hidden_states = ttnn.sharded_to_interleaved(hidden_states, ttnn.L1_MEMORY_CONFIG)
         gate = hidden_states[:, :, :, hidden_states.shape[3] // 2 :]
         hidden_states = hidden_states[:, :, :, : hidden_states.shape[3] // 2]
+        gate = ttnn.gelu(gate)
 
         # ttnn.split not working properly
         # hidden_states, gate = ttnn.split(hidden_states, ceil(hidden_states.shape[3] / 2), 3)
 
-        return ttnn.multiply(hidden_states, ttnn.gelu(gate))
+        return ttnn.multiply(hidden_states, gate)
