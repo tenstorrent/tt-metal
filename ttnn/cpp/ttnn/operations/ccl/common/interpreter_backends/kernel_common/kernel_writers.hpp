0891bec6523aed4c432bd0a9dc21107594a0640a
Djordje Ivanovic <133343976+djordje-tt@users.noreply.github.com>
2025-06-13 16:56:58 +0200

    Revert "Lower noc_addr.h to tt_metal" (#23537)
    Reverts tenstorrent/tt-metal#23382
    
    This commit introduces another failure
    - galaxy quick before this commit:
    https://github.com/tenstorrent/tt-metal/actions/runs/15626820064
    - galaxy quick on this commit:
    https://github.com/tenstorrent/tt-metal/actions/runs/15627915860/job/44026139844#step:5:2067
    
    Another reason is that this PR is changing code that needs to be
    reverted as well due to Llama failures on TG Quick and TG Unit and demo.

--- ttnn/cpp/ttnn/operations/ccl/common/interpreter_backends/kernel_common/kernel_writers.hpp
+++ ttnn/cpp/ttnn/operations/ccl/common/interpreter_backends/kernel_common/kernel_writers.hpp
@@ -7,7 +7,7 @@
 // CCL Kernel common includes
 #include "tt_metal/fabric/hw/inc/edm_fabric/fabric_connection_manager.hpp"
 #include <tt-metalium/fabric_edm_packet_header.hpp>
-#include "tt_metal/fabric/hw/inc/noc_addr.h"
+#include "cpp/ttnn/operations/ccl/common/interpreter_backends/kernel_common/noc_addr.hpp"
 #include "cpp/ttnn/operations/ccl/common/interpreter_backends/kernel_common/command_interpreter_base.hpp"
 #include "cpp/ttnn/operations/ccl/common/interpreter_backends/kernel_common/ccl_command_base.hpp"
 
