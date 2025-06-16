64cca78b6b49d969c3ebe4d083caf79569222702
Johanna Rock <jrock@tenstorrent.com>
2025-06-16 12:21:30 +0000

    Revert "Pass in indices as tensor; hanging"
    This reverts commit 6cabf8f8df45f1ca65d2438a120cc2c02c8666ad.

--- ttnn/cpp/ttnn/operations/reduction/topk/topk.cpp
+++ ttnn/cpp/ttnn/operations/reduction/topk/topk.cpp
@@ -37,8 +37,7 @@ std::vector<Tensor> post_topk_transform_tensor(
     const uint32_t adjusted_k,
     const Shape& original_lshape,
     const MemoryConfig& input_memory_config,
-    const CoreRangeSet& sub_core_grids,
-    const std::optional<const Tensor>& indices_tensor) {
+    const CoreRangeSet& sub_core_grids) {
     auto input_shape = input_tensor.padded_shape();
     const auto orig_rank = input_shape.rank();
 
@@ -109,7 +108,6 @@ std::vector<Tensor> ExecuteTopK::invoke(
     const bool sorted,
     const std::optional<MemoryConfig>& memory_config,
     const std::optional<CoreRangeSet>& sub_core_grids,
-    const std::optional<const Tensor>& indices_tensor,
     std::optional<std::tuple<Tensor, Tensor>> optional_output_tensors) {
     ttnn::Shape original_lshape = input_tensor.logical_shape();
 
@@ -134,7 +132,7 @@ std::vector<Tensor> ExecuteTopK::invoke(
     auto output_tensor_vec = tt::tt_metal::operation::run(
         TopK{adjusted_k, -1, largest, sorted, input_memory_config, used_sub_core_grids},
         {padded_tensor},
-        {indices_tensor},
+        {},
         optional_output_tensors.has_value()
             ? reduction_common::tuple_to_vector_optional(optional_output_tensors.value())
             : std::vector<std::optional<Tensor>>{},
@@ -149,7 +147,7 @@ std::vector<Tensor> ExecuteTopK::invoke(
         adjusted_k,
         original_lshape,
         input_memory_config,
-        used_sub_core_grids,
-        indices_tensor);
+        used_sub_core_grids);
+}
 
 }  // namespace ttnn::operations::reduction
