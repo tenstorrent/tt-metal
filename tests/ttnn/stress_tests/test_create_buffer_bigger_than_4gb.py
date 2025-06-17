082611b0e6d28f35cae58afc4f0d0afba0e505a7
Pavlo Hilei <philei@tenstorrent.com>
2025-06-17 14:19:02 +0200

    Update test_large_tensor_creation_sd

--- tests/ttnn/stress_tests/test_create_buffer_bigger_than_4gb.py
+++ tests/ttnn/stress_tests/test_create_buffer_bigger_than_4gb.py
@@ -15,32 +15,19 @@ import os
         (96 * 96, 1, 32 * 228),  # 4Gb + 8Mb after padding
     ],
 )
-@pytest.mark.parametrize(
-    "fast_dispatch",
-    [False],  # TODO: add fast dispatch fix
-)
 @pytest.mark.slow
-def test_large_tensor_creation(device, shape, fast_dispatch):
-    original_dispatch = os.environ.get("TT_METAL_SLOW_DISPATCH_MODE")
-    try:
-        shape = (2**6, 2**13, 2**13 + 32)
-        if fast_dispatch:
-            os.environ.pop("TT_METAL_SLOW_DISPATCH_MODE", None)
-        else:
-            os.environ["TT_METAL_SLOW_DISPATCH_MODE"] = "1"
+def test_large_tensor_creation_sd(device, shape):
+    slow_dispatch = os.environ.get("TT_METAL_SLOW_DISPATCH_MODE")
+    shape = (2**6, 2**13, 2**13 + 32)
+    if slow_dispatch is None or slow_dispatch == "0":
+        pytest.skip("Requires slow dispatch, skipping test")
 
-        torch_input = torch.full(shape, 1).bfloat16()
-        torch_output = torch_input
+    torch_input = torch.full(shape, 1).bfloat16()
+    torch_output = torch_input
 
-        input_tensor = ttnn.from_torch(torch_input, layout=ttnn.TILE_LAYOUT, dtype=ttnn.bfloat16, device=device)
-        output_tensor = ttnn.from_device(input_tensor)
-        output_tensor = ttnn.to_torch(output_tensor).bfloat16()
+    input_tensor = ttnn.from_torch(torch_input, layout=ttnn.TILE_LAYOUT, dtype=ttnn.bfloat16, device=device)
+    output_tensor = ttnn.from_device(input_tensor)
+    output_tensor = ttnn.to_torch(output_tensor).bfloat16()
 
-        assert torch_output.shape == output_tensor.shape
-        assert torch.all(torch_output == output_tensor)
-    finally:
-        # Make sure to restore original dispatch value
-        if original_dispatch is None:
-            os.environ.pop("TT_METAL_SLOW_DISPATCH_MODE", None)
-        else:
-            os.environ["TT_METAL_SLOW_DISPATCH_MODE"] = original_dispatch
+    assert torch_output.shape == output_tensor.shape
+    assert torch.all(torch_output == output_tensor)
