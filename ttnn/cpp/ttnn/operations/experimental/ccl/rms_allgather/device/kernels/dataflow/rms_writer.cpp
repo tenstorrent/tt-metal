d75df5a3d8c47423b3c3b45f515a93fcc16b0e23
Juan Camilo Vega <jvega@tenstorrent.com>
2025-06-17 19:05:00 +0000

    Adding a comment to specify noc1_only in rms does not work

--- ttnn/cpp/ttnn/operations/experimental/ccl/rms_allgather/device/kernels/dataflow/rms_writer.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/rms_allgather/device/kernels/dataflow/rms_writer.cpp
@@ -133,13 +133,12 @@ void kernel_main() {
         uint32_t core_id = 0;
         auto* pkt_hdr = reinterpret_cast<PACKET_HEADER_TYPE*>(packet_header_buffer_seminc);
         uint64_t out_ready_sem_noc_addr_in_pkt =
-            safe_get_noc_addr(out_ready_sem_noc0_x, out_ready_sem_noc0_y, out_ready_sem_bank_addr, 0);
+            safe_get_noc_addr(out_ready_sem_noc0_x, out_ready_sem_noc0_y, out_ready_sem_bank_addr);
         // Send data and the semaphore with the last tile
         uint32_t num_tiles_to_read_this_core = 1;
         cb_wait_front(cb_to_allgather_writer, num_tiles_to_read_this_core);
         size_t l1_read_addr = get_read_ptr(cb_to_allgather_writer);
-        uint64_t noc0_dest_noc_addr =
-            get_noc_addr(core_noc_x[core_id], core_noc_y[core_id], tensor_address0, 0 /*noc_id*/);
+        uint64_t noc0_dest_noc_addr = safe_get_noc_addr(core_noc_x[core_id], core_noc_y[core_id], tensor_address0);
         noc0_dest_noc_addr += shard_tile_id * tensor0_page_size;
         fused_write_atomic_and_advance_local_read_address_for_fabric_write(
             noc0_dest_noc_addr,
--- ttnn/cpp/ttnn/operations/experimental/ccl/rms_allgather/device/multi_core/rms_allgather_pf.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/rms_allgather/device/multi_core/rms_allgather_pf.cpp
@@ -89,6 +89,7 @@ operation::ProgramWithCallbacks frmsnorm_multi_core_sharded(
     ttnn::MeshDevice* mesh_device = a.mesh_device();
     tt::tt_metal::Program program{};
     bool use_noc1_only = false;
+    // Setting to True causes a kernel hang in fabric creation to be investigated
     bool is_first_chip = ring_index == 0;
     bool is_last_chip = ring_index == ring_size - 1;
     uint32_t output_page_size = 0;
