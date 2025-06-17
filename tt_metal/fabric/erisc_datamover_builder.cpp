f001d286f33223d3430065f84b40d7bb11ee9b6e
yugaoTT <yugao@tenstorrent.com>
2025-06-17 20:51:41 +0000

    #0: cleanup

--- tt_metal/fabric/erisc_datamover_builder.cpp
+++ tt_metal/fabric/erisc_datamover_builder.cpp
@@ -496,7 +496,7 @@ FabricEriscDatamoverConfig::FabricEriscDatamoverConfig(
         num_remote_receiver_buffer_slots,
         num_downstream_sender_buffer_slots);
 
-    log_info(
+    log_trace(
         tt::LogOp,
         "is_dateline {} is_dateline_upstream {} is_dateline_upstream_adj_dev {}, is_dateline_upstream_adj_dev_upstream "
         "{}",
@@ -504,11 +504,11 @@ FabricEriscDatamoverConfig::FabricEriscDatamoverConfig(
         is_dateline_upstream,
         is_dateline_upstream_adj_dev,
         is_dateline_upstream_adj_dev_upstream);
-    log_info(tt::LogOp, "num_sender_buffer_slots: {}", num_sender_buffer_slots);
-    log_info(tt::LogOp, "num_remote_sender_buffer_slots: {}", num_remote_sender_buffer_slots);
-    log_info(tt::LogOp, "num_receiver_buffer_slots: {}", num_receiver_buffer_slots);
-    log_info(tt::LogOp, "num_remote_receiver_buffer_slots: {}", num_remote_receiver_buffer_slots);
-    log_info(tt::LogOp, "num_downstream_sender_buffer_slots: {}", num_downstream_sender_buffer_slots);
+    log_trace(tt::LogOp, "num_sender_buffer_slots: {}", num_sender_buffer_slots);
+    log_trace(tt::LogOp, "num_remote_sender_buffer_slots: {}", num_remote_sender_buffer_slots);
+    log_trace(tt::LogOp, "num_receiver_buffer_slots: {}", num_receiver_buffer_slots);
+    log_trace(tt::LogOp, "num_remote_receiver_buffer_slots: {}", num_remote_receiver_buffer_slots);
+    log_trace(tt::LogOp, "num_downstream_sender_buffer_slots: {}", num_downstream_sender_buffer_slots);
 
     size_t total_sender_slots = std::accumulate(
         num_sender_buffer_slots.begin(), num_sender_buffer_slots.begin() + this->num_used_sender_channels, size_t{0});
