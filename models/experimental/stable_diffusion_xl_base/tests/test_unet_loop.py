01971f2f97a002cb919942fa2c977670adb2d99c
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2025-06-17 09:09:07 +0200

    SDXL: UNet loop pcc test (#23589)
    ### What's changed
    Added a test for pcc on the entire UNet loop. Test not added to the CI
    because it takes 15min to execute (18s/iter because of host UNet).
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/15684455492)
    CI passes

--- models/experimental/stable_diffusion_xl_base/tests/test_unet_loop.py
+++ models/experimental/stable_diffusion_xl_base/tests/test_unet_loop.py
@@ -9,7 +9,6 @@ from diffusers import DiffusionPipeline
 from loguru import logger
 import ttnn
 from models.experimental.stable_diffusion_xl_base.tt.tt_unet import TtUNet2DConditionModel
-from models.experimental.stable_diffusion_xl_base.vae.tt.tt_autoencoder_kl import TtAutoencoderKL
 from models.experimental.stable_diffusion_xl_base.tt.tt_euler_discrete_scheduler import TtEulerDiscreteScheduler
 from models.experimental.stable_diffusion_xl_base.tt.model_configs import ModelOptimisations
 from models.experimental.stable_diffusion_xl_base.tests.test_common import (
@@ -17,15 +16,98 @@ from models.experimental.stable_diffusion_xl_base.tests.test_common import (
     retrieve_timesteps,
     run_tt_iteration,
 )
-import os
-import gc
+from tests.ttnn.utils_for_testing import assert_with_pcc, comp_pcc
+import matplotlib.pyplot as plt
+
+
+def run_tt_denoising(
+    tt_latents,
+    iter,
+    ttnn_device,
+    tt_unet,
+    tt_scheduler,
+    input_shape,
+    ttnn_prompt_embeds,
+    ttnn_added_cond_kwargs,
+    tt_t,
+    classifier_free_guidance,
+    guidance_scale,
+    extra_step_kwargs,
+):
+    B, C, H, W = input_shape
+    unet_outputs = []
+    for unet_slice in range(len(ttnn_prompt_embeds[iter])):
+        tt_latent_model_input = tt_latents
+        noise_pred, noise_shape = run_tt_iteration(
+            ttnn_device,
+            tt_unet,
+            tt_scheduler,
+            tt_latent_model_input,
+            [B, C, H, W],
+            ttnn_prompt_embeds[iter][unet_slice],
+            ttnn_added_cond_kwargs[iter][unet_slice],
+            tt_t,
+            iter,
+        )
+        C, H, W = noise_shape
+
+        unet_outputs.append(noise_pred)
+
+    # perform guidance
+    if classifier_free_guidance:
+        noise_pred_uncond, noise_pred_text = unet_outputs
+        noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+        ttnn.deallocate(noise_pred_uncond)
+        ttnn.deallocate(noise_pred_text)
+    else:
+        noise_pred = unet_outputs[0]
+
+    tt_latents = tt_scheduler.step(
+        noise_pred, tt_scheduler.timesteps[iter], tt_latents, **extra_step_kwargs, return_dict=False
+    )[0]
+
+    ttnn.deallocate(noise_pred)
+    tt_latents = ttnn.move(tt_latents)
+
+    return tt_latents, [C, H, W]
+
+
+def run_torch_denoising(
+    latents,
+    iter,
+    pipeline,
+    prompt_embeds,
+    added_cond_kwargs,
+    t,
+    classifier_free_guidance,
+    guidance_scale,
+    extra_step_kwargs,
+):
+    latent_model_input = torch.cat([latents] * 2) if classifier_free_guidance else latents
+
+    latent_model_input = pipeline.scheduler.scale_model_input(latent_model_input, t)
+
+    noise_pred = pipeline.unet(
+        latent_model_input,
+        t,
+        encoder_hidden_states=prompt_embeds[iter],
+        timestep_cond=None,
+        cross_attention_kwargs=None,
+        added_cond_kwargs=added_cond_kwargs[iter],
+        return_dict=False,
+    )[0]
+
+    if classifier_free_guidance:
+        noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+        noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+    latents = pipeline.scheduler.step(noise_pred, t, latents, **extra_step_kwargs, return_dict=False)[0]
+    return latents
 
 
 @torch.no_grad()
-def run_demo_inference(
-    ttnn_device, is_ci_env, prompts, num_inference_steps, classifier_free_guidance, vae_on_device, evaluation_range
-):
-    start_from, _ = evaluation_range
+def run_unet_inference(ttnn_device, prompts, num_inference_steps, classifier_free_guidance=True):
     torch.manual_seed(0)
 
     if isinstance(prompts, str):
@@ -53,7 +135,7 @@ def run_demo_inference(
         use_safetensors=True,
     )
 
-    # 2. Load tt_unet, tt_vae and tt_scheduler
+    # 2. Load tt_unet and tt_scheduler
     tt_model_config = ModelOptimisations(conv_w_dtype=ttnn.bfloat16)
     tt_unet = TtUNet2DConditionModel(
         ttnn_device,
@@ -61,9 +143,6 @@ def run_demo_inference(
         "unet",
         model_config=tt_model_config,
         transformer_weights_dtype=ttnn.bfloat16,
-    )
-    tt_vae = (
-        TtAutoencoderKL(ttnn_device, pipeline.vae.state_dict(), model_config=tt_model_config) if vae_on_device else None
     )
     tt_scheduler = TtEulerDiscreteScheduler(
         ttnn_device,
@@ -85,7 +164,6 @@ def run_demo_inference(
         pipeline.scheduler.config.rescale_betas_zero_snr,
         pipeline.scheduler.config.final_sigmas_type,
     )
-    pipeline.scheduler = tt_scheduler
 
     cpu_device = "cpu"
 
@@ -123,10 +201,11 @@ def run_demo_inference(
 
     # Prepare timesteps
     timesteps, num_inference_steps = retrieve_timesteps(pipeline.scheduler, num_inference_steps, cpu_device, None, None)
+    tt_timesteps, tt_num_inference_steps = retrieve_timesteps(tt_scheduler, num_inference_steps, cpu_device, None, None)
 
     # Convert timesteps to ttnn
     ttnn_timesteps = []
-    for t in timesteps:
+    for t in tt_timesteps:
         scalar_tensor = torch.tensor(t).unsqueeze(0)
         ttnn_timesteps.append(
             ttnn.from_torch(
@@ -274,25 +353,21 @@ def run_demo_inference(
             }
         ]
 
-    scaling_factor = ttnn.from_torch(
-        torch.Tensor([pipeline.vae.config.scaling_factor]),
-        dtype=ttnn.bfloat16,
-        device=ttnn_device,
-        layout=ttnn.TILE_LAYOUT,
-        memory_config=ttnn.DRAM_MEMORY_CONFIG,
-    )
+    if classifier_free_guidance:
+        prompt_embeds = [torch.cat([t1, t2], dim=0) for t1, t2 in zip(negative_prompt_embeds, prompt_embeds)]
+        add_text_embeds = [torch.cat([t1, t2], dim=0) for t1, t2 in zip(negative_pooled_prompt_embeds, add_text_embeds)]
+        add_time_ids = [torch.cat([t1, t2], dim=0) for t1, t2 in zip(negative_add_time_ids, add_time_ids)]
+    added_cond_kwargs = [{"text_embeds": t1, "time_ids": t2} for t1, t2 in zip(add_text_embeds, add_time_ids)]
 
     logger.info("Performing warmup run, to make use of program caching in actual inference...")
     B, C, H, W = latents.shape
 
     # All device code will work with channel last tensors
-    latents = torch.permute(latents, (0, 2, 3, 1))
-    latents = latents.reshape(1, 1, B * H * W, C)
+    tt_latents = torch.permute(latents, (0, 2, 3, 1))
+    tt_latents = tt_latents.reshape(1, 1, B * H * W, C)
 
-    latents_clone = latents.clone()
-
-    latents = ttnn.from_torch(
-        latents,
+    tt_latents = ttnn.from_torch(
+        tt_latents,
         dtype=ttnn.bfloat16,
         device=ttnn_device,
         layout=ttnn.TILE_LAYOUT,
@@ -300,111 +375,70 @@ def run_demo_inference(
     )
 
     # UNet will deallocate the input tensor
-    latent_model_input = ttnn.clone(latents)
+    tt_latent_model_input = ttnn.clone(tt_latents)
 
     # Compile run of Scheduler and UNet
     run_tt_iteration(
         ttnn_device,
         tt_unet,
         tt_scheduler,
-        latent_model_input,
+        tt_latent_model_input,
         [B, C, H, W],
         ttnn_prompt_embeds[0][0],
         ttnn_added_cond_kwargs[0][0],
         ttnn_timesteps[0],
         0,
     )
-    if not is_ci_env and not os.path.exists("output"):
-        os.mkdir("output")
-
-    images = []
+    pcc_per_iter = []
     logger.info("Starting ttnn inference...")
     for iter in range(len(prompts)):
         logger.info(f"Running inference for prompt {iter + 1}/{len(prompts)}: {prompts[iter]}")
-        for i, t in tqdm(enumerate(ttnn_timesteps), total=len(ttnn_timesteps)):
-            unet_outputs = []
-            for unet_slice in range(len(ttnn_prompt_embeds[iter])):
-                latent_model_input = latents
-                noise_pred, noise_shape = run_tt_iteration(
-                    ttnn_device,
-                    tt_unet,
-                    tt_scheduler,
-                    latent_model_input,
-                    [B, C, H, W],
-                    ttnn_prompt_embeds[iter][unet_slice],
-                    ttnn_added_cond_kwargs[iter][unet_slice],
-                    t,
-                    i,
-                )
-                C, H, W = noise_shape
+        for i, (t, tt_t) in tqdm(enumerate(zip(timesteps, ttnn_timesteps)), total=len(ttnn_timesteps)):
+            tt_latents, [C, H, W] = run_tt_denoising(
+                tt_latents=tt_latents,
+                iter=iter,
+                ttnn_device=ttnn_device,
+                tt_unet=tt_unet,
+                tt_scheduler=tt_scheduler,
+                input_shape=[B, C, H, W],
+                ttnn_prompt_embeds=ttnn_prompt_embeds,
+                ttnn_added_cond_kwargs=ttnn_added_cond_kwargs,
+                tt_t=tt_t,
+                classifier_free_guidance=classifier_free_guidance,
+                guidance_scale=guidance_scale,
+                extra_step_kwargs=extra_step_kwargs,
+            )
+            latents = run_torch_denoising(
+                latents=latents,
+                iter=iter,
+                pipeline=pipeline,
+                prompt_embeds=prompt_embeds,
+                added_cond_kwargs=added_cond_kwargs,
+                t=t,
+                classifier_free_guidance=classifier_free_guidance,
+                guidance_scale=guidance_scale,
+                extra_step_kwargs=extra_step_kwargs,
+            )
 
-                unet_outputs.append(noise_pred)
+            torch_tt_latents = ttnn.to_torch(tt_latents)
+            torch_tt_latents = torch.reshape(torch_tt_latents, (B, H, W, C))
+            torch_tt_latents = torch.permute(torch_tt_latents, (0, 3, 1, 2))
 
-            # perform guidance
-            if classifier_free_guidance:
-                noise_pred_uncond, noise_pred_text = unet_outputs
-                noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
-
-                ttnn.deallocate(noise_pred_uncond)
-                ttnn.deallocate(noise_pred_text)
-            else:
-                noise_pred = unet_outputs[0]
-
-            latents = tt_scheduler.step(
-                noise_pred, tt_scheduler.timesteps[i], latents, **extra_step_kwargs, return_dict=False
-            )[0]
-
-            ttnn.deallocate(noise_pred)
-            latents = ttnn.move(latents)
+            _, pcc_message = comp_pcc(latents, torch_tt_latents, 0.8)
+            logger.info(f"PCC of {i}. iteration is: {pcc_message}")
+            pcc_per_iter.append(float(pcc_message))
 
         tt_scheduler.set_step_index(0)
 
-        if vae_on_device:
-            latents = ttnn.div(latents, scaling_factor)
+    assert_with_pcc(latents, torch_tt_latents, 0.82)
 
-            # Workaround for #22017
-            ttnn_device.disable_and_clear_program_cache()
-
-            logger.info("Running TT VAE")
-            image = tt_vae.forward(latents, [B, C, H, W])
-            ttnn_device.enable_program_cache()
-        else:
-            latents = ttnn.from_device(latents).to_torch()
-            latents = latents.reshape(B, H, W, C)
-            latents = torch.permute(latents, (0, 3, 1, 2))
-
-            latents = latents.to(pipeline.vae.dtype)
-
-            # VAE upcasting to float32 is happening in the reference SDXL demo if VAE dtype is float16. If it's bfloat16, it will not be upcasted.
-            latents = latents / pipeline.vae.config.scaling_factor
-
-            image = pipeline.vae.decode(latents, return_dict=False)[0]
-
-        image = pipeline.image_processor.postprocess(image, output_type="pil")[0]
-        images.append(image)
-
-        if is_ci_env:
-            logger.info(f"Image {iter + 1}/{len(prompts)} generated successfully")
-        else:
-            image.save(f"output/output{iter + start_from}.png")
-            logger.info(f"Image saved to output/output{iter + start_from}.png")
-
-        if vae_on_device:
-            ttnn.deallocate(latents)
-        else:
-            del latents
-            gc.collect()
-
-        latents = latents_clone.clone()
-        latents = ttnn.from_torch(
-            latents,
-            dtype=ttnn.bfloat16,
-            device=ttnn_device,
-            layout=ttnn.TILE_LAYOUT,
-            memory_config=ttnn.DRAM_MEMORY_CONFIG,
-        )
-
-    return images
+    plt.plot(pcc_per_iter, marker="o")
+    plt.title("PCC per iteration")
+    plt.xlabel("Iteration")
+    plt.ylabel("PCC")
+    plt.grid(True)
+    plt.savefig("pcc_plot.png", dpi=300, bbox_inches="tight")
+    plt.close()
 
 
 @pytest.mark.parametrize("device_params", [{"l1_small_size": SDXL_L1_SMALL_SIZE}], indirect=True)
@@ -420,28 +454,14 @@ def run_demo_inference(
     "classifier_free_guidance",
     [
         (True),
-        (False),
     ],
-    ids=("with_classifier_free_guidance", "no_classifier_free_guidance"),
+    ids=("with_classifier_free_guidance",),
 )
-@pytest.mark.parametrize(
-    "vae_on_device",
-    [
-        (True),
-        (False),
-    ],
-    ids=("device_vae", "host_vae"),
-)
-def test_demo(
+def test_unet_loop(
     device,
     use_program_cache,
-    is_ci_env,
     prompt,
     num_inference_steps,
     classifier_free_guidance,
-    vae_on_device,
-    evaluation_range,
 ):
-    return run_demo_inference(
-        device, is_ci_env, prompt, num_inference_steps, classifier_free_guidance, vae_on_device, evaluation_range
-    )
+    return run_unet_inference(device, prompt, num_inference_steps, classifier_free_guidance)
