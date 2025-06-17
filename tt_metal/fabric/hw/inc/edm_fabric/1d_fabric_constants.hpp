22ff0011b4b9dc65ecc198db44be0a88a1fc9953
Yu Gao <145494740+yugaoTT@users.noreply.github.com>
2025-06-17 10:22:31 -0400

    #0: add extra buffering on dateline-upstream-adjcent-upstream edm (#23540)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/23057)
    
    ### Problem description
    there's extra buffer slots discovered on the adjacent devices of
    dateline device, remove sender channel 2 from
    "DatelineUpstreamAdjacentDeviceUpstream" EDM.
    
    Also need to throw in the translation check optimization because of
    regression on full ring 8chip test.
    Summary:
    T3K: 2KB packet test perf dropped for full ring 8chip test, other tests
    all improved.
    
    ![image](https://github.com/user-attachments/assets/cc8a2a12-5141-46ee-aa2d-14bda9b47386)
    full comparison:
    
    [fabric_edm_bandwidth_jun13.csv](https://github.com/user-attachments/files/20729761/fabric_edm_bandwidth_jun13.csv)
    
    GEOMEAN (based on the ratio new perf / old perf): 
    
    ![image](https://github.com/user-attachments/assets/3e9cc6bb-5c16-40b4-a66a-5ebd631d1adc)
    
    6U:
    ![Screenshot 2025-06-17 at 9 14
    17 AM](https://github.com/user-attachments/assets/efe35990-c500-4e7c-9adc-afc7aa71bea6)
    
    
    [bandwidth_summary_jun16.csv](https://github.com/user-attachments/files/20776039/bandwidth_summary_jun16.csv)
    
    ### Checklist
    - [x] [All post commit]
    https://github.com/tenstorrent/tt-metal/actions/runs/15683374255
    - [x] ubenchmark
    https://github.com/tenstorrent/tt-metal/actions/runs/15683380491/job/44180795398
    - need update perf
    - [x] T3K
    https://github.com/tenstorrent/tt-metal/actions/runs/15684949935 - known
    hang issue in main
    - [x] TG
    https://github.com/tenstorrent/tt-metal/actions/runs/15684956433 -
    random hang, same as main

--- tt_metal/fabric/hw/inc/edm_fabric/1d_fabric_constants.hpp
+++ tt_metal/fabric/hw/inc/edm_fabric/1d_fabric_constants.hpp
@@ -211,8 +211,9 @@ constexpr size_t DOWNSTREAM_SENDER_NUM_BUFFERS = DOWNSTREAM_SENDER_NUM_BUFFERS_A
 constexpr size_t SKIP_CHANNEL_IDX = DOWNSTREAM_SENDER_NUM_BUFFERS_IDX + NUM_DOWNSTREAM_CHANNELS;
 constexpr bool skip_receiver_channel_1_connection = get_compile_time_arg_val(SKIP_CHANNEL_IDX);
 constexpr bool skip_sender_channel_1_connection = get_compile_time_arg_val(SKIP_CHANNEL_IDX + 1);
+constexpr bool skip_sender_vc1_channel_connection = get_compile_time_arg_val(SKIP_CHANNEL_IDX + 2);
 
-constexpr size_t MAIN_CT_ARGS_IDX_1 = SKIP_CHANNEL_IDX + 2;
+constexpr size_t MAIN_CT_ARGS_IDX_1 = SKIP_CHANNEL_IDX + 3;
 constexpr size_t local_sender_0_channel_address = get_compile_time_arg_val(MAIN_CT_ARGS_IDX_1);
 constexpr size_t local_sender_channel_0_connection_info_addr = get_compile_time_arg_val(MAIN_CT_ARGS_IDX_1 + 1);
 constexpr size_t local_sender_1_channel_address = get_compile_time_arg_val(MAIN_CT_ARGS_IDX_1 + 2);
@@ -382,6 +383,9 @@ constexpr std::array<uint32_t, MAX_NUM_SENDER_CHANNELS> to_sender_packets_comple
 
 // Miscellaneous configuration
 constexpr size_t DEFAULT_HANDSHAKE_CONTEXT_SWITCH_TIMEOUT = 0;
+
+// TODO: move this to compile time args if we need to enable it
+constexpr bool enable_trid_flush_check_on_noc_txn = false;
 
 namespace tt::tt_fabric {
 static_assert(
