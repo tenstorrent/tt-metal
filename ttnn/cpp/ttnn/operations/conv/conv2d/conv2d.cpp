31171b96d3936ed97bf12565b62cb13685ae0da1
Sankar Manoj <smanoj@tenstorrent.com>
2025-06-17 19:29:48 +0000

    #0: comments

--- ttnn/cpp/ttnn/operations/conv/conv2d/conv2d.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/conv2d.cpp
@@ -376,6 +376,7 @@ Result conv2d_DRAM(
             sliced_output_tensor = ttnn::untilize(sliced_output_tensor);
         }
         if (sliced_output_tensor.memory_config().memory_layout() == TensorMemoryLayout::INTERLEAVED) {
+            // slice_write expects the output tensor to be correctly shaped when its in interleaved memory layout.
             sliced_output_tensor = ttnn::reshape(
                 sliced_output_tensor,
                 ttnn::Shape({batch_size, output_slice_height, output_slice_width, out_channels}),
