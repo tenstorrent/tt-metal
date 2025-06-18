379cbac8ecae6a26aa28a9d234a6b39534b58808
Radoica Draskic <rdraskic@tenstorrent.com>
2025-06-18 19:20:06 +0000

    Address comments: add condition

--- models/tt_transformers/tt/model_config.py
+++ models/tt_transformers/tt/model_config.py
@@ -819,7 +819,7 @@ class ModelArgs:
                 grid_size=self.find_prefill_grid(
                     num_rows(seq_len) // self.tile_size,
                     k_dim // self.tile_size,
-                    dram_shard_grid_width=dram_shard_grid_width,
+                    dram_shard_grid_width=dram_shard_grid_width if dram_sharded_wo else None,
                 ),
                 in0_block_w=1 if self.is_galaxy else None,
                 fuse_batch=seq_len <= 1024,
