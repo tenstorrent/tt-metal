25e0933512372f0e1d72230fedac68972f82e9c8
Pavle Popovic <33969586+pavlepopovic@users.noreply.github.com>
2025-06-18 12:25:19 +0200

    #23273: Shard feedworward sequences (#23664)
    ### Ticket
    #23273:
    
    ### Problem description
    Split geglu into 2 mms instead of 1, to get rid of split op.
    Shard entire geglu + ff
    
    ### What's changed
    Describe the approach used to solve the problem.
    Summarize the changes made and its impact.
    
    ### Checklist
    - [x] [Frequent
    tests]https://github.com/tenstorrent/tt-metal/actions/runs/15729103955/job/44326223843
    unrelated failures, this PR is fixing sdxl failures
    - [ ] [Device performance
    regression]https://github.com/tenstorrent/tt-metal/actions/runs/15729093575
    CI clogged, ran numbers locally, merging to fix frequent tests
    
    Co-authored-by: ppopovic <ppopovic@tenstorrent.com>

--- models/experimental/stable_diffusion_xl_base/tt/tt_geglu.py
+++ models/experimental/stable_diffusion_xl_base/tt/tt_geglu.py
@@ -13,37 +13,55 @@ class TtGEGLU(nn.Module):
         super().__init__()
 
         self.device = device
-        weights = state_dict[f"{module_path}.proj.weight"].unsqueeze(0).unsqueeze(0)
+        weights = state_dict[f"{module_path}.proj.weight"]
         bias = state_dict[f"{module_path}.proj.bias"]
+        w1, w2 = weights.chunk(2, dim=0)  # Each: [out_dim // 2, in_dim]
+        b1, b2 = bias.chunk(2, dim=0)  # Each: [out_dim // 2]
 
-        self.tt_weights, self.tt_bias = prepare_linear_params(device, weights, bias, weights_dtype)
-        self.program_config = model_config.get_matmul_config(matmul_path=f"{module_path}.proj")
+        w1 = w1.unsqueeze(0).unsqueeze(0)  # [1, 1, out_dim // 2, in_dim]
+        w2 = w2.unsqueeze(0).unsqueeze(0)  # same
+
+        self.tt_weights_1, self.tt_bias_1 = prepare_linear_params(device, w1, b1, weights_dtype)
+        self.tt_weights_2, self.tt_bias_2 = prepare_linear_params(device, w2, b2, weights_dtype)
+        self.program_config = model_config.get_matmul_config(matmul_path=f"{module_path}.proj.split")
+        self.program_config_gelu = model_config.get_matmul_config(matmul_path=f"{module_path}.proj.split.gelu")
         self.compute_config = model_config.get_mm_compute_config(f"{module_path}.proj")
 
+        assert self.program_config is not None, "Program config for split weights is None"
+        assert self.program_config_gelu is not None, "Program config for split weights with GELU is None"
+
     def forward(self, input_tensor):
-        output_memory_config = ttnn.create_sharded_memory_config(
-            (self.program_config.per_core_M * 32, self.program_config.per_core_N * 32),
-            core_grid=ttnn.CoreGrid(y=8, x=8),
-            strategy=ttnn.ShardStrategy.WIDTH if input_tensor.shape[-1] == 1280 else ttnn.ShardStrategy.BLOCK,
-            orientation=ttnn.ShardOrientation.ROW_MAJOR,
-            use_height_and_width_as_shard_shape=True,
-        )
+        if input_tensor.shape[2] == 4096:
+            # due to block sharded mm constraints, if we block shard the input tensor, we can only run it on 56 cores
+            # hence using L1 memory config instead
+            input_tensor = ttnn.to_memory_config(input_tensor, ttnn.L1_MEMORY_CONFIG)
+        else:
+            # here we can run the block sharded matmul on 64 cores
+            block_sharded_mem_config = ttnn.create_sharded_memory_config(
+                (input_tensor.shape[2] // 8, input_tensor.shape[3] // 8),
+                core_grid=ttnn.CoreGrid(y=8, x=8),
+                strategy=ttnn.ShardStrategy.BLOCK,
+                orientation=ttnn.ShardOrientation.ROW_MAJOR,
+                use_height_and_width_as_shard_shape=True,
+            )
+            input_tensor = ttnn.to_memory_config(input_tensor, block_sharded_mem_config)
+
         hidden_states = ttnn.linear(
             input_tensor,
-            self.tt_weights,
-            bias=self.tt_bias,
-            memory_config=output_memory_config,
+            self.tt_weights_1,
+            bias=self.tt_bias_1,
+            memory_config=ttnn.L1_BLOCK_SHARDED_MEMORY_CONFIG,
             program_config=self.program_config,
             compute_kernel_config=self.compute_config,
         )
+        gate = ttnn.linear(
+            input_tensor,
+            self.tt_weights_2,
+            bias=self.tt_bias_2,
+            memory_config=ttnn.L1_BLOCK_SHARDED_MEMORY_CONFIG,
+            program_config=self.program_config_gelu,
+            compute_kernel_config=self.compute_config,
+        )
         ttnn.deallocate(input_tensor)
-
-        hidden_states = ttnn.sharded_to_interleaved(hidden_states, ttnn.L1_MEMORY_CONFIG)
-        gate = hidden_states[:, :, :, hidden_states.shape[3] // 2 :]
-        hidden_states = hidden_states[:, :, :, : hidden_states.shape[3] // 2]
-        gate = ttnn.gelu(gate)
-
-        # ttnn.split not working properly
-        # hidden_states, gate = ttnn.split(hidden_states, ceil(hidden_states.shape[3] / 2), 3)
-
-        return ttnn.multiply(hidden_states, gate)
+        hidden_states = ttnn.mul_(hidden_states, gate)
+        return hidden_states
