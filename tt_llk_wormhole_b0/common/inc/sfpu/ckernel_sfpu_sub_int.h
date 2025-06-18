449487390ef6bdb77412ce2d9cb2b827b391f6e1
Anasuya G Nair <mcw-anair@ext.tenstorrent.com>
2025-06-18 12:42:02 +0530

    Modify sub, mul LLK to include uint16 (#351)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/23398
    
    ### Problem description
    Need to include uint16 support in LLK integer subtraction,
    multiplication implementation
    
    ### What's changed
    Added instr mode param
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update

--- tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_sub_int.h
+++ tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_sub_int.h
@@ -17,27 +17,28 @@ namespace sfpu
 {
 
 template <bool APPROXIMATION_MODE, int ITERATIONS, InstrModLoadStore INSTRUCTION_MODE, bool SIGN_MAGNITUDE_FORMAT>
-inline void _add_int_(const uint dst_offset)
+inline void _sub_int_(const uint dst_offset)
 {
     static_assert(is_valid_instruction_mode(INSTRUCTION_MODE), "INSTRUCTION_MODE must be one of: INT32_2S_COMP, INT32, LO16.");
 
-    // For int32, use '12' if Dest is in sign-magnitude format and '4' for 2's complement,
+    // Use '12' if Dest is in sign-magnitude format and '4' for 2's complement,
     // because TTI_SFPIADD requires 2's complement format in LREGs
     constexpr int sfpload_instr_mod = SIGN_MAGNITUDE_FORMAT ? INT32_2S_COMP : static_cast<std::underlying_type_t<InstrModLoadStore>>(INSTRUCTION_MODE);
 
-    // Operand A is input1 (int32/uint16/uint32)
-    // Operand B is input2 (int32/uint16/uint32)
-    // Output is int32/uint16/uint32
+// Operand A is input1 (int32/uint16)
+// Operand B is input2 (int32/uint16)
+// Output is int32/uint16
 #pragma GCC unroll 8
     for (int d = 0; d < ITERATIONS; d++)
     {
         // operand A
-        TTI_SFPLOAD(p_sfpu::LREG0, sfpload_instr_mod, ADDR_MOD_3, 0);
+        TTI_SFPLOAD(p_sfpu::LREG1 /*lreg*/, sfpload_instr_mod, ADDR_MOD_3 /*addr_mode*/, 0 /*dest*/);
         // operand B
-        TT_SFPLOAD(p_sfpu::LREG1, sfpload_instr_mod, ADDR_MOD_3, dst_offset * 64);
-        TTI_SFPIADD(0, p_sfpu::LREG1, p_sfpu::LREG0, 4);
+        TT_SFPLOAD(p_sfpu::LREG0 /*lreg*/, sfpload_instr_mod, ADDR_MOD_3 /*addr_mode*/, dst_offset * 64 /*dest*/);
+        // Use 6 or LO16 as imod to convert operand B to 2's complement
+        TTI_SFPIADD(0 /*imm*/, p_sfpu::LREG1 /*lreg*/, p_sfpu::LREG0 /*ldest*/, 6 /*imod*/);
         // LREG_0 -> dest
-        TTI_SFPSTORE(0, sfpload_instr_mod, ADDR_MOD_3, 0);
+        TTI_SFPSTORE(p_sfpu::LREG0 /*lreg_ind*/, sfpload_instr_mod, ADDR_MOD_3 /*addr_mode*/, 0 /*dest*/);
         sfpi::dst_reg++;
     }
 }
