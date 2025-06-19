915fcd8ffd3cef808e8973ff280b5c1a8d326c36
Diego Gomez <dgomez@tenstorrent.com>
2025-06-19 11:55:12 -0400

    Fixed formatting issue

--- tests/ttnn/unit_tests/operations/test_scatter.py
+++ tests/ttnn/unit_tests/operations/test_scatter.py
@@ -76,7 +76,6 @@ def test_scatter_normal(input_shape, dim, index_and_source_shape, input_dtype, i
     torch.manual_seed(22052025)
     # https://github.com/tenstorrent/tt-metal/issues/23205
     device.disable_and_clear_program_cache()
-    
     torch_dtype = select_torch_dtype(input_dtype)
     torch_index_dtype = select_torch_dtype(index_dtype)
     ##
