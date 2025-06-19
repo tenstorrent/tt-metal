ff2c7b5a9050983be39956ea3f0385a208086b49
Evan Smal <esmal@tenstorrent.com>
2025-06-19 09:30:39 -0500

    Get UNet working on P150

--- models/experimental/functional_unet/tests/test_unet_perf.py
+++ models/experimental/functional_unet/tests/test_unet_perf.py
@@ -28,6 +28,7 @@ def test_unet_model(batch, groups, device, iterations, use_program_cache, reset_
     if (
         not is_wormhole_b0(device)
         and device.compute_with_storage_grid_size().x * device.compute_with_storage_grid_size().y != 110
+        and device.compute_with_storage_grid_size().x * device.compute_with_storage_grid_size().y != 130
     ):
         pytest.skip(f"Shallow UNet only support 110 cores on BH (was {device.compute_with_storage_grid_size()})")
     device.disable_and_clear_program_cache()  # Needed to give consistent device perf between iterations
@@ -93,8 +94,9 @@ def test_unet_trace_perf(
     if (
         not is_wormhole_b0(device)
         and device.compute_with_storage_grid_size().x * device.compute_with_storage_grid_size().y != 110
+        and device.compute_with_storage_grid_size().x * device.compute_with_storage_grid_size().y != 130
     ):
-        pytest.skip(f"shallow unet only support 110 cores on bh (was {device.compute_with_storage_grid_size()})")
+        pytest.skip(f"Shallow UNet only support 110 cores on BH (was {device.compute_with_storage_grid_size()})")
 
     from models.experimental.functional_unet.tests.test_unet_trace import (
         test_unet_trace_2cq,
