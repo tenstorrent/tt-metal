76c51e358fe9b2bb0f51c40b4f73ce68286e3e0f
Diego Gomez <dgomez@tenstorrent.com>
2025-06-19 11:53:59 -0400

    Experimenting with signals

--- ttnn/api/ttnn/decorators.hpp
+++ ttnn/api/ttnn/decorators.hpp
@@ -180,6 +180,7 @@ private:
         });
 
         if (future.wait_for(timeout) == std::future_status::timeout) {
+            pthread_kill(t.native_handle(), SIGKILL);
             t.detach();
             TT_THROW(
                 "TIMEOUT: ttnn operation {} timed out, potential hang detected, please check the graph capture",
