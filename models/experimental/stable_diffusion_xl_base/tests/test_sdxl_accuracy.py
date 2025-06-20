22040f1bacdb12c859fe6642bcc0fc404e0b2619
Aleksandar Cvejic <acvejic@tenstorrent.com>
2025-06-20 15:37:23 +0200

    change naming

--- models/experimental/stable_diffusion_xl_base/tests/test_sdxl_accuracy.py
+++ models/experimental/stable_diffusion_xl_base/tests/test_sdxl_accuracy.py
@@ -112,7 +112,7 @@ def test_accuracy_sdxl(
         },
     }
 
-    out_root, file_name = "test_reports", "benchmark_sdxl_results.json"
+    out_root, file_name = "test_reports", "benchmark_sdxl_report.json"
     os.makedirs(out_root, exist_ok=True)
 
     with open(f"{out_root}/{file_name}", "w") as f:
