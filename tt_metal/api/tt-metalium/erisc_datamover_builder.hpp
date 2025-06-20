2e37f8f3ef9df026a2ba7bc51bf27695ceec8c28
Yu Gao <145494740+yugaoTT@users.noreply.github.com>
2025-06-20 10:35:22 -0400

    #0: refactor fabric buffer config (#23793)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/23731)
    
    ### Problem description
    currently tests need to pass in separate args for enable different
    buffer configs, wrap them into one struct to avoid passing too many
    args.
    
    ### Checklist
    - [x] [All post commit]
    https://github.com/tenstorrent/tt-metal/actions/runs/15764137071
    - [ ] ubenchmark
    https://github.com/tenstorrent/tt-metal/actions/runs/15764143325

--- tt_metal/api/tt-metalium/erisc_datamover_builder.hpp
+++ tt_metal/api/tt-metalium/erisc_datamover_builder.hpp
@@ -71,15 +71,19 @@ enum class FabricEriscDatamoverAxis : std::size_t {
     Invalid = 2,
 };
 
-// enable extra buffer slots configuration based on sender/receiver channel and EDM type.
-struct FabricEriscDatamoverOptions {
-    FabricEriscDatamoverType edm_type = FabricEriscDatamoverType::Default;
-    FabricEriscDatamoverAxis edm_axis = FabricEriscDatamoverAxis::Short;
+struct FabricRouterBufferConfig {
     bool enable_dateline_sender_extra_buffer_slots = false;
     bool enable_dateline_receiver_extra_buffer_slots = false;
     bool enable_dateline_upstream_sender_extra_buffer_slots = false;
     bool enable_dateline_upstream_receiver_extra_buffer_slots = false;
     bool enable_dateline_upstream_adjacent_sender_extra_buffer_slots = false;
+};
+
+// enable extra buffer slots configuration based on sender/receiver channel and EDM type.
+struct FabricEriscDatamoverOptions {
+    FabricEriscDatamoverType edm_type = FabricEriscDatamoverType::Default;
+    FabricEriscDatamoverAxis edm_axis = FabricEriscDatamoverAxis::Short;
+    FabricRouterBufferConfig edm_buffer_config = FabricRouterBufferConfig{};
 };
 
 struct FabricEriscDatamoverConfig {
