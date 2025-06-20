dfca321de695c71bb1017e15dd3a56e81852c364
Yu Gao <145494740+yugaoTT@users.noreply.github.com>
2025-06-20 17:10:49 -0400

    Minor fabric  optimzations (#23849)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/23057)
    
    ### Problem description
    remove some riscv overhead for fabric kernel code. Use posted noc txn in
    downstream connection, so that when we do flush it only checks local
    chip noc txn.
    
    ### What's changed
    6U perf:
    ![Screenshot 2025-06-20 at 1 20
    46 PM](https://github.com/user-attachments/assets/4dc71e1c-d5e2-43a5-a735-fb4e329dcf58)
    
    
    [bandwidth_summary_6u_jun20.csv](https://github.com/user-attachments/files/20840569/bandwidth_summary_6u_jun20.csv)
    
    t3k:
    ![Screenshot 2025-06-20 at 1 21
    31 PM](https://github.com/user-attachments/assets/6ebe2463-adba-432f-b945-27579c43f60c)
    
    
    [fabric_edm_bandwidth_jun20.csv](https://github.com/user-attachments/files/20840568/fabric_edm_bandwidth_jun20.csv)
    
    
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes

--- tt_metal/fabric/hw/inc/edm_fabric/edm_fabric_utils.hpp
+++ tt_metal/fabric/hw/inc/edm_fabric/edm_fabric_utils.hpp
@@ -25,10 +25,10 @@ FORCE_INLINE void send_chunk_from_address_with_trid(
     uint8_t noc,
     uint8_t cmd_buf) {
     if constexpr (stateful_api) {
-        noc_async_write_one_packet_with_trid_with_state<false, false>(
+        noc_async_write_one_packet_with_trid_with_state<false, true>(
             local_l1_address, remote_l1_write_addr_l, page_size * num_pages, trid, cmd_buf, noc);
     } else {
-        noc_async_write_one_packet_with_trid<false, false>(
+        noc_async_write_one_packet_with_trid<false, true>(
             local_l1_address,
             get_noc_addr_helper(remote_l1_write_addr_h, remote_l1_write_addr_l),
             page_size * num_pages,
