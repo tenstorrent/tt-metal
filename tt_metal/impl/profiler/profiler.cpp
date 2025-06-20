bbea2d5d0831bc366461e6251c2fc504e58e50ed
Samarth Agarwal <sagarwal@tenstorrent.com>
2025-06-20 17:26:53 +0000

    Cleanup

--- tt_metal/impl/profiler/profiler.cpp
+++ tt_metal/impl/profiler/profiler.cpp
@@ -3,7 +3,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "dev_msgs.h"
-#include <common/TracyTTDeviceData.hpp>
 #include <device.hpp>
 #include <distributed.hpp>
 #include "device_pool.hpp"
@@ -740,7 +739,7 @@ void DeviceProfiler::logPacketDataToCSV(
     }
 
     log_file_ofs << fmt::format(
-        "{},{},{},{},{},{},{},{},{},{},{},{},\n",
+        "{},{},{},{},{},{},{},{},{},{},{},{},{}\n",
         device_id,
         core_x,
         core_y,
@@ -750,7 +749,7 @@ void DeviceProfiler::logPacketDataToCSV(
         data,
         run_host_id,
         zone_name,
-        // magic_enum::enum_name(packet_type),
+        magic_enum::enum_name(packet_type),
         source_line,
         source_file,
         metaDataStr);
