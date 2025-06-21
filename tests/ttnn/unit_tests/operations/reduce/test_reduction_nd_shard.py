7a7d7e253134c25c00a4fda06de0c20a35ee1051
Borys Bradel <bbradel@tenstorrent.com>
2025-06-21 12:57:28 +0000

    #22690: add reduction nd shard test

---
+++ tests/ttnn/unit_tests/operations/reduce/test_reduction_nd_shard.py
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+import pytest
+
+import torch
+from functools import partial
+
+import ttnn
+from tests.ttnn.utils_for_testing import assert_with_pcc
+from models.utility_functions import torch_random
+
+from tests.tt_eager.python_api_testing.sweep_tests.generation_funcs import gen_func_with_cast_tt
+
+
+@pytest.mark.parametrize(
+    "shapes",
+    [
+        ([1, 1, 512, 4096], [1, 1, 512, 256]),
+    ],
+)
+@pytest.mark.parametrize("keepdim", [True, False])
+def test_nd_shard(device, shapes, keepdim):
+    dim = -2
+    input_shape, shard_shape = shapes
+    torch_input_tensor = torch.rand(input_shape)
+    torch_output_tensor = torch.sum(torch_input_tensor, dim, keepdim)
+
+    memory_config = ttnn.MemoryConfig(
+        buffer_type=ttnn.BufferType.L1,
+        nd_shard_spec=ttnn.NdShardSpec(
+            shard_shape,
+            ttnn.CoreRangeSet({ttnn.CoreRange(ttnn.CoreCoord(0, 0), ttnn.CoreCoord(2, 4))}),
+        ),
+    )
+    input_tensor = ttnn.from_torch(
+        torch_input_tensor, dtype=ttnn.float32, device=device, layout=ttnn.TILE_LAYOUT, memory_config=memory_config
+    )
+    op_output_tensor = ttnn.sum(input_tensor, dim=dim, keepdim=keepdim)
+    output_tensor = ttnn.to_torch(op_output_tensor)
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc=0.999)
