397a0a36cfaf4ba97a2cce8db0d55f1975c7d0d4
Jaroslaw Bartosz Bieniek <jbbieniek@tenstorrent.com>
2025-06-23 18:39:39 +0000

    23050: use use_program_cache

--- tests/ttnn/unit_tests/operations/test_tosa_scatter.py
+++ tests/ttnn/unit_tests/operations/test_tosa_scatter.py
@@ -30,7 +30,7 @@ def select_torch_dtype(ttnn_dtype):
         (20, 40, 30, 10, ttnn.bfloat16, ttnn.uint32, ttnn.Layout.TILE),
     ],
 )
-def test_tosa_scatter_normal(N, K, W, C, input_dtype, index_dtype, input_layout, device):
+def test_tosa_scatter_normal(N, K, W, C, input_dtype, index_dtype, input_layout, device, use_program_cache):
     input_torch_dtype = select_torch_dtype(input_dtype)
 
     input_shape = [N, K, C]
@@ -49,7 +49,8 @@ def test_tosa_scatter_normal(N, K, W, C, input_dtype, index_dtype, input_layout,
     torch_index = torch_index.unsqueeze(-1).expand([N, W, C])
 
     torch_output = torch.scatter(torch_input, dim=dim, index=torch_index, src=torch_source)
-    device.enable_program_cache()
+    if use_program_cache:
+        device.enable_program_cache()
     for _ in range(2):
         ttnn_output = ttnn.experimental.tosa_scatter(ttnn_input, ttnn_index, ttnn_source)
         assert ttnn_output.shape == ttnn_input.shape
