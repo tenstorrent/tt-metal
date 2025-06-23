f4b8056a3b1a34a9f4d2f3587d0f643e5388845c
Aliaksei Sala <aliaksei_sala@epam.com>
2025-06-23 15:58:05 -0700

    ttnn.where native implementation (#22329)
    ### Ticket
    [Write native ttnn.where operation
    Issue](https://github.com/tenstorrent/tt-metal/issues/20868)
    
    ### Problem description
    The current ttnn.where implementation uses a composite pattern,
    dispatching multiple operations to the device, which introduces overhead
    and reduces performance.
    A native, single-kernel implementation will improve the performance.
    
    ### What's changed
    
    - Implemented ttnn::experimental::where operation
        - Implemented WhereDeviceOperation
    - Added a program factory and program implementation that supports
    true_values and false_values as tensors
    - Developed native kernels for read, write, and compute operations
    specific to this use case
    - Included performance benchmarks
    - Added corresponding unit tests
    
    ### What's is not supported
    
    - Integer data types (Integer types are not supported by the underlying
    element-wise multiplication operation.)
    - bfloat4_b type (ttnn.from_torch(a, dtype=ttnn.bfloat4_b,
    layout=ttnn.TILE_LAYOUT) doesn't work as expected)
    - Row-major tensor layout (The underlying unary operations (gtz,lez)
    only support the TILE layout.)
    - Sharding strategy
    - Broadcasting support
    - Overload variants using scalar values
    
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI passes (if applicable)
    - [ ] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: Artem Yerofieiev <169092593+ayerofieiev-tt@users.noreply.github.com>

--- ttnn/cpp/ttnn/operations/experimental/experimental_pybind.cpp
+++ ttnn/cpp/ttnn/operations/experimental/experimental_pybind.cpp
@@ -51,6 +51,7 @@
 #include "ttnn/operations/experimental/gather/gather_pybind.hpp"
 #include "ttnn/operations/experimental/gather/tosa/gather_tosa_pybind.hpp"
 #include "ttnn/operations/experimental/padded_slice/padded_slice_pybind.hpp"
+#include "ttnn/operations/experimental/where/where_pybind.hpp"
 
 namespace py = pybind11;
 
@@ -123,6 +124,8 @@ void py_module(py::module& module) {
     ccl::py_module(m_experimental_ccl);
 
     broadcast_to::detail::py_bind_broadcast_to(module);
+
+    operations::experimental::ternary::detail::bind_where(module);
 }
 
 }  // namespace ttnn::operations::experimental
---
+++ ttnn/cpp/ttnn/operations/experimental/where/device/program_factory/elemwise_factory_common.hpp
@@ -0,0 +1,143 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "ttnn/operations/experimental/where/device/kernels/dataflow/elemwise_reader_kernel_args.hpp"
+#include "ttnn/operations/experimental/where/device/kernels/dataflow/elemwise_writer_kernel_args.hpp"
+#include "ttnn/operations/experimental/where/device/kernels/compute/elemwise_where_kernel_args.hpp"
+
+#include "ttnn/tensor/tensor.hpp"
+
+#include <tt-metalium/work_split.hpp>
+#include <tt-metalium/constants.hpp>
+#include <tt-metalium/util.hpp>
+#include <tt-metalium/host_api.hpp>
+
+#include <optional>
+
+namespace ttnn::operations::experimental::ternary {
+
+template <bool initialize_args>
+inline void set_eltwise_ternary_runtime_args(
+    tt::tt_metal::Program& program,
+    const Tensor& condition_tensor,
+    const Tensor& true_value_tensor,
+    const Tensor& false_value_tensor,
+    const Tensor& output,
+    const tt::tt_metal::KernelHandle reader_kernel_id,
+    const tt::tt_metal::KernelHandle writer_kernel_id,
+    const tt::tt_metal::KernelHandle compute_kernel_id,
+    const CoreRangeSet& all_device_cores) {
+    using namespace ttnn::kernel::eltwise::where_args;
+    using namespace ttnn::kernel_utils;
+    using namespace tt;
+    using namespace tt::tt_metal;
+    using namespace tt::constants;
+
+    // zero_start_grid is a flag to indicate that we are using a single rectangular grid that starts at (0, 0)
+    // as well as having the sharded tensors (if any) start at (0, 0)
+    // This will run the original work/core distribution algorithms that are specifically for this setup, as these
+    // are faster than the generic work/core distribution algorithms that work on arbitrary CoreRangeSets
+    bool zero_start_grid = false;
+    CoreCoord compute_with_storage_grid_size;
+    if (all_device_cores.size() == 1) {
+        const auto& cr = *all_device_cores.ranges().begin();
+        if (cr.start_coord.x == 0 && cr.start_coord.y == 0) {
+            zero_start_grid = true;
+            compute_with_storage_grid_size = CoreCoord(cr.end_coord.x + 1, cr.end_coord.y + 1);
+        }
+    }
+
+    uint32_t num_tiles = condition_tensor.volume() / TILE_HW;
+    bool row_major = true;
+    auto [num_cores, all_cores, core_group_1, core_group_2, num_tiles_per_core_group_1, num_tiles_per_core_group_2] =
+        zero_start_grid ? tt::tt_metal::split_work_to_cores(compute_with_storage_grid_size, num_tiles, row_major)
+                        : tt::tt_metal::split_work_to_cores(all_device_cores, num_tiles, row_major);
+
+    uint32_t num_cores_total = zero_start_grid ? compute_with_storage_grid_size.x * compute_with_storage_grid_size.y
+                                               : all_device_cores.num_cores();
+    auto cores =
+        zero_start_grid
+            ? grid_to_cores(
+                  num_cores_total, compute_with_storage_grid_size.x, compute_with_storage_grid_size.y, row_major)
+            : corerange_to_cores(all_device_cores, {}, row_major);
+
+    std::vector<std::vector<uint32_t>> reader_args_array{
+        cores.size(), std::vector<uint32_t>{amount_of_fields<ElemwiseReaderKernelArgs>(), 0}};
+    std::vector<std::vector<uint32_t>> compute_args_array{
+        cores.size(), std::vector<uint32_t>(amount_of_fields<ElemwiseComputeKernelArgs>(), 0)};
+    std::vector<std::vector<uint32_t>> writer_args_array{
+        cores.size(), std::vector<uint32_t>(amount_of_fields<ElemwiseWriterKernelArgs>(), 0)};
+
+    auto& cached_reader_args = GetRuntimeArgs(program, reader_kernel_id);
+    auto& cached_eltwise_args = GetRuntimeArgs(program, compute_kernel_id);
+    auto& cached_writer_args = GetRuntimeArgs(program, writer_kernel_id);
+
+    for (uint32_t i = 0, tile_ofs = 0; i < num_cores_total; ++i) {
+        const CoreCoord& core = cores.at(i);
+
+        uint32_t block_cnt_per_core = 0;
+        uint32_t block_size_per_core = 1;
+        uint32_t num_tiles_per_core = 0;
+
+        if (i < core_group_1.num_cores()) {
+            num_tiles_per_core = num_tiles_per_core_group_1;
+            block_cnt_per_core = num_tiles_per_core_group_1;
+        } else if (i < num_cores) {
+            num_tiles_per_core = num_tiles_per_core_group_2;
+            block_cnt_per_core = num_tiles_per_core_group_2;
+        } else {
+            // Zero out non-working cores RT args. Only necessary in override
+            // since initialization pushes zero vectors to unused cores.
+            if constexpr (!initialize_args) {
+                // RuntimeArgsData
+                auto& reader_args = cached_reader_args.at(core.x).at(core.y);
+                reader_args[2] = 0;
+                auto& eltwise_args = cached_eltwise_args.at(core.x).at(core.y);
+                eltwise_args[0] = 0;
+                auto& writer_args = cached_writer_args.at(core.x).at(core.y);
+                writer_args[1] = 0;
+            }
+            continue;
+        }
+
+        ElemwiseReaderKernelArgs read_kern_args = {
+            .condition_tensor_base_addr = condition_tensor.buffer()->address(),
+            .true_tensor_base_addr = true_value_tensor.buffer()->address(),
+            .false_tensor_base_addr = false_value_tensor.buffer()->address(),
+            .num_tiles = num_tiles_per_core,
+            .tile_ofs = tile_ofs};
+
+        ElemwiseComputeKernelArgs compute_kern_args = {
+            .per_core_block_cnt = block_cnt_per_core, .per_core_block_size = block_size_per_core};
+
+        ElemwiseWriterKernelArgs write_kern_args = {
+            .dst_base_addr = output.buffer()->address(), .num_tiles = num_tiles_per_core, .tile_ofs = tile_ofs};
+
+        reader_args_array[i] = to_vector(read_kern_args);
+        compute_args_array[i] = to_vector(compute_kern_args);
+        writer_args_array[i] = to_vector(write_kern_args);
+        if constexpr (!initialize_args) {
+            auto& core_reader_args = cached_reader_args.at(core.x).at(core.y);
+            std::ranges::copy(reader_args_array[i], core_reader_args.data());
+
+            auto& core_eltwise_args = cached_eltwise_args.at(core.x).at(core.y);
+            std::ranges::copy(compute_args_array[i], core_eltwise_args.data());
+
+            auto& core_writer_args = cached_writer_args.at(core.x).at(core.y);
+            std::ranges::copy(writer_args_array[i], core_writer_args.data());
+        }
+
+        tile_ofs += num_tiles_per_core;
+    }
+
+    if constexpr (initialize_args) {
+        SetRuntimeArgs(program, reader_kernel_id, cores, reader_args_array);
+        SetRuntimeArgs(program, compute_kernel_id, cores, compute_args_array);
+        SetRuntimeArgs(program, writer_kernel_id, cores, writer_args_array);
+    }
+}
+
+}  // namespace ttnn::operations::experimental::ternary
