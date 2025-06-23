f5cc53d9044ca3fe89216c2248b83d8eb73133e5
Aswinmcw <azayasankaran@tenstorrent.com>
2025-06-23 11:27:18 +0000

    fixes

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_unary_comp.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_unary_comp.h
@@ -104,7 +104,7 @@ inline void llk_math_eltwise_unary_sfpu_unary_ge_init() {
 template <bool APPROXIMATE>
 inline void llk_math_eltwise_unary_sfpu_unary_ge(uint dst_index, uint param0, int vector_mode = (int)VectorMode::RC) {
     llk_math_eltwise_unary_sfpu_params<APPROXIMATE>(
-        ckernel::sfpu::calculate_unary_ge<APPROXIMATE>, dst_index, vector_mode, param0);
+        ckernel::sfpu::_calculate_comp_unary_<APPROXIMATE>, dst_index, vector_mode, param0);
 }
 
 // Unary lesser than or equal to
@@ -123,6 +123,6 @@ inline void llk_math_eltwise_unary_sfpu_unary_le_init() {
 template <bool APPROXIMATE>
 inline void llk_math_eltwise_unary_sfpu_unary_le(uint dst_index, uint param0, int vector_mode = (int)VectorMode::RC) {
     llk_math_eltwise_unary_sfpu_params<APPROXIMATE>(
-        ckernel::sfpu::calculate_unary_le<APPROXIMATE>, dst_index, vector_mode, param0);
+        ckernel::sfpu::_calculate_comp_unary_<APPROXIMATE>, dst_index, vector_mode, param0);
 }
 }  // namespace ckernel
