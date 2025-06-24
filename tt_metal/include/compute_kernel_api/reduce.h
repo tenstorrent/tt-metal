c898ee0cce8b380a0e5cf9e7400be215ac5024a4
Radomir Djogo <rdjogo@tenstorrent.com>
2025-06-24 21:46:09 +0000

    Update API docs with new param

--- tt_metal/include/compute_kernel_api/reduce.h
+++ tt_metal/include/compute_kernel_api/reduce.h
@@ -35,13 +35,14 @@ namespace ckernel {
  *
  * Return value: None
  *
- * | Param Type | Name         | Description                                                     | Type      | Valid Range                                    | Required |
- * |------------|--------------|-----------------------------------------------------------------|-----------|------------------------------------------------|----------|
- * | Template   | reduce_type  | The type of reduce op - sum, average or maximum                 | PoolType  | {SUM, AVG, MAX}                                | True     |
- * | Template   | reduce_dim   | The dimension of reduce op - row, column or both                | ReduceDim | {REDUCE_ROW, REDUCE_COL, REDUCE_SCALAR}        | True     |
- * | Function   | icb          | The identifier of the circular buffer (CB) containing operand A | uint32_t  | 0 to 31                                        | True     |
- * | Function   | icb_scaler   | CB holding scaling factors (see above)                          | uint32_t  | 0 to 31                                        | True     |
- * | Function   | ocb          | The identifier of the output circular buffer (CB)               | uint32_t  | 0 to 31                                        | True     |
+ * | Param Type | Name           | Description                                                     | Type      | Valid Range                                    | Required |
+ * |------------|----------------|-----------------------------------------------------------------|-----------|------------------------------------------------|----------|
+ * | Template   | reduce_type    | The type of reduce op - sum, average or maximum                 | PoolType  | {SUM, AVG, MAX}                                | True     |
+ * | Template   | reduce_dim     | The dimension of reduce op - row, column or both                | ReduceDim | {REDUCE_ROW, REDUCE_COL, REDUCE_SCALAR}        | True     |
+ * | Template   | fp32_transpose | Enable accumulation of reduction in full FP32 precision         | bool      | {true, false}                                  | True     |
+ * | Function   | icb            | The identifier of the circular buffer (CB) containing operand A | uint32_t  | 0 to 31                                        | True     |
+ * | Function   | icb_scaler     | CB holding scaling factors (see above)                          | uint32_t  | 0 to 31                                        | True     |
+ * | Function   | ocb            | The identifier of the output circular buffer (CB)               | uint32_t  | 0 to 31                                        | True     |
  */
 // clang-format on
 template <PoolType reduce_type = REDUCE_OP, ReduceDim reduce_dim = REDUCE_DIM, bool fp32_transpose = false>
@@ -50,6 +51,22 @@ ALWI void reduce_init(uint32_t icb, uint32_t icb_scaler, uint32_t ocb) {
     MATH((llk_math_reduce_init<reduce_type, reduce_dim, MATH_FIDELITY>()));
     PACK((llk_pack_reduce_mask_config<false /*untilize*/, reduce_dim>()));
 }
+
+// clang-format off
+/**
+ * Resets the packer edge mask configuration to its default state by clearing any previously set masks. Needs to be called after
+ * reduce_tile if the next operation requires default packer state. In case that the next operation is reduce operation across the
+ * same dimension, this call can be omitted. If this function is not called, the packer will continue to use the edge masks set
+ * by the latest reduce_init call, which may lead to incorrect packing behavior in subsequent operations.
+ *
+ * NOTE: This function is not in line with our programming model, and will be removed by the end of 2025.
+ *
+ * | Param Type | Name | Description                                      | Type | Valid Range | Required |
+ * |------------|------|--------------------------------------------------|------|-------------|----------|
+ * | Function   | -    | No parameters                                    |  -   |      -      |    -     |
+ */
+// clang-format on
+ALWI void reduce_uninit() { PACK((llk_pack_reduce_mask_clear())); }
 
 // clang-format off
 /**
@@ -74,15 +91,16 @@ ALWI void reduce_init(uint32_t icb, uint32_t icb_scaler, uint32_t ocb) {
  * NOTE: For other valid ways of populating the `icb_scaler`, refer to the ISA documentation.
  * Return value: None
  *
- * | Param Type | Name     | Description                                                     | Type     | Valid Range                                    | Required |
- * |------------|----------|-----------------------------------------------------------------|----------|------------------------------------------------|----------|
- * | Template   | reduce_type | The type of reduce op - sum, average or maximum              | PoolType | {SUM, AVG, MAX}                                | True     |
- * | Template   | reduce_dim  | The dimension of reduce op - row, column or both             | ReduceDim| {REDUCE_ROW, REDUCE_COL, REDUCE_SCALAR}        | True     |
- * | Function   | icb      | The identifier of the circular buffer (CB) containing operand A | uint32_t | 0 to 31                                        | True     |
- * | Function   | icb_scaler  | CB holding scaling factors (see above)                          | uint32_t | 0 to 31                                        | True     |
- * | Function   | itile    | The index of the tile within the first CB                       | uint32_t | Must be less than the size of the CB           | True     |
- * | Function   | itile_sclaer | The index of the tile within the scaling factor CB.             | uint32_t | Must be less than the size of the CB           | True     |
- * | Function   | idst     | The index of the tile in DST REG for the result                 | uint32_t | Must be less than the acquired size of DST REG | True     |
+ * | Param Type | Name           | Description                                                     | Type      | Valid Range                                    | Required |
+ * |------------|----------------|-----------------------------------------------------------------|-----------|------------------------------------------------|----------|
+ * | Template   | reduce_type    | The type of reduce op - sum, average or maximum                 | PoolType  | {SUM, AVG, MAX}                                | True     |
+ * | Template   | reduce_dim     | The dimension of reduce op - row, column or both                | ReduceDim | {REDUCE_ROW, REDUCE_COL, REDUCE_SCALAR}        | True     |
+ * | Template   | fp32_transpose | Enable accumulation of reduction in full FP32 precision         | bool      | {true, false}                                  | True     |
+ * | Function   | icb            | The identifier of the circular buffer (CB) containing operand A | uint32_t  | 0 to 31                                        | True     |
+ * | Function   | icb_scaler     | CB holding scaling factors (see above)                          | uint32_t  | 0 to 31                                        | True     |
+ * | Function   | itile          | The index of the tile within the first CB                       | uint32_t  | Must be less than the size of the CB           | True     |
+ * | Function   | itile_sclaer   | The index of the tile within the scaling factor CB.             | uint32_t  | Must be less than the size of the CB           | True     |
+ * | Function   | idst           | The index of the tile in DST REG for the result                 | uint32_t  | Must be less than the acquired size of DST REG | True     |
  */
 // clang-format on
 template <PoolType reduce_type = REDUCE_OP, ReduceDim reduce_dim = REDUCE_DIM, bool fp32_transpose = false>
@@ -95,12 +113,13 @@ ALWI void reduce_tile(uint32_t icb, uint32_t icb_scaler, uint32_t itile, uint32_
 /**
  * Performs a math-only reduction operation on a tile in the DST register. Assumes that source tiles are already in source registers.
  *
- * | Param Type | Name         | Description                                                     | Type      | Valid Range                                    | Required |
- * |------------|--------------|-----------------------------------------------------------------|-----------|------------------------------------------------|----------|
- * | Template   | reduce_type  | The type of reduce op - sum, average or maximum                 | PoolType  | {SUM, AVG, MAX}                                | True     |
- * | Template   | reduce_dim   | The dimension of reduce op - row, column or both                | ReduceDim | {REDUCE_ROW, REDUCE_COL, REDUCE_SCALAR}        | True     |
- * | Function   | idst         | The index of the tile in DST REG for the result                 | uint32_t  | Must be less than the acquired size of DST REG | True     |
- * | Function   | num_faces    | Number of faces to reduce (optional, default 4)                 | uint32_t  | >= 1                                           | False    |
+ * | Param Type | Name           | Description                                                     | Type      | Valid Range                                    | Required |
+ * |------------|----------------|-----------------------------------------------------------------|-----------|------------------------------------------------|----------|
+ * | Template   | reduce_type    | The type of reduce op - sum, average or maximum                 | PoolType  | {SUM, AVG, MAX}                                | True     |
+ * | Template   | reduce_dim     | The dimension of reduce op - row, column or both                | ReduceDim | {REDUCE_ROW, REDUCE_COL, REDUCE_SCALAR}        | True     |
+ * | Template   | fp32_transpose | Enable accumulation of reduction in full FP32 precision         | bool      | {true, false}                                  | True     |
+ * | Function   | idst           | The index of the tile in DST REG for the result                 | uint32_t  | Must be less than the acquired size of DST REG | True     |
+ * | Function   | num_faces      | Number of faces to reduce (optional, default 4)                 | uint32_t  | >= 1                                           | False    |
  */
 // clang-format on
 template <PoolType reduce_type = REDUCE_OP, ReduceDim reduce_dim = REDUCE_DIM, bool fp32_transpose = false>
