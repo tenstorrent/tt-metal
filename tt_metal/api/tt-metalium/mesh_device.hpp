ad7dada15be033904d2ee3504b69efdc679c35ce
Diego Gomez <dgomez@tenstorrent.com>
2025-06-24 12:47:00 -0400

    Trying to reset the device in cpp

--- tt_metal/api/tt-metalium/mesh_device.hpp
+++ tt_metal/api/tt-metalium/mesh_device.hpp
@@ -147,7 +147,6 @@ public:
     MeshDevice(MeshDevice&&) = delete;
     MeshDevice& operator=(MeshDevice&&) = delete;
 
-    // IDevice interface implementation
     tt::ARCH arch() const override;
     int id() const override;
     chip_id_t build_id() const override;
