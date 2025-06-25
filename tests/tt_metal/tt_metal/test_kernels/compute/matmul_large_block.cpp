1ca7f9372b151dfd3210962b34b88682f2cd9ac9
Nikola Cvetkovic <ncvetkovic@tenstorrent.com>
2025-06-25 14:36:21 +0200

    #22219: Tilize init cleanup - first pass (#22850)
    ### Ticket
    #22940 
    
    ### Problem description
    Compute API currently has a lot of unused or redundant function
    arguments, and even unused functions. Furthermore, the programming model
    is violated at several places and the boundary between full and short
    inits is not clear. Lastly, some ops require calling an
    `_uninit`/`_revert` function, but not all, which introduces more
    confusion. We will do a series of PRs to fix these issues for all ops so
    that in the end Compute kernels look like this:
    ```
    compute_kernel_hw_startup # called once at the start of the kernel
    <op1>_init # called before op1
    <op1>_tile / <op1>_block # We will add block ops where applicable
    <op2>_init
    <op2>_tile / <op2>_block
    ...
    ```
    
    ### What's changed
    This PR addresses the mentioned drawbacks of Compute API for **tilize**
    op. It is the initial PR that doesn't fix all the problems, but does the
    following:
    
    * Uses `compute_kernel_hw_startup` function at kernel beginnings.
    * `tilize_init_short` renamed to `tilize_init` and old full init
    removed.
    * Remove unused Compute API like `tilize_init_unpack` and
    `unpack_tilize_block`
    * Adapt API calls in all kernels to reflect the changes.
    * Add documentation skeleton for functions that will remain in
    `tilize.h` at the end of the effort.
    
    ### Review Process Suggestion:
    * Code owners should check their files and see whether all `tilize_`
    init and uninit calls are modified. All `tilize_init_short` calls should
    be swapped with `tilize_init` calls.
    * LLK team will review `tilize.h` for functional, structural and code
    documentation changes.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes -
    https://github.com/tenstorrent/tt-metal/actions/runs/15873590382
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable) -
    https://github.com/tenstorrent/tt-metal/actions/runs/15873549061
    - [x] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable) - waiting for CI to get fixed
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable) - waiting for CI to get fixed
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes

--- tests/tt_metal/tt_metal/test_kernels/compute/matmul_large_block.cpp
+++ tests/tt_metal/tt_metal/test_kernels/compute/matmul_large_block.cpp
@@ -13,7 +13,7 @@
 
 inline void tilize_activation(
     uint32_t in0_cb, uint32_t in0_subblock_h, uint32_t in0_block_w, uint32_t in0_num_subblocks, uint32_t out_cb) {
-    tilize_init_short(in0_cb, in0_block_w, out_cb);
+    tilize_init(in0_cb, in0_block_w, out_cb);
 
     for (uint32_t in0_subblock = 0; in0_subblock < in0_num_subblocks; in0_subblock++) {
         for (uint32_t h = 0; h < in0_subblock_h; h++) {
