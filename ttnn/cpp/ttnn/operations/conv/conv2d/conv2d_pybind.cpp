165a3fa8abbb77aa7a42625ab74487d07a1acaf7
Sankar Manoj <smanoj@tenstorrent.com>
2025-06-25 15:38:28 +0000

    #0: Revert

--- ttnn/cpp/ttnn/operations/conv/conv2d/conv2d_pybind.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/conv2d_pybind.cpp
@@ -19,7 +19,6 @@
 #include "ttnn/operations/conv/conv2d/device/conv2d_op.hpp"
 #include "ttnn/operations/conv/conv2d/prepare_conv2d_weights.hpp"
 #include "ttnn/operations/sliding_window/sliding_window_pybind.hpp"
-#include "ttnn/tensor/enum_types.hpp"
 #include "ttnn/types.hpp"
 #include <tt-metalium/constants.hpp>
 
@@ -403,7 +402,6 @@ void py_bind_conv2d(py::module& module) {
         | The corresponding input tensor needed to calculate that output is determined and sliced.
         | If the size of the slice dimension is not divisible by num_slices, then the last slice will be smaller than the rest.
         )doc");
-
     py::enum_<Conv2dSliceConfig::SliceType>(py_conv_slice_config, "SliceTypeEnum")
         .value("SliceHeight", Conv2dSliceConfig::SliceType::HEIGHT)
         .value("SliceWidth", Conv2dSliceConfig::SliceType::WIDTH);
