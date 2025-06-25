1a1ecc994b4b769899f1cfa44fa7e516b99affcd
Rui Zhang <ruizhang@tenstorrent.com>
2025-06-25 20:22:37 +0000

    Fix noc_get_interim_inline_value_addr for dynamic NOC
    With dynamic noc, both BRISC and NCRISC can use either NOC.
    If they use the same NOC for an inline transaction, there may be a data race
    because the interim data storage returned by noc_get_interim_inline_value_addr
    is the same.  To fix the issue, allocate separate space for BRISC and NCRISC.

--- tt_metal/hw/inc/blackhole/noc_nonblocking_api.h
+++ tt_metal/hw/inc/blackhole/noc_nonblocking_api.h
@@ -142,7 +142,15 @@ inline __attribute__((always_inline)) uint32_t noc_get_interim_inline_value_addr
     // needs to respect 4B alignment.
     ASSERT((dst_noc_addr & 0x3) == 0);
     uint32_t offset = dst_noc_addr & 0xF;
-    uint32_t src_addr = (uint32_t)MEM_L1_INLINE_BASE + (uint32_t)(noc * MEM_L1_INLINE_SIZE_PER_NOC) + offset;
+    uint32_t src_addr;
+#if defined(COMPILE_FOR_BRISC)
+    src_addr = MEM_BRISC_L1_INLINE_BASE;
+#elif defined(COMPILE_FOR_NCRISC)
+    src_addr = MEM_NCRISC_L1_INLINE_BASE;
+#else
+    ASSERT(0);
+#endif
+    src_addr += noc * MEM_L1_INLINE_SIZE_PER_NOC + offset;
     return src_addr;
 }
 
