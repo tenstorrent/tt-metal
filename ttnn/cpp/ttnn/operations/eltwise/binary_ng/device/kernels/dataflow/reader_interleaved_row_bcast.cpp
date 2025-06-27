7bd49d1cfa567a99073d62b72b0dbcd035c60eeb
Patrick Roberts <proberts@tenstorrent.com>
2025-06-27 22:16:31 +0000

    #21967: Support 5D broadcast

--- ttnn/cpp/ttnn/operations/eltwise/binary_ng/device/kernels/dataflow/reader_interleaved_row_bcast.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/binary_ng/device/kernels/dataflow/reader_interleaved_row_bcast.cpp
@@ -14,13 +14,15 @@ void kernel_main() {
     const uint32_t dst_num_tiles = get_arg_val<uint32_t>(3);
     const uint32_t dst_shard_width = get_arg_val<uint32_t>(4);
     const uint32_t nD_stride = get_arg_val<uint32_t>(5);
-    const uint32_t n_stride = get_arg_val<uint32_t>(6);
-    const uint32_t c_stride = get_arg_val<uint32_t>(7);
-    const uint32_t N = get_arg_val<uint32_t>(8);
-    const uint32_t C = get_arg_val<uint32_t>(9);
-    const uint32_t Ht = get_arg_val<uint32_t>(10);
-    const uint32_t Wt = get_arg_val<uint32_t>(11);
-    const uint32_t cND = get_arg_val<uint32_t>(12);  // collapsed dims > 4
+    const uint32_t d_stride = get_arg_val<uint32_t>(6);
+    const uint32_t n_stride = get_arg_val<uint32_t>(7);
+    const uint32_t c_stride = get_arg_val<uint32_t>(8);
+    const uint32_t D = get_arg_val<uint32_t>(9);
+    const uint32_t N = get_arg_val<uint32_t>(10);
+    const uint32_t C = get_arg_val<uint32_t>(11);
+    const uint32_t Ht = get_arg_val<uint32_t>(12);
+    const uint32_t Wt = get_arg_val<uint32_t>(13);
+    const uint32_t cND = get_arg_val<uint32_t>(14);  // collapsed dims > 5
     const uint32_t HtWt = Ht * Wt;
 
     constexpr bool src_is_dram = get_compile_time_arg_val(0) == 1;
@@ -33,41 +35,48 @@ void kernel_main() {
     const InterleavedAddrGenFast<src_is_dram> src = {
         .bank_base_address = src_addr, .page_size = src_tile_bytes, .data_format = src_data_format};
 
-    const uint32_t tiles_per_depth = N * C * HtWt;
-    uint32_t start_d = start_tile_id / tiles_per_depth;  // ND index
-    uint32_t start_remaining_1 = start_tile_id % tiles_per_depth;
-    uint32_t tiles_per_batch = HtWt * C;
-    uint32_t start_n = start_remaining_1 / tiles_per_batch;  // N index
-    uint32_t start_remaining_2 = start_remaining_1 % tiles_per_batch;
-    uint32_t tiles_per_channel = HtWt;
-    uint32_t start_c = start_remaining_2 / tiles_per_channel;  // C index
-    uint32_t start_t = start_remaining_2 % tiles_per_channel;  // tile index within HtWt
-    uint32_t start_th = start_t / Wt;                          // H index
-    uint32_t start_tw = start_t % Wt;                          // W index
+    const uint32_t tiles_per_n = C * HtWt;
+    const uint32_t tiles_per_d = N * tiles_per_n;
+    const uint32_t tiles_per_nd = D * tiles_per_d;
+    const uint32_t offset_nd = start_tile_id % tiles_per_nd;
+    const uint32_t offset_d = offset_nd % tiles_per_d;
+    const uint32_t offset_n = offset_d % tiles_per_n;
+    const uint32_t offset_c = offset_n % HtWt;
+    uint32_t start_nd = start_tile_id / tiles_per_nd;
+    uint32_t start_d = offset_nd / tiles_per_d;
+    uint32_t start_n = offset_d / tiles_per_n;
+    uint32_t start_c = offset_n / HtWt;
+    uint32_t start_th = offset_c / Wt;
+    uint32_t start_tw = offset_c % Wt;
 
     // this is the INPUT tile offset
-    uint32_t tile_offset = start_d * nD_stride + start_n * n_stride + start_c * c_stride;
-    uint32_t next_batch_shift = n_stride - c_stride * C;
-    uint32_t next_depth_shift = nD_stride - (n_stride * N);
+    uint32_t tile_offset = start_nd * nD_stride + start_d * d_stride + start_n * n_stride + start_c * c_stride;
+    uint32_t next_n_shift = n_stride - c_stride * C;
+    uint32_t next_d_shift = d_stride - n_stride * N;
+    uint32_t next_nd_shift = nD_stride - d_stride * D;
 
     uint32_t num_tiles_read = 0;
-    for (uint32_t nd = start_d; nd < cND && num_tiles_read < dst_num_tiles; ++nd, start_n = 0) {
-        for (uint32_t n = start_n; n < N && num_tiles_read < dst_num_tiles; ++n, start_c = 0) {
-            for (uint32_t c = start_c; c < C && num_tiles_read < dst_num_tiles; ++c, start_th = 0) {
-                for (uint32_t th = start_th; th < Ht && num_tiles_read < dst_num_tiles; ++th, start_tw = 0) {
-                    for (uint32_t tw = start_tw; tw < Wt && num_tiles_read < dst_num_tiles; ++tw, ++num_tiles_read) {
-                        cb_reserve_back(cb_id_src, onetile);
-                        uint32_t l1_write_addr_src = get_write_ptr(cb_id_src);
-                        noc_async_read_tile(tile_offset + tw, src, l1_write_addr_src);
-                        noc_async_read_barrier();
-                        FILL_TILE_WITH_FIRST_ROW(cb_id_src);
-                        cb_push_back(cb_id_src, onetile);
+    for (uint32_t nd = start_nd; nd < cND && num_tiles_read < dst_num_tiles; ++nd, start_d = 0) {
+        for (uint32_t d = start_d; d < D && num_tiles_read < dst_num_tiles; ++d, start_n = 0) {
+            for (uint32_t n = start_n; n < N && num_tiles_read < dst_num_tiles; ++n, start_c = 0) {
+                for (uint32_t c = start_c; c < C && num_tiles_read < dst_num_tiles; ++c, start_th = 0) {
+                    for (uint32_t th = start_th; th < Ht && num_tiles_read < dst_num_tiles; ++th, start_tw = 0) {
+                        for (uint32_t tw = start_tw; tw < Wt && num_tiles_read < dst_num_tiles;
+                             ++tw, ++num_tiles_read) {
+                            cb_reserve_back(cb_id_src, onetile);
+                            uint32_t l1_write_addr_src = get_write_ptr(cb_id_src);
+                            noc_async_read_tile(tile_offset + tw, src, l1_write_addr_src);
+                            noc_async_read_barrier();
+                            FILL_TILE_WITH_FIRST_ROW(cb_id_src);
+                            cb_push_back(cb_id_src, onetile);
+                        }
                     }
+                    tile_offset += c_stride;
                 }
-                tile_offset += c_stride;
+                tile_offset += next_n_shift;
             }
-            tile_offset += next_batch_shift;
+            tile_offset += next_d_shift;
         }
-        tile_offset += next_depth_shift;
+        tile_offset += next_nd_shift;
     }
 }
