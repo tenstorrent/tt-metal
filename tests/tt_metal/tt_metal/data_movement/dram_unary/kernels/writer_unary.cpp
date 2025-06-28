c523088f715fe87e89f5abf85818f4c0e04924df
Ata Tuzuner <atuzuner@tenstorrent.com>
2025-06-28 00:01:32 -0400

    [DM]: Adding static plots of sweep tests (#23861)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/23506)
    
    ### Problem description
    We want to harbour static plots of our primitive test performance
    results to which we can point developers when needed. Currently, our
    script outputs all test plots into one PNG file. We should modify the
    script to output separate PNG files for each test that is run.
    
    ### What's changed
    Created a `perf_plots` directory in the `documentation` folder under
    which I placed the plots for `PacketSizes` tests for each arch.
    
    Increased max transaction size in each test to 64 KB. To accommodate
    this, tests were modified to allocate one (or two depending on loopback)
    transaction size of memory in their respective L1. Relevant PCC checks
    were modified to operate on only the last transaction.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/15935992937)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/15935995670)
    CI passes (if applicable)
    - [x] [metal - Run
    microbenchmarks](https://github.com/tenstorrent/tt-metal/actions/runs/15935997718)

--- tests/tt_metal/tt_metal/data_movement/dram_unary/kernels/writer_unary.cpp
+++ tests/tt_metal/tt_metal/data_movement/dram_unary/kernels/writer_unary.cpp
@@ -6,7 +6,7 @@
 
 // L1 to DRAM write
 void kernel_main() {
-    uint32_t dst_addr = get_compile_time_arg_val(0);
+    constexpr uint32_t dst_addr = get_compile_time_arg_val(0);
     constexpr uint32_t bank_id = get_compile_time_arg_val(1);
     constexpr uint32_t num_of_transactions = get_compile_time_arg_val(2);
     constexpr uint32_t transaction_num_pages = get_compile_time_arg_val(3);
@@ -25,18 +25,9 @@ void kernel_main() {
     uint32_t l1_read_addr = get_read_ptr(cb_id_out0);
     {
         DeviceZoneScopedN("RISCV0");
-        uint64_t dst_base_noc_addr = get_noc_addr_from_bank_id<true>(bank_id, 0);
+        uint64_t dst_noc_addr = get_noc_addr_from_bank_id<true>(bank_id, dst_addr);
         for (uint32_t i = 0; i < num_of_transactions; i++) {
-            /* The 64-bit NOC addresses consists of a 32-bit local address and a NOC XY coordinate. The local address
-             * occupies the lower 32 bits and the NOC XY coordinate occupies the next 12 (unicast) to 24 (multicast)
-             * bits. In the get_noc_addr call, we set the local address to 0 to get the base address. Then, we OR it
-             * with the local address (dst_addr) in each iteration to get the full NOC address. */
-            uint64_t dst_noc_addr = dst_base_noc_addr | dst_addr;
-
             noc_async_write(l1_read_addr, dst_noc_addr, transaction_size_bytes);
-
-            l1_read_addr += transaction_size_bytes;
-            dst_addr += transaction_size_bytes;
         }
         noc_async_write_barrier();
     }
