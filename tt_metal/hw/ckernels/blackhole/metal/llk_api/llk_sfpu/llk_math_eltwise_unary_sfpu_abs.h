a1e6a56146abd4ea07481b325637e502ec42c86b
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2025-06-28 22:21:33 +0530

    feat: add abs unary kernel implementation

---
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_abs.h
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "llk_math_eltwise_unary_sfpu_init.h"
+#include "llk_math_eltwise_unary_sfpu_params.h"
+#include "ckernel_sfpu_abs.h"
+#include "llk_math_eltwise_unary_sfpu_macros.h"
+
+SFPU_UNARY_KERNEL(abs)
+SFPU_UNARY_INT32_KERNEL(abs)
