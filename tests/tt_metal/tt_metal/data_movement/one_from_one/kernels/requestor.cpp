1a558a3d1d830e0a249d20a2eef38fad87d0e53a
Ata Tuzuner <atuzuner@tenstorrent.com>
2025-06-28 00:01:32 -0400

    [DM]: Adding static plots of sweep tests (#23861)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/23506)
    
    ### Problem description
    We want to harbour static plots of our primitive test performance
    results to which we can point developers when needed. Currently, our
    script outputs all test plots into one PNG file. We should modify the
    script to output separate PNG files for each test that is run.
    
    ### What's changed
    Created a `perf_plots` directory in the `documentation` folder under
    which I placed the plots for `PacketSizes` tests for each arch.
    
    Increased max transaction size in each test to 64 KB. To accommodate
    this, tests were modified to allocate one (or two depending on loopback)
    transaction size of memory in their respective L1. Relevant PCC checks
    were modified to operate on only the last transaction.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/15935992937)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/15935995670)
    CI passes (if applicable)
    - [x] [metal - Run
    microbenchmarks](https://github.com/tenstorrent/tt-metal/actions/runs/15935997718)

--- tests/tt_metal/tt_metal/data_movement/one_from_one/kernels/requestor.cpp
+++ tests/tt_metal/tt_metal/data_movement/one_from_one/kernels/requestor.cpp
@@ -7,7 +7,7 @@
 
 // L1 to L1 request
 void kernel_main() {
-    uint32_t l1_local_addr = get_compile_time_arg_val(0);
+    constexpr uint32_t l1_local_addr = get_compile_time_arg_val(0);
     constexpr uint32_t num_of_transactions = get_compile_time_arg_val(1);
     constexpr uint32_t transaction_size_bytes = get_compile_time_arg_val(2);
     constexpr uint32_t test_id = get_compile_time_arg_val(3);
@@ -17,22 +17,13 @@ void kernel_main() {
 
     DeviceTimestampedData("Number of transactions", num_of_transactions);
     DeviceTimestampedData("Transaction size in bytes", transaction_size_bytes);
-    DeviceTimestampedData("Total bytes transferred", num_of_transactions * transaction_size_bytes);
     DeviceTimestampedData("Test id", test_id);
 
     {
         DeviceZoneScopedN("RISCV1");
-        uint64_t src_base_noc_addr = get_noc_addr(responder_x_coord, responder_y_coord, 0);
+        uint64_t src_noc_addr = get_noc_addr(responder_x_coord, responder_y_coord, l1_local_addr);
         for (uint32_t i = 0; i < num_of_transactions; i++) {
-            /* The 64-bit NOC addresses consists of a 32-bit local address and a NOC XY coordinate. The local address
-             * occupies the lower 32 bits and the NOC XY coordinate occupies the next 12 (unicast) to 24 (multicast)
-             * bits. In the get_noc_addr call, we set the local address to 0 to get the base address. Then, we OR it
-             * with the local address (src_addr) in each iteration to get the full NOC address. */
-            uint64_t src_noc_addr = src_base_noc_addr | l1_local_addr;
-
             noc_async_read(src_noc_addr, l1_local_addr, transaction_size_bytes);
-
-            l1_local_addr += transaction_size_bytes;
         }
         noc_async_read_barrier();
     }
