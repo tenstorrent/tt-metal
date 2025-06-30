3619ed9e7e91280aa83a9904dc0166049174d0ae
Evan Banerjee <ebanerjee@tenstorrent.com>
2025-06-30 20:29:06 +0000

    removing personal files

--- bbuild_rebuild_for_debug.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cmake --preset default -DBUILD_TT_TRAIN=FALSE
-#cmake --build default --preset dev --clean-first -- -k 0
-cmake --build default --preset dev --clean-first -- -k 0 > build.log 2>&1
