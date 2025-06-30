5da1f883cff5cd44307114d5611fa67e3dca8124
Vishal Chaudhary <ctr-vchaudhary@ext.tenstorrent.com>
2025-06-30 16:22:16 +0000

    feat: migrate exp related ops llk implementation

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_elu.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_elu.h
@@ -15,17 +15,24 @@ using namespace sfpi;
 namespace ckernel {
 namespace sfpu {
 
-template <bool APPROXIMATION_MODE>
+template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
 inline void calculate_elu(uint slope) {
+    const bool FAST_APPROX = false;          // Elu does not use fast approximation.
+    const bool SCALE_EN = false;             // Elu does not use scale.
+    const bool SKIP_POSITIVE_CHECK = false;  // Elu does not skip positive check.
     // SFPU microcode
     vFloat s = Converter::as_float(slope);
-
 #pragma GCC unroll 0
     for (int d = 0; d < 8; d++) {
         vFloat v = dst_reg[0];
 
         v_if(v < 0.0f) {
-            vFloat v_exp = calculate_exponential_body_improved<APPROXIMATION_MODE>(v);
+            vFloat v_exp = _calculate_exponential_body_improved_<
+                APPROXIMATION_MODE,
+                SCALE_EN,
+                ITERATIONS,
+                FAST_APPROX,
+                SKIP_POSITIVE_CHECK>(v);
             v = s * (v_exp - 1.0f);
         }
         v_endif;
@@ -38,15 +45,8 @@ inline void calculate_elu(uint slope) {
 
 template <bool APPROXIMATION_MODE>
 void elu_init() {
-    if constexpr (APPROXIMATION_MODE) {
-        vConstFloatPrgm0 = 1.442695f;  // ln2_recip
-        vConstFloatPrgm1 = s2vFloat16b(p_exp::C23_73);
-        vConstFloatPrgm2 = s2vFloat16b(p_exp::ADJ_EXP);
-    } else {
-        vConstFloatPrgm0 = 1.442695f;  // ln2_recip
-        vConstFloatPrgm1 = 2.0f;
-        vConstFloatPrgm2 = 0.863281f;
-    }
+    const bool FAST_APPROX = false;  // Elu does not use fast approximation.
+    exp_init<APPROXIMATION_MODE, FAST_APPROX>();
 }
 
 }  // namespace sfpu
--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_exp2.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_exp2.h
@@ -16,13 +16,21 @@ namespace sfpu {
 
 template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
 inline void calculate_exp2() {
+    const bool FAST_APPROX = false;          // Exp2 does not use fast approximation.
+    const bool SCALE_EN = false;             // Exp2 does not use scale.
+    const bool SKIP_POSITIVE_CHECK = false;  // Exp2 does not skip positive check.
     // SFPU microcode
     for (int d = 0; d < ITERATIONS; d++) {
         vFloat v = dst_reg[0];
         // log(2) = 0.6931471805;
         v = v * 0.6931471805f;
         // exp = e^(v)
-        vFloat exp = calculate_exponential_body_improved<APPROXIMATION_MODE>(v);
+        vFloat exp = _calculate_exponential_body_improved_<
+            APPROXIMATION_MODE,
+            SCALE_EN,
+            ITERATIONS,
+            FAST_APPROX,
+            SKIP_POSITIVE_CHECK>(v);
         dst_reg[0] = exp;
         dst_reg++;
     }
@@ -30,15 +38,8 @@ inline void calculate_exp2() {
 
 template <bool APPROXIMATION_MODE>
 inline void exp2_init() {
-    if constexpr (APPROXIMATION_MODE) {
-        vConstFloatPrgm0 = 1.442695f;  // ln2_recip
-        vConstFloatPrgm1 = s2vFloat16b(p_exp::C23_73);
-        vConstFloatPrgm2 = s2vFloat16b(p_exp::ADJ_EXP);
-    } else {
-        vConstFloatPrgm0 = 1.442695f;  // ln2_recip
-        vConstFloatPrgm1 = 2.0f;
-        vConstFloatPrgm2 = 0.863281f;
-    }
+    const bool FAST_APPROX = false;  // Exp2 does not use fast approximation.
+    exp_init<APPROXIMATION_MODE, FAST_APPROX>();
 }
 
 }  // namespace sfpu
