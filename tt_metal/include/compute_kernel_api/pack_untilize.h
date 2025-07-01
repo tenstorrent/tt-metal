4313b749e7dae5c16392cd42e081871359caace7
Pavle Milenkovic <pmilenkovic@tenstorrent.com>
2025-07-01 10:43:02 +0200

    Fix block_ct_dim != full_ct_dim issue on BH (#24061)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/11573
    ### Problem description
    Note: This PR was originally reverted due to BH nightly tests failing.
    This lead to the discovery of a small issue on the op level which [got
    fixed](https://github.com/tenstorrent/tt-metal/pull/23834).
    
    Untilize operation can be performed either using the slow algorithm
    (unpack_untilize) or by using the fast algorithm (pack_untilize).
    However, pack_untilize had the limitation of input tensor fitting only
    in one Destination register bank (8 tiles width). The goal of this fix
    is to enable the pack_untilize operation to work for any input size, by
    fixing the block_ct_dim != full_ct_dim code path, where block_ct_dim is
    the number of tiles in the single Destination register bank, and
    full_ct_dim is the width of the whole input tensor expressed in tiles.
    
    This PR aims to solve the untilize portion of the issue
    https://github.com/tenstorrent/tt-metal/issues/16860 as well.
    
    ### What's changed
    
    - Adapted testing kernel to set block_ct_dim based on the size of the
    input
    - Modified pack_untilize test to use larger dimensions
    - Introduced [LLK
    changes](https://github.com/tenstorrent/tt-llk/pull/280)
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    [Passing](https://github.com/tenstorrent/tt-metal/actions/runs/15967668011)
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    [Passing](https://github.com/tenstorrent/tt-metal/actions/runs/15967672128)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable) [Same as
    main](https://github.com/tenstorrent/tt-metal/actions/runs/15967676803)
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    [Passing](https://github.com/tenstorrent/tt-metal/actions/runs/15967704218)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes

--- tt_metal/include/compute_kernel_api/pack_untilize.h
+++ tt_metal/include/compute_kernel_api/pack_untilize.h
@@ -33,11 +33,23 @@ ALWI void pack_untilize_init(uint32_t icb, uint32_t ocb) {
         false, false, icb)));  // init must be after configure
 }
 
+// clang-format off
 /**
  * Perform the untilize operation on a block of tiles. This simply loops over the provided block size.
+ * Performs the untilize operation on a block of tiles. Loops over the provided block size.
+ *
+ * | Param Type | Name          | Description                                 | Type      | Valid Range     | Required |
+ * |------------|---------------|---------------------------------------------|-----------|-----------------|----------|
+ * | Template   | block_ct_dim  | Width of a single block in tiles            | uint32_t  | 1 to 8          | False    |
+ * | Template   | full_ct_dim   | Width of a full input in tiles              | uint32_t  | >= block_ct_dim | False    |
+ * | Function   | icb           | Input circular buffer identifier            | uint32_t  | 0 to 31         | True     |
+ * | Function   | block_rt_dim  | Height of a single block in tiles           | uint32_t  | >= 1            | True     |
+ * | Function   | ocb           | Output circular buffer identifier           | uint32_t  | 0 to 31         | True     |
+ * | Function   | block_c_index | Index of the currently processed block      | uint32_t  | >= 0            | False    |
  */
+// clang-format on
 template <uint32_t block_ct_dim = 8, uint32_t full_ct_dim = block_ct_dim>
-ALWI void pack_untilize_block(uint32_t icb, uint32_t block_rt_dim, uint32_t ocb) {
+ALWI void pack_untilize_block(uint32_t icb, uint32_t block_rt_dim, uint32_t ocb, uint32_t block_c_index = 0) {
     for (uint32_t r = 0; r < block_rt_dim; ++r) {
         MATH((llk_math_wait_for_dest_available()));
         for (uint32_t c = 0; c < block_ct_dim; ++c) {
@@ -45,10 +57,11 @@ ALWI void pack_untilize_block(uint32_t icb, uint32_t block_rt_dim, uint32_t ocb)
                 (llk_unpack_A<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, UnpackToDestEn>(icb, c)));
             MATH((llk_math_eltwise_unary_datacopy<A2D, DST_ACCUM_MODE, BroadcastType::NONE, UnpackToDestEn>(c)));
         }
+
         MATH((llk_math_dest_section_done<DST_ACCUM_MODE>()));
 
         PACK((llk_packer_wait_for_math_done()));
-        PACK((llk_pack_untilize<block_ct_dim, full_ct_dim>(1 /*num_blocks*/, ocb)));
+        PACK((llk_pack_untilize<block_ct_dim, full_ct_dim>(1 /*num_blocks*/, ocb, FACE_R_DIM, 4, block_c_index)));
         PACK((llk_pack_dest_section_done<DST_ACCUM_MODE>()));
     }
 }
