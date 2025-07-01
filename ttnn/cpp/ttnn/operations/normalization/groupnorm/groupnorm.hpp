7709423246c100105adc93512af32b390a2a5cf2
Ryan Miller <rmiller@tenstorrent.com>
2025-07-01 18:50:24 -0400

    Enable compute kernel config as parameter to group_norm (#24101)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/23534
    
    ### Problem description
    group_norm was not taking a compute kernel config as an argument,
    preventing users from configuring the compute kernel
    
    ### What's changed
    Plumbed through and exposed the DeviceComputeKernelConfig for the
    groupnorm op as is done for other normalization ops e.g. layernorm.
    Preserved the default values that were hardcoded before
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/15978881829
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/16000085432
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/normalization/groupnorm/groupnorm.hpp
+++ ttnn/cpp/ttnn/operations/normalization/groupnorm/groupnorm.hpp
@@ -7,6 +7,8 @@
 #include "ttnn/decorators.hpp"
 #include "device/groupnorm_types.hpp"
 
+#include "ttnn/operations/core/compute_kernel/compute_kernel_config.hpp"
+
 namespace ttnn {
 namespace operations {
 namespace normalization {
@@ -24,7 +26,8 @@ struct ExecuteGroupNorm {
         std::optional<CoreGrid> core_grid = std::nullopt,
         std::optional<bool> inplace = std::nullopt,
         std::optional<ttnn::Layout> output_layout = std::nullopt,
-        std::optional<int> num_out_blocks = std::nullopt);
+        std::optional<int> num_out_blocks = std::nullopt,
+        std::optional<DeviceComputeKernelConfig> compute_kernel_config = std::nullopt);
 };
 
 }  // namespace normalization
