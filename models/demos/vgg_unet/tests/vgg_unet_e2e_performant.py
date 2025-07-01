4ee8e1a242054f763e8e26c259d486c698a0cb3e
Harini Mohan <164483522+HariniMohan0102@users.noreply.github.com>
2025-07-01 23:47:17 +0530

    Segmentation evaluation for Vanilla Unet, VGG Unet #23641 (#23642)
    ### Ticket
    [#23641](https://github.com/tenstorrent/tt-metal/issues/23641)
    
    ### Problem description
    Segmentation evaluation script for Vanilla_Unet, VGG_Unet
    
    ### What's changed
    - Added the evaluation script for Vanilla_UNet and VGG_Unet
    segmentation.
    - Includes VGG_Unet Demo fix changes
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml):
    [CI
    Failing]([harini/segmentation_eval_script](https://github.com/tenstorrent/tt-metal/tree/harini/segmentation_eval_script))
    [now
    passing](https://github.com/tenstorrent/tt-metal/actions/runs/16003733729)
    - [x] [Integration Tests
    ](https://github.com/tenstorrent/tt-metal/actions/workflows/fast-dispatch-full-regressions-and-models.yaml):
    [VGG Unet Passed
    
    ](https://github.com/tenstorrent/tt-metal/actions/runs/15999277107/attempts/2)
    - [x] (For models and ops writers)[Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml):
    [VGG UNet
    Passed](https://github.com/tenstorrent/tt-metal/actions/runs/15999489170)
    
    Co-authored-by: Dalar Vartanians <132954887+dvartaniansTT@users.noreply.github.com>

--- models/demos/vgg_unet/tests/vgg_unet_e2e_performant.py
+++ models/demos/vgg_unet/tests/vgg_unet_e2e_performant.py
@@ -21,11 +21,9 @@ class VggUnetTrace2CQ:
         self,
         device,
         model_location_generator,
+        use_pretrained_weight=False,
     ):
-        self.test_infra = create_test_infra(
-            device,
-            model_location_generator,
-        )
+        self.test_infra = create_test_infra(device, model_location_generator, use_pretrained_weight)
         self.device = device
         self.tt_inputs_host, sharded_mem_config_DRAM, self.input_mem_config = self.test_infra.setup_dram_sharded_input(
             device
