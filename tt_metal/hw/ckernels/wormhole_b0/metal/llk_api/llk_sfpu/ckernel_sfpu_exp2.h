d4d9430cc50939e7820b8951f7590b5fd63cfdf5
Vishal Chaudhary <ctr-vchaudhary@ext.tenstorrent.com>
2025-07-02 10:51:16 +0000

    feat: updated llk apis for exp related ops

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_exp2.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_exp2.h
@@ -5,9 +5,7 @@
 #pragma once
 
 #include "ckernel.h"
-#include "ckernel_defs.h"
-#include "noc_nonblocking_api.h"
-#include "ckernel_sfpu_exp.h"
+#include "sfpu/ckernel_sfpu_exp2.h"
 
 using namespace sfpi;
 
@@ -16,30 +14,12 @@ namespace sfpu {
 
 template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
 inline void calculate_exp2() {
-    const bool FAST_APPROX = false;          // Exp2 does not use fast approximation.
-    const bool SCALE_EN = false;             // Exp2 does not use scale.
-    const bool SKIP_POSITIVE_CHECK = false;  // Exp2 does not skip positive check.
-    // SFPU microcode
-    for (int d = 0; d < ITERATIONS; d++) {
-        vFloat v = dst_reg[0];
-        // log(2) = 0.6931471805;
-        v = v * 0.6931471805f;
-        // exp = e^(v)
-        vFloat exp = _calculate_exponential_body_improved_<
-            APPROXIMATION_MODE,
-            SCALE_EN,
-            ITERATIONS,
-            FAST_APPROX,
-            SKIP_POSITIVE_CHECK>(v);
-        dst_reg[0] = exp;
-        dst_reg++;
-    }
+    _calculate_exp2_<APPROXIMATION_MODE, ITERATIONS>();
 }
 
 template <bool APPROXIMATION_MODE>
 inline void exp2_init() {
-    const bool FAST_APPROX = false;  // Exp2 does not use fast approximation.
-    exp_init<APPROXIMATION_MODE, FAST_APPROX>();
+    _init_exp2_<APPROXIMATION_MODE>();
 }
 
 }  // namespace sfpu
