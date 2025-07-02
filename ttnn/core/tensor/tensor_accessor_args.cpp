bc0725fc5bb3dc0c4e03157ca8b6154197ed8d54
Stanislav Minakov <sminakov@tenstorrent.com>
2025-07-02 20:55:28 +0000

    Extend 2D sharding and add ND sharding support for reduce op, TensorAccessor API improvements (#24414)
    ### Ticket
    
    ### Problem description
    We're starting to add support for ND sharding and extending sharding
    support across OPs using newly introduced TensorAccessor
    After this PR reduce op supports every sharding for input and output
    
    ### What's changed
    This PR is based on https://github.com/tenstorrent/tt-metal/pull/23802
    cc @bbradelTT
    Updated TensorAccessor APIs to be more compatible with other AddrGens
    Updated interleaved reduce kernels to use TensorAccessor
    Added `writer_unary_universal_start_id`, based on the interleaved
    version. Interleaved version left as-is, because it's being used by
    other OPs.
    Updated host code to provide the needed arguments for TensorAccessor and
    properly fallback on the universal kernels than more specific ones
    aren't available
    Added all TensorAccessor files to CMake to properly distribute them
    Added tests
    
    ### Checklist
    - [x] [All post commit CI
    passes](https://github.com/tenstorrent/tt-metal/actions/runs/16015237244)
    - [x] Manually verified that kernel assembly is effectively the same for
    interleaved cases before and after
    - [x] New/Existing tests provide coverage for changes

--- ttnn/core/tensor/tensor_accessor_args.cpp
+++ ttnn/core/tensor/tensor_accessor_args.cpp
@@ -11,8 +11,8 @@ namespace tt::tt_metal {
 
 namespace {
 namespace CMAKE_UNIQUE_NAMESPACE {
-std::pair<std::vector<uint32_t>, std::vector<uint32_t>> prepare_sharded_args(
-    const Buffer& buffer, tensor_accessor::ArgsConfig args_config) {
+void append_sharded_args(
+    const Buffer& buffer, tensor_accessor::ArgsConfig args_config, std::vector<uint32_t>& args, bool is_runtime) {
     TT_FATAL(buffer.buffer_distribution_spec(), "Buffer must have a buffer distribution spec");
 
     const auto& buffer_distribution_spec = buffer.buffer_distribution_spec().value();
@@ -20,11 +20,11 @@ std::pair<std::vector<uint32_t>, std::vector<uint32_t>> prepare_sharded_args(
     const auto& shard_shape = buffer_distribution_spec.get_shard_shape_in_pages();
     const auto& bank_coords = buffer_distribution_spec.get_cores();
 
-    auto rank_rt = args_config.test(tensor_accessor::ArgConfig::RuntimeRank);
-    auto num_banks_rt = args_config.test(tensor_accessor::ArgConfig::RuntimeNumBanks);
-    auto tensor_shape_rt = args_config.test(tensor_accessor::ArgConfig::RuntimeTensorShape);
-    auto shard_shape_rt = args_config.test(tensor_accessor::ArgConfig::RuntimeShardShape);
-    auto bank_coords_rt = args_config.test(tensor_accessor::ArgConfig::RuntimeBankCoords);
+    auto add_rank = args_config.test(tensor_accessor::ArgConfig::RuntimeRank) == is_runtime;
+    auto add_num_banks = args_config.test(tensor_accessor::ArgConfig::RuntimeNumBanks) == is_runtime;
+    auto add_tensor_shape = args_config.test(tensor_accessor::ArgConfig::RuntimeTensorShape) == is_runtime;
+    auto add_shard_shape = args_config.test(tensor_accessor::ArgConfig::RuntimeShardShape) == is_runtime;
+    auto add_bank_coords = args_config.test(tensor_accessor::ArgConfig::RuntimeBankCoords) == is_runtime;
 
     size_t rank = tensor_shape.size();
     size_t n_banks = bank_coords.size();
@@ -32,64 +32,113 @@ std::pair<std::vector<uint32_t>, std::vector<uint32_t>> prepare_sharded_args(
         rank <= tt::tt_metal::MAX_NUM_DIMENSIONS,
         "Rank must be less than or equal to {} for rank",
         tt::tt_metal::MAX_NUM_DIMENSIONS);
-    TT_FATAL(
-        !rank_rt || (tensor_shape_rt && shard_shape_rt),
-        "If rank is runtime, tensor_shape and shard_shape must also be runtime");
-    TT_FATAL(!num_banks_rt || bank_coords_rt, "If num_banks is runtime, bank_coords must also be runtime");
 
-    size_t n_compile_time_args = 1 + !rank_rt + !num_banks_rt + rank * !tensor_shape_rt + rank * !shard_shape_rt +
-                                 n_banks * !bank_coords_rt;  // +1 for the crta config
-    size_t n_runtime_args =
-        rank_rt + num_banks_rt + rank * tensor_shape_rt + rank * shard_shape_rt + n_banks * bank_coords_rt;
-    std::vector<uint32_t> compile_time_args;
-    std::vector<uint32_t> runtime_args;
-    compile_time_args.reserve(n_compile_time_args);
-    runtime_args.reserve(n_runtime_args);
-    auto& rank_args = rank_rt ? runtime_args : compile_time_args;
-    auto& num_banks_args = num_banks_rt ? runtime_args : compile_time_args;
-    auto& tensor_shape_args = tensor_shape_rt ? runtime_args : compile_time_args;
-    auto& shard_shape_args = shard_shape_rt ? runtime_args : compile_time_args;
-    auto& bank_coords_args = bank_coords_rt ? runtime_args : compile_time_args;
+    size_t n_args =
+        add_rank + add_num_banks + rank * add_tensor_shape + rank * add_shard_shape + n_banks * add_bank_coords;
+    if (!is_runtime) {
+        n_args += 1;  // +1 for the args_config config
+    }
+    args.reserve(args.size() + n_args);
 
-    compile_time_args.push_back(args_config.raw());
-    rank_args.push_back(rank);
-    num_banks_args.push_back(n_banks);
-    tensor_shape_args.insert(tensor_shape_args.end(), tensor_shape.cbegin(), tensor_shape.cend());
-    shard_shape_args.insert(shard_shape_args.end(), shard_shape.cbegin(), shard_shape.cend());
-
-    auto device = buffer.device();
-    auto bank_type = buffer.core_type();
-    for (size_t i = 0; i < n_banks; i += 2) {
-        const auto virtual_coord1 = device->virtual_core_from_logical_core(bank_coords[i], bank_type);
-
-        if (i + 1 < n_banks) {
-            // Pack two coordinates into one uint32_t if we have a pair
-            const auto virtual_coord2 = device->virtual_core_from_logical_core(bank_coords[i + 1], bank_type);
-            bank_coords_args.push_back(
-                ((virtual_coord2.x & 0xFF) << 24) | ((virtual_coord2.y & 0xFF) << 16) |
-                ((virtual_coord1.x & 0xFF) << 8) | (virtual_coord1.y & 0xFF));
-        } else {
-            // Handle odd number of coordinates by setting the second coordinate to zero
-            bank_coords_args.push_back(((virtual_coord1.x & 0xFF) << 8) | (virtual_coord1.y & 0xFF));
-        }
+    if (!is_runtime) {
+        args.push_back(args_config.raw());
     }
 
-    return {std::move(compile_time_args), std::move(runtime_args)};
+    if (add_rank) {
+        args.push_back(rank);
+    }
+    if (add_num_banks) {
+        args.push_back(n_banks);
+    }
+    if (add_tensor_shape) {
+        args.insert(args.end(), tensor_shape.cbegin(), tensor_shape.cend());
+    }
+    if (add_shard_shape) {
+        args.insert(args.end(), shard_shape.cbegin(), shard_shape.cend());
+    }
+
+    if (add_bank_coords) {
+        auto device = buffer.device();
+        auto bank_type = buffer.core_type();
+        for (size_t i = 0; i < n_banks; i += 2) {
+            const auto virtual_coord1 = device->virtual_core_from_logical_core(bank_coords[i], bank_type);
+
+            if (i + 1 < n_banks) {
+                // Pack two coordinates into one uint32_t if we have a pair
+                const auto virtual_coord2 = device->virtual_core_from_logical_core(bank_coords[i + 1], bank_type);
+                args.push_back(
+                    ((virtual_coord2.x & 0xFF) << 24) | ((virtual_coord2.y & 0xFF) << 16) |
+                    ((virtual_coord1.x & 0xFF) << 8) | (virtual_coord1.y & 0xFF));
+            } else {
+                // Handle odd number of coordinates by setting the second coordinate to zero
+                args.push_back(((virtual_coord1.x & 0xFF) << 8) | (virtual_coord1.y & 0xFF));
+            }
+        }
+    }
 }
 }  // namespace CMAKE_UNIQUE_NAMESPACE
 }  // namespace
 
-TensorAccessorArgs::TensorAccessorArgs(const Buffer& buffer, tensor_accessor::ArgsConfig args_config) {
+TensorAccessorArgs::TensorAccessorArgs(const Buffer& buffer, tensor_accessor::ArgsConfig args_config) :
+    buffer_(&buffer), args_config_(args_config) {
     if (is_sharded(buffer.buffer_layout())) {
-        args_config.set(tensor_accessor::ArgConfig::Sharded);
-        args_config.set(tensor_accessor::ArgConfig::IsDram, buffer.is_dram());
-        std::tie(compile_time_args, runtime_args) = CMAKE_UNIQUE_NAMESPACE::prepare_sharded_args(buffer, args_config);
-        return;
+        args_config_.set(tensor_accessor::ArgConfig::Sharded);
+    } else {
+        args_config_ = tensor_accessor::ArgConfig::None;
     }
+    args_config_.set(tensor_accessor::ArgConfig::IsDram, buffer.is_dram());
 
-    args_config = tensor_accessor::ArgConfig::None;
-    args_config.set(tensor_accessor::ArgConfig::IsDram, buffer.is_dram());
-    compile_time_args = {args_config.raw()};
+    if (args_config_.test(tensor_accessor::ArgConfig::RuntimeRank)) {
+        TT_FATAL(
+            args_config_.test(tensor_accessor::ArgConfig::RuntimeTensorShape) &&
+                args_config_.test(tensor_accessor::ArgConfig::RuntimeShardShape),
+            "If rank is runtime, tensor_shape and shard_shape must also be runtime");
+    }
+    if (args_config_.test(tensor_accessor::ArgConfig::RuntimeNumBanks)) {
+        TT_FATAL(
+            args_config_.test(tensor_accessor::ArgConfig::RuntimeBankCoords),
+            "If num_banks is runtime, bank_coords must also be runtime");
+    }
+}
+
+void TensorAccessorArgs::append_args(
+    std::vector<uint32_t>& compile_time_args, std::vector<uint32_t>& common_runtime_args) const {
+    if (args_config_.test(tensor_accessor::ArgConfig::Sharded)) {
+        CMAKE_UNIQUE_NAMESPACE::append_sharded_args(*buffer_, args_config_, compile_time_args, /* is_runtime */ false);
+        CMAKE_UNIQUE_NAMESPACE::append_sharded_args(*buffer_, args_config_, common_runtime_args, /* is_runtime */ true);
+    } else {
+        compile_time_args.push_back(args_config_.raw());
+    }
+}
+
+void TensorAccessorArgs::append_args(std::vector<uint32_t>& compile_time_args) const {
+    TT_FATAL(
+        (args_config_ & tensor_accessor::ArgConfig::Runtime).raw() == 0,
+        "Common runtime arguments are required for ArgsConfig {}",
+        args_config_.raw());
+    if (args_config_.test(tensor_accessor::ArgConfig::Sharded)) {
+        CMAKE_UNIQUE_NAMESPACE::append_sharded_args(*buffer_, args_config_, compile_time_args, /* is_runtime */ false);
+    } else {
+        compile_time_args.push_back(args_config_.raw());
+    }
+}
+
+std::vector<uint32_t> TensorAccessorArgs::get_compile_time_args() const {
+    std::vector<uint32_t> compile_time_args;
+    if (args_config_.test(tensor_accessor::ArgConfig::Sharded)) {
+        CMAKE_UNIQUE_NAMESPACE::append_sharded_args(*buffer_, args_config_, compile_time_args, /* is_runtime */ false);
+    } else {
+        compile_time_args.push_back(args_config_.raw());
+    }
+    return compile_time_args;
+}
+
+std::vector<uint32_t> TensorAccessorArgs::get_common_runtime_args() const {
+    std::vector<uint32_t> common_runtime_args;
+    if (args_config_.test(tensor_accessor::ArgConfig::Sharded)) {
+        CMAKE_UNIQUE_NAMESPACE::append_sharded_args(*buffer_, args_config_, common_runtime_args, /* is_runtime */ true);
+    }
+    return common_runtime_args;
 }
 
 }  // namespace tt::tt_metal
