679dfe956ced11fda8704e8b25e133de501b954f
Evan Smal <esmal@tenstorrent.com>
2025-07-02 16:17:02 +0000

    Remove redundant performance tests in YoloV9c

--- models/demos/yolov9c/runner/performant_runner.py
+++ models/demos/yolov9c/runner/performant_runner.py
@@ -95,8 +95,6 @@ class YOLOv9PerformantRunner:
             self.input_tensor = ttnn.reshard(self.tt_image_res, self.input_mem_config, self.input_tensor)
         self.op_event = ttnn.record_event(self.device, 0)
         ttnn.execute_trace(self.device, self.tid, cq_id=0, blocking=False)
-        ttnn.synchronize_device(self.device)
-
         ttnn_output_tensor = self.runner_infra.output_tensor
         return ttnn_output_tensor
 
