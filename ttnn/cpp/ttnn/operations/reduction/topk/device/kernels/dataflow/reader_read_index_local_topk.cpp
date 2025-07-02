d04cc94732841eff77faeb2b913f82896f68ae19
Johanna Rock <jrock@tenstorrent.com>
2025-07-02 09:20:02 +0000

    Dispatch NOC reads async in topk reader

--- ttnn/cpp/ttnn/operations/reduction/topk/device/kernels/dataflow/reader_read_index_local_topk.cpp
+++ ttnn/cpp/ttnn/operations/reduction/topk/device/kernels/dataflow/reader_read_index_local_topk.cpp
@@ -28,30 +28,27 @@ void kernel_main() {
     constexpr uint32_t tile_bytes_indices = get_tile_size(cb_id_in1);
     constexpr DataFormat data_format_indices = get_dataformat(cb_id_in1);
 
-    const InterleavedAddrGenFast<src_is_dram> s = {
+    const InterleavedAddrGenFast<src_is_dram> s_input = {
         .bank_base_address = src_addr, .page_size = tile_bytes, .data_format = data_format};
 
     const InterleavedAddrGenFast<src_indices_is_dram> s_indices = {
         .bank_base_address = src_indices_addr, .page_size = tile_bytes_indices, .data_format = data_format_indices};
 
-    // Stream in input tensor and generate the relevant index tensor tiles
+    // Stream in input tensor and index tensor
     // The input buffer has four tiles as we double-buffer for the two tiles needed for topk_local_sort to start
     // We could load in an entire row of tiles at a time but that would require substantially more memory (we would be
     // double buffering four Wt_local sized CBs)
     for (uint32_t i = start_ht; i < Ht; ++i) {
         for (uint32_t j = start_wt; j < start_wt + Wt_local; ++j) {
-            // Read input tensor
+            // Read input tensor and indices tensor
             cb_reserve_back(cb_id_in0, onetile);
-            uint32_t l1_write_addr = get_write_ptr(cb_id_in0);
-            noc_async_read_tile(i * Wt + j, s, l1_write_addr);
-            noc_async_read_barrier();
-            cb_push_back(cb_id_in0, onetile);
-
-            // Read indices tensor for same tile
             cb_reserve_back(cb_id_in1, onetile);
+            uint32_t l1_write_addr_input = get_write_ptr(cb_id_in0);
             uint32_t l1_write_addr_indices = get_write_ptr(cb_id_in1);
+            noc_async_read_tile(i * Wt + j, s_input, l1_write_addr_input);
             noc_async_read_tile(i * Wt + j, s_indices, l1_write_addr_indices);
             noc_async_read_barrier();
+            cb_push_back(cb_id_in0, onetile);
             cb_push_back(cb_id_in1, onetile);
         }
     }
