f864a7993a5207e99d771f56aff0331741d330a0
Diego Gomez <dgomez@tenstorrent.com>
2025-07-03 14:06:48 -0400

    added cache to mamba test

--- models/demos/wormhole/mamba/demo/demo.py
+++ models/demos/wormhole/mamba/demo/demo.py
@@ -415,9 +415,6 @@ def run_mamba_demo(
     ),
 )
 def test_demo(user_input, device, get_tt_cache_path, model_version, max_gen_len):
-    # https://github.com/tenstorrent/tt-metal/issues/23282
-    device.disable_and_clear_program_cache()
-
     return run_mamba_demo(
         prompts=user_input,
         device=device,
