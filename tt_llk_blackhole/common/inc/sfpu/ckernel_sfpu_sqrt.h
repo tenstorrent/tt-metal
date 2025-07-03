9003e216005358a8a74001576163a7e4f4bad22c
Mouliraj Elamurugan <ctr-mcw-melamurugan@ext.tenstorrent.com>
2025-07-03 10:38:01 +0530

    Add atanh implementation (#380)
    ### Ticket
    Link to Github Issue
    https://github.com/tenstorrent/tt-metal/issues/23171
    
    Problem description
    
    atanh has been implemented as a composite operation.
    
    What's changed
    
    Added llk support for atanh
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    
    Co-authored-by: mouliraj-mcw <ctr-mcw-mouliraj@ext.tenstorrent.com>

--- tt_llk_blackhole/common/inc/sfpu/ckernel_sfpu_sqrt.h
+++ tt_llk_blackhole/common/inc/sfpu/ckernel_sfpu_sqrt.h
@@ -12,6 +12,50 @@ namespace ckernel
 namespace sfpu
 {
 
+template <bool APPROXIMATION_MODE, int RECIPROCAL_ITERATIONS>
+sfpi_inline sfpi::vFloat _calculate_sqrt_body_(sfpi::vFloat val)
+{
+    sfpi::vFloat result;
+    if constexpr (APPROXIMATION_MODE)
+    {
+        sfpi::vUInt magic = sfpi::vConstIntPrgm0;
+
+        // sqrt initial approximation
+        //  adjust bias
+        sfpi::vUInt val_s = magic + sfpi::reinterpret<sfpi::vUInt>(val);
+
+        // approximation of square root
+        val_s >>= 1;
+        result = sfpi::reinterpret<sfpi::vFloat>(val_s);
+    }
+    else
+    {
+        // Recip root method
+        //// Init approx
+        // u.i = SQRT_MAGIC_F - (u.i >> 1);
+        v_if (val != 0.0f)
+        {
+            sfpi::vUInt magic   = sfpi::vConstIntPrgm0;
+            sfpi::vFloat approx = sfpi::reinterpret<sfpi::vFloat>(magic - (sfpi::reinterpret<sfpi::vUInt>(val) >> 1));
+
+            // Reciproot iterations
+            for (int r = 0; r < RECIPROCAL_ITERATIONS; r++)
+            {
+                // x*r*(1.5f - xhalf*r*r);
+                approx = ((approx * approx) * (val * -0.5f) + 1.5f) * approx;
+            }
+
+            result = approx * val;
+        }
+        v_else
+        {
+            result = val;
+        }
+        v_endif;
+    }
+    return result;
+}
+
 template <bool APPROXIMATION_MODE, int ITERATIONS, int RECIPROCAL_ITERATIONS>
 inline void _calculate_sqrt_(const int iterations)
 {
@@ -19,41 +63,7 @@ inline void _calculate_sqrt_(const int iterations)
     for (int d = 0; d < iterations; d++)
     {
         sfpi::vFloat val = sfpi::dst_reg[0];
-
-        if constexpr (APPROXIMATION_MODE)
-        {
-            sfpi::vUInt magic = sfpi::vConstIntPrgm0;
-
-            // sqrt initial approximation
-            //  adjust bias
-            sfpi::vUInt val_s = magic + sfpi::reinterpret<sfpi::vUInt>(val);
-
-            // approximation of square root
-            val_s >>= 1;
-            sfpi::dst_reg[0] = sfpi::reinterpret<sfpi::vFloat>(val_s);
-        }
-        else
-        {
-            // Recip root method
-            //// Init approx
-            // u.i = SQRT_MAGIC_F - (u.i >> 1);
-            v_if (val != 0.0f)
-            {
-                sfpi::vUInt magic   = sfpi::vConstIntPrgm0;
-                sfpi::vFloat approx = sfpi::reinterpret<sfpi::vFloat>(magic - (sfpi::reinterpret<sfpi::vUInt>(val) >> 1));
-
-                // Reciproot iterations
-                for (int r = 0; r < RECIPROCAL_ITERATIONS; r++)
-                {
-                    // x*r*(1.5f - xhalf*r*r);
-                    approx = ((approx * approx) * (val * -0.5f) + 1.5f) * approx;
-                }
-
-                sfpi::dst_reg[0] = approx * val;
-            }
-            v_endif;
-        }
-
+        sfpi::dst_reg[0] = _calculate_sqrt_body_<APPROXIMATION_MODE, RECIPROCAL_ITERATIONS>(val);
         sfpi::dst_reg++;
     }
 }
