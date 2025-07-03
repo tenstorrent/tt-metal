a3817324826b4c620b5e3706b658df0668eaa2ca
Radomir Djogo <159184120+rdjogoTT@users.noreply.github.com>
2025-07-03 12:20:09 -0400

    Rd/topk 32bit (#356)
    ### Ticket
    [<!-- Link to Github Issue
    -->](https://github.com/tenstorrent/tt-metal/issues/20294)
    
    ### Problem description
    TopK LLK did not support use of 32bit indices
    
    ### What's changed
    Pass down fp32 flag to kernel to decide whether to load indices as int32
    or uint16 depending on Dest acc mode.
    Also for BH, the fp32 cfg reg must be 0 for MOVD2B/B2D to work
    correctly, so an added cfg call was put before and after the
    transpose_wh llk call.
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [x] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16034058666)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16034060157)
    CI passes (if applicable)

--- tt_llk_blackhole/common/inc/sfpu/ckernel_sfpu_topk.h
+++ tt_llk_blackhole/common/inc/sfpu/ckernel_sfpu_topk.h
@@ -25,9 +25,11 @@ inline void set_dst_write_addr(uint32_t addr)
     TT_SETC16(DEST_TARGET_REG_CFG_MATH_Offset_ADDR32, dst_index);
 }
 
+template <bool is_fp32_dest_acc_en>
 inline void bitonic_topk_load8(uint offset, uint dist)
 {
     constexpr uint dst_indices_offset = 128; // 2 tile x 64 rows per tile
+    constexpr uint8_t instr_mod_index = is_fp32_dest_acc_en ? InstrModLoadStore::INT32 : InstrModLoadStore::LO16;
 
     uint face_offset = offset >> 4;
     uint ld_offset   = (offset & 0xF) + face_offset * 32;
@@ -37,13 +39,15 @@ inline void bitonic_topk_load8(uint offset, uint dist)
     TT_SFPLOAD(p_sfpu::LREG1, 0, ADDR_MOD_7, ld_offset + dist);
 
     // Load 16 consecutive indices
-    TT_SFPLOAD(p_sfpu::LREG4, 6, ADDR_MOD_7, dst_indices_offset + ld_offset); // How to load indices ? This is unpacked directly to dest!
-    TT_SFPLOAD(p_sfpu::LREG5, 6, ADDR_MOD_7, dst_indices_offset + ld_offset + dist);
+    TT_SFPLOAD(p_sfpu::LREG4, instr_mod_index, ADDR_MOD_7, dst_indices_offset + ld_offset);
+    TT_SFPLOAD(p_sfpu::LREG5, instr_mod_index, ADDR_MOD_7, dst_indices_offset + ld_offset + dist);
 }
 
+template <bool is_fp32_dest_acc_en>
 inline void bitonic_topk_store8(uint offset, uint dist)
 {
     constexpr uint dst_indices_offset = 128; // 2 tile x 64 rows per tile
+    constexpr uint8_t instr_mod_index = is_fp32_dest_acc_en ? InstrModLoadStore::INT32 : InstrModLoadStore::LO16;
 
     uint face_offset = offset >> 4;
     uint ld_offset   = (offset & 0xF) + face_offset * 32;
@@ -53,13 +57,15 @@ inline void bitonic_topk_store8(uint offset, uint dist)
     TT_SFPSTORE(p_sfpu::LREG1, 0, ADDR_MOD_7, ld_offset + dist);
 
     // Load 16 consecutive indices
-    TT_SFPSTORE(p_sfpu::LREG4, 6, ADDR_MOD_7, dst_indices_offset + ld_offset + 0); // How to load indices ? This is unpacked directly to dest!
-    TT_SFPSTORE(p_sfpu::LREG5, 6, ADDR_MOD_7, dst_indices_offset + ld_offset + dist);
+    TT_SFPSTORE(p_sfpu::LREG4, instr_mod_index, ADDR_MOD_7, dst_indices_offset + ld_offset + 0);
+    TT_SFPSTORE(p_sfpu::LREG5, instr_mod_index, ADDR_MOD_7, dst_indices_offset + ld_offset + dist);
 }
 
+template <bool is_fp32_dest_acc_en>
 inline void bitonic_topk_load16(uint dist0, uint dist1)
 {
     constexpr uint dst_indices_offset = 128; // 2 tile x 64 rows per tile
+    constexpr uint8_t instr_mod_index = is_fp32_dest_acc_en ? InstrModLoadStore::INT32 : InstrModLoadStore::LO16;
 
     // Load 16 consecutive numbers
     TTI_SFPLOAD(p_sfpu::LREG0, 0, ADDR_MOD_7, 0);
@@ -77,25 +83,26 @@ inline void bitonic_topk_load16(uint dist0, uint dist1)
     }
 
     // Load 16 consecutive indices
-    TTI_SFPLOAD(p_sfpu::LREG4, 6, ADDR_MOD_7, dst_indices_offset + 0); // How to load indices ? This is unpacked directly to dest!
+    TTI_SFPLOAD(p_sfpu::LREG4, instr_mod_index, ADDR_MOD_7, dst_indices_offset + 0);
     if ((dist0 == 4) && (dist1 == 8))
     {
-        TTI_SFPLOAD(p_sfpu::LREG5, 6, ADDR_MOD_7, dst_indices_offset + 4);
-        TTI_SFPLOAD(p_sfpu::LREG6, 6, ADDR_MOD_7, dst_indices_offset + 8);
-        TTI_SFPLOAD(p_sfpu::LREG7, 6, ADDR_MOD_7, dst_indices_offset + 12);
+        TTI_SFPLOAD(p_sfpu::LREG5, instr_mod_index, ADDR_MOD_7, dst_indices_offset + 4);
+        TTI_SFPLOAD(p_sfpu::LREG6, instr_mod_index, ADDR_MOD_7, dst_indices_offset + 8);
+        TTI_SFPLOAD(p_sfpu::LREG7, instr_mod_index, ADDR_MOD_7, dst_indices_offset + 12);
     }
     else
     {
-        TT_SFPLOAD(p_sfpu::LREG5, 6, ADDR_MOD_7, dst_indices_offset + 0 + dist0);
-        TT_SFPLOAD(p_sfpu::LREG6, 6, ADDR_MOD_7, dst_indices_offset + dist1);
-        TT_SFPLOAD(p_sfpu::LREG7, 6, ADDR_MOD_7, dst_indices_offset + dist1 + dist0);
+        TT_SFPLOAD(p_sfpu::LREG5, instr_mod_index, ADDR_MOD_7, dst_indices_offset + 0 + dist0);
+        TT_SFPLOAD(p_sfpu::LREG6, instr_mod_index, ADDR_MOD_7, dst_indices_offset + dist1);
+        TT_SFPLOAD(p_sfpu::LREG7, instr_mod_index, ADDR_MOD_7, dst_indices_offset + dist1 + dist0);
     }
 }
 
-template <bool alt_addr_mod = false>
+template <bool is_fp32_dest_acc_en, bool alt_addr_mod = false>
 inline void bitonic_topk_store16(uint dist0, uint dist1)
 {
     constexpr uint dst_indices_offset = 128; // 2 tile x 64 rows per tile
+    constexpr uint8_t instr_mod_index = is_fp32_dest_acc_en ? InstrModLoadStore::INT32 : InstrModLoadStore::LO16;
 
     // Load 16 consecutive numbers
     TTI_SFPSTORE(p_sfpu::LREG0, 0, ADDR_MOD_7, 0);
@@ -113,18 +120,18 @@ inline void bitonic_topk_store16(uint dist0, uint dist1)
     }
 
     // Load 16 consecutive indices
-    TTI_SFPSTORE(p_sfpu::LREG4, 6, ADDR_MOD_7, dst_indices_offset + 0); // How to load indices ? This is unpacked directly to dest!
+    TTI_SFPSTORE(p_sfpu::LREG4, instr_mod_index, ADDR_MOD_7, dst_indices_offset + 0);
     if ((dist0 == 4) && (dist1 == 8))
     {
-        TTI_SFPSTORE(p_sfpu::LREG5, 6, ADDR_MOD_7, dst_indices_offset + 4);
-        TTI_SFPSTORE(p_sfpu::LREG6, 6, ADDR_MOD_7, dst_indices_offset + 8);
-        TTI_SFPSTORE(p_sfpu::LREG7, 6, alt_addr_mod ? ADDR_MOD_6 : ADDR_MOD_7, dst_indices_offset + 12);
+        TTI_SFPSTORE(p_sfpu::LREG5, instr_mod_index, ADDR_MOD_7, dst_indices_offset + 4);
+        TTI_SFPSTORE(p_sfpu::LREG6, instr_mod_index, ADDR_MOD_7, dst_indices_offset + 8);
+        TTI_SFPSTORE(p_sfpu::LREG7, instr_mod_index, alt_addr_mod ? ADDR_MOD_6 : ADDR_MOD_7, dst_indices_offset + 12);
     }
     else
     {
-        TT_SFPSTORE(p_sfpu::LREG5, 6, ADDR_MOD_7, dst_indices_offset + 0 + dist0);
-        TT_SFPSTORE(p_sfpu::LREG6, 6, ADDR_MOD_7, dst_indices_offset + dist1);
-        TT_SFPSTORE(p_sfpu::LREG7, 6, alt_addr_mod ? ADDR_MOD_6 : ADDR_MOD_7, dst_indices_offset + dist1 + dist0);
+        TT_SFPSTORE(p_sfpu::LREG5, instr_mod_index, ADDR_MOD_7, dst_indices_offset + 0 + dist0);
+        TT_SFPSTORE(p_sfpu::LREG6, instr_mod_index, ADDR_MOD_7, dst_indices_offset + dist1);
+        TT_SFPSTORE(p_sfpu::LREG7, instr_mod_index, alt_addr_mod ? ADDR_MOD_6 : ADDR_MOD_7, dst_indices_offset + dist1 + dist0);
     }
 }
 
@@ -264,7 +271,7 @@ inline void bitonic_topk_inc_x4_dest(uint inc, bool cr)
     }
 }
 
-template <bool APPROXIMATION_MODE, int ITERATIONS>
+template <bool APPROXIMATION_MODE, bool is_fp32_dest_acc_en, int ITERATIONS>
 inline void _bitonic_topk_phases_steps(const int idir, const int i_end_phase, const int i_start_phase, const int i_end_step, const int i_start_step)
 {
     // If more than 1 phase is requested, do all the steps from all phases
@@ -294,7 +301,7 @@ inline void _bitonic_topk_phases_steps(const int idir, const int i_end_phase, co
                             // Groups of 16 datums being sorted at the same time
                             if (init_load)
                             {
-                                load_replay_buf<Exec>(0, 8, [] { bitonic_topk_load16(4, 8); });
+                                load_replay_buf<Exec>(0, 8, [] { bitonic_topk_load16<is_fp32_dest_acc_en>(4, 8); });
                                 init_load = false;
                             }
                             else
@@ -312,7 +319,7 @@ inline void _bitonic_topk_phases_steps(const int idir, const int i_end_phase, co
                             }
                             if (init_store)
                             {
-                                load_replay_buf<Exec>(8, 8, [] { bitonic_topk_store16<true>(4, 8); });
+                                load_replay_buf<Exec>(8, 8, [] { bitonic_topk_store16<is_fp32_dest_acc_en, true>(4, 8); });
                                 init_store = false;
                             }
                             else
@@ -383,9 +390,10 @@ inline void _bitonic_topk_phases_steps(const int idir, const int i_end_phase, co
                             {
                                 for (uint ii = 0; ii < inner_d; ii++)
                                 {
-                                    bitonic_topk_load16(4, 2 * dist); // load/store with offset of face 1 (in row major face layout)
+                                    bitonic_topk_load16<is_fp32_dest_acc_en>(4, 2 * dist); // load/store with offset of face 1 (in row major face layout)
                                     bitonic_topk_step_N(dir);
-                                    bitonic_topk_store16<false>(4, 2 * dist); // load/store with offset of face 1 (in row major face layout)
+                                    bitonic_topk_store16<is_fp32_dest_acc_en, false>(
+                                        4, 2 * dist); // load/store with offset of face 1 (in row major face layout)
                                     uint dst_inc = 8;
                                     dst_offset += dst_inc;
                                     bool dst_cr = false;
@@ -429,7 +437,7 @@ inline void _bitonic_topk_phases_steps(const int idir, const int i_end_phase, co
     topk_replay_init = -1;
 }
 
-template <bool APPROXIMATION_MODE, bool top_min, int ITERATIONS>
+template <bool APPROXIMATION_MODE, bool is_fp32_dest_acc_en, bool top_min, int ITERATIONS>
 inline void _bitonic_topk_merge(const int m_iter, const int k)
 {
     uint dst_addr_offset = 0;
@@ -453,9 +461,9 @@ inline void _bitonic_topk_merge(const int m_iter, const int k)
             {
                 for (uint ii = 0; ii < inner_d; ii++)
                 {
-                    bitonic_topk_load8(dst_offset, ld_dist);
+                    bitonic_topk_load8<is_fp32_dest_acc_en>(dst_offset, ld_dist);
                     TTI_SFPSWAP(0, top_min ? p_sfpu::LREG1 : p_sfpu::LREG0, top_min ? p_sfpu::LREG0 : p_sfpu::LREG1, p_sfpswap::ALL_ROWS_MAX);
-                    bitonic_topk_store8(dst_offset, ld_dist);
+                    bitonic_topk_store8<is_fp32_dest_acc_en>(dst_offset, ld_dist);
                     datums_compared += 8;
                     if (ii == (inner_d - 1))
                     {
@@ -476,7 +484,7 @@ inline void _bitonic_topk_merge(const int m_iter, const int k)
     }
 }
 
-template <bool APPROXIMATION_MODE, int ITERATIONS>
+template <bool APPROXIMATION_MODE, bool is_fp32_dest_acc_en, int ITERATIONS>
 inline void _bitonic_topk_rebuild(const bool idir, const int m_iter, const int k, const int logk, const int skip_second)
 {
     // init replay buffer for rebuild interation 'm_iter' if uninitialized
@@ -518,9 +526,9 @@ inline void _bitonic_topk_rebuild(const bool idir, const int m_iter, const int k
                                     22,
                                     [ld_offset]
                                     {
-                                        bitonic_topk_load8(0, ld_offset);
+                                        bitonic_topk_load8<is_fp32_dest_acc_en>(0, ld_offset);
                                         bitonic_topk_ph1_st2_to_1();
-                                        bitonic_topk_store8(0, ld_offset);
+                                        bitonic_topk_store8<is_fp32_dest_acc_en>(0, ld_offset);
                                         bitonic_topk_inc_x8_dest(64, false);
                                     });
                                 init_rebuild = false;
@@ -546,9 +554,9 @@ inline void _bitonic_topk_rebuild(const bool idir, const int m_iter, const int k
                                     26,
                                     [ld_offset, ld_dist]
                                     {
-                                        bitonic_topk_load16(ld_offset, ld_dist);
+                                        bitonic_topk_load16<is_fp32_dest_acc_en>(ld_offset, ld_dist);
                                         bitonic_topk_ph1_st2_to_1();
-                                        bitonic_topk_store16<true>(ld_offset, ld_dist);
+                                        bitonic_topk_store16<is_fp32_dest_acc_en, true>(ld_offset, ld_dist);
                                         TTI_INCRWC(0, 8, 0, 0);
                                         TTI_INCRWC(0, 8, 0, 0);
                                         TTI_INCRWC(0, 8, 0, 0);
@@ -575,9 +583,9 @@ inline void _bitonic_topk_rebuild(const bool idir, const int m_iter, const int k
                                 29,
                                 [ld_offset]
                                 {
-                                    bitonic_topk_load16(4, ld_offset);
+                                    bitonic_topk_load16<is_fp32_dest_acc_en>(4, ld_offset);
                                     bitonic_topk_ph2_st3_to_1();
-                                    bitonic_topk_store16<true>(4, ld_offset);
+                                    bitonic_topk_store16<is_fp32_dest_acc_en, true>(4, ld_offset);
                                     TTI_INCRWC(0, 8, 0, 0);
                                     TTI_INCRWC(0, 8, 0, 0);
                                     TTI_INCRWC(0, 8, 0, 0);
@@ -598,14 +606,14 @@ inline void _bitonic_topk_rebuild(const bool idir, const int m_iter, const int k
                         // Groups of 16 datums being sorted at the same time
                         if (init_rebuild)
                         {
-                            load_replay_buf<Exec>(0, 8, [] { bitonic_topk_load16(4, 8); });
+                            load_replay_buf<Exec>(0, 8, [] { bitonic_topk_load16<is_fp32_dest_acc_en>(4, 8); });
                             bitonic_topk_ph3_st4_to_1(dir, init_rebuild, 8);
                             load_replay_buf<Exec>(
                                 13,
                                 12,
                                 []
                                 {
-                                    bitonic_topk_store16<true>(4, 8);
+                                    bitonic_topk_store16<is_fp32_dest_acc_en, true>(4, 8);
                                     TTI_INCRWC(0, 8, 0, 0);
                                     TTI_INCRWC(0, 8, 0, 0);
                                     TTI_INCRWC(0, 8, 0, 0);
@@ -641,9 +649,9 @@ inline void _bitonic_topk_rebuild(const bool idir, const int m_iter, const int k
                         {
                             for (uint ii = 0; ii < inner_d; ii++)
                             {
-                                bitonic_topk_load16(4, 2 * dist); // load/store with offset of face 1 (in row major face layout)
+                                bitonic_topk_load16<is_fp32_dest_acc_en>(4, 2 * dist); // load/store with offset of face 1 (in row major face layout)
                                 bitonic_topk_step_N(dir);
-                                bitonic_topk_store16<false>(4, 2 * dist); // load/store with offset of face 1 (in row major face layout)
+                                bitonic_topk_store16<is_fp32_dest_acc_en, false>(4, 2 * dist); // load/store with offset of face 1 (in row major face layout)
                                 uint dst_inc = 8;
                                 dst_offset += dst_inc;
                                 bool dst_cr = false;
@@ -672,9 +680,9 @@ inline void _bitonic_topk_rebuild(const bool idir, const int m_iter, const int k
                     {
                         if (init_rebuild)
                         {
-                            load_replay_buf<Exec>(0, 8, [] { bitonic_topk_load16(4, 8); });
+                            load_replay_buf<Exec>(0, 8, [] { bitonic_topk_load16<is_fp32_dest_acc_en>(4, 8); });
                             bitonic_topk_ph3_st4_to_1(dir, init_rebuild, 8);
-                            load_replay_buf<Exec>(13, 8, [] { bitonic_topk_store16<true>(4, 8); });
+                            load_replay_buf<Exec>(13, 8, [] { bitonic_topk_store16<is_fp32_dest_acc_en, true>(4, 8); });
                         }
                         else
                         {
