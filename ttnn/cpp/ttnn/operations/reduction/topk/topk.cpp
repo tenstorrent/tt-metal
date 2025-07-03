e42eb23e0e84e6cbc8bd98b47a93b7af5e9eedbe
Johanna Rock <129077594+johanna-rock-tt@users.noreply.github.com>
2025-07-03 14:04:36 +0200

    Topk optimisation: indices as optional input tensor (#24434)
    ### Ticket
    -
    
    ### What's changed
    Indices for topk can now be passed to the op as optional tensor, if
    passed in there is no indices creation in the reader kernel which speeds
    up the op considerably.
    
    Perf for [1,1,32,16k] inputs (llama shape per device) on 9 cores:
    Old: 620 us
    New: 322 us
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16021381126)
    passing
    - [ ] [Again after nullopt
    change](https://github.com/tenstorrent/tt-metal/actions/runs/16027214396)

--- ttnn/cpp/ttnn/operations/reduction/topk/topk.cpp
+++ ttnn/cpp/ttnn/operations/reduction/topk/topk.cpp
@@ -37,7 +37,8 @@ std::vector<Tensor> post_topk_transform_tensor(
     const uint32_t adjusted_k,
     const Shape& original_lshape,
     const MemoryConfig& input_memory_config,
-    const CoreRangeSet& sub_core_grids) {
+    const CoreRangeSet& sub_core_grids,
+    const std::optional<Tensor>& indices_tensor = std::nullopt) {
     const auto& input_shape = input_tensor.padded_shape();
     const auto orig_rank = input_shape.rank();
 
@@ -108,6 +109,7 @@ std::vector<Tensor> ExecuteTopK::invoke(
     const bool sorted,
     const std::optional<MemoryConfig>& memory_config,
     const std::optional<CoreRangeSet>& sub_core_grids,
+    const std::optional<Tensor>& indices_tensor,
     std::optional<std::tuple<Tensor, Tensor>> optional_output_tensors) {
     const ttnn::Shape& original_lshape = input_tensor.logical_shape();
 
@@ -132,10 +134,10 @@ std::vector<Tensor> ExecuteTopK::invoke(
     auto output_tensor_vec = tt::tt_metal::operation::run(
         TopK{adjusted_k, -1, largest, sorted, input_memory_config, used_sub_core_grids},
         {padded_tensor},
-        {},
+        {indices_tensor},
         optional_output_tensors.has_value()
             ? reduction_common::tuple_to_vector_optional(optional_output_tensors.value())
-            : std::vector<std::optional<Tensor>>{},
+            : std::vector<std::optional<Tensor>>{std::nullopt, std::nullopt},
         queue_id);
 
     return CMAKE_UNIQUE_NAMESPACE::post_topk_transform_tensor(
@@ -147,7 +149,8 @@ std::vector<Tensor> ExecuteTopK::invoke(
         adjusted_k,
         original_lshape,
         input_memory_config,
-        used_sub_core_grids);
+        used_sub_core_grids,
+        indices_tensor);
 }
 
 }  // namespace ttnn::operations::reduction
