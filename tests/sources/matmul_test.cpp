1512a496aee0c3642c5a276a089559ff003bac7c
Lazar Djurovic <ldjurovic@tenstorrent.com>
2025-07-04 11:18:35 +0200

    Matmul for multiple tiles ( square tensor case ) (#450)
    ### Ticket
    #235 
    
    ### Problem description
    Matmul test only ran in case of 1x1 tile which is 32x32. 
    
    ### What's changed
    Intiduced tilize_blcok functions to prepare input for matmul and changed
    cpp test to iterate over kt dimension and multiply partials along the
    way.
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update

--- tests/sources/matmul_test.cpp
+++ tests/sources/matmul_test.cpp
@@ -21,17 +21,40 @@ uint32_t math_sync_tile_dst_index = 0;
 
 void run_kernel()
 {
-    std::uint32_t ct_dim = 1;
-    std::uint32_t rt_dim = 1;
-    std::uint32_t kt_dim = 1;
+    std::uint32_t ct_dim = BLOCK_CT_DIM;
+    std::uint32_t rt_dim = BLOCK_RT_DIM;
+    std::uint32_t kt_dim = BLOCK_CT_DIM; // for square matrices, kt_dim == ct_dim
 
     std::uint32_t tile_size = 128;
 
-    _llk_unpack_AB_matmul_hw_configure_<is_fp32_dest_acc_en, StochRndType::None>(UNPACK_A_IN, UNPACK_B_IN, UNPACK_A_OUT, UNPACK_B_OUT);
-    _llk_unpack_AB_matmul_init_<>();
-    for (int i = 0; i < TILE_CNT; i++)
+    if constexpr (static_cast<DataFormat>(UNPACK_A_IN) == DataFormat::Bfp8_b)
     {
-        _llk_unpack_AB_matmul_<>(L1_ADDRESS(buffer_A[i]), L1_ADDRESS(buffer_B[i]), 0, 0, tile_size, tile_size);
+        tile_size = 68;
+    }
+    else if constexpr (static_cast<DataFormat>(UNPACK_A_IN) == DataFormat::Float32)
+    {
+        tile_size = 256;
+    }
+
+    _llk_unpack_AB_matmul_hw_configure_<is_fp32_dest_acc_en, StochRndType::None>(
+        UNPACK_A_IN, UNPACK_B_IN, UNPACK_A_OUT, UNPACK_B_OUT, FACE_R_DIM, FACE_R_DIM, 0, 4, 4, tile_size, tile_size);
+    _llk_unpack_AB_matmul_init_<>(0, ct_dim, rt_dim, kt_dim, FACE_R_DIM, FACE_R_DIM);
+    for (uint32_t j = 0; j < kt_dim; j++)
+    {
+        _llk_unpack_AB_matmul_<>(
+            L1_ADDRESS(buffer_A[0]),
+            L1_ADDRESS(buffer_B[0]),
+            j,
+            j * ct_dim,
+            tile_size,
+            tile_size,
+            FACE_R_DIM,
+            FACE_R_DIM,
+            false,
+            false,
+            ct_dim,
+            rt_dim,
+            kt_dim);
     }
 }
 
@@ -45,14 +68,19 @@ void run_kernel()
 
 void run_kernel()
 {
-    _llk_math_matmul_init_<MATH_FIDELITY, DstTileFaceLayout::RowMajor>();
+    std::uint32_t ct_dim = BLOCK_CT_DIM;
+    std::uint32_t rt_dim = BLOCK_RT_DIM;
+    std::uint32_t kt_dim = BLOCK_CT_DIM; // for square matrices, kt_dim == ct_dim
+
+    _llk_math_matmul_init_<MATH_FIDELITY, DstTileFaceLayout::RowMajor>(TILE_R_DIM, TILE_C_DIM, TILE_R_DIM, TILE_C_DIM, false, 0, ct_dim, rt_dim, kt_dim);
     _llk_math_pack_sync_init_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
     _llk_math_hw_configure_<false, false>(MATH_FORMAT, MATH_FORMAT);
-    for (int i = 0; i < TILE_CNT; i++)
+    _llk_math_wait_for_dest_available_<DstSync::SyncHalf>();
+    for (uint32_t j = 0; j < kt_dim; j++)
     {
-        _llk_math_wait_for_dest_available_<DstSync::SyncHalf>();
-        _llk_math_matmul_<MATH_FIDELITY, DstTileFaceLayout::RowMajor>(i);
+        _llk_math_matmul_<MATH_FIDELITY, DstTileFaceLayout::RowMajor>(0, 0, ct_dim, rt_dim, kt_dim);
     }
+
     _llk_math_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
 }
 
@@ -76,9 +104,9 @@ void run_kernel()
     _llk_pack_dest_init_<DstSync::SyncHalf, is_fp32_dest_acc_en, DstTileFaceLayout::RowMajor, false>();
 #endif
 
-    _llk_packer_wait_for_math_done_();
     for (int i = 0; i < TILE_CNT; i++)
     {
+        _llk_packer_wait_for_math_done_();
         _llk_pack_<DstSync::SyncHalf, is_fp32_dest_acc_en, false>(i, L1_ADDRESS(buffer_Res[i]));
     }
     _llk_pack_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
