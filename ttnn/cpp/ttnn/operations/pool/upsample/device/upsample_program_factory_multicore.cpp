e4dbc6260cf44b3fe672873fe7d6120904da2e12
Aleksandar Stancov <astancov@tenstorrent.com>
2025-07-04 15:36:18 +0000

    temp commit - needs some work

--- ttnn/cpp/ttnn/operations/pool/upsample/device/upsample_program_factory_multicore.cpp
+++ ttnn/cpp/ttnn/operations/pool/upsample/device/upsample_program_factory_multicore.cpp
@@ -2,7 +2,9 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+#include <cstddef>
 #include <cstdint>
+#include <tt-logger/tt-logger.hpp>
 #include <vector>
 
 #include "upsample_op.hpp"
@@ -32,11 +34,15 @@ static Tensor create_config_tensor(
     const uint32_t scale_factor_w,
     const bool is_height_sharded) {
     uint16_t core_idx = 0;        // Tracks the current core being processed
+    uint16_t core_idx_start = 0;
     uint16_t stick_offset = 0;    // Tracks the current stick offset within the core
+    uint16_t stick_offset_start = 0;  // Tracks the current stick offset within the core
+    uint16_t stick_cnt = 0;
     uint16_t ch_start_core = 0;   // Starting core index where channels are distributed
     uint16_t ch_end_core = 0;     // Ending core index where channels are distributed
     uint16_t nhw_start_core = 0;  // Starting core index for NHW distribution
     const uint32_t input_nsticks_per_core = shard_spec.shape[0];
+    const uint32_t output_nsticks_per_core = input_nsticks_per_core * scale_factor_h;
 
     auto logical_cores = corerange_to_cores(
         shard_spec.grid, shard_spec.num_cores(), shard_spec.orientation == ShardOrientation::ROW_MAJOR);
@@ -56,82 +62,134 @@ static Tensor create_config_tensor(
     }
 
     std::vector<uint16_t> logical_core_to_stick_map;
-    const size_t logical_core_to_stick_map_entry_size = 3;
-    size_t row_size = logical_core_to_stick_map_entry_size * in_w;
+    std::vector<uint16_t> dst_core_end_idx_map;
+    const size_t logical_core_to_stick_map_entry_size = 4;
+    bool do_insert = false;
     // Create map of core and respective offsets in input
     for (uint32_t b = 0; b < batch_size; ++b) {
         for (uint32_t h = 0; h < in_h; ++h) {
-            for (uint32_t w = 0; w < in_w; ++w, ++stick_offset) {
-                if (stick_offset == input_nsticks_per_core) {
-                    stick_offset = 0, ++core_idx;
+            for (uint32_t j = 0; j < scale_factor_h; ++j) {
+                stick_offset_start = stick_offset;
+                core_idx_start = core_idx;
+
+                for (uint32_t w = 0; w < in_w; ++w, ++stick_offset, ++stick_cnt) {
+                    if (stick_offset == input_nsticks_per_core) {
+                        stick_offset = 0;
+                        core_idx++;
+                    }
+                    if (stick_cnt != 0 && stick_cnt % output_nsticks_per_core == 0) {
+                        dst_core_end_idx_map.push_back(logical_core_to_stick_map.size());  // didn't -1, < later
+
+                        if (is_height_sharded) {
+                            auto& c = logical_cores[core_idx];
+                            logical_core_to_stick_map.push_back(c.x);
+                            logical_core_to_stick_map.push_back(c.y);
+                        } else {
+                            logical_core_to_stick_map.push_back(nhw_start_core + core_idx);
+                            logical_core_to_stick_map.push_back(0);
+                        }
+                        logical_core_to_stick_map.push_back(stick_offset);  // stick_offset_start
+                        logical_core_to_stick_map.push_back(stick_offset);  // stick_offset_end, updated later
+                    } else if (stick_offset == 0 || do_insert) {
+                        if (do_insert) {
+                            do_insert = false;
+                        }
+                        // Insert new entry
+                        if (is_height_sharded) {
+                            auto& c = logical_cores[core_idx];
+                            logical_core_to_stick_map.push_back(c.x);
+                            logical_core_to_stick_map.push_back(c.y);
+                        } else {
+                            logical_core_to_stick_map.push_back(nhw_start_core + core_idx);
+                            logical_core_to_stick_map.push_back(0);
+                        }
+                        logical_core_to_stick_map.push_back(stick_offset);  // stick_offset_start
+                        logical_core_to_stick_map.push_back(stick_offset);  // stick_offset_end, updated later
+                    } else {
+                        logical_core_to_stick_map[logical_core_to_stick_map.size() - 1]++;
+                    }
                 }
-                if (is_height_sharded) {
-                    logical_core_to_stick_map.push_back(logical_cores[core_idx].x);
-                    logical_core_to_stick_map.push_back(logical_cores[core_idx].y);
-                } else {
-                    logical_core_to_stick_map.push_back(nhw_start_core + core_idx);
-                    logical_core_to_stick_map.push_back(0);
+                if (j < scale_factor_h - 1) {
+                    stick_offset = stick_offset_start;
+                    core_idx = core_idx_start;
+                    do_insert = true;  // insert new entry in next loop
                 }
-                logical_core_to_stick_map.push_back(stick_offset);
-            }
-            for (uint32_t j = 1; j < scale_factor_h; ++j) {
-                logical_core_to_stick_map.insert(
-                    logical_core_to_stick_map.end(),
-                    logical_core_to_stick_map.end() - row_size,
-                    logical_core_to_stick_map.end());
             }
         }
     }
+    dst_core_end_idx_map.push_back(logical_core_to_stick_map.size());
 
-    /* Each entry in config_vector contains 2 elements:
+    uint32_t elems_per_core = 0;
+    uint32_t last_ind = 0;
+    for (size_t it = 0; it < dst_core_end_idx_map.size(); it++) {
+        uint32_t elem_num = (dst_core_end_idx_map[it] - last_ind) / 4;
+        elems_per_core = elem_num > elems_per_core ? elem_num : elems_per_core;
+        last_ind = dst_core_end_idx_map[it];
+    }
+
+    /* Each entry in config_vector contains 4 elements:
      * {{core_coords.x, core_coords.y}, stick_offset(in input_cb)}
      * - core_coords.x: X coordinate of the core
      * - core_coords.y: Y coordinate of the core
-     * - stick_offset: Offset within the input circular buffer
+     * - stick_offset_start: Offset start within the input circular buffer
+     * - stick_offset_end: Offset end within the input circular buffer
+     * - pad: Padding element to ensure entry size is a multiple of 4*bfloat16
      */
     std::vector<uint16_t> config_vector;
 
-    const uint32_t config_buffer_entry_size = 2;
-    const uint32_t elems_per_core = config_buffer_entry_size * scale_factor_h * input_nsticks_per_core;
+    const uint32_t config_buffer_entry_size = 4;
+    elems_per_core *= config_buffer_entry_size;
 
-    // In case last input shard is not full, fill the rest of the config vector with the last two elements
+    // Based on core calculate physical location of cores
+    CoreCoord core_coords;
+
+    // In case last input shard is not full, fill the rest of the config vector with placeholder values
     const auto pad_uneven_shards = [elems_per_core, config_buffer_entry_size](
                                        auto& config_vector, size_t slice_begin = 0) {
         const uint32_t slice_length = config_vector.size() - slice_begin;
         const uint32_t remainder = (elems_per_core - (slice_length % elems_per_core)) % elems_per_core;
         if (remainder != 0) {
-            uint16_t before_last = config_vector[config_vector.size() - 2];
-            uint16_t last = config_vector[config_vector.size() - 1];
+            // uint16_t last4 = config_vector[config_vector.size() - 4];
+            // uint16_t last2 = config_vector[config_vector.size() - 2];
             for (int i = 0; i < remainder / config_buffer_entry_size; i++) {
-                config_vector.push_back(before_last);
-                config_vector.push_back(last);
+                config_vector.push_back(0);  // core
+                config_vector.push_back(1);  // stick offset start
+                config_vector.push_back(0);  // stick offset end
+                config_vector.push_back(0);  // pad
             }
         }
     };
 
-    // Based on core calculate physical location of cores
-    CoreCoord core_coords;
     if (is_height_sharded) {
-        for (size_t j = 0; j < logical_core_to_stick_map.size(); j += logical_core_to_stick_map_entry_size) {
-            core_coords = device->worker_core_from_logical_core(
-                CoreCoord(logical_core_to_stick_map[j], logical_core_to_stick_map[j + 1]));
-            // Combine the x and y coordinates of the core into a single 16-bit value.
-            const uint16_t cores = (core_coords.x << 8) + core_coords.y;
-            config_vector.push_back(cores);
-            config_vector.push_back(logical_core_to_stick_map[j + 2]);
-        }
-        pad_uneven_shards(config_vector);
-    } else {
-        for (size_t i = ch_start_core; i <= ch_end_core; i++) {
+        for (size_t ind = 0, j = 0; ind < dst_core_end_idx_map.size(); ind++) {
             const size_t chan_slice_begin = config_vector.size();
-            for (size_t j = 0; j < logical_core_to_stick_map.size(); j += logical_core_to_stick_map_entry_size) {
-                core_coords = device->worker_core_from_logical_core(CoreCoord(i, logical_core_to_stick_map[j]));
+            for (; j < dst_core_end_idx_map[ind]; j += 4) {
+                core_coords = device->worker_core_from_logical_core(
+                    CoreCoord(logical_core_to_stick_map[j], logical_core_to_stick_map[j + 1]));
                 // Combine the x and y coordinates of the core into a single 16-bit value.
                 const uint16_t cores = (core_coords.x << 8) + core_coords.y;
                 config_vector.push_back(cores);
                 config_vector.push_back(logical_core_to_stick_map[j + 2]);
+                config_vector.push_back(logical_core_to_stick_map[j + 3]);
+                config_vector.push_back(0);
             }
             pad_uneven_shards(config_vector, chan_slice_begin);
+        }
+    } else {
+        for (size_t i = ch_start_core; i <= ch_end_core; i++) {
+            for (size_t ind = 0, j = 0; ind < dst_core_end_idx_map.size(); ind++) {
+                const size_t chan_slice_begin = config_vector.size();
+                for (; j < dst_core_end_idx_map[ind]; j += 4) {
+                    core_coords = device->worker_core_from_logical_core(CoreCoord(i, logical_core_to_stick_map[j]));
+                    // Combine the x and y coordinates of the core into a single 16-bit value.
+                    const uint16_t cores = (core_coords.x << 8) + core_coords.y;
+                    config_vector.push_back(cores);
+                    config_vector.push_back(logical_core_to_stick_map[j + 2]);
+                    config_vector.push_back(logical_core_to_stick_map[j + 3]);
+                    config_vector.push_back(0);
+                }
+                pad_uneven_shards(config_vector, chan_slice_begin);
+            }
         }
     }
 
@@ -259,7 +317,8 @@ operation::ProgramWithCallbacks upsample_multi_core(
         input_stick_nbytes,
         input_nsticks_per_core,
         scale_factor_h,
-        scale_factor_w};
+        scale_factor_w,
+        (uint32_t)config_tensor.logical_shape()[-1] / 4};
     std::string writer_kernel_fname =
         "ttnn/cpp/ttnn/operations/pool/upsample/device/kernels/dataflow/writer_upsample_multi_core_sharded.cpp";
     auto writer_kernel =
