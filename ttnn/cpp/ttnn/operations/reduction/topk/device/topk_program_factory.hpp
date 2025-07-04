7a4a25c620ddbeb898aaaddc709e326c4dea310d
Radomir Djogo <rdjogo@tenstorrent.com>
2025-07-04 15:20:49 +0000

    fix clang tidy

--- ttnn/cpp/ttnn/operations/reduction/topk/device/topk_program_factory.hpp
+++ ttnn/cpp/ttnn/operations/reduction/topk/device/topk_program_factory.hpp
@@ -8,11 +8,11 @@ namespace ttnn::operations::reduction::detail {
 
 tt::tt_metal::operation::ProgramWithCallbacks topk_single_core_interleaved(
     const Tensor& input_tensor,
-    const uint32_t k,
-    const int8_t dim,
-    const bool largest,
-    const bool sorted,
-    const bool uint16_output,
+    uint32_t k,
+    int8_t dim,
+    bool largest,
+    bool sorted,
+    bool uint16_output,
     const CoreRangeSet& sub_core_grids,
     Tensor& value_tensor,
     Tensor& index_tensor);
