e3ec36bf48f84ab751c93e8735c0f8a0b403c873
Djordje <divanovic@tenstorrent.com>
2025-07-07 10:16:36 +0000

    #0: Update AR ff2 target

--- models/demos/llama3_subdevices/tests/tg_perf_unit_tests/test_ccl_async_perf_TG_llama.py
+++ models/demos/llama3_subdevices/tests/tg_perf_unit_tests/test_ccl_async_perf_TG_llama.py
@@ -70,7 +70,7 @@ def test_ag_tg_llama_perf(
 @pytest.mark.parametrize(
     "ar_type, warmup_iters, perf_target_us",
     [
-        ("ff2", 15, 18.6),
+        ("ff2", 15, 19.0),
     ],
 )
 @pytest.mark.models_device_performance_bare_metal
