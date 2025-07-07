24a670d1b6b35a3a79050c36317e0e5a13cf2805
Adrian Morrison <amorrison@tenstorrent.com>
2025-07-07 18:06:38 +0000

    missed a license and an add

--- ttnn/cpp/ttnn/operations/ccl/common/kernels/moe_utils.hpp
+++ ttnn/cpp/ttnn/operations/ccl/common/kernels/moe_utils.hpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
 #include "tt_metal/fabric/hw/inc/tt_fabric_api.h"
 #include "tt_metal/fabric/hw/inc/tt_fabric_interface.h"
 #include "tt_metal/fabric/hw/inc/edm_fabric/fabric_connection_manager.hpp"
