14dd89aeaba24fbe5ae3a7d325d296015d0ebde3
Ata Tuzuner <atuzuner@tenstorrent.com>
2025-07-07 09:13:44 -0400

    [DM]: Updating static plots (#24476)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/23506)
    
    ### Problem description
    There are some remaining items for Data Movement static plots:
    
    - All-to-all/All-from-all median plots (too noisy otherwise)
    - One-to-all separate loopback plotting
    - Add tables of the BW values
    
    ### What's changed
    All above items are addressed. In addition, broken BH regression CI
    pipeline is fixed.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16080172384)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16080173248)
    CI with demo tests passes (if applicable)
    - [x] [metal - Run
    microbenchmarks](https://github.com/tenstorrent/tt-metal/actions/runs/16080174029)

--- tests/tt_metal/tt_metal/data_movement/dram_unary/test_unary_dram.cpp
+++ tests/tt_metal/tt_metal/data_movement/dram_unary/test_unary_dram.cpp
@@ -278,12 +278,13 @@ TEST_F(DeviceFixture, TensixDataMovementDRAMSharded) {
 
 /* ========== Directed ideal test case; Test id = 3 ========== */
 TEST_F(DeviceFixture, TensixDataMovementDRAMDirectedIdeal) {
+    // Physical Constraints
+    auto [page_size_bytes, max_transmittable_bytes, max_transmittable_pages] =
+        tt::tt_metal::unit_tests::dm::compute_physical_constraints(arch_, devices_.at(0));
+
     // Parameters
-    uint32_t num_of_transactions = 179;
-    uint32_t transaction_size_pages = 4 * 32;
-    uint32_t page_size_bytes = arch_ == tt::ARCH::BLACKHOLE ? 64 : 32;  // =Flit size: 32 bytes for WH, 64 for BH
-    // Max transaction size = 4 * 32 pages = 128 * 32 bytes = 4096 bytes for WH; 8192 bytes for BH
-    // Max total transaction size = 179 * 8192 bytes = 1466368 bytes = 1.4 MB = L1 capacity
+    uint32_t num_of_transactions = 1;
+    uint32_t transaction_size_pages = max_transmittable_pages / num_of_transactions;
 
     // Cores
     CoreRange core_range({0, 0}, {0, 0});
