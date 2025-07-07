125430c2e563355e12618ab62a43bbfc405d843e
David Popov <dpopov@tenstorrent.com>
2025-07-07 16:21:06 +0000

    Remove trace debug from cmake call

--- setup.py
+++ setup.py
@@ -185,8 +185,6 @@ class CMakeBuild(build_ext):
             if "CIBUILDWHEEL" in os.environ:
                 cmake_args = [
                     "cmake",
-                    "--trace",
-                    "--trace-expand",
                     "-B",
                     build_dir,
                     "-G",
