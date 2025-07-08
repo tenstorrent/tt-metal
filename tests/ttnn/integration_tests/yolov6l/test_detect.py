cc8f795d1b877dc1921efea94ee95b7e2df8f9a5
Punith Sekar <148861607+punithsekar@users.noreply.github.com>
2025-07-08 22:03:01 +0530

    Fix yolov6l CI failure (#24638)
    ### Problem description
    (Single-card) Frequent model and ttnn tests CI test is failing due to
    Yolov6l model,
    https://github.com/tenstorrent/tt-metal/actions/runs/16047427538/job/45295642873#step:8:5137.
    
    ### What's changed
    Fixed the issues on yolov6l.
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [(Single-card) Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/fast-dispatch-full-regressions-and-models.yaml)
    CI passes

--- tests/ttnn/integration_tests/yolov6l/test_detect.py
+++ tests/ttnn/integration_tests/yolov6l/test_detect.py
@@ -2,26 +2,22 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-import torch
-import ttnn
 import pytest
-import sys
-from models.experimental.yolov6l.reference.yolov6l_utils import fuse_model
-from models.experimental.yolov6l.tt.model_preprocessing import create_yolov6l_model_parameters_detect
+
+import torch
+
+import ttnn
+from models.experimental.yolov6l.tt.model_preprocessing import (
+    create_yolov6l_model_parameters_detect,
+    load_torch_model_yolov6l,
+)
 from models.experimental.yolov6l.tt.ttnn_detect import TtDetect
 from tests.ttnn.utils_for_testing import assert_with_pcc
-from models.utility_functions import comp_pcc
-
-sys.path.append("models/experimental/yolov6l/reference/")
 
 
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
 def test_detect(device, reset_seeds):
-    weights = "tests/ttnn/integration_tests/yolov6l/yolov6l.pt"
-    ckpt = torch.load(weights, map_location=torch.device("cpu"), weights_only=False)
-    model = ckpt["ema" if ckpt.get("ema") else "model"].float()
-    model = fuse_model(model).eval()
-    stride = int(model.stride.max())
+    model = load_torch_model_yolov6l()
 
     model = model.detect
 
