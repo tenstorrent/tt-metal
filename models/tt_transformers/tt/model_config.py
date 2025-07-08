5319de264c586bc5fadf6a4c2e4bd90f86e4f4f2
velonica <2120210430@mail.nankai.edu.cn>
2025-07-08 18:36:10 +0800

    reference_attention

--- models/tt_transformers/tt/model_config.py
+++ models/tt_transformers/tt/model_config.py
@@ -2089,9 +2089,9 @@ class ModelArgs:
         else:
             model = self.reference_transformer(wrap=False)
             layer = model.model.layers[0].self_attn
-            use_position_embeddings = layer.__class__.__name__ == "Qwen3Attention"
+            # use_position_embeddings = layer.__class__.__name__ == "Qwen3Attention"
             wrapper = HfAttentionWrapper(
-                layer, self.head_dim, model.model.rotary_emb if use_position_embeddings else None
+                layer, self.head_dim, model.model.rotary_emb 
             )
             return wrapper
 
