b45d2de613d3def65055af7aebbe08e74abffacf
Sean Nijjar <sean.nijjar@gmail.com>
2025-07-08 12:35:11 -0400

    Enable Erisc IRAM by default when persistent fabric is launched for Wormhole (#20523)
    Note that this PR was originally intended to enable erisc IRAM support
    by default for all kernels. However, that was not possible due to
    performance regressions for some legacy CCLs where the overhead to copy
    to IRAM was an overall net loss in performance when compared to the
    instruction fetch speedups.
    
    Therefore global default enablement is deferred until legacy CCLs are
    ported to run on fabric. For this PR, we instead only enable the IRAM
    feature on Erisc when building fabric.
    
    The overall flow for deciding if Erisc IRAM is enabled is then as
    follows:
    - if user specifies `TT_METAL_ENABLE_ERISC_IRAM` flag, override all
    other behaviour with user choice
    - else if/when fabric instantiated at device init time, enable IRAM
      - disable when torn down
    - else Erisc iram disabled
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI: https://github.com/tenstorrent/tt-metal/actions/runs/16078141887
    - [x] BH PC:
    https://github.com/tenstorrent/tt-metal/actions/runs/16081848208
    - [x] T3K unit:
    https://github.com/tenstorrent/tt-metal/actions/runs/16078143816
    - [x] T3K demo, frequent, nightly, model perf:
    https://github.com/tenstorrent/tt-metal/actions/runs/16081850565
    - demo failures same as main:
    https://github.com/tenstorrent/tt-metal/actions/runs/16062787249
    - frequent:
    https://github.com/tenstorrent/tt-metal/actions/runs/16133050434 (after
    testlist fixes)
    - [x] TG:
    https://github.com/tenstorrent/tt-metal/actions/runs/15982260830
    - [x] TG Stress:
    https://github.com/tenstorrent/tt-metal/actions/runs/15914430447
    - [x] metal ubench:
    https://github.com/tenstorrent/tt-metal/actions/runs/15916997396
      - same fails as main 
    - [x] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI: https://github.com/tenstorrent/tt-metal/actions/runs/14866679747
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI: https://github.com/tenstorrent/tt-metal/actions/runs/14866681192
    - [x] **(For models and ops writers)** Full [new models
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/full-new-models-suite.yaml)
    CI: https://github.com/tenstorrent/tt-metal/actions/runs/14866686623
    
    Closes #20340

--- models/demos/llama3_subdevices/tests/test_decoder_device_perf.py
+++ models/demos/llama3_subdevices/tests/test_decoder_device_perf.py
@@ -381,7 +381,7 @@ def load_perf_targets(galaxy_type):
 @pytest.mark.timeout(900)
 @pytest.mark.models_device_performance_bare_metal
 # To update:
-# Run FAKE_DEVICE=TG TT_METAL_ENABLE_ERISC_IRAM=1 pytest models/demos/llama3_subdevices/tests/test_decoder_device_perf.py::test_llama_TG_perf_device
+# Run FAKE_DEVICE=TG pytest models/demos/llama3_subdevices/tests/test_decoder_device_perf.py::test_llama_TG_perf_device
 # Copy the printed kernel_duration_per_instance_averaged_dict and dispatch_duration_per_instance_averaged_dict dictionaries
 # Manually compare each entry between old-expected and the new average values
 # - Any perf regressions? Everything as expected?
@@ -787,7 +787,7 @@ def test_llama_TG_perf_device(
 @pytest.mark.timeout(900)
 @pytest.mark.models_device_performance_bare_metal
 # To update:
-# Run FAKE_DEVICE=TG TT_METAL_ENABLE_ERISC_IRAM=1 TT_METAL_KERNELS_EARLY_RETURN=1  pytest models/demos/llama3_subdevices/tests/test_decoder_device_perf.py::test_llama_TG_perf_device_non_overlapped_dispatch
+# Run FAKE_DEVICE=TG TT_METAL_KERNELS_EARLY_RETURN=1  pytest models/demos/llama3_subdevices/tests/test_decoder_device_perf.py::test_llama_TG_perf_device_non_overlapped_dispatch
 # Copy the printed dispatch_duration_per_instance_averaged_dict dictionary
 # Manually compare each entry between old-expected and the new average values
 # - Any perf regressions? Everything as expected?
