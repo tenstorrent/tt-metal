f44977549b321500530a267169e2ef55efeed411
Marko Staletovic <mstaletovic@tenstorrent.com>
2025-07-08 14:39:35 +0000

    Added autoshard support for bilinear upsample operation

--- ttnn/cpp/ttnn/operations/pool/upsample/upsample.cpp
+++ ttnn/cpp/ttnn/operations/pool/upsample/upsample.cpp
@@ -3,10 +3,44 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "upsample.hpp"
+#include <algorithm>
 #include "device/upsample_op.hpp"
+#include "tt-metalium/buffer_types.hpp"
+#include "tt-metalium/work_split.hpp"
 #include "ttnn/run_operation.hpp"
+#include "ttnn/tensor/types.hpp"
 
 namespace ttnn::operations::upsample {
+using namespace tt;
+using namespace tt::tt_metal;
+
+static ShardSpec compute_bilinear_autoshard_spec(const ttnn::Tensor& input_tensor) {
+    /*
+    Calculates the sharding spec required for bilinear transform
+    */
+    const auto& input_shape = input_tensor.logical_shape();
+    const auto batch_size = input_shape[0];
+    const auto input_h = input_shape[1];
+    const auto input_w = input_shape[2];
+    const auto num_channels = input_shape[3];
+    auto compute_grid_size = input_tensor.device()->compute_with_storage_grid_size();
+    uint32_t max_num_cores = compute_grid_size.x * compute_grid_size.y;
+    uint32_t max_num_shards = batch_size * input_h * input_w;
+    uint32_t num_shards = std::min(max_num_cores, max_num_shards);
+    while (num_shards > 0) {
+        if ((batch_size * input_h % num_shards == 0) &&
+            ((batch_size * input_h * input_w / num_shards) % input_w == 0)) {
+            break;
+        }
+        num_shards--;
+    }
+    uint32_t num_cores = num_shards;
+    CoreRangeSet core_range_set = num_cores_to_corerangeset(num_cores, compute_grid_size, true);
+    uint32_t shard_height = batch_size * input_h * input_w / num_cores;
+    uint32_t shard_width = num_channels;
+    auto shard_orientation = ShardOrientation::ROW_MAJOR;
+    return ShardSpec(core_range_set, {shard_height, shard_width}, shard_orientation);
+}
 
 ttnn::Tensor ExecuteUpSample::invoke(
     const ttnn::Tensor& input_tensor,
@@ -15,8 +49,6 @@ ttnn::Tensor ExecuteUpSample::invoke(
     const std::optional<MemoryConfig>& output_mem_config,
     const std::optional<DeviceComputeKernelConfig>& compute_kernel_config) {
     MemoryConfig mem_config = output_mem_config.value_or(input_tensor.memory_config());
-    ttnn::DeviceComputeKernelConfig config = compute_kernel_config.value_or(
-        ttnn::init_device_compute_kernel_config(input_tensor.device()->arch(), std::nullopt, MathFidelity::HiFi4));
 
     int scale_h = 1;
     int scale_w = 1;
@@ -36,28 +68,33 @@ ttnn::Tensor ExecuteUpSample::invoke(
         },
         scale_factor);
 
-    // DEBUG
-    // fmt::print("scale_h: {}, scale_w: {}\n", scale_h, scale_w);
-
-    // Mozda mogu i ovde nesto da stavim? U fazonu ako je non sharded a bilinear je, ovde da ga shardujem
-    // Verovatno ima vise smisla tbh
-
     if (!input_tensor.is_sharded() && mode == "bilinear") {
-    }
+        // Bilinear mode with non sharded input is not supported. Performing autosharding
 
-    if (input_tensor.is_sharded()) {
-        // TT_FATAL(not input_tensor.is_sharded(), "Error");
-        int shard_height = input_tensor.memory_config().shard_spec().value().shape[0];
-        const auto& input_shape = input_tensor.logical_shape();
-        const auto batch_size = input_shape[0];
-        const auto input_h = input_shape[1];
-        const auto input_w = input_shape[2];
-        const auto num_channels = input_shape[3];
-        if (shard_height % input_w != 0) {
-            TT_FATAL(shard_height % input_w != 0, "Error");
+        auto input_tensor_sharded = input_tensor;
+        auto memory_layout = TensorMemoryLayout::HEIGHT_SHARDED;
+        auto buffer_type = BufferType::L1;
+
+        ShardSpec shard_spec = compute_bilinear_autoshard_spec(input_tensor_sharded);
+        MemoryConfig sharded_memory_config = MemoryConfig(memory_layout, buffer_type, shard_spec);
+        input_tensor_sharded = to_memory_config(input_tensor_sharded, sharded_memory_config);
+        ttnn::DeviceComputeKernelConfig config = compute_kernel_config.value_or(ttnn::init_device_compute_kernel_config(
+            input_tensor_sharded.device()->arch(), std::nullopt, MathFidelity::HiFi4));
+        // Output sharding should be the same as input
+        // Output shard shape gets rescaled in op
+        auto output_tensor =
+            tt::tt_metal::operation::run(
+                UpSample{scale_h, scale_w, mode, sharded_memory_config, config}, {input_tensor_sharded})
+                .front();
+
+        if (output_mem_config.has_value() && output_tensor.memory_config() != output_mem_config) {
+            output_tensor = to_memory_config(output_tensor, output_mem_config.value());
         }
+        return output_tensor;
     }
 
+    ttnn::DeviceComputeKernelConfig config = compute_kernel_config.value_or(
+        ttnn::init_device_compute_kernel_config(input_tensor.device()->arch(), std::nullopt, MathFidelity::HiFi4));
     // return ttnn::upsample(input_tensor, scale_h, scale_w, mem_config);
     auto output_tensor =
         tt::tt_metal::operation::run(UpSample{scale_h, scale_w, mode, mem_config, config}, {input_tensor}).front();
