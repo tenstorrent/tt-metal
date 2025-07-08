2f767f1e9ef84d0a23ae97471d1a2916b06206c5
Bartłomiej Cieślar <37981302+barci2@users.noreply.github.com>
2025-07-08 09:27:32 +0200

    Separate the _new_state into an externally called create_state (#24622)
    ### DeepSeek implementation API change
    We separate the model state creation into a separate function to
    decouple it from the prefill and decode configs and 1. allow it to use
    the hf_config directly and 2. for create_run_config to be able to take
    the model configs separately (and not both at the same time)
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    
    Signed-off-by: Bartłomiej Cieślar <bcieslar@tenstorrent.com>

--- models/demos/deepseek_v3/utils/config_dataclass.py
+++ models/demos/deepseek_v3/utils/config_dataclass.py
@@ -1,11 +1,8 @@
 # SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC.
 # SPDX-License-Identifier: Apache-2.0
 
-import itertools
-from dataclasses import dataclass, fields, is_dataclass
-from enum import Enum
-from types import NoneType
-from typing import Any, Callable, Union
+from dataclasses import dataclass, fields
+from typing import Any, Union
 
 import ttnn
 
@@ -21,7 +18,10 @@ ProgramConfig = Union[
 @dataclass(frozen=True)
 class FromWeightConfig:
     """A stub in a model config that gets replaced with a real ttnn.Tensor when creating the RunConfig.
-    Needs a matching entry in the WeightConfig"""
+    Needs a matching entry in the WeightConfig. During the creation of the run config, mesh_device
+    gets filled with the ttnn.Device to load the tensor on"""
+
+    mesh_device: "ConfigDevice"
 
 
 @dataclass(frozen=True, eq=True)
@@ -30,7 +30,7 @@ class MeshDeviceStub:
 
     mesh_shape: tuple[int, int]
 
-    def __init__(self, mesh_shape: tuple[int, int] | ttnn.MeshDevice):
+    def __init__(self, mesh_shape: tuple[int, int] | ttnn.MeshShape):
         object.__setattr__(self, "mesh_shape", tuple(mesh_shape))
 
 
@@ -122,59 +122,3 @@ class RMSNormConfig(OpConfigBase):
     is_distributed: bool = False
     topology: ttnn.Topology = ttnn.Topology.Linear
     norm_category: str = None
-
-
-ConfigWithOp = dict[str, Any] | OpConfigBase
-ConfigWithoutOp = dict[str, Any]
-
-
-def merge_config_containers(
-    cfg_a: Any,
-    cfg_b: Any,
-    merge_config_specific_items: Callable[[Any, Any, ttnn.MeshDevice], Any],
-    mesh_device: ttnn.MeshDevice,
-) -> Any:
-    """Helper function to merge two configs, where the first one may partially consist of OpConfigs."""
-    if cfg_a is None and cfg_b is None:
-        return None
-
-    if is_op_config(cfg_a):
-        op_config_dict = {f.name: getattr(cfg_a, f.name) for f in fields(cfg_a)}  # type: ignore
-        return cfg_a.__class__(**merge_config_containers(op_config_dict, cfg_b, merge_config_specific_items, mesh_device))  # type: ignore
-
-    # If both configs are lists/tuples of the same length or one of them is None, merge them as a list/tuple.
-    if isinstance(cfg_a, (list, tuple, NoneType)) and isinstance(cfg_b, (list, tuple, NoneType)):
-        if cfg_a is None or cfg_b is None or (len(cfg_a) == len(cfg_b) and type(cfg_a) == type(cfg_b)):
-            container = type(cfg_a) if cfg_a is not None else type(cfg_b)
-            cfg_a = cfg_a or (container([None]) * len(cfg_b))
-            cfg_b = cfg_b or (container([None]) * len(cfg_a))
-            return container(
-                merge_config_containers(a, b, merge_config_specific_items, mesh_device)
-                for a, b in zip(cfg_a, cfg_b, strict=True)
-            )
-
-    if isinstance(cfg_a, (dict, NoneType)) and isinstance(cfg_b, (dict, NoneType)):
-        cfg_a = cfg_a or {}
-        cfg_b = cfg_b or {}
-        return {
-            k: merge_config_containers(cfg_a.get(k, None), cfg_b.get(k, None), merge_config_specific_items, mesh_device)
-            for k in itertools.chain(cfg_a.keys(), cfg_b.keys())
-        }
-
-    return merge_config_specific_items(cfg_a, cfg_b, mesh_device)
-
-
-def is_op_config(obj: Any) -> bool:
-    """Check if the object is an op config instance."""
-    return issubclass(type(obj), OpConfigBase) and is_dataclass(obj)
-
-
-WeightConfig = dict[str, "WeightConfig | str"]
-
-_PRIMITIVE_COPYABLE_TYPES = bool | int | float | complex | str | bytes | None | Enum
-# In general, we require ModelConfig to be deepcopyable
-ModelPrefillConfig = dict[str, "ModelPrefillConfig | _PRIMITIVE_COPYABLE_TYPES"] | OpConfigBase
-ModelDecodeConfig = dict[str, "ModelDecodeConfig | _PRIMITIVE_COPYABLE_TYPES"] | OpConfigBase
-
-RunPrefillConfig = dict[str, "RunPrefillConfig | _PRIMITIVE_COPYABLE_TYPES"] | OpConfigBase
-RunDecodeConfig = dict[str, "RunDecodeConfig | _PRIMITIVE_COPYABLE_TYPES"] | OpConfigBase
