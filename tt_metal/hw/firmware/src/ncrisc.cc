2e29ad630e845737fec12691dc16bab13fda9982
John Bauman <jbauman@tenstorrent.com>
2025-07-09 15:39:55 -0400

    Use mask to avoid processing unused CBs in firmware (#22551)
    ### Problem description
    Currently the firmware has to initialize all CBs up to
    max_local_cb_end_index, even if they're not all used. Since initializing
    each CB takes around 25 cycles (on wormhole), this leads to increased
    kernel-kernel initialization latency. We've tried to get op writers to
    make CBs contiguous, but that hasn't always happened.
    
    ### What's changed
    Instead of storing max_local_cb_end_index, store a mask of used local
    cbs. Then we can skip most work associated with unused CBs, at the cost
    of a few (~1) extra instructions.
    
    Also switch to using inline assembly for initializing CBs. The compiler
    can't fully optimize the existing code and has some stalls waiting for
    loads and some unnecessary instructions. For 32 CBs, the inline assembly
    reduces the number of cycles from around 770 to around 595.
    
    Some llama3 benchmark measurements need to be updated. In addition, the
    calculation of the non-overlapped dispatch time needs to be fixed: it
    was only considering the op-to-op time but in addition the kernel time
    for the previous op needs to be included as well. On any single core the
    kernel time is negligible (since TT_METAL_KERNELS_EARLY_RETURN is set),
    but across all cores some start the kernel earlier and others start the
    kernel later, so the total time (measured from first start to last end)
    is nontrivial. The previous program was ready to dispatch when the first
    kernel loaded, so the dispatcher is busy loading the next kernel
    throughout the kernel time and it should be counted towards that next
    kernel's dispatch time.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [x] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes

--- tt_metal/hw/firmware/src/ncrisc.cc
+++ tt_metal/hw/firmware/src/ncrisc.cc
@@ -130,15 +130,15 @@ int main(int argc, char *argv[]) {
 #endif
         uint32_t tt_l1_ptr* cb_l1_base =
             (uint32_t tt_l1_ptr*)(kernel_config_base + launch_msg->kernel_config.local_cb_offset);
-        uint32_t end_cb_index = launch_msg->kernel_config.max_local_cb_end_index;
-        setup_local_cb_read_write_interfaces(cb_l1_base, 0, end_cb_index, true, true, false);
+        uint32_t local_cb_mask = launch_msg->kernel_config.local_cb_mask;
+        setup_local_cb_read_write_interfaces<true, true, false>(cb_l1_base, 0, local_cb_mask);
 
 #if defined(ARCH_WORMHOLE)
         l1_to_ncrisc_iram_copy_wait();
 #endif
 
         cb_l1_base = (uint32_t tt_l1_ptr*)(kernel_config_base + launch_msg->kernel_config.remote_cb_offset);
-        end_cb_index = launch_msg->kernel_config.min_remote_cb_start_index;
+        uint32_t end_cb_index = launch_msg->kernel_config.min_remote_cb_start_index;
         // NOC argument is unused
         experimental::setup_remote_cb_interfaces<false>(cb_l1_base, end_cb_index, 0, 0, 0, 0);
         my_relative_x_ = my_logical_x_ - launch_msg->kernel_config.sub_device_origin_x;
