123b0e87b917c4883884028eef9009c562aa9c31
grechsteinerTT <grechsteiner@tenstorrent.com>
2025-07-09 19:45:51 +0000

    Host side fixes

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/device/all_gather_async_program_minimal_variants.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/device/all_gather_async_program_minimal_variants.cpp
@@ -7,6 +7,7 @@
 #include <tt-metalium/core_coord.hpp>
 #include <tt-metalium/buffer.hpp>
 #include <tt-metalium/fabric.hpp>
+#include <tt-metalium/hal.hpp>
 #include "ttnn/tensor/tensor_impl.hpp"
 #include "ttnn/operations/experimental/ccl/all_gather_async/device/all_gather_async_op.hpp"
 #include "ttnn/operations/experimental/ccl/llama_common.hpp"
@@ -149,11 +150,10 @@ tt::tt_metal::operation::ProgramWithCallbacks all_gather_async_minimal_interleav
     uint32_t l1_scratch_cb_page_size_bytes = op_config.get_page_size();
 
     // scatter-write currently only supports 2 distinct noc addresses, and is only supported for wormhole
-#ifdef ARCH_WORMHOLE
-    uint32_t max_target_noc_addresses_per_packet = 2;
-#else
     uint32_t max_target_noc_addresses_per_packet = 1;
-#endif
+    if (tt::tt_metal::hal::get_arch() == tt::ARCH::WORMHOLE_B0) {
+        uint32_t max_target_noc_addresses_per_packet = 2;
+    }
 
     // for bfloat8_b, tile_num_per_link=6, we would need to send 2 packages, but they can be of size 3 instead of 4
     uint32_t num_pages_per_packet = packet_size_bytes / l1_scratch_cb_page_size_bytes;
