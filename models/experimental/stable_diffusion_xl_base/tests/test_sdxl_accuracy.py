c550342df84e0676edee7d91f0755cb6e6d055c4
Janko Mitrović <jmitrovic@tenstorrent.com>
2025-07-09 10:51:30 +0000

    Replace prompt logic with sdxl_get_prompts function call

--- models/experimental/stable_diffusion_xl_base/tests/test_sdxl_accuracy.py
+++ models/experimental/stable_diffusion_xl_base/tests/test_sdxl_accuracy.py
@@ -45,27 +45,11 @@ def test_accuracy_sdxl(
 ):
     start_from, num_prompts = evaluation_range
 
-    assert (
-        0 <= start_from < 5000 and start_from + num_prompts <= 5000
-    ), "start_from must be between 0 and 4999, and start_from + num_prompts must not exceed 5000."
-
-    prompts = []
-
-    if not os.path.isfile(captions_path):
-        logger.info(f"File {captions_path} not found. Downloading...")
-        os.makedirs(os.path.dirname(captions_path), exist_ok=True)
-        urllib.request.urlretrieve(COCO_CAPTIONS_DOWNLOAD_PATH, captions_path)
-        logger.info("Download complete.")
-
-    with open(captions_path, "r") as tsv_file:
-        reader = csv.reader(tsv_file, delimiter="\t")
-        next(reader)
-        for index, row in enumerate(reader):
-            if index < start_from:
-                continue
-            if index >= start_from + num_prompts:
-                break
-            prompts.append(row[2])
+    prompts = sdxl_get_prompts(
+        captions_path,
+        start_from,
+        num_prompts,
+    )
 
     logger.info(f"Start inference from prompt index: {start_from} to {start_from + num_prompts}")
 
