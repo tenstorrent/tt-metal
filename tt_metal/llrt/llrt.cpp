caf424dd1969132f25dc69bf132c8642436667f0
John Bauman <jbauman@tenstorrent.com>
2025-07-10 17:02:12 -0400

    Fix TSAN error creating ethernet kernel (#24862)
    ### Problem description
    We get a TSAN error when doing EthernetKernel::read_binaries
    
    ### What's changed
    Add a callback to get_risc_binary to do some initialization before
    returning the kernel. That way the initialization will only be done once
    and won't cause a TSAN error.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes

--- tt_metal/llrt/llrt.cpp
+++ tt_metal/llrt/llrt.cpp
@@ -41,7 +41,8 @@ using std::uint16_t;
 using std::uint32_t;
 using std::uint64_t;
 
-const ll_api::memory& get_risc_binary(std::string_view path, ll_api::memory::Loading loading) {
+const ll_api::memory& get_risc_binary(
+    std::string_view path, ll_api::memory::Loading loading, std::function<void(ll_api::memory&)> update_callback) {
     static struct {
       std::unordered_map<std::string, std::unique_ptr<ll_api::memory const>> map;
       std::mutex mutex;
@@ -50,15 +51,19 @@ const ll_api::memory& get_risc_binary(std::string_view path, ll_api::memory::Loa
 
     std::unique_lock lock(cache.mutex);
     auto [slot, inserted] = cache.map.try_emplace(std::string(path));
-    ll_api::memory const* ptr = nullptr;
+    const ll_api::memory* ptr = nullptr;
     if (inserted) {
       // We're the first with PATH. Create and insert.
       lock.unlock();
-      ptr = new ll_api::memory(path, loading);
+      ll_api::memory* mutable_ptr = new ll_api::memory(path, loading);
+      if (update_callback) {
+          update_callback(*mutable_ptr);
+      }
 
       lock.lock();
       // maps have iterator stability, so SLOT is still valid.
-      slot->second = decltype(slot->second)(ptr);
+      slot->second = decltype(slot->second)(mutable_ptr);
+      ptr = mutable_ptr;
       // We can't wake just those waiting on this slot, so wake them
       // all. Should be a rare event anyway.
       cache.cvar.notify_all();
