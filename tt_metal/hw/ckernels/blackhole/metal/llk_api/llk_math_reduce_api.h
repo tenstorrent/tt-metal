866b67de50c50405cd1c304f43d9d4c82e5954aa
Radomir Djogo <159184120+rdjogoTT@users.noreply.github.com>
2025-07-10 16:58:38 -0400

    Rd/reduce w fp32 (#24665)
    ### Ticket
    https://github.com/tenstorrent/tt-llk/issues/122
    https://github.com/tenstorrent/tt-metal/issues/23850
    
    ### Problem description
    Reduce W implementation performs transpose of Dest register in Tf32
    losing a significant amount of precision in the accumulated result.
    
    ### What's changed
    Performing a 32bit transpose of Dest can be done by performing it on
    hi16 and lo16 bits separately, and can be used to maintain the fp32
    accumulated result precision in the reduce OP.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16202867978)
    CI has same api failure as main
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16202871834)
    CI with demo tests passes (if applicable)

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_math_reduce_api.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_math_reduce_api.h
@@ -15,9 +15,11 @@ template <
     ReduceDim dim,
     bool is_fp32_dest_acc_en,
     int num_fidelity_phases = 0,
-    bool is_int_fpu_en = false>
+    bool is_int_fpu_en = false,
+    bool fp32_transpose = false>
 inline void llk_math_reduce(const uint dst_index, const uint num_faces = 4) {
-    _llk_math_reduce_<type, dim, is_fp32_dest_acc_en, num_fidelity_phases, is_int_fpu_en>(dst_index, false, num_faces);
+    _llk_math_reduce_<type, dim, is_fp32_dest_acc_en, num_fidelity_phases, is_int_fpu_en, fp32_transpose>(
+        dst_index, false, num_faces);
 }
 
 template <
@@ -25,12 +27,14 @@ template <
     ReduceDim dim,
     bool is_fp32_dest_acc_en,
     int num_fidelity_phases = 0,
-    bool is_int_fpu_en = false>
+    bool is_int_fpu_en = false,
+    bool fp32_transpose = false>
 inline void llk_math_reduce(const std::uint32_t operandA, const std::uint32_t operandB, const std::uint32_t dst_index) {
     const std::uint32_t operand_id = get_operand_id(operandA);
     const std::uint32_t num_faces = get_operand_num_faces(operand_id);
 
-    _llk_math_reduce_<type, dim, is_fp32_dest_acc_en, num_fidelity_phases, is_int_fpu_en>(dst_index, false, num_faces);
+    _llk_math_reduce_<type, dim, is_fp32_dest_acc_en, num_fidelity_phases, is_int_fpu_en, fp32_transpose>(
+        dst_index, false, num_faces);
 }
 
 template <PoolType type, ReduceDim dim, int num_fidelity_phases = 0>
