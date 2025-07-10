a33d991228f9eada51e8887d0a42770a1a723b69
Aliaksei Sala <aliaksei_sala@epam.com>
2025-07-10 14:20:23 -0700

    Added more unit tests

--- tests/ttnn/unit_tests/test_to_dtype.py
+++ tests/ttnn/unit_tests/test_to_dtype.py
@@ -71,26 +71,51 @@ def test_to_float_dtype(height, width, from_dtype, to_dtype):
     assert_with_pcc(torch_input_tensor, output_tensor, 0.960 if to_dtype == ttnn.bfloat4_b else 0.9999)
 
 
-@pytest.mark.parametrize("height", [32])
-@pytest.mark.parametrize("width", [32])
-@pytest.mark.parametrize("to_dtype", [ttnn.uint8])
-@pytest.mark.parametrize("from_dtype", [ttnn.bfloat16])
-def test_to_float_dtype_local(height, width, from_dtype, to_dtype):
-    # torch_input_tensor = torch.rand((height, width), dtype=tt_dtype_to_torch_dtype[from_dtype])
+@pytest.mark.parametrize("height", [36])
+@pytest.mark.parametrize("width", [36])
+@pytest.mark.parametrize("to_dtype", ALL_TYPES)
+@pytest.mark.parametrize("from_dtype", ALL_TYPES)
+def test_to_dtype_unaligned_shape(height, width, from_dtype, to_dtype):
+    if (
+        from_dtype == ttnn.bfloat4_b
+        or from_dtype == ttnn.bfloat8_b
+        or to_dtype == ttnn.bfloat4_b
+        or to_dtype == ttnn.bfloat8_b
+    ):
+        pytest.skip("bfloat4_b and bfloat8_b require align shape divisible by tile")
+
     torch_input_tensor = torch.randint(0, 10, (height, width), dtype=tt_dtype_to_torch_dtype[from_dtype])
 
     input_tensor = ttnn.from_torch(torch_input_tensor)
     assert input_tensor.layout == ttnn.ROW_MAJOR_LAYOUT
 
     output_tensor = ttnn.to_dtype(input_tensor, to_dtype)
-    print(output_tensor)
 
     assert output_tensor.dtype == to_dtype
     assert tuple(output_tensor.shape) == (height, width)
-    if to_dtype == ttnn.bfloat8_b or to_dtype == ttnn.bfloat4_b:
-        assert output_tensor.layout == ttnn.TILE_LAYOUT
-    else:
-        assert output_tensor.layout == ttnn.ROW_MAJOR_LAYOUT
+    assert output_tensor.layout == ttnn.ROW_MAJOR_LAYOUT
 
+    # How to estimate pcc for conversion loss? No guarantee that we almost hit 0.960 target which could break CI
+    output_tensor = ttnn.to_torch(output_tensor, dtype=torch_input_tensor.dtype)
+    assert_with_pcc(torch_input_tensor, output_tensor, 0.960 if to_dtype == ttnn.bfloat4_b else 0.9999)
+
+
+@pytest.mark.parametrize("height", [32])
+@pytest.mark.parametrize("width", [32])
+@pytest.mark.parametrize("to_dtype", ALL_TYPES)
+@pytest.mark.parametrize("from_dtype", ALL_TYPES)
+def test_to_dtype_with_tile_layout(height, width, from_dtype, to_dtype):
+    torch_input_tensor = torch.randint(0, 10, (height, width), dtype=tt_dtype_to_torch_dtype[from_dtype])
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT)
+    assert input_tensor.layout == ttnn.TILE_LAYOUT
+
+    output_tensor = ttnn.to_dtype(input_tensor, to_dtype)
+
+    assert output_tensor.dtype == to_dtype
+    assert tuple(output_tensor.shape) == (height, width)
+    assert output_tensor.layout == ttnn.TILE_LAYOUT
+
+    # How to estimate pcc for conversion loss? No guarantee that we almost hit 0.960 target which could break CI
     output_tensor = ttnn.to_torch(output_tensor, dtype=torch_input_tensor.dtype)
     assert_with_pcc(torch_input_tensor, output_tensor, 0.960 if to_dtype == ttnn.bfloat4_b else 0.9999)
