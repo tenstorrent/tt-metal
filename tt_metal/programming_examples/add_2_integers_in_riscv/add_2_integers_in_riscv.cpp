1d1eb781cf23f7d836c9e67d9a86467ab2e654c2
bcheemasabirTT <bcheemasabir@tenstorrent.com>
2025-07-11 01:43:41 +0000

    migrated add_2_integers

--- tt_metal/programming_examples/add_2_integers_in_riscv/add_2_integers_in_riscv.cpp
+++ tt_metal/programming_examples/add_2_integers_in_riscv/add_2_integers_in_riscv.cpp
@@ -7,6 +7,7 @@
 #include <tt-metalium/host_api.hpp>
 #include <tt-metalium/device.hpp>
 #include "tt-metalium/buffer.hpp"
+#include <tt-metalium/distributed.hpp>
 
 using namespace tt;
 using namespace tt::tt_metal;
@@ -24,14 +25,16 @@ int main() {
     }
 
     // Initialize a device
-    IDevice* device = CreateDevice(0);
+    std::shared_ptr<distributed::MeshDevice> mesh_device = distributed::MeshDevice::create_unit_mesh(0);
 
     // Create a command queue and program
     // Command queue
     //    * Submit work (execute programs and read/write buffers) to the device
     // Program
     //    * Contains kernels that perform computations or data movement
-    CommandQueue& cq = device->command_queue();
+    distributed::MeshCommandQueue& cq = mesh_device->mesh_command_queue();
+    distributed::MeshWorkload workload;
+    distributed::MeshCoordinateRange device_range = distributed::MeshCoordinateRange(mesh_device->shape());
     Program program = CreateProgram();
     // We will only be using one Tensix core for this particular example. As Tenstorrent processors are a 2D grid of
     // cores we can specify the core coordinates as (0, 0).
@@ -44,18 +47,23 @@ int main() {
     // access to DRAM, an extra buffer on L1 (SRAM) is required to read/write data from/to DRAM.
     // page_size is the size of each page in the buffer. In most applications this will be set to the size of a tile.
     // But for this example we set it to the size of a single integer as that is what we are adding.
-    InterleavedBufferConfig dram_config{
-        .device = device, .size = buffer_size, .page_size = buffer_size, .buffer_type = BufferType::DRAM};
-    InterleavedBufferConfig l1_config{
-        .device = device, .size = buffer_size, .page_size = buffer_size, .buffer_type = BufferType::L1};
+    distributed::DeviceLocalBufferConfig dram_config{
+        .page_size = buffer_size,
+        .buffer_type = BufferType::DRAM};
+    distributed::DeviceLocalBufferConfig l1_config{
+        .page_size = buffer_size,
+        .buffer_type = BufferType::L1};
+    distributed::ReplicatedBufferConfig buffer_config{
+        .size = buffer_size,
+    };
 
     // Create the DRAM and SRAM buffers
-    auto src0_dram_buffer = CreateBuffer(dram_config);
-    auto src1_dram_buffer = CreateBuffer(dram_config);
-    auto dst_dram_buffer = CreateBuffer(dram_config);
-    auto src0_l1_buffer = CreateBuffer(l1_config);
-    auto src1_l1_buffer = CreateBuffer(l1_config);
-    auto dst_l1_buffer = CreateBuffer(l1_config);
+    auto src0_dram_buffer = distributed::MeshBuffer::create(buffer_config, dram_config, mesh_device.get());
+    auto src1_dram_buffer = distributed::MeshBuffer::create(buffer_config, dram_config, mesh_device.get());
+    auto dst_dram_buffer = distributed::MeshBuffer::create(buffer_config, dram_config, mesh_device.get());
+    auto src0_l1_buffer = distributed::MeshBuffer::create(buffer_config, l1_config, mesh_device.get());
+    auto src1_l1_buffer = distributed::MeshBuffer::create(buffer_config, l1_config, mesh_device.get());
+    auto dst_l1_buffer = distributed::MeshBuffer::create(buffer_config, l1_config, mesh_device.get());
 
     // Create source data and write to DRAM
     std::vector<uint32_t> src0_vec = {14};
@@ -65,8 +73,8 @@ int main() {
     // write operation should block until the data is written to the device. In this case, we set it to false for
     // asynchronous writes, allowing the program to continue executing while the data is being written. This is
     // recommended for most writes to device in applications to improve performance.
-    EnqueueWriteBuffer(cq, src0_dram_buffer, src0_vec, /*blocking=*/false);
-    EnqueueWriteBuffer(cq, src1_dram_buffer, src1_vec, /*blocking=*/false);
+    EnqueueWriteMeshBuffer(cq, src0_dram_buffer, src0_vec, /*blocking=*/false);
+    EnqueueWriteMeshBuffer(cq, src1_dram_buffer, src1_vec, /*blocking=*/false);
 
     // Create the kernel (code that runs on the Tensix core) that will perform the addition of the 2 integers.
     // The Data Movement cores are the only cores that can read/write data from/to DRAM. Thus we use them for
@@ -97,7 +105,8 @@ int main() {
     // Enqueue the kernel for execution on the device. Setting blocking to false allows the program to continue
     // executing while the kernel is being executed on the device (which is for demonstration purposes here as
     // immidiately after we read the result).
-    EnqueueProgram(cq, program, /*blocking=*/false);
+    distributed::AddProgramToMeshWorkload(workload, std::move(program), device_range);
+    distributed::EnqueueMeshWorkload(cq, workload, /*blocking=*/false);
 
     // Read in result into a host vector. This time we set blocking to true as we can only compare the result after the
     // data has been read from the device.
@@ -105,18 +114,18 @@ int main() {
     // In no conditions a read on the command queue will be executed before kernel execution in front of it has
     // completed.
     std::vector<uint32_t> result_vec;
-    EnqueueReadBuffer(cq, dst_dram_buffer, result_vec, /*blocking=*/true);
+    distributed::EnqueueReadMeshBuffer(cq, result_vec, dst_dram_buffer, /*blocking=*/true);
     if (result_vec.size() != 1) {
         std::cout << "Error: Expected result vector size of 1, got " << result_vec.size() << std::endl;
-        CloseDevice(device);
+        mesh_device->close();
         return -1;
     }
     if (result_vec[0] != 21) {
         std::cout << "Error: Expected result of 21, got " << result_vec[0] << std::endl;
-        CloseDevice(device);
+        mesh_device->close();
         return -1;
     }
 
     std::cout << "Success: Result is " << result_vec[0] << std::endl;
-    CloseDevice(device);
+    mesh_device->close();
 }
