48e99416b1949a4de05930d064ea312f6fb6dd1f
Stanislav Minakov <sminakov@tenstorrent.com>
2025-07-10 19:28:23 -0700

    Cleanup

--- tests/ttnn/unit_tests/gtests/tensor/test_tensor_nd_sharding.cpp
+++ tests/ttnn/unit_tests/gtests/tensor/test_tensor_nd_sharding.cpp
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 // SPDX-License-Identifier: Apache-2.0
 
-#include <tt-logger/tt-logger.hpp>
-#include "core_coord.hpp"
 #include "gtest/gtest.h"
 
 #include "ttnn/tensor/tensor.hpp"
