81b9818c2c8f14a794316ba743a2d57b52f26b24
vguduruTT <venkatesh.guduru@multicorewareinc.com>
2025-07-14 08:11:25 +0000

    #25002: restructure trace and add DP support(n300) for mobv2

--- models/demos/mobilenetv2/README.md
+++ models/demos/mobilenetv2/README.md
@@ -1,7 +1,10 @@
 # Mobilenetv2 Model
 
-## Platforms:
-    WH N150/N300
+### Platforms:
+
+Wormhole N150, N300
+
+
 **Note:** On N300 ,Make sure to use `WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml` with the pytest.
 
 Or, make sure to set the following environment variable in the terminal:
@@ -14,27 +17,46 @@ To obtain the perf reports through profiler, please build with following command
 ./build_metal.sh -p
 ```
 
-## Introduction
+### Introduction
 The MobileNetV2 model is a convolutional neural network (CNN) architecture designed for efficient mobile and embedded vision applications. It was introduced in the paper ["MobileNetV2: Inverted Residuals and Linear Bottlenecks"](https://arxiv.org/abs/1801.04381). </br>
 The MobileNetV2 model has been pre-trained on the ImageNet dataset and can be used for various tasks such as image classification, object detection, and semantic segmentation. It has achieved state-of-the-art performance on several benchmarks 1 for mobile and embedded vision applications.
 
-## Details
-- The entry point to mobilenetv2 model is MobileNetV2 in `models/demos/mobilenetv2/tt/ttnn_monilenetv2.py`.
+### Details
+- The entry point to mobilenetv2 model is MobileNetV2 in `models/demos/mobilenetv2/tt/ttnn_mobilenetv2.py`.
 - Supported Input Resolution - (224,224) (Height,Width)
-- Batch Size :8
+- Batch Size :10
 - Dataset used for evaluation - **imagenet-1k**
 
-### Model performant running with Trace+2CQ
+### How to Run:
 
-#### For 224x224:
+Use the following command to run the model :
 
-- end-2-end perf is 2808 FPS
-
-```bash
-pytest models/demos/mobilenetv2/tests/test_e2e_performant.py
+```
+pytest --disable-warnings tests/ttnn/integration_tests/mobilenetv2/test_mobilenetv2.py::test_mobilenetv2
 ```
 
-### Demo on ImageNet:
+### Performant Model with Trace+2CQ
+
+#### Single Device (BS=10):
+
+- end-2-end perf is `2470` FPS
+
+Use the following command to run the performant Model with Trace+2CQs:
+
+```
+pytest --disable-warnings models/demos/mobilenetv2/tests/test_e2e_performant.py::test_mobilenetv2_e2e
+```
+#### Multi Device (DP=2, N300):
+
+- end-2-end perf is `4580` FPS
+
+Use the following command to run the performant Model with Trace+2CQs:
+
+```
+pytest --disable-warnings models/demos/mobilenetv2/tests/test_e2e_performant.py::test_mobilenetv2_e2e_dp
+```
+
+### Performant Demo with Trace+2CQ
 
 You will need a huggingFace account to download ImageNet dataset as part of this test. You may create a token from:
 ```bash
@@ -51,17 +73,40 @@ If running the test from terminal you may log into HuggingFace by running:
 huggingface-cli login
 ```
 
-Use the following command to run the Demo on ImageNet dataset:
-```bash
-pytest  models/demos/mobilenetv2/demo/demo.py::test_mobilenetv2_imagenet_demo
+#### Single Device (BS=10):
+
+Use the following command to run the performant Demo with Trace+2CQs:
+
+```
+pytest --disable-warnings models/demos/mobilenetv2/demo/demo.py::test_mobilenetv2_imagenet_demo
 ```
 
-### Performant evaluation with Trace+2CQ
+#### Multi Device (DP=2, N300):
+
+Use the following command to run the performant Demo with Trace+2CQs:
+
+```
+pytest --disable-warnings models/demos/mobilenetv2/demo/demo.py::test_mobilenetv2_imagenet_demo_dp
+```
+
+### Performant Data evaluation with Trace+2CQ
+
+#### Single Device (BS=10):
+
 Use the following command to run the performant evaluation with Trace+2CQs:
 
 ```
-pytest models/experimental/classification_eval/classification_eval.py::test_mobilenetv2_image_classification_eval[8-224-tt_model-device_params0]
+pytest --disable-warnings models/experimental/classification_eval/classification_eval.py::test_mobilenetv2_image_classification_eval
 ```
+
+#### Multi Device (DP=2, N300):
+
+Use the following command to run the performant evaluation with Trace+2CQs:
+
+```
+pytest --disable-warnings models/experimental/classification_eval/classification_eval.py::test_mobilenetv2_image_classification_eval_dp
+```
+
 Note: The model is evaluated with 512 samples.
 
 #### Note:
--- models/demos/mobilenetv2/demo/demo.py
+++ models/demos/mobilenetv2/demo/demo.py
@@ -9,33 +9,27 @@ from tqdm import tqdm
 from transformers import AutoImageProcessor
 
 import ttnn
-from models.demos.mobilenetv2.tests.mobilenetv2_e2e_performant import MobileNetV2Trace2CQ
+from models.demos.mobilenetv2.runner.performant_runner import MobileNetV2Trace2CQ
+from models.demos.mobilenetv2.tests.mobilenetv2_common import MOBILENETV2_BATCH_SIZE, MOBILENETV2_L1_SMALL_SIZE
+from models.demos.mobilenetv2.tt.model_preprocessing import get_mesh_mappers
 from models.demos.ttnn_resnet.tests.demo_utils import get_batch, get_data_loader
 from models.utility_functions import profiler, run_for_wormhole_b0
 
 NUM_VALIDATION_IMAGES_IMAGENET = 49920
 
 
-@run_for_wormhole_b0()
-@pytest.mark.parametrize(
-    "device_params", [{"l1_small_size": 24576, "trace_region_size": 1605632, "num_command_queues": 2}], indirect=True
-)
-@pytest.mark.parametrize(
-    "batch_size_per_device, iterations, act_dtype, weight_dtype",
-    ((8, 100, ttnn.bfloat8_b, ttnn.bfloat8_b),),
-)
-def test_mobilenetv2_imagenet_demo(
+def run_mobilenetv2_imagenet_demo(
     device,
-    batch_size_per_device,
+    batch_size,
     iterations,
     imagenet_label_dict,
     act_dtype,
     weight_dtype,
-    model_location_generator,
+    model_location_generator=None,
     entire_imagenet_dataset=False,
     expected_accuracy=0.68,
 ):
-    batch_size = batch_size_per_device * device.get_num_devices()
+    inputs_mesh_mapper, _, output_mesh_composer = get_mesh_mappers(device)
     iterations = iterations // device.get_num_devices()
 
     if entire_imagenet_dataset:
@@ -48,7 +42,7 @@ def test_mobilenetv2_imagenet_demo(
         profiler.start(f"compile")
         mobilenetv2_trace_2cq.initialize_mobilenetv2_trace_2cqs_inference(
             device,
-            batch_size_per_device,
+            batch_size,
             act_dtype,
             weight_dtype,
         )
@@ -57,7 +51,9 @@ def test_mobilenetv2_imagenet_demo(
         image_processor = AutoImageProcessor.from_pretrained(model_version)
         logger.info("ImageNet-1k validation Dataset")
         input_loc = str(model_location_generator("ImageNet_data"))
-        data_loader = get_data_loader(input_loc, batch_size, iterations, entire_imagenet_dataset)
+        data_loader = get_data_loader(
+            input_loc, batch_size * (device.get_num_devices()), iterations, entire_imagenet_dataset
+        )
 
         input_tensors_all = []
         input_labels_all = []
@@ -75,14 +71,25 @@ def test_mobilenetv2_imagenet_demo(
             torch_input_tensor = input_tensors_all[iter]
             labels = input_labels_all[iter]
             profiler.start(f"run")
-
             n, c, h, w = torch_input_tensor.shape
             torch_input_tensor = torch_input_tensor.permute(0, 2, 3, 1)
-            torch_input_tensor = torch_input_tensor.reshape(1, 1, h * w * n, c)
-            tt_inputs_host = ttnn.from_torch(torch_input_tensor, dtype=ttnn.bfloat16, layout=ttnn.ROW_MAJOR_LAYOUT)
-            tt_inputs_host = ttnn.pad(tt_inputs_host, [1, 1, n * h * w, 16], [0, 0, 0, 0], 0)
-            output = mobilenetv2_trace_2cq.execute_mobilenetv2_trace_2cqs_inference(tt_inputs_host)
-            output = ttnn.to_torch(output)
+            ttnn_input_tensor = ttnn.from_torch(
+                torch_input_tensor, dtype=ttnn.bfloat16, layout=ttnn.ROW_MAJOR_LAYOUT, mesh_mapper=inputs_mesh_mapper
+            )
+            ttnn_input_tensor = ttnn.reshape(
+                ttnn_input_tensor,
+                (
+                    1,
+                    1,
+                    ttnn_input_tensor.shape[0] * ttnn_input_tensor.shape[1] * ttnn_input_tensor.shape[2],
+                    ttnn_input_tensor.shape[3],
+                ),
+            )
+            ttnn_input_tensor = ttnn.pad(
+                ttnn_input_tensor, [1, 1, (n // (device.get_num_devices())) * h * w, 16], [0, 0, 0, 0], 0
+            )
+            output = mobilenetv2_trace_2cq.execute_mobilenetv2_trace_2cqs_inference(ttnn_input_tensor)
+            output = ttnn.to_torch(output, mesh_composer=output_mesh_composer)
             prediction = output.argmax(dim=-1)
 
             profiler.end(f"run")
@@ -97,7 +104,9 @@ def test_mobilenetv2_imagenet_demo(
         mobilenetv2_trace_2cq.release_mobilenetv2_trace_2cqs_inference()
         accuracy = correct / (batch_size * iterations)
         logger.info(f"=============")
-        logger.info(f"Accuracy for  batch size: {batch_size} over {iterations} iterations is: {accuracy}")
+        logger.info(
+            f"Accuracy for total batch size: {batch_size* device.get_num_devices()} over {iterations} iterations is: {accuracy}"
+        )
         if entire_imagenet_dataset:
             assert (
                 accuracy < expected_accuracy
@@ -107,3 +116,59 @@ def test_mobilenetv2_imagenet_demo(
         inference_time_avg = total_inference_time / (iterations)
 
         compile_time = first_iter_time - 2 * inference_time_avg
+
+
+@run_for_wormhole_b0()
+@pytest.mark.parametrize(
+    "device_params",
+    [{"l1_small_size": MOBILENETV2_L1_SMALL_SIZE, "trace_region_size": 6434816, "num_command_queues": 2}],
+    indirect=True,
+)
+@pytest.mark.parametrize(
+    "batch_size",
+    ((MOBILENETV2_BATCH_SIZE),),
+)
+@pytest.mark.parametrize(
+    "iterations, act_dtype, weight_dtype",
+    ((100, ttnn.bfloat8_b, ttnn.bfloat8_b),),
+)
+def test_mobilenetv2_imagenet_demo(
+    device, batch_size, iterations, act_dtype, weight_dtype, imagenet_label_dict, model_location_generator
+):
+    run_mobilenetv2_imagenet_demo(
+        device, batch_size, iterations, imagenet_label_dict, act_dtype, weight_dtype, model_location_generator
+    )
+
+
+@run_for_wormhole_b0()
+@pytest.mark.parametrize(
+    "device_params",
+    [{"l1_small_size": MOBILENETV2_L1_SMALL_SIZE, "trace_region_size": 6434816, "num_command_queues": 2}],
+    indirect=True,
+)
+@pytest.mark.parametrize(
+    "batch_size_per_device",
+    ((MOBILENETV2_BATCH_SIZE),),
+)
+@pytest.mark.parametrize(
+    "iterations, act_dtype, weight_dtype",
+    ((100, ttnn.bfloat8_b, ttnn.bfloat8_b),),
+)
+def test_mobilenetv2_imagenet_demo_dp(
+    mesh_device,
+    batch_size_per_device,
+    iterations,
+    act_dtype,
+    weight_dtype,
+    imagenet_label_dict,
+    model_location_generator,
+):
+    run_mobilenetv2_imagenet_demo(
+        mesh_device,
+        batch_size_per_device,
+        iterations,
+        imagenet_label_dict,
+        act_dtype,
+        weight_dtype,
+        model_location_generator,
+    )
