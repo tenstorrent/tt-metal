eeef5ebec79f1529ea36c70f0b8126b300e5bc43
Diego Gomez <dgomez@tenstorrent.com>
2025-07-15 12:05:00 -0400

    added cache again for AsyncExecutionWorksCQ0CQ1 (#25089)
    ### Ticket
    [Link to Github Issue
    ](https://github.com/tenstorrent/tt-metal/issues/24235)
    
    ### Problem description
    When we implemented the cache by default, this tests was failing
    
    ### What's changed
    Multiple fixes have made the device cache more stable
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
    CI passes (if applicable)
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [x] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- tests/ttnn/unit_tests/gtests/multi_thread/test_ccl_multi_cq_multi_device.cpp
+++ tests/ttnn/unit_tests/gtests/multi_thread/test_ccl_multi_cq_multi_device.cpp
@@ -225,11 +225,6 @@ TEST_F(T3000MultiCQFabricMeshDeviceFixture, AsyncExecutionWorksCQ0CQ1) {
         view.get_device(MeshCoordinate(0, 2)),
         view.get_device(MeshCoordinate(0, 3))};
 
-    // https://github.com/tenstorrent/tt-metal/issues/24235
-    for (auto device : devices) {
-        device->disable_and_clear_program_cache();
-    }
-
     const size_t num_devices = devices.size();
     TT_FATAL(
         test_expected_num_devices == num_devices,
