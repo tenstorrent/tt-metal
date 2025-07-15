8b286d923a64793b0a3a49699ce7a6ea13af7fc2
Tomer Levin <tlevin@tenstorrent.com>
2025-07-15 11:14:35 -0400

    sdpa: Adding changes for MLA prefill (#24855)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/23022
    
    ### Problem description
    This PR is similar to
    https://github.com/tenstorrent/tt-metal/pull/24577, for MLA prefill
    
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16281563737)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16281574669)
    CI with demo tests passes (if applicable)
    - [x] [New/Existing tests provide coverage for
    changes](https://github.com/tenstorrent/tt-metal/actions/runs/16281563737/job/46019961068#step:5:5543)
    
    ---------
    
    Signed-off-by: Tomer Levin <tlevin@tenstorrent.com>

--- ttnn/cpp/ttnn/operations/transformer/sdpa/device/sdpa_program_factory.hpp
+++ ttnn/cpp/ttnn/operations/transformer/sdpa/device/sdpa_program_factory.hpp
@@ -23,6 +23,8 @@ tt::tt_metal::operation::ProgramWithCallbacks sdpa_multi_core(
     std::size_t q_chunk_size,
     std::size_t k_chunk_size,
     DeviceComputeKernelConfig compute_kernel_config,
-    std::optional<SDPAProgramConfig> program_config);
+    std::optional<SDPAProgramConfig> program_config,
+    bool use_mla,
+    uint32_t head_dim_v = 0);
 
 }  // namespace ttnn::operations::transformer::detail
