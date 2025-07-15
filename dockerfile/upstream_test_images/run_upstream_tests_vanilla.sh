30550ec6fc927a4bb760977f359d72301f38ec6b
Basim Cheema-Sabir <bcheemasabir@tenstorrent.com>
2025-07-15 12:38:29 -0700

    Migrate EnqueueProgram tests to use TT-Mesh APIs (#24802)
    ### Ticket
    [Link to Github Issue
    ](https://github.com/tenstorrent/tt-metal/issues/22835)
    ### Problem description
    Need to migrate more tests to use EnqueueMeshWorkload instead of
    EnqueueProgram. This is required to deprecate legacy CommandQueue APIs.
    
    ### What's changed
    - Added `UnitMeshCQSingleCardFixture`,
    `UnitMeshCQSingleCardProgramFixture`, `UnitMeshCQMultiDeviceFixture`,
    `CQMultiDeviceOnFabricFixture` to `command_queue_fixture.hpp`
    - Added `UnitMeshMultiCQSingleDeviceFixture`,
    `UnitMeshMultiCQSingleDeviceProgramFixture`,
    `UnitMeshMultiCQMultiDeviceFixture`, `MultiCQMultiDeviceOnFabricFixture`
    to `multi_command_queue_fixture.hpp`
    - All above test fixtures use `MeshDevices` and parallel their legacy
    counterparts.
    - Removed legacy `TestLogicalCoordinatesEth` tests as they are not
    compatible with `MeshDevices`
    - Fixtures in `random_program_fixture.hpp` have not been migrated yet as
    they will be in an upcoming PR
    - **Note:** Additional Fixtures will be used to replace legacy ones once
    migration is complete
    ### Checklist
    - [x] [All Post-Commit
    Tests](https://github.com/tenstorrent/tt-metal/actions/runs/16288607427)
    - [x] [T3K Unit
    Tests](https://github.com/tenstorrent/tt-metal/actions/runs/16283628321)
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: asaigal <asaigal@tenstorrent.com>

--- dockerfile/upstream_test_images/run_upstream_tests_vanilla.sh
+++ dockerfile/upstream_test_images/run_upstream_tests_vanilla.sh
@@ -13,9 +13,12 @@ test_suite_bh_single_pcie_metal_unit_tests() {
     ARCH_NAME=blackhole TT_METAL_SLOW_DISPATCH_MODE=1 ./tests/scripts/run_cpp_fd2_tests.sh
     # I wonder why we can't put these in the validation suite?
     ./build/test/tt_metal/unit_tests_dispatch --gtest_filter=CommandQueueSingleCardProgramFixture.*
+    ./build/test/tt_metal/unit_tests_dispatch --gtest_filter=UnitMeshCQSingleCardProgramFixture.*
     ./build/test/tt_metal/unit_tests_dispatch --gtest_filter=CommandQueueProgramFixture.*
+    ./build/test/tt_metal/unit_tests_dispatch --gtest_filter=UnitMeshCQProgramFixture.*
     ./build/test/tt_metal/unit_tests_dispatch --gtest_filter=RandomProgramFixture.*
     ./build/test/tt_metal/unit_tests_dispatch --gtest_filter=CommandQueueSingleCardBufferFixture.* # Tests EnqueueRead/EnqueueWrite Buffer from DRAM/L1
+
     TT_METAL_SLOW_DISPATCH_MODE=1 ./build/test/tt_metal/unit_tests_api --gtest_filter=*SimpleDram*:*SimpleL1* # Executable is dependent on arch (provided through GitHub CI workflow scripts)
 }
 
@@ -81,7 +84,9 @@ test_suite_wh_6u_metal_unit_tests() {
     echo "[upstream-tests] running WH 6U upstream metalium unit tests. Note that skips should be treated as failures"
     ./build/test/tt_metal/tt_fabric/test_system_health
     TT_METAL_SKIP_ETH_CORES_WITH_RETRAIN=1 ./build/test/tt_metal/unit_tests_dispatch --gtest_filter="CommandQueueSingleCardFixture.*"
+    TT_METAL_SKIP_ETH_CORES_WITH_RETRAIN=1 ./build/test/tt_metal/unit_tests_dispatch --gtest_filter="UntiMeshCQSingleCardFixture.*"
     TT_METAL_SKIP_ETH_CORES_WITH_RETRAIN=1 ./build/test/tt_metal/unit_tests_dispatch --gtest_filter="CommandQueueSingleCardProgramFixture.*"
+    TT_METAL_SKIP_ETH_CORES_WITH_RETRAIN=1 ./build/test/tt_metal/unit_tests_dispatch --gtest_filter="UnitMeshCQSingleCardProgramFixture.*"
     TT_METAL_SKIP_ETH_CORES_WITH_RETRAIN=1 ./build/test/tt_metal/unit_tests_dispatch --gtest_filter="CommandQueueSingleCardBufferFixture.ShardedBufferLarge*ReadWrites"
     TT_METAL_SLOW_DISPATCH_MODE=1 ./build/test/tt_metal/tt_fabric/fabric_unit_tests --gtest_filter="Fabric2D*Fixture.*"
 }
--- tests/tt_metal/tt_metal/common/command_queue_fixture.hpp
+++ tests/tt_metal/tt_metal/common/command_queue_fixture.hpp
@@ -24,7 +24,7 @@
 #include "llrt.hpp"
 
 namespace tt::tt_metal {
-
+// #22835: These Fixtures will be removed once tests are fully migrated, and replaced by UnitMeshCQFixtures
 class CommandQueueFixture : public DispatchFixture {
 protected:
     tt::tt_metal::IDevice* device_;
@@ -80,7 +80,7 @@ protected:
     void CreateDevice(const size_t trace_region_size) { this->create_device(trace_region_size); }
 };
 
-class UnitMeshCommandQueueFixture : public DispatchFixture {
+class UnitMeshCQFixture : public DispatchFixture {
 protected:
     void SetUp() override {
         if (!this->validate_dispatch_mode()) {
@@ -131,6 +131,10 @@ protected:
     std::vector<std::shared_ptr<distributed::MeshDevice>> devices_;
 };
 
+class UnitMeshCQProgramFixture : public UnitMeshCQFixture {};
+
+// #22835: These Fixtures will be removed once tests are fully migrated, and replaced by UnitMeshCQSingleCardFixture and
+// UnitMeshCQSingleCardProgramFixture
 class CommandQueueSingleCardFixture : virtual public DispatchFixture {
 protected:
     void SetUp() override {
@@ -198,6 +202,62 @@ protected:
     std::map<chip_id_t, tt::tt_metal::IDevice*> reserved_devices_;
 };
 
+class UnitMeshCQSingleCardFixture : virtual public DispatchFixture {
+protected:
+    static void SetUpTestSuite() {}
+    static void TearDownTestSuite() {}
+
+    void SetUp() override {
+        if (!this->validate_dispatch_mode()) {
+            GTEST_SKIP();
+        }
+        this->arch_ = tt::get_arch_from_string(tt::test_utils::get_umd_arch_name());
+        this->create_devices();
+    }
+
+    void TearDown() override {
+        for (auto& device : devices_) {
+            device.reset();
+        }
+    }
+
+    bool validate_dispatch_mode() {
+        this->slow_dispatch_ = false;
+        auto slow_dispatch = getenv("TT_METAL_SLOW_DISPATCH_MODE");
+        if (slow_dispatch) {
+            log_info(tt::LogTest, "This suite can only be run with fast dispatch or TT_METAL_SLOW_DISPATCH_MODE unset");
+            this->slow_dispatch_ = false;
+            return false;
+        }
+        return true;
+    }
+
+    void create_devices(std::size_t trace_region_size = DEFAULT_TRACE_REGION_SIZE) {
+        const auto& dispatch_core_config =
+            tt::tt_metal::MetalContext::instance().rtoptions().get_dispatch_core_config();
+        const chip_id_t mmio_device_id = *tt::tt_metal::MetalContext::instance().get_cluster().mmio_chip_ids().begin();
+        std::vector<chip_id_t> chip_ids;
+        auto enable_remote_chip = getenv("TT_METAL_ENABLE_REMOTE_CHIP");
+        if (enable_remote_chip or
+            tt::tt_metal::MetalContext::instance().get_cluster().get_board_type(0) == BoardType::UBB) {
+            for (chip_id_t id : tt::tt_metal::MetalContext::instance().get_cluster().user_exposed_chip_ids()) {
+                chip_ids.push_back(id);
+            }
+        } else {
+            chip_ids.push_back(mmio_device_id);
+        }
+        auto reserved_devices = distributed::MeshDevice::create_unit_meshes(
+            chip_ids, DEFAULT_L1_SMALL_SIZE, trace_region_size, 1, dispatch_core_config);
+        for (const auto& [id, device] : reserved_devices) {
+            this->devices_.push_back(device);
+        }
+    }
+
+    std::vector<std::shared_ptr<distributed::MeshDevice>> devices_;
+};
+
+class UnitMeshCQSingleCardProgramFixture : virtual public UnitMeshCQSingleCardFixture {};
+
 class CommandQueueSingleCardBufferFixture : public CommandQueueSingleCardFixture {};
 
 class CommandQueueSingleCardTraceFixture : virtual public CommandQueueSingleCardFixture {
@@ -213,6 +273,48 @@ protected:
 
 class CommandQueueSingleCardProgramFixture : virtual public CommandQueueSingleCardFixture {};
 
+class UnitMeshCQMultiDeviceFixture : public DispatchFixture {
+protected:
+    void SetUp() override {
+        this->slow_dispatch_ = false;
+        auto slow_dispatch = getenv("TT_METAL_SLOW_DISPATCH_MODE");
+        if (slow_dispatch) {
+            log_info(tt::LogTest, "This suite can only be run with fast dispatch or TT_METAL_SLOW_DISPATCH_MODE unset");
+            this->slow_dispatch_ = true;
+            GTEST_SKIP();
+        }
+
+        arch_ = tt::get_arch_from_string(tt::test_utils::get_umd_arch_name());
+
+        num_devices_ = tt::tt_metal::GetNumAvailableDevices();
+        if (num_devices_ < 2) {
+            GTEST_SKIP();
+        }
+
+        std::vector<chip_id_t> chip_ids;
+        for (chip_id_t id : tt::tt_metal::MetalContext::instance().get_cluster().all_chip_ids()) {
+            chip_ids.push_back(id);
+        }
+
+        auto dispatch_core_config = tt::tt_metal::MetalContext::instance().rtoptions().get_dispatch_core_config();
+        auto reserved_devices = distributed::MeshDevice::create_unit_meshes(
+            chip_ids, DEFAULT_L1_SMALL_SIZE, DEFAULT_TRACE_REGION_SIZE, 1, dispatch_core_config);
+        for (const auto& [id, device] : reserved_devices) {
+            devices_.push_back(device);
+        }
+    }
+
+    void TearDown() override {
+        for (auto& device : devices_) {
+            device.reset();
+        }
+    }
+
+    std::vector<std::shared_ptr<distributed::MeshDevice>> devices_;
+    size_t num_devices_;
+};
+
+// #22835: These Fixtures will be removed once tests are fully migrated, and replaced by UnitMeshCQMultiDeviceFixtures
 class CommandQueueMultiDeviceFixture : public DispatchFixture {
 protected:
     void SetUp() override {
@@ -256,8 +358,8 @@ class CommandQueueMultiDeviceProgramFixture : public CommandQueueMultiDeviceFixt
 
 class CommandQueueMultiDeviceBufferFixture : public CommandQueueMultiDeviceFixture {};
 
-class DISABLED_CommandQueueMultiDeviceOnFabricFixture
-    : public CommandQueueMultiDeviceFixture,
+class DISABLED_CQMultiDeviceOnFabricFixture
+    : public UnitMeshCQMultiDeviceFixture,
       public ::testing::WithParamInterface<tt::tt_fabric::FabricConfig> {
 private:
     bool original_fd_fabric_en_ = false;
@@ -279,7 +381,7 @@ protected:
         tt::tt_metal::MetalContext::instance().rtoptions().set_fd_fabric(true);
         // This will force dispatch init to inherit the FabricConfig param
         tt::tt_fabric::SetFabricConfig(GetParam(), tt::tt_fabric::FabricReliabilityMode::STRICT_SYSTEM_HEALTH_SETUP_MODE);
-        CommandQueueMultiDeviceFixture::SetUp();
+        UnitMeshCQMultiDeviceFixture::SetUp();
 
         if (::testing::Test::IsSkipped()) {
             tt::tt_fabric::SetFabricConfig(tt::tt_fabric::FabricConfig::DISABLED);
@@ -290,7 +392,7 @@ protected:
         if (::testing::Test::IsSkipped()) {
             return;
         }
-        CommandQueueMultiDeviceFixture::TearDown();
+        UnitMeshCQMultiDeviceFixture::TearDown();
         tt::tt_fabric::SetFabricConfig(tt::tt_fabric::FabricConfig::DISABLED);
         tt::tt_metal::MetalContext::instance().rtoptions().set_fd_fabric(original_fd_fabric_en_);
     }
