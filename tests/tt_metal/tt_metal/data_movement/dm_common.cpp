5b0f3bf1fc892bb99f2f88e8105a7e50052b6a92
Eugene Lee <169178025+eugeneleeTT@users.noreply.github.com>
2025-07-15 11:44:09 -0400

    [DM] Replaced Buffer Usage with HAL APIs (#24553)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/24315)
    
    ### Problem description
    Previously, DRAM Unary used hardcoded memory region to run tests.
    However, using hardcoded values is not a flexible method of configuring
    the tests.
    
    ### What's changed
    - Replaced all instances of buffer usage with: 
        - HAL APIs to obtain available memory addresses and space
        - `tt_metal` APIs to read from and write to DRAM directly
    - Other APIs for obtaining available DRAM channels, converting them into
    virtual coordinates, and writing to/reading from those coordinates in
    the kernels
    - Added a semaphore to ensure that the reader kernel executes before the
    writer kernel
    - Removed the sharded sweep test
    - Added a DRAM channel sweep test
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes

--- tests/tt_metal/tt_metal/data_movement/dm_common.cpp
+++ tests/tt_metal/tt_metal/data_movement/dm_common.cpp
@@ -9,8 +9,8 @@
 namespace tt::tt_metal::unit_tests::dm {
 
 // Hardcoded constants for L1 memory base address and size
-constexpr uint64_t HARDCODED_L1_MEMORY_BASE_ADDRESS = 1024 * 256;  // 256 KB
-constexpr uint64_t HARDCODED_L1_MEMORY_SIZE_BYTES = 1024 * 1024;   // 1 MB
+// constexpr uint64_t HARDCODED_L1_MEMORY_BASE_ADDRESS = 1024 * 256;  // 256 KB
+// constexpr uint64_t HARDCODED_L1_MEMORY_SIZE_BYTES = 1024 * 1024;   // 1 MB
 
 uint32_t runtime_host_id = 0;
 
@@ -20,16 +20,25 @@ static uint32_t obtain_page_size_bytes(tt::ARCH arch) { return (arch == tt::ARCH
 L1AddressInfo get_l1_address_and_size(const IDevice* device, const CoreCoord& core_coord) {
     // Obtaining L1 address and size for a specific core //
 
-    /*CoreCoord physical_core = device->worker_core_from_logical_core(core_coord);
+    CoreCoord physical_core = device->worker_core_from_logical_core(core_coord);
 
     uint64_t core_l1_base_address = device->get_dev_addr(physical_core, HalL1MemAddrType::DEFAULT_UNRESERVED);
     uint64_t core_l1_size = device->get_dev_size(physical_core, HalL1MemAddrType::DEFAULT_UNRESERVED);
 
-    return {core_l1_base_address, core_l1_size};*/
+    return {core_l1_base_address, core_l1_size};
 
     // Obtaining hardcoded values for L1 address and size //
 
-    return {HARDCODED_L1_MEMORY_BASE_ADDRESS, HARDCODED_L1_MEMORY_SIZE_BYTES};
+    // return {HARDCODED_L1_MEMORY_BASE_ADDRESS, HARDCODED_L1_MEMORY_SIZE_BYTES};
+}
+
+DramAddressInfo get_dram_address_and_size(const IDevice* device) {
+    // Obtaining DRAM address and size //
+
+    auto dram_base_address = tt::tt_metal::MetalContext::instance().hal().get_dev_addr(HalDramMemAddrType::UNRESERVED);
+    uint32_t dram_size = tt::tt_metal::MetalContext::instance().hal().get_dev_size(HalDramMemAddrType::UNRESERVED);
+
+    return {dram_base_address, dram_size};
 }
 
 std::tuple<uint32_t, uint32_t, uint32_t> compute_physical_constraints(const tt::ARCH arch, const IDevice* device) {
