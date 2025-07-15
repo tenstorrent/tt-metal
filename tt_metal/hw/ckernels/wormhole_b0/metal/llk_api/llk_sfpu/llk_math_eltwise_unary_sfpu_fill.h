2a4439898c732aeeec8d0e13077effc79ca7ff47
Aswin Zayasankaran <156493059+Aswinmcw@users.noreply.github.com>
2025-07-15 12:24:27 +0530

    Add int32 support for fill op (#24828)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/24610
    
    ### Problem description
    Int32 support not available for fill op
    
    ### What's changed
    Add int32 support and port to LLK
    
    https://github.com/tenstorrent/tt-llk/pull/481
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/16192490203
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/16192492159
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_fill.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_fill.h
@@ -4,7 +4,6 @@
 
 #pragma once
 
-#include "ckernel_sfpu_fill.h"
 #include "llk_math_eltwise_unary_sfpu_params.h"
 #include "llk_math_eltwise_unary_sfpu_init.h"
 
@@ -17,17 +16,23 @@ inline void llk_math_eltwise_unary_sfpu_fill_init() {
     llk_math_eltwise_unary_sfpu_init<SfpuType::fill, APPROXIMATE>();
 }
 
-template <bool APPROXIMATE>
+template <bool APPROXIMATE, int ITERATIONS = 8>
 inline void llk_math_eltwise_unary_sfpu_fill(uint dst_index, float param0, int vector_mode = (int)VectorMode::RC) {
     _llk_math_eltwise_unary_sfpu_params_<APPROXIMATE>(
-        ckernel::sfpu::calculate_fill<APPROXIMATE>, dst_index, vector_mode, param0);
+        ckernel::sfpu::_calculate_fill_<APPROXIMATE, ITERATIONS>, dst_index, vector_mode, param0);
 }
 
-template <bool APPROXIMATE>
+template <bool APPROXIMATE, int ITERATIONS = 8>
+inline void llk_math_eltwise_unary_sfpu_fill_int(uint dst_index, uint param0, int vector_mode = (int)VectorMode::RC) {
+    _llk_math_eltwise_unary_sfpu_params_<APPROXIMATE>(
+        ckernel::sfpu::_calculate_fill_int_<APPROXIMATE, ITERATIONS>, dst_index, vector_mode, param0);
+}
+
+template <bool APPROXIMATE, int ITERATIONS = 8>
 inline void llk_math_eltwise_unary_sfpu_fill_bitcast(
     uint dst_index, uint32_t param0, int vector_mode = (int)VectorMode::RC) {
     _llk_math_eltwise_unary_sfpu_params_<APPROXIMATE>(
-        ckernel::sfpu::calculate_fill_bitcast<APPROXIMATE>, dst_index, vector_mode, param0);
+        ckernel::sfpu::_calculate_fill_bitcast_<APPROXIMATE, ITERATIONS>, dst_index, vector_mode, param0);
 }
 
 }  // namespace ckernel
