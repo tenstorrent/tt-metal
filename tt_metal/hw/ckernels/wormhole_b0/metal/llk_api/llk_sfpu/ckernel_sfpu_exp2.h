9f7f181f48865bcf91466561ce8defa130966808
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2025-07-16 15:07:48 +0000

    #0: Update for exp2

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_exp2.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_exp2.h
@@ -5,13 +5,23 @@
 #pragma once
 
 #include "sfpu/ckernel_sfpu_exp2.h"
+#include "ckernel_sfpu_exp.h"
 
 namespace ckernel {
 namespace sfpu {
 
 template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
 inline void calculate_exp2() {
-    _calculate_exp2_<APPROXIMATION_MODE, ITERATIONS>();
+    // SFPU microcode
+    for (int d = 0; d < ITERATIONS; d++) {
+        vFloat v = dst_reg[0];
+        // log(2) = 0.6931471805;
+        v = v * 0.6931471805f;
+        // exp = e^(v)
+        vFloat exp = _sfpu_exp_21f_(v);
+        dst_reg[0] = exp;
+        dst_reg++;
+    }
 }
 
 template <bool APPROXIMATION_MODE>
