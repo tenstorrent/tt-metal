3e829b97fe081e60f4f5e1149bd62e49f4ea6a47
Allan Liu <130597713+aliuTT@users.noreply.github.com>
2025-07-16 17:14:24 -0400

    CP: bring up changes for big mesh (#24784)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/20945)
    
    ### Problem description
    Bring up changes needed for multi host control plane. In particular,
    adding support for big mesh, and general support for multihost on 6Us.
    
    ### What's changed
    - distribute fabric init, mesh validation across multiple hosts
    - added support multihost apis for 6U
    - added HostRankId to IntermeshMeshTable 
    - test changes
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes

---
+++ tests/tt_metal/tt_fabric/fabric_router/test_multi_host.cpp
@@ -0,0 +1,41 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <gtest/gtest.h>
+#include <tt-metalium/control_plane.hpp>
+#include <tt-metalium/mesh_graph.hpp>
+#include <filesystem>
+#include <memory>
+#include <vector>
+
+#include "fabric_fixture.hpp"
+#include <tt-metalium/fabric_types.hpp>
+#include <tt-metalium/mesh_coord.hpp>
+#include "impl/context/metal_context.hpp"
+#include <tt-metalium/tt_metal.hpp>
+#include <tt-metalium/distributed_context.hpp>
+
+namespace tt::tt_fabric {
+namespace multi_host_tests {
+
+TEST(MultiHost, TestDualGalaxyControlPlaneInit) {
+    const std::filesystem::path dual_galaxy_mesh_graph_desc_path =
+        std::filesystem::path(tt::tt_metal::MetalContext::instance().rtoptions().get_root_dir()) /
+        "tt_metal/fabric/mesh_graph_descriptors/dual_galaxy_mesh_graph_descriptor.yaml";
+    auto control_plane = std::make_unique<ControlPlane>(dual_galaxy_mesh_graph_desc_path.string());
+
+    control_plane->configure_routing_tables_for_fabric_ethernet_channels(
+        tt::tt_fabric::FabricConfig::FABRIC_2D_DYNAMIC,
+        tt::tt_fabric::FabricReliabilityMode::STRICT_SYSTEM_HEALTH_SETUP_MODE);
+}
+
+TEST(MultiHost, TestDualGalaxyFabricSanity) {
+    tt::tt_metal::MetalContext::instance().set_fabric_config(
+        tt::tt_fabric::FabricConfig::FABRIC_2D_DYNAMIC,
+        tt::tt_fabric::FabricReliabilityMode::STRICT_SYSTEM_HEALTH_SETUP_MODE);
+    tt::tt_metal::MetalContext::instance().initialize_fabric_config();
+}
+
+}  // namespace multi_host_tests
+}  // namespace tt::tt_fabric
