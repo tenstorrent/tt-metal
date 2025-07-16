35231febcaaeab7a6e6b68115f16584d54d80ec8
Anthony Kirby <akirby@tenstorrent.com>
2025-07-16 14:31:42 -0700

    updates to docker-wrapper script

--- docker-wrapper.sh
+++ docker-wrapper.sh
@@ -2,4 +2,13 @@
 
 set -x
 
-docker run --rm --device /dev/tenstorrent -v /dev/hugepages-1G:/dev/hugepages-1G ghcr.io/tenstorrent/tt-metal/tt-metalium/ubuntu-22.04-dev-amd64 ${@:1}
+case $OMPI_COMM_WORLD_LOCAL_RANK in
+    0)
+        device_args="--device /dev/tenstorrent/0" # set from ansible inventory?
+        ;;
+    1)
+        device_args="--device /dev/tenstorrent/1" # set from ansible inventory?
+        ;;
+esac
+
+docker run --rm $device_args -v /dev/hugepages-1G:/dev/hugepages-1G ghcr.io/tenstorrent/tt-metal/tt-metalium/ubuntu-22.04-dev-amd64 ${@:1}
