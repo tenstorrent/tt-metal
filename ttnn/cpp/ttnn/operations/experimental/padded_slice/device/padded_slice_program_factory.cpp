26766ea60488db64d994ab969221088460a7bd92
Artem Yerofieiev <169092593+ayerofieiev-tt@users.noreply.github.com>
2025-07-16 23:50:20 -0700

    Update padded_slice_program_factory.cpp

--- ttnn/cpp/ttnn/operations/experimental/padded_slice/device/padded_slice_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/experimental/padded_slice/device/padded_slice_program_factory.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
 #include "padded_slice_program_factory.hpp"
 
 namespace ttnn::operations::experimental::detail {
