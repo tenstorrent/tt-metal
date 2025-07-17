40e2a24459c2c150fc6b2650704d1f6bcf460f0d
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2025-07-17 09:23:04 +0200

    #23433: SDXL: VAE optimization (#24381)
    ### What's changed
    Adjusted SDPA program config. Reduced number of blocks for VAE group
    norm. Removed unnecessary ops from the rest of VAE.
    
    Note: Option for VAE GroupNorm host fallback will be removed as part of
    this PR, as it is not utilized.
    
    New device time: 2,761,296us
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/15976938174)
    CI passes
    - [x] [(Single-card) Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16322756152)
    CI passes

--- models/experimental/stable_diffusion_xl_base/vae/tests/pcc/test_module_tt_decoder.py
+++ models/experimental/stable_diffusion_xl_base/vae/tests/pcc/test_module_tt_decoder.py
@@ -17,14 +17,13 @@ from loguru import logger
 
 @torch.no_grad()
 @pytest.mark.parametrize(
-    "input_shape, host_fallback, pcc",
+    "input_shape, pcc",
     [
-        ((1, 4, 128, 128), True, 0.92),
-        ((1, 4, 128, 128), False, 0.84),
+        ((1, 4, 128, 128), 0.84),
     ],
 )
 @pytest.mark.parametrize("device_params", [{"l1_small_size": SDXL_L1_SMALL_SIZE}], indirect=True)
-def test_vae_decoder(device, input_shape, host_fallback, pcc, reset_seeds):
+def test_vae_decoder(device, input_shape, pcc, reset_seeds):
     vae = AutoencoderKL.from_pretrained(
         "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float32, use_safetensors=True, subfolder="vae"
     )
@@ -35,7 +34,7 @@ def test_vae_decoder(device, input_shape, host_fallback, pcc, reset_seeds):
 
     logger.info("Loading weights to device")
     model_config = ModelOptimisations()
-    tt_vae = TtDecoder(device, state_dict, model_config=model_config, gn_fallback=host_fallback)
+    tt_vae = TtDecoder(device, state_dict, model_config=model_config)
     logger.info("Loaded weights")
     torch_input_tensor = torch_random(input_shape, -0.1, 0.1, dtype=torch.float32)
 
