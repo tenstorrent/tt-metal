7bbca224727ec2f18036f74cbf2f422262f6990c
Bryan Wilder Field Lozano <blozano@tenstorrent.com>
2025-07-17 12:03:56 +0100

    Update detect-slow-tests.py

--- .github/actions/detect-slow-tests/detect-slow-tests.py
+++ .github/actions/detect-slow-tests/detect-slow-tests.py
@@ -20,10 +20,8 @@ class SlowTestsExceededError(SlowTestDetectionError):
 
 
 def detect_slow_tests(report_dir, timeout):
-    # Hardcoded list of tests to exclude from slow test detection
-    exceptions = [
-        "DispatchFixture.TensixFailOnDuplicateKernelCreationDataflow",
-    ]
+    # Hardcoded list of tests to exclude from slow test detection (Don't use this unless you HAVE TO)
+    exceptions = []
 
     # Find all XML files in the report directory
     report_files = [
