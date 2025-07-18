320d25d9628015d41bbef1a112560d1eaba80f67
adjordjevic-TT <adjordjevic@tenstorrent.com>
2025-07-18 08:36:44 +0000

    Make --gdb argument only viable if -V is used

--- scripts/debugging_scripts/tt-triage.py
+++ scripts/debugging_scripts/tt-triage.py
@@ -7,7 +7,7 @@
 Script Name: tt-triage.py
 
 Usage:
-    tt-triage [--halt-on-error] [--inspector-log-path=<inspector_log_path>] [-v | --verbose] [-V | --vverbose] [-g | --gdb [--port=<port>]] [--dev=<device_id>]...
+    tt-triage [--halt-on-error] [--inspector-log-path=<inspector_log_path>] [-v | --verbose] [-V | --vverbose [-g | --gdb [--port=<port>]]] [--dev=<device_id>]...
 
 Options:
     -h --help                                  Show this screen.
