2b95801eb2894ab48a270dae5460fedcb2597fed
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2025-07-18 09:30:39 +0000

    CIv2 weights

--- models/experimental/stable_diffusion_xl_base/tests/pcc/test_module_tt_unet.py
+++ models/experimental/stable_diffusion_xl_base/tests/pcc/test_module_tt_unet.py
@@ -76,14 +76,17 @@ def run_unet_model(
     temb_shape,
     time_ids_shape,
     is_ci_env,
+    is_ci_v2_env,
+    model_location_generator,
     iterations=1,
 ):
+    model_location = model_location_generator("stable-diffusion-xl-base-1.0", download_if_ci_v2=True)
     unet = UNet2DConditionModel.from_pretrained(
-        "stabilityai/stable-diffusion-xl-base-1.0",
+        "stabilityai/stable-diffusion-xl-base-1.0" if not is_ci_v2_env else model_location,
         torch_dtype=torch.float32,
         use_safetensors=True,
         subfolder="unet",
-        local_files_only=is_ci_env,
+        local_files_only=is_ci_env or is_ci_v2_env,
         cache_dir=SDXL_CI_WEIGHTS_PATH if is_ci_env else None,
     )
     unet.eval()
@@ -192,6 +195,8 @@ def test_unet(
     temb_shape,
     time_ids_shape,
     is_ci_env,
+    is_ci_v2_env,
+    model_location_generator,
     reset_seeds,
 ):
     run_unet_model(
@@ -202,4 +207,6 @@ def test_unet(
         temb_shape,
         time_ids_shape,
         is_ci_env,
+        is_ci_v2_env,
+        model_location_generator,
     )
