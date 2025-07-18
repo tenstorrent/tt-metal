7bd9f5148b6e07746ccc9e1a7e45d34cc4a1a4d5
Austin Ho <aho@tenstorrent.com>
2025-07-18 22:25:54 +0000

    #0: Add api to get eth chan dir

--- tt_metal/fabric/fabric.cpp
+++ tt_metal/fabric/fabric.cpp
@@ -242,6 +242,16 @@ void SetFabricConfig(
     tt::tt_metal::MetalContext::instance().set_fabric_config(fabric_config, reliability_mode, num_routing_planes);
 }
 
+std::optional<eth_chan_directions> get_eth_forwarding_direction(
+    FabricNodeId src_fabric_node_id, FabricNodeId dst_fabric_node_id) {
+    const auto& control_plane = tt::tt_metal::MetalContext::instance().get_control_plane();
+    auto routing_direction = control_plane.get_forwarding_direction(src_fabric_node_id, dst_fabric_node_id);
+    if (!routing_direction.has_value()) {
+        return std::nullopt;
+    }
+    return control_plane.routing_direction_to_eth_direction(routing_direction.value());
+}
+
 namespace experimental {
 
 size_t get_number_of_available_routing_planes(
