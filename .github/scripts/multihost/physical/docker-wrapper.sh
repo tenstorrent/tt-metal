4b44f9d4cbc81034518c75c5e5a415e65e7a7601
Anthony Kirby <akirby@tenstorrent.com>
2025-07-18 11:48:43 -0700

    Updating to build artifact and testing

--- .github/scripts/multihost/physical/docker-wrapper.sh
+++ .github/scripts/multihost/physical/docker-wrapper.sh
@@ -10,5 +10,10 @@ docker run --rm --device /dev/tenstorrent \
     -w $(pwd) \
     -v /dev/hugepages-1G:/dev/hugepages-1G \
     --network=host \
+    -e TT_METAL_HOME=$(pwd) \
+    -e PYTHONPATH=$(pwd) \
+    -e LD_LIBRARY_PATH=$(pwd)/build/lib \
+    -e ARCH_NAME=wormhole_b0 \
+    -e LOGURU_LEVEL=INFO \
     ghcr.io/tenstorrent/tt-metal/tt-metalium/ubuntu-22.04-dev-amd64 \
     orted "$@"
