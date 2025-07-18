86b1c63eeb54aa5e0aa133c01366bc51dc6c115c
Nigel Huang <nhuang@tenstorrent.com>
2025-07-18 15:43:26 +0000

    Undo

--- tt_metal/impl/program/dispatch.cpp
+++ tt_metal/impl/program/dispatch.cpp
@@ -1810,12 +1810,7 @@ void initialize_worker_config_buf_mgr(WorkerConfigBufferMgr& config_buffer_mgr, 
     // Subtract 1 from the number of entries, so the watcher can read information (e.g. fired asserts) from the
     // previous launch message.
     config_buffer_mgr.init_add_buffer(0, launch_msg_buffer_num_entries - 1);
-    if (hal.get_core_has_kernel_config_buffer(HalProgrammableCoreType::ACTIVE_ETH)) {
-        config_buffer_mgr.init_add_buffer(
-            hal.get_dev_addr(HalProgrammableCoreType::ACTIVE_ETH, tt::tt_metal::HalL1MemAddrType::KERNEL_CONFIG), 1);
-    } else {
-        config_buffer_mgr.init_add_buffer(0, 1);
-    }
+    config_buffer_mgr.init_add_buffer(0, 1);
 }
 
 void reserve_space_in_kernel_config_buffer(
