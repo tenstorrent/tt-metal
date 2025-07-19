5d77d5634d2f1a268fa0e307bdbac054f7b9cf72
Pavle Josipović <pavlejosipovic@users.noreply.github.com>
2025-07-19 17:33:51 +0200

    Pjosipovic/bump device perf conv models (#25364)
    Bump device perf targets on a few CNN models, to get green device perf
    ci.
    https://github.com/tenstorrent/tt-metal/actions/runs/16389419582
    
    ---------
    
    Co-authored-by: Pavle Josipovic <pjosipovic@tenstorrent.com>

--- models/demos/wormhole/vit/demo/test_vit_device_perf.py
+++ models/demos/wormhole/vit/demo/test_vit_device_perf.py
@@ -46,7 +46,7 @@ def test_vit_device_ops(
 @pytest.mark.parametrize(
     "expected_kernel_samples_per_sec",
     [
-        1460,
+        1509,
     ],
 )
 @pytest.mark.models_device_performance_bare_metal
