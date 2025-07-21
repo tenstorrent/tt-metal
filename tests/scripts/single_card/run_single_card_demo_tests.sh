475fe1331479df2a39ceac393971c30e8fc3204e
punith.sekar <punith.sekar@multicorewareinc.com>
2025-07-21 04:44:41 +0000

    update yolov6l in run_single_card_demo_tests

--- tests/scripts/single_card/run_single_card_demo_tests.sh
+++ tests/scripts/single_card/run_single_card_demo_tests.sh
@@ -332,7 +332,7 @@ run_yolov6l_perf() {
 
 run_yolov6l_demo() {
 
-  WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest -n auto models/demos/yolov6l/demo/demo.py --timeout 600
+  WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml pytest models/demos/yolov6l/demo/demo.py
 
 }
 
