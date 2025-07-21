d38fe9aec98092f59b15c335dfb296fa8a4f4990
HariniMohan0102 <harini.mohan@multicorewareinc.com>
2025-07-21 10:46:55 +0000

    13047: Add trace perf pipeline

--- models/experimental/efficientnetb0/tests/perf/test_e2e_performant.py
+++ models/experimental/efficientnetb0/tests/perf/test_e2e_performant.py
@@ -11,11 +11,14 @@ from loguru import logger
 import ttnn
 from models.experimental.efficientnetb0.runner.performant_runner import EfficientNetb0PerformantRunner
 from models.utility_functions import run_for_wormhole_b0
+from models.experimental.efficientnetb0.common import EFFICIENTNETB0_L1_SMALL_SIZE
 
 
 @run_for_wormhole_b0()
 @pytest.mark.parametrize(
-    "device_params", [{"l1_small_size": 79104, "trace_region_size": 23887872, "num_command_queues": 2}], indirect=True
+    "device_params",
+    [{"l1_small_size": EFFICIENTNETB0_L1_SMALL_SIZE, "trace_region_size": 23887872, "num_command_queues": 2}],
+    indirect=True,
 )
 @pytest.mark.parametrize(
     "batch_size, act_dtype, weight_dtype",
@@ -28,7 +31,6 @@ from models.utility_functions import run_for_wormhole_b0
     ],
 )
 @pytest.mark.models_performance_bare_metal
-@pytest.mark.models_performance_virtual_machine
 def test_e2e_performant(
     device,
     batch_size,
@@ -43,22 +45,21 @@ def test_e2e_performant(
         act_dtype,
         weight_dtype,
         resolution=resolution,
-        model_location_generator=None,
+        model_location_generator=model_location_generator,
     )
     performant_runner._capture_efficientnetb0_trace_2cqs()
     input_shape = (1, 3, *resolution)
     torch_input_tensor = torch.randn(input_shape, dtype=torch.float32)
 
-    inference_times = []
+    t0 = time.time()
     for _ in range(10):
-        t0 = time.time()
         out = performant_runner.run(torch_input_tensor)
-        t1 = time.time()
-        inference_times.append(t1 - t0)
+    ttnn.synchronize_device(device)
+    t1 = time.time()
 
     performant_runner.release()
 
-    inference_time_avg = round(sum(inference_times) / len(inference_times), 6)
+    inference_time_avg = round((t1 - t0) / 10, 6)
     logger.info(
         f"ttnn_efficientnetb0_batch_size: {batch_size}, resolution: {resolution}. One inference iteration time (sec): {inference_time_avg}, FPS: {round(batch_size/inference_time_avg)}"
     )
--- models/experimental/efficientnetb0/tt/efficientnetb0.py
+++ models/experimental/efficientnetb0/tt/efficientnetb0.py
@@ -36,11 +36,6 @@ class EfficientNetb0Conv2D:
         self.cache = cache
         self.parameters = parameters
         self.shard_layout = shard_layout
-<<<<<<< HEAD
-=======
-        self.use_shallow_covariant = use_shallow_covariant
-        self.activation_dtype = activation_dtype
->>>>>>> 84af45e0f4 (13047: EfficientNetb0 ttnn bringup)
         self.output_layout = output_layout
         self.shard_layout = shard_layout
         self.dilation = dilation
@@ -50,10 +45,6 @@ class EfficientNetb0Conv2D:
 
     def _initialize_conv_config(self):
         conv_config = ttnn.Conv2dConfig(
-<<<<<<< HEAD
-=======
-            dtype=self.activation_dtype,
->>>>>>> 84af45e0f4 (13047: EfficientNetb0 ttnn bringup)
             weights_dtype=ttnn.bfloat8_b,
             activation="",
             shard_layout=self.shard_layout,
@@ -112,10 +103,6 @@ class Conv2dDynamicSamePadding:
         conv_params,
         batch=1,
         is_width_sharded=False,
-<<<<<<< HEAD
-=======
-        use_shallow_covariant=False,
->>>>>>> 84af45e0f4 (13047: EfficientNetb0 ttnn bringup)
     ):
         self.device = device
         self.batch = batch
@@ -128,10 +115,6 @@ class Conv2dDynamicSamePadding:
         self.input_height = conv_params.input_height
         self.input_width = conv_params.input_width
         self.shard_layout = shard_layout
-<<<<<<< HEAD
-=======
-        self.use_shallow_covariant = use_shallow_covariant
->>>>>>> 84af45e0f4 (13047: EfficientNetb0 ttnn bringup)
         ih, iw = self.input_height, self.input_width
         kh, kw = self.kernel_size
         sh, sw = self.stride
@@ -147,10 +130,6 @@ class Conv2dDynamicSamePadding:
                 conv_params,
                 device,
                 shard_layout=ttnn.TensorMemoryLayout.BLOCK_SHARDED,
-<<<<<<< HEAD
-=======
-                use_shallow_covariant=self.use_shallow_covariant,
->>>>>>> 84af45e0f4 (13047: EfficientNetb0 ttnn bringup)
             )
         else:
             self.dynamic_conv = EfficientNetb0Conv2D(
@@ -158,10 +137,6 @@ class Conv2dDynamicSamePadding:
                 conv_params,
                 device=device,
                 shard_layout=self.shard_layout,
-<<<<<<< HEAD
-=======
-                use_shallow_covariant=self.use_shallow_covariant,
->>>>>>> 84af45e0f4 (13047: EfficientNetb0 ttnn bringup)
             )
 
         self.parameters_conv = conv_params
