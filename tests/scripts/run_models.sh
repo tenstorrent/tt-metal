b9d4b25bbc6cd412319bc6d338475ed70ec3024a
Raymond Kim <109366641+tt-rkim@users.noreply.github.com>
2025-07-22 11:48:30 -0400

    #25343: Remove ttnn integration tests from frequent ttnn + models CI, remove references in MODEL_ADD.md for model developers, and remove singular bert perf test in e2e perf since we don't care about these tests anymore (#25344)
    ### Ticket
    
    #25343
    
    ### Problem description
    Provide context for the problem.
    
    ### What's changed
    Describe the approach used to solve the problem.
    Summarize the changes made and its impact.
    
    ### Checklist
    - [ ] frequent
    https://github.com/tenstorrent/tt-metal/actions/runs/16378710855
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
    CI passes (if applicable)
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- tests/scripts/run_models.sh
+++ tests/scripts/run_models.sh
@@ -18,9 +18,3 @@ if [[ -z "$TT_METAL_HOME" ]]; then
 fi
 cd $TT_METAL_HOME
 export PYTHONPATH=$TT_METAL_HOME
-
-# THIS DOES NOT RUN LOL leaving this here for record keeping
-if [[ $ARCH_NAME == "wormhole" ]]; then
-  env pytest tests/ttnn/integration_tests/unet
-  env pytest tests/ttnn/integration_tests/stable_diffusion
-fi
