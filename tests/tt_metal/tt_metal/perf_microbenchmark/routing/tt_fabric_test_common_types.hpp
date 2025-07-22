8eca11ba8c07426448027a6fe631a5e043f63dc3
Abhishek Agarwal <aagarwal@tenstorrent.com>
2025-07-21 18:13:39 -0700

    [Fabric] Updated test infra (3/n) (#25371)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/25444)
    
    ### Problem description
    Allocation algo had a bug where it only looked once for the best fit
    core instead of re-trying.
    
    ### What's changed
    1. The algo now re-tries to find uniform receivers for mcast
    2. Also fixed the initialization of allocation policies to correctly
    update the chunk size in case a user just updates the number of configs
    per recv core
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    (https://github.com/tenstorrent/tt-metal/actions/runs/16424616689)
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
    CI passes (if applicable)
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- tests/tt_metal/tt_metal/perf_microbenchmark/routing/tt_fabric_test_common_types.hpp
+++ tests/tt_metal/tt_metal/perf_microbenchmark/routing/tt_fabric_test_common_types.hpp
@@ -152,6 +152,7 @@ constexpr uint32_t DEFAULT_MAX_RECEIVER_CONFIGS_PER_CORE = 2;
 constexpr uint32_t DEFAULT_SENDER_INITIAL_POOL_SIZE = 1;
 constexpr uint32_t DEFAULT_SENDER_POOL_REFILL_SIZE = 1;
 constexpr uint32_t DEFAULT_PAYLOAD_CHUNK_SIZE_BYTES = 0x80000;  // 512KB
+constexpr uint32_t DEFAULT_RECEIVER_L1_SIZE = 0x100000;
 }  // namespace detail
 
 enum class CoreType {
@@ -175,26 +176,56 @@ struct CoreAllocationConfig {
     uint32_t initial_pool_size = 0;
     // When the pool is exhausted, how many new cores to add to the active set.
     uint32_t pool_refill_size = 1;
+
+    static CoreAllocationConfig get_default_sender_allocation_config() {
+        // Default sender policy: one sender per core.
+        return CoreAllocationConfig{
+            .policy = CoreAllocationPolicy::ExhaustFirst,
+            .max_configs_per_core = detail::DEFAULT_MAX_SENDER_CONFIGS_PER_CORE,
+            .initial_pool_size = 1,  // ExhaustFirst is equivalent to a pool size of 1.
+            .pool_refill_size = detail::DEFAULT_SENDER_POOL_REFILL_SIZE,
+        };
+    }
+
+    static CoreAllocationConfig get_default_receiver_allocation_config() {
+        // Default receiver policy: reuse a core until it's full.
+        return CoreAllocationConfig{
+            .policy = CoreAllocationPolicy::ExhaustFirst,
+            .max_configs_per_core = detail::DEFAULT_MAX_RECEIVER_CONFIGS_PER_CORE,
+            // No default pool sizes for receivers. The pool will be populated with all remaining cores
+            // after senders have been allocated.
+        };
+    }
 };
 
 struct AllocatorPolicies {
     CoreAllocationConfig sender_config;
     CoreAllocationConfig receiver_config;
-    std::optional<uint32_t> default_payload_chunk_size;
+    uint32_t default_payload_chunk_size;
 
-    AllocatorPolicies() {
-        // Default sender policy: one sender per core to isolate performance.
-        sender_config.policy = CoreAllocationPolicy::ExhaustFirst;
-        sender_config.max_configs_per_core = detail::DEFAULT_MAX_SENDER_CONFIGS_PER_CORE;
-        sender_config.pool_refill_size = detail::DEFAULT_SENDER_POOL_REFILL_SIZE;
-        sender_config.initial_pool_size = 1;  // ExhaustFirst is equivalent to a pool size of 1.
+    AllocatorPolicies(
+        std::optional<CoreAllocationConfig> sender_config = std::nullopt,
+        std::optional<CoreAllocationConfig> receiver_config = std::nullopt,
+        std::optional<uint32_t> default_payload_chunk_size = std::nullopt) {
+        if (sender_config.has_value()) {
+            this->sender_config = sender_config.value();
+        } else {
+            this->sender_config = CoreAllocationConfig::get_default_sender_allocation_config();
+        }
 
-        // Default receiver policy: reuse a core until it's full (shared receiver model).
-        receiver_config.policy = CoreAllocationPolicy::ExhaustFirst;
-        receiver_config.max_configs_per_core = detail::DEFAULT_MAX_RECEIVER_CONFIGS_PER_CORE;
-        // No default pool sizes for receivers. The pool will be populated with all remaining cores
-        // after senders have been allocated.
-        default_payload_chunk_size = detail::DEFAULT_PAYLOAD_CHUNK_SIZE_BYTES;
+        if (receiver_config.has_value()) {
+            this->receiver_config = receiver_config.value();
+        } else {
+            this->receiver_config = CoreAllocationConfig::get_default_receiver_allocation_config();
+        }
+
+        if (default_payload_chunk_size.has_value()) {
+            this->default_payload_chunk_size = default_payload_chunk_size.value();
+        } else {
+            // derive a reasonable default based on the number of configs served per receiver core
+            this->default_payload_chunk_size =
+                detail::DEFAULT_RECEIVER_L1_SIZE / this->receiver_config.max_configs_per_core;
+        }
     }
 };
 
