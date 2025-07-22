5dca914a4f92b289e3e4d73c12809d7f66c4ebd8
River Wu <riverwu@tenstorrent.com>
2025-07-22 21:32:30 +0000

    implement multi-device test using DevicePool

--- tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/benchmark_rw_buffer.cpp
+++ tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/benchmark_rw_buffer.cpp
@@ -2,6 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+#include <algorithm>
 #include <chrono>
 #include <fmt/base.h>
 #include <fmt/format.h>
@@ -12,7 +13,6 @@
 #include <cmath>
 #include <cstring>
 #include <memory>
-#include <optional>
 #include <string>
 #include <variant>
 #include <vector>
@@ -23,8 +23,9 @@
 #include <tt-metalium/device.hpp>
 #include <tt-logger/tt-logger.hpp>
 #include <benchmark/benchmark.h>
-#include "command_queue.hpp"
-#include "test_common.hpp"
+#include "device_pool.hpp"
+#include "umd/device/types/cluster_descriptor_types.h"
+#include "hostdevcommon/common_values.hpp"
 #include "impl/context/metal_context.hpp"
 
 using namespace tt;
@@ -42,17 +43,18 @@ using std::chrono::microseconds;
 // Page Size: 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768
 // Transfer Size: 32k, 512M
 // Read & Write
-// Device: 0 (local), 1 (remote)
+// Device: 0 (local), 1 (remote) (when possible)
 //
 ////////////////////////////////////////////////////////////////////////////////
 
 static const auto PAGE_SIZE_ARGS = benchmark::CreateRange(32, 32768, 2);
 static const std::vector<int64_t> TRANSFER_SIZE_ARGS{32 * 1024, 512 * 1024 * 1024};
-static const auto BENCHMARK_ARGS = {PAGE_SIZE_ARGS, TRANSFER_SIZE_ARGS};
 
-static void BM_write(benchmark::State& state, tt_metal::IDevice* device) {
+static void BM_write(benchmark::State& state) {
     auto page_size = state.range(0);
     auto transfer_size = state.range(1);
+    auto device = DevicePool::instance().get_active_device(state.range(2));
+
     auto random_buffer_seed = std::chrono::system_clock::now().time_since_epoch().count();
 
     auto device_buffer = Buffer::create(device, transfer_size, page_size, BufferType::DRAM);
@@ -66,9 +68,10 @@ static void BM_write(benchmark::State& state, tt_metal::IDevice* device) {
     state.SetBytesProcessed(transfer_size * state.iterations());
 }
 
-static void BM_read(benchmark::State& state, tt_metal::IDevice* device) {
+static void BM_read(benchmark::State& state) {
     auto page_size = state.range(0);
     auto transfer_size = state.range(1);
+    auto device = DevicePool::instance().get_active_device(state.range(2));
 
     auto device_buffer = Buffer::create(device, transfer_size, page_size, BufferType::DRAM);
     std::vector<uint32_t> host_buffer;
@@ -80,33 +83,34 @@ static void BM_read(benchmark::State& state, tt_metal::IDevice* device) {
     state.SetBytesProcessed(transfer_size * state.iterations());
 }
 
+std::vector<chip_id_t> setup_device_pool() {
+    auto available_device_ids = MetalContext::instance().get_cluster().all_chip_ids();
+    TT_ASSERT(available_device_ids.contains(0));
+
+    std::vector<chip_id_t> device_ids = {0};
+    log_info(tt::LogTest, "Device 1 available, enable testing on device 1 assuming it's a remote device");
+    if (available_device_ids.contains(1)) {
+        device_ids.push_back(1);
+    }
+
+    DevicePool::initialize(device_ids, 1, DEFAULT_L1_SMALL_SIZE, DEFAULT_TRACE_REGION_SIZE, DispatchCoreConfig{});
+
+    return device_ids;
+}
+
 int main(int argc, char** argv) {
     benchmark::Initialize(&argc, argv);
 
-    // TODO: Test Across Multiple devices.
-    auto device_id = 0;
-    if (device_id >= MetalContext::instance().get_cluster().number_of_devices()) {
-        log_info(LogTest, "Skip! Device id {} is not applicable on this system", device_id);
-        return 1;
-    }
+    auto device_args = setup_device_pool();
+    auto benchmark_args = {PAGE_SIZE_ARGS, TRANSFER_SIZE_ARGS, {device_args.begin(), device_args.end()}};
 
-    tt_metal::IDevice* device = tt_metal::CreateDevice(device_id);
-
-    if (!device->using_fast_dispatch()) {
-        log_info(LogTest, "Skip! This test needs to be run with fast dispatch enabled");
-        return 1;
-    }
-
-    benchmark::RegisterBenchmark("EnqueueWriteBuffer", BM_write, device)
-        ->ArgsProduct(BENCHMARK_ARGS)
-        // Google Benchmark uses CPU time to calculate throughput by default, which is not suitable for this
-        // benchmark
-        ->UseRealTime();
-
-    benchmark::RegisterBenchmark("EnqueueReadBuffer", BM_read, device)->ArgsProduct(BENCHMARK_ARGS)->UseRealTime();
+    // Google Benchmark uses CPU time to calculate throughput by default, which is not suitable for this
+    // benchmark
+    benchmark::RegisterBenchmark("EnqueueWriteBuffer", BM_write)->ArgsProduct(benchmark_args)->UseRealTime();
+    benchmark::RegisterBenchmark("EnqueueReadBuffer", BM_read)->ArgsProduct(benchmark_args)->UseRealTime();
 
     benchmark::RunSpecifiedBenchmarks();
-    tt_metal::CloseDevice(device);
+    DevicePool::instance().close_devices(DevicePool::instance().get_all_active_devices());
     benchmark::Shutdown();
 
     return 0;
