31918903eb5605ab2802ca1ccc168b4dfa673ccf
Ambrose Ling <aling@tenstorrent.com>
2025-07-23 04:17:18 -0400

    TopK Index Passing Model Integration  (#25359)
    ### Problem description
    Relanding Johanna's PR for integration index tensor passing to Top K
    kernel, previously there was a `thread '<unnamed>' panicked at 'no entry
    found for key', src/lib.rs:201:37` due to the decoded index was out of
    vocabulary size.
    
    ### What's changed
    - Reason for above error was because we used
    `self.args.padded_vocab_size` as the hidden dimension when we should use
    `per_device_vocab_size`. Moreover, this tensor should be replicated
    rather than sharded across devices so the Shard2dTensor config was
    changed. Using `padded_vocab_size` caused the error because indices can
    only be less than 2^16-1 and the kernel only supports 16 bit indices,
    and each device should only deal with 16k vocab size
    
    ### Checklist
    - E2E Perf Gains: 70.13 main -> 71.519 tok/s/u (with Topk index model
    integration)
    - TG pipelines
    [passing](https://github.com/tenstorrent/tt-metal/actions/runs/16384011372)
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
    CI passes (if applicable)
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: Johanna Rock <jrock@tenstorrent.com>

--- models/demos/llama3_subdevices/tt/sampling.py
+++ models/demos/llama3_subdevices/tt/sampling.py
@@ -73,6 +73,19 @@ class TTSampling(LightweightModule):
             memory_config=ttnn.DRAM_MEMORY_CONFIG,
         )
 
+        assert per_device_vocab_size == 16 * 1024, "Per device vocab size is incorrect (should be 16k)"
+        indices_tensor_torch = torch.zeros(1, 1, self.max_batch_size, per_device_vocab_size, dtype=torch.int32)
+        for i in range(per_device_vocab_size):
+            indices_tensor_torch[:, :, :, i] = i
+        self.tt_indices_tensor = ttnn.from_torch(
+            indices_tensor_torch,
+            dtype=ttnn.uint16,
+            layout=ttnn.Layout.TILE,
+            device=self.mesh_device,
+            mesh_mapper=ttnn.ShardTensor2dMesh(self.mesh_device, dims=(None, None), mesh_shape=self.args.cluster_shape),
+            memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        )
+
     def forward(
         self,
         x: ttnn.Tensor,
@@ -99,9 +112,12 @@ class TTSampling(LightweightModule):
 
         # Local top k
         topk_values, topk_indices = ttnn.topk(
-            x_bf16, k=self.max_top_k, dim=-1, sub_core_grids=self.args.sub_core_grid_topk
+            x_bf16,
+            k=self.max_top_k,
+            dim=-1,
+            sub_core_grids=self.args.sub_core_grid_topk,
+            indices_tensor=self.tt_indices_tensor,
         )
-
         # Gather values
         # Note: Persistent output buffer used, do not deallocate output!
         topk_values_gathered = self.tt_ccl.line_all_gather(
