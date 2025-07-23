6318587f4e8746567cb33f894c534bb09fd72b86
asaigal <asaigal@tenstorrent.com>
2025-07-23 03:31:24 +0000

    Add all_gather. Add reduce_scatter (not implemented error)

--- tt-train/sources/ttml/ttnn_fixed/distributed/ttnn_ops.cpp
+++ tt-train/sources/ttml/ttnn_fixed/distributed/ttnn_ops.cpp
@@ -13,6 +13,23 @@
 
 namespace ttml::ttnn_fixed::distributed {
 
+tt::tt_metal::Tensor all_gather(const tt::tt_metal::Tensor& tensor, int dim) {
+    auto* current_device = &ttml::autograd::ctx().get_device();
+    auto num_devices = current_device->num_devices();
+    if (num_devices == 1U) {
+        throw std::logic_error("All gather should not be called for a single device case");
+    }
+    auto& ccl_resources = ttml::autograd::ctx().get_ccl_resources();
+    std::vector<tt::tt_metal::GlobalSemaphore> global_semaphores{ccl_resources.get_all_gather_semaphore()};
+    return ttnn::experimental::all_gather_async(
+        tensor,
+        dim,
+        global_semaphores,
+        /* num_links */ 1,
+        /* memory_config */ std::nullopt,
+        ttnn::ccl::Topology::Linear);
+}
+
 tt::tt_metal::Tensor all_reduce(const tt::tt_metal::Tensor& tensor) {
     auto* current_device = &ttml::autograd::ctx().get_device();
     auto num_devices = current_device->num_devices();
@@ -25,18 +42,8 @@ tt::tt_metal::Tensor all_reduce(const tt::tt_metal::Tensor& tensor) {
         throw std::logic_error("All reduce supports only 4D tensors");
     }
 
-    auto& ccl_resources = ttml::autograd::ctx().get_ccl_resources();
-
     auto reshaped_tensor = ttnn::reshape(tensor, ttnn::Shape({1, shape[0] * shape[1], shape[2], shape[3]}));
-    std::vector<tt::tt_metal::GlobalSemaphore> global_semaphores{ccl_resources.get_all_gather_semaphore()};
-    auto gathered_tensor = ttnn::experimental::all_gather_async(
-        reshaped_tensor,
-        /* dim */ 0,
-        global_semaphores,
-        /* num_links */ 1,
-        /* memory_config */ std::nullopt,
-        ttnn::ccl::Topology::Linear);
-
+    auto gathered_tensor = all_gather(reshaped_tensor, 0);
     auto reduced_tensor = ttnn::moreh_sum(
         gathered_tensor,
         0,
@@ -48,75 +55,9 @@ tt::tt_metal::Tensor all_reduce(const tt::tt_metal::Tensor& tensor) {
     return reduced_tensor;
 }
 
-tt::tt_metal::Tensor scatter(const tt::tt_metal::Tensor& tensor, int dim) {
-    auto* current_device = &ttml::autograd::ctx().get_device();
-    auto num_devices = current_device->num_devices();
-    if (num_devices == 1U) {
-        throw std::logic_error("Scatter should not be called for a single device case");
-    }
-
-    auto device_grid_shape = current_device->shape();
-    const auto& storage = std::get<tt::tt_metal::DeviceStorage>(tensor.storage());
-    const auto num_tensor_buffers = storage.coords.size();
-
-    if (num_devices != num_tensor_buffers) {
-        throw std::logic_error(fmt::format(
-            "Number of buffers should be equal to the number of devices. Tensor is not properly replicated."
-            " Number of devices: {}, number of buffers: {}",
-            num_devices,
-            num_tensor_buffers));
-    }
-
-    auto tensor_shape = tensor.logical_shape();
-    auto tensor_rank = tensor_shape.rank();
-    if (tensor_rank != 4U) {
-        throw std::logic_error(
-            fmt::format("Scatter supports only 4D tensors. Shape {} Rank {}", tensor_shape, tensor_rank));
-    }
-    auto split_axis_size = tensor_shape[dim];
-    if (split_axis_size % num_devices != 0) {
-        throw std::logic_error(fmt::format(
-            "Split axis size should be divisible by number of devices. Split axis size: {}, number of devices: {}",
-            split_axis_size,
-            num_devices));
-    }
-    auto split_size_per_device = split_axis_size / num_devices;
-    if (split_size_per_device % 32 != 0) {
-        throw std::logic_error(fmt::format(
-            "ttnn::slice does not support output dimension that is not divisible by 32."
-            "Requested output dimension: {}",
-            split_size_per_device));
-    }
-
-    ttnn::SmallVector<uint32_t> scattered_shape{tensor_shape[0], tensor_shape[1], tensor_shape[2], tensor_shape[3]};
-    scattered_shape[dim] = split_size_per_device;
-
-    ttnn::Tensor scattered_tensor = tt::tt_metal::allocate_tensor_on_device(
-        ttnn::TensorSpec(
-            ttnn::Shape(scattered_shape),
-            tt::tt_metal::TensorLayout(tensor.dtype(), tensor.layout(), tensor.memory_config())),
-        current_device);
-    auto scattered_tensors = ttnn::distributed::get_device_tensors(scattered_tensor);
-    if (scattered_tensors.size() != num_devices) {
-        throw std::logic_error(fmt::format(
-            "Number of scattered tensors should be equal to the number of devices. Tensor is not properly replicated."
-            " Number of devices: {}, number of scattered tensors: {}",
-            num_devices,
-            scattered_tensors.size()));
-    }
-
-    ttnn::SmallVector<uint32_t> start{0, 0, 0, 0};
-    ttnn::SmallVector<uint32_t> end{tensor_shape[0], tensor_shape[1], tensor_shape[2], tensor_shape[3]};
-    const ttnn::SmallVector<uint32_t> stride{1U, 1U, 1U, 1U};
-    size_t idx = 0;
-    for (const auto& tensor_shard : ttnn::distributed::get_device_tensors(tensor)) {
-        start[dim] = split_size_per_device * idx;
-        end[dim] = split_size_per_device * (idx + 1);
-
-        ttnn::slice(tensor_shard, start, end, stride, std::nullopt, scattered_tensors[idx]);
-        ++idx;
-    }
-    return ttnn::distributed::combine_device_tensors(scattered_tensors);
+tt::tt_metal::Tensor reduce_scatter(const tt::tt_metal::Tensor& tensor, int dim) {
+    throw std::runtime_error(
+        "Reduce scatter is not implemented! Will be added once ttnn::experimental::reduce_scatter is available.");
 }
 
 }  // namespace ttml::ttnn_fixed::distributed
--- tt-train/tests/ttnn_fixed/distributed/distributed_ttnn_ops_test.cpp
+++ tt-train/tests/ttnn_fixed/distributed/distributed_ttnn_ops_test.cpp
@@ -46,7 +46,7 @@ TEST_F(TrivialTnnFixedDistributedTest, TestCustomScatterDim0) {
     auto shape = ttnn::Shape({size, 1, 1, 1});
     auto tensor = ttml::core::from_vector(data, shape, device);
 
-    auto scattered_tensor = ttml::ttnn_fixed::distributed::scatter(tensor, /* dim */ 0);
+    auto scattered_tensor = ttml::ttnn_fixed::distributed::reduce_scatter(tensor, /* dim */ 0);
 
     auto xtensors_back = ttml::core::to_xtensor(scattered_tensor, ttml::core::IdentityComposer{});
 
@@ -70,7 +70,7 @@ TEST_F(TrivialTnnFixedDistributedTest, TestCustomScatterDim1) {
     auto shape = ttnn::Shape({1, size, 1, 1});
     auto tensor = ttml::core::from_vector(data, shape, device);
 
-    auto scattered_tensor = ttml::ttnn_fixed::distributed::scatter(tensor, /* dim */ 1);
+    auto scattered_tensor = ttml::ttnn_fixed::distributed::reduce_scatter(tensor, /* dim */ 1);
 
     auto xtensors_back = ttml::core::to_xtensor(scattered_tensor, ttml::core::IdentityComposer{});
 
@@ -94,7 +94,7 @@ TEST_F(TrivialTnnFixedDistributedTest, TestCustomScatterDim2) {
     auto shape = ttnn::Shape({1, 1, size, 1});
     auto tensor = ttml::core::from_vector(data, shape, device);
 
-    auto scattered_tensor = ttml::ttnn_fixed::distributed::scatter(tensor, /* dim */ 2);
+    auto scattered_tensor = ttml::ttnn_fixed::distributed::reduce_scatter(tensor, /* dim */ 2);
 
     auto xtensors_back = ttml::core::to_xtensor(scattered_tensor, ttml::core::IdentityComposer{});
 
@@ -118,7 +118,7 @@ TEST_F(TrivialTnnFixedDistributedTest, TestCustomScatterDim3) {
     auto shape = ttnn::Shape({1, 1, 1, size});
     auto tensor = ttml::core::from_vector(data, shape, device);
 
-    auto scattered_tensor = ttml::ttnn_fixed::distributed::scatter(tensor, /* dim */ 3);
+    auto scattered_tensor = ttml::ttnn_fixed::distributed::reduce_scatter(tensor, /* dim */ 3);
 
     auto xtensors_back = ttml::core::to_xtensor(scattered_tensor, ttml::core::IdentityComposer{});
 
