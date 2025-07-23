6634c807a7d86499e4980e1f1b17b6fd6ae4fbd5
grechsteinerTT <grechsteiner@tenstorrent.com>
2025-07-23 14:44:37 +0000

    Falcon

--- models/demos/t3000/falcon40b/tests/test_falcon_decoder.py
+++ models/demos/t3000/falcon40b/tests/test_falcon_decoder.py
@@ -8,6 +8,7 @@ from loguru import logger
 
 import ttnn
 from models.demos.t3000.falcon40b.reference.hf_modeling_falcon import FalconForCausalLM
+from models.demos.t3000.falcon40b.tt.falcon_ccl import TT_CCL
 from models.demos.t3000.falcon40b.tt.falcon_decoder import TtFalconDecoderLayer
 from models.demos.t3000.falcon40b.tt.model_config import get_model_config
 from models.demos.t3000.falcon40b.tt.model_utils import generate_layernorm_persistent_tensors
@@ -229,8 +230,10 @@ def run_test_FalconDecoder_inference(
     )
 
     # TT hardware execution =================================================================
+    tt_ccl = TT_CCL(mesh_device)
     tt_FalconDecoder_model = TtFalconDecoderLayer(
         mesh_device,
+        tt_ccl,
         state_dict,
         base_url,
         layer_num,
@@ -348,6 +351,7 @@ def run_test_FalconDecoder_inference(
     ],
     ids=["BFLOAT8_B-SHARDED", "BFLOAT16-SHARDED", "BFLOAT8_B-DRAM", "BFLOAT16-DRAM"],
 )
+@pytest.mark.parametrize("device_params", [{"fabric_config": ttnn.FabricConfig.FABRIC_1D}], indirect=True)
 def test_FalconDecoder_inference(
     num_devices,
     model_version,
