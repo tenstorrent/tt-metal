8bb7249be2a89f7d8fde2aac51133a96e0c3a368
marvinmok-TT <marvinmok@tenstorrent.com>
2025-07-23 20:59:27 +0000

    added EnqueueWaitForEvent migration

--- tests/tt_metal/tt_metal/dispatch/multi_command_queue_fixture.hpp
+++ tests/tt_metal/tt_metal/dispatch/multi_command_queue_fixture.hpp
@@ -288,13 +288,19 @@ protected:
     }
 
     std::vector<std::shared_ptr<distributed::MeshDevice>> devices_;
+    distributed::MeshCoordinate zero_coord_ = distributed::MeshCoordinate::zero_coordinate(2);
+    distributed::MeshCoordinateRange device_range_ = distributed::MeshCoordinateRange(zero_coord_, zero_coord_);
 };
 
+<<<<<<< HEAD
 class UnitMeshMultiCQMultiDeviceBufferFixture : public UnitMeshMultiCQMultiDeviceFixture {};
 
 class MultiCommandQueueMultiDeviceBufferFixture : public MultiCommandQueueMultiDeviceFixture {};
+=======
+class UnitMeshMultiCQMultDeviceEventFixture : public UnitMeshMultiCQMultiDeviceFixture {};
+>>>>>>> cf8f88d18c (added EnqueueWaitForEvent migration)
 
-class MultiCommandQueueMultiDeviceEventFixture : public MultiCommandQueueMultiDeviceFixture {};
+class MultiCommandQueueMultiDeviceBufferFixture : public MultiCommandQueueMultiDeviceFixture {};
 
 class DISABLED_MultiCQMultiDeviceOnFabricFixture : public UnitMeshMultiCQMultiDeviceFixture,
                                                    public ::testing::WithParamInterface<tt::tt_fabric::FabricConfig> {
