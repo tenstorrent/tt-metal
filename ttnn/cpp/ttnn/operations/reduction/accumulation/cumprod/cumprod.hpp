817d4eb3c79ff3cf4b7604ec4728656f0309c67a
Jarosław Bartosz Bieniek <jbbieniek@tenstorrent.com>
2025-07-24 20:17:10 +0200

    cumprod + cumsum => accumulation (#24575)
    ### Ticket
    #22294
    
    ### Problem description
    merge cumsum and cumprod
    
    ### What's changed
    - merge cumsum and cumprod device operations
    - move cumsum and cumprod to the common `accumulation` directory
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [x] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: Jarek Bieniek <jbbieniekTT@tenstorrent.com>

--- ttnn/cpp/ttnn/operations/reduction/accumulation/cumprod/cumprod.hpp
+++ ttnn/cpp/ttnn/operations/reduction/accumulation/cumprod/cumprod.hpp
@@ -10,7 +10,7 @@
 #include "ttnn/decorators.hpp"
 
 namespace ttnn {
-namespace operations::reduction {
+namespace operations::reduction::accumulation {
 
 struct CumprodOperation {
     static Tensor invoke(
@@ -18,12 +18,14 @@ struct CumprodOperation {
         const Tensor& input_tensor,
         const int32_t& dim,
         std::optional<DataType>& dtype,
+        const bool& reverse_order,
         std::optional<Tensor> optional_out,
         const std::optional<MemoryConfig>& memory_config = std::nullopt);
 };
 
-}  // namespace operations::reduction
+}  // namespace operations::reduction::accumulation
 
-constexpr auto cumprod = ttnn::register_operation<"ttnn::cumprod", ttnn::operations::reduction::CumprodOperation>();
+constexpr auto cumprod =
+    ttnn::register_operation<"ttnn::cumprod", ttnn::operations::reduction::accumulation::CumprodOperation>();
 
 }  // namespace ttnn
