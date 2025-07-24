6ba5e336a46c69ccd989bfbd6c30450d195503e4
Sean Nijjar <sean.nijjar@gmail.com>
2025-07-24 23:33:45 +0000

    add a note about the telemetry flag

--- tt_metal/llrt/rtoptions.hpp
+++ tt_metal/llrt/rtoptions.hpp
@@ -470,6 +470,9 @@ public:
     }
     inline std::string get_custom_fabric_mesh_graph_desc_path() const { return custom_fabric_mesh_graph_desc_path; }
 
+    // If true, the fabric (routers) will collect coarse grain telemetry data in software. This flag's state does not
+    // affect the ability to capture Ethernet Subsystem register-read-based telemetry data.
+    // This BW telemetry is coarse grain and records the total time that the reouter has unsent and inflight packets.
     inline bool get_enable_fabric_telemetry() const { return enable_fabric_telemetry; }
     inline void set_enable_fabric_telemetry(bool enable) { enable_fabric_telemetry = enable; }
 
