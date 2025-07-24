c71e55f8257c582dc4a2117fe396579a710ca3fc
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2025-07-24 09:11:54 +0000

    #0: Improve accuracy of exp2

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_exp2.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_exp2.h
@@ -9,14 +9,38 @@
 namespace ckernel {
 namespace sfpu {
 
-template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
-inline void calculate_exp2() {
-    _calculate_exp2_<APPROXIMATION_MODE, ITERATIONS>();
+sfpi_inline sfpi::vFloat _sfpu_exp2_21f_(sfpi::vFloat val) {
+    sfpi::vFloat y = 0.0f;
+    v_if(val > -127.f) {
+        val = val * 0.6931471805f;
+        sfpi::vInt z = sfpu::_float_to_int32_(val * sfpi::vFloat(0x00b8aa3b) + sfpi::vFloat(0x3f800000));
+        sfpi::vInt zii = exexp(sfpi::reinterpret<sfpi::vFloat>(z));         // Extract exponent
+        sfpi::vInt zif = sfpi::exman9(sfpi::reinterpret<sfpi::vFloat>(z));  // Extract mantissa
+
+        sfpi::vFloat d1 = sfpi::vFloat(0.40196114e-7);
+        sfpi::vFloat d2 = sfpi::int32_to_float(sfpi::vInt(0xf94ee7) + zif);
+        sfpi::vFloat d3 = sfpi::int32_to_float(sfpi::vInt(0x560) + zif);
+        d2 = d1 * d2;
+        zif = sfpu::_float_to_int32_(d2 * d3);
+
+        zii = sfpi::reinterpret<sfpi::vInt>(
+            sfpi::setexp(sfpi::reinterpret<sfpi::vFloat>(zif), 127U + zii));  // restore exponent
+
+        y = sfpi::reinterpret<sfpi::vFloat>(zii);
+    }
+    v_endif;
+    return y;
 }
 
-template <bool APPROXIMATION_MODE>
-inline void exp2_init() {
-    _init_exp2_<APPROXIMATION_MODE>();
+template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
+inline void calculate_exp2() {
+    // SFPU microcode
+    for (int d = 0; d < ITERATIONS; d++) {
+        vFloat v = dst_reg[0];
+        vFloat exp = _sfpu_exp2_21f_(v);
+        dst_reg[0] = exp;
+        dst_reg++;
+    }
 }
 
 }  // namespace sfpu
