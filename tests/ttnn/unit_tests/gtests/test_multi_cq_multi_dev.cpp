7467ef1c8ab109e1b2d25f2db2d488223a9b8198
blozano-tt <blozano@tenstorrent.com>
2025-07-25 17:22:04 +0000

    Reduce loop count for two T3K tests

--- tests/ttnn/unit_tests/gtests/test_multi_cq_multi_dev.cpp
+++ tests/ttnn/unit_tests/gtests/test_multi_cq_multi_dev.cpp
@@ -74,9 +74,9 @@ TEST_F(MultiCommandQueueT3KFixture, Test2CQMultiDeviceProgramsOnCQ1) {
     uint32_t datum_size_bytes = 2;
     auto host_data = std::shared_ptr<bfloat16[]>(new bfloat16[buf_size_datums]);
     auto readback_data = std::shared_ptr<bfloat16[]>(new bfloat16[buf_size_datums]);
-    for (int outer_loop = 0; outer_loop < 5; outer_loop++) {
+    for (int outer_loop = 0; outer_loop < 2; outer_loop++) {
         log_info(LogTest, "Running outer loop {}", outer_loop);
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 5; i++) {
             for (auto& dev : this->devs) {
                 auto dev_idx = dev.first;
                 auto device = dev.second;
@@ -129,9 +129,9 @@ TEST_F(MultiCommandQueueT3KFixture, Test2CQMultiDeviceProgramsOnCQ0) {
 
     TensorSpec tensor_spec(shape, TensorLayout(DataType::BFLOAT16, PageConfig(Layout::TILE), mem_cfg));
     ASSERT_EQ(buf_size_datums * datum_size_bytes, tensor_spec.compute_packed_buffer_size_bytes());
-    for (int outer_loop = 0; outer_loop < 5; outer_loop++) {
+    for (int outer_loop = 0; outer_loop < 2; outer_loop++) {
         log_info(LogTest, "Running outer loop {}", outer_loop);
-        for (int i = 0; i < 30; i++) {
+        for (int i = 0; i < 5; i++) {
             for (auto& dev : this->devs) {
                 auto dev_idx = dev.first;
                 auto device = dev.second;
