9a22aac2990bb8907412925cd0df342f3ee57211
Aleksandar Stancov <astancov@tenstorrent.com>
2025-07-25 09:42:08 +0000

    cleaned up single core multicast logic

--- ttnn/cpp/ttnn/operations/conv/conv2d/device/conv2d_op_width_sharded_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/device/conv2d_op_width_sharded_program_factory.cpp
@@ -350,11 +350,10 @@ tt::tt_metal::operation::ProgramWithCallbacks multi_core_optimized_conv_width_sh
 
     compute_defines["WIDTH_SHARDED"] = "1";
 
-    if (output_num_cores == 1) {
-        writer_mcast_sender_defines["SKIP_MCAST"] = "1";
-    }
-    bool skip_mcast = is_singlecore_skip_mcast(p_config);
-    if (skip_mcast) {
+    const std::tuple<bool, bool>& skip_mcast =
+        conv_skip_mcast(parallelization_config, a.memory_config().memory_layout());
+    const bool skip_activation_mcast = std::get<0>(skip_mcast);
+    if (skip_activation_mcast) {
         reader_defines["SKIP_MCAST"] = "1";
     }
     if (has_bias) {
@@ -403,7 +402,7 @@ tt::tt_metal::operation::ProgramWithCallbacks multi_core_optimized_conv_width_sh
         a.memory_config().shard_spec().value().shape,
         has_bias,
         false,
-        skip_mcast);
+        skip_activation_mcast);
 
     std::vector<std::vector<uint16_t>> conv_sharded_input_top_left_indices =
         ttnn::operations::sliding_window::generate_sliding_window_op_config(
@@ -451,9 +450,7 @@ tt::tt_metal::operation::ProgramWithCallbacks multi_core_optimized_conv_width_sh
 
         bias_ntiles,
         get_cb_info_by_name(cb_info, Conv2dCb::BIAS).index,
-
-        skip_mcast ? get_cb_info_by_name(cb_info, Conv2dCb::ACT_TILIZED).index
-                   : get_cb_info_by_name(cb_info, Conv2dCb::ACT).index,
+        get_cb_info_by_name(cb_info, Conv2dCb::ACT).index,
         get_cb_info_by_name(cb_info, Conv2dCb::WEIGHTS).index,
         get_cb_info_by_name(cb_info, Conv2dCb::ACT_ROW_MAJOR_BFLOAT16).index,
         get_cb_info_by_name(cb_info, Conv2dCb::ACT_SECOND_READER).index,
