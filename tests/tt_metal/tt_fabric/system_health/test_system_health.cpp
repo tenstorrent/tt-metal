2ec28233332feb915de87b30f3d6ec2456b646ff
Ambrose Ling <aling@tenstorrent.com>
2025-07-25 19:36:16 +0000

    change mmio chip count and add p150x8 as supported devices

--- tests/tt_metal/tt_fabric/system_health/test_system_health.cpp
+++ tests/tt_metal/tt_fabric/system_health/test_system_health.cpp
@@ -329,7 +329,7 @@ TEST(Cluster, TestMeshFullConnectivity) {
         num_connections_per_side = 4;
     } else if (cluster_type == tt::ClusterType::P150_X8) {
         num_expected_chips = 8;
-        num_expected_mmio_chips = 4;
+        num_expected_mmio_chips = 8;
         num_connections_per_side = 2;
     } else {
         GTEST_SKIP() << "Mesh check not supported for system type " << magic_enum::enum_name(cluster_type);
