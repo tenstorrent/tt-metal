f62b2a944eb41a1c6258eb2afb6dc4d0ad1cbc26
Oleg Milyutin <omilyutin@tenstorrent.com>
2025-07-25 18:27:34 +0000

    Multi-host aware mesh device reshapes

--- ttnn/cpp/ttnn/operations/experimental/ccl/send_recv_async/recv_async/device/recv_async_program.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/send_recv_async/recv_async/device/recv_async_program.cpp
@@ -38,7 +38,7 @@ tt::tt_metal::operation::ProgramWithCallbacks recv_async_multicore(
         if (socket_mesh_device->get_device(connection.receiver_core.device_coord)->id() == target_device->id()) {
             receiver_core_coord = connection.receiver_core.core_coord;
             receiver_fabric_node_id =
-                output_tensor.mesh_device()->get_device_fabric_node_id(connection.sender_core.device_coord);
+                output_tensor.mesh_device()->get_fabric_node_id(connection.sender_core.device_coord);
             sender_fabric_node_id = mesh_socket.get_fabric_node_id(
                 tt::tt_metal::distributed::SocketEndpoint::SENDER, connection.sender_core.device_coord);
             break;
