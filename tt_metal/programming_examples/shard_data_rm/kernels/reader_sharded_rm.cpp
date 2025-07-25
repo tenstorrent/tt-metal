6e43a55c9c849ff6367022410a9eedc21ef1b717
marty1885 <marty188586@gmail.com>
2025-07-25 20:59:46 +0800

    fix bad rebase

--- tt_metal/programming_examples/shard_data_rm/kernels/reader_sharded_rm.cpp
+++ tt_metal/programming_examples/shard_data_rm/kernels/reader_sharded_rm.cpp
@@ -10,7 +10,7 @@
 
 void kernel_main() {
     const uint32_t src_addr = get_arg_val<uint32_t>(0);
-    const uint32_t shard_size = get_arg_val<uint32_t>(1);
+    const uint32_t stick_size = get_arg_val<uint32_t>(1);
     const uint32_t padded_offset_bytes = get_arg_val<uint32_t>(2);
     const uint32_t start_id = get_arg_val<uint32_t>(3);
 
@@ -18,11 +18,12 @@ void kernel_main() {
     constexpr auto s0_args = TensorAccessorArgs<1>();
     const auto s0 = TensorAccessor(s0_args, src_addr, stick_size);
     uint32_t stick_id = start_id;
-    cb_reserve_back(cb_id_in0, shard_size);
+    cb_reserve_back(cb_id_in0, stick_size);
     uint32_t l1_write_addr = get_write_ptr(cb_id_in0);
 
     DPRINT << "Core (" << (uint32_t)get_absolute_logical_x() << "," << (uint32_t)get_absolute_logical_y() << "): ";
-    const uint32_t n_sticks = shard_size / element_per_stick;
+    constexpr uint32_t element_per_stick = 2;  // Each stick contains two bfloat16 values
+    const uint32_t n_sticks = stick_size / element_per_stick;
     for (uint32_t i = 0; i < n_sticks; i++) {
         uint64_t src_noc_addr = get_noc_addr(stick_id, s0);
         // Read a tick at a time from the source address and write it to the L1 write address.
@@ -36,7 +37,7 @@ void kernel_main() {
         l1_write_addr += padded_offset_bytes;
     }
     DPRINT << ENDL();
-    cb_push_back(cb_id_in0, shard_size);
+    cb_push_back(cb_id_in0, stick_size);
 
     // At this point we have read all the sticks into the circular buffer. Computation and proceed knowing
     // that it has data in circular buffer and in a specific format.
