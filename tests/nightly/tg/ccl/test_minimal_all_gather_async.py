da9c16c6480ef207355cd48e7c09bd6a3b016615
Jonathan Su <jonathansu@tenstorrent.com>
2025-07-26 07:14:51 -0700

    All gather/Reduce Scatter (line and ring) integrate Fabric Mux (multiple workers per direction) and chunking (#25222)
    ### Problem description
    All-gather and reduce-scatter are read bound, and as such it would be
    beneficial to have multiple workers per link/per direction. However, to
    enable this, we need to incorporate fabric mux. Moreover, both CCLs
    write full slices before writing a semaphore increment that would let
    the receiving device know that it can start processing the next slice of
    input. As fabric mux round-robin's between the multiple channels/workers
    on a single link, each receiver worker will not receive the semaphore
    and begin reading/compute until all workers have received the semaphore.
    To prevent this, we need to send semaphores more frequently - to enable
    this we introduce chunking per slice.
    
    ### What's changed
    Two main changes
    1) Integrate fabric mux to enable multiple workers per direction
    (forward/backward) per link. num_workers_per_link is exposed to the
    user, and defaults to 1 to mimic the non-fabric mux behavior.
    2) Introduce chunks_per_sync, which defines the number of tiles sent
    before a semaphore inc is sent. This defaults to the number of tiles in
    the full slice to mimic the non-chunked before.
    
    On the Stable Diffusion 3.5 shapes on 6U (in
    tests/nightly/tg/ccl/test_minimal_all_gather_async.py and
    tests/nightly/tg/ccl/test_minimal_reduce_scatter_async.py), bandwidth
    improves from:
    All Gather:
    1024x5120 output shape, 8 devices, fabric_1D line
    Non-fabric mux single link BW: 5.46 GB/s
    Fabric mux + 2 workers, single link BW: 7.82 GB/s
    Fabric mux + 2 workers + 10 chunks per sync (also scatter_write), single
    link BW: 10.01 GB/s
    
    Reduce Scatter:
    1024x2560 input shape, 4 devices, fabric_1D line
    Non-fabric mux single link BW: 5.25 GB/s
    Fabric mux + 2 workers, single link BW: 6.47 GB/s
    Fabric mux + 2 workers + 2 chunks per sync (tile_granularity is 8 and
    scatter_write), single link BW: 8.55 GB/s
    
    Similar bandwidth improvements for ring.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16536492495)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16536486943)
    CI with demo tests passes
    - [x] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/runs/16536479568)
    CI passes
    - [x] [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16536473549)
    CI passes
    - [x] [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16536467678)
    CI passes
    - [x] [T3000 nightly
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16536462813)
    CI passes
    - [x] [TG quick,demo,perf,nightly
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/pipeline-select-galaxy.yaml)
    CI passes
    
    ---------
    
    Co-authored-by: Colman Glagovich <cglagovich@tenstorrent.com>
    Co-authored-by: grechsteinerTT <grechsteiner@tenstorrent.com>

--- tests/nightly/tg/ccl/test_minimal_all_gather_async.py
+++ tests/nightly/tg/ccl/test_minimal_all_gather_async.py
@@ -4,31 +4,23 @@
 
 import pytest
 import ttnn
-from tests.nightly.t3000.ccl.test_minimal_reduce_scatter_async import run_reduce_scatter_impl
+from tests.nightly.t3000.ccl.test_minimal_all_gather_async import run_all_gather_impl
 
 
 @pytest.mark.parametrize("num_links", [3], ids=["3links"])
 @pytest.mark.parametrize(
-    "num_devices, rs_input_shape, dim, layout, rs_input_dtype",
+    "num_devices, ag_output_shape, dim, layout, ag_input_dtype",
     [
-        (8, [8, 1, 512, 2560], 3, ttnn.TILE_LAYOUT, ttnn.bfloat16),  # use batching when fused
-        (8, [4, 1, 1024, 2560], 3, ttnn.TILE_LAYOUT, ttnn.bfloat16),  # use batching when fused
-        (4, [1, 1, 1024, 2560], 3, ttnn.TILE_LAYOUT, ttnn.bfloat16),  # use batching when fused
-        (4, [1, 1, 352, 2560], 3, ttnn.TILE_LAYOUT, ttnn.bfloat16),  # use batching when fused
-        (8, [2, 1, 2048, 2560], 3, ttnn.TILE_LAYOUT, ttnn.bfloat16),  # use batching when fused
-        (8, [1, 1, 4096, 2560], 3, ttnn.TILE_LAYOUT, ttnn.bfloat16),  # use batching when fusedd
+        (8, [1, 1, 1024, 5120], 3, ttnn.TILE_LAYOUT, ttnn.bfloat16),
+        (8, [1, 1, 352, 5120], 3, ttnn.TILE_LAYOUT, ttnn.bfloat16),
     ],
     ids=[
-        "batch_8",
-        "batch_4",
-        "batch_1_sd35_spatial",
-        "batch_1_sd35_prompt",
-        "batch_2",
-        "batch_1",
+        "sd35_spatial",
+        "sd35_prompt",
     ],
 )
 @pytest.mark.parametrize(
-    "mem_config_input, mem_config_rs",
+    "mem_config_input, mem_config_ag",
     [
         (
             ttnn.MemoryConfig(ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.DRAM),
@@ -45,47 +37,47 @@ from tests.nightly.t3000.ccl.test_minimal_reduce_scatter_async import run_reduce
     ids=["perf", "check"],
 )
 @pytest.mark.parametrize(
-    "device_params, rs_topology",
+    "device_params, all_gather_topology",
     [
         ({"fabric_config": ttnn.FabricConfig.FABRIC_1D, "trace_region_size": 90112}, ttnn.Topology.Linear),
     ],
     indirect=["device_params"],
     ids=["fabric_linear"],
 )
-@pytest.mark.parametrize("chunks_per_sync", [2])
+@pytest.mark.parametrize("chunks_per_sync", [20])
 @pytest.mark.parametrize("num_workers_per_link", [2])
-@pytest.mark.parametrize("num_buffers_per_channel", [8])
+@pytest.mark.parametrize("num_buffers_per_channel", [2])
 @pytest.mark.parametrize("mesh_device", [(8, 4)], indirect=True)
-def test_reduce_scatter_async(
+def test_all_gather_async(
     mesh_device,
     num_devices,
-    num_links,
-    rs_input_shape,
+    ag_output_shape,
     dim,
+    num_links,
+    ag_input_dtype,
     layout,
-    rs_input_dtype,
     mem_config_input,
-    mem_config_rs,
+    mem_config_ag,
     enable_trace,
+    all_gather_topology,
     num_iters,
-    rs_topology,
     chunks_per_sync,
     num_workers_per_link,
     num_buffers_per_channel,
 ):
     submesh_device = mesh_device.create_submesh(ttnn.MeshShape((num_devices, 1)))
     cluster_axis = 0
-    run_reduce_scatter_impl(
+    run_all_gather_impl(
         submesh_device,
         num_devices,
-        rs_input_shape,
+        ag_output_shape,
         dim,
         num_links,
-        rs_input_dtype,
+        ag_input_dtype,
         layout,
         mem_config_input,
-        mem_config_rs,
-        rs_topology=rs_topology,
+        mem_config_ag,
+        all_gather_topology=all_gather_topology,
         enable_trace=enable_trace,
         num_iters=num_iters,
         cluster_axis=cluster_axis,
