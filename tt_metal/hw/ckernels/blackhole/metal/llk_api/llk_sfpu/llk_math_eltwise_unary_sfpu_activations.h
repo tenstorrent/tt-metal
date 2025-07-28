b62df820fc7a1f17efbe8a7860a7fd8ec47fe8a5
KalaivaniMCW <azayasankaran@tenstorrent.com>
2025-07-28 05:33:49 +0000

    BH update

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_activations.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_activations.h
@@ -41,15 +41,15 @@ inline void llk_math_eltwise_unary_sfpu_softsign(uint dst_index, int vector_mode
 template <bool APPROXIMATE>
 inline void llk_math_eltwise_unary_sfpu_celu_init() {
     llk_math_eltwise_unary_sfpu_init<SfpuType::celu, APPROXIMATE>(
-        // sfpu::exp_init<APPROXIMATE>);
-        ckernel::sfpu::_init_hardsigmoid_<APPROXIMATE>);
+        ckernel::sfpu::_init_exponential_<APPROXIMATE, APPROXIMATE, p_sfpu::kCONST_1_FP16B>);
 }
 
 template <bool APPROXIMATE, ckernel::ActivationType ACTIVATION, int ITERATIONS = 8>
 inline void llk_math_eltwise_unary_sfpu_celu(
     uint dst_index, uint32_t alpha, uint32_t alpha_recip, int vector_mode = (int)VectorMode::RC) {
     _llk_math_eltwise_unary_sfpu_params_<APPROXIMATE>(
-        static_cast<void (*)(float, float)>(ckernel::sfpu::_calculate_activation_<APPROXIMATE, ACTIVATION, ITERATIONS>),
+        static_cast<void (*)(uint32_t, uint32_t)>(
+            ckernel::sfpu::_calculate_activation_<APPROXIMATE, ACTIVATION, ITERATIONS>),
         dst_index,
         vector_mode,
         alpha,
