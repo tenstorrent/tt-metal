acde8820f9308c5e25aa277cf7cf4553497f3004
Nour Ardo <nardo@tenstorrent.com>
2025-07-28 16:07:39 -0400

    Profiling TM ops (#24812)
    ### Ticket
    Link to Github Issue
    https://github.com/tenstorrent/tt-metal/issues/18238
    
    ### Problem description
    Currently, the profiler assigns a latency of 1 for all data movement
    ops. However, the TM ops are actually bound by the latency of the noc
    transactions, and by the LLK compute time if applicable.
    
    ### What's changed
    In this PR, we are adding a general model that estimates the latency of
    TM ops based on the noc bandwidth and the tensor specs.
    The model was tested on a set of TM ops. The idea of the model is as
    follows: 
    1- Extract the specs from the input and output tensors and find the
    transaction size 
    2- Find the number of read and write transactions, and parallelize the
    work among worker cores while accounting for noc congestion 
    3- Calculate the latency needed by the noc transactions based on the
    excel sheet values
    (https://docs.google.com/spreadsheets/d/1nN1ZPUNc2f0d6YEenBOHS7BdkpGlBysZ6SCArinGOrE/edit?gid=2113527795#gid=2113527795)
    4- If the op has an LLK compute kernel, find its latency, and assign the
    final value to the maximum between the two. Note that the only LLK
    values added are the currently available ones, which are tilize/untilize
    ops for now
    	
    The details of the steps and assumptions of this roofline model can be
    found in this issue:
    https://github.com/tenstorrent/tt-metal/issues/24809
    
    Some op specific assumptions to avoid overestimation:
    - Transpose/permute ops write the output tensor per faceline if it is in
    tile layout and row invariant
    - Slice op only needs to read the non-sliced part of the tensor
    - Split output shape is similar to input shape because we need to
    account for all the output splits. Similar idea for concat op
    - Bcast op has two input tensors: we add the latency of reading the
    second tensor while accounting also for the specs of the first one
    
    
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/16509523357
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ttnn/cpp/ttnn/operations/data_movement/sharded/reshard/device/reshard_op.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/sharded/reshard/device/reshard_op.cpp
@@ -9,6 +9,7 @@
 #include <tt-metalium/buffer_types.hpp>
 #include <tt-metalium/constants.hpp>
 #include <tt-metalium/work_split.hpp>
+#include "ttnn/operations/data_movement/common/common.hpp"
 
 #include "reshard_program_factory.hpp"
 #include "nd_reshard_program_factory.hpp"
@@ -115,6 +116,19 @@ std::vector<ttnn::TensorSpec> ReshardDeviceOperation::compute_output_specs(
             output_mem_config,
             input_tensor.logical_shape(),
             input_tensor.padded_shape()))};
+}
+
+tt::tt_metal::operation::OpPerformanceModelGeneral<std::vector<Tensor>>
+ReshardDeviceOperation::create_op_performance_model(
+    const std::vector<Tensor>& input_tensors,
+    const std::vector<std::optional<const Tensor>>& optional_input_tensors,
+    std::vector<Tensor>& output_tensors) const {
+    const auto& input_tensor = input_tensors.at(0);
+    const auto& output_tensor = output_tensors.at(0);
+    int ideal_dev_clock_cycles = common_tm_bw_model(input_tensor, output_tensor);
+    tt::tt_metal::operation::OpPerformanceModelGeneral<std::vector<Tensor>> result(
+        input_tensors, output_tensors, ideal_dev_clock_cycles);
+    return result;
 }
 
 operation::ProgramWithCallbacks ReshardDeviceOperation::create_program(
