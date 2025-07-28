a20720003781a1bc536a49f64d4ca456f46c55f2
Tiamoesg <tiamoesg@gmail.com>
2025-07-28 17:38:57 -0500

    Update create_stubs.sh
    Added formatting to match install scripts

--- scripts/build_scripts/create_stubs.sh
+++ scripts/build_scripts/create_stubs.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
+PURPLE="\e[35m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+RESET="\e[0m"
+
 if [ -z "$PYTHON_ENV_DIR" ]; then
     PYTHON_ENV_DIR=$(pwd)/python_env
 fi
 
 sanity=0
-echo "!! Did you build metal and create python_env? !!"
+echo -e "${YELLOW}!! Did you build metal and create python_env? !!${RESET}"
 source $PYTHON_ENV_DIR/bin/activate
 python3 -c "import tt_lib" >/dev/null 2>&1; sanity+=$?
 python3 -c "import ttnn" >/dev/null 2>&1; sanity+=$?
 if [ $sanity -ne 0 ]; then
-    echo "!! tt_lib or ttnn not found. Please build+install metal and create python_env first !!"
+    echo -e "${YELLOW}!! tt_lib or ttnn not found. Please build+install metal and create python_env first !!${RESET}"
     exit 1
 fi
 
-echo "Generating stubs"
-stubgen -m tt_lib -m tt_lib.device -m tt_lib.profiler -m tt_lib.tensor -m tt_lib.operations -m tt_lib.operations.primary -m tt_lib.operations.primary.transformers -o tt_eager
+echo -e "${PURPLE}Generating stubs...${RESET}"
+stubgen -m tt_lib -m tt_lib.device -m tt_lib.profiler -m tt_lib.tensor \
+        -m tt_lib.operations -m tt_lib.operations.primary \
+        -m tt_lib.operations.primary.transformers -o tt_eager
+
+echo -e "${GREEN}Processed tt_lib modules. Stubs generated under tt_eager/tt_lib${RESET}"
+
 stubgen -p ttnn._ttnn -o ttnn
+echo -e "${GREEN}Processed ttnn modules. Stubs generated under ttnn/ttnn/_ttnn/${RESET}"
+
 sed -i 's/\._C/tt_lib/g' tt_eager/tt_lib/__init__.pyi
+echo -e "${GREEN}Stubs updated with tt_lib references${RESET}"
