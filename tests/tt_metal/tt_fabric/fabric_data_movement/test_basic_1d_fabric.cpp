9cc8f303603a11c11a2a8b34707e3bf5e5491cf5
Neel Nyamagoudar <nnyamagoudar@tenstorrent.com>
2025-07-28 11:27:09 -0700

    Adding 4kb packet support to 2D Fabric (#25665)
    ### Problem description
    As of right now, in 2D Fabric mode, we are using 2 tile packets due to
    size constraints, with 8 sender channel buffers.
    
    ### What's changed
    Added support for 4 tile packets, also modified the amount of sender
    channel buffers to 4 in order to avoid using too much bandwidth (2 tiles
    * 8 == 4 tiles* 4).
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
    CI passes (if applicable)
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- tests/tt_metal/tt_fabric/fabric_data_movement/test_basic_1d_fabric.cpp
+++ tests/tt_metal/tt_fabric/fabric_data_movement/test_basic_1d_fabric.cpp
@@ -66,7 +66,7 @@ WorkerMemMap generate_worker_mem_map(tt_metal::IDevice* device, Topology topolog
     uint32_t target_address = source_l1_buffer_address;
     uint32_t notification_mailbox_address = test_results_address + TEST_RESULTS_SIZE_BYTES;
 
-    uint32_t packet_payload_size_bytes = (topology == Topology::Mesh) ? 2048 : 4096;
+    uint32_t packet_payload_size_bytes = get_tt_fabric_max_payload_size_bytes();
 
     return {
         source_l1_buffer_address,
