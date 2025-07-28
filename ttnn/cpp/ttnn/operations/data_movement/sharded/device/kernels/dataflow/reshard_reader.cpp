3365137ab8aa180aee1f6ad153ee57d31c356ea9
nour ardo <nardo@tenstorrent.com>
2025-07-28 23:25:54 +0000

    reshard changes

--- ttnn/cpp/ttnn/operations/data_movement/sharded/device/kernels/dataflow/reshard_reader.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/sharded/device/kernels/dataflow/reshard_reader.cpp
@@ -10,14 +10,21 @@ void kernel_main() {
     constexpr uint32_t num_x_cores = get_compile_time_arg_val(1);
     constexpr uint32_t num_y_cores = get_compile_time_arg_val(2);
     constexpr uint32_t page_size = get_compile_time_arg_val(3);
+    constexpr uint32_t local_config_cb_id = get_compile_time_arg_val(4);
 
     uint32_t y_offset = num_x_cores;
 
+    const uint32_t local_config_data_l1_addr = get_read_ptr(local_config_cb_id);
+    const tt_l1_ptr uint32_t* local_config_data =
+        reinterpret_cast<const tt_l1_ptr uint32_t*>(local_config_data_l1_addr);
+    for (uint32_t i = 0; i < 23; i++) {
+        DPRINT << "RT ARG AT " << i << ": " << local_config_data[i] << "\n";
+    }
     uint32_t arg_index = num_x_cores + num_y_cores;
-    const uint32_t input_shard_addr = get_arg_val<uint32_t>(arg_index++);
-    const uint32_t num_output_pages = get_arg_val<uint32_t>(arg_index++);
-    const uint32_t num_ranges = get_arg_val<uint32_t>(arg_index++);
-    const uint32_t output_page_offset = get_arg_val<uint32_t>(arg_index++);
+    const uint32_t input_shard_addr = local_config_data[arg_index++];
+    const uint32_t num_output_pages = local_config_data[arg_index++];
+    const uint32_t num_ranges = local_config_data[arg_index++];
+    const uint32_t output_page_offset = local_config_data[arg_index++];
 
     uint32_t l1_write_addr = get_write_ptr(shard_cb) + output_page_offset * page_size;
 
@@ -25,16 +32,16 @@ void kernel_main() {
     uint32_t mask_short = 0x0ffff;  // 16 bits
 
     for (uint32_t range_id = 0; range_id < num_ranges; range_id++) {
-        const uint32_t core_start_stride = get_arg_val<uint32_t>(arg_index++);
+        const uint32_t core_start_stride = local_config_data[arg_index++];
         const uint32_t start_x_index = (core_start_stride >> 24);
         const uint32_t start_y_index = (core_start_stride >> 16) & mask_byte;
         const uint32_t stride_x = (core_start_stride >> 8) & mask_byte;
         const uint32_t stride_y = (core_start_stride)&mask_byte;
-        const uint32_t start_x = get_arg_val<uint32_t>(start_x_index);
-        const uint32_t start_y = get_arg_val<uint32_t>(y_offset + start_y_index);
+        const uint32_t start_x = local_config_data[start_x_index];
+        const uint32_t start_y = local_config_data[y_offset + start_y_index];
 
-        const uint32_t stride_data_offset = get_arg_val<uint32_t>(arg_index++);
-        const uint32_t stride_size_num_strides_skip = get_arg_val<uint32_t>(arg_index++);
+        const uint32_t stride_data_offset = local_config_data[arg_index++];
+        const uint32_t stride_size_num_strides_skip = local_config_data[arg_index++];
         const uint32_t num_strides = ((stride_size_num_strides_skip)&mask_short) >> 8;
         const bool skip = (((stride_size_num_strides_skip)&mask_byte) == 1);
 
@@ -49,8 +56,8 @@ void kernel_main() {
 
         for (uint32_t stride_idx = 0; stride_idx < num_strides; stride_idx++) {
             if (!skip) {
-                uint32_t core_id_x = get_arg_val<uint32_t>(core_id_x_index);
-                uint32_t core_id_y = get_arg_val<uint32_t>(y_offset + core_id_y_index);
+                uint32_t core_id_x = local_config_data[core_id_x_index];
+                uint32_t core_id_y = local_config_data[y_offset + core_id_y_index];
                 uint64_t noc_address = get_noc_addr(core_id_x, core_id_y, input_shard_addr + addr_offset);
                 noc_async_read(noc_address, l1_write_addr, stride_size);
                 l1_write_addr += stride_size;
