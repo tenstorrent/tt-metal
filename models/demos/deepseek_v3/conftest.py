3ce4c25f593a3f968fb45894ff87b4447c53027b
Bartłomiej Cieślar <37981302+barci2@users.noreply.github.com>
2025-07-29 12:16:44 +0200

    Tests for DeepSeekV3 with IO from a real model run (#25509)
    ### What's changed
    - A new test data source was added (generated by the
    generate_test_inputs_outputs.py script) which allows for testing the
    modules with data distributions as seen by the huggingface deepseek
    model. This is optimized to be able to run on the IRD servers without
    overflowing the RAM with the dequantized weights.
    - A test suite (for rms norm, mlp and embedding) refactor to use said
    data source
    - Tests for the SharedExpert and NonExpert MLPs
    
    ### Checklist
    [![All post-commit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml/badge.svg?branch=bcieslar%2Fdeepseek-real-io-tests)](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    [![(TG) TG DeepSeek
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-deepseek-tests.yaml/badge.svg?branch=bcieslar%2Fdeepseek-real-io-tests)](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-deepseek-tests.yaml)
    (failing because #25716 is not merged yet)

--- models/demos/deepseek_v3/conftest.py
+++ models/demos/deepseek_v3/conftest.py
@@ -1,18 +1,13 @@
 # SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC.
-
 # SPDX-License-Identifier: Apache-2.0
-import json
 import os
 from pathlib import Path
 
 import pytest
-import safetensors.torch
-import torch
 from loguru import logger
 from transformers import AutoConfig
 
 import ttnn
-from models.demos.deepseek_v3.scripts.generate_test_inputs_outputs import __file__ as REFERENCE_IO_SCRIPT_NAME
 from tests.scripts.common import get_updated_device_params
 
 
@@ -34,20 +29,21 @@ def mesh_device(request, device_params):
     import ttnn
 
     device_ids = ttnn.get_device_ids()
+    request.node.pci_ids = [ttnn.GetPCIeDeviceID(i) for i in device_ids]
 
-    if len(device_ids) == 32:
-        mesh_shape = ttnn.MeshShape(4, 8)
-    elif len(device_ids) == 8:
-        mesh_shape = ttnn.MeshShape(1, 8)
+    if len(device_ids) == 32:  # If running on Galaxy system
+        default_mesh_shape = ttnn.MeshShape(4, 8)
     else:
-        mesh_shape = ttnn.MeshShape(1, 1)
-        request.node.pci_ids = [ttnn.GetPCIeDeviceID(i) for i in device_ids[:1]]
+        default_mesh_shape = ttnn.MeshShape(1, len(device_ids))
 
     updated_device_params = get_updated_device_params(device_params)
+
     fabric_config = updated_device_params.pop("fabric_config", None)
     if fabric_config:
         ttnn.set_fabric_config(fabric_config)
-    mesh_device = ttnn.open_mesh_device(mesh_shape=mesh_shape, **updated_device_params)
+
+    updated_device_params.setdefault("mesh_shape", default_mesh_shape)
+    mesh_device = ttnn.open_mesh_device(**updated_device_params)
 
     logger.debug(f"multidevice with {mesh_device.get_num_devices()} devices is created with shape {mesh_device.shape}")
     yield mesh_device
@@ -66,63 +62,11 @@ def model_path():
     return Path(os.getenv("HF_MODEL", "models/demos/deepseek_v3/reference"))
 
 
-@pytest.fixture
-def reference_io(request):
-    param = getattr(request, "param", None)
-    if (
-        param is None
-        or not isinstance(param, tuple)
-        or len(param) != 2
-        or param[0] not in ["prefill", "decode"]
-        or not isinstance(param[1], str)
-    ):
-        raise ValueError(
-            "Reference IO fixture requires a mode ('prefill', 'decode') and a module path to load the reference IO for."
-        )
-    mode, module = param
-    path = (
-        Path(os.getenv("DEEPSEEK_V3_CACHE", "/proj_sw/user_dev/deepseek-v3-cache"))
-        / f"test_io_cache/{mode}.{module}.pt"
-    )
-    if not path.is_file():
-        raise FileNotFoundError(
-            f"Reference IO cache file not found at {path}. Please run the {REFERENCE_IO_SCRIPT_NAME} script to create it. Did you set the 'HF_MODEL' environment variable coorectly?"
-        )
-    return torch.load(path)
-
-
 @pytest.fixture(scope="session")
 def hf_config(model_path):
     """Load DeepSeek config for testing"""
     config = AutoConfig.from_pretrained(model_path, trust_remote_code=True)
     return config
-
-
-@pytest.fixture
-def state_dict(request, model_path):
-    param = getattr(request, "param", None)
-    if param is None or not isinstance(param, str):
-        raise ValueError(
-            "State dict fixture requires either a module path to load the weights for (empty string to load the entire state dict)."
-        )
-
-    if param:
-        param += "."  # So that the later matches include the separating dot
-
-    weight_paths = json.load(open(model_path / "model.safetensors.index.json", "r"))["weight_map"]
-    per_safetensor_weights = {}
-
-    for weight_name in weight_paths.keys():
-        if not weight_name.startswith(param):
-            continue
-        per_safetensor_weights.setdefault(weight_paths[weight_name], []).append(weight_name)
-
-    return {
-        weight_name[len(param) :]: safetensor_state_dict[weight_name]
-        for safetensor_file_path, weight_names in per_safetensor_weights.items()
-        for safetensor_state_dict in [safetensors.torch.load_file(model_path / safetensor_file_path)]
-        for weight_name in weight_names
-    }
 
 
 @pytest.fixture
---
+++ models/demos/deepseek_v3/utils/test_utils.py
@@ -0,0 +1,140 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import json
+import os
+from pathlib import Path
+from typing import Any, Literal
+
+import safetensors.torch
+import torch
+from loguru import logger
+
+import ttnn
+from models.demos.deepseek_v3.scripts.generate_test_inputs_outputs import __file__ as REFERENCE_IO_SCRIPT_NAME
+from models.demos.deepseek_v3.utils.abstract_module import AbstractModule
+from models.demos.deepseek_v3.utils.config_helpers import SEQ_LEN_CHUNK_SIZE
+from models.utility_functions import comp_pcc
+
+
+def load_state_dict(model_path: Path, module_path: str):
+    if not not module_path:
+        module_path += "."  # So that the later matches include the separating dot
+
+    weight_paths = json.load(open(model_path / "model.safetensors.index.json", "r"))["weight_map"]
+    per_safetensor_weights = {}
+
+    for weight_name in weight_paths.keys():
+        if not weight_name.startswith(module_path):
+            continue
+        per_safetensor_weights.setdefault(weight_paths[weight_name], []).append(weight_name)
+
+    return {
+        weight_name[len(module_path) :]: safetensor_state_dict[weight_name]
+        for safetensor_file_path, weight_names in per_safetensor_weights.items()
+        for safetensor_state_dict in [safetensors.torch.load_file(model_path / safetensor_file_path)]
+        for weight_name in weight_names
+    }
+
+
+def load_reference_io_tensors_for_module(
+    mode: Literal["prefill", "decode"],
+    module: str,
+    seq_len: int,
+    concat_dim: int = 1,
+) -> tuple[torch.Tensor, torch.Tensor]:
+    assert mode in ["prefill", "decode"], f"Unsupported mode: {mode}"
+
+    reference_io = load_reference_io(mode, module)
+    assert all(len(logs) <= 1 for logs in reference_io), f"Expected a non-range module"
+    assert all(
+        len(logs) > 0 for logs in reference_io
+    ), f"Some logs for module {module} {mode} not generated. Please run the {REFERENCE_IO_SCRIPT_NAME} script to create it."
+    if mode == "prefill":
+        assert len(reference_io) == 1, f"Expected one log for {module} prefill, got {len(reference_io)}"
+        (((io_module_path, (torch_input,), _, reference_output),),) = reference_io
+        assert io_module_path == module
+    else:
+        io_module_paths, torch_args, _, reference_outputs = zip(*[logs[0] for logs in reference_io])
+        (torch_inputs,) = zip(*torch_args)
+        assert set(io_module_paths) == {module}
+        torch_input = torch.concat(torch_inputs, dim=concat_dim)
+        reference_output = torch.concat(reference_outputs, dim=concat_dim)
+    return pad_tensor(torch_input, mode, seq_len), reference_output
+
+
+def load_reference_io(mode: Literal["prefill", "decode"], module_range: str):
+    path = (
+        Path(os.getenv("DEEPSEEK_V3_CACHE", "/proj_sw/user_dev/deepseek-v3-cache"))
+        / f"test_io_cache/{mode}.{module_range}.pt"
+    )
+    if not path.is_file():
+        raise FileNotFoundError(
+            f"Reference IO cache file not found at {path}. Please run the {REFERENCE_IO_SCRIPT_NAME} script to create it. Did you set the 'HF_MODEL' environment variable coorectly?"
+        )
+    return torch.load(path)
+
+
+def pad_tensor(tensor: torch.Tensor, mode: Literal["prefill", "decode"], seq_len: int) -> torch.Tensor:
+    assert mode in ["prefill", "decode"], f"Unsupported mode: {mode}"
+
+    tensor_seq_len = tensor.shape[-2]
+    seq_len = min(seq_len, tensor_seq_len)
+    if mode == "decode" or seq_len < SEQ_LEN_CHUNK_SIZE:
+        return tensor[..., :seq_len, :].clone()
+
+    padded_seq_len = ttnn.core.roundup(seq_len, SEQ_LEN_CHUNK_SIZE)
+    padded_tensor_shape = list(tensor.shape)
+    padded_tensor_shape[-2] = padded_seq_len
+
+    padded_tensor = torch.zeros(padded_tensor_shape, dtype=tensor.dtype, device=tensor.device)
+    padded_tensor[..., : min(padded_seq_len, tensor_seq_len), :] = tensor
+
+    return padded_tensor
+
+
+def get_model_config(ModuleClass: type[AbstractModule], mode: Literal["prefill", "decode"], *args, **kwargs) -> Any:
+    """Get the module config for the given mode and kwargs."""
+    if mode == "prefill":
+        return ModuleClass.prefill_model_config(*args, **kwargs)
+    elif mode == "decode":
+        return ModuleClass.decode_model_config(*args, **kwargs)
+    else:
+        raise ValueError(f"Unsupported mode: {mode}. Supported modes are 'prefill' and 'decode'.")
+
+
+def run_module_forward(ModuleClass: type[AbstractModule], mode: Literal["prefill", "decode"], *args, **kwargs) -> Any:
+    """Run the module forward pass for the given mode and kwargs."""
+    if mode == "prefill":
+        return ModuleClass.forward_prefill(*args, **kwargs)
+    elif mode == "decode":
+        return ModuleClass.forward_decode(*args, **kwargs)
+    else:
+        raise ValueError(f"Unsupported mode: {mode}. Supported modes are 'prefill' and 'decode'.")
+
+
+def assert_hidden_dim_pcc(
+    tt_output_torch: torch.Tensor, reference_output: torch.Tensor, pcc_required: float = 0.98
+) -> float:
+    assert (
+        tt_output_torch.ndim == reference_output.ndim == 4
+    ), f"Both model and reference outputs must be 4D tensors; got {tt_output_torch.ndim}D and {reference_output.ndim}D instead"
+    assert (
+        tt_output_torch.shape[0] == reference_output.shape[0]
+    ), f"Model and reference output shape mismatch on dim 0 ({tt_output_torch.shape[0]} != {reference_output.shape[0]})"
+    assert (
+        tt_output_torch.shape[1] == reference_output.shape[1]
+    ), f"Model and reference output shape mismatch on dim 1 ({tt_output_torch.shape[1]} != {reference_output.shape[1]})"
+    assert (
+        tt_output_torch.shape[3] == reference_output.shape[3]
+    ), f"Model and reference output shape mismatch on dim 3 ({tt_output_torch.shape[3]} != {reference_output.shape[3]})"
+
+    seq_len = min(tt_output_torch.shape[2], reference_output.shape[2])
+    tt_output_torch = tt_output_torch[:, :, :seq_len, :]
+    reference_output = reference_output[:, :, :seq_len, :]
+
+    passing, pcc = comp_pcc(tt_output_torch, reference_output, pcc_required)
+    logger.info(f"PCC: {pcc}")
+    assert passing, f"Pearson Correlation Coefficient {pcc} is below required {pcc_required}."
+    return pcc
