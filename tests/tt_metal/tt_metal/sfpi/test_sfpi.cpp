9878909307997c4e1e7b010f0f6cbe787759e904
Aditya Saigal <129097327+tt-asaigal@users.noreply.github.com>
2025-07-29 16:02:48 -0700

    Uplift additional TT-Metal tests to use TT-Mesh APIs (#25967)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/22835
    
    ### Problem description
    Additional tests need to be migrated to use TT-Mesh APIs, as part of the
    legacy API deprecation effort.
    
    ### What's changed
    - Migrate the following
    
    ```
    tests/tt_metal/tt_metal/device/test_device_init_and_teardown.cpp
    tests/tt_metal/tt_metal/sfpi/test_sfpi.cpp
    tests/tt_metal/tt_metal/test_eltwise_binary.cpp
    ```
    
    - Delete the following tests since they were not being built or run
    (these are also extremely redundant)
    ```
    tests/tt_metal/tt_metal/test_enqueue_program.cpp
    tests/tt_metal/tt_metal/test_matmul_multi_core_multi_dram.cpp
    ```
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
    CI passes (if applicable)
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- tests/tt_metal/tt_metal/sfpi/test_sfpi.cpp
+++ tests/tt_metal/tt_metal/sfpi/test_sfpi.cpp
@@ -19,18 +19,26 @@
 #include <string_view>
 
 #include <gtest/gtest.h>
+#include <tt-metalium/distributed.hpp>
 
 namespace {
 
 // We recursively scan this directory for kernels named '*.cpp'.
 constexpr std::string_view KernelDir = "tests/tt_metal/tt_metal/test_kernels/sfpi";
 
-bool runTest(tt::tt_metal::IDevice* device, const CoreCoord& coord, const std::string& path, unsigned baseLen) {
-    uint32_t args_addr = device->allocator()->get_base_allocator_addr(tt::tt_metal::HalMemType::L1);
+using namespace tt::tt_metal;
+
+bool runTest(
+    std::shared_ptr<distributed::MeshDevice> mesh_device,
+    const CoreCoord& coord,
+    const std::string& path,
+    unsigned baseLen) {
+    uint32_t args_addr = mesh_device->allocator()->get_base_allocator_addr(tt::tt_metal::HalMemType::L1);
 
     std::vector<uint32_t> compile_args{args_addr};
 
     auto program(tt::tt_metal::CreateProgram());
+    distributed::MeshWorkload workload = distributed::CreateMeshWorkload();
     auto kernel = CreateKernel(
         program,
         path,
@@ -38,11 +46,14 @@ bool runTest(tt::tt_metal::IDevice* device, const CoreCoord& coord, const std::s
         tt::tt_metal::ComputeConfig{
             .compile_args = compile_args,
         });
-    EnqueueProgram(device->command_queue(), program, false);
-    Finish(device->command_queue());
+    distributed::AddProgramToMeshWorkload(
+        workload, std::move(program), distributed::MeshCoordinateRange(mesh_device->shape()));
+    distributed::EnqueueMeshWorkload(mesh_device->mesh_command_queue(), workload, false);
 
-    tt::tt_metal::MetalContext::instance().get_cluster().l1_barrier(device->id());
-    auto noc_xy = device->worker_core_from_logical_core(coord);
+    distributed::Finish(mesh_device->mesh_command_queue());
+
+    tt::tt_metal::MetalContext::instance().get_cluster().l1_barrier(mesh_device->get_devices()[0]->id());
+    auto noc_xy = mesh_device->worker_core_from_logical_core(coord);
     unsigned expected = 0;
     // If path ends in -[digits], extract the expected value
     // If we need more sofphisticate tuning, we should add tags to the
@@ -57,7 +68,8 @@ bool runTest(tt::tt_metal::IDevice* device, const CoreCoord& coord, const std::s
         }
         expected |= 0x4000;
     }
-    std::vector<uint32_t> args = tt::llrt::read_hex_vec_from_core(device->id(), noc_xy, args_addr, sizeof(uint32_t));
+    std::vector<uint32_t> args =
+        tt::llrt::read_hex_vec_from_core(mesh_device->get_devices()[0]->id(), noc_xy, args_addr, sizeof(uint32_t));
     unsigned result = args[0];
     bool pass = result == expected;
     if (pass) {
@@ -72,7 +84,10 @@ bool runTest(tt::tt_metal::IDevice* device, const CoreCoord& coord, const std::s
 }
 
 bool runTests(
-    tt::tt_metal::IDevice* device, const tt::tt_metal::CoreCoord coord, std::string& path, unsigned baseLen) {
+    std::shared_ptr<distributed::MeshDevice> mesh_device,
+    const tt::tt_metal::CoreCoord coord,
+    std::string& path,
+    unsigned baseLen) {
     bool pass = true;
     std::vector<std::string> files;
     std::vector<std::string> dirs;
@@ -90,13 +105,13 @@ bool runTests(
     path.push_back('/');
     for (const auto& file : files) {
         path.append(file);
-        pass &= runTest(device, coord, path, baseLen);
+        pass &= runTest(mesh_device, coord, path, baseLen);
         path.erase(path.size() - file.size());
     }
 
     for (const auto& dir : dirs) {
         path.append(dir);
-        pass &= runTests(device, coord, path, baseLen);
+        pass &= runTests(mesh_device, coord, path, baseLen);
         path.erase(path.size() - dir.size());
     }
     path.pop_back();
@@ -104,7 +119,7 @@ bool runTests(
     return pass;
 }
 
- bool runTestsuite(tt::tt_metal::IDevice* device, const tt::tt_metal::CoreCoord coord) {
+bool runTestsuite(std::shared_ptr<distributed::MeshDevice> mesh_device, const tt::tt_metal::CoreCoord coord) {
     std::string path;
     if (auto* var = std::getenv("TT_METAL_HOME")) {
         path.append(var);
@@ -113,16 +128,15 @@ bool runTests(
         }
     }
     path.append(KernelDir);
-    return runTests(device, coord, path, path.find_last_of('/') + 1);
+    return runTests(mesh_device, coord, path, path.find_last_of('/') + 1);
 }
 
 using tt::tt_metal::CommandQueueSingleCardProgramFixture;
 
-TEST_F(CommandQueueSingleCardProgramFixture, TensixSFPI) {
+TEST_F(UnitMeshCQFixture, TensixSFPI) {
     CoreCoord core{0, 0};
-    for (auto* device : devices_) {
-        EXPECT_TRUE(runTestsuite(device, core));
+    for (auto mesh_device : devices_) {
+        EXPECT_TRUE(runTestsuite(mesh_device, core));
     }
 }
-
 }
