54e09bb53ecfaa965115ebdc822b869d281a7fb5
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2025-07-29 12:20:59 +0000

    #0: Improve accuracy of exp

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_exp.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_exp.h
@@ -13,6 +13,32 @@ namespace sfpu {
 
 sfpi_inline sfpi::vFloat sfpu_exp(sfpi::vFloat val) { return _sfpu_exp_(val); }
 
+sfpi_inline sfpi::vFloat _sfpu_exp_21f_(sfpi::vFloat val) {
+    sfpi::vFloat y = 0.0f;
+    v_if(val > -88.0f) {
+        sfpi::vInt z = sfpu::_float_to_int32_(val * sfpi::vFloat(0x00b8aa3b) + sfpi::vFloat(0x3f800000));
+        sfpi::vInt zii = exexp(sfpi::reinterpret<sfpi::vFloat>(z));         // Extract exponent
+        sfpi::vInt zif = sfpi::exman9(sfpi::reinterpret<sfpi::vFloat>(z));  // Extract mantissa
+
+        constexpr float CONST_D1 = 0.40196114e-7f;
+        constexpr int CONST_D2 = 0xf94ee7;
+        constexpr int CONST_D3 = 0x560;
+
+        sfpi::vFloat d1 = sfpi::vFloat(CONST_D1);
+        sfpi::vFloat d2 = sfpi::int32_to_float(sfpi::vInt(CONST_D2) + zif, 0);
+        sfpi::vFloat d3 = sfpi::int32_to_float(sfpi::vInt(CONST_D3) + zif, 0);
+        d2 = d1 * d2;
+        zif = sfpu::_float_to_int32_(d2 * d3);
+
+        zii = sfpi::reinterpret<sfpi::vInt>(
+            sfpi::setexp(sfpi::reinterpret<sfpi::vFloat>(zif), 127U + zii));  // restore exponent
+
+        y = sfpi::reinterpret<sfpi::vFloat>(zii);
+    }
+    v_endif;
+    return y;
+}
+
 template <
     bool APPROXIMATION_MODE,
     bool FAST_APPROX,
@@ -20,8 +46,20 @@ template <
     int ITERATIONS = 8,
     bool SKIP_POSITIVE_CHECK = false>
 void calculate_exponential(const uint exp_base_scale_factor = p_sfpu::kCONST_1_FP16B) {
-    _calculate_exponential_<APPROXIMATION_MODE, SCALE_EN, ITERATIONS, FAST_APPROX, SKIP_POSITIVE_CHECK>(
-        exp_base_scale_factor);
+    if constexpr (APPROXIMATION_MODE) {
+        _calculate_exponential_<APPROXIMATION_MODE, SCALE_EN, ITERATIONS, FAST_APPROX, SKIP_POSITIVE_CHECK>(
+            exp_base_scale_factor);
+    } else {
+        for (int d = 0; d < ITERATIONS; d++) {
+            sfpi::vFloat val = sfpi::dst_reg[0];
+            if constexpr (SCALE_EN) {
+                val = val * sfpi::s2vFloat16b(exp_base_scale_factor);
+            }
+            sfpi::vFloat result = _sfpu_exp_21f_(val);
+            sfpi::dst_reg[0] = result;
+            sfpi::dst_reg++;
+        }
+    }
 }
 
 template <bool APPROXIMATION_MODE, bool FAST_APPROX, uint32_t scale = 0x3F800000>
