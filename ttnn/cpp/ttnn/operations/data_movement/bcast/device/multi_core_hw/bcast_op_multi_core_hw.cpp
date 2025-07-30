tenstorrent/tt-metal

commit 3f306b05561b768c699072ea668edbb75a5c89c5

#25975: Migrate bcast and binary eltwise kernels to use TensorAccessor (#25976)
### Ticket
https://github.com/tenstorrent/tt-metal/issues/25975

### Problem description
We need to migrate all kernels to use TensorAccessor instead of
InterleavedAddrGen to make them support ND sharding. This is the first
step of adding universal sharding support to all OPs

### What's changed
Updated all broadcast and eltwise unary kernels to use TensorAccessor
Updated corresponding program factories to pass different compile time
arguments

### Checklist
- [x] [All post commit CI
passes](https://github.com/tenstorrent/tt-metal/actions/runs/16609714250)
- [x] New/Existing tests provide coverage for changes

ttnn/cpp/ttnn/operations/data_movement/bcast/device/multi_core_hw/bcast_op_multi_core_hw.cpp
@@ -11,6 +11,7 @@
 
 #include <tt-metalium/constants.hpp>
 #include <tt-metalium/util.hpp>
+#include <tt-metalium/tensor_accessor_args.hpp>
 
 using namespace tt;
 using namespace constants;
@@ -110,9 +111,9 @@ operation::ProgramWithCallbacks bcast_multi_core_hw(
     }
     auto cb_output = tt_metal::CreateCircularBuffer(program, all_device_cores, output_cb_config);
 
-    bool src0_is_dram = src0_buffer->buffer_type() == tt_metal::BufferType::DRAM;
-    bool src1_is_dram = src1_buffer->buffer_type() == tt_metal::BufferType::DRAM;
-    std::vector<uint32_t> reader_compile_time_args = {(uint32_t)src0_is_dram, (uint32_t)src1_is_dram};
+    std::vector<uint32_t> reader_compile_time_args;
+    TensorAccessorArgs(*src0_buffer).append_to(reader_compile_time_args);
+    TensorAccessorArgs(*src1_buffer).append_to(reader_compile_time_args);
 
     bool dst_is_dram = dst_buffer->buffer_type() == tt_metal::BufferType::DRAM;
     std::vector<uint32_t> writer_compile_time_args = {(std::uint32_t)output_cb_index, (std::uint32_t)dst_is_dram};

tests/ttnn/unit_tests/gtests/test_generic_op.cpp
@@ -8,6 +8,7 @@
 #include <tt_metal/api/tt-metalium/assert.hpp>
 #include <tt-metalium/program_descriptors.hpp>
 #include <tt-metalium/constants.hpp>
+#include <tt-metalium/tensor_accessor_args.hpp>
 
 #include <tt-logger/tt-logger.hpp>
 #include "ttnn_test_fixtures.hpp"
@@ -305,10 +306,9 @@ TEST_F(TTNNFixtureWithDevice, DISABLED_TestGenericOpBinaryEltwiseAdd) {
     };
 
     bool block_or_width_sharded = false;
-    uint32_t src0_is_dram = device_input_tensor_a.buffer()->buffer_type() == tt::tt_metal::BufferType::DRAM ? 1 : 0;
-    uint32_t src1_is_dram = device_input_tensor_b.buffer()->buffer_type() == tt::tt_metal::BufferType::DRAM ? 1 : 0;
-    const KernelDescriptor::CompileTimeArgs reader_compile_time_args = {
-        src0_is_dram, src1_is_dram, (uint32_t)block_or_width_sharded};
+    KernelDescriptor::CompileTimeArgs reader_compile_time_args = {(uint32_t)block_or_width_sharded};
+    TensorAccessorArgs(*device_input_tensor_a.buffer()).append_to(reader_compile_time_args);
+    TensorAccessorArgs(*device_input_tensor_b.buffer()).append_to(reader_compile_time_args);
 
     uint32_t dst_is_dram = device_output_tensor.buffer()->buffer_type() == tt::tt_metal::BufferType::DRAM ? 1 : 0;
     const KernelDescriptor::CompileTimeArgs writer_compile_time_args = {dst_cb_index, dst_is_dram};

tt_metal/hw/inc/dataflow_api.h
@@ -973,6 +973,26 @@ FORCE_INLINE void noc_async_read_tile(
  * Refer to template <typename AddrGen> noc_async_read_page for a generic implementation and more details.
  */
 // clang-format on
+template <typename DSpec>
+[[deprecated("Use <typename AddrGen> noc_async_read_page instead.")]]
+FORCE_INLINE void noc_async_read_tile(
+    const uint32_t id,
+    const TensorAccessor<DSpec>& addrgen,
+    uint32_t dst_local_l1_addr,
+    uint32_t offset = 0,
+    uint8_t noc = noc_index) {
+    noc_async_read_page<TensorAccessor<DSpec>>(id, addrgen, dst_local_l1_addr, offset, noc);
+}
+
+// clang-format off
+/**
+ * THIS API IS DEPRECATED AND WILL BE REMOVED SOON. Use <typename AddrGen> noc_async_read_page instead.
+ *
+ * Initiates an asynchronous read for a single packet with transaction size and source location determined by the InterleavedAddrGen object.
+ * This function is a convenience wrapper around noc_async_read_page for InterleavedAddrGen objects.
+ * Refer to template <typename AddrGen> noc_async_read_page for a generic implementation and more details.
+ */
+// clang-format on
 template <bool DRAM>
 [[deprecated("Use <typename AddrGen> noc_async_read_page instead.")]]
 FORCE_INLINE void noc_async_read_page(

ttnn/cpp/ttnn/operations/data_movement/bcast/device/kernels/dataflow/reader_bcast_h_interleaved.cpp
@@ -16,8 +16,8 @@ void kernel_main() {
     uint32_t Wt = get_arg_val<uint32_t>(11);
     uint32_t nc1 = get_arg_val<uint32_t>(12);  // if 1 we expect the bcast tensor to have NC=1
 
-    constexpr bool src0_is_dram = get_compile_time_arg_val(0) == 1;
-    constexpr bool src1_is_dram = get_compile_time_arg_val(1) == 1;
+    constexpr auto src0_args = TensorAccessorArgs<0>();
+    constexpr auto src1_args = TensorAccessorArgs<src0_args.next_compile_time_args_offset()>();
 
     constexpr uint32_t cb_id_in0 = 0;
     constexpr uint32_t cb_id_in1 = 1;
@@ -25,13 +25,9 @@ void kernel_main() {
 
     // single-tile ublocks
     const uint32_t tile_bytes = get_tile_size(cb_id_in0);
-    const DataFormat data_format = get_dataformat(cb_id_in0);
 
-    const InterleavedAddrGenFast<src0_is_dram> s0 = {
-        .bank_base_address = src0_addr, .page_size = tile_bytes, .data_format = data_format};
-
-    const InterleavedAddrGenFast<src1_is_dram> s1 = {
-        .bank_base_address = src1_addr, .page_size = tile_bytes, .data_format = data_format};
+    const auto s0 = TensorAccessor(src0_args, src0_addr, tile_bytes);
+    const auto s1 = TensorAccessor(src1_args, src1_addr, tile_bytes);
 
     uint32_t l1_write_addr_in0;
     uint32_t l1_write_addr_in1;

ttnn/cpp/ttnn/operations/data_movement/bcast/device/kernels/dataflow/reader_bcast_h_interleaved_input_rows_partitioned.cpp
@@ -18,8 +18,8 @@ void kernel_main() {
     uint32_t start_id = get_arg_val<uint32_t>(13);
     uint32_t HtWt = get_arg_val<uint32_t>(14);  // HtWt of input tensor
 
-    constexpr bool src0_is_dram = get_compile_time_arg_val(0) == 1;
-    constexpr bool src1_is_dram = get_compile_time_arg_val(1) == 1;
+    constexpr auto src0_args = TensorAccessorArgs<0>();
+    constexpr auto src1_args = TensorAccessorArgs<src0_args.next_compile_time_args_offset()>();
 
     constexpr uint32_t cb_id_in0 = 0;
     constexpr uint32_t cb_id_in1 = 1;
@@ -27,15 +27,10 @@ void kernel_main() {
 
     // single-tile ublocks
     const uint32_t in0_tile_bytes = get_tile_size(cb_id_in0);
-    const DataFormat in0_data_format = get_dataformat(cb_id_in0);
     const uint32_t in1_tile_bytes = get_tile_size(cb_id_in1);
-    const DataFormat in1_data_format = get_dataformat(cb_id_in1);
 
-    const InterleavedAddrGenFast<src0_is_dram> s0 = {
-        .bank_base_address = src0_addr, .page_size = in0_tile_bytes, .data_format = in0_data_format};
-
-    const InterleavedAddrGenFast<src1_is_dram> s1 = {
-        .bank_base_address = src1_addr, .page_size = in1_tile_bytes, .data_format = in1_data_format};
+    const auto s0 = TensorAccessor(src0_args, src0_addr, in0_tile_bytes);
+    const auto s1 = TensorAccessor(src1_args, src1_addr, in1_tile_bytes);
 
     uint32_t l1_write_addr_in0;
     uint32_t l1_write_addr_in1;

ttnn/cpp/ttnn/operations/data_movement/bcast/device/kernels/dataflow/reader_bcast_h_sharded.cpp
@@ -13,9 +13,8 @@ void kernel_main() {
     uint32_t NC = get_arg_val<uint32_t>(4);
     uint32_t batch_offset = get_arg_val<uint32_t>(5);  // if weight has multiple batches
 
-    // constexpr bool src0_is_dram = get_compile_time_arg_val(0) == 1;
-    constexpr bool src1_is_dram = get_compile_time_arg_val(1) == 1;
     constexpr uint32_t cb_id_in0 = get_compile_time_arg_val(0);
+    constexpr auto src1_args = TensorAccessorArgs<1>();
 
     // constexpr uint32_t cb_id_in0 = 0;
     constexpr uint32_t cb_id_in1 = 1;
@@ -23,10 +22,8 @@ void kernel_main() {
 
     // single-tile ublocks
     const uint32_t tile_bytes = get_tile_size(cb_id_in1);
-    const DataFormat data_format = get_dataformat(cb_id_in1);
 
-    const InterleavedAddrGenFast<src1_is_dram> s1 = {
-        .bank_base_address = src1_addr, .page_size = tile_bytes, .data_format = data_format};
+    const auto s1 = TensorAccessor(src1_args, src1_addr, tile_bytes);
 
     uint32_t l1_write_addr_in0;
     uint32_t l1_write_addr_in1;

ttnn/cpp/ttnn/operations/data_movement/bcast/device/kernels/dataflow/reader_bcast_h_sharded_optimised.cpp
@@ -14,9 +14,8 @@ void kernel_main() {
     uint32_t w_blk = get_arg_val<uint32_t>(5);
     uint32_t batch_b = get_arg_val<uint32_t>(6);
 
-    // constexpr bool src0_is_dram = get_compile_time_arg_val(0) == 1;
-    constexpr bool src1_is_dram = get_compile_time_arg_val(1) == 1;
     constexpr uint32_t cb_id_in0 = get_compile_time_arg_val(0);
+    constexpr auto src1_args = TensorAccessorArgs<1>();
 
     // constexpr uint32_t cb_id_in0 = 0;
     constexpr uint32_t cb_id_in1 = 1;
@@ -24,10 +23,8 @@ void kernel_main() {
 
     // single-tile ublocks
     const uint32_t tile_bytes = get_tile_size(cb_id_in1);
-    const DataFormat data_format = get_dataformat(cb_id_in1);
 
-    const InterleavedAddrGenFast<src1_is_dram> s1 = {
-        .bank_base_address = src1_addr, .page_size = tile_bytes, .data_format = data_format};
+    const auto s1 = TensorAccessor(src1_args, src1_addr, tile_bytes);
 
     uint32_t l1_write_addr_in0;
     uint32_t l1_write_addr_in1;

ttnn/cpp/ttnn/operations/data_movement/bcast/device/kernels/dataflow/reader_bcast_hw_interleaved.cpp
@@ -17,10 +17,11 @@ void kernel_main() {
     uint32_t nc1 = get_arg_val<uint32_t>(12);  // if 1 we expect the bcast tensor to have NC=1 and wrap around in NC
 
 #ifndef IN0_SHARDED
-    constexpr bool src0_is_dram = get_compile_time_arg_val(0) == 1;
+    constexpr auto src0_args = TensorAccessorArgs<0>();
+    constexpr auto src1_args = TensorAccessorArgs<src0_args.next_compile_time_args_offset()>();
+#else
+    constexpr auto src1_args = TensorAccessorArgs<0>();
 #endif
-
-    constexpr bool src1_is_dram = get_compile_time_arg_val(1) == 1;
 
     constexpr uint32_t cb_id_in0 = 0;
     constexpr uint32_t cb_id_in1 = 1;
@@ -28,9 +29,7 @@ void kernel_main() {
 
     // single-tile ublocks
     const uint32_t in0_tile_bytes = get_tile_size(cb_id_in0);
-    const DataFormat in0_data_format = get_dataformat(cb_id_in0);
     const uint32_t in1_tile_bytes = get_tile_size(cb_id_in1);
-    const DataFormat in1_data_format = get_dataformat(cb_id_in1);
 
     uint32_t l1_write_addr_in0;
     uint32_t l1_write_addr_in1;
@@ -40,15 +39,13 @@ void kernel_main() {
 
 #ifndef IN0_SHARDED
     uint32_t i = 0;
-    const InterleavedAddrGenFast<src0_is_dram> s0 = {
-        .bank_base_address = src0_addr, .page_size = in0_tile_bytes, .data_format = in0_data_format};
+    const auto s0 = TensorAccessor(src0_args, src0_addr, in0_tile_bytes);
 #else
     cb_reserve_back(cb_id_in0, num_tiles);
     cb_push_back(cb_id_in0, num_tiles);
 #endif
 
-    const InterleavedAddrGenFast<src1_is_dram> s1 = {
-        .bank_base_address = src1_addr, .page_size = in1_tile_bytes, .data_format = in1_data_format};
+    const auto s1 = TensorAccessor(src1_args, src1_addr, in1_tile_bytes);
 
 #ifdef BCAST_SCALAR
     cb_reserve_back(cb_id_in1, onetile);

commit b99ef20d1f40cebf054acde307d89eea2d748591

Fix Blackhole APC tests related to recent migration to TensorAccessor (#26037)
### Ticket

### Problem description
Currently Blackhole APC tests are broken by
https://github.com/tenstorrent/tt-metal/pull/25976
The issue is caused by accidentally passing incorrect compile time
arguments in some cases

### What's changed
Updated `bcast_op_multi_core_hw.cpp` to pass correct arguments to the
reader kernel

### Checklist
- [x] [All post commit CI
passes](https://github.com/tenstorrent/tt-metal/actions/runs/16633949935)
- [x] [Blackhole Post commit CI with demo tests
passes](http://github.com/tenstorrent/tt-metal/actions/runs/16633957431)
- [x] New/Existing tests provide coverage for changes

ttnn/cpp/ttnn/operations/data_movement/bcast/device/multi_core_hw/bcast_op_multi_core_hw.cpp
@@ -111,14 +111,11 @@ operation::ProgramWithCallbacks bcast_multi_core_hw(
     }
     auto cb_output = tt_metal::CreateCircularBuffer(program, all_device_cores, output_cb_config);
 
-    std::vector<uint32_t> reader_compile_time_args;
-    TensorAccessorArgs(*src0_buffer).append_to(reader_compile_time_args);
-    TensorAccessorArgs(*src1_buffer).append_to(reader_compile_time_args);
-
     bool dst_is_dram = dst_buffer->buffer_type() == tt_metal::BufferType::DRAM;
     std::vector<uint32_t> writer_compile_time_args = {(std::uint32_t)output_cb_index, (std::uint32_t)dst_is_dram};
 
     std::map<std::string, std::string> reader_defines;
+    std::vector<uint32_t> reader_compile_time_args;
     std::map<std::string, std::string> bcast_compute_defines = bcast_op_utils::get_defines(BcastOpDim::HW, bcast_math);
     if (bnc1) {
         reader_defines["BCAST_SCALAR"] = "1";
@@ -126,7 +123,10 @@ operation::ProgramWithCallbacks bcast_multi_core_hw(
     }
     if (src0_sharded) {
         reader_defines["IN0_SHARDED"] = "1";
+    } else {
+        TensorAccessorArgs(*src0_buffer).append_to(reader_compile_time_args);
     }
+    TensorAccessorArgs(*src1_buffer).append_to(reader_compile_time_args);
     KernelHandle binary_reader_kernel_id = tt_metal::CreateKernel(
         program,
         "ttnn/cpp/ttnn/operations/data_movement/bcast/device/kernels/dataflow/"