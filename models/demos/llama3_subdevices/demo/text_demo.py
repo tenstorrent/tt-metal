48f5e066e69aaa52f6292c7546dafbaaf66be7e9
Stuti Raizada <sraizada@tenstorrent.com>
2025-07-30 16:15:40 +0000

    update 4U expected outputs

--- models/demos/llama3_subdevices/demo/text_demo.py
+++ models/demos/llama3_subdevices/demo/text_demo.py
@@ -1152,7 +1152,7 @@ def test_demo_text(
         "decode_t/s/u": target_decode_tok_s_u,
     }
     if repeat_batches > 1 and batch_size == 1:
-        assert avg_time_to_first_token * 1000 < 122, f"TTFT {avg_time_to_first_token} ms is too high, should be < 121."
+        assert avg_time_to_first_token * 1000 < 99, f"TTFT {avg_time_to_first_token} ms is too high, should be < 98."
 
     # Save benchmark data for CI dashboard
     if is_ci_env and repeat_batches > 1:
