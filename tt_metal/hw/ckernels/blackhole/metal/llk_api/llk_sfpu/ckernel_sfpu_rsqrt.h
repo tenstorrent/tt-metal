6515d6234f05cd542b1c9ae0945288395b89132d
Jason Davies <jason@jasondavies.com>
2025-07-31 21:34:47 +0100

    Add missing fp32_dest_acc_en param for BH.

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_rsqrt.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_rsqrt.h
@@ -12,14 +12,14 @@ using namespace sfpi;
 namespace ckernel {
 namespace sfpu {
 
-template <bool APPROXIMATION_MODE, int ITERATIONS = 8, bool fp32_dest_acc_en>
+template <bool APPROXIMATION_MODE, int ITERATIONS = 8, bool fp32_dest_acc_en, bool layernorm_compat>
 inline void calculate_rsqrt() {
-    _calculate_rsqrt_<APPROXIMATION_MODE, ITERATIONS, fp32_dest_acc_en>(ITERATIONS);
+    _calculate_rsqrt_<APPROXIMATION_MODE, ITERATIONS, fp32_dest_acc_en, layernorm_compat>(ITERATIONS);
 }
 
-template <bool APPROXIMATION_MODE>
+template <bool APPROXIMATION_MODE, bool layernorm_compat>
 void rsqrt_init() {
-    _init_rsqrt_<APPROXIMATION_MODE>();
+    _init_rsqrt_<APPROXIMATION_MODE, layernorm_compat>();
 }
 
 }  // namespace sfpu
