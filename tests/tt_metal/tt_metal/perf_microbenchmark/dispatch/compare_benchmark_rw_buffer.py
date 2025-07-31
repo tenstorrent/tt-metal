21fff00a4295f8e4e5e0e6f750c31b275d60dcaf
River Wu <riverwu@tenstorrent.com>
2025-07-31 22:51:33 +0000

    Detect Failed tests

--- tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/compare_benchmark_rw_buffer.py
+++ tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/compare_benchmark_rw_buffer.py
@@ -42,18 +42,21 @@ def parse_args():
 
 
 def collect_benchmarks(benchmark_obj):
-    result = {}
+    successed_benchmarks, failed_benchmarks = {}, {}
     for benchmark in benchmark_obj["benchmarks"]:
-        result[benchmark["name"]] = float(benchmark["bytes_per_second"])
-    return result
+        if "error_occurred" in benchmark and benchmark["error_occurred"]:
+            failed_benchmarks[benchmark["name"]] = benchmark["error_message"]
+        else:
+            successed_benchmarks[benchmark["name"]] = float(benchmark["bytes_per_second"])
+    return successed_benchmarks, failed_benchmarks
 
 
-def compare_benchmarks(golden_benchmarks, result_benchmarks, tolerance):
+def compare_benchmarks(golden_benchmarks, result_benchmarks, result_failed_benchmarks, tolerance):
     golden_benchmarks_names = set(golden_benchmarks.keys())
     result_benchmarks_names = set(result_benchmarks.keys())
 
     successed_benchmarks = []
-    failed_benchmarks = []
+    underperforming_benchmarks = []
 
     for name in golden_benchmarks_names & result_benchmarks_names:
         golden_value = golden_benchmarks[name]
@@ -61,18 +64,28 @@ def compare_benchmarks(golden_benchmarks, result_benchmarks, tolerance):
 
         pct_diff = ((result_value - golden_value) / golden_value) * 100
         if abs(pct_diff) > tolerance:
-            failed_benchmarks.append((name, result_value, golden_value, pct_diff))
+            underperforming_benchmarks.append((name, result_value, golden_value, pct_diff))
         else:
             successed_benchmarks.append((name, result_value, golden_value, pct_diff))
 
-    success = golden_benchmarks_names == result_benchmarks_names and not failed_benchmarks
+    success = golden_benchmarks_names == result_benchmarks_names and not underperforming_benchmarks and not result_failed_benchmarks
 
     print(f"Benchmark {'FAILED' if not success else 'PASSED'}:")
     print("----------------------------------------------------")
-    print(f"Total failed benchmarks: {len(failed_benchmarks)}")
+    print(f"Total underperforming benchmarks: {len(underperforming_benchmarks)}")
     print(f"Total successed benchmarks: {len(successed_benchmarks)}")
     print(f"Total benchmarks: {len(result_benchmarks_names)}")
     print(f"Mismatched benchmarks: {len(golden_benchmarks_names ^ result_benchmarks_names)}")
+    print("----------------------------------------------------")
+
+    print("Successed benchmarks:")
+    for name, result_value, golden_value, pct_diff in successed_benchmarks:
+        print(f"PASSED | Benchmark {name}: {result_value:.2f} vs {golden_value:.2f} (diff: {pct_diff:+.2f}%)")
+    print("----------------------------------------------------")
+
+    print("Failed benchmarks:")
+    for name, error_message in result_failed_benchmarks.items():
+        print(f"FAILED | Benchmark {name} failed to run: {error_message}")
     print("----------------------------------------------------")
 
     print("Missing benchmarks:")
@@ -82,14 +95,9 @@ def compare_benchmarks(golden_benchmarks, result_benchmarks, tolerance):
         print(f"FAILED | Benchmark {name}: excess from results")
     print("----------------------------------------------------")
 
-    print("Failed benchmarks:")
-    for name, result_value, golden_value, pct_diff in sorted(failed_benchmarks, key=lambda x: x[3]):
+    print("Underperforming benchmarks: (bytes per second, result vs golden)")
+    for name, result_value, golden_value, pct_diff in sorted(underperforming_benchmarks, key=lambda x: x[3]):
         print(f"FAILED | Benchmark {name}: {result_value:.2f} vs {golden_value:.2f} (diff: {pct_diff:+.2f}%)")
-    print("----------------------------------------------------")
-
-    print("Successed benchmarks:")
-    for name, result_value, golden_value, pct_diff in sorted(successed_benchmarks):
-        print(f"PASSED | Benchmark {name}: {result_value:.2f} vs {golden_value:.2f} (diff: {pct_diff:+.2f}%)")
     print("----------------------------------------------------")
 
     return success
@@ -98,14 +106,15 @@ def compare_benchmarks(golden_benchmarks, result_benchmarks, tolerance):
 if __name__ == "__main__":
     golden_file, result_file, tolerance = parse_args()
 
-    golden_benchmarks = collect_benchmarks(json.load(golden_file))
-    result_benchmarks = collect_benchmarks(json.load(result_file))
+    golden_benchmarks, golden_failed_benchmarks = collect_benchmarks(json.load(golden_file))
+    result_benchmarks, result_failed_benchmarks = collect_benchmarks(json.load(result_file))
+
+    assert not golden_failed_benchmarks, "Golden benchmarks should not have failed benchmarks"
 
     print(f"Comparing throughput benchmarks ({golden_file} vs {result_file})...")
     print("Note: Benchmark name follows: Function/ Page Size/ Transfer Size/ Device ID")
-    print("Benchmark results are in bytes per second, higher is better. Result vs Golden.")
     print(f"Tolerance is {tolerance}%.")
     print("----------------------------------------------------")
 
-    if not compare_benchmarks(golden_benchmarks, result_benchmarks, tolerance):
+    if not compare_benchmarks(golden_benchmarks, result_benchmarks, result_failed_benchmarks, tolerance):
         sys.exit("Some benchmarks did not meet the golden values. Please review the output above.")
