7dc98c05a405f331116128cc5f6c7863d8c6318a
Bart Trzynadlowski <btrzynadlowski@tenstorrent.com>
2025-07-31 20:37:37 +0000

    Fixed build to be able to access Metal impl

--- tt_telemetry/server/main.cpp
+++ tt_telemetry/server/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+#include <tt-logger/tt-logger.hpp>
+#include <tt-metalium/control_plane.hpp>
+#include <tt-metalium/mesh_graph.hpp>
+#include "impl/context/metal_context.hpp"
+
 int main() {
+    const tt::tt_metal::MetalContext &instance = tt::tt_metal::MetalContext::instance();
+    const tt::Cluster &cluster = instance.get_cluster();
+    
     std::cout << "hello from tt_telemetry" << std::endl;
     return 0;
 }
