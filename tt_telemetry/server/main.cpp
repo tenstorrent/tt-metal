7fa77d1a1435b6b8c5c6127d6b2cb49232cf5b78
Bart Trzynadlowski <btrzynadlowski@tenstorrent.com>
2025-07-31 23:43:12 +0000

    Print Ethernet connections in order of chip ID

--- tt_telemetry/server/main.cpp
+++ tt_telemetry/server/main.cpp
@@ -5,15 +5,43 @@
 #include <tt-metalium/mesh_graph.hpp>
 #include "impl/context/metal_context.hpp"
 
-struct UbbId {
+
+/**************************************************************************************************
+ Galaxy UBB ID
+
+ Identifies the position of a chip in a Galaxy system. This is only valid for Galaxy boxes.
+**************************************************************************************************/
+
+struct GalaxyUbbId {
     std::uint32_t tray_id;
     std::uint32_t asic_id;
 }; 
 
-static std::ostream &operator<<(std::ostream &os, const UbbId &ubb_id) {
-    os << "[Tray " << ubb_id.tray_id << ", ASIC " << ubb_id.asic_id << ']';
+static std::ostream &operator<<(std::ostream &os, const GalaxyUbbId &ubb_id) {
+    os << "[Tray " << ubb_id.tray_id << " N" << ubb_id.asic_id << ']';
     return os;
 }
+
+static GalaxyUbbId get_galaxy_ubb_id(chip_id_t chip_id) {
+    const std::unordered_map<tt::ARCH, std::vector<std::uint16_t>> ubb_bus_ids = {
+        {tt::ARCH::WORMHOLE_B0, {0xC0, 0x80, 0x00, 0x40}},
+        {tt::ARCH::BLACKHOLE, {0x00, 0x40, 0xC0, 0x80}},
+    };
+    const auto& cluster = tt::tt_metal::MetalContext::instance().get_cluster();
+    const auto& tray_bus_ids = ubb_bus_ids.at(cluster.arch());
+    const auto bus_id = cluster.get_bus_id(chip_id);
+    auto tray_bus_id_it = std::find(tray_bus_ids.begin(), tray_bus_ids.end(), bus_id & 0xF0);
+    if (tray_bus_id_it != tray_bus_ids.end()) {
+        auto ubb_asic_id = bus_id & 0x0F;
+        return GalaxyUbbId{tray_bus_id_it - tray_bus_ids.begin() + 1, ubb_asic_id};
+    }
+    return GalaxyUbbId{0, 0};  // Invalid UBB ID if not found
+}
+
+
+/**************************************************************************************************
+ Cluster Type
+**************************************************************************************************/
 
 static std::ostream &operator<<(std::ostream &os, const tt::tt_metal::ClusterType cluster_type) {
     switch (cluster_type) {
@@ -37,43 +65,50 @@ static std::ostream &operator<<(std::ostream &os, const tt::tt_metal::ClusterTyp
     return os;
 }
 
-static UbbId get_ubb_id(chip_id_t chip_id) {
-    const std::unordered_map<tt::ARCH, std::vector<std::uint16_t>> ubb_bus_ids = {
-        {tt::ARCH::WORMHOLE_B0, {0xC0, 0x80, 0x00, 0x40}},
-        {tt::ARCH::BLACKHOLE, {0x00, 0x40, 0xC0, 0x80}},
-    };
-    const auto& cluster = tt::tt_metal::MetalContext::instance().get_cluster();
-    const auto& tray_bus_ids = ubb_bus_ids.at(cluster.arch());
-    const auto bus_id = cluster.get_bus_id(chip_id);
-    auto tray_bus_id_it = std::find(tray_bus_ids.begin(), tray_bus_ids.end(), bus_id & 0xF0);
-    if (tray_bus_id_it != tray_bus_ids.end()) {
-        auto ubb_asic_id = bus_id & 0x0F;
-        return UbbId{tray_bus_id_it - tray_bus_ids.begin() + 1, ubb_asic_id};
+
+/**************************************************************************************************
+ Link Status App
+**************************************************************************************************/
+
+auto make_ordered_ethernet_connections(const auto &unordered_connections) {
+    std::map<
+        tt::umd::chip_id_t,
+        std::map<
+            tt::umd::ethernet_channel_t,
+            std::tuple<tt::umd::chip_id_t, tt::umd::ethernet_channel_t>
+        >
+    > ordered_connections;
+    
+    for (const auto& [chip_id, channel_map] : unordered_connections) {
+        for (const auto& [channel, connection_tuple] : channel_map) {
+            ordered_connections[chip_id][channel] = connection_tuple;
+        }
     }
-    return UbbId{0, 0};  // Invalid UBB ID if not found
+    
+    return ordered_connections;
 }
 
 int main() {
     const tt::tt_metal::MetalContext &instance = tt::tt_metal::MetalContext::instance();
     const tt::Cluster &cluster = instance.get_cluster();
-    const std::unordered_map<
+    const std::map<
         tt::umd::chip_id_t, 
-        std::unordered_map<
+        std::map<
             tt::umd::ethernet_channel_t, 
             std::tuple<tt::umd::chip_id_t, tt::umd::ethernet_channel_t>
         >
-    > ethernet_connections = cluster.get_ethernet_connections();
+    > ethernet_connections = make_ordered_ethernet_connections(cluster.get_ethernet_connections());
     tt::tt_metal::ClusterType cluster_type = cluster.get_cluster_type();
 
     std::cout << "Cluster Type: " << cluster_type << std::endl;
 
     for (const auto &[chip_id, remote_chip_and_channel_by_channel]: ethernet_connections) {
-        std::cout << "Chip " << chip_id << " (UBB ID: " << get_ubb_id(chip_id) << ")" << std::endl;
+        std::cout << "Chip " << chip_id << ' ' << get_galaxy_ubb_id(chip_id) << std::endl;
         for (const auto &[channel, remote_chip_and_channel]: remote_chip_and_channel_by_channel) {
             tt::umd::chip_id_t remote_chip_id;
             tt::umd::ethernet_channel_t remote_channel;
             std::tie(remote_chip_id, remote_channel) = remote_chip_and_channel;
-            std::cout << "  Channel " << channel << " -> Chip " << remote_chip_id << ", Channel " << remote_channel << std::endl;
+            std::cout << "  Channel " << channel << " -> Chip " << remote_chip_id << ' ' << get_galaxy_ubb_id(remote_chip_id) << ", Channel " << remote_channel << std::endl;
         }
     }
     return 0;
