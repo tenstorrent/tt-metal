d88a21194901854a5660f0228132c0f3173a1053
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2025-08-01 15:19:02 +0000

    #0: All close

--- tests/ttnn/unit_tests/operations/eltwise/test_exp2.py
+++ tests/ttnn/unit_tests/operations/eltwise/test_exp2.py
@@ -5,7 +5,7 @@
 import torch
 import pytest
 import ttnn
-from tests.ttnn.utils_for_testing import assert_with_ulp
+from tests.ttnn.utils_for_testing import assert_with_ulp, assert_allclose
 
 
 @pytest.mark.parametrize(
@@ -85,3 +85,39 @@ def test_exp2_optional(input_shapes, device):
     ttnn.exp2(tt_in, output_tensor=tt_out, queue_id=cq_id)
     result = ttnn.to_torch(tt_out)
     assert_with_ulp(golden, result)
+
+
+@pytest.mark.parametrize(
+    "input_shapes",
+    (
+        (torch.Size([1, 1, 32, 32])),
+        (torch.Size([1, 2, 64, 120])),
+        (torch.Size([1, 3, 320, 320])),
+    ),
+)
+@pytest.mark.parametrize(
+    "low, high",
+    [
+        (-127, -1),  # Max ATOL Delta: 0.001953125, Max RTOL Delta: 1.0
+        (-3.3e38, -1),  # Max ATOL Delta: 0.0, Max RTOL Delta: nan
+    ],
+)
+def test_exp2_atol(input_shapes, low, high, device):
+    num_elements = torch.prod(torch.tensor(input_shapes)).item()
+    torch_input = torch.linspace(high, low, num_elements, dtype=torch.bfloat16)
+    torch_input = torch_input[:num_elements].reshape(input_shapes)
+
+    golden_function = ttnn.get_golden_function(ttnn.exp2)
+    golden = golden_function(torch_input, device=device)
+
+    tt_in = ttnn.from_torch(
+        torch_input,
+        dtype=ttnn.bfloat16,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    tt_result = ttnn.exp2(tt_in)
+    result = ttnn.to_torch(tt_result)
+    assert_allclose(tt_result, golden, rtol=1, atol=1e-3)
