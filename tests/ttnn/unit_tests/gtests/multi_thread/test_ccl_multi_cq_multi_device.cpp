4cb34f0fc8eb5e9b444f433ff18a044f1ac44663
Oleg Milyutin <omilyutin@tenstorrent.com>
2025-08-01 04:48:04 +0000

    Fix to AsyncExecutionWorksCQ0CQ1

--- tests/ttnn/unit_tests/gtests/multi_thread/test_ccl_multi_cq_multi_device.cpp
+++ tests/ttnn/unit_tests/gtests/multi_thread/test_ccl_multi_cq_multi_device.cpp
@@ -312,7 +312,8 @@ TEST_F(MultiCQFabricMeshDevice2x4Fixture, AsyncExecutionWorksCQ0CQ1) {
                 // Set output_tensor into device_tensor for allgather
                 device_tensors[dev_idx] = dispatch_ops_to_device(input_tensor, ttnn::QueueId(op_cq_id));
 
-                auto operation_event = ttnn::record_event(single_mesh->mesh_command_queue(op_cq_id));
+                auto operation_event = tt::tt_metal::distributed::EnqueueRecordEvent(
+                    single_mesh->mesh_command_queue(op_cq_id), std::vector<SubDeviceId>{SubDeviceId{0}});
                 // Enqueue the task waiting for the operation_event to the ccl`s command queue
                 ttnn::wait_for_event(single_mesh->mesh_command_queue(ccl_cq_id), operation_event);
 
