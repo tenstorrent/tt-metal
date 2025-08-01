a7ed20d69d9dea4e2da2c0eabb7434a9b7b54333
Keerthana <152590865+kkeerthana0573@users.noreply.github.com>
2025-08-01 20:14:01 +0530

    #24618: Add DP Support for Yolov8x (#24625)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/24618)
    
    ### Problem description
    Add DataParallel support for Yolov8x model.
    
    
    ### What's changed
    Added data parallel support for Yolov8x model to :
    - Demo and
    - e2e performant
      - For 640x640 resolution, 
        - Single Device : 40 FPS
        - Multi Device (N300) : 77 FPS
    - ~Due to the issue reported in
    [#25718](https://github.com/tenstorrent/tt-metal/issues/25718), FPS
    drops in multi-device~
    - Used ttnn.from_host_shards, during input tensor sharding and improved
    FPS
      - For 640x640 resolution,
        - Single Device : 50 FPS
        - Multi Device (N300) : 100 FPS
      
    
    ### Checklist
    - [x] [All post-commit
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16641607515)
    - [x] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/runs/16641619367)
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/runs/16641613240)
    - [x] [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16641609816)
    - [x] [Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16641616498)

--- models/demos/yolov8x/tt/ttnn_yolov8x_utils.py
+++ models/demos/yolov8x/tt/ttnn_yolov8x_utils.py
@@ -7,7 +7,7 @@ import torch
 import ttnn
 
 
-def fold_batch_norm2d_into_conv2d(device, state_dict, path, eps=1e-03, bfloat8=False):
+def fold_batch_norm2d_into_conv2d(device, state_dict, path, eps=1e-03, bfloat8=False, mesh_mapper=None):
     bn_weight = state_dict[path + f".bn.weight"].unsqueeze(1).unsqueeze(1).unsqueeze(1)
     bn_bias = state_dict[path + f".bn.bias"].unsqueeze(1).unsqueeze(1).unsqueeze(1)
     bn_running_mean = state_dict[path + f".bn.running_mean"].unsqueeze(1).unsqueeze(1).unsqueeze(1)
@@ -19,9 +19,15 @@ def fold_batch_norm2d_into_conv2d(device, state_dict, path, eps=1e-03, bfloat8=F
     bias = bias.reshape(1, 1, 1, -1)
 
     if bfloat8:
-        return (ttnn.from_torch(weight, dtype=ttnn.float32), ttnn.from_torch(bias, dtype=ttnn.float32))
+        return (
+            ttnn.from_torch(weight, dtype=ttnn.float32, mesh_mapper=mesh_mapper),
+            ttnn.from_torch(bias, dtype=ttnn.float32, mesh_mapper=mesh_mapper),
+        )
 
-    return (ttnn.from_torch(weight, dtype=ttnn.bfloat16), ttnn.from_torch(bias, dtype=ttnn.bfloat16))
+    return (
+        ttnn.from_torch(weight, dtype=ttnn.bfloat16, mesh_mapper=mesh_mapper),
+        ttnn.from_torch(bias, dtype=ttnn.bfloat16, mesh_mapper=mesh_mapper),
+    )
 
 
 def autopad(k, p=None, d=1):
@@ -32,7 +38,7 @@ def autopad(k, p=None, d=1):
     return p if isinstance(p, int) else p[0]
 
 
-def make_anchors(device, feats, strides, grid_cell_offset=0.5):
+def make_anchors(device, feats, strides, grid_cell_offset=0.5, mesh_mapper=None):
     anchor_points, stride_tensor = [], []
     assert feats is not None
     for i, stride in enumerate(strides):
@@ -47,8 +53,8 @@ def make_anchors(device, feats, strides, grid_cell_offset=0.5):
     b = torch.cat(stride_tensor).transpose(0, 1)
 
     return (
-        ttnn.from_torch(a, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device),
-        ttnn.from_torch(b, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device),
+        ttnn.from_torch(a, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device, mesh_mapper=mesh_mapper),
+        ttnn.from_torch(b, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device, mesh_mapper=mesh_mapper),
     )
 
 
@@ -71,17 +77,21 @@ def ttnn_decode_bboxes(device, distance, anchor_points, xywh=True, dim=1):
         return ttnn.concat([c_xy, wh], 1)
 
 
-def preprocess_parameters(state_dict, path, bias=True, bfloat8=True):
+def preprocess_parameters(state_dict, path, bias=True, bfloat8=True, mesh_mapper=None):
     if bias:
         conv_weight = state_dict[f"{path}.2.weight"]
         conv_bias = state_dict[f"{path}.2.bias"]
 
         if bfloat8:
-            conv_weight = ttnn.from_torch(conv_weight, dtype=ttnn.float32)
-            conv_bias = ttnn.reshape(ttnn.from_torch(conv_bias, dtype=ttnn.float32), (1, 1, 1, -1))
+            conv_weight = ttnn.from_torch(conv_weight, dtype=ttnn.float32, mesh_mapper=mesh_mapper)
+            conv_bias = ttnn.reshape(
+                ttnn.from_torch(conv_bias, dtype=ttnn.float32, mesh_mapper=mesh_mapper), (1, 1, 1, -1)
+            )
         else:
-            conv_weight = ttnn.from_torch(conv_weight, dtype=ttnn.bfloat16)
-            conv_bias = ttnn.reshape(ttnn.from_torch(conv_bias, dtype=ttnn.bfloat16), (1, 1, 1, -1))
+            conv_weight = ttnn.from_torch(conv_weight, dtype=ttnn.bfloat16, mesh_mapper=mesh_mapper)
+            conv_bias = ttnn.reshape(
+                ttnn.from_torch(conv_bias, dtype=ttnn.bfloat16, mesh_mapper=mesh_mapper), (1, 1, 1, -1)
+            )
 
         return (conv_weight, conv_bias)
 
@@ -89,14 +99,14 @@ def preprocess_parameters(state_dict, path, bias=True, bfloat8=True):
         conv_weight = state_dict[f"{path}.conv.weight"]
 
         if bfloat8:
-            conv_weight = ttnn.from_torch(conv_weight, dtype=ttnn.float32)
+            conv_weight = ttnn.from_torch(conv_weight, dtype=ttnn.float32, mesh_mapper=mesh_mapper)
         else:
-            conv_weight = ttnn.from_torch(conv_weight, dtype=ttnn.bfloat16)
+            conv_weight = ttnn.from_torch(conv_weight, dtype=ttnn.bfloat16, mesh_mapper=mesh_mapper)
 
         return (conv_weight, None)
 
 
-def custom_preprocessor(device, state_dict, inp_h=640, inp_w=640):
+def custom_preprocessor(device, state_dict, inp_h=640, inp_w=640, mesh_mapper=None):
     pairs = [
         ("model.0", True),
         ("model.1", True),
@@ -200,26 +210,63 @@ def custom_preprocessor(device, state_dict, inp_h=640, inp_w=640):
     parameters = {}
 
     for path, bfloat8 in pairs:
-        parameters[path] = fold_batch_norm2d_into_conv2d(device, state_dict, path=path, bfloat8=True)
+        parameters[path] = fold_batch_norm2d_into_conv2d(
+            device, state_dict, path=path, bfloat8=True, mesh_mapper=mesh_mapper
+        )
 
-    parameters["model.22.cv2.0"] = preprocess_parameters(state_dict, "model.22.cv2.0", bfloat8=True)
-    parameters["model.22.cv3.0"] = preprocess_parameters(state_dict, "model.22.cv3.0", bfloat8=True)
+    parameters["model.22.cv2.0"] = preprocess_parameters(
+        state_dict, "model.22.cv2.0", bfloat8=True, mesh_mapper=mesh_mapper
+    )
+    parameters["model.22.cv3.0"] = preprocess_parameters(
+        state_dict, "model.22.cv3.0", bfloat8=True, mesh_mapper=mesh_mapper
+    )
 
-    parameters["model.22.cv2.1"] = preprocess_parameters(state_dict, "model.22.cv2.1", bfloat8=True)
-    parameters["model.22.cv3.1"] = preprocess_parameters(state_dict, "model.22.cv3.1", bfloat8=True)
+    parameters["model.22.cv2.1"] = preprocess_parameters(
+        state_dict, "model.22.cv2.1", bfloat8=True, mesh_mapper=mesh_mapper
+    )
+    parameters["model.22.cv3.1"] = preprocess_parameters(
+        state_dict, "model.22.cv3.1", bfloat8=True, mesh_mapper=mesh_mapper
+    )
 
-    parameters["model.22.cv2.2"] = preprocess_parameters(state_dict, "model.22.cv2.2", bfloat8=True)
-    parameters["model.22.cv3.2"] = preprocess_parameters(state_dict, "model.22.cv3.2", bfloat8=True)
+    parameters["model.22.cv2.2"] = preprocess_parameters(
+        state_dict, "model.22.cv2.2", bfloat8=True, mesh_mapper=mesh_mapper
+    )
+    parameters["model.22.cv3.2"] = preprocess_parameters(
+        state_dict, "model.22.cv3.2", bfloat8=True, mesh_mapper=mesh_mapper
+    )
 
-    parameters["model.22.dfl"] = preprocess_parameters(state_dict, "model.22.dfl", bias=False, bfloat8=True)
+    parameters["model.22.dfl"] = preprocess_parameters(
+        state_dict, "model.22.dfl", bias=False, bfloat8=True, mesh_mapper=mesh_mapper
+    )
 
     strides = [8, 16, 32]
 
     feats = [(inp_h // 8, inp_w // 8), (inp_h // 16, inp_w // 16), (inp_h // 32, inp_w // 32)]  # value depend on res
 
-    anchors, strides = make_anchors(device, feats, strides)  # Optimization: Processing make anchors outside model run
+    anchors, strides = make_anchors(
+        device, feats, strides, mesh_mapper=mesh_mapper
+    )  # Optimization: Processing make anchors outside model run
 
     parameters["anchors"] = anchors
     parameters["strides"] = strides
 
     return parameters
+
+
+def get_mesh_mappers(device):
+    if device.get_num_devices() > 1:
+        inputs_mesh_mapper = ttnn.ShardTensorToMesh(device, dim=0)
+        weights_mesh_mapper = ttnn.ReplicateTensorToMesh(device)
+        output_mesh_composer = ttnn.ConcatMeshToTensor(device, dim=0)
+    else:
+        inputs_mesh_mapper = None
+        weights_mesh_mapper = None
+        output_mesh_composer = None
+    return inputs_mesh_mapper, weights_mesh_mapper, output_mesh_composer
+
+
+def create_custom_mesh_preprocessor(device, mesh_mapper=None):
+    def custom_mesh_preprocessor(model):
+        return custom_preprocessor(device, model.state_dict(), inp_h=640, inp_w=640, mesh_mapper=mesh_mapper)
+
+    return custom_mesh_preprocessor
