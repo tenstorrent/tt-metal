115200f8390c69c675b8b80ab22ec136e4ff7129
Nigel Huang <nhuang@tenstorrent.com>
2025-08-01 19:10:59 +0000

    Move files around

--- tt_metal/fabric/impl/kernels/edm_fabric/fabric_erisc_datamover.cpp
+++ tt_metal/fabric/impl/kernels/edm_fabric/fabric_erisc_datamover.cpp
@@ -22,6 +22,8 @@
 #include "tt_metal/fabric/hw/inc/tt_fabric_utils.h"
 #include "tt_metal/fabric/hw/inc/edm_fabric/edm_fabric_tmp_utils.hpp"
 #include "tt_metal/fabric/hw/inc/edm_fabric/fabric_router_flow_control.hpp"
+#include "tt_metal/fabric/hw/inc/edm_fabric/edm_fabric_flow_control_helpers.hpp"
+#include "tt_metal/fabric/hw/inc/edm_fabric/fabric_packet_recorder.hpp"
 
 #include "noc_overlay_parameters.h"
 #include "tt_metal/hw/inc/utils/utils.h"
@@ -274,91 +276,6 @@ static constexpr std::array<uint32_t, NUM_ROUTER_CARDINAL_DIRECTIONS> receiver_c
     receiver_channel_0_free_slots_from_north_stream_id,
     receiver_channel_0_free_slots_from_south_stream_id};
 
-/*
- * Tracks receiver channel pointers (from sender side)
- */
-template <uint8_t RECEIVER_NUM_BUFFERS>
-struct OutboundReceiverChannelPointers {
-    uint32_t num_free_slots = RECEIVER_NUM_BUFFERS;
-    BufferIndex remote_receiver_buffer_index{0};
-    size_t cached_next_buffer_slot_addr = 0;
-
-    FORCE_INLINE bool has_space_for_packet() const { return num_free_slots; }
-};
-
-/*
- * Tracks receiver channel pointers (from receiver side). Must call reset() before using.
- */
-template <uint8_t RECEIVER_NUM_BUFFERS>
-struct ReceiverChannelPointers {
-    ChannelCounter<RECEIVER_NUM_BUFFERS> wr_sent_counter;
-    ChannelCounter<RECEIVER_NUM_BUFFERS> wr_flush_counter;
-    ChannelCounter<RECEIVER_NUM_BUFFERS> ack_counter;
-    ChannelCounter<RECEIVER_NUM_BUFFERS> completion_counter;
-    std::array<uint8_t, RECEIVER_NUM_BUFFERS> src_chan_ids;
-
-    FORCE_INLINE void set_src_chan_id(BufferIndex buffer_index, uint8_t src_chan_id) {
-        src_chan_ids[buffer_index.get()] = src_chan_id;
-    }
-
-    FORCE_INLINE uint8_t get_src_chan_id(BufferIndex buffer_index) const { return src_chan_ids[buffer_index.get()]; }
-
-    FORCE_INLINE void reset() {
-        wr_sent_counter.reset();
-        wr_flush_counter.reset();
-        ack_counter.reset();
-        completion_counter.reset();
-    }
-};
-
-// Forward‐declare the Impl primary template:
-template <template <uint8_t> class ChannelType, auto& BufferSizes, typename Seq>
-struct ChannelPointersTupleImpl;
-
-// Provide the specialization that actually holds the tuple and `get<>`:
-template <template <uint8_t> class ChannelType, auto& BufferSizes, size_t... Is>
-struct ChannelPointersTupleImpl<ChannelType, BufferSizes, std::index_sequence<Is...>> {
-    std::tuple<ChannelType<BufferSizes[Is]>...> channel_ptrs;
-
-    template <size_t I>
-    constexpr auto& get() {
-        return std::get<I>(channel_ptrs);
-    }
-};
-
-// Simplify the "builder" so that make() returns the Impl<…> directly:
-template <template <uint8_t> class ChannelType, auto& BufferSizes>
-struct ChannelPointersTuple {
-    static constexpr size_t N = std::size(BufferSizes);
-
-    static constexpr auto make() {
-        return ChannelPointersTupleImpl<ChannelType, BufferSizes, std::make_index_sequence<N>>{};
-    }
-};
-
-struct PacketHeaderRecorder {
-    volatile uint32_t* buffer_ptr;
-    size_t buffer_n_headers;
-    size_t buffer_index;
-
-    PacketHeaderRecorder(volatile uint32_t* buffer_ptr, size_t buffer_n_headers) :
-        buffer_ptr(buffer_ptr), buffer_n_headers(buffer_n_headers), buffer_index(0) {}
-
-    void record_packet_header(volatile uint32_t* packet_header_ptr) {
-        uint32_t dest_l1_addr = (uint32_t)buffer_ptr + buffer_index * sizeof(PACKET_HEADER_TYPE);
-        noc_async_write(
-            (uint32_t)packet_header_ptr,
-            get_noc_addr(my_x[0], my_y[0], dest_l1_addr),
-            sizeof(PACKET_HEADER_TYPE),
-            1 - noc_index  // avoid the contention on main noc
-        );
-        buffer_index++;
-        if (buffer_index == buffer_n_headers) {
-            buffer_index = 0;
-        }
-    }
-};
-
 enum PacketLocalForwardType : uint8_t {
     PACKET_FORWARD_INVALID = 0x0,
     PACKET_FORWARD_LOCAL_ONLY = 0x1,
@@ -462,16 +379,6 @@ FORCE_INLINE void receiver_send_received_ack(
     };
 
     receiver_channel_response_credit_sender.send_ack_credit(src_id);
-}
-
-// MUST CHECK !is_eth_txq_busy() before calling
-FORCE_INLINE void receiver_send_completion_ack(
-    ReceiverChannelResponseCreditSender& receiver_channel_response_credit_sender, uint8_t src_id) {
-    if constexpr (ETH_TXQ_SPIN_WAIT_RECEIVER_SEND_COMPLETION_ACK) {
-        while (internal_::eth_txq_is_busy(receiver_txq_id)) {
-        };
-    }
-    receiver_channel_response_credit_sender.send_completion_credit(src_id);
 }
 
 template <uint8_t SENDER_NUM_BUFFERS>
