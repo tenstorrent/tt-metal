205bab52c4d17f7f19eea4c26c690e0af6f8f24e
Djordje Ivanovic <divanovic@tenstorrent.com>
2025-08-01 12:31:36 +0000

    #0: Add llama shaped test and update dtype in unit test

--- tests/ttnn/unit_tests/operations/reduce/test_topk.py
+++ tests/ttnn/unit_tests/operations/reduce/test_topk.py
@@ -20,7 +20,7 @@ def run_topk_test(N, C, H, W, k, dtype, dim, sorted, largest, device, sub_core_g
         indices_tensor_torch = torch.zeros(shape, dtype=torch.int32)
         for i in range(W):
             indices_tensor_torch[:, :, :, i] = i
-        indices_tensor = ttnn.from_torch(indices_tensor_torch, ttnn.uint32, layout=ttnn.Layout.TILE, device=device)
+        indices_tensor = ttnn.from_torch(indices_tensor_torch, ttnn.uint16, layout=ttnn.Layout.TILE, device=device)
     else:
         indices_tensor = None
 
