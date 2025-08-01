1a83bd8b2e91f7ab92360b9595bf469025f9de06
Billy Ransom <wransom@tenstorrent.com>
2025-08-01 09:30:09 -0700

    Support Asymmetric Padding in Generic Pools (#25994)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/25840
    https://github.com/tenstorrent/tt-metal/issues/22743
    
    ### Problem description
    Generic pool currently only allows symmetric height, width padding.
    
    The unit tests are also a mess and need to be cleaned up especially with
    the introduction of a new padding parameterization.
    
    ### What's changed
    - Average pool's scalar calculation has been updated to support
    asymmetric padding
    - Control of Top, Bottom, Left, Right padding has been exposed to the
    user
    - A correction algorithm has been added to the average pool tests to
    adjust torch's output when asymmetric padding is used (which is not
    supported by torch)
    
    #### Test Updates
    - Functionality of the average and max pool unit tests has been unified
    so the procedures are the same
    - The max pool unit test has been reworked to use the nightly
    `run_max_pool` function
    - The `test_pool_core_nondivis` and its accompanying mass of duplicated
    code has been removed as it tests nothing special, only non power of 2
    NHW with height sharding which is trivial, note it has an unused config
    override parameter which was likely once meant to change the grid size,
    but it is not currently used, and in any case we test non-power of 2
    multiples and partial grids elsewhere
    - Coverage has been improved generic pool tests for kernel sizes and
    padding combinations
    - Redundant shapes have been removed from height sharding tests
    - The width and block sharding tests have been adjusted so that all the
    tests actually run, previously many of the tests had channel
    combinations which would just end up getting skipped since there weren't
    enough channels per core after shading
    - Parameterizations for Width and Block sharding have been greatly
    reduced as these are redundant with height sharding
    - Many of the skips in the nightly test have been eliminated to make it
    clearer which parameterizations shown are actually being ran
    - Average Pool coverage has been improved minding the new 4 tile max per
    reduction
    - Trivial average pool parameterization cases have been skipped
    - Non-height sharding cases have been removed from Average pool tests as
    these are redundant with MaxPool
    
    #### Current Test Run Times:
    - Max Pool Unit Test: 5 min
    - Avg Pool Unit Test: 5 min
    - Max Pool Nightly Test: 24 min
    - Avg Pool Nightly Test: 22 min
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/16661987041
    Failing on main, hitting error unrelated to this PR
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/16661988872
    Failing on main, hitting error unrelated to this PR
    - [x] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/16661997127
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes:
    Currently disabled, changes shouldn't affect device perf
    - [x] [Nightly
    L2](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/16661993767
    - [x] [Nightly
    Blackhole](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-nightly-tests.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/16661990624
    - [x] [Frequent
    model](https://github.com/tenstorrent/tt-metal/actions/workflows/fast-dispatch-full-regressions-and-models.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/16661995380
    - [x] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/pool/pool_utils.hpp
+++ ttnn/cpp/ttnn/operations/pool/pool_utils.hpp
@@ -31,8 +31,10 @@ struct AvgPoolConfig {
     uint32_t ceil_h;
     uint32_t ceil_w;
     bool count_include_pad;
-    uint32_t pad_h;
-    uint32_t pad_w;
+    uint32_t pad_t;
+    uint32_t pad_b;
+    uint32_t pad_l;
+    uint32_t pad_r;
     uint32_t out_nhw_per_core;
     std::optional<int32_t> divisor_override;
 };
