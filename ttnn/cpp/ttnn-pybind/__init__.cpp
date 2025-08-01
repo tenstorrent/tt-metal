1f83caf3be2d0a2b50124f6c123d3d72f4634093
Vincent Tang <157417475+vtangTT@users.noreply.github.com>
2025-07-31 23:41:45 -0500

    TensorAccessorArgs pybinds (#25979)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/25655)
    
    ### Problem description
    Want to expose `TensorAccessorArgs` to more easily generate args to pass
    to kernels in ttnn.
    
    ### What's changed
    - pybinded `TensorAccessorArgs` and `ArgsConfig` class
    - `TensorAccessorArgs` takes a tensor instead of a buffer ptr, as to not
    expose `tt_metal::Buffer` type to python
    - pybinded `ArgConfig` enum
    - added some basic tests
    
    ### Checklist
    - [x] [All post
    commit]https://github.com/tenstorrent/tt-metal/actions/runs/16633959456
    - [x] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn-pybind/__init__.cpp
+++ ttnn/cpp/ttnn-pybind/__init__.cpp
@@ -24,6 +24,7 @@
 #include "ttnn-pybind/operations/trace.hpp"
 #include "ttnn-pybind/profiler.hpp"
 #include "ttnn-pybind/program_descriptors.hpp"
+#include "ttnn-pybind/tensor_accessor_args.hpp"
 #include "ttnn-pybind/reports.hpp"
 #include "ttnn-pybind/tensor.hpp"
 #include "ttnn-pybind/types.hpp"
@@ -226,6 +227,7 @@ PYBIND11_MODULE(_ttnn, module) {
     auto m_operations = module.def_submodule("operations", "ttnn Operations");
     auto m_fabric = module.def_submodule("fabric", "Fabric instantiation APIs");
     auto m_program_descriptors = module.def_submodule("program_descriptor", "Program descriptors types");
+    auto m_tensor_accessor_args = module.def_submodule("tensor_accessor_args", "Tensor accessor args types");
 
     // TYPES
     ttnn::tensor::tensor_mem_config_module_types(m_tensor);
@@ -245,6 +247,7 @@ PYBIND11_MODULE(_ttnn, module) {
     ttnn::mesh_socket::py_module_types(m_mesh_socket);
     ttnn::reports::py_module_types(m_reports);
     ttnn::program_descriptors::py_module_types(m_program_descriptors);
+    ttnn::tensor_accessor_args::py_module_types(m_tensor_accessor_args);
 
     // FUNCTIONS / OPERATIONS
     ttnn::tensor::tensor_mem_config_module(m_tensor);
@@ -271,6 +274,7 @@ PYBIND11_MODULE(_ttnn, module) {
     ttnn::mesh_socket::py_module(m_mesh_socket);
     ttnn::profiler::py_module(m_profiler);
     ttnn::reports::py_module(m_reports);
+    ttnn::tensor_accessor_args::py_module(m_tensor_accessor_args);
 
     // ttnn operations have to come before the deprecated ones,
     // because ttnn defines additional type bindings.
