tenstorrent/tt-metal

commit 4e79da83dd7469bc0ebf77770d7391dfbafb3a0d

#25975: Migrate pad, permute, repeat, reshape OPs to use TensorAccessor (#26091)
### Ticket
https://github.com/tenstorrent/tt-metal/issues/25975

### Problem description
We need to migrate all kernels to use TensorAccessor instead of
InterleavedAddrGen to make them support ND sharding. This is the first
step of adding universal sharding support to all OPs

### What's changed
Updated all pad, permute, repeat, reshape kernels to use TensorAccessor
Updated corresponding program factories to pass different compile time
arguments

### Checklist
- [x] [All post commit CI
passes](https://github.com/tenstorrent/tt-metal/actions/runs/16665319175)
- [x] [Blackhole Post commit CI with demo tests
passes](https://github.com/tenstorrent/tt-metal/actions/runs/16660949453)
- [x] New/Existing tests provide coverage for changes

ttnn/cpp/ttnn/operations/data_movement/pad/device/kernels/dataflow/reader_pad_dims_rm_interleaved_v2.cpp
@@ -25,24 +25,24 @@ void kernel_main() {
     uint32_t front_pad_h = get_arg_val<uint32_t>(6);
     tt_l1_ptr uint32_t* start_dim_offset = (tt_l1_ptr uint32_t*)(get_arg_addr(7));
 
-    constexpr bool src_is_dram = get_compile_time_arg_val(0) == 1;
-    constexpr uint32_t N = get_compile_time_arg_val(1);
-    constexpr uint32_t H = get_compile_time_arg_val(2);
-    constexpr uint32_t C = get_compile_time_arg_val(3);
-    constexpr uint32_t stick_size_bytes = get_compile_time_arg_val(4);
-    constexpr uint32_t N_padded = get_compile_time_arg_val(5);
-    constexpr uint32_t H_padded = get_compile_time_arg_val(6);
-    constexpr uint32_t C_padded = get_compile_time_arg_val(7);
-    constexpr uint32_t stick_size_padded = get_compile_time_arg_val(8);
-    constexpr uint32_t stick_size_padded_front = get_compile_time_arg_val(9);
-    constexpr uint32_t stick_size_padded_end = get_compile_time_arg_val(10);
-    constexpr uint32_t num_zero_pad_sticks_read = get_compile_time_arg_val(11);
-    constexpr uint32_t last_zero_stick_size = get_compile_time_arg_val(12);
-    constexpr uint32_t stick_size_padded_aligned = get_compile_time_arg_val(21);
+    constexpr uint32_t N = get_compile_time_arg_val(0);
+    constexpr uint32_t H = get_compile_time_arg_val(1);
+    constexpr uint32_t C = get_compile_time_arg_val(2);
+    constexpr uint32_t stick_size_bytes = get_compile_time_arg_val(3);
+    constexpr uint32_t N_padded = get_compile_time_arg_val(4);
+    constexpr uint32_t H_padded = get_compile_time_arg_val(5);
+    constexpr uint32_t C_padded = get_compile_time_arg_val(6);
+    constexpr uint32_t stick_size_padded = get_compile_time_arg_val(7);
+    constexpr uint32_t stick_size_padded_front = get_compile_time_arg_val(8);
+    constexpr uint32_t stick_size_padded_end = get_compile_time_arg_val(9);
+    constexpr uint32_t num_zero_pad_sticks_read = get_compile_time_arg_val(10);
+    constexpr uint32_t last_zero_stick_size = get_compile_time_arg_val(11);
+    constexpr uint32_t stick_size_padded_aligned = get_compile_time_arg_val(18);
 
-    constexpr bool not_pad_by_zero = get_compile_time_arg_val(13) == 1;
-    constexpr uint32_t front_padding = get_compile_time_arg_val(9);
-    constexpr bool unaligned = get_compile_time_arg_val(22) == 1;
+    constexpr bool not_pad_by_zero = get_compile_time_arg_val(12) == 1;
+    constexpr uint32_t front_padding = get_compile_time_arg_val(8);
+    constexpr bool unaligned = get_compile_time_arg_val(19) == 1;
+    constexpr auto src_args = TensorAccessorArgs<20>();
 
     uint32_t packed_pad_value = 0;
     uint32_t row_major_min_bytes = 0;
@@ -61,12 +61,7 @@ void kernel_main() {
     constexpr uint32_t cb_pad = tt::CBIndex::c_1;
     constexpr uint32_t cb_pad_align = tt::CBIndex::c_2;
 
-    constexpr bool stick_size_is_pow2 = get_compile_time_arg_val(19) == 1;
-    constexpr uint32_t log_base_2_of_page_size = get_compile_time_arg_val(20);
-    constexpr uint32_t page_size = get_compile_time_arg_val(20);
-
-    const auto s =
-        get_interleaved_addr_gen<src_is_dram, stick_size_is_pow2>(src_addr, page_size, log_base_2_of_page_size);
+    const auto s = TensorAccessor(src_args, src_addr, stick_size_bytes);
 
     uint64_t zeros_noc_addr = get_noc_addr(MEM_ZEROS_BASE);
 

ttnn/cpp/ttnn/operations/data_movement/pad/device/kernels/dataflow/pad_dims_rm_interleaved.cpp
@@ -1,70 +1,0 @@
-// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
-//
-// SPDX-License-Identifier: Apache-2.0
-
-#include <stdint.h>
-#include "dataflow_api.h"
-
-void kernel_main() {
-    const uint32_t src_addr = get_arg_val<uint32_t>(0);
-    const uint32_t dst_addr = get_arg_val<uint32_t>(1);
-    const uint32_t num_unpadded_W = get_arg_val<uint32_t>(2);
-    const uint32_t num_total_W = get_arg_val<uint32_t>(3);
-    const uint32_t num_unpadded_Z = get_arg_val<uint32_t>(4);
-    const uint32_t num_total_Z = get_arg_val<uint32_t>(5);
-    const uint32_t num_unpadded_Y = get_arg_val<uint32_t>(6);
-    const uint32_t num_total_Y = get_arg_val<uint32_t>(7);
-    const uint32_t num_unpadded_X = get_arg_val<uint32_t>(8);
-    const uint32_t num_total_X = get_arg_val<uint32_t>(9);
-    const uint32_t unpadded_X_size = get_arg_val<uint32_t>(10);
-    const uint32_t padded_X_size = get_arg_val<uint32_t>(11);
-    const uint32_t padded_X_diff_size = get_arg_val<uint32_t>(12);
-    const uint32_t pad_value = get_arg_val<uint32_t>(13);
-    const uint32_t dst_buffer_l1_addr = get_arg_val<uint32_t>(14);
-
-    tt_l1_ptr std::uint32_t* dst_buffer = (tt_l1_ptr uint32_t*)(dst_buffer_l1_addr);
-    volatile tt_l1_ptr std::uint32_t* dst_pad = (volatile tt_l1_ptr uint32_t*)(dst_buffer_l1_addr + unpadded_X_size);
-
-    constexpr bool src0_is_dram = get_compile_time_arg_val(0) == 1;
-    constexpr bool dst_is_dram = get_compile_time_arg_val(1) == 1;
-    constexpr bool src_stick_size_is_pow2 = get_compile_time_arg_val(2) == 1;
-    constexpr uint32_t src_log_base_2_of_page_size = get_compile_time_arg_val(3);
-    constexpr bool dst_stick_size_is_pow2 = get_compile_time_arg_val(4) == 1;
-    constexpr uint32_t dst_log_base_2_of_page_size = get_compile_time_arg_val(5);
-
-    const auto s0 = get_interleaved_addr_gen<src0_is_dram, src_stick_size_is_pow2>(
-        src_addr, unpadded_X_size, src_log_base_2_of_page_size);
-    const auto s1 = get_interleaved_addr_gen<dst_is_dram, dst_stick_size_is_pow2>(
-        dst_addr, padded_X_size, dst_log_base_2_of_page_size);
-
-    uint32_t padded_X_size_by_4 = padded_X_size >> 2;
-    uint32_t padded_X_diff_size_by_4 = padded_X_diff_size >> 2;
-
-    uint32_t src_stick_id = 0;
-    uint32_t dst_stick_id = 0;
-    for (uint32_t w = 0; w < num_total_W; w++) {
-        for (uint32_t z = 0; z < num_total_Z; z++) {
-            for (uint32_t y = 0; y < num_total_Y; y++) {
-                if (y >= num_unpadded_Y || z >= num_unpadded_Z || w >= num_unpadded_W) {
-                    // pad the tile with pad_value
-                    for (uint32_t i = 0; i < padded_X_size_by_4; i++) {
-                        dst_buffer[i] = pad_value;
-                    }
-                } else {
-                    uint64_t src_noc_addr = get_noc_addr(src_stick_id, s0);
-                    noc_async_read(src_noc_addr, dst_buffer_l1_addr, unpadded_X_size);
-                    // Pad Columns first
-                    for (uint32_t i = 0; i < padded_X_diff_size_by_4; i++) {
-                        dst_pad[i] = pad_value;
-                    }
-                    noc_async_read_barrier();
-                    src_stick_id++;
-                }
-                uint64_t dst_noc_addr = get_noc_addr(dst_stick_id, s1);
-                noc_async_write(dst_buffer_l1_addr, dst_noc_addr, padded_X_size);
-                noc_async_write_barrier();
-                dst_stick_id++;
-            }
-        }
-    }
-}

ttnn/cpp/ttnn/operations/data_movement/pad/device/kernels/dataflow/pad_dims_rm_interleaved_opt.cpp
@@ -1,108 +1,0 @@
-// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
-//
-// SPDX-License-Identifier: Apache-2.0
-
-#include <stdint.h>
-#include "dataflow_api.h"
-
-inline __attribute__((always_inline)) void fill_with_val_async(
-    uint64_t in_noc_addr,
-    uint32_t begin_addr,
-    uint32_t begin_addr_aligned,
-    uint32_t size_nbytes,
-    uint32_t chunk_nbytes,
-    uint16_t pad_value) {
-    uint32_t curr_addr = begin_addr;
-    while (curr_addr < begin_addr_aligned && size_nbytes > 0) {
-        reinterpret_cast<uint16_t*>(curr_addr)[0] = pad_value;
-        curr_addr += 2;
-        size_nbytes -= 2;
-    }
-    uint32_t nchunks = size_nbytes / chunk_nbytes;
-    uint32_t rem_nbytes = size_nbytes % chunk_nbytes;
-    for (uint32_t i = 0; i < nchunks; ++i) {
-        noc_async_read(in_noc_addr, curr_addr, chunk_nbytes);
-        curr_addr += chunk_nbytes;
-    }
-    if (rem_nbytes > 0) {
-        noc_async_read(in_noc_addr, curr_addr, rem_nbytes);
-    }
-}
-
-void kernel_main() {
-    const uint32_t src_addr = get_arg_val<uint32_t>(0);
-    const uint32_t dst_addr = get_arg_val<uint32_t>(1);
-    const uint32_t num_unpadded_W = get_arg_val<uint32_t>(2);
-    const uint32_t num_total_W = get_arg_val<uint32_t>(3);
-    const uint32_t num_unpadded_Z = get_arg_val<uint32_t>(4);
-    const uint32_t num_total_Z = get_arg_val<uint32_t>(5);
-    const uint32_t num_unpadded_Y = get_arg_val<uint32_t>(6);
-    const uint32_t num_total_Y = get_arg_val<uint32_t>(7);
-    const uint32_t num_unpadded_X = get_arg_val<uint32_t>(8);
-    const uint32_t num_total_X = get_arg_val<uint32_t>(9);
-    const uint32_t unpadded_X_nbytes = get_arg_val<uint32_t>(10);
-    const uint32_t padded_X_nbytes = get_arg_val<uint32_t>(11);
-    const uint32_t padded_X_diff_nbytes = get_arg_val<uint32_t>(12);
-    const uint32_t pad_value_const_buffer_addr = get_arg_val<uint32_t>(13);
-    const uint32_t pad_value_const_buffer_nbytes =
-        get_arg_val<uint32_t>(14);  // assumed to be 64 bytes. TODO: generalize?
-    const uint32_t pad_value_packed = get_arg_val<uint32_t>(15);
-    const uint32_t dst_buffer_l1_addr = get_arg_val<uint32_t>(16);
-
-    const uint32_t dst_buffer_l1_addr_partial = dst_buffer_l1_addr + unpadded_X_nbytes;
-    const uint32_t dst_buffer_l1_addr_partial_aligned =
-        dst_buffer_l1_addr_partial + (32 - dst_buffer_l1_addr_partial % 32);
-
-    constexpr bool src0_is_dram = get_compile_time_arg_val(0) == 1;
-    constexpr bool dst_is_dram = get_compile_time_arg_val(1) == 1;
-    constexpr bool src_stick_size_is_pow2 = get_compile_time_arg_val(2) == 1;
-    constexpr uint32_t src_log_base_2_of_page_size = get_compile_time_arg_val(3);
-    constexpr uint32_t dst_log_base_2_of_page_size = get_compile_time_arg_val(5);
-
-    const auto s0 = get_interleaved_addr_gen<src0_is_dram, src_stick_size_is_pow2>(
-        src_addr, unpadded_X_nbytes, src_log_base_2_of_page_size);
-    const auto s1 = get_interleaved_addr_gen<dst_is_dram, dst_stick_size_is_pow2>(
-        dst_addr, padded_X_nbytes, dst_log_base_2_of_page_size);
-
-    const InterleavedPow2AddrGen<false> s_const = {
-        .bank_base_address = pad_value_const_buffer_addr,
-        .log_base_2_of_page_size = 6  // TODO: generalize. Currently hardcoded for 32 16b values (2^6 = 64)
-    };
-    uint64_t const_buffer_noc_addr = get_noc_addr(0, s_const);
-
-    uint16_t pad_value = pad_value_packed >> 16;
-
-    uint32_t src_stick_id = 0;
-    uint32_t dst_stick_id = 0;
-    for (uint32_t w = 0; w < num_total_W; ++w) {
-        for (uint32_t z = 0; z < num_total_Z; ++z) {
-            for (uint32_t y = 0; y < num_total_Y; ++y) {
-                if (y >= num_unpadded_Y || z >= num_unpadded_Z || w >= num_unpadded_W) {
-                    fill_with_val_async(
-                        const_buffer_noc_addr,
-                        dst_buffer_l1_addr,
-                        dst_buffer_l1_addr,
-                        padded_X_nbytes,
-                        pad_value_const_buffer_nbytes,
-                        pad_value);
-                } else {
-                    uint64_t src_noc_addr = get_noc_addr(src_stick_id, s0);
-                    noc_async_read(src_noc_addr, dst_buffer_l1_addr, unpadded_X_nbytes);
-                    fill_with_val_async(
-                        const_buffer_noc_addr,
-                        dst_buffer_l1_addr_partial,
-                        dst_buffer_l1_addr_partial_aligned,
-                        padded_X_diff_nbytes,
-                        pad_value_const_buffer_nbytes,
-                        pad_value);
-                    ++src_stick_id;
-                }
-                noc_async_read_barrier();
-                uint64_t dst_noc_addr = get_noc_addr(dst_stick_id, s1);
-                noc_async_write(dst_buffer_l1_addr, dst_noc_addr, padded_X_nbytes);
-                noc_async_write_barrier();
-                ++dst_stick_id;
-            }
-        }
-    }
-}

ttnn/cpp/ttnn/operations/data_movement/pad/device/kernels/dataflow/reader_pad_dims_rm_interleaved.cpp
@@ -51,8 +51,10 @@ void kernel_main() {
     const uint32_t full_unpadded_X_nbytes = get_arg_val<uint32_t>(23);
     const uint32_t num_local_W = get_arg_val<uint32_t>(26);
 
-    constexpr bool src0_is_dram = get_compile_time_arg_val(0) == 1;
-    constexpr bool src_stick_size_is_pow2 = get_compile_time_arg_val(2) == 1;
+    constexpr uint32_t page_size = get_compile_time_arg_val(0);
+    constexpr auto src_args = TensorAccessorArgs<2>();
+    constexpr auto dst_args = TensorAccessorArgs<src_args.next_compile_time_args_offset()>();
+    constexpr auto pad_tensor_args = TensorAccessorArgs<dst_args.next_compile_time_args_offset()>();
 
     constexpr uint32_t cb_id = tt::CBIndex::c_0;
 
@@ -61,20 +63,9 @@ void kernel_main() {
     const uint32_t l1_addr_align_offset =
         32 - l1_addr_partial % 32;  // NOTE: this is fine with double buffering since offset will be same for each page
 
-    // #if (src_stick_size_is_pow2)
-    //     constexpr uint32_t src_log_base_2_of_page_size = get_compile_time_arg_val(3);
-    //     const InterleavedPow2AddrGen<src0_is_dram> s0 = {
-    //         .bank_base_address = src_addr,
-    //         .log_base_2_of_page_size = src_log_base_2_of_page_size
-    //     };
-    // #else
-    const InterleavedAddrGen<src0_is_dram> s0 = {.bank_base_address = src_addr, .page_size = full_unpadded_X_nbytes};
-    // #endif
+    const auto s0 = TensorAccessor(src_args, src_addr, page_size);
 
-    const InterleavedPow2AddrGen<false> s_const = {
-        .bank_base_address = pad_value_const_buffer_addr,
-        .log_base_2_of_page_size = 6  // TODO: generalize. Currently hardcoded for 32 16b values (2^6 = 64)
-    };
+    const auto s_const = TensorAccessor(pad_tensor_args, pad_value_const_buffer_addr, pad_value_const_buffer_nbytes);
     const uint64_t const_buffer_noc_addr = get_noc_addr(0, s_const);
 
     uint16_t pad_value = pad_value_packed >> 16;

ttnn/cpp/ttnn/operations/data_movement/pad/device/kernels/dataflow/writer_pad_dims_rm_interleaved.cpp
@@ -20,20 +20,13 @@ void kernel_main() {
     const uint32_t dst_stick_offset = get_arg_val<uint32_t>(25);  // == start_src_stick_wi * elem_size
     const uint32_t num_local_W = get_arg_val<uint32_t>(26);
 
-    constexpr bool dst_is_dram = get_compile_time_arg_val(1) == 1;
-    constexpr bool dst_stick_size_is_pow2 = get_compile_time_arg_val(4) == 1;
+    constexpr uint32_t page_size = get_compile_time_arg_val(1);
+    constexpr auto src_args = TensorAccessorArgs<2>();
+    constexpr auto dst_args = TensorAccessorArgs<src_args.next_compile_time_args_offset()>();
 
     constexpr uint32_t cb_id = tt::CBIndex::c_0;
 
-    // #if (dst_stick_size_is_pow2)
-    //     constexpr uint32_t dst_log_base_2_of_page_size = get_compile_time_arg_val(5);
-    //     const InterleavedPow2AddrGen<dst_is_dram> s1 = {
-    //         .bank_base_address = dst_addr,
-    //         .log_base_2_of_page_size = dst_log_base_2_of_page_size
-    //     };
-    // #else
-    const InterleavedAddrGen<dst_is_dram> s1 = {.bank_base_address = dst_addr, .page_size = full_padded_X_nbytes};
-    // #endif
+    const auto s1 = TensorAccessor(dst_args, dst_addr, page_size);
 
     uint32_t dst_stick_id = start_dst_stick_id;
     uint32_t dst_stick_wi = start_dst_stick_wi;
@@ -43,7 +36,7 @@ void kernel_main() {
                 // DPRINT << "WR: " << w << ", " << z << ", " << y << ENDL();
                 cb_wait_front(cb_id, 1);
                 uint32_t l1_addr = get_read_ptr(cb_id);
-                uint64_t dst_noc_addr = get_noc_addr<dst_is_dram>(dst_stick_id, s1, dst_stick_offset);
+                uint64_t dst_noc_addr = get_noc_addr(dst_stick_id, s1, dst_stick_offset);
                 noc_async_write(l1_addr, dst_noc_addr, padded_X_nbytes);
                 noc_async_write_barrier();
                 ++dst_stick_id;

ttnn/cpp/ttnn/operations/data_movement/pad/device/kernels/dataflow/writer_pad_dims_rm_interleaved_v2.cpp
@@ -11,16 +11,11 @@ void kernel_main() {
     uint32_t start_id = get_arg_val<uint32_t>(3);
 
     constexpr uint32_t cb_out0 = get_compile_time_arg_val(0);
-    constexpr bool dst_is_dram = get_compile_time_arg_val(1) == 1;
-    constexpr uint32_t W_size_bytes = get_compile_time_arg_val(2);
-    constexpr uint32_t stick_size_padded_aligned = get_compile_time_arg_val(5);
+    constexpr uint32_t stick_size_bytes = get_compile_time_arg_val(1);
+    constexpr uint32_t stick_size_padded_aligned = get_compile_time_arg_val(2);
+    constexpr auto dst_args = TensorAccessorArgs<3>();
 
-    const uint32_t stick_size_bytes = W_size_bytes;
-
-    constexpr bool stick_size_is_pow2 = get_compile_time_arg_val(3) == 1;
-    constexpr uint32_t size = get_compile_time_arg_val(4);
-
-    const auto s = get_interleaved_addr_gen<dst_is_dram, stick_size_is_pow2>(dst_addr, size);
+    const auto s = TensorAccessor(dst_args, dst_addr, stick_size_bytes);
 
     uint32_t i_stick = start_id;
     for (uint32_t iter = 0; iter < num_sticks_per_core;) {

ttnn/cpp/ttnn/operations/data_movement/pad/device/kernels/dataflow/writer_unary_pad_dims_interleaved.cpp
@@ -19,13 +19,11 @@ void kernel_main() {
 
     constexpr uint32_t cb_id_out0 = get_compile_time_arg_val(0);
     constexpr uint32_t cb_id_out1 = get_compile_time_arg_val(1);
-    constexpr bool dst_is_dram = get_compile_time_arg_val(2) == 1;
+    constexpr auto dst_args = TensorAccessorArgs<2>();
 
     const uint32_t tile_size = get_tile_size(cb_id_out0);
-    const DataFormat data_format = get_dataformat(cb_id_out0);
 
-    const InterleavedAddrGenFast<dst_is_dram> s1 = {
-        .bank_base_address = dst_addr, .page_size = tile_size, .data_format = data_format};
+    const auto s1 = TensorAccessor(dst_args, dst_addr, tile_size);
 
     cb_reserve_back(cb_id_out1, 1);  // in this kernel we are not pushing anything into CBs, just using the space
 

ttnn/cpp/ttnn/operations/data_movement/pad/device/pad_program_factory.cpp
@@ -13,6 +13,7 @@
 #include "ttnn/operation.hpp"
 #include "ttnn/operations/data_movement/common/common.hpp"
 #include <tt-metalium/tt_align.hpp>
+#include <tt-metalium/tensor_accessor_args.hpp>
 
 static const uint32_t max_read_size = 2048;  // max read size in bytes for reader and writer kernels
 using namespace tt::constants;
@@ -64,21 +65,10 @@ operation::ProgramWithCallbacks pad_rm_reader_writer(
         tt::tt_metal::CircularBufferConfig(cb_npages * cb_pagesize, {{cb_id, in_df}}).set_page_size(cb_id, cb_pagesize);
     tt::tt_metal::CreateCircularBuffer(program, cores, cb_config);
 
-    bool src0_is_dram = src0_buffer->buffer_type() == BufferType::DRAM;
-    bool dst_is_dram = dst_buffer->buffer_type() == BufferType::DRAM;
-    bool src_stick_size_is_power_of_two = is_power_of_two_at_least_32(unpadded_row_size_nbytes);
-    uint32_t src_log2_stick_size =
-        src_stick_size_is_power_of_two ? (std::uint32_t)std::log2(unpadded_row_size_nbytes) : 0;
-    bool dst_stick_size_is_power_of_two = is_power_of_two_at_least_32(padded_row_size_nbytes);
-    uint32_t dst_log2_stick_size =
-        dst_stick_size_is_power_of_two ? (std::uint32_t)std::log2(padded_row_size_nbytes) : 0;
-    std::vector<uint32_t> reader_ct_args = {
-        (std::uint32_t)src0_is_dram,
-        (std::uint32_t)dst_is_dram,
-        (std::uint32_t)src_stick_size_is_power_of_two,
-        (std::uint32_t)src_log2_stick_size,
-        (std::uint32_t)dst_stick_size_is_power_of_two,
-        (std::uint32_t)dst_log2_stick_size};
+    std::vector<uint32_t> reader_ct_args = {unpadded_row_size_nbytes, padded_row_size_nbytes};
+    TensorAccessorArgs(*src0_buffer).append_to(reader_ct_args);
+    TensorAccessorArgs(*dst_buffer).append_to(reader_ct_args);
+    TensorAccessorArgs(*pad_value_const_tensor.buffer()).append_to(reader_ct_args);
     const std::vector<uint32_t>& writer_ct_args = reader_ct_args;
 
     bfloat16 bfloat_pad_value = bfloat16(pad_value);
@@ -258,13 +248,12 @@ operation::ProgramWithCallbacks pad_tile(
     // Reader compile-time args
     // Data is 32 byte aligned
     bool src0_is_dram = src0_buffer->buffer_type() == tt::tt_metal::BufferType::DRAM;
-    bool dst_is_dram = dst_buffer->buffer_type() == tt::tt_metal::BufferType::DRAM;
     std::vector<uint32_t> reader_compile_time_args = {// interleaved accessor args
                                                       (std::uint32_t)src0_is_dram};
     std::vector<uint32_t> writer_compile_time_args = {// interleaved accessor args
                                                       (std::uint32_t)src0_cb_index,
-                                                      (std::uint32_t)src1_cb_index,
-                                                      (std::uint32_t)dst_is_dram};
+                                                      (std::uint32_t)src1_cb_index};
+    TensorAccessorArgs(*dst_buffer).append_to(writer_compile_time_args);
     // Tilized reader
     tt::tt_metal::KernelHandle unary_reader_kernel_id = tt::tt_metal::CreateKernel(
         program,
@@ -515,21 +504,10 @@ operation::ProgramWithCallbacks pad_rm_reader_writer_multi_core(
         tt::tt_metal::CircularBufferConfig(cb_npages * cb_pagesize, {{cb_id, in_df}}).set_page_size(cb_id, cb_pagesize);
     tt::tt_metal::CreateCircularBuffer(program, all_cores, cb_config);
 
-    bool src0_is_dram = src0_buffer->buffer_type() == BufferType::DRAM;
-    bool dst_is_dram = dst_buffer->buffer_type() == BufferType::DRAM;
-    bool src_stick_size_is_power_of_two = is_power_of_two_at_least_32(unpadded_row_size_nbytes);
-    uint32_t src_log2_stick_size =
-        src_stick_size_is_power_of_two ? (std::uint32_t)std::log2(unpadded_row_size_nbytes) : 0;
-    bool dst_stick_size_is_power_of_two = is_power_of_two_at_least_32(padded_row_size_nbytes);
-    uint32_t dst_log2_stick_size =
-        dst_stick_size_is_power_of_two ? (std::uint32_t)std::log2(padded_row_size_nbytes) : 0;
-    std::vector<uint32_t> reader_ct_args = {
-        (std::uint32_t)src0_is_dram,
-        (std::uint32_t)dst_is_dram,
-        (std::uint32_t)src_stick_size_is_power_of_two,
-        (std::uint32_t)src_log2_stick_size,
-        (std::uint32_t)dst_stick_size_is_power_of_two,
-        (std::uint32_t)dst_log2_stick_size};
+    std::vector<uint32_t> reader_ct_args = {unpadded_row_size_nbytes, padded_row_size_nbytes};
+    TensorAccessorArgs(*src0_buffer).append_to(reader_ct_args);
+    TensorAccessorArgs(*dst_buffer).append_to(reader_ct_args);
+    TensorAccessorArgs(*pad_value_const_tensor.buffer()).append_to(reader_ct_args);
     std::vector<uint32_t> writer_ct_args = reader_ct_args;
 
     bfloat16 bfloat_pad_value = bfloat16(pad_value);
@@ -857,14 +835,12 @@ operation::ProgramWithCallbacks pad_rm_reader_writer_multi_core_v2(
     bfloat16 bfloat_pad_value = bfloat16(pad_value);
     uint32_t packed_pad_value = pack_two_bfloat16_into_uint32({bfloat_pad_value, bfloat_pad_value});
 
-    bool src0_is_dram = src0_buffer->buffer_type() == BufferType::DRAM;
     bool dst_is_dram = dst_buffer->buffer_type() == BufferType::DRAM;
     bool src_stick_size_is_power_of_two = is_power_of_two_at_least_32(stick_size);
     uint32_t src_log2_stick_size = src_stick_size_is_power_of_two ? (std::uint32_t)std::log2(stick_size) : 0;
     bool dst_stick_size_is_power_of_two = is_power_of_two_at_least_32(stick_size_padded);
     uint32_t dst_log2_stick_size = dst_stick_size_is_power_of_two ? (std::uint32_t)std::log2(stick_size_padded) : 0;
     std::vector<uint32_t> reader_ct_args = {
-        (std::uint32_t)src0_is_dram,
         (std::uint32_t)N + front_pad[-4],
         (std::uint32_t)H + front_pad[-2],
         (std::uint32_t)C + front_pad[-3],
@@ -883,17 +859,13 @@ operation::ProgramWithCallbacks pad_rm_reader_writer_multi_core_v2(
         (std::uint32_t)(stick_size_padded_front / row_major_min_bytes),
         (std::uint32_t)(stick_size_padded_end / row_major_min_bytes),
         (std::uint32_t)(stick_size_padded / row_major_min_bytes),
-        (std::uint32_t)src_stick_size_is_power_of_two,
-        (std::uint32_t)src_stick_size_is_power_of_two ? src_log2_stick_size : stick_size,
         (std::uint32_t)stick_size_padded_aligned,
         (std::uint32_t)unaligned};
+    TensorAccessorArgs(*src0_buffer).append_to(reader_ct_args);
+
     std::vector<uint32_t> writer_ct_args = {
-        (std::uint32_t)src0_cb_index,
-        (std::uint32_t)dst_is_dram,
-        (std::uint32_t)stick_size_padded,
-        (std::uint32_t)dst_stick_size_is_power_of_two,
-        (std::uint32_t)dst_stick_size_is_power_of_two ? dst_log2_stick_size : stick_size_padded,
-        (std::uint32_t)stick_size_padded_aligned};
+        (std::uint32_t)src0_cb_index, (std::uint32_t)stick_size_padded, (std::uint32_t)stick_size_padded_aligned};
+    TensorAccessorArgs(*dst_buffer).append_to(writer_ct_args);
 
     KernelHandle reader_kernel_id = CreateKernel(
         program,

commit c577fa8b7eab0c7fb7ec5fae5846e4380ff15afd

fix for a bug introduced in PR #26091, just some CT offset updates

ttnn/cpp/ttnn/operations/data_movement/pad/device/kernels/dataflow/reader_pad_dims_rm_interleaved_v2.cpp
@@ -50,11 +50,11 @@ void kernel_main() {
     uint32_t num_end_pad_sticks_read = 0;
     uint32_t num_sticks_padded_read = 0;
     if constexpr (not_pad_by_zero) {
-        packed_pad_value = kernel_compile_time_args[14];
-        row_major_min_bytes = kernel_compile_time_args[15];
-        num_front_pad_sticks_read = kernel_compile_time_args[16];
-        num_end_pad_sticks_read = kernel_compile_time_args[17];
-        num_sticks_padded_read = kernel_compile_time_args[18];
+        packed_pad_value = kernel_compile_time_args[13];
+        row_major_min_bytes = kernel_compile_time_args[14];
+        num_front_pad_sticks_read = kernel_compile_time_args[15];
+        num_end_pad_sticks_read = kernel_compile_time_args[16];
+        num_sticks_padded_read = kernel_compile_time_args[17];
     }
 
     constexpr uint32_t cb_in0 = tt::CBIndex::c_0;