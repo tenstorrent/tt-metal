854bdbcee231687b5b6b4a417a09a843a0f1236a
punith.sekar <punith.sekar@multicorewareinc.com>
2025-08-02 10:45:52 +0000

    occ_head

--- models/experimental/uniad/reference/utils.py
+++ models/experimental/uniad/reference/utils.py
@@ -601,3 +601,26 @@ def anchor_coordinate_transform(anchors, bbox_results, with_translation_transfor
             transformed_anchors = bbox_centers[:, None, None, None, :2] + transformed_anchors
         batched_anchors.append(transformed_anchors)
     return torch.stack(batched_anchors)
+
+
+def calculate_birds_eye_view_parameters(x_bounds, y_bounds, z_bounds):
+    """
+    Parameters
+    ----------
+        x_bounds: Forward direction in the ego-car.
+        y_bounds: Sides
+        z_bounds: Height
+
+    Returns
+    -------
+        bev_resolution: Bird's-eye view bev_resolution
+        bev_start_position Bird's-eye view first element
+        bev_dimension Bird's-eye view tensor spatial dimension
+    """
+    bev_resolution = torch.tensor([row[2] for row in [x_bounds, y_bounds, z_bounds]])
+    bev_start_position = torch.tensor([row[0] + row[2] / 2.0 for row in [x_bounds, y_bounds, z_bounds]])
+    bev_dimension = torch.tensor(
+        [(row[1] - row[0]) / row[2] for row in [x_bounds, y_bounds, z_bounds]], dtype=torch.long
+    )
+
+    return bev_resolution, bev_start_position, bev_dimension
