cecf06d847e9e691e160676f405514356810fc10
punith.sekar <punith.sekar@multicorewareinc.com>
2025-08-03 11:45:04 +0000

    ttnn head

---
+++ models/experimental/uniad/tt/ttnn_utils.py
@@ -0,0 +1,155 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+
+import ttnn
+
+import torch
+import torch.nn.functional as F
+import numpy as np
+
+
+def inverse_sigmoid(x, eps: float = 1e-5):
+    x = ttnn.to_layout(x, layout=ttnn.TILE_LAYOUT)
+    x = ttnn.clamp(x, min=0, max=1)
+    x1 = ttnn.clamp(x, min=eps)
+    if len(x.shape) == 3:
+        x_temp = ttnn.ones(shape=[x.shape[0], x.shape[1], x.shape[2]], layout=ttnn.TILE_LAYOUT, device=x.device())
+    elif len(x.shape) == 2:
+        x_temp = ttnn.ones(shape=[x.shape[0], x.shape[1]], layout=ttnn.TILE_LAYOUT, device=x.device())
+    else:
+        x_temp = ttnn.ones(
+            shape=[x.shape[0], x.shape[1], x.shape[2], x.shape[3]], layout=ttnn.TILE_LAYOUT, device=x.device()
+        )
+    x_temp = x_temp - x
+    x2 = ttnn.clamp(x_temp, min=eps)
+    return ttnn.log(ttnn.div(x1, x2))
+
+
+def multi_scale_deformable_attn_pytorch(
+    value,
+    value_spatial_shapes,
+    level_start_index,
+    sampling_locations,
+    attention_weights,
+    im2col_step,
+    device,
+):
+    bs, num_keys, num_heads, head_dim = value.shape
+    num_levels = value_spatial_shapes.shape[0]
+    num_queries = sampling_locations.shape[1]
+    num_points = sampling_locations.shape[4]
+
+    # Split value into a list of tensors for each level
+    value_list = []
+    start = 0
+    for lvl in range(num_levels):
+        h_l, w_l = value_spatial_shapes[lvl]
+        h_l = int(h_l.item())
+        w_l = int(w_l.item())
+        len_l = h_l * w_l
+        value_l = value[:, start : start + len_l, :, :]
+        value_list.append(value_l)
+        start += len_l
+
+    # Normalize sampling locations to [-1, 1]
+    sampling_grids = []
+    for lvl in range(num_levels):
+        h_l, w_l = value_spatial_shapes[lvl]
+        h_l = int(h_l.item())
+        w_l = int(w_l.item())
+        grid = sampling_locations[:, :, :, lvl, :, :]
+        grid = ttnn.to_memory_config(grid, memory_config=ttnn.DRAM_MEMORY_CONFIG)
+        grid = ttnn.clone(grid)
+        grid = ttnn.to_torch(grid)
+        grid[..., 0] = grid[..., 0] / w_l * 2 - 1
+        grid[..., 1] = grid[..., 1] / h_l * 2 - 1
+        grid = ttnn.from_torch(grid, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device)
+        sampling_grids.append(grid)
+
+    # Perform sampling and attention
+    output = ttnn.zeros(
+        [bs, num_queries, num_heads, head_dim], device=device, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT
+    )
+    for lvl in range(num_levels):
+        h_l, w_l = value_spatial_shapes[lvl]
+        h_l = int(h_l.item())
+        w_l = int(w_l.item())
+        value_l = ttnn.permute(value_list[lvl], (0, 2, 3, 1))
+        value_l = ttnn.reshape(value_l, (bs * num_heads, head_dim, h_l, w_l))
+        grid = ttnn.permute(sampling_grids[lvl], (0, 2, 1, 3, 4))
+        grid = ttnn.reshape(grid, (bs * num_heads, num_queries * num_points, 1, 2))
+        value_l = ttnn.to_torch(value_l).to(dtype=torch.float)
+        grid = ttnn.to_torch(grid).to(dtype=torch.float)
+        sampled = F.grid_sample(value_l, grid, mode="bilinear", padding_mode="zeros", align_corners=False)
+        sampled = ttnn.from_torch(sampled, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+        sampled = ttnn.reshape(sampled, (bs, num_heads, head_dim, num_queries, num_points))
+        sampled = ttnn.permute(sampled, (0, 3, 1, 4, 2))
+        attn = attention_weights[:, :, :, lvl, :]
+        attn = ttnn.unsqueeze(attn, -1)
+        output += ttnn.sum((sampled * attn), -2)
+
+    output = ttnn.reshape(output, (bs, num_queries, num_heads * head_dim))
+
+    return output
+
+
+def limit_period(val, offset: float = 0.5, period: float = np.pi):
+    # limited_val = val - torch.floor(val / period + offset) * period
+    tmp_val = ttnn.add(ttnn.div(val, period), offset)
+    tmp_val = ttnn.floor(tmp_val)
+    tmp_val = ttnn.mul(tmp_val, period)
+
+    limited_val = ttnn.sub(val, tmp_val)
+    return limited_val
+
+
+def denormalize_bbox(normalized_bboxes, pc_range):
+    # rotation
+    rot_sine = normalized_bboxes[..., 6:7]
+
+    rot_cosine = normalized_bboxes[..., 7:8]
+    rot_sine = ttnn.to_layout(rot_sine, layout=ttnn.TILE_LAYOUT)
+    rot_cosine = ttnn.to_layout(rot_cosine, layout=ttnn.TILE_LAYOUT)
+    rot = ttnn.atan2(rot_sine, rot_cosine)
+
+    # rot = -rot - np.pi / 2
+    rot = ttnn.mul(rot, -1)
+    rot = ttnn.sub(rot, np.pi / 2)
+
+    rot = limit_period(rot, period=np.pi * 2)
+
+    # center in the bev
+    cx = normalized_bboxes[..., 0:1]
+    cy = normalized_bboxes[..., 1:2]
+    cz = normalized_bboxes[..., 4:5]
+
+    # size
+    length = normalized_bboxes[..., 2:3]
+    width = normalized_bboxes[..., 3:4]
+    height = normalized_bboxes[..., 5:6]
+
+    width = ttnn.to_layout(width, layout=ttnn.TILE_LAYOUT)
+    length = ttnn.to_layout(length, layout=ttnn.TILE_LAYOUT)
+    height = ttnn.to_layout(height, layout=ttnn.TILE_LAYOUT)
+
+    width = ttnn.exp(width)
+    length = ttnn.exp(length)
+    height = ttnn.exp(height)
+    if normalized_bboxes.shape[-1] > 8:
+        # velocity
+        vx = normalized_bboxes[:, 8:9]
+        vy = normalized_bboxes[:, 9:10]
+        cx = ttnn.to_layout(cx, layout=ttnn.TILE_LAYOUT)
+        cy = ttnn.to_layout(cy, layout=ttnn.TILE_LAYOUT)
+        cz = ttnn.to_layout(cz, layout=ttnn.TILE_LAYOUT)
+        vx = ttnn.to_layout(vx, layout=ttnn.TILE_LAYOUT)
+        vy = ttnn.to_layout(vy, layout=ttnn.TILE_LAYOUT)
+        denormalized_bboxes = ttnn.concat([cx, cy, cz, length, width, height, rot, vx, vy], dim=-1)
+    else:
+        cx = ttnn.to_layout(cx, layout=ttnn.TILE_LAYOUT)
+        cy = ttnn.to_layout(cy, layout=ttnn.TILE_LAYOUT)
+        cz = ttnn.to_layout(cz, layout=ttnn.TILE_LAYOUT)
+        denormalized_bboxes = torch.concat([cx, cy, cz, length, width, height, rot], dim=-1)
+
+    return denormalized_bboxes
