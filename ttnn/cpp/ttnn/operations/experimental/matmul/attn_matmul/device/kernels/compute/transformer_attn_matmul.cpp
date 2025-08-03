28d1dfd34f0368877764d1192bd8dad17dc0b031
Pavle Milenkovic <pmilenkovic@tenstorrent.com>
2025-08-03 15:47:16 +0000

    #25421: Fix attn_matmul kernel to work for bfloat8_b

--- ttnn/cpp/ttnn/operations/experimental/matmul/attn_matmul/device/kernels/compute/transformer_attn_matmul.cpp
+++ ttnn/cpp/ttnn/operations/experimental/matmul/attn_matmul/device/kernels/compute/transformer_attn_matmul.cpp
@@ -84,7 +84,7 @@ void MAIN {
                 cb_push_back(out_cb_id, onetile);
 
                 cb_pop_front(cb_intermed2, onetile);
-                tilize_uninit(cb_intermed2, out_cb_id);
+                tilize_uninit_with_dt(cb_intermed2, cb_in1, out_cb_id);
 
                 pack_reconfig_data_format(out_cb_id, cb_intermed0);
                 // Workaround, needs to be reverted to the following when fix is found:
