5b4a5268931a08cc62137934a1f8b45e4592778a
punith.sekar <punith.sekar@multicorewareinc.com>
2025-08-03 11:13:24 +0000

    clean up

--- models/experimental/uniad/reference/detr_transformer_decoder.py
+++ models/experimental/uniad/reference/detr_transformer_decoder.py
@@ -9,13 +9,7 @@ import copy
 
 from models.experimental.uniad.reference.detr_transformer_encoder import MultiScaleDeformableAttention
 from models.experimental.uniad.reference.ffn import FFN
-
-
-def inverse_sigmoid(x, eps=1e-5):
-    x = x.clamp(min=0, max=1)
-    x1 = x.clamp(min=eps)
-    x2 = (1 - x).clamp(min=eps)
-    return torch.log(x1 / x2)
+from models.experimental.uniad.reference.utils import inverse_sigmoid
 
 
 class MultiheadAttention(nn.Module):
