130779feef24744fd3952aa9c025af8cf40e79f7
mcw-anasuya <mcw-anair@ext.tenstorrent.com>
2025-08-04 13:25:41 +0000

    Provide int32 support for square

--- tests/ttnn/unit_tests/operations/eltwise/test_math.py
+++ tests/ttnn/unit_tests/operations/eltwise/test_math.py
@@ -26,6 +26,8 @@ def run_math_unary_test(device, h, w, ttnn_function, pcc=0.9999):
     input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
     output_tensor = ttnn_function(input_tensor)
     output_tensor = ttnn.to_torch(output_tensor)
+    print(torch_output_tensor)
+    print(output_tensor)
 
     assert_with_pcc(torch_output_tensor, output_tensor, pcc)
 
--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_square.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_square.h
@@ -1,30 +1,31 @@
-// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+// SPDX-FileCopyrightText: © 2023 Tenstorrent AI ULC
 //
 // SPDX-License-Identifier: Apache-2.0
 
 #pragma once
 
-#include "ckernel.h"
-#include "ckernel_defs.h"
-#include "noc_nonblocking_api.h"
+#include "sfpi.h"
+#include "ckernel_sfpu_mul_int32.h"
 
-using namespace sfpi;
-
-namespace ckernel {
-namespace sfpu {
+namespace ckernel::sfpu {
 
 template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
 inline void calculate_square() {
 #pragma GCC unroll 8
     for (int d = 0; d < ITERATIONS; d++) {
-        vFloat in = dst_reg[0];
-        vFloat result = in * in;
+        sfpi::vFloat in = sfpi::dst_reg[0];
+        sfpi::vFloat result = in * in;
 
-        dst_reg[0] = result;
+        sfpi::dst_reg[0] = result;
 
-        dst_reg++;
+        sfpi::dst_reg++;
     }
 }
 
-}  // namespace sfpu
-}  // namespace ckernel
+template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
+inline void calculate_square_int32() {
+    constexpr uint dst_offset = 0;
+    mul_int32<APPROXIMATION_MODE, ITERATIONS>(dst_offset);
+}
+
+}  // namespace ckernel::sfpu
