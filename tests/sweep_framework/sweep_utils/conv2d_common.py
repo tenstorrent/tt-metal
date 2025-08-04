0f76e02a3c4610c2c782f9eca5773971e3a8eafd
Pavle Josipović <pavlejosipovic@users.noreply.github.com>
2025-08-04 16:02:24 +0200

    Remove enable_subblock_padding from conv2d (#26200)
    Remove enable_subblock_padding from conv2d.
    Logic for selecting subblock dims in conv2d_utils.cpp is good enough and
    can't be improved by post processing it.
    
    In all cases where enable_subblock_padding was used, it wasn't making
    any impact, subblock dims would be the same.
    
    In addition in conv2d_op_sharded_program_factory.cpp code for mcast
    logic has been consolidated.
    Also function declaration for width shared factory has been moved out of
    conv2d_op_sharded_program_factory.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16719315895)
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/runs/16706649752)
    - [x] [Nightly tt-metal L2
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16707749716)
    - [x] [(Single-card) Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16706654480)
    
    Co-authored-by: Pavle Josipovic <pjosipovic@tenstorrent.com>

--- tests/sweep_framework/sweep_utils/conv2d_common.py
+++ tests/sweep_framework/sweep_utils/conv2d_common.py
@@ -64,7 +64,6 @@ def run_conv2d_full_sweep(
     has_bias,
     enable_act_double_buffer,
     enable_split_reader,
-    enable_subblock_padding,
     activations_dtype,
     weights_dtype,
     math_fidelity,
@@ -128,7 +127,6 @@ def run_conv2d_full_sweep(
         output_layout=output_layout,
         enable_act_double_buffer=enable_act_double_buffer,
         enable_split_reader=enable_split_reader,
-        enable_subblock_padding=enable_subblock_padding,
     )
     compute_config = ttnn.init_device_compute_kernel_config(
         device.arch(),
