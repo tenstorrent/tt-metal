56db4541f334c972df01c35995709e987ffc8809
Adrian Morrison <amorrison@tenstorrent.com>
2025-08-04 16:55:46 -0400

    point-to-point send/receive op (#22880)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/21391
    https://github.com/tenstorrent/tt-metal/issues/23425
    
    ### Problem description
    A point-to-point send/receive Op was required.
     
    ### What's changed
    - send_receive op built.
    - A couple of tiny helper functions added to `data_movement/common`
    - unit test specifically for MoE use case
    
    ### TODO
    - fix issues with unaligned RM pages
    - ~add semaphore wait to writer to deal with device drift~
    - ~Clean up program dispatch so only sender/receiver devices run
    kernels~
    https://github.com/tenstorrent/tt-metal/issues/25905
    
    Note: it's currently not possible actually allocate a Tensor on a single
    device such that it can be used for distributed Ops so at the moment the
    inputs and outputs are the single device shards of multi-device mesh
    tensors which is awkward but tt_distributed improvements in this area
    are forthcoming.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/16527136996
    - [x] T3K test
    https://github.com/tenstorrent/tt-metal/actions/runs/16503868841
    - [x] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: asaigal <asaigal@tenstorrent.com>

---
+++ ttnn/cpp/ttnn/operations/point_to_point/point_to_point.hpp
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+///
+#pragma once
+#include <tt-metalium/mesh_coord.hpp>
+
+#include "ttnn/core.hpp"
+#include "ttnn/decorators.hpp"
+#include "ttnn/global_semaphore.hpp"
+#include "ttnn/operations/ccl/ccl_host_types.hpp"
+
+namespace ttnn {
+namespace operations::point_to_point {
+
+struct ExecutePointToPoint {
+    static ttnn::Tensor invoke(
+        QueueId queue_id,
+        const ttnn::Tensor& input_tensor,
+        const MeshCoordinate& receiver_coord,
+        const MeshCoordinate& sender_coord,
+        ccl::Topology topology,
+        const GlobalSemaphore& semaphore,
+        const std::optional<ttnn::Tensor>& optional_output_tensor = std::nullopt);
+};
+}  // namespace operations::point_to_point
+
+constexpr auto point_to_point =
+    ttnn::register_operation<"ttnn::point_to_point", ttnn::operations::point_to_point::ExecutePointToPoint>();
+
+}  // namespace ttnn
