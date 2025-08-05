aa6133e7c03a2f45e6ba8eef7325fe5a54008804
Ilija Anastasijevic <ianastasijevic@tenstorrent.com>
2025-08-05 12:26:48 +0000

    Empty module for ASPP

---
+++ models/experimental/panoptic_deeplab/tt/tt_aspp.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+# SPDX-License-Identifier: Apache-2.0
+
+import torch.nn as nn
+
+
+class TtASPP(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, input_tensor):
+        pass
