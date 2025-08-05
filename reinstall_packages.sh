f21c145483399ca8b1f617fcb517c328bb5f0cb8
Evan Banerjee <ebanerjee@tenstorrent.com>
2025-08-05 11:33:09 -0700

    reverting dockerfile and changing names

---
+++ reinstall_packages.sh
@@ -0,0 +1,4 @@
+sudo apt install ./.build/default/tt-metalium_*.deb
+sudo apt install ./.build/default/tt-metalium-jit_*.deb
+sudo apt install ./.build/default/tt-nn_*.deb
+sudo apt install ./.build/default/tt-nn-validation_*.deb
