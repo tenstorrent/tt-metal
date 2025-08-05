f0661838ea2b951364d3a104f307e20b56883390
Saad Jameel <sjameel@tenstorrent.com>
2025-08-05 22:41:05 +0000

    use 4 links for 6U test and 3 links for 4u

--- tests/ttnn/unit_tests/operations/ccl/test_all_to_all_combine_6U.py
+++ tests/ttnn/unit_tests/operations/ccl/test_all_to_all_combine_6U.py
@@ -32,7 +32,7 @@ from tests.ttnn.unit_tests.operations.ccl.test_all_to_all_combine_t3000 import (
 @pytest.mark.parametrize("seq", [1, 2])
 @pytest.mark.parametrize("local_reduce", [False, True])
 @pytest.mark.parametrize("num_iters", [2])
-@pytest.mark.parametrize("num_links", [1])
+@pytest.mark.parametrize("num_links", [4])
 @pytest.mark.parametrize("topology", [ttnn.Topology.Linear])
 @pytest.mark.parametrize("dtype", [ttnn.bfloat16])
 @pytest.mark.parametrize("input_memory_config", [ttnn.DRAM_MEMORY_CONFIG, ttnn.L1_MEMORY_CONFIG], ids=["dram", "l1"])
