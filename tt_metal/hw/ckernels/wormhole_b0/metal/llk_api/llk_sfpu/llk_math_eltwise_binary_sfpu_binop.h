8447478e68825cb561877c12d9cc38312a91e3b2
Nathan Maurice <nmaurice@tenstorrent.com>
2025-08-05 08:01:35 +0000

    Add is_fp32_dest_acc_en to binary_power LLK

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_binary_sfpu_binop.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_binary_sfpu_binop.h
@@ -19,9 +19,13 @@ inline void llk_math_eltwise_binary_sfpu_binop_init() {
 }
 
 template <bool APPROXIMATE, ckernel::BinaryOp BINOP>
-inline void llk_math_eltwise_binary_sfpu_binop(uint dst_index0, uint32_t dst_index1, int vector_mode = VectorMode::RC) {
+inline void llk_math_eltwise_binary_sfpu_binop(
+    uint dst_index0, uint32_t dst_index1, int vector_mode = VectorMode::RC, bool is_fp32_dest_acc_en = false) {
     _llk_math_eltwise_binary_sfpu_params_<APPROXIMATE>(
-        ckernel::sfpu::calculate_sfpu_binary<APPROXIMATE, BINOP>, dst_index0, dst_index1, vector_mode);
+        ckernel::sfpu::calculate_sfpu_binary<APPROXIMATE, BINOP, 8, is_fp32_dest_acc_en>,
+        dst_index0,
+        dst_index1,
+        vector_mode);
 }
 
 }  // namespace ckernel
