09eb4b6905fe651a05af5235ec3d5aef7afc30c8
Yousef <yalrawwash@tesntorrent.com>
2025-08-06 15:30:48 +0000

    Revert "Resolving Llama 70B model device perf hang on TG and 6U (#25978)"
    This reverts commit a654b649cf75022564a3e2aa08510a56a42a0f5b.

--- ttnn/cpp/ttnn/operations/transformer/sdpa_decode/device/kernels/compute/sdpa_flash_decode.cpp
+++ ttnn/cpp/ttnn/operations/transformer/sdpa_decode/device/kernels/compute/sdpa_flash_decode.cpp
@@ -308,7 +308,6 @@ void MAIN {
                  * else:
                  *  cur_max = max(qk, dim=-1)
                  */
-
                 reduce_c<PoolType::MAX, ReduceDim::REDUCE_ROW, cb_qk_im, cb_identity_scale_in, Sq_chunk_t, vector_mode>(
                     cb_cur_max, cb_prev_max, Sk_chunk_t_dynamic, k_chunk > k_chunk_start);
 
@@ -330,10 +329,8 @@ void MAIN {
 
                 reconfig_data_format(cb_qk_im, cb_identity_scale_in);
                 pack_reconfig_data_format(cb_cur_sum);
-                uint32_t cb_sum_dest = k_chunk > k_chunk_start ? cb_cur_sum : cb_prev_sum;
-
                 reduce_c<PoolType::SUM, ReduceDim::REDUCE_ROW, cb_qk_im, cb_identity_scale_in, Sq_chunk_t, vector_mode>(
-                    cb_sum_dest, cb_sum_dest, Sk_chunk_t_dynamic, false);
+                    cb_cur_sum, cb_cur_sum, Sk_chunk_t_dynamic, false);
 
                 /* OUT_IM = QK @ V_CHUNK */
                 reconfig_data_format(cb_qk_im, cb_v_in);  // DEBUG
@@ -381,7 +378,7 @@ void MAIN {
                     /* cb_cur_sum += cb_prev_sum */
                     reconfig_data_format(cb_cur_sum, cb_prev_sum);  // DEBUG
                     pack_reconfig_data_format(cb_cur_sum);
-                    add_block(cb_cur_sum, cb_prev_sum, cb_prev_sum, Sq_chunk_t);
+                    add_block_inplace<true>(cb_cur_sum, cb_prev_sum, Sq_chunk_t);
 
                     /* cb_out_accumulate_im += cb_out_im */
                     reconfig_data_format(cb_out_accumulate_im, cb_out_im);  // DEBUG
@@ -392,12 +389,13 @@ void MAIN {
                 if (k_chunk < k_chunk_end - 1 || do_reduce) {
                     reconfig_data_format(cb_cur_max, cb_cur_max);  // DEBUG
                     pack_reconfig_data_format(cb_prev_max);
+                    move_block<true>(cb_cur_sum, cb_prev_sum, Sq_chunk_t);
                     move_block<true>(cb_cur_max, cb_prev_max, Sq_chunk_t);
                 } else {
                     // Write o, m, l into cb_out
                     move_block<true>(cb_out_accumulate_im, cb_out_o, out_chunk_tiles);
                     move_block<true>(cb_cur_max, cb_out_m, Sq_chunk_t);
-                    move_block<true>(cb_prev_sum, cb_out_l, Sq_chunk_t);
+                    move_block<true>(cb_cur_sum, cb_out_l, Sq_chunk_t);
                 }
             }
         }
