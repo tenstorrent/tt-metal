tenstorrent/tt-metal

commit 2eb36c2e5cb8b53ad35b58576bbfa6e8552e2cda

Refactor test_rw_buffer to Google Benchmark (#25304)
### Ticket
#25132 (Sub task 1).

### Problem description
Create a Google Benchmark version of
`.../perf_microbenchmark/3_pcie_transfer/test_rw_buffer.cpp` with the
benchmark parameters from
`.../perf_microbenchmark/3_pcie_transfer/sweep_rw_buffer.sh`.

### What's changed
1. Copied `.../perf_microbenchmark/3_pcie_transfer/test_rw_buffer.cpp`
to `.../perf_microbenchmark/dispatch/benchmark_rw_buffer.cpp`
2. Refactored the new benchmark test to use Google Benchmark
3. Encoded the benchmark parameter matrix into the new benchmark

### Checklist
- [x] [All post
commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
CI passes (Run
[here](https://github.com/tenstorrent/tt-metal/actions/runs/16377640685))
- [ ] [Blackhole Post
commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
CI with demo tests passes (if applicable)
- [ ] [Model
regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
CI passes (if applicable)
- [ ] [Device performance
regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
CI passes (if applicable)
- [ ] (For models and ops writers) [Single-card demo
tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
CI passes (if applicable) See [recommended dev
flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
- [ ] [Galaxy
quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
CI passes (if applicable)
- [ ] [TG demo tests, for
Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
CI passes, if applicable, because of current Llama work
- [ ] (For runtime and ops writers) [T3000 unit
tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)

tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/benchmark_rw_buffer.cpp
@@ -1,0 +1,113 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <chrono>
+#include <fmt/base.h>
+#include <fmt/format.h>
+#include <stdint.h>
+#include <cstdint>
+#include <tt-metalium/bfloat16.hpp>
+#include <tt-metalium/host_api.hpp>
+#include <cmath>
+#include <cstring>
+#include <memory>
+#include <optional>
+#include <string>
+#include <variant>
+#include <vector>
+
+#include <tt-metalium/assert.hpp>
+#include <tt-metalium/buffer.hpp>
+#include <tt-metalium/buffer_types.hpp>
+#include <tt-metalium/device.hpp>
+#include <tt-logger/tt-logger.hpp>
+#include <benchmark/benchmark.h>
+#include "command_queue.hpp"
+#include "test_common.hpp"
+#include "impl/context/metal_context.hpp"
+
+using namespace tt;
+using namespace tt::tt_metal;
+using std::chrono::duration_cast;
+using std::chrono::microseconds;
+
+////////////////////////////////////////////////////////////////////////////////
+// This test measures the bandwidth of host-to-device data transfer and
+// device-to-host data transfer. It uses EnqueueReadBuffer and
+// EnqueueWriteBuffer APIs to transfer the data. The device memory object
+// (buffer) will be in DRAM.
+//
+// Benchmark Matrix:
+// Page Size: 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768
+// Transfer Size: 32k, 512M
+// Read & Write
+// Device: 0 (local), 1 (remote)
+//
+////////////////////////////////////////////////////////////////////////////////
+
+static const auto PAGE_SIZE_ARGS = benchmark::CreateRange(32, 32768, 2);
+static const std::vector<int64_t> TRANSFER_SIZE_ARGS{32 * 1024, 512 * 1024 * 1024};
+static const auto BENCHMARK_ARGS = {PAGE_SIZE_ARGS, TRANSFER_SIZE_ARGS};
+
+static void BM_write(benchmark::State& state, tt_metal::IDevice* device) {
+    auto page_size = state.range(0);
+    auto transfer_size = state.range(1);
+    auto random_buffer_seed = std::chrono::system_clock::now().time_since_epoch().count();
+
+    auto device_buffer = Buffer::create(device, transfer_size, page_size, BufferType::DRAM);
+    auto host_buffer = create_random_vector_of_bfloat16(transfer_size, 1000, random_buffer_seed);
+
+    for (auto _ : state) {
+        EnqueueWriteBuffer(device->command_queue(), device_buffer, host_buffer, false);
+        Finish(device->command_queue());
+    }
+
+    state.SetBytesProcessed(transfer_size * state.iterations());
+}
+
+static void BM_read(benchmark::State& state, tt_metal::IDevice* device) {
+    auto page_size = state.range(0);
+    auto transfer_size = state.range(1);
+
+    auto device_buffer = Buffer::create(device, transfer_size, page_size, BufferType::DRAM);
+    std::vector<uint32_t> host_buffer;
+
+    for (auto _ : state) {
+        EnqueueReadBuffer(device->command_queue(), device_buffer, host_buffer, true);
+    }
+
+    state.SetBytesProcessed(transfer_size * state.iterations());
+}
+
+int main(int argc, char** argv) {
+    benchmark::Initialize(&argc, argv);
+
+    // TODO: Test Across Multiple devices.
+    auto device_id = 0;
+    if (device_id >= MetalContext::instance().get_cluster().number_of_devices()) {
+        log_info(LogTest, "Skip! Device id {} is not applicable on this system", device_id);
+        return 1;
+    }
+
+    tt_metal::IDevice* device = tt_metal::CreateDevice(device_id);
+
+    if (!device->using_fast_dispatch()) {
+        log_info(LogTest, "Skip! This test needs to be run with fast dispatch enabled");
+        return 1;
+    }
+
+    benchmark::RegisterBenchmark("EnqueueWriteBuffer", BM_write, device)
+        ->ArgsProduct(BENCHMARK_ARGS)
+        // Google Benchmark uses CPU time to calculate throughput by default, which is not suitable for this
+        // benchmark
+        ->UseRealTime();
+
+    benchmark::RegisterBenchmark("EnqueueReadBuffer", BM_read, device)->ArgsProduct(BENCHMARK_ARGS)->UseRealTime();
+
+    benchmark::RunSpecifiedBenchmarks();
+    tt_metal::CloseDevice(device);
+    benchmark::Shutdown();
+
+    return 0;
+}

tests/tt_metal/tt_metal/perf_microbenchmark/CMakeLists.txt
@@ -1,5 +1,6 @@
 set(PERF_MICROBENCH_TESTS_SRCS
     dispatch/test_pgm_dispatch.cpp
+    dispatch/benchmark_rw_buffer.cpp
     ethernet/test_ethernet_read_and_send_data.cpp
     ethernet/test_workers_and_erisc_datamover_unidirectional.cpp
     ethernet/test_ethernet_bidirectional_bandwidth_no_edm.cpp
@@ -65,9 +66,14 @@ foreach(TEST_SRC ${PERF_MICROBENCH_TESTS_SRCS})
 
     add_executable(${TEST_TARGET} ${TEST_SRC})
     target_link_libraries(${TEST_TARGET} PRIVATE test_metal_common_libs)
+
     if(${TEST_SRC} STREQUAL "dispatch/test_pgm_dispatch.cpp")
         target_link_libraries(${TEST_TARGET} PRIVATE benchmark::benchmark)
     endif()
+    if(${TEST_SRC} STREQUAL "dispatch/benchmark_rw_buffer.cpp")
+        target_link_libraries(${TEST_TARGET} PRIVATE benchmark::benchmark)
+    endif()
+
     if(${TEST_SRC} STREQUAL "routing/test_tt_fabric.cpp")
         target_link_libraries(${TEST_TARGET} PRIVATE yaml-cpp::yaml-cpp)
     endif()

commit 15f914fa1099a42a27389be1c3e52f87e2a5cd64

Wire up new benchmark_rw_buffer into CI (#25918)
### Ticket
#25132

### Problem description
benchmark_rw_buffer introduced by #25304 and #25791 is not checked in
CI.

### What's changed
Wire up benchmark_rw_buffer into the fast-dispatch-frequent-tests CI
pipeline with performance comparison script and golden output.

### Example output:
```
Comparing throughput benchmarks (<_io.TextIOWrapper name='mock_golden.json' mode='r' encoding='UTF-8'> vs <_io.TextIOWrapper name='mock_result.json' mode='r' encoding='UTF-8'>)...
Note: Benchmark name follows: Function/ Page Size/ Transfer Size/ Device ID
Tolerance is 5%.
----------------------------------------------------
Benchmark FAILED:
----------------------------------------------------
# normal benchmarks: 1
# failed benchmarks: 1
# benchmarks outside of expected variance: 1
# mismatched benchmarks: 2
# total benchmarks: 4
----------------------------------------------------
Successed benchmarks: (bytes per second, result vs golden)
PASSED | Benchmark normal_variance_1pct: 11830000000.00 vs 11943259164.69 (diff: -0.95%)
----------------------------------------------------
Failed benchmarks:
FAILED | Benchmark failed_test failed to run: Memory allocation failed
----------------------------------------------------
Missing benchmarks:
FAILED | Benchmark missing_test: missing from results
FAILED | Benchmark excess_test: excess from results
----------------------------------------------------
Benchmarks outside of expected variance: (bytes per second, result vs golden)
FAILED | Benchmark normal_variance_6pct: 205969150.09 vs 218264539.10 (diff: -5.63%)
----------------------------------------------------
Some benchmarks did not meet the golden values. Please review the output above.
```

(Mock Results:)

[mock_golden.json](https://github.com/user-attachments/files/21538992/mock_golden.json)

[mock_result.json](https://github.com/user-attachments/files/21538993/mock_result.json)




### Checklist
- [ ] [All post

.github/workflows/fast-dispatch-frequent-tests-impl.yaml
@@ -26,10 +26,18 @@ jobs:
             runs-on: ["cloud-virtual-machine", "N300", "in-service"]
             run-args: |
               mkdir -p generated/test_reports
-              ./build/test/tt_metal/perf_microbenchmark/dispatch/test_pgm_dispatch --benchmark_out_format=json --benchmark_out=bench.json
-              ./tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/compare_pgm_dispatch_perf_ci.py bench.json
+              ./build/test/tt_metal/perf_microbenchmark/dispatch/test_pgm_dispatch --benchmark_out_format=json --benchmark_out=pgm_bench.json
+              ./tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/compare_pgm_dispatch_perf_ci.py pgm_bench.json
             timeout: 15
-            # Allan Liu
+          - name: "WH N300 dispatch bandwidth nightly"
+            arch: wormhole_b0
+            runs-on: ["N300", "pipeline-perf"]
+            run-args: |
+              mkdir -p generated/test_reports
+              ./build/test/tt_metal/perf_microbenchmark/dispatch/benchmark_rw_buffer --benchmark_out_format=json --benchmark_out=buffer_bench.json
+              # We shouldn't fail the job for now as this benchmark is not yet stable
+              ./tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/compare_benchmark_rw_buffer.py buffer_bench.json || true
+            timeout: 15
     name: ${{ matrix.test-group.name }}
     env:
       LOGURU_LEVEL: INFO
@@ -86,5 +94,12 @@ jobs:
         if: ${{ !cancelled() }}
         with:
           path: |
-            bench.json
+            pgm_bench.json
           prefix: "pgm_benchmarks_json_"
+      - uses: tenstorrent/tt-metal/.github/actions/upload-artifact-with-job-uuid@main
+        timeout-minutes: 10
+        if: ${{ !cancelled() }}
+        with:
+          path: |
+            buffer_bench.json
+          prefix: "buffer_benchmarks_json_"

tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/benchmark_rw_buffer.cpp
@@ -9,8 +9,6 @@
 #include <cstdint>
 #include <tt-metalium/bfloat16.hpp>
 #include <tt-metalium/host_api.hpp>
-#include <cmath>
-#include <cstring>
 #include <memory>
 #include <string>
 #include <vector>
@@ -38,42 +36,52 @@ using namespace tt::tt_metal::distributed;
 // (mesh buffer) will be in DRAM.
 //
 // Benchmark Matrix:
-// Page Size: 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768
-// Transfer Size: 32k, 512M
 // Read & Write
+// Page Size (Bytes): 32, 64, 128, 256, 512, 1024, 2048
+// Transfer Size: 64 MB
+// Buffer Type: DRAM, L1
 // Device: 0 (local), 1 (remote) (when possible)
 //
 ////////////////////////////////////////////////////////////////////////////////
 
+/*
+ * The upper bound of the page size & transfer size is derived experimentally on July 31st, 2025.
+ * They are set at the point where diminishing returns are observed.
+ *
+ * Link to data sheet as follows:
+ * https://docs.google.com/spreadsheets/d/1zy1teJtgf7hsMMdgy5uIOtcuI73AGVqy4lnyYwL7YFQ/edit
+ */
+
 static const auto KB = 1024;
 static const auto MB = 1024 * KB;
 
-static const BufferType TARGET_BUFFER_TYPE = tt_metal::BufferType::DRAM;
+static const std::vector<int64_t> PAGE_SIZE_ARGS = benchmark::CreateRange(32, 2048, 2);
+static const std::vector<int64_t> TRANSFER_SIZE_ARGS = {64 * MB};
 
-static const auto PAGE_SIZE_ARGS = benchmark::CreateRange(32, 32 * KB, 2);
-static const std::vector<int64_t> TRANSFER_SIZE_ARGS{32 * KB, 512 * MB};
-static const auto BENCHMARK_ARGS = {PAGE_SIZE_ARGS, TRANSFER_SIZE_ARGS};
-
-// Create a buffer of total transfer_size big that is paged with page_size
-std::shared_ptr<MeshBuffer> create_buffer(int page_size, int transfer_size, std::shared_ptr<MeshDevice> device) {
-    using DataType = uint32_t;
-    auto num_data = transfer_size / sizeof(DataType);
-
-    // Need to use Replicated Buffer instead of Sharded Buffer as we operate on a unit mesh.
-    ReplicatedBufferConfig mesh_buffer_config{transfer_size};
-    DeviceLocalBufferConfig device_local_config{.page_size = page_size, .buffer_type = TARGET_BUFFER_TYPE};
-
-    return MeshBuffer::create(mesh_buffer_config, device_local_config, device.get());
-}
+static constexpr std::array<BufferType, 2> BUFFER_TYPES = {BufferType::DRAM, BufferType::L1};
+static const std::vector<int64_t> BUFFER_TYPE_ARGS = {0, 1};
 
 static void BM_write(benchmark::State& state, std::shared_ptr<MeshDevice> mesh_device) {
     auto page_size = state.range(0);
     auto transfer_size = state.range(1);
+    auto buffer_type = BUFFER_TYPES[state.range(2)];
+    auto device_id = state.range(3);
+
+    log_debug(
+        LogTest,
+        "Running Write Benchmark for Page Size: {}, Transfer Size: {}, Buffer Type: {}, Device ID: {}",
+        page_size,
+        transfer_size,
+        buffer_type == BufferType::DRAM ? "DRAM" : "L1",
+        device_id);
 
     auto random_buffer_seed = std::chrono::system_clock::now().time_since_epoch().count();
     auto host_buffer = create_random_vector_of_bfloat16(transfer_size, 1000, random_buffer_seed);
 
-    auto device_buffer = create_buffer(page_size, transfer_size, mesh_device);
+    auto device_buffer = MeshBuffer::create(
+        ReplicatedBufferConfig{transfer_size},
+        DeviceLocalBufferConfig{.page_size = page_size, .buffer_type = buffer_type},
+        mesh_device.get());
 
     for (auto _ : state) {
         EnqueueWriteMeshBuffer(mesh_device->mesh_command_queue(), device_buffer, host_buffer, true);
@@ -85,8 +93,21 @@ static void BM_write(benchmark::State& state, std::shared_ptr<MeshDevice> mesh_d
 static void BM_read(benchmark::State& state, std::shared_ptr<MeshDevice> mesh_device) {
     auto page_size = state.range(0);
     auto transfer_size = state.range(1);
+    auto buffer_type = BUFFER_TYPES[state.range(2)];
+    auto device_id = state.range(3);
 
-    auto device_buffer = create_buffer(page_size, transfer_size, mesh_device);
+    log_debug(
+        LogTest,
+        "Running Read Benchmark for Page Size: {}, Transfer Size: {}, Buffer Type: {}, Device ID: {}",
+        page_size,
+        transfer_size,
+        buffer_type == BufferType::DRAM ? "DRAM" : "L1",
+        device_id);
+
+    auto device_buffer = MeshBuffer::create(
+        ReplicatedBufferConfig{transfer_size},
+        DeviceLocalBufferConfig{.page_size = page_size, .buffer_type = buffer_type},
+        mesh_device.get());
     std::vector<uint32_t> host_buffer;
 
     for (auto _ : state) {
@@ -115,7 +136,7 @@ int main(int argc, char** argv) {
     auto devices = MeshDevice::create_unit_meshes(device_ids);
     for (auto [device_id, device] : devices) {
         // Device ID embedded here for extraction
-        auto benchmark_args = {PAGE_SIZE_ARGS, TRANSFER_SIZE_ARGS, {device_id}};
+        auto benchmark_args = {PAGE_SIZE_ARGS, TRANSFER_SIZE_ARGS, BUFFER_TYPE_ARGS, {device_id}};
         // Google Benchmark uses CPU time to calculate throughput by default, which is not suitable for this
         // benchmark
         benchmark::RegisterBenchmark("Write", BM_write, device)->ArgsProduct(benchmark_args)->UseRealTime();

tests/tt_metal/tt_metal/perf_microbenchmark/dispatch/compare_benchmark_rw_buffer.py
@@ -1,0 +1,125 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import argparse
+import json
+import sys
+import os
+import pathlib
+
+DEFAULT_GOLDEN_FILE = os.path.join(
+    pathlib.Path(__file__).parent.resolve(),
+    "benchmark_rw_buffer_golden.json",
+)
+
+DEFAULT_BANDWIDTH_VARIANCE_TOLERANCE_PCT = 5
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Compare performance benchmarks against golden values.")
+    parser.add_argument(
+        "json",
+        type=argparse.FileType("r"),
+        help="Path to the JSON file with benchmark results.",
+    )
+    parser.add_argument(
+        "--golden",
+        type=argparse.FileType("r"),
+        help="Path to the JSON file with golden benchmark values.",
+        default=DEFAULT_GOLDEN_FILE,
+    )
+    parser.add_argument(
+        "--tolerance",
+        type=float,
+        help="Tolerance for benchmark results.",
+        default=DEFAULT_BANDWIDTH_VARIANCE_TOLERANCE_PCT,
+    )
+    args = parser.parse_args()
+    return args.golden, args.json, args.tolerance
+
+
+def collect_benchmarks(benchmark_obj):
+    successed_benchmarks, failed_benchmarks = {}, {}
+    for benchmark in benchmark_obj["benchmarks"]:
+        if "error_occurred" in benchmark and benchmark["error_occurred"]:
+            failed_benchmarks[benchmark["name"]] = benchmark["error_message"]
+        else:
+            successed_benchmarks[benchmark["name"]] = float(benchmark["bytes_per_second"])
+    return successed_benchmarks, failed_benchmarks
+
+
+def compare_benchmarks(golden_benchmarks, result_benchmarks, result_failed_benchmarks, tolerance):
+    golden_benchmarks_names = set(golden_benchmarks.keys())
+    result_benchmarks_names = set(result_benchmarks.keys())
+
+    successed_benchmarks = []
+    underperforming_benchmarks = []
+
+    for name in golden_benchmarks_names & result_benchmarks_names:
+        golden_value = golden_benchmarks[name]
+        result_value = result_benchmarks[name]
+
+        pct_diff = ((result_value - golden_value) / golden_value) * 100
+        if abs(pct_diff) > tolerance:
+            underperforming_benchmarks.append((name, result_value, golden_value, pct_diff))
+        else:
+            successed_benchmarks.append((name, result_value, golden_value, pct_diff))
+
+    success = (
+        golden_benchmarks_names == result_benchmarks_names
+        and not underperforming_benchmarks
+        and not result_failed_benchmarks
+    )
+
+    print(f"Benchmark {'FAILED' if not success else 'PASSED'}:")
+    print("----------------------------------------------------")
+    print(f"# normal benchmarks: {len(successed_benchmarks)}")
+    print(f"# failed benchmarks: {len(result_failed_benchmarks)}")
+    print(f"# benchmarks outside of expected variance: {len(underperforming_benchmarks)}")
+    print(f"# mismatched benchmarks: {len(golden_benchmarks_names ^ result_benchmarks_names)}")
+    print(f"# total benchmarks: {len(result_benchmarks_names) + len(result_failed_benchmarks)}")
+    print("----------------------------------------------------")
+
+    print("Successed benchmarks: (bytes per second, result vs golden)")
+    for name, result_value, golden_value, pct_diff in successed_benchmarks:
+        print(f"PASSED | Benchmark {name}: {result_value:.2f} vs {golden_value:.2f} (diff: {pct_diff:+.2f}%)")
+    print("----------------------------------------------------")
+
+    print("Failed benchmarks:")
+    for name, error_message in result_failed_benchmarks.items():
+        print(f"FAILED | Benchmark {name} failed to run: {error_message}")
+    print("----------------------------------------------------")
+
+    print("Missing benchmarks:")
+    for name in golden_benchmarks_names - result_benchmarks_names:
+        print(f"FAILED | Benchmark {name}: missing from results")
+    for name in result_benchmarks_names - golden_benchmarks_names:
+        print(f"FAILED | Benchmark {name}: excess from results")
+    print("----------------------------------------------------")
+
+    print("Benchmarks outside of expected variance: (bytes per second, result vs golden)")
+    for name, result_value, golden_value, pct_diff in sorted(underperforming_benchmarks, key=lambda x: x[3]):
+        print(f"FAILED | Benchmark {name}: {result_value:.2f} vs {golden_value:.2f} (diff: {pct_diff:+.2f}%)")
+    print("----------------------------------------------------")
+
+    return success
+
+
+if __name__ == "__main__":
+    golden_file, result_file, tolerance = parse_args()
+
+    golden_benchmarks, golden_failed_benchmarks = collect_benchmarks(json.load(golden_file))
+    result_benchmarks, result_failed_benchmarks = collect_benchmarks(json.load(result_file))
+
+    assert not golden_failed_benchmarks, "Golden benchmarks should not have failed benchmarks"
+
+    print(f"Comparing throughput benchmarks ({golden_file} vs {result_file})...")
+    print("Note: Benchmark name follows: Function/ Page Size/ Transfer Size/ Device ID")
+    print(f"Tolerance is {tolerance}%.")
+    print("----------------------------------------------------")
+
+    if not compare_benchmarks(golden_benchmarks, result_benchmarks, result_failed_benchmarks, tolerance):
+        sys.exit("Some benchmarks did not meet the golden values. Please review the output above.")