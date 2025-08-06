b26cc15d96813e9e58453be8bac503a9decacca1
mouliraj-mcw <ctr-mcw-melamurugan@ext.tenstorrent.com>
2025-08-06 09:51:45 +0000

    #26330: Update tanh_accurate to use where llk

--- ttnn/cpp/ttnn/operations/eltwise/unary/tanh_accurate/device/tanh_accurate_pgm_factory.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/unary/tanh_accurate/device/tanh_accurate_pgm_factory.cpp
@@ -84,20 +84,6 @@ TanhAccurateProgramFactory::cached_program_t TanhAccurateProgramFactory::create(
             .set_page_size(im5_cb_index, single_tile_size);
     tt::tt_metal::CreateCircularBuffer(program, all_cores, cb_im5_config);
 
-    // output for x > 3.5
-    uint32_t im6_cb_index = tt::CBIndex::c_7;
-    tt::tt_metal::CircularBufferConfig cb_im6_config =
-        tt::tt_metal::CircularBufferConfig(num_input_tiles * single_tile_size, {{im6_cb_index, cb_data_format}})
-            .set_page_size(im6_cb_index, single_tile_size);
-    tt::tt_metal::CreateCircularBuffer(program, all_cores, cb_im6_config);
-
-    // output for x <= 3.5
-    uint32_t im7_cb_index = tt::CBIndex::c_8;
-    tt::tt_metal::CircularBufferConfig cb_im7_config =
-        tt::tt_metal::CircularBufferConfig(num_input_tiles * single_tile_size, {{im7_cb_index, cb_data_format}})
-            .set_page_size(im7_cb_index, single_tile_size);
-    tt::tt_metal::CreateCircularBuffer(program, all_cores, cb_im7_config);
-
     // output buffer
     uint32_t output_cb_index = tt::CBIndex::c_2;
     uint32_t num_output_tiles = 2;
