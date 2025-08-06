06461f286290b2b01715b1b05a59ba179e125f57
punith.sekar <punith.sekar@multicorewareinc.com>
2025-08-06 12:50:39 +0000

    Add detr_track_3d_coder

--- models/experimental/uniad/tt/ttnn_utils.py
+++ models/experimental/uniad/tt/ttnn_utils.py
@@ -11,6 +11,33 @@ from torch import Tensor
 import math
 from typing import Any, Sequence, Tuple, Union, Dict, List
 
+from abc import ABCMeta, abstractmethod
+
+
+# taken from mmdet.models.task_modules import BaseBBoxCoder
+class TtBaseBBoxCoder(metaclass=ABCMeta):
+    """Base bounding box coder.
+
+    Args:
+        use_box_type (bool): Whether to warp decoded boxes with the
+            box type data structure. Defaults to False.
+    """
+
+    # The size of the last of dimension of the encoded tensor.
+    encode_size = 4
+
+    def __init__(self, use_box_type: bool = False, **kwargs):
+        self.use_box_type = use_box_type
+
+    @abstractmethod
+    def encode(self, bboxes, gt_bboxes):
+        """Encode deltas between bboxes and ground truth boxes."""
+
+    @abstractmethod
+    def decode(self, bboxes, bboxes_pred):
+        """Decode the predicted bboxes according to prediction and base
+        boxes."""
+
 
 def inverse_sigmoid(x, eps: float = 1e-5):
     x = ttnn.to_layout(x, layout=ttnn.TILE_LAYOUT)
@@ -267,21 +294,20 @@ def trajectory_coordinate_transform(
     return ttnn.stack(batched_trajectories, dim=0)
 
 
-def denormalize_bbox(normalized_bboxes, pc_range):
+def denormalize_bbox(normalized_bboxes, pc_range, device=None):
     # rotation
     rot_sine = normalized_bboxes[..., 6:7]
-
     rot_cosine = normalized_bboxes[..., 7:8]
-    rot_sine = ttnn.to_layout(rot_sine, layout=ttnn.TILE_LAYOUT)
-    rot_cosine = ttnn.to_layout(rot_cosine, layout=ttnn.TILE_LAYOUT)
-    rot = ttnn.atan2(rot_sine, rot_cosine)
 
-    # rot = -rot - np.pi / 2
-    rot = ttnn.mul(rot, -1)
-    rot = ttnn.sub(rot, np.pi / 2)
+    rot_sine = ttnn.typecast(rot_sine, ttnn.bfloat16)
+    rot_cosine = ttnn.typecast(rot_cosine, ttnn.bfloat16)
+
+    rot = ttnn.atan2(rot_sine, rot_cosine)  # Does not support float32
+
+    rot = -1 * rot
+    rot = rot - np.pi / 2
 
     rot = limit_period(rot, period=np.pi * 2)
-
     # center in the bev
     cx = normalized_bboxes[..., 0:1]
     cy = normalized_bboxes[..., 1:2]
@@ -292,28 +318,24 @@ def denormalize_bbox(normalized_bboxes, pc_range):
     width = normalized_bboxes[..., 3:4]
     height = normalized_bboxes[..., 5:6]
 
-    width = ttnn.to_layout(width, layout=ttnn.TILE_LAYOUT)
-    length = ttnn.to_layout(length, layout=ttnn.TILE_LAYOUT)
-    height = ttnn.to_layout(height, layout=ttnn.TILE_LAYOUT)
-
     width = ttnn.exp(width)
     length = ttnn.exp(length)
     height = ttnn.exp(height)
+
     if normalized_bboxes.shape[-1] > 8:
         # velocity
         vx = normalized_bboxes[:, 8:9]
         vy = normalized_bboxes[:, 9:10]
-        cx = ttnn.to_layout(cx, layout=ttnn.TILE_LAYOUT)
-        cy = ttnn.to_layout(cy, layout=ttnn.TILE_LAYOUT)
-        cz = ttnn.to_layout(cz, layout=ttnn.TILE_LAYOUT)
-        vx = ttnn.to_layout(vx, layout=ttnn.TILE_LAYOUT)
-        vy = ttnn.to_layout(vy, layout=ttnn.TILE_LAYOUT)
-        denormalized_bboxes = ttnn.concat([cx, cy, cz, length, width, height, rot, vx, vy], dim=-1)
+
+        if ttnn.to_torch(cx).numel() == 0:
+            denormalized_bboxes = ttnn.from_torch(
+                torch.empty(0, 9), dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device
+            )
+            print("Tensor is empty, Creating empty ttnn tensor")
+        else:
+            denormalized_bboxes = ttnn.concat([cx, cy, cz, length, width, height, rot, vx, vy], dim=-1)
     else:
-        cx = ttnn.to_layout(cx, layout=ttnn.TILE_LAYOUT)
-        cy = ttnn.to_layout(cy, layout=ttnn.TILE_LAYOUT)
-        cz = ttnn.to_layout(cz, layout=ttnn.TILE_LAYOUT)
-        denormalized_bboxes = torch.concat([cx, cy, cz, length, width, height, rot], dim=-1)
+        denormalized_bboxes = ttnn.concat([cx, cy, cz, length, width, height, rot], dim=-1)
 
     return denormalized_bboxes
 
