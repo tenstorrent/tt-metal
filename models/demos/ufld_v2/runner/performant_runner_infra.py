6fb756c9a2c47bd261a129636935e21fff376ccb
Keerthana <152590865+kkeerthana0573@users.noreply.github.com>
2025-08-06 11:49:05 +0530

    #0: Move UFLD_V2 tests to models/demos/ufld_v2/tests (#26206)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/18824)
    
    ### Problem description
    Move integration_test to models/demos/ufld_v2/tests
    
    ### What's changed
    - Moved integration_test to models/demos/ufld_v2/tests
    - Added model to CIs.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16721741635)
    - [x] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/runs/16721753292/job/47327933422#step:8:4204)
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/runs/16721749894/job/47327987715)
    - [x] [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16721745190/job/47327989259)
    - [x] [Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16721738802/job/47327902512)
    
    ---------
    
    Co-authored-by: Saichand Akella <133634875+saichandax@users.noreply.github.com>

--- models/demos/ufld_v2/runner/performant_runner_infra.py
+++ models/demos/ufld_v2/runner/performant_runner_infra.py
@@ -2,43 +2,17 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-import os
 
 import torch
 from loguru import logger
 from ttnn.model_preprocessing import infer_ttnn_module_args, preprocess_model_parameters
 
 import ttnn
-from models.demos.ufld_v2.reference.ufld_v2_model import TuSimple34
+from models.demos.ufld_v2.common import load_torch_model
+from models.demos.ufld_v2.tests.pcc.test_ttnn_ufld_v2 import custom_preprocessor_whole_model
 from models.demos.ufld_v2.ttnn.ttnn_ufld_v2 import TtnnUFLDv2
 from models.utility_functions import divup, is_wormhole_b0
-from tests.ttnn.integration_tests.ufld_v2.test_ttnn_ufld_v2 import custom_preprocessor_whole_model
 from tests.ttnn.utils_for_testing import assert_with_pcc
-
-
-def load_torch_model(model_location_generator=None, use_pretrained_weight=False):
-    torch_model = TuSimple34(input_height=320, input_width=800)
-    torch_model.eval()
-    if not use_pretrained_weight:
-        return torch_model
-
-    if model_location_generator == None or "TT_GH_CI_INFRA" not in os.environ:
-        weights_path = "models/demos/ufld_v2/tusimple_res34.pth"
-        if not os.path.exists(weights_path):
-            os.system("bash models/demos/ufld_v2/weights_download.sh")
-    else:
-        weights_path = (
-            model_location_generator("vision-models/ufldv2", model_subdir="", download_if_ci_v2=True)
-            / "tusimple_res34.pth"
-        )
-
-    state_dict = torch.load(weights_path)
-    new_state_dict = {}
-    for key, value in state_dict["model"].items():
-        new_key = key.replace("model.", "res_model.")
-        new_state_dict[new_key] = value
-    torch_model.load_state_dict(new_state_dict)
-    return torch_model
 
 
 def load_ttnn_model(device, torch_model, torch_input_tensor):
@@ -81,7 +55,7 @@ class UFLDPerformanceRunnerInfra:
             if self.torch_input_tensor is None
             else self.torch_input_tensor
         )
-        self.torch_model = load_torch_model()
+        self.torch_model = load_torch_model(model_location_generator=self.model_location_generator)
         self.ttnn_ufld_v2_model = load_ttnn_model(self.device, self.torch_model, self.torch_input_tensor)
         self.torch_output_tensor_1, self.torch_output_tensor_2 = self.torch_model(self.torch_input_tensor)
 
