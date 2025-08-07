4b160c7855d8e185dc18a5b978bbc308729d4cae
wransom-TT <wransom@tenstorrent.com>
2025-08-07 23:20:03 +0000

    #0: returning the actual uninitialized index output tensor from the infra need to expose to kernel by CB and begin topk implementation

--- ttnn/cpp/ttnn/operations/pool/generic/device/pool_op.hpp
+++ ttnn/cpp/ttnn/operations/pool/generic/device/pool_op.hpp
@@ -36,7 +36,7 @@ struct Pool2D {
     };
 
     using spec_return_value_t = TensorSpec;
-    using tensor_return_value_t = Tensor;
+    using tensor_return_value_t = std::vector<Tensor>;
 
     struct MultiCore {
         struct shared_variables_t {
@@ -69,10 +69,10 @@ struct Pool2D {
     static void validate_on_program_cache_miss(const operation_attributes_t&, const tensor_args_t&);
     static void validate_on_program_cache_hit(const operation_attributes_t&, const tensor_args_t&);
     static spec_return_value_t compute_output_specs(const operation_attributes_t&, const tensor_args_t&);
-    static Tensor create_output_tensors(const operation_attributes_t&, const tensor_args_t&);
+    static tensor_return_value_t create_output_tensors(const operation_attributes_t&, const tensor_args_t&);
     static tt::stl::hash::hash_t compute_program_hash(const operation_attributes_t&, const tensor_args_t&);
     static tt::tt_metal::operation::OpPerformanceModelGeneral<tensor_return_value_t> create_op_performance_model(
-        const operation_attributes_t&, const tensor_args_t&, const Tensor&);
+        const operation_attributes_t&, const tensor_args_t&, const tensor_return_value_t&);
 
     static std::tuple<operation_attributes_t, tensor_args_t> invoke(
         const Tensor& input_tensor,
