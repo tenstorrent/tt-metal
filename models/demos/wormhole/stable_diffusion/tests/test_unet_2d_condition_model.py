e3cbcf138c8b6aee2969ca64e41cc9251fa0ab74
Aleksandar Stancov <astancov@tenstorrent.com>
2025-08-07 11:35:26 +0200

    Removed warmup run from stable_diffusion device perf test (#26424)
    ### Problem description
    Warmup model run is not needed when running device perf.
    It increases the test time without any gain and increases device perf
    report generation duration.
    
    ### What's changed
    Removed warmup run from
    `models/demos/wormhole/stable_diffusion/tests/test_unet_2d_condition_model.py::
    test_unet_2d_condition_model_512x512`

--- models/demos/wormhole/stable_diffusion/tests/test_unet_2d_condition_model.py
+++ models/demos/wormhole/stable_diffusion/tests/test_unet_2d_condition_model.py
@@ -2,7 +2,6 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-import time
 
 import pytest
 import torch
@@ -113,7 +112,6 @@ def test_unet_2d_condition_model_512x512(device, batch_size, in_channels, input_
     encoder_hidden_states = ttnn.to_device(encoder_hidden_states, device, memory_config=ttnn.L1_MEMORY_CONFIG)
     model = UNet2D(device, parameters, batch_size, input_height, input_width)
 
-    first_iter = time.time()
     use_signpost = True
     try:
         from tracy import signpost
@@ -121,7 +119,7 @@ def test_unet_2d_condition_model_512x512(device, batch_size, in_channels, input_
         use_signpost = False
     if use_signpost:
         signpost(header="start")
-    ttnn_output_ = model(
+    ttnn_output = model(
         input,
         timestep=ttnn_timestep,
         encoder_hidden_states=encoder_hidden_states,
@@ -133,22 +131,6 @@ def test_unet_2d_condition_model_512x512(device, batch_size, in_channels, input_
     )
     if use_signpost:
         signpost(header="stop")
-    first_iter = time.time() - first_iter
-    print(f"First iteration took {first_iter} seconds")
-
-    second_iter = time.time()
-    ttnn_output = model(
-        input,
-        timestep=ttnn_timestep,
-        encoder_hidden_states=encoder_hidden_states,
-        class_labels=class_labels,
-        attention_mask=attention_mask,
-        cross_attention_kwargs=cross_attention_kwargs,
-        return_dict=return_dict,
-        config=config,
-    )
-    second_iter = time.time() - second_iter
-    print(f"Second iteration took {second_iter} seconds")
 
     ttnn_output = ttnn.to_torch(ttnn_output)
     assert_with_pcc(torch_output, ttnn_output, 0.995)
