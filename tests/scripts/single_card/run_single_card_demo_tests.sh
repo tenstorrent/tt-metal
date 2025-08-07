b704f3eed412379bed9eaeeb05858db093812a20
Saichand Akella <133634875+saichandax@users.noreply.github.com>
2025-08-08 02:04:56 +0530

    Update run_single_card_demo_tests.sh

--- tests/scripts/single_card/run_single_card_demo_tests.sh
+++ tests/scripts/single_card/run_single_card_demo_tests.sh
@@ -341,11 +341,11 @@ run_yolov12x_demo() {
 
 }
 
-# commenting out the test from CI due to HF issue. TODO explore AWS alternative suggested by infra team.
-# Raised issue to whitelist dataset- https://github.com/tenstorrent/tt-metal/issues/25866
+# commenting out the test from CI due to HF issue. TODO demo will be enabled with CIv2 dataset .
+# Created a PR to enable demo with CIv2 dataset soon- https://github.com/tenstorrent/tt-metal/pull/26236
 # run_vovnet_demo(){
 
-#  pytest models/experimental/vovnet/demo/demo.py --timeout 600
+#  pytest models/experimental/vovnet/demo/demo.py
 
 # }
 
