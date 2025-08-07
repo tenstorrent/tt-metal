a806b81a2152fb9e78359ca44ba3e1f1a09eb3c2
grechsteinerTT <grechsteiner@tenstorrent.com>
2025-08-07 03:28:13 +0000

    Handle single device on N300

--- models/tt_transformers/conftest.py
+++ models/tt_transformers/conftest.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
 
 # SPDX-License-Identifier: Apache-2.0
+import os
+
 import pytest
 
 import ttnn
@@ -12,10 +14,13 @@ def device_params(request, galaxy_type):
     # Get param dict passed in from test parametrize (or default to empty dict)
     params = getattr(request, "param", {}).copy()
 
+    mesh_device = os.environ.get("MESH_DEVICE"), len(ttnn.get_device_ids())
+    is_single_device = (mesh_device == (1, 1)) if isinstance(mesh_device, tuple) else (mesh_device == 1)
+
     if "fabric_config" in params:
         # TODO: 26411
         # Remove this blackhole condition once fabric CCLs are working on blackhole
-        if is_blackhole():
+        if is_blackhole() or is_single_device:
             params["fabric_config"] = None
         elif params["fabric_config"] == True:
             params["fabric_config"] = (
