06f14d16e5ad990970f6da77e1ad2d969b9dd071
Radomir Djogo <159184120+rdjogoTT@users.noreply.github.com>
2025-08-07 14:44:47 -0400

    #25126: [skip ci] Enable autodetect in didt test for multidevice (#26379)
    ### Ticket
    Need to enable didt testing on BH QB upstreaming tests
    
    ### Problem description
    Didt tests can be run on variable number of devices, but setting this
    requires a different command for each device.
    
    ### What's changed
    Autodetect the number of devices that are available and run the didt
    test on all of them if using the "all" flag. Still maintains all of the
    old options (1chip, 2chip, galaxy, etc.) but adds this feature which
    works on all configs (except for rackbox for now but this will be
    revisited shortly)
    
    ### Checklist
    - [x] upstreaming tests pass :
    https://github.com/tenstorrent/tt-metal/actions/runs/16780799039/job/47521591812
    (same failure as on main, didt tests pass)
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16787253183)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16787256679)
    CI with demo tests passes (if applicable)
    - [x] l2 nightly didt tests :
    https://github.com/tenstorrent/tt-metal/actions/runs/16787266257
    - [x] New/Existing tests provide coverage for changes

--- dockerfile/upstream_test_images/run_upstream_tests_vanilla.sh
+++ dockerfile/upstream_test_images/run_upstream_tests_vanilla.sh
@@ -29,12 +29,12 @@ test_suite_bh_single_pcie_small_ml_model_tests() {
     pytest models/demos/blackhole/resnet50/tests/upstream_pipeline
 }
 
-test_suite_bh_single_pcie_didt_tests() {
+test_suite_bh_pcie_didt_tests() {
     echo "[upstream-tests] Running BH upstream didt tests"
-    pytest tests/didt/test_resnet_conv.py::test_resnet_conv -k "1chips" --didt-workload-iterations 100 --determinism-check-interval 1
-    pytest tests/didt/test_ff1_matmul.py::test_ff1_matmul -k "without_gelu and 1chips" --didt-workload-iterations 100 --determinism-check-interval 1
-    pytest tests/didt/test_ff1_matmul.py::test_ff1_matmul -k "with_gelu and 1chips" --didt-workload-iterations 100 --determinism-check-interval 1
-    pytest tests/didt/test_lm_head_matmul.py::test_lm_head_matmul -k "1chips" --didt-workload-iterations 100 --determinism-check-interval 1
+    pytest tests/didt/test_resnet_conv.py::test_resnet_conv -k "all" --didt-workload-iterations 100 --determinism-check-interval 1
+    pytest tests/didt/test_ff1_matmul.py::test_ff1_matmul -k "without_gelu and all" --didt-workload-iterations 100 --determinism-check-interval 1
+    pytest tests/didt/test_ff1_matmul.py::test_ff1_matmul -k "with_gelu and all" --didt-workload-iterations 100 --determinism-check-interval 1
+    pytest tests/didt/test_lm_head_matmul.py::test_lm_head_matmul -k "all" --didt-workload-iterations 100 --determinism-check-interval 1
 }
 
 verify_llama_dir_() {
@@ -172,21 +172,25 @@ test_suite_wh_6u_llama_long_stress_tests() {
 declare -A hw_topology_test_suites
 
 # Store test suites as newline-separated lists
-hw_topology_test_suites["blackhole"]="test_suite_bh_single_pcie_python_unit_tests
+hw_topology_test_suites["blackhole"]="
+test_suite_bh_pcie_didt_tests
+test_suite_bh_single_pcie_python_unit_tests
 test_suite_bh_single_pcie_metal_unit_tests
-test_suite_bh_single_pcie_didt_tests
 test_suite_bh_single_pcie_small_ml_model_tests
 test_suite_bh_single_pcie_llama_demo_tests" # NOTE: This test MUST be last because of the requirements install currently in the llama tests
 
-hw_topology_test_suites["blackhole_no_models"]="test_suite_bh_single_pcie_python_unit_tests
-test_suite_bh_single_pcie_metal_unit_tests
-test_suite_bh_single_pcie_didt_tests"
+hw_topology_test_suites["blackhole_no_models"]="
+test_suite_bh_pcie_didt_tests
+test_suite_bh_single_pcie_python_unit_tests
+test_suite_bh_single_pcie_metal_unit_tests"
 
 hw_topology_test_suites["blackhole_llmbox"]="
+test_suite_bh_pcie_didt_tests
 test_suite_bh_multi_pcie_metal_unit_tests
 test_suite_bh_multi_pcie_llama_demo_tests"
 
 hw_topology_test_suites["blackhole_deskbox"]="
+test_suite_bh_pcie_didt_tests
 test_suite_bh_multi_pcie_metal_unit_tests
 test_suite_bh_multi_pcie_llama_demo_tests"
 
