46fd13eb6c532cfa85111017638886f09893bfb3
Daiki Aminaka <daminaka@tenstorrent.com>
2025-08-08 00:16:25 +0900

    Enable route_id for fabric API (#26248)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/24881
    
    ### Problem description
    We want to remove developer's burden to manage header and write fabric
    operation for each direction.
    
    ### What's changed
    Associate route_id to headers for each direction fabric operation. Now
    single function call covers all direction.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/16796006541
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/16796600178
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [x] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
    CI passes (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/16796005435
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    https://github.com/tenstorrent/tt-metal/actions/runs/16796005435
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    https://github.com/tenstorrent/tt-metal/actions/runs/16796002657
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    https://github.com/tenstorrent/tt-metal/actions/runs/16796002657
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- tt_metal/fabric/hw/inc/linear/api.h
+++ tt_metal/fabric/hw/inc/linear/api.h
@@ -7,6 +7,7 @@
 #include <cstdint>
 #include "dataflow_api.h"
 #include "tt_metal/fabric/hw/inc/packet_header_pool.h"
+#include "tt_metal/fabric/hw/inc/tt_fabric.h"
 #include "tt_metal/fabric/hw/inc/edm_fabric/edm_fabric_worker_adapters.hpp"
 #include "tt-metalium/fabric_edm_packet_header.hpp"
 
@@ -26,6 +27,19 @@ FORCE_INLINE void fabric_unicast_noc_unicast_write(
     client_interface->send_payload_flush_non_blocking_from_address((uint32_t)packet_header, sizeof(PACKET_HEADER_TYPE));
 }
 
+FORCE_INLINE void fabric_unicast_noc_unicast_write(
+    tt_l1_ptr tt::tt_fabric::WorkerToFabricEdmSender* client_interfaces,
+    uint8_t route_id,
+    uint32_t src_addr,
+    uint32_t size,
+    tt::tt_fabric::NocUnicastCommandHeader noc_unicast_command_header,
+    uint8_t* num_hops) {
+    PacketHeaderPool::for_each_header(route_id, [&](volatile PACKET_HEADER_TYPE* packet_header, uint8_t i) {
+        fabric_unicast_noc_unicast_write(
+            &client_interfaces[i], packet_header, src_addr, size, noc_unicast_command_header, num_hops[i]);
+    });
+}
+
 FORCE_INLINE void fabric_unicast_noc_unicast_atomic_inc(
     tt_l1_ptr tt::tt_fabric::WorkerToFabricEdmSender* client_interface,
     volatile PACKET_HEADER_TYPE* packet_header,
@@ -35,6 +49,17 @@ FORCE_INLINE void fabric_unicast_noc_unicast_atomic_inc(
     packet_header->to_noc_unicast_atomic_inc(noc_unicast_atomic_inc_command_header);
     client_interface->wait_for_empty_write_slot();
     client_interface->send_payload_flush_non_blocking_from_address((uint32_t)packet_header, sizeof(PACKET_HEADER_TYPE));
+}
+
+FORCE_INLINE void fabric_unicast_noc_unicast_atomic_inc(
+    tt_l1_ptr tt::tt_fabric::WorkerToFabricEdmSender* client_interfaces,
+    uint8_t route_id,
+    tt::tt_fabric::NocUnicastAtomicIncCommandHeader noc_unicast_atomic_inc_command_header,
+    uint8_t* num_hops) {
+    PacketHeaderPool::for_each_header(route_id, [&](volatile PACKET_HEADER_TYPE* packet_header, uint8_t i) {
+        fabric_unicast_noc_unicast_atomic_inc(
+            &client_interfaces[i], packet_header, noc_unicast_atomic_inc_command_header, num_hops[i]);
+    });
 }
 
 FORCE_INLINE void fabric_unicast_noc_scatter_write(
@@ -51,6 +76,19 @@ FORCE_INLINE void fabric_unicast_noc_scatter_write(
     client_interface->send_payload_flush_non_blocking_from_address((uint32_t)packet_header, sizeof(PACKET_HEADER_TYPE));
 }
 
+FORCE_INLINE void fabric_unicast_noc_scatter_write(
+    tt_l1_ptr tt::tt_fabric::WorkerToFabricEdmSender* client_interfaces,
+    uint8_t route_id,
+    uint32_t src_addr,
+    uint32_t size,
+    tt::tt_fabric::NocUnicastScatterCommandHeader noc_unicast_scatter_command_header,
+    uint8_t* num_hops) {
+    PacketHeaderPool::for_each_header(route_id, [&](volatile PACKET_HEADER_TYPE* packet_header, uint8_t i) {
+        fabric_unicast_noc_scatter_write(
+            &client_interfaces[i], packet_header, src_addr, size, noc_unicast_scatter_command_header, num_hops[i]);
+    });
+}
+
 FORCE_INLINE void fabric_unicast_noc_unicast_inline_write(
     tt_l1_ptr tt::tt_fabric::WorkerToFabricEdmSender* client_interface,
     volatile PACKET_HEADER_TYPE* packet_header,
@@ -60,6 +98,17 @@ FORCE_INLINE void fabric_unicast_noc_unicast_inline_write(
     packet_header->to_noc_unicast_inline_write(noc_unicast_inline_write_command_header);
     client_interface->wait_for_empty_write_slot();
     client_interface->send_payload_flush_non_blocking_from_address((uint32_t)packet_header, sizeof(PACKET_HEADER_TYPE));
+}
+
+FORCE_INLINE void fabric_unicast_noc_unicast_inline_write(
+    tt_l1_ptr tt::tt_fabric::WorkerToFabricEdmSender* client_interfaces,
+    uint8_t route_id,
+    tt::tt_fabric::NocUnicastInlineWriteCommandHeader noc_unicast_inline_write_command_header,
+    uint8_t* num_hops) {
+    PacketHeaderPool::for_each_header(route_id, [&](volatile PACKET_HEADER_TYPE* packet_header, uint8_t i) {
+        fabric_unicast_noc_unicast_inline_write(
+            &client_interfaces[i], packet_header, noc_unicast_inline_write_command_header, num_hops[i]);
+    });
 }
 
 FORCE_INLINE void fabric_unicast_noc_fused_unicast_with_atomic_inc(
@@ -74,6 +123,24 @@ FORCE_INLINE void fabric_unicast_noc_fused_unicast_with_atomic_inc(
     client_interface->wait_for_empty_write_slot();
     client_interface->send_payload_without_header_non_blocking_from_address(src_addr, size);
     client_interface->send_payload_flush_non_blocking_from_address((uint32_t)packet_header, sizeof(PACKET_HEADER_TYPE));
+}
+
+FORCE_INLINE void fabric_unicast_noc_fused_unicast_with_atomic_inc(
+    tt_l1_ptr tt::tt_fabric::WorkerToFabricEdmSender* client_interfaces,
+    uint8_t route_id,
+    uint32_t src_addr,
+    uint32_t size,
+    tt::tt_fabric::NocUnicastAtomicIncFusedCommandHeader noc_fused_unicast_atomic_inc_command_header,
+    uint8_t* num_hops) {
+    PacketHeaderPool::for_each_header(route_id, [&](volatile PACKET_HEADER_TYPE* packet_header, uint8_t i) {
+        fabric_unicast_noc_fused_unicast_with_atomic_inc(
+            &client_interfaces[i],
+            packet_header,
+            src_addr,
+            size,
+            noc_fused_unicast_atomic_inc_command_header,
+            num_hops[i]);
+    });
 }
 
 FORCE_INLINE void fabric_multicast_noc_unicast_write(
@@ -91,6 +158,26 @@ FORCE_INLINE void fabric_multicast_noc_unicast_write(
     client_interface->send_payload_flush_non_blocking_from_address((uint32_t)packet_header, sizeof(PACKET_HEADER_TYPE));
 }
 
+FORCE_INLINE void fabric_multicast_noc_unicast_write(
+    tt_l1_ptr tt::tt_fabric::WorkerToFabricEdmSender* client_interfaces,
+    uint8_t route_id,
+    uint32_t src_addr,
+    uint32_t size,
+    tt::tt_fabric::NocUnicastCommandHeader noc_unicast_command_header,
+    uint8_t* start_distance,
+    uint8_t* range) {
+    PacketHeaderPool::for_each_header(route_id, [&](volatile PACKET_HEADER_TYPE* packet_header, uint8_t i) {
+        fabric_multicast_noc_unicast_write(
+            &client_interfaces[i],
+            packet_header,
+            src_addr,
+            size,
+            noc_unicast_command_header,
+            start_distance[i],
+            range[i]);
+    });
+}
+
 FORCE_INLINE void fabric_multicast_noc_unicast_atomic_inc(
     tt_l1_ptr tt::tt_fabric::WorkerToFabricEdmSender* client_interface,
     volatile PACKET_HEADER_TYPE* packet_header,
@@ -101,6 +188,18 @@ FORCE_INLINE void fabric_multicast_noc_unicast_atomic_inc(
     packet_header->to_noc_unicast_atomic_inc(noc_unicast_atomic_inc_command_header);
     client_interface->wait_for_empty_write_slot();
     client_interface->send_payload_flush_non_blocking_from_address((uint32_t)packet_header, sizeof(PACKET_HEADER_TYPE));
+}
+
+FORCE_INLINE void fabric_multicast_noc_unicast_atomic_inc(
+    tt_l1_ptr tt::tt_fabric::WorkerToFabricEdmSender* client_interfaces,
+    uint8_t route_id,
+    tt::tt_fabric::NocUnicastAtomicIncCommandHeader noc_unicast_atomic_inc_command_header,
+    uint8_t* start_distance,
+    uint8_t* range) {
+    PacketHeaderPool::for_each_header(route_id, [&](volatile PACKET_HEADER_TYPE* packet_header, uint8_t i) {
+        fabric_multicast_noc_unicast_atomic_inc(
+            &client_interfaces[i], packet_header, noc_unicast_atomic_inc_command_header, start_distance[i], range[i]);
+    });
 }
 
 FORCE_INLINE void fabric_multicast_noc_scatter_write(
@@ -118,6 +217,26 @@ FORCE_INLINE void fabric_multicast_noc_scatter_write(
     client_interface->send_payload_flush_non_blocking_from_address((uint32_t)packet_header, sizeof(PACKET_HEADER_TYPE));
 }
 
+FORCE_INLINE void fabric_multicast_noc_scatter_write(
+    tt_l1_ptr tt::tt_fabric::WorkerToFabricEdmSender* client_interfaces,
+    uint8_t route_id,
+    uint32_t src_addr,
+    uint32_t size,
+    tt::tt_fabric::NocUnicastScatterCommandHeader noc_unicast_scatter_command_header,
+    uint8_t* start_distance,
+    uint8_t* range) {
+    PacketHeaderPool::for_each_header(route_id, [&](volatile PACKET_HEADER_TYPE* packet_header, uint8_t i) {
+        fabric_multicast_noc_scatter_write(
+            &client_interfaces[i],
+            packet_header,
+            src_addr,
+            size,
+            noc_unicast_scatter_command_header,
+            start_distance[i],
+            range[i]);
+    });
+}
+
 FORCE_INLINE void fabric_multicast_noc_unicast_inline_write(
     tt_l1_ptr tt::tt_fabric::WorkerToFabricEdmSender* client_interface,
     volatile PACKET_HEADER_TYPE* packet_header,
@@ -128,6 +247,18 @@ FORCE_INLINE void fabric_multicast_noc_unicast_inline_write(
     packet_header->to_noc_unicast_inline_write(noc_unicast_inline_write_command_header);
     client_interface->wait_for_empty_write_slot();
     client_interface->send_payload_flush_non_blocking_from_address((uint32_t)packet_header, sizeof(PACKET_HEADER_TYPE));
+}
+
+FORCE_INLINE void fabric_multicast_noc_unicast_inline_write(
+    tt_l1_ptr tt::tt_fabric::WorkerToFabricEdmSender* client_interfaces,
+    uint8_t route_id,
+    tt::tt_fabric::NocUnicastInlineWriteCommandHeader noc_unicast_inline_write_command_header,
+    uint8_t* start_distance,
+    uint8_t* range) {
+    PacketHeaderPool::for_each_header(route_id, [&](volatile PACKET_HEADER_TYPE* packet_header, uint8_t i) {
+        fabric_multicast_noc_unicast_inline_write(
+            &client_interfaces[i], packet_header, noc_unicast_inline_write_command_header, start_distance[i], range[i]);
+    });
 }
 
 FORCE_INLINE void fabric_multicast_noc_fused_unicast_with_atomic_inc(
@@ -143,6 +274,26 @@ FORCE_INLINE void fabric_multicast_noc_fused_unicast_with_atomic_inc(
     client_interface->wait_for_empty_write_slot();
     client_interface->send_payload_without_header_non_blocking_from_address(src_addr, size);
     client_interface->send_payload_flush_non_blocking_from_address((uint32_t)packet_header, sizeof(PACKET_HEADER_TYPE));
+}
+
+FORCE_INLINE void fabric_multicast_noc_fused_unicast_with_atomic_inc(
+    tt_l1_ptr tt::tt_fabric::WorkerToFabricEdmSender* client_interfaces,
+    uint8_t route_id,
+    uint32_t src_addr,
+    uint32_t size,
+    tt::tt_fabric::NocUnicastAtomicIncFusedCommandHeader noc_fused_unicast_atomic_inc_command_header,
+    uint8_t* start_distance,
+    uint8_t* range) {
+    PacketHeaderPool::for_each_header(route_id, [&](volatile PACKET_HEADER_TYPE* packet_header, uint8_t i) {
+        fabric_multicast_noc_fused_unicast_with_atomic_inc(
+            &client_interfaces[i],
+            packet_header,
+            src_addr,
+            size,
+            noc_fused_unicast_atomic_inc_command_header,
+            start_distance[i],
+            range[i]);
+    });
 }
 
 }  // namespace tt::tt_fabric::linear::experimental
