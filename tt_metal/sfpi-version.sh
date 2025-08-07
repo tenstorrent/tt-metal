9c3ea5205a45345ab96f322432d1f7ef9e6ca4d7
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-08-07 14:55:31 -0400

    SFPI v6.17.0 (#26467)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/26284
    
    ### Problem description
    Compiler borked sfpstORe and sfpload encodings
    Compiler borked wormhole RAW hazard workaround
    
    ### What's changed
    Fixed compiler
    
    ### Checklist
    - [YES] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [YES] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
    CI passes (if applicable)
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- tt_metal/sfpi-version.sh
+++ tt_metal/sfpi-version.sh
@@ -1,13 +1,13 @@
 # set SFPI release version information
 
-sfpi_version=v6.16.1
+sfpi_version=v6.17.0
 sfpi_url=https://github.com/tenstorrent/sfpi/releases/download
 
 # convert md5 file into these variables
 # sed 's/^\([0-9a-f]*\) \*sfpi-\([a-z0-9_A-Z]*\)\.\([a-z]*\)$/sfpi_\2_\3_md5=\1/'
-sfpi_aarch64_Linux_txz_md5=b500c36c84c780259a5e4ba95def2339
-sfpi_aarch64_Linux_deb_md5=5fbcfd2da0c445bf8a1235513958b46e
-sfpi_aarch64_Linux_rpm_md5=a16ebf12ea2a0070909599a4b73b5d21
-sfpi_x86_64_Linux_txz_md5=faf2e68c95544eec714a607712b6cdf0
-sfpi_x86_64_Linux_deb_md5=87575d71b03571fa5be20051f41137df
-sfpi_x86_64_Linux_rpm_md5=5286554401f85379ca59b0c3c43f0746
+sfpi_aarch64_Linux_txz_md5=15ba323821ed9f2d30807cf965430b5d
+sfpi_aarch64_Linux_deb_md5=71f9532a5dc9e491503b220ce12e00bf
+sfpi_aarch64_Linux_rpm_md5=effd19fed9e1a13e8ed4c346499b832e
+sfpi_x86_64_Linux_txz_md5=fdcd38cb5d1d718904f6ca38043aeef4
+sfpi_x86_64_Linux_deb_md5=e337647c11db17796d0db1b0b985ebaa
+sfpi_x86_64_Linux_rpm_md5=14661bc1f60a10bf992cace08423cbb1
