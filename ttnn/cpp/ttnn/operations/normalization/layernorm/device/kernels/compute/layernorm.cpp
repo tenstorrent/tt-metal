f0628b689ab10dea921e942e863560b5e69ae8d1
vsureshTT <vsuresh@tenstorrent.com>
2025-08-07 16:42:44 +0000

    made copy worl

--- ttnn/cpp/ttnn/operations/normalization/layernorm/device/kernels/compute/layernorm.cpp
+++ ttnn/cpp/ttnn/operations/normalization/layernorm/device/kernels/compute/layernorm.cpp
@@ -22,6 +22,8 @@
 #include "compute_kernel_api/welford.h"
 #include "compute_kernel_api/transpose_wh_dest.h"
 #include "compute_kernel_api/tile_move_copy.h"
+#include "compute_kernel_api/eltwise_unary/eltwise_unary.h"
+#include "dprint_tensix.h"
 
 ALWI void ACQ() { acquire_dst(); }
 ALWI void REL() { release_dst(); }
