d0839e06f6fe46987c515c96f0f5bb616a0c3eea
Saichand Akella <133634875+saichandax@users.noreply.github.com>
2025-08-08 02:20:16 +0530

    Update run_single_card_demo_tests.sh

--- tests/scripts/single_card/run_single_card_demo_tests.sh
+++ tests/scripts/single_card/run_single_card_demo_tests.sh
@@ -321,11 +321,11 @@ run_yolov6l_perf() {
 
 }
 
-# commenting out the test from CI due to HF issue. TODO explore AWS alternative suggested by infra team.
-# Raised issue to whitelist dataset- https://github.com/tenstorrent/tt-metal/issues/25866
+# commenting out the test from CI due to HF issue. TODO demo will be enabled with CIv2 dataset .
+# Created a PR to enable demo with CIv2 dataset soon- https://github.com/tenstorrent/tt-metal/pull/26236
 # run_swin_s_demo() {
 
-#   pytest -n auto models/experimental/swin_s/demo/demo.py
+#   pytest models/experimental/swin_s/demo/demo.py
 
 # }
 
