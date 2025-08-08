cde222f40aa29491d30f17b6e2555d38156c1757
daminakaTT <daminaka@tenstorrent.com>
2025-08-08 05:42:32 +0000

    [Fabric] support set_state for linear API

---
+++ tests/tt_metal/tt_fabric/fabric_data_movement/kernels/test_linear_common.hpp
@@ -0,0 +1,84 @@
+
+#include "fabric_edm_packet_header.hpp"
+#include "tt_metal/fabric/hw/inc/linear/api.h"
+using namespace tt::tt_fabric::linear::experimental;
+
+template <uint8_t num_send_dir>
+union HopInfo {
+    struct {
+        uint8_t num_hops[num_send_dir];
+    } ucast;
+    struct {
+        uint8_t start_distance[num_send_dir];
+        uint8_t range[num_send_dir];
+    } mcast;
+};
+
+template <bool is_chip_multicast, uint8_t num_send_dir>
+HopInfo<num_send_dir> get_hop_info_from_args(size_t& rt_arg_idx) {
+    HopInfo<num_send_dir> hop_info;
+    if constexpr (is_chip_multicast) {
+        for (uint32_t i = 0; i < num_send_dir; i++) {
+            hop_info.mcast.start_distance[i] = static_cast<uint8_t>(get_arg_val<uint32_t>(rt_arg_idx++));
+            hop_info.mcast.range[i] = static_cast<uint8_t>(get_arg_val<uint32_t>(rt_arg_idx++));
+        }
+    } else {
+        for (uint32_t i = 0; i < num_send_dir; i++) {
+            hop_info.ucast.num_hops[i] = static_cast<uint8_t>(get_arg_val<uint32_t>(rt_arg_idx++));
+        }
+    }
+    return hop_info;
+}
+
+template <uint32_t packet_size, uint8_t num_send_dir, bool is_chip_multicast, tt::tt_fabric::NocSendType noc_send_type>
+void set_state(uint8_t route_id, HopInfo<num_send_dir>& hop_info) {
+    if constexpr (is_chip_multicast) {
+        switch (noc_send_type) {
+            case NOC_UNICAST_WRITE: {
+                fabric_multicast_noc_unicast_write_set_state<packet_size>(
+                    route_id, hop_info.mcast.start_distance, hop_info.mcast.range);
+            } break;
+            case NOC_UNICAST_INLINE_WRITE: {
+                fabric_multicast_noc_unicast_inline_write_set_state(
+                    route_id, hop_info.mcast.start_distance, hop_info.mcast.range);
+            } break;
+            case NOC_UNICAST_SCATTER_WRITE: {
+                fabric_multicast_noc_scatter_write_set_state<packet_size>(
+                    route_id, hop_info.mcast.start_distance, hop_info.mcast.range);
+            } break;
+            case NOC_UNICAST_ATOMIC_INC: {
+                fabric_multicast_noc_unicast_atomic_inc_set_state(
+                    route_id, hop_info.mcast.start_distance, hop_info.mcast.range);
+            } break;
+            case NOC_FUSED_UNICAST_ATOMIC_INC: {
+                fabric_multicast_noc_fused_unicast_with_atomic_inc_set_state<packet_size>(
+                    route_id, hop_info.mcast.start_distance, hop_info.mcast.range);
+            } break;
+            default: {
+                ASSERT(false);
+            } break;
+        }
+    } else {
+        switch (noc_send_type) {
+            case NOC_UNICAST_WRITE: {
+                fabric_unicast_noc_unicast_write_set_state<packet_size>(route_id, hop_info.ucast.num_hops);
+            } break;
+            case NOC_UNICAST_INLINE_WRITE: {
+                fabric_unicast_noc_unicast_inline_write_set_state(route_id, hop_info.ucast.num_hops);
+            } break;
+            case NOC_UNICAST_SCATTER_WRITE: {
+                fabric_unicast_noc_scatter_write_set_state<packet_size>(route_id, hop_info.ucast.num_hops);
+            } break;
+            case NOC_UNICAST_ATOMIC_INC: {
+                fabric_unicast_noc_unicast_atomic_inc_set_state(route_id, hop_info.ucast.num_hops);
+            } break;
+            case NOC_FUSED_UNICAST_ATOMIC_INC: {
+                fabric_unicast_noc_fused_unicast_with_atomic_inc_set_state<packet_size>(
+                    route_id, hop_info.ucast.num_hops);
+            }
+            default: {
+                ASSERT(false);
+            } break;
+        }
+    }
+}
