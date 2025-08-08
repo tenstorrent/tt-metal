85366f59f4cf61bee451c135b410439ef25333d4
Atul Krishnadas <atul.krishnadas@outlook.com>
2025-08-08 12:49:36 -0700

    Enable tilize_with_val_padding for int32, add tile/untile unit tests, add fatals for unsupported variants (#26297)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/26294)
    
    ### Problem description
    Needed to make sure tilize/untilize is fully enabled for uint32/int32 as
    well as add unit tests to test_tilize_untilize_2d. We also want to make
    sure we fatal out if new pack isn't being used, as that is what enables
    uint32/int32
    
    ### What's changed
    Enabled int32 in tilize_with_val_padding, added int32 to unit tests,
    added fatals when use_pack==True and dtype== unit32/int32
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16760165271)
    CI passes

--- ttnn/cpp/ttnn/operations/data_movement/tilize_with_val_padding/device/tilize_with_val_padding_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/tilize_with_val_padding/device/tilize_with_val_padding_program_factory.cpp
@@ -31,8 +31,9 @@ uint32_t get_packed_value(const Tensor tensor, const ttnn::PadValue pad_value) {
                     return pack_two_bfloat16_into_uint32({bfloat_pad_value, bfloat_pad_value});
                 } else {
                     TT_FATAL(
-                        tensor.dtype() == DataType::FLOAT32 or tensor.dtype() == DataType::UINT32,
-                        "only supporting bfloat16, float32, and uint32");
+                        tensor.dtype() == DataType::FLOAT32 or tensor.dtype() == DataType::UINT32 or
+                            tensor.dtype() == DataType::INT32,
+                        "only supporting bfloat16, float32, and int32/uint32");
                     return (uint32_t)((pad_value));
                 }
             } else if constexpr (std::is_same_v<T, uint32_t>) {
