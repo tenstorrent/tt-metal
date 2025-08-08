c0fc984079a5d72d61dfc3b5e48d74352aef3f9f
Radomir Djogo <159184120+rdjogoTT@users.noreply.github.com>
2025-08-08 14:55:19 -0400

    Update LLK compute API for Reduce with full fp32 accumulation (#26528)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/25266)
    
    ### Problem description
    Reduce init function was not configuring the ALU Format for SrcA, so if
    it was set to Tf32 the kernel would not work correctly.
    
    ### What's changed
    Passing down all the necessary parameters for the LLK init to correctly
    config the HW.
    Also renaming the related flag from `fp32_transpose` to
    `enforce_fp32_accumulation` for greater clarity.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16833535686)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/16833537470)
    CI with demo tests passes (if applicable)

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_math_reduce_api.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_math_reduce_api.h
@@ -16,9 +16,9 @@ template <
     bool is_fp32_dest_acc_en,
     int num_fidelity_phases = 0,
     bool is_int_fpu_en = false,
-    bool fp32_transpose = false>
+    bool enforce_fp32_accumulation = false>
 inline void llk_math_reduce(const uint dst_index, const uint num_faces = 4) {
-    _llk_math_reduce_<type, dim, is_fp32_dest_acc_en, num_fidelity_phases, is_int_fpu_en, fp32_transpose>(
+    _llk_math_reduce_<type, dim, is_fp32_dest_acc_en, num_fidelity_phases, is_int_fpu_en, enforce_fp32_accumulation>(
         dst_index, false, num_faces);
 }
 
@@ -28,18 +28,24 @@ template <
     bool is_fp32_dest_acc_en,
     int num_fidelity_phases = 0,
     bool is_int_fpu_en = false,
-    bool fp32_transpose = false>
+    bool enforce_fp32_accumulation = false>
 inline void llk_math_reduce(const std::uint32_t operandA, const std::uint32_t operandB, const std::uint32_t dst_index) {
     const std::uint32_t operand_id = get_operand_id(operandA);
     const std::uint32_t num_faces = get_operand_num_faces(operand_id);
 
-    _llk_math_reduce_<type, dim, is_fp32_dest_acc_en, num_fidelity_phases, is_int_fpu_en, fp32_transpose>(
+    _llk_math_reduce_<type, dim, is_fp32_dest_acc_en, num_fidelity_phases, is_int_fpu_en, enforce_fp32_accumulation>(
         dst_index, false, num_faces);
 }
 
-template <PoolType type, ReduceDim dim, int num_fidelity_phases = 0>
+template <
+    PoolType type,
+    ReduceDim dim,
+    bool is_fp32_dest_acc_en,
+    int num_fidelity_phases = 0,
+    bool enforce_fp32_accumulation = false /*unused*/>
 inline void llk_math_reduce_init(
     const std::uint32_t within_face_16x16_transpose =
         0) {  // within_face_16x16_transpose used for unpack, ignored by math
-    _llk_math_reduce_init_<type, dim, num_fidelity_phases>(within_face_16x16_transpose);
+    _llk_math_reduce_init_<type, dim, is_fp32_dest_acc_en, num_fidelity_phases, enforce_fp32_accumulation>(
+        within_face_16x16_transpose);
 }
