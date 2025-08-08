a93800ed80a9c01e2733f5aad9709045c039c488
Virdhatchani Narayanamoorthy <138196495+VirdhatchaniKN@users.noreply.github.com>
2025-08-08 18:11:04 +0530

    #25417: Improve accuracy of exp2 (#25724)
    ### Ticket
    #25417, #13002
    
    ### Problem description
    Improve accuracy of exp2
    
    ### What's changed
    
    - This implementation follows that of pwr_21f (Section 5) from [Simple
    Multiple Precision Algorithms for Exponential Functions [Tips &
    Tricks]](https://www.researchgate.net/publication/361844431_Simple_Multiple_Precision_Algorithms_for_Exponential_Functions_Tips_Tricks_IEEE_Signal_Processing_Magazine_394_130-137)
    paper from 2022 by Moroz et al.
    - ULP Values and graphs are as mentioned
    [here](https://github.com/tenstorrent/tt-metal/issues/25417#issuecomment-3126583356)
    
    ### Checklist
    - [x] [Galaxy
    Quick](https://github.com/tenstorrent/tt-metal/actions/runs/16757880234)
    : PASSED
    - [x] [(Single-card) Model perf
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16757884922)
    : PASSED
    - [x] [All post-commit
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16823801073)
    : PASSED
    - [x] [(T3K) T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16757836403)
    : Failed as in main branch
    - [x] [(TG) TG demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16757828606)
    : PASSED
    - [x] [(Single-card) Device perf
    regressions](https://github.com/tenstorrent/tt-metal/actions/runs/16757886511)
    : Failed as in main branch
    - [x] [(Single-card) Demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16757870386)
    : Failed as in main branch
    - [x] [(T3K) T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16757868261)
    : Failed as in main branch
    - [x] [(Blackhole) Blackhole nightly
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16757838135)
    : Failed as in main branch
    - [x] [Blackhole post-commit
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/16792876269)
    : PASSED
    - [x] New/Existing tests provide coverage for changes

---
+++ tests/ttnn/unit_tests/operations/eltwise/test_exp2.py
@@ -0,0 +1,111 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+import pytest
+import ttnn
+import numpy as np
+from tests.ttnn.utils_for_testing import assert_with_ulp, assert_allclose
+
+
+def test_exp2_arange_masking(device):
+    # Exp2 Working range - Overflow from 128(inf), Underflow till -127(<0)
+    low = -126.0
+    high = 127.0
+
+    # Generate all possible bit pattersn for bf16
+    all_bitpatterns = np.arange(0, 2**16, dtype=np.uint16)
+    float32_bits = all_bitpatterns.astype(np.uint32) << 16
+    all_values = float32_bits.view(np.float32)
+
+    # masking to working range
+    mask = (all_values >= low) & (all_values < high)
+    selected_values = all_values[mask]
+    input_tensor = torch.tensor(selected_values, dtype=torch.bfloat16)
+
+    tt_in = ttnn.from_torch(
+        input_tensor,
+        dtype=ttnn.bfloat16,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    golden_function = ttnn.get_golden_function(ttnn.exp2)
+    golden = golden_function(input_tensor, device=device)
+
+    tt_result = ttnn.exp2(tt_in)
+    result = ttnn.to_torch(tt_result)
+    assert_with_ulp(golden, result, 1)
+
+
+@pytest.mark.parametrize(
+    "input_shapes",
+    (
+        (torch.Size([1, 1, 32, 32])),
+        (torch.Size([1, 2, 64, 120])),
+        (torch.Size([1, 3, 320, 320])),
+    ),
+)
+@pytest.mark.parametrize(
+    "low, high",
+    [
+        (-5, 5),
+        (-126, 127),
+    ],
+)
+def test_exp2_ULP(input_shapes, low, high, device):
+    num_elements = torch.prod(torch.tensor(input_shapes)).item()
+    torch_input = torch.linspace(high, low, num_elements, dtype=torch.bfloat16)
+    torch_input = torch_input[:num_elements].reshape(input_shapes)
+
+    golden_function = ttnn.get_golden_function(ttnn.exp2)
+    golden = golden_function(torch_input, device=device)
+
+    tt_in = ttnn.from_torch(
+        torch_input,
+        dtype=ttnn.bfloat16,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    tt_result = ttnn.exp2(tt_in)
+    result = ttnn.to_torch(tt_result)
+    assert_with_ulp(golden, result, 1)
+
+
+@pytest.mark.parametrize(
+    "input_shapes",
+    (
+        (torch.Size([1, 1, 32, 32])),
+        (torch.Size([1, 2, 64, 120])),
+        (torch.Size([1, 3, 320, 320])),
+    ),
+)
+@pytest.mark.parametrize(
+    "low, high",
+    [
+        (-127, -126),
+    ],
+)
+def test_exp2_atol(input_shapes, low, high, device):
+    num_elements = torch.prod(torch.tensor(input_shapes)).item()
+    torch_input = torch.linspace(high, low, num_elements, dtype=torch.bfloat16)
+    torch_input = torch_input[:num_elements].reshape(input_shapes)
+
+    golden_function = ttnn.get_golden_function(ttnn.exp2)
+    golden = golden_function(torch_input, device=device)
+
+    tt_in = ttnn.from_torch(
+        torch_input,
+        dtype=ttnn.bfloat16,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    tt_result = ttnn.exp2(tt_in)
+    result = ttnn.to_torch(tt_result)
+    assert_allclose(tt_result, golden, rtol=1e-2, atol=1e-3)
