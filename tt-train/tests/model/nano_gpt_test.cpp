4f45b44a8ac22598b96f8fe4e0748eec8b1773b1
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2025-08-08 03:04:28 +0000

    fixed

--- tt-train/tests/model/nano_gpt_test.cpp
+++ tt-train/tests/model/nano_gpt_test.cpp
@@ -23,7 +23,7 @@ namespace {
 Nightly tests could be enabled by setting the environment variable ENABLE_NIGHTLY_TT_TRAIN_TESTS=1
 or setting 'is_nigthly_tt_train_tests_enabled' variable to true.
 */
-constexpr bool is_nigthly_tt_train_tests_enabled = false;
+constexpr bool is_nigthly_tt_train_tests_enabled = true;
 
 [[nodiscard]] bool is_wormhole_b0() {
     static bool arch_is_wormhole_b0 = []() {
@@ -303,7 +303,7 @@ void train_test(bool use_tensor_parallel = false, bool use_ddp = false) {
 
     fmt::print("run_type: {}\n", run_type);
     std::unordered_map<std::string, uint32_t> expected_program_cache_entries_map = {
-        {"SingleDevice", 80}, {"DDP", 94}, {"TP", 103}};
+        {"SingleDevice", 79}, {"DDP", 93}, {"TP", 103}};
     EXPECT_NEAR(program_cache_entries, expected_program_cache_entries_map.at(run_type), abs_tol);
 
     // verify time per step
