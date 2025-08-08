15ce977367e5bbc6dd56bf04f3b2596222249545
Ubuntu <ioannis_alexiou@epam.com>
2025-08-08 15:10:58 +0000

    bash file gt edit

--- models/tt_transformers/demo/benchmarks.sh
+++ models/tt_transformers/demo/benchmarks.sh
@@ -17,7 +17,7 @@ llama11b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.2-11B-Vision-Instruct/
 llama70b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.1-70B-Instruct/
 llama90b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.2-90B-Vision-Instruct/
 
-MESH_DEVICE=T3K
+MESH_DEVICE=TG
 for LLAMA_DIR in "$llama1b" "$llama3b" "$llama8b" "$llama11b"; do
     MESH_DEVICE=$MESH_DEVICE LLAMA_DIR=$LLAMA_DIR WH_ARCH_YAML=$wh_arch_yaml pytest -n auto models/tt_transformers/demo/simple_text_demo.py -k "accuracy and ci-token-matching" --timeout=3600 ; fail+=$?
 done
