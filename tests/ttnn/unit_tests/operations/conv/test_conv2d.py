3c4492a29a966059274ce8c30f16fc63e5890ed7
Marko Bezulj <mbezulj@tenstorrent.com>
2025-08-08 07:30:18 +0000

    test_conv_dram_panoptic

--- tests/ttnn/unit_tests/operations/conv/test_conv2d.py
+++ tests/ttnn/unit_tests/operations/conv/test_conv2d.py
@@ -106,34 +106,24 @@ SliceHeight = ttnn.Conv2dSliceHeight
 SliceWidth = ttnn.Conv2dSliceWidth
 
 
-# @skip_for_blackhole("Not fully tested on Blackhole")
+@skip_for_blackhole("Not fully tested on Blackhole")
 @pytest.mark.parametrize(
     "input_layout, dtype",
-    [
-        [ttnn.TILE_LAYOUT, ttnn.bfloat8_b],
-    ],  # , [ttnn.ROW_MAJOR_LAYOUT, ttnn.bfloat16]],
+    [[ttnn.TILE_LAYOUT, ttnn.bfloat8_b], [ttnn.ROW_MAJOR_LAYOUT, ttnn.bfloat16]],
 )
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
 @pytest.mark.parametrize(
     "batch_size, input_channels, output_channels, input_height, input_width, slice_type, num_slices, weights_dtype, kernel, stride, padding, dilation, act_block_h_override,  math_fidelity",
     # fmt: off
     (
-        # (2,  13,   31,  313,    71,   SliceWidth,   16,  ttnn.bfloat8_b, (5, 5), (1, 1), (2, 2), (2, 2), 32 * 4,  ttnn.MathFidelity.LoFi  ),
-        # (2,  63,  129,  981,    39,   SliceHeight,  16,  ttnn.bfloat8_b, (3, 3), (2, 2), (2, 2), (1, 1),      0,  ttnn.MathFidelity.LoFi  ),
-        # (2, 512,  512,  128,   128,   SliceWidth,    4,  ttnn.bfloat8_b, (3, 3), (1, 1), (1, 1), (1, 1), 32 * 8,  ttnn.MathFidelity.LoFi  ),
-        # (2, 64,   64,   384,   64,    SliceHeight,   6,  ttnn.bfloat8_b, (4, 4), (2, 2), (1, 1), (1, 1), 0,       ttnn.MathFidelity.LoFi  ),
-        # (1, 4,    32,   1024,  1024,  SliceWidth,    4,  ttnn.bfloat8_b, (5, 5), (1, 1), (0, 0), (1, 1), 32,      ttnn.MathFidelity.LoFi  ),
-        # (1, 64,   128,  992,   992,   SliceWidth,   64,  ttnn.bfloat8_b, (2, 2), (1, 1), (0, 0), (1, 1), 32 * 4,  ttnn.MathFidelity.LoFi  ),
-        # (1, 2904, 2904,  48,    48,   SliceWidth,   4,  ttnn.bfloat8_b, (3, 3), (1, 1), (0, 0), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ),
-        # (1, 2944, 2944,  48,    48,   SliceWidth,   4,  ttnn.bfloat8_b,  (3, 3), (1, 1), (0, 0), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ),
-        (1, 2048, 256,  32,    64,   SliceHeight,   2,  ttnn.bfloat8_b,  (3, 3), (1, 1), (12, 12), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ), #Panotpic
-        # (1, 2048, 256,  32,    64,   SliceHeight,   2,  ttnn.bfloat8_b,  (3, 3), (1, 1), (18, 18), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ), #Panotpic OOM for SliceHeight(2,4,8,16,32 slices), hangs for SliceWidth
-        (1, 2048, 256,  32,    64,   SliceWidth,   2,  ttnn.bfloat8_b,  (3, 3), (1, 1), (6, 6), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ), #Panotpic OOM for SliceHeight (2,4,8,16,32 slices)
-        (1, 256, 256,  128,    256,   SliceHeight,   2,  ttnn.bfloat8_b,  (3, 3), (1, 1), (1, 1), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ), #Panotpic
-        (1, 256, 512,  128,    256,   SliceHeight,   4,  ttnn.bfloat8_b,  (1, 1), (2, 2), (1, 1), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ), #Panotpic
-        (1, 288, 256,  128,    256,   SliceHeight,   4,  ttnn.bfloat8_b,  (3, 3), (1, 1), (1, 1), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ), #Panotpic
-        (1, 64, 128,  256,    512,   SliceHeight,   2,  ttnn.bfloat8_b,  (3, 3), (1, 1), (1, 1), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ), #Panotpic
-        (1, 64, 64,  256,    512,   SliceHeight,   2,  ttnn.bfloat8_b,  (3, 3), (1, 1), (1, 1), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ), #Panotpic
+        (2,  13,   31,  313,    71,   SliceWidth,   16,  ttnn.bfloat8_b, (5, 5), (1, 1), (2, 2), (2, 2), 32 * 4,  ttnn.MathFidelity.LoFi  ),
+        (2,  63,  129,  981,    39,   SliceHeight,  16,  ttnn.bfloat8_b, (3, 3), (2, 2), (2, 2), (1, 1),      0,  ttnn.MathFidelity.LoFi  ),
+        (2, 512,  512,  128,   128,   SliceWidth,    4,  ttnn.bfloat8_b, (3, 3), (1, 1), (1, 1), (1, 1), 32 * 8,  ttnn.MathFidelity.LoFi  ),
+        (2, 64,   64,   384,   64,    SliceHeight,   6,  ttnn.bfloat8_b, (4, 4), (2, 2), (1, 1), (1, 1), 0,       ttnn.MathFidelity.LoFi  ),
+        (1, 4,    32,   1024,  1024,  SliceWidth,    4,  ttnn.bfloat8_b, (5, 5), (1, 1), (0, 0), (1, 1), 32,      ttnn.MathFidelity.LoFi  ),
+        (1, 64,   128,  992,   992,   SliceWidth,   64,  ttnn.bfloat8_b, (2, 2), (1, 1), (0, 0), (1, 1), 32 * 4,  ttnn.MathFidelity.LoFi  ),
+        (1, 2904, 2904,  48,    48,   SliceWidth,   4,  ttnn.bfloat8_b, (3, 3), (1, 1), (0, 0), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ),
+        (1, 2944, 2944,  48,    48,   SliceWidth,   4,  ttnn.bfloat8_b,  (3, 3), (1, 1), (0, 0), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ),
     )
     # fmt: on
 )
@@ -290,4 +280,92 @@ def test_conv2d_panoptic(
         padding=(pad_h, pad_w),
         groups=groups,
         has_bias=has_bias,
+    )
+
+
+# @skip_for_blackhole("Not fully tested on Blackhole")
+@pytest.mark.parametrize(
+    "input_layout, dtype",
+    [
+        [ttnn.TILE_LAYOUT, ttnn.bfloat8_b],
+    ],  # , [ttnn.ROW_MAJOR_LAYOUT, ttnn.bfloat16]],
+)
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 32768}], indirect=True)
+@pytest.mark.parametrize(
+    "batch_size, input_channels, output_channels, input_height, input_width, slice_type, num_slices, weights_dtype, kernel, stride, padding, dilation, act_block_h_override,  math_fidelity",
+    # fmt: off
+    (
+        (1, 2048, 256,  32,    64,   SliceHeight,   2,  ttnn.bfloat8_b,  (3, 3), (1, 1), (12, 12), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ), # Panoptic
+        # (1, 2048, 256,  32,    64,   SliceHeight,   2,  ttnn.bfloat8_b,  (3, 3), (1, 1), (18, 18), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ), # Panoptic OOM for SliceHeight(2,4,8,16,32 slices), hangs for SliceWidth
+        (1, 2048, 256,  32,    64,   SliceWidth,   2,  ttnn.bfloat8_b,  (3, 3), (1, 1), (6, 6), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ), # Panoptic OOM for SliceHeight (2,4,8,16,32 slices)
+        (1, 256, 256,  128,    256,   SliceHeight,   2,  ttnn.bfloat8_b,  (3, 3), (1, 1), (1, 1), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ), # Panoptic
+        (1, 256, 512,  128,    256,   SliceHeight,   4,  ttnn.bfloat8_b,  (1, 1), (2, 2), (1, 1), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ), # Panoptic
+        (1, 288, 256,  128,    256,   SliceHeight,   4,  ttnn.bfloat8_b,  (3, 3), (1, 1), (1, 1), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ), # Panoptic
+        (1, 64, 128,  256,    512,   SliceHeight,   2,  ttnn.bfloat8_b,  (3, 3), (1, 1), (1, 1), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ), # Panoptic
+        (1, 64, 64,  256,    512,   SliceHeight,   2,  ttnn.bfloat8_b,  (3, 3), (1, 1), (1, 1), (1, 1), 32,  ttnn.MathFidelity.HiFi4  ), # Panoptic
+    )
+    # fmt: on
+)
+@pytest.mark.parametrize(
+    "has_bias, fp32_accum, packer_l1_acc",
+    [[True, True, False]],
+)
+def test_conv_dram_panoptic(
+    device,
+    torch_tensor_map,
+    batch_size,
+    output_channels,
+    input_channels,
+    input_height,
+    input_width,
+    has_bias,
+    weights_dtype,
+    dtype,
+    slice_type,
+    num_slices,
+    kernel,
+    stride,
+    padding,
+    dilation,
+    act_block_h_override,
+    math_fidelity,
+    fp32_accum,
+    input_layout,
+    packer_l1_acc,
+):
+    if device.core_grid.y == 7:
+        pytest.skip("Tests have been configured for N150.")
+    config = {
+        "act_block_h": act_block_h_override,
+    }
+
+    run_conv(
+        device,
+        torch_tensor_map,
+        math_fidelity,
+        dtype,
+        weights_dtype,
+        batch_size,
+        output_channels,
+        input_channels,
+        input_height,
+        input_width,
+        kernel[0],
+        kernel[1],
+        stride[0],
+        stride[1],
+        padding,
+        config,
+        has_bias=True,
+        fp32_accum=fp32_accum,
+        packer_l1_acc=packer_l1_acc,
+        input_dtype=dtype,
+        input_layout=input_layout,
+        output_layout=input_layout,
+        run_twice=True,
+        fast_compare=True,
+        slice_config=ttnn.Conv2dSliceConfig(
+            slice_type=slice_type,
+            num_slices=num_slices,
+        ),
     )
