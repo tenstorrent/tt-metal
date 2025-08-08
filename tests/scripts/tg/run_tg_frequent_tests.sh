2b52bb6f091fec80af91746cc6f9813833cad027
Djordje Ivanovic <divanovic@tenstorrent.com>
2025-08-08 09:13:47 +0000

    #0: Skip test and run freq 20 times

--- tests/scripts/tg/run_tg_frequent_tests.sh
+++ tests/scripts/tg/run_tg_frequent_tests.sh
@@ -13,8 +13,13 @@ run_tg_llama3_tests() {
   # Run all Llama3 tests for 8B, 1B, and 3B weights
   # for llama_dir in "$llama1b" "$llama3b" "$llama8b" "$llama11b" "$llama70b"; do
   for llama_dir in "$llama70b"; do
-    LLAMA_DIR=$llama_dir FAKE_DEVICE=TG pytest --timeout 1800 -n auto models/demos/llama3_subdevices/tests/test_llama_model_nd.py --timeout=1800 ; fail+=$?
-    LLAMA_DIR=$llama_dir FAKE_DEVICE=TG pytest --timeout 1800 -n auto models/demos/llama3_subdevices/tests/test_llama_model.py -k full --timeout=1800 ; fail+=$?
+    # LLAMA_DIR=$llama_dir FAKE_DEVICE=TG pytest --timeout 1800 -n auto models/demos/llama3_subdevices/tests/test_llama_model_nd.py --timeout=1800 ; fail+=$?
+    # for i in range 20 run it 20 times
+    for i in {1..20}; do
+      echo "LOG_METAL: Running Llama3 tests, Iteration: $i"
+      LLAMA_DIR=$llama_dir FAKE_DEVICE=TG pytest --timeout 1800 -n auto models/demos/llama3_subdevices/tests/test_llama_model.py -k full --timeout=1800 ; fail+=$?
+    done
+
     echo "LOG_METAL: Llama3 tests for $llama_dir completed"
   done
 
