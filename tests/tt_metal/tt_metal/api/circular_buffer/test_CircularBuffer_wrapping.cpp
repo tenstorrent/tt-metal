241b24e374c6412c0a9e2365bd93a53da83d812b
River Wu <riverwu@tenstorrent.com>
2025-08-09 02:48:57 +0000

    attempt to reproduce incorrect behavior

---
+++ tests/tt_metal/tt_metal/api/circular_buffer/test_CircularBuffer_wrapping.cpp
@@ -0,0 +1,48 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <vector>
+#include "device_fixture.hpp"
+#include "gtest/gtest.h"
+#include "host_api.hpp"
+#include "tt_metal.hpp"
+
+using namespace tt;
+using namespace tt::tt_metal;
+
+constexpr CoreCoord worker_core = {0, 0};
+
+// CB have 64 pages.
+constexpr size_t cb_size = 128;
+constexpr size_t cb_page_size = 2;
+
+/**
+ *  This test checks that the cb_reserve_back will wait correctly when the buffer is full.
+ *  There's two ways to test this:
+ *  1. Fill the buffer up till it wraps around, and then check if this operation corupts any data in DRAM.
+ */
+
+TEST_F(DeviceFixture, TensixTestCircularBufferWrapping) {
+    auto device = devices_.at(0);
+    Program program;
+    CreateKernel(
+        program,
+        "tests/tt_metal/tt_metal/test_kernels/misc/circular_buffer/cb_wrapping_test_writer.cpp",
+        worker_core,
+        WriterDataMovementConfig{});
+
+    CreateKernel(
+        program,
+        "tests/tt_metal/tt_metal/test_kernels/misc/circular_buffer/cb_wrapping_test_reader.cpp",
+        worker_core,
+        ComputeConfig{});
+
+    CreateCircularBuffer(
+        program,
+        worker_core,
+        CircularBufferConfig{cb_size, {{CBIndex::c_0, DataFormat::UInt32}}}.set_page_size(CBIndex::c_0, cb_page_size));
+
+    detail::CompileProgram(device, program);
+    detail::LaunchProgram(device, program, true);
+}
---
+++ tests/tt_metal/tt_metal/test_kernels/misc/circular_buffer/cb_wrapping_test_writer.cpp
@@ -0,0 +1,56 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <cstdint>
+#include <limits>
+#include <tuple>
+#include "circular_buffer.h"
+#include "debug/debug.h"
+#include "dataflow_api.h"
+#include "debug/dprint.h"
+
+using namespace tt;
+
+/*
+ * The CB holdes 64 pages.
+ */
+
+static constexpr auto cb_id = tt::CBIndex::c_0;
+static constexpr auto cb_step_size = 32;
+
+void kernel_main() {
+    // We bring the acked and received a single page (32) from overflow
+    for (uint32_t i = 0; i < 2046; i++) {
+        cb_reserve_back(cb_id, cb_step_size);
+        cb_push_back(cb_id, cb_step_size);
+    }
+
+    // Synchronize with the reader
+    while (*get_cb_tiles_acked_ptr(cb_id) != *get_cb_tiles_received_ptr(cb_id)) {
+    }
+
+    // Both counter would be at 0xFFC0, 64 pages away from wrapping.
+    if (*get_cb_tiles_received_ptr(cb_id) != 0xFFC0) {
+        DPRINT << "Not stopping at 0xFFC0 as expected! Exiting" << ENDL();
+        return;
+    }
+
+    // Here we overflow the received count and fill the buffer.
+    cb_reserve_back(cb_id, cb_step_size);
+    cb_push_back(cb_id, cb_step_size);
+
+    // Note: Reader is not pulling any more data out of the buffer,
+    // buffer stays full.
+
+    DPRINT << "Before: Received: " << HEX() << *get_cb_tiles_received_ptr(cb_id)
+           << " Acked: " << *get_cb_tiles_acked_ptr(cb_id) << ENDL();
+
+    // This reserve should not return
+    cb_reserve_back(cb_id, cb_step_size + 1);
+
+    DPRINT << "Should be unreachable" << ENDL();
+
+    DPRINT << "After: Received: " << HEX() << *get_cb_tiles_received_ptr(cb_id)
+           << " Acked: " << *get_cb_tiles_acked_ptr(cb_id) << ENDL();
+}
