tenstorrent/tt-metal

commit 45a016c8f44ae39c583fb06969ed4f4f38946912

ControlPlane API changes updated to support big mesh (#25672)
### Ticket
[Link to Github
Issue](https://github.com/tenstorrent/tt-metal/issues/25668)

### Problem description
Some fabric direction getting apis in ControlPlane are erroring out for
Big Mesh when the dest is not on local host.

### What's changed
Updated apis to account for remote host dest fabric ids.
Added tests.
Added trimming of routing planes globally in a big mesh
Added pinning for nw chip for UBB.

### Checklist
- [ ] [All post
commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
CI passes
- [ ] [Blackhole Post
commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
CI with demo tests passes (if applicable)
- [ ] [Model
regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
CI passes (if applicable)
- [ ] [Device performance
regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
CI passes (if applicable)
- [ ] (For models and ops writers) [Single-card demo
tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
CI passes (if applicable) See [recommended dev
flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
- [ ] [Galaxy
quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
CI passes (if applicable)
- [ ] [TG demo tests, for
Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
CI passes, if applicable, because of current Llama work
- [ ] (For runtime and ops writers) [T3000 unit
tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
CI passes (if applicable, since this is run on push to main)
- [ ] (For models and ops writers) [T3000 demo
tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)

tt_metal/fabric/control_plane.cpp
@@ -52,6 +52,29 @@ namespace tt::tt_fabric {
 
 namespace {
 
+// TODO: remove once we have system descriptor apis
+struct UbbId {
+    std::uint32_t tray_id;
+    std::uint32_t asic_id;
+};
+
+const std::unordered_map<tt::ARCH, std::vector<std::uint16_t>> ubb_bus_ids = {
+    {tt::ARCH::WORMHOLE_B0, {0xC0, 0x80, 0x00, 0x40}},
+    {tt::ARCH::BLACKHOLE, {0x00, 0x40, 0xC0, 0x80}},
+};
+
+UbbId get_ubb_id(chip_id_t chip_id) {
+    const auto& cluster = tt::tt_metal::MetalContext::instance().get_cluster();
+    const auto& tray_bus_ids = ubb_bus_ids.at(cluster.arch());
+    const auto bus_id = cluster.get_bus_id(chip_id);
+    auto tray_bus_id_it = std::find(tray_bus_ids.begin(), tray_bus_ids.end(), bus_id & 0xF0);
+    if (tray_bus_id_it != tray_bus_ids.end()) {
+        auto ubb_asic_id = bus_id & 0x0F;
+        return UbbId{tray_bus_id_it - tray_bus_ids.begin() + 1, ubb_asic_id};
+    }
+    return UbbId{0, 0};  // Invalid UBB ID if not found
+}
+
 // Helper to extract intermesh ports from config value
 std::vector<chan_id_t> extract_intermesh_eth_links(uint32_t config_value, chip_id_t chip_id) {
     std::vector<chan_id_t> intermesh_eth_links;
@@ -213,6 +236,7 @@ void ControlPlane::initialize_dynamic_routing_plane_counts(
         }
     };
 
+    const auto& distributed_context = tt::tt_metal::MetalContext::instance().global_distributed_context();
     // For each mesh in the system
     for (auto mesh_id : user_meshes) {
         const auto& mesh_shape = this->get_physical_mesh_shape(MeshId{mesh_id});
@@ -268,11 +292,21 @@ void ControlPlane::initialize_dynamic_routing_plane_counts(
 
             // TODO: specialize by topology for better perf
             if (topology == Topology::Mesh || topology == Topology::Torus) {
+                const auto& mesh_host_ranks = this->routing_table_generator_->mesh_graph->get_host_ranks(mesh_id);
                 const auto rows_min = std::min_element(row_min_planes.begin(), row_min_planes.end());
                 const auto cols_min = std::min_element(col_min_planes.begin(), col_min_planes.end());
-                const auto mesh_min = std::min(*rows_min, *cols_min);
-                std::fill(row_min_planes.begin(), row_min_planes.end(), mesh_min);
-                std::fill(col_min_planes.begin(), col_min_planes.end(), mesh_min);
+                auto mesh_min = std::min(*rows_min, *cols_min);
+
+                std::vector<size_t> recv_buf(*distributed_context.size());
+                distributed_context.all_gather(
+                    tt::stl::Span<std::byte>(reinterpret_cast<std::byte*>(&mesh_min), sizeof(size_t)),
+                    tt::stl::as_writable_bytes(tt::stl::Span<size_t>{recv_buf.data(), recv_buf.size()}));
+
+                distributed_context.barrier();
+
+                auto global_mesh_min = std::min(recv_buf.begin(), recv_buf.end());
+                std::fill(row_min_planes.begin(), row_min_planes.end(), *global_mesh_min);
+                std::fill(col_min_planes.begin(), col_min_planes.end(), *global_mesh_min);
             }
 
             // Second pass: Apply minimums to each device
@@ -536,6 +570,17 @@ std::map<FabricNodeId, chip_id_t> ControlPlane::get_logical_chip_to_physical_chi
             const auto& mesh_container = this->routing_table_generator_->mesh_graph->get_chip_ids(mesh_id, host_rank_id);
 
             std::optional<chip_id_t> nw_chip_physical_id = std::nullopt;
+            const auto& cluster = tt::tt_metal::MetalContext::instance().get_cluster();
+            // TODO: remove once we use global physical graph to map logical big mesh to physical chips
+            if (cluster.get_board_type(0) == BoardType::UBB) {
+                for (const auto& chip_id : cluster.all_chip_ids()) {
+                    auto candidate_ubb_id = get_ubb_id(chip_id);
+                    if (candidate_ubb_id.tray_id == 1 && candidate_ubb_id.asic_id == 1) {
+                        nw_chip_physical_id = chip_id;
+                    }
+                }
+            }
+
             const auto& physical_chip_ids = this->get_mesh_physical_chip_ids(mesh_container, nw_chip_physical_id);
 
             std::uint32_t i = 0;
@@ -1159,7 +1204,12 @@ std::vector<std::pair<FabricNodeId, chan_id_t>> ControlPlane::get_fabric_route(
     // Fabric Route will terminate at the exit node if the host does not own the destination node. i.e. dest is not on a
     // mesh or coordinte range owned by the host.
     bool end_route_at_exit_node =
-        !(this->is_local_mesh(dst_fabric_node_id.mesh_id) and host_local_coord_range.contains(dst_mesh_coord));
+        ((dst_fabric_node_id.mesh_id != src_fabric_node_id.mesh_id) and
+         !(this->is_local_mesh(dst_fabric_node_id.mesh_id)) and (host_local_coord_range.contains(dst_mesh_coord)));
+    // If getting route within mesh, but the dest is not local, we stop at the last local node in the mesh.
+    bool end_route_at_edge_of_local_mesh =
+        ((dst_fabric_node_id.mesh_id == src_fabric_node_id.mesh_id) and
+         !(host_local_coord_range.contains(dst_mesh_coord)));
 
     TT_FATAL(
         valid_src,
@@ -1169,20 +1219,9 @@ std::vector<std::pair<FabricNodeId, chan_id_t>> ControlPlane::get_fabric_route(
         this->local_mesh_binding_.host_rank,
         src_fabric_node_id.mesh_id);
 
-    std::vector<FabricNodeId> candidate_end_nodes;
-    if (end_route_at_exit_node) {
-        // If routing to a mesh remote to the host, we need to generate a path to a local exit node that can direct
-        // traffic to the remote mesh.
-        candidate_end_nodes = routing_table_generator_->get_exit_nodes_routing_to_mesh(dst_fabric_node_id.mesh_id);
-    } else {
-        // If routing to a mesh local to the host, we can route directly to the destination chip.
-        candidate_end_nodes.push_back(dst_fabric_node_id);
-    }
-
     std::vector<std::pair<FabricNodeId, chan_id_t>> route;
     int i = 0;
-    while (std::find(candidate_end_nodes.begin(), candidate_end_nodes.end(), src_fabric_node_id) ==
-           candidate_end_nodes.end()) {
+    while (src_fabric_node_id != dst_fabric_node_id) {
         i++;
         auto src_mesh_id = src_fabric_node_id.mesh_id;
         auto src_chip_id = src_fabric_node_id.chip_id;
@@ -1212,15 +1251,31 @@ std::vector<std::pair<FabricNodeId, chan_id_t>> ControlPlane::get_fabric_route(
             route.push_back({src_fabric_node_id, next_chan_id});
         }
 
-        std::tie(src_fabric_node_id, src_chan_id) =
-            this->get_connected_mesh_chip_chan_ids(src_fabric_node_id, next_chan_id);
-        route.push_back({src_fabric_node_id, src_chan_id});
-    }
-    if (end_route_at_exit_node) {
-        // When routing to a remote mesh, append the exit node to the route.
-        route.push_back(
-            {src_fabric_node_id,
-             this->inter_mesh_routing_tables_.at(src_fabric_node_id)[src_chan_id][*dst_fabric_node_id.mesh_id]});
+        auto dst_mesh_coord = this->routing_table_generator_->mesh_graph->chip_to_coordinate(
+            dst_fabric_node_id.mesh_id, dst_fabric_node_id.chip_id);
+        if (this->is_local_mesh(src_mesh_id) and host_local_coord_range.contains(dst_mesh_coord)) {
+            std::tie(src_fabric_node_id, src_chan_id) =
+                this->get_connected_mesh_chip_chan_ids(src_fabric_node_id, next_chan_id);
+            route.push_back({src_fabric_node_id, src_chan_id});
+        } else {
+            TT_ASSERT(
+                end_route_at_exit_node or end_route_at_edge_of_local_mesh,
+                "ControlPlane: route between {} and {} should not end at exit node or try to exit local mesh",
+                src_fabric_node_id,
+                dst_fabric_node_id);
+            if (end_route_at_exit_node) {
+                std::vector<FabricNodeId> candidate_end_nodes =
+                    routing_table_generator_->get_exit_nodes_routing_to_mesh(dst_fabric_node_id.mesh_id);
+                // Check that the current node is a valid exit node
+                TT_ASSERT(
+                    std::find(candidate_end_nodes.begin(), candidate_end_nodes.end(), src_fabric_node_id) !=
+                        candidate_end_nodes.end(),
+                    "ControlPlane: src_fabric_node_id {} not found in candidate_end_nodes",
+                    src_fabric_node_id);
+            }
+            route.push_back({src_fabric_node_id, src_chan_id});
+            break;
+        }
     }
     return route;
 }

tests/tt_metal/tt_fabric/fabric_router/test_multi_host.cpp
@@ -45,5 +45,41 @@ TEST(MultiHost, TestDualGalaxyFabricSanity) {
     tt::tt_metal::MetalContext::instance().initialize_fabric_config();
 }
 
+TEST(MultiHost, TestDualGalaxyControlPlaneAPIs) {
+    tt::tt_metal::MetalContext::instance().set_fabric_config(
+        tt::tt_fabric::FabricConfig::FABRIC_2D_DYNAMIC,
+        tt::tt_fabric::FabricReliabilityMode::STRICT_SYSTEM_HEALTH_SETUP_MODE);
+    tt::tt_metal::MetalContext::instance().initialize_fabric_config();
+    auto& control_plane = tt::tt_metal::MetalContext::instance().get_control_plane();
+
+    FabricNodeId src_node_id(MeshId{0}, 3);  // On host rank 0
+    MeshCoordinate src_mesh_coord(0, 3);
+    FabricNodeId dst_node_id(MeshId{0}, 4);  // On host rank 1
+    MeshCoordinate dst_mesh_coord(0, 4);
+
+    auto host_local_coord_range = control_plane.get_coord_range(MeshId{0}, MeshScope::LOCAL);
+    if (host_local_coord_range.contains(src_mesh_coord)) {
+        const auto& direction = control_plane.get_forwarding_direction(src_node_id, dst_node_id);
+        EXPECT_EQ(direction, RoutingDirection::E);
+
+        const auto& eth_chans_by_direction =
+            control_plane.get_forwarding_eth_chans_to_chip(src_node_id, dst_node_id, RoutingDirection::E);
+        EXPECT_TRUE(!eth_chans_by_direction.empty());
+        const auto& eth_chans = control_plane.get_forwarding_eth_chans_to_chip(src_node_id, dst_node_id);
+        EXPECT_TRUE(!eth_chans.empty());
+    }
+
+    if (host_local_coord_range.contains(dst_mesh_coord)) {
+        const auto& reverse_direction = control_plane.get_forwarding_direction(dst_node_id, src_node_id);
+        EXPECT_EQ(reverse_direction, RoutingDirection::W);
+
+        const auto& eth_chans = control_plane.get_forwarding_eth_chans_to_chip(dst_node_id, src_node_id);
+        EXPECT_TRUE(!eth_chans.empty());
+        const auto& eth_chans_by_direction =
+            control_plane.get_forwarding_eth_chans_to_chip(dst_node_id, src_node_id, RoutingDirection::W);
+        EXPECT_TRUE(!eth_chans_by_direction.empty());
+    }
+}
+
 }  // namespace multi_host_tests
 }  // namespace tt::tt_fabric

tt_metal/fabric/fabric_host_utils.cpp
@@ -66,6 +66,7 @@ std::vector<uint32_t> get_forwarding_link_indices_in_direction(
         forwarding_channels =
             control_plane.get_forwarding_eth_chans_to_chip(src_fabric_node_id, dst_fabric_node_id, direction);
     } else {
+        // TODO: not going to work for Big Mesh
         const auto src_chip_id = control_plane.get_physical_chip_id_from_fabric_node_id(src_fabric_node_id);
         const auto dst_chip_id = control_plane.get_physical_chip_id_from_fabric_node_id(dst_fabric_node_id);
         // for 1D check if each port has an active connection to the dst_chip_id

commit 4d08b8c3b01ade3806177d4fb4db1a3c9a12dbe6

Resolve issues when get_fabric_route is called for Cross-Host Endpoints (#26590)
### Ticket
No Ticket.

### Problem description
- Multi-Process Fabric tests are failing on main after
https://github.com/tenstorrent/tt-metal/commit/45a016c8f44ae39c583fb06969ed4f4f38946912.
- Failures occur because the logic in `ControlPlane::get_fabric_route`
to determine if the current link is on an exit node is incorrect.

### What's changed
- Update check to query if the current link is a cross host link instead
of checking if its on a mesh that is not local to the current host
- The latter will never be true

### Checklist
- [ ] [All post
commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
CI passes
- [ ] [Blackhole Post
commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
CI with demo tests passes (if applicable)
- [ ] [Model
regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
CI passes (if applicable)
- [ ] [Device performance
regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
CI passes (if applicable)
- [ ] (For models and ops writers) [Single-card demo
tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
CI passes (if applicable) See [recommended dev
flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
- [ ] [Galaxy
quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
CI passes (if applicable)
- [ ] [TG demo tests, for
Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
CI passes, if applicable, because of current Llama work
- [ ] (For runtime and ops writers) [T3000 unit
tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
CI passes (if applicable, since this is run on push to main)

tt_metal/fabric/control_plane.cpp
@@ -1202,7 +1202,7 @@ std::vector<std::pair<FabricNodeId, chan_id_t>> ControlPlane::get_fabric_route(
     bool valid_src =
         this->is_local_mesh(src_fabric_node_id.mesh_id) and host_local_coord_range.contains(src_mesh_coord);
     // Fabric Route will terminate at the exit node if the host does not own the destination node. i.e. dest is not on a
-    // mesh or coordinte range owned by the host.
+    // mesh or coordinate range owned by the host.
     bool end_route_at_exit_node =
         ((dst_fabric_node_id.mesh_id != src_fabric_node_id.mesh_id) and
          !(this->is_local_mesh(dst_fabric_node_id.mesh_id)) and (host_local_coord_range.contains(dst_mesh_coord)));
@@ -1251,13 +1251,18 @@ std::vector<std::pair<FabricNodeId, chan_id_t>> ControlPlane::get_fabric_route(
             route.push_back({src_fabric_node_id, next_chan_id});
         }
 
-        auto dst_mesh_coord = this->routing_table_generator_->mesh_graph->chip_to_coordinate(
-            dst_fabric_node_id.mesh_id, dst_fabric_node_id.chip_id);
-        if (this->is_local_mesh(src_mesh_id) and host_local_coord_range.contains(dst_mesh_coord)) {
+        auto physical_chip_id = this->get_physical_chip_id_from_fabric_node_id(src_fabric_node_id);
+        const auto& soc_desc = tt::tt_metal::MetalContext::instance().get_cluster().get_soc_desc(physical_chip_id);
+        auto next_eth_core = soc_desc.get_eth_core_for_channel(next_chan_id, CoordSystem::LOGICAL);
+        // Check if this link routes to a different host. If so, the connected link cannot be directly queried.
+        // The fabric route will end at the current chip, since it must be an exit node or an edge chip.
+        if (!this->is_intermesh_eth_link(physical_chip_id, CoreCoord(next_eth_core.x, next_eth_core.y))) {
             std::tie(src_fabric_node_id, src_chan_id) =
                 this->get_connected_mesh_chip_chan_ids(src_fabric_node_id, next_chan_id);
             route.push_back({src_fabric_node_id, src_chan_id});
         } else {
+            // Verify that the remote eth link could not be queried because the current node is an exit node or
+            // edge chip of the local mesh.
             TT_ASSERT(
                 end_route_at_exit_node or end_route_at_edge_of_local_mesh,
                 "ControlPlane: route between {} and {} should not end at exit node or try to exit local mesh",