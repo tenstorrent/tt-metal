a4bde2f57aa4b60348f4ff1c1b6167788266eea5
kpaigwar <kpaigwar@tenstorrent.com>
2025-08-11 19:47:16 +0000

    rms n150 test

--- models/demos/gpt_oss/tests/unit/test_rms_norm.py
+++ models/demos/gpt_oss/tests/unit/test_rms_norm.py
@@ -5,28 +5,23 @@ import torch
 from transformers import AutoConfig
 
 import ttnn
-
-
-class ReferenceRMSNorm(torch.nn.Module):
-    def __init__(self, num_features: int, eps: float = 1e-05, device: torch.device | None = None):
-        super().__init__()
-        self.num_features = num_features
-        self.eps = eps
-        self.scale = torch.nn.Parameter(torch.randn(num_features, device=device, dtype=torch.float32))
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        assert x.shape[-1] == self.num_features
-        t, dtype = x.float(), x.dtype
-        t = t * torch.rsqrt(torch.mean(t**2, dim=-1, keepdim=True) + self.eps)
-        return (t * self.scale).to(dtype)
+from models.demos.gpt_oss.reference.modeling_gpt_oss import GptOssRMSNorm
+from models.demos.gpt_oss.tt.rms_norm import RMSNorm
+from tests.ttnn.utils_for_testing import comp_pcc
 
 
 @pytest.fixture
 def hf_config():
-    """Load DeepSeek config for testing"""
-    path = os.getenv("HF_MODEL", "openai/gpt-oss-20b")
+    """Load GPT-OSS config for testing"""
+    path = os.getenv("HF_MODEL", "models/demos/gpt_oss/reference")
     config = AutoConfig.from_pretrained(path, trust_remote_code=True)
     return config
+
+
+@pytest.fixture
+def reference_model(hf_config):
+    model = GptOssRMSNorm(hf_config.hidden_size, hf_config.rms_norm_eps)
+    return model
 
 
 @pytest.mark.parametrize(
@@ -55,9 +50,46 @@ def test_rms_norm(
     mode,
     seq_len,
     hf_config,
+    reference_model,
 ):
     torch.manual_seed(0)
-    breakpoint()
+    mesh_shape = tuple(mesh_device.shape)
+    ref_rms_norm = reference_model.eval()
 
-    print(hf_config.hidden_size)
-    ref_rms_norm = ReferenceRMSNorm(hf_config.hidden_size)
+    # create a random tensor with shape (batch_size, seq_len, hidden_size)
+    torch_input = torch.randn(1, seq_len, hf_config.hidden_size)
+
+    # forward pass through the reference model
+    with torch.no_grad():
+        ref_output = ref_rms_norm(torch_input)
+
+    tt_input = ttnn.from_torch(
+        torch_input.unsqueeze(0),
+        device=mesh_device,
+        mesh_mapper=ttnn.ShardTensor2dMesh(mesh_device, mesh_device.shape, dims=(None, -1)),
+        dtype=ttnn.bfloat16,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        layout=ttnn.TILE_LAYOUT,
+    )
+    per_device_input_shape = tuple(tt_input.shape)
+    num_sharded_cores = 45
+    input_memory_config = ttnn.create_sharded_memory_config(
+        shape=(ttnn.TILE_SIZE, per_device_input_shape[-1] // num_sharded_cores),
+        core_grid=ttnn.CoreGrid(y=9, x=5),
+        strategy=ttnn.ShardStrategy.WIDTH,
+        orientation=ttnn.ShardOrientation.ROW_MAJOR,
+        use_height_and_width_as_shard_shape=True,
+    )
+    tt_input = ttnn.to_memory_config(tt_input, input_memory_config)
+
+    tt_rms_norm = RMSNorm(mesh_device, hf_config, reference_model.state_dict())
+    tt_output = tt_rms_norm(tt_input)
+
+    tt_output_torch = ttnn.to_torch(
+        tt_output,
+        mesh_composer=ttnn.ConcatMesh2dToTensor(mesh_device, mesh_device.shape, dims=(0, -1)),
+    )[0]
+
+    expected_pcc = 0.99
+    passing, pcc_message = comp_pcc(tt_output_torch, ref_output, pcc=expected_pcc)
+    assert passing, f"PCC is {pcc_message}, expected {expected_pcc}"
---
+++ models/demos/gpt_oss/tt/rms_norm.py
@@ -0,0 +1,77 @@
+from torch import nn
+
+import ttnn
+
+
+class RMSNorm(nn.Module):
+    def __init__(self, mesh_device, hf_config, state_dict):
+        super().__init__()
+        torch_weight = state_dict["weight"]
+        self.tt_weight = ttnn.as_tensor(
+            torch_weight.reshape((1, 1, -1, ttnn.TILE_SIZE)),
+            device=mesh_device,
+            dtype=ttnn.bfloat16,
+            layout=ttnn.ROW_MAJOR_LAYOUT,
+            memory_config=ttnn.DRAM_MEMORY_CONFIG,
+            mesh_mapper=ttnn.ShardTensor2dMesh(mesh_device, mesh_device.shape, dims=(None, -2)),
+        )
+
+        self.eps = hf_config.rms_norm_eps
+        self.is_distributed = mesh_device.shape[1] > 1
+        self.mesh_device = mesh_device
+        self.mesh_shape = tuple(mesh_device.shape)
+
+    def forward(self, x):
+        activation_grid_bounding_box_size = x.memory_config().shard_spec.grid.bounding_box().grid_size()
+        shard_height, shard_width = x.memory_config().shard_spec.shape
+        program_config = ttnn.LayerNormShardedMultiCoreProgramConfig(
+            compute_with_storage_grid_size=activation_grid_bounding_box_size,
+            subblock_w=1,
+            block_h=ttnn.core.divup(shard_height, ttnn.TILE_SIZE),
+            block_w=ttnn.core.divup(shard_width, ttnn.TILE_SIZE),
+            inplace=False,
+        )
+        if self.is_distributed:
+            # If the activation is sharded, we need to use an optimized rmsnorm
+
+            tt_gathered_stats_memory_config = ttnn.create_sharded_memory_config(
+                shape=[1, 1, 32, 32 * self.mesh_shape[1]],
+                core_grid=ttnn.CoreGrid(y=1, x=1),
+                strategy=ttnn.ShardStrategy.WIDTH,
+                orientation=ttnn.ShardOrientation.ROW_MAJOR,
+            )
+            # Run distributed rmsnorm part 1
+            tt_stats = ttnn.rms_norm_pre_all_gather(x, program_config=program_config, dtype=ttnn.bfloat16)
+
+            # AllGather stats
+            tt_gathered_stats = ttnn.all_gather(
+                tt_stats,
+                dim=3,
+                num_links=1,
+                cluster_axis=1,
+                mesh_device=self.mesh_device,
+                memory_config=tt_gathered_stats_memory_config,
+                topology=ttnn.Topology.Linear,
+            )
+            ttnn.deallocate(tt_stats)
+
+            # Run distributed rmsnorm part 2
+            tt_output = ttnn.rms_norm_post_all_gather(
+                x,
+                tt_gathered_stats,
+                program_config=program_config,
+                epsilon=self.eps,
+                weight=self.tt_weight,
+                dtype=ttnn.bfloat16,
+                stats=tt_gathered_stats,
+            )
+            ttnn.deallocate(tt_stats)
+            return tt_output
+        else:
+            tt_output = ttnn.rms_norm(
+                x,
+                weight=self.tt_weight,
+                epsilon=self.eps,
+                program_config=program_config,
+            )
+            return tt_output
