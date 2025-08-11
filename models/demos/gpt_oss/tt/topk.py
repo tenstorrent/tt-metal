d0d27f2077cf1d84ca41b4713a6cfde9ff596179
Colman Glagovich <cglagovich@tenstorrent.com>
2025-08-11 17:54:38 +0000

    Add topk and topk tests

---
+++ models/demos/gpt_oss/tt/topk.py
@@ -0,0 +1,46 @@
+import ttnn
+
+
+def topk_router(g, experts_per_token):
+    expert_weights, expert_indices = ttnn.topk(g, k=experts_per_token, dim=-1, sorted=True)
+    compute_config = ttnn.init_device_compute_kernel_config(
+        g.device().arch(),
+        math_fidelity=ttnn.MathFidelity.HiFi4,
+        math_approx_mode=False,
+        fp32_dest_acc_en=True,
+        packer_l1_acc=False,
+    )
+    expert_weights = ttnn.softmax(expert_weights, dim=1, numeric_stable=True, compute_kernel_config=compute_config)
+    router_scores = ttnn.scatter(ttnn.zeros_like(g), dim=1, index=expert_indices, src=expert_weights)
+    return router_scores, expert_weights, expert_indices
+
+
+class TopKRouter:
+    def __init__(self, config, state_dict, mesh_device):
+        self.top_k = config.num_experts_per_tok
+        self.num_experts = config.num_local_experts
+        self.hidden_dim = config.hidden_size
+        self.weight = ttnn.from_torch(
+            state_dict["weight"].transpose(0, 1), device=mesh_device, layout=ttnn.TILE_LAYOUT, dtype=ttnn.bfloat16
+        )
+        self.bias = ttnn.from_torch(
+            state_dict["bias"].unsqueeze(0), device=mesh_device, layout=ttnn.TILE_LAYOUT, dtype=ttnn.bfloat16
+        )
+
+        # NOTE: bad outputs when I provide any reasonable compute config
+        # self.compute_config = ttnn.init_device_compute_kernel_config(
+        #     mesh_device.arch(),
+        #     math_fidelity=ttnn.MathFidelity.HiFi2,
+        #     math_approx_mode=False,
+        #     fp32_dest_acc_en=False,
+        #     packer_l1_acc=False,
+        # )
+        self.compute_config = None
+
+    def __call__(self, hidden_states):
+        # hidden_states = hidden_states.reshape(-1, self.hidden_dim)
+        router_logits = ttnn.linear(
+            hidden_states, self.weight, bias=self.bias, compute_kernel_config=self.compute_config
+        )
+        router_scores, _expert_weights, router_indices = topk_router(router_logits, self.top_k)
+        return router_scores, router_indices, router_logits
