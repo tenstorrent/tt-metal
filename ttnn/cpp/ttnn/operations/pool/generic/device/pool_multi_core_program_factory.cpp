20ffe437bab6cd39a3bcfcb61545ac17b5568cab
wransom-TT <wransom@tenstorrent.com>
2025-08-11 20:47:20 +0000

    #0: reader kernel updated, avg and maxpool unit tests passing

--- ttnn/cpp/ttnn/operations/pool/generic/device/pool_multi_core_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/pool/generic/device/pool_multi_core_program_factory.cpp
@@ -352,6 +352,7 @@ Pool2D::MultiCore::cached_program_t pool2d_multi_core_sharded_with_halo_v2_impl_
     } else {
         if (params.is_wide_reduction) {
             in_cb_sz = params.MAX_TILES_PER_REDUCTION * tt::constants::TILE_WIDTH * params.num_tilized_rows;
+            in_nblocks_c = std::ceil((float)params.in_ntiles_c / params.MAX_TILES_PER_REDUCTION);
         } else {
             in_cb_sz = params.in_ntiles_c * tt::constants::TILE_WIDTH * params.num_tilized_rows;
         }
