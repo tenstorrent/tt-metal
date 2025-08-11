49d7c7b249f9bb4d5c79e17efa2e6737af09ec24
Colman Glagovich <cglagovich@tenstorrent.com>
2025-08-11 22:14:21 +0000

    Add layer (RMSNorm + MoE) test

---
+++ models/demos/gpt_oss/tt/layer.py
@@ -0,0 +1,44 @@
+from models.experimental.stable_diffusion_35_large.tt.substate import substate
+
+from .mlp import MLP
+from .rms_norm import RMSNorm
+
+
+class DecoderLayer:
+    def __init__(self, mesh_device, hf_config, state_dict, ccl_manager):
+        self.input_layernorm = RMSNorm(mesh_device, hf_config, substate(state_dict, "input_layernorm"))
+        self.post_attention_layernorm = RMSNorm(
+            mesh_device, hf_config, substate(state_dict, "post_attention_layernorm")
+        )
+        self.mlp = MLP(mesh_device, hf_config, substate(state_dict, "mlp"), ccl_manager)
+
+    def __call__(
+        self,
+        hidden_states,
+        attention_mask,
+        position_ids,
+        past_key_values,
+        use_cache,
+        cache_position,
+        position_embeddings,
+        **kwargs,
+    ):
+        # residual = hidden_states
+        # hidden_states = self.input_layernorm(hidden_states)
+        # hidden_states, _ = self.self_attn(
+        #     hidden_states=hidden_states,
+        #     attention_mask=attention_mask,
+        #     position_ids=position_ids,
+        #     past_key_values=past_key_values,
+        #     use_cache=use_cache,
+        #     cache_position=cache_position,
+        #     position_embeddings=position_embeddings,
+        #     **kwargs,
+        # )
+        # hidden_states = residual + hidden_states
+
+        residual = hidden_states
+        hidden_states = self.post_attention_layernorm(hidden_states)
+        hidden_states, _ = self.mlp(hidden_states)  # diff with llama: router scores
+        hidden_states = residual + hidden_states
+        return hidden_states
