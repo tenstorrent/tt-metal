c0c093c4b4324054f9d6f6748bcbfafb90ec6770
Aliaksei Sala <aliaksei_sala@epam.com>
2025-08-12 10:43:07 -0700

    Remove WITH LLVM-exception from  SPDX-License-Identifier

--- tt_stl/tt_stl/memory_alloc.hpp
+++ tt_stl/tt_stl/memory_alloc.hpp
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC.
 //
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// SPDX-License-Identifier: Apache-2.0
 //===----------------------------------------------------------------------===//
 //===- MemAlloc.h - Memory allocation functions -----------------*- C++ -*-===//
 //
