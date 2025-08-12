cc08a3cf1fb5de3fec40ef13da5d2a7873575753
Nathan Maurice <nmaurice@tenstorrent.com>
2025-08-12 08:07:49 +0000

    Move _float_to_int32_positive_ helper function to ckernel_sfpu_rounding_ops.h file

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_binary.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_binary.h
@@ -6,37 +6,13 @@
 
 #include "ckernel.h"
 #include "ckernel_defs.h"
+#include "ckernel_sfpu_rounding_ops.h"
 #include "sfpi.h"
 
 using namespace sfpi;
 
 namespace ckernel {
 namespace sfpu {
-
-// Helper function for _sfpu_binary_power_
-// This function is based on _float32_to_int32_, but expects a positive input, which allows us to optimize
-// away several lines (and make it faster)
-sfpi_inline sfpi::vInt _float_to_int32_positive_(sfpi::vFloat in) {
-    sfpi::vInt result;
-    sfpi::vInt exp = exexp(in);  // extract exponent
-    v_if(exp < 0) { result = 0; }
-    v_elseif(exp > 30)  // overflow occurs above this range
-    {
-        // set to int32 max value in case of overflow
-        result = std::numeric_limits<int32_t>::max();
-    }
-    v_else {
-        // extract mantissa
-        sfpi::vInt man = exman8(in);
-        // shift the mantissa by (23-exponent) to the right
-        sfpi::vInt shift = exp - 23;  // 23 is number of mantissa bits in float32
-        man = sfpi::reinterpret<sfpi::vInt>(shft(sfpi::reinterpret<sfpi::vUInt>(man), shift));
-
-        result = man;
-    }
-    v_endif;
-    return result;
-}
 
 /**
  * @brief Computes base raised to the power of pow (base**pow)
