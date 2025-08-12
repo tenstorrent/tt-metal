5b16c54710ce82f76d5e0d0af1eabbeb2effbeaf
Boris Janjic <bjanjic@tenstorrent.com>
2025-08-12 10:31:58 +0000

    group norm error

--- tests/ttnn/unit_tests/operations/fused/test_group_norm_DRAM.py
+++ tests/ttnn/unit_tests/operations/fused/test_group_norm_DRAM.py
@@ -9,6 +9,7 @@ import torch
 from loguru import logger
 
 import ttnn
+import math
 
 from tests.ttnn.utils_for_testing import assert_with_pcc, check_with_pcc
 from models.utility_functions import skip_for_wormhole_b0, skip_for_blackhole
@@ -132,3 +133,108 @@ def test_group_norm_DRAM(device, N, C, H, W, num_groups, num_out_blocks, cores_y
     output_tensor = ttnn.to_torch(output_tensor)
 
     assert_with_pcc(torch_output_tensor, output_tensor, 0.9996)
+
+
+def _nearest_32_per_core(x, core):
+    return math.ceil(x / core / 32) * 32 * core
+
+
+def _nearest_32(x):
+    return math.ceil(x / 32) * 32
+
+
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 0}], indirect=True)
+@pytest.mark.parametrize(
+    "N, C, H, W, num_groups, num_out_blocks, cores_y, cores_x",
+    [
+        ### oft
+        (1, 256, 159, 159, 16, 3, 4, 4),
+        (1, 512, 159, 159, 16, 3, 4, 4),
+        (1, 64, 192, 640, 16, 10, 4, 4),  # pcc drop AssertionError: 0.05151822326267651
+    ],
+)
+def test_group_norm_DRAM_oft(device, N, C, H, W, num_groups, num_out_blocks, cores_y, cores_x):
+    compute_grid = device.compute_with_storage_grid_size()
+    if compute_grid.x != 5 or compute_grid.y != 4:
+        pytest.skip(f"Test requires compute grid size of 5x4, but got {compute_grid.x}x{compute_grid.y}")
+
+    torch.manual_seed(0)
+    grid_size = ttnn.CoreGrid(y=cores_y, x=cores_x)
+    # torch input tensor
+    torch_input_tensor = torch.rand((N, C, H, W), dtype=torch.bfloat16)
+    torch_weight = torch.rand((C,), dtype=torch.bfloat16)
+    torch_bias = torch.rand((C,), dtype=torch.bfloat16)
+    torch_output_tensor = torch.nn.functional.group_norm(
+        torch_input_tensor, num_groups, weight=torch_weight, bias=torch_bias
+    )
+    torch_output_tensor = torch_output_tensor.permute(0, 2, 3, 1).view(N, 1, W * H, C)
+
+    # input tensor
+    input_tensor = torch_input_tensor.permute(0, 2, 3, 1).view(N, 1, W * H, C)
+    input_tensor_row_major = ttnn.from_torch(
+        input_tensor,
+        dtype=ttnn.DataType.BFLOAT16,
+        layout=ttnn.ROW_MAJOR_LAYOUT,
+        device=device,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+    print(
+        f"input_tensor_row_major shape: {input_tensor_row_major.shape} padded shape: {input_tensor_row_major.padded_shape}"
+    )
+    unpadded_shape = input_tensor_row_major.shape
+    out_shape = [
+        unpadded_shape[0],
+        unpadded_shape[1],
+        _nearest_32_per_core(unpadded_shape[2], cores_x),
+        _nearest_32_per_core(unpadded_shape[3], cores_y),
+    ]
+    print(f"unpadded_shape: {unpadded_shape} out_shape: {out_shape}")
+    input_tensor_tilized = ttnn.tilize_with_val_padding(
+        input_tensor_row_major, output_tensor_shape=out_shape, pad_value=0, use_multicore=True
+    )
+    print(f"input_tensor_tilized shape: {input_tensor_tilized.shape} padded shape: {input_tensor_tilized.padded_shape}")
+    input_mask_tensor = ttnn.create_group_norm_input_mask(C, num_groups, grid_size.y)
+    input_mask_tensor = ttnn.from_torch(
+        input_mask_tensor,
+        dtype=ttnn.DataType.BFLOAT8_B,
+        layout=ttnn.TILE_LAYOUT,
+        device=device,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+    # gamma/beta
+    gamma = ttnn.create_group_norm_weight_bias_rm(torch_weight, C, grid_size.y)
+    beta = ttnn.create_group_norm_weight_bias_rm(torch_bias, C, grid_size.y)
+    gamma_t = ttnn.from_torch(
+        gamma,
+        dtype=ttnn.DataType.BFLOAT16,
+        layout=ttnn.ROW_MAJOR_LAYOUT,
+        device=device,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+    beta_t = ttnn.from_torch(
+        beta,
+        dtype=ttnn.DataType.BFLOAT16,
+        layout=ttnn.ROW_MAJOR_LAYOUT,
+        device=device,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+    # groupnorm
+    output_tensor = ttnn.group_norm(
+        input_tensor_tilized,
+        num_groups=num_groups,
+        input_mask=input_mask_tensor,
+        weight=gamma_t,
+        bias=beta_t,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        output_layout=ttnn.TILE_LAYOUT,
+        core_grid=grid_size,
+        inplace=False,
+        num_out_blocks=num_out_blocks,
+        epsilon=1e-5,
+    )
+
+    ttnn.synchronize_device(device)
+    output_tensor = ttnn.from_device(output_tensor)
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor[:, :, : H * W, :C], 0.9995)
