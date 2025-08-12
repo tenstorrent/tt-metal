b011feb10e4578023b1ce8e849a2a5f7ec3fabe0
John Bauman <jbauman@tenstorrent.com>
2025-08-12 19:38:51 +0000

    fix check

--- tt_metal/hw/ckernels/blackhole/metal/llk_io/llk_io_pack.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_io/llk_io_pack.h
@@ -40,7 +40,8 @@ inline void llk_wait_for_free_tiles(const std::int32_t operand, const std::int32
         free_tiles = (std::int32_t)free_tiles_wrap;
     } while (free_tiles < num_tiles);
     // Assert that region is contiguous.
-    ASSERT(get_local_cb_interface(output).fifo_wr_ptr + num_tiles <= get_local_cb_interface(output).fifo_limit);
+    ASSERT(get_local_cb_interface(output).fifo_wr_ptr + num_tiles * get_local_cb_interface(output).fifo_page_size <=
+           get_local_cb_interface(output).fifo_limit);
 }
 
 inline void llk_push_to_brisc(const std::int32_t operand, const std::int32_t num_tiles, const std::int32_t num_words) {
