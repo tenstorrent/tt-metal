1bed8a5d1e00a3dd8ac2e1ee734d86ae4a852c01
Ryan Miller <rmiller@tenstorrent.com>
2025-08-13 13:29:03 +0000

    Fix a bad merge

--- ttnn/cpp/ttnn/operations/normalization/layernorm/device/multi_core/layernorm_op_multi_core.cpp
+++ ttnn/cpp/ttnn/operations/normalization/layernorm/device/multi_core/layernorm_op_multi_core.cpp
@@ -346,10 +346,6 @@ operation::ProgramWithCallbacks layernorm_multi_core(
         all_cores,
         tt::tt_metal::WriterDataMovementConfig(writer_compile_time_args));
 
-#ifdef LAYERNORM_WELFORD
-    std::vector<uint32_t> compute_args = {
-        Wt, block_size, gamma.has_value(), beta.has_value(), fp32_dest_acc_en, W, ttnn::types::TILE_SIZE};
-#else
     std::vector<uint32_t> compute_args = {Wt, block_size, gamma.has_value(), beta.has_value(), fp32_dest_acc_en};
     if (use_welford) {
         compute_args.push_back(W);
