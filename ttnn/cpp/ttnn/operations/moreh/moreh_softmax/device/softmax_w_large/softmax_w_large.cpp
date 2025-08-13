57d075047372bd3b4f696fd252c6449b2ba89ba7
mgajewskiTT <mgajewski@tenstorrent.com>
2025-08-13 09:17:27 +0000

    21906 - FP32 fix.

--- ttnn/cpp/ttnn/operations/moreh/moreh_softmax/device/softmax_w_large/softmax_w_large.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_softmax/device/softmax_w_large/softmax_w_large.cpp
@@ -43,6 +43,10 @@ MorehSoftmaxOperation::MorehSoftmaxWLargeFactory::create(
     auto [math_fidelity, math_approx_mode, fp32_dest_acc_en, packer_l1_acc, dst_full_sync_en] =
         get_compute_kernel_config_args(arch, compute_kernel_config);
 
+    if (input.dtype() == DataType::FLOAT32) {
+        fp32_dest_acc_en = true;
+    }
+
     Program program = Program();
 
     // create circular buffers
