1370318b59d1765df2736e73b1a4edf68fb19581
vguduruTT <venkatesh.guduru@multicorewareinc.com>
2025-08-13 16:09:36 +0000

    enable demo for eff_net, swin_v2, vovnet in civ2

--- models/experimental/efficientnetb0/demo/demo.py
+++ models/experimental/efficientnetb0/demo/demo.py
@@ -9,7 +9,7 @@ from loguru import logger
 
 import ttnn
 from models.utility_functions import run_for_wormhole_b0
-from models.experimental.efficientnetb0.demo.demo_utils import get_data_loader, get_batch
+from models.demos.utils.common_demo_utils import get_data_loader, get_batch, load_imagenet_dataset
 from models.experimental.efficientnetb0.runner.performant_runner import EfficientNetb0PerformantRunner
 from models.experimental.efficientnetb0.common import load_torch_model, EFFICIENTNETB0_L1_SMALL_SIZE
 from models.demos.utils.common_demo_utils import get_mesh_mappers
