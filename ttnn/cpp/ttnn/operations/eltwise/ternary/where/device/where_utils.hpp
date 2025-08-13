fedf1890498368fc4c88f86f81939108fd59101c
Aswinmcw <azayasankaran@tenstorrent.com>
2025-08-13 11:01:38 +0000

    Add row bcast support for llk where

--- ttnn/cpp/ttnn/operations/eltwise/ternary/where/device/where_utils.hpp
+++ ttnn/cpp/ttnn/operations/eltwise/ternary/where/device/where_utils.hpp
@@ -20,6 +20,7 @@ enum class KernelName {
     ReaderNoBcastTSS,
     ReaderColBcastTTT,
     ReaderOuterBcastTTT,
+    ReaderRowBcastTTT,
     WriterNoBcastTTT,
     WriterNoBcastTST,
     WriterNoBcastTTS,
