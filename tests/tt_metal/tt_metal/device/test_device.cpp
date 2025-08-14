73d423d0b4b9ce1c941f2be7695250d9a7cec3fe
John Bauman <jbauman@tenstorrent.com>
2025-08-14 21:22:52 +0000

    add high bits as well

--- tests/tt_metal/tt_metal/device/test_device.cpp
+++ tests/tt_metal/tt_metal/device/test_device.cpp
@@ -322,7 +322,7 @@ TEST_F(MeshDeviceFixture, TensixTestL1ToPCIeAt16BAlignedAddress) {
         DataMovementConfig{
             .processor = DataMovementProcessor::RISCV_1,
             .noc = NOC::RISCV_0_default,
-            .compile_args = {base_l1_src_address, base_pcie_dst_address, num_16b_writes}});
+            .compile_args = {base_l1_src_address, base_pcie_dst_address, 0, num_16b_writes}});
 
     distributed::EnqueueMeshWorkload(cq, workload, false);
 
@@ -447,7 +447,7 @@ TEST_F(UnitMeshCQSingleCardFixture, MeshL1ToPinnedMemoryAt16BAlignedAddress) {
         DataMovementConfig{
             .processor = DataMovementProcessor::RISCV_1,
             .noc = NOC::RISCV_0_default,
-            .compile_args = {base_l1_src_address, (uint32_t)pinned_memory_device_addr, num_16b_writes}});
+            .compile_args = {base_l1_src_address, (uint32_t)pinned_memory_device_addr, (uint32_t)(pinned_memory_device_addr >> 32), num_16b_writes}});
 
     // Create mesh workload and add program
     MeshWorkload mesh_workload = CreateMeshWorkload();
