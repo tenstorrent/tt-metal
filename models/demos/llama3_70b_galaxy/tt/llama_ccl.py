37b0f88f2195d59e89a6ce1e7c0a5d86afeb4b93
Miguel Tairum <150826086+mtairum@users.noreply.github.com>
2025-08-14 18:11:53 +0100

    Add barrier sync to LLama-3.3-70B Galaxy prefill CCLs (#26518)
    Issue: https://github.com/tenstorrent/tt-metal/issues/26423
    
    Changes introduced:
    - Added barrier synchronization to prefill CCLs
    - Revamped the repeated_batches config to rotate between prompts
    correctly (independently of batch size)
    - Added new evals config to `text_demo.py` to mimic a customers use
    case. These configs were added to CI
    - TTFT when running `repeat2` config in `text_demo.py` is measured at
    54.04 ms
    
    CI pipelines:
    - [x] APC:
    https://github.com/tenstorrent/tt-metal/actions/runs/16910882537
    - [x] Galaxy Demo:
    https://github.com/tenstorrent/tt-metal/actions/runs/16933142122
    - [x] 2nd Galaxy Demo:
    https://github.com/tenstorrent/tt-metal/actions/runs/16937775679
    - [x] TG Demo:
    https://github.com/tenstorrent/tt-metal/actions/runs/16933143944
    - [x] 2nd TG demo:
    https://github.com/tenstorrent/tt-metal/actions/runs/16937781247
    - This timed out I've since reduced the configs so it avoids timing out
    when merged.

--- models/demos/llama3_70b_galaxy/tt/llama_ccl.py
+++ models/demos/llama3_70b_galaxy/tt/llama_ccl.py
@@ -53,12 +53,18 @@ class TT_CCL:
 
         # Double buffered on each axis
         self.gather_semaphore_handles = [[], []]
+        self.barrier_semaphore_handles = []
         if mode == "prefill":
             self.from_semaphore_handles = [[], []]
             self.to_semaphore_handles = [[], []]
             self.reduce_semaphore_handles = [[], []]
+
         for i in range(2):
             for _ in range(self.num_cbs):
+                self.barrier_semaphore_handles.append(
+                    ttnn.create_global_semaphore(self.mesh_device, self.sub_device_crs, 0)
+                )
+
                 if self.use_ring_ag_prefill:
                     self.gather_semaphore_handles[i].append(
                         [ttnn.create_global_semaphore(self.mesh_device, self.sub_device_crs, 0) for _ in range(2)]
@@ -86,6 +92,7 @@ class TT_CCL:
 
         self.gather_idx = [0, 0]
         self.reduce_scatter_buffer_idx = [0, 0]
+        self.barrier_semaphore_idx = 0
         self.persistent_buffers = {}
         self.all_gather_buffers = {}
         if mode == "decode":
@@ -94,7 +101,8 @@ class TT_CCL:
             self.reduce_scatter_buffers = self.get_decode_reduce_scatter_buffers()
             self.rs_create_heads_buffers = self.get_decode_rs_create_heads_buffers()
         if mode == "prefill":
-            self.support_seqlens = [8192, 4096, 1024, 2048, 128]
+            # For some prefill seqlens we always allocate CCL buffers. Otherwise they will require barrier syncing
+            self.support_seqlens = [8192, 4096, 2048, 1024, 128]
             if allocate_prefill_buffers:
                 self.persistent_buffers = (
                     self.get_ring_prefill_reduce_scatter_buffers()
@@ -110,6 +118,12 @@ class TT_CCL:
     def reset_gather_and_buffer_idx(self):
         self.gather_idx = [0, 0]
         self.reduce_scatter_buffer_idx = [0, 0]
+        self.barrier_semaphore_idx = 0
+
+    def get_and_cycle_barrier_semaphore_handle(self):
+        current_idx = self.barrier_semaphore_idx
+        self.barrier_semaphore_idx = (self.barrier_semaphore_idx + 1) % (2 + self.num_cbs)
+        return self.barrier_semaphore_handles[current_idx]
 
     def get_all_gather_concat_inter_buffer(self):
         intermediate_core_range_set = ttnn.CoreRangeSet(
@@ -886,6 +900,7 @@ class TT_CCL:
             persistent_output_buffers=persistent_buffers_list,
             dim=dim,
             multi_device_global_semaphore=self.reduce_semaphore_handles[cluster_axis][self.gather_idx[cluster_axis]],
+            barrier_semaphore=self.get_and_cycle_barrier_semaphore_handle(),
             num_links=num_links,
             memory_config=memory_config,
             topology=ttnn.Topology.Ring,
@@ -993,6 +1008,7 @@ class TT_CCL:
             dim=dim,
             multi_device_global_semaphore=self.gather_semaphore_handles[cluster_axis][self.gather_idx[cluster_axis]],
             num_links=num_links,
+            barrier_semaphore=self.get_and_cycle_barrier_semaphore_handle(),
             memory_config=memory_config,
             topology=ttnn.Topology.Ring,
             subdevice_id=self.worker_sub_device_id,
