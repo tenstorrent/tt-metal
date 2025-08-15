0689b14ca6da5d60ee11d2ccaea874c7d05ad2a9
avoraTT <avora@tenstorrent.com>
2025-08-15 15:11:23 +0000

    Adding attention_sink param to sdpa_decode. Next: integrate.

--- ttnn/cpp/ttnn/operations/transformer/sdpa_decode/device/sdpa_decode_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/transformer/sdpa_decode/device/sdpa_decode_program_factory.cpp
@@ -29,6 +29,7 @@ operation::ProgramWithCallbacks sdpa_decode_multi_core(
     std::optional<const Tensor> cur_pos_tensor,
     std::optional<const Tensor> page_table_tensor,
     std::optional<const Tensor> attn_mask,
+    std::optional<const Tensor> attention_sink,
     const Tensor& output_tensor,
     bool is_causal,
     const std::vector<uint32_t>& cur_pos_ids,
