1cb314054f1e0c08b673948584d4ba3644892a51
Diego Gomez <dgomez@tenstorrent.com>
2025-08-15 10:22:15 -0400

    removed a function we don't need anymore

--- ttnn/api/ttnn/tensor/serialization.hpp
+++ ttnn/api/ttnn/tensor/serialization.hpp
@@ -14,7 +14,6 @@ namespace tt::tt_metal {
 void dump_tensor(const std::string& file_name, const Tensor& tensor);
 
 Tensor load_tensor(const std::string& file_name, distributed::MeshDevice* device = nullptr);
-bool is_tensor_empty(const Tensor& tensor);
 
 void dump_memory_config(FILE* output_file, const MemoryConfig& memory_config);
 void dump_memory_config(const std::string& file_name, const MemoryConfig& memory_config);
