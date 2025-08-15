ad7e3809537805ee22cd0ff21e958dd7c64e76bb
Stanislav Minakov <sminakov@tenstorrent.com>
2025-08-15 00:54:46 -0700

    layernorm_distributed

--- ttnn/cpp/ttnn/operations/normalization/layernorm_distributed/device/kernels/dataflow/reader_unary_interleaved_ln_rm_gb_pre_allgather.cpp
+++ ttnn/cpp/ttnn/operations/normalization/layernorm_distributed/device/kernels/dataflow/reader_unary_interleaved_ln_rm_gb_pre_allgather.cpp
@@ -23,13 +23,11 @@ void kernel_main() {
 
     // ublocks size defined in tiles
     const uint32_t src0_tile_bytes = get_tile_size(cb_inp);
-    const DataFormat src0_data_format = get_dataformat(cb_inp);
 
-    constexpr bool src0_is_dram = get_compile_time_arg_val(0) == 1;
-    constexpr uint32_t blk = get_compile_time_arg_val(1);
+    constexpr uint32_t blk = get_compile_time_arg_val(0);
+    constexpr auto src_args = TensorAccessorArgs<1>();
 
-    const InterleavedAddrGenFast<src0_is_dram> src_a = {
-        .bank_base_address = src_addr, .page_size = src0_tile_bytes, .data_format = src0_data_format};
+    const auto src_a = TensorAccessor(src_args, src_addr, src0_tile_bytes);
 
     // Generate constant tiles for reduce scalar
     uint32_t scaler = get_arg_val<uint32_t>(4);
