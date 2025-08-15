dedb2d0fead438d079fd94629e3e4640f74ad286
Colman Glagovich <114512306+cglagovichTT@users.noreply.github.com>
2025-08-15 15:22:18 -0400

    TT-DiT refactor of SD3.5 (#26874)
    This PR implements v0 of our TT-DiT library, with SD3.5 as the first
    model to be supported.
    Compared to the [previous
    implementation](https://github.com/tenstorrent/tt-metal/tree/main/models/experimental/stable_diffusion_35_large)
    of SD3.5, we've made some big changes following a new philosophy to make
    TT-DiT easily extended, make model bringup fast, and embed parallelism
    directly into a model implementation. We took inspiration from HF
    diffusers, xDiT, and fairscale.
    - simple, reusable layers are separate from the model implementation and
    tested independently (in the style of diffusers)
    - model-specific modules compose layers
    - layers are optionally, explicitly parallel, given the minimum amount
    of information necessary to fracture themselves (in the style of
    fairscale)
    - CCL-specific parameters (semaphores, links, topology, persistent
    buffers) are managed by a `CCLManager`, instantiated on each submesh
    - DiT parallelism is configured by a `DiTParallelConfig` object, which a
    model consumes to determine how its layers will be parallelized
    - pipelines are model-specific and therefore implemented per-model
    
    
    SD3.5 is the first model to be brought up in TT-DiT. This PR refactors
    the denoising transformer. Soon after, we will merge a refactored VAE,
    CLIP, and T5.
    We will then deprecate the old SD3.5 implementation and update CI tests
    to point to TT-DiT.

---
+++ models/experimental/tt_dit/utils/tensor.py
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+import ttnn
+
+
+def bf16_tensor(
+    x: torch.Tensor, device: ttnn.Device | None = None, mesh_axis=None, shard_dim=None, layout=ttnn.TILE_LAYOUT
+) -> ttnn.Tensor:
+    assert (mesh_axis is None) == (shard_dim is None)
+    mesh_mapper = None
+    if mesh_axis is not None:
+        mapper_dims = [None, None]
+        mapper_dims[mesh_axis] = shard_dim
+        mesh_mapper = ttnn.ShardTensor2dMesh(device, mesh_shape=tuple(device.shape), dims=mapper_dims)
+
+    return ttnn.from_torch(
+        x,
+        layout=layout,
+        dtype=ttnn.bfloat16,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        device=device,
+        mesh_mapper=mesh_mapper,
+    )
+
+
+def bf16_tensor_2dshard(x: torch.Tensor, device: ttnn.Device, shard_mapping: dict[int, int]) -> ttnn.Tensor:
+    assert len(shard_mapping) == 2
+    assert all(0 <= k <= 1 and 0 <= v < len(x.shape) for k, v in shard_mapping.items())
+    mapper_dims = [None, None]
+    for k, v in shard_mapping.items():
+        mapper_dims[k] = v
+    mesh_mapper = ttnn.ShardTensor2dMesh(device, mesh_shape=tuple(device.shape), dims=mapper_dims)
+    return ttnn.from_torch(
+        x,
+        layout=ttnn.TILE_LAYOUT,
+        dtype=ttnn.bfloat16,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        device=device,
+        mesh_mapper=mesh_mapper,
+    )
