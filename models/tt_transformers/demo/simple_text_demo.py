3393918c62379774577e29499a7f3f188d3896ca
Ubuntu <ioannis_alexiou@epam.com>
2025-08-18 13:16:57 +0000

    simple demo tg edit

--- models/tt_transformers/demo/simple_text_demo.py
+++ models/tt_transformers/demo/simple_text_demo.py
@@ -618,10 +618,10 @@ def test_demo_text(
 
         tg_enabled = (data_parallel == 4 and is_33_70b) or (data_parallel in [4, 16, 32] and is_31_8b)
 
-        if num_devices == 32 and not tg_enabled:
-            pytest.skip("CI only runs Llama3 70b DP = 4, TP = 8 or Llama3 8b DP = 4/16/32, TP = 8/2/1 on TG")
-        if num_devices == 8 and data_parallel > 1 and not (is_32_1b or is_31_8b):
-            pytest.skip("CI only runs hybrid Llama3 1b and 8b on T3K")
+        # if num_devices == 32 and not tg_enabled:
+        #     pytest.skip("CI only runs Llama3 70b DP = 4, TP = 8 or Llama3 8b DP = 4/16/32, TP = 8/2/1 on TG")
+        # if num_devices == 8 and data_parallel > 1 and not (is_32_1b or is_31_8b):
+        #     pytest.skip("CI only runs hybrid Llama3 1b and 8b on T3K")
 
     if not stop_at_eos:
         logger.info(f"The decode generation will only stop at the max_generated_tokens limit == {max_generated_tokens}")
