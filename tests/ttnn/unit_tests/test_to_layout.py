51249e1c0a2ba4106ec8ca007e8b99e0913d1e5a
Nour Ardo <nardo@tenstorrent.com>
2025-08-18 10:09:59 -0400

    Fix alignment bug in untilize with unpadding (#26892)
    ### Ticket
    Link to Github Issue
    https://github.com/tenstorrent/tt-metal/issues/22606
    
    ### Problem description
    to_layout_op is causing PCC failures in sigmoid operation
    
    ### What's changed
    The PCC failures is due to an alignment issue in the sharded kernels of
    untilize with unpadding. This PR fixes the bug
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/16997797897
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/16990700039
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
    CI passes (if applicable)
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- tests/ttnn/unit_tests/test_to_layout.py
+++ tests/ttnn/unit_tests/test_to_layout.py
@@ -456,3 +456,61 @@ def test_untilize_int32_t(shape, dtype, device):
     output_tensor = ttnn.untilize(input_tensor)
     output_tensor = ttnn.to_torch(output_tensor)
     assert_with_pcc(input_a, output_tensor)
+
+
+def run_unary_with_aprox_mode_fruit_test(
+    device, h, w, memory_type, shard_shape, ttnn_function, vector_mode, approx_mode, pcc=0.9999
+):
+    torch.manual_seed(0)
+
+    torch_input_tensor = torch.rand((1, 1, h, w), dtype=torch.bfloat16)
+    golden_function = ttnn.get_golden_function(ttnn_function)
+    torch_output_tensor = golden_function(torch_input_tensor, device=device)
+    if memory_type == "L1":
+        core_grid = device.compute_with_storage_grid_size()
+        num_cores = 64
+
+        shard_spec = ttnn.ShardSpec(
+            grid=ttnn.num_cores_to_corerangeset(num_cores, device.compute_with_storage_grid_size()),
+            shard_shape=shard_shape,
+            shard_orientation=ttnn.ShardOrientation.ROW_MAJOR,
+        )
+        memory_config = ttnn.MemoryConfig(
+            memory_layout=ttnn.TensorMemoryLayout.HEIGHT_SHARDED, buffer_type=ttnn.BufferType.L1, shard_spec=shard_spec
+        )
+    else:  # memory_type == "DRAM":
+        memory_config = ttnn.MemoryConfig(
+            memory_layout=ttnn.TensorMemoryLayout.INTERLEAVED, buffer_type=ttnn.BufferType.DRAM
+        )
+    input_tensor = ttnn.from_torch(
+        torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device, memory_config=memory_config
+    )
+    print(
+        f"shape: {input_tensor.shape}, dtype: {input_tensor.dtype}, layout: {input_tensor.layout}, memory_config: {input_tensor.memory_config()}"
+    )
+    output_tensor = ttnn_function(input_tensor, vector_mode=vector_mode, fast_and_approximate_mode=approx_mode)
+    output_tensor = ttnn.to_layout(output_tensor, ttnn.ROW_MAJOR_LAYOUT)
+    output_tensor = ttnn.from_device(output_tensor)
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_with_pcc(torch_output_tensor, output_tensor, pcc)
+
+
+@pytest.mark.parametrize("h", [1024 * 128])
+@pytest.mark.parametrize("w", [1])
+@pytest.mark.parametrize("memory_type", ["L1", "DRAM"])
+@pytest.mark.parametrize("shard_shape", [(2048, 32)])
+@pytest.mark.parametrize("approx_mode", [True, False])
+@pytest.mark.parametrize("vector_mode", [4])
+def test_sigmoid_fruit(device, h, w, memory_type, shard_shape, vector_mode, approx_mode):
+    run_unary_with_aprox_mode_fruit_test(
+        device,
+        h,
+        w,
+        memory_type,
+        shard_shape,
+        ttnn.sigmoid,
+        vector_mode=vector_mode,
+        approx_mode=approx_mode,
+        pcc=0.999,
+    )
