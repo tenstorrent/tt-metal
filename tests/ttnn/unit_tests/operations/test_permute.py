6f96f709b154d746e3327c511f5bf0f36d82c46d
Jason Davies <jason@jasondavies.com>
2025-08-18 18:40:56 +0100

    Revert "Fix."
    This reverts commit 0a7c6e6d7aed43f7ed39c6b71a7a48a5738acef8.

--- tests/ttnn/unit_tests/operations/test_permute.py
+++ tests/ttnn/unit_tests/operations/test_permute.py
@@ -333,7 +333,7 @@ def test_nil_volume_permute(device, dtype):
     output_tensor = ttnn.permute(input_tensor, (0, 1, 3, 2))
     output_tensor = ttnn.to_torch(output_tensor)
     torch_output = torch.permute(torch_tensor, (0, 1, 3, 2))
-    assert_equal(torch_output, output_tensor)
+    assert torch_output.shape == output_tensor.shape
 
 
 @pytest.mark.parametrize("dtype", [ttnn.bfloat16, ttnn.int32])
