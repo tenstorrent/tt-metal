58276545d54da4f70a9049f450e23645060f36dd
Bart Trzynadlowski <btrzynadlowski@tenstorrent.com>
2025-08-18 14:45:51 +0000

    Reorg

--- tt_telemetry/include/telemetry/telemetry_provider.hpp
+++ tt_telemetry/include/telemetry/telemetry_provider.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
 /*
- * server/telemetry_provider.hpp
+ * telemetry/telemetry_provider.hpp
  *
  * Polls telemetry data on a periodic loop and sends to subscribers.
  */
 
 #include <vector>
 
-#include <server/telemetry_subscriber.hpp>
+#include <telemetry/telemetry_subscriber.hpp>
 
 void run_telemetry_provider(std::vector<std::shared_ptr<TelemetrySubscriber>> subscribers);
