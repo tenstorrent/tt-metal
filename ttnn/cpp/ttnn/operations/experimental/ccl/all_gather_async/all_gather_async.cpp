ff62c3bcf216c5b88b02c983c0855b53509e9190
Grayson Rechsteiner <grechsteiner@tenstorrent.com>
2025-08-18 15:58:13 -0400

    Use Composite-AG when Gathering on Outer Dims and Outer Dims are Non-One (#26989)
    ### Problem description
    - The native implementation of `all_gather_async` gives bad PCC when
    gathering on dim 0 or dim 1, and the input shape at dim 0 or dim 1 is
    non-one
    
    ### What's changed
    - Route these cases to the composite-AG implementation, which does
    handle these shape - gather-dim combos
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/17030051606
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/17030053210
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
    CI passes (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/17030056103
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    https://github.com/tenstorrent/tt-metal/actions/runs/17030057598
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    https://github.com/tenstorrent/tt-metal/actions/runs/17030059253
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    https://github.com/tenstorrent/tt-metal/actions/runs/17041933873
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes
    - [ ] T3K Nightly
    https://github.com/tenstorrent/tt-metal/actions/runs/17030065177
    - [ ] TG Nightly
    https://github.com/tenstorrent/tt-metal/actions/runs/17030066355

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/all_gather_async.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/all_gather_async.cpp
@@ -19,6 +19,8 @@ bool use_composite_all_gather(const ttnn::Tensor& input_tensor, const int32_t di
     uint32_t tile_height = tile_shape[0];
     uint32_t tile_width = tile_shape[1];
 
+    auto input_shape = input_tensor.logical_shape();
+
     int32_t rank = input_tensor.logical_shape().rank();
     int32_t gather_dim = (dim < 0) ? rank + dim : dim;
 
@@ -28,16 +30,21 @@ bool use_composite_all_gather(const ttnn::Tensor& input_tensor, const int32_t di
     }
 
     // Use composite if tiled and padded on the gather dim
-    auto input_shape = input_tensor.logical_shape();
-    if ((gather_dim == 2 && input_shape[2] % tile_height != 0) ||
-        (gather_dim == 3 && input_shape[3] % tile_width != 0)) {
+    bool is_tiled_and_padded_on_gather_dim =
+        input_tensor.layout() == Layout::TILE && ((gather_dim == 2 && input_shape[2] % tile_height != 0) ||
+                                                  (gather_dim == 3 && input_shape[3] % tile_width != 0));
+    if (is_tiled_and_padded_on_gather_dim) {
+        return true;
+    }
+
+    // Use composite if gathering on dim 0 or dim 1, and input_shape[0] != 1 or input_shape[1] != 1
+    if ((gather_dim == 0 || gather_dim == 1) && (input_shape[0] != 1 || input_shape[1] != 1)) {
         return true;
     }
 
     return false;
 }
 
-// Composite always runs in row-major
 ttnn::Tensor composite_all_gather(
     ttnn::Tensor input_tensor,
     const int32_t dim,
@@ -45,15 +52,25 @@ ttnn::Tensor composite_all_gather(
     const std::optional<ttnn::MemoryConfig>& memory_config,
     std::optional<tt::tt_metal::SubDeviceId> subdevice_id,
     std::optional<uint32_t> cluster_axis) {
-    DataType input_dtype = input_tensor.dtype();
-    bool convert_to_bfloat16_for_composite = input_dtype == DataType::BFLOAT8_B;
-    bool is_tiled = input_tensor.layout() == Layout::TILE;
+    auto tile_shape = input_tensor.tensor_spec().tile().get_tile_shape();
+    uint32_t tile_height = tile_shape[0];
+    uint32_t tile_width = tile_shape[1];
+
+    auto input_shape = input_tensor.logical_shape();
 
     int32_t rank = input_tensor.logical_shape().rank();
     int32_t gather_dim = (dim < 0) ? rank + dim : dim;
 
+    bool is_tiled_and_not_tile_aligned = input_tensor.layout() == Layout::TILE &&
+                                         (input_shape[2] % tile_height != 0 || input_shape[3] % tile_width != 0);
+
+    // If we need to convert to row-major, then if the input dtype is bfloat8_b we need to typecast before untilizing
+    // and after re-tilizing
+    DataType input_dtype = input_tensor.dtype();
+    bool convert_to_bfloat16_for_composite = is_tiled_and_not_tile_aligned && input_dtype == DataType::BFLOAT8_B;
+
     // Convert to row major
-    if (is_tiled) {
+    if (is_tiled_and_not_tile_aligned) {
         // If input is tiled bfloat8_b, convert to bfloat16 to do the all_broadcast_async + concat
         if (convert_to_bfloat16_for_composite) {
             input_tensor = ttnn::typecast(input_tensor, DataType::BFLOAT16);
@@ -67,7 +84,7 @@ ttnn::Tensor composite_all_gather(
     ttnn::Tensor all_gather_output_tensor = ttnn::concat(broadcasted_tensors, gather_dim);
 
     // Convert back to tiled
-    if (is_tiled) {
+    if (is_tiled_and_not_tile_aligned) {
         all_gather_output_tensor = ttnn::to_layout(all_gather_output_tensor, Layout::TILE);
         // If we had to convert the input dtype in order to execute the row-major composite op, convert back to the
         // input dtype
