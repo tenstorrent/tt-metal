64aa4d2f69c6466e85226713721ef1c52b522907
Roman Furko <rfurko@tenstorrent.com>
2025-08-19 16:47:36 -0700

    [tt-train] Switch Legacy CCLs to Fabric  (#27102)
    ### Problem description
    Legacy CCLs are getting deprecated, we need to switch to FABRIC based
    CCLs.
    
    ### What's changed
    Used reduce_scatter vs scatter.
    Add CCLResources to handle new parameters that are required for fabric
    CCLs.
    Adjusted a few tests and examples according to reduce_scatter. 
    Fixed a few bugs with forward + backward connection. 
    
    Testing 
    
    * ddp training works
    * tensor parallel training works
    * all tests now pass
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/17081072197)
    CI passes
    - [x] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: asaigal <asaigal@tenstorrent.com>
    Co-authored-by: Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
    Co-authored-by: grechsteinerTT <grechsteiner@tenstorrent.com>
    Co-authored-by: vmelnykovTT <vmelnykov@tenstorrent.com>

--- tt-train/sources/ttml/autograd/auto_context.cpp
+++ tt-train/sources/ttml/autograd/auto_context.cpp
@@ -111,4 +111,11 @@ core::TTProfiler& AutoContext::get_profiler() {
     return *m_profiler;
 }
 
+[[nodiscard]] core::distributed::CCLResources& AutoContext::get_ccl_resources() {
+    if (!m_ccl_resources) {
+        m_ccl_resources = std::make_unique<core::distributed::CCLResources>();
+    }
+    return *m_ccl_resources;
+}
+
 }  // namespace ttml::autograd
