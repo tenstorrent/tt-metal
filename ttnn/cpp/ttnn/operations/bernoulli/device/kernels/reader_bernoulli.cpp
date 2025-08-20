989bd0d53a5230281dccc72ee78ca7a3d981b5d0
Andy Wei Liu <awliu@tenstorrent.com>
2025-08-20 12:12:37 -0700

    Refactoring Bernoulli Operation to use TensorAccessor (#27033)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/25975
    
    ### Problem description
    We need to migrate all kernels to use TensorAccessor instead of
    InterleavedAddrGen to make them support ND sharding. This is the first
    step of adding universal sharding support to all OPs
    ### What's changed
    Refactored the Bernoulli program_factory, reader kernel, and writer
    kernel to use TensorAccessor.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/17050533046)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/17050559703)
    CI with demo tests passes
    - [x] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/bernoulli/device/kernels/reader_bernoulli.cpp
+++ ttnn/cpp/ttnn/operations/bernoulli/device/kernels/reader_bernoulli.cpp
@@ -6,15 +6,15 @@
 
 void kernel_main() {
     constexpr uint32_t in_cb_id = get_compile_time_arg_val(0);
-    constexpr bool input_is_dram = get_compile_time_arg_val(1) == 1;
 
     uint32_t input_addr = get_arg_val<uint32_t>(0);
     uint32_t start_id = get_arg_val<uint32_t>(1);
     uint32_t num_tiles = get_arg_val<uint32_t>(2);
     uint32_t end_id = start_id + num_tiles;
 
-    const InterleavedAddrGenFast<input_is_dram> input_addrg = {
-        .bank_base_address = input_addr, .page_size = get_tile_size(in_cb_id), .data_format = get_dataformat(in_cb_id)};
+    const uint32_t tile_size_bytes = get_tile_size(in_cb_id);
+    constexpr auto input_args = TensorAccessorArgs<1>();
+    const auto input_addrg = TensorAccessor(input_args, input_addr, tile_size_bytes);
 
     for (uint32_t i = start_id; i < end_id; ++i) {
         cb_reserve_back(in_cb_id, 1);
