4aabd55c8543e8fad3329b24adc372aecd89c586
River Wu <riverwu@tenstorrent.com>
2025-08-20 23:41:58 +0000

    cleanup

--- tests/tt_metal/tt_metal/test_kernels/misc/circular_buffer/cb_wrapping_test_blocking_reader.cpp
+++ tests/tt_metal/tt_metal/test_kernels/misc/circular_buffer/cb_wrapping_test_blocking_reader.cpp
@@ -3,23 +3,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include <cstdint>
-#include "circular_buffer.h"
-#include "debug/debug.h"
 #include "dataflow_api.h"
 #include "debug/dprint.h"
 
 using namespace tt;
 
-static constexpr auto CB_ID = tt::CBIndex::c_0;
+static constexpr auto CB_ID = CBIndex::c_0;
 static constexpr std::size_t CB_STEP_SIZE = 32;
 
 using DataT = std::uint32_t;
-
-static constexpr std::size_t PAGE_SIZE_BYTES = 16;
-static constexpr std::size_t PAGE_SIZE = PAGE_SIZE_BYTES / sizeof(DataT);
-
-// A span would be more appropriate here, but we are currently in C++17 land.
-using page_t = std::array<DataT, PAGE_SIZE>;
 
 static constexpr std::size_t CHURN_TARGET = (0x10000 - 2 * CB_STEP_SIZE);
 static constexpr std::size_t CHURN_LOOP_COUNT = CHURN_TARGET / CB_STEP_SIZE;
@@ -27,16 +19,14 @@ static constexpr std::size_t CHURN_LOOP_COUNT = CHURN_TARGET / CB_STEP_SIZE;
 // This should be enough spining time for the writer to fill the CB with 2 steps of data.
 static constexpr std::size_t NUM_WAIT_CYCLES = 1024 * 1024;
 
-page_t read_page() {
+void report_page(std::size_t i) {
+    invalidate_l1_cache();
+    auto result_ptr = get_arg_val<DataT*>(0);
     auto read_ptr = reinterpret_cast<DataT*>(get_read_ptr(CB_ID));
-    page_t result;
-    std::copy(read_ptr, read_ptr + PAGE_SIZE, result.begin());
-    return result;
+    result_ptr[i] = read_ptr[0];
 }
 
 void kernel_main() {
-    auto result_ptr = get_arg_val<DataT*>(0);
-
     for (auto i = 0ul; i < CHURN_LOOP_COUNT; i++) {
         cb_wait_front(CB_ID, CB_STEP_SIZE);
         cb_pop_front(CB_ID, CB_STEP_SIZE);
@@ -48,8 +38,7 @@ void kernel_main() {
 
     for (auto i = 0ul; i < 3; i++) {
         cb_wait_front(CB_ID, CB_STEP_SIZE);
-        invalidate_l1_cache();
-        result_ptr[i] = read_page()[0];
+        report_page(i);
         cb_pop_front(CB_ID, CB_STEP_SIZE);
     }
 }
