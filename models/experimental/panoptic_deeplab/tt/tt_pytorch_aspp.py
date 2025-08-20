0d6701a92292ee233f73432213c049762fa0ee80
Luka Zdravic <lzdravic@tenstorrent.com>
2025-08-20 16:18:19 +0000

    SemanticSegmentation Pytorch implementation

--- models/experimental/panoptic_deeplab/tt/tt_pytorch_aspp.py
+++ models/experimental/panoptic_deeplab/tt/tt_pytorch_aspp.py
@@ -2,67 +2,7 @@ from copy import deepcopy
 import torch
 from torch import nn
 from torch.nn import functional as F
-
-import warnings
-
-TORCH_VERSION = tuple(int(x) for x in torch.__version__.split(".")[:2])
-
-
-def check_if_dynamo_compiling():
-    if TORCH_VERSION >= (2, 1):
-        from torch._dynamo import is_compiling
-
-        return is_compiling()
-    else:
-        return False
-
-
-class Conv2d(torch.nn.Conv2d):
-    """
-    A wrapper around :class:`torch.nn.Conv2d` to support empty inputs and more features.
-    """
-
-    def __init__(self, *args, **kwargs):
-        """
-        Extra keyword arguments supported in addition to those in `torch.nn.Conv2d`:
-
-        Args:
-            norm (nn.Module, optional): a normalization layer
-            activation (callable(Tensor) -> Tensor): a callable activation function
-
-        It assumes that norm layer is used before activation.
-        """
-        norm = kwargs.pop("norm", None)
-        activation = kwargs.pop("activation", None)
-        super().__init__(*args, **kwargs)
-
-        self.norm = norm
-        self.activation = activation
-
-    def forward(self, x):
-        # torchscript does not support SyncBatchNorm yet
-        # https://github.com/pytorch/pytorch/issues/40507
-        # and we skip these codes in torchscript since:
-        # 1. currently we only support torchscript in evaluation mode
-        # 2. features needed by exporting module to torchscript are added in PyTorch 1.6 or
-        # later version, `Conv2d` in these PyTorch versions has already supported empty inputs.
-        if not torch.jit.is_scripting():
-            # Dynamo doesn't support context managers yet
-            is_dynamo_compiling = check_if_dynamo_compiling()
-            if not is_dynamo_compiling:
-                with warnings.catch_warnings(record=True):
-                    if x.numel() == 0 and self.training:
-                        # https://github.com/pytorch/pytorch/issues/12013
-                        assert not isinstance(
-                            self.norm, torch.nn.SyncBatchNorm
-                        ), "SyncBatchNorm does not support empty inputs!"
-
-        x = F.conv2d(x, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
-        if self.norm is not None:
-            x = self.norm(x)
-        if self.activation is not None:
-            x = self.activation(x)
-        return x
+from .pytorch_conv2dWrapper import Conv2d
 
 
 class LayerNorm(nn.Module):
