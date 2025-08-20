32ab2bf1b6041bb6a46c8da0fbd29a97268568ef
sadesoyeTT <sadesoye@tenstorrent.com>
2025-08-20 00:17:30 +0000

    fix hang

--- models/experimental/tt_dit/utils/tensor.py
+++ models/experimental/tt_dit/utils/tensor.py
@@ -26,6 +26,24 @@ def bf16_tensor(
     )
 
 
+def bf16_tensor_host(
+    x: torch.Tensor, device: ttnn.Device | None = None, mesh_axis=None, shard_dim=None, layout=ttnn.TILE_LAYOUT
+) -> ttnn.Tensor:
+    assert (mesh_axis is None) == (shard_dim is None)
+    mesh_mapper = None
+    if mesh_axis is not None:
+        mapper_dims = [None, None]
+        mapper_dims[mesh_axis] = shard_dim
+        mesh_mapper = ttnn.ShardTensor2dMesh(device, mesh_shape=tuple(device.shape), dims=mapper_dims)
+
+    return ttnn.from_torch(
+        x,
+        layout=layout,
+        dtype=ttnn.bfloat16,
+        mesh_mapper=mesh_mapper,
+    )
+
+
 def bf16_tensor_2dshard(x: torch.Tensor, device: ttnn.Device, shard_mapping: dict[int, int]) -> ttnn.Tensor:
     assert len(shard_mapping) == 2
     assert all(0 <= k <= 1 and 0 <= v < len(x.shape) for k, v in shard_mapping.items())
