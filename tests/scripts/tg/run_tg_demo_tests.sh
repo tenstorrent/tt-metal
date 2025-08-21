893de6408c56c2a90758031d3133614a6a4112ad
mtairum <mtairum@tenstorrent.com>
2025-08-21 15:15:14 +0000

    Revert "[REVERT] Comment tests"
    This reverts commit eaccd0f5d099b303068b584dd961c922596140d1.

--- tests/scripts/tg/run_tg_demo_tests.sh
+++ tests/scripts/tg/run_tg_demo_tests.sh
@@ -11,9 +11,9 @@ run_tg_llama3_tests() {
   llama70b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.3-70B-Instruct/
 
   for llama_dir in "$llama70b"; do
-    # LLAMA_DIR=$llama_dir FAKE_DEVICE=TG pytest -n auto models/demos/llama3_70b_galaxy/demo/demo_decode.py -k "full" --timeout 1000; fail+=$?;
+    LLAMA_DIR=$llama_dir FAKE_DEVICE=TG pytest -n auto models/demos/llama3_70b_galaxy/demo/demo_decode.py -k "full" --timeout 1000; fail+=$?;
     LLAMA_DIR=$llama_dir FAKE_DEVICE=TG pytest -n auto models/demos/llama3_70b_galaxy/demo/text_demo.py -k "repeat" --timeout 1000; fail+=$?;
-    # LLAMA_DIR=$llama_dir FAKE_DEVICE=TG pytest -n auto models/demos/llama3_70b_galaxy/demo/text_demo.py -k "pcc-80L" --timeout 1000; fail+=$?;
+    LLAMA_DIR=$llama_dir FAKE_DEVICE=TG pytest -n auto models/demos/llama3_70b_galaxy/demo/text_demo.py -k "pcc-80L" --timeout 1000; fail+=$?;
 
     echo "LOG_METAL: Llama3 tests for $llama_dir completed"
   done
