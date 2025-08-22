73a2b55b6f058026499f1790459332ff0523dfcf
dpopovTT <dpopov@tenstorrent.com>
2025-08-22 11:35:07 +0000

    Revert "Only use --build-all tag for building metal"
    This reverts commit f3ad563117be6c509b89401a4932ff427953eea7.

--- tests/scripts/tt_bisect.sh
+++ tests/scripts/tt_bisect.sh
@@ -99,7 +99,8 @@ while [[ "$found" = "false" ]]; do
    rm -rf .cpmcache  build_Release build_Debug build
 
    build_rc=0
-   ./build_metal.sh --build-all --enable-ccache || build_rc=$?
+   ./build_metal.sh --build-dir build --build-type Release --toolchain-path cmake/x86_64-linux-clang-17-libstdcpp-toolchain.cmake --build-all --enable-ccache --configure-only || build_rc=$?
+   cmake --build build --target install || build_rc=$?
    echo "::endgroup::"
 
    if [[ $build_rc -ne 0 ]]; then
