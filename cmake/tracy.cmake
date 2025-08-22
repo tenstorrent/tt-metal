a95ff3282618b92258bd9a23023bc318564fea28
Aliaksei Sala <aliaksei_sala@epam.com>
2025-08-22 12:06:26 -0700

    Define TRACY_TIMER_FALLBACK if TRACY is disabled to fix CI

--- cmake/tracy.cmake
+++ cmake/tracy.cmake
@@ -25,6 +25,7 @@ target_compile_definitions(
     TracyClient
     PUBLIC
         TRACY_ENABLE
+        "$<$<NOT:$<BOOL:${ENABLE_TRACY}>>:TRACY_NO_INVARIANT_CHECK>"
         "$<$<BOOL:${ENABLE_TRACY_TIMER_FALLBACK}>:TRACY_TIMER_FALLBACK>"
 )
 target_compile_options(
