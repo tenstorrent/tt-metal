35c9b25a788fc1b2bc07a83265c045c207e59b7b
Stanislav Minakov <sminakov@tenstorrent.com>
2025-08-22 22:09:55 +0000

    Port experimental OPs to TensorAccessor, part 1 (attempt #2) (#27330)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/25975
    
    ### Problem description
    We need to migrate all kernels to use TensorAccessor instead of
    InterleavedAddrGen to make them support ND sharding. This is the first
    step of adding universal sharding support to all OPs.
    
    ### What's changed
    This is the second attempt of the previously reverted PR
    https://github.com/tenstorrent/tt-metal/pull/27145.
    The only difference is that I removed the changes to paged_cache OP
    which were causing failures. It will be ported separately in a future
    PR.
    
    Refactored all of the kernels for the following experimental OPs to use
    TensorAccessor:
    - bcast_to
    - dropout
    - padded_slice
    - slice_write
    - ssm
    - where
    
    This PR was generated by AI: Cursor + GPT 5 Max using this updated
    [prompt](https://gist.github.com/sminakov-tt/0ee29006457ccee6aa9bdb534663cc46).
    
    ### Checklist
    - [x] [All post commit CI
    passes](https://github.com/tenstorrent/tt-metal/actions/runs/17108765423)
    - [x] [Blackhole Post commit CI with demo tests
    passes](https://github.com/tenstorrent/tt-metal/actions/runs/17108770593)
    - [x] [Blackhole nightly tests CI
    passes](https://github.com/tenstorrent/tt-metal/actions/runs/17109434156)
    - [x] [Nightly L2 conv and pool tests CI
    passes](https://github.com/tenstorrent/tt-metal/actions/runs/17109441516)
    - [x] [TG demo all-post-commit tests CI
    passes](https://github.com/tenstorrent/tt-metal/actions/runs/17166127549)
    - [x] [Galaxy Quick CI
    passes](https://github.com/tenstorrent/tt-metal/actions/runs/17166131662)
    - [x] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/experimental/bcast_to/device/kernels/dataflow/reader_interleaved_scalar_bcast_to.cpp
+++ ttnn/cpp/ttnn/operations/experimental/bcast_to/device/kernels/dataflow/reader_interleaved_scalar_bcast_to.cpp
@@ -22,14 +22,12 @@ void kernel_main() {
     uint32_t Ht = get_arg_val<uint32_t>(arg_index++);
     uint32_t Wt = get_arg_val<uint32_t>(arg_index++);
 
-    constexpr bool src_is_dram = get_compile_time_arg_val(0) == 1;
-    constexpr auto cb_id_src = get_compile_time_arg_val(1);
+    constexpr auto cb_id_src = get_compile_time_arg_val(0);
+    constexpr auto src_args = TensorAccessorArgs<1>();
     constexpr uint32_t onetile = 1;
 
     const uint32_t src_tile_bytes = get_tile_size(cb_id_src);
-    const DataFormat src_data_format = get_dataformat(cb_id_src);
-    const InterleavedAddrGenFast<src_is_dram> src = {
-        .bank_base_address = src_addr, .page_size = src_tile_bytes, .data_format = src_data_format};
+    const auto src = TensorAccessor(src_args, src_addr, src_tile_bytes);
 
     uint32_t HtWt = Ht * Wt;
     // this is the INPUT tile offset
