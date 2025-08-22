5fb5bb953d9bcd1d30f6acd4e6cad434756243c1
Adrian F. Morrison <amorrison@tenstorrent.com>
2025-08-22 09:17:40 -0700

    drop import *

--- tests/sweep_framework/framework/serialize.py
+++ tests/sweep_framework/framework/serialize.py
@@ -4,8 +4,10 @@
 
 import ttnn
 import json
+from framework.statuses import VectorValidity
 from tests.sweep_framework.framework.sweeps_logger import sweeps_logger as logger
 
+TTNN_NAME =ttnn.__name__
 
 def convert_enum_values_to_strings(data):
     """Convert enum integer values to human-readable strings for PostgreSQL storage."""
@@ -65,15 +67,47 @@ def serialize(object, warnings=[]):
         return str(object)
 
 
+def _ttnn_type_from_name(type_name):
+    
+    uq_type_name = type_name.split(".")[-1]
+    
+    the_type = None
+    try:
+        the_type = getattr(ttnn, uq_type_name)
+    except AttributeError as e:
+        logger.debug(f"Hopefully not an enum {e}")
+    
+    return the_type
+
+def _deserialize_ttnn_enum(obj_name:str):
+    
+    uq_obj_name_parts = list(filter(lambda p: p!= TTNN_NAME, obj_name.split(".")))
+    enum_type = _ttnn_type_from_name(uq_obj_name_parts[0])
+    
+    if enum_type is None:
+        return None
+    
+    return eval(f"enum_type.{uq_obj_name_parts[1]}")
+
+
 def deserialize(object):
-    if isinstance(object, dict):
-        type = eval(object["type"])
-        return type.from_json(object["data"])
-    else:
+    try:
+        if isinstance(object, dict):
+            type = _ttnn_type_from_name(object["type"])
+            return type.from_json(object["data"])
+        
+        elif isinstance(object,str) and "." in object:
+            maybe_enum = _deserialize_ttnn_enum(object)
+            if maybe_enum is not None:
+                return maybe_enum 
         try:
             return eval(object)
-        except:
+        except (SyntaxError,NameError):
             return str(object)
+    
+    except Exception as e:
+        logger.exception(f"deserialize failed {e}")
+        raise
 
 
 def serialize_structured(object, warnings=[]):
