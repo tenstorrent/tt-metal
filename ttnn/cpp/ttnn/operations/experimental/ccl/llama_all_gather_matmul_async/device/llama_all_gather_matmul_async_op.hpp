936c30c7145203d9e934625934eaf27f83091897
Ligang Long <llong@tenstorrent.com>
2025-08-25 11:53:14 -0700

    Llama AG + 1D ring MM fusion (#25438)
    ### Ticket
    [Link to Github
    Issue](https://docs.google.com/spreadsheets/d/1IBdiTGx18Lt-WqtJxP9Co9hcpG1D650zPvDKtTn_xZE/edit?gid=397681784#gid=397681784)
    
    ### Problem description
    AG + MM fusion, that can give us about 5us perf gain.
    
    ### What's changed
    Fusion of AG + 1D ring MM, break the 1D single chip ring MM into CCL
    cross-device ring MM. The data from local chip get in MM first and then
    data from other devices come in ring gather fashion.
    
    What it expects from Prefetcher is that block size is 6 time bigger(24
    blocks -> 4 blocks), no padding needed for each block(in1 is 3584x2048,
    3584/24 = 149.333, while 3584/4 = 896)
    
    unit test perf gain close to 5us in kernel time. 
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
    CI passes (if applicable)
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: avoraTT <avora@tenstorrent.com>
    Co-authored-by: sohaibnadeemTT <sohaibnadeem@tenstorrent.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ ttnn/cpp/ttnn/operations/experimental/ccl/llama_all_gather_matmul_async/device/llama_all_gather_matmul_async_op.hpp
@@ -0,0 +1,196 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <cstdint>
+#include <tt-metalium/core_coord.hpp>
+#include <tt-metalium/buffer.hpp>
+#include "ttnn/tensor/tensor.hpp"
+#include "ttnn/operations/ccl/shared_with_host/hetergeneous_data_structs.hpp"
+#include <tt-metalium/constants.hpp>
+#include "ttnn/operations/ccl/ccl_host_datastructures.hpp"
+#include "ttnn/operations/ccl/ccl_common.hpp"
+#include "ttnn/operations/ccl/ccl_op_fusion.hpp"
+#include <tt-metalium/global_semaphore.hpp>
+#include "ttnn/global_semaphore.hpp"
+#include "ttnn/operations/matmul/device/matmul_op.hpp"
+
+#include "ttnn/run_operation.hpp"
+
+#include <optional>
+#include <vector>
+
+namespace ttnn {
+
+using ccl::EriscDatamoverBuilder;
+struct AllGatherParams {
+    std::vector<IDevice*> devices;
+    const uint32_t dim;
+    const uint32_t num_links;
+    const uint32_t ring_size;
+    const MemoryConfig output_mem_config;
+    const ccl::Topology topology;
+    const GlobalSemaphore semaphore;
+    std::optional<tt::tt_metal::SubDeviceId> sub_device_id;
+    std::optional<uint32_t> cluster_axis;
+
+    AllGatherParams(
+        std::vector<IDevice*> devices,
+        uint32_t dim,
+        uint32_t num_links,
+        uint32_t ring_size,
+        MemoryConfig output_mem_config,
+        ccl::Topology topology,
+        GlobalSemaphore semaphore,
+        std::optional<tt::tt_metal::SubDeviceId>& sub_device_id,
+        std::optional<uint32_t> cluster_axis) :
+        devices(std::move(devices)),
+        dim(dim),
+        num_links(num_links),
+        ring_size(ring_size),
+        output_mem_config(output_mem_config),
+        topology(topology),
+        semaphore(semaphore),
+        sub_device_id(sub_device_id),
+        cluster_axis(cluster_axis) {}
+};
+
+struct LlamaAllGatherMatmulAsync {
+    /* All Gather Replicate Params */
+    const ttnn::AllGatherParams all_gather_params;
+
+    /* Matmul Params */
+    const operations::matmul::Matmul matmul_struct;
+
+    /* Physical Devices this op runs on*/
+    std::vector<IDevice*> devices;
+
+    /* General */
+    void validate_with_output_tensors(
+        const std::vector<Tensor>& input_tensors,
+        const std::vector<std::optional<const Tensor>>& optional_input_tensors,
+        const std::vector<std::optional<Tensor>>& output_tensors) const;
+    std::vector<ttnn::TensorSpec> compute_output_specs(const std::vector<Tensor>& input_tensors) const;
+    std::vector<Tensor> create_output_tensors(
+        const std::vector<Tensor>& input_tensors,
+        const std::vector<std::optional<Tensor>>& optional_output_tensors) const;
+
+    tt::tt_metal::operation::MeshWorkloadWithCallbacks create_mesh_workload(
+        const ttnn::MeshCoordinateRangeSet& tensor_coords,
+        const std::vector<Tensor>& input_tensors,
+        const std::vector<std::optional<const Tensor>>& optional_input_tensors,
+        std::vector<Tensor>& output_tensors) const;
+
+    tt::tt_metal::operation::ProgramWithCallbacks create_program_at(
+        const ttnn::MeshCoordinate& mesh_coordinate,
+        const std::vector<Tensor>& input_tensors,
+        const std::vector<std::optional<const Tensor>>& optional_input_tensors,
+        std::vector<Tensor>& output_tensors) const;
+
+    tt::tt_metal::operation::Hash compute_program_hash(
+        const std::vector<Tensor>& input_tensors,
+        const std::vector<std::optional<const Tensor>>& optional_input_tensors) const;
+
+    static constexpr auto attribute_names = std::forward_as_tuple("matmul_struct", "devices");
+    auto attribute_values() const { return std::forward_as_tuple(this->matmul_struct, this->devices); }
+};
+
+// llama All Gather MM Variants
+tt::tt_metal::operation::ProgramWithCallbacks llama_all_gather_matmul_async_sharded(
+    const Tensor& input_tensor,
+    const Tensor& input1,
+    Tensor& output_tensor,
+    const Tensor& intermediate_tensor,
+    const Tensor& aggregated_tensor,
+    IDevice* target_device,
+    std::optional<IDevice*> forward_device,
+    std::optional<IDevice*> backward_device,
+    uint32_t dim,
+    uint32_t num_links,
+    uint32_t ring_size,
+    uint32_t ring_index,
+    ccl::Topology topology,
+    const GlobalSemaphore& semaphore,
+    const std::optional<tt::tt_metal::SubDeviceId>& sub_device_id,
+    DeviceComputeKernelConfig compute_kernel_config,
+    const operations::matmul::MatmulProgramConfig& program_config,
+    const std::optional<const tt::tt_metal::experimental::GlobalCircularBuffer>& global_cb);
+
+tt::tt_metal::operation::ProgramWithCallbacks matmul_multi_core_agmm_fusion_helper(
+    tt::tt_metal::Program& program,
+    const Tensor& input0,
+    const std::vector<Tensor>& input_tensors_b,
+    const std::optional<const Tensor>& bias,
+    const std::vector<Tensor>& output_tensors,
+    bool bcast_batch,
+    DeviceComputeKernelConfig compute_kernel_config,
+    const operations::matmul::MatmulProgramConfig& program_config,
+    bool untilize_out,
+    std::optional<ttnn::experimental::ccl::MatmulFusedOpSignaler>& fused_op_signaler,
+    const std::optional<const tt::tt_metal::experimental::GlobalCircularBuffer>& global_cb,
+    const std::optional<tt::tt_metal::SubDeviceId>& sub_device_id);
+
+namespace operations {
+namespace experimental {
+namespace ccl {
+
+Tensor llama_all_gather_matmul_async(
+    const Tensor& input_tensor,
+    const Tensor& input1,
+    const Tensor& intermediate_tensor,
+    int32_t dim,
+    uint32_t cluster_axis,
+    const MeshDevice& mesh_device,
+    ttnn::ccl::Topology topology,
+    const GlobalSemaphore& multi_device_global_semaphore,
+    const std::optional<MemoryConfig>& ag_memory_config = std::nullopt,
+    const std::optional<MemoryConfig>& mm_memory_config = std::nullopt,
+    std::optional<size_t> num_preferred_links = std::nullopt,
+    std::optional<tt::tt_metal::SubDeviceId> sub_device_id = std::nullopt,
+    const std::optional<const operations::matmul::MatmulProgramConfig>& program_config = std::nullopt,
+    std::optional<const ttnn::DeviceComputeKernelConfig> compute_kernel_config = std::nullopt,
+    std::optional<const DataType> dtype = std::nullopt,
+    const std::optional<const tt::tt_metal::experimental::GlobalCircularBuffer>& global_cb = std::nullopt);
+
+LlamaAllGatherMatmulAsync create_llama_all_gather_matmul_async_struct(
+    const ttnn::AllGatherParams& all_gather_params,
+    const operations::matmul::Matmul& matmul_struct,
+    const std::vector<IDevice*>& devices);
+
+}  // namespace ccl
+}  // namespace experimental
+
+namespace llama_matmul {
+
+tt::tt_metal::operation::ProgramWithCallbacks matmul_multi_core_agmm_fusion_helper(
+    tt::tt_metal::Program& program,
+    const Tensor& a,
+    const std::vector<Tensor>& b_tensors,
+    const std::optional<const Tensor>& bias,
+    const std::vector<Tensor>& output_tensors,
+    bool broadcast_batch,
+    DeviceComputeKernelConfig compute_kernel_config,
+    const matmul::MatmulProgramConfig& program_config,
+    bool untilize_out,
+    std::optional<ttnn::experimental::ccl::MatmulFusedOpSignaler>& fused_op_signaler,
+    const std::optional<const tt::tt_metal::experimental::GlobalCircularBuffer>& global_cb,
+    const std::optional<tt::tt_metal::SubDeviceId>& sub_device_id);
+
+}  // namespace llama_matmul
+
+}  // namespace operations
+
+}  // namespace ttnn
+
+namespace llama_agmm_fusion_helpers {
+void override_program_parameters(
+    const ttnn::operations::matmul::matmul_mcast_1d_common_override_variables_t& override_variables,
+    const void* operation,
+    tt::tt_metal::Program& program,
+    const std::vector<tt::tt_metal::Tensor>& input_tensors,
+    const std::vector<std::optional<const tt::tt_metal::Tensor>>& optional_input_tensors,
+    const std::vector<tt::tt_metal::Tensor>& output_tensors);
+
+}  // namespace llama_agmm_fusion_helpers
