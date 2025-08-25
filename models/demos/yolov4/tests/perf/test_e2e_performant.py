0be365125480fa09b8c55b63c90a94b4b765e575
keerthana-r-mcw <keerthana.rajendran@multicorewareinc.com>
2025-08-25 06:27:17 +0000

    #27264: Yolov4 320x320 opt

--- models/demos/yolov4/tests/perf/test_e2e_performant.py
+++ models/demos/yolov4/tests/perf/test_e2e_performant.py
@@ -140,7 +140,7 @@ def run_perf_e2e_yolov4(
 )
 @pytest.mark.parametrize(
     "batch_size_per_device, act_dtype, weight_dtype",
-    ((1, ttnn.bfloat16, ttnn.bfloat16),),
+    ((1, ttnn.bfloat8_b, ttnn.bfloat8_b),),
 )
 @pytest.mark.parametrize(
     "resolution, expected_inference_throughput",
