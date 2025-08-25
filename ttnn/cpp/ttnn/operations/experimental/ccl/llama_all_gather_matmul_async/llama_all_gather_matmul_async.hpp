97a466a157b026643dc48a8f445096e64585e02f
Ligang Long <llong@tenstorrent.com>
2025-08-25 11:53:14 -0700

    Llama AG + 1D ring MM fusion (#25438)
    ### Ticket
    [Link to Github
    Issue](https://docs.google.com/spreadsheets/d/1IBdiTGx18Lt-WqtJxP9Co9hcpG1D650zPvDKtTn_xZE/edit?gid=397681784#gid=397681784)
    
    ### Problem description
    AG + MM fusion, that can give us about 5us perf gain.
    
    ### What's changed
    Fusion of AG + 1D ring MM, break the 1D single chip ring MM into CCL
    cross-device ring MM. The data from local chip get in MM first and then
    data from other devices come in ring gather fashion.
    
    What it expects from Prefetcher is that block size is 6 time bigger(24
    blocks -> 4 blocks), no padding needed for each block(in1 is 3584x2048,
    3584/24 = 149.333, while 3584/4 = 896)
    
    unit test perf gain close to 5us in kernel time. 
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
    CI passes (if applicable)
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: avoraTT <avora@tenstorrent.com>
    Co-authored-by: sohaibnadeemTT <sohaibnadeem@tenstorrent.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ ttnn/cpp/ttnn/operations/experimental/ccl/llama_all_gather_matmul_async/llama_all_gather_matmul_async.hpp
@@ -0,0 +1,44 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "ttnn/decorators.hpp"
+#include "ttnn/operations/ccl/ccl_host_datastructures.hpp"
+#include "ttnn/global_semaphore.hpp"
+#include "ttnn/operations/experimental/ccl/llama_all_gather_matmul_async/device/llama_all_gather_matmul_async_op.hpp"
+
+namespace ttnn {
+namespace operations::experimental::ccl {
+
+struct ExecuteAllGatherMatmulAsync {
+    static ttnn::Tensor invoke(
+        const ttnn::Tensor& input0,
+        const ttnn::Tensor& input1,
+        const ttnn::Tensor& intermediate_tensor,
+        int32_t dim,
+        uint32_t cluster_axis,
+        const MeshDevice& mesh_device,
+        ttnn::ccl::Topology topology,
+        const GlobalSemaphore& multi_device_global_semaphore,
+        const std::optional<MemoryConfig>& ag_memory_config = std::nullopt,
+        const std::optional<MemoryConfig>& mm_memory_config = std::nullopt,
+        std::optional<size_t> num_preferred_links = std::nullopt,
+        std::optional<tt::tt_metal::SubDeviceId> subdevice_id = std::nullopt,
+        const std::optional<const operations::matmul::MatmulProgramConfig>& program_config = std::nullopt,
+        std::optional<const ttnn::DeviceComputeKernelConfig> compute_kernel_config = std::nullopt,
+        std::optional<const DataType> dtype = std::nullopt,
+        const std::optional<const tt::tt_metal::experimental::GlobalCircularBuffer>& global_cb = std::nullopt);
+};
+
+}  // namespace operations::experimental::ccl
+
+namespace experimental {
+
+constexpr auto llama_all_gather_matmul_async = ttnn::register_operation<
+    "ttnn::experimental::llama_all_gather_matmul_async",
+    ttnn::operations::experimental::ccl::ExecuteAllGatherMatmulAsync>();
+
+}  // namespace experimental
+}  // namespace ttnn
