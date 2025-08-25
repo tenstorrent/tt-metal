a250fcf94354580a11d0c88da760edc60426381c
mgajewskiTT <mgajewski@tenstorrent.com>
2025-08-25 06:59:30 +0000

    26841 - Scaffold.

--- ttnn/cpp/ttnn/operations/normalization/softmax_2/softmax.cpp
+++ ttnn/cpp/ttnn/operations/normalization/softmax_2/softmax.cpp
@@ -5,7 +5,6 @@
 #include "softmax.hpp"
 
 namespace ttnn::operations::normalization {
-
 Tensor ExecuteSoftmax::invoke(
     QueueId queue_id,
     const ttnn::Tensor& input_tensor,
@@ -17,4 +16,47 @@ Tensor ExecuteSoftmax::invoke(
     return Tensor{};  // TODO: Fix
 }
 
+Tensor ExecuteScaleMaskSoftmax::invoke(
+    const ttnn::Tensor& input_tensor,
+    std::optional<float> scalet,
+    const std::optional<const Tensor>& mask,
+    const std::optional<ttnn::MemoryConfig>& memory_config,
+    bool is_causal_mask,
+    std::optional<const DeviceComputeKernelConfig> compute_kernel_config,
+    bool numeric_stable) {
+    // TODO: Fill
+    return Tensor{};  // TODO: Fix
+}
+
+Tensor ExecuteSoftmaxInPlace::invoke(
+    const ttnn::Tensor& input_tensor,
+    const SoftmaxProgramConfig& program_config,
+    std::optional<const DeviceComputeKernelConfig> compute_kernel_config,
+    bool numeric_stable) {
+    // TODO: Fill
+    return Tensor{};  // TODO: Fix
+}
+
+Tensor ExecuteScaleMaskSoftmaxInPlace::invoke(
+    const ttnn::Tensor& input_tensor,
+    std::optional<float> scale,
+    const std::optional<const Tensor>& mask,
+    const SoftmaxProgramConfig& program_config,
+    bool is_causal_mask,
+    std::optional<const DeviceComputeKernelConfig> compute_kernel_config,
+    bool numeric_stable) {
+    // TODO: Fill
+    return Tensor{};  // TODO: Fix
+}
+
+Tensor ExecuteScaleCausalMaskHWSoftmaxInPlace::invoke(
+    const ttnn::Tensor& input_tensor,
+    std::optional<float> scale,
+    const std::optional<const Tensor>& mask,
+    const SoftmaxProgramConfig& program_config,
+    std::optional<const DeviceComputeKernelConfig> compute_kernel_config,
+    bool numeric_stable) {
+    // TODO: Fill
+    return Tensor{};  // TODO: Fix
+}
 }  // namespace ttnn::operations::normalization
