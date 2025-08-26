08068e8fdbfd36b34bbf1ab709356e97bc4fb0cc
Nilaykumar Patel <nkpatel@tenstorrent.com>
2025-08-26 04:23:59 +0000

    Read 2 bytes at a time instead of single.
    Signed-off-by: Nilaykumar Patel <nkpatel@tenstorrent.com>

--- ttnn/cpp/ttnn/operations/data_movement/fold/device/kernels/dataflow/writer_cb2dram_for_rm_input.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/fold/device/kernels/dataflow/writer_cb2dram_for_rm_input.cpp
@@ -5,7 +5,9 @@
 #include <stdint.h>
 #include <cstdint>
 #include "dataflow_api.h"
+#include "ttnn/operations/data_movement/common/kernels/common.hpp"
 
+using namespace tt::data_movement::common;
 void kernel_main() {
     constexpr uint32_t stick_nbytes = get_compile_time_arg_val(0);
     constexpr uint32_t cb_id_in0 = get_compile_time_arg_val(1);
@@ -23,15 +25,16 @@ void kernel_main() {
     const auto s_out = TensorAccessor(dst_args, dst_addr, stick_nbytes * patch_size);
     uint32_t dst_index = get_arg_val<uint32_t>(1);
     uint32_t intermed_l1_scratch = get_write_ptr(cb_id_in1);
-    volatile tt_l1_ptr uint8_t* patch_data = (volatile uint8_t*)intermed_l1_scratch;
+    // Datatypes will be multiple of 2 bytes only so it is safe to use uint16_t pointer
+    volatile tt_l1_ptr uint16_t* patch_data = (volatile uint16_t*)intermed_l1_scratch;
     for (uint32_t input_idx = 0; input_idx < work_per_core; input_idx++) {
         uint32_t idx = 0;
         cb_wait_front(cb_id_in0, 1);
         uint32_t l1_addr = get_read_ptr(cb_id_in0);
         if constexpr (!is_l1_aligned) {
             for (uint32_t i = 0; i < patch_size; i++) {
-                for (uint32_t j = 0; j < stick_nbytes; j++) {
-                    patch_data[idx++] = *(volatile uint8_t*)(l1_addr + j);
+                for (uint32_t j = 0; j < (stick_nbytes / 2); j++) {
+                    patch_data[idx++] = *(volatile uint16_t*)(l1_addr + j * 2);
                 }
                 l1_addr += aligned_stick_nbytes_dram;
             }
