55ba9f6e715462e39a14886395189e0bce38a2d5
Artem Yerofieiev <artem@tenstorrent.com>
2025-08-26 22:43:09 +0000

    fix

--- ttnn/cpp/ttnn/operations/ccl/all_to_all_dispatch/all_to_all_dispatch.cpp
+++ ttnn/cpp/ttnn/operations/ccl/all_to_all_dispatch/all_to_all_dispatch.cpp
@@ -27,8 +27,7 @@ std::array<ttnn::Tensor, 2> ExecuteAllToAllDispatch::invoke(
     std::optional<uint32_t> num_links,
     std::optional<tt::tt_fabric::Topology> topology,
     const std::optional<ttnn::MemoryConfig>& memory_config,
-    const std::optional<tt::tt_metal::SubDeviceId>& subdevice_id,
-    const std::optional<GlobalSemaphore>& global_semaphore) {
+    const std::optional<tt::tt_metal::SubDeviceId>& subdevice_id) {
     auto mesh_device = input_tensor.device();
     auto sd_id = subdevice_id.value_or(mesh_device->get_sub_device_ids().at(0));
     auto subdevice_core_range_set = mesh_device->worker_cores(tt::tt_metal::HalProgrammableCoreType::TENSIX, sd_id);
