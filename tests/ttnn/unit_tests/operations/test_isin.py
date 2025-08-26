ee2534499e332a3b524f699316bcfcd2f9947a82
Jaroslaw Bartosz Bieniek <jbbieniek@tenstorrent.com>
2025-08-26 08:38:51 +0000

    26065: add a unit test

---
+++ tests/ttnn/unit_tests/operations/test_isin.py
@@ -0,0 +1,57 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import torch
+import ttnn
+
+from tests.ttnn.utils_for_testing import assert_allclose
+
+
+def select_torch_dtype(ttnn_dtype):
+    if ttnn_dtype is ttnn.bfloat16:
+        return torch.bfloat16
+    if ttnn_dtype is ttnn.float32:
+        return torch.float32
+    if ttnn_dtype is ttnn.uint8:
+        return torch.uint8
+    if ttnn_dtype is ttnn.uint16:
+        return torch.int64
+    if ttnn_dtype is ttnn.int32:
+        return torch.int64
+    if ttnn_dtype is ttnn.uint32:
+        return (
+            torch.int64
+        )  # !!! there is a strict requirement for the index tensor in Torch to be int64, and there is no int64 in ttnn
+
+
+@pytest.mark.parametrize(
+    "elements, test_elements, dtype",
+    [
+        (
+            [i for i in range(200)],
+            [1, 2, 3],
+            ttnn.int32,
+        ),
+        (
+            [i for i in range(10, 200)],
+            [11, 2, 3, 24],
+            ttnn.int32,
+        ),
+    ],
+)
+def test_isin_normal(elements, test_elements, dtype, device):
+    torch_dtype = select_torch_dtype(dtype)
+    elements_torch = torch.tensor(elements, dtype=torch_dtype)
+    test_elements_torch = torch.tensor(test_elements, dtype=torch_dtype)
+
+    elements_ttnn = ttnn.from_torch(elements_torch, device=device)
+    test_elements_ttnn = ttnn.from_torch(test_elements_torch, device=device)
+
+    torch_isin_result = torch.isin(elements_torch, test_elements_torch)
+    ttnn_isin_result = ttnn.experimental.isin(elements_ttnn, test_elements_ttnn)
+
+    assert torch_isin_result.shape == ttnn_isin_result.shape
+
+    assert_allclose(torch_isin_result, ttnn.to_torch(ttnn_isin_result))
