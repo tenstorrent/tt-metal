bb59a53a85b3c723b75dfd8c3334b29e764ff4eb
Ubuntu <ssinghal@tenstorrent.com>
2025-08-26 22:46:28 +0000

    fixed postprocessing

--- models/demos/yolov6l/web_demo/server/fast_api_yolov6l.py
+++ models/demos/yolov6l/web_demo/server/fast_api_yolov6l.py
@@ -7,19 +7,103 @@ from io import BytesIO
 
 import numpy as np
 import torch
+import torchvision
 from fastapi import FastAPI, File, UploadFile
 from PIL import Image
 
 import ttnn
+
+# from models.experimental.yolo_common.yolo_web_demo.yolo_evaluation_utils import postprocess
+from models.demos.utils.common_demo_utils import xywh2xyxy
 from models.demos.yolov6l.common import YOLOV6L_L1_SMALL_SIZE
 from models.demos.yolov6l.runner.performant_runner import YOLOv6lPerformantRunner
-from models.experimental.yolo_common.yolo_web_demo.yolo_evaluation_utils import postprocess
 
 app = FastAPI(
     title="YOLOv6l object detection",
     description="Inference engine to detect objects in image.",
     version="0.0",
 )
+
+
+def non_max_suppression(
+    prediction, conf_thres=0.25, iou_thres=0.45, classes=None, agnostic=False, multi_label=False, max_det=300
+):
+    num_classes = prediction.shape[2] - 5  # number of classes
+    pred_candidates = torch.logical_and(
+        prediction[..., 4] > conf_thres, torch.max(prediction[..., 5:], axis=-1)[0] > conf_thres
+    )  # candidates
+    # Check the parameters.
+    assert 0 <= conf_thres <= 1, f"conf_thresh must be in 0.0 to 1.0, however {conf_thres} is provided."
+    assert 0 <= iou_thres <= 1, f"iou_thres must be in 0.0 to 1.0, however {iou_thres} is provided."
+
+    # Function settings.
+    max_wh = 4096  # maximum box width and height
+    max_nms = 30000  # maximum number of boxes put into torchvision.ops.nms()
+    time_limit = 10.0  # quit the function when nms cost time exceed the limit time.
+    multi_label &= num_classes > 1  # multiple labels per box
+
+    output = [torch.zeros((0, 6), device=prediction.device)] * prediction.shape[0]
+    for img_idx, x in enumerate(prediction):  # image index, image inference
+        x = x[pred_candidates[img_idx]]  # confidence
+
+        # If no box remains, skip the next process.
+        if not x.shape[0]:
+            continue
+
+        # confidence multiply the objectness
+        x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
+
+        # (center x, center y, width, height) to (x1, y1, x2, y2)
+        box = xywh2xyxy(x[:, :4])
+
+        # Detections matrix's shape is  (n,6), each row represents (xyxy, conf, cls)
+        if multi_label:
+            box_idx, class_idx = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
+            x = torch.cat((box[box_idx], x[box_idx, class_idx + 5, None], class_idx[:, None].float()), 1)
+        else:  # Only keep the class with highest scores.
+            conf, class_idx = x[:, 5:].max(1, keepdim=True)
+            x = torch.cat((box, conf, class_idx.float()), 1)[conf.view(-1) > conf_thres]
+
+        # Filter by class, only keep boxes whose category is in classes.
+        if classes is not None:
+            x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
+        # Check shape
+        num_box = x.shape[0]  # number of boxes
+        if not num_box:  # no boxes kept.
+            continue
+        elif num_box > max_nms:  # excess max boxes' number.
+            x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence
+
+        # Batched NMS
+        class_offset = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
+        boxes, scores = x[:, :4] + class_offset, x[:, 4]  # boxes (offset by class), scores
+        keep_box_idx = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
+        if keep_box_idx.shape[0] > max_det:  # limit detections
+            keep_box_idx = keep_box_idx[:max_det]
+
+        output[img_idx] = x[keep_box_idx]
+
+    return output
+
+
+def Boxes(data):
+    return {"xyxy": data[:, :4], "conf": data[:, -2], "cls": data[:, -1]}
+
+
+def postprocess(preds, img, orig_imgs, conf=0.25, max_det=300):
+    args = {"conf": conf, "iou": 0.7, "agnostic_nms": False, "max_det": max_det, "classes": None}
+
+    preds = non_max_suppression(
+        preds,
+        args["conf"],
+        max_det=args["max_det"],
+    )
+
+    results = []
+    for pred, orig_img in zip(preds, orig_imgs):
+        results.append({"orig_img": orig_img, "boxes": Boxes(pred)})
+
+    return results
 
 
 @app.get("/")
