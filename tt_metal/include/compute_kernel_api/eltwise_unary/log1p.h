73eafec7c82fde6c15d3a9e90f1a3f552f906bcb
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2025-08-26 15:21:26 +0530

    Consistently capitalize FAST_APPROX parameter in log1p_tile_init and log1p_tile functions

--- tt_metal/include/compute_kernel_api/eltwise_unary/log1p.h
+++ tt_metal/include/compute_kernel_api/eltwise_unary/log1p.h
@@ -19,8 +19,8 @@ namespace ckernel {
 /**
  * Please refer to documentation for any_init.
  */
-ALWI void log1p_tile_init(bool fast_and_approx = true) {
-    MATH(SFPU_INIT_KERNEL_CALL_FAST_APPROX(log1p, sfpu::log1p_init, APPROX, fast_and_approx));
+ALWI void log1p_tile_init(bool FAST_APPROX = true) {
+    MATH(SFPU_INIT_KERNEL_CALL_FAST_APPROX(log1p, sfpu::log1p_init, APPROX, FAST_APPROX));
 }
 
 // clang-format off
@@ -37,8 +37,8 @@ ALWI void log1p_tile_init(bool fast_and_approx = true) {
  * | idst            | The index of the tile in DST register buffer to perform the computation on | uint32_t | Must be less than the size of the DST register buffer | True     |
  */
 // clang-format on
-ALWI void log1p_tile(uint32_t idst, bool fast_and_approx = true) {
-    MATH(SFPU_UNARY_NO_PARAM_KERNEL_LOG1P(log1p, RC, APPROX, fast_and_approx, idst));
+ALWI void log1p_tile(uint32_t idst, bool FAST_APPROX = true) {
+    MATH(SFPU_UNARY_NO_PARAM_KERNEL_LOG1P(log1p, RC, APPROX, FAST_APPROX, idst));
 }
 
 }  // namespace ckernel
