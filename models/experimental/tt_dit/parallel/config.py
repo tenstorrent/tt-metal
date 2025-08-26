81110e2db3d05067e7172d34fcda796758218dda
Dorsa Rohani <dorsa.rohani@gmail.com>
2025-08-26 11:33:54 -0500

    Add TT-Dit SD3.5 CLIP & T5 (#27249)
    ### Ticket
    N/A
    
    ### What's changed
    Adds TT-DiT SD3.5 CLIP and T5 encoders
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    
    ---------
    
    Co-authored-by: Colman Glagovich <cglagovich@tenstorrent.com>

--- models/experimental/tt_dit/parallel/config.py
+++ models/experimental/tt_dit/parallel/config.py
@@ -19,6 +19,10 @@ class DiTParallelConfig(NamedTuple):
     sequence_parallel: ParallelFactor
 
 
+class EncoderParallelConfig(NamedTuple):
+    tensor_parallel: ParallelFactor
+
+
 class OldParallelConfig(NamedTuple):
     mesh_shape: tuple[int, int]
     factor: int
@@ -113,75 +117,3 @@ def create_vae_parallel_manager(
         ],  # Ping pong
         num_links=ccl_manager.num_links,
     )
-
-
-# TODO: Simplify with CCLManager
-class EncoderParallelManager:
-    def __init__(
-        self,
-        mesh_device,
-        topology,
-        mesh_axis,
-        num_links=1,
-    ):
-        self.mesh_device = mesh_device
-        self.num_links = num_links
-        self.topology = topology
-
-        self.tensor_parallel = OldParallelConfig(
-            mesh_shape=tuple(mesh_device.shape), factor=mesh_device.shape[mesh_axis], mesh_axis=mesh_axis
-        )
-
-        # Setup semaphores
-        self._init_subdevice()
-
-        # SD35-specific semaphores
-        semaphore_names = [("ping_pong", 2 * 2), ("rs_ping_pong", 3 * 2), ("ar_ping_pong", 3 * 2)]
-        self._init_semaphores(semaphore_names)
-        self.ping_pong_idx = 0
-        self.rs_ping_pong_idx = 0
-        self.ar_ping_pong_idx = 0
-
-    def _init_subdevice(self):
-        compute_grid_size = self.mesh_device.compute_with_storage_grid_size()
-        self.ccl_cores = ttnn.CoreRangeSet(
-            {ttnn.CoreRange(ttnn.CoreCoord(0, 0), ttnn.CoreCoord(compute_grid_size.x - 1, compute_grid_size.y - 1))}
-        )
-
-        _worker_sub_device = ttnn.SubDevice(
-            [
-                self.ccl_cores,
-            ]
-        )
-        self.ccl_sub_device_id = ttnn.SubDeviceId(0)
-
-    def _init_semaphores(self, semaphore_names):
-        # Initialize semaphores for each necessary CCL, None if a single semaphore else length of semaphore list
-        self.sems = {}
-        for sem_name, len_sem in semaphore_names:
-            n_sems = 1 if len_sem is None else len_sem
-            sems = [ttnn.create_global_semaphore(self.mesh_device, self.ccl_cores, 0) for _ in range(n_sems)]
-            if len_sem is None:
-                sems = sems[0]
-            self.sems[sem_name] = sems
-
-    def get_ping_pong_semaphore(self):
-        cur_idx = self.ping_pong_idx
-        self.ping_pong_idx = 2 - self.ping_pong_idx
-        return self.sems["ping_pong"][cur_idx : cur_idx + 2]
-
-    def get_rs_ping_pong_semaphore(self):
-        cur_idx = self.rs_ping_pong_idx
-        n_sems = 3
-        self.rs_ping_pong_idx = (cur_idx + 1) % 2
-        return self.sems["rs_ping_pong"][cur_idx * n_sems : (cur_idx + 1) * n_sems]
-
-    def get_ar_ping_pong_semaphore(self):
-        cur_idx = self.ar_ping_pong_idx
-        n_sems = 3
-        self.ar_ping_pong_idx = (cur_idx + 1) % 2
-        return self.sems["ar_ping_pong"][cur_idx * n_sems : (cur_idx + 1) * n_sems]
-
-    @property
-    def is_tensor_parallel(self):
-        return self.tensor_parallel.factor > 1
