311c58b6aa20f3b2d9ce488939660c5ba6089dd3
Brian Liu <109366241+TT-BrianLiu@users.noreply.github.com>
2025-08-26 11:58:04 -0400

    #0: Make create_mesh_workload optional if create_program_at/create_at is defined (#26812)
    ### Ticket
    None
    
    ### Problem description
    Looks like most multi-chip OPs (including all CCLs?) implement
    `create_mesh_workload` in exactly the same way by looping through
    single-device program creation and inserting to a mesh workload. This PR
    adds this pattern to be natively supported in OP infra so it better
    reflects intention of this pattern.
    
    ### What's changed
    - Make changes to both OP infras to support this
    - Remove create_mesh_workload from OP as examples:
      * all gather (uses old type-erased OP infra)
      * all to all dispatch (uses new compile-time interfaces OP infra)
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/17218183763
    - T3K: https://github.com/tenstorrent/tt-metal/actions/runs/17218205182
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
    CI passes (if applicable)
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/ccl/barrier/device/barrier_op.cpp
+++ ttnn/cpp/ttnn/operations/ccl/barrier/device/barrier_op.cpp
@@ -28,16 +28,6 @@ std::vector<Tensor> Barrier::create_output_tensors(const std::vector<Tensor>& in
     return input_tensors;
 }
 
-tt::tt_metal::operation::MeshWorkloadWithCallbacks Barrier::create_mesh_workload(
-    const ttnn::MeshCoordinateRangeSet& tensor_coords,
-    const std::vector<Tensor>& input_tensors,
-    std::vector<Tensor>& output_tensors) const {
-    return ccl::create_mesh_workload_from_programs(
-        tensor_coords, input_tensors, output_tensors, [&, this](const ttnn::MeshCoordinate& coord) {
-            return create_program_at(coord, input_tensors, output_tensors);
-        });
-}
-
 tt::tt_metal::operation::ProgramWithCallbacks Barrier::create_program_at(
     const ttnn::MeshCoordinate& mesh_coord,
     const std::vector<Tensor>& input_tensors,
