b5097bc125141b9035bd3f85066fbc154e1709c8
Umair <ucheema@tenstorrent.com>
2025-08-27 20:27:09 +0000

    remove fabric_route. move riscv related helpers to risc_common.h. emptied out tt_fabric.h

--- tt_metal/fabric/hw/inc/tt_fabric.h
+++ tt_metal/fabric/hw/inc/tt_fabric.h
@@ -11,6 +11,7 @@
 #include "ethernet/dataflow_api.h"
 #include "eth_chan_noc_mapping.h"
 #include "hostdevcommon/fabric_common.h"
+#include "tt_metal/hw/inc/risc_common.h"
 
 using namespace tt::tt_fabric;
 
@@ -19,36 +20,3 @@ using namespace tt::tt_fabric;
 #define LOW_LATENCY_ROUTING
 #endif
 #endif
-
-inline uint64_t get_timestamp() {
-    uint32_t timestamp_low = reg_read(RISCV_DEBUG_REG_WALL_CLOCK_L);
-    uint32_t timestamp_high = reg_read(RISCV_DEBUG_REG_WALL_CLOCK_H);
-    return (((uint64_t)timestamp_high) << 32) | timestamp_low;
-}
-
-inline uint64_t get_timestamp_32b() { return reg_read(RISCV_DEBUG_REG_WALL_CLOCK_L); }
-
-void zero_l1_buf(tt_l1_ptr uint32_t* buf, uint32_t size_bytes) {
-    for (uint32_t i = 0; i < size_bytes / 4; i++) {
-        buf[i] = 0;
-    }
-}
-
-static FORCE_INLINE void write_test_results(tt_l1_ptr uint32_t* const buf, uint32_t i, uint32_t val) {
-    if (buf != nullptr) {
-        buf[i] = val;
-    }
-}
-
-static FORCE_INLINE void write_kernel_status(tt_l1_ptr uint32_t* const buf, uint32_t i, uint32_t val) {
-    if (buf != nullptr) {
-        buf[i] = val;
-    }
-}
-
-static FORCE_INLINE void set_64b_result(uint32_t* buf, uint64_t val, uint32_t index = 0) {
-    if (buf != nullptr) {
-        buf[index] = val >> 32;
-        buf[index + 1] = val & 0xFFFFFFFF;
-    }
-}
