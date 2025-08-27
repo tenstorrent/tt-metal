1a8af120c01c54296869d7ad6b40d70337f2d3fb
Gongyu Wang <gwang@tenstorrent.com>
2025-08-27 12:57:35 +0000

    Update TTFT target perf

--- models/tt_transformers/demo/simple_text_demo.py
+++ models/tt_transformers/demo/simple_text_demo.py
@@ -1081,7 +1081,7 @@ def test_demo_text(
                 # N300 targets
                 # "N300_Qwen2.5-7B": 150,  # too much variability in CI (https://github.com/tenstorrent/tt-metal/issues/24754)
                 # T3K targets
-                "T3K_Llama-3.1-70B": 188,
+                "T3K_Llama-3.1-70B": 204,
                 # "T3K_Qwen2.5-Coder-32B": 180,  # too much variability in CI (https://github.com/tenstorrent/tt-metal/issues/24754)
                 # "T3K_Qwen2.5-72B": 211,  # too much variability in CI (https://github.com/tenstorrent/tt-metal/issues/24754)
                 # "T3K_Qwen3-32B": 250, # too much variability in CI (https://github.com/tenstorrent/tt-metal/issues/24754)
