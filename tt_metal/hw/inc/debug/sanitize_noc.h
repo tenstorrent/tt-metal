009b2ab2e9a44bed078fb21d01bf9ba3025e4428
Rose Li <roseli@tenstorrent.com>
2025-08-27 09:27:05 -0400

    Revert "Remove (most) uses of riscv_id_t in Watcher" (#27496)
    Reverts tenstorrent/tt-metal#27432
    
    APC deterministic [hang in
    watcher](https://github.com/tenstorrent/tt-metal/actions/runs/17254635566/job/48964687819#step:5:2744)
    after [Remove (most) uses of riscv_id_t in
    Watcher](https://github.com/tenstorrent/tt-metal/commit/cc26ffd15411a7caedf5d4d88617de5330bdd5c1)

--- tt_metal/hw/inc/debug/sanitize_noc.h
+++ tt_metal/hw/inc/debug/sanitize_noc.h
@@ -246,7 +246,7 @@ void __attribute__((noinline)) debug_sanitize_post_noc_addr_and_hang(
         v[noc_id].noc_addr = noc_addr;
         v[noc_id].l1_addr = l1_addr;
         v[noc_id].len = len;
-        v[noc_id].which_risc = PROCESSOR_INDEX;
+        v[noc_id].which_risc = debug_get_which_riscv();
         v[noc_id].is_multicast = (multicast == DEBUG_SANITIZE_NOC_MULTICAST);
         v[noc_id].is_write = (dir == DEBUG_SANITIZE_NOC_WRITE);
         v[noc_id].is_target = (which_core == DEBUG_SANITIZE_NOC_TARGET);
