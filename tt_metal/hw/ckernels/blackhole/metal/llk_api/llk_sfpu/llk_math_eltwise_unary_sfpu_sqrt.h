f3ce588668d155c8f6586b18207f67cf086b7dbe
Jason Davies <jason@jasondavies.com>
2025-08-27 01:39:59 +0100

    Fix missing template param.

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_sqrt.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_sqrt.h
@@ -18,7 +18,7 @@ inline void llk_math_eltwise_unary_sfpu_sqrt(uint dst_index, int vector_mode = (
         ckernel::sfpu::calculate_sqrt<APPROXIMATE, 8, fp32_dest_acc_en, legacy_compat>, dst_index, vector_mode);
 }
 
-template <bool APPROXIMATE>
+template <bool APPROXIMATE, bool legacy_compat>
 inline void llk_math_eltwise_unary_sfpu_sqrt_init() {
     llk_math_eltwise_unary_sfpu_init<SfpuType::sqrt, APPROXIMATE>(sfpu::sqrt_init<APPROXIMATE, legacy_compat>);
 }
