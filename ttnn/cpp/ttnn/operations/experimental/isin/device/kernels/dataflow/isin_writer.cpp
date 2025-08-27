098f8938225cace858239e0a9c69d0cb41e1deec
Jaroslaw Bartosz Bieniek <jbbieniek@tenstorrent.com>
2025-08-27 11:32:07 +0000

    26065: multi-core

--- ttnn/cpp/ttnn/operations/experimental/isin/device/kernels/dataflow/isin_writer.cpp
+++ ttnn/cpp/ttnn/operations/experimental/isin/device/kernels/dataflow/isin_writer.cpp
@@ -14,14 +14,18 @@ void kernel_main() {
     constexpr auto ctas = get_ctas();
 
     const uint32_t output_buffer_address = get_arg_val<uint32_t>(0);
+    const uint32_t subchunks_per_core = get_arg_val<uint32_t>(1);
+    const uint32_t subchunks_offset = get_arg_val<uint32_t>(2);
     const auto output_addr_gtor = TensorAccessor{ctas.output_accessor_args, output_buffer_address, ctas.elements_size};
 
-    for (uint32_t output_subchunk_id = 0, output_offset = 0; output_offset < ctas.elements_size;
+    for (uint32_t output_subchunk_id = subchunks_offset,
+                  output_offset = subchunks_offset * ctas.single_fetch_subchunk_size;
+         output_offset < ((subchunks_offset + subchunks_per_core) * ctas.single_fetch_subchunk_size);
          ++output_subchunk_id, output_offset += ctas.single_fetch_subchunk_size) {
         const uint32_t output_subchunk_size =
             std::min(ctas.elements_size - output_offset, ctas.single_fetch_subchunk_size);
         // const uint32_t output_l1_write_addr = get_write_ptr(ctas.output_cb);
-        write_to_dram<output_number_type, decltype(output_addr_gtor)>(
+        write_to_dram<decltype(output_addr_gtor)>(
             ctas.output_cb, output_addr_gtor, output_offset, output_subchunk_size);
     }
 }
