56e8e78ff0461947fdbb50a2e7040a5788d8604b
River Wu <riverwu@tenstorrent.com>
2025-08-27 21:03:43 +0000

    add coments for each functions

--- tt_metal/api/tt-metalium/program.hpp
+++ tt_metal/api/tt-metalium/program.hpp
@@ -68,39 +68,76 @@ public:
     Program(Program&& other) noexcept;
     Program& operator=(Program&& other) noexcept;
 
+    //////////////////////////////
+    // Runtime ID related functions:
+    //////////////////////////////
+
+    // Extensively used by tests
     void set_runtime_id(id_t id);
+    // Both used in tracing
     id_t get_id() const;
     id_t get_runtime_id() const;
 
+    //////////////////////////////
+    // Buffer related functions:
+    //////////////////////////////
+
+    // Only used in AssignGlobalBufferToProgram in tt_metal.cpp
     void add_buffer(std::shared_ptr<Buffer> buf);
+    // Only used in EnqueueProgram in host_runtime_commands.cpp
     void release_buffers();
 
+    // Used in ops.
     const std::vector<std::shared_ptr<CircularBuffer>>& circular_buffers() const;
+
+    // Only used in UpdateCircularBufferTotalSize in tt_metal.cpp
     void invalidate_circular_buffer_allocation();
+
+    // Always used in conjuction with validate_circular_buffer_region, which is in impl.
     void allocate_circular_buffers(const IDevice* device);
 
+    //////////////////////////////
+    // Kernel related functions:
+    //////////////////////////////
+
+    // Used in tests
     std::size_t num_kernels() const;
+    // Used in test_kernel_compile_cache.cpp, MeshWorkLoadImpl::get_kernels (which is not used anywhere else)
     std::unordered_map<KernelHandle, std::shared_ptr<Kernel>>& get_kernels(uint32_t programmable_core_type_index);
+    // Used in EnqueueProgram in hardware_command_queue.cpp
     void allocate_kernel_bin_buf_on_device(IDevice* device);
+    // Used in tests, fabric, CaptureCreateKernel, light metal, etc.
     std::shared_ptr<Kernel> get_kernel(KernelHandle kernel_id) const;
 
+    //////////////////////////////
+    // Semaphore related functions:
+    //////////////////////////////
+
+    // Used by MeshWorkloadImpl::semaphores, which is not used anywhere else
     const std::vector<Semaphore>& semaphores() const;
+    // Not used outside of tests
     std::size_t num_semaphores() const;
+    // Used in ConfigureDeviceWithProgram in tt_metal.cpp
     void init_semaphores(
         const IDevice& device, const CoreCoord& logical_core, uint32_t programmable_core_type_index) const;
 
+    // Used in ConfigureDeviceWithProgram in tt_metal.cpp
+    // Used in Device::init_command_queue_device, Device::configure_fabric in device.cpp
     std::vector<std::vector<CoreCoord>> logical_cores() const;
 
-    void compile(IDevice* device, bool force_slow_dispatch = false);
+    //////////////////////////////
+    // Program Binary Status related functions:
+    //////////////////////////////
 
-    void generate_dispatch_commands(IDevice* device, bool use_prefetcher_cache);
-
-    void finalize_offsets(IDevice* device);
-    bool is_finalized() const;
-
-    // Should this be public?
+    // Both used in hardware_command_queue.cpp and host_runtime_commands.cpp
     ProgramBinaryStatus get_program_binary_status(chip_id_t device_id) const;
     void set_program_binary_status(chip_id_t device_id, ProgramBinaryStatus status);
+
+    // We are removing these...
+    void compile(IDevice* device, bool force_slow_dispatch = false);
+    void generate_dispatch_commands(IDevice* device, bool use_prefetcher_cache);
+    void finalize_offsets(IDevice* device);
+    bool is_finalized() const;
 
     // debug/test
     detail::ProgramImpl& impl() { return *internal_; }
