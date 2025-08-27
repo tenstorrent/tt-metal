163c365155b1f8da187a4947a0a647762a715bdf
Ryan Zhu <ryanzhu@tenstorrent.com>
2025-08-27 21:45:00 +0000

    update tests to use TensorAccessor

--- tests/tt_metal/tt_metal/data_movement/multi_interleaved/kernels/multi_interleaved_write.cpp
+++ tests/tt_metal/tt_metal/data_movement/multi_interleaved/kernels/multi_interleaved_write.cpp
@@ -6,23 +6,10 @@
 #include <cstdint>
 #include "dataflow_api.h"
 #include "tensix_types.h"
-
-#include "debug/dprint.h"
-#include "debug/dprint_pages.h"
-
-template <uint32_t num_of_transactions, uint32_t num_pages, uint32_t page_size_bytes, bool is_dram>
-FORCE_INLINE void noc_write_helper(const uint32_t l1_read_addr, const InterleavedAddrGen<is_dram>& s) {
-    for (uint32_t i = 0; i < num_of_transactions; i++) {
-        for (uint32_t p = 0; p < num_pages; p++) {
-            noc_async_write_page(p, s, l1_read_addr + p * page_size_bytes);
-        }
-    }
-    noc_async_write_barrier();
-}
+#include "accessor/tensor_accessor.h"
 
 // L1 to DRAM write
 void kernel_main() {
-    DPRINT << "Kernel start" << ENDL();
     uint32_t dst_addr = get_arg_val<uint32_t>(0);
     uint32_t l1_read_addr = get_arg_val<uint32_t>(1);
 
@@ -33,22 +20,30 @@ void kernel_main() {
     constexpr uint32_t test_id = get_compile_time_arg_val(4);
     constexpr bool sync = get_compile_time_arg_val(5) == 1;
 
-    const InterleavedAddrGen<true> s = {.bank_base_address = dst_addr, .page_size = page_size_bytes};
+    // Tensor accessor compile time args appended to kernel's compile time args
+    // so the index is offset to start at 6
+    auto args = TensorAccessorArgs<6>();
+    auto s = TensorAccessor(args, dst_addr, page_size_bytes);
 
     constexpr uint32_t transaction_size_bytes = page_size_bytes;
     DeviceTimestampedData("Number of transactions", num_of_transactions * num_pages);
     DeviceTimestampedData("Transaction size in bytes", transaction_size_bytes);
     DeviceTimestampedData("Test id", test_id);
-    DPRINT << "Before cb wait" << ENDL();
+
     if constexpr (sync) {
         cb_wait_front(cb_id_out0, 1);
     }
     {
         DeviceZoneScopedN("RISCV1");
-        noc_write_helper<num_of_transactions, num_pages, page_size_bytes, true>(l1_read_addr, s);
+        for (uint32_t i = 0; i < num_of_transactions; i++) {
+            for (uint32_t p = 0; p < num_pages; p++) {
+                uint64_t noc_addr = s.get_noc_addr(p);
+                noc_async_write(l1_read_addr + p * page_size_bytes, noc_addr, page_size_bytes);
+            }
+        }
+        noc_async_write_barrier();
     }
     if constexpr (sync) {
         cb_pop_front(cb_id_out0, 1);
     }
-    DPRINT << "Kernel end" << ENDL();
 }
