7faeb8a4114ad4467936b8c27b8623da747bd909
gsarabandoTT <ctr-gsarabando@ext.tenstorrent.com>
2025-08-27 19:04:14 +0000

    Add temporary redirecting files

---
+++ models/generation_utils.py
@@ -0,0 +1,13 @@
+# autoflake: skip_file
+
+# generation_utils.py has been moved to models/common. This is a temporary redirecting file.
+# Update your imports accordingly as soon as possible.
+# Use: models.common.generation_utils
+
+from models.common.generation_utils import (
+    _get_logits_processor,
+    _merge_criteria_processor_list,
+    get_logits_processor,
+    pad_input_32,
+    run_generate,
+)
---
+++ models/helper_funcs.py
@@ -0,0 +1,7 @@
+# autoflake: skip_file
+
+# helper_funcs.py has been moved to models/common. This is a temporary redirecting file.
+# Update your imports accordingly as soon as possible.
+# Use: models.common.helper_funcs
+
+from models.common.helper_funcs import Linear
