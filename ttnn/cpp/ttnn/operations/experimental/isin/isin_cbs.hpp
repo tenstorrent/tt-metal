55c1c3a0536c72020897547a0b39507c501a78eb
Jaroslaw Bartosz Bieniek <jbbieniek@tenstorrent.com>
2025-08-27 11:32:07 +0000

    26065: multi-core

--- ttnn/cpp/ttnn/operations/experimental/isin/isin_cbs.hpp
+++ ttnn/cpp/ttnn/operations/experimental/isin/isin_cbs.hpp
@@ -15,8 +15,8 @@ using namespace tt;
 enum class IsInCB : std::underlying_type_t<CBIndex> {
     ELEMENTS = CBIndex::c_0,
     TEST_ELEMENTS = CBIndex::c_1,
-    INDEX_HINT = CBIndex::c_2,
-    OUTPUT = CBIndex::c_3
+    // INDEX_HINT = CBIndex::c_2,
+    OUTPUT = CBIndex::c_2
 };
 
 }  // namespace ttnn::operations::experimental::isin
