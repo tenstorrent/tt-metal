db7d40edeb3d48a396f8938c31c548a4ea415ecc
Brandon Rapanan <brapanan@tenstorrent.com>
2025-08-28 19:18:06 +0000

    Changed build flag for ttnn-op-runtime-predictor from --build-mlp-op-perf to --build-ttnn-op-runtime-predictor.

--- build_metal.sh
+++ build_metal.sh
@@ -51,6 +51,7 @@ show_help() {
     echo "  --without-distributed            Disable distributed compute support (OpenMPI dependency). Enabled by default."
     echo "  --without-python-bindings        Disable Python bindings (ttnncpp will be available as standalone library, otherwise ttnn will include the cpp backend and the python bindings), Enabled by default"
     echo "  --enable-fake-kernels-target     Enable fake kernels target, to enable generation of compile_commands.json for the kernels to enable IDE support."
+    echo "  --build-ttnn-op-runtime-predictor Build ttnn-op-runtime-predictor."
 }
 
 clean() {
@@ -85,7 +86,7 @@ cpm_source_cache=""
 c_compiler_path=""
 ttnn_shared_sub_libs="OFF"
 toolchain_path="cmake/x86_64-linux-clang-17-libstdcpp-toolchain.cmake"
-build_mlp_op_perf="OFF"
+build_ttnn_op_runtime_predictor="OFF"
 
 # Requested handling for 20.04 -> 22.04 migration
 if [[ "$FLAVOR" == "ubuntu" && "$VERSION" == "20.04" ]]; then
@@ -137,6 +138,7 @@ enable-coverage
 without-distributed
 without-python-bindings
 enable-fake-kernels-target
+build-ttnn-op-runtime-predictor
 "
 
 # Flatten LONGOPTIONS into a comma-separated string for getopt
@@ -220,8 +222,8 @@ while true; do
             build_type="Debug";;
         --clean)
 	    clean; exit 0;;
-        --build-mlp-op-perf)
-            build_mlp_op_perf="ON";;
+        --build-ttnn-op-runtime-predictor)
+            build_ttnn_op_runtime_predictor="ON";;
         --)
             shift;break;;
     esac
@@ -277,7 +279,7 @@ echo "INFO: TTNN Shared sub libs : $ttnn_shared_sub_libs"
 echo "INFO: Enable Light Metal Trace: $light_metal_trace"
 echo "INFO: Enable Distributed: $enable_distributed"
 echo "INFO: With python bindings: $with_python_bindings"
-echo "INFO: With submodule mlp-op-perf: $build_mlp_op_perf"
+echo "INFO: With cpm module ttnn-op-runtime-predictor: $build_ttnn_op_runtime_predictor"
 
 # Prepare cmake arguments
 cmake_args+=("-B" "$build_dir")
@@ -399,6 +401,10 @@ if [ "$enable_fake_kernels_target" = "ON" ]; then
     cmake_args+=("-DENABLE_FAKE_KERNELS_TARGET=ON")
 else
     cmake_args+=("-DENABLE_FAKE_KERNELS_TARGET=OFF")
+if [ "$build_ttnn_op_runtime_predictor" = "ON" ]; then
+    cmake_args+=("-DBUILD_TTNN_OP_RUNTIME_PREDICTOR=ON")
+else
+    cmake_args+=("-DBUILD_TTNN_OP_RUNTIME_PREDICTOR=OFF")
 fi
 
 # toolchain and cxx_compiler settings would conflict with eachother
