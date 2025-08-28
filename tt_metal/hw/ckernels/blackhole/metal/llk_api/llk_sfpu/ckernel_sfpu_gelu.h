299591d9e44a0124be6595bf42b4c6ff28c49d96
CodeMan62 <sharmahimanshu15082007@gmail.com>
2025-08-28 22:37:53 +0530

    slight changes

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_gelu.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_gelu.h
@@ -6,74 +6,24 @@
 
 #include "ckernel_defs.h"
 #include "ckernel.h"
-
+#include "llk_defs.h"
 namespace ckernel {
 namespace sfpu {
 
-#define POLYVAL15(c15, c14, c13, c12, c11, c10, c9, c8, c7, c6, c5, c4, c3, c2, c1, c0, x)                         \
-    (((((((((((((((c15) * (x) + (c14)) * (x) + (c13)) * (x) + (c12)) * (x) + (c11)) * (x) + (c10)) * (x) + (c9)) * \
-                (x) +                                                                                              \
-            (c8)) *                                                                                                \
-               (x) +                                                                                               \
-           (c7)) *                                                                                                 \
-              (x) +                                                                                                \
-          (c6)) *                                                                                                  \
-             (x) +                                                                                                 \
-         (c5)) *                                                                                                   \
-            (x) +                                                                                                  \
-        (c4)) *                                                                                                    \
-           (x) +                                                                                                   \
-       (c3)) *                                                                                                     \
-          (x) +                                                                                                    \
-      (c2)) *                                                                                                      \
-         (x) +                                                                                                     \
-     (c1)) * (x) +                                                                                                 \
-        (c0)
-
-inline sfpi::vFloat calculate_gelu_chebyshev(sfpi::vFloat val) {
-    sfpi::vFloat result = 0.0f;
-    v_if(val >= -5.5f) {
-        result = POLYVAL15(
-            -1.81205228163e-09,
-            -4.59055119276e-08,
-            -3.74540617693e-07,
-            -2.29754133825e-07,
-            1.19076782913e-05,
-            4.25116466215e-05,
-            -0.000138391838381,
-            -0.000862052441087,
-            0.000768340223025,
-            0.0092074331601,
-            -0.00208478037614,
-            -0.0656369476513,
-            0.00244542739174,
-            0.398579460781,
-            0.499174645395,
-            2.98325768482e-05,
-            val);
-
-        // Ensure result has the same sign as input using setsgn
-        result = setsgn(result, val);
-    }
-    v_endif;
-
-    return result;
-}
-
-template <bool APPROXIMATION_MODE>
+template <ApproximationMode APPROX_MODE>
 void gelu_init() {
-    _init_gelu_<APPROXIMATION_MODE>();
+    _init_gelu_<APPROX_MODE>();
 }
 
-template <bool APPROXIMATION_MODE>
+template <ApproximationMode APPROX_MODE>
 void gelu_derivative_init() {
-    _init_gelu_derivative_<APPROXIMATION_MODE>();
+    _init_gelu_derivative_<APPROX_MODE>();
 }
 
-template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
+template <ApproximationMode APPROX_MODE, int ITERATIONS = 8>
 inline void calculate_gelu() {
-    if constexpr (APPROXIMATION_MODE) {
-        _calculate_gelu_<APPROXIMATION_MODE, ITERATIONS>();
+    if constexpr (APPROX_MODE == ApproximationMode::Fast) {
+        _calculate_gelu_<APPROX_MODE, ITERATIONS>();
     } else {
 #pragma GCC unroll 8
     for (int d = 0; d < ITERATIONS; d++) {
@@ -87,9 +37,10 @@ inline void calculate_gelu() {
     }
     }
 }
-template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
+
+template <ApproximationMode APPROX_MODE, int ITERATIONS = 8>
 inline void calculate_gelu_derivative() {
-    _calculate_gelu_derivative_<APPROXIMATION_MODE, ITERATIONS>();
+    _calculate_gelu_derivative_<APPROX_MODE, ITERATIONS>();
 }
 
 }  // namespace sfpu
