d564b8970ffc944fefdc04aeb7b9fd0abd639e1b
Aswinmcw <azayasankaran@tenstorrent.com>
2025-08-28 12:42:23 +0000

    reuse

--- ttnn/cpp/ttnn/operations/eltwise/ternary/where/device/where_utils.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/ternary/where/device/where_utils.cpp
@@ -84,7 +84,6 @@ std::string get_kernel_file_path(KernelName kernel_name) {
         case KernelName::ComputeNoBcastTSS: return fmt::format(compute, root, "where_sfpu_no_bcast_tss.cpp");
         case KernelName::ComputeColBcastTTT:
             return "ttnn/cpp/ttnn/operations/eltwise/ternary/where/device/kernels/compute/where_sfpu_col_bcast_ttt.cpp";
-        case KernelName::ComputeRowBcastTTT: return fmt::format(compute, root, "where_sfpu_no_bcast_ttt.cpp");
         default: __builtin_unreachable();  // GCC 12 doesn't compile even though we exhaustively match
     }
 }
