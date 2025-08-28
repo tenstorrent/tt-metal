5691f5bc309624d3823c772c30c84b3ccbd5415e
Sankar Manoj <smanoj@tenstorrent.com>
2025-08-28 20:17:53 +0000

    #0: Config Tensors in DRAM for Halo and Conv2D ops

--- tests/ttnn/nightly/unit_tests/operations/conv/test_conv2d.py
+++ tests/ttnn/nightly/unit_tests/operations/conv/test_conv2d.py
@@ -125,6 +125,7 @@ def run_conv(
     sharded_cfg=None,
     throttle_level=ttnn.ThrottleLevel.NO_THROTTLE,
     enable_activation_reuse=False,
+    config_tensors_in_dram=False,
 ):
     if isinstance(device, ttnn.MeshDevice) and len(device.get_device_ids()) > 1:
         assert input_mesh_mapper is not None, "Expected mesh mapper for input tensor when running on multiple devices"
@@ -254,6 +255,7 @@ def run_conv(
         enable_kernel_stride_folding=enable_kernel_stride_folding,
         full_inner_dim=bs_full_inner_dim,
         enable_activation_reuse=enable_activation_reuse,
+        config_tensors_in_dram=config_tensors_in_dram,
     )
 
     compute_config = ttnn.init_device_compute_kernel_config(
