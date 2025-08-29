9da300965154ac71dd3d8a86e1dcebe2dfe8eb90
Nikhil Soraba <nsoraba@tenstorrent.com>
2025-08-29 05:31:37 +0000

    Revert "Revert "Port normalization and reduction kernels to TensorAccessor (#26916)""
    This reverts commit c9a704fabc3e10855deea8a2223487ceaa6be457.

--- ttnn/cpp/ttnn/operations/normalization/batch_norm/device/kernels/dataflow/reader_running_statistics.cpp
+++ ttnn/cpp/ttnn/operations/normalization/batch_norm/device/kernels/dataflow/reader_running_statistics.cpp
@@ -19,17 +19,14 @@ void kernel_main() {
     uint32_t N = get_arg_val<uint32_t>(7);
     uint32_t C = get_arg_val<uint32_t>(8);
 
-    constexpr bool src_is_dram = get_compile_time_arg_val(0) == 1;
-
-    constexpr auto cb_id_src = get_compile_time_arg_val(1);
-    constexpr auto cb_id_momentum = get_compile_time_arg_val(2);
-    constexpr auto cb_id_one = get_compile_time_arg_val(3);
+    constexpr auto cb_id_src = get_compile_time_arg_val(0);
+    constexpr auto cb_id_momentum = get_compile_time_arg_val(1);
+    constexpr auto cb_id_one = get_compile_time_arg_val(2);
+    constexpr auto src_args = TensorAccessorArgs<3>();
     constexpr uint32_t onetile = 1;
 
     const uint32_t src_tile_bytes = get_tile_size(cb_id_src);
-    const DataFormat src_data_format = get_dataformat(cb_id_src);
-    const InterleavedAddrGenFast<src_is_dram> src = {
-        .bank_base_address = src_addr, .page_size = src_tile_bytes, .data_format = src_data_format};
+    const auto src = TensorAccessor(src_args, src_addr, src_tile_bytes);
 
     uint32_t tiles_per_batch = HtWt * C;
     uint32_t start_n = start_tile_id / tiles_per_batch;
