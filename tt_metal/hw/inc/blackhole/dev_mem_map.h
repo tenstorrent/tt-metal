f34fc81cf24421c220b0d8255f21b036968390bf
Austin Ho <109362939+tt-aho@users.noreply.github.com>
2025-08-29 02:52:35 -0400

    #0: Remove remaining hardcoding of eth addresses and direct reads from L1 in control plane (#27588)
    ### Ticket
    Link to Github Issue
    
    ### Problem description
    A number of eth addresses/values were added in control plane for
    querying local/remote asic id and connected remote channel id. These can
    be removed since this can be queried from UMD.
    
    ### What's changed
    Remove hardcoded addresses/queries from control plane and query UMD
    instead.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/17288250650
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
    CI passes (if applicable)
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [x] New/Existing tests provide coverage for changes
    Multi-Host:
    https://github.com/tenstorrent/tt-metal/actions/runs/17288294489
    Galaxy Quick:
    https://github.com/tenstorrent/tt-metal/actions/runs/17288264417
    T3K Unit:
    https://github.com/tenstorrent/tt-metal/actions/runs/17288257440

--- tt_metal/hw/inc/blackhole/dev_mem_map.h
+++ tt_metal/hw/inc/blackhole/dev_mem_map.h
@@ -183,9 +183,6 @@
 // Common Misc
 #define MEM_RETRAIN_COUNT_ADDR 0x7CC10
 #define MEM_RETRAIN_FORCE_ADDR 0x1EFC
-// These values are taken from WH, These may need to be updated when BH needs to support
-// intermesh routing.
-#define MEM_ETH_LINK_REMOTE_INFO_ADDR 0x1EC0
 
 #define MEM_SYSENG_ETH_RESULTS_BASE_ADDR 0x7CC00
 #define MEM_SYSENG_ETH_MAILBOX_BASE_ADDR 0x7D000
