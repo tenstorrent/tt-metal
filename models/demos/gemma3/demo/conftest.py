cd8c5ecafab667e6daef382f7861ca11c9085b13
handrewsTT <handrews@tenstorrent.com>
2025-08-29 07:20:02 +0000

    comment other tests and make mounts rw

--- models/demos/gemma3/demo/conftest.py
+++ models/demos/gemma3/demo/conftest.py
@@ -1,12 +1,6 @@
 # SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
 
 # SPDX-License-Identifier: Apache-2.0
-
-import os
-
-import pytest
-
-import ttnn
 from models.demos.gemma3.tt.model_config import parse_optimizations
 
 
@@ -63,24 +57,3 @@ def pytest_addoption(parser):
         dest="enable_trace",
         help="Whether to enable tracing",
     )
-
-
-@pytest.fixture
-def device_params(request, galaxy_type):
-    # Get param dict passed in from test parametrize (or default to empty dict)
-    params = getattr(request, "param", {}).copy()
-
-    mesh_device = {"N150": (1, 1), "N300": (1, 2), "N150x4": (1, 4), "T3K": (1, 8), "TG": (8, 4)}.get(
-        os.environ.get("MESH_DEVICE"), len(ttnn.get_device_ids())
-    )
-    is_single_device = (mesh_device == (1, 1)) if isinstance(mesh_device, tuple) else (mesh_device == 1)
-
-    if "fabric_config" in params:
-        if is_single_device:
-            params["fabric_config"] = None
-        elif params["fabric_config"] == True:
-            params["fabric_config"] = (
-                ttnn.FabricConfig.FABRIC_1D_RING if galaxy_type == "6U" else ttnn.FabricConfig.FABRIC_1D
-            )
-
-    return params
