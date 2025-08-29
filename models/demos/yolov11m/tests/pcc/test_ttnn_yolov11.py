3654aa9b5168db77a10f944ecfd8e0355ec32841
Dimitri Gnidash <dimitri@tenstorrent.com>
2025-08-29 15:52:23 -0400

    #27629: Update the tests to use the medium size model

--- models/demos/yolov11m/tests/pcc/test_ttnn_yolov11.py
+++ models/demos/yolov11m/tests/pcc/test_ttnn_yolov11.py
@@ -5,10 +5,10 @@
 import pytest
 
 import ttnn
-from models.demos.yolov11.common import YOLOV11_L1_SMALL_SIZE, load_torch_model
-from models.demos.yolov11.reference import yolov11
-from models.demos.yolov11.tt import ttnn_yolov11
-from models.demos.yolov11.tt.model_preprocessing import create_yolov11_input_tensors, create_yolov11_model_parameters
+from models.demos.yolov11m.common import YOLOV11_L1_SMALL_SIZE, load_torch_model
+from models.demos.yolov11m.reference import yolov11
+from models.demos.yolov11m.tt import ttnn_yolov11
+from models.demos.yolov11m.tt.model_preprocessing import create_yolov11_input_tensors, create_yolov11_model_parameters
 from tests.ttnn.utils_for_testing import assert_with_pcc
 
 
