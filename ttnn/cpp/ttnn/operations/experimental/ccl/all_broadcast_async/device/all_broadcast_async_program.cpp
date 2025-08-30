589dd3fff48a8654a482b0a9b0358ee690a83040
Grayson Rechsteiner <grechsteiner@tenstorrent.com>
2025-08-30 17:54:09 -0400

    Port AG Command Processor to Separate Op (#27550)
    ### Ticket
    [Link to Github Issue
    ](https://github.com/tenstorrent/tt-metal/issues/27180)
    
    ### Problem description
    - The old command processor implementation of all gather is currently
    embedded inside of `all_gather_async`
    - We don't plan on adding any functionality to this implementation (or
    patching any bugs in it) at any point in the future, and would like to
    decouple it from the primary implementation
    - Primarily because if people call `all_gather_async` with a single
    semaphore they'll be multiplexed into the command processor
    implementation, instead of the newer minimal implementation which
    requires 2 semaphores
    - We can't completely delete the implementation, as it's still needed by
    moreh and `all_reduce_async`
    
    ### What's changed
    - Move the command processor implementation into it's own op
    - Update existing tests and `all_reduce_async` appropriately to call the
    "new" op
    
    ### Pipelines
    - APC: https://github.com/tenstorrent/tt-metal/actions/runs/17256224012
    - T3K Nightly:
    https://github.com/tenstorrent/tt-metal/actions/runs/17256369119
    - T3K Frequent:
    https://github.com/tenstorrent/tt-metal/actions/runs/17278676321
    - T3K Unit:
    https://github.com/tenstorrent/tt-metal/actions/runs/17278683828
    
    - NOTE: TG runners are down atm so can't run the relevant TG pipelines.
    We'll wait for runners to come back up and for the relevant pipelines to
    pass before merging.
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
    CI passes (if applicable)
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_broadcast_async/device/all_broadcast_async_program.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_broadcast_async/device/all_broadcast_async_program.cpp
@@ -9,7 +9,6 @@
 #include <tt-metalium/fabric.hpp>
 #include "ttnn/tensor/tensor_impl.hpp"
 #include "ttnn/operations/experimental/ccl/all_broadcast_async/device/all_broadcast_async_op.hpp"
-#include "ttnn/operations/experimental/ccl/all_gather_async/device/all_gather_async_op.hpp"
 #include "ttnn/operations/ccl/shared_with_host/hetergeneous_data_structs.hpp"
 #include "ttnn/operations/ccl/ccl_host_datastructures.hpp"
 #include "ttnn/operations/ccl/ccl_common.hpp"
