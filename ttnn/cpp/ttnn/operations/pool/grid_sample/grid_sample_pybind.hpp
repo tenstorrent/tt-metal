aeba81c6b3ed005c873862ce9fce00071551ba17
Marko Staletovic <mstaletovic@tenstorrent.com>
2025-09-01 13:58:23 +0200

    Added support for a precomputed grid in grid sample (#27485)
    ### Problem description
    
    Computing the bilinear interpolaton weights and coordinates for the grid
    sample operation is expensive. If the grid is known prior to the
    execution, it is possible to precompute it and save expensive floating
    point operations on risc.
    
    ### What's changed
    - Added support for the usage of a precomputed grid in the grid sample
    op
    - Added ttnn.prepare_grid_sample_grid, that takes in a ttnn tensor on
    host and converts it to an appropriate format. Example usage can be seen
    in tests/ttnn/nightly/unit_tests/operations/pool/test_grid_sample.py
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/17132216197)
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/17132220518)
    - [x] [L2
    Nightly](https://github.com/tenstorrent/tt-metal/actions/runs/17132249139)

--- ttnn/cpp/ttnn/operations/pool/grid_sample/grid_sample_pybind.hpp
+++ ttnn/cpp/ttnn/operations/pool/grid_sample/grid_sample_pybind.hpp
@@ -10,4 +10,9 @@ namespace ttnn::operations::grid_sample {
 
 void py_bind_grid_sample(pybind11::module& module);
 
+namespace detail {
+void bind_grid_sample(pybind11::module& module);
+void bind_prepare_grid_sample_grid(pybind11::module& module);
+}  // namespace detail
+
 }  // namespace ttnn::operations::grid_sample
