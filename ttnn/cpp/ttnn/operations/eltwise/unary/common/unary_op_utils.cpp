4fb66d466c39d2cf4a969e35e0fd29ddf7da4c23
Mouliraj Elamurugan <ctr-mcw-melamurugan@ext.tenstorrent.com>
2025-09-01 17:17:57 +0530

    #27149: Add uint16 support for square and squared_difference (#27585)
    ### Ticket
    Link to Github Issue #27149.
    
    ### What's changed
    
    - Added uint16 support for square op.
    - Added uint16 support for squared difference
    
    ### Checklist
    - [ ] All post commit
    - [ ] Blackhole Post commit

--- ttnn/cpp/ttnn/operations/eltwise/unary/common/unary_op_utils.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/unary/common/unary_op_utils.cpp
@@ -550,6 +550,8 @@ std::pair<std::string, std::string> get_op_init_and_func_default(
                 input_dtype.has_value(), "Missing input dtype: Expected a valid input dtype, but none was provided.");
             if (input_dtype.value() == DataType::INT32) {
                 op_init_and_name = {"mul_int32_tile_init();", fmt::format("mul_int32_tile({0}, {0}, {0});", idst)};
+            } else if (input_dtype.value() == DataType::UINT16) {
+                op_init_and_name = {"mul_int_tile_init();", fmt::format("mul_uint16_tile({0}, {0}, {0});", idst)};
             } else {
                 op_init_and_name = {"square_tile_init();", fmt::format("square_tile({});", idst)};
             }
