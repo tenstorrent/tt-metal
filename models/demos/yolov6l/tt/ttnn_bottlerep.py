2c070c912cb9a00c07074ee765f41cdff8748320
punith.sekar <punith.sekar@multicorewareinc.com>
2025-09-01 07:14:23 +0000

    Add signpost

--- models/demos/yolov6l/tt/ttnn_bottlerep.py
+++ models/demos/yolov6l/tt/ttnn_bottlerep.py
@@ -5,6 +5,14 @@
 import ttnn
 from models.demos.yolov6l.tt.common import Yolov6l_Conv2D
 
+try:
+    from tracy import signpost
+
+    use_signpost = True
+
+except ModuleNotFoundError:
+    use_signpost = False
+
 
 class TtBottleRep:
     def __init__(self, device, parameters, model_params, shard_layout=ttnn.TensorMemoryLayout.HEIGHT_SHARDED):
@@ -31,6 +39,8 @@ class TtBottleRep:
         )
 
     def __call__(self, x):
+        if use_signpost:
+            signpost(header="TtBottleRep Start")
         x_conv1 = self.cv1(x)
         x_conv2, out_h, out_w = self.cv2(x_conv1)
 
@@ -49,4 +59,6 @@ class TtBottleRep:
 
         ttnn.deallocate(x_conv2)
         ttnn.deallocate(x)
+        if use_signpost:
+            signpost(header="TtBottleRep End")
         return output, out_h, out_w
