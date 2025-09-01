a4bc951241a504d1cbaffb4ff3cc156007d11f43
Aleksandar Stancov <astancov@tenstorrent.com>
2025-09-01 09:21:58 +0200

    Stable diffusion L1_small size in tests unification (#27662)
    ### Problem description
    Stable diffusion model has L1_small size which differs from test to
    test.
    Value for L1_Small needs to be unified
    
    ### What's changed
    - added L1_Small variable and used it across the stable_diffusion
    codebase
    ### Checklist
    - [X] [(Single-card) Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/fast-dispatch-full-regressions-and-models.yaml)
    CI [stable_diffusion
    passes](https://github.com/tenstorrent/tt-metal/actions/runs/17317980956)

--- models/demos/wormhole/stable_diffusion/demo/demo.py
+++ models/demos/wormhole/stable_diffusion/demo/demo.py
@@ -19,6 +19,7 @@ from transformers import CLIPTextModel, CLIPTokenizer
 from ttnn.model_preprocessing import preprocess_model_parameters
 
 import ttnn
+from models.demos.wormhole.stable_diffusion.common import SD_L1_SMALL_SIZE
 from models.demos.wormhole.stable_diffusion.custom_preprocessing import custom_preprocessor
 from models.demos.wormhole.stable_diffusion.sd_helper_funcs import compile_trace_sd
 from models.demos.wormhole.stable_diffusion.sd_pndm_scheduler import TtPNDMScheduler
@@ -521,7 +522,7 @@ def run_demo_inference_diffusiondb(
 
 @pytest.mark.parametrize(
     "device_params",
-    [{"l1_small_size": 11 * 8192, "trace_region_size": 789835776}],
+    [{"l1_small_size": SD_L1_SMALL_SIZE, "trace_region_size": 789835776}],
     indirect=True,
 )
 @pytest.mark.parametrize(
