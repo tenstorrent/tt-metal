4b59f383fbf6f5b6e2586306ed6cc3aaa9c3995d
Ridvan Song <56486612+Riddy21@users.noreply.github.com>
2025-09-02 18:13:02 -0400

    MGD 2.0 Protobuf Infrastructure (#27331)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/26480)
    
    ### Problem description
    Transitioning to MGD 2.0:
    https://docs.google.com/document/d/1291H1Wl_pSkIGHP9B_L6oikaD3MflAGXg3Lox1O8S0c/edit?tab=t.0
    
    ### What's changed
    - Added infra and dependencies for protobuf
    - Setup initial infrastructure for MGD 2.0
    
    ### Checklist
    - [ ]
    [APC](https://github.com/tenstorrent/tt-metal/actions/runs/17312286317)
    
    ---------
    
    Co-authored-by: Riddy21 <rsong@g14glx03.tenstorrent.net>

---
+++ tt_metal/fabric/mesh_graph_descriptor.cpp
@@ -0,0 +1,54 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdexcept>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+#include <algorithm>
+#include "assert.hpp"
+
+#include "tt-metalium/mesh_graph_descriptor.hpp"
+#include "protobuf/mesh_graph_descriptor.pb.h"
+
+#include <google/protobuf/text_format.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+
+namespace tt::tt_fabric {
+
+namespace {
+
+std::string read_file_to_string(const std::filesystem::path &file_path) {
+    std::ifstream input(file_path);
+    if (!input.is_open()) {
+        throw std::runtime_error("Failed to open file: " + file_path.string());
+    }
+    std::ostringstream buffer;
+    buffer << input.rdbuf();
+    return buffer.str();
+}
+
+}  // namespace
+
+MeshGraphDescriptor::MeshGraphDescriptor(const std::string& text_proto) {
+    proto::MeshGraphDescriptor temp_proto;
+    google::protobuf::TextFormat::Parser parser;
+
+    // Allowing for back and forward compatibility for fields not currently in the proto file
+    parser.AllowUnknownField(true);
+    parser.AllowUnknownExtension(true);
+
+    TT_FATAL(parser.ParseFromString(text_proto, &temp_proto), "Failed to parse MeshGraphDescriptor textproto");
+
+    // TODO: Add validation here
+
+    proto_ = std::make_unique<proto::MeshGraphDescriptor>(temp_proto);
+}
+
+MeshGraphDescriptor::MeshGraphDescriptor(const std::filesystem::path& text_proto_file_path) :
+    MeshGraphDescriptor(read_file_to_string(text_proto_file_path.string())) {}
+
+MeshGraphDescriptor::~MeshGraphDescriptor() = default;
+
+}  // namespace tt::tt_fabric
