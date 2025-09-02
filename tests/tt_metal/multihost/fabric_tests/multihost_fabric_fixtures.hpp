c878c063ba888601228f76cc713a48b99693802a
asaigal <asaigal@tenstorrent.com>
2025-09-02 20:56:33 +0000

    Remove eth coord hardcoding in fixture

--- tests/tt_metal/multihost/fabric_tests/multihost_fabric_fixtures.hpp
+++ tests/tt_metal/multihost/fabric_tests/multihost_fabric_fixtures.hpp
@@ -20,10 +20,10 @@ void validate_and_setup_control_plane_config(Fixture* fixture) {
     const char* mesh_id_str = std::getenv("TT_MESH_ID");
     TT_FATAL(mesh_id_str != nullptr, "TT_MESH_ID environment variable must be set for Multi-Host Fabric Tests.");
 
-    auto chip_to_eth_coord_mapping = multihost_utils::get_physical_chip_mapping_from_eth_coords_mapping(
-        fixture->get_eth_coord_mapping(), std::stoi(mesh_id_str));
-    tt::tt_metal::MetalContext::instance().set_custom_fabric_topology(
-        fixture->get_path_to_mesh_graph_desc(), chip_to_eth_coord_mapping);
+    // auto chip_to_eth_coord_mapping = multihost_utils::get_physical_chip_mapping_from_eth_coords_mapping(
+    //     fixture->get_eth_coord_mapping(), std::stoi(mesh_id_str));
+    // tt::tt_metal::MetalContext::instance().set_custom_fabric_topology(
+    //     fixture->get_path_to_mesh_graph_desc(), chip_to_eth_coord_mapping);
     TT_FATAL(
         tt::tt_metal::MetalContext::instance().get_control_plane().system_has_intermesh_links(),
         "Multi-Host Routing tests require ethernet links to a remote host.");
