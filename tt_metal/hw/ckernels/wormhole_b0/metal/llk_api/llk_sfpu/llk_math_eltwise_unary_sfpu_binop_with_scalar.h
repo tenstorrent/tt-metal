0d1899a38c0e8eca8e9f3392cb406bb44433e973
mouliraj-mcw <ctr-mcw-melamurugan@ext.tenstorrent.com>
2025-09-02 10:49:19 +0000

    #27152: Add int32 support for add_sfpu

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_binop_with_scalar.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_binop_with_scalar.h
@@ -14,14 +14,21 @@ namespace ckernel {
 
 template <bool APPROXIMATE, int binop_mode>
 inline void llk_math_eltwise_unary_sfpu_binop_with_scalar(
-    uint dst_index, uint32_t param1, int vector_mode = VectorMode::RC) {
+    uint dst_index, uint32_t scalar, int vector_mode = VectorMode::RC) {
     _llk_math_eltwise_unary_sfpu_params_<APPROXIMATE>(
-        ckernel::sfpu::calculate_binop_with_scalar<APPROXIMATE, binop_mode, 8>, dst_index, vector_mode, param1);
+        ckernel::sfpu::calculate_binop_with_scalar<APPROXIMATE, binop_mode, 8>, dst_index, vector_mode, scalar);
 }
 
 template <bool APPROXIMATE>
 inline void llk_math_eltwise_unary_sfpu_binop_with_scalar_init() {
     llk_math_eltwise_unary_sfpu_init<SfpuType::unused, APPROXIMATE>();
+}
+
+template <bool APPROXIMATE, int ITERATIONS = 8>
+inline void llk_math_eltwise_unary_sfpu_binop_with_scalar_add_int32(
+    uint dst_index, uint32_t scalar, int vector_mode = VectorMode::RC) {
+    _llk_math_eltwise_unary_sfpu_params_<APPROXIMATE>(
+        ckernel::sfpu::calculate_add_int32<APPROXIMATE, ITERATIONS>, dst_index, vector_mode, scalar);
 }
 
 }  // namespace ckernel
