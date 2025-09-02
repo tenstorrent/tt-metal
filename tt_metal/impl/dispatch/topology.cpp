e8ac73eedbccedccf4b33b3b6448160f4fc90f30
Yu Gao <145494740+yugaoTT@users.noreply.github.com>
2025-09-02 11:16:30 -0400

    skipping unused sender channels in fabric tensix mode (#27390)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/26792)
    
    ### Problem description
    currently we still keep all the sender channels (for 1d s0, s1, s2, for
    2d E W N S) in fabric router, even though the tensix extension already
    moved all the sender channels there. We need to properly skip the fabric
    router sender channels and set those channels to 0 buffer slots.
    
    ### What's changed
    for 1D, skip the s1/s2 channels in router, since those are moved to
    tensix extension, and only keep s0 there for mux connection.
    for 2D, skip the non-worker sender channels (worker channel is the
    channel for current direction), and keep only the worker channel on
    router for mux to connect.
    Fix a bug in fabric router, where it checks sender channel 0 to see if
    all the sender channels have been serviced. this is not true for 2D
    case, and should use the direction to index into the is_channel_serviced
    array.
    
    fill in reserved L1 with dispatch/worker/tensix connection info,
    dispatch connection info is used by dispatch (relay mux) to connect to
    the fabric router
    worker connection info is used by workers to connect to either mux or
    fabric router
    tensix connection info is used by tensix(mux) to connect to the fabric
    router
    
    <img width="979" height="344" alt="Screenshot 2025-08-29 at 3 33 29 PM"
    src="https://github.com/user-attachments/assets/7be95883-ff03-4711-a9a8-286c793ae912"
    />
    
    ### Checklist
    - [ ] [All post commit]
    https://github.com/tenstorrent/tt-metal/actions/runs/17406693823
    - [x] galaxy quick
    https://github.com/tenstorrent/tt-metal/actions/runs/17405375209
    - [x] t3k unit
    https://github.com/tenstorrent/tt-metal/actions/runs/17406418281

--- tt_metal/impl/dispatch/topology.cpp
+++ tt_metal/impl/dispatch/topology.cpp
@@ -1008,6 +1008,11 @@ void build_tt_fabric_program(
         return (tunnels_from_mmio.size() - 1) > 0;
     }();
 
+    auto is_dispatch_link = [&](auto eth_chan, uint32_t dispatch_link_idx) {
+        auto link_idx = control_plane.get_routing_plane_id(fabric_node_id, eth_chan);
+        return device_has_dispatch_tunnel && link_idx == dispatch_link_idx;
+    };
+
     for (const auto& direction : tt::tt_fabric::FabricContext::routing_directions) {
         auto active_eth_chans =
             control_plane.get_active_fabric_eth_routing_planes_in_direction(fabric_node_id, direction);
@@ -1074,15 +1079,34 @@ void build_tt_fabric_program(
             remote_fabric_node_id.chip_id,
             wrap_around_mesh);
 
-        const auto& curr_edm_config = fabric_context.get_fabric_router_config(fabric_edm_type, fabric_edm_axis);
-
         // Create fabric tensix builder for this ethernet channel
         // Skip the link used by dispatch using relay mux API
         uint32_t dispatch_link_idx = RelayMux::get_dispatch_link_index(fabric_node_id, remote_fabric_node_id, device);
 
+        auto get_fabric_router_config =
+            [&](bool fabric_tensix_extension_enabled, bool is_dispatch_link, auto eth_direction) {
+                auto fabric_tensix_config = tt::tt_fabric::FabricTensixConfig::DISABLED;
+                // if not the link used by dispatch, get the fabric router config with tensix extension.
+                if (fabric_tensix_extension_enabled && !is_dispatch_link) {
+                    fabric_tensix_config = tt::tt_fabric::FabricTensixConfig::MUX;
+                }
+                return fabric_context.get_fabric_router_config(
+                    fabric_edm_type, fabric_edm_axis, fabric_tensix_config, eth_direction);
+            };
+
         for (const auto& eth_chan : active_fabric_eth_channels[direction]) {
             auto eth_direction = control_plane.routing_direction_to_eth_direction(direction);
             auto eth_logical_core = soc_desc.get_eth_core_for_channel(eth_chan, CoordSystem::LOGICAL);
+
+            bool dispatch_link = is_dispatch_link(eth_chan, dispatch_link_idx);
+            const auto& curr_edm_config =
+                get_fabric_router_config(fabric_tensix_extension_enabled, dispatch_link, eth_direction);
+
+            bool has_tensix_extension = false;
+            if (fabric_tensix_extension_enabled && !dispatch_link) {
+                has_tensix_extension = true;
+            }
+
             auto edm_builder = tt::tt_fabric::FabricEriscDatamoverBuilder::build(
                 device,
                 *fabric_program_ptr,
@@ -1092,13 +1116,13 @@ void build_tt_fabric_program(
                 curr_edm_config,
                 false, /* build_in_worker_connection_mode */
                 fabric_edm_type,
-                eth_direction);
+                eth_direction,
+                has_tensix_extension);
             edm_builders.insert({eth_chan, edm_builder});
 
-            auto link_idx = control_plane.get_routing_plane_id(fabric_node_id, eth_chan);
             if (fabric_tensix_extension_enabled) {
                 // Only create tensix builder if this channel is not used by dispatch
-                if (!(device_has_dispatch_tunnel && link_idx == dispatch_link_idx)) {
+                if (!dispatch_link) {
                     auto tensix_builder = tt::tt_fabric::FabricTensixDatamoverBuilder::build(
                         device, *fabric_program_ptr, fabric_node_id, remote_fabric_node_id, eth_chan, eth_direction);
                     tensix_builders.insert({eth_chan, tensix_builder});
@@ -1129,8 +1153,14 @@ void build_tt_fabric_program(
                 auto& tensix_builder1 = tensix_builders.at(eth_chan_dir1);
                 auto& tensix_builder2 = tensix_builders.at(eth_chan_dir2);
 
-                edm_builder1.connect_to_downstream_edm(tensix_builder2);
-                edm_builder2.connect_to_downstream_edm(tensix_builder1);
+                // need to also pass in edm_builder because it is used to build vc1 connection
+                edm_builder1.connect_to_downstream_edm(tensix_builder2, edm_builder2);
+                edm_builder2.connect_to_downstream_edm(tensix_builder1, edm_builder1);
+            } else {
+                // build the downstream connection for the eth channels without tensix extension (dispatch routing
+                // plane)
+                edm_builder1.connect_to_downstream_edm(edm_builder2);
+                edm_builder2.connect_to_downstream_edm(edm_builder1);
             }
         } else {
             edm_builder1.connect_to_downstream_edm(edm_builder2);
