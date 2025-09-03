8b78f4af6fefc503436e59d334619da7a2977735
Bryan Wilder Field Lozano <blozano@tenstorrent.com>
2025-09-03 15:19:22 -0700

    Apply suggestions from code review

--- ttnn/cpp/ttnn/operations/eltwise/unary_backward/unary_backward.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/unary_backward/unary_backward.cpp
@@ -18,8 +18,6 @@
 #include "ttnn/operations/creation.hpp"
 #include "ttnn/operations/eltwise/complex/complex.hpp"
 #include "ttnn/operations/eltwise/unary_backward/unary_backward.hpp"
-
-#include <cmath>
 #include "ttnn/operations/eltwise/complex_unary/complex_unary.hpp"
 #include "ttnn/operations/eltwise/complex_binary/device/complex_binary_op.hpp"
 #include "ttnn/operations/reduction/generic/generic_reductions.hpp"
