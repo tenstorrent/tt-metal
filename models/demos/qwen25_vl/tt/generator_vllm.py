da8c0ee37e3fe2e058cc5a70823cc35d91df8f3b
ssanjay <ssanjay@tenstorrent.com>
2025-09-03 21:47:21 +0000

    linting

--- models/demos/qwen25_vl/tt/generator_vllm.py
+++ models/demos/qwen25_vl/tt/generator_vllm.py
@@ -172,8 +172,6 @@ class Qwen2_5_VLForConditionalGeneration(QwenVLGenerator, SupportsMultiModal):
         # reconstruct the inputs that Qwen2.5-VL expects
         inputs = CustomNamespace()
         inputs.input_ids = tokens.to(images[0].attention_mask.dtype)
-        inputs.pixel_values = torch.concat([im.pixel_values for im in images], dim=0)
-        logger.info(f"inputs.pixel_values: {inputs.pixel_values.shape}")
         inputs.attention_mask = torch.concat(
             [
                 torch.nn.functional.pad(im.attention_mask, (0, padded_seq_len - im.attention_mask.shape[-1]), value=0)
@@ -182,17 +180,19 @@ class Qwen2_5_VLForConditionalGeneration(QwenVLGenerator, SupportsMultiModal):
             dim=0,
         )
         logger.info(f"inputs.attention_mask: {inputs.attention_mask.shape}")
-        inputs.image_grid_thw = torch.concat([im.image_grid_thw for im in images], dim=0)
+        if "pixel_values" in images[0]:
+            # we currently do not support mixed inputs of text-only users and text-image users; hence checking images[0] is enough
+            inputs.pixel_values = torch.concat([im.pixel_values for im in images], dim=0)
+            inputs.image_grid_thw = torch.concat([im.image_grid_thw for im in images], dim=0)
 
-        time_input_reconstruction = time.time() - start_step
-        logger.info(f"Input reconstruction time: {time_input_reconstruction:.4f}s")
-
-        # Vision prefill
-        start_step = time.time()
-        image_embeds = self.visual_model(inputs.pixel_values, grid_thw=inputs.image_grid_thw)
-        time_vision_prefill = time.time() - start_step
-        logger.info(f"image_embeds: {image_embeds.shape}")
-        logger.info(f"Vision prefill time: {time_vision_prefill:.4f}s")
+            start_step = time.time()
+            image_embeds = self.visual_model(inputs.pixel_values, grid_thw=inputs.image_grid_thw)
+            time_vision_prefill = time.time() - start_step
+            logger.info(f"image_embeds: {image_embeds.shape}")
+            logger.info(f"Vision prefill time: {time_vision_prefill:.4f}s")
+        else:
+            # text-only users
+            image_embeds = torch.tensor([], dtype=torch.bfloat16)  # [INFO] empty tensor
 
         # Prepare text + vision inputs for decoder model
         start_step = time.time()
