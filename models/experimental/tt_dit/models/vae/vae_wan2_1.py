c464b434c709c13a5f1342da04d38c89002104ee
Colman Glagovich <cglagovich@tenstorrent.com>
2025-09-03 21:14:49 +0000

    Wan VAE RMSNorm, Attention, Conv3d, and ResidualBlock complete

---
+++ models/experimental/tt_dit/models/vae/vae_wan2_1.py
@@ -0,0 +1,350 @@
+import torch
+import ttnn
+from loguru import logger
+from ...layers.normalization import RMSNorm
+from ...layers.linear import Linear
+from ...utils.conv3d import _ntuple, get_conv3d_config, prepare_conv3d_weights
+from ...utils.substate import substate
+
+CACHE_T = 2
+
+
+class WanAttentionBlock:
+    def __init__(
+        self,
+        dim,
+        mesh_device,
+    ):
+        self.dim = dim
+        self.mesh_device = mesh_device
+
+        self.norm = RMSNorm(
+            embedding_dim=dim,
+            norm_eps=1e-6,
+            norm_elementwise_affine=True,
+            bias=False,
+            mesh_device=mesh_device,
+        )
+        self.to_qkv = Linear(
+            in_features=dim,
+            out_features=dim * 3,
+            mesh_device=mesh_device,
+        )
+        self.proj = Linear(
+            in_features=dim,
+            out_features=dim,
+            mesh_device=mesh_device,
+        )
+
+        self.sdpa_compute_kernel_config = ttnn.init_device_compute_kernel_config(
+            self.mesh_device.arch(),
+            math_fidelity=ttnn.MathFidelity.HiFi2,
+            math_approx_mode=False,
+            fp32_dest_acc_en=True,
+        )
+        self.sdpa_program_config = ttnn.SDPAProgramConfig(
+            compute_with_storage_grid_size=self.mesh_device.compute_with_storage_grid_size(),
+            q_chunk_size=32,
+            k_chunk_size=256,
+            exp_approx_mode=False,  # NOTE: False is more correct
+        )
+        self.hifi4_compute_kernel_config = ttnn.init_device_compute_kernel_config(
+            self.mesh_device.arch(),
+            math_fidelity=ttnn.MathFidelity.HiFi4,
+            math_approx_mode=False,
+            fp32_dest_acc_en=True,
+            packer_l1_acc=True,
+        )
+        device_grid = self.mesh_device.compute_with_storage_grid_size()
+        self.core_grid = ttnn.CoreGrid(x=device_grid.x, y=device_grid.y)
+
+    def load_state_dict(self, state_dict):
+        def permute_conv2d_weights(weight):
+            out_c, in_c, kh, kw = weight.shape
+            assert kh == kw == 1
+            weight = weight.permute(0, 2, 3, 1).reshape(out_c, in_c)
+            return weight
+
+        self.to_qkv.load_state_dict(
+            {
+                "weight": permute_conv2d_weights(state_dict["to_qkv.weight"]),
+                "bias": state_dict["to_qkv.bias"],
+            }
+        )
+        self.proj.load_state_dict(
+            {
+                "weight": permute_conv2d_weights(state_dict["proj.weight"]),
+                "bias": state_dict["proj.bias"],
+            }
+        )
+
+        self.norm.load_state_dict(
+            {
+                "weight": state_dict["norm.gamma"].squeeze(),
+            }
+        )
+
+    def __call__(self, x_BTHWC):
+        assert len(x_BTHWC.shape) == 5
+        assert x_BTHWC.layout == ttnn.ROW_MAJOR_LAYOUT
+        residual_BTHWC = x_BTHWC
+        B, T, H, W, C = x_BTHWC.shape
+        x_TNC = ttnn.reshape(x_BTHWC, (B * T, H * W, C))
+        x_TNC = ttnn.to_layout(x_TNC, ttnn.TILE_LAYOUT)
+        x_TNC = self.norm(x_TNC, compute_kernel_config=self.hifi4_compute_kernel_config)
+        x_TND = self.to_qkv(x_TNC, compute_kernel_config=self.hifi4_compute_kernel_config, core_grid=self.core_grid)
+        q_THNC, k_THNC, v_THNC = ttnn.transformer.split_query_key_value_and_split_heads(
+            x_TND, num_heads=1, transpose_key=False
+        )
+        out_THNC = ttnn.transformer.scaled_dot_product_attention(
+            q_THNC,
+            k_THNC,
+            v_THNC,
+            is_causal=False,
+            program_config=self.sdpa_program_config,
+            compute_kernel_config=self.sdpa_compute_kernel_config,
+        )
+
+        out_TNC = ttnn.transformer.concatenate_heads(out_THNC)
+        out_TND = self.proj(out_TNC, compute_kernel_config=self.hifi4_compute_kernel_config, core_grid=self.core_grid)
+        out_TND = ttnn.to_layout(out_TND, ttnn.ROW_MAJOR_LAYOUT)
+        out_BTHWC = ttnn.reshape(out_TND, (B, T, H, W, C))
+
+        return out_BTHWC + residual_BTHWC
+
+
+class WanCausalConv3d:
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        mesh_device,
+        stride=1,
+        padding=0,
+    ):
+        self.in_channels = in_channels
+        self.unpadded_out_channels = out_channels
+        self.TILE_WIDTH = 32
+        self.out_channels = self.TILE_WIDTH if out_channels < self.TILE_WIDTH else out_channels
+        if self.out_channels != self.unpadded_out_channels:
+            logger.warning(f"Padding out_channels from {self.unpadded_out_channels} to {self.out_channels}")
+
+        self.kernel_size = _ntuple(kernel_size, 3)
+        self.stride = _ntuple(stride, 3)
+        self.mesh_device = mesh_device
+
+        padding = _ntuple(padding, 3)
+        # t padding is handled explicitly and depends on the cache.
+        # conv3d can handle HW padding internally.
+        self.t_front_padding = 2 * padding[0]
+        self.padding = (0, padding[1], padding[2])
+
+        self.conv_config = get_conv3d_config(
+            self.in_channels,
+            self.out_channels,
+            self.kernel_size,
+            self.stride,
+            self.padding,
+            padding_mode="zeros",
+            grid_size=self.mesh_device.compute_with_storage_grid_size(),
+        )
+        print(f"Loaded conv_config: {self.conv_config}")
+
+        self.compute_kernel_config = ttnn.init_device_compute_kernel_config(
+            self.mesh_device.arch(),
+            math_fidelity=ttnn.MathFidelity.HiFi4,
+            math_approx_mode=False,
+            fp32_dest_acc_en=True,
+            packer_l1_acc=False,
+        )
+
+    def load_state_dict(self, state_dict):
+        def maybe_pad_out_channels(weight, bias):
+            if self.out_channels != self.unpadded_out_channels:
+                weight = torch.nn.functional.pad(
+                    weight, (0, 0, 0, 0, 0, 0, 0, 0, 0, self.out_channels - self.unpadded_out_channels)
+                )
+                bias = torch.nn.functional.pad(bias, (0, self.out_channels - self.unpadded_out_channels))
+            return weight, bias
+
+        padded_weight, padded_bias = maybe_pad_out_channels(state_dict["weight"], state_dict["bias"])
+
+        self.conv_weight, self.conv_bias = prepare_conv3d_weights(
+            self.mesh_device,
+            padded_weight,
+            padded_bias,
+            self.conv_config,
+        )
+        print(f"Loaded conv_weight: {self.conv_weight.shape}, conv_bias: {self.conv_bias.shape}")
+
+    def __call__(self, x_BTHWC, cache_x_BTHWC=None):
+        # NOTE: T padding is handled explicitly and depends on the cache
+
+        print(
+            f"causal conv3d: x_BTHWC: {x_BTHWC.shape}, cache_x_BTHWC: {cache_x_BTHWC.shape if cache_x_BTHWC is not None else None}"
+        )
+        print(f"t_front_padding: {self.t_front_padding}")
+        t_front_padding = self.t_front_padding
+        if cache_x_BTHWC is not None and t_front_padding > 0:
+            # concat on T
+            x_BTHWC = ttnn.concat([cache_x_BTHWC, x_BTHWC], dim=1)
+            t_front_padding -= cache_x_BTHWC.shape[1]
+            print(f"Reduced t_front_padding to {t_front_padding}")
+        if t_front_padding > 0:
+            # Padding only works on the lowest 3 dims. reshape input.
+            B, T, H, W, C = x_BTHWC.shape
+            x_BTNC = ttnn.reshape(x_BTHWC, (B, T, H * W, C))
+            x_BTNC = ttnn.pad(x_BTNC, [(0, 0), (t_front_padding, 0), (0, 0), (0, 0)], value=0.0)
+            x_BTHWC = ttnn.reshape(x_BTNC, (B, T + t_front_padding, H, W, C))
+            print(f"Padded x_BTHWC to {x_BTHWC.shape}")
+
+        return ttnn.experimental.conv3d(
+            input_tensor=x_BTHWC,
+            weight_tensor=self.conv_weight,
+            bias_tensor=self.conv_bias,
+            config=self.conv_config,
+            compute_kernel_config=self.compute_kernel_config,
+        )
+
+
+class WanResidualBlock:
+    def __init__(
+        self,
+        in_dim,
+        out_dim,
+        mesh_device,
+    ):
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.mesh_device = mesh_device
+
+        self.norm1 = RMSNorm(
+            embedding_dim=in_dim,
+            norm_eps=1e-6,
+            norm_elementwise_affine=True,
+            bias=False,
+            mesh_device=mesh_device,
+        )
+        self.conv1 = WanCausalConv3d(
+            in_channels=in_dim,
+            out_channels=out_dim,
+            kernel_size=3,
+            padding=1,
+            mesh_device=mesh_device,
+        )
+        self.norm2 = RMSNorm(
+            embedding_dim=out_dim,
+            norm_eps=1e-6,
+            norm_elementwise_affine=True,
+            bias=False,
+            mesh_device=mesh_device,
+        )
+        self.conv2 = WanCausalConv3d(
+            in_channels=out_dim,
+            out_channels=out_dim,
+            kernel_size=3,
+            padding=1,
+            mesh_device=mesh_device,
+        )
+
+        if in_dim != out_dim:
+            self.conv_shortcut = Linear(
+                in_features=in_dim,
+                out_features=out_dim,
+                mesh_device=mesh_device,
+            )
+        else:
+            self.conv_shortcut = None
+
+        self.hifi4_compute_kernel_config = ttnn.init_device_compute_kernel_config(
+            self.mesh_device.arch(),
+            math_fidelity=ttnn.MathFidelity.HiFi4,
+            math_approx_mode=False,
+            fp32_dest_acc_en=True,
+            packer_l1_acc=False,
+        )
+        device_grid = self.mesh_device.compute_with_storage_grid_size()
+        self.core_grid = ttnn.CoreGrid(x=device_grid.x, y=device_grid.y)
+
+    def load_state_dict(self, state_dict):
+        def rename_norm_state(state):
+            return {"weight": state["gamma"].squeeze()}
+
+        self.norm1.load_state_dict(rename_norm_state(substate(state_dict, "norm1")))
+        self.norm2.load_state_dict(rename_norm_state(substate(state_dict, "norm2")))
+        self.conv1.load_state_dict(substate(state_dict, "conv1"))
+        self.conv2.load_state_dict(substate(state_dict, "conv2"))
+
+        def conv_1d_to_matmul_weight(weight):
+            out_c, in_c, kt, kh, kw = weight.shape
+            assert kt == kh == kw == 1
+            weight = weight.reshape(out_c, in_c)
+            return weight
+
+        if self.conv_shortcut is not None:
+            self.conv_shortcut.load_state_dict(
+                {
+                    "weight": conv_1d_to_matmul_weight(state_dict["conv_shortcut.weight"]),
+                    "bias": state_dict["conv_shortcut.bias"],
+                }
+            )
+
+    def __call__(self, x_BTHWC, feat_cache=None, feat_idx=[0]):
+        x_tile_BTHWC = ttnn.to_layout(x_BTHWC, ttnn.TILE_LAYOUT)
+        h_tile_BTHWC = (
+            self.conv_shortcut(
+                x_tile_BTHWC, compute_kernel_config=self.hifi4_compute_kernel_config, core_grid=self.core_grid
+            )
+            if self.conv_shortcut is not None
+            else x_tile_BTHWC
+        )
+        x_tile_BTHWC = self.norm1(x_tile_BTHWC, compute_kernel_config=self.hifi4_compute_kernel_config)
+        x_tile_BTHWC = ttnn.silu(x_tile_BTHWC)  # NOTE: potential correctness issue
+        x_BTHWC = ttnn.to_layout(x_tile_BTHWC, ttnn.ROW_MAJOR_LAYOUT)
+
+        # Cached conv
+        if feat_cache is not None:
+            # Prepare to cache the current activation for future use
+            idx = feat_idx[0]
+            t_start = x_BTHWC.shape[1] - CACHE_T
+            cache_x_BTHWC = x_BTHWC[:, t_start:, :, :, :]
+            if cache_x_BTHWC.shape[1] < 2 and feat_cache[idx] is not None:
+                # Current activation is too short, so append the cached activation as well
+                cache_x_BTHWC = ttnn.concat([feat_cache[idx][:, -1:, :, :, :], cache_x_BTHWC], dim=1)
+
+            x_BTHWC = self.conv1(x_BTHWC, feat_cache[idx])
+            # NOTE: Should deallocate feat_cache[idx] after it's reassigned
+            feat_cache[idx] = cache_x_BTHWC
+            feat_idx[0] += 1
+        else:
+            x_BTHWC = self.conv1(x_BTHWC)
+
+        x_tile_BTHWC = ttnn.to_layout(x_BTHWC, ttnn.TILE_LAYOUT)
+        x_tile_BTHWC = self.norm2(x_tile_BTHWC, compute_kernel_config=self.hifi4_compute_kernel_config)
+        x_tile_BTHWC = ttnn.silu(x_tile_BTHWC)  # NOTE: potential correctness issue
+        x_BTHWC = ttnn.to_layout(x_tile_BTHWC, ttnn.ROW_MAJOR_LAYOUT)
+
+        # Cached conv
+        if feat_cache is not None:
+            # Prepare to cache the current activation for future use
+            idx = feat_idx[0]
+            t_start = x_BTHWC.shape[1] - CACHE_T
+            cache_x_BTHWC = x_BTHWC[:, t_start:, :, :, :]
+            if cache_x_BTHWC.shape[1] < 2 and feat_cache[idx] is not None:
+                # Current activation is too short, so append the cached activation as well
+                cache_x_BTHWC = ttnn.concat([feat_cache[idx][:, -1:, :, :, :], cache_x_BTHWC], dim=1)
+
+            x_BTHWC = self.conv2(x_BTHWC, feat_cache[idx])
+            # NOTE: Should deallocate feat_cache[idx] after it's reassigned
+            feat_cache[idx] = cache_x_BTHWC
+            feat_idx[0] += 1
+        else:
+            x_BTHWC = self.conv2(x_BTHWC)
+
+        # Add residual
+        x_tile_BTHWC = ttnn.to_layout(x_BTHWC, ttnn.TILE_LAYOUT)
+        x_tile_BTHWC = ttnn.add(h_tile_BTHWC, x_tile_BTHWC)
+        x_BTHWC = ttnn.to_layout(x_tile_BTHWC, ttnn.ROW_MAJOR_LAYOUT)
+
+        return x_BTHWC
