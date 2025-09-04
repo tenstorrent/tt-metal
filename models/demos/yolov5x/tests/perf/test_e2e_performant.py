a37c43ce73350533359b16fec37554e2f95cb311
Sudharsan-V <sudharsan.vijayaraghavan@multicorewareinc.com>
2025-09-04 10:52:42 +0000

    Demo performance improvement

--- models/demos/yolov5x/tests/perf/test_e2e_performant.py
+++ models/demos/yolov5x/tests/perf/test_e2e_performant.py
@@ -48,15 +48,15 @@ def test_e2e_performant(
     )
     performant_runner._capture_yolov5x_trace_2cqs()
     inference_times = []
+    t0 = time.time()
     for _ in range(10):
-        t0 = time.time()
         _ = performant_runner.run()
-        t1 = time.time()
-        inference_times.append(t1 - t0)
+    ttnn.synchronize_device(device)
+    t1 = time.time()
 
     performant_runner.release()
 
-    inference_time_avg = round(sum(inference_times) / len(inference_times), 6)
+    inference_time_avg = round((t1 - t0) / 10, 6)
     logger.info(
         f"ttnn_yolov5x_batch_size: {batch_size}, resolution: {resolution}. One inference iteration time (sec): {inference_time_avg}, FPS: {round(batch_size/inference_time_avg)}"
     )
