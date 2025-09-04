a73f2a410db1e9b234347bd3980c9157b8f4f9db
Jonathan Su <jonathansu@tenstorrent.com>
2025-09-04 04:40:37 +0000

    allow halo'ing on non leading dim

--- ttnn/cpp/ttnn/operations/experimental/ccl/neighbor_pad_async/device/kernels/minimal_default_writer.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/neighbor_pad_async/device/kernels/minimal_default_writer.cpp
@@ -35,23 +35,21 @@ void kernel_main() {
     const address_t output_tensor_address = get_arg_val<address_t>(arg_idx++);
     const uint32_t stick_size = get_arg_val<uint32_t>(arg_idx++);
     const uint32_t stick_start_id = get_arg_val<uint32_t>(arg_idx++);
-    const uint32_t input_outer_dim_size = get_arg_val<uint32_t>(arg_idx++);
-    const uint32_t output_outer_dim_size = get_arg_val<uint32_t>(arg_idx++);
+    const uint32_t input_halo_dim_size = get_arg_val<uint32_t>(arg_idx++);
+    const uint32_t output_halo_dim_size = get_arg_val<uint32_t>(arg_idx++);
+    const uint32_t outer_dim_size = get_arg_val<uint32_t>(arg_idx++);
     const uint32_t padding = get_arg_val<uint32_t>(arg_idx++);
     const uint32_t padding_left = get_arg_val<uint32_t>(arg_idx++);
     const uint32_t num_sticks_to_read = get_arg_val<uint32_t>(arg_idx++);
-    const uint32_t num_sticks_per_outer_dim = get_arg_val<uint32_t>(arg_idx++);
+    const uint32_t num_sticks_per_halo_dim = get_arg_val<uint32_t>(arg_idx++);
     const uint8_t out_ready_sem_noc0_x = get_arg_val<uint32_t>(arg_idx++);
     const uint8_t out_ready_sem_noc0_y = get_arg_val<uint32_t>(arg_idx++);
     size_t out_ready_sem = get_arg_val<uint32_t>(arg_idx++);
     size_t arg_for_fab = arg_idx;
     auto fabric_connection = FabricConnectionManager::build_from_args(arg_for_fab);
 
-    constexpr auto src_args = TensorAccessorArgs<5>();
     uint32_t read_size = stick_size;
-    const auto src_accessor = TensorAccessor(src_args, input_tensor_address, stick_size);
-
-    constexpr auto dst_args = TensorAccessorArgs<src_args.next_compile_time_args_offset()>();
+    constexpr auto dst_args = TensorAccessorArgs<5>();
     const auto dst_accessor = TensorAccessor(dst_args, output_tensor_address, stick_size);
 
     // packet header cb
@@ -67,60 +65,25 @@ void kernel_main() {
 
     fabric_connection.open();
 
-    if (is_first_chip) {
-        // Replicate a slice of 1 from input to output
-        uint32_t dst_stick_id = 0;
-        if (direction) {
-            dst_stick_id = (output_outer_dim_size - padding) * num_sticks_per_outer_dim + stick_start_id;
-        } else {
-            dst_stick_id = stick_start_id;
-        }
-        for (uint32_t iter = 0; iter < num_sticks_to_read; ++iter) {
-            cb_wait_front(cb_output_id, 1);
-            uint32_t l1_read_addr = get_read_ptr(cb_output_id);
-
-            for (uint32_t pad_id = 0; pad_id < padding; pad_id++) {
-                uint64_t dst_noc_addr = get_noc_addr(dst_stick_id + pad_id * num_sticks_per_outer_dim, dst_accessor);
-                noc_async_write(l1_read_addr, dst_noc_addr, stick_size);
-            }
-            dst_stick_id++;
-
-            noc_async_write_barrier();
-            cb_pop_front(cb_output_id, 1);
-        }
-    }
-
-    if (!is_last_chip) {
-        // Read the "end" of each slice into the CB to write to the neighbor
-        for (uint32_t pad_id = 0; pad_id < padding; pad_id++) {
+    uint32_t outer_dim_offset = 0;
+    for (uint32_t outer_dim = 0; outer_dim < outer_dim_size; outer_dim++) {
+        if (is_first_chip) {
+            // Replicate a slice of 1 from input to output
             uint32_t dst_stick_id = 0;
             if (direction) {
-                dst_stick_id = (output_outer_dim_size - (padding - pad_id)) * num_sticks_per_outer_dim + stick_start_id;
+                dst_stick_id = (output_halo_dim_size - padding) * num_sticks_per_halo_dim + stick_start_id;
             } else {
-                dst_stick_id = pad_id * num_sticks_per_outer_dim + stick_start_id;
+                dst_stick_id = stick_start_id;
             }
+            dst_stick_id += outer_dim_offset;
             for (uint32_t iter = 0; iter < num_sticks_to_read; ++iter) {
                 cb_wait_front(cb_output_id, 1);
                 uint32_t l1_read_addr = get_read_ptr(cb_output_id);
 
-                uint64_t dst_noc_addr = get_noc_addr(dst_stick_id, dst_accessor, 0, 0);
-
-                pkt_hdr->to_noc_unicast_write(tt::tt_fabric::NocUnicastCommandHeader{dst_noc_addr}, stick_size);
-                if (direction) {
-                    fabric_connection.get_backward_connection().wait_for_empty_write_slot();
-                    fabric_connection.get_backward_connection().send_payload_without_header_non_blocking_from_address(
-                        l1_read_addr, stick_size);
-                    fabric_connection.get_backward_connection().send_payload_flush_non_blocking_from_address(
-                        (uint32_t)pkt_hdr, sizeof(PACKET_HEADER_TYPE));
-                } else {
-                    fabric_connection.get_forward_connection().wait_for_empty_write_slot();
-                    fabric_connection.get_forward_connection().send_payload_without_header_non_blocking_from_address(
-                        l1_read_addr, stick_size);
-                    fabric_connection.get_forward_connection().send_payload_flush_non_blocking_from_address(
-                        (uint32_t)pkt_hdr, sizeof(PACKET_HEADER_TYPE));
+                for (uint32_t pad_id = 0; pad_id < padding; pad_id++) {
+                    uint64_t dst_noc_addr = get_noc_addr(dst_stick_id + pad_id * num_sticks_per_halo_dim, dst_accessor);
+                    noc_async_write(l1_read_addr, dst_noc_addr, stick_size);
                 }
-                noc_async_writes_flushed();
-
                 dst_stick_id++;
 
                 noc_async_write_barrier();
@@ -128,45 +91,89 @@ void kernel_main() {
             }
         }
 
-        // unicast output ready semaphore
-        uint64_t out_ready_sem_noc_addr_in_pkt =
-            safe_get_noc_addr(out_ready_sem_noc0_x, out_ready_sem_noc0_y, out_ready_sem, 0);
-        auto* pkt_hdr_sem_inc = reinterpret_cast<PACKET_HEADER_TYPE*>(packet_header_buffer_seminc);
-        pkt_hdr_sem_inc->to_noc_unicast_atomic_inc(tt::tt_fabric::NocUnicastAtomicIncCommandHeader{
-            out_ready_sem_noc_addr_in_pkt,
-            static_cast<uint16_t>(1),  // increment 1
-            32});
-        // Write the unicast packet
+        if (!is_last_chip) {
+            // Read the "end" of each slice into the CB to write to the neighbor
+            for (uint32_t pad_id = 0; pad_id < padding; pad_id++) {
+                uint32_t dst_stick_id = 0;
+                if (direction) {
+                    dst_stick_id =
+                        (output_halo_dim_size - (padding - pad_id)) * num_sticks_per_halo_dim + stick_start_id;
+                } else {
+                    dst_stick_id = pad_id * num_sticks_per_halo_dim + stick_start_id;
+                }
+                dst_stick_id += outer_dim_offset;
+                for (uint32_t iter = 0; iter < num_sticks_to_read; ++iter) {
+                    cb_wait_front(cb_output_id, 1);
+                    uint32_t l1_read_addr = get_read_ptr(cb_output_id);
+
+                    uint64_t dst_noc_addr = get_noc_addr(dst_stick_id, dst_accessor, 0, 0);
+
+                    pkt_hdr->to_noc_unicast_write(tt::tt_fabric::NocUnicastCommandHeader{dst_noc_addr}, stick_size);
+                    if (direction) {
+                        fabric_connection.get_backward_connection().wait_for_empty_write_slot();
+                        fabric_connection.get_backward_connection()
+                            .send_payload_without_header_non_blocking_from_address(l1_read_addr, stick_size);
+                        fabric_connection.get_backward_connection().send_payload_flush_non_blocking_from_address(
+                            (uint32_t)pkt_hdr, sizeof(PACKET_HEADER_TYPE));
+                    } else {
+                        fabric_connection.get_forward_connection().wait_for_empty_write_slot();
+                        fabric_connection.get_forward_connection()
+                            .send_payload_without_header_non_blocking_from_address(l1_read_addr, stick_size);
+                        fabric_connection.get_forward_connection().send_payload_flush_non_blocking_from_address(
+                            (uint32_t)pkt_hdr, sizeof(PACKET_HEADER_TYPE));
+                    }
+                    noc_async_writes_flushed();
+
+                    dst_stick_id++;
+
+                    noc_async_write_barrier();
+                    cb_pop_front(cb_output_id, 1);
+                }
+            }
+
+            // unicast output ready semaphore
+            uint64_t out_ready_sem_noc_addr_in_pkt =
+                safe_get_noc_addr(out_ready_sem_noc0_x, out_ready_sem_noc0_y, out_ready_sem, 0);
+            auto* pkt_hdr_sem_inc = reinterpret_cast<PACKET_HEADER_TYPE*>(packet_header_buffer_seminc);
+            pkt_hdr_sem_inc->to_noc_unicast_atomic_inc(tt::tt_fabric::NocUnicastAtomicIncCommandHeader{
+                out_ready_sem_noc_addr_in_pkt,
+                static_cast<uint16_t>(1),  // increment 1
+                32});
+            // Write the unicast packet
+            if (direction) {
+                fabric_connection.get_backward_connection().wait_for_empty_write_slot();
+                pkt_hdr_sem_inc->to_chip_unicast(1);
+                fabric_connection.get_backward_connection().send_payload_flush_blocking_from_address(
+                    packet_header_buffer_seminc, sizeof(PACKET_HEADER_TYPE));
+            } else {
+                fabric_connection.get_forward_connection().wait_for_empty_write_slot();
+                pkt_hdr_sem_inc->to_chip_unicast(1);
+                fabric_connection.get_forward_connection().send_payload_flush_blocking_from_address(
+                    packet_header_buffer_seminc, sizeof(PACKET_HEADER_TYPE));
+            }
+        }
+
+        // Copy the entire input
         if (direction) {
-            fabric_connection.get_backward_connection().wait_for_empty_write_slot();
-            pkt_hdr_sem_inc->to_chip_unicast(1);
-            fabric_connection.get_backward_connection().send_payload_flush_blocking_from_address(
-                packet_header_buffer_seminc, sizeof(PACKET_HEADER_TYPE));
-        } else {
-            fabric_connection.get_forward_connection().wait_for_empty_write_slot();
-            pkt_hdr_sem_inc->to_chip_unicast(1);
-            fabric_connection.get_forward_connection().send_payload_flush_blocking_from_address(
-                packet_header_buffer_seminc, sizeof(PACKET_HEADER_TYPE));
-        }
-    }
+            for (uint32_t t = 0; t < input_halo_dim_size; t++) {
+                uint32_t dst_stick_id = (t + padding_left) * num_sticks_per_halo_dim + stick_start_id;
+                dst_stick_id += outer_dim_offset;
+                for (uint32_t iter = 0; iter < num_sticks_to_read; ++iter) {
+                    cb_wait_front(cb_output_id, 1);
+                    uint32_t l1_read_addr = get_read_ptr(cb_output_id);
 
-    // Copy the entire input
-    if (direction) {
-        for (uint32_t t = 0; t < input_outer_dim_size; t++) {
-            uint32_t dst_stick_id = (t + padding_left) * num_sticks_per_outer_dim + stick_start_id;
-            for (uint32_t iter = 0; iter < num_sticks_to_read; ++iter) {
-                cb_wait_front(cb_output_id, 1);
-                uint32_t l1_read_addr = get_read_ptr(cb_output_id);
+                    uint64_t dst_noc_addr = get_noc_addr(dst_stick_id, dst_accessor);
+                    noc_async_write(l1_read_addr, dst_noc_addr, stick_size);
 
-                uint64_t dst_noc_addr = get_noc_addr(dst_stick_id, dst_accessor);
-                noc_async_write(l1_read_addr, dst_noc_addr, stick_size);
+                    dst_stick_id++;
 
-                dst_stick_id++;
-
-                noc_async_write_barrier();
-                cb_pop_front(cb_output_id, 1);
+                    noc_async_write_barrier();
+                    cb_pop_front(cb_output_id, 1);
+                }
             }
         }
+
+        outer_dim_offset += (num_sticks_per_halo_dim * output_halo_dim_size);
     }
 
     fabric_connection.close();
