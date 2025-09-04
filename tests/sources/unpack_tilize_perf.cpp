1904113cec1f1fa800e9226f678302f034144ce3
Strahinja Stanisic <sstanisic@tenstorrent.com>
2025-09-04 14:38:22 +0200

    perf :zap:: unpack tilize benchmark (#634)
    ### Ticket
    <!-- Link to Github Issue -->
    #431 
    
    ### Problem description
    <!-- Provide context for the problem. -->
    
    ### What's changed
    <!-- Describe the approach used to solve the problem.
    Summarize the changes made and its impact. -->
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)

---
+++ tests/sources/unpack_tilize_perf.cpp
@@ -0,0 +1,200 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <algorithm>
+#include <cstdint>
+#include <cstdio>
+
+#include "build.h"
+#include "ckernel.h"
+#include "cunpack_common.h"
+#include "llk_defs.h"
+#include "params.h"
+#include "perf.h"
+#include "profiler.h"
+
+// Globals
+uint32_t unp_cfg_context          = 0;
+uint32_t pack_sync_tile_dst_ptr   = 0;
+uint32_t math_sync_tile_dst_index = 0;
+
+// Invariants for the test to run correctly
+static_assert(BLOCK_RT_DIM * BLOCK_CT_DIM == TILE_CNT, "BLOCK_RT_DIM * BLOCK_CT_DIM must be equal to TILE_CNT");
+
+static_assert(PERF_RUN_TYPE != PerfRunType::MATH_ISOLATE, "Math isolation not supported for unpack_tilize");
+
+#ifdef LLK_TRISC_UNPACK
+
+#include "llk_unpack_common.h"
+#include "llk_unpack_tilize.h"
+
+void run_kernel()
+{
+    {
+        ZONE_SCOPED("INIT")
+        _llk_unpack_tilize_hw_configure_<is_fp32_dest_acc_en, StochRndType::None>(formats.unpack_src, formats.unpack_dst, FACE_R_DIM, 0, 4);
+        _llk_unpack_tilize_init_(formats.unpack_src, formats.unpack_dst, BLOCK_CT_DIM, FACE_R_DIM, false);
+        PROFILER_SYNC();
+    }
+
+    {
+        ZONE_SCOPED("TILE_LOOP")
+        if constexpr (PERF_RUN_TYPE == PerfRunType::PACK_ISOLATE)
+        {
+            return;
+        }
+
+        for (uint32_t loop = 0; loop < LOOP_FACTOR; loop++)
+        {
+            uint32_t read_offset = 0;
+            for (uint32_t i = 0; i < BLOCK_RT_DIM; i++)
+            {
+                for (uint32_t j = 0; j < BLOCK_CT_DIM; j++)
+                {
+                    _llk_unpack_tilize_(L1_ADDRESS(buffer_A[read_offset]), j, formats.unpack_src, BLOCK_CT_DIM, FACE_R_DIM, 4, false);
+                }
+                read_offset += BLOCK_RT_DIM;
+            }
+        }
+        PROFILER_SYNC();
+    }
+}
+
+#endif
+
+const bool TILIZE = true;
+
+#ifdef LLK_TRISC_MATH
+
+#include "llk_math_common.h"
+#include "llk_math_eltwise_unary_datacopy.h"
+
+using namespace ckernel;
+
+void run_kernel()
+{
+    const bool is_int_fpu_en = false;
+
+    {
+        ZONE_SCOPED("INIT")
+        // copy srca to dest
+#ifdef ARCH_BLACKHOLE
+        // set tilize flag to true
+        _llk_math_eltwise_unary_datacopy_init_<DataCopyType::A2D, is_fp32_dest_acc_en, BroadcastType::NONE, TILIZE, is_int_fpu_en>(0, 0, 4, formats.math);
+#else
+        _llk_math_eltwise_unary_datacopy_init_<DataCopyType::A2D, is_fp32_dest_acc_en, BroadcastType::NONE, is_int_fpu_en>(0, 0, 4, formats.math);
+#endif
+        _llk_math_pack_sync_init_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
+        _llk_math_hw_configure_<false, false>(formats.math, formats.math);
+        PROFILER_SYNC();
+    }
+
+    {
+        ZONE_SCOPED("TILE_LOOP")
+
+        if constexpr (PERF_RUN_TYPE == PerfRunType::PACK_ISOLATE)
+        {
+            return;
+        }
+
+        else if constexpr (PERF_RUN_TYPE == PerfRunType::UNPACK_ISOLATE || PERF_RUN_TYPE == PerfRunType::L1_CONGESTION)
+        {
+#ifdef ARCH_BLACKHOLE
+            // Due to the blackhole tilize bug mitigation
+            // DVALID is set for each tile, instead of each face.
+            constexpr uint32_t NUM_DVALIDS = TILE_CNT;
+#else
+            constexpr uint32_t NUM_DVALIDS = TILE_CNT * TILE_NUM_FACES;
+#endif
+            if constexpr (!unpack_to_dest)
+            {
+                _perf_math_loop_clear_valid<true, true>(LOOP_FACTOR * NUM_DVALIDS);
+                return;
+            }
+
+            for (uint32_t loop = 0; loop < LOOP_FACTOR; loop++)
+            {
+                for (uint32_t i = 0; i < TILE_CNT; i++)
+                {
+                    _llk_math_eltwise_unary_datacopy_<DataCopyType::A2D, DstSync::SyncHalf, is_fp32_dest_acc_en, BroadcastType::NONE, unpack_to_dest>(
+                        i, formats.math, formats.math);
+                }
+            }
+            return;
+        }
+
+        for (uint32_t loop = 0; loop < LOOP_FACTOR; loop++)
+        {
+            for (uint32_t i = 0; i < TILE_CNT; i++)
+            {
+                _llk_math_wait_for_dest_available_<DstSync::SyncHalf>();
+                _llk_math_eltwise_unary_datacopy_<DataCopyType::A2D, DstSync::SyncHalf, is_fp32_dest_acc_en, BroadcastType::NONE, unpack_to_dest>(
+                    i, formats.math, formats.math);
+            }
+            _llk_math_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
+        }
+        PROFILER_SYNC();
+    }
+}
+
+#endif
+
+#ifdef LLK_TRISC_PACK
+
+#include "llk_pack.h"
+#include "llk_pack_common.h"
+
+void run_kernel()
+{
+    const bool UNTILIZE = false;
+
+    {
+        ZONE_SCOPED("INIT")
+
+#ifdef ARCH_BLACKHOLE
+        _llk_pack_hw_configure_<is_fp32_dest_acc_en, UNTILIZE, TILIZE>(formats.pack_src, formats.pack_dst, 16 * 16 * 4);
+        _llk_pack_init_<UNTILIZE, false, DstTileFaceLayout::RowMajor, false, TILIZE>(formats.pack_dst);
+        _llk_pack_dest_init_<DstSync::SyncHalf, is_fp32_dest_acc_en, DstTileFaceLayout::RowMajor>();
+#else
+        _llk_pack_hw_configure_<is_fp32_dest_acc_en, UNTILIZE>(formats.pack_src, formats.pack_dst, 16 * 16 * 4);
+        _llk_pack_init_<UNTILIZE, false, DstTileFaceLayout::RowMajor, false>(formats.pack_dst);
+        _llk_pack_dest_init_<DstSync::SyncHalf, is_fp32_dest_acc_en, DstTileFaceLayout::RowMajor, UNTILIZE>();
+#endif
+        PROFILER_SYNC();
+    }
+    {
+        ZONE_SCOPED("TILE_LOOP")
+
+        if constexpr (PERF_RUN_TYPE == PerfRunType::UNPACK_ISOLATE)
+        {
+            return;
+        }
+
+        if constexpr (PERF_RUN_TYPE == PerfRunType::PACK_ISOLATE || PERF_RUN_TYPE == PerfRunType::L1_CONGESTION)
+        {
+            for (uint32_t loop = 0; loop < LOOP_FACTOR; loop++)
+            {
+                for (int i = 0; i < TILE_CNT; ++i)
+                {
+                    _llk_pack_<DstSync::SyncHalf, is_fp32_dest_acc_en, UNTILIZE>(i, L1_ADDRESS(buffer_Res[i]));
+                }
+            }
+            PROFILER_SYNC();
+            return;
+        }
+
+        for (uint32_t loop = 0; loop < LOOP_FACTOR; loop++)
+        {
+            _llk_packer_wait_for_math_done_();
+            for (int i = 0; i < TILE_CNT; ++i)
+            {
+                _llk_pack_<DstSync::SyncHalf, is_fp32_dest_acc_en, UNTILIZE>(i, L1_ADDRESS(buffer_Res[i]));
+            }
+            _llk_pack_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
+        }
+        PROFILER_SYNC();
+    }
+}
+
+#endif
