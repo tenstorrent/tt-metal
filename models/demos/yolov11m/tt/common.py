a645e8dbbfb4a287ae0016a9e1b3ae8eadbd7dc5
Dimitri Gnidash <dimitri@tenstorrent.com>
2025-09-04 15:58:28 -0400

    Reduce the layout to 4x4

--- models/demos/yolov11m/tt/common.py
+++ models/demos/yolov11m/tt/common.py
@@ -22,7 +22,7 @@ class Yolov11Conv2D:
         is_detect=False,
         is_dfl=False,
         config_override=None,
-        deallocate_activation=True,  # Enable aggressive memory deallocation
+        deallocate_activation=False,  # Enable aggressive memory deallocation
     ):
         self.is_detect = is_detect
         self.activation = activation
