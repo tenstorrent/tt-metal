cc629e00892e0b22197d0241ec95cffbacdb6ef9
Janko Mitrović <jmitrovic@tenstorrent.com>
2025-09-05 14:08:34 +0000

    testing

--- models/experimental/stable_diffusion_xl_base/tests/test_sdxl_accuracy.py
+++ models/experimental/stable_diffusion_xl_base/tests/test_sdxl_accuracy.py
@@ -14,7 +14,6 @@ from models.experimental.stable_diffusion_xl_base.utils.fid_score import calcula
 from models.experimental.stable_diffusion_xl_base.tests.test_common import SDXL_L1_SMALL_SIZE, SDXL_TRACE_REGION_SIZE
 import json
 from models.utility_functions import profiler
-from models.experimental.stable_diffusion_xl_base.conftest import get_device_name
 
 test_demo.__test__ = False
 COCO_CAPTIONS_DOWNLOAD_PATH = "https://github.com/mlcommons/inference/raw/4b1d1156c23965172ae56eacdd8372f8897eb771/text_to_image/coco2014/captions/captions_source.tsv"
@@ -85,6 +84,7 @@ def test_accuracy_sdxl(
 
     logger.info(f"Start inference from prompt index: {start_from} to {start_from + num_prompts}")
 
+    profiler.start("end_to_end_generation")
     images = test_demo(
         mesh_device,
         is_ci_env,
@@ -97,6 +97,11 @@ def test_accuracy_sdxl(
         evaluation_range,
         guidance_scale,
     )
+    profiler.end("end_to_end_generation")
+
+    logger.info("=" * 80)
+    logger.info(f"End-to-end generation time: {profiler.get('end_to_end_generation'):.2f} seconds")
+    logger.info("=" * 80)
 
     clip = CLIPEncoder()
 
@@ -120,38 +125,71 @@ def test_accuracy_sdxl(
     print(f"Average CLIP Score: {average_clip_score}")
     print(f"Standard Deviation of CLIP Scores: {deviation_clip_score}")
 
+    # data = {
+    #     "model": "sdxl",  # For compatibility with current processes
+    #     "metadata": {
+    #         "device": get_device_name(),
+    #         "device_vae": vae_on_device,
+    #         "capture_trace": capture_trace,
+    #         "encoders_on_device": encoders_on_device,
+    #         "num_inference_steps": num_inference_steps,
+    #         "start_from": start_from,
+    #         "num_prompts": num_prompts,
+    #         "negative_prompt": negative_prompt,
+    #         "guidance_scale": guidance_scale,
+    #         "model_name": "sdxl",
+    #     },
+    #     "benchmarks_summary": [
+    #         {
+    #             "device": get_device_name(),
+    #             "model": "sdxl",
+    #             "average_denoising_time": profiler.get("denoising_loop"),
+    #             "average_vae_time": profiler.get("vae_decode"),
+    #             "average_inference_time": profiler.get("denoising_loop") + profiler.get("vae_decode"),
+    #             "min_inference_time": min(
+    #                 i + j for i, j in zip(profiler.times["denoising_loop"], profiler.times["vae_decode"])
+    #             ),
+    #             "max_inference_time": max(
+    #                 i + j for i, j in zip(profiler.times["denoising_loop"], profiler.times["vae_decode"])
+    #             ),
+    #             "average_clip": average_clip_score,
+    #             "deviation_clip": deviation_clip_score,
+    #             "fid_score": fid_score,
+    #         }
+    #     ],
+    # }
+
     data = {
-        "model": "sdxl",  # For compatibility with current processes
+        "model": "sdxl",
         "metadata": {
-            "device": get_device_name(),
-            "device_vae": vae_on_device,
-            "capture_trace": capture_trace,
-            "encoders_on_device": encoders_on_device,
-            "num_inference_steps": num_inference_steps,
-            "start_from": start_from,
-            "num_prompts": num_prompts,
-            "negative_prompt": negative_prompt,
-            "guidance_scale": guidance_scale,
             "model_name": "sdxl",
+            "device": "n150",
         },
         "benchmarks_summary": [
             {
-                "device": get_device_name(),
                 "model": "sdxl",
-                "average_denoising_time": profiler.get("denoising_loop"),
-                "average_vae_time": profiler.get("vae_decode"),
-                "average_inference_time": profiler.get("denoising_loop") + profiler.get("vae_decode"),
-                "min_inference_time": min(
-                    i + j for i, j in zip(profiler.times["denoising_loop"], profiler.times["vae_decode"])
-                ),
-                "max_inference_time": max(
-                    i + j for i, j in zip(profiler.times["denoising_loop"], profiler.times["vae_decode"])
-                ),
-                "average_clip": average_clip_score,
-                "deviation_clip": deviation_clip_score,
-                "fid_score": fid_score,
+                "device": "n150",
+                "avg_gen_time": 23,
+                "target_checks": {
+                    "functional": {"avg_gen_time": 125, "avg_gen_time_ratio": 3.5, "avg_gen_time_check": 3},
+                    "complete": {"avg_gen_time": 25, "avg_gen_time_ratio": 0.8, "avg_gen_time_check": 2},
+                    "target": {"avg_gen_time": 12.5, "avg_gen_time_ratio": 0.1244943820224719, "avg_gen_time_check": 2},
+                },
             }
         ],
+        "evals": [
+            {
+                "model": "sdxl",
+                "device": "n150",
+                "average_clip": 30.890213131904602,
+                "deviation_clip": 2.1092374437350743,
+                "fid_score": 228.6775915239897,
+                "average_clip_accuracy_check": 3,
+                "fid_score_accuracy_check": 2,
+                "accuracy_check": 2,
+            }
+        ],
+        "benchmarks": [],
     }
 
     os.makedirs(OUT_ROOT, exist_ok=True)
