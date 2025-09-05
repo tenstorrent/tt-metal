f9052770f1c2a1ad8eff9af1d25c6c8665788fa3
Adam Thompson <athompson@tenstorrent.com>
2025-09-05 18:56:30 +0000

    Python bindings for [BidirectionalFabric, Fabric, I, MPI]Socket
    *Add TODO regarding pybind11 version- a newer version has more robust
    support for binding virtual function overrides.
    
    *Add PyISocket, a pybind11 trampoline class for virtual function
    overrides of the ISocket class.
    
    *Define Python bindings for BidirectionalFabricSocket, FabricSocket,
    ISocket, MPISocket.  Located in multi_device submodule.
    
    *Add BidirectionalFabricSocket, FabricSocket, ISocket, MPISocket to
    multi_device imports in ttnn's __init__.py

--- ttnn/cpp/ttnn-pybind/__init__.cpp
+++ ttnn/cpp/ttnn-pybind/__init__.cpp
@@ -17,6 +17,7 @@
 #include "ttnn-pybind/fabric.hpp"
 #include "ttnn-pybind/global_circular_buffer.hpp"
 #include "ttnn-pybind/global_semaphore.hpp"
+#include "ttnn-pybind/isocket.hpp"
 #include "ttnn-pybind/mesh_socket.hpp"
 #include "ttnn-pybind/operations/copy.hpp"
 #include "ttnn-pybind/operations/core.hpp"
@@ -258,6 +259,7 @@ PYBIND11_MODULE(_ttnn, module) {
     ttnn::reports::py_module_types(m_reports);
     ttnn::program_descriptors::py_module_types(m_program_descriptors);
     ttnn::tensor_accessor_args::py_module_types(m_tensor_accessor_args);
+    ttnn::distributed::py_isocket_module_types(m_multi_device);
 
     // FUNCTIONS / OPERATIONS
     ttnn::tensor::tensor_mem_config_module(m_tensor);
