34a581d9d06accf942da1953eec6eb8e736c25cd
Marko Bezulj <mbezulj@tenstorrent.com>
2025-09-05 12:37:58 +0000

    use_precomputed_grid, expected_pcc

--- models/experimental/oft/tests/test_oft.py
+++ models/experimental/oft/tests/test_oft.py
@@ -3,8 +3,9 @@ import ttnn
 import pytest
 from models.experimental.oft.tt.tt_oft import OFT
 from models.experimental.oft.reference.oft import OFT as ReferenceOFT
-from tests.ttnn.utils_for_testing import check_with_pcc
+from tests.ttnn.utils_for_testing import assert_with_pcc
 from models.experimental.oft.tt.model_preprocessing import create_OFT_model_parameters_oft
+from loguru import logger
 
 
 def make_grid(grid_size, grid_offset, grid_res):
@@ -22,16 +23,21 @@ def make_grid(grid_size, grid_offset, grid_res):
 
 
 @pytest.mark.parametrize(
-    "input_shape, channels, cell_size, grid_height, scale",
+    "input_shape, channels, cell_size, grid_height, scale, use_precomputed_grid, expected_pcc",
     [
-        ((1, 256, 48, 160), 256, 0.5, 4, 1 / 8),  # feats8
-        ((1, 256, 24, 80), 256, 0.5, 4, 1 / 16),  # feats16
-        ((1, 256, 12, 40), 256, 0.5, 4, 1 / 32),  # feats32
+        ((1, 256, 48, 160), 256, 0.5, 4, 1 / 8, True, 0.900),  # feats8
+        ((1, 256, 24, 80), 256, 0.5, 4, 1 / 16, True, 0.771),  # feats16
+        ((1, 256, 12, 40), 256, 0.5, 4, 1 / 32, True, 0.562),  # feats32
+        ((1, 256, 48, 160), 256, 0.5, 4, 1 / 8, False, 0.880),  # feats8
+        ((1, 256, 24, 80), 256, 0.5, 4, 1 / 16, False, 0.790),  # feats16
+        ((1, 256, 12, 40), 256, 0.5, 4, 1 / 32, False, 0.603),  # feats32
     ],
 )
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 10 * 1024}], indirect=True)
 @pytest.mark.parametrize("seed", [0])
-def test_oft_forward_new(device, input_shape, channels, cell_size, grid_height, scale, seed):
+def test_oft_forward_new(
+    device, input_shape, channels, cell_size, grid_height, scale, seed, use_precomputed_grid, expected_pcc
+):
     torch.manual_seed(seed)
     features = torch.randn(*input_shape)
     print(f"features shape: {features.shape}, dtype: {features.dtype}")
@@ -60,12 +66,23 @@ def test_oft_forward_new(device, input_shape, channels, cell_size, grid_height, 
     tt_calib = ttnn.from_torch(calib, dtype=ttnn.bfloat16, layout=ttnn.ROW_MAJOR_LAYOUT, device=device)
     tt_grid = ttnn.from_torch(grid, dtype=ttnn.bfloat16, layout=ttnn.ROW_MAJOR_LAYOUT, device=device)
 
-    tt_oft = OFT(device, params, channels, cell_size, grid_height, features, calib, grid, scale=scale)
+    tt_oft = OFT(
+        device,
+        params,
+        channels,
+        cell_size,
+        grid_height,
+        features,
+        calib,
+        grid,
+        scale=scale,
+        use_precomputed_grid=use_precomputed_grid,
+    )
     tt_out = tt_oft.forward(device, tt_features, tt_calib, tt_grid)
     tt_out = ttnn.to_torch(tt_out)
 
     n, c, h, w = ref_out.shape
     ref_out = ref_out.permute(0, 2, 3, 1).view(1, 1, h * w, c)
-    message, pcc = check_with_pcc(tt_out, ref_out, 0.99)
-
-    print(f"Passing: {message}, PCC: {pcc} seed: {seed}")
+    pcc_passed, pcc_message = assert_with_pcc(tt_out, ref_out, expected_pcc)
+    assert pcc_passed, pcc_message
+    logger.info(f"Passing: {features.shape=} PCC: {pcc_message} seed: {seed}")
