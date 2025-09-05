648c2968309f4ac9f35f7afd908bf52f295c22e5
Aleksandar Stancov <astancov@tenstorrent.com>
2025-09-05 13:26:10 +0000

    rebase fix

--- models/demos/vgg_unet/ttnn/ttnn_vgg_unet.py
+++ models/demos/vgg_unet/ttnn/ttnn_vgg_unet.py
@@ -140,7 +140,6 @@ class Conv_transpose:
             deallocate_activation=conv_param.deallocate_activation,
             enable_act_double_buffer=conv_param.enable_act_double_buffer,
             enable_weights_double_buffer=True,
-            enable_split_reader=conv_param.enable_split_reader,
             output_layout=output_layout,
         )
         config_override = None
