8e2c9f3c43d21313eb83d81bbb022d728d09c8b4
Pavle Josipović <pavlejosipovic@users.noreply.github.com>
2025-09-05 08:44:35 +0200

    Conv2d decouple input and output shard grids (#27767)
    This feature already exists for width sharded conv2d, where depending On
    num input_channels and num output_channels, input and output shard grids
    can be different. This commit extends this feature to block sharded
    conv2d.
    
    Motivation is to make conv2d more flexible, so that it can be used in
    more cases w/o requiring reshard of the sharded input tensor. This avoid
    expensive reshard calls in SDXL and SD1.4. Additional benefit is that
    input/ouput tensor have better chances of being better spread out into
    more cores leaving more L1 space. Moreover this can lead to better
    image2col performance or matmul performance in conv2d.
    
    Most important changer is in BS activation reader kernel, where now core
    can be sender, receiver of both, to account for decoupled input/output
    shard grids.
    
    This change changed the output shard of many convs which was a breaking
    change for many models and lots of adjusting was required.
    
    Additional during model fix I've hit an issue where conv2d which maps to
    matmul device op, matmul doesn't support input shard having padding on
    channel (inner) dim. Due to this condition to run reshard for these
    cases was added.
    
    SD 1.4 ResNet block was refactored and in cases conv2d outputs shard
    with padding on channel dim, reshard was added. W/O this I2S failed in
    some cases with PCC error. Perf is improvement overall even with
    additional reshard calls.
    
    ResNet50 manual shard configs were removed this change is most of the
    work required to get it running on p100 (fold op needs to be adjusted as
    well).
    
    VGG_UNET model was adjusted not to use auto shard path for convs which
    led to perf degradation. This way 150% perf improvement was achieved.
    
    In some models reshard was added since binary eltwise op is strict in
    requiring both inputs to have same shard. In some cases shard changed as
    output of conv2d and reshard was required to match the other input.
    
    OptimizedConvBlockConfig and OptimizedConvParallelizationConfig were
    removed from pybind as they are unused (and shouldn't be used).
    
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/17348986418)
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/17358826418)
    - [x] [Nightly tt-metal L2
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/17406087420)
    - [x] [(Single-card) Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/17400106443)
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/runs/17463393072)
    CI passes (if applicable)
    
    Co-authored-by: Pavle Josipovic <pjosipovic@tenstorrent.com>

--- models/demos/wormhole/stable_diffusion/tests/test_perf.py
+++ models/demos/wormhole/stable_diffusion/tests/test_perf.py
@@ -408,7 +408,7 @@ def test_stable_diffusion_perf(device, batch_size, num_inference_steps, expected
 @pytest.mark.models_device_performance_bare_metal
 @pytest.mark.parametrize(
     "expected_kernel_samples_per_second",
-    ((12.00),),
+    ((12.8),),
 )
 def test_stable_diffusion_device_perf(expected_kernel_samples_per_second):
     subdir = "ttnn_stable_diffusion"
