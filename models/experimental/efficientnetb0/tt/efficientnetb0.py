7ec60ea5b4c1080ca21bb22d82a3c99d96939d11
Dragica Stoiljkovic <dstoiljkovic@tenstorrent.com>
2025-09-05 15:13:55 +0000

    Removal of global avg pool tests and replacing the global_avg_pool call with pool2d

--- models/experimental/efficientnetb0/tt/efficientnetb0.py
+++ models/experimental/efficientnetb0/tt/efficientnetb0.py
@@ -244,7 +244,11 @@ class MBConvBlock:
         if x.is_sharded():
             x = ttnn.sharded_to_interleaved(x, ttnn.L1_MEMORY_CONFIG)
         x = ttnn.to_layout(x, layout=ttnn.ROW_MAJOR_LAYOUT)
-        x = ttnn.global_avg_pool2d(x)
+        # Global average pooling: use avg_pool2d with kernel_size = input spatial dimensions
+        N, H, W, C = x.shape
+        x = ttnn.avg_pool2d(
+            x, batch_size=N, input_h=H, input_w=W, channels=C, kernel_size=(H, W), stride=(H, W), padding=(0, 0)
+        )
 
         x = self._se_reduce(x)
 
@@ -455,7 +459,11 @@ class Efficientnetb0:
 
         x = ttnn.sharded_to_interleaved(x, memory_config=ttnn.L1_MEMORY_CONFIG)
         x = ttnn.to_layout(x, layout=ttnn.ROW_MAJOR_LAYOUT)
-        x = ttnn.global_avg_pool2d(x)
+        # Global average pooling: use avg_pool2d with kernel_size = input spatial dimensions
+        N, H, W, C = x.shape
+        x = ttnn.avg_pool2d(
+            x, batch_size=N, input_h=H, input_w=W, channels=C, kernel_size=(H, W), stride=(H, W), padding=(0, 0)
+        )
 
         x = ttnn.reshape(x, (1, -1))
 
