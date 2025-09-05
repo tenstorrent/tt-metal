7ea0533993c1f04a3cead161310cc4bf26385b10
Dimitri Gnidash <dimitri@tenstorrent.com>
2025-09-05 14:41:48 -0400

    #27629: Implement Yolo v11 Medium-size. Adjust c3k2 blocks to use the c3 blocks instead of Bottlenecks  - Change the model channel parameters to fit with medium sized model  - Change the attention head parameters  - Deallocate tensors more aggressively since the tensor data is much larger in medium

--- models/demos/yolov11m/common.py
+++ models/demos/yolov11m/common.py
@@ -7,19 +7,19 @@ import os
 import torch
 from ultralytics import YOLO
 
-from models.demos.yolov11.reference import yolov11
+from models.demos.yolov11m.reference import yolov11
 
-YOLOV11_L1_SMALL_SIZE = 24576
+YOLOV11_L1_SMALL_SIZE = 16000
 
 
 def load_torch_model(model_location_generator=None):
     torch_model = yolov11.YoloV11()
     if model_location_generator == None or "TT_GH_CI_INFRA" not in os.environ:
-        model = YOLO("yolo11n.pt")
+        model = YOLO("yolo11m.pt")
         state_dict = {k.replace("model.", "", 1): v for k, v in model.state_dict().items()}
     else:
         weights_path = (
-            model_location_generator("vision-models/yolov11n", model_subdir="", download_if_ci_v2=True) / "yolov11n.pt"
+            model_location_generator("vision-models/yolov11m", model_subdir="", download_if_ci_v2=True) / "yolov11m.pt"
         )
         yolov11_ckpt = torch.load(weights_path)
         state_dict = yolov11_ckpt["model"].float().state_dict()
