2a8d795c9f5cb00d56be9af1f96bea8125ae10ee
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2025-09-06 06:52:15 +0000

    #0: Update

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_selu.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_selu.h
@@ -13,7 +13,7 @@ namespace ckernel {
 
 template <bool APPROXIMATE>
 inline void llk_math_eltwise_unary_sfpu_selu_init() {
-    llk_math_eltwise_unary_sfpu_init<SfpuType::selu, APPROXIMATE>(sfpu::selu_init);
+    llk_math_eltwise_unary_sfpu_init<SfpuType::selu, APPROXIMATE>();
 }
 
 template <bool APPROXIMATE, bool is_fp32_dest_acc_en = false, int ITERATIONS = 8>
