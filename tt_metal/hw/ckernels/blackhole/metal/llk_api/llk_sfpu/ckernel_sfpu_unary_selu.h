a1f3b660c6bfdc7ebc357e78c10081a6b7d8c34c
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2025-09-06 06:52:15 +0000

    #0: Update

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_unary_selu.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_unary_selu.h
@@ -7,32 +7,10 @@
 #include "ckernel.h"
 #include "ckernel_defs.h"
 #include "sfpu/ckernel_sfpu_converter.h"
+#include "ckernel_sfpu_exp.h"
 
 namespace ckernel {
 namespace sfpu {
-
-sfpi_inline sfpi::vFloat _sfpu_selu_exp_21f_(sfpi::vFloat val) {
-    sfpi::vFloat y = 0.0f;
-    v_if(val > -88.0f) {
-        sfpi::vInt z = sfpu::_float_to_int32_(val * sfpi::vFloat(0x00b8aa3b) + sfpi::vFloat(0x3f800000));
-        sfpi::vInt zii = exexp(sfpi::reinterpret<sfpi::vFloat>(z));
-        sfpi::vInt zif = sfpi::exman9(sfpi::reinterpret<sfpi::vFloat>(z));
-
-        sfpi::vFloat d1 = sfpi::vFloat(sfpi::vConstFloatPrgm0);
-        sfpi::vFloat d2 = sfpi::int32_to_float(sfpi::vConstIntPrgm1 + zif, 0);
-        sfpi::vFloat d3 = sfpi::int32_to_float(sfpi::vConstIntPrgm2 + zif, 0);
-        d2 = d1 * d2;
-        zif = sfpu::_float_to_int32_(d2 * d3);
-
-        // Restore exponent
-        zii = sfpi::reinterpret<sfpi::vInt>(
-            sfpi::setexp(sfpi::reinterpret<sfpi::vFloat>(zif), 127U + zii));  // restore exponent
-
-        y = sfpi::reinterpret<sfpi::vFloat>(zii);
-    }
-    v_endif;
-    return y;
-}
 
 // SELU(x) = scale ∗ ( max(0, x) + min(0, α ∗ (exp(x)−1) ) )
 template <bool APPROXIMATION_MODE, bool is_fp32_dest_acc_en = false, int ITERATIONS>
@@ -45,7 +23,8 @@ inline void calculate_selu(uint scale, uint alpha) {
         sfpi::vFloat v = sfpi::dst_reg[0];
         v_if(v >= 0.0f) { sfpi::dst_reg[0] = v * scale_value; }
         v_else {
-            sfpi::vFloat exp_calc = _sfpu_selu_exp_21f_(v);
+            sfpi::vFloat exp_calc = _sfpu_exp_21f_<true>(
+                v);  // is_fp32_dest_acc_en set to true to avoid rounding as it has to be done at the end of operation
             sfpi::vFloat minus_mul = exp_calc - 1.0f;
             sfpi::vFloat result = minus_mul * alpha_value * scale_value;
 
@@ -57,12 +36,6 @@ inline void calculate_selu(uint scale, uint alpha) {
         v_endif;
         sfpi::dst_reg++;
     }
-}
-
-inline void selu_init() {
-    sfpi::vConstFloatPrgm0 = 0.40196114e-7f;
-    sfpi::vConstIntPrgm1 = 0xf94ee7;
-    sfpi::vConstIntPrgm2 = 0x560e;
 }
 
 }  // namespace sfpu
