05ce5f0e1130dc8973e6425f75bbfee375024f6a
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2025-09-07 16:44:17 +0000

    #0: Improve elu accuracy with exp 21f

--- tt_metal/include/compute_kernel_api/eltwise_unary/elu.h
+++ tt_metal/include/compute_kernel_api/eltwise_unary/elu.h
@@ -26,11 +26,13 @@ namespace ckernel {
  *
  * | Argument       | Description                                                                | Type     | Valid Range                                           | Required |
  * |----------------|----------------------------------------------------------------------------|----------|-------------------------------------------------------|----------|
- * | tile_index     | The index of the tile in DST register buffer to perform the computation on | uint32_t | Must be less than the size of the DST register buffer | True     | 
+ * | tile_index     | The index of the tile in DST register buffer to perform the computation on | uint32_t | Must be less than the size of the DST register buffer | True     |
  * | slope          | slope used in elu calculation                                              | uint32_t | Greater than 0                                        | True     |
  */
- // clang-format on
-ALWI void elu_tile(uint32_t idst, uint32_t param0) { MATH((llk_math_eltwise_unary_sfpu_elu<APPROX>(idst, param0))); }
+// clang-format on
+ALWI void elu_tile(uint32_t idst, uint32_t param0) {
+    MATH((llk_math_eltwise_unary_sfpu_elu<APPROX, DST_ACCUM_MODE>(idst, param0)));
+}
 
 /**
  * Please refer to documentation for any_init.
