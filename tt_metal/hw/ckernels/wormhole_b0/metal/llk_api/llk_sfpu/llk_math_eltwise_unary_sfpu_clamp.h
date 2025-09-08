e455c6895dfb8eaa46b7e34843578043061afcf2
Mouliraj Elamurugan <ctr-mcw-melamurugan@ext.tenstorrent.com>
2025-09-08 15:14:20 +0530

    #21352: Add int32 support for clamp TSS (#27225)
    ### Ticket
    Link to Github Issue #21352 
    
    ### What's changed
    
    - Added int32 for clamp TSS.
    - Added optional output tensor and queue id support clamp op.
    
    ### Checklist
    - [ ] All post commit
    - [ ] Blackhole Post commit

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_clamp.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_clamp.h
@@ -19,9 +19,16 @@ inline void llk_math_eltwise_unary_sfpu_clamp_init() {
 
 template <bool APPROXIMATE, int ITERATIONS = 8>
 inline void llk_math_eltwise_unary_sfpu_clamp(
-    uint dst_index, uint param0, uint param1, int vector_mode = (int)VectorMode::RC) {
+    uint dst_index, uint min_val, uint max_val, int vector_mode = (int)VectorMode::RC) {
     _llk_math_eltwise_unary_sfpu_params_<APPROXIMATE>(
-        ckernel::sfpu::calculate_clamp<APPROXIMATE, ITERATIONS>, dst_index, vector_mode, param0, param1);
+        ckernel::sfpu::calculate_clamp<APPROXIMATE, ITERATIONS>, dst_index, vector_mode, min_val, max_val);
+}
+
+template <bool APPROXIMATE, int ITERATIONS = 8>
+inline void llk_math_eltwise_unary_sfpu_clamp_int32(
+    uint dst_index, uint min_val, uint max_val, int vector_mode = (int)VectorMode::RC) {
+    _llk_math_eltwise_unary_sfpu_params_<APPROXIMATE>(
+        ckernel::sfpu::calculate_clamp_int32<APPROXIMATE, ITERATIONS>, dst_index, vector_mode, min_val, max_val);
 }
 
 }  // namespace ckernel
