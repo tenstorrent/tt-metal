385a2641e1be2a805e00453953a8b10e2e7ee8af
Nour Ardo <nardo@tenstorrent.com>
2025-09-08 19:55:07 +0000

    fix conflict with main

--- ttnn/cpp/ttnn/operations/experimental/ccl/common.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/common.cpp
@@ -51,7 +51,6 @@ bool use_composite_reduce_scatter(
 
 bool use_all_gather_async_llama_sharded(const ttnn::Tensor& input_tensor, const ttnn::MemoryConfig& output_mem_config) {
     auto input_tensor_shape = input_tensor.padded_shape();
-    auto input_tensor_page_layout = input_tensor.layout();
     auto input_tensor_memory_config = input_tensor.memory_config();
     bool input_is_sharded = input_tensor_memory_config.shard_spec().has_value();
     bool output_is_sharded = output_mem_config.shard_spec().has_value();
