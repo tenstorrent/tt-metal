47448dffd09af3b43c6fdac6e09d9105334a2824
Nigel Huang <nhuang@tenstorrent.com>
2025-09-08 20:36:39 +0000

    Test

--- tt_metal/lite_fabric/hw/inc/kernel_api.hpp
+++ tt_metal/lite_fabric/hw/inc/kernel_api.hpp
@@ -4,19 +4,17 @@
 
 #pragma once
 
-#if defined(COMPILE_FOR_ERISC) && defined(ROUTING_FW_ENABLED)
-
 #include "tt_metal/lite_fabric/hw/inc/host_interface.hpp"
 #include "tt_metal/lite_fabric/hw/inc/lf_dev_mem_map.hpp"
 
 namespace lite_fabric {
 
 inline void service_lite_fabric_channels() {
+#if defined(COMPILE_FOR_ERISC) && defined(ROUTING_FW_ENABLED)
     auto config = reinterpret_cast<volatile lite_fabric::FabricLiteMemoryMap*>(LITE_FABRIC_CONFIG_START);
     void (*service_routing)() = (void (*)())((uint32_t*)config->service_lite_fabric_addr);
     service_routing();
+#endif
 }
 
 }  // namespace lite_fabric
-
-#endif
