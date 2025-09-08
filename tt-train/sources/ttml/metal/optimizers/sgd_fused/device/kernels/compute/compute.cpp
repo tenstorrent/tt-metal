92d4629a08c799346fcb275b8339ccdebadd7f01
Ziemowit Bączewski <zbaczewski@tenstorrent.com>
2025-09-08 16:26:43 +0000

    Add kernels; For now gradient is not multiplied by LR

---
+++ tt-train/sources/ttml/metal/optimizers/sgd_fused/device/kernels/compute/compute.cpp
@@ -0,0 +1,62 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <cstdint>
+
+#include "compute_kernel_api.h"
+#include "compute_kernel_api/common.h"
+#include "compute_kernel_api/eltwise_binary.h"
+
+namespace NAMESPACE {
+
+constexpr auto kParamInCbIndex = tt::CBIndex::c_0;
+constexpr auto kGradCbIndex = tt::CBIndex::c_1;
+constexpr auto kLrCbIndex = tt::CBIndex::c_2;
+
+constexpr auto kComputeCbIndex = tt::CBIndex::c_8;
+
+constexpr auto kOutputCbIndex = tt::CBIndex::c_16;
+
+constexpr uint32_t num_rows_per_core = get_compile_time_arg_val(0);
+constexpr uint32_t block_size = get_compile_time_arg_val(1);
+constexpr uint32_t Wt = get_compile_time_arg_val(2);
+
+void MAIN() {
+    uint32_t runtime_args_counter = 0;
+    uint32_t lr = get_arg_val<uint32_t>(runtime_args_counter++);
+
+    for (uint32_t row = 0; row < num_rows_per_core; ++row) {
+        for (uint32_t col = 0; col < Wt; col += block_size) {
+            cb_wait_front(kParamInCbIndex, block_size);
+            cb_wait_front(kGradCbIndex, block_size);
+            cb_reserve_back(kOutputCbIndex, block_size);
+
+            tile_regs_acquire();
+            for (uint32_t block_idx = 0; block_idx < block_size; ++block_idx) {
+                copy_tile_init(kParamInCbIndex);
+                copy_tile(kParamInCbIndex, /* tile_index */ block_idx, /* register_idx */ block_idx);
+
+                const uint32_t grad_register = block_size + block_idx;
+                copy_tile_init(kGradCbIndex);
+                copy_tile(kGradCbIndex, /* tile_index */ block_idx, /* register_idx */ grad_register);
+
+                // binop_with_scalar_tile_init();
+                // mul_unary_tile(grad_register, lr);
+
+                sub_tiles_init(block_idx, grad_register);
+                sub_tiles(
+                    /* register_param_in_idx */ block_idx,
+                    /* register_grad_idx */ grad_register,
+                    /* register_out_idx */ block_idx);
+            }
+            tile_regs_commit();
+
+            pack_and_push_block(kOutputCbIndex, block_size);
+
+            cb_pop_front(kParamInCbIndex, block_size);
+            cb_pop_front(kGradCbIndex, block_size);
+        }
+    }
+}
+}  // namespace NAMESPACE
---
+++ tt-train/sources/ttml/metal/optimizers/sgd_fused/device/kernels/dataflow/write.cpp
@@ -0,0 +1,51 @@
+#include <cstdint>
+
+#include "dataflow_api.h"
+#include "hostdevcommon/kernel_structs.h"
+
+constexpr auto kParamOutCbIndex = tt::CBIndex::c_16;
+
+constexpr uint32_t block_size = get_compile_time_arg_val(0);
+constexpr uint32_t Wt = get_compile_time_arg_val(1);
+
+template <typename AddrGen>
+inline void write_cb_block_to_dram(
+    uint32_t cb_idx,
+    const AddrGen& addr_gen,
+    uint32_t start_idx,
+    uint32_t block_size,
+    uint32_t current_block_size,
+    uint32_t tile_size_bytes) {
+    cb_wait_front(cb_idx, block_size);
+    uint32_t l1_read_addr = get_read_ptr(cb_idx);
+    // We wait for the block_size tiles to be available in the CB, but we only write the current_block_size tiles
+    // because C % block_size may not be zero.
+    for (uint32_t block_idx = 0; block_idx < current_block_size; ++block_idx) {
+        noc_async_write_tile(start_idx + block_idx, addr_gen, l1_read_addr);
+        l1_read_addr += tile_size_bytes;
+    }
+}
+
+void kernel_main() {
+    uint32_t runtime_args_counter = 0;
+    uint32_t param_out_addr = get_arg_val<uint32_t>(runtime_args_counter++);
+    uint32_t num_rows_to_process = get_arg_val<uint32_t>(runtime_args_counter++);
+    uint32_t start_row = get_arg_val<uint32_t>(runtime_args_counter++);
+
+    const uint32_t tile_size_bytes = get_tile_size(kParamOutCbIndex);
+    constexpr auto param_out_args = TensorAccessorArgs<2U>();
+    const auto param_out_addr_generator = TensorAccessor(param_out_args, param_out_addr, tile_size_bytes);
+
+    uint32_t end_row = start_row + num_rows_to_process;
+    for (uint32_t r = start_row; r < end_row; ++r) {
+        for (uint32_t c = 0; c < Wt; c += block_size) {
+            uint32_t current_block_size = (c + block_size <= Wt) ? block_size : (Wt - c);
+            uint32_t start_idx = (r * Wt) + c;
+
+            write_cb_block_to_dram(
+                kParamOutCbIndex, param_out_addr_generator, start_idx, block_size, current_block_size, tile_size_bytes);
+            noc_async_write_barrier();
+            cb_pop_front(kParamOutCbIndex, block_size);
+        }
+    }
+}
