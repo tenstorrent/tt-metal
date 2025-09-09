96e38a4326f9d904ccfe2009944f43a7f298ef9e
bcheemasabirTT <bcheemasabir@tenstorrent.com>
2025-09-09 00:41:09 +0000

    migrated test_conv_hardcoded.cpp

--- tests/tt_metal/tt_metal/data_movement/conv_hardcoded/test_conv_hardcoded.cpp
+++ tests/tt_metal/tt_metal/data_movement/conv_hardcoded/test_conv_hardcoded.cpp
@@ -31,13 +31,20 @@ struct ConvConfig {
 /// @param device
 /// @param test_config - Configuration of the test -- see struct
 /// @return
-bool run_dm(IDevice* device, const ConvConfig& test_config) {
+bool run_dm(std::shared_ptr<distributed::MeshDevice> mesh_device, const ConvConfig& test_config) {
     // Program
+    distributed::MeshWorkload workload;
+    auto zero_coord = distributed::MeshCoordinate(0, 0);
+    auto device_range = distributed::MeshCoordinateRange(zero_coord, zero_coord);
     Program program = CreateProgram();
+    distributed::AddProgramToMeshWorkload(workload, std::move(program), device_range);
+    auto& program_ = workload.get_programs().at(device_range);
+    auto& cq = mesh_device->mesh_command_queue();
+    auto device = mesh_device->get_devices()[0];
 
     // Kernels
     auto receiver_kernel = CreateKernel(
-        program,
+        program_,
         test_config.kernel_name,
         test_config.dest_core_set,
         DataMovementConfig{
@@ -46,23 +53,23 @@ bool run_dm(IDevice* device, const ConvConfig& test_config) {
             .compile_args = test_config.dest_core_compile_args});
 
     // Runtime Arguments
-    SetRuntimeArgs(program, receiver_kernel, test_config.dest_core_set, test_config.dest_core_runtime_args);
+    SetRuntimeArgs(program_, receiver_kernel, test_config.dest_core_set, test_config.dest_core_runtime_args);
 
     // Assign unique id
     log_info(LogTest, "Running Test ID: {}, Run ID: {}", test_config.test_id, unit_tests::dm::runtime_host_id);
-    program.set_runtime_id(unit_tests::dm::runtime_host_id++);
+    program_.set_runtime_id(unit_tests::dm::runtime_host_id++);
 
     // Launch program using slow dispatch
     MetalContext::instance().get_cluster().l1_barrier(device->id());
-    tt::tt_metal::detail::LaunchProgram(device, program);
+    distributed::EnqueueMeshWorkload(cq, workload, true);
 
     return true;
 }
 }  // namespace unit_tests::dm::conv_hardcoded
 
-TEST_F(DeviceFixture, TensixDataMovementConvActHalo3x3) {
-    IDevice* device = devices_.at(0);
-    auto arch_ = device->arch();
+TEST_F(MeshDeviceFixture, TensixDataMovementConvActHalo3x3) {
+    auto mesh_device = devices_.at(0);
+    auto arch_ = mesh_device->arch();
 
     if (arch_ != ARCH::BLACKHOLE) {
         GTEST_SKIP() << "Skipping test for non-BH architecture";
@@ -129,12 +136,12 @@ TEST_F(DeviceFixture, TensixDataMovementConvActHalo3x3) {
     };
 
     // Run
-    EXPECT_TRUE(run_dm(device, test_config));
+    EXPECT_TRUE(run_dm(mesh_device, test_config));
 }
 
-TEST_F(DeviceFixture, TensixDataMovementConvActHalo3x3Smaller) {
-    IDevice* device = devices_.at(0);
-    auto arch_ = device->arch();
+TEST_F(MeshDeviceFixture, TensixDataMovementConvActHalo3x3Smaller) {
+    auto mesh_device = devices_.at(0);
+    auto arch_ = mesh_device->arch();
 
     if (arch_ != ARCH::BLACKHOLE) {
         GTEST_SKIP() << "Skipping test for non-BH architecture";
@@ -201,12 +208,12 @@ TEST_F(DeviceFixture, TensixDataMovementConvActHalo3x3Smaller) {
     };
 
     // Run
-    EXPECT_TRUE(run_dm(device, test_config));
+    EXPECT_TRUE(run_dm(mesh_device, test_config));
 }
 
-TEST_F(DeviceFixture, TensixDataMovementConvHaloGather) {
-    IDevice* device = devices_.at(0);
-    auto arch_ = device->arch();
+TEST_F(MeshDeviceFixture, TensixDataMovementConvHaloGather) {
+    auto mesh_device = devices_.at(0);
+    auto arch_ = mesh_device->arch();
 
     if (arch_ != ARCH::BLACKHOLE) {
         GTEST_SKIP() << "Skipping test for non-BH architecture";
@@ -266,7 +273,7 @@ TEST_F(DeviceFixture, TensixDataMovementConvHaloGather) {
     };
 
     // Run
-    EXPECT_TRUE(run_dm(device, test_config));
+    EXPECT_TRUE(run_dm(mesh_device, test_config));
 }
 
 }  // namespace tt::tt_metal
