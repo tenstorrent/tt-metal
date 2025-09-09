52928cfc053ecd3d15c908315ea16704c8aa6730
Juan Camilo Vega <jvega@tenstorrent.com>
2025-09-09 21:27:09 +0000

    fixing all gather nightly

--- tests/ttnn/unit_tests/operations/ccl/blackhole_CI/nightly/test_all_gather_nightly.py
+++ tests/ttnn/unit_tests/operations/ccl/blackhole_CI/nightly/test_all_gather_nightly.py
@@ -80,6 +80,7 @@ def validate_test(num_devices, topology, shape, cluster_axis):
     indirect=["device_params"],
     ids=["fabric_linear", "fabric_ring"],
 )
+@pytest.mark.parametrize("cluster_axis", [0, 1])
 @pytest.mark.parametrize("chunks_per_sync", [20])
 @pytest.mark.parametrize("num_workers_per_link", [2])
 @pytest.mark.parametrize("num_buffers_per_channel", [2])
@@ -99,10 +100,12 @@ def test_all_gather_nightly(
     chunks_per_sync,
     num_workers_per_link,
     num_buffers_per_channel,
+    cluster_axis,
 ):
-    cluster_axis = 0
-    validate_test(num_devices, all_gather_topology, bh_1d_mesh_device.shape, cluster_axis)
-    submesh_device = bh_1d_mesh_device.create_submesh(ttnn.MeshShape((num_devices, 1)))
+    if cluster_axis == 0:
+        submesh_device = bh_1d_mesh_device.create_submesh(ttnn.MeshShape((num_devices, 1)))
+    else:
+        submesh_device = bh_1d_mesh_device.create_submesh(ttnn.MeshShape((1, num_devices)))
     run_all_gather_impl(
         submesh_device,
         num_devices,
