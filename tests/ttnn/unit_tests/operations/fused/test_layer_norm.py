6c90a9a995303e48b54a2a54a40bd572b9c8b29b
Ryan Miller <rmiller@tenstorrent.com>
2025-09-09 16:02:07 +0000

    Add the previously-problematic test case

--- tests/ttnn/unit_tests/operations/fused/test_layer_norm.py
+++ tests/ttnn/unit_tests/operations/fused/test_layer_norm.py
@@ -120,23 +120,12 @@ def test_layer_norm_with_tile_layout(device, h, w):
     assert_with_pcc(torch_output_tensor, output_tensor, 0.9998)
 
 
-@pytest.mark.parametrize("h, w", [(2048, 2048)])
-@pytest.mark.parametrize("d_type", [torch.bfloat16, torch.float32])
-def test_large_layer_norm(device, h, w, d_type):
+@pytest.mark.parametrize("h", [1024, 2080])
+@pytest.mark.parametrize("w", [3200, 4128])
+def test_large_layer_norm(device, h, w):
     torch.manual_seed(0)
 
-    base_row = torch.arange(w, dtype=torch.float64).unsqueeze(0)  # shape: [1, 4096]
-
-    # Create a column vector with exponents from 0 to rows-1
-    exponents = torch.arange(h, dtype=torch.float64).unsqueeze(1)  # shape: [2048, 1]
-
-    # Compute the multipliers as powers of 10 (10^0, 10^1, ..., 10^(rows-1))
-    multipliers = 10**exponents  # shape: [2048, 1]
-
-    # Multiply the multipliers with the base_row using broadcasting.
-    # Each row becomes 10 times the previous row.
-    tensor = multipliers * base_row  # shape: [2048, 4096]
-    torch_input_tensor = torch.rand((h, w), dtype=d_type)
+    torch_input_tensor = torch.rand((h, w), dtype=torch.float32)
     torch_output_tensor = torch.nn.functional.layer_norm(torch_input_tensor, normalized_shape=[w])
 
     input_tensor = ttnn.from_torch(torch_input_tensor, layout=ttnn.TILE_LAYOUT, device=device)
@@ -145,7 +134,7 @@ def test_large_layer_norm(device, h, w, d_type):
     output_tensor = ttnn.from_device(output_tensor)
     output_tensor = ttnn.to_torch(output_tensor)
 
-    assert_with_pcc(torch_output_tensor, output_tensor, 0.97)
+    assert_with_pcc(torch_output_tensor, output_tensor, 0.9998)
 
 
 @pytest.mark.parametrize("h, w", [(2048, 2048)])
