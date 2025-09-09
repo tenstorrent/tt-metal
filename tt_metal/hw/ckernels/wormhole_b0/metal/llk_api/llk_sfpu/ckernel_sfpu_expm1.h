722a82b0a926ff6d3b50e2c68186beaba30b991f
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2025-09-09 05:40:27 +0000

    #0: update

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_expm1.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_expm1.h
@@ -7,10 +7,42 @@
 namespace ckernel {
 namespace sfpu {
 
+/*
+ * This function implements the exponential function using a polynomial approximation algorithm
+ * based on "Simple Multiple Precision Algorithms for Exponential Functions [Tips & Tricks]"
+ * by Moroz et al. 2022 (https://doi.org/10.1109/MSP.2022.3157460).
+ * More specifically, it is the implementation of the `exp_21f` algorithm described in Section 5
+ *
+ * @param val The input value (sfpi::vFloat vector), can be any floating point number
+ *
+ * @return sfpi::vFloat Result of exp(val)
+ *
+ * @see Moroz et al. 2022 - "Simple Multiple Precision Algorithms for Exponential Functions"
+ *      ( https://doi.org/10.1109/MSP.2022.3157460 )
+ */
 template <bool is_fp32_dest_acc_en = false>
 sfpi_inline sfpi::vFloat _sfpu_expm1_(sfpi::vFloat val) {
-    sfpi::vFloat y = -1.0f;
-    v_if(val > -88.0f) {
+    sfpi::vFloat y = sfpi::vConstNeg1;
+    // When x is very small, exp(x) is very close to 1. Hence, for improved precision, we use Taylor expansion of
+    // expm1(x) = x + (x^2/2) + (x^3/3^2)
+    // y = (val * (val * (val * 1.166f + 0.5f )+ 1)
+    v_if(sfpi::abs(val) < sfpi::vFloat(1e-3f)) {
+        // y = val + (sfpi::vFloat(0.5f) * val * val) + (val * val * val * sfpi::vFloat(1.166f));
+        y = val * (1.0f + val * (sfpi::vFloat(0.5f) + val * sfpi::vFloat(1.166f)));
+
+        if constexpr (!is_fp32_dest_acc_en) {
+            // LRegs work on float32 data. If DST is bfloat16 then SFPSTORE will truncate it.
+            // This can reduce accuracy: for instance, 9**2 = 80.8 gets round to 80.5
+            // rather than 81 (which would have been correct).
+            // To avoid this issue, we explicitly convert to bfloat16 using round-to-nearest-even.
+            y = sfpi::reinterpret<sfpi::vFloat>(sfpi::float_to_fp16b(y, 0));
+        }
+    }
+    v_elseif(val > -88.0f) {
+        // The paper relies on the following formula (c.f. Section 2 and 3 of paper):
+        // z = (bias + x * factor * N_m; where:
+        // factor = 0x00b8aa3b (computed through log(e))
+        // bias = 0x3f800000
         sfpi::vInt z = sfpu::_float_to_int32_(val * sfpi::vFloat(0x00b8aa3b) + sfpi::vFloat(0x3f800000));
         sfpi::vInt zii = exexp(sfpi::reinterpret<sfpi::vFloat>(z));
         sfpi::vInt zif = sfpi::exman9(sfpi::reinterpret<sfpi::vFloat>(z));
@@ -21,18 +53,16 @@ sfpi_inline sfpi::vFloat _sfpu_expm1_(sfpi::vFloat val) {
         d2 = d1 * d2;
         zif = sfpu::_float_to_int32_(d2 * d3);
 
-        zii = sfpi::reinterpret<sfpi::vInt>(
-            sfpi::setexp(sfpi::reinterpret<sfpi::vFloat>(zif), 127U + zii));  // restore exponent
+        // Restore exponent
+        zii = sfpi::reinterpret<sfpi::vInt>(sfpi::setexp(sfpi::reinterpret<sfpi::vFloat>(zif), 127U + zii));
 
         y = sfpi::reinterpret<sfpi::vFloat>(zii) - sfpi::vConst1;
 
-        // When x is very small, exp(x) is very close to 1. Hence, for improved precision, we use Taylor expansion of
-        // expm1(x) = x + (x^2/2) + (x^3/3^2)
-        v_if(sfpi::abs(val) < sfpi::vFloat(1e-3f)) {
-            y = val + (sfpi::vFloat(0.5f) * val * val) + (val * val * val * sfpi::vFloat(1.166f));
-        }
-        v_endif;
         if constexpr (!is_fp32_dest_acc_en) {
+            // LRegs work on float32 data. If DST is bfloat16 then SFPSTORE will truncate it.
+            // This can reduce accuracy: for instance, 9**2 = 80.8 gets round to 80.5
+            // rather than 81 (which would have been correct).
+            // To avoid this issue, we explicitly convert to bfloat16 using round-to-nearest-even.
             y = sfpi::reinterpret<sfpi::vFloat>(sfpi::float_to_fp16b(y, 0));
         }
     }
@@ -52,6 +82,7 @@ inline void calculate_expm1() {
 
 template <bool APPROXIMATION_MODE>
 void expm1_init() {
+    // Polynomial coefficients for approximation of exp on [1; 2]
     sfpi::vConstFloatPrgm0 = 0.40196114e-7f;
     sfpi::vConstIntPrgm1 = 0xf94ee7;
     sfpi::vConstIntPrgm2 = 0x560e;
