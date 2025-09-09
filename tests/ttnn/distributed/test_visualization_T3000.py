19451dee0784a8397b4d4cef5f9e3c0fabdb1205
Siddharth Sundaram <ssundaram@tenstorrent.com>
2025-09-09 18:13:27 +0000

    added working 1d placements/override unit test

--- tests/ttnn/distributed/test_visualization_T3000.py
+++ tests/ttnn/distributed/test_visualization_T3000.py
@@ -118,7 +118,7 @@ def test_visualize_tensor_submesh_distribution(mesh_device):
 
 
 @pytest.mark.parametrize("mesh_device", [pytest.param((2, 4), id="2x4_grid")], indirect=True)
-def test_visualize_tensor_1d_placements(mesh_device):
+def test_visualize_tensor_1d_placements_and_override(mesh_device):
     rows, cols = mesh_device.shape
     tile_size = 32
     full_tensor = torch.rand((1, 1, tile_size * rows, tile_size * cols), dtype=torch.bfloat16)
@@ -126,7 +126,7 @@ def test_visualize_tensor_1d_placements(mesh_device):
         mesh_device,
         ttnn.MeshMapperConfig(
             placements=[ttnn.PlacementShard(3)],
-            mesh_shape_override=ttnn.MeshShape(1, 8),
+            mesh_shape_override=ttnn.MeshShape([8]),
         ),
     )
     ttnn_tensor = ttnn.from_torch(full_tensor, mesh_mapper=mesh_mapper, layout=ttnn.Layout.ROW_MAJOR)
