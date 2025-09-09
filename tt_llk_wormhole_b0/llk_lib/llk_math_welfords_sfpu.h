838bf451c51d91210bc99c29c1946cfa153e3482
Vasisht Suresh <vsuresh@tenstorrent.com>
2025-09-09 08:21:12 -0700

    Vsuresh tt/welfords llk (#588)
    ### Ticket
    <!-- Link to Github Issue -->
    This is a non documented issue, but related to 
    
    [ttnn.layer_norm causes low accuracy in mixer_b16_224.goog_in21k model
    in pytorch2.0_ttnn
    project](https://github.com/tenstorrent/tt-metal/issues/18548)
    
    ### Problem description
    <!-- Provide context for the problem. -->
    
    This algorithm increase the performance of the layernorm kernel as well
    the accuracy of layernorm. With the main focus being to help with
    accuracy.
    
    Added a new LLk for WH.
    
    Will add for BH as well
    ### What's changed
    Introduction on how welfords uses a recurrence relation to calculate
    mean and variance simultaneously
    https://changyaochen.github.io/welford/
    
    I implemented a new LLK that calculates welfords algorithm for mean and
    variance along the column dimension.
    
    Each row of inputs are seen as independent entries, and the row beneath
    is correlated index wise with the row above.
    
    The LLK then does a max of 32 iterations of welfords.
    
    It then saves the mean to dst1 and variance to dst2. 
    
    The input is sent to dst0.
    
    
    This LLK works similar to reduce_tile, where it accumulates the past
    mean and variance values into dst1 and dst2 respectively.
    
    
    <!-- Describe the approach used to solve the problem.
    Summarize the changes made and its impact. -->
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [X] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    
    ---------
    
    Co-authored-by: Nikhil Soraba <nsoraba@tenstorrent.com>
    Co-authored-by: Filip Vranic <fvranic@tenstorrent.com>

---
+++ tt_llk_wormhole_b0/llk_lib/llk_math_welfords_sfpu.h
@@ -0,0 +1,68 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+#include <type_traits>
+
+#include "ckernel_globals.h"
+#include "ckernel_include.h"
+#include "ckernel_ops.h"
+#include "ckernel_sfpu.h"
+#include "ckernel_template.h"
+#include "cmath_common.h"
+#include "llk_math_common.h"
+#include "llk_sfpu_types.h"
+#include "sfpu/ckernel_sfpu_welfords.h"
+
+// local function declarations
+inline void welfords_sfpu_configure_addrmod()
+{
+    addr_mod_t {
+        .srca = {.incr = 0},
+        .srcb = {.incr = 0},
+        .dest = {.incr = 0},
+    }
+        .set(ADDR_MOD_7);
+}
+
+inline void welfords_sfpu_configure_mop();
+
+template <DstSync Dst>
+inline void _llk_math_welfords_sfpu_start_(const uint dst_index)
+{
+    math::set_dst_write_addr<DstTileLayout::Default, DstTileShape::Tile32x32>(dst_index);
+
+    math::set_addr_mod_base();
+    TTI_STALLWAIT(p_stall::STALL_SFPU, p_stall::MATH);
+}
+
+inline void _llk_math_welfords_sfpu_done_()
+{
+    math::clear_dst_reg_addr();
+
+    TTI_STALLWAIT(p_stall::STALL_CFG, p_stall::WAIT_SFPU);
+    math::clear_addr_mod_base();
+}
+
+inline void _llk_math_welfords_sfpu_inc_dst_face_addr_()
+{
+    math::inc_dst_addr<8>();
+    math::inc_dst_addr<8>();
+}
+
+inline void _llk_math_welfords_sfpu_init_()
+{
+    sfpu::_init_sfpu_config_reg();
+    welfords_sfpu_configure_addrmod();
+    math::reset_counters(p_setrwc::SET_ABD_F);
+    lltt::record(0, 23);
+    _compute_welfords_math_();      // 9 TTI instructions
+    _welfords_load_initial_data_(); // 9 TTI instructions
+    TTI_SFPTRANSP(0, 0, 0, 0);
+    /*past_mean = dst1*/ TTI_SFPLOAD(p_sfpu::LREG4, 0, ADDR_MOD_3, 64);
+    /*past_var = dst2*/ TTI_SFPLOAD(p_sfpu::LREG5, 0, ADDR_MOD_3, 128);
+    // wiping LREG 6 and 7 since they may be filled with garbage data
+    TTI_SFPLOADI(p_sfpu::LREG6, 0, 0);
+    TTI_SFPLOADI(p_sfpu::LREG7, 0, 0);
+}
