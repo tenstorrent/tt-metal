1ba4b6fb518a56acad2b3d4d91b1e2be48549aaa
riverwuTT <riverwu@tenstorrent.com>
2025-09-09 16:39:14 +0000

    move max_runtime_args

--- tests/tt_metal/tt_metal/dispatch/random_program_fixture.hpp
+++ tests/tt_metal/tt_metal/dispatch/random_program_fixture.hpp
@@ -11,7 +11,7 @@
 #include <tt-metalium/tt_metal.hpp>
 #include <gtest/gtest.h>
 #include <tt-metalium/circular_buffer_constants.h>
-#include <tt-metalium/kernel.hpp>
+#include <tt-metalium/kernel_types.hpp>
 #include <tt-metalium/tt_backend_api_types.hpp>
 #include <tt-metalium/semaphore.hpp>
 #include "dispatch_test_utils.hpp"
--- tt_metal/api/tt-metalium/kernel.hpp
+++ tt_metal/api/tt-metalium/kernel.hpp
@@ -4,13 +4,6 @@
 
 #pragma once
 
-#include <cstdint>
-
 namespace tt::tt_metal {
-
-// 341 = (4096/(3 * sizeof(uint32_t)), where
-// - 4096 - packet size in dispatch
-// - 3 - number of kernels per tensix
-constexpr uint32_t max_runtime_args = 341;
 
 }  // namespace tt::tt_metal
