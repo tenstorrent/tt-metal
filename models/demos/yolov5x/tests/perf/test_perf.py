dcf13e00d01deb6de38b5791f1281bcd07e6c410
Sudharsan-V <sudharsan.vijayaraghavan@multicorewareinc.com>
2025-09-10 09:46:48 +0000

    C3 conv sharding, sppf S2I changes

--- models/demos/yolov5x/tests/perf/test_perf.py
+++ models/demos/yolov5x/tests/perf/test_perf.py
@@ -13,7 +13,7 @@ from models.utility_functions import run_for_wormhole_b0
 @pytest.mark.parametrize(
     "batch_size, expected_perf",
     [
-        [1, 55],
+        [1, 71],
     ],
 )
 @pytest.mark.models_device_performance_bare_metal
