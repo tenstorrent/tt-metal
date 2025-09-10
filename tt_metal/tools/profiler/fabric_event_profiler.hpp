c4a56ce5c916271e397cb3d4c06d5103b0b9dcf3
Sohaib Nadeem <sohaibnadeem@tenstorrent.com>
2025-09-10 10:16:06 -0400

    Add NoC tracing support for 2d routing fabric configs (low-latency mode) (#26738)
    ### Ticket
    Link to Github Issue: https://github.com/tenstorrent/tt-npe/issues/72
    
    ### Problem description
    Provide context for the problem.
    
    ### What's changed
    Describe the approach used to solve the problem.
    Summarize the changes made and its impact.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-quick-trigger.yaml)
    CI passes (if applicable)
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [x] New/Existing tests provide coverage for changes:
    test_fabric_event_profiler_2d in test_device_profiler.py

--- tt_metal/tools/profiler/fabric_event_profiler.hpp
+++ tt_metal/tools/profiler/fabric_event_profiler.hpp
@@ -12,6 +12,54 @@
 
 namespace kernel_profiler {
 
+// how slow is this? alternative is sotring entire route buffer which isn't ideal either...
+template <uint32_t STATIC_ID = 12345>
+FORCE_INLINE void recordRoutingFields2D(
+    const volatile tt::tt_fabric::LowLatencyMeshRoutingFields& routing_fields, const volatile uint8_t* route_buffer) {
+    KernelProfilerNocEventMetadata ev_md;
+    auto& routing_fields_2d = ev_md.data.fabric_routing_fields_2d;
+    routing_fields_2d.noc_xfer_type = KernelProfilerNocEventMetadata::NocEventType::FABRIC_ROUTING_FIELDS_2D;
+
+    // dimension order routing: first we have N/S forwarding with possible branching/local writes for mcast
+    uint8_t total_hops = 0;
+    while (route_buffer[total_hops] & tt::tt_fabric::LowLatencyMeshRoutingFields::WRITE_AND_FORWARD_NS) {
+        total_hops++;
+    }
+
+    routing_fields_2d.ns_hops = total_hops;
+
+    // compute e/w hops and check for e/w line mcast
+    while (route_buffer[total_hops] != tt::tt_fabric::LowLatencyMeshRoutingFields::NOOP) {
+        total_hops++;
+    }
+
+    // Look at last entry in buffer to check if west branch exists
+    // If west branch exists, compute west hops and east hops as remaining
+    // Otherwise, we only have east hops (which is trivially to 0 if we have no e/w hops at all)
+    if (route_buffer[total_hops - 1] == tt::tt_fabric::LowLatencyMeshRoutingFields::FORWARD_EAST) {
+        routing_fields_2d.w_hops = total_hops - routing_fields.branch_west_offset;
+        routing_fields_2d.e_hops = routing_fields.branch_west_offset - routing_fields_2d.ns_hops;
+    } else {
+        routing_fields_2d.e_hops = total_hops - routing_fields_2d.ns_hops;
+    }
+
+    // look at first entries of trunk/branches in buffer to check for N/S line mcast, E/W line mcast, or 2d mcast
+    // the last check is for the 1N1E1W edge case (which is a 2d mcast)
+    if (routing_fields_2d.ns_hops > 0 &&
+            (route_buffer[0] & tt::tt_fabric::LowLatencyMeshRoutingFields::WRITE_AND_FORWARD_NS) ==
+                tt::tt_fabric::LowLatencyMeshRoutingFields::WRITE_AND_FORWARD_NS ||
+        routing_fields_2d.e_hops > 0 && route_buffer[routing_fields.branch_east_offset] ==
+                                            tt::tt_fabric::LowLatencyMeshRoutingFields::WRITE_AND_FORWARD_EW ||
+        routing_fields_2d.w_hops > 0 && route_buffer[routing_fields.branch_west_offset] ==
+                                            tt::tt_fabric::LowLatencyMeshRoutingFields::WRITE_AND_FORWARD_EW ||
+        routing_fields_2d.e_hops > 0 && routing_fields_2d.w_hops > 0) {
+        routing_fields_2d.is_mcast = true;
+    }
+
+    kernel_profiler::flush_to_dram_if_full<kernel_profiler::DoingDispatch::DISPATCH>();
+    kernel_profiler::timeStampedData<STATIC_ID, kernel_profiler::DoingDispatch::DISPATCH>(ev_md.asU64());
+}
+
 void record_fabric_header(const volatile PACKET_HEADER_TYPE* fabric_header_ptr) {
     // determine routing fields type at compile time
     KernelProfilerNocEventMetadata::FabricPacketType routing_fields_type;
@@ -23,6 +71,7 @@ void record_fabric_header(const volatile PACKET_HEADER_TYPE* fabric_header_ptr) 
         routing_fields_type = KernelProfilerNocEventMetadata::FabricPacketType::REGULAR;
     }
 
+    // first profiler packet stores XY address data as well as packet type tag (used to decode routing fields)
     auto noc_send_type = fabric_header_ptr->get_noc_send_type();
     switch (noc_send_type) {
         case tt::tt_fabric::NocSendType::NOC_UNICAST_WRITE: {
@@ -30,8 +79,7 @@ void record_fabric_header(const volatile PACKET_HEADER_TYPE* fabric_header_ptr) 
             noc_event_profiler::recordFabricNocEvent(
                 KernelProfilerNocEventMetadata::NocEventType::FABRIC_UNICAST_WRITE,
                 routing_fields_type,
-                unicast_write_cmd.noc_address,
-                fabric_header_ptr->routing_fields.value);
+                unicast_write_cmd.noc_address);
             break;
         }
         case tt::tt_fabric::NocSendType::NOC_UNICAST_ATOMIC_INC: {
@@ -39,8 +87,7 @@ void record_fabric_header(const volatile PACKET_HEADER_TYPE* fabric_header_ptr) 
             noc_event_profiler::recordFabricNocEvent(
                 KernelProfilerNocEventMetadata::NocEventType::FABRIC_UNICAST_ATOMIC_INC,
                 routing_fields_type,
-                unicast_write_cmd.noc_address,
-                fabric_header_ptr->routing_fields.value);
+                unicast_write_cmd.noc_address);
             break;
         }
         case tt::tt_fabric::NocSendType::NOC_FUSED_UNICAST_ATOMIC_INC: {
@@ -48,8 +95,7 @@ void record_fabric_header(const volatile PACKET_HEADER_TYPE* fabric_header_ptr) 
             noc_event_profiler::recordFabricNocEvent(
                 KernelProfilerNocEventMetadata::NocEventType::FABRIC_FUSED_UNICAST_ATOMIC_INC,
                 routing_fields_type,
-                unicast_write_cmd.noc_address,
-                fabric_header_ptr->routing_fields.value);
+                unicast_write_cmd.noc_address);
             break;
         }
         case tt::tt_fabric::NocSendType::NOC_UNICAST_INLINE_WRITE: {
@@ -57,8 +103,7 @@ void record_fabric_header(const volatile PACKET_HEADER_TYPE* fabric_header_ptr) 
             noc_event_profiler::recordFabricNocEvent(
                 KernelProfilerNocEventMetadata::NocEventType::FABRIC_UNICAST_INLINE_WRITE,
                 routing_fields_type,
-                unicast_write_cmd.noc_address,
-                fabric_header_ptr->routing_fields.value);
+                unicast_write_cmd.noc_address);
             break;
         }
         case tt::tt_fabric::NocSendType::NOC_UNICAST_SCATTER_WRITE: {
@@ -68,8 +113,7 @@ void record_fabric_header(const volatile PACKET_HEADER_TYPE* fabric_header_ptr) 
                 routing_fields_type,
                 unicast_write_cmd.noc_address,
                 unicast_write_cmd.chunk_size,
-                NOC_SCATTER_WRITE_MAX_CHUNKS,
-                fabric_header_ptr->routing_fields.value);
+                NOC_SCATTER_WRITE_MAX_CHUNKS);
             break;
         }
         case tt::tt_fabric::NocSendType::NOC_MULTICAST_WRITE: {
@@ -80,8 +124,7 @@ void record_fabric_header(const volatile PACKET_HEADER_TYPE* fabric_header_ptr) 
                 mcast_write_cmd.noc_x_start,
                 mcast_write_cmd.noc_y_start,
                 mcast_write_cmd.mcast_rect_size_x,
-                mcast_write_cmd.mcast_rect_size_y,
-                fabric_header_ptr->routing_fields.value);
+                mcast_write_cmd.mcast_rect_size_y);
             break;
         }
         case tt::tt_fabric::NocSendType::NOC_MULTICAST_ATOMIC_INC: {
@@ -92,10 +135,20 @@ void record_fabric_header(const volatile PACKET_HEADER_TYPE* fabric_header_ptr) 
                 mcast_write_cmd.noc_x_start,
                 mcast_write_cmd.noc_y_start,
                 mcast_write_cmd.size_x,
-                mcast_write_cmd.size_y,
-                fabric_header_ptr->routing_fields.value);
+                mcast_write_cmd.size_y);
             break;
         }
+    }
+
+    // following profiler event just stores the routing fields
+    if constexpr (std::is_base_of_v<tt::tt_fabric::LowLatencyMeshRoutingFields, ROUTING_FIELDS_TYPE>) {
+#if defined(FABRIC_2D) && !defined(DYNAMIC_ROUTING_ENABLED)
+        recordRoutingFields2D(fabric_header_ptr->routing_fields, fabric_header_ptr->route_buffer);
+#endif
+    } else if constexpr (std::is_base_of_v<tt::tt_fabric::LowLatencyRoutingFields, ROUTING_FIELDS_TYPE>) {
+        noc_event_profiler::recordRoutingFields1D(fabric_header_ptr->routing_fields.value);
+    } else if constexpr (std::is_base_of_v<tt::tt_fabric::RoutingFields, ROUTING_FIELDS_TYPE>) {
+        noc_event_profiler::recordRoutingFields1D(fabric_header_ptr->routing_fields.value);
     }
 }
 }  // namespace kernel_profiler
