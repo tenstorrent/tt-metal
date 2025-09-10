ba992d5ba0623ff1a3f489b1641af0c7229c5599
riverwuTT <riverwu@tenstorrent.com>
2025-09-10 18:01:45 +0000

    fold config class in to avoid spamming namespace

--- tt_metal/impl/kernels/kernel_impl.hpp
+++ tt_metal/impl/kernels/kernel_impl.hpp
@@ -45,10 +45,10 @@ struct KernelSource {
 };
 
 
-using Config = std::variant<DataMovementConfig, EthernetConfig, ComputeConfig>;
-
 class Kernel {
 public:
+    using Config = std::variant<DataMovementConfig, EthernetConfig, ComputeConfig>;
+
     virtual ~Kernel() = default;
 
     std::string name() const;
