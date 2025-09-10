116eea9e56abd655ebae56a82f93a58464af0df8
Tomer Levin <tlevin@tenstorrent.com>
2025-09-10 20:15:53 +0000

    wip
    Signed-off-by: Tomer Levin <tlevin@tenstorrent.com>

---
+++ artifacts/prof_runs/_bench_runner_1757534847979.py
@@ -0,0 +1,28 @@
+import subprocess, sys
+
+subprocess.run(
+    [
+        "build/test/tt_metal/tt_fabric/bench_unicast",
+        "--src-dev",
+        "0:0",
+        "--dst-dev",
+        "0:1",
+        "--size",
+        "262144",
+        "--page",
+        "2048",
+        "--send-core",
+        "0,0",
+        "--recv-core",
+        "1,0",
+        "--iters",
+        "5",
+        "--warmup",
+        "1",
+        "--format",
+        "csv",
+        "--csv",
+        "artifacts/unicast_sweep.csv",
+    ],
+    check=True,
+)
