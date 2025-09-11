5743fb5b80f61bb7101c38be84796cb32c53d3fe
yugi957 <atul.krishnadas@outlook.com>
2025-09-11 21:04:25 +0000

    untilize op retilize

--- ttnn/cpp/ttnn/operations/experimental/slice_write/slice_write.cpp
+++ ttnn/cpp/ttnn/operations/experimental/slice_write/slice_write.cpp
@@ -19,8 +19,8 @@ namespace ttnn::operations::experimental {
 
 ttnn::Tensor SliceWriteOperation::invoke(
     const ttnn::Tensor& input_tensor,
-    const ttnn::Tensor& output_tensor,
-    // ttnn::Tensor& output_tensor,
+    // const ttnn::Tensor& output_tensor,
+    ttnn::Tensor& output_tensor,
     const ttnn::SmallVector<uint32_t>& begins,
     const ttnn::SmallVector<uint32_t>& ends,
     const ttnn::SmallVector<uint32_t>& step) {
@@ -30,21 +30,19 @@ ttnn::Tensor SliceWriteOperation::invoke(
 
     bool no_step = std::all_of(step.begin(), step.end(), [](uint32_t s) { return s == 1; });
 
-    // TT_FATAL(no_step, "Slice Write does not support strides");
-
-    // bool rm_only_not_sharded = (input_tensor.layout() == Layout::TILE || output_tensor.layout() == Layout::TILE)
-    //                             && !(input_tensor.is_sharded() || output_tensor.is_sharded());
-    // std::cout << "rm_only_not_sharded: " << rm_only_not_sharded << std::endl; // --- IGNORE ---
-    // ttnn::Tensor input = input_tensor;
-    // if (rm_only_not_sharded) {
-    //     input = ttnn::to_layout(input_tensor, Layout::ROW_MAJOR);
-    // }
-
-    bool rm_only = !no_step && input_tensor.layout() == Layout::TILE;
+    bool rm_only_not_sharded = (input_tensor.layout() == Layout::TILE || output_tensor.layout() == Layout::TILE) &&
+                               !(input_tensor.is_sharded() || output_tensor.is_sharded());
+    std::cout << "rm_only_not_sharded: " << rm_only_not_sharded << std::endl;  // --- IGNORE ---
     ttnn::Tensor input = input_tensor;
-    if (rm_only) {
+    if (rm_only_not_sharded) {
         input = ttnn::to_layout(input_tensor, Layout::ROW_MAJOR);
     }
+
+    // bool rm_only = !no_step && input_tensor.layout() == Layout::TILE;
+    // ttnn::Tensor input = input_tensor;
+    // if (rm_only) {
+    //     input = ttnn::to_layout(input_tensor, Layout::ROW_MAJOR);
+    // }
 
     TT_FATAL(!output_tensor.is_sharded(), "Slice Write currently doesn't support sharded output tensors.");
     const bool tiled = input.layout() == Layout::TILE;
@@ -136,14 +134,17 @@ ttnn::Tensor SliceWriteOperation::invoke(
         log_debug(tt::LogOp, "Invoking SliceWriteDeviceOperation");
 
         // If the operation has stride and output is tiled, convert output to RM
-        // if (rm_only_not_sharded) {
-        //     output_tensor = ttnn::to_layout(output_tensor, Layout::ROW_MAJOR);
-        // }
+        if (rm_only_not_sharded) {
+            output_tensor = ttnn::to_layout(output_tensor, Layout::ROW_MAJOR);
+        }
         (void)tt::tt_metal::operation::run(
             SliceWriteDeviceOperation{ttnn::Shape(begins), ttnn::Shape(padded_ends), ttnn::Shape(step)},
             {input},
             {},
             {output_tensor})[0];
+        if (rm_only_not_sharded) {
+            output_tensor = ttnn::to_layout(output_tensor, output_tensor.layout());
+        }
         return output_tensor;
     }
 
