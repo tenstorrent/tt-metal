fa709ccff7522306dd0f52b6dce76eaeedcec0fe
Sean Nijjar <sean.nijjar@gmail.com>
2025-09-11 21:07:16 +0000

    force BH fabric to use noc1 only
    force counter updates in `noc_async_write_one_packet_with_trid` for BH
    
    We must do this because in BH, inline writes spoof their inline writes
    by first writing the value to L1 and then sending a noc write out of that
    L1 location to avoid a HW bug. A result of this is that those inline write
    calls on BH implicitly barrier. That barrier call compares against
    `noc_nonposted_writes_num_issued`, which will NOT be updated if `update_counter` is false.
    
    Therefore, we must override `update_counter` to true if `posted` is false.
    
    f
    
    apply some workarounds to (temporarily) avoid incorrect binary size error
    
    squash with noc=1 fabric on BH changes

--- tt_metal/fabric/hw/inc/edm_fabric/fabric_edm_packet_transmission.hpp
+++ tt_metal/fabric/hw/inc/edm_fabric/fabric_edm_packet_transmission.hpp
@@ -120,7 +120,7 @@ FORCE_INLINE void flush_write_to_noc_pipeline(uint8_t rx_channel_id) {
 // Since we unicast to local, we must omit the packet header
 // This function only does reads, and within scope there are no modifications to the packet header
 __attribute__((optimize("jump-tables")))
-#ifndef FABRIC_2D
+#if !defined(FABRIC_2D) && !(defined(RING_TOPOLOGY) && defined(ARCH_BLACKHOLE))
 FORCE_INLINE
 #endif
     void
@@ -295,7 +295,7 @@ template <
     bool stateful_api,
     bool increment_pointers = true,
     uint8_t NUM_SENDER_BUFFERS>
-#ifndef FABRIC_2D
+#if !defined(FABRIC_2D) && !(defined(RING_TOPOLOGY) && defined(ARCH_BLACKHOLE))
 FORCE_INLINE
 #endif
     void
