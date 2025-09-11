8fc3b08d17fdba7b98ae3b6b36062ac5b23eaa4d
Sean Nijjar <sean.nijjar@gmail.com>
2025-09-11 17:46:21 +0000

    incremental change - some relocation

--- tt_metal/fabric/hw/inc/edm_fabric/fabric_erisc_datamover_channels.hpp
+++ tt_metal/fabric/hw/inc/edm_fabric/fabric_erisc_datamover_channels.hpp
@@ -20,7 +20,7 @@
 #include "fabric_edm_types.hpp"
 #include "edm_fabric_flow_control_helpers.hpp"
 #include "tt_metal/fabric/hw/inc/edm_fabric/fabric_connection_interface.hpp"
-
+#include "tt_metal/fabric/hw/inc/edm_fabric/datastructures/outbound_channel.hpp"
 #include "hostdevcommon/fabric_common.h"
 
 namespace tt::tt_fabric {
@@ -38,28 +38,6 @@ template <typename T>
 FORCE_INLINE auto wrap_increment(T val, size_t max) {
     return (val == max - 1) ? 0 : val + 1;
 }
-
-// A base sender channel interface class that will be specialized for different
-// channel architectures (e.g. static vs elastic sizing)
-template <typename HEADER_TYPE, uint8_t NUM_BUFFERS, typename DERIVED_T>
-class SenderEthChannelInterface {
-public:
-    explicit SenderEthChannelInterface() = default;
-
-    FORCE_INLINE void init(
-        size_t channel_base_address, size_t max_eth_payload_size_in_bytes, size_t header_size_bytes) {
-        static_cast<DERIVED_T*>(this)->init_impl(
-            channel_base_address, max_eth_payload_size_in_bytes, header_size_bytes);
-    }
-
-    FORCE_INLINE size_t get_cached_next_buffer_slot_addr() const {
-        return static_cast<const DERIVED_T*>(this)->get_cached_next_buffer_slot_addr_impl();
-    }
-
-    FORCE_INLINE void advance_to_next_cached_buffer_slot_addr() {
-        static_cast<DERIVED_T*>(this)->advance_to_next_cached_buffer_slot_addr_impl();
-    }
-};
 
 // This class implements the interface for static sized sender channels.
 // Static sized sender channels have a fixed number of buffer slots, defined
@@ -110,9 +88,6 @@ private:
     std::size_t cached_next_buffer_slot_addr;
     BufferIndex next_packet_buffer_index;
 };
-
-template <typename HEADER_TYPE, uint8_t NUM_BUFFERS>
-using SenderEthChannel = StaticSizedSenderEthChannel<HEADER_TYPE, NUM_BUFFERS>;
 
 template <typename HEADER_TYPE, uint8_t NUM_BUFFERS>
 class EthChannelBuffer final {
