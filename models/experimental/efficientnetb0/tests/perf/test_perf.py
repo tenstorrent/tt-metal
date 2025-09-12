74f1fcc21599ec6341c73e6436b7a898dc5fa81b
punith.sekar <punith.sekar@multicorewareinc.com>
2025-09-12 08:47:48 +0000

    update perf number

--- models/experimental/efficientnetb0/tests/perf/test_perf.py
+++ models/experimental/efficientnetb0/tests/perf/test_perf.py
@@ -15,7 +15,7 @@ from models.common.utility_functions import (
 @pytest.mark.parametrize(
     "batch_size, expected_perf",
     [
-        [1, 158],
+        [1, 161],
     ],
 )
 @pytest.mark.models_device_performance_bare_metal
