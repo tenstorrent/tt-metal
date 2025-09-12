650e04b0b8ebdb3173e3d82c15760891cbca0b02
punith.sekar <punith.sekar@multicorewareinc.com>
2025-09-12 10:30:42 +0000

    optimize conv

--- models/experimental/efficientnetb0/tt/efficientnetb0.py
+++ models/experimental/efficientnetb0/tt/efficientnetb0.py
@@ -55,13 +55,10 @@ class EfficientNetb0Conv2D:
         conv_config = ttnn.Conv2dConfig(
             weights_dtype=ttnn.bfloat8_b,
             shard_layout=self.shard_layout,
-            act_block_w_div=1,
-            transpose_shards=False,
             deallocate_activation=self.deallocate_activation,
-            enable_act_double_buffer=False,
-            enable_split_reader=False,
-            output_layout=self.output_layout,
-            reallocate_halo_output=False,
+            enable_act_double_buffer=True,
+            enable_weights_double_buffer=True if self.shard_layout == ttnn.TensorMemoryLayout.BLOCK_SHARDED else False,
+            enable_split_reader=True,
             reshard_if_not_optimal=True,
         )
 
