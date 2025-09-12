10602b7bb9ffaf040c173e6f55daf6ccfbe4fea9
riverwuTT <riverwu@tenstorrent.com>
2025-09-12 22:21:20 +0000

    add #warning for original assert.hpp for soft transition

---
+++ tt_metal/api/tt-metalium/assert.hpp
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#warning "This file is deprecated. Please use <tt_stl/assert.hpp> instead."
+#include <tt_stl/assert.hpp>
