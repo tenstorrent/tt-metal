32031ea9671a0d8f23efb85ee85cba74f0a527a1
Nigel Huang <nhuang@tenstorrent.com>
2025-09-12 00:13:33 +0000

    Increase timeouts

--- tt_metal/hw/inc/blackhole/dev_mem_map.h
+++ tt_metal/hw/inc/blackhole/dev_mem_map.h
@@ -45,6 +45,7 @@
 #define MEM_TRISC_LOCAL_SIZE (4 * 1024)
 
 // Taken from base fw executable. It places the stack here.
+#define MEM_ERISC_LOCAL_SIZE (8 * 1024)
 #define MEM_ERISC_BASE_FW_LOCAL_SIZE 0x700
 #define MEM_ERISC_KERNEL_SIZE (24 * 1024)
 #define MEM_ERISC_RESERVED1 0
