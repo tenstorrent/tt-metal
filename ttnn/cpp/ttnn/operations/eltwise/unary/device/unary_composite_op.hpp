84c749321db5d55ab8fe1c318f78f60db7c85946
Aswinmcw <azayasankaran@tenstorrent.com>
2025-09-12 07:07:41 +0000

    Remove rpow operation and related tests from the codebase
    - Deleted the rpow operation implementation and its associated bindings in the unary composite operations.
    - Updated the `test_unary_rpow_ttnn` to simplify the test case and remove unused assertions.
    - Cleaned up commented-out code related to rpow in various files to improve code clarity.

--- ttnn/cpp/ttnn/operations/eltwise/unary/device/unary_composite_op.hpp
+++ ttnn/cpp/ttnn/operations/eltwise/unary/device/unary_composite_op.hpp
@@ -32,7 +32,6 @@ enum class UnaryCompositeOpType {
     POLYGAMMA,
     LOGIT,
     LOGICAL_NOT_,
-    // RPOW,
     NORMALIZE_GLOBAL,
     FRAC,
 };
@@ -58,7 +57,6 @@ Tensor _triu(const Tensor&, int32_t diag = 0, const std::optional<MemoryConfig>&
 Tensor _polygamma(const Tensor&, int32_t, const std::optional<MemoryConfig>&);
 Tensor _logit(const Tensor& a, float eps = 0.0f, const std::optional<MemoryConfig>& output_mem_config = std::nullopt);
 Tensor _logical_not_(const Tensor&, const std::optional<MemoryConfig>&);
-// Tensor _rpow(const Tensor& a, float param, const std::optional<MemoryConfig>&);
 Tensor _normalize_global(const Tensor&, const std::optional<MemoryConfig>&);
 Tensor _frac(const Tensor&, const std::optional<MemoryConfig>&);
 
@@ -189,13 +187,6 @@ struct OpHandler<UnaryCompositeOpType::LOGICAL_NOT_> {
         return _logical_not_(t1, mem_cfg);
     }
 };
-
-// template <>
-// struct OpHandler<UnaryCompositeOpType::RPOW> {
-//     static Tensor handle(const Tensor& t1, float param, const std::optional<MemoryConfig>& mem_cfg) {
-//         return _rpow(t1, param, mem_cfg);
-//     }
-// };
 
 template <>
 struct OpHandler<UnaryCompositeOpType::FRAC> {
