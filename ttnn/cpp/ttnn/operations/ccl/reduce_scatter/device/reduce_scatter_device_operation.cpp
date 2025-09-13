9a3ae9c02ad62b0652535bb75b6d3c85ce571e61
Saad Jameel <sjameel@tenstorrent.com>
2025-09-13 23:18:34 +0000

    add proper fatals

--- ttnn/cpp/ttnn/operations/ccl/reduce_scatter/device/reduce_scatter_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/ccl/reduce_scatter/device/reduce_scatter_device_operation.cpp
@@ -18,11 +18,110 @@ ReduceScatterDeviceOperation::program_factory_t ReduceScatterDeviceOperation::se
 
 void ReduceScatterDeviceOperation::validate_on_program_cache_miss(
     const operation_attributes_t& operation_attributes, const tensor_args_t& tensor_args) {
+    validate_on_program_cache_hit(operation_attributes, tensor_args);
+    auto output_specs = compute_output_specs(operation_attributes, tensor_args);
     auto input_tensor = tensor_args.input_tensor;
+    auto page_size = input_tensor.buffer()->page_size();
+    TT_FATAL(
+        page_size % input_tensor.buffer()->alignment() == 0,
+        "page_size {} must be divisible by alignment {}",
+        page_size,
+        input_tensor.buffer()->alignment());
+    TT_FATAL(operation_attributes.cluster_axis.has_value(), "cluster_axis must be set");
+    TT_FATAL(operation_attributes.dim == 3, "dim must be 3");
+    TT_FATAL(input_tensor.storage_type() == StorageType::DEVICE, "input_tensor must be on device");
+    TT_FATAL(input_tensor.buffer() != nullptr, "input_tensor must have a buffer");
+    TT_FATAL(operation_attributes.num_links > 0, "num_links must be greater than 0");
+    TT_FATAL(
+        operation_attributes.topology == ::ttnn::ccl::Topology::Ring ||
+            operation_attributes.topology == ::ttnn::ccl::Topology::Linear,
+        "topology must be Ring or Linear");
+    TT_FATAL(input_tensor.storage_type() == StorageType::DEVICE, "input_tensor must be on device");
+    TT_FATAL(input_tensor.buffer() != nullptr, "input_tensor must have a buffer");
+
+    if (input_tensor.memory_config().memory_layout() == TensorMemoryLayout::BLOCK_SHARDED) {
+        TT_FATAL(input_tensor.memory_config().buffer_type() == BufferType::L1, "DRAM block sharding is not supported");
+    }
+
+    uint32_t axis = operation_attributes.cluster_axis.value();
+    log_debug(tt::LogOp, "axis: {}", axis);
+    TT_FATAL(axis == 0 || axis == 1, "axis must be 0 or 1");
+    auto mesh_view = input_tensor.device()->get_view();
+    uint32_t reduction_devices = axis == 0 ? mesh_view.num_rows() : mesh_view.num_cols();
+    log_debug(tt::LogOp, "reduction_devices: {}", reduction_devices);
+
+    if (tensor_args.optional_output_tensor.has_value()) {
+        const auto& output_tensor = tensor_args.optional_output_tensor.value();
+
+        TT_FATAL(
+            output_tensor.memory_config().memory_layout() == TensorMemoryLayout::INTERLEAVED ||
+                output_tensor.memory_config().memory_layout() == TensorMemoryLayout::WIDTH_SHARDED ||
+                output_tensor.memory_config().memory_layout() == TensorMemoryLayout::BLOCK_SHARDED ||
+                output_tensor.memory_config().memory_layout() == TensorMemoryLayout::HEIGHT_SHARDED,
+            "Unsupported output tensor memory layout {}.",
+            output_tensor.memory_config().memory_layout());
+
+        TT_FATAL(
+            output_tensor.storage_type() == StorageType::DEVICE,
+            "Operands to reduce_scatter_minimal_async need to be on device!");
+        TT_FATAL(
+            output_tensor.layout() == input_tensor.layout(),
+            "Error, Output tensor layout should be same as input tensor layout but has {}",
+            output_tensor.layout());
+        TT_FATAL(
+            output_tensor.dtype() == input_tensor.dtype(),
+            "Error, Output tensor dtype should be same as input tensor dtype but has {}",
+            output_tensor.dtype());
+        TT_FATAL(
+            output_tensor.tensor_spec().page_config() == input_tensor.tensor_spec().page_config(),
+            "Error, Output tensor page config should be same as input tensor page config but has {}",
+            output_tensor.tensor_spec().page_config());
+
+        // check the output tensor size
+        auto output_shape = output_tensor.padded_shape();
+        auto input_shape = input_tensor.padded_shape();
+        TT_FATAL(
+            output_shape.size() == input_shape.size(),
+            "Error, Output tensor shape should have same number of dimensions as input tensor but has {}",
+            output_shape.size());
+        for (size_t i = 0; i < input_shape.size(); ++i) {
+            if (i == operation_attributes.dim) {
+                TT_FATAL(
+                    output_shape[i] == input_shape[i] / reduction_devices,
+                    "Error, Output tensor shape at dimension {} should be {} but has {}",
+                    i,
+                    input_shape[i] / reduction_devices,
+                    output_shape[i]);
+            } else {
+                TT_FATAL(
+                    output_shape[i] == input_shape[i],
+                    "Error, Output tensor shape at dimension {} should be {} but has {}",
+                    i,
+                    input_shape[i],
+                    output_shape[i]);
+            }
+        }
+
+        // Don't support DRAM block sharding
+        if (output_tensor.memory_config().memory_layout() == TensorMemoryLayout::BLOCK_SHARDED) {
+            TT_FATAL(
+                output_tensor.memory_config().buffer_type() == BufferType::L1,
+                "We don't support output DRAM block sharding");
+        }
+    }
 }
 
 void ReduceScatterDeviceOperation::validate_on_program_cache_hit(
-    const operation_attributes_t& operation_attributes, const tensor_args_t& tensor_args) {}
+    const operation_attributes_t& operation_attributes, const tensor_args_t& tensor_args) {
+    if (tensor_args.optional_output_tensor.has_value()) {
+        auto output_specs = compute_output_specs(operation_attributes, tensor_args);
+        TT_FATAL(
+            tensor_args.optional_output_tensor.value().tensor_spec() == output_specs.at(1),
+            "Output tensor spec {} does not match computed output spec {}",
+            tensor_args.optional_output_tensor.value().tensor_spec(),
+            output_specs.at(1));
+    }
+}
 
 ReduceScatterDeviceOperation::spec_return_value_t ReduceScatterDeviceOperation::compute_output_specs(
     const operation_attributes_t& operation_attributes, const tensor_args_t& tensor_args) {
