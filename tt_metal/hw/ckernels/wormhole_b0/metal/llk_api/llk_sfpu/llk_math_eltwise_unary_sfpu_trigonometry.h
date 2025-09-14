572c5744aa3887cb4341587a26b70c4e6b290352
Mouliraj Elamurugan <mcw-melamurugan@ext.tenstorrent.com>
2025-09-14 22:12:59 +0530

    #21828: Migrate cosh as device op (#22345)
    ### Ticket
    Link to Github Issue #21828 
    
    ### Problem description
    cosh currently implemented as composite op
    
    ### What's changed
    Updated it as LLK version
    
    ### Perf Results:
    **cosh LLK**:
    
    1 tile : 7196ns (~56% faster)
    4k tile: 1456001ns(~45% faster)
    
    **cosh composite**:
    
    1 tile : 16585ns
    4k tile: 2675721ns
    
    ### ULP comparisons:
    https://github.com/tenstorrent/tt-metal/issues/21828#issuecomment-3265838396
    ### Checklist
    - [ ] [All post commit]
    - [ ] [Black post commit]

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_trigonometry.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_trigonometry.h
@@ -123,4 +123,16 @@ inline void llk_math_eltwise_unary_sfpu_atanh(uint dst_index, int vector_mode = 
         ckernel::sfpu::_calculate_atanh_<APPROXIMATE, is_fp32_dest_acc_en, ITERATIONS>, dst_index, vector_mode);
 }
 
+// cosh
+template <bool APPROXIMATE>
+inline void llk_math_eltwise_unary_sfpu_cosh_init() {
+    llk_math_eltwise_unary_sfpu_init<SfpuType::cosh, APPROXIMATE>(ckernel::sfpu::init_cosh<APPROXIMATE>);
+}
+
+template <bool APPROXIMATE, bool is_fp32_dest_acc_en, int ITERATIONS = 8>
+inline void llk_math_eltwise_unary_sfpu_cosh(uint dst_index, int vector_mode = (int)VectorMode::RC) {
+    _llk_math_eltwise_unary_sfpu_params_<APPROXIMATE>(
+        ckernel::sfpu::calculate_cosh<APPROXIMATE, is_fp32_dest_acc_en, ITERATIONS>, dst_index, vector_mode);
+}
+
 }  // namespace ckernel
