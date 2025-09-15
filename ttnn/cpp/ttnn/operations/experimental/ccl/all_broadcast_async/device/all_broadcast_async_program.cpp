03dccad7a1d028bfd337754d7c329c9ca7183a3d
daminakaTT <daminaka@tenstorrent.com>
2025-09-15 16:42:19 +0000

    Apply 1D API for broacast

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_broadcast_async/device/all_broadcast_async_program.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_broadcast_async/device/all_broadcast_async_program.cpp
@@ -118,16 +118,6 @@ tt::tt_metal::operation::ProgramWithCallbacks all_broadcast_async_multicore(
                              .set_page_size(src0_cb_index, buffer_page_size);
     }
     CreateCircularBuffer(program, sender_worker_core_range, cb_src0_config);
-    // Set aside a buffer we can use for storing packet headers in (particularly for atomic incs)
-    const auto reserved_packet_header_CB_index = tt::CB::c_in1;
-    static constexpr auto num_packet_headers_storable = 8;
-    auto packet_header_size_bytes = tt::tt_fabric::get_tt_fabric_packet_header_size_bytes();
-    tt::tt_metal::CircularBufferConfig cb_reserved_packet_header_config =
-        tt::tt_metal::CircularBufferConfig(
-            num_packet_headers_storable * packet_header_size_bytes * 2,
-            {{reserved_packet_header_CB_index, tt::DataFormat::RawUInt32}})
-            .set_page_size(reserved_packet_header_CB_index, packet_header_size_bytes);
-    CreateCircularBuffer(program, sender_worker_core_range, cb_reserved_packet_header_config);
 
     // Tensor Info
     const auto input_tensor_num_pages = input_tensor.buffer()->num_pages();
@@ -151,20 +141,16 @@ tt::tt_metal::operation::ProgramWithCallbacks all_broadcast_async_multicore(
     }
 
     std::vector<uint32_t> writer_compile_args = {
-        reserved_packet_header_CB_index,  // reserved_packet_header_cb_id
-        num_packet_headers_storable,      // num_packet_headers_storable
-        src0_cb_index,                    // cb0_id
-        num_pages_per_packet,             // packet_size_in_pages
-        op_config.get_page_size(),        // tensor0_page_size
-        num_targets_forward,              // num_targets_forward_direction
-        num_targets_backward,             // num_targets_backward_direction
+        src0_cb_index,              // cb0_id
+        num_pages_per_packet,       // packet_size_in_pages
+        op_config.get_page_size(),  // tensor0_page_size
+        num_targets_forward,        // num_targets_forward_direction
+        num_targets_backward,       // num_targets_backward_direction
     };
 
     if (!tilized) {
         writer_compile_args = {
-            reserved_packet_header_CB_index,  // reserved_packet_header_cb_id
-            num_packet_headers_storable,      // num_packet_headers_storable
-            src0_cb_index,                    // cb0_id
+            src0_cb_index,  // cb0_id
             num_width_shards > 1 ? buffer_page_size : page_size,
             row_size,
             max_packet_size,
@@ -255,12 +241,13 @@ tt::tt_metal::operation::ProgramWithCallbacks all_broadcast_async_multicore(
             barrier_core.x,                                  // barrier_sem_noc0_x
             barrier_core.y                                   // barrier_sem_noc0_y
         };
+        writer_rt_args.push_back((int)forward_device.has_value() + (int)backward_device.has_value());
         if (sharded) {
             shard_builder::extend_sharding_run_time_args(input_tensor, writer_rt_args);
         }
 
-        writer_rt_args.push_back(forward_device.has_value());
         if (forward_device.has_value()) {
+            writer_rt_args.push_back(0);  // fwd tag
             const auto sender_fabric_node_id =
                 tt::tt_fabric::get_fabric_node_id_from_physical_chip_id(sender_device->id());
             const auto forward_device_fabric_node_id =
@@ -268,8 +255,8 @@ tt::tt_metal::operation::ProgramWithCallbacks all_broadcast_async_multicore(
             tt::tt_fabric::append_fabric_connection_rt_args(
                 sender_fabric_node_id, forward_device_fabric_node_id, link, program, {core}, writer_rt_args);
         }
-        writer_rt_args.push_back(backward_device.has_value());
         if (backward_device.has_value()) {
+            writer_rt_args.push_back(1);  // bwd tag
             const auto sender_fabric_node_id =
                 tt::tt_fabric::get_fabric_node_id_from_physical_chip_id(sender_device->id());
             const auto backward_device_fabric_node_id =
