11bede0c85215070aab3c8c27f0b42d74d4e4850
Reku Dyu <rekudyu@protonmail.com>
2025-09-15 16:14:55 +0530

    added support for alpine linux
    * fixed PAGE_SIZE same variable name with library
    
    * link to execinfo as alpine linux doesn't have function such as backtrace and libexecinfo a seprate library provide it on alpine linux.

--- tests/tt_metal/tt_metal/debug_tools/watcher/test_noc_sanitize_delays.cpp
+++ tests/tt_metal/tt_metal/debug_tools/watcher/test_noc_sanitize_delays.cpp
@@ -75,7 +75,7 @@ void RunDelayTestOnCore(
     const uint32_t NUM_TILES = 4;
     const uint32_t DRAM_BUFFER_SIZE =
         SINGLE_TILE_SIZE * NUM_TILES;  // NUM_TILES of FP16_B, hard-coded in the reader/writer kernels
-    const uint32_t PAGE_SIZE = DRAM_BUFFER_SIZE;
+    const uint32_t PAGE_SZE = DRAM_BUFFER_SIZE;
 
     distributed::DeviceLocalBufferConfig dram_config{.page_size = PAGE_SIZE, .buffer_type = tt_metal::BufferType::DRAM};
     distributed::ReplicatedBufferConfig buffer_config{.size = DRAM_BUFFER_SIZE};
