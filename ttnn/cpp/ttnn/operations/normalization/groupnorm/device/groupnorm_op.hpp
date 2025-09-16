3bfc3f834f09bfebb167a51fd27866458e85a5d8
Rui Zhang <ruizhang@tenstorrent.com>
2025-09-16 03:08:06 +0000

    Remove using namespace tt::tt_metal from groupnorm_types.hpp

--- ttnn/cpp/ttnn/operations/normalization/groupnorm/device/groupnorm_op.hpp
+++ ttnn/cpp/ttnn/operations/normalization/groupnorm/device/groupnorm_op.hpp
@@ -7,6 +7,7 @@
 #include <optional>
 
 #include "ttnn/operations/core/compute_kernel/compute_kernel_config.hpp"
+#include "ttnn/operations/functions.hpp"
 #include "ttnn/tensor/tensor.hpp"
 #include "ttnn/run_operation.hpp"
 #include "groupnorm_types.hpp"
@@ -26,7 +27,7 @@ Ref: https://pytorch.org/docs/stable/generated/torch.nn.GroupNorm.html
 >>> output = m(input)
 */
 
-operation::ProgramWithCallbacks groupnorm_multi_core(
+tt::tt_metal::operation::ProgramWithCallbacks groupnorm_multi_core(
     const Tensor& a,
     const std::optional<const Tensor>& gamma,
     const std::optional<const Tensor>& beta,
@@ -41,7 +42,7 @@ operation::ProgramWithCallbacks groupnorm_multi_core(
     uint32_t num_out_blocks,
     const DeviceComputeKernelConfig& compute_kernel_config);
 
-operation::ProgramWithCallbacks groupnorm_multi_core_sharded(
+tt::tt_metal::operation::ProgramWithCallbacks groupnorm_multi_core_sharded(
     const Tensor& a,
     const std::optional<const Tensor>& gamma,
     const std::optional<const Tensor>& beta,
@@ -51,7 +52,7 @@ operation::ProgramWithCallbacks groupnorm_multi_core_sharded(
     float eps,
     uint32_t num_groups,
     uint32_t num_batches,
-    tt::tt_metal::DataType im_data_format,
+    DataType im_data_format,
     CoreCoord grid_size,
     bool inplace,
     const DeviceComputeKernelConfig& compute_kernel_config);
