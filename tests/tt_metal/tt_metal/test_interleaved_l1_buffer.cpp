585ef35483d02ca1e0c484f90346e80f8bfe7209
bcheemasabirTT <bcheemasabir@tenstorrent.com>
2025-09-16 22:28:11 +0000

    migrated test_interleaved_l1_buffer.cpp

--- tests/tt_metal/tt_metal/test_interleaved_l1_buffer.cpp
+++ tests/tt_metal/tt_metal/test_interleaved_l1_buffer.cpp
@@ -19,6 +19,7 @@
 #include <tt-metalium/buffer.hpp>
 #include <tt-metalium/buffer_types.hpp>
 #include <tt-logger/tt-logger.hpp>
+#include <tt-metalium/distributed.hpp>
 
 namespace tt {
 namespace tt_metal {
@@ -30,40 +31,43 @@ class IDevice;
 // TODO: explain what test does
 //////////////////////////////////////////////////////////////////////////////////////////
 using namespace tt;
+using namespace tt::tt_metal;
 
-bool test_interleaved_l1_buffer(tt_metal::IDevice* device, int num_pages_one, int num_pages_two, uint32_t page_size) {
+bool test_interleaved_l1_buffer(
+    std::shared_ptr<distributed::MeshDevice> mesh_device, int num_pages_one, int num_pages_two, uint32_t page_size) {
     bool pass = true;
+    auto& cq = mesh_device->mesh_command_queue();
 
     uint32_t buffer_size = num_pages_one * page_size;
 
-    tt_metal::InterleavedBufferConfig buff_config_0{
-        .device = device, .size = buffer_size, .page_size = page_size, .buffer_type = tt_metal::BufferType::L1};
-    auto interleaved_buffer = CreateBuffer(buff_config_0);
+    distributed::DeviceLocalBufferConfig local_config = {
+        .page_size = page_size, .buffer_type = tt_metal::BufferType::L1};
+    distributed::ReplicatedBufferConfig buffer_config = {.size = buffer_size};
+    auto interleaved_buffer = distributed::MeshBuffer::create(buffer_config, local_config, mesh_device.get());
 
     std::vector<uint32_t> host_buffer =
         create_random_vector_of_bfloat16(buffer_size, 100, std::chrono::system_clock::now().time_since_epoch().count());
 
-    tt_metal::detail::WriteToBuffer(interleaved_buffer, host_buffer);
+    distributed::WriteShard(cq, interleaved_buffer, host_buffer, distributed::MeshCoordinate(0, 0));
 
     std::vector<uint32_t> readback_buffer;
-    tt_metal::detail::ReadFromBuffer(interleaved_buffer, readback_buffer);
+    distributed::ReadShard(cq, readback_buffer, interleaved_buffer, distributed::MeshCoordinate(0, 0));
 
     pass &= (host_buffer == readback_buffer);
 
     uint32_t second_buffer_size = num_pages_two * page_size;
 
-    tt_metal::InterleavedBufferConfig buff_config_1{
-        .device = device, .size = second_buffer_size, .page_size = page_size, .buffer_type = tt_metal::BufferType::L1};
-
-    auto second_interleaved_buffer = CreateBuffer(buff_config_1);
+    distributed::ReplicatedBufferConfig second_buffer_config = {.size = second_buffer_size};
+    auto second_interleaved_buffer =
+        distributed::MeshBuffer::create(second_buffer_config, local_config, mesh_device.get());
 
     std::vector<uint32_t> second_host_buffer = create_random_vector_of_bfloat16(
         second_buffer_size, 100, std::chrono::system_clock::now().time_since_epoch().count());
 
-    tt_metal::detail::WriteToBuffer(second_interleaved_buffer, second_host_buffer);
+    distributed::WriteShard(cq, second_interleaved_buffer, second_host_buffer, distributed::MeshCoordinate(0, 0));
 
     std::vector<uint32_t> second_readback_buffer;
-    tt_metal::detail::ReadFromBuffer(second_interleaved_buffer, second_readback_buffer);
+    distributed::ReadShard(cq, second_readback_buffer, second_interleaved_buffer, distributed::MeshCoordinate(0, 0));
 
     pass &= (second_host_buffer == second_readback_buffer);
 
@@ -80,16 +84,16 @@ int main(int argc, char** argv) {
         //                      Device Setup
         ////////////////////////////////////////////////////////////////////////////
         int device_id = 0;
-        tt_metal::IDevice* device = tt_metal::CreateDevice(device_id);
+        std::shared_ptr<distributed::MeshDevice> mesh_device = distributed::MeshDevice::create_unit_mesh(device_id);
 
         uint32_t page_size = 2 * 1024;
 
         int num_bank_pages_one = 258;
         int num_bank_pages_two = 378;
 
-        pass &= test_interleaved_l1_buffer(device, num_bank_pages_one, num_bank_pages_two, page_size);
+        pass &= test_interleaved_l1_buffer(mesh_device, num_bank_pages_one, num_bank_pages_two, page_size);
 
-        pass &= tt_metal::CloseDevice(device);
+        pass &= mesh_device->close();
 
     } catch (const std::exception& e) {
         pass = false;
