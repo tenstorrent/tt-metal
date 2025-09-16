1a4500262792c50eaa01ee750ed260ed9df04a6e
Roman Furko <rfurko@tenstorrent.com>
2025-09-16 20:47:30 +0000

    Add model

--- tt-train/sources/ttml/models/distributed/pipeline_parallel_llama.cpp
+++ tt-train/sources/ttml/models/distributed/pipeline_parallel_llama.cpp
@@ -13,4 +13,159 @@ void PipelineParallelLlama::verify() const {
     }
 }
 
+PipelineParallelLlama::PipelineParallelLlama(
+    const LlamaConfig& config, const PipelineParallelConfig& pipeline_parallel_config, bool is_tensor_parallel) {
+    uint32_t vocab_size = config.vocab_size;
+    uint32_t max_sequence_length = config.max_sequence_length;
+    uint32_t embedding_dim = config.embedding_dim;
+    uint32_t num_heads = config.num_heads;
+    uint32_t num_groups = config.num_groups;
+    float dropout_prob = config.dropout_prob;
+    uint32_t num_blocks = config.num_blocks;
+    runner_type = config.runner_type;
+    float theta = config.theta;
+
+    fmt::print("Llama configuration:\n");
+    fmt::print("    Vocab size: {}\n", vocab_size);
+    fmt::print("    Max sequence length: {}\n", max_sequence_length);
+    fmt::print("    Embedding dim: {}\n", embedding_dim);
+    fmt::print("    Num heads: {}\n", num_heads);
+    fmt::print("    Num groups: {}\n", num_groups);
+    fmt::print("    Dropout probability: {}\n", dropout_prob);
+    fmt::print("    Num blocks: {}\n", num_blocks);
+    fmt::print("    Positional embedding type: RoPE\n");
+    fmt::print("    Runner type: {}\n", runner_type == RunnerType::Default ? "Default" : "Memory efficient");
+    fmt::print("    Theta: {}\n", theta);
+
+    fmt::println("  Pipeline parallel configuration:");
+    fmt::println("    Num blocks: {}\n", pipeline_parallel_config.num_blocks);
+    fmt::println("    Blocks per rank:");
+    for (const auto& [rank, blocks] : pipeline_parallel_config.blocks_per_rank) {
+        fmt::println("      Rank {}: {}", rank, blocks);
+    }
+
+    uint32_t vocab_size_divisible_by_32 = (vocab_size + 31) / 32 * 32;
+    if (max_sequence_length % 32 != 0) {
+        throw std::logic_error(fmt::format(
+            "Max sequence length should be divisible by 32 due to current limitations in tensor. Provided "
+            "max_sequence_length={}",
+            max_sequence_length));
+    }
+    if (embedding_dim % 32 != 0) {
+        throw std::logic_error(fmt::format(
+            "Embedding size should be divisible by 32 due to current limitations in tensor. Provided "
+            "embedding_dim={}",
+            embedding_dim));
+    }
+
+    ops::RopeScalingParams rope_scaling_params;
+    if (config.scaling_factor != 0.0F && config.original_context_length != 0U) {
+        rope_scaling_params.original_context_length = config.original_context_length;
+        rope_scaling_params.scaling_factor = config.scaling_factor;
+        rope_scaling_params.high_freq_factor = config.high_freq_factor;
+        rope_scaling_params.low_freq_factor = config.low_freq_factor;
+
+        fmt::print("    RoPE scaling enabled:\n");
+        fmt::print("        Scaling factor: {}\n", config.scaling_factor);
+        fmt::print("        Original context length: {}\n", config.original_context_length);
+        fmt::print("        High freq factor: {}\n", config.high_freq_factor);
+        fmt::print("        Low freq factor: {}\n", config.low_freq_factor);
+    }
+    rope_params = ops::build_rope_params(
+        /*sequence_length=*/max_sequence_length,
+        /*head_dim=*/embedding_dim / num_heads,
+        /*theta=*/theta,
+        /*rope_scaling_params=*/rope_scaling_params);
+
+    if (is_first_rank()) {
+        tok_emb = std::make_shared<ttml::modules::Embedding>(vocab_size_divisible_by_32, embedding_dim);
+    }
+
+    auto blocks_to_skip = get_blocks_to_skip();
+    auto blocks_to_load = get_blocks_to_load();
+
+    blocks.reserve(blocks_to_load);
+    for (uint32_t block_idx = blocks_to_skip; block_idx < blocks_to_skip + blocks_to_load; ++block_idx) {
+        if (is_tensor_parallel) {
+            blocks.push_back(std::make_shared<ttml::modules::distributed::DistributedLlamaBlock>(
+                embedding_dim, num_heads, num_groups, rope_params, dropout_prob));
+        } else {
+            blocks.push_back(std::make_shared<ttml::modules::distributed::LlamaBlock>(
+                embedding_dim, num_heads, num_groups, rope_params, dropout_prob));
+        }
+    }
+
+    if (is_last_rank()) {
+        ln_fc = std::make_shared<ttml::modules::RMSNormLayer>(embedding_dim);
+        if (is_tensor_parallel) {
+            fc = std::make_shared<ttml::modules::distributed::ColumnParallelLinear>(
+                embedding_dim, vocab_size, /* has_bias */ false, /* gather_output */ true);
+        } else {
+            fc = std::make_shared<ttml::modules::LinearLayer>(embedding_dim, vocab_size, /* bias */ false);
+        }
+    }
+
+    create_name("pipeline_parallel_llama");
+    if (tok_emb) {
+        register_module(tok_emb, "tok_emb");
+    }
+    for (uint32_t block_idx = 0; block_idx < blocks_to_load; ++block_idx) {
+        register_module(blocks[block_idx], fmt::format("llama_block_{}", block_idx + blocks_to_skip));
+    }
+    if (ln_fc) {
+        assert(fc != nullptr);
+        register_module(ln_fc, "ln_fc");
+        register_module(fc, "fc");
+    }
+
+    initialize_weights(*this);
+}
+
+autograd::TensorPtr PipelineParallelLlama::operator()(const autograd::TensorPtr& x, const autograd::TensorPtr& mask) {
+    auto out = x;
+    if (is_first_rank()) {
+        out = (*tok_emb)(out);
+    } else {
+        auto distributed_ctx = autograd::ctx().get_distributed_ctx();
+        auto rank = distributed_ctx.get_rank();
+        auto recv_rank = core::distributed::Rank(rank.get() - 1U);
+
+        auto batch_size = out->get_value().logical_shape()[0];
+        auto seq_len = out->get_value().logical_shape()[-1];
+
+        auto recv_tensor = ttnn::empty(
+            {batch_size, 1U, seq_len, embedding_dim},
+            ttnn::DataType::BFLOAT16,
+            ttnn::Layout::TILE,
+            &autograd::ctx().get_device());
+        out = autograd::create_tensor(recv_tensor);
+        out = ops::distributed::intermesh_recv(out, recv_rank);
+    }
+
+    for (auto& block : blocks) {
+        if (runner_type == RunnerType::MemoryEfficient) {
+            out = common::transformer::memory_efficient_runner(*block, out, mask);
+        } else if (runner_type == RunnerType::Default) {
+            out = (*block)(out, mask);
+        }
+    }
+
+    if (is_last_rank()) {
+        out = (*ln_fc)(out);
+        out = (*fc)(out);
+    } else {
+        auto distributed_ctx = autograd::ctx().get_distributed_ctx();
+        auto rank = distributed_ctx.get_rank();
+        auto send_rank = core::distributed::Rank(rank.get() + 1U);
+        out = ops::distributed::intermesh_send(out, send_rank);
+    }
+
+    return out;
+}
+
+std::shared_ptr<PipelineParallelLlama> create(
+    const LlamaConfig& config, const PipelineParallelConfig& pipeline_parallel_config, bool is_tensor_parallel) {
+    return std::make_shared<PipelineParallelLlama>(config, pipeline_parallel_config, is_tensor_parallel);
+}
+
 }  // namespace ttml::models::distributed::pipeline_parallel_llama
