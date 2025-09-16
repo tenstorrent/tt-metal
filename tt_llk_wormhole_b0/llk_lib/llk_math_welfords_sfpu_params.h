02391b7294e0cbcc81ea9f7273a33e9d2a71d0d4
Nikhil Soraba <nsoraba@tenstorrent.com>
2025-09-16 16:42:48 -0500

    Welfords LLK: Add an option to use a precomputed Look Up Table (LUT) for reciprocals (#649)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/22019
    
    ### Problem description
    At present, the Welfords LLK does a floating point division for every 32
    elements. This division is expensive on RISC-V cores. The floating point
    divisions are known reciprocals, i.e. 1, 1/2, 1/3 ..
    It is more efficient to precompute these reciprocals and use them at
    runtime.
    
    ### What's changed
    Following the scaffolding that was added in
    https://github.com/tenstorrent/tt-llk/pull/588, this PR lets the LLK use
    the look up table if one is provided. If it is not, then it falls back
    to the existing implementation.
    
    ### Type of change
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    
    ---------
    
    Co-authored-by: vsureshTT <vsuresh@tenstorrent.com>

--- tt_llk_wormhole_b0/llk_lib/llk_math_welfords_sfpu_params.h
+++ tt_llk_wormhole_b0/llk_lib/llk_math_welfords_sfpu_params.h
@@ -12,15 +12,7 @@
 template <typename Callable, typename... ARGS>
 inline void _llk_math_welfords_sfpu_params_(Callable&& sfpu_func, uint dst_index0, uint dst_index1, uint dst_index2, ARGS&&... args)
 {
-    // Compute minimum destination index
-    // not sure why we use the min. But this LLK is designed such that
-    //
-    // dst_index0 is the input tile (TILIZED FORMAT ONLY)
-    // dst_index1 is the previous mean. It will be zero otherwise
-    // dst_index1 is the previous var
-    //
-    uint dst_index = std::min(dst_index0, std::min(dst_index1, dst_index2));
-    _llk_math_eltwise_ternary_sfpu_start_<DST_SYNC_MODE>(dst_index); // Reuse same sync primitive
-    std::forward<Callable>(sfpu_func)(std::forward<ARGS>(args)...);  // Need to replace the above line with this
-    _llk_math_eltwise_ternary_sfpu_done_();                          // Finalize
+    _llk_math_eltwise_ternary_sfpu_start_<DST_SYNC_MODE>(dst_index0);
+    std::forward<Callable>(sfpu_func)(std::forward<ARGS>(args)...);
+    _llk_math_eltwise_ternary_sfpu_done_(); // Finalize
 }
