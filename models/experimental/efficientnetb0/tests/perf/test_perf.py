5516e7d4b4e956612011083ead8d04127f21601b
Punith Sekar <148861607+punithsekar@users.noreply.github.com>
2025-09-16 11:22:19 +0530

    EfficientNet B0 model optimization  (#28402)
    ### Ticket
    #28301 
    
    ### Problem description
    Optimize EfficientNet B0 Model
    
    ### What's changed
    Optimized model from 90 FPS to 157 FPS single device(N150)
    Recent perf,
    [performance_efficientnet_b0.csv](https://github.com/user-attachments/files/22347943/performance_efficientnet_b0.csv)
    
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/17740972525)
    CI passes
    - [x] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/runs/17741056663)
    CI passes (if applicable)
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/runs/17741396884)
    CI passes (if applicable)
    - [x]
    [PCC](https://github.com/tenstorrent/tt-metal/actions/runs/17741033843)
    CI passes
    - [x] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/17742216313)
    CI passes

--- models/experimental/efficientnetb0/tests/perf/test_perf.py
+++ models/experimental/efficientnetb0/tests/perf/test_perf.py
@@ -15,7 +15,7 @@ from models.common.utility_functions import (
 @pytest.mark.parametrize(
     "batch_size, expected_perf",
     [
-        [1, 95.6],
+        [1, 173.5],
     ],
 )
 @pytest.mark.models_device_performance_bare_metal
