5250406b201102e9df9860d1af67d480225cabd2
Sheran Cardoza <scardoza@tenstorrent.com>
2025-09-17 13:58:52 -0400

    #28260: Vector of mesh support for All Gather minimal (#28549)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/28260)
    
    ### Problem description
    AG minimal doesn't support vector of mesh devices (vector of tensors).
    
    ### What's changed
    I've updated the device discovery logic in AG minimal to handle vector
    of mesh devices. Generally, querying a single tensor's device() will
    return all mesh devices, but in this case a single tensor only maps to a
    single device. So we accumulate the device()'s of all tensors.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    (https://github.com/tenstorrent/tt-metal/actions/runs/17745408318)
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    (https://github.com/tenstorrent/tt-metal/actions/runs/17745449874)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-quick.yaml)
    CI passes (if applicable)
    - [ ] [Galaxy demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [x] T3000 frequent tests
    (https://github.com/tenstorrent/tt-metal/actions/runs/17745485870)
    - [ ] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/all_gather_async.hpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/all_gather_async.hpp
@@ -40,6 +40,23 @@ struct ExecuteAllGatherAsync {
         std::optional<uint32_t> num_workers_per_link = std::nullopt,
         std::optional<uint32_t> num_buffers_per_channel = std::nullopt);
 
+    // same as above but for vector of mesh
+    static std::vector<ttnn::Tensor> invoke(
+        const std::vector<ttnn::Tensor>& input_tensors,
+        const std::optional<ttnn::Tensor>& persistent_output_buffer,  // TODO should this be a vector of tensors?
+        int32_t dim,
+        const std::vector<global_semaphore::MultiDeviceGlobalSemaphore>& multi_device_global_semaphore,
+        uint32_t num_links = 1,
+        const std::optional<ttnn::MemoryConfig>& memory_config = std::nullopt,
+        ttnn::ccl::Topology topology = ttnn::ccl::Topology::Ring,
+        std::optional<tt::tt_metal::SubDeviceId> subdevice_id = std::nullopt,
+        std::optional<uint32_t> cluster_axis = std::nullopt,
+        bool use_optimal_ccl_for_llama = false,
+        const std::optional<std::vector<GlobalSemaphore>>& barrier_semaphore = std::nullopt,
+        std::optional<uint32_t> chunks_per_sync = std::nullopt,
+        std::optional<uint32_t> num_workers_per_link = std::nullopt,
+        std::optional<uint32_t> num_buffers_per_channel = std::nullopt);
+
     static ttnn::Tensor invoke(
         const ttnn::Tensor& input_tensor,
         int32_t dim,
