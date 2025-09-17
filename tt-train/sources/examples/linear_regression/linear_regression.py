fa6927d7f5bfa9965fcc360c5d15e5cf129ce1b0
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2025-09-17 19:27:00 +0000

    fixed tt-train buil;d

--- tt-train/sources/examples/linear_regression/linear_regression.py
+++ tt-train/sources/examples/linear_regression/linear_regression.py
@@ -2,12 +2,11 @@
 from sklearn import datasets
 from sklearn.metrics import mean_squared_error, r2_score
 
-import numpy
-
+import numpy as np
 import sys
 import os
 
-sys.path.append(f'{os.environ["HOME"]}/tt-metal/build/tt-train/sources/ttml/')
+sys.path.append(f'{os.environ["HOME"]}/git/tt-metal/tt-train/build/sources/ttml')
 import _ttml
 
 batch_size = 32
@@ -20,17 +19,17 @@ X, Y = datasets.make_regression(n_samples=batch_size * 16, n_features=2, n_targe
 # Use only one feature diabetes_X = diabetes_X.astype(numpy.float32)
 
 # Split the data into training/testing sets
-x_train = X[:-batch_size].astype(numpy.bfloat16)
-x_test = Y[-batch_size:].astype(numpy.bfloat16)
+x_train = X[:-eval_size].astype(np.float32)
+x_test = X[-eval_size:].astype(np.float32)
 
 # Split the targets into training/testing sets
-y_train = X[:-batch_size].astype(numpy.bfloat16)
-y_test = Y[-batch_size:].astype(numpy.bfloat16)
+y_train = Y[:-eval_size].astype(np.float32)
+y_test = Y[-eval_size:].astype(np.float32)
 
 model = _ttml.autograd.create_linear_regression_model(2, 1)
 loss_function = _ttml.autograd.mse_loss
-opt_config = _ttml.autograd.SGDConfig.make(0.01, 0.0, 0.0, 0.0, False)
-opt = _ttml.autograd.SGD({"weight": model.parameters()}, opt_config)
+opt_config = _ttml.autograd.SGDConfig.make(0.1, 0.0, 0.0, 0.0, False)
+opt = _ttml.autograd.SGD(model.parameters(), opt_config)
 
 model.train()
 
@@ -47,7 +46,7 @@ for _ in range(epoch_num):
         tt_y_slice = _ttml.autograd.Tensor.from_numpy(y_slice)
 
         opt.zero_grad()
-        tt_y_pred = model(tt_y_slice)
+        tt_y_pred = model(tt_x_slice)
         tt_loss = loss_function(tt_y_pred, tt_y_slice, _ttml.autograd.ReduceType.MEAN)
         print(f"{tt_loss.to_numpy()} {tt_loss.dtype()}")
         tt_loss.backward(False)
@@ -62,6 +61,6 @@ print("Coefficients: \n", model.get_weight_numpy())
 # 938.23786125
 # The mean squared error
 
-print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
+# print("Mean squared error: %.2f" % mean_squared_error(y_test, y_pred))
 # # The coefficient of determination: 1 is perfect prediction
-print("Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
+# print("Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
