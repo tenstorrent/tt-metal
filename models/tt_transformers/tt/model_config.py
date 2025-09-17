3fef326501ef93c9597c271cfabd0c9bf5288d2e
Maksim Tsishkouski <maksim_tsishkouski@epam.com>
2025-09-17 03:32:41 -0600

    fix is_llama_vision

--- models/tt_transformers/tt/model_config.py
+++ models/tt_transformers/tt/model_config.py
@@ -1756,7 +1756,9 @@ class ModelArgs:
 )"""
 
     def is_llama_vision(self):
-        return ("vision" in self.CKPT_DIR.lower()) and ("llama" in self.model_name.lower())
+        hf_llama_vision = ("vision" in self.CKPT_DIR.lower()) and ("llama" in self.model_name.lower())
+        llama_dir_llama_vision = self.vision_chunk_size > 0
+        return hf_llama_vision or llama_dir_llama_vision
 
     def get_state_dict_prefix(self, module_name, layer_num, is_vision=False):
         text_prefix = self.state_dict_text_prefix
