7d819f12638e53b778fa4151ad36d46326559ce4
William Ly <williamly@tenstorrent.com>
2025-09-18 15:55:36 -0400

    Revert "#11816: WORKAROUND: Add reshuffle_rows to Blackhole" (#28853)
    Reverts tenstorrent/tt-metal#28742
    
    Breaking APC ttnn and tt-train tests
    
    
    https://github.com/tenstorrent/tt-metal/actions/runs/17836351226/job/50721764982
    ```
    test_embedding_bw[input_dtype=DataType.BFLOAT16-output_dtype=DataType.BFLOAT16-batch_size=2-seq_len=64-embedding_dim=160-num_embeddings=96] assert False
    ```
    tt-train:
    https://github.com/tenstorrent/tt-metal/actions/runs/17836351226/job/50721764613
    ```
    The following tests FAILED:
    	187 - EmbeddingOpTest.EmbeddingForwardBackward (Failed)
    Errors while running CTest
    ```

--- tests/ttnn/unit_tests/operations/test_backward_embedding.py
+++ tests/ttnn/unit_tests/operations/test_backward_embedding.py
@@ -12,6 +12,7 @@ from models.utility_functions import skip_for_blackhole
 from loguru import logger
 
 
+@skip_for_blackhole("Fails on BH. Issue #11816")
 @pytest.mark.parametrize(
     "batch_size, seq_len, embedding_dim, num_embeddings",
     [
@@ -67,6 +68,7 @@ def test_embedding_bw(input_dtype, output_dtype, batch_size, seq_len, embedding_
     assert comp_pass
 
 
+@skip_for_blackhole("Fails on BH. Issue #11816")
 @pytest.mark.parametrize(
     "batch_size, seq_len, embedding_dim, num_embeddings",
     [
