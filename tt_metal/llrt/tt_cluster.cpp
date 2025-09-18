f81cc3a9a8df21b11047b19ce4c6817fa75461fc
Bojan Rosko <brosko@tenstorrent.com>
2025-09-18 15:59:49 +0000

    fix build

--- tt_metal/llrt/tt_cluster.cpp
+++ tt_metal/llrt/tt_cluster.cpp
@@ -72,7 +72,7 @@ tt::tt_metal::ClusterType Cluster::get_cluster_type_from_cluster_desc(
     const llrt::RunTimeOptions& rtoptions, const tt_ClusterDescriptor* cluster_desc) {
     if (rtoptions.get_simulator_enabled()) {
         auto soc_desc =
-            tt::umd::SocDescriptor::get_soc_descriptor_path_from_simulator_path(rtoptions.get_simulator_path());
+            tt::umd::SimulationDevice::get_soc_descriptor_path_from_simulator_path(rtoptions.get_simulator_path());
         auto arch = tt::umd::SocDescriptor::get_arch_from_soc_descriptor_path(soc_desc);
         if (arch == tt::ARCH::WORMHOLE_B0) {
             return tt::tt_metal::ClusterType::SIMULATOR_WORMHOLE_B0;
