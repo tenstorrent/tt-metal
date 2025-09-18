82de3cd32f05cb9294cc8754b8e3fd467ba5749f
Adam Thompson <athompson@tenstorrent.com>
2025-09-18 20:13:31 +0000

    fixup

--- tt-train/sources/ttml/nanobind/nb_autograd.cpp
+++ tt-train/sources/ttml/nanobind/nb_autograd.cpp
@@ -70,15 +70,14 @@ void py_module(nb::module_& m) {
         py_tensor.def("is_grad_initialized", &Tensor::is_grad_initialized);
         py_tensor.def_static(
             "from_numpy", [](nb::ndarray<> numpy_tensor) { return create_tensor(make_metal_tensor(numpy_tensor)); });
-        py_tensor.def(
-            "to_numpy", [](const std::shared_ptr<Tensor>& tensor, std::optional<tt::tt_metal::DataType> new_type) {
-                return make_numpy_tensor(tensor->get_value(PreferredPrecision::FULL), new_type);
-            });
+        py_tensor.def("to_numpy", [](const Tensor& tensor, std::optional<tt::tt_metal::DataType> new_type) {
+            return make_numpy_tensor(tensor.get_value(PreferredPrecision::FULL), new_type);
+        });
         py_tensor.def("to_string", [](const Tensor& tensor) {
             return tensor.get_value(PreferredPrecision::FULL).write_to_string();
         });
         py_tensor.def("shape", [](const Tensor& tensor) {
-            const tt::tt_metal::Shape& shape = tensor.get_value(PreferredPrecision::FULL).logical_shape();
+            const tt::tt_metal::Shape& shape = tensor.get_shape();
             nb::list ret;
             for (auto it = shape.cbegin(); it != shape.cend(); ++it) {
                 ret.append(*it);
