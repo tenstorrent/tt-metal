3bc2dfac7197842c4e8ddeb8675d2fafdd4cc922
Adam Thompson <athompson@tenstorrent.com>
2025-09-18 23:15:54 +0000

    add llama, mlp models

--- tt-train/sources/ttml/nanobind/nb_modules.cpp
+++ tt-train/sources/ttml/nanobind/nb_modules.cpp
@@ -10,10 +10,14 @@
 
 #include "autograd/auto_context.hpp"
 #include "autograd/autocast_tensor.hpp"
+#include "models/base_transformer.hpp"
 #include "models/gpt2.hpp"
 #include "models/linear_regression.hpp"
+#include "models/llama.hpp"
+#include "models/mlp.hpp"
 #include "modules/linear_module.hpp"
 #include "modules/module_base.hpp"
+#include "modules/multi_layer_perceptron.hpp"
 #include "nb_export_enum.hpp"
 #include "nb_fwd.hpp"
 #include "nb_util.hpp"
@@ -22,9 +26,16 @@ namespace ttml::modules {
 
 void py_module_types(nb::module_& m) {
     nb::export_enum<RunMode>(m);
+    nb::export_enum<models::llama::RunnerType>(m);
+    nb::export_enum<models::llama::WeightTyingType>(m);
 
     nb::class_<ModuleBase>(m, "ModuleBase");
+    nb::class_<models::BaseTransformer, ModuleBase>(m, "BaseTransformer");
     nb::class_<LinearLayer, ModuleBase>(m, "LinearLayer");
+    nb::class_<models::llama::LlamaConfig>(m, "LlamaConfig");
+    nb::class_<models::llama::Llama, models::BaseTransformer>(m, "Llama");
+    nb::class_<MultiLayerPerceptronParameters>(m, "MultiLayerPerceptronParameters");
+    nb::class_<MultiLayerPerceptron, ModuleBase>(m, "MultiLayerPerceptron");
 }
 
 void py_module(nb::module_& m) {
@@ -42,6 +53,12 @@ void py_module(nb::module_& m) {
     }
 
     {
+        auto py_base_transformer =
+            static_cast<nb::class_<models::BaseTransformer, ModuleBase>>(m.attr("BaseTransformer"));
+        py_base_transformer.def("load_from_safetensors", &models::BaseTransformer::load_from_safetensors);
+    }
+
+    {
         auto py_linear_layer = static_cast<nb::class_<LinearLayer, ModuleBase>>(m.attr("LinearLayer"));
         py_linear_layer.def(nb::init<uint32_t, uint32_t, bool>());
         py_linear_layer.def("__call__", &LinearLayer::operator());
@@ -52,7 +69,68 @@ void py_module(nb::module_& m) {
         });
     }
 
+    {
+        auto py_llama_config = static_cast<nb::class_<models::llama::LlamaConfig>>(m.attr("LlamaConfig"));
+        py_llama_config.def(
+            nb::init<
+                uint32_t,
+                uint32_t,
+                uint32_t,
+                std::optional<uint32_t>,
+                float,
+                float,
+                uint32_t,
+                uint32_t,
+                uint32_t,
+                models::llama::RunnerType,
+                models::llama::WeightTyingType,
+                float,
+                float,
+                float,
+                uint32_t>(),
+            nb::arg("num_heads") = 6U,
+            nb::arg("num_groups") = 3U,
+            nb::arg("embedding_dim") = 384U,
+            nb::arg("intermediate_dim") = std::nullopt,
+            nb::arg("dropout_prop") = 0.0F,
+            nb::arg("theta") = 10000.0F,
+            nb::arg("num_blocks") = 6U,
+            nb::arg("vocab_size") = 256U,
+            nb::arg("max_sequence_length") = 256U,
+            nb::arg("runner_type") = models::llama::RunnerType::Default,
+            nb::arg("weight_tying") = models::llama::WeightTyingType::Disabled,
+            nb::arg("scaling_factor") = 0.0F,
+            nb::arg("high_freq_factor") = 4.0F,
+            nb::arg("low_freq_factor") = 0.0F,
+            nb::arg("original_context_length") = 0U);
+
+        auto py_llama = static_cast<nb::class_<models::llama::LlamaConfig>>(m.attr("Llama"));
+        py_llama.def(nb::init<models::llama::LlamaConfig>());
+        py_llama.def_static(
+            "create", [](const models::llama::LlamaConfig& config) { return models::llama::create(config); });
+    }
+
+    {
+        auto py_mlp_params =
+            static_cast<nb::class_<MultiLayerPerceptronParameters>>(m.attr("MultiLayerPerceptronParameters"));
+        py_mlp_params.def(
+            "create",
+            [](uint32_t input_features, const std::vector<uint32_t>& hidden_features, uint32_t output_features) {
+                return MultiLayerPerceptronParameters{
+                    .input_features = input_features,
+                    .hidden_features = hidden_features,
+                    .output_features = output_features};
+            });
+
+        auto py_mlp = static_cast<nb::class_<MultiLayerPerceptron, ModuleBase>>(m.attr("MultiLayerPerceptron"));
+        py_mlp.def(nb::init<const MultiLayerPerceptronParameters&>());
+        py_mlp.def_static("create", [](const modules::MultiLayerPerceptronParameters& config) {
+            return models::mlp::create(config);
+        });
+    }
+
     m.def("create_linear_regression_model", &models::linear_regression::create);
+    m.def("load_gpt2_model_from_safetensors", &models::gpt2::load_model_from_safetensors);
     m.def("load_gpt2_model_from_safetensors", &models::gpt2::load_model_from_safetensors);
 }
 
