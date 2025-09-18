1c91e0661068ed8be3d25cc630bccf1ecb52eb39
Sean Nijjar <sean.nijjar@gmail.com>
2025-09-18 10:52:38 -0400

    cleanup stream reg inc value packing in fabric (#28735)
    cleanup to avoid any bugs when porting to new archs or in case there are
    bugs in current impl, fix is localized.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/17807629055
    (rebase+rerun failing job
    https://github.com/tenstorrent/tt-metal/actions/runs/17831563722 passes.
    There was a period in main with this job failing this way).
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable):
    https://github.com/tenstorrent/tt-metal/actions/runs/17807627075
    
    Co-authored-by: Sean Nijjar <snijjar@tenstorrent.com>

--- tt_metal/fabric/hw/inc/edm_fabric/fabric_router_adapter.hpp
+++ tt_metal/fabric/hw/inc/edm_fabric/fabric_router_adapter.hpp
@@ -232,13 +232,9 @@ public:
         const uint64_t noc_sem_addr = get_noc_addr(
             this->edm_noc_x, this->edm_noc_y, this->edm_buffer_remote_free_slots_update_addr, EDM_TO_DOWNSTREAM_NOC);
         noc_sem_addr_ = noc_sem_addr;
+        auto packed_val = pack_value_for_inc_on_write_stream_reg_write(-1);
         noc_inline_dw_write_set_state<true, true>(
-            noc_sem_addr,
-            (-1) << REMOTE_DEST_BUF_WORDS_FREE_INC,
-            0xF,
-            this->sync_noc_cmd_buf,
-            EDM_TO_DOWNSTREAM_NOC,
-            EDM_TO_DOWNSTREAM_NOC_VC);
+            noc_sem_addr, packed_val, 0xF, this->sync_noc_cmd_buf, EDM_TO_DOWNSTREAM_NOC, EDM_TO_DOWNSTREAM_NOC_VC);
     }
 
     FORCE_INLINE bool edm_has_space_for_packet() const {
@@ -327,8 +323,8 @@ private:
         if constexpr (stateful_api) {
             if constexpr (enable_deadlock_avoidance) {
                 if constexpr (vc1_has_different_downstream_dest) {
-                    noc_inline_dw_write<InlineWriteDst::REG>(
-                        noc_sem_addr_, (-1) << REMOTE_DEST_BUF_WORDS_FREE_INC, 0xf, noc);
+                    auto packed_val = pack_value_for_inc_on_write_stream_reg_write(-1);
+                    noc_inline_dw_write<InlineWriteDst::REG>(noc_sem_addr_, packed_val, 0xf, noc);
                 } else {
                     noc_inline_dw_write_with_state<true, false, true>(
                         0,  // val unused
@@ -344,9 +340,10 @@ private:
                     noc);
             }
         } else {
+            auto packed_val = pack_value_for_inc_on_write_stream_reg_write(-1);
             const uint64_t noc_sem_addr =
                 get_noc_addr(this->edm_noc_x, this->edm_noc_y, this->edm_buffer_remote_free_slots_update_addr, noc);
-            noc_inline_dw_write<InlineWriteDst::REG>(noc_sem_addr, (-1) << REMOTE_DEST_BUF_WORDS_FREE_INC, 0xf, noc);
+            noc_inline_dw_write<InlineWriteDst::REG>(noc_sem_addr, packed_val, 0xf, noc);
         }
         // Write to the atomic increment stream register (write of -1 will subtract 1)
         increment_local_update_ptr_val(worker_credits_stream_id, -1);
