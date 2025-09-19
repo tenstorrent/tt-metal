988012e4bf5ecbb4fb81d57a572b4652efa422a6
John Bauman <jbauman@tenstorrent.com>
2025-09-19 15:27:19 +0000

    format

--- tt_metal/api/tt-metalium/mesh_device.hpp
+++ tt_metal/api/tt-metalium/mesh_device.hpp
@@ -148,7 +148,6 @@ private:
     MemoryPinningParameters memory_pinning_params_{};
 
 public:
-
     MeshDevice(
         std::shared_ptr<ScopedDevices> scoped_devices,
         std::unique_ptr<MeshDeviceView> mesh_device_view,
@@ -335,9 +334,7 @@ public:
      * @return Unique pointer to the created PinnedMemory instance
      */
     std::unique_ptr<PinnedMemory> pin_memory(
-        const MeshCoordinateRangeSet& coordinate_range_set,
-        HostBuffer& host_buffer,
-        bool map_to_noc = false);
+        const MeshCoordinateRangeSet& coordinate_range_set, HostBuffer& host_buffer, bool map_to_noc = false);
 
     MemoryPinningParameters get_memory_pinning_parameters() const;
 
