bc15d582368bf9fc1f9b3b09f1b97a088e6de3a1
Ziemowit Bączewski <zbaczewski@tenstorrent.com>
2025-09-19 12:24:37 +0000

    Baseline momentum not written to dram

--- tt-train/sources/ttml/metal/optimizers/sgd_fused/device/kernels/compute/compute.cpp
+++ tt-train/sources/ttml/metal/optimizers/sgd_fused/device/kernels/compute/compute.cpp
@@ -20,6 +20,7 @@ constexpr auto cb_grad_idx = tt::CBIndex::c_1;
 
 constexpr auto cb_momentum_in_idx = tt::CBIndex::c_2;
 constexpr auto cb_momentum_out_idx = tt::CBIndex::c_3;
+constexpr auto cb_momentum_to_dram_idx = tt::CBIndex::c_4;
 
 constexpr auto cb_output_idx = tt::CBIndex::c_16;
 
@@ -33,18 +34,18 @@ void MAIN {
     uint32_t lr = get_arg_val<uint32_t>(runtime_args_counter++);
 
     init_sfpu(cb_grad_idx, cb_grad_idx);
-    init_sfpu(cb_momentum_in_idx, cb_momentum_out_idx);
+    // init_sfpu(cb_momentum_in_idx, cb_momentum_out_idx);
 
     for (uint32_t row = 0; row < num_rows_per_core; ++row) {
         for (uint32_t col = 0; col < Wt; col += block_size) {
             cb_wait_front(cb_grad_idx, block_size);
-            cb_wait_front(cb_momentum_in_idx, block_size);
+            // cb_wait_front(cb_momentum_in_idx, block_size);
 
             tile_regs_acquire();
             for (uint32_t block_idx = 0; block_idx < block_size; ++block_idx) {
                 copy_tile_init(cb_grad_idx);
                 const uint32_t update_register = block_idx;
-                copy_tile(cb_grad_idx, /* tile_idx */ block_idx, /* register_idx */ update_register);
+                copy_tile(cb_grad_idx, /* tile_idx */ block_idx, /* register_idx */ block_idx);
                 copy_tile_init(cb_momentum_in_idx);
                 const uint32_t momentum_register = block_size + block_idx;
                 copy_tile(cb_momentum_in_idx, /* tile_idx */ block_idx, /* register_idx */ momentum_register);
@@ -73,6 +74,7 @@ void MAIN {
             pack_and_push_block(cb_output_idx, block_size);
 
             cb_pop_front(cb_param_in_idx, block_size);
+            cb_pop_front(cb_momentum_out_idx, block_size);
         }
     }
 }
