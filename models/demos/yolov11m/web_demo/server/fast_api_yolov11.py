c435a26abc618d2041560052fe85e77b762bd757
Dimitri Gnidash <dimitri@tenstorrent.com>
2025-09-19 11:16:45 -0400

    Fix the webserver params for L1 and trace

--- models/demos/yolov11m/web_demo/server/fast_api_yolov11.py
+++ models/demos/yolov11m/web_demo/server/fast_api_yolov11.py
@@ -12,6 +12,7 @@ from PIL import Image
 
 import ttnn
 from models.demos.yolov11m.runner.performant_runner import YOLOv11PerformantRunner
+from models.demos.yolov11m.common import YOLOV11M_L1_SMALL_SIZE
 from models.experimental.yolo_common.yolo_web_demo.yolo_evaluation_utils import postprocess
 
 app = FastAPI(
@@ -36,7 +37,7 @@ logging.basicConfig(
 async def startup():
     global model
     device_id = 0
-    device = ttnn.CreateDevice(device_id, l1_small_size=24576, trace_region_size=3211264, num_command_queues=2)
+    device = ttnn.CreateDevice(device_id, l1_small_size=YOLOV11M_L1_SMALL_SIZE, trace_region_size=23887872, num_command_queues=2)
     device.enable_program_cache()
     model = YOLOv11PerformantRunner(device)
     model._capture_yolov11_trace_2cqs()
