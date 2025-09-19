ae5c8d8736e7c29610a6e2e8e2f1dbd75c110d64
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2025-09-19 10:22:30 +0200

    #28487: SDXL: revert #28488 (#28829)
    ### What's changed
    As [#28674](https://github.com/tenstorrent/tt-metal/pull/28674) reverted
    the original PR that caused SDXL pcc drop; reverting lowering of pcc
    thresholds intrduced in #28488
    
    ### Checklist
    - [x] [(Single-card) Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/17836312756)
    CI passes
    - [x] [Nightly tt-metal L2
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/17836338944)
    CI passes

--- models/experimental/stable_diffusion_xl_base/tests/pcc/test_module_tt_crossattndownblock2d.py
+++ models/experimental/stable_diffusion_xl_base/tests/pcc/test_module_tt_crossattndownblock2d.py
@@ -18,9 +18,8 @@ from models.experimental.stable_diffusion_xl_base.tests.test_common import SDXL_
 @pytest.mark.parametrize(
     "input_shape, temb_shape, encoder_shape, query_dim, num_attn_heads, out_dim, down_block_id, pcc",
     [
-        # TODO: Restore pcc after issue #28487 is resolved
-        ((1, 320, 64, 64), (1, 1280), (1, 77, 2048), 640, 10, 640, 1, 0.9966),
-        ((1, 640, 32, 32), (1, 1280), (1, 77, 2048), 1280, 20, 1280, 2, 0.9945),
+        ((1, 320, 64, 64), (1, 1280), (1, 77, 2048), 640, 10, 640, 1, 0.997),
+        ((1, 640, 32, 32), (1, 1280), (1, 77, 2048), 1280, 20, 1280, 2, 0.995),
     ],
 )
 @pytest.mark.parametrize("device_params", [{"l1_small_size": SDXL_L1_SMALL_SIZE}], indirect=True)
