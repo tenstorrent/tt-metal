01329ff69a072a7753391ac04a5b5324619fba63
Nigel Huang <nhuang@tenstorrent.com>
2025-09-19 18:28:15 +0000

    Fix padding

--- tt_metal/llrt/hal/tt-1xx/blackhole/bh_hal_eth_asserts.hpp
+++ tt_metal/llrt/hal/tt-1xx/blackhole/bh_hal_eth_asserts.hpp
@@ -26,6 +26,10 @@ static constexpr uint32_t ETH_PROFILER_CHECK =
     TT_ARCH_MAX_NOC_WRITE_ALIGNMENT;
 static_assert(ETH_LAUNCH_CHECK == 0);
 static_assert(ETH_PROFILER_CHECK == 0);
+static_assert(
+    (MEM_IERISC_MAILBOX_BASE + offsetof(mailboxes_t, go_message_index)) % TT_ARCH_MAX_NOC_WRITE_ALIGNMENT == 0);
+static_assert(
+    (MEM_AERISC_MAILBOX_BASE + offsetof(mailboxes_t, go_message_index)) % TT_ARCH_MAX_NOC_WRITE_ALIGNMENT == 0);
 static_assert(MEM_IERISC_FIRMWARE_BASE % TT_ARCH_MAX_NOC_WRITE_ALIGNMENT == 0);
 static_assert(MEM_AERISC_MAILBOX_BASE % TT_ARCH_MAX_NOC_WRITE_ALIGNMENT == 0);
 // Ensure Lite Fabric and Dev mem map values are consistent
