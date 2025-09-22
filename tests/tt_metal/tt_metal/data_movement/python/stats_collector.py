1749284dd11ef24279afaf82e655b7fa1052cf28
Ryan Zhu <ryanzhu@tenstorrent.com>
2025-09-22 23:37:11 +0000

    clean up stats_collector.py

--- tests/tt_metal/tt_metal/data_movement/python/stats_collector.py
+++ tests/tt_metal/tt_metal/data_movement/python/stats_collector.py
@@ -57,7 +57,6 @@ class StatsCollector:
                         run["core"] = core
                     dm_stats[risc]["analysis"]["stats"][core] = core_analysis[analysis_key]["stats"]
                     dm_stats[risc]["analysis"]["series"].extend(core_analysis[analysis_key]["series"])
-                    # breakpoint()
 
         # Gather test attributes
 
@@ -152,7 +151,6 @@ class StatsCollector:
                 grouped_durations[run_host_id].append(duration)
                 grouped_bandwidths[run_host_id].append(bandwidth)
                 grouped_cores[run_host_id].append(entry["core"])
-                # breakpoint()
 
             agg = {}
             for run_host_id, durations in grouped_durations.items():
@@ -260,7 +258,6 @@ class StatsCollector:
         test_case = aggregate_stats[risc][run_host_id]
         for i in range(len(test_case["all_bandwidths"])):
             result[test_case["all_cores"][i]] = test_case["all_bandwidths"][i]
-        # consider adding aggregate_stats[risc][run_host_id]["attributes"] to result if needed to add context to heatmap image
         attrs = test_case["attributes"]
         attrs["Risc"] = 0 if risc == "riscv_0" else 1
         return result, attrs
@@ -275,6 +272,13 @@ if __name__ == "__main__":
 
     file_path = sys.argv[1]
     collector = StatsCollector(file_path, test_id_to_name={}, test_type_attributes={}, verbose=True)
-    result, attrs = collector.gather_bw_per_core(16, 4096, "riscv_0", 117)
-    print(result)
-    print(attrs)
+    dm_stats, aggregate_stats = collector.gather_analysis_stats()
+    for riscv, stats in aggregate_stats.items():
+        print(f"RISC-V Processor: {riscv}")
+        for run_host_id, data in stats.items():
+            print(f"  Run Host ID: {run_host_id}")
+            print(f"    Duration (cycles): {data['duration_cycles']}")
+            print(f"    Bandwidth (bytes/cycle): {data['bandwidth']}")
+            print(f"    Attributes: {data['attributes']}")
+            print(f"    All Durations: {data['all_durations']}")
+            print(f"    All Bandwidths: {data['all_bandwidths']}")
