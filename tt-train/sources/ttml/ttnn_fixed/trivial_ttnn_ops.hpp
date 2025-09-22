95a2bb56909a8bf815788ca09891796ff764a969
Adriel Bustamante <abustamante@tenstorrent.com>
2025-09-22 16:11:32 -0700

    [TT-Train] Update tt-train/nanoGPT example to use TTNN ops for sampling (#29095)
    ### Problem description
    Updates TT-Train NanoGPT example to use TT ops for hardware acceleration
    
    ### What's changed
    Previous implementation of nanoGPT example used CPU-based sampling. Now
    composite sampling using TT ops and TT argmax
    
    ### Checklist
    https://github.com/tenstorrent/tt-metal/actions/runs/17930645420
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [x] New/Existing tests provide coverage for changes

--- tt-train/sources/ttml/ttnn_fixed/trivial_ttnn_ops.hpp
+++ tt-train/sources/ttml/ttnn_fixed/trivial_ttnn_ops.hpp
@@ -21,6 +21,12 @@ tt::tt_metal::Tensor mean_ttnn(const tt::tt_metal::Tensor& t, int dim, bool keep
 tt::tt_metal::Tensor sum_moreh(const tt::tt_metal::Tensor& t, int dim, bool keep_dim);
 tt::tt_metal::Tensor sum_ttnn(const tt::tt_metal::Tensor& t, int dim, bool keep_dim);
 
+tt::tt_metal::Tensor sample(
+    const tt::tt_metal::Tensor& t,
+    float temperature,
+    uint32_t seed,
+    std::optional<tt::tt_metal::Tensor> logits_padding_mask = std::nullopt);
+
 tt::tt_metal::Tensor to_l1_interleaved(const tt::tt_metal::Tensor& t);
 tt::tt_metal::Tensor to_dram_interleaved(const tt::tt_metal::Tensor& t);
 
