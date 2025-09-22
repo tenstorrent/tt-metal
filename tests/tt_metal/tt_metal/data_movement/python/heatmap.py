89f9c3918ff2ba14aa50b9b5dda2cd800d8114b1
Ryan Zhu <ryanzhu@tenstorrent.com>
2025-09-22 22:25:04 +0000

    add architecture components to heatmap

--- tests/tt_metal/tt_metal/data_movement/python/heatmap.py
+++ tests/tt_metal/tt_metal/data_movement/python/heatmap.py
@@ -1,31 +1,34 @@
 import os
-from tests.tt_metal.tt_metal.data_movement.python.stats_collector import StatsCollector
+import argparse
+import yaml
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
-from tests.tt_metal.tt_metal.data_movement.python.plotter import Plotter
-import numpy as np
+from tests.tt_metal.tt_metal.data_movement.python.stats_collector import StatsCollector
 from tests.tt_metal.tt_metal.data_movement.python.constants import *
-from tt_metal.tools.profiler.process_device_log import extract_device_info
+from tracy.process_device_log import extract_device_info
 from tests.tt_metal.tt_metal.data_movement.python.config import DataMovementConfig
 from tests.tt_metal.tt_metal.data_movement.python.test_metadata import TestMetadataLoader
 from loguru import logger
 
-if __name__ == "__main__":
-    import sys  # is sys already imported by argparse?
-    import argparse
 
+def matrix_arch_fill(matrix, coords, val):
+    for coord in coords:
+        if isinstance(coord, str):
+            x, y = map(int, coord.split("-"))
+            matrix[y][x] = val
+        else:
+            raise ValueError(f"Unexpected coordinate type: {type(coord)}")
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Produce heatmap data from profiler log CSV")
     parser.add_argument("log_csv", type=str, help="Path to the log CSV file")
-    # add architecture here or extract from csv?
     parser.add_argument("-r", "--risc", choices=["riscv_0", "riscv_1"], help="RISC-V processor to analyze")
     parser.add_argument("-n", "--num_transactions", type=int, help="Number of transactions")
     parser.add_argument("-s", "--transaction_size", type=int, help="Transaction size in bytes")
     parser.add_argument("-i", "--test_id", type=int, help="Test ID to analyze")
     args = parser.parse_args()
-
-    # if len(sys.argv) < 2:
-    #     print("Usage: python stats_collector.py <path_to_log_csv>")
-    #     sys.exit(1)
 
     file_path = args.log_csv
     collector = StatsCollector(file_path, test_id_to_name={}, test_type_attributes={}, verbose=True)
@@ -38,39 +41,42 @@ if __name__ == "__main__":
         output_dir,
         f"heatmap_{attrs['Test id']}_{attrs['Risc']}_{attrs['Number of transactions']}_{attrs['Transaction size in bytes']}.png",
     )
-    print(output_dir)
-    print(output_file)
     os.makedirs(output_dir, exist_ok=True)
 
-    # extract from architecture later
-    height_cores = 12
-    width_cores = 17
+    # Load architecture YAML
+    arch_yaml_file_name = "blackhole_140_arch.yaml" if arch == "blackhole" else "wormhole_b0_80_arch.yaml"
+    arch_yaml_path = os.path.join("tt_metal", "soc_descriptors", arch_yaml_file_name)
+    with open(arch_yaml_path, "r") as arch_yaml_file:
+        arch_data = yaml.safe_load(arch_yaml_file)
 
+    height_cores = arch_data["grid"]["y_size"]
+    width_cores = arch_data["grid"]["x_size"]
+    HM_ARC = -0.000001
+    HM_PCIE = -0.000002
+    HM_DRAM = -0.000003
+    HM_ETH = -0.000004
+    HM_ROUTER = -0.000005
+
+    # Fill in bandwidth per core
     matrix = np.zeros((height_cores, width_cores))
     for (x, y), bw in result.items():
-        # testing gradient display
-        # if bw < 0.6:
-        #     bw *= 20
-        # elif bw < 1:
-        #     bw *= 10
-        # elif bw < 5:
-        #     bw *= 3
-        # elif bw < 11:
-        #     bw *= 2
         matrix[y][x] = bw
         if bw > NOC_WIDTHS.get(arch, 64):
             logger.warning(f"Warning: Bandwidth {bw}b/c on core ({x}, {y}) exceeds maximum for {arch}")
 
-    # matrix = np.random.rand(height_cores, width_cores) * 6 + 50
-    # matrix[0][0] = 0
-    # matrix[3][0] = 30
+    # Fill in architecture components
+    matrix_arch_fill(matrix, arch_data["arc"], HM_ARC)
+    matrix_arch_fill(matrix, arch_data["pcie"], HM_PCIE)
+    for row in arch_data.get("dram", []):
+        matrix_arch_fill(matrix, row, HM_DRAM)
+    matrix_arch_fill(matrix, arch_data["eth"], HM_ETH)
+    matrix_arch_fill(matrix, arch_data["router_only"], HM_ROUTER)
 
     bw_colors = [(0, "black"), (0.00001, "red"), (1, "green")]
     bw_cmap = LinearSegmentedColormap.from_list("bw_cmap", bw_colors)
     textcolor_threshold = matrix.max() / 2
 
     fig, (ax1, ax2) = plt.subplots(1, 2)
-    # fig.figsize = (DEFAULT_PLOT_WIDTH, DEFAULT_PLOT_HEIGHT)
     fig.set_size_inches(DEFAULT_PLOT_WIDTH * 2.5, DEFAULT_PLOT_HEIGHT * 1.5)
 
     im1 = ax1.imshow(matrix, cmap=bw_cmap, vmin=0, vmax=64)
@@ -80,11 +86,24 @@ if __name__ == "__main__":
 
     for i in range(height_cores):
         for j in range(width_cores):
-            label = "X" if matrix[i, j] == 0 else f"{matrix[i, j]:.2f}"
+            if matrix[i, j] == HM_ARC:
+                label = "ARC"
+            elif matrix[i, j] == HM_PCIE:
+                label = "PCIe"
+            elif matrix[i, j] == HM_DRAM:
+                label = "DRAM"
+            elif matrix[i, j] == HM_ETH:
+                label = "ETH"
+            elif matrix[i, j] == HM_ROUTER:
+                label = "RTR"
+            elif matrix[i, j] == 0:
+                label = "X"
+            else:
+                label = f"{matrix[i, j]:.2f}"
             ax1.text(j, i, label, ha="center", va="center", color="white")
             ax2.text(
                 j, i, label, ha="center", va="center", color="white" if matrix[i, j] < textcolor_threshold else "black"
-            )  # later try change color from black to white based on value
+            )
 
     config = DataMovementConfig()
     metadata_loader = TestMetadataLoader(config)
@@ -101,5 +120,6 @@ if __name__ == "__main__":
     plt.savefig(output_file)
     plt.close()
 
-    print("Printing result:")
+    print("Bandwidth per core:")
     print(result)
+    print(f"Saved heatmap to {output_file}")
