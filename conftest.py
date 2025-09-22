1979f440d9f732367dee7badf2ef17485b3acf3e
Allan Liu <aliu@tenstorrent.com>
2025-09-22 17:26:44 +0000

    Allow mpi to multi thread in CI
    - add python env tests, ccls
    - add relax mode to conftest
    - minor 1d fabric trimming fix

--- conftest.py
+++ conftest.py
@@ -349,21 +349,24 @@ def all_devices(request, device_params):
 # Reset fabric config to DISABLED if not None, and do nothing otherwise
 # Temporarily require previous state to be passed in as even setting it to DISABLED might be unstable
 # This is to ensure that we don't propagate the instability to the rest of CI
-def reset_fabric(fabric_config):
+def reset_fabric(fabric_config, reliability_mode):
     import ttnn
 
     if fabric_config:
-        ttnn.set_fabric_config(ttnn.FabricConfig.DISABLED)
+        ttnn.set_fabric_config(ttnn.FabricConfig.DISABLED, reliability_mode)
 
 
 # Set fabric config to passed in value
 # Do nothing if not set
 # Must be called before creating the mesh device
-def set_fabric(fabric_config, fabric_tensix_config=None):
+def set_fabric(fabric_config, reliability_mode=None, fabric_tensix_config=None):
     import ttnn
 
     # If fabric_config is not None, set it to fabric_config
     if fabric_config:
+        if reliability_mode is None:
+            reliability_mode = ttnn.FabricReliabilityMode.STRICT_INIT
+
         # Apply default logic for fabric_tensix_config,
         # fabric_tensix_config is used for enabling tensix extensions for the fabric router,
         # some sender channels in the fabric router are moved to the fabric tensix extension
@@ -371,9 +374,7 @@ def set_fabric(fabric_config, fabric_tensix_config=None):
         if fabric_tensix_config is None:
             fabric_tensix_config = get_default_fabric_tensix_config()
 
-        ttnn.set_fabric_config(
-            fabric_config, ttnn.FabricReliabilityMode.STRICT_INIT, None, fabric_tensix_config  # num_planes
-        )
+        ttnn.set_fabric_config(fabric_config, reliability_mode, None, fabric_tensix_config)  # num_planes
 
 
 def get_default_fabric_tensix_config():
@@ -429,7 +430,7 @@ def mesh_device(request, silicon_arch_name, device_params):
     fabric_config = updated_device_params.pop("fabric_config", None)
     fabric_tensix_config = updated_device_params.pop("fabric_tensix_config", None)
     reliability_mode = updated_device_params.pop("reliability_mode", None)
-    set_fabric(fabric_config, fabric_tensix_config)
+    set_fabric(fabric_config, reliability_mode, fabric_tensix_config)
     mesh_device = ttnn.open_mesh_device(mesh_shape=mesh_shape, **updated_device_params)
 
     logger.debug(f"multidevice with {mesh_device.get_num_devices()} devices is created")
@@ -439,7 +440,7 @@ def mesh_device(request, silicon_arch_name, device_params):
         ttnn.close_mesh_device(submesh)
 
     ttnn.close_mesh_device(mesh_device)
-    reset_fabric(fabric_config)
+    reset_fabric(fabric_config, reliability_mode)
     del mesh_device
 
 
