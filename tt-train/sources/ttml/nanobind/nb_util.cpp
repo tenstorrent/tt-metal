11151af45c505a0820f6f6302f846bfd0573fb33
Adam Thompson <athompson@tenstorrent.com>
2025-09-23 15:59:39 +0000

    clarify function, type names, add comments, disallow bf16 numpy types

--- tt-train/sources/ttml/nanobind/nb_util.cpp
+++ tt-train/sources/ttml/nanobind/nb_util.cpp
@@ -22,26 +22,30 @@ constexpr auto BFLOAT = "Unsupported type: Bfloat";
 
 nb::ndarray<nb::numpy> make_numpy_tensor(
     const tt::tt_metal::Tensor& t, std::optional<tt::tt_metal::DataType> new_type) {
-    auto const numpy_tensor_from_data = []<typename U>(
-                                            const auto& tensor_data,
-                                            const auto& tensor_spec,
-                                            [[maybe_unused]] const auto& tensor_strides) {
-        const tt::tt_metal::Shape& tensor_shape = tensor_spec.logical_shape();
+    auto const make_numpy_tensor_from_data =
+        []<typename NumpyType>(
+            const auto& tensor_data, const auto& tensor_spec, [[maybe_unused]] const auto& tensor_strides) {
+            const tt::tt_metal::Shape& tensor_shape = tensor_spec.logical_shape();
 
-        const auto tensor_shape_rank = tensor_shape.rank();
-        std::vector<size_t> numpy_shape(tensor_shape_rank);
-        std::copy(tensor_shape.cbegin(), tensor_shape.cend(), numpy_shape.begin());
+            const auto tensor_shape_rank = tensor_shape.rank();
+            std::vector<size_t> numpy_shape(tensor_shape_rank);
+            std::copy(tensor_shape.cbegin(), tensor_shape.cend(), numpy_shape.begin());
 
-        // std::vector<int64_t> numpy_strides;
-        // numpy_strides.assign(tensor_strides.cbegin(), tensor_strides.cend());
+            // std::vector<int64_t> numpy_strides;
+            // numpy_strides.assign(tensor_strides.cbegin(), tensor_strides.cend());
 
-        U* numpy_data = new U[tensor_data.size()];
-        std::copy(tensor_data.begin(), tensor_data.end(), numpy_data);
+            auto* numpy_data = new NumpyType[tensor_data.size()];
+            std::copy(tensor_data.begin(), tensor_data.end(), numpy_data);
 
-        const nb::capsule owner(numpy_data, [](void* p) noexcept { delete[] static_cast<U*>(p); });
-        return nb::ndarray<nb::numpy>(
-            numpy_data, tensor_shape_rank, numpy_shape.data(), owner, nullptr /*numpy_strides.data()*/, nb::dtype<U>());
-    };
+            const nb::capsule owner(numpy_data, [](void* p) noexcept { delete[] static_cast<NumpyType*>(p); });
+            return nb::ndarray<nb::numpy>(
+                numpy_data,
+                tensor_shape_rank,
+                numpy_shape.data(),
+                owner,
+                nullptr /*numpy_strides.data()*/,
+                nb::dtype<NumpyType>());
+        };
 
     auto const convert_to_row_major = [](const tt::tt_metal::Tensor& tensor) {
         tt::tt_metal::Shape output_tensor_end(ttsl::SmallVector<uint32_t>(tensor.logical_shape().rank(), 0));
@@ -53,29 +57,33 @@ nb::ndarray<nb::numpy> make_numpy_tensor(
         return ttnn::untilize_with_unpadding(tensor, output_tensor_end, std::nullopt);
     };
 
-    auto const impl = [&numpy_tensor_from_data,
-                       &convert_to_row_major]<typename T, typename U>(const tt::tt_metal::Tensor& tensor) {
+    auto const impl = [&make_numpy_tensor_from_data, &convert_to_row_major]<typename MetalType, typename NumpyType>(
+                          const tt::tt_metal::Tensor& tensor) {
+        static_assert(!std::is_same_v<bfloat16, NumpyType>, "Numpy does not support bfloat16, use float");
         if (tensor.storage_type() == ttnn::types::StorageType::HOST) {
             if (tensor.layout() != tt::tt_metal::Layout::ROW_MAJOR) {
                 auto const row_major_tensor = convert_to_row_major(tensor);
-                const auto row_major_tensor_data = tt::tt_metal::host_buffer::get_as<T const>(row_major_tensor);
-                return numpy_tensor_from_data.template operator()<U>(
+                const auto row_major_tensor_data = tt::tt_metal::host_buffer::get_as<MetalType const>(row_major_tensor);
+                return make_numpy_tensor_from_data.template operator()<NumpyType>(
                     row_major_tensor_data, row_major_tensor.tensor_spec(), row_major_tensor.strides());
             }
-            const auto tensor_data = tt::tt_metal::host_buffer::get_as<const T>(tensor);
-            return numpy_tensor_from_data.template operator()<U>(tensor_data, tensor.tensor_spec(), tensor.strides());
+            const auto tensor_data = tt::tt_metal::host_buffer::get_as<const MetalType>(tensor);
+            return make_numpy_tensor_from_data.template operator()<NumpyType>(
+                tensor_data, tensor.tensor_spec(), tensor.strides());
         }
         const auto cpu_tensor = tensor.cpu(/*blocking=*/true);
-        const auto cpu_tensor_data = tt::tt_metal::host_buffer::get_as<const T>(cpu_tensor);
+        const auto cpu_tensor_data = tt::tt_metal::host_buffer::get_as<const MetalType>(cpu_tensor);
         const auto cpu_tensor_spec = cpu_tensor.tensor_spec();
         const auto cpu_tensor_strides = cpu_tensor.strides();
 
         if (tt::tt_metal::tensor_impl::logical_matches_physical(cpu_tensor_spec)) {
-            return numpy_tensor_from_data.template operator()<U>(cpu_tensor_data, cpu_tensor_spec, cpu_tensor_strides);
+            return make_numpy_tensor_from_data.template operator()<NumpyType>(
+                cpu_tensor_data, cpu_tensor_spec, cpu_tensor_strides);
         }
 
         const auto decoded_data = tt::tt_metal::tensor_impl::decode_tensor_data(cpu_tensor_data, cpu_tensor_spec);
-        return numpy_tensor_from_data.template operator()<U>(decoded_data, cpu_tensor_spec, cpu_tensor_strides);
+        return make_numpy_tensor_from_data.template operator()<NumpyType>(
+            decoded_data, cpu_tensor_spec, cpu_tensor_strides);
     };
 
     const auto& tensor_spec = t.tensor_spec();
@@ -85,6 +93,7 @@ nb::ndarray<nb::numpy> make_numpy_tensor(
             case tt::tt_metal::DataType::INT32: return impl.template operator()<int32_t, int32_t>(t);
             case tt::tt_metal::DataType::UINT32: return impl.template operator()<uint32_t, uint32_t>(t);
             case tt::tt_metal::DataType::FLOAT32: return impl.template operator()<float, float>(t);
+            // impl<bfloat16, float>(t) is intentional below
             case tt::tt_metal::DataType::BFLOAT16: return impl.template operator()<bfloat16, float>(t);
             case tt::tt_metal::DataType::BFLOAT8_B: TT_THROW(UnsupportedMessages::BFLOAT8_B); break;
             case tt::tt_metal::DataType::BFLOAT4_B: TT_THROW(UnsupportedMessages::BFLOAT4_B); break;
@@ -101,7 +110,8 @@ nb::ndarray<nb::numpy> make_numpy_tensor(
                 case tt::tt_metal::DataType::INT32: return impl.template operator()<int32_t, int32_t>(t);
                 case tt::tt_metal::DataType::UINT32: return impl.template operator()<int32_t, uint32_t>(t);
                 case tt::tt_metal::DataType::FLOAT32: return impl.template operator()<int32_t, float>(t);
-                case tt::tt_metal::DataType::BFLOAT16: return impl.template operator()<int32_t, bfloat16>(t);
+                // impl<int32_t, float>(t) is intentional below
+                case tt::tt_metal::DataType::BFLOAT16: return impl.template operator()<int32_t, float>(t);
                 case tt::tt_metal::DataType::BFLOAT8_B: TT_THROW(UnsupportedMessages::BFLOAT8_B); break;
                 case tt::tt_metal::DataType::BFLOAT4_B: TT_THROW(UnsupportedMessages::BFLOAT4_B); break;
                 case tt::tt_metal::DataType::UINT8: TT_THROW(UnsupportedMessages::UINT8); break;
@@ -113,7 +123,8 @@ nb::ndarray<nb::numpy> make_numpy_tensor(
                 case tt::tt_metal::DataType::INT32: return impl.template operator()<uint32_t, int32_t>(t);
                 case tt::tt_metal::DataType::UINT32: return impl.template operator()<uint32_t, uint32_t>(t);
                 case tt::tt_metal::DataType::FLOAT32: return impl.template operator()<uint32_t, float>(t);
-                case tt::tt_metal::DataType::BFLOAT16: return impl.template operator()<uint32_t, bfloat16>(t);
+                // impl<uint32_t, float>(t) is intentional below
+                case tt::tt_metal::DataType::BFLOAT16: return impl.template operator()<uint32_t, float>(t);
                 case tt::tt_metal::DataType::BFLOAT8_B: TT_THROW(UnsupportedMessages::BFLOAT8_B); break;
                 case tt::tt_metal::DataType::BFLOAT4_B: TT_THROW(UnsupportedMessages::BFLOAT4_B); break;
                 case tt::tt_metal::DataType::UINT8: TT_THROW(UnsupportedMessages::UINT8); break;
@@ -125,7 +136,8 @@ nb::ndarray<nb::numpy> make_numpy_tensor(
                 case tt::tt_metal::DataType::INT32: return impl.template operator()<float, int32_t>(t);
                 case tt::tt_metal::DataType::UINT32: return impl.template operator()<float, uint32_t>(t);
                 case tt::tt_metal::DataType::FLOAT32: return impl.template operator()<float, float>(t);
-                case tt::tt_metal::DataType::BFLOAT16: return impl.template operator()<float, bfloat16>(t);
+                // impl<float, float>(t) is intentional below
+                case tt::tt_metal::DataType::BFLOAT16: return impl.template operator()<float, float>(t);
                 case tt::tt_metal::DataType::BFLOAT8_B: TT_THROW(UnsupportedMessages::BFLOAT8_B); break;
                 case tt::tt_metal::DataType::BFLOAT4_B: TT_THROW(UnsupportedMessages::BFLOAT4_B); break;
                 case tt::tt_metal::DataType::UINT8: TT_THROW(UnsupportedMessages::UINT8); break;
@@ -137,7 +149,8 @@ nb::ndarray<nb::numpy> make_numpy_tensor(
                 case tt::tt_metal::DataType::INT32: return impl.template operator()<bfloat16, int32_t>(t);
                 case tt::tt_metal::DataType::UINT32: return impl.template operator()<bfloat16, uint32_t>(t);
                 case tt::tt_metal::DataType::FLOAT32: return impl.template operator()<bfloat16, float>(t);
-                case tt::tt_metal::DataType::BFLOAT16: return impl.template operator()<bfloat16, bfloat16>(t);
+                // impl<bfloat16, float>(t) is intentional below
+                case tt::tt_metal::DataType::BFLOAT16: return impl.template operator()<bfloat16, float>(t);
                 case tt::tt_metal::DataType::BFLOAT8_B: TT_THROW(UnsupportedMessages::BFLOAT8_B); break;
                 case tt::tt_metal::DataType::BFLOAT4_B: TT_THROW(UnsupportedMessages::BFLOAT4_B); break;
                 case tt::tt_metal::DataType::UINT8: TT_THROW(UnsupportedMessages::UINT8); break;
@@ -161,12 +174,13 @@ tt::tt_metal::Tensor make_metal_tensor(
 
     const auto rank = data.ndim();
 
-    const auto impl = [&data_type, rank, &data, target_layout]<typename T>(tt::tt_metal::DataType tensor_data_type) {
-        static_assert(std::is_same_v<T, std::remove_cvref_t<T>>);
+    const auto impl = [&data_type, rank, &data, target_layout]<typename NumpyType>(
+                          tt::tt_metal::DataType tensor_data_type) {
+        static_assert(std::is_same_v<NumpyType, std::remove_cvref_t<NumpyType>>);
         TT_FATAL(
-            data_type.bits == (sizeof(T) * 8),
+            data_type.bits == (sizeof(NumpyType) * 8),
             "Unsupported precision: expected {} bits, got {} bits",
-            sizeof(T) * 8,
+            sizeof(NumpyType) * 8,
             data_type.bits);
 
         tt::tt_metal::ShapeBase::Container shape_container(rank);
@@ -192,13 +206,13 @@ tt::tt_metal::Tensor make_metal_tensor(
         tt::tt_metal::TensorLayout tensor_layout(tensor_data_type, tensor_page_config, tensor_memory_config);
         tt::tt_metal::TensorSpec tensor_spec(tensor_shape, tensor_layout);
 
-        const auto make_device_tensor = [&]<typename Type>() -> tt::tt_metal::Tensor {
+        const auto make_device_tensor = [&]<typename MetalType>() -> tt::tt_metal::Tensor {
             auto* device = &ttml::autograd::ctx().get_device();
             // device->enable_program_cache();
 
-            std::span<const T> data_span(static_cast<const T*>(data.data()), data.size());
-            if constexpr (sizeof(Type) != sizeof(T)) {
-                std::vector<Type> converted_data;
+            std::span<const NumpyType> data_span(static_cast<const NumpyType*>(data.data()), data.size());
+            if constexpr (sizeof(MetalType) != sizeof(NumpyType)) {
+                std::vector<MetalType> converted_data;
                 converted_data.assign(data_span.begin(), data_span.end());
 
                 auto row_major_tensor = tt::tt_metal::Tensor::from_vector(converted_data, tensor_spec, device);
