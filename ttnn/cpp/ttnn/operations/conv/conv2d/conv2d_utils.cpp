858747781f20f7875e256db72a9820218f61bf08
= <=>
2025-09-23 17:19:38 +0000

    addressing copilots issues with the code

--- ttnn/cpp/ttnn/operations/conv/conv2d/conv2d_utils.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/conv2d_utils.cpp
@@ -546,8 +546,7 @@ std::tuple<ttnn::Shape, ttnn::MemoryConfig> determine_input_memory_config(
     return {input_padded_shape, input_tensor_sharded_memory_config};
 };
 
-namespace {
-std::tuple<ttnn::Shape, ttnn::MemoryConfig, bool> get_conv_padded_input_shape_and_mem_config(
+static std::tuple<ttnn::Shape, ttnn::MemoryConfig, bool> get_conv_padded_input_shape_and_mem_config(
     MeshDevice* device,
     const ttnn::Tensor& input_tensor_,
     const Conv2dConfig& conv_config,
@@ -689,7 +688,7 @@ std::tuple<ttnn::Shape, ttnn::MemoryConfig, bool> get_conv_padded_input_shape_an
     } else {
         return {input_tensor.logical_shape(), input_tensor.memory_config(), needs_shard_or_reshard};
     }
-}  // namespace
+}
 
 ttnn::Shape flatten_4d_shape(const ttnn::Shape& input_shape) {
     TT_FATAL(input_shape.size() == 4, "Expected 4D shape");
