cade982563c20024dbcb88bb9af7fd2f0cf5bb27
Ryan Miller <rmiller@tenstorrent.com>
2025-09-23 20:24:35 +0000

    Make RMS norm a CTA instead of define in reader kernels

--- ttnn/cpp/ttnn/operations/normalization/layernorm/device/kernels/dataflow/reader_mcast_receiver_unary_sharded_ln.cpp
+++ ttnn/cpp/ttnn/operations/normalization/layernorm/device/kernels/dataflow/reader_mcast_receiver_unary_sharded_ln.cpp
@@ -23,6 +23,7 @@ void kernel_main() {
     constexpr uint32_t num_blocks_first_stage = get_compile_time_arg_val(12);
     constexpr uint32_t num_blocks_second_stage = get_compile_time_arg_val(13);
     uint32_t reduce_second_stage_semaphore_addr = get_semaphore(get_compile_time_arg_val(14));
+    constexpr bool rms_norm = get_compile_time_arg_val(15) == 1;
 
     const bool is_last_all_to_all_worker = get_arg_val<uint32_t>(0);
     const uint32_t all_to_all_tile_offset_bytes = get_arg_val<uint32_t>(1);
@@ -213,8 +214,9 @@ void kernel_main() {
             cb_push_back(cb_ex_global, num_tiles);
         }
     };
-#ifndef RMSNORM
-    global_reduce_receiver(cb_ex_partial, cb_ex_external, cb_ex, cb_ex_global, cb_ex);
-#endif
+
+    if constexpr (!rms_norm) {
+        global_reduce_receiver(cb_ex_partial, cb_ex_external, cb_ex, cb_ex_global, cb_ex);
+    }
     global_reduce_receiver(cb_ex_partial2, cb_ex_external2, cb_ex2pe, cb_ex_global, cb_ex2);
 }
