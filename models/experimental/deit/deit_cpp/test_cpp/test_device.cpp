3fe5eb5bb31b4d772d38dacdf64123eccad3341b
velonica0 <like@mail.nankai.edu.cn>
2025-09-23 17:04:10 +0800

    Device error

---
+++ models/experimental/deit/deit_cpp/test_cpp/test_device.cpp
@@ -0,0 +1,14 @@
+#include <ttnn/types.hpp>
+#include <ttnn/device.hpp>
+#include <tt-metalium/host_api.hpp>
+#include <tt-metalium/device.hpp>
+
+int main() {
+    auto device = ttnn::MeshDevice::create_unit_mesh(0, 
+        /*l1_small_size=*/24576,
+        /*trace_region_size=*/6434816,
+        /*num_command_queues=*/2,
+        /*dispatch_core_config=*/tt::tt_metal::DispatchCoreConfig(tt::tt_metal::DispatchCoreType::ETH));
+    std::cout << "Device created" << std::endl;
+    return 0;
+}
