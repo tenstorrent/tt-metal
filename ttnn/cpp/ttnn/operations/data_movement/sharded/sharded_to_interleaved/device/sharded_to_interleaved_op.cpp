96892e789bd1a6d32427a20bf4b946cb88c6db5c
Stanislav Minakov <sminakov@tenstorrent.com>
2025-09-23 23:11:18 +0000

    Remove all usages of logical sharding from the codebase (#29097)
    ### Ticket
    
    ### Problem description
    We're removing support for Logical sharding from the codebase, starting
    from replacing the only 2 usages in convolutions. In those 2 cases
    logical sharding was abused, with the sole purpose of trying to add some
    extra padding to the tensor and preserve it.
    
    ### What's changed
    Remove usages of logical sharding, converted it to physical.
    Explicitly handled the required padding/alignment.
    Added output tensor argument to `to_memory_config`,
    `interleaved_to_sharded`, `sharded_to_interleaved` allowing to manually
    preserve additional padding.
    Fixed a bug in `ttnn::move`, which stripped out padding from a tensor
    while moving it.
    
    ### Checklist
    - [x] [All post commit CI
    passes](https://github.com/tenstorrent/tt-metal/actions/runs/17930672511)
    - [x] [Blackhole Post commit CI with demo tests
    passes](https://github.com/tenstorrent/tt-metal/actions/runs/17930675102)
    - [x] [Model regression CI
    passes](https://github.com/tenstorrent/tt-metal/actions/runs/17930677783)
    - [x] [Single-card demo tests CI
    passes](https://github.com/tenstorrent/tt-metal/actions/runs/17930681120)
    - [x] [Nightly L2 tests pass for conv and
    pool](https://github.com/tenstorrent/tt-metal/actions/runs/17930690681)
    - [x] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/data_movement/sharded/sharded_to_interleaved/device/sharded_to_interleaved_op.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/sharded/sharded_to_interleaved/device/sharded_to_interleaved_op.cpp
@@ -12,13 +12,22 @@ using namespace tt::tt_metal;
 
 namespace ttnn::operations::data_movement {
 
-void ShardedToInterleavedDeviceOperation::validate(const std::vector<Tensor>& input_tensors) const {
+void ShardedToInterleavedDeviceOperation::validate_with_output_tensors(
+    const std::vector<Tensor>& input_tensors, const std::vector<std::optional<Tensor>>& output_tensors) const {
     const auto& input_tensor = input_tensors.at(0);
     auto shard_spec = input_tensor.shard_spec().value();
     TT_FATAL(input_tensor.storage_type() == StorageType::DEVICE, "Operands to shard need to be on device!");
     TT_FATAL(input_tensor.buffer() != nullptr, "Operands to shard need to be allocated in buffers on device!");
     TT_FATAL(input_tensor.memory_config().is_sharded(), "Input tensor must be sharded");
     TT_FATAL(input_tensor.memory_config().buffer_type() == BufferType::L1, "Input tensor must be in L1");
+    if (!output_tensors.empty() && output_tensors[0].has_value()) {
+        const auto& output_tensor = output_tensors[0].value();
+        TT_FATAL(output_tensor.memory_config() == this->output_mem_config, "Mismatched output memory config");
+        TT_FATAL(output_tensor.dtype() == this->output_dtype, "Mismatched output dtype");
+        TT_FATAL(output_tensor.storage_type() == StorageType::DEVICE, "Operands to shard need to be on device!");
+        TT_FATAL(output_tensor.buffer() != nullptr, "Operands to shard need to be allocated in buffers on device!");
+        TT_FATAL(output_tensor.device() == input_tensor.device(), "Operands to shard need to be on the same device!");
+    }
     TT_FATAL(
         this->output_mem_config.memory_layout() == TensorMemoryLayout::INTERLEAVED,
         "Output memory config must be Interleaved");
@@ -35,7 +44,10 @@ void ShardedToInterleavedDeviceOperation::validate(const std::vector<Tensor>& in
 }
 
 std::vector<ttnn::TensorSpec> ShardedToInterleavedDeviceOperation::compute_output_specs(
-    const std::vector<Tensor>& input_tensors) const {
+    const std::vector<Tensor>& input_tensors, const std::vector<std::optional<Tensor>>& output_tensors) const {
+    if (!output_tensors.empty() && output_tensors[0].has_value()) {
+        return {output_tensors[0]->tensor_spec()};
+    }
     const auto& input_tensor = input_tensors.at(0);
     return {TensorSpec(
         input_tensor.logical_shape(),
@@ -45,6 +57,16 @@ std::vector<ttnn::TensorSpec> ShardedToInterleavedDeviceOperation::compute_outpu
             output_mem_config,
             input_tensor.logical_shape(),
             input_tensor.padded_shape()))};
+}
+
+std::vector<Tensor> ShardedToInterleavedDeviceOperation::create_output_tensors(
+    const std::vector<Tensor>& input_tensors, const std::vector<std::optional<Tensor>>& output_tensors) const {
+    if (!output_tensors.empty() && output_tensors[0].has_value()) {
+        return {output_tensors[0].value()};
+    }
+    const auto& input_tensor = input_tensors.at(0);
+    auto spec = compute_output_specs(input_tensors, output_tensors)[0];
+    return {create_device_tensor(spec, input_tensor.device())};
 }
 
 tt::tt_metal::operation::OpPerformanceModelGeneral<std::vector<Tensor>>
