94e121a1873bdc5918c6d23710d3b439a11402ad
Vuk Jovanovic <157483960+tt-vjovanovic@users.noreply.github.com>
2025-09-24 02:04:37 +0200

    Inspector RPC implementation using Cap'N Proto (#27718)
    Inspector will now have ability of RPC. Even though this RPC was
    intended as a way to expose data from host runtime to debugging tools,
    it can be used to trigger something in the host as well. For example,
    during long running workloads (like training), we can write tools that
    can query data and also change the runtime configuration data if needed
    (reduce number of iterations during training).
    
    New runtime options have been added:
    ```bash
       export TT_METAL_INSPECTOR_RPC_SERVER_HOST=127.0.0.1      # optional: set the host of the Inspector RPC server. Default is `127.0.0.1`.
       export TT_METAL_INSPECTOR_RPC_SERVER_PORT=50051          # optional: set the port of the Inspector RPC server. Default is `50051`.
       export TT_METAL_INSPECTOR_RPC=1                          # optional: enable/disable the Inspector RPC server. Default is `1` (enabled).
    ```
    
    Two additional build steps have been added (one required, one optional -
    meaning scripts can be executed without it):
    - First step will generate `RpcServer` which helps simple callback
    redirection with `std::function` and implements automatic data
    serialization of functions that don't have arguments
    - Second step generates python stub file for PyLance and mypy - not
    needed for running the scripts, but useful when editing tt-triage
    scripts that use Inspector data (auto complete, static analysis, etc.)
    New CPM dependency added as well (smallest build dependency of all
    recommended RPC solutions).
    
    Inspector now has its own directory inside `tt_metal/impl/debug`
    directory.
    
    New background thread has been added for Inspector RPC.
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: NSexton <216619413+nsextonTT@users.noreply.github.com>

---
+++ tt_metal/impl/debug/inspector/rpc_server_controller.cpp
@@ -0,0 +1,117 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "rpc_server_controller.hpp"
+#include <kj/async.h>
+#include <tt-logger/tt-logger.hpp>
+#include <chrono>
+#include <thread>
+
+namespace tt::tt_metal::inspector {
+
+RpcServerController::~RpcServerController() {
+    stop();
+}
+
+void RpcServerController::start(const std::string& host, uint16_t port) {
+    if (is_running) {
+        log_warning(tt::LogInspector, "Inspector RPC server already running");
+        return;
+    }
+
+    std::lock_guard<std::mutex> lock(start_stop_mutex);
+
+    if (is_running) {
+        log_warning(tt::LogInspector, "Inspector RPC server already running");
+        return;
+    }
+
+    // Create the RPC implementation and set up callbacks
+    this->host = host;
+    this->port = port;
+
+    should_stop = false;
+    is_running = true;
+    server_thread = std::thread(&RpcServerController::run_server, this);
+
+    // Wait for server to start or fail
+    std::unique_lock start_lock(server_start_mutex);
+    server_start_cv.wait(start_lock, [this] { return this->server_start_finished.load(); });
+    if (!is_running) {
+        throw std::runtime_error(server_start_error_message);
+    }
+}
+
+void RpcServerController::stop() {
+    if (!is_running) {
+        return;
+    }
+
+    std::lock_guard<std::mutex> lock(start_stop_mutex);
+
+    if (!is_running) {
+        return;
+    }
+
+    should_stop = true;
+
+    if (server_thread.joinable()) {
+        server_thread.join();
+    }
+
+    log_info(tt::LogInspector, "Inspector RPC server stopped");
+    is_running = false;
+}
+
+void RpcServerController::run_server() {
+    try {
+        // Create and configure the RPC server
+        ::capnp::EzRpcServer rpc_server(::kj::Own<RpcServer>(&rpc_server_implementation, ::kj::NullDisposer::instance), host, port);
+
+        // Check if server is started correctly
+        auto& waitScope = rpc_server.getWaitScope();
+        rpc_server.getPort().wait(waitScope);
+
+        // Signal back to RpcServerController::start that the server is ready to accept connections
+        {
+            std::lock_guard lock(server_start_mutex);
+            server_start_finished = true;
+        }
+        server_start_cv.notify_one();
+        log_info(tt::LogInspector, "Inspector RPC server listening on {}:{}", host, port);
+
+        // Keep server running until stopped
+        auto last_events = std::chrono::high_resolution_clock::now();
+
+        while (!should_stop) {
+            auto count = waitScope.poll();
+            // If external client is querying, avoid sleeping too much
+            if (count > 0) {
+                last_events = std::chrono::high_resolution_clock::now();
+            }
+            // If no events for a while, sleep a bit to reduce CPU usage
+            else if (std::chrono::high_resolution_clock::now() - last_events > std::chrono::milliseconds(10)) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            }
+        }
+    } catch (const kj::Exception& e) {
+        server_start_error_message = e.getDescription().cStr();
+    } catch (const std::exception& e) {
+        server_start_error_message = e.what();
+    }
+    is_running = false;
+
+    // In case of failure, signal back to RpcServerController::start that the server start failed
+    {
+        std::lock_guard lock(server_start_mutex);
+        server_start_finished = true;
+    }
+    server_start_cv.notify_one();
+}
+
+RpcServer& RpcServerController::get_rpc_server() {
+    return rpc_server_implementation;
+}
+
+} // namespace tt::tt_metal::inspector
