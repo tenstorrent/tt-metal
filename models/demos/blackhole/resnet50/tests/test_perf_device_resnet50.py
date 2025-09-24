35acd32f6cdb2ed79e29418b5b3099d448111bc7
Nilaykumar K Patel <nkpatel@tenstorrent.com>
2025-09-24 05:45:27 +0000

    Add IDs.
    Signed-off-by: Nilaykumar K Patel <nkpatel@tenstorrent.com>

--- models/demos/blackhole/resnet50/tests/test_perf_device_resnet50.py
+++ models/demos/blackhole/resnet50/tests/test_perf_device_resnet50.py
@@ -6,7 +6,7 @@ import pytest
 
 import ttnn
 from models.common.utility_functions import run_for_blackhole
-from models.demos.ttnn_resnet.tests.perf_device_resnet50 import run_perf_device
+from models.demos.ttnn_resnet.tests.common.perf_device_resnet50 import run_perf_device
 from models.demos.ttnn_resnet.tt.ttnn_functional_resnet50_model_utils import is_blackhole_p100
 
 
@@ -24,40 +24,36 @@ DEVICE_TYPE = get_device_type()
 @run_for_blackhole()
 @pytest.mark.models_device_performance_bare_metal
 @pytest.mark.parametrize(
-    "batch_size, test, expected_perf, device_type",
+    "batch_size, test, expected_perf",
     [
         pytest.param(
             16,
-            "act_dtype0-weight_dtype0-math_fidelity0-16-device_params0",
+            "True-act_dtype0-weight_dtype0-math_fidelity0-16-device_params0",
             8371.0,
-            "p100",
             marks=pytest.mark.skipif(DEVICE_TYPE != "p100", reason=f"Skipping P100 test on {DEVICE_TYPE} device"),
         ),
         pytest.param(
             32,
-            "act_dtype0-weight_dtype0-math_fidelity0-32-device_params0",
+            "True-act_dtype0-weight_dtype0-math_fidelity0-32-device_params0",
             9380.0,
-            "p100",
             marks=pytest.mark.skipif(DEVICE_TYPE != "p100", reason=f"Skipping P100 test on {DEVICE_TYPE} device"),
         ),
         pytest.param(
             16,
-            "act_dtype0-weight_dtype0-math_fidelity0-16-device_params0",
+            "True-act_dtype0-weight_dtype0-math_fidelity0-16-device_params0",
             10601.0,
-            "p150",
             marks=pytest.mark.skipif(DEVICE_TYPE != "p150", reason=f"Skipping P150 test on {DEVICE_TYPE} device"),
         ),
         pytest.param(
             32,
-            "act_dtype0-weight_dtype0-math_fidelity0-32-device_params0",
+            "True-act_dtype0-weight_dtype0-math_fidelity0-32-device_params0",
             14779.0,
-            "p150",
             marks=pytest.mark.skipif(DEVICE_TYPE != "p150", reason=f"Skipping P150 test on {DEVICE_TYPE} device"),
         ),
     ],
     ids=["batch16-p100", "batch32-p100", "batch16-p150", "batch32-p150"],
 )
-def test_perf_device(batch_size, test, expected_perf, device_type):
+def test_perf_device(batch_size, test, expected_perf):
     command = (
         f"pytest models/demos/blackhole/resnet50/tests/test_resnet50_performant.py::test_run_resnet50_inference[{test}]"
     )
