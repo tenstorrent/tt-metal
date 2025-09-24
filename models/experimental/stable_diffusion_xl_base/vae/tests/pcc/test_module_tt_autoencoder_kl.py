be1482018e84d69c125fbfdae5aaccdb0abf6243
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2025-09-24 16:20:42 +0200

    SDXL: VAE attn update (#29226)
    ### What's changed
    No need for attention in VAE to use the DRAM GroupNorm; using sharded op
    version now.
    ### Checklist
    - [x] [(Single-card) Demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/17976188670)
    CI passes
    - [x] [(Single-card) Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/17976196919)
    CI passes
    - [x] [(Single-card) Device perf
    regressions](https://github.com/tenstorrent/tt-metal/actions/runs/17976207132)
    CI passes

--- models/experimental/stable_diffusion_xl_base/vae/tests/pcc/test_module_tt_autoencoder_kl.py
+++ models/experimental/stable_diffusion_xl_base/vae/tests/pcc/test_module_tt_autoencoder_kl.py
@@ -19,7 +19,7 @@ from loguru import logger
 @pytest.mark.parametrize(
     "input_shape, pcc, vae_block",
     [
-        ((1, 4, 128, 128), 0.928, "decoder"),
+        ((1, 4, 128, 128), 0.933, "decoder"),
         ((1, 3, 1024, 1024), 0.977, "encoder"),
     ],
     ids=("test_decode", "test_encode"),
