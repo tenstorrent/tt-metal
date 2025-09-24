5b06ce72a12f03fff74326dca3fa8040562cc53d
Strahinja Stanisic <sstanisic@tenstorrent.com>
2025-09-24 12:59:28 +0200

    perf :zap:: improve perf_unpack_transpose (#689)
    ### Ticket
    <!-- Link to Github Issue -->
    
    ### Problem description
    <!-- Provide context for the problem. -->
    
    ### What's changed
    <!-- Describe the approach used to solve the problem.
    Summarize the changes made and its impact. -->
    Fixes compilation bug in test and improves it to better represent the
    peak runtime of the algorithm. This is done by utilizing the dest
    register more efficiently.
    
    ### Type of change
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)

--- tests/sources/unpack_transpose_perf.cpp
+++ tests/sources/unpack_transpose_perf.cpp
@@ -2,6 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+#include <algorithm>
 #include <cstdint>
 
 #include "ckernel.h"
@@ -16,30 +17,35 @@ uint32_t unp_cfg_context          = 0;
 uint32_t pack_sync_tile_dst_ptr   = 0;
 uint32_t math_sync_tile_dst_index = 0;
 
+static_assert(PERF_RUN_TYPE != PerfRunType::MATH_ISOLATE, "Math isolation not supported for unpack_transpose");
+static_assert(PERF_RUN_TYPE != PerfRunType::PACK_ISOLATE, "Pack isolation not supported for unpack_transpose");
+static_assert(PERF_RUN_TYPE != PerfRunType::L1_CONGESTION, "L1 congestion not supported for unpack_transpose");
+
+static constexpr uint32_t MAX_TILES_DEST = is_fp32_dest_acc_en ? 4 : 8;
+
 #ifdef LLK_TRISC_UNPACK
 
 #include "llk_unpack_A.h"
 
 void run_kernel()
 {
-    constexpr uint32_t src_a = 0x1a000;
-
     {
         ZONE_SCOPED("INIT")
+
         _llk_unpack_A_hw_configure_<is_fp32_dest_acc_en, StochRndType::None, false>(
             formats.unpack_src, formats.unpack_dst, FACE_R_DIM, UNPACK_TRANSPOSE_WITHIN_FACE, TILE_NUM_FACES);
         _llk_unpack_A_init_<>(UNPACK_TRANSPOSE_FACES, UNPACK_TRANSPOSE_WITHIN_FACE, FACE_R_DIM, TILE_NUM_FACES, formats.unpack_src, formats.unpack_dst);
-        ckernel::tensix_sync(); // -> perf
+        PROFILER_SYNC();
     }
 
     {
         ZONE_SCOPED("TILE_LOOP")
+
         for (uint32_t tile = 0; tile < TILE_CNT; tile++)
         {
-            _llk_unpack_A_<>(
-                L1_ADDRESS(src_a + (tile % 8) * 0x1000), UNPACK_TRANSPOSE_FACES, formats.unpack_src, formats.unpack_dst); // TODO SS<-LP use PERF_ADDRESS here
+            _llk_unpack_A_<>(PERF_ADDRESS(PERF_INPUT_A, tile), UNPACK_TRANSPOSE_FACES, formats.unpack_src, formats.unpack_dst);
         }
-        ckernel::tensix_sync(); // -> perf
+        PROFILER_SYNC();
     }
 }
 
@@ -64,25 +70,30 @@ void run_kernel()
         _llk_math_eltwise_unary_datacopy_init_<DataCopyType::A2D, is_fp32_dest_acc_en, BroadcastType::NONE, false>(
             UNPACK_TRANSPOSE_FACES, UNPACK_TRANSPOSE_WITHIN_FACE, TILE_NUM_FACES, formats.math);
 #endif
-        ckernel::tensix_sync(); // -> perf
+        PROFILER_SYNC();
     }
 
     {
         ZONE_SCOPED("TILE_LOOP")
         if constexpr (PERF_RUN_TYPE == PerfRunType::UNPACK_ISOLATE)
         {
-            // _llk_unpack_A sets both A and B valid
+            // _llk_unpack_A     sets both A and B valid
             return _perf_math_loop_clear_valid<true, true>(TILE_CNT * TILE_NUM_FACES);
         }
 
-        for (int i = 0; i < TILE_CNT; i++)
+        for (uint32_t block_start = 0; block_start < TILE_CNT; block_start += MAX_TILES_DEST)
         {
+            uint32_t block_tiles = std::min(TILE_CNT - block_start, MAX_TILES_DEST);
+
             _llk_math_wait_for_dest_available_<DstSync::SyncHalf>();
-            _llk_math_eltwise_unary_datacopy_<DataCopyType::A2D, DstSync::SyncHalf, is_fp32_dest_acc_en, BroadcastType::NONE, unpack_to_dest>(
-                0, formats.math, formats.math);
+            for (uint32_t block_tile = 0; block_tile < block_tiles; block_tile++)
+            {
+                _llk_math_eltwise_unary_datacopy_<DataCopyType::A2D, DstSync::SyncHalf, is_fp32_dest_acc_en, BroadcastType::NONE, unpack_to_dest>(
+                    block_tile, formats.math, formats.math);
+            }
             _llk_math_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
         }
-        ckernel::tensix_sync(); // -> perf
+        PROFILER_SYNC();
     }
 }
 
@@ -95,13 +106,13 @@ void run_kernel()
 
 void run_kernel()
 {
-    constexpr uint32_t dst = 0x1E000;
     {
         ZONE_SCOPED("INIT")
+
         _llk_pack_hw_configure_<is_fp32_dest_acc_en>(formats.pack_src, formats.pack_dst, TILE_WIDTH * TILE_HEIGHT);
         _llk_pack_init_<>(formats.pack_dst);
         _llk_pack_dest_init_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
-        ckernel::tensix_sync(); // -> perf
+        PROFILER_SYNC();
     }
     {
         ZONE_SCOPED("TILE_LOOP")
@@ -110,13 +121,18 @@ void run_kernel()
             return;
         }
 
-        for (uint32_t tile = 0; tile < TILE_CNT; tile++)
+        for (uint32_t block_start = 0; block_start < TILE_CNT; block_start += MAX_TILES_DEST)
         {
+            uint32_t block_tiles = std::min(TILE_CNT - block_start, MAX_TILES_DEST);
+
             _llk_packer_wait_for_math_done_();
-            _llk_pack_<DstSync::SyncHalf, is_fp32_dest_acc_en>(0, L1_ADDRESS(dst + (tile % 8) * 0x1000)); // TODO SS<-LP use PERF_ADDRESS here
+            for (uint32_t block_tile = 0; block_tile < block_tiles; block_tile++)
+            {
+                _llk_pack_<DstSync::SyncHalf, is_fp32_dest_acc_en>(block_tile, PERF_ADDRESS(PERF_OUTPUT, block_start + block_tile));
+            }
             _llk_pack_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
         }
-        ckernel::tensix_sync(); // -> perf
+        PROFILER_SYNC();
     }
 }
 
