75d9dde395c2d2a0399c5d7a8f1d766f43692279
riverwuTT <riverwu@tenstorrent.com>
2025-09-24 23:00:10 +0000

    join jit_build_utils.hpp/cpp

--- tt_metal/jit_build/jit_build_utils.hpp
+++ tt_metal/jit_build/jit_build_utils.hpp
@@ -4,15 +4,48 @@
 
 #pragma once
 
+#include <stdlib.h>
+#include <filesystem>
+#include <iostream>
+#include <mutex>
 #include <string>
+#include <fstream>
 
-namespace tt {
-namespace jit_build {
-namespace utils {
+namespace tt::jit_build::utils {
 
-bool run_command(const std::string& cmd, const std::string& log_file, bool verbose);
-void create_file(const std::string& file_path_str);
+inline bool run_command(const std::string& cmd, const std::string& log_file, const bool verbose) {
+    // ZoneScoped;
+    // ZoneText( cmd.c_str(), cmd.length());
+    int ret;
+    static std::mutex io_mutex;
+    if (getenv("TT_METAL_BACKEND_DUMP_RUN_CMD") or verbose) {
+        {
+            std::lock_guard<std::mutex> lk(io_mutex);
+            std::cout << "===== RUNNING SYSTEM COMMAND:" << std::endl;
+            std::cout << cmd << std::endl << std::endl;
+        }
+        ret = system(cmd.c_str());
+        // {
+        //     std::lock_guard<std::mutex> lk(io_mutex);
+        //     cout << "===== DONE SYSTEM COMMAND: " << cmd << std::endl;
+        // }
 
-}  // namespace utils
-}  // namespace jit_build
-}  // namespace tt
+    } else {
+        std::string redirected_cmd = cmd + " >> " + log_file + " 2>&1";
+        ret = system(redirected_cmd.c_str());
+    }
+
+    return (ret == 0);
+}
+
+inline void create_file(const std::string& file_path_str) {
+    namespace fs = std::filesystem;
+
+    fs::path file_path(file_path_str);
+    fs::create_directories(file_path.parent_path());
+
+    std::ofstream ofs(file_path);
+    ofs.close();
+}
+
+}  // namespace tt::jit_build::utils
