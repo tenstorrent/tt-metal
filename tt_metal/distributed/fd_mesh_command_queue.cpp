ebcb7b2af03ad3ba8c0fb24e50b42dcd75d824d2
Mihir Pise <mpise@tenstorrent.com>
2025-09-24 23:31:39 +0000

    Pinned buffer read/write changes for interleaved buffers

--- tt_metal/distributed/fd_mesh_command_queue.cpp
+++ tt_metal/distributed/fd_mesh_command_queue.cpp
@@ -518,7 +518,8 @@ void FDMeshCommandQueue::write_shard_to_device(
     const MeshCoordinate& device_coord,
     const void* src,
     const std::optional<BufferRegion>& region,
-    tt::stl::Span<const SubDeviceId> sub_device_ids) {
+    tt::stl::Span<const SubDeviceId> sub_device_ids,
+    std::shared_ptr<PinnedMemory> pinned_memory) {
     ZoneScoped;
     if (!mesh_device_->is_local(device_coord)) {
         return;
@@ -536,7 +537,13 @@ void FDMeshCommandQueue::write_shard_to_device(
 
     sub_device_ids = buffer_dispatch::select_sub_device_ids(mesh_device_, sub_device_ids);
     buffer_dispatch::write_to_device_buffer(
-        src, *shard_view, id_, expected_num_workers_completed_, this->dispatch_core_type(), sub_device_ids);
+        src,
+        pinned_memory,
+        *shard_view,
+        id_,
+        expected_num_workers_completed_,
+        this->dispatch_core_type(),
+        sub_device_ids);
 }
 
 void FDMeshCommandQueue::read_shard_from_device(
