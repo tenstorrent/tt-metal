3e3e2a50e08d8d27a35e53fb66adff0b4478b310
Dimitri Gnidash <dimitri@tenstorrent.com>
2025-09-24 09:49:35 -0400

    Smaller L1

--- models/demos/yolov11m/common.py
+++ models/demos/yolov11m/common.py
@@ -9,7 +9,7 @@ from ultralytics import YOLO
 
 from models.demos.yolov11m.reference import yolov11
 
-YOLOV11_L1_SMALL_SIZE = 9000
+YOLOV11_L1_SMALL_SIZE = 8000
 
 
 def load_torch_model(model_location_generator=None):
