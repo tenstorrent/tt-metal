f20c2424b23545352ff2822995d5704a441e5553
riverwuTT <riverwu@tenstorrent.com>
2025-09-25 06:12:30 +0000

    removes assert.hpp from metal API

--- tt_metal/api/tt-metalium/assert.hpp
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
-//
-// SPDX-License-Identifier: Apache-2.0
-
-#pragma once
-
-#warning "This file is deprecated. Please use <tt_stl/assert.hpp> instead."
-#include <tt_stl/assert.hpp>
