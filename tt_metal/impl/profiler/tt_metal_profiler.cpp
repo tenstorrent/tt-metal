3c9d97577b58e614794f9c62b9d71dd169cbe468
Samarth Agarwal <sagarwal@tenstorrent.com>
2025-09-25 01:38:35 +0000

    Saving

--- tt_metal/impl/profiler/tt_metal_profiler.cpp
+++ tt_metal/impl/profiler/tt_metal_profiler.cpp
@@ -958,6 +958,8 @@ void ReadMeshDeviceProfilerResults(
 #if defined(TRACY_ENABLE)
     ZoneScoped;
 
+    log_info(tt::LogMetal, "ReadMeshDeviceProfilerResults {}", state);
+
     if (getDeviceProfilerState()) {
         TT_ASSERT(mesh_device.is_initialized());
 
