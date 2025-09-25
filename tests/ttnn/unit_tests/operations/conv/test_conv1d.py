14e91053706aac61cf26919766ed9c98f544724c
Sankar Manoj <smanoj@tenstorrent.com>
2025-09-25 08:56:53 +0000

    #0: Updated auto slice

--- tests/ttnn/unit_tests/operations/conv/test_conv1d.py
+++ tests/ttnn/unit_tests/operations/conv/test_conv1d.py
@@ -410,7 +410,6 @@ def test_with_prepare_weights(
             groups=groups,
             device=device,
             input_dtype=ttnn.bfloat16,
-            slice_config=ttnn.Conv2dL1FullSliceConfig,
         )
 
     conv_config = ttnn.Conv1dConfig(
