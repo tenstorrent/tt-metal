2e9eb8e2e16bb22940703f07dc4a145faa6965c6
riverwuTT <riverwu@tenstorrent.com>
2025-09-26 04:51:50 +0000

    fix CI build fail

--- tt_metal/api/tt-metalium/hlk_desc.hpp
+++ tt_metal/api/tt-metalium/hlk_desc.hpp
@@ -14,7 +14,7 @@
 #include <tt-metalium/base_types.hpp>
 #include <tt-metalium/tt_backend_api_types.hpp>
 #include <tt-metalium/circular_buffer_constants.h>
-#include <tt_stl/tt_stl/reflection.hpp>
+#include <tt_stl/reflection.hpp>
 
 namespace tt {
 /**
