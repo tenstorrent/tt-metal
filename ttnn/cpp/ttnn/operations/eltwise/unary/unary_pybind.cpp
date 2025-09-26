21b2e2ceff2a132e27728efde08ff20f122eb490
Anasuya G Nair <mcw-anair@ext.tenstorrent.com>
2025-09-26 18:18:09 +0530

    Default approx flag in log-related ops to false (#27345)
    ### Ticket
    #27655
    
    ### Problem description
    As per
    https://github.com/tenstorrent/tt-metal/pull/26675#issuecomment-3213907002,
    set fast and approx flag for log as false by default so that negative
    inputs return NaN by default (for bf16 it returns inf).
    
    ### What's changed
    - Changed fast_approx flag to `false` by default.
    - Removed `ExecuteUnaryWithFastAndApproximateModeTrue` template since
    its not being used by any other ops
    - Passed fast_approx flag as `true` for failing tests.
    
    ### Checklist
    - [x] All post commit -
    https://github.com/tenstorrent/tt-metal/actions/runs/17541190299 -
    PASSED
    Latest run:
    https://github.com/tenstorrent/tt-metal/actions/runs/17787864093 -
    PASSED
    - [x] Blackhole Post commit -
    https://github.com/tenstorrent/tt-metal/actions/runs/17541194231 -
    PASSED
    Latest run:
    https://github.com/tenstorrent/tt-metal/actions/runs/17643388671 - same
    as main
    - [x] (Blackhole) Demo tests -
    https://github.com/tenstorrent/tt-metal/actions/runs/17788045759 -
    PASSED
    - [x] (Blackhole) Blackhole nightly tests -
    https://github.com/tenstorrent/tt-metal/actions/runs/17787876048 - same
    as main
    - [x] (Single-card) Demo tests -
    https://github.com/tenstorrent/tt-metal/actions/runs/17201875696 - same
    as main
    Latest run:
    https://github.com/tenstorrent/tt-metal/actions/runs/17603637367/job/50030806191
    - same as main
    - [x] (Single-card) Device perf regressions -
    https://github.com/tenstorrent/tt-metal/actions/runs/17201898990 -
    PASSED
    Latest run:
    https://github.com/tenstorrent/tt-metal/actions/runs/17679355191 -
    PASSED
    - [x] (Single-card) Frequent model and ttnn tests -
    https://github.com/tenstorrent/tt-metal/actions/runs/17201923339 -
    PASSED
    - [x] (Single-card) Model perf tests -
    https://github.com/tenstorrent/tt-metal/actions/runs/17201944949 -
    PASSED
    Latest run:
    https://github.com/tenstorrent/tt-metal/actions/runs/17603486998 - same
    as main
    - [x] (Single-card) Fast dispatch frequent tests -
    https://github.com/tenstorrent/tt-metal/actions/runs/17202042933 -
    PASSED
    - [x] T3K -
    https://github.com/tenstorrent/tt-metal/actions/runs/17541155683 - same
    as main but for model perf tests
    - [x] (T3K) T3000 model perf tests -
    https://github.com/tenstorrent/tt-metal/actions/runs/17585663474 - same
    as main
    - [x] (Galaxy) Quick -
    https://github.com/tenstorrent/tt-metal/actions/runs/17788137362 - same
    as main
    - [x] TG -
    https://github.com/tenstorrent/tt-metal/actions/runs/17202399004 - same
    as main
    - [x] (TG) Demo tests -
    https://github.com/tenstorrent/tt-metal/actions/runs/17211015786 - same
    as main

--- ttnn/cpp/ttnn/operations/eltwise/unary/unary_pybind.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/unary/unary_pybind.cpp
@@ -427,7 +427,6 @@ void bind_unary_operation_with_fast_and_approximate_mode(
     const unary_operation_t& operation,
     const std::string& range = "",
     const std::string& supported_dtype = "BFLOAT16",
-    bool fast_approx_mode = false,
     const std::string& info_doc = "") {
     auto doc = fmt::format(
         R"doc(
@@ -471,7 +470,6 @@ void bind_unary_operation_with_fast_and_approximate_mode(
         operation.python_fully_qualified_name(),
         range,
         supported_dtype,
-        fast_approx_mode,
         info_doc);
 
     bind_registered_operation(
@@ -488,7 +486,7 @@ void bind_unary_operation_with_fast_and_approximate_mode(
             },
             py::arg("input_tensor"),
             py::kw_only(),
-            py::arg("fast_and_approximate_mode") = fast_approx_mode,
+            py::arg("fast_and_approximate_mode") = false,
             py::arg("memory_config") = std::nullopt,
             py::arg("output_tensor") = std::nullopt});
 }
@@ -2111,14 +2109,13 @@ void py_module(py::module& module) {
     bind_unary_operation_with_fast_and_approximate_mode(module, ttnn::erf, R"doc(BFLOAT16, BFLOAT8_B)doc");
     bind_unary_operation_with_fast_and_approximate_mode(module, ttnn::erfc, R"doc(BFLOAT16, BFLOAT8_B)doc");
     bind_unary_operation_with_fast_and_approximate_mode(module, ttnn::gelu, R"doc(BFLOAT16, BFLOAT8_B)doc");
+    bind_unary_operation_with_fast_and_approximate_mode(module, ttnn::log, "", R"doc(BFLOAT16, BFLOAT8_B, FLOAT32)doc");
     bind_unary_operation_with_fast_and_approximate_mode(
-        module, ttnn::log, "", R"doc(BFLOAT16, BFLOAT8_B, FLOAT32)doc", true);
+        module, ttnn::log10, "", R"doc(BFLOAT16, BFLOAT8_B, FLOAT32)doc");
     bind_unary_operation_with_fast_and_approximate_mode(
-        module, ttnn::log10, "", R"doc(BFLOAT16, BFLOAT8_B, FLOAT32)doc", true);
+        module, ttnn::log2, "", R"doc(BFLOAT16, BFLOAT8_B, FLOAT32)doc");
     bind_unary_operation_with_fast_and_approximate_mode(
-        module, ttnn::log2, "", R"doc(BFLOAT16, BFLOAT8_B, FLOAT32)doc", true);
-    bind_unary_operation_with_fast_and_approximate_mode(
-        module, ttnn::log1p, R"doc([Supported range: [-1, 1e7]])doc", R"doc(BFLOAT16, BFLOAT8_B, FLOAT32)doc", true);
+        module, ttnn::log1p, R"doc([Supported range: [-1, 1e7]])doc", R"doc(BFLOAT16, BFLOAT8_B, FLOAT32)doc");
 
     // Unaries with float parameter
     bind_unary_composite_with_default_float(
