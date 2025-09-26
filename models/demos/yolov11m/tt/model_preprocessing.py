7dc8b8f902f53dfd424a3587903363c584e20d80
Dimitri Gnidash <dimitri@tenstorrent.com>
2025-09-26 11:52:20 -0400

    Change the datatype of float32

--- models/demos/yolov11m/tt/model_preprocessing.py
+++ models/demos/yolov11m/tt/model_preprocessing.py
@@ -50,7 +50,7 @@ def create_yolov11_input_tensors(
         
         ttnn_input_tensor = ttnn.from_torch(
             torch_input_tensor,
-            dtype=ttnn.bfloat16,
+            dtype=ttnn.float32,
             layout=ttnn.ROW_MAJOR_LAYOUT,
             device=device,
             memory_config=input_mem_config,
