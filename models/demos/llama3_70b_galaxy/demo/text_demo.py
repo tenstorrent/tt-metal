fd40613cf64853b8c83648fa7d33027f7f157837
mtairum <mtairum@tenstorrent.com>
2025-09-26 16:49:09 +0000

    More round of debugging. This adds device delays to CCLs and changes the semaphore logic used

--- models/demos/llama3_70b_galaxy/demo/text_demo.py
+++ models/demos/llama3_70b_galaxy/demo/text_demo.py
@@ -249,7 +249,7 @@ def create_tt_model(
             False,  # apc_test
             False,  # pcc_check
             False,  # prefill-only profile
-            80,  # num layers
+            1,  # num layers
             False,  # print_outputs
             False,  # is_cur_pos_sharded
             False,  # is_page_table_sharded
