b15f5c24a1774284a0b496ca48885227c4197b60
Keerthana <152590865+kkeerthana0573@users.noreply.github.com>
2025-09-28 17:05:45 +0530

    #28396: Swin V2 Optimization (#29000)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/28396)
    
    ### Problem description
    Improve SwinV2s model FPS 
    
    ### What's changed
    Refactored SwinV2s model. 
    Improved FPS (7-> 8) and PCC (0.98 -> 0.99). 
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/17906021849)
    - Passed.
    - [x] Model regression -
    [N150](https://github.com/tenstorrent/tt-metal/actions/runs/17906046783),
    [N300](https://github.com/tenstorrent/tt-metal/actions/runs/17906056176)
    - Passed.
    - [x] Device performance regression -
    [N150](https://github.com/tenstorrent/tt-metal/actions/runs/17906076332),
    [N300](https://github.com/tenstorrent/tt-metal/actions/runs/17906086277)
    - Passed.
    - [x] [Model Pcc
    checks](https://github.com/tenstorrent/tt-metal/actions/runs/17906031319)
    - Passed.
    - [x] [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/17906596966)
    - Passed.

--- models/experimental/swin_v2/tt/tt_patchmerging_v2.py
+++ models/experimental/swin_v2/tt/tt_patchmerging_v2.py
@@ -7,7 +7,7 @@ import ttnn
 program_configs = {
     "linear_config_1": ttnn.MatmulMultiCoreReuseMultiCast1DProgramConfig(
         compute_with_storage_grid_size=(8, 8),
-        in0_block_w=1,
+        in0_block_w=2,
         out_subblock_h=1,
         out_subblock_w=3,
         per_core_M=2,
@@ -40,22 +40,20 @@ class TtPatchMergingV2:
         x1 = x[..., 1::2, 0::2, :]  # ... H/2 W/2 C
         x2 = x[..., 0::2, 1::2, :]  # ... H/2 W/2 C
         x3 = x[..., 1::2, 1::2, :]  # ... H/2 W/2 C
-        x0 = ttnn.to_layout(x0, layout=ttnn.TILE_LAYOUT, memory_config=ttnn.L1_MEMORY_CONFIG)
-        x1 = ttnn.to_layout(x1, layout=ttnn.TILE_LAYOUT, memory_config=ttnn.L1_MEMORY_CONFIG)
-        x2 = ttnn.to_layout(x2, layout=ttnn.TILE_LAYOUT, memory_config=ttnn.L1_MEMORY_CONFIG)
-        x3 = ttnn.to_layout(x3, layout=ttnn.TILE_LAYOUT, memory_config=ttnn.L1_MEMORY_CONFIG)
+
         x = ttnn.concat([x0, x1, x2, x3], -1, memory_config=ttnn.L1_MEMORY_CONFIG)
 
         ttnn.deallocate(x0)
         ttnn.deallocate(x1)
         ttnn.deallocate(x2)
         ttnn.deallocate(x3)
+
         if x.shape[-1] == 384:
             x = ttnn.to_memory_config(
                 x,
                 memory_config=ttnn.create_sharded_memory_config(
                     x.shape,
-                    core_grid=ttnn.CoreGrid(y=8, x=8),
+                    core_grid=self.device.core_grid,
                     strategy=ttnn.ShardStrategy.HEIGHT,
                     orientation=ttnn.ShardOrientation.ROW_MAJOR,
                 ),
@@ -77,7 +75,7 @@ class TtPatchMergingV2:
                 x,
                 memory_config=ttnn.create_sharded_memory_config(
                     x.shape,
-                    core_grid=ttnn.CoreGrid(y=8, x=8),
+                    core_grid=self.device.core_grid,
                     strategy=ttnn.ShardStrategy.BLOCK,
                     orientation=ttnn.ShardOrientation.ROW_MAJOR,
                 ),
@@ -101,7 +99,7 @@ class TtPatchMergingV2:
                 compute_kernel_config=ttnn.WormholeComputeKernelConfig(
                     math_fidelity=ttnn.MathFidelity.LoFi,
                 ),
-                core_grid=ttnn.CoreGrid(y=8, x=8),
+                core_grid=self.device.core_grid,
                 memory_config=ttnn.L1_MEMORY_CONFIG,
             )
 
