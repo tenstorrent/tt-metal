a8e47ee8112dfebbd5f1e0df958d908a90fe5832
Daiki Aminaka <daminaka@tenstorrent.com>
2025-09-28 14:46:15 -0700

    [Fabric] use pre-calculated 1D fabric_set_unicast_route (#28753)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/28457
    https://github.com/tenstorrent/tt-metal/issues/29449
    
    ### Problem description
    Pre calculated routing data is ready on L1.
    1D API supports both num_hops and dst_chip_id based query.
    
    ### What's changed
    Replace conventional `to_chip_unicast` to `fabric_set_unicast_route`
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-quick.yaml)
    CI passes (if applicable)
    - [ ] [Galaxy demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [x] New/Existing tests provide coverage for changes

--- tests/tt_metal/tt_fabric/fabric_data_movement/kernels/fabric_mux_sender_client.cpp
+++ tests/tt_metal/tt_fabric/fabric_data_movement/kernels/fabric_mux_sender_client.cpp
@@ -86,7 +86,7 @@ void kernel_main() {
     if constexpr (is_2d_fabric) {
         fabric_set_unicast_route((LowLatencyMeshPacketHeader*)packet_header, dst_device_id, dst_mesh_id);
     } else {
-        packet_header->to_chip_unicast(static_cast<uint8_t>(num_hops));
+        fabric_set_unicast_route<false>((LowLatencyPacketHeader*)packet_header, num_hops);
     }
 
     uint64_t local_credit_handshake_noc_address = get_noc_addr(0) + credit_handshake_address;
