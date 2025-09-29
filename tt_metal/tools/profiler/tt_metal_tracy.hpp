3420aa8152579ef30b8fa4d3e284a88e999850a9
Samarth Agarwal <sagarwal@tenstorrent.com>
2025-09-29 20:04:48 +0000

    Cleanup

--- tt_metal/tools/profiler/tt_metal_tracy.hpp
+++ tt_metal/tools/profiler/tt_metal_tracy.hpp
@@ -8,24 +8,6 @@
 
 #if defined(TRACY_ENABLE)
 
-#define TracyTTMetalBeginTrace(device_id, trace_id)                                                 \
-    std::string trace_message = fmt::format("`TT_METAL_TRACE_BEGIN: {}, {}`", device_id, trace_id); \
-    TracyMessage(trace_message.c_str(), trace_message.size());
-
-#define TracyTTMetalEndTrace(device_id, trace_id)                                                 \
-    std::string trace_message = fmt::format("`TT_METAL_TRACE_END: {}, {}`", device_id, trace_id); \
-    TracyMessage(trace_message.c_str(), trace_message.size());
-
-#define TracyTTMetalReplayTrace(device_id, trace_id)                                                  \
-    std::string trace_message = fmt::format("`TT_METAL_TRACE_REPLAY: {}, {}`", device_id, trace_id);  \
-    tt::tt_metal::MetalContext::instance().profiler_state_manager()->add_trace_id_to_device_profiler( \
-        device_id, trace_id);                                                                         \
-    TracyMessage(trace_message.c_str(), trace_message.size());
-
-#define TracyTTMetalReleaseTrace(device_id, trace_id)                                                 \
-    std::string trace_message = fmt::format("`TT_METAL_TRACE_RELEASE: {}, {}`", device_id, trace_id); \
-    TracyMessage(trace_message.c_str(), trace_message.size());
-
 #define TracyTTMetalBeginMeshTrace(device_ids, trace_id)                                                \
     for (auto device_id : device_ids) {                                                                 \
         std::string trace_message = fmt::format("`TT_METAL_TRACE_BEGIN: {}, {}`", device_id, trace_id); \
@@ -53,11 +35,6 @@
     }
 
 #else
-
-#define TracyTTMetalBeginTrace(device_id, trace_id)
-#define TracyTTMetalEndTrace(device_id, trace_id)
-#define TracyTTMetalReplayTrace(device_id, trace_id)
-#define TracyTTMetalReleaseTrace(device_id, trace_id)
 
 #define TracyTTMetalBeginMeshTrace(device_ids, trace_id)
 #define TracyTTMetalEndMeshTrace(device_ids, trace_id)
