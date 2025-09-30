3ed298490b53ba96fd2302f4a98a45c804e1ae6e
Mihir Pise <mpise@tenstorrent.com>
2025-09-30 10:05:51 -0700

    Added new stateful NOC APIs that enable 64-bit TARG/RET addresses. Up… (#28953)
    …date CQ prefetcher process_relay_linear and dispatcher
    process_write_linear commands to use the new APIs in anticipation of
    forthcoming usage in PCIE access to pinned Host memory regions that
    would require 64-bit addressing
    
    ### Ticket
    Link to Github Issue
    
    ### Problem description
    Blackhole opens up the possibility for device addressing 64-bit address
    space on host, via [NOC_TARG/RET_ADDR_LO/MID
    registers](https://github.com/tenstorrent/tt-isa-documentation/blob/main/BlackholeA0/NoC/MemoryMap.md#niu-request-initiators).
    A forthcoming PR will enable the device to address pinned user buffers
    directly via Host IOMMU, foregoing intermediate copy (hugepage), for
    reads/writes to buffers. This PR is an intermediate commit to that goal.
    Wormhole also needs > 32-bit addressing to directly access pinned host
    buffers via the PCIE tile.
    
    ### What's changed
    Updated and added new NOC APIs
    Updated the dispatch commands that we anticipate using for direct access
    to pinned user buffer on Host to enable their 64-bit addressing.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-quick.yaml)
    CI passes (if applicable)
    - [ ] [Galaxy demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- tests/tt_metal/tt_metal/dispatch/dispatch_util/test_device_command.cpp
+++ tests/tt_metal/tt_metal/dispatch/dispatch_util/test_device_command.cpp
@@ -150,9 +150,11 @@ TEST(DeviceCommandTest, AddDispatchWritePaged) {
     {
         DeviceCommandCalculator calculator;
         calculator.add_dispatch_write_paged<false>(1, 5);
-
+        // Do PCIE alignment for out-of-line data
+        calculator.add_alignment();
         HostMemDeviceCommand command(calculator.write_offset_bytes());
         command.add_dispatch_write_paged<false>(false, 0, 0, 0, 1, 5);
+        command.align_write_offset();
         EXPECT_EQ(command.size_bytes(), command.write_offset_bytes());
     }
     {
