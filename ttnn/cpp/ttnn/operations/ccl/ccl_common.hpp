2a8049b7b4bcb57274fa60c9aa369beb761140fb
Saad Jameel <163029024+sjameelTT@users.noreply.github.com>
2025-09-30 12:15:23 -0400

    Add multi-host support to all gather, all broadcast, reduce scatter, point_to_point  (#29371)
    ### Ticket
    #28636
    #28637
    #28638
    #26630
    #28926
    
    Co-authored by: @nardoTT 
    
    ### Problem description
    Can't use physical device IDs and device pointers for multi-host
    compatibility. DeepSeek requires multihost CCLs for Big Mesh.
    
    ### What's changed
    
    - Remove usages of physical device IDs and physical device based
    pointers for critical CCLs like reduce scatter, all gather and all
    broadcast.
    - Add reduce scatter and all gather multihost tests for 2x4 and 8x8 Big
    Mesh
    - Add all gather tests for cluster_axis=1 which were missing before
    - Make some fixes to Tensor Topology that were added during an initial
    draft of this change, but later no longer needed
    
    
    ### Checklist
    - [ ] APC
    https://github.com/tenstorrent/tt-metal/actions/runs/18022909240
    - [ ] Blackhole
    https://github.com/tenstorrent/tt-metal/actions/runs/18022962275
    - [ ] Galaxy
    https://github.com/tenstorrent/tt-metal/actions/runs/18109673954
    - [ ] New/Existing tests provide coverage for changes
    - [ ] T3K:
    https://github.com/tenstorrent/tt-metal/actions/runs/18111588871/
    - [ ] Multihost:
    https://github.com/tenstorrent/tt-metal/actions/runs/18043019998
    
    ---------
    
    Co-authored-by: Nour Ardo <nardo@tenstorrent.com>
    Co-authored-by: Brian Liu <bliu@tenstorrent.com>
    Co-authored-by: asaigal <asaigal@tenstorrent.com>
    Co-authored-by: Allan Liu <aliu@tenstorrent.com>

--- ttnn/cpp/ttnn/operations/ccl/ccl_common.hpp
+++ ttnn/cpp/ttnn/operations/ccl/ccl_common.hpp
@@ -20,6 +20,21 @@
 namespace ttnn {
 namespace ccl {
 
+uint32_t get_topological_dimension(
+    const Tensor& tensor, const std::optional<uint32_t>& cluster_axis);
+
+uint32_t get_linearized_index_from_physical_coord(
+    const Tensor& tensor,
+    const MeshCoordinate& physical_coord,
+    const std::optional<uint32_t>& cluster_axis);
+
+std::optional<MeshCoordinate> get_physical_neighbor_from_physical_coord(
+    const Tensor& tensor,
+    const MeshCoordinate& physical_coord,
+    int offset,
+    ttnn::ccl::Topology topology,
+    const std::optional<uint32_t>& cluster_axis);
+
 struct SyncModeSpec {
     uint32_t num_signals = 0;
     CoreCoord core;
@@ -716,11 +731,7 @@ private:
 std::tuple<size_t, size_t, bool> get_forward_backward_configuration(size_t ring_size, size_t ring_index, Topology topology);
 
 // Forward/backward devices are assumed to be neighbors for 1D fabric for now
-std::tuple<std::array<uint32_t, 2>, std::array<uint32_t, 2>> get_forward_backward_line_unicast_configuration(
-    Topology topology,
-    IDevice* src_device,
-    std::optional<IDevice*> forward_device,
-    std::optional<IDevice*> backward_device);
+std::tuple<std::array<uint32_t, 2>, std::array<uint32_t, 2>> get_forward_backward_line_unicast_configuration(Topology topology, const distributed::MeshCoordinate& src_device_coord, const std::optional<distributed::MeshCoordinate>& forward_device_coord, const std::optional<distributed::MeshCoordinate>& backward_device_coord, distributed::MeshDevice* mesh_device);
 
 std::tuple<uint32_t, uint32_t> get_forward_backward_line_mcast_distance(
     size_t ring_size,
@@ -730,7 +741,7 @@ std::tuple<uint32_t, uint32_t> get_forward_backward_line_mcast_distance(
 
 // Forward/backward devices are assumed to be neighbors for 1D fabric for now
 std::tuple<std::array<uint32_t, 6>, std::array<uint32_t, 6>> get_forward_backward_line_mcast_configuration(
-    Topology topology, IDevice* src_device, std::optional<IDevice*> forward_device, std::optional<IDevice*> backward_device, uint32_t num_targets_forward, uint32_t num_targets_backward);
+    Topology topology, const distributed::MeshCoordinate& src_device_coord, const std::optional<distributed::MeshCoordinate>& forward_device_coord, const std::optional<distributed::MeshCoordinate>& backward_device_coord, uint32_t num_targets_forward, uint32_t num_targets_backward, distributed::MeshDevice* mesh_device);
 
 
 }  // namespace ccl
