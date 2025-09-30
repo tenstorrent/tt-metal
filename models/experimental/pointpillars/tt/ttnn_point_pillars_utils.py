e8b4bb57b41803b2a684790c4f343f406ec9649e
HariniMohan0102 <harini.mohan@multicorewareinc.com>
2025-09-30 16:08:22 +0000

    Added input instructions

--- models/experimental/pointpillars/tt/ttnn_point_pillars_utils.py
+++ models/experimental/pointpillars/tt/ttnn_point_pillars_utils.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
 from typing import List, Tuple, Union, Optional, Sequence
 import torch
 from torch import Tensor
