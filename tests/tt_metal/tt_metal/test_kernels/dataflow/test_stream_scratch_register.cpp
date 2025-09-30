d22d8b8d123156f6ea824faa108fe22b04c59e60
Sean Nijjar <sean.nijjar@gmail.com>
2025-09-30 21:13:24 +0000

    fixes after rebase

--- tests/tt_metal/tt_metal/test_kernels/dataflow/test_stream_scratch_register.cpp
+++ tests/tt_metal/tt_metal/test_kernels/dataflow/test_stream_scratch_register.cpp
@@ -13,7 +13,7 @@ void kernel_main() {
     // Validate that we can write up to 24 bits; use the get address API
     auto scratch_register_address = reinterpret_cast<volatile uint32_t*>(get_stream_scratch_register_address(0));
     for (uint32_t i = 1; i < 24; i++) {
-        auto test_value = 1 << (i - 1);
+        uint32_t test_value = 1 << (i - 1);
         *scratch_register_address = test_value;
         uint32_t readback = *scratch_register_address;
         if (readback != test_value) {
