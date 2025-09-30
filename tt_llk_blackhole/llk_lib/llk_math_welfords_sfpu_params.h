0df10b1457f971b19878e64d8f1bae8506a9f9b5
Vasisht Suresh <vsuresh@tenstorrent.com>
2025-09-30 12:24:07 -0700

    adding port of welfords to BH after being tested extensively on WH (#699)
    ### Ticket
    <!-- Link to Github Issue -->
    https://github.com/tenstorrent/tt-metal/issues/28826
    ### Problem description
    <!-- Provide context for the problem. -->
    Followup to previous PR that merged in the WH version. No changes
    happened between the two archs.
    ### What's changed
    <!-- Describe the approach used to solve the problem.
    Summarize the changes made and its impact. -->
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    
    ---------
    
    Co-authored-by: Filip Vranic <fvranic@tenstorrent.com>

---
+++ tt_llk_blackhole/llk_lib/llk_math_welfords_sfpu_params.h
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+#include <algorithm>
+#include <utility>
+
+#include "llk_math_eltwise_ternary_sfpu.h"
+#include "llk_sfpu_types.h"
+
+template <typename Callable, typename... ARGS>
+inline void _llk_math_welfords_sfpu_params_(Callable&& sfpu_func, uint dst_index0, uint dst_index1, uint dst_index2, ARGS&&... args)
+{
+    _llk_math_eltwise_ternary_sfpu_start_<DST_SYNC_MODE>(dst_index0);
+    std::forward<Callable>(sfpu_func)(std::forward<ARGS>(args)...);
+    _llk_math_eltwise_ternary_sfpu_done_(); // Finalize
+}
