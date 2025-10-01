79ad70c0fd55ab143254e8f241e09bc624d75849
Adrian F. Morrison <amorrison@tenstorrent.com>
2025-10-01 17:36:48 +0000

    clang tidy

--- ttnn/cpp/ttnn/operations/data_movement/reshape_view/reshape.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/reshape_view/reshape.hpp
@@ -35,20 +35,20 @@ struct ReshapeViewOperation {
         const ttnn::Shape& logical_shape,
         const std::optional<MemoryConfig>& memory_config = std::nullopt,
         const std::optional<PadValue>& pad_value = std::nullopt,
-        const TileReshapeMapMode = TileReshapeMapMode::CACHE);
+        TileReshapeMapMode = TileReshapeMapMode::CACHE);
     static ttnn::Tensor invoke(
         const ttnn::Tensor& input_tensor,
         const ttnn::Shape& logical_shape,
         const ttnn::Shape& padded_shape,
         const std::optional<MemoryConfig>& memory_config = std::nullopt,
         const std::optional<PadValue>& pad_value = std::nullopt,
-        const TileReshapeMapMode = TileReshapeMapMode::CACHE);
+        TileReshapeMapMode = TileReshapeMapMode::CACHE);
     static ttnn::Tensor invoke(
         const ttnn::Tensor& input_tensor,
         tt::stl::Span<const int32_t> shape_vector,
         const std::optional<MemoryConfig>& memory_config = std::nullopt,
         const std::optional<PadValue>& pad_value = std::nullopt,
-        const TileReshapeMapMode = TileReshapeMapMode::CACHE);
+        TileReshapeMapMode = TileReshapeMapMode::CACHE);
 };
 
 }  // namespace operations::data_movement
