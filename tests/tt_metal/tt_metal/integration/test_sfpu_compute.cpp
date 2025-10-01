7c171208acaf10b932a471540b39ea5c2f91f006
riverwuTT <riverwu@tenstorrent.com>
2025-10-01 00:56:12 +0000

    normalize include path

--- tests/tt_metal/tt_metal/integration/test_sfpu_compute.cpp
+++ tests/tt_metal/tt_metal/integration/test_sfpu_compute.cpp
@@ -7,7 +7,7 @@
 #include <gtest/gtest.h>
 #include <stddef.h>
 #include <stdint.h>
-#include "tt_metal/impl/dispatch/command_queue.hpp"
+#include "impl/dispatch/command_queue.hpp"
 #include <tt-metalium/device.hpp>
 #include <tt-metalium/host_api.hpp>
 #include <algorithm>
