b0443ead894330a5e8d002688ab8a8d13cb876e9
Jonathan Su <jonathansu@tenstorrent.com>
2025-09-30 20:52:46 -0700

    Multi device halo ccl - neighborpad (#29212)
    ### Problem description
    In the VAE's for Mochi and Wan 2.2, we had two specific cases where we
    needed to be able to exchange boundary values across devices:
    1) In order to shard a conv3d op across devices, we needed to shard the
    input across H and W, and prepad the input with the correct values from
    neighboring devices.
    2) When we shard across the T dimension (also in the context of a conv3d
    op), we need to be able to pad the front of the time dimension with the
    back of the shard from the backward device.
    
    ### What's changed
    To support this, we created a generic CCL that allows exchanging
    boundary values, like a halo op on a single device. For directions that
    don't have neighbor devices, zero and replication padding is supported.
    This exchange supports different cluster axes, multilink, and exchanging
    any dimension.
    
    A barrier semaphore is required instead of using persistent buffers.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/18088337778)
    - [x] [T3K
    nightly](https://github.com/tenstorrent/tt-metal/actions/runs/18088347003)

---
+++ ttnn/cpp/ttnn/operations/experimental/ccl/neighbor_pad_async/device/kernels/minimal_default_reader.cpp
@@ -0,0 +1,147 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "dataflow_api.h"
+#include <tt-metalium/buffer_types.hpp>
+#include "ttnn/operations/ccl/kernel_common/worker_sync_utils.hpp"
+#include "ttnn/operations/ccl/ccl_host_types.hpp"
+#include <cstdint>
+#include <utility>
+
+using address_t = uint32_t;
+using tt::tt_metal::BufferType;
+using ttnn::ccl::Topology;
+
+constexpr bool is_first_chip = get_compile_time_arg_val(0);
+constexpr bool is_last_chip = get_compile_time_arg_val(1);
+constexpr uint32_t cb_output_id = get_compile_time_arg_val(2);
+constexpr bool direction = get_compile_time_arg_val(3);
+constexpr bool is_padding_zeros = get_compile_time_arg_val(4);
+constexpr uint32_t stick_size = get_compile_time_arg_val(5);
+
+template <uint32_t stick_size_bytes>
+inline void zeroPad(uint32_t cb_output_id) {
+    //  Zero-fill from MEM_ZEROS
+    constexpr uint32_t num_full_reads = stick_size_bytes / MEM_ZEROS_SIZE;
+    constexpr uint32_t partial_read_size = stick_size_bytes % MEM_ZEROS_SIZE;
+    const uint64_t zeros_noc_addr = get_noc_addr(MEM_ZEROS_BASE);
+    uint32_t cb_write_addr = get_write_ptr(cb_output_id);
+
+    for (uint32_t i = 0; i < num_full_reads; ++i) {
+        noc_async_read(zeros_noc_addr, cb_write_addr, MEM_ZEROS_SIZE);
+        cb_write_addr += MEM_ZEROS_SIZE;
+    }
+    if (partial_read_size > 0) {
+        noc_async_read(zeros_noc_addr, cb_write_addr, partial_read_size);
+    }
+}
+
+void kernel_main() {
+    ///////////////////////////////////////////////////
+    // ARGS
+    ///////////////////////////////////////////////////
+    uint32_t arg_idx = 0;
+    // Load the input tensor spec
+    const address_t input_tensor_address = get_arg_val<address_t>(arg_idx++);
+    const address_t output_tensor_address = get_arg_val<address_t>(arg_idx++);
+    const uint32_t outer_dim_offset_start_id = get_arg_val<uint32_t>(arg_idx++);
+    const uint32_t stick_start_id = get_arg_val<uint32_t>(arg_idx++);
+    const uint32_t input_halo_dim_size = get_arg_val<uint32_t>(arg_idx++);
+    const uint32_t outer_dim_size = get_arg_val<uint32_t>(arg_idx++);
+    const uint32_t padding = get_arg_val<uint32_t>(arg_idx++);
+    const uint32_t num_sticks_to_read = get_arg_val<uint32_t>(arg_idx++);
+    const uint32_t num_sticks_per_halo_dim = get_arg_val<uint32_t>(arg_idx++);
+    size_t out_ready_sem = get_arg_val<uint32_t>(arg_idx++);
+
+    constexpr auto src_args = TensorAccessorArgs<6>();
+    uint32_t read_size = stick_size;
+    const auto src_accessor = TensorAccessor(src_args, input_tensor_address, stick_size);
+
+    uint32_t outer_dim_offset = outer_dim_offset_start_id;
+    for (uint32_t outer_dim = 0; outer_dim < outer_dim_size; outer_dim++) {
+        if (is_first_chip) {
+            if (!is_padding_zeros) {
+                // Replicate a slice of 1 from input to output
+                uint32_t src_stick_id = 0;
+                if (direction) {
+                    src_stick_id = num_sticks_per_halo_dim * (input_halo_dim_size - 1) + stick_start_id;
+                } else {
+                    src_stick_id = stick_start_id;
+                }
+                src_stick_id += outer_dim_offset;
+                for (uint32_t iter = 0; iter < num_sticks_to_read; ++iter) {
+                    cb_reserve_back(cb_output_id, 1);
+                    uint32_t src_buffer_l1_addr = get_write_ptr(cb_output_id);
+
+                    uint64_t src_noc_addr = get_noc_addr(src_stick_id, src_accessor);
+                    noc_async_read(src_noc_addr, src_buffer_l1_addr, read_size);
+
+                    src_stick_id++;
+
+                    noc_async_read_barrier();
+                    cb_push_back(cb_output_id, 1);
+                }
+            } else {
+                cb_reserve_back(cb_output_id, 1);
+                zeroPad<stick_size>(cb_output_id);
+                noc_async_read_barrier();
+                cb_push_back(cb_output_id, 1);
+            }
+        }
+
+        if (!is_last_chip) {
+            // Read the "end" of each slice into the CB to write to the neighbor
+            for (uint32_t pad_id = padding; pad_id > 0; pad_id--) {
+                uint32_t src_stick_id = 0;
+                if (direction) {
+                    src_stick_id = (padding - pad_id) * num_sticks_per_halo_dim + stick_start_id;
+                } else {
+                    src_stick_id = (input_halo_dim_size - pad_id) * num_sticks_per_halo_dim + stick_start_id;
+                }
+                src_stick_id += outer_dim_offset;
+                for (uint32_t iter = 0; iter < num_sticks_to_read; ++iter) {
+                    cb_reserve_back(cb_output_id, 1);
+                    uint32_t src_buffer_l1_addr = get_write_ptr(cb_output_id);
+
+                    uint64_t src_noc_addr = get_noc_addr(src_stick_id, src_accessor);
+                    noc_async_read(src_noc_addr, src_buffer_l1_addr, read_size);
+
+                    src_stick_id++;
+
+                    noc_async_read_barrier();
+                    cb_push_back(cb_output_id, 1);
+                }
+            }
+        }
+
+        if (direction) {
+            for (uint32_t t = 0; t < input_halo_dim_size; t++) {
+                // Copy the entire input
+                uint32_t src_stick_id = t * num_sticks_per_halo_dim + stick_start_id;
+                src_stick_id += outer_dim_offset;
+                for (uint32_t iter = 0; iter < num_sticks_to_read; ++iter) {
+                    cb_reserve_back(cb_output_id, 1);
+                    uint32_t src_buffer_l1_addr = get_write_ptr(cb_output_id);
+
+                    uint64_t src_noc_addr = get_noc_addr(src_stick_id, src_accessor);
+                    noc_async_read(src_noc_addr, src_buffer_l1_addr, read_size);
+
+                    src_stick_id++;
+
+                    noc_async_read_barrier();
+                    cb_push_back(cb_output_id, 1);
+                }
+            }
+        }
+
+        outer_dim_offset += (num_sticks_per_halo_dim * input_halo_dim_size);
+    }
+
+    // Check that the semaphore is received
+    if (!is_first_chip) {
+        noc_semaphore_wait_min(reinterpret_cast<volatile tt_l1_ptr uint32_t*>(out_ready_sem), outer_dim_size);
+    }
+
+    noc_semaphore_set(reinterpret_cast<volatile tt_l1_ptr uint32_t*>(out_ready_sem), 0);
+}
