c6140b9179b8ff9c44f4060b94b60ebfb538dbad
Adam Thompson <athompson@tenstorrent.com>
2025-10-01 20:40:24 +0000

    update bindings/includes
    *needed create_socket, export_enum includes
    *add bindings as the trampoline class/binding is not working as
    expected

--- ttnn/cpp/ttnn-pybind/isocket.cpp
+++ ttnn/cpp/ttnn-pybind/isocket.cpp
@@ -6,10 +6,12 @@
 // #include <pybind11/trampoline_self_life_support.h>
 
 #include "api/ttnn/distributed/bidirectional_fabric_socket.hpp"
+#include "api/ttnn/distributed/create_socket.hpp"
 #include "api/ttnn/distributed/fabric_socket.hpp"
 #include "api/ttnn/distributed/isocket.hpp"
 #include "api/ttnn/distributed/mpi_socket.hpp"
 #include "ttnn-pybind/isocket.hpp"
+#include "ttnn-pybind/export_enum.hpp"
 
 namespace ttnn::distributed {
 
@@ -62,8 +64,10 @@ auto constexpr tensor_arg_string = "tensor";
 }  // namespace
 
 void py_isocket_module_types(py::module& module) {
+    export_enum<ttnn::distributed::SocketType>(module, "SocketType");
+    export_enum<ttnn::distributed::EndpointSocketType>(module, "EndpointSocketType");
+
     py::class_<ttnn::distributed::ISocket, PyISocket /*, py::smart_holder*/>(module, "ISocket")
-        .def(py::init<>())
         .def("send", &ISocket::send, py::arg(tensor_arg_string), send_doc_string)
         .def("recv", &ISocket::recv, py::arg(tensor_arg_string), recv_doc_string)
         .def("get_rank", &ISocket::get_rank, get_rank_doc_string)
@@ -75,35 +79,61 @@ void py_isocket_module_types(py::module& module) {
              const std::shared_ptr<tt::tt_metal::distributed::MeshDevice>&,
              const tt::tt_metal::distributed::SocketConfig&,
              const tt::tt_metal::distributed::SocketConfig&>())
-        .def("send", &BidirectionalFabricSocket::send, py::arg(tensor_arg_string), send_doc_string)
-        .def("recv", &BidirectionalFabricSocket::recv, py::arg(tensor_arg_string), recv_doc_string)
-        .def("get_rank", &BidirectionalFabricSocket::get_rank, get_rank_doc_string)
+        .def_static("create", &BidirectionalFabricSocket::create)
+        .def("send", &ttnn::distributed::BidirectionalFabricSocket::send, py::arg(tensor_arg_string), send_doc_string)
+        .def("recv", &ttnn::distributed::BidirectionalFabricSocket::recv, py::arg(tensor_arg_string), recv_doc_string)
+        .def("get_rank", &ttnn::distributed::BidirectionalFabricSocket::get_rank, get_rank_doc_string)
         .def(
             "get_distributed_context",
-            &BidirectionalFabricSocket::get_distributed_context,
-            get_distributed_context_doc_string)
-        .def_static("create", &BidirectionalFabricSocket::create);
+            &ttnn::distributed::BidirectionalFabricSocket::get_distributed_context,
+            get_distributed_context_doc_string);
 
     py::class_<ttnn::distributed::FabricSocket, ttnn::distributed::ISocket /*, py::smart_holder*/>(
         module, "FabricSocket")
         .def(py::init<
              const std::shared_ptr<tt::tt_metal::distributed::MeshDevice>&,
              const tt::tt_metal::distributed::SocketConfig&>())
-        .def("send", &FabricSocket::send, py::arg(tensor_arg_string), send_doc_string)
-        .def("recv", &FabricSocket::recv, py::arg(tensor_arg_string), recv_doc_string)
-        .def("get_rank", &FabricSocket::get_rank, get_rank_doc_string)
-        .def("get_distributed_context", &FabricSocket::get_distributed_context, get_distributed_context_doc_string)
-        .def_static("create", &FabricSocket::create);
+        .def_static("create", &FabricSocket::create)
+        .def("send", &ttnn::distributed::FabricSocket::send, py::arg(tensor_arg_string), send_doc_string)
+        .def("recv", &ttnn::distributed::FabricSocket::recv, py::arg(tensor_arg_string), recv_doc_string)
+        .def("get_rank", &ttnn::distributed::FabricSocket::get_rank, get_rank_doc_string)
+        .def(
+            "get_distributed_context",
+            &ttnn::distributed::FabricSocket::get_distributed_context,
+            get_distributed_context_doc_string);
 
     py::class_<ttnn::distributed::MPISocket, ttnn::distributed::ISocket /*, py::smart_holder*/>(module, "MPISocket")
         .def(py::init<
              const std::shared_ptr<tt::tt_metal::distributed::MeshDevice>&,
              const tt::tt_metal::distributed::SocketConfig&>())
-        .def("send", &MPISocket::send, py::arg(tensor_arg_string), send_doc_string)
-        .def("recv", &MPISocket::recv, py::arg(tensor_arg_string), recv_doc_string)
-        .def("get_rank", &MPISocket::get_rank, get_rank_doc_string)
-        .def("get_distributed_context", &MPISocket::get_distributed_context, get_distributed_context_doc_string)
-        .def_static("create", &MPISocket::create);
+        .def_static("create", &MPISocket::create)
+        .def("send", &ttnn::distributed::MPISocket::send, py::arg(tensor_arg_string), send_doc_string)
+        .def("recv", &ttnn::distributed::MPISocket::recv, py::arg(tensor_arg_string), recv_doc_string)
+        .def("get_rank", &ttnn::distributed::MPISocket::get_rank, get_rank_doc_string)
+        .def(
+            "get_distributed_context",
+            &ttnn::distributed::MPISocket::get_distributed_context,
+            get_distributed_context_doc_string);
+
+    module.def(
+        "create_socket",
+        [](ttnn::distributed::SocketType socket_type,
+           ttnn::distributed::EndpointSocketType endpoint_socket_type,
+           std::shared_ptr<tt::tt_metal::distributed::MeshDevice>& mesh_device,
+           int other_rank,
+           const tt::tt_metal::distributed::SocketConfig& socket_config) {
+            return ttnn::distributed::create_socket(
+                socket_type,
+                endpoint_socket_type,
+                mesh_device,
+                tt::tt_metal::distributed::multihost::Rank(other_rank),
+                socket_config);
+        },
+        py::arg("socket_type"),
+        py::arg("endpoint_socket_type"),
+        py::arg("mesh_device"),
+        py::arg("other_rank"),
+        py::arg("socket_config"));
 }
 
 }  // namespace ttnn::distributed
