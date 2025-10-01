90cda8d1ba171070d3322c17b808e9f45d667db7
Adam Thompson <athompson@tenstorrent.com>
2025-10-01 17:13:57 +0000

    remove duplicate type alias

--- tt-train/sources/ttml/metal/ops/rmsnorm_bw/device/rmsnorm_bw_device_operation.hpp
+++ tt-train/sources/ttml/metal/ops/rmsnorm_bw/device/rmsnorm_bw_device_operation.hpp
@@ -17,7 +17,6 @@ struct RMSNormBackwardDeviceOperation {
     using tensor_args_t = ttml::metal::ops::rmsnorm_bw::device::tensor_args_t;
     using spec_return_value_t = ttml::metal::ops::rmsnorm_bw::device::spec_return_value_t;
     using tensor_return_value_t = ttml::metal::ops::rmsnorm_bw::device::tensor_return_value_t;
-    using tensor_return_value_t = ttml::metal::ops::rmsnorm_bw::device::tensor_return_value_t;
     using program_factory_t = std::variant<RMSNormBackwardProgramFactory>;
 
     static program_factory_t select_program_factory(const operation_attributes_t&, const tensor_args_t&);
