48180d72d16a26a353eedb7d45ebbcfc814bb6a7
Colman Glagovich <cglagovich@tenstorrent.com>
2025-10-02 20:33:37 +0000

    Single core MM functional. Config takes blocking factors

--- ttnn/cpp/ttnn/operations/experimental/minimal_matmul/device/minimal_matmul_device_operation.hpp
+++ ttnn/cpp/ttnn/operations/experimental/minimal_matmul/device/minimal_matmul_device_operation.hpp
@@ -16,14 +16,40 @@
 namespace ttnn::operations::experimental::minimal_matmul {
 
 struct MinimalMatmulConfig {
-    MinimalMatmulConfig(CoreCoord compute_with_storage_grid_size_ = {1, 1}) :
+    MinimalMatmulConfig(
+        uint32_t M_block_size_ = 1,
+        uint32_t K_block_size_ = 1,
+        uint32_t N_block_size_ = 1,
+        uint32_t subblock_h_ = 1,
+        uint32_t subblock_w_ = 1,
+        CoreCoord compute_with_storage_grid_size_ = {1, 1}) :
+        M_block_size(M_block_size_),
+        K_block_size(K_block_size_),
+        N_block_size(N_block_size_),
+        subblock_h(subblock_h_),
+        subblock_w(subblock_w_),
         compute_with_storage_grid_size(compute_with_storage_grid_size_) {}
+
+    uint32_t M_block_size;
+    uint32_t K_block_size;
+    uint32_t N_block_size;
+    uint32_t subblock_h;
+    uint32_t subblock_w;
 
     CoreCoord compute_with_storage_grid_size;
 
-    static constexpr auto attribute_names = std::make_tuple("compute_with_storage_grid_size");
+    static constexpr auto attribute_names = std::make_tuple(
+        "M_block_size", "K_block_size", "N_block_size", "subblock_h", "subblock_w", "compute_with_storage_grid_size");
 
-    auto attribute_values() const { return std::forward_as_tuple(this->compute_with_storage_grid_size); }
+    auto attribute_values() const {
+        return std::forward_as_tuple(
+            this->M_block_size,
+            this->K_block_size,
+            this->N_block_size,
+            this->subblock_h,
+            this->subblock_w,
+            this->compute_with_storage_grid_size);
+    }
 };
 
 struct MinimalMatmulOp {
--- ttnn/cpp/ttnn/operations/experimental/minimal_matmul/device/minimal_matmul_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/experimental/minimal_matmul/device/minimal_matmul_program_factory.cpp
@@ -55,12 +55,12 @@ tt::tt_metal::operation::ProgramWithCallbacks minimal_matmul_factory(
     /**
      * TODO: Pick optimal subblock sizes, hardcoded to 2x4 or 1x4 or 2x2
      */
-    uint32_t subblock_h = 1;
-    uint32_t subblock_w = 1;
+    uint32_t subblock_h = config.subblock_h;
+    uint32_t subblock_w = config.subblock_w;
 
-    uint32_t M_block = 1;
-    uint32_t K_block = 1;
-    uint32_t N_block = 1;
+    uint32_t M_block = config.M_block_size;
+    uint32_t K_block = config.K_block_size;
+    uint32_t N_block = config.N_block_size;
 
     uint32_t in0_block_num_tiles = M_block * K_block;
     uint32_t in1_block_num_tiles = K_block * N_block;
@@ -75,25 +75,64 @@ tt::tt_metal::operation::ProgramWithCallbacks minimal_matmul_factory(
     // Create circular buffers for vol2col, weights, bias and matmul intermediates
     uint32_t next_cb_index = tt::CBIndex::c_0;
     uint32_t in0_cb_id = next_cb_index++;
-    tt::tt_metal::create_cb(
-        in0_cb_id, program, core_grid, in0_cb_num_tiles * input_tile_size, in0_cb_num_tiles, data_format);
+    tt::tt_metal::create_cb(in0_cb_id, program, core_grid, input_tile_size, in0_cb_num_tiles, data_format);
 
     uint32_t in1_cb_id = next_cb_index++;
-    tt::tt_metal::create_cb(
-        in1_cb_id, program, core_grid, in1_cb_num_tiles * input_tile_size, in1_cb_num_tiles, data_format);
+    tt::tt_metal::create_cb(in1_cb_id, program, core_grid, input_tile_size, in1_cb_num_tiles, data_format);
 
     uint32_t out_cb_id = next_cb_index++;
-    tt::tt_metal::create_cb(
-        out_cb_id, program, core_grid, out_cb_num_tiles * input_tile_size, out_cb_num_tiles, data_format);
+    tt::tt_metal::create_cb(out_cb_id, program, core_grid, input_tile_size, out_cb_num_tiles, data_format);
 
     uint32_t intermediate_cb_id = next_cb_index++;
     tt::tt_metal::create_cb(
-        intermediate_cb_id,
-        program,
-        core_grid,
-        interm_cb_num_tiles * intermediate_tile_size,
-        interm_cb_num_tiles,
-        data_format);
+        intermediate_cb_id, program, core_grid, intermediate_tile_size, interm_cb_num_tiles, intermediate_data_format);
+
+    log_info(tt::LogOp, "in0_cb_id: {}", in0_cb_id);
+    log_info(tt::LogOp, "in1_cb_id: {}", in1_cb_id);
+    log_info(tt::LogOp, "out_cb_id: {}", out_cb_id);
+    log_info(tt::LogOp, "intermediate_cb_id: {}", intermediate_cb_id);
+    log_info(tt::LogOp, "M_tiles: {}", M_tiles);
+    log_info(tt::LogOp, "K_tiles: {}", K_tiles);
+    log_info(tt::LogOp, "N_tiles: {}", N_tiles);
+    log_info(tt::LogOp, "M_block: {}", M_block);
+    log_info(tt::LogOp, "K_block: {}", K_block);
+    log_info(tt::LogOp, "N_block: {}", N_block);
+    log_info(tt::LogOp, "subblock_h: {}", subblock_h);
+    log_info(tt::LogOp, "subblock_w: {}", subblock_w);
+    log_info(tt::LogOp, "input_tile_size: {}", input_tile_size);
+    log_info(tt::LogOp, "intermediate_tile_size: {}", intermediate_tile_size);
+    log_info(tt::LogOp, "intermediate_data_format: {}", intermediate_data_format);
+    log_info(tt::LogOp, "in0_cb_num_tiles: {}", in0_cb_num_tiles);
+    log_info(tt::LogOp, "in1_cb_num_tiles: {}", in1_cb_num_tiles);
+    log_info(tt::LogOp, "out_cb_num_tiles: {}", out_cb_num_tiles);
+    log_info(tt::LogOp, "interm_cb_num_tiles: {}", interm_cb_num_tiles);
+
+    // Get environment variables to optionally skip data movement kernels
+    bool skip_in0 = false;
+    bool skip_in1 = false;
+    bool skip_out = false;
+    if (const char* env_p = std::getenv("TT_MM_SKIP_IN0")) {
+        skip_in0 = std::string(env_p) == "1";
+    }
+    if (const char* env_p = std::getenv("TT_MM_SKIP_IN1")) {
+        skip_in1 = std::string(env_p) == "1";
+    }
+    if (const char* env_p = std::getenv("TT_MM_SKIP_OUT")) {
+        skip_out = std::string(env_p) == "1";
+    }
+    std::map<std::string, std::string> defines;
+    if (skip_in0) {
+        defines["SKIP_IN0"] = "1";
+        log_warning(tt::LogOp, "Skipping in0 data movement! PCC will be wrong!");
+    }
+    if (skip_in1) {
+        defines["SKIP_IN1"] = "1";
+        log_warning(tt::LogOp, "Skipping in1 data movement! PCC will be wrong!");
+    }
+    if (skip_out) {
+        defines["SKIP_OUT"] = "1";
+        log_warning(tt::LogOp, "Skipping out data movement! PCC will be wrong!");
+    }
 
     std::vector<uint32_t> reader_compile_time_args = {
         M_tiles, K_tiles, N_tiles, M_block, K_block, N_block, input_tile_size};
@@ -103,7 +142,7 @@ tt::tt_metal::operation::ProgramWithCallbacks minimal_matmul_factory(
         program,
         "ttnn/cpp/ttnn/operations/experimental/minimal_matmul/device/kernels/dm_in0.cpp",
         core_grid,
-        tt::tt_metal::ReaderDataMovementConfig(reader_compile_time_args));
+        tt::tt_metal::ReaderDataMovementConfig(reader_compile_time_args, defines));
 
     // const uint32_t dst_size = fp32_dest_acc_en ? 4 : 8;
     // const uint32_t in0_block_w = matmul_K_t;
@@ -142,7 +181,7 @@ tt::tt_metal::operation::ProgramWithCallbacks minimal_matmul_factory(
         program,
         "ttnn/cpp/ttnn/operations/experimental/minimal_matmul/device/kernels/dm_in1_out.cpp",
         core_grid,
-        tt::tt_metal::WriterDataMovementConfig(writer_compile_time_args));
+        tt::tt_metal::WriterDataMovementConfig(writer_compile_time_args, defines));
 
     uint32_t input_addr = input_tensor.buffer()->address();
     uint32_t weight_addr = weight_tensor.buffer()->address();
@@ -161,6 +200,8 @@ tt::tt_metal::operation::ProgramWithCallbacks minimal_matmul_factory(
 
         writer_args_per_core[core_id] = {weight_addr, out_addr};
     }
+    SetRuntimeArgs(program, reader_kernels_id, cores, reader_args_per_core);
+    SetRuntimeArgs(program, writer_kernels_id, cores, writer_args_per_core);
 
     auto override_runtime_arguments_callback =
         [num_cores, cores, reader_kernels_id, writer_kernels_id](
