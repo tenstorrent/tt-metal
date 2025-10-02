e6b9773dea45714721a6869d1c3928a1a9b42ba0
Siddharth Sundaram <ssundaram@tenstorrent.com>
2025-10-02 13:49:47 -0500

    #26591: Migrate `EnqueueRecordEventToHost` to `MeshCommandQueue::enqueue_record_event` (#29725)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/26591)
    
    ### Problem description
    N/A
    
    ### What's changed
    
    - Migration of `EnqueueRecordEventToHost` API, contributing to the
    effort to deprecate/remove `tt_metal/api/tt-metalium/distributed.hpp`
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes ([link to
    run](https://github.com/tenstorrent/tt-metal/actions/runs/18141581213))
    - [x] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main) ([link to
    run](https://github.com/tenstorrent/tt-metal/actions/runs/18141587486))

--- tt_metal/distributed/distributed.cpp
+++ tt_metal/distributed/distributed.cpp
@@ -23,13 +23,6 @@ void EnqueueMeshWorkload(MeshCommandQueue& mesh_cq, MeshWorkload& mesh_workload,
     mesh_cq.enqueue_mesh_workload(mesh_workload, blocking);
 }
 
-MeshEvent EnqueueRecordEventToHost(
-    MeshCommandQueue& mesh_cq,
-    tt::stl::Span<const SubDeviceId> sub_device_ids,
-    const std::optional<MeshCoordinateRange>& device_range) {
-    return mesh_cq.enqueue_record_event_to_host(sub_device_ids, device_range);
-}
-
 void EventSynchronize(const MeshEvent& event) {
     if (!tt::tt_metal::MetalContext::instance().rtoptions().get_fast_dispatch()) {
         return;
