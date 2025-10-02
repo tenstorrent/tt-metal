e3d0b3bc4249ea1ec7a2a5eb8110e6e26162534a
Vladyslav Tsilytskyi <vtsilytskyi@tenstorrent.com>
2025-10-02 16:59:45 +0000

    Some kernel impl, runtime error

--- ttnn/cpp/ttnn/operations/normalization/softmax_backward/device/softmax_backward_device_operation.hpp
+++ ttnn/cpp/ttnn/operations/normalization/softmax_backward/device/softmax_backward_device_operation.hpp
@@ -4,81 +4,66 @@
 
 #pragma once
 
-#include <optional>
-#include <variant>
+#include "softmax_backward_operation_types.hpp"
+#include "softmax_backward_program_factory.hpp"
 
-#include "ttnn/tensor/tensor.hpp"
-#include "ttnn/core.hpp"
-#include "ttnn/device_operation.hpp"
-#include "ttnn/types.hpp"
 #include "ttnn/decorators.hpp"
+
+#include <tuple>
+#include <variant>
 
 namespace ttnn::operations::normalization {
 namespace softmax_backward {
 
 struct SoftmaxBackwardDeviceOperation {
-    // Define the operation attributes. This is it to store all variables needed by operations that aren't tensors
-    struct operation_attributes_t {
-        const uint32_t dim;
-    };
-
-    // Define the tensor arguments. This is it to store all tensors passed in and/or out of the operation
-    // Tensor arguments don't need to be just input tensors, they can be output tensors, input/output tensors, optional
-    // tensors, etc.
-    struct tensor_args_t {
-        const Tensor& softmax_output;
-        const Tensor& upstream_grad;
-    };
-
-    // Define the return types for the spec(s) of the operation
-    // Can be a single ttnn::TensorSpec, std::optional<ttnn::TensorSpec>, std::vector<ttnn::TensorSpec>,
-    // std::tuple<ttnn::TensorSpec> etc.
-    using spec_return_value_t = ttnn::TensorSpec;
-
-    // Define the return types for the tensor(s) of the operation
-    // Can be a single Tensor, std::optional<Tensor, ...>, std::vector<Tensor>, std::tuple<Tensor, ...> etc.
-    using tensor_return_value_t = Tensor;
+    using operation_attributes_t = softmax_backward::operation_attributes_t;
+    using tensor_args_t = softmax_backward::tensor_args_t;
+    using spec_return_value_t = softmax_backward::spec_return_value_t;
+    using tensor_return_value_t = softmax_backward::tensor_return_value_t;
 
     // Note spec_return_value_t and tensor_return_value_t should follow the same pattern
     // i.e. if spec_return_value_t is a std::vector<std::optional<ttnn::TensorSpec>> then tensor_return_value_t should
     // be std::vector<std::optional<Tensor>>
     struct SingleCore {
-        // Shared variables are the variables that are shared between the create and override_runtime_arguments methods
-        struct shared_variables_t {
-            tt::tt_metal::KernelHandle unary_reader_kernel_id;
-            tt::tt_metal::KernelHandle unary_writer_kernel_id;
-        };
-        using cached_program_t = ttnn::device_operation::CachedProgram<shared_variables_t>;
+        using shared_variables_t = SoftmaxBackwardProgramFactory::shared_variables_t;
+        using cached_program_t = SoftmaxBackwardProgramFactory::cached_program_t;
 
         static cached_program_t create(
             const operation_attributes_t& operation_attributes,
             const tensor_args_t& tensor_args,
-            tensor_return_value_t& tensor_return_value);
+            tensor_return_value_t& tensor_return_value) {
+            return SoftmaxBackwardProgramFactory::create(operation_attributes, tensor_args, tensor_return_value);
+        }
+
         static void override_runtime_arguments(
             cached_program_t& cached_program,
             const operation_attributes_t& operation_attributes,
             const tensor_args_t& tensor_args,
-            tensor_return_value_t& tensor_return_value);
+            tensor_return_value_t& tensor_return_value) {
+            SoftmaxBackwardProgramFactory::override_runtime_arguments(
+                cached_program, operation_attributes, tensor_args, tensor_return_value);
+        }
     };
+
     struct MultiCore {
-        // Shared variables are the variables that are shared between the create and override_runtime_arguments methods
-        struct shared_variables_t {
-            tt::tt_metal::KernelHandle unary_reader_kernel_id;
-            tt::tt_metal::KernelHandle unary_writer_kernel_id;
-            std::size_t num_cores;
-            std::size_t num_cores_y;
-        };
-        using cached_program_t = ttnn::device_operation::CachedProgram<shared_variables_t>;
+        using shared_variables_t = SoftmaxBackwardProgramFactory::shared_variables_t;
+        using cached_program_t = SoftmaxBackwardProgramFactory::cached_program_t;
 
         static cached_program_t create(
             const operation_attributes_t& operation_attributes,
             const tensor_args_t& tensor_args,
-            tensor_return_value_t& tensor_return_value);
+            tensor_return_value_t& tensor_return_value) {
+            return SoftmaxBackwardProgramFactory::create(operation_attributes, tensor_args, tensor_return_value);
+        }
+
         static void override_runtime_arguments(
             cached_program_t& cached_program,
             const operation_attributes_t& operation_attributes,
             const tensor_args_t& tensor_args,
-            tensor_return_value_t& tensor_return_value);
+            tensor_return_value_t& tensor_return_value) {
+            SoftmaxBackwardProgramFactory::override_runtime_arguments(
+                cached_program, operation_attributes, tensor_args, tensor_return_value);
+        }
     };
 
     using program_factory_t = std::variant<SingleCore, MultiCore>;
@@ -103,11 +88,9 @@ struct SoftmaxBackwardDeviceOperation {
     // API call to map user arguments to operation attributes and tensor args.
     // This is the only method that is called by the user
     // The user will be able to call the operation using `tensor_return_value_t output =
-    // ttnn::prim::example(input_tensor)` after the op is registered Keep in mind that the the overload with `queue_id`
-    // argument will be added automatically for primitive operations So, the user can also call this operation using
-    // `tensor_return_value_t output = ttnn::prim::example(queue_id, input_tensor)`
+    // ttnn::prim::softmax_backward(softmax_output, upstream_grad, dim)` after the op is registered.
     static std::tuple<operation_attributes_t, tensor_args_t> invoke(
-        const Tensor& input_tensor, const Tensor& grad, uint32_t dim);
+        const ttnn::Tensor& softmax_output, const ttnn::Tensor& upstream_grad, uint32_t dim);
 
     // Optional methods
 
@@ -125,10 +108,10 @@ struct SoftmaxBackwardDeviceOperation {
     */
 };
 
-Tensor softmax_backward(
-    const Tensor& y,     // softmax output
-    const Tensor& grad,  // upstream grad dL/dy
-    uint32_t dim         // reduction dimension (same as fwd)
+ttnn::Tensor softmax_backward(
+    const ttnn::Tensor& softmax_output,  // softmax output
+    const ttnn::Tensor& upstream_grad,   // upstream grad dL/dy
+    uint32_t dim                         // reduction dimension (same as fwd)
 );
 
 }  // namespace softmax_backward
