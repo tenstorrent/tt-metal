5cecc4ecdaa0c1bbe94be2422dda3bd3f410fcd8
Ubuntu <ioannis_alexiou@epam.com>
2025-10-03 09:13:58 +0000

    added cache path1

--- models/tt_transformers/tests/multimodal/run_test.sh
+++ models/tt_transformers/tests/multimodal/run_test.sh
@@ -6,7 +6,7 @@ fail=0
 llama11b=meta-llama/Llama-3.2-11B-Vision-Instruct
 tt_cache=/mnt/MLPerf/huggingface/tt_cache/$llama11b
 
-HF_HUB_CACHE=/mnt/MLPerf/huggingface/hub HF_MODEL=llama11b TT_CACHE_PATH=$tt_cache pytest -n auto models/tt_transformers/tests/multimodal/test_llama_image_transformer.py  --timeout 420 || fail=1
+HF_HUB_CACHE=/mnt/MLPerf/huggingface/hub HF_MODEL=$llama11b TT_CACHE_PATH=$tt_cache pytest -n auto models/tt_transformers/tests/multimodal/test_llama_image_transformer.py  --timeout 420 || fail=1
 echo "LOG_METAL: Llama3 accuracy tests for $llama_dir completed"
 if [[ $fail -ne 0 ]]; then
 exit 1
