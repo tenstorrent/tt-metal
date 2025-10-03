470378973c60e5b465a4531b7b98df18f44cf4d7
Edwin Lee <edwinlee@tenstorrent.com>
2025-10-03 12:32:47 -0400

    Docs Update for Fused/Matmul/Reduce (#29845)
    ### Ticket
    #28959 
    #29861
    
    ### Problem description
    Fused/Matmul/Reduce needed further attention. 
    
    ### What's changed
    - Function headers were added for all ops
    - Matmul support matrices were added
    - Memory support section added for all ops
    - Ensure ops mention that tensors need to be on-device
    - Assorted spacing issues were fixed 
    - The prod op now documents the NC multidim variant
    - Some notes on the output tensors were added
    - Fixed some examples that could not be run
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI
    [passes](https://github.com/tenstorrent/tt-metal/actions/runs/18173341478)
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-quick.yaml)
    CI passes (if applicable)
    - [ ] [Galaxy demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/reduction/accumulation/cumprod/cumprod_pybind.cpp
+++ ttnn/cpp/ttnn/operations/reduction/accumulation/cumprod/cumprod_pybind.cpp
@@ -18,6 +18,8 @@ namespace ttnn::operations::reduction::accumulation::detail {
 void bind_reduction_cumprod_operation(py::module& module) {
     auto docstring =
         R"doc(
+        ``ttnn.cumprod(input: ttnn.Tensor, dim: int, dtype: Optional[ttnn.DataType] = None, reverse_order: bool = False, out: Optional[ttnn.Tensor] = None, memory_config: Optional[ttnn.MemoryConfig] = None) -> ttnn.Tensor``
+
         Returns cumulative product of `input` along dimension `dim`
         For a given `input` of size N, the `output` will also contain N elements and be such that:
 
@@ -26,7 +28,7 @@ void bind_reduction_cumprod_operation(py::module& module) {
 
 
         Args:
-            input (ttnn.Tensor): input tensor
+            input (ttnn.Tensor): input tensor. Must be on the device.
             dim (int): dimension along which to compute cumulative product
 
         Keyword Args:
@@ -36,10 +38,6 @@ void bind_reduction_cumprod_operation(py::module& module) {
 
         Returns:
             ttnn.Tensor: the output tensor.
-
-        Example:
-            input_tensor = ttnn.rand((N, N), device=device)
-            output_tensor = ttnn.cumprod(input_tensor, dim=0)
 
         Note:
             If both `dtype` and `output` are specified then `output.dtype` must match `dtype`.
@@ -62,22 +60,27 @@ void bind_reduction_cumprod_operation(py::module& module) {
                  - 3, 4, 5
                  - dim in {0, 1, ..., rank - 3} or dim in {-rank, -rank + 1, ..., -3}
 
+        Memory Support:
+            - Interleaved: DRAM and L1
+
+        Limitations:
+            - Preallocated output must have the same shape as the input
+            - Preallocated output for integer types is not supported
+
         Example:
+            .. code-block:: python
 
-        .. code-block:: python
+                # Create tensor
+                tensor_input = ttnn.rand((2,3,4), device=device)
 
-            import ttnn
+                # Apply ttnn.cumprod() on dim=0
+                tensor_output = ttnn.cumprod(tensor_input, dim=0)
 
-            # Create tensor
-            tensor_input = ttnn.rand((2,3,4), device=device)
+                # With preallocated output and dtype
+                preallocated_output = ttnn.rand([2, 3, 4], dtype=ttnn.bfloat16, device=device)
 
-            # Apply ttnn.cumprod() on dim=0
-            tensor_output = ttnn.cumprod(tensor_input, dim=0)
+                tensor_output = ttnn.cumprod(tensor_input, dim=0, dtype=ttnn.bfloat16, out=preallocated_output)
 
-            # With preallocated output and dtype
-            preallocated_output = ttnn.from_torch(torch.rand([2, 3, 4]), dtype=ttnn.bfloat16, device=device)
-
-            tensor_output = ttnn.cumprod(tensor_input, dim=0, dtype=torch.bfloat16, output=preallocated_output)
         )doc";
 
     using OperationType = decltype(ttnn::cumprod);
