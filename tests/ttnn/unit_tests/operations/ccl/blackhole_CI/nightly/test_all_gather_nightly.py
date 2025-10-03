ec7c327a8dd4a46bb00a70d19752e81108571a03
Juan Camilo Vega <jvega@tenstorrent.com>
2025-10-03 18:11:04 +0000

    Add galaxy All Gather test

--- tests/ttnn/unit_tests/operations/ccl/blackhole_CI/nightly/test_all_gather_nightly.py
+++ tests/ttnn/unit_tests/operations/ccl/blackhole_CI/nightly/test_all_gather_nightly.py
@@ -234,6 +234,7 @@ def test_all_gather_linear_4D_nightly(
 
 @skip_for_wormhole_b0()
 @skip_for_n_or_less_dev(2)
+@skip_for_n_dev(32)
 @pytest.mark.parametrize("num_links", [1, 2], ids=["1_link", "2_links"])
 @pytest.mark.parametrize(
     "ag_output_shape, dim, layout",
@@ -303,6 +304,109 @@ def test_all_gather_ring_nightly(
     cluster_axis,
 ):
     num_devices = bh_1d_mesh_device.shape[0]
+    validate_test(num_devices, all_gather_topology, bh_1d_mesh_device.shape, cluster_axis)
+    if cluster_axis == 0:
+        submesh_device = bh_1d_mesh_device.create_submesh(ttnn.MeshShape((num_devices, 1)))
+    else:
+        submesh_device = bh_1d_mesh_device.create_submesh(ttnn.MeshShape((1, num_devices)))
+    run_all_gather_impl(
+        submesh_device,
+        num_devices,
+        ag_output_shape,
+        dim,
+        num_links,
+        ag_input_dtype,
+        layout,
+        mem_config_input,
+        mem_config_ag,
+        all_gather_topology=all_gather_topology,
+        enable_trace=enable_trace,
+        num_iters=num_iters,
+        cluster_axis=cluster_axis,
+        chunks_per_sync=chunks_per_sync,
+        num_workers_per_link=num_workers_per_link,
+        num_buffers_per_channel=num_buffers_per_channel,
+        allowed_pcc=0.9999,
+    )
+    ttnn.ReadDeviceProfiler(submesh_device)
+
+
+@pytest.mark.parametrize("num_links", [1, 2], ids=["1_link", "2_links"])
+@pytest.mark.parametrize(
+    "num_devices, ag_output_shape, dim, layout",
+    [
+        (8, [1, 1, 128, 2048], 3, ttnn.TILE_LAYOUT),
+        (8, [1, 1, 256, 768], 3, ttnn.TILE_LAYOUT),
+    ],
+    ids=["4_device_test", "4_device_large_test"],
+)
+@pytest.mark.parametrize(
+    "ag_input_dtype",
+    [
+        ttnn.uint32,
+        ttnn.bfloat8_b,
+    ],
+    ids=[
+        "uint_32",
+        "bfloat_8",
+    ],
+)
+@pytest.mark.parametrize(
+    "mem_config_input, mem_config_ag",
+    [
+        (
+            ttnn.MemoryConfig(ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.DRAM),
+            ttnn.MemoryConfig(ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.DRAM),
+        ),
+        (
+            ttnn.MemoryConfig(ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.L1),
+            ttnn.MemoryConfig(ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.L1),
+        ),
+    ],
+)
+@pytest.mark.parametrize(
+    "enable_trace, num_iters",
+    [
+        (True, 3),
+        (False, 3),
+    ],
+    ids=["trace", "non-trace"],
+)
+@pytest.mark.parametrize(
+    "device_params, all_gather_topology",
+    [
+        ({"fabric_config": ttnn.FabricConfig.FABRIC_1D, "trace_region_size": 90112}, ttnn.Topology.Linear),
+    ],
+    indirect=["device_params"],
+)
+@pytest.mark.parametrize(
+    "cluster_axis",
+    [
+        0,
+        1,
+    ],
+)
+@pytest.mark.parametrize("chunks_per_sync", [20])
+@pytest.mark.parametrize("num_workers_per_link", [2])
+@pytest.mark.parametrize("num_buffers_per_channel", [2])
+def test_all_gather_galaxy(
+    bh_1d_mesh_device,
+    num_devices,
+    ag_output_shape,
+    dim,
+    num_links,
+    ag_input_dtype,
+    layout,
+    mem_config_input,
+    mem_config_ag,
+    enable_trace,
+    all_gather_topology,
+    num_iters,
+    chunks_per_sync,
+    num_workers_per_link,
+    num_buffers_per_channel,
+    cluster_axis,
+):
     validate_test(num_devices, all_gather_topology, bh_1d_mesh_device.shape, cluster_axis)
     if cluster_axis == 0:
         submesh_device = bh_1d_mesh_device.create_submesh(ttnn.MeshShape((num_devices, 1)))
