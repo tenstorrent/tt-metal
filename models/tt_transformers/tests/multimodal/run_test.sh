a231f5ab80ad1ecd7f1317a9601793e85730a69b
Ubuntu <ioannis_alexiou@epam.com>
2025-10-03 08:43:56 +0000

    edit path weights

--- models/tt_transformers/tests/multimodal/run_test.sh
+++ models/tt_transformers/tests/multimodal/run_test.sh
@@ -3,7 +3,7 @@ set -e
 
 fail=0
 # Llama3.2-11B
-llama11b=meta-llama/Llama-3.2-11B-Vision-Instruct
+llama11b=/mnt/MLPerf/huggingface/hub/meta-llama/Llama-3.2-11B-Vision-Instruct
 
 HF_MODEL=$llama11b pytest -n auto models/tt_transformers/tests/multimodal/test_llama_image_transformer.py  --timeout 420 || fail=1
 echo "LOG_METAL: Llama3 accuracy tests for $llama_dir completed"
