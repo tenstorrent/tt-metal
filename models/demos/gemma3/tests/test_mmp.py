efa17d11c7a3099c09209da24cba132c53681454
mstojko <mstojko@tenstorrent.com>
2025-10-04 18:47:30 +0000

    another for running

--- models/demos/gemma3/tests/test_mmp.py
+++ models/demos/gemma3/tests/test_mmp.py
@@ -37,6 +37,8 @@ def test_multi_modal_inference(seq_len, batch_size, reset_seeds, device, model_l
     dtype = ttnn.bfloat16
     mode = "decode" if seq_len <= 32 else "prefill"
 
+    logger.info("================================= stojkooooo - " + os.getenv("TT_GH_CI_INFRA"))
+
     tt_model_args = ModelArgs(
         device,
         model_location_generator=model_location_generator,
