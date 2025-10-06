e0c85ff229ede779e1ffab6ca8df7e83052c32e8
Patrick Roberts <proberts@tenstorrent.com>
2025-10-06 22:54:56 +0000

    Added Python bindings for Expression types

---
+++ ttnn/cpp/ttnn/operations/eltwise/fused/materialize_pybind.cpp
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "materialize_pybind.hpp"
+
+#include <pybind11/pybind11.h>
+
+namespace ttnn::operations::fused {
+
+void py_module(py::module& module) {}
+
+}  // namespace ttnn::operations::fused
