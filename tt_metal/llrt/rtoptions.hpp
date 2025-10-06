332f3ccb966c46664f767f5aa227fa008facf383
Bryan Wilder Field Lozano <blozano@tenstorrent.com>
2025-10-06 16:05:04 -0700

    Remove `library_tweaks.py` (#29936)
    ### Ticket
    #13726 
    
    ### Problem description
    In our codebase, there is a file called `library_tweaks.py` that
    introduces a workaround: it sets the `TT_METAL_HOME` environment
    variable at runtime on behalf of the user.
    
    This approach is problematic. Environment variables are intended to be
    fixed, process-level inputs supplied by the user or execution
    environment, not values modified unexpectedly during program execution.
    Allowing a process to override them dynamically increases the risk of
    confusion and inconsistent behavior.
    
    The issue is compounded by the fact that many developers are unaware of
    this workaround. As a result, they may set `TT_METAL_HOME` themselves,
    leading to subtle and difficult-to-debug runtime issues when the
    variable is redefined or conflicts arise.
    
    ### What's changed
    Add an API to to explicitly set the root directory.
    - `ttnn.set_root_dir()` at python level
    - `SetRootDir` at tt_metal c++ level
    
    This new API can be called automatically from python upon module import.
    
    `MetalContext::instance()` - is now `MetalContext::instance(root_dir =
    "")`.
    - The Metal Context is composed of members that have initialization time
    interdependencies.
    - This PR tries to provide external write access to
    MetalContext::rtoptions_
    - However, because rtoptions_ is used to construct dependent objects,
    the root_dir string must be passed into MetalContext constructor.
    - Any alternative, would be much more invasive ... i.e. breaking the
    init time dependencies between MetalContext class members.
    
    ### Impact 
    `TT_METAL_HOME` will not be required in ANY WAY for packaged python
    flows.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/18236303790)
    CI passes
    - [x] [RERUN AFTER CHANGE TO
    GLOBAL](https://github.com/tenstorrent/tt-metal/actions/runs/18289856507)
    - [x] New/Existing tests provide coverage for changes

--- tt_metal/llrt/rtoptions.hpp
+++ tt_metal/llrt/rtoptions.hpp
@@ -31,6 +31,9 @@ namespace tt {
 
 namespace llrt {
 
+inline std::string g_root_dir;
+inline std::once_flag g_root_once;
+
 // Enumerates the debug features that can be enabled at runtime. These features allow for
 // fine-grained control over targeted cores, chips, harts, etc.
 enum RunTimeDebugFeatures {
@@ -92,7 +95,7 @@ struct InspectorSettings {
 };
 
 class RunTimeOptions {
-    bool is_root_dir_env_var_set = false;
+    bool is_root_dir_set = false;
     std::string root_dir;
 
     bool is_cache_dir_env_var_set = false;
@@ -219,7 +222,8 @@ public:
     RunTimeOptions(const RunTimeOptions&) = delete;
     RunTimeOptions& operator=(const RunTimeOptions&) = delete;
 
-    bool is_root_dir_specified() const { return this->is_root_dir_env_var_set; }
+    bool is_root_dir_specified() const { return this->is_root_dir_set; }
+    static void set_root_dir(const std::string& root_dir);
     const std::string& get_root_dir() const;
 
     bool is_cache_dir_specified() const { return this->is_cache_dir_env_var_set; }
