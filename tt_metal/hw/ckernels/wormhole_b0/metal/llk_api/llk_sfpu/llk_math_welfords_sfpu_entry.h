585c527a3be46bb570d8450c9147859aa0fe491f
Nikhil Soraba <nsoraba@tenstorrent.com>
2025-10-06 20:41:27 +0000

    Reduce the first loop of DRAM reads

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_welfords_sfpu_entry.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_welfords_sfpu_entry.h
@@ -12,13 +12,31 @@ namespace ckernel {
 
 inline void llk_math_welfords_sfpu_init() { _llk_math_welfords_sfpu_init_(); }
 
-inline void llk_math_welfords_sfpu_clear_previous_mean_and_m2() { ckernel::sfpu::_clear_previous_mean_and_m2_(); }
-
-template <uint32_t reciprocal_size>
-inline void llk_math_welfords_sfpu_calculate_welfords_tile_(
-    uint32_t input_dst_idx, uint32_t start_idx, const std::array<uint32_t, reciprocal_size>& reciprocal_lut) {
+template <
+    uint32_t input_dst_index,
+    uint32_t mean_dst_index,
+    uint32_t m2_dst_index,
+    bool reformat_dst_to_col_on_end,
+    bool convert_M2_to_var_on_end,
+    uint32_t reciprocal_size>
+inline void llk_math_welfords_sfpu(
+    uint32_t current_row,
+    uint32_t final_row,
+    uint32_t num_skip_rows,
+    uint32_t group_id,
+    const std::array<uint32_t, reciprocal_size>& reciprocal_lut) {
     _llk_math_welfords_sfpu_params_(
-        ckernel::sfpu::_calculate_welfords_tile_<reciprocal_size>, input_dst_idx, start_idx, reciprocal_lut);
+        ckernel::sfpu::_calculate_welfords_online_<reciprocal_size>,
+        input_dst_index,
+        mean_dst_index,
+        m2_dst_index,
+        current_row,
+        final_row,
+        num_skip_rows,
+        group_id,
+        reciprocal_lut,
+        reformat_dst_to_col_on_end,
+        convert_M2_to_var_on_end);
 }
 
 template <uint32_t reciprocal_size>
