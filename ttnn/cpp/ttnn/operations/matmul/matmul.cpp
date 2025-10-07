48ba81d0792484d2ec066628a1f028fec21e7872
Edwin Lee <edwinlee@tenstorrent.com>
2025-10-07 17:29:40 -0400

    Matmul Activations - Generalise Unfused Activation for all Unary Ops (#30149)
    ## Ticket
    #29886 
    
    ### Problem description
    When using the matmul activation parameter (i.e. not the
    fused_activation parameter of matmul program configs), bound_matmul only
    explicitly supported relu, gelu, and silu. Other activations, such as
    relu6, resulted in an error.
    
    ### What's changed
    Changed the activation functionality so it invokes the implementation
    associated with the Unary op, rather than manually specifying a call to
    ttnn:[activation].
    
    Added relu6 to the linear unit tests, demonstrating the expanded
    activation functionality.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI
    [passes](https://github.com/tenstorrent/tt-metal/actions/runs/18318667616)
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests
    [passes](https://github.com/tenstorrent/tt-metal/actions/runs/18318673570)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-quick.yaml)
    CI passes (if applicable)
    - [ ] [Galaxy demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/matmul/matmul.cpp
+++ ttnn/cpp/ttnn/operations/matmul/matmul.cpp
@@ -157,15 +157,11 @@ ttnn::Tensor bound_matmul(
 
     if (parameters.user_fused_activation.has_value() && !has_user_grid) {
         const UnaryOpType& op_type = parameters.user_fused_activation.value().op_type;
-        if (op_type == UnaryOpType::RELU) {
-            output_tensor = ttnn::relu(output_tensor, parameters.output_mem_config, optional_output_tensor);
-        } else if (op_type == UnaryOpType::GELU) {
-            output_tensor = ttnn::gelu(output_tensor, false, parameters.output_mem_config, optional_output_tensor);
-        } else if (op_type == UnaryOpType::SILU) {
-            output_tensor = ttnn::silu(output_tensor, parameters.output_mem_config, optional_output_tensor);
-        } else {
-            TT_THROW("ttnn.matmul: Unsupported activation function");
-        }
+        output_tensor = ttnn::operations::unary::Unary_chain::invoke(
+            output_tensor,
+            {ttnn::operations::unary::EltwiseUnaryWithParam{op_type}},
+            parameters.output_mem_config,
+            optional_output_tensor);
     }
 
     return output_tensor;
