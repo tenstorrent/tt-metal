b20250ec6b7e4722693187bc9660a351c71b63c7
Ryan Miller <rmiller@tenstorrent.com>
2025-10-07 17:44:16 +0000

    Kernel cleanup and documentation

--- ttnn/cpp/ttnn/operations/normalization/layernorm/device/kernels/compute/layernorm_sharded_welford.cpp
+++ ttnn/cpp/ttnn/operations/normalization/layernorm/device/kernels/compute/layernorm_sharded_welford.cpp
@@ -15,10 +15,58 @@
 #include "compute_kernel_api/welford.h"
 #include "ttnn/operations/normalization/kernel_util/compute/combine_welford.hpp"
 
-// SPLIT REDUCE across Cores
+/**
+ * @brief This kernel computes layernorm for sharded tensors using
+ *        Welford's algorithm for the mean and variance calculations
+ *
+ * @details Computes layernorm(x) = (x - E[x]) / sqrt(Var[x] + eps) * gamma + beta
+ *
+ * There are two flavors of sharded layernorm. The details
+ * here are for row-major tensors, but the logic for column-major
+ * is the same (core/tensor rows are replaced by core/tensor columns):
+ * 1. Single-stage reduce:
+ *   - Each core gets a width slice (of size `block_wt` tiles) of
+ *     one or more rows of the tensor (the number of rows each core
+ *     is assigned is `num_tiles_per_allgather_worker`)
+ *   - Each core computes its partial mean and variance of its slices
+ *     for all rows it is assigned (using Welford's algorithm) and pushes
+ *     the interleaved mean and variance results to `cb_ex_partial`.
+ *     This produces 1 mean tile and 1 variance tile per tile row
+ *   - The reader kernels populate `cb_ex_external` with the core's
+ *     partial result + the other partial results from cores in the
+ *     same core row for each row the core is assigned
+ *   - Each core combines all partial results for its row(s) in
+ *     `cb_ex_external` into `cb_ex`. `cb_ex` contains 1 mean tile
+ *     followed by 1 tile of 1/sqrt(var + eps) for each assigned row
+ *   - The core row's sender core (the first column of cores) collects
+ *     all of these combined tiles and multicasts to all cores in the row
+ *     into `cb_ex_global`
+ * 2. Two-stage reduce:
+ *   - Used for width-sharded tensors, where each core has a
+ *     tensor-height-tall slice of the tensor
+ *   - The top row of cores are designated as "second-stage readers"
+ *   - As in single-stage reduce, each core in a row computes
+ *     its partial mean and variance for each of its assigned rows
+ *     for its width shard then combines it with the other partials
+ *     in its core row. This is the first stage of the reduce.
+ *   - Second stage of the reduce: The reader kernels add the
+ *     first stage's combined results into the second stage readers'
+ *     `cb_ex_external`. The results in `cb_ex_external` are combined
+ *     in the same way.
+ *   - The final combined results are collected by the sender core
+ *     and multicasted to all cores into `cb_ex_global`
+ *
+ * After one of the two reduce paths above, the rest of the layernorm
+ * calculation is done using the global mean and 1/sqrt(var + eps) results.
+ *
+ * @note Depending on the tensor and core grid shape, some cores may
+ *       not participate in the reduction. These cores just receive the
+ *       multicasted global results and perform the rest of layernorm
+ *       for their row(s).
+ */
 namespace NAMESPACE {
 namespace {
-// Get the set size of the next block in the Welford combine.
+// Get the set size of the next block in the Welford combine
 inline auto get_next_set_size(
     const uint32_t block,
     const uint32_t num_blocks_combine,
@@ -45,6 +93,13 @@ inline auto get_next_set_size(
 }
 }  // namespace
 void MAIN {
+    // ============================================================================
+    // Kernel setup
+    // ============================================================================
+
+    // ---------------------------------------------------------------------------
+    // Compile-time arguments
+    // ---------------------------------------------------------------------------
     constexpr uint32_t is_top_row = get_compile_time_arg_val(0);
     constexpr uint32_t do_gamma = get_compile_time_arg_val(1);
     constexpr uint32_t do_beta = get_compile_time_arg_val(2);
@@ -66,6 +121,36 @@ void MAIN {
     constexpr uint32_t W = get_compile_time_arg_val(16);
     constexpr uint32_t eps = get_compile_time_arg_val(17);
 
+    // ---------------------------------------------------------------------------
+    // CB definitions
+    // ---------------------------------------------------------------------------
+    constexpr uint32_t cb_in0 = tt::CBIndex::c_0;
+    constexpr uint32_t cb_in1 = tt::CBIndex::c_1;
+    constexpr uint32_t cb_eps = tt::CBIndex::c_3;
+    constexpr uint32_t cb_gamma = tt::CBIndex::c_5;
+    constexpr uint32_t cb_beta = tt::CBIndex::c_6;
+    constexpr uint32_t cb_x = tt::CBIndex::c_24;          // x minus mean
+    constexpr uint32_t cb_xmm = tt::CBIndex::c_18;        // x minus mean
+    constexpr uint32_t cb_ex_partial = tt::CBIndex::c_8;  // Interleaved E[x] and Var[x] partial results
+    constexpr uint32_t cb_ex = tt::CBIndex::c_9;          // Interleaved E[x] and Var[x] global reduce
+    constexpr uint32_t cb_ex_external = tt::CBIndex::c_10;
+    constexpr uint32_t cb_ex_global = tt::CBIndex::c_15;  // Interleaved E[x] and Var[x] final global mcast result
+    constexpr uint32_t cb_transpose = tt::CBIndex::c_22;  // Transpose interleaved E[x] and Var[x] to columns
+                                                          // (workaround for bug in transpose_wh_dest)
+    constexpr uint32_t cb_fusion = tt::CBIndex::c_18;     // stream gamma/beta
+    constexpr uint32_t cb_out = tt::CBIndex::c_16;
+
+    constexpr uint32_t cb_im = (do_gamma | do_beta) ? cb_x : cb_out;
+    constexpr uint32_t cb_outgamma = do_beta ? cb_fusion : cb_out;
+#ifdef FUSE_PRE_ADD
+    constexpr uint32_t cb_in = cb_x;
+#else
+    constexpr uint32_t cb_in = cb_in0;
+#endif
+
+    // ---------------------------------------------------------------------------
+    // Derived quantities
+    // ---------------------------------------------------------------------------
     // set block_ht to volatile to disable automatically unroll of the loops, avoid code overflow
     const uint32_t block_ht = (block_wt == 1) ? block_ht_volatile : block_ht_const;
     const uint32_t subblock_wt = (block_wt <= 2) ? subblock_wt_volatile : subblock_wt_const;
@@ -107,38 +192,26 @@ void MAIN {
     constexpr uint32_t welford_mean_dst = 1;
     constexpr uint32_t welford_var_dst = 2;
 
-    constexpr uint32_t cb_in0 = tt::CBIndex::c_0;
-    constexpr uint32_t cb_in1 = tt::CBIndex::c_1;
-    constexpr uint32_t cb_eps = tt::CBIndex::c_3;
-    constexpr uint32_t cb_gamma = tt::CBIndex::c_5;
-    constexpr uint32_t cb_beta = tt::CBIndex::c_6;
-    constexpr uint32_t cb_x = tt::CBIndex::c_24;          // x minus mean
-    constexpr uint32_t cb_xmm = tt::CBIndex::c_18;        // x minus mean
-    constexpr uint32_t cb_ex_partial = tt::CBIndex::c_8;  // Interleaved E[x] and Var[x] partial results
-    constexpr uint32_t cb_ex = tt::CBIndex::c_9;          // Interleaved E[x] and Var[x] global reduce
-    constexpr uint32_t cb_ex_external = tt::CBIndex::c_10;
-    constexpr uint32_t cb_ex_global = tt::CBIndex::c_15;  // Interleaved E[x] and Var[x] final global mcast result
-    constexpr uint32_t cb_transpose = tt::CBIndex::c_22;  // Transpose interleaved E[x] and Var[x] to columns
-                                                          // (workaround for bug in transpose_wh_dest)
-    constexpr uint32_t cb_fusion = tt::CBIndex::c_18;     // stream gamma/beta
-    constexpr uint32_t cb_out = tt::CBIndex::c_16;
-
     int index_subblock_w_offset = 0;
     int index_h_offset = 0;
     int index = 0;
 
+    // ============================================================================
+    // Main kernel logic
+    // ============================================================================
+
+    // ---------------------------------------------------------------------------
+    // Op initialization
+    // ---------------------------------------------------------------------------
 #ifdef FUSE_PRE_ADD
-    constexpr uint32_t cb_in = cb_x;
     binary_op_init_common(cb_in0, cb_in1, cb_in);
-    // pack_reconfig_data_format(cb_in);
 #else
-    constexpr uint32_t cb_in = cb_in0;
     unary_op_init_common(cb_in, cb_ex_partial);
 #endif
-    constexpr uint32_t cb_im = (do_gamma | do_beta) ? cb_x : cb_out;
-    constexpr uint32_t cb_outgamma = do_beta ? cb_fusion : cb_out;
 
-// pre-add x + y
+    // ---------------------------------------------------------------------------
+    // Pre-add x + y
+    // ---------------------------------------------------------------------------
 #ifdef FUSE_PRE_ADD
     reconfig_data_format_srcb(cb_in0, cb_in1);
     add_tiles_init(cb_in0, cb_in1);
@@ -163,14 +236,13 @@ void MAIN {
     }
     cb_push_back(cb_in, num_tiles_per_block);
     cb_wait_front(cb_in, num_tiles_per_block);
-#endif  // FUSE_PRE_ADD
+#endif
 
+    // ---------------------------------------------------------------------------
     // Compute E[x] and Var[x] using Welford's algorithm
-    const uint32_t num_partial_tiles = num_block_ht_result_tiles;
-
+    // ---------------------------------------------------------------------------
     reconfig_data_format_srca(cb_in);
-
-    cb_reserve_back(cb_ex_partial, num_partial_tiles);
+    cb_reserve_back(cb_ex_partial, num_block_ht_result_tiles);
     transpose_wh_init_short(cb_in);
     welford_init();
     index_h_offset = 0;
@@ -191,16 +263,17 @@ void MAIN {
         tile_regs_release();
         index_h_offset += block_wt;
     }
-    cb_push_back(cb_ex_partial, num_partial_tiles);
-    cb_wait_front(cb_ex_partial, num_partial_tiles);
-    // tt::compute::common::print_full_tile(cb_ex_partial, 0, true);
-    // tt::compute::common::print_full_tile(cb_ex_partial, 1, true);
+    cb_push_back(cb_ex_partial, num_block_ht_result_tiles);
+    cb_wait_front(cb_ex_partial, num_block_ht_result_tiles);
 
-    reconfig_data_format_srca(cb_ex_partial);
-
+    // ---------------------------------------------------------------------------
     // Combine Welford local partials with external partials
     // cb_ex <-- cb_ex_external, cb_ex_partial
-    // where "ex" is mean and var interleaved.
+    // If reduction is single-stage, or this core is a second-stage reader,
+    // then cb_ex contains mean and 1/sqrt(var + eps) interleaved.
+    // Otherwise, cb_ex contains mean and var interleaved.
+    // ---------------------------------------------------------------------------
+    reconfig_data_format_srca(cb_ex_partial);
     if constexpr (is_allgather_worker) {
         cb_reserve_back(cb_ex, 2 * num_tiles_per_allgather_worker);
         for (uint32_t i = 0; i < num_tiles_per_allgather_worker; i++) {
@@ -224,6 +297,9 @@ void MAIN {
         cb_wait_front(cb_ex, 2 * num_tiles_per_allgather_worker);
     }
 
+    // ---------------------------------------------------------------------------
+    // Receive the global reduce result and transpose back to columns
+    // ---------------------------------------------------------------------------
     cb_wait_front(cb_ex_global, num_block_ht_result_tiles);
     cb_reserve_back(cb_transpose, num_block_ht_result_tiles);
     transpose_wh_init(cb_ex_global, cb_transpose);
@@ -247,28 +323,21 @@ void MAIN {
 
     cb_wait_front(cb_transpose, num_block_ht_result_tiles);
 
-    // Compute (x - E[x])
-    // Pack to cb_xmm
+    // ---------------------------------------------------------------------------
+    // Compute x - E[x]
+    // ---------------------------------------------------------------------------
     if constexpr (FLOAT32_DTYPE) {
         reconfig_data_format(cb_in, cb_transpose);
     }
     index_h_offset = 0;
     sub_bcast_cols_init_short(cb_in, cb_transpose);
     cb_reserve_back(cb_xmm, num_tiles_per_block);
-
-    // See above notes about keeping mean/var as rows
-    // and about needing a full tranpose init
-    // transpose_wh_init(cb_ex_global, cb_ex_global);
     for (uint32_t i = 0; i < block_ht; i++) {
         index_subblock_w_offset = 0;
         const auto mean_idx = 2 * i;
         cb_wait_front(cb_transpose, mean_idx + 1);
-        // DPRINT << "mean tile:" << ENDL();
-        // tt::compute::common::print_full_tile(cb_transpose, mean_idx, true);
         for (uint32_t j = 0; j < num_subblocks_w; j++) {
             tile_regs_acquire();
-            // // Transpose back to columns
-            // transpose_wh_tile(cb_ex_global, mean_idx, cb_ex_global);
             for (uint32_t w = 0; w < subblock_wt; w++) {
                 index = w + index_subblock_w_offset;
                 sub_tiles_bcast_cols(cb_in, cb_transpose, index, mean_idx, w);
@@ -289,19 +358,14 @@ void MAIN {
     reconfig_data_format_srca(cb_in, cb_xmm);
 #endif
     cb_wait_front(cb_xmm, num_tiles_per_block);
-    // DPRINT << "xmm after packing" << ENDL();
-    // uint32_t offset = 0;
-    // for (uint32_t i = 0; i < block_ht; i++) {
-    //     tt::compute::common::print_full_tile(cb_xmm, offset, true);
-    //     offset += block_wt;
-    // }
 
     if constexpr (do_gamma == 0 && do_beta == 0) {
         pack_reconfig_data_format(cb_out);
     }
 
-    // (x - Ex) * 1/[sqrt(Var + eps)]
-    // Pack to cb_im
+    // ---------------------------------------------------------------------------
+    // Scale by 1/sqrt(Var[x] + eps)
+    // ---------------------------------------------------------------------------
     if constexpr (FLOAT32_DTYPE) {
         reconfig_data_format(cb_xmm, cb_transpose);
     }
@@ -311,33 +375,29 @@ void MAIN {
     for (uint32_t i = 0; i < block_ht; i++) {
         index_subblock_w_offset = 0;
         for (uint32_t j = 0; j < num_subblocks_w; j++) {
-            // if (j == 0) {
-            //     tt::compute::common::print_full_tile(cb_xmm, index_h_offset, true);
-            //     tt::compute::common::print_full_tile(cb_transpose, 1, true);
-            // }
             tile_regs_acquire();
             for (uint32_t w = 0; w < subblock_wt; w++) {
                 index = w + index_subblock_w_offset + index_h_offset;
                 mul_tiles_bcast_cols(cb_xmm, cb_transpose, index, /*1/sqrt(var+eps) idx*/ 1, w);
             }
             tile_regs_commit();
-
             tile_regs_wait();
             for (uint32_t i = 0; i < subblock_wt; i++) {
                 pack_tile(i, cb_im);
             }
             tile_regs_release();
-
             index_subblock_w_offset += subblock_wt;
         }
         index_h_offset += block_wt;
         cb_pop_front(cb_transpose, 2);
     }
     cb_push_back(cb_im, num_tiles_per_block);
-
     cb_pop_front(cb_xmm, num_tiles_per_block);
-    cb_wait_front(cb_im, num_tiles_per_block);
 
+    // ---------------------------------------------------------------------------
+    // Scale by gamma
+    // ---------------------------------------------------------------------------
+    cb_wait_front(cb_im, num_tiles_per_block);
     if constexpr (do_gamma) {
         reconfig_data_format(cb_im, cb_gamma);
         if constexpr (do_beta == 0) {
@@ -370,6 +430,9 @@ void MAIN {
         cb_wait_front(cb_outgamma, num_tiles_per_block);
     }
 
+    // ---------------------------------------------------------------------------
+    // Add beta
+    // ---------------------------------------------------------------------------
     if constexpr (do_beta) {
         reconfig_data_format(cb_fusion, cb_beta);
         pack_reconfig_data_format(cb_out);
