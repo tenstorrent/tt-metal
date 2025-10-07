a14ae2be32d13087eb691f22eb9f395df47d437b
Ryan Miller <rmiller@tenstorrent.com>
2025-10-07 19:54:27 +0000

    Docstring for function

--- ttnn/cpp/ttnn/operations/normalization/kernel_util/compute/combine_welford.h
+++ ttnn/cpp/ttnn/operations/normalization/kernel_util/compute/combine_welford.h
@@ -22,7 +22,13 @@
 
 namespace norm::kernel_util::compute {
 namespace detail {
-// C++17 compatible bit_cast replacement using union
+/**
+ * @brief C++17 compatible bit_cast replacement using union
+ * @tparam To The type to cast to
+ * @tparam From The type to cast from
+ * @param from The value to cast from
+ * @return The casted value
+ */
 template <typename To, typename From>
 inline To bit_cast(const From& from) noexcept {
     static_assert(sizeof(To) == sizeof(From), "Types must have same size");
