299f65093a11aa7ef9b789b3eff916ec1206460c
Kannika Kabilar <kkabilar@tenstorrent.com>
2025-10-07 16:37:03 -0500

    add separate functions for l2nightly and apc

--- tests/scripts/run_python_model_tests.sh
+++ tests/scripts/run_python_model_tests.sh
@@ -27,6 +27,68 @@ run_python_model_tests_grayskull() {
     pytest models/demos/falcon7b_common/tests/unit_tests/test_falcon_attn_matmul.py
 }
 
+run_python_model_tests_apc_wormhole_b0() {
+    # Yolov4
+    pytest models/demos/yolov4/tests/pcc/test_ttnn_yolov4.py -k "pretrained_weight_false"
+
+    # Yolov8
+    pytest models/demos/yolov8x/tests/pcc/test_yolov8x.py::test_yolov8x_640
+
+    # Yolov8s
+    pytest models/demos/yolov8s/tests/pcc/test_yolov8s.py::test_yolov8s_640
+
+}
+
+run_python_model_tests_l2nightly_wormhole_b0() {
+    # Falcon tests
+    # attn_matmul_from_cache is currently not used in falcon7b
+    pytest models/demos/falcon7b_common/tests/unit_tests/test_falcon_attn_matmul.py -k "not attn_matmul_from_cache"
+    # higher sequence lengths and different formats trigger memory issues
+    pytest models/demos/falcon7b_common/tests/unit_tests/test_falcon_matmuls_and_bmms_with_mixed_precision.py -k "seq_len_128 and in0_BFLOAT16-in1_BFLOAT8_B-out_BFLOAT16-weights_DRAM"
+    pytest models/demos/wormhole/resnet50/tests/test_resnet50_functional.py -k "pretrained_weight_false"
+
+    # Unet Shallow
+    pytest -svv models/experimental/functional_unet/tests/test_unet_model.py
+
+    # Mamba
+    pytest -svv models/demos/wormhole/mamba/tests/test_residual_block.py -k "pretrained_weight_false"
+
+    # Mobilenetv2git
+    pytest -svv models/demos/mobilenetv2/tests/pcc/test_mobilenetv2.py
+
+    #Yolov10
+    pytest -svv models/demos/yolov10x/tests/pcc/test_ttnn_yolov10x.py::test_yolov10x
+
+    #Yolov7
+    pytest -svv models/demos/yolov7/tests/pcc/test_ttnn_yolov7.py
+
+    # ViT-base
+    pytest -svv models/demos/vit/tests/pcc/test_ttnn_optimized_sharded_vit_wh.py
+
+
+    # Llama3.1-8B
+    llama8b=/mnt/MLPerf/tt_dnn-models/llama/Meta-Llama-3.1-8B-Instruct/
+    # Llama3.2-1B
+    llama1b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.2-1B-Instruct/
+    # Llama3.2-3B
+    llama3b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.2-3B-Instruct/
+    # Llama3.2-11B
+    llama11b=/mnt/MLPerf/tt_dnn-models/llama/Llama3.2-11B-Vision-Instruct/
+
+    # Run all Llama3 tests for 8B, 1B, and 3B weights - dummy weights with tight PCC check
+    for llama_dir in  "$llama1b" "$llama3b" "$llama8b" "$llama11b"; do
+        LLAMA_DIR=$llama_dir pytest models/tt_transformers/tests/test_model.py -k "quick" ; fail+=$?
+        echo "LOG_METAL: Llama3 tests for $llama_dir completed"
+    done
+
+    # Mistral-7B-v0.3
+    mistral_weights=mistralai/Mistral-7B-Instruct-v0.3
+    HF_MODEL=$mistral_weights pytest models/tt_transformers/tests/test_model.py -k "quick" ; fail+=$?
+
+    run_python_model_tests_slow_runtime_mode_wormhole_b0()
+
+}
+
 run_python_model_tests_wormhole_b0() {
     # Falcon tests
     # attn_matmul_from_cache is currently not used in falcon7b
