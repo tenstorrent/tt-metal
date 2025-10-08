a4562417e2219010d76d5b68c65d69fb270b69ce
Miguel Tairum <150826086+mtairum@users.noreply.github.com>
2025-10-08 21:16:58 +0100

    [Llama-3.3-70B Galaxy] Fix CCL buffer/semaphore initialization causing race conditions (#30157)
    https://github.com/tenstorrent/tt-metal/issues/28472
    
    Initial "dirty fix for the race condition introduced by the staggered
    initialization of CCL buffers and semaphores.
    The issue was caused by initializing first the prefill buffers and
    semaphores, then execute the prefill forward pass and only after that
    initialize the decode buffers and semaphores.
    
    This PR fixes the bad accuracy caused by race conditions. We've tested
    this both in TT-Metal and with vLLM server.
    There is still a separate issue on the vLLM side where a batch of 32
    users, all with the same prompt, might see users in groups of 8
    generating output deviations. I.e. First group of 8 users, all matching
    output. Second group of 8 users, all matching between them, but
    different than first group, and so on.
    Will open an issue to start debugging that one.
    
    - Ensures that all buffers/semaphores for CCLs and prefetcher
    initialization is done during model creation, for both prefill and
    decode
    - Fixes 2 persistent buffers on prefill side
    - Updates semaphore logic on prefill side
    - Cleanup of unnecessary prefill forward pass when capturing prefill
    trace
    
    - I will follow this PR with a cleaner, more thorough update of the
    Llama CCL logic. We have some legacy code in there that can be removed
    and we should initialize both the prefill and decode buffers with a
    single call, instead of the current patch of initializing prefill ->
    switching mode -> initializing decode -> switch mode.
    
    - [x] [Galaxy
    APC](https://github.com/tenstorrent/tt-metal/actions/runs/18338349334)
    - :x: [Galaxy
    model-pr](https://github.com/tenstorrent/tt-metal/actions/runs/18342121390)
    (Did some fixes re-running below)
    - [x] [Galaxy quick
    New](https://github.com/tenstorrent/tt-metal/actions/runs/18345743000)
    - [x] [Galaxy frequent
    new](https://github.com/tenstorrent/tt-metal/actions/runs/18345775102)
    (llama passed, stopped the workflow to avoid running the other tests and
    slow down CI)
    - [x] [Galaxy demo
    new](https://github.com/tenstorrent/tt-metal/actions/runs/18347179089)

--- models/demos/llama3_70b_galaxy/tt/llama_model.py
+++ models/demos/llama3_70b_galaxy/tt/llama_model.py
@@ -33,6 +33,7 @@ class TtTransformer(LightweightModule):
         enable_prefetcher_performance_mode=False,
         mode="decode",
         allocate_prefill_buffers=True,
+        decode_mode_only=False,
     ):
         super().__init__()
         self.args = args
@@ -47,6 +48,7 @@ class TtTransformer(LightweightModule):
         state_dict_prefix = args.get_state_dict_prefix("", None)
         self.allocate_prefill_buffers = allocate_prefill_buffers
         self.paged_attention_config = paged_attention_config
+        self.decode_mode_only = decode_mode_only
 
         self.embd = TtLlamaEmbedding(
             mesh_device=mesh_device,
@@ -73,12 +75,9 @@ class TtTransformer(LightweightModule):
         self.mesh_sub_device_manager_id_decode = None
         self.mesh_sub_device_manager_id_prefill = None
 
-        if mode == "decode":
-            self.setup_decode()
-            self.is_decode_setup = True
-        else:
-            self.setup_prefill()
-            self.is_prefill_setup = True
+        # First initialization of decode CCLs and prefetcher
+        self.setup_decode()
+        self.is_decode_setup = True
 
         self.layers = [
             TtTransformerBlock(
@@ -129,6 +128,13 @@ class TtTransformer(LightweightModule):
             tt_ccl=self.tt_ccl,
             prefetcher_setup=self.prefetcher_setup,
         )
+        if not self.decode_mode_only:  # demo_decode.py uses decode mode only. In this case avoid initializing prefill
+            # First initialization of prefill CCLs and prefetcher. It needs to be after initialization of layers, norm and lm_head since those switch modes as well
+            # This initialization is required to avoid race condition due to all buffers and semaphores not being allocated at initialization
+            self.switch_mode("prefill")
+            self.setup_prefill()
+            self.is_prefill_setup = True
+
         if mode == "decode":
             self.tt_tensors = self.prefetcher_setup.get_input_tensors()
         self.tt_rot_mats_prefill = None
