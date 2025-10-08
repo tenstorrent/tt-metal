4f730fcb12205f286ab6c79aed23c85a24f9fc9e
Ryan Miller <rmiller@tenstorrent.com>
2025-10-08 14:36:14 +0000

    Clean up and document the sender kernel

--- ttnn/cpp/ttnn/operations/normalization/layernorm/device/kernels/dataflow/reader_mcast_sender_unary_sharded_ln.cpp
+++ ttnn/cpp/ttnn/operations/normalization/layernorm/device/kernels/dataflow/reader_mcast_sender_unary_sharded_ln.cpp
@@ -9,8 +9,33 @@
 
 namespace df = norm::layernorm::device::kernels::dataflow;
 
-// split REDUCE across cores
+/**
+ * @brief This kernel implements the sender (coordinator) logic for
+ *        the mean and variance calculations for the sharded layernorm
+ *        kernels
+ *
+ * @details The kernel's objective is to coordinate a distributed (sharded)
+ * mean and variance calculation, where shards are placed on other cores
+ * in the communication network.The responsibilities of this kernel are:
+ * 1. Do its partial mean/variance reduction for its assigned tiles
+ * 2. Coordinate the waiting on partial results from the other cores
+ * 3. Read partials from other cores to do its global combine
+ *    for its assigned tiles.
+ * 4. Coordinate the waiting on all global combined results to be ready
+ * 5. Collect all global combined results and multicast to all cores
+ *
+ * @note If the reduce is two-stage, the kernel additionally waits
+ *       on the combined results from the first stage and uses them
+ *       in its own combine
+ */
 void kernel_main() {
+    // ============================================================================
+    // Kernel setup
+    // ============================================================================
+
+    // ---------------------------------------------------------------------------
+    // Compile-time arguments
+    // ---------------------------------------------------------------------------
     uint32_t reduce_receiver_semaphore_addr = get_semaphore(get_compile_time_arg_val(0));
     uint32_t reduce_sender_semaphore_addr = get_semaphore(get_compile_time_arg_val(1));
     constexpr uint32_t num_blocks = get_compile_time_arg_val(2);
@@ -31,6 +56,9 @@ void kernel_main() {
     constexpr bool rms_norm = get_compile_time_arg_val(17) == 1;
     constexpr bool use_welford = get_compile_time_arg_val(18) == 1;
 
+    // ---------------------------------------------------------------------------
+    // Runtime arguments
+    // ---------------------------------------------------------------------------
     const uint32_t mcast_dest_noc_start_x = get_arg_val<uint32_t>(0);
     const uint32_t mcast_dest_noc_start_y = get_arg_val<uint32_t>(1);
     const uint32_t mcast_dest_noc_end_x = get_arg_val<uint32_t>(2);
@@ -41,6 +69,9 @@ void kernel_main() {
     df::L1Ptr in0_remote_noc_x = (df::L1Ptr)(get_arg_addr(6));
     df::L1Ptr in0_remote_noc_y = (df::L1Ptr)(get_arg_addr(6 + num_x));
 
+    // ---------------------------------------------------------------------------
+    // CB definitions
+    // ---------------------------------------------------------------------------
     constexpr uint32_t cb_ex_partial = tt::CBIndex::c_8;
     constexpr uint32_t cb_ex = tt::CBIndex::c_9;
     constexpr uint32_t cb_ex_external = tt::CBIndex::c_10;
@@ -50,9 +81,12 @@ void kernel_main() {
     constexpr uint32_t cb_ex2pe = tt::CBIndex::c_20;
     constexpr uint32_t cb_ex_global = tt::CBIndex::c_15;  // E[x] global reduce
 
+    // ---------------------------------------------------------------------------
+    // Set up constants for the kernel
+    // ---------------------------------------------------------------------------
     const uint32_t single_tile_size_bytes = get_tile_size(cb_ex_partial);
 
-    // Compute the NOC addresses for cores that interact with this core
+    // Compute the NOC addresses for remote cores that interact with this core
     df::RemoteNocAddrs<num_blocks> remote_noc_addrs{};
     df::compute_single_stage_noc_addrs<row_major, num_blocks>(
         remote_noc_addrs, in0_remote_noc_x, in0_remote_noc_y, start_x, start_y, num_x, num_y);
@@ -69,17 +103,29 @@ void kernel_main() {
     volatile tt_l1_ptr uint32_t* reduce_second_stage_semaphore_addr_ptr =
         reinterpret_cast<volatile tt_l1_ptr uint32_t*>(reduce_second_stage_semaphore_addr);
 
+    // ============================================================================
+    // Main kernel worker function
+    // Performs partial reduction for its assigned tiles, coordinates
+    // the waiting on partial and combined results for all cores, and performs
+    // the multicast of the final results to all cores
+    // ============================================================================
     const auto& global_reduce_sender = [&](const uint32_t cb_partial,
                                            const uint32_t cb_external,
                                            const uint32_t cb_ex,
                                            const uint32_t cb_ex_global,
                                            const uint32_t cb_reduce_first_stage,
                                            const uint32_t num_tiles_scaler) __attribute__((always_inline)) {
-        // global reduce
-        // wait for local data ready
+        // ============================================================================
+        // Partial reduction
+        // Partials stored `cb_partial`
+        // ============================================================================
+
+        // Wait for this core's partial results to be ready
         cb_wait_front(cb_partial, block_h * num_tiles_scaler);
 
-        // inc semaphore of other cores, tell other all-to-all workers to start
+        // Wait for partial results from the other cores to be ready.
+        // Once all cores finish partials, notify the cores to
+        // start reading remote data and doing their global combines
         if constexpr (num_blocks > 1) {
             *reduce_sender_semaphore_addr_ptr = VALID;
             noc_semaphore_wait(reduce_receiver_semaphore_addr_ptr, num_blocks - 1);
@@ -87,9 +133,21 @@ void kernel_main() {
             noc_semaphore_set_multicast(reduce_sender_semaphore_addr, reduce_sender_semaphore_noc_addr, num_blocks - 1);
         }
 
-        // read data from other cores - first stage reduce
+        // ============================================================================
+        // Combine partial results
+        // Read from the partial buffers into the external buffer `cb_external`.
+        // Will read a total of:
+        // (num_blocks_first_stage + num_blocks_second_stage - 1) * num_tiles_scaler
+        // tiles for each assigned tile row (or column, if not row-major).
+        // For the second stage, read from `cb_reduce_first_stage` instead of `cb_partial`,
+        // as it will contain the combined results from the first stage.
+        // Combined results written to `cb_ex`.
+        // ============================================================================
+
+        // ---------------------------------------------------------------------------
+        // Read remote partial data
+        // ---------------------------------------------------------------------------
         uint32_t l1_read_addr_ex_par = get_read_ptr(cb_partial);
-        // read data from other cores - second stage reduce
         uint32_t l1_read_addr_ex = 0;
         uint32_t block_index_stride = 0;
         if constexpr (use_two_stage_reduce) {
@@ -100,9 +158,8 @@ void kernel_main() {
                 block_index_stride = num_y;
             }
         }
-        // read from both stage
         for (uint32_t i = 0; i < num_tiles_per_worker; ++i) {
-            // first stage
+            // Read in the partials from the cores in the same row
             cb_reserve_back(cb_external, num_blocks_first_stage * num_tiles_scaler);
             uint32_t l1_write_addr_external = get_write_ptr(cb_external);
             for (uint32_t block = 0; block < num_blocks_first_stage; ++block) {
@@ -115,13 +172,17 @@ void kernel_main() {
             noc_async_read_barrier();
             cb_push_back(cb_external, num_blocks_first_stage * num_tiles_scaler);
 
-            // sync with second-stage all-to-all workers
+            // Handle the two-stage reduce
             if constexpr (use_two_stage_reduce) {
+                // Wait for the signal to read in the combined results from
+                // the first stage (from cores in our core column) are ready
                 if (i == 0) {
                     noc_semaphore_wait(reduce_second_stage_semaphore_addr_ptr, num_blocks_second_stage - 1);
                     noc_semaphore_set(reduce_second_stage_semaphore_addr_ptr, 0);
                 }
 
+                // Read in the combined results from the cores in our column
+                // into the external buffer
                 uint32_t curr_block_index = block_index_stride;
                 cb_reserve_back(cb_external, (num_blocks_second_stage - 1) * num_tiles_scaler);
                 for (uint32_t block = 0; block < num_blocks_second_stage - 1; ++block) {
@@ -137,17 +198,27 @@ void kernel_main() {
             }
         }
 
-        l1_read_addr_ex = get_read_ptr(cb_ex);
-        uint32_t l1_write_addr_ex_global = get_write_ptr(cb_ex_global);
+        // ---------------------------------------------------------------------------
+        // Wait for all final combined results to be ready
+        // ---------------------------------------------------------------------------
+
+        // Our results
         cb_wait_front(cb_ex, num_tiles_per_worker * num_tiles_scaler);
 
-        // sync with other all-to-all workers, on the same row
+        // Other cores' results
         if constexpr (num_all_to_all_workers_first_stage > 1) {
             noc_semaphore_wait(reduce_receiver_semaphore_addr_ptr, num_all_to_all_workers_first_stage - 1);
             noc_semaphore_set(reduce_receiver_semaphore_addr_ptr, 0);
         }
 
-        // gather data to top row
+        // ============================================================================
+        // Gather all final combined results and multicast to all cores.
+        // Read from `cb_ex` into `cb_ex_global`, multicast `cb_ex_global` to all cores
+        // ============================================================================
+
+        // Gather final results
+        l1_read_addr_ex = get_read_ptr(cb_ex);
+        uint32_t l1_write_addr_ex_global = get_write_ptr(cb_ex_global);
         cb_reserve_back(cb_ex_global, block_h * num_tiles_scaler);
         for (uint32_t block = 0; block < num_all_to_all_workers_first_stage; ++block) {
             uint64_t noc_addr_ex = remote_noc_addrs[block] | l1_read_addr_ex;
@@ -162,7 +233,7 @@ void kernel_main() {
         }
         noc_async_read_barrier();
 
-        // mcast
+        // Multicast
         uint32_t l1_read_addr_ex_global = get_read_ptr(cb_ex_global);
         cb_push_back(cb_ex_global, block_h * num_tiles_scaler);
         if constexpr (num_blocks > 1) {
