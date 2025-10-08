a97c575fd01efb131d849655492bdc92b9d1dc47
Marija Djuric <mdjuric@tenstorrent.com>
2025-10-08 13:02:42 +0000

    Hotfix: perf test fix

--- models/experimental/stable_diffusion_xl_refiner/tests/test_sdxl_refiner_perf.py
+++ models/experimental/stable_diffusion_xl_refiner/tests/test_sdxl_refiner_perf.py
@@ -43,15 +43,14 @@ def test_unet(
         is_ci_env,
         is_ci_v2_env,
         model_location_generator,
-        iterations=iterations,
     )
 
 
 @pytest.mark.models_device_performance_bare_metal
 def test_sdxl_refiner_perf_device():
-    expected_device_perf_cycles_per_iteration = 1_066_002_193
+    expected_device_perf_cycles_per_iteration = 1_059_996_057
 
-    command = f"pytest models/experimental/stable_diffusion_xl_refiner/tests/test_sdxl_perf.py::test_unet"
+    command = f"pytest models/experimental/stable_diffusion_xl_refiner/tests/test_sdxl_refiner_perf.py::test_unet"
     cols = ["DEVICE FW", "DEVICE KERNEL", "DEVICE BRISC KERNEL"]
 
     batch_size = 1
