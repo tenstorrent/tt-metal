4c6a00ccb9bfa9583aa5168a47d006ef9129a462
Bojan Rosko <brosko@tenstorrent.com>
2025-10-08 17:00:22 +0000

    test

--- tt_metal/lite_fabric/host_util.cpp
+++ tt_metal/lite_fabric/host_util.cpp
@@ -81,7 +81,9 @@ void SetResetState(tt::Cluster& cluster, tt_cxy_pair virtual_core, bool assert_r
     // We run on ERISC1. Don't touch ERISC0. It is running base firmware
     if (assert_reset) {
         // Assert all cores except ERISC0.
-        tt::umd::RiscType reset_val = tt::umd::RiscType::ALL & ~tt::umd::RiscType::ERISC0;
+        TensixSoftResetOptions reset_val = TENSIX_ASSERT_SOFT_RESET;
+        reset_val = reset_val & static_cast<TensixSoftResetOptions>(
+                                    ~std::underlying_type<TensixSoftResetOptions>::type(TensixSoftResetOptions::BRISC));
         cluster.assert_risc_reset_at_core(virtual_core, reset_val);
     } else {
         // Deassert only ERISC1.
