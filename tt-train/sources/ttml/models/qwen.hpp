138a485429afc492c0028e33d223b38f8c1d19e5
scorbunny-cinderace <ra.zhang@hl-it.cn>
2025-10-09 17:54:07 +0800

    change module base dir

--- tt-train/sources/ttml/models/qwen.hpp
+++ tt-train/sources/ttml/models/qwen.hpp
@@ -4,7 +4,7 @@
 
 #pragma once
 
-#include "autograd/module_base.hpp"
+#include "modules/module_base.hpp"
 #include "autograd/tensor.hpp"
 #include "base_transformer.hpp"
 #include "common/transformer_common.hpp"
@@ -41,10 +41,10 @@ class Qwen : public BaseTransformer {
 private:
     RunnerType runner_type = RunnerType::Default;
     QwenConfig m_config;
-    std::shared_ptr<ttml::autograd::ModuleBase> tok_emb;
+    std::shared_ptr<ttml::modules::ModuleBase> tok_emb;
     std::vector<std::shared_ptr<ModuleBase>> blocks;
     std::shared_ptr<ModuleBase> ln_fc;
-    std::shared_ptr<ttml::autograd::ModuleBase> fc;
+    std::shared_ptr<ttml::modules::ModuleBase> fc;
     ops::RotaryEmbeddingParams m_rope_params;
 
 public:
