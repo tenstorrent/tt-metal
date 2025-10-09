d0690b255374b2946c918b68ca12f03582871b87
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2025-10-09 00:30:59 +0000

    added a few more

--- tt-train/sources/ttml/nanobind/nb_core.cpp
+++ tt-train/sources/ttml/nanobind/nb_core.cpp
@@ -7,6 +7,8 @@
 #include <nanobind/nanobind.h>
 #include <nanobind/stl/unique_ptr.h>
 
+#include <ttnn/distributed/distributed_tensor.hpp>
+
 #include "serialization/serializable.hpp"
 
 // Make NamedParameters opaque - must be before unordered_map include
@@ -28,6 +30,7 @@ void py_module_types(nb::module_& m) {
     nb::class_<ttnn::distributed::TensorToMesh>(py_distributed, "TensorToMesh");
     // Expose MeshToTensor composer for composing distributed tensors back to single tensor
     nb::class_<ttnn::distributed::MeshToTensor>(py_distributed, "MeshToTensor");
+    nb::class_<ttnn::distributed::MeshComposerConfig>(py_distributed, "MeshComposerConfig");
 }
 
 void py_module(nb::module_& m) {
@@ -49,6 +52,22 @@ void py_module(nb::module_& m) {
             nb::arg("mesh_device"),
             nb::arg("dim"));
 
+        py_distributed.def(
+            "create_mesh_composer",
+            &ttnn::distributed::create_mesh_composer,
+            nb::arg("mesh_device"),
+            nb::arg("config"));
+        py_distributed.def(
+            "create_mesh_composer_config",
+            [](nb::list dims, nb::list override) -> ttnn::distributed::MeshComposerConfig {
+                ttsl::SmallVector<int> sdims;
+                ttsl::SmallVector<uint32_t> soverride;
+                for (nb::handle h : dims) sdims.push_back(nb::cast<int>(h));
+                for (nb::handle h : override) soverride.push_back(nb::cast<int>(h));
+                return ttnn::distributed::MeshComposerConfig(sdims, tt::tt_metal::distributed::MeshShape{soverride});
+            },
+            nb::arg("dims"),
+            nb::arg("mesh_shape_override"));
         // Synchronize gradients across devices for DDP
         py_distributed.def(
             "synchronize_parameters", &ttml::core::distributed::synchronize_parameters, nb::arg("parameters"));
