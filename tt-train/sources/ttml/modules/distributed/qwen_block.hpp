39f9e4f8d65b3fcb75f450e62c0ae68f764a9f40
scorbunny-cinderace <ra.zhang@hl-it.cn>
2025-10-09 17:51:30 +0800

    change module base dir

--- tt-train/sources/ttml/modules/distributed/qwen_block.hpp
+++ tt-train/sources/ttml/modules/distributed/qwen_block.hpp
@@ -7,25 +7,25 @@
 #include <cstdint>
 #include <optional>
 
-#include "autograd/module_base.hpp"
+#include "modules/module_base.hpp"
 #include "ops/rope_op.hpp"
 
 namespace ttml::modules::distributed {
 
-class DistributedQwenMLP : public autograd::ModuleBase {
+class DistributedQwenMLP : public ModuleBase {
 public:
     DistributedQwenMLP(
         uint32_t embedding_size, float dropout_prob, std::optional<uint32_t> intermediate_dim = std::nullopt);
     autograd::TensorPtr operator()(const autograd::TensorPtr& input) override;
 
 private:
-    std::shared_ptr<autograd::ModuleBase> m_gate_proj;
-    std::shared_ptr<autograd::ModuleBase> m_up_proj;
-    std::shared_ptr<autograd::ModuleBase> m_down_proj;
-    std::shared_ptr<autograd::ModuleBase> m_dropout;
+    std::shared_ptr<modules::ModuleBase> m_gate_proj;
+    std::shared_ptr<modules::ModuleBase> m_up_proj;
+    std::shared_ptr<modules::ModuleBase> m_down_proj;
+    std::shared_ptr<modules::ModuleBase> m_dropout;
 };
 
-class DistributedQwenBlock : public autograd::ModuleBase {
+class DistributedQwenBlock : public modules::ModuleBase {
 public:
     explicit DistributedQwenBlock(
         uint32_t embedding_size,
@@ -38,10 +38,10 @@ public:
     autograd::TensorPtr operator()(const autograd::TensorPtr& input, const autograd::TensorPtr& mask) override;
 
 private:
-    std::shared_ptr<autograd::ModuleBase> m_mlp;
-    std::shared_ptr<autograd::ModuleBase> m_input_layernorm;
-    std::shared_ptr<autograd::ModuleBase> m_post_attention_layernorm;
-    std::shared_ptr<autograd::ModuleBase> m_self_attn;
+    std::shared_ptr<modules::ModuleBase> m_mlp;
+    std::shared_ptr<modules::ModuleBase> m_input_layernorm;
+    std::shared_ptr<modules::ModuleBase> m_post_attention_layernorm;
+    std::shared_ptr<modules::ModuleBase> m_self_attn;
 };
 
 }  // namespace ttml::modules::distributed
