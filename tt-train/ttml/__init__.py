26fefd3cc937ce6062c27df33a7ac1d015fb06d6
Adam Thompson <athompson@tenstorrent.com>
2025-10-09 22:48:07 +0000

    give up on re-exec, force loading of built artifacts if editable

--- tt-train/ttml/__init__.py
+++ tt-train/ttml/__init__.py
@@ -2,48 +2,47 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-def is_editable_install(package_name):
-    import importlib.metadata
-    import json
+def _preload_if_editable():
+    def is_editable_install(package_name):
+        import importlib.metadata
+        import json
 
-    try:
-        # Get the distribution object for the package
-        dist = importlib.metadata.distribution(package_name)
+        try:
+            # Get the distribution object for the package
+            dist = importlib.metadata.distribution(package_name)
 
-        # Check for direct_url.json in the metadata
-        direct_url_json = dist.read_text("direct_url.json")
-        if direct_url_json:
-            data = json.loads(direct_url_json).get("dir_info")
-            # Look for the 'editable' key
-            return data and data.get("editable") or False
+            # Check for direct_url.json in the metadata
+            direct_url_json = dist.read_text("direct_url.json")
+            if direct_url_json:
+                data = json.loads(direct_url_json).get("dir_info")
+                # Look for the 'editable' key
+                return data and data.get("editable") or False
 
-    except importlib.metadata.PackageNotFoundError:
-        # Package not found, so it's not installed
-        pass
-    except FileNotFoundError:
-        # direct_url.json not found, likely not an editable install
-        pass
-    except json.JSONDecodeError:
-        # Error parsing direct_url.json
-        pass
-    return False
+        except importlib.metadata.PackageNotFoundError:
+            # Package not found, so it's not installed
+            pass
+        except FileNotFoundError:
+            # direct_url.json not found, likely not an editable install
+            pass
+        except json.JSONDecodeError:
+            # Error parsing direct_url.json
+            pass
+        return False
+
+    if is_editable_install(__name__):
+        import os
+
+        tt_metal_home = os.getenv("TT_METAL_HOME") or f'{os.getenv("HOME")/tt-metal}'
+
+        import ctypes
+
+        for filename in ["libtt_metal.so", "_ttnncpp.so", "_ttnn.so"]:
+            file = f"{tt_metal_home}/build/lib/{filename}"
+            try:
+                ctypes.cdll.LoadLibrary(file)
+            except:
+                pass
 
 
-if is_editable_install(__name__):
-    import os
-
-    ld_library_path = "LD_LIBRARY_PATH"
-    tt_metal_home = os.getenv("TT_METAL_HOME") or f'{os.getenv("HOME")/tt-metal}'
-
-    if ld_library_path in os.environ.keys():
-        os.environ[ld_library_path] = (
-            f"{tt_metal_home}/build/lib" + os.pathsep + os.environ[ld_library_path]
-        )
-    else:
-        os.environ[ld_library_path] = f"{tt_metal_home}/build/lib"
-
-    import sys
-
-    os.execl(sys.executable, sys.executable, *sys.argv)
-
+_preload_if_editable()
 from ._ttml import *
