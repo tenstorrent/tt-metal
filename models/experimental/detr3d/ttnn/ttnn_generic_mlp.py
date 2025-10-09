85cd6a16d1da716e9b1fdbad46502afd42e344e4
Navaneeth Krishnan <navaneeth.krishnan@ignitarium.com>
2025-10-09 11:04:26 +0000

    Updates generic mlp layer

--- models/experimental/detr3d/ttnn/ttnn_generic_mlp.py
+++ models/experimental/detr3d/ttnn/ttnn_generic_mlp.py
@@ -3,19 +3,29 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import ttnn
+import torch
 from models.experimental.detr3d.ttnn.common import TtnnConv1D
-from models.experimental.detr3d.reference.detr3d_model import Conv1d
-
-
-def p(x, a="x"):
-    print(f"{a}'s  shape: {x.shape}")
-    print(f"{a}'s  layout: {x.layout}")
-    print(f"{a}'s  dtype: {x.dtype}")
-    print(f"{a}'s config: {x.memory_config()}")
 
 
 class TttnnGenericMLP:
-    def __init__(self, module, parameters, device):
+    def __init__(
+        self,
+        # input_dim,
+        # hidden_dims,
+        # output_dim,
+        # norm_fn_name=None,
+        # activation="relu",
+        # use_conv=False,
+        # dropout=None,
+        # hidden_use_bias=False,
+        # output_use_bias=True,
+        # output_use_activation=False,
+        # output_use_norm=False,
+        # weight_init_name=None,
+        module=None,
+        parameters=None,
+        device=None,
+    ):
         self.device = device
         self.parameters = parameters
         self.module = module
@@ -23,47 +33,17 @@ class TttnnGenericMLP:
         print("parameters is", parameters)
         self.tt_layers = []
         for i, layer in enumerate(module.layers):
-            # dropout
-            # if isinstance(layer, torch.nn.Dropout):
-            #     print(f"Skipping Dropout at layers[{i}]")
-            # bn1d  - torch
-            # elif isinstance(layer, torch.nn.BatchNorm1d):
-            #     print("module is", layer)
-            #     print(f"Adding BatchNorm1d from layers[{i}]")
-            #     self.tt_layers.append(layer)
-            if isinstance(layer, Conv1d):
-                if layer.activation:
-                    activation = ttnn.UnaryWithParam(ttnn.UnaryOpType.RELU)
-                else:
-                    activation = None
-                conv1d_layer = TtnnConv1D(layer, parameters.layers[f"{i}"], device, activation=activation)
-                self.tt_layers.append(conv1d_layer)
-            # elif isinstance(layer, torch.nn.Conv1d):
-            #     conv1d_layer = TtnnConv1D(module.layers[i], parameters.layers[i], device)
-            #     # conv1d_layer = TtnnConv1D(module.layers[i], parameters.layers[i], device)
+            # if isinstance(layer, torch.nn.Conv1d):
+            #     conv1d_layer = TtnnConv1D(layer, parameters.layers[f"{i}"], device, activation=activation)
             #     self.tt_layers.append(conv1d_layer)
-            # elif isinstance(layer, torch.nn.ReLU):
-            #     relu_layer = ttnn.relu
-            #     self.tt_layers.append(relu_layer)
-
-        print(self.tt_layers)
+            if isinstance(layer, torch.nn.Conv1d):
+                conv1d_layer = TtnnConv1D(layer, parameters.layers[i], device)
+                self.tt_layers.append(conv1d_layer)
+            elif isinstance(layer, torch.nn.ReLU):
+                relu_layer = ttnn.relu
+                self.tt_layers.append(relu_layer)
 
     def __call__(self, x):
-        for i, layer in enumerate(self.tt_layers):
-            # if isinstance(layer, torch.nn.BatchNorm1d):
-            #     if x.is_sharded():
-            #         x = ttnn.sharded_to_interleaved(x, ttnn.L1_MEMORY_CONFIG)
-            #     print(f"{x.shape=}")
-            #     x = ttnn.to_torch(x).squeeze(dim=0).permute(0, 2, 1)
-            #     x = layer(x).unsqueeze(dim=0).permute(0, 1, 3, 2)  # torch bn1d
-            #     x = ttnn.from_torch(
-            #         x,
-            #         dtype=ttnn.bfloat16,
-            #         layout=ttnn.TILE_LAYOUT,
-            #         device=self.device,
-            #         memory_config=ttnn.L1_MEMORY_CONFIG,
-            #     )
-            # else:
+        for layer in self.tt_layers:
             x = layer(x)
-            print(f"ttnn layer no {i}out is", x.shape)
         return x
