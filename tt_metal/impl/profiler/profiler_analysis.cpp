2af987f5de4c709ecbf33966568a4341afe5d8df
Samarth Agarwal <sagarwal@tenstorrent.com>
2025-10-09 16:09:22 +0000

    Saving

--- tt_metal/impl/profiler/profiler_analysis.cpp
+++ tt_metal/impl/profiler/profiler_analysis.cpp
@@ -4,6 +4,7 @@
 
 #include <algorithm>
 #include <string>
+#include <tracy/Tracy.hpp>
 #include <tt-logger/tt-logger.hpp>
 #include <tt_stl/assert.hpp>
 #include <type_traits>
@@ -38,12 +39,12 @@ namespace tt {
 
 namespace tt_metal {
 
-bool matches_start_end_risc(AnalysisRisc marker_risc, AnalysisRisc config_risc) {
-    return static_cast<std::underlying_type_t<AnalysisRisc>>(config_risc & marker_risc) != 0;
+bool matches_start_end_risc(tracy::RiscType risc_type, AnalysisRiscTypes config_risc_types) {
+    return config_risc_types.find(risc_type) != config_risc_types.end();
 }
 
-bool matches_start_end_marker_type(AnalysisMarkerType marker_type, AnalysisMarkerType config_marker_type) {
-    return static_cast<std::underlying_type_t<AnalysisMarkerType>>(config_marker_type & marker_type) != 0;
+bool matches_start_end_marker_type(tracy::TTDeviceMarkerType marker_type, AnalysisMarkerTypes config_marker_types) {
+    return config_marker_types.find(marker_type) != config_marker_types.end();
 }
 
 bool matches_start_end_marker_name_keywords(
@@ -52,6 +53,10 @@ bool matches_start_end_marker_name_keywords(
         static_cast<std::underlying_type_t<tracy::MarkerDetails::MarkerNameKeyword>>(
             tracy::MarkerDetails::MarkerNameKeyword::COUNT)>& marker_name_keywords,
     const AnalysisMarkerNameKeywords& config_marker_name_keywords) {
+    if (config_marker_name_keywords.empty()) {
+        return true;
+    }
+
     bool match = false;
     for (tracy::MarkerDetails::MarkerNameKeyword keyword : config_marker_name_keywords) {
         match |=
@@ -61,8 +66,8 @@ bool matches_start_end_marker_name_keywords(
 }
 
 bool matches_start_end_config(const tracy::TTDeviceMarker& marker, const AnalysisStartEndConfig& start_end_config) {
-    return matches_start_end_risc(marker.risc, start_end_config.risc) &&
-           matches_start_end_marker_type(marker.marker_type, start_end_config.marker_type) &&
+    return matches_start_end_risc(marker.risc, start_end_config.risc_types) &&
+           matches_start_end_marker_type(marker.marker_type, start_end_config.marker_types) &&
            matches_start_end_marker_name_keywords(
                marker.marker_name_keyword_flags, start_end_config.marker_name_keywords);
 }
@@ -78,40 +83,24 @@ AnalysisResults parse_duration(
 
     for (const auto& marker_ref : markers) {
         const tracy::TTDeviceMarker& marker = marker_ref.get();
-        if (results_per_op_id.find({marker.runtime_host_id, marker.trace_id, marker.trace_id_counter}) ==
-            results_per_op_id.end()) {
-            results_per_op_id[{marker.runtime_host_id, marker.trace_id, marker.trace_id_counter}] =
-                AnalysisResults::INVALID_SINGLE_RESULT;
-        }
+        const OpId op_id = {marker.runtime_host_id, marker.trace_id, marker.trace_id_counter};
+        auto [op_id_results_it, _] = results_per_op_id.try_emplace(op_id, AnalysisResults::INVALID_SINGLE_RESULT);
 
         if (matches_start_end_config(marker, analysis_config.start_config)) {
-            if (results_per_op_id[{marker.runtime_host_id, marker.trace_id, marker.trace_id_counter}] ==
-                AnalysisResults::INVALID_SINGLE_RESULT) {
-                results_per_op_id[{marker.runtime_host_id, marker.trace_id, marker.trace_id_counter}].start_timestamp =
-                    marker.timestamp;
-                results_per_op_id[{marker.runtime_host_id, marker.trace_id, marker.trace_id_counter}].start_marker =
-                    marker_ref.get();
+            if (op_id_results_it->second == AnalysisResults::INVALID_SINGLE_RESULT) {
+                op_id_results_it->second.start_timestamp = marker.timestamp;
+                op_id_results_it->second.start_marker = marker_ref.get();
             }
         }
         if (matches_start_end_config(marker, analysis_config.end_config)) {
-            if (results_per_op_id[{marker.runtime_host_id, marker.trace_id, marker.trace_id_counter}] !=
-                AnalysisResults::INVALID_SINGLE_RESULT) {
-                results_per_op_id[{marker.runtime_host_id, marker.trace_id, marker.trace_id_counter}].end_timestamp =
-                    marker.timestamp;
-                results_per_op_id[{marker.runtime_host_id, marker.trace_id, marker.trace_id_counter}].end_marker =
-                    marker_ref.get();
+            if (op_id_results_it->second != AnalysisResults::INVALID_SINGLE_RESULT) {
+                op_id_results_it->second.end_timestamp = marker.timestamp;
+                op_id_results_it->second.end_marker = marker_ref.get();
             }
         }
     }
 
-    for (auto& [op_id, result] : results_per_op_id) {
-        log_info(
-            tt::LogMetal,
-            "Op ID: {}, Start Timestamp: {}, End Timestamp: {}",
-            op_id.runtime_id,
-            result.start_timestamp,
-            result.end_timestamp);
-
+    for (auto& [_, result] : results_per_op_id) {
         if (result != AnalysisResults::INVALID_SINGLE_RESULT) {
             TT_ASSERT(result.start_timestamp <= result.end_timestamp);
             result.duration = result.end_timestamp - result.start_timestamp;
@@ -139,6 +128,8 @@ AnalysisResults generateAnalysisForDeviceMarkers(
 std::map<OpId, OpsPerfResults::SingleOpPerfResults::OpMetaData> getMetaDataForOps(
     const std::vector<std::reference_wrapper<const tracy::TTDeviceMarker>>& markers) {
     std::map<OpId, OpsPerfResults::SingleOpPerfResults::OpMetaData> op_id_to_meta_data;
+
+    std::unordered_map<OpId, std::unordered_set<CoreCoord>, OpIdHasher> fw_cores_per_op_id;
     for (const auto& marker_ref : markers) {
         const tracy::TTDeviceMarker& marker = marker_ref.get();
         if (op_id_to_meta_data.find({marker.runtime_host_id, marker.trace_id, marker.trace_id_counter}) ==
@@ -172,8 +163,13 @@ std::map<OpId, OpsPerfResults::SingleOpPerfResults::OpMetaData> getMetaDataForOp
         if (marker
                 .marker_name_keyword_flags[static_cast<std::underlying_type_t<tracy::MarkerDetails::MarkerNameKeyword>>(
                     tracy::MarkerDetails::MarkerNameKeyword::_FW)]) {
-            it.num_fw_cores++;
+            fw_cores_per_op_id[{marker.runtime_host_id, marker.trace_id, marker.trace_id_counter}].emplace(
+                marker.core_x, marker.core_y);
         }
+    }
+
+    for (const auto& [op_id, fw_cores] : fw_cores_per_op_id) {
+        op_id_to_meta_data[op_id].num_fw_cores = fw_cores.size();
     }
 
     return op_id_to_meta_data;
@@ -183,6 +179,8 @@ OpsPerfResults generatePerfResultsForOps(
     const std::vector<AnalysisConfig>& analysis_configs,
     const std::vector<std::reference_wrapper<const tracy::TTDeviceMarker>>& device_markers,
     ThreadPool& thread_pool) {
+    ZoneScoped;
+
     OpsPerfResults ops_perf_results;
     std::map<OpId, OpsPerfResults::SingleOpPerfResults>& op_id_to_perf_results = ops_perf_results.op_id_to_perf_results;
     std::vector<AnalysisResultsConfig>& analysis_results_configs = ops_perf_results.analysis_results_configs;
@@ -219,25 +217,39 @@ OpsPerfResults generatePerfResultsForOps(
 }
 
 void writeOpsPerfResultsToCSV(const OpsPerfResults& ops_perf_results, const std::filesystem::path& report_path) {
+    ZoneScoped;
+
     std::scoped_lock lock(tt::tt_metal::MetalContext::instance().profiler_state_manager()->ops_perf_report_write_mutex);
 
     std::map<OpId, std::string> results_string_per_op_id;
 
     for (const auto& [op_id, op_perf_results] : ops_perf_results.op_id_to_perf_results) {
         results_string_per_op_id[op_id] =
-            std::to_string(op_id.runtime_id) + "," + std::to_string(op_id.trace_id) + "," +
-            std::to_string(op_id.trace_id_counter) + "," + std::to_string(op_perf_results.op_meta_data.device_id) +
-            "," + arch_to_str(op_perf_results.op_meta_data.device_arch) + "," + op_perf_results.op_meta_data.op_name +
-            "," + std::to_string(op_perf_results.op_meta_data.num_fw_cores) + "," +
+            std::to_string(op_id.runtime_id) + "," +
+            (op_id.trace_id == tracy::TTDeviceMarker::INVALID_NUM ? "" : std::to_string(op_id.trace_id)) + "," +
+            (op_id.trace_id_counter == tracy::TTDeviceMarker::INVALID_NUM ? ""
+                                                                          : std::to_string(op_id.trace_id_counter)) +
+            "," + std::to_string(op_perf_results.op_meta_data.device_id) + "," +
+            arch_to_str(op_perf_results.op_meta_data.device_arch) + "," + op_perf_results.op_meta_data.op_name + "," +
+            std::to_string(op_perf_results.op_meta_data.num_fw_cores) + "," +
             std::to_string(op_perf_results.op_meta_data.num_available_worker_cores);
 
         for (uint32_t i = 0; i < op_perf_results.analysis_results.size(); i++) {
             const AnalysisResults::SingleResult& analysis_result = op_perf_results.analysis_results[i];
             const AnalysisResultsConfig& analysis_result_config = ops_perf_results.analysis_results_configs[i];
-            results_string_per_op_id[op_id] += "," + std::to_string(analysis_result.duration);
+            results_string_per_op_id[op_id] += "," + (analysis_result == AnalysisResults::INVALID_SINGLE_RESULT
+                                                          ? ""
+                                                          : std::to_string(analysis_result.duration));
             if (analysis_result_config.display_start_and_end_timestamps) {
-                results_string_per_op_id[op_id] += "," + std::to_string(analysis_result.start_timestamp) + "," +
-                                                   std::to_string(analysis_result.end_timestamp);
+                results_string_per_op_id[op_id] += "," +
+                                                   (analysis_result == AnalysisResults::INVALID_SINGLE_RESULT
+                                                        ? ""
+                                                        : std::to_string(analysis_result.start_timestamp)) +
+                                                   "," +
+                                                   (analysis_result == AnalysisResults::INVALID_SINGLE_RESULT
+                                                        ? ""
+                                                        : std::to_string(analysis_result.end_timestamp));
+                std::to_string(analysis_result.end_timestamp);
             }
         }
     }
@@ -293,31 +305,30 @@ void from_json(const nlohmann::json& j, AnalysisResultsConfig& config) {
 void from_json(const nlohmann::json& j, AnalysisStartEndConfig& config) {
     if (j.contains("risc")) {
         if (j["risc"].is_array()) {
-            uint8_t risc_val = 0;
+            std::unordered_set<tracy::RiscType> risc_types;
             for (const auto& risc : j["risc"]) {
-                risc_val |= static_cast<std::underlying_type_t<AnalysisRisc>>(risc.get<AnalysisRisc>());
+                risc_types.insert(risc.get<tracy::RiscType>());
             }
-            config.risc = static_cast<AnalysisRisc>(risc_val);
+            config.risc_types = risc_types;
         } else {
-            j.at("risc").get_to(config.risc);
+            config.risc_types = {j.at("risc").get<tracy::RiscType>()};
         }
     } else {
-        config.risc = AnalysisRiscAny;
+        config.risc_types = AnalysisRiscTypesAny;
     }
 
     if (j.contains("marker_type")) {
         if (j["marker_type"].is_array()) {
-            uint8_t marker_type_val = 0;
+            std::unordered_set<tracy::TTDeviceMarkerType> marker_types;
             for (const auto& marker_type : j["marker_type"]) {
-                marker_type_val |=
-                    static_cast<std::underlying_type_t<AnalysisMarkerType>>(marker_type.get<AnalysisMarkerType>());
+                marker_types.insert(marker_type.get<tracy::TTDeviceMarkerType>());
             }
-            config.marker_type = static_cast<AnalysisMarkerType>(marker_type_val);
+            config.marker_types = marker_types;
         } else {
-            j.at("marker_type").get_to(config.marker_type);
+            config.marker_types = {j.at("marker_type").get<tracy::TTDeviceMarkerType>()};
         }
     } else {
-        config.marker_type = AnalysisMarkerTypeAny;
+        config.marker_types = AnalysisMarkerTypesAny;
     }
 
     if (j.contains("marker_name_keywords")) {
