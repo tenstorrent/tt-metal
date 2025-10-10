c9cfc8c423331f3e2bee0320b9aefc2d382d016d
asaigal <asaigal@tenstorrent.com>
2025-10-10 06:05:52 +0000

    Run local UMD cluster discovery when GSD run_discovery is called

--- tools/scaleout/validation/run_cluster_validation.cpp
+++ tools/scaleout/validation/run_cluster_validation.cpp
@@ -142,15 +142,9 @@ PhysicalSystemDescriptor generate_physical_system_descriptor(const InputArgs& in
         return physical_system_descriptor;
     } else {
         log_output_rank0("Running Physical Discovery");
-        constexpr bool run_discovery = true;
         auto& context = tt::tt_metal::MetalContext::instance();
-        const auto& driver = context.get_cluster().get_driver();
         auto physical_system_descriptor = tt::tt_metal::PhysicalSystemDescriptor(
-            driver,
-            context.get_distributed_context_ptr(),
-            &context.hal(),
-            context.rtoptions().get_mock_enabled(),
-            run_discovery);
+            context.get_distributed_context_ptr(), &context.hal(), context.rtoptions().get_mock_enabled());
         log_output_rank0("Physical Discovery Complete");
         log_output_rank0("Detected Hosts: " + log_hostnames(physical_system_descriptor.get_all_hostnames()));
         return physical_system_descriptor;
