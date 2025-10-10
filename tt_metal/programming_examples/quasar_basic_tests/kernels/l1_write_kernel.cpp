c6afe4f14d2d1c694e7fc8730e2d5d0690e04f4f
arikTT <ayaacob@tenstorrent.com>
2025-10-10 15:39:59 +0000

    add a test that include CT and Rt arguments

---
+++ tt_metal/programming_examples/quasar_basic_tests/kernels/l1_write_kernel.cpp
@@ -0,0 +1,10 @@
+#include "dataflow_api.h"
+
+void kernel_main() {
+    uint32_t dst_addr = get_arg_val<uint32_t>(0);
+    uint32_t value = get_common_arg_val<uint32_t>(0);
+    uint32_t buffer_size = get_named_ct_arg("buffer_size");
+    DPRINT << "buffer_size: " << buffer_size << ENDL();
+    *((uint32_t*)(dst_addr + MEM_L1_UNCACHED_BASE)) =
+        value;  // use cache write-around for now, in the future use cache flush
+}
