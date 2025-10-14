82cd8296f27b0063630a0240783f69d6f034f2ff
Bojan Rosko <brosko@tenstorrent.com>
2025-10-14 21:13:25 +0000

    <umd/device/tt_core_coordinates.h> -> <umd/device/types/core_coordinates.hpp>

--- tests/tt_metal/tt_metal/device/test_device.cpp
+++ tests/tt_metal/tt_metal/device/test_device.cpp
@@ -6,7 +6,7 @@
 #include <gtest/gtest.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <umd/device/tt_core_coordinates.h>
+#include <umd/device/types/core_coordinates.hpp>
 #include <tt-metalium/allocator.hpp>
 #include <tt-metalium/host_api.hpp>
 #include <tt-metalium/tt_metal.hpp>
