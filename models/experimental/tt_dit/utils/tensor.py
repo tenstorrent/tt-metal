61da836d1ae5f097b9d225b131a1fcaab7078bde
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2025-10-14 13:01:26 +0200

    revert tensor changes to old functions

--- models/experimental/tt_dit/utils/tensor.py
+++ models/experimental/tt_dit/utils/tensor.py
@@ -21,34 +21,57 @@ def bf16_tensor(
     Replicates or shards a tensor based on the mesh_axis and shard_dim
     """
     assert (mesh_axis is None) == (shard_dim is None)
+    mesh_mapper = None
     if mesh_axis is not None:
-        axes = [None] * len(x.shape)
-        axes[shard_dim] = mesh_axis
-    else:
-        axes = None
-    return from_torch(x, device=device, layout=layout, mesh_axes=axes)
+        mapper_dims = [None, None]
+        mapper_dims[mesh_axis] = shard_dim
+        mesh_mapper = ttnn.ShardTensor2dMesh(device, mesh_shape=tuple(device.shape), dims=mapper_dims)
+
+    return ttnn.from_torch(
+        x,
+        layout=layout,
+        dtype=ttnn.bfloat16,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        device=device,
+        mesh_mapper=mesh_mapper,
+    )
 
 
 def bf16_tensor_host(
     x: torch.Tensor, device: ttnn.Device | None = None, mesh_axis=None, shard_dim=None, layout=ttnn.TILE_LAYOUT
 ) -> ttnn.Tensor:
     assert (mesh_axis is None) == (shard_dim is None)
+    mesh_mapper = None
     if mesh_axis is not None:
-        axes = [None] * len(x.shape)
-        axes[shard_dim] = mesh_axis
-    else:
-        axes = None
-    return from_torch(x, device=device, layout=layout, mesh_axes=axes, on_host=True)
+        mapper_dims = [None, None]
+        mapper_dims[mesh_axis] = shard_dim
+        mesh_mapper = ttnn.ShardTensor2dMesh(device, mesh_shape=tuple(device.shape), dims=mapper_dims)
+
+    return ttnn.from_torch(
+        x,
+        layout=layout,
+        dtype=ttnn.bfloat16,
+        mesh_mapper=mesh_mapper,
+    )
 
 
 def bf16_tensor_2dshard(
     x: torch.Tensor, device: ttnn.Device, shard_mapping: dict[int, int], layout=ttnn.TILE_LAYOUT
 ) -> ttnn.Tensor:
     assert len(shard_mapping) == 2
-    axes = [None] * len(x.shape)
-    for mesh_axis, tensor_dim in shard_mapping.items():
-        axes[tensor_dim] = mesh_axis
-    return from_torch(x, device=device, layout=layout, mesh_axes=axes)
+    assert all(0 <= k <= 1 and 0 <= v < len(x.shape) for k, v in shard_mapping.items())
+    mapper_dims = [None, None]
+    for k, v in shard_mapping.items():
+        mapper_dims[k] = v
+    mesh_mapper = ttnn.ShardTensor2dMesh(device, mesh_shape=tuple(device.shape), dims=mapper_dims)
+    return ttnn.from_torch(
+        x,
+        layout=layout,
+        dtype=ttnn.bfloat16,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        device=device,
+        mesh_mapper=mesh_mapper,
+    )
 
 
 def from_torch(
