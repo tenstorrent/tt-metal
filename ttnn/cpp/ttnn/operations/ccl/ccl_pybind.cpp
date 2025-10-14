281c49dc877e15b96501b857d3de869525a31c0c
Saad Jameel <163029024+sjameelTT@users.noreply.github.com>
2025-10-14 12:28:44 -0400

    The return of ttnn.all_gather (#30414)
    ### Ticket
    #26456 
    #26952 
    #30348
    
    ### Problem description
    `ttnn.all_gather` exposed op internals like semaphores and required
    persistent buffers. As well, it contained hard coded implementations
    that we often accidentally demux-ed too. This is required for us to
    proceed with #30348 as we would otherwise need an API to compose a
    parent mesh global semaphore from submeshes.
    
    ### What's changed
    Add back ttnn.all_gather with semaphores removed, automatic topology and
    link count deduction, and hyperparameters unexposed. We still have the
    freedom to pass all of these in but they're no longer required.
    
    ### Checklist
    - APC: https://github.com/tenstorrent/tt-metal/actions/runs/18420171850
    - T3K: https://github.com/tenstorrent/tt-metal/actions/runs/18420182231

--- ttnn/cpp/ttnn/operations/ccl/ccl_pybind.cpp
+++ ttnn/cpp/ttnn/operations/ccl/ccl_pybind.cpp
@@ -9,6 +9,7 @@
 
 #include "ttnn/operations/ccl/mesh_partition/mesh_partition_pybind.hpp"
 #include "ttnn/operations/ccl/barrier/barrier_pybind.hpp"
+#include "ttnn/operations/ccl/all_gather/all_gather_pybind.hpp"
 #include "ttnn/operations/ccl/all_to_all_combine/all_to_all_combine_pybind.hpp"
 #include "ttnn/operations/ccl/all_to_all_dispatch/all_to_all_dispatch_pybind.hpp"
 #include "ttnn/operations/ccl/reduce_scatter/reduce_scatter_pybind.hpp"
@@ -28,6 +29,7 @@ void py_module(py::module& module) {
     ccl::py_bind_common(module);
     ccl::py_bind_mesh_partition(module);
     ccl::py_bind_barrier(module);
+    ccl::py_bind_all_gather(module);
     ccl::py_bind_all_to_all_combine(module);
     ccl::py_bind_all_to_all_dispatch(module);
     ccl::py_bind_reduce_scatter(module);
