156a6998835deaf0f0d13ae4da47e43d74b6693b
Aleksandar Stancov <astancov@tenstorrent.com>
2025-10-16 08:23:39 +0000

    bumped l1_small size for sdxl

--- models/experimental/stable_diffusion_xl_base/tests/test_common.py
+++ models/experimental/stable_diffusion_xl_base/tests/test_common.py
@@ -19,7 +19,7 @@ import ttnn
 
 from models.experimental.stable_diffusion_xl_base.vae.tt.tt_autoencoder_kl import TtAutoencoderKL
 
-SDXL_L1_SMALL_SIZE = 23000
+SDXL_L1_SMALL_SIZE = 24000
 SDXL_TRACE_REGION_SIZE = 34000000
 SDXL_CI_WEIGHTS_PATH = "/mnt/MLPerf/tt_dnn-models/hf_home"
 SDXL_FABRIC_CONFIG = ttnn.FabricConfig.FABRIC_1D
