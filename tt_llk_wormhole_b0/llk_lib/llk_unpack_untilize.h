2e62975bf69321a3ded3b131c21a86c1bf23eb2d
Filip Vranic <fvranic@tenstorrent.com>
2025-10-17 23:19:05 +0200

    Revert "make include_setup_calls default value to  true (#715)" (#749)
    ### Ticket
    None
    
    ### Problem description
    There was a regression observed in tt-metal:
    https://github.com/tenstorrent/tt-metal/actions/runs/18583035444/job/52982150700
    The problem hasn't been caught in time since all post-commit tests have
    passed despite the fact that SDXL PCC has dropped (caught by another
    pipeline).
    
    ### What's changed
    This reverts commit 81f10ae5fd49d03c4d1bdf9ddefba36ba7f5f085.
    
    ### Type of change
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update

--- tt_llk_wormhole_b0/llk_lib/llk_unpack_untilize.h
+++ tt_llk_wormhole_b0/llk_lib/llk_unpack_untilize.h
@@ -68,7 +68,7 @@ inline void _llk_unpack_untilize_init_(
     const std::uint32_t tile_size,
     const std::uint32_t face_r_dim                 = FACE_R_DIM,
     [[maybe_unused]] const std::uint32_t num_faces = 4,
-    const bool include_setup_calls                 = true)
+    const bool include_setup_calls                 = false)
 {
     if (include_setup_calls)
     {
