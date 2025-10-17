df83ff355dfd105b78fe08c8510d7a9062df5d5a
Boris Janjic <bjanjic@tenstorrent.com>
2025-10-17 06:48:10 +0000

    update test function

--- models/experimental/panoptic_deeplab/tests/test_device_perf_pdl.py
+++ models/experimental/panoptic_deeplab/tests/test_device_perf_pdl.py
@@ -6,28 +6,6 @@ import pytest
 from models.perf.device_perf_utils import run_device_perf, check_device_perf, prep_device_perf_report
 
 
-def run_device_perf_tests(
-    command, expected_device_perf_cycles_per_iteration, subdir, model_name, num_iterations=1, batch_size=1, margin=0.015
-):
-    cols = ["DEVICE FW", "DEVICE KERNEL", "DEVICE BRISC KERNEL"]
-
-    inference_time_key = "AVG DEVICE KERNEL DURATION [ns]"
-    post_processed_results = run_device_perf(
-        command, subdir=subdir, num_iterations=num_iterations, cols=cols, batch_size=batch_size
-    )
-    expected_perf_cols = {inference_time_key: expected_device_perf_cycles_per_iteration}
-    expected_results = check_device_perf(
-        post_processed_results, margin=margin, expected_perf_cols=expected_perf_cols, assert_on_fail=True
-    )
-    prep_device_perf_report(
-        model_name=model_name,
-        batch_size=batch_size,
-        post_processed_results=post_processed_results,
-        expected_results=expected_results,
-        comments="",
-    )
-
-
 @pytest.mark.parametrize(
     "command, expected_device_perf_cycles_per_iteration, subdir, model_name, num_iterations, batch_size, margin",
     [
@@ -49,12 +27,20 @@ def run_device_perf_tests(
 def test_device_perf_pdl(
     command, expected_device_perf_cycles_per_iteration, subdir, model_name, num_iterations, batch_size, margin
 ):
-    run_device_perf_tests(
-        command,
-        expected_device_perf_cycles_per_iteration,
-        subdir=subdir,
+    cols = ["DEVICE FW", "DEVICE KERNEL", "DEVICE BRISC KERNEL"]
+
+    inference_time_key = "AVG DEVICE KERNEL DURATION [ns]"
+    post_processed_results = run_device_perf(
+        command, subdir=subdir, num_iterations=num_iterations, cols=cols, batch_size=batch_size
+    )
+    expected_perf_cols = {inference_time_key: expected_device_perf_cycles_per_iteration}
+    expected_results = check_device_perf(
+        post_processed_results, margin=margin, expected_perf_cols=expected_perf_cols, assert_on_fail=True
+    )
+    prep_device_perf_report(
         model_name=model_name,
-        num_iterations=num_iterations,
         batch_size=batch_size,
-        margin=margin,
+        post_processed_results=post_processed_results,
+        expected_results=expected_results,
+        comments="",
     )
