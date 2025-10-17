d199db5bb207427d20810620b5563ebcb017d627
Denys Makoviichuk <dmakoviichuk@tenstorrent.com>
2025-10-17 10:12:09 -0700

    [TT-Train] add assign function (#30050)
    ### Problem description
    We can only create new tensor and it is impossible to update existing
    one in python
    
    ### What's changed
    Describe the approach used to solve the problem.
    Summarize the changes made and its impact.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes

--- tt-train/sources/ttml/ttml/common/utils.py
+++ tt-train/sources/ttml/ttml/common/utils.py
@@ -42,7 +42,8 @@ def initialize_device(yaml_config: dict):
     from ttml.common.config import DeviceConfig
 
     device_config = DeviceConfig(yaml_config)
-    ttml.core.distributed.enable_fabric(device_config.total_devices())
+    if device_config.total_devices() > 1:
+        ttml.core.distributed.enable_fabric(device_config.total_devices())
     ttml.autograd.AutoContext.get_instance().open_device(device_config.mesh_shape, device_config.device_ids)
 
 
