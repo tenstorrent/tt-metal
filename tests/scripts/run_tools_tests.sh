588392768d0574249c623080d45333519eb2fa00
David Popov <dpopov@tenstorrent.com>
2025-10-20 13:10:39 +0200

    Revert "Make `TT_METAL_HOME` Optional (#30502)" (#30825)
    This reverts commit 48eef803dc2f7a79e8cf44b5faf1e5bf88dd0ebc.
    
    ### Problem description
    APC is red beacuse of this commit
    
    ### What's changed
    Revert 48eef803dc2f7a79e8cf44b5faf1e5bf88dd0ebc
    
    ### Checklist
    - [x] [(Galaxy) APC fast
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/18647514408)
    CI passes

--- tests/scripts/run_tools_tests.sh
+++ tests/scripts/run_tools_tests.sh
@@ -2,6 +2,11 @@
 
 set -eo pipefail
 
+if [[ -z "$TT_METAL_HOME" ]]; then
+    echo "Must provide TT_METAL_HOME in environment" 1>&2
+    exit 1
+fi
+
 if [ -z "${ARCH_NAME}" ]; then
   echo "Error: ARCH_NAME is not set. Exiting." >&2
   exit 1
