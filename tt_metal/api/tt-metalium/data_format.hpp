4352c0e0b961183ff7f0dbaa6f30c34afa0b456b
riverwuTT <riverwu@tenstorrent.com>
2025-10-20 18:42:36 +0000

    remove completely unused functions

--- tt_metal/api/tt-metalium/data_format.hpp
+++ tt_metal/api/tt-metalium/data_format.hpp
@@ -18,10 +18,8 @@ enum class ExpPrecision : std::uint8_t {
     B = 1,
 };
 
-bool is_valid_conversion(DataFormat input_format, DataFormat output_format);
 bool is_exp_b_format(DataFormat data_format);
 ExpPrecision get_exp_precision(DataFormat data_format);
-void dump_data_formats(DataFormat data_format[NUM_CIRCULAR_BUFFERS]);
 
 /*
  * Checks operand data formats for same exponent width format
@@ -29,20 +27,11 @@ void dump_data_formats(DataFormat data_format[NUM_CIRCULAR_BUFFERS]);
  */
 DataFormat check_consistent_format_across_buffers(DataFormat data_format[NUM_CIRCULAR_BUFFERS]);
 
-/*
- * Checks operand data formats for same data format
- * Returns the last valid data-format in operand buffers.
- */
-DataFormat check_same_format_across_buffers(DataFormat data_format[NUM_CIRCULAR_BUFFERS]);
-
 DataFormat check_valid_formats_in_out_data_formats(DataFormat data_format[NUM_CIRCULAR_BUFFERS]);
 ExpPrecision get_data_exp_precision(DataFormat data_formats[NUM_CIRCULAR_BUFFERS]);
 
 // Checks if all formats in format array are fp32/tf32/invalid, then data can be unpacked as tf32 for fp32 accumulation
 bool is_all_fp32_formats(const DataFormat data_format[NUM_CIRCULAR_BUFFERS]);
-
-const DataFormat get_single_pack_src_format(
-    DataFormat input_format, DataFormat unpack_conditional_dst_format, bool fp32_dest_acc_en, tt::ARCH arch);
 
 std::vector<DataFormat> get_unpack_src_formats(DataFormat buf_formats[NUM_CIRCULAR_BUFFERS]);
 std::vector<DataFormat> get_unpack_dst_formats(
