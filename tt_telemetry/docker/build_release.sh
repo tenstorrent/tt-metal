632dc60dc471711bf208cae90f9223c59bd1b889
Bart Trzynadlowski <btrzynadlowski@tenstorrent.com>
2025-10-20 19:12:42 +0000

    Fixed release build script

--- tt_telemetry/docker/build_release.sh
+++ tt_telemetry/docker/build_release.sh
@@ -16,4 +16,4 @@ fi
 
 
 # Build release image (pulls from GitHub, uses TT_METAL_HOME only to locate Dockerfile)
-docker build --target release-t ghcr.io/btrzynadlowski-tt/tt-telemetry:latest -f $TT_METAL_HOME/tt_telemetry/docker/Dockerfile $TT_METAL_HOME
+docker build --target release -t ghcr.io/btrzynadlowski-tt/tt-telemetry:latest -f $TT_METAL_HOME/tt_telemetry/docker/Dockerfile $TT_METAL_HOME
