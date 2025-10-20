9ea70e902e11b394dd6d8e3ad0b537289737fc58
Andrew Fuller <afuller@tenstorrent.com>
2025-10-20 09:25:47 -0700

    [skip ci] temp revert of scikit (#30841)
    ### Ticket
    NA
    
    ### Problem description
    The transition to use scikit-build overlooked the impact on the existing
    editable install developer flow.
    That flow relied on a two part build.
    1. create_venv.sh -> creates a ttnn virtual environment without compiled
    C++ code
    2. build_metal.sh -> compiles C++ code, and allows the editable install
    ttnn module to pick up compiled libraries
    
    ### What's changed
    - Revert the changes to `pyproject.toml` and `setup.py`.
    - Do not revert the enhancements made to ensure that missing files are
    in the debian package.
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/18655238019)
    CI passes
    - [x] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: blozano-tt <blozano@tenstorrent.com>

--- setup.py
+++ setup.py
@@ -105,6 +105,13 @@ def attempt_get_env_var(env_var_name):
     return os.environ[env_var_name]
 
 
+def get_is_srcdir_build():
+    build_dir = CMakeBuild.get_working_dir()
+    assert build_dir.is_dir()
+    git_dir = build_dir / ".git"
+    return git_dir.exists()
+
+
 def get_metal_local_version_scheme(metal_build_config, version):
     if version.dirty:
         return f"+g{version.node}"
@@ -183,6 +190,205 @@ class EditableWheel(editable_wheel):
         WheelFile(os.path.join(self.dist_dir, wheel), mode="a").writestr(pth_filename, pth_content)
 
 
+class CMakeBuild(build_ext):
+    @staticmethod
+    def get_build_env():
+        return {
+            **os.environ.copy(),
+            "CXX": "clang++-17",
+        }
+
+    @staticmethod
+    def get_working_dir():
+        working_dir = Path(__file__).parent
+        assert working_dir.is_dir()
+        return working_dir
+
+    # This should only run when building the wheel. Should not be running for any dev flow
+    # Taking advantage of the fact devs run editable pip install -> "pip install -e ."
+    def run(self) -> None:
+        if self.is_editable_install_():
+            assert get_is_srcdir_build(), f"Editable install detected in a non-srcdir environment, aborting"
+            return
+
+        build_env = CMakeBuild.get_build_env()
+        source_dir = (
+            metal_build_config.from_precompiled_dir
+            if metal_build_config.from_precompiled_dir
+            else CMakeBuild.get_working_dir()
+        )
+        assert source_dir.is_dir(), f"Source dir {source_dir} seems to not exist"
+
+        if metal_build_config.from_precompiled_dir:
+            build_dir = source_dir / "build"
+            assert (build_dir / "lib").exists() and (
+                source_dir / "runtime"
+            ).exists(), "The precompiled option is selected via `TT_FROM_PRECOMPILED` \
+            env var. Please place files into `build/lib` and `runtime` folders."
+        else:
+            # Determine desired build type for wheel builds (e.g., Release, Debug, RelWithDebInfo)
+            build_type = os.environ.get("CIBW_BUILD_TYPE", "Release")
+            build_dir = source_dir / f"build_{build_type}"
+            # We indirectly set a wheel build for our CMake build by using BUILD_SHARED_LIBS. This does the following things:
+            # - Bundles (most) of our libraries into a static library to deal with a potential singleton bug error with tt_cluster (to fix)
+            build_script_args = ["--build-static-libs", "--release"]
+
+            if "CIBUILDWHEEL" in os.environ:
+                cmake_args = [
+                    "cmake",
+                    "-B",
+                    build_dir,
+                    "-G",
+                    "Ninja",
+                    f"-DCMAKE_BUILD_TYPE={build_type}",
+                    f"-DCMAKE_INSTALL_PREFIX=build_{build_type}",
+                    "-DBUILD_SHARED_LIBS=ON",
+                    "-DTT_INSTALL=ON",
+                    "-DTT_UNITY_BUILDS=ON",
+                    "-DTT_ENABLE_LIGHT_METAL_TRACE=ON",
+                    "-DWITH_PYTHON_BINDINGS=ON",
+                    "-DTT_USE_SYSTEM_SFPI=ON",
+                    "-DENABLE_CCACHE=TRUE",
+                ]
+
+                # Add Tracy flags if enabled
+                if os.environ.get("CIBW_ENABLE_TRACY") == "ON":
+                    cmake_args.extend(
+                        [
+                            "-DENABLE_TRACY=ON",
+                        ]
+                    )
+                else:
+                    cmake_args.extend(
+                        [
+                            "-DENABLE_TRACY=OFF",
+                        ]
+                    )
+
+                cmake_args.extend(["-S", source_dir])
+
+                subprocess.check_call(cmake_args)
+                subprocess.check_call(
+                    [
+                        "cmake",
+                        "--build",
+                        build_dir,
+                    ]
+                )
+                subprocess.check_call(
+                    [
+                        "cmake",
+                        "--install",
+                        build_dir,
+                    ]
+                )
+            else:
+                subprocess.check_call(["./build_metal.sh", *build_script_args], cwd=source_dir, env=build_env)
+
+        # Some verbose sanity logging to see what files exist in the outputs
+        subprocess.check_call(["ls", "-hal"], cwd=source_dir, env=build_env)
+        subprocess.check_call(["ls", "-hal", str(build_dir / "lib")], cwd=source_dir, env=build_env)
+        subprocess.check_call(["ls", "-hal", "runtime"], cwd=source_dir, env=build_env)
+
+        # Copy needed C++ shared libraries and runtime assets into wheel (sfpi, FW etc)
+        lib_patterns = ["_ttnn.so", "_ttnncpp.so", "libtt_metal.so", "libdevice.so", "libtt_stl.so"]
+        runtime_patterns = [
+            "hw/**/*",
+        ]
+        runtime_exclude_files = []
+        if BUNDLE_SFPI:
+            runtime_patterns.append("sfpi/**/*")
+            runtime_exclude_files = [
+                "riscv32-unknown-elf-lto-dump",
+                "riscv32-unknown-elf-gdb",
+                "riscv32-unknown-elf-objdump",
+                "riscv32-unknown-elf-run",
+                "riscv32-unknown-elf-ranlib",
+                "riscv32-unknown-elf-gprof",
+                "riscv32-unknown-elf-strings",
+                "riscv32-unknown-elf-size",
+                "riscv32-unknown-elf-readelf",
+                "riscv32-unknown-elf-nm",
+                "riscv32-unknown-elf-c++filt",
+                "riscv32-unknown-elf-addr2line",
+                "riscv32-unknown-elf-gcov",
+                "riscv32-unknown-elf-gcov-tool",
+                "riscv32-unknown-elf-gcov-dump",
+                "riscv32-unknown-elf-elfedit",
+                "riscv32-unknown-elf-gcc-ranlib",
+                "riscv32-unknown-elf-gcc-nm",
+                "riscv32-unknown-elf-gdb-add-index",
+            ]
+        ttnn_patterns = [
+            # These weren't supposed to be in the JIT API, but one file currently is
+            "api/ttnn/tensor/enum_types.hpp",
+        ]
+        ttnn_cpp_patterns = [
+            "ttnn/deprecated/**/kernels/**/*",
+            "ttnn/operations/**/kernels/**/*",
+            "ttnn/operations/**/kernels_ng/**/*",
+            "ttnn/operations/kernel_helper_functions/*",
+            "ttnn/operations/ccl/**/*",
+            "ttnn/operations/data_movement/**/*",
+            "ttnn/operations/moreh/**/*",
+            "ttnn/kernel/*",
+        ]
+        tt_metal_patterns = [
+            "api/tt-metalium/buffer_constants.hpp",
+            "api/tt-metalium/buffer_types.hpp",
+            "api/tt-metalium/circular_buffer_constants.h",
+            "api/tt-metalium/constants.hpp",
+            "api/tt-metalium/dev_msgs.h",
+            "api/tt-metalium/fabric_edm_types.hpp",
+            "fabric/fabric_edm_packet_header.hpp",
+            "api/tt-metalium/edm_fabric_counters.hpp",
+            "core_descriptors/*.yaml",
+            "fabric/hw/**/*",
+            "fabric/mesh_graph_descriptors/*.yaml",
+            "fabric/mesh_graph_descriptors/*.textproto",
+            "fabric/impl/kernels/edm_fabric/fabric_erisc_router.cpp",
+            "fabric/impl/kernels/tt_fabric_mux.cpp",
+            "lite_fabric/hw/**/*",
+            "hw/**/*",
+            "hostdevcommon/api/hostdevcommon/**/*",
+            "impl/dispatch/kernels/**/*",
+            "include/**/*",
+            "kernels/**/*",
+            "third_party/tt_llk/**/*",
+            "tools/profiler/**/*",
+            "soc_descriptors/*.yaml",
+        ]
+        copy_tree_with_patterns(build_dir / get_lib_dir(), self.build_lib + f"/ttnn/build/lib", lib_patterns)
+        copy_tree_with_patterns(build_dir, self.build_lib + "/ttnn/build/lib", ["sfpi-version.json"])
+        copy_tree_with_patterns(
+            source_dir / "runtime", self.build_lib + "/ttnn/runtime", runtime_patterns, runtime_exclude_files
+        )
+        copy_tree_with_patterns(source_dir / "ttnn", self.build_lib + "/ttnn", ttnn_patterns)
+        copy_tree_with_patterns(source_dir / "ttnn/cpp", self.build_lib + "/ttnn/ttnn/cpp", ttnn_cpp_patterns)
+        copy_tree_with_patterns(source_dir / "tt_metal", self.build_lib + "/ttnn/tt_metal", tt_metal_patterns)
+
+        # Move built final built _ttnn SO into appropriate location in ttnn Python tree in wheel
+        assert len(self.extensions) == 1, f"Detected {len(self.extensions)} extensions, but should be only 1: ttnn"
+        ext = list(self.extensions)[0]
+        fullname = self.get_ext_fullname(ext.name)
+        filename = self.get_ext_filename(fullname)
+
+        build_lib = self.build_lib
+        full_lib_path = build_lib + "/" + filename
+
+        dir_path = os.path.dirname(full_lib_path)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+        dest_ttnn_build_dir = self.build_lib + "/ttnn/build"
+        src = os.path.join(dest_ttnn_build_dir, build_constants_lookup[ext].so_src_location)
+        self.copy_file(src, full_lib_path)
+        os.remove(src)
+
+    def is_editable_install_(self):
+        return self.inplace
+
+
 packages = find_packages(where="ttnn", exclude=["ttnn.examples", "ttnn.examples.*"])
 packages += find_packages("tools")
 
@@ -208,7 +414,7 @@ setup(
         "triage": "tools/triage",
     },
     ext_modules=ext_modules,
-    cmdclass=dict(editable_wheel=EditableWheel),
+    cmdclass=dict(build_ext=CMakeBuild, editable_wheel=EditableWheel),
     zip_safe=False,
     long_description=readme,
     long_description_content_type="text/markdown",
