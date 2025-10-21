80cab8656b8e8f7830f2b37e5d76ec991f512ae7
Anil Mahmud <amahmud@tenstorrent.com>
2025-10-21 02:42:40 -0400

    Stall changing packer config till packer finishes & add uninit functions for unpack_tilizeA_B (#738)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/27504
    
    ### Problem description
    The stallwait for waiting for packer to finish before changing config
    registers is used too far down the code, after some registers which
    affect packing are changed. May be this was an optimization of some sort
    where we knew which registers were latched and when. But according to
    some discussions in LLK meetings we decided to not use such
    optimizations.
    
    There is no unpack_tilzieA_B_uninit which is also needed for the
    relevant issue to be fixed.
    
    ### What's changed
    Stall till packer finishes before changing relevant configuration
    registers.
    Added unpack_tilize_A_B_uninit 
    
    ### Type of change
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)

--- tt_llk_wormhole_b0/llk_lib/llk_unpack_tilize.h
+++ tt_llk_wormhole_b0/llk_lib/llk_unpack_tilize.h
@@ -447,6 +447,27 @@ inline void _llk_unpack_tilize_uninit_(const std::uint32_t unpack_dst_format, co
         p_gpr_unpack::FACE_DIM_16x16); // GPR preloaded with  16 | (16 << 16)}
 }
 
+inline void _llk_unpack_tilizeA_B_uninit_(const std::uint32_t unpack_dst_format, const std::uint32_t face_r_dim = FACE_R_DIM)
+{
+    TT_SETADCXX(p_setadc::UNP_A, face_r_dim * FACE_C_DIM - 1, 0x0);
+    TT_SETADCXX(p_setadc::UNP_B, face_r_dim * FACE_C_DIM - 1, 0x0);
+    unpack_config_u config = {0};
+
+    config.f.out_data_format = unpack_dst_format;
+    config.f.throttle_mode   = 2;
+    TT_SETDMAREG(0, LOWER_HALFWORD(config.val[0]), 0, LO_16(p_gpr_unpack::TMP0));
+    TT_SETDMAREG(0, UPPER_HALFWORD(config.val[0]), 0, HI_16(p_gpr_unpack::TMP0));
+    TTI_REG2FLOP(1, 0, 0, 0, THCON_SEC0_REG2_Out_data_format_ADDR32 + 0 - THCON_CFGREG_BASE_ADDR32,
+                 p_gpr_unpack::TMP0); // Load unpack config[0]
+    TTI_REG2FLOP(
+        1,
+        0,
+        0,
+        0,
+        THCON_SEC0_REG5_Tile_x_dim_cntx0_ADDR32 - THCON_CFGREG_BASE_ADDR32,
+        p_gpr_unpack::FACE_DIM_16x16); // GPR preloaded with  16 | (16 << 16)}
+}
+
 /*************************************************************************
  * LLK UNPACK FAST TILIZE (Tilize single input using both unpackers and packer)
  * full_dim is the tensor width in number of tiles
