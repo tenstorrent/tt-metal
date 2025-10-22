1cd7c3b25d21f46406d18e4358930b52c007e3d5
Umair Bilal Cheema <129103768+ubcheema@users.noreply.github.com>
2025-10-22 08:16:47 -0700

    Reserve an L1 block for various tt-fabric router needs. (#27977)
    Currently we are only reserving 2064 bytes for routing tables in erisc
    L1.
    This change is to reserve an extra 4096 bytes so that we have a block of
    6060 bytes available for different purposes.
    It will include routing tables, any host<->fabric router mailboxes,
    telemetry buffers etc.
    Currently we are only loading routing tables, but other usage will be
    added as new features come up.
    
    ### Ticket
    Link to Github Issue
    
    ### Problem description
    Provide context for the problem.
    
    ### What's changed
    Describe the approach used to solve the problem.
    Summarize the changes made and its impact.
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-quick.yaml)
    CI passes (if applicable)
    - [ ] [TG demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/tg-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- tt_metal/hw/inc/tt-1xx/wormhole/eth_l1_address_map.h
+++ tt_metal/hw/inc/tt-1xx/wormhole/eth_l1_address_map.h
@@ -48,7 +48,7 @@ struct address_map {
     static constexpr std::int32_t KERNEL_BASE = 0xA840;
     static constexpr std::int32_t ROUTING_FW_RESERVED_BASE = L1_EPOCH_Q_BASE + APP_FIRMWARE_SIZE;
 
-    static constexpr std::int32_t MAX_L1_LOADING_ADDR = 0x3E000;
+    static constexpr std::int32_t MAX_L1_LOADING_ADDR = 0x3D000;
 
     // TT Metal Specific
     //     Optional FW reserved space for routing FW
@@ -86,8 +86,11 @@ struct address_map {
     static constexpr std::uint32_t ERISC_MEM_MAILBOX_SIZE = 5072;
     static constexpr std::uint32_t ERISC_MEM_MAILBOX_END = ERISC_MEM_MAILBOX_BASE + ERISC_MEM_MAILBOX_SIZE;
     static constexpr std::int32_t ERISC_L1_KERNEL_CONFIG_BASE = ERISC_MEM_MAILBOX_END;
-    static constexpr std::int32_t FABRIC_ROUTER_CONFIG_BASE =
+    static constexpr std::int32_t FABRIC_ROUTER_RESERVED_BASE =
         (ERISC_L1_KERNEL_CONFIG_BASE + ERISC_L1_KERNEL_CONFIG_SIZE + 31) & ~31;
+    static constexpr std::int32_t FABRIC_ROUTER_RESERVED_SIZE = 6160;
+
+    static constexpr std::int32_t FABRIC_ROUTER_CONFIG_BASE = FABRIC_ROUTER_RESERVED_BASE;
     static constexpr std::int32_t FABRIC_ROUTER_CONFIG_SIZE = 1296;
 
     static constexpr std::uint32_t FABRIC_COMPRESSED_ROUTING_PATH_SIZE_1D = 0;
@@ -102,7 +105,9 @@ struct address_map {
     static constexpr std::int32_t IERISC_FABRIC_ROUTING_PATH_BASE_2D = MEM_IERISC_FABRIC_ROUTING_PATH_BASE_2D;
     static constexpr std::int32_t FABRIC_ROUTING_PATH_SIZE = FABRIC_ROUTING_PATH_SIZE_1D + FABRIC_ROUTING_PATH_SIZE_2D;
 
-    static constexpr std::int32_t ERISC_BARRIER_BASE = (FABRIC_ROUTING_PATH_BASE + FABRIC_ROUTING_PATH_SIZE + 31) & ~31;
+
+    static constexpr std::int32_t ERISC_BARRIER_BASE =
+        (FABRIC_ROUTER_RESERVED_BASE + FABRIC_ROUTER_RESERVED_SIZE + 31) & ~31;
     static_assert(ERISC_BARRIER_BASE < MAX_SIZE, "Erisc config region is greater than MAX_SIZE");
 
     // This scratch address is same as ERISC_L1_UNRESERVED_BASE, as the scratch space is used to copy data during
