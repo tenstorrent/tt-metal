0555492a79c7545dbf9d6dfb25332b3825fb37fd
vsureshTT <vsuresh@tenstorrent.com>
2025-10-22 21:06:15 +0000

    saving changes

--- ttnn/cpp/ttnn/operations/normalization/layernorm_distributed/device/kernels/compute/layernorm_pre_allgather_welfords.cpp
+++ ttnn/cpp/ttnn/operations/normalization/layernorm_distributed/device/kernels/compute/layernorm_pre_allgather_welfords.cpp
@@ -3,22 +3,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 /*
- * This kernel computes layernorm or rmsnorm, dependent on the RMSNORM define.
- * For layernorm it receives E(x**2) and E(x) and computes the remaining normalization based on gamma, beta and epsilon.
- *   E(x**2) and E(x) are contained in a two tile wide tensor containing E(x**2) and E(x) in the left most columns per
- * tile. For rmsnorm it receives E(x**2) and computes the remaining normalization based on gamma, beta and epsilon.
- *   E(x**2) is contained in a one tile wide tensor containing E(x**2) in the left most column.
  */
 
 #include <cstdint>
 
-#define REDUCE_OP PoolType::SUM
-#define REDUCE_DIM ReduceDim::REDUCE_ROW
-
 #define BCAST_LLKOP EltwiseBinaryType::ELWMUL
 #define BCAST_DIM BroadcastType::COL
 
-#include "compute_kernel_api/reduce.h"
 #include "compute_kernel_api/bcast.h"
 #include "compute_kernel_api/eltwise_binary.h"
 #include "compute_kernel_api/layernorm.h"
@@ -35,13 +26,13 @@ namespace NAMESPACE {
 void MAIN {
     uint32_t NCHt = get_arg_val<uint32_t>(0);
     constexpr uint32_t Wt = get_compile_time_arg_val(0);
-    constexpr uint32_t blk = get_compile_time_arg_val(1);
-    constexpr uint32_t stats_tiles_cols = get_compile_time_arg_val(2);
-    constexpr uint32_t do_gamma = get_compile_time_arg_val(3);
-    constexpr uint32_t do_beta = get_compile_time_arg_val(4);
-    constexpr bool FLOAT32_DTYPE = get_compile_time_arg_val(5) == 1;
-    constexpr bool FLOAT32_REDUCTION = get_compile_time_arg_val(6) == 1;
-    constexpr bool LEGACY_RSQRT = get_compile_time_arg_val(7) == 1;
+    constexpr uint32_t W = get_compile_time_arg_val(1);
+    DPRINT << "W: " << W << ENDL();
+    constexpr uint32_t blk = get_compile_time_arg_val(2);
+    constexpr uint32_t stats_tiles_cols = get_compile_time_arg_val(3) / 2;
+    constexpr uint32_t do_gamma = get_compile_time_arg_val(4);
+    constexpr uint32_t do_beta = get_compile_time_arg_val(5);
+    constexpr bool FLOAT32_DTYPE = get_compile_time_arg_val(6) == 1;
 
     constexpr uint32_t onetile = 1;
 
@@ -59,13 +50,17 @@ void MAIN {
     constexpr uint32_t cb_x_normed = tt::CBIndex::c_12;  // (x - E(x)) * 1/sqrt(var+eps) or x * 1/sqrt(E(x**2) + eps)
 
     constexpr uint32_t cb_var = tt::CBIndex::c_8;  // E(x**2) - E(x)**2 or E(x**2)
-
+#ifndef RMSNORM
     // Layernorm-specific CBs
     constexpr uint32_t cb_mean_squared = tt::CBIndex::c_7;   // E(x)**2
     constexpr uint32_t cb_x_minus_mean = tt::CBIndex::c_11;  // x - E(x)
 
     constexpr uint32_t cb_norm_x_input = cb_x_minus_mean;
     constexpr uint32_t stats_tile_stride = 2;
+#else
+    constexpr uint32_t cb_norm_x_input = cb_inp;
+    constexpr uint32_t stats_tile_stride = 1;
+#endif
 
     constexpr uint32_t cb_gamma = tt::CBIndex::c_2;
     constexpr uint32_t cb_beta = tt::CBIndex::c_3;
@@ -74,79 +69,28 @@ void MAIN {
         cb_times_gamma_out = tt::CBIndex::c_13;
     }
 
-    DPRINT << "pre_bin init " << ENDL();
     binary_op_init_common(cb_inp, cb_inp, cb_stats_reduced);
 
     cb_wait_front(cb_reduce, 1);  // comes from the reader
     cb_wait_front(cb_eps, 1);     // comes from the reader
-    DPRINT << "post_bin init " << ENDL();
 
     for (uint32_t ncht = 0; ncht < NCHt; ncht++) {
         constexpr int onetile = 1;
         constexpr int dst0 = 0;
 
-        reconfig_data_format(cb_stats, cb_reduce);
-        pack_reconfig_data_format(cb_stats_reduced);
-
-        /*
-         * Reduce stats input.
-         * cb_stats = [sum(x0**2), sum(x0), sum(x1**2), sum(x1), ...]
-         * RMSNorm packs mean(x**2) into cb_var. Layernorm just uses cb_stats_reduced.
-         */
-        reduce_init<REDUCE_OP, REDUCE_DIM, FLOAT32_REDUCTION>(cb_stats, cb_reduce, cb_stats_reduced);
-        cb_wait_front(cb_stats, stats_tiles_cols);
-        cb_reserve_back(cb_stats_reduced, stats_tile_stride);
-
-        ACQ();
-        // Reduce sum(x**2) first
-        for (uint32_t i = 0; i < stats_tiles_cols; i += stats_tile_stride) {
-            reduce_tile<REDUCE_OP, REDUCE_DIM, FLOAT32_REDUCTION>(cb_stats, cb_reduce, i, 0, 0);
-        }
-        pack_tile(0, cb_stats_reduced);
-
-        // Reduce sum(x) next
-        for (uint32_t i = 1; i < stats_tiles_cols; i += stats_tile_stride) {
-            reduce_tile<REDUCE_OP, REDUCE_DIM, FLOAT32_REDUCTION>(cb_stats, cb_reduce, i, 0, 1);
-        }
-        pack_tile(1, cb_stats_reduced);
-
-        REL();
-        cb_push_back(cb_stats_reduced, stats_tile_stride);
-        cb_pop_front(cb_stats, stats_tiles_cols);
-
-        reduce_uninit();
-
-        /*
-         * E[x]**2
-         */
-        reconfig_data_format(cb_stats_reduced, cb_stats_reduced);
-        pack_reconfig_data_format(cb_mean_squared);
-        mul_tiles_init(cb_stats_reduced, cb_stats_reduced);
-        cb_reserve_back(cb_mean_squared, onetile);
-        cb_wait_front(cb_stats_reduced, stats_tile_stride);
-        ACQ();
-        mul_tiles(cb_stats_reduced, cb_stats_reduced, 1, 1, 0);
-        pack_tile(0, cb_mean_squared);
-        REL();
-
-        cb_push_back(cb_mean_squared, 1);
-
-        /*
-         * E[x**2] - E[x]**2
-         */
-        reconfig_data_format(cb_stats_reduced, cb_mean_squared);
-        pack_reconfig_data_format(cb_var);
-        sub_tiles_init(cb_stats_reduced, cb_mean_squared);
-
-        cb_reserve_back(cb_var, onetile);
-        cb_wait_front(cb_mean_squared, 1);
-        ACQ();
-        sub_tiles(cb_stats_reduced, cb_mean_squared, 0, 0, 0);
-        pack_tile(0, cb_var);
-        REL();
-        cb_push_back(cb_var, 1);
-        cb_pop_front(cb_mean_squared, 1);
-
+        // combine_welford_partials()
+        DPRINT << "pre_combine " << ENDL();
+        norm::kernel_util::compute::combine_welford_partials(
+            cb_stats,
+            cb_stats_reduced,
+            stats_tiles_cols,
+            [&](uint32_t b) { return (static_cast<float>(W)); },
+            norm::kernel_util::compute::RSqrtPolicy{false, 0});
+        cb_push_back(cb_stats_reduced, 2);
+        cb_wait_front(cb_stats_reduced, 2);
+        UNPACK(tt::compute::common::print_full_tile(cb_stats_reduced, 0, true));
+        UNPACK(tt::compute::common::print_full_tile(cb_stats_reduced, 1, true));
+        DPRINT << "here " << ENDL();
         /*
          * x - E[x]
          */
@@ -166,10 +110,6 @@ void MAIN {
             cb_push_back(cb_x_minus_mean, blk);
             cb_pop_front(cb_inp, blk);
         }
-
-        // free up CBs
-        cb_pop_front(cb_stats_reduced, stats_tile_stride);
-
         /*
          * 1/sqrt(var + eps)
          */
@@ -180,9 +120,9 @@ void MAIN {
 
         add_tiles_init(cb_stats_reduced, cb_eps);
         ACQ();
-        add_tiles(cb_var, cb_eps, 0, 0, 0);
-        rsqrt_tile_init<LEGACY_RSQRT>();
-        rsqrt_tile<LEGACY_RSQRT>(0);
+        add_tiles(cb_stats_reduced, cb_eps, 1, 0, 0);
+        rsqrt_tile_init<true>();
+        rsqrt_tile<true>(0);
         pack_tile(0, cb_recip_sqrt_var);
         REL();
         cb_push_back(cb_recip_sqrt_var, 1);
