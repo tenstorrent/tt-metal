1359b942c60567cfaca10fd5d1ca02074285254c
handrewsTT <handrews@tenstorrent.com>
2025-10-22 14:17:33 +0000

    remove attn masks

--- models/tt_transformers/tt/generator.py
+++ models/tt_transformers/tt/generator.py
@@ -311,11 +311,6 @@ class Generator:
             tt_current_pos.append(tt_current_pos_i)
             tt_rot_mat_idxs.append(tt_rot_mat_idxs_i)
             tt_page_table.append(tt_page_table_i)
-            if (
-                hasattr(self.model[i], "device_decode_sliding_mask")
-                and self.model[i].device_decode_sliding_mask is not None
-            ):
-                self.model[i].update_attention_masks(current_pos[i])
 
         for i in range(self.data_parallel):
             user_kv_cache = kv_cache[i] if kv_cache is not None else None
@@ -416,13 +411,6 @@ class Generator:
                     host_tensors=host_inputs_i,
                     device_tensors=self.trace_inputs_text[i],
                 )
-
-        for i in range(self.data_parallel):
-            if (
-                hasattr(self.model[i], "device_decode_sliding_mask")
-                and self.model[i].device_decode_sliding_mask is not None
-            ):
-                self.model[i].update_attention_masks(current_pos[i])
 
         for i, trace_id in self.trace_ids_text.items():
             ttnn.execute_trace(self.model_args[i].mesh_device, trace_id, cq_id=0, blocking=False)
