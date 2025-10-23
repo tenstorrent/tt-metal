fix attention test default scaling

models/tt_transformers/tests/test_attention.py
@@ -135,13 +135,14 @@ def test_attention_inference(
         configuration=model_args,
         paged_attention_config=paged_attention_config,
     )
+
     cos, sin = precompute_freqs(
         model_args.head_dim,
         model_args.max_seq_len * 2,
         model_args.rope_theta,
         model_args.rope_scaling.factor if model_args.rope_scaling else None,
         model_args.rope_scaling.original_max_position_embeddings if model_args.rope_scaling else None,
-        model_args.rope_scaling.rope_type.value if model_args.rope_scaling else None,
+        model_args.rope_scaling.rope_type.value if model_args.rope_scaling else "llama3",
     )
     freqs_cis = torch.complex(cos, sin)
 