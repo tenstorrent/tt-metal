cbc221174883adb876db0d2d64891504e9935a9b
Stephen Osborne <sosborne@tenstorrent.com>
2025-10-23 09:16:07 -0700

    fix linting pre-commit hook test error

--- models/experimental/tt_dit/pipelines/mochi/pipeline_mochi.py
+++ models/experimental/tt_dit/pipelines/mochi/pipeline_mochi.py
@@ -655,7 +655,9 @@ class MochiPipeline(DiffusionPipeline):
                 cache_dict = load_cache_dict(cache_path)
                 self.transformer.from_cached_state_dict(cache_dict)
             else:
-                raise NotImplementedError("use_cache must be True when DiT model reloading is enabled (reload_dit_model=True)")
+                raise NotImplementedError(
+                    "use_cache must be True when DiT model reloading is enabled (reload_dit_model=True)"
+                )
             logger.info("Recreated MochiTransformer3DModel")
 
         # 4. Prepare latent variables
