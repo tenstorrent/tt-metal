e0ac441cb580a35be17845d27c5a35fe887ed857
mgajewskiTT <mgajewski@tenstorrent.com>
2025-10-23 08:36:13 +0000

    31033 - Outpus removed

---
+++ ttnn/tutorials/basic_python/ttnn_basic_matrix_multiplication.py
@@ -0,0 +1,50 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+# SPDX-License-Identifier: Apache-2.0
+
+
+import os
+
+import ttnn
+
+
+def main():
+    device_id = 0
+    device = ttnn.open_device(device_id=device_id)
+
+    device.enable_program_cache()
+
+    m = 1024
+    k = 1024
+    n = 1024
+
+    a = ttnn.rand((m, k), dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+    b = ttnn.rand((k, n), dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+
+    output = a @ b
+
+    output = a @ b
+
+    print(output.layout)
+
+    output = ttnn.to_layout(output, ttnn.ROW_MAJOR_LAYOUT)
+
+    print("Printing ttnn tensor")
+    print(f"shape: {output.shape}")
+    print(f"chunk of a tensor:\n{output[:1, :32]}")
+
+    a = ttnn.rand(
+        (m, k), dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT, memory_config=ttnn.L1_MEMORY_CONFIG
+    )
+    b = ttnn.rand(
+        (k, n), dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT, memory_config=ttnn.L1_MEMORY_CONFIG
+    )
+
+    output = ttnn.matmul(a, b, memory_config=ttnn.L1_MEMORY_CONFIG, core_grid=ttnn.CoreGrid(y=8, x=8))
+
+    output = ttnn.matmul(a, b, memory_config=ttnn.L1_MEMORY_CONFIG, core_grid=ttnn.CoreGrid(y=8, x=8))
+
+    ttnn.close_device(device)
+
+
+if __name__ == "__main__":
+    main()
