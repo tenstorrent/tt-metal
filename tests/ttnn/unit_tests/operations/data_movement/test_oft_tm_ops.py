f59f14b15ae7e0e8a0a152cc481c65acb7afe395
Sheran Cardoza <scardoza@tenstorrent.com>
2025-10-23 19:18:48 +0000

    fix permute tests

--- tests/ttnn/unit_tests/operations/data_movement/test_oft_tm_ops.py
+++ tests/ttnn/unit_tests/operations/data_movement/test_oft_tm_ops.py
@@ -11,6 +11,13 @@ from tests.ttnn.utils_for_testing import assert_with_pcc, assert_equal
 from models.common.utility_functions import is_blackhole
 
 
+# constants
+RM = ttnn.ROW_MAJOR_LAYOUT
+TILE = ttnn.TILE_LAYOUT
+DRAM = ttnn.DRAM_MEMORY_CONFIG
+L1 = ttnn.L1_MEMORY_CONFIG
+
+
 def skip_if_not_20_cores(device):
     compute_grid = device.compute_with_storage_grid_size()
     if compute_grid.x != 5 or compute_grid.y != 4:
@@ -31,28 +38,48 @@ def random_torch_tensor(dtype, shape):
 
 
 @pytest.mark.parametrize(
-    "shape, perm, dtype, layout",
+    "shape, perm, dtype, layout, in_mem_config, out_mem_config",
     [
-        ([1, 48, 160, 256], [2, 1, 0, 3], ttnn.uint32, ttnn.TILE_LAYOUT),
-        ([160, 48, 32, 256], [2, 1, 0, 3], ttnn.uint32, ttnn.TILE_LAYOUT),
-        ([1, 24, 80, 256], [2, 1, 0, 3], ttnn.uint32, ttnn.TILE_LAYOUT),
-        ([80, 24, 32, 256], [2, 1, 0, 3], ttnn.uint32, ttnn.TILE_LAYOUT),
-        ([1, 12, 40, 256], [2, 1, 0, 3], ttnn.uint32, ttnn.TILE_LAYOUT),
-        ([40, 12, 1, 256], [2, 1, 0, 3], ttnn.uint32, ttnn.TILE_LAYOUT),
-        ([1, 1, 25281, 9], [0, 3, 1, 2], ttnn.bfloat16, ttnn.ROW_MAJOR_LAYOUT),
-        ([1, 3, 159, 160], [0, 2, 3, 1], ttnn.bfloat16, ttnn.TILE_LAYOUT),
-        ([1, 1, 159, 160], [0, 2, 3, 1], ttnn.bfloat16, ttnn.TILE_LAYOUT),
+        ([1, 48, 160, 256], [2, 1, 0, 3], ttnn.uint32, TILE, DRAM, DRAM),
+        ([160, 48, 1, 256], [2, 1, 0, 3], ttnn.uint32, TILE, DRAM, DRAM),
+        ([1, 24, 80, 256], [2, 1, 0, 3], ttnn.uint32, TILE, DRAM, DRAM),
+        ([80, 24, 1, 256], [2, 1, 0, 3], ttnn.uint32, TILE, DRAM, DRAM),
+        ([1, 12, 40, 256], [2, 1, 0, 3], ttnn.uint32, TILE, DRAM, DRAM),
+        ([40, 12, 1, 256], [2, 1, 0, 3], ttnn.uint32, TILE, DRAM, DRAM),
+        ([1, 1, 25281, 9], [0, 3, 1, 2], ttnn.bfloat16, RM, DRAM, L1),
+        ([1, 3, 159, 160], [0, 2, 3, 1], ttnn.bfloat16, TILE, L1, L1),
+        ([1, 1, 159, 160], [0, 2, 3, 1], ttnn.bfloat16, TILE, L1, L1),
     ],
 )
-def test_permute(device, shape, perm, dtype, layout):
+@pytest.mark.parametrize("device_params", [{"trace_region_size": 100000}], indirect=True)
+def test_permute(device, shape, perm, dtype, layout, in_mem_config, out_mem_config):
     skip_if_not_20_cores(device)
 
     torch.manual_seed(2005)
     torch_input_tensor = random_torch_tensor(dtype, shape)
     torch_output_tensor = torch.permute(torch_input_tensor, perm)
 
-    input_tensor = ttnn.from_torch(torch_input_tensor, layout=layout, dtype=dtype, device=device)
-    output_tensor = ttnn.permute(input_tensor, perm)
+    input_tensor = ttnn.from_torch(
+        torch_input_tensor, layout=layout, dtype=dtype, device=device, memory_config=in_mem_config
+    )
+    output_tensor = ttnn.permute(input_tensor, perm, memory_config=out_mem_config)
     output_tensor = ttnn.to_torch(output_tensor)
 
     assert_equal(torch_output_tensor, output_tensor)
+
+    ## Compile run
+    # output_tensor = ttnn.permute(input_tensor, perm)
+    # ttnn.synchronize_device(device)
+
+    ## Capture trace
+    # num_iters = 20
+    # trace_id = ttnn.begin_trace_capture(device, cq_id=0)
+    # for _ in range(num_iters):
+    #    output_tensor = ttnn.permute(input_tensor, perm)
+    # ttnn.end_trace_capture(device, trace_id)
+    # ttnn.synchronize_device(device)
+
+    ## Run the trace
+    # ttnn.execute_trace(device, trace_id, cq_id=0, blocking=False)
+    # ttnn.release_trace(device, trace_id)
+    # ttnn.synchronize_device(device)
