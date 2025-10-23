a3599694a19ca5e31206cd5eb23f2f74cd24e32f
Siddharth Sundaram <ssundaram@tenstorrent.com>
2025-10-23 16:48:43 +0000

    updated count limits

--- tests/tt_metal/tools/profiler/test_device_profiler.py
+++ tests/tt_metal/tools/profiler/test_device_profiler.py
@@ -408,7 +408,7 @@ def test_dispatch_cores():
 # Eth dispatch will be deprecated
 @skip_for_blackhole()
 def test_ethernet_dispatch_cores():
-    REF_COUNT_DICT = {"Ethernet CQ Dispatch": [590, 840, 1430, 1660, 2320], "Ethernet CQ Prefetch": [572, 4030]}
+    REF_COUNT_DICT = {"Ethernet CQ Dispatch": [590, 840, 1430, 1660, 3769], "Ethernet CQ Prefetch": [1058, 4030]}
     devicesData = run_device_profiler_test(
         testName=f"pytest {TRACY_TESTS_DIR}/test_dispatch_profiler.py::test_with_ops -k DispatchCoreType.ETH",
         setupAutoExtract=True,
