5d2ce781a92fc2ec58c305a2eee717b3d03ebb38
Sheran Cardoza <scardoza@tenstorrent.com>
2025-10-23 17:36:14 +0000

    add pytest for permute ops used by oft model

---
+++ tests/ttnn/unit_tests/operations/data_movement/test_oft_tm_ops.py
@@ -0,0 +1,58 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import torch
+import ttnn
+import itertools
+
+from tests.ttnn.utils_for_testing import assert_with_pcc, assert_equal
+from models.common.utility_functions import is_blackhole
+
+
+def skip_if_not_20_cores(device):
+    compute_grid = device.compute_with_storage_grid_size()
+    if compute_grid.x != 5 or compute_grid.y != 4:
+        pytest.skip(
+            f"This test is intended to run only with 20 cores. Core grid [{compute_grid.x},{compute_grid.y}] must be [5, 4]."
+        )
+
+
+def random_torch_tensor(dtype, shape):
+    if dtype == ttnn.int32:
+        return torch.randint(-(2**31), 2**31, shape, dtype=torch.int32)
+    if dtype == ttnn.uint32:
+        return torch.randint(0, 2**31, shape, dtype=torch.int32)
+    if dtype == ttnn.float32:
+        return torch.rand(shape, dtype=torch.float32)
+    if dtype == ttnn.bfloat16:
+        return torch.rand(shape, dtype=torch.bfloat16)
+
+
+@pytest.mark.parametrize(
+    "shape, perm, dtype, layout",
+    [
+        ([1, 48, 160, 256], [2, 1, 0, 3], ttnn.uint32, ttnn.TILE_LAYOUT),
+        ([160, 48, 32, 256], [2, 1, 0, 3], ttnn.uint32, ttnn.TILE_LAYOUT),
+        ([1, 24, 80, 256], [2, 1, 0, 3], ttnn.uint32, ttnn.TILE_LAYOUT),
+        ([80, 24, 32, 256], [2, 1, 0, 3], ttnn.uint32, ttnn.TILE_LAYOUT),
+        ([1, 12, 40, 256], [2, 1, 0, 3], ttnn.uint32, ttnn.TILE_LAYOUT),
+        ([40, 12, 1, 256], [2, 1, 0, 3], ttnn.uint32, ttnn.TILE_LAYOUT),
+        ([1, 1, 25281, 9], [0, 3, 1, 2], ttnn.bfloat16, ttnn.ROW_MAJOR_LAYOUT),
+        ([1, 3, 159, 160], [0, 2, 3, 1], ttnn.bfloat16, ttnn.TILE_LAYOUT),
+        ([1, 1, 159, 160], [0, 2, 3, 1], ttnn.bfloat16, ttnn.TILE_LAYOUT),
+    ],
+)
+def test_permute(device, shape, perm, dtype, layout):
+    skip_if_not_20_cores(device)
+
+    torch.manual_seed(2005)
+    torch_input_tensor = random_torch_tensor(dtype, shape)
+    torch_output_tensor = torch.permute(torch_input_tensor, perm)
+
+    input_tensor = ttnn.from_torch(torch_input_tensor, layout=layout, dtype=dtype, device=device)
+    output_tensor = ttnn.permute(input_tensor, perm)
+    output_tensor = ttnn.to_torch(output_tensor)
+
+    assert_equal(torch_output_tensor, output_tensor)
