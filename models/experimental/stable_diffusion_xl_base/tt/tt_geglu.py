0ce3454eb0a79160c52dc4d0662a44f946f79ff3
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2025-10-24 11:35:45 +0000

    transformer model

--- models/experimental/stable_diffusion_xl_base/tt/tt_geglu.py
+++ models/experimental/stable_diffusion_xl_base/tt/tt_geglu.py
@@ -9,7 +9,7 @@ from models.experimental.stable_diffusion_xl_base.tt.sdxl_utility import prepare
 
 
 class TtGEGLU(LightweightModule):
-    def __init__(self, device, state_dict, module_path, model_config=None):
+    def __init__(self, device, state_dict, module_path, model_config):
         super().__init__()
 
         self.device = device
@@ -21,20 +21,19 @@ class TtGEGLU(LightweightModule):
         w1 = w1.unsqueeze(0).unsqueeze(0)  # [1, 1, out_dim // 2, in_dim]
         w2 = w2.unsqueeze(0).unsqueeze(0)  # same
 
-        ff_weights_dtype = model_config.ff_weights_dtype if model_config is not None else ttnn.bfloat8_b
+        ff_weights_dtype = model_config.ff_weights_dtype
         self.tt_weights_1, self.tt_bias_1 = prepare_linear_params(device, w1, b1, ff_weights_dtype)
         self.tt_weights_2, self.tt_bias_2 = prepare_linear_params(device, w2, b2, ff_weights_dtype)
 
-        if model_config is not None:
-            self.program_config = model_config.get_matmul_config(matmul_path=f"{module_path}.proj.split")
-            self.program_config_gelu = model_config.get_matmul_config(matmul_path=f"{module_path}.proj.split.gelu")
-            self.compute_config = model_config.get_mm_compute_config(f"{module_path}.proj")
+        self.program_config = model_config.get_matmul_config(matmul_path=f"{module_path}.proj.split")
+        self.program_config_gelu = model_config.get_matmul_config(matmul_path=f"{module_path}.proj.split.gelu")
+        self.compute_config = model_config.get_mm_compute_config(f"{module_path}.proj")
 
-            assert self.program_config is not None, "Program config for split weights is None"
-            assert self.program_config_gelu is not None, "Program config for split weights with GELU is None"
+        # assert self.program_config is not None, "Program config for split weights is None"
+        # assert self.program_config_gelu is not None, "Program config for split weights with GELU is None"
 
     def forward(self, input_tensor):
-        if hasattr(self, "program_config"):
+        if self.program_config is not None:
             if input_tensor.shape[2] == 4096:
                 # due to block sharded mm constraints, if we block shard the input tensor, we can only run it on 56 cores
                 # hence using L1 memory config instead
@@ -54,19 +53,19 @@ class TtGEGLU(LightweightModule):
             input_tensor,
             self.tt_weights_1,
             bias=self.tt_bias_1,
-            memory_config=ttnn.L1_BLOCK_SHARDED_MEMORY_CONFIG if hasattr(self, "program_config") else None,
-            program_config=self.program_config if hasattr(self, "program_config") else None,
-            compute_kernel_config=self.compute_config if hasattr(self, "compute_config") else None,
+            memory_config=ttnn.L1_BLOCK_SHARDED_MEMORY_CONFIG if self.program_config is not None else None,
+            program_config=self.program_config,
+            compute_kernel_config=self.compute_config,
         )
         gate = ttnn.linear(
             input_tensor,
             self.tt_weights_2,
             bias=self.tt_bias_2,
-            memory_config=ttnn.L1_BLOCK_SHARDED_MEMORY_CONFIG if hasattr(self, "program_config_gelu") else None,
-            program_config=self.program_config_gelu if hasattr(self, "program_config_gelu") else None,
-            compute_kernel_config=self.compute_config if hasattr(self, "compute_config") else None,
+            memory_config=ttnn.L1_BLOCK_SHARDED_MEMORY_CONFIG if self.program_config_gelu is not None else None,
+            program_config=self.program_config_gelu,
+            compute_kernel_config=self.compute_config,
         )
-        if not hasattr(self, "program_config"):
+        if self.program_config_gelu is None:
             gate = ttnn.gelu(gate)
         ttnn.deallocate(input_tensor)
         hidden_states = ttnn.mul_(hidden_states, gate, use_legacy=False)
