7258afdbe7c6ea6a0b80da5e03f2d779623edb97
John Bauman <jbauman@tenstorrent.com>
2025-10-24 20:47:25 +0000

    Remove CommandQueue::enqueue_program

--- tt_metal/impl/dispatch/host_runtime_commands.hpp
+++ tt_metal/impl/dispatch/host_runtime_commands.hpp
@@ -5,26 +5,11 @@
 #pragma once
 
 #include <stdint.h>
-#include <algorithm>
-#include <chrono>
-#include <condition_variable>
-#include <fstream>
-#include <memory>
-#include <span>
-#include <thread>
-#include <utility>
-#include <vector>
+// pruned unused includes
 
-#include "core_coord.hpp"
-#include "device_command.hpp"
-#include "env_lib.hpp"
-#include "multi_producer_single_consumer_queue.hpp"
-#include "dispatch_settings.hpp"
+// pruned unused includes
 #include "tt-metalium/program.hpp"
-#include "sub_device_types.hpp"
-#include "trace/trace_buffer.hpp"
-#include "tt_metal/impl/program/program_command_sequence.hpp"
-#include "worker_config_buffer.hpp"
+// pruned unused includes
 #include "program/dispatch.hpp"
 
 #include <umd/device/types/core_coordinates.hpp>
@@ -52,7 +37,6 @@ enum class EnqueueCommandType {
     GET_BUF_ADDR,
     ADD_BUFFER_TO_PROGRAM,
     SET_RUNTIME_ARGS,
-    ENQUEUE_PROGRAM,
     ENQUEUE_RECORD_EVENT,
     ENQUEUE_WAIT_FOR_EVENT,
     FINISH,
@@ -67,45 +51,6 @@ public:
     virtual ~Command() = default;
     virtual void process() {};
     virtual EnqueueCommandType type() = 0;
-};
-
-class EnqueueProgramCommand : public Command {
-private:
-    uint32_t command_queue_id;
-    IDevice* device;
-    Program& program;
-    SystemMemoryManager& manager;
-    WorkerConfigBufferMgr& config_buffer_mgr;
-    CoreCoord dispatch_core;
-    CoreType dispatch_core_type;
-    uint32_t expected_num_workers_completed;
-    uint32_t packed_write_max_unicast_sub_cmds;
-    uint32_t multicast_cores_launch_message_wptr = 0;
-    uint32_t unicast_cores_launch_message_wptr = 0;
-    // TODO: There will be multiple ids once programs support spanning multiple sub_devices
-    SubDeviceId sub_device_id = SubDeviceId{0};
-    program_dispatch::ProgramDispatchMetadata& dispatch_metadata;
-
-public:
-    EnqueueProgramCommand(
-        uint32_t command_queue_id,
-        IDevice* device,
-        NOC noc_index,
-        Program& program,
-        CoreCoord& dispatch_core,
-        SystemMemoryManager& manager,
-        WorkerConfigBufferMgr& config_buffer_mgr,
-        uint32_t expected_num_workers_completed,
-        uint32_t multicast_cores_launch_message_wptr,
-        uint32_t unicast_cores_launch_message_wptr,
-        SubDeviceId sub_device_id,
-        program_dispatch::ProgramDispatchMetadata& dispatch_md);
-
-    void process() override;
-
-    EnqueueCommandType type() override { return EnqueueCommandType::ENQUEUE_PROGRAM; }
-
-    constexpr bool has_side_effects() { return true; }
 };
 
 class EnqueueTerminateCommand : public Command {
