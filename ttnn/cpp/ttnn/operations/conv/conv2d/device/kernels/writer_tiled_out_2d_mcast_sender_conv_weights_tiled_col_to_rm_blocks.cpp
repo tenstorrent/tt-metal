a64ab3b288022cd4ee1faf7eedaa8e79abdb0cc6
Sofija Jovic <sjovic@tenstorrent.com>
2025-10-24 08:20:51 +0000

    PR feedback

--- ttnn/cpp/ttnn/operations/conv/conv2d/device/kernels/writer_tiled_out_2d_mcast_sender_conv_weights_tiled_col_to_rm_blocks.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/device/kernels/writer_tiled_out_2d_mcast_sender_conv_weights_tiled_col_to_rm_blocks.cpp
@@ -128,7 +128,7 @@ void kernel_main() {
 
     // read in bias if enabled (done only once for all batches)
     constexpr uint32_t bias_pagesize =
-        fuse_bias ? get_tile_size(bias_cb_id) : 2048;  // dummy value in case bias is not enabled
+        fuse_bias ? get_tile_size(bias_cb_id) : 0;  // dummy value in case bias is not enabled
     const auto s_bias = TensorAccessor(s_bias_args, bias_addr, bias_pagesize);
 
     bool load_bias = true;
