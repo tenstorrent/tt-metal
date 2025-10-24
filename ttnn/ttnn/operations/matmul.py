0a492a0fa8e49fe3db26014c2bfacff1c911c37b
Edwin Lee <edwinlee@tenstorrent.com>
2025-10-24 15:44:16 +0000

    Cleanup MM golden map

--- ttnn/ttnn/operations/matmul.py
+++ ttnn/ttnn/operations/matmul.py
@@ -26,13 +26,12 @@ def _get_golden_activation_function(activation):
         ttnn.UnaryOpType.SILU: torch.nn.functional.silu,
         ttnn.UnaryOpType.MISH: torch.nn.functional.mish,
         ttnn.UnaryOpType.SIGMOID: torch.nn.functional.sigmoid,
+        ttnn.UnaryOpType.HARDSIGMOID: torch.nn.functional.hardsigmoid,
         ttnn.UnaryOpType.TANH: torch.nn.functional.tanh,
         ttnn.UnaryOpType.LOG: torch.log,
         ttnn.UnaryOpType.SOFTPLUS: torch.nn.functional.softplus,
         ttnn.UnaryOpType.GELU: torch.nn.functional.gelu,
         ttnn.UnaryOpType.SQRT: torch.sqrt,
-        ttnn.UnaryOpType.HARDSIGMOID: torch.nn.functional.hardsigmoid,
-        ttnn.UnaryOpType.MISH: torch.nn.functional.mish,
     }
 
     if activation in golden_activations_map:
