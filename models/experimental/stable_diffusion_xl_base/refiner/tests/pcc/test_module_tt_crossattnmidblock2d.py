dd458ae909b44da423620e657b2b6ce59932a755
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2025-10-24 13:52:54 +0000

    mid block

--- models/experimental/stable_diffusion_xl_base/refiner/tests/pcc/test_module_tt_crossattnmidblock2d.py
+++ models/experimental/stable_diffusion_xl_base/refiner/tests/pcc/test_module_tt_crossattnmidblock2d.py
@@ -1,13 +1,12 @@
 # SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 
 # SPDX-License-Identifier: Apache-2.0
-
 import gc
 from loguru import logger
 import torch
 import pytest
 import ttnn
-from models.experimental.stable_diffusion_xl_base.tt.tt_resnetblock2d import TtResnetBlock2D
+from models.experimental.stable_diffusion_xl_base.tt.tt_crossattnmidblock2d import TtUNetMidBlock2DCrossAttn
 from models.experimental.stable_diffusion_xl_base.refiner.tt.model_configs import RefinerModelOptimisations
 from diffusers import UNet2DConditionModel
 from tests.ttnn.utils_for_testing import assert_with_pcc
@@ -16,28 +15,20 @@ from models.experimental.stable_diffusion_xl_base.tests.test_common import SDXL_
 
 
 @pytest.mark.parametrize(
-    "input_shape, temb_shape, down_block_id, resnet_id, conv_shortcut, split_in, block, pcc",
+    "input_shape, temb_shape, encoder_shape, query_dim, num_attn_heads, out_dim",
     [
-        ((1, 384, 128, 128), (1, 1536), 0, 0, False, 1, "down_blocks", 0.999),
-        ((1, 384, 64, 64), (1, 1536), 1, 0, True, 1, "down_blocks", 0.999),
-        ((1, 768, 64, 64), (1, 1536), 1, 1, False, 1, "down_blocks", 0.999),
-        ((1, 768, 32, 32), (1, 1536), 2, 0, True, 1, "down_blocks", 0.999),
-        ((1, 1536, 32, 32), (1, 1536), 2, 1, False, 1, "down_blocks", 0.999),
-        ((1, 1536, 16, 16), (1, 1536), 3, 0, False, 1, "down_blocks", 0.999),
-        ((1, 1536, 16, 16), (1, 1536), -1, 0, False, 1, "mid_block", 0.997),
+        ((1, 1536, 16, 16), (1, 1536), (1, 77, 1280), 1536, 24, 1536),
     ],
 )
 @pytest.mark.parametrize("device_params", [{"l1_small_size": SDXL_L1_SMALL_SIZE}], indirect=True)
-def test_resnetblock2d(
+def test_crossattnmid(
     device,
-    temb_shape,
     input_shape,
-    down_block_id,
-    resnet_id,
-    conv_shortcut,
-    split_in,
-    block,
-    pcc,
+    temb_shape,
+    encoder_shape,
+    query_dim,
+    num_attn_heads,
+    out_dim,
     debug_mode,
     is_ci_env,
     reset_seeds,
@@ -52,38 +43,33 @@ def test_resnetblock2d(
     unet.eval()
     state_dict = unet.state_dict()
 
-    if block == "down_blocks":
-        torch_resnet = unet.down_blocks[down_block_id].resnets[resnet_id]
-        block = f"down_blocks.{down_block_id}"
-    elif block == "mid_block":
-        torch_resnet = unet.mid_block.resnets[resnet_id]
-    elif block == "up_blocks":
-        torch_resnet = unet.up_blocks[down_block_id].resnets[resnet_id]
-        block = f"up_blocks.{down_block_id}"
-    else:
-        assert "Incorrect block name"
+    torch_crosattn = unet.mid_block
 
     model_config = RefinerModelOptimisations()
-    tt_resnet = TtResnetBlock2D(
+    tt_crosattn = TtUNetMidBlock2DCrossAttn(
         device,
         state_dict,
-        f"{block}.resnets.{resnet_id}",
+        f"mid_block",
         model_config,
-        conv_shortcut,
-        split_in,
+        query_dim,
+        num_attn_heads,
+        out_dim,
         debug_mode=debug_mode,
     )
-
     torch_input_tensor = torch_random(input_shape, -0.1, 0.1, dtype=torch.float32)
     torch_temb_tensor = torch_random(temb_shape, -0.1, 0.1, dtype=torch.float32)
-    torch_output_tensor = torch_resnet(torch_input_tensor, torch_temb_tensor)
+    torch_encoder_tensor = torch_random(encoder_shape, -0.1, 0.1, dtype=torch.float32)
+
+    torch_output_tensor = torch_crosattn(
+        torch_input_tensor, temb=torch_temb_tensor, encoder_hidden_states=torch_encoder_tensor
+    )
 
     ttnn_input_tensor = ttnn.from_torch(
         torch_input_tensor,
         dtype=ttnn.bfloat16,
         device=device,
         layout=ttnn.TILE_LAYOUT,
-        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        memory_config=ttnn.L1_MEMORY_CONFIG,
     )
     B, C, H, W = list(ttnn_input_tensor.shape)
 
@@ -97,14 +83,23 @@ def test_resnetblock2d(
         layout=ttnn.TILE_LAYOUT,
         memory_config=ttnn.L1_MEMORY_CONFIG,
     )
-    ttnn_output_tensor, output_shape = tt_resnet.forward(ttnn_input_tensor, ttnn_temb_tensor, [B, C, H, W])
+    ttnn_encoder_tensor = ttnn.from_torch(
+        torch_encoder_tensor,
+        dtype=ttnn.bfloat16,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.L1_MEMORY_CONFIG,
+    )
+    ttnn_output_tensor, output_shape = tt_crosattn.forward(
+        ttnn_input_tensor, [B, C, H, W], temb=ttnn_temb_tensor, encoder_hidden_states=ttnn_encoder_tensor
+    )
 
     output_tensor = ttnn.to_torch(ttnn_output_tensor)
-    output_tensor = output_tensor.reshape(input_shape[0], output_shape[1], output_shape[2], output_shape[0])
+    output_tensor = output_tensor.reshape(B, output_shape[1], output_shape[2], output_shape[0])
     output_tensor = torch.permute(output_tensor, (0, 3, 1, 2))
 
-    del unet
+    del unet, tt_crosattn
     gc.collect()
 
-    _, pcc_message = assert_with_pcc(torch_output_tensor, output_tensor, pcc)
-    logger.info(f"PCC is {pcc_message}")
+    _, pcc_message = assert_with_pcc(torch_output_tensor, output_tensor, 0.991)
+    logger.info(f"PCC is: {pcc_message}")
