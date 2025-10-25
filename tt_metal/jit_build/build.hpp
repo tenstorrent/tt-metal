5ee6dd4f13ba9a7660be646e7110d87c2d3720e5
Rui Zhang <ruizhang@tenstorrent.com>
2025-10-25 23:09:07 +0000

    Fix profiler tests

--- tt_metal/jit_build/build.hpp
+++ tt_metal/jit_build/build.hpp
@@ -116,6 +116,7 @@ protected:
     // Used when JitBuildSettings is not provided
     std::string default_linker_opt_level_;
 
+    bool need_compile(const std::string& out_dir, const std::string& obj) const;
     size_t compile(const std::string& log_file, const std::string& out_path, const JitBuildSettings* settings) const;
     void compile_one(
         const std::string& log_file,
