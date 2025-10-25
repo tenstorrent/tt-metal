06dd0ab560026b660dcafa2e9621e3af3185b7ae
Andrew Fuller <afuller@tenstorrent.com>
2025-10-25 15:31:45 +0000

    fixup! fixup! fixup! fixup! fafo

--- ttnn/cpp/ttnn-nanobind/pytensor.cpp
+++ ttnn/cpp/ttnn-nanobind/pytensor.cpp
@@ -665,9 +665,8 @@ nb::object convert_tt_tensor_to_torch_tensor(const RowMajorHostBuffer& row_major
             return pytorch_empty(row_major_host_buffer.shape, nb::arg("dtype") = torch_dtype);
         }
         auto view = row_major_host_buffer.buffer.view_bytes();
-        nb::ndarray<const uint8_t, nb::shape<-1>, nb::c_contig, nb::device::cpu> array_view(
-            reinterpret_cast<const uint8_t*>(view.data()), {view.size()});
-        return frombuffer(array_view, nb::arg("dtype") = torch_dtype);
+        nb::bytes buffer_bytes(reinterpret_cast<const char*>(view.data()), view.size());
+        return frombuffer(buffer_bytes, nb::arg("dtype") = torch_dtype);
     }();
 
     tensor = tensor.attr("reshape")(row_major_host_buffer.shape);
@@ -699,7 +698,16 @@ nb::object convert_tt_tensor_to_numpy_tensor(const RowMajorHostBuffer& row_major
         TT_THROW("Unreachable");
     }();
 
-    auto tensor = frombuffer(row_major_host_buffer.buffer, nb::arg("dtype") = np_dtype);
+    auto tensor = [&]() {
+        if (row_major_host_buffer.buffer.view_bytes().empty()) {
+            auto numpy_empty = np.attr("empty");
+            return numpy_empty(row_major_host_buffer.shape, nb::arg("dtype") = np_dtype);
+        }
+        auto view = row_major_host_buffer.buffer.view_bytes();
+        nb::bytes buffer_bytes(reinterpret_cast<const char*>(view.data()), view.size());
+        return frombuffer(buffer_bytes, nb::arg("dtype") = np_dtype);
+    }();
+
     tensor = tensor.attr("reshape")(row_major_host_buffer.shape);
     tensor = np.attr("ascontiguousarray")(tensor);
     GraphTracker::instance().track_function_end(tensor);
