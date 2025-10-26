60e74993c0aaacb00c816124ebf0a5ab3c24970c
Roman Furko <rfurko@tenstorrent.com>
2025-10-26 10:18:21 +0000

    Removed shell script

--- tt-train/sources/examples/gsm8k_finetune/bisect_test.sh
+++
@@ -1,5 +0,0 @@
-git sumbmodule update --init --recursive
-build_metal.sh -b Release
-cmake --build ~/git/tt-metal/tt-train/build --config Release --target all
-python3 tt-train/sources/examples/gsm8k_finetune/gsm8k_finetune.py
-exit $?
