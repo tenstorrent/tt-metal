5d1e031b2e22112d79d1d29a3f2fd5623a572077
Roman Furko <rfurko@tenstorrent.com>
2025-10-26 10:15:34 +0000

    Updated test script

--- tt-train/sources/examples/gsm8k_finetune/bisect_test.sh
+++ tt-train/sources/examples/gsm8k_finetune/bisect_test.sh
@@ -1,5 +1,5 @@
 git sumbmodule update --init --recursive
-../../../../build_metal.sh -b Release
+build_metal.sh -b Release
 cmake --build ~/git/tt-metal/tt-train/build --config Release --target all
-python3 gsm8k_finetune.py
+python3 tt-train/sources/examples/gsm8k_finetune/gsm8k_finetune.py
 exit $?
