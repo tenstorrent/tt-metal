bcd635749878fadc86cc78b41bd3b1c587432bd7
Juan Camilo Vega <jvega@tenstorrent.com>
2025-10-27 20:40:08 +0000

    Fixed small error from earlier

--- tests/ttnn/unit_tests/operations/ccl/blackhole_CI/all_post_commit/test_all_gather_apc.py
+++ tests/ttnn/unit_tests/operations/ccl/blackhole_CI/all_post_commit/test_all_gather_apc.py
@@ -17,6 +17,7 @@ from tests.ttnn.unit_tests.operations.ccl.blackhole_CI.nightly.test_all_gather_n
 
 
 @skip_for_wormhole_b0()
+@skip_for_n_or_less_dev(1)
 @pytest.mark.parametrize(
     "num_devices, ag_output_shape, dim, layout",
     [
@@ -86,6 +87,8 @@ def test_all_gather_2D_line(
     num_workers_per_link,
     num_buffers_per_channel,
 ):
+    validate_test(num_devices, all_gather_topology, bh_2d_mesh_device.shape, 0)
+
     submesh_device = bh_2d_mesh_device.create_submesh(ttnn.MeshShape((num_devices, 1)))
     cluster_axis = 0
     run_all_gather_impl(
@@ -111,6 +114,7 @@ def test_all_gather_2D_line(
 
 
 @skip_for_wormhole_b0()
+@skip_for_n_or_less_dev(2)
 @pytest.mark.parametrize(
     "num_devices, ag_output_shape, dim, layout",
     [
@@ -181,6 +185,7 @@ def test_all_gather_4D_line(
     num_workers_per_link,
     num_buffers_per_channel,
 ):
+    validate_test(num_devices, all_gather_topology, bh_2d_mesh_device.shape, 0)
     submesh_device = bh_2d_mesh_device.create_submesh(ttnn.MeshShape((num_devices, 1)))
     cluster_axis = 0
     run_all_gather_impl(
@@ -206,6 +211,7 @@ def test_all_gather_4D_line(
 
 
 @skip_for_wormhole_b0()
+@skip_for_n_or_less_dev(2)
 @pytest.mark.parametrize(
     "ag_output_shape, dim, layout",
     [
@@ -259,7 +265,7 @@ def test_all_gather_4D_line(
 @pytest.mark.parametrize("num_workers_per_link", [2])
 @pytest.mark.parametrize("num_buffers_per_channel", [2])
 def test_all_gather_ring(
-    bh_2d_mesh_device,
+    bh_1d_mesh_device,
     ag_output_shape,
     dim,
     num_links,
@@ -274,10 +280,11 @@ def test_all_gather_ring(
     num_workers_per_link,
     num_buffers_per_channel,
 ):
-    if cluster_axis == 0:
-        submesh_device = bh_2d_mesh_device.create_submesh(ttnn.MeshShape((8, 1)))
-    else:
-        submesh_device = bh_2d_mesh_device.create_submesh(ttnn.MeshShape((1, 8)))
+    num_devices = bh_1d_mesh_device.shape[0]
+    validate_test(num_devices, all_gather_topology, bh_1d_mesh_device.shape, 0)
+
+    submesh_device = bh_1d_mesh_device.create_submesh(ttnn.MeshShape((num_devices, 1)))
+    cluster_axis = 0
     run_all_gather_impl(
         submesh_device,
         num_devices,
@@ -305,7 +312,7 @@ def test_all_gather_ring(
 @pytest.mark.parametrize(
     "num_devices, ag_output_shape, dim, layout",
     [
-        (8, [1, 1, 128, 128], 3, ttnn.TILE_LAYOUT),
+        (2, [1, 1, 128, 128], 3, ttnn.TILE_LAYOUT),
     ],
 )
 @pytest.mark.parametrize("num_links", [2], ids=["2_links"])
@@ -352,10 +359,9 @@ def test_all_gather_ring(
     indirect=["device_params"],
 )
 @pytest.mark.parametrize("chunks_per_sync", [20])
-@pytest.mark.parametrize("cluster_axis", [0, 1])
 @pytest.mark.parametrize("num_workers_per_link", [2])
 @pytest.mark.parametrize("num_buffers_per_channel", [2])
-def test_all_gather_8D(
+def test_all_gather_8D_vertical(
     bh_2d_mesh_device,
     num_devices,
     ag_output_shape,
@@ -371,12 +377,112 @@ def test_all_gather_8D(
     chunks_per_sync,
     num_workers_per_link,
     num_buffers_per_channel,
-    cluster_axis,
 ):
-    if cluster_axis == 0:
-        submesh_device = bh_2d_mesh_device.create_submesh(ttnn.MeshShape((num_devices, 1)))
-    else:
-        submesh_device = bh_2d_mesh_device.create_submesh(ttnn.MeshShape((1, num_devices)))
+    validate_test(num_devices, all_gather_topology, bh_2d_mesh_device.shape, 1)
+    submesh_device = bh_2d_mesh_device.create_submesh(ttnn.MeshShape((1, num_devices)))
+    cluster_axis = 1
+    run_all_gather_impl(
+        submesh_device,
+        num_devices,
+        ag_output_shape,
+        dim,
+        num_links,
+        ag_input_dtype,
+        layout,
+        mem_config_input,
+        mem_config_ag,
+        all_gather_topology=all_gather_topology,
+        enable_trace=enable_trace,
+        num_iters=num_iters,
+        cluster_axis=cluster_axis,
+        chunks_per_sync=chunks_per_sync,
+        num_workers_per_link=num_workers_per_link,
+        num_buffers_per_channel=num_buffers_per_channel,
+        allowed_pcc=0.9999,
+    )
+    ttnn.ReadDeviceProfiler(submesh_device)
+
+
+@skip_for_wormhole_b0("This test is for blackhole")
+@pytest.mark.parametrize("num_links", [2], ids=["2_links"])
+@pytest.mark.parametrize(
+    "num_devices, ag_output_shape,dim",
+    [
+        (4, [3, 1, 4096, 576], 3),
+        (4, [3, 4, 4096, 144], 1),
+    ],
+    ids=["gemma dim 3", "gemma dim 1"],
+)
+@pytest.mark.parametrize(
+    "layout",
+    [ttnn.TILE_LAYOUT, ttnn.ROW_MAJOR_LAYOUT],
+    ids=[
+        "tile",
+        "row major",
+    ],
+)
+@pytest.mark.parametrize(
+    "ag_input_dtype",
+    [
+        ttnn.bfloat16,
+        ttnn.uint32,
+        ttnn.bfloat8_b,
+    ],
+    ids=[
+        "float_16",
+        "uint_32",
+        "bfloat_8",
+    ],
+)
+@pytest.mark.parametrize(
+    "mem_config_input, mem_config_ag",
+    [
+        (
+            ttnn.MemoryConfig(ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.DRAM),
+            ttnn.MemoryConfig(ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.DRAM),
+        ),
+    ],
+    ids=["dram_only"],
+)
+@pytest.mark.parametrize(
+    "enable_trace, num_iters",
+    [
+        (False, 10),
+    ],
+    ids=["non-trace"],
+)
+@pytest.mark.parametrize(
+    "device_params, all_gather_topology",
+    [
+        ({"fabric_config": ttnn.FabricConfig.FABRIC_1D, "trace_region_size": 150000}, ttnn.Topology.Linear),
+        ({"fabric_config": ttnn.FabricConfig.FABRIC_1D_RING, "trace_region_size": 150000}, ttnn.Topology.Ring),
+    ],
+    indirect=["device_params"],
+    ids=["fabric_linear", "fabric_ring"],
+)
+@pytest.mark.parametrize("chunks_per_sync", [20])
+@pytest.mark.parametrize("num_workers_per_link", [2])
+@pytest.mark.parametrize("num_buffers_per_channel", [2])
+def test_all_gather_failing_shapes(
+    bh_1d_mesh_device,
+    num_devices,
+    ag_output_shape,
+    dim,
+    num_links,
+    ag_input_dtype,
+    layout,
+    mem_config_input,
+    mem_config_ag,
+    enable_trace,
+    all_gather_topology,
+    num_iters,
+    chunks_per_sync,
+    num_workers_per_link,
+    num_buffers_per_channel,
+):
+    validate_test(num_devices, all_gather_topology, bh_1d_mesh_device.shape, 0)
+    submesh_device = bh_1d_mesh_device.create_submesh(ttnn.MeshShape((num_devices, 1)))
+    cluster_axis = 0
     run_all_gather_impl(
         submesh_device,
         num_devices,
