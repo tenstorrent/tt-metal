e722e8766b6bf882bfabcb932bc37fe0a935d71d
Dragica Stoiljkovic <dstoiljkovic@tenstorrent.com>
2025-10-27 14:58:01 +0000

    Adding hang to the op

--- ttnn/cpp/ttnn/operations/pool/generic/device/kernels/compute/compute_pool_2d.cpp
+++ ttnn/cpp/ttnn/operations/pool/generic/device/kernels/compute/compute_pool_2d.cpp
@@ -268,7 +268,7 @@ void MAIN {
                         reduce_tile_math(math_tile_idx, num_faces_in_input_tile);
                     }
                 }
-                cb_pop_front(curr_in_cb_id, 1);
+                // cb_pop_front(curr_in_cb_id, 1);
             }
             tile_regs_commit();
             tile_regs_wait();
