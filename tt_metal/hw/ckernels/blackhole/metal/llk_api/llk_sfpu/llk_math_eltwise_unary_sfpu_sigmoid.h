4b890b76fd1d82e9a2897b01c2787aed35669d64
Nathan Maurice <nmaurice@tenstorrent.com>
2025-10-27 14:25:05 +0000

    Add exp-based sigmoid_tile implementation

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_sigmoid.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_sigmoid.h
@@ -15,10 +15,10 @@ inline void llk_math_eltwise_unary_sfpu_sigmoid_init() {
     llk_math_eltwise_unary_sfpu_init<SfpuType::sigmoid, APPROXIMATE>(sfpu::sigmoid_init<APPROXIMATE>);
 }
 
-template <bool APPROXIMATE>
+template <bool APPROXIMATE, bool is_fp32_dest_acc_en = false>
 inline void llk_math_eltwise_unary_sfpu_sigmoid(uint dst_index, int vector_mode = (int)VectorMode::RC) {
     _llk_math_eltwise_unary_sfpu_params_<APPROXIMATE>(
-        ckernel::sfpu::calculate_sigmoid<APPROXIMATE>, dst_index, vector_mode);
+        ckernel::sfpu::calculate_sigmoid<APPROXIMATE, 8, is_fp32_dest_acc_en>, dst_index, vector_mode);
 }
 
 }  // namespace ckernel
