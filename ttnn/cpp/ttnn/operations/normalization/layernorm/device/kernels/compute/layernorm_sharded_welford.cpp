95fe06ab98aafba4c2ae08b5058b6523306fd875
Ryan Miller <rmiller@tenstorrent.com>
2025-10-28 10:44:58 -0400

    Welford layernorm perf improvements (#31133)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/30680
    
    ### Problem description
    This PR implements a few performance enhancements to the Welford
    layernorm paths (interleaved and sharded):
    
    1. Pre-compute reciprocals for Welford LLK in program factory and shard
    to cores
    2. Remove the generation and cb_wait for scaler CB's that are not used
    in Welford path
    3. Get rid of a binary_op_init that was unnecessary
    
    The overall improvement in e2e time for the DRAM interleaved path over
    the previous implementation (computing the reciprocals in the Welford
    LLK using division) is shown in the table below:
    
    Baseline: Legacy layernorm implementation (using reduce_tile)
    Before: Welford layernorm using division
    After: Welford layernorm using pre-computed reciprocals
    
    E2E op times averaged over 10 runs (DRAM interleaved):
    <img width="1510" height="554" alt="image"
    src="https://github.com/user-attachments/assets/03d19814-0f82-4fd6-9bba-a31170d33e0c"
    />
    
    ### What's changed
    Described above
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/18852542211
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-quick.yaml)
    CI passes (if applicable)
    - [ ] [Galaxy demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/normalization/layernorm/device/kernels/compute/layernorm_sharded_welford.cpp
+++ ttnn/cpp/ttnn/operations/normalization/layernorm/device/kernels/compute/layernorm_sharded_welford.cpp
@@ -15,6 +15,7 @@
 #include "compute_kernel_api/welford.h"
 #include "compute_kernel_api/eltwise_binary.h"
 #include "ttnn/operations/normalization/kernel_util/compute/combine_welford.h"
+#include "ttnn/operations/normalization/kernel_util/compute/memory.h"
 
 /**
  * @brief This kernel computes layernorm for sharded tensors using
@@ -61,10 +62,10 @@
  * calculation is done using the global mean and 1/sqrt(var + eps) results.
  *
  * @note Depending on the tensor and core grid shape, some cores may
- *       not participate in the reduction (i.e., `is_allgather_worker`
- *       will be false). These cores just receive the multicasted
- *       global results and perform the rest of layernorm for their
- *       row(s) width slices.
+ *       not participate in the combine (i.e., `is_allgather_worker`
+ *       will be false). These cores do their partial reduction and
+ *       receive the multicasted global results and perform
+ *       the rest of layernorm for their row(s) width slices.
  */
 namespace NAMESPACE {
 namespace {
@@ -122,6 +123,7 @@ void MAIN {
     constexpr uint32_t last_tile_w = get_compile_time_arg_val(15);
     constexpr uint32_t W = get_compile_time_arg_val(16);
     constexpr uint32_t eps = get_compile_time_arg_val(17);
+    constexpr uint32_t per_core_recip_lut_size = get_compile_time_arg_val(18);
 
     // ---------------------------------------------------------------------------
     // CB definitions
@@ -141,7 +143,7 @@ void MAIN {
                                                           // (workaround for bug in transpose_wh_dest)
     constexpr uint32_t cb_fusion = tt::CBIndex::c_18;     // stream gamma/beta
     constexpr uint32_t cb_out = tt::CBIndex::c_16;
-
+    constexpr uint32_t cb_reciprocals = tt::CBIndex::c_25;  // LUT of pre-computed reciprocals for Welford's algorithm
     constexpr uint32_t cb_im = (do_gamma | do_beta) ? cb_x : cb_out;
     constexpr uint32_t cb_outgamma = do_beta ? cb_fusion : cb_out;
 #ifdef FUSE_PRE_ADD
@@ -193,6 +195,11 @@ void MAIN {
     constexpr uint32_t welford_input_dst = 0;
     constexpr uint32_t welford_mean_dst = 1;
     constexpr uint32_t welford_var_dst = 2;
+
+    // Pointer to the reciprocal LUT
+
+    using recip_lut_t = std::array<uint32_t, per_core_recip_lut_size>;
+    auto p_reciprocals = norm::kernel_util::compute::memory::get_pointer_to_cb_data<recip_lut_t>(cb_reciprocals, 0);
 
     int index_subblock_w_offset = 0;
     int index_h_offset = 0;
@@ -252,8 +259,8 @@ void MAIN {
         tile_regs_acquire();
         for (uint32_t w = 0; w < num_reduce_tiles_per_block_h; w++) {
             transpose_wh_tile(cb_in, w + index_h_offset, welford_input_dst);
-            welford_tile<welford_input_dst, welford_mean_dst, welford_var_dst, true, 0>(
-                w * tile_width, partial_reduce_W, 0, {});
+            welford_tile<welford_input_dst, welford_mean_dst, welford_var_dst, true, per_core_recip_lut_size>(
+                w * tile_width, partial_reduce_W, 0, *p_reciprocals);
         }
         // We should transpose back to columns here
         // However, transpose_wh_dest() is currently buggy.
