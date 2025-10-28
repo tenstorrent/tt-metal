e5b24ee0c0e0897d14dc9b5a36a660e068a2f6bc
ldjurovicTT <ldjurovic@tenstorrent.com>
2025-10-28 16:07:14 +0000

    Initial api

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_sfpu_reduce_sdpa.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_sfpu_reduce_sdpa.h
@@ -11,14 +11,17 @@
 namespace ckernel {
 
 template <bool APPROXIMATE>
-inline void llk_math_eltwise_unary_sfpu_sigmoid_init() {
+inline void llk_math_sfpu_reduce_max_sdpa_init() {
     llk_math_eltwise_unary_sfpu_init<SfpuType::reduce, APPROXIMATE>(sfpu::_init_reduce_sdpa_<DataFormat::Float16_b>);
 }
 
 template <bool APPROXIMATE>
-inline void llk_math_eltwise_unary_sfpu_sigmoid(uint dst_index, int vector_mode = (int)VectorMode::RC_custom) {
+inline void llk_math_sfpu_reduce_max_sdpa(uint dst_index, int vector_mode = (int)VectorMode::RC_custom) {
     _llk_math_eltwise_unary_sfpu_params_<APPROXIMATE>(
-        ckernel::sfpu::calculate_sigmoid<APPROXIMATE>, dst_index, vector_mode);
+        ckernel::sfpu::_calculate_reduce_sdpa_<PoolType::MAX, REDUCE_COL, DataFormat::Float16_b>,
+        dst_index,
+        vector_mode,
+        4);
 }
 
 }  // namespace ckernel
--- tt_metal/include/compute_kernel_api/eltwise_unary/eltwise_unary.h
+++ tt_metal/include/compute_kernel_api/eltwise_unary/eltwise_unary.h
@@ -7,12 +7,23 @@
 #include "compute_kernel_api/common.h"
 #ifdef TRISC_MATH
 #include "llk_math_unary_datacopy_api.h"
+#include "llk_math_sfpu_reduce_sdpa.h"
 #endif
 #ifdef TRISC_UNPACK
 #include "llk_unpack_A_api.h"
 #endif
 
 namespace ckernel {
+
+template <bool fast_and_approx = false>
+ALWI void sfpu_reduce_max_sdpa_init() {
+    MATH((llk_math_sfpu_reduce_max_sdpa_init<fast_and_approx>()));
+}
+
+template <bool fast_and_approx = false>
+ALWI void sfpu_reduce_max_sdpa(uint32_t idst) {
+    MATH((llk_math_sfpu_reduce_max_sdpa<fast_and_approx>(idst)));
+}
 
 ALWI void unary_op_init_common(uint32_t icb, uint32_t ocb) {
     UNPACK((llk_unpack_A_hw_configure_disaggregated<DST_ACCUM_MODE, StochRndType::None, true>(icb)));
