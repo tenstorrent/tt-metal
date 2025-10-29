d071913a7e5f0e885cd01235bdea1130c4479b5b
vsureshTT <vsuresh@tenstorrent.com>
2025-10-29 21:31:21 +0000

    format, and removed all mention of distributed_ln_program_config

--- ttnn/cpp/ttnn/operations/normalization/layernorm_distributed/device/layernorm_pre_all_gather_op.cpp
+++ ttnn/cpp/ttnn/operations/normalization/layernorm_distributed/device/layernorm_pre_all_gather_op.cpp
@@ -57,7 +57,12 @@ operation::ProgramWithCallbacks LayerNormPreAllGather::create_program(
             if constexpr (std::is_same_v<ProgramConfigType, LayerNormDefaultProgramConfig>) {
                 if (program_config.use_welford == true) {
                     return layernorm_pre_allgather_welford_multi_core(
-                        a, output_tensor, this->norm_type, this->compute_kernel_config, this->use_2d_core_grid);
+                        a,
+                        output_tensor,
+                        this->norm_type,
+                        this->compute_kernel_config,
+                        this->use_2d_core_grid,
+                        this->program_config);
                 } else {
                     return layernorm_pre_allgather_multi_core(
                         a, output_tensor, this->norm_type, this->compute_kernel_config, this->use_2d_core_grid, this->program_config);
