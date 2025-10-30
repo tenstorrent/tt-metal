2d997c0f7cd284fd828cf3ed82eacedb37a642f3
yugaoTT <yugao@tenstorrent.com>
2025-10-30 20:34:45 +0000

    add more test

--- tests/tt_metal/tt_fabric/common/fabric_fixture.hpp
+++ tests/tt_metal/tt_fabric/common/fabric_fixture.hpp
@@ -182,7 +182,7 @@ protected:
     }
 };
 
-class Fabric1DUDMModeFixture : public BaseFabricFixture {
+class NightlyFabric1DUDMModeFixture : public BaseFabricFixture {
 protected:
     static void SetUpTestSuite() {
         BaseFabricFixture::DoSetUpTestSuite(
