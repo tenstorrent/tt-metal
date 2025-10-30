7b1a76a712c421d070482bfc651d3f519da8000a
Adrian Morrison <amorrison@tenstorrent.com>
2025-10-30 19:05:32 +0000

    unit test case

--- tests/nightly/t3000/ccl/test_minimal_all_gather_async.py
+++ tests/nightly/t3000/ccl/test_minimal_all_gather_async.py
@@ -1070,3 +1070,37 @@ def test_nd(mesh_device, input_shape, dim, cluster_axis, dtype, memory_config, t
 
         eq, mess = comp_pcc(torch_reference, tt_output_tensor)
         assert eq, mess
+
+
+@pytest.mark.parametrize("device_params", [{"fabric_config": ttnn.FabricConfig.FABRIC_1D_RING}], indirect=True)
+@pytest.mark.parametrize("mesh_device", [MESH_SHAPE], indirect=True)
+@pytest.mark.parametrize("input_shape", [[1, 1, 32, 32]])
+@pytest.mark.parametrize("dtype", [torch.bfloat16])
+@pytest.mark.parametrize("layout", [ttnn.TILE_LAYOUT, ttnn.ROW_MAJOR_LAYOUT])  # RM goes to composite
+@pytest.mark.parametrize("dim", [0])
+@pytest.mark.parametrize("cluster_axis", [1])  # cluster_axis 0 hangs, 1 fatals.
+@pytest.mark.parametrize("topology", [ttnn.Topology.Ring])
+def test_mesh_ring(mesh_device, input_shape, dim, cluster_axis, dtype, layout, topology):
+    tt_input, torch_reference = _get_tensors(
+        input_shape,
+        tuple(mesh_device.shape),
+        dim,
+        cluster_axis,
+        dtype,
+        ttnn.DRAM_MEMORY_CONFIG,
+        layout,
+        mesh_device,
+    )
+
+    for i in range(NUM_ITERS):
+        tt_out_tensor = ttnn.all_gather(
+            tt_input,
+            dim,
+            cluster_axis=cluster_axis,
+            topology=topology,
+        )
+
+        tt_output_tensor = torch.cat([ttnn.to_torch(t) for t in ttnn.get_device_tensors(tt_out_tensor)])
+
+        eq, mess = comp_pcc(torch_reference, tt_output_tensor)
+        assert eq, mess
