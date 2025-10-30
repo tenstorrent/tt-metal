758ffe2b9eea6f07ad24eb2278da3bc4322d4411
John Bauman <jbauman@tenstorrent.com>
2025-10-30 18:57:21 +0000

    imporove comment

--- tt_metal/impl/dispatch/kernels/cq_common.hpp
+++ tt_metal/impl/dispatch/kernels/cq_common.hpp
@@ -450,7 +450,7 @@ public:
     // Get new CB pages and release old pages to writer. If we move to next block, we call on_boundary to allow the
     // caller to handle the orphan data that would otherwise be lost.  The first argument to on_boundary is the size of
     // the remaining data in the block. The second argument is whether the next block is the first block in the circular
-    // buffer (in which case cmd_ptr is set to the base address).
+    // buffer (in which case cmd_ptr is set to the base address after on_boundary is called).
     template <typename OnBoundaryFn>
     FORCE_INLINE uint32_t get_cb_page_and_release_pages(uint32_t& cmd_ptr, OnBoundaryFn&& on_boundary) {
         if (this->cb_fence_ == this->block_next_start_addr_[this->rd_block_idx_]) {
