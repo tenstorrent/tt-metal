10cf52f2578c25b93310d6a31d891d9061488110
Piotr Bara <pbara@tenstorrent.com>
2025-10-31 19:51:47 +0000

    Falcon7B: Add TP/DP settings to benchmark data

--- models/demos/falcon7b_common/demo/demo.py
+++ models/demos/falcon7b_common/demo/demo.py
@@ -539,6 +539,10 @@ def run_falcon_demo_kv(
     run_type = f"demo_perf_{num_devices}chip" if perf_mode else f"demo_generate_{num_devices}chip"
     if galaxy_type:
         run_type += f"_{galaxy_type}"
+
+    data_parallel = 1
+    tensor_parallel = num_devices // data_parallel
+    config_params = configuration.to_dict() | {"data_parallel": data_parallel, "tensor_parallel": tensor_parallel}
     benchmark_data.save_partial_run_json(
         profiler,
         run_type=run_type,
@@ -546,7 +550,7 @@ def run_falcon_demo_kv(
         ml_model_type="llm",
         num_layers=num_layers,
         batch_size=batch_size,
-        config_params=configuration.to_dict(),
+        config_params=config_params,
         precision=f"prefill[{model_config_strs_prefill_decode[0]}]_decode[{model_config_strs_prefill_decode[1]}]",
         input_sequence_length=num_input_tokens,
         output_sequence_length=1 if perf_mode else output_token_index + 1,
