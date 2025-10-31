0210dfa796a205140e7c3210d6155428189cd9b2
Ubuntu <ioannis_alexiou@epam.com>
2025-10-31 09:59:44 +0000

    edit on test

--- models/tt_transformers/tests/multimodal/run_test.sh
+++ models/tt_transformers/tests/multimodal/run_test.sh
@@ -4,6 +4,7 @@ set -e
 fail=0
 # Llama3.2-11B
 # CI=true HF_HUB_CACHE=/mnt/MLPerf/huggingface/hub HF_MODEL=$llama11b TT_CACHE_PATH=$tt_cache pytest -n auto models/tt_transformers/tests/multimodal/test_llama_image_transformer.py  --timeout 420 || fail=1
+TT_CACHE_HOME=/mnt/MLPerf/huggingface/tt_cache
 llama11b=meta-llama/Llama-3.2-11B-Vision-Instruct
 tt_cache_llama11b=$TT_CACHE_HOME/$llama11b
 
