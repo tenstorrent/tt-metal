22f841c633b86589254f06b360ad97ece8b3ce36
Ubuntu <ioannis_alexiou@epam.com>
2025-10-31 09:40:22 +0000

    edit on custom test

--- models/tt_transformers/tests/multimodal/run_test.sh
+++ models/tt_transformers/tests/multimodal/run_test.sh
@@ -3,14 +3,11 @@ set -e
 
 fail=0
 # Llama3.2-11B
-llama11b=meta-llama/Llama-3.2-11B-Vision-Instruct
-tt_cache=/mnt/MLPerf/huggingface/tt_cache/$llama11b
-
 # CI=true HF_HUB_CACHE=/mnt/MLPerf/huggingface/hub HF_MODEL=$llama11b TT_CACHE_PATH=$tt_cache pytest -n auto models/tt_transformers/tests/multimodal/test_llama_image_transformer.py  --timeout 420 || fail=1
+llama11b=meta-llama/Llama-3.2-11B-Vision-Instruct
+tt_cache_llama11b=$TT_CACHE_HOME/$llama11b
 
-llama11b=/mnt/MLPerf/huggingface/hub/models--meta-llama--Llama-3.2-11B-Vision-Instruct/snapshots/9eb2daaa8597bf192a8b0e73f848f3a102794df5/
-#/mnt/MLPerf/tt_dnn-models/llama/Llama3.2-11B-Vision-Instruct/
-HF_MODEL=$llama11b pytest -n auto models/tt_transformers/tests/multimodal/test_llama_image_transformer.py  --timeout 420 || fail=1
+HF_MODEL=$llama11b TT_CACHE_PATH=$tt_cache_llama11b  pytest -n auto models/tt_transformers/tests/multimodal/test_llama_image_transformer.py ; fail+=$?
 
 echo "LOG_METAL: Llama3 accuracy tests for $llama11b completed"
 if [[ $fail -ne 0 ]]; then
