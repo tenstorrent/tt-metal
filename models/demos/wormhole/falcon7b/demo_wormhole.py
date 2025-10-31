32e604b77e2d6b138a537d42c5b56d5b131fdb21
Aliaksei Sala <aliaksei_sala@epam.com>
2025-10-31 06:40:05 -0700

    update single test perf for 128 seq len

--- models/demos/wormhole/falcon7b/demo_wormhole.py
+++ models/demos/wormhole/falcon7b/demo_wormhole.py
@@ -11,7 +11,7 @@ from models.demos.falcon7b_common.demo.demo import run_falcon_demo_kv
 @pytest.mark.parametrize(
     "perf_mode, max_seq_len, expected_perf_metrics, greedy_sampling, expected_greedy_output_path",
     (
-        (True, 128, {"prefill_t/s": 1720, "decode_t/s": 549, "decode_t/s/u": 17.16}, False, None),
+        (True, 128, {"prefill_t/s": 1990, "decode_t/s": 549, "decode_t/s/u": 17.16}, False, None),
         (True, 1024, {"prefill_t/s": 2300, "decode_t/s": 487, "decode_t/s/u": 15.24}, False, None),
         (True, 2048, {"prefill_t/s": 2163, "decode_t/s": 445, "decode_t/s/u": 13.91}, False, None),
         (True, 128, None, False, None),
