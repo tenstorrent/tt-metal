d092ba7537d84ae0f7bbf256c5cca1f237f3b99f
awliu-TT <awliu@tenstorrent.com>
2025-10-31 19:37:40 +0000

    support for selecting which cores on TTSIM to perform TTSIM_TENSIX_DUMP on using the TT_METAL_DPRINT_CORES env variable

--- tt_metal/impl/debug/dprint_server.cpp
+++ tt_metal/impl/debug/dprint_server.cpp
@@ -42,6 +42,7 @@
 #include "llrt.hpp"
 #include "impl/context/metal_context.hpp"
 #include "tt_backend_api_types.hpp"
+#include <umd/device/simulation/tt_sim_chip.hpp>
 
 using std::flush;
 using std::int32_t;
@@ -758,6 +759,19 @@ void DPrintServer::Impl::attach_device(ChipId device_id) {
         device_to_core_range_.count(device_id) == 0, "Device {} added to DPRINT server more than once!", device_id);
     device_to_core_range_[device_id] = print_cores_sanitized;
     device_to_core_range_lock_.unlock();
+
+    // if TTSIM is enabled, set the debug core for the cores enabled for DPRINT
+    if (tt::tt_metal::MetalContext::instance().rtoptions().get_simulator_enabled()) {
+        for (const auto& core_desc : print_cores_sanitized) {
+            uint32_t x = core_desc.coord.x;
+            uint32_t y = core_desc.coord.y;
+            auto& cluster = tt::tt_metal::MetalContext::instance().get_cluster();
+            tt::umd::Chip* chip = cluster.get_driver()->get_chip(device_id);
+            if (auto* sim = dynamic_cast<tt::umd::TTSimChip*>(chip)) {
+                sim->set_debug_core(x, y);
+            }
+        }
+    }
     log_info(tt::LogMetal, "DPRINT Server attached device {}", device_id);
 }  // attach_device
 
