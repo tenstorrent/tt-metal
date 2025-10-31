1f85e209b4b5a46571a4469222b1b75716bcf4cb
John Bauman <jbauman@tenstorrent.com>
2025-10-31 20:08:20 +0000

    Add comment

--- tt_metal/impl/dispatch/kernels/cq_common.hpp
+++ tt_metal/impl/dispatch/kernels/cq_common.hpp
@@ -429,6 +429,7 @@ public:
     }
 
     // Returns how much data is available. Will block until data is available. May release old pages before cmd_ptr to writer.
+    // noc_nonposted_writes_num_issued[noc_index] must be updated before calling this function.
     FORCE_INLINE uint32_t wait_for_availabe_data_and_release_old_pages(uint32_t& cmd_ptr) {
         if (this->available_space(cmd_ptr) == 0) {
             get_cb_page_and_release_pages(cmd_ptr);
@@ -440,6 +441,7 @@ public:
     // caller to handle the orphan data that would otherwise be lost.  The first argument to on_boundary is the size of
     // the remaining data in the block. The second argument is whether the next block is the first block in the circular
     // buffer (in which case cmd_ptr is set to the base address after on_boundary is called).
+    // noc_nonposted_writes_num_issued[noc_index] must be updated before on_boundary returns.
     template <typename OnBoundaryFn>
     FORCE_INLINE uint32_t get_cb_page_and_release_pages(uint32_t& cmd_ptr, OnBoundaryFn&& on_boundary) {
         if (this->cb_fence_ == this->block_next_start_addr_[this->rd_block_idx_]) {
