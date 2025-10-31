e072de03e82cb16c3aa9c0cdac67313efdfec33a
awliu-TT <awliu@tenstorrent.com>
2025-10-31 22:40:51 +0000

    changing garbage instruction used to trigger a TTSIM_TENSIX_DUMP. Added flag to dump DST

--- tt_metal/hw/inc/debug/ttsim_dump.h
+++ tt_metal/hw/inc/debug/ttsim_dump.h
@@ -1,16 +1,33 @@
 #pragma once
 #ifdef USING_TTSIM
 class TTSIM_Dump_Guard {
-public:
-    TTSIM_Dump_Guard() { __asm__ volatile(".word 0x00000000"); }
-    ~TTSIM_Dump_Guard() { __asm__ volatile(".word 0x00000001"); }
-};
+private:
+    bool dump_dst;
 
-#define TTSIM_TENSIX_DUMP auto dump_guard = TTSIM_Dump_Guard()
-#define TTSIM_START_TENSIX_DUMP auto dump_guard_ptr = new TTSIM_Dump_Guard()
+public:
+    TTSIM_Dump_Guard(bool dump_dst = false) {
+        this->dump_dst = dump_dst;
+        if (this->dump_dst) {
+            __asm__ volatile(".word 0x4000003F");
+        } else {
+            __asm__ volatile(".word 0x0000003F");
+        }
+    }
+    ~TTSIM_Dump_Guard() {
+        if (this->dump_dst) {
+            __asm__ volatile(".word 0xC000003F");
+        } else {
+            __asm__ volatile(".word 0x8000003F");
+        }
+    }
+};
+#define TTSIM_DUMP_DST true
+#define TTSIM_TENSIX_DUMP(dump_dst) auto dump_guard = TTSIM_Dump_Guard(dump_dst)
+#define TTSIM_START_TENSIX_DUMP auto dump_guard_ptr = new TTSIM_Dump_Guard(dump_dst)
 #define TTSIM_END_TENSIX_DUMP delete dump_guard_ptr
 #else
 #define TTSIM_TENSIX_DUMP
 #define TTSIM_START_TENSIX_DUMP
 #define TTSIM_END_TENSIX_DUMP
+#define TTSIM_DUMP_DST
 #endif
