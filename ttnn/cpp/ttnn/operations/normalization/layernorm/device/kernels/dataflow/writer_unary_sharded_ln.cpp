7d1198374710b8087f5cb8e80c95a8e0e58db3a2
Ryan Miller <rmiller@tenstorrent.com>
2025-10-31 09:29:03 -0400

    Welford layernorm perf improvements + expanded layernorm/RMSnorm test coverage (#31448)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/30680
    
    ### Problem description
    This PR implements a few performance enhancements to the Welford
    layernorm paths (interleaved and sharded):
    
    1. Pre-compute reciprocals for Welford LLK in program factory and shard
    to cores
    2. Remove the generation and cb_wait for scaler CB's that are not used
    in Welford path
    3. Get rid of a binary_op_init that was unnecessary
    
    The overall improvement in e2e time for the DRAM interleaved path over
    the previous implementation (computing the reciprocals in the Welford
    LLK using division) is shown in the table below:
    
    Baseline: Legacy layernorm implementation (using reduce_tile)
    Before: Welford layernorm using division
    After: Welford layernorm using pre-computed reciprocals
    
    E2E op times averaged over 10 runs (DRAM interleaved):
    <img width="1510" height="554" alt="image"
    src="https://github.com/user-attachments/assets/03d19814-0f82-4fd6-9bba-a31170d33e0c"
    />
    
    This PR also greatly expands layernorm and rms norm test coverage to
    include sharded rms norm and row-major gamma/beta to invoke the
    row-major reader kernels.
    
    ### What's changed
    Described above
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/18921802999
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/18921808825
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/18942142377
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-quick.yaml)
    CI passes (if applicable)
    - [ ] [Galaxy demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: mtairum <mtairum@tenstorrent.com>

--- ttnn/cpp/ttnn/operations/normalization/layernorm/device/kernels/dataflow/writer_unary_sharded_ln.cpp
+++ ttnn/cpp/ttnn/operations/normalization/layernorm/device/kernels/dataflow/writer_unary_sharded_ln.cpp
@@ -43,20 +43,20 @@ void kernel_main() {
 
     const uint32_t out_single_tile_size_bytes = get_tile_size(cb_out);
 
-    if (!use_welford) {
+    if constexpr (!use_welford) {
         constexpr uint32_t cb_in_2 = tt::CBIndex::c_2;
         const uint32_t scalar_w = get_arg_val<uint32_t>(1);
         generate_reduce_scaler(cb_in_2, scalar_w);
-    }
-    if constexpr (is_all_to_all_worker && !use_welford) {
-        constexpr uint32_t cb_in_4 = tt::CBIndex::c_4;
-        const uint32_t scalar_c = get_arg_val<uint32_t>(0);
-        generate_reduce_scaler(cb_in_4, scalar_c);
-    }
-    constexpr uint32_t eps_cb_id = 3;
-    const uint32_t eps = get_arg_val<uint32_t>(2);
-    if (!use_welford) {
+
+        constexpr uint32_t eps_cb_id = tt::CBIndex::c_3;
+        const uint32_t eps = get_arg_val<uint32_t>(2);
         generate_bcast_col_scalar(eps_cb_id, eps);
+
+        if constexpr (is_all_to_all_worker) {
+            constexpr uint32_t cb_in_4 = tt::CBIndex::c_4;
+            const uint32_t scalar_c = get_arg_val<uint32_t>(0);
+            generate_reduce_scaler(cb_in_4, scalar_c);
+        }
     }
 
     if constexpr (fuse_gamma) {
