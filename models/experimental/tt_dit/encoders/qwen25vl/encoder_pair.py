9ee5914a3be545aeda264995187aa7685b6fb321
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2025-10-31 15:54:53 +0100

    fix test

--- models/experimental/tt_dit/encoders/qwen25vl/encoder_pair.py
+++ models/experimental/tt_dit/encoders/qwen25vl/encoder_pair.py
@@ -14,9 +14,6 @@ from models.demos.qwen25_vl.tt.model import Transformer
 from models.tt_transformers.tt.model_config import DecodersPrecision, ModelArgs
 from transformers import PreTrainedTokenizerBase, Qwen2_5_VLForConditionalGeneration, Qwen2Tokenizer
 
-from ...parallel.config import EncoderParallelConfig
-from ...parallel.manager import CCLManager
-
 if TYPE_CHECKING:
     from collections.abc import Iterable
 
@@ -26,24 +23,17 @@ class Qwen25VlTokenizerEncoderPair:
         self,
         checkpoint: str,
         *,
-        tokenizer_subfolder: str | None = None,
-        encoder_subfolder: str | None = None,
         max_batch_size: int,
         max_sequence_length: int,
         device: ttnn.MeshDevice,
-        ccl_manager: CCLManager,
-        parallel_config: EncoderParallelConfig,
         use_torch: bool,
     ) -> None:
         self._device = device
-        self._ccl_manager = ccl_manager
-        self._parallel_config = parallel_config
 
-        self._tokenizer = Qwen2Tokenizer.from_pretrained(checkpoint, subfolder=tokenizer_subfolder)
-        self._encoder = self._load_encoder(
+        self._tokenizer = Qwen2Tokenizer.from_pretrained(checkpoint)
+        self._torch_encoder, self._encoder = self._load_encoder(
             checkpoint,
-            subfolder=encoder_subfolder,
-            use_torch=use_torch,
+            torch_only=use_torch,
             max_batch_size=max_batch_size,
             max_sequence_length=max_sequence_length,
         )
@@ -52,15 +42,12 @@ class Qwen25VlTokenizerEncoderPair:
         self,
         checkpoint: str,
         *,
-        subfolder: str | None = None,
-        use_torch: bool,
+        torch_only: bool,
         max_batch_size: int,
         max_sequence_length: int,
-    ) -> Qwen2_5_VLForConditionalGeneration | Transformer:
-        torch_model = Qwen2_5_VLForConditionalGeneration.from_pretrained(checkpoint, subfolder=subfolder)
-
-        if use_torch:
-            return torch_model
+    ) -> tuple[Qwen2_5_VLForConditionalGeneration, Transformer | None]:
+        if torch_only:
+            return Qwen2_5_VLForConditionalGeneration.from_pretrained(checkpoint), None
 
         logger.info("creating encoder on device...")
 
@@ -70,12 +57,15 @@ class Qwen25VlTokenizerEncoderPair:
             max_batch_size=max_batch_size,
             # optimizations=lambda model_args: DecodersPrecision.performance(model_args.n_layers, model_args.model_name),
             max_seq_len=max_sequence_length,
+            cache_hf=True,
         )
         state_dict = model_args.load_state_dict()
+        torch_model = model_args.cached_hf_model
+        assert isinstance(torch_model, Qwen2_5_VLForConditionalGeneration)
 
         dtype = ttnn.bfloat16
 
-        return Transformer(
+        return torch_model, Transformer(
             args=model_args,
             mesh_device=self._device,
             dtype=dtype,
@@ -89,6 +79,7 @@ class Qwen25VlTokenizerEncoderPair:
             num_images_per_prompt=num_images_per_prompt,
             tokenizer=self._tokenizer,
             text_encoder=self._encoder,
+            torch_text_encoder=self._torch_encoder,
             max_sequence_length=512,
             mesh_device=self._device,
         )
@@ -97,7 +88,8 @@ class Qwen25VlTokenizerEncoderPair:
 # adapted from https://github.com/huggingface/diffusers/blob/v0.35.2/src/diffusers/pipelines/qwenimage/pipeline_qwenimage.py#L188
 def _get_qwen_prompt_embeds(
     prompts: Iterable[str],
-    text_encoder: Qwen2_5_VLForConditionalGeneration | Transformer,
+    text_encoder: Transformer | None,
+    torch_text_encoder: Qwen2_5_VLForConditionalGeneration,
     tokenizer: PreTrainedTokenizerBase,
     mesh_device: ttnn.MeshDevice | None,
     max_sequence_length: int,
@@ -128,7 +120,7 @@ def _get_qwen_prompt_embeds(
     if untruncated_tokens.shape[-1] >= tokens.shape[-1] and not torch.equal(tokens, untruncated_tokens):
         logger.warning("input text was truncated")
 
-    if isinstance(text_encoder, Transformer):
+    if text_encoder is not None:
         assert mesh_device is not None
 
         tt_tokens = ttnn.from_torch(
@@ -154,10 +146,10 @@ def _get_qwen_prompt_embeds(
 
         hidden_states = ttnn.to_torch(ttnn.get_device_tensors(tt_hidden_states)[0])
     else:
-        tokens = tokens.to(device=text_encoder.device)
+        tokens = tokens.to(device=torch_text_encoder.device)
 
         with torch.no_grad():
-            output = text_encoder.forward(
+            output = torch_text_encoder.forward(
                 tokens,
                 attention_mask=attention_mask,
                 output_hidden_states=True,
--- models/experimental/tt_dit/tests/encoders/qwen25vl/test_qwen25vl.py
+++ models/experimental/tt_dit/tests/encoders/qwen25vl/test_qwen25vl.py
@@ -2,30 +2,29 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-import diffusers as reference
+import diffusers.pipelines.qwenimage.pipeline_qwenimage as reference
 import pytest
 import torch
 import ttnn
 from loguru import logger
 
 from ....encoders.qwen25vl.encoder_pair import Qwen25VlTokenizerEncoderPair
-from ....parallel.config import EncoderParallelConfig, ParallelFactor
-from ....parallel.manager import CCLManager
 from ....utils.check import assert_quality
 
 
 @pytest.mark.parametrize(
-    ("mesh_device", "submesh_shape", "tp_axis", "num_links"),
+    ("mesh_device", "submesh_shape"),
     [
-        pytest.param((2, 4), (2, 4), 1, 1, id="2x4sp0tp1"),
-        pytest.param((4, 8), (4, 8), 1, 4, id="4x8sp0tp1"),
+        pytest.param((1, 1), (1, 1), id="1x1"),
+        # pytest.param((2, 4), (2, 4), id="2x4sp0tp1"),
+        # pytest.param((4, 8), (4, 8), id="4x8sp0tp1"),
     ],
     indirect=["mesh_device"],
 )
 @pytest.mark.parametrize(
-    ("prompts", "num_images_per_prompt"),
+    "prompts",
     [
-        (["hello", "meow"], 4),
+        ["hello", "meow"],
     ],
 )
 @pytest.mark.parametrize(
@@ -36,38 +35,31 @@ from ....utils.check import assert_quality
 def test_qwen25vl(
     mesh_device: ttnn.MeshDevice,
     submesh_shape: tuple[int, int],
-    tp_axis: int,
-    num_links: int,
     prompts: list[str],
-    num_images_per_prompt: int,
 ) -> None:
     submesh_device = mesh_device.create_submesh(ttnn.MeshShape(*submesh_shape))
 
-    tp_factor = tuple(submesh_device.shape)[tp_axis]
+    # There is a bug in the HF implementation where the prompt_embeds_mask is incorrectly repeated
+    # if num_images_per_prompt != 1.
+    # https://github.com/huggingface/diffusers/blob/v0.35.2/src/diffusers/pipelines/qwenimage/pipeline_qwenimage.py#L262
+    # is
+    # prompt_embeds_mask = prompt_embeds_mask.repeat(1, num_images_per_prompt, 1)
+    # but should be
+    # prompt_embeds_mask = prompt_embeds_mask.repeat(1, num_images_per_prompt)
+    num_images_per_prompt = 1
 
-    checkpoint = "Qwen/Qwen-Image"
+    pipeline_checkpoint = "Qwen/Qwen-Image"
+    text_model_checkpoint = "Qwen/Qwen2.5-VL-7B-Instruct"
     max_sequence_length = 512
 
-    torch_pipeline = reference.QwenImagePipeline.from_pretrained(checkpoint)
+    torch_pipeline = reference.QwenImagePipeline.from_pretrained(pipeline_checkpoint)
     assert isinstance(torch_pipeline, reference.QwenImagePipeline)
 
-    ccl_manager = CCLManager(
-        mesh_device=submesh_device,
-        num_links=num_links,
-        topology=ttnn.Topology.Linear,
-    )
-
     tt_encoder_pair = Qwen25VlTokenizerEncoderPair(
-        checkpoint,
-        tokenizer_subfolder="tokenizer",
-        encoder_subfolder="text_encoder",
+        text_model_checkpoint,
         max_sequence_length=max_sequence_length,
         max_batch_size=len(prompts) * num_images_per_prompt,
         device=submesh_device,
-        ccl_manager=ccl_manager,
-        parallel_config=EncoderParallelConfig(
-            tensor_parallel=ParallelFactor(factor=tp_factor, mesh_axis=tp_axis),
-        ),
         use_torch=False,
     )
 
@@ -82,5 +74,5 @@ def test_qwen25vl(
     logger.info("running TT model...")
     tt_embeds, tt_mask = tt_encoder_pair.encode(prompts, num_images_per_prompt=num_images_per_prompt)
 
-    assert_quality(embeds, tt_embeds, pcc=0, relative_rmse=1)
-    assert_quality(mask, tt_mask, pcc=0, relative_rmse=1)
+    assert_quality(embeds, tt_embeds, pcc=1, relative_rmse=0)
+    assert_quality(mask, tt_mask, pcc=1, relative_rmse=0)
