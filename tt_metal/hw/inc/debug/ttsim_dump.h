c7a1a0dc94d61feca1e14a4b5a98fad799c065c4
awliu-TT <awliu@tenstorrent.com>
2025-10-31 16:35:01 +0000

    extending ifdef guard to cover the TTSIM_Dump_Guard class to guard against use on silicon

--- tt_metal/hw/inc/debug/ttsim_dump.h
+++ tt_metal/hw/inc/debug/ttsim_dump.h
@@ -1,12 +1,11 @@
 #pragma once
-
+#ifdef ENABLE_TTSIM_TENSIX_DUMP
 class TTSIM_Dump_Guard {
 public:
     TTSIM_Dump_Guard() { __asm__ volatile(".word 0x00000000"); }
     ~TTSIM_Dump_Guard() { __asm__ volatile(".word 0x00000001"); }
 };
 
-#ifdef ENABLE_TTSIM_TENSIX_DUMP
 #define TTSIM_TENSIX_DUMP auto dump_guard = TTSIM_Dump_Guard()
 #define TTSIM_START_TENSIX_DUMP auto dump_guard_ptr = new TTSIM_Dump_Guard()
 #define TTSIM_END_TENSIX_DUMP delete dump_guard_ptr
