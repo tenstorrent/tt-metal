4816177757115184081cfb6ab7dd75400f4a6d4e
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2025-11-03 14:09:31 +0000

    perf test fix

--- models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py
+++ models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py
@@ -116,35 +116,6 @@ def test_refiner_unet(
 
 
 @pytest.mark.models_device_performance_bare_metal
-def test_sdxl_unet_perf_device():
-    expected_device_perf_cycles_per_iteration = 200_766_079
-
-    command = f"pytest models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py::test_unet"
-    cols = ["DEVICE FW", "DEVICE KERNEL", "DEVICE BRISC KERNEL"]
-
-    batch_size = 1
-    total_batch_size = batch_size * UNET_DEVICE_TEST_TOTAL_ITERATIONS
-
-    inference_time_key = "AVG DEVICE KERNEL DURATION [ns]"
-    post_processed_results = run_device_perf(
-        command, subdir="sdxl_unet", num_iterations=1, cols=cols, batch_size=total_batch_size
-    )
-    expected_perf_cols = {
-        inference_time_key: expected_device_perf_cycles_per_iteration * UNET_DEVICE_TEST_TOTAL_ITERATIONS
-    }
-    expected_results = check_device_perf(
-        post_processed_results, margin=0.015, expected_perf_cols=expected_perf_cols, assert_on_fail=True
-    )
-    prep_device_perf_report(
-        model_name=f"sdxl_unet",
-        batch_size=total_batch_size,
-        post_processed_results=post_processed_results,
-        expected_results=expected_results,
-        comments=f"iterations={UNET_DEVICE_TEST_TOTAL_ITERATIONS}",
-    )
-
-
-@pytest.mark.models_device_performance_bare_metal
 def test_sdxl_refiner_unet_perf_device():
     expected_device_perf_cycles_per_iteration = 640_816_818
 
@@ -156,7 +127,7 @@ def test_sdxl_refiner_unet_perf_device():
 
     inference_time_key = "AVG DEVICE KERNEL DURATION [ns]"
     post_processed_results = run_device_perf(
-        command, subdir="sdxl_unet", num_iterations=1, cols=cols, batch_size=total_batch_size
+        command, subdir="sdxl_refiner_unet", num_iterations=1, cols=cols, batch_size=total_batch_size
     )
     expected_perf_cols = {
         inference_time_key: expected_device_perf_cycles_per_iteration * UNET_DEVICE_TEST_TOTAL_ITERATIONS
