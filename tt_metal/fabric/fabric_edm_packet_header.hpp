f2ad3773411111290268290c360d2fd0e8dd4157
blozano-tt <blozano@tenstorrent.com>
2025-11-03 23:51:01 +0000

    Fix some thing

--- tt_metal/fabric/fabric_edm_packet_header.hpp
+++ tt_metal/fabric/fabric_edm_packet_header.hpp
@@ -18,6 +18,11 @@
 #include <tt_stl/assert.hpp>
 #endif
 
+// These functions have different behavior on host or device.
+// This causes problems trying to detect unused parameters.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
 namespace tt::tt_fabric {
 
 enum TerminationSignal : uint32_t {
@@ -728,3 +733,5 @@ static_assert(false, "non supported ROUTING_MODE: " TOSTRING(ROUTING_MODE));
 #endif  // ROUTING_MODE
 
 }  // namespace tt::tt_fabric
+
+#pragma GCC diagnostic pop
