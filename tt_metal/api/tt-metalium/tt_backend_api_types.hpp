a3449db65c44ef094e502f84b977abc21811e949
riverwuTT <riverwu@tenstorrent.com>
2025-11-04 20:10:06 +0000

    Move nonconstexpr implementation out of API

--- tt_metal/api/tt-metalium/tt_backend_api_types.hpp
+++ tt_metal/api/tt-metalium/tt_backend_api_types.hpp
@@ -6,10 +6,7 @@
 
 #include <cstddef>
 #include <cstdint>
-#include <fstream>
 #include <functional>
-#include <stdexcept>
-#include <string>
 
 #include <fmt/base.h>
 #include <umd/device/types/arch.hpp>
@@ -48,42 +45,20 @@ enum class DataFormat : uint8_t {
 /**
  * @brief Evaluates to true if the data format is an integer type.
  */
-inline bool is_integer_format(DataFormat format) {
-    return (
-        (format == DataFormat::UInt32) || (format == DataFormat::Int8) || (format == DataFormat::UInt16) ||
-        (format == DataFormat::UInt8) || (format == DataFormat::Int32) || (format == DataFormat::RawUInt32) ||
-        (format == DataFormat::RawUInt16) || (format == DataFormat::RawUInt8));
-}
+bool is_integer_format(DataFormat format);
 
-inline std::ostream& operator<<(std::ostream& os, const DataFormat& format) {
-    switch (format) {
-        case DataFormat::Bfp2: os << "Bfp2"; break;
-        case DataFormat::Bfp2_b: os << "Bfp2_b"; break;
-        case DataFormat::Bfp4: os << "Bfp4"; break;
-        case DataFormat::Bfp4_b: os << "Bfp4_b"; break;
-        case DataFormat::Bfp8: os << "Bfp8"; break;
-        case DataFormat::Bfp8_b: os << "Bfp8_b"; break;
-        case DataFormat::Float16: os << "Float16"; break;
-        case DataFormat::Float16_b: os << "Float16_b"; break;
-        case DataFormat::Float32: os << "Float32"; break;
-        case DataFormat::Tf32: os << "Tf32"; break;
-        case DataFormat::Int8: os << "Int8"; break;
-        case DataFormat::UInt8: os << "UInt8"; break;
-        case DataFormat::Lf8: os << "Lf8"; break;
-        case DataFormat::UInt16: os << "UInt16"; break;
-        case DataFormat::UInt32: os << "UInt32"; break;
-        case DataFormat::Int32: os << "Int32"; break;
-        case DataFormat::RawUInt8: os << "RawUInt8"; break;
-        case DataFormat::RawUInt16: os << "RawUInt16"; break;
-        case DataFormat::RawUInt32: os << "RawUInt32"; break;
-        case DataFormat::Invalid: os << "Invalid"; break;
-        default: throw std::invalid_argument("Unknown format");
-    }
-    return os;
-}
+std::ostream& operator<<(std::ostream& os, const DataFormat& format);
 
-// Size of datum in bytes
-constexpr static uint32_t datum_size(const DataFormat& format) {
+/**
+ * Returns size of datum in bytes
+ *
+ * Return value: uint32_t
+ *
+ * | Argument    | Description    | Type                | Valid Range | Required |
+ * |-------------|----------------|---------------------|-------------|----------|
+ * | format      | Format of data | tt::DataFormat enum |             | Yes      |
+ */
+constexpr uint32_t datum_size(const DataFormat& format) {
     switch (format) {
         case DataFormat::Bfp2:
         case DataFormat::Bfp2_b:
