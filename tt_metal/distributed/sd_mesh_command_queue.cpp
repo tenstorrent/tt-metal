0ab41b9fd09b48c57a4fb60b16a6cd413cc01e54
Austin Ho <aho@tenstorrent.com>
2025-11-04 15:22:36 +0000

    #0: Fix finish calls to do a host synchronize for the mesh device to prevent races between hosts in a BigMesh

--- tt_metal/distributed/sd_mesh_command_queue.cpp
+++ tt_metal/distributed/sd_mesh_command_queue.cpp
@@ -7,6 +7,7 @@
 #include "tt_metal/common/thread_pool.hpp"
 #include <mesh_device.hpp>
 #include <mesh_event.hpp>
+#include <tt-metalium/control_plane.hpp>
 #include <tt-metalium/tt_metal.hpp>
 #include <tt-metalium/graph_tracking.hpp>
 #include <utility>
@@ -108,6 +109,13 @@ void SDMeshCommandQueue::finish(tt::stl::Span<const SubDeviceId>) {
     for (const auto& device : mesh_device_->get_devices()) {
         tt::tt_metal::MetalContext::instance().get_cluster().dram_barrier(device->id());
         tt::tt_metal::MetalContext::instance().get_cluster().l1_barrier(device->id());
+    }
+
+    // Barrier across all hosts of the mesh
+    auto distributed_context = tt::tt_metal::MetalContext::instance().get_control_plane().get_distributed_context(
+        mesh_device_->get_view().mesh_id());
+    if (*(distributed_context->size()) > 1) {
+        distributed_context->barrier();
     }
 }
 
