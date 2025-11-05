b70cac0704cd31f6e3f75b6ce34ab1d461e8f35a
Gongyu Wang <gwang@tenstorrent.com>
2025-11-05 17:18:30 -0500

    Add max prefill chunk size for Qwen2.5-VL-7B (#31831)
    ### Ticket
    Link to Github Issue
    
    ### Problem description
    Previous PR fixed 7B variant of Qwen2.5-VL model but missed a
    configuration for max prefill chunk size.
    
    ### What's changed
    This PR added the missing configuration for Qwen2.5-VL-7B.
    
    ### Checklist
    - [x] [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes
    - https://github.com/tenstorrent/tt-metal/actions/runs/19079329340 -->
    failed cases not related to this PR.

--- models/tt_transformers/tt/model_config.py
+++ models/tt_transformers/tt/model_config.py
@@ -590,6 +590,7 @@ class ModelArgs:
                 "Qwen2.5-7B": {"N150": 4, "N300": 32, "T3K": 128, "TG": 128, "P150x4": 128},
                 "Qwen2.5-72B": {"N150": None, "N300": None, "T3K": 16, "TG": 128, "P150x4": 128},
                 "Qwen2.5-VL-3B": {"N150": 128, "N300": 128, "T3K": None, "TG": None, "P150x4": None},
+                "Qwen2.5-VL-7B": {"N150": 64, "N300": 128, "T3K": None, "TG": None, "P150x4": None},
                 "Qwen2.5-VL-32B": {"N150": None, "N300": None, "T3K": 64, "TG": None, "P150x4": None},
                 "Qwen2.5-VL-72B": {"N150": None, "N300": None, "T3K": 32, "TG": None, "P150x4": None},
                 "DeepSeek-R1-Distill-Qwen-14B": {"N150": 4, "N300": 64, "T3K": 128, "TG": None, "P150x4": None},
