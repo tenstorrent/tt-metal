a9e6c45e31ad1092a26dd50a0f5d1e4750cdce7b
Sheran Cardoza <scardoza@tenstorrent.com>
2025-11-06 14:43:13 -0500

    #30296: Handle 2 devices with ring topology in CCLs (#30337)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/30296)
    
    ### Problem description
    Trying to initialize ring on 2 devices causes a fabric assertion as they
    interpret all links to be in one direction. In these cases we should
    initialize with line instead. At the OP creation level we need if the
    user requests ring and 2 devices, it should override the topology to be
    line instead.
    
    ### What's changed
    - Updated every CCL op to switch to Line topology if user requests Ring
    topology with 2 devices
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/18984930033)
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/18695946279)
    - [x] [Blackhole nightly
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/19075107258)
    - [x] [Blackhole demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/18695975067)
    - [x] [T3K nightly
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/18695990798)
    - [x] [T3K unit
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/18696004184)
    - [x] [T3K demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/18984964011)
    - [x] [Galaxy demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/18984974767)

--- ttnn/cpp/ttnn/operations/ccl/all_to_all_combine/all_to_all_combine.cpp
+++ ttnn/cpp/ttnn/operations/ccl/all_to_all_combine/all_to_all_combine.cpp
@@ -34,8 +34,7 @@ ttnn::Tensor ExecuteAllToAllCombine::invoke(
     auto subdevice_core_range_set = mesh_device->worker_cores(tt::tt_metal::HalProgrammableCoreType::TENSIX, sd_id);
     uint32_t shard_dim = output_shard_dim.value_or(1);
     uint32_t num_links_ = num_links.value_or(common::get_num_links(*mesh_device, axis));
-    tt::tt_fabric::Topology topology_ =
-        topology.value_or(::ttnn::ccl::get_usable_topology(input_tensor, tt::tt_fabric::get_fabric_topology(), axis));
+    tt::tt_fabric::Topology topology_ = ::ttnn::ccl::get_usable_topology(input_tensor, topology, axis);
     auto memory_config_ = memory_config.value_or(input_tensor.memory_config());
 
     // create zeros tensor
