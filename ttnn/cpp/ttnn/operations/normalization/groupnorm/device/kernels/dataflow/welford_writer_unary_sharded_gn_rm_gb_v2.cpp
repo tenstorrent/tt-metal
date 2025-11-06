4117d4b03b91095313d13c8c15b4855233b4de95
Nikhil Soraba <nsoraba@tenstorrent.com>
2025-11-06 06:47:24 +0000

    Add new writer kernels

--- ttnn/cpp/ttnn/operations/normalization/groupnorm/device/kernels/dataflow/welford_writer_unary_sharded_gn_rm_gb_v2.cpp
+++ ttnn/cpp/ttnn/operations/normalization/groupnorm/device/kernels/dataflow/welford_writer_unary_sharded_gn_rm_gb_v2.cpp
@@ -2,8 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#include <cstdint>
-#include "tt-metalium/constants.hpp"
+#include <stdint.h>
 #include "dataflow_api.h"
 #include "hostdevcommon/common_values.hpp"
 #include "ttnn/deprecated/tt_dnn/kernels/dataflow/generate_bcast_scalar.hpp"
@@ -68,38 +67,22 @@ void kernel_main() {
 
     if constexpr (fuse_gamma) {
         constexpr uint32_t gamma_tile_bytes = get_tile_size(cb_gamma);
-        constexpr uint32_t gamma_element_bytes = gamma_tile_bytes / tt::constants::TILE_HW;
-        constexpr uint32_t gamma_face_bytes = gamma_element_bytes * tt::constants::FACE_HW;
-        constexpr uint32_t gamma_face_w_bytes = gamma_element_bytes * tt::constants::FACE_WIDTH;
         const auto gamma = TensorAccessor(gamma_args, gamma_addr, size);
 
         cb_reserve_back(cb_gamma, num_cols_tile_gamma_beta);
         auto l1_write_addr_gamma = get_write_ptr(cb_gamma);
-
-        // We want this data to appear as the first row of the tile.
-        // This is 32B at the start of the first face, 32B at the start of the second face
-        // However we must read at a 64 byte granularity for Blackhole NOC compatibility on DRAM reads
-        // So instead of two 32B reads to the correct addresses, we read 64 bytes into the first face here
-        // Then later, copy the second set of 32 bytes into the start of the second face
-        // L1-L1 NOC transactions only need 16 byte alignment on BH, so this is legal after data is loaded
-        // to L1
-
         for (uint32_t w = 0; w < num_cols_tile_gamma_beta; w++) {
             uint32_t tile_id = gamma_tile_start_id + w;
             uint64_t gamma_noc_addr = get_noc_addr(tile_id, gamma);
-
-            // Read the first 64 bytes of the tile into the first face
 #ifdef ARCH_BLACKHOLE
-            noc_async_read(gamma_noc_addr, l1_write_addr_gamma, NOC_DRAM_READ_ALIGNMENT_BYTES);
-            gamma_noc_addr = get_noc_addr(l1_write_addr_gamma + gamma_face_w_bytes);
+            noc_async_read(gamma_noc_addr, l1_write_addr_gamma, 32 * 2);
+            gamma_noc_addr = get_noc_addr(l1_write_addr_gamma + 32);
             noc_async_read_barrier();
 #else
-            noc_async_read(gamma_noc_addr, l1_write_addr_gamma, gamma_face_w_bytes);
-            gamma_noc_addr += gamma_face_w_bytes;
+            noc_async_read(gamma_noc_addr, l1_write_addr_gamma, 32);
+            gamma_noc_addr += 32;
 #endif
-
-            // Copy the second set of 32 bytes into the second face
-            noc_async_read(gamma_noc_addr, l1_write_addr_gamma + gamma_face_bytes, gamma_face_w_bytes);
+            noc_async_read(gamma_noc_addr, l1_write_addr_gamma + 512, 32);
             l1_write_addr_gamma += gamma_tile_bytes;
         }
         noc_async_read_barrier();
@@ -107,34 +90,23 @@ void kernel_main() {
     }
 
     if constexpr (fuse_beta) {
-        // Just like gamma, we read at a 64 byte granularity for Blackhole NOC compatibility
-        // Then copy the second set of 32 bytes into the second face
-
         constexpr uint32_t beta_tile_bytes = get_tile_size(cb_beta);
-        constexpr uint32_t beta_element_bytes = beta_tile_bytes / tt::constants::TILE_HW;
-        constexpr uint32_t beta_face_bytes = beta_element_bytes * tt::constants::FACE_HW;
-        constexpr uint32_t beta_face_w_bytes = beta_element_bytes * tt::constants::FACE_WIDTH;
         const auto beta = TensorAccessor(beta_args, beta_addr, size);
 
         cb_reserve_back(cb_beta, num_cols_tile_gamma_beta);
         auto l1_write_addr_beta = get_write_ptr(cb_beta);
-
         for (uint32_t w = 0; w < num_cols_tile_gamma_beta; w++) {
             uint32_t tile_id = beta_tile_start_id + w;
             uint64_t beta_noc_addr = get_noc_addr(tile_id, beta);
-
-            // Read the first 64 bytes of the tile into the first face
 #ifdef ARCH_BLACKHOLE
-            noc_async_read(beta_noc_addr, l1_write_addr_beta, NOC_DRAM_READ_ALIGNMENT_BYTES);
-            beta_noc_addr = get_noc_addr(l1_write_addr_beta + beta_face_w_bytes);
+            noc_async_read(beta_noc_addr, l1_write_addr_beta, 32 * 2);
+            beta_noc_addr = get_noc_addr(l1_write_addr_beta + 32);
             noc_async_read_barrier();
 #else
-            noc_async_read(beta_noc_addr, l1_write_addr_beta, beta_face_w_bytes);
-            beta_noc_addr += beta_face_w_bytes;
+            noc_async_read(beta_noc_addr, l1_write_addr_beta, 32);
+            beta_noc_addr += 32;
 #endif
-
-            // Copy the second set of 32 bytes into the second face
-            noc_async_read(beta_noc_addr, l1_write_addr_beta + beta_face_bytes, beta_face_w_bytes);
+            noc_async_read(beta_noc_addr, l1_write_addr_beta + 512, 32);
             l1_write_addr_beta += beta_tile_bytes;
         }
         noc_async_read_barrier();
