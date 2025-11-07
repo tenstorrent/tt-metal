0be8ad32acc355ff2ff2ce26bed798a10043f4ea
Nigel Huang <nhuang@tenstorrent.com>
2025-11-06 17:12:43 -0800

    Update comment

--- tt_telemetry/telemetry/hal/hal.cpp
+++ tt_telemetry/telemetry/hal/hal.cpp
@@ -12,10 +12,10 @@ std::unique_ptr<tt::tt_metal::Hal> create_hal(const std::unique_ptr<tt::umd::Clu
     tt::umd::ClusterDescriptor* cluster_descriptor = cluster->get_cluster_description();
     bool is_base_routing_fw_enabled =
         tt::Cluster::is_base_routing_fw_enabled(tt::Cluster::get_cluster_type_from_cluster_desc(rtoptions, cluster_descriptor));
-    // is_2_erisc_mode is true. But it can change to false on the Metal runtime to maintain backward compabaility with
-    // older firmware.
-    // This value doesn't affect the addresses of any buffers.
-    // Telemetry doesn't care about number of ERISC processors as it reads all ERISCs anyway, so it's ok to not be in sync with the value on Metal. 
-    bool is_2_erisc_mode = rtoptions.get_enable_2_erisc_mode();
+    // Telemetry currently doesn't collect data on the granularity of each ERISC processor / RISCV.
+    // is_2_erisc_mode is hardcoded to True to show 2 ERISC processors on Blackhole with the Hal::get_num_risc_processors API.
+    // This parameter doesn't affect the addresses of any buffers in L1. If Metal only has 1 ERISC enabled for Blackhole then telemtry
+    // will simply read empty values for the other ERISC.
+    constexpr bool is_2_erisc_mode = true;
     return std::make_unique<tt::tt_metal::Hal>(tt::tt_metal::get_platform_architecture(rtoptions), is_base_routing_fw_enabled, is_2_erisc_mode);
 }
