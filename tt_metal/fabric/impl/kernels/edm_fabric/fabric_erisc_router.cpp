bb8104d22c6aabc911af7bb487345a05d50554c2
Bart Trzynadlowski <btrzynadlowski@tenstorrent.com>
2025-11-07 18:49:18 +0000

    Sync with Daiki's branch

--- tt_metal/fabric/impl/kernels/edm_fabric/fabric_erisc_router.cpp
+++ tt_metal/fabric/impl/kernels/edm_fabric/fabric_erisc_router.cpp
@@ -2193,9 +2193,6 @@ FORCE_INLINE void run_fabric_edm_main_loop(
                 }
             }
         }
-
-        local_heartbeat_counter++;
-        *heartbeat_addr = local_heartbeat_counter;
     }
 }
 
