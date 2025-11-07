7601cbd3b83b32314b291a07b52dc29390a9fd00
awliu-TT <awliu@tenstorrent.com>
2025-11-07 08:05:20 +0000

    making QueueId argument optional in record_mesh_event and wait_for_mesh_event

--- ttnn/core/events.cpp
+++ ttnn/core/events.cpp
@@ -18,14 +18,23 @@ using ::tt::tt_metal::distributed::EventSynchronize;
 
 MeshEvent record_mesh_event(
     MeshDevice* mesh_device,
-    QueueId cq_id,
+    std::optional<QueueId> cq_id,
     const std::vector<tt::tt_metal::SubDeviceId>& sub_device_ids,
     const std::optional<ttnn::MeshCoordinateRange>& device_range) {
-    return mesh_device->mesh_command_queue(*cq_id).enqueue_record_event_to_host(sub_device_ids, device_range);
+    if (!cq_id.has_value()) {
+        return mesh_device->mesh_command_queue().enqueue_record_event_to_host(sub_device_ids, device_range);
+    } else {
+        return mesh_device->mesh_command_queue(*(cq_id.value()))
+            .enqueue_record_event_to_host(sub_device_ids, device_range);
+    }
 }
 
-void wait_for_mesh_event(QueueId cq_id, const MeshEvent& event) {
-    event.device()->mesh_command_queue(*cq_id).enqueue_wait_for_event(event);
+void wait_for_mesh_event(std::optional<QueueId> cq_id, const MeshEvent& event) {
+    if (!cq_id.has_value()) {
+        event.device()->mesh_command_queue().enqueue_wait_for_event(event);
+    } else {
+        event.device()->mesh_command_queue(*(cq_id.value())).enqueue_wait_for_event(event);
+    }
 }
 
 void event_synchronize(const MeshEvent& event) { EventSynchronize(event); }
