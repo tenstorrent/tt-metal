22d73c8a461dbc6c3e28c6f04d875c3e939596bc
Nigel Huang <nhuang@tenstorrent.com>
2025-11-06 16:38:47 -0800

    Add comment

--- tt_telemetry/telemetry/hal/hal.cpp
+++ tt_telemetry/telemetry/hal/hal.cpp
@@ -12,6 +12,10 @@ std::unique_ptr<tt::tt_metal::Hal> create_hal(const std::unique_ptr<tt::umd::Clu
     tt::umd::ClusterDescriptor* cluster_descriptor = cluster->get_cluster_description();
     bool is_base_routing_fw_enabled =
         tt::Cluster::is_base_routing_fw_enabled(tt::Cluster::get_cluster_type_from_cluster_desc(rtoptions, cluster_descriptor));
+    // is_2_erisc_mode is true. But it can change to false on the Metal runtime to maintain backward compabaility with
+    // older firmware.
+    // This value doesn't affect the addresses of any buffers.
+    // Telemetry doesn't care about number of ERISC processors so it's ok to not be in sync with the value on Metal. 
     bool is_2_erisc_mode = rtoptions.get_enable_2_erisc_mode();
     return std::make_unique<tt::tt_metal::Hal>(tt::tt_metal::get_platform_architecture(rtoptions), is_base_routing_fw_enabled, is_2_erisc_mode);
 }
