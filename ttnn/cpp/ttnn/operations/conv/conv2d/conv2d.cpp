e20db763d39d7186738b6c920741918e24e62393
edwinleeTT <edwinlee@tenstorrent.com>
2025-11-07 20:22:44 +0000

    Conv fix - conv2d was making premature reference to value without checking if optional activation param had a value

--- ttnn/cpp/ttnn/operations/conv/conv2d/conv2d.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/conv2d.cpp
@@ -287,7 +287,7 @@ Result conv2d_DRAM(
             mm_output_memory_config,
             output_dtype,
             program_config,
-            conv_config.activation.value(),
+            conv_config.activation,
             compute_config);
         if (conv_config.deallocate_activation) {
             input_tensor_on_device.deallocate(true);
