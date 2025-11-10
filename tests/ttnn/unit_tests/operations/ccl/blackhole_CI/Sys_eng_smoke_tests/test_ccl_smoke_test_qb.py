0259d6071d5f1a4db84544d962481000b7535fe6
Juan Camilo Vega <jvega@tenstorrent.com>
2025-11-10 20:32:25 +0000

    Remove unnecessary import

--- tests/ttnn/unit_tests/operations/ccl/blackhole_CI/Sys_eng_smoke_tests/test_ccl_smoke_test_qb.py
+++ tests/ttnn/unit_tests/operations/ccl/blackhole_CI/Sys_eng_smoke_tests/test_ccl_smoke_test_qb.py
@@ -6,7 +6,7 @@ import pytest
 import ttnn
 
 from tests.nightly.t3000.ccl.test_minimal_all_gather_async import run_all_gather_impl
-from models.common.utility_functions import skip_for_blackhole, skip_for_wormhole_b0, run_for_n_dev
+from models.common.utility_functions import skip_for_wormhole_b0, run_for_n_dev
 from tests.ttnn.unit_tests.operations.ccl.blackhole_CI.box.nightly.test_all_gather_nightly import validate_test
 
 
