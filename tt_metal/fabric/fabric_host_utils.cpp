6508bf46ba7e5eacb04d3ba87b0bd653f181b321
Ridvan Song <Riddy21@users.noreply.github.com>
2025-11-10 20:42:10 +0000

    Fixing 1x2 case

--- tt_metal/fabric/fabric_host_utils.cpp
+++ tt_metal/fabric/fabric_host_utils.cpp
@@ -35,11 +35,10 @@ FabricType get_fabric_type(tt::tt_fabric::FabricConfig fabric_config) {
         // WH T3K currently do not support Torus_XY fabric type, because they do not have wrapping connections.
         // If you want to use 1D Ring on t3k please use 1x8 MGD.
         case tt::tt_fabric::FabricConfig::FABRIC_1D_RING: {
-            if ((cluster_type == tt::tt_metal::ClusterType::T3K) ||
-                (cluster_type == tt::tt_metal::ClusterType::P150_X8)) {
-                return FabricType::MESH;
+            if (cluster_type == tt::tt_metal::ClusterType::GALAXY) {
+                return FabricType::TORUS_XY;
             }
-            return FabricType::TORUS_XY;
+            return FabricType::MESH;
         }
         case tt::tt_fabric::FabricConfig::FABRIC_2D_TORUS_X: return FabricType::TORUS_X;
         case tt::tt_fabric::FabricConfig::FABRIC_2D_TORUS_Y: return FabricType::TORUS_Y;
