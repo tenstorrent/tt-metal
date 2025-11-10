d31c59a56d9122dc92d5aa2d884253cf8cb95a08
Edwin Lee <edwinlee@tenstorrent.com>
2025-11-10 18:53:37 +0000

    Revert "conv changes"
    This reverts commit 6d68589b941e4c78b15c10c7e9297a5f8d30843f.

--- ttnn/cpp/ttnn/operations/eltwise/unary/common/unary_op_utils.hpp
+++ ttnn/cpp/ttnn/operations/eltwise/unary/common/unary_op_utils.hpp
@@ -14,6 +14,8 @@ namespace ttnn::operations::unary::utils {
 
 UnaryWithParam string_to_unary_with_param(const std::string& name);
 
+std::string unary_with_param_to_string(const UnaryWithParam& unary_op);
+
 bool get_op_approx_mode(UnaryOpType op_type);
 using DataType = tt::tt_metal::DataType;
 
