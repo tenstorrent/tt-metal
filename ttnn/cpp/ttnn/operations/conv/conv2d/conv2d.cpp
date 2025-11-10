621466540a40743153d97fc7ce376ef1cf66bed5
Billy Ransom <wransom@tenstorrent.com>
2025-11-10 09:37:36 -0800

    Remove In Place Halo (#31516) N/A In Place Halo is buggy, non-performant, and in most practical cases uses more memory than normal halo. In Place Halo has been removed. - [x] [All post commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml) CI passes: https://github.com/tenstorrent/tt-metal/actions/runs/19160386656 - [x] [Blackhole Post commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml) CI with demo tests passes (if applicable): https://github.com/tenstorrent/tt-metal/actions/runs/19160388541 - [x] [Single-card demo tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml) CI passes: https://github.com/tenstorrent/tt-metal/actions/runs/19160384623 Failing on main this PR hits same unrelated error - [x] [Nightly L2](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml) CI passes: https://github.com/tenstorrent/tt-metal/actions/runs/19160391294 - [x] [Nightly Blackhole](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-nightly-tests.yaml) CI passes: https://github.com/tenstorrent/tt-metal/actions/runs/19160394014 Failing on main this PR hits same unrelated error - [x] [Frequent model](https://github.com/tenstorrent/tt-metal/actions/workflows/fast-dispatch-full-regressions-and-models.yaml) CI passes: https://github.com/tenstorrent/tt-metal/actions/runs/19160398249 Failing on main this PR hits same unrelated error - [x] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/conv/conv2d/conv2d.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/conv2d.cpp
@@ -859,7 +859,6 @@ Result conv2d_L1(
                 parallel_config.shard_orientation == ShardOrientation::COL_MAJOR,
                 input_tensor_post_tm.memory_config(),
                 true,
-                conv_config.in_place,
                 conv_config.config_tensors_in_dram);
 
             if (conv_config.deallocate_activation) {
