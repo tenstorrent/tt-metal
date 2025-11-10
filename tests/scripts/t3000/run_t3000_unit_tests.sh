29b8d3f5f0887c770a564e4932f954bb5d60682c
Rose Li <roseli@tenstorrent.com>
2025-11-10 17:54:04 -0500

    [skip ci] Disable multi device t3k test (#32043)
    Temporarily disabling t3k unit test until triaged
    
    Tracked by issue https://github.com/tenstorrent/tt-metal/issues/32042
    
    Testing:
    
    - [x] https://github.com/tenstorrent/tt-metal/actions/runs/19182099121

--- tests/scripts/t3000/run_t3000_unit_tests.sh
+++ tests/scripts/t3000/run_t3000_unit_tests.sh
@@ -103,11 +103,11 @@ run_t3000_ttnn_tests() {
   ./build/test/ttnn/unit_tests_ttnn_ccl_ops
   ./build/test/ttnn/unit_tests_ttnn_accessor
   ./build/test/ttnn/test_ccl_multi_cq_multi_device
-  pytest tests/ttnn/unit_tests/base_functionality/test_multi_device_trace.py ; fail+=$?
-  pytest tests/ttnn/unit_tests/base_functionality/test_multi_device_events.py ; fail+=$?
+  # pytest tests/ttnn/unit_tests/base_functionality/test_multi_device_trace.py ; fail+=$?
+  # pytest tests/ttnn/unit_tests/base_functionality/test_multi_device_events.py ; fail+=$?
   pytest tests/ttnn/unit_tests/operations/transformers/test_prefetcher.py::test_run_prefetcher_post_commit_multi_device ; fail+=$?
-  pytest -n auto tests/ttnn/unit_tests/base_functionality/test_multi_device.py ; fail+=$?
-  pytest -n auto tests/ttnn/unit_tests/base_functionality/test_multi_device_async.py ; fail+=$?
+  # pytest -n auto tests/ttnn/unit_tests/base_functionality/test_multi_device.py ; fail+=$?
+  # pytest -n auto tests/ttnn/unit_tests/base_functionality/test_multi_device_async.py ; fail+=$?
   pytest tests/ttnn/distributed/test_tensor_parallel_example_T3000.py ; fail+=$?
   pytest tests/ttnn/distributed/test_data_parallel_example.py ; fail+=$?
   pytest tests/ttnn/distributed/test_hybrid_data_tensor_parallel_example_T3000.py ; fail+=$?
