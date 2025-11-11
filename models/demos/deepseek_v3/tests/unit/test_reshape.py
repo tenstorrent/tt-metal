620ef23ffaf513d3fb4c6a2534ad8c746c1a0dda
Sheran Cardoza <scardoza@tenstorrent.com>
2025-11-11 21:03:07 +0000

    adding trace run for reshape

--- models/demos/deepseek_v3/tests/unit/test_reshape.py
+++ models/demos/deepseek_v3/tests/unit/test_reshape.py
@@ -2,11 +2,11 @@
 #
 #  SPDX-License-Identifier: Apache-2.0
 
-
 import pytest
 import torch
 
 import ttnn
+from models.common.utility_functions import profiler
 from tests.ttnn.utils_for_testing import assert_with_pcc
 
 
@@ -18,6 +18,52 @@ def random_torch_tensor(dtype, shape):
         return torch.rand(shape, dtype=torch.float32)
     if dtype == ttnn.bfloat16:
         return torch.rand(shape, dtype=torch.bfloat16)
+
+
+def run_test(mesh_device, run_op_proc, check_op_proc):
+    ####################### NON-TRACE RUN #######################
+
+    # Run the op
+    tt_output = run_op_proc()
+    tt_output = ttnn.to_torch(tt_output, mesh_composer=ttnn.ConcatMeshToTensor(mesh_device, dim=0))
+
+    # Every device executes the same op, check that each device returned the
+    # same result
+    N = tt_output.shape[0] // mesh_device.get_num_devices()
+    for dev in range(mesh_device.get_num_devices()):
+        i = dev * N
+        j = (dev + 1) * N
+        check_op_proc(tt_output[i:j, ...])
+
+    ####################### TRACE RUN #######################
+
+    # Compile the op
+    tt_output = run_op_proc()
+    tt_output.deallocate(True)
+
+    # Capture the trace
+    trace_id = ttnn.begin_trace_capture(mesh_device, cq_id=0)
+    tt_output = run_op_proc()
+    ttnn.end_trace_capture(mesh_device, trace_id, cq_id=0)
+
+    # Execute trace
+    profiler.start("op_perf")
+    ttnn.execute_trace(mesh_device, trace_id, cq_id=0, blocking=False)
+    profiler.end("op_perf")
+    ttnn.release_trace(mesh_device, trace_id)
+
+    # Get results
+    tt_output = ttnn.to_torch(tt_output, mesh_composer=ttnn.ConcatMeshToTensor(mesh_device, dim=0))
+    # op_time = profiler.get("op_perf")
+    profiler.print(units="us")
+
+    # Every device executes the same op, check that each device returned the
+    # same result
+    N = tt_output.shape[0] // mesh_device.get_num_devices()
+    for dev in range(mesh_device.get_num_devices()):
+        i = dev * N
+        j = (dev + 1) * N
+        check_op_proc(tt_output[i:j, ...])
 
 
 @pytest.mark.parametrize("mesh_device", [(8, 8)], indirect=True)
@@ -39,8 +85,10 @@ def random_torch_tensor(dtype, shape):
     ],
 )
 @pytest.mark.parametrize("dtype", [ttnn.bfloat16])
-@pytest.mark.parametrize("device_params", [{"fabric_config": ttnn.FabricConfig.FABRIC_1D}], indirect=True)
-def test_repeat(mesh_device, in_shape, out_shape, layout, mem_config, dtype):
+@pytest.mark.parametrize(
+    "device_params", [{"trace_region_size": 10000, "fabric_config": ttnn.FabricConfig.FABRIC_1D}], indirect=True
+)
+def test_reshape(mesh_device, in_shape, out_shape, layout, mem_config, dtype):
     torch_input = random_torch_tensor(dtype, in_shape)
     torch_output = torch_input.reshape(out_shape)
 
@@ -52,12 +100,11 @@ def test_repeat(mesh_device, in_shape, out_shape, layout, mem_config, dtype):
         memory_config=mem_config,
         mesh_mapper=ttnn.ReplicateTensorToMesh(mesh_device),
     )
-    tt_output = ttnn.reshape(tt_input, out_shape)
-    tt_output = ttnn.to_torch(tt_output, mesh_composer=ttnn.ConcatMeshToTensor(mesh_device, dim=0))
 
-    # Every device executes the same op, check that each device returned the
-    # same result
-    for dev in range(mesh_device.get_num_devices()):
-        i = dev * out_shape[0]
-        j = (dev + 1) * out_shape[0]
-        assert_with_pcc(torch_output, tt_output[i:j, ...], 0.9999)
+    def run_op():
+        return ttnn.reshape(tt_input, out_shape)
+
+    def check_op(tt_output):
+        assert_with_pcc(torch_output, tt_output, 0.9999)
+
+    run_test(mesh_device, run_op, check_op)
