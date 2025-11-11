d8f305b0e0ec88537681b0f7456724ab8505b117
Brian Liu <bliu@tenstorrent.com>
2025-11-11 21:27:54 +0000

    #0: Refactor

--- ttnn/cpp/ttnn/operations/experimental/deepseek_b1/matmul_1d/device/kernels/compute.cpp
+++ ttnn/cpp/ttnn/operations/experimental/deepseek_b1/matmul_1d/device/kernels/compute.cpp
@@ -10,23 +10,13 @@
 namespace NAMESPACE {
 
 void MAIN {
-    constexpr uint32_t in0_block_w = get_compile_time_arg_val(0);        // inner block size in tiles
-    constexpr uint32_t in0_num_subblocks = get_compile_time_arg_val(1);  // outer row block size (in inner row blocks)
-    constexpr uint32_t in0_block_num_tiles =
-        get_compile_time_arg_val(2);  // out_subblock_h*in0_block_w*in0_num_subblocks;
-    constexpr uint32_t in0_subblock_num_tiles = get_compile_time_arg_val(3);  // out_subblock_h*in0_block_w
-    constexpr uint32_t in1_num_subblocks =
-        get_compile_time_arg_val(4);  // outer column block size (in inner column blocks)
-    constexpr uint32_t in1_block_num_tiles =
-        get_compile_time_arg_val(5);                               // out_subblock_w*in0_block_w* in1_num_subblocks;
-    constexpr uint32_t in1_block_w = get_compile_time_arg_val(6);  // out_subblock_w*in1_num_subblocks
-    constexpr uint32_t num_blocks_inner_dim = get_compile_time_arg_val(7);     // outer inner dim (in inner dim blocks)
-    constexpr uint32_t num_blocks_w_dim = get_compile_time_arg_val(8);         // outer inner dim (in inner dim blocks)
-    constexpr uint32_t num_blocks_h_dim = get_compile_time_arg_val(9);         // outer inner dim (in inner dim blocks)
+    constexpr uint32_t in0_block_w = get_compile_time_arg_val(0);              // inner block size in tiles
+    constexpr uint32_t in0_block_num_tiles = get_compile_time_arg_val(2);      // out_subblock_h*in0_block_w
+    constexpr uint32_t in1_block_num_tiles = get_compile_time_arg_val(5);      // out_subblock_w*in0_block_w
+    constexpr uint32_t in1_block_w = get_compile_time_arg_val(6);              // out_subblock_w
     constexpr uint32_t out_subblock_h = get_compile_time_arg_val(10);          // inner row block size in tiles
     constexpr uint32_t out_subblock_w = get_compile_time_arg_val(11);          // inner column block size in tiles
-    constexpr uint32_t out_subblock_num_tiles = get_compile_time_arg_val(12);  // out_subblock_h * out_subblock_w;
-    constexpr uint32_t batch = get_compile_time_arg_val(13);                   // batch dim
+    constexpr uint32_t out_subblock_num_tiles = get_compile_time_arg_val(12);  // out_subblock_h * out_subblock_w
     constexpr bool untilize_out = get_compile_time_arg_val(15);                // untilize output
 
     constexpr uint32_t in0_cb_id = tt::CBIndex::c_0;
@@ -41,69 +31,47 @@ void MAIN {
 
     mm_block_init(
         in0_cb_id, in1_cb_id, mm_partials_cb_id, in1_transpose_tile, out_subblock_w, out_subblock_h, in0_block_w);
-    for (uint32_t b = 0; b < batch; b++) {
-        for (uint32_t bh = 0; bh < num_blocks_h_dim; ++bh) {
-            for (uint32_t bw = 0; bw < num_blocks_w_dim; ++bw) {
-                for (uint32_t block = 0; block < num_blocks_inner_dim; block++) {
-                    bool last_out = block == (num_blocks_inner_dim - 1);
 
-                    cb_wait_front(in0_cb_id, in0_block_num_tiles);
-                    cb_wait_front(in1_cb_id, in1_block_num_tiles);
+    cb_wait_front(in0_cb_id, in0_block_num_tiles);
+    cb_wait_front(in1_cb_id, in1_block_num_tiles);
 
-                    int in0_index_subblock_offset = 0;
-                    for (uint32_t in0_subblock = 0; in0_subblock < in0_num_subblocks; in0_subblock++) {
-                        int in1_index_subblock_offset = 0;
-                        for (uint32_t in1_subblock = 0; in1_subblock < in1_num_subblocks; in1_subblock++) {
-                            tile_regs_acquire();
+    tile_regs_acquire();
 
-                            // Compute output sub-block
-                            uint32_t dst_index =
-                                0;  // start at 0, each call to matmul_block internally increments dst_index
-                            uint32_t in0_index = in0_index_subblock_offset;  // offset into in0 block
-                            uint32_t in1_index = in1_index_subblock_offset;  // offset into in1 block
-                            // inner dim that we accumualte is the inner dim of in0/in1, which is in0_block_w
-                            for (uint32_t inner_dim_idx = 0; inner_dim_idx < in0_block_w; ++inner_dim_idx) {
-                                // matmul outer product of (out_subblock_h x out_subblock_w) tiles that fill dst
-                                // accumulation is done by iterating matmul_block across inner dim
-                                // in0_block_w is passed as innder dim (kt) to matmul_block, interally used to stride
-                                // in0
-                                matmul_block(
-                                    in0_cb_id,
-                                    in1_cb_id,
-                                    in0_index,
-                                    in1_index,
-                                    dst_index,
-                                    in1_transpose_tile,
-                                    out_subblock_w,
-                                    out_subblock_h,
-                                    in0_block_w);
-                                in0_index++;               // stride right by 1
-                                in1_index += in1_block_w;  // to stride down by 1 need to stride by in_per_core_w
-                                                           // (should be called in1_block_w)
-                            }
-
-                            if (last_out) {
-                                tile_regs_commit();
-                                // Pack out to output buffer
-                                cb_reserve_back(mm_out_cb_id, out_subblock_num_tiles);
-                                tile_regs_wait();
-
-                                uint32_t start_dst_index = 0;
-                                pack_tile_block(start_dst_index, mm_out_cb_id, out_subblock_num_tiles);
-
-                                tile_regs_release();
-                                cb_push_back(mm_out_cb_id, out_subblock_num_tiles);
-                            }
-                            in1_index_subblock_offset += out_subblock_w;
-                        }
-                        in0_index_subblock_offset += in0_subblock_num_tiles;
-                    }
-
-                    cb_pop_front(in0_cb_id, in0_block_num_tiles);
-                    cb_pop_front(in1_cb_id, in1_block_num_tiles);
-                }
-            }
-        }
+    // Compute output sub-block
+    uint32_t dst_index = 0;  // start at 0, each call to matmul_block internally increments dst_index
+    uint32_t in0_index = 0;
+    uint32_t in1_index = 0;
+    // inner dim that we accumulate is the inner dim of in0/in1, which is in0_block_w
+    for (uint32_t inner_dim_idx = 0; inner_dim_idx < in0_block_w; ++inner_dim_idx) {
+        // matmul outer product of (out_subblock_h x out_subblock_w) tiles that fill dst
+        // accumulation is done by iterating matmul_block across inner dim
+        // in0_block_w is passed as inner dim (kt) to matmul_block, internally used to stride in0
+        matmul_block(
+            in0_cb_id,
+            in1_cb_id,
+            in0_index,
+            in1_index,
+            dst_index,
+            in1_transpose_tile,
+            out_subblock_w,
+            out_subblock_h,
+            in0_block_w);
+        in0_index++;               // stride right by 1
+        in1_index += in1_block_w;  // stride down by 1
     }
+
+    tile_regs_commit();
+    // Pack out to output buffer
+    cb_reserve_back(mm_out_cb_id, out_subblock_num_tiles);
+    tile_regs_wait();
+
+    uint32_t start_dst_index = 0;
+    pack_tile_block(start_dst_index, mm_out_cb_id, out_subblock_num_tiles);
+
+    tile_regs_release();
+    cb_push_back(mm_out_cb_id, out_subblock_num_tiles);
+
+    cb_pop_front(in0_cb_id, in0_block_num_tiles);
+    cb_pop_front(in1_cb_id, in1_block_num_tiles);
 }
 }  // namespace NAMESPACE
