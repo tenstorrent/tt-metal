a8045e13cb9bffbdb0f10907e164a3f3a719350a
aliraza556 <alirazaakhtar1122@gmail.com>
2025-11-12 12:30:46 +0500

    Fix boolean logical operations for INT32/UINT16/UINT32 data types

--- ttnn/cpp/ttnn/operations/eltwise/binary_ng/device/binary_ng_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/binary_ng/device/binary_ng_device_operation.cpp
@@ -19,13 +19,24 @@ bool is_binary_sfpu_op(BinaryOpType val, DataType a, DataType b, bool fast_and_a
         case MUL:
         case EQ:
         case NE:
-            return (
-                (a == FLOAT32 && b == FLOAT32) || (a == INT32 && b == INT32) || (a == UINT32 && b == UINT32) ||
-                (a == UINT16 && b == UINT16));
+            return a == b && (
+                a == FLOAT32 ||
+                a == INT32 ||
+                a == UINT32 ||
+                a == UINT16);
         case LOGICAL_AND:
         case LOGICAL_OR:
-        case LOGICAL_XOR: return a == b && (a == FLOAT32 || a == INT32 || a == UINT32 || a == UINT16);
-        case SQUARED_DIFFERENCE: return a == b && (a == FLOAT32 || a == INT32 || a == UINT16);
+        case LOGICAL_XOR:
+            return a == b && (
+                a == FLOAT32 ||
+                a == INT32 ||
+                a == UINT32 ||
+                a == UINT16);
+        case SQUARED_DIFFERENCE:
+            return a == b && (
+                a == FLOAT32 ||
+                a == INT32 ||
+                a == UINT16);
         case LOGADDEXP:
         case LOGADDEXP2:
         case LDEXP:
