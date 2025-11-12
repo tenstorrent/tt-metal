95ac69744d68d601e711c6e7d6e1745425b618ec
Sheran Cardoza <scardoza@tenstorrent.com>
2025-11-12 18:04:24 +0000

    add pytests for transpose

---
+++ models/demos/deepseek_v3/tests/unit/test_transpose.py
@@ -0,0 +1,161 @@
+#  SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+#
+#  SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import torch
+
+import ttnn
+from models.common.utility_functions import profiler
+from tests.ttnn.utils_for_testing import assert_with_pcc
+
+
+def random_torch_tensor(dtype, shape):
+    torch.manual_seed(1234)
+    if dtype == ttnn.uint8:
+        return torch.randint(0, 100, shape).to(torch.int16)
+    if dtype == ttnn.uint16:
+        return torch.randint(0, 100, shape).to(torch.int16)
+    if dtype == ttnn.int32:
+        return torch.randint(-(2**31), 2**31, shape, dtype=torch.int32)
+    if dtype == ttnn.uint32:
+        return torch.randint(0, 2**31, shape, dtype=torch.int32)
+    if dtype == ttnn.float32:
+        return torch.rand(shape, dtype=torch.float32)
+    if dtype == ttnn.bfloat16:
+        return torch.rand(shape, dtype=torch.bfloat16)
+    # return torch.rand(shape).bfloat16().float()
+    assert False, f"Unsupported dtype {dtype}"
+
+
+def run_test(mesh_device, run_op_proc, check_op_proc):
+    ####################### NON-TRACE RUN #######################
+
+    # Run the op
+    tt_output = run_op_proc()
+    tt_output = ttnn.to_torch(tt_output, mesh_composer=ttnn.ConcatMeshToTensor(mesh_device, dim=0))
+
+    # Every device executes the same op, check that each device returned the
+    # same result
+    N = tt_output.shape[0] // mesh_device.get_num_devices()
+    for dev in range(mesh_device.get_num_devices()):
+        i = dev * N
+        j = (dev + 1) * N
+        check_op_proc(tt_output[i:j, ...])
+
+    ####################### TRACE RUN #######################
+
+    # Compile the op
+    tt_output = run_op_proc()
+    tt_output.deallocate(True)
+
+    # Capture the trace
+    trace_id = ttnn.begin_trace_capture(mesh_device, cq_id=0)
+    tt_output = run_op_proc()
+    ttnn.end_trace_capture(mesh_device, trace_id, cq_id=0)
+
+    # Execute trace
+    profiler.start("op_perf")
+    ttnn.execute_trace(mesh_device, trace_id, cq_id=0, blocking=False)
+    profiler.end("op_perf")
+    ttnn.release_trace(mesh_device, trace_id)
+
+    # Get results
+    tt_output = ttnn.to_torch(tt_output, mesh_composer=ttnn.ConcatMeshToTensor(mesh_device, dim=0))
+    # op_time = profiler.get("op_perf")
+    profiler.print(units="us")
+
+    # Every device executes the same op, check that each device returned the
+    # same result
+    N = tt_output.shape[0] // mesh_device.get_num_devices()
+    for dev in range(mesh_device.get_num_devices()):
+        i = dev * N
+        j = (dev + 1) * N
+        check_op_proc(tt_output[i:j, ...])
+
+
+@pytest.mark.parametrize("mesh_device", [(8, 8)], indirect=True)
+@pytest.mark.parametrize(
+    "shape, transpose_type, pad_val",
+    [
+        ([1, 1, 32, 64], "HC", 0),
+        # ([1, 1, 32, 64],    "HC", 0),
+        ([1, 16, 32, 512], "HC", 0),
+        ([32, 1, 16, 64], "CN", None),
+        ([1, 32, 16, 576], "HC", 0),
+        ([1, 128, 32, 576], "HC", 0),
+        # ([1, 1, 32, 64],    "HC", 0),
+        ([1, 32, 1, 64], "HC", 0),
+        ([1, 32, 32, 576], "HC", 0),
+        ([1, 32, 128, 512], "HC", 0),
+        # ([1, 16, 32, 512],  "HC", 0),
+        ([1, 32, 16, 512], "HC", 0),
+        ([1, 128, 32, 128], "HC", 0),
+        # ([1, 16, 32, 512],  "HC", 0),
+        # ([32, 1, 16, 64],   "CN", None),
+        # ([1, 32, 16, 576],  "HC", 0),
+        # ([1, 128, 32, 576], "HC", 0),
+        # ([1, 1, 32, 64],    "HC", 0),
+        # ([1, 32, 1, 64],    "HC", 0),
+        # ([1, 32, 32, 576],  "HC", 0),
+        # ([1, 32, 128, 512], "HC", 0),
+        # ([1, 16, 32, 512],  "HC", 0),
+        # ([1, 32, 16, 512],  "HC", 0),
+        # ([1, 128, 32, 128], "HC", 0),
+        # ([1, 16, 32, 512],  "HC", 0),
+        # ([32, 1, 16, 64],   "CN", None),
+        # ([1, 32, 16, 576],  "HC", 0),
+        # ([1, 128, 32, 576], "HC", 0),
+        # ([1, 1, 32, 64],    "HC", 0),
+        # ([1, 32, 1, 64],    "HC", 0),
+        # ([1, 32, 32, 576],  "HC", 0),
+        # ([1, 32, 128, 512], "HC", 0),
+        # ([1, 16, 32, 512],  "HC", 0),
+        # ([1, 32, 16, 512],  "HC", 0),
+        # ([1, 128, 32, 128], "HC", 0),
+        # ([1, 16, 32, 512],  "HC", 0),
+        # ([32, 1, 16, 64],   "CN", None),
+        # ([1, 32, 16, 576],  "HC", 0),
+        # ([1, 128, 32, 576], "HC", 0),
+        # ([1, 1, 32, 64],    "HC", 0),
+        # ([1, 32, 1, 64],    "HC", 0),
+        # ([1, 32, 32, 576],  "HC", 0),
+        # ([1, 32, 128, 512], "HC", 0),
+        # ([1, 16, 32, 512],  "HC", 0),
+        # ([1, 32, 16, 512],  "HC", 0),
+        # ([1, 128, 32, 128], "HC", 0),
+    ],
+)
+@pytest.mark.parametrize("dtype", [ttnn.bfloat16])
+@pytest.mark.parametrize("layout", [ttnn.TILE_LAYOUT])
+@pytest.mark.parametrize("mem_config", [ttnn.DRAM_MEMORY_CONFIG])
+@pytest.mark.parametrize(
+    "device_params", [{"trace_region_size": 10000, "fabric_config": ttnn.FabricConfig.FABRIC_1D}], indirect=True
+)
+def test_transpose(mesh_device, shape, transpose_type, pad_val, dtype, layout, mem_config):
+    profiler.clear()
+
+    if transpose_type == "HC":
+        dim2, dim1 = 2, 1
+    elif transpose_type == "CN":
+        dim2, dim1 = 1, 0
+
+    torch_input = random_torch_tensor(dtype, shape)
+    torch_output = torch.transpose(torch_input, dim1, dim2)
+
+    tt_input = ttnn.from_torch(
+        torch_input,
+        device=mesh_device,
+        dtype=dtype,
+        layout=layout,
+        memory_config=mem_config,
+        mesh_mapper=ttnn.ReplicateTensorToMesh(mesh_device),
+    )
+
+    def run_op():
+        return ttnn.transpose(tt_input, dim1, dim2, pad_value=pad_val)
+
+    def check_op(tt_output):
+        assert_with_pcc(torch_output, tt_output, 0.9999)
+
+    run_test(mesh_device, run_op, check_op)
