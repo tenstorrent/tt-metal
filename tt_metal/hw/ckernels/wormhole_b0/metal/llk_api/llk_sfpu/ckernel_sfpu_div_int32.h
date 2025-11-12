e1a07b2b33dbf38f459a61fd4b33fa12ff4313fa
mcw-anasuya <mcw-anair@ext.tenstorrent.com>
2025-11-12 13:17:48 +0000

    Enable int32 for ttnn.divide

---
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_div_int32.h
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "ckernel.h"
+#include "ckernel_defs.h"
+#include "sfpi.h"
+
+namespace ckernel::sfpu {
+template <bool APPROXIMATION_MODE, int ITERATIONS>
+inline void calculate_div_int32(const uint dst_index_in0, const uint dst_index_in1, const uint dst_index_out) {
+#pragma GCC unroll 8
+    for (int d = 0; d < ITERATIONS; d++) {
+        // size of each tile in Dest is 64/SFP_DESTREG_STRIDE = 32 rows when using sfpi to load/store
+        constexpr uint dst_tile_size_sfpi = 32;
+        sfpi::vInt in0 = sfpi::dst_reg[dst_index_in0 * dst_tile_size_sfpi];
+        sfpi::vInt in1 = sfpi::dst_reg[dst_index_in1 * dst_tile_size_sfpi];
+        sfpi::vInt result;
+
+        sfpi::vFloat float_in0 = sfpi::int32_to_float(in0, 0);
+        sfpi::vFloat float_in1 = sfpi::int32_to_float(in1, 0);
+        sfpi::vFloat float_result = float_in0 * sfpi::setsgn(_sfpu_reciprocal_<3>(float_in1), float_in1);
+        result = sfpu::_float_to_int32_(float_result);
+
+        sfpi::dst_reg[dst_index_out * dst_tile_size_sfpi] = result;
+        sfpi::dst_reg++;
+    }
+}
+}  // namespace ckernel::sfpu
