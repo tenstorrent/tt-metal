068f55406b401f87ee99a84b59ad04dfd96a240a
William Ly <williamly@tenstorrent.com>
2025-11-12 13:15:00 -0500

    Remove stress test from upstream test script

--- dockerfile/upstream_test_images/run_upstream_tests_vanilla.sh
+++ dockerfile/upstream_test_images/run_upstream_tests_vanilla.sh
@@ -267,7 +267,6 @@ test_suite_bh_glx_llama_demo_tests() {
     verify_llama_dir_
 
     pytest models/tt_transformers/demo/simple_text_demo.py -k "performance and ci-32" --data_parallel 32
-    pytest models/tt_transformers/demo/simple_text_demo.py -k "performance-ci-stress-1" --data_parallel 32 --max_generated_tokens 220
 }
 
 # Define test suite mappings for different hardware topologies
