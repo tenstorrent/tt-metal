b861adf31ecc5dbf538b4abe14d6cb0459c200e1
Jarosław Bartosz Bieniek <jbbieniek@tenstorrent.com>
2025-11-12 18:07:26 +0100

    add ttnn.experimental.integral_image (#32097)
    ### Ticket
    #28156
    
    ### Problem description
    implementation of integral image in only one kernel
    
    ### What's changed
    - added the integral image op
    
    #### speedup (last column assumes that the bug-free LLK works at the
    same speed as how the correct implementation would work like) (the
    current speedup is of the second version):
    <img width="1960" height="159" alt="image"
    src="https://github.com/user-attachments/assets/f6b8dd33-e85e-472b-bcb6-56ae3929c19a"
    />
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes ==>
    https://github.com/tenstorrent/tt-metal/actions/runs/19297666830
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    https://github.com/tenstorrent/tt-metal/actions/runs/19240626643
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-quick.yaml)
    CI passes (if applicable)
    - [ ] [Galaxy demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: Jarek Bieniek <jbbieniekTT@tenstorrent.com>
    Co-authored-by: Bezulj Marko <mbezulj@tenstorrent.com>

---
+++ ttnn/cpp/ttnn/operations/experimental/reduction/integral_image/intimg_pybind.hpp
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "ttnn-pybind/pybind_fwd.hpp"
+
+namespace ttnn::operations::experimental::reduction::detail {
+namespace py = pybind11;
+void bind_reduction_intimg_operation(py::module& module);
+
+}  // namespace ttnn::operations::experimental::reduction::detail
