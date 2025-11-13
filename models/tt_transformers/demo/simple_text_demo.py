0ee6bc527a57cedf382c7d993c375bc9d4b5faba
Suhail Alnahari <salnahari@tenstorrent.com>
2025-11-13 17:21:46 -0500

    llama3_tracing: added changes to enable tracing

--- models/tt_transformers/demo/simple_text_demo.py
+++ models/tt_transformers/demo/simple_text_demo.py
@@ -698,7 +698,7 @@ def prepare_generator_args(
 )
 @pytest.mark.parametrize(
     "device_params",
-    [{"fabric_config": True, "trace_region_size": 50000000, "num_command_queues": 1}],
+    [{"fabric_config": True, "trace_region_size": 70000000, "num_command_queues": 1}],
     indirect=True,
 )
 @pytest.mark.parametrize(
