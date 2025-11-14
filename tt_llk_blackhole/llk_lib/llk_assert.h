91fdd38642547684cfb4a4e2665700e09eeab228
Strahinja Stanisic <sstanisic@tenstorrent.com>
2025-11-14 15:22:53 +0100

    infra :construction:: short-circuit metal asserts in llk (#838)
    ### Ticket
    <!-- Link to Github Issue -->
    #502 
    
    ### Problem description
    <!-- Provide context for the problem. -->
    
    Short circuit tt-metal asserts into LLK repo. This means that we will be
    able to use asserts when running in metal with Watcher enabled. Asserts
    in LLK code will for now be no-op in LLK infra
    
    ### What's changed
    <!-- Describe the approach used to solve the problem.
    Summarize the changes made and its impact. -->
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)

---
+++ tt_llk_blackhole/llk_lib/llk_assert.h
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#ifndef ENV_LLK_INFRA
+
+// Assume we are executing in tt-metal and we have assert already available.
+#include "debug/assert.h"
+
+#define LLK_ASSERT(condition, message) ASSERT(condition)
+
+#else
+
+// TODO: implement asserts in LLK infra
+#define LLK_ASSERT(condition, message) ((void)(condition))
+
+#endif
