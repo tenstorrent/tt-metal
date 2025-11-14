2ffe15d1bc2e026ce5b687e3997a63895ee02e40
Pavlo Hilei <philei@tenstorrent.com>
2025-11-14 18:56:09 +0100

    Remove run_with_autoformat

--- ttnn/api/ttnn/run_operation.hpp
+++ ttnn/api/ttnn/run_operation.hpp
@@ -41,55 +41,6 @@ inline auto run(
     }
 }
 
-Tensors run_with_autoformat(
-    DeviceOperation<Tensors>&& operation,
-    const Tensors& input_tensors,
-    const OptionalConstTensors& optional_input_tensors = {},
-    const OptionalTensors& optional_output_tensors = {},
-    float pad_value = 0);
-
-template <typename ConcreteOperation>
-inline auto run_with_autoformat(
-    ConcreteOperation&& concrete_op,
-    const std::vector<Tensor>& input_tensors,
-    const std::vector<std::optional<const Tensor>>& optional_input_tensors = {},
-    const std::vector<std::optional<Tensor>>& optional_output_tensors = {},
-    const float pad_value = 0) -> Tensors {
-    auto operation = DeviceOperation<Tensors>(std::forward<ConcreteOperation>(concrete_op));
-    return run_with_autoformat(
-        std::move(operation), input_tensors, optional_input_tensors, optional_output_tensors, pad_value);
-}
-
-Tensors run_with_autoformat(
-    DeviceOperation<Tensors>&& operation,
-    const Tensors& input_tensors,
-    const std::vector<FormatParams>& input_formatting,
-    const std::vector<Layout>& output_layouts,
-    const OptionalConstTensors& optional_input_tensors = {},
-    const std::vector<std::optional<FormatParams>>& optional_input_formatting = {},
-    const OptionalTensors& optional_output_tensors = {});
-
-template <typename ConcreteOperation>
-inline auto run_with_autoformat(
-    ConcreteOperation&& concrete_op,
-    const std::vector<Tensor>& input_tensors,
-    const std::vector<FormatParams>& input_formatting,
-    const std::vector<Layout>& output_layouts,
-    const std::vector<std::optional<const Tensor>>& optional_input_tensors = {},
-    const std::vector<std::optional<FormatParams>>& optional_input_formatting = {},
-    const OptionalTensors& optional_output_tensors = {}) -> ProgramOutputTensors<ConcreteOperation> {
-    using OutputTensors = ProgramOutputTensors<ConcreteOperation>;
-    auto operation = DeviceOperation<OutputTensors>(std::forward<ConcreteOperation>(concrete_op));
-    return run_with_autoformat(
-        std::move(operation),
-        input_tensors,
-        input_formatting,
-        output_layouts,
-        optional_input_tensors,
-        optional_input_formatting,
-        optional_output_tensors);
-}
-
 namespace detail {
 distributed::MeshDevice* get_device(
     const Tensors& input_tensors, const OptionalConstTensors& optional_input_tensors = {});
