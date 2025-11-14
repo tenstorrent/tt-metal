59f009ce5e9746096aaa1bba3cce881b291d6007
aliraza556 <alirazaakhtar1122@gmail.com>
2025-11-14 06:19:50 +0500

    Reverted the formatting changes in binary_ng_device_operation.cpp

--- ttnn/cpp/ttnn/operations/eltwise/binary_ng/device/binary_ng_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/binary_ng/device/binary_ng_device_operation.cpp
@@ -18,26 +18,11 @@ bool is_binary_sfpu_op(BinaryOpType val, DataType a, DataType b, bool fast_and_a
         case SUB:
         case MUL:
         case EQ:
-        case NE:
-            return a == b && (
-                a == FLOAT32 ||
-                a == INT32 ||
-                a == UINT32 ||
-                a == UINT16);
+        case NE: return a == b && (a == FLOAT32 || a == INT32 || a == UINT32 || a == UINT16);
         case LOGICAL_AND:
         case LOGICAL_OR:
-        case LOGICAL_XOR:
-            return a == b && (
-                a == FLOAT32 ||
-                a == INT32 ||
-                a == UINT32 ||
-                a == UINT16);
-        case SQUARED_DIFFERENCE:
-            return a == b && (
-                a == FLOAT32 ||
-                a == INT32 ||
-                a == UINT32 ||
-                a == UINT16);
+        case LOGICAL_XOR: return a == b && (a == FLOAT32 || a == INT32 || a == UINT32 || a == UINT16);
+        case SQUARED_DIFFERENCE: return a == b && (a == FLOAT32 || a == INT32 || a == UINT32 || a == UINT16);
         case LOGADDEXP:
         case LOGADDEXP2:
         case LDEXP:
