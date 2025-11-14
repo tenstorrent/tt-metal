aadae87b9e39905574f6a7bdc8ff85e00afd1344
skotarac <skotarac@tenstorrent.com>
2025-11-14 11:00:24 +0000

    Remove default tile indices

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_add_top_row.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_add_top_row.h
@@ -12,7 +12,7 @@
 namespace ckernel::sfpu {
 
 template <DataFormat format>
-inline void calculate_add_top_row(const uint tile_idx_0 = 0, const uint tile_idx_1 = 0, const uint tile_idx_dst = 0) {
+inline void calculate_add_top_row(const uint tile_idx_0, const uint tile_idx_1, const uint tile_idx_dst) {
     // Use the specified format and tile indices for the add_top_row operation
     _calculate_add_top_row_<format>(tile_idx_0, tile_idx_1, tile_idx_dst);
 }
