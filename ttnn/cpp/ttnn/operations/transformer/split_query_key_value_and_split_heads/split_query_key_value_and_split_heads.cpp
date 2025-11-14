73fdef453b42b87cba90b7b4d913df2d8ff347eb
ddilbazTT <ddilbaz@tenstorrent.com>
2025-11-14 15:10:28 +0000

    Add modulo check before divisibility and parameterize transpose_key

--- ttnn/cpp/ttnn/operations/transformer/split_query_key_value_and_split_heads/split_query_key_value_and_split_heads.cpp
+++ ttnn/cpp/ttnn/operations/transformer/split_query_key_value_and_split_heads/split_query_key_value_and_split_heads.cpp
@@ -130,6 +130,18 @@ std::tuple<Tensor, Tensor, Tensor> SplitQueryKeyValueAndSplitHeadsOperation::inv
             input_shape[1]);
 
         TT_FATAL(
+            (input_shape[2] % num_heads) == 0,
+            "Query dimension ({}) must be divisible by num_heads ({}).",
+            input_shape[2],
+            num_heads);
+
+        TT_FATAL(
+            (input_shape_kv[2] % (2 * num_kv_heads.value())) == 0,
+            "KV dimension ({}) must be divisible by 2 * num_kv_heads ({}).",
+            input_shape_kv[2],
+            (2 * num_kv_heads.value()));
+
+        TT_FATAL(
             (input_shape_kv[2] / (2 * num_kv_heads.value())) == (input_shape[2] / num_heads),
             "Dimension mismatch: KV head size ({}) must be equal to query head size ({}).",
             (input_shape_kv[2] / (2 * num_kv_heads.value())),
