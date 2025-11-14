1eacf4c2a7593d9196bc3ebc9a85bb8af6534d24
Velicko Krsmanovic <vkrsmanovic@tenstorrent.com>
2025-11-14 11:01:17 -0500

    State pollution issue for unpacker initalization fix (#825)
    ### Ticket
    <!-- Link to Github Issue -->
    
    ### Problem description
    <!-- Provide context for the problem. -->
    
    ### What's changed
    <!-- transpose register initialization for unpack_A. -->
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)

--- tt_llk_wormhole_b0/llk_lib/llk_unpack_A.h
+++ tt_llk_wormhole_b0/llk_lib/llk_unpack_A.h
@@ -189,13 +189,16 @@ template <
     EltwiseBinaryReuseDestType binary_reuse_dest = EltwiseBinaryReuseDestType::NONE,
     bool unpack_to_dest                          = false>
 inline void _llk_unpack_A_init_(
-    const std::uint32_t transpose_of_faces                           = 0,
-    [[maybe_unused]] const std::uint32_t within_face_16x16_transpose = 0,
-    const std::uint32_t face_r_dim                                   = FACE_R_DIM,
-    const std::uint32_t num_faces                                    = 4,
-    const std::uint32_t unpack_src_format                            = 0,
-    const std::uint32_t unpack_dst_format                            = 0)
+    const std::uint32_t transpose_of_faces          = 0,
+    const std::uint32_t within_face_16x16_transpose = 0,
+    const std::uint32_t face_r_dim                  = FACE_R_DIM,
+    const std::uint32_t num_faces                   = 4,
+    const std::uint32_t unpack_src_format           = 0,
+    const std::uint32_t unpack_dst_format           = 0)
 {
+    // Set transpose register to prevent state pollution
+    cfg_reg_rmw_tensix<THCON_SEC0_REG2_Haloize_mode_RMW>(within_face_16x16_transpose);
+
     constexpr std::uint32_t UNP_SEL = (BType == BroadcastType::NONE) ? p_setadc::UNP_A : p_setadc::UNP_B;
     config_unpacker_x_end<UNP_SEL>(face_r_dim);
     _llk_unpack_A_mop_config_<BType, acc_to_dest, binary_reuse_dest, unpack_to_dest>(transpose_of_faces > 0, num_faces, unpack_src_format, unpack_dst_format);
