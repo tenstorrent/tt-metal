c2d6f8c3a68e57bfef000eee3d8496ab266d1a73
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2025-11-14 11:24:52 +0100

    #27491: SDXL Transformer sharding (#32281)
    ### Ticket
    #27491
    
    ### What's changed
    As part of perf optimizations transformer part of SDXL UNet is sharded
    for 1280 number of channels. We avoid sharding 640 channels to maximize
    the core grid that is used.
    
    Note: accuracy test passed in Shield CI
    
    ### Checklist
    - [x] [(Single-card) Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/19301687527)
    CI passes
    - [x] [Nightly tt-metal L2
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/19301644736)
    CI passes
    - [x] [(Single-card) Device perf
    regressions](https://github.com/tenstorrent/tt-metal/actions/runs/19301657709)
    CI passes
    - [x] [(Single-card) Demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/19301862098)
    CI passes

--- models/experimental/stable_diffusion_xl_base/tt/tt_geglu.py
+++ models/experimental/stable_diffusion_xl_base/tt/tt_geglu.py
@@ -35,16 +35,6 @@ class TtGEGLU(LightweightModule):
                 # due to block sharded mm constraints, if we block shard the input tensor, we can only run it on 56 cores
                 # hence using L1 memory config instead
                 input_tensor = ttnn.to_memory_config(input_tensor, ttnn.L1_MEMORY_CONFIG)
-            else:
-                # here we can run the block sharded matmul on 64 cores
-                block_sharded_mem_config = ttnn.create_sharded_memory_config(
-                    (input_tensor.shape[2] // 8, input_tensor.shape[3] // 8),
-                    core_grid=ttnn.CoreGrid(y=8, x=8),
-                    strategy=ttnn.ShardStrategy.BLOCK,
-                    orientation=ttnn.ShardOrientation.ROW_MAJOR,
-                    use_height_and_width_as_shard_shape=True,
-                )
-                input_tensor = ttnn.to_memory_config(input_tensor, block_sharded_mem_config)
 
         hidden_states = ttnn.linear(
             input_tensor,
