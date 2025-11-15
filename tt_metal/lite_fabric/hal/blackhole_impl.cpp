f009ec176f86fc71b73ff9cac3afdc50356d4947
Reku Dyu <rekudyu@protonmail.com>
2025-11-15 23:02:03 +0530

    replaced tt_version with semver_t

--- tt_metal/lite_fabric/hal/blackhole_impl.cpp
+++ tt_metal/lite_fabric/hal/blackhole_impl.cpp
@@ -54,8 +54,8 @@ void BlackholeLiteFabricHal::set_pc(tt_cxy_pair virtual_core, uint32_t pc_val) {
     cluster.write_core(reinterpret_cast<void*>(&pc_val), sizeof(uint32_t), virtual_core, LITE_FABRIC_RESET_PC);
 }
 
-tt::umd::tt_version BlackholeLiteFabricHal::get_binary_version() {
-    return tt::umd::tt_version{0, 0, 0};
+tt::umd::semver_t BlackholeLiteFabricHal::get_binary_version() {
+    return tt::umd::semver_t{0, 0, 0};
 }
 
 void BlackholeLiteFabricHal::launch(const std::filesystem::path& bin_path) {
