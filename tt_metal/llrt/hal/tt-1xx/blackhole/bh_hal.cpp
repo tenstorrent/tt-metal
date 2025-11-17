5c35e9dc9d89df3f62b3ebf73df395e34af2cb06
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-11-17 14:03:52 -0500

    SFPI 7.9.0 (#32432)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/29186
    
    ### Problem description
    We are using wrongly named flags `-fFOO` for machine-specific options.
    
    ### What's changed
    Replace with `-mBAR` options, using more consistent naming, see
    https://github.com/tenstorrent/sfpi/releases/tag/7.9.0
    This is also preparation for separating more of the conflation in the
    above issue.
    Update uses in tt-metal
    
    ### Checklist
    - [YES] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [YES] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-quick.yaml)
    CI passes (if applicable)
    - [ ] [Galaxy demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- tt_metal/llrt/hal/tt-1xx/blackhole/bh_hal.cpp
+++ tt_metal/llrt/hal/tt-1xx/blackhole/bh_hal.cpp
@@ -177,7 +177,7 @@ public:
                                      params.processor_class == HalProcessorClassType::COMPUTE
                                  ? "-mcpu=tt-bh-tensix "
                                  : "-mcpu=tt-bh ";
-        cflags += "-fno-rvtt-sfpu-replay ";
+        cflags += "-mno-tt-tensix-optimize-replay ";
         if (!(params.core_type == HalProgrammableCoreType::TENSIX &&
               params.processor_class == HalProcessorClassType::COMPUTE)) {
             cflags += "-fno-tree-loop-distribute-patterns ";  // don't use memcpy for cpy loops
