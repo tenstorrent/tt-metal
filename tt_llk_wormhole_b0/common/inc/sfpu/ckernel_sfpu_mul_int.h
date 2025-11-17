60740c1b78ad60339043292f6de10bf318a31035
Jason Davies <jason@jasondavies.com>
2025-11-17 16:17:20 +0000

    Fix 16-bit integer multiplication. (#843)
    ### Ticket
    
    https://github.com/tenstorrent/tt-metal/issues/32495
    
    Needs to be paired with tt-metal PR
    https://github.com/tenstorrent/tt-metal/pull/32519 to call init
    function.
    
    ### Problem description
    
    The current implementation doesn't work for all u16 inputs; the result
    is shifted and truncated if it is greater than 24 bits.
    
    ### What's changed
    
    - **Breaking:** added `_init_mul_int_` function to initialise constants
    (only used on WH). Can be used in future for further optimisations.
    - BH: use `SFPMUL24` for **6 cycles** per row.
    - WH: split into 8-bit chunks, cast to fp32, do 3 multiplications,
    convert to u16 and add: **22 cycles** per row.
    
    This produces correct results for all u16 inputs.
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)

--- tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_mul_int.h
+++ tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_mul_int.h
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+// SPDX-FileCopyrightText: © 2025 Jason Davies <jason@jasondavies.com>
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -18,27 +19,73 @@ inline void _mul_int_(const uint dst_index_in0, const uint dst_index_in1, const 
 #pragma GCC unroll 8
     for (int d = 0; d < ITERATIONS; d++)
     {
-        // size of each tile in Dest is 64 rows
+        // Split u16 inputs a and b into a = (a1 << 8) | a0; b = (b1 << 8) | b0,
+        // where a0, a1, b0, b1 are u8.  Then cast to fp32, and calculate:
+        //   lo  = a0*b0
+        //   hi0 = a0*b1
+        //   hi1 = a1*b0
+        // Observe that these are < 2**16.  This allows conversion back to u16
+        // using TTI_SFP_STOCH_RND.
+        // Finally, the result will be lo + ((hi0 + hi1) << 8).
+
         constexpr uint dst_tile_size = 64;
-        // operand A - uint16
+
+        // a0
         TT_SFPLOAD(p_sfpu::LREG0, LO16, ADDR_MOD_3, dst_index_in0 * dst_tile_size);
-        // operand B - uint16
+
+        // a1
+        TTI_SFPSHFT2(p_sfpu::LREG0, p_sfpu::LREG13, p_sfpu::LREG2, 5);
+        TTI_SFPCAST(p_sfpu::LREG2, p_sfpu::LREG2, 0);
+
+        // a0 = (a0 & mask) as fp32
+        TTI_SFPAND(0, p_sfpu::LREG12, p_sfpu::LREG0, 0);
+        TTI_SFPCAST(p_sfpu::LREG0, p_sfpu::LREG0, 0);
+
+        // b0
         TT_SFPLOAD(p_sfpu::LREG1, LO16, ADDR_MOD_3, dst_index_in1 * dst_tile_size);
 
-        // The following cast+mul+cast method provides accurate results if the product of the inputs < 2**24
-        // since float32 can exactly represent integer up to 2**24. To preserve accuracy beyond 2**24,
-        // we could split the 16-bit input into two 8-bit chunks, cast to fp32, multiply and then cast back.
-        // uint16 -> fp32
-        TTI_SFPCAST(p_sfpu::LREG0, p_sfpu::LREG0, 0);
+        // b1
+        TTI_SFPSHFT2(p_sfpu::LREG1, p_sfpu::LREG13, p_sfpu::LREG3, 5);
+        TTI_SFPCAST(p_sfpu::LREG3, p_sfpu::LREG3, 0);
+
+        // b0 = (b0 & mask) as fp32
+        TTI_SFPAND(0, p_sfpu::LREG12, p_sfpu::LREG1, 0);
         TTI_SFPCAST(p_sfpu::LREG1, p_sfpu::LREG1, 0);
-        TTI_SFPMUL(p_sfpu::LREG0, p_sfpu::LREG1, p_sfpu::LCONST_0, p_sfpu::LREG0, 0);
-        TTI_NOP;
-        // fp32 -> uint16
-        TTI_SFP_STOCH_RND(0, 0, 0, p_sfpu::LREG0, p_sfpu::LREG0, 6);
+
+        // hi0 = a0*b1
+        TTI_SFPMAD(p_sfpu::LREG0, p_sfpu::LREG3, p_sfpu::LCONST_0, p_sfpu::LREG3, 0);
+        // lo = a0*b0
+        TTI_SFPMAD(p_sfpu::LREG0, p_sfpu::LREG1, p_sfpu::LCONST_0, p_sfpu::LREG0, 0);
+        // hi1 = a1*b0
+        TTI_SFPMAD(p_sfpu::LREG2, p_sfpu::LREG1, p_sfpu::LCONST_0, p_sfpu::LREG2, 0);
+
+        // lo = rnd(lo)
+        TTI_SFP_STOCH_RND(2, 0, 0, p_sfpu::LREG0, p_sfpu::LREG0, 6);
+
+        // hi1 = rnd(hi1)
+        TTI_SFP_STOCH_RND(2, 0, 0, p_sfpu::LREG2, p_sfpu::LREG2, 6);
+        // hi0 = rnd(hi0)
+        TTI_SFP_STOCH_RND(2, 0, 0, p_sfpu::LREG3, p_sfpu::LREG3, 6);
+
+        // hi = hi0 + hi1
+        TTI_SFPIADD(0, p_sfpu::LREG3, p_sfpu::LREG2, sfpi::SFPIADD_MOD1_CC_NONE);
+        // hi <<= 8
+        TTI_SFPSHFT(8, 0, p_sfpu::LREG2, 1);
+
+        // lo += hi
+        TTI_SFPIADD(0, p_sfpu::LREG2, p_sfpu::LREG0, sfpi::SFPIADD_MOD1_CC_NONE);
+
         TT_SFPSTORE(p_sfpu::LREG0, LO16, ADDR_MOD_3, dst_index_out * dst_tile_size);
 
         sfpi::dst_reg++;
     }
+}
+
+template <bool APPROXIMATION_MODE>
+inline void _init_mul_int_()
+{
+    sfpi::vConstIntPrgm0 = 0xff; // LREG12
+    sfpi::vConstIntPrgm1 = -8;   // LREG13
 }
 
 } // namespace sfpu
