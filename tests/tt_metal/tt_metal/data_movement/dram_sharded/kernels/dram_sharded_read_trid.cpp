e06786998238cf42d84be09b26e9df8408f66749
Ryan Zhu <ryanzhu@tenstorrent.com>
2025-11-18 13:25:02 -0500

    [DM] Test DRAM sharded stateful transaction ID read and generalize API names (#31964)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/29739)
    
    ### Problem description
    Data movement test suite does not have coverage for reads from DRAM
    sharded banks using transaction IDs and stateful APIs. Additionally
    these APIs are not necessarily specific to DRAM sharded scenarios and so
    are misleadingly named.
    
    ### What's changed
    - Added a case which tests stateful reading using transaction IDs from
    DRAM sharded banks
    - Renamed `noc_async_read_tile_dram_sharded_with_state_with_trid` to
    `noc_async_read_one_packet_with_state_with_trid`
    - Renamed `noc_async_read_tile_dram_sharded_set_trid` to
    `noc_async_read_set_trid`
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-quick.yaml)
    CI passes (if applicable)
    - [ ] [Galaxy demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

--- tests/tt_metal/tt_metal/data_movement/dram_sharded/kernels/dram_sharded_read_trid.cpp
+++ tests/tt_metal/tt_metal/data_movement/dram_sharded/kernels/dram_sharded_read_trid.cpp
@@ -20,13 +20,14 @@ void kernel_main() {
     constexpr uint32_t pages_per_bank = get_compile_time_arg_val(2);
     constexpr uint32_t page_size_bytes = get_compile_time_arg_val(3);
     constexpr uint32_t test_id = get_compile_time_arg_val(4);
+    constexpr uint32_t num_of_trids = get_compile_time_arg_val(5);
 
     DeviceTimestampedData("Number of transactions", num_of_transactions);
     DeviceTimestampedData("Transaction size in bytes", num_banks * pages_per_bank * page_size_bytes);
     DeviceTimestampedData("Test id", test_id);
 
     uint32_t dst_addr = l1_addr;
-
+    uint32_t curr_trid = 1;  // Start trids from 1, 0 may break in the future
     {
         DeviceZoneScopedN("RISCV0");
         for (uint32_t n = 0; n < num_of_transactions; n++) {
@@ -34,12 +35,17 @@ void kernel_main() {
             for (uint32_t bank_id = 0; bank_id < num_banks; bank_id++) {
                 uint64_t src_noc_addr = get_noc_addr_from_bank_id<true>(bank_id, src_addr);
                 noc_async_read_one_packet_set_state(src_noc_addr, page_size_bytes);
+                noc_async_read_set_trid(curr_trid);
                 for (uint32_t i = 0; i < pages_per_bank; i++) {
-                    noc_async_read_one_packet_with_state(src_noc_addr + i * page_size_bytes, dst_addr);
+                    noc_async_read_one_packet_with_state_with_trid(
+                        src_noc_addr, i * page_size_bytes, dst_addr, curr_trid);
                     dst_addr += page_size_bytes;
                 }
+                curr_trid = (curr_trid % (num_of_trids - 1)) + 1;  // keep trid between 1 and num_of_trids-1
             }
         }
-        noc_async_read_barrier();
+        for (uint32_t t = 1; t < num_of_trids; t++) {
+            noc_async_read_barrier_with_trid(t);
+        }
     }
 }
