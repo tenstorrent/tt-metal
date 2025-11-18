09672217ba0963127b116a12bff8e6e71dd4bb39
Nikola Ostojic <nostojic@tenstorrent.com>
2025-11-18 15:44:54 +0000

    lol

--- models/demos/gemma3/tt/model_config.py
+++ models/demos/gemma3/tt/model_config.py
@@ -1268,8 +1268,7 @@ class ModelArgs:
 
         # TODO: If no specific sequence lengths are listed for a model and device, the default one will be used (from the default_supported_seq_lens dictionary)
         model_specific_supported_seq_lens = {
-            # EXAMPLE: #base_model_name : {device_name : [sequence_lengths]}
-            # "gemma-3-27b": {
+            # EXAMPLE: "gemma-3-4b": {
             #     "N150": [128, 256, 512, 1024, 2048],
             # }
         }
