e590d83e698937a2dc14196c76923c83f2890253
Ziemowit Bączewski <zbaczewski@tenstorrent.com>
2025-11-19 12:29:29 +0000

    Basic fused AdamW implementation

--- tt-train/sources/ttml/metal/optimizers/adamw_fused/adamw_fused.cpp
+++ tt-train/sources/ttml/metal/optimizers/adamw_fused/adamw_fused.cpp
@@ -11,11 +11,13 @@ namespace ttml::metal {
 ttnn::Tensor adamw_fused(
     [[maybe_unused]] const ttnn::Tensor& param_in,
     [[maybe_unused]] const ttnn::Tensor& grad,
-    [[maybe_unused]] const ttnn::Tensor& exp_avg_in,
-    [[maybe_unused]] const ttnn::Tensor& exp_avg_sq_in,
+    [[maybe_unused]] const ttnn::Tensor& exp_avg,
+    [[maybe_unused]] const ttnn::Tensor& exp_avg_sq,
     [[maybe_unused]] const float lr,
     [[maybe_unused]] const float beta1,
     [[maybe_unused]] const float beta2,
+    [[maybe_unused]] const float beta1_pow,
+    [[maybe_unused]] const float beta2_pow,
     [[maybe_unused]] const float epsilon,
     [[maybe_unused]] const float weight_decay,
     [[maybe_unused]] const uint32_t step) {
