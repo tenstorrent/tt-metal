46f5a11b472a504eee3001036947070d7bf2f2ca
Artem Yerofieiev <ayerofieiev@tenstorrent.com>
2025-11-19 05:45:35 +0000

    migrate op to infra: convert_to_chw

---
+++ ttnn/cpp/ttnn/operations/experimental/cnn/convert_to_chw/device/convert_to_chw_device_operation_types.hpp
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "ttnn/tensor/tensor.hpp"
+
+namespace ttnn::operations::experimental::cnn {
+
+struct operation_attributes_t {
+    const tt::tt_metal::MemoryConfig memory_config;
+    const tt::tt_metal::DataType dtype;
+};
+
+struct tensor_args_t {
+    const Tensor& input;
+    std::optional<Tensor> preallocated_output;
+};
+
+using tensor_return_value_t = Tensor;
+
+using spec_return_value_t = TensorSpec;
+
+}  // namespace ttnn::operations::experimental::cnn
