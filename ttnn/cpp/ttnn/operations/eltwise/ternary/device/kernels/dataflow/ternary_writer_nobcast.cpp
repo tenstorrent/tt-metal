a42c24c8b6de9890eadc7aa66866fb15aa52a21c
mouliraj-mcw <ctr-mcw-melamurugan@ext.tenstorrent.com>
2025-11-19 10:28:42 +0000

    #31776: Update files

--- ttnn/cpp/ttnn/operations/eltwise/ternary/device/kernels/dataflow/ternary_writer_nobcast.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/ternary/device/kernels/dataflow/ternary_writer_nobcast.cpp
@@ -22,9 +22,7 @@ void kernel_main() {
     constexpr uint32_t cb_id_out = get_compile_time_arg_val(0);
     constexpr auto dst_args = TensorAccessorArgs<1>();
 
-#ifdef DST_SHARDED
-    cb_wait_front(cb_id_out, dst_num_tiles);
-#else
+#if !DST_SHARDED
     constexpr uint32_t onetile = 1;
     const uint32_t tile_bytes = get_tile_size(cb_id_out);
     const auto s = TensorAccessor(dst_args, dst_addr, tile_bytes);
