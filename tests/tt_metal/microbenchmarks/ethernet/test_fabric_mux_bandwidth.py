ade1f0b203aafbbd0b2aa12b3cf8f6b62a66370c
Sean Nijjar <sean.nijjar@gmail.com>
2025-11-19 17:17:35 +0000

    sq

--- tests/tt_metal/microbenchmarks/ethernet/test_fabric_mux_bandwidth.py
+++ tests/tt_metal/microbenchmarks/ethernet/test_fabric_mux_bandwidth.py
@@ -27,7 +27,7 @@ GOLDEN_FILE_PATH = os.path.join(GOLDEN_FILE_DIR, GOLDEN_FILE_NAME)
 SUMMARY_FILE_NAME = "fabric_mux_bandwidth_summary.txt"
 SUMMARY_FILE_PATH = os.path.join(OUTPUT_FILE_DIR, SUMMARY_FILE_NAME)
 
-BW_THRESHOLD = 0.1
+BW_THRESHOLD = 0.25
 
 SPEEDUPS = []  # Track speedup for each test
 
