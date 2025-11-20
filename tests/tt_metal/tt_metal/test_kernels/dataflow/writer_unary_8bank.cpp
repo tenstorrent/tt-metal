4d839e7c404961b806fa5860a95aa13c82428843
“Bryan <bzimmerman@tenstorrent.com>
2025-11-20 19:39:35 +0000

    Code refactoring

--- tests/tt_metal/tt_metal/test_kernels/dataflow/writer_unary_8bank.cpp
+++ tests/tt_metal/tt_metal/test_kernels/dataflow/writer_unary_8bank.cpp
@@ -4,22 +4,24 @@
 
 #include "dataflow_api.h"
 
+#include <cstdint>
+
 void kernel_main() {
-    uint32_t dst_addr  = get_arg_val<uint32_t>(0);
-    uint32_t num_tiles = get_arg_val<uint32_t>(2); // Index 2 to match with regular writer_unary
+    const uint32_t dst_addr{get_arg_val<uint32_t>(0U)};
+    const uint32_t num_tiles{get_arg_val<uint32_t>(2U)}; // Index 2 to match with regular writer_unary
 
-    constexpr uint32_t cb_id_out0 = 16;
-    constexpr uint32_t onetile = 1;
-    uint32_t tile_bytes = get_tile_size(cb_id_out0);
+    constexpr uint32_t cb_id_out0{16U};
+    constexpr uint32_t onetile{1U};
+    const uint32_t tile_bytes{get_tile_size(cb_id_out0)};
 
-    constexpr auto dst_args = TensorAccessorArgs<0>();
-    const auto s = TensorAccessor(dst_args, dst_addr, tile_bytes);
+    constexpr auto dst_args{TensorAccessorArgs<0U>()};
+    const auto s{TensorAccessor(dst_args, dst_addr, tile_bytes)};
 
-    for (uint32_t i = 0; i < num_tiles; i++) {
-        uint64_t dst_noc_addr = get_noc_addr(i, s);
+    for (uint32_t i{0U}; i < num_tiles; ++i) {
+        const uint64_t dst_noc_addr{get_noc_addr(i, s)};
 
         cb_wait_front(cb_id_out0, onetile);
-        uint32_t l1_read_addr = get_read_ptr(cb_id_out0);
+        const uint32_t l1_read_addr{get_read_ptr(cb_id_out0)};
 
         noc_async_write(l1_read_addr, dst_noc_addr, tile_bytes);
 
