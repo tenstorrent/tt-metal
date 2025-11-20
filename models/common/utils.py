9b98d3f4b7d88870578118b5891d3ab038eabbdd
Djordje Ivanovic <divanovic@tenstorrent.com>
2025-11-20 15:02:14 +0000

    Add LogProbsCalculator class to support log-probs in TTT Rebase to latest main

--- models/common/utils.py
+++ models/common/utils.py
@@ -5,6 +5,8 @@ import torch
 import torch.nn.functional as F
 from torch import Tensor
 
+import ttnn
+
 
 def top_k_top_p_filtering(
     logits: Tensor,
@@ -45,3 +47,204 @@ def top_k_top_p_filtering(
         indices_to_remove = sorted_indices_to_remove.scatter(1, sorted_indices, sorted_indices_to_remove)
         logits[indices_to_remove] = filter_value
     return logits
+
+
+class LogProbsCalculator:
+    """
+    Class to calculate log-probs for a given logits tensor and indices tensor.
+
+    Args:
+        mesh_device: MeshDevice to use for all-gather operations
+        vocab_size: Vocabulary size
+    """
+
+    def __init__(self, vocab_size: int, mesh_device: ttnn.MeshDevice):
+        self.global_max = None
+        self.global_exp_sum = None
+        self.vocab_size = vocab_size
+        self.mesh_device = mesh_device
+        self.enable_log_probs = False  # default to False
+
+        # Create mask for each user on each chip.
+        batch_size = 32
+
+        num_devices = self.mesh_device.get_num_devices()
+        # TODO: Test this for 6U Galaxy
+        # Enforce working on 8 devices instead of all 32 since logits are sharded across 8 devices
+        num_devices_for_sharding = 8 if num_devices == 32 else num_devices
+
+        # Create mask tensor with shape (num_devices, batch_size)
+        # Each row will have device_id starting from 0 to num_devices - 1
+        mask_tensor = torch.arange(num_devices_for_sharding).unsqueeze(1).expand(num_devices_for_sharding, batch_size)
+
+        if self.mesh_device.get_num_devices() == 32:
+            self.cluster_shape = (8, 4)
+            mesh_mapper = ttnn.ShardTensor2dMesh(self.mesh_device, dims=(None, 0), mesh_shape=self.cluster_shape)
+        elif self.mesh_device.get_num_devices() == 8:
+            mesh_mapper = ttnn.ShardTensorToMesh(self.mesh_device, dim=0)
+        else:
+            # TODO: Implement method for other number of devices, for now replicate tensor to mesh to support all the ops
+            mesh_mapper = ttnn.ReplicateTensorToMesh(self.mesh_device)
+
+        self.mask = ttnn.as_tensor(
+            mask_tensor,
+            dtype=ttnn.bfloat16,
+            memory_config=ttnn.DRAM_MEMORY_CONFIG,
+            device=self.mesh_device,
+            layout=ttnn.ROW_MAJOR_LAYOUT,
+            preprocess=lambda x: x.to(torch.bfloat16),
+            mesh_mapper=mesh_mapper,
+        )
+        self.output_tensor = ttnn.as_tensor(
+            torch.ones(1, 1, 1, batch_size),
+            dtype=ttnn.bfloat16,
+            memory_config=ttnn.DRAM_MEMORY_CONFIG,
+            device=self.mesh_device,
+            layout=ttnn.TILE_LAYOUT,
+            mesh_mapper=ttnn.ReplicateTensorToMesh(self.mesh_device),
+        )
+
+    def set_log_probs_mode(self, enable_log_probs: bool | list[bool] = False):
+        if isinstance(enable_log_probs, list):
+            # If any of the users in the batch have log_probs enabled,
+            # then whole batch will run log-probs calculation
+            enable_log_probs_result = any(enable_log_probs)
+        else:
+            enable_log_probs_result = enable_log_probs
+
+        self.enable_log_probs = enable_log_probs_result
+
+    def compute_global_stats(
+        self,
+        logits_tensor: ttnn.Tensor,
+    ):
+        """
+        To calculate log-probs, we need to calculate the global max and global sum(exp(logits - global_max)) for each chip.
+        This is done by all-gathering the max and sum(exp(logits - global_max)) for each chip and then taking the max and sum of the gathered tensors.
+        log-prob formula: log-prob(x) = logits(x) - global_max - log(sum(exp(logits - global_max)))
+
+        Args:
+            logits_tensor (ttnn.Tensor): Logits as model output (1, 1, batch_size, vocab_size_per_device)
+        """
+        if not self.enable_log_probs:
+            return
+
+        if self.mesh_device.get_num_devices() != 8:
+            self.global_max = None
+            self.global_exp_sum = None
+            return
+        # Calculate local max
+        local_max_tensor = ttnn.max(logits_tensor, dim=-1, keepdim=True)
+
+        # All-gather local max to get global max
+        gathered_max_tensors = ttnn.all_gather(
+            local_max_tensor,
+            dim=3,
+            num_links=1,
+            memory_config=ttnn.DRAM_MEMORY_CONFIG,
+            cluster_axis=None,
+            topology=ttnn.Topology.Linear,
+        )
+        self.global_max = ttnn.max(gathered_max_tensors, dim=-1, keepdim=True)
+
+        # Calculate stable local sum-exp using subtract of global-max from each local logit
+        subtracted_tensor = ttnn.subtract(logits_tensor, self.global_max)
+        sum_exp_tensor = ttnn.sum(ttnn.exp(subtracted_tensor), dim=-1, keepdim=True)
+
+        # All-gather stable local sum-exp to get global sum-exp
+        gathered_sum_exp_tensors = ttnn.all_gather(
+            sum_exp_tensor,
+            dim=3,
+            num_links=1,
+            memory_config=ttnn.DRAM_MEMORY_CONFIG,
+            cluster_axis=None,
+            topology=ttnn.Topology.Linear,
+        )
+        self.global_exp_sum = ttnn.sum(gathered_sum_exp_tensors, dim=-1, keepdim=True)
+
+        # reshape global_max and global_exp_sum to support same output shape as sampling output -> (1, 1, 1, 32)
+        self.global_max = ttnn.reshape(self.global_max, (1, 1, 1, 32))
+        self.global_exp_sum = ttnn.reshape(self.global_exp_sum, (1, 1, 1, 32))
+
+    def prepare_relevant_logits(self, logits_tensor: ttnn.Tensor, global_idx_tensor: ttnn.Tensor):
+        """
+        Prepare global idx tensor with correct values on all devices.
+        """
+        if not self.enable_log_probs:
+            return logits_tensor
+
+        if self.mesh_device.get_num_devices() != 8:
+            # TODO: Implement method for Llama 3.70b Galaxy with sub_core_grid support for all the ops
+            return None
+
+        if not self.enable_log_probs:
+            return logits_tensor
+
+        size_per_device = logits_tensor.shape[-1]
+
+        # convert global_idx_tensor to ttnn.TILE_LAYOUT
+        global_idx_tilized_tensor = ttnn.to_layout(global_idx_tensor, ttnn.TILE_LAYOUT)
+
+        # TODO: Raise an issue on this since for UINT_32 ttnn.div produces incorrect output (all zeros)
+        global_idx_tilized_tensor = ttnn.typecast(global_idx_tilized_tensor, ttnn.float32)
+
+        # Get chip_id for each user based on global_idx values in global_idx_tensor
+        chip_ids_tensor = ttnn.div(
+            global_idx_tilized_tensor, size_per_device, round_mode="floor", memory_config=ttnn.DRAM_MEMORY_CONFIG
+        )
+
+        # Get local index for each user based on global_idx values in global_idx_tensor
+        remainder_tensor = ttnn.remainder(
+            global_idx_tilized_tensor, size_per_device, memory_config=ttnn.DRAM_MEMORY_CONFIG
+        )
+
+        # Convert remainder_tensor to int32
+        remainder_tensor = ttnn.reshape(ttnn.typecast(remainder_tensor, ttnn.uint32), (1, 1, 32, 1))
+
+        # Get logits for each user on each chip based on local index
+        selected_logits_tensor = ttnn.gather(logits_tensor, dim=3, index=remainder_tensor)
+
+        selected_logits_tensor = ttnn.reshape(selected_logits_tensor, (1, 1, 1, 32))
+        # Compare mask to chip_ids tensor and select correct positions for each user on all chips inplace
+        ttnn.eq_(chip_ids_tensor, self.mask)
+
+        # Multiply selected_logits_tensor with chip_ids_tensor to get expected logits for each user
+        selected_logits_tensor = ttnn.multiply(selected_logits_tensor, chip_ids_tensor)
+
+        # Use ttnn.all_gather to get logits across all devices
+        selected_logits_tensor = ttnn.all_gather(
+            selected_logits_tensor,
+            dim=2,
+            memory_config=ttnn.DRAM_MEMORY_CONFIG,
+            cluster_axis=None,
+            topology=ttnn.Topology.Linear,
+        )
+
+        # Apply sum over device dimension to get logits for each user on all chips
+        selected_logits_tensor = ttnn.sum(selected_logits_tensor, dim=2, keepdim=True)
+
+        return selected_logits_tensor
+
+    def calculate_log_probs(self, logits_tensor: ttnn.Tensor):
+        """
+        Calculate log-probs for a given logits tensor.
+        """
+        if not self.enable_log_probs:
+            return self.output_tensor
+
+        if self.mesh_device.get_num_devices() != 8:
+            # TODO: Currently not implemented for 32 devices due sub_core_grid not supported for all the ops
+            return self.output_tensor
+
+        if not self.enable_log_probs:
+            return self.output_tensor
+
+        if self.global_max is None or self.global_exp_sum is None:
+            raise ValueError("Global max or global exp sum is not calculated yet. Call compute_global_stats first.")
+
+        # Calculate log-probs with formula:
+        # logits_tensor - self.global_max - ttnn.log(self.global_exp_sum)
+        out = ttnn.subtract(logits_tensor, self.global_max)
+        out = ttnn.subtract(out, ttnn.log(self.global_exp_sum))
+
+        return out
--- models/tt_transformers/tt/generator.py
+++ models/tt_transformers/tt/generator.py
@@ -43,6 +43,7 @@ class SamplingParams:
     frequency_penalty: float | list[float] = 0.0
     repetition_penalty: float | list[float] = 1.0
     seed: int | list[int] = 0
+    enable_log_probs: bool | list[bool] = False
 
 
 SAMPLING_PARAM_FIELDS = tuple(f.name for f in fields(SamplingParams))
@@ -546,8 +547,7 @@ class Generator:
         Performs text decode step.
         Returns tt_logits on device
         """
-        tt_logits = []
-
+        tt_output = []
         tt_tokens = []
         tt_current_pos = []
         tt_rot_mat_idxs = []
@@ -568,7 +568,7 @@ class Generator:
 
         for i in range(self.data_parallel):
             user_kv_cache = kv_cache[i] if kv_cache is not None else None
-            tt_logits_i = self.model[i].ttnn_decode_forward(
+            tt_logits_i, tt_log_probs_i = self.model[i].ttnn_decode_forward(
                 tt_tokens[i],
                 tt_current_pos[i],
                 rot_mat_idxs=tt_rot_mat_idxs[i],
@@ -576,9 +576,9 @@ class Generator:
                 kv_cache=user_kv_cache,
                 sampling_on_device=sampling_on_device,
             )
-            tt_logits.append(tt_logits_i)
+            tt_output.append((tt_logits_i, tt_log_probs_i))
 
-        return tt_logits
+        return tt_output
 
     def _capture_decode_trace_text(
         self,
@@ -1031,7 +1031,7 @@ class Generator:
         read_from_device=True,
     ):
         if not self.model_args[0].is_llama_vision():
-            return self.decode_forward_text(
+            output = self.decode_forward_text(
                 tokens,
                 start_pos,
                 enable_trace=enable_trace,
@@ -1039,7 +1039,7 @@ class Generator:
                 kv_cache=kv_cache,
             )
         else:
-            return self.decode_forward_llama_vision(
+            output = self.decode_forward_llama_vision(
                 start_pos,
                 tokens,
                 prefill_cross_attention_masks,
@@ -1053,19 +1053,38 @@ class Generator:
                 enable_trace,
                 read_from_device,
             )
+        # skip returning log-probs
+        if isinstance(output, tuple):
+            return output[0]
+        else:
+            return output
 
     # Note: This function is called by vLLM
     def read_decode_output(self, tt_out, async_read=False):
         """
-        Input tt_out is a list of ttnn device tensors
+        Input tt_out is list of tuples of (tt_out_tok, tt_log_probs)
+
         """
         if not async_read:
-            return [out.cpu() for out in tt_out]
+            # output is a tuple of (tt_out_tok, tt_log_probs)
+            if isinstance(tt_out[0], tuple):
+                return [
+                    (out[0].cpu(), out[1].cpu() if out[1] is not None else None)  # logits should never be None
+                    for out in tt_out
+                ]
+            elif isinstance(tt_out[0], ttnn.Tensor):
+                return [out.cpu() for out in tt_out]
 
         host_outputs = []
         read_events = []
         for i in range(self.data_parallel):
-            host_outputs.append(tt_out[i].cpu(blocking=False))
+            if isinstance(tt_out[i], tuple):
+                outputs = (tt_out[i][0].cpu(blocking=False), tt_out[i][1].cpu(blocking=False))  # logits  # log-probs
+                host_outputs.append(outputs)
+            elif isinstance(tt_out[i], ttnn.Tensor):
+                outputs = tt_out[i].cpu(blocking=False)
+                host_outputs.append(outputs)
+
             read_events.append(ttnn.record_event(self.model[i].mesh_device, 0))
 
         return host_outputs, read_events
@@ -1079,13 +1098,31 @@ class Generator:
         max_batch_size_per_model = self.model_args[0].max_batch_size
 
         logits = []
+        log_probs = []
         for i in range(self.data_parallel):
-            logits_i = self.model[i].process_output_decode(
-                tt_out[i], max_batch_size_per_model, S=1, is_tokens=is_tokens
-            )
-            logits.append(logits_i)
-
-        return torch.cat(logits, 0)
+            if isinstance(tt_out[i], tuple):
+                logits_i = self.model[i].process_output_decode(
+                    tt_out[i][0], max_batch_size_per_model, S=1, is_tokens=is_tokens
+                )
+                log_probs_i = (
+                    self.model[i].process_output_decode(
+                        tt_out[i][1], max_batch_size_per_model, S=1, is_tokens=is_tokens, is_log_probs=True
+                    )
+                    if tt_out[i][1] is not None
+                    else torch.ones(logits_i.shape)
+                )
+                logits.append(logits_i)
+                log_probs.append(log_probs_i)
+            elif isinstance(tt_out[i], ttnn.Tensor):
+                logits_i = self.model[i].process_output_decode(
+                    tt_out[i], max_batch_size_per_model, S=1, is_tokens=is_tokens
+                )
+                logits.append(logits_i)
+                # add dummy tensor for log_probs
+                log_probs.append(torch.ones(logits_i.shape))
+            else:
+                raise ValueError(f"Invalid type of tt_out: {type(tt_out[i])}")
+        return (torch.cat(logits, 0), torch.cat(log_probs, 0))
 
     def _decode_forward_no_trace(
         self,
@@ -1152,10 +1189,11 @@ class Generator:
             tt_cross_page_table.append(tt_cross_page_table_i)
 
         tt_logits = []
+        tt_log_probs = []
         for i in range(self.data_parallel):
             user_kv_cache = kv_cache[i] if kv_cache is not None else None
             xattn_cache = xattn_caches[i] if xattn_caches is not None else None
-            tt_logits_i = self.model[i].ttnn_decode_forward(
+            tt_logits_i, tt_log_probs_i = self.model[i].ttnn_decode_forward(
                 tt_h[i],
                 tt_xattn_mask[i],
                 tt_full_text_mask_expand_1NSH[i],
@@ -1168,8 +1206,9 @@ class Generator:
                 cross_page_table=tt_cross_page_table[i],
             )
             tt_logits.append(tt_logits_i)
+            tt_log_probs.append(tt_log_probs_i)
 
-        return tt_logits
+        return tt_logits, tt_log_probs
 
     def _capture_trace(
         self,
@@ -1231,8 +1270,8 @@ class Generator:
         for i in range(self.data_parallel):
             user_kv_cache = kv_cache[i] if kv_cache is not None else None
             xattn_cache = xattn_caches[i] if xattn_caches is not None else None
-            # tt_logits_rm unused later, no need to make a list
-            tt_logits_rm = self.model[i].ttnn_decode_forward(
+            # tt_logits_rm and tt_log_probs_rm unused later, no need to make a list
+            tt_logits_rm, tt_log_probs_rm = self.model[i].ttnn_decode_forward(
                 tt_h[i],
                 tt_xattn_mask[i],
                 tt_full_text_mask_expand_1NSH[i],
@@ -1313,6 +1352,7 @@ class Generator:
         tt_h_trace_input = tt_h
 
         tt_logits_rm = []
+        tt_log_probs_rm = []
         trace_ids = {}
         # Do on-device transformations of inputs before forward
         for i in range(self.data_parallel):
@@ -1336,7 +1376,7 @@ class Generator:
                 B=B,
             )
 
-            tt_logits_rm_i = self.model[i].ttnn_decode_forward(
+            tt_logits_rm_i, tt_log_probs_rm_i = self.model[i].ttnn_decode_forward(
                 tt_h_transform,
                 tt_xattn_mask_transform,
                 tt_full_text_mask_expand_1NSH_transform,
@@ -1349,12 +1389,14 @@ class Generator:
                 cross_page_table=tt_cross_page_table[i],
             )
             tt_logits_rm.append(tt_logits_rm_i)
+            tt_log_probs_rm.append(tt_log_probs_rm_i)
             ttnn.end_trace_capture(self.model_args[i].mesh_device, trace_id, cq_id=0)
         logger.info("Done Capturing Decode Trace")
 
         return (
             trace_ids,
             tt_logits_rm,
+            tt_log_probs_rm,
             tt_h,
             tt_xattn_mask,
             tt_full_text_mask_expand_1NSH,
