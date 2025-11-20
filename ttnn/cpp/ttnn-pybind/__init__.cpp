bbd6a450aee52ba8715ae8e368da7eed28c033bf
Siddharth Sundaram <ssundaram@tenstorrent.com>
2025-11-20 21:00:26 +0000

    ^

--- ttnn/cpp/ttnn-pybind/__init__.cpp
+++ ttnn/cpp/ttnn-pybind/__init__.cpp
@@ -307,7 +307,8 @@ PYBIND11_MODULE(_ttnn, module) {
         []() -> std::uint64_t { return ttnn::CoreIDs::instance().fetch_and_increment_python_operation_id(); },
         "Increment tensor id and return the previously held id");
 
-    module.def("next_tensor_id", &ttnn::Tensor::next_id, "Atomically fetch and increment the tensor ID counter");
+    module.def(
+        "next_tensor_id", &tt::tt_metal::Tensor::next_id, "Atomically fetch and increment the tensor ID counter");
 
     module.def(
         "get_device_operation_id",
