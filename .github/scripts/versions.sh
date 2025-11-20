3d5f07c0fb92bc2fdd1831e4d77f97e5e2a60ca6
Filip Vranic <fvranic@tenstorrent.com>
2025-11-20 11:58:26 +0100

    build: update tt-exalens version (#862)
    ### Ticket
    None
    
    ### Problem description
    Update tt-exalens version to the latest, containing methods needed for
    getting the call stack.
    
    ### What's changed
    New version of exalens.
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update

--- .github/scripts/versions.sh
+++ .github/scripts/versions.sh
@@ -7,7 +7,7 @@
 # This file should be sourced by installation scripts
 
 # tt-exalens configuration
-export EXALENS_VERSION="0.1.251103+dev.7172499-cp310-cp310-linux_x86_64"
+export EXALENS_VERSION="0.1.251118+dev.0bb2d30-cp310-cp310-linux_x86_64"
 export EXALENS_TAG="${EXALENS_VERSION%%+*}"
 export EXALENS_WHEEL="ttexalens-${EXALENS_VERSION}.whl"
 export EXALENS_URL="https://github.com/tenstorrent/tt-exalens/releases/download/${EXALENS_TAG}/${EXALENS_WHEEL}"
