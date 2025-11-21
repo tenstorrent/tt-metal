380cc6198ddf61e2c9c379439c9f365766a666d0
Artem Yerofieiev <ayerofieiev@tenstorrent.com>
2025-11-21 15:47:32 +0000

    fix clang tidy

--- ttnn/cpp/ttnn/operations/experimental/paged_cache/device/update_cache/paged_update_cache_device_operation.hpp
+++ ttnn/cpp/ttnn/operations/experimental/paged_cache/device/update_cache/paged_update_cache_device_operation.hpp
@@ -45,9 +45,9 @@ struct PagedUpdateCacheDeviceOperation {
         const Tensor& input_tensor,
         const std::vector<uint32_t>& update_idxs,
         const std::optional<const Tensor>& update_idxs_tensor,
-        const std::optional<bool> share_cache,
+        std::optional<bool> share_cache,
         const std::optional<const Tensor>& page_table,
-        const uint32_t batch_offset,
+        uint32_t batch_offset,
         std::optional<const ttnn::DeviceComputeKernelConfig> compute_kernel_config,
         const std::optional<const std::set<ttnn::MeshCoordinate>>& mesh_coords);
 };
