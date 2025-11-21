2395e465af6d78a2f792ba7ce25609077122e281
Nathan Maurice <nmaurice@tenstorrent.com>
2025-11-21 10:17:45 +0000

    Apply micro-optimizations to exp_61f

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_exp.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_exp.h
@@ -101,34 +101,64 @@ sfpi_inline sfpi::vFloat _sfpu_exp_21f_(sfpi::vFloat val) {
     return y;
 }
 
+/*
+ * This function implements the exponential function using a polynomial approximation algorithm
+ * based on "Simple Multiple Precision Algorithms for Exponential Functions [Tips & Tricks]"
+ * by Moroz et al. 2022 (https://doi.org/10.1109/MSP.2022.3157460).
+ * More specifically, it is the implementation of the `exp_61f` algorithm described in Section 5
+ *
+ * @param val The input value (sfpi::vFloat vector), can be any floating point number
+ *
+ * @return sfpi::vFloat Result of exp(val)
+ *
+ * @see Moroz et al. 2022 - "Simple Multiple Precision Algorithms for Exponential Functions"
+ *      ( https://doi.org/10.1109/MSP.2022.3157460 )
+ */
 sfpi_inline sfpi::vFloat _sfpu_exp_61f_(sfpi::vFloat val) {
-    sfpi::vFloat y = sfpi::vConst0;
-    v_if(val > -87.3f) {
-        // The paper relies on the following formula (c.f. Section 2 and 3 of paper):
-        // z = (bias + x * factor * N_m); where:
-        // factor = 0x00b8aa3b (computed through log(e))
-        // bias = 0x3f800000
-        sfpi::vInt z = sfpu::_float_to_int32_(val * sfpi::vFloat(0x00b8aa3b) + sfpi::vFloat(0x3f800000));
-        sfpi::vInt zii = sfpi::exexp(sfpi::reinterpret<sfpi::vFloat>(z));   // Extract exponent
-        sfpi::vInt zif = sfpi::exman9(sfpi::reinterpret<sfpi::vFloat>(z));  // Extract mantissa
+    // This function computes exp(x) by leverage mathematic properties of exp(x):
+    // That is, exp(x) = 2**(x / ln2) = 2**(x_i) * 2**(x_f) where
+    // - z_i = trunc(x / ln2) (integer part)
+    // - z_f = x/ln2 - trunc(x/ln2) (fractional part)
+    //
+    // The paper relies on the following formula (c.f. Section 2 and 3 of paper):
+    // z = (bias + x * factor * N_m); where:
+    // factor = log(2) * 2^23
+    // bias = 127 * 2^23
+    // Fundamentally, the formula in the paper computes
+    // z = val * log(2) * 2^23 + 127 * 2^23
+    // This formula prepares for the computation of exp(x) = 2^(x/log(2))
+    //
+    // In our case, we will let the multiplication by 2^23 be done implicitly in _float_to_int32_exp21f_ function
+    sfpi::vFloat xlog2 = val * sfpi::vConstFloatPrgm0 + 127.f;
 
-        // Normalize mantissa field into a fractional value in [0,1)
-        sfpi::vFloat frac = sfpi::int32_to_float(zif, 0) * sfpi::vFloat(1.1920929e-7f);
+    // Intermedirary values can overflow in xlog2 is outside of [0, 256[ which leads to invalid resutls instead of 0
+    // (when input < -88.5) and +inf (when input > 88.5)
+    // To avoid this, we clamp xlog2 to [0, 255]
+    // (thresholds values are rounded to bf16, as it does not change result but only requires one SFPLOADI vs. two)
+    sfpi::vFloat threshold_low = 0.f;
+    sfpi::vFloat threshold_high = sfpi::vFloat(255.f);
+    vec_min_max(threshold_low, xlog2);
+    vec_min_max(xlog2, threshold_high);
 
-        // Evaluate degree-6 polynomial coefficients using Horner’s rule
-        // Note: Unlike exp_21f, in exp_61f all polynomial coefficients are floating-point values.
-        // In exp_21f, the paper mixes integer and float constants to perform bit-level manipulation of the exponent and
-        // mantissa fields (using bit manipulation techniques - BMT) for exactness. In exp_61f, all coefficients are
-        // floating-point values derived from the Chebyshev polynomial approach, making the implementation simpler and
-        // purely mathematical without integer-based operations.
-        sfpi::vFloat poly = POLYVAL7<sfpi::vFloat>(
-            0.0002170391f, 0.001243946f, 0.0096788315f, 0.055483369f, 0.24022982f, 0.69314699f, 1.0000000018f, frac);
+    sfpi::vInt z = _float_to_int32_exp21f_(xlog2);
 
-        // Restore exponent
-        zii = sfpi::reinterpret<sfpi::vInt>(sfpi::setexp(poly, 127U + zii));
-        y = sfpi::reinterpret<sfpi::vFloat>(zii);
-    }
-    v_endif;
+    sfpi::vInt exponential_part =
+        exexp_nodebias(sfpi::reinterpret<sfpi::vFloat>(z));  // Extract exponent ( = 2**(integer part of val/ln2))
+    sfpi::vInt fractional_part =
+        sfpi::exman9(sfpi::reinterpret<sfpi::vFloat>(z));  // Extract mantissa ( = leftover part, in [0; 1])
+
+    sfpi::vFloat frac = sfpi::int32_to_float(fractional_part, 0);
+    frac = sfpi::addexp(frac, -23);
+
+    // To refine approximation of 2**(x_f), we use an approximation of 2**x on [0; 1]
+    // This uses a 2nd degree polynomial adjustment of the fractional part
+    frac = PolynomialEvaluator::eval(
+        frac, sfpi::vConst1, 0.69314699f, 0.24022982f, 0.055483369f, 0.0096788315f, 0.001243946f, 0.0002170391f);
+    // frac = PolynomialEvaluator::eval(frac, sfpi::vConst1, 8.2629562e-08, 3.413871e-15, 9.399248e-23, 1.9546244e-30,
+    // 0.001243946f, 2.9946911e-38);
+
+    // Recombined exponent and mantissa: this is equivalent to 2**(x_i) * 2**(x_f)
+    sfpi::vFloat y = sfpi::setexp(frac, exponential_part);
 
     return y;
 }
