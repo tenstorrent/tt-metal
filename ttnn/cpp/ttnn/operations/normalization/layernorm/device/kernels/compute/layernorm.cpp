f0b5bed6dbf6c3f1f954973f781aea48a9c73aee
Ryan Miller <rmiller@tenstorrent.com>
2025-11-21 21:42:41 +0000

    Remove unused headers

--- ttnn/cpp/ttnn/operations/normalization/layernorm/device/kernels/compute/layernorm.cpp
+++ ttnn/cpp/ttnn/operations/normalization/layernorm/device/kernels/compute/layernorm.cpp
@@ -10,17 +10,9 @@
 #define BCAST_LLKOP EltwiseBinaryType::ELWMUL
 #define BCAST_DIM BroadcastType::COL
 
-#include "compute_kernel_api/reduce.h"
 #include "compute_kernel_api/bcast.h"
 #include "compute_kernel_api/eltwise_binary.h"
 #include "compute_kernel_api/layernorm.h"
-#include "compute_kernel_api/tile_move_copy.h"
-#include "compute_kernel_api/eltwise_binary_sfpu.h"
-#include "compute_kernel_api/eltwise_unary/eltwise_unary.h"
-#include "compute_kernel_api/eltwise_unary/sqrt.h"
-#include "compute_kernel_api/eltwise_unary/recip.h"
-#include "compute_kernel_api/transpose_wh_dest.h"
-#include "compute_kernel_api/eltwise_unary/binop_with_scalar.h"
 #include "ttnn/operations/normalization/kernel_util/compute/numeric.h"
 
 ALWI void ACQ() { acquire_dst(); }
