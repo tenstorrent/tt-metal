905e5a025d5d049bdc2a708b87559673d5ec8817
Adriel Bustamante <abustamante@tenstorrent.com>
2025-11-21 22:31:53 +0000

    Removed exit()

--- tt-train/sources/examples/nano_gpt/main.cpp
+++ tt-train/sources/examples/nano_gpt/main.cpp
@@ -436,8 +436,6 @@ int main(int argc, char **argv) {
         ttml::ttnn_fixed::distributed::enable_fabric(num_devices);
     }
 
-    exit(0);
-
     initialize_device(device_config.mesh_shape, device_config.device_ids);
     auto *device = &ttml::autograd::ctx().get_device();
 
