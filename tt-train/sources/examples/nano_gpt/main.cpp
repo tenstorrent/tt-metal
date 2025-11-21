e961184096df50fa69093228f9d07e980ed3987d
Adriel Bustamante <abustamante@tenstorrent.com>
2025-11-21 00:43:43 +0000

    Formatting

--- tt-train/sources/examples/nano_gpt/main.cpp
+++ tt-train/sources/examples/nano_gpt/main.cpp
@@ -400,10 +400,15 @@ int main(int argc, char **argv) {
     }
 
     auto create_dataset =
-        [](const auto &text, const auto sequence_length, const auto &tokenizer_type) {
+        [](const auto &text, const auto sequence_length, const auto &config) {
+
+            std::string tokenizer_type = config.tokenizer_type;
+
             if (tokenizer_type == "char") {
                 auto [dataset, tokenizer] = ttml::datasets::create_in_memory_token_dataset<ttml::tokenizers::CharTokenizer>(
                     std::get<std::string>(text), sequence_length);
+
+                    std::visit([&](auto &&arg) { arg.vocab_size = vocab_size; }, config.transformer_config);
 
                 return dataset;
             }
@@ -423,7 +428,7 @@ int main(int argc, char **argv) {
             }
         };
 
-    auto dataset = create_dataset(text_or_tokens, sequence_length, training_config.tokenizer_type);
+    auto dataset = create_dataset(text_or_tokens, sequence_length, training_config);
 
     fmt::print("Dataset size: {}\n", dataset.get_size());
 
