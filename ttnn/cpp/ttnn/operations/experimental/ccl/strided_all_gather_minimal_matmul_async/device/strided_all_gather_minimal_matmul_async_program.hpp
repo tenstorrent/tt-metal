49e08d44eb551a3d644f38b314d634f87600e38c
Jonathan Su <jonathansu@tenstorrent.com>
2025-11-22 00:19:52 +0000

    helper function is only used in cpp

--- ttnn/cpp/ttnn/operations/experimental/ccl/strided_all_gather_minimal_matmul_async/device/strided_all_gather_minimal_matmul_async_program.hpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/strided_all_gather_minimal_matmul_async/device/strided_all_gather_minimal_matmul_async_program.hpp
@@ -35,34 +35,6 @@ struct StridedAllGatherMinimalMatmulAsyncProgramFactory {
         const operation_attributes_t& operation_attributes,
         const tensor_args_t& tensor_args,
         tensor_return_value_t& tensor_return_value);
-
-    static cached_program_t strided_all_gather_minimal_matmul_async_program(
-        /* General Params */
-        const Tensor& input_tensor,
-        Tensor& all_gather_output_tensor,
-        const Tensor& weight_tensor,
-        Tensor& matmul_output_tensor,
-        IDevice* target_device,
-        const MeshCoordinate& target_device_coord,
-        const std::optional<MeshCoordinate>& forward_coord,
-        const std::optional<MeshCoordinate>& backward_coord,
-        uint32_t dim,
-        uint32_t num_links,
-        uint32_t ring_size,
-        uint32_t ring_index,
-        ttnn::ccl::Topology topology,
-        const std::vector<GlobalSemaphore>& semaphore,
-        const std::optional<GlobalSemaphore>& barrier_semaphore,
-        const std::optional<tt::tt_metal::SubDeviceId>& sub_device_id,
-        std::optional<uint32_t> num_workers_per_direction_opt,
-        std::optional<uint32_t> num_buffers_per_channel,
-        CoreCoord core_grid_offset,
-
-        /* Matmul Params */
-        const std::optional<const Tensor>& bias,
-        const std::optional<operations::unary::UnaryWithParam>& fused_activation,
-        minimal_matmul::MinimalMatmulConfig config,
-        DeviceComputeKernelConfig compute_kernel_config);
 };
 
 }  // namespace ttnn::operations::experimental::ccl::strided_all_gather_minimal_matmul_async::program
