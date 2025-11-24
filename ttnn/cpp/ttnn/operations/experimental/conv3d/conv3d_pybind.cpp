1b3d5774a95d3da78c865adc18863ff803e0a88a
velonica0 <like@mail.nankai.edu.cn>
2025-11-24 18:10:48 +0800

    compile success

--- ttnn/cpp/ttnn/operations/experimental/conv3d/conv3d_pybind.cpp
+++ ttnn/cpp/ttnn/operations/experimental/conv3d/conv3d_pybind.cpp
@@ -46,60 +46,68 @@ void py_bind_conv3d(py::module& module) {
                const ttnn::Tensor& weight_tensor,
                const std::optional<ttnn::Tensor>& bias_tensor,
                const Conv3dConfig& config,
+               const tt::tt_metal::DataType& dtype,
+               const uint32_t& output_channels,
+               const std::array<uint32_t, 3>& kernel_size,
+               const std::array<uint32_t, 3>& stride,
+               const std::array<uint32_t, 3>& padding,
+               const std::array<uint32_t, 3>& dilation,
+               const std::string& padding_mode,
+               const uint32_t& groups,
                const std::optional<const MemoryConfig>& memory_config,
                const std::optional<DeviceComputeKernelConfig>& compute_kernel_config) {
-                return self(input_tensor, weight_tensor, bias_tensor, config, memory_config, compute_kernel_config);
+                return self(
+                    input_tensor,
+                    weight_tensor,
+                    bias_tensor,
+                    dtype,
+                    output_channels,
+                    kernel_size,
+                    stride,
+                    padding,
+                    dilation,
+                    padding_mode,
+                    groups,
+                    config,
+                    memory_config,
+                    compute_kernel_config);
             },
             py::kw_only(),
             py::arg("input_tensor"),
             py::arg("weight_tensor"),
             py::arg("bias_tensor") = std::nullopt,
             py::arg("config"),
+            py::arg("dtype"),
+            py::arg("output_channels"),
+            py::arg("kernel_size"),
+            py::arg("stride"),
+            py::arg("padding"),
+            py::arg("dilation"),
+            py::arg("padding_mode") = "zeros",
+            py::arg("groups") = 1,
             py::arg("memory_config") = std::nullopt,
             py::arg("compute_kernel_config") = std::nullopt});
 
-    auto py_conv3d_config = py::class_<Conv3dConfig>(
-                                module,
-                                "Conv3dConfig",
-                                R"doc(
+    auto py_conv3d_config =
+        py::class_<Conv3dConfig>(
+            module,
+            "Conv3dConfig",
+            R"doc(
                             Configuration for the Conv3D operation.
                             )doc")
-                                .def(py::init<>())
-                                .def(
-                                    py::init<
-                                        DataType,
-                                        DataType,
-                                        Layout,
-                                        uint32_t,
-                                        uint32_t,
-                                        uint32_t,
-                                        uint32_t,
-                                        uint32_t,
-                                        uint32_t,
-                                        const std::array<uint32_t, 3>&,
-                                        const std::array<uint32_t, 3>&,
-                                        const std::array<uint32_t, 3>&,
-                                        const std::string&,
-                                        uint32_t,
-                                        CoreCoord>(),
-                                    py::kw_only(),
-                                    py::arg("dtype") = DataType::BFLOAT16,
-                                    py::arg("weights_dtype") = DataType::BFLOAT16,
-                                    py::arg("output_layout") = Layout::ROW_MAJOR,
-                                    py::arg("T_out_block") = 1,
-                                    py::arg("W_out_block") = 1,
-                                    py::arg("H_out_block") = 1,
-                                    py::arg("C_out_block") = 0,
-                                    py::arg("C_in_block") = 0,
-                                    py::arg("output_channels"),
-                                    py::arg("kernel_size"),
-                                    py::arg("stride"),
-                                    py::arg("padding"),
-                                    py::arg("padding_mode") = "zeros",
-                                    py::arg("groups") = 1,
-                                    py::arg("compute_with_storage_grid_size") = CoreCoord{1, 1});
+            .def(py::init<>())
+            .def(
+                py::init<DataType, Layout, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t, CoreCoord>(),
+                py::kw_only(),
+                py::arg("weights_dtype") = DataType::BFLOAT16,
+                py::arg("output_layout") = Layout::ROW_MAJOR,
+                py::arg("T_out_block") = 1,
+                py::arg("W_out_block") = 1,
+                py::arg("H_out_block") = 1,
+                py::arg("C_out_block") = 0,
+                py::arg("C_in_block") = 0,
+                py::arg("compute_with_storage_grid_size") = CoreCoord{1, 1});
 
-    py_conv3d_config.def_readwrite("dtype", &Conv3dConfig::dtype, "");
     py_conv3d_config.def_readwrite("weights_dtype", &Conv3dConfig::weights_dtype, "");
     py_conv3d_config.def_readwrite("output_layout", &Conv3dConfig::output_layout, "");
     py_conv3d_config.def_readwrite("T_out_block", &Conv3dConfig::T_out_block, "");
@@ -107,12 +115,6 @@ void py_bind_conv3d(py::module& module) {
     py_conv3d_config.def_readwrite("H_out_block", &Conv3dConfig::H_out_block, "");
     py_conv3d_config.def_readwrite("C_out_block", &Conv3dConfig::C_out_block, "");
     py_conv3d_config.def_readwrite("C_in_block", &Conv3dConfig::C_in_block, "");
-    py_conv3d_config.def_readwrite("output_channels", &Conv3dConfig::output_channels, "");
-    py_conv3d_config.def_readwrite("kernel_size", &Conv3dConfig::kernel_size, "");
-    py_conv3d_config.def_readwrite("stride", &Conv3dConfig::stride, "");
-    py_conv3d_config.def_readwrite("padding", &Conv3dConfig::padding, "");
-    py_conv3d_config.def_readwrite("padding_mode", &Conv3dConfig::padding_mode, "");
-    py_conv3d_config.def_readwrite("groups", &Conv3dConfig::groups, "");
     py_conv3d_config.def_readwrite("compute_with_storage_grid_size", &Conv3dConfig::compute_with_storage_grid_size, "");
 
     py_conv3d_config.def("__repr__", [](const Conv3dConfig& config) { return fmt::format("{}", config); });
