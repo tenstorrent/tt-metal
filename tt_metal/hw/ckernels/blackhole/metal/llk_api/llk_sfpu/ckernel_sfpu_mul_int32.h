4fb550bbdcd511feec17fd8f33c041b487d7e36f
Jason Davies <jason@jasondavies.com>
2025-11-24 13:31:10 +0000

    Clarify comments.

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_mul_int32.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_mul_int32.h
@@ -87,7 +87,7 @@ inline void mul_int32_init() {
     constexpr uint b1 = p_sfpu::LREG2;
     constexpr uint c = p_sfpu::LREG4;
 
-    // Load instruction templates 0-3.  This is more efficient than using
+    // Load instruction templates i=0-3.  This is more efficient than using
     // SFPCONFIG, but requires DISABLE_BACKDOOR_LOAD=false (the default).
     // Instruction template `i` is specified using `VD=12+i`.
     // Other register parameters (e.g. VA, VB, or VC) are set to 0 if they're
@@ -99,49 +99,55 @@ inline void mul_int32_init() {
     TTI_SFPIADD(0, c, 15, sfpi::SFPIADD_MOD1_CC_NONE);
 
     // Configure macros.
+    //
     // See:
-    // https://github.com/tenstorrent/tt-isa-documentation/blob/main/BlackholeA0/TensixTile/TensixCoprocessor/SFPLOADMACRO.md
+    //   https://github.com/tenstorrent/tt-isa-documentation/blob/main/BlackholeA0/TensixTile/TensixCoprocessor/SFPLOADMACRO.md
+    //
     // For each of simple, MAD, round, store, a macro can use up to one
-    // instruction template.  8 bits for each unit:
-    // 0x80 means set VB=VD instead of VC=VD.
-    // 0x40 means set VD=16 (only readable by scheduled SFPSTORE).
-    // 3 bits for the delay.
-    // 3 bits for template index (4+i), or 3 means SFPSTORE.
+    // instruction template.  Specify 8 bits per unit (high to low):
+    //
+    // - bit 7: set means VB=VD instead of the default, VC=VD.
+    // - bit 6: set means VD=16 (only readable by scheduled SFPSTORE).
+    // - bits 5-3: delay (0-7).
+    // - bits 2-0: template index (4+i), or 3 means SFPSTORE.
 
     // Macro 0:
     {
         constexpr uint simple_bits = 0;
-        constexpr uint mad_bits = 0x80 | 0x00 | (1 << 3) | 5;
-        constexpr uint round_bits = 0x80 | 0x00 | (0 << 3) | 4;
+        constexpr uint mad_bits = 0x80 | 0x00 | (1 << 3) | (4 + 1);
+        constexpr uint round_bits = 0x80 | 0x00 | (0 << 3) | (4 + 0);
         constexpr uint store_bits = 0;
 
         TTI_SFPLOADI(0, sfpi::SFPLOADI_MOD0_LOWER, (mad_bits << 8) | simple_bits);
         TTI_SFPLOADI(0, sfpi::SFPLOADI_MOD0_UPPER, (store_bits << 8) | round_bits);
 
+        // Configure Sequence[0], via VD=4+0
         TTI_SFPCONFIG(0, 4, 0);
     }
     // Macro 1:
     {
-        constexpr uint simple_bits = 0x80 | 0x00 | (4 << 3) | 7;
-        constexpr uint mad_bits = 0x80 | 0x00 | (1 << 3) | 5;
-        constexpr uint round_bits = 0x80 | 0x00 | (0 << 3) | 4;
+        constexpr uint simple_bits = 0x80 | 0x00 | (4 << 3) | (4 + 3);
+        constexpr uint mad_bits = 0x80 | 0x00 | (1 << 3) | (4 + 1);
+        constexpr uint round_bits = 0x80 | 0x00 | (0 << 3) | (4 + 0);
         constexpr uint store_bits = 0;
 
         TTI_SFPLOADI(0, sfpi::SFPLOADI_MOD0_LOWER, (mad_bits << 8) | simple_bits);
         TTI_SFPLOADI(0, sfpi::SFPLOADI_MOD0_UPPER, (store_bits << 8) | round_bits);
 
-        TTI_SFPCONFIG(0, 5, 0);
+        // Configure Sequence[1], via VD=4+1
+        TTI_SFPCONFIG(0, 4+1, 0);
     }
     // Macro 2:
     {
-        constexpr uint simple_bits = 0x80 | 0x40 | (4 << 3) | 7;
-        constexpr uint mad_bits = 0x80 | 0x00 | (1 << 3) | 5;
+        constexpr uint simple_bits = 0x80 | 0x40 | (4 << 3) | (4 + 3);
+        constexpr uint mad_bits = 0x80 | 0x00 | (1 << 3) | (4 + 1);
 
-        TTI_SFPCONFIG((mad_bits << 8) | simple_bits, 6, 1);
+        // Configure Sequence[2], via VD=4+2
+        TTI_SFPCONFIG((mad_bits << 8) | simple_bits, 4+2, 1);
     }
     // Macro 3:
     {
-        constexpr uint simple_bits = 0x80 | 0x00 | (0 << 3) | 6;
+        constexpr uint simple_bits = 0x80 | 0x00 | (0 << 3) | (4 + 2);
         constexpr uint mad_bits = 0;
         constexpr uint round_bits = 0;
         constexpr uint store_bits = 0x00 | 0x40 | (2 << 3) | 3;
@@ -149,7 +155,8 @@ inline void mul_int32_init() {
         TTI_SFPLOADI(0, sfpi::SFPLOADI_MOD0_LOWER, (mad_bits << 8) | simple_bits);
         TTI_SFPLOADI(0, sfpi::SFPLOADI_MOD0_UPPER, (store_bits << 8) | round_bits);
 
-        TTI_SFPCONFIG(0, 7, 0);
+        // Configure Sequence[3], via VD=4+3
+        TTI_SFPCONFIG(0, 4+3, 0);
     }
     // Misc: {
     //   StoreMod0: MOD0_FMT_SRCB,
