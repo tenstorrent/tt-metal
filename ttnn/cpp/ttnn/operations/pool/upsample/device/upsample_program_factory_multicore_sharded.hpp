c283e71ce539cc424b0d603832bdcc140f230960
awliu-TT <awliu@tenstorrent.com>
2025-11-25 18:09:23 +0000

    initializing shared_variables struct fields for clang tidy warning

--- ttnn/cpp/ttnn/operations/pool/upsample/device/upsample_program_factory_multicore_sharded.hpp
+++ ttnn/cpp/ttnn/operations/pool/upsample/device/upsample_program_factory_multicore_sharded.hpp
@@ -10,12 +10,12 @@ namespace ttnn::operations::pool::upsample::program {
 
 struct UpsampleMultiCoreShardedProgramFactory {
     struct shared_variables_t {
-        tt::tt_metal::KernelHandle writer_kernel;
-        tt::tt_metal::CBHandle cb_src0;
-        tt::tt_metal::CBHandle out_cb;
-        tt::tt_metal::CBHandle config_cb;
+        tt::tt_metal::KernelHandle writer_kernel{};
+        tt::tt_metal::CBHandle cb_src0{};
+        tt::tt_metal::CBHandle out_cb{};
+        tt::tt_metal::CBHandle config_cb{};
         tt::tt_metal::DeviceStorage config_storage;
-        tt::tt_metal::Buffer* config_buffer;
+        tt::tt_metal::Buffer* config_buffer = nullptr;
     };
 
     using cached_program_t = ttnn::device_operation::CachedProgram<shared_variables_t>;
