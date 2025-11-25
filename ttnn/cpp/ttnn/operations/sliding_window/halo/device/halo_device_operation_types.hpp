796f75d6a657bd4450cb1c18aa108c3a94de72ba
awliu-TT <awliu@tenstorrent.com>
2025-11-25 20:00:04 +0000

    initializing ope arg fields

--- ttnn/cpp/ttnn/operations/sliding_window/halo/device/halo_device_operation_types.hpp
+++ ttnn/cpp/ttnn/operations/sliding_window/halo/device/halo_device_operation_types.hpp
@@ -10,16 +10,16 @@
 namespace ttnn::operations::sliding_window::halo {
 
 struct operation_attributes_t {
-    SlidingWindowConfig config;
-    ParallelConfig parallel_config;
-    uint32_t pad_val;
-    bool remote_read;
-    bool transpose_mcast;
-    uint32_t max_out_nsticks_per_core;
-    uint32_t in_nsticks_per_core;
+    SlidingWindowConfig config{};
+    ParallelConfig parallel_config{};
+    uint32_t pad_val = 0;
+    bool remote_read = false;
+    bool transpose_mcast = false;
+    uint32_t max_out_nsticks_per_core = 0;
+    uint32_t in_nsticks_per_core = 0;
     tt::tt_metal::MemoryConfig output_memory_config;
-    bool is_out_tiled;
-    bool config_tensors_in_dram;
+    bool is_out_tiled = false;
+    bool config_tensors_in_dram = false;
 };
 
 struct tensor_args_t {
