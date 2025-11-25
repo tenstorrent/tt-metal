d8a987807ab6b1bf319090ca6ae75982ddeae967
Nigel Huang <nhuang@tenstorrent.com>
2025-11-25 22:57:18 +0000

    Update poll_l1, read_logical_to_virtual_table

--- tests/tt_metal/tt_metal/test_kernels/dataflow/poll_l1.cpp
+++ tests/tt_metal/tt_metal/test_kernels/dataflow/poll_l1.cpp
@@ -3,22 +3,23 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include <cstdint>
-#include "debug/dprint.h"
+#include "hw/inc/dataflow_api.h"
 
 void kernel_main() {
     set_l1_data_cache<true>();
     uint32_t poll_addr = get_arg_val<uint32_t>(0);
     uint32_t expected_value = get_arg_val<uint32_t>(1);
-    auto sem_addr = reinterpret_cast<volatile uint32_t*>(get_semaphore(get_arg_val<uint32_t>(2)));
+    uint32_t semaphore_id = get_arg_val<uint32_t>(2);
     uint32_t invalidate_cache =
         get_arg_val<uint32_t>(3);  // For CI this will be true but for debug this can be modified
 
-    volatile tt_l1_ptr uint32_t* poll_addr_ptr = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(poll_addr);
-    uint32_t base_value = *poll_addr_ptr;  // Read once to cache it
+    experimental::CoreLocalMem<uint32_t> poll_value(poll_addr);
+    uint32_t base_value = poll_value[0];  // Read once to cache it
 
-    noc_semaphore_set(sem_addr, 1);
+    experimental::Semaphore sem(semaphore_id);
+    sem.set(1);
 
-    while (*poll_addr_ptr != expected_value) {
+    while (poll_value[0] != expected_value) {
         if (invalidate_cache) {
             invalidate_l1_cache();
         }
