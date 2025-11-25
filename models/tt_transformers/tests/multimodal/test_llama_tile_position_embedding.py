6b20d06fdbb84fc6946139e35ab0222a2a5ad3be
Ubuntu <ioannis_alexiou@epam.com>
2025-11-25 15:36:58 +0000

    migrated to HF

--- models/tt_transformers/tests/multimodal/test_llama_tile_position_embedding.py
+++ models/tt_transformers/tests/multimodal/test_llama_tile_position_embedding.py
@@ -5,16 +5,35 @@
 import itertools
 import os
 
-import llama_models.llama3.reference_impl.multimodal.model as llama_reference_mod
 import pytest
 import torch
 from loguru import logger
+from safetensors import safe_open
+from transformers.models.mllama.image_processing_mllama import (
+    convert_aspect_ratios_to_ids,
+    get_all_supported_aspect_ratios,
+)
+from transformers.models.mllama.modeling_mllama import MllamaPrecomputedAspectRatioEmbedding
 
 import ttnn
 from models.common.utility_functions import comp_allclose, comp_pcc, nearest_32
 from models.tt_transformers.tt.model_config import ModelArgs
 from models.tt_transformers.tt.multimodal.llama_tile_position_embedding import TtLlamaTilePositionEmbedding
 from ttnn import ConcatMeshToTensor, ReplicateTensorToMesh
+
+
+def load_partial_weights(weights_path, embedding_layer_prefix):
+    partial_state_dict = {}
+    safetensor_files = [f for f in os.listdir(weights_path) if f.endswith(".safetensors")]
+    for fname in safetensor_files:
+        with safe_open(os.path.join(weights_path, fname), framework="pt", device="cpu") as f:
+            keys = f.keys()
+            for key in keys:
+                if embedding_layer_prefix in key:
+                    print(f"✅ Found in {fname}: {key}")
+                    key_name = "embedding.weight" if "weight" in key else "gate"
+                    partial_state_dict.update({key_name: f.get_tensor(key)})
+    return partial_state_dict
 
 
 @pytest.mark.parametrize(
@@ -56,9 +75,7 @@ def test_conv2d_inference(
     first_layer_prefix = "vision_model.vision_encoder." + (
         "pre_tile_pos_embed." if pre_embed else "post_tile_pos_embed."
     )
-    partial_state_dict = {
-        k[len(first_layer_prefix) :]: v for k, v in state_dict.items() if (k.startswith(first_layer_prefix))
-    }
+    embedding_layer_prefix = "pre_tile_positional_embedding" if pre_embed else "post_tile_positional_embedding"
 
     ntok = nearest_32(model_args.vision_chunk_ntok - (0 if pre_embed else 1))
     dim = model_args.vision_dim
@@ -95,13 +112,27 @@ def test_conv2d_inference(
     tt_aspect_ratios = aspect_ratios.tolist()
 
     ##### Perform the torch ops #####
-    reference_model = llama_reference_mod.TilePositionEmbedding(
-        num_tiles=max_num_tiles,
-        width=dim,
-        gated=gated,
-    )
+    supported_aspect_ratios = get_all_supported_aspect_ratios(max_num_tiles)
+
+    class Config:
+        def __init__(
+            self,
+            max_num_tiles=max_num_tiles,
+            hidden_size=dim,
+            max_aspect_ratio_id=len(supported_aspect_ratios),
+            is_gated=gated,
+        ):
+            self.max_num_tiles = max_num_tiles
+            self.hidden_size = hidden_size
+            self.max_aspect_ratio_id = max_aspect_ratio_id
+            self.is_gated = is_gated
+
+    partial_state_dict = load_partial_weights(model_args.model_base_path.__str__(), embedding_layer_prefix)
+    reference_model = MllamaPrecomputedAspectRatioEmbedding(Config())
     reference_model.load_state_dict(partial_state_dict)
-    reference_output = reference_model(input_tensor, aspect_ratios)
+
+    aspect_ratios_id = torch.from_numpy(convert_aspect_ratios_to_ids(aspect_ratios.unsqueeze(0), max_num_tiles))
+    reference_output = reference_model(input_tensor, aspect_ratios_id)
 
     ##### Perform the TT ops #####
     tt_model = TtLlamaTilePositionEmbedding(
