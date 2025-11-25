5e47eef703fc887737a0395a8101fe7094051443
nour ardo <nardo@tenstorrent.com>
2025-11-25 22:45:10 +0000

    start with mux logic

--- ttnn/cpp/ttnn/operations/ccl/reduce_to_root/device/kernels/sender_writer_kernel.cpp
+++ ttnn/cpp/ttnn/operations/ccl/reduce_to_root/device/kernels/sender_writer_kernel.cpp
@@ -8,6 +8,16 @@
 #include "ttnn/cpp/ttnn/operations/point_to_point/device/kernels/common.hpp"
 #include "ttnn/cpp/ttnn/operations/ccl/common/kernels/moe_utils.hpp"
 
+#include "tt_metal/fabric/hw/inc/noc_addr.h"
+#include "tt_metal/fabric/hw/inc/packet_header_pool.h"
+#include "cpp/ttnn/operations/ccl/kernel_common/worker_routing_utils.hpp"
+#include "cpp/ttnn/operations/ccl/kernel_common/worker_sync_utils.hpp"
+#include "cpp/ttnn/operations/ccl/ccl_host_types.hpp"
+#include "tt_metal/fabric/hw/inc/tt_fabric_status.h"
+#include <cstdint>
+#include <utility>
+#include "tt_metal/fabric/hw/inc/linear/api.h"
+
 using tt::data_movement::common::round_up;
 using tt::data_movement::common::tt_memmove;
 
@@ -25,18 +35,21 @@ inline void print_full_tile(uint32_t cb_id, uint32_t tile_id = 0, bool untilize 
 
 void kernel_main() {
     DPRINT << "sender writer kernel started\n";
-    constexpr uint32_t accessor_2_idx = get_compile_time_arg_val(0);
+    constexpr uint32_t fabric_ct_idx = get_compile_time_arg_val(0);
     constexpr uint32_t cb_id_l = get_compile_time_arg_val(1);
     constexpr uint32_t cb_id_s = get_compile_time_arg_val(2);
     constexpr uint32_t cb_id_m = get_compile_time_arg_val(3);
     constexpr uint32_t packet_header_cb_id = get_compile_time_arg_val(4);
     constexpr uint32_t packet_cb_id = get_compile_time_arg_val(5);
     constexpr uint32_t alignment = get_compile_time_arg_val(6);
-    constexpr uint32_t core_noc_x = get_compile_time_arg_val(7);
-    constexpr uint32_t core_noc_y = get_compile_time_arg_val(8);
-    constexpr auto dst_buffer_args = TensorAccessorArgs<9>();
 
     constexpr size_t packet_header_size_bytes = sizeof(PACKET_HEADER_TYPE);
+
+    constexpr uint8_t fabric_mux_num_buffers_per_channel = get_compile_time_arg_val(fabric_ct_idx);
+    constexpr size_t fabric_mux_channel_buffer_size_bytes = get_compile_time_arg_val(fabric_ct_idx + 1);
+    constexpr size_t fabric_mux_status_address = get_compile_time_arg_val(fabric_ct_idx + 2);
+    constexpr size_t fabric_mux_termination_signal_address = get_compile_time_arg_val(fabric_ct_idx + 3);
+    constexpr uint32_t num_mux_clients = get_compile_time_arg_val(fabric_ct_idx + 4);
 
     const uint32_t receiver_base_address = get_arg_val<uint32_t>(0);
     const uint32_t page_idx_start = get_arg_val<uint32_t>(1);
@@ -50,7 +63,10 @@ void kernel_main() {
     const uint32_t max_pages_per_packet_ms = 2;
     const auto page_segments = get_arg_val<uint32_t>(5);  // always 1 delete unecessay parts
     const uint32_t receive_semaphore_addr = get_arg_val<uint32_t>(6);
-    const bool dst_is_forward = get_arg_val<uint32_t>(7);
+    const uint32_t core_noc_x = get_arg_val<uint32_t>(7);
+    const uint32_t core_noc_y = get_arg_val<uint32_t>(8);
+    const uint32_t out_ready_sem_x = get_arg_val<uint32_t>(9);
+    const uint32_t out_ready_sem_y = get_arg_val<uint32_t>(10);
 
     const uint32_t aligned_page_size_bytes = round_up(page_size_bytes, alignment);
 
@@ -59,11 +75,53 @@ void kernel_main() {
     DPRINT << "new payload size bytes: " << (uint32_t)new_payload_size_bytes << ENDL();
 
     // reusing the last arg for fabric setup, therefore index overlaps.
-    size_t conn_arg_idx = 7;
+    size_t arg_idx = 11;
 
-    auto fabric_connection = FabricConnectionManager::build_from_args<
-        FabricConnectionManager::BuildFromArgsMode::BUILD_AND_OPEN_CONNECTION_START_ONLY>(conn_arg_idx);
+    bool is_forward = get_arg_val<uint32_t>(arg_idx++) == 1;
+    const bool is_termination_master = get_arg_val<uint32_t>(arg_idx++);
+    const uint8_t fabric_mux_x = get_arg_val<uint32_t>(arg_idx++);
+    const uint8_t fabric_mux_y = get_arg_val<uint32_t>(arg_idx++);
+    const size_t fabric_mux_channel_base_address = get_arg_val<uint32_t>(arg_idx++);
+    const size_t fabric_mux_connection_info_address = get_arg_val<uint32_t>(arg_idx++);
+    const size_t fabric_mux_connection_handshake_address = get_arg_val<uint32_t>(arg_idx++);
+    const size_t fabric_mux_flow_control_address = get_arg_val<uint32_t>(arg_idx++);
+    const size_t fabric_mux_buffer_index_address = get_arg_val<uint32_t>(arg_idx++);
+    const uint8_t fabric_mux_channel_id = get_arg_val<uint32_t>(arg_idx++);
 
+    uint32_t termination_sync_address = get_semaphore(get_arg_val<uint32_t>(arg_idx++));
+    uint32_t local_fabric_mux_status_address = get_semaphore(get_arg_val<uint32_t>(arg_idx++));
+    uint32_t local_flow_control_address = get_semaphore(get_arg_val<uint32_t>(arg_idx++));
+    uint32_t local_teardown_address = get_semaphore(get_arg_val<uint32_t>(arg_idx++));
+    uint32_t local_buffer_index_address = get_semaphore(get_arg_val<uint32_t>(arg_idx++));
+
+    uint32_t termination_master_noc_x = get_arg_val<uint32_t>(arg_idx++);
+    uint32_t termination_master_noc_y = get_arg_val<uint32_t>(arg_idx++);
+
+    tt::tt_fabric::WorkerToFabricMuxSender<fabric_mux_num_buffers_per_channel>* mux_connection_handle;
+    tt::tt_fabric::WorkerToFabricMuxSender<fabric_mux_num_buffers_per_channel> mux_connection;
+    mux_connection = tt::tt_fabric::build_connection_to_fabric_endpoint<fabric_mux_num_buffers_per_channel>(
+        fabric_mux_x,
+        fabric_mux_y,
+        fabric_mux_channel_id,
+        fabric_mux_num_buffers_per_channel,
+        fabric_mux_channel_buffer_size_bytes,
+        fabric_mux_channel_base_address,
+        fabric_mux_connection_info_address,
+        fabric_mux_connection_handshake_address,
+        fabric_mux_flow_control_address,
+        fabric_mux_buffer_index_address,
+        local_flow_control_address,
+        local_teardown_address,
+        local_buffer_index_address);
+    mux_connection_handle = &mux_connection;
+    DPRINT << "after fabric connection build\n";
+    tt::tt_fabric::wait_for_fabric_endpoint_ready(
+        fabric_mux_x, fabric_mux_y, fabric_mux_status_address, local_fabric_mux_status_address);
+    DPRINT << "after fabric endpoint ready\n";
+
+    tt::tt_fabric::fabric_client_connect(*mux_connection_handle);
+
+    DPRINT << "after fabric client connect\n";
     // set up packet header buffer
     cb_reserve_back(packet_header_cb_id, 1);
     uint32_t packet_header_addr = get_read_ptr(packet_header_cb_id);
@@ -71,8 +129,6 @@ void kernel_main() {
 
     auto* packet_header_ptr = reinterpret_cast<volatile PACKET_HEADER_TYPE*>(packet_header_addr);
     fabric_set_unicast_route<false>((tt::tt_fabric::LowLatencyPacketHeader*)packet_header_ptr, dst_num_hops);
-
-    const auto dst_buffer = TensorAccessor(dst_buffer_args, receiver_base_address, new_payload_size_bytes);
 
     // working memory to hold coalesced packet
     cb_reserve_back(packet_cb_id, 1);
@@ -83,19 +139,18 @@ void kernel_main() {
     uint32_t curr_pages_per_packet = std::min(max_pages_per_packet_l, page_idx_end - page_idx_start);
     uint32_t packet_idx = page_idx_start / max_pages_per_packet_l;
 
+    DPRINT << "before semaphore wait\n";
     // wait for receiver to signal it is ready
     auto local_semaphore_ptr = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(receive_semaphore_addr);
-    noc_semaphore_wait(local_semaphore_ptr, 1);
+
+    DPRINT << "waiting on semaphore at addr: " << (uint32_t)receive_semaphore_addr << "\n";
+    // HEREEEE Change to 1 if this doesn't work
+    noc_semaphore_wait_min(local_semaphore_ptr, 1);
     // clean up semaphore – needs to be done before the sender side semaphore increment if we're re-using the semaphore
     // in subsequent program cache hits
-    noc_semaphore_set(local_semaphore_ptr, 0);
+    // noc_semaphore_set(local_semaphore_ptr, 0);
 
-    fabric_connection.open_finish();
-    auto& connection_direction =
-        tt::point_to_point::common::connection_direction_collection(dst_is_forward, fabric_connection);
-
-    // uint64_t dst_noc_addr = get_noc_addr(core_noc_x, core_noc_y, receiver_base_address, 0);
-
+    DPRINT << "before loop\n";
     for (uint32_t page_idx = page_idx_start, packet_page_idx = 0; page_idx < page_idx_end; ++page_idx) {
         cb_wait_front(cb_id_l, 1);
         uint32_t src_page_base_addr = get_read_ptr(cb_id_l);
@@ -121,12 +176,19 @@ void kernel_main() {
                 packet_m_addr + aligned_page_size_bytes, src_page_base_addr_s, page_size_bytes);
             cb_pop_front(cb_id_s, 1);
             DPRINT << "finished adding s and m data to the packet\n";
-            const uint64_t dst_noc_addr = dst_buffer.get_noc_addr(packet_idx, 0, 0);
+            // const uint64_t dst_noc_addr = dst_buffer.get_noc_addr(packet_idx, 0, 0);
+            const uint32_t dst_noc_addr = get_noc_addr(core_noc_x, core_noc_y, receiver_base_address);
             DPRINT << "before NOC UNICAST WRITE\n";
-            tt::tt_fabric::linear::to_noc_unicast_write(
-                align(new_payload_size_bytes, alignment), packet_header_ptr, packet_idx, dst_buffer);
+            packet_header_ptr->to_noc_unicast_write(
+                NocUnicastCommandHeader{dst_noc_addr}, align(new_payload_size_bytes, alignment));
+
             DPRINT << "AFTER NOC UNICAST WRITE\n";
-            perform_payload_send(connection_direction, packet_base_addr, new_payload_size_bytes, packet_header_ptr);
+            // perform_payload_send(mux_connection, packet_base_addr, new_payload_size_bytes, packet_header_ptr);
+            mux_connection.wait_for_empty_write_slot();
+            mux_connection.send_payload_without_header_non_blocking_from_address(
+                packet_base_addr, new_payload_size_bytes);
+            mux_connection.send_payload_flush_non_blocking_from_address(
+                (uint32_t)packet_header_ptr, sizeof(PACKET_HEADER_TYPE));
 
             DPRINT << "performing packet send for packet idx: " << (uint32_t)packet_idx << ENDL();
             // reset counters
@@ -150,9 +212,20 @@ void kernel_main() {
     fabric_set_unicast_route<false>((tt::tt_fabric::LowLatencyPacketHeader*)sem_header_ptr, dst_num_hops);
     sem_header_ptr->to_noc_unicast_atomic_inc(tt::tt_fabric::NocUnicastAtomicIncCommandHeader{receive_sem_noc_addr, 1});
 
-    connection_direction.wait_for_empty_write_slot();
-    connection_direction.send_payload_flush_blocking_from_address((uint32_t)sem_header_ptr, packet_header_size_bytes);
+    mux_connection.wait_for_empty_write_slot();
+    mux_connection.send_payload_flush_blocking_from_address((uint32_t)sem_header_ptr, packet_header_size_bytes);
 
-    fabric_connection.close();
+    tt::tt_fabric::fabric_client_disconnect(*mux_connection_handle);
+    if (is_termination_master) {
+        auto* termination_sync_ptr = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(termination_sync_address);
+        noc_semaphore_wait(termination_sync_ptr, num_mux_clients - 1);
+        tt::tt_fabric::fabric_endpoint_terminate(fabric_mux_x, fabric_mux_y, fabric_mux_termination_signal_address);
+    } else {
+        uint64_t dest_addr =
+            safe_get_noc_addr(termination_master_noc_x, termination_master_noc_y, termination_sync_address, 0);
+        noc_semaphore_inc(dest_addr, 1);
+        noc_async_atomic_barrier();
+    }
+
     DPRINT << "sender writer kernel completed\n";
 }
