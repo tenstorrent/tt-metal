54cf7c34fe6d519499839ae4f6b34e7b10647545
Miłosz Gajewski <mgajewski@tenstorrent.com>
2025-11-25 16:09:41 +0100

    #32539: implement `ttnn.manual_seed` op (#33043)
    ### Ticket
    #32539
    
    ### Problem description
    
    `ttnn.manual_seed` consisted only of scaffolding.
    
    ### What's changed
     
    - Implemented device logic covering all 4 cases from ticked description,
    - Updated test,
    - Updated documentation,
    - Added example usage,
    - Added implementation docs.
    
    ### Next steps
    
    This PR focuses on delivering the complete and correct functionality.
    Future improvements—both in performance and dispatch efficiency—may
    include:
    * Having a single core read the `user_id` and `seed` tensors and then
    multicast them to the remaining cores,
    * Moving the `core_id == user_ids[i]` check into the reader kernel page.
    * If sub_core_grid is not passed we can calculate core_id with
    `get_absolute_logical_y() * compute_with_storage_grid_size_x +
    get_absolute_logical_x();` instead of iterating on host side.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/19637444350
    
    + tested also on wh-lb-92 - 4 x n300

---
+++ ttnn/cpp/ttnn/operations/reduction/manual_seed/device/kernels/compute/manual_seed_single_seed_receive_user_id.cpp
@@ -0,0 +1,43 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "compute_kernel_api.h"
+#include "compute_kernel_api/common.h"
+#include "compute_kernel_api/eltwise_unary/rand.h"
+
+#include <cstdint>
+
+namespace NAMESPACE {
+void MAIN {
+    // Runtime args
+    const uint32_t number_of_ids = get_arg_val<uint32_t>(0);
+
+    // Compile time args
+    constexpr uint32_t core_id = get_compile_time_arg_val(0);
+    constexpr uint32_t user_ids_cb_index = get_compile_time_arg_val(1);
+    constexpr uint32_t seed = get_compile_time_arg_val(2);
+
+    // Constants
+    constexpr uint32_t one_tile = 1;
+    constexpr uint32_t metadata_fields = 4;
+
+    // Read user_id from circular buffer
+    cb_wait_front(user_ids_cb_index, one_tile);
+    uint32_t* user_ids = nullptr;
+    cb_get_tile(user_ids_cb_index, 0, &user_ids);
+    user_ids += metadata_fields;  // Skip metadata
+
+    // Get info
+    for (uint32_t i = 0; i < number_of_ids; i++) {
+        if (core_id == user_ids[i]) {
+            // Set random generator with seed
+            rand_tile_init(seed);
+            break;
+        }
+    }
+
+    // Remove read token
+    cb_pop_front(user_ids_cb_index, one_tile);
+}
+}  // namespace NAMESPACE
