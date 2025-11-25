518ec1fa4ae8d89c18298fbb173dd333d9f205f5
Andy Wei Liu <awliu@tenstorrent.com>
2025-11-24 20:12:51 -0800

    Update ttnn/cpp/ttnn/operations/kv_cache/device/update_cache_multi_core_program_factory.hpp
    Co-authored-by: Artem Yerofieiev <169092593+ayerofieiev-tt@users.noreply.github.com>

--- ttnn/cpp/ttnn/operations/kv_cache/device/update_cache_multi_core_program_factory.hpp
+++ ttnn/cpp/ttnn/operations/kv_cache/device/update_cache_multi_core_program_factory.hpp
@@ -10,13 +10,13 @@ namespace ttnn::operations::kv_cache::program {
 
 struct UpdateCacheMultiCoreProgramFactory {
     struct shared_variables_t {
-        tt::tt_metal::KernelHandle unary_reader_kernel_id;
-        tt::tt_metal::KernelHandle unary_writer_kernel_id;
+        tt::tt_metal::KernelHandle unary_reader_kernel_id {};
+        tt::tt_metal::KernelHandle unary_writer_kernel_id {};
         std::vector<CoreCoord> cores;
-        uint32_t Wbytes;
-        uint32_t Wt;
+        uint32_t Wbytes = 0;
+        uint32_t Wt = 0;
         std::vector<uint32_t> cache_start_ids;
-        tt::tt_metal::CBHandle cb_src1;
+        tt::tt_metal::CBHandle cb_src1 {};
     };
 
     using cached_program_t = ttnn::device_operation::CachedProgram<shared_variables_t>;
