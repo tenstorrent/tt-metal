44f3709377cfafaf9e4d4a2bf281ac6e8c043b60
Petar Milojevic <pmilojevic@tenstorrent.com>
2025-11-26 10:43:00 +0000

    Made current changes for load_checkpoints.py refactorization

--- models/demos/gemma3/tt/model_config.py
+++ models/demos/gemma3/tt/model_config.py
@@ -13,12 +13,8 @@ from loguru import logger
 
 import ttnn
 from models.common.utility_functions import is_blackhole, is_wormhole_b0, nearest_32
-from models.demos.gemma3.tt.load_checkpoints import (
-    convert_meta_to_hf,
-    convert_vision_hf_to_meta,
-    convert_vision_meta_to_hf,
-    standardize_hf_keys_multimodal,
-)
+from models.demos.gemma3.tt.load_checkpoints import convert_vision_hf_to_meta
+from models.demos.t3000.llama2_70b.reference.llama.llama31_8b.model import Transformer
 from models.tt_transformers.tt.common import (
     calculate_hidden_dim,
     encode_prompt_hf,
@@ -30,10 +26,13 @@ from models.tt_transformers.tt.common import (
 )
 from models.tt_transformers.tt.load_checkpoints import (
     convert_hf_to_meta,
+    convert_meta_to_hf,
+    convert_vision_meta_to_hf,
     load_hf_state_dict,
     load_meta_state_dict,
     reverse_permute,
     standardize_hf_keys,
+    standardize_hf_keys_multimodal,
 )
 from models.tt_transformers.tt.model_config import (
     CheckpointType,
