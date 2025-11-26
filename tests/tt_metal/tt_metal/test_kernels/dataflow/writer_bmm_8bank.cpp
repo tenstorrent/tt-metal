3eb1beba6de2633319be9602ccdb7c6458669ec9
Nigel Huang <nhuang@tenstorrent.com>
2025-11-26 01:08:13 +0000

    Update writer_bmm_8bank

--- tests/tt_metal/tt_metal/test_kernels/dataflow/writer_bmm_8bank.cpp
+++ tests/tt_metal/tt_metal/test_kernels/dataflow/writer_bmm_8bank.cpp
@@ -20,17 +20,22 @@ void kernel_main() {
     constexpr auto dst_args = TensorAccessorArgs<0>();
     const auto s = TensorAccessor(dst_args, dst_addr, tile_bytes);
 
+    experimental::CircularBuffer cb(cb_id_out0);
+    experimental::Noc noc;
+
     // C is MN so we iterate in tile RM order
     for (uint32_t nb = 0; nb < batch; nb++) {
         for (uint32_t mt_C = 0; mt_C < Mt; ++mt_C) {      // output tile of C
             for (uint32_t nt_C = 0; nt_C < Nt; ++nt_C) {  // output tile index of C
                 // bmm will generate C's tiles C=A*B, MN=MK*KN, in row major order, we just read them from CB and write
                 // out to DRAM
-                cb_wait_front(cb_id_out0, onetile);
+                cb.wait_front(onetile);
                 uint32_t l1_read_addr = get_read_ptr(cb_id_out0);
+
+                // Using legacy API because async_write_tile is not supported yet
                 noc_async_write_tile(itileC, s, l1_read_addr);
-                noc_async_write_barrier();
-                cb_pop_front(cb_id_out0, onetile);
+                noc.async_write_barrier();
+                cb.pop_front(onetile);
                 // DPRINT << 'W' << 'C' << itileC << ' ' << 'a' << dst_addr << ENDL();
                 // DPRINT << itileC << ' ' << uint32_t(dst_noc_addr) << ENDL();
                 itileC++;
