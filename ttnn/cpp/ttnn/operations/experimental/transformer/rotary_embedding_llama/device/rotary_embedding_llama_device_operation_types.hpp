9fb88f5951ab51ed832b75d82b4e5c3ed26d7311
Aliaksei Sala <aliaksei_sala@epam.com>
2025-11-26 15:44:21 -0800

    Migrate rotary_embedding_llama to new infra

---
+++ ttnn/cpp/ttnn/operations/experimental/transformer/rotary_embedding_llama/device/rotary_embedding_llama_device_operation_types.hpp
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "ttnn/tensor/tensor.hpp"
+#include "ttnn/operations/core/compute_kernel/compute_kernel_config.hpp"
+
+namespace ttnn::operations::experimental::transformer::rotary_embedding_llama {
+
+struct operation_attributes_t {
+    bool is_decode_mode{};
+    tt::tt_metal::MemoryConfig output_mem_config;
+    ttnn::DeviceComputeKernelConfig compute_kernel_config;
+};
+
+struct tensor_args_t {
+    tt::tt_metal::Tensor input_tensor;
+    tt::tt_metal::Tensor cos_cache;
+    tt::tt_metal::Tensor sin_cache;
+    tt::tt_metal::Tensor trans_mat;
+};
+
+}  // namespace ttnn::operations::experimental::transformer::rotary_embedding_llama
