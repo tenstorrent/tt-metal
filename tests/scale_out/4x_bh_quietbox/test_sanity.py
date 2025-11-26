06a4f841e4df3653d3f216b7c8bd478f63ac56f7
nour ardo <nardo@tenstorrent.com>
2025-11-26 20:57:16 +0000

    revert sanity test

--- tests/scale_out/4x_bh_quietbox/test_sanity.py
+++ tests/scale_out/4x_bh_quietbox/test_sanity.py
@@ -10,7 +10,7 @@ from tests.ttnn.utils_for_testing import assert_with_pcc
 
 
 @pytest.mark.parametrize("device_params", [{"fabric_config": ttnn.FabricConfig.FABRIC_2D_DYNAMIC}], indirect=True)
-@pytest.mark.parametrize("mesh_device", [pytest.param((4, 2), id="4x2_grid")], indirect=True)
+@pytest.mark.parametrize("mesh_device", [pytest.param((4, 4), id="4x4_grid")], indirect=True)
 def test_multihost_sanity(mesh_device):
     torch.manual_seed(0)
 
