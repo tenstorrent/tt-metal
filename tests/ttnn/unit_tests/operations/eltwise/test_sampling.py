848757cbfba82b8bd7efb34696141310ae5071e8
Djordje Ivanovic <divanovic@tenstorrent.com>
2025-11-27 19:56:06 +0000

    Fix pcc issue

--- tests/ttnn/unit_tests/operations/eltwise/test_sampling.py
+++ tests/ttnn/unit_tests/operations/eltwise/test_sampling.py
@@ -282,7 +282,7 @@ def test_log_probs_calculation(shape, mesh_device):
     "shape",
     [
         # [1, 1, 256, 256],  # llama on TG and T3K
-        [1, 1, 32, 8 * 18992],  # llama on N300
+        [1, 1, 32, 8 * 64],  # llama on N300
         # [1, 1, 32, 128256],  # llama on T3K with 8 chips
     ],
 )
@@ -300,7 +300,7 @@ def test_log_probs_calculation2(shape, mesh_device):
 
     log_probs_calculator = LogProbsCalculator(shape[-1], mesh_device)
 
-    torch_tensor = torch.arange(0, shape[-1]).repeat(1, 1, 32, 1)
+    torch_tensor = torch.randn(shape)
     # shuffle the tensor in last 2 dimensions
     for i in range(shape[-2]):
         torch_tensor[:, :, i, :] = torch_tensor[:, :, i, torch.randperm(shape[-1])]
@@ -315,16 +315,16 @@ def test_log_probs_calculation2(shape, mesh_device):
         mesh_mapper=ttnn.ShardTensorToMesh(mesh_device, dim=-1),
         memory_config=ttnn.DRAM_MEMORY_CONFIG,
     )
-
+    log_probs_calculator.set_log_probs_mode(True)
     log_probs_calculator.compute_global_stats(logits_tensor)
 
     argmax_tensor = torch.argmax(torch_tensor, dim=-1, keepdim=True)
-    argmax_tensor = argmax_tensor.reshape(
+    indices_tensor = argmax_tensor.reshape(
         argmax_tensor.shape[0], argmax_tensor.shape[1], argmax_tensor.shape[-1], argmax_tensor.shape[-2]
     )
 
     ttnn_indices_tensor = ttnn.from_torch(
-        argmax_tensor,
+        indices_tensor,
         device=mesh_device,
         dtype=ttnn.int32,
         layout=ttnn.TILE_LAYOUT,
@@ -343,6 +343,7 @@ def test_log_probs_calculation2(shape, mesh_device):
     log_probs_torch = F.log_softmax(torch_tensor.float(), dim=-1)
 
     log_probs_torch_argmax = torch.gather(log_probs_torch, dim=-1, index=argmax_tensor)
+    log_probs_torch_argmax = torch.reshape(log_probs_torch_argmax, (1, 1, 1, 32))
 
     passing, pcc = comp_pcc(log_probs_torch_argmax, log_probs_tt_host, pcc=0.99)
     print(f"pcc={pcc}")
