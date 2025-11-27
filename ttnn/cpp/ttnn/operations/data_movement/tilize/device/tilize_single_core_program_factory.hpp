1dda93ff4aea58ecaa6774c680d05416ac84ce09
Artem Yerofieiev <169092593+ayerofieiev-tt@users.noreply.github.com>
2025-11-27 08:11:21 -0800

    Apply suggestion from @ayerofieiev-tt

--- ttnn/cpp/ttnn/operations/data_movement/tilize/device/tilize_single_core_program_factory.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/tilize/device/tilize_single_core_program_factory.hpp
@@ -10,8 +10,8 @@
 namespace ttnn::operations::data_movement::program {
 struct TilizeSingleCoreProgramFactory {
     struct shared_variables_t {
-        tt::tt_metal::KernelHandle unary_reader_kernel_id;
-        tt::tt_metal::KernelHandle unary_writer_kernel_id;
+        tt::tt_metal::KernelHandle unary_reader_kernel_id{};
+        tt::tt_metal::KernelHandle unary_writer_kernel_id{};
     };
     using cached_program_t = ttnn::device_operation::CachedProgram<shared_variables_t>;
 
