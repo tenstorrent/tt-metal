9db39cb1f32dd9e72cbf6961aab6210b7009c29f
Jarek Bieniek <jbbieniekTT@tenstorrent.com>
2025-11-27 09:20:04 +0000

    scatter: initial work on bf16 reduce drift minimisation

--- ttnn/cpp/ttnn/operations/data_movement/scatter/device/scatter_program_factory.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/scatter/device/scatter_program_factory.hpp
@@ -18,6 +18,7 @@ enum class ScatterCB : std::underlying_type_t<tt::CBIndex> {
     INPUT = CBIndex::c_0,
     SRC = CBIndex::c_1,
     INDEX = CBIndex::c_2,
+
     DST = CBIndex::c_3
 };
 
@@ -37,20 +38,21 @@ struct ScatterProgramFactory {
 
     static void override_runtime_arguments(
         cached_program_t&, const operation_attributes_t&, const tensor_args_t&, tensor_return_value_t&);
+};
 
-    static CBHandle create_cb(
-        Program& program,
-        const DataType& dtype,
-        const ScatterCB& scatter_cb,
-        const CoreRangeSet& core_range_set,
-        const uint32_t& tiles_num);
+struct ScatterReduceBfloat16ProgramFactory {
+    struct shared_variables_t {
+        KernelHandle reader_kernel_id{};
+        KernelHandle writer_kernel_id{};
+        std::vector<CoreCoord> cores;
+    };
 
-    static KernelHandle create_kernel(
-        Program& program,
-        const char* kernel_path,
-        const CoreRangeSet& core_range_set,
-        const std::variant<DataMovementConfig, ComputeConfig, EthernetConfig>& config,
-        const std::vector<uint32_t>& runtime_args = {});
+    using cached_program_t = ttnn::device_operation::CachedProgram<shared_variables_t>;
+
+    static cached_program_t create(const operation_attributes_t&, const tensor_args_t&, tensor_return_value_t&);
+
+    static void override_runtime_arguments(
+        cached_program_t&, const operation_attributes_t&, const tensor_args_t&, tensor_return_value_t&);
 };
 
 }  // namespace ttnn::operations::data_movement::scatter
