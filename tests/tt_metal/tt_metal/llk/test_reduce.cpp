b5475189cca88972c49b31d7c821b7d8012477f9
riverwuTT <riverwu@tenstorrent.com>
2025-11-27 03:45:07 +0000

    trim off more includes update

--- tests/tt_metal/tt_metal/llk/test_reduce.cpp
+++ tests/tt_metal/tt_metal/llk/test_reduce.cpp
@@ -29,7 +29,6 @@
 #include <tt-metalium/core_coord.hpp>
 #include <tt-metalium/data_types.hpp>
 #include <tt-metalium/tensor_accessor_args.hpp>
-#include <tt-metalium/tile.hpp>
 #include "device_fixture.hpp"
 #include <tt-metalium/distributed.hpp>
 #include "hostdevcommon/kernel_structs.h"
