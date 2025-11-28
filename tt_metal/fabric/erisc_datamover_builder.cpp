bcc41324b7a62195560e3da30838d97d1c4ecbaa
Sean Nijjar <sean.nijjar@gmail.com>
2025-11-28 21:09:29 +0000

    fixes to disable dcache for BH fabric (5-10% performance improvement)

--- tt_metal/fabric/erisc_datamover_builder.cpp
+++ tt_metal/fabric/erisc_datamover_builder.cpp
@@ -891,6 +891,7 @@ std::vector<uint32_t> FabricEriscDatamoverBuilder::get_compile_time_args(uint32_
     TT_FATAL(receiver_channel_num_buffers > 0, "Receiver channel num buffers must be greater than 0");
 
     const auto& stream_ids = StreamRegAssignments::get_all_stream_ids();
+    static constexpr bool enable_risc_cpu_data_cache = false;
     auto ct_args = std::vector<uint32_t>(stream_ids.begin(), stream_ids.end());
     ct_args.push_back(0xFFEE0001);
 
@@ -925,7 +926,8 @@ std::vector<uint32_t> FabricEriscDatamoverBuilder::get_compile_time_args(uint32_
         this->handshake_address,
         this->channel_buffer_size,
         vc1_has_different_downstream_dest,
-        this->has_tensix_extension};
+        this->has_tensix_extension,
+        enable_risc_cpu_data_cache};
 
     const std::vector<uint32_t> main_args_part2 = {
         config.skip_receiver_channel_1_connection,
