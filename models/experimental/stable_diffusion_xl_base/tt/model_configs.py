0607228ad68141b702f9722faad7fc74539ee989
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2025-11-28 10:49:47 +0000

    model config

--- models/experimental/stable_diffusion_xl_base/tt/model_configs.py
+++ models/experimental/stable_diffusion_xl_base/tt/model_configs.py
@@ -110,6 +110,16 @@ class ModelOptimisations:
         )
 
         # BLOCK SHARDED
+        override_output_sharding_config = True
+        override_output_core_grid = ttnn.CoreRangeSet(
+            {
+                ttnn.CoreRange(
+                    ttnn.CoreCoord(0, 0),
+                    ttnn.CoreCoord(4, 7),
+                ),
+            }
+        )
+
         self.conv_configs["ABH_0_ADB_WDB_BS"] = ttnn.Conv2dConfig(
             weights_dtype=self.conv_ws_dtype,
             shard_layout=ttnn.TensorMemoryLayout.BLOCK_SHARDED,
@@ -120,6 +130,8 @@ class ModelOptimisations:
             reshard_if_not_optimal=True,
             act_block_w_div=1,
             act_block_h_override=0,
+            override_output_sharding_config=override_output_sharding_config,
+            core_grid=override_output_core_grid,
         )
 
         self.conv_configs["ABH_0_ADB_WDB_NO_DEALLOC_BS"] = ttnn.Conv2dConfig(
@@ -132,6 +144,8 @@ class ModelOptimisations:
             reshard_if_not_optimal=True,
             act_block_w_div=1,
             act_block_h_override=0,
+            override_output_sharding_config=override_output_sharding_config,
+            core_grid=override_output_core_grid,
         )
         self.conv_configs["ABH_64_ADB_WDB_BS"] = ttnn.Conv2dConfig(
             weights_dtype=self.conv_ws_dtype,
@@ -143,6 +157,8 @@ class ModelOptimisations:
             reshard_if_not_optimal=True,
             act_block_w_div=1,
             act_block_h_override=64,
+            override_output_sharding_config=override_output_sharding_config,
+            core_grid=override_output_core_grid,
         )
         self.conv_configs["ABH_128_ADB_WDB_BS"] = ttnn.Conv2dConfig(
             weights_dtype=self.conv_ws_dtype,
@@ -154,6 +170,8 @@ class ModelOptimisations:
             reshard_if_not_optimal=True,
             act_block_w_div=1,
             act_block_h_override=128,
+            override_output_sharding_config=override_output_sharding_config,
+            core_grid=override_output_core_grid,
         )
         self.conv_configs["ABH_128_ADB_WDB_MOVE_BS"] = ttnn.Conv2dConfig(
             weights_dtype=self.conv_ws_dtype,
@@ -165,6 +183,8 @@ class ModelOptimisations:
             reshard_if_not_optimal=True,
             act_block_w_div=1,
             act_block_h_override=128,
+            override_output_sharding_config=override_output_sharding_config,
+            core_grid=override_output_core_grid,
         )
         self.conv_configs["ABH_256_NO_ADB_BS"] = ttnn.Conv2dConfig(
             weights_dtype=self.conv_w_dtype,
@@ -199,6 +219,8 @@ class ModelOptimisations:
             reshard_if_not_optimal=True,
             act_block_w_div=1,
             act_block_h_override=256,
+            override_output_sharding_config=override_output_sharding_config,
+            core_grid=override_output_core_grid,
         )
 
         self.conv_configs["ABH_512_NO_ADB_BS"] = ttnn.Conv2dConfig(
@@ -235,6 +257,8 @@ class ModelOptimisations:
             reshard_if_not_optimal=True,
             act_block_w_div=1,
             act_block_h_override=512,
+            override_output_sharding_config=override_output_sharding_config,
+            core_grid=override_output_core_grid,
         )
 
         self.conv_configs["ABH_1024_NO_ADB_BS"] = ttnn.Conv2dConfig(
@@ -259,6 +283,8 @@ class ModelOptimisations:
             reshard_if_not_optimal=True,
             act_block_w_div=1,
             act_block_h_override=1024,
+            override_output_sharding_config=override_output_sharding_config,
+            core_grid=override_output_core_grid,
         )
 
         # DEFAULT CONF
@@ -1004,9 +1030,9 @@ class ModelOptimisations:
 
             # UP BLOCK 0
             elif ("up_blocks.0.resnets.0.conv1" == conv_path) or ("up_blocks.0.resnets.1.conv1" == conv_path):
-                return self.conv_configs["ABH_0_ADB_WDB_BS"]
+                return self.conv_configs["ABH_64_ADB_WDB_BS"]
             elif "up_blocks.0.upsamplers.0" == conv_path:
-                return self.conv_configs["ABH_256_ADB_WDB_BS"]
+                return self.conv_configs["ABH_128_ADB_WDB_BS"]
             elif ("up_blocks.0.resnets" in conv_path) and ("conv2" in conv_path):
                 return self.conv_configs["ABH_0_ADB_WDB_BS"]
             elif "up_blocks.0.resnets.2.conv1" == conv_path:
@@ -1022,7 +1048,7 @@ class ModelOptimisations:
             elif ("up_blocks.1.resnets" in conv_path) and ("conv2" in conv_path):
                 return self.conv_configs["ABH_0_ADB_WDB_BS"]
             elif "up_blocks.1.upsamplers.0" == conv_path:
-                return self.conv_configs["ABH_256_ADB_WDB_BS"]
+                return self.conv_configs["ABH_128_ADB_WDB_BS"]
 
             # UP BLOCK 2
             elif "up_blocks.2.resnets.0.conv1" == conv_path:
