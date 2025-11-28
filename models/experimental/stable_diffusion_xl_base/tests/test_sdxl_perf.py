898b343b956c6a616da168dd8af4b062c8a0c10a
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2025-11-28 13:49:26 +0000

    perf

--- models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py
+++ models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py
@@ -52,7 +52,7 @@ def test_unet(
 
 @pytest.mark.models_device_performance_bare_metal
 def test_sdxl_unet_perf_device():
-    expected_device_perf_cycles_per_iteration = 158_803_279
+    expected_device_perf_cycles_per_iteration = 185_870_812
 
     command = f"pytest models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py::test_unet"
     cols = ["DEVICE FW", "DEVICE KERNEL", "DEVICE BRISC KERNEL"]
@@ -117,7 +117,7 @@ def test_refiner_unet(
 
 @pytest.mark.models_device_performance_bare_metal
 def test_sdxl_refiner_unet_perf_device():
-    expected_device_perf_cycles_per_iteration = 523_063_550
+    expected_device_perf_cycles_per_iteration = 553_188_997
 
     command = f"pytest models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py::test_refiner_unet"
     cols = ["DEVICE FW", "DEVICE KERNEL", "DEVICE BRISC KERNEL"]
