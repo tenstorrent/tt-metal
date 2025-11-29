e649021e8af532f3599660e8b37d80b8bbe13eec
Vladyslav Tsilytskyi <vtsilytskyi@tenstorrent.com>
2025-11-29 22:19:59 +0100

    Migrate op to new infra: fill_rm (#33414)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/32744
    
    ### Problem description
    First, it is confusing to have 2 infrastructures to write operations.
    Second, it is harder to do mass refactoring/development with AI because
    we follow 2 structures.
    
    ### What's changed
    Migrate fill_rm op to the new infrastructure.
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-quick.yaml)
    CI passes (if applicable)
    - [ ] [Galaxy demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes

---
+++ ttnn/cpp/ttnn/operations/data_movement/fill_rm/device/fill_rm_device_operation.hpp
@@ -0,0 +1,57 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "ttnn/tensor/tensor.hpp"
+#include "fill_rm_program_factory.hpp"
+
+#include "ttnn/device_operation.hpp"
+#include "ttnn/decorators.hpp"
+
+#include "fill_rm_device_operation_types.hpp"
+
+namespace ttnn::operations::data_movement::fill_rm {
+
+struct FillRMDeviceOperation {
+    using operation_attributes_t = fill_rm::operation_attributes_t;
+    using tensor_args_t = fill_rm::tensor_args_t;
+    using spec_return_value_t = fill_rm::spec_return_value_t;
+    using tensor_return_value_t = fill_rm::tensor_return_value_t;
+    using program_factory_t = std::variant<program::FillRMProgramFactory>;
+
+    static program_factory_t select_program_factory(const operation_attributes_t&, const tensor_args_t&);
+
+    static void validate_on_program_cache_hit(const operation_attributes_t&, const tensor_args_t&);
+    static void validate_on_program_cache_miss(const operation_attributes_t&, const tensor_args_t&);
+
+    static spec_return_value_t compute_output_specs(const operation_attributes_t&, const tensor_args_t&);
+
+    static tensor_return_value_t create_output_tensors(
+        const operation_attributes_t& operation_attributes, const tensor_args_t&);
+
+    static tt::tt_metal::operation::OpPerformanceModelGeneral<tensor_return_value_t> create_op_performance_model(
+        const operation_attributes_t& operation_attributes,
+        const tensor_args_t& tensor_args,
+        const tensor_return_value_t& tensor_return_value);
+
+    static std::tuple<operation_attributes_t, tensor_args_t> invoke(
+        uint32_t N,
+        uint32_t C,
+        uint32_t H,
+        uint32_t W,
+        uint32_t hFill,
+        uint32_t wFill,
+        const Tensor& input,
+        float val_hi,
+        float val_lo,
+        const MemoryConfig& output_memory_config);
+};
+
+}  // namespace ttnn::operations::data_movement::fill_rm
+
+namespace ttnn::prim {
+constexpr auto fill_rm =
+    ttnn::register_operation<"ttnn::prim::fill_rm", ttnn::operations::data_movement::fill_rm::FillRMDeviceOperation>();
+}  // namespace ttnn::prim
