4d46e94c7e6de1f2797773e23ba6d43e23a1dce0
CodeMan62 <sharmahimanshu15082007@gmail.com>
2025-11-29 09:54:54 +0530

    remove remaining bools

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_unary_max_min.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_unary_max_min.h
@@ -62,7 +62,7 @@ sfpi_inline void calculate_unary_max_min_int32_body() {
     }
 }
 
-template <bool IS_MAX_OP = true, bool APPROXIMATION_MODE, int ITERATIONS = 8>
+template <bool IS_MAX_OP = true, ApproximationMode APPROX_MODE, int ITERATIONS = 8>
 inline void calculate_unary_max_min(uint value) {
     // Load value param to lreg2
     load_value_param_float(value);
