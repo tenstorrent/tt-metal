78d9af6f6fcc7433498bed6158cdb0fd14ac6be3
Billy Ransom <wransom@tenstorrent.com>
2025-11-29 21:57:18 -0800

    Pool Test Cleanup (#33118)
    ### Ticket
    N/A
    
    ### What's changed
    - Removed PCC checks
    - Removed max pool common functions, routing all test through nightly
    - Ensure no tests are skipped for unit or sweeps unless explicit
    - Add avg pool sweep to sweep framework directory
    - Ensure all tests use tensor map
    - Fix to use non unity stride in the ceil mode massive kernel unit tests
    - Fixes to use tuples where appropriate for skip logic
    - Use auto sharding for max pool sweeps
    - Add Bfloat8 coverage to avg pool sweeps
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/19689131505
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/19689133427
    - [x] [Nightly
    L2](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/19717387804
    - [x] [Nightly
    Blackhole](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-nightly-tests.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/19689139327
    - [x] [Frequent
    model](https://github.com/tenstorrent/tt-metal/actions/workflows/fast-dispatch-full-regressions-and-models.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/19689144355
    - [x] [TTNN
    Sweeps](https://github.com/tenstorrent/tt-metal/actions/workflows/ttnn-run-sweeps.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/19689186682
    - [x] New/Existing tests provide coverage for changes

--- tests/sweep_framework/sweeps/pool2d/short/max_pool2d_short_sweep.py
+++ tests/sweep_framework/sweeps/pool2d/short/max_pool2d_short_sweep.py
@@ -2,17 +2,17 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-from typing import Optional, Tuple, List
-import itertools
-import random
-import torch
-import math
-
 import ttnn
+import pytest
+from tests.ttnn.nightly.unit_tests.operations.pool.test_maxpool2d import run_max_pool2d
 
-from tests.ttnn.utils_for_testing import check_with_pcc, start_measuring_time, stop_measuring_time
-from models.common.utility_functions import torch_random
-from tests.sweep_framework.sweep_utils.max_pool2d_common import run_max_pool2d, mesh_device_fixture
+
+# Cache map used for torch tensor reuse - the tensor will not be generated if a tensor of the same dimensions has already been generated
+@pytest.fixture(scope="module")
+def tensor_map(request):
+    tensor_map = {}
+
+    return tensor_map
 
 
 parameters = {
@@ -60,89 +60,36 @@ parameters = {
 }
 
 
-def invalidate_vector(test_vector) -> Tuple[bool, Optional[str]]:
-    return False, None
-
-
-def run(
-    input_specs,
-    dtype,
-    *,
-    device,
-):
-    (
-        in_n,
-        in_c,
-        in_h,
-        in_w,
-        kernel_h,
-        kernel_w,
-        stride_h,
-        stride_w,
-        pad_h,
-        pad_w,
-        dilation_h,
-        dilation_w,
-        ceil_mode,
-    ) = input_specs
-    sharding = ttnn.TensorMemoryLayout.HEIGHT_SHARDED
-    return run_max_pool2d(
-        in_n,
-        in_c,
-        in_h,
-        in_w,
-        kernel_h,
-        kernel_w,
-        stride_h,
-        stride_w,
-        pad_h,
-        pad_w,
-        dilation_h,
-        dilation_w,
-        dtype,
-        device,
-        sharding,
-        ceil_mode,
-    )
-
-
-import pytest
-
-
 @pytest.mark.parametrize("input_spec", parameters["max_pool2d_short_sweep_suite"]["input_specs"])
 @pytest.mark.parametrize("dtype", parameters["max_pool2d_short_sweep_suite"]["dtype"])
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 16384}], indirect=True)
-def test_max_pool2d_localrun(device, dtype, input_spec):
+def test_ttnn_pytorch_sweep(device, dtype, input_spec, tensor_map):
     (
-        batch_size,
-        input_channels,
-        input_height,
-        input_width,
-        kernel_height,
-        kernel_width,
+        in_n,
+        in_c,
+        in_h,
+        in_w,
+        kernel_h,
+        kernel_w,
         stride_h,
-        strid_w,
+        stride_w,
         pad_h,
         pad_w,
         dilation_h,
         dilation_w,
         ceil_mode,
     ) = input_spec
+
     run_max_pool2d(
-        batch_size,
-        input_channels,
-        input_height,
-        input_width,
-        kernel_height,
-        kernel_width,
-        stride_h,
-        strid_w,
-        pad_h,
-        pad_w,
-        dilation_h,
-        dilation_w,
-        dtype,
+        [in_n, in_c, in_h, in_w],
+        (kernel_h, kernel_w),
+        (pad_h, pad_w),
+        (stride_h, stride_w),
+        (dilation_h, dilation_w),
         device,
-        sharding=ttnn.TensorMemoryLayout.HEIGHT_SHARDED,
+        tensor_map,
+        dtype,
+        shard_scheme=None,
         ceil_mode=ceil_mode,
+        nightly_skips=False,
     )
--- tests/ttnn/nightly/unit_tests/operations/pool/test_max_pool2d_sweeps.py
+++ tests/ttnn/nightly/unit_tests/operations/pool/test_max_pool2d_sweeps.py
@@ -2,19 +2,26 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-from tests.sweep_framework.sweep_utils.max_pool2d_common import run_max_pool2d
-from tests.sweep_framework.sweeps.max_pool2d.short.max_pool2d_short_sweep import parameters as parameters_ttnn_pytorch
-from models.common.utility_functions import is_wormhole_b0
+from tests.ttnn.nightly.unit_tests.operations.pool.test_maxpool2d import run_max_pool2d
+from tests.sweep_framework.sweeps.pool2d.short.max_pool2d_short_sweep import parameters
 
 
 import pytest
 import ttnn
 
 
-@pytest.mark.parametrize("input_spec", parameters_ttnn_pytorch["max_pool2d_short_sweep_suite"]["input_specs"])
-@pytest.mark.parametrize("dtype", [ttnn.bfloat16, ttnn.bfloat8_b])
+# Cache map used for torch tensor reuse - the tensor will not be generated if a tensor of the same dimensions has already been generated
+@pytest.fixture(scope="module")
+def tensor_map(request):
+    tensor_map = {}
+
+    return tensor_map
+
+
+@pytest.mark.parametrize("input_spec", parameters["max_pool2d_short_sweep_suite"]["input_specs"])
+@pytest.mark.parametrize("dtype", parameters["max_pool2d_short_sweep_suite"]["dtype"])
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 16384}], indirect=True)
-def test_ttnn_pytorch_sweep(device, dtype, input_spec):
+def test_ttnn_pytorch_sweep(device, dtype, input_spec, tensor_map):
     (
         in_n,
         in_c,
@@ -32,20 +39,15 @@ def test_ttnn_pytorch_sweep(device, dtype, input_spec):
     ) = input_spec
 
     run_max_pool2d(
-        in_n,
-        in_c,
-        in_h,
-        in_w,
-        kernel_h,
-        kernel_w,
-        stride_h,
-        stride_w,
-        pad_h,
-        pad_w,
-        dilation_h,
-        dilation_w,
-        dtype,
+        [in_n, in_c, in_h, in_w],
+        (kernel_h, kernel_w),
+        (pad_h, pad_w),
+        (stride_h, stride_w),
+        (dilation_h, dilation_w),
         device,
-        ttnn.TensorMemoryLayout.HEIGHT_SHARDED,
-        ceil_mode,
+        tensor_map,
+        dtype,
+        shard_scheme=None,
+        ceil_mode=ceil_mode,
+        nightly_skips=False,
     )
