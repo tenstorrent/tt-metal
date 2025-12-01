daec436e3be3498342909a624e7019e6d85b788a
Nathan Maurice <nmaurice@tenstorrent.com>
2025-12-01 14:49:43 +0000

    Clean-up and comment erfinv code

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_erfinv.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_erfinv.h
@@ -23,9 +23,9 @@ sfpi_inline sfpi::vFloat calculate_sqrt_custom(sfpi::vFloat in) {
         // See: https://en.wikipedia.org/wiki/Fast_inverse_square_root
         sfpi::vUInt magic = sfpi::reinterpret<sfpi::vUInt>(sfpi::vFloat(sfpi::s2vFloat16b(0x5f37)));
         sfpi::vFloat approx = sfpi::reinterpret<sfpi::vFloat>(magic - (sfpi::reinterpret<sfpi::vUInt>(val) >> 1));
-        for (int r = 0; r < 2; r++) {
-            approx = ((approx * approx) * (val * -0.5f) + 1.5f) * approx;
-        }
+        sfpi::vFloat neg_half_val = val * -0.5f;
+        approx = ((approx * approx) * neg_half_val + 1.5f) * approx;
+        approx = ((approx * approx) * neg_half_val + 1.5f) * approx;
         out = approx * val;
     }
     v_else { out = val; }
@@ -35,17 +35,36 @@ sfpi_inline sfpi::vFloat calculate_sqrt_custom(sfpi::vFloat in) {
 
 template <bool APPROXIMATION_MODE>
 sfpi_inline sfpi::vFloat calculate_erfinv_body(sfpi::vFloat in) {
+    // Algorithm based on "A handy approximation for the error function and its inverse" by Sergei Winitzki (2008)
+    // This approximation defines erfinv(x) as:
+    // erfinv(x) = sqrt( - 2/(pi*a) - log(1 - x^2)/2 + sqrt( ( 2/(pi*a) + log(1 - x^2)) ^2 - 1/a log(1 - x^2)) )
+    // Where a is a polynomial coefficient used in the approximation of the error function (and re-used in inverse error
+    // function)
+
+    // Compute log(1 - x^2)
     sfpi::vFloat log_value = in * in;
     log_value = 1 - log_value;
     log_value = calculate_log_body<false, false, true>(log_value, 0);  // use fp32 to avoid intermediate rounding
+
     sfpi::vFloat temp = log_value * 0.5;
-    temp = 4.5469 + temp;
+
+    // Paper sets a constant a = 0.147.
+    // This constant is used to compute two constant expressions:
+    constexpr float TwoPiA = 4.330746750799873f;   // 2 / (pi * a)
+    constexpr float OneDivA = 6.802721088435375f;  // 1/a
+
+    // tmp = -2 / (pi * a) - log(1 - x^2)/2
+    temp = TwoPiA + temp;
     temp = -temp;
 
-    sfpi::vFloat calculated_value = (temp * temp) - (log_value * 7.1427);
+    // calculated_value = temp + sqrt( temp^2 - log_value / a)
+    sfpi::vFloat calculated_value = (temp * temp) - (log_value * OneDivA);
     sfpi::vFloat intermediate_result = calculate_sqrt_custom<false>(calculated_value);
     calculated_value = temp + intermediate_result;
-    log_value = calculate_sqrt_custom<false>(calculated_value);
+
+    // result = sqrt(calculated_value)
+    sfpi::vFloat result = calculate_sqrt_custom<false>(calculated_value);
+
     return log_value;
 }
 
@@ -56,17 +75,19 @@ inline void calculate_erfinv() {
     for (int d = 0; d < ITERATIONS; d++) {
         sfpi::vFloat v = sfpi::dst_reg[0];
         sfpi::vFloat result;
-        v_if(v == 1.0f) { result = std::numeric_limits<float>::infinity(); }
-        v_elseif(v == -1.0f) { result = -std::numeric_limits<float>::infinity(); }
-        v_elseif((v < -1.0f) || (v > 1.0f)) {  // Nan not supported
+
+        // Since erfinv(-x) = -erfinv(x), we can compute the result for the absolute value of the input.
+        // This reduces the number of edge cases.
+        sfpi::vFloat abs_v = sfpi::abs(v);
+
+        v_if(abs_v == 1.0f) { result = std::numeric_limits<float>::infinity(); }
+        v_elseif(abs_v > 1.0f) {  // Nan not supported
             result = std::numeric_limits<float>::quiet_NaN();
         }
-        v_elseif(v < 0.0f) {
-            result = calculate_erfinv_body<true>(v);
-            result = -result;
-        }
-        v_else { result = calculate_erfinv_body<true>(v); }
+        v_else { result = calculate_erfinv_body<true>(abs_v); }
         v_endif;
+
+        result = sfpi::setsgn(result, v);  // restore sign
 
         sfpi::dst_reg[0] = result;
         sfpi::dst_reg++;
