18e6d3bd0fbd444a685aae5f3e4bc5945f3a61fd
Adam Thompson <athompson@tenstorrent.com>
2025-12-01 18:27:09 +0000

    use jthread in legacy parallel, use subspan

--- tt-train/sources/ttml/core/random.hpp
+++ tt-train/sources/ttml/core/random.hpp
@@ -36,7 +36,7 @@ void parallel_generate(
     }
 
     size_t num_threads = std::min(max_threads, std::thread::hardware_concurrency());
-    std::vector<std::thread> threads;
+    std::vector<std::jthread> threads;
     threads.reserve(num_threads);
 
     size_t chunk_size = seq.size() / num_threads;
@@ -46,14 +46,9 @@ void parallel_generate(
     for (size_t i = 0; i < num_threads; ++i) {
         auto adjusted_chunk_size = chunk_size + (i == num_threads - 1 ? remainder : 0);
         threads.emplace_back([&dist_factory, &seq, offset, adjusted_chunk_size, seed, i]() {
-            std::span<T> chunk{seq.data() + offset, adjusted_chunk_size};
-            sequential_generate(chunk, dist_factory, seed + i);
+            sequential_generate(seq.subspan(offset, adjusted_chunk_size), dist_factory, seed + i);
         });
         offset += adjusted_chunk_size;
-    }
-
-    for (auto& thread : threads) {
-        thread.join();
     }
 }
 
