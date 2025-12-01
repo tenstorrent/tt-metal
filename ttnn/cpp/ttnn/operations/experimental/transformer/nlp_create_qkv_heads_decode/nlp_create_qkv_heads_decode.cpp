551cc171d0eeb1d36a9bc82702b0d180952db330
Siddharth Sundaram <ssundaram@tenstorrent.com>
2025-12-01 18:16:23 +0000

    migrate to new infra

--- ttnn/cpp/ttnn/operations/experimental/transformer/nlp_create_qkv_heads_decode/nlp_create_qkv_heads_decode.cpp
+++ ttnn/cpp/ttnn/operations/experimental/transformer/nlp_create_qkv_heads_decode/nlp_create_qkv_heads_decode.cpp
@@ -6,7 +6,6 @@
 
 #include <utility>
 #include "device/nlp_create_qkv_heads_decode_device_operation.hpp"
-#include "ttnn/run_operation.hpp"
 #include "ttnn/operations/core/core.hpp"
 
 namespace ttnn::operations::experimental::transformer {
@@ -52,24 +51,17 @@ std::tuple<ttnn::Tensor, ttnn::Tensor, ttnn::Tensor> NLPCreateHeadsDecodeOperati
         input_tensor.padded_shape()[3],
         num_heads + 2 * num_kv_heads_val);
     uint32_t head_dim = input_tensor.padded_shape()[3] / (num_heads + 2 * num_kv_heads_val);
-    auto optional_outputs = std::vector<std::optional<Tensor>>{};
-    if (optional_output_tensors.has_value()) {
-        optional_outputs = {optional_output_tensors.value().begin(), optional_output_tensors.value().end()};
-    } else {
-        optional_outputs = {};
-    }
-    auto out = tt::tt_metal::operation::run(
-        NLPCreateHeadsDecodeDeviceOperation{
-            num_heads,
-            num_kv_heads_val,
-            head_dim,
-            overlap_qk_coregrid_val,
-            input_on_subcoregrids,
-            slice_size,
-            output_mem_config},
-        {input_tensor},
-        {batch_offset},
-        optional_outputs);
+
+    auto out = ttnn::prim::nlp_create_qkv_heads_decode(
+        input_tensor,
+        num_heads,
+        num_kv_heads_val,
+        head_dim,
+        overlap_qk_coregrid_val,
+        input_on_subcoregrids,
+        batch_offset,
+        slice_size,
+        output_mem_config);
     return {out.at(0), out.at(1), out.at(2)};
 }
 
