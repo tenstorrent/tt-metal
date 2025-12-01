30841d0d69fdfc2a645d956a67b87cec18e46672
“Bryan <bzimmerman@tenstorrent.com>
2025-12-01 14:16:45 +0000

    Remove equeue_read_buffer

--- tt_metal/impl/dispatch/hardware_command_queue.hpp
+++ tt_metal/impl/dispatch/hardware_command_queue.hpp
@@ -79,12 +79,6 @@ public:
 
     // This function is temporarily needed since MeshCommandQueue relies on the CommandQueue object
     WorkerConfigBufferMgr& get_config_buffer_mgr(uint32_t index) override;
-    void enqueue_read_buffer(
-        const std::variant<std::reference_wrapper<Buffer>, std::shared_ptr<Buffer>>& buffer,
-        void* dst,
-        const BufferRegion& region,
-        bool blocking,
-        tt::stl::Span<const SubDeviceId> sub_device_ids = {}) override;
 
     void enqueue_record_event(
         const std::shared_ptr<Event>& event, tt::stl::Span<const SubDeviceId> sub_device_ids = {}) override;
