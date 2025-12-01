4276506044d67d9af03a3b6b2ec90d9bfda74284
Siddharth Sundaram <ssundaram@tenstorrent.com>
2025-12-01 23:03:08 +0000

    default enum types

--- ttnn/cpp/ttnn/operations/normalization/layernorm_distributed/device/layernorm_pre_all_gather_device_operation_types.hpp
+++ ttnn/cpp/ttnn/operations/normalization/layernorm_distributed/device/layernorm_pre_all_gather_device_operation_types.hpp
@@ -11,8 +11,8 @@
 namespace ttnn::operations::normalization {
 
 struct operation_attributes_t {
-    LayerNormDistributedType norm_type;
-    tt::tt_metal::DataType dtype;
+    LayerNormDistributedType norm_type = LayerNormDistributedType::LAYERNORM;
+    tt::tt_metal::DataType dtype = tt::tt_metal::DataType::INVALID;
     DeviceComputeKernelConfig compute_kernel_config;
     std::optional<bool> use_2d_core_grid;
     LayerNormDistributedDefaultProgramConfig program_config;
