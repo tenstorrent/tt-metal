934c1c3a163081d2311aa9c9d101e1d10abeb324
Ryan Miller <rmiller@tenstorrent.com>
2025-12-02 16:22:20 +0000

    Clean up docs

--- ttnn/cpp/ttnn/operations/normalization/kernel_util/compute/numeric.h
+++ ttnn/cpp/ttnn/operations/normalization/kernel_util/compute/numeric.h
@@ -74,8 +74,10 @@ inline void accumulate_compute_loop(
         }
     };
 
+    // Accumulate the input CB
     accumulate_cb(cb_in);
 
+    // Accumulate any additional CBs
     constexpr uint32_t num_additional_cbs = sizeof...(cb_additional);
     if constexpr (num_additional_cbs > 0) {
         const std::array<uint32_t, num_additional_cbs> additional_cbs_array = {cb_additional...};
@@ -91,20 +93,37 @@ inline void accumulate_compute_loop(
 }  // namespace detail
 
 /**
- * @brief Accumulate (sum) the tiles in a CB and push the result
- * to an output CB
+ * @brief Accumulate (sum) along the rows of tiles in a CB, apply
+ * an optional compute epilogue, and push the result to an output CB
  *
- * @param cb_in The input CB to accumulate
- * @param cb_scalar The CB containing the scalar tile to use in reduce
- * @param cb_out The output CB to store the accumulated value
- * @param num_tiles The number of tiles in the input CB
- * @param block_size The number of tiles to wait for at a time
+ * @param cb_in Input CB to accumulate
+ * @param cb_scalar CB containing the scalar tile to use in reduce
+ * @param cb_out Output CB to store the accumulated value
+ * @param num_tiles Number of tiles containing the data
+ * @param block_size Number of tiles to process at a time
  * @param epilogue Optional functor to call after the accumulation before tile registers
  * are committed and packed
  * @param additional_cbs Optional additional input CBs to accumulate
  * @tparam FLOAT32_REDUCTION Whether to reduce the sum in FP32 precision
  * @tparam pop_input_policy The policy for whether to pop the input CB after processing
  * @tparam wait_at_end_policy The policy for whether to wait at the end of the function
+ * @tparam Epilogue The type of the epilogue functor
+ * @tparam AdditionalCBs The types of the additional input CBs (must be uint32_t)
+ *
+ * @note dst0 is used to accumuate the sum, so it
+ * will be overwritten here @anchor dst0_overwritten
+ * @note It is up to the caller to ensure that the scalar tile
+ * is correctly populated. If it doesn't contain 1's, the result
+ * will be incorrect @anchor scalar_tile_ones
+ * @note This function must be called from a compilation unit
+ * that has the following preprocessor directives (this is for
+ * the reduce interface):
+ * #define REDUCE_OP PoolType::SUM
+ * #define REDUCE_DIM ReduceDim::REDUCE_ROW @anchor reduce_defines
+ * @note All input CBs will wait on the same number of tiles in a block,
+ * and will have the same pop policy
+ * @note This first streams all `num_tiles` tiles from `cb_in0` then
+ * streams all `num_tiles` tiles from `cb_in1` @anchor stream_cbs
  */
 template <
     bool FLOAT32_REDUCTION,
@@ -112,7 +131,7 @@ template <
     policies::WaitAtEndPolicy wait_at_end_policy = policies::WaitAtEndPolicy::WAIT,
     typename Epilogue = decltype(detail::no_op),
     typename... AdditionalCBs>
-inline void accumulate_cbs_with_epilogue(
+inline void row_wise_accumulate_with_epilogue(
     uint32_t cb_in,
     uint32_t cb_scalar,
     uint32_t cb_out,
@@ -148,28 +167,18 @@ inline void accumulate_cbs_with_epilogue(
 /**
  * @brief Compute the row-wise mean of an entire input CB
  *
- * @param cb_in The input CB to compute mean of
- * @param cb_scalar The CB containing a scalar reduce tile of 1's
- * @param cb_out The output CB to store the mean
- * @param one_over_N The inverse of the number of entries in the
+ * @param cb_in Input CB to compute mean of
+ * @param cb_scalar CB containing a scalar reduce tile of 1's
+ * @param cb_out Output CB to store the mean
+ * @param one_over_N Inverse of the number of entries in the
  * mean (1/N) encoded as a uint32_t
- * @param num_tiles The number of tiles in the mean
- * before processing the next block. Must be an even number
- * @param block_size The number of tiles to wait for at a time
+ * @param num_tiles Number of tiles containing the data
+ * @param block_size Number of tiles to process at a time
  * @tparam FLOAT32_REDUCTION Whether to reduce the sum in FP32 precision
  * @tparam pop_input_policy The policy for whether to pop the input CB after processing
  * @tparam wait_at_end_policy The policy for whether to wait at the end of the function
  *
- * @note dst0 is used to accumuate the sum, so it
- * will be overwritten here
- * @note It is up to the caller to ensure that the scalar tile
- * is correctly populated. If it doesn't contain 1's, the result
- * will be incorrect
- * @note This function must be called from a compilation unit
- * that has the following preprocessor directives (this is for
- * the reduce interface):
- * #define REDUCE_OP PoolType::SUM
- * #define REDUCE_DIM ReduceDim::REDUCE_ROW
+ * See \ref dst0_overwritten, \ref scalar_tile_ones, \ref reduce_defines, \ref stream_cbs
  */
 template <
     bool FLOAT32_REDUCTION,
@@ -177,7 +186,7 @@ template <
     policies::WaitAtEndPolicy wait_at_end_policy = policies::WaitAtEndPolicy::WAIT>
 inline void row_wise_mean(
     uint32_t cb_in, uint32_t cb_scalar, uint32_t cb_out, uint32_t one_over_N, uint32_t num_tiles, uint32_t block_size) {
-    accumulate_cbs_with_epilogue<FLOAT32_REDUCTION, pop_input_policy, wait_at_end_policy>(
+    row_wise_accumulate_with_epilogue<FLOAT32_REDUCTION, pop_input_policy, wait_at_end_policy>(
         cb_in, cb_scalar, cb_out, num_tiles, block_size, [&one_over_N]() {
             detail::scale_dest(detail::dst0, one_over_N);
         });
@@ -186,34 +195,19 @@ inline void row_wise_mean(
 /**
  * @brief Compute the row-wise mean of the sum of two input CBs
  *
- * @param cb_in0 The first input CB to compute mean of
- * @param cb_in1 The second input CB to compute mean of
- * @param cb_scalar The CB containing a scalar reduce tile of 1's
- * @param cb_sum The intermediate CB to store the sum of the two input CBs
- * @param cb_out The output CB to store the mean
- * @param one_over_N The inverse of the number of entries in the
+ * @param cb_in Input CB to compute mean of
+ * @param cb_in1 Additional input CB to accumulate
+ * @param cb_scalar CB containing a scalar reduce tile of 1's
+ * @param cb_out Output CB to store the mean
+ * @param one_over_N Inverse of the number of entries in the
  * mean (1/N) encoded as a uint32_t
- * @param num_tiles The number of tiles in the mean
- * before processing the next block. Must be an even number
- * @param block_size The number of tiles to wait for at a time
+ * @param num_tiles Number of tiles containing the data
+ * @param block_size Number of tiles to process at a time
  * @tparam FLOAT32_REDUCTION Whether to reduce the sum in FP32 precision
  * @tparam pop_input_policy The policy for whether to pop the input CB after processing
  * @tparam wait_at_end_policy The policy for whether to wait at the end of the function
  *
- * @note dst0 is used to accumuate the sum, so it
- * will be overwritten here
- * @note It is up to the caller to ensure that the scalar tile
- * is correctly populated. If it doesn't contain 1's, the result
- * will be incorrect
- * @note This function must be called from a compilation unit
- * that has the following preprocessor directives (this is for
- * the reduce interface):
- * #define REDUCE_OP PoolType::SUM
- * #define REDUCE_DIM ReduceDim::REDUCE_ROW
- * @note Both input CBs will wait on the same number of tiles in a block,
- * and will have the same pop policy
- * @note This first streams all `num_tiles` tiles from `cb_in0` then
- * streams all `num_tiles` tiles from `cb_in1`
+ * See \ref dst0_overwritten, \ref scalar_tile_ones, \ref reduce_defines, \ref stream_cbs
  */
 template <
     bool FLOAT32_REDUCTION,
@@ -227,7 +221,7 @@ inline void row_wise_mean_with_pre_add(
     uint32_t one_over_N,
     uint32_t num_tiles,
     uint32_t block_size) {
-    accumulate_cbs_with_epilogue<FLOAT32_REDUCTION, pop_input_policy, wait_at_end_policy>(
+    row_wise_accumulate_with_epilogue<FLOAT32_REDUCTION, pop_input_policy, wait_at_end_policy>(
         cb_in0,
         cb_scalar,
         cb_out,
