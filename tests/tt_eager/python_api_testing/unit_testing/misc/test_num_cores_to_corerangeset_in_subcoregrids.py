3568f770b36eb9aaff3384b19f5d358f9f8a3978
Ambrose Ling <aling@tenstorrent.com>
2025-12-02 12:31:33 -0500

    fix sub core grid allocation logic

--- tests/tt_eager/python_api_testing/unit_testing/misc/test_num_cores_to_corerangeset_in_subcoregrids.py
+++ tests/tt_eager/python_api_testing/unit_testing/misc/test_num_cores_to_corerangeset_in_subcoregrids.py
@@ -27,7 +27,7 @@ import pytest
                 ]
             ),
         ),
-        # Test Case 2: Basic Column-wise processing
+        # # Test Case 2: Basic Column-wise processing
         (
             ttnn.CoreCoord(1, 0),
             32,
@@ -45,7 +45,7 @@ import pytest
                 ]
             ),
         ),
-        # Test Case 3: row-wise scenario with small target cores and start offset
+        # # Test Case 3: row-wise scenario with small target cores and start offset
         (
             ttnn.CoreCoord(3, 2),
             8,
@@ -64,7 +64,7 @@ import pytest
                 ]
             ),
         ),
-        # Test Case 4: col-wise scenario with small target cores and start offset
+        # # Test Case 4: col-wise scenario with small target cores and start offset
         (
             ttnn.CoreCoord(1, 8),
             8,
@@ -79,6 +79,44 @@ import pytest
                 [
                     ttnn.CoreRange(ttnn.CoreCoord(1, 8), ttnn.CoreCoord(1, 9)),
                     ttnn.CoreRange(ttnn.CoreCoord(2, 0), ttnn.CoreCoord(2, 5)),
+                ]
+            ),
+        ),
+        # Test Case 5: non-rectangular sub_core_grids row-wise
+        (
+            ttnn.CoreCoord(0, 0),
+            17,
+            ttnn.CoreRangeSet(
+                [
+                    ttnn.CoreRange(ttnn.CoreCoord(0, 0), ttnn.CoreCoord(7, 1)),
+                    ttnn.CoreRange(ttnn.CoreCoord(0, 2), ttnn.CoreCoord(0, 2)),
+                ]
+            ),
+            True,
+            ttnn.CoreRangeSet(
+                [
+                    ttnn.CoreRange(ttnn.CoreCoord(0, 0), ttnn.CoreCoord(7, 1)),
+                    ttnn.CoreRange(ttnn.CoreCoord(0, 2), ttnn.CoreCoord(0, 2)),
+                ]
+            ),
+        ),
+        # Test Case 6: non-rectangular sub_core_grids row-wise middle start core
+        (
+            ttnn.CoreCoord(3, 0),
+            5,
+            ttnn.CoreRangeSet(
+                [
+                    ttnn.CoreRange(ttnn.CoreCoord(0, 0), ttnn.CoreCoord(3, 0)),  # 4 cores
+                    ttnn.CoreRange(ttnn.CoreCoord(0, 1), ttnn.CoreCoord(2, 1)),  # +3 cores
+                    ttnn.CoreRange(ttnn.CoreCoord(0, 2), ttnn.CoreCoord(0, 2)),  # +1 core (needed for 5th)
+                ]
+            ),
+            True,
+            ttnn.CoreRangeSet(
+                [
+                    ttnn.CoreRange(ttnn.CoreCoord(3, 0), ttnn.CoreCoord(3, 0)),  # 1 core
+                    ttnn.CoreRange(ttnn.CoreCoord(0, 1), ttnn.CoreCoord(2, 1)),  # next 3 cores
+                    ttnn.CoreRange(ttnn.CoreCoord(0, 2), ttnn.CoreCoord(0, 2)),  # final core
                 ]
             ),
         ),
