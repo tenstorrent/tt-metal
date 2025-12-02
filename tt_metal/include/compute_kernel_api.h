b3932aed00b93c54bead051cace82975090f7485
ldjurovicTT <ldjurovic@tenstorrent.com>
2025-12-02 11:36:50 +0000

    API for 4x2 subblock that is in _custom LLKs

--- tt_metal/include/compute_kernel_api.h
+++ tt_metal/include/compute_kernel_api.h
@@ -43,6 +43,7 @@
 #include "llk_unpack_AB_matmul_api.h"
 #include "llk_unpack_A_api.h"
 #include "llk_unpack_AB_api.h"
+
 #include "llk_unpack_reduce_api.h"
 #include "llk_unpack_tilize_api.h"
 #include "llk_unpack_untilize_api.h"
