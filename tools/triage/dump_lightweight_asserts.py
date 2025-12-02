716399c3897f639b645b481d9474a0fcb88927d0
Vuk Jovanovic <vjovanovic@tenstorrent.com>
2025-12-02 20:46:59 +0000

    Printing asserts from ncrisc as well and simplifying table output

--- tools/triage/dump_lightweight_asserts.py
+++ tools/triage/dump_lightweight_asserts.py
@@ -12,8 +12,15 @@ Description:
 """
 
 
-from triage import ScriptConfig, log_check_risc, run_script
-from callstack_provider import run as get_callstack_provider, CallstackProvider, CallstacksData
+from dataclasses import dataclass
+from triage import ScriptConfig, log_check_risc, run_script, triage_field
+from callstack_provider import (
+    KernelCallstackWithMessage,
+    format_callstack_with_message,
+    run as get_callstack_provider,
+    CallstackProvider,
+    CallstacksData,
+)
 from run_checks import run as get_run_checks
 from ttexalens.coordinate import OnChipCoordinate
 from ttexalens.context import Context
@@ -26,11 +33,19 @@ script_config = ScriptConfig(
 )
 
 
+@dataclass
+class LightweightAssertInfo:
+    kernel_name: str | None = triage_field("Kernel Name")
+    kernel_callstack_with_message: KernelCallstackWithMessage = triage_field(
+        "Kernel Callstack", format_callstack_with_message
+    )
+
+
 def dump_callstacks(
     location: OnChipCoordinate,
     risc_name: str,
     callstack_provider: CallstackProvider,
-) -> CallstacksData | None:
+) -> LightweightAssertInfo | None:
     try:
         risc_debug = location._device.get_block(location).get_risc_debug(risc_name)
 
@@ -38,21 +53,43 @@ def dump_callstacks(
         if risc_debug.is_in_reset():
             return None
 
-        # We cannot read NCRISC private memory to verify ebreak hit
+        # Define two instructions
+        previous_instruction = None
+        current_instruction = None
+
+        # Check if PC is in code private memory (this only can be true for NCRISC on Wormhole)
         pc = risc_debug.get_pc()
-        if pc >= 0xFFB00000:
-            return None
-        if pc < 4:
-            return None
+        code_private_memory = risc_debug.get_code_private_memory()
+        if code_private_memory is not None and code_private_memory.contains_private_address(pc):
+            dispatcher_core_data = callstack_provider.dispatcher_data.get_core_data(location, risc_name)
+            elf = callstack_provider.elfs_cache[dispatcher_core_data.kernel_path].elf
+            text_section = elf.get_section_by_name(".text")
+            if text_section is None or dispatcher_core_data.kernel_offset is None:
+                return None
+            data: bytes = text_section.data()
+            address: int = dispatcher_core_data.kernel_offset
+            offset = pc - address
+            current_instruction = int.from_bytes(data[offset : offset + 4], "little")
+            if offset >= 4:
+                previous_instruction = int.from_bytes(data[offset - 4 : offset], "little")
+        else:
+            # Otherwise, read instructions directly from device
+            current_instruction = read_word_from_device(location, pc)
+            if pc >= 4:
+                previous_instruction = read_word_from_device(location, pc - 4)
 
         # Check if core hit ebreak
         rewind_pc_for_ebreak = False
-        if read_word_from_device(location, pc - 4) == 0x00100073:
+        if previous_instruction == 0x00100073:
             rewind_pc_for_ebreak = True
-        elif read_word_from_device(location, pc) != 0x00100073:
+        elif current_instruction != 0x00100073:
             return None
 
-        return callstack_provider.get_callstacks(location, risc_name, rewind_pc_for_ebreak)
+        callstack_data = callstack_provider.get_callstacks(location, risc_name, rewind_pc_for_ebreak)
+        return LightweightAssertInfo(
+            kernel_name=callstack_data.dispatcher_core_data.kernel_name,
+            kernel_callstack_with_message=callstack_data.kernel_callstack_with_message,
+        )
 
     except Exception as e:
         log_check_risc(
