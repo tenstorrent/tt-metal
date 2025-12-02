f1e290375801f57714d4b5905788bcbac5240f16
Neel Nyamagoudar <nnyamagoudar@tenstorrent.com>
2025-12-02 15:38:18 +0000

    Minor formatting modifications

--- dockerfile/upstream_test_images/run_upstream_tests_vanilla.sh
+++ dockerfile/upstream_test_images/run_upstream_tests_vanilla.sh
@@ -230,8 +230,8 @@ test_suite_bh_glx_metal_unit_tests() {
     RELIABILITY_MODE=relaxed ./build/test/tt_metal/tt_fabric/fabric_unit_tests --gtest_filter="*Fabric2D*.*"
     RELIABILITY_MODE=relaxed ./build/test/tt_metal/tt_fabric/fabric_unit_tests --gtest_filter="*Fabric1D*.*":-NightlyFabric1DFixture.TestEDMConnectionStressTestQuick
     RELIABILITY_MODE=relaxed TT_METAL_CLEAR_L1=1 build/test/tt_metal/perf_microbenchmark/routing/test_tt_fabric --test_config tests/tt_metal/tt_metal/perf_microbenchmark/routing/test_fabric_sanity_common.yaml
-    # Disable 2 ERISC mode for deadlock stability tests - These validate 2D Torus (QSFP Link) stability
-    RELIABILITY_MODE=relaxed TT_METAL_CLEAR_L1=1  build/test/tt_metal/perf_microbenchmark/routing/test_tt_fabric --test_config tests/tt_metal/tt_metal/perf_microbenchmark/routing/test_fabric_deadlock_stability_bh_6U_galaxy.yaml
+    # Deadlock stability tests - These validate 2D Torus (QSFP Link) stability
+    RELIABILITY_MODE=relaxed TT_METAL_CLEAR_L1=1 build/test/tt_metal/perf_microbenchmark/routing/test_tt_fabric --test_config tests/tt_metal/tt_metal/perf_microbenchmark/routing/test_fabric_deadlock_stability_bh_6U_galaxy.yaml
 
     # Dispatch
     build/test/tt_metal/unit_tests_eth --gtest_filter=UnitMeshCQMultiDeviceProgramFixture.ActiveEthKernelsSendInterleavedBufferAllConnectedChips
