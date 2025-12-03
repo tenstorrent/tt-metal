794d7727a123411fa05be077e3299931cd566dfe
Juan Camilo Vega <jvega@tenstorrent.com>
2025-12-03 14:50:34 +0000

    Enhancing test to look at PCC

--- tests/ttnn/unit_tests/base_functionality/test_to_layout.py
+++ tests/ttnn/unit_tests/base_functionality/test_to_layout.py
@@ -14,20 +14,39 @@ from models.common.utility_functions import is_grayskull, is_blackhole, torch_ra
 
 
 @pytest.mark.parametrize("mesh_device", [(2, 4)], ids=["t3k"], indirect=True)
-def test_failure(mesh_device):
-    torch_input_tensor = torch.zeros((1, 6240, 384), dtype=torch.bfloat16)
-    input_tensor = ttnn.from_torch(
-        torch_input_tensor,
-        layout=ttnn.Layout.ROW_MAJOR,
-        dtype=ttnn.bfloat16,
-        memory_config=ttnn.DRAM_MEMORY_CONFIG,
-        device=mesh_device,
-        mesh_mapper=ttnn.ShardTensor2dMesh(mesh_device, mesh_shape=tuple(mesh_device.shape), dims=[None, None, None]),
-    )
-    output_tensor = ttnn.to_layout(input_tensor, ttnn.TILE_LAYOUT)
-    torch_output_tensor = ttnn.to_torch(
-        output_tensor, dtype=torch.bfloat16, mesh_composer=ttnn.ConcatMeshToTensor(mesh_device, dim=0)
-    )
+def test_wan22_failure_t3k(mesh_device):
+    for i in range(5):
+        torch_input_tensor = torch.rand((1, 6240, 384), dtype=torch.bfloat16)
+        input_tensor = ttnn.from_torch(
+            torch_input_tensor,
+            layout=ttnn.Layout.ROW_MAJOR,
+            dtype=ttnn.bfloat16,
+            memory_config=ttnn.DRAM_MEMORY_CONFIG,
+            device=mesh_device,
+            mesh_mapper=ttnn.ShardTensor2dMesh(
+                mesh_device, mesh_shape=tuple(mesh_device.shape), dims=[None, None, None]
+            ),
+        )
+        output_tensor = ttnn.to_layout(input_tensor, ttnn.TILE_LAYOUT)
+        torch_output_tensor = ttnn.to_torch(
+            output_tensor, dtype=torch.bfloat16, mesh_composer=ttnn.ConcatMeshToTensor(mesh_device, dim=0)
+        )
+        expanded_input_tensor = torch_input_tensor.expand(8, 6240, 384)
+        assert_with_pcc(expanded_input_tensor, torch_output_tensor)
+
+
+def test_wan22_failure():
+    for i in range(5):
+        torch_input_tensor = torch.rand((1, 6240, 384), dtype=torch.bfloat16)
+        input_tensor = ttnn.from_torch(
+            torch_input_tensor,
+            layout=ttnn.Layout.ROW_MAJOR,
+            dtype=ttnn.bfloat16,
+            memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        )
+        output_tensor = ttnn.to_layout(input_tensor, ttnn.TILE_LAYOUT)
+        torch_output_tensor = ttnn.to_torch(output_tensor, dtype=torch.bfloat16)
+        assert_with_pcc(torch_input_tensor, torch_output_tensor)
 
 
 @pytest.mark.parametrize("height", [32, 30])
