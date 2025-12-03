25c445d489a50c9d7cd2fcc26875c7d5f2c25d9e
Diego Gomez <dgomez@tenstorrent.com>
2025-12-03 09:48:13 -0500

    untilize

---
+++ ttnn/cpp/ttnn/operations/data_movement/untilize/device/untilize_device_operation_types.hpp
@@ -0,0 +1,40 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+#include "ttnn/tensor/tensor.hpp"
+
+namespace ttnn::operations::data_movement::untilize {
+struct operation_attributes_t {
+    tt::tt_metal::MemoryConfig output_mem_config{};
+    bool use_multicore{};
+    bool use_pack_untilize{};
+    bool fp32_dest_acc_en{};
+    std::optional<CoreRangeSet> sub_core_grids{};
+    bool enough_space_width{};
+    bool enough_space_height{};
+    uint32_t pf_type{};
+};
+
+struct tensor_args_t {
+    Tensor input{};
+};
+
+using tensor_return_value_t = Tensor;
+using spec_return_value_t = ttnn::TensorSpec;
+
+using shape_return_value_t = ttnn::Shape;
+
+uint32_t get_largest_divisor(uint32_t dividend, uint32_t starting_divisor, uint32_t divisor_factor = 1);
+
+namespace program {
+struct untilize_shared_variables_t {
+    tt::tt_metal::KernelHandle reader_kernel_id{};
+    tt::tt_metal::KernelHandle writer_kernel_id{};
+    tt::tt_metal::CBHandle cb_src0{};
+    tt::tt_metal::CBHandle cb_output{};
+    std::vector<CoreCoord> cores_with_runtime_args{};
+};
+}  // namespace program
+}  // namespace ttnn::operations::data_movement::untilize
