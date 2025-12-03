9edea482a58683ad89a2157748b84600136d4beb
awliu-TT <awliu@tenstorrent.com>
2025-12-03 10:55:38 +0000

    fix clang tidy errors

--- ttnn/cpp/ttnn/operations/conv/conv2d/conv2d_op_program_factory_common.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv2d/conv2d_op_program_factory_common.cpp
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "conv2d_op_program_factory_common.hpp"
+#include "ttnn/operations/conv/conv2d/conv2d_utils.hpp"
 #include <umd/device/types/arch.hpp>
 #include <algorithm>
 #include <cstdint>
