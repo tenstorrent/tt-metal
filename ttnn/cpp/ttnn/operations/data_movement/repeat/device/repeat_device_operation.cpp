d530483131bdd4da56273afd80bbe68c2d71d422
Bazyli Klockiewicz <bklockiewicz@tenstorrent.com>
2025-12-03 18:29:23 +0000

    fix choice of factories

--- ttnn/cpp/ttnn/operations/data_movement/repeat/device/repeat_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/repeat/device/repeat_device_operation.cpp
@@ -15,9 +15,9 @@ RepeatDeviceOperation::program_factory_t RepeatDeviceOperation::select_program_f
     const operation_attributes_t& operation_attributes, const tensor_args_t& tensor_args) {
     bool is_last_dim = operation_attributes.m_is_last_dim;
     if (is_last_dim) {
-        return program::RepeatProgramFactoryLastDim{};
-    } else {
         return program::RepeatProgramFactorySecondDim{};
+    } else {
+        return program::RepeatProgramFactoryLastDim{};
     }
 }
 
