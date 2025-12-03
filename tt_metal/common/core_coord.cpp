157b09ec016dce524960e17d4025c47cbcceb94b
Diego Gomez <dgomez@tenstorrent.com>
2025-12-03 18:30:43 -0500

    Add additional argument handling in graph serializer (#29563)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/28692)
    
    ### Problem description
    There are some arguments in graph tracing that were not being serialized
    
    ### What's changed
    Extended the amount of arguments we support, improved the json output
    and made the whole system a bit more flexible
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-quick.yaml)
    CI passes (if applicable)
    - [ ] [Galaxy demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ] New/Existing tests provide coverage for changes
    
    
    
    Example of serialized deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
    
    https://drive.google.com/file/d/1Q3zPUg-PjhAn-EpnQHcllAbBr9ixana8/view?usp=sharing
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tt_metal/common/core_coord.cpp
+++ tt_metal/common/core_coord.cpp
@@ -731,3 +731,8 @@ CoreRangeSet from_json_t<CoreRangeSet>::operator()(const nlohmann::json& json) n
 }
 
 }  // namespace ttsl::json
+
+std::ostream& operator<<(std::ostream& os, const CoreRangeSet& core_range_set) {
+    tt::stl::reflection::operator<<(os, core_range_set);
+    return os;
+}
