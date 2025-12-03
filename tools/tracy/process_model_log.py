b130dface0d81e0c10395a6eed64e3fc068a0d71
Samarth Agarwal <sagarwal@tenstorrent.com>
2025-12-03 17:51:46 +0000

    Formatting

--- tools/tracy/process_model_log.py
+++ tools/tracy/process_model_log.py
@@ -61,7 +61,11 @@ def run_device_profiler(
     python_post_process=True,
     capture_perf_counters_groups=[],
     sum_profiling=False,
-    # Default op support count is multiplied by 1.333 because previously the profiler would reserve space for approximately 33% more ops than the default. Several model tests call this function and rely on this extra space to ensure that all ops are captured by the profiler. Now that the profiler doesn't reserve this extra space, we multiply the default by 1.333 to ensure that these model tests continue to capture all ops.
+    # Default op support count is multiplied by 1.333 because previously the profiler would reserve space
+    # for approximately 33% more ops than the default. Several model tests call this function and rely on
+    # this extra space to ensure that all ops are captured by the profiler. Now that the profiler doesn't
+    # reserve this extra space, we multiply the default by 1.333 to ensure that these model tests continue
+    # to capture all ops.
     op_support_count=int(PROFILER_DEFAULT_OP_SUPPORT_COUNT * 1.333),
     is_command_binary_exe=False,
 ):
