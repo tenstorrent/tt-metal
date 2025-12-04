488df6200332e298264e4cedb42ced3a053bdd28
gsarabandoTT <ctr-gsarabando@ext.tenstorrent.com>
2025-12-04 21:46:18 +0000

    Move stable_diffusion to vision/generative

--- tests/scripts/single_card/run_single_card_demo_tests.sh
+++ tests/scripts/single_card/run_single_card_demo_tests.sh
@@ -228,7 +228,7 @@ run_efficientnet_b0_func(){
 
 run_stable_diffusion_func() {
 
-  $PYTEST_CMD --input-path="models/demos/wormhole/stable_diffusion/demo/input_data.json" models/demos/wormhole/stable_diffusion/demo/demo.py::test_demo
+  $PYTEST_CMD --input-path="models/demos/vision/generative/stable_diffusion/wormhole/demo/input_data.json" models/demos/vision/generative/stable_diffusion/wormhole/demo/demo.py::test_demo
 
 }
 
