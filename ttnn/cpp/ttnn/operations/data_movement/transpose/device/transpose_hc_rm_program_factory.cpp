9ef2b6eed4f06590bac7827aae2b53a21571ef2c
Ilia Shutov <Ilia_Shutov@epam.com>
2025-12-04 10:35:18 +0000

    fix: missed TT_ASSERT after the refactoring

--- ttnn/cpp/ttnn/operations/data_movement/transpose/device/transpose_hc_rm_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/transpose/device/transpose_hc_rm_program_factory.cpp
@@ -152,6 +152,7 @@ TransposeHCRMProgramFactory::cached_program_t TransposeHCRMProgramFactory::creat
         split_work_to_cores(compute_with_storage_grid_size, NCH);
 
     Buffer* dst_buffer = output_tensor.buffer();
+    TT_ASSERT(dst_buffer != nullptr, "Output buffer should be allocated on device!");
 
     uint32_t src0_cb_index = 0;
 
