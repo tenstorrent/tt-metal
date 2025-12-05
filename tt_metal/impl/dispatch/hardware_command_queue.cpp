26306c4f92ac1c434ced4969d410e0acbe48e558
“Bryan <bzimmerman@tenstorrent.com>
2025-12-05 17:05:43 +0000

    Remove reset_worker_state

--- tt_metal/impl/dispatch/hardware_command_queue.cpp
+++ tt_metal/impl/dispatch/hardware_command_queue.cpp
@@ -150,7 +150,7 @@ std::optional<uint32_t> HWCommandQueue::tid() const { return this->tid_; }
 
 SystemMemoryManager& HWCommandQueue::sysmem_manager() { return this->manager_; }
 
-<void HWCommandQueue::set_go_signal_noc_data_and_dispatch_sems(
+void HWCommandQueue::set_go_signal_noc_data_and_dispatch_sems(
     uint32_t num_dispatch_sems, const vector_aligned<uint32_t>& noc_mcast_unicast_data) {
     program_dispatch::set_num_worker_sems_on_dispatch(device_, this->manager_, id_, num_dispatch_sems);
     program_dispatch::set_go_signal_noc_data_on_dispatch(device_, noc_mcast_unicast_data, this->manager_, id_);
