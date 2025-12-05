498e08d7d9ffe5f4c9132e9a6f11cf102d429135
Vladyslav Tsilytskyi <vtsilytskyi@tenstorrent.com>
2025-12-05 22:37:06 +0100

    Migrate op to new infra: concat (#33779)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/32741
    
    ### Problem description
    
    We have 2 ways of writing ops, which should be unified into one.
    
    ### What's changed
    
    Migrate concat op to the new infrastructure.
    
    ### Checklist
    - [X] [All post commit
    #19965136674](https://github.com/tenstorrent/tt-metal/actions/runs/19965136674)
    CI passes except C++ unit tests (timeout, happens all the time, not
    related).

--- ttnn/cpp/ttnn/operations/data_movement/concat/device/concat_s2i_program_factory.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/concat/device/concat_s2i_program_factory.hpp
@@ -6,19 +6,19 @@
 
 #include "concat_device_operation_types.hpp"
 #include "ttnn/device_operation.hpp"
-#include <tt-metalium/work_split.hpp>
 
 namespace ttnn::operations::data_movement::concat::program {
 
-// Shared variables for interleaved concat
-struct ConcatSharedVariables {
+// Shared variables for s2i concat
+struct ConcatS2ISharedVariables {
+    uint32_t num_input_tensors = 0;
     tt::tt_metal::KernelHandle reader_kernel_id = 0;
     tt::tt_metal::KernelHandle writer_kernel_id = 0;
-    std::vector<CoreCoord> cores;
+    CoreRangeSet all_cores;
 };
 
-struct ConcatProgramFactory {
-    using shared_variables_t = ConcatSharedVariables;
+struct ConcatS2IProgramFactory {
+    using shared_variables_t = ConcatS2ISharedVariables;
     using cached_program_t = ttnn::device_operation::CachedProgram<shared_variables_t>;
 
     static cached_program_t create(
