792816dd307f19be32ffe576f1e036cb57ec0e5c
John Bauman <jbauman@tenstorrent.com>
2025-12-05 19:10:11 +0000

    improve naming

--- tt_metal/hw/inc/tt-1xx/blackhole/noc_nonblocking_api.h
+++ tt_metal/hw/inc/tt-1xx/blackhole/noc_nonblocking_api.h
@@ -262,7 +262,8 @@ inline __attribute__((always_inline)) bool ncrisc_noc_read_with_transaction_id_f
     uint32_t noc, uint32_t transcation_id) {
     return (NOC_STATUS_READ_REG(noc, NIU_MST_REQS_OUTSTANDING_ID(transcation_id)) == 0);
 }
-inline __attribute__((always_inline)) uint32_t ncrisc_noc_available_read_transactions(uint32_t noc, uint32_t trid) {
+
+inline __attribute__((always_inline)) uint32_t ncrisc_noc_available_transactions(uint32_t noc, uint32_t trid) {
     return NOC_MAX_TRANSACTION_ID_COUNT - NOC_STATUS_READ_REG(noc, NIU_MST_REQS_OUTSTANDING_ID(trid));
 }
 
