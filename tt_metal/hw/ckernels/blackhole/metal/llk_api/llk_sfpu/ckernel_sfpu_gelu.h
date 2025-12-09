04867fbf8706896ac3e618791e9c84ea23aec6a7
CodeMan62 <sharmahimanshu15082007@gmail.com>
2025-12-10 00:14:21 +0530

    pre-commit+gelu fix

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_gelu.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_gelu.h
@@ -7,8 +7,59 @@
 #include "ckernel_defs.h"
 #include "ckernel.h"
 #include "llk_defs.h"
+
 namespace ckernel {
 namespace sfpu {
+
+#define POLYVAL15(c15, c14, c13, c12, c11, c10, c9, c8, c7, c6, c5, c4, c3, c2, c1, c0, x)                         \
+    (((((((((((((((c15) * (x) + (c14)) * (x) + (c13)) * (x) + (c12)) * (x) + (c11)) * (x) + (c10)) * (x) + (c9)) * \
+                (x) +                                                                                              \
+            (c8)) *                                                                                                \
+               (x) +                                                                                               \
+           (c7)) *                                                                                                 \
+              (x) +                                                                                                \
+          (c6)) *                                                                                                  \
+             (x) +                                                                                                 \
+         (c5)) *                                                                                                   \
+            (x) +                                                                                                  \
+        (c4)) *                                                                                                    \
+           (x) +                                                                                                   \
+       (c3)) *                                                                                                     \
+          (x) +                                                                                                    \
+      (c2)) *                                                                                                      \
+         (x) +                                                                                                     \
+     (c1)) * (x) +                                                                                                 \
+        (c0)
+
+inline sfpi::vFloat calculate_gelu_chebyshev(sfpi::vFloat val) {
+    sfpi::vFloat result = 0.0f;
+    v_if(val >= -5.5f) {
+        result = POLYVAL15(
+            -1.81205228163e-09,
+            -4.59055119276e-08,
+            -3.74540617693e-07,
+            -2.29754133825e-07,
+            1.19076782913e-05,
+            4.25116466215e-05,
+            -0.000138391838381,
+            -0.000862052441087,
+            0.000768340223025,
+            0.0092074331601,
+            -0.00208478037614,
+            -0.0656369476513,
+            0.00244542739174,
+            0.398579460781,
+            0.499174645395,
+            2.98325768482e-05,
+            val);
+
+        // Ensure result has the same sign as input using setsgn
+        result = setsgn(result, val);
+    }
+    v_endif;
+
+    return result;
+}
 
 template <ApproximationMode APPROX_MODE>
 void gelu_init() {
@@ -37,7 +88,6 @@ inline void calculate_gelu() {
     }
     }
 }
-
 template <ApproximationMode APPROX_MODE, int ITERATIONS = 8>
 inline void calculate_gelu_derivative() {
     _calculate_gelu_derivative_<APPROX_MODE, ITERATIONS>();
--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_clamp.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_clamp.h
@@ -7,6 +7,7 @@
 #include "llk_math_eltwise_unary_sfpu_init.h"
 #include "llk_math_eltwise_unary_sfpu_params.h"
 #include "ckernel_sfpu_clamp.h"
+#include "llk_defs.h"
 
 namespace ckernel {
 
@@ -19,14 +20,14 @@ template <bool APPROXIMATE, int ITERATIONS = 8>
 inline void llk_math_eltwise_unary_sfpu_clamp(
     uint dst_index, uint min_val, uint max_val, int vector_mode = (int)VectorMode::RC) {
     _llk_math_eltwise_unary_sfpu_params_<APPROXIMATE>(
-        ckernel::sfpu::calculate_clamp<APPROXIMATE, ITERATIONS>, dst_index, vector_mode, min_val, max_val);
+        ckernel::sfpu::calculate_clamp<(APPROXIMATE ? ApproximationMode::Fast : ApproximationMode::Precise), ITERATIONS>, dst_index, vector_mode, min_val, max_val);
 }
 
 template <bool APPROXIMATE, int ITERATIONS = 8>
 inline void llk_math_eltwise_unary_sfpu_clamp_int32(
     uint dst_index, uint min_val, uint max_val, int vector_mode = (int)VectorMode::RC) {
     _llk_math_eltwise_unary_sfpu_params_<APPROXIMATE>(
-        ckernel::sfpu::calculate_clamp_int32<APPROXIMATE, ITERATIONS>, dst_index, vector_mode, min_val, max_val);
+        ckernel::sfpu::calculate_clamp_int32<(APPROXIMATE ? ApproximationMode::Fast : ApproximationMode::Precise), ITERATIONS>, dst_index, vector_mode, min_val, max_val);
 }
 
 }  // namespace ckernel
