0112a703b8309a56b89820d5aab1b4aa93e56cc1
Nikola Ostojic <nostojic@tenstorrent.com>
2025-12-09 19:47:45 +0100

    Refactor warmup traces to be called from vLLM before first healthy signal (#33143)
    ### Ticket
    - Link to Github Issue - https://github.com/tenstorrent/vllm/issues/220
    - `tenstorrent/vllm` PR that is also part of this issue -
    https://github.com/tenstorrent/vllm/pull/254
    - latest `vllm-nightly` run -
    https://github.com/tenstorrent/tt-metal/actions/runs/19941353422
    
    
    ### Problem description
    - User that would send the first request would experience slower
    performance due to prefill, decode trace capturing and model compilation
    happening within the first request
    
    ### Checklist
    - [x] (For models and ops writers) [Single-card demo tests]
    https://github.com/tenstorrent/tt-metal/actions/runs/20058047405
    - [x] [Galaxy demo tests, for Llama]
    https://github.com/tenstorrent/tt-metal/actions/runs/20049374045
    - [x] (For runtime and ops writers) [T3000 unit tests]
    https://github.com/tenstorrent/tt-metal/actions/runs/20068234251
    - [x] T3000 mixtral test (failed above) -
    https://github.com/tenstorrent/tt-metal/actions/runs/20070744756
    - [ ] (For models and ops writers) [T3000 demo tests]
    https://github.com/tenstorrent/tt-metal/actions/runs/20058028720
    - [x] (Blackhole, demo tests)
    https://github.com/tenstorrent/tt-metal/actions/runs/20058014328
    - [x] (vLLM nightly tests)
    https://github.com/tenstorrent/tt-metal/actions/runs/20063933765
    - [x] Additional Galaxy test for GPT-OSS that failed in the run above -
    https://github.com/tenstorrent/tt-metal/actions/runs/20063422837/job/57546733252

--- models/demos/gemma3/tt/model_config.py
+++ models/demos/gemma3/tt/model_config.py
@@ -16,6 +16,7 @@ from models.common.utility_functions import is_blackhole, is_wormhole_b0, neares
 from models.demos.gemma3.tt.load_checkpoints import convert_vision_hf_to_meta, convert_vision_meta_to_hf
 from models.tt_transformers.tt.common import (
     calculate_hidden_dim,
+    calculate_prefill_warmup_seq_lens,
     encode_prompt_hf,
     get_base_model_name,
     get_out_subblock_w,
@@ -1207,6 +1208,28 @@ class ModelArgs:
             logger.info(f"LM head grid: {self.lm_head_core_grid}")
 
         self.trace_prefill_supported_seq_lens = self.get_trace_prefill_supported_seq_lens()
+
+    def get_warmup_prefill_supported_seq_lens(self):
+        DEFAULT_VALUE = 8192
+        # This dictionary is used to override the default ceil warmup prefill value
+        model_specific_ceil_warmup_lengths = {
+            # e.g. "gemma-3-4b": 4096
+        }
+
+        max_seq_len_to_warmup = model_specific_ceil_warmup_lengths.get(self.base_model_name, DEFAULT_VALUE)
+
+        to_warmup_seq_lens = calculate_prefill_warmup_seq_lens(
+            max_seq_len_to_warmup, self.trace_prefill_supported_seq_lens, self.max_seq_len
+        )
+
+        to_warmup_seq_lens = self.filter_warmup_seq_lens(to_warmup_seq_lens)
+
+        return to_warmup_seq_lens
+
+    def filter_warmup_seq_lens(self, to_warmup_seq_lens):
+        # TODO: Add more model-specific filtering here
+        # This filtering is based on the current PR's (https://github.com/tenstorrent/tt-metal/pull/33143) sequence lengths that are used for warmup
+        return to_warmup_seq_lens
 
     def get_trace_prefill_supported_seq_lens(self):
         default_supported_seq_lens = {
--- models/demos/llama3_70b_galaxy/tt/generator.py
+++ models/demos/llama3_70b_galaxy/tt/generator.py
@@ -722,6 +722,23 @@ class Generator:
             padded_page_table[user_id, :] = page_table[0, :]
         return padded_page_table
 
+    def warmup_model_prefill(self, kv_cache, enable_trace, sampling_params) -> None:
+        # page_table gets padded properly in prefill_forward_text
+        # be sure to pad correctly for non traced sequences in future warmup calls
+        page_table = torch.zeros(1, 1, dtype=torch.int32)
+        # in case of multiple sampling parameters, we need to warmup for each one
+        for s in sampling_params:
+            self.warmup_prefill_traces(
+                tokens=None,
+                page_table=page_table,
+                kv_cache=kv_cache,
+                prompt_lens=None,
+                enable_trace=enable_trace,
+                sampling_params=s,
+                empty_slots=None,
+                tt_out_logits_all_users=None,
+            )
+
     ## Destructor (used to delete ttnn trace if exists)
 
     def __del__(self):
