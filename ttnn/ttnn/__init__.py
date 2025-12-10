1467f60a489760b1c17d1e0cbe50505d49394459
Vasisht Suresh <vsuresh@tenstorrent.com>
2025-12-10 12:54:04 -0800

    Remove duplicate import of create_group_norm_weight_bias_rm

--- ttnn/ttnn/__init__.py
+++ ttnn/ttnn/__init__.py
@@ -353,7 +353,6 @@ from ttnn.operations.normalization import (
     SoftmaxShardedMultiCoreProgramConfig,
     LayerNormDefaultProgramConfig,
     LayerNormShardedMultiCoreProgramConfig,
-    create_group_norm_weight_bias_rm,
     create_group_norm_input_mask,
     create_group_norm_input_negative_mask,
     create_group_norm_weight_bias_rm,
