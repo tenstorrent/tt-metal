d33f62527a63cd72954a3c4689796289996473e4
Colman Glagovich <114512306+cglagovichTT@users.noreply.github.com>
2025-12-10 16:39:53 -0500

    SDPA - minor compute optimization (#34092)
    ### Problem description
    LLK team @ncvetkovicTT provided an optimized `reduce_block_max_row`
    function which computes the max across a block of tiles in an optimized
    manner.
    
    ### What's changed
    This PR uses `reduce_block_max_row` in SDPA, and applies a few other
    simple compute optimizations
    - remove eltwise `max_tile` by copying previous input into DST before
    calling `reduce_block_max_row`
    - granularize CB wait / push between certain functions of SDPA to allow
    UNPACK of a following function to overlap with PACK of a previous
    function
    
    Device profiler shows that we achieve a small speedup across a variety
    of shapes by applying this optimization.
    
    Baseline performance
    
    | Causal | Shape ID | q_chunk | k_chunk | Measured Perf (ms) |
    |--------|----------|---------|---------|--------------------|
    | causal | shape_1_8_8_1024_128 | 128 | 256 | 0.167 |
    | causal | shape_1_10_10_2432_128 | 128 | 256 | 0.896 |
    | causal | shape_1_10_10_2560_128 | 256 | 512 | 0.891 |
    | causal | shape_1_10_10_9472_128 | 256 | 512 | 9.275 |
    | causal | shape_1_1_1_16384_128 | 256 | 512 | 2.597 |
    | noncausal | shape_1_8_8_1024_128 | 128 | 256 | 0.195 |
    | noncausal | shape_1_10_10_2432_128 | 256 | 512 | 0.907 |
    | noncausal | shape_1_10_10_2560_128 | 256 | 512 | 0.888 |
    | noncausal | shape_1_10_10_9472_128 | 256 | 512 | 10.762 |
    | noncausal | shape_1_1_1_16384_128 | 256 | 512 | 2.732 |
    
    
    This PR performance
    
    | Causal | Shape ID | q_chunk | k_chunk | Measured Perf (ms) |
    |--------|----------|---------|---------|--------------------|
    | causal | shape_1_8_8_1024_128 | 128 | 128 | 0.158 |
    | causal | shape_1_10_10_2432_128 | 256 | 256 | 0.842 |
    | causal | shape_1_10_10_2560_128 | 256 | 512 | 0.849 |
    | causal | shape_1_10_10_9472_128 | 256 | 512 | 8.717 |
    | causal | shape_1_1_1_16384_128 | 256 | 512 | 2.402 |
    | noncausal | shape_1_8_8_1024_128 | 256 | 512 | 0.190 |
    | noncausal | shape_1_10_10_2432_128 | 256 | 512 | 0.853 |
    | noncausal | shape_1_10_10_2560_128 | 256 | 512 | 0.842 |
    | noncausal | shape_1_10_10_9472_128 | 256 | 512 | 10.168 |
    | noncausal | shape_1_1_1_16384_128 | 256 | 256 | 2.715 |
    
    
    ### Checklist
    - [x] APC
    https://github.com/tenstorrent/tt-metal/actions/runs/20077005015
    - [x] BH post commit
    https://github.com/tenstorrent/tt-metal/actions/runs/20077021351
    - [x] L2 nightly
    https://github.com/tenstorrent/tt-metal/actions/runs/20077043998
    - [x] T3K nightly
    https://github.com/tenstorrent/tt-metal/actions/runs/20077350665
    - [x] galaxy nightly
    https://github.com/tenstorrent/tt-metal/actions/runs/20077364878
    - [x] T3K unit
    https://github.com/tenstorrent/tt-metal/actions/runs/20079289401

--- ttnn/cpp/ttnn/operations/transformer/sdpa/device/kernels/compute/ring_joint_sdpa.cpp
+++ ttnn/cpp/ttnn/operations/transformer/sdpa/device/kernels/compute/ring_joint_sdpa.cpp
@@ -99,8 +99,6 @@ void MAIN {
             uint32_t alias_mm2_prev_out = cb_out_im_A;
             uint32_t alias_mm2_cur_out = cb_out_im_B;
 
-            cb_wait_front(cb_q_in, q_chunk_tiles);
-
             for (uint32_t k_chunk = iter_k_chunk_start; k_chunk < iter_k_chunk_end; ++k_chunk) {
                 if (k_chunk >= global_logical_NK_chunks && k_chunk < global_padded_NK_chunks) {
                     // This is a KV chunk on spatial input beyond the chunk-padded length of the spatial input.
@@ -158,7 +156,6 @@ void MAIN {
                 sub_exp_block_bcast_cols_inplace<cb_qk_im, Sq_chunk_t, Sk_chunk_t, scale_fp32>(
                     alias_cur_max, alias_cur_sum);
 
-                cb_wait_front(cb_qk_im, qk_chunk_tiles);
                 /* OUT_IM = QK @ V_CHUNK */
                 matmul_blocks(
                     cb_qk_im,
