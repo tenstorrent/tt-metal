d74dc3d9eed0a64e13800ad8d8716f1b16f2ae61
Borys Bradel <bbradel@tenstorrent.com>
2025-12-10 15:17:19 +0000

    #32584: adjust where dtype is checked

--- ttnn/cpp/ttnn/operations/matmul/device/matmul_op.cpp
+++ ttnn/cpp/ttnn/operations/matmul/device/matmul_op.cpp
@@ -1388,10 +1388,6 @@ Matmul create_matmul_struct(
          (input_tensor_b.dtype() == DataType::BFLOAT8_B || input_tensor_b.dtype() == DataType::BFLOAT4_B));
     const auto increase_fidelity = !has_program_config && !has_user_grid && !are_inputs_low_precision_df;
     auto math_fidelity = increase_fidelity ? MathFidelity::HiFi3 : MathFidelity::LoFi;
-    std::optional<DataType> output_dtype = parameters.output_dtype;
-    bool is_float_32 = (input_tensor_a.dtype() == DataType::FLOAT32 && input_tensor_b.dtype() == DataType::FLOAT32) ||
-                       (output_dtype == DataType::FLOAT32);
-    math_fidelity = is_float_32 ? MathFidelity::HiFi4 : math_fidelity;
 
     bool broadcast_batch =
         parameters.bcast_batch.value_or(get_broadcast_batch(input_tensor_a, input_tensor_b, parameters.program_config));
@@ -1401,6 +1397,7 @@ Matmul create_matmul_struct(
         !optional_output_tensors.empty() && optional_output_tensors.at(0).has_value();
     MemoryConfig output_mem_config = parameters.output_mem_config;
 
+    std::optional<DataType> output_dtype = parameters.output_dtype;
     if (is_optional_output_tensor) {
         const auto& optional_output_tensor = optional_output_tensors.at(0);
         if (output_mem_config == tt::tt_metal::operation::DEFAULT_OUTPUT_MEMORY_CONFIG) {
@@ -1428,6 +1425,9 @@ Matmul create_matmul_struct(
             output_dtype = input_tensor_a.dtype();
         }
     }
+    bool is_float_32 = (input_tensor_a.dtype() == DataType::FLOAT32 && input_tensor_b.dtype() == DataType::FLOAT32) ||
+                       (output_dtype == DataType::FLOAT32);
+    math_fidelity = is_float_32 ? MathFidelity::HiFi4 : math_fidelity;
     auto kernel_config_val = init_device_compute_kernel_config(
         arch,
         parameters.compute_kernel_config,
