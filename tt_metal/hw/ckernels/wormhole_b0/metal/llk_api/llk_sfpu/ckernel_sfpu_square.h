b44822d2fece1b0bfacb55ab4b74c707a680119f
Filip Vranic <fvranic@tenstorrent.com>
2025-12-10 23:58:57 +0100

    Update ckernel_sfpu_square.h

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_square.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_square.h
@@ -5,7 +5,7 @@
 #pragma once
 
 #include "sfpi.h"
-// NOTE: The following header is from the external tt-llk and not defined in tt-metal 
+// NOTE: The following header is from the external tt-llk and not defined in tt-metal
 #include "sfpu/ckernel_sfpu_square.h"
 
 namespace ckernel::sfpu {
