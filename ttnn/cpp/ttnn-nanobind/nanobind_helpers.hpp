292a2c14ecfa185c04d5d4cb093144de419a950f
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-12-10 01:08:00 -0500

    Add helper for unique_ptr interop

--- ttnn/cpp/ttnn-nanobind/nanobind_helpers.hpp
+++ ttnn/cpp/ttnn-nanobind/nanobind_helpers.hpp
@@ -12,12 +12,27 @@
 
 namespace nanobind_helpers {
 
+// https://nanobind.readthedocs.io/en/latest/api_extra.html#unique-pointer-deleter
+// https://nanobind.readthedocs.io/en/latest/ownership.html#unique-pointers
+// Calling a function taking a unique pointer from Python invalidates the
+// passed Python object. Nanobind will refuse further use of it. Use this
+// custom deleter version to allow for ownership transfer.
 template <typename T>
 using unique_ptr = std::unique_ptr<T, nanobind::deleter<T>>;
 
 template <class T, class... Args>
 auto make_unique(Args&&... args) {
     return unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+template <typename T, typename Deleter>
+auto steal_rewrap_unique(std::unique_ptr<T, Deleter>& uptr) noexcept {
+    return unique_ptr<T>(uptr.release());
+}
+
+template <typename T, typename Deleter>
+auto steal_rewrap_unique(std::unique_ptr<T, Deleter>&& uptr) noexcept {
+    return unique_ptr<T>(uptr.release());
 }
 
 // nanobind does not bind properly to std::optional<std::reference_wrapper<T>>, so use a pointer
