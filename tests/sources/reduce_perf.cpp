da2794c4cb21677ced94d11ff497c27eb38ce587
Nemanja Divnic <ndivnic@tenstorrent.com>
2025-12-11 14:56:48 +0100

    #0: Fix FACE_R_DIM param value to 16 in perf tests for unpacker kernel init phase (#927)
    ### Ticket
    #0
    
    ### Problem description
    There is observed mismatch in LLK perf tests between usage of 2 methods
    during unpacker init phase:
    - _llk_unpack_AB_hw_configure_
    - _llk_unpack_AB_init_
    
    Both methods have param face_r_dim and usually in most tests the passed
    value is 16. However, in perf tests, for HW configure method, passed
    value is hard-coded 8. The fix is simple - pass FACE_R_DIM (const value
    16) instead and align the values between these 2 methods.
    
    ### What's changed
    The change is simple, just a change in argument passed for HW configure
    method in unpacker init phase of perf tests.
    
    ### Type of change
    
    - [*] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)

--- tests/sources/reduce_perf.cpp
+++ tests/sources/reduce_perf.cpp
@@ -31,7 +31,14 @@ void run_kernel()
 {
     {
         ZONE_SCOPED("INIT")
-        _llk_unpack_AB_hw_configure_<is_fp32_dest_acc_en>(formats.unpack_src, formats.unpack_src, formats.unpack_dst, formats.unpack_dst, 8, false, 4);
+        _llk_unpack_AB_hw_configure_<is_fp32_dest_acc_en>(
+            formats.unpack_src,
+            formats.unpack_src,
+            formats.unpack_dst,
+            formats.unpack_dst,
+            FACE_R_DIM,
+            /* within_face_16x16_transpose */ 0,
+            /* num_faces */ 4);
         _llk_unpack_AB_init_<>(
             FACE_R_DIM,
             TILE_NUM_FACES,
