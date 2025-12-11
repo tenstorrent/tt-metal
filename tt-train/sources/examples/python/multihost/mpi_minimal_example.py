3f1daaa98a60cf85471134e546058cffdaf6eb94
Ryan Reece <rreece@tenstorrent.com>
2025-12-11 00:32:58 +0000

    import fixes

--- tt-train/sources/examples/python/multihost/mpi_minimal_example.py
+++ tt-train/sources/examples/python/multihost/mpi_minimal_example.py
@@ -40,6 +40,7 @@ import ttml
 
 if __name__ == "__main__":
     autograd_ctx = ttml.autograd.AutoContext.get_instance()
+    print("DEBUG:", sys.argv)
     autograd_ctx.initialize_distributed_context(*sys.argv)
     distributed_ctx = autograd_ctx.get_distributed_context()
 
@@ -49,14 +50,20 @@ if __name__ == "__main__":
     rank = distributed_ctx.rank()
     world_size = distributed_ctx.size()
 
-    assert world_size > 1, f"World size must be greater than 1, world size: {world_size}"
+    assert (
+        world_size > 1
+    ), f"World size must be greater than 1, world size: {world_size}"
 
     if rank == 0:
         print("Rank 0 is sending data")
-        tensor_data = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0], dtype=np.float32)
+        tensor_data = np.array(
+            [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0], dtype=np.float32
+        )
         tensor_data = tensor_data.reshape(1, 1, 2, 4)
         tensor = ttml.autograd.Tensor.from_numpy(
-            tensor_data, layout=ttml.Layout.TILE, new_type=ttml.autograd.DataType.BFLOAT16
+            tensor_data,
+            layout=ttml.Layout.TILE,
+            new_type=ttml.autograd.DataType.BFLOAT16,
         )
 
         for dest_rank in range(1, world_size):
@@ -65,7 +72,9 @@ if __name__ == "__main__":
         print(f"Rank {rank} is receiving data")
         tensor_data = np.zeros((1, 1, 2, 4), dtype=np.float32)
         tensor = ttml.autograd.Tensor.from_numpy(
-            tensor_data, layout=ttml.Layout.TILE, new_type=ttml.autograd.DataType.BFLOAT16
+            tensor_data,
+            layout=ttml.Layout.TILE,
+            new_type=ttml.autograd.DataType.BFLOAT16,
         )
         tensor = socket_manager.recv(tensor, distributed_ctx, 0)
         tensor_data = tensor.to_numpy().flatten()
