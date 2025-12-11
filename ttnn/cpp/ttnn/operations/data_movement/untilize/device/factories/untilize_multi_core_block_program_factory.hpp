54b3e020f767fcb365c9985a0e836a758ee65946
Diego Gomez <dgomez@tenstorrent.com>
2025-12-11 09:30:38 -0500

    fixed licenses and relative paths

--- ttnn/cpp/ttnn/operations/data_movement/untilize/device/factories/untilize_multi_core_block_program_factory.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/untilize/device/factories/untilize_multi_core_block_program_factory.hpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC.
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -6,7 +6,7 @@
 
 #include "ttnn/operation.hpp"
 #include "ttnn/device_operation.hpp"
-#include "../untilize_device_operation_types.hpp"
+#include "ttnn/operations/data_movement/untilize/device/untilize_device_operation_types.hpp"
 
 namespace ttnn::operations::data_movement::program {
 
