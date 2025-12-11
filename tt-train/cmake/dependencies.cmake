f22d586ce9c5432d4736fb4ab96adcacf0c47c59
Adam Thompson <athompson@tenstorrent.com>
2025-12-11 18:54:19 +0000

    use upstream fix

--- tt-train/cmake/dependencies.cmake
+++ tt-train/cmake/dependencies.cmake
@@ -157,22 +157,12 @@ find_package(
 CPMAddPackage(
     NAME nanobind
     GITHUB_REPOSITORY wjakob/nanobind
-    GIT_TAG v2.9.2
-    PATCH_COMMAND
-    patch
-    --dry-run
-    -p1
-    -R
-    <
-    ${CMAKE_CURRENT_LIST_DIR}/nanobind_dlpack_bfloat.patch
-    ||
-    patch
-    -p1
-    <
-    ${CMAKE_CURRENT_LIST_DIR}/nanobind_dlpack_bfloat.patch
+    GIT_TAG
+        68b9ae82a3ed1ac34b2b96b141b45554ee79a497 # v2.9.2 + ThisIsFineTM bfloat patch
     OPTIONS
         "CMAKE_MESSAGE_LOG_LEVEL NOTICE"
         "NB_USE_SUBMODULE_DEPS ON"
+    CUSTOM_CACHE_KEY "2_9_2_with_bfloat"
 )
 
 ####################################################################################################################
