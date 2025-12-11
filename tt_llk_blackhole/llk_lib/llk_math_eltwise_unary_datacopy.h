565130b666ece06e1f99b64445141aef3df70f7b
Paytin Gardner <pgardner@tenstorrent.com>
2025-12-11 23:37:12 +0000

    pipeline test

--- tt_llk_blackhole/llk_lib/llk_math_eltwise_unary_datacopy.h
+++ tt_llk_blackhole/llk_lib/llk_math_eltwise_unary_datacopy.h
@@ -379,7 +379,7 @@ inline void eltwise_unary_configure_mop(uint rows_per_inst, uint total_rows, con
 template <DataCopyType type, bool is_fp32_dest_acc_en, BroadcastType src_b_bcast_type = BroadcastType::NONE, bool tilize = false, bool is_int_fpu_en = false>
 inline void _llk_math_eltwise_unary_datacopy_init_(const std::uint32_t num_faces = 4, const std::uint32_t dst_format = 255)
 {
-    LLK_ASSERT(num_faces == 1 || num_faces == 2 || num_faces == 4, "num_faces must be 1, 2, or 4");
+    // LLK_ASSERT(num_faces == 1 || num_faces == 2 || num_faces == 4, "num_faces must be 1, 2, or 4");
     eltwise_unary_configure_addrmod<type, src_b_bcast_type>();
 
     if constexpr (type == A2D && src_b_bcast_type == BroadcastType::NONE)
