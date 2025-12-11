be0da2639b98c0fb8a41d827b6e55c2b62b81afd
Djordje Ivanovic <133343976+djordje-tt@users.noreply.github.com>
2025-12-11 15:24:13 +0100

    Add log probs feature to TT-Transformers on T3K (#33343)
    ### Ticket
    #[32262](https://github.com/tenstorrent/tt-metal/issues/32262)
    
    ### Problem description
    We are missing log-probs feature in tt-metal which is requirement for UF
    release eow.
    
    ### Feature description: 
    For each sampled token by sampling op, it's required to return
    log-probability of it. It gives confidence of token that is sampled as
    extra information apart from produced token.
    
    ### What's changed
    - Implementation of LogProbsCalculator class for calculating log-probs
    - Added unit tests with Qwen3 shapes
    - Integration into simple_text_demo.py 
    - To calculate it, LogProbsCalculator class is responsible for. All the
    implementation
    
    Warning: Implementation of penalties here should be skipped reviewing
    since it's rebase to @sraizada-tt's branch:
    
    Review appreciated on: 
    - `logProbsCalculator` class inside utils.py
    - propagation in `generator.py`
    - ignore function names in unit testing since it's still not polished.
    Mostly early opened to get early feedback on implementation and
    integration approach
    cc: @skhorasganiTT @sraizada-tt @rdraskicTT your feedback is much
    appreciated!
    
    pipelines: 
    - [BH Multi-card
    demo](https://github.com/tenstorrent/tt-metal/actions/runs/19853858206)
    - [Galaxy
    Quick](https://github.com/tenstorrent/tt-metal/actions/runs/19854152113)
    - [Galaxy
    Demo](https://github.com/tenstorrent/tt-metal/actions/runs/19854177690)
    - [(Single-card) Demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/19853904911)
    - [(T3K) demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/19853915344)

--- models/tt_transformers/demo/simple_text_demo.py
+++ models/tt_transformers/demo/simple_text_demo.py
@@ -322,6 +322,30 @@ def prepare_generator_args(
             None,  # num_layers, if None -> defaults to all layers
             "full",  # performs both prefill and decode
         ),
+        (  # Batch-32 run (Throughput) - 32 users, small prompt with log-probs
+            "models/tt_transformers/demo/sample_prompts/input_data_questions_prefill_128.json",  # input_prompts
+            True,  # instruct mode
+            1,  # repeat_batches
+            1024,  # max_seq_len
+            32,  # batch_size
+            200,  # max_generated_tokens
+            True,  # paged_attention
+            {"page_block_size": 32, "page_max_num_blocks_per_dp": 1024},  # page_params
+            {
+                "temperature": torch.linspace(0.0, 1.0, steps=32).tolist(),
+                "top_p": torch.linspace(0.08, 1.0, steps=32).tolist(),
+                "top_k": torch.arange(1, 33).tolist(),  # 1 to 32 inclusive
+                "enable_log_probs": [True] * 32,
+            },  # sampling_params (non-uniform)
+            True,  # stop_at_eos
+            False,  # ci_only
+            1,  # data_parallel
+            False,  # token_accuracy
+            False,  # stress_test
+            True,  # enable_trace
+            None,  # num_layers, if None -> defaults to all layers
+            "full",  # performs both prefill and decode
+        ),
         (  # long-context-64k run - Single user, long prompt (may vary based on the model's tokenizer)
             "models/tt_transformers/demo/sample_prompts/input_data_long_64k.json",  # input_prompts
             True,  # instruct mode
@@ -671,6 +695,7 @@ def prepare_generator_args(
     ids=[
         "batch-1",  # latency
         "batch-32",  # throughput
+        "batch-32-log-probs",  # throughput with log-probs
         "long-context-64k",  # 64k context, max_seq_len=128k
         "long-context-32k",  # 32k context, max_seq_len=32k
         "long-context-16k",  # 16k context, max_seq_len=32k
@@ -1007,6 +1032,9 @@ def test_demo_text(
                 repetition_penalty=sampling_params["repetition_penalty"]
                 if "repetition_penalty" in sampling_params
                 else 1.0,
+                enable_log_probs=sampling_params["enable_log_probs"]
+                if "enable_log_probs" in sampling_params
+                else False,
             )
             if model[0]._supports_on_device_sampling
             else None
@@ -1015,7 +1043,7 @@ def test_demo_text(
             # host sampling only supports single sample param for all users in a batch
             sampling_params["temperature"] = sampling_params["temperature"][0]
             sampling_params["top_p"] = sampling_params["top_p"][0]
-
+            sampling_params["enable_log_probs"] = sampling_params["enable_log_probs"][0]
         # Initial positions
         current_pos = torch.tensor([decoding_pos[b] if mode == "full" else 0 for b in range(global_batch_size)])
 
@@ -1049,7 +1077,7 @@ def test_demo_text(
                 out_tok[0] = token_acc.collect_predicted_tokens(out_tok[0].item())
 
             # Run decode forward
-            logits = generator.decode_forward_text(
+            logits, log_probs = generator.decode_forward_text(
                 out_tok,
                 current_pos,
                 enable_trace=enable_trace,
