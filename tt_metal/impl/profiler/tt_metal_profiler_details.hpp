00d779a46b2fcbd864d4dd9e2305a04b1e07ce9a
riverwuTT <riverwu@tenstorrent.com>
2025-12-12 19:48:51 +0000

    fix doc format

--- tt_metal/impl/profiler/tt_metal_profiler_details.hpp
+++ tt_metal/impl/profiler/tt_metal_profiler_details.hpp
@@ -13,53 +13,55 @@ class IDevice;
 
 namespace detail {
 
+// clang-format off
 /**
  * Clear profiler control buffer
  *
  * Return value: void
  *
- * | Argument      | Description                                                        | Type            | Valid Range
- * | Required |
- * |---------------|--------------------------------------------------------------------|-----------------|---------------------------|----------|
- * | device        | Clear profiler control buffer before any core attempts to profler  | IDevice*        | | True     |
+ * | Argument | Description                                                         | Type     | Valid Range | Required |
+ * |----------|---------------------------------------------------------------------|----------|-------------|----------|
+ * | device   | Clear profiler control buffer before any core attempts to profiler  | IDevice* |             | True     |
  * */
+// clang-format on
 void ClearProfilerControlBuffer(IDevice* device);
 
+// clang-format off
 /**
  * Initialize device profiling data buffers
  *
  * Return value: void
  *
- * | Argument      | Description                                       | Type            | Valid Range               |
- * Required |
- * |---------------|---------------------------------------------------|-----------------|---------------------------|----------|
- * | device        | The device holding the program being profiled.    | IDevice*        |                           |
- * True     |
+ * | Argument | Description                                    | Type     | Valid Range | Required |
+ * |----------|------------------------------------------------|----------|-------------|----------|
+ * | device   | The device holding the program being profiled. | IDevice* |             | True     |
  * */
+// clang-format on
 void InitDeviceProfiler(IDevice* device);
 
+// clang-format off
 /**
  * Set the directory for device-side CSV logs produced by the profiler instance in the tt-metal module
  *
  * Return value: void
  *
- * | Argument     | Description                                             |  Data type  | Valid range              |
- * required |
- * |--------------|---------------------------------------------------------|-------------|--------------------------|----------|
- * | output_dir   | The output directory that will hold the output CSV logs  | std::string | Any valid directory path |
- * No       |
+ * | Argument   | Description                                              | Data type   | Valid range              | Required |
+ * |------------|----------------------------------------------------------|-------------|--------------------------|----------|
+ * | output_dir | The output directory that will hold the output CSV logs  | std::string | Any valid directory path | No       |
  * */
+// clang-format on
 void SetDeviceProfilerDir(const std::string& output_dir = "");
 
+// clang-format off
 /**
  * Start a fresh log for the device side profile results
  *
  * Return value: void
  *
- * | Argument     | Description                                             |  Data type  | Valid range              |
- * required |
- * |--------------|---------------------------------------------------------|-------------|--------------------------|----------|
+ * | Argument | Description | Data type | Valid range | Required |
+ * |----------|-------------|-----------|-------------|----------|
  * */
+// clang-format on
 void FreshProfilerDeviceLog();
 
 }  // namespace detail
