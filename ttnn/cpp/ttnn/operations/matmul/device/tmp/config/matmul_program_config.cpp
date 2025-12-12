6069e06af0c37b2af3c563601d9eada067cd1c0d
Aliaksei Sala <ctr-asala@ext.tenstorrent.com>
2025-12-12 14:20:01 -0700

    Get breaking  changes from main

--- ttnn/cpp/ttnn/operations/matmul/device/tmp/config/matmul_program_config.cpp
+++ ttnn/cpp/ttnn/operations/matmul/device/tmp/config/matmul_program_config.cpp
@@ -483,8 +483,8 @@ MatmulProgramConfig create_matmul_program_config(
                 compute_kernel_config,
                 output_dtype);
         }
-        uint32_t k = a_shape[-1] / ttnn::TILE_SIZE;
-        uint32_t n = b_shape[-1] / ttnn::TILE_SIZE;
+        uint32_t k = a_padded_shape[-1] / ttnn::TILE_SIZE;
+        uint32_t n = b_padded_shape[-1] / ttnn::TILE_SIZE;
         auto shard_shape = input_tensor_a_memory_config.shard_spec().value().shape;
         m_tiles_per_core = shard_shape[0] / ttnn::TILE_SIZE;
         n_tiles_per_core = (n * shard_shape[1]) / (k * ttnn::TILE_SIZE);
