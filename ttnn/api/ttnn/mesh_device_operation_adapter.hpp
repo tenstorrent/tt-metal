70abf9b99805d12c31d57e32e7755e78d44c129f
Artem Yerofieiev <ayerofieiev@tenstorrent.com>
2025-12-12 21:28:24 +0000

    take into account current cores and subdevice setup in hashing

--- ttnn/api/ttnn/mesh_device_operation_adapter.hpp
+++ ttnn/api/ttnn/mesh_device_operation_adapter.hpp
@@ -149,33 +149,16 @@ struct MeshDeviceOperationAdapter {
         for (const auto& coord : mesh_device_operation_utils::extract_tensor_coordinates(tensor_args, mesh_device)) {
             ttsl::hash::hash_combine(hash, coord);
         }
-        return hash;
-    }
 
-    static tt::tt_metal::operation::OpPerformanceModelGeneral<tensor_return_value_t> create_op_performance_model(
-        const operation_attributes_t& attributes,
-        const tensor_args_t& tensor_args,
-        tensor_return_value_t& tensor_return_value) {
-        if constexpr (requires {
-                          device_operation_t::create_op_performance_model(attributes, tensor_args, tensor_return_value);
-                      }) {
-            // Custom Performance Model detected for this Op
-            return device_operation_t::create_op_performance_model(attributes, tensor_args, tensor_return_value);
-        } else {
-            // Use generic Op Performance Models
-            if constexpr (requires { tensor_args.input_tensors; }) {
-                // tensor_args_t for Op contains input_tensors attributes (mirror what's done in
-                // OldInfraDeviceOperation)
-                return tt::tt_metal::operation::OpPerformanceModelGeneral(
-                    tensor_args.input_tensors,
-                    tensor_return_value,
-                    1 /* ideal_compute_cycles: specify as 1, since op perf model is not provided*/);
-            } else {
-                // tensor_args_t does not comply with interface used by OldInfraDeviceOperation, use default performance
-                // model
-                return tt::tt_metal::operation::OpPerformanceModelGeneral<tensor_return_value_t>{};
-            }
-        }
+        auto compute_cores = mesh_device->get_compute_cores();
+        auto sub_device_manager_id = mesh_device->get_active_sub_device_manager_id();
+
+        log_info(tt::LogOp, "Compute cores: {}", compute_cores);
+        log_info(tt::LogOp, "Sub device manager id: {}", sub_device_manager_id);
+
+        ttsl::hash::hash_combine(hash, compute_cores);
+        ttsl::hash::hash_combine(hash, sub_device_manager_id);
+        return hash;
     }
 };
 
