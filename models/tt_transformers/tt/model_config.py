a63af0173bc6f327334928ed6695d5f1959e0893
Ambrose Ling <aling@tenstorrent.com>
2025-12-12 08:11:50 +0000

    bring back filtering for p100

--- models/tt_transformers/tt/model_config.py
+++ models/tt_transformers/tt/model_config.py
@@ -1321,6 +1321,20 @@ class ModelArgs:
             max_seq_len_to_warmup, self.trace_prefill_supported_seq_lens
         )
 
+        to_warmup_seq_lens = self.filter_warmup_seq_lens(to_warmup_seq_lens)
+
+        return to_warmup_seq_lens
+
+    def filter_warmup_seq_lens(self, to_warmup_seq_lens):
+        # TODO: Add more model-specific filtering here
+        # This filtering is based on the current PR's (https://github.com/tenstorrent/tt-metal/pull/33143) sequence lengths that are used for warmup
+
+        # TODO: https://github.com/tenstorrent/tt-metal/issues/33991 - for P100 only, P150 has assert for ISL > 1K
+        if self.base_model_name == "Llama-3.1-8B" and self.device_name == "P100":
+            for seq_len in to_warmup_seq_lens:
+                if seq_len > 1024:
+                    to_warmup_seq_lens = to_warmup_seq_lens[: to_warmup_seq_lens.index(seq_len)]
+                    break
         return to_warmup_seq_lens
 
     def get_trace_prefill_supported_seq_lens(self):
