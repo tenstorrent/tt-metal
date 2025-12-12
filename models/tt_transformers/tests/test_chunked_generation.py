c163ccce04eb53fd0587850cea528b55f28e131f
Viktor Pus <viktorpus@tenstorrent.com>
2025-12-12 14:32:45 +0000

    Fixed q_chunk_size setting. Checking and documentation of SDPA q_chunk_size vs chunk_start_idx limitation. cleanup.

--- models/tt_transformers/tests/test_chunked_generation.py
+++ models/tt_transformers/tests/test_chunked_generation.py
@@ -8,6 +8,7 @@ import torch
 from loguru import logger
 
 import ttnn
+from models.common.utility_functions import comp_allclose, comp_pcc
 from models.tt_transformers.tt.common import PagedAttentionConfig, get_block_size, num_blocks_in_seq
 from models.tt_transformers.tt.generator import Generator
 from models.tt_transformers.tt.model import Transformer
@@ -133,20 +134,18 @@ def test_chunked_prefill_single_user(
     tt_kv_cache = [l.attention.layer_past for l in tt_model.layers]
     # Slice out relevant part of page table
     block_size = get_block_size(tt_kv_cache)
-    print(f"Pytest block_size: {block_size}")
     num_blocks = num_blocks_in_seq(seq_len, block_size)
-    print(f"Pytest num_blocks: {num_blocks}")
     static_page_table = static_page_table[:, :num_blocks]
 
     start_pos = 0
     logger.info("Running reference model")
-    # ref_output = reference_model(pt_prefill_input, start_pos, mode="decode")
+    ref_output = reference_model(pt_prefill_input, start_pos, mode="decode")
 
     # Run TT model, collecting various last_token_idxs
     logger.info("Running TT model")
-    for last_token_idx in (prefill_chunk_size - 1, prefill_chunk_size):
+    for last_token_idx in (prefill_chunk_size - 1, prefill_chunk_size, prefill_chunk_size + 1):
         prefill_input_trimmed = tt_prefill_input[:, : last_token_idx + 1]
-        for start_pos in (0, block_size, 1 * block_size):  # Reuse zero or one block of cache
+        for start_pos in (0, block_size, 2 * block_size):  # Reuse zero, one or two block of cache
             logger.info(f"Running TT model for last_token_idx: {last_token_idx}, start_pos: {start_pos}")
             tt_output_torch = generator.prefill_forward_text(
                 prefill_input_trimmed,
@@ -154,13 +153,10 @@ def test_chunked_prefill_single_user(
                 kv_cache=[tt_kv_cache],
                 start_pos=[start_pos],
             )
-            # ref_output_slice = ref_output[:, last_token_idx : last_token_idx + 1, :]
+            ref_output_slice = ref_output[:, last_token_idx : last_token_idx + 1, :]
 
-            # print(f"ref_output_slice: {ref_output_slice}")
-            print(f"tt_output_torch: {tt_output_torch}")
+            passing, pcc_message = comp_pcc(ref_output_slice, tt_output_torch, pcc)
 
-            # passing, pcc_message = comp_pcc(ref_output_slice, tt_output_torch, pcc)
-
-            # logger.info(comp_allclose(ref_output_slice, tt_output_torch))
-            # logger.info(f"PCC: {pcc_message}")
-            # assert passing
+            logger.info(comp_allclose(ref_output_slice, tt_output_torch))
+            logger.info(f"PCC: {pcc_message}")
+            assert passing
