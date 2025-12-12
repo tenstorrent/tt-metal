523f7d009b0f2409a4208945088c9b547083839c
Diego Gomez <dgomez@tenstorrent.com>
2025-12-12 11:58:04 -0500

    fix clang

--- ttnn/cpp/ttnn/operations/data_movement/untilize_with_unpadding/device/untilize_with_unpadding_device_operation.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/untilize_with_unpadding/device/untilize_with_unpadding_device_operation.hpp
@@ -11,6 +11,7 @@
 #include "factories/untilize_with_unpadding_multi_core_col_interleaved_program_factory.hpp"
 #include "factories/untilize_with_unpadding_multi_core_block_interleaved_program_factory.hpp"
 #include "ttnn/device_operation.hpp"
+#include "ttnn/decorators.hpp"
 #include <variant>
 
 namespace ttnn::operations::data_movement {
