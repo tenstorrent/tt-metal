698dc46587efa7a40fe085aa0729305387402a45
Jason Davies <jason@jasondavies.com>
2025-12-13 16:30:16 +0000

    Add missing typecast init.

--- ttnn/cpp/ttnn/operations/copy/typecast/device/typecast_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/copy/typecast/device/typecast_program_factory.cpp
@@ -282,6 +282,10 @@ TypecastSubgridProgramFactory::cached_program_t TypecastSubgridProgramFactory::c
     bool math_approx_mode = false;
 
     std::map<std::string, std::string> unary_defines;
+    unary_defines["TYPECAST_LLK_INIT"] = fmt::format(
+        "typecast_tile_init<{0}u, {1}u>",
+        (uint32_t)datatype_to_dataformat_converter(input_dtype),
+        (uint32_t)datatype_to_dataformat_converter(output_dtype));
     unary_defines["TYPECAST_LLK"] = fmt::format(
         "typecast_tile<{0}u, {1}u>",
         (uint32_t)datatype_to_dataformat_converter(input_dtype),
