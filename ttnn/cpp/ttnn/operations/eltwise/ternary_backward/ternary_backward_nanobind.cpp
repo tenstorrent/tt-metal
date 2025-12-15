0e22d5ae36baaab92f67e81ecf1fad18bbea04a8
ThisIsFineTM <184989147+ThisIsFineTM@users.noreply.github.com>
2025-12-14 19:00:30 -0500

    resync with main

--- ttnn/cpp/ttnn/operations/eltwise/ternary_backward/ternary_backward_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/ternary_backward/ternary_backward_nanobind.cpp
@@ -60,14 +60,6 @@ void bind_ternary_backward(
                  - 2, 3, 4
 
             {4}
-
-        Example:
-            >>> value = 1.0
-            >>> grad_tensor = ttnn.from_torch(torch.tensor([[1, 2], [3, 4]], dtype=torch.bfloat16), layout=ttnn.TILE_LAYOUT, device=device)
-            >>> tensor1 = ttnn.from_torch(torch.tensor([[1, 2], [3, 4]], dtype=torch.bfloat16, requires_grad=True), layout=ttnn.TILE_LAYOUT, device=device)
-            >>> tensor2 = ttnn.from_torch(torch.tensor([[1, 2], [3, 4]], dtype=torch.bfloat16, requires_grad=True), layout=ttnn.TILE_LAYOUT, device=device)
-            >>> tensor3 = ttnn.from_torch(torch.tensor([[1, 2], [3, 4]], dtype=torch.bfloat16, requires_grad=True), layout=ttnn.TILE_LAYOUT, device=device)
-            >>> output = {1}(grad_tensor, tensor1, tensor2, tensor3, value)
         )doc",
         operation.base_name(),
         operation.python_fully_qualified_name(),
@@ -153,15 +145,6 @@ void bind_ternary_backward_op(
                  - 2, 3, 4
 
             bfloat8_b/bfloat4_b is only supported on TILE_LAYOUT
-
-
-        Example:
-            >>> grad_tensor = ttnn.from_torch(torch.tensor([[1, 2], [3, 4]], dtype=torch.bfloat16), layout=ttnn.TILE_LAYOUT, device=device)
-            >>> tensor1 = ttnn.from_torch(torch.tensor([[1, 2], [3, 4]], dtype=torch.bfloat16, requires_grad=True), layout=ttnn.TILE_LAYOUT, device=device)
-            >>> tensor2 = ttnn.from_torch(torch.tensor([[1, 2], [3, 4]], dtype=torch.bfloat16, requires_grad=True), layout=ttnn.TILE_LAYOUT, device=device)
-            >>> tensor3 = ttnn.from_torch(torch.tensor([[1, 2], [3, 4]], dtype=torch.bfloat16, requires_grad=True), layout=ttnn.TILE_LAYOUT, device=device)
-            >>> output = {1}(grad_tensor, tensor1, tensor2, tensor3/scalar)
-
         )doc",
 
         operation.base_name(),
@@ -243,14 +226,6 @@ void bind_ternary_backward_optional_output(
                * - {3}
                  - TILE
                  - 2, 3, 4
-
-
-        Example:
-            >>> grad_tensor = ttnn.from_torch(torch.tensor([[1, 2], [3, 4]], dtype=torch.bfloat16), layout=ttnn.TILE_LAYOUT, device=device)
-            >>> tensor1 = ttnn.from_torch(torch.tensor([[1, 0], [1, 0]], dtype=torch.bfloat16, requires_grad=True), layout=ttnn.TILE_LAYOUT, device=device)
-            >>> tensor2 = ttnn.from_torch(torch.tensor([[1, 2], [3, 4]], dtype=torch.bfloat16, requires_grad=True), layout=ttnn.TILE_LAYOUT, device=device)
-            >>> tensor3 = ttnn.from_torch(torch.tensor([[1, 2], [3, 4]], dtype=torch.bfloat16, requires_grad=True), layout=ttnn.TILE_LAYOUT, device=device)
-            >>> output = {1}(grad_tensor, tensor1, tensor2, tensor3)
         )doc",
         operation.base_name(),
         operation.python_fully_qualified_name(),
--- ttnn/cpp/ttnn/operations/normalization/rmsnorm_distributed/rmsnorm_distributed_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/normalization/rmsnorm_distributed/rmsnorm_distributed_nanobind.cpp
@@ -23,32 +23,59 @@ void bind_normalization_rmsnorm_pre_all_gather_operation(nb::module_& mod) {
         mod,
         ttnn::rms_norm_pre_all_gather,
         R"doc(
-            Compute sum(:attr:`input_tensor`ˆ2) and sum(:attr:`input_tensor`) over the last dimension.
+              This operation is used in conjunction with :func:`ttnn.rms_norm_post_all_gather` to compute RMS norm on a distributed setup, where RMS norm is defined as:
 
-            Note:
-              Supported data types and layouts by tensor ::
+              .. math::
+                \text{RMS_norm}(x, \gamma, \beta, \epsilon) = \frac{x}{\sqrt{\epsilon+\frac{1}{N}\sum_{i=1}^{N}x^{2}}} \cdot \gamma + \beta
 
-              .. list-table:: input_tensor
-                :header-rows: 1
+              Where:
+                  - :math:`\gamma` and :math:`\beta` are optional scale and shift parameters
+                  - :math:`\epsilon` is a small constant
 
-                * - dtype
-                  - layout
-                * - BFLOAT16, FLOAT32, BFLOAT8_B
-                  - TILE
+              See `Root Mean Square Layer Normalization <https://arxiv.org/pdf/1910.07467>`_ for more details.
 
-              .. list-table:: residual_input_tensor
-                :header-rows: 1
+              This operation computes :math:`\sum_{}^{}x` and :math:`\sum_{}^{}x^2` over the last dimension.
+              Its output should be combined across devices with :func:`ttnn.all_gather`, then followed by :func:`ttnn.rms_norm_post_all_gather` to compute the RMS norm.
 
-                * - dtype
-                  - layout
-                * - BFLOAT16, FLOAT32, BFLOAT8_B
-                  - TILE
+              Args:
+                input_tensor (ttnn.Tensor): the input tensor.
 
-            Limitations:
-              - All tensors must be on-device.
-              - Unsharded inputs must be interleaved
-              - Sharded inputs cannot be height-sharded, padded height must equal TILE_HEIGHT (32). If :attr:`residual_input_tensor` is provided, it must match input's padded shape and sharding.
+              Keyword args:
+                dtype (ttnn.DataType, optional): the data type of the output tensor. Defaults to BFLOAT16.
+                residual_input_tensor (ttnn.Tensor, optional): the residual input tensor. Defaults to None.
+                compute_kernel_config (ttnn.DeviceComputeKernelConfig, optional): the compute kernel configuration. Defaults to None.
+                program_config (ttnn.ProgramConfig, optional): the program configuration. Defaults to None.
+                distributed_program_config (ttnn.LayerNormDistributedDefaultProgramConfig, optional): the distributed program configuration. Defaults to LayerNormDistributedDefaultProgramConfig().
+                memory_config (ttnn.MemoryConfig, optional): the memory configuration. Defaults to None.
 
+              Returns:
+                ttnn.Tensor: the output tensor.
+
+              Note:
+                Supported data types and layouts by tensor:
+
+                .. list-table:: input_tensor
+                  :header-rows: 1
+
+                  * - dtype
+                    - layout
+                  * - BFLOAT16, FLOAT32, BFLOAT8_B
+                    - TILE
+
+                .. list-table:: residual_input_tensor
+                  :header-rows: 1
+
+                  * - dtype
+                    - layout
+                  * - BFLOAT16, FLOAT32, BFLOAT8_B
+                    - TILE
+
+                Output stats tensor will in TILE layout and have dtype of BFLOAT16.
+
+              Limitations:
+                - All tensors must be on-device.
+                - Unsharded inputs must be interleaved
+                - Sharded inputs cannot be height-sharded, padded height must equal TILE_HEIGHT (32). If :attr:`residual_input_tensor` is provided, it must match input's padded shape and sharding.
               )doc",
         ttnn::nanobind_arguments_t{
             nb::arg("input_tensor"),
@@ -66,40 +93,71 @@ void bind_normalization_rmsnorm_post_all_gather_operation(nb::module_& mod) {
         mod,
         ttnn::rms_norm_post_all_gather,
         R"doc(
-            Performs the second part of a distributed RMSNorm operation normalizing the input based on the gathered statistics input.
+                This operation is used in conjunction with :func:`ttnn.rms_norm_pre_all_gather` to compute RMS norm on a distributed setup, where RMS norm is defined as:
 
-            Note:
-              Supported data types and layouts:
+                .. math::
+                  \text{RMS_norm}(x, \gamma, \beta, \epsilon) = \frac{x}{\sqrt{\epsilon+\frac{1}{N}\sum_{i=1}^{N}x^{2}}} \cdot \gamma + \beta
 
-              .. list-table:: input_tensor
-                :header-rows: 1
+                Where:
+                    - :math:`\gamma` and :math:`\beta` are optional scale and shift parameters
+                    - :math:`\epsilon` is a small constant
 
-                * - dtype
-                  - layout
-                * - BFLOAT16, BFLOAT8_B
-                  - TILE
+                See `Root Mean Square Layer Normalization <https://arxiv.org/pdf/1910.07467>`_ for more details.
 
-              .. list-table:: stats
-                :header-rows: 1
+                Performs the second part of a distributed RMSNorm operation, using the gathered statistics to compute the mean and variance, and finally normalizing the input.
+                The input :attr:`stats` tensor should be computed by first using :func:`ttnn.rms_norm_pre_all_gather` and then using :func:`ttnn.all_gather` to gather the statistics across all devices.
 
-                * - dtype
-                  - layout
-                * - BFLOAT16, BFLOAT8_B
-                  - TILE
+                Args:
+                  input_tensor (ttnn.Tensor): the input tensor.
+                  stats (ttnn.Tensor): the stats tensor.
 
-              .. list-table:: weight (gamma) and bias (beta)
-                :header-rows: 1
+                Keyword args:
+                  epsilon (float, optional): the epsilon value. Defaults to 1e-12.
+                  weight (ttnn.Tensor, optional): the weight tensor. Defaults to None.
+                  bias (ttnn.Tensor, optional): the bias tensor. Defaults to None.
+                  memory_config (ttnn.MemoryConfig, optional): the memory configuration. Defaults to None.
+                  compute_kernel_config (ttnn.DeviceComputeKernelConfig, optional): the compute kernel configuration. Defaults to None.
+                  program_config (ttnn.ProgramConfig, optional): the program configuration. Defaults to None.
+                  distributed_program_config (ttnn.LayerNormDistributedDefaultProgramConfig, optional): the distributed program configuration. Defaults to LayerNormDistributedDefaultProgramConfig().
+                  dtype (ttnn.DataType, optional): the data type of the output tensor. Defaults to None.
 
-                * - dtype
-                  - layout
-                * - BFLOAT16, FLOAT32
-                  - TILE, ROW_MAJOR
+                Returns:
+                  ttnn.Tensor: the output tensor.
 
-            Limitations:
-              - All tensors must be on-device.
-              - The last padded dim of :attr:`stats` must be a multiple of TILE_WIDTH, and its first three padded dims must match :attr:`input_tensor`.
-              - If :attr:`weight` (gamma) is provided, :attr:`bias` (beta) must also be provided. Gamma and beta must have the same layout. If this is ROW_MAJOR, last padded dim must be TILE_WIDTH.
-              - Sharded runs: inputs cannot be height-sharded; padded height must equal TILE_HEIGHT (32). When sharded, :attr:`stats` must be sharded across one core.
+                Note:
+                  Supported data types and layouts:
+
+                  .. list-table:: input_tensor
+                    :header-rows: 1
+
+                    * - dtype
+                      - layout
+                    * - BFLOAT16, BFLOAT8_B
+                      - TILE
+
+                  .. list-table:: stats
+                    :header-rows: 1
+
+                    * - dtype
+                      - layout
+                    * - BFLOAT16
+                      - TILE
+
+                  .. list-table:: weight (gamma) and bias (beta)
+                    :header-rows: 1
+
+                    * - dtype
+                      - layout
+                    * - BFLOAT16, FLOAT32
+                      - TILE, ROW_MAJOR
+
+                  Output tensor will be in TILE layout and have the same dtype as the :attr:`input_tensor`
+
+                Limitations:
+                  - All tensors must be on-device.
+                  - The last padded dim of :attr:`stats` must be a multiple of TILE_WIDTH, and its first three padded dims must match :attr:`input_tensor`.
+                  - If :attr:`weight` (gamma) is provided, :attr:`bias` (beta) must also be provided. Gamma and beta must have the same layout. If this is ROW_MAJOR, last padded dim must be TILE_WIDTH.
+                  - Sharded runs: inputs cannot be height-sharded; padded height must equal TILE_HEIGHT (32). When sharded, :attr:`stats` must be sharded across one core.
         )doc",
         ttnn::nanobind_arguments_t{
             nb::arg("input_tensor"),
