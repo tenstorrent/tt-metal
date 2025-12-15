921f007c546ef4a3e9d8667a0472b9ce9efe4b96
“Bryan <bzimmerman@tenstorrent.com>
2025-12-15 15:22:21 -0600

    try to remove finish

--- tt_metal/impl/dispatch/hardware_command_queue.cpp
+++ tt_metal/impl/dispatch/hardware_command_queue.cpp
@@ -188,6 +188,7 @@ void HWCommandQueue::set_exit_condition() {
 
 IDevice* HWCommandQueue::device() { return this->device_; }
 
+/*
 template <typename T>
 void HWCommandQueue::enqueue_command(T& command, bool blocking, tt::stl::Span<const SubDeviceId> sub_device_ids) {
     command.process();
@@ -195,6 +196,7 @@ void HWCommandQueue::enqueue_command(T& command, bool blocking, tt::stl::Span<co
         this->finish(sub_device_ids);
     }
 }
+*/
 
 CoreType HWCommandQueue::get_dispatch_core_type() {
     return MetalContext::instance().get_dispatch_core_manager().get_dispatch_core_type();
@@ -298,8 +300,10 @@ void HWCommandQueue::read_completion_queue() {
     }
 }
 
-void HWCommandQueue::finish(tt::stl::Span<const SubDeviceId> sub_device_ids) {
-    ZoneScopedN("HWCommandQueue_finish");
+
+void HWCommandQueue::finish(tt::stl::Span<const SubDeviceId> /*sub_device_ids*/) {
+    TT_FATAL(false, "HWCommandQueue::finish is disabled and should not be used.");
+/*    ZoneScopedN("HWCommandQueue_finish");
     log_debug(tt::LogDispatch, "Finish for command queue {}", this->id_);
     std::shared_ptr<Event> event = std::make_shared<Event>();
     this->enqueue_record_event(event, sub_device_ids);
@@ -326,6 +330,7 @@ void HWCommandQueue::finish(tt::stl::Span<const SubDeviceId> sub_device_ids) {
         auto& sub_device_entry = sub_device_cq_owner[*sub_device_id];
         sub_device_entry.finished(this->id_);
     }
+*/      
 }
 
 const CoreCoord& HWCommandQueue::virtual_enqueue_program_dispatch_core() const {
@@ -542,7 +547,8 @@ void HWCommandQueue::terminate() {
     TT_FATAL(!this->manager_.get_bypass_mode(), "Terminate cannot be used with tracing");
     log_debug(tt::LogDispatch, "Terminating dispatch kernels for command queue {}", this->id_);
     auto command = EnqueueTerminateCommand(this->id_, this->device_, this->manager_);
-    this->enqueue_command(command, false, {});
+    command.process();
+    //this->enqueue_command(command, false, {});
 }
 
 WorkerConfigBufferMgr& HWCommandQueue::get_config_buffer_mgr(uint32_t index) { return config_buffer_mgr_[index]; }
