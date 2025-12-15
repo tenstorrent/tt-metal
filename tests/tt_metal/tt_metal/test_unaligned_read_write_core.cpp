704cb278802b0fc93ee20fef96c512349f0977e5
awliu-TT <awliu@tenstorrent.com>
2025-12-15 22:47:49 +0000

    adding unit test for unaligned DMA reads/writes

---
+++ tests/tt_metal/tt_metal/test_unaligned_read_write_core.cpp
@@ -0,0 +1,172 @@
+// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <chrono>
+#include <errno.h>
+#include <fmt/base.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <tt-metalium/bfloat16.hpp>
+#include <tt-metalium/host_api.hpp>
+#include <tt-metalium/tt_metal.hpp>
+#include <algorithm>
+#include <cstdint>
+#include <cstring>
+#include <exception>
+#include <map>
+#include <array>
+#include <set>
+#include <memory>
+#include <utility>
+#include <variant>
+#include <vector>
+
+#include <tt_stl/assert.hpp>
+#include <tt-metalium/buffer.hpp>
+#include <tt-metalium/buffer_types.hpp>
+#include <tt-metalium/core_coord.hpp>
+#include <tt-metalium/data_types.hpp>
+#include <tt-logger/tt-logger.hpp>
+#include <tt_stl/span.hpp>
+
+namespace tt::tt_metal {
+class IDevice;
+}  // namespace tt::tt_metal
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// TODO: explain what test does
+//////////////////////////////////////////////////////////////////////////////////////////
+using std::vector;
+using namespace tt;
+
+int main(int argc, char** argv) {
+    bool pass = true;
+
+    auto* slow_dispatch_mode = getenv("TT_METAL_SLOW_DISPATCH_MODE");
+    TT_FATAL(slow_dispatch_mode, "This test only supports TT_METAL_SLOW_DISPATCH_MODE");
+
+    try {
+        ////////////////////////////////////////////////////////////////////////////
+        //                      Device Setup
+        ////////////////////////////////////////////////////////////////////////////
+        int device_id = 0;
+        tt_metal::IDevice* device = tt_metal::CreateDevice(device_id);
+
+        ////////////////////////////////////////////////////////////////////////////
+        //                      Application Setup
+        ////////////////////////////////////////////////////////////////////////////
+        uint32_t single_tile_size = 2 * 1024;
+        uint32_t num_tiles = 2048;
+        uint32_t dram_buffer_size =
+            single_tile_size * num_tiles;  // + 2;  // num_tiles of FP16_B, hard-coded in the reader/writer kernels
+        // Write Read Unaligned DRAM Interleaved Buffer
+        tt_metal::InterleavedBufferConfig dram_interleaved_buffer_config{
+            .device = device,
+            .size = dram_buffer_size,
+            .page_size = dram_buffer_size,
+            .buffer_type = tt_metal::BufferType::DRAM};
+        auto device_dram_interleaved_buffer = CreateBuffer(dram_interleaved_buffer_config);
+
+        std::vector<uint32_t> src_vec_dram_interleaved_case = create_random_vector_of_bfloat16(
+            dram_buffer_size, 100, std::chrono::system_clock::now().time_since_epoch().count());
+
+        tt_metal::detail::WriteToBuffer(device_dram_interleaved_buffer, src_vec_dram_interleaved_case);
+
+        std::vector<uint32_t> result_vec_dram_interleaved_case;
+
+        tt_metal::detail::ReadFromBuffer(device_dram_interleaved_buffer, result_vec_dram_interleaved_case);
+        pass &= (src_vec_dram_interleaved_case == result_vec_dram_interleaved_case);
+        TT_FATAL(pass, "Error");
+        log_info(LogTest, "Unaligned Read Write DRAM Interleaved Buffer Test Passed");
+
+        // Write Read Unaligned DRAM Sharded Buffer
+        CoreRangeSet shard_grid(std::set<CoreRange>({CoreRange(CoreCoord(0, 0), CoreCoord(0, 0))}));
+        auto shard_spec = tt_metal::ShardSpecBuffer(
+            shard_grid,
+            {1, dram_buffer_size / sizeof(uint32_t)},
+            tt_metal::ShardOrientation::ROW_MAJOR,
+            {1, dram_buffer_size / sizeof(uint32_t)},
+            {1, 1});
+        auto device_dram_sharded_buffer = CreateBuffer(tt_metal::ShardedBufferConfig{
+            .device = device,
+            .size = dram_buffer_size,
+            .page_size = dram_buffer_size,
+            .buffer_type = tt_metal::BufferType::DRAM,
+            .buffer_layout = tt_metal::TensorMemoryLayout::HEIGHT_SHARDED,
+            .shard_parameters = shard_spec});
+
+        std::vector<uint32_t> src_vec_dram_sharded_case = create_random_vector_of_bfloat16(
+            dram_buffer_size, 200, std::chrono::system_clock::now().time_since_epoch().count());
+
+        tt_metal::detail::WriteToBuffer(device_dram_sharded_buffer, src_vec_dram_sharded_case);
+
+        std::vector<uint32_t> result_vec_dram_sharded_case;
+        tt_metal::detail::ReadFromBuffer(device_dram_sharded_buffer, result_vec_dram_sharded_case);
+        pass &= (src_vec_dram_sharded_case == result_vec_dram_sharded_case);
+        TT_FATAL(pass, "Error");
+        log_info(LogTest, "Unaligned Read Write DRAM Sharded Buffer Test Passed");
+        // Write Read Unaligned L1 Interleaved Buffer
+        uint32_t l1_buffer_size = single_tile_size * 4;
+        tt_metal::InterleavedBufferConfig l1_interleaved_buffer_config{
+            .device = device,
+            .size = l1_buffer_size,
+            .page_size = l1_buffer_size,
+            .buffer_type = tt_metal::BufferType::L1};
+        auto device_l1_interleaved_buffer = CreateBuffer(l1_interleaved_buffer_config);
+
+        std::vector<uint32_t> src_vec_l1_interleaved_case = create_random_vector_of_bfloat16(
+            l1_buffer_size, 300, std::chrono::system_clock::now().time_since_epoch().count());
+
+        tt_metal::detail::WriteToBuffer(device_l1_interleaved_buffer, src_vec_l1_interleaved_case);
+
+        std::vector<uint32_t> result_vec_l1_interleaved_case;
+        tt_metal::detail::ReadFromBuffer(device_l1_interleaved_buffer, result_vec_l1_interleaved_case);
+        pass &= (src_vec_l1_interleaved_case == result_vec_l1_interleaved_case);
+        TT_FATAL(pass, "Error");
+        log_info(LogTest, "Unaligned Read Write L1 Interleaved Buffer Test Passed");
+        // Write Read Unaligned L1 Sharded Buffer
+        CoreRangeSet l1_shard_grid(std::set<CoreRange>({CoreRange(CoreCoord(0, 0), CoreCoord(0, 0))}));
+        auto l1_shard_spec = tt_metal::ShardSpecBuffer(
+            l1_shard_grid,
+            {1, l1_buffer_size / sizeof(uint32_t)},
+            tt_metal::ShardOrientation::ROW_MAJOR,
+            {1, l1_buffer_size / sizeof(uint32_t)},
+            {1, 1});
+        auto device_l1_sharded_buffer = CreateBuffer(tt_metal::ShardedBufferConfig{
+            .device = device,
+            .size = l1_buffer_size,
+            .page_size = l1_buffer_size,
+            .buffer_type = tt_metal::BufferType::L1,
+            .buffer_layout = tt_metal::TensorMemoryLayout::HEIGHT_SHARDED,
+            .shard_parameters = l1_shard_spec});
+
+        std::vector<uint32_t> src_vec_l1_sharded_case = create_random_vector_of_bfloat16(
+            l1_buffer_size, 400, std::chrono::system_clock::now().time_since_epoch().count());
+
+        tt_metal::detail::WriteToBuffer(device_l1_sharded_buffer, src_vec_l1_sharded_case);
+
+        std::vector<uint32_t> result_vec_l1_sharded_case;
+        tt_metal::detail::ReadFromBuffer(device_l1_sharded_buffer, result_vec_l1_sharded_case);
+        pass &= (src_vec_l1_sharded_case == result_vec_l1_sharded_case);
+        TT_FATAL(pass, "Error");
+        log_info(LogTest, "Unaligned Read Write L1 Sharded Buffer Test Passed");
+        pass &= tt_metal::CloseDevice(device);
+
+    } catch (const std::exception& e) {
+        pass = false;
+        // Capture the exception error message
+        log_error(LogTest, "{}", e.what());
+        // Capture system call errors that may have returned from driver/kernel
+        log_error(LogTest, "System error message: {}", std::strerror(errno));
+    }
+
+    if (pass) {
+        log_info(LogTest, "Test Passed");
+    } else {
+        TT_THROW("Test Failed");
+    }
+    TT_FATAL(pass, "Error");
+
+    return 0;
+}
