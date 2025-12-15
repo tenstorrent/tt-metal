fbbbd2da8cfab49ddf43d28dd9c0813a3c3ee2bd
Djordje Ivanovic <133343976+djordje-tt@users.noreply.github.com>
2025-12-15 17:43:38 +0100

    Revert "Adding welford support, Gamma beta Tile support, Sliding window Compu…"
    This reverts commit 45fd2811f884fe1576683113cfe83f4e045daa26.

--- ttnn/ttnn/__init__.py
+++ ttnn/ttnn/__init__.py
@@ -353,6 +353,7 @@ from ttnn.operations.normalization import (
     SoftmaxShardedMultiCoreProgramConfig,
     LayerNormDefaultProgramConfig,
     LayerNormShardedMultiCoreProgramConfig,
+    LayerNormDistributedDefaultProgramConfig,
     create_group_norm_input_mask,
     create_group_norm_input_negative_mask,
     create_group_norm_weight_bias_rm,
