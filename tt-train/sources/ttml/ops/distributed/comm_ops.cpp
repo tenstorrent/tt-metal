d60e475df4d685e6e895524204d2833b8d1a2d48
dyurshevichTT <dyurshevich@tenstorrent.com>
2025-12-15 17:35:18 +0000

    Add linear regression TP/DP examples and related distributed updates

--- tt-train/sources/ttml/ops/distributed/comm_ops.cpp
+++ tt-train/sources/ttml/ops/distributed/comm_ops.cpp
@@ -13,33 +13,33 @@
 
 namespace ttml::ops::distributed {
 
-autograd::TensorPtr reduce_scatter(const autograd::TensorPtr& tensor, int dim) {
-    auto out = autograd::create_tensor(ttnn_fixed::distributed::reduce_scatter(tensor->get_value(), dim));
-    autograd::GradFunction grad = [tensor, out, dim]() {
-        tensor->add_grad(ttnn_fixed::distributed::all_gather(out->get_grad(), dim));
+autograd::TensorPtr reduce_scatter(const autograd::TensorPtr& tensor, int dim, std::optional<uint32_t> cluster_axis) {
+    auto out = autograd::create_tensor(ttnn_fixed::distributed::reduce_scatter(tensor->get_value(), dim, cluster_axis));
+    autograd::GradFunction grad = [tensor, out, dim, cluster_axis]() {
+        tensor->add_grad(ttnn_fixed::distributed::all_gather(out->get_grad(), dim, cluster_axis));
     };
     auto links = autograd::get_links(tensor);
     out->set_node(autograd::ctx().add_backward_node(std::move(grad), links));
     return out;
 }
 
-autograd::TensorPtr all_gather(const autograd::TensorPtr& tensor, int dim) {
-    auto out = autograd::create_tensor(ttnn_fixed::distributed::all_gather(tensor->get_value(), dim));
-    autograd::GradFunction grad = [tensor, out, dim]() {
-        tensor->add_grad(ttnn_fixed::distributed::reduce_scatter(out->get_grad(), dim));
+autograd::TensorPtr all_gather(const autograd::TensorPtr& tensor, int dim, std::optional<uint32_t> cluster_axis) {
+    auto out = autograd::create_tensor(ttnn_fixed::distributed::all_gather(tensor->get_value(), dim, cluster_axis));
+    autograd::GradFunction grad = [tensor, out, dim, cluster_axis]() {
+        tensor->add_grad(ttnn_fixed::distributed::reduce_scatter(out->get_grad(), dim, cluster_axis));
     };
     auto links = autograd::get_links(tensor);
     out->set_node(autograd::ctx().add_backward_node(std::move(grad), links));
     return out;
 }
 
-autograd::TensorPtr all_reduce(const autograd::TensorPtr& tensor, bool noop_backward) {
-    auto out = autograd::create_tensor(ttnn_fixed::distributed::all_reduce(tensor->get_value()));
-    autograd::GradFunction grad = [tensor, out, noop_backward]() {
+autograd::TensorPtr all_reduce(const autograd::TensorPtr& tensor, bool noop_backward, std::optional<uint32_t> cluster_axis) {
+    auto out = autograd::create_tensor(ttnn_fixed::distributed::all_reduce(tensor->get_value(), cluster_axis));
+    autograd::GradFunction grad = [tensor, out, noop_backward, cluster_axis]() {
         if (noop_backward) {
             tensor->add_grad(out->get_grad());
         } else {
-            tensor->add_grad(ttnn_fixed::distributed::all_reduce(out->get_grad()));
+            tensor->add_grad(ttnn_fixed::distributed::all_reduce(out->get_grad(), cluster_axis));
         }
     };
     auto links = autograd::get_links(tensor);
@@ -47,10 +47,10 @@ autograd::TensorPtr all_reduce(const autograd::TensorPtr& tensor, bool noop_back
     return out;
 }
 
-autograd::TensorPtr broadcast(const autograd::TensorPtr& tensor) {
+autograd::TensorPtr broadcast(const autograd::TensorPtr& tensor, std::optional<uint32_t> cluster_axis) {
     auto out = autograd::create_tensor(tensor->get_value());
-    autograd::GradFunction grad = [tensor, out]() {
-        tensor->add_grad(ttnn_fixed::distributed::all_reduce(out->get_grad()));
+    autograd::GradFunction grad = [tensor, out, cluster_axis]() {
+        tensor->add_grad(ttnn_fixed::distributed::all_reduce(out->get_grad(), cluster_axis));
     };
     auto links = autograd::get_links(tensor);
     out->set_node(autograd::ctx().add_backward_node(std::move(grad), links));
