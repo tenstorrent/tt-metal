f28e6c82e31b07677e58cbeffca3753b52eb52a0
awliu-TT <awliu@tenstorrent.com>
2025-12-17 01:09:45 +0000

    changed TT_FATAL to warning in case where api is called before device opens

--- tt_metal/impl/profiler/tt_metal_profiler.cpp
+++ tt_metal/impl/profiler/tt_metal_profiler.cpp
@@ -1059,7 +1059,14 @@ std::map<ChipId, std::set<ProgramAnalysisData>> GetLatestProgramsPerfData() {
 
     const std::unique_ptr<ProfilerStateManager>& profiler_state_manager =
         MetalContext::instance().profiler_state_manager();
-    TT_FATAL(profiler_state_manager != nullptr, "Profiler state manager is nullptr");
+
+    if (profiler_state_manager == nullptr) {
+        log_warning(
+            tt::LogMetal,
+            "Profiler state manager is nullptr. Either enable profiling or open the device before calling this API.");
+        return {};
+    }
+
     for (const auto& [device_id, device_programs_perf_analyses] :
          profiler_state_manager->device_programs_perf_analyses_map) {
         if (device_programs_perf_analyses.empty()) {
@@ -1084,7 +1091,13 @@ std::map<ChipId, std::set<ProgramAnalysisData>> GetAllProgramsPerfData() {
 
     const std::unique_ptr<ProfilerStateManager>& profiler_state_manager =
         MetalContext::instance().profiler_state_manager();
-    TT_FATAL(profiler_state_manager != nullptr, "Profiler state manager is nullptr");
+
+    if (profiler_state_manager == nullptr) {
+        log_warning(
+            tt::LogMetal,
+            "Profiler state manager is nullptr. Either enable profiling or open the device before calling this API.");
+        return {};
+    }
 
     for (const auto& [device_id, device_programs_perf_analyses] :
          profiler_state_manager->device_programs_perf_analyses_map) {
