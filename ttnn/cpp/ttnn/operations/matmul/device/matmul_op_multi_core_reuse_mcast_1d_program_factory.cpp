a3ca17c8fbaf2bdc08f6ec400ae5719da0cd03a2
Borys Bradel <164946524+bbradelTT@users.noreply.github.com>
2025-12-18 10:34:53 -0500

    34413: remove incorrect arg override if condition for ring matmul (#34662)
    ### Ticket
    Link to Github Issue #34413
    
    ### Problem description
    override_gather_in0_program_parameters had an incorrect condition for an
    if statement
    
    The code that creates the program unconditionally added a buffer address
    to the runtime arg:
    ```
            std::vector<uint32_t> mm_in1_args = {
                (std::uint32_t)core_type,
                in1_buffer->address(),  // in1_tensor_addr
                i,                      // ring_idx
            };
    ```
    
    ### What's changed
    - Remove the condition
    - Add a unit test
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    https://github.com/tenstorrent/tt-metal/actions/runs/20340959471
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable)
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-quick.yaml)
    CI passes (if applicable)
    - [ ] [Galaxy demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [ ]
    [cpp-unit-tests](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml)
    job passes
    - [ ] New/Existing tests provide coverage for changes

--- ttnn/cpp/ttnn/operations/matmul/device/matmul_op_multi_core_reuse_mcast_1d_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/matmul/device/matmul_op_multi_core_reuse_mcast_1d_program_factory.cpp
@@ -2589,16 +2589,13 @@ inline void override_gather_in0_program_parameters(
             UpdateDynamicCircularBufferAddress(program, cb_output, *out_buffer);
         }
     }
+    auto& writer_runtime_args_by_core = GetRuntimeArgs(program, override_variables.kernels.at(0));
+    for (uint32_t i = 0; i < override_variables.cores.size(); ++i) {
+        const auto& core = override_variables.cores[i];
+        auto& writer_runtime_args = writer_runtime_args_by_core[core.x][core.y];
 
-    if (not src1_sharded) {
-        auto& writer_runtime_args_by_core = GetRuntimeArgs(program, override_variables.kernels.at(0));
-        for (uint32_t i = 0; i < override_variables.cores.size(); ++i) {
-            const auto& core = override_variables.cores[i];
-            auto& writer_runtime_args = writer_runtime_args_by_core[core.x][core.y];
-
-            /* in1 */
-            writer_runtime_args[1] = src_buffer_b->address();
-        }
+        /* in1 */
+        writer_runtime_args[1] = src_buffer_b->address();
     }
 }
 
