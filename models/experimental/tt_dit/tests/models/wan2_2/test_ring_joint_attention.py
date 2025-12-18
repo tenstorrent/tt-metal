41f8a8a9cdfa27248f446b381387412047643ecc
Colman Glagovich <cglagovich@tenstorrent.com>
2025-12-18 13:53:45 +0000

    Update performance test to sweep shapes for each model config

--- models/experimental/tt_dit/tests/models/wan2_2/test_ring_joint_attention.py
+++ models/experimental/tt_dit/tests/models/wan2_2/test_ring_joint_attention.py
@@ -11,30 +11,32 @@ from models.experimental.tt_dit.utils.padding import get_padded_vision_seq_len
 from tracy.process_model_log import run_device_profiler, post_process_ops_log
 
 
-model_input_shapes = [
-    (1, 40, 75600, 0, 128),
-    (1, 40, 32760, 0, 128),
-    (1, 40, 27280, 0, 128),
-    (1, 24, 44520, 118, 128),
-    (1, 24, 4096, 512, 128),
-    (1, 38, 4096, 333, 64),
-    (1, 24, 4224, 128, 128),
+benchmark_model_input_shapes = [
+    # shape (b, nh, base_seq_len, joint_seq_len, d), parallel_config (rp_axis, rp_factor, up_axis, up_factor)
+    [(1, 40, 32760, 0, 128), (0, 2, 1, 4)],
+    [(1, 24, 44520, 118, 128), (0, 2, 1, 4)],
+    # [(1, 24, 4096, 512, 128), (0, 2, 1, 4)],
+    # [(1, 38, 4096, 333, 64), (0, 2, 1, 2)],
 ]
 
-model_input_ids = ["wan_14b_720p", "wan_14b_480p", "wan_5b_720p", "mochi", "flux", "sd35", "uneven"]
+benchmark_model_input_ids = [
+    "wan_14b_480p",
+    "mochi",
+    # "flux",
+    # "sd35",
+]
 
 
-@pytest.mark.parametrize("dtype", [ttnn.bfloat16], ids=["bf16"])
 @pytest.mark.parametrize(
-    "b, nh, base_seq_len, joint_seq_len, d",
-    model_input_shapes,
-    ids=model_input_ids,
+    "model_input_shape, parallel_config",
+    benchmark_model_input_shapes,
+    ids=benchmark_model_input_ids,
 )
-@pytest.mark.parametrize("q_chunk_size", [32, 64, 128, 256], ids=["q32", "q64", "q128", "q256"])
-@pytest.mark.parametrize("k_chunk_size", [32, 64, 128, 256], ids=["k32", "k64", "k128", "k256"])
-@pytest.mark.parametrize(
-    "n_iters, trace_enabled, skip_check", [(1, False, False), (10, True, True)], ids=["no_trace", "yes_trace"]
-)
+@pytest.mark.parametrize("q_chunk_size", [64, 128, 256], ids=["q64", "q128", "q256"])
+@pytest.mark.parametrize("k_chunk_size", [64, 128, 256, 512], ids=["k64", "k128", "k256", "k512"])
+# @pytest.mark.parametrize("q_chunk_size", [64], ids=["q64"])
+# @pytest.mark.parametrize("k_chunk_size", [64], ids=["k64"])
+@pytest.mark.parametrize("n_iters, trace_enabled, skip_check", [(1, False, False)], ids=["no_trace"])
 @pytest.mark.parametrize("num_links", [1, 2, 4], ids=["1link", "2link", "4link"])
 @pytest.mark.parametrize(
     "device_params, all_gather_topology",
@@ -55,40 +57,22 @@ model_input_ids = ["wan_14b_720p", "wan_14b_480p", "wan_5b_720p", "mochi", "flux
     ids=["8x4", "2x4"],
     indirect=True,
 )
-@pytest.mark.parametrize(
-    "rp_axis, rp_factor, up_axis, up_factor",
-    [
-        [0, 8, 1, 4],  # 8x4 RP x UP
-        [0, 8, 1, 1],
-        [1, 4, 0, 2],
-    ],
-    ids=[
-        "8rpx4up",
-        "8rpx1up",
-        "4rpx2up",
-    ],
-)
 def test_ring_joint_sdpa(
     mesh_device,
-    b,
-    nh,
-    base_seq_len,
-    joint_seq_len,
-    d,
+    model_input_shape,
+    parallel_config,
     q_chunk_size,
     k_chunk_size,
-    dtype,
     n_iters,
     trace_enabled,
     num_links,
-    rp_axis,
-    rp_factor,
-    up_axis,
-    up_factor,
     all_gather_topology,
     skip_check,
     reset_seeds,
 ):
+    dtype = ttnn.bfloat16
+    b, nh, base_seq_len, joint_seq_len, d = model_input_shape
+    rp_axis, rp_factor, up_axis, up_factor = parallel_config
     mesh_device_shape = list(mesh_device.shape)
     assert mesh_device_shape[rp_axis] >= rp_factor and mesh_device_shape[up_axis] >= up_factor
 
@@ -126,33 +110,22 @@ def test_ring_joint_sdpa(
     [(8, 4), (2, 4)],
     ids=["8x4", "2x4"],
 )
-@pytest.mark.parametrize(
-    "rp_axis, rp_factor, up_axis, up_factor",
-    [
-        [0, 8, 1, 4],  # 8x4 RP x UP
-        [0, 8, 1, 1],
-        [1, 4, 0, 2],
-    ],
-    ids=[
-        "8rpx4up",
-        "8rpx1up",
-        "4rpx2up",
-    ],
-)
 @pytest.mark.parametrize("num_links", [1, 2, 4], ids=["1link", "2link", "4link"])
-def test_ring_joint_sdpa_perf_table(mesh_device_shape, rp_axis, rp_factor, up_axis, up_factor, num_links):
+def test_ring_joint_sdpa_perf_table(mesh_device_shape, num_links):
     results = []
-    for model_input_shape, model_input_id in zip(model_input_shapes, model_input_ids):
-        b, nh, base_seq_len, joint_seq_len, d = model_input_shape
-
-        parallel_config = f"{rp_factor}rpx{up_factor}up"
+    for (model_input_shape, parallel_config), model_input_id in zip(
+        benchmark_model_input_shapes, benchmark_model_input_ids
+    ):
+        rp_axis, rp_factor, up_axis, up_factor = parallel_config
+        parallel_name = f"{rp_factor}rpx{up_factor}up"
         mesh_name = f"{mesh_device_shape[0]}x{mesh_device_shape[1]}"
-        k_expr = f"{model_input_id} and bf16 and {parallel_config} and {mesh_name} and {num_links}link and no_trace"
+        k_expr = f"{model_input_id} and {mesh_name} and {num_links}link and no_trace"
         command = f"-m 'pytest models/experimental/tt_dit/tests/models/wan2_2/test_ring_joint_attention.py::test_ring_joint_sdpa -k \"{k_expr}\"'"
 
         run_device_profiler(
             command,
             "ring_joint_sdpa",
+            check_test_return_code=False,
             device_analysis_types=["device_kernel_duration"],
             is_command_binary_exe=True,
         )
@@ -161,18 +134,20 @@ def test_ring_joint_sdpa_perf_table(mesh_device_shape, rp_axis, rp_factor, up_ax
         durations = r["DEVICE KERNEL DURATION [ns]"].tolist()
         result = sorted(zip(durations, attrs), key=lambda x: x[0])[0]
         best_duration, best_attrs = result
-        results.append([model_input_shape, best_duration, best_attrs])
+        results.append([model_input_shape, model_input_id, parallel_name, best_duration, best_attrs])
 
-    header = "| model_input_shape | padded seq | qchunk, kchunk | measured perf (ms) |"
-    sep = "|---:|---:|---:|---:|"
+    header = "| model_input_id | model_input_shape | parallel_name | padded seq | qchunk, kchunk | measured perf (ms) |"
+    sep = "|---:|---:|---:|---:|---:|---:|"
     print(header)
     print(sep)
     for result in results:
-        model_input_shape, duration, attrs = result
+        model_input_shape, model_input_id, parallel_name, duration, attrs = result
         q_chunk = attrs.split("q_chunk_size=")[1].split(";")[0]
         k_chunk = attrs.split("k_chunk_size=")[1].split(";")[0]
-        new_seqlen = get_padded_vision_seq_len(int(model_input_shape[2]), max(int(q_chunk), int(k_chunk)), rp_factor)
-        print(f"| {model_input_shape} | {new_seqlen} | {q_chunk}, {k_chunk} | {duration / 1e6:.3f} |")
+        new_seqlen = get_padded_vision_seq_len(int(model_input_shape[2]), rp_factor)
+        print(
+            f"| {model_input_id} | {model_input_shape} | {parallel_name} | {new_seqlen} | {q_chunk}, {k_chunk} | {duration / 1e6:.3f} |"
+        )
 
 
 model_input_shapes = [
@@ -307,3 +282,87 @@ def test_ring_joint_sdpa_shapes(
         skip_check,
         0.999,
     )
+
+
+# @pytest.mark.parametrize(
+#     "input_shape, parallel_config, chunk_sizes",
+#     # input_shape: (b, nh, base_seq_len, joint_seq_len, d)
+#     # parallel_config: (rp_axis, rp_factor, up_axis, up_factor)
+#     # chunk_sizes: (q_chunk_size, k_chunk_size)
+#     [
+#         [],
+#         [],
+#         [],
+#         [(1, 40, 32760, 0, 128), (0, 2, 1, 4), (...)],
+#     ],
+#     ids=[
+#         "sd35_2x2",
+#         "flux_2x4",
+#         "mochi_2x4",
+#         "wan_14b_480p_2x4",
+#     ]
+# )
+# @pytest.mark.parametrize(
+#     "device_params, all_gather_topology",
+#     [
+#         (
+#             {"worker_l1_size": 1344544, "trace_region_size": 1000000, "fabric_config": ttnn.FabricConfig.FABRIC_1D},
+#             ttnn.Topology.Linear,
+#         ),
+#     ],
+#     indirect=["device_params"],
+#     ids=[
+#         "line",
+#     ],
+# )
+# @pytest.mark.parametrize("mesh_device", [(2, 4)], ids=["2x4"], indirect=True)
+# def test_ring_joint_sdpa_dit_t3k(
+#     mesh_device,
+#     input_shape,
+#     parallel_config,
+#     chunk_sizes,
+#     all_gather_topology,
+#     reset_seeds,
+# ):
+#     b, nh, base_seq_len, joint_seq_len, d = input_shape
+#     rp_axis, rp_factor, up_axis, up_factor = parallel_config
+#     q_chunk_size, k_chunk_size = chunk_sizes
+
+#     # contants
+#     dtype = ttnn.bfloat16
+#     n_iters = 1
+#     trace_enabled = False
+#     skip_check = False
+#     num_links = 1
+#     pcc_threshold = 0.999
+
+#     mesh_device_shape = list(mesh_device.shape)
+#     assert mesh_device_shape[rp_axis] >= rp_factor and mesh_device_shape[up_axis] >= up_factor
+
+#     submesh = create_ring_joint_sdpa_submesh(mesh_device, rp_axis, rp_factor, up_axis, up_factor)
+
+#     padded_seq_len = get_padded_vision_seq_len(base_seq_len, mesh_device_shape[rp_axis])
+
+#     logger.debug(f"RP axis: {rp_axis} factor: {rp_factor}, UP axis: {up_axis} factor: {up_factor}")
+#     logger.debug(f"submesh: {submesh.shape}")
+
+#     run_ring_joint_sdpa(
+#         submesh,
+#         b,
+#         nh,
+#         base_seq_len,
+#         padded_seq_len,
+#         joint_seq_len,
+#         d,
+#         q_chunk_size,
+#         k_chunk_size,
+#         dtype,
+#         n_iters,
+#         trace_enabled,
+#         num_links,
+#         rp_axis,
+#         up_axis,
+#         all_gather_topology,
+#         skip_check,
+#         pcc_threshold,
+#     )
