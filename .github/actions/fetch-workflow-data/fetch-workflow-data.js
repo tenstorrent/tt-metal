b56e0c9763461bd23373ef971c0a6ea67fda774f
Evan Banerjee <ebanerjee@tenstorrent.com>
2025-12-20 12:07:52 +1300

    ready to merge (undid testing changes)

--- .github/actions/fetch-workflow-data/fetch-workflow-data.js
+++ .github/actions/fetch-workflow-data/fetch-workflow-data.js
@@ -107,8 +107,7 @@ async function run() {
     if (forceFresh) {
       core.info('[CACHE] force-fresh enabled, skipping artifact restoration and starting fresh');
     } else {
-      // const previousRunId = await findPreviousAggregateRun(octokit, github.context, branch);
-      const previousRunId = 20380161184
+      const previousRunId = await findPreviousAggregateRun(octokit, github.context, branch);
       if (previousRunId) {
         const restored = await restoreArtifactsFromPreviousRun(octokit, github.context, previousRunId, workspace, cutoffDate, days);
         cachedGrouped = restored.cachedGrouped;
