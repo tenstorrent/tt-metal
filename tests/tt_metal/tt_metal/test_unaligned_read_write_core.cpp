81f5b1c80019af3e2d23e35867b0394b98ec1210
Andy Wei Liu <awliu@tenstorrent.com>
2025-12-19 17:12:56 -0800

    Fixing data movement buffer alignment issues for WH DMA transactions between host and device (#32392)
    ### Ticket
    [#32348
    ](https://github.com/tenstorrent/tt-metal/issues/32348)
    ### Problem description
    The host buffer used to read tensor data from device and write to device
    does not prevent reads/writes of non-4-byte-aligned size buffer data.
    This causes issues specifically for WH silicon, since DMA operations
    here require that the data size be 4-byte-aligned.
    ### What's changed
    In `tt_metal.cpp,` instances of reading from and writing to device were
    changed to use a buffer of size that satisfies the 4-byte alignment on
    WH instead of `buffer.page_size()`. For writes when DMA is used and a
    page size is not 4-byte aligned, the page is now split into 2 chunks:
    one that is 4-byte aligned (can be written with DMA) and another with
    the remainder data (will not be written using DMA). For reads, a 4-byte
    aligned read is done from the device and the necessary data is copied
    into the host buffer. These change the buffer size used in calls to
    `read_core` and `write_core`. Additionally, a cpp unit test has been
    added to show the previously-failing cases. This unit test can be run
    with the command `TT_METAL_SLOW_DISPATCH_MODE=1
    ~/tt-metal/build/test/tt_metal/test_unaligned_read_write_core`.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/20349439864/job/58474482398)
    CI passes
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/19341359315)
    CI with demo tests
    - [x] [T3000
    Frequent](https://github.com/tenstorrent/tt-metal/actions/runs/19341410714)
    - [x] [T3000
    Demo](https://github.com/tenstorrent/tt-metal/actions/runs/19341445578)
    - [x] [Galaxy
    Quick](https://github.com/tenstorrent/tt-metal/actions/runs/19341469033)
    - [x] New/Existing tests provide coverage for changes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ tests/tt_metal/tt_metal/test_unaligned_read_write_core.cpp
@@ -0,0 +1,190 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <chrono>
+#include <errno.h>
+#include <fmt/base.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <tt-metalium/bfloat16.hpp>
+#include <tt-metalium/host_api.hpp>
+#include <tt-metalium/tt_metal.hpp>
+#include <algorithm>
+#include <cstdint>
+#include <cstring>
+#include <exception>
+#include <map>
+#include <array>
+#include <set>
+#include <memory>
+#include <utility>
+#include <variant>
+#include <vector>
+
+#include <tt_stl/assert.hpp>
+#include <tt-metalium/buffer.hpp>
+#include <tt-metalium/buffer_types.hpp>
+#include <tt-metalium/core_coord.hpp>
+#include <tt-metalium/data_types.hpp>
+#include <tt-logger/tt-logger.hpp>
+#include <tt_stl/span.hpp>
+
+namespace tt::tt_metal {
+class IDevice;
+}  // namespace tt::tt_metal
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// This test verifies that the slow dispatch path can perform device reads and writes when the page size is not a
+// multiple of the DMA alignment requirement.
+//////////////////////////////////////////////////////////////////////////////////////////
+using std::vector;
+using namespace tt;
+
+int main(int argc, char** argv) {
+    bool pass = true;
+
+    auto* slow_dispatch_mode = getenv("TT_METAL_SLOW_DISPATCH_MODE");
+    TT_FATAL(slow_dispatch_mode, "This test only supports TT_METAL_SLOW_DISPATCH_MODE");
+
+    try {
+        ////////////////////////////////////////////////////////////////////////////
+        //                      Device Setup
+        ////////////////////////////////////////////////////////////////////////////
+        int device_id = 0;
+        tt_metal::IDevice* device = tt_metal::CreateDevice(device_id);
+
+        ////////////////////////////////////////////////////////////////////////////
+        //                      Write Read Unaligned DRAM Interleaved Buffer
+        ////////////////////////////////////////////////////////////////////////////
+        uint32_t single_tile_size = 2 * 1024;
+        uint32_t num_tiles = 2048;
+        uint32_t dram_buffer_size =
+            (single_tile_size * num_tiles) +
+            2; /*** Non 4-byte aligned buffer size for BFLOAT16s. This is the point of this unaligned test. ***/
+
+        tt_metal::InterleavedBufferConfig dram_interleaved_buffer_config{
+            .device = device,
+            .size = dram_buffer_size,
+            .page_size = dram_buffer_size,
+            .buffer_type = tt_metal::BufferType::DRAM};
+        auto device_dram_interleaved_buffer = CreateBuffer(dram_interleaved_buffer_config);
+
+        std::vector<uint8_t> src_vec_dram_interleaved_case(dram_buffer_size);
+        for (auto& v : src_vec_dram_interleaved_case) {
+            v = static_cast<uint8_t>(std::rand() % 256);
+        }
+        tt_metal::detail::WriteToBuffer(device_dram_interleaved_buffer, src_vec_dram_interleaved_case);
+        std::vector<uint8_t> result_vec_dram_interleaved_case;
+        tt_metal::detail::ReadFromBuffer(device_dram_interleaved_buffer, result_vec_dram_interleaved_case);
+        pass &= (src_vec_dram_interleaved_case == result_vec_dram_interleaved_case);
+        TT_FATAL(pass, "Error");
+        log_info(LogTest, "Passed Non-4-byte-aligned Read Write DRAM Interleaved Buffer Test");
+
+        ////////////////////////////////////////////////////////////////////////////
+        //                      Write Read Unaligned DRAM Sharded Buffer
+        ////////////////////////////////////////////////////////////////////////////
+        CoreRangeSet shard_grid(std::set<CoreRange>({CoreRange(CoreCoord(0, 0), CoreCoord(0, 0))}));
+        auto shard_spec = tt_metal::ShardSpecBuffer(
+            shard_grid,
+            {1, dram_buffer_size / sizeof(uint16_t)},
+            tt_metal::ShardOrientation::ROW_MAJOR,
+            {1, dram_buffer_size / sizeof(uint16_t)},
+            {1, 1});
+        auto device_dram_sharded_buffer = CreateBuffer(tt_metal::ShardedBufferConfig{
+            .device = device,
+            .size = dram_buffer_size,
+            .page_size = dram_buffer_size,
+            .buffer_type = tt_metal::BufferType::DRAM,
+            .buffer_layout = tt_metal::TensorMemoryLayout::HEIGHT_SHARDED,
+            .shard_parameters = shard_spec});
+
+        std::vector<uint8_t> src_vec_dram_sharded_case(dram_buffer_size);
+        for (auto& v : src_vec_dram_sharded_case) {
+            v = static_cast<uint8_t>(std::rand() % 256);
+        }
+
+        tt_metal::detail::WriteToBuffer(device_dram_sharded_buffer, src_vec_dram_sharded_case);
+
+        std::vector<uint8_t> result_vec_dram_sharded_case;
+        tt_metal::detail::ReadFromBuffer(device_dram_sharded_buffer, result_vec_dram_sharded_case);
+        pass &= (src_vec_dram_sharded_case == result_vec_dram_sharded_case);
+        TT_FATAL(pass, "Error");
+        log_info(LogTest, "Passed Non-4-byte-aligned Read Write DRAM Sharded Buffer Test");
+
+        ////////////////////////////////////////////////////////////////////////////
+        //                      Write Read Unaligned L1 Interleaved Buffer
+        ////////////////////////////////////////////////////////////////////////////
+        uint32_t l1_buffer_size =
+            (single_tile_size * 4) +
+            2; /*** Non 4-byte aligned buffer size for BFLOAT16s. This is the point of this unaligned test. ***/
+        tt_metal::InterleavedBufferConfig l1_interleaved_buffer_config{
+            .device = device,
+            .size = l1_buffer_size,
+            .page_size = l1_buffer_size,
+            .buffer_type = tt_metal::BufferType::L1};
+        auto device_l1_interleaved_buffer = CreateBuffer(l1_interleaved_buffer_config);
+
+        std::vector<uint8_t> src_vec_l1_interleaved_case(l1_buffer_size);
+        for (auto& v : src_vec_l1_interleaved_case) {
+            v = static_cast<uint8_t>(std::rand() % 256);
+        }
+
+        tt_metal::detail::WriteToBuffer(device_l1_interleaved_buffer, src_vec_l1_interleaved_case);
+
+        std::vector<uint8_t> result_vec_l1_interleaved_case;
+        tt_metal::detail::ReadFromBuffer(device_l1_interleaved_buffer, result_vec_l1_interleaved_case);
+        pass &= (src_vec_l1_interleaved_case == result_vec_l1_interleaved_case);
+        TT_FATAL(pass, "Error");
+        log_info(LogTest, "Passed Non-4-byte-aligned Read Write L1 Interleaved Buffer Test");
+
+        ////////////////////////////////////////////////////////////////////////////
+        //                      Write Read Unaligned L1 Sharded Buffer
+        ////////////////////////////////////////////////////////////////////////////
+        CoreRangeSet l1_shard_grid(std::set<CoreRange>({CoreRange(CoreCoord(0, 0), CoreCoord(0, 0))}));
+        auto l1_shard_spec = tt_metal::ShardSpecBuffer(
+            l1_shard_grid,
+            {1, l1_buffer_size / sizeof(uint16_t)},
+            tt_metal::ShardOrientation::ROW_MAJOR,
+            {1, l1_buffer_size / sizeof(uint16_t)},
+            {1, 1});
+        auto device_l1_sharded_buffer = CreateBuffer(tt_metal::ShardedBufferConfig{
+            .device = device,
+            .size = l1_buffer_size,
+            .page_size = l1_buffer_size,
+            .buffer_type = tt_metal::BufferType::L1,
+            .buffer_layout = tt_metal::TensorMemoryLayout::HEIGHT_SHARDED,
+            .shard_parameters = l1_shard_spec});
+
+        std::vector<uint8_t> src_vec_l1_sharded_case(l1_buffer_size);
+        for (auto& v : src_vec_l1_sharded_case) {
+            v = static_cast<uint8_t>(std::rand() % 256);
+        }
+
+        tt_metal::detail::WriteToBuffer(device_l1_sharded_buffer, src_vec_l1_sharded_case);
+
+        std::vector<uint8_t> result_vec_l1_sharded_case;
+        tt_metal::detail::ReadFromBuffer(device_l1_sharded_buffer, result_vec_l1_sharded_case);
+        pass &= (src_vec_l1_sharded_case == result_vec_l1_sharded_case);
+        TT_FATAL(pass, "Error");
+        log_info(LogTest, "Passed Non-4-byte-aligned Read Write L1 Sharded Buffer Test");
+
+        pass &= tt_metal::CloseDevice(device);
+
+    } catch (const std::exception& e) {
+        pass = false;
+        // Capture the exception error message
+        log_error(LogTest, "{}", e.what());
+        // Capture system call errors that may have returned from driver/kernel
+        log_error(LogTest, "System error message: {}", std::strerror(errno));
+    }
+
+    if (pass) {
+        log_info(LogTest, "Test Passed");
+    } else {
+        TT_THROW("Test Failed");
+    }
+    TT_FATAL(pass, "Error");
+
+    return 0;
+}
