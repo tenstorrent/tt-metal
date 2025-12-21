d11c8dd1211788f241202ada85aaa4f1378439a4
Filip Vranic <fvranic@tenstorrent.com>
2025-12-21 21:53:38 +0100

    fix: revert "instrn-buffer TOS (#929)" (#967)
    ### Ticket
    None
    
    ### Problem description
    Previous commit caused regression in perf tests. See
    [here](https://github.com/tenstorrent/tt-metal/actions/runs/20399421007/job/58619788251)
    for more information.
    FYI @nathan-TT 
    
    ### What's changed
    We decided to revert and investigate further.
    This reverts commit 276a8115bae84402834d0d2589b8115ee5cc4136.
    
    ### Type of change
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)

--- tt_llk_blackhole/common/inc/ckernel.h
+++ tt_llk_blackhole/common/inc/ckernel.h
@@ -64,8 +64,13 @@ constexpr uint KERNEL_COMPLETE    = 1;
 extern volatile uint tt_reg_ptr *reg_base;
 extern volatile uint tt_reg_ptr *pc_buf_base;
 extern volatile uint tt_reg_ptr *regfile;
-#define __INSTRN_BUFFER_TOS 1
-extern volatile uint32_t tt_reg_ptr *const instrn_buffer;
+} // namespace ckernel
+
+extern volatile uint32_t __instrn_buffer[];
+
+namespace ckernel
+{
+constexpr inline volatile uint32_t(tt_reg_ptr &instrn_buffer)[] = __instrn_buffer;
 extern volatile uint tt_reg_ptr *mailbox_base[4];
 
 extern uint32_t cfg_state_id;
