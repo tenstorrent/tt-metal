faa5789923a578d595aef9fea6007a0dac18cb17
Igor Michalak <imichalak@tenstorrent.com>
2025-12-22 22:17:47 +0200

    Migrate op to new infra: experimental/ccl/slice_reshard_async (#34890)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/34850)
    
    ### Problem description
    Old operation is represented by a fairly simple structure, where inputs
    and outputs are processed as vectors. So even for unary operations we do
    head allocations for inputs and the output.
    
    New template metaprogramming-based approach solves it. It is more
    verbose and allows precise input/output types, which allows us to remove
    unnecessary heap allocations.
    
    ### What's changed
    Migrate
    experimental/ccl/slice_reshard_async/device/slice_reshard_async_op.hpp
    operation to the TMP infra.
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/20435618209)
    CI in progress
    - [ ] [T3K nightly
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/20435634002)
    CI in progress

---
+++ ttnn/cpp/ttnn/operations/experimental/ccl/slice_reshard_async/device/slice_reshard_async_device_operation.cpp
@@ -0,0 +1,118 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "ttnn/operations/experimental/ccl/slice_reshard_async/device/slice_reshard_async_device_operation.hpp"
+
+#include <tt-metalium/experimental/fabric/fabric.hpp>
+#include "ttnn/global_semaphore.hpp"
+#include "ttnn/operations/functions.hpp"
+#include "ttnn/operations/math.hpp"
+#include "ttnn/tensor/tensor.hpp"
+#include "ttnn/tensor/tensor_utils.hpp"
+#include "ttnn/operations/experimental/ccl/slice_reshard_async/device/slice_reshard_async_device_operation_types.hpp"
+#include "ttnn/operations/experimental/ccl/slice_reshard_async/device/slice_reshard_async_program_factory.hpp"
+
+namespace ttnn::operations::experimental::ccl::slice_reshard_async {
+
+SliceReshardAsyncDeviceOperation::program_factory_t SliceReshardAsyncDeviceOperation::select_program_factory(
+    const operation_attributes_t& args, const tensor_args_t& tensor_args) {
+    return program::SliceReshardAsyncProgramFactory{};
+}
+
+void SliceReshardAsyncDeviceOperation::validate_on_program_cache_hit(
+    const operation_attributes_t& args, const tensor_args_t& tensor_args) {
+    validate_on_program_cache_miss(args, tensor_args);
+}
+
+void SliceReshardAsyncDeviceOperation::validate_on_program_cache_miss(
+    const operation_attributes_t& args, const tensor_args_t& tensor_args) {
+    TT_FATAL(args.dim == 0, "Error, neighbor pad currently only supports sharding dim 0, provided {}", args.dim);
+    TT_FATAL(
+        tensor_args.input.layout() == Layout::ROW_MAJOR,
+        "Unsupported input tensor layout {}.",
+        tensor_args.input.layout());
+
+    TT_FATAL(!tensor_args.input.is_sharded(), "Slice reshard does not support sharded input tensors.");
+
+    TT_FATAL(
+        !(args.output_dim_shape % args.ring_size), "Output dim shape must be divisible by num devices on cluster axis");
+
+    TT_FATAL(args.cluster_axis == 0 || args.cluster_axis == 1, "Unsupported cluster axis {}.", args.cluster_axis);
+
+    TT_FATAL(args.num_links > 0, "Error, num_links should be more than 0 but has {}", args.num_links);
+}
+
+spec_return_value_t SliceReshardAsyncDeviceOperation::compute_output_specs(
+    const operation_attributes_t& args, const tensor_args_t& tensor_args) {
+    const auto& input_tensor = tensor_args.input;
+    auto shape = input_tensor.logical_shape();
+    shape[args.dim] = args.output_dim_shape / args.ring_size;
+    return TensorSpec(
+        shape, TensorLayout(input_tensor.dtype(), input_tensor.tensor_spec().page_config(), args.output_mem_config));
+}
+
+tensor_return_value_t SliceReshardAsyncDeviceOperation::create_output_tensors(
+    const operation_attributes_t& args, const tensor_args_t& tensor_args) {
+    return create_device_tensor(compute_output_specs(args, tensor_args), tensor_args.input.device());
+}
+
+tt::stl::hash::hash_t SliceReshardAsyncDeviceOperation::compute_program_hash(
+    const operation_attributes_t& args, const tensor_args_t& tensor_args) {
+    auto input_shape = tensor_args.input.padded_shape();
+    auto input_memory_layout = tensor_args.input.layout();
+    auto input_dtype = tensor_args.input.dtype();
+    auto input_memory_config = tensor_args.input.memory_config();
+
+    return tt::tt_metal::operation::hash_operation<SliceReshardAsyncDeviceOperation>(
+        args.dim,
+        args.output_dim_offset,
+        args.output_dim_shape,
+        args.num_links,
+        args.output_mem_config,
+        args.topology,
+        args.cluster_axis,
+        args.ring_size,
+        input_shape,
+        input_memory_layout,
+        input_dtype,
+        input_memory_config);
+}
+
+std::tuple<SliceReshardAsyncDeviceOperation::operation_attributes_t, SliceReshardAsyncDeviceOperation::tensor_args_t>
+SliceReshardAsyncDeviceOperation::invoke(
+    const ttnn::Tensor& input_tensor,
+    const int32_t dim,
+    const uint32_t output_dim_offset,
+    const uint32_t output_dim_shape,
+    const uint32_t cluster_axis,
+    const GlobalSemaphore& final_semaphore,
+    const GlobalSemaphore& barrier_semaphore,
+    const size_t num_links,
+    const MemoryConfig& memory_config,
+    const ttnn::ccl::Topology topology) {
+    std::vector<IDevice*> devices = ttnn::ccl::get_active_physical_devices(input_tensor);
+    uint32_t num_devices;
+    auto* mesh_device = input_tensor.device();
+    const auto& mesh_view = mesh_device->get_view();
+    // Use the mesh dimensions to determine the ring size
+    num_devices = (cluster_axis == 0) ? mesh_view.num_rows() : mesh_view.num_cols();
+    TT_FATAL(num_devices > 1, "slice_reshard_async op will only work for num_devices > 1, but has {}", num_devices);
+
+    return {
+        operation_attributes_t(
+            devices,
+            dim,
+            output_dim_offset,
+            output_dim_shape,
+            cluster_axis,
+            final_semaphore,
+            barrier_semaphore,
+            num_links,
+            memory_config,
+            topology,
+            num_devices),
+        tensor_args_t{.input = input_tensor}};
+}
+
+}  // namespace ttnn::operations::experimental::ccl::slice_reshard_async
