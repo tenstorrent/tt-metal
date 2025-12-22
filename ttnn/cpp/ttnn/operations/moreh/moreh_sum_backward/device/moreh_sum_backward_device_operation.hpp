18baa381d2efa502292ed4cca3e024b3ef700258
Pavle Josipović <pavlejosipovic@users.noreply.github.com>
2025-12-22 13:05:02 +0100

    Enable and fix readability-inconsistent-declaration-parameter-name clang-tidy check (#34589)
    ### Ticket
    Closes [Issue
    34339](https://github.com/tenstorrent/tt-metal/issues/34339)
    
    ### Problem description
    Function parameter names inconsistent between declaration and definition
    
    ### What's changed
    Enable clang-tidy check for this and fix instances of this problem
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/20367762108)
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/20367766866)
    (same as main)
    - [x] [Merge
    gate](https://github.com/tenstorrent/tt-metal/actions/runs/20371352841/job/58538539463)
    
    ---------
    
    Co-authored-by: Pavle Josipovic <pjosipovic@tenstorrent.com>

--- ttnn/cpp/ttnn/operations/moreh/moreh_sum_backward/device/moreh_sum_backward_device_operation.hpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_sum_backward/device/moreh_sum_backward_device_operation.hpp
@@ -45,7 +45,7 @@ struct MorehSumBackwardOperation {
             cached_program_t& cached_program,
             const operation_attributes_t& operation_attributes,
             const tensor_args_t& tensor_args,
-            tensor_return_value_t& output_tensor);
+            tensor_return_value_t& tensor_return_value);
     };
 
     using program_factory_t = std::variant<ProgramFactory>;
