bfcf0f6e077904f48785d22411efb14ca3d13b7e
Mountagha <muntaghaba@gmail.com>
2025-12-22 11:06:09 -0500

    README, visualization graph...

--- models/demos/patchtsmixer/reference/patchtsmixer_comp.py
+++ models/demos/patchtsmixer/reference/patchtsmixer_comp.py
@@ -2,8 +2,8 @@ import numpy as np
 import torch
 
 # Load reference
-x_ref   = np.load("patchtsmixer_ref/ref_input_past_values.npy")  # (B, L, C)
-y_ref   = np.load("patchtsmixer_ref/ref_predictions.npy")        # (B, H, C)
+x_ref = np.load("patchtsmixer_ref/ref_input_past_values.npy")  # (B, L, C)
+y_ref = np.load("patchtsmixer_ref/ref_predictions.npy")  # (B, H, C)
 
 x_ref_t = torch.tensor(x_ref, dtype=torch.float32)
 
@@ -11,11 +11,9 @@ x_ref_t = torch.tensor(x_ref, dtype=torch.float32)
 y_ttnn = run_ttnn_patchtsmixer(x_ref_t)  # (B, H, C) from hardware
 
 # Compare
-mse = torch.mean((y_ttnn - torch.tensor(y_ref))**2).item()
+mse = torch.mean((y_ttnn - torch.tensor(y_ref)) ** 2).item()
 mae = torch.mean(torch.abs(y_ttnn - torch.tensor(y_ref))).item()
-corr = torch.corrcoef(
-    torch.stack([y_ttnn.flatten(), torch.tensor(y_ref).flatten()])
-)[0, 1].item()
+corr = torch.corrcoef(torch.stack([y_ttnn.flatten(), torch.tensor(y_ref).flatten()]))[0, 1].item()
 
 print("MSE:", mse)
 print("MAE:", mae)
