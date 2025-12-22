9e004d7bac4e1436a1042b82431bc9c9c325499a
Igor Michalak <imichalak@tenstorrent.com>
2025-12-22 22:17:47 +0200

    Migrate op to new infra: experimental/ccl/slice_reshard_async (#34890)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/34850)
    
    ### Problem description
    Old operation is represented by a fairly simple structure, where inputs
    and outputs are processed as vectors. So even for unary operations we do
    head allocations for inputs and the output.
    
    New template metaprogramming-based approach solves it. It is more
    verbose and allows precise input/output types, which allows us to remove
    unnecessary heap allocations.
    
    ### What's changed
    Migrate
    experimental/ccl/slice_reshard_async/device/slice_reshard_async_op.hpp
    operation to the TMP infra.
    
    ### Checklist
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/20435618209)
    CI in progress
    - [ ] [T3K nightly
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/20435634002)
    CI in progress

---
+++ ttnn/cpp/ttnn/operations/experimental/ccl/slice_reshard_async/device/slice_reshard_async_program_factory.hpp
@@ -0,0 +1,45 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "ttnn/operations/experimental/ccl/slice_reshard_async/device/slice_reshard_async_device_operation_types.hpp"
+
+#include "ttnn/device_operation.hpp"
+
+namespace ttnn::operations::experimental::ccl::slice_reshard_async::program {
+
+struct SliceReshardAsyncSharedVariables {
+    uint32_t num_directions = 0;
+    std::vector<tt::tt_metal::KernelHandle> reader_kernel_ids;
+    std::vector<tt::tt_metal::KernelHandle> writer_kernel_ids;
+};
+
+struct SliceReshardAsyncProgramFactory {
+    using shared_variables_t = SliceReshardAsyncSharedVariables;
+    using cached_mesh_workload_t = ttnn::device_operation::AdaptedCachedMeshWorkload<shared_variables_t>;
+
+    static cached_mesh_workload_t create_mesh_workload(
+        const operation_attributes_t& args,
+        const ttnn::MeshCoordinateRangeSet& tensor_coords,
+        const tensor_args_t& tensor_args,
+        tensor_return_value_t& tensor_return_value);
+
+    static void override_runtime_arguments(
+        cached_mesh_workload_t& cached_workload,
+        const operation_attributes_t& args,
+        const tensor_args_t& tensor_args,
+        tensor_return_value_t& tensor_return_value);
+
+private:
+    using cached_program_t = ttnn::device_operation::CachedProgram<shared_variables_t>;
+
+    static cached_program_t create_at(
+        const operation_attributes_t& args,
+        const ttnn::MeshCoordinate& mesh_coord,
+        const tensor_args_t& tensor_args,
+        tensor_return_value_t& tensor_return_value);
+};
+
+}  // namespace ttnn::operations::experimental::ccl::slice_reshard_async::program
