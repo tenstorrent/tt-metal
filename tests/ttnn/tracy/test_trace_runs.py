e812a4417d9e9df4a28cc39f252c5fa29a101b7a
mo-tenstorrent <mmemarian@tenstorrent.com>
2025-12-23 20:52:27 +0000

    Add unit test

--- tests/ttnn/tracy/test_trace_runs.py
+++ tests/ttnn/tracy/test_trace_runs.py
@@ -44,6 +44,37 @@ def test_with_ops(device):
 @pytest.mark.parametrize(
     "device_params", [{"trace_region_size": 1996800, "dispatch_core_type": ttnn.DispatchCoreType.WORKER}], indirect=True
 )
+def test_with_high_trace_replay_count(device):
+    torch.manual_seed(0)
+    m = 1024
+    k = 1024
+    n = 1024
+    torch_a = torch.randn((m, k), dtype=torch.bfloat16)
+    torch_b = torch.randn((k, n), dtype=torch.bfloat16)
+
+    a = ttnn.from_torch(torch_a)
+    b = ttnn.from_torch(torch_b)
+
+    a = ttnn.to_device(a, device, memory_config=ttnn.L1_MEMORY_CONFIG)
+    b = ttnn.to_device(b, device, memory_config=ttnn.L1_MEMORY_CONFIG)
+
+    a = ttnn.to_layout(a, ttnn.TILE_LAYOUT)
+    b = ttnn.to_layout(b, ttnn.TILE_LAYOUT)
+
+    ttnn.matmul(a, b, core_grid=ttnn.CoreGrid(y=8, x=8))
+    tid = ttnn.begin_trace_capture(device, cq_id=0)
+    for i in range(100):
+        ttnn.matmul(a, b, core_grid=ttnn.CoreGrid(y=8, x=8))
+    ttnn.end_trace_capture(device, tid, cq_id=0)
+
+    for i in range(5):
+        ttnn.execute_trace(device, tid, cq_id=0, blocking=True)
+    ttnn.release_trace(device, tid)
+
+
+@pytest.mark.parametrize(
+    "device_params", [{"trace_region_size": 1996800, "dispatch_core_type": ttnn.DispatchCoreType.WORKER}], indirect=True
+)
 @pytest.mark.parametrize("num_iterations", [(100, 5)])
 def test_with_ops_single_core(device, num_iterations):
     torch.manual_seed(0)
