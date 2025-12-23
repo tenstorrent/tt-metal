934b458e115d57aa0a5f72f3cb76bea6210d2173
Nikola Stamatovic <nstamatovic@tenstorrent.com>
2025-12-23 10:29:57 +0100

    [DM] noc api durations tests (#34523)
    ### Ticket
    [Link to Github Issue
    
    ](https://github.com/orgs/tenstorrent/projects/135/views/10?pane=issue&itemId=116830943&issue=tenstorrent%7Ctt-metal%7C24075)
    
    ### Problem description
    There are already tests that measure and map the performance of API
    calls. It is beneficial to know how many cycles it takes to issue a full
    noc transaction.
    
    ### What's changed
    These tests are using the dataflow api 2.0. We are measuring the issue
    time of different NoC api calls: Unicast read and write, both stateful
    and stateless, multicast 2x2, multicast 5x5, and multicast on full grid.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/20432231268)
    CI passes
    - [x] [(TM-DM) Data Movement Test Suite
    ](https://github.com/tenstorrent/tt-metal/actions/runs/20374884748)

---
+++ tests/tt_metal/tt_metal/data_movement/noc_api_latency/kernels/multicast_write_2_0.cpp
@@ -0,0 +1,56 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include "api/dataflow/dataflow_api.h"
+
+void kernel_main() {
+    constexpr uint32_t l1_local_addr = get_compile_time_arg_val(0);
+    constexpr uint32_t num_transactions = get_compile_time_arg_val(1);
+    constexpr uint32_t transaction_size = get_compile_time_arg_val(2);
+    constexpr uint32_t test_id = get_compile_time_arg_val(3);
+    constexpr uint32_t packed_dest_core_start = get_compile_time_arg_val(4);
+    constexpr uint32_t packed_dest_core_end = get_compile_time_arg_val(5);
+    constexpr uint32_t loopback = get_compile_time_arg_val(6);
+    constexpr uint32_t num_dests = get_compile_time_arg_val(7);
+
+    uint32_t dest_x_start = packed_dest_core_start >> 16;
+    uint32_t dest_y_start = packed_dest_core_start & 0xFFFF;
+    uint32_t dest_x_end = packed_dest_core_end >> 16;
+    uint32_t dest_y_end = packed_dest_core_end & 0xFFFF;
+
+    if (noc_index == 1) {
+        std::swap(dest_x_start, dest_x_end);
+        std::swap(dest_y_start, dest_y_end);
+    }
+
+    experimental::Noc noc(noc_index);
+    experimental::UnicastEndpoint unicast_endpoint;
+    experimental::MulticastEndpoint mcast_endpoint;
+    constexpr auto mcast_mode =
+        loopback ? experimental::Noc::McastMode::INCLUDE_SRC : experimental::Noc::McastMode::EXCLUDE_SRC;
+    {
+        DeviceZoneScopedN("RISCV0");
+#pragma GCC unroll 256
+        for (uint32_t i = 0; i < num_transactions; i++) {
+            noc.async_write_multicast<mcast_mode>(
+                unicast_endpoint,
+                mcast_endpoint,
+                transaction_size,
+                num_dests,
+                {.addr = l1_local_addr},
+                {.noc_x_start = dest_x_start,
+                 .noc_y_start = dest_y_start,
+                 .noc_x_end = dest_x_end,
+                 .noc_y_end = dest_y_end,
+                 .addr = l1_local_addr});
+        }
+    }
+
+    noc.async_write_barrier();
+
+    DeviceTimestampedData("Test id", test_id);
+    DeviceTimestampedData("NoC Index", noc.get_noc_id());
+    DeviceTimestampedData("Number of transactions", num_transactions);
+    DeviceTimestampedData("Transaction size in bytes", transaction_size);
+}
