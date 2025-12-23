a529722940a603d62f5cebedd981c40f67e39205
Suhail Alnahari <salnahari@tenstorrent.com>
2025-12-23 17:54:28 -0500

    tt_symbiote: updated CNN/Resnet Code

--- models/tt_symbiote/modules/conv.py
+++ models/tt_symbiote/modules/conv.py
@@ -6,11 +6,82 @@ from torch import nn
 import ttnn
 from models.tt_cnn.tt.builder import Conv2dConfiguration, TtConv2d
 from models.tt_symbiote.core.module import TTNNModule
-from models.tt_symbiote.modules.tensor import TTNNPermute
+from models.tt_symbiote.modules.activation import TTNNReLU
+from models.tt_symbiote.modules.tensor import TTNNPermute, TTNNReshape
+
+
+def fold_batch_norm2d_into_conv2d(weight, scale, shift, running_mean, running_var, eps):
+    weight = weight * (scale / torch.sqrt(running_var + eps))[:, None, None, None]
+    bias = shift - running_mean * (scale / torch.sqrt(running_var + eps))
+    bias = torch.reshape(bias, (-1,))
+    return weight, bias
+
+
+class NHWCConvPytorch(nn.Module):
+    """A wrapper around nn.Conv2d to handle NHWC input/output."""
+
+    def __init__(
+        self,
+        conv: nn.Conv2d,
+    ) -> None:
+        super().__init__()
+        self.conv = conv
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass through Conv2d with NHWC input/output."""
+        x = x.permute(0, 3, 1, 2)  # NHWC to NCHW
+        x = self.conv(x)
+        x = x.permute(0, 2, 3, 1)  # NCHW to NHWC
+        return x
+
+
+class NHWCConvBNPytorch(nn.Module):
+    """A wrapper around nn.Conv2d to handle NHWC input/output."""
+
+    def __init__(
+        self,
+        conv: nn.Conv2d,
+        bn: nn.BatchNorm2d,
+    ) -> None:
+        super().__init__()
+        self.conv = conv
+        self.bn = bn
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass through Conv2d with NHWC input/output."""
+        x = x.permute(0, 3, 1, 2)  # NHWC to NCHW
+        x = self.conv(x)
+        x = self.bn(x)
+        x = x.permute(0, 2, 3, 1)  # NCHW to NHWC
+        return x
+
+
+class NHWCConvBNActivationPytorch(nn.Module):
+    """A wrapper around nn.Conv2d to handle NHWC input/output."""
+
+    def __init__(
+        self,
+        conv: nn.Conv2d,
+        bn: nn.BatchNorm2d,
+        activation: nn.Module,
+    ) -> None:
+        super().__init__()
+        self.conv = conv
+        self.bn = bn
+        self.activation = activation
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass through Conv2d with NHWC input/output."""
+        x = x.permute(0, 3, 1, 2)  # NHWC to NCHW
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.activation(x)
+        x = x.permute(0, 2, 3, 1)  # NCHW to NHWC
+        return x
 
 
 class TTNNConv2dNHWC(TTNNModule):
-    """TTNN-accelerated linear layer."""
+    """TTNN-accelerated Conv layer."""
 
     def __init__(
         self,
@@ -32,6 +103,7 @@ class TTNNConv2dNHWC(TTNNModule):
         self.dilation = dilation
         self.groups = groups
         self.slice_config = slice_config
+        self.reshape = TTNNReshape()
 
     @classmethod
     def from_torch(cls, conv: nn.Conv2d, slice_config=None) -> "TTNNConv2d":
@@ -46,28 +118,25 @@ class TTNNConv2dNHWC(TTNNModule):
             groups=conv.groups,
             slice_config=slice_config,
         )
-        new_conv.torch_weight = conv.weight
-        new_conv.torch_bias = conv.bias
-        new_conv._fallback_torch_layer = conv
+        new_conv._fallback_torch_layer = NHWCConvPytorch(conv)
         return new_conv
 
     def preprocess_weights_impl(self):
         """Preprocess linear weights for TTNN."""
         if self.torch_layer is None:
-            self._fallback_torch_layer = nn.Conv2d(
-                in_channels=self.in_channels,
-                out_channels=self.out_channels,
-                kernel_size=self.kernel_size,
-                stride=self.stride,
-                padding=self.padding,
-                dilation=self.dilation,
-                groups=self.groups,
+            self._fallback_torch_layer = NHWCConvPytorch(
+                nn.Conv2d(
+                    in_channels=self.in_channels,
+                    out_channels=self.out_channels,
+                    kernel_size=self.kernel_size,
+                    stride=self.stride,
+                    padding=self.padding,
+                    dilation=self.dilation,
+                    groups=self.groups,
+                )
             )
-            self.torch_weight = self._fallback_torch_layer.weight
-            self.torch_bias = self._fallback_torch_layer.bias
-        torch_weight, torch_bias = self.torch_weight, self.torch_bias
         self.tt_weight, self.tt_bias = Conv2dConfiguration.convert_torch_weight_and_bias_to_ttnn(
-            torch_weight, torch_bias
+            self.torch_layer.conv.weight, self.torch_layer.conv.bias
         )
 
     def move_weights_to_host_impl(self):
@@ -103,19 +172,126 @@ class TTNNConv2dNHWC(TTNNModule):
         layer = TtConv2d(config, input_tensor.device())
         if reshape_output:
             out, h_w = layer(input_tensor, return_output_dim=reshape_output)
-            out = ttnn.reshape(
-                out,
-                [batch_size, h_w[0], h_w[1], -1],
-            )
+            out = self.reshape(out, [batch_size, h_w[0], h_w[1], -1])
             return out
         return layer(input_tensor)
 
 
-def fold_batch_norm2d_into_conv2d(weight, scale, shift, running_mean, running_var, eps):
-    weight = weight * (scale / torch.sqrt(running_var + eps))[:, None, None, None]
-    bias = shift - running_mean * (scale / torch.sqrt(running_var + eps))
-    bias = torch.reshape(bias, (-1,))
-    return weight, bias
+class TTNNConv2dBNNHWC(TTNNConv2dNHWC):
+    """TTNN-accelerated Fused Conv BN layer."""
+
+    @classmethod
+    def from_torch(cls, conv: nn.Conv2d, bn: nn.BatchNorm2d, slice_config=None) -> "TTNNConv2d":
+        """Create TTNNConv2d from PyTorch Conv2d layer."""
+        new_conv = TTNNConv2dBNNHWC(
+            in_channels=conv.in_channels,
+            out_channels=conv.out_channels,
+            kernel_size=conv.kernel_size,
+            stride=conv.stride,
+            padding=conv.padding,
+            dilation=conv.dilation,
+            groups=conv.groups,
+            slice_config=slice_config,
+        )
+        new_conv._fallback_torch_layer = NHWCConvBNPytorch(conv, bn)
+        return new_conv
+
+    def _preprocess_weights_local(self):
+        torch_weight, torch_bias = fold_batch_norm2d_into_conv2d(
+            self.torch_layer.conv.weight,
+            self.torch_layer.bn.weight,
+            self.torch_layer.bn.bias,
+            self.torch_layer.bn.running_mean,
+            self.torch_layer.bn.running_var,
+            self.torch_layer.bn.eps,
+        )
+        self.tt_weight, self.tt_bias = Conv2dConfiguration.convert_torch_weight_and_bias_to_ttnn(
+            torch_weight, torch_bias
+        )
+
+    def preprocess_weights_impl(self):
+        """Preprocess linear weights for TTNN."""
+        if self.torch_layer is None:
+            self._fallback_torch_layer = NHWCConvBNPytorch(
+                nn.Conv2d(
+                    in_channels=self.in_channels,
+                    out_channels=self.out_channels,
+                    kernel_size=self.kernel_size,
+                    stride=self.stride,
+                    padding=self.padding,
+                    dilation=self.dilation,
+                    groups=self.groups,
+                ),
+                nn.BatchNorm2d(self.out_channels),
+            )
+        self._preprocess_weights_local()
+
+
+class TTNNConv2dBNActivationNHWC(TTNNConv2dBNNHWC):
+    """TTNN-accelerated Fused Conv BN layer."""
+
+    @classmethod
+    def from_torch(cls, conv: nn.Conv2d, bn: nn.BatchNorm2d, activation, slice_config=None) -> "TTNNConv2d":
+        """Create TTNNConv2d from PyTorch Conv2d layer."""
+        new_conv = TTNNConv2dBNActivationNHWC(
+            in_channels=conv.in_channels,
+            out_channels=conv.out_channels,
+            kernel_size=conv.kernel_size,
+            stride=conv.stride,
+            padding=conv.padding,
+            dilation=conv.dilation,
+            groups=conv.groups,
+            slice_config=slice_config,
+        )
+        new_conv._fallback_torch_layer = NHWCConvBNActivationPytorch(conv, bn, activation)
+        assert isinstance(activation, nn.ReLU), "Only ReLU activation is supported in TTNNConv2dBNActivationNHWC."
+        return new_conv
+
+    def preprocess_weights_impl(self):
+        """Preprocess linear weights for TTNN."""
+        if self.torch_layer is None:
+            self._fallback_torch_layer = NHWCConvBNPytorch(
+                nn.Conv2d(
+                    in_channels=self.in_channels,
+                    out_channels=self.out_channels,
+                    kernel_size=self.kernel_size,
+                    stride=self.stride,
+                    padding=self.padding,
+                    dilation=self.dilation,
+                    groups=self.groups,
+                ),
+                nn.BatchNorm2d(self.out_channels),
+                nn.ReLU(),
+            )
+        self._preprocess_weights_local()
+
+    def forward(self, input_tensor: ttnn.Tensor, reshape_output=True) -> ttnn.Tensor:
+        """Forward pass through linear layer."""
+        batch_size, input_height, input_width, _ = input_tensor.shape
+        config = Conv2dConfiguration(
+            input_height=input_height,
+            input_width=input_width,
+            in_channels=self.in_channels,
+            out_channels=self.out_channels,
+            batch_size=batch_size,
+            kernel_size=self.kernel_size,
+            stride=self.stride,
+            padding=self.padding,
+            groups=self.groups,
+            dilation=self.dilation,
+            weight=self.tt_weight,
+            bias=self.tt_bias,
+            slice_strategy=self.slice_config,
+            activation=ttnn.UnaryOpType.RELU,
+            math_fidelity=ttnn.MathFidelity.HiFi2,
+            fp32_dest_acc_en=True,
+        )
+        layer = TtConv2d(config, input_tensor.device())
+        if reshape_output:
+            out, h_w = layer(input_tensor, return_output_dim=reshape_output)
+            out = self.reshape(out, [batch_size, h_w[0], h_w[1], -1])
+            return out
+        return layer(input_tensor)
 
 
 class TTNNBottleneck(TTNNModule):
@@ -132,32 +308,15 @@ class TTNNBottleneck(TTNNModule):
         assert (
             self._fallback_torch_layer is not None
         ), "Fallback torch layer must be set before initializing submodules."
-        self.conv1 = TTNNConv2dNHWC.from_torch(self.torch_layer.conv1)
         assert isinstance(self.torch_layer.bn1, nn.BatchNorm2d), "Only BatchNorm2d is supported in Bottleneck blocks."
-        self.batch_norm_args1 = {
-            "scale": self.torch_layer.bn1.weight,
-            "shift": self.torch_layer.bn1.bias,
-            "running_mean": self.torch_layer.bn1.running_mean,
-            "running_var": self.torch_layer.bn1.running_var,
-            "eps": self.torch_layer.bn1.eps,
-        }
-        self.conv2 = TTNNConv2dNHWC.from_torch(self.torch_layer.conv2)
-        self.batch_norm_args2 = {
-            "scale": self.torch_layer.bn2.weight,
-            "shift": self.torch_layer.bn2.bias,
-            "running_mean": self.torch_layer.bn2.running_mean,
-            "running_var": self.torch_layer.bn2.running_var,
-            "eps": self.torch_layer.bn2.eps,
-        }
-        self.conv3 = TTNNConv2dNHWC.from_torch(self.torch_layer.conv3)
-        self.batch_norm_args3 = {
-            "scale": self.torch_layer.bn3.weight,
-            "shift": self.torch_layer.bn3.bias,
-            "running_mean": self.torch_layer.bn3.running_mean,
-            "running_var": self.torch_layer.bn3.running_var,
-            "eps": self.torch_layer.bn3.eps,
-        }
-        self.relu = nn.ReLU()
+        self.conv1 = TTNNConv2dBNActivationNHWC.from_torch(
+            self.torch_layer.conv1, self.torch_layer.bn1, self.torch_layer.relu
+        )
+        self.conv2 = TTNNConv2dBNActivationNHWC.from_torch(
+            self.torch_layer.conv2, self.torch_layer.bn2, self.torch_layer.relu
+        )
+        self.conv3 = TTNNConv2dBNNHWC.from_torch(self.torch_layer.conv3, self.torch_layer.bn3)
+        self.relu = TTNNReLU()
         self.nchw_to_nhwc_permute = TTNNPermute(perm=[0, 2, 3, 1])
         self.nhwc_to_nchw_permute = TTNNPermute(perm=[0, 3, 1, 2])
 
@@ -171,50 +330,13 @@ class TTNNBottleneck(TTNNModule):
         new_bottleneck.initilize_submodules()
         return new_bottleneck
 
-    def to_device(self, device: str):
-        """Set device for the module and its submodules."""
-        super().to_device(device)
-        self.conv1.to_device(device)
-        self.conv2.to_device(device)
-        self.conv3.to_device(device)
-        self.nhwc_to_nchw_permute.to_device(device)
-        self.nchw_to_nhwc_permute.to_device(device)
-
     def preprocess_weights_impl(self):
         """Preprocess attention weights for TTNN."""
         assert (
             self.torch_layer is not None
         ), "Torch layer must be set before preprocessing weights. This layer can only be created from a torch layer (e.g. from_torch method)."
-
-        if self.batch_norm_args1 is not None:
-            self.conv1.torch_weight, self.conv1.torch_bias = fold_batch_norm2d_into_conv2d(
-                self.conv1.torch_weight,
-                self.batch_norm_args1["scale"],
-                self.batch_norm_args1["shift"],
-                self.batch_norm_args1["running_mean"],
-                self.batch_norm_args1["running_var"],
-                self.batch_norm_args1["eps"],
-            )
         self.conv1.preprocess_weights()
-        if self.batch_norm_args2 is not None:
-            self.conv2.torch_weight, self.conv2.torch_bias = fold_batch_norm2d_into_conv2d(
-                self.conv2.torch_weight,
-                self.batch_norm_args2["scale"],
-                self.batch_norm_args2["shift"],
-                self.batch_norm_args2["running_mean"],
-                self.batch_norm_args2["running_var"],
-                self.batch_norm_args2["eps"],
-            )
         self.conv2.preprocess_weights()
-        if self.batch_norm_args3 is not None:
-            self.conv3.torch_weight, self.conv3.torch_bias = fold_batch_norm2d_into_conv2d(
-                self.conv3.torch_weight,
-                self.batch_norm_args3["scale"],
-                self.batch_norm_args3["shift"],
-                self.batch_norm_args3["running_mean"],
-                self.batch_norm_args3["running_var"],
-                self.batch_norm_args3["eps"],
-            )
         self.conv3.preprocess_weights()
 
     def move_weights_to_device_impl(self):
@@ -233,24 +355,18 @@ class TTNNBottleneck(TTNNModule):
             x = self.nchw_to_nhwc_permute(x)
             identity = x
         out = self.conv1(x)
-        out = self.relu(out)
-
         out = self.conv2(out)
-        out = self.relu(out)
-
         out = self.conv3(out)
 
         if self.downsample is not None:
             from models.tt_symbiote.core.tensor import TorchTTNNTensor
 
-            identity = self.downsample(TorchTTNNTensor(identity))
+            identity = self.downsample(TorchTTNNTensor(identity, dtype=torch.bfloat16))
             if identity.to_ttnn.device() != out.to_ttnn.device():
                 identity = ttnn.to_device(identity.to_ttnn, out.to_ttnn.device())
-            out = self.nhwc_to_nchw_permute(out)
-            out += identity
-        else:
-            out += identity
+
+            identity = self.nchw_to_nhwc_permute(identity)
+        out += identity
         out = self.relu(out)
-        if self.downsample is None:
-            out = self.nhwc_to_nchw_permute(out)
+        out = self.nhwc_to_nchw_permute(out)
         return out
