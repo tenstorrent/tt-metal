e972e6dbefb616aedfbb7baa93bfd2e587ae2343
Suhail Alnahari <salnahari@tenstorrent.com>
2025-12-22 21:48:58 -0500

    tt_symbiote: added DPL/SEL and fixed dtypes

---
+++ models/tt_symbiote/core/torch_dispatcher.py
@@ -0,0 +1,20 @@
+def handle_view(func, args, kwargs):
+    """Handle view operation."""
+    return args[0].reshape(args[1])
+
+
+func_to_torch = {
+    "aten::view": handle_view,
+}
+
+
+def can_dispatch_to_torch(func_name: str, args=None, kwargs=None) -> bool:
+    if func_name == "aten::view":
+        if len(args) != 2 or len(kwargs or {}) != 0:
+            return False
+    return func_name in func_to_torch
+
+
+def dispatch_to_torch(func_name, args, kwargs):
+    """Dispatch operation to TTNN handler."""
+    return func_to_torch[func_name](func_name, args, kwargs)
