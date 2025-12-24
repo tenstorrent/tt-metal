4c19c358752ab2a9d479ebccbc199c512e37b71e
Suhail Alnahari <salnahari@tenstorrent.com>
2025-12-23 20:11:59 -0500

    tt_symbiote: modified permute and removed transformers deps from attention module

--- models/tt_symbiote/modules/tensor.py
+++ models/tt_symbiote/modules/tensor.py
@@ -9,26 +9,24 @@ from models.tt_symbiote.core.module import TTNNModule
 class TorchPermute(torch.nn.Module):
     """Fallback PyTorch Permute activation function."""
 
-    def __init__(self, perm):
+    def __init__(self):
         super().__init__()
-        self.perm = perm
 
-    def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
+    def forward(self, input_tensor: torch.Tensor, perm) -> torch.Tensor:
         """Forward pass through Permute activation."""
-        return input_tensor.permute(self.perm)
+        return input_tensor.permute(perm)
 
 
 class TTNNPermute(TTNNModule):
     """TTNN-accelerated Permute activation function."""
 
-    def __init__(self, perm):
+    def __init__(self):
         super().__init__()
-        self.perm = perm
-        self._fallback_torch_layer = TorchPermute(perm)
+        self._fallback_torch_layer = TorchPermute()
 
-    def forward(self, input_tensor: ttnn.Tensor) -> ttnn.Tensor:
+    def forward(self, input_tensor: ttnn.Tensor, perm) -> ttnn.Tensor:
         """Forward pass through Permute activation."""
-        tt_output = ttnn.permute(input_tensor, self.perm, memory_config=ttnn.DRAM_MEMORY_CONFIG)
+        tt_output = ttnn.permute(input_tensor, perm, memory_config=ttnn.DRAM_MEMORY_CONFIG)
         return tt_output
 
 
