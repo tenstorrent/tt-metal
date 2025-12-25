c0e99194f57cce1ff067cdd7a5b79467360e15a0
Bojan Rosko <brosko@tenstorrent.com>
2025-12-25 16:08:55 +0000

    omg

--- bisect.sh
+++ bisect.sh
@@ -5,7 +5,16 @@ export temporigbranch=brosko/test_glx_umd
 # export tempcommit=("5f72bff" "8a63e7f" "f21759e" "2646d30" "6862db7")
 export allcommits=("72cd7112" "af52aa16" "bc14a3b5" "f865ace3" "82e7f387" "45742f8d" "c582ec5b" "fb6ce5c9" "78677932" "14ac4416" "0a30b1e2" "c9fc7294" "03055725" "67a67152" "d86b83ac" "30cc18fd" "8b24e259" "2568ba11" "0e071c64" "cba7b617" "646dbd58" "ba4ab7b5" "7aa315ea" "d0936031" "a780c582" "5a6d9f71" "77cfe2d8" "9e68b60e" "2a923097" "6711cedc" "8e2877c6" "9cb38471" "4aeef666" "9e6bfc61" "70db3062" "ae043aaa" "159e3004" "cb9b4f55" "10059405" "bdcda51e" "dab66912" "d2d07866" "58e5f0ea" "ad06de83" "2e824226" "ea62cf52" "b61cf119" )
 # Now take every 7th commit
-export tempcommit=("${allcommits[@]:0:7}")
+# export tempcommit=("${allcommits[@]:0:7}")
+
+# Now take every 7th commit (indices 0, 7, 14, 21, 28, 35, 42, ...)
+# tempcommit=()
+# for ((i=0; i<${#allcommits[@]}; i+=7)); do
+#   tempcommit+=("${allcommits[$i]}")
+# done
+# export tempcommit
+export tempcommit=("7aa315ea" "d0936031" "a780c582" "5a6d9f71" "77cfe2d8" "9e68b60e")
+
 export tempbranch=brosko/test_glx_umd_
 
 # Declare associative array to store commit hash -> run URL mapping
@@ -85,28 +94,28 @@ done
 # c9fc7294 Remove all lite fabric related code (#1683)
 # 03055725 Change cached TLB size in LocalChip (#1675)
 # 67a67152 Make ERISC FW hash check optional (#1711)
-# d86b83ac Add ERISC FW 7.2.0 for CMFW 19.4.1 (#1710)
+N# d86b83ac Add ERISC FW 7.2.0 for CMFW 19.4.1 (#1710)
 # 30cc18fd Read logical remote eth id for all BH systems (#1682)
 # 8b24e259 Improve soc. desc paths for tests (#1703)
 # 2568ba11 Make routing firmware check not throw exception (#1697)
 # 0e071c64 Additional python interface for exalens (#1678)
 # cba7b617 Add wh-erisc version 7.3.0 for fw 19.4.0 (#1704)
 # 646dbd58 Driver version mismatch removal (#1700)
-# ba4ab7b5 Remove BAR4 mapping from PCI device (#1684)
+N# ba4ab7b5 Remove BAR4 mapping from PCI device (#1684)
 # 7aa315ea Add unity build support to UMD (#1693)
 # d0936031 Calculate WH-ERISC Board ID instead of reading it (#1691)
 # a780c582 Add BH-ERISC expected version 1.7.1 for FW bundle 19.3.0 (#1685)
 # 5a6d9f71 Add precommit to check for periods in comments (#1646)
 # 77cfe2d8 Verify ERISC FW hash in TopologyDiscovery (#1613)
 # 9e68b60e Add TopologyDiscovery discover method (#1654)
-# 2a923097 Fix DMA2 tests (#1681)
+Y# 2a923097 Fix DMA2 tests (#1681)
 # 6711cedc Add standalone ASIO library (#1674)
 # 8e2877c6 Fix BH p300 machine runs (#1672)
 # 9cb38471 Introduce static lib (#1662)
 # 4aeef666 Fix RemoteChip in case of 0 host channels (#1679)
 # 9e6bfc61 Fix PCIe DMA read and write functions (#1676)
 # 70db3062 Remove leftover deprecated arc_msg implementation (#1658)
-# ae043aaa Use 1MB for WH cached TLB (#1669)
+Y# ae043aaa Use 1MB for WH cached TLB (#1669)
 # 159e3004 Change EXPECT to ASSERT in sysmem tests (#1670)
 # cb9b4f55 Remove unused variable from TlbManager (#1661)
 # 10059405 Remove tt::umd when unnecessary (#1663)
