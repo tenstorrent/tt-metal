ec751b8e625f9b91ad2100ea795824666307bbf0
mountagha <muntaghaba@gmail.com>
2025-12-25 20:44:38 +0000

    Added Positional encoding module.

--- models/demos/patchtsmixer/tt/patchtsmixer.py
+++ models/demos/patchtsmixer/tt/patchtsmixer.py
@@ -11,6 +11,24 @@ class TtPatchTSMixerGatedAttention:
 
     def __call__(self, x):
         # x: TTNN tensor, last dimension = d_model
-        y = ttnn.linear(x, self.weight, self.bias)
+        y = ttnn.linear(x, self.weight, bias=self.bias)
         w = ttnn.softmax(y, dim=-1)
         return ttnn.multiply(x, w)
+
+
+class TtPatchTSMixerPositionalEncoding:
+    """
+    TTNN equivalent of PatchTSMixerPositionalEncoding.
+
+    Expects:
+        x: (B, C, N_p, D) as TTNN tensor
+        pe: stored as (1, 1, N_p, D) TTNN tensor for broadcast
+    """
+
+    def __init__(self, device, base_address: str, parameters: dict):
+        self.device = device
+        self.base = base_address
+        self.pe = parameters[f"{self.base}.pe"]
+
+    def __call__(self, x):
+        return ttnn.add(x, self.pe)
