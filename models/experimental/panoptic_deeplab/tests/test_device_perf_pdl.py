165783c34024d7ad8134f912ff2b2a2445276da9
Nikola Stamatovic <nstamatovic@tenstorrent.com>
2025-12-29 17:10:01 +0000

    update expected perf

--- models/experimental/panoptic_deeplab/tests/test_device_perf_pdl.py
+++ models/experimental/panoptic_deeplab/tests/test_device_perf_pdl.py
@@ -54,7 +54,7 @@ def test_device_perf_pdl_20_cores(
     [
         (
             "pytest models/experimental/panoptic_deeplab/tests/pcc/test_tt_model.py::test_model_panoptic_deeplab -k panoptic_deeplab_110_cores",
-            12_445_978,
+            12_241_745,
             PANOPTIC_DEEPLAB,
             PANOPTIC_DEEPLAB,
             1,
