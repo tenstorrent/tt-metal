782f5f622330d8e5125cf0541b2c0bf54f9fceef
Jason Davies <jason@jasondavies.com>
2025-12-29 22:03:57 +0000

    Add optimised fp32/bf16 to u16; u32/i32 to bf16.

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_typecast.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_typecast.h
@@ -16,8 +16,8 @@ namespace ckernel {
 namespace sfpu {
 
 template <bool APPROXIMATION_MODE, int ITERATIONS>
-inline void calculate_typecast_fp16b_to_uint16() {
-    _calculate_typecast_fp16b_to_uint16_<APPROXIMATION_MODE, ITERATIONS>();
+inline void calculate_typecast_fp32_to_uint16() {
+    _calculate_typecast_fp32_to_uint16_<APPROXIMATION_MODE, ITERATIONS>();
 }
 
 template <bool APPROXIMATION_MODE, int ITERATIONS>
@@ -108,6 +108,21 @@ inline void init_typecast_uint16_to_fp32() {
 template <bool APPROXIMATION_MODE>
 inline void init_typecast_uint16_to_fp16b() {
     _init_typecast_uint16_to_fp16b_<APPROXIMATION_MODE>();
+}
+
+template <bool APPROXIMATION_MODE>
+inline void init_typecast_int32_to_fp16b() {
+    _init_typecast_int32_to_fp16b_<APPROXIMATION_MODE>();
+}
+
+template <bool APPROXIMATION_MODE>
+inline void init_typecast_uint32_to_fp16b() {
+    _init_typecast_uint32_to_fp16b_<APPROXIMATION_MODE>();
+}
+
+template <bool APPROXIMATION_MODE>
+inline void init_typecast_fp32_to_uint16() {
+    _init_typecast_fp32_to_uint16_<APPROXIMATION_MODE>();
 }
 
 }  // namespace sfpu
