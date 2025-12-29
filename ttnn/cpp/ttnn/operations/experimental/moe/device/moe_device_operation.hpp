9227a277297f67c7d7f402cd0bdeb1897678a33c
Nikhil Soraba <nsoraba@tenstorrent.com>
2025-12-29 23:29:47 +0000

    Add scaffolding for MoE Op + some placeholder tests

---
+++ ttnn/cpp/ttnn/operations/experimental/moe/device/moe_device_operation.hpp
@@ -0,0 +1,42 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <optional>
+#include <variant>
+
+#include "ttnn/tensor/tensor.hpp"
+#include "ttnn/device_operation.hpp"
+#include "ttnn/decorators.hpp"
+#include "moe_device_operation_types.hpp"
+#include "moe_program_factory.hpp"
+
+namespace ttnn::operations::experimental::moe {
+
+struct MoEDeviceOperation {
+    using operation_attributes_t = moe::operation_attributes_t;
+    using tensor_args_t = moe::tensor_args_t;
+    using spec_return_value_t = moe::spec_return_value_t;
+    using tensor_return_value_t = moe::tensor_return_value_t;
+    using program_factory_t = std::variant<program::MoEProgramFactory>;
+
+    static program_factory_t select_program_factory(const operation_attributes_t&, const tensor_args_t&);
+
+    static void validate_on_program_cache_hit(const operation_attributes_t&, const tensor_args_t&);
+    static void validate_on_program_cache_miss(const operation_attributes_t&, const tensor_args_t&);
+    static spec_return_value_t compute_output_specs(const operation_attributes_t&, const tensor_args_t&);
+    static tensor_return_value_t create_output_tensors(
+        const operation_attributes_t& operation_attributes, const tensor_args_t&);
+
+    static std::tuple<operation_attributes_t, tensor_args_t> invoke(
+        const Tensor& input_tensor, const Tensor& weight_tensor);
+};
+
+}  // namespace ttnn::operations::experimental::moe
+
+namespace ttnn::prim {
+constexpr auto moe =
+    ttnn::register_operation<"ttnn::prim::moe", ttnn::operations::experimental::moe::MoEDeviceOperation>();
+}  // namespace ttnn::prim
