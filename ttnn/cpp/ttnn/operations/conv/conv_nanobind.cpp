f7ebec50769dcef55929dcae88a7f40db73dd3ed
Bhuvanesh194Sankar <bhuvanesh.sankar@multicorewareinc.com>
2025-12-30 12:23:46 +0530

    Revoke Deform Conv composite implementation

--- ttnn/cpp/ttnn/operations/conv/conv_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv_nanobind.cpp
@@ -9,14 +9,12 @@
 #include "conv2d/conv2d_nanobind.hpp"
 #include "conv_transpose2d/conv_transpose2d_nanobind.hpp"
 #include "conv1d/conv1d_nanobind.hpp"
-#include "deform_conv2d/deform_conv2d_nanobind.hpp"
 
 namespace ttnn::operations::conv {
 
 void py_module(nb::module_& mod) {
     ttnn::operations::conv::conv1d::bind_conv1d(mod);
     ttnn::operations::conv::conv2d::bind_conv2d(mod);
-    ttnn::operations::conv::deform_conv2d::bind_deform_conv2d(mod);
     ttnn::operations::conv::conv_transpose2d::bind_conv_transpose2d(mod);
 }
 }  // namespace ttnn::operations::conv
