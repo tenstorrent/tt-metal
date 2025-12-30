49b8370e007b7032afa84da25aaf548eac18191a
Stephen Osborne <sosborne@tenstorrent.com>
2025-12-30 11:04:42 -0800

    Increase Wan perf threshold for BH QB (#35183)
    ### Problem description
    The nightly Galaxy pipeline is intermittently failing on a Wan model
    perf test. Example of a failure:
    https://github.com/tenstorrent/tt-metal/actions/runs/20577160288/job/59097042440.
    
    ### What's changed
    Increase encoder threshold.
    
    ### Checklist
    
    - [x] [![All post-commit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml/badge.svg?branch=sosborne/wan-encoder-perf-threshold)](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml?query=branch:sosborne/wan-encoder-perf-threshold)

--- models/experimental/tt_dit/tests/models/wan2_2/test_performance_wan.py
+++ models/experimental/tt_dit/tests/models/wan2_2/test_performance_wan.py
@@ -258,7 +258,7 @@ def test_pipeline_performance(
     elif tuple(mesh_device.shape) == (1, 4) and height == 480:
         assert is_blackhole(), "1x4 is only supported for blackhole"
         expected_metrics = {
-            "encoder": 17.0,
+            "encoder": 27.0,
             "denoising": 680.0,
             "vae": 60.0,
             "total": 760.0,
@@ -266,7 +266,7 @@ def test_pipeline_performance(
     elif tuple(mesh_device.shape) == (1, 4) and height == 720:
         assert is_blackhole(), "1x4 is only supported for blackhole"
         expected_metrics = {
-            "encoder": 15.0,
+            "encoder": 27.0,
             "denoising": 3200.0,
             "vae": 200.0,
             "total": 3415.0,
