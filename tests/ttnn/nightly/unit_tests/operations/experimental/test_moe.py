1e8055e5051c0fc91743f235a913eb8e6b9672d7
Nikhil Soraba <nsoraba@tenstorrent.com>
2025-12-30 22:34:24 +0000

    Add CBs and tensor accessors

--- tests/ttnn/nightly/unit_tests/operations/experimental/test_moe.py
+++ tests/ttnn/nightly/unit_tests/operations/experimental/test_moe.py
@@ -9,6 +9,9 @@ from loguru import logger
 
 from models.common.utility_functions import comp_pcc
 
+# Disable per module device fixture
+
+
 from tracy.process_model_log import (
     get_latest_ops_log_filename,
     run_device_profiler,
@@ -25,26 +28,35 @@ def get_accuracy_metrics(torch_output, tt_output):
     }
 
 
-def run_test_moe(device, M, K, N):
-    logger.info(f"Running test_moe with M={M}, K={K}, N={N}")
+def run_test_moe(device, M, K, N, check_accuracy):
+    logger.info(f"Running test_moe with M={M}, K={K}, N={N}, check_accuracy={check_accuracy}")
 
-    torch_input = torch.randn((M, K), dtype=torch.float32)
-    torch_w0 = torch.randn((K, N), dtype=torch.float32)
-    torch_w1 = torch.randn((K, N), dtype=torch.float32)
-    torch_w2 = torch.randn((N, K), dtype=torch.float32)
+    if check_accuracy:
+        torch_input = torch.randn((M, K), dtype=torch.float32)
+        torch_w0 = torch.randn((K, N), dtype=torch.float32)
+        torch_w1 = torch.randn((K, N), dtype=torch.float32)
+        torch_w2 = torch.randn((N, K), dtype=torch.float32)
 
     # Prepare TT tensors
-    tt_input = ttnn.from_torch(torch_input, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
-    tt_weight0 = ttnn.from_torch(torch_w0, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
-    tt_weight1 = ttnn.from_torch(torch_w1, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
-    tt_weight2 = ttnn.from_torch(torch_w2, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
-    tt_output = ttnn.empty((M, K), dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+    if check_accuracy:
+        tt_input = ttnn.from_torch(torch_input, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+        tt_weight0 = ttnn.from_torch(torch_w0, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+        tt_weight1 = ttnn.from_torch(torch_w1, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+        tt_weight2 = ttnn.from_torch(torch_w2, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+        tt_output = ttnn.empty((M, K), dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+    else:
+        tt_input = ttnn.empty((M, K), dtype=ttnn.bfloat8_b, device=device, layout=ttnn.TILE_LAYOUT)
+        tt_weight0 = ttnn.empty((K, N), dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+        tt_weight1 = ttnn.empty((K, N), dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+        tt_weight2 = ttnn.empty((N, K), dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+        tt_output = ttnn.empty((M, K), dtype=ttnn.bfloat8_b, device=device, layout=ttnn.TILE_LAYOUT)
 
-    with torch.no_grad():
-        w0_output = torch.nn.functional.silu(torch_input @ torch_w0)
-        w1_output = torch_input @ torch_w1
-        w2_input = w0_output * w1_output
-        torch_output = w2_input @ torch_w2
+    if check_accuracy:
+        with torch.no_grad():
+            torch_w0_output = torch.nn.functional.silu(torch_input @ torch_w0)
+            torch_w1_output = torch_input @ torch_w1
+            torch_w2_input = torch_w0_output * torch_w1_output
+            torch_output = torch_w2_input @ torch_w2
 
     tt_output = ttnn.experimental.moe(
         tt_input,
@@ -54,41 +66,52 @@ def run_test_moe(device, M, K, N):
         output_tensor=tt_output,
     )
     tt_output = ttnn.to_torch(tt_output)
-    return get_accuracy_metrics(torch_output, tt_output)
+    if check_accuracy:
+        return get_accuracy_metrics(torch_output, tt_output)
+    return {}
 
 
-TABLE_CONFIGS = [
-    (32, 7168, 2048),
-]
+SHAPE2TIME = {
+    (32, 7168, 2048): 250,
+}
 
 
 @pytest.mark.parametrize(
     "M, K, N",
-    TABLE_CONFIGS,
+    SHAPE2TIME.keys(),
 )
-def test_moe(device, M, K, N):
+@pytest.mark.parametrize("check_accuracy", [True, False], ids=["default", "no_check"])
+def test_moe(device, M, K, N, check_accuracy):
     accuracy_metrics = run_test_moe(
         device,
         M,
         K,
         N,
+        check_accuracy,
     )
-    # assert accuracy_metrics["pcc"] > 0.999_500
-    # assert accuracy_metrics["relative_rmse"] < 0.02
+
+    # if check_accuracy:
+    #     assert accuracy_metrics["pcc"] > 0.999_500
+    #     assert accuracy_metrics["relative_rmse"] < 0.02
 
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 @pytest.mark.parametrize(
     "M, K, N",
-    TABLE_CONFIGS,
+    SHAPE2TIME.keys(),
 )
-def test_moe_performance(M, K, N):
-    float_cols = ["DEVICE KERNEL DURATION [ns]"]
-    command = f"pytest tests/ttnn/nightly/unit_tests/operations/experimental/test_moe.py::test_moe[M={M}-K={K}-N={N}]"
+@pytest.mark.parametrize("check_accuracy", ["default", "no_check"])
+def test_moe_performance(M, K, N, check_accuracy):
+    command = f"pytest tests/ttnn/nightly/unit_tests/operations/experimental/test_moe.py::test_moe[{check_accuracy}-M={M}-K={K}-N={N}]"
 
     run_device_profiler(command, "ttnn_moe_performance", device_analysis_types=["device_kernel_duration"])
-    r = post_process_ops_log("ttnn_moe_performance", float_columns=float_cols)
+    r = post_process_ops_log("ttnn_moe_performance", float_columns=["DEVICE KERNEL DURATION [ns]"])
     duration_ns = int(r["DEVICE KERNEL DURATION [ns]"].min())
+    logger.info(f"Performance: {duration_ns} ns")
+
+    assert (
+        duration_ns < SHAPE2TIME[(M, K, N)]
+    ), f"Performance {duration_ns} ns is greater than expected {SHAPE2TIME[(M, K, N)]} ns"
 
 
 def post_process_ops_log(output_logs_subdir: str, float_columns: list[str]) -> dict[str, float]:
