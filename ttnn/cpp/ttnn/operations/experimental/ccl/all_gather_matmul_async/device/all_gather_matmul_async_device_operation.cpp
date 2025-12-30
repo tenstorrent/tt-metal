4dc2998f81f79287199c2d56a2db949baee95ba8
Kevin Wu <ctr-kevinwu@tenstorrent.com>
2025-12-30 23:06:53 +0000

    TEST: Fix tensor args passed to all gather async

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_matmul_async/device/all_gather_matmul_async_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_matmul_async/device/all_gather_matmul_async_device_operation.cpp
@@ -39,7 +39,7 @@ void AllGatherMatmulAsyncDeviceOperation::validate_on_program_cache_miss(
         const auto& all_gather_output_tensor = tensor_args.persistent_output_buffer.value();
         // All Gather validate
         all_gather_async::AllGatherAsyncDeviceOperation::validate_on_program_cache_miss(
-            operation_attributes.all_gather_async_attributes, operation_attributes.all_gather_async_tensor_args);
+            operation_attributes.all_gather_async_attributes, {input_tensor, tensor_args.persistent_output_buffer});
 
         // Matmul validate
         operation_attributes.matmul.validate(
@@ -88,7 +88,7 @@ spec_return_value_t AllGatherMatmulAsyncDeviceOperation::compute_output_specs(
     const operation_attributes_t& operation_attributes, const tensor_args_t& tensor_args) {
     // All Gather shape
     auto all_gather_output_specs = all_gather_async::AllGatherAsyncDeviceOperation::compute_output_specs(
-        operation_attributes.all_gather_async_attributes, operation_attributes.all_gather_async_tensor_args);
+        operation_attributes.all_gather_async_attributes, {tensor_args.input_tensor});
 
     // Matmul shape
     auto matmul_output_specs = operation_attributes.matmul.compute_output_specs(
@@ -103,7 +103,8 @@ tensor_return_value_t AllGatherMatmulAsyncDeviceOperation::create_output_tensors
     std::vector<std::optional<Tensor>> optional_output_tensors = {tensor_args.persistent_output_buffer};
     // All Gather output tensor
     auto all_gather_output_tensor = all_gather_async::AllGatherAsyncDeviceOperation::create_output_tensors(
-        operation_attributes.all_gather_async_attributes, operation_attributes.all_gather_async_tensor_args);
+        operation_attributes.all_gather_async_attributes,
+        {tensor_args.input_tensor, tensor_args.persistent_output_buffer});
 
     // Matmul output tensor
     auto matmul_output_tensor = operation_attributes.matmul.create_output_tensors(
