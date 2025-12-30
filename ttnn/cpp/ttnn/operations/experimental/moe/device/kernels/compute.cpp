58c8b4f7e3b4d4e5707f50b45347c29574b7a468
Nikhil Soraba <nsoraba@tenstorrent.com>
2025-12-30 22:34:24 +0000

    Add CBs and tensor accessors

--- ttnn/cpp/ttnn/operations/experimental/moe/device/kernels/compute.cpp
+++ ttnn/cpp/ttnn/operations/experimental/moe/device/kernels/compute.cpp
@@ -5,5 +5,18 @@
 #include "compute_kernel_api.h"
 
 namespace NAMESPACE {
-void MAIN {}
+void MAIN {
+    // CBs
+    constexpr auto cb_r2c_w0 = tt::CBIndex::c_0;
+    constexpr auto cb_s2c_in = tt::CBIndex::c_1;
+    constexpr auto cb_c2c_mm0 = tt::CBIndex::c_2;
+    constexpr auto cb_c2c_mm1 = tt::CBIndex::c_3;
+    constexpr auto cb_c2w_elt = tt::CBIndex::c_4;
+    constexpr auto cb_r2c_in2 = tt::CBIndex::c_5;
+    constexpr auto cb_c2w_mm2 = tt::CBIndex::c_6;
+
+    // CB Aliases
+    constexpr auto cb_r2c_w1 = tt::CBIndex::c_0;
+    constexpr auto cb_r2c_w2 = tt::CBIndex::c_0;
+}
 }  // namespace NAMESPACE
