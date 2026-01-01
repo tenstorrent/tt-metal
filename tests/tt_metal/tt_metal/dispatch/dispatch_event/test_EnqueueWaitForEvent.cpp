6223e8c20a04e771b5c44616a406aecd39d9f1fd
Bryan Wilder Field Lozano <blozano@tenstorrent.com>
2026-01-01 12:43:50 -0800

    Revert "Revert "Modernize loop convert"" (#35219)
    Reverts tenstorrent/tt-metal#35218
    
    Metal Infra Alerts Bot blamed this commit for several failures.
    
    Upon investigation, I think all issues were non determinstic.
    
    The following pipelines pass:
    
    - [x] [Single Card Model
    Perf](https://github.com/tenstorrent/tt-metal/actions/runs/20642071780)
    - [x] [Single Card Demo
    Tests](https://github.com/tenstorrent/tt-metal/actions/runs/20642225570)
    - [x] [All Post
    Commit](https://github.com/tenstorrent/tt-metal/actions/runs/20642086451)
    - [x] [Black Hole Post
    Commit](https://github.com/tenstorrent/tt-metal/actions/runs/20642094810)
    - [x] [T3K Unit
    Tests](https://github.com/tenstorrent/tt-metal/actions/runs/20642111542)
    - [x] [T3K Model Perf - Same Failures as
    Main](https://github.com/tenstorrent/tt-metal/actions/runs/20642274211)
    - [x] [L2
    Nightly](https://github.com/tenstorrent/tt-metal/actions/runs/20642246445)
    - [x] [Merge
    Gate](https://github.com/tenstorrent/tt-metal/actions/runs/20642422761)

--- tests/tt_metal/tt_metal/dispatch/dispatch_event/test_EnqueueWaitForEvent.cpp
+++ tests/tt_metal/tt_metal/dispatch/dispatch_event/test_EnqueueWaitForEvent.cpp
@@ -35,8 +35,8 @@ using std::vector;
 namespace local_test_functions {
 
 void FinishAllCqs(vector<std::reference_wrapper<distributed::MeshCommandQueue>>& cqs) {
-    for (uint i = 0; i < cqs.size(); i++) {
-        distributed::Finish(cqs[i]);
+    for (auto& cq : cqs) {
+        distributed::Finish(cq);
     }
 }
 }  // namespace local_test_functions
