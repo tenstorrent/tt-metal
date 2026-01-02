5408f42f316972887be99eec4c9c138fdfc2ba5a
Jonathan Su <jonathansu@tenstorrent.com>
2026-01-02 22:37:12 +0000

    bring mm over

---
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_minimal_matmul_async/device/all_gather_minimal_matmul_async_program_factory.hpp
@@ -0,0 +1,47 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "ttnn/operation.hpp"
+#include "ttnn/operations/eltwise/unary/common/unary_op_utils.hpp"
+#include "ttnn/operations/ccl/ccl_op_fusion.hpp"
+
+namespace ttnn::operations::experimental::all_gather_minimal_matmul_async {
+
+namespace helpers {
+void override_program_parameters(
+    const ttnn::operations::experimental::all_gather_minimal_matmul_async::
+        all_gather_minimal_matmul_async_override_variables_t& override_variables,
+    const void* operation,
+    tt::tt_metal::Program& program,
+    const std::vector<tt::tt_metal::Tensor>& input_tensors,
+    const std::vector<std::optional<const tt::tt_metal::Tensor>>& optional_input_tensors,
+    const std::vector<tt::tt_metal::Tensor>& output_tensors);
+}
+
+namespace detail {
+ttnn::operations::experimental::all_gather_minimal_matmul_async::all_gather_minimal_matmul_async_override_variables_t
+all_gather_minimal_matmul_async_factory_helper(
+    tt::tt_metal::Program& program,
+    const Tensor& input_tensor,
+    const Tensor& weight_tensor,
+    const std::optional<const Tensor>& bias_tensor,
+    const std::optional<unary::UnaryWithParam>& fused_activation,
+    const std::optional<const AllGatherMinimalMatmulAsyncConfig>& config,
+    const Tensor& output_tensor,
+    const DeviceComputeKernelConfig& compute_kernel_config,
+    std::optional<ttnn::experimental::ccl::MinimalMatmulFusedOpSignaler>& fused_op_signaler);
+
+tt::tt_metal::operation::ProgramWithCallbacks all_gather_minimal_matmul_async_factory(
+    const Tensor& input_tensor,
+    const Tensor& weight_tensor,
+    const std::optional<const Tensor>& bias_tensor,
+    const std::optional<unary::UnaryWithParam>& fused_activation,
+    const std::optional<const AllGatherMinimalMatmulAsyncConfig>& config,
+    const Tensor& output_tensor,
+    const DeviceComputeKernelConfig& compute_kernel_config);
+
+}  // namespace detail
+}  // namespace ttnn::operations::experimental::all_gather_minimal_matmul_async
