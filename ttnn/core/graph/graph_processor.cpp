1d7101fe87c167bc4c2c79e093aca2a635edc1c8
Siddharth Sundaram <ssundaram@tenstorrent.com>
2026-01-02 13:43:50 -0600

    #32045: Privatize `tensor_id` and remove counter getter/setter (#32332)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/32045)
    
    ### Problem description
    We should disallow setting `tensor_id_counter` to some value, because it
    can used maliciously. `tensor_id` should be a private field.
    
    ### What's changed
    
    - Removed `tensor_id_counter` getter and setter
    - Made `tensor_id` member variable private
    - Replaced `set_tensor_id` with `Tensor::set_tensor_id` and updated call
    sites
    - Added `get_tensor_id`
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes ([link to
    run](https://github.com/tenstorrent/tt-metal/actions/runs/20664663295))
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI with demo tests passes (if applicable)
    - [ ] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes (if applicable)
    - [ ] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes (if applicable)
    - [ ] (For models and ops writers) [Single-card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/single-card-demo-tests.yaml)
    CI passes (if applicable) See [recommended dev
    flow](https://github.com/tenstorrent/tt-metal/blob/main/models/docs/MODEL_ADD.md#a-recommended-dev-flow-on-github-for-adding-new-models).
    - [ ] [Galaxy
    quick](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-quick.yaml)
    CI passes (if applicable)
    - [ ] [Galaxy demo tests, for
    Llama](https://github.com/tenstorrent/tt-metal/actions/workflows/galaxy-demo-tests.yaml)
    CI passes, if applicable, because of current Llama work
    - [ ] (For runtime and ops writers) [T3000 unit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-unit-tests.yaml)
    CI passes (if applicable, since this is run on push to main)
    - [ ] (For models and ops writers) [T3000 demo
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/t3000-demo-tests.yaml)
    CI passes (if applicable, since this is required for release)
    - [x] New/Existing tests provide coverage for changes

--- ttnn/core/graph/graph_processor.cpp
+++ ttnn/core/graph/graph_processor.cpp
@@ -311,17 +311,14 @@ node_id GraphProcessor::add_tensor(const Tensor& t) {
         },
         storage);
 
-    // TODO #32045: Remove the check for INVALID_TENSOR_ID since IDs are assigned in the constructor.
-    std::uint64_t tensor_id = t.tensor_id;
-    if (tensor_id == tt::tt_metal::Tensor::INVALID_TENSOR_ID) {
-        log_debug(
+    std::uint64_t tensor_id = t.get_id();
+    if (tensor_id == Tensor::INVALID_TENSOR_ID) {
+        log_warning(
             tt::LogAlways,
-            "Tensor doesn't have tensor_id (sentinel value is INVALID_TENSOR_ID), generating new one. Ideally this "
-            "should not happen. "
-            "Please set tensor_id "
-            "for this tensor ahead of time.");
-        tensor_id = tt::tt_metal::Tensor::next_tensor_id();
+            "Tensor doesn't have a valid ID, this tensor must have been moved and should therefore not be tracked");
+        return -1;
     }
+
     node_id tensor_counter = tensor_id_to_counter.contains(tensor_id) ? tensor_id_to_counter[tensor_id] : graph.size();
     auto shape = t.logical_shape();
 
