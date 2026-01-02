dc8a85d99c0d23c525a77f920ccde8711ea3b626
kkannan <kkannan@tenstorrent.com>
2026-01-02 08:19:20 +0000

    OOm in maxpool2d in unet smp

---
+++ tests/ttnn/unit_tests/operations/maxpool2d/test_maxpool2d_org.py
@@ -0,0 +1,105 @@
+import ttnn
+import tests.ttnn.unit_tests.operations.maxpool2d.utils as utils
+
+
+def _main(input):
+    input_0 = input[0]
+    ttnn_to_layout_0 = ttnn.to_layout(
+        input_0,
+        ttnn.Layout.TILE,
+        None,
+        memory_config=ttnn.MemoryConfig(
+            ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.DRAM, None
+        ),
+    )
+    ttnn.deallocate(input_0, False)
+    ttnn_permute_0 = ttnn.permute(
+        ttnn_to_layout_0,
+        [0, 2, 3, 1],
+        memory_config=ttnn.MemoryConfig(
+            ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.DRAM, None
+        ),
+        pad_value=0.0,
+    )
+    ttnn.deallocate(ttnn_to_layout_0, False)
+    ttnn_reshape_0 = ttnn.reshape(
+        ttnn_permute_0,
+        [1, 1, 476672, 64],
+        memory_config=ttnn.MemoryConfig(
+            ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.DRAM, None
+        ),
+    )
+    ttnn.deallocate(ttnn_permute_0, False)
+    ttnn_to_layout_1 = ttnn.to_layout(
+        ttnn_reshape_0,
+        ttnn.Layout.ROW_MAJOR,
+        None,
+        memory_config=ttnn.MemoryConfig(
+            ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.DRAM, None
+        ),
+    )
+    ttnn.deallocate(ttnn_reshape_0, False)
+    ttnn_max_pool2d_0 = ttnn.max_pool2d(
+        ttnn_to_layout_1,
+        1,
+        608,
+        784,
+        64,
+        [3, 3],
+        [2, 2],
+        [1, 1],
+        [1, 1],
+        ceil_mode=False,
+        memory_config=ttnn.MemoryConfig(
+            ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.DRAM, None
+        ),
+        applied_shard_scheme=None,
+        reallocate_halo_output=False,
+    )
+    ttnn.deallocate(ttnn_to_layout_1, False)
+    ttnn_to_layout_2 = ttnn.to_layout(
+        ttnn_max_pool2d_0,
+        ttnn.Layout.TILE,
+        None,
+        memory_config=ttnn.MemoryConfig(
+            ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.DRAM, None
+        ),
+    )
+    ttnn.deallocate(ttnn_max_pool2d_0, False)
+    ttnn_reshape_1 = ttnn.reshape(
+        ttnn_to_layout_2,
+        [1, 304, 392, 64],
+        memory_config=ttnn.MemoryConfig(
+            ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.DRAM, None
+        ),
+    )
+    ttnn.deallocate(ttnn_to_layout_2, False)
+    ttnn_permute_1 = ttnn.permute(
+        ttnn_reshape_1,
+        [0, 3, 1, 2],
+        memory_config=ttnn.MemoryConfig(
+            ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.DRAM, None
+        ),
+        pad_value=0.0,
+    )
+    ttnn.deallocate(ttnn_reshape_1, False)
+    util_create_list_0 = [ttnn_permute_1]
+    return util_create_list_0
+
+
+def create_inputs_for__main():
+    utils_DeviceGetter_get_device_0 = utils.DeviceGetter.get_device((1, 1))
+    ttnn_ones_0 = ttnn.ones(
+        shape=ttnn.Shape([1, 64, 608, 784]),
+        dtype=ttnn.DataType.BFLOAT16,
+        layout=ttnn.Layout.ROW_MAJOR,
+        device=utils_DeviceGetter_get_device_0,
+    )
+    util_create_list_1 = [ttnn_ones_0]
+    return util_create_list_1
+
+
+def test_maxpool2d():
+    create_inputs_for__main_0 = create_inputs_for__main()
+    _main_0 = _main(create_inputs_for__main_0)
+    
