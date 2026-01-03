a184ab630d41913b139ad8f3825db41e094f6753
NSexton <216619413+nsextonTT@users.noreply.github.com>
2026-01-02 23:38:56 -0500

    Revert "Add fedora to CI" (#35240)
    Reverts tenstorrent/tt-metal#35238

--- create_venv.sh
+++ create_venv.sh
@@ -14,9 +14,6 @@ if ! command -v $PYTHON_CMD &>/dev/null; then
     exit 1
 fi
 
-echo "Getting uv and python 3.12"
-${PYTHON_CMD} -m pip install uv
-
 # Set Python environment directory
 if [ -z "$PYTHON_ENV_DIR" ]; then
     PYTHON_ENV_DIR=$(pwd)/python_env
@@ -24,7 +21,7 @@ fi
 echo "Creating virtual env in: $PYTHON_ENV_DIR"
 
 # Create and activate virtual environment
-uv venv $PYTHON_ENV_DIR --python 3.12
+$PYTHON_CMD -m venv $PYTHON_ENV_DIR
 source $PYTHON_ENV_DIR/bin/activate
 
 # Import functions for detecting OS
@@ -34,19 +31,19 @@ detect_os
 
 if [ "$OS_ID" = "ubuntu" ] && [ "$OS_VERSION" = "22.04" ]; then
     echo "Ubuntu 22.04 detected: force pip/setuptools/wheel versions"
-    uv pip config set global.extra-index-url https://download.pytorch.org/whl/cpu
-    uv pip install setuptools wheel==0.45.1
+    pip install --force-reinstall pip==25.1.1
+    python3 -m pip config set global.extra-index-url https://download.pytorch.org/whl/cpu
+    python3 -m pip install setuptools wheel==0.45.1
 else
     echo "$OS_ID $OS_VERSION detected: updating wheel and setuptools to latest"
-    uv pip install --upgrade wheel setuptools
+    python3 -m pip install --upgrade wheel setuptools
 fi
 
 echo "Installing dev dependencies"
-# need the index strategy to fallback to specified torch version buried in reqs
-uv pip install --index-strategy unsafe-best-match -r $(pwd)/tt_metal/python_env/requirements-dev.txt
+python3 -m pip install -r $(pwd)/tt_metal/python_env/requirements-dev.txt
 
 echo "Installing tt-metal"
-uv pip install -e .
+pip install -e .
 
 # Do not install hooks when this is a worktree
 if [ $(git rev-parse --git-dir) = $(git rev-parse --git-common-dir) ]; then
