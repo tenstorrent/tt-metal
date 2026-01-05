e5cadfcabb2801228734822b233349e40286be2b
Bryan Wilder Field Lozano <blozano@tenstorrent.com>
2026-01-05 10:47:22 -0800

    Update tt_metal/jit_build/jit_build_utils.cpp

--- tt_metal/jit_build/jit_build_utils.cpp
+++ tt_metal/jit_build/jit_build_utils.cpp
@@ -26,7 +26,7 @@ bool run_command(const std::string& cmd, const std::string& log_file, const bool
         {
             std::lock_guard<std::mutex> lk(io_mutex);
             std::cout << "===== RUNNING SYSTEM COMMAND:\n";
-            std::cout << cmd << "\n\n";
+            std::cout << cmd << "\n" << std::endl;
         }
         ret = system(cmd.c_str());
     } else {
