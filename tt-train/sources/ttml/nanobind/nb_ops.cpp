a74d75f4d21853b0920ae9edddf4a7dc07de1342
dyurshevichTT <dyurshevich@tenstorrent.com>
2026-01-05 17:39:08 +0000

    precommit hooks fix

--- tt-train/sources/ttml/nanobind/nb_ops.cpp
+++ tt-train/sources/ttml/nanobind/nb_ops.cpp
@@ -107,20 +107,19 @@ void py_module(nb::module_& m) {
             nb::arg("noop_backward") = false,
             nb::arg("cluster_axis") = nb::none());
         py_distributed.def(
-            "reduce_scatter", 
-            &ttml::ops::distributed::reduce_scatter, 
-            nb::arg("tensor"), 
-            nb::arg("dim"),
-            nb::arg("cluster_axis") = nb::none());
-        py_distributed.def("all_gather", 
-            &ttml::ops::distributed::all_gather, 
-            nb::arg("tensor"), 
-            nb::arg("dim"),
-            nb::arg("cluster_axis") = nb::none());
-        py_distributed.def("broadcast", 
-            &ttml::ops::distributed::broadcast, 
+            "reduce_scatter",
+            &ttml::ops::distributed::reduce_scatter,
             nb::arg("tensor"),
+            nb::arg("dim"),
             nb::arg("cluster_axis") = nb::none());
+        py_distributed.def(
+            "all_gather",
+            &ttml::ops::distributed::all_gather,
+            nb::arg("tensor"),
+            nb::arg("dim"),
+            nb::arg("cluster_axis") = nb::none());
+        py_distributed.def(
+            "broadcast", &ttml::ops::distributed::broadcast, nb::arg("tensor"), nb::arg("cluster_axis") = nb::none());
     }
 
     {
