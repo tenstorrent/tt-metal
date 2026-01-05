b5711f55450f078df6f8ca1683767952a16ed39d
Sofija Jovic <sjovic@tenstorrent.com>
2026-01-05 11:58:37 +0000

    update vovnet treshold

--- models/experimental/vovnet/tests/perf/test_e2e_performant.py
+++ models/experimental/vovnet/tests/perf/test_e2e_performant.py
@@ -16,7 +16,7 @@ from models.experimental.vovnet.common import VOVNET_L1_SMALL_SIZE
 
 
 def get_expected_times(name):
-    base = {"vovnet": (172, 0.0082)}
+    base = {"vovnet": (172, 0.0085)}
     return base[name]
 
 
