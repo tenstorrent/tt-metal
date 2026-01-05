8fd3d98a723f66f35175c64112426645b6eb12fe
jpanasiuk <jpanasiuk@tenstorrent.com>
2026-01-05 18:33:07 +0000

    create a framework for multi process send/recv tests

--- ttnn/api/ttnn/cluster.hpp
+++ ttnn/api/ttnn/cluster.hpp
@@ -6,6 +6,7 @@
 
 #include "ttnn/types.hpp"
 #include <tt-metalium/tt_metal.hpp>
+#include <tt-metalium/mesh_coord.hpp>
 
 namespace ttnn {
 
@@ -13,6 +14,7 @@ namespace cluster {
 
 tt::tt_metal::ClusterType get_cluster_type();
 std::string serialize_cluster_descriptor();
+tt::tt_metal::distributed::MeshShape get_mesh_shape();
 
 }  // namespace cluster
 
