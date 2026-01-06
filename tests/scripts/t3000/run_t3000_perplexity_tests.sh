c8ceee95fe05bddcc1edaee6e2020f4b449905ea
Salar Hosseini <159165450+skhorasganiTT@users.noreply.github.com>
2026-01-06 16:35:22 -0500

    Remove falcon7b perplexity tests from CI since they're no longer useful (#35341)
    ### Ticket
    N/A
    
    ### Problem description
    - We no longer monitor perplexity scores for Falcon7b and they're
    redundant with the output checks in the demo tests / the pcc checks in
    the perf tests for monitoring accuracy regressions.
    
    ### What's changed
    - Removed the falcon7b perplexity tests from CI
    
    ### Checklist
    
    - T3K perplexity tests -
    https://github.com/tenstorrent/tt-metal/actions/runs/20761384144
    
    ---------
    
    Signed-off-by: Salar <skhorasgani@tenstorrent.com>

--- tests/scripts/t3000/run_t3000_perplexity_tests.sh
+++ tests/scripts/t3000/run_t3000_perplexity_tests.sh
@@ -2,25 +2,6 @@
 
 TT_CACHE_HOME=/mnt/MLPerf/huggingface/tt_cache
 
-run_t3000_falcon7b_perplexity_tests() {
-  # Record the start time
-  fail=0
-  start_time=$(date +%s)
-
-  echo "LOG_METAL: Running run_t3000_falcon7b_perplexity_tests"
-
-  # Falcon7B perplexity tests
-  pytest -n auto models/demos/falcon7b_common/tests/perplexity/test_perplexity_falcon.py --timeout=1500 ; fail+=$?
-
-  # Record the end time
-  end_time=$(date +%s)
-  duration=$((end_time - start_time))
-  echo "LOG_METAL: run_t3000_falcon7b_perplexity_tests $duration seconds to complete"
-  if [[ $fail -ne 0 ]]; then
-    exit 1
-  fi
-}
-
 run_t3000_falcon40b_perplexity_tests() {
   # Record the start time
   fail=0
@@ -240,9 +221,6 @@ run_t3000_tests() {
 
   # Run Qwen3 perplexity tests
   run_t3000_qwen3_perplexity_tests
-
-  # Run Falcon-7B perplexity tests
-  run_t3000_falcon7b_perplexity_tests
 
   # Run Falcon-40B perplexity tests
   run_t3000_falcon40b_perplexity_tests
