f4e7b614ba7bbd08e054879f163181a164bb3345
River Wu <riverwu@tenstorrent.com>
2026-01-06 15:10:00 -0800

    Cleanup `circular_buffer.hpp` in API folder (#31554)
    ### Ticket
    Close #29724
    
    ### Problem description
    Surface Area Reduction Stage 1 for Circular Buffer related headers.
    
    Inspected Area:
    1. circular_buffer.hpp
    2. ~~circular_buffer_constants.h~~ omitted as there's device side
    dependency.
    3. ~~circular_buffer_config.hpp~~ omitted as there's external depdency.
    
    ### What's changed
    Removed unused circular buffer APIs from the API directory.
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/runs/19724596178)

--- tt_metal/api/tt-metalium/host_api.hpp
+++ tt_metal/api/tt-metalium/host_api.hpp
@@ -9,6 +9,7 @@
 #include <variant>
 #include <vector>
 
+#include <tt-metalium/buffer.hpp>
 #include <tt-metalium/circular_buffer.hpp>
 #include <tt-metalium/dispatch_core_common.hpp>
 #include <tt-metalium/runtime_args_data.hpp>
@@ -18,6 +19,8 @@
 #include <tt_stl/span.hpp>
 #include <tt-metalium/profiler_types.hpp>
 #include <tt-metalium/profiler_optional_metadata.hpp>
+#include <tt-metalium/kernel_types.hpp>
+#include <tt-metalium/circular_buffer_config.hpp>
 
 /** @file */
 
@@ -38,7 +41,6 @@ struct TraceDescriptor;
 class Program;
 class IDevice;
 class Trace;
-class CircularBuffer;
 struct Event;
 class Buffer;
 class GlobalSemaphore;
