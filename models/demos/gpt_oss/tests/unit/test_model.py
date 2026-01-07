78102f1bb9aa261bc3326481ecb7539034dd5f2f
handrewsTT <handrews@tenstorrent.com>
2026-01-07 13:11:32 +0000

    acc test

--- models/demos/gpt_oss/tests/unit/test_model.py
+++ models/demos/gpt_oss/tests/unit/test_model.py
@@ -8,6 +8,7 @@ Full model integration test with accuracy testing
 import os
 import pickle
 
+import pytest
 import torch
 from loguru import logger
 
@@ -139,7 +140,18 @@ def run_accuracy(
 
 
 @parametrize_mesh_with_fabric()
-def test_full_model_accuracy(mesh_device, device_params, reset_seeds, state_dict):
+@pytest.mark.parametrize(
+    "mesh_shape",
+    [
+        (1, 8),
+        (4, 8),
+    ],
+    ids=[
+        "mesh_1x8",
+        "mesh_4x8",
+    ],
+)
+def test_full_model_accuracy(mesh_device, mesh_shape, device_params, reset_seeds, state_dict):
     """Test full model with accuracy testing using new abstractions"""
 
     # Cache file for reference tokens
