749a65c169bb70be16e4e4872c91d2af2694cd89
grechsteinerTT <grechsteiner@tenstorrent.com>
2026-01-07 16:18:10 +0000

    send_recv_async

--- ttnn/cpp/ttnn/operations/experimental/ccl/send_recv_async/send_async/device/send_async_op_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/send_recv_async/send_async/device/send_async_op_device_operation.cpp
@@ -46,10 +46,9 @@ SendAsyncDeviceOperation::tensor_return_value_t SendAsyncDeviceOperation::create
 
 tt::stl::hash::hash_t SendAsyncDeviceOperation::compute_program_hash(
     const operation_attributes_t& args, const tensor_args_t& tensor_args) {
-    const auto& mesh_socket = args.mesh_socket;
-    const auto& input_tensors = tensor_args.input_tensor;
+    const ttnn::Tensor& input_tensor = tensor_args.input_tensor;
 
-    return tt::tt_metal::operation::hash_operation<SendAsyncDeviceOperation>(mesh_socket, input_tensors);
+    return tt::tt_metal::operation::hash_operation<SendAsyncDeviceOperation>(args.mesh_socket, input_tensor);
 }
 
 }  // namespace ttnn::operations::experimental::ccl::send_async
