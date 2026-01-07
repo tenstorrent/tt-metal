423d6a863b9d76b38b644e5b6b42a23612391fde
Colman Glagovich <cglagovich@tenstorrent.com>
2026-01-07 20:35:48 +0000

    Specialize welford's distributed norm to its own op

---
+++ ttnn/cpp/ttnn/operations/experimental/transformer/dit_layernorm_distributed/dit_layernorm_distributed_nanobind.hpp
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "ttnn-nanobind/nanobind_fwd.hpp"
+
+namespace ttnn::operations::experimental::transformer {
+namespace nb = nanobind;
+void bind_dit_layernorm_distributed(nb::module_& mod);
+}  // namespace ttnn::operations::experimental::transformer
