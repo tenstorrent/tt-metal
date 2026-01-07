a140943f78c7b51f070d85c9934f133f909e1bd0
grechsteinerTT <grechsteiner@tenstorrent.com>
2026-01-07 21:09:16 +0000

    bcast

--- ttnn/cpp/ttnn/operations/data_movement/bcast/device/bcast_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/bcast/device/bcast_device_operation.cpp
@@ -231,18 +231,19 @@ Tensor BcastDeviceOperation::create_output_tensors(
 
 tt::stl::hash::hash_t BcastDeviceOperation::compute_program_hash(
     const operation_attributes_t& operation_attributes, const tensor_args_t& tensor_args) {
-    const program_factory_t program_factory = select_program_factory(operation_attributes, tensor_args);
-    const bool bcast_scalar = (tensor_args.input_b.padded_shape()[-2] * tensor_args.input_b.padded_shape()[-1] == 1) &&
-                              operation_attributes.dim == BcastOpDim::HW;
+    log_trace(tt::LogOp, "BcastDeviceOperation::compute_program_hash is called");
+
+    const ttnn::Tensor& input_a = tensor_args.input_a;
+    const ttnn::Tensor& input_b = tensor_args.input_b;
+    const std::optional<ttnn::Tensor>& preallocated_output_tensor = tensor_args.preallocated_output;
+
+    const bool bcast_scalar =
+        (input_b.padded_shape()[-2] * input_b.padded_shape()[-1] == 1) && operation_attributes.dim == BcastOpDim::HW;
+
+    auto program_factory = select_program_factory(operation_attributes, tensor_args);
+
     return operation::hash_operation<BcastDeviceOperation>(
-        operation_attributes,
-        program_factory.index(),
-        tensor_args.input_a.memory_config(),
-        tensor_args.input_a.dtype(),
-        tensor_args.input_b.memory_config(),
-        tensor_args.input_b.dtype(),
-        bcast_scalar,
-        operation_attributes.in_place);
+        operation_attributes, bcast_scalar, input_a, input_b, preallocated_output_tensor, program_factory.index());
 }
 
 tt::tt_metal::operation::OpPerformanceModelGeneral<Tensor> BcastDeviceOperation::create_op_performance_model(
