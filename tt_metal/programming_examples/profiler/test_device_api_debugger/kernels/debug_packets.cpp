c3261dd140304fb93f7bad98e009e6d6ab49d3ba
Nigel Huang <nhuang@tenstorrent.com>
2026-01-07 00:26:13 +0000

    debug test

--- tt_metal/programming_examples/profiler/test_device_api_debugger/kernels/debug_packets.cpp
+++ tt_metal/programming_examples/profiler/test_device_api_debugger/kernels/debug_packets.cpp
@@ -9,9 +9,11 @@
 using EMD = KernelProfilerNocEventMetadata;
 
 void kernel_main() {
+#if defined(PROFILE_NOC_EVENTS)
     riscv_wait(START_DELAY);
     // Make this really large so it exceeds the max profiler sizes to test mid run dumps
     for (uint32_t i = 0; i < 10000; ++i) {
         noc_event_profiler::recordNocEvent(EMD::NocEventType::READ, my_x[0], my_y[0], 1000, 0, 0, i);
     }
+#endif
 }
