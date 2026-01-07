91a32ae57149e409396ece9a70eac8213014c3c0
Aswin Zayasankaran <156493059+Aswinmcw@users.noreply.github.com>
2026-01-07 15:31:45 +0530

    Fix formatting in common.hpp return statement

--- ttnn/cpp/ttnn/operations/data_movement/common/common.hpp
+++ ttnn/cpp/ttnn/operations/data_movement/common/common.hpp
@@ -127,7 +127,7 @@ public:
             if (p2(args...)) {
                 *t2_required = true;
             }
-            return *t1_required or * t2_required;
+            return *t1_required or *t2_required;
         };
 
         auto merged_pre_transform = [t1 = this->pre_transform_,
