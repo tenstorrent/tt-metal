a0626faa1e71ee03eab9b9e98e75dded21134b52
kpaigwar <kpaigwar@tenstorrent.com>
2026-01-08 22:45:59 +0000

    make same in0_block_w

--- models/demos/llama3_70b_galaxy/tt/model_config.py
+++ models/demos/llama3_70b_galaxy/tt/model_config.py
@@ -793,7 +793,7 @@ class TtModelArgs:
 
             def w1_w3_prg_config(seq_len, use_interleaved):
                 if seq_len == 128:
-                    return self.matmul_1d_config(128, 2048, 3584, grid=ttnn.CoreGrid(x=7, y=4), overwrite_per_core_k=16)
+                    return self.matmul_1d_config(128, 2048, 3584, grid=ttnn.CoreGrid(x=7, y=4), overwrite_per_core_k=4)
                 if not use_interleaved:
                     return ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
                         compute_with_storage_grid_size=(7, 10),
@@ -1040,7 +1040,7 @@ class TtModelArgs:
 
             self.model_config["WO_PREFILL_PROGCFG"] = (
                 lambda seq_len: self.matmul_1d_config(
-                    seq_len, 1024, 2048, grid=ttnn.CoreGrid(x=7, y=10), overwrite_per_core_k=16
+                    seq_len, 1024, 2048, grid=ttnn.CoreGrid(x=7, y=10), overwrite_per_core_k=8
                 )
                 if seq_len == 128
                 else (
@@ -1108,7 +1108,7 @@ class TtModelArgs:
             self.min_kv_prefill_shard_seqlen = (self.tile_size * 8 * 8) / (self.n_kv_heads // self.cluster_shape[1])
             self.model_config["XQKV_PREFILL_PROGCFG"] = (
                 lambda seq_len: self.matmul_1d_config(
-                    seq_len, 2048, 1280, grid=ttnn.CoreGrid(x=4, y=10), overwrite_per_core_k=16
+                    seq_len, 2048, 1280, grid=ttnn.CoreGrid(x=4, y=10), overwrite_per_core_k=8
                 )
                 if seq_len == 128
                 else (
