774b9de29e17adabdb2ed6c348dacc1739963cc0
vsureshTT <vsuresh@tenstorrent.com>
2026-01-09 16:42:02 +0000

    changes where MM0 and MM1 work well PCC. But craters when testing PCC of eltwise

--- ttnn/cpp/ttnn/operations/experimental/moe/moe.cpp
+++ ttnn/cpp/ttnn/operations/experimental/moe/moe.cpp
@@ -12,8 +12,11 @@ ttnn::Tensor ExecuteMoE::invoke(
     const ttnn::Tensor& w0_tensor,
     const ttnn::Tensor& w1_tensor,
     const ttnn::Tensor& w2_tensor,
-    const ttnn::Tensor& output_tensor) {
-    return ttnn::prim::moe(input_tensor, w0_tensor, w1_tensor, w2_tensor, output_tensor);
+    const ttnn::Tensor& output_tensor,
+    MathFidelity math_fidelity,
+    bool fp32_dest_acc_en) {
+    return ttnn::prim::moe(
+        input_tensor, w0_tensor, w1_tensor, w2_tensor, output_tensor, math_fidelity, fp32_dest_acc_en);
 }
 
 }  // namespace ttnn::operations::experimental::moe
