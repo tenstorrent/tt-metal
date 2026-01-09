0fa7c3b8ea638f5c91dbe85cfceb86e169035ff0
Ziemowit Bączewski <zbaczewski@tenstorrent.com>
2026-01-09 16:09:02 +0000

    Use maximal available core grid instead of hardcoded; Remove unnecessary include

--- tt-train/sources/ttml/ttnn_fixed/matmuls.cpp
+++ tt-train/sources/ttml/ttnn_fixed/matmuls.cpp
@@ -7,8 +7,12 @@
 #include "core/compute_kernel_config.hpp"
 
 namespace ttml::ttnn_fixed {
+
 tt::tt_metal::Tensor matmul(
     const tt::tt_metal::Tensor& a, const tt::tt_metal::Tensor& b, bool transpose_a, bool transpose_b) {
+    const auto grid_size = a.device()->compute_with_storage_grid_size();
+    std::optional<ttnn::CoreGrid> core_grid = ttnn::CoreGrid{grid_size.x, grid_size.y};
+
     return ttnn::matmul(
         a,
         b,
@@ -20,7 +24,7 @@ tt::tt_metal::Tensor matmul(
         /* activation */ std::nullopt,
         /* compute_kernel_config */
         ttml::core::ComputeKernelConfig::matmul(),
-        /* core_grid */ ttnn::CoreGrid{7, 8},
+        /* core_grid */ core_grid,
         /* output_tile */ std::nullopt);
 }
 
