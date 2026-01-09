ac9a220212e6d50081ee25fedf10aca2e2f2e59d
asaigal <asaigal@tenstorrent.com>
2026-01-09 02:27:46 +0000

    Add fabric unit tests script for 8x16 + light validation script

---
+++ run_fabric_tests_8x16.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source python_env/bin/activate
+export TT_METAL_HOME=/home/local-syseng/tt-metal
+tt-run --rank-binding tests/tt_metal/distributed/config/quad_galaxy_rank_bindings.yaml --mpi-args "--host bh-glx-c01u02,bh-glx-c01u08,bh-glx-c02u02,bh-glx-c02u08 --mca btl_tcp_if_include ens5f0np0 --tag-output" ./build/test/tt_metal/perf_microbenchmark/routing/test_tt_fabric --test_config tests/tt_metal/tt_metal/perf_microbenchmark/routing/test_fabric_deadlock_stability_bh_6U_galaxy.yaml
+tt-run --rank-binding tests/tt_metal/distributed/config/quad_galaxy_rank_bindings.yaml --mpi-args "--host bh-glx-c01u02,bh-glx-c01u08,bh-glx-c02u02,bh-glx-c02u08 --mca btl_tcp_if_include ens5f0np0 --tag-output" ./build/test/tt_metal/perf_microbenchmark/routing/test_tt_fabric --test_config tests/tt_metal/tt_metal/perf_microbenchmark/routing/test_dual_galaxy_fabric_2d_sanity.yaml
--- tt_metal/llrt/rtoptions.hpp
+++ tt_metal/llrt/rtoptions.hpp
@@ -253,7 +253,7 @@ class RunTimeOptions {
     bool disable_fabric_2_erisc_mode = false;
 
     // feature flag to enable 2-erisc mode on Blackhole (general, not fabric-specific)
-    bool enable_2_erisc_mode = false;
+    bool enable_2_erisc_mode = true;
 
     // Log kernels compilation commands
     bool log_kernels_compilation_commands = false;
