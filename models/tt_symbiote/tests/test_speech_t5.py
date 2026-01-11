5742f98868469558fd41e6a7026f26bbbb0104b9
Suhail Alnahari <salnahari@tenstorrent.com>
2026-01-10 20:37:38 -0500

    tt_symbiote: added speech_t5 timing code

--- models/tt_symbiote/tests/test_speech_t5.py
+++ models/tt_symbiote/tests/test_speech_t5.py
@@ -1,9 +1,11 @@
 """Test for Speech T5 model with TTNN backend."""
 
+import soundfile as sf
 import torch
 from torch import nn
 from transformers import SpeechT5ForTextToSpeech, SpeechT5HifiGan, SpeechT5Processor
 
+from models.tt_symbiote.core.run_config import DispatchManager
 from models.tt_symbiote.modules.activation import TTNNSilu
 from models.tt_symbiote.modules.linear import TTNNLinear
 from models.tt_symbiote.modules.normalization import TTNNLayerNorm
@@ -20,11 +22,13 @@ def test_speech_t5(device):
         nn.LayerNorm: TTNNLayerNorm,
     }
     processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
-    model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
+    model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts").to(dtype=torch.bfloat16)
     vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
-    inputs = processor(text="Hello, my dog is cute.", return_tensors="pt")
+    inputs = processor(
+        text="What is your favorite condiment? There are so many condiments to choose from, each bringing its unique flavor and texture to enhance different dishes. Do you prefer the classic taste of ketchup, the creamy richness of mayonnaise, the spicy kick of mustard, or perhaps something more exotic like sriracha or hoisin sauce? Maybe you enjoy the tangy zest of salsa or the smooth and savory taste of aioli. Share what your favorite condiment is and why you love it. Does it remind you of a specific dish or meal?",
+        return_tensors="pt",
+    )
     inputs["speaker_embeddings"] = torch.randn((1, 512))
-    inputs["maxlenratio"] = 1
     model.eval()  # Disables dropout, batch norm updates
     vocoder.eval()
     register_module_replacement_dict(model, nn_to_ttnn, model_config=None)
@@ -32,4 +36,8 @@ def test_speech_t5(device):
     set_device(model, device)
     set_device(vocoder, device)
     torch.set_grad_enabled(False)  # Disables autograd overhead
+    DispatchManager.clear_timings()
     speech = model.generate_speech(**inputs, vocoder=vocoder)
+    DispatchManager.save_stats_to_file("speech_t5_timing_stats.csv")
+    sf.write("output.wav", speech.squeeze().detach().numpy(), samplerate=16000)
+    print("Speech T5 TTNN test passed, output.wav generated.")
