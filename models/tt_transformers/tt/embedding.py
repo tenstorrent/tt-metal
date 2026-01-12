dae7b1bbd8501b320c07ffa455ca2aaab5b82942
KalaivaniMCW <kbaskar@tenstorrent.com>
2026-01-12 13:52:03 +0000

    #33701: fix in MLP , tranformers

--- models/tt_transformers/tt/embedding.py
+++ models/tt_transformers/tt/embedding.py
@@ -44,5 +44,5 @@ class ScaledEmbedding(Embedding):
 
     def forward(self, x: ttnn.Tensor) -> ttnn.Tensor:
         e = ttnn.embedding(x, self.weights, layout=ttnn.TILE_LAYOUT, memory_config=ttnn.DRAM_MEMORY_CONFIG)
-        s = ttnn.multiply(e, self.embed_scale, memory_config=ttnn.DRAM_MEMORY_CONFIG)
+        s = ttnn.multiply(e, self.embed_scale, memory_config=ttnn.DRAM_MEMORY_CONFIG, fast_and_approximate_mode=True)
         return s
