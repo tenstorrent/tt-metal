412f2d2f0449211330fc31ad7c0bb07b1b8ba1a0
Nenad Babin <nbabin@tenstorrent.com>
2026-01-12 16:00:54 +0000

    Optimized FeedForward Linear ops

--- models/experimental/stable_diffusion_xl_base/refiner/tests/matmul_opt/test_matmul_optimized.py
+++ models/experimental/stable_diffusion_xl_base/refiner/tests/matmul_opt/test_matmul_optimized.py
@@ -796,3 +796,369 @@ def test_RESNET_LINEAR2_16384x768x384(device):
         program_config=program_config,
         compute_kernel_config=compute_kernel_config,
     )
+
+
+def test_GEGLU_LINEAR1_4096x768x3072(device):
+    in0_shape = [1, 1, 4096, 768]
+    in1_shape = [768, 3072]
+    bias_shape = [3072]
+
+    in0_torch = torch.randn(in0_shape).bfloat16().float()
+    in1_torch = torch.randn(in1_shape).bfloat16().float()
+    bias_torch = torch.randn(bias_shape).bfloat16().float()
+
+    in0_tt = ttnn.from_torch(
+        in0_torch,
+        dtype=ttnn.bfloat16,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    in1_tt = ttnn.from_torch(
+        in1_torch,
+        dtype=ttnn.bfloat8_b,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    bias_tt = ttnn.from_torch(
+        bias_torch, dtype=ttnn.bfloat8_b, device=device, layout=ttnn.TILE_LAYOUT, memory_config=ttnn.DRAM_MEMORY_CONFIG
+    )
+
+    compute_kernel_config = ttnn.WormholeComputeKernelConfig(
+        math_fidelity=ttnn.MathFidelity.HiFi2,
+        math_approx_mode=True,
+        fp32_dest_acc_en=False,
+        packer_l1_acc=True,
+    )
+
+    grid_size = (8, 8)
+    per_core_M = (4096 // 32) // grid_size[0]
+    per_core_N = (3072 // 32) // grid_size[1]
+    program_config = ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+        compute_with_storage_grid_size=grid_size,
+        in0_block_w=6,
+        per_core_M=per_core_M,
+        per_core_N=per_core_N,
+        out_subblock_h=1,
+        out_subblock_w=6,
+        transpose_mcast=False,
+        fused_activation=None,
+    )
+
+    out = ttnn.linear(
+        in0_tt,
+        in1_tt,
+        bias=bias_tt,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        dtype=ttnn.bfloat16,
+        program_config=program_config,
+        compute_kernel_config=compute_kernel_config,
+    )
+
+
+def test_GEGLU_LINEAR1_1024x1536x6144(device):
+    in0_shape = [1, 1, 1024, 1536]
+    in1_shape = [1536, 6144]
+    bias_shape = [6144]
+
+    in0_torch = torch.randn(in0_shape).bfloat16().float()
+    in1_torch = torch.randn(in1_shape).bfloat16().float()
+    bias_torch = torch.randn(bias_shape).bfloat16().float()
+
+    in0_tt = ttnn.from_torch(
+        in0_torch,
+        dtype=ttnn.bfloat16,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    in1_tt = ttnn.from_torch(
+        in1_torch,
+        dtype=ttnn.bfloat8_b,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    bias_tt = ttnn.from_torch(
+        bias_torch, dtype=ttnn.bfloat8_b, device=device, layout=ttnn.TILE_LAYOUT, memory_config=ttnn.DRAM_MEMORY_CONFIG
+    )
+
+    compute_kernel_config = ttnn.WormholeComputeKernelConfig(
+        math_fidelity=ttnn.MathFidelity.HiFi2,
+        math_approx_mode=True,
+        fp32_dest_acc_en=False,
+        packer_l1_acc=True,
+    )
+
+    grid_size = (8, 8)
+    per_core_M = (1024 // 32) // grid_size[0]
+    per_core_N = (6144 // 32) // grid_size[1]
+    program_config = ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+        compute_with_storage_grid_size=grid_size,
+        in0_block_w=8,
+        per_core_M=per_core_M,
+        per_core_N=per_core_N,
+        out_subblock_h=1,
+        out_subblock_w=8,
+        transpose_mcast=False,
+        fused_activation=None,
+    )
+
+    out = ttnn.linear(
+        in0_tt,
+        in1_tt,
+        bias=bias_tt,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        dtype=ttnn.bfloat16,
+        program_config=program_config,
+        compute_kernel_config=compute_kernel_config,
+    )
+
+
+def test_GEGLU_LINEAR1_256x1536x6144(device):
+    in0_shape = [1, 1, 256, 1536]
+    in1_shape = [1536, 6144]
+    bias_shape = [6144]
+
+    in0_torch = torch.randn(in0_shape).bfloat16().float()
+    in1_torch = torch.randn(in1_shape).bfloat16().float()
+    bias_torch = torch.randn(bias_shape).bfloat16().float()
+
+    in0_tt = ttnn.from_torch(
+        in0_torch,
+        dtype=ttnn.bfloat16,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    in1_tt = ttnn.from_torch(
+        in1_torch,
+        dtype=ttnn.bfloat8_b,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    bias_tt = ttnn.from_torch(
+        bias_torch, dtype=ttnn.bfloat8_b, device=device, layout=ttnn.TILE_LAYOUT, memory_config=ttnn.DRAM_MEMORY_CONFIG
+    )
+
+    compute_kernel_config = ttnn.WormholeComputeKernelConfig(
+        math_fidelity=ttnn.MathFidelity.HiFi2,
+        math_approx_mode=True,
+        fp32_dest_acc_en=False,
+        packer_l1_acc=True,
+    )
+
+    grid_size = (8, 8)
+    per_core_M = (256 // 32) // grid_size[0]
+    per_core_N = (6144 // 32) // grid_size[1]
+    program_config = ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+        compute_with_storage_grid_size=grid_size,
+        in0_block_w=4,
+        per_core_M=per_core_M,
+        per_core_N=per_core_N,
+        out_subblock_h=1,
+        out_subblock_w=6,
+        transpose_mcast=False,
+        fused_activation=None,
+    )
+
+    out = ttnn.linear(
+        in0_tt,
+        in1_tt,
+        bias=bias_tt,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        dtype=ttnn.bfloat16,
+        program_config=program_config,
+        compute_kernel_config=compute_kernel_config,
+    )
+
+
+def test_FF_LINEAR_4096x3072x768(device):
+    in0_shape = [1, 1, 4096, 3072]
+    in1_shape = [3072, 768]
+    bias_shape = [768]
+
+    in0_torch = torch.randn(in0_shape).bfloat16().float()
+    in1_torch = torch.randn(in1_shape).bfloat16().float()
+    bias_torch = torch.randn(bias_shape).bfloat16().float()
+
+    in0_tt = ttnn.from_torch(
+        in0_torch,
+        dtype=ttnn.bfloat16,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    in1_tt = ttnn.from_torch(
+        in1_torch,
+        dtype=ttnn.bfloat8_b,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    bias_tt = ttnn.from_torch(
+        bias_torch, dtype=ttnn.bfloat8_b, device=device, layout=ttnn.TILE_LAYOUT, memory_config=ttnn.DRAM_MEMORY_CONFIG
+    )
+
+    compute_kernel_config = ttnn.WormholeComputeKernelConfig(
+        math_fidelity=ttnn.MathFidelity.HiFi2,
+        math_approx_mode=True,
+        fp32_dest_acc_en=False,
+        packer_l1_acc=True,
+    )
+
+    grid_size = (8, 8)
+    per_core_M = (4096 // 32) // grid_size[0]
+    per_core_N = (768 // 32) // grid_size[1]
+    program_config = ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+        compute_with_storage_grid_size=grid_size,
+        in0_block_w=6,
+        per_core_M=per_core_M,
+        per_core_N=per_core_N,
+        out_subblock_h=2,
+        out_subblock_w=3,
+        transpose_mcast=False,
+        fused_activation=None,
+    )
+
+    out = ttnn.linear(
+        in0_tt,
+        in1_tt,
+        bias=bias_tt,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        dtype=ttnn.bfloat16,
+        program_config=program_config,
+        compute_kernel_config=compute_kernel_config,
+    )
+
+
+def test_FF_LINEAR_1024x6144x1536(device):
+    in0_shape = [1, 1, 1024, 6144]
+    in1_shape = [6144, 1536]
+    bias_shape = [1536]
+
+    in0_torch = torch.randn(in0_shape).bfloat16().float()
+    in1_torch = torch.randn(in1_shape).bfloat16().float()
+    bias_torch = torch.randn(bias_shape).bfloat16().float()
+
+    in0_tt = ttnn.from_torch(
+        in0_torch,
+        dtype=ttnn.bfloat16,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    in1_tt = ttnn.from_torch(
+        in1_torch,
+        dtype=ttnn.bfloat8_b,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    bias_tt = ttnn.from_torch(
+        bias_torch, dtype=ttnn.bfloat8_b, device=device, layout=ttnn.TILE_LAYOUT, memory_config=ttnn.DRAM_MEMORY_CONFIG
+    )
+
+    compute_kernel_config = ttnn.WormholeComputeKernelConfig(
+        math_fidelity=ttnn.MathFidelity.HiFi2,
+        math_approx_mode=True,
+        fp32_dest_acc_en=False,
+        packer_l1_acc=True,
+    )
+
+    grid_size = (8, 8)
+    per_core_M = (1024 // 32) // grid_size[0]
+    per_core_N = (1536 // 32) // grid_size[1]
+    program_config = ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+        compute_with_storage_grid_size=grid_size,
+        in0_block_w=16,
+        per_core_M=per_core_M,
+        per_core_N=per_core_N,
+        out_subblock_h=1,
+        out_subblock_w=6,
+        transpose_mcast=False,
+        fused_activation=None,
+    )
+
+    out = ttnn.linear(
+        in0_tt,
+        in1_tt,
+        bias=bias_tt,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        dtype=ttnn.bfloat16,
+        program_config=program_config,
+        compute_kernel_config=compute_kernel_config,
+    )
+
+
+def test_FF_LINEAR_256x6144x1536(device):
+    in0_shape = [1, 1, 256, 6144]
+    in1_shape = [6144, 1536]
+    bias_shape = [1536]
+
+    in0_torch = torch.randn(in0_shape).bfloat16().float()
+    in1_torch = torch.randn(in1_shape).bfloat16().float()
+    bias_torch = torch.randn(bias_shape).bfloat16().float()
+
+    in0_tt = ttnn.from_torch(
+        in0_torch,
+        dtype=ttnn.bfloat16,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    in1_tt = ttnn.from_torch(
+        in1_torch,
+        dtype=ttnn.bfloat8_b,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+    )
+
+    bias_tt = ttnn.from_torch(
+        bias_torch, dtype=ttnn.bfloat8_b, device=device, layout=ttnn.TILE_LAYOUT, memory_config=ttnn.DRAM_MEMORY_CONFIG
+    )
+
+    compute_kernel_config = ttnn.WormholeComputeKernelConfig(
+        math_fidelity=ttnn.MathFidelity.HiFi2,
+        math_approx_mode=True,
+        fp32_dest_acc_en=False,
+        packer_l1_acc=True,
+    )
+
+    grid_size = (8, 8)
+    per_core_M = (256 // 32) // grid_size[0]
+    per_core_N = (1536 // 32) // grid_size[1]
+    program_config = ttnn.MatmulMultiCoreReuseMultiCastProgramConfig(
+        compute_with_storage_grid_size=grid_size,
+        in0_block_w=12,
+        per_core_M=per_core_M,
+        per_core_N=per_core_N,
+        out_subblock_h=1,
+        out_subblock_w=1,
+        transpose_mcast=False,
+        fused_activation=None,
+    )
+
+    out = ttnn.linear(
+        in0_tt,
+        in1_tt,
+        bias=bias_tt,
+        memory_config=ttnn.DRAM_MEMORY_CONFIG,
+        dtype=ttnn.bfloat16,
+        program_config=program_config,
+        compute_kernel_config=compute_kernel_config,
+    )
