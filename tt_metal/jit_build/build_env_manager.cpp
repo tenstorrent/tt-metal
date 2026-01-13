7e73f36029c468c8ee4ff6a7ed9d4e0801aa4416
Samarth Agarwal <sagarwal@tenstorrent.com>
2026-01-13 17:52:12 +0000

    Modify to use the hal to get the number of fw binaries that should be generated

--- tt_metal/jit_build/build_env_manager.cpp
+++ tt_metal/jit_build/build_env_manager.cpp
@@ -131,7 +131,19 @@ std::vector<JitBuildState> create_build_state(JitBuildEnv& build_env, bool is_fw
 
     // Prepare the container for build states
     const auto& hal = MetalContext::instance().hal();
-    uint32_t num_build_states = hal.get_total_num_risc_processors();
+    uint32_t num_build_states = 0;
+    if (is_fw) {
+        for (uint32_t programmable_core = 0; programmable_core < hal.get_programmable_core_type_count();
+             programmable_core++) {
+            const uint32_t processor_class_count =
+                hal.get_processor_classes_count(hal.get_programmable_core_type(programmable_core));
+            for (uint32_t processor_class = 0; processor_class < processor_class_count; processor_class++) {
+                num_build_states += hal.get_processor_class_num_fw_binaries(programmable_core, processor_class);
+            }
+        }
+    } else {
+        num_build_states = hal.get_total_num_risc_processors();
+    }
     std::vector<JitBuildState> build_states;
     build_states.reserve(num_build_states);
 
@@ -149,11 +161,13 @@ std::vector<JitBuildState> create_build_state(JitBuildEnv& build_env, bool is_fw
                 .is_cooperative = hal.get_eth_fw_is_cooperative(),
             };
 
-            // On Quasar, all DM processors share the same firmware binary, so we only need to create a single firmware
-            // build state.
-            if (is_fw && config.processor_class == HalProcessorClassType::DM &&
-                MetalContext::instance().get_cluster().arch() == ARCH::QUASAR) {
-                build_states.emplace_back(build_env, config);
+            if (is_fw) {
+                const uint32_t processor_class_num_fw_binaries =
+                    hal.get_processor_class_num_fw_binaries(programmable_core, processor_class);
+                for (uint32_t processor_type = 0; processor_type < processor_class_num_fw_binaries; processor_type++) {
+                    config.processor_id = processor_type;
+                    build_states.emplace_back(build_env, config);
+                }
             } else {
                 const uint32_t processor_types_count =
                     hal.get_processor_types_count(programmable_core, processor_class);
@@ -164,9 +178,8 @@ std::vector<JitBuildState> create_build_state(JitBuildEnv& build_env, bool is_fw
             }
         }
     }
-    if (!(is_fw && MetalContext::instance().get_cluster().arch() == ARCH::QUASAR)) {
-        TT_ASSERT(build_states.size() == num_build_states);
-    }
+
+    TT_ASSERT(build_states.size() == num_build_states);
     return build_states;
 }
 
