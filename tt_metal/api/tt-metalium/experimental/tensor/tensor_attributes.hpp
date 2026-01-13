32e40bfbb7cf933166865c539e0052bc7cf304de
riverwuTT <riverwu@tenstorrent.com>
2026-01-13 03:04:21 +0000

    init directory structure

---
+++ tt_metal/api/tt-metalium/experimental/tensor/tensor_attributes.hpp
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: © 2026 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+namespace tt::tt_metal {
+
+// TODO: Implement tensor attributes
+
+}  // namespace tt::tt_metal
