a1a87e531ff2d45760909d426351e1de6e72da37
CodeMan62 <sharmahimanshu15082007@gmail.com>
2026-01-15 01:01:22 +0530

    test failures

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_binary_sfpu_max_pool_indices.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/llk_math_eltwise_binary_sfpu_max_pool_indices.h
@@ -7,13 +7,14 @@
 #include "llk_math_eltwise_binary_sfpu_init.h"
 #include "llk_math_eltwise_binary_sfpu_params.h"
 #include "ckernel_sfpu_max_pool_indices.h"
+#include "llk_defs.h"
 
 namespace ckernel {
 
 template <bool APPROXIMATE, ckernel::DataLayout layout = ckernel::DataLayout::TILE>
 inline void llk_math_eltwise_binary_sfpu_max_pool_with_indices_init() {
     llk_math_eltwise_binary_sfpu_init<SfpuType::max_pool_with_indices, APPROXIMATE>(
-        sfpu::init_max_pool_with_indices<APPROXIMATE, layout>);
+        sfpu::init_max_pool_with_indices<(APPROXIMATE ? ApproximationMode::Fast : ApproximationMode::Precise), layout>);
 }
 
 template <
@@ -25,7 +26,12 @@ template <
 inline void llk_math_eltwise_binary_sfpu_max_pool_with_indices(
     uint dst_index, uint32_t idx_index, int vector_mode = (int)VectorMode::RC_custom) {
     _llk_math_eltwise_binary_sfpu_params_<APPROXIMATE>(
-        ckernel::sfpu::calculate_max_pool_with_indices<APPROXIMATE, is_fp32_dest_acc_en, num_rows, ITERATIONS, layout>,
+        ckernel::sfpu::calculate_max_pool_with_indices<
+            (APPROXIMATE ? ApproximationMode::Fast : ApproximationMode::Precise),
+            is_fp32_dest_acc_en,
+            num_rows,
+            ITERATIONS,
+            layout>,
         dst_index,
         idx_index,
         vector_mode);
