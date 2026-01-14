b8b87916b9cd9419281f2ff18f2ed9bd99d0b645
riverwuTT <riverwu@tenstorrent.com>
2026-01-14 20:38:15 +0000

    map out explicit synopsis

--- tt_metal/api/tt-metalium/experimental/tensor/device_tensor.hpp
+++ tt_metal/api/tt-metalium/experimental/tensor/device_tensor.hpp
@@ -4,11 +4,68 @@
 
 #pragma once
 
-namespace tt::tt_metal {
+#include <tt-metalium/shape.hpp>
 
-// TODO: Doc
+namespace tt::tt_metal /*::tensor*/ {
+
+class IDevice;
+
+/**
+ * DeviceTensor is a device memory object. The user’s mental model of DeviceTensor is an owning handle to
+ * device-allocated memory.
+ *
+ * DeviceTensor should have RAII semantics with unique ownership:
+ * - Device memory resource lifetime == object lifetime
+ *   - Device memory is allocated on construction, and released on destruction.
+ *   - The programmer explicitly manages the device-allocated memory lifetime.
+ *   - This can be tricky in an asynchronous runtime environment. For now, the onus is on the programmer to correctly
+ *     manage DeviceTensor lifetime around queue synchronization events.
+ * - Movable (RAII transfer of ownership)
+ * - Non-copyable
+ * - No equality/inequality operator. (If we did add this, equality would mean the same underlying allocation – no value
+ *   semantics)
+ *
+ */
 class DeviceTensor {
-    // TODO: Implement device tensor functionality
+public:
+    // Special Member functions
+
+    /**
+     * Construct a tensor that does not own any device memory.
+     */
+    DeviceTensor() = default;
+    /**
+     * Deallocates any owning device memory.
+     */
+    ~DeviceTensor() = default;
+
+    // TODO: what if clients actually want to copy?
+    DeviceTensor(const DeviceTensor&) = delete;
+    DeviceTensor& operator=(const DeviceTensor&) = delete;
+
+    // Transfers ownership of other's memory
+    DeviceTensor(DeviceTensor&& other) = default;
+    DeviceTensor& operator=(DeviceTensor&& other) = default;
+
+    // End speical member functions
+
+    /**
+     * Deallocate and release owned device memory.
+     */
+    void deallocate();
+
+    // reshape transformation, mutating version
+    // TODO: figure out what we will be doing for reshape
+    void reshape(/* */);
+
+    // Getters
+
+    IDevice* get_device() const;
+
+    // TODO(River): understand what is sharding better
+    bool is_sharded() const;
+    // TODO: what is the return type here?
+    Shape element_size() const;
 };
 
 }  // namespace tt::tt_metal
--- tt_metal/api/tt-metalium/experimental/tensor/host_tensor.hpp
+++ tt_metal/api/tt-metalium/experimental/tensor/host_tensor.hpp
@@ -4,11 +4,80 @@
 
 #pragma once
 
-namespace tt::tt_metal {
+#include <string>
+#include <vector>
 
-// TODO: Doc
+#include <tt-metalium/host_buffer.hpp>
+
+// TODO: this will be moved
+#include <tt-metalium/shape.hpp>
+
+namespace tt::tt_metal /*::tensor*/ {
+
+struct Unsure {};
+
+/**
+ * HostTensor is a host data class. It has the semantics of a container, and all host <-> device communications are
+ * explicit.
+ *
+ */
 class HostTensor {
-    // TODO: Implement host tensor functionality
+    /**
+     * To avoid disruption to existing users, HostTensor will deviate very little from the existing (host) Tensor
+     * semantics. The only significant changes are:
+     * - Eliminating implicit data movement APIs.
+     * - Remove transformation methods like to_layout and pad from the class methods. These seem better as free
+     *   functions that operate on a HostTensor than as methods of HostTensor. (Separation of data storage and data
+     *   manipulation.) In the existing Tensor, these are already duplicated as both methods and free functions.
+     */
+
+public:
+    // Special Member functions
+
+    /**
+     * Constructs an empty host tensor.
+     */
+    HostTensor() = default;
+    ~HostTensor() = default;
+
+    HostTensor(const HostTensor&) = default;
+    HostTensor& operator=(const HostTensor&) = default;
+
+    HostTensor(HostTensor&&) = default;
+    HostTensor& operator=(HostTensor&&) = default;
+
+    // End special member functions
+
+    // constructions:
+
+    // TODO: Spec? maybe better if this is `from_host_buffer`?
+    explicit HostTensor(const HostBuffer&);
+
+    static HostBuffer from_borrowed_data(/* */);
+    static HostBuffer from_span(/* */);
+    static HostBuffer from_vector(/* */);
+
+    // getters
+    std::vector<Unsure /* What would this be?? */> to_vector() const;
+    // TODO: This method has been removed from original Tensor, see: #32600
+    Unsure item() const;
+
+    // TODO: we should just specialize std::to_string
+    std::string write_to_string() const;
+
+    // TODO: parity with DistributedHostBuffer
+    const HostBuffer& get_host_buffer() const;
+
+    // TODO(River): understand what is sharding better
+    bool is_sharded() const;
+    // TODO: what is the return type here?
+    Shape element_size() const;
+
+    // TODO: dump other getters.
+
+    // reshape transformation, mutating version
+    // TODO: figure out what we will be doing for reshape
+    void reshape(/* */);
 };
 
 }  // namespace tt::tt_metal
