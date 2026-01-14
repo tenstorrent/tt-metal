32f6df87bd2f189f53be77b8fb77bc55b3fe4f2d
Gagan Deep Singh <gagandeepsingh@tenstorrent.com>
2026-01-14 18:14:38 +0000

    added use_loop functionality

--- tt_metal/hw/inc/api/debug/timing_perturbation.h
+++ tt_metal/hw/inc/api/debug/timing_perturbation.h
@@ -15,23 +15,34 @@ namespace tt::compute::common {
  *
  * @tparam num_nops: number of nops to insert
  * @tparam is_riscv_nop: 0 for Tensix nop, 1 for RISC-V nop
+ * @tparam use_loop: 0 for inline assembly, 1 for loop-based nops
  */
-template <const int num_nops, const int is_riscv_nop>
+template <const int num_nops, const int is_riscv_nop, const int use_loop = 0>
 inline void add_nops() {
-    if constexpr (is_riscv_nop) {
-        asm volatile(
-            ".rept %0\n\t"
-            "nop\n\t"
-            ".endr\n\t"
-            :
-            : "i"(num_nops));
+    if constexpr (use_loop) {
+        for (int i = 0; i < num_nops; i++) {
+            if constexpr (is_riscv_nop) {
+                asm volatile("nop");
+            } else {
+                TTI_NOP;
+            }
+        }
     } else {
-        asm volatile(
-            ".rept %0\n\t"
-            ".ttinsn %1\n\t"
-            ".endr\n\t"
-            :
-            : "i"(num_nops), "i"(TT_OP_NOP));
+        if constexpr (is_riscv_nop) {
+            asm volatile(
+                ".rept %0\n\t"
+                "nop\n\t"
+                ".endr\n\t"
+                :
+                : "i"(num_nops));
+        } else {
+            asm volatile(
+                ".rept %0\n\t"
+                ".ttinsn %1\n\t"
+                ".endr\n\t"
+                :
+                : "i"(num_nops), "i"(TT_OP_NOP));
+        }
     }
 }
 
@@ -42,21 +53,27 @@ inline void add_nops() {
  * @tparam num_math_nops: number of math nops to insert
  * @tparam num_pack_nops: number of pack nops to insert
  * @tparam is_riscv_nop: 0 for Tensix nop, 1 for RISC-V nop
+ * @tparam use_loop: 0 for inline assembly, 1 for loop-based nops
  */
-template <const int num_unpack_nops, const int num_math_nops, const int num_pack_nops, const int is_riscv_nop>
+template <
+    const int num_unpack_nops,
+    const int num_math_nops,
+    const int num_pack_nops,
+    const int is_riscv_nop,
+    const int use_loop = 0>
 inline void add_compute_nops() {
     DPRINT << "is_riscv_nop: " << (uint32_t)is_riscv_nop << " Unpack NOPs: " << (uint32_t)num_unpack_nops
            << " Math NOPs: " << (uint32_t)num_math_nops << " Pack NOPs: " << (uint32_t)num_pack_nops << ENDL();
     if constexpr (num_unpack_nops) {
-        UNPACK((add_nops<num_unpack_nops, is_riscv_nop>()));
+        UNPACK((add_nops<num_unpack_nops, is_riscv_nop, use_loop>()));
     }
 
     if constexpr (num_math_nops) {
-        MATH((add_nops<num_math_nops, is_riscv_nop>()));
+        MATH((add_nops<num_math_nops, is_riscv_nop, use_loop>()));
     }
 
     if constexpr (num_pack_nops) {
-        PACK((add_nops<num_pack_nops, is_riscv_nop>()));
+        PACK((add_nops<num_pack_nops, is_riscv_nop, use_loop>()));
     }
 }
 }  // namespace tt::compute::common
