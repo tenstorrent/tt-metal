390492fa587d4a7ac83a89fdb5665abeccee4a8f
handrewsTT <handrews@tenstorrent.com>
2026-01-14 10:07:30 +0000

    Revert "attn fix for 1x8 mesh"
    This reverts commit a8e14777782e5795bf3660a42ca9d6502e271aa2.

--- models/demos/gpt_oss/tt/experts/prefill.py
+++ models/demos/gpt_oss/tt/experts/prefill.py
@@ -284,6 +284,7 @@ def prefill_forward(
     next_states = ttnn.reshape(
         next_states,
         (1, batch_size, seq_len_global, config.hidden_size),
+        (1, batch_size, max(32, seq_len_global), config.hidden_size),
     )
 
     return next_states
