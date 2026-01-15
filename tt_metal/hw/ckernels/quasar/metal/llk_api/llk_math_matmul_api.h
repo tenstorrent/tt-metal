d7ce8fe462f880bb9069faf07f60e8346305b5bd
Ana Mokan <amokan@tenstorrent.com>
2026-01-15 13:54:35 +0000

    Configure all buf descs in trisc.cc, unify 32bitdest arguments for math_hw_config, add docstrings

--- tt_metal/hw/ckernels/quasar/metal/llk_api/llk_math_matmul_api.h
+++ tt_metal/hw/ckernels/quasar/metal/llk_api/llk_math_matmul_api.h
@@ -10,12 +10,52 @@
  * LLK MATMUL
  *************************************************************************/
 
-template <int NUM_FIDELITY_PHASES, bool EN_DI = false, bool EN_X2 = false>
+/**
+*
+* @brief Initialize matrix multiply operation of Input 0 * Input 1 -> SrcB * SrcA
+
+* @tparam NUM_FIDELITY_PHASES: 0 = LoFi, 2 = HiFi2, 3 = HiFi3, 4 = HiFi4 - controls precision of multiplication when
+math is in Fp32 format
+* @param ct_dim: number of tiles in the column dimension for a matrix multiply
+* @param rt_dim: number of tiles in the row dimension for a matrix multiply
+*
+* This function initializes the matrix multiply operation where:
+* Input 0 * Input 1 -> SrcB * SrcA
+* Input 0 dim = [rt_dim, 1], Input 1 dim = [1, ct_dim]
+* Output is a matrix block of dimension [rt_dim, ct_dim]
+*/
+template <std::uint8_t NUM_FIDELITY_PHASES>
 inline void llk_math_matmul_init(const std::uint32_t ct_dim = 1, const std::uint32_t rt_dim = 1) {
-    _llk_math_matmul_init_<NUM_FIDELITY_PHASES, EN_DI, EN_X2>(ct_dim, rt_dim);
+    _llk_math_matmul_init_<static_cast<ckernel::MathFidelity>(NUM_FIDELITY_PHASES), false, false>(ct_dim, rt_dim);
 }
 
-inline void llk_math_matmul(
-    const std::uint32_t dst_index, const std::uint32_t ct_dim = 1, const std::uint32_t rt_dim = 1) {
-    _llk_math_matmul_tile_(dst_index);
+/**
+ * @brief Performs matrix multiply operation, where Input 0, Input 1 and Output are each 1 tile
+ *
+ * @param dst_index: Tile index into the destination register
+ *
+ * This function performs the matrix multiply operation of Input 0 * Input 1 -> SrcB * SrcA,
+ * Input 0 = 1 tile -> SrcB reg, Input 1 = 1 tile -> SrcA reg,
+ * Output = 1 tile -> Dst reg at specified dst_index
+ */
+inline void llk_math_matmul_tile(const std::uint32_t dst_index) { _llk_math_matmul_tile_(dst_index); }
+
+/**
+ *
+ * @brief Performs matrix multiply operation of Input 0 * Input 1 -> SrcB * SrcA, where
+ * Input 0 dim = [rt_dim, 1], Input 1 dim = [1, ct_dim]
+ * Output is a matrix block of dimension [rt_dim, ct_dim]
+ *
+ * @param ct_dim: number of tiles in the column dimension for a matrix multiply
+ * @param rt_dim: number of tiles in the row dimension for a matrix multiply
+ *
+ * This function performs the matrix multiply operation of Input 0 * Input 1 -> SrcB * SrcA,
+ * Input 0 dim = [rt_dim, 1] -> SrcB reg, Input 1 dim = [1, ct_dim] -> SrcA reg
+ * Output is a matrix block of dimension [rt_dim, ct_dim]
+ * This function does not iterate over kt_dim, must iterate over kt_dim externally to this function
+ * Dest index is always assumed to start at 0 for this operation
+ *
+ */
+inline void llk_math_matmul_block(const std::uint32_t ct_dim, const std::uint32_t rt_dim) {
+    _llk_math_matmul_block_(ct_dim, rt_dim);
 }
