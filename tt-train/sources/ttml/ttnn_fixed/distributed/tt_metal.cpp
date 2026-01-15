9e7273758aba9f944c00ba5bdcc06945aef9bd29
dyurshevichTT <dyurshevich@testorrent.com>
2026-01-15 11:31:09 +0000

    update

--- tt-train/sources/ttml/ttnn_fixed/distributed/tt_metal.cpp
+++ tt-train/sources/ttml/ttnn_fixed/distributed/tt_metal.cpp
@@ -40,6 +40,7 @@ void enable_fabric(uint32_t num_devices) {
             fmt::println("[tt-train] TT_MESH_GRAPH_DESC_PATH is set to {}", mesh_graph_descriptor_path);
         }
     }
+
     tt::tt_fabric::SetFabricConfig(tt::tt_fabric::FabricConfig::FABRIC_2D);
 }
 
