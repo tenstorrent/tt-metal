92e1c2879c4d5beb0f3d36bf40c2da68fad19155
Nikhil Soraba <nsoraba@tenstorrent.com>
2026-01-15 08:12:36 +0000

    Phase 1 optimization for compute kernel: optimize unpacker

--- ttnn/cpp/ttnn/operations/experimental/moe/device/kernels/dm0.cpp
+++ ttnn/cpp/ttnn/operations/experimental/moe/device/kernels/dm0.cpp
@@ -58,13 +58,28 @@ void kernel_main() {
     constexpr uint32_t num_w2_tiles_h = 64;
 
     const uint32_t num_w0_w1_tiles_w = (core_id < 8) ? 5 : 6;
-    const uint32_t num_w2_tiles_w = (core_id < 8) ? 19 : 18;
+    const uint32_t num_w2_tiles_w = (core_id < 8) ? 18 : 20;
 
     const uint32_t num_elt_tiles = num_w0_w1_tiles_w;
     const uint32_t num_in2_tiles = num_w2_tiles_w;
     const uint32_t num_mm2_tiles = num_w2_tiles_w;
 
+    // W0 and W1 reading constants
+    constexpr uint32_t w0_w1_tiles_per_txn = 14;
+    constexpr uint32_t w0_w1_bytes_per_txn = w0_w1_tiles_per_txn * w0_tile_size;  // 14 * 576B = 7488B
+    const uint32_t w0_w1_txns = (core_id < 8) ? 2 * 80 : 2 * 96;
+    // num_w0_w1_tiles_w * num_w0_w1_tiles_h / w0_w1_tiles_per_txn;  // (5|6 * 224) / 14 = 80|96
+
+    // W2 reading constants
+    // Total tiles of w2 does not divide evenly by w2_tiles_per_txn (14), so we do it in two steps
+    constexpr uint32_t w2_tiles_per_txn = 14;
+    constexpr uint32_t w2_bytes_per_txn = w2_tiles_per_txn * w2_tile_size;                      // 14 * 576B = 7488B
+    constexpr uint32_t w2_txns_h = (num_w2_tiles_h + w2_tiles_per_txn - 1) / w2_tiles_per_txn;  // round up
+    const uint32_t w2_txns = w2_txns_h * num_w2_tiles_w;
+
+    //-------------------------------------------------------------------------
     // DRAM Reading constants
+    //-------------------------------------------------------------------------
     const uint32_t dram_bank_id = core_id;
     const uint64_t dram_noc_addr = get_noc_addr_from_bank_id<true>(dram_bank_id, /*bank_address_offset=*/0);
     uint32_t curr_trid = 1, prev_trid = 2;
@@ -76,20 +91,8 @@ void kernel_main() {
     const uint32_t total_size_per_layer = num_experts * total_size_per_expert;
     const uint32_t layer_base_offset = layer_id * total_size_per_layer;
 
-    // W0 and W1 reading constants
-    constexpr uint32_t w0_w1_tiles_per_txn = 14;
-    constexpr uint32_t w0_w1_bytes_per_txn = w0_w1_tiles_per_txn * w0_tile_size;  // 14 * 576B = 7488B
-    const uint32_t w0_w1_txns =
-        num_w0_w1_tiles_w * num_w0_w1_tiles_h / w0_w1_tiles_per_txn;  // (5|6 * 224) / 14 = 80|96
-
-    // W2 reading constants
-    // Total tiles of w2 does not divide evenly by w2_tiles_per_txn (14), so we do it in two steps
-    constexpr uint32_t w2_tiles_per_txn = 14;
-    constexpr uint32_t w2_bytes_per_txn = w2_tiles_per_txn * w2_tile_size;  // 14 * 576B = 7488B
-    const uint32_t w2_txns = ((num_w2_tiles_h * num_w2_tiles_w) + w2_tiles_per_txn - 1) / w2_tiles_per_txn;  // round up
-
     //-------------------------------------------------------------------------
-    // Read W0 with pipelined reads
+    // Read W0 and W1 with pipelined reads
     //-------------------------------------------------------------------------
     uint32_t read_offset = layer_base_offset;
     for (uint32_t expert_id = 0; expert_id < num_experts; ++expert_id) {
@@ -121,38 +124,6 @@ void kernel_main() {
         // Final cleanup
         noc_async_read_barrier_with_trid(prev_trid);
         cb_push_back(cb_r2c_w0, w0_w1_tiles_per_txn);
-
-        //-------------------------------------------------------------------------
-        // Read W1 with pipelined reads
-        //-------------------------------------------------------------------------
-        noc_async_read_one_packet_set_state</*use_vc=*/true>(
-            /*src_noc_addr=*/dram_noc_addr, /*size=*/w0_w1_bytes_per_txn, /*vc=*/vchannel);
-
-        for (uint32_t txn = 0; txn < w0_w1_txns; ++txn) {
-            // Issue reads with current trid
-            cb_reserve_back(cb_r2c_w1, w0_w1_tiles_per_txn);
-            uint32_t write_addr = get_write_ptr(cb_r2c_w1);
-
-            noc_async_read_set_trid(curr_trid);
-            noc_async_read_one_packet_with_state_with_trid</*skip_ptr_update=*/false, /*skip_cmdbuf_chk=*/true>(
-                /*src_base_addr=*/dram_noc_addr, /*src_addr=*/read_offset, /*dest_addr=*/write_addr, curr_trid);
-
-            // After first block: wait for OTHER trid, push
-            if (txn > 0) {
-                noc_async_read_barrier_with_trid(prev_trid);
-                cb_push_back(cb_r2c_w1, w0_w1_tiles_per_txn);
-            }
-
-            // Swap trids
-            std::swap(curr_trid, prev_trid);
-
-            // Increment read offset
-            read_offset += w0_w1_bytes_per_txn;
-        }
-
-        // Final cleanup
-        noc_async_read_barrier_with_trid(prev_trid);
-        cb_push_back(cb_r2c_w1, w0_w1_tiles_per_txn);
 
         //-------------------------------------------------------------------------
         // Read W2 with pipelined reads
