2b828365a7b38aefd06c7c83bbc85028554f347f
yugaoTT <yugao@tenstorrent.com>
2026-01-19 20:12:32 +0000

    add to tm fabric ci

--- tests/scripts/run_cpp_fabric_tests.sh
+++ tests/scripts/run_cpp_fabric_tests.sh
@@ -38,3 +38,11 @@ echo "Running fabric sanity tests now...";
 ./build/test/tt_metal/perf_microbenchmark/routing/test_tt_fabric --test_config ${TT_METAL_HOME}/tests/tt_metal/tt_metal/perf_microbenchmark/routing/test_fabric_sanity_common.yaml
 
 ./build/test/tt_metal/tt_fabric/fabric_elastic_channels_host_test 8 2 16 4352 1 4096 10000 4 4
+
+#############################################
+# UDM MODE TESTS                            #
+#############################################
+echo "Running UDM mode tests now...";
+
+# UDM fabric API tests (basic tests that work with N300 2-chip config)
+./build/test/tt_metal/tt_fabric/fabric_unit_tests --gtest_filter="Fabric2DUDMModeFixture.*"
