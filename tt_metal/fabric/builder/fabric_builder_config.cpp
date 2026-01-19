763c108f87f98819d31953cb19a2d47c4f0f3bdc
Umair Cheema <ucheema@g08blx04.tenstorrent.net>
2026-01-19 21:53:35 +0000

    Fix indexing bug causing VC1 teardown address corruption.

--- tt_metal/fabric/builder/fabric_builder_config.cpp
+++ tt_metal/fabric/builder/fabric_builder_config.cpp
@@ -41,7 +41,7 @@ uint32_t get_num_tensix_sender_channels(Topology topology, tt::tt_fabric::Fabric
 }
 
 uint32_t get_downstream_edm_count(bool is_2D_routing) {
-    return is_2D_routing ? builder_config::num_downstream_edms_2d : builder_config::num_downstream_edms_1d;
+    return is_2D_routing ? builder_config::max_downstream_edms : builder_config::num_downstream_edms_1d;
 }
 
 uint32_t get_vc0_downstream_edm_count(bool is_2D_routing) {
