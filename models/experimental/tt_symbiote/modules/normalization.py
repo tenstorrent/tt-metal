c6a2ec91acea0233f2aa601d3fee9f0ba547157d
Suhail Alnahari <salnahari@tenstorrent.com>
2026-01-20 12:09:41 -0500

    tt_symbiote: added deepseek modules

--- models/experimental/tt_symbiote/modules/normalization.py
+++ models/experimental/tt_symbiote/modules/normalization.py
@@ -5,16 +5,13 @@
 """Normalization layer implementations for TTNN."""
 
 from torch import nn
-
+import torch
 import ttnn
 from models.experimental.tt_symbiote.core.module import TTNNModule
 
 
 class TTNNLayerNorm(TTNNModule):
     """TTNN-accelerated LayerNorm."""
-
-    def __init__(self):
-        super().__init__()
 
     @classmethod
     def from_torch(cls, layer_norm: nn.LayerNorm):
@@ -49,3 +46,50 @@ class TTNNLayerNorm(TTNNModule):
             bias=self.tt_bias,
         )
         return tt_output
+
+
+class DeepseekV2RMSNorm(nn.Module):
+    def __init__(self, hidden_size, eps=1e-6):
+        """
+        DeepseekV2RMSNorm is equivalent to T5LayerNorm
+        """
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.variance_epsilon = eps
+
+    def forward(self, hidden_states):
+        input_dtype = hidden_states.dtype
+        hidden_states = hidden_states.to(torch.float32)
+        variance = hidden_states.pow(2).mean(-1, keepdim=True)
+        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
+        return self.weight * hidden_states.to(input_dtype)
+
+
+class TtRMSNorm(TTNNModule):
+    @classmethod
+    def from_torch(cls, rms_norm: DeepseekV2RMSNorm):
+        """Create  from PyTorch LayerNorm."""
+        if rms_norm.weight is None:
+            print(f"Warning: LayerNorm layer {rms_norm} has no weight. Using standard RMSNorm.")
+            return rms_norm
+        new_layer_norm = cls()
+        new_layer_norm._fallback_torch_layer = rms_norm
+        return new_layer_norm
+
+    def preprocess_weights_impl(self):
+        """Preprocess LayerNorm weights for TTNN."""
+        if self.torch_layer is None:
+            self._fallback_torch_layer = DeepseekV2RMSNorm(hidden_size=1)
+        self.tt_weight = ttnn.from_torch(
+            self.torch_layer.weight.unsqueeze(0).expand(32, -1), dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT
+        )
+
+    def move_weights_to_device_impl(self):
+        """Move weights to TTNN device."""
+        self.tt_weight = ttnn.to_device(self.tt_weight, self.device)
+
+    def forward(self, x: ttnn.Tensor) -> ttnn.Tensor:
+        if x.layout != ttnn.TILE_LAYOUT:
+            x = ttnn.to_layout(x, ttnn.TILE_LAYOUT, memory_config=ttnn.DRAM_MEMORY_CONFIG)
+        x = ttnn.rms_norm(x, weight=self.tt_weight, epsilon=self.torch_layer.variance_epsilon)
+        return x
