781fdff1ecd947d62c880f8d733342983d775b76
jpanasiukTT <jpanasiuk@tenstorrent.com>
2026-01-20 15:50:44 +0000

    python3 on top

--- tools/scaleout/exabox/analyze_validation_results.py
+++ tools/scaleout/exabox/analyze_validation_results.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
+
 # SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 #
 # SPDX-License-Identifier: Apache-2.0
 
-#!/usr/bin/env python3
 """Analyze validation output logs for health status and errors."""
 
 import argparse
