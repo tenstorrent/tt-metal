ced4089cab055081f6a92a5cdcd89c55b0571788
Justin Hai <jhai@tenstorrent.com>
2026-01-21 20:47:26 +0000

    Further reverted writer_all_to_all_dispatch

--- ttnn/cpp/ttnn/operations/ccl/all_to_all_dispatch/device/kernels/dataflow/writer_all_to_all_dispatch.cpp
+++ ttnn/cpp/ttnn/operations/ccl/all_to_all_dispatch/device/kernels/dataflow/writer_all_to_all_dispatch.cpp
@@ -191,15 +191,14 @@ void kernel_main() {
         uint32_t input_token_read_addr = get_read_ptr(input_tensor_cb_id);
         uint16_t* token_indices = (uint16_t*)(get_read_ptr(indices_tensor_cb_id));
 
-        // get the expert that is chosen for the current token
-        // find the devices that the expert lives on and dispatch the input tokens to them
-        // if there is no tensor parallelism, then the token will only be sent to one device
         for (uint32_t k = 0; k < selected_experts_k; k++) {
             // get the expert that is chosen for the current token
             uint16_t expert_chosen = token_indices[k];
             uint32_t expert_offset = expert_chosen * aligned_mapping_page_size;
             uint16_t* devices_for_expert = (uint16_t*)(get_read_ptr(mapping_tensor_cb_id) + expert_offset);
 
+            // find the devices that the expert lives on and dispatch the input tokens to them
+            // if there is no tensor parallelism, then the token will only be sent to one device
             for (uint32_t d = device_begin_idx; d < device_end_idx; d += device_stride) {
                 if (devices_for_expert[d] == 1 &&
                     send_preparation_buffer[(local_token - token_start_idx) * num_devices + d] == 0) {
