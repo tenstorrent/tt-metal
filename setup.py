c5d8ab534298920cbcd60c595c0f6d861d6c891b
Rustam Kazmi <ashfaqkazmi1975@gmail.com>
2026-01-21 09:57:24 +0500

    Allow cibuildwheel to opt out of system SFPI

--- setup.py
+++ setup.py
@@ -247,7 +247,7 @@ class CMakeBuild(build_ext):
                     "-DTT_UNITY_BUILDS=ON",
                     "-DTT_ENABLE_LIGHT_METAL_TRACE=ON",
                     "-DWITH_PYTHON_BINDINGS=ON",
-                    "-DTT_USE_SYSTEM_SFPI=ON",
+                    f"-DTT_USE_SYSTEM_SFPI={os.environ.get('CIBW_USE_SYSTEM_SFPI', 'OFF')}",
                     "-DENABLE_CCACHE=TRUE",
                 ]
 
