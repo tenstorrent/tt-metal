24a370c8aabe7934bcbf5d2e9328a368d4408468
Bazyli Klockiewicz <bklockiewicz@tenstorrent.com>
2026-01-21 16:36:15 +0000

    introduce first cb read in the writer

--- ttnn/cpp/ttnn/operations/experimental/ccl/strided_reduce_scatter_async/device/kernels/minimal_ring_strided_reduce_scatter_async_reader.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/strided_reduce_scatter_async/device/kernels/minimal_ring_strided_reduce_scatter_async_reader.cpp
@@ -164,21 +164,20 @@ void kernel_main() {
                         DPRINT << "input_row_offset: " << input_row_offset << ENDL();
                         // DPRINT << "direction_offset: " << direction_offset << ENDL();
 
-                        if (do_reduce) {
-                            // TODO: remove the if (reduce) after introducing the writer kernel
-                            cb_reserve_back(cb_in0, tile_granularity);
-                            uint32_t l1_write_addr = get_write_ptr(cb_in0);
-                            for (uint32_t j = 0; j < tiles_to_read_in_current_direction; ++j) {
-                                uint32_t input_tile_id = input_tile_id_start + input_row_offset + direction_offset + j;
-                                DPRINT << "writing to input_tile_id: " << input_tile_id << ENDL();
-                                uint64_t noc_read_addr = get_noc_addr(input_tile_id, input_tensor_addrgen);
-                                noc_async_read(noc_read_addr, l1_write_addr, page_size);
-                                l1_write_addr += page_size;
-                            }
-                            // TODO: can move it outside after removing the if (reduce)
-                            noc_async_read_barrier();
-                            cb_push_back(cb_in0, tile_granularity);
+                        // TODO: remove the if (reduce) after introducing the writer kernel
+                        cb_reserve_back(cb_in0, tile_granularity);
+                        uint32_t l1_write_addr = get_write_ptr(cb_in0);
+                        for (uint32_t j = 0; j < tiles_to_read_in_current_direction; ++j) {
+                            uint32_t input_tile_id = input_tile_id_start + input_row_offset + direction_offset + j;
+                            DPRINT << "writing to input_tile_id: " << input_tile_id << ENDL();
+                            uint64_t noc_read_addr = get_noc_addr(input_tile_id, input_tensor_addrgen);
+                            noc_async_read(noc_read_addr, l1_write_addr, page_size);
+                            l1_write_addr += page_size;
                         }
+                        // TODO: can move it outside after removing the if (reduce)
+                        noc_async_read_barrier();
+                        cb_push_back(cb_in0, tile_granularity);
+
                         if (do_reduce) {
                             // TODO: read the next intermediate slice out of the intermediate buffer, and put it in
                             // intermediate CB
