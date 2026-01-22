8e2ced6fa67dfcf6baea5592df7ef414b3deb50a
Jason Davies <jason@jasondavies.com>
2026-01-22 17:39:51 +0000

    Fix int32 binary max/min handling of INT_MIN.

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_binary_max_min.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_binary_max_min.h
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+// SPDX-FileCopyrightText: © 2026 Jason Davies <jason@jasondavies.com>
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -24,6 +25,21 @@ inline void calculate_binary_max_min(const uint dst_index_in0, const uint dst_in
 
         // Swap and store maximum in lreg1, minimum in lreg0
         TTI_SFPSWAP(0, p_sfpu::LREG1, p_sfpu::LREG0, 1);
+
+        if constexpr (INSTRUCTION_MODE == InstrModLoadStore::INT32) {
+            // The values are two's complement signed integers, but SFPSWAP
+            // treats them as sign-magnitude.  The result is still correct
+            // unless both values are negative, in which case the result simply
+            // needs to be inverted via unconditional swap.
+
+            TTI_SFPSETCC(0, p_sfpu::LREG0, 0, 0);
+            TTI_SFPSETCC(0, p_sfpu::LREG1, 0, 0);
+
+            // If a < 0 and b < 0, then invert the result.
+            TTI_SFPSWAP(0, p_sfpu::LREG1, p_sfpu::LREG0, 0);
+
+            TTI_SFPENCC(0, 0, 0, 0);
+        }
 
         if constexpr (IS_MAX_OP) {
             TT_SFPSTORE(p_sfpu::LREG1, INSTRUCTION_MODE, ADDR_MOD_3, dst_index_out * dst_tile_size);
