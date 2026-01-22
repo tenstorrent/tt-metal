3515b2dbd6d79f2d98844e3b17698ddfce6b4d1c
Dragica Stoiljkovic <dstoiljkovic@tenstorrent.com>
2026-01-22 10:47:42 +0000

    Adding needed skips for models-unit-tests

--- models/common/utility_functions.py
+++ models/common/utility_functions.py
@@ -1047,6 +1047,16 @@ def is_grayskull():
     return "grayskull" in ARCH_NAME
 
 
+def is_watcher_enabled():
+    watcher = os.environ.get("TT_METAL_WATCHER")
+    lightweight_asserts = os.environ.get("TT_METAL_LIGHTWEIGHT_KERNEL_ASSERTS")
+    return (watcher is not None and watcher != "") or lightweight_asserts == "1"
+
+
+def is_n300():
+    return os.environ.get("MESH_DEVICE", "N150") == "N300"
+
+
 def is_slow_dispatch():
     return os.environ.get("TT_METAL_SLOW_DISPATCH_MODE") == "1"
 
@@ -1061,6 +1071,10 @@ def skip_for_blackhole(reason_str="not a blackhole test"):
 
 def skip_for_wormhole_b0(reason_str="not a wormhole test"):
     return ti_skip(is_wormhole_b0(), reason=reason_str)
+
+
+def skip_with_watcher(reason_str="Test is not passing with watcher enabled"):
+    return ti_skip(is_watcher_enabled(), reason=reason_str)
 
 
 def run_for_blackhole(reason_str="only runs for Blackhole"):
