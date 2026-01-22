786d0bc812dfbc7b63d8f2db061b1eed389a94bf
Ana Mokan <amokan@tenstorrent.com>
2026-01-22 16:21:55 +0000

    Clean up modified files pt1

--- tt_metal/hw/inc/internal/circular_buffer_interface.h
+++ tt_metal/hw/inc/internal/circular_buffer_interface.h
@@ -81,11 +81,6 @@ struct LocalCBInterface {
 
     // used by packer for in-order packing
     uint32_t fifo_wr_tile_ptr;
-
-    // Tile indices tracking how many tiles from CB base the rd/wr pointers are
-    // These wrap around at fifo_num_pages
-    uint32_t fifo_rd_tile_idx;
-    uint32_t fifo_wr_tile_idx;
 };
 
 struct CBInterface {
