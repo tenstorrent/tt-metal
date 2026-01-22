699e1223d6a14c190b708f5bcfe3d01a55113642
velonica0 <like@mail.nankai.edu.cn>
2026-01-22 15:25:28 +0800

    prepare weight
    compile success
    
    success
    
    remove log
    
    fix a bug
    
    other example
    
    fix test
    
    fix test
    
    fix warning
    
    nitpick
    
    prepared and unprepared weights
    
    remove prepare_weights_ and prepare_conv3d_weights function
    
    resolve tt_dit
    
    Parameter and pytest
    
    alignment in config
    
    layout in Parameter
    
    mochi
    
    mochi
    
    mochi
    
    expose prepare weights
    
    test_conv3d_no_config
    
    all test

--- models/tt_dit/models/vae/vae_wan2_1.py
+++ models/tt_dit/models/vae/vae_wan2_1.py
@@ -17,7 +17,7 @@ from ...layers.module import Module, ModuleList, Parameter
 from ...layers.normalization import RMSNorm
 from ...parallel.config import VaeHWParallelConfig
 from ...parallel.manager import CCLManager
-from ...utils.conv3d import _ntuple, aligned_channels, count_convs, get_conv3d_config, prepare_conv3d_weights
+from ...utils.conv3d import _ntuple, aligned_channels, count_convs, get_conv3d_config
 from ...utils.substate import pop_substate, rename_substate
 from ...utils.tensor import typed_tensor
 
@@ -274,7 +274,20 @@ class WanCausalConv3d(Module):
         )
 
         d = self.kernel_size[0] * self.kernel_size[1] * self.kernel_size[2] * self.in_channels
-        self.weight = Parameter(total_shape=[d, self.out_channels], device=mesh_device, pad_value=0, dtype=dtype)
+        self.weight = Parameter(
+            total_shape=[
+                self.out_channels,
+                self.unpadded_in_channels,
+                self.kernel_size[0],
+                self.kernel_size[1],
+                self.kernel_size[2],
+            ],
+            device=mesh_device,
+            layout=ttnn.ROW_MAJOR_LAYOUT,
+            on_host=True,
+            pad_value=0,
+            dtype=dtype
+        )
         self.bias = Parameter(total_shape=[1, self.out_channels], device=mesh_device, pad_value=0, dtype=dtype)
 
         self.mask_cache = {}
@@ -290,7 +303,7 @@ class WanCausalConv3d(Module):
 
         if "weight" in state and "bias" in state:
             weight, bias = maybe_pad_out_channels(state["weight"], state["bias"])
-            state["weight"], state["bias"] = prepare_conv3d_weights(weight, bias, self.conv_config)
+            state["weight"], state["bias"] = weight, bias.reshape(1, -1)
 
     def get_cached_mask(self, x_BTHWC, logical_h):
         sharded_h = x_BTHWC.shape[2]
@@ -397,6 +410,7 @@ class WanCausalConv3d(Module):
             input_tensor=x_BTHWC,
             weight_tensor=self.weight.data,
             bias_tensor=self.bias.data,
+            device=self.mesh_device,
             config=self.conv_config,
             output_channels=self.out_channels,
             kernel_size=self.kernel_size,
@@ -661,6 +675,7 @@ class WanConv2d(Module):
         super().__init__()
 
         self.in_channels = in_channels
+        self.unpadded_in_channels = in_channels
         self.unpadded_out_channels = out_channels
         self.TILE_WIDTH = 32
         self.out_channels = self.TILE_WIDTH if out_channels < self.TILE_WIDTH else out_channels
@@ -710,16 +725,27 @@ class WanConv2d(Module):
         )
 
         d = self.kernel_size[0] * self.kernel_size[1] * self.kernel_size[2] * self.in_channels
-        self.weight = Parameter(total_shape=[d, self.out_channels], device=mesh_device, pad_value=0, dtype=dtype)
-        self.bias = Parameter(total_shape=[1, self.out_channels], device=mesh_device, pad_value=0, dtype=dtype)
+        self.weight = Parameter(
+            total_shape=[
+                self.out_channels,
+                self.unpadded_in_channels,
+                self.kernel_size[0],
+                self.kernel_size[1],
+                self.kernel_size[2],
+            ],
+            device=mesh_device,
+            layout=ttnn.ROW_MAJOR_LAYOUT,
+            on_host=True,
+            pad_value=0,
+            dtype=dtype
+        )
+        self.bias = Parameter(total_shape=[1, self.out_channels], device=mesh_device, pad_value=0)
 
         self.mask_cache = {}
 
     def _prepare_torch_state(self, state: dict[str, torch.Tensor]) -> None:
         if "weight" in state and "bias" in state:
-            state["weight"], state["bias"] = prepare_conv3d_weights(
-                state["weight"].unsqueeze(2), state["bias"], self.conv_config
-            )
+            state["weight"], state["bias"] = state["weight"].unsqueeze(2), state["bias"].reshape(1, -1)
 
     def get_cached_mask(self, x_BTHWC, logical_h):
         sharded_h = x_BTHWC.shape[2]
@@ -803,6 +829,7 @@ class WanConv2d(Module):
             input_tensor=x_BTHWC,
             weight_tensor=self.weight.data,
             bias_tensor=self.bias.data,
+            device=self.mesh_device,
             config=self.conv_config,
             output_channels=self.out_channels,
             kernel_size=self.kernel_size,
