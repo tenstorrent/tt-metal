5c29ff15d3d80a96ce2ab4b363ad44f0b259ebb8
Artem Yerofieiev <ayerofieiev@tenstorrent.com>
2026-01-22 07:57:16 +0000

    remove comments from operations

--- tt-train/sources/ttml/metal/operations.hpp
+++ tt-train/sources/ttml/metal/operations.hpp
@@ -17,33 +17,3 @@
 #include "ops/softmax/softmax.hpp"
 #include "ops/swiglu_fw/swiglu_fw.hpp"
 #include "optimizers/sgd_fused/sgd_fused.hpp"
-
-namespace ttml::metal {
-
-// rmsnorm_fw is now a free function declared in ops/rmsnorm_fw/rmsnorm_fw.hpp
-
-// rmsnorm_bw is now a free function declared in ops/rmsnorm_bw/rmsnorm_bw.hpp
-
-// layernorm_bw is now a free function declared in ops/layernorm_bw/layernorm_bw.hpp
-
-// layernorm_fw is now a free function declared in ops/layernorm_fw/layernorm_fw.hpp
-
-// cross_entropy_fw is now a free function declared in ops/cross_entropy_fw/cross_entropy_fw.hpp
-
-// cross_entropy_bw is now a free function declared in ops/cross_entropy_bw/cross_entropy_bw.hpp
-
-// softmax is now a free function declared in ops/softmax/softmax.hpp
-
-// profiler_no_op is now a free function declared in ops/profiler_no_op/profiler_no_op.hpp
-
-// silu_bw is now a free function declared in ops/silu_bw/silu_bw.hpp
-
-// sdpa_fw is now a free function declared in ops/sdpa_fw/sdpa_fw.hpp
-
-// sdpa_bw is now a free function declared in ops/sdpa_bw/sdpa_bw.hpp
-
-// swiglu_fw is now a free function declared in ops/swiglu_fw/swiglu_fw.hpp
-
-// sgd_fused is now a free function declared in optimizers/sgd_fused/sgd_fused.hpp
-
-}  // namespace ttml::metal
