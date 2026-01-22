2c6f7926e318f0070ff1aac4c85f44bc526bdbc3
Diego Gomez <dgomez@tenstorrent.com>
2026-01-22 16:31:41 -0500

    wip

---
+++ tests/ttnn/unit_tests/base_functionality/test_tracer_from_torch.py
@@ -0,0 +1,35 @@
+# SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import torch
+import transformers
+
+import ttnn
+
+
+@pytest.mark.parametrize(
+    "shape",
+    [(1, 1, 5632, 64)],
+)
+@pytest.mark.parametrize(
+    "input_dtype",
+    [
+        ttnn.bfloat16,
+        ttnn.bfloat8_b,
+    ],
+)
+def test_to_with_different_dtypes_and_trace(device, shape, input_dtype):
+    torch.random.manual_seed(1234)
+    torch_input_tensor = torch.randn(shape).bfloat16().float()
+    interleaved_memory_config = ttnn.DRAM_MEMORY_CONFIG
+    with ttnn.tracer.trace():
+        tensor = ttnn.from_torch(
+            torch_input_tensor,
+            device=device,
+            layout=ttnn.TILE_LAYOUT,
+            memory_config=interleaved_memory_config,
+            dtype=input_dtype,
+        )
+    assert True
