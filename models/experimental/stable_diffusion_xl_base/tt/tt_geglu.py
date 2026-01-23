c34fc1dd650a3afbd33eeb917f4fc6c67f3c23bf
Nenad Babin <nbabin@tenstorrent.com>
2026-01-23 10:54:26 +0100

    SDXL Refiner Matmul config optimizations (#36311)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/31676
    
    ### Problem description
    Matmul ops in SDXL Refiner are not optimized.
    
    ### What's changed
    This PR optimizes Matmul ops by setting in0_block_w, out_subblock_h and
    out_subblock_w in 1D or 2D program configs of Matmul ops.
    Perf report with updated Matmul configs in the SDXL Refiner:
    [link](https://docs.google.com/spreadsheets/d/1-BYXmpATqQ0zhyregcJSgasX7Sasu2Ui2uZLFm-VxPw/edit?gid=1978382097#gid=1978382097)
    Perf report of the SDXL Refiner with no optimizations:
    [link](https://docs.google.com/spreadsheets/d/1gv35ErIKF7cvU6qNgcif-OFsgJJ-7WZciafvw6W_u1o/edit?gid=775911596#gid=775911596)
    Perf report of the base SDXL Unet:
    [link](https://docs.google.com/spreadsheets/d/1_IkP5Y5cQ9LFzXhxm_ZcpmnIMZcsfAyz1hUCssPce_I/edit?gid=1760520236#gid=1760520236)
    
    ### Checklist
    - [x] [All post-commit
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/21242287698)
    CI passes
    - [x] [(Single-card) Device perf
    regressions](https://github.com/tenstorrent/tt-metal/actions/runs/21242627128)
    CI passes
    - [x] [(Single-card) Demo tests (perf
    runs)](https://github.com/tenstorrent/tt-metal/actions/runs/21242349025)
    CI passes
    - [x] [Nightly tt-metal L2
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/21242543340)
    CI passes
    - [x] [(Single-card) Frequent model and ttnn
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/21245724846)
    CI passes

--- models/experimental/stable_diffusion_xl_base/tt/tt_geglu.py
+++ models/experimental/stable_diffusion_xl_base/tt/tt_geglu.py
@@ -37,7 +37,6 @@ class TtGEGLU(LightweightModule):
         self.output_memory_config_gelu = model_config.get_mm_output_memory_config(f"{module_path}.proj.split.gelu")
 
     def forward(self, input_tensor):
-        # TODO: self.program_config is not None is used to differentiate base and refiner; remove this with refiner matmul optimizations
         hidden_states = ttnn.linear(
             input_tensor,
             self.tt_weights_1,
