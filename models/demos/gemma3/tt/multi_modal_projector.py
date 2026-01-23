8e00ca8b6263b741f66ef74fca5c1210ea59a9e2
Petar Milojevic <pmilojevic@tenstorrent.com>
2026-01-23 17:21:22 +0100

    Fix for gemma3 reshape with TILED inputs hang (#36404)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/36275
    
    ### Problem description
    Gemma3 hangs on `reshape` op with `TILE` input in multi modal projector
    for image prefill.
    
    ### What's changed
    Changed input tensors from `TILE_LAYOUT` to `ROW_MAJOR_LAYOUT`.
    
    ### Checklist
    
    - [ ] [All
    post-commit](https://github.com/tenstorrent/tt-metal/actions/runs/21285194723/job/61265367982)
    - [ ] [Single card demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/21283208250/job/61258673029)
    - [ ] [Single card unit
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/21283214579)
    - [ ] [T3K demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/21283768832/job/61260578780)
    - [ ] [T3K unit
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/21283246481)
    - [ ]
    [vLLM-nightly](https://github.com/tenstorrent/tt-metal/actions/runs/21283252629/job/61258837391)
    
    #### Model tests
    - [Models-CI with
    hang](https://github.com/tenstorrent/tt-shield/actions/runs/21227340841/job/61080247006)
    - [Models-CI without
    hang](https://github.com/tenstorrent/tt-shield/actions/runs/21259537281/job/61183327601)

--- models/demos/gemma3/tt/multi_modal_projector.py
+++ models/demos/gemma3/tt/multi_modal_projector.py
@@ -86,12 +86,13 @@ class TtGemma3MultiModalProjector(LightweightModule):
 
         ttnn.deallocate(vision_outputs)
 
+        reshaped_vision_outputs = ttnn.to_layout(reshaped_vision_outputs, ttnn.ROW_MAJOR_LAYOUT)
+
         reshaped_vision_outputs = ttnn.reshape(
             reshaped_vision_outputs, (batch_size, seq_length, self.patches_per_image, self.patches_per_image)
         )
 
         in_n, in_c, in_h, in_w = reshaped_vision_outputs.shape
-        reshaped_vision_outputs = ttnn.to_layout(reshaped_vision_outputs, ttnn.ROW_MAJOR_LAYOUT)
         reshaped_vision_outputs = ttnn.permute(reshaped_vision_outputs, (0, 2, 3, 1))
         reshaped_vision_outputs = ttnn.reshape(reshaped_vision_outputs, (1, 1, in_n * in_h * in_w, in_c))
         pooled_vision_outputs = ttnn.avg_pool2d(
@@ -113,11 +114,12 @@ class TtGemma3MultiModalProjector(LightweightModule):
         pooled_vision_outputs = ttnn.reshape(pooled_vision_outputs, (in_n, HOUT, WOUT, in_c))
 
         pooled_vision_outputs = ttnn.permute(pooled_vision_outputs, (0, 3, 1, 2))
-        pooled_vision_outputs = ttnn.to_layout(pooled_vision_outputs, ttnn.TILE_LAYOUT)
 
         pooled_vision_outputs = ttnn.reshape(
             pooled_vision_outputs, (pooled_vision_outputs.shape[0], pooled_vision_outputs.shape[1], -1)
         )
+
+        pooled_vision_outputs = ttnn.to_layout(pooled_vision_outputs, ttnn.TILE_LAYOUT)
 
         # # Flatten(2)
         pooled_vision_outputs = ttnn.transpose(pooled_vision_outputs, 1, 2)
