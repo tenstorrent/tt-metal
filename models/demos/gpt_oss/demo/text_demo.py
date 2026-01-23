2a01bd00baf459feec538ba9bafad898d92c3165
handrewsTT <handrews@tenstorrent.com>
2026-01-23 15:11:07 +0000

    prefill128

--- models/demos/gpt_oss/demo/text_demo.py
+++ models/demos/gpt_oss/demo/text_demo.py
@@ -235,8 +235,7 @@ def prepare_gpt_oss_generator_args(
             True,  # warmup_prefill
         ),
         (
-            # "models/demos/gpt_oss/demo/sample_prompts/input_data_questions_prefill_128.json",  # input_prompts
-            "models/tt_transformers/demo/sample_prompts/input_data_long_1k.json",  # input_prompts
+            "models/demos/gpt_oss/demo/sample_prompts/input_data_questions_prefill_128.json",  # input_prompts
             1,  # data_parallel
             128,  # batch_size
             1,  # repeat_batches
