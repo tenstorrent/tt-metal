5f3e40b8c2c2e94b96b873a9247bed269b8fa179
Nikola Cvetkovic <ncvetkovic@tenstorrent.com>
2026-01-24 17:58:19 +0000

    #0: fix max_tile to binary_max_tile

--- tests/tt_metal/tt_metal/test_kernels/misc/sdpa/reduce_block_max_row/compute.cpp
+++ tests/tt_metal/tt_metal/test_kernels/misc/sdpa/reduce_block_max_row/compute.cpp
@@ -40,7 +40,7 @@ void kernel_main() {
     cb_wait_front(qk_im_cb, num_tiles);
     cb_reserve_back(out_max_cb, rows);
 
-    max_tile_init();
+    binary_max_tile_init();
     constexpr uint32_t reduce_dst_idx = 0;
     constexpr uint32_t prev_max_dst_idx = 1;
 
@@ -53,7 +53,7 @@ void kernel_main() {
         if (do_eltwise) {
             copy_tile_to_dst_init_short(prev_max_cb);
             copy_tile(prev_max_cb, i, prev_max_dst_idx);
-            max_tile(reduce_dst_idx, prev_max_dst_idx, static_cast<int>(VectorMode::C));
+            binary_max_tile(reduce_dst_idx, prev_max_dst_idx, reduce_dst_idx);
         }
 
         pack_tile(reduce_dst_idx, out_max_cb);
