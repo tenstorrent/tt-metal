ec82eea0f5ca5061dd2ec48773e04c36bda1317c
Dragica Stoiljkovic <dstoiljkovic@tenstorrent.com>
2026-01-26 13:31:05 +0000

    Skip fo matmul

--- tests/tt_eager/python_api_testing/unit_testing/misc/test_matmul_dram_sharded.py
+++ tests/tt_eager/python_api_testing/unit_testing/misc/test_matmul_dram_sharded.py
@@ -255,12 +255,10 @@ def test_matmul_in1_dram_sharded_with_program_cache(
         is_watcher_enabled()
         and M == 32
         and K == 8192
-        and N == 1280
+        and N in (1280, 1024)
         and fidelity in (ttnn.MathFidelity.HiFi2, ttnn.MathFidelity.LoFi)
     ):
         pytest.skip("Test is not passing with watcher enabled github issue #36314")
-    if is_watcher_enabled() and M == 32 and K == 8192 and N == 1024 and fidelity == ttnn.MathFidelity.HiFi2:
-        pytest.skip("Test is not passing with watcher enabled, see issue #XXXXX")
     for _ in range(2):
         run_test_matmul_in1_dram_sharded(
             device,
