e10c91bbb96dae3ad6ffece37c32a0e66b71ef6c
riverwuTT <riverwu@tenstorrent.com>
2026-01-26 22:57:26 +0000

    put in comments regarding the namesapce

--- tt_metal/api/tt-metalium/experimental/tensor/host_tensor.hpp
+++ tt_metal/api/tt-metalium/experimental/tensor/host_tensor.hpp
@@ -4,6 +4,11 @@
 
 #pragma once
 
+// It is intentional to not reflect the experimental status of this header in it's namespace,
+// as most of the code movements are based on implementations in TTNN that are well tested and production ready for a
+// long time, it is expected for the implementation to graudate out of experimental really quickly.
+//
+// Using namespace tt::tt_metal avoids double namespace renaming for the refactoring effort.
 namespace tt::tt_metal {
 
 // TODO: Implement host tensor functionality
