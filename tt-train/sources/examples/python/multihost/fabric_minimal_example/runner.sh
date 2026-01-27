896146fc358057de395c4cbca8ec7668a796e4da
Adriel Bustamante <abustamante@tenstorrent.com>
2026-01-27 00:53:34 +0000

    Removed hard-coded nodelists in runner files

--- tt-train/sources/examples/python/multihost/fabric_minimal_example/runner.sh
+++ tt-train/sources/examples/python/multihost/fabric_minimal_example/runner.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH --nodes=2
-#SBATCH --nodelist=metal-wh-09,metal-wh-18
 #SBATCH --partition=debug
-#SBATCH --job-name=test
-#SBATCH --output=test_%j.out
-#SBATCH --error=test_%j.err
+#SBATCH --job-name=fabric_minimal
+#SBATCH --output=fabric_minimal_`%j.out
+#SBATCH --error=fabric_minimal_`%j.err
 
 # Set environmental variables
 export TT_METAL_HOME="/data/abustamante/tt-metal"
 export PYTHONPATH="${TT_METAL_HOME}:${PYTHONPATH}"
 source ${TT_METAL_HOME}/python_env/bin/activate
+export LD_LIBRARY_PATH="/opt/openmpi-v5.0.7-ulfm/lib:$LD_LIBRARY_PATH"
 
-CONFIG_FILE="training_configs/training_shakespeare_tinyllama_tensor_parallel_3tier_fabric.yaml"
+FABRIC_MINIMAL="/data/${USER}/tt-metal/tt-train/sources/examples/python/multihost/fabric_minimal_example"
+CONFIG_FILE="training_configs/training_shakespeare_tinyllama_2tier_fabric.yaml"
 
-tt-run --mpi-args "-x TT_METAL_HOME --mca mpi_show_mca_params all --mca btl_tcp_if_include eno1 --mca oob_tcp_if_include eno1 --mca btl self,tcp --tag-output" --rank-binding configurations/2loudboxes/rank_bindings.yaml python ${TT_METAL_HOME}/tt-train/sources/examples/python/multihost/fabric_minimal_example/example.py -c ${CONFIG_FILE}
+tt-run --mpi-args "--rankfile /data/slurm/rankfile_${SLURM_JOB_ID}.txt -x TT_METAL_HOME --mca mpi_show_mca_params all --mca btl_tcp_if_include eno1 --mca oob_tcp_if_include eno1 --mca btl self,tcp --tag-output" --rank-binding /data/slurm/rank_bindings_${SLURM_JOB_ID}.yaml python ${FABRIC_MINIMAL}/example.py -c ${CONFIG_FILE}
--- tt-train/sources/examples/python/multihost/pipeline_parallel_training/runner.sh
+++ tt-train/sources/examples/python/multihost/pipeline_parallel_training/runner.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#SBATCH --nodes=5
-#SBATCH --nodelist=metal-wh-18,metal-wh-09,metal-wh-10,metal-wh-11,metal-wh-12
 #SBATCH --partition=debug
 #SBATCH --job-name=pipeline_parallel_training
 #SBATCH --output=pipeline_parallel_training_%j.out
@@ -11,5 +9,6 @@ export PYTHONPATH="${TT_METAL_HOME}:${PYTHONPATH}"
 source ${TT_METAL_HOME}/python_env/bin/activate
 
 CONFIG_FILE="training_configs/training_shakespeare_llama7b_pp_fabric.yaml"
+PP_ROOT="${TT_METAL_HOME}/tt-train/sources/examples/python/multihost/pipeline_parallel_training"
 
-tt-run --mpi-args "-x TT_METAL_HOME --mca mpi_show_mca_params all --mca btl_tcp_if_include eno1 --mca oob_tcp_if_include eno1 --mca btl self,tcp --tag-output" --rank-binding configurations/5loudboxes/rank_bindings.yaml python ${TT_METAL_HOME}/tt-train/sources/examples/python/multihost/pipeline_parallel_training/training.py -c ${CONFIG_FILE}
+tt-run --mpi-args "--hostfile ${PP_ROOT}/configurations/${HOST_CONFIG}/hosts.txt -x TT_METAL_HOME --mca mpi_show_mca_params all --mca btl_tcp_if_include eno1 --mca oob_tcp_if_include eno1 --mca btl self,tcp --tag-output" --rank-binding ${PP_ROOT}/configurations/5loudboxes/rank_bindings.yaml python ${PP_ROOT}/training.py -c ${CONFIG_FILE}
