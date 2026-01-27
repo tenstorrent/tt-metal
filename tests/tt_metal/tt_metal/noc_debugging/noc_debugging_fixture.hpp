cc6e3d97f0bdc6510b8ec03004506a23d419fab5
Nigel Huang <nhuang@tenstorrent.com>
2026-01-27 01:52:03 +0000

    Capture missing barriers at end of kernel

--- tests/tt_metal/tt_metal/noc_debugging/noc_debugging_fixture.hpp
+++ tests/tt_metal/tt_metal/noc_debugging/noc_debugging_fixture.hpp
@@ -32,6 +32,26 @@ public:
         return noc_debug_state->get_issues(core, processor_id).has_issue(NOCDebugIssueType::READ_BARRIER);
     }
 
+    bool has_unflushed_semaphore_mcast_issue(ChipId chip_id, CoreCoord virtual_core, int processor_id) const {
+        auto& noc_debug_state = tt::tt_metal::MetalContext::instance().noc_debug_state();
+        if (!noc_debug_state) {
+            return false;
+        }
+        tt_cxy_pair core{chip_id, {virtual_core.x, virtual_core.y}};
+        return noc_debug_state->get_issues(core, processor_id)
+            .has_issue(NOCDebugIssueType::UNFLUSHED_SEMAPHORE_MCAST_AT_END);
+    }
+
+    bool has_unflushed_write_mcast_issue(ChipId chip_id, CoreCoord virtual_core, int processor_id) const {
+        auto& noc_debug_state = tt::tt_metal::MetalContext::instance().noc_debug_state();
+        if (!noc_debug_state) {
+            return false;
+        }
+        tt_cxy_pair core{chip_id, {virtual_core.x, virtual_core.y}};
+        return noc_debug_state->get_issues(core, processor_id)
+            .has_issue(NOCDebugIssueType::UNFLUSHED_WRITE_MCAST_AT_END);
+    }
+
     template <typename T>
     void RunTestOnDevice(
         const std::function<void(T*, std::shared_ptr<distributed::MeshDevice>)>& run_function,
