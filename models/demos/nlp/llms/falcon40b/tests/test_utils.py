f525d657cfc1304251a97770527b9aff44d2f6e9
gsarabandoTT <ctr-gsarabando@ext.tenstorrent.com>
2026-01-28 21:09:35 +0000

    Move falcon40b to nlp/llms

---
+++ models/demos/nlp/llms/falcon40b/tests/test_utils.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+
+from models.demos.nlp.llms.falcon40b.reference.hf_modeling_falcon import FalconForCausalLM
+
+
+def load_hf_model(model_version):
+    hugging_face_reference_model = FalconForCausalLM.from_pretrained(
+        model_version, local_files_only=os.getenv("CI") == "true", low_cpu_mem_usage=True
+    )
+    hugging_face_reference_model.eval()
+    state_dict = hugging_face_reference_model.state_dict()
+
+    return hugging_face_reference_model, state_dict
