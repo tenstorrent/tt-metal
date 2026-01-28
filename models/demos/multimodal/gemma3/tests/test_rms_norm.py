15cc6945398d0fa9f85f60dd88177d85cc44c089
Radoica Draskic <rdraskic@tenstorrent.com>
2026-01-28 11:27:17 +0100

    Fix gemma tests (#36637)
    - [x] [single
    card](https://github.com/tenstorrent/tt-metal/actions/runs/21431646818)

--- models/demos/multimodal/gemma3/tests/test_rms_norm.py
+++ models/demos/multimodal/gemma3/tests/test_rms_norm.py
@@ -10,7 +10,7 @@ from loguru import logger
 import ttnn
 from models.common.rmsnorm import RMSNorm as RMSNorm
 from models.common.utility_functions import comp_allclose, comp_pcc
-from models.demos.gemma3.tt.model_config import ModelArgs as Gemma3ModelArgs
+from models.demos.multimodal.gemma3.tt.model_config import ModelArgs as Gemma3ModelArgs
 from models.tt_transformers.tt.ccl import TT_CCL
 from models.tt_transformers.tt.distributed_norm import DistributedNorm
 
