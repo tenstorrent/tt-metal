60e2e49f69c1f404356077e8cb7b5799c29d5e80
Frank Plavec <fplavec@tenstorrent.com>
2026-01-29 18:13:43 -0500

    Created matmul lab 2 for universities  (#36584)
    ### Ticket
    #33844
    
    ### Problem description
    We need a lab exercise to implement multi core matrix multiplication
    (matmul) targeting TT hardware, including basic implementation and an
    implementation demonstrating data reuse. Target audience are university
    students at the senior undergrad or junior gradate level.
    
    ### What's changed
    - Added detailed writeup for the lab. Some of the content was reused
    from
    https://docs.tenstorrent.com/tt-metal/latest/tt-metalium/tt_metal/examples/matmul_multi_core.html
    for Exercise 1,
    https://docs.tenstorrent.com/tt-metal/latest/tt-metalium/tt_metal/examples/matmul_multi_core_optimizations/data_reuse.html
    for Exercise 2, and various other documentation.
    - The main difference from the "data_reuse" programming example to
    Exercise 2 is that it was changed to focus on two key concepts:
      1. Blocking to maximize potential for data reuse
    2. Limiting on-chip memory usage by splitting the K dimension into
    smaller chunks.
    Other optimizations that are part of the original "data_reuse"
    programming example are briefly mentioned at the end of the lab as
    "Potential Additional Optimizations"
    - Updated lab 1 to make loop limits more explicit and consistent between
    lab 1 and lab 2
    
    ### Checklist
    
    - [x] [![All post-commit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml/badge.svg?branch=fplavec-33844_matmul_lab_2_pr)](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml?query=branch:fplavec-33844_matmul_lab_2_pr)
    - [x] [![Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml/badge.svg?branch=fplavec-33844_matmul_lab_2_pr)](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml?query=branch:fplavec-33844_matmul_lab_2_pr)
    - [x] New/Existing tests provide coverage for changes

--- tt_metal/api/tt-metalium/work_split.hpp
+++ tt_metal/api/tt-metalium/work_split.hpp
@@ -37,14 +37,17 @@ CoreRangeSet num_cores_to_corerangeset(uint32_t target_num_cores, CoreCoord grid
 
 CoreRangeSet num_cores_to_corerangeset_in_subcoregrids(
     CoreCoord start_core, uint32_t target_num_cores, const CoreRangeSet& sub_core_grids, bool row_wise = false);
-// This function takes in the core grid size, as well as the number of units of work to divide between the cores
+// This function takes in the core grid size, as well as the number of units of work to divide between the cores.
 // This function returns the number of cores, the CoreRangeSet of all cores, and then the CoreRangeSet that does
 // the greater amount of work, and the CoreRangeSet that does less work if work cannot be evenly divided
 // If it can be evenly divided, the second CoreRangeSet is the same as the first, and the last is empty
-// The last 2 args are the units of work for the two core grids
+// The last 2 elements of the tuple are the units of work for the two core grids.
+// This overload assumes that the core grid starts at (0, 0).
 std::tuple<uint32_t, CoreRangeSet, CoreRangeSet, CoreRangeSet, uint32_t, uint32_t> split_work_to_cores(
     CoreCoord grid_size, uint32_t units_to_divide, bool row_wise = false);
 
+// This overload takes in an arbitrary CoreRangeSet of cores, and the number of units of work to divide
+// between the cores.
 std::tuple<uint32_t, CoreRangeSet, CoreRangeSet, CoreRangeSet, uint32_t, uint32_t> split_work_to_cores(
     const CoreRangeSet& core_grid, uint32_t units_to_divide, bool row_wise = false);
 
