4527655c35131c66f3f64138d1dcc76ffab0a283
Billy Ransom <wransom@tenstorrent.com>
2026-01-28 19:59:21 -0800

    Large Kernel Support for MPWI (#35216)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/27845
    
    ### LLK PR
    https://github.com/tenstorrent/tt-llk/pull/1045
    
    ### Problem description
    MPWI currently only supports `kernel_hw<=32`.
    
    ### What's changed
    MPWI now supports arbitrary kernel sizes.
    - SFPU MPWI functions have been updated to do accumulation over multiple
    chunks
    - SFPU `copy_dest_values` has been generalized for all supported data
    types
    - DST multibuffering is disabled for large kernels with MPWI
    - new MPWI kernels have been implemented to separate the increasingly
    complicated logic from normal pool
    - new CBs have been added to handle additional increments and other
    calcualtions
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/20646299295
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/20646300971
    - [x] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/20646316458
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/20646311906
    - [x] [Nightly
    L2](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/20646304099
    - [x] [Nightly
    Blackhole](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-nightly-tests.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/20647793333
    - [x] [Frequent
    model](https://github.com/tenstorrent/tt-metal/actions/workflows/fast-dispatch-full-regressions-and-models.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/20646320125

--- tt_metal/include/compute_kernel_api/copy_dest_values.h
+++ tt_metal/include/compute_kernel_api/copy_dest_values.h
@@ -7,13 +7,36 @@
 #include "compute_kernel_api/common_globals.h"
 #ifdef TRISC_MATH
 #include "llk_math_eltwise_binary_sfpu_copy_dest_values.h"
+#include "ckernel_sfpu_copy_dest_values.h"
 #endif
 
 namespace ckernel {
 
 // clang-format off
 /**
- * Copies all values from the tile in idst1 to the tile in idst0 in the DST register buffer.
+ * Copies all values from the tile in idst_in to the tile in idst_out in the DST register buffer.
+ * This is a generalized version that takes a DataFormat template parameter.
+ *
+ * The DST register buffer must be in acquired state via *tile_regs_acquire* call. This call is blocking and is only
+ * available on the compute engine.
+ *
+ * Return value: None
+ *
+ * | Argument       | Description                                                           | Type                     | Valid Range                                           | Required |
+ * |----------------|-----------------------------------------------------------------------|--------------------------|-------------------------------------------------------|----------|
+ * | DATA_FORMAT    | The data format for copy operation                                    | DataFormat               | Any valid DataFormat enum value                       | True     |
+ * | idst_in        | The index of the tile in DST register buffer to copy values from      | uint32_t                 | Must be less than the size of the DST register buffer | True     |
+ * | idst_out       | The index of the tile in DST register buffer to copy values to        | uint32_t                 | Must be less than the size of the DST register buffer | True     |
+ */
+// clang-format on
+template <DataFormat DATA_FORMAT>
+ALWI void copy_dest_values(uint32_t idst_in, uint32_t idst_out) {
+    MATH(llk_math_eltwise_binary_sfpu_copy_dest_values<static_cast<DataFormat>(DATA_FORMAT)>(idst_in, idst_out));
+}
+
+// clang-format off
+/**
+ * Copies all values from the tile in idst_in to the tile in idst_out in the DST register buffer.
  *
  * The DST register buffer must be in acquired state via *tile_regs_acquire* call. This call is blocking and is only
  * available on the compute engine.
@@ -22,12 +45,13 @@ namespace ckernel {
  *
  * | Argument       | Description                                                           | Type     | Valid Range                                           | Required |
  * |----------------|-----------------------------------------------------------------------|----------|-------------------------------------------------------|----------|
- * | idst0          | The index of the tile in DST register buffer to copy values to        | uint32_t | Must be less than the size of the DST register buffer | True     |
- * | idst1          | The index of the tile in DST register buffer to copy values from      | uint32_t | Must be less than the size of the DST register buffer | True     |
+ * | idst_in        | The index of the tile in DST register buffer to copy values from      | uint32_t | Must be less than the size of the DST register buffer | True     |
+ * | idst_out       | The index of the tile in DST register buffer to copy values to        | uint32_t | Must be less than the size of the DST register buffer | True     |
  */
 // clang-format on
-ALWI void copy_dest_values(uint32_t idst0, uint32_t idst1) {
-    MATH(llk_math_eltwise_binary_sfpu_copy_dest_values(idst0, idst1));
+[[deprecated("Use copy_dest_values<DataFormat> instead")]]
+ALWI void copy_dest_values(uint32_t idst_in, uint32_t idst_out) {
+    MATH(llk_math_eltwise_binary_sfpu_copy_dest_values(idst_in, idst_out));
 }
 
 ALWI void copy_dest_values_init() { MATH(llk_math_eltwise_binary_sfpu_copy_dest_values_init()); }
