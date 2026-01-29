1f3b6a01727cdf21be0541b9e595dbcc5f0b202d
Colman Glagovich <cglagovich@tenstorrent.com>
2026-01-29 22:11:08 +0000

    Add 4x32 option to transformer block test. Add SDPA chunk size option for 4x32
    (cherry picked from commit 8399198cd69863a0f9e746fca561e4b2057a81c5)

--- models/tt_dit/models/transformers/wan2_2/attention_wan.py
+++ models/tt_dit/models/transformers/wan2_2/attention_wan.py
@@ -25,8 +25,8 @@ class WanAttention(Module):
         (False, 2, 4): (256, 256),
         (False, 8, 4): (256, 256),
         (True, 2, 2): (128, 512),
-        (True, 8, 4): (288, 512),
-        (True, 32, 4): (224, 512),
+        (True, 8, 4): (128, 512),
+        (True, 32, 4): (128, 128),
     }
     default_sdpa_chunk_size = (256, 256)
 
