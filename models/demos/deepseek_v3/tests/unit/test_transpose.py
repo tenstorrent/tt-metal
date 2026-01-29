2d4837288b276dd997ed7ce2dce545e688b32d64
Ronnie Li <yuqiaoli@tenstorrent.com>
2026-01-29 17:44:16 +0000

    Add test

--- models/demos/deepseek_v3/tests/unit/test_transpose.py
+++ models/demos/deepseek_v3/tests/unit/test_transpose.py
@@ -96,3 +96,33 @@ def test_transpose(mesh_device, shape, transpose_type, pad_val, dtype, layout, m
         assert_with_pcc(torch_output, tt_output, 0.9999)
 
     run_test(mesh_device, run_op, check_op, enable_trace)
+
+
+@pytest.mark.parametrize("layout", [ttnn.TILE_LAYOUT, ttnn.ROW_MAJOR_LAYOUT])
+@pytest.mark.parametrize("mesh_device", [(8, 8)], indirect=True)
+def test_mesh_transpose(mesh_device, layout):
+    torch_input = torch.randn([1, 1, 1, 1], dtype=torch.bfloat16)
+    torch_transposed = torch.transpose(torch_input, 1, 2)
+    tt_input = ttnn.from_torch(
+        torch_input,
+        dtype=ttnn.bfloat16,
+        layout=layout,
+        device=mesh_device,
+        memory_config=ttnn.MemoryConfig(ttnn.TensorMemoryLayout.INTERLEAVED, ttnn.BufferType.L1),
+        mesh_mapper=ttnn.ShardTensorToMesh(mesh_device, 0),
+    )
+    if layout == ttnn.TILE_LAYOUT:
+        tt_untilize = ttnn.untilize(tt_input)
+    else:
+        tt_untilize = tt_input
+    ttnn.synchronize_device(mesh_device)
+    tt_output = ttnn.transpose(tt_untilize, 1, 2)
+
+    tt_untilize_torch = ttnn.to_torch(tt_untilize, mesh_composer=ttnn.ConcatMeshToTensor(mesh_device, dim=0))
+    tt_output_torch = ttnn.to_torch(tt_output, mesh_composer=ttnn.ConcatMeshToTensor(mesh_device, dim=0))
+
+    print(torch_transposed, tt_untilize_torch, tt_output_torch)
+    eq, output = assert_with_pcc(torch_input, tt_untilize_torch, 0.9999)
+    eq, output = assert_with_pcc(torch_transposed, tt_output_torch, 0.9999)
+    if not eq:
+        assert False, f"FAILED: permute mismatch"
