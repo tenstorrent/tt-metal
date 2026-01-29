706b8ba855246a21f899c1769df6a437bb9d919b
Billy Ransom <wransom@tenstorrent.com>
2026-01-28 22:32:50 -0800

    Large Kernel Support for MPWI (#35216)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/27845
    
    ### LLK PR
    https://github.com/tenstorrent/tt-llk/pull/1045
    
    ### Problem description
    MPWI currently only supports `kernel_hw<=32`.
    
    ### What's changed
    MPWI now supports arbitrary kernel sizes.
    - SFPU MPWI functions have been updated to do accumulation over multiple
    chunks
    - SFPU `copy_dest_values` has been generalized for all supported data
    types
    - DST multibuffering is disabled for large kernels with MPWI
    - new MPWI kernels have been implemented to separate the increasingly
    complicated logic from normal pool
    - new CBs have been added to handle additional increments and other
    calcualtions
    
    ### Checklist
    - [x] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/20646299295
    - [x] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/20646300971
    - [x] [Model
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-models.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/20646316458
    - [x] [Device performance
    regression](https://github.com/tenstorrent/tt-metal/actions/workflows/perf-device-models.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/20646311906
    - [x] [Nightly
    L2](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/20646304099
    - [x] [Nightly
    Blackhole](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-nightly-tests.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/20647793333
    - [x] [Frequent
    model](https://github.com/tenstorrent/tt-metal/actions/workflows/fast-dispatch-full-regressions-and-models.yaml)
    CI passes:
    https://github.com/tenstorrent/tt-metal/actions/runs/20646320125

--- ttnn/cpp/ttnn/operations/pool/pool_utils.cpp
+++ ttnn/cpp/ttnn/operations/pool/pool_utils.cpp
@@ -148,9 +148,7 @@ FactoryParameters get_factory_parameters(
         !last_tile_is_partial ? tt::constants::TILE_HEIGHT : tt::constants::TILE_HEIGHT / 2;
     const bool is_large_kernel = kernel_size_hw > max_rows_for_reduction;
     if (return_indices) {
-        TT_FATAL(
-            !is_avg_pool && !is_large_kernel,
-            "Currently only small full width max pool is supported with return_indices");
+        TT_FATAL(!is_avg_pool, "return_indices only applies for MaxPool");
     }
     const uint32_t MAX_TILES_PER_REDUCTION = return_indices ? 1 : (is_avg_pool && is_large_kernel) ? 4 : 8;
     const bool is_wide_reduction = in_ntiles_c > MAX_TILES_PER_REDUCTION;
@@ -234,14 +232,11 @@ uint32_t calculate_L1_usage(
     uint32_t clear_value_cb_size = tt::constants::TILE_HW * params.nbytes;
 
     uint32_t in_cb_sz = 0;
-    if (return_indices) {
-        in_cb_sz = params.MAX_TILES_PER_REDUCTION * tt::constants::TILE_WIDTH * tt::constants::TILE_HEIGHT;
+    if (return_indices || params.is_wide_reduction) {
+        uint32_t height_multiplier = return_indices ? tt::constants::TILE_HEIGHT : params.num_tilized_rows;
+        in_cb_sz = params.MAX_TILES_PER_REDUCTION * tt::constants::TILE_WIDTH * height_multiplier;
     } else {
-        if (params.is_wide_reduction) {
-            in_cb_sz = params.MAX_TILES_PER_REDUCTION * tt::constants::TILE_WIDTH * params.num_tilized_rows;
-        } else {
-            in_cb_sz = params.in_ntiles_c * tt::constants::TILE_WIDTH * params.num_tilized_rows;
-        }
+        in_cb_sz = params.in_ntiles_c * tt::constants::TILE_WIDTH * params.num_tilized_rows;
     }
 
     uint32_t in_cb_page_padded = tt::round_up(in_cb_sz, tt::constants::TILE_HW);
@@ -260,9 +255,13 @@ uint32_t calculate_L1_usage(
         uint32_t tile_elems = tt::constants::TILE_WIDTH * tt::constants::TILE_HEIGHT;
         uint32_t idx_tile_size = params.index_nbytes * tile_elems * 1;  // 1 page
         uint32_t data_tile_size = params.nbytes * tile_elems * 1;       // 1 page
-        // 1 data sized tile (pack_tmp_cb) and 5 index sized tiles (in_idx, pack_idx_tmp, right_inc, down_left_wrap_inc,
-        // up_left_wrap_inc)
-        total_mpwi_cb_size = (5 * idx_tile_size) + data_tile_size;
+        // 1 data sized tile (pack_tmp_cb) and 6 index sized tiles (in_idx, pack_idx_tmp, right_inc, down_left_wrap_inc,
+        // up_left_wrap_inc, compute_idx_tmp)
+        total_mpwi_cb_size = (6 * idx_tile_size) + data_tile_size;
+        if (params.is_large_kernel) {
+            // additional temp data tile for large kernel (intra_kernel_right_inc, intra_kernel_down_left_wrap_inc)
+            total_mpwi_cb_size += 2 * idx_tile_size;
+        }
     }
 
     uint32_t out_cb_pagesize;
