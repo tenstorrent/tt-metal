1cfda7e35ed4625599004ca6e4951d30a7f9af6f
William Ly <williamly@tenstorrent.com>
2026-01-30 12:49:05 -0500

    fixup

--- dockerfile/upstream_test_images/run_upstream_tests_vanilla.sh
+++ dockerfile/upstream_test_images/run_upstream_tests_vanilla.sh
@@ -265,7 +265,7 @@ test_suite_bh_glx_torus_xyz_health_check_tests() {
     ./build/tools/scaleout/run_cluster_validation --cabling-descriptor-path tools/tests/scaleout/cabling_descriptors/bh_galaxy_xy_torus_z_ports.textproto --hard-fail --send-traffic
 }
 
-test_suite_wh_qb2_tests() {
+test_suite_wh_qb2_unit_tests() {
     echo "[upstream-tests] running WH QB2 upstream metal tests"
     export TT_MESH_GRAPH_DESC_PATH=tt_metal/fabric/mesh_graph_descriptors/n300_x2_mesh_graph_descriptor.textproto
     TT_METAL_SLOW_DISPATCH_MODE=1 ./build/test/tt_metal/unit_tests_dispatch --gtest_filter="UnitMeshCQSingleCardFixture.*"
@@ -276,8 +276,10 @@ test_suite_wh_qb2_tests() {
 
     echo "[upstream-tests] running WH QB2 upstream ttnn tests"
     ./build/test/ttnn/unit_tests_ttnn_ccl
+}
 
-    echo "[upstream-tests] running WH QB2 upstream demo test"
+test_suite_wh_qb2_llama_demo_tests() {
+    echo "[upstream-tests] running WH QB2 upstream Llama demo tests with weights"
     verify_llama_dir_
     pytest models/tt_transformers/demo/simple_text_demo.py -k batch-32
 }
@@ -341,7 +343,8 @@ test_suite_bh_glx_python_unit_tests
 test_suite_bh_glx_llama_demo_tests"
 
 hw_topology_test_suites["wh_qb2"]="
-test_suite_wh_qb2_tests"
+test_suite_wh_qb2_unit_tests
+test_suite_wh_qb2_llama_demo_tests"
 
 # Function to display help
 show_help() {
