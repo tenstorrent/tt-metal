05e35f1270366050d032c855c31b4b5d9ff2abc8
Joe Malone <jmalone@tenstorrent.com>
2026-01-30 02:52:55 +0000

    WIP adding configs for exabox

--- tt-train/sources/examples/python/multihost/fabric_minimal_example/runner.sh
+++ tt-train/sources/examples/python/multihost/fabric_minimal_example/runner.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-#SBATCH --partition=debug
+#SBATCH --partition=bh_pod_4x32_1
+#SBATCH --nodelist=bh-glx-b04u02,bh-glx-b04u08
 #SBATCH --job-name=fabric_minimal
 #SBATCH --output=fabric_minimal_%j.out
 #SBATCH --error=fabric_minimal_%j.err
 
 # Set environmental variables
-export TT_METAL_HOME="/data/${USER}/tt-metal"
+export TT_METAL_HOME="/data/${USER}/pr_review/tt-metal"
 export PYTHONPATH="${TT_METAL_HOME}:${PYTHONPATH}"
 source ${TT_METAL_HOME}/python_env/bin/activate
 export LD_LIBRARY_PATH="/opt/openmpi-v5.0.7-ulfm/lib:$LD_LIBRARY_PATH"
 
-FABRIC_MINIMAL="/data/${USER}/tt-metal/tt-train/sources/examples/python/multihost/fabric_minimal_example"
+FABRIC_MINIMAL="/data/${USER}/pr_review/tt-metal/tt-train/sources/examples/python/multihost/fabric_minimal_example"
 CONFIG_FILE="training_configs/training_shakespeare_tinyllama_2tier_fabric.yaml"
 
-# Create hostfile from Slurm allocation
-HOSTFILE="/tmp/hostfile_${SLURM_JOB_ID}"
-scontrol show hostnames $SLURM_JOB_NODELIST | while read host; do
-    echo "${host} slots=1"
-done > ${HOSTFILE}
+HOSTLIST="/data/jmalone/config/2loudboxes/hosts.txt"
+RANK_BINDINGS="/data/jmalone/config/2loudboxes/rank_bindings.yaml"
 
-tt-run --mpi-args "--hostfile ${HOSTFILE} -x TT_METAL_HOME --mca mpi_show_mca_params all --mca btl_tcp_if_include eno1 --mca oob_tcp_if_include eno1 --mca btl self,tcp --tag-output" --rank-binding ${FABRIC_MINIMAL}/configurations/2loudboxes/rank_bindings.yaml python ${FABRIC_MINIMAL}/example.py -c ${CONFIG_FILE}
+tt-run --mpi-args "--map-by rankfile:file=${HOSTLIST} -x TT_METAL_HOME --mca mpi_show_mca_params all --mca btl_tcp_if_include eno1 --mca oob_tcp_if_include eno1 --mca btl self,tcp --tag-output" --rank-binding ${RANK_BINDINGS} python ${FABRIC_MINIMAL}/example.py -c ${CONFIG_FILE}
--- tt-train/sources/examples/python/multihost/pipeline_parallel_training/runner.sh
+++ tt-train/sources/examples/python/multihost/pipeline_parallel_training/runner.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
-#SBATCH --partition=debug
+#SBATCH --partition=bh_pod_4x32_2
+#SBATCH --nodes=4
+#SBATCH --nodelist=bh-glx-b08u02,bh-glx-b08u08,bh-glx-b09u02,bh-glx-b09u08
 #SBATCH --job-name=pipeline_parallel_training
 #SBATCH --output=pipeline_parallel_training_%j.out
 #SBATCH --error=pipeline_parallel_training_%j.err
+
 # Set environmental variables
-export TT_METAL_HOME="/data/${USER}/tt-metal"
-export PYTHONPATH="${TT_METAL_HOME}:${PYTHONPATH}"
+export TT_METAL_HOME="/data/${USER}/pr_review/tt-metal"
+export PYTHONPATH="${TT_METAL_HOME}"
 source ${TT_METAL_HOME}/python_env/bin/activate
 export LD_LIBRARY_PATH="/opt/openmpi-v5.0.7-ulfm/lib:$LD_LIBRARY_PATH"
 
+mpirun -np 4 tt-smi -r
+
+HOSTFILE="/tmp/hostfile_${SLURM_JOB_ID}"
+scontrol show hostnames $SLURM_JOB_NODELIST | while read host; do
+    echo "${host} slots=1"
+done > ${HOSTFILE}
+
 CONFIG_FILE="training_configs/training_shakespeare_llama7b_pp_fabric.yaml"
 PP_ROOT="${TT_METAL_HOME}/tt-train/sources/examples/python/multihost/pipeline_parallel_training"
-HOST_CONFIG="5loudboxes"
+HOST_CONFIG="4galaxies"
 
-tt-run --mpi-args "--hostfile ${PP_ROOT}/configurations/${HOST_CONFIG}/hosts.txt -x TT_METAL_HOME --mca mpi_show_mca_params all --mca btl_tcp_if_include eno1 --mca oob_tcp_if_include eno1 --mca btl self,tcp --tag-output" --rank-binding ${PP_ROOT}/configurations/${HOST_CONFIG}/rank_bindings.yaml python ${PP_ROOT}/training.py -c ${CONFIG_FILE}
+tt-run --verbose \
+    --mpi-args "--hostfile ${HOSTFILE} --mca btl_tcp_if_exclude docker0,lo  --mca btl self,tcp --tag-output" \
+    --rank-binding ${PP_ROOT}/configurations/${HOST_CONFIG}/rank_bindings.yaml \
+    python ${PP_ROOT}/training.py -c ${CONFIG_FILE}
