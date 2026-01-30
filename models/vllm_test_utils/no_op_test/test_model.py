c25073d2c967a9c984b812a58f811971f9d14ac1
Salar Hosseini <159165450+skhorasganiTT@users.noreply.github.com>
2026-01-30 17:08:05 -0500

    Add vLLM DummyNoOpModel for testing vLLM host overhead (#36692)
    ### Ticket
    #36363
    
    ### Problem description
    - Needed a way to test E2E vLLM host overhead excluding model
    contribution
    
    ### What's changed
    - Added DummyNoOpModel test model for vLLM which does no computation,
    only returns output torch token tensors.
    - Added test to vLLM nightly for running DummyNoOpModel and measuring
    E2E perf
    - Corresponding vLLM PR which registers the new test model:
    https://github.com/tenstorrent/vllm/pull/305
    
    ### Checklist
    - [ ] vLLM nightly -
    https://github.com/tenstorrent/tt-metal/actions/runs/21459313092
    
    ---------
    
    Signed-off-by: Salar <skhorasgani@tenstorrent.com>

---
+++ models/vllm_test_utils/no_op_test/test_model.py
@@ -0,0 +1,40 @@
+# SPDX-FileCopyrightText: © 2026 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+
+
+class DummyNoOpModel:
+    """
+    Dummy model class which does nothing for prefill and decode forward.
+    Assumes sampling on device. Used to measure the host overheads from vLLM.
+    """
+
+    def __init__(self, mesh_device, max_batch_size, vocab_size):
+        self.mesh_device = mesh_device
+        self.max_batch_size = max_batch_size
+        self.vocab_size = vocab_size
+        self.decode_out = torch.ones(max_batch_size, 1, dtype=torch.int32)
+
+    @classmethod
+    def initialize_vllm_model(cls, hf_config, mesh_device, max_batch_size, **kwargs):
+        vocab_size = hf_config.vocab_size
+        return cls(mesh_device, max_batch_size, vocab_size)
+
+    def prefill_forward(self, *args, **kwargs):
+        tokens = kwargs.get("tokens")
+        # Run nothing for prefill forward in this dummy model
+        return torch.ones(tokens.shape[0], 1, 1, dtype=torch.int32)
+
+    def decode_forward(self, *args, **kwargs):
+        # Run nothing for decode forward in this dummy model
+        tokens = kwargs.get("tokens")
+        assert tokens.shape[0] == self.max_batch_size, "Batch size mismatch"
+        return self.decode_out
+
+    def allocate_kv_cache(self, *args, **kwargs):
+        return None
+
+    def warmup_model_prefill(self, *args, **kwargs):
+        pass
