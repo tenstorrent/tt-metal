1189fc8d95814d7bf7771a0f145bb641273fffb8
Ambrose Ling <aling@tenstorrent.com>
2026-01-31 16:01:29 +0000

    remove changes to experimental, not tested in ci

--- models/experimental/gemma3_4b/tests/test_decoder.py
+++ models/experimental/gemma3_4b/tests/test_decoder.py
@@ -9,7 +9,7 @@ import pytest
 from loguru import logger
 import os
 import ttnn
-from models.experimental.gemma3_4b.tt.model_config import ModelArgs
+from models.tt_transformers.tt.model_config import ModelArgs
 from models.tt_transformers.tt.ccl import TT_CCL
 from models.experimental.gemma3_4b.tt.decoder import TransformerBlock
 from models.common.utility_functions import (
