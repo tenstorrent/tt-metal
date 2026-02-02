6720554f899cd184584c6b7ef8bd3fe431c569d3
awliu-TT <awliu@tenstorrent.com>
2026-02-02 23:56:29 +0000

    fixing comment formatting

--- ttnn/cpp/ttnn/operations/data_movement/untilize/device/factories/untilize_multi_core_nd_shard_input_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/untilize/device/factories/untilize_multi_core_nd_shard_input_program_factory.cpp
@@ -80,10 +80,11 @@ UntilizeMultiCoreNDShardInputProgramFactory::cached_program_t UntilizeMultiCoreN
 
     // Input CB
     uint32_t input_cb_num_tiles;
-    if (num_input_blocks_per_full_core ==
-        1) {  // No need to double buffer if the core is only processing a single block
+    if (num_input_blocks_per_full_core == 1) {
+        // No need to double buffer if the core is only processing a single block
         input_cb_num_tiles = num_tiles_per_input_block;
     } else {
+        // Double buffer if the core is processing 2+ blocks
         input_cb_num_tiles = num_tiles_per_input_block * 2;
     }
     auto [src0_cb_index, cb_src0] = create_cb(
