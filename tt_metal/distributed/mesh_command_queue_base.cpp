3759170709ce51e89b1b17bc1c727295fa1f9e69
John Bauman <jbauman@tenstorrent.com>
2026-02-03 06:20:55 +0000

    format

--- tt_metal/distributed/mesh_command_queue_base.cpp
+++ tt_metal/distributed/mesh_command_queue_base.cpp
@@ -282,9 +282,10 @@ void MeshCommandQueueBase::enqueue_write(
         auto buf = host_buffer.get_shard(host_buffer_coord);
         if (buf.has_value()) {
             auto shard_data_transfer = distributed::ShardDataTransfer{MeshCoordinate(host_buffer_coord)}
-                                               .host_data(buf->view_bytes().data())
-                                               .region(BufferRegion(0, buf->view_bytes().size()));
-            experimental::ShardDataTransferSetPinnedMemory(shard_data_transfer, experimental::HostBufferGetPinnedMemory(*buf));
+                                           .host_data(buf->view_bytes().data())
+                                           .region(BufferRegion(0, buf->view_bytes().size()));
+            experimental::ShardDataTransferSetPinnedMemory(
+                shard_data_transfer, experimental::HostBufferGetPinnedMemory(*buf));
             shard_data_transfers.push_back(std::move(shard_data_transfer));
         }
     }
