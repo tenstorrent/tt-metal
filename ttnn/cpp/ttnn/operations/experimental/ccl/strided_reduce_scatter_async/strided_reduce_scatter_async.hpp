0ff030fccb2f6263b0c75ca42cdfac3f2c052fc5
Bazyli Klockiewicz <bklockiewicz@tenstorrent.com>
2026-02-03 15:32:50 +0000

    remove unused variable (can restore later if needed)

--- ttnn/cpp/ttnn/operations/experimental/ccl/strided_reduce_scatter_async/strided_reduce_scatter_async.hpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/strided_reduce_scatter_async/strided_reduce_scatter_async.hpp
@@ -31,7 +31,6 @@ struct ExecuteStridedReduceScatterAsync {
         std::optional<uint32_t> mm_cores_y = std::nullopt,
         std::optional<uint32_t> mm_block_ht = std::nullopt,
         std::optional<uint32_t> mm_block_wt = std::nullopt,
-        std::optional<uint32_t> mm_M_block_ht = std::nullopt,
         std::optional<uint32_t> mm_N_block_wt = std::nullopt,
         std::optional<uint32_t> chunk_width_in_mm_blocks = std::nullopt);
 };
