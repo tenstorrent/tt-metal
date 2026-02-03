d371bcef873975fb7cc7cd84e5fa7f9a9b83ba50
Momcilo Vasilijevic <mvasilijevic@tenstorrent.com>
2026-02-03 11:04:02 +0000

    Use kernel_main, clean DPRINT

--- ttnn/cpp/ttnn/operations/madd/device/kernels/compute/madd_compute.cpp
+++ ttnn/cpp/ttnn/operations/madd/device/kernels/compute/madd_compute.cpp
@@ -6,13 +6,23 @@
 #include <compute_kernel_api/common.h>
 #include <compute_kernel_api/eltwise_binary.h>
 
-#include "api/debug/dprint.h"
-#include "tt_metal/hw/inc/api/debug/dprint_pages.h"
+// #include "api/debug/dprint.h"
+// #include "tt_metal/hw/inc/api/debug/dprint_pages.h"
 
 #include <cstdint>
 
-namespace NAMESPACE {
-void MAIN {
+// --- Sample code for DPRINT debugging ---
+//         DPRINT << "Processing page " << i << " / " << num_pages << ENDL();
+
+//         DPRINT << "Reading tile A from CB " << cb_srcA_index << ENDL();
+//         cb_wait_front(cb_srcA_index, 1);
+// #ifdef DEBUG_PRINT_ENABLED
+//         tt::compute::common::print_full_tile(cb_srcA_index, 0, false); // Math doesn't have direct access to CB data
+// #endif
+//         DPRINT << "Reading tile B from CB " << cb_srcB_index << ENDL();
+// ---------------------------------------
+
+void kernel_main() {
     constexpr uint32_t num_pages = get_compile_time_arg_val(0);
     constexpr uint32_t cb_srcA_index = get_compile_time_arg_val(1);
     constexpr uint32_t cb_srcB_index = get_compile_time_arg_val(2);
@@ -29,19 +39,9 @@ void MAIN {
     cb_wait_front(cb_zero_index, 1);
 
     for (uint32_t i = 0; i < num_pages; ++i) {
-        DPRINT << "Processing page " << i << " / " << num_pages << ENDL();
-
-        DPRINT << "Reading tile A from CB " << cb_srcA_index << ENDL();
         cb_wait_front(cb_srcA_index, 1);
-#ifdef DEBUG_PRINT_ENABLED
-        tt::compute::common::print_full_tile(cb_srcA_index, 0, false);
-#endif
-        DPRINT << "Reading tile B from CB " << cb_srcB_index << ENDL();
         cb_wait_front(cb_srcB_index, 1);
 
-#ifdef DEBUG_PRINT_ENABLED
-        tt::compute::common::print_full_tile(cb_srcB_index, 0, false);
-#endif
         tile_regs_acquire();  // math core acquires dst, init dsts to zero
         mul_tiles_init(cb_srcA_index, cb_srcB_index);
         mul_tiles(cb_srcA_index, cb_srcB_index, 0, 0, dst0);
@@ -49,16 +49,7 @@ void MAIN {
         cb_pop_front(cb_srcA_index, 1);
         cb_pop_front(cb_srcB_index, 1);
 
-        DPRINT << "Reading tile C from CB " << cb_srcC_index << ENDL();
         cb_wait_front(cb_srcC_index, 1);
-#ifdef DEBUG_PRINT_ENABLED
-        tt::compute::common::print_full_tile(cb_srcC_index, 0, false);
-#endif
-
-        DPRINT << "Reading tile zero from CB " << cb_zero_index << ENDL();
-#ifdef DEBUG_PRINT_ENABLED
-        tt::compute::common::print_full_tile(cb_zero_index, 0, false);
-#endif
 
         // Load C and add with result in DST[0] + cb_srcC_index -> DST[0]
         binary_dest_reuse_tiles_init<EltwiseBinaryType::ELWADD, EltwiseBinaryReuseDestType::DEST_TO_SRCA>(
@@ -72,19 +63,13 @@ void MAIN {
         tile_regs_commit();
         cb_pop_front(cb_srcC_index, 1);
 
-        DPRINT << "Writing output tile to CB " << cb_out_index << ENDL();
         cb_reserve_back(cb_out_index, 1);  // Pack
-
         tile_regs_wait();                  // Called by packer
         pack_tile(dst0, cb_out_index, 0);  // Pack
         tile_regs_release();               // Pack
 
-#ifdef DEBUG_PRINT_ENABLED
-        tt::compute::common::print_full_tile(cb_out_index, 0, false);
-#endif
         cb_push_back(cb_out_index, 1);  // Pack
     }
 
     cb_pop_front(cb_zero_index, 1);
-}  // MAIN
-}  // namespace NAMESPACE
+}
