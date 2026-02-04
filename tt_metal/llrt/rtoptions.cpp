81c4529a3f6095e892a5e2f0f238ffdcfef64126
Milos Acimovic <macimovic@tenstorrent.com>
2026-02-04 08:40:11 +0000

    bad merge fix

--- tt_metal/llrt/rtoptions.cpp
+++ tt_metal/llrt/rtoptions.cpp
@@ -123,6 +123,7 @@ enum class EnvVarID {
     TT_METAL_ARC_DEBUG_BUFFER_SIZE,                // ARC processor debug buffer size
     TT_METAL_OPERATION_TIMEOUT_SECONDS,            // Operation timeout duration
     TT_METAL_DISPATCH_TIMEOUT_COMMAND_TO_EXECUTE,  // Terminal command to execute on dispatch timeout.
+    TT_METAL_NOC_DEBUG_DUMP,                       // Enable experimental NOC debug dump to detect missing barriers
     TT_METAL_EXIT_ON_DISPATCH_TIMEOUT,             // Exit application on dispatch timeout with exit code
     TT_METAL_DISPATCH_TIMEOUT_EXIT_CODE,           // Exit code to use when exiting on dispatch timeout
     TT_METAL_DEVICE_DEBUG_DUMP_ENABLED,            // Enable experimental debug dump mode for profiler
