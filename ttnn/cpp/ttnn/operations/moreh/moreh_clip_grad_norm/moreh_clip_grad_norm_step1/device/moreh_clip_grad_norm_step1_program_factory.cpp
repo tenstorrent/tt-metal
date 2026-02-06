60beb3645529ace0bf07bb58473b4985247c8c21
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2026-02-06 04:52:56 +0000

    #0: Update

--- ttnn/cpp/ttnn/operations/moreh/moreh_clip_grad_norm/moreh_clip_grad_norm_step1/device/moreh_clip_grad_norm_step1_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/moreh/moreh_clip_grad_norm/moreh_clip_grad_norm_step1/device/moreh_clip_grad_norm_step1_program_factory.cpp
@@ -16,12 +16,10 @@ namespace ttnn::operations::moreh::moreh_clip_grad_norm_step1 {
 
 std::tuple<uint32_t, float, bool> get_p_decimal_p_is_negative(float ord) {
     auto p = std::floor(ord);
-    auto decimal = ord - p;
+    auto fractional_part = ord - p;
     const bool p_is_negative = p < 0.0f;
-    if (p_is_negative) {
-        p = -p;
-    }
-    return std::make_tuple(static_cast<uint32_t>(p), decimal, p_is_negative);
+    uint32_t integer_part = static_cast<uint32_t>(std::abs(p));
+    return std::make_tuple(integer_part, fractional_part, p_is_negative);
 }
 
 MorehClipGradNormStep1Operation::ProgramFactory::cached_program_t
