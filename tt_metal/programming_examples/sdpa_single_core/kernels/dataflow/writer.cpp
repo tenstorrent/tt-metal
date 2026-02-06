9cbfd1d643a0cd9df44175b94dc02842152b9817
Djordje Nijemcevic <dnijemcevic@tenstorrent.com>
2026-02-06 15:58:33 +0000

    Adding the @V part. Overlapping the first row compute with the previous (EXP) drain.

--- tt_metal/programming_examples/sdpa_single_core/kernels/dataflow/writer.cpp
+++ tt_metal/programming_examples/sdpa_single_core/kernels/dataflow/writer.cpp
@@ -8,9 +8,10 @@
 void kernel_main() {
     constexpr uint32_t Sq_chunk_t = get_compile_time_arg_val(0);
     constexpr uint32_t Sk_chunk_t = get_compile_time_arg_val(1);
-    constexpr uint32_t head_dim_t = get_compile_time_arg_val(2);
-    constexpr uint32_t num_iter = get_compile_time_arg_val(3);
-    constexpr uint32_t identity_scalar_packed = get_compile_time_arg_val(4);
+    constexpr uint32_t Sv_chunk_t = get_compile_time_arg_val(2);
+    constexpr uint32_t head_dim_t = get_compile_time_arg_val(3);
+    constexpr uint32_t num_iter = get_compile_time_arg_val(4);
+    constexpr uint32_t identity_scalar_packed = get_compile_time_arg_val(5);
 
     constexpr uint32_t cb_identity_scale_in = tt::CBIndex::c_5;
     generate_reduce_scaler(cb_identity_scale_in, identity_scalar_packed);
