a3c592ec9be4d38c95c67c32c7dd69ee0425e218
Yu Gao <145494740+yugaoTT@users.noreply.github.com>
2026-02-09 13:31:14 -0500

    DeepSeek Blitz MOE routed expert  (#37294)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/35538)
    
    ### Problem description
    Fuse routed expert (without the final reduce to one)
    
    ### What's changed
    Fuse routed expert, tested on single device vs 4x2 mesh, all passed. 
    flow:
    mcast
    gate_mm
    gather
    gate
    mcast (index and scalar)
    dram_mm (gate_proj) + silu
    dram_mm (up_proj)
    mul + mul_scalar
    gather
    mcast
    dram_mm
    fused_add
    
    
    moved mul-scalar to before down-proj mm for less computation.
    
    kenrel duration: 65us
    
    ### Checklist
    
    - [ ] [![All post-commit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml/badge.svg?branch={{branch_name}})](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml?query=branch:{{branch_name}})
    - [ ] [![Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml/badge.svg?branch={{branch_name}})](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml?query=branch:{{branch_name}})
    - [ ]
    [![cpp-unit-tests](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml/badge.svg?branch={{branch_name}})](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml?query=branch:{{branch_name}})
    - [ ] New/Existing tests provide coverage for changes

--- models/demos/deepseek_v3_b1/kernel_includes/tt_metal/include/compute_kernel_api/custom_mm.h
+++ models/demos/deepseek_v3_b1/kernel_includes/tt_metal/include/compute_kernel_api/custom_mm.h
@@ -13,21 +13,21 @@
 #endif
 namespace ckernel {
 
-template <bool transpose = false, bool split_acc = false, bool dense_packing = false>
+template <bool transpose = false, bool split_acc = false, bool dense_packing = false, bool fp32_dest_acc_en = DST_ACCUM_MODE>
 ALWI void custom_mm_block_init(
     const std::uint32_t in0_cb_id,
     const std::uint32_t in1_cb_id,
     const std::uint32_t out_cb_id,
     const std::uint32_t ct_dim = 1) {
-    UNPACK((llk_unpack_hw_configure<DST_ACCUM_MODE>(in1_cb_id, in0_cb_id)));
+    UNPACK((llk_unpack_hw_configure<fp32_dest_acc_en>(in1_cb_id, in0_cb_id)));
     UNPACK((llk_unpack_AB_custom_mm_init<transpose>(in0_cb_id, in1_cb_id, ct_dim)));
 
-    MATH((llk_math_pack_sync_init<DST_ACCUM_MODE>()));
-    MATH((llk_math_hw_configure<DST_ACCUM_MODE>(in0_cb_id, in1_cb_id)));
+    MATH((llk_math_pack_sync_init<fp32_dest_acc_en>()));
+    MATH((llk_math_hw_configure<fp32_dest_acc_en>(in0_cb_id, in1_cb_id)));
     MATH((llk_math_custom_mm_init<transpose, split_acc, dense_packing>(in0_cb_id, in1_cb_id, ct_dim)));
 
-    PACK((llk_pack_dest_init<DST_ACCUM_MODE, false>()));
-    PACK((llk_pack_hw_configure<DST_ACCUM_MODE>(out_cb_id)));
+    PACK((llk_pack_dest_init<fp32_dest_acc_en, false>()));
+    PACK((llk_pack_hw_configure<fp32_dest_acc_en>(out_cb_id)));
     PACK((llk_pack_init<false, false>(out_cb_id)));
     if constexpr (dense_packing) {
         PACK((cfg_reg_rmw_tensix<PCK0_ADDR_CTRL_ZW_REG_0_Wstride_RMW>(
---
+++ models/demos/deepseek_v3_b1/micro_ops/eltwise_add/eltwise_add_kernel.cpp
@@ -0,0 +1,44 @@
+// SPDX-FileCopyrightText: © 2026 Tenstorrent AI ULC
+// SPDX-License-Identifier: Apache-2.0
+
+// Eltwise Add kernel for MoE fused operation
+// Adds fused_add tensor (indexed by sender_index) to down_proj output
+// Uses CB read pointer update instead of copy for efficiency
+
+#include "../../unified_kernels/eltwise_add.hpp"
+#include "../../unified_kernels/kernel_utils.hpp"
+
+void kernel_main() {
+#if defined(COMPILE_FOR_NCRISC)
+    // Setup sharded buffers - signal that input data is ready
+    constexpr uint32_t cb_in0 = get_named_compile_time_arg_val("add_cb_in0");
+    constexpr uint32_t cb_in1 = get_named_compile_time_arg_val("add_cb_in1");
+    constexpr uint32_t cb_in0_num_tiles =
+        get_named_compile_time_arg_val("add_cb_in0_wait_tiles");  // pages in 32x32-view CB
+    constexpr uint32_t cb_in1_wait_tiles =
+        get_named_compile_time_arg_val("add_cb_in1_wait_tiles");  // pages in 32x32-view CB
+
+    unified_kernels::setup_sharded_buffer(cb_in0, cb_in0_num_tiles);
+    unified_kernels::setup_sharded_buffer(cb_in1, cb_in1_wait_tiles);
+
+    using CTArgs = deepseek_b1_ops::EltwiseAdd::ReaderCTArgs;
+#elif defined(COMPILE_FOR_BRISC)
+    using CTArgs = deepseek_b1_ops::EltwiseAdd::WriterCTArgs;
+#elif defined(COMPILE_FOR_TRISC)
+    // TRISC: Element-wise add with CB read pointer update for indexed access
+    using CTArgs = deepseek_b1_ops::EltwiseAdd::ComputeCTArgs<
+        get_named_compile_time_arg_val("add_cb_in0"),
+        get_named_compile_time_arg_val("add_cb_in1"),
+        get_named_compile_time_arg_val("add_cb_out"),
+        get_named_compile_time_arg_val("add_num_tiles"),
+        get_named_compile_time_arg_val("add_cb_in0"),  // cb_in0_wait = cb_in0 (same CB)
+        get_named_compile_time_arg_val("add_cb_in0_wait_tiles"),
+        get_named_compile_time_arg_val("add_cb_in1_wait_tiles"),
+        get_named_compile_time_arg_val("add_sender_index"),
+        get_named_compile_time_arg_val("add_slice_size_bytes")>;
+#endif
+
+    // Execute
+    deepseek_b1_ops::EltwiseAdd::Op<CTArgs, true> add_op;
+    add_op();
+}
