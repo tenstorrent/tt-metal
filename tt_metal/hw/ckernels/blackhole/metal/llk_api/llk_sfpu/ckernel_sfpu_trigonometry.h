7197c142d28db830751d9edc22b3ab7922614882
Jason Davies <jason@jasondavies.com>
2026-02-09 13:43:39 +0000

    Checkpoint optimised cos.

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_trigonometry.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_trigonometry.h
@@ -21,6 +21,9 @@ static const float PI_2 = 1.5707964f;
 static const float PI_4 = 0.7853982f;
 static const float FRAC_1_PI = 0.31830987f;
 
+static const float PI_2_HI = 0x1.921fb4p+0f;
+static const float PI_2_LO = 0x1.4442d2p-24f;
+
 static sfpi_inline vFloat sfpu_tan_large(vFloat x) {
     const vFloat r = 4.0f * sfpi::abs(x) - 5.0f;
     const vFloat y =
@@ -107,8 +110,13 @@ sfpi_inline vFloat sfpu_sinpi<false>(vFloat x) {
            ((((0x1.406628p-4f * xx - 0x9.93f86p-4f) * xx + 0x2.8cd64p+0f) * xx - 0x5.2aef6p+0f) * xx + 0x3.243f6cp+0f);
 }
 
-template <bool APPROXIMATION_MODE, int ITERATIONS>
-inline void calculate_sine() {
+template <bool IS_COSINE, bool APPROXIMATION_MODE, int ITERATIONS>
+inline void calculate_sincos_internal() {
+    // 1. Reduce argument using a four-stage Cody-Waite reduction to the interval [-pi/2, pi/2].
+    // 2. In the case of cos(x), use cos(x) = sin(pi / 2 - abs(x)), using a two-part subtraction to avoid precision
+    // loss.
+    // 3. Now use a polynomial sin(x) = x + x^3 (C0 + x^2 (C1 + x^2 (C2 + x^2 C3))) on [0, pi/2].
+
     // Constants for four-stage Cody-Waite reduction with -PI = P0 + P1 + vConstFloatPrgm0 + vConstFloatPrgm1
     const float P0 = -0x1.92p+1f;               // representable as bf16
     const float P1 = -0x1.fbp-11f;              // representable as fp16
@@ -135,13 +143,103 @@ inline void calculate_sine() {
         // First, j = v * (1 / PI) + 1.5*2^23 shifts the mantissa bits to give round-to-nearest-even.
         vFloat j;
         j.get() = __builtin_rvtt_bh_sfpmad(v.get(), inv_pi, rounding_bias.get(), SFPMAD_MOD1_OFFSET_NONE);
+
         // At this point, the mantissa bits of j contain the integer.
         // Store for later; the LSB determines the sign of the result.
         vInt q = reinterpret<vInt>(j);
         // Shift mantissa bits back; j is now round(v / PI) in fp32.
-        j -= rounding_bias;
+        j = j - rounding_bias;
 
         // Four-stage Cody-Waite reduction; a = v + j * -PI.
+        // P0 representable as bf16; generates a single SFPLOADI, filling NOP slot from previous SFPADDI.
+        vFloat a = v + j * P0;
+        // P1 representable as fp16; generates a single SFPLOADI, filling NOP slot from previous SFPMAD.
+        a = a + j * P1;
+        a = a + j * sfpi::vConstFloatPrgm0;
+        a = a + j * sfpi::vConstFloatPrgm1;
+
+        // If we're computing cosine, we want cos(a) = sin(pi/2 - abs(a)).
+        // Use two-part subtraction to avoid precision loss.
+        if constexpr (IS_COSINE) {
+            a = sfpi::abs(a);
+            a = PI_2 - a;
+            a = a + PI_2_LO;
+        }
+
+        q <<= 31;
+        vFloat s = a * a;
+        a = reinterpret<vFloat>(reinterpret<vInt>(a) ^ q);
+        vFloat r = C3 * s + C2;
+        r = r * s + C1;
+        vFloat c = a * s;
+        r = r * s + C0;
+        r = r * c + a;
+
+        dst_reg[0] = r;
+        dst_reg++;
+    }
+}
+
+template <bool APPROXIMATION_MODE, int ITERATIONS>
+inline void calculate_sine() {
+    calculate_sincos_internal<false, APPROXIMATION_MODE, ITERATIONS>();
+}
+
+template <bool APPROXIMATION_MODE, int ITERATIONS>
+inline void calculate_cosine() {
+    // 1. Reduce argument using a four-stage Cody-Waite reduction to the interval [-pi/2, pi/2].
+    // 2. In the case of cos(x), use cos(x) = sin(pi / 2 + x).
+    // 3. Now use a polynomial sin(x) = x + x^3 (C0 + x^2 (C1 + x^2 (C2 + x^2 C3))) on [0, pi/2].
+
+    // Constants for four-stage Cody-Waite reduction with -PI/2 = P0 + P1 + vConstFloatPrgm0 + vConstFloatPrgm1
+    const float P0 = -0x1.92p+0f;               // representable as bf16
+    const float P1 = -0x1.fbp-12f;              // representable as fp16
+    sfpi::vConstFloatPrgm0 = -0x1.51p-22f;      // requires fp32
+    sfpi::vConstFloatPrgm1 = -0x1.0b4612p-34f;  // requires fp32
+
+    sfpi::vConstFloatPrgm2 = FRAC_1_PI;
+
+    // Constants for sin(a) = a + a^3 (C0 + a^2 (C1 + a^2 (C2 + a^2 C3))) on [0, pi/2].
+    vFloat C3 = 0x1.5dc908p-19f;
+    vFloat C2 = -0x1.9f70fp-13f;
+    vFloat C1 = 0x1.110edap-7f;
+    vFloat C0 = -0x1.55554cp-3f;
+
+    const float ROUNDING_BIAS = 12582912.0f;
+    const float NEG_ROUNDING_BIAS = -12582912.0f;
+
+    for (int d = 0; d < ITERATIONS; d++) {
+        vFloat v = dst_reg[0];
+
+        // Workaround for SFPI's insistence on generating SFPADDI+SFPMUL instead of SFPLOADI+SFPMAD here.
+        vFloat half;
+        half.get() = __builtin_rvtt_sfpxloadi(0, 0x3f00);  // 0.5
+        __rvtt_vec_t inv_pi = __builtin_rvtt_sfpreadlreg(sfpi::vConstFloatPrgm2.get());
+        __rvtt_vec_t one = __builtin_rvtt_sfpreadlreg(sfpi::vConst1.get());
+        __rvtt_vec_t neg_one = __builtin_rvtt_sfpreadlreg(sfpi::vConstNeg1.get());
+
+        // Compute j = round(v / PI).
+        // First, j = v * (2 / PI) + 1.5*2^23 shifts the mantissa bits to give round-to-nearest-even.
+        vFloat j;
+        j.get() = __builtin_rvtt_bh_sfpmad(v.get(), inv_pi, half.get(), SFPMAD_MOD1_OFFSET_NONE);
+
+        // vFloat rounding_bias;
+        // rounding_bias.get() = __builtin_rvtt_sfpxloadi(0, 0x4b40);  // 1.5*2^23
+        // j.get() = __builtin_rvtt_bh_sfpmad(v.get(), one, rounding_bias.get(), SFPMAD_MOD1_OFFSET_NONE);
+
+        j = j + ROUNDING_BIAS;
+
+        // At this point, the mantissa bits of j contain the integer.
+        // Store for later; the LSB determines the sign of the result.
+        vInt q = reinterpret<vInt>(j);
+
+        j = j + NEG_ROUNDING_BIAS;
+
+        vFloat two;
+        two.get() = __builtin_rvtt_sfpxloadi(0, 0x4000);  // 2.0
+        j.get() = __builtin_rvtt_bh_sfpmad(j.get(), two.get(), neg_one, SFPMAD_MOD1_OFFSET_NONE);
+
+        // Four-stage Cody-Waite reduction; a = v + j * -PI / 2.
         // P0 representable as bf16; generates a single SFPLOADI, filling NOP slot from previous SFPADDI.
         vFloat a = v + j * P0;
         // P1 representable as fp16; generates a single SFPLOADI, filling NOP slot from previous SFPMAD.
@@ -159,22 +257,6 @@ inline void calculate_sine() {
         r = r * c + a;
 
         dst_reg[0] = r;
-        dst_reg++;
-    }
-}
-
-template <bool APPROXIMATION_MODE, int ITERATIONS>
-inline void calculate_cosine() {
-    // SFPU microcode
-    for (int d = 0; d < ITERATIONS; d++) {
-        vFloat v = dst_reg[0] * FRAC_1_PI + 0.5f;
-        vInt whole_v = float_to_int16(v, 0);
-        v -= int32_to_float(whole_v, 0);
-        v = sfpu_sinpi<APPROXIMATION_MODE>(v);
-
-        v_if(whole_v & 1) { v = -v; }
-        v_endif;
-        dst_reg[0] = v;
         dst_reg++;
     }
 }
