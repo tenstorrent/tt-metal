0cb954185058ad9c73844b94ce597e6c96e17a7c
ssundaramTT <ssundaram@tenstorrent.com>
2026-02-09 19:00:23 +0000

    updated golden

--- models/demos/deepseek_v3_b1/micro_ops/tilize_8x32/kernels/tilize_8x32_kernel.cpp
+++ models/demos/deepseek_v3_b1/micro_ops/tilize_8x32/kernels/tilize_8x32_kernel.cpp
@@ -12,23 +12,12 @@ void kernel_main() {
 #if defined(COMPILE_FOR_TRISC)
     constexpr uint32_t in_cb = get_named_compile_time_arg_val("in_cb");
     constexpr uint32_t out_cb = get_named_compile_time_arg_val("out_cb");
-    constexpr uint32_t num_blocks = get_named_compile_time_arg_val("num_blocks");
     constexpr uint32_t block_size = get_named_compile_time_arg_val("block_size");
 
     compute_kernel_hw_startup(in_cb, out_cb);
     tilize_init(in_cb, block_size, out_cb);
 
-    for (uint32_t i = 0; i < num_blocks; i++) {
-        // DPRINT << "BEFORE WAIT FRONT" << ENDL();
-        // cb_wait_front(in_cb, block_size);
-        // DPRINT << "AFTER WAIT FRONT" << ENDL();
-        // cb_reserve_back(out_cb, block_size);
-        // DPRINT << "AFTER RESERVE BACK" << ENDL();
-        tilize_block(in_cb, block_size, out_cb);
-        // DPRINT << "AFTER TILIZE BLOCK" << ENDL();
-        // cb_push_back(out_cb, block_size);
-        // DPRINT << "AFTER PUSH BACK" << ENDL();
-        cb_pop_front(in_cb, block_size);
-    }
+    tilize_block(in_cb, block_size, out_cb);
+    cb_pop_front(in_cb, block_size);
 #endif
 }
