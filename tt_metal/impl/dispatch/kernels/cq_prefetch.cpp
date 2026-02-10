7ba1db7de90852c5440f0ffba32c36f659964230
John Bauman <jbauman@tenstorrent.com>
2026-02-10 22:42:42 +0000

    cleanup

--- tt_metal/impl/dispatch/kernels/cq_prefetch.cpp
+++ tt_metal/impl/dispatch/kernels/cq_prefetch.cpp
@@ -1953,32 +1953,33 @@ uint32_t process_relay_linear_packed_h_cmd(uint32_t cmd_ptr, uint32_t& downstrea
     CQPrefetchRelayLinearPackedSubCmd tt_l1_ptr* sub_cmd = (CQPrefetchRelayLinearPackedSubCmd tt_l1_ptr*)(l1_cache);
     uint64_t current_addr = sub_cmd->addr;
     uint32_t current_length = sub_cmd->length;
-    // Total amount to read into the scratch db half from all sub-commands.
-    uint32_t amt_to_read =
+    // Total amount to read into the scratch db half from all sub-commands (that fit)
+    uint32_t amt_to_read_to_scratch_db =
         (scratch_db_half_size - start_offset > total_length) ? total_length : scratch_db_half_size - start_offset;
-    uint32_t amt_read = 0;
 
-    auto read_sub_cmd = [&](uint32_t start_offset) {
-        uint32_t amt_to_read_sub_cmd = (scratch_db_half_size - start_offset - amt_read > current_length)
-                                    ? current_length
-                                    : scratch_db_half_size - start_offset - amt_read;
-        noc_read_64bit_any_len<true>(noc_xy_addr, current_addr, scratch_read_addr, amt_to_read_sub_cmd);
-        scratch_read_addr += amt_to_read_sub_cmd;
-        amt_read += amt_to_read_sub_cmd;
-        current_addr += amt_to_read_sub_cmd;
-        current_length -= amt_to_read_sub_cmd;
+    uint32_t amt_read = amt_to_read_to_scratch_db;
 
-        // If we've consumed the current sub-command, move to the next one
-        if (current_length == 0) {
-            sub_cmd++;
-            current_addr = sub_cmd->addr;
-            current_length = sub_cmd->length;
+    auto fill_scratch_db = [&]() {
+        while (amt_to_read_to_scratch_db > 0) {
+            uint32_t amt_to_read_sub_cmd =
+                amt_to_read_to_scratch_db > current_length ? current_length : amt_to_read_to_scratch_db;
+            noc_read_64bit_any_len<true>(noc_xy_addr, current_addr, scratch_read_addr, amt_to_read_sub_cmd);
+            scratch_read_addr += amt_to_read_sub_cmd;
+
+            amt_to_read_to_scratch_db -= amt_to_read_sub_cmd;
+            current_addr += amt_to_read_sub_cmd;
+            current_length -= amt_to_read_sub_cmd;
+
+            // If we've consumed the current sub-command, move to the next one
+            if (current_length == 0) {
+                sub_cmd++;
+                current_addr = sub_cmd->addr;
+                current_length = sub_cmd->length;
+            }
         }
     };
 
-    while (amt_read < amt_to_read) {
-        read_sub_cmd(start_offset);
-    }
+    fill_scratch_db();
     noc_async_read_barrier();
 
     // Second step - read into DB[x], write from DB[x], toggle x, iterate
@@ -2002,11 +2003,9 @@ uint32_t process_relay_linear_packed_h_cmd(uint32_t cmd_ptr, uint32_t& downstrea
             std::swap(scratch_read_start_addr, scratch_write_start_addr);
 
             uint32_t amt_to_write = amt_read;
-            amt_read = 0;
-            amt_to_read = (scratch_db_half_size > read_length) ? read_length : scratch_db_half_size;
-            while (amt_read < amt_to_read) {
-                read_sub_cmd(0);
-            }
+            amt_to_read_to_scratch_db = (scratch_db_half_size > read_length) ? read_length : scratch_db_half_size;
+            amt_read = amt_to_read_to_scratch_db;
+            fill_scratch_db();
 
             // Third step - write from DB. amt_to_write is greater than a page, so we don't need to test for nonzero.
             relay_linear_to_downstream<false>(downstream_data_ptr, scratch_write_addr, amt_to_write);
