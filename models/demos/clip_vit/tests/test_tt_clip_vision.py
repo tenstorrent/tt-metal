35068f35552409ce38c433d6d3aaa333d8cf14c1
ajumpa <ajumpa@comcast.net>
2026-02-10 20:32:31 +0000

    vision encoder optimzied, move on to text encoder

--- models/demos/clip_vit/tests/test_tt_clip_vision.py
+++ models/demos/clip_vit/tests/test_tt_clip_vision.py
@@ -16,12 +16,10 @@ def test_clip_vision_embeddings(torch_model, vision_config, pixel_values, device
 
     ttnn_embedding = TtCLIPVisionEmbeddings(vision_config, torch_embedding, device)
 
-    ttnn_pixel_values = ttnn.from_torch(pixel_values, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
-
-    ttnn_pixel_values = ttnn.permute(ttnn_pixel_values, [0, 2, 3, 1])
+    ttnn_pixel_values = ttnn.from_torch(pixel_values, dtype=ttnn.bfloat8_b, device=device, layout=ttnn.TILE_LAYOUT)
 
     with torch.no_grad():
-        torch_output = torch_embedding(pixel_values).to(torch.bfloat16)
+        torch_output = torch_embedding(pixel_values)
 
     ttnn_output = ttnn_embedding(ttnn_pixel_values)
     ttnn_output_torch = ttnn.to_torch(ttnn_output)
@@ -35,12 +33,12 @@ def test_clip_vision_mlp(torch_model, vision_config, pixel_values, device):
     ttnn_mlp = TtCLIPVisionMLP(vision_config, torch_mlp, device)
 
     with torch.no_grad():
-        pixel_values = torch_embedding(pixel_values).to(torch.bfloat16)
+        pixel_values = torch_embedding(pixel_values)
 
-    ttnn_pixel_values = ttnn.from_torch(pixel_values, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+    ttnn_pixel_values = ttnn.from_torch(pixel_values, dtype=ttnn.bfloat8_b, device=device, layout=ttnn.TILE_LAYOUT)
 
     with torch.no_grad():
-        torch_output = torch_mlp(pixel_values.to(torch.bfloat16))
+        torch_output = torch_mlp(pixel_values)
 
     ttnn_output = ttnn_mlp(ttnn_pixel_values)
     ttnn_output_torch = ttnn.to_torch(ttnn_output)
@@ -53,9 +51,9 @@ def test_clip_vision_attention(torch_model, vision_config, pixel_values, device)
     torch_attention = torch_model.encoder.layers[0].self_attn
     ttnn_attention = TtCLIPVisionAttention(vision_config, torch_attention, device)
     with torch.no_grad():
-        pixel_values = torch_embedding(pixel_values).to(torch.bfloat16)
+        pixel_values = torch_embedding(pixel_values)
 
-    ttnn_pixel_values = ttnn.from_torch(pixel_values, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
+    ttnn_pixel_values = ttnn.from_torch(pixel_values, dtype=ttnn.bfloat8_b, device=device, layout=ttnn.TILE_LAYOUT)
 
     with torch.no_grad():
         torch_output = torch_attention(pixel_values)[0]
@@ -70,12 +68,12 @@ def test_clip_vision_encoder_layer(torch_model, vision_config, pixel_values, dev
     torch_embedding = torch_model.embeddings
     torch_encoder = torch_model.encoder.layers[0]
     with torch.no_grad():
-        pixel_embeddings = torch_embedding(pixel_values).to(torch.bfloat16)
+        pixel_embeddings = torch_embedding(pixel_values)
 
     ttnn_encoder = TtCLIPVisionEncoderLayer(vision_config, torch_encoder, device)
 
     ttnn_pixel_embeddings = ttnn.from_torch(
-        pixel_embeddings, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT
+        pixel_embeddings, dtype=ttnn.bfloat8_b, device=device, layout=ttnn.TILE_LAYOUT
     )
 
     with torch.no_grad():
@@ -90,14 +88,12 @@ def test_clip_vision_encoder_layer(torch_model, vision_config, pixel_values, dev
 def test_clip_vision_encoder(torch_vision_encoder, vision_config, pixel_values, device):
     ttnn_vision_encoder = TtCLIPVisionModel(vision_config, torch_vision_encoder, device)
 
-    ttnn_pixel_values = ttnn.from_torch(pixel_values, dtype=ttnn.bfloat16, device=device, layout=ttnn.TILE_LAYOUT)
-
-    ttnn_pixel_values = ttnn.permute(ttnn_pixel_values, [0, 2, 3, 1])
+    ttnn_pixel_values = ttnn.from_torch(pixel_values, dtype=ttnn.bfloat8_b, device=device, layout=ttnn.TILE_LAYOUT)
 
     with torch.no_grad():
         torch_outputs = torch_vision_encoder(pixel_values=pixel_values)
-        torch_last_hidden_state = torch_outputs.last_hidden_state.to(torch.bfloat16)
-        torch_pooler_output = torch_outputs.pooler_output.to(torch.bfloat16)
+        torch_last_hidden_state = torch_outputs.last_hidden_state
+        torch_pooler_output = torch_outputs.pooler_output
 
     ttnn_pooler_output = ttnn_vision_encoder(pixel_values=ttnn_pixel_values)
 
