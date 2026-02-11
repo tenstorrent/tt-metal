c16dde95ab74dbdec24dfbf9e0b788aa319f59ad
Stephen Osborne <sosborne@tenstorrent.com>
2026-02-11 14:42:36 -0800

    Update SDPA to use the new fast approximate exponential function (#36795)
    ### Ticket
    [27912](https://github.com/tenstorrent/tt-metal/issues/27912).
    
    ### What's changed
    Update SDPA (prefill and decode) to use the [new fast approximate
    exponential function](https://github.com/tenstorrent/tt-llk/pull/1190).
    On WH T3K `sub_exp_block_bcast_cols_inplace` is 1.3x faster with the new
    function.
    
    ### Checklist
    
    - [x] [![All post-commit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml/badge.svg?branch=sosborne/exp-pack-relu)](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml?query=branch:sosborne/exp-pack-relu)
    - [x] [![Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml/badge.svg?branch=sosborne/exp-pack-relu)](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml?query=branch:sosborne/exp-pack-relu)
    [Same failures as
    main](https://github.com/tenstorrent/tt-metal/actions/runs/21836396876)
    - [ ]
    [![cpp-unit-tests](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml/badge.svg?branch=sosborne/exp-pack-relu)](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml?query=branch:sosborne/exp-pack-relu)
    [Failures are infra
    issues](https://github.com/tenstorrent/tt-metal/actions/runs/21836506277)
    
    
    #### Model tests
    
    - [x] [Single card
    frequent](https://github.com/tenstorrent/tt-metal/actions/runs/21873518821)
    Same failures as main plus one unrelated infra issue plus some main
    tests now passing
    - [x] [Blackhole multi-card
    demo](https://github.com/tenstorrent/tt-metal/actions/runs/21847311344)
    Passes
    - [x] [T3K integration
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/21847309682)
    1 failure, same as main
    - [x] [T3K demo
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/21873442204)
    Same failures as main plus some infra failures plus some main failures
    now passing
    - [x] [T3K perf
    tests](https://github.com/tenstorrent/tt-metal/actions/runs/21890540821)
    Same failures as main
    - [x] [Galaxy
    integration](https://github.com/tenstorrent/tt-metal/actions/runs/21890175452)
    Same failures as main
    - [x] [Galaxy
    demo](https://github.com/tenstorrent/tt-metal/actions/runs/21890178397)
    Same failures as main

--- tt_metal/hw/inc/api/compute/eltwise_unary/exp.h
+++ tt_metal/hw/inc/api/compute/eltwise_unary/exp.h
@@ -13,15 +13,37 @@
 namespace ckernel {
 
 /**
+ * Controls whether the fast approximate exponential clamps very negative inputs.
+ *
+ * ClampToNegative (default): Inputs below ~-88.5 are clamped to -88.5. Safer but slightly slower.
+ * None: No input clamping. Faster, but inputs below ~-88.5 will produce incorrect outputs. They
+ *     will be guaranteed to be negative, so consider enabling packer ReLU when using this mode.
+ */
+enum class InputClamping : uint8_t {
+    ClampToNegative = 1,
+    None = 0,
+};
+
+/**
  * Please refer to documentation for any_init.
  *
- * Template scale parameter is used when aprox and fast_and_approx are true and exp_tile is called with scale_en set to
+ * Template scale parameter is used when approx and fast_and_approx are true and exp_tile is called with scale_en set to
  * true.
  *
  */
-template <bool approx = false, bool fast_and_approx = true, uint32_t scale = 0x3F800000>
+template <
+    bool approx = false,
+    bool fast_and_approx = true,
+    uint32_t scale = 0x3F800000,
+    InputClamping input_clamping = InputClamping::ClampToNegative>
 ALWI void exp_tile_init() {
-    MATH(SFPU_TEMPLATE_INIT_KERNEL(exponential, sfpu::exp_init, approx, fast_and_approx, scale));
+    MATH(SFPU_TEMPLATE_INIT_KERNEL(
+        exponential,
+        sfpu::exp_init,
+        approx,
+        fast_and_approx,
+        scale,
+        (input_clamping == InputClamping::ClampToNegative)));
 }
 
 // clang-format off
@@ -39,6 +61,7 @@ ALWI void exp_tile_init() {
  * | fast_and_approx         | If approx is true, enable fast approximation.                  | bool     | true, false      | true   |
  * | scale_en                | Enable input scaling by a constant factor in approximate or non-approximate mode | bool     | true, false      | false   |
  * | skip_positive_check     | Skip large-positive input check                                | bool     | true, false      | false   |
+ * | input_clamping          | If approx && fast_and_approx, controls whether very negative inputs are clamped to prevent incorrect outputs | InputClamping | ClampToNegative, None | ClampToNegative |
  * | iterations              | Number of iterations over 32-SFPU lanes to run                 | int      | Positive integer | 8       |
  *
  * | Argument    | Description                                                                | Type     | Valid Range                                           | Required |
@@ -53,6 +76,7 @@ template <
     bool fast_and_approx = true,
     bool scale_en = false,
     bool skip_positive_check = false,
+    InputClamping input_clamping = InputClamping::ClampToNegative,
     int iterations = 8>
 ALWI void exp_tile(uint32_t idst, int vector_mode = (int)VectorMode::RC, uint16_t scale = p_sfpu::kCONST_1_FP16B) {
     MATH(SFPU_TEMPLATE_PARAMS_KERNEL_FN(
@@ -62,6 +86,7 @@ ALWI void exp_tile(uint32_t idst, int vector_mode = (int)VectorMode::RC, uint16_
         DST_ACCUM_MODE,
         scale_en,
         skip_positive_check,
+        (input_clamping == InputClamping::ClampToNegative),
         iterations,
         idst,
         vector_mode,
