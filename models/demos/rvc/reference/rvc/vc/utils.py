1d940103362beed0f4da3fb45f268b57f47d6a25
Mert Kurttutan <kurttutan.mert@gmail.com>
2026-02-11 20:17:45 +0300

    sync with rvc-nano

--- models/demos/rvc/reference/rvc/vc/utils.py
+++ models/demos/rvc/reference/rvc/vc/utils.py
@@ -1,7 +1,7 @@
+import json
 import os
-from typing import Any
 
-import torch
+from safetensors.torch import load_file
 
 from .hubert import (
     HubertModel,
@@ -19,42 +19,33 @@ def get_maybe_sharded_checkpoint_filename(filename: str, suffix: str) -> str:
         return filename
 
 
-def setup_task(state_dict: dict[str, Any]):
-    return HubertPretrainingTask(HubertPretrainingConfig(), state_dict)
+def setup_task():
+    return HubertPretrainingTask(HubertPretrainingConfig())
 
 
 def load_model_ensemble_and_task(
-    filename,
-    strict=True,
+    model_path,
+    model_cfg_path,
     suffix="",
 ):
     cfg = None
-    orig_filename = filename
-    filename = get_maybe_sharded_checkpoint_filename(orig_filename, suffix)
+    task = setup_task()
 
-    if not os.path.exists(filename):
-        raise IOError("Model file not found: {}".format(filename))
-    with open(filename, "rb") as f:
-        state = torch.load(f, map_location=torch.device("cpu"), weights_only=False)
-
-    if state.get("cfg", None) is not None:
-        cfg = state["cfg"]
-    else:
-        raise RuntimeError(f"No cfg exist in state keys = {state.keys()}")
-
-    task = setup_task(state["task_state"])
-
+    with open(model_cfg_path) as f:
+        cfg = json.load(f)
     model = HubertModel.build_model(cfg["model"], task)
+    hubert_state_safetensors = load_file(model_path)
 
-    model.load_state_dict(state["model"], strict=strict)
+    model.load_state_dict(hubert_state_safetensors, strict=True)
     return model
 
 
-def load_hubert(config, hubert_path: str):
+def load_hubert(config, hubert_path: str, hubert_cfg_path: str):
     hubert_model = load_model_ensemble_and_task(
         hubert_path,
+        hubert_cfg_path,
         suffix="",
     )
     hubert_model = hubert_model.to(config.device)
-    hubert_model = hubert_model.half() if config.is_half else hubert_model.float()
+    hubert_model = hubert_model.float()
     return hubert_model.eval()
