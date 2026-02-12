084f9fe755b1bc23ce995cd493e74e21025fb93c
mert-kurttutan <kurttutan.mert@gmail.com>
2026-02-12 13:41:30 +0000

    RVC Demo: Add license notice

--- models/demos/rvc/reference/vc/hubert.py
+++ models/demos/rvc/reference/vc/hubert.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+
 import math
 from collections.abc import Callable
 from dataclasses import dataclass, field
