42e916ecd8fff1783ed54b24f28ba9c73fc831b3
Bazyli Klockiewicz <bklockiewicz@tenstorrent.com>
2026-02-12 15:47:22 +0000

    move chunk_width computation earlier in reduction and writer kernels

--- ttnn/cpp/ttnn/operations/experimental/ccl/strided_reduce_scatter_async/device/kernels/minimal_ring_reduction.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/strided_reduce_scatter_async/device/kernels/minimal_ring_reduction.cpp
@@ -80,6 +80,8 @@ void MAIN {
 
             for (uint32_t chunk_idx = 0; chunk_idx < chunks_per_mm_N_block; chunk_idx++) {
                 DPRINT << "chunk_idx: " << chunk_idx << " started" << ENDL();
+                uint32_t effective_chunk_width_in_tiles =
+                    get_effective_chunk_width_in_tiles(chunk_idx, chunk_width_in_tiles, slice_Wt);
 
                 for (uint32_t i = 1; i < ring_size; i++) {
                     DPRINT << "************************************************" << ENDL();
@@ -92,8 +94,6 @@ void MAIN {
                         uint32_t first_tile_row_in_mm_M_block = 0;
                         uint32_t first_chunk_col_in_tiles = 0;
                         uint32_t first_mm_core_idx = 0;
-                        uint32_t effective_chunk_width_in_tiles =
-                            get_effective_chunk_width_in_tiles(chunk_idx, chunk_width_in_tiles, slice_Wt);
                         uint32_t effective_chunk_piece_size = mm_block_ht * effective_chunk_width_in_tiles;
                         get_next_tile_coordinates(
                             first_tile_row_in_mm_M_block,
--- ttnn/cpp/ttnn/operations/experimental/ccl/strided_reduce_scatter_async/device/kernels/minimal_ring_strided_reduce_scatter_async_writer.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/strided_reduce_scatter_async/device/kernels/minimal_ring_strided_reduce_scatter_async_writer.cpp
@@ -278,6 +278,8 @@ void kernel_main() {
 
                 for (uint32_t chunk_idx = 0; chunk_idx < chunks_per_mm_N_block; chunk_idx++) {
                     DPRINT << "chunk_idx: " << chunk_idx << " started" << ENDL();
+                    uint32_t effective_chunk_width_in_tiles =
+                        get_effective_chunk_width_in_tiles(chunk_idx, chunk_width_in_tiles, slice_Wt);
                     int32_t slice_idx = direction ? my_chip_id - 1 : my_chip_id + 1;
 
                     for (uint32_t i = 0; i < ring_size; i++) {
@@ -304,8 +306,6 @@ void kernel_main() {
                             uint32_t first_mm_core_idx = 0;
                             uint32_t global_tile_idx;
                             uint32_t slice_tile_idx;
-                            uint32_t effective_chunk_width_in_tiles =
-                                get_effective_chunk_width_in_tiles(chunk_idx, chunk_width_in_tiles, slice_Wt);
                             uint32_t effective_chunk_piece_size = mm_block_ht * effective_chunk_width_in_tiles;
                             get_next_tile_coordinates(
                                 first_tile_row_in_mm_M_block,
