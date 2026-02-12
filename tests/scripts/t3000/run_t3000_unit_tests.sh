5e793255a6e38842614f0c296ba8712dfa762ba8
Colman Glagovich <cglagovich@tenstorrent.com>
2026-02-12 18:01:27 +0000

    Refactor wan transformer tests. Remove model caching test

--- tests/scripts/t3000/run_t3000_unit_tests.sh
+++ tests/scripts/t3000/run_t3000_unit_tests.sh
@@ -643,7 +643,7 @@ run_t3000_tt_dit_tests() {
   pytest models/tt_dit/tests/models/wan2_2/test_vae_wan2_1.py::test_wan_decoder[wormhole_b0-device_params0-2x4_h1_w0-check_output-fake_weights-0-1-_1f-480p] ; fail+=$?
 
   #DITs Wan2.2 Transformer
-  DIT_UNIT_TEST=1 pytest models/tt_dit/tests/models/wan2_2/test_transformer_wan.py::test_wan_transformer_model[wormhole_b0-no_load_cache-short_seq-2x4sp0tp1-True] ; fail+=$?
+  DIT_UNIT_TEST=1 pytest models/tt_dit/tests/models/wan2_2/test_transformer_wan.py::test_wan_transformer_model[wormhole_b0-short_seq-2x4sp0tp1-True] ; fail+=$?
 
   #Mochi Transformer
   DIT_UNIT_TEST=1 pytest models/tt_dit/tests/models/mochi/test_transformer_mochi.py::test_mochi_transformer_model[wormhole_b0-device_params0-no_load_cache-no_test_attention_mask-short_seq-2x4sp0tp1-True] ; fail+=$?
