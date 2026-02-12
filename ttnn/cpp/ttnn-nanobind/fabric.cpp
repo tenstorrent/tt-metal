c9d89e8393c56d6407088b0a010e630a3f9aba6d
Abhishek Agarwal <aagarwal@tenstorrent.com>
2026-02-12 21:43:07 +0000

    patch for galaxy

--- ttnn/cpp/ttnn-nanobind/fabric.cpp
+++ ttnn/cpp/ttnn-nanobind/fabric.cpp
@@ -56,6 +56,7 @@ void bind_fabric_api(nb::module_& mod) {
         .value("FABRIC_1D_RING", tt::tt_fabric::FabricConfig::FABRIC_1D_RING)
         .value("FABRIC_1D_NEIGHBOR_EXCHANGE", tt::tt_fabric::FabricConfig::FABRIC_1D_NEIGHBOR_EXCHANGE)
         .value("FABRIC_2D", tt::tt_fabric::FabricConfig::FABRIC_2D)
+        .value("FABRIC_2D_TORUS_XY", tt::tt_fabric::FabricConfig::FABRIC_2D_TORUS_XY)
         .value(
             "CUSTOM", tt::tt_fabric::FabricConfig::CUSTOM);  // DISABLED = 0, FABRIC_1D = 1, FABRIC_2D = 2, CUSTOM = 4
 
