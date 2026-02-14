898dc27381f0fa44c201b1fd5958d3400d20a298
Mateusz Nowak <mateusznowak@tenstorrent.com>
2026-02-14 23:19:17 +0000

    Allow vector_init to preallocate more if needed

--- ttnn/api/ttnn/common/vector_init.hpp
+++ ttnn/api/ttnn/common/vector_init.hpp
@@ -7,12 +7,20 @@
 #include <vector>
 #include <type_traits>
 
-template <typename T, typename... Vs>
+template <typename T, size_t N, typename... Vs>
 std::vector<T> vector_init(Vs&&... init_values)
-    requires requires { std::disjunction_v<std::is_same<T, Vs>...>; }
+    requires requires {
+        std::disjunction_v<std::is_same<T, Vs>...>;
+        requires N >= sizeof...(Vs);
+    }
 {
     std::vector<T> vec;
-    vec.reserve(sizeof...(Vs));
+    vec.reserve(N);
     (vec.emplace_back(std::forward<Vs>(init_values)), ...);
     return vec;
+}
+
+template <typename T, typename... Vs>
+std::vector<T> vector_init(Vs&&... init_values) {
+    return vector_init<T, sizeof...(Vs), Vs...>(std::forward<Vs>(init_values)...);
 }
