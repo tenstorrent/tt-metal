37ba679521ebf4fe88b686cfca0d8af5b1f3e9de
Jonathan Su <jonathansu@tenstorrent.com>
2026-02-14 00:21:21 +0000

    remove invoke/register_operation and default's for primitives in struct

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_minimal_matmul_async/all_gather_minimal_matmul_async.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_minimal_matmul_async/all_gather_minimal_matmul_async.cpp
@@ -5,19 +5,18 @@
 #include "device/all_gather_minimal_matmul_async_device_operation.hpp"
 #include <tt-metalium/math.hpp>
 #include <tt-metalium/tt_metal.hpp>
-#
 #include "ttnn/common/constants.hpp"
 #include "ttnn/operations/core/compute_kernel/compute_kernel_config.hpp"
 
 using namespace tt::tt_metal;
 
-namespace ttnn::operations::experimental::all_gather_minimal_matmul_async {
+namespace ttnn {
 
-ttnn::Tensor ExecuteAllGatherMinimalMatmulAsync::invoke(
+ttnn::Tensor all_gather_minimal_matmul_async(
     const ttnn::Tensor& input_tensor,
     const ttnn::Tensor& weight_tensor,
     const std::optional<ttnn::Tensor>& bias_tensor,
-    std::optional<unary::UnaryWithParam> fused_activation,
+    std::optional<ttnn::operations::unary::UnaryWithParam> fused_activation,
     const std::optional<const ::ttnn::experimental::prim::AllGatherMinimalMatmulAsyncConfig>& config,
     const std::vector<GlobalSemaphore>& multi_device_global_semaphore,
     const ttnn::ccl::Topology topology,
@@ -51,4 +50,4 @@ ttnn::Tensor ExecuteAllGatherMinimalMatmulAsync::invoke(
         num_buffers_per_channel);
 }
 
-}  // namespace ttnn::operations::experimental::all_gather_minimal_matmul_async
+}  // namespace ttnn
