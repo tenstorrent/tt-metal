5e9445ca4b29888a7e50024169d2d71dc89bbc70
alnah005 <salnahari@tenstorrent.com>
2026-02-15 21:47:45 +0000

    tt_symbiote: added failing moe test case

--- models/experimental/tt_symbiote/tests/test_moe.py
+++ models/experimental/tt_symbiote/tests/test_moe.py
@@ -10,7 +10,7 @@ import torch
 from models.experimental.tt_symbiote.modules.moe import (
     Glm4MoeConfig,
     Glm4MoeMoE,
-    TTNNGlm4MoeMoE,
+    TTNNMoE,
 )
 from models.experimental.tt_symbiote.utils.device_management import set_device
 from models.experimental.tt_symbiote.core.utils import compare_fn_outputs
@@ -21,13 +21,13 @@ import ttnn
 def default_moe_config():
     """Default MoE configuration for testing."""
     return Glm4MoeConfig(
-        hidden_size=4096,
-        intermediate_size=10944,
-        moe_intermediate_size=1408,
-        num_local_experts=128,
+        hidden_size=2048,
+        intermediate_size=10240,
+        moe_intermediate_size=1536,
+        num_local_experts=64,
         num_experts_per_tok=4,
         n_shared_experts=1,
-        routed_scaling_factor=1.0,
+        routed_scaling_factor=1.8,
         n_group=1,
         topk_group=1,
         norm_topk_prob=True,
@@ -43,10 +43,10 @@ def test_glm4_moe_full(mesh_device, default_moe_config):
     model.eval()
     torch.set_grad_enabled(False)
 
-    batch_size, seq_len = 2, 4
+    batch_size, seq_len = 1, 115
     inputs = torch.randn((batch_size, seq_len, default_moe_config.hidden_size), dtype=torch.bfloat16)
     outputs_torch = model(inputs)
-    ttnn_model = TTNNGlm4MoeMoE.from_torch(model)
+    ttnn_model = TTNNMoE.from_torch(model)
     set_device(ttnn_model, mesh_device)
     outputs_ttnn = ttnn_model(inputs)
     compare_fn_outputs(outputs_torch, outputs_ttnn, "Glm4MoeMoE")
