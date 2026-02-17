59dc5b33176f3e9cd3ff0de28056b166e96b211d
Bing Li <bli@tenstorrent.com>
2026-02-17 03:26:57 +0000

    Added proof of concept for ETH link recovery in blackhole
    - 2 step process:
      - 1. Bring all missing connections down via an ETH MSG
      - 2. Ask all missing connections to reinit via an ETH MSG
    - Added run_validation_recovery_test.sh to help test
    - Updated run_validation.sh to allow running via local build
    - Updated analyze_validation_results to check for link retraining

--- tools/scaleout/validation/run_cluster_validation.cpp
+++ tools/scaleout/validation/run_cluster_validation.cpp
@@ -372,8 +372,9 @@ int main(int argc, char* argv[]) {
     AsicTopology missing_asic_topology = run_connectivity_validation(input_args, physical_system_descriptor);
 
     bool links_reset = false;
+    auto& cluster = tt::tt_metal::MetalContext::instance().get_cluster();
     // Ethernet Link Retraining through SW is currently only supported for Wormhole
-    bool link_retrain_supported = tt::tt_metal::MetalContext::instance().get_cluster().arch() == tt::ARCH::WORMHOLE_B0;
+    bool link_retrain_supported = (cluster.arch() == tt::ARCH::WORMHOLE_B0 || cluster.arch() == tt::ARCH::BLACKHOLE);
     constexpr uint32_t MAX_RETRAINS_BEFORE_FAILURE =
         5;  // If links don't come up after 5 retrains, the system is in an unrecoverable state.
     uint32_t num_retrains = 0;
@@ -385,11 +386,13 @@ int main(int argc, char* argv[]) {
         missing_asic_topology = run_connectivity_validation(input_args, physical_system_descriptor);
     }
 
+    distributed_context.barrier();
     if (num_retrains == MAX_RETRAINS_BEFORE_FAILURE && !missing_asic_topology.empty()) {
         TT_THROW("Encountered unrecoverable state. Please check the system and try again.");
         return -1;
     }
     if (links_reset) {
+        // Return and ask user to run again, otherwise some incorrect HW states might persist and cause hangs
         log_output_rank0("Ethernet Links were Retrained. Please run the validation tool again to issue traffic.");
         return 0;
     }
