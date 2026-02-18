f8140ec8ca4b6fc0261fcbe34fbed4afa55f73c1
arikTT <ayaacob@tenstorrent.com>
2026-02-18 21:44:52 +0000

    add TODO

--- tt_metal/hw/firmware/src/tt-2xx/trisc.cc
+++ tt_metal/hw/firmware/src/tt-2xx/trisc.cc
@@ -80,6 +80,8 @@ constexpr bool cb_init_write = false;
 using namespace ckernel;
 
 void init_sync_registers() {
+    // TODO: check if this is needed with tranistion to DFBs
+    // https://github.com/tenstorrent/tt-metal/issues/36889
     // volatile tt_reg_ptr uint* tiles_received_ptr;
     // volatile tt_reg_ptr uint* tiles_acked_ptr;
     // for (uint32_t operand = 0; operand < NUM_CIRCULAR_BUFFERS; operand++) {
