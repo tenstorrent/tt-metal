1ee5696da6949a255d76c37f1ebb569992952117
John Bauman <jbauman@tenstorrent.com>
2026-02-18 17:33:46 +0000

    fix stack usage

--- tests/scripts/run_tools_tests.sh
+++ tests/scripts/run_tools_tests.sh
@@ -22,7 +22,7 @@ if [[ -z "$TT_METAL_SLOW_DISPATCH_MODE" ]] ; then
     # Check that stack dumping is working
     ./build/test/tt_metal/unit_tests_debug_tools --gtest_filter=*TestWatcherRingBufferBrisc
     ./build/tools/watcher_dump -d=0 -w
-    grep "brisc highest stack usage:" generated/watcher/watcher.log > /dev/null || { echo "Error: couldn't find stack usage in watcher log after dump." ; exit 1; }
+    grep "BRISC highest stack usage:" generated/watcher/watcher.log > /dev/null || { echo "Error: couldn't find stack usage in watcher log after dump." ; exit 1; }
     echo "Watcher stack usage test - Pass"
 
     # Remove created files.
