78d6a7c6c8d4df75636333b5d39d498f25765cc5
Almeet Bhullar <109364210+abhullar-tt@users.noreply.github.com>
2026-02-19 14:30:32 -0500

    [Quasar DFB] Update to support running on Tensix and update tile counter assignment to respect remapper rules  (#37861)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/36889
    
    ### Problem description
    Continuation of DFB support. This PR adds:
    - ability for tensix kernels to use DFBs. FYI @arikTT and @amokanTT I
    was only able to run on single neo and needed to comment out ckernel and
    llk apis that were causing compilation issues
    - uplifted tc assignment when using remapper to ensure: producer and
    consumer don't use same tile counter interface
    - added ability to support using remapper in nSxmS cases if another DFB
    on the same core needs remapper
    
    
    ### Checklist
    
    - [ ] [![All post-commit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml/badge.svg?branch=abhullar/remapper-3)](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml?query=branch:abhullar/remapper-3)
    - [ ] [![Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml/badge.svg?branch=abhullar/remapper-3)](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml?query=branch:abhullar/remapper-3)
    - [ ]
    [![cpp-unit-tests](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml/badge.svg?branch=abhullar/remapper-3)](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml?query=branch:abhullar/remapper-3)
    - [ ] New/Existing tests provide coverage for changes

--- tests/tt_metal/tt_metal/test_kernels/compute/dfb_t6_consumer.cpp
+++ tests/tt_metal/tt_metal/test_kernels/compute/dfb_t6_consumer.cpp
@@ -3,54 +3,33 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "experimental/dataflow_buffer.h"
-#include "experimental/noc.h"
-#include "experimental/tensor.h"
+#include "api/compute/common.h"
 #include "api/debug/dprint.h"
 
 void kernel_main() {
     const uint32_t dst_addr_base = get_compile_time_arg_val(0);
     const uint32_t num_entries_per_consumer = get_compile_time_arg_val(1);
     const uint32_t blocked_consumer = get_compile_time_arg_val(2);
-    constexpr auto dst_args = TensorAccessorArgs<3>();
 
     uint32_t consumer_mask = get_arg_val<uint32_t>(0);
-    uint32_t logical_dfb_id = get_arg_val<uint32_t>(1);
     const uint32_t num_consumers = static_cast<uint32_t>(__builtin_popcount(consumer_mask));
 
-    experimental::DataflowBuffer dfb(logical_dfb_id);
-    experimental::Noc noc;
-
-    std::uint64_t hartid;
-    asm volatile("csrr %0, mhartid" : "=r"(hartid));
-    uint32_t consumer_idx = static_cast<uint32_t>(__builtin_popcount(consumer_mask & ((1u << hartid) - 1u)));
+    experimental::DataflowBuffer dfb(0);
 
     // DPRINT << "consumer_idx: " << consumer_idx << " num_entries_per_consumer: " << num_entries_per_consumer <<
     // ENDL();
 
     // uint32_t dst_addr_base = get_arg_val<uint32_t>(0);
-    uint32_t entry_size = dfb.get_entry_size();
-    const auto tensor_accessor = TensorAccessor(dst_args, dst_addr_base, entry_size);
 
     for (uint32_t tile_id = 0; tile_id < num_entries_per_consumer; tile_id++) {
         // DPRINT << "wfw" << ENDL();
         dfb.wait_front(1);
         // in blocked case maybe each consumer can modify the data so host knows that each have consumed it
         // DPRINT << "wfd" << ENDL();
-        uint32_t page_id = 0;
-        if constexpr (blocked_consumer) {
-            page_id = tile_id;
-        } else {
-            page_id = tile_id * num_consumers + consumer_idx;
-        }
-        DPRINT << "consumer tile id " << tile_id << " page id " << page_id << ENDL();
-        // for blocked consumer each consumer reads each tile .. user kernel shouldn't have to think about this (tensor
-        // accessor will abstract)
-        noc.async_write(dfb, tensor_accessor, entry_size, {}, {.page_id = page_id});
         // DPRINT << "pfw" << ENDL();
+        DPRINT << "consumer tile id " << tile_id << ENDL();
         dfb.pop_front(1);
         // DPRINT << "pfd" << ENDL();
     }
-    DPRINT << "CBW" << ENDL();
-    noc.async_write_barrier();
     DPRINT << "CBWD" << ENDL();
 }
