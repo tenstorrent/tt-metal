f45d74d824c78330bb431e040da2e916baa62fd5
asaigal <asaigal@tenstorrent.com>
2026-02-19 00:39:49 +0000

    Pipeline functional across chain of intermediate devices

--- models/demos/deepseek_v3_b1/tests/unit_tests/test_host_io.py
+++ models/demos/deepseek_v3_b1/tests/unit_tests/test_host_io.py
@@ -26,7 +26,7 @@ def create_fabric_router_config(max_payload_size):
 @pytest.mark.parametrize(
     "tensor_size_bytes, fifo_size, num_iterations",
     [
-        (64, 128, 512),
+        (64, 128, 1),
         (64, 256, 512),
         (64, 512, 512),
         (64, 1024, 512),
@@ -41,6 +41,11 @@ def create_fabric_router_config(max_payload_size):
         ttnn.H2DMode.HOST_PUSH,
         ttnn.H2DMode.DEVICE_PULL,
     ],
+)
+@pytest.mark.parametrize(
+    "mesh_device",
+    [(8, 4)],
+    indirect=True,
 )
 @pytest.mark.parametrize(
     "device_params",
@@ -59,14 +64,22 @@ def test_host_io_loopback(mesh_device, tensor_size_bytes, fifo_size, num_iterati
     tensor_size_datums = tensor_size_bytes // 4
 
     start_device_coord = ttnn.MeshCoordinate(0, 0)
-    intermed_device_coord_1 = ttnn.MeshCoordinate(0, 1)
-    intermed_device_coord_2 = ttnn.MeshCoordinate(0, 2)
-    end_device_coord = ttnn.MeshCoordinate(0, 3)
+    intermed_device_coord_1 = ttnn.MeshCoordinate(1, 0)
+    intermed_device_coord_2 = ttnn.MeshCoordinate(2, 0)
+    intermed_device_coord_3 = ttnn.MeshCoordinate(3, 0)
+    intermed_device_coord_4 = ttnn.MeshCoordinate(4, 0)
+    intermed_device_coord_5 = ttnn.MeshCoordinate(5, 0)
+    intermed_device_coord_6 = ttnn.MeshCoordinate(6, 0)
+    end_device_coord = ttnn.MeshCoordinate(7, 0)
     core_coord = ttnn.CoreCoord(0, 0)
 
     h2d_core = ttnn.MeshCoreCoord(start_device_coord, core_coord)
     fwd_core_0 = ttnn.MeshCoreCoord(intermed_device_coord_1, core_coord)
     fwd_core_1 = ttnn.MeshCoreCoord(intermed_device_coord_2, core_coord)
+    fwd_core_2 = ttnn.MeshCoreCoord(intermed_device_coord_3, core_coord)
+    fwd_core_3 = ttnn.MeshCoreCoord(intermed_device_coord_4, core_coord)
+    fwd_core_4 = ttnn.MeshCoreCoord(intermed_device_coord_5, core_coord)
+    fwd_core_5 = ttnn.MeshCoreCoord(intermed_device_coord_6, core_coord)
     d2h_core = ttnn.MeshCoreCoord(end_device_coord, core_coord)
 
     print("Mesh Device Shape: ", mesh_device.shape)
@@ -74,11 +87,16 @@ def test_host_io_loopback(mesh_device, tensor_size_bytes, fifo_size, num_iterati
 
     print("H2D Device ID: ", mesh_device.get_device_id(start_device_coord))
     print("FWD Device ID: ", mesh_device.get_device_id(intermed_device_coord_1))
-    print("FWD Device ID: ", mesh_device.get_device_id(intermed_device_coord_2))
+    print("FWD Device ID 2: ", mesh_device.get_device_id(intermed_device_coord_2))
+    print("FWD Device ID 3: ", mesh_device.get_device_id(intermed_device_coord_3))
+    print("FWD Device ID 4: ", mesh_device.get_device_id(intermed_device_coord_4))
+    print("FWD Device ID 5: ", mesh_device.get_device_id(intermed_device_coord_5))
+    print("FWD Device ID 6: ", mesh_device.get_device_id(intermed_device_coord_6))
     print("D2H Device ID: ", mesh_device.get_device_id(end_device_coord))
 
     h2d_socket = ttnn.H2DSocket(mesh_device, h2d_core, ttnn.BufferType.L1, fifo_size, h2d_mode)
     d2h_socket = ttnn.D2HSocket(mesh_device, d2h_core, fifo_size)
+    print("Creating Host Interface")
     host_io = HostInterface(
         h2d_socket,
         d2h_socket,
@@ -86,21 +104,45 @@ def test_host_io_loopback(mesh_device, tensor_size_bytes, fifo_size, num_iterati
         tensor_size_bytes,
         core_to_core_socket_buffer_size=fifo_size,
         h2d_downstream_core=fwd_core_0,
-        d2h_upstream_core=fwd_core_1,
+        d2h_upstream_core=fwd_core_5,
     )
-
-    socket_interface = SocketInterface(
+    print("Creating Socket Interface 1")
+    socket_interface_1 = SocketInterface(
         tensor_size_bytes,
         fifo_size,
         tensor_size_bytes,
         fwd_core_0,
         fwd_core_1,
         upstream_socket=host_io.get_downstream_socket(),
+        downstream_core_coord=fwd_core_2,
+        mesh_device=mesh_device,
+    )
+    print("Creating Socket Interface 2")
+    socket_interface_2 = SocketInterface(
+        tensor_size_bytes,
+        fifo_size,
+        tensor_size_bytes,
+        fwd_core_2,
+        fwd_core_3,
+        upstream_socket=socket_interface_1.get_downstream_socket(),
+        downstream_core_coord=fwd_core_4,
+        mesh_device=mesh_device,
+    )
+
+    socket_interface_3 = SocketInterface(
+        tensor_size_bytes,
+        fifo_size,
+        tensor_size_bytes,
+        fwd_core_4,
+        fwd_core_5,
+        upstream_socket=socket_interface_2.get_downstream_socket(),
         downstream_socket=host_io.get_upstream_socket(),
     )
 
     host_io.run()
-    socket_interface.run()
+    socket_interface_1.run()
+    socket_interface_2.run()
+    socket_interface_3.run()
 
     logger.info(f"Transferring Data Over H <-> D Interface for {num_iterations} iterations")
     logger.info(f"Tensor Size: {tensor_size_bytes} bytes, FIFO Size: {fifo_size} bytes")
@@ -122,7 +164,9 @@ def test_host_io_loopback(mesh_device, tensor_size_bytes, fifo_size, num_iterati
         assert match, f"H2D → D2H loopback data mismatch!\nExpected: {torch_input}\nGot: {result_torch}"
 
     host_io.terminate()
-    socket_interface.terminate()
+    socket_interface_1.terminate(False)
+    socket_interface_2.terminate(False)
+    socket_interface_3.terminate(True)
 
 
 @pytest.mark.parametrize(
