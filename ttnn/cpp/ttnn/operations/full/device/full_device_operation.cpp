473a11aee45835c582935fb3b3a3964f29b6ace3
awliu-TT <awliu@tenstorrent.com>
2026-02-19 22:25:17 +0000

    opus nd shard support full op

--- ttnn/cpp/ttnn/operations/full/device/full_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/full/device/full_device_operation.cpp
@@ -9,11 +9,25 @@
 #include "ttnn/tensor/tensor.hpp"
 
 namespace ttnn::operations::full {
+
+FullOperation::program_factory_t FullOperation::select_program_factory(
+    const operation_attributes_t& operation_attributes, const tensor_args_t& /*tensor_args*/) {
+    if (operation_attributes.memory_config.is_sharded()) {
+        if (operation_attributes.memory_config.shard_spec().has_value()) {
+            return FullShardedProgramFactory{};
+        }
+        return FullNDShardedProgramFactory{};
+    }
+    return FullInterleavedProgramFactory{};
+}
+
 void FullOperation::validate_inputs(
     const operation_attributes_t& operation_attributes, const tensor_args_t& /*tensor_args*/) {
     TT_FATAL(
-        operation_attributes.memory_config.memory_layout() == TensorMemoryLayout::INTERLEAVED,
-        "Full operation error: Not currently supporting sharding");
+        operation_attributes.dtype == DataType::BFLOAT16 || operation_attributes.dtype == DataType::INT32 ||
+            operation_attributes.dtype == DataType::FLOAT32,
+        "Full: Unsupported data type {}",
+        operation_attributes.dtype);
 
     const auto shape = operation_attributes.shape;
 
@@ -22,7 +36,7 @@ void FullOperation::validate_inputs(
         "Full operation error: Shape size must be greater than 1, but got shape size = {}",
         shape.size());
 
-    for (int i = 0; i < shape.size(); i++) {
+    for (size_t i = 0; i < shape.size(); i++) {
         TT_FATAL(
             shape[i] > 0,
             "Full operation error: Invalid shape at index {}. Each dimension of the shape must be greater than 0, but"
