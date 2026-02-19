b021283bc1d3ec3732fa3951aed725fe8d864d78
Piotr Rybicki <prybicki@tenstorrent.com>
2026-02-19 13:30:45 +0000

    Add L2 cache flush support for Quasar DM cores

--- tests/tt_metal/tt_metal/test_kernels/dataflow/simple_l1_write.cpp
+++ tests/tt_metal/tt_metal/test_kernels/dataflow/simple_l1_write.cpp
@@ -4,12 +4,15 @@
 
 #include "api/dataflow/dataflow_api.h"
 #include "api/debug/dprint.h"
+#include "risc_common.h"
 
 void kernel_main() {
     uintptr_t dst_addr = get_arg_val<uint32_t>(0);
     uint32_t value = get_common_arg_val<uint32_t>(0);
-    uint32_t buffer_size = get_named_ct_arg("buffer_size");
-    DPRINT << "buffer_size: " << buffer_size << ENDL();
-    *((uint32_t*)(dst_addr + MEM_L1_UNCACHED_BASE)) =
-        value;  // use cache write-around for now, in the future use cache flush
+
+    // Write to cacheable L1 address
+    *((volatile uint32_t*)dst_addr) = value;
+
+    // Flush the cache line to TL1 (node memory) so host can read it
+    flush_l2_cache_line(dst_addr);
 }
