d2e367749283aeb5568a26df684c2bee4b8835be
adjordjevic-TT <adjordjevic@tenstorrent.com>
2026-02-20 14:57:23 +0000

    Add output path as script argument

--- tools/triage/dump_risc_debug_signals.py
+++ tools/triage/dump_risc_debug_signals.py
@@ -5,7 +5,7 @@
 
 """
 Usage:
-    dump_risc_debug_signals
+    dump_risc_debug_signals [--path=<path>]
 
 Description:
     Goes through all tensix and idle_eth blocks and tries to halt every RISC core inside them.
@@ -20,6 +20,7 @@ from collections import defaultdict
 import json
 import os
 from triage import ScriptConfig, log_warning, run_script, log_check_location
+from ttexalens.umd_device import TimeoutDeviceRegisterError
 from run_checks import run as get_run_checks
 from dispatcher_data import run as get_dispatcher_data, DispatcherData
 from elfs_cache import run as get_elfs_cache, ElfsCache
@@ -75,6 +76,7 @@ def collect_debug_bus_signals(
             continue
 
         try:
+            raise RiscHaltError(risc_name, location)
             with risc_debug.ensure_halted():
                 pass
         except RiscHaltError:
@@ -109,7 +111,8 @@ def collect_debug_bus_signals(
             "failed_riscs": failed_riscs,
             "debug_bus_signal_groups": debug_bus_data,
         }
-
+    except TimeoutDeviceRegisterError:
+        raise
     except Exception as e:
         log_check_location(location, False, f"Failed to collect debug bus signals: {e}")
         return None
@@ -153,7 +156,7 @@ def run(args, context: Context):
             }
 
         if all_debug_bus_data:
-            output_path = os.getenv("TT_TRIAGE_DUMP_RISC_DEBUG_SIGNALS_PATH", "debug_bus_signal_groups.json")
+            output_path = args["--path"] if args["--path"] else "debug_bus_signal_groups.json"
             with open(output_path, "w") as f:
                 json.dump(all_debug_bus_data, f, indent=2)
             log_warning(f"Some riscs are broken. Generated JSON file with debug bus signals at {output_path}")
