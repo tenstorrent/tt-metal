1315e6f3b1767f530518d544218e1b7d21c6111b
kkannan <kkannan@tenstorrent.com>
2026-02-20 13:32:03 +0000

    Shape rank conversion issue in prod

---
+++ tests/ttnn/unit_tests/operations/prod/test_prod.py
@@ -0,0 +1,64 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+import ttnn
+import math
+import pytest
+
+class DeviceGetter:
+    _instance = None
+    _mesh_shape = None
+    l1_small_size = 1 << 15
+
+    def __init__(self):
+        raise RuntimeError("This is Singleton, invoke get_device() instead.")
+
+    def __del__(self):
+        if self._instance is not None:
+            ttnn.close_mesh_device(self._instance)
+            ttnn.set_fabric_config(ttnn.FabricConfig.DISABLED)
+
+    @classmethod
+    def get_device(cls, mesh_shape):
+        if cls._instance == None:
+            if (
+                not isinstance(mesh_shape, (list, tuple))
+                or len(mesh_shape) == 0
+                or not all(isinstance(x, int) and x > 0 for x in mesh_shape)
+            ):
+                raise ValueError(
+                    f"mesh_shape must be a non-empty list or tuple of positive integers, got {mesh_shape}"
+                )
+            cls._mesh_shape = mesh_shape
+
+            if math.prod(mesh_shape) >= 2:
+                ttnn.set_fabric_config(ttnn.FabricConfig.FABRIC_1D)
+            cls._instance = ttnn.open_mesh_device(
+                mesh_shape=ttnn.MeshShape(mesh_shape),
+                l1_small_size=cls.l1_small_size,
+            )
+            print(f"Device: {cls._instance}")
+
+        # Compare requested mesh_shape with _mesh_shape used to initialize the device
+        if tuple(cls._mesh_shape) != tuple(mesh_shape):
+            raise ValueError(
+                f"Device already initialized with mesh_shape={cls._mesh_shape}, but got mesh_shape={mesh_shape}"
+            )
+
+        return cls._instance
+
+@pytest.mark.parametrize("input_values", [[224, 224], [54, 54]])
+def test_prod(input_values):
+
+    device = DeviceGetter.get_device((1, 1))
+
+    # prepare input
+    torch_input = torch.tensor(input_values, dtype=torch.int32)
+    as_bf16 = torch_input.to(torch.bfloat16)
+    tt_input = ttnn.from_torch(as_bf16, dtype=ttnn.bfloat16, layout=ttnn.TILE_LAYOUT, device=device)
+
+    # operation
+    prod_result = ttnn.prod(tt_input, dim=0, keepdim=False)
+    
