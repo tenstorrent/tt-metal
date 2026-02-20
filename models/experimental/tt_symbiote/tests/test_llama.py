804b9bae9b631bc21e9581a2abeffe827972c68d
gtobarTT <gtobar@tenstorrent.com>
2026-02-20 12:27:56 -0500

    tt_symbiote: added additional test for intelligent tnn linear implementation

--- models/experimental/tt_symbiote/tests/test_llama.py
+++ models/experimental/tt_symbiote/tests/test_llama.py
@@ -16,6 +16,7 @@ from models.experimental.tt_symbiote.modules.normalization import TTNNLayerNorm,
 from models.experimental.tt_symbiote.modules.attention import LlamaAttention
 from models.experimental.tt_symbiote.utils.device_management import set_device
 from models.experimental.tt_symbiote.utils.module_replacement import register_module_replacement_dict
+from models.experimental.tt_symbiote.modules.linear_intelligent import SmartTTNNLinear
 
 
 class LlamaMLP(nn.Module):
@@ -77,4 +78,46 @@ def test_llama(device):
     DispatchManager.clear_timings()
     outputs = model.generate(**inputs, max_new_tokens=128, use_cache=True)
     DispatchManager.save_stats_to_file("llama_timing_stats.csv")
+    print(tokenizer.decode(outputs[0][inputs["input_ids"].shape[-1] :]))
+
+
+def test_llama_intelligent(device):
+    """Test LLaMA model with TTNN acceleration."""
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B-Instruct")
+    model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B-Instruct").to(dtype=torch.bfloat16)
+    nn_to_nn = {
+        model.model.layers[0].mlp.__class__: LlamaMLP,
+    }
+    nn_to_ttnn = {
+        nn.Linear: SmartTTNNLinear,
+        nn.SiLU: TTNNSilu,
+        nn.LayerNorm: TTNNLayerNorm,
+        model.model.layers[0].input_layernorm.__class__: TTNNRMSNorm,
+        model.model.layers[0].self_attn.__class__: LlamaAttention,
+    }
+    modules = register_module_replacement_dict(model, nn_to_nn, model_config=None)
+    messages = [
+        {
+            "role": "user",
+            "content": "What is your favorite condiment? There are so many condiments to choose from, each bringing its unique flavor and texture to enhance different dishes. Do you prefer the classic taste of ketchup, the creamy richness of mayonnaise, the spicy kick of mustard, or perhaps something more exotic like sriracha or hoisin sauce? Maybe you enjoy the tangy zest of salsa or the smooth and savory taste of aioli. Share what your favorite condiment is and why you love it. Does it remind you of a specific dish or meal?",
+        },
+    ]
+    inputs = tokenizer.apply_chat_template(
+        messages,
+        add_generation_prompt=True,
+        tokenize=True,
+        return_dict=True,
+        return_tensors="pt",
+    ).to(model.device)
+    modules = register_module_replacement_dict(model, nn_to_ttnn, model_config=None, exclude_replacement={"lm_head"})
+
+    set_device(model, device)
+    for k, v in tqdm(modules.items()):
+        v.preprocess_weights()
+        v.move_weights_to_device()
+    model.eval()  # Disables dropout, batch norm updates
+    torch.set_grad_enabled(False)  # Disables autograd overhead
+    DispatchManager.clear_timings()
+    outputs = model.generate(**inputs, max_new_tokens=128, use_cache=True)
+    DispatchManager.save_stats_to_file("llama_intelligent_timing_stats_3.csv")
     print(tokenizer.decode(outputs[0][inputs["input_ids"].shape[-1] :]))
