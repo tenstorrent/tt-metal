682beb8e2168233e9b555aa3a8f3bb68739821bd
Adriel Bustamante <abustamante@tenstorrent.com>
2026-02-24 23:05:13 +0000

    Updated runner

--- tt-train/sources/examples/python/multihost/hierarchical_parallel/runner.sh
+++ tt-train/sources/examples/python/multihost/hierarchical_parallel/runner.sh
@@ -7,7 +7,6 @@
 # Set environmental variables
 
 export TT_METAL_HOME="/data/${USER}/tt-metal"
-export PYTHONPATH="${TT_METAL_HOME}:${PYTHONPATH}"
 source ${TT_METAL_HOME}/python_env/bin/activate
 export TT_METAL_FABRIC_ROUTER_SYNC_TIMEOUT_MS=120000
 export HIERARCHICAL_ROOT="/data/${USER}/tt-metal/tt-train/sources/examples/python/multihost/hierarchical_parallel"
@@ -16,4 +15,4 @@ export LD_LIBRARY_PATH="/opt/openmpi-v5.0.7-ulfm/lib:$LD_LIBRARY_PATH"
 CONFIG_FILE="training_configs/training_shakespeare_tinyllama_3tier_fabric.yaml"
 HOST_CONFIG="5loudboxes"
 
-tt-run --mpi-args "--rankfile configurations/${HOST_CONFIG}/hosts.txt -x TT_METAL_HOME --mca mpi_show_mca_params all --mca btl_tcp_if_include eno1 --mca oob_tcp_if_include eno1 --mca btl self,tcp --tag-output" --rank-binding ${HIERARCHICAL_ROOT}/configurations/${HOST_CONFIG}/rank_bindings.yaml python ${TT_METAL_HOME}/tt-train/sources/examples/python/multihost/hierarchical_parallel/training.py -c ${CONFIG_FILE} --verbose
+tt-run --mpi-args "--rankfile configurations/${HOST_CONFIG}/hosts.txt -x TT_METAL_HOME --tag-output" --rank-binding ${HIERARCHICAL_ROOT}/configurations/${HOST_CONFIG}/rank_bindings.yaml python ${TT_METAL_HOME}/tt-train/sources/examples/python/multihost/hierarchical_parallel/training.py -c ${CONFIG_FILE} --tcp-interface eno1 --verbose
