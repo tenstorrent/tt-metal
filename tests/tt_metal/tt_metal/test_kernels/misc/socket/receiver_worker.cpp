5b95e31d440e2f562030e152ec009784e0f64972
jpanasiukTT <jpanasiuk@tenstorrent.com>
2026-02-25 20:56:53 +0000

    restore changes to the files that werent added

--- tests/tt_metal/tt_metal/test_kernels/misc/socket/receiver_worker.cpp
+++ tests/tt_metal/tt_metal/test_kernels/misc/socket/receiver_worker.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: © 2026 Tenstorrent AI ULC
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 //
 // SPDX-License-Identifier: Apache-2.0
 #include <cstdint>
@@ -6,6 +6,7 @@
 #include "api/socket_api.h"
 
 void kernel_main() {
+    // Get this value from MeshSocket struct on host
     constexpr uint32_t socket_config_addr = get_compile_time_arg_val(0);
     constexpr uint32_t local_l1_buffer_addr = get_compile_time_arg_val(1);
     constexpr uint32_t page_size = get_compile_time_arg_val(2);
@@ -14,6 +15,23 @@ void kernel_main() {
 
     SocketReceiverInterface receiver_socket = create_receiver_socket_interface(socket_config_addr);
     set_receiver_socket_page_size(receiver_socket, page_size);
+
+    // Reads one page at a time and sends ack to sender, can be optimized to notify
+    // receiver after reading larger chunks
+    // In this example the local_cb isn't actually needed (we can just get the rptr)
+    // from the socket itself
+    // Its there to illustrate how a local CB can be connected to the socket interface
+    // If the local CB was being used for compute:
+    //  - The reader (this kernel) would:
+    //     - Handshake with sender
+    //     - Call cb_reserve_back on the recv_cb_id (shared between reader and compute)
+    //     - Call cb_push_back to notify the compute kernel of data
+    //  - The compute kernel would:
+    //     - Call cb_wait_front
+    //     - Call cb_pop_front to notify the receiver that pages are available
+    //  - In this case, an additional CB would be needed to synchronize the receiver and
+    //    compute kernels (socket_notify_sender cannot be called until compute is done with
+    //    pages in the local CB)
 
     for (uint32_t i = 0; i < num_iterations; i++) {
         uint32_t outstanding_data_size = data_size;
