6ca93fb3e28a856633e640ed6b3167821f372834
velonica0 <like@mail.nankai.edu.cn>
2026-02-25 15:16:31 +0800

    download_weight

--- models/experimental/mobilenet/mobilenetv2_cpp/script/gen_mobilenetv2_script.py
+++ models/experimental/mobilenet/mobilenetv2_cpp/script/gen_mobilenetv2_script.py
@@ -2,16 +2,17 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import os
-
+import urllib.request
 import torch
 
 from models.demos.mobilenetv2.reference.mobilenetv2 import Mobilenetv2
 
-weights_path = "models/demos/mobilenetv2/mobilenet_v2-b0353104.pth"
-if not os.path.exists(weights_path):
-    os.system("bash models/demos/mobilenetv2/weights_download.sh")
+script_dir = os.path.dirname(os.path.abspath(__file__))
+save_path = os.path.join(script_dir, "mobilenet_v2-b0353104.pth")
+url = "https://download.pytorch.org/models/mobilenet_v2-b0353104.pth"
+urllib.request.urlretrieve(url, save_path)
 
-state_dict = torch.load(weights_path)
+state_dict = torch.load(save_path)
 ds_state_dict = {k: v for k, v in state_dict.items()}
 
 torch_model = Mobilenetv2()
@@ -24,4 +25,9 @@ torch_model.load_state_dict(new_state_dict)
 torch_model.eval()
 dummy_input = torch.randn(1, 3, 224, 224)
 script_module = torch.jit.trace(torch_model, dummy_input)
-script_module.save("models/mobilenetv2_cpp/mobilenet_v2-b0353104-script.pt")
+
+save_path_pt = os.path.join(script_dir, "mobilenet_v2-b0353104.pt")
+script_module.save(save_path_pt)
+
+if os.path.exists(save_path):
+    os.remove(save_path)
