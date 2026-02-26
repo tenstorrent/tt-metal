54d1c7e0f6f9476e84897c8868eec7c4d51e7101
Nikola Milicevic <nmilicevic@tenstorrent.com>
2026-02-26 19:10:26 +0000

    Refactor perf test

--- models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py
+++ models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py
@@ -105,12 +105,68 @@ def test_refiner_unet(
     )
 
 
+# Device-specific performance expectations (ns per iteration)
+# Format: {test_id: {"wormhole": perf_ns, "blackhole": perf_ns}}
+DEVICE_PERF_EXPECTATIONS = {
+    "unet_1024x1024": {
+        "wormhole": 191_651_771 * UNET_DEVICE_TEST_TOTAL_ITERATIONS,
+        "blackhole": 119_138_750 * UNET_DEVICE_TEST_TOTAL_ITERATIONS,
+    },
+    "unet_512x512": {
+        "wormhole": 91_463_635 * UNET_DEVICE_TEST_TOTAL_ITERATIONS,
+        "blackhole": None,  # Only 1024x1024 tested on Blackhole
+    },
+    "refiner_unet_1024x1024": {
+        "wormhole": 244_107_203 * UNET_DEVICE_TEST_TOTAL_ITERATIONS,
+        "blackhole": 131_795_269 * UNET_DEVICE_TEST_TOTAL_ITERATIONS,
+    },
+    "refiner_unet_512x512": {
+        "wormhole": 79_843_092 * UNET_DEVICE_TEST_TOTAL_ITERATIONS,
+        "blackhole": None,  # Only 1024x1024 tested on Blackhole
+    },
+    "vae_decode_1024x1024": {
+        "wormhole": 663_083_865,
+        "blackhole": 369_666_097,
+    },
+    "vae_decode_512x512": {
+        "wormhole": 171_560_642,
+        "blackhole": None,  # Only 1024x1024 tested on Blackhole
+    },
+    "vae_encode_1024x1024": {
+        "wormhole": 324_271_938,
+        "blackhole": 170_093_216,
+    },
+    "vae_encode_512x512": {
+        "wormhole": 83_537_085,
+        "blackhole": None,  # Only 1024x1024 tested on Blackhole
+    },
+    "clip_encoder_1": {
+        "wormhole": 13_112_562,
+        "blackhole": 6_795_180,
+    },
+    "clip_encoder_2": {
+        "wormhole": 63_591_763,  # Note: this is an average value of 30 test runs due to high variability
+        "blackhole": 31_220_061,
+    },
+}
+
+
+def get_device_perf(test_id):
+    """Get expected performance for the current device."""
+    perfs = DEVICE_PERF_EXPECTATIONS.get(test_id, {})
+    if is_blackhole():
+        return perfs.get("blackhole")
+    elif is_wormhole_b0():
+        return perfs.get("wormhole")
+    return None
+
+
 @pytest.mark.parametrize(
-    "command, expected_device_perf_ns_per_iteration, subdir, model_name, num_iterations, batch_size, margin, comments",
+    "test_id, command, subdir, model_name, num_iterations, batch_size, margin, comments",
     [
         (
+            "unet_1024x1024",
             'pytest models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py::test_unet -k "1024x1024"',
-            191_651_771 * UNET_DEVICE_TEST_TOTAL_ITERATIONS,
             "sdxl_unet_1024x1024",
             "sdxl_unet_1024x1024",
             1,
@@ -119,8 +175,8 @@ def test_refiner_unet(
             f"iterations={UNET_DEVICE_TEST_TOTAL_ITERATIONS}",
         ),
         (
+            "unet_512x512",
             'pytest models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py::test_unet -k "512x512"',
-            91_463_635 * UNET_DEVICE_TEST_TOTAL_ITERATIONS,
             "sdxl_unet_512x512",
             "sdxl_unet_512x512",
             1,
@@ -129,8 +185,8 @@ def test_refiner_unet(
             f"iterations={UNET_DEVICE_TEST_TOTAL_ITERATIONS}",
         ),
         (
+            "refiner_unet_1024x1024",
             'pytest models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py::test_refiner_unet -k "1024x1024"',
-            244_107_203 * UNET_DEVICE_TEST_TOTAL_ITERATIONS,
             "sdxl_refiner_unet_1024x1024",
             "sdxl_refiner_unet_1024x1024",
             1,
@@ -139,8 +195,8 @@ def test_refiner_unet(
             f"iterations={UNET_DEVICE_TEST_TOTAL_ITERATIONS}",
         ),
         (
+            "refiner_unet_512x512",
             'pytest models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py::test_refiner_unet -k "512x512"',
-            79_843_092 * UNET_DEVICE_TEST_TOTAL_ITERATIONS,
             "sdxl_refiner_unet_512x512",
             "sdxl_refiner_unet_512x512",
             1,
@@ -149,8 +205,8 @@ def test_refiner_unet(
             f"iterations={UNET_DEVICE_TEST_TOTAL_ITERATIONS}",
         ),
         (
+            "vae_decode_1024x1024",
             "pytest models/experimental/stable_diffusion_xl_base/vae/tests/pcc/test_module_tt_autoencoder_kl.py::test_vae -k 'test_1024x1024_decode'",
-            663_083_865,
             "sdxl_vae",
             "sdxl_vae_decode_1024x1024",
             VAE_DEVICE_TEST_TOTAL_ITERATIONS,
@@ -159,8 +215,8 @@ def test_refiner_unet(
             "",
         ),
         (
+            "vae_decode_512x512",
             "pytest models/experimental/stable_diffusion_xl_base/vae/tests/pcc/test_module_tt_autoencoder_kl.py::test_vae -k 'test_512x512_decode'",
-            171_560_642,
             "sdxl_vae",
             "sdxl_vae_decode_512x512",
             VAE_DEVICE_TEST_TOTAL_ITERATIONS,
@@ -169,8 +225,8 @@ def test_refiner_unet(
             "",
         ),
         (
+            "vae_encode_1024x1024",
             "pytest models/experimental/stable_diffusion_xl_base/vae/tests/pcc/test_module_tt_autoencoder_kl.py::test_vae -k 'test_1024x1024_encode'",
-            324_271_938,
             "sdxl_vae",
             "sdxl_vae_encode_1024x1024",
             VAE_DEVICE_TEST_TOTAL_ITERATIONS,
@@ -179,8 +235,8 @@ def test_refiner_unet(
             "",
         ),
         (
+            "vae_encode_512x512",
             "pytest models/experimental/stable_diffusion_xl_base/vae/tests/pcc/test_module_tt_autoencoder_kl.py::test_vae -k 'test_512x512_encode'",
-            83_537_085,
             "sdxl_vae",
             "sdxl_vae_encode_512x512",
             VAE_DEVICE_TEST_TOTAL_ITERATIONS,
@@ -189,8 +245,8 @@ def test_refiner_unet(
             "",
         ),
         (
+            "clip_encoder_1",
             "pytest models/experimental/stable_diffusion_xl_base/tests/pcc/test_sdxl_clip_encoders.py::test_clip_encoder -k 'encoder_1'",
-            13_112_562,
             "sdxl_clip_encoder_1",
             "sdxl_clip_encoder_1",
             CLIP_ENCODER_DEVICE_TEST_TOTAL_ITERATIONS,
@@ -199,8 +255,8 @@ def test_refiner_unet(
             "",
         ),
         (
+            "clip_encoder_2",
             "pytest models/experimental/stable_diffusion_xl_base/tests/pcc/test_sdxl_clip_encoders.py::test_clip_encoder -k 'encoder_2'",
-            63_591_763,  # Note: this is an average value of 30 test runs due to high variability
             "sdxl_clip_encoder_2",
             "sdxl_clip_encoder_2",
             CLIP_ENCODER_DEVICE_TEST_TOTAL_ITERATIONS,
@@ -222,105 +278,18 @@ def test_refiner_unet(
         "test_sdxl_clip_encoder_2",
     ],
 )
-@pytest.mark.skipif(not is_wormhole_b0(), reason="Wormhole performance test")
 @pytest.mark.models_device_performance_bare_metal
-def test_sdxl_perf_device(
-    command, expected_device_perf_ns_per_iteration, subdir, model_name, num_iterations, batch_size, margin, comments
-):
-    os.environ["TT_MM_THROTTLE_PERF"] = "0" if "clip_encoder" in command else "5"
+def test_sdxl_perf_device(test_id, command, subdir, model_name, num_iterations, batch_size, margin, comments):
+    expected_perf = get_device_perf(test_id)
+    if expected_perf is None:
+        pytest.skip(f"Test {test_id} not configured for current device")
+
+    if is_wormhole_b0():
+        os.environ["TT_MM_THROTTLE_PERF"] = "0" if "clip_encoder" in command else "5"
+
     run_model_device_perf_test(
         command=command,
-        expected_device_perf_ns_per_iteration=expected_device_perf_ns_per_iteration,
-        subdir=subdir,
-        model_name=model_name,
-        num_iterations=num_iterations,
-        batch_size=batch_size,
-        margin=margin,
-        comments=comments,
-    )
-
-
-@pytest.mark.parametrize(
-    "command, expected_device_perf_ns_per_iteration, subdir, model_name, num_iterations, batch_size, margin, comments",
-    [
-        (
-            "pytest models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py::test_unet",
-            119_138_750,
-            "sdxl_unet",
-            "sdxl_unet_blackhole",
-            1,
-            1 * UNET_DEVICE_TEST_TOTAL_ITERATIONS,
-            0.015,
-            f"iterations={UNET_DEVICE_TEST_TOTAL_ITERATIONS}",
-        ),
-        (
-            "pytest models/experimental/stable_diffusion_xl_base/tests/test_sdxl_perf.py::test_refiner_unet",
-            131_795_269,
-            "sdxl_refiner_unet",
-            "sdxl_refiner_unet_blackhole",
-            1,
-            1 * UNET_DEVICE_TEST_TOTAL_ITERATIONS,
-            0.06,
-            f"iterations={UNET_DEVICE_TEST_TOTAL_ITERATIONS}",
-        ),
-        (
-            "pytest models/experimental/stable_diffusion_xl_base/vae/tests/pcc/test_module_tt_autoencoder_kl.py::test_vae -k 'test_decode'",
-            369_666_097,
-            "sdxl_vae",
-            "sdxl_vae_decode_blackhole",
-            VAE_DEVICE_TEST_TOTAL_ITERATIONS,
-            1,
-            0.015,
-            "",
-        ),
-        (
-            "pytest models/experimental/stable_diffusion_xl_base/vae/tests/pcc/test_module_tt_autoencoder_kl.py::test_vae -k 'test_encode'",
-            170_093_216,
-            "sdxl_vae",
-            "sdxl_vae_encode_blackhole",
-            VAE_DEVICE_TEST_TOTAL_ITERATIONS,
-            1,
-            0.015,
-            "",
-        ),
-        (
-            "pytest models/experimental/stable_diffusion_xl_base/tests/pcc/test_sdxl_clip_encoders.py::test_clip_encoder -k 'encoder_1'",
-            6_795_180,
-            "sdxl_clip_encoder_1",
-            "sdxl_clip_encoder_1_blackhole",
-            CLIP_ENCODER_DEVICE_TEST_TOTAL_ITERATIONS,
-            1,
-            0.015,
-            "",
-        ),
-        (
-            "pytest models/experimental/stable_diffusion_xl_base/tests/pcc/test_sdxl_clip_encoders.py::test_clip_encoder -k 'encoder_2'",
-            31_220_061,
-            "sdxl_clip_encoder_2",
-            "sdxl_clip_encoder_2_blackhole",
-            CLIP_ENCODER_DEVICE_TEST_TOTAL_ITERATIONS,
-            1,
-            0.020,
-            "",
-        ),
-    ],
-    ids=[
-        "test_sdxl_unet_blackhole",
-        "test_sdxl_refiner_unet_blackhole",
-        "test_sdxl_vae_decode_blackhole",
-        "test_sdxl_vae_encode_blackhole",
-        "test_sdxl_clip_encoder_1_blackhole",
-        "test_sdxl_clip_encoder_2_blackhole",
-    ],
-)
-@pytest.mark.skipif(not is_blackhole(), reason="Blackhole performance test")
-@pytest.mark.models_device_performance_bare_metal
-def test_sdxl_perf_device_blackhole(
-    command, expected_device_perf_ns_per_iteration, subdir, model_name, num_iterations, batch_size, margin, comments
-):
-    run_model_device_perf_test(
-        command=command,
-        expected_device_perf_ns_per_iteration=expected_device_perf_ns_per_iteration,
+        expected_device_perf_ns_per_iteration=expected_perf,
         subdir=subdir,
         model_name=model_name,
         num_iterations=num_iterations,
