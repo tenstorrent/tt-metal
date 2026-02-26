86c8289919626a4ae741bc321478ea88ce7e5ab2
riverwuTT <riverwu@tenstorrent.com>
2026-02-26 00:22:42 +0000

    fix reduction op

---
+++ tt_metal/api/tt-metalium/experimental/tensor/details/mesh_tensor_backdoor.hpp
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: © 2026 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+/**
+ * This header contains backdoor support for accmulation op to allow update of the underlying storage of a MeshTensor.
+ * This is a temporary solution for #37807 and should be quickly refactored out.
+ */
+
+#include <tt-metalium/experimental/tensor/mesh_tensor.hpp>
+
+namespace tt::tt_metal::do_not_use {
+
+// Update the underlying storage of a MeshTensor
+inline void do_not_use_update_mesh_tensor_storage(MeshTensor& mesh_tensor, const DeviceStorage& storage) {
+    mesh_tensor.impl->storage_ = storage;
+}
+
+}  // namespace tt::tt_metal::do_not_use
--- ttnn/cpp/ttnn/operations/reduction/accumulation/accumulation_common.cpp
+++ ttnn/cpp/ttnn/operations/reduction/accumulation/accumulation_common.cpp
@@ -5,6 +5,7 @@
 #include "accumulation_common.hpp"
 #include "ttnn/operations/copy/typecast/typecast.hpp"
 
+#include <tt-metalium/experimental/tensor/details/mesh_tensor_backdoor.hpp>
 namespace ttnn::operations::reduction::accumulation::common {
 
 Tensor preprocess_input_tensor(
@@ -124,7 +125,8 @@ Tensor accumulation_invoke(
         // TODO(#37807):
         // This is a temporary fix, the op needs to be refactored to apply the output directly to optional_out,
         // or pass in optional_out as a reference.
-        optional_out->tensor_attributes = wip_tensor.tensor_attributes;
+        using namespace tt::tt_metal::do_not_use;
+        do_not_use_update_mesh_tensor_storage(optional_out->device_tensor(), wip_tensor.device_tensor().get_storage());
     }
     return wip_tensor;
 }
