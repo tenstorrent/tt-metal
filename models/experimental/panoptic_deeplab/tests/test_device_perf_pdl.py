df1c27045fc3b62867cd138c224395add38fe1eb
dchenTT <dchen@tenstorrent.com>
2026-02-26 05:38:11 +0000

    #36444 binary_ng support bfloat8-/4_b generality
    More perf adjustment for testing purpose

--- models/experimental/panoptic_deeplab/tests/test_device_perf_pdl.py
+++ models/experimental/panoptic_deeplab/tests/test_device_perf_pdl.py
@@ -64,7 +64,7 @@ def test_device_perf_pdl_20_cores(
         ),
         (
             "pytest models/experimental/panoptic_deeplab/tests/pcc/test_tt_model.py::test_model_panoptic_deeplab -k deeplab_v3_plus_110_cores",
-            4_182_858,
+            4_380_000,
             DEEPLAB_V3_PLUS,
             DEEPLAB_V3_PLUS,
             1,
