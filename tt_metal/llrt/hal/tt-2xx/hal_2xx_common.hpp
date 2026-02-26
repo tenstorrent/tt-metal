b7f8b835847ba2b8b56493051fd37768f2ab8024
Rui Zhang <ruizhang@tenstorrent.com>
2026-02-26 11:18:17 -0800

    Decouple HalJitBuildQuery from MetalContext::instance() (#38606)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/38397
    
    ### Problem description
    It was a mistake to have HalJitBuildQuery depend on the global
    MetalContext::instance(). Specifically, this does not work for offline
    build, because we are constructing our own Hal and RunTimeOptions
    objects instead of inferring from the live device.
    
    ### What's changed
    - HalJitBuildQuery objects will store a reference to the Hal object that
    creates it.
    - rtoptions is now passed as a parameter to the query interface, for
    anything that depends on it.
    
    ### Checklist
    
    - [ ] [![All post-commit
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml/badge.svg?branch=ruizhang/decouple-hal-jit-build-query)](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml?query=branch:ruizhang/decouple-hal-jit-build-query)
    - [ ] [![Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml/badge.svg?branch=ruizhang/decouple-hal-jit-build-query)](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml?query=branch:ruizhang/decouple-hal-jit-build-query)
    - [ ]
    [![cpp-unit-tests](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml/badge.svg?branch=ruizhang/decouple-hal-jit-build-query)](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml?query=branch:ruizhang/decouple-hal-jit-build-query)
    - [ ] New/Existing tests provide coverage for changes

--- tt_metal/llrt/hal/tt-2xx/hal_2xx_common.hpp
+++ tt_metal/llrt/hal/tt-2xx/hal_2xx_common.hpp
@@ -10,10 +10,14 @@ namespace tt::tt_metal::hal_2xx {
 
 class HalJitBuildQueryBase : public HalJitBuildQueryInterface {
 public:
+    HalJitBuildQueryBase(const Hal& hal) : hal_(hal) {}
     std::vector<std::string> defines(const Params& params) const override;
     std::vector<std::string> srcs(const Params& params) const override;
     std::string target_name(const Params& params) const override;
     std::string weakened_firmware_target_name(const Params& params) const override;
+
+protected:
+    const Hal& hal_;
 };
 
 }  // namespace tt::tt_metal::hal_2xx
