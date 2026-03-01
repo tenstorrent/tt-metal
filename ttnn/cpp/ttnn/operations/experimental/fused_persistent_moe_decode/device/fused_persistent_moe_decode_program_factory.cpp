77aaf7c7829b10e21bf6dbda103126456f6c60f3
ttuser <ttuser@tt.local>
2026-03-01 13:13:09 +0000

    Implement blocked matmul and swiglu logic for fused_persistent_moe_decode compute kernel

--- ttnn/cpp/ttnn/operations/experimental/fused_persistent_moe_decode/device/fused_persistent_moe_decode_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/experimental/fused_persistent_moe_decode/device/fused_persistent_moe_decode_program_factory.cpp
@@ -30,17 +30,17 @@ ExecuteFusedPersistentMoeDecodeDeviceOperation::SingleCore::create(
     auto compute_with_storage_grid_size = device->compute_with_storage_grid_size();
     CoreRangeSet all_cores(CoreRange(CoreCoord(0, 0), CoreCoord(compute_with_storage_grid_size.x - 1, compute_with_storage_grid_size.y - 1)));
 
-    uint32_t in0_tile_size = input_tensor.buffer()->page_size();
-    uint32_t in0_num_tiles = input_tensor.buffer()->num_pages();
-    uint32_t out0_tile_size = output_tensor.buffer()->page_size();
+    uint32_t in0_tile_size = tt::tile_size(tt::tt_metal::datatype_to_dataformat_converter(input_tensor.dtype()));
+    uint32_t in0_num_tiles = input_tensor.logical_shape().volume() / tt::constants::TILE_HW;
+    uint32_t out0_tile_size = tt::tile_size(tt::tt_metal::datatype_to_dataformat_converter(output_tensor.dtype()));
 
     uint32_t cb_in0_index = tt::CB::c_in0;
-    CircularBufferConfig cb_in0_config = CircularBufferConfig(2 * in0_tile_size, {{cb_in0_index, tt::tt_metal::datatype_to_dataformat_converter(input_tensor.dtype())}})
+    CircularBufferConfig cb_in0_config = CircularBufferConfig(in0_num_tiles * in0_tile_size, {{cb_in0_index, tt::tt_metal::datatype_to_dataformat_converter(input_tensor.dtype())}})
         .set_page_size(cb_in0_index, in0_tile_size);
     auto cb_in0 = CreateCircularBuffer(program, all_cores, cb_in0_config);
 
     uint32_t cb_out0_index = tt::CB::c_out0;
-    CircularBufferConfig cb_out0_config = CircularBufferConfig(2 * out0_tile_size, {{cb_out0_index, tt::tt_metal::datatype_to_dataformat_converter(output_tensor.dtype())}})
+    CircularBufferConfig cb_out0_config = CircularBufferConfig(in0_num_tiles * out0_tile_size, {{cb_out0_index, tt::tt_metal::datatype_to_dataformat_converter(output_tensor.dtype())}})
         .set_page_size(cb_out0_index, out0_tile_size);
     auto cb_out0 = CreateCircularBuffer(program, all_cores, cb_out0_config);
 
@@ -49,47 +49,48 @@ ExecuteFusedPersistentMoeDecodeDeviceOperation::SingleCore::create(
     
     // w1_experts (c_in1)
     uint32_t cb_w1_index = tt::CB::c_in1;
-    uint32_t w1_tile_size = w1_experts.buffer()->page_size();
+    uint32_t w1_tile_size = tt::tile_size(tt::tt_metal::datatype_to_dataformat_converter(w1_experts.dtype()));
     CircularBufferConfig cb_w1_config = CircularBufferConfig(2 * num_weight_tiles * w1_tile_size, {{cb_w1_index, tt::tt_metal::datatype_to_dataformat_converter(w1_experts.dtype())}})
         .set_page_size(cb_w1_index, w1_tile_size);
     auto cb_w1 = CreateCircularBuffer(program, all_cores, cb_w1_config);
 
     // w3_experts (c_in2)
     uint32_t cb_w3_index = tt::CB::c_in2;
-    uint32_t w3_tile_size = w3_experts.buffer()->page_size();
+    uint32_t w3_tile_size = tt::tile_size(tt::tt_metal::datatype_to_dataformat_converter(w3_experts.dtype()));
     CircularBufferConfig cb_w3_config = CircularBufferConfig(2 * num_weight_tiles * w3_tile_size, {{cb_w3_index, tt::tt_metal::datatype_to_dataformat_converter(w3_experts.dtype())}})
         .set_page_size(cb_w3_index, w3_tile_size);
     auto cb_w3 = CreateCircularBuffer(program, all_cores, cb_w3_config);
 
     // w2_experts (c_in3)
     uint32_t cb_w2_index = tt::CB::c_in3;
-    uint32_t w2_tile_size = w2_experts.buffer()->page_size();
+    uint32_t w2_tile_size = tt::tile_size(tt::tt_metal::datatype_to_dataformat_converter(w2_experts.dtype()));
     CircularBufferConfig cb_w2_config = CircularBufferConfig(2 * num_weight_tiles * w2_tile_size, {{cb_w2_index, tt::tt_metal::datatype_to_dataformat_converter(w2_experts.dtype())}})
         .set_page_size(cb_w2_index, w2_tile_size);
     auto cb_w2 = CreateCircularBuffer(program, all_cores, cb_w2_config);
 
     // topk_expert_indices (c_in4)
     uint32_t cb_idx_index = tt::CB::c_in4;
-    uint32_t idx_tile_size = topk_expert_indices.buffer()->page_size();
+    uint32_t idx_tile_size = tt::tile_size(tt::tt_metal::datatype_to_dataformat_converter(topk_expert_indices.dtype()));
     CircularBufferConfig cb_idx_config = CircularBufferConfig(2 * idx_tile_size, {{cb_idx_index, tt::tt_metal::datatype_to_dataformat_converter(topk_expert_indices.dtype())}})
         .set_page_size(cb_idx_index, idx_tile_size);
     auto cb_idx = CreateCircularBuffer(program, all_cores, cb_idx_config);
 
     // topk_expert_weights (c_in5)
     uint32_t cb_wt_index = tt::CB::c_in5;
-    uint32_t wt_tile_size = topk_expert_weights.buffer()->page_size();
+    uint32_t wt_tile_size = tt::tile_size(tt::tt_metal::datatype_to_dataformat_converter(topk_expert_weights.dtype()));
     CircularBufferConfig cb_wt_config = CircularBufferConfig(2 * wt_tile_size, {{cb_wt_index, tt::tt_metal::datatype_to_dataformat_converter(topk_expert_weights.dtype())}})
         .set_page_size(cb_wt_index, wt_tile_size);
     auto cb_wt = CreateCircularBuffer(program, all_cores, cb_wt_config);
 
     // Intermediate CBs for math
     uint32_t cb_interm0_index = tt::CB::c_intermed0;
-    CircularBufferConfig cb_interm0_config = CircularBufferConfig(2 * out0_tile_size, {{cb_interm0_index, tt::tt_metal::datatype_to_dataformat_converter(output_tensor.dtype())}})
+    uint32_t interm_num_tiles = w1_experts.logical_shape()[3] / tt::constants::TILE_WIDTH; // 512 / 32 = 16 tiles
+    CircularBufferConfig cb_interm0_config = CircularBufferConfig(interm_num_tiles * out0_tile_size, {{cb_interm0_index, tt::tt_metal::datatype_to_dataformat_converter(output_tensor.dtype())}})
         .set_page_size(cb_interm0_index, out0_tile_size);
     auto cb_interm0 = CreateCircularBuffer(program, all_cores, cb_interm0_config);
 
     uint32_t cb_interm1_index = tt::CB::c_intermed1;
-    CircularBufferConfig cb_interm1_config = CircularBufferConfig(2 * out0_tile_size, {{cb_interm1_index, tt::tt_metal::datatype_to_dataformat_converter(output_tensor.dtype())}})
+    CircularBufferConfig cb_interm1_config = CircularBufferConfig(interm_num_tiles * out0_tile_size, {{cb_interm1_index, tt::tt_metal::datatype_to_dataformat_converter(output_tensor.dtype())}})
         .set_page_size(cb_interm1_index, out0_tile_size);
     auto cb_interm1 = CreateCircularBuffer(program, all_cores, cb_interm1_config);
 
