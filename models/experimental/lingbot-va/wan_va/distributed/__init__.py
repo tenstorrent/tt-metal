38fc4c56314ca2ad3b48172a380410b4bcd6ad32
ign-steve <stevegodly.mathew@ignitarium.com>
2026-03-02 07:26:42 +0000

    reference cleanup

--- models/experimental/lingbot-va/wan_va/distributed/__init__.py
+++
@@ -1,1 +0,0 @@
-# Copyright 2024-2025 The Alibaba Wan Team Authors. All rights reserved.
--- models/experimental/lingbot-va/wan_va/modules/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2024-2025 The Robbyant Team Authors. All rights reserved.
-from .utils import load_text_encoder, load_tokenizer, load_transformer, load_vae
-
-__all__ = ["load_transformer", "load_text_encoder", "load_tokenizer", "load_vae", "WanVAEStreamingWrapper"]
