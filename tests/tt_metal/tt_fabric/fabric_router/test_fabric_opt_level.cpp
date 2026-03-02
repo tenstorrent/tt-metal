05da1a4712fe3a4f7609c2a09b6aa2cd823ffc18
Sean Nijjar <sean.nijjar@gmail.com>
2026-03-02 18:56:16 +0000

    update tests

--- tests/tt_metal/tt_fabric/fabric_router/test_fabric_opt_level.cpp
+++ tests/tt_metal/tt_fabric/fabric_router/test_fabric_opt_level.cpp
@@ -5,6 +5,8 @@
 #include <gtest/gtest.h>
 #include <tt-metalium/kernel_types.hpp>
 #include "rtoptions.hpp"
+#include "fabric_fixture.hpp"
+#include "impl/context/metal_context.hpp"
 
 namespace tt::llrt {
 
@@ -95,4 +97,48 @@ TEST_F(FabricOptLevelTest, InvalidValueThrows) {
     EXPECT_ANY_THROW(create_rtoptions());
 }
 
+TEST_F(FabricOptLevelTest, SetterOverridesDefault) {
+    unsetenv("TT_METAL_FABRIC_OPT_LEVEL");
+    auto opts = create_rtoptions();
+    EXPECT_FALSE(opts.get_fabric_kernel_opt_level().has_value());
+
+    opts.set_fabric_kernel_opt_level(tt::tt_metal::KernelBuildOptLevel::Os);
+    ASSERT_TRUE(opts.get_fabric_kernel_opt_level().has_value());
+    EXPECT_EQ(opts.get_fabric_kernel_opt_level().value(), tt::tt_metal::KernelBuildOptLevel::Os);
+}
+
+TEST_F(FabricOptLevelTest, SetterClearsOverride) {
+    setenv("TT_METAL_FABRIC_OPT_LEVEL", "O3", 1);
+    auto opts = create_rtoptions();
+    ASSERT_TRUE(opts.get_fabric_kernel_opt_level().has_value());
+
+    opts.set_fabric_kernel_opt_level(std::nullopt);
+    EXPECT_FALSE(opts.get_fabric_kernel_opt_level().has_value());
+}
+
 }  // namespace tt::llrt
+
+namespace tt::tt_fabric::fabric_router_tests {
+
+class FabricOptLevelTest_Fabric1DWithOsOverride : public BaseFabricFixture {
+protected:
+    static void SetUpTestSuite() {
+        tt::tt_metal::MetalContext::instance().rtoptions().set_fabric_kernel_opt_level(
+            tt::tt_metal::KernelBuildOptLevel::Os);
+        BaseFabricFixture::DoSetUpTestSuite(tt::tt_fabric::FabricConfig::FABRIC_1D);
+    }
+    static void TearDownTestSuite() {
+        BaseFabricFixture::DoTearDownTestSuite();
+        tt::tt_metal::MetalContext::instance().rtoptions().set_fabric_kernel_opt_level(std::nullopt);
+    }
+};
+
+TEST_F(FabricOptLevelTest_Fabric1DWithOsOverride, UnicastRawWithOsOverride) {
+    auto opt_level = tt::tt_metal::MetalContext::instance().rtoptions().get_fabric_kernel_opt_level();
+    ASSERT_TRUE(opt_level.has_value());
+    EXPECT_EQ(opt_level.value(), tt::tt_metal::KernelBuildOptLevel::Os);
+
+    RunTestUnicastRaw(this, 1, RoutingDirection::E);
+}
+
+}  // namespace tt::tt_fabric::fabric_router_tests
