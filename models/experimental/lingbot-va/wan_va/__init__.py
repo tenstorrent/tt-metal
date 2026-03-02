49aed9f1125743da90ede074f7f4eebb3167b2b0
ign-steve <stevegodly.mathew@ignitarium.com>
2026-03-02 07:26:42 +0000

    reference cleanup

--- models/experimental/lingbot-va/wan_va/__init__.py
+++
@@ -1,2 +0,0 @@
-# Copyright 2024-2025 The Alibaba Wan Team Authors. All rights reserved.
-from . import distributed, modules
--- models/experimental/lingbot-va/wan_va/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .utils import get_mesh_id
-
-__all__ = ["get_mesh_id"]
