1bedcc1d0db588e91a3c7e3af59f4b111f7f7dd3
Aliaksei Sala <ctr-asala@ext.tenstorrent.com>
2026-03-03 12:31:38 -0800

    Remove restriction for the op which is no longer needed

--- ttnn/cpp/ttnn/operations/embedding/device/embedding_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/embedding/device/embedding_device_operation.cpp
@@ -94,10 +94,6 @@ void EmbeddingsDeviceOperation::validate_on_program_cache_miss(
 
     if (!tensor_args.input_tensor_arg.nd_shard_spec().has_value() &&
         !operation_attributes.output_mem_config.nd_shard_spec().has_value()) {
-        TT_FATAL(
-            a.padded_shape().rank() < 3 || a.padded_shape().rank() == 4,
-            "Input tensor must be 1D,2D or 4D, got {}",
-            a.padded_shape().rank());
         if (a.layout() == Layout::ROW_MAJOR && a.padded_shape().rank() == 4) {
             TT_FATAL(
                 a.padded_shape()[1] == 1 && a.padded_shape()[2] == 1,
