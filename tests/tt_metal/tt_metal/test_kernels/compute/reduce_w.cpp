dbefcd1e5bc97dda1239bfcfac2df30b530d0e47
Ana Mokan <amokan@tenstorrent.com>
2026-03-04 14:33:05 +0000

    Add dfbs to compute, reader, writer tests

--- tests/tt_metal/tt_metal/test_kernels/compute/reduce_w.cpp
+++ tests/tt_metal/tt_metal/test_kernels/compute/reduce_w.cpp
@@ -5,18 +5,30 @@
 #include <cstdint>
 
 #include "api/compute/reduce.h"
+#ifdef ARCH_QUASAR
+#include "experimental/dataflow_buffer.h"
+#else
 #include "experimental/circular_buffer.h"
+#endif
 
 void kernel_main() {
     constexpr uint32_t Ht = get_compile_time_arg_val(0);
     constexpr uint32_t Wt = get_compile_time_arg_val(1);
     constexpr uint32_t NC = get_compile_time_arg_val(2);
-    compute_kernel_hw_startup(tt::CBIndex::c_0, tt::CBIndex::c_2, tt::CBIndex::c_16);
-    reduce_init(tt::CBIndex::c_0, tt::CBIndex::c_2, tt::CBIndex::c_16);
 
+#ifdef ARCH_QUASAR
+    experimental::DataflowBuffer dfb_in(0);
+    experimental::DataflowBuffer dfb_in_scaler(2);
+    experimental::DataflowBuffer dfb_out(16);
+    compute_kernel_hw_startup(dfb_in.get_id(), dfb_in_scaler.get_id(), dfb_out.get_id());
+    reduce_init(dfb_in.get_id(), dfb_in_scaler.get_id(), dfb_out.get_id());
+#else
     experimental::CircularBuffer cb0(tt::CBIndex::c_0);
     experimental::CircularBuffer cb2(tt::CBIndex::c_2);
     experimental::CircularBuffer cb16(tt::CBIndex::c_16);
+    compute_kernel_hw_startup(tt::CBIndex::c_0, tt::CBIndex::c_2, tt::CBIndex::c_16);
+    reduce_init(tt::CBIndex::c_0, tt::CBIndex::c_2, tt::CBIndex::c_16);
+#endif
 
     cb2.wait_front(1);  // scaler tile from the reader
     for (uint32_t nc = 0; nc < NC; nc++) {
@@ -28,6 +40,18 @@ void kernel_main() {
             // in this case we just sequentially add to accumulator all the W-tiles in a row
             acquire_dst();
             for (uint32_t wt = 0; wt < Wt; ++wt) {
+#ifdef ARCH_QUASAR
+                dfb_in.wait_front(onetile);
+#if (MATH_ONLY == 1)
+                UNPACK((llk_unpack_AB_reduce(dfb_in.get_id(), dfb_in_scaler.get_id(), 0, 0)));
+                // REDUCE_OP is expected to come from add_define
+                reduce_tile_math(reduce_dst_idx);
+#elif (MATH_ONLY == 0)
+                // REDUCE_OP is expected to come from add_define
+                reduce_tile(dfb_in.get_id(), dfb_in_scaler.get_id(), 0, 0, reduce_dst_idx);
+#endif
+                dfb_in.pop_front(onetile);
+#else
                 cb0.wait_front(onetile);
 #if (MATH_ONLY == 1)
                 UNPACK((llk_unpack_AB_reduce(tt::CBIndex::c_0, tt::CBIndex::c_2, 0, 0)));
@@ -38,11 +62,17 @@ void kernel_main() {
                 reduce_tile(tt::CBIndex::c_0, tt::CBIndex::c_2, 0, 0, reduce_dst_idx);
 #endif
                 cb0.pop_front(onetile);
+#endif
             }
-
+#ifdef ARCH_QUASAR
+            dfb_out.reserve_back(onetile);
+            pack_tile(reduce_dst_idx, dfb_out.get_id());
+            dfb_out.push_back(onetile);
+#else
             cb16.reserve_back(onetile);
             pack_tile(reduce_dst_idx, tt::CBIndex::c_16);
             cb16.push_back(onetile);
+#endif
             release_dst();
         }
     }
