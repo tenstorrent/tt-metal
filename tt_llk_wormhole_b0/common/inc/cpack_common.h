9b5c8730e4c7e22b2c0ff33397a9e107c0e68653
Strahinja Stanisic <sstanisic@tenstorrent.com>
2026-03-04 10:35:49 +0100

    fix :bug:: exp_threshold cross kernel leak (#1346)
    ### Ticket
    
    ### Problem description
    Exponent threshold state is not properly set/reset by
    configure/reconfigure leading to possible precision loss in the
    following kernel/call (by keeping EXPA threshold in the following EXPB
    call leading to datums being flushed to 0)
    
    ### What's changed
    <!-- Describe the approach used to solve the problem.
    Summarize the changes made and its impact. -->
    
    ### Type of change
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    - [ ] Refactoring
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    - [ ] [Assert
    validation](https://github.com/tenstorrent/tt-llk/blob/main/docs/Introduction_to_asserts.md)
    Complied with assert doc (if applicable)

--- tt_llk_wormhole_b0/common/inc/cpack_common.h
+++ tt_llk_wormhole_b0/common/inc/cpack_common.h
@@ -246,10 +246,13 @@ inline void set_packer_config(
     config.f.in_data_format    = pack_src_format;
     config.f.pack_per_xy_plane = 1;
 
+    config.f.exp_threshold_en = 0;
+    config.f.exp_threshold    = 0;
+
     // Workaround for bug in HW: tenstorrent/budabackend#1394
     if constexpr (is_fp32_dest_acc_en)
     {
-        if (IS_A_FORMAT(pack_dst_format))
+        if (IS_BFP_A_FORMAT(pack_dst_format))
         {
             config.f.exp_threshold_en = 1;
             config.f.exp_threshold    = 113;
@@ -484,28 +487,25 @@ inline void reconfig_packer_data_format(
 
     TT_SETDMAREG(0, LOWER_HALFWORD(tile_size), 0, LO_16(p_gpr_pack::TILE_HEADER));
 
+    config.val[3]             = 0;
+    config.f.exp_threshold_en = 0;
+    config.f.exp_threshold    = 0;
+
     // Workaround for HW bug: tenstorrent/budabackend#1394
     if constexpr (is_fp32_dest_acc_en)
     {
         if (IS_BFP_A_FORMAT(pack_dst_format))
         {
-            config.val[3]             = 0; // Only need to modify word[2][15:0]
             config.f.exp_threshold_en = 1;
             config.f.exp_threshold    = 113;
-            TT_SETDMAREG(0, UPPER_HALFWORD(config.val[3]), 0, HI_16(p_gpr_pack::TMP_HI));
-            TTI_REG2FLOP(1, 0, 0, 0, THCON_SEC0_REG1_Row_start_section_size_ADDR32 + 3 - THCON_CFGREG_BASE_ADDR32, p_gpr_pack::TMP_HI);
-            TTI_REG2FLOP(1, 0, 0, 0, THCON_SEC1_REG1_Row_start_section_size_ADDR32 + 3 - THCON_CFGREG_BASE_ADDR32, p_gpr_pack::TMP_HI);
-            TTI_REG2FLOP(1, 0, 0, 0, THCON_SEC0_REG8_Row_start_section_size_ADDR32 + 3 - THCON_CFGREG_BASE_ADDR32, p_gpr_pack::TMP_HI);
-            TTI_REG2FLOP(1, 0, 0, 0, THCON_SEC1_REG8_Row_start_section_size_ADDR32 + 3 - THCON_CFGREG_BASE_ADDR32, p_gpr_pack::TMP_HI);
-        }
-        else
-        {
-            TTI_REG2FLOP(1, 0, 0, 0, THCON_SEC0_REG1_Row_start_section_size_ADDR32 + 3 - THCON_CFGREG_BASE_ADDR32, p_gpr::ZERO);
-            TTI_REG2FLOP(1, 0, 0, 0, THCON_SEC1_REG1_Row_start_section_size_ADDR32 + 3 - THCON_CFGREG_BASE_ADDR32, p_gpr::ZERO);
-            TTI_REG2FLOP(1, 0, 0, 0, THCON_SEC0_REG8_Row_start_section_size_ADDR32 + 3 - THCON_CFGREG_BASE_ADDR32, p_gpr::ZERO);
-            TTI_REG2FLOP(1, 0, 0, 0, THCON_SEC1_REG8_Row_start_section_size_ADDR32 + 3 - THCON_CFGREG_BASE_ADDR32, p_gpr::ZERO);
         }
     }
+
+    TT_SETDMAREG(0, UPPER_HALFWORD(config.val[3]), 0, HI_16(p_gpr_pack::TMP_HI));
+    TTI_REG2FLOP(1, 0, 0, 0, THCON_SEC0_REG1_Row_start_section_size_ADDR32 + 3 - THCON_CFGREG_BASE_ADDR32, p_gpr_pack::TMP_HI);
+    TTI_REG2FLOP(1, 0, 0, 0, THCON_SEC1_REG1_Row_start_section_size_ADDR32 + 3 - THCON_CFGREG_BASE_ADDR32, p_gpr_pack::TMP_HI);
+    TTI_REG2FLOP(1, 0, 0, 0, THCON_SEC0_REG8_Row_start_section_size_ADDR32 + 3 - THCON_CFGREG_BASE_ADDR32, p_gpr_pack::TMP_HI);
+    TTI_REG2FLOP(1, 0, 0, 0, THCON_SEC1_REG8_Row_start_section_size_ADDR32 + 3 - THCON_CFGREG_BASE_ADDR32, p_gpr_pack::TMP_HI);
 
     cfg_reg_rmw_tensix<ALU_FORMAT_SPEC_REG2_Dstacc_RMW>(pack_src_format);
 
