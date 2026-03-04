d3acb40fe950ddea37eff1206a8243a279a73fcc
Maxim Artemov <ctr-martemov@ext.tenstorrent.com>
2026-03-04 09:03:27 +0000

    code review comments

--- ttnn/cpp/ttnn/operations/loss/loss.hpp
+++ ttnn/cpp/ttnn/operations/loss/loss.hpp
@@ -11,20 +11,17 @@
 #include "ttnn/decorators.hpp"
 
 namespace ttnn {
-
-using operations::loss::LossReductionMode;
-
 Tensor mse_loss(
     const Tensor& ref,
     const Tensor& prediction,
-    LossReductionMode mode = LossReductionMode::NONE,
+    operations::loss::LossReductionMode mode = operations::loss::LossReductionMode::NONE,
     const std::optional<MemoryConfig>& memory_config = std::nullopt,
     const std::optional<Tensor>& optional_output_tensor = std::nullopt);
 
 Tensor l1_loss(
     const Tensor& ref,
     const Tensor& prediction,
-    LossReductionMode mode = LossReductionMode::NONE,
+    operations::loss::LossReductionMode mode = operations::loss::LossReductionMode::NONE,
     const std::optional<MemoryConfig>& memory_config = std::nullopt,
     const std::optional<Tensor>& optional_output_tensor = std::nullopt);
 
