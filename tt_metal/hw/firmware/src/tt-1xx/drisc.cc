681f63ebf26605f2d88426dc8a149ddfdab0dd85
John Bauman <jbauman@tenstorrent.com>
2026-03-05 21:20:43 +0000

    initial kernel support commit

--- tt_metal/hw/firmware/src/tt-1xx/drisc.cc
+++ tt_metal/hw/firmware/src/tt-1xx/drisc.cc
@@ -30,6 +30,15 @@ uint16_t l1_bank_to_noc_xy[NUM_NOCS][NUM_L1_BANKS] __attribute__((used));
 int32_t bank_to_dram_offset[NUM_DRAM_BANKS] __attribute__((used));
 int32_t bank_to_l1_offset[NUM_L1_BANKS] __attribute__((used));
 
+uint32_t tt_l1_ptr* rta_l1_base __attribute__((used));
+uint32_t tt_l1_ptr* crta_l1_base __attribute__((used));
+uint32_t tt_l1_ptr* sem_l1_base[ProgrammableCoreType::COUNT] __attribute__((used));
+uint32_t rta_count __attribute__((used));
+uint32_t crta_count __attribute__((used));
+
+uint8_t worker_logical_col_to_virtual_col[round_up_to_mult_of_4(noc_size_x)] __attribute__((used));
+uint8_t worker_logical_row_to_virtual_row[round_up_to_mult_of_4(noc_size_y)] __attribute__((used));
+
 tt_l1_ptr mailboxes_t* const mailboxes = (tt_l1_ptr mailboxes_t*)(MEM_DRISC_MAILBOX_BASE);
 
 int main() {
@@ -59,7 +68,25 @@ int main() {
         }
         WAYPOINT("GD");
 
+        uint32_t launch_msg_rd_ptr = mailboxes->launch_msg_rd_ptr;
+        launch_msg_t* launch_msg = &mailboxes->launch[launch_msg_rd_ptr];
+
+        firmware_config_init(mailboxes, ProgrammableCoreType::DRAM, 0);
+
+        uint32_t kernel_lma = launch_msg->kernel_config.kernel_text_offset[0];
+        invalidate_l1_cache();
+        flush_erisc_icache();
+
+        WAYPOINT("R");
+        reinterpret_cast<uint32_t (*)()>(kernel_lma)();
+        WAYPOINT("D");
+
         mailboxes->go_messages[0].signal = RUN_MSG_DONE;
+
+        if (launch_msg->kernel_config.mode == DISPATCH_MODE_DEV) {
+            launch_msg->kernel_config.enables = 0;
+            mailboxes->launch_msg_rd_ptr = (launch_msg_rd_ptr + 1) & (launch_msg_buffer_num_entries - 1);
+        }
         WAYPOINT("GW");
     }
 
