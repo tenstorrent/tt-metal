5d222f058ad420f8a5be5a1ac3b2810bbbdc7756
Никола Радојевић <nikola.radojevic@outlogix.com>
2026-03-06 17:21:31 +0100

    Handle alignment in the L1 allocator

--- tests/tt_metal/tt_metal/deployment/deployment_common.hpp
+++ tests/tt_metal/tt_metal/deployment/deployment_common.hpp
@@ -1,12 +1,18 @@
 #ifndef _DEPLOYMENT_COMMON_H
 #define _DEPLOYMENT_COMMON_H
 
+#include "tt_metal/api/tt-metalium/hal.hpp"
 #include "command_queue_fixture.hpp"
 
 struct l1_allocator {
     uint32_t start;
     uint32_t end;
 };
+
+#define ROUND_UP(x, a) ((((x) + (a) - 1) / (a)) * (a))
+#define ROUND_DOWN(x, a) (((x) / (a)) * (a))
+
+#define ALIGNMENT (tt::tt_metal::hal::get_l1_alignment())
 
 static inline struct l1_allocator new_tensix_allocator() {
     using namespace tt::tt_metal;
@@ -18,8 +24,8 @@ static inline struct l1_allocator new_tensix_allocator() {
         MetalContext::instance().hal().get_dev_size(HalProgrammableCoreType::TENSIX, HalL1MemAddrType::UNRESERVED);
 
     return (struct l1_allocator){
-        .start = start,
-        .end = end,
+        .start = ROUND_UP(start, ALIGNMENT),
+        .end = ROUND_DOWN(end, ALIGNMENT),
     };
 }
 
@@ -33,12 +39,14 @@ static inline struct l1_allocator new_erisc_allocator() {
         MetalContext::instance().hal().get_dev_size(HalProgrammableCoreType::ACTIVE_ETH, HalL1MemAddrType::UNRESERVED);
 
     return (struct l1_allocator){
-        .start = start,
-        .end = end,
+        .start = ROUND_UP(start, ALIGNMENT),
+        .end = ROUND_DOWN(end, ALIGNMENT),
     };
 }
 
 static inline uint32_t l1_alloc(struct l1_allocator& alloc, uint32_t size) {
+    size = ROUND_UP(size, ALIGNMENT);
+
     TT_FATAL(alloc.start + size <= alloc.end, "Couldn't allocate in L1");
 
     uint32_t ret = alloc.start;
