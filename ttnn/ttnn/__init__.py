e99053b242ecac07958cfc149d871006b521493b
Edwin Lee <edwinlee@tenstorrent.com>
2026-03-06 19:42:13 +0000

    Add new helper function for interleaved use cases

--- ttnn/ttnn/__init__.py
+++ ttnn/ttnn/__init__.py
@@ -425,6 +425,7 @@ from ttnn.operations.normalization import (
     create_group_norm_reciprocals,
     create_layer_norm_reciprocals,
     determine_expected_group_norm_sharded_config_and_grid_size,
+    determine_expected_group_norm_dram_grid_size,
     dram_group_norm_params_from_torch,
     layernorm_default_compute_config,
     rmsnorm_default_compute_config,
