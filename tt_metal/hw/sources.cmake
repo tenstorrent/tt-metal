21bde9f96bddb34d5cc25952ca284deef63062c1
Vuk Jovanovic <vjovanovic@tenstorrent.com>
2026-03-06 15:15:12 +0000

    Add device print headers to JIT API sources

--- tt_metal/hw/sources.cmake
+++ tt_metal/hw/sources.cmake
@@ -7,6 +7,7 @@ set(HW_JIT_API_HEADERS
     inc/api/debug/assert.h
     inc/api/debug/dprint.h
     inc/api/debug/timing_perturbation.h
+    inc/api/debug/device_print.h
     inc/api/debug/dprint_pages.h
     inc/api/debug/dprint_tensix.h
     inc/api/debug/dprint_tile.h
