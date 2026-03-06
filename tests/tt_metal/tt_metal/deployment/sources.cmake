4a250ffee62f395b2b43dd7031b49fc5f0a9df43
Никола Радојевић <nikola.radojevic@outlogix.com>
2026-03-06 16:53:51 +0100

    Add the "link up" test

--- tests/tt_metal/tt_metal/deployment/sources.cmake
+++ tests/tt_metal/tt_metal/deployment/sources.cmake
@@ -1,4 +1,4 @@
 # Source files for tt_metal deployment tests
 # Module owners should update this file when adding/removing/renaming source files
 
-set(UNIT_TESTS_DEPLOYMENT_SRC test.cpp)
+set(UNIT_TESTS_DEPLOYMENT_SRC eth/test_eth_link_up.cpp)
