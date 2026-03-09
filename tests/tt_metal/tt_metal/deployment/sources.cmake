dc0f44f8aea25867de70d90aa00b47907ff97fcf
Никола Радојевић <nikola.radojevic@outlogix.com>
2026-03-09 14:36:41 +0100

    Add ethernet bandwidth deployment test

--- tests/tt_metal/tt_metal/deployment/sources.cmake
+++ tests/tt_metal/tt_metal/deployment/sources.cmake
@@ -1,4 +1,7 @@
 # Source files for tt_metal deployment tests
 # Module owners should update this file when adding/removing/renaming source files
 
-set(UNIT_TESTS_DEPLOYMENT_SRC eth/test_eth_link_up.cpp)
+set(UNIT_TESTS_DEPLOYMENT_SRC
+    eth/test_eth_link_up.cpp
+    eth/test_eth_bandwidth.cpp
+)
