dba17689de27d68cfb1a2f94dc761a2c4bbcd8c6
TTvvukomanovic <vvukomanovic@tenstorrent.com>
2026-03-09 18:46:29 +0000

    Add noc_init call for all dm cores

--- tt_metal/hw/firmware/src/tt-2xx/dmk.cc
+++ tt_metal/hw/firmware/src/tt-2xx/dmk.cc
@@ -46,7 +46,7 @@ uint32_t _start() {
     do_thread_crt1(__tdata_lma);
 
     if constexpr (NOC_MODE == DM_DEDICATED_NOC) {
-        // noc_local_state_init(NOC_INDEX); //TODO revisit this
+        noc_init(MEM_NOC_ATOMIC_RET_VAL_ADDR);
     }
 #ifdef ALIGN_LOCAL_CBS_TO_REMOTE_CBS
     ALIGN_LOCAL_CBS_TO_REMOTE_CBS
