d4149610754f6fae98a124bf19c4fcf0b26a1b36
Adrian F. Morrison <amorrison@tenstorrent.com>
2026-03-10 16:17:31 +0000

    PR comments

--- ttnn/cpp/ttnn/operations/ccl/common/kernels/moe_utils.hpp
+++ ttnn/cpp/ttnn/operations/ccl/common/kernels/moe_utils.hpp
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 //
 // SPDX-License-Identifier: Apache-2.0
-#include <ranges>
 #include <tuple>
 
 #include "tt_metal/fabric/hw/inc/tt_fabric_api.h"
