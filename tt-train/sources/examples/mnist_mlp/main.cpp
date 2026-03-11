457a4604dd4fbf292935633c39dda95dfe951221
Adriel Bustamante <abustamante@tenstorrent.com>
2026-03-11 23:31:39 +0000

    Revert "Updated default path in MNIST example"
    This reverts commit fde2b96eac101568dc9894b039daceb450612acb.

--- tt-train/sources/examples/mnist_mlp/main.cpp
+++ tt-train/sources/examples/mnist_mlp/main.cpp
@@ -152,7 +152,7 @@ int main(int argc, char **argv) {
     CLI::App app{"Mnist Example"};
     argv = app.ensure_utf8(argv);
 
-    std::string config_name = "mnist.yaml";
+    std::string config_name = std::string(CONFIGS_FOLDER) + "/training_mnist_mlp.yaml";
     bool is_eval = false;
     bool enable_tp = false;
     app.add_option("-c,--config", config_name, "Training config name (resolved from configs/training_configs/)")
