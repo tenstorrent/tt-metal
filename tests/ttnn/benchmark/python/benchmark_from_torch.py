5336496fb42e766cd710776fe7380c0ca51d0ded
Aliaksei Sala <ctr-asala@ext.tenstorrent.com>
2026-03-11 16:31:49 -0700

    Split from_torch and to_torch benchmarks into separate files

--- tests/ttnn/benchmark/python/benchmark_from_torch.py
+++ tests/ttnn/benchmark/python/benchmark_from_torch.py
@@ -7,39 +7,6 @@ import ttnn
 import pytest
 
 
-def test_benchmark_from_torch_zero_copy(benchmark):
-    # Zero copy from_torch: row_major + physical_shape == logical_shape2d + bfloat16->bfloat16
-    torch_tensor = torch.rand((8096, 8096), dtype=torch.bfloat16)
-
-    def from_torch():
-        ttnn.from_torch(torch_tensor, layout=ttnn.ROW_MAJOR_LAYOUT, dtype=ttnn.bfloat16)
-
-    benchmark.pedantic(from_torch, iterations=100, rounds=10, warmup_rounds=1)
-
-
-def test_benchmark_from_torch_one_copy(benchmark):
-    # One copy from_torch tile layout + physical_shape == logical_shape2d + bfloat16->bfloat16
-    # - Copy on tilizing
-    torch_tensor = torch.rand((8096, 8096), dtype=torch.bfloat16)
-
-    def from_torch():
-        ttnn.from_torch(torch_tensor, layout=ttnn.TILE_LAYOUT, dtype=ttnn.bfloat16)
-
-    benchmark.pedantic(from_torch, iterations=10, rounds=5, warmup_rounds=1)
-
-
-def test_benchmark_from_torch_two_copy(benchmark):
-    # Two copied from_torch tile layout + physical_shape != logical_shape2d + bfloat16->bfloat16
-    # - First copy on padding to tile_size
-    # - Second copy on tilizing
-    torch_tensor = torch.rand((8096, 8100), dtype=torch.bfloat16)
-
-    def from_torch():
-        ttnn.from_torch(torch_tensor, layout=ttnn.TILE_LAYOUT, dtype=ttnn.bfloat16)
-
-    benchmark.pedantic(from_torch, iterations=10, rounds=5, warmup_rounds=1)
-
-
 TORCH_BENCH_TYPES = [
     torch.float16,
     torch.bfloat16,
@@ -58,15 +25,30 @@ TTNN_BENCH_TYPES = [
     ttnn.int32,
 ]
 
+TORCH_TO_TTNN = {
+    torch.float32: ttnn.float32,
+    torch.bfloat16: ttnn.bfloat16,
+    torch.uint32: ttnn.uint32,
+    torch.int32: ttnn.int32,
+    torch.uint16: ttnn.uint16,
+    torch.uint8: ttnn.uint8,
+}
+
 
 @pytest.mark.parametrize("use_device", [True, False])
 @pytest.mark.parametrize("ttnn_layout", [ttnn.TILE_LAYOUT, ttnn.ROW_MAJOR_LAYOUT])
 @pytest.mark.parametrize("torch_dtype", TORCH_BENCH_TYPES)
 @pytest.mark.parametrize("ttnn_dtype", TTNN_BENCH_TYPES)
-@pytest.mark.parametrize("size", [8192])
-def test_benchmark_from_torch(benchmark, device, use_device, ttnn_dtype, torch_dtype, ttnn_layout, size, request):
+@pytest.mark.parametrize("size", [1024])
+def test_benchmark_from_torch(benchmark, device, use_device, ttnn_dtype, torch_dtype, ttnn_layout, size):
     if ttnn_layout == ttnn.ROW_MAJOR_LAYOUT and ttnn_dtype in [ttnn.bfloat8_b, ttnn.bfloat4_b]:
         pytest.skip("ROW_MAJOR_LAYOUT not supported with bfloat8_b/bfloat4_b")
+
+    if ttnn_layout == ttnn.ROW_MAJOR_LAYOUT and ttnn_dtype == TORCH_TO_TTNN.get(torch_dtype, None):
+        pytest.skip(
+            "Don't benchmark tensors which are already in the correct layout and dtype.(borrowed data + data transfer)"
+        )
+
     if torch_dtype in [torch.int32, torch.uint8, torch.int64]:
         torch_input_tensor = torch.randint(0, 100, (size, size), dtype=torch_dtype)
     else:
@@ -78,6 +60,7 @@ def test_benchmark_from_torch(benchmark, device, use_device, ttnn_dtype, torch_d
             device=device if use_device else None,
             dtype=ttnn_dtype,
             layout=ttnn_layout,
+            fast_approx=True,
         )
 
         if not use_device:
@@ -86,28 +69,3 @@ def test_benchmark_from_torch(benchmark, device, use_device, ttnn_dtype, torch_d
         ttnn.synchronize_device(device)
 
     benchmark.pedantic(from_torch, iterations=10, rounds=2, warmup_rounds=1)
-
-
-@pytest.mark.parametrize("use_device", [True, False])
-@pytest.mark.parametrize("ttnn_layout", [ttnn.TILE_LAYOUT, ttnn.ROW_MAJOR_LAYOUT])
-@pytest.mark.parametrize("torch_dtype", TORCH_BENCH_TYPES)
-@pytest.mark.parametrize("ttnn_dtype", TTNN_BENCH_TYPES)
-@pytest.mark.parametrize("size", [8192])
-def test_benchmark_to_torch(benchmark, device, use_device, ttnn_dtype, torch_dtype, size, ttnn_layout):
-    if ttnn_layout == ttnn.ROW_MAJOR_LAYOUT and ttnn_dtype in [ttnn.bfloat8_b, ttnn.bfloat4_b]:
-        pytest.skip("ROW_MAJOR_LAYOUT not supported with bfloat8_b/bfloat4_b")
-
-    match ttnn_dtype:
-        case ttnn.int32 | ttnn.uint8:
-            tmp_torch = torch.randint(0, 100, (size, size), dtype=torch.int32)
-
-        case _:
-            tmp_torch = torch.rand(size, size, dtype=torch.float32)
-
-    ttnn_input_tensor = ttnn.from_torch(tmp_torch, device=device, dtype=ttnn_dtype, layout=ttnn_layout)
-
-    def to_torch():
-        ttnn.to_torch(ttnn_input_tensor, device=device if use_device else None, dtype=torch_dtype)
-        ttnn.synchronize_device(device)
-
-    benchmark.pedantic(to_torch, iterations=10, rounds=2, warmup_rounds=1)
