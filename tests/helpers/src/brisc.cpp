db95342b45ae8ac3011eb17fc25962ceb4a036fa
Andreja Jankovic <ajankovic@tenstorrent.com>
2026-03-12 17:07:33 +0100

    [infra.] Refactored C-runtime (#1452)
    ### What's changed
    Refactore the way the variants are compiled. Before we had C-runtime,
    trisc.cpp and test kernel compiled separately, now the are all compiled
    with a single invokation of compilation toolchain.
    - C runtime is moved from `tmu-crt0.S` to `do_crt0()` function in
    `boot.h`
    - `test_name.cpp` and `trisc.cpp` are merged together like this 
    ```cpp
    #include <sources/test_name.cpp>
    #include <trisc.cpp>
    ```
    in a temporary string that is sent to `g++`'s `stdin`.
    This is why we don't need `extern void run_kernel(RUNTIME_PARAMETERS
    params)` anymore.
    - `-I` flags are updated to include relevant folders for resolution of
    included `*.cpp` files.
    - `RUNTIME_PARAMETERS` macro is introduced to encapsuate runtime
    parameter struct type. This is done so in new PRs when we want to tinker
    with that type we don't to manualy change it in every test file. This
    will be propagated to other tests in PRs following this one.
    - `topk_test.cpp` is modified so unused variables are not there anymore.
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    - [x] Refactoring
    
    ### Checklist
    <!-- These are required steps and need to be run from tt-metal
    repository's Actions. Use links below and replace them with your run -->
    - [ ] [All post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/all-post-commit-workflows.yaml)
    CI passes
    - [ ] [Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml)
    CI passes (if applicable)
    - [ ] [Assert
    validation](https://github.com/tenstorrent/tt-llk/blob/main/docs/Introduction_to_asserts.md)
    Complied with assert doc (if applicable)

--- tests/helpers/src/brisc.cpp
+++ tests/helpers/src/brisc.cpp
@@ -8,12 +8,17 @@
 #include "boot.h"
 #endif
 
-int main()
+extern "C" __attribute__((section(".init"), naked, noreturn)) std::uint32_t _start()
 {
+    do_crt0();
 #ifdef LLK_BOOT_MODE_BRISC
     device_setup();
 
     // Release reset of triscs here in order to achieve brisc <-> trisc synchronization
     clear_trisc_soft_reset();
 #endif
+
+    for (;;)
+    {
+    } // Loop forever
 }
