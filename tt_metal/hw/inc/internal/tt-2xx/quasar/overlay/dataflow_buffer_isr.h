7d7b5cfc6abb4296943b0a8c307b1fd5fb7927af
Almeet Bhullar <109364210+abhullar-tt@users.noreply.github.com>
2026-03-13 13:40:51 -0700

    [DFB Feature]: Enable implicit sync method to post/ack credits from DMs (#39690)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/36891
    
    ### What's changed
    Quasar DMs can automatically post/ack tile counter credits using
    interrupts on transaction ids. This pr introduces that support.
    Added new "implicit sync" read/write api on dataflow buffer and
    dfb_txn_id_descriptor_t to set up transaction ids + isr. These allow
    caller to issue a noc read into/noc write out of the dfb's backing
    memory. When a specified threshold (num_entries_to_process_threshold) of
    reads land into the buffer/writes are sent for a particular transaction
    id, an ISR fires to post/ack credits to tile counters spanning that
    region of the dfb.
    
    Added a global array that tells isr which tile counters need to be
    updated (and with how many credits) when a particular txn ID threshold
    is met.
    
    Currently only use two transaction ids per DFB and only DM0 (capable of
    fast context switch) services the interrupts for all transactions. In
    the future we can play around with distributing this handling per txnID
    or per DFB across DMs
    
    Note: HW doesn't expose "pending tiles to post" so the dfb class tracks
    how many credits should have been posted. Details about this issue
    [here](https://docs.google.com/document/d/1L2DT0flwW8ENmOc5OvaZoua286Inoq3v8jexCdL6w0s/edit?tab=t.0#heading=h.os8orrgqzptp)
    
    ### Checklist
    
    - [x] [![Sanity
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/sanity-tests.yaml/badge.svg?branch=abhullar/dfb-is)](https://github.com/tenstorrent/tt-metal/actions/workflows/sanity-tests.yaml?query=branch:abhullar/dfb-is)
    - [ ] [![Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml/badge.svg?branch=abhullar/dfb-is)](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml?query=branch:abhullar/dfb-is)
    - [ ]
    [![cpp-unit-tests](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml/badge.svg?branch=abhullar/dfb-is)](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml?query=branch:abhullar/dfb-is)
    - [x] New/Existing tests provide coverage for changes
    - [x] (Optional) Ran [clang-tidy code
    analysis](https://github.com/tenstorrent/tt-metal/actions/workflows/code-analysis.yaml)
    on the PR branch to catch linting errors early

---
+++ tt_metal/hw/inc/internal/tt-2xx/quasar/overlay/dataflow_buffer_isr.h
@@ -0,0 +1,71 @@
+// SPDX-FileCopyrightText: © 2026 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "internal/dataflow_buffer_interface.h"
+#include "internal/tt-2xx/quasar/overlay/llk_intf_api.hpp"
+
+namespace experimental {
+
+extern volatile TxnDFBDescriptor g_txn_dfb_descriptor[32];
+
+inline __attribute__((always_inline)) void dfb_tile_poster_irq_handler() {
+    uint64_t fired_trids = CMDBUF_RD_REG(OVERLAY_RD_CMD_BUF, TT_ROCC_ACCEL_TT_ROCC_CPU0_CMD_BUF_R_PER_TR_ID_IP_1_REG_OFFSET);
+    uint64_t pending = (fired_trids >> 32) & 0xFFFFFFFFULL;
+    while (pending) {
+        uint64_t trid = __builtin_ctzll(pending);
+
+        volatile TxnDFBDescriptor& txn_dfb_descriptor = g_txn_dfb_descriptor[trid];
+        for (uint8_t i = 0; i < txn_dfb_descriptor.num_counters; i++) {
+            PackedTileCounter packed_tile_counter = txn_dfb_descriptor.tile_counters[i];
+            uint8_t tensix_id = get_tensix_id(packed_tile_counter);
+            uint8_t tc_id = get_counter_id(packed_tile_counter);
+            fast_llk_intf_inc_posted(tensix_id, tc_id, txn_dfb_descriptor.tiles_to_post);
+        }
+
+        CMDBUF_CLEAR_TILES_TO_PROCESS_TR_ACK(OVERLAY_RD_CMD_BUF, trid);
+        asm volatile("nop");  // must give time for the clear to propagate (hw bug, this is needed)
+
+        pending &= (pending - 1);
+    }
+    if ((fired_trids >> 32) != 0) {
+        uint64_t to_clear = (fired_trids >> 32) & 0xFFFFFFFFULL;
+        uint64_t clear_val = fired_trids & ~(to_clear << 32);
+        CMDBUF_WR_REG(OVERLAY_RD_CMD_BUF, TT_ROCC_ACCEL_TT_ROCC_CPU0_CMD_BUF_R_PER_TR_ID_IP_1_REG_OFFSET, clear_val);
+    }
+}
+
+inline __attribute__((always_inline)) void dfb_tile_acker_irq_handler() {
+    uint64_t fired_trids = CMDBUF_RD_REG(OVERLAY_WR_CMD_BUF, TT_ROCC_ACCEL_TT_ROCC_CPU0_CMD_BUF_R_PER_TR_ID_IP_2_REG_OFFSET);
+    uint64_t pending = fired_trids & 0xFFFFFFFFULL;
+    while (pending) {
+        uint64_t trid = __builtin_ctzll(pending);
+
+        volatile TxnDFBDescriptor& txn_dfb_descriptor = g_txn_dfb_descriptor[trid];
+        for (uint8_t i = 0; i < txn_dfb_descriptor.num_counters; i++) {
+            PackedTileCounter packed_tile_counter = txn_dfb_descriptor.tile_counters[i];
+            uint8_t tensix_id = get_tensix_id(packed_tile_counter);
+            uint8_t tc_id = get_counter_id(packed_tile_counter);
+            fast_llk_intf_inc_acked(tensix_id, tc_id, txn_dfb_descriptor.tiles_to_ack);
+        }
+
+        CMDBUF_CLEAR_TILES_TO_PROCESS_WR_SENT(OVERLAY_WR_CMD_BUF, trid);
+        asm volatile("nop");  // must give time for the clear to propagate (this is needed)
+
+        pending &= (pending - 1);
+    }
+    if ((fired_trids & 0xFFFFFFFFULL) != 0) {
+        uint64_t to_clear = fired_trids & 0xFFFFFFFFULL;
+        uint64_t clear_val = fired_trids & ~to_clear;
+        CMDBUF_WR_REG(OVERLAY_WR_CMD_BUF, TT_ROCC_ACCEL_TT_ROCC_CPU0_CMD_BUF_R_PER_TR_ID_IP_2_REG_OFFSET, clear_val);
+    }
+}
+
+inline __attribute__((interrupt, hot)) void dfb_implicit_sync_handler() {
+    dfb_tile_poster_irq_handler();
+    dfb_tile_acker_irq_handler();
+}
+
+}  // namespace experimental
