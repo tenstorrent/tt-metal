021341263821ec7840de7d806efd7454633299c4
Ana Mokan <amokan@tenstorrent.com>
2026-03-14 10:29:30 +0000

    Clean up, remove outdated comments

--- tt_metal/hw/inc/api/compute/compute_kernel_hw_startup.h
+++ tt_metal/hw/inc/api/compute/compute_kernel_hw_startup.h
@@ -58,7 +58,7 @@ ALWI void compute_kernel_hw_startup(uint32_t icb0, uint32_t icb1, uint32_t ocb) 
 
     PACK((llk_pack_hw_configure(ocb)));
     PACK((llk_pack_init(ocb)));
-#endif  // TODO: AM; add Quasar implementation
+#endif
 }
 
 // clang-format off
@@ -73,11 +73,7 @@ ALWI void compute_kernel_hw_startup(uint32_t icb0, uint32_t icb1, uint32_t ocb) 
  * | Function   | ocb   | The identifier of the output circular buffer (CB)                  | uint32_t | 0 to 31     | True     |
  */
 // clang-format on
-ALWI void compute_kernel_hw_startup(uint32_t icb0, uint32_t ocb) {
-#ifndef ARCH_QUASAR
-    compute_kernel_hw_startup(icb0, icb0, ocb);
-#endif  // TODO: AM; add Quasar implementation
-}
+ALWI void compute_kernel_hw_startup(uint32_t icb0, uint32_t ocb) { compute_kernel_hw_startup(icb0, icb0, ocb); }
 
 // clang-format off
 /**
