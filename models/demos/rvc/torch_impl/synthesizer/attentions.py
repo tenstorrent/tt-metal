05f164ba327d5ea970971b2f78b45eee1ceebf40
Mert <kurttutan.mert@gmail.com>
2026-03-16 10:04:01 +0000

    RVC Demo: use modified layernorm inside the module

--- models/demos/rvc/torch_impl/synthesizer/attentions.py
+++ models/demos/rvc/torch_impl/synthesizer/attentions.py
@@ -8,8 +8,22 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-from models.demos.rvc.torch_impl.synthesizer.modules import LayerNorm
 from models.demos.rvc.torch_impl.utils import linear_channel_first
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, channels, eps=1e-5):
+        super().__init__()
+        self.channels = channels
+        self.eps = eps
+
+        self.gamma = nn.Parameter(torch.ones(channels))
+        self.beta = nn.Parameter(torch.zeros(channels))
+
+    def forward(self, x):
+        x = x.transpose(1, -1)
+        x = F.layer_norm(x, (self.channels,), self.gamma, self.beta, self.eps)
+        return x.transpose(1, -1)
 
 
 class Encoder(nn.Module):
