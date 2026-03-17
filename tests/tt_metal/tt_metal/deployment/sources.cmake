ca02fbc366ad2dc39fcaf28b92e0aa824f28f9f1
Никола Радојевић <nikola.radojevic@outlogix.com>
2026-03-17 10:18:07 +0100

    First draft of bidirectional ethernet bandwidth tests

--- tests/tt_metal/tt_metal/deployment/sources.cmake
+++ tests/tt_metal/tt_metal/deployment/sources.cmake
@@ -2,6 +2,7 @@
 # Module owners should update this file when adding/removing/renaming source files
 
 set(UNIT_TESTS_DEPLOYMENT_SRC
-    eth/test_eth_link_up.cpp
+    eth/test_eth_bandwidth_bidir.cpp
     eth/test_eth_bandwidth.cpp
+    eth/test_eth_link_up.cpp
 )
