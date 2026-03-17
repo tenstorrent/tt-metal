4827d44a997c1903a3837bf5bfc95b263df4a613
Virdhatchani Narayanamoorthy <138196495+VirdhatchaniKN@users.noreply.github.com>
2026-03-17 09:16:50 +0530

    [skip uplift] Migrate exp 21f,61f to tt-llk (#1478)
    ### Ticket
    https://github.com/tenstorrent/tt-metal/issues/34061
    
    ### Problem description
    Moving metal exp implementation , specifically 21f,61f algorithms to
    tt-llk implementation. Not Moving exp main implementation as further
    work is needed on that for SDPA (In progress task)
    
    ### What's changed
    
    - Migrated exp 21f,61f algorithms to tt-llk implementation. 
    - LLK kernels are now sourced from Metal via sparse checkout. Hence, to
    avoid redefinition error, I've updated the functions names of all exp
    algorithm functions ported
    
    ### Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Documentation update
    - [x] Refactoring
    
    ### Checklist
    - [x] [![Sanity
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/sanity-tests.yaml/badge.svg?branch=virdhatchani/exp_migrate)](https://github.com/tenstorrent/tt-metal/actions/workflows/sanity-tests.yaml?query=branch:virdhatchani/exp_migrate)
    - [x] [![Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml/badge.svg?branch=virdhatchani/exp_migrate)](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml?query=branch:virdhatchani/exp_migrate)
    - [x]
    [![cpp-unit-tests](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml/badge.svg?branch=virdhatchani/exp_migrate)](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml?query=branch:virdhatchani/exp_migrate)
    - [ ] [Assert
    validation](https://github.com/tenstorrent/tt-llk/blob/main/docs/Introduction_to_asserts.md)
    Complied with assert doc (if applicable)

--- tt_llk_blackhole/common/inc/sfpu/ckernel_sfpu_polyval.h
+++ tt_llk_blackhole/common/inc/sfpu/ckernel_sfpu_polyval.h
@@ -55,7 +55,7 @@ struct PolynomialEvaluator
     }
 
     template <typename U, typename Coefficient0>
-    sfpi_inline static constexpr auto eval(U x, Coefficient0 coeff0)
+    sfpi_inline static constexpr auto eval([[maybe_unused]] U x, Coefficient0 coeff0)
     {
         // Base case: f(x) = coeff0 (0-th degree polynomial)
         return coeff0;
