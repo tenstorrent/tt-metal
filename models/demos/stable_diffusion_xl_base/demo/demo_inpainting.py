7839731ce87849f733b2d359cd3d4e7560ea33af
Nikola Milicevic <nmilicevic@tenstorrent.com>
2026-03-17 19:30:39 +0000

    pre-commit fix

--- models/demos/stable_diffusion_xl_base/demo/demo_inpainting.py
+++ models/demos/stable_diffusion_xl_base/demo/demo_inpainting.py
@@ -13,7 +13,7 @@ from loguru import logger
 from transformers import CLIPTextModel, CLIPTextModelWithProjection
 
 from conftest import is_galaxy
-from models.common.utility_functions import profiler, is_blackhole
+from models.common.utility_functions import is_blackhole, profiler
 from models.demos.stable_diffusion_xl_base.tests.test_common import (
     CONCATENATED_TEXT_EMBEDINGS_SIZE,
     MAX_SEQUENCE_LENGTH,
