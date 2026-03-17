fb634cdb53d149b7b6c0f334fa82ce0cf2185159
Filip Bajraktari <fbajraktari@tenstorrent.com>
2026-03-17 11:26:42 +0000

    Update sdxl perf for 1024x1024 image resoluion on BH

--- models/demos/stable_diffusion_xl_base/tests/test_sdxl_perf.py
+++ models/demos/stable_diffusion_xl_base/tests/test_sdxl_perf.py
@@ -112,7 +112,7 @@ def test_refiner_unet(
 DEVICE_PERF_EXPECTATIONS = {
     "unet_1024x1024": {
         "wormhole": 191_201_442 * UNET_DEVICE_TEST_TOTAL_ITERATIONS,
-        "blackhole": 119_138_750 * UNET_DEVICE_TEST_TOTAL_ITERATIONS,
+        "blackhole": 87_143_154 * UNET_DEVICE_TEST_TOTAL_ITERATIONS,
     },
     "unet_512x512": {
         "wormhole": 83_531_333 * UNET_DEVICE_TEST_TOTAL_ITERATIONS,
