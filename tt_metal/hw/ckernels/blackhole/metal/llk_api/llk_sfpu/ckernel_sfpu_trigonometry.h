737b200b568da8c1456c2cf1c904f3e581e56646
Virdhatchani Narayanamoorthy <138196495+VirdhatchaniKN@users.noreply.github.com>
2026-03-17 22:42:04 +0530

    Migrate exp 21f, 61f to tt-llk (#39932)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-metal/issues/34061)
    
    ### Problem description
    Moving metal exp implementation , specifically 21f,61f algorithms to
    tt-llk implementation. Not Moving exp main implementation as further
    work is needed on that for SDPA (In progress task)
    
    ### What's changed
    - Migrated exp 21f,61f algorithms to tt-llk implementation. 
    - LLK kernels are now sourced from Metal via sparse checkout. Hence, to
    avoid redefinition error, I've updated the functions names of all exp
    algorithm functions ported
    
    ### Checklist
    
    - [x] [![Sanity
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/sanity-tests.yaml/badge.svg?branch=virdhatchani/exp_migrate)](https://github.com/tenstorrent/tt-metal/actions/workflows/sanity-tests.yaml?query=branch:virdhatchani/exp_migrate)
    - [x] [![Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml/badge.svg?branch=virdhatchani/exp_migrate)](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml?query=branch:virdhatchani/exp_migrate)
    - [x]
    [![cpp-unit-tests](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml/badge.svg?branch=virdhatchani/exp_migrate)](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml?query=branch:virdhatchani/exp_migrate)
    - [ ] New/Existing tests provide coverage for changes
    - [ ] (Optional) Ran [clang-tidy code
    analysis](https://github.com/tenstorrent/tt-metal/actions/workflows/code-analysis.yaml)
    on the PR branch to catch linting errors early

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_trigonometry.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_trigonometry.h
@@ -8,7 +8,7 @@
 #include "ckernel.h"
 #include "ckernel_defs.h"
 #include "ckernel_sfpu_recip.h"
-#include "ckernel_sfpu_exp.h"
+#include "sfpu/ckernel_sfpu_exp.h"
 #include "sfpu/ckernel_sfpu_polyval.h"
 #include "sfpi.h"
 
@@ -435,7 +435,8 @@ inline void calculate_cosh() {
     // SFPU microcode
     for (int d = 0; d < ITERATIONS; d++) {
         sfpi::vFloat v = sfpi::dst_reg[0];
-        sfpi::vFloat result = (_sfpu_exp_21f_<is_fp32_dest_acc_en>(v) + _sfpu_exp_21f_<is_fp32_dest_acc_en>(-v)) * 0.5f;
+        sfpi::vFloat result =
+            (_sfpu_exp_21f_bf16_<is_fp32_dest_acc_en>(v) + _sfpu_exp_21f_bf16_<is_fp32_dest_acc_en>(-v)) * 0.5f;
         sfpi::dst_reg[0] = result;
         sfpi::dst_reg++;
     }
@@ -447,7 +448,8 @@ inline void calculate_sinh() {
     // SFPU microcode
     for (int d = 0; d < ITERATIONS; d++) {
         sfpi::vFloat v = sfpi::dst_reg[0];
-        sfpi::vFloat result = (_sfpu_exp_21f_<is_fp32_dest_acc_en>(v) - _sfpu_exp_21f_<is_fp32_dest_acc_en>(-v)) * 0.5f;
+        sfpi::vFloat result =
+            (_sfpu_exp_21f_bf16_<is_fp32_dest_acc_en>(v) - _sfpu_exp_21f_bf16_<is_fp32_dest_acc_en>(-v)) * 0.5f;
         sfpi::dst_reg[0] = result;
         sfpi::dst_reg++;
     }
