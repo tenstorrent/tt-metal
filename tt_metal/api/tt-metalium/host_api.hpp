1afd949fb868987e492554a3316ea1f12a93e7b6
Samarth Agarwal <sagarwal@tenstorrent.com>
2026-03-18 00:38:52 +0000

    Delete EventQuery() function

--- tt_metal/api/tt-metalium/host_api.hpp
+++ tt_metal/api/tt-metalium/host_api.hpp
@@ -626,18 +626,6 @@ void ReadMeshDeviceProfilerResults(
 
 // clang-format off
 /**
- * Host will query an event for completion status on device.
- * Return value: bool.  True if event is completed, false otherwise.
- * | Argument     | Description                                                            | Type                          | Valid Range                        | Required |
- * |--------------|------------------------------------------------------------------------|-------------------------------|------------------------------------|----------|
- * | event        | The event object that host will query for completion.                  | std::shared_ptr<Event>        |                                    | Yes      |
- */
-// clang-format on
-[[deprecated("Use distributed::EventQuery with distributed::MeshEvent instead.")]]
-bool EventQuery(const std::shared_ptr<Event>& event);
-
-// clang-format off
-/**
  * Push the current command queue id to the stack.
  * Return value: void
  * | Argument     | Description                                                                       | Type                          | Valid Range                        | Required |
