cb1d8204bb1d6b3439d995d907af1d66b74a1e2f
Yousef <yalrawwash@tenstorrent.com>
2026-03-18 14:29:55 +0000

    latest embedding op API

--- ttnn/cpp/ttnn/operations/embedding/embedding.cpp
+++ ttnn/cpp/ttnn/operations/embedding/embedding.cpp
@@ -32,19 +32,20 @@ ttnn::Tensor embedding(
         mutable_weight = ttnn::to_layout(mutable_weight, ttnn::ROW_MAJOR_LAYOUT);
     }
     auto hidden_embedding_dim = mutable_weight.logical_shape()[-1];
+    auto original_input_rank = mutable_input_tensor.logical_shape().rank();
     auto weight = ttnn::unsqueeze_to_4D(mutable_weight);
 
     // If indices tensor is 1 dimensional, batch size is 1
     auto batch_size = (mutable_input_tensor.logical_shape().rank() == 1) ? 1 : mutable_input_tensor.logical_shape()[0];
     auto sentence_size = mutable_input_tensor.logical_shape()[-1];
-    auto input_tensor = mutable_input_tensor;
+    auto embedding_input_tensor = mutable_input_tensor;
     if (mutable_input_tensor.layout() == ttnn::ROW_MAJOR_LAYOUT) {
-        input_tensor = ttnn::reshape(mutable_input_tensor, ttnn::Shape({batch_size, 1, 1, sentence_size}));
+        embedding_input_tensor = ttnn::reshape(mutable_input_tensor, ttnn::Shape({batch_size, 1, 1, sentence_size}));
     }
 
     // If layout is row major, OR if the input tensor is not a multiple of TILE_HEIGHT, then we cannot use tilized
     bool fused_tilized = false;
-    if (input_tensor.padded_shape()[-1] % tt::constants::TILE_HEIGHT == 0 &&
+    if (embedding_input_tensor.padded_shape()[-1] % tt::constants::TILE_HEIGHT == 0 &&
         weight.padded_shape()[-1] % tt::constants::TILE_WIDTH == 0) {
         if (layout.has_value()) {
             if (layout.value() == ttnn::TILE_LAYOUT) {
@@ -59,9 +60,15 @@ ttnn::Tensor embedding(
     }
 
     auto embeddings = ttnn::prim::embedding(
-        input_tensor, weight, fused_tilized, embeddings_type, memory_config, pad_token, optional_output_tensor);
+        embedding_input_tensor,
+        weight,
+        fused_tilized,
+        embeddings_type,
+        memory_config,
+        pad_token,
+        optional_output_tensor);
     // Don't include batch_size if there was none
-    if (input_tensor.logical_shape().rank() == 1) {
+    if (original_input_rank == 1) {
         embeddings = ttnn::reshape(embeddings, Shape({sentence_size, hidden_embedding_dim}));
     } else {
         embeddings = ttnn::reshape(embeddings, Shape({batch_size, sentence_size, hidden_embedding_dim}));
