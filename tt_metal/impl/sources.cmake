8a6654981b096b918a718b596c403400046c3c6d
Audrey Kertesz <akertesz@tenstorrent.com>
2026-03-18 05:29:15 +0000

    Rename to program_spec.cpp

--- tt_metal/impl/sources.cmake
+++ tt_metal/impl/sources.cmake
@@ -43,7 +43,7 @@ set(IMPL_SRC
     ${CMAKE_CURRENT_SOURCE_DIR}/data_format/tilize_utils.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/data_format/uint8.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/dataflow_buffer/dataflow_buffer.cpp
-    ${CMAKE_CURRENT_SOURCE_DIR}/metal2_host_api/program.cpp
+    ${CMAKE_CURRENT_SOURCE_DIR}/metal2_host_api/program_spec.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/kernels/kernel.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/kernels/kernel_types.cpp
     ${CMAKE_CURRENT_SOURCE_DIR}/allocator/algorithms/free_list_opt.cpp
