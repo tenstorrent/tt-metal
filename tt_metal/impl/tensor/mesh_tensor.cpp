cb9dd0a438f3b73f8579a15ee11354c2e57fdb01
River Wu <riverwu@tenstorrent.com>
2026-03-18 18:30:10 -0700

    Implement Host and MeshTensor (#40129)
    ### Ticket
    #36877
    
    ### Type
    New Feature
    
    ### Problem description
    Runtime is introducing two new construct, host and mesh tensor.
    
    ### What's changed
    Wires up internals of the Host and MeshTensor.
    More infrastructure will come down with the refactoring of ttnn::Tensor
    to use Host and MeshTensor as backing storage.
    
    ### Checklist
    
    - [x] [![Sanity
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/sanity-tests.yaml/badge.svg?branch=riverwu/tensor-impl)](https://github.com/tenstorrent/tt-metal/actions/workflows/sanity-tests.yaml?query=branch:riverwu/tensor-impl)
    - [ ] [![Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml/badge.svg?branch=riverwu/tensor-impl)](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml?query=branch:riverwu/tensor-impl)
    - [ ]
    [![cpp-unit-tests](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml/badge.svg?branch=riverwu/tensor-impl)](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml?query=branch:riverwu/tensor-impl)
    - [x] New/Existing tests provide coverage for changes
    - [ ] (Optional) Ran [clang-tidy code
    analysis](https://github.com/tenstorrent/tt-metal/actions/workflows/code-analysis.yaml)
    on the PR branch to catch linting errors early
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tt_metal/impl/tensor/mesh_tensor.cpp
+++ tt_metal/impl/tensor/mesh_tensor.cpp
@@ -6,32 +6,88 @@
 
 namespace tt::tt_metal {
 
-/*
+class MeshTensorImpl {
+public:
+    MeshTensorImpl(std::shared_ptr<distributed::MeshBuffer> mesh_buffer, TensorSpec spec, TensorTopology topology) :
+        MeshTensorImpl(std::move(mesh_buffer), std::move(spec), std::move(topology), nullptr) {}
 
-// TODO: Implement once DeviceStorage is migrated (#37692)
-// MeshTensor::MeshTensor(DeviceStorage storage, TensorSpec tensor_spec, TensorTopology tensor_topology)
+    MeshTensorImpl(
+        std::shared_ptr<distributed::MeshBuffer> mesh_buffer,
+        TensorSpec spec,
+        TensorTopology topology,
+        std::shared_ptr<distributed::MeshBuffer> root_mesh_buffer) :
+        mesh_buffer_(std::move(mesh_buffer)),
+        spec_(std::move(spec)),
+        topology_(std::move(topology)),
+        root_mesh_buffer_(std::move(root_mesh_buffer)) {
+        TT_FATAL(mesh_buffer_ != nullptr, "MeshBuffer cannot be nullptr.");
+        TT_FATAL(mesh_buffer_->is_allocated(), "MeshBuffer must be allocated.");
+        TT_FATAL(
+            mesh_buffer_->size() >= spec_.compute_packed_buffer_size_bytes(),
+            "MeshBuffer must be large enough to hold the tensor.");
+    }
 
-// TODO: Implement once DeviceStorage is migrated (#37692)
-// bool MeshTensor::is_allocated() const
+    // Two step construction for MeshTensor,
+    // for transiet purpose.
+    MeshTensorImpl(MeshTensorImpl&& other, TensorSpec spec, TensorTopology topology) :
+        mesh_buffer_(std::move(other.mesh_buffer_)),
+        spec_(std::move(spec)),
+        topology_(std::move(topology)),
+        root_mesh_buffer_(std::move(other.root_mesh_buffer_)) {}
 
-// TODO: Implement once DeviceStorage is migrated (#37692)
-// std::shared_ptr<distributed::MeshBuffer> MeshTensor::mesh_buffer_invariant_breaking() const
+    const std::shared_ptr<distributed::MeshBuffer>& mesh_buffer() const { return mesh_buffer_; }
+    const TensorSpec& spec() const { return spec_; }
+    const TensorTopology& topology() const { return topology_; }
+    void update_topology(TensorTopology topology) { topology_ = std::move(topology); }
 
-// TODO: Implement once DeviceStorage is migrated (#37692)
-// const TensorSpec& MeshTensor::tensor_spec() const
+private:
+    // Invariant:
+    // 1. Cannot be nullptr and must be allocated.
+    // 2. Must be large enough to hold a tensor describale with spec_
+    std::shared_ptr<distributed::MeshBuffer> mesh_buffer_;
+    TensorSpec spec_;
+    // TODO(river): What is the invariant of topology?
+    TensorTopology topology_;
 
-// TODO: Implement once DeviceStorage is migrated (#37692)
-// const TensorTopology& MeshTensor::tensor_topology() const
+    // Experimental feature: Accomodates #38101
+    // This keeps mesh_buffer_ alive in limited cases.
+    std::shared_ptr<distributed::MeshBuffer> root_mesh_buffer_;
+};
 
-// TODO: Implement once DeviceStorage is migrated (#37692)
-// const DeviceStorage& MeshTensor::get_legacy_device_storage() const
+MeshTensor::MeshTensor() = default;
 
-// TODO: Implement once DeviceStorage is migrated (#37692)
-// void MeshTensor::update_tensor_topology(TensorTopology tensor_topology)
+MeshTensor::MeshTensor(MeshTensor&& other) noexcept = default;
+
+MeshTensor& MeshTensor::operator=(MeshTensor&& other) noexcept = default;
+
+MeshTensor::MeshTensor(std::shared_ptr<distributed::MeshBuffer> mesh_buffer, TensorSpec spec, TensorTopology topology) :
+    impl(std::make_unique<MeshTensorImpl>(std::move(mesh_buffer), std::move(spec), std::move(topology))) {}
+
+MeshTensor::MeshTensor(MeshTensor&& other, TensorSpec spec, TensorTopology topology) {
+    TT_FATAL(other.impl != nullptr, "Cannot move from a default-constructed or moved-from MeshTensor.");
+    impl = std::make_unique<MeshTensorImpl>(std::move(*other.impl), std::move(spec), std::move(topology));
+}
+
+MeshTensor::~MeshTensor() = default;
 
 distributed::MeshBuffer& MeshTensor::mesh_buffer() const { return *mesh_buffer_invariant_breaking(); }
 
+std::shared_ptr<distributed::MeshBuffer> MeshTensor::mesh_buffer_invariant_breaking() const {
+    TT_ASSERT(impl != nullptr, "MeshTensor is in default constructed state.");
+    return impl->mesh_buffer();
+}
+
 distributed::MeshDevice& MeshTensor::device() const { return *mesh_buffer().device(); }
+
+const TensorSpec& MeshTensor::tensor_spec() const {
+    TT_ASSERT(impl != nullptr, "MeshTensor is in default constructed state.");
+    return impl->spec();
+}
+
+const TensorTopology& MeshTensor::tensor_topology() const {
+    TT_ASSERT(impl != nullptr, "MeshTensor is in default constructed state.");
+    return impl->topology();
+}
 
 DeviceAddr MeshTensor::address() const { return mesh_buffer().address(); }
 
@@ -71,6 +127,9 @@ std::size_t MeshTensor::element_size() const {
 
 Strides MeshTensor::strides() const { return tensor_spec().tensor_layout().compute_strides(logical_shape()); }
 
-*/
+void MeshTensor::update_tensor_topology(TensorTopology tensor_topology) {
+    TT_ASSERT(impl != nullptr, "MeshTensor is in default constructed state.");
+    impl->update_topology(std::move(tensor_topology));
+}
 
 }  // namespace tt::tt_metal
