03b4cd55715a8d54aab1404403042788071625d2
Jyothi Hariharan <jyothi.hariharan@ignitarium.com>
2026-03-23 15:35:35 +0000

    tring to us bfloat8_b weight maintaining the pcc

--- models/demos/molmo2/tt/text_attention.py
+++ models/demos/molmo2/tt/text_attention.py
@@ -198,7 +198,7 @@ class TextAttention(LightweightModule):
 
         self.q_norm_weight = ttnn.as_tensor(
             q_norm.reshape(1, 1, 1, -1),
-            dtype=ttnn.bfloat16,
+            dtype=dtype,
             device=mesh_device,
             mesh_mapper=norm_mesh_mapper,
             layout=ttnn.TILE_LAYOUT,
@@ -208,7 +208,7 @@ class TextAttention(LightweightModule):
 
         self.k_norm_weight = ttnn.as_tensor(
             k_norm.reshape(1, 1, 1, -1),
-            dtype=ttnn.bfloat16,
+            dtype=dtype,
             device=mesh_device,
             mesh_mapper=norm_mesh_mapper,
             layout=ttnn.TILE_LAYOUT,
