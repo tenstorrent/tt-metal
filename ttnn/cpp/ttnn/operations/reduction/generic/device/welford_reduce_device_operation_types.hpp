94497a19252989724bf38c661deb460f712f93d7
Frank Plavec <fplavec@tenstorrent.com>
2026-03-23 15:28:34 +0000

    Fixed corner case for scalar tensor retunring inf when it should return NaN

--- ttnn/cpp/ttnn/operations/reduction/generic/device/welford_reduce_device_operation_types.hpp
+++ ttnn/cpp/ttnn/operations/reduction/generic/device/welford_reduce_device_operation_types.hpp
@@ -14,7 +14,7 @@ namespace ttnn::prim {
 
 struct WelfordReduceParams {
     tt::tt_metal::ReduceOpMath math_op{};
-    float scaler;
+    float scalar{1.0f};
     tt::tt_metal::MemoryConfig output_mem_config;
     tt::tt_metal::DataType output_dtype{tt::tt_metal::DataType::INVALID};
     ttnn::DeviceComputeKernelConfig compute_kernel_config;
