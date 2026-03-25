f649808f9eaa1266a10515918977bcd48405224a
Alexey Zaytsev <ctr-azaytsev@ext.tenstorrent.com>
2026-03-25 12:21:13 +0000

    rename of function - fix

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_gelu.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_gelu.h
@@ -6,7 +6,7 @@
 
 #include "ckernel_defs.h"
 #include "ckernel.h"
-#include "ckernel_sfpu_exp.h"  // For _sfpu_round_nearest_int32_
+#include "ckernel_sfpu_exp.h"  // For _sfpu_round_to_nearest_int32_
 #include "sfpu/ckernel_sfpu_polyval.h"
 
 namespace ckernel {
