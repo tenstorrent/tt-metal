f44e4301d5b0f9ad9c13d83a5037642d9cf29a37
awliu-TT <awliu@tenstorrent.com>
2026-03-26 18:42:37 +0000

    clang tidy 4

--- ttnn/cpp/ttnn/operations/data_movement/sharded/redistribute_to_memory_config/device/redistribute_to_memory_config_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/sharded/redistribute_to_memory_config/device/redistribute_to_memory_config_device_operation.cpp
@@ -79,7 +79,7 @@ RedistributeToMemoryConfigDeviceOperation::compute_output_specs(
     const auto& input_tensor = tensor_args.input_tensor;
     return tt::tt_metal::TensorSpec(
         input_tensor.logical_shape(),
-        TensorLayout(
+        tt::tt_metal::TensorLayout(
             operation_attributes.output_dtype,
             tt::tt_metal::PageConfig(input_tensor.layout()),
             operation_attributes.output_mem_config));
