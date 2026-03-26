09eceeac396ee9c192990ab6929d1b8a0dcbf9f7
Salar <skhorasgani@tenstorrent.com>
2026-03-26 21:51:50 +0000

    [Blaze DS] Optimize prefill-by-decode to fully utilize pipeline stages
    Signed-off-by: Salar <skhorasgani@tenstorrent.com>

--- models/demos/deepseek_v3_b1/demo/stage.py
+++ models/demos/deepseek_v3_b1/demo/stage.py
@@ -28,7 +28,8 @@ from models.demos.deepseek_v3_b1.prepare_weights import (
 
 # Global constants used by multiple stage kinds (and exported to pipeline/cli)
 TOKEN_PAGE_SIZE_BYTES = 64
-TOKEN_FIFO_SIZE = 1024
+TOKEN_FIFO_NUM_PAGES = 64
+TOKEN_FIFO_SIZE = TOKEN_PAGE_SIZE_BYTES * TOKEN_FIFO_NUM_PAGES
 ACTIVATION_DIM = 7168
 ACTIVATION_PAGE_SIZE_BYTES = ACTIVATION_DIM * 2
 ACTIVATION_FIFO_SIZE = ACTIVATION_PAGE_SIZE_BYTES * 1
