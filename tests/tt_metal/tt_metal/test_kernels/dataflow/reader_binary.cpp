3770931882617a78b94550b1a51ef59bb5956d0c
Gagan Deep Singh <gagandeepsingh@tenstorrent.com>
2026-03-26 16:11:43 +0000

    Modified relevant kernels for Eltwise Binary

--- tests/tt_metal/tt_metal/test_kernels/dataflow/reader_binary.cpp
+++ tests/tt_metal/tt_metal/test_kernels/dataflow/reader_binary.cpp
@@ -4,8 +4,13 @@
 
 #include <stdint.h>
 #include "api/dataflow/dataflow_api.h"
-#include "experimental/circular_buffer.h"
 #include "experimental/endpoints.h"
+
+#ifdef ARCH_QUASAR
+#include "experimental/dataflow_buffer.h"
+#else
+#include "experimental/circular_buffer.h"
+#endif
 
 void kernel_main() {
     uint32_t src0_addr = get_arg_val<uint32_t>(0);
@@ -14,32 +19,47 @@ void kernel_main() {
     uint32_t src1_bank_id = get_arg_val<uint32_t>(3);
     uint32_t num_tiles = get_arg_val<uint32_t>(4);
 
-    constexpr uint32_t cb_id_in0 = 0;
-    constexpr uint32_t cb_id_in1 = 1;
-
-    // single-tile ublocks
-
-    experimental::CircularBuffer cb0(cb_id_in0);
-    experimental::CircularBuffer cb1(cb_id_in1);
     experimental::Noc noc;
     experimental::AllocatorBank<experimental::AllocatorBankType::DRAM> dram_src;
-
     uint32_t ublock_size_tiles = 1;
-    uint32_t ublock_size_bytes_0 = cb0.get_tile_size() * ublock_size_tiles;
-    uint32_t ublock_size_bytes_1 = cb1.get_tile_size() * ublock_size_tiles;
+
+    // single-tile ublocks
+    #ifdef ARCH_QUASAR
+        experimental::DataflowBuffer dfb0(0);
+        experimental::DataflowBuffer dfb1(1);
+        uint32_t ublock_size_bytes_0 = dfb0.get_entry_size() * ublock_size_tiles;
+        uint32_t ublock_size_bytes_1 = dfb1.get_entry_size() * ublock_size_tiles;
+    #else
+        constexpr uint32_t cb_id_in0 = 0;
+        constexpr uint32_t cb_id_in1 = 1;
+        experimental::CircularBuffer cb0(cb_id_in0);
+        experimental::CircularBuffer cb1(cb_id_in1);
+        uint32_t ublock_size_bytes_0 = cb0.get_tile_size() * ublock_size_tiles;
+        uint32_t ublock_size_bytes_1 = cb1.get_tile_size() * ublock_size_tiles;
+    #endif
 
     // read ublocks from src0/src1 to CB0/CB1, then push ublocks to compute (unpacker)
     for (uint32_t i=0; i<num_tiles; i += ublock_size_tiles) {
         uint64_t src0_noc_addr = get_noc_addr_from_bank_id<true>(src0_bank_id, src0_addr);
         uint64_t src1_noc_addr = get_noc_addr_from_bank_id<true>(src1_bank_id, src1_addr);
 
-        cb0.reserve_back(ublock_size_tiles);
-        cb1.reserve_back(ublock_size_tiles);
-        noc.async_read(dram_src, cb0, ublock_size_bytes_0, {.bank_id = src0_bank_id, .addr = src0_addr}, {});
-        noc.async_read(dram_src, cb1, ublock_size_bytes_1, {.bank_id = src1_bank_id, .addr = src1_addr}, {});
-        noc.async_read_barrier();
-        cb0.push_back(ublock_size_tiles);
-        cb1.push_back(ublock_size_tiles);
+        #ifdef ARCH_QUASAR
+            dfb0.reserve_back(ublock_size_tiles);
+            dfb1.reserve_back(ublock_size_tiles);
+            noc.async_read(dram_src, dfb0, ublock_size_bytes_0, {.bank_id = src0_bank_id, .addr = src0_addr}, {});
+            noc.async_read(dram_src, dfb1, ublock_size_bytes_1, {.bank_id = src1_bank_id, .addr = src1_addr}, {});
+            noc.async_read_barrier();
+            dfb0.push_back(ublock_size_tiles);
+            dfb1.push_back(ublock_size_tiles);
+        #else
+            cb0.reserve_back(ublock_size_tiles);
+            cb1.reserve_back(ublock_size_tiles);
+            noc.async_read(dram_src, cb0, ublock_size_bytes_0, {.bank_id = src0_bank_id, .addr = src0_addr}, {});
+            noc.async_read(dram_src, cb1, ublock_size_bytes_1, {.bank_id = src1_bank_id, .addr = src1_addr}, {});
+            noc.async_read_barrier();
+            cb0.push_back(ublock_size_tiles);
+            cb1.push_back(ublock_size_tiles);
+        #endif
         src0_addr += ublock_size_bytes_0;
         src1_addr += ublock_size_bytes_1;
     }
@@ -52,17 +72,28 @@ void kernel_main() {
     uint32_t src2_addr = get_arg_val<uint32_t>(5);
     uint32_t src2_bank_id = get_arg_val<uint32_t>(6);
 
-    constexpr uint32_t cb_id_in2 = 2;
-    experimental::CircularBuffer cb2(cb_id_in2);
-    uint32_t ublock_size_bytes_2 = cb2.get_tile_size() * ublock_size_tiles;
+    #ifdef ARCH_QUASAR
+        experimental::DataflowBuffer dfb2(2);
+        uint32_t ublock_size_bytes_2 = dfb2.get_entry_size() * ublock_size_tiles;
+    #else
+        constexpr uint32_t cb_id_in2 = 2;
+        experimental::CircularBuffer cb2(cb_id_in2);
+        uint32_t ublock_size_bytes_2 = cb2.get_tile_size() * ublock_size_tiles;
+    #endif
 
     for (uint32_t i=0; i<num_tiles; i += ublock_size_tiles) {
         uint64_t src2_noc_addr = get_noc_addr_from_bank_id<true>(src2_bank_id, src2_addr);
-
-        cb2.reserve_back(ublock_size_tiles);
-        noc.async_read(dram_src, cb2, ublock_size_bytes_2, {.bank_id = src2_bank_id, .addr = src2_addr}, {});
-        noc.async_read_barrier();
-        cb2.push_back(ublock_size_tiles);
+        #ifdef ARCH_QUASAR
+            dfb2.reserve_back(ublock_size_tiles);
+            noc.async_read(dram_src, dfb2, ublock_size_bytes_2, {.bank_id = src2_bank_id, .addr = src2_addr}, {});
+            noc.async_read_barrier();
+            dfb2.push_back(ublock_size_tiles);
+        #else
+            cb2.reserve_back(ublock_size_tiles);
+            noc.async_read(dram_src, cb2, ublock_size_bytes_2, {.bank_id = src2_bank_id, .addr = src2_addr}, {});
+            noc.async_read_barrier();
+            cb2.push_back(ublock_size_tiles);
+        #endif
         src2_addr += ublock_size_bytes_2;
     }
 #endif
--- tt_metal/kernels/compute/eltwise_binary.cpp
+++ tt_metal/kernels/compute/eltwise_binary.cpp
@@ -9,67 +9,120 @@
 #include "api/compute/eltwise_unary/sfpu_split_includes.h"
 #include "api/compute/tile_move_copy.h"
 
+#ifdef ARCH_QUASAR
+#include "experimental/dataflow_buffer.h"
+#else
+#include "experimental/circular_buffer.h"
+#endif
+
 void kernel_main() {
     uint32_t per_core_block_cnt = get_arg_val<uint32_t>(0);
     uint32_t per_core_block_size = get_arg_val<uint32_t>(1);
     uint32_t acc_to_dst = get_arg_val<uint32_t>(2);
 
-    constexpr auto cb_in0 = tt::CBIndex::c_0;
-    constexpr auto cb_in1 = tt::CBIndex::c_1;
-    constexpr auto cb_inp0 = cb_in0;
-    constexpr auto cb_inp1 = cb_in1;
-    constexpr auto cb_out0 = tt::CBIndex::c_16;
 
-    constexpr auto cb_in2 = tt::CBIndex::c_2;
-
-    binary_op_init_common(cb_inp0, cb_inp1, cb_out0);
-
-#if not defined ELTWISE_DEST_REUSE_TYPE
-#ifdef FULL_INIT
-    binary_tiles_init<true, ELTWISE_OP_TYPE>(cb_in0, cb_in1);
-#else
-    binary_tiles_init<false, ELTWISE_OP_TYPE>(cb_in0, cb_in1);
-#endif
-#endif
+    #ifdef ARCH_QUASAR
+        experimental::DataflowBuffer dfb_in0(0);
+        experimental::DataflowBuffer dfb_in1(1);
+        experimental::DataflowBuffer dfb_in2(2);
+        experimental::DataflowBuffer dfb_out(3);
+        binary_op_init_common(dfb_in0.get_id(), dfb_in1.get_id(), dfb_out.get_id());
+        #if not defined ELTWISE_DEST_REUSE_TYPE
+            #ifdef FULL_INIT
+                binary_tiles_init<true, ELTWISE_OP_TYPE>(dfb_in0.get_id(), dfb_in1.get_id());
+            #else
+                binary_tiles_init<false, ELTWISE_OP_TYPE>(dfb_in0.get_id(), dfb_in1.get_id());
+            #endif
+        #endif
+    #else
+        constexpr auto cb_in0 = tt::CBIndex::c_0;   
+        constexpr auto cb_in1 = tt::CBIndex::c_1;
+        constexpr auto cb_inp0 = cb_in0;
+        constexpr auto cb_inp1 = cb_in1;
+        constexpr auto cb_out0 = tt::CBIndex::c_16;
+        constexpr auto cb_in2 = tt::CBIndex::c_2;
+        binary_op_init_common(cb_inp0, cb_inp1, cb_out0);
+        #if not defined ELTWISE_DEST_REUSE_TYPE
+            #ifdef FULL_INIT
+                binary_tiles_init<true, ELTWISE_OP_TYPE>(cb_in0, cb_in1);
+            #else
+                binary_tiles_init<false, ELTWISE_OP_TYPE>(cb_in0, cb_in1);
+            #endif
+        #endif
+    #endif
 
 #ifdef PACK_RELU
     PACK((llk_pack_relu_config(ReluType::ZERO_RELU)));
 #endif
 
     for (uint32_t block = 0; block < per_core_block_cnt; ++block) {
-        cb_wait_front(cb_inp0, per_core_block_size);
-        cb_wait_front(cb_inp1, per_core_block_size);
-        cb_reserve_back(cb_out0, per_core_block_size);
-
+        #ifdef ARCH_QUASAR
+            dfb_in0.wait_front(per_core_block_size);
+            dfb_in1.wait_front(per_core_block_size);
+            dfb_out.reserve_back(per_core_block_size);
+        #else
+            cb_wait_front(cb_inp0, per_core_block_size);
+            cb_wait_front(cb_inp1, per_core_block_size);
+            cb_reserve_back(cb_out0, per_core_block_size);
+        #endif
         tile_regs_acquire();
 
 #if defined(DST_ACCUM_MODE) || defined(ELTWISE_DEST_REUSE_TYPE)
-        cb_wait_front(cb_in2, per_core_block_size);
-        copy_tile_to_dst_init_short(cb_in2);
-        for (uint32_t i = 0; i < per_core_block_size; ++i) {
-            copy_tile(cb_in2, i, i);  // copy from c_in[0] to DST[0]
-        }
-        cb_pop_front(cb_in2, per_core_block_size);
+    #ifdef ARCH_QUASAR
+            dfb_in2.wait_front(per_core_block_size);
+            copy_tile_to_dst_init_short(dfb_in2.get_id());
+            for (uint32_t i = 0; i < per_core_block_size; ++i) {
+                copy_tile(dfb_in2.get_id(), i, i);  // copy from c_in[0] to DST[0]
+            }
+            dfb_in2.pop_front(per_core_block_size);
+    #else
+            cb_wait_front(cb_in2, per_core_block_size);
+            copy_tile_to_dst_init_short(cb_in2);
+            for (uint32_t i = 0; i < per_core_block_size; ++i) {
+                copy_tile(cb_in2, i, i);  // copy from c_in[0] to DST[0]
+            }
+            cb_pop_front(cb_in2, per_core_block_size);
+    #endif
 #endif
 
 #ifdef DST_ACCUM_MODE
 // The following define is needed if mul_tiles/_init is used
 #ifdef MUL_TILES_WITH_DST_ACCUM
+    #ifdef ARCH_QUASAR
+        ELTWISE_OP_INIT(dfb_in0.get_id(), dfb_in1.get_id());
+    #else
         ELTWISE_OP_INIT(cb_inp0, cb_inp1);
+    #endif
 #else
+    #ifdef ARCH_QUASAR
+        ELTWISE_OP_INIT(dfb_in0.get_id(), dfb_in1.get_id(), true);
+    #else
         ELTWISE_OP_INIT(cb_inp0, cb_inp1, true);
+    #endif
 #endif
 #endif
 
 #ifdef ELTWISE_DEST_REUSE_TYPE
+    #ifdef ARCH_QUASAR
+        binary_dest_reuse_tiles_init<ELTWISE_OP_TYPE, ELTWISE_DEST_REUSE_TYPE>(dfb_in0.get_id());
+    #else
         binary_dest_reuse_tiles_init<ELTWISE_OP_TYPE, ELTWISE_DEST_REUSE_TYPE>(cb_inp0);
+    #endif
 #endif
 
         for (uint32_t i = 0; i < per_core_block_size; ++i) {
 #ifdef ELTWISE_DEST_REUSE_TYPE
+    #ifdef ARCH_QUASAR
+            binary_dest_reuse_tiles<ELTWISE_OP_TYPE, ELTWISE_DEST_REUSE_TYPE>(dfb_in0.get_id(), i, i);
+    #else
             binary_dest_reuse_tiles<ELTWISE_OP_TYPE, ELTWISE_DEST_REUSE_TYPE>(cb_inp0, i, i);
+    #endif
 #else
+    #ifdef ARCH_QUASAR
+            ELTWISE_OP(dfb_in0.get_id(), dfb_in1.get_id(), i, i, i);
+    #else
             ELTWISE_OP(cb_inp0, cb_inp1, i, i, i);
+    #endif
 #endif
 
 #ifdef SFPU_OP_CHAIN_0
@@ -80,12 +133,22 @@ void kernel_main() {
 
         tile_regs_wait();
         for (uint32_t i = 0; i < per_core_block_size; ++i) {
+        #ifdef ARCH_QUASAR
+            pack_tile(i, dfb_out.get_id());
+        #else
             pack_tile(i, cb_out0);
+        #endif
         }
         tile_regs_release();
 
-        cb_pop_front(cb_inp0, per_core_block_size);
-        cb_pop_front(cb_inp1, per_core_block_size);
-        cb_push_back(cb_out0, per_core_block_size);
+        #ifdef ARCH_QUASAR
+            dfb_in0.pop_front(per_core_block_size);
+            dfb_in1.pop_front(per_core_block_size);
+            dfb_out.push_back(per_core_block_size);
+        #else
+            cb_pop_front(cb_inp0, per_core_block_size);
+            cb_pop_front(cb_inp1, per_core_block_size);
+            cb_push_back(cb_out0, per_core_block_size);
+        #endif
     }
 }
