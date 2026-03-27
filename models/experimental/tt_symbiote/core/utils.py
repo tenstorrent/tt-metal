77ebd97aefc5e1d46d3c06a93365a56cc841fd90
Sreeshankar <sreeshankar.h@ignitarium.com>
2026-03-27 14:42:31 +0530

    Full model test working

--- models/experimental/tt_symbiote/core/utils.py
+++ models/experimental/tt_symbiote/core/utils.py
@@ -135,6 +135,7 @@ def flat_map_bypass(func, data):
     elif isinstance(data, list):
         return [flat_map_bypass(func, x) for x in data]
     return func(data)
+
 def safe_permute(tensor, dims):
     """Thin wrapper around ``ttnn.permute`` for symbiote modules (single import site)."""
     return ttnn.permute(tensor, tuple(dims))
