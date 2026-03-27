81a2018a069d1265158b77c8953296044a858968
Evan Smal <esmal@tenstorrent.com>
2026-03-27 17:50:04 +0000

    Remove dprints

--- models/demos/deepseek_v3_b1/fused_ops/decoder_block/kernels/decoder_block_kernel.cpp
+++ models/demos/deepseek_v3_b1/fused_ops/decoder_block/kernels/decoder_block_kernel.cpp
@@ -2724,20 +2724,14 @@ void kernel_main() {
             constexpr bool is_bcast_root = get_named_compile_time_arg_val("bcast_is_root") == 1;
             if constexpr (is_bcast_root && Core::is_sender_core) {
                 auto next_iter_sem = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(persistent_next_iter_sem_addr);
-                DPRINT << "Bcast sem wait iteration " << iteration << ENDL();
                 noc_semaphore_wait(next_iter_sem, 1);
-                DPRINT << "Bcast sem wait done iteration " << iteration << ENDL();
                 noc_semaphore_set(next_iter_sem, 0);
             }
         }
 #endif
-        // DPRINT << "Reconfig CB interfaces iteration " << iteration << ENDL();
         unified_kernels::reconfig_cb_interfaces(mla_cb_config);
-        // DPRINT << "Setup MLA sharded buffers iteration " << iteration << ENDL();
         setup_mla_sharded_buffers();
-        // DPRINT << "MLA body iteration " << iteration << ENDL();
         mla_body();
-        // DPRINT << "Reconfig MoE CB interfaces iteration " << iteration << ENDL();
         unified_kernels::reconfig_cb_interfaces(moe_cb_config);
 #if defined(COMPILE_FOR_BRISC)
         *pos_ptr += 1;
