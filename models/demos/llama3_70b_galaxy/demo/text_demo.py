3f41728ab9c41bf4d81eb4eb8a1a9816616a976c
Ivan Fabijanić <ctr-ifabijanic@ext.tenstorrent.com>
2026-03-27 16:04:16 +0100

    Removed Deepseek distil 70B perf numbers from checks

--- models/demos/llama3_70b_galaxy/demo/text_demo.py
+++ models/demos/llama3_70b_galaxy/demo/text_demo.py
@@ -1465,8 +1465,7 @@ def test_demo_text(
 
     PERFORMANCE_TARGETS = {
         "TG_Llama-3.1-70B": {"ttft": 73.00 if galaxy_type == "6U" else 99.00, "tsu": 71.5},
-        "TG_Llama-3.3-70B": {"ttft": 73.00 if galaxy_type == "6U" else 99.00, "tsu": 71.5},
-        "TG_Deepseek-R1-Distill-70B": {"ttft": 20.0, "tsu": 63.0},  # TODO Update target  # TODO Update target
+        "TG_Llama-3.3-70B": {"ttft": 73.00 if galaxy_type == "6U" else 99.00, "tsu": 71.5}
     }
 
     model_key = f"{model_args.device_name}_{model_args.base_model_name}"
