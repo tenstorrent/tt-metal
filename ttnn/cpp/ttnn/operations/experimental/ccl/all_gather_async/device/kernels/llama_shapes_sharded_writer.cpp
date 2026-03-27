ffd52cbdbf12e3fcdee4e843c432149e8c7b081a
llong <llong@tenstorrent.com>
2026-03-27 16:50:19 +0000

    switch from noc_async_write_barrier to noc_async_atomic_barrier.

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/device/kernels/llama_shapes_sharded_writer.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/device/kernels/llama_shapes_sharded_writer.cpp
@@ -186,5 +186,5 @@ void kernel_main() {
         noc_semaphore_set(reinterpret_cast<volatile tt_l1_ptr uint32_t*>(out_ready_sem_bank_addr), 0);
     }
 
-    noc_async_write_barrier();
+    noc_async_atomic_barrier();
 }
