e62dee3c72104076743806e9252179f3cac66e06
Yury Vinogradov <ctr-ivinogradov@ext.tenstorrent.com>
2026-03-27 14:31:33 +0100

    add custom dataset path

--- models/demos/audio/whisper/demo/demo.py
+++ models/demos/audio/whisper/demo/demo.py
@@ -430,7 +430,7 @@ def run_demo_whisper_for_conditional_generation_dataset(
     )
 
     # load data
-    if is_ci_env:
+    if os.getenv("CI") == "true":
         ds = load_dataset("/mnt/MLPerf/huggingface/hub/datasets--hf-internal-testing--librispeech_asr_dummy", "clean", split="validation")
     else:
         ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
