75bf77f3ffb57e86bf7120d2d3da341c4958d349
Saurabh Dawle <sdawle@tenstorrent.com>
2026-03-27 04:28:46 +0000

    Fix black formatting in utility_functions.py
    Add missing blank line before try/except block and remove redundant
    tuple parentheses to satisfy pre-commit black check.

--- models/common/utility_functions.py
+++ models/common/utility_functions.py
@@ -9,6 +9,7 @@ import time
 from typing import Union
 
 import numpy as np
+
 try:
     import pytest
 except ImportError:
@@ -958,7 +959,7 @@ def blocked_mm_with_conv_act(
                 act_address_map_index += 1
                 weight_address_map_this_block_size = weight_address_map[weight_address_map_index]
                 weight_address_map_index += 1
-                (mm_act_block, act_address_map_index) = read_conv_act_into_mm_act_block(
+                mm_act_block, act_address_map_index = read_conv_act_into_mm_act_block(
                     conv_act,
                     act_address_map_index,
                     act_address_map,
