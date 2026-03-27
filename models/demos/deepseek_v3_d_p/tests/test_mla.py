009e38ba7d741db41861ecd4c577e53f241fb653
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2026-03-27 15:17:15 +0000

    module test

--- models/demos/deepseek_v3_d_p/tests/test_mla.py
+++ models/demos/deepseek_v3_d_p/tests/test_mla.py
@@ -87,8 +87,8 @@ def random_weights(config_only):
 # sp x tp
 @pytest.mark.parametrize(
     "mesh_device",
-    [(8, 4), (2, 4)],
-    ids=["8x4", "2x4"],
+    [(32, 4), (8, 4), (2, 4)],
+    ids=["32x4", "8x4", "2x4"],
     indirect=True,
 )
 @pytest.mark.parametrize(
@@ -329,6 +329,14 @@ def test_mla(
         ).to(torch.bfloat16)
         tt_kvpe_cache_torch = tt_kvpe_cache_torch[:, :1, :, :]
 
+        logger.info("Starting synchronize call")
+        ttnn.synchronize_device(mesh_device)
+        logger.info("Synchronize call ended")
+
+        logger.debug("  Distributed synchronization started")
+        ttnn.distributed_context_barrier()
+        logger.debug("✓ Distributed synchronization completed")
+
         if is_balanced:
             tt_kvpe_cache_torch = reverse_reorder_tensor_chunks(tt_kvpe_cache_torch, chunk_order, seq_dim=2)
 
@@ -343,6 +351,12 @@ def test_mla(
         )
         logger.info(f"KVPE cache PE part PCC is {pe_pcc_message}")
     else:
+        logger.info("Starting synchronize call")
         ttnn.synchronize_device(mesh_device)
+        logger.info("Synchronize call ended")
+
+        logger.debug("  Distributed synchronization started")
+        ttnn.distributed_context_barrier()
+        logger.debug("✓ Distributed synchronization completed")
 
     logger.success(f"✓ Reference and TT comparison with {weight_type} weights successful")
