47d6e9c78c5bcec688da4d4cc7dbc519c09f229c
grechsteiner <grechsteiner@tenstorrent.com>
2026-03-28 21:08:18 +0000

    integrate fused packet

--- ttnn/cpp/ttnn/operations/experimental/ccl/deepseek_moe_reduce_scatter/device/kernels/deepseek_moe_reduce_scatter_writer.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/deepseek_moe_reduce_scatter/device/kernels/deepseek_moe_reduce_scatter_writer.cpp
@@ -133,27 +133,21 @@ void kernel_main() {
 
     // execute pre op barrier send phase
     uint64_t pre_op_barrier_semaphore_noc_address = safe_get_noc_addr(
-        pre_op_barrier_semaphore_noc0_x, pre_op_barrier_semaphore_noc0_y, pre_op_barrier_semaphore, 0);
+        pre_op_barrier_semaphore_noc0_x, pre_op_barrier_semaphore_noc0_y, pre_op_barrier_semaphore, 1);
     fabric_unicast_noc_unicast_atomic_inc(
         fabric_connection,
         unicast_sem_inc_route_id,
         tt::tt_fabric::NocUnicastAtomicIncCommandHeader{pre_op_barrier_semaphore_noc_address, 1, false},
         unicast_num_hops);
 
-    // set state for op semaphore
-    uint64_t op_semaphore_noc_address = safe_get_noc_addr(op_semaphore_noc0_x, op_semaphore_noc0_y, op_semaphore, 0);
-    fabric_unicast_noc_unicast_atomic_inc_set_state<UnicastAtomicIncUpdateMask::All>(
-        fabric_connection,
-        unicast_sem_inc_route_id,
-        unicast_num_hops,
-        tt::tt_fabric::NocUnicastAtomicIncCommandHeader{op_semaphore_noc_address, 1, true});
-
-    // set state for scatter write
-    fabric_unicast_noc_scatter_write_set_state<UnicastScatterWriteUpdateMask::All>(
+    // set state for scatter write and op semaphore
+    uint64_t op_semaphore_noc_address = safe_get_noc_addr(op_semaphore_noc0_x, op_semaphore_noc0_y, op_semaphore, 1);
+    fabric_unicast_noc_fused_scatter_write_atomic_inc_set_state<UnicastFusedScatterWriteAtomicIncUpdateMask::All>(
         fabric_connection,
         unicast_scatter_write_route_id,
         unicast_num_hops,
-        NocUnicastScatterCommandHeader({0, 0}, {static_cast<uint16_t>(page_size)}),
+        tt::tt_fabric::NocUnicastScatterAtomicIncFusedCommandHeader(
+            {0, 0}, op_semaphore_noc_address, {static_cast<uint16_t>(page_size)}, 1, true),
         page_size * 2);
 
     // execute pre op barrier wait phase
@@ -230,28 +224,26 @@ void kernel_main() {
 
                 // op hardcoded for each worker handling even multiple of 2 tiles, so always use scatter_write
                 cb_wait_front(reduced_cb_id, tile_granularity);
-                size_t intermediate_slice_l1_read_addr = get_read_ptr(reduced_cb_id);
-                fabric_unicast_noc_scatter_write_with_state<UnicastScatterWriteUpdateMask::DstAddrs>(
+                uint32_t intermediate_slice_l1_read_addr = get_read_ptr(reduced_cb_id);
+                fabric_unicast_noc_fused_scatter_write_atomic_inc_with_state<
+                    UnicastFusedScatterWriteAtomicIncUpdateMask::WriteDstAddrs>(
                     fabric_connection,
                     unicast_scatter_write_route_id,
                     intermediate_slice_l1_read_addr,
-                    NocUnicastScatterCommandHeader(
+                    tt::tt_fabric::NocUnicastScatterAtomicIncFusedCommandHeader(
                         {intermediate_slice_noc_address_one, intermediate_slice_noc_address_two},
-                        {static_cast<uint16_t>(page_size)}));
+                        op_semaphore_noc_address,
+                        {static_cast<uint16_t>(page_size)},
+                        1,
+                        true));
 
                 noc_async_writes_flushed();
                 cb_pop_front(reduced_cb_id, tile_granularity);
-
-                // TODO: #35925 fuse with data packet after support is added by fabric team
-                fabric_unicast_noc_unicast_atomic_inc_with_state<UnicastAtomicIncUpdateMask::None>(
-                    fabric_connection,
-                    unicast_sem_inc_route_id,
-                    tt::tt_fabric::NocUnicastAtomicIncCommandHeader{op_semaphore_noc_address, 1, true});
             }
         } else {
             while (tiles_read < tiles_to_read) {
                 cb_wait_front(reduced_cb_id, tile_granularity);
-                size_t output_l1_read_addr = get_read_ptr(reduced_cb_id);
+                uint32_t output_l1_read_addr = get_read_ptr(reduced_cb_id);
                 for (uint32_t j = 0; j < tile_granularity; ++j) {
                     noc_async_write_page(tiles_read, output_tensor_accessor, output_l1_read_addr);
                     output_l1_read_addr += page_size;
