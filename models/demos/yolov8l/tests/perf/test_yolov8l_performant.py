20cad5b691367e9152e47f8c7801cf072a5363c2
ign-christy <christy.varghese@ignitarium.com>
2026-03-30 13:23:38 +0000

    Added yolov8l infra, WIP

---
+++ models/demos/yolov8l/tests/perf/test_yolov8l_performant.py
@@ -0,0 +1,54 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+
+from models.demos.yolov8l.common import YOLOV8L_L1_SMALL_SIZE
+from models.demos.yolov8l.runner.yolov8l_performant import (
+    run_yolov8l_inference,
+    run_yolov8l_trace_2cqs_inference,
+    run_yolov8l_trace_inference,
+)
+
+
+@pytest.mark.parametrize("device_params", [{"l1_small_size": YOLOV8L_L1_SMALL_SIZE}], indirect=True)
+@pytest.mark.parametrize("device_batch_size", [(1)])
+def test_run_yolov8l_inference(
+    device,
+    device_batch_size,
+):
+    run_yolov8l_inference(
+        device,
+        device_batch_size,
+    )
+
+
+@pytest.mark.parametrize(
+    "device_params", [{"l1_small_size": YOLOV8L_L1_SMALL_SIZE, "trace_region_size": 3686400}], indirect=True
+)
+@pytest.mark.parametrize("device_batch_size", [(1)])
+def test_run_yolov8l_trace_inference(
+    device,
+    device_batch_size,
+):
+    run_yolov8l_trace_inference(
+        device,
+        device_batch_size,
+    )
+
+
+@pytest.mark.parametrize(
+    "device_params",
+    [{"l1_small_size": YOLOV8L_L1_SMALL_SIZE, "trace_region_size": 3686400, "num_command_queues": 2}],
+    indirect=True,
+)
+@pytest.mark.parametrize("device_batch_size", [(1)])
+def test_run_yolov8l_trace_2cq_inference(
+    device,
+    device_batch_size,
+):
+    run_yolov8l_trace_2cqs_inference(
+        device=device,
+        device_batch_size=device_batch_size,
+    )
