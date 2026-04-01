36c79e8edc0508e674f64a55fb47048ca4a8466e
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2026-04-01 14:14:36 +0000

    profiler

--- models/demos/deepseek_v3_d_p/tt/mla/mla.py
+++ models/demos/deepseek_v3_d_p/tt/mla/mla.py
@@ -480,6 +480,11 @@ class ttMLA:
             **self._get_mm_kwargs("wkv_b2", seq_len_local),
         )
 
+        from tracy import Profiler
+
+        profiler = Profiler()
+
+        profiler.enable()
         ttnn.synchronize_device(self.mesh_device)
         ttnn.distributed_context_barrier()
 
@@ -513,6 +518,8 @@ class ttMLA:
             is_balanced=self.is_balanced,
         )
         ttnn.synchronize_device(self.mesh_device)
+        profiler.disable()
+
         # ttnn.distributed_context_barrier()
         end = time.time()
         logger.debug(f"SDPA time: {end - start}s")
