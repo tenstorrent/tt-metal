1202970dcf7ecd2dc660a88e43434640dd14837e
suzusuzu <s.suzugamine@gmail.com>
2026-04-02 01:54:10 +0000

    Apply the same change as #41062 to remove the ttnn decorator

--- ttnn/cpp/ttnn/operations/randn/randn.hpp
+++ ttnn/cpp/ttnn/operations/randn/randn.hpp
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #pragma once
-#include "ttnn/decorators.hpp"
+#include "ttnn/types.hpp"
 #include "ttnn/operations/core/compute_kernel/compute_kernel_config.hpp"
 
 namespace ttnn {
