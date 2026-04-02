6f857dffb3c4a0b378db58010d72867d796e1af9
Audrey Kertesz <akertesz@tenstorrent.com>
2026-04-02 23:31:00 +0000

    Re-disable Quasar tests. Will enable in a new PR.

--- tests/tt_metal/tt_metal/api/metal2_host_api/test_program_spec.cpp
+++ tests/tt_metal/tt_metal/api/metal2_host_api/test_program_spec.cpp
@@ -42,6 +42,7 @@ using test_helpers::MakeMinimalWorker;
 class ProgramSpecTestQuasar : public ::testing::Test {
 protected:
     void SetUp() override {
+        GTEST_SKIP() << "Re-enable tests after Quasar mock device support is checked in";
         //  Configure global mock mode for Quasar
         //  This way, the HAL is initialized for arch check and Program creation.
         experimental::configure_mock_mode(tt::ARCH::QUASAR, 1);
