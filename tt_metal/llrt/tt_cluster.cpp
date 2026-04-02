e5a30e8cd43b6338941328ac1bc95a9d552cf024
Audrey Kertesz <akertesz@tenstorrent.com>
2026-04-02 14:42:29 -0600

    Add Quasar mock device support (#41170)
    ### Ticket
    GitHub issue: https://github.com/tenstorrent/tt-metal/issues/40982
    UMD PR: https://github.com/tenstorrent/tt-umd/pull/2396
    
    ### PR Type
    New feature
    
    ### Problem description
    We want a Quasar mock device to enable API testing (see
    https://github.com/tenstorrent/tt-metal/pull/41169).
    This gives us a functional HAL without requiring the Zebu emulator.
    
    ### What's changed
    
    - Add a Quasar mock device, with:
         - SOC descriptor `quasar_32_arch.yaml` (pre-existing)
    - Fictional cluster descriptor `quasar_Q1.yaml` ([UMD
    PR](https://github.com/tenstorrent/tt-umd/pull/2396)).
    - Add null checks for Watcher server in `kernel.cpp`. 
    - This was necessary to circumvent a crash when creating kernels in mock
    mode.
    
    ### Checklist
    
    - [ ] [![Sanity
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/sanity-tests.yaml/badge.svg?branch=akertesz/quasar_mockdevice)](https://github.com/tenstorrent/tt-metal/actions/workflows/sanity-tests.yaml?query=branch:akertesz/quasar_mockdevice)
    - [ ] [![Blackhole Post
    commit](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml/badge.svg?branch=akertesz/quasar_mockdevice)](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml?query=branch:akertesz/quasar_mockdevice)
    - [ ]
    [![cpp-unit-tests](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml/badge.svg?branch=akertesz/quasar_mockdevice)](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml?query=branch:akertesz/quasar_mockdevice)
    - [x] New/Existing tests provide coverage for changes
    - [x] (Optional) Ran [clang-tidy code
    analysis](https://github.com/tenstorrent/tt-metal/actions/workflows/code-analysis.yaml)
    on the PR branch to catch linting errors early

--- tt_metal/llrt/tt_cluster.cpp
+++ tt_metal/llrt/tt_cluster.cpp
@@ -62,7 +62,7 @@ inline std::string get_soc_description_file(
     switch (arch) {
         case tt::ARCH::WORMHOLE_B0: file = "wormhole_b0_80_arch.yaml"; break;
         case tt::ARCH::BLACKHOLE: file = "blackhole_140_arch.yaml"; break;
-        case tt::ARCH::QUASAR:  // Quasar is currently only supported for simulation
+        case tt::ARCH::QUASAR: file = "quasar_32_arch.yaml"; break;
         default: throw std::runtime_error("Unsupported device arch");
     }
     path += file;
