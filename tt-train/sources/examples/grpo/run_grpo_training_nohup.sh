c23fbc0be4e53a3cdb9590adf3b87437755a252b
Igor Chovpan <ichovpan@tenstorrent.com>
2026-04-02 23:03:22 +0000

    grpo .sh scripts: pass argument to the python3 script

--- tt-train/sources/examples/grpo/run_grpo_training_nohup.sh
+++ tt-train/sources/examples/grpo/run_grpo_training_nohup.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 cd "$(cd "$(dirname "${BASH_SOURCE[0]}")/../../../../" && pwd)"
-nohup python tt-train/sources/examples/grpo/grpo_training.py &
+nohup python tt-train/sources/examples/grpo/grpo_training.py $@ &
 echo "PID: $!"
