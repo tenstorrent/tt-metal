58a1fa5e300ef34ef69931ede5ad6791fd27cb08
Pavlo Hilei <45758974+philei-tt@users.noreply.github.com>
2026-04-03 20:12:58 +0200

    [tt-train] fix double bf16 parameters allocation in fp32 adamw (#41385)
    ### Ticket
    N/A
    
    ### Problem description
    Fix autocast tensor always creating bf16 copy for fp32 tensors. 
    Reduces AdamW FullPrecision optimizer state by a factor of **1.5**
    
    ### What's changed
    - Create copy of different precision lazily, when requested via
    get_tensor()
    - Add tests
    
    tinyllama / char tokenizer / AdamWFullPrecision memory usage change: 
    
    <img width="1782" height="1185" alt="image"
    src="https://github.com/user-attachments/assets/4ed3398f-1378-4557-9273-0a80dd739d61"
    />
    
    
    ### Checklist
    
    - [ ] [![Sanity
    tests](https://github.com/tenstorrent/tt-metal/actions/workflows/sanity-tests.yaml/badge.svg?branch=philei/fix-double-bf16-parameters-allocation-in-fp32)](https://github.com/tenstorrent/tt-metal/actions/workflows/sanity-tests.yaml?query=branch:philei/fix-double-bf16-parameters-allocation-in-fp32)

--- tt-train/sources/ttml/autograd/autocast_tensor.hpp
+++ tt-train/sources/ttml/autograd/autocast_tensor.hpp
@@ -12,8 +12,8 @@ namespace ttml::autograd {
 enum class PreferredPrecision : uint8_t { HALF = 0, FULL = 1 };
 
 class AutocastTensor {
-    tt::tt_metal::Tensor m_half_precision_tensor{};
-    tt::tt_metal::Tensor m_full_precision_tensor{};
+    mutable tt::tt_metal::Tensor m_half_precision_tensor{};
+    mutable tt::tt_metal::Tensor m_full_precision_tensor{};
 
 public:
     AutocastTensor() = default;
@@ -27,6 +27,9 @@ public:
     void set_tensor(const tt::tt_metal::Tensor &tensor);
     [[nodiscard]] const tt::tt_metal::Tensor &get_tensor(
         PreferredPrecision preferred_precision = PreferredPrecision::HALF) const;
+
+    [[nodiscard]] bool has_half() const;
+    [[nodiscard]] bool has_full() const;
 };
 
 }  // namespace ttml::autograd
