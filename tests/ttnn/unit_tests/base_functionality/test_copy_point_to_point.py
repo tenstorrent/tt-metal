da53d6ad2ea07b2fa7c1928a64ff80492fd19eff
awliu-TT <awliu@tenstorrent.com>
2026-04-07 08:07:31 +0000

    adding license

--- tests/ttnn/unit_tests/base_functionality/test_copy_point_to_point.py
+++ tests/ttnn/unit_tests/base_functionality/test_copy_point_to_point.py
@@ -1,12 +1,16 @@
+# SPDX-FileCopyrightText: © 2026 Tenstorrent AI ULC.
+
+# SPDX-License-Identifier: Apache-2.0
 import pytest
 
 import torch
 
 import ttnn
 
+
 @pytest.mark.parametrize("device_params", [{"fabric_config": ttnn.FabricConfig.FABRIC_1D}], indirect=True)
 @pytest.mark.parametrize("mesh_device", [(1, 2)], indirect=True)
-@pytest.mark.parametrize("shape", [(30, 60), (10, 10, 30, 60), (16, 64) ])
+@pytest.mark.parametrize("shape", [(30, 60), (10, 10, 30, 60), (16, 64)])
 def test_copy_output_as_point_to_point_preallocated(mesh_device, shape):
     coord0 = ttnn.MeshCoordinate(0, 0)
     coord1 = ttnn.MeshCoordinate(0, 1)
