a168a04ed04f262cefebc7c7ef42eab881b11f47
John Bauman <jbauman@tenstorrent.com>
2026-04-08 15:55:10 +0000

    Remove some

--- tt_metal/impl/dispatch/kernel_config/fd_kernel.hpp
+++ tt_metal/impl/dispatch/kernel_config/fd_kernel.hpp
@@ -116,34 +116,7 @@ public:
         get_control_plane_(get_control_plane),
         get_dispatch_query_manager_(get_dispatch_query_manager),
         get_max_num_eth_cores_(get_max_num_eth_cores),
-        get_reads_dispatch_cores_(get_reads_dispatch_cores) {
-        bool is_galaxy_cluster = descriptor_.cluster().is_galaxy_cluster();
-        bool are_fd_kernels_on_same_core = descriptor.cluster().arch() == tt::ARCH::QUASAR && descriptor.num_cqs() == 1;
-        dispatch_mem_map_[enchantum::to_underlying(CoreType::WORKER)] = std::make_unique<tt::tt_metal::DispatchMemMap>(
-            CoreType::WORKER,
-            descriptor.num_cqs(),
-            descriptor.hal(),
-            is_galaxy_cluster,
-            are_fd_kernels_on_same_core,
-            descriptor.rtoptions());
-        dispatch_mem_map_[enchantum::to_underlying(CoreType::ETH)] = std::make_unique<tt::tt_metal::DispatchMemMap>(
-            CoreType::ETH,
-            descriptor.num_cqs(),
-            descriptor.hal(),
-            is_galaxy_cluster,
-            /*are_fd_kernels_on_same_core=*/false,
-            descriptor.rtoptions());
-        if (descriptor.cluster().arch() == tt::ARCH::QUASAR &&
-            descriptor.hal().has_programmable_core_type(HalProgrammableCoreType::DISPATCH)) {
-            dispatch_mem_map_[enchantum::to_underlying(CoreType::DISPATCH)] = std::make_unique<tt::tt_metal::DispatchMemMap>(
-                CoreType::DISPATCH,
-                descriptor.num_cqs(),
-                descriptor.hal(),
-                is_galaxy_cluster,
-                are_fd_kernels_on_same_core,
-                descriptor.rtoptions());
-        }
-    }
+        get_reads_dispatch_cores_(get_reads_dispatch_cores) {}
     virtual ~FDKernel() = default;
 
     // Populate the static configs for this kernel (ones that do not depend on configs from other kernels), including
@@ -217,6 +190,8 @@ public:
     uint32_t get_max_num_eth_cores() const;
 
 protected:
+    const DispatchMemMap& get_dispatch_mem_map() const;
+
     // Attributes for an EDM client to connect to the router
     struct FDKernelEdmConnectionAttributes {
         size_t worker_flow_control_sem{0};
@@ -272,7 +247,6 @@ protected:
     GetDispatchQueryManagerFn get_dispatch_query_manager_;
     GetMaxNumEthCoresFn get_max_num_eth_cores_;
     GetReadsDispatchCoresFn get_reads_dispatch_cores_;
-    std::array<std::unique_ptr<DispatchMemMap>, static_cast<size_t>(CoreType::COUNT)> dispatch_mem_map_;
 };
 
 }  // namespace tt::tt_metal
--- tt_metal/impl/dispatch/topology.cpp
+++ tt_metal/impl/dispatch/topology.cpp
@@ -31,6 +31,7 @@
 #include <umd/device/types/core_coordinates.hpp>
 #include <umd/device/types/xy_pair.hpp>
 #include "dispatch_mem_map.hpp"
+#include "impl/context/metal_context.hpp"
 #include <llrt/tt_cluster.hpp>
 #include "dispatch_core_manager.hpp"
 
@@ -418,32 +419,6 @@ DispatchTopology::DispatchTopology(
     get_max_num_eth_cores_(get_max_num_eth_cores),
     get_reads_dispatch_cores_(get_reads_dispatch_cores) {
     command_queue_compile_group_ = std::make_unique<detail::ProgramCompileGroup>();
-    bool is_galaxy_cluster = descriptor_.cluster().is_galaxy_cluster();
-    bool are_fd_kernels_on_same_core = descriptor_.cluster().arch() == tt::ARCH::QUASAR && descriptor_.num_cqs() == 1;
-    dispatch_mem_map_[enchantum::to_underlying(CoreType::WORKER)] = std::make_unique<DispatchMemMap>(
-        CoreType::WORKER,
-        descriptor_.num_cqs(),
-        descriptor_.hal(),
-        is_galaxy_cluster,
-        are_fd_kernels_on_same_core,
-        descriptor_.rtoptions());
-    dispatch_mem_map_[enchantum::to_underlying(CoreType::ETH)] = std::make_unique<DispatchMemMap>(
-        CoreType::ETH,
-        descriptor_.num_cqs(),
-        descriptor_.hal(),
-        is_galaxy_cluster,
-        /*are_fd_kernels_on_same_core=*/false,
-        descriptor_.rtoptions());
-    if (descriptor_.cluster().arch() == tt::ARCH::QUASAR &&
-        descriptor_.hal().has_programmable_core_type(HalProgrammableCoreType::DISPATCH)) {
-        dispatch_mem_map_[enchantum::to_underlying(CoreType::DISPATCH)] = std::make_unique<DispatchMemMap>(
-            CoreType::DISPATCH,
-            descriptor_.num_cqs(),
-            descriptor_.hal(),
-            is_galaxy_cluster,
-            are_fd_kernels_on_same_core,
-            descriptor_.rtoptions());
-    }
 }
 
 DispatchTopology::~DispatchTopology() { reset(); }
@@ -795,7 +770,7 @@ void DispatchTopology::configure_dispatch_cores(Device* device) {
     // Set up completion_queue_writer core. This doesn't actually have a kernel so keep it out of the struct and config
     // it here. TODO: should this be in the struct?
     CoreType dispatch_core_type = this->dispatch_core_manager_.get_dispatch_core_type();
-    const auto& my_dispatch_constants = *dispatch_mem_map_[enchantum::to_underlying(dispatch_core_type)];
+    const auto& my_dispatch_constants = descriptor_.metal_context().dispatch_mem_map();
     uint32_t cq_start = my_dispatch_constants.get_host_command_queue_addr(CommandQueueHostAddrType::UNRESERVED);
     uint32_t cq_size = device->sysmem_manager().get_cq_size();
     std::vector<uint32_t> zero = {0x0};
