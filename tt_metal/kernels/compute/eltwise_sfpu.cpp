558265ce41983a1e0f721dd1df0c1e103b40640c
Ryan Zhu <ryanzhu@tenstorrent.com>
2026-04-09 20:40:06 +0000

    update test and kernels to use quasar dm apis

--- tt_metal/kernels/compute/eltwise_sfpu.cpp
+++ tt_metal/kernels/compute/eltwise_sfpu.cpp
@@ -8,30 +8,58 @@
 #include "api/compute/eltwise_unary/eltwise_unary.h"
 #include "api/compute/eltwise_unary/sfpu_split_includes.h"
 
+#ifdef ARCH_QUASAR
+#include "experimental/dataflow_buffer.h"
+#endif
+
 void kernel_main() {
     uint32_t per_core_block_cnt = get_compile_time_arg_val(0);
     uint32_t per_core_block_dim = get_compile_time_arg_val(1);
 
+#ifdef ARCH_QUASAR
+    constexpr uint32_t dfb_in_id = get_compile_time_arg_val(2);
+    constexpr uint32_t dfb_out_id = get_compile_time_arg_val(3);
+    experimental::DataflowBuffer dfb_in(dfb_in_id);
+    experimental::DataflowBuffer dfb_out(dfb_out_id);
+    init_sfpu(dfb_in.get_id(), dfb_out.get_id());
+#else
     init_sfpu(tt::CBIndex::c_0, tt::CBIndex::c_16);
+#endif
     for (uint32_t block_index = 0; block_index < per_core_block_cnt; block_index++) {
+#ifdef ARCH_QUASAR
+        dfb_out.reserve_back(per_core_block_dim);
+#else
         cb_reserve_back(tt::CBIndex::c_16, per_core_block_dim);
+#endif
         for (uint32_t tile_index = 0; tile_index < per_core_block_dim; ++tile_index) {
             tile_regs_acquire();
 
             // Pop tile after tile, copy to DST and pack
+#ifdef ARCH_QUASAR
+            dfb_in.wait_front(1);
+            copy_tile(dfb_in.get_id(), 0, 0);
+#else
             cb_wait_front(tt::CBIndex::c_0, 1);
             copy_tile(tt::CBIndex::c_0, 0, 0);
-
+#endif
 #ifdef SFPU_OP_CHAIN_0
             SFPU_OP_CHAIN_0
 #endif
             tile_regs_commit();
             tile_regs_wait();
+#ifdef ARCH_QUASAR
+            pack_tile(0, dfb_out.get_id());
+            dfb_in.pop_front(1);
+#else
             pack_tile(0, tt::CBIndex::c_16);
-
             cb_pop_front(tt::CBIndex::c_0, 1);
+#endif
             tile_regs_release();
         }
+#ifdef ARCH_QUASAR
+        dfb_out.push_back(per_core_block_dim);
+#else
         cb_push_back(tt::CBIndex::c_16, per_core_block_dim);
+#endif
     }
 }
--- tt_metal/kernels/dataflow/reader_unary.cpp
+++ tt_metal/kernels/dataflow/reader_unary.cpp
@@ -5,20 +5,48 @@
 #include <stdint.h>
 
 #include "api/dataflow/dataflow_api.h"
+#ifdef ARCH_QUASAR
+#include "experimental/dataflow_buffer.h"
+#else
+#include "experimental/circular_buffer.h"
+#endif
+#include "experimental/endpoints.h"
 
 void kernel_main() {
     uint32_t src_addr  = get_arg_val<uint32_t>(0);
     uint32_t bank_id = get_arg_val<uint32_t>(1);
     uint32_t num_tiles = get_arg_val<uint32_t>(2);
 
+#ifdef ARCH_QUASAR
+    uint32_t dfb_id = get_compile_time_arg_val(0);
+    experimental::DataflowBuffer dfb_in(dfb_id);
+#else
     constexpr uint32_t cb_id_in0 = 0;
+#endif
 
     // ublocks size defined in tiles
     constexpr uint32_t ublock_size_tiles = 1;
+#ifdef ARCH_QUASAR
+    uint32_t ublock_size_bytes = dfb_in.get_entry_size() * ublock_size_tiles;
+#else
     uint32_t ublock_size_bytes = get_tile_size(cb_id_in0) * ublock_size_tiles;
+#endif
+
+    experimental::Noc noc;
 
     // read a ublock of tiles from src to CB, and then push the ublock to unpacker
     for (uint32_t i = 0; i < num_tiles; i += ublock_size_tiles) {
+#ifdef ARCH_QUASAR
+        dfb_in.reserve_back(ublock_size_tiles);
+        noc.async_read(
+            experimental::AllocatorBank<experimental::AllocatorBankType::DRAM>{},
+            dfb_in,
+            ublock_size_bytes,
+            {.bank_id = bank_id, .addr = src_addr},
+            {});
+        noc.async_read_barrier();
+        dfb_in.push_back(ublock_size_tiles);
+#else
         uint64_t src_noc_addr = get_noc_addr_from_bank_id<true>(bank_id, src_addr);
 
         cb_reserve_back(cb_id_in0, ublock_size_tiles);
@@ -28,6 +56,7 @@ void kernel_main() {
         noc_async_read_barrier();
 
         cb_push_back(cb_id_in0, ublock_size_tiles);
+#endif
         src_addr += ublock_size_bytes;
     }
 }
