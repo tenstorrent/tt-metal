4769784bb95384ceb1ea21ba4e9f051a746dc3eb
awliu-TT <awliu@tenstorrent.com>
2026-04-11 10:13:51 +0000

    decltype hack to preserve this weird paging of point_to_point

--- ttnn/cpp/ttnn/operations/point_to_point/device/kernels/dataflow/reader_receive.cpp
+++ ttnn/cpp/ttnn/operations/point_to_point/device/kernels/dataflow/reader_receive.cpp
@@ -56,7 +56,8 @@ void kernel_main() {
 
     fabric_connection.close();
 
-    const auto packet_buffer = TensorAccessor(packet_buffer_args, intermediate_base_addr);
+    const decltype(TensorAccessor(packet_buffer_args, intermediate_base_addr)) packet_buffer(
+        packet_buffer_args, intermediate_base_addr, packet_size_bytes);
 
     cb_reserve_back(packet_cb_id, 1);
     const uint64_t packet_l1_addr = get_write_ptr(packet_cb_id);
