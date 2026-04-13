6c491bd1a3739cdc7678cc7da2a8702f98471954
Sreeshankar <sreeshankar.h@ignitarium.com>
2026-04-13 12:22:58 +0000

    BGE-M3 device time imporve by 9 mili sec on n300

--- models/experimental/SSD512/tests/perf/test_device_perf_ssd.py
+++ models/experimental/SSD512/tests/perf/test_device_perf_ssd.py
@@ -5,6 +5,7 @@ import pytest
 from models.perf.device_perf_utils import check_device_perf, prep_device_perf_report, run_device_perf
 
 
+@pytest.mark.timeout(900)
 @pytest.mark.parametrize(
     "batch_size, model_name, expected_perf",
     [
@@ -17,14 +18,14 @@ def test_perf_device_bare_metal_ssd512(batch_size, model_name, expected_perf):
     num_iterations = 1
     margin = 0.04
 
-    command = f"pytest models/experimental/SSD512/tests/pcc/test_ssd.py"
+    command = "pytest models/experimental/SSD512/tests/pcc/test_ssd.py"
 
     cols = ["DEVICE FW", "DEVICE KERNEL", "DEVICE BRISC KERNEL"]
 
     inference_time_key = "AVG DEVICE KERNEL SAMPLES/S"
     expected_perf_cols = {inference_time_key: expected_perf}
 
-    post_processed_results = run_device_perf(command, subdir, num_iterations, cols, batch_size, has_signposts=True)
+    post_processed_results = run_device_perf(command, subdir, num_iterations, cols, batch_size, has_signposts=False)
     expected_results = check_device_perf(post_processed_results, margin, expected_perf_cols)
     prep_device_perf_report(
         model_name=f"ttnn_functional_{model_name}_{batch_size}",
