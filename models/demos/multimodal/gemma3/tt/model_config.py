deab6592649b37d5423f39185cab6b7367bf3411
Pooja Susan Korah <pooja.korah@ignitarium.com>
2026-04-14 08:05:51 +0000

    sampling changes :import fix

--- models/demos/multimodal/gemma3/tt/model_config.py
+++ models/demos/multimodal/gemma3/tt/model_config.py
@@ -12,9 +12,18 @@ from loguru import logger
 import ttnn
 from models.common.utility_functions import is_blackhole, is_wormhole_b0
 from models.demos.multimodal.gemma3.tt.load_checkpoints import convert_vision_hf_to_meta, convert_vision_meta_to_hf
-from models.tt_transformers.tt.common import calculate_prefill_warmup_seq_lens, cap_seq_lens_to_max_prefill_chunk_size
+from models.tt_transformers.tt.common import (
+    Mode,
+    calculate_prefill_warmup_seq_lens,
+    cap_seq_lens_to_max_prefill_chunk_size,
+)
 from models.tt_transformers.tt.load_checkpoints import convert_hf_to_meta, convert_meta_to_hf, standardize_hf_keys
-from models.tt_transformers.tt.model_config import HfAttentionWrapper, HfDecoderWrapper, HfModelWrapper
+from models.tt_transformers.tt.model_config import (
+    HfAttentionWrapper,
+    HfDecoderWrapper,
+    HfModelWrapper,
+    MathFidelitySetting,
+)
 from models.tt_transformers.tt.model_config import ModelArgs as TTModelArgs
 from models.tt_transformers.tt.model_config import OpGroup
 
