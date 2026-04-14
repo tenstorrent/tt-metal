8264552c312b225918a76f85a59d0a1a47634203
ign-venkatesh <venkatesh.b@ignitarium.com>
2026-04-14 12:02:52 +0000

    Converted Attention MLP to ttnn

--- models/demos/qwen3_tts/tt/rmsnorm.py
+++ models/demos/qwen3_tts/tt/rmsnorm.py
@@ -141,7 +141,7 @@ class QKNorm(LightweightModule):
         )
 
         self.compute_kernel_config = ttnn.WormholeComputeKernelConfig(
-            math_fidelity=ttnn.MathFidelity.HiFi4,
+            math_fidelity=ttnn.MathFidelity.HiFi2,
             math_approx_mode=False,
             fp32_dest_acc_en=True,
             packer_l1_acc=True,
