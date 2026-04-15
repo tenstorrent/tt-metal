e7f14d979d76c69b7e33bd12b513a39b33234d20
Vladimir Ignjatijevic <vignjatijevic@tenstorrent.com>
2026-04-15 09:17:09 +0000

    [ttnn-unary-sfpu-operation-tester] fix 1: add Python binding and fix include paths

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_softcap.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_softcap.h
@@ -6,7 +6,7 @@
 
 #include "llk_math_eltwise_unary_sfpu_init.h"
 #include "llk_math_eltwise_unary_sfpu_params.h"
-#include "ckernel_sfpu_softcap.h"
+#include "sfpu/ckernel_sfpu_softcap.h"
 
 namespace ckernel {
 
