d3e65ff10df1e1b68b7a42c5786b1ad5271d8883
Ryan Zhu <ryanzhu@tenstorrent.com>
2026-04-15 16:20:19 +0000

    initial commit

---
+++ tt_metal/hw/ckernels/quasar/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_sigmoid.h
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: © 2026 Tenstorrent USA, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "llk_math_eltwise_unary_sfpu_init.h"
+#include "llk_math_eltwise_unary_sfpu_common.h"
+#include "ckernel_sfpu_sigmoid.h"
+
+namespace ckernel {
+
+template <bool APPROXIMATE>
+inline void llk_math_eltwise_unary_sfpu_sigmoid_init() {
+    llk_math_eltwise_unary_sfpu_init<SfpuType::sigmoid, APPROXIMATE>();
+}
+
+template <bool APPROXIMATE, bool is_fp32_dest_acc_en, int ITERATIONS = 8>
+inline void llk_math_eltwise_unary_sfpu_sigmoid(uint dst_index, int vector_mode = (int)VectorMode::RC) {
+    _llk_math_eltwise_unary_sfpu_params_<APPROXIMATE>(sfpu::calculate_sigmoid, dst_index, ITERATIONS);
+}
+
+}  // namespace ckernel
---
+++ tt_metal/hw/ckernels/quasar/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_silu.h
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: © 2026 Tenstorrent USA, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "llk_math_eltwise_unary_sfpu_init.h"
+#include "llk_math_eltwise_unary_sfpu_params.h"
+#include "ckernel_sfpu_silu.h"
+
+namespace ckernel {
+
+template <bool APPROXIMATE>
+inline void llk_math_eltwise_unary_sfpu_silu_init() {
+    llk_math_eltwise_unary_sfpu_init<SfpuType::silu, APPROXIMATE>();
+}
+
+template <bool APPROXIMATE, bool is_fp32_dest_acc_en, int ITERATIONS = 8>
+inline void llk_math_eltwise_unary_sfpu_silu(uint dst_index) {
+    _llk_math_eltwise_unary_sfpu_params_<APPROXIMATE>(ckernel::sfpu::calculate_silu, dst_index, ITERATIONS);
+}
+
+}  // namespace ckernel
