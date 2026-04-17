df52d95de798f68cee9cd33baa6b240b9d3f6e85
Aliaksei Sala <ctr-asala@ext.tenstorrent.com>
2026-04-16 19:15:45 -0700

    Fix reader kernel with multiple reserve cb

--- ttnn/cpp/ttnn/operations/eltwise/binary_ng/device/kernels_ng/dataflow/reader_interleaved_dram_optimized.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/binary_ng/device/kernels_ng/dataflow/reader_interleaved_dram_optimized.cpp
@@ -31,7 +31,6 @@ void kernel_main() {
     const auto a_tensor = TensorAccessor(src_a_args, a_addr, get_tile_size(cb_a));
     const auto b_tensor = TensorAccessor(src_b_args, b_addr, get_tile_size(cb_b));
 
-    constexpr uint32_t num_tiles_per_cycle = 1;  // c_args.num_tiles_per_cycle;
     const uint32_t a_tile_size = get_tile_size(cb_a);
     const uint32_t b_tile_size = get_tile_size(cb_b);
 
@@ -44,22 +43,33 @@ void kernel_main() {
     uint32_t trid_to_wait = trid;
 
     const uint32_t large_chunk = num_batches * num_tiles_per_batch;
-    uint32_t remaining = num_tiles;
+    uint32_t remaining_tiles = num_tiles;
     bool first_iter = true;
     uint32_t prev_chunk = 0;
 
-    cb_reserve_back(cb_a, large_chunk);
-    cb_reserve_back(cb_b, large_chunk);
+    auto get_num_tiles = [&](uint32_t remaining_tiles) {
+        if (remaining_tiles >= large_chunk) {
+            return large_chunk;
+        } else if (remaining_tiles >= num_tiles_per_batch) {
+            return num_tiles_per_batch;
+        }
+        return remaining_tiles;
+    };
+    {
+        uint32_t chunk = get_num_tiles(remaining_tiles);
+        uint32_t chunk2 = get_num_tiles(remaining_tiles - chunk);
+        cb_reserve_back(cb_a, chunk + chunk2);
+        cb_reserve_back(cb_b, chunk + chunk2);
+    }
 
     // get_write_ptr must be called after reserve_back
     uint32_t a_write_base_ptr = get_write_ptr(cb_a);
     uint32_t b_write_base_ptr = get_write_ptr(cb_b);
-
-    uint32_t a_write_ptr = a_write_base_ptr;
-    uint32_t b_write_ptr = b_write_base_ptr;
-
     uint32_t a_write_end_ptr = a_write_base_ptr + CB_PAGE_COUNT(cb_a) * a_tile_size;
     uint32_t b_write_end_ptr = b_write_base_ptr + CB_PAGE_COUNT(cb_b) * b_tile_size;
+
+    uint32_t a_cb_ptr = a_write_base_ptr;
+    uint32_t b_cb_ptr = b_write_base_ptr;
 
     auto next_a_cb_addr = [&](uint32_t addr, uint32_t num_tiles = 1) {
         for (auto j = 0u; j < num_tiles; j++) {
@@ -82,28 +92,21 @@ void kernel_main() {
         return addr;
     };
 
-    while (remaining > 0) {
-        uint32_t chunk;
-        if (remaining >= large_chunk) {
-            chunk = large_chunk;
-        } else if (remaining >= num_tiles_per_batch) {
-            chunk = num_tiles_per_batch;
-        } else {
-            chunk = remaining;
-        }
-
+    while (remaining_tiles > 0) {
+        uint32_t chunk = get_num_tiles(remaining_tiles);
         uint32_t transfer_sz = chunk * a_tile_size;
+
         noc_async_read_set_trid(trid);
 
         noc_async_read_one_packet_set_state<true>(a_noc_addr, transfer_sz, vc);
-        noc_async_read_one_packet_with_state_with_trid(a_noc_addr, a_addr_ofs, a_write_ptr, trid);
+        noc_async_read_one_packet_with_state_with_trid(a_noc_addr, a_addr_ofs, a_cb_ptr, trid);
         a_addr_ofs += transfer_sz;
-        a_write_ptr = next_a_cb_addr(a_write_ptr, chunk);
+        a_cb_ptr = next_a_cb_addr(a_cb_ptr, chunk);
 
         noc_async_read_one_packet_set_state<true>(b_noc_addr, transfer_sz, vc);
-        noc_async_read_one_packet_with_state_with_trid(b_noc_addr, b_addr_ofs, b_write_ptr, trid);
+        noc_async_read_one_packet_with_state_with_trid(b_noc_addr, b_addr_ofs, b_cb_ptr, trid);
         b_addr_ofs += transfer_sz;
-        b_write_ptr = next_b_cb_addr(b_write_ptr, chunk);
+        b_cb_ptr = next_b_cb_addr(b_cb_ptr, chunk);
 
         if (!first_iter) {
             noc_async_read_barrier_with_trid(trid_to_wait);
@@ -112,14 +115,15 @@ void kernel_main() {
             cb_push_back(cb_a, prev_chunk);
             cb_push_back(cb_b, prev_chunk);
 
-            cb_reserve_back(cb_a, prev_chunk);
-            cb_reserve_back(cb_b, prev_chunk);
+            // Reserve two blocks of space to issue multiple block reads in parallel
+            cb_reserve_back(cb_a, 2 * prev_chunk);
+            cb_reserve_back(cb_b, 2 * prev_chunk);
         }
 
         trid = get_next_trid(trid);
         first_iter = false;
         prev_chunk = chunk;
-        remaining -= chunk;
+        remaining_tiles -= chunk;
     }
 
     noc_async_read_barrier_with_trid(trid_to_wait);
