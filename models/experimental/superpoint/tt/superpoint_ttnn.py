33c0baa74b82dbf364621b63d76858578fefc292
Hyunggi Chang <hyunggi.chang95@gmail.com>
2026-04-18 14:26:22 +0000

    experiment(superpoint): enable_act_double_buffer=True on convs

--- models/experimental/superpoint/tt/superpoint_ttnn.py
+++ models/experimental/superpoint/tt/superpoint_ttnn.py
@@ -73,6 +73,7 @@ class TtConv2D:
             shard_layout=shard_layout,
             deallocate_activation=False,
             output_layout=ttnn.TILE_LAYOUT,
+            enable_act_double_buffer=True,
         )
         if act_block_h_override is not None:
             self.conv_config.act_block_h_override = act_block_h_override
