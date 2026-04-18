66ad836d2a2078b65c93837eed3e0047f3003b70
Strahinja Stanisic <sstanisic@tenstorrent.com>
2026-04-18 21:05:42 +0000

    [PATCH] metal -> silence pack_untilize_uninit contract breakage errors

--- tt_metal/hw/inc/api/compute/pack_untilize.h
+++ tt_metal/hw/inc/api/compute/pack_untilize.h
@@ -258,9 +258,12 @@ ALWI void pack_untilize_uninit(uint32_t ocb) {
 #ifndef ARCH_QUASAR
     // Reconfigure data format to match the initial configuration, before calling init.
     // Init is called to ensure special untilize init overrides are cleaned up.
-    PACK((llk_init_packer_dest_offset_registers<false>()));
-    PACK((llk_pack_reconfig_data_format<DST_ACCUM_MODE>(ocb)));
-    PACK((llk_pack_init(ocb)));
+    {
+        LLK_SAN_SILENT_ZONE();
+        PACK((llk_init_packer_dest_offset_registers<false>()));
+        PACK((llk_pack_reconfig_data_format<DST_ACCUM_MODE>(ocb)));
+        PACK((llk_pack_init(ocb)));
+    }
 
 #ifdef ARCH_BLACKHOLE
     PACK((llk_pack_untilize_uninit(ocb)));
