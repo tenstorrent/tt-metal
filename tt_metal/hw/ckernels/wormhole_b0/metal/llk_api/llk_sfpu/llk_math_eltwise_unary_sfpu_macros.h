b7bddd8c6f416a4f3f2be79861e70c1291ab58f9
Nemanja Divnic <ndivnic@tenstorrent.com>
2026-04-21 17:11:18 +0000

    First set of fixes.

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_macros.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_macros.h
@@ -141,18 +141,18 @@ inline __attribute__((always_inline)) void _sfpu_check_and_call_(
 
 // Bare init: no callback.
 //   SFPU_INIT(abs);
-#define SFPU_INIT(OP) ::ckernel::llk_math_eltwise_unary_sfpu_init<::ckernel::SfpuType::OP>()
+#define SFPU_INIT(OP) ::ckernel::llk_math_eltwise_unary_sfpu_init<::SfpuType::OP>()
 
 // Init with a templated callback.
 //   SFPU_INIT_CB(erf, sfpu::erf_init, (APPROXIMATE));
 //   SFPU_INIT_CB(log, sfpu::log_init, (APPROX, fp32, FAST));
 #define SFPU_INIT_CB(OP, INIT_CB, TEMPLATES) \
-    ::ckernel::llk_math_eltwise_unary_sfpu_init<::ckernel::SfpuType::OP>(INIT_CB<_SFPU_EXPAND TEMPLATES>)
+    ::ckernel::llk_math_eltwise_unary_sfpu_init<::SfpuType::OP>(INIT_CB<_SFPU_EXPAND TEMPLATES>)
 
 // Init with a templated callback and extra runtime arguments.
 //   SFPU_INIT_CB_ARGS(exponential, sfpu::exp_init, (APPROX), scale, clamp_neg);
 #define SFPU_INIT_CB_ARGS(OP, INIT_CB, TEMPLATES, ...) \
-    ::ckernel::llk_math_eltwise_unary_sfpu_init<::ckernel::SfpuType::OP>(INIT_CB<_SFPU_EXPAND TEMPLATES>, ##__VA_ARGS__)
+    ::ckernel::llk_math_eltwise_unary_sfpu_init<::SfpuType::OP>(INIT_CB<_SFPU_EXPAND TEMPLATES>, ##__VA_ARGS__)
 
 // =============================================================================
 // Backward-compatible aliases
