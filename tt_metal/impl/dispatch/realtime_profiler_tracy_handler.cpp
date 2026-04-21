1ee595aa2bd082fa5ef70734cc6a4d37ba8b090f
Mo <mmemarian@tenstorrent.com>
2026-04-21 17:33:12 +0000

    Tracy off build fixes

--- tt_metal/impl/dispatch/realtime_profiler_tracy_handler.cpp
+++ tt_metal/impl/dispatch/realtime_profiler_tracy_handler.cpp
@@ -50,15 +50,21 @@ RealtimeProfilerTracyHandler::RealtimeProfilerTracyHandler() {
 RealtimeProfilerTracyHandler::~RealtimeProfilerTracyHandler() {
     tt::UnregisterProgramRealtimeProfilerCallback(callback_handle_);
 
+#if defined(TRACY_ENABLE)
     std::lock_guard<std::mutex> lock(mutex_);
-    for (auto& [chip_id, ctx] : tracy_contexts_) {
-        TracyTTDestroy(ctx);
+    for (auto& entry : tracy_contexts_) {
+        TracyTTDestroy(entry.second);
     }
     tracy_contexts_.clear();
+#endif
 }
 
 void RealtimeProfilerTracyHandler::AddDevice(
-    uint32_t chip_id, int64_t host_start, double first_timestamp, double frequency) {
+    [[maybe_unused]] uint32_t chip_id,
+    [[maybe_unused]] int64_t host_start,
+    [[maybe_unused]] double first_timestamp,
+    [[maybe_unused]] double frequency) {
+#if defined(TRACY_ENABLE)
     std::lock_guard<std::mutex> lock(mutex_);
     if (tracy_contexts_.count(chip_id)) {
         log_warning(tt::LogMetal, "RealtimeProfilerTracyHandler: device {} already added, skipping", chip_id);
@@ -71,9 +77,11 @@ void RealtimeProfilerTracyHandler::AddDevice(
     TracyTTContextName(ctx, name.c_str(), name.size());
 
     tracy_contexts_[chip_id] = ctx;
+#endif
 }
 
-void RealtimeProfilerTracyHandler::RemoveDevice(uint32_t chip_id) {
+void RealtimeProfilerTracyHandler::RemoveDevice([[maybe_unused]] uint32_t chip_id) {
+#if defined(TRACY_ENABLE)
     std::lock_guard<std::mutex> lock(mutex_);
     auto it = tracy_contexts_.find(chip_id);
     if (it == tracy_contexts_.end()) {
@@ -81,6 +89,7 @@ void RealtimeProfilerTracyHandler::RemoveDevice(uint32_t chip_id) {
     }
     TracyTTDestroy(it->second);
     tracy_contexts_.erase(it);
+#endif
 }
 
 TracyTTCtx RealtimeProfilerTracyHandler::GetContext(uint32_t chip_id) {
@@ -89,7 +98,7 @@ TracyTTCtx RealtimeProfilerTracyHandler::GetContext(uint32_t chip_id) {
     return it != tracy_contexts_.end() ? it->second : nullptr;
 }
 
-void RealtimeProfilerTracyHandler::HandleRecord(const tt::ProgramRealtimeRecord& record) {
+void RealtimeProfilerTracyHandler::HandleRecord([[maybe_unused]] const tt::ProgramRealtimeRecord& record) {
 #if defined(TRACY_ENABLE)
     TracyTTCtx ctx = GetContext(record.chip_id);
     if (!ctx) {
@@ -136,7 +145,8 @@ void RealtimeProfilerTracyHandler::HandleRecord(const tt::ProgramRealtimeRecord&
 #endif
 }
 
-void RealtimeProfilerTracyHandler::PushSyncCheckMarker(uint32_t chip_id, uint64_t device_timestamp, double frequency) {
+void RealtimeProfilerTracyHandler::PushSyncCheckMarker(
+    [[maybe_unused]] uint32_t chip_id, [[maybe_unused]] uint64_t device_timestamp, [[maybe_unused]] double frequency) {
 #if defined(TRACY_ENABLE)
     TracyTTCtx ctx = GetContext(chip_id);
     if (!ctx) {
@@ -176,7 +186,10 @@ void RealtimeProfilerTracyHandler::PushSyncCheckMarker(uint32_t chip_id, uint64_
 }
 
 void RealtimeProfilerTracyHandler::CalibrateDevice(
-    uint32_t chip_id, int64_t host_time, uint64_t device_timestamp, double frequency) {
+    [[maybe_unused]] uint32_t chip_id,
+    [[maybe_unused]] int64_t host_time,
+    [[maybe_unused]] uint64_t device_timestamp,
+    [[maybe_unused]] double frequency) {
 #if defined(TRACY_ENABLE)
     TracyTTCtx ctx = GetContext(chip_id);
     if (!ctx) {
