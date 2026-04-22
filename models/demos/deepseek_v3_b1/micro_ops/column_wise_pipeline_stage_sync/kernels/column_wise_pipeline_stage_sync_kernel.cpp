2344e394caa6edbb21fd8aefb8ab46672495dfc8
grechsteinerTT <grechsteiner@tenstorrent.com>
2026-04-22 17:32:00 +0000

    intermediate kernel layer

--- models/demos/deepseek_v3_b1/micro_ops/column_wise_pipeline_stage_sync/kernels/column_wise_pipeline_stage_sync_kernel.cpp
+++ models/demos/deepseek_v3_b1/micro_ops/column_wise_pipeline_stage_sync/kernels/column_wise_pipeline_stage_sync_kernel.cpp
@@ -3,63 +3,59 @@
 // SPDX-License-Identifier: Apache-2.0
 
 /**
- * Unified PipelineStageSync Kernel.
+ * Unified ColumnWisePipelineStageSync Kernel.
  *
- * Uses the unified PipelineStageSync op from unified_kernels/pipeline_stage_sync.hpp
+ * Uses the unified ColumnWisePipelineStageSync op from unified_kernels/column_wise_pipeline_stage_sync.hpp
  */
 
 #include "../../../unified_kernels/kernel_op_api.hpp"
 #include "../../../unified_kernels/kernel_utils.hpp"
-#include "../../../unified_kernels/pipeline_stage_sync.hpp"
+#include "../../../unified_kernels/column_wise_pipeline_stage_sync.hpp"
 
 void kernel_main() {
-    using PipelineStageSync = deepseek_b1_ops::PipelineStageSync;
+    using ColumnWisePipelineStageSync = deepseek_b1_ops::ColumnWisePipelineStageSync;
 
 #if defined(COMPILE_FOR_NCRISC)
     // Reader CTArgs
-    using CTArgs = PipelineStageSync::ReaderCTArgs<
-        get_named_compile_time_arg_val("run_stalling_logic_on_ncrisc"),
-        get_named_compile_time_arg_val("run_signalling_logic_on_ncrisc"),
-        get_named_compile_time_arg_val("is_intermediate_signaller"),
-        get_named_compile_time_arg_val("is_signalling_to_intermediate_signaller"),
-        get_named_compile_time_arg_val("signalling_core_noc_x_addr"),
-        get_named_compile_time_arg_val("signalling_core_noc_y_addr"),
-        get_named_compile_time_arg_val("stalling_core_noc_x_addr"),
-        get_named_compile_time_arg_val("stalling_core_noc_y_addr"),
-        get_named_compile_time_arg_val("semaphore_l1_addr"),
+    using CTArgs = ColumnWisePipelineStageSync::ReaderCTArgs<
+        get_named_compile_time_arg_val("run_entry_device_logic_on_ncrisc"),
+        get_named_compile_time_arg_val("run_exit_device_logic_on_ncrisc"),
+        get_named_compile_time_arg_val("entry_device_core_noc_x_addr"),
+        get_named_compile_time_arg_val("entry_device_core_noc_y_addr"),
+        get_named_compile_time_arg_val("r1_semaphore_l1_addr"),
+        get_named_compile_time_arg_val("r2_semaphore_l1_addr"),
+        get_named_compile_time_arg_val("r3_semaphore_l1_addr"),
         get_named_compile_time_arg_val("fabric_arg_base")>;
 
     // Reader runtime args
-    PipelineStageSync::ReaderArgs rt_args = {};
+    ColumnWisePipelineStageSync::ReaderArgs rt_args = {};
 
 #elif defined(COMPILE_FOR_BRISC)
     // Writer CTArgs
-    using CTArgs = PipelineStageSync::WriterCTArgs<
-        get_named_compile_time_arg_val("run_stalling_logic_on_brisc"),
-        get_named_compile_time_arg_val("run_signalling_logic_on_brisc"),
-        get_named_compile_time_arg_val("is_intermediate_signaller"),
-        get_named_compile_time_arg_val("is_signalling_to_intermediate_signaller"),
-        get_named_compile_time_arg_val("signalling_core_noc_x_addr"),
-        get_named_compile_time_arg_val("signalling_core_noc_y_addr"),
-        get_named_compile_time_arg_val("stalling_core_noc_x_addr"),
-        get_named_compile_time_arg_val("stalling_core_noc_y_addr"),
-        get_named_compile_time_arg_val("semaphore_l1_addr"),
+    using CTArgs = ColumnWisePipelineStageSync::WriterCTArgs<
+        get_named_compile_time_arg_val("run_entry_device_logic_on_brisc"),
+        get_named_compile_time_arg_val("run_exit_device_logic_on_brisc"),
+        get_named_compile_time_arg_val("entry_device_core_noc_x_addr"),
+        get_named_compile_time_arg_val("entry_device_core_noc_y_addr"),
+        get_named_compile_time_arg_val("r1_semaphore_l1_addr"),
+        get_named_compile_time_arg_val("r2_semaphore_l1_addr"),
+        get_named_compile_time_arg_val("r3_semaphore_l1_addr"),
         get_named_compile_time_arg_val("fabric_arg_base")>;
 
     // Writer runtime args
-    PipelineStageSync::WriterArgs rt_args = {};
+    ColumnWisePipelineStageSync::WriterArgs rt_args = {};
 
 #elif defined(COMPILE_FOR_TRISC)
     // Compute CTArgs
-    using CTArgs = PipelineStageSync::ComputeCTArgs;
+    using CTArgs = ColumnWisePipelineStageSync::ComputeCTArgs;
 
     // Compute runtime args
-    PipelineStageSync::ComputeArgs rt_args = {};
+    ColumnWisePipelineStageSync::ComputeArgs rt_args = {};
 #endif
 
     // Execute the op (looped for testing iteration correctness)
     constexpr uint32_t num_iterations = get_named_compile_time_arg_val("num_iterations");
-    PipelineStageSync::Op<CTArgs> op;
+    ColumnWisePipelineStageSync::Op<CTArgs> op;
     for (uint32_t iteration = 0; iteration < num_iterations; ++iteration) {
         op(rt_args);
     }
