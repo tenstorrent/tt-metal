e34968d38ff49579ad3c1038f5c7e5a22482c0b8
Ryan Zhu <ryanzhu@tenstorrent.com>
2026-04-22 19:03:19 +0000

    comment for unused template in init

--- tt_metal/hw/ckernels/quasar/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_init.h
+++ tt_metal/hw/ckernels/quasar/metal/llk_api/llk_sfpu/llk_math_eltwise_unary_sfpu_init.h
@@ -8,11 +8,13 @@
 
 namespace ckernel {
 
+// sfpu_op template parameter is unused, but kept for backwards compatibility
 template <SfpuType sfpu_op>
 inline void llk_math_eltwise_unary_sfpu_init() {
     _llk_math_eltwise_unary_sfpu_init_();
 }
 
+// sfpu_op template parameter is unused, but kept for backwards compatibility
 template <SfpuType sfpu_op, class F, class... ARGS>
 inline void llk_math_eltwise_unary_sfpu_init(F&& init_func, ARGS&&... args) {
     _llk_math_eltwise_unary_sfpu_init_();
