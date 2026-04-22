62da7c64b1866a7ec6135115bed82f16c0aeee9d
Filip Vranic <fvranic@tenstorrent.com>
2026-04-22 18:37:16 +0200

    test: refactor square calculation to use sfpi code

--- tt_metal/tt-llk/tt_llk_quasar/common/inc/sfpu/ckernel_sfpu_square.h
+++ tt_metal/tt-llk/tt_llk_quasar/common/inc/sfpu/ckernel_sfpu_square.h
@@ -12,24 +12,16 @@ namespace ckernel
 {
 namespace sfpu
 {
-// Calculates SQUARE for number of rows of output SFPU ops (Quasar = 2 rows)
-template <bool APPROXIMATION_MODE>
-inline void _calculate_square_sfp_rows_()
-{
-    TTI_SFPLOAD(p_sfpu::LREG0, p_sfpu::sfpmem::DEFAULT, ADDR_MOD_7, 0, 0); // load from dest into lreg[0]
-    // Multiply LREG0 * LREG0, store result in LREG0
-    TTI_SFPMUL(p_sfpu::LREG0, p_sfpu::LREG0, p_sfpu::LCONST_0, p_sfpu::LREG0, 0);
-    // Store result back to destination
-    TTI_SFPSTORE(p_sfpu::LREG0, p_sfpu::sfpmem::DEFAULT, ADDR_MOD_7, 0, 0);
-}
 
 inline void _calculate_square_(const int iterations)
 {
 #pragma GCC unroll 8
     for (int d = 0; d < iterations; d++)
     {
-        _calculate_square_sfp_rows_<false>();
-        ckernel::math::_incr_counters_<0x0, 0x0, ckernel::math::SFP_ROWS, 0x0>(); // does the dest_reg++ (increments by 2 rows)
+        sfpi::vFloat in     = sfpi::dst_reg[0];
+        sfpi::vFloat result = in * in;
+        sfpi::dst_reg[0]    = result;
+        sfpi::dst_reg++;
     }
 }
 
