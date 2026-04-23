911224cb17d35965bc5cfb93e3edf51fc184a357
pjanevski <pjanevski@tenstorrent.com>
2026-04-23 13:18:02 +0000

    take only one sysmem page for simulation

--- tt_metal/llrt/tt_cluster.cpp
+++ tt_metal/llrt/tt_cluster.cpp
@@ -410,7 +410,7 @@ void Cluster::open_driver(const bool& /*skip_driver_allocs*/) {
             mock_cluster_desc = get_mock_cluster_desc(rtoptions_);
             device_driver = std::make_unique<tt::umd::Cluster>(tt::umd::ClusterOptions{
                 .chip_type = tt::umd::ChipType::SIMULATION,
-                .num_host_mem_ch_per_mmio_device = 4,
+                .num_host_mem_ch_per_mmio_device = 1,
                 .sdesc_path = sdesc_path,
                 .cluster_descriptor = mock_cluster_desc.get(),
                 .simulator_directory = rtoptions_.get_simulator_path(),
@@ -418,7 +418,7 @@ void Cluster::open_driver(const bool& /*skip_driver_allocs*/) {
         } else {
             device_driver = std::make_unique<tt::umd::Cluster>(tt::umd::ClusterOptions{
                 .chip_type = tt::umd::ChipType::SIMULATION,
-                .num_host_mem_ch_per_mmio_device = 4,
+                .num_host_mem_ch_per_mmio_device = 1,
                 .target_devices = {0},
                 .simulator_directory = rtoptions_.get_simulator_path(),
             });
