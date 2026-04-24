84faac11c34ec728945ffcd442db0fc3ad64151b
yalrawwashTT <yalrawwash@tenstorrent.com>
2026-04-24 16:17:34 +0000

    undo all local needed tests changes

--- tests/scripts/multihost/run_quad_galaxy_tests.sh
+++ tests/scripts/multihost/run_quad_galaxy_tests.sh
@@ -697,14 +697,14 @@ run_quad_deepseekv3_integration_tests() {
 
 run_all_needed_local_tests() {
     local saved_upr_mode="${DEEPSEEK_DEMO_UPR_MODE:-}"
-    export DEEPSEEK_DEMO_UPR_MODE="8"
+    export DEEPSEEK_DEMO_UPR_MODE="all"
 
-    # run_dual_teacher_forced_test
+    run_dual_teacher_forced_test
     run_dual_demo_test
-    # run_dual_demo_stress_test
-    # run_quad_teacher_forced_test
+    run_dual_demo_stress_test
+    run_quad_teacher_forced_test
     run_quad_demo_test
-    # run_quad_demo_stress_test
+    run_quad_demo_stress_test
 
     if [[ -n "${saved_upr_mode}" ]]; then
         export DEEPSEEK_DEMO_UPR_MODE="${saved_upr_mode}"
