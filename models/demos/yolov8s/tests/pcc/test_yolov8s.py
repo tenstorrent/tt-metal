c06c87feee89f2f7692a5a7033890cfda6e32f9b
ign-saurav <sauravkumar.mishra@ignitarium.com>
2026-04-27 12:24:03 +0000

    yolov8s batch2,4,8 perf test enabled

--- models/demos/yolov8s/tests/pcc/test_yolov8s.py
+++ models/demos/yolov8s/tests/pcc/test_yolov8s.py
@@ -5,12 +5,14 @@
 import pytest
 import torch
 from loguru import logger
+from ttnn.model_preprocessing import preprocess_model_parameters
 
 import ttnn
 
 # from models.common.utility_functions import disable_persistent_kernel_cache
+from models.demos.utils.common_demo_utils import get_mesh_mappers
 from models.demos.yolov8s.common import YOLOV8S_L1_SMALL_SIZE, load_torch_model
-from models.demos.yolov8s.tt.tt_yolov8s_utils import custom_preprocessor
+from models.demos.yolov8s.tt.tt_yolov8s_utils import create_custom_mesh_preprocessor, custom_preprocessor
 from models.demos.yolov8s.tt.ttnn_yolov8s import TtYolov8sModel
 from tests.ttnn.utils_for_testing import assert_with_pcc
 
@@ -56,3 +58,85 @@ def test_yolov8s_640(device, input_tensor, use_pretrained_weights, model_locatio
 
     passing, pcc = assert_with_pcc(ttnn_model_output, torch_model_output, 0.99)
     logger.info(f"Passing: {passing}, PCC: {pcc}")
+
+
+def _run_yolov8s_dp_mesh_pcc(mesh_device, use_pretrained_weights, model_location_generator, min_channels=16):
+    num_devices = mesh_device.get_num_devices()
+    batch_size = num_devices
+
+    torch_model = load_torch_model(model_location_generator)
+
+    inputs_mesh_mapper, weights_mesh_mapper, output_mesh_composer = get_mesh_mappers(mesh_device)
+
+    parameters = preprocess_model_parameters(
+        initialize_model=lambda: torch_model,
+        custom_preprocessor=create_custom_mesh_preprocessor(mesh_device, weights_mesh_mapper),
+        device=mesh_device,
+    )
+    ttnn_model = TtYolov8sModel(device=mesh_device, parameters=parameters, res=(640, 640))
+
+    torch_input_tensor = torch.rand((batch_size, 3, 640, 640))
+
+    n, c, h, w = torch_input_tensor.shape
+    n_per_device = n // num_devices if n // num_devices != 0 else n
+    c_padded = min_channels if c < min_channels else c
+
+    input_mem_config = ttnn.create_sharded_memory_config(
+        [n_per_device, c_padded, h, w],
+        ttnn.CoreGrid(x=8, y=8),
+        ttnn.ShardStrategy.HEIGHT,
+    )
+
+    ttnn_input = ttnn.from_torch(
+        torch_input_tensor,
+        dtype=ttnn.bfloat16,
+        layout=ttnn.ROW_MAJOR_LAYOUT,
+        mesh_mapper=inputs_mesh_mapper,
+    )
+    ttnn_input = ttnn_input.to(mesh_device, input_mem_config)
+
+    with torch.inference_mode():
+        ttnn_model_output = ttnn_model(ttnn_input)[0]
+        ttnn_model_output = ttnn.to_torch(ttnn_model_output, mesh_composer=output_mesh_composer)
+
+    with torch.inference_mode():
+        torch_model_output = torch_model(torch_input_tensor)[0]
+
+    passing, pcc = assert_with_pcc(ttnn_model_output, torch_model_output, 0.99)
+    logger.info(f"Passing: {passing}, PCC: {pcc}")
+
+
+@pytest.mark.parametrize(
+    "mesh_device",
+    [(1, 2)],
+    indirect=True,
+    ids=["n300_1x2"],
+)
+@pytest.mark.parametrize("device_params", [{"l1_small_size": YOLOV8S_L1_SMALL_SIZE}], indirect=True)
+@pytest.mark.parametrize("use_pretrained_weights", [True])
+def test_yolov8s_dp_batch2(mesh_device, use_pretrained_weights, model_location_generator):
+    _run_yolov8s_dp_mesh_pcc(mesh_device, use_pretrained_weights, model_location_generator)
+
+
+@pytest.mark.parametrize(
+    "mesh_device",
+    [(1, 4)],
+    indirect=True,
+    ids=["wh_1x4"],
+)
+@pytest.mark.parametrize("device_params", [{"l1_small_size": YOLOV8S_L1_SMALL_SIZE}], indirect=True)
+@pytest.mark.parametrize("use_pretrained_weights", [True])
+def test_yolov8s_dp_batch4(mesh_device, use_pretrained_weights, model_location_generator):
+    _run_yolov8s_dp_mesh_pcc(mesh_device, use_pretrained_weights, model_location_generator)
+
+
+@pytest.mark.parametrize(
+    "mesh_device",
+    [(1, 8)],
+    indirect=True,
+    ids=["t3k_1x8"],
+)
+@pytest.mark.parametrize("device_params", [{"l1_small_size": YOLOV8S_L1_SMALL_SIZE}], indirect=True)
+@pytest.mark.parametrize("use_pretrained_weights", [True])
+def test_yolov8s_dp_batch8(mesh_device, use_pretrained_weights, model_location_generator):
+    _run_yolov8s_dp_mesh_pcc(mesh_device, use_pretrained_weights, model_location_generator)
--- models/demos/yolov8s/tests/perf/test_perf_yolov8s.py
+++ models/demos/yolov8s/tests/perf/test_perf_yolov8s.py
@@ -9,18 +9,19 @@ from loguru import logger
 from models.perf.device_perf_utils import check_device_perf, prep_device_perf_report, run_device_perf
 
 
-@pytest.mark.parametrize(
-    "batch_size, expected_perf",
-    [
-        [1, 236.95],
-    ],
-)
-@pytest.mark.models_device_performance_bare_metal
-def test_perf_device_yolov8s(batch_size, expected_perf):
+def _run_yolov8s_perf(name_suffix, batch_size, expected_perf, test_filter, min_mesh_devices):
+    if min_mesh_devices > 1:
+        import ttnn
+
+        if not ttnn.using_distributed_env() and ttnn.get_num_devices() < min_mesh_devices:
+            pytest.skip(
+                f"{name_suffix} needs a {min_mesh_devices}-device mesh (skips single-device N150 and other undersized hosts)"
+            )
+
     subdir = "ttnn_yolov8s"
     num_iterations = 1
     margin = 0.05
-    command = f"pytest models/demos/yolov8s/tests/pcc/test_yolov8s.py::test_yolov8s_640"
+    command = f'pytest models/demos/yolov8s/tests/pcc/test_yolov8s.py -k "{test_filter}"'
     cols = ["DEVICE FW", "DEVICE KERNEL", "DEVICE BRISC KERNEL"]
 
     inference_time_key = "AVG DEVICE KERNEL SAMPLES/S"
@@ -32,9 +33,29 @@ def test_perf_device_yolov8s(batch_size, expected_perf):
     logger.info(f"{expected_results}")
 
     prep_device_perf_report(
-        model_name=f"ttnn_yolov8s{batch_size}",
+        model_name=f"ttnn_yolov8s_{name_suffix}",
         batch_size=batch_size,
         post_processed_results=post_processed_results,
         expected_results=expected_results,
         comments="",
     )
+
+
+@pytest.mark.models_device_performance_bare_metal
+def test_perf_device_yolov8s_b1():
+    _run_yolov8s_perf("b1_640", 1, 236.95, "test_yolov8s_640", 1)
+
+
+@pytest.mark.models_device_performance_bare_metal
+def test_perf_device_yolov8s_b2():
+    _run_yolov8s_perf("b2_640", 2, 0, "test_yolov8s_dp_batch2", 2)
+
+
+@pytest.mark.models_device_performance_bare_metal
+def test_perf_device_yolov8s_b4():
+    _run_yolov8s_perf("b4_640", 4, 0, "test_yolov8s_dp_batch4", 4)
+
+
+@pytest.mark.models_device_performance_bare_metal
+def test_perf_device_yolov8s_b8():
+    _run_yolov8s_perf("b8_640", 8, 0, "test_yolov8s_dp_batch8", 8)
