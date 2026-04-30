1c9b9479ee33641e5476b87ce481709406f1c5e0
pjanevski <pjanevski@tenstorrent.com>
2026-04-30 09:05:23 +0000

    Fix target device

--- tt_metal/llrt/tt_cluster.cpp
+++ tt_metal/llrt/tt_cluster.cpp
@@ -474,7 +474,7 @@ void Cluster::start_driver(umd::DeviceParams& device_params) const {
     // May block waiting for other processes to release the device.
     this->driver_->start_device(device_params);
 
-    if (device_params.init_device) {
+    if ((this->target_type_ == TargetDevice::Silicon || this->target_type_ == TargetDevice::Simulator) && device_params.init_device) {
         // Configure TLBs on all MMIO devices in parallel
         std::vector<std::shared_future<void>> futures;
         const auto& mmio_device_ids = driver_->get_target_mmio_device_ids();
