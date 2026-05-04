812f4339a7df397c34c450ccc1e12e2e3c53791b
Juan Camilo Vega <jvega@tenstorrent.com>
2026-05-04 15:40:11 +0000

    Revert "Fabric changes to default to torus_xy and use mesh only in t3k"
    This reverts commit 109b3c80b4a1de335287c7d1aa1dae339a4e1b75.

--- tests/tt_metal/tt_fabric/fabric_router/test_multi_host.cpp
+++ tests/tt_metal/tt_fabric/fabric_router/test_multi_host.cpp
@@ -560,8 +560,7 @@ TEST(MultiHost, Test32x4QuadGalaxyFabric2DSanity) {
 
     // Validate control plane apis
     auto& control_plane = tt::tt_metal::MetalContext::instance().get_control_plane();
-    const auto fabric_type =
-        get_fabric_type(tt::tt_fabric::FabricConfig::FABRIC_2D_TORUS_XY, tt::tt_metal::ClusterType::BLACKHOLE_GALAXY);
+    const auto fabric_type = get_fabric_type(tt::tt_fabric::FabricConfig::FABRIC_2D_TORUS_XY, true);
 
     FabricNodeId src_node_id(MeshId{0}, 3);  // On host rank 0
     MeshCoordinate src_mesh_coord(0, 3);
@@ -675,8 +674,7 @@ TEST(MultiHost, TestQuadGalaxyFabric2DSanity) {
 
     // Validate control plane apis
     auto& control_plane = tt::tt_metal::MetalContext::instance().get_control_plane();
-    const auto fabric_type =
-        get_fabric_type(tt::tt_fabric::FabricConfig::FABRIC_2D_TORUS_XY, tt::tt_metal::ClusterType::BLACKHOLE_GALAXY);
+    const auto fabric_type = get_fabric_type(tt::tt_fabric::FabricConfig::FABRIC_2D_TORUS_XY, true);
 
     FabricNodeId src_node_id(MeshId{0}, 3);  // On host rank 0
     MeshCoordinate src_mesh_coord(0, 3);
