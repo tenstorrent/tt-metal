1d2bcf20a4007e62e0cc4ca9cd91178cedd63dab
Maxim Artemov <ctr-martemovTT@ext.tenstorrent.com>
2026-05-05 12:29:01 +0000

    clang-tidy CI fix

--- ttnn/cpp/ttnn/operations/data_movement/bcast/device/bcast_sharded_h_optimised_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/bcast/device/bcast_sharded_h_optimised_program_factory.cpp
@@ -10,6 +10,7 @@
 #include <tt-metalium/tensor_accessor_args.hpp>
 #include <tt-metalium/work_split.hpp>
 #include "ttnn/operations/data_movement/bcast/bcast_types.hpp"
+#include <tt-metalium/tilize_utils.hpp>
 
 namespace ttnn::prim {
 
