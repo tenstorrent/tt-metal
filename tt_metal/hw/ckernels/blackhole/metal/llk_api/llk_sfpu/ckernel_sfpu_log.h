3e8d63d5134754c53f4020ee7982d0763c8f1089
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-05-06 23:01:52 +0000

    #40182: Replace float_to_$TYPE with convert<TYPE>

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_log.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_log.h
@@ -68,7 +68,7 @@ sfpi_inline sfpi::vFloat calculate_log_body(sfpi::vFloat in, const uint log_base
     }
 
     if constexpr (!is_fp32_dest_acc_en) {
-        result = sfpi::float_to_fp16b(result, sfpi::RoundMode::NearestEven);
+        result = sfpi::convert<sfpi::vFloat16b>(result, sfpi::RoundMode::NearestEven);
     }
 
     return result;
