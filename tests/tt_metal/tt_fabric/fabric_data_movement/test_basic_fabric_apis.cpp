3217bc7c9b304f2be95cf2fed2d2036091e7ff8a
Dragica Stoiljkovic <dstoiljkovic@tenstorrent.com>
2026-05-06 14:40:48 +0000

    Skipping a test

--- tests/tt_metal/tt_fabric/fabric_data_movement/test_basic_fabric_apis.cpp
+++ tests/tt_metal/tt_fabric/fabric_data_movement/test_basic_fabric_apis.cpp
@@ -714,6 +714,9 @@ TEST_F(Fabric2DUDMModeFixture, TestUDMFabricUnicastAtomicIncWest) {
     UDMFabricUnicastCommon(this, NocPacketType::NOC_UNICAST_ATOMIC_INC, std::make_tuple(RoutingDirection::W, 1));
 }
 TEST_F(Fabric2DUDMModeFixture, TestUDMFabricReadEast) {
+    if (tt::tt_metal::MetalContext::instance().rtoptions().get_watcher_enabled()) {
+        GTEST_SKIP() << "Skipping with watcher enabled";
+    }
     UDMFabricUnicastCommon(this, NocPacketType::NOC_UNICAST_READ, std::make_tuple(RoutingDirection::E, 1));
 }
 TEST_F(Fabric2DUDMModeFixture, TestUDMFabricReadWest) {
