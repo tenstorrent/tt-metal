19b89f87fe3ce1d9b22980fa532b0f5b3ca75f40
Steven Lee <stevenlee@tenstorrent.com>
2026-05-07 22:44:50 +0000

    remove irrelevant files

--- model_tracer/galaxy_trace_logs/run_trace.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Helper to run a single model trace under the right env
-set -eo pipefail
-cd /data/stevenlee/tt-metal
-source python_env/bin/activate
-export TT_METAL_HOME=$(pwd)
-export PYTHONPATH=$(pwd)
-export ARCH_NAME=wormhole_b0
-exec "$@"
