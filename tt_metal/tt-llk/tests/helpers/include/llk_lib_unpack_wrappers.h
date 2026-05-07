513efb4220514e4999b03009eb0e765455bab0f3
Nemanja Divnic <ndivnic@tenstorrent.com>
2026-05-07 13:55:47 +0000

    Revert unnecessary added param comments.

--- tt_metal/tt-llk/tests/helpers/include/llk_lib_unpack_wrappers.h
+++ tt_metal/tt-llk/tests/helpers/include/llk_lib_unpack_wrappers.h
@@ -8,17 +8,15 @@
 
 #include "llk_unpack_tilize.h"
 
-using ckernel::FACE_R_DIM;
-
 #ifdef ARCH_WORMHOLE
 
 inline void _llk_unpack_tilize_init_wrapper_(
-    const std::uint32_t unpack_src_format = 0 /* unpack_src_format */,
-    const std::uint32_t unpack_dst_format = 0 /* unpack_dst_format */,
-    const std::uint32_t ct_dim            = 0 /* ct_dim */,
-    const std::uint32_t face_r_dim        = FACE_R_DIM,
-    const bool narrow_tile                = false /* narrow_tile */,
-    const std::uint32_t num_faces         = 4 /* num_faces */)
+    const std::uint32_t unpack_src_format = 0,
+    const std::uint32_t unpack_dst_format = 0,
+    const std::uint32_t ct_dim            = 0,
+    const std::uint32_t face_r_dim        = ckernel::FACE_R_DIM,
+    const bool narrow_tile                = false,
+    const std::uint32_t num_faces         = 4)
 {
     (void)num_faces;
     _llk_unpack_tilize_init_(unpack_src_format, unpack_dst_format, ct_dim, face_r_dim, narrow_tile);
@@ -27,25 +25,25 @@ inline void _llk_unpack_tilize_init_wrapper_(
 inline void _llk_unpack_tilize_wrapper_(
     const std::uint32_t base_address,
     const std::uint32_t tile_index,
-    const std::uint32_t unpack_src_format = 0 /* unpack_src_format */,
-    const std::uint32_t unpack_dst_format = 0 /* unpack_dst_format */,
-    const std::uint32_t block_ct_dim      = 0 /* block_ct_dim */,
-    const std::uint32_t face_r_dim        = FACE_R_DIM,
-    const std::uint32_t num_faces         = 4 /* num_faces */,
-    const bool narrow_tile                = false /* narrow_tile */)
+    const std::uint32_t unpack_src_format = 0,
+    const std::uint32_t unpack_dst_format = 0,
+    const std::uint32_t block_ct_dim      = 0,
+    const std::uint32_t face_r_dim        = ckernel::FACE_R_DIM,
+    const std::uint32_t num_faces         = 4,
+    const bool narrow_tile                = false)
 {
     _llk_unpack_tilize_(base_address, tile_index, unpack_src_format, unpack_dst_format, block_ct_dim, face_r_dim, num_faces, narrow_tile);
 }
 
-#else // ARCH_BLACKHOLE version of the wrappers
+#elif defined(ARCH_BLACKHOLE)
 
 inline void _llk_unpack_tilize_init_wrapper_(
-    const std::uint32_t unpack_src_format = 0 /* unpack_src_format */,
-    const std::uint32_t unpack_dst_format = 0 /* unpack_dst_format */,
-    const std::uint32_t ct_dim            = 0 /* ct_dim */,
-    const std::uint32_t face_r_dim        = FACE_R_DIM,
-    const bool narrow_tile                = false /* narrow_tile */,
-    const std::uint32_t num_faces         = 4 /* num_faces */)
+    const std::uint32_t unpack_src_format = 0,
+    const std::uint32_t unpack_dst_format = 0,
+    const std::uint32_t ct_dim            = 0,
+    const std::uint32_t face_r_dim        = ckernel::FACE_R_DIM,
+    const bool narrow_tile                = false,
+    const std::uint32_t num_faces         = 4)
 {
     _llk_unpack_tilize_init_(unpack_src_format, unpack_dst_format, ct_dim, face_r_dim, narrow_tile, num_faces);
 }
@@ -53,15 +51,18 @@ inline void _llk_unpack_tilize_init_wrapper_(
 inline void _llk_unpack_tilize_wrapper_(
     const std::uint32_t base_address,
     const std::uint32_t tile_index,
-    const std::uint32_t unpack_src_format = 0 /* unpack_src_format */,
-    const std::uint32_t unpack_dst_format = 0 /* unpack_dst_format */,
-    const std::uint32_t block_ct_dim      = 0 /* block_ct_dim */,
-    const std::uint32_t face_r_dim        = FACE_R_DIM,
-    const std::uint32_t num_faces         = 4 /* num_faces */,
-    const bool narrow_tile                = false /* narrow_tile */)
+    const std::uint32_t unpack_src_format = 0,
+    const std::uint32_t unpack_dst_format = 0,
+    const std::uint32_t block_ct_dim      = 0,
+    const std::uint32_t face_r_dim        = ckernel::FACE_R_DIM,
+    const std::uint32_t num_faces         = 4,
+    const bool narrow_tile                = false)
 {
     (void)block_ct_dim;
     _llk_unpack_tilize_(base_address, tile_index, unpack_src_format, unpack_dst_format, face_r_dim, num_faces, narrow_tile);
 }
+
+#else
+#error "Unsupported architecture for LLK unpack tilize wrappers"
 
 #endif
