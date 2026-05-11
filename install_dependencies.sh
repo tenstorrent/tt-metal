b5bdde030b627f0ad18c612f29d3747a21f3ee8d
SaeHie Park <seanpark@bos-semi.com>
2026-05-11 13:30:38 +0900

    Enable to add custom build settings
    This will enable to add custom build settings for other platforms.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -247,6 +247,10 @@ init_packages() {
             fi
             ;;
     esac
+
+    if declare -F init_packages_custom >/dev/null; then
+        init_packages_custom
+    fi
 }
 
 prep_system() {
@@ -547,6 +551,12 @@ main() {
     cleanup
 
 }
+
+SCRIPT_DIR="$(CDPATH= cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+CUSTOM_DEPENDS_SCRIPT="${SCRIPT_DIR}/install_depends_custom.sh"
+if [ -f "${CUSTOM_DEPENDS_SCRIPT}" ]; then
+    source "${CUSTOM_DEPENDS_SCRIPT}"
+fi
 
 if [ "${1}" != "--source-only" ]; then
     main "${@}"
