b396c2ee064d9ad15515909de82b5303b99ad748
Ryan Zhu <ryanzhu@tenstorrent.com>
2026-05-11 14:10:22 -0400

    [Quasar] Sigmoid/SiLU Bringup (#43668)
    ### PR Category
    <!-- What kind of PR is this? Clean category helps keep PR small and
    review high quality.
    Available options: Feature, Performance, Bug fix, Cleanup, Test Only.
         Please include this in your PR title -->
    Feature
    
    ### Summary
    <!-- Explain the motivation for this change. What problem does it solve?
    To link an issue: Closes #<number> / Fixes #<number> / Relates to
    #<number> -->
    Issue: https://github.com/tenstorrent/tt-metal/issues/40231
    - Bringup for Sigmoid and SiLU from LLKs in Quasar
    - Adds a test for SiLU to
    `tt-metal/tests/tt_metal/tt_metal/llk/test_sfpu_compute.cpp`
    
    ### Notes for reviewers
    <!-- Where should reviewers focus? Call out anything non-obvious,
    tradeoffs, or areas of uncertainty. -->
    
    ### CI Status
    _Auto-generated on every push. Badges update live. Click a badge to
    filter runs by this branch._
    
    -
    [![](https://github.com/tenstorrent/tt-metal/actions/workflows/sanity-tests.yaml/badge.svg?branch=ryanzhu/quasar-sigmoid-bringup)](https://github.com/tenstorrent/tt-metal/actions/workflows/sanity-tests.yaml?query=branch:ryanzhu/quasar-sigmoid-bringup)
    -
    [![](https://github.com/tenstorrent/tt-metal/actions/workflows/runtime-sanity-tests.yaml/badge.svg?branch=ryanzhu/quasar-sigmoid-bringup)](https://github.com/tenstorrent/tt-metal/actions/workflows/runtime-sanity-tests.yaml?query=branch:ryanzhu/quasar-sigmoid-bringup)
    -
    [![](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml/badge.svg?branch=ryanzhu/quasar-sigmoid-bringup)](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml?query=branch:ryanzhu/quasar-sigmoid-bringup)
    -
    [![](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml/badge.svg?branch=ryanzhu/quasar-sigmoid-bringup)](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml?query=branch:ryanzhu/quasar-sigmoid-bringup)
    -
    [![](https://github.com/tenstorrent/tt-metal/actions/workflows/all-model-tests.yaml/badge.svg?branch=ryanzhu/quasar-sigmoid-bringup)](https://github.com/tenstorrent/tt-metal/actions/workflows/all-model-tests.yaml?query=branch:ryanzhu/quasar-sigmoid-bringup)
    -
    [![](https://github.com/tenstorrent/tt-metal/actions/workflows/pipeline-select.yaml/badge.svg?branch=ryanzhu/quasar-sigmoid-bringup)](https://github.com/tenstorrent/tt-metal/actions/workflows/pipeline-select.yaml?query=branch:ryanzhu/quasar-sigmoid-bringup)
    -
    [![](https://github.com/tenstorrent/tt-metal/actions/workflows/pipeline-select-t3k.yaml/badge.svg?branch=ryanzhu/quasar-sigmoid-bringup)](https://github.com/tenstorrent/tt-metal/actions/workflows/pipeline-select-t3k.yaml?query=branch:ryanzhu/quasar-sigmoid-bringup)
    -
    [![](https://github.com/tenstorrent/tt-metal/actions/workflows/pipeline-select-galaxy.yaml/badge.svg?branch=ryanzhu/quasar-sigmoid-bringup)](https://github.com/tenstorrent/tt-metal/actions/workflows/pipeline-select-galaxy.yaml?query=branch:ryanzhu/quasar-sigmoid-bringup)

--- tt_metal/hw/inc/api/compute/compute_kernel_api.h
+++ tt_metal/hw/inc/api/compute/compute_kernel_api.h
@@ -20,9 +20,9 @@
 #include "llk_math_common_api.h"
 #include "llk_math_matmul_api.h"
 #include "llk_math_unary_datacopy_api.h"
+#include "llk_math_unary_sfpu_api.h"
 #ifndef ARCH_QUASAR
 #include "llk_math_binary_api.h"
-#include "llk_math_unary_sfpu_api.h"
 #include "llk_math_binary_sfpu_api.h"
 #include "llk_math_reduce_api.h"
 #endif
@@ -34,10 +34,12 @@
 #ifdef TRISC_PACK
 #include "llk_pack_api.h"
 #include "llk_io_pack.h"
+#ifndef ARCH_QUASAR
 #include "llk_math_eltwise_unary_sfpu_silu.h"
 #include "llk_math_eltwise_unary_sfpu_tanh.h"
 #include "llk_math_eltwise_unary_sfpu_sigmoid.h"
 #include "llk_math_eltwise_unary_sfpu_activations.h"
+#endif
 #define PACK(x) x
 #else
 #define PACK(x)
@@ -61,7 +63,6 @@
 
 namespace ckernel {
 
-#ifndef ARCH_QUASAR
 /**
  * Please refer to documentation for any_init.
  */
@@ -69,11 +70,6 @@ namespace ckernel {
 template <bool fast_and_approx = false>
 ALWI void sigmoid_tile_init() {
     MATH((llk_math_eltwise_unary_sfpu_sigmoid_init<fast_and_approx>()));
-}
-
-template <bool fast_and_approx = false>
-ALWI void sigmoid_tile_init_pack() {
-    PACK((llk_math_eltwise_unary_sfpu_sigmoid_init<fast_and_approx>()));
 }
 
 // clang-format off
@@ -93,6 +89,32 @@ ALWI void sigmoid_tile_init_pack() {
 template <int vec_mode = VectorMode::RC, bool fast_and_approx = false>
 ALWI void sigmoid_tile(uint32_t idst) {
     MATH((llk_math_eltwise_unary_sfpu_sigmoid<fast_and_approx, DST_ACCUM_MODE>(idst, vec_mode)));
+}
+
+// clang-format off
+/**
+ * Performs SILU (same as Swish) operation on each element of a tile
+ * in DST register at index tile_index. Uses the following implementation:
+ * Silu[x] = x*Sigmoid[x]
+ *
+ * Ref: https://pytorch.org/docs/stable/generated/torch.nn.SiLU.html?highlight=silu#torch.nn.SiLU
+ *
+ * Return value: None
+ *
+ * | Argument        | Description                                                                | Type     | Valid Range                                           | Required |
+ * |-----------------|----------------------------------------------------------------------------|----------|-------------------------------------------------------|----------|
+ * | idst            | The index of the tile in DST register buffer to perform the computation on | uint32_t | Must be less than the size of the DST register buffer | True     |
+ */
+// clang-format on
+ALWI void silu_tile(uint32_t idst) { MATH((llk_math_eltwise_unary_sfpu_silu<APPROX, DST_ACCUM_MODE>(idst))); }
+
+ALWI void silu_tile_init() { MATH((llk_math_eltwise_unary_sfpu_silu_init<APPROX>())); }
+
+#ifndef ARCH_QUASAR
+
+template <bool fast_and_approx = false>
+ALWI void sigmoid_tile_init_pack() {
+    PACK((llk_math_eltwise_unary_sfpu_sigmoid_init<fast_and_approx>()));
 }
 
 template <int vec_mode = VectorMode::RC, bool fast_and_approx = false>
@@ -475,25 +497,7 @@ ALWI void expm1_tile_init() {
     MATH((llk_math_eltwise_unary_sfpu_expm1_init<approx, DST_ACCUM_MODE>()));
 }
 
-// clang-format off
-/**
- * Performs SILU (same as Swish) operation on each element of a tile
- * in DST register at index tile_index. Uses the following implementation:
- * Silu[x] = x*Sigmoid[x]
- *
- * Ref: https://pytorch.org/docs/stable/generated/torch.nn.SiLU.html?highlight=silu#torch.nn.SiLU
- *
- * Return value: None
- *
- * | Argument        | Description                                                                | Type     | Valid Range                                           | Required |
- * |-----------------|----------------------------------------------------------------------------|----------|-------------------------------------------------------|----------|
- * | idst            | The index of the tile in DST register buffer to perform the computation on | uint32_t | Must be less than the size of the DST register buffer | True     |
- */
-// clang-format on
-ALWI void silu_tile(uint32_t idst) { MATH((llk_math_eltwise_unary_sfpu_silu<APPROX, DST_ACCUM_MODE>(idst))); }
 ALWI void silu_tile_pack(uint32_t idst) { PACK((llk_math_eltwise_unary_sfpu_silu<APPROX, DST_ACCUM_MODE>(idst))); }
-
-ALWI void silu_tile_init() { MATH((llk_math_eltwise_unary_sfpu_silu_init<APPROX>())); }
 ALWI void silu_tile_init_pack() { PACK((llk_math_eltwise_unary_sfpu_silu_init<APPROX>())); }
 
 // topK local sort
