102d79ce6f5d29ee01a1089c2e8a583c6cbcffaa
Samyuktha Sudheer <samyuktha.sudheer@ignitarium.com>
2026-05-11 05:48:15 +0000

    Add Kokoro STFT preprocessing and testing functionality

--- models/experimental/kokoro/tt/__init__.py
+++ models/experimental/kokoro/tt/__init__.py
@@ -10,6 +10,8 @@ from .ttnn_adain_resblk_encode import (
     preprocess_encode_parameters,
 )
 from ..reference.kokoro_source_module_preprocess import preprocess_source_module_hn_nsf_parameters
+from ..reference.kokoro_stft_preprocess import preprocess_kokoro_conv_stft_parameters
+from .ttnn_kokoro_stft import KokoroConvStft
 from .ttnn_source_module_hn_nsf import SourceModuleHnNSF
 
 __all__ = [
@@ -19,5 +21,7 @@ __all__ = [
     "preprocess_adain_resblk1d_parameters",
     "preprocess_encode_parameters",
     "preprocess_source_module_hn_nsf_parameters",
+    "preprocess_kokoro_conv_stft_parameters",
+    "KokoroConvStft",
     "SourceModuleHnNSF",
 ]
