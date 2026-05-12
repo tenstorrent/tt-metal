4887fabab11d6d21e8a7d31b19d1c211082e64ec
uvelimirovic <uvelimirovic@tenstorrent.com>
2026-05-12 08:43:27 +0000

    Added pack_mxint8 declarations in stimuli_config and utils.

--- tt_metal/tt-llk/tests/python_tests/helpers/stimuli_config.py
+++ tt_metal/tt-llk/tests/python_tests/helpers/stimuli_config.py
@@ -33,6 +33,7 @@ from .pack import (
     pack_mxfp4,
     pack_mxfp8p,
     pack_mxfp8r,
+    pack_mxint8,
     pack_uint8,
     pack_uint16,
     pack_uint32,
@@ -261,6 +262,7 @@ class StimuliConfig:
             DataFormat.MxFp8R: pack_mxfp8r,
             DataFormat.MxFp8P: pack_mxfp8p,
             DataFormat.MxFp4: pack_mxfp4,
+            DataFormat.MxInt8: pack_mxint8,
             DataFormat.Fp8_e4m3: pack_fp8_e4m3,
             DataFormat.UInt32: pack_uint32,
             DataFormat.Int16: pack_int16,
@@ -301,7 +303,7 @@ class StimuliConfig:
             tile_elements = num_faces * face_r_dim * FACE_C_DIM
 
         def _pack_tile(buffer_tile):
-            if pack_function in (pack_mxfp8r, pack_mxfp8p, pack_mxfp4):
+            if pack_function in (pack_mxfp8r, pack_mxfp8p, pack_mxfp4, pack_mxint8):
                 return pack_function(
                     buffer_tile,
                     num_faces=num_faces,
@@ -353,7 +355,7 @@ class StimuliConfig:
         tile_elements = tile_r * tile_c  # Dense: use actual tile dimensions
 
         def _pack_tile(buffer_tile):
-            if pack_function in (pack_mxfp8r, pack_mxfp8p, pack_mxfp4):
+            if pack_function in (pack_mxfp8r, pack_mxfp8p, pack_mxfp4, pack_mxint8):
                 return pack_function(
                     buffer_tile,
                     num_faces=num_faces,
