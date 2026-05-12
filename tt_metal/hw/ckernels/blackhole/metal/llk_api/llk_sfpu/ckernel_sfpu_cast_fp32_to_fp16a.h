85375c8a2089d969fb3ed5874acb2dd65fc6a04c
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-05-12 18:11:10 +0000

    #40182: Replace sfpi::setstgn with sfpi::copysgn as needed
    casting fixes
    
    fi
    
    fix inverted test
    
    use new API
    
    qualifier
    
    tpl fix
    
    fixes
    
    fix
    
    srcb fmt
    
    Fix float_to_fp16
    
    fix store
    
    fmt fixes
    
    Fix conversion

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_cast_fp32_to_fp16a.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_cast_fp32_to_fp16a.h
@@ -16,11 +16,9 @@ template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
 inline void cast_fp32_to_fp16a() {
 #pragma GCC unroll 8
     for (int d = 0; d < ITERATIONS; d++) {
-        // vFloat val = dst_reg[0];
-        // dst_reg[0] = sfpi::float_to_fp16a(val, sfpi::RoundMode::NearestEven);
-        TTI_SFPLOAD(0, 0, 3, 0);
-        TTI_SFP_STOCH_RND(0, 0, 0, 0, 0, 8);
-        TTI_SFPSTORE(0, 1, 3, 0);
+        sfpi::vFloart x = sfpi::dst_reg[0];
+        sfpi::dst_reg[0].mode<sfpi::SFPSTORE_MOD0_FMT_FP16A>() =
+            sfpi::convert<sfpi::vFloat16a>(x, sfpi::RoundMode::NearestEven);
         dst_reg++;
     }
 }
