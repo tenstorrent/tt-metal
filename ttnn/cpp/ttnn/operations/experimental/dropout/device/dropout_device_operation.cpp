44f22949751ea418a139f7c0c40e974e6643a966
Maxim Artemov <ctr-martemovTT@ext.tenstorrent.com>
2026-05-13 08:23:17 +0000

    reinstate custom hash logic

--- ttnn/cpp/ttnn/operations/experimental/dropout/device/dropout_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/experimental/dropout/device/dropout_device_operation.cpp
@@ -4,6 +4,7 @@
 
 #include "dropout_device_operation.hpp"
 
+#include "ttnn/tensor/tensor_utils.hpp"
 #include "ttnn/tensor/tensor_ops.hpp"
 
 using namespace tt::tt_metal;
@@ -103,6 +104,23 @@ Tensor DropoutDeviceOperation::create_output_tensors(
         return *tensor_args.preallocated_output;
     }
     return create_device_tensor(compute_output_specs(args, tensor_args), tensor_args.input.device());
+}
+
+ttsl::hash::hash_t DropoutDeviceOperation::compute_program_hash(
+    const operation_attributes_t& args, const tensor_args_t& tensor_args) {
+    const auto& input_tensor = tensor_args.input;
+    const auto& input_shape = input_tensor.padded_shape();
+    auto args_without_seed = args;
+    args_without_seed.seed = 0;
+    auto program_factory = select_program_factory(args, tensor_args);
+    operation::Hash hash = operation::hash_operation<DropoutDeviceOperation>(
+        args_without_seed,
+        program_factory.index(),
+        input_tensor.dtype(),
+        input_tensor.memory_config(),
+        input_shape.volume());
+
+    return hash;
 }
 
 }  // namespace ttnn::experimental::prim
