282d22d97e3cde033c7ec2b45c377635bc2738d9
Ryan Zhu <ryanzhu@tenstorrent.com>
2026-05-13 19:19:36 +0000

    add asserts on unused vars

--- tt_metal/hw/ckernels/quasar/metal/llk_api/llk_sfpu/ckernel_sfpu_exp.h
+++ tt_metal/hw/ckernels/quasar/metal/llk_api/llk_sfpu/ckernel_sfpu_exp.h
@@ -6,6 +6,7 @@
 
 #include "ckernel.h"
 #include "llk_math_eltwise_unary_sfpu_init.h"
+#include "llk_assert.h"
 #include "sfpu/ckernel_sfpu_exp.h"
 
 namespace ckernel {
@@ -18,6 +19,12 @@ template <
     int ITERATIONS = 8,
     [[maybe_unused]] bool CLAMP_NEGATIVE = true>
 void calculate_exponential([[maybe_unused]] const uint exp_base_scale_factor = p_sfpu::kCONST_1_FP16B) {
+    static_assert(is_fp32_dest_acc_en == false);
+    static_assert(SCALE_EN == false);
+    static_assert(CLAMP_NEGATIVE == true);
+    LLK_ASSERT(
+        exp_base_scale_factor == p_sfpu::kCONST_1_FP16B,
+        "Scaling is not supported in the current version of exp on quasar.");
     _calculate_exp_<APPROXIMATION_MODE>(ITERATIONS);
 }
 
@@ -26,6 +33,8 @@ template <
     [[maybe_unused]] uint32_t scale = 0x3F800000,
     [[maybe_unused]] bool CLAMP_NEGATIVE = true>
 void exp_init() {
+    static_assert(scale == 0x3F800000);
+    static_assert(CLAMP_NEGATIVE == true);
     llk_math_eltwise_unary_sfpu_init<SfpuType::exponential>();
 }
 
