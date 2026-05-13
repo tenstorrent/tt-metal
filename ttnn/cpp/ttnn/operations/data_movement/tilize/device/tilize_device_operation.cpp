f05733de2da4a41f6157725300987d749fd7ccfb
Juan Camilo Vega <jvega@tenstorrent.com>
2026-05-13 18:08:14 +0000

    Basic support for fp8 input in tilize (to bfp8 output)

--- ttnn/cpp/ttnn/operations/data_movement/tilize/device/tilize_device_operation.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/tilize/device/tilize_device_operation.cpp
@@ -109,8 +109,8 @@ void TilizeDeviceOperation::validate_on_program_cache_miss(
     TT_FATAL(
         input_tensor_a.dtype() == DataType::BFLOAT16 or input_tensor_a.dtype() == DataType::FLOAT32 or
             input_tensor_a.dtype() == DataType::UINT32 or input_tensor_a.dtype() == DataType::INT32 or
-            input_tensor_a.dtype() == DataType::UINT16,
-        "data type must be bfloat16, float32, uint32, int32, or uint16");
+            input_tensor_a.dtype() == DataType::UINT16 or input_tensor_a.dtype() == DataType::FP8_E4M3,
+        "data type must be bfloat16, float32, uint32, int32, uint16, or fp8_e4m3");
 
     uint32_t stick_size = stick_s * input_tensor_a.element_size();  // Assuming bfloat16 dataformat
 
