9bef041ab9bce3b3c65e83a1afdca1146d049301
Jason Davies <jason@jasondavies.com>
2026-05-13 23:54:37 +0000

    Better

--- tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_log.h
+++ tt_metal/hw/ckernels/wormhole_b0/metal/llk_api/llk_sfpu/ckernel_sfpu_log.h
@@ -102,35 +102,50 @@ sfpi_inline sfpi::vFloat calculate_log_body(sfpi::vFloat in, const uint log_base
 
 template <bool HAS_BASE_SCALING>
 sfpi_inline sfpi::vFloat calculate_log_f32_body(sfpi::vFloat a, const uint log_base_scale_factor) {
-    sfpi::vFloat result;
-
     sfpi::vFloat three_quarters = 0.75f;
     sfpi::vInt e = sfpi::reinterpret<sfpi::vInt>(a) - sfpi::reinterpret<sfpi::vInt>(three_quarters);
-    e = e & 0xff800000;
+    e = sfpi::reinterpret<sfpi::vInt>(sfpi::setman(sfpi::reinterpret<sfpi::vFloat>(e), 0));
     sfpi::vFloat m = sfpi::reinterpret<sfpi::vFloat>(sfpi::reinterpret<sfpi::vInt>(a) - e);
 
-    sfpi::vInt abs_e = sfpi::abs(e);
-    sfpi::vFloat e_float = sfpi::int32_to_float(abs_e, sfpi::RoundMode::NearestEven);
-    e_float = sfpi::setsgn(e_float, sfpi::reinterpret<sfpi::vFloat>(e));
+    sfpi::vFloat result = std::numeric_limits<float>::quiet_NaN();
+
+    // normalise a (-0.0 and subnormals become +0.0)
+    a = a * sfpi::vConst1 + sfpi::vConst0;
 
     // m in [0.75, 1.5). Compute log1p(m - 1) for m - 1 in [-0.25, 0.5).
     m -= sfpi::vConst1;
-    sfpi::vFloat s = m * m;
-    sfpi::vFloat r = -0x1.92cp-5f;
-    r = r * m + 0x1.b84p-4f;
-    r = r * m + -0x1.0c4p-3f;
-    r = r * m + 0x1.274p-3f;
-    r = r * m + -0x1.55p-3f;
-    r = r * m + 0x1.998p-3f;
-    r = r * m + sfpi::vConstFloatPrgm1;
-    r = r * m + sfpi::vConstFloatPrgm2;
-    r = r * m + -0.5f;
-    r = r * s + m;
-    result = e_float * sfpi::vConstFloatPrgm0 + r;
 
-    if constexpr (HAS_BASE_SCALING) {
-        result *= sfpi::reinterpret<sfpi::vFloat>(sfpi::vUInt(log_base_scale_factor));
+    v_if(a >= 0.0f) {
+        sfpi::vFloat s = m * m;
+        sfpi::vFloat r = -0x1.92cp-5f;
+        r = r * m + 0x1.b84p-4f;
+        r = r * m + -0x1.0c4p-3f;
+        r = r * m + 0x1.274p-3f;
+        r = r * m + -0x1.55p-3f;
+        r = r * m + 0x1.998p-3f;
+        sfpi::vInt abs_e = sfpi::abs(e);
+        r = r * m + sfpi::vConstFloatPrgm1;
+        sfpi::vFloat e_float = sfpi::int32_to_float(abs_e, sfpi::RoundMode::NearestEven);
+        r = r * m + sfpi::vConstFloatPrgm2;
+        sfpi::vFloat neg_half = -0.5f;
+        r = __builtin_rvtt_sfpmad(r.get(), m.get(), neg_half.get(), sfpi::SFPMAD_MOD1_OFFSET_NONE);
+
+        // if a==0, then a=inf; does nothing if a==nan or inf
+        a = sfpi::addexp(a, -1);
+
+        r = r * s + m;
+        e_float = sfpi::copysgn(e_float, sfpi::reinterpret<sfpi::vFloat>(e));
+        result = e_float * sfpi::vConstFloatPrgm0 + r;
+
+        if constexpr (HAS_BASE_SCALING) {
+            result *= sfpi::reinterpret<sfpi::vFloat>(sfpi::vUInt(log_base_scale_factor));
+        }
+
+        // if a==nan or inf, result will be nan or ±inf
+        v_if(sfpi::exexp_nodebias(a) - 255 >= 0) { result *= a; }
+        v_endif;
     }
+    v_endif;
 
     return result;
 }
