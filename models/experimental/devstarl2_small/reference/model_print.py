009cb1e6ed9b35762f32423a36933c4e7a8affda
Rahul Thelasseri Parambil <rahul.tp@ignitarium.com>
2026-05-15 04:34:31 +0000

    removed devstral2_directory

--- models/experimental/devstarl2_small/reference/model_print.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoModel, AutoTokenizer
-
-model_name = "mistralai/Devstral-Small-2-24B-Instruct-2512"
-
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModel.from_pretrained(model_name)
-print(model)
--- models/experimental/devstarl2_small/tests/hello_world.py
+++
@@ -1,2 +0,0 @@
-print("Hello, World!")
-print("How are you?")
