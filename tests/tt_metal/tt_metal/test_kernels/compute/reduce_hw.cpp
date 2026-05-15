cb4c9048f1656f8b49a31aa9455a4bc8ae738fa9
Shengxiang Ji <shengxiangji@tenstorrent.com>
2026-05-15 22:15:07 +0000

    migrate test_reduce

--- tests/tt_metal/tt_metal/test_kernels/compute/reduce_hw.cpp
+++ tests/tt_metal/tt_metal/test_kernels/compute/reduce_hw.cpp
@@ -5,45 +5,23 @@
 #include <cstdint>
 
 #include "api/compute/reduce.h"
-#ifdef ARCH_QUASAR
 #include "api/dataflow/dataflow_buffer.h"
 #include "experimental/kernel_args.h"
-#else
-#include "api/dataflow/circular_buffer.h"
-#endif
 
 void kernel_main() {
-#ifdef ARCH_QUASAR
     constexpr uint32_t Ht = get_arg(args::Ht);
     constexpr uint32_t Wt = get_arg(args::Wt);
     constexpr uint32_t NC = get_arg(args::NC);
-#else
-    constexpr uint32_t Ht = get_compile_time_arg_val(0);
-    constexpr uint32_t Wt = get_compile_time_arg_val(1);
-    constexpr uint32_t NC = get_compile_time_arg_val(2);
-#endif
 
     constexpr uint32_t onetile = 1;
 
-#ifdef ARCH_QUASAR
     DataflowBuffer dfb_in(dfb::in_data);
     DataflowBuffer dfb_in_scaler(dfb::in_scaler);
     DataflowBuffer dfb_out(dfb::out);
     compute_kernel_hw_startup(dfb_in.get_id(), dfb_in_scaler.get_id(), dfb_out.get_id());
     reduce_init<REDUCE_OP, REDUCE_DIM>(dfb_in.get_id(), dfb_in_scaler.get_id(), dfb_out.get_id());
-#else
-    CircularBuffer cb0(tt::CBIndex::c_0);
-    CircularBuffer cb2(tt::CBIndex::c_2);
-    CircularBuffer cb16(tt::CBIndex::c_16);
-    compute_kernel_hw_startup(tt::CBIndex::c_0, tt::CBIndex::c_2, tt::CBIndex::c_16);
-    reduce_init<REDUCE_OP, REDUCE_DIM>(tt::CBIndex::c_0, tt::CBIndex::c_2, tt::CBIndex::c_16);
-#endif
 
-#ifdef ARCH_QUASAR
     dfb_in_scaler.wait_front(onetile);
-#else
-    cb2.wait_front(onetile);  // scaler tile from the reader
-#endif
     for (uint32_t nc = 0; nc < NC; nc++) {
         int reduce_dst_idx = 0;
         acquire_dst();
@@ -52,7 +30,6 @@ void kernel_main() {
             // reducing in W means out[h][0] = sum(w=0..W-1, in[h][w])
             // in this case we just sequentially add to accumulator all the W-tiles in a row
             for (uint32_t wt = 0; wt < Wt; ++wt) {
-#ifdef ARCH_QUASAR
                 dfb_in.wait_front(onetile);
 #if (MATH_ONLY == 1)
                 UNPACK((llk_unpack_AB_reduce(dfb_in.get_id(), dfb_in_scaler.get_id(), 0, 0)));
@@ -63,29 +40,11 @@ void kernel_main() {
                 reduce_tile<REDUCE_OP, REDUCE_DIM>(dfb_in.get_id(), dfb_in_scaler.get_id(), 0, 0, reduce_dst_idx);
 #endif
                 dfb_in.pop_front(onetile);
-#else
-                cb0.wait_front(onetile);
-#if (MATH_ONLY == 1)
-                UNPACK((llk_unpack_AB_reduce<REDUCE_OP, REDUCE_DIM>(tt::CBIndex::c_0, tt::CBIndex::c_2, 0, 0)));
-                // REDUCE_OP and REDUCE_DIM are expected to come from add_define
-                reduce_tile_math<REDUCE_OP, REDUCE_DIM>(reduce_dst_idx);
-#elif (MATH_ONLY == 0)
-                // REDUCE_OP and REDUCE_DIM are expected to come from add_define
-                reduce_tile<REDUCE_OP, REDUCE_DIM>(tt::CBIndex::c_0, tt::CBIndex::c_2, 0, 0, reduce_dst_idx);
-#endif
-                cb_pop_front(tt::CBIndex::c_0, onetile);
-#endif
             }
         }
-#ifdef ARCH_QUASAR
         dfb_out.reserve_back(onetile);
         pack_tile(reduce_dst_idx, dfb_out.get_id());
         dfb_out.push_back(onetile);
-#else
-        cb16.reserve_back(onetile);
-        pack_tile(reduce_dst_idx, tt::CBIndex::c_16);
-        cb16.push_back(onetile);
-#endif
         release_dst();
     }
     reduce_uninit();
