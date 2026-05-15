1834aff681d74718a44a149f4ebadb7e663a34b3
Shengxiang Ji <shengxiangji@tenstorrent.com>
2026-05-15 21:21:37 +0000

    migrate test_sfpu_compute

--- tt_metal/kernels/dataflow/reader_unary_2_0.cpp
+++ tt_metal/kernels/dataflow/reader_unary_2_0.cpp
@@ -5,23 +5,21 @@
 #include <stdint.h>
 
 #include "api/dataflow/dataflow_api.h"
+#include "api/dataflow/dataflow_buffer.h"
 #include "api/dataflow/endpoints.h"
-#include "api/dataflow/circular_buffer.h"
+#include "experimental/kernel_args.h"
 
 void kernel_main() {
-    uint32_t src_addr = get_arg_val<uint32_t>(0);
-    uint32_t bank_id = get_arg_val<uint32_t>(1);
-    uint32_t num_tiles = get_arg_val<uint32_t>(2);
+    uint32_t src_addr = get_arg(args::src_addr);
+    uint32_t bank_id = get_arg(args::bank_id);
+    uint32_t num_tiles = get_arg(args::num_tiles);
 
     Noc noc;
-    // ublocks size defined in tiles
     constexpr uint32_t ublock_size_tiles = 1;
-    uint32_t ublock_size_bytes;
 
-    constexpr uint32_t cb_id_in0 = 0;
-    CircularBuffer buff_in(cb_id_in0);
-    ublock_size_bytes = get_tile_size(cb_id_in0) * ublock_size_tiles;
-    // read a ublock of tiles from src to CB, and then push the ublock to unpacker
+    DataflowBuffer buff_in(dfb::out);
+    uint32_t ublock_size_bytes = buff_in.get_entry_size() * ublock_size_tiles;
+    // read a ublock of tiles from src to DFB, and then push the ublock to unpacker
     for (uint32_t i = 0; i < num_tiles; i += ublock_size_tiles) {
         buff_in.reserve_back(ublock_size_tiles);
         noc.async_read(
