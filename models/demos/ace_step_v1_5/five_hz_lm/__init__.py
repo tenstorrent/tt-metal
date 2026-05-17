003128233852156aea070362496a793170ff896e
ign-sakthi <sakthi.adhavan@ignitarium.com>
2026-05-17 11:14:10 +0000

    moved fivehz to ttnn folder

--- models/demos/ace_step_v1_5/five_hz_lm/__init__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: © 2026 Tenstorrent USA, Inc.
-#
-# SPDX-License-Identifier: Apache-2.0
-
-"""Vendored ACE-Step 5 Hz language model stack for tt-metal ACE-Step demos."""
-
-from .local_five_hz_llm import LocalFiveHzLMHandler
-
-__all__ = ["LocalFiveHzLMHandler"]
