b8ce605eaebe92a578456b5db42602371df6f853
tchedaTT <tcheda@tenstorrent.com>
2026-05-19 04:00:34 +0000

    Watcher fix (may not be needed)

--- ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/device/kernels/llama_shapes_sharded_writer.cpp
+++ ttnn/cpp/ttnn/operations/experimental/ccl/all_gather_async/device/kernels/llama_shapes_sharded_writer.cpp
@@ -186,5 +186,7 @@ void kernel_main() {
         noc_semaphore_set(reinterpret_cast<volatile tt_l1_ptr uint32_t*>(out_ready_sem_bank_addr), 0);
     }
 
+    // noc_async_atomic_barrier();
+    //  TODO: Remove if watcher run is clean, restore if not
     noc_async_full_barrier();
 }
