8b8e54ff3f851ad8f6a89e6eb5e80f7694904fda
Hriday Algh <halgh@tenstorrent.com>
2026-05-20 20:18:20 +0000

    Move to stimulus generator v2

--- tt_metal/tt-llk/tests/python_tests/test_dest_copy.py
+++ tt_metal/tt-llk/tests/python_tests/test_dest_copy.py
@@ -8,7 +8,7 @@ from helpers.format_config import DataFormat
 from helpers.llk_params import format_dict
 from helpers.param_config import input_output_formats, parametrize
 from helpers.stimuli_config import StimuliConfig
-from helpers.stimuli_generator import generate_stimuli
+from helpers.stimuli_generator_v2 import generate_stimuli_v2
 from helpers.test_config import TestConfig
 
 
@@ -16,6 +16,13 @@ from helpers.test_config import TestConfig
     formats=input_output_formats(
         [
             DataFormat.Float32,
+            DataFormat.Float16,
+            DataFormat.Float16_b,
+            DataFormat.Int32,
+            DataFormat.Int8,
+            DataFormat.UInt32,
+            DataFormat.UInt16,
+            DataFormat.UInt8,
         ],
         same=True,
     ),
@@ -29,7 +36,7 @@ def test_dump_dest(formats):
 
     input_dimensions = [32, 32]
 
-    src_A, tile_cnt_A, src_B, tile_cnt_B = generate_stimuli(
+    src_A, tile_cnt_A, src_B, tile_cnt_B = generate_stimuli_v2(
         stimuli_format_A=formats.input_format,
         input_dimensions_A=input_dimensions,
         stimuli_format_B=formats.input_format,
