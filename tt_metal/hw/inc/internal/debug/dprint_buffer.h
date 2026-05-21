b4877a2d1eba3a17dc54d3c5e1b8a37e39fcb8d6
Vuk Jovanovic <vjovanovic@tenstorrent.com>
2026-05-21 11:44:43 +0000

    Refactor: Remove DPRINT functionality and replace with DevicePrintMemoryLayout

--- tt_metal/hw/inc/internal/debug/dprint_buffer.h
+++ tt_metal/hw/inc/internal/debug/dprint_buffer.h
@@ -4,16 +4,8 @@
 
 #pragma once
 
-#include "hostdevcommon/dprint_common.h"
 #include "hostdev/dev_msgs.h"
-#include "internal/hw_thread.h"
-
-#include "hostdevcommon/dprint_common.h"
-// Returns the buffer address for current thread+core. Differs for NC/BR/ER/TR0-2.
-inline volatile tt_l1_ptr DebugPrintMemLayout* get_debug_print_buffer() {
-    return GET_MAILBOX_ADDRESS_DEV(dprint_buf.data[internal_::get_hw_thread_idx()]);
-}
 
 inline volatile tt_l1_ptr DevicePrintMemoryLayout* get_device_print_buffer() {
-    return GET_MAILBOX_ADDRESS_DEV(dprint_buf.shared_data);
+    return GET_MAILBOX_ADDRESS_DEV(dprint_buf);
 }
