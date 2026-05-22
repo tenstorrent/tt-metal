36ba516469cfafccc618b7a7f74103f88887f522
Sreeshankar <sreeshankar.h@ignitarium.com>
2026-05-22 12:39:42 +0000

    reference added

---
+++ models/experimental/vibevoice/reference/vibevoice/processor/__init__.py
@@ -0,0 +1,8 @@
+from .vibevoice_processor import VibeVoiceProcessor
+from .vibevoice_tokenizer_processor import AudioNormalizer, VibeVoiceTokenizerProcessor
+
+__all__ = [
+    "VibeVoiceProcessor",
+    "VibeVoiceTokenizerProcessor",
+    "AudioNormalizer",
+]
