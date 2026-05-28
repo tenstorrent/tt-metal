e2144f053da905fce9c000a469fb6a7f9d1739dc
uvelimirovic <uvelimirovic@tenstorrent.com>
2026-05-28 09:32:29 +0000

    Changed the API call in test_mxfp6_typecast.

--- tests/tt_metal/tt_metal/llk/test_mxfp6_typecast.cpp
+++ tests/tt_metal/tt_metal/llk/test_mxfp6_typecast.cpp
@@ -83,9 +83,7 @@ static vector<uint32_t> run_mxfp6_typecast(
 
     experimental::metal2_host_api::KernelSpec reader_spec{
         .unique_id = READER,
-        .source =
-            experimental::metal2_host_api::KernelSpec::SourceFilePath{
-                "tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/dram/direct_reader_unary.cpp"},
+        .source = "tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/dram/direct_reader_unary.cpp",
         .num_threads = 1,
         .dfb_bindings = {{
             .dfb_spec_name = INPUT_DFB,
@@ -104,9 +102,7 @@ static vector<uint32_t> run_mxfp6_typecast(
 
     experimental::metal2_host_api::KernelSpec writer_spec{
         .unique_id = WRITER,
-        .source =
-            experimental::metal2_host_api::KernelSpec::SourceFilePath{
-                "tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/dram/direct_writer_unary.cpp"},
+        .source = "tests/tt_metal/tt_metal/test_kernels/dataflow/unit_tests/dram/direct_writer_unary.cpp",
         .num_threads = 1,
         .dfb_bindings = {{
             .dfb_spec_name = OUTPUT_DFB,
@@ -125,9 +121,7 @@ static vector<uint32_t> run_mxfp6_typecast(
 
     experimental::metal2_host_api::KernelSpec compute_spec{
         .unique_id = COMPUTE,
-        .source =
-            experimental::metal2_host_api::KernelSpec::SourceFilePath{
-                "tests/tt_metal/tt_metal/test_kernels/compute/eltwise_copy.cpp"},
+        .source = "tests/tt_metal/tt_metal/test_kernels/compute/eltwise_copy.cpp",
         .num_threads = 1,
         .dfb_bindings =
             {{
