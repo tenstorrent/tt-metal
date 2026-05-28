14c1eb84f289406ab00c8c0ed1bdf4dbb817a0b4
Borys Bradel <bbradel@tenstorrent.com>
2026-05-28 01:59:17 +0000

    allow sampling to run on quasar

--- ttnn/cpp/ttnn/operations/reduction/sampling/device/kernels/dataflow/reader_values_indices_tensor.cpp
+++ ttnn/cpp/ttnn/operations/reduction/sampling/device/kernels/dataflow/reader_values_indices_tensor.cpp
@@ -14,15 +14,15 @@ template <typename CB>
 FORCE_INLINE void generate_index_tile(CB& cb, const uint32_t wt) {
     cb.reserve_back(1);
     CoreLocalMem<volatile uint32_t> ptr(cb.get_write_ptr());
-    uint16_t wt_offset = wt << 5;
+    uint32_t wt_offset = wt << 5;
 
     uint32_t count = 0;
     for (uint32_t i = 0; i < 2; ++i) {
         for (uint32_t j = 0; j < 2; ++j) {
             for (uint32_t k = 0; k < 16; ++k) {
-                for (uint32_t l = 0; l < 16; l += 2) {
-                    uint16_t value = l + 16 * j + wt_offset;
-                    ptr[count] = (value + 1) << 16 | value;
+                for (uint32_t l = 0; l < 16; ++l) {
+                    uint32_t value = l + 16 * j + wt_offset;
+                    ptr[count] = value;
                     count++;
                 }
             }
@@ -36,6 +36,7 @@ void kernel_main() {
     constexpr uint32_t Wt = get_arg(args::Wt);
     constexpr uint32_t input_indices_page_size = get_arg(args::input_indices_page_size);
     constexpr uint32_t tile_height = get_arg(args::tile_height);
+    constexpr uint32_t num_users = get_arg(args::num_users);
 
     constexpr uint32_t onetile = 1;
     constexpr uint32_t tile_bytes_input_values = get_tile_size(dfb::cb_input_values);
@@ -62,7 +63,8 @@ void kernel_main() {
     }
 
     // input indices RM
-    for (uint32_t j = 0; j < Ht * tile_height; ++j) {
+    // for (uint32_t j = 0; j < Ht * tile_height; ++j) {
+    for (uint32_t j = 0; j < num_users; ++j) {
         input_indices_cb.reserve_back(onetile);
         noc.async_read(s1, input_indices_cb, input_indices_page_size, {.page_id = j}, {.offset_bytes = 0});
         noc.async_read_barrier();
