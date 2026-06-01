eb431fdb6377d22d28d89944069e5ee95911d3c8
Lingjun Wen <lingjunwen@tenstorrent.com>
2026-06-01 20:07:01 +0000

    hf_cache=False removed because we don't load real model weights.

--- models/demos/qwen35_27b/tests/test_fused_mlp.py
+++ models/demos/qwen35_27b/tests/test_fused_mlp.py
@@ -61,7 +61,6 @@ def test_mlp_inference(seq_len, batch_size, mesh_device, hf_model, tmp_path, res
         mesh_device,
         max_batch_size=batch_size,
         max_seq_len=128000,
-        cache_hf=False,
     )
 
     hf_mlp = model_args.reference_mlp()
