f861b0645134e52ec6d98b6cf4f42791931ec09f
Velicko Krsmanovic <vkrsmanovic@tenstorrent.com>
2026-06-02 16:07:40 -0400

    [Quasar] Add pack data-format reconfig (#45447)
    ### Issue
    https://github.com/tenstorrent/tt-metal/issues/43271
    
    ### PR Category
    <!-- Feature for pack data format reconfig like it's done for unpacker
    side -->
    
    ### Summary
    <!-- Adds Quasar llk_pack_reconfig_data_format (gasket IN_DATA_FORMAT
    only) and wires pack_reconfig_data_format in the compute API on
    ARCH_QUASAR. Includes a DFB test (TensixPackReconfigQuasarDfb) with
    three matmuls and per-output pack reconfig + pack_init + pack_tile. -->
    
    ### Notes for reviewers
    <!-- Relates to https://github.com/tenstorrent/tt-metal/issues/43271 -->
    
    ### CI Status
    _Auto-generated on every push. Badges update live. Click a badge to
    filter runs by this branch._
    
    -
    [![](https://github.com/tenstorrent/tt-metal/actions/workflows/sanity-tests.yaml/badge.svg?branch=reconfigPackQuasar)](https://github.com/tenstorrent/tt-metal/actions/workflows/sanity-tests.yaml?query=branch:reconfigPackQuasar)
    -
    [![](https://github.com/tenstorrent/tt-metal/actions/workflows/runtime-sanity-tests.yaml/badge.svg?branch=reconfigPackQuasar)](https://github.com/tenstorrent/tt-metal/actions/workflows/runtime-sanity-tests.yaml?query=branch:reconfigPackQuasar)
    -
    [![](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml/badge.svg?branch=reconfigPackQuasar)](https://github.com/tenstorrent/tt-metal/actions/workflows/blackhole-post-commit.yaml?query=branch:reconfigPackQuasar)
    -
    [![](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml/badge.svg?branch=reconfigPackQuasar)](https://github.com/tenstorrent/tt-metal/actions/workflows/tt-metal-l2-nightly.yaml?query=branch:reconfigPackQuasar)
    -
    [![](https://github.com/tenstorrent/tt-metal/actions/workflows/all-model-tests.yaml/badge.svg?branch=reconfigPackQuasar)](https://github.com/tenstorrent/tt-metal/actions/workflows/all-model-tests.yaml?query=branch:reconfigPackQuasar)
    -
    [![](https://github.com/tenstorrent/tt-metal/actions/workflows/pipeline-select.yaml/badge.svg?branch=reconfigPackQuasar)](https://github.com/tenstorrent/tt-metal/actions/workflows/pipeline-select.yaml?query=branch:reconfigPackQuasar)
    -
    [![](https://github.com/tenstorrent/tt-metal/actions/workflows/pipeline-select-t3k.yaml/badge.svg?branch=reconfigPackQuasar)](https://github.com/tenstorrent/tt-metal/actions/workflows/pipeline-select-t3k.yaml?query=branch:reconfigPackQuasar)
    -
    [![](https://github.com/tenstorrent/tt-metal/actions/workflows/pipeline-select-galaxy.yaml/badge.svg?branch=reconfigPackQuasar)](https://github.com/tenstorrent/tt-metal/actions/workflows/pipeline-select-galaxy.yaml?query=branch:reconfigPackQuasar)

--- tests/tt_metal/tt_metal/test_kernels/compute/reconfig_pack_quasar.cpp
+++ tests/tt_metal/tt_metal/test_kernels/compute/reconfig_pack_quasar.cpp
@@ -15,9 +15,11 @@ void kernel_main() {
     DataflowBuffer d3(dfb::in3);
     DataflowBuffer d4(dfb::in4);
     DataflowBuffer d5(dfb::in5);
-    DataflowBuffer dout(dfb::out);
+    DataflowBuffer out0(dfb::out0);
+    DataflowBuffer out1(dfb::out1);
+    DataflowBuffer out2(dfb::out2);
 
-    mm_init(d0.get_id(), d1.get_id(), dout.get_id());
+    mm_init(d0.get_id(), d1.get_id(), out0.get_id());
 
     d0.wait_front(1);
     d1.wait_front(1);
@@ -25,9 +27,12 @@ void kernel_main() {
     d3.wait_front(1);
     d4.wait_front(1);
     d5.wait_front(1);
-    dout.reserve_back(3);
+    out0.reserve_back(1);
+    out1.reserve_back(1);
+    out2.reserve_back(1);
 
-    // OP[0]: d0×d1 (Float16_b); OP[1]: d2×d3 (Float32, reconfig); OP[2]: d4×d5 (Float16_b, reconfig).
+    // OP[0]: d0×d1 (Float16_b); OP[1]: d2×d3 (Float32); OP[2]: d4×d5 (Float16_b).
+    // Unpack reconfig + matmul init between ops (same as reconfig_unpack_quasar).
     tile_regs_acquire();
 
     matmul_tiles(d0.get_id(), d1.get_id(), 0, 0, 0);
@@ -43,9 +48,15 @@ void kernel_main() {
     tile_regs_commit();
 
     tile_regs_wait();
-    pack_tile(0, dout.get_id());
-    pack_tile(1, dout.get_id());
-    pack_tile(2, dout.get_id());
+    pack_reconfig_data_format(out2.get_id(), out0.get_id());
+    pack_init(out0.get_id());
+    pack_tile(0, out0.get_id());
+    pack_reconfig_data_format(out0.get_id(), out1.get_id());
+    pack_init(out1.get_id());
+    pack_tile(1, out1.get_id());
+    pack_reconfig_data_format(out1.get_id(), out2.get_id());
+    pack_init(out2.get_id());
+    pack_tile(2, out2.get_id());
     tile_regs_release();
 
     d0.pop_front(1);
@@ -54,5 +65,7 @@ void kernel_main() {
     d3.pop_front(1);
     d4.pop_front(1);
     d5.pop_front(1);
-    dout.push_back(3);
+    out0.push_back(1);
+    out1.push_back(1);
+    out2.push_back(1);
 }
