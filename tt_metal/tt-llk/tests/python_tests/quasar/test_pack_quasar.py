0eb7439935aa9243430358774b9861235a1d5c35
Jihoon You <jihoonyou@tenstorrent.com>
2026-06-04 18:15:01 +0000

    updated tests to skip unsupported mxfp4 cases

--- tt_metal/tt-llk/tests/python_tests/quasar/test_pack_quasar.py
+++ tt_metal/tt-llk/tests/python_tests/quasar/test_pack_quasar.py
@@ -172,6 +172,13 @@ def test_pack_quasar(formats_dest_acc_sync_dims_relu, boot_mode=BootMode.DEFAULT
         tile_dimensions,
     ) = formats_dest_acc_sync_dims_relu[0]
 
+    if (
+        formats.output_format == DataFormat.MxFp4
+        or formats.input_format == DataFormat.MxFp4
+    ):
+        if tuple(tile_dimensions) not in ((32, 32), (16, 16)):
+            pytest.skip("MxFp4 only supported for num_faces = 1, 4")
+
     tile_shape = construct_tile_shape(tile_dimensions)
 
     src_A, tile_cnt_A, src_B, _ = generate_stimuli(
