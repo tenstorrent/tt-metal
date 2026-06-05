e5690952a48148a7f50c1faebc2a8eac599b69ae
Nihar Kodkani <nkodkani@tenstorrent.com>
2026-06-05 22:51:22 +0000

    fixed PSD

--- tt_metal/fabric/physical_system_discovery.cpp
+++ tt_metal/fabric/physical_system_discovery.cpp
@@ -24,7 +24,6 @@
 #include "tt_metal/llrt/hal.hpp"
 #include "tt_metal/fabric/serialization/physical_system_descriptor_serialization.hpp"
 #include "tt_metal/fabric/fabric_host_utils.hpp"
-#include <board/board.hpp>
 #include <board/port_lookup.hpp>
 
 namespace tt::tt_metal {
