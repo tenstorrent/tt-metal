344650636b13b19997e50fff957cb65f711e0679
Sreeshankar <sreeshankar.h@ignitarium.com>
2026-06-08 12:21:07 +0000

    nlp_create_qkv_heads added

--- models/experimental/glm4_moe_lite/fused_ops/__init__.py
+++ models/experimental/glm4_moe_lite/fused_ops/__init__.py
@@ -1,2 +1,6 @@
 # SPDX-FileCopyrightText: © 2026 Tenstorrent AI ULC
 # SPDX-License-Identifier: Apache-2.0
+
+from models.experimental.glm4_moe_lite.fused_ops.q_split_heads import q_split_heads
+
+__all__ = ["q_split_heads"]
