c68ab8122c6d2c00c4951df602594df5d334f5c2
sadesoyeTT <sadesoye@tenstorrent.com>
2026-06-09 21:55:23 +0000

    move statetensor to tracing module

--- models/tt_dit/utils/tracing.py
+++ models/tt_dit/utils/tracing.py
@@ -14,6 +14,7 @@ import torch
 from loguru import logger
 
 import ttnn
+from models.tt_dit.utils import tensor
 
 from .tensor import from_torch
 
@@ -43,12 +44,22 @@ class StateTensor:
         value: torch.Tensor | ttnn.Tensor,
         traced: bool,
         dtype: ttnn.DataType = ttnn.bfloat16,
-        mesh_axes: list[int | None] | None = None,
-        device: ttnn.MeshDevice | None = None,
+        mesh_axes: list[int] | None = None,
+        device: ttnn.Device | None = None,
     ) -> None:
+        """Update the state tensor with a new value.
+           If the tensor is used during tracing, the value is copied to (or used to initalize) the underlying tensor.
+
+        Args:
+            value: The new value to update the state tensor with.
+            traced: Whether the tensor is used during tracing.
+            dtype: The data type of the value.
+            mesh_axes: The mesh axes of the value.
+            device: The device of the value.
+        """
         if torch.is_tensor(value):
             assert device is not None, "device must be provided if using torch tensor"
-            value = from_torch(value, device=device, mesh_axes=mesh_axes, dtype=dtype)
+            value = tensor.from_torch(value, device=device, mesh_axes=mesh_axes, dtype=dtype)
         if self._value is None or not traced:
             self._value = value
         else:
