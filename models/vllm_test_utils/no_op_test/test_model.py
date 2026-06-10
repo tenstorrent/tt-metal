e3aff71ebb279ee09cda8d59e6f3902c00d26ad8
Viktor Pus <viktorpus@tenstorrent.com>
2026-06-10 13:30:53 +0000

    Fix shapes returned by NoOp model

--- models/vllm_test_utils/no_op_test/test_model.py
+++ models/vllm_test_utils/no_op_test/test_model.py
@@ -10,7 +10,8 @@ from models.vllm_test_utils.generative_base import GenerativeTestModelBase
 class DummyNoOpModel(GenerativeTestModelBase):
     """
     Dummy model class which does nothing for prefill and decode forward.
-    Assumes sampling on device. Used to measure the host overheads from vLLM.
+    Returns zero logits for host-side sampling. Used to measure the host
+    overheads from vLLM.
     """
 
     def __init__(self, mesh_device, max_batch_size, vocab_size, **kwargs):
@@ -19,7 +20,7 @@ class DummyNoOpModel(GenerativeTestModelBase):
         self.mesh_device = mesh_device
         self.max_batch_size = max_batch_size
         self.vocab_size = vocab_size
-        self.decode_out = torch.ones(max_batch_size, 1, dtype=torch.int32)
+        self.decode_out = torch.zeros(max_batch_size, 1, vocab_size, dtype=torch.float32)
 
     @classmethod
     def initialize_vllm_model(cls, hf_config, mesh_device, max_batch_size, **kwargs):
@@ -29,7 +30,7 @@ class DummyNoOpModel(GenerativeTestModelBase):
     def prefill_forward(self, *args, **kwargs):
         tokens = kwargs.get("tokens")
         # Run nothing for prefill forward in this dummy model
-        return torch.ones(tokens.shape[0], 1, 1, dtype=torch.int32)
+        return torch.zeros(tokens.shape[0], 1, self.vocab_size, dtype=torch.float32)
 
     def decode_forward(self, *args, **kwargs):
         # Run nothing for decode forward in this dummy model
