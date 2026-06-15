b00c54150aa7d17ca68edef009a0c3bf34d9077c
ign-sakthi <sakthi.adhavan@ignitarium.com>
2026-06-15 10:38:19 +0000

    Add VAE encoder & fixed torch dependencies in VAE decoder part

--- models/experimental/hunyuan_image_3_0/tt/vae/__init__.py
+++ models/experimental/hunyuan_image_3_0/tt/vae/__init__.py
@@ -1,41 +1,50 @@
 # SPDX-FileCopyrightText: © 2026 Tenstorrent AI ULC
 # SPDX-License-Identifier: Apache-2.0
 
-from .attention import HunyuanTtAttention, HunyuanTtRMSNorm, HunyuanTtRoPE2D
-from .vae import (
+from .conv3d import HunyuanSymmetricConv3d, promote_conv3d_fallback_to_exact
+from .decoder import (
+    AttnBlockTTNN,
     ConvInTTNN,
     ConvOutTTNN,
     DecoderTailTTNN,
     DecoderUpTTNN,
+    MidBlockTTNN,
+    NormOutTTNN,
+    ResnetBlockTTNN,
+    UpBlockTTNN,
+    UpsampleDCAETTNN,
+    VAEDecoderTTNN,
+    VAEDecoderUpTailTTNN,
+    bcthw_to_bthwc,
+    bthwc_to_bcthw,
+)
+from .encoder import (
     DownBlockTTNN,
     DownsampleDCAETTNN,
     EncoderConvInTTNN,
     EncoderDownTTNN,
     EncoderHeadTTNN,
     EncoderMidBlockTTNN,
-    MidBlockTTNN,
-    NormOutTTNN,
-    UpBlockTTNN,
-    UpsampleDCAETTNN,
-    VAEDecoderTTNN,
-    VAEDecoderUpTailTTNN,
     VAEEncoderTTNN,
 )
 
 __all__ = [
-    "HunyuanTtAttention",
-    "HunyuanTtRMSNorm",
-    "HunyuanTtRoPE2D",
+    "HunyuanSymmetricConv3d",
+    "promote_conv3d_fallback_to_exact",
+    "AttnBlockTTNN",
     "ConvInTTNN",
     "ConvOutTTNN",
     "DecoderTailTTNN",
     "DecoderUpTTNN",
     "MidBlockTTNN",
     "NormOutTTNN",
+    "ResnetBlockTTNN",
     "UpBlockTTNN",
     "UpsampleDCAETTNN",
     "VAEDecoderTTNN",
     "VAEDecoderUpTailTTNN",
+    "bcthw_to_bthwc",
+    "bthwc_to_bcthw",
     "DownBlockTTNN",
     "DownsampleDCAETTNN",
     "EncoderConvInTTNN",
