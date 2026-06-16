909d594b7c3d0238aac4343a485e2f9875f3f8f2
Nolen <nzhao@tenstorrent.com>
2026-06-16 18:42:47 +0000

    resolve chipp_id -> fnid

--- tt_metal/sources.cmake
+++ tt_metal/sources.cmake
@@ -107,6 +107,7 @@ set(TT_METAL_PUBLIC_API
     api/tt-metalium/hal_types.hpp
     api/tt-metalium/host_api.hpp
     api/tt-metalium/host_buffer.hpp
+    api/tt-metalium/internal/cluster.hpp
     api/tt-metalium/kernel_types.hpp
     api/tt-metalium/math.hpp
     api/tt-metalium/maybe_remote.hpp
