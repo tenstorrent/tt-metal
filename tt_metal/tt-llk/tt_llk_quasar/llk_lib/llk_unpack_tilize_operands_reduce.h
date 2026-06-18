5bed925e72c1896c99649155b5b5a15c1ec5270e
Ana Mokan <amokan@tenstorrent.com>
2026-06-18 08:09:03 +0000

    Add tensor shape assert and clean up

--- tt_metal/tt-llk/tt_llk_quasar/llk_lib/llk_unpack_tilize_operands_reduce.h
+++ tt_metal/tt-llk/tt_llk_quasar/llk_lib/llk_unpack_tilize_operands_reduce.h
@@ -7,6 +7,7 @@
 #include <cstdint>
 
 #include "ckernel_trisc_common.h"
+#include "llk_assert.h"
 #include "llk_defs.h"
 #include "llk_unpack_common.h"
 #include "tensor_shape.h"
@@ -16,18 +17,18 @@
  *
  * Tilizes a full face into SrcA. This MOP is called multiple times to tilize a full tile in the execute function.
  *
- * @param buf_desc_id_0: Buffer descriptor ID for the operand that will be tilized
+ * @param buf_desc_id: Buffer descriptor ID for the operand that will be tilized into srcA
  * @param full_ct_dim: Number of tiles in a row of the input tensor
  * @param tensor_shape: Contains all the information of the tile shape for the input that will be tilized: num faces, face row/col dim, etc
  */
-inline void _llk_unpack_tilize_operands_reduce_mop_config_(const std::uint32_t buf_desc_id_0, const std::uint32_t full_ct_dim, const TensorShape& tensor_shape)
+inline void _llk_unpack_tilize_operands_reduce_mop_config_(const std::uint32_t buf_desc_id, const std::uint32_t full_ct_dim, const TensorShape& tensor_shape)
 {
     constexpr std::uint32_t MOP_OUTER_LOOP = 1;
     constexpr std::uint32_t MOP_INNER_LOOP = 1;
 
     const std::uint32_t unpack_half_face_instrn =
-        TT_OP_UNPACR0_STRIDE(ckernel::unpack::UNPACR_STRIDE_MAX_ROWS /*Src_Reg_Y_Cntr_Incr*/, 0, 0, 0, 0, buf_desc_id_0, 0 /*Set Dvalid*/);
-    const std::uint32_t unpack_half_face_instrn_dvalid = TT_OP_UNPACR0_STRIDE(0 /*Src_Reg_Y_Cntr_Incr*/, 0, 0, 0, 0, buf_desc_id_0, 1 /*Set Dvalid*/);
+        TT_OP_UNPACR0_STRIDE(ckernel::unpack::UNPACR_STRIDE_MAX_ROWS /*Src_Reg_Y_Cntr_Incr*/, 0, 0, 0, 0, buf_desc_id, 0 /*Set Dvalid*/);
+    const std::uint32_t unpack_half_face_instrn_dvalid = TT_OP_UNPACR0_STRIDE(0 /*Src_Reg_Y_Cntr_Incr*/, 0, 0, 0, 0, buf_desc_id, 1 /*Set Dvalid*/);
 
     const std::uint32_t increment_half_face_instrn = TT_OP_INC_SRC_TILE_FACE_ROW_IDX(
         p_set_inc_sel::TILE_SEL, p_unpacr::UNP_A, tensor_shape.num_faces_c_dim * ckernel::unpack::UNPACR_STRIDE_MAX_ROWS * full_ct_dim);
@@ -42,12 +43,14 @@ inline void _llk_unpack_tilize_operands_reduce_mop_config_(const std::uint32_t b
 /**
  * @brief Initializes unpacker to tilize a full tile for SrcA and unpack a single face for SrcB (scalar), compatible with math reduce kernels.
  *
- * @param buf_desc_id_0: Buffer descriptor ID for the operand that will be tilized
+ * @param buf_desc_id: Buffer descriptor ID for the operand that will be tilized into srcA
  * @param full_ct_dim: Number of tiles in a row of the input tensor
  * @param tensor_shape: Contains all the information of the tile shape for the input that will be tilized: num faces, face row/col dim, etc
  */
-inline void _llk_unpack_tilize_operands_reduce_init_(const std::uint32_t buf_desc_id_0, const std::uint32_t full_ct_dim, const TensorShape& tensor_shape)
+inline void _llk_unpack_tilize_operands_reduce_init_(const std::uint32_t buf_desc_id, const std::uint32_t full_ct_dim, const TensorShape& tensor_shape)
 {
+    LLK_ASSERT(validate_tensor_shape_tile_dependent_ops_(tensor_shape), "Invalid tensor shape for tile-dependent op");
+
     cfg_rmw(THCON_UNPACKER0_REG0_TRANSPOSE_RMW, 0);
     cfg_rmw(THCON_UNPACKER1_REG0_TRANSPOSE_RMW, 0);
 
@@ -58,7 +61,7 @@ inline void _llk_unpack_tilize_operands_reduce_init_(const std::uint32_t buf_des
     cfg[THCON_UNPACKER0_REG1_UNPACK_TILIZE_SRC_Z_STRIDE_ADDR32] = unpk_cfg.val[0];
     cfg[THCON_UNPACKER0_REG2_UNPACK_STRIDE_OFFSET_0_ADDR32]     = unpk_cfg.val[2];
 
-    _llk_unpack_tilize_operands_reduce_mop_config_(buf_desc_id_0, full_ct_dim, tensor_shape);
+    _llk_unpack_tilize_operands_reduce_mop_config_(buf_desc_id, full_ct_dim, tensor_shape);
 }
 
 /**
@@ -68,14 +71,14 @@ inline void _llk_unpack_tilize_operands_reduce_init_(const std::uint32_t buf_des
  * L1 index is set to each face in the tile. The L1 index is in units derived from the x/y/z dimensions of the
  * buffer descriptor. The buffer descriptor should be set to x_dim = 16, y_dim = 1, z_dim = 1, so the unit is 1 row.
  *
- * @param buf_desc_id_1: Buffer descriptor ID for scaler operand
+ * @param buf_desc_id: Buffer descriptor ID for scaler operand that will be unpacked into srcB
  * @param full_ct_dim: Number of tiles in a row of the input tensor
  * @param tensor_shape: Contains all the information of the tile shape for the input that will be tilized: num faces, face row/col dim, etc
  * @param start_l1_tile_idx_0: L1 index for UNPACKER0, unpacks to SrcA
  * @param start_l1_tile_idx_1: L1 index for UNPACKER1, unpacks to SrcB
  */
 inline void _llk_unpack_tilize_operands_reduce_(
-    const std::uint32_t buf_desc_id_1,
+    const std::uint32_t buf_desc_id,
     const std::uint32_t full_ct_dim,
     const TensorShape& tensor_shape,
     const std::uint32_t start_l1_tile_idx_0,
@@ -86,7 +89,7 @@ inline void _llk_unpack_tilize_operands_reduce_(
     TTI_SET_DST_TILE_FACE_ROW_IDX(p_set_inc_sel::TILE_SEL, p_unpacr::UNP_B, 0);
 
     // Unpack scaler input face into SrcB
-    TT_UNPACR1_FACE_INC(0, 0, 0, 0, buf_desc_id_1, 1 /*Set Dvalid*/);
+    TT_UNPACR1_FACE_INC(0, 0, 0, 0, buf_desc_id, 1 /*Set Dvalid*/);
 
     // Set L1 and src reg indices for tilizing F0 into SrcA
     TT_SET_SRC_TILE_FACE_ROW_IDX(p_set_inc_sel::TILE_SEL, p_unpacr::UNP_A, start_l1_tile_idx_0 * tensor_shape.num_faces_c_dim);
