fcf4b35fc11319ddcdd895136cc452589acdfe77
Danilo Djekic <ddjekic@tenstorrent.com>
2026-06-19 12:33:09 +0000

    Adjusting scores pcc threshold

--- models/demos/deepseek_v3_d_p/tests/pcc/test_moe_gate_prefill2d.py
+++ models/demos/deepseek_v3_d_p/tests/pcc/test_moe_gate_prefill2d.py
@@ -255,7 +255,7 @@ def test_forward_pass(
     else:
         recall_threshold = 0.95
         logits_pcc_threshold = 0.997
-        scores_pcc_threshold = 0.985
+        scores_pcc_threshold = 0.97
 
     seq_len_per_device = reference_logits.shape[0] // mesh_device.shape[0]
     sp_composer = get_sp_mesh_composer(mesh_device)
