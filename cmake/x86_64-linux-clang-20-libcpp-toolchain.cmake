481555394364ec69d42295e6877afd2c17646356
mert-kurttutan <kurttutan.mert@gmail.com>
2026-06-21 18:27:40 +0000

    nix-dev: add sfpi_root option to make sfpi installation compatible with cmake setting

--- cmake/x86_64-linux-clang-20-libcpp-toolchain.cmake
+++ cmake/x86_64-linux-clang-20-libcpp-toolchain.cmake
@@ -1,8 +1,8 @@
 set(CMAKE_SYSTEM_PROCESSOR "x86_64")
 
-set(CMAKE_C_COMPILER clang-20 CACHE INTERNAL "C compiler")
+set(CMAKE_C_COMPILER clang-20-libcxx CACHE INTERNAL "C compiler")
 
-set(CMAKE_CXX_COMPILER clang++-20 CACHE INTERNAL "C++ compiler")
+set(CMAKE_CXX_COMPILER clang++-20-libcxx CACHE INTERNAL "C++ compiler")
 
 set(CMAKE_CXX_FLAGS_INIT "-stdlib=libc++")
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-lc++ -lc++abi")
