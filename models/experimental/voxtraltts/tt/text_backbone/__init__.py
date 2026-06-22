d695ae25956a1c3df3f3ab4a2b89a2e3bbb0af98
ign-saurav <sauravkumar.mishra@ignitarium.com>
2026-06-22 10:33:38 +0000

    added text_model transformer for voxtral_tts locally

---
+++ models/experimental/voxtraltts/tt/text_backbone/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: © 2026 Tenstorrent USA, Inc.
+# SPDX-License-Identifier: Apache-2.0
+"""Voxtral-local fork of the tt_transformers text framework (vendored, zero tt_transformers calls)."""
