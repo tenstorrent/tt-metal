8a9bc4d8a1d4738832d9181518171e8efb6050f5
tdowdallTT <tdowdall@tenstorrent.com>
2026-06-22 14:10:02 -0400

    ttsim workflows reorg

--- .github/scripts/utils/find-changed-files.sh
+++ .github/scripts/utils/find-changed-files.sh
@@ -142,6 +142,7 @@ while IFS= read -r FILE; do
             ANY_CODE_CHANGED=true
             ;;
         .github/workflows/build-artifact.yaml|.github/workflows/build-docker-artifact.yaml|.github/workflows/ttsim.yaml)
+56c70 (ttsim workflows reorg)
             BUILD_WORKFLOWS_CHANGED=true
             ANY_CODE_CHANGED=true
             ;;
