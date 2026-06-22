1e199039c467f021de56061e96d762cbce15ad08
KalaivaniMCW <kbaskar@tenstorrent.com>
2026-06-22 18:22:06 +0000

    Update CircularBuffer

--- ttnn/cpp/ttnn/operations/data_movement/concat/device/kernels/dataflow/reader_writer_block_sharded_concat.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/concat/device/kernels/dataflow/reader_writer_block_sharded_concat.cpp
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include <stdint.h>
-#include "experimental/circular_buffer.h"
+#include "api/dataflow/circular_buffer.h"
 
 // Dual-RISC block-sharded concat kernel.
 // Used as both reader (BRISC/NOC0) and writer (NCRISC/NOC1) with the host
@@ -20,7 +20,7 @@
 void kernel_main() {
     constexpr uint32_t output_cb_id = get_compile_time_arg_val(0);
 
-    experimental::CircularBuffer output_cb(output_cb_id);
+    CircularBuffer output_cb(output_cb_id);
     uint32_t dst_base = output_cb.get_write_ptr();
 
     uint32_t arg_idx = 0;
@@ -37,7 +37,7 @@ void kernel_main() {
         uint32_t copy_size = get_arg_val<uint32_t>(arg_idx++);
         uint32_t num_rows = get_arg_val<uint32_t>(arg_idx++);
 
-        experimental::CircularBuffer src_cb(src_cb_id);
+        CircularBuffer src_cb(src_cb_id);
         uint32_t src_l1_addr = src_cb.get_read_ptr() + src_l1_offset;
         uint64_t src_noc_addr = get_noc_addr(src_noc_x, src_noc_y, src_l1_addr);
         uint32_t dst_addr = dst_base + dst_offset;
