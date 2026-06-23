c2cd5f101caf022aaeb36f8ad6cc36af584db38c
Izajasz Wrosz <iwrosz@tenstorrent.com>
2026-06-23 14:30:11 +0200

    Update writer_rotate_half_interleaved_start_id.cpp

--- ttnn/cpp/ttnn/operations/experimental/transformer/rotate_half/device/kernels/dataflow/writer_rotate_half_interleaved_start_id.cpp
+++ ttnn/cpp/ttnn/operations/experimental/transformer/rotate_half/device/kernels/dataflow/writer_rotate_half_interleaved_start_id.cpp
@@ -21,6 +21,7 @@ void kernel_main() {
     constexpr uint32_t cb_id_out_mul = get_compile_time_arg_val(1);
     constexpr auto dst_args = TensorAccessorArgs<2>();
 
+    // ublocks size defined in tiles
     const uint32_t tile_bytes = get_tile_size(cb_id_out_no_mul);
 
     constexpr uint32_t onetile = 1;
@@ -31,6 +32,7 @@ void kernel_main() {
 
     uint32_t out_no_mul_curr_id = start_id + half_row_width;
     uint32_t out_mul_curr_id = start_id;
+    // read a ublock of tiles from src to CB, and then push the ublock to unpacker
     for (uint32_t i = 0; i < num_rows; i++) {
         for (uint32_t j = 0; j < half_row_width; j++) {
             cb_out_no_mul.wait_front(onetile);
