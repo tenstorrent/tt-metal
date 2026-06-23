39f0f9c966eb0713fb9f3e3a32c5ac59e38482d3
skotarac <skotarac@tenstorrent.com>
2026-06-23 12:56:53 +0000

    Try 8 tiles; original test

--- tests/tt_metal/tt_metal/api/test_direct.cpp
+++ tests/tt_metal/tt_metal/api/test_direct.cpp
@@ -571,7 +571,7 @@ TEST_F(MeshDeviceFixture, TensixSingleCoreDirectDramReaderWriter) {
         .l1_data_format = tt::DataFormat::Float16_b,
         .node = experimental::NodeCoord(0, 0)};
     for (unsigned int id = 0; id < num_devices_; id++) {
-        test_config.num_tiles = 1;
+        test_config.num_tiles = 8;
         ASSERT_TRUE(unit_tests::dram::direct::reader_writer(devices_.at(id), test_config));
         // test_config.num_tiles = 4;
         // ASSERT_TRUE(unit_tests::dram::direct::reader_writer(devices_.at(id), test_config));
