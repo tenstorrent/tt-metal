d945874c729c52a7f79ed9092795c957933ec6b1
Marko Radosavljevic <markoradosavljevic@tenstorrent.com>
2026-06-24 14:10:18 +0000

    disable use matmul for wh

--- ttnn/cpp/ttnn/kernel_lib/reduce_helpers_common.hpp
+++ ttnn/cpp/ttnn/kernel_lib/reduce_helpers_common.hpp
@@ -14,12 +14,7 @@
  */
 template <ckernel::PoolType pool_type, ckernel::ReduceDim reduce_dim>
 constexpr bool reduce_uses_matmul() {
-#ifdef ARCH_BLACKHOLE
     return false;
-#else
-    return (pool_type == ckernel::PoolType::SUM || pool_type == ckernel::PoolType::AVG) &&
-           reduce_dim == ckernel::ReduceDim::REDUCE_ROW;
-#endif
 }
 
 /**
