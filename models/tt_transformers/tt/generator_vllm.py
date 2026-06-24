5ad7c8fdc11be5dc8a7aed2301d22fe865808a31
Adriel Bustamante <abustamante@tenstorrent.com>
2026-06-24 11:01:22 -0700

    Enable dummy weights

--- models/tt_transformers/tt/generator_vllm.py
+++ models/tt_transformers/tt/generator_vllm.py
@@ -349,6 +349,7 @@ def initialize_vllm_text_transformer(
             max_batch_size=max_batch_size // tt_data_parallel,
             optimizations=lambda model_args: optimizations(model_args.n_layers, model_args.model_name),
             max_seq_len=max_seq_len,
+            dummy_weights=True,
         )
 
         assert model_args_i.model_name.replace("-", "") in hf_config._name_or_path.replace(
