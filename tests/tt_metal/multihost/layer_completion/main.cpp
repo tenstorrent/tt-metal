49b0de3bb3de4a0970a14ceb1c0abd6b8cc70e04
Nolen Zhao <nzhao@tenstorrent.com>
2026-07-03 20:48:23 +0000

    del tests

--- tests/tt_metal/multihost/layer_completion/main.cpp
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: © 2026 Tenstorrent USA, Inc.
-//
-// SPDX-License-Identifier: Apache-2.0
-
-#include <gtest/gtest.h>
-
-#include "common/multihost_test_tools.hpp"
-
-int main(int argc, char** argv) { return multihost::common::multihost_main(argc, argv); }
