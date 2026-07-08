8e8fb1ce8d96b5d94ebfec707526788b7ce102a6
Никола Радојевић <nikola.radojevic@outlogix.com>
2026-07-08 17:26:10 +0200

    Clean up kernels

--- tests/tt_metal/tt_metal/deployment/kernels/eth_simple_recv_kernel.cpp
+++ tests/tt_metal/tt_metal/deployment/kernels/eth_simple_recv_kernel.cpp
@@ -4,15 +4,23 @@
 
 #include "args.hpp"
 
-#define ARGS(X)                \
-    X(uint32_t, transfer_size) \
-    X(uint32_t, transfer_count)
+#define ARGS(X)                 \
+    X(uint32_t, transfer_size)  \
+    X(uint32_t, transfer_count) \
+    X(uint32_t, progress_counter_addr)
 
 void kernel_main() {
     ARG_INIT(ARGS);
 
+    uint32_t* progress_counter = (uint32_t*)progress_counter_addr;
+    *progress_counter = 0;
+
     for (uint32_t i = 0; i < transfer_count; i++) {
         eth_wait_for_bytes(transfer_size);
         eth_receiver_done();
+
+        *progress_counter = i;
     }
+
+    *progress_counter = transfer_count;
 }
