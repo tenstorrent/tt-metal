fea378689941e23f9374864a3ca57e5d7f28c5ab
Edwin Lee <edwinlee@tenstorrent.com>
2026-07-08 23:20:12 +0000

    Conv/pool initial pass

--- ttnn/cpp/ttnn/operations/pool/upsample/device/kernels/dataflow/reader_upsample_unary_stick_layout_interleaved_start_id.cpp
+++ ttnn/cpp/ttnn/operations/pool/upsample/device/kernels/dataflow/reader_upsample_unary_stick_layout_interleaved_start_id.cpp
@@ -4,6 +4,7 @@
 
 #include <stdint.h>
 #include <api/dataflow/dataflow_api.h>
+#include "api/dataflow/dataflow_buffer.h"
 #include <ttnn/operations/pool/device/kernels/experimental_device_api.hpp>
 
 void kernel_main() {
@@ -17,19 +18,19 @@ void kernel_main() {
     constexpr auto src_args = TensorAccessorArgs<2>();
     const auto s0 = TensorAccessor(src_args, src_addr);
 
-    experimental::CB in_cb(cb_id_in0);
+    DataflowBuffer in_dfb(cb_id_in0);
     Noc noc;
 
     const uint32_t end_id = start_page_id + num_pages;
 
     // reader copied the data from DRAM to CB buffer.
     for (uint32_t i = start_page_id; i < end_id; ++i) {
-        in_cb.reserve_back(1);
+        in_dfb.reserve_back(1);
 
-        noc.async_read(s0, in_cb, page_size, {.page_id = i}, {});
+        noc.async_read(s0, in_dfb, page_size, {.page_id = i}, {});
 
         noc.async_read_barrier();
 
-        in_cb.push_back(1);
+        in_dfb.push_back(1);
     }
 }
