912e6fd4e4f3ea746cb1c64617bf09bd4f0578b7
njokovic <njokovic@tenstorrent.com>
2026-07-09 07:42:37 +0000

    fix up define

--- tt_metal/tt-llk/tests/python_tests/quasar/test_eltwise_binary_sfpu_quasar.py
+++ tt_metal/tt-llk/tests/python_tests/quasar/test_eltwise_binary_sfpu_quasar.py
@@ -741,6 +741,7 @@ def _run_quant(binary_op, tile_indices):
             DATA_COPY_TYPE(DataCopyType.A2D),
             UNPACKER_ENGINE_SEL(UnpackerEngine.UnpDest),
             DEST_SYNC(),
+            TYPECAST_FORMATS(),
         ],
         runtimes=[
             TILE_COUNT(tile_cnt),
