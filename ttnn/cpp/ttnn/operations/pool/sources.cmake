90d03200f11f61da7d1e1af46e07b1bebacdac54
Marko Staletovic <mstaletovic@tenstorrent.com>
2026-07-10 15:47:08 +0000

    refactor: streamline ttnn operation registration and module wiring

--- ttnn/cpp/ttnn/operations/pool/sources.cmake
+++ ttnn/cpp/ttnn/operations/pool/sources.cmake
@@ -2,17 +2,12 @@
 # Module owners should update this file when adding/removing/renaming source files.
 
 set(TTNN_OP_POOL_API_HEADERS
-    generic/generic_pools.hpp
     grid_sample/grid_sample.hpp
     grid_sample/grid_sample_prepare_grid.hpp
     rotate/rotate.hpp
-    upsample/upsample.hpp
 )
 
 set(TTNN_OP_POOL_SRCS
-    generic/device/pool_multi_core_program_factory.cpp
-    generic/device/pool_op.cpp
-    generic/generic_pools.cpp
     grid_sample/device/grid_sample_device_operation.cpp
     grid_sample/device/grid_sample_bilinear_program_factory.cpp
     grid_sample/device/grid_sample_nearest_program_factory.cpp
@@ -24,11 +19,4 @@ set(TTNN_OP_POOL_SRCS
     rotate/device/rotate_bilinear_program_factory.cpp
     rotate/rotate.cpp
     pool_utils.cpp
-    upsample/device//upsample_bilinear_program_factory_multicore.cpp
-    upsample/device/upsample_common.cpp
-    upsample/device/upsample_device_operation.cpp
-    upsample/device/upsample_program_factory_multicore_sharded.cpp
-    upsample/device/upsample_program_factory_multicore_interleaved.cpp
-    upsample/device/upsample_nearest_float_program_factory.cpp
-    upsample/upsample.cpp
 )
