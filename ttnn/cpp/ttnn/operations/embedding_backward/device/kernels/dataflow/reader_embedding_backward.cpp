d370fad21927fdb6632d2731d5477a041e35d957
Izajasz Wrosz <iwrosz@tenstorrent.com>
2026-07-13 18:45:33 +0000

    Rename CB identifiers: object -> cb_<name>, index -> cb_<name>_idx
    Flip the naming convention in the embedding_backward compute and reader
    kernels so the D2 CircularBuffer wrapper object takes the clean name
    (cb_grad, cb_mask, ...) and the tt::CBIndex constant carries the _idx
    suffix (cb_grad_idx, ...). Raw-index APIs (unary_op_init_common,
    copy_tile, pack_tile, get_tile_size, get_tile_address, read_tile_value)
    take the _idx constant; wrapper method calls use the object.

--- ttnn/cpp/ttnn/operations/embedding_backward/device/kernels/dataflow/reader_embedding_backward.cpp
+++ ttnn/cpp/ttnn/operations/embedding_backward/device/kernels/dataflow/reader_embedding_backward.cpp
@@ -101,15 +101,15 @@ void kernel_main() {
     constexpr uint32_t seq_len_tiles = get_compile_time_arg_val(2);
     constexpr uint32_t num_embeddings = get_compile_time_arg_val(3);
 
-    constexpr uint32_t cb_grad = tt::CBIndex::c_0;
-    constexpr uint32_t cb_index = tt::CBIndex::c_1;
-    constexpr uint32_t cb_out_intermed = tt::CBIndex::c_2;
-    constexpr uint32_t cb_mask = tt::CBIndex::c_24;
-    constexpr uint32_t cb_chunk_count_scratch = tt::CBIndex::c_25;
-    constexpr uint32_t cb_id_out0 = tt::CBIndex::c_16;
+    constexpr uint32_t cb_grad_idx = tt::CBIndex::c_0;
+    constexpr uint32_t cb_index_idx = tt::CBIndex::c_1;
+    constexpr uint32_t cb_out_intermed_idx = tt::CBIndex::c_2;
+    constexpr uint32_t cb_mask_idx = tt::CBIndex::c_24;
+    constexpr uint32_t cb_chunk_count_scratch_idx = tt::CBIndex::c_25;
+    constexpr uint32_t cb_id_out0_idx = tt::CBIndex::c_16;
 
-    constexpr uint32_t grad_page_size = get_tile_size(cb_grad);
-    constexpr uint32_t out_page_size = get_tile_size(cb_id_out0);
+    constexpr uint32_t grad_page_size = get_tile_size(cb_grad_idx);
+    constexpr uint32_t out_page_size = get_tile_size(cb_id_out0_idx);
 
     constexpr auto grad_args = TensorAccessorArgs<7>();
     constexpr auto index_args = TensorAccessorArgs<grad_args.next_compile_time_args_offset()>();
@@ -120,20 +120,20 @@ void kernel_main() {
     const auto out_s = TensorAccessor(out_args, output_tensor_addr);
 
     Noc noc;
-    CircularBuffer cb_grad_obj(cb_grad);
-    CircularBuffer cb_index_obj(cb_index);
-    CircularBuffer cb_out_intermed_obj(cb_out_intermed);
-    CircularBuffer cb_mask_obj(cb_mask);
-    CircularBuffer cb_chunk_count_scratch_obj(cb_chunk_count_scratch);
-    CircularBuffer cb_id_out0_obj(cb_id_out0);
+    CircularBuffer cb_grad(cb_grad_idx);
+    CircularBuffer cb_index(cb_index_idx);
+    CircularBuffer cb_out_intermed(cb_out_intermed_idx);
+    CircularBuffer cb_mask(cb_mask_idx);
+    CircularBuffer cb_chunk_count_scratch(cb_chunk_count_scratch_idx);
+    CircularBuffer cb_id_out0(cb_id_out0_idx);
 
-    uint32_t index_block_size = get_tile_size(cb_index) >> 5;  // we only need 32 elements
-    uint32_t index_l1_addr = cb_index_obj.get_write_ptr();     // static
+    uint32_t index_block_size = get_tile_size(cb_index_idx) >> 5;  // we only need 32 elements
+    uint32_t index_l1_addr = cb_index.get_write_ptr();             // static
 
     uint32_t chunk_indexes[INPUT_SIZE];
 
     // ZERO out output this core is responsible for
-    uint32_t out_read_ptr = cb_out_intermed_obj.get_read_ptr();
+    uint32_t out_read_ptr = cb_out_intermed.get_read_ptr();
     // Fill one tile with zeros
     generate_zeros_cb(out_read_ptr);
     // Fill all of output (for this core) to zeros
@@ -147,7 +147,7 @@ void kernel_main() {
     }
     noc.async_write_barrier();
 
-    uint32_t chunk_count_l1_addr = cb_chunk_count_scratch_obj.get_read_ptr();
+    uint32_t chunk_count_l1_addr = cb_chunk_count_scratch.get_read_ptr();
     auto chunk_count_ptr = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(chunk_count_l1_addr);
 
     uint32_t grad_tile_idx = hidden_offset;
@@ -170,8 +170,8 @@ void kernel_main() {
             // Pass chunk_count to compute UNPACK
             chunk_count_ptr[0] = chunk_count;
 
-            cb_grad_obj.reserve_back(max_tiles_per_core);
-            uint32_t grad_write_ptr = cb_grad_obj.get_write_ptr();
+            cb_grad.reserve_back(max_tiles_per_core);
+            uint32_t grad_write_ptr = cb_grad.get_write_ptr();
             for (uint32_t hidden_dim = 0; hidden_dim < tiles_per_core; hidden_dim++) {
                 noc.async_read(
                     grad_s,
@@ -182,14 +182,14 @@ void kernel_main() {
                 grad_write_ptr += grad_page_size;
             }
             noc.async_read_barrier();
-            cb_grad_obj.push_back(max_tiles_per_core);
+            cb_grad.push_back(max_tiles_per_core);
             grad_tile_idx += tiles_per_hidden;
 
             for (uint32_t chunk = 0; chunk < chunk_count; ++chunk) {
                 uint32_t chunk_idx = chunk_indexes[chunk];
 
-                cb_mask_obj.reserve_back(1);
-                uint32_t mask_l1_addr = cb_mask_obj.get_write_ptr();
+                cb_mask.reserve_back(1);
+                uint32_t mask_l1_addr = cb_mask.get_write_ptr();
                 generate_mask(index_l1_addr, chunk_idx, mask_l1_addr);
 
 // TODO: Remove debug prints
@@ -200,10 +200,10 @@ void kernel_main() {
                     DPRINT("{}: {} -> {}\n", chunk, idx, msk);
                 }
 #endif
-                cb_mask_obj.push_back(1);
+                cb_mask.push_back(1);
 
-                cb_out_intermed_obj.reserve_back(max_tiles_per_core);
-                uint32_t out_write_ptr = cb_out_intermed_obj.get_write_ptr();
+                cb_out_intermed.reserve_back(max_tiles_per_core);
+                uint32_t out_write_ptr = cb_out_intermed.get_write_ptr();
                 out_tile_idx = chunk_idx * tiles_per_hidden + hidden_offset;
                 for (uint32_t hidden_dim = 0; hidden_dim < tiles_per_core; hidden_dim++) {
                     noc.async_read(
@@ -215,10 +215,10 @@ void kernel_main() {
                     out_write_ptr += out_page_size;
                 }
                 noc.async_read_barrier();
-                cb_out_intermed_obj.push_back(max_tiles_per_core);
+                cb_out_intermed.push_back(max_tiles_per_core);
 
-                cb_id_out0_obj.wait_front(max_tiles_per_core);
-                uint32_t out0_read_ptr = cb_id_out0_obj.get_read_ptr();
+                cb_id_out0.wait_front(max_tiles_per_core);
+                uint32_t out0_read_ptr = cb_id_out0.get_read_ptr();
                 for (uint32_t hidden_dim = 0; hidden_dim < tiles_per_core; hidden_dim++) {
                     noc.async_write(
                         CoreLocalMem<uint32_t>(out0_read_ptr),
@@ -229,7 +229,7 @@ void kernel_main() {
                     out0_read_ptr += out_page_size;
                 }
                 noc.async_write_barrier();
-                cb_id_out0_obj.pop_front(max_tiles_per_core);
+                cb_id_out0.pop_front(max_tiles_per_core);
             }  // chunk_count
         }  // seq_len_tiles
     }  // batch_size
