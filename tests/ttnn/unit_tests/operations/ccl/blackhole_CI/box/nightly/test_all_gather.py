805715518c3bec0aa48c745e7366ea6a380f05ee
pjosipovic <pjosipovic@tenstorrent.com>
2026-07-17 15:21:44 +0000

    Add new ttnn.all_gather implementation
    Squashed changes from tenstorrent/tt-metal#48301.

--- tests/ttnn/unit_tests/operations/ccl/blackhole_CI/box/nightly/test_all_gather.py
+++ tests/ttnn/unit_tests/operations/ccl/blackhole_CI/box/nightly/test_all_gather.py
@@ -4,14 +4,13 @@
 
 import pytest
 import ttnn
-from tests.nightly.t3000.ccl.test_minimal_all_gather_async import run_all_gather_impl
+from tests.nightly.t3000.ccl.test_all_gather import run_all_gather_impl
 from models.common.utility_functions import skip_for_wormhole_b0, skip_for_n_or_less_dev
 from tests.ttnn.unit_tests.operations.ccl.blackhole_CI.box.nightly.test_all_gather_nightly import validate_test
 
 
 @skip_for_wormhole_b0()
 @skip_for_n_or_less_dev(7)
-@pytest.mark.parametrize("num_links", [2])
 @pytest.mark.parametrize("ag_output_shape", [[1, 1, 128, 128]])
 @pytest.mark.parametrize("dim", [3])
 @pytest.mark.parametrize("layout", [ttnn.TILE_LAYOUT])
@@ -27,33 +26,23 @@ from tests.ttnn.unit_tests.operations.ccl.blackhole_CI.box.nightly.test_all_gath
 )
 @pytest.mark.parametrize("enable_trace", [False])
 @pytest.mark.parametrize("num_iters", [3])
-@pytest.mark.parametrize("use_semaphore_free_all_gather_impl", [True])
 @pytest.mark.parametrize(
-    "device_params, all_gather_topology",
+    "device_params",
     [
-        ({"fabric_config": ttnn.FabricConfig.FABRIC_2D, "trace_region_size": 90112}, ttnn.Topology.Linear),
+        {"fabric_config": ttnn.FabricConfig.FABRIC_2D, "trace_region_size": 90112},
     ],
-    indirect=["device_params"],
+    indirect=True,
 )
-@pytest.mark.parametrize("chunks_per_sync", [20])
-@pytest.mark.parametrize("num_workers_per_link", [2])
-@pytest.mark.parametrize("num_buffers_per_channel", [2])
-def test_all_gather_async_training_shapes(
+def test_all_gather_training_shapes(
     bh_2d_mesh_device,
     ag_output_shape,
     dim,
-    num_links,
     ag_input_dtype,
     layout,
     mem_config_input,
     mem_config_ag,
     enable_trace,
-    all_gather_topology,
     num_iters,
-    chunks_per_sync,
-    num_workers_per_link,
-    num_buffers_per_channel,
-    use_semaphore_free_all_gather_impl,
 ):
     num_devices = bh_2d_mesh_device.shape[0]
     cluster_axis = 0
@@ -63,21 +52,14 @@ def test_all_gather_async_training_shapes(
 
     run_all_gather_impl(
         submesh_device,
-        num_devices,
         ag_output_shape,
         dim,
-        num_links,
         ag_input_dtype,
         layout,
         mem_config_input,
         mem_config_ag,
-        all_gather_topology=all_gather_topology,
         enable_trace=enable_trace,
         num_iters=num_iters,
         cluster_axis=cluster_axis,
-        chunks_per_sync=chunks_per_sync,
-        num_workers_per_link=num_workers_per_link,
-        num_buffers_per_channel=num_buffers_per_channel,
         allowed_pcc=0.9999,
-        use_semaphore_free_all_gather_impl=use_semaphore_free_all_gather_impl,
     )
