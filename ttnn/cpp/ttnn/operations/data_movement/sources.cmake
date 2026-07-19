9ce6beb7d2ff3eddaeb8a8f269e70201b845c18c
abdulla-TT <ctr-mohammedaa@ext.tenstorrent.com>
2026-07-19 21:26:08 +0000

    #49900: update cmake

--- ttnn/cpp/ttnn/operations/data_movement/sources.cmake
+++ ttnn/cpp/ttnn/operations/data_movement/sources.cmake
@@ -167,9 +167,9 @@ set(TTNN_OP_DATA_MOVEMENT_SRCS
     untilize_with_unpadding/device/factories/untilize_with_unpadding_single_core_program_factory.cpp
     untilize_with_unpadding/device/factories/untilize_with_unpadding_multi_core_interleaved_program_factory.cpp
     untilize_with_unpadding/device/factories/untilize_with_unpadding_multi_core_sharded_program_factory.cpp
-    untilize_with_unpadding/device/factories/untilize_with_unpadding_multi_core_col_interleaved_program_factory.cpp
     untilize_with_unpadding/device/factories/untilize_with_unpadding_multi_core_block_interleaved_program_factory.cpp
     untilize_with_unpadding/device/factories/untilize_with_unpadding_multi_core_nd_sharded_program_factory.cpp
+    untilize_with_unpadding/device/factories/untilize_with_unpadding_row_major_program_factory.cpp
     untilize_with_unpadding/untilize_with_unpadding.cpp
     view/view.cpp
     sort/sort.cpp
