6dda43dfd1dfae693df8f9826e7d55f58125f8b1
Ivan Klikovac <iklikovac@tenstorrent.com>
2026-07-19 16:27:20 +0000

    reverting momentarily

--- tt_metal/tt-llk/tests/python_tests/conftest.py
+++ tt_metal/tt-llk/tests/python_tests/conftest.py
@@ -34,9 +34,9 @@ if _SHOULD_RUN_SIMULATOR and _SIMULATOR_PATH and _SIMULATOR_PATH.endswith(".so")
 
     _tt_exalens_init.init_ttexalens(simulation_directory=_SIMULATOR_PATH)
 
-from ttexalens import umd_device as _umd_device
+# from ttexalens import umd_device as _umd_device
 
-_umd_device.UmdDevice.can_use_dma = False
+# _umd_device.UmdDevice.can_use_dma = False
 
 import helpers.order_processing as order_processing
 import helpers.utils as utils_module
