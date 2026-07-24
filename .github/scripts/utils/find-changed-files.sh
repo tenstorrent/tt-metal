ed14af86ebe48a913727d941a688c4a3e78baca1
Nemanja Divnic <ndivnic@tenstorrent.com>
2026-07-24 14:20:31 +0000

    Add Build Quasar Perf on Merge gate.

--- .github/scripts/utils/find-changed-files.sh
+++ .github/scripts/utils/find-changed-files.sh
@@ -85,7 +85,7 @@ while IFS= read -r FILE; do
         tt_metal/tt-llk/tests/**)
             LLK_TESTS_CHANGED=true
             ;;
-        .github/workflows/llk-*.yaml|.github/scripts/llk-*.sh|tests/pipeline_reorg/llk_unit_tests.yaml|tests/pipeline_reorg/llk_merge_gate_tests.yaml)
+        .github/workflows/llk-*.yaml|.github/workflows/build-quasar-perf.yml|.github/scripts/llk-*.sh|tests/pipeline_reorg/llk_unit_tests.yaml|tests/pipeline_reorg/llk_merge_gate_tests.yaml)
             LLK_CI_CHANGED=true
             ;;
         tt_metal/**/*.@(h|hpp|c|cpp|cc|py))
