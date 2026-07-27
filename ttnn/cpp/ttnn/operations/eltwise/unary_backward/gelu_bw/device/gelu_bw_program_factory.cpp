4bac3f957d8779ce84047e2dec1d3ac9e77047ec
KalaivaniMCW <kbaskar@tenstorrent.com>
2026-07-27 15:36:17 +0000

    Update gelu_bw tanh approx kernel

--- ttnn/cpp/ttnn/operations/eltwise/unary_backward/gelu_bw/device/gelu_bw_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/eltwise/unary_backward/gelu_bw/device/gelu_bw_program_factory.cpp
@@ -132,6 +132,11 @@ tt::tt_metal::ProgramDescriptor GeluBwProgramFactory::create_descriptor(
     } else {
         compute_defines["COPY_DEST_VALUES"] = "copy_dest_values<DataFormat::Float16_b>";
     }
+    if (output.dtype() == DataType::BFLOAT16) {
+        compute_defines["ROUNDING_MODE"] = "1";
+    } else {
+        compute_defines["ROUNDING_MODE"] = "0";
+    }
     KernelDescriptor compute_desc;
     compute_desc.kernel_source = compute_kernel_path;
     compute_desc.source_type = KernelDescriptor::SourceType::FILE_PATH;
