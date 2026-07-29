d06e4f6caf7326361794b581ac6b56e1983385bd
Oleksandr Bacherikov <obacherikov@tenstorrent.com>
2026-07-29 23:09:39 +0000

    Restore old header as deprecated

---
+++ tt_metal/api/tt-metalium/queue_id.hpp
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent USA, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+// DEPRECATED: QueueId and related utilities were moved to ttnn:
+//
+//   #include <ttnn/api/ttnn/common/queue_id.hpp>
+
+#include <optional>
+#include <tt_stl/strong_type.hpp>
+#include <cstdint>
+
+namespace tt::tt_metal {
+
+using QueueId
+    [[deprecated("This is deprecated and will be removed. Replaced with ttnn::QueueId located in "
+                 "include <ttnn/api/ttnn/common/queue_id.hpp>")]] = ttsl::StrongType<uint8_t, struct QueueIdTag>;
+
+[[deprecated(
+    "This is deprecated and will be removed. Replaced with ttnn::raw_optional located in "
+    "include <ttnn/api/ttnn/common/queue_id.hpp>")]]
+inline std::optional<uint8_t> raw_optional(const std::optional<QueueId>& cq_id) {
+    return cq_id.has_value() ? std::make_optional(cq_id.value().get()) : std::nullopt;
+}
+
+}  // namespace tt::tt_metal
