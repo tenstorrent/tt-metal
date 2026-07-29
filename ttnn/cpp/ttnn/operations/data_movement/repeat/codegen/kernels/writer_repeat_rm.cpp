4c98788ad378a1c2dfe91e9c08f8c972755de7bf
Milosz Moscicki <mmoscicki@tenstorrent.com>
2026-07-29 14:16:51 +0000

    [codegen-port] repeat phase 3 (wh-20260714-1934)

--- ttnn/cpp/ttnn/operations/data_movement/repeat/codegen/kernels/writer_repeat_rm.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/repeat/codegen/kernels/writer_repeat_rm.cpp
@@ -19,6 +19,8 @@
 // CT args: cb_out, xfer_size, l1_stride, TensorAccessorArgs(out_t), BATCH
 // RT args: dst_addr, num_pages, start_id
 #include "api/dataflow/dataflow_api.h"
+#include "api/dataflow/noc.h"
+#include "api/dataflow/circular_buffer.h"
 
 void kernel_main() {
     uint32_t dst_addr = get_arg_val<uint32_t>(0);
@@ -33,6 +35,9 @@ void kernel_main() {
 
     const auto d = TensorAccessor(dst_args, dst_addr);
 
+    Noc noc;
+    CircularBuffer cb(cb_out);
+
     uint32_t page_id = start_id;
 
     if constexpr (BATCH > 1) {
@@ -40,11 +45,11 @@ void kernel_main() {
 
         // Prime the pipeline
         uint32_t batch = (pages_left < BATCH) ? pages_left : BATCH;
-        cb_wait_front(cb_out, batch);
-        uint32_t l1_addr = get_read_ptr(cb_out);
+        cb.wait_front(batch);
+        uint32_t l1_offset = 0;
         for (uint32_t t = 0; t < batch; t++) {
-            noc_async_write_page(page_id++, d, l1_addr, xfer_size);
-            l1_addr += l1_stride;
+            noc.async_write(cb, d, xfer_size, {.offset_bytes = l1_offset}, {.page_id = page_id++, .offset_bytes = 0});
+            l1_offset += l1_stride;
         }
         pages_left -= batch;
         uint32_t prev_batch = batch;
@@ -52,29 +57,30 @@ void kernel_main() {
         // Steady state
         while (pages_left > 0) {
             batch = (pages_left < BATCH) ? pages_left : BATCH;
-            cb_wait_front(cb_out, prev_batch + batch);
-            noc_async_writes_flushed();
-            cb_pop_front(cb_out, prev_batch);
+            cb.wait_front(prev_batch + batch);
+            noc.async_writes_flushed();
+            cb.pop_front(prev_batch);
 
-            l1_addr = get_read_ptr(cb_out);
+            l1_offset = 0;
             for (uint32_t t = 0; t < batch; t++) {
-                noc_async_write_page(page_id++, d, l1_addr, xfer_size);
-                l1_addr += l1_stride;
+                noc.async_write(
+                    cb, d, xfer_size, {.offset_bytes = l1_offset}, {.page_id = page_id++, .offset_bytes = 0});
+                l1_offset += l1_stride;
             }
             pages_left -= batch;
             prev_batch = batch;
         }
 
         // Drain
-        noc_async_writes_flushed();
-        cb_pop_front(cb_out, prev_batch);
+        noc.async_writes_flushed();
+        cb.pop_front(prev_batch);
     } else {
         for (uint32_t i = 0; i < num_pages; i++) {
-            cb_wait_front(cb_out, 1);
-            noc_async_write_page(page_id++, d, get_read_ptr(cb_out), xfer_size);
-            noc_async_writes_flushed();
-            cb_pop_front(cb_out, 1);
+            cb.wait_front(1);
+            noc.async_write(cb, d, xfer_size, {.offset_bytes = 0}, {.page_id = page_id++, .offset_bytes = 0});
+            noc.async_writes_flushed();
+            cb.pop_front(1);
         }
     }
-    noc_async_write_barrier();
+    noc.async_write_barrier();
 }
