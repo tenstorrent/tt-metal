666872667f4e8f0fb9ad88d4e2acc4439f528547
Oleksandr Bacherikov <obacherikov@tenstorrent.com>
2026-07-30 00:20:14 +0000

    Deprecate header

--- tt_metal/api/tt-metalium/queue_id.hpp
+++ tt_metal/api/tt-metalium/queue_id.hpp
@@ -4,9 +4,7 @@
 
 #pragma once
 
-// DEPRECATED: QueueId and related utilities were moved to ttnn:
-//
-//   #include <ttnn/api/ttnn/common/queue_id.hpp>
+#warning "tt-metalium/queue_id.hpp header is deprecated. Replaced with #include <ttnn/api/ttnn/common/queue_id.hpp>"
 
 #include <optional>
 #include <tt_stl/strong_type.hpp>
@@ -15,12 +13,12 @@
 namespace tt::tt_metal {
 
 using QueueId
-    [[deprecated("This is deprecated and will be removed. Replaced with ttnn::QueueId located in "
-                 "include <ttnn/api/ttnn/common/queue_id.hpp>")]] = ttsl::StrongType<uint8_t, struct QueueIdTag>;
+    [[deprecated("This is deprecated and will be removed. Replaced with ttnn::QueueId in "
+                 "#include <ttnn/api/ttnn/common/queue_id.hpp>")]] = ttsl::StrongType<uint8_t, struct QueueIdTag>;
 
 [[deprecated(
-    "This is deprecated and will be removed. Replaced with ttnn::raw_optional located in "
-    "include <ttnn/api/ttnn/common/queue_id.hpp>")]]
+    "This is deprecated and will be removed. Replaced with ttnn::raw_optional in "
+    "#include <ttnn/api/ttnn/common/queue_id.hpp>")]]
 inline std::optional<uint8_t> raw_optional(const std::optional<QueueId>& cq_id) {
     return cq_id.has_value() ? std::make_optional(cq_id.value().get()) : std::nullopt;
 }
