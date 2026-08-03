9c9990c21523ab2f98b1a980b9f19a7a2e28f711
Marko Radosavljevic <markoradosavljevic@tenstorrent.com>
2026-08-03 14:47:41 +0000

    unpack tilize

--- tt_metal/tt-llk/tests/python_tests/fuser/quasar/parser.py
+++ tt_metal/tt-llk/tests/python_tests/fuser/quasar/parser.py
@@ -40,6 +40,7 @@ from .sfpu.binary import BinarySfpu
 from .sfpu.unary import UnarySfpu
 from .unpacker.matmul import MatmulUnpacker
 from .unpacker.reduce import ReduceUnpacker
+from .unpacker.tilize_a import UnpackerTilizeA
 from .unpacker.unpack_a import UnpackerA
 from .unpacker.unpack_ab import UnpackerAB
 
@@ -121,6 +122,11 @@ _datacopy_unpacker = (
     "Datacopy: unpacker must be UnpackerA or UnpackerTilizeA",
 )
 
+_block_full_width = (
+    lambda s, a, b: s._block_size[1] != a.dimensions[1],
+    "block width must be same as operand width",
+)
+
 _forced_unpacker = lambda name: (
     lambda s, a, b: s.unpacker is not None and s.unpacker != name,
     f"unpacker must be {name}",
@@ -145,6 +151,10 @@ UNPACKER_MAP = {
     "UnpackerA": (
         lambda s: UnpackerA(reuse_dest=s.reuse_dest),
         [_no_transpose_unpack_to_dest, _no_transpose_mismatch],
+    ),
+    "UnpackerTilizeA": (
+        lambda s: UnpackerTilizeA(),
+        [_no_transpose, _block_full_width],
     ),
     "UnpackerAB": (
         lambda s: UnpackerAB(),
