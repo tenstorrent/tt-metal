497566ad47331cdf9167591e8e18be6f0881316f
Micah White <micahwhite@tenstorrent.com>
2026-08-03 17:02:26 +0000

    move logic into send_packets

--- tt_metal/hw/inc/internal/ethernet/tunneling.h
+++ tt_metal/hw/inc/internal/ethernet/tunneling.h
@@ -82,7 +82,7 @@ FORCE_INLINE bool eth_txq_is_busy(uint32_t q_num) {
 }
 
 template <bool ctx_switch = true>
-FORCE_INLINE bool eth_send_packet(uint32_t q_num, uint32_t src_word_addr, uint32_t dest_word_addr, uint32_t num_words) {
+FORCE_INLINE void eth_send_packet(uint32_t q_num, uint32_t src_word_addr, uint32_t dest_word_addr, uint32_t num_words) {
     DEBUG_SANITIZE_ETH(src_word_addr << 4, dest_word_addr << 4, num_words << 4);
     WATCHER_CHECK_ETH_LINK_STATUS();
     bool context_switched = false;
@@ -97,7 +97,18 @@ FORCE_INLINE bool eth_send_packet(uint32_t q_num, uint32_t src_word_addr, uint32
     eth_txq_reg_write(q_num, ETH_TXQ_DEST_ADDR, dest_word_addr << 4);
     eth_txq_reg_write(q_num, ETH_TXQ_TRANSFER_SIZE_BYTES, num_words << 4);
     eth_txq_reg_write(q_num, ETH_TXQ_CMD, ETH_TXQ_CMD_START_DATA);
-    return context_switched;
+    if constexpr (ctx_switch) {
+        static uint32_t count = 0;
+        constexpr uint32_t wait_min = 100;
+        if (context_switched) {
+            count = 0;
+        } else if (count == wait_min) {
+            count = 0;
+            risc_context_switch();
+        } else {
+            count++;
+        }
+    }
 }
 
 FORCE_INLINE
