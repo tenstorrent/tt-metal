460d1914200814ac851929bd873890c0104c7958
riverwuTT <riverwu@tenstorrent.com>
2026-08-04 21:37:51 +0000

    Add more experimental move outs

--- ttnn/api/ttnn/tensor/host_buffer/functions.hpp
+++ ttnn/api/ttnn/tensor/host_buffer/functions.hpp
@@ -6,7 +6,7 @@
 
 #include "ttnn/tensor/tensor.hpp"
 
-#include <tt-metalium/experimental/tensor/tensor_apis.hpp>
+#include <tt-metalium/experimental/distributed_tensor/distributed_tensor_apis.hpp>
 
 namespace tt::tt_metal::host_buffer {
 
