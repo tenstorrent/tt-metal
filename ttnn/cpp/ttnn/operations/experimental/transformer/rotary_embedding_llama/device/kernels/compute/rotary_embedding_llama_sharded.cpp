e4482bf1a9a76cc0f84a42b511287eea0137e4c2
Nikola Ostojic <nostojic@tenstorrent.com>
2026-08-05 16:39:00 +0000

    Revert "[Metal 2.0]: Port `experimental/transformer/rotary_embedding_llama` factories (#51490)"
    This reverts commit c0745b85460ffe4f2218188da1fea863deee9bed.

--- ttnn/cpp/ttnn/operations/experimental/transformer/rotary_embedding_llama/device/kernels/compute/rotary_embedding_llama_sharded.cpp
+++ ttnn/cpp/ttnn/operations/experimental/transformer/rotary_embedding_llama/device/kernels/compute/rotary_embedding_llama_sharded.cpp
@@ -9,8 +9,7 @@
 #include "api/compute/bcast.h"
 #include "api/compute/matmul.h"
 #include "api/compute/compute_kernel_hw_startup.h"
-#include "api/dataflow/dataflow_buffer.h"
-#include "experimental/kernel_args.h"
+#include "api/dataflow/circular_buffer.h"
 
 ALWI void ACQ() {
     tile_regs_acquire();
@@ -23,110 +22,108 @@ ALWI void REL() {
 
 void kernel_main() {
     constexpr uint32_t onetile = 1;
-    // Magic CB indices are gone: each buffer is a named DFB binding. The local
-    // aliases keep the LLK/FIFO call sites readable; each is the dfb:: handle.
-    constexpr auto in_dfb = dfb::input;
-    constexpr auto cos_dfb = dfb::cos;
-    constexpr auto sin_dfb = dfb::sin;
-    constexpr auto trans_mat_dfb = dfb::trans_mat;
+    constexpr uint32_t in_cb = get_compile_time_arg_val(0);
+    constexpr uint32_t cos_cb = get_compile_time_arg_val(1);
+    constexpr uint32_t sin_cb = get_compile_time_arg_val(2);
+    constexpr uint32_t trans_mat_cb = get_compile_time_arg_val(3);
 
-    constexpr auto rotated_in_interm_dfb = dfb::rotated_interm;
-    constexpr auto cos_interm_dfb = dfb::cos_interm;
-    constexpr auto sin_interm_dfb = dfb::sin_interm;
-    constexpr auto out_dfb = dfb::out;
-    constexpr auto Wt = get_arg(args::Wt);
-    constexpr auto Ht = get_arg(args::Ht);  // How many rows (tiles) in n_heads dimension
+    constexpr uint32_t rotated_in_interm_cb = get_compile_time_arg_val(4);
+    constexpr uint32_t cos_interm_cb = get_compile_time_arg_val(5);
+    constexpr uint32_t sin_interm_cb = get_compile_time_arg_val(6);
+    constexpr uint32_t out_cb = get_compile_time_arg_val(7);
+    constexpr uint32_t Wt = get_compile_time_arg_val(8);
+    constexpr uint32_t Ht = get_compile_time_arg_val(9);  // How many rows (tiles) in n_heads dimension
 
-    DataflowBuffer in_dfb_obj(in_dfb);
-    DataflowBuffer cos_dfb_obj(cos_dfb);
-    DataflowBuffer sin_dfb_obj(sin_dfb);
-    DataflowBuffer trans_mat_dfb_obj(trans_mat_dfb);
-    DataflowBuffer rotated_in_interm_dfb_obj(rotated_in_interm_dfb);
-    DataflowBuffer cos_interm_dfb_obj(cos_interm_dfb);
-    DataflowBuffer sin_interm_dfb_obj(sin_interm_dfb);
-    DataflowBuffer out_dfb_obj(out_dfb);
+    CircularBuffer in_cb_obj(in_cb);
+    CircularBuffer cos_cb_obj(cos_cb);
+    CircularBuffer sin_cb_obj(sin_cb);
+    CircularBuffer trans_mat_cb_obj(trans_mat_cb);
+    CircularBuffer rotated_in_interm_cb_obj(rotated_in_interm_cb);
+    CircularBuffer cos_interm_cb_obj(cos_interm_cb);
+    CircularBuffer sin_interm_cb_obj(sin_interm_cb);
+    CircularBuffer out_cb_obj(out_cb);
 
-    compute_kernel_hw_startup<SrcOrder::Reverse>(in_dfb, trans_mat_dfb, out_dfb);
-    matmul_init(in_dfb, trans_mat_dfb);
-    binary_op_init_common(rotated_in_interm_dfb, sin_dfb, sin_interm_dfb);  // General Init for all binary ops
+    compute_kernel_hw_startup<SrcOrder::Reverse>(in_cb, trans_mat_cb, out_cb);
+    matmul_init(in_cb, trans_mat_cb);
+    binary_op_init_common(rotated_in_interm_cb, sin_cb, sin_interm_cb);  // General Init for all binary ops
 
     // Get the trans_mat
-    trans_mat_dfb_obj.reserve_back(onetile);
-    trans_mat_dfb_obj.push_back(onetile);
-    trans_mat_dfb_obj.wait_front(onetile);
+    trans_mat_cb_obj.reserve_back(onetile);
+    trans_mat_cb_obj.push_back(onetile);
+    trans_mat_cb_obj.wait_front(onetile);
 
     // Get the sin/cos matrices
     // TODO: To parallelize across multiple batch, this should be in a batch loop
-    sin_dfb_obj.reserve_back(Wt);
-    cos_dfb_obj.reserve_back(Wt);
+    sin_cb_obj.reserve_back(Wt);
+    cos_cb_obj.reserve_back(Wt);
 
-    sin_dfb_obj.push_back(Wt);
-    cos_dfb_obj.push_back(Wt);
+    sin_cb_obj.push_back(Wt);
+    cos_cb_obj.push_back(Wt);
 
     for (uint32_t ht = 0; ht < Ht; ht++) {  // Over n_heads_t dimension
-        rotated_in_interm_dfb_obj.reserve_back(Wt);
-        sin_interm_dfb_obj.reserve_back(Wt);
-        cos_interm_dfb_obj.reserve_back(Wt);
-        out_dfb_obj.reserve_back(Wt);
+        rotated_in_interm_cb_obj.reserve_back(Wt);
+        sin_interm_cb_obj.reserve_back(Wt);
+        cos_interm_cb_obj.reserve_back(Wt);
+        out_cb_obj.reserve_back(Wt);
 
         // Get the input
-        in_dfb_obj.reserve_back(Wt);
-        in_dfb_obj.push_back(Wt);
-        in_dfb_obj.wait_front(Wt);
+        in_cb_obj.reserve_back(Wt);
+        in_cb_obj.push_back(Wt);
+        in_cb_obj.wait_front(Wt);
 
         // Do the computation
 
         // rotated = x @ trans_mat
-        matmul_init(in_dfb, trans_mat_dfb);
+        matmul_init(in_cb, trans_mat_cb);
         ACQ();
         for (uint32_t j = 0; j < Wt; ++j) {
-            matmul_tiles(in_dfb, trans_mat_dfb, j, 0, j);
-            pack_tile(j, rotated_in_interm_dfb, j);
+            matmul_tiles(in_cb, trans_mat_cb, j, 0, j);
+            pack_tile(j, rotated_in_interm_cb, j);
         }
         REL();
-        rotated_in_interm_dfb_obj.push_back(Wt);
-        rotated_in_interm_dfb_obj.wait_front(Wt);
+        rotated_in_interm_cb_obj.push_back(Wt);
+        rotated_in_interm_cb_obj.wait_front(Wt);
 
-        mul_bcast_rows_init_short(rotated_in_interm_dfb, sin_dfb);
+        mul_bcast_rows_init_short(rotated_in_interm_cb, sin_cb);
         ACQ();
         for (uint32_t j = 0; j < Wt; ++j) {
             // sin_interim = rotated * sin
-            mul_tiles_bcast<BroadcastType::ROW>(rotated_in_interm_dfb, sin_dfb, j, j, j);
-            pack_tile(j, sin_interm_dfb, j);
+            mul_tiles_bcast<BroadcastType::ROW>(rotated_in_interm_cb, sin_cb, j, j, j);
+            pack_tile(j, sin_interm_cb, j);
         }
         REL();
-        sin_interm_dfb_obj.push_back(Wt);
-        rotated_in_interm_dfb_obj.pop_front(Wt);
+        sin_interm_cb_obj.push_back(Wt);
+        rotated_in_interm_cb_obj.pop_front(Wt);
 
         ACQ();
         for (uint32_t j = 0; j < Wt; ++j) {
             // cos_interim = x * cos
-            mul_tiles_bcast<BroadcastType::ROW>(in_dfb, cos_dfb, j, j, j);
-            pack_tile(j, cos_interm_dfb, j);
+            mul_tiles_bcast<BroadcastType::ROW>(in_cb, cos_cb, j, j, j);
+            pack_tile(j, cos_interm_cb, j);
         }
         REL();
-        cos_interm_dfb_obj.push_back(Wt);
-        in_dfb_obj.pop_front(Wt);  // Done with input
+        cos_interm_cb_obj.push_back(Wt);
+        in_cb_obj.pop_front(Wt);  // Done with input
 
-        sin_interm_dfb_obj.wait_front(Wt);
-        cos_interm_dfb_obj.wait_front(Wt);
-        add_tiles_init(cos_interm_dfb, sin_interm_dfb);
+        sin_interm_cb_obj.wait_front(Wt);
+        cos_interm_cb_obj.wait_front(Wt);
+        add_tiles_init(cos_interm_cb, sin_interm_cb);
         ACQ();
         for (uint32_t j = 0; j < Wt; ++j) {
             // out = cos_interim + sin_interim
-            add_tiles(cos_interm_dfb, sin_interm_dfb, j, j, j);
-            pack_tile(j, out_dfb, j);
+            add_tiles(cos_interm_cb, sin_interm_cb, j, j, j);
+            pack_tile(j, out_cb, j);
         }
         REL();
-        out_dfb_obj.push_back(Wt);
-        sin_interm_dfb_obj.pop_front(Wt);
-        cos_interm_dfb_obj.pop_front(Wt);
+        out_cb_obj.push_back(Wt);
+        sin_interm_cb_obj.pop_front(Wt);
+        cos_interm_cb_obj.pop_front(Wt);
     }
 
     // Done with the sin/cos matrices, so remove from CB
-    sin_dfb_obj.pop_front(Wt);
-    cos_dfb_obj.pop_front(Wt);
+    sin_cb_obj.pop_front(Wt);
+    cos_cb_obj.pop_front(Wt);
 
     // Done with the transformation matrix, so remove from CB
-    trans_mat_dfb_obj.pop_front(onetile);
+    trans_mat_cb_obj.pop_front(onetile);
 }
--- ttnn/cpp/ttnn/operations/experimental/transformer/rotary_embedding_llama/device/kernels/dataflow/writer_rotary_embedding_llama_interleaved_start_id.cpp
+++ ttnn/cpp/ttnn/operations/experimental/transformer/rotary_embedding_llama/device/kernels/dataflow/writer_rotary_embedding_llama_interleaved_start_id.cpp
@@ -4,10 +4,9 @@
 
 #include "api/dataflow/dataflow_api.h"
 #include "api/dataflow/noc.h"
-#include "api/dataflow/dataflow_buffer.h"
+#include "api/dataflow/circular_buffer.h"
 #include "api/core_local_mem.h"
 #include "api/tensor/noc_traits.h"
-#include "experimental/kernel_args.h"
 
 FORCE_INLINE void zero_tile_at(uint32_t l1_write_addr, uint32_t tile_bytes) {
     volatile tt_l1_ptr uint32_t* ptr = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(l1_write_addr);
@@ -19,32 +18,36 @@ FORCE_INLINE void zero_tile_at(uint32_t l1_write_addr, uint32_t tile_bytes) {
 void kernel_main() {
     Noc noc;
 
-    auto batch_start = get_arg(args::batch_start);
-    auto batch_end = get_arg(args::batch_end);
-    auto seq_t_start = get_arg(args::seq_t_start);
-    auto seq_t_end = get_arg(args::seq_t_end);
+    uint32_t argrt = 0;
+    uint32_t dst_addr = get_arg_val<uint32_t>(argrt++);
+    uint32_t batch_start = get_arg_val<uint32_t>(argrt++);
+    uint32_t batch_end = get_arg_val<uint32_t>(argrt++);
+    uint32_t seq_t_start = get_arg_val<uint32_t>(argrt++);
+    uint32_t seq_t_end = get_arg_val<uint32_t>(argrt++);
 
-    constexpr auto n_heads = get_arg(args::n_heads);
-    constexpr auto Wt = get_arg(args::Wt);
-    constexpr auto Ht = get_arg(args::Ht);
-    constexpr auto rotary_Ht = get_arg(args::rotary_Ht);
+    constexpr uint32_t cb_id_out = get_compile_time_arg_val(0);
+    constexpr uint32_t cb_id_zero = get_compile_time_arg_val(1);
+    constexpr uint32_t n_heads = get_compile_time_arg_val(2);
+    constexpr uint32_t Wt = get_compile_time_arg_val(3);
+    constexpr uint32_t Ht = get_compile_time_arg_val(4);
+    constexpr uint32_t rotary_Ht = get_compile_time_arg_val(5);
+    constexpr auto dst_args = TensorAccessorArgs<6>();
 
-    const auto s = TensorAccessor(tensor::output);
+    const uint32_t tile_bytes = get_tile_size(cb_id_out);
+    const uint32_t zero_tile_bytes = get_tile_size(cb_id_zero);
+    const auto s = TensorAccessor(dst_args, dst_addr);
 
-    DataflowBuffer dfb_out(dfb::out);
-    DataflowBuffer dfb_zero(dfb::zero);
+    CircularBuffer cb_out(cb_id_out);
+    CircularBuffer cb_zero(cb_id_zero);
 
-    const uint32_t tile_bytes = dfb_out.get_entry_size();
-    const uint32_t zero_tile_bytes = dfb_zero.get_entry_size();
-
-    dfb_zero.reserve_back(Wt);
-    uint32_t zero_l1_write_addr = dfb_zero.get_write_ptr();
+    cb_zero.reserve_back(Wt);
+    uint32_t zero_l1_write_addr = cb_zero.get_write_ptr();
     for (uint32_t j = 0; j < Wt; j++) {
         zero_tile_at(zero_l1_write_addr, zero_tile_bytes);
         zero_l1_write_addr += zero_tile_bytes;
     }
-    dfb_zero.push_back(Wt);
-    dfb_zero.wait_front(Wt);
+    cb_zero.push_back(Wt);
+    cb_zero.wait_front(Wt);
 
     for (uint32_t batch_id = batch_start; batch_id < batch_end; ++batch_id) {
         for (uint32_t head_num = 0; head_num < n_heads; ++head_num) {
@@ -52,10 +55,10 @@ void kernel_main() {
                 uint32_t output_curr_idx = batch_id * n_heads * Ht * Wt + head_num * Ht * Wt + seq_tile * Wt;
                 const bool write_rotary_output = seq_tile < rotary_Ht;
                 if (write_rotary_output) {
-                    dfb_out.wait_front(Wt);
+                    cb_out.wait_front(Wt);
                 }
 
-                uint32_t l1_read_addr = write_rotary_output ? dfb_out.get_read_ptr() : dfb_zero.get_read_ptr();
+                uint32_t l1_read_addr = write_rotary_output ? cb_out.get_read_ptr() : cb_zero.get_read_ptr();
                 const uint32_t l1_read_stride = write_rotary_output ? tile_bytes : zero_tile_bytes;
                 const uint32_t write_bytes = write_rotary_output ? tile_bytes : zero_tile_bytes;
                 for (uint32_t j = 0; j < Wt; j++) {
@@ -67,11 +70,11 @@ void kernel_main() {
                 noc.async_write_barrier();
 
                 if (write_rotary_output) {
-                    dfb_out.pop_front(Wt);
+                    cb_out.pop_front(Wt);
                 }
             }
         }
     }
 
-    dfb_zero.pop_front(Wt);
+    cb_zero.pop_front(Wt);
 }
