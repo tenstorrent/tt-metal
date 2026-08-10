5396be87ecc15f7ae2c70d34ae697992d825f4c7
Sofija Jovic <sjovic@tenstorrent.com>
2026-08-10 08:08:07 +0000

    Rename matmul weights and bias multicast pipe

--- ttnn/cpp/ttnn/operations/matmul/device/kernels/dataflow/reader_bmm_tile_layout_in1_receiver_writer_padding.cpp
+++ ttnn/cpp/ttnn/operations/matmul/device/kernels/dataflow/reader_bmm_tile_layout_in1_receiver_writer_padding.cpp
@@ -102,7 +102,7 @@ void kernel_main() {
     CircularBuffer cb_in3(cb_id_in3);
 #endif
 
-    auto in1_pipe = in1_mcast_args.receiver(noc);
+    auto weights_bias_pipe = in1_mcast_args.receiver(noc);
 
     // WRITER
     const auto s = TensorAccessor(out_args, out_tensor_addr);
@@ -118,7 +118,7 @@ void kernel_main() {
                 for (uint32_t block = 0; block < num_blocks_inner_dim; ++block) {
                     // Operand 1
                     cb_in1.reserve_back(in1_block_num_tiles);
-                    in1_pipe.receive();
+                    weights_bias_pipe.receive();
                     cb_in1.push_back(in1_block_num_tiles);
                 }
 
@@ -127,7 +127,7 @@ void kernel_main() {
                 if ((b == 0 && bh == 0) || num_blocks_w_dim > 1) {
                     // Operand 2
                     cb_in3.reserve_back(in3_block_w);
-                    in1_pipe.receive();
+                    weights_bias_pipe.receive();
                     cb_in3.push_back(in3_block_w);
                 }
 #endif
