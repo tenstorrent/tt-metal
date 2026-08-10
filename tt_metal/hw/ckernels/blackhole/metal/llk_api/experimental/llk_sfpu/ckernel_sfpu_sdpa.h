c3ad39c7d7964f7889c08464a9b6ce04da4992e9
Ivan Klikovac <iklikovac@tenstorrent.com>
2026-08-10 12:14:58 +0000

    fix wormhole include paths

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/experimental/llk_sfpu/ckernel_sfpu_sdpa.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/experimental/llk_sfpu/ckernel_sfpu_sdpa.h
@@ -67,7 +67,7 @@ template <
     bool USE_SFPARECIP_INSTR,
     int POLY_DEGREE,
     bool IS_FP32_DEST_ACC_EN,
-    uint16_t SCALE_BF16>
+    std::uint16_t SCALE_BF16>
 inline void calculate_exponential_polynomial() {
     addr_mod_t{
         .srca = {.incr = 0},
@@ -205,7 +205,7 @@ inline void calculate_exponential_polynomial() {
     }
 }
 
-template <bool SDPA_EXP_APPROX_MODE, uint16_t scale_bf16>
+template <bool SDPA_EXP_APPROX_MODE, std::uint16_t scale_bf16>
 inline void calculate_exponential_first_column() {
     constexpr int ITERATIONS_HALF_FACE = 4;
     if constexpr (SDPA_EXP_APPROX_MODE) {
@@ -231,11 +231,11 @@ inline void calculate_exponential_first_column() {
 
 inline void calculate_fused_max_sub_exp_add_tile(int scale_bf16) {
     constexpr int ITERATIONS_HALF_FACE = 4;
-    constexpr uint32_t prev_max_base_idx = 0;
-    constexpr uint32_t worker_max_base_idx = 32;
-    constexpr uint32_t cur_max_base_idx = 64;
-    constexpr uint32_t prev_sum_base_idx = 96;
-    constexpr uint32_t worker_sum_base_idx = 128;
+    constexpr std::uint32_t prev_max_base_idx = 0;
+    constexpr std::uint32_t worker_max_base_idx = 32;
+    constexpr std::uint32_t cur_max_base_idx = 64;
+    constexpr std::uint32_t prev_sum_base_idx = 96;
+    constexpr std::uint32_t worker_sum_base_idx = 128;
 
     for (int d = 0; d < ITERATIONS_HALF_FACE; d++) {
         sfpi::vFloat prev_max_vec = sfpi::dst_reg[prev_max_base_idx];
@@ -269,7 +269,7 @@ inline void calculate_fused_max_sub_exp_add_tile(int scale_bf16) {
     }
 }
 
-inline void calculate_softplus_first_column(uint param0, uint param1, uint param2) {
+inline void calculate_softplus_first_column(std::uint32_t param0, std::uint32_t param1, std::uint32_t param2) {
     constexpr int ITERATIONS_HALF_FACE = 4;
     float beta = ckernel::sfpu::Converter::as_float(param0);
     float beta_reciprocal = ckernel::sfpu::Converter::as_float(param1);
