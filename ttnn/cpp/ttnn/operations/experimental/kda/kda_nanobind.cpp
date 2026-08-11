37a89b32d6092efc585939d8835f5d6e627f2b37
Momcilo Vasilijevic <mvasilijevic@tenstorrent.com>
2026-08-11 02:38:26 +0000

    Add experimental KDA affine transform reduction

--- ttnn/cpp/ttnn/operations/experimental/kda/kda_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/experimental/kda/kda_nanobind.cpp
@@ -5,12 +5,14 @@
 
 #include <nanobind/nanobind.h>
 
+#include "ttnn/operations/experimental/kda/reduce_affine_transforms/reduce_affine_transforms_nanobind.hpp"
 #include "ttnn/operations/experimental/kda/sigmoid_gated_rms_norm/sigmoid_gated_rms_norm_nanobind.hpp"
 
 namespace ttnn::operations::experimental::kda::detail {
 
 void bind_kda(nb::module_& mod) {
     auto kda_module = mod.def_submodule("kda", "Experimental KDA operations");
+    reduce_affine_transforms::detail::bind_reduce_affine_transforms(kda_module);
     sigmoid_gated_rms_norm::detail::bind_sigmoid_gated_rms_norm(kda_module);
 }
 
---
+++ ttnn/cpp/ttnn/operations/experimental/kda/reduce_affine_transforms/sources.cmake
@@ -0,0 +1,14 @@
+set(TTNN_OP_EXPERIMENTAL_KDA_REDUCE_AFFINE_TRANSFORMS_API_HEADERS reduce_affine_transforms.hpp)
+
+set(TTNN_OP_EXPERIMENTAL_KDA_REDUCE_AFFINE_TRANSFORMS_SRCS
+    reduce_affine_transforms.cpp
+    device/reduce_affine_transforms_device_operation.cpp
+    device/reduce_affine_transforms_program_factory.cpp
+)
+
+set(TTNN_OP_EXPERIMENTAL_KDA_SHARED_SRCS ../factory/kda_factory_utils.cpp)
+
+set(TTNN_OP_EXPERIMENTAL_KDA_REDUCE_AFFINE_TRANSFORMS_NANOBIND_SRCS
+    reduce_affine_transforms_nanobind.cpp
+    ../kda_nanobind.cpp
+)
