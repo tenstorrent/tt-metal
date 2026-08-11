1eab4fc2f5955b4592c90bc10ad1a699253ff137
Momcilo Vasilijevic <mvasilijevic@tenstorrent.com>
2026-08-11 13:18:21 +0000

    Qualify chunk recurrence gate data type

--- ttnn/cpp/ttnn/operations/experimental/kda/prepare_chunk_recurrence/device/prepare_chunk_recurrence_program_factory.hpp
+++ ttnn/cpp/ttnn/operations/experimental/kda/prepare_chunk_recurrence/device/prepare_chunk_recurrence_program_factory.hpp
@@ -8,7 +8,11 @@
 namespace ttnn::experimental::prim {
 
 uint32_t prepare_chunk_recurrence_cb_size_bytes(
-    uint32_t chunk_size, uint32_t key_dim, uint32_t value_dim, DataType gate_dtype, uint32_t output_bf16_mask);
+    uint32_t chunk_size,
+    uint32_t key_dim,
+    uint32_t value_dim,
+    tt::tt_metal::DataType gate_dtype,
+    uint32_t output_bf16_mask);
 
 struct PrepareChunkRecurrenceProgramFactory {
     static tt::tt_metal::ProgramDescriptor create_descriptor(
