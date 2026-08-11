f9f6d4841d930b864866e2f2fdae5710672838a8
Ivan Klikovac <iklikovac@tenstorrent.com>
2026-08-11 12:59:41 +0000

    review dawg

--- tt_metal/tt-llk/tests/python_tests/test_hadamard.py
+++ tt_metal/tt-llk/tests/python_tests/test_hadamard.py
@@ -110,9 +110,7 @@ def _config(
         "sources/hadamard_test.cpp",
         formats,
         templates=[
-            HADAMARD(
-                hadamard_normalize=normalize, h16_tile_index=h16_tile_index
-            ),
+            HADAMARD(hadamard_normalize=normalize, h16_tile_index=h16_tile_index),
             MATH_FIDELITY(fidelity),
             DEST_SYNC(dest_sync),
         ],
