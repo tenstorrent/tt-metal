647959bb01620735529ad3a1cc7462904818dd8c
Oleksandr Bacherikov <obacherikov@tenstorrent.com>
2026-08-15 01:37:38 +0000

    Delete factories

--- ttnn/api/ttnn/global_semaphore.hpp
+++ ttnn/api/ttnn/global_semaphore.hpp
@@ -28,20 +28,6 @@ GlobalSemaphore create_global_semaphore(
     uint32_t initial_value,
     BufferType buffer_type = BufferType::L1);
 
-// Multi-device Creation API
-MultiDeviceGlobalSemaphore create_global_semaphore(
-    const std::vector<IDevice*>& devices,
-    const CoreRangeSet& cores,
-    uint32_t initial_value,
-    BufferType buffer_type = BufferType::L1);
-MultiDeviceGlobalSemaphore create_global_semaphore_with_same_address(
-    const std::vector<IDevice*>& devices,
-    const CoreRangeSet& cores,
-    uint32_t initial_value,
-    BufferType buffer_type,
-    uint32_t attempts,
-    bool search_max = false);
-
 tt::tt_metal::DeviceAddr get_global_semaphore_address(const GlobalSemaphore& global_semaphore);
 
 void reset_global_semaphore_value(const GlobalSemaphore& global_semaphore, uint32_t reset_value);
