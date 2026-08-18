fd08ea8ef68cd134c03abc8dbb6efb39e29fb194
Momcilo Vasilijevic <mvasilijevic@tenstorrent.com>
2026-08-18 18:02:02 +0000

    Migrate affine reduction to Metalium 2.0

--- ttnn/cpp/ttnn/operations/experimental/kda/reduce_affine_transforms/device/reduce_affine_transforms_program_factory.hpp
+++ ttnn/cpp/ttnn/operations/experimental/kda/reduce_affine_transforms/device/reduce_affine_transforms_program_factory.hpp
@@ -4,11 +4,12 @@
 #pragma once
 
 #include "reduce_affine_transforms_device_operation_types.hpp"
+#include "ttnn/metal_v2_artifacts.hpp"
 
 namespace ttnn::experimental::prim {
 
 struct ReduceAffineTransformsProgramFactory {
-    static tt::tt_metal::ProgramDescriptor create_descriptor(
+    static ttnn::device_operation::ProgramArtifacts create_program_artifacts(
         const ReduceAffineTransformsParams&, const ReduceAffineTransformsInputs&, std::vector<Tensor>&);
 };
 
