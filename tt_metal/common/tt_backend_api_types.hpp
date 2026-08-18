566ee6ac89b3410f4f34ee4f5cf44945a31fe997
riverwuTT <riverwu@tenstorrent.com>
2026-08-18 21:11:46 +0000

    [Cleanup] Keep is_fp8_format and is_data_format_supported public
    Restore these DataFormat predicates on tt_backend_api_types.hpp; they are sane to keep on the public surface.
    
    Co-Authored-By: BrAIn <brain@tenstorrent.com>

--- tt_metal/common/tt_backend_api_types.hpp
+++ tt_metal/common/tt_backend_api_types.hpp
@@ -7,16 +7,11 @@
 #include <string>
 #include <umd/device/types/arch.hpp>
 
-#include <tt-metalium/tt_backend_api_types.hpp>
-
 namespace tt {
 
 std::string get_string(ARCH arch);
 std::string get_string_lowercase(ARCH arch);
 std::string get_alias(ARCH arch);
 ARCH get_arch_from_string(const std::string& arch_str);
-
-bool is_fp8_format(DataFormat format);
-bool is_data_format_supported(DataFormat format, ARCH arch);
 
 }  // namespace tt
--- tt_metal/impl/program/program.cpp
+++ tt_metal/impl/program/program.cpp
@@ -13,7 +13,6 @@
 #include "impl/buffers/semaphore.hpp"
 #include <ranges>
 #include <tt_align.hpp>
-#include "common/tt_backend_api_types.hpp"
 #include <algorithm>
 #include <array>
 #include <atomic>
