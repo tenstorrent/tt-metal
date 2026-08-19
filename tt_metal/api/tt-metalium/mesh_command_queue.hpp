9e0ca7ed78f2f1099f70013b5dd97822891fb2f0
Oleksandr Bacherikov <obacherikov@tenstorrent.com>
2026-08-19 22:40:20 +0000

    Less namespaces

--- tt_metal/api/tt-metalium/mesh_command_queue.hpp
+++ tt_metal/api/tt-metalium/mesh_command_queue.hpp
@@ -100,14 +100,14 @@ public:
     // * calling enqueue_record_event_to_host() and then waiting for the event to complete on the host.
     virtual void enqueue_write_shards(
         const std::shared_ptr<MeshBuffer>& mesh_buffer,
-        const std::vector<distributed::ShardDataTransfer>& shard_data_transfers,
+        const std::vector<ShardDataTransfer>& shard_data_transfers,
         bool blocking) = 0;
 
     // MeshBuffer Read APIs
     virtual void enqueue_read_mesh_buffer(
         void* host_data, const std::shared_ptr<MeshBuffer>& buffer, bool blocking) = 0;
     virtual void enqueue_read_shards(
-        const std::vector<distributed::ShardDataTransfer>& shard_data_transfers,
+        const std::vector<ShardDataTransfer>& shard_data_transfers,
         const std::shared_ptr<MeshBuffer>& mesh_buffer,
         bool blocking) = 0;
     // TODO: does "enqueue" make sense anymore? Return the object by value instead.
