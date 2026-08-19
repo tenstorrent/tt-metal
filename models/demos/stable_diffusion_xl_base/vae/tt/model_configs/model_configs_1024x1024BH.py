5da0f47668983322d853452ab3137b50193391a0
Friedrich Schöller <friedrich.schoeller@spacelike.ai>
2026-08-19 20:21:35 +0100

    Improve SDXL VAE accuracy

--- models/demos/stable_diffusion_xl_base/vae/tt/model_configs/model_configs_1024x1024BH.py
+++ models/demos/stable_diffusion_xl_base/vae/tt/model_configs/model_configs_1024x1024BH.py
@@ -77,6 +77,15 @@ class VAEModelOptimisationsBH(ModelOptimisations1024x1024BH):
             "negative_mask": False,
         }
 
+        groupnorm_compute_config = ttnn.WormholeComputeKernelConfig(
+            math_fidelity=ttnn.MathFidelity.HiFi4,
+            math_approx_mode=False,
+            fp32_dest_acc_en=True,
+            packer_l1_acc=True,
+        )
+        for groupnorm_config in self.groupnorm_configs.values():
+            groupnorm_config["op_config"]["compute_kernel_config"] = groupnorm_compute_config
+
     def get_matmul_config(self, matmul_path):
         return None
 
