19b8b13681175da4ef1b421f78c41d049cdcec04
Momcilo Vasilijevic <mvasilijevic@tenstorrent.com>
2026-08-20 10:32:44 +0000

    Remove the unused KDA compute config adapter

--- ttnn/cpp/ttnn/operations/experimental/kda/factory/kda_factory_utils.cpp
+++ ttnn/cpp/ttnn/operations/experimental/kda/factory/kda_factory_utils.cpp
@@ -5,25 +5,10 @@
 
 #include <algorithm>
 #include <set>
-#include <tuple>
 
 #include <tt_stl/assert.hpp>
 
 namespace ttnn::experimental::prim::kda_factory_detail {
-
-tt::tt_metal::ComputeConfigDescriptor kda_compute_cfg(
-    tt::ARCH arch, const DeviceComputeKernelConfig& config, bool honor_caller_config) {
-    if (!honor_caller_config) {
-        return tt::tt_metal::ComputeConfigDescriptor{
-            .math_fidelity = tt::tt_metal::MathFidelity::HiFi4, .fp32_dest_acc_en = true, .math_approx_mode = false};
-    }
-    const auto args = get_compute_kernel_config_args(arch, config);
-    return tt::tt_metal::ComputeConfigDescriptor{
-        .math_fidelity = std::get<0>(args),
-        .fp32_dest_acc_en = std::get<2>(args),
-        .dst_full_sync_en = std::get<4>(args),
-        .math_approx_mode = std::get<1>(args)};
-}
 
 KdaPrepWorkDist distribute_prep(tt::tt_metal::CoreCoord grid, uint32_t total, uint32_t core_cap) {
     const uint32_t max_cores = std::min<uint32_t>(grid.x * grid.y, core_cap);
--- ttnn/cpp/ttnn/operations/experimental/kda/factory/kda_factory_utils.hpp
+++ ttnn/cpp/ttnn/operations/experimental/kda/factory/kda_factory_utils.hpp
@@ -6,14 +6,8 @@
 #include <vector>
 
 #include <tt-metalium/core_coord.hpp>
-#include <tt-metalium/program_descriptors.hpp>
-
-#include "ttnn/operations/core/compute_kernel/compute_kernel_config.hpp"
 
 namespace ttnn::experimental::prim::kda_factory_detail {
-
-tt::tt_metal::ComputeConfigDescriptor kda_compute_cfg(
-    tt::ARCH arch, const DeviceComputeKernelConfig& config, bool honor_caller_config = true);
 
 struct KdaPrepWorkDist {
     std::vector<tt::tt_metal::CoreCoord> cores;
