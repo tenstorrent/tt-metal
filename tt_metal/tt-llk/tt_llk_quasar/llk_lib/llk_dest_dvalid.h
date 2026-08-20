2de73129dd268606719ba96cb792fda05574acc2
Marko Radosavljevic <markoradosavljevic@tenstorrent.com>
2026-08-20 08:30:56 +0000

    sfpu hack

--- tt_metal/tt-llk/tt_llk_quasar/llk_lib/llk_dest_dvalid.h
+++ tt_metal/tt-llk/tt_llk_quasar/llk_lib/llk_dest_dvalid.h
@@ -61,6 +61,7 @@ inline void wait_client_idle()
         case client::UNPACK:
             return wait_unpack_idle();
         case client::FPU:
+            wait_sfpu_idle();
             return wait_fpu_idle();
         case client::PACK:
             return wait_pack_idle();
@@ -80,19 +81,40 @@ inline mode configured_mode = mode::UNPACK_TO_DEST_DIS;
 namespace dest_dvalid
 {
 
+constexpr std::uint32_t make_ctrl(std::uint32_t wait_mask, std::uint32_t wait_polarity, std::uint32_t toggle_mask)
+{
+    return (wait_mask << UNPACK_TO_DEST_DVALID_CTRL_wait_mask_SHAMT) | (wait_polarity << UNPACK_TO_DEST_DVALID_CTRL_wait_polarity_SHAMT) |
+           (toggle_mask << UNPACK_TO_DEST_DVALID_CTRL_toggle_mask_SHAMT);
+}
+
 template <client CLIENT, bool UNPACK_TO_DEST>
 constexpr std::uint32_t ctrl_value()
 {
-    constexpr std::uint32_t OWN  = config_of<CLIENT>.bit;
-    constexpr std::uint32_t SUCC = (CLIENT == client::UNPACK) ? config_of<client::FPU>.bit : config_of<client::PACK>.bit;
-    constexpr bool FIRST         = (CLIENT == client::UNPACK) || (CLIENT == client::FPU && !UNPACK_TO_DEST);
+    constexpr std::uint32_t F = p_cleardvalid::FPU;
+    constexpr std::uint32_t P = p_cleardvalid::PACK;
 
-    constexpr std::uint32_t WAIT_MASK     = FIRST ? ALL_CLIENTS : OWN;
-    constexpr std::uint32_t WAIT_POLARITY = FIRST ? 0u : OWN;
-    constexpr std::uint32_t TOGGLE_MASK   = FIRST ? SUCC : (OWN | SUCC);
+    if constexpr (CLIENT == client::UNPACK)
+    {
+        return make_ctrl(P, 0, F | P);
+    }
+    else if constexpr (CLIENT == client::FPU)
+    {
+        return UNPACK_TO_DEST ? make_ctrl(F, F, F) : make_ctrl(F | P, 0, P);
+    }
+    else
+    {
+        return make_ctrl(F | P, P, P);
+    }
+}
 
-    return (WAIT_MASK << UNPACK_TO_DEST_DVALID_CTRL_wait_mask_SHAMT) | (WAIT_POLARITY << UNPACK_TO_DEST_DVALID_CTRL_wait_polarity_SHAMT) |
-           (TOGGLE_MASK << UNPACK_TO_DEST_DVALID_CTRL_toggle_mask_SHAMT);
+template <bool UNPACK_TO_DEST>
+constexpr std::uint32_t sfpu_ctrl_value()
+{
+    constexpr std::uint32_t F = p_cleardvalid::FPU;
+    constexpr std::uint32_t P = p_cleardvalid::PACK;
+    constexpr std::uint32_t S = p_cleardvalid::SFPU;
+
+    return UNPACK_TO_DEST ? make_ctrl(F, F, S) : make_ctrl(F | P, 0, S);
 }
 
 } // namespace dest_dvalid
@@ -116,6 +138,10 @@ inline void _llk_dest_dvalid_configure_()
     }
 
     cfg_rmw(dest_dvalid::config_of<CLIENT>.ctrl_addr32, 0, dest_dvalid::CTRL_MASK, dest_dvalid::ctrl_value<CLIENT, UNPACK_TO_DEST>());
+    if constexpr (CLIENT == dest_dvalid::client::FPU)
+    {
+        cfg_rmw(SFPU_DEST_DVALID_CTRL_wait_mask_ADDR32, 0, dest_dvalid::CTRL_MASK, dest_dvalid::sfpu_ctrl_value<UNPACK_TO_DEST>());
+    }
     TTI_STALLWAIT(p_stall::STALL_THREAD, p_stall::NOTHING, p_stall::CFGEXU, p_stall::TRISC_CFG);
 
     if constexpr (CLIENT == dest_dvalid::client::UNPACK)
@@ -129,7 +155,7 @@ inline void _llk_dest_dvalid_math_init_()
     dest_dvalid::configured_mode = dest_dvalid::mode::UNPACK_TO_DEST_DIS;
     cfg_rmw(
         dest_dvalid::config_of<dest_dvalid::client::FPU>.ctrl_addr32, 0, dest_dvalid::CTRL_MASK, dest_dvalid::ctrl_value<dest_dvalid::client::FPU, false>());
-    cfg_rmw(SFPU_DEST_DVALID_CTRL_wait_mask_ADDR32, 0, dest_dvalid::CTRL_MASK, 0);
+    cfg_rmw(SFPU_DEST_DVALID_CTRL_wait_mask_ADDR32, 0, dest_dvalid::CTRL_MASK, dest_dvalid::sfpu_ctrl_value<false>());
     TTI_STALLWAIT(p_stall::STALL_THREAD, p_stall::NOTHING, p_stall::CFGEXU, p_stall::TRISC_CFG);
 }
 
@@ -160,11 +186,6 @@ inline void _llk_dest_dvalid_signal_()
 
     TTI_STALLWAIT(p_stall::STALL_MATH, p_stall::NOTHING, p_stall::WAIT_SFPU, dest_dvalid::config_of<CLIENT>.drain_res);
 
-    if constexpr (CLIENT == dest_dvalid::client::FPU)
-    {
-        TTI_STALLWAIT(p_stall::STALL_SFPU, p_stall::NOTHING, p_stall::WAIT_SFPU, dest_dvalid::config_of<CLIENT>.drain_res);
-    }
-
     if constexpr (CLIENT == dest_dvalid::client::PACK)
     {
         if constexpr (DST == DstSync::SyncFull)
@@ -175,6 +196,15 @@ inline void _llk_dest_dvalid_signal_()
         {
             TT_ZEROACC(p_zeroacc::CLR_HALF, EN_32BIT_DEST, 0, ADDR_MOD_0, ckernel::pack::clear_dest_bank_id);
             ckernel::pack::_update_clear_dest_bank_id_();
+        }
+    }
+
+    if constexpr (CLIENT == dest_dvalid::client::FPU)
+    {
+        TTI_CLEARDVALID(0, 0, 0, 0, p_cleardvalid::SFPU, 0);
+        if constexpr (DST == DstSync::SyncFull)
+        {
+            TTI_CLEARDVALID(0, 0, 0, p_cleardvalid::SFPU, p_cleardvalid::SFPU, 0);
         }
     }
 
