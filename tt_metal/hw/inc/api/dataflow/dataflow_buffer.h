ff8cdc29302b02052eaa774f96bc7ccf25ae8555
riverwuTT <riverwu@tenstorrent.com>
2026-08-24 20:44:29 +0000

    Bring null_state into the tokens

--- tt_metal/hw/inc/api/dataflow/dataflow_buffer.h
+++ tt_metal/hw/inc/api/dataflow/dataflow_buffer.h
@@ -92,6 +92,7 @@ template <>
 struct DFBBindingToken<NonNull> {
     explicit constexpr DFBBindingToken(uint16_t id) noexcept : id_(id) {}
 
+    static constexpr NullState null_state = NonNull;
     static constexpr bool is_null = false;
     constexpr operator bool() const noexcept { return true; }
 
@@ -112,6 +113,7 @@ template <>
 struct DFBBindingToken<Null> {
     constexpr DFBBindingToken() noexcept = default;
 
+    static constexpr NullState null_state = Null;
     static constexpr bool is_null = true;
     constexpr operator bool() const noexcept { return false; }
 };
