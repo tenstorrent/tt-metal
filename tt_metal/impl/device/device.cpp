400eb8727d32d79a2a7ad4ce86f11a0be5280204
Dusan Lukic <dlukicTT@github.com>
2026-08-24 16:06:35 +0000

    eth placement awareness API

--- tt_metal/impl/device/device.cpp
+++ tt_metal/impl/device/device.cpp
@@ -766,6 +766,11 @@ CoreCoord Device::physical_worker_core_from_logical_core(const CoreCoord& logica
     return soc_desc.get_physical_tensix_core_from_logical(logical_core);
 }
 
+CoreCoord Device::physical_eth_core_from_logical_core(const CoreCoord& logical_core) const {
+    const metal_SocDescriptor& soc_desc = MetalEnvAccessor(*env_).impl().get_cluster().get_soc_desc(this->id_);
+    return soc_desc.get_physical_ethernet_core_from_logical(logical_core);
+}
+
 std::vector<CoreCoord> Device::worker_cores_from_logical_cores(const std::vector<CoreCoord>& logical_cores) const {
     std::vector<CoreCoord> worker_cores(logical_cores.size());
     for (std::size_t idx = 0; idx < logical_cores.size(); idx++) {
