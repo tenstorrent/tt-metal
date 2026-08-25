1d24834a57b8d0cd56debac6870825c8b1cd274c
riverwuTT <riverwu@tenstorrent.com>
2026-08-25 18:29:46 +0000

    Move tensor binding to the tensor accessor

--- tt_metal/hw/inc/api/tensor/tensor_binding_token.h
+++ tt_metal/hw/inc/api/tensor/tensor_binding_token.h
@@ -8,14 +8,14 @@
 
 #include "api/tensor/tensor_accessor_args.h"
 
+namespace tensor_accessor {
+
 // NullTensorBindingToken: emitted when this ProgramSpec did not declare the named TensorParameter.
 // Naming the symbol always compiles. Constructing a TensorAccessor / LocalTensorAccessor from it
 // does not.
 struct NullTensorBindingToken {
     static constexpr bool is_null = true;
 };
-
-namespace tensor_accessor {
 
 // TensorBindingToken:
 //
@@ -38,7 +38,7 @@ namespace tensor_accessor {
 //   - A type alias:  using my_TA_name_t = TensorBindingToken<CTA_OFFSET, ADDR_CRTA_OFFSET>;
 //   - A token value: constexpr my_TA_name_t my_TA_name{};
 // For a null binding (named TensorParameter not declared on this ProgramSpec):
-//   - using my_TA_name_t = NullTensorBindingToken;
+//   - using my_TA_name_t = ::tensor_accessor::NullTensorBindingToken;
 //   - constexpr my_TA_name_t my_TA_name{};
 // The null token has no args_t / addr_crta_offset payload (offsets are not meaningful).
 //
