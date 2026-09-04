db0118ee37e5e11f624c4f8a91d8be0685e6b688
Frank Plavec <fplavec@tenstorrent.com>
2026-09-04 03:13:49 +0000

    Fixed softplus bug

--- ttnn/cpp/ttnn/operations/matmul/device/kernels/compute/bmm_large_block_zm_fused_bias_activation.cpp
+++ ttnn/cpp/ttnn/operations/matmul/device/kernels/compute/bmm_large_block_zm_fused_bias_activation.cpp
@@ -556,7 +556,11 @@ void kernel_main() {
                             DEST_TARGET_REG_CFG_MATH_Offset_ADDR32, ckernel::packer::get_packer_dest_offset()));
 
                         for (uint32_t i = 0; i < out_subblock_num_tiles; i++) {
-                            ActivationApplyHelper<activation_type, activation_param0, activation_param1>::apply(i);
+                            ActivationApplyHelper<
+                                activation_type,
+                                activation_param0,
+                                activation_param1,
+                                activation_param2>::apply(i);
                         }
 
                         PACK(TTI_STALLWAIT(p_stall::STALL_PACK, p_stall::WAIT_SFPU));
