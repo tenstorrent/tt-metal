b57b4590b8cd62026587345f7d8fc7bd0c4b9131
wenbinlyuTT <wenbinlyu@tenstorrent.com>
2026-09-07 22:32:34 +0000

    Update taskflow
    Signed-off-by: wenbinlyuTT <wenbinlyu@tenstorrent.com>

--- tt_metal/common/executor.hpp
+++ tt_metal/common/executor.hpp
@@ -23,7 +23,9 @@
 #endif
 #include <taskflow/taskflow.hpp>
 #include <cstdlib>
-#include <cstring>
+#include <functional>
+#include <future>
+#include <memory>
 #include <thread>
 
 namespace tt::tt_metal::detail {
@@ -109,33 +111,18 @@ inline Executor& GetExecutor() {
 #endif
 }
 
-inline std::mutex& GetExecutorMutex() {
-    static std::mutex exec_mutex;
-    return exec_mutex;
-}
-
-// When a taskflow worker thread throws an exception, Taskflow does not propagate this to main thread
-// (https://github.com/taskflow/taskflow/issues/479) This wrapper ensures that exceptions are re-thrown by packaged_task
-// whenever .get() is called on future object
-// https://stackoverflow.com/questions/16344663/is-there-a-packaged-taskset-exception-equivalent
-// launch an async tf job only if not all workers are occupied
 template <class F, class... Args>
 auto async(F&& func, Args&&... args) {
     using return_type = std::invoke_result_t<F, Args...>;
-
     auto task = std::make_shared<std::packaged_task<return_type()>>(
         std::bind(std::forward<F>(func), std::forward<Args>(args)...));
-    std::shared_future<return_type> res(std::move(task->get_future()));
-    GetExecutorMutex().lock();
-
-    if (GetExecutor().num_topologies() >= GetExecutor().num_workers()) {
-        GetExecutorMutex().unlock();
+    auto future = task->get_future();
+    auto& executor = GetExecutor();
+    if (executor.num_topologies() >= executor.num_workers()) {
         (*task)();
-        return res;
+    } else {
+        executor.silent_async([task]() { (*task)(); });
     }
-
-    GetExecutor().silent_async([task] { (*task)(); });
-    GetExecutorMutex().unlock();
-    return res;
+    return std::shared_future<return_type>(std::move(future));
 }
 }  // namespace tt::tt_metal::detail
