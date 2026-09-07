8724a8b5dd01e8ac386b01ba99e568f40e028ec3
Danilo Djekic <ddjekic@tenstorrent.com>
2026-09-07 15:57:48 +0000

    Remove bloatware, unneccessary tests and python binding for compressor state exchange

--- ttnn/cpp/ttnn/operations/experimental/experimental_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/experimental/experimental_nanobind.cpp
@@ -79,7 +79,6 @@
 #include "ttnn/operations/experimental/deepseek_prefill/dispatch/dispatch_nanobind.hpp"
 #include "ttnn/operations/experimental/deepseek_prefill/combine/combine_nanobind.hpp"
 #include "ttnn/operations/experimental/deepseek_prefill/combine_fabric2d/combine_fabric2d_nanobind.hpp"
-#include "ttnn/operations/experimental/deepseek_prefill/compressor_state_exchange/compressor_state_exchange_nanobind.hpp"
 #include "ttnn/operations/experimental/deepseek_prefill/csa_compressor/csa_compressor_nanobind.hpp"
 #include "ttnn/operations/experimental/deepseek_prefill/routed_expert_ffn/routed_expert_ffn_nanobind.hpp"
 #include "ttnn/operations/experimental/deepseek_prefill/unified_routed_expert_ffn/unified_routed_expert_ffn_nanobind.hpp"
@@ -217,7 +216,6 @@ void py_module(nb::module_& mod) {
     moe_gpt::detail::bind_moe_gpt(mod);
 
     // DeepSeek prefill MoE operations
-    deepseek_prefill::detail::bind_compressor_state_exchange(mod);
     deepseek_prefill::detail::bind_csa_compressor(mod);
     deepseek_prefill::detail::bind_dispatch(mod);
     deepseek_prefill::detail::bind_combine(mod);
