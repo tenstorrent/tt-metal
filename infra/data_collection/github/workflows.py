e43a00521209050de55839f62d246dc388a24b3d
William Ly <williamly@tenstorrent.com>
2026-09-08 19:51:20 -0400

    disable civ2 annotation download

--- infra/data_collection/github/workflows.py
+++ infra/data_collection/github/workflows.py
@@ -465,8 +465,8 @@ def get_civ2_node_name_and_serial_from_job_log(workflow_outputs_dir, workflow_ru
     From the API, we can always see the annotations.
     E.g. https://api.github.com/repos/tenstorrent/tt-metal/check-runs/<job id>/annotations
 
-    So we should never have to exercise this fallback parser because we always generate pipeline data on each run attempt's jobs only.
-    (Unless github has an outage and doesn't upload annotations for some reason)
+    09/08/2026: Due to Github rate limits, we only download annotations for failed jobs. 
+    This is now the usual path to resolve the host_name for CIv2 (tt-ubuntu) runners.
 
     Returns (node_name, serial), each None if not found (CPU-only runners have no serial).
     """
