2426c7f77629ea93bea08b72fbb68fb1da667dec
Matt Craighead <mcraighead@tenstorrent.com>
2026-09-08 12:00:02 -0500

    bug fix

--- tt_metal/tt-llk/tests/python_tests/fuser/l1_operation.py
+++ tt_metal/tt-llk/tests/python_tests/fuser/l1_operation.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional, Tuple
 
 from helpers.llk_params import (
