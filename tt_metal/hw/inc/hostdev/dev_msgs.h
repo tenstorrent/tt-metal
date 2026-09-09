e5246e58631eb08b298e32e326ac2b49a19ed8bc
Nemanja Divnic <ndivnic@tenstorrent.com>
2026-09-09 10:04:24 +0000

    Fix.

--- tt_metal/hw/inc/hostdev/dev_msgs.h
+++ tt_metal/hw/inc/hostdev/dev_msgs.h
@@ -447,7 +447,7 @@ struct mailboxes_t {
 };
 
 // DevicePrintMemoryLayout asserts
-static_assert(sizeof(DevicePrintMemoryLayout) == DPRINT_BUFFER_SIZE * PROCESSOR_COUNT);
+static_assert(sizeof(DevicePrintMemoryLayout) == DEVICE_PRINT_BUFFER_SIZE);
 static_assert(sizeof(DevicePrintMemoryLayout) % 4 == 0);
 #if defined(ARCH_WORMHOLE) || defined(ARCH_BLACKHOLE)
 static_assert(decltype(DevicePrintMemoryLayout::buffer)::processor_count == PROCESSOR_COUNT);
