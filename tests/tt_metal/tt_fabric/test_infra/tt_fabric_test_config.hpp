81e4e1064974bbadbac6bd413ec66f654b1130c9
Neel Nyamagoudar <nnyamagoudar@tenstorrent.com>
2026-09-09 22:22:27 +0000

    more ci testing pushes

--- tests/tt_metal/tt_fabric/test_infra/tt_fabric_test_config.hpp
+++ tests/tt_metal/tt_fabric/test_infra/tt_fabric_test_config.hpp
@@ -544,7 +544,10 @@ private:
     void expand_all_devices_uniform_pattern(ParsedTestConfig& test, const ParsedTrafficPatternConfig& base_pattern);
 
     void expand_one_or_all_to_all_multicast(
-        ParsedTestConfig& test, const ParsedTrafficPatternConfig& base_pattern, HighLevelTrafficPattern pattern_type);
+        ParsedTestConfig& test,
+        const ParsedTrafficPatternConfig& base_pattern,
+        HighLevelTrafficPattern pattern_type,
+        MeshTrafficScope mesh_scope = MeshTrafficScope::ALL);
 
     void expand_unidirectional_linear_unicast_or_multicast(
         ParsedTestConfig& test, const ParsedTrafficPatternConfig& base_pattern);
