442904519b12d2206375773cee30288fa707837b
Jash <jashambaliya1@gmail.com>
2026-09-10 23:36:08 +0530

    Propagate the sqrt_custom non-finite guard to the Quasar kernel

--- tt_metal/hw/ckernels/quasar/metal/llk_api/llk_sfpu/ckernel_sfpu_sqrt_custom.h
+++ tt_metal/hw/ckernels/quasar/metal/llk_api/llk_sfpu/ckernel_sfpu_sqrt_custom.h
@@ -15,7 +15,9 @@ template <bool APPROXIMATION_MODE>
 sfpi_inline sfpi::vFloat sfpu_sqrt_custom(sfpi::vFloat in) {
     sfpi::vFloat val = in;
     sfpi::vFloat out = val;
-    v_if(val != 0.0f) {
+    // Exclude non-finite: the +inf seed squares to a denormal, SFPMAD flushes it to +0, and
+    // 0 * -inf = NaN. Same guard as Wormhole and Blackhole, see #53757.
+    v_if(val != 0.0f && sfpi::exexp(val, sfpi::ExponentMode::Biased) != 255) {
         // Fast inverse square-root seed + two Newton-Raphson refinements.
         sfpi::vUInt magic = sfpi::as<sfpi::vUInt>(sfpi::vFloat(sfpi::sFloat16b(0x5f37)));
         sfpi::vFloat approx = sfpi::as<sfpi::vFloat>(magic - (sfpi::as<sfpi::vUInt>(val) >> 1));
