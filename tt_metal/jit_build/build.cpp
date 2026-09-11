b77acde6a8e8ecea04f99f8b4dc2f6997df55536
blozano-tt <blozano@tenstorrent.com>
2026-09-11 16:04:07 -0700

    [Performance] Reduce RISC-V kernel debug info to -g1

--- tt_metal/jit_build/build.cpp
+++ tt_metal/jit_build/build.cpp
@@ -183,7 +183,7 @@ void JitBuildEnv::init(
     }
 
     if (rtoptions.get_riscv_debug_info_enabled()) {
-        common_flags += "-g ";
+        common_flags += "-g1 ";
     }
 
     this->cflags_ = common_flags;
