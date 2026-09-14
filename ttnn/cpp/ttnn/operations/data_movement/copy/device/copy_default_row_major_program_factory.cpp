da16b90545e7b0fb20d9e22a6e60fb8160da4853
Momcilo Vasilijevic <mvasilijevic@tenstorrent.com>
2026-09-14 07:20:09 +0000

    fix(ttnn): include alignment API for adapter copy

--- ttnn/cpp/ttnn/operations/data_movement/copy/device/copy_default_row_major_program_factory.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/copy/device/copy_default_row_major_program_factory.cpp
@@ -12,6 +12,7 @@
 #include <tt-metalium/constants.hpp>
 #include <tt-metalium/core_coord.hpp>
 #include <tt-metalium/math.hpp>
+#include <tt-metalium/tt_align.hpp>
 #include <tt-metalium/work_split.hpp>
 
 #include <tt-metalium/experimental/metal2_host_api/dataflow_buffer_spec.hpp>
