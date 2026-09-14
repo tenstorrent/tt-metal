3d681fcdcd0dbb9e758fa215b33f7cff2f70c3cd
Gagan Deep Singh <gagandeepsingh@tenstorrent.com>
2026-09-14 17:37:03 +0000

    BMM metal tests w/ transpose

--- tests/tt_metal/tt_metal/test_kernels/compute/bmm.cpp
+++ tests/tt_metal/tt_metal/test_kernels/compute/bmm.cpp
@@ -16,32 +16,33 @@ using std::uint32_t;
 void kernel_main() {
     constexpr int onetile = 1;
 
-    constexpr uint32_t batch = get_arg(args::batch);
-    constexpr uint32_t Mt = get_arg(args::Mt);
-    constexpr uint32_t Kt = get_arg(args::Kt);
-    constexpr uint32_t Nt = get_arg(args::Nt);
+    constexpr std::uint32_t batch = get_arg(args::batch);
+    constexpr std::uint32_t Mt = get_arg(args::Mt);
+    constexpr std::uint32_t Kt = get_arg(args::Kt);
+    constexpr std::uint32_t Nt = get_arg(args::Nt);
+    constexpr std::uint32_t transpose = get_arg(args::transpose);
 
-    uint32_t compute_id = get_my_thread_id();
-    uint32_t num_threads = get_num_threads();
+    std::uint32_t compute_id = get_my_thread_id();
+    std::uint32_t num_threads = get_num_threads();
 
     DataflowBuffer dfb0(dfb::src0);
     DataflowBuffer dfb1(dfb::src1);
     DataflowBuffer dfb_out(dfb::dst);
 
     compute_kernel_hw_startup<SrcOrder::Reverse>(dfb::src0, dfb::src1, dfb::dst);
-    matmul_init(dfb::src0, dfb::src1);
+    matmul_init(dfb::src0, dfb::src1, transpose);
 
     // the simplest possible version of outer product blocked matmul
     // the reader is expected to read the A's and B's tile rows and tile columns for each output tile
-    for (uint32_t nb = 0; nb < batch; nb++) {
-        for (uint32_t mt_C = 0; mt_C < Mt; ++mt_C) {  // output tile of C
+    for (std::uint32_t nb = 0; nb < batch; nb++) {
+        for (std::uint32_t mt_C = 0; mt_C < Mt; ++mt_C) {  // output tile of C
             if (mt_C % num_threads != compute_id) {
                 continue;
             }
-            for (uint32_t nt_C = 0; nt_C < Nt; ++nt_C)  // output tile index of C
+            for (std::uint32_t nt_C = 0; nt_C < Nt; ++nt_C)  // output tile index of C
             {
                 tile_regs_acquire();
-                for (uint32_t kt = 0; kt < Kt; kt++) {
+                for (std::uint32_t kt = 0; kt < Kt; kt++) {
                     dfb0.wait_front(onetile);
                     dfb1.wait_front(onetile);
                     matmul_tiles(dfb::src0, dfb::src1, 0, 0, 0);
