4178d9c03f85a4139dfea25d7a666ca50fbd7eae
blozano-tt <blozano@tenstorrent.com>
2026-09-14 22:44:02 -0700

    Break device assertion header include cycle

--- tt_metal/hw/inc/api/debug/assert.h
+++ tt_metal/hw/inc/api/debug/assert.h
@@ -11,9 +11,11 @@
 #include "internal/tt-2xx/quasar/error_handling.h"
 #endif
 
-// assert_and_hang below needs flush_l2_cache_range from here. This header includes us back, so
-// ASSERT must already be defined above.
-#include "risc_common.h"
+#if defined(ARCH_QUASAR) && defined(COMPILE_FOR_DM)
+#include "internal/tt-2xx/quasar/cache.h"
+#elif defined(ARCH_QUASAR) && defined(COMPILE_FOR_TRISC)
+#include "tensix.h"
+#endif
 
 #if defined(WATCHER_ENABLED) && !defined(WATCHER_DISABLE_ASSERT) && !defined(FORCE_WATCHER_OFF)
 
