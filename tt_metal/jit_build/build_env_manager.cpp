724d77c51225aa2bcbe53c1c6d87a6dfe578cba4
Samarth Agarwal <sagarwal@tenstorrent.com>
2026-09-15 19:37:05 +0000

    Saving

--- tt_metal/jit_build/build_env_manager.cpp
+++ tt_metal/jit_build/build_env_manager.cpp
@@ -168,6 +168,8 @@ uint64_t compute_build_key(const JitDeviceConfig& config, const llrt::RunTimeOpt
     hasher.update(static_cast<uint32_t>(config.resolved_dispatch_core_type));
     hasher.update(static_cast<uint32_t>(config.dispatch_core_axis));
 
+    hasher.update(static_cast<uint32_t>(config.fds_signalling));
+
     // Hash the number of hardware command queues
     hasher.update(static_cast<uint32_t>(config.num_hw_cqs));
 
