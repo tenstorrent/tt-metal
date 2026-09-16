bfc0f1c40e3a5dc67b0f681331fe41dc77a280ac
Gagan Deep Singh <gagandeepsingh@tenstorrent.com>
2026-09-16 13:08:44 +0000

    Removed deleted file reference from CMake

--- tt_metal/hw/sources.cmake
+++ tt_metal/hw/sources.cmake
@@ -282,7 +282,6 @@ set(HW_JIT_API_HEADERS
     inc/internal/tt-2xx/dataflow_buffer/dataflow_buffer_init.h
     inc/internal/tt-2xx/dataflow_buffer/dataflow_buffer_interface.h
     inc/internal/tt-2xx/dataflow_buffer/dataflow_buffer_isr.h
-    inc/internal/tt-2xx/quasar/c_tensix_core.h
     inc/internal/tt-2xx/quasar/cfg_defines.h
     inc/internal/tt-2xx/quasar/core_config.h
     inc/internal/tt-2xx/quasar/dev_mem_map.h
