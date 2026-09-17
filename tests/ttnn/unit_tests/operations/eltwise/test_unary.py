cf7491663ebe8a0c701b2bca951eb8622de70dca
abdulla-TT <ctr-mohammedaa@ext.tenstorrent.com>
2026-09-17 16:50:55 +0000

    #56823: remove test

--- tests/ttnn/unit_tests/operations/eltwise/test_unary.py
+++ tests/ttnn/unit_tests/operations/eltwise/test_unary.py
@@ -626,44 +626,6 @@ def run_unary_test_with_float_remainder(device, h, w, scalar, ttnn_function, ulp
     assert_with_ulp(expected_result=torch_output_tensor, actual_result=output_tensor, ulp_threshold=ulp)
 
 
-@pytest.mark.parametrize("lower_limit", [0, 1.0, 2, -5.5])
-@pytest.mark.parametrize("h", [64])
-@pytest.mark.parametrize("w", [128])
-@pytest.mark.parametrize("dtype", [ttnn.bfloat16])
-def test_relu_min(device, h, w, lower_limit, dtype):
-    torch.manual_seed(0)
-
-    torch_input_tensor = torch.rand((h, w), dtype=torch.bfloat16)
-
-    golden_function = ttnn.get_golden_function(ttnn.relu_min)
-    torch_output_tensor = golden_function(torch_input_tensor, lower_limit=lower_limit)
-
-    input_tensor = ttnn.from_torch(torch_input_tensor, dtype=dtype, layout=ttnn.TILE_LAYOUT, device=device)
-    output_tensor = ttnn.relu_min(input_tensor, lower_limit)
-    output_tensor = ttnn.to_torch(output_tensor)
-
-    assert torch.equal(torch_output_tensor, output_tensor)
-
-
-@pytest.mark.parametrize("upper_limit", [0, 1.0, 2, -5.5])
-@pytest.mark.parametrize("h", [64])
-@pytest.mark.parametrize("w", [128])
-@pytest.mark.parametrize("dtype", [ttnn.bfloat16])
-def test_relu_max(device, h, w, upper_limit, dtype):
-    torch.manual_seed(0)
-
-    torch_input_tensor = torch.rand((h, w), dtype=torch.bfloat16)
-
-    golden_function = ttnn.get_golden_function(ttnn.relu_max)
-    torch_output_tensor = golden_function(torch_input_tensor, upper_limit=upper_limit)
-
-    input_tensor = ttnn.from_torch(torch_input_tensor, dtype=dtype, layout=ttnn.TILE_LAYOUT, device=device)
-    output_tensor = ttnn.relu_max(input_tensor, upper_limit)
-    output_tensor = ttnn.to_torch(output_tensor)
-
-    assert torch.equal(torch_output_tensor, output_tensor)
-
-
 @pytest.mark.parametrize("input_shapes", [torch.Size([1, 2, 32, 128])])
 @pytest.mark.parametrize("value_ranges", [INT32_BANDED_RANGES])
 @pytest.mark.parametrize("upper_limit", [-(2**31), -(2**30), -7, 0, 7, 1000, 2**24 + 1, 2**30, 2147483647])
