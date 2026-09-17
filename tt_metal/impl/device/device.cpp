fa1769e0c10ebe290a4acc966ddcadff1e17c2ff
Sohaib Nadeem <sohaibnadeem@tenstorrent.com>
2026-09-17 23:06:36 +0000

    fixes

--- tt_metal/impl/device/device.cpp
+++ tt_metal/impl/device/device.cpp
@@ -821,7 +821,7 @@ CoreCoord Device::worker_core_from_logical_core(const CoreCoord& logical_core) c
 
 CoreCoord Device::logical_core_from_worker_core(const CoreCoord& virtual_coord) const {
     const auto& soc_desc = MetalEnvAccessor(*env_).impl().get_cluster().get_soc_desc(this->id_);
-    tt::umd::CoreCoord coord{{virtual_coord.x, virtual_coord.y}, CoreType::WORKER, tt::CoordSystem::TRANSLATED};
+    tt::umd::CoreCoord coord{{virtual_coord.x, virtual_coord.y}, tt::CoreType::TENSIX, tt::CoordSystem::TRANSLATED};
     auto logical = soc_desc.translate_coord_to(coord, tt::CoordSystem::LOGICAL);
     return CoreCoord{logical.x, logical.y};
 }
