a613c8717c2b6f4c2bac4d0bfa2f08e4e4a95e16
Nikola Stojic <nstojic@tenstorrent.com>
2026-09-17 02:21:30 +0200

    bisect: measure with speed of light off

--- tt_metal/tt-llk/tests/sources/matmul_test.cpp
+++ tt_metal/tt-llk/tests/sources/matmul_test.cpp
@@ -249,6 +249,7 @@ void run_kernel(RUNTIME_PARAMETERS params)
                         "Block tile index exceeds maximum destination tiles for matmul");
                     _llk_pack_<dest_sync, is_fp32_dest_acc_en, ckernel::PackMode::Default>(tile_index, PERF_ADDRESS(PERF_OUTPUT, tile_index));
                 }
+                TTI_STALLWAIT(p_stall::STALL_THREAD, p_stall::PACK);
             }
         }
         else
