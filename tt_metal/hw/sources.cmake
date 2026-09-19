786a31c119b2899fae400aedd37ac537f2bb1a42
Nachiket Kapre <nkapre@tenstorrent.com>
2026-09-18 20:22:09 -0400

    Keep shared LLK install entries clear of upstream API additions

--- tt_metal/hw/sources.cmake
+++ tt_metal/hw/sources.cmake
@@ -1,6 +1,4 @@
 set(HW_JIT_API_HEADERS
-    ckernels/common/llk_sfpu/ckernel_sfpu_erfinv_bf16.h
-    ckernels/common/llk_sfpu/ckernel_sfpu_tt_poly_log_square_factorized_odd.h
     inc/experimental/drisc_mode.h
     inc/experimental/gddr_dma.h
     inc/experimental/blaze_rt_arg.h
@@ -345,4 +343,6 @@ set(HW_JIT_API_HEADERS
     toolchain/erisc-b0-app-sections.ld
     toolchain/erisc-b0-memory.ld
     toolchain/erisc-b0-kernel.ld
+    ckernels/common/llk_sfpu/ckernel_sfpu_erfinv_bf16.h
+    ckernels/common/llk_sfpu/ckernel_sfpu_tt_poly_log_square_factorized_odd.h
 )
