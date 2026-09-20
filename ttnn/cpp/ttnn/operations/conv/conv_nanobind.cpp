281fa07e9bafbe7e2219d4a2c643e3eb585d530b
Marko Staletovic <mstaletovic@tenstorrent.com>
2026-09-20 23:20:58 +0000

    Nuke conv2d for agent evaluation

--- ttnn/cpp/ttnn/operations/conv/conv_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/conv/conv_nanobind.cpp
@@ -6,7 +6,6 @@
 
 #include <nanobind/nanobind.h>
 
-#include "conv2d/conv2d_nanobind.hpp"
 #include "conv_transpose2d/conv_transpose2d_nanobind.hpp"
 #include "conv1d/conv1d_nanobind.hpp"
 
@@ -14,7 +13,6 @@ namespace ttnn::operations::conv {
 
 void py_module(nb::module_& mod) {
     ttnn::operations::conv::conv1d::bind_conv1d(mod);
-    ttnn::operations::conv::conv2d::bind_conv2d(mod);
     ttnn::operations::conv::conv_transpose2d::bind_conv_transpose2d(mod);
 }
 }  // namespace ttnn::operations::conv
