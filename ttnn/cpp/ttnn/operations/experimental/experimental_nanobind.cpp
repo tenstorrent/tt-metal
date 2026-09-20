865d363f565f5a7e07003a166a1022a68c063c4b
Marko Staletovic <mstaletovic@tenstorrent.com>
2026-09-20 23:20:54 +0000

    Nuke indexer_score (ring indexer) for agent evaluation

--- ttnn/cpp/ttnn/operations/experimental/experimental_nanobind.cpp
+++ ttnn/cpp/ttnn/operations/experimental/experimental_nanobind.cpp
@@ -72,7 +72,6 @@
 #include "ttnn/operations/experimental/deepseek/mla/matmul_wo/matmul_wo_nanobind.hpp"
 #include "ttnn/operations/experimental/deepseek/moe/deepseek_moe_gate/deepseek_moe_gate_nanobind.hpp"
 #include "ttnn/operations/experimental/deepseek/moe/generalized_moe_gate/generalized_moe_gate_nanobind.hpp"
-#include "ttnn/operations/experimental/indexer_score/indexer_score_nanobind.hpp"
 #include "ttnn/operations/experimental/kda/kda_nanobind.hpp"
 #include "ttnn/operations/experimental/ccl/moe_gpt/moe_gpt_nanobind.hpp"
 #include "ttnn/operations/experimental/deepseek_prefill/dispatch/dispatch_nanobind.hpp"
@@ -208,7 +207,6 @@ void py_module(nb::module_& mod) {
     topk_large_indices::detail::bind_topk_large_indices(mod);
     topk_router_gpt::detail::bind_topk_router_gpt(mod);
     deepseek::mla::detail::bind_matmul_wo(mod);
-    indexer_score::detail::bind_indexer_score(mod);
     kda::detail::bind_kda(mod);
     moe_gpt::detail::bind_moe_gpt(mod);
 
