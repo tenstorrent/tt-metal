872d9910a90fb39e7c0d940d125d4079a71dd71e
blozano-tt <blozano@tenstorrent.com>
2026-09-21 16:30:25 -0700

    Replace reserved identifiers in include guards

--- tests/tt_metal/tt_metal/deployment/eth/common.hpp
+++ tests/tt_metal/tt_metal/deployment/eth/common.hpp
@@ -2,8 +2,8 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#ifndef _ETH_COMMON_HPP
-#define _ETH_COMMON_HPP
+#ifndef ETH_COMMON_HPP
+#define ETH_COMMON_HPP
 
 #include <chrono>
 
@@ -939,4 +939,4 @@ static bool ensure_links(std::span<std::shared_ptr<distributed::MeshDevice>> dev
 
 }  // namespace tt::tt_metal
 
-#endif /* _ETH_COMMON_HPP */
+#endif /* ETH_COMMON_HPP */
