61e9819f37e3ce5653afe47e1b893ff602975dec
Marko Radosavljevic <markoradosavljevic@tenstorrent.com>
2026-09-21 12:49:35 +0000

    fix comment

--- tt_metal/tt-llk/tt_llk_quasar/llk_lib/llk_math_reduce.h
+++ tt_metal/tt-llk/tt_llk_quasar/llk_lib/llk_math_reduce.h
@@ -378,7 +378,7 @@ inline void _llk_math_reduce_scalar_mop_config_(const TensorShape& tensor_shape)
         0,
         [tensor_shape]
         {
-            // Set up a dest addr to output temp results into, has to be less than 64 (to not write into next tile)
+            // Set up a dest addr to output temp results into, has to be less than 16 (to not write into next tile)
             // but also has to be greater than 0 (where results are expected)
             constexpr std::uint32_t scratch_dst_addr = 8;
 
