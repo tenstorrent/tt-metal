9d3401e1a55614952bd3e3379a2453a48dfeddb5
Iva Potkonjak <ipotkonjak@tenstorrent.com>
2026-09-23 08:06:06 +0000

    Revert "Enable Fabric express link routing (#48280)"
    This reverts commit eaa4a2ac610fa14e91b2c56fef2b73c0f85967cc.

--- tt_metal/fabric/fabric_router_builder.cpp
+++ tt_metal/fabric/fabric_router_builder.cpp
@@ -15,18 +15,17 @@ std::unique_ptr<FabricRouterBuilder> FabricRouterBuilder::create(
     tt::tt_metal::IDevice* device,
     tt::tt_metal::Program& program,
     FabricNodeId local_node,
-    const RouterLocation& location,
-    const ChipRoutingFacts& chip_facts) {
+    const RouterLocation& location) {
     // Query fabric context to determine router type
     const auto& fabric_context = tt::tt_metal::MetalContext::instance().get_control_plane().get_fabric_context();
     bool is_switch_mesh = fabric_context.is_switch_mesh(local_node.mesh_id);
 
     if (is_switch_mesh) {
-        return SwitchMeshRouterBuilder::build(device, program, local_node, location, chip_facts);
+        return SwitchMeshRouterBuilder::build(device, program, local_node, location);
     }
 
     // Create compute mesh router - it handles its own config lookup
-    return ComputeMeshRouterBuilder::build(device, program, local_node, location, chip_facts);
+    return ComputeMeshRouterBuilder::build(device, program, local_node, location);
 }
 
 }  // namespace tt::tt_fabric
