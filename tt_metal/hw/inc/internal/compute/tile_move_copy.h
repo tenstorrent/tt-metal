db3889d9f2b0f8bf46bb18910fd4cdd8b57a6759
ke1rro <nikitalenyk2@gmail.com>
2026-09-23 17:25:20 +0000

    fix(llk): validate narrow unpack-to-dest copies

--- tt_metal/hw/inc/internal/compute/tile_move_copy.h
+++ tt_metal/hw/inc/internal/compute/tile_move_copy.h
@@ -12,7 +12,7 @@ namespace ckernel::internal {
 ALWI void copy_tile_to_dst_32x16(uint32_t in_cb_id, uint32_t in_tile_index, uint32_t dst_tile_index) {
     UNPACK((llk_unpack_A<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, UnpackToDestEn>(
         in_cb_id, in_tile_index)));
-    MATH((llk_math_eltwise_unary_datacopy_to_dest_32x16(dst_tile_index)));
+    MATH((llk_math_eltwise_unary_datacopy_to_dest_32x16(dst_tile_index, in_cb_id)));
 }
 
 }  // namespace ckernel::internal
