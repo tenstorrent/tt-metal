b3630240aba0a5ed670bfa365a5693611e1c3bda
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-09-25 18:55:01 +0000

    fudge quasar one

--- tt_metal/tt-llk/tt_llk_quasar/common/inc/ckernel_ops.h
+++ tt_metal/tt-llk/tt_llk_quasar/common/inc/ckernel_ops.h
@@ -9,8 +9,10 @@
 #define TT_INSN(ENCODING)  void(::ckernel::instrn_buffer[0] = unsigned(ENCODING))
 #elif defined(ARCH_QUASAR) && defined(LLK_TEST)
 // The llk test infra compiles this on brisc and somehow executes it. So icky.
-#define TTI_INSN(ENCODING) ({ __asm__ __volatile__(".word (3 & (%0 >> 30)) | (0xfffffffc & (%0 << 2))" : : "n"(unsigned(ENCODING))); })
-#define TT_INSN(ENCODING)  ({ __asm__ __volatile__(".error \"TT_INSN in non-tensix code\"" ::"X"(unsigned(ENCODING))); })
+// #define TTI_INSN(ENCODING) ({ __asm__ __volatile__(".word (3 & (%0 >> 30)) | (0xfffffffc & (%0 << 2))" : : "n"(unsigned(ENCODING))); })
+#define TTI_INSN(ENCODING) ({ __asm__ __volatile__(".ttinsn %0" : : "n"(unsigned(ENCODING))); })
+// #define TT_INSN(ENCODING)  ({ __asm__ __volatile__(".error \"TT_INSN in non-tensix code\"" ::"X"(unsigned(ENCODING))); })
+#define TT_INSN(ENCODING)  void(::ckernel::instrn_buffer[0] = unsigned(ENCODING))
 #else
 #define TTI_INSN(ENCODING) ({ __asm__ __volatile__(".error \"TTI_INSN in non-tensix code\"" ::"X"(unsigned(ENCODING))); })
 #define TT_INSN(ENCODING)  ({ __asm__ __volatile__(".error \"TT_INSN in non-tensix code\"" ::"X"(unsigned(ENCODING))); })
