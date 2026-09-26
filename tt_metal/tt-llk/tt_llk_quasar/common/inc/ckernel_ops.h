0514563270bd8467300046db694aa8384c4bdf97
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-09-26 00:49:43 +0000

    shift in C

--- tt_metal/tt-llk/tt_llk_quasar/common/inc/ckernel_ops.h
+++ tt_metal/tt-llk/tt_llk_quasar/common/inc/ckernel_ops.h
@@ -18,7 +18,8 @@
 #define TTI_INSN(ENCODING) ({ __asm__ __volatile__(".ttinsn %0" ::"n"(unsigned(ENCODING))); })
 #else
 // fails
-#define TTI_INSN(ENCODING) ({ __asm__ __volatile__(".4byte (3 & (%0 >> 30)) | (0xfffffffc & (%0 << 2))" ::"n"(unsigned(ENCODING))); })
+// #define TTI_INSN(ENCODING) ({ __asm__ __volatile__(".4byte (3 & (%0 >> 30)) | (0xfffffffc & (%0 << 2))" ::"n"(unsigned(ENCODING))); })
+#define TTI_INSN(ENCODING) ({ __asm__ __volatile__(".word %0" ::"n"((((ENCODING) >> 30) & 3) | (((ENCODING) & 0x3ffffff) << 2))); })
 #endif
 #define TT_INSN(ENCODING)  ({ __asm__ __volatile__(".error \"TT_INSN in non-tensix code\"" ::"X"(unsigned(ENCODING))); })
 #endif
